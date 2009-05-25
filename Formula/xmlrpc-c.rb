21bbb46ae6f3b16962701c836d41bde791baba06
Max Howell <max@methylblue.com>
2009-05-25 11:49:36 +0100

    xmlrpc-c won't build -j2

--- Formula/xmlrpc-c.rb
+++ Formula/xmlrpc-c.rb
@@ -6,6 +6,7 @@ url='http://kent.dl.sourceforge.net/sourceforge/xmlrpc-c/xmlrpc-c-1.06.33.tgz'
 md5='7dda4d8c5d26ae877d3809e428ce7962'
 
 Formula.new(url, md5).brew do |prefix|
+  ENV['MAKEFLAGS']='' #parallel build doesn't work
   # choosing --enable-libxml2-backend to lose some weight and not statically
   # link in expat
   #NOTE seemingly it isn't possible to build dylibs with this thing
