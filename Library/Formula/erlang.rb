0c0c36732c1f03a68a0fdfcb3a7ad5874f67c98e
Max Howell <max@methylblue.com>
2009-06-05 23:38:23 +0100

    Erlang formula

---
+++ Library/Formula/erlang.rb
@@ -0,0 +1,14 @@
+require 'brewkit'
+
+class Erlang <Formula
+  @homepage='http://www.erlang.org'
+  @url='http://erlang.org/download/otp_src_R13B.tar.gz'
+  @md5='6d8c256468a198458b9f08ba6aa1a384'
+
+  def install
+    ENV['MAKEFLAGS']='' #parallel not work
+    system "./configure --disable-debug --prefix='#{prefix}' --enable-kernel-poll --enable-threads --enable-dynamic-ssl-lib --enable-smp-support --enable-hipe"
+    system "make"
+    system "make install"
+  end
+end
