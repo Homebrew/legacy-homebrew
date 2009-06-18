7a0aede96287c1bda17a61b0626e9ce45a29ab59
Max Howell <max@methylblue.com>
2009-06-18 09:53:36 +0100

    libvorbis formula

---
+++ Library/Formula/libvorbis.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Libvorbis <Formula
+  @url='http://downloads.xiph.org/releases/vorbis/libvorbis-1.2.2rc1.tar.bz2'
+  @md5='6a7086ee666b8c62e122d29d107f7bec'
+  @homepage='http://vorbis.com'
+
+  def deps
+    test_lib 'ogg'
+  end
+
+  def install
+    system "./configure --enable-docs --disable-debug --disable-dependency-tracking --prefix='#{prefix}'"
+    system "make install"
+  end
+end
