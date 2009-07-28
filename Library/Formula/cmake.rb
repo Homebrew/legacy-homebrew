691ba568381539b5023983ae7966791db25b04b9
Max Howell <max@methylblue.com>
2009-07-28 02:28:46 +0100

    CMake 2.6.4
    Also removed xmlrpc-c dep, by coercing cmake into using its own xmlrpc
    copy. It statically linked xmlrpc before anyway, so it's better for our
    users not to have this crappy dependency.
    
    Still completely in the dark about how cmake can possibly need an
    xmlrpc library!

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -1,15 +1,18 @@
 require 'brewkit'
 
 class Cmake <Formula
-  @url='http://www.cmake.org/files/v2.6/cmake-2.6.3.tar.gz'
-  @md5='5ba47a94ce276f326abca1fd72a7e7c6'
+  @url='http://www.cmake.org/files/v2.6/cmake-2.6.4.tar.gz'
+  @md5='50f387d0436696c4a68b5512a72c9cde'
   @homepage='http://www.cmake.org/'
 
-  def deps
-    LibraryDep.new 'xmlrpc', 'xmlrpc-c'
-  end
-
   def install
+    # xmlrpc is a stupid little library, rather than waste our users' time
+    # just let cmake use its own copy. God knows why something like cmake
+    # needs an xmlrpc library anyway! It is amazing!
+    inreplace 'CMakeLists.txt',
+              "# Mention to the user what system libraries are being used.",
+              "SET(CMAKE_USE_SYSTEM_XMLRPC 0)"
+
     system "./bootstrap --prefix=#{prefix} --system-libs --datadir=/share/cmake --docdir=/share/cmake --mandir=/share/man"
     system "make install"
 
