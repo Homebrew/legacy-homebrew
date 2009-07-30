c6d6902af69ea44f61bd32ea1fe2ce4ed71d7714
Max Howell <max@methylblue.com>
2009-07-30 21:35:22 +0100

    Git 1.6.4

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -1,19 +1,23 @@
 require 'brewkit'
 
 class GitManuals <UnidentifiedFormula
-  @url='http://www.kernel.org/pub/software/scm/git/git-manpages-1.6.3.3.tar.bz2'
-  @md5='36be16310d1e24f23c966c8e17a499d7'
+  @url='http://www.kernel.org/pub/software/scm/git/git-manpages-1.6.4.tar.bz2'
+  @md5='851e1df833895c5046a994c28d3d8368'
 end
 
 class Git <Formula
-  @url='http://www.kernel.org/pub/software/scm/git/git-1.6.3.3.tar.bz2'
-  @md5='91ae46ac01dadab1962beb064abd5b60'
+  @url='http://kernel.org/pub/software/scm/git/git-1.6.4.tar.bz2'
+  @md5='d41e3d43c8f1d23024fb3d3fe6aa3858'
   @homepage='http://git-scm.com'
 
   def install
     # the manuals come separately, well sort of, it's easier this way though
     man.mkpath
     GitManuals.new.brew { FileUtils.mv Dir['*'], man }
+
+    # if these things are installed, tell git build system to not use them
+    ENV['NO_FINK']='1'
+    ENV['NO_DARWIN_PORTS']='1'
 
     system "./configure --prefix='#{prefix}' --disable-debug"
     system "make install"
