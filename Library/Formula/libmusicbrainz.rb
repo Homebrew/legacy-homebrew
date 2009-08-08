7aa95191dde546990ea48978634b1c919a1b57a7
Max Howell <max@methylblue.com>
2009-08-08 13:52:48 +0100

    Neon and libmusicbrainz formulae

---
+++ Library/Formula/libmusicbrainz.rb
@@ -0,0 +1,17 @@
+require 'brewkit'
+
+class Libmusicbrainz <Formula
+  @url='http://ftp.musicbrainz.org/pub/musicbrainz/libmusicbrainz-3.0.2.tar.gz'
+  @homepage='http://musicbrainz.org'
+  @md5='648ecd43f7b80852419aaf73702bc23f'
+
+  def deps
+    LibraryDep.new 'neon'
+    OptionalLibraryDep.new 'libdiscid'
+  end
+
+  def install
+    system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
