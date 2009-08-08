814ef547b14e2010677c65687a950eb83794be00
Max Howell <max@methylblue.com>
2009-08-08 14:08:13 +0100

    FIX ImageMagick formula

--- Library/Formula/imagemagick.rb
+++ Library/Formula/imagemagick.rb
@@ -1,8 +1,8 @@
 require 'brewkit'
 
 class Imagemagick <Formula
-  @url='ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.5.3-10.tar.bz2'
-  @md5='ad485ed0eca5eb8b8a1649bae40cec7d'
+  @url='ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.5.4-9.tar.bz2'
+  @md5='25391113fbcd5642b67949756fd078cb'
   @homepage='http://www.imagemagick.org'
 
   def deps
@@ -10,11 +10,27 @@ class Imagemagick <Formula
   end
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--without-maximum-compile-warnings",
-                          "--prefix=#{prefix}"
+    ENV.libpng
     ENV.deparallelize
+
+    # versioned stuff in main tree is pointless for us
+    inreplace 'configure', '${PACKAGE_NAME}-${PACKAGE_VERSION}', '${PACKAGE_NAME}'
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--without-maximum-compile-warnings",
+                          "--prefix=#{prefix}",
+                          "--disable-osx-universal-binary",
+                          "--without-perl" # I couldn't make this compile
     system "make install"
+
+    # We already copy these in
+    d=prefix+'share'+'ImageMagick'
+    (d+'NEWS.txt').unlink
+    (d+'LICENSE').unlink
+    (d+'ChangeLog').unlink
+  end
+
+  def caveats
+    "This package is a bit of a mess, lots of components don't get compiled."
   end
 end
