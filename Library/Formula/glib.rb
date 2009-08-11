ba8ac7d02ddb6e7782e8645bae64d36a1143e979
Max Howell <max@methylblue.com>
2009-08-11 01:31:47 +0100

    Some frivalous mods to glib

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -5,12 +5,22 @@ class Glib <Formula
   @md5='1173688c58b4b62809c83bb07a2cf71a'
   @homepage='http://www.gtk.org'
 
+  def deps
+    BinaryDep.new 'pkg-config'
+    LibraryDep.new 'gettext'
+  end
+
   def install
     # indeed, amazingly, -w causes gcc to emit spurious errors for this package!
     ENV.enable_warnings
 
-    system "./configure --disable-debug --prefix='#{prefix}' --disable-dependency-tracking"
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--disable-rebuilds"
     system "make"
     system "make install"
+    
+    (prefix+'share'+'gtk-doc').rmtree
   end
 end
