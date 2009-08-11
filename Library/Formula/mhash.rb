778dc398e60b5620f520e9da3a7d102a487c070b
Max Howell <max@methylblue.com>
2009-08-11 23:42:20 +0100

    Mhash library formula for scoates at ##homebrew

---
+++ Library/Formula/mhash.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Mhash <Formula
+  @url='http://sourceforge.net/projects/mhash/files/mhash/0.9.9.9/mhash-0.9.9.9.tar.gz'
+  @homepage='http://mhash.sourceforge.net/'
+  @md5='ee66b7d5947deb760aeff3f028e27d25'
+
+  def install
+    system "./configure --prefix=#{prefix} --disable-debug --disable-dependency-tracking"
+    system "make install"
+  end
+end
