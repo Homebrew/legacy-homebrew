7acc39efeb83f8830931332cfd4e602d2a8d06d2
Adam Vandenberg <flangy@gmail.com>
2009-08-24 16:28:12 -0700

    Bump sqlite version.

--- Library/Formula/sqlite.rb
+++ Library/Formula/sqlite.rb
@@ -1,8 +1,8 @@
 require 'brewkit'
 
 class Sqlite <Formula
-  @url='http://www.sqlite.org/sqlite-amalgamation-3.6.16.tar.gz'
-  @md5='f21ebadcca1e931212000661e64bb20c'
+  @url='http://www.sqlite.org/sqlite-amalgamation-3.6.17.tar.gz'
+  @md5='3172f8a23e7e7f0e5b295062e339a149'
   @homepage='http://www.sqlite.org/'
 
   def install
