26073783e62ebc07aef8bf83257636e49fb9703c
Adam Vandenberg <flangy@gmail.com>
2009-08-27 09:46:09 -0700

    Bump Git to version 1.6.4.1

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -1,13 +1,13 @@
 require 'brewkit'
 
 class GitManuals <Formula
-  @url='http://www.kernel.org/pub/software/scm/git/git-manpages-1.6.4.tar.bz2'
-  @md5='851e1df833895c5046a994c28d3d8368'
+  @url='http://kernel.org/pub/software/scm/git/git-manpages-1.6.4.1.tar.bz2'
+  @md5='b725aab52c1f6948134cd820d57f20a9'
 end
 
 class Git <Formula
-  @url='http://kernel.org/pub/software/scm/git/git-1.6.4.tar.bz2'
-  @md5='d41e3d43c8f1d23024fb3d3fe6aa3858'
+  @url='http://kernel.org/pub/software/scm/git/git-1.6.4.1.tar.bz2'
+  @md5='bdc9b8be7d56a3beabad2d228f2e8e9a'
   @homepage='http://git-scm.com'
 
   def install
