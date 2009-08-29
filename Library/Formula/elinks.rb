e28b4356f051981e9449ed49ae6291522f2bfafa
Joshua Peek <josh@joshpeek.com>
2009-08-29 12:22:57 -0500

    Formula for elinks

---
+++ Library/Formula/elinks.rb
@@ -0,0 +1,13 @@
+require 'brewkit'
+
+class Elinks <Formula
+  @homepage='http://elinks.or.cz/'
+  @url='http://elinks.or.cz/download/elinks-0.11.7.tar.bz2'
+  @md5='fcd087a6d2415cd4c6fd1db53dceb646'
+
+  def install
+    ENV.deparallelize
+    system "./configure --prefix='#{prefix}'"
+    system "make install"
+  end
+end
