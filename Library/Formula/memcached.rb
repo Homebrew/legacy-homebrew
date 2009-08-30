85701b75c8a53194d7222a6a1a8e1455ba660576
Joshua Peek <josh@joshpeek.com>
2009-08-29 19:07:52 -0500

    Formula for memcached

---
+++ Library/Formula/memcached.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Memcached <Formula
+  @url='http://memcached.googlecode.com/files/memcached-1.4.0.tar.gz'
+  @homepage='http://www.danga.com/memcached/'
+  @md5='d7651ecb8bf345144cb17900d9a46c85'
+
+  def deps
+    LibraryDep.new 'libevent'
+  end
+
+  def install
+    system "./configure --prefix='#{prefix}'"
+    system "make install"
+  end
+end
