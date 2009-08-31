8b0bcaddf83cb13b25c87c3f6eb84b0d82f72e15
Sean Wolfe <sean@iheartsquares.com>
2009-08-31 16:35:09 -0500

    Ruby Fastri service and library
    > The fri executable will run much faster if you install FastRI from a
    > tarball, since RubyGems introduces a noticeable overhead.
    
    Source: http://eigenclass.org/hiki/fastri

---
+++ Library/Formula/fastri.rb
@@ -0,0 +1,11 @@
+require 'brewkit'
+
+class Fastri <Formula
+  @homepage='http://eigenclass.org/hiki/fastri'
+  @url='http://rubyforge.org/frs/download.php/31654/fastri-0.3.1.tar.gz'
+  @md5='3a7d0a64b1c8e230a34ef7b4bad30dbe'
+
+  def install
+    system "ruby setup.rb all --prefix='#{prefix}'"
+  end
+end
