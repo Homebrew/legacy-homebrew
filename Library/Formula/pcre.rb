74369ee3a52b7a3a8694095ef357e3b17a7044ac
Adam Vandenberg <flangy@gmail.com>
2009-08-30 22:31:16 -0700

    PCRE Formula
    The PCRE library is a set of functions that implement regular expression
    pattern matching using the same syntax and semantics as Perl 5. PCRE has its
    own native API, as well as a set of wrapper functions that correspond to the
    POSIX regular expression API. The PCRE library is free, even for building
    commercial software.

---
+++ Library/Formula/pcre.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Pcre <Formula
+  @url='ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-7.9.tar.bz2'
+  @homepage='http://www.pcre.org/'
+  @md5='b6a9669d1863423f01ea46cdf00f93dc'
+
+  def install
+    system "./configure", "--prefix='#{prefix}'", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
