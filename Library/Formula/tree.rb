7b1d2410675f9f70695f3292468e9b696a6de7de
Brett Porter <brett@apache.org>
2009-08-31 21:18:17 +1000

    Tree formula
    Tree is a recursive directory listing program that produces a depth indented
    listing of files, which is colorized ala dircolors if the LS_COLORS
    environment variable is set and output is to tty.

---
+++ Library/Formula/tree.rb
@@ -0,0 +1,13 @@
+require 'brewkit'
+
+class Tree <Formula
+  @url='ftp://mama.indstate.edu/linux/tree/tree-1.5.2.2.tgz'
+  @homepage='http://mama.indstate.edu/users/ice/tree/'
+  @md5='a7731a898e2c0d7e422a57a84ffbb06c'
+
+  def install
+    system "gcc #{ENV['CFLAGS']} -o tree tree.c strverscmp.c"
+    bin.install "tree"
+    man1.install "man/tree.1"
+  end
+end
