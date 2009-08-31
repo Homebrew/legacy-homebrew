fafbbbea7976c196362bbf0ae47b308ce56bb5b1
Max Howell <max@methylblue.com>
2009-08-31 16:01:36 +0100

    Change license to BSD
    I confirmed this change with all relevant contributors first.

--- Library/Homebrew/hw.model.c
+++ Library/Homebrew/hw.model.c
@@ -1,3 +1,8 @@
+/*
+   This software is in the public domain, furnished "as is", without technical
+   support, and with no warranty, express or implied, as to its usefulness for
+   any purpose.
+*/
 #include <sys/sysctl.h>
 #include <stdio.h>
 
