d60fc4ba86d45255b0093303c45f6b1c2196fda2
Christian Mayer <git@mifix.com>
2009-08-31 18:33:40 +0200

    If forked, open Github history of forked repository

--- Library/Homebrew/brew.h.rb
+++ Library/Homebrew/brew.h.rb
@@ -94,7 +94,10 @@ end
 def info name
   require 'formula'
 
-  history="http://github.com/mxcl/homebrew/commits/masterbrew/Library/Formula/#{Formula.path(name).basename}"
+  user=`cd #{HOMEBREW_PREFIX}; git remote -v show`.scan(/github.com:(.*)\/.*fetch/).to_s
+  user='mxcl' if user.empty?
+  history="http://github.com/#{user}/homebrew/commits/masterbrew/Library/Formula/#{Formula.path(name).basename}"
+
   exec 'open', history if ARGV.flag? '--github'
 
   f=Formula.factory name
