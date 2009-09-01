343b01f766ce466497d80af92c46e7ddd8155393
Adam Vandenberg <flangy@gmail.com>
2009-09-01 12:21:45 -0700

    Update Subersion to 1.6.5, add SSL support, turn off BDB support.

--- Library/Formula/subversion.rb
+++ Library/Formula/subversion.rb
@@ -1,14 +1,14 @@
 require 'brewkit'
 
 class SubversionDeps <Formula
-  @url='http://subversion.tigris.org/downloads/subversion-deps-1.6.3.tar.bz2'
-  @md5='22d3687ae93648fcecf945c045931272'
+  @url='http://subversion.tigris.org/downloads/subversion-deps-1.6.5.tar.bz2'
+  @md5='8272316e1670d4d2bea451411e438bde'
 end
 
 class Subversion <Formula
-  @url='http://subversion.tigris.org/downloads/subversion-1.6.3.tar.bz2'
+  @url='http://subversion.tigris.org/downloads/subversion-1.6.5.tar.bz2'
   @homepage='http://subversion.tigris.org/'
-  @md5='8bf7637ac99368db0890e3f085fa690d'
+  @md5='1a53a0e72bee0bf814f4da83a9b6a636'
 
   def install
     # Slot dependencies into place
@@ -17,14 +17,15 @@ class Subversion <Formula
       d.install Dir['*']
     end
 
-    # Use existing system zlib
-    # Use dep-provided other libraries
+    # Use existing system zlib, dep-provided other libraries
     # Don't mess with Apache modules (since we're not sudo)
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
+                          "--with-ssl",
                           "--with-zlib=/usr/lib",
                           "--disable-mod-activation",
-                          "--without-apache-libexecdir"
+                          "--without-apache-libexecdir",
+                          "--without-berkeley-db"
     system "make"
     system "make install"
   end
