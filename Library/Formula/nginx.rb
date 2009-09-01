5098dff6b1b4206be022b8ce1b698fda0e012ee7
Julien BLANCHARD <julien.blanchard@me.com>
2009-09-01 22:23:31 +0200

    NGinx formula
    A HTTP and mail proxy server licensed under a 2-clause BSD-like license.

---
+++ Library/Formula/nginx.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Nginx <Formula
+  @url='http://sysoev.ru/nginx/nginx-0.7.61.tar.gz'
+  @homepage='http://nginx.net/'
+  @md5='6ebf89b9b00a3b82734e93c32da7df07'
+
+  def deps
+    LibraryDep.new 'pcre'
+  end
+    
+  def install
+    system "./configure", "--prefix=#{prefix}", "--with-http_ssl_module"
+    system "make install"
+  end 
+end
