417773db7b42b37f45a26eac08e9f5952d99b21e
Ben Alpert <spicyjalapeno@gmail.com>
2009-08-31 22:42:03 -0600

    GNU Ddrescue formula
    GNU ddrescue is a data recovery tool. It copies data from one file or block
    device (hard disc, cdrom, etc) to another, trying hard to rescue data in case
    of read errors.

---
+++ Library/Formula/ddrescue.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Ddrescue <Formula
+  @url='http://ftp.gnu.org/gnu/ddrescue/ddrescue-1.11.tar.gz'
+  @homepage='http://www.gnu.org/software/ddrescue/ddrescue.html'
+  @md5='7146046bb1851351d1337bb1f5b4e903'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
