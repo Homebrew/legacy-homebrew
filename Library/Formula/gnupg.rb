8a62a76f3918b186a361944085ea0bd206d9b5c1
Brett Porter <brett@apache.org>
2009-09-01 10:33:33 +1000

    GnuPG 1.4 formula
    Doesn't build on 10.6 currently.

---
+++ Library/Formula/gnupg.rb
@@ -0,0 +1,21 @@
+require 'brewkit'
+
+class Gnupg <Formula
+  @url='ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.9.tar.bz2'
+  @homepage='http://www.gnupg.org/'
+  @sha1='826f4bef1effce61c3799c8f7d3cc8313b340b55'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+
+    system "make"
+    system "make check"
+
+    # amazingly even the GNU folks can bugger up their Makefiles, so we need
+    # to create this directories because the install target has the dependency
+    # order wrong
+    bin.mkpath
+    (libexec+'gnupg').mkpath
+    system "make install"
+  end
+end
