93b7e583b70022292575061ad5be413727ab84a1
Ben Alpert <spicyjalapeno@gmail.com>
2009-08-31 22:57:16 -0600

    Antiword formula
    Antiword is a free MS Word reader that converts the binary files into plain
    text or Postscript.

---
+++ Library/Formula/antiword.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Antiword <Formula
+  @url='http://www.winfield.demon.nl/linux/antiword-0.37.tar.gz'
+  @homepage='http://www.winfield.demon.nl/'
+  @md5='f868e2a269edcbc06bf77e89a55898d1'
+
+  def install
+    system 'make'
+    bin.install 'antiword'
+  end
+end
