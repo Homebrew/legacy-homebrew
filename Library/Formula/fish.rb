c555e79d101b6e6fd1e5643b0dfdb9e8eb9b39f6
August Lilleaas <augustlilleaas@gmail.com>
2009-09-02 21:27:58 +0200

    fish shell formula
    fish is a user friendly command line shell for UNIX-like operating
    systems such as Linux.

---
+++ Library/Formula/fish.rb
@@ -0,0 +1,21 @@
+require 'brewkit'
+
+class Fish <Formula
+  @url='http://fishshell.org/files/1.23.1/fish-1.23.1.tar.gz'
+  @homepage='http:://fishshell.org/'
+  @md5='4b2436843e63bebba467cc4add11428a'
+
+  def deps
+    LibraryDep.new 'readline'
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--without-xsel"
+    system "make install"
+  end
+
+  def caveats
+    "You will need to add #{HOMEBREW_PREFIX}/bin/fish to /etc/shells"+
+    "Run `chsh -s #{HOMEBREW_PREFIX}/bin/fish' to make fish your default shell."
+  end
+end
