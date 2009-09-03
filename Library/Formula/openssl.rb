03a95bdda7b9e4f60fd61d8f965b52d6bb4d9029
Sean Wolfe <sean@iheartsquares.com>
2009-09-04 00:09:25 +0800

    Openssl 1.0.0-beta3 formula
    Signed-off-by: Max Howell <max@methylblue.com>

---
+++ Library/Formula/openssl.rb
@@ -0,0 +1,18 @@
+require 'brewkit'
+
+class Openssl <Formula
+  @url='http://www.openssl.org/source/openssl-1.0.0-beta3.tar.gz'
+  @homepage='http://www.openssl.org'
+  @md5='cf5a32016bb9da0b9578099727bf15c9'
+
+  def install
+    ENV.deparallelize
+    system "./Configure", "darwin64-x86_64-cc", "shared", "zlib-dynamic", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+  
+  def caveats
+    "Please note, OS X provides openssl, this is a slightly newer beta version"
+  end
+end
