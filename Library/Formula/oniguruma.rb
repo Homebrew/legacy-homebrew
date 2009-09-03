9f93a4f355eee7547d7ff2804c8dd48098f152e0
Andre Arko <andre@arko.net>
2009-09-02 20:18:24 -0700

    Oniguruma formula
    Oniguruma is a regular expressions library. The characteristics of this
    library is that different character encoding for every regular expression
    object can be specified.

---
+++ Library/Formula/oniguruma.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Oniguruma <Formula
+  @url='http://www.geocities.jp/kosako3/oniguruma/archive/onig-5.9.1.tar.gz'
+  @homepage='http://www.geocities.jp/kosako3/oniguruma/'
+  @md5='5ce5f9bba5e83f0ea6ec24e1ac77091c'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
