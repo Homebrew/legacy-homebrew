e6cffbb158431aa06727fc672ae144269af9b584
Andre Arko <andre@arko.net>
2009-09-05 03:07:38 -0700

    Add znc-extras sub-package with more useful modules

--- Library/Formula/znc.rb
+++ Library/Formula/znc.rb
@@ -1,5 +1,11 @@
 require 'brewkit'
 
+class ZncExtra <Formula
+  @url='http://downloads.sourceforge.net/project/znc/znc-extra/0.074/znc-extra-0.074.tar.gz'
+  @homepage='http://en.znc.in/wiki/ZNC'
+  @md5='34c0f43318c8c4a2cfb3a962e1740255'
+end
+
 class Znc <Formula
   @url='http://downloads.sourceforge.net/project/znc/znc/0.074/znc-0.074.tar.gz'
   @homepage='http://en.znc.in/wiki/ZNC'
@@ -22,5 +28,11 @@ class Znc <Formula
     # but I don't know how to tell znc that. Perl is only used for user plugins, anyway.
     system "./configure", "--prefix=#{prefix}", "--disable-perl"
     system "make install"
+
+    # Extra modules that don't come in the base package -- add them to the znc.conf file to enable
+    ZncExtra.new.brew do
+      system "./configure", "--prefix=#{prefix}"
+      system "make install"
+    end
   end
 end
