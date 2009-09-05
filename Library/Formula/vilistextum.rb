9fd65033691792fa735a44e03ac369fae6aef5c2
Geoff Cant <nem@lisp.geek.nz>
2009-09-05 23:55:59 +0200

    Vilistextum formula
    Vilistextum is a html to text / ascii converter specifically programmed to get
    the best out of incorrect html.

---
+++ Library/Formula/vilistextum.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Vilistextum <Formula
+  @url='http://bhaak.dyndns.org/vilistextum/vilistextum-2.6.9.tar.gz'
+  @homepage='http://bhaak.dyndns.org/vilistextum/'
+  @md5='5ba56ffdc56758da716bb46c3e0f517e'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
