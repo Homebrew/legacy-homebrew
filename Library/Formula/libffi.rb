44ca1004dfdcffcd1e6c04a9a7a07ef0b5f9d599
Paul J. Davis <paul.joseph.davis@gmail.com>
2009-09-07 15:22:45 -0400

    Libffi formula
    libffi - FFI stands for Foreign Function Interface. A foreign function
    interface is the popular name for the interface that allows code written
    in one language to call code written in another language.

---
+++ Library/Formula/libffi.rb
@@ -0,0 +1,18 @@
+require 'brewkit'
+
+class Libffi <Formula
+  @url='ftp://sourceware.org/pub/libffi/libffi-3.0.8.tar.gz'
+  @homepage='http://sources.redhat.com/libffi/'
+  @sha1='ce44d10c39d9a37479c8777e206cac0f36c48712'
+
+  def patches
+    host = "http://trac.macports.org"
+    base = "export/57218/trunk/dports/devel/libffi/files"
+    ["#{host}/#{base}/patch-includedir.diff"]
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
