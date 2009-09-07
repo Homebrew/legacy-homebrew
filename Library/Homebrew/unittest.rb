312849b5b04065132f1857b337ece80b7bd6287b
Étienne Barrié <etienne.barrie@gmail.com>
2009-09-07 16:10:50 +0200

    Add support for SHA256

--- Library/Homebrew/unittest.rb
+++ Library/Homebrew/unittest.rb
@@ -295,6 +295,26 @@ class BeerTasting <Test::Unit::TestCase
       nostdout { invalid_sha1.new.brew {} }
     end
   end
+
+  def test_sha256
+    valid_sha256 = Class.new(TestBall) do
+      @sha256='ccbf5f44743b74add648c7e35e414076632fa3b24463d68d1f6afc5be77024f8'
+    end
+
+    assert_nothing_raised do
+      nostdout { valid_sha256.new.brew {} }
+    end
+  end
+
+  def test_badsha256
+    invalid_sha256 = Class.new(TestBall) do
+      @sha256='dcbf5f44743b74add648c7e35e414076632fa3b24463d68d1f6afc5be77024f8'
+    end
+
+    assert_raises RuntimeError do
+      nostdout { invalid_sha256.new.brew {} }
+    end
+  end
   
   FOOBAR='foo-bar'
   def test_formula_funcs
