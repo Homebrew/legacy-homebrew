17a13ad91f2e2e7ce682106b424f9d10db8048df
Geoff Cant <nem@erlang.geek.nz>
2009-09-07 15:02:36 +0200

    Adding a formula for the pstree command.

---
+++ Library/Formula/pstree.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Pstree <Formula
+  @url='ftp://ftp.thp.uni-duisburg.de/pub/source/pstree-2.32.tar.gz'
+  @homepage='http://freshmeat.net/projects/pstree/'
+  @md5='ba6e274e06d63910cf9bb8664b932808'
+
+  def install
+    system "make pstree"
+    bin.install "pstree"
+  end
+end
