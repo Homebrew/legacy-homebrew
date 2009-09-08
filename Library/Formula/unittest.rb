65cf4e87d26e778ca8ed494f202c496c8211bba1
Dane Jensen <djensen@odin.(none)>
2009-09-08 01:38:38 -0700

    Unittest formula
    This is the web page for a C++ unit test framework. Its design goals are to be
    simple, to be idiomatic C++, and to follow the basic xUnit style to the extent
    that doing so is compatible with the earlier goals. Its main differences from
    other xUnit frameworks are that it uses constructors and destructors for
    setup/teardown and that it requires you to represent tests as classes, instead
    of methods.

---
+++ Library/Formula/unittest.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Unittest <Formula
+  @url='http://unittest.red-bean.com/tar/unittest-0.50-62.tar.gz'
+  @homepage='http://unittest.red-bean.com/'
+  @md5='6eaa2823620c2e21fc745bd8da6a26b2'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
