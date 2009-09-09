56dbfb8e553d0525af51ee0642862f57507b812a
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2009-09-09 14:03:04 +0200

    S-Lang formula
    S-Lang is a multi-platform programmer's library designed to allow a developer
    to create robust multi-platform software.
    
    <http://www.s-lang.org/>

---
+++ Library/Formula/s-lang.rb
@@ -0,0 +1,20 @@
+require 'brewkit'
+
+class SLang <Formula
+  @url='ftp://space.mit.edu/pub/davis/slang/v2.2/slang-2.2.0.tar.gz'
+  @homepage='http://www.s-lang.org/'
+  @md5='81cd7456c70a21937497f4b04b77433c'
+
+  def deps
+    OptionalLibraryDep.new 'pcre'
+    OptionalLibraryDep.new 'oniguruma'
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-png=/usr/X11R6"
+    system "make"
+    
+    ENV.deparallelize
+    system "make install"
+  end
+end
