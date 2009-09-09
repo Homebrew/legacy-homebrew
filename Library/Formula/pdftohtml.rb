5aad0ae03debf6ddded9e125dc3c7051303b88a7
Andre Arko <andre@arko.net>
2009-09-09 15:32:19 -0700

    Add pdftohtml formula

---
+++ Library/Formula/pdftohtml.rb
@@ -0,0 +1,13 @@
+require 'brewkit'
+
+class Pdftohtml <Formula
+  @url='http://downloads.sourceforge.net/project/pdftohtml/Experimental%20Versions/pdftohtml%200.40/pdftohtml-0.40a.tar.gz'
+  @homepage='http://pdftohtml.sourceforge.net/'
+  @md5='2d82996faaf2b9439f8395743c1c163d'
+
+  def install
+    system "make"
+    system "mkdir -p #{prefix}/bin"
+    system "cp src/pdftohtml #{prefix}/bin"
+  end
+end
