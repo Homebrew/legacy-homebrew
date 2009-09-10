903a727d113c84530c9ed7892eb2241509f6fdc4
Max Howell <max@methylblue.com>
2009-09-10 17:13:26 +0100

    Taglib Extras 0.1.7

--- Library/Formula/taglib.rb
+++ Library/Formula/taglib.rb
@@ -1,8 +1,8 @@
 require 'brewkit'
 
 class TaglibExtras <Formula
-  @url='http://kollide.net/~jefferai/taglib-extras-0.1.6.tar.gz'
-  @md5='706a82fe4c25606f731faf4c14b5edb0'
+  @url='http://kollide.net/~jefferai/taglib-extras-0.1.7.tar.gz'
+  @md5='09bf9c89d953a238b55ac74bf9db0be3'
 end
 
 class Taglib <Formula
