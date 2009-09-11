0ce88504a7ca0630031993dc92091aa10871d40c
Luke Amdor <luke@absoluterubbish.net>
2009-09-11 15:52:51 -0500

    Maven formula
    Apache Maven is a software project management and comprehension tool. Based on
    the concept of a project object model (POM), Maven can manage a project's
    build, reporting and documentation from a central piece of information.

---
+++ Library/Formula/maven.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Maven <Formula
+  @url='http://apache.mirrors.timporter.net/maven/binaries/apache-maven-2.2.1-bin.tar.gz'
+  @version="2.2.1"
+  @homepage='http://maven.apache.org/'
+  @md5='3f829ed854cbacdaca8f809e4954c916'
+
+  def install
+    prefix.install %w[bin conf boot lib]
+  end
+end
