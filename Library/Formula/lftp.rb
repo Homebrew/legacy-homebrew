86d169cefb31b68a7ab242df0fd7d6dd269ddc51
David Reynolds <david@reynoldsfamily.org.uk>
2009-09-11 20:13:01 +0100

    LFTP formula
    LFTP is sophisticated ftp/http client, file transfer program supporting a
    number of network protocols. Like BASH, it has job control and uses readline
    library for input. It has bookmarks, built-in mirror, can transfer several
    files in parallel. It was designed with reliability in mind.
    
    Signed-off-by: Max Howell <max@methylblue.com>
    
    Added MD5 and modified tweaks to the ENV slightly.

---
+++ Library/Formula/lftp.rb
@@ -0,0 +1,18 @@
+require 'brewkit'
+
+class Lftp <Formula
+  @url='http://ftp.yars.free.net/pub/source/lftp/lftp-3.7.15.tar.gz'
+  @homepage='http://lftp.yar.ru/'
+  @md5='6c43ffdb59234ff0533cfdda0c3c305c'
+
+  def deps
+    LibraryDep.new 'readline'
+  end
+
+  def install
+    ENV['CXXFLAGS'] += " -fno-exceptions -fno-rtti -fno-implement-inlines"
+    ENV['LDFLAGS'] += " -Xlinker -search_paths_first -L/usr/local/lib"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-openssl", "--disable-shared", "--disable-nls"
+    system "make install"
+  end
+end
