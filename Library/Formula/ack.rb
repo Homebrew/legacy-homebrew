590f072067257f9a316da74a067160ae44a5d5cf
Max Howell <max@methylblue.com>
2009-09-11 16:26:16 +0100

    Ack 1.90
    <http://groups.google.com/group/ack-announce/t/584c2c2df9b1ad3>

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -1,11 +1,14 @@
 require 'brewkit'
 
 class Ack <ScriptFileFormula
-  @version='1.88'
+  # NOTE you don't need to specify the version, usually it is determined
+  # automatically by examination of the URL, however in this case our auto
+  # determination magic is inadequete
+  @version='1.90'
   @url="http://ack.googlecode.com/svn/tags/#{@version}/ack"
-  @md5='8009a13ab0fc66047bea0ea2ad89419c'
+  @md5='d15d059166beff6103d2401aa2d783c7'
   @homepage='http://betterthangrep.com/'
-  
+
   # because our url looks like a svn one Homebrew defaults to the svn strategy
   def download_strategy
     HttpDownloadStrategy
