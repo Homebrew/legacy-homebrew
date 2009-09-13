8ae23fe4bbde1a5d2cae2a62512a7cb58f64fb1e
Mike Enriquez <mike@enriquez.me>
2009-09-13 12:59:04 -0400

    Ruby 64-bit formula
    Signed-off-by: Max Howell <max@methylblue.com>
    
    I adapted our existing ruby formula using the best bits of them both.

--- Library/Formula/ruby.rb
+++ Library/Formula/ruby.rb
@@ -1,14 +1,19 @@
 require 'brewkit'
 
-# TODO deversion the include and lib directories
+# TODO de-version the include and lib directories
 
 class Ruby <Formula
-  @url='ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p243.tar.gz'
-  @homepage='http://www.ruby-lang.org'
-  @md5='515bfd965814e718c0943abf3dde5494'
-
+  @url='ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.7-p174.tar.gz'
+  @homepage='http://www.ruby-lang.org/en/'
+  @md5='18dcdfef761a745ac7da45b61776afa5'
+  
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-shared",
+                          "--enable-pthread",
+                          "CFLAGS=-D_XOPEN_SOURCE=1"
     system "make"
     system "make install"
   end
