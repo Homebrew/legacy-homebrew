74c4f8b540993258f4024a924d28986280db15fd
Max Howell <max@methylblue.com>
2009-09-16 19:29:50 +0100

    TagLib 1.6

--- Library/Formula/taglib.rb
+++ Library/Formula/taglib.rb
@@ -1,26 +1,14 @@
 require 'brewkit'
 
-class TaglibExtras <Formula
-  @url='http://kollide.net/~jefferai/taglib-extras-0.1.7.tar.gz'
-  @md5='09bf9c89d953a238b55ac74bf9db0be3'
-end
-
 class Taglib <Formula
-  @url='http://developer.kde.org/~wheeler/files/src/taglib-1.5.tar.gz'
-  @md5='7b557dde7425c6deb7bbedd65b4f2717'
+  @url='http://developer.kde.org/~wheeler/files/src/taglib-1.6.tar.gz'
+  @md5='5ecad0816e586a954bd676a86237d054'
   @homepage='http://developer.kde.org/~wheeler/taglib.html'
 
-  def deps
-    BinaryDep.new 'cmake'
-  end
-
   def install
-    system "./configure --disable-debug --prefix='#{prefix}'"
+    system "./configure", "--enable-mp4", "--enable-asf",
+                          "--disable-debug", "--prefix=#{prefix}"
+    system "make"
     system "make install"
-    
-    TaglibExtras.new.brew do
-      system "cmake . #{std_cmake_parameters}"
-      system "make install"
-    end unless ARGV.include? '--no-extras'
   end
 end
