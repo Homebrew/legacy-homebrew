1b6c9d8a9850e424d91bc8518ec58f5f1c7880a7
dbr <dbr.onix@gmail.com>
2009-09-17 19:41:05 +0100

    Add weechat formula

---
+++ Library/Formula/weechat.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Weechat <Formula
+  @url='http://www.weechat.org/files/src/weechat-0.3.0.tar.bz2'
+  @homepage='http://www.weechat.org'
+  @md5='c31cfc229e964ff9257cc9c7f9e6c9bc'
+
+ def deps
+   BinaryDep.new 'cmake'
+ end
+
+  def install
+    system "cmake", "-DDISABLE_PERL=ON", std_cmake_parameters, "."
+    system "make install"
+  end
+end
