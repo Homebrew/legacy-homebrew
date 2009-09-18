a608ead3d28c54ead76890ee007d7052f1145b80
dbr <dbr.onix@gmail.com>
2009-09-18 14:38:43 +0100

    weechat depends_on gnutls, and two FIXME notices

--- Library/Formula/weechat.rb
+++ Library/Formula/weechat.rb
@@ -6,8 +6,11 @@ class Weechat <Formula
   @md5='c31cfc229e964ff9257cc9c7f9e6c9bc'
 
   depends_on 'cmake'
+  depends_on 'gnutls'
 
   def install
+    #FIXME: Compiling perl module doesn't work
+    #FIXME: GnuTLS support isn't detected
     system "cmake", "-DDISABLE_PERL=ON", std_cmake_parameters, "."
     system "make install"
   end
