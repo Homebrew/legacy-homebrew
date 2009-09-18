a01d36771753b5ffb23d8988bb99836f26915676
elliottcable <git@elliottcable.name>
2009-09-18 00:13:46 -0800

    Fixing the mplayer formula, for now.
    Some day I hope to make it utilize the ffmpeg-mt and speed patches, as well as
    providing optimization info in the notes, but I can’t currently get that to
    work on Snow Leopard. Maybe later.

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -2,12 +2,14 @@ require 'brewkit'
 
 class Mplayer <Formula
   @homepage='http://www.mplayerhq.hu/'
-  @url='http://www.mplayerhq.hu/MPlayer/releases/mplayer-checkout-snapshot.tar.bz2'
-  @md5=''
-  @verison='snapshot'
+  @head='svn://svn.mplayerhq.hu/mplayer/trunk'
 
   def install
-    system "./configure --disable-debug --prefix='#{prefix}'"
+    # Information from http://blog.bloople.net/read/mplayer-on-snow-leopard
+    # seems to claim that we have to build against an x86_64 target even if on
+    # i386 /-:
+    system "./configure --prefix='#{prefix}' --target=x86_64-Darwin"
+    system "make"
     system "make install"
   end
 end
