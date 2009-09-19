4b416e58d9963e4a96d355aa0f65a4d1db8e9e9e
David Höppner <0xffea@gmail.com>
2009-09-19 19:59:58 +0200

    Boehm-Demers-Weiser garbage collector formula
    The Boehm-Demers-Weiser conservative garbage collector can be used as a garbage collecting
    replacement for C malloc or C++ new. It allows you to allocate memory basically as you
    normally would, without explicitly deallocating memory that is no longer useful. The collector
    automatically recycles memory when it determines that it can no longer be otherwise accessed.
    
    Adam Vandenberg:
        Update bdw-gc formula for 10.6

---
+++ Library/Formula/bdw-gc.rb
@@ -0,0 +1,21 @@
+require 'brewkit'
+
+class BdwGc <Formula
+  @url='http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc-7.1.tar.gz'
+  @homepage='http://www.hpl.hp.com/personal/Hans_Boehm/gc/'
+  @md5='2ff9924c7249ef7f736ecfe6f08f3f9b'
+
+  def install
+    if MACOS_VERSION == 10.6
+      # LLVM gives an unsupported inline asm error
+      ENV.gcc_4_2
+      
+      # ucontext has been deprecated in 10.6
+      # use this flag to force the header to compile
+      ENV['CPPFLAGS'] = ENV['CPPFLAGS'] + " -D_XOPEN_SOURCE"
+    end
+
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
