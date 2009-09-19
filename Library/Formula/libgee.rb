0b27712e6fa11c8153af692ba32046d2d29a5659
David Höppner <0xffea@gmail.com>
2009-09-19 11:33:04 +0200

    Libgee formula
    Libgee is a collection library providing GObject-based interfaces and
    classes for commonly used data structures.

---
+++ Library/Formula/libgee.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Libgee <Formula
+  @url='http://ftp.gnome.org/pub/GNOME/sources/libgee/0.3/libgee-0.3.0.tar.bz2'
+  @homepage='http://live.gnome.org/Libgee'
+  @md5='1ca2b8a87950ef1b14342fb32db3e558'
+
+  def deps
+    BinaryDep.new 'valac'
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
