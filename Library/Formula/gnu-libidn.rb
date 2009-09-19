d992d701a3b83d75f909c4672a2e2f71963ad144
David Höppner <0xffea@gmail.com>
2009-09-19 11:50:52 +0200

    GNU Libidn formula
    GNU Libidn is a fully documented implementation of the Stringprep, Punycode and
    IDNA specifications. Libidn's purpose is to encode and decode internationalized
    domain names.

---
+++ Library/Formula/gnu-libidn.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class GnuLibidn <Formula
+  @url='http://ftp.gnu.org/gnu/libidn/libidn-1.9.tar.gz'
+  @homepage='http://www.gnu.org/software/libidn/'
+  @md5='f4d794639564256a367566302611224e'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--disable-csharp"
+    system "make install"
+  end
+end
