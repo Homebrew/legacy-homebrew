a765fcb5801efe0d9eab5d258915625e3031deaa
David Höppner <0xffea@gmail.com>
2009-09-19 11:37:39 +0200

    Automoc formula
    KDE automatic moc generator

---
+++ Library/Formula/automoc4.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Automoc4 <Formula
+  @url='http://ftp-stud.fht-esslingen.de/Mirrors/ftp.kde.org/pub/kde/stable/automoc4/0.9.88/automoc4-0.9.88.tar.bz2'
+  @homepage='http://techbase.kde.org/Development/Tools/Automoc4'
+  @md5='91bf517cb940109180ecd07bc90c69ec'
+
+  def deps
+    BinaryDep.new 'cmake'
+  end
+
+  def install
+    system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
