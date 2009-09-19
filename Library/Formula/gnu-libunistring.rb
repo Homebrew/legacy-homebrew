a2e7d9e521730ddc277049c62d3074faec2363c6
David Höppner <0xffea@gmail.com>
2009-09-19 19:55:25 +0200

    GNU Libunistring
    Libunistring provides functions for manipulating Unicode strings and for manipulating
    C strings according to the Unicode standard.

---
+++ Library/Formula/gnu-libunistring.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class GnuLibunistring <Formula
+  @url='http://www.de-mirrors.de/gnuftp/libunistring/libunistring-0.9.1.1.tar.gz'
+  @homepage='http://www.gnu.org/software/libunistring/'
+  @md5='f8bdb65eaf1746bc6b659fb65c5ac685'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
