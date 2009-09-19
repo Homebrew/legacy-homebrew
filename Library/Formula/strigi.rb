1266e7453be17c98fedea445cbbabc0ae88b78c0
David Höppner <0xffea@gmail.com>
2009-09-19 11:48:25 +0200

    Strigi formula
    Strigi is a daemon which uses a very fast and efficient crawler that can
    index data on your harddrive. Indexing operations are performed without
    hammering your system, this makes Strigi the fastest and smallest desktop
    searching program.

---
+++ Library/Formula/strigi.rb
@@ -0,0 +1,20 @@
+require 'brewkit'
+
+class Strigi <Formula
+  @url='http://surfnet.dl.sourceforge.net/project/strigi/strigi/strigi-0.6.4/strigi-0.6.4.tar.bz2'
+  @homepage='http://strigi.sourceforge.net/'
+  @md5='324fd9606ac77765501717ff92c04f9a'
+
+  def deps
+    BinaryDep.new('cmake')
+    LibraryDep.new('CLucene')
+  end
+
+  def install
+    ENV['CLUCENE_HOME'] = HOMEBREW_PREFIX
+    ENV['EXPAT_HOME'] = '/usr/'
+
+    system "cmake . #{std_cmake_parameters} -DENABLE_EXPAT:BOOL=ON"
+    system "make install"
+  end
+end
