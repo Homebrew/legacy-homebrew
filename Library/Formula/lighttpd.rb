3c5db62ed732fb594d92b8e8c5e969fb7d06620f
David Höppner <0xffea@gmail.com>
2009-09-19 11:46:33 +0200

    Lighttpd formula
    Security, speed, compliance, and flexibility -- all of these describe lighttpd
    (pron. lighty) which is rapidly redefining efficiency of a webserver; as it is
    designed and optimized for high performance environments. With a small memory
    footprint compared to other web-servers, effective management of the cpu-load,
    and advanced feature set (FastCGI, SCGI, Auth, Output-Compression, URL-Rewriting
    and many more) lighttpd is the perfect solution for every server that is suffering
    load problems. And best of all it's Open Source licensed under the revised BSD
    license.

---
+++ Library/Formula/lighttpd.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Lighttpd <Formula
+  @url='http://www.lighttpd.net/download/lighttpd-1.4.23.tar.bz2'
+  @homepage='http://www.lighttpd.net/'
+  @md5='0ab6bb7b17bf0f515ce7dce68e5e215a'
+
+  def deps
+    LibraryDep.new 'pcre'
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-openssl", "--with-ldap"
+    system "make install"
+  end
+end
