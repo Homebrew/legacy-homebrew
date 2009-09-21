c276c6fd473d04823a1329d1c9ba907db84d7803
Max Howell <max@methylblue.com>
2009-09-21 23:51:18 +0100

    SDL on 10.6
    Using MacPorts patch. We need their people!

--- Library/Formula/sdl.rb
+++ Library/Formula/sdl.rb
@@ -5,8 +5,15 @@ class Sdl <Formula
   @homepage='http://www.libsdl.org/index.php'
   @md5='c6660feea2a6834de10bc71b2f8e4d88'
 
+  def patches
+    { :p0 => "http://methylblue.com/junk/libsdl-1.2.13-10.6.patch" }
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-video-x11",
+                          "--disable-nasm" # seems essential to build on 10.6
     system "make install"
   end
 end
