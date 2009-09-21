6786e52811d39afa6e3a24b099f631b4eaae2da2
Max Howell <max@methylblue.com>
2009-09-21 20:22:09 +0100

    Automatically add keg_only? deps to relevent ENV
    Eg gettext gets added into LDFLAGS, INCLUDE and that. I hope I got everything
    that is typical. Prolly not. But we'll find out.
    
    Made readline keg_only because the BSD version is provided by OS X, and I
    don't want bug reports that are tricky to solve due to unexpected differences
    between the two.

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -16,8 +16,6 @@ class Glib <Formula
   depends_on 'gettext'
 
   def install
-    ENV.gnu_gettext
-
     # Snow Leopard libiconv doesn't have a 64bit version of the libiconv_open
     # function, which breaks things for us, so we build our own
     # http://www.mail-archive.com/gtk-list@gnome.org/msg28747.html
--- Library/Formula/readline.rb
+++ Library/Formula/readline.rb
@@ -5,6 +5,13 @@ class Readline <Formula
   @homepage='http://tiswww.case.edu/php/chet/readline/rltop.html'
   @md5='b7f65a48add447693be6e86f04a63019'
 
+  def keg_only? ; <<-EOS
+OS X provides the BSD Readline library. In order to prevent conflicts when
+programs look for libreadline we are defaulting this GNU Readline installation
+to keg-only.
+    EOS
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
