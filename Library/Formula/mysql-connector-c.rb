618ae3eb1d9f923148bbfbce2354be97d0cc6c58
Adam Vandenberg <flangy@gmail.com>
2009-09-22 13:14:00 -0700

    Bump MysqlConnectorC to 6.0.2

--- Library/Formula/mysql-connector-c.rb
+++ Library/Formula/mysql-connector-c.rb
@@ -2,8 +2,8 @@ require 'brewkit'
 
 class MysqlConnectorC <Formula
   @homepage='http://dev.mysql.com/downloads/connector/c/6.0.html'
-  @url='http://mysql.llarian.net/Downloads/Connector-C/mysql-connector-c-6.0.1.tar.gz'
-  @md5='348a869fa72957062ea4e7ad3865623c'
+  @url='http://mysql.llarian.net/Downloads/Connector-C/mysql-connector-c-6.0.2.tar.gz'
+  @md5='67e478df66e8f66536e54388cfa29854'
 
   depends_on 'cmake'
 
