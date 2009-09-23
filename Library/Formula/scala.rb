7d200c380bdfbf3885a4b68c7b0dd46bc96c146c
Rune Botten <rbotten@gmail.com>
2009-09-23 11:39:50 +0200

    Update and correct scala formula

--- Library/Formula/scala.rb
+++ Library/Formula/scala.rb
@@ -1,10 +1,10 @@
 require 'brewkit'
 
 class Scala <Formula
-  @homepage='http://www.gnu.org/software/wget/'
-  @url='http://www.scala-lang.org/downloads/distrib/files/scala-2.7.5.final.tgz'
-  @version='2.7.5'
-  @md5='40a62c98d13689d83920f564a0532a8d'
+  @homepage='http://www.scala-lang.org/'
+  @url='http://www.scala-lang.org/downloads/distrib/files/scala-2.7.6.final.tgz'
+  @version='2.7.6'
+  @md5='82934acf64d0c026de78b84e984f6743'
 
   def install
     prefix.install Dir['*']
