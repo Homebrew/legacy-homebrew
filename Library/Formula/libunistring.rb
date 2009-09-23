f6a491d1f0811c09f9eeb5e92a9d5d59b00952f6
Adam Vandenberg <flangy@gmail.com>
2009-09-23 07:48:14 -0700

    Fix name of libunistring, libidn and cluence classes

--- Library/Formula/libunistring.rb
+++ Library/Formula/libunistring.rb
@@ -1,6 +1,6 @@
 require 'brewkit'
 
-class GnuLibunistring <Formula
+class Libunistring <Formula
   @url='http://www.de-mirrors.de/gnuftp/libunistring/libunistring-0.9.1.1.tar.gz'
   @homepage='http://www.gnu.org/software/libunistring/'
   @md5='f8bdb65eaf1746bc6b659fb65c5ac685'
