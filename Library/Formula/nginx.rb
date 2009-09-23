056a293741228bc879d96f488e540e0a3cd5d66d
JD Harrington <jd@ronin.(none)>
2009-09-23 09:23:48 -0400

    add '--with-passenger' option to nginx Formula

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -6,9 +6,33 @@ class Nginx < Formula
   @md5='ab22f1b7f098a90d803a3abb94d23f7e'
 
   depends_on 'pcre'
+  
+  def options
+    [
+      ['--with-passenger', "Compile with support for Phusion Passenger module"]
+    ]
+  end
     
   def install
-    system "./configure", "--prefix=#{prefix}", "--with-http_ssl_module"
+    configure_args = [
+      "--prefix=#{prefix}",
+      "--with-http_ssl_module"
+    ]
+    
+    if ARGV.include? '--with-passenger'
+      passenger_root = `passenger-config --root`.chomp
+      
+      if File.directory?(passenger_root)
+        configure_args << "--add-module=#{passenger_root}/ext/nginx"
+      else
+        puts "Unable to install nginx with passenger support. The passenger"
+        puts "gem must be installed and passenger-config must be in your path"
+        puts "in order to continue."
+        exit
+      end
+    end
+    
+    system "./configure", *configure_args
     system "make install"
   end 
 end
