8da18b0504d20b1957a9f5484bfae9ef1181f205
Joe Shaw <joe@joeshaw.org>
2009-09-22 22:30:16 -0400

    Add a formula for GNU sed.
    Installs as $prefix/bin/gsed, like the Macports version.  This
    makes it easier to alias so that you don't have to put $prefix/bin
    ahead of /usr/bin in your $PATH.

---
+++ Library/Formula/gnu-sed.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class GnuSed <Formula
+  @url='http://ftp.gnu.org/gnu/sed/sed-4.2.1.tar.bz2'
+  @homepage='http://www.gnu.org/software/sed/'
+  @md5='7d310fbd76e01a01115075c1fd3f455a'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--program-prefix=g"
+    system "make install"
+  end
+end
