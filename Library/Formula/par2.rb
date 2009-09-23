a83d45deaccc108666cc7af6a5eff8bd4f6f6e13
Jari Bakken <jari.bakken@gmail.com>
2009-09-23 20:46:43 +0200

    Add formula for par2 utility

---
+++ Library/Formula/par2.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+class Par2 <Formula
+  @url='http://downloads.sourceforge.net/project/parchive/par2cmdline/0.4/par2cmdline-0.4.tar.gz'
+  @homepage='http://parchive.sourceforge.net/'
+  @sha1='2fcdc932b5d7b4b1c68c4a4ca855ca913d464d2f'
+
+  def patches
+    %w[http://sage.math.washington.edu/home/binegar/src/par2cmdline-0.4-gcc4.patch]
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
