8d405a04bf5c1bd90149e3ef8843f77d34a9de30
Max Howell <max@methylblue.com>
2009-09-24 18:49:32 +0100

    Use new instance var syntax in example formula

--- Library/Formula/wget.rb
+++ Library/Formula/wget.rb
@@ -1,12 +1,12 @@
 require 'brewkit'
 
 class Wget <Formula
-  @homepage='http://www.gnu.org/software/wget/'
-  @url='http://ftp.gnu.org/gnu/wget/wget-1.11.4.tar.bz2'
-  @md5='f5076a8c2ec2b7f334cb6e3059820f9c'
+  homepage 'http://www.gnu.org/software/wget/'
+  url 'http://ftp.gnu.org/gnu/wget/wget-1.11.4.tar.bz2'
+  md5 'f5076a8c2ec2b7f334cb6e3059820f9c'
 
   def install
-    system "./configure --disable-debug --prefix='#{prefix}'"
+    system "./configure", "--disable-debug", "--prefix='#{prefix}'"
     system "make install"
   end
 end
