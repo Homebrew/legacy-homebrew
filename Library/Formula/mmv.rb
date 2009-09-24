e81eff278f6590da0e8431c1465a0d91775c854d
Olivier ROUX <skoot@dailymotion.com>
2009-09-24 19:59:29 +0200

    mmv formula
    mmv is a program to move/copy/append/link multiple files according to a set of
    wildcard patterns. This multiple action is performed safely, i.e. without any
    unexpected deletion of files due to collisions of target names with existing
    filenames or with other target names.

---
+++ Library/Formula/mmv.rb
@@ -0,0 +1,24 @@
+require 'brewkit'
+
+class Mmv <Formula
+  version '1.01b'
+  url "http://ftp.de.debian.org/debian/pool/main/m/mmv/mmv_#{version}.orig.tar.gz"
+  md5 '1b2135ab2f17bdfa9e08debbb3c46ad8'
+  homepage 'http://packages.debian.org/unstable/utils/mmv'
+
+  def patches
+    "http://ftp.de.debian.org/debian/pool/main/m/mmv/mmv_1.01b-15.diff.gz"
+  end
+
+  def install
+    system "make"
+
+    bin.install 'mmv'
+    man1.install 'mmv.1'
+
+    %w[mcp mad mln].each do |mxx|
+      FileUtils.ln bin+'mmv', bin+mxx
+      FileUtils.ln man1+'mmv.1', man1+"#{mxx}.1"
+    end
+  end
+end
