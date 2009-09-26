56b1334942245bf33ae582f1d168d874e575227e
Kieran Pilkington <kieran@katipo.co.nz>
2009-09-26 14:24:35 +1200

    Fixing vorbis-tools formula
    The ogg and vorbis formulas didn't exist (libogg, libvorbis do though).

--- Library/Formula/vorbis-tools.rb
+++ Library/Formula/vorbis-tools.rb
@@ -5,10 +5,10 @@ class VorbisTools <Formula
   @md5='df976d24e51ef3d87cd462edf747bf9a'
   @homepage='http://vorbis.com'
 
-  depends_on 'ogg'
-  depends_on 'vorbis'
-  depends_on 'ao' => :optional
-  depends_on 'ogg123' => :optional
+  depends_on 'libogg'
+  depends_on 'libvorbis'
+  #depends_on 'ao' => :optional
+  #depends_on 'ogg123' => :optional
 
   def install
     system "./configure --disable-debug --disable-nls --disable-dependency-tracking --prefix='#{prefix}'"
