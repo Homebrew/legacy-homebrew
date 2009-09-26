d0e294157a62492ddf02d025f85efa65c50feefd
bendyworks <dev@bendyworks.com>
2009-09-26 18:09:50 -0500

    Updated imagemagick formula to play nicely with rmagick gem
    Fixes #71

--- Library/Formula/imagemagick.rb
+++ Library/Formula/imagemagick.rb
@@ -25,7 +25,6 @@ class Imagemagick <Formula
                           "--with-modules",
                           "--without-magick-plus-plus",
                           "--disable-dependency-tracking",
-                          "--disable-shared",
                           "--without-maximum-compile-warnings",
                           "--prefix=#{prefix}",
                           "--disable-osx-universal-binary",
