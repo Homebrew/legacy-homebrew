08306c3129a1c03e5dcc2597befd9d70fb53cbe3
Philip Weaver <pweaver@signalicorp.com>
2009-09-25 22:54:53 -0700

    Add Formula for aspell.
    GNU Aspell is a Free and Open Source spell checker designed to eventually
    replace Ispell.
    
    A pretty vanilla formula.  I don't know what configure options would be
    useful. As it is, it works for me with Emacs.

---
+++ Library/Formula/aspell.rb
@@ -0,0 +1,13 @@
+require 'brewkit'
+
+class Aspell <Formula
+  @url='ftp://ftp.gnu.org/gnu/aspell/aspell-0.60.6.tar.gz'
+  @homepage='http://aspell.net/'
+  @md5='bc80f0198773d5c05086522be67334eb'
+
+  def install
+    ENV.gcc_4_2
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
