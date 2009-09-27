eb1ce2caf8da0f8fc7ab60518d7afcb48ea72887
Christoph Sturm <me@christophsturm.com>
2009-09-28 01:50:59 +0200

    Enable x264 features in ffmpeg
    Add dependency on x264 package.
    
    Only build 64 bit on SL, this probably fixes the build on leopard.

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -3,12 +3,16 @@ require 'brewkit'
 class Ffmpeg <Formula
   @head='svn://svn.ffmpeg.org/ffmpeg/trunk'
   @homepage='http://ffmpeg.org/'
+  depends_on 'x264'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
+    configure_flags = [ "--prefix=#{prefix}",
                           "--disable-debug",
-                          "--enable-nonfree", 
-                          "arch=x86_64"
+                          "--enable-nonfree",
+                          "--enable-gpl",
+                          "--enable-libx264"]
+    configure_flags << "--arch=x64_64" if Hardware.is_64_bit? and MACOS_VERSION >= 10.6
+    system "./configure", *configure_flags
     system "make install"
   end
 end
