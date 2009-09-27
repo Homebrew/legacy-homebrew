0fb7b742acbf58f2eee6220fb82747b61fe353f5
Adam Vandenberg <flangy@gmail.com>
2009-09-26 17:56:10 -0700

    Bump Neon version and add --with-ssl

--- Library/Formula/neon.rb
+++ Library/Formula/neon.rb
@@ -1,12 +1,15 @@
 require 'brewkit'
 
 class Neon <Formula
-  @url='http://www.webdav.org/neon/neon-0.28.5.tar.gz'
+  @url='http://www.webdav.org/neon/neon-0.29.0.tar.gz'
   @homepage='http://www.webdav.org/neon/'
-  @md5='8c160bc0e358a3b58645acbba40fe873'
+  @md5='18a3764b70f9317f8b61509fd90d9e7a'
 
   def install
-    system "./configure --prefix='#{prefix}' --disable-debug --disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug", 
+                          "--disable-dependency-tracking",
+                          "--with-ssl"
     system "make install"
   end
 end
