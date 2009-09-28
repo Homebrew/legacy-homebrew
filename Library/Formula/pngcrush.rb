a2a1ee098193f54476cdc95b27dccb7ff970140b
Austin Ziegler <austin@halostatue.ca>
2009-09-27 23:43:50 -0400

    Updated pngcrush to 1.7.2; fixed penryn build.

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -1,14 +1,16 @@
 require 'brewkit'
 
 class Pngcrush <Formula
-  @homepage='http://pmt.sourceforge.net/pngcrush/'
-  @url='http://downloads.sourceforge.net/sourceforge/pmt/pngcrush-1.7.0.tar.bz2'
-  @md5='033f1542ef452952b1ba585cf21be70b'
+  homepage 'http://pmt.sourceforge.net/pngcrush/'
+  url "http://downloads.sourceforge.net/sourceforge/pmt/pngcrush-1.7.2.tar.bz2"
+  md5 '24ead6781e7e017e8ee2bf61db93a355'
 
   def install
-    # use our CFLAGS and LDFLAGS thanks :P
+    # use our CFLAGS, LDFLAGS, CC, and LD
     inreplace 'Makefile', 'CFLAGS = -I. -O3 -fomit-frame-pointer -Wall -Wshadow', ''
     inreplace 'Makefile', 'LDFLAGS =', ''
+    inreplace 'Makefile', 'CC = gcc', ''
+    inreplace 'Makefile', 'LD = gcc', ''
 
     system "make"
     bin.install 'pngcrush'
