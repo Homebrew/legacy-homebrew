6c0d810a2490f583c49259a3af050b256c65fecb
Alexander Solovyov <piranha@piranha.org.ua>
2009-09-28 22:16:15 +0300

    libyaml formula
    LibYAML is a YAML 1.1 parser and emitter written in C.

---
+++ Library/Formula/libyaml.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Libyaml <Formula
+  url 'http://pyyaml.org/download/libyaml/yaml-0.1.3.tar.gz'
+  homepage 'http://pyyaml.org/wiki/LibYAML'
+  md5 'b8ab9064e8e0330423fe640de76608cd'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
