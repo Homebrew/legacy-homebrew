acdb877c559db5d36ab70cfbb585f63d7dfafb4b
Max Howell <max@methylblue.com>
2009-09-28 16:22:07 +0100

    Fix ack download location
    This should be more reliable, still, he could rebase!

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -4,13 +4,8 @@ class Ack <ScriptFileFormula
   # NOTE you don't need to specify the version, usually it is determined
   # automatically by examination of the URL, however in this case our auto
   # determination magic is inadequete
-  @version='1.90'
-  @url="http://github.com/petdance/ack/raw/#{@version}/ack"
-  @md5='d15d059166beff6103d2401aa2d783c7'
-  @homepage='http://betterthangrep.com/'
-
-  # because our url looks like a svn one Homebrew defaults to the svn strategy
-  def download_strategy
-    HttpDownloadStrategy
-  end
+  version '1.90'
+  url "http://github.com/petdance/ack/raw/c03b98d10d44a6eca4218c5a729b07b43d12c8d1/ack"
+  md5 'd15d059166beff6103d2401aa2d783c7'
+  homepage 'http://betterthangrep.com/'
 end
