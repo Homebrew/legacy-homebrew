9971b2ee1412d8f347a65377d6313a3c0a476917
Olivier ROUX <skoot@dailymotion.com>
2009-09-28 14:55:41 +0200

    bchunk formula
    binchunker converts a CD image in a ".bin / .cue" format (sometimes ".raw /
    .cue") to a set of .iso and .cdr tracks.

---
+++ Library/Formula/bchunk.rb
@@ -0,0 +1,15 @@
+require 'brewkit'
+
+class Bchunk <Formula
+  url 'http://he.fi/bchunk/bchunk-1.2.0.tar.gz'
+  homepage 'http://he.fi/bchunk/'
+  md5 '6a613da3f34f9a303f202d2e9731d231'
+
+# depends_on 'cmake'
+
+  def install
+    system "make"
+    bin.install 'bchunk'
+    man1.install 'bchunk.1'
+  end
+end
