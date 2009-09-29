ed371ea06a8482554b8ea1f7f3562f1ce48c2867
Max Howell <max@methylblue.com>
2009-09-29 23:25:50 +0100

    Removing skip_clean? didn't break gawk for me

--- Library/Formula/gawk.rb
+++ Library/Formula/gawk.rb
@@ -5,12 +5,6 @@ class Gawk <Formula
   @homepage='http://www.gnu.org/software/gawk/'
   @md5='674cc5875714315c490b26293d36dfcf'
 
-  # For some reason, without this gawk (but not pgawk)
-  # loses its executable bit.  I have no idea why.
-  def skip_clean? path
-    true
-  end
-
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug"
     system "make"
