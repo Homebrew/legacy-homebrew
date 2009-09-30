85e759383382c30636610cb361f2cd498b3eb10e
Matt Kemp <matt@mattikus.com>
2009-09-30 00:19:25 -0500

    Added formula for tmux
    tmux is a "terminal multiplexer", it enables a number of terminals
    (or windows) to be accessed and controlled from a single terminal.  tmux is
    intended to be a simple, modern, BSD-licensed alternative to
    programs such as GNU screen.

---
+++ Library/Formula/tmux.rb
@@ -0,0 +1,15 @@
+require 'brewkit'
+
+class Tmux <Formula
+  url 'http://downloads.sourceforge.net/tmux/tmux-1.0.tar.gz'
+  homepage 'tmux.sourceforge.net'
+  md5 '716b12d9ea052f57d917bf2869d419df'
+
+  def install
+    ENV['PREFIX'] = "#{prefix}"
+    system "./configure"
+    inreplace "GNUmakefile", " -g bin -o root", ""
+    inreplace "GNUmakefile", "man/man1", "share/man/man1"
+    system "make install"
+  end
+end
