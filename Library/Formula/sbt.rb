c5b11cac08dab0a0ce8ccb191cb5d87d5ad5cb8e
Luke Amdor <luke@absoluterubbish.net>
2009-09-30 10:18:16 -0500

    Updated sbt to 0.5.5

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -1,26 +1,28 @@
 require 'brewkit'
 
 # even though "file -b" reports this as a zip archive, it's just a binary
-class HttpDownloadStrategy
+class SbtHttpDownloadStrategy <HttpDownloadStrategy
   def stage
     FileUtils.mv @dl, File.basename(@url)
   end
 end
 
 class Sbt <Formula
-  @url='http://simple-build-tool.googlecode.com/files/sbt-launcher-0.5.3.jar'
-  @homepage='http://code.google.com/p/simple-build-tool'
-  @md5='ff1a0b6da067573b8a06e4b07707b562'
+  JAR = 'sbt-launcher-0.5.5.jar'
+  url "http://simple-build-tool.googlecode.com/files/#{JAR}"
+  homepage 'http://code.google.com/p/simple-build-tool'
+  md5 'e3593448b3be17ce1666c6241b8d2f90'
+
+  def download_strategy
+    SbtHttpDownloadStrategy
+  end
 
   def install
-    sbt_exec = bin+'sbt'
-    sbt_exec.write <<-EOS
+    (bin+'sbt').write <<-EOS
 #!/bin/sh
 
-java -Xmx512M -jar #{prefix}/sbt-launcher-0.5.3.jar "$@"
+java -Xmx512M -jar #{prefix}/#{JAR} "$@"
 EOS
-
-    File.chmod(0755, sbt_exec)
 
     prefix.install Dir['*']
   end
