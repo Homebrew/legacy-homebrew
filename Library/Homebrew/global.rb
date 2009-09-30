ef38aba714011d9ab23bb04658b46b1af5418fc3
Steve Losh <steve@stevelosh.com>
2009-09-30 02:36:21 -0400

    Determine the repo path in a symlink-friendly way.
    The Contributions/selflink.sh script does not symlink the .git directory,
    so 'brew update' will fail because the prefix (/usr/local/) is not a git
    repository.
    
    To determine the actual location of the git repo we can get the realpath
    of the Library (to expand any symlinks) and take the parent of that.

--- Library/Homebrew/global.rb
+++ Library/Homebrew/global.rb
@@ -35,10 +35,13 @@ else
 end
 
 HOMEBREW_PREFIX = (Pathname.getwd+__FILE__).dirname.parent.parent.cleanpath
-HOMEBREW_REPOSITORY = HOMEBREW_PREFIX.realpath
 HOMEBREW_CELLAR = HOMEBREW_PREFIX+'Cellar'
 HOMEBREW_VERSION = 0.4
 HOMEBREW_WWW = 'http://bit.ly/Homebrew'
+
+# we use Library as we allow people to symlink their Homebrew into another
+# directory so they can hack in one place and use it in another
+HOMEBREW_REPOSITORY = (HOMEBREW_PREFIX+'Library').realpath.parent
 
 MACOS_FULL_VERSION = `/usr/bin/sw_vers -productVersion`.chomp
 MACOS_VERSION = /(10\.\d+)(\.\d+)?/.match(MACOS_FULL_VERSION).captures.first.to_f
