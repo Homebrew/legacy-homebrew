427706ecc6a6de692beeb668b4487d984d47e21a
Adam Vandenberg <flangy@gmail.com>
2009-09-30 12:32:27 -0700

    Formula for Factor (trunk from git).

---
+++ Library/Formula/factor.rb
@@ -0,0 +1,24 @@
+require 'brewkit'
+
+# This formula installs Factor TRUNK, as there is no
+# good stable release. It also takes up to and including
+# "a very long time" to bootstrap the image.
+
+class Factor <Formula
+  url 'git://github.com/slavapestov/factor.git'
+  version 'trunk'
+  homepage 'http://factorcode.org/'
+
+  def install
+    system "make"
+    system "curl http://factorcode.org/images/latest/boot.unix-x86.64.image -O"
+    system "./factor -i=boot.unix-x86.64.image"
+    libexec.install "Factor.app"
+    libexec.install "factor.image"
+  end
+  
+  def caveats
+    "Cocoa app installed to #{libexec}.\n\n"\
+    "Makes use of 'factor.image' in the same folder."
+  end
+end
