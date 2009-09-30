093b2d396a8afa398f8d6271eabf913ce8491660
Max Howell <max@methylblue.com>
2009-09-30 01:50:19 +0100

    OpenSSL is now keg-only
    OS X provides OpenSSL already. Thus policy dictates this formula be keg-only.
    
    In fact policy dictates we shouldn't even provide this beta version in master tree. So I'm not sure if it should even stay. Supposedly erlang needs it. I think this is false.

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,9 +1,11 @@
 require 'brewkit'
 
 class Openssl <Formula
-  @url='http://www.openssl.org/source/openssl-1.0.0-beta3.tar.gz'
-  @homepage='http://www.openssl.org'
-  @md5='cf5a32016bb9da0b9578099727bf15c9'
+  url 'http://www.openssl.org/source/openssl-1.0.0-beta3.tar.gz'
+  homepage 'http://www.openssl.org'
+  md5 'cf5a32016bb9da0b9578099727bf15c9'
+
+  def keg_only?; true end
 
   def install
     ENV.deparallelize
@@ -11,7 +13,7 @@ class Openssl <Formula
     system "make"
     system "make install"
   end
-  
+
   def caveats
     "Please note, OS X provides openssl, this is a slightly newer beta version"
   end
