f0b78897a401397bd409570a76a45717670aefbd
David Reynolds <david@reynoldsfamily.org.uk>
2009-10-01 22:47:28 +0100

    timedog formula
    A Perl script that displays the set of files that were saved for any given
    backup created by Mac OS X Time Machine.

---
+++ Library/Formula/timedog.rb
@@ -0,0 +1,11 @@
+require 'brewkit'
+
+class Timedog <Formula
+  url 'http://timedog.googlecode.com/files/timedog-1.2.zip'
+  md5 '0ab0f08df51ce74a10b94dfd4fb3df27'
+  homepage 'http://timedog.googlecode.com/'
+
+  def install
+    bin.install Dir['timedog']
+  end
+end
