8382935a32e6ef6f360fe34389dbac92e7bad845
Clinton R. Nixon <crnixon@gmail.com>
2009-10-01 16:18:01 -0400

    updated MongoDB formula to version 1.1.1

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -1,9 +1,9 @@
 require 'brewkit'
 
 class Mongodb <Formula
-  @url='http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.0.0.tgz'
-  @homepage='http://www.mongodb.org/'
-  @md5='21a7071f9bbc922c028b11a9b2b6963c'
+  url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.1.1.tgz'
+  homepage 'http://www.mongodb.org/'
+  md5 '8d22a1a5c7ae9d84ff6092e801fd5ebf'
 
   def install
     system "cp -prv * #{prefix}"
