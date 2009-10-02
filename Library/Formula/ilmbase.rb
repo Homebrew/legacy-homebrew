43d351a6865dca869c04bbaa1b0aa372c9601381
David Höppner <0xffea@gmail.com>
2009-10-02 19:54:00 +0200

    Ilmbase formula

---
+++ Library/Formula/ilmbase.rb
@@ -0,0 +1,30 @@
+require 'brewkit'
+
+class Ilmbase <Formula
+  url 'http://download.savannah.gnu.org/releases/openexr/ilmbase-1.0.1.tar.gz'
+  homepage 'http://www.openexr.com/'
+  md5 'f76f094e69a6079b0beb93d97e2a217e'
+
+  def patches
+    DATA
+  end
+
+  def install    
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
+
+__END__
+--- a/configure
++++ b/configure
+@@ -21049,7 +21049,7 @@ Please re-run configure with these options:
+     CXXFLAGS="$CXXFLAGS -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch ppc -arch i386"
+       fi
+ 
+-  CXXFLAGS="$CXXFLAGS -Wno-long-double"
++  CXXFLAGS="$CXXFLAGS"
+   ;;
+ esac
+ 
+
