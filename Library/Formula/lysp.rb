14b90282e74cc1b0a5b692c023bfa0b9b6fb88cf
Ben Alpert <spicyjalapeno@gmail.com>
2009-10-01 18:52:47 -0600

    LYSP formula
    a _really_ tiny lisp

---
+++ Library/Formula/lysp.rb
@@ -0,0 +1,52 @@
+require 'brewkit'
+
+class Lysp <Formula
+  url 'http://www.piumarta.com/software/lysp/lysp-1.0.tar.gz'
+  homepage 'http://www.piumarta.com/software/lysp/'
+  md5 '1cd5df2f83956c5ce58a7394b9d8b92e'
+
+  depends_on 'bdw-gc'
+
+  def patches
+    DATA
+  end
+
+  def install
+    system "make"
+
+    bin.install "lysp"
+    bin.install "gclysp"
+  end
+end
+
+__END__
+diff --git a/Makefile b/Makefile
+index fc3f5d9..0b0e20d 100644
+--- a/Makefile
++++ b/Makefile
+@@ -1,6 +1,3 @@
+-CFLAGS  = -O  -g -Wall
+-CFLAGSO = -O3 -g -Wall -DNDEBUG
+-CFLAGSs = -Os -g -Wall -DNDEBUG
+ LDLIBS  = -rdynamic
+ 
+ all : lysp gclysp
+@@ -10,15 +7,15 @@ lysp : lysp.c gc.c
+ 	size $@
+ 
+ olysp: lysp.c gc.c
+-	$(CC) $(CFLAGSO) -DBDWGC=0 -o $@ lysp.c gc.c $(LDLIBS) -ldl
++	$(CC) $(CFLAGS) -DBDWGC=0 -o $@ lysp.c gc.c $(LDLIBS) -ldl
+ 	size $@
+ 
+ ulysp: lysp.c gc.c
+-	$(CC) $(CFLAGSs) -DBDWGC=0 -o $@ lysp.c gc.c $(LDLIBS) -ldl
++	$(CC) $(CFLAGS) -DBDWGC=0 -o $@ lysp.c gc.c $(LDLIBS) -ldl
+ 	size $@
+ 
+ gclysp: lysp.c
+-	$(CC) $(CFLAGSO) -DBDWGC=1  -o $@ lysp.c $(LDLIBS) -lgc
++	$(CC) $(CFLAGS) -DBDWGC=1  -o $@ lysp.c $(LDLIBS) -lgc
+ 	size $@
+ 
+ run : all
