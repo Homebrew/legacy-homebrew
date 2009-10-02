da49258c18b4cf0cd2cf247e0dbcf23f7c2024b6
Rob Hudson <rob@cogit8.org>
2009-10-02 15:22:45 -0700

    [postgis] Added extra needed files to share and bin.

--- Library/Formula/postgis.rb
+++ Library/Formula/postgis.rb
@@ -12,5 +12,11 @@ class Postgis <Formula
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
+
+    # Copy some of the generated files to the share folder
+    (share+'postgis').install %w( spatial_ref_sys.sql postgis/postgis.sql postgis/postgis_upgrade.sql postgis/uninstall_postgis.sql )
+    # Copy loader and utils binaries to bin folder
+    bin.install %w( loader/pgsql2shp loader/shp2pgsql utils/new_postgis_restore.pl utils/postgis_proc_upgrade.pl utils/postgis_restore.pl utils/profile_intersects.pl )
+
   end
 end
