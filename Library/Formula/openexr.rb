604e6f51bffc23645397cf623756f492da1583d5
David Höppner <0xffea@gmail.com>
2009-10-02 19:56:31 +0200

    Openexr formula
    OpenEXR is a high dynamic-range (HDR) image file format developed by
    Industrial Light & Magic for use in computer imaging applications.

---
+++ Library/Formula/openexr.rb
@@ -0,0 +1,35 @@
+require 'brewkit'
+
+class Openexr <Formula
+  url 'http://download.savannah.gnu.org/releases/openexr/openexr-1.6.1.tar.gz'
+  homepage 'http://www.openexr.com/'
+  md5 '11951f164f9c872b183df75e66de145a'
+
+  depends_on 'ilmbase'
+  depends_on 'pkg-config'
+
+  def patches
+    DATA
+  end
+  
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking"
+    system "make install"
+  end
+end
+
+__END__
+--- a/configure
++++ b/configure
+@@ -21504,7 +21504,7 @@ Please re-run configure with these options:
+     CXXFLAGS="$CXXFLAGS -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch ppc -arch i386"
+       fi
+ 
+-  CXXFLAGS="$CXXFLAGS -Wno-long-double"
++  CXXFLAGS="$CXXFLAGS"
+   ;;
+ esac
+ 
+
