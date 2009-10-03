644b31ce2bddd95c1d55b28f6bbda6d7372d35d8
David Höppner <0xffea@gmail.com>
2009-10-03 18:02:09 +0200

    GNU Lightning formula
    GNU lightning is a library that generates assembly language code
    at run-time; it is very fast, making it ideal for Just-In-Time
    compilers, and it abstracts over the target CPU, as it exposes to
    the clients a standardized RISC instruction set inspired by the
    MIPS and SPARC chips.

---
+++ Library/Formula/lightning.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Lightning <Formula
+  url 'http://ftp.gnu.org/gnu/lightning/lightning-1.2.tar.gz'
+  homepage 'http://www.gnu.org/software/lightning/'
+  md5 'dcd2c46ee4dd5d99edd9930022ad2153'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
