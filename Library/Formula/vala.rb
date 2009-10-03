78df9f5e3d1c6006c3058bf181ce9ba380b63394
David Höppner <0xffea@gmail.com>
2009-10-03 15:08:27 +0200

    Vala formula update to new version

--- Library/Formula/vala.rb
+++ Library/Formula/vala.rb
@@ -1,9 +1,9 @@
 require 'brewkit'
 
 class Vala <Formula
-  @url='http://download.gnome.org/sources/vala/0.7/vala-0.7.5.tar.bz2'
+  @url='http://download.gnome.org/sources/vala/0.7/vala-0.7.7.tar.bz2'
   @homepage='http://live.gnome.org/Vala'
-  @md5='a102d582b2ac75b6bcdc5785683263fc'
+  @md5='7d11fcddb2bd30b9ecbdacfaa20f2769'
 
   depends_on 'glib'
   depends_on 'gettext'
