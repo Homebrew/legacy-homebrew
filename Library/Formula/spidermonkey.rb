8ee8f2432a34ef1a24d140716abc5f86e644b5d7
Max Howell <max@methylblue.com>
2009-10-04 15:43:04 +0100

    Make Spidermonkey work with Couchdb
    I had to remove the THREADSAFE flag to make it work.
    
    Couchdb seems the reason to use Spidermonkey at this point. Without these
    changes Couchdb would crash hard when loading the Spidermonkey dylib.
    
    If the threadsafety is required then we should look into making a keg-only
    Spidermonkey for Couchdb's personal use.
    
    The nspr dependency isn't documented as required, and doesn't seem to make a
    difference if it is removed. So I removed it.

--- Library/Formula/spidermonkey.rb
+++ Library/Formula/spidermonkey.rb
@@ -1,12 +1,11 @@
 require 'brewkit'
 
 class Spidermonkey <Formula  
-  @url="http://ftp.mozilla.org/pub/mozilla.org/js/js-1.7.0.tar.gz"
-  @homepage='https://developer.mozilla.org/en/SpiderMonkey'
-  @md5='5571134c3863686b623ebe4e6b1f6fe6'
+  url "http://ftp.mozilla.org/pub/mozilla.org/js/js-1.7.0.tar.gz"
+  homepage 'https://developer.mozilla.org/en/SpiderMonkey'
+  md5 '5571134c3863686b623ebe4e6b1f6fe6'
 
   depends_on 'readline'
-  depends_on 'nspr'
 
   def patches
     DATA
@@ -26,9 +25,9 @@ class Spidermonkey <Formula
     ENV['CFLAGS'] = ENV['CFLAGS'].gsub(/-msse[^\s]+/, '')
 
     Dir.chdir "src" do
-      system "make JS_DIST=#{HOMEBREW_PREFIX} JS_THREADSAFE=1 DEFINES=-DJS_C_STRINGS_ARE_UTF8 -f Makefile.ref"
-      system "make JS_DIST=#{prefix} -f Makefile.ref export"
-      system "ranlib #{prefix}/lib/libjs.a"
+      system "make DEFINES=-DJS_C_STRINGS_ARE_UTF8 -f Makefile.ref"
+      system "make JS_DIST='#{prefix}' -f Makefile.ref export"
+      system "ranlib #{lib}/libjs.a"
     end
   end
 end
