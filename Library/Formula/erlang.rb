b1a819ecae6915851dd2b094933710f522a91a28
Max Howell <max@methylblue.com>
2009-10-04 10:18:07 +0100

    Fixes #61; Erlang crypto driver fails to load
    Turns out the problem was zealous stripping… again.

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -1,41 +1,23 @@
 require 'brewkit'
 
 class ErlangManuals <Formula
-  @version='R13B02-1'
-  @homepage='http://www.erlang.org'
-  @url='http://www.erlang.org/download/otp_doc_man_R13B02-1.tar.gz'
-  @md5='b5f7b20faa049a8b6a753fc7a462d02d'
+  url 'http://www.erlang.org/download/otp_doc_man_R13B02-1.tar.gz'
+  md5 'b5f7b20faa049a8b6a753fc7a462d02d'
 end
 
 class ErlangHtmlDocs <Formula
-  @version='R13B02-1'
-  @homepage='http://www.erlang.org'
-  @url='http://erlang.org/download/otp_doc_html_R13B02-1.tar.gz'
-  @md5='d48da533b49f7b32c94032f2a53c0073'
+  url 'http://erlang.org/download/otp_doc_html_R13B02-1.tar.gz'
+  md5 'd48da533b49f7b32c94032f2a53c0073'
 end
 
 class Erlang <Formula
-  @version='R13B02-1'
-  @homepage='http://www.erlang.org'
-  @url='http://erlang.org/download/otp_src_R13B02-1.tar.gz'
-  @md5='2593b9312eb1b15bf23a968743138c52'
-
-  # def patches
-  #   [
-  #     "http://pastie.org/603456.txt",
-  #     "http://pastie.org/603462.txt",
-  #     "http://svn.macports.org/repository/macports/trunk/dports/lang/erlang/files/patch-lib-erl_interface-include-ei.h.diff",
-  #     "http://svn.macports.org/repository/macports/trunk/dports/lang/erlang/files/patch-lib-erl_interface-src-connect-ei_connect.c.diff",
-  #     "http://pastie.org/603469.txt",
-  #     "http://pastie.org/603475.txt",
-  #     "http://pastie.org/603478.txt",
-  #     "http://pastie.org/603480.txt",
-  #     "http://pastie.org/603481.txt",
-  #     "http://pastie.org/603485.txt"
-  #   ]
-  # end
+  url 'http://erlang.org/download/otp_src_R13B02-1.tar.gz'
+  md5 '2593b9312eb1b15bf23a968743138c52'
+  version 'R13B02-1'
+  homepage 'http://www.erlang.org'
 
   depends_on 'icu4c'
+  skip_clean 'lib'
 
   def install
     ENV.deparallelize
