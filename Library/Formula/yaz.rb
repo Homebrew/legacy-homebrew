5a6b52edfb5e1872ce839271a640acf26f717f5a
Walter McGinnis <walter@katipo.co.nz>
2009-10-06 12:12:01 +1300

    Upgraded to yaz 3.0.49 release.

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -4,9 +4,9 @@ require 'brewkit'
 # if this is a problem for you then please fix it! Thanks.
 
 class Yaz <Formula
-  url 'http://ftp.indexdata.dk/pub/yaz/yaz-3.0.48.tar.gz'
+  url 'http://ftp.indexdata.dk/pub/yaz/yaz-3.0.49.tar.gz'
   homepage 'http://www.indexdata.com/yaz'
-  md5 '6cf27fd487c6ae7d0cbc6dc9e8ae3ba8'
+  md5 '7402c6444386dc7db6be8bb9617e1e4d'
 
   # depends_on 'openssl'   # we can suffice with the os x default for now
 
