3e5e29ab7481b3259ebb23980351adfef2e16309
David Conrad <lessen42@gmail.com>
2009-10-06 01:56:38 -0400

    Update libogg, libvorbis, and libtheora

--- Library/Formula/theora.rb
+++ Library/Formula/theora.rb
@@ -1,9 +1,9 @@
 require 'brewkit'
 
 class Theora <Formula
-  url 'http://downloads.xiph.org/releases/theora/libtheora-1.1.0.tar.bz2'
+  url 'http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2'
   homepage 'http://www.theora.org/'
-  md5 'd0f83cf7f13e2b3bd068a858ca1398ad'
+  md5 '292ab65cedd5021d6b7ddd117e07cd8e'
 
   depends_on 'libogg'
   depends_on 'libvorbis'
