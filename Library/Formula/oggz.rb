ffec849b05989358be3fc356dc6302cd464aab55
David Conrad <lessen42@gmail.com>
2009-10-06 02:06:05 -0400

    Oggz formula
    Signed-off-by: Max Howell <max@methylblue.com>
    
    The formula was committed as liboggz.rb, but the homepage is consistent in
    calling it Oggz so I renamed the formula.

---
+++ Library/Formula/oggz.rb
@@ -0,0 +1,16 @@
+require 'brewkit'
+
+# TODO alias: liboggz
+
+class Oggz <Formula
+  url 'http://downloads.xiph.org/releases/liboggz/liboggz-1.0.0.tar.gz'
+  md5 '57359f6f0824b3e9bad85b49a6418514'
+  homepage 'http://www.xiph.org/oggz/'
+
+  depends_on 'libogg'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
