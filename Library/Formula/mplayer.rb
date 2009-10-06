3dd01b6c9199ce10e3514c2ff6c7c4c2158f08c2
David Conrad <lessen42@gmail.com>
2009-10-06 00:09:57 -0400

    MPlayer formula fixes
    Don't force x86_64; configure detects it properly now
    
    Use gcc-4.2 as llvm-gcc-4.2 chokes on mlp inline asm
    
    Add recommended dependancy on pkg-config

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -3,12 +3,12 @@ require 'brewkit'
 class Mplayer <Formula
   @homepage='http://www.mplayerhq.hu/'
   @head='svn://svn.mplayerhq.hu/mplayer/trunk'
+  
+  depends_on 'pkg-config' => :recommended
 
   def install
-    # Information from http://blog.bloople.net/read/mplayer-on-snow-leopard
-    # seems to claim that we have to build against an x86_64 target even if on
-    # i386 /-:
-    system "./configure --prefix='#{prefix}' --target=x86_64-Darwin"
+    ENV.gcc_4_2   # llvm chokes on labels within mlp inline asm
+    system "./configure --prefix='#{prefix}'"
     system "make"
     system "make install"
   end
