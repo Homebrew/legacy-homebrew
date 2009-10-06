9aa47b1b53099294f06eaa2968453092ae59287a
David Conrad <lessen42@gmail.com>
2009-10-05 23:49:48 -0400

    ffmpeg configure script detects x86_64 correctly on 10.6 as of r20134

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -1,8 +1,9 @@
 require 'brewkit'
 
 class Ffmpeg <Formula
-  @head='svn://svn.ffmpeg.org/ffmpeg/trunk'
-  @homepage='http://ffmpeg.org/'
+  head 'svn://svn.ffmpeg.org/ffmpeg/trunk'
+  homepage 'http://ffmpeg.org/'
+
   depends_on 'x264'
 
   def install
@@ -11,7 +12,6 @@ class Ffmpeg <Formula
                           "--enable-nonfree",
                           "--enable-gpl",
                           "--enable-libx264"]
-    configure_flags << "--arch=x64_64" if Hardware.is_64_bit? and MACOS_VERSION >= 10.6
     system "./configure", *configure_flags
     system "make install"
   end
