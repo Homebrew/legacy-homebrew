6e2d51e2367acc49911d2660a8253b412db9a404
David Höppner <0xffea@gmail.com>
2009-10-07 12:24:34 +0200

    Dirac formula

---
+++ Library/Formula/dirac.rb
@@ -0,0 +1,14 @@
+require 'brewkit'
+
+class Dirac <Formula
+  url 'http://diracvideo.org/download/dirac-research/dirac-1.0.2.tar.gz'
+  md5 'a57c2c5e58062d437d9ab13dffb28f0f'
+  homepage 'http://diracvideo.org/'
+
+  def install
+    ENV.gcc_4_2
+
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
