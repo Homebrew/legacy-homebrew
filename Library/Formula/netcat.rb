f2971afaff3bd7d35e088952ef88514b4bfc89d0
Mike Skalnik <mike.skalnik@gmail.com>
2009-10-07 17:59:47 -0400

    Added netcat formula

---
+++ Library/Formula/netcat.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Netcat <Formula
+  url 'http://downloads.sourceforge.net/sourceforge/netcat/netcat-0.7.1.tar.bz2'
+  md5 '0a29eff1736ddb5effd0b1ec1f6fe0ef'
+  homepage 'http://netcat.sourceforge.net/'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
