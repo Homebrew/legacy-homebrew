f6d581c21d0a92ae419881d8241533da3d8d3fa2
David Höppner <0xffea@gmail.com>
2009-10-09 19:53:01 +0200

    Libcddb formula
    Libcddb is a C library to access data on a CDDB server.

---
+++ Library/Formula/libcddb.rb
@@ -0,0 +1,15 @@
+require 'brewkit'
+
+class Libcddb <Formula
+  url 'http://prdownloads.sourceforge.net/libcddb/libcddb-1.3.2.tar.bz2'
+  md5 '8bb4a6f542197e8e9648ae597cd6bc8a'
+  homepage 'http://libcddb.sourceforge.net/'
+
+  depends_on 'pkg-config'
+  depends_on 'libcdio'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
