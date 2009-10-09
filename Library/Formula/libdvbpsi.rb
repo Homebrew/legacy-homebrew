8d94d43d5c550152a916423d89d024bff225a402
David Höppner <0xffea@gmail.com>
2009-10-09 20:07:00 +0200

    Libdvbpsi formula
    libdvbpsi is a simple library designed for decoding and generation
    of MPEG TS and DVB PSI tables.

---
+++ Library/Formula/libdvbpsi.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Libdvbpsi <Formula
+  url 'http://download.videolan.org/pub/libdvbpsi/0.1.6/libdvbpsi5-0.1.6.tar.bz2'
+  md5 'bd2d9861be3311e1e03c91cd9345f542'
+  homepage 'http://www.videolan.org/developers/libdvbpsi.html'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--enable-release"
+    system "make install"
+  end
+end
