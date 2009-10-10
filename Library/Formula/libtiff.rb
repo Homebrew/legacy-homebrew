f47a21609718b3ec646f05828f87698c93d3ba6d
Andrew White <andyw@pixeltrix.co.uk>
2009-10-10 09:49:17 +0100

    Update to tiff-3.9.1

--- Library/Formula/libtiff.rb
+++ Library/Formula/libtiff.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libtiff <Formula
-  @url='ftp://ftp.remotesensing.org/libtiff/tiff-3.8.2.tar.gz'
+  @url='ftp://ftp.remotesensing.org/libtiff/tiff-3.9.1.tar.gz'
   @homepage='http://www.libtiff.org/'
-  @md5='fbb6f446ea4ed18955e2714934e5b698'
+  @md5='63c59a44f34ae0787f2d71de3d256e20'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--mandir=#{prefix}/share/man"
