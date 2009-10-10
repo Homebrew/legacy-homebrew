ee3401dfe198ce1c7007174bddc1dde8cd972a30
Andre Arko <andre@arko.net>
2009-10-10 15:47:16 -0700

    Formula for MRTG, a tool to monitor server stats using SNMP
    Also MRTG's dep, the GD library

---
+++ Library/Formula/gd.rb
@@ -0,0 +1,13 @@
+require 'brewkit'
+
+class Gd <Formula
+  version "2.0.36RC1"
+  url "http://www.libgd.org/releases/gd-#{version}.tar.gz"
+  homepage "http://www.libgd.org"
+  md5 "39ac48e6d5e0012a3bd2248a0102f209"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
