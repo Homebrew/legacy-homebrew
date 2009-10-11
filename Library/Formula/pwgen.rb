a7a9f0c8aca520c534bbf56b3511691642c0fd74
Jannis Leidel <jannis@leidel.info>
2009-10-11 13:32:25 +0200

    pwgen formula

---
+++ Library/Formula/pwgen.rb
@@ -0,0 +1,12 @@
+require 'brewkit'
+
+class Pwgen <Formula
+  url 'http://downloads.sourceforge.net/project/pwgen/pwgen/2.06/pwgen-2.06.tar.gz'
+  homepage 'http://pwgen.sourceforge.net/'
+  md5 '935aebcbe610fbc9de8125e7b7d71297'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
