9bd0f725c49d403d20646715cebf3b2529fdf42a
Jiayong Ou <jou@kozue.room101.xx>
2009-10-12 22:45:40 +0200

    pkg-config support for mad
    Added a .pc file so that pkg-config can find mad.

--- Library/Formula/mad.rb
+++ Library/Formula/mad.rb
@@ -5,8 +5,27 @@ class Mad <Formula
   url 'http://downloads.sourceforge.net/project/mad/libmad/0.15.1b/libmad-0.15.1b.tar.gz'
   md5 '1be543bc30c56fb6bea1d7bf6a64e66c'
 
+  def mad_pc
+    return <<-EOS
+prefix=#{HOMEBREW_PREFIX}
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+
+Name: mad
+Description: MPEG Audio Decoder
+Version: #{@version}
+Requires:
+Conflicts:
+Libs: -L${libdir} -lmad -lm
+Cflags: -I${includedir}
+    EOS
+  end
+
   def install
     system "./configure --disable-debugging --enable-fpm=intel --prefix='#{prefix}'"
     system "make install"
+
+    (lib+'pkgconfig'+'mad.pc').write mad_pc
   end
 end
