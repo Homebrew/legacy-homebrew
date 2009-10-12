f09224f53a9b1c2ed4c6a33598f48c9c0fe007ff
Max Howell <max@methylblue.com>
2009-10-12 14:16:51 +0100

    Git 1.6.5
    No longer builds with LLVM due to a bug in LLVM headers.
    
    Offending lines contain htonl().

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -1,16 +1,20 @@
 require 'brewkit'
 
 class GitManuals <Formula
-  url 'http://kernel.org/pub/software/scm/git/git-manpages-1.6.4.4.tar.bz2'
-  md5 '5ee880f408f1299d541c273fb7e3c1db'
+  url 'http://kernel.org/pub/software/scm/git/git-manpages-1.6.5.tar.bz2'
+  md5 '17b46ccbec5e02e82a76f7ee81a0b182'
 end
 
 class Git <Formula
-  url 'http://kernel.org/pub/software/scm/git/git-1.6.4.4.tar.bz2'
-  md5 'b150352782998ca1f84185e6af53ec26'
+  url 'http://kernel.org/pub/software/scm/git/git-1.6.5.tar.bz2'
+  md5 'da86c1736c55edb9f446828581137b51'
   homepage 'http://git-scm.com'
 
   def install
+    # sadly, there's a bug in LLVM:
+    # http://www.mail-archive.com/llvmbugs@cs.uiuc.edu/msg03791.html
+    ENV.gcc_4_2
+
     # if these things are installed, tell git build system to not use them
     ENV['NO_FINK']='1'
     ENV['NO_DARWIN_PORTS']='1'
