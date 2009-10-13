d0ae2921310b51e0a6754d19f589f18a089989a0
Jiayong Ou <jou@orly.ch>
2009-10-13 15:16:33 +0200

    shell.fm formula
    shell-fm is a lightweight, console-based player for last.fm radio streams.
    
    Signed-off-by: Max Howell <max@methylblue.com>
    
    The h1 on the page is Shell.fm, everywhere else he calls it shell-fm. Thanks developer-guy. I switched the formula to .fm as that is more consitent with how you would write it if you heard it mentioned in a podcast etc.

---
+++ Library/Formula/shell.fm.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+#TODO aliases: shell-fm
+
+class ShellFm <Formula
+  url 'http://github.com/jkramer/shell-fm/tarball/v0.7'
+  version '0.7'
+  homepage 'http://nex.scrapping.cc/shell-fm/'
+  md5 '3f83866622a892ee89685f1ed079eefd'
+
+  # homepage says that libao is optional, but it doesn't seem
+  # to build without it…
+  depends_on 'libao'
+  depends_on 'mad'
+
+  def install
+    system "make"
+    bin.install 'source/shell-fm'
+    man1.install 'manual/shell-fm.1.gz'
+  end
+end
