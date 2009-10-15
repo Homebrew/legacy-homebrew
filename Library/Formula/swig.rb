e25b647e6a79ced7464ecf077dd1b4dfd4e06c46
Max Howell <max@methylblue.com>
2009-10-15 15:53:03 +0100

    Deversion swig/share
    Versioning dirs is redundant with Homebrew so I try to remove it where possible.

--- Library/Formula/swig.rb
+++ Library/Formula/swig.rb
@@ -5,9 +5,15 @@ class Swig <Formula
   homepage 'http://www.swig.org/'
   md5 '2df766c9e03e02811b1ab4bba1c7b9cc'
 
+  def patches
+    DATA
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-    system "make"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking",
+                         # turns prefix/share/swig/1.3.40 into prefix/share/swig
+                         # as versioned dirs are redundant with Homebrew
+                          "--with-swiglibdir=#{prefix}/share/swig"
     system "make install"
   end
 end
