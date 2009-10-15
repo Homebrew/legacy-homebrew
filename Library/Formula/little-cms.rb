4bed772808379cbeb7c08b04a1906e88acfab59b
rick <technoweenie@gmail.com>
2009-10-15 16:40:35 -0700

    Update littlecms to 1.18a

--- Library/Formula/little-cms.rb
+++ Library/Formula/little-cms.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class LittleCms <Formula
-  @url='http://www.littlecms.com/lcms-1.17.tar.gz'
-  @homepage='http://www.littlecms.com/'
-  @md5='07bdbb4cfb05d21caa58fe3d1c84ddc1'
+  url 'http://www.littlecms.com/lcms-1.18a.tar.gz'
+  homepage 'http://www.littlecms.com/'
+  md5 'f4abfe1c57ea3f633c2e9d034e74e3e8'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug"
