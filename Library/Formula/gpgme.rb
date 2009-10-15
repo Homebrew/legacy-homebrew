065fc37ec25ce5a438c23081fc45ef3b1798252d
Dean Strelau <dean@mintdigital.com>
2009-10-15 15:04:16 -0400

    GPGME formula
    The GNU Privacy Guard Made Easy C library.

---
+++ Library/Formula/gpgme.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Gpgme < Formula
+  url 'ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-1.1.8.tar.bz2'
+  homepage 'http://www.gnupg.org'
+  sha1 'e56da614f3e6acc1cb51af767c77f4a95b05b1e8'
+
+  depends_on 'libgpg-error'
+
+  def install
+    ENV.gcc_4_2
+    system "./configure", "--prefix=#{prefix}",
+      "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
