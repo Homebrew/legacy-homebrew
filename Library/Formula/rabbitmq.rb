febf611ce3437468050931aa313d0b74d7c9ccb3
Matthew Scott <gldnspud@gmail.com>
2009-10-17 12:27:27 -0700

    Fix rabbitmq 1.7.0 startup
    (rabbitmq 1.7.0 has centralized rabbitmq-env to locate binaries)

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -20,13 +20,16 @@ class Rabbitmq <Formula
     (var + "lib" + "rabbitmq").mkpath
     (var + "log" + "couchdb").mkpath
 
-    %w{rabbitmq-server rabbitmq-multi rabbitmqctl}.each do |script|
+    %w{rabbitmq-server rabbitmq-multi rabbitmqctl rabbitmq-env}.each do |script|
       inreplace sbin+script, '/etc/rabbitmq', "#{etc}/rabbitmq"
       inreplace sbin+script, '/var/log/rabbitmq', "#{var}/log/rabbitmq"
       inreplace sbin+script, '/var/lib/rabbitmq', "#{var}/lib/rabbitmq"
+    end
+
+    %w{rabbitmq-env}.each do |script|
       # RabbitMQ Erlang binaries are installed in lib/rabbitmq/erlang/lib/rabbitmq-x.y.z/ebin
       # therefore need to add this path for erl -pa
-      inreplace sbin+script, '`dirname $0`/..', "#{target_dir}"
+      inreplace sbin+script, '${SCRIPT_DIR}/..', "#{target_dir}"
     end
   end
 end
