65f2cf19984f037baa3bc4674e763c9f29c30eb8
Mihai Anca <ropiku@gmail.com>
2009-10-19 19:54:43 +0100

    Updated Tokyo Cabinet to 1.4.35

--- Library/Formula/tokyo-cabinet.rb
+++ Library/Formula/tokyo-cabinet.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class TokyoCabinet <Formula
-  @url='http://1978th.net/tokyocabinet/tokyocabinet-1.4.33.tar.gz'
-  @homepage='http://1978th.net/tokyocabinet'
-  @sha1='c3ded8ee0bde93f072b9436a6244dc7690abd5c6'
+  url 'http://1978th.net/tokyocabinet/tokyocabinet-1.4.35.tar.gz'
+  homepage 'http://1978th.net/tokyocabinet'
+  sha1 '3c97e96d3b304121ec0d4eaf5000bf6cc6b727e6'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--enable-fastest"
+    system "./configure", "--prefix=#{prefix}", "--enable-fastest",
+            "--libdir=#{lib}", "--includedir=#{include}"
     system "make"
     system "make install"
   end
