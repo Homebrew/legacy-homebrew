a48e4b1324753d21f39601850868af64c234a45a
David Reynolds <david@reynoldsfamily.org.uk>
2009-10-20 22:01:32 +0100

    Remove some unecessary compiler flags

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -1,16 +1,14 @@
 require 'formula'
 
 class Lftp <Formula
-  @url='http://ftp.yars.free.net/pub/source/lftp/lftp-3.7.15.tar.gz'
-  @homepage='http://lftp.yar.ru/'
-  @md5='6c43ffdb59234ff0533cfdda0c3c305c'
+  url 'http://ftp.yars.free.net/pub/source/lftp/lftp-3.7.15.tar.gz'
+  homepage 'http://lftp.yar.ru/'
+  md5 '6c43ffdb59234ff0533cfdda0c3c305c'
 
   depends_on 'readline'
 
   def install
-    ENV['CXXFLAGS'] += " -fno-exceptions -fno-rtti -fno-implement-inlines"
-    ENV['LDFLAGS'] += " -Xlinker -search_paths_first -L/usr/local/lib"
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-openssl", "--disable-shared", "--disable-nls"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
 end
