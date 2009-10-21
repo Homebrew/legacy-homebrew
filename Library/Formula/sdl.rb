023f577e5d36791ca49ccfdbe2e8c2ec61d11f13
Adam Vandenberg <flangy@gmail.com>
2009-10-20 20:11:35 -0700

    Update SDL for Snow Leopard. Add libmikmod.

--- Library/Formula/sdl.rb
+++ Library/Formula/sdl.rb
@@ -1,19 +1,14 @@
 require 'formula'
 
 class Sdl <Formula
-  @url='http://www.libsdl.org/release/SDL-1.2.13.tar.gz'
-  @homepage='http://www.libsdl.org/index.php'
-  @md5='c6660feea2a6834de10bc71b2f8e4d88'
-
-  def patches
-    { :p0 => "http://methylblue.com/junk/libsdl-1.2.13-10.6.patch" }
-  end
+  url 'http://www.libsdl.org/release/SDL-1.2.14.tar.gz'
+  homepage 'http://www.libsdl.org/'
+  md5 'e52086d1b508fa0b76c52ee30b55bec4'
 
   def install
     ENV.gcc_4_2
     system "./configure", "--prefix=#{prefix}", "--disable-debug",
                           "--disable-dependency-tracking",
-                          "--disable-video-x11",
                           "--disable-nasm"
     system "make install"
     
