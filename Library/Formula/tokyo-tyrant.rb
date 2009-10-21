62eff05096796a034df281afc5ea713fcf6a34d8
Mel Gray <melgray@gmail.com>
2009-10-20 17:37:33 -0700

    Latest version wouldn't work with Ruby bindings
    So bumping down.

--- Library/Formula/tokyo-tyrant.rb
+++ Library/Formula/tokyo-tyrant.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class TokyoTyrant <Formula
-  url 'http://1978th.net/tokyotyrant/tokyotyrant-1.1.36.tar.gz'
+  # For some reason TT-1.1.36 wouldn't compile against TC-1.4.33
+  # Also it appears the Ruby (Rufus) bindings will only work with up to 1.1.33
+  url 'http://1978th.net/tokyotyrant/tokyotyrant-1.1.33.tar.gz'
   homepage 'http://1978th.net/tokyotyrant/'
-  sha1 '116c413affdc649602439adc815c9dcb133cc502'
+  md5 '48b153ed85b5f7057eedd3ce304eca34'
 
   depends_on 'tokyo-cabinet'
   depends_on 'lua'
