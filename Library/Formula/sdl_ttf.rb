0b7e5cd4eb57445f941b1307d3bdb13d01ac90ec
Max Howell <max@methylblue.com>
2009-10-23 19:22:39 +0100

    Fixes #77

--- Library/Formula/sdl_ttf.rb
+++ Library/Formula/sdl_ttf.rb
@@ -11,16 +11,10 @@ class SdlTtf <Formula
     ENV.x11 # For Freetype
 
     system "./configure", "--prefix=#{prefix}",
-                          "--includedir=#{prefix}/priv_include",
                           "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-sdltest",
                           "--with-freetype-exec-prefix=/usr/X11"
     system "make install"
-
-    # Hack alert:
-    # Since SDL is installed as a dependency, we know it exists, so we
-    # symlink our new header file into its brewed location.
-    FileUtils.ln_s "#{prefix}/priv_include/SDL/SDL_ttf.h", "#{HOMEBREW_PREFIX}/include/SDL"
   end
 end
