7e43f6ffdddb723bcb93588b55be7b7e3c4c4347
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-10-26 16:52:46 +0100

    Optionally run "make check" or build as shared library when installing Gambit Scheme

--- Library/Formula/gambit-scheme.rb
+++ Library/Formula/gambit-scheme.rb
@@ -5,6 +5,13 @@ class GambitScheme <Formula
   homepage 'http://dynamo.iro.umontreal.ca/~gambit/wiki/index.php/Main_Page'
   md5 '71bd4b5858f807c4a8ce6ce68737db16'
 
+  def options
+    [
+      ['--with-check', 'Execute "make check" before installing. Runs some basic scheme programs to ensure that gsi and gsc are working'],
+      ['--enable-shared', 'Build Gambit Scheme runtime as shared library']
+    ]
+  end
+
   def install
     # Gambit Scheme currently fails to build with llvm-gcc
     # (ld crashes during the build process)
@@ -13,12 +20,19 @@ class GambitScheme <Formula
     # optimizations until safe values can be figured out.
     ENV.no_optimization
 
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug",
-                          # Recommended to improve the execution speed and compactness
-                          # of the generated executables. Increases compilation times.
-                          "--enable-single-host"
+    configure_args = [
+      "--prefix=#{prefix}",
+      "--disable-debug",
+      # Recommended to improve the execution speed and compactness
+      # of the generated executables. Increases compilation times.
+      "--enable-single-host"
+    ]
 
+    configure_args << "--enable-shared" if ARGV.include? '--enable-shared'
+
+    system "./configure", *configure_args
+
+    system "make check" if ARGV.include? '--with-check'
     system "make install"
   end
 end
