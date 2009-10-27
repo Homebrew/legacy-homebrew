b1cc1c8bd181e0f1413b2060a3addb6b5b6ed3eb
Chris Thachuk <cthachuk@cs.ubc.ca>
2009-10-27 16:02:05 -0700

    GNU Binutils formula.
    The GNU Binutils are a collection of binary tools.

---
+++ Library/Formula/binutils.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Binutils <Formula
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.20.tar.gz'
+  homepage 'http://www.gnu.org/software/binutils/binutils.html'
+  md5 'e99487e0c4343d6fa68b7c464ff4a962'
+
+  def install
+    ENV['CPPFLAGS']="#{ENV['CPPFLAGS']} -I${prefix}/include"
+    system "./configure", "--prefix=#{prefix}", \
+                          "--disable-debug", \
+                          "--disable-dependency-tracking", \
+                          "--infodir=#{prefix}/share/info", \
+                          "--mandir=#{prefix}/share/man", \
+                          "--disable-werror", \
+                          "--program-prefix=g"
+    system "make"
+    system "make install"
+  end
+end
