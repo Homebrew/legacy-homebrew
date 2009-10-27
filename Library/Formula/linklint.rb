9db1d786ffb139407c0acbc21b335434bbdf415a
Caleb Land <caleb.land@gmail.com>
2009-10-27 13:49:38 -0400

    added a formula for linklint
    linklint is a link checking spider
    
    Signed-off-by: Max Howell <max@methylblue.com>
    
    I amended it somewhat heavily to only install what was needed.

---
+++ Library/Formula/linklint.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Linklint <Formula
+  url 'http://linklint.org/download/linklint-2.3.5.tar.gz'
+  homepage 'http://linklint.org'
+  md5 'c1ae0860199da59ded28771d1fa7b800'
+
+  def install
+    FileUtils.mv 'READ_ME.txt', 'README'
+    bin.install 'linklint-2.3.5'
+    Dir.chdir bin
+    FileUtils.mv 'linklint-2.3.5', 'linklint'
+  end
+end
