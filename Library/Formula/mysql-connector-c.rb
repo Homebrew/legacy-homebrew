3f1680500a86b129f7f4de84672c64ff145533fe
Adam Vandenberg <flangy@gmail.com>
2009-10-28 11:40:52 -0700

    Fix md5 on MySQL Connector (C)

--- Library/Formula/mysql-connector-c.rb
+++ Library/Formula/mysql-connector-c.rb
@@ -3,7 +3,7 @@ require 'formula'
 class MysqlConnectorC <Formula
   @homepage='http://dev.mysql.com/downloads/connector/c/6.0.html'
   @url='http://mysql.llarian.net/Downloads/Connector-C/mysql-connector-c-6.0.2.tar.gz'
-  @md5='67e478df66e8f66536e54388cfa29854'
+  @md5='f922b778abdd25f7c1c95a8329144d56'
 
   depends_on 'cmake'
 
