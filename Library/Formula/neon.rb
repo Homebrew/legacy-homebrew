9439f7555bfb4bebf75dd7d7e4e14caf0ba4a129
Max Howell <max@methylblue.com>
2009-11-04 15:50:12 +0000

    Make Neon keg-only
    Also make it build a dylib, not a static library. This is because the default linker behaviour is to run through the search path twice. First time looking for dylibs and second time for .a files. So duplicate formula must build a dylib.
    
    The other option is the -search_paths_first link flag. I don't like this though as every flag we bung in there adds more possible compile issues to projects that depend on such flags being off.

--- Library/Formula/neon.rb
+++ Library/Formula/neon.rb
@@ -5,10 +5,16 @@ class Neon <Formula
   @homepage='http://www.webdav.org/neon/'
   @md5='18a3764b70f9317f8b61509fd90d9e7a'
 
+  def keg_only?
+    :provided_by_osx
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--disable-debug", 
                           "--disable-dependency-tracking",
+                          "--enable-shared",
+                          "--disable-static",
                           "--with-ssl"
     system "make install"
   end
