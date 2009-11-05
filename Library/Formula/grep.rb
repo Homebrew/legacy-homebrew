039e2d15c08793521ac34f9758cd8289d3e9a240
Adam Vandenberg <flangy@gmail.com>
2009-11-05 10:26:31 -0800

    GNU Grep 2.5.4
    This is newer than the OS X provided Grep, and has new features required by
    the nice_find plugin for TextMate:
    
        http://github.com/briancollins/nice_find
    
    "OS X grep doesn't support --exclude-dir and --exclude only works on filenames"

---
+++ Library/Formula/grep.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Grep <Formula
+  homepage 'http://www.gnu.org/software/grep/'
+  url 'http://mirror.its.uidaho.edu/pub/gnu/grep/grep-2.5.4.tar.bz2'
+  md5 '5650ee2ae6ea4b39e9459d7d0585b315'
+
+  depends_on 'gettext'
+  depends_on 'pcre'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-nls",
+                          "--infodir=#{info}",
+                          "--mandir=#{man}"
+
+    # Configure gives me 2 copies of -lpcre and no -lintl, so fix that
+    inreplace "src/Makefile", /^LIBS = .*$/, "LIBS = -lintl  -lpcre"
+
+    system "make"
+    system "make install"
+  end
+end
