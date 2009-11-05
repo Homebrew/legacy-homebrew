3da4e7b741ee0b3296dd679e12bffece12688a63
Max Howell <max@methylblue.com>
2009-11-05 18:13:00 +0000

    Install into Homebrew prefix
    This is the problem with doing our own Python stuff. But easy_install doesn't
    work great. The solution is probably jezdez's pip formula.

--- Library/Formula/pyqt.rb
+++ Library/Formula/pyqt.rb
@@ -1,15 +1,36 @@
 require 'formula'
 
-class Pyqt <Formula
-  url 'http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-mac-gpl-4.6.tar.gz'
-  homepage 'http://www.riverbankcomputing.co.uk/software/pyqt'
-  md5 '35258016c30a80f97689e643fba4704b'
+# NOTE this should be provided by pip eventually
+# currently easy_install doesn't seem to support it
 
+class Pyqt <Formula
+  url 'http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-mac-gpl-4.6.1.tar.gz'
+  homepage 'http://www.riverbankcomputing.co.uk/software/pyqt'
+  md5 'c04aade8b63795f30ee7da2d3405d7e7'
+
+  depends_on 'sip'
   depends_on 'qt'
 
   def install
-    system "python", "./configure.py", "-g"
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    ENV.prepend 'PYTHONPATH', "#{HOMEBREW_PREFIX}/lib/python", ':'
+    
+    system "python", "./configure.py", "-g", "--confirm-license",
+                                       "--bindir=#{bin}",
+                                       "--destdir=#{lib}/python",
+                                       "--sipdir=#{share}/sip"
+    system "make"
     system "make install"
+  end
+  
+  def caveats; <<-EOS
+This formula won't function until you add the following to your PYTHONPATH
+environment variable:
+
+#{HOMEBREW_PREFIX}/lib/python
+
+Installing with easy_install would be ideal; then the libraries are installed
+to /Library/Python which is in the default OS X Python library path. However
+easy_install does not support this formula.
+    EOS
   end
 end
