ac3c153dd4988b21c3de597cfbdf2b8df647bd8f
Alexander Solovyov <piranha@piranha.org.ua>
2009-11-05 11:18:23 +0200

    add plist to memcached

--- Library/Formula/memcached.rb
+++ Library/Formula/memcached.rb
@@ -10,5 +10,44 @@ class Memcached <Formula
   def install
     system "./configure --prefix='#{prefix}'"
     system "make install"
+
+    (prefix+'com.danga.memcached.plist').write startup_plist
+  end
+
+  def caveats; <<-EOS
+You can enabled memcached to automatically load on login with:
+    launchctl load -w #{prefix}/com.danga.memcached.plist
+
+Or start it manually:
+    #{HOMEBREW_PREFIX}/bin/memcached
+
+Add "-d" to start it as a daemon.
+    EOS
+  end
+
+  def startup_plist
+    return <<-EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>com.danga.memcached</string>
+  <key>KeepAlive</key>
+  <true/>
+  <key>ProgramArguments</key>
+  <array>
+    <string>#{HOMEBREW_PREFIX}/bin/memcached</string>
+    <string>-d</string>
+  </array>
+  <key>RunAtLoad</key>
+  <true/>
+  <key>UserName</key>
+  <string>#{`whoami`}</string>
+  <key>WorkingDirectory</key>
+  <string>#{HOMEBREW_PREFIX}</string>
+</dict>
+</plist>
+    EOPLIST
   end
 end
