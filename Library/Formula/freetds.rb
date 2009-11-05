944da17839f8e35bc5678984a5a3142d66fc9890
Hugh Evans <hugh@artpop.com.au>
2009-11-05 13:43:32 +1100

    FreeTDS 0.82: http://www.freetds.org/

---
+++ Library/Formula/freetds.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Freetds < Formula
+  url 'http://ibiblio.org/pub/Linux/ALPHA/freetds/stable/freetds-0.82.tar.gz'
+  homepage 'http://www.freetds.org/'
+  md5 '3df6b2e83fd420e90f1becbd1162990a'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--with-tdsver=7.0", "--enable-msdblib"
+    system 'make'
+    system 'make install'
+  end
+end
