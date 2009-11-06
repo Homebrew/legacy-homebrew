eef50b417bd04b7594b60ae4e852e506629e8057
Adam Vandenberg <flangy@gmail.com>
2009-11-06 11:34:54 -0800

    MongoDB - add --32bit option to force 32 bits; don't strip binaries.

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -3,12 +3,18 @@ require 'formula'
 class Mongodb <Formula
   homepage 'http://www.mongodb.org/'
 
-  if Hardware.is_64_bit?
+  if Hardware.is_64_bit? and not ARGV.include? '--32bit'
     url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.1.2.tgz'
     md5 '7fdcd8173fa975295b961e760eed1751'
+    version '1.1.2-x86_64'
   else
     url 'http://downloads.mongodb.org/osx/mongodb-osx-i386-1.1.2.tgz'
     md5 '0e10467e2a0877d189e4d4c0783ebc4b'
+    version '1.1.2-i386'
+  end
+  
+  def skip_clean? path
+    true
   end
 
   def install
