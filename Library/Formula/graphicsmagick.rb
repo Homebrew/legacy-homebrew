9f389f52476fea77abc926ea7ef1b18c9c945e75
Max Howell <max@methylblue.com>
2009-11-06 00:15:11 +0000

    Derive GraphicsMagick from ImageMagick
    Ruby is cool init?

--- Library/Formula/graphicsmagick.rb
+++ Library/Formula/graphicsmagick.rb
@@ -1,30 +1,24 @@
 require 'formula'
+require "#{File.dirname __FILE__}/imagemagick.rb"
 
-class Graphicsmagick <Formula
+class Graphicsmagick <Imagemagick
   url 'http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.7/GraphicsMagick-1.3.7.tar.bz2'
   homepage 'http://www.graphicsmagick.org/'
   md5 '42bfd382ddcda399880721170bcbf61b'
 
-  depends_on 'jpeg'
-  depends_on 'ghostscript' => :recommended
-  depends_on 'libwmf' => :optional
-  depends_on 'libtiff' => :optional
-  depends_on 'little-cms' => :optional
-  depends_on 'jasper' => :optional
+  def deps
+    # well this was an unexpected consequence of the DSL syntax, the dependencies
+    # aren't inherited
+    @deps ||= Formula.factory('imagemagick').deps
+  end
 
   def install
     ENV.libpng
     ENV.gcc_4_2
 
-    # we do not need to version the stuff in the main tree
-    inreplace 'configure', '${PACKAGE_NAME}-${PACKAGE_VERSION}', '${PACKAGE_NAME}'
+    fix_configure
 
-    system "./configure", "--prefix=#{prefix}", 
-                          "--disable-dependency-tracking",
-                          "--enable-shared",
-                          "--disable-static",
-                          "--with-modules",
-                          "--without-magick-plus-plus"
+    system "./configure", *configure_args
 
     system "make install"
   end
