c7b56e70a159900c13df217a831b9450ea7218f5
Max Howell <max@methylblue.com>
2009-11-07 18:09:48 +0000

    Set ENV[HOMEBREW_BREW_FILE]
    Thus install.rb gets the path to the instantiating brew process rather than whichever is first in the PATH. Not to mention Homebrew doesn't *have* to be in the PATH.

--- Library/Homebrew/global.rb
+++ Library/Homebrew/global.rb
@@ -38,6 +38,10 @@ else
   HOMEBREW_CACHE=Pathname.new("~/Library/Caches/Homebrew").expand_path
 end
 
+if not defined? HOMEBREW_BREW_FILE
+  HOMEBREW_BREW_FILE = ENV['HOMEBREW_BREW_FILE'] or `which brew`.chomp
+end
+
 HOMEBREW_PREFIX = Pathname.new(HOMEBREW_BREW_FILE).dirname.parent # Where we link under
 HOMEBREW_REPOSITORY = Pathname.new(HOMEBREW_BREW_FILE).realpath.dirname.parent # Where .git is found
 
