da3d4044753ba2208c4dcc94f3afbf2d381db609
Adam Vandenberg <flangy@gmail.com>
2009-11-12 10:37:47 -0800

    wxWidgets - use bz2 download.

--- Library/Formula/wxmac.rb
+++ Library/Formula/wxmac.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Wxmac <Formula
-  url 'http://prdownloads.sourceforge.net/wxwindows/wxMac-2.8.10.tar.gz'
+  url 'http://downloads.sourceforge.net/project/wxwindows/wxMac/2.8.10/wxMac-2.8.10.tar.bz2'
   homepage 'http://www.wxwidgets.org'
-  md5 'd18668cbf7026b8633e2cfc69e2f3868'
+  md5 '67e5eb6823907081fc979d41e00f93d7'
 
   def install
+    # Force i386
     %w{ CFLAGS CXXFLAGS LDFLAGS OBJCFLAGS OBJCXXFLAGS }.each do |compiler_flag|
       ENV.remove compiler_flag, "-arch x86_64"
       ENV.append compiler_flag, "-arch i386"
