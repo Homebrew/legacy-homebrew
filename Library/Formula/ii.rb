15b040dc5bf03950cf8459cf7cce9eafbdd8b11c
Jordan Bracco <jordan@bracco.name>
2009-11-12 19:31:36 +0100

    ii; minimalist FIFO and filesystem-based IRC client

---
+++ Library/Formula/ii.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Ii <Formula
+  url 'http://dl.suckless.org/tools/ii-1.4.tar.gz'
+  homepage 'http://tools.suckless.org/ii'
+  md5 '535d88c23e84d510edf773db12728ac0'
+
+  def install
+    inreplace 'config.mk', '/usr/local', prefix
+    system "make install"
+  end
+end
