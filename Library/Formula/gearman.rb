a6ac1e5ad206233b977e85ff1060440f77e4ac17
Alexander Solovyov <piranha@piranha.org.ua>
2009-11-14 11:32:52 +0200

    Gearman C server formula

---
+++ Library/Formula/gearman.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Gearman <Formula
+  url 'http://launchpad.net/gearmand/trunk/0.10/+download/gearmand-0.10.tar.gz'
+  homepage 'http://gearman.org/'
+  md5 '74d29c260bc7096c9193d3f6af57298f'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
