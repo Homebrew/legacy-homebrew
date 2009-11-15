85618f8491a198bd3e50064b6d870ecdfa39befc
Paul J. Davis <paul.joseph.davis@gmail.com>
2009-11-14 23:09:23 -0500

    New formula for Google's V8 JavaScript engine.
    The visibility=default flag will cause me nightmares.

--- Library/Formula/v8.rb
+++ Library/Formula/v8.rb
@@ -2,15 +2,23 @@ require 'formula'
 require 'hardware'
 
 class V8 <Formula
-  head 'git://github.com/v8/v8.git'
+  head 'http://v8.googlecode.com/svn/trunk/'
   homepage 'http://code.google.com/p/v8/'
 
   depends_on 'scons'
 
   def install
     arch = Hardware.is_64_bit? ? 'x64' : 'ia32'
-    system 'scons', "-j #{Hardware.processor_count}", 'mode=release', 'snapshot=on', "arch=#{arch}", 'sample=shell'
-    system 'scons', "-j #{Hardware.processor_count}", 'mode=release', 'snapshot=on', "arch=#{arch}", 'library=shared'
+
+    system "scons",
+            "-j #{Hardware.processor_count}",
+            "arch=#{arch}",
+            "mode=release",
+            "snapshot=on",
+            "library=shared",
+            "visibility=default",
+            "console=readline",
+            "sample=shell"
 
     include.install(Dir['include/*'])
     lib.install(Dir['libv8.*'])
