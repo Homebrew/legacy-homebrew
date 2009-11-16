9f85210e76d0d20f920f51a27da3ac3c4998a6ff
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-11-16 13:45:52 +0100

    Update LLVM formula to 2.6

--- Library/Formula/llvm.rb
+++ Library/Formula/llvm.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class Llvm <Formula
-  @url='http://llvm.org/releases/2.5/llvm-2.5.tar.gz'
+  @url='http://llvm.org/releases/2.6/llvm-2.6.tar.gz'
   @homepage='http://llvm.org/'
-  @md5='55df2ea8665c8094ad2ef85187b9fc74'
+  @md5='34a11e807add0f4555f691944e1a404a'
 
   def install
     ENV.gcc_4_2 # llvm can't compile itself
 
     system "./configure", "--prefix=#{prefix}",
-                          "--enable-targets=x86",
+                          "--enable-targets=host-only",
                           "--enable-optimized"
     system "make"
     system "make install" # seperate steps required, otherwise the build fails
