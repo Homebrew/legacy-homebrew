363b8abed927b276da342ee83ff161eec530c3e9
Adam Vandenberg <flangy@gmail.com>
2009-11-16 10:41:22 -0800

    Update mpg123 to 1.9.1 and fix it for 10.5

--- Library/Formula/mpg123.rb
+++ Library/Formula/mpg123.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Mpg123 <Formula
-  @url='http://downloads.sourceforge.net/project/mpg123/mpg123/1.8.1/mpg123-1.8.1.tar.bz2'
-  @homepage='http://www.mpg123.de/'
-  @md5='856893f14b29b1cddf4aba32469860b4'
+  url 'http://downloads.sourceforge.net/project/mpg123/mpg123/1.9.1/mpg123-1.9.1.tar.bz2'
+  homepage 'http://www.mpg123.de/'
+  md5 '39aa4407b53fa8c86f7d963bfe0702c9'
 
   def skip_clean? path
     # mpg123 can't find its plugins if there are no la files
@@ -11,19 +11,30 @@ class Mpg123 <Formula
   end
 
   def install
-    if MACOS_VERSION < 10.6
-      # otherwise the exe segfaults, I couldn't diagnose why
-      ENV.osx_10_4
-      ENV.gcc_4_0_1
-    end
+    args = ["--disable-debug", "--disable-dependency-tracking",
+            "--with-optimization=4",
+            "--with-cpu=sse_alone",
+            "--prefix=#{prefix}",
+            "--with-audio=coreaudio",
+            "--with-default-audio=coreaudio"]
 
-    args = ["--disable-debug", "--with-optimization=4",
-                               "--with-cpu=sse_alone",
-                               "--prefix=#{prefix}"]
-
-    args << "--with-cpu=x86-64" if Hardware.is_64_bit?
+    # This option causes compilation failure on 10.5 nehalem MacPro.
+    # args << "--with-cpu=x86-64" if Hardware.is_64_bit?
 
     system "./configure", *args
+    
+    # ./configure incorrectly detects 10.5 as 10.4
+    # Cut that crap out.
+    
+    ['', 'src/', 'src/output/', 'src/libmpg123/'].each do |path|
+      inreplace "#{path}Makefile", # CFLAGS
+        "-mmacosx-version-min=10.4 -isysroot /Developer/SDKs/MacOSX10.4u.sdk", ""
+
+      inreplace "#{path}Makefile", # LDFLAGS
+        "LDFLAGS =  -Wl,-syslibroot,/Developer/SDKs/MacOSX10.4u.sdk -Wl,-classic_linker -Wl,-read_only_relocs,suppress", 
+        "LDFLAGS =  -Wl,-read_only_relocs,suppress"
+    end
+    
     system "make install"
   end
 end
