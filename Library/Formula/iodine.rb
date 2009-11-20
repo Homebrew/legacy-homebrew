636cdda1cc675e56a28b6dba8e92335786c1b678
Dan Walters <dan@danwalters.net>
2009-11-20 19:54:44 -0300

    iodine 0.5.2
    iodine lets you tunnel IPv4 data through a DNS server.

---
+++ Library/Formula/iodine.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Iodine <Formula
+  url 'http://code.kryo.se/iodine/iodine-0.5.2.tar.gz'
+  homepage 'http://code.kryo.se/iodine/'
+  md5 '6952343cc4614857f83dbb81247871e7'
+
+  def install
+    if MACOS_VERSION >= 10.6
+      ["src/common.c", "src/dns.c", "src/iodine.c", "src/iodined.c"].each do |filename|
+        inreplace filename, "arpa/nameser8_compat", "arpa/nameser_compat"
+      end
+    end
+
+    system "make install prefix=#{prefix}"
+  end
+end
