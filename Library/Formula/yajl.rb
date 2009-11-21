5efa5afdc54fa7ff4484e9b7fd2ed58ebd12ff41
Adam Vandenberg <flangy@gmail.com>
2009-11-21 15:34:34 -0800

    Update yajl to 1.0.7

--- Library/Formula/yajl.rb
+++ Library/Formula/yajl.rb
@@ -1,17 +1,16 @@
 require 'formula'
 
 class Yajl <Formula
-  @homepage='http://lloyd.github.com/yajl/'
-  @url='http://github.com/lloyd/yajl/tarball/1.0.6'
-  @md5='bb201150143352b117be702a1bc78405'
+  homepage 'http://lloyd.github.com/yajl/'
+  url 'http://cloud.github.com/downloads/lloyd/yajl/yajl-1.0.7.tar.gz'
+  md5 'bb201150143352b117be702a1bc78405'
 
+  # Configure uses cmake, even though it looks like we're
+  # just using autotools below.
   depends_on 'cmake'
 
   def install
     ENV.deparallelize
-
-    # I have pushed this fix upstream
-    inreplace 'configure', 'cmake ..', "cmake #{std_cmake_parameters} .." if @version == "1.0.5"
 
     system "./configure --prefix '#{prefix}'"
     system "make install"
