d6f974c5d3401f7e6f227ac9c18450a661e893db
Adam Vandenberg <flangy@gmail.com>
2009-11-20 22:38:52 -0800

    Update AsciiDoc to 8.5.1

--- Library/Formula/asciidoc.rb
+++ Library/Formula/asciidoc.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Asciidoc <Formula
-  @url='http://downloads.sourceforge.net/project/asciidoc/asciidoc/8.4.5/asciidoc-8.4.5.tar.gz'
-  @md5='9f21d6e352b3ab668f9def3eb7497da2'
-  @homepage='http://www.methods.co.nz/asciidoc'
+  url 'http://downloads.sourceforge.net/project/asciidoc/asciidoc/8.5.1/asciidoc-8.5.1.tar.gz'
+  md5 '412fb0c86c3dcb4cc159ef63bd274c90'
+  homepage 'http://www.methods.co.nz/asciidoc'
 
   def install
-    system "./configure --disable-debug --prefix='#{prefix}'"
+    system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make install"
   end
 end
