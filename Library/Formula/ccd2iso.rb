f2231e2416e51ef9f6a529853dd32db2cb2e7ca6
Mihai Anca <ropiku@gmail.com>
2009-11-22 23:38:52 +0000

    Added ccd2iso 0.3

---
+++ Library/Formula/ccd2iso.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Ccd2iso <Formula
+  url 'http://downloads.sourceforge.net/project/ccd2iso/ccd2iso/ccd2iso-0.3/ccd2iso-0.3.tar.gz'
+  homepage 'http://ccd2iso.sourceforge.net/'
+  sha1 'cbefdc086adf45138d921a4a7c0ff012dd7c9ca7'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
