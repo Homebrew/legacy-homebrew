6f93273daa801ef92ab598ee5b60f1d05f17ed41
David Höppner <0xffea@gmail.com>
2009-11-25 11:19:18 +0100

    vala: update to version 0.7.8

--- Library/Formula/vala.rb
+++ Library/Formula/vala.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Vala <Formula
-  @url='http://download.gnome.org/sources/vala/0.7/vala-0.7.7.tar.bz2'
-  @homepage='http://live.gnome.org/Vala'
-  @md5='7d11fcddb2bd30b9ecbdacfaa20f2769'
+  url 'http://download.gnome.org/sources/vala/0.7/vala-0.7.8.tar.bz2'
+  homepage 'http://live.gnome.org/Vala'
+  md5 'accd0d350c6d6de7527a0a65c40f8be2'
 
   depends_on 'glib'
   depends_on 'gettext'
