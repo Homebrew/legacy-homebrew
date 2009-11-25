88dd0dd68d97c019924a8ff4b4a44ecbc3ff2132
Florian Sowade <f.sowade@lab9.de>
2009-11-26 00:39:55 +0100

    Bump wget from 1.11.4 to 1.12

--- Library/Formula/wget.rb
+++ Library/Formula/wget.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wget <Formula
   homepage 'http://www.gnu.org/software/wget/'
-  url 'http://ftp.gnu.org/gnu/wget/wget-1.11.4.tar.bz2'
-  md5 'f5076a8c2ec2b7f334cb6e3059820f9c'
+  url 'http://ftp.gnu.org/gnu/wget/wget-1.12.tar.bz2'
+  md5 '308a5476fc096a8a525d07279a6f6aa3'
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
