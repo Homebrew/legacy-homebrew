b6e8d652de258c862cd4ea8b76958d61eaea63fe
Adam Vandenberg <flangy@gmail.com>
2009-11-25 22:46:19 -0800

    Reformat c-ares

--- Library/Formula/c-ares.rb
+++ Library/Formula/c-ares.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class CAres <Formula
-  @url='http://c-ares.haxx.se/c-ares-1.6.0.tar.gz'
-  @homepage='http://c-ares.haxx.se/'
-  @md5='4503b0db3dd79d3c1f58d87722dbab46'
+  url 'http://c-ares.haxx.se/c-ares-1.6.0.tar.gz'
+  homepage 'http://c-ares.haxx.se/'
+  md5 '4503b0db3dd79d3c1f58d87722dbab46'
 
   def install
     system "./configure", "--prefix=#{prefix}"
