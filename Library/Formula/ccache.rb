5b58da9504ed80071dac074cb6973dc12034caaf
Adam Vandenberg <flangy@gmail.com>
2009-11-25 22:46:53 -0800

    Reformat ccache

--- Library/Formula/ccache.rb
+++ Library/Formula/ccache.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Ccache <Formula
-  @url='http://samba.org/ftp/ccache/ccache-2.4.tar.gz'
-  @homepage='http://ccache.samba.org/'
-  @md5='73c1ed1e767c1752dd0f548ec1e66ce7'
+  url 'http://samba.org/ftp/ccache/ccache-2.4.tar.gz'
+  homepage 'http://ccache.samba.org/'
+  md5 '73c1ed1e767c1752dd0f548ec1e66ce7'
 
   def install
     system "./configure", "--prefix=#{prefix}"
