6e4039bf3beac6876e72a522458c4e515c32b4a5
Adam Vandenberg <flangy@gmail.com>
2009-11-25 22:46:29 -0800

    Reformat cadaver.

--- Library/Formula/cadaver.rb
+++ Library/Formula/cadaver.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Cadaver <Formula
-  @url='http://www.webdav.org/cadaver/cadaver-0.23.2.tar.gz'
-  @homepage='http://www.webdav.org/cadaver/'
-  @md5='5ac79e41f3b7b4f68bf4003beed5dc26'
+  url 'http://www.webdav.org/cadaver/cadaver-0.23.2.tar.gz'
+  homepage 'http://www.webdav.org/cadaver/'
+  md5 '5ac79e41f3b7b4f68bf4003beed5dc26'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-ssl"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--with-ssl"
     system "(cd lib/intl; make)"
     system "make install"
   end
