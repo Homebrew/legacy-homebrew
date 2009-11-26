c7d8cccb6a1c2730dfd331f6810873a08aa56211
Adam Vandenberg <flangy@gmail.com>
2009-11-25 22:46:59 -0800

    Reformat chicken

--- Library/Formula/chicken.rb
+++ Library/Formula/chicken.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Chicken <Formula
-  @url='http://www.call-with-current-continuation.org/chicken-4.2.0.tar.gz'
-  @homepage='http://www.call-with-current-continuation.org/'
-  @md5='4705b7634447a571ff083f435c110fe3'
+  url 'http://www.call-with-current-continuation.org/chicken-4.2.0.tar.gz'
+  homepage 'http://www.call-with-current-continuation.org/'
+  md5 '4705b7634447a571ff083f435c110fe3'
 
   def install
     ENV.deparallelize
