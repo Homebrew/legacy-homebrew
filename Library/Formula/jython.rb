48f18e72197d1c13a54b468a0d662985fef6b917
Adam Vandenberg <flangy@gmail.com>
2009-11-25 22:55:45 -0800

    Remove duplication of Jar name in Jython.

--- Library/Formula/jython.rb
+++ Library/Formula/jython.rb
@@ -1,7 +1,8 @@
 require 'formula'
 
 class Jython <Formula
-  url 'http://downloads.sourceforge.net/project/jython/jython/2.5.1/jython_installer-2.5.1.jar'
+  JAR = 'jython_installer-2.5.1.jar'
+  url "http://downloads.sourceforge.net/project/jython/jython/2.5.1/#{JAR}"
   homepage 'http://www.jython.org'
   md5 '2ee978eff4306b23753b3fe9d7af5b37'
 
@@ -10,6 +11,6 @@ class Jython <Formula
   end
 
   def install
-    system "java", "-jar", "jython_installer-2.5.1.jar", "-s", "-d", prefix
+    system "java", "-jar", JAR, "-s", "-d", prefix
   end
 end
