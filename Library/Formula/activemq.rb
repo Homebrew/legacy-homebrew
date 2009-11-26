fa219a2036a8e9a3484559af07abd02d150b7e1d
Adam Vandenberg <flangy@gmail.com>
2009-11-25 22:44:52 -0800

    Update ActiveMQ to 5.3.0

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Activemq <Formula
-  @url='http://www.apache.org/dist/activemq/apache-activemq/5.2.0/apache-activemq-5.2.0-bin.tar.gz'
-  @homepage='http://activemq.apache.org/'
-  @md5='f11ca54e89689a0570dd607528966ba5'
+  url 'http://www.apache.org/dist/activemq/apache-activemq/5.3.0/apache-activemq-5.3.0-bin.tar.gz'
+  homepage 'http://activemq.apache.org/'
+  md5 'f4b88a2b4ffd21cb804cdbd4f2e0152f'
   
   def install
     prefix.install Dir['*']
