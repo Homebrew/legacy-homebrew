08c51fb69107614adf4ff0c1c03895c1197aa7b6
Adam Vandenberg <flangy@gmail.com>
2009-12-01 11:37:57 -0800

    libevent 1.4.13; fix Mac Pro compilation

--- Library/Formula/libevent.rb
+++ Library/Formula/libevent.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class Libevent <Formula
-  @version='1.4.12'
-  @url="http://www.monkey.org/~provos/libevent-#{@version}-stable.tar.gz"
-  @homepage='http://www.monkey.org/~provos/libevent/'
-  @md5='77b0d8b9885496871bb083165b35ba11'
+  version '1.4.13'
+  url "http://www.monkey.org/~provos/libevent-#{@version}-stable.tar.gz"
+  homepage 'http://www.monkey.org/~provos/libevent/'
+  md5 '0b3ea18c634072d12b3c1ee734263664'
 
   def install
-    system "./configure --prefix='#{prefix}'"
+    ENV.j1 # Needed for Mac Pro compilation
+    system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 end
