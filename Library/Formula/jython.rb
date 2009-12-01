b84b3e031c6ab9154476b92d3be3a1497f108e70
Adam Vandenberg <flangy@gmail.com>
2009-12-01 12:01:29 -0800

    Use extracted download strategy.

--- Library/Formula/jython.rb
+++ Library/Formula/jython.rb
@@ -1,19 +1,12 @@
 require 'formula'
 
-# even though "file -b" reports this as a zip archive, it's just a binary
-class JythonHttpDownloadStrategy <CurlDownloadStrategy
-  def stage
-    FileUtils.mv @dl, File.basename(@url)
-  end
-end
-
 class Jython <Formula
   url 'http://downloads.sourceforge.net/project/jython/jython/2.5.1/jython_installer-2.5.1.jar'
   homepage 'http://www.jython.org'
   md5 '2ee978eff4306b23753b3fe9d7af5b37'
 
   def download_strategy
-    JythonHttpDownloadStrategy
+    NoUnzipCurlDownloadStrategy # Don't unzip jar.
   end
 
   def install
