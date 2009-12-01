18fbfef5d2d3736fcd471563b8b14f266c913b8f
Max Howell <max@methylblue.com>
2009-12-01 10:50:04 +0000

    Erlang R13B03
    Remove the __END__ patch as it is now fixed.
    
    Removed some patches that make no sense for us.

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -11,24 +11,25 @@ class ErlangHtmlDocs <Formula
 end
 
 class Erlang <Formula
-  url 'http://erlang.org/download/otp_src_R13B02-1.tar.gz'
-  md5 '2593b9312eb1b15bf23a968743138c52'
-  version 'R13B02-1'
+  url 'http://erlang.org/download/otp_src_R13B03.tar.gz'
+  version 'R13B03'
+  md5 '411fcb29f0819973f71e28f6b56d9948'
   homepage 'http://www.erlang.org'
 
   depends_on 'icu4c'
-  skip_clean 'lib'
+
+  def skip_clean? path
+    true if path =~ %r[#{lib}/erlang/erts-(\d+\.?)+/bin/beam(\.smp)?] # breaks crypto_drv.so loading
+    true if path =~ %r[#{lib}/erlang/lib] # crypto_drv.so etc. can't be stripped as plugins
+  end
 
   def patches
-    { :p1 => DATA,
-      :p0 => ["patch-toolbar.erl",
-              "patch-erts_emulator_Makefile.in",
+    { :p0 => ["patch-erts_emulator_Makefile.in",
               "patch-erts_emulator_hipe_hipe_amd64_asm.m4.diff",
               "patch-erts_emulator_hipe_hipe_amd64_bifs.m4.diff",
               "patch-erts_emulator_hipe_hipe_amd64_glue.S.diff",
               "patch-erts_emulator_hipe_hipe_amd64.c.diff",
               "patch-erts_emulator_sys_unix_sys_float.c.diff",
-              "patch-erts_configure.diff",
               "patch-lib_ssl_c_src_esock_openssl.c",
               "patch-lib_wx_configure.in",
               "patch-lib_wx_configure"
@@ -68,38 +69,3 @@ class Erlang <Formula
     #ErlangHtmlDocs.new.brew { doc.install Dir['*'] }
   end
 end
-
-
-__END__
---- otp_src_R13B02-1/configure	2009-09-21 11:29:51.000000000 +0200
-+++ otp_src_R13B02-1-hvt/configure	2009-11-11 19:02:24.000000000 +0200
-@@ -2702,7 +2702,7 @@
- 	export LDFLAGS
- fi
- if test X${enable_darwin_64bit} = Xyes; then
--	if test X"$TMPSYS" '!=' X"Darwin-i386"; then
-+	if test X"$TMPSYS" '!=' X"Darwin-i386" '-a' X"$TMPSYS" '!=' X"Darwin-x86_64"; then
- 		{ { echo "$as_me:$LINENO: error: --enable-darwin-64bit only supported on x86 host" >&5
- echo "$as_me: error: --enable-darwin-64bit only supported on x86 host" >&2;}
-    { (exit 1); exit 1; }; }
-@@ -2712,7 +2712,7 @@
- 	export CFLAGS
- 	LDFLAGS="-m64 $LDFLAGS"
- 	export LDFLAGS
--elif test X"$TMPSYS" '=' X"Darwin-i386"; then
-+elif test X"$TMPSYS" '=' X"Darwin-i386" '-o' X"$TMPSYS" '!=' X"Darwin-x86_64"; then
- 	CFLAGS="-m32 $CFLAGS"
- 	export CFLAGS
- 	LDFLAGS="-m32 $LDFLAGS"
-diff -r -u otp_src_R13B02-1/erts/configure otp_src_R13B02-1-hvt/erts/configure
---- otp_src_R13B02-1/erts/configure	2009-09-21 11:29:49.000000000 +0200
-+++ otp_src_R13B02-1-hvt/erts/configure	2009-11-11 19:05:49.000000000 +0200
-@@ -2803,7 +2803,7 @@
- 	esac
- fi
- if test X${enable_darwin_64bit} = Xyes; then
--	if test X"$TMPSYS" '!=' X"Darwin-i386"; then
-+	if test X"$TMPSYS" '!=' X"Darwin-i386" '-a' X"$TMPSYS" '!=' X"Darwin-x86_64"; then
- 		{ { echo "$as_me:$LINENO: error: --enable-darwin-64bit only supported on x86 host" >&5
- echo "$as_me: error: --enable-darwin-64bit only supported on x86 host" >&2;}
-    { (exit 1); exit 1; }; }
