39a7b061bd2e5c89c6a152857c308fb1f6ef50b3
Travis Cline <travis.cline@gmail.com>
2009-12-02 17:03:24 -0600

    Added gdal formula.
    GDAL is a translator library for raster geospatial data formats.

---
+++ Library/Formula/gdal.rb
@@ -0,0 +1,71 @@
+require 'formula'
+
+class Gdal <Formula
+  url 'http://download.osgeo.org/gdal/gdal-1.6.3.tar.gz'
+  homepage 'http://www.gdal.org/'
+  md5 'a8aade344a29ae4a92a0f17cc33f561a'
+
+  depends_on 'libtiff'
+  depends_on 'libpng'
+  depends_on 'giflib'
+  depends_on 'proj'
+  depends_on 'geos'
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--mandir=#{prefix}/share/man",
+                          "--with-pthreads",
+                          "--with-local=#{prefix}",
+                          "--with-libz=#{prefix}",
+                          #"--with-png=#{prefix}",
+                          "--with-libtiff=#{prefix}",
+                          #"--with-geotiff=#{prefix}",
+                          "--with-jpeg=#{prefix}",
+                          "--with-gif=#{prefix}",
+                          "--with-static-proj4=#{prefix}",
+                          "--without-pg",
+                          "--without-grass",
+                          "--without-libgrass",
+                          "--without-cfitsio",
+                          "--without-pcraster",
+                          "--without-netcdf",
+                          "--without-ogdi",
+                          "--without-fme",
+                          "--without-hdf4",
+                          "--without-hdf5",
+                          "--without-jasper",
+                          "--without-ecw",
+                          "--without-kakadu",
+                          "--without-mrsid",
+                          "--without-jp2mrsid",
+                          "--without-msg",
+                          "--without-bsb",
+                          "--without-oci",
+                          "--without-grib",
+                          "--without-mysql",
+                          "--without-ingres",
+                          "--without-xerces",
+                          "--without-expat",
+                          "--without-odbc",
+                          "--with-dods-support=no",
+                          "--without-curl",
+                          "--without-sqlite3",
+                          "--without-dwgdirect",
+                          "--without-idb",
+                          "--without-sde",
+                          #"--without-geos",
+                          "--without-pam",
+                          "--without-macosx-framework",
+                          "--without-perl",
+                          "--without-php",
+                          #"--without-ruby",
+                          #"--without-python",
+                          "--without-ogpython",
+                          "--without-xerces"
+
+    system "make"
+    system "make install"
+  end
+end
