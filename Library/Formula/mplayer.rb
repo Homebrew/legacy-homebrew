1e099d36bf739b63521ee44e7dbbddc49c186a32
Max Howell <max@methylblue.com>
2009-12-02 12:26:51 +0000

    Depend on subversion for Leopard; Fixes #87
    The Subversion bundled with Leopard doesn't export externals, so the build fails.

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -6,6 +6,9 @@ class Mplayer <Formula
   
   depends_on 'pkg-config' => :recommended
 
+  # http://github.com/mxcl/homebrew/issues/#issue/87
+  depends_on :subversion if MACOS_VERSION < 10.6
+
   def install
     ENV.gcc_4_2   # llvm chokes on labels within mlp inline asm
     system "./configure --prefix='#{prefix}'"
