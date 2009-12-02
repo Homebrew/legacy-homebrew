56f86f8ff0fc922538ca08799795360ba189d675
Max Howell <max@methylblue.com>
2009-12-02 09:45:15 +0000

    Put more aliases in the formula

--- Library/Formula/beanstalk.rb
+++ Library/Formula/beanstalk.rb
@@ -1,12 +1,11 @@
 require 'formula'
 
-# TODO alias: beanstalkd
-
 class Beanstalk <Formula
   url 'http://xph.us/dist/beanstalkd/beanstalkd-1.4.tar.gz'
   md5 'eea47c86c722c4448087fb28be7357cd'
   homepage 'http://xph.us/software/beanstalkd/'
 
+  aka 'beanstalkd'
   depends_on 'libevent'
 
   def install
--- Library/Formula/gsl.rb
+++ Library/Formula/gsl.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
-# TODO alias GNU Scientific Library
-
 class Gsl <Formula
   url 'ftp://ftp.gnu.org/gnu/gsl/gsl-1.13.tar.gz'
   homepage 'http://www.gnu.org/software/gsl/'
   md5 'd9fcfa367c44ab68a25b4edf34c3c5f7'
+  aka 'gnu-scientific-library'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
