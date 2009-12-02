e9b0a335621382f92733e031fd5057b6dbf66cd2
Max Howell <max@methylblue.com>
2009-12-02 12:37:21 +0000

    Revert "Don't install gem or rake"
    This reverts commit d0ed812b3dfa94507ac3c6c9b4a8ec54c1f3251f.
    
    Fixes #129

--- Library/Formula/ruby.rb
+++ Library/Formula/ruby.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
+# TODO de-version the include and lib directories
+
 class Ruby <Formula
-  url 'ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p243.tar.gz'
-  homepage 'http://www.ruby-lang.org/en/'
-  md5 '515bfd965814e718c0943abf3dde5494'
+  @url='ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.1-p243.tar.gz'
+  @homepage='http://www.ruby-lang.org/en/'
+  @md5='515bfd965814e718c0943abf3dde5494'
 
   depends_on 'readline'
-  skip_clean 'bin/ruby'
   
   def install
     ENV.gcc_4_2
@@ -16,29 +17,12 @@ class Ruby <Formula
                           "--enable-shared"
     system "make"
     system "make install"
-
-    unless ARGV.include? '--enable-super-dupe'
-      Dir.chdir prefix
-      FileUtils.rm_rf Dir['lib/ruby/*/rubygems']
-      FileUtils.rm_rf Dir['lib/ruby/*/rake']
-      File.unlink 'bin/gem'
-      File.unlink 'bin/rake'
-      File.unlink man1+'rake.1'
-    end
   end
   
-  def caveats; <<-EOS
-By default we don't install the bundled Rake or RubyGems.
-
-This is because they both come with the system installed Ruby. You can upgrade
-them with `gem update --system`.
-
-If you really want them though do:
-
-    brew install ruby --force --enable-super-dupe
-
-If you disagree with this decision, please create an issue on GitHub as we 
-should discuss the matter.
-    EOS
+  def skip_clean? path
+    # TODO only skip the clean for the files that need it, we didn't get a
+    # comment about why we're skipping the clean, so you'll need to figure
+    # that out first --mxcl
+    true
   end
 end
