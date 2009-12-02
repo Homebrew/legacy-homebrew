eb290d63c24115b2a941c6cc47abe0ab46084ed2
Max Howell <max@methylblue.com>
2009-12-02 15:06:01 +0000

    Fix Parrot on Leopard; Fixes #146

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -1,20 +1,24 @@
 require 'formula'
 
 class Parrot <Formula
-  url 'ftp://ftp.parrot.org//pub/parrot/releases/devel/1.6.0/parrot-1.6.0.tar.gz'
+  url 'ftp://ftp.parrot.org//pub/parrot/releases/devel/1.8.0/parrot-1.8.0.tar.gz'
   homepage 'http://www.parrot.org/'
-  md5 '6fde2d91278a3990213c8671a0856e4a'
+  md5 'a61fd27bf5033eeb0792da3686cd5af7'
 
   depends_on 'pcre'
 
   def install
-    system "perl Configure.pl --prefix=#{prefix} --debugging=0 --without-opengl --cc=#{ENV['CC']}"
+    confargs = %W[--prefix=#{prefix} --debugging=0 --without-opengl]
+    # the arguments Parrot tries to pass to gcc-4.2 on 10.5 don't compile
+    confargs << "--cc=#{ENV['CC']}" if MACOS_VERSION >= 10.6
+
+    system "perl", "Configure.pl", *confargs
+    system "make"
     system "make install"
 
-    h = "#{HOMEBREW_PREFIX}/Cellar/parrot/1.6.0/bin/"
-    l = %x{otool -L #{h}parrot}[/\S*blib\/lib\S*/]
-    %w{parrot parrot_config parrot_debugger}.each do |f|
-      system "install_name_tool -change #{l} #{HOMEBREW_PREFIX}/lib/libparrot.dylib #{h}#{f}"
+    l = %x{otool -L #{bin}/parrot}[/\S*blib\/lib\S*/]
+    %w{parrot parrot_config parrot_debugger}.each do |fn|
+      system "install_name_tool -change #{l} #{lib}/libparrot.dylib #{bin+fn}"
     end
   end
 end
