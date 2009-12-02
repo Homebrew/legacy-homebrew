92e09451a59aa66dcb9803a8852d738539fbcf5b
Max Howell <max@methylblue.com>
2009-12-02 11:06:31 +0000

    Update SDL_mixer to 1.2.11
    Fixes #147

--- Library/Formula/sdl_mixer.rb
+++ Library/Formula/sdl_mixer.rb
@@ -2,9 +2,9 @@ require 'formula'
 require Formula.path('sdl')
 
 class SdlMixer <Formula
-  url 'http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.9.tar.gz'
+  url 'http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.11.tar.gz'
   homepage 'http://www.libsdl.org/projects/SDL_mixer/'
-  md5 'a9eb8750e920829ff41dbe7555850156'
+  md5 '65ada3d997fe85109191a5fb083f248c'
 
   depends_on 'sdl'
   depends_on 'flac' => :optional
