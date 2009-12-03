199ac98a99f2c17a6df5a69c30767746cea67e9a
Dan Peterson <dpiddy@gmail.com>
2009-12-03 08:42:33 -0800

    add picocom

---
+++ Library/Formula/picocom.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Picocom <Formula
+  url 'http://picocom.googlecode.com/files/picocom-1.4.tar.gz'
+  homepage 'http://code.google.com/p/picocom/'
+  md5 '08fcc5f6bb9e7676a2569386d5ea9f70'
+
+  def install
+    system "make"
+    bin.install "picocom"
+  end
+end
