76989e3d9c083bd0e4d0e408dacb5b3264ab6b07
David Höppner <0xffea@gmail.com>
2009-12-03 14:18:53 +0100

    jruby: Fixes #173

--- Library/Formula/jruby.rb
+++ Library/Formula/jruby.rb
@@ -4,9 +4,9 @@ require 'formula'
 # TODO trim that a bit? Seems crazy.
 
 class Jruby < Formula
-  url 'http://jruby.kenai.com/downloads/1.4.0/jruby-bin-1.4.0.tar.gz'
+  url 'http://jruby.kenai.com/downloads/1.4.0/jruby-src-1.4.0.tar.gz'
   homepage 'http://www.jruby.org'
-  md5 'f37322c18e9134e91e064aebb4baa4c7'
+  md5 'a363b6c2ea24f0ef8df478c93ac8cc59'
 
   def install
     system "ant"
