9830ca960fd7b8535a135a434bb28ec0866fc4ac
Kevin Ballard <kevin@sb.org>
2009-12-03 19:42:40 -0800

    Fix irssi's perl support
    The system-provided Perl adds various -arch flags which are incompatible with
    some flags that homebrew sets in CFLAGS. Patch the configure file to remove
    the -arch flags for the Perl module compilation.
    
    Fixes #367

--- Library/Formula/irssi.rb
+++ Library/Formula/irssi.rb
@@ -12,20 +12,38 @@ class Irssi <Formula
     path == bin+'irssi'
   end
 
+  def patches
+    DATA
+  end
+
   def install
     ENV.append 'ARCHFLAGS', ' ' # wtf?
 
     cargs = ["--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking",
-             "--with-modules", "--enable-ssl", "--enable-ipv6"]
-
-    # http://github.com/mxcl/homebrew/issues/#issue/367
-    if MACOS_VERSION > 10.5
-      cargs << "--with-perl=no"
-    else
-      cargs << "--with-perl=yes"
-    end
+             "--with-modules", "--enable-ssl", "--enable-ipv6", "--with-perl=yes"]
 
     system "./configure", *cargs
     system "make install"
   end
 end
+__END__
+--- a/configure	2009-12-03 19:35:07.000000000 -0800
++++ b/configure	2009-12-03 19:35:33.000000000 -0800
+@@ -27419,7 +27419,7 @@
+ 	if test -z "$perlpath"; then
+ 		perl_check_error="perl binary not found"
+ 	else
+-		PERL_CFLAGS=`$perlpath -MExtUtils::Embed -e ccopts 2>/dev/null`
++		PERL_CFLAGS=`$perlpath -MExtUtils::Embed -e ccopts 2>/dev/null | $SED -e 's/-arch [^ ]\{1,\}//g'`
+ 	fi
+ 
+ 	if test "x$ac_cv_c_compiler_gnu" = "xyes" -a -z "`echo $host_os|grep 'bsd\|linux'`"; then
+@@ -27437,7 +27437,7 @@
+ $as_echo "not found, building without Perl" >&6; }
+ 		want_perl=no
+ 	else
+-		PERL_LDFLAGS=`$perlpath -MExtUtils::Embed -e ldopts 2>/dev/null`
++		PERL_LDFLAGS=`$perlpath -MExtUtils::Embed -e ldopts 2>/dev/null | $SED -e 's/-arch [^ ]\{1,\}//g'`
+ 
+ 		if test "x$DYNLIB_MODULES" = "xno" -a "$want_perl" != "static"; then
+ 						want_perl=static
