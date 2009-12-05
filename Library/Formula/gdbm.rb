b3fb6e770012aea83399f8977ffcfb900ad4fb70
Adam Vandenberg <flangy@gmail.com>
2009-12-04 17:52:02 -0800

    Add gdbm 1.8.3

---
+++ Library/Formula/gdbm.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Gdbm <Formula
+  url 'http://gnu.sixsigmaonline.org/gnu/gdbm/gdbm-1.8.3.tar.gz'
+  homepage 'http://www.gnu.org/software/gdbm/'
+  md5 '1d1b1d5c0245b1c00aff92da751e9aa1'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    inreplace "Makefile", "-o $(BINOWN) -g $(BINGRP)", ""
+    system "make install"
+  end
+end
