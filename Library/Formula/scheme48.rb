4108d7d986836247d2c473f69a95731794d314c0
Drew Hess <dhess-src@bothan.net>
2009-12-04 18:52:33 -0800

    Formula for Scheme 48.

---
+++ Library/Formula/scheme48.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Scheme48 <Formula
+  url 'http://groups.csail.mit.edu/mac/projects/s48/1.8/scheme48-1.8.tgz'
+  homepage 'http://groups.csail.mit.edu/mac/projects/s48/'
+  md5 'f1c0a515039d4df4e07721f21940ad6d'
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--enable-gc=bibop"
+    system "make"
+    system "make install"
+  end
+end
