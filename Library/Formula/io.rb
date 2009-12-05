9d7a7d356d9f1447909f1a5778d970ae56661cb5
Max Howell <max@methylblue.com>
2009-12-05 15:42:26 +0000

    Force IO to install into the prefix of our choosing

--- Library/Formula/io.rb
+++ Library/Formula/io.rb
@@ -3,8 +3,41 @@ require 'formula'
 class Io <Formula
   head 'git://github.com/stevedekorte/io.git'
   homepage 'http://iolanguage.com/'
-  
+
+  def hardcoded_prefixes; %w[
+    addons/Cairo/build.io
+    addons/Flux/io/Flux.io
+    addons/MySQL/build.io
+    addons/SGML/source/libsgml-1.1.4/acgeneral.m4
+    addons/SGML/source/libsgml-1.1.4/configure
+    addons/SGML/source/libsgml-1.1.4/libsgml.doxy
+    addons/SGML/source/libsgml-1.1.4/Makefile
+    addons/SGML/source/libsgml-1.1.4/Makefile.in
+    addons/SGML/source/libsgml-1.1.4_osx/acgeneral.m4
+    addons/SGML/source/libsgml-1.1.4_osx/config/configure
+    addons/SGML/source/libsgml-1.1.4_osx/libsgml.doxy
+    addons/SGML/source/libsgml-1.1.4_osx/Makefile
+    addons/SGML/source/libsgml-1.1.4_osx/Makefile.in
+    addons/TagDB/build.io
+    build/AddonBuilder.io
+    docs/IoGuide.html
+    docs/IoReference.html
+    extras/osx/osx.xcodeproj/project.pbxproj
+    extras/osxmain/osxmain.xcodeproj/project.pbxproj
+    libs/basekit/source/Hash_fnv.c
+    libs/basekit/source/Hash_fnv.h
+    libs/iovm/source/IoSystem.c
+    Makefile
+    tools/io/docs2html.io] << 
+    'extras/SyntaxHighlighters/Io.tmbundle/Commands/Run Io Program (ioServer).plist'
+  end
+
   def install
+    inreplace 'addons/SGML/build.io', 'sudo ', ''
+    inreplace 'addons/TagDB/build.io', 'sudo ', ''
+
+    hardcoded_prefixes.each{ |fn| inreplace fn, '/usr/local', prefix }
+
     system "make vm"
     system "make"
     system "make install"
