75260f5e785c951ec1af60febc919a8ee0fdc033
Max Howell <max@methylblue.com>
2009-12-05 17:39:20 +0000

    Add missing md5 and homepage, install manpages to correct location

--- Library/Formula/cmatrix.rb
+++ Library/Formula/cmatrix.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Cmatrix <Formula
   url 'http://www.asty.org/cmatrix/dist/cmatrix-1.2a.tar.gz'
-  homepage ''
-  md5 ''
+  homepage 'http://www.asty.org/cmatrix/'
+  md5 'ebfb5733104a258173a9ccf2669968a1'
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make"
     system "make install"
   end
