13a5234edd1dd042b9d8c5efbaab61c748e94bd9
Max Howell <max@methylblue.com>
2009-12-07 15:31:51 +0000

    Fix mplayer formula; Fixes #172
    This is horrible in that I had to disable all optimisations and mplayer is the kind of tool that really gets a boost out of them!
    
    I hope they release another tarball soon.

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -1,17 +1,26 @@
 require 'formula'
 
 class Mplayer <Formula
-  @homepage='http://www.mplayerhq.hu/'
-  @head='svn://svn.mplayerhq.hu/mplayer/trunk'
-  
+  homepage 'http://www.mplayerhq.hu/'
+  head 'svn://svn.mplayerhq.hu/mplayer/trunk'
+
   depends_on 'pkg-config' => :recommended
 
   # http://github.com/mxcl/homebrew/issues/#issue/87
   depends_on :subversion if MACOS_VERSION < 10.6
 
   def install
-    ENV.gcc_4_2   # llvm chokes on labels within mlp inline asm
-    system "./configure", "--prefix=#{prefix}"
+    # LLVM-2206 can't handle some of the assembly
+    # Using gcc_4_2 doesn't work, failing mysteriously (check this again)
+    ENV['CC'] = ''
+    ENV['LD'] = ''
+    # any kind of optimisation breaks the build
+    ENV['CFLAGS'] = HOMEBREW_SAFE_CFLAGS
+
+    args = ['./configure', "--prefix=#{prefix}"]
+    args << "--target=x86_64-Darwin" if Hardware.is_64_bit? and MACOS_VERSION >= 10.6
+
+    system *args 
     system "make"
     system "make install"
   end
