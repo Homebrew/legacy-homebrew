e4c834b24ccda22952c8c1c801a14c8f457c04e3
Michael Dwan <mpdwan@gmail.com>
2009-12-06 18:55:01 -0600

    Updating mongodb to version 1.1.4

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -5,13 +5,13 @@ class Mongodb <Formula
   homepage 'http://www.mongodb.org/'
 
   if Hardware.is_64_bit? and not ARGV.include? '--32bit'
-    url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.1.3.tgz'
-    md5 '3d0d84de2e9c826c597b8b22f752ff71'
-    version '1.1.3-x86_64'
+    url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.1.4.tgz'
+    md5 '77744c7cd163c3c10b4763a59f20561e'
+    version '1.1.4-x86_64'
   else
-    url 'http://downloads.mongodb.org/osx/mongodb-osx-i386-1.1.3.tgz'
-    md5 '8df71bdc8e127ee16db3ddd0fa748cf3'
-    version '1.1.3-i386'
+    url 'http://downloads.mongodb.org/osx/mongodb-osx-i386-1.1.4.tgz'
+    md5 '4b6cbcccf10fa6e65b6450f9b098cb27'
+    version '1.1.4-i386'
   end
   
   def skip_clean? path
