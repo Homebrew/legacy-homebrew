d951ec9dd4f32a870f3c088512d73c1f81fa5325
Max Howell <max@methylblue.com>
2009-12-07 20:03:08 +0000

    Use SF gateway url for id3v2

--- Library/Formula/id3v2.rb
+++ Library/Formula/id3v2.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Id3v2 <Formula
-  url 'http://prdownloads.sourceforge.net/id3v2/id3v2-0.1.11.tar.gz'
+  url 'http://downloads.sourceforge.net/project/id3v2/id3v2/0.1.11/id3v2-0.1.11.tar.gz'
   homepage 'http://id3v2.sourceforge.net/'
   md5 '68afc3827cf01501dfb22949f901f1d8'
 
