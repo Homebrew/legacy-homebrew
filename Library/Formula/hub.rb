49e6d55e069e21242a51baaf3bc67c9a6011d1e5
Joshua Peek <josh@joshpeek.com>
2009-12-11 10:50:15 -0600

    New version of hub has an install rake task

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,23 +1,17 @@
 require 'formula'
 
 class Hub <Formula
-  head 'git://github.com/defunkt/hub.git', :tag => 'v0.1.2'
+  head 'git://github.com/defunkt/hub.git', :tag => 'v0.1.3'
   homepage 'http://github.com/defunkt/hub'
 
   def initialize(*args)
     super
 
     # Force version instead of HEAD
-    @version = '0.1.2'
+    @version = '0.1.3'
   end
 
   def install
-    # standalone task runs tests which fail
-    # system 'rake standalone'
-    system %(ruby -Ilib -rhub -e "Hub::Standalone.save('hub')")
-    system 'rake man'
-
-    bin.install 'hub'
-    man1.install 'man/hub.1'
+    system "rake install prefix=#{prefix}"
   end
 end
