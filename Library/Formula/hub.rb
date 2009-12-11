ce737039fe4d6cbadf4be1d6c43cb10c854f0816
Joshua Peek <josh@joshpeek.com>
2009-12-11 10:26:08 -0600

    Use path.install dsl for hub formula

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -15,11 +15,9 @@ class Hub <Formula
     # standalone task runs tests which fail
     # system 'rake standalone'
     system %(ruby -Ilib -rhub -e "Hub::Standalone.save('hub')")
-    FileUtils.mkdir_p "#{prefix}/bin"
-    FileUtils.cp 'hub', "#{prefix}/bin/hub"
-
     system 'rake man'
-    FileUtils.mkdir_p "#{prefix}/share/man/man1"
-    FileUtils.cp 'man/hub.1', "#{prefix}/share/man/man1"
+
+    bin.install 'hub'
+    man1.install 'man/hub.1'
   end
 end
