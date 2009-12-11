f0c76190ccb08af74aaa78c54577896719119040
Dan Larkin <dan@danlarkin.org>
2009-12-11 02:19:53 -0500

    update Leiningen formula to 1.0.1

--- Library/Formula/leiningen.rb
+++ Library/Formula/leiningen.rb
@@ -1,13 +1,18 @@
 require 'formula'
 
 class Leiningen <Formula
-  version '0.5.0'
-  url 'http://github.com/technomancy/leiningen/tarball/0.5.0'
+  version '1.0.1'
+  url 'http://github.com/technomancy/leiningen/tarball/1.0.1'
   homepage 'http://github.com/technomancy/leiningen'
-  md5 '9f63981bbcf11272270bdff0747fff2f'
+  md5 'eb287442bb1bcac2de537d00c4d1b1d3'
 
   def install
     system "bin/lein self-install"
-    prefix.install  %w[bin README.md]
+    prefix.install  %w[bin README.md COPYING NEWS]
+
+    # Install the lein bash completion file
+    system "mv bash_completion.bash lein-completion.bash"
+    (etc+'bash_completion.d').install 'lein-completion.bash'
+
   end
 end
