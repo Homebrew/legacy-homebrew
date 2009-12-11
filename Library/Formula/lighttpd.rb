bb79641f7cdcf9256f5743ee0c3fa2bd5c6daeb4
Adam Vandenberg <flangy@gmail.com>
2009-12-11 09:41:23 -0800

    Update Lighttpd to 1.4.25

--- Library/Formula/lighttpd.rb
+++ Library/Formula/lighttpd.rb
@@ -1,14 +1,17 @@
 require 'formula'
 
 class Lighttpd <Formula
-  @url='http://www.lighttpd.net/download/lighttpd-1.4.23.tar.bz2'
-  @homepage='http://www.lighttpd.net/'
-  @md5='0ab6bb7b17bf0f515ce7dce68e5e215a'
+  url 'http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.25.tar.bz2'
+  md5 '2027c49fb46530e45338c5e2da13c02f'
+  homepage 'http://www.lighttpd.net/'
 
-  depends_on :pcre
+  depends_on 'pkg-config'
+  depends_on 'pcre'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-openssl", "--with-ldap"
+    args = ["--prefix=#{prefix}", "--disable-dependency-tracking", 
+            "--with-openssl", "--with-ldap"]
+    system "./configure", *args
     system "make install"
   end
 end
