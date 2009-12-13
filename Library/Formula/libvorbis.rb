4c9d6e5a2c63ad459ba218660a8aeb1f7d10b2f6
Adam Vandenberg <flangy@gmail.com>
2009-12-12 17:36:07 -0800

    Reformat libogg and libvorbis

--- Library/Formula/libvorbis.rb
+++ Library/Formula/libvorbis.rb
@@ -1,14 +1,16 @@
 require 'formula'
 
 class Libvorbis <Formula
-  @url='http://downloads.xiph.org/releases/vorbis/libvorbis-1.2.3.tar.bz2'
-  @md5='67beb237faf97d74782ec7071756b2b6'
-  @homepage='http://vorbis.com'
+  url 'http://downloads.xiph.org/releases/vorbis/libvorbis-1.2.3.tar.bz2'
+  md5 '67beb237faf97d74782ec7071756b2b6'
+  homepage 'http://vorbis.com'
 
   depends_on 'libogg'
 
   def install
-    system "./configure --enable-docs --disable-debug --disable-dependency-tracking --prefix='#{prefix}'"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-docs"
     system "make install"
   end
 end
