c4a51615adadf618e57391227fedde39234ffef9
Adam Vandenberg <flangy@gmail.com>
2009-12-12 17:36:07 -0800

    Reformat libogg and libvorbis

--- Library/Formula/libogg.rb
+++ Library/Formula/libogg.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Libogg <Formula
-  @homepage='http://www.xiph.org/ogg/'
-  @url='http://downloads.xiph.org/releases/ogg/libogg-1.1.4.tar.gz'
-  @md5='10200ec22543841d9d1c23e0aed4e5e9'
+  homepage 'http://www.xiph.org/ogg/'
+  url 'http://downloads.xiph.org/releases/ogg/libogg-1.1.4.tar.gz'
+  md5 '10200ec22543841d9d1c23e0aed4e5e9'
 
   def install
-    system "./configure --disable-debug --disable-dependency-tracking --prefix='#{prefix}'"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug",
+                          "--disable-dependency-tracking"
     system "make"
     ENV.deparallelize
     system "make install"
