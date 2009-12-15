9660cc3f406e5291a06d16e27ad5b2816a771e32
Adam Vandenberg <flangy@gmail.com>
2009-12-15 07:41:53 -0800

    Update bazaar to 2.0.3

--- Library/Formula/bazaar.rb
+++ Library/Formula/bazaar.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
 class Bazaar <Formula
-  url 'http://launchpad.net/bzr/2.0/2.0.2/+download/bzr-2.0.2.tar.gz'
+  url 'http://launchpad.net/bzr/2.0/2.0.3/+download/bzr-2.0.3.tar.gz'
+  md5 '60758e61b3fd3686966d7ab0ea17fa64'
   homepage 'http://bazaar-vcs.org/'
-  md5 '845743611ef6f1ece8e7bf9824c0bb95'
   
   def install
     ENV.minimal_optimization
-    system "python setup.py build"
+    system "python", "setup.py", "build"
     system "python", "setup.py", "install", "--prefix=#{prefix}"
   end
 end
