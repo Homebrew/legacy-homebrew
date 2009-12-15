433e84c0c6f300cbfbb21edaf2c67d13c178cdc7
Drew Hess <dhess-src@bothan.net>
2009-12-15 03:30:03 -0800

    Updated to chicken 4.3.0.

--- Library/Formula/chicken.rb
+++ Library/Formula/chicken.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Chicken <Formula
-  url 'http://www.call-with-current-continuation.org/chicken-4.2.0.tar.gz'
+  url 'http://www.call-with-current-continuation.org/chicken-4.3.0.tar.gz'
   homepage 'http://www.call-with-current-continuation.org/'
-  md5 '4705b7634447a571ff083f435c110fe3'
+  md5 '4c8e0b8100ca6537c1198a5c2555c8c0'
 
   def install
     ENV.deparallelize
