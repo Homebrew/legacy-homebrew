c479205c00a66a70212a4488f78d057ce07667b8
Joe <joe@der-dieb.local>
2009-12-16 12:42:36 -0800

    Skip clean all of ocaml
    Fixes #188.
    
    Signed-off-by: Max Howell <max@methylblue.com>
    
    I tested just cleaning lib and the test cases still didn't work.

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -17,5 +17,7 @@ class ObjectiveCaml <Formula
     system "make install"
   end
 
+  # note it indeed seems necessary to clean everything
+  # see http://github.com/mxcl/homebrew/issues#issue/188
   def skip_clean? path; true; end
 end
