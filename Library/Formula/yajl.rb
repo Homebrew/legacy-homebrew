7aebd95dafeffb4e9fdc542d9f640e0b91daef13
Martin Kuehl <martin.kuehl@gmail.com>
2009-12-16 14:21:29 +0100

    Fix MD5 checksum for Yajl 1.0.7

--- Library/Formula/yajl.rb
+++ Library/Formula/yajl.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Yajl <Formula
   homepage 'http://lloyd.github.com/yajl/'
   url 'http://cloud.github.com/downloads/lloyd/yajl/yajl-1.0.7.tar.gz'
-  md5 'bb201150143352b117be702a1bc78405'
+  md5 'a4436163408fe9b8c9545ca028ef1b4f'
 
   # Configure uses cmake, even though it looks like we're
   # just using autotools below.
