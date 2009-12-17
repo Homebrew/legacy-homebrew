509da99d600e48ccf6df1ea8aa3e1136509efcf0
Max Howell <max@methylblue.com>
2009-12-17 14:58:10 +0000

    htop doesn't seem to require ncursesw
    otool -L showed it linked to the system version, and it compiled without ncursesw installed.

--- Library/Formula/htop.rb
+++ Library/Formula/htop.rb
@@ -4,12 +4,9 @@ class Htop <Formula
   head 'git://github.com/AndyA/htop-osx.git', :branch => 'osx'
   homepage 'http://htop.sourceforge.net/'
 
-  depends_on 'ncursesw'
-
   def install
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make", "install", "DEFAULT_INCLUDES='-iquote .'"
   end
-
 end
