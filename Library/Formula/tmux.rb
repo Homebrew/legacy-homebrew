4d5510e7f634e7be1532b4dd28e75e8f425a8c79
Adam Vandenberg <flangy@gmail.com>
2009-12-17 15:10:48 -0800

    Update tmux to use new inreplace features.

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -2,14 +2,20 @@ require 'formula'
 
 class Tmux <Formula
   url 'http://downloads.sourceforge.net/tmux/tmux-1.1.tar.gz'
-  homepage 'http://tmux.sourceforge.net'
   md5 'faf2fc52ac3ae63d899f6fece2c112cd'
+  homepage 'http://tmux.sourceforge.net'
 
   def install
     ENV['PREFIX'] = "#{prefix}"
     system "./configure"
-    inreplace "GNUmakefile", " -g bin -o root", ""
-    inreplace "GNUmakefile", "man/man1", "share/man/man1"
+
+    inreplace "GNUmakefile" do |f|
+      # Fix 'install' flags
+      f.gsub! " -g bin -o root", ""
+      # Put docs in the right place
+      f.gsub! "man/man1", "share/man/man1"
+    end
+
     system "make install"
   end
 end
