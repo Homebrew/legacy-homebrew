ec6f290cc0f88b6a54b6da2668dba8cf08177722
Max Howell <max@methylblue.com>
2009-12-19 14:10:18 +0000

    Revert "Mercurial - use post-fix if"
    This reverts commit 6a8b3cc71feccbf73225bc7dc8c5e6972de9953d.
    
    This was a bad merge on my part.
    
    Fixes #221. Fixes #220.

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -243,7 +243,7 @@ class MercurialDownloadStrategy <AbstractDownloadStrategy
     url=@url.sub(%r[^hg://], '')
 
     unless @clone.exist?
-      safe_system 'hg', 'clone', *checkout_args
+      safe_system 'hg', 'clone', url, @clone
     else
       puts "Updating #{@clone}"
       Dir.chdir(@clone) { safe_system 'hg', 'update' }
