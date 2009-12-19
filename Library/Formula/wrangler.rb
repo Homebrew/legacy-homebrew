9cd389d338754a113a4149d11f0c23732ba3d42a
joewilliams <joe@der-dieb.local>
2009-12-18 21:33:02 -0800

    wrangler upgrade and fix
    Fixes #219

--- Library/Formula/wrangler.rb
+++ Library/Formula/wrangler.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class Wrangler <Formula
-  @url='http://www.cs.kent.ac.uk/projects/forse/wrangler/wrangler-0.8/wrangler-0.8.1.tar.gz'
+  @url='http://www.cs.kent.ac.uk/projects/forse/wrangler/wrangler-0.8/wrangler-0.8.4.tar.gz'
   @homepage='http://www.cs.kent.ac.uk/projects/forse/'
-  @md5='0ddc9308eb82382d11d0a6df486050b5'
+  @md5='84466a243b91ea7467296a945fb644fe'
 
   depends_on 'erlang'
 
   def install
+    ENV.deparallelize
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make"
     system "make install"
