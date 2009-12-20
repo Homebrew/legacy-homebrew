e87777a565ce0c8fc45893bdec166e7e3ea53999
Adam Vandenberg <flangy@gmail.com>
2009-12-19 23:04:02 -0800

    Remove quotes from prefix in libsamplerate.

--- Library/Formula/libsamplerate.rb
+++ Library/Formula/libsamplerate.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Libsamplerate <Formula
-  @homepage='http://www.mega-nerd.com/SRC'
-  @url='http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz'
-  @md5='ad093e60ec44f0a60de8e29983ddbc0f'
+  homepage 'http://www.mega-nerd.com/SRC'
+  url 'http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz'
+  md5 'ad093e60ec44f0a60de8e29983ddbc0f'
 
   def install
-    system "./configure --disable-debug --prefix='#{prefix}'"
+    system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make install"
   end
 end
