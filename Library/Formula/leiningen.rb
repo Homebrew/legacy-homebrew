14a174c479223a4ef985968e312b01c570379f46
Max Howell <max@methylblue.com>
2009-12-22 04:43:51 +0000

    Clean up
    Don't need to specify version as url is sufficient.
    
    Homebrew installs COPYING and README.md for you.
    
    Use Ruby functions where possible not BSD mv.

--- Library/Formula/leiningen.rb
+++ Library/Formula/leiningen.rb
@@ -1,18 +1,16 @@
 require 'formula'
 
 class Leiningen <Formula
-  version '1.0.1'
   url 'http://github.com/technomancy/leiningen/tarball/1.0.1'
   homepage 'http://github.com/technomancy/leiningen'
   md5 'eb287442bb1bcac2de537d00c4d1b1d3'
 
   def install
     system "bin/lein self-install"
-    prefix.install  %w[bin README.md COPYING NEWS]
+    prefix.install 'bin'
 
     # Install the lein bash completion file
-    system "mv bash_completion.bash lein-completion.bash"
+    mv 'bash_completion.bash', 'lein-completion.bash'
     (etc+'bash_completion.d').install 'lein-completion.bash'
-
   end
 end
