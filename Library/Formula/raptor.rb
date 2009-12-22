899b0cfc09cb2323dc3016dfc54e9f70d110b422
Ryan Shaw <ryanshaw@ischool.berkeley.edu>
2009-12-22 12:40:31 -0800

    Updated raptor to 1.4.20.

--- Library/Formula/raptor.rb
+++ Library/Formula/raptor.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Raptor <Formula
-  @url='http://download.librdf.org/source/raptor-1.4.19.tar.gz'
-  @homepage='http://librdf.org/raptor/'
-  @md5='50acbd3b416c3f9e1a3a8ddb825bac25'
+  url 'http://download.librdf.org/source/raptor-1.4.20.tar.gz'
+  homepage 'http://librdf.org/raptor/'
+  md5 '4d9c950ea794d3e141ff60875b0a2bbc'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
