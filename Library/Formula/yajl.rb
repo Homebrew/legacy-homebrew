23ddf9bd5a325397e23c0ba44600ee61b3214ef5
ggironda <gabriel.gironda@centro.net>
2009-12-23 13:28:10 -0600

    Updates the YAJL formula and adds a Orderly formula
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yajl.rb
+++ Library/Formula/yajl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Yajl <Formula
   homepage 'http://lloyd.github.com/yajl/'
-  url 'http://cloud.github.com/downloads/lloyd/yajl/yajl-1.0.7.tar.gz'
-  md5 'a4436163408fe9b8c9545ca028ef1b4f'
+  url 'http://cloud.github.com/downloads/lloyd/yajl/yajl-1.0.8.tar.gz'
+  md5 '26116d41b6466f6b4da7d9e8450f2200'
 
   # Configure uses cmake, even though it looks like we're
   # just using autotools below.
