36c5a42667305d7564159946c0bb01f3505984e6
Benjamin Kramer <benny.kra@gmail.com>
2009-12-26 15:11:03 +0100

    Add MacVim formula
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/macvim.rb
@@ -0,0 +1,38 @@
+require 'formula'
+
+class Macvim <Formula
+  head 'git://repo.or.cz/MacVim.git'
+  homepage 'http://code.google.com/p/macvim'
+
+  def install
+    # MacVim's Xcode project gets confused by $CC, disable it until someone
+    # figures out why it fails.
+    ENV['CC'] = nil
+    ENV['CFLAGS'] = nil
+    ENV['CXX'] = nil
+    ENV['CXXFLAGS'] = nil
+
+    system "./configure",
+           # Add some features
+           "--with-features=huge",
+           "--enable-perlinterp",
+           "--enable-pythoninterp",
+           "--enable-rubyinterp",
+           "--enable-tclinterp"
+    system "make"
+
+    prefix.install "src/MacVim/build/Release/MacVim.app"
+    inreplace "src/MacVim/mvim", /^# VIM_APP_DIR=\/Applications$/,
+              "VIM_APP_DIR=#{prefix}"
+    bin.install "src/MacVim/mvim"
+
+    # Create MacVim vimdiff, view, ex equivalents
+    %w[mvimdiff mview mvimex].each do |f|
+      (bin + f).make_symlink("#{bin}/mvim")
+    end
+  end
+
+  def caveats
+    "MacVim.app installed to #{prefix}."
+  end
+end
