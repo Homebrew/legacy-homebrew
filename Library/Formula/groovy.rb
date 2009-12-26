9afaef40e11292ab2c57264c99104ae1acb5db62
Sean Brandt <sean@fuzzymagic.com>
2009-12-26 10:27:05 -0500

    update groovy to 1.7.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Groovy <Formula
-  @url='http://dist.groovy.codehaus.org/distributions/groovy-binary-1.6.5.zip'
+  @url='http://dist.groovy.codehaus.org/distributions/groovy-binary-1.7.0.zip'
   @homepage='http://groovy.codehaus.org/'
-  @version='1.6.5'
-  @sha256='db3d4c08ad76392ae94eba830e8c9072fda9e5774c9e7d220c90d0f91a5d7aaf'
+  @version='1.7.0'
+  @sha256='7f08ce19c757ddf8b14f5dcf909048719c82348584aa707bbe0a65a9dd9c6afc'
 
   def install
     prefix.install %w[bin conf lib]
