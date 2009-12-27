336c7218c4a3d04c4eaeef0520f507c50c386564
Alexis Hildebrandt <afh@surryhill.net>
2009-12-27 12:34:58 +0100

    Dwm 5.7.2
    Dynamic window manager for X.
    
    It manages windows in tiled, monocle and floating layouts.  Either
    layout can be applied dynamically, optimising the environment for the
    application in use and the task performed.
    
    In tiled layout windows are managed in a master and stacking area. The
    master area contains the window which currently needs most attention,
    whereas the stacking area contains all other windows. In monocle layout
    all windows are maximised to the screen size. In floating layout
    windows can be resized and moved freely. Dialog windows are always
    managed floating, regardless of the layout applied.
    
    Windows are grouped by tags. Each window can be tagged with one or
    multiple tags. Selecting certain tags displays all windows with these
    tags.
    
    Each screen contains a small status bar which displays all available
    tags, the layout, the number of visible windows, the title of the
    focused window, and the text read from the root window name property,
    if the screen is focused. A floating window is indicated with an empty
    square and a maximised floating window is indicated with a filled
    square before the windows title. The selected tags are indicated with a
    different color. The tags of the focused window are indicated with a
    filled square in the top left corner. The tags which are applied to one
    or more windows are indicated with an empty square in the top left
    corner.
    
    Dwm draws a small border around windows to indicate the focus state.

---
+++ Library/Formula/dwm.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Dwm <Formula
+  url 'http://dl.suckless.org/dwm/dwm-5.7.2.tar.gz'
+  homepage 'http://dwm.suckless.org/'
+  md5 'a0b8a799ddc5034dd8a818c9bd76f3a3'
+  head 'http://hg.suckless.org/dwm'
+
+  def install
+    # The dwm default quit keybinding Mod1-Shift-q collides with 
+    # the Mac OS X Log Out shortcut in the Apple menu.
+    inreplace 'config.def.h',
+    '{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },',
+    '{ MODKEY|ControlMask,           XK_q,      quit,           {0} },'
+    inreplace 'dwm.1', '.B Mod1\-Shift\-q', '.B Mod1\-Control\-q'
+    system "make PREFIX=#{prefix} install"
+  end
+
+  def caveats
+    <<EOF
+    In order to use the Mac OS X command key '⌘' for dwm commands
+    change the X11 keyboard modifier map using xmodmap (1).
+
+    e.g. by running the following command from $HOME/.xinitrc
+    xmodmap -e 'remove Mod2 = Meta_L' -e 'add Mod1 = Meta_L'&
+EOF
+  end
+end
