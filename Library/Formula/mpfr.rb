e78349f317f7626b53ff07b68dd2280da9222f8d
Alexis Hildebrandt <afh@surryhill.net>
2009-12-27 12:30:02 +0100

    Mpfr 2.4.2
    A C library for multiple-precision floating-point computations
    with correct rounding.

---
+++ Library/Formula/mpfr.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Mpfr <Formula
+  url 'http://www.mpfr.org/mpfr-current/mpfr-2.4.2.tar.bz2'
+  homepage 'http://www.mpfr.org/'
+  md5 '89e59fe665e2b3ad44a6789f40b059a0'
+
+  depends_on 'gmp'
+
+  def install
+      configure_args = [
+          "--prefix=#{prefix}",
+          "--disable-debug",
+          "--disable-dependency-tracking",
+      ]
+    system "./configure", *configure_args
+    system "make install"
+  end
+end
