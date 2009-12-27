3cf08f4fd655c9c2e16e2c89edaf45bbcb4b5e27
Alexis Hildebrandt <afh@surryhill.net>
2009-12-27 14:10:14 +0100

    Urlview 0.9-18.1
    This utility is used to extract URL from text files, especially from
    mail messages in order to launch some browser to view them.
    
    This used to be a part of mutt but has now become an independent tool.

---
+++ Library/Formula/urlview.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Urlview <Formula
+  url 'http://ftp.de.debian.org/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz'
+  homepage 'http://packages.debian.org/unstable/misc/urlview'
+  md5 '4f253c82b59136181b1f0bf218ef059d'
+  
+  def patches
+    "http://ftp.de.debian.org/debian/pool/main/u/urlview/urlview_0.9-18.1.diff.gz"
+  end
+
+  def install
+    inreplace 'urlview.man', '/etc/urlview/url_handler.sh', 'open'
+    inreplace 'urlview.c',
+      '#define DEFAULT_COMMAND "/etc/urlview/url_handler.sh %s"',
+      '#define DEFAULT_COMMAND "open %s"'
+    configure_args = [
+        "--prefix=#{prefix}",
+        "--disable-debug",
+        "--disable-dependency-tracking",
+    ]
+    system "./configure", *configure_args
+    man1.mkpath()
+    system "make install"
+  end
+end
