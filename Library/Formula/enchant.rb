cac686d77a128919dc35dec255109929d9cc334d
Alexis Hildebrandt <afh@surryhill.net>
2009-12-28 01:11:47 +0100

    Enchant 1.5.0
    On the surface, Enchant appears to be a generic spell checking library.
    You can request dictionaries from it, ask if a word is correctly
    spelled, get corrections for a misspelled word, etc...
    
    Beneath the surface, Enchant is a whole lot more - and less - than that.
    You'll see that Enchant isn't really a spell checking library at all.
    
    "What's that?" you ask. Well, Enchant doesn't try to do any of the work
    itself. It's lazy, and requires backends to do most of its dirty work.
    Looking closer, you'll see the Enchant is more-or-less a fancy wrapper
    around the dlopen() system call. Enchant steps in to provide uniformity
    and conformity on top of these libraries, and implement certain features
    that may be lacking in any individual provider library. Everything
    should "just work" for any and every definition of "just working."

---
+++ Library/Formula/enchant.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Enchant <Formula
+  url 'http://www.abisource.com/downloads/enchant/1.5.0/enchant-1.5.0.tar.gz'
+  homepage 'http://www.abisource.com/projects/enchant/'
+  md5 '7dfaed14e142b4a0004b770c9568ed02'
+
+  depends_on 'aspell' => :optional
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
