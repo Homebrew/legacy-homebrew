084b94fcca61c65b58907395ac7dd502cb73e938
Alexis Hildebrandt <afh@surryhill.net>
2009-12-28 13:53:25 +0100

    Ktoblzcheck 1.12
    A library to check account numbers and bank codes of German banks.
    
    Both a library for other programs as well as a short command-line tool is
    available.
    
    It is possible to check pairs of account numbers and bank codes (BLZ) of German
    banks, and to map bank codes (BLZ) to the clear-text name and location of the
    bank.

---
+++ Library/Formula/ktoblzcheck.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Ktoblzcheck <Formula
+  url 'http://freefr.dl.sourceforge.net/project/ktoblzcheck/ktoblzcheck/ktoblzcheck-1.24.tar.gz'
+  homepage 'http://ktoblzcheck.sourceforge.net/'
+  md5 '6385d5eca2db5f7eb3bd49312166eaf7'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make"
+    ENV.j1
+    system "make install"
+  end
+end
