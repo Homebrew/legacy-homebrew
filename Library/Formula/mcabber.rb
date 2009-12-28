1082708af3fa5d1771b9ad3a57172d4a67af84dd
Alexis Hildebrandt <afh@surryhill.net>
2009-12-28 01:12:56 +0100

    Mcabber 0.9.10
    A small Jabber console client.
    
    Mcabber includes features such as:
    
     - SSL support
    
     - MUC (Multi-User Chat) support
    
     - history logging
    
     - command completion
    
     - OpenPGP encryption
    
     - OTR (Off-the-Record Messaging) support and external action triggers

---
+++ Library/Formula/mcabber.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Mcabber <Formula
+  url 'http://mcabber.com/files/mcabber-0.9.10.tar.bz2'
+  homepage 'http://mcabber.com/'
+  md5 '887415d16c32af58eab2ec2d9bb17fa6'
+
+  depends_on 'glib'
+  depends_on 'gpgme' => :optional
+  depends_on 'aspell' => :optional
+  depends_on 'enchant' => :optional
+  depends_on 'libotr' => :optional
+  depends_on 'libgcrypt' => :optional
+
+  def install
+    configure_args = [
+        "--prefix=#{prefix}",
+        "--disable-debug",
+        "--disable-dependency-tracking",
+        "--enable-aspell",
+        "--enable-enchant",
+        "--enable-otr",
+        "--with-ssl"
+    ]
+    system "./configure", *configure_args
+    system "make install"
+  end
+end
