a35101dfb295ea674d13365ed856ebe690d9db62
tstevens <tstevens@me.com>
2010-01-01 03:14:37 -0500

    Fix compile error with gnu-prolog.

--- Library/Formula/gnu-prolog.rb
+++ Library/Formula/gnu-prolog.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class GnuProlog <Formula
-  url 'http://www.gprolog.org/gprolog-1.3.1.tar.gz'
+  url 'http://gprolog.univ-paris1.fr/unstable/gprolog-20091217.tgz'
   homepage 'http://www.gprolog.org/'
-  md5 'cbae19c31e17bcfca4b57fe35ec4aba2'
+  md5 '3a0c9994927c8ff9e0a9c6edac0b2e69'
 
   def skip_clean? path; true; end
 
   def install
     # make won't run in parallel
     ENV.j1
-    
+
     Dir.chdir 'src' do
       system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
       system "make"
