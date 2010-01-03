89293d37815fa11568981e3038ce5aeacbbd7e53
Jonathan Wright <quaggy@gmail.com>
2010-01-03 15:19:34 +1300

    ncdu 1.6
    ncdu (NCurses Disk Usage) is a curses-based version of the well-known
    'du', and provides a fast way to see what directories are using your
    disk space.
    
    Signed-off-by: David Höppner <0xffea@gmail.com>

---
+++ Library/Formula/ncdu.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Ncdu <Formula
+  url 'http://dev.yorhel.nl/download/ncdu-1.6.tar.gz'
+  homepage 'http://dev.yorhel.nl/ncdu'
+  md5 '95d29cf64af2d8cf4b5005e6e3d60384'
+
+  def install
+    # ncdu segfaults when built for 64-bit
+    ENV.m32
+
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
