d3a8493d5ba9e1c56336b765b8c6b1faeadbea17
Jonathan Wright <quaggy@gmail.com>
2010-01-04 21:20:59 +1300

    Pngnq 1.0
    Pngnq quantizes 24-bit (RGB) and 32-bit (RGBA) PNG images down to 8-bit
    (palette) PNG8 images.
    
    http://pngnq.sourceforge.net/
    
    See http://www.sitepoint.com/blogs/2007/09/18/png8-the-clear-winner/ for
    one motivation on using PNG8 images (IE6 support). Another motiviation
    is that PNG8 images are smaller and do a good job of lossy compression
    of screen shots.

---
+++ Library/Formula/pngnq.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Pngnq <Formula
+  url 'http://downloads.sourceforge.net/project/pngnq/pngnq/1.0/pngnq-1.0.tar.gz'
+  homepage 'http://pngnq.sourceforge.net/'
+  md5 '2d2cdacf0284477c662fee888c8092d5'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
