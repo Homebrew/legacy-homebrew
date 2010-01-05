1b4954c69cbad3866ac730d983b3dfe8c09a66bd
toothrot <arakoczy@gmail.com>
2010-01-05 11:41:09 -0600

    Updates broken LFTP formula

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -1,11 +1,13 @@
 require 'formula'
 
 class Lftp <Formula
-  url 'http://ftp.yars.free.net/pub/source/lftp/lftp-3.7.15.tar.gz'
+  url 'http://ftp.yars.free.net/pub/source/lftp/lftp-4.0.5.tar.gz'
   homepage 'http://lftp.yar.ru/'
-  md5 '6c43ffdb59234ff0533cfdda0c3c305c'
+  md5 '73ea519c9e9cdfa47b714c5c91093a0d'
 
   depends_on 'readline'
+  depends_on 'pkg-config'
+  depends_on 'gnutls'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
