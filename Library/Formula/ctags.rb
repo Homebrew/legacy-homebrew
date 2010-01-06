62ea752cd32a1cd55a48d012c2dc75cca01d6471
toothrot <scissorjammer@gmail.com>
2010-01-06 10:54:34 -0600

    ctags aka ctags-exuberant

--- Library/Formula/ctags.rb
+++ Library/Formula/ctags.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
-# TODO keywords for search include 'exuberant'
-
 class Ctags <Formula
   @url='http://prdownloads.sourceforge.net/ctags/ctags-5.8.tar.gz'
   @homepage='http://ctags.sourceforge.net/'
   @md5='c00f82ecdcc357434731913e5b48630d'
+
+  aka 'ctags-exuberant'
 
   def install
     system "./configure", "--prefix=#{prefix}",
