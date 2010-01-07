e421f59bf40b54abb2ef19b850e674c8dc2a7a0a
Jeremy Larkin <jeremy@formcollective.com>
2010-01-07 13:15:11 -0500

    Added formula for peg/leg 0.1.4
    The formula patches the Makefile to enable simpler installation of the
    programs and to install the manual file.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    Use brew install functions instead of patch.

---
+++ Library/Formula/peg.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Peg <Formula
+  url 'http://piumarta.com/software/peg/peg-0.1.4.tar.gz'
+  homepage 'ihttp://piumarta.com/software/peg/'
+  md5 'feb52a19a749f08fa8602387f1acbd93'
+  
+  aka "leg"
+
+  def install
+    system "make all"
+    bin.install %w[peg leg]
+    man1.install gzip("peg.1")
+  end
+end
