231197ed51ae7176b561a2b499e7818ddcb0fed3
David Höppner <0xffea@gmail.com>
2010-01-07 15:09:20 +0100

    slrn: slang was not found

--- Library/Formula/slrn.rb
+++ Library/Formula/slrn.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Slrn <Formula
   url 'ftp://space.mit.edu/pub/davis/slrn/slrn-0.9.9p1.tar.gz'
-  homepage 'ihttp://www.slrn.org/'
+  homepage 'http://www.slrn.org/'
   md5 '6cc8ac6baaff7cc2a8b78f7fbbe3187f'
   version '0.9.9p1'
 
@@ -14,6 +14,7 @@ class Slrn <Formula
         "--disable-debug",
         "--disable-dependency-tracking",
         "--with-ssl",
+        "--with-slang=#{HOMEBREW_PREFIX}",
     ]
     system "./configure", *configure_args
     system "make all slrnpull"
