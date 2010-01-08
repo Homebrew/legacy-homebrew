55283a88e82f3296d766a8ef01de57d5c3841e30
kashif <kashif@nomad-labs.com>
2010-01-08 12:02:50 +0100

    Updated postgis to 1.4.1
    This is a minor release addressing:
        * #241, crash in ST_LineCrossingDirection
        * #210, segmentation faults in ST_Union with NULLs
        * #277, crash on very large numeric inputs
        * #179, ST_MakeLine crash server with NULLs
        * #253, inconsistent results when using the ~= operator
        * #276, ST_AsGML producing non-compliant GML
        * #239, memory issues with ST_As* functions
        * #272, ST_LineCrossingDirection should be negatively symmetric
        * #316, parsing problem for invalid higher dimensional geometries

--- Library/Formula/postgis.rb
+++ Library/Formula/postgis.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Postgis <Formula
-  url 'http://postgis.refractions.net/download/postgis-1.4.0.tar.gz'
+  url 'http://postgis.refractions.net/download/postgis-1.4.1.tar.gz'
   homepage 'http://postgis.refractions.net/'
-  md5 'bc5b97d5399bd20ca90bfdf784ab6c33'
+  md5 '78d13c4294f3336502ad35c8a30e5583'
 
   depends_on 'postgresql'
   depends_on 'proj'
