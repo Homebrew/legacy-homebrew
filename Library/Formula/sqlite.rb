760ceef59d2ac08ddc449fa13424cabcd5a10a64
Aku Kotkavuo <aku@hibana.net>
2010-01-11 07:37:26 +0200

    Update sqlite to 3.6.22
    Signed-off-by: David Höppner <0xffea@gmail.com>

--- Library/Formula/sqlite.rb
+++ Library/Formula/sqlite.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Sqlite <Formula
-  url 'http://www.sqlite.org/sqlite-amalgamation-3.6.20.tar.gz'
-  md5 '4bb3e9ee5d25e88b8ff8533fbeb168aa'
+  url 'http://www.sqlite.org/sqlite-amalgamation-3.6.22.tar.gz'
+  md5 'b683b3903e79ab8a6d928dc9d4a56937'
   homepage 'http://www.sqlite.org/'
 
   def install
