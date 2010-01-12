ca1c2aa7fedcbb04bb1013ef697b5dcc2958fe92
Alex Zhukov <baron.pampa@gmail.com>
2010-01-13 02:07:52 +0300

    Update nginx to version 0.7.64
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Nginx < Formula
-  @url='http://sysoev.ru/nginx/nginx-0.7.62.tar.gz'
+  @url='http://sysoev.ru/nginx/nginx-0.7.64.tar.gz'
   @homepage='http://nginx.net/'
-  @md5='ab22f1b7f098a90d803a3abb94d23f7e'
+  @md5='cfa97c0f784532820110b5eee57fef6d'
 
   depends_on 'pcre'
 
