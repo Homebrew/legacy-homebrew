1342debdb257d52e07387da18c84ff1d11520aa8
Jason Baker <amnorvend@gmail.com>
2010-01-12 10:23:12 -0600

    Adding a stable install of yeti.

--- Library/Formula/yeti.rb
+++ Library/Formula/yeti.rb
@@ -1,17 +1,16 @@
 require 'formula'
 
 class Yeti <Formula
+  url 'http://linux.ee/~mzz/yeti/yeti.jar'
   homepage 'http://mth.github.com/yeti/'
-  md5 'd11756e552d0a15a007ed3403d0d86e1'
+  md5 '1d30c9bbace011e2ccb1dcced64c89b2'
   head 'git://github.com/mth/yeti.git'
-  version 'HEAD'
+  version '082009' # Yeti doesn't do any versioning that I can see, so
+                   # tracking this by date
   JAR = 'yeti.jar'
 
   def install
-    system '/usr/bin/ant'
-    system '/usr/bin/ant jar'
     prefix.install JAR
-
     (bin+'yeti').write(eval('"'+DATA.read+'"'))
   end
 end
