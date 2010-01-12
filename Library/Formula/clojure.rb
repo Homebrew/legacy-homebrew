b976a58f6dc87a321c552c014b433ce0e57a8d1e
Jason Baker <amnorvend@gmail.com>
2010-01-12 11:15:03 -0600

    Updating clojure formula to work against version 1.1.0

--- Library/Formula/clojure.rb
+++ Library/Formula/clojure.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Clojure <Formula
-  url 'http://clojure.googlecode.com/files/clojure_1.0.0.zip'
-  md5 'e7a50129040df7fe52287006988ecbb2'
+  url 'http://clojure.googlecode.com/files/clojure-1.1.0.zip'
+  md5 '9c9e92f85351721b76f40578f5c1a94a'
   head 'git://github.com/richhickey/clojure.git'
   homepage 'http://clojure.org/'
-  JAR = "clojure-1.0.0.jar"
+  JAR = "clojure.jar"
 
   def script
     DATA.read.gsub 'CLOJURE_JAR_PATH_PLACEHOLDER', prefix+JAR
