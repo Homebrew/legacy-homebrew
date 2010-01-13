477bfcad291f0dc45315e90d4aa65fb3adf35419
Max Howell <max@methylblue.com>
2010-01-13 05:12:09 +0000

    Update pngcrush to 1.7.7

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pngcrush <Formula
   homepage 'http://pmt.sourceforge.net/pngcrush/'
-  url "http://downloads.sourceforge.net/sourceforge/pmt/pngcrush-1.7.2.tar.bz2"
-  md5 '24ead6781e7e017e8ee2bf61db93a355'
+  url "http://downloads.sourceforge.net/sourceforge/pmt/pngcrush-1.7.7.tar.bz2"
+  md5 '0ac097be4c7eb28504f8a583ee92b103'
 
   def install
     # use our CFLAGS, LDFLAGS, CC, and LD
