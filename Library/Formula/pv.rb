244473c5c40666127d5cc52c037d84db0b1d2195
Florian Sowade <f.sowade@lab9.de>
2010-01-14 01:10:46 +0100

    pv failed to compile with nls
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pv.rb
+++ Library/Formula/pv.rb
@@ -10,7 +10,7 @@ class Pv <Formula
   def install
     ENV.gcc_4_2
 
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--disable-nls"
     system "make install"
   end
 end
