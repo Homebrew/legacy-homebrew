8e400b0bb90ffe8670409e67cb4684ff1d6ef0dd
Max Howell <max@methylblue.com>
2010-01-19 14:41:25 +0000

    Warn user about PYTHON_PATH. Closes #316

--- Library/Formula/bazaar.rb
+++ Library/Formula/bazaar.rb
@@ -10,4 +10,13 @@ class Bazaar <Formula
     system "python", "setup.py", "build"
     system "python", "setup.py", "install", "--prefix=#{prefix}"
   end
+
+  def caveats; <<-EOS
+Really Bazaar should be installed by easy_install or pip, but currently this
+doesn't work properly. As a result you need to set PYTHONPATH:
+
+    export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/python2.6/site-packages
+
+    EOS
+  end
 end
