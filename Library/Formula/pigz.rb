6aa96ba602d79671705c784ab3fd6de076d27596
Michael Barton <mail@michaelbarton.me.uk>
2010-01-20 14:57:51 -0700

    Fix broken url for pigz tarball

--- Library/Formula/pigz.rb
+++ Library/Formula/pigz.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Pigz <Formula
-  url 'http://www.zlib.net/pigz/pigz-2.1.5.tar.gz'
+  url 'http://www.zlib.net/pigz/pigz-2.1.6.tar.gz'
   homepage 'http://www.zlib.net/pigz/'
-  md5 '580873165ef3a369674f0c0af4c96d67'
+  md5 'cbe9030c4be3d0ef2438ee5f8b169ca4'
 
   def install
     system "make"
