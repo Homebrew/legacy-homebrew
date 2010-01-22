3f514089c1289366e7bed0e690ac49046e9cee65
kmowery <kmowery@gmail.com>
2010-01-21 21:13:03 -0800

    texinfo 4.13a
    Official documentation format of the GNU project.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    * Removed tex-live dependency
    * Keg-only formula, newer than provied by OS X, needed for some projects.

---
+++ Library/Formula/texinfo.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Texinfo <Formula
+  url 'http://ftp.gnu.org/gnu/texinfo/texinfo-4.13a.tar.gz'
+  homepage 'http://www.gnu.org/software/texinfo/'
+  md5 '71ba711519209b5fb583fed2b3d86fcb'
+
+  def keg_only?
+    :provided_by_osx
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--disable-install-warnings",
+                          "--prefix=#{prefix}"
+    system "make install"
+
+    # The install warns about needing to install texinfo.tex and some other support files.
+    # The texinfo.tex in tex-live 2008 is identical to texinfo's version, so we can ignore this.
+
+    # However, it complains about installing epsf.tex in TEXMF/tex/generic/dvips, so let's do that...
+    # This somewhat breaks the homebrew philosophy, I am sorry.
+    # Also, we don't depend on tex-live, but this directory only exists if it is installed.
+    if File.exist? "#{HOMEBREW_PREFIX}/share/texmf-dist/" then
+      system "cp", "doc/epsf.tex", "#{HOMEBREW_PREFIX}/share/texmf-dist/tex/generic/dvips/"
+    end
+  end
+end
