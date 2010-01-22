e808289148d98d1c952e620a0211c5130578d585
kmowery <kmowery@gmail.com>
2010-01-21 21:07:14 -0800

    Pango 1.26.2
    Update to jphasting's Pango formula. Uses OSX cairo, X, and fontconfig.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/pango.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Pango <Formula
+  url 'http://ftp.gnome.org/pub/gnome/sources/pango/1.26/pango-1.26.2.tar.bz2'
+  homepage 'http://www.pango.org/'
+  md5 'f30212b8833af3ce5c82121dc309e3d0'
+
+  depends_on 'glib'
+  depends_on 'pkg-config'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--with-x", "--with-fontconfig"
+    system "make install"
+  end
+end
