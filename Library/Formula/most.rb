6098d5931877774f6d75ff862b062ede0e23c74c
Max Howell <max@methylblue.com>
2010-01-23 11:31:35 +0000

    Fixes #489; Most can't install outside of /usr/local

--- Library/Formula/most.rb
+++ Library/Formula/most.rb
@@ -7,8 +7,34 @@ class Most <Formula
 
   depends_on 's-lang'
 
+  def patches; DATA end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
 end
+
+
+# most looks in a few set directories for slang, so at least add our prefix
+__END__
+diff --git a/configure b/configure
+index f24b2f1..1cb7eb1 100755
+--- a/configure
++++ b/configure
+@@ -9936,13 +9936,8 @@ echo "$as_me: error: --with-slanginc requres a value" >&2;}
+             $jd_prefix_incdir \
+             /usr/local/slang/include \
+             /usr/local/include/slang \
+-  	  /usr/local/include \
+-  	  /usr/include/slang \
+-  	  /usr/slang/include \
+-  	  /usr/include \
+-  	  /opt/include/slang \
+-  	  /opt/slang/include \
+-  	  /opt/include"
++            /usr/local/include \
++            `brew --prefix`"
+ 
+        for X in $lib_include_dirs
+        do
