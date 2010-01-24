571b21d3542228c84f3096db19185f449aa5c676
spahl <sebastien.pahl@gmail.com>
2010-01-24 12:44:47 +0100

    collectd 4.9.1
    A daemon which collects system performance statistics periodically.

---
+++ Library/Formula/collectd.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Collectd <Formula
+  url 'http://collectd.org/files/collectd-4.9.1.tar.bz2'
+  homepage 'http://collectd.org/'
+  md5 '5753496651c8c84afaea1fe290876bfc'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
