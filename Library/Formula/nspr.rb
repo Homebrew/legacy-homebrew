162783bec497f3f8988532f58d016b4bad60f2fc
Max Howell <max@methylblue.com>
2010-01-24 13:57:44 +0000

    Enable 64 bits on 64 bit machines only

--- Library/Formula/nspr.rb
+++ Library/Formula/nspr.rb
@@ -6,15 +6,18 @@ class Nspr <Formula
   @md5='c78384602b4b466081a55025446641db'
 
   def install
+    require 'hardware'
+
     ENV.deparallelize
     Dir.chdir "mozilla/nsprpub" do
       inreplace "pr/src/Makefile.in", "-framework CoreServices -framework CoreFoundation", ""
-      system "./configure", "--prefix=#{prefix}",
-                            "--disable-debug",
-                            "--enable-strip",
-                            "--enable-optimize",
-                            "--enable-64bit"
+
+      conf = %W[--prefix=#{prefix} --disable-debug --enable-strip --enable-optimize]
+      conf << "--enable-64bit" if Hardware.is_64_bit? and MACOS_VERSION >= 10.6
+      system "./configure", *conf
+
       inreplace "config/autoconf.mk", "-install_name @executable_path/$@ ", ""
+
       system "make"
       system "make install"
     end
