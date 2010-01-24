c8d02bcceabe29b1c6af18c24464517f14478c7f
Chris Wanstrath <chris@ozmm.org>
2010-01-23 17:17:11 -0700

    Update hub to 0.3.0
    Closes #543

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Hub <Formula
-  head 'git://github.com/defunkt/hub.git', :tag => 'v0.1.3'
+  head 'git://github.com/defunkt/hub.git', :tag => 'v0.3.0'
   homepage 'http://github.com/defunkt/hub'
 
   def initialize(*args)
     super
 
     # Force version instead of HEAD
-    @version = '0.1.3'
+    @version = '0.3.0'
   end
 
   def install
