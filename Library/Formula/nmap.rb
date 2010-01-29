d0c0132c62a20939b5c9bb3488cd7ece819f0c93
Kevin M. Dulzo <kdulzo@me.com>
2010-01-28 21:24:07 -0600

    Update nmap to 5.21.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/nmap.rb
+++ Library/Formula/nmap.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Nmap <Formula
-  @url='http://nmap.org/dist/nmap-5.00.tar.bz2'
+  @url='http://nmap.org/dist/nmap-5.21.tar.bz2'
   @homepage='http://nmap.org/5/'
-  @md5='32d27de32166c02d670bb4a086185886'
+  @md5='f77fa51d89ab27d35e5cd87bb086b858'
 
   def install
     ENV.gcc_4_2
@@ -15,13 +15,4 @@ class Nmap <Formula
     system "make install" # seperate steps required otherwise the build fails
   end
 
-  def caveats; <<-EOS
-Root level scans do not work under Snow Leopard. 
-More information: http://seclists.org/nmap-dev/2009/q3/0904.html
-
-There is a fix but it was made after the 5.00 release, so if you need it 
-install the latest Nmap from the subversion repository.
-http://nmap.org/book/install.html#inst-svn
-    EOS
-  end
 end
