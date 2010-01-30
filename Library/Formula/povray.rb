72f92c8d144293b30ce2d918c944f1c64f204de0
Jonathan Wright <quaggy@gmail.com>
2010-01-30 18:47:01 +1300

    New formula Povray 3.6.1
    The Persistence of Vision Raytracer is a high-quality, totally free
    tool for creating stunning three-dimensional graphics.
    
    Signed-off-by: David Höppner <0xffea@gmail.com>

---
+++ Library/Formula/povray.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Povray <Formula
+  url 'http://www.povray.org/ftp/pub/povray/Official/Unix/povray-3.6.1.tar.bz2'
+  homepage 'http://www.povray.org/'
+  md5 'b5789bb7eeaed0809c5c82d0efda571d'
+
+  def install
+    system "./configure", "COMPILED_BY=homebrew", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
