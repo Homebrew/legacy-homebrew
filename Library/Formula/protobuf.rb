9614446e38ba487812929d33dda3eac8860bdc7a
Adam Vandenberg <flangy@gmail.com>
2010-01-30 19:16:52 -0800

    protobuf 2.3.0

--- Library/Formula/protobuf.rb
+++ Library/Formula/protobuf.rb
@@ -1,12 +1,16 @@
 require 'formula'
  
 class Protobuf <Formula
-  @url='http://protobuf.googlecode.com/files/protobuf-2.2.0.tar.bz2'
-  @homepage='http://code.google.com/p/protobuf/'
-  @sha1='a0aff9df8dc93c5337553d586bbe726808d342a6'
+  url 'http://protobuf.googlecode.com/files/protobuf-2.3.0.tar.bz2'
+  sha1 'db0fbdc58be22a676335a37787178a4dfddf93c6'
+  homepage 'http://code.google.com/p/protobuf/'
  
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    ENV.gcc_4_2
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", 
+                          "--disable-dependency-tracking",
+                          "--with-zlib"
+    system "make"
     system "make install"
   end
 end
