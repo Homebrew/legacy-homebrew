38838aaf242265dcd7599b04773ea39312377df6
Mike Arthur <mike@mikearthur.co.uk>
2010-01-31 15:24:27 +0000

    Update Soprano to 2.3.70 and update dependencies.

--- Library/Formula/soprano.rb
+++ Library/Formula/soprano.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
 class Soprano <Formula
-  @url='http://downloads.sourceforge.net/project/soprano/Soprano/2.3.1/soprano-2.3.1.tar.bz2'
+  @url='http://downloads.sourceforge.net/project/soprano/Soprano/2.3.70/soprano-2.3.70.tar.bz2'
   @homepage='http://soprano.sourceforge.net/'
-  @md5='c9a2c008b80cd5d76599e9d48139dfe9'
+  @md5='de5cf230a95fc7218425aafdfb4a5e47'
 
   depends_on 'cmake'
+  depends_on 'd-bus'
   depends_on 'qt'
-  depends_on 'clucene'
   depends_on 'redland'
 
   def install
