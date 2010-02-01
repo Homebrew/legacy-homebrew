1bdf6146e204c522e3d1f6e34a2867e328defd27
Adam Vandenberg <flangy@gmail.com>
2010-02-01 12:18:30 -0800

    uwsgi 0.9.4

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -1,28 +1,25 @@
 require 'formula'
 
 class Uwsgi <Formula
-  url 'http://projects.unbit.it/downloads/uwsgi-0.9.3.tar.gz'
+  url 'http://projects.unbit.it/downloads/uwsgi-0.9.4.tar.gz'
   homepage 'http://projects.unbit.it/uwsgi/'
-  md5 'dd72040daea5a9ee982f3b3b98946ed9'
-
-  def python_archs
-    archs_for_command("python").collect{ |arch| "-arch #{arch}" }.join(' ')
-  end
+  md5 '07c633072b48c9790fa5d4030c7c9aa3'
 
   def python_version
     `python -c "import sys; print '%s.%s' % sys.version_info[:2]"`.chomp
   end
 
   def install
-    mv 'Makefile.OSX.ub.Py25', 'Makefile'
-
-    inreplace "Makefile" do |s|
-      s.gsub! "python2.5-config", "python#{ python_version }-config"
-      # The arch flags should match your Python's arch flags
-      s.gsub! "-arch ppc -arch i386", python_archs
+    case python_version
+    when '2.5'
+      makefile = "Makefile"
+      program = "uwsgi"
+    when '2.6'
+      makefile = "Makefile.Py26"
+      program = "uwsgi26"
     end
-
-    system "make all"
-    bin.install "uwsgi"
+    
+    system "make -f #{makefile}"
+    bin.install program
   end
 end
