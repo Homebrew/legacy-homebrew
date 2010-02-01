d2e3b324bd59f10272e2440dbe7e5f441c151ff9
Dan Peterson <dpiddy@gmail.com>
2010-01-31 23:22:11 -0400

    Add dnstracer
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/dnstracer.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Dnstracer <Formula
+  url 'http://www.mavetju.org/download/dnstracer-1.9.tar.gz'
+  homepage 'http://www.mavetju.org/unix/dnstracer.php'
+  md5 '7db73ce3070119c98049a617fe52ea84'
+
+  def install
+    ENV["LDFLAGS"] += " -lresolv"
+
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
