c9bdaa65bbfda89e6b0bae3c47bb26498eb4f0e8
Erik Kastner <kastner@gmail.com>
2010-02-03 09:51:57 -0500

    tesseract: adding about
    Signed-off-by: David Höppner <0xffea@gmail.com>

--- Library/Formula/tesseract.rb
+++ Library/Formula/tesseract.rb
@@ -13,6 +13,17 @@ class Tesseract <Formula
 
   depends_on 'libtiff'
 
+  def about
+    <<-EOF
+    Tesseract is an OCR (Optical Character Recognition) engine.
+
+    The easiest way to use it is to convert the source to a Grayscale tiff:
+    `convert source.png -type Grayscale terre_input.tif`
+    then run tesseract:
+    `tesseract terre_input.tif output`
+    EOF
+  end
+
   def install
     # Executable 'tesseract' segfaults on 10.6 when compiled with llvm-gcc (LLVM build 2206)
     ENV.gcc_4_2
