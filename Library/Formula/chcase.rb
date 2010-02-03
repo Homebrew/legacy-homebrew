e2679ee6d08230ecbde0ce8fbc76141ab3d1d2f6
Rick Fletcher <fletch@pobox.com>
2010-02-03 14:07:16 -0800

    Added formula chcase
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/chcase.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Chcase <ScriptFileFormula
+  version '2.0'
+  url 'http://www.primaledge.ca/chcase'
+  md5 '6d7e35fd597c60ba5ebbdd8d5e57eee7'
+  homepage 'http://www.primaledge.ca/chcase.html'
+
+  def patches
+    # add a shebang so that brew properly sets it executable
+    DATA
+  end
+end
+
+__END__
+diff --git a/chcase b/chcase
+index 689fc79..93efae8
+--- a/chcase
++++ b/chcase
+@@ -1,3 +1,4 @@
++#!/bin/sh -- # -*- perl -*-
+ eval 'exec perl $0 ${1+"$@"}'
+ if 0;
+ # don't modify below here
