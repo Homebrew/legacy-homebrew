7bba5cd559c1f1d46ad0adffd61b4595670a70ca
Rick Fletcher <fletch@pobox.com>
2010-02-03 12:08:27 -0800

    Added formula titlecase
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/titlecase.rb
@@ -0,0 +1,10 @@
+require 'formula'
+
+class Titlecase <Formula
+  head 'git://github.com/ap/titlecase.git'
+  homepage 'http://plasmasturm.org/code/titlecase/'
+
+  def install
+    bin.install "titlecase"
+  end
+end
