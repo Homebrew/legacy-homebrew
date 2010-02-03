ac91c03d28e15915675ad6c3c3f37d071cede894
Tim Harper <timcharper@gmail.com>
2010-02-03 15:23:41 -0700

    lzip formula
    Signed-off-by: David Höppner <0xffea@gmail.com>

---
+++ Library/Formula/lzip.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Lzip <Formula
+  url 'http://mirrors.igsobe.com/nongnu/lzip/lzip-1.9.tar.gz'
+  homepage 'http://www.nongnu.org/lzip/lzip.html'
+  md5 '9e1d7f4db5b953e9f75b8500ebe0d4c0'
+
+  def install
+    system "./configure"
+    system "make install"
+  end
+end
