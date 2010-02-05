ea098c74736ab8c7ee660da1e57d218622ee39b3
Micheil Smith <micheil@brandedcode.com>
2010-02-06 00:48:52 +1100

    Updated Formula: Node to latest version.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,13 +1,15 @@
 require 'formula'
 
 class Node <Formula
-  url 'http://s3.amazonaws.com/four.livejournal/20100120/node-v0.1.26.tar.gz'
+  url 'http://s3.amazonaws.com/four.livejournal/20100203/node-v0.1.27.tar.gz'
   head 'git://github.com/ry/node.git'
   homepage 'http://nodejs.org/'
-  md5 '8c65ce4bca6789d838401c29b41bcb58'
+  md5 '7dffe5ee6a6f531d711235d1227b0989'
 
   aka 'node.js'
-
+  
+  depends_on 'gnutls' => :recommended
+  
   def skip_clean? path
     # TODO: at some point someone should tweak this so it only skips clean
     # for the bits that break the build otherwise
