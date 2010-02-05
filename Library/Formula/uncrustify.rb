23342062ee7cf3ca3503afee50c02d18b7e3bacf
David Höppner <0xffea@gmail.com>
2010-02-05 01:48:38 +0100

    uncrustify: remove explicit version

--- Library/Formula/uncrustify.rb
+++ Library/Formula/uncrustify.rb
@@ -1,10 +1,9 @@
 require 'formula'
 
 class Uncrustify <Formula
-  url 'http://downloads.sourceforge.net/project/uncrustify/uncrustify/uncrustify-0.55/uncrustify-0.55.tar.gz?use_mirror=kent'
+  url 'http://downloads.sourceforge.net/project/uncrustify/uncrustify/uncrustify-0.55/uncrustify-0.55.tar.gz'
   homepage 'http://uncrustify.sourceforge.net/'
   md5 'ecf61c26d5206bbc81367468827b55ac'
-  version '0.55'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
