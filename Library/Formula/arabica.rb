57bc8d4f1355a74486f33d84b52f33bfb5ca935a
Ash Berlin <ash_github@firemirror.com>
2010-02-05 00:03:28 +0000

    new formula: arabica - XML processing toolkit
    Signed-off-by: David Höppner <0xffea@gmail.com>

---
+++ Library/Formula/arabica.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Arabica <Formula
+  homepage 'http://www.jezuk.co.uk/cgi-bin/view/arabica'
+  url 'http://github.com/ashb/Arabica/tarball/20100203'
+  version '20100203'
+  md5 '9318c4d498957cd356e533f2132d6956'
+
+  def install
+    system "autoreconf"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
