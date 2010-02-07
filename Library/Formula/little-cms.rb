2faf4bb984ada3b024dce14f7d4eab9b7e325891
Adam Vandenberg <flangy@gmail.com>
2010-02-07 13:37:46 -0800

    Update little cms to 1.19

--- Library/Formula/little-cms.rb
+++ Library/Formula/little-cms.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class LittleCms <Formula
-  url 'http://www.littlecms.com/lcms-1.18a.tar.gz'
+  url 'http://www.littlecms.com/lcms-1.19.tar.gz'
   homepage 'http://www.littlecms.com/'
-  md5 'f4abfe1c57ea3f633c2e9d034e74e3e8'
+  md5 '8af94611baf20d9646c7c2c285859818'
+  
+  aka 'lcms'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug"
