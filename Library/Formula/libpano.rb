997bf23085079222d38027552807a9712b659473
Adam Vandenberg <flangy@gmail.com>
2010-02-09 13:31:17 -0800

    libpano - add version and fix man location

--- Library/Formula/libpano.rb
+++ Library/Formula/libpano.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Libpano <Formula
   url 'http://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.17_beta1/libpano13-2.9.17_beta1.tar.gz'
+  version '13-2.9.17_beta1'
   homepage 'http://panotools.sourceforge.net/'
   md5 'd3708c6c29f6a19b69c74232853e73ae'
 
@@ -10,7 +11,8 @@ class Libpano <Formula
   depends_on 'libpng'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--mandir=#{man}"
     system "make install"
   end
 end
