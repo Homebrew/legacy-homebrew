6b06826ce2ca7165e0df520fa78624c5b1c34a04
yury <yury.korolev@gmail.com>
2010-02-10 00:09:22 +0300

    New formula: libpano (Panorama Tools)
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/libpano.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Libpano <Formula
+  url 'http://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.17_beta1/libpano13-2.9.17_beta1.tar.gz'
+  homepage 'http://panotools.sourceforge.net/'
+  md5 'd3708c6c29f6a19b69c74232853e73ae'
+
+  depends_on 'libjpeg'
+  depends_on 'libtiff'
+  depends_on 'libpng'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
