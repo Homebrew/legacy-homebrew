002de11d43a7709752372c3a3f06e6c94df892db
kashif <kashif@nomad-labs.com>
2010-02-10 11:57:56 +0100

    New formula: spatialite-tools version 2.4.0
    depends on libspatialite
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/spatialite-tools.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class SpatialiteTools <Formula
+  url 'http://www.gaia-gis.it/spatialite-2.4.0/spatialite-tools-2.4.0.tar.gz'
+  homepage 'http://www.gaia-gis.it/spatialite/'
+  md5 'ea508c0d7c7a58d4b75fe82ee62d8591'
+
+  depends_on 'libspatialite'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", 
+                          "--disable-debug",
+                          "--target=macosx"
+                          "--disable-dependency-tracking"
+    system "make install"
+  end
+end
