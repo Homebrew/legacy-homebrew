7bfeb4e6acf82de64f04be05261747bc8715fc2a
Lars Eggert <eggert@wlan-226.fit.nokia.com>
2010-02-11 10:56:14 +0200

    New formula: idcomments
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/idcomments.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Idcomments <Formula
+  url 'http://tools.ietf.org/tools/idcomments/idcomments-0.18.tgz'
+  homepage 'http://tools.ietf.org/tools/idcomments/'
+  md5 '6ceb271af37754657c7f81b3dba334de'
+
+  def install
+    bin.install "idcomments"
+    doc.install %w(about changelog copyright todo)
+  end
+end
