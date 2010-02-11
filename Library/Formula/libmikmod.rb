2a524770af18f469fbe4273cc6354e49b37448cd
Adam Vandenberg <flangy@gmail.com>
2010-02-11 11:54:21 -0800

    Fix some more man paths.

--- Library/Formula/libmikmod.rb
+++ Library/Formula/libmikmod.rb
@@ -8,7 +8,8 @@ class Libmikmod <Formula
   aka :mikmod
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--mandir=#{man}"
     system "make install"
   end
 end
