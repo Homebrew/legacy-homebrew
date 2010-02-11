5184e1c2b0110c51d13f6130c66c7ca3bfa3fea2
Adam Vandenberg <flangy@gmail.com>
2010-02-11 14:50:16 -0800

    Update Aria2 to 1.8.2; use GCC.

--- Library/Formula/aria2.rb
+++ Library/Formula/aria2.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Aria2 <Formula
-  @url='http://downloads.sourceforge.net/project/aria2/stable/latest/aria2-1.8.0.tar.bz2'
-  @homepage='http://aria2.sourceforge.net/'
-  @md5='13944c95529de6846f0ab9ba09c53e3e'
+  url 'http://downloads.sourceforge.net/project/aria2/stable/latest/aria2-1.8.2.tar.bz2'
+  md5 'ec72262dbe4b4091dfe29d08f8e4d097'
+  homepage 'http://aria2.sourceforge.net/'
 
   def install
+    ENV.gcc_4_2 # 1.8.2 didn't work w/ LLVM on 10.6
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
