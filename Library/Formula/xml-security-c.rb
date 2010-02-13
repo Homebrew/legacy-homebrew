1368ea9fdb7e18797000a56a5aa407686bc80831
David Höppner <0xffea@gmail.com>
2010-02-13 19:16:23 +0100

    New formula XML Security
    The XML Security project is aimed at providing implementation of
    security standards for XML.

---
+++ Library/Formula/xml-security-c.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class XmlSecurityC <Formula
+  url 'http://santuario.apache.org/dist/c-library/xml-security-c-1.5.1.tar.gz'
+  homepage 'http://santuario.apache.org/'
+  md5 '2c47c4ec12e8d6abe967aa5e5e99000c'
+
+  depends_on 'xerces-c'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
