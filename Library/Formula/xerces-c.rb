1b7b5c087b9c5ed1d8177f44a439a71ef55dd432
David Höppner <0xffea@gmail.com>
2010-02-13 19:10:25 +0100

    xerces-c: update version to 3.1.0

--- Library/Formula/xerces-c.rb
+++ Library/Formula/xerces-c.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class XercesC <Formula
-  @url='http://www.osnt.org/apache/xerces/c/3/sources/xerces-c-3.0.1.tar.gz'
-  @homepage='http://xerces.apache.org/xerces-c/'
-  @md5='293c03f59bf8e956020d73f5b122094c'
+  url 'http://apache.copahost.com/xerces/c/3/sources/xerces-c-3.1.0.tar.gz'
+  homepage 'http://xerces.apache.org/xerces-c/'
+  md5 '4dc9460e011ed4857dcd290f22dd6f1b'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
