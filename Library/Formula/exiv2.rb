f841047db9b62a4c843240567dacfb36d8ced2f1
Aleksandar Topuzovic <aleksandar.topuzovic@gmail.com>
2010-02-15 23:01:29 +0100

    New formula exiv2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/exiv2.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Exiv2 <Formula
+  url 'http://www.exiv2.org/exiv2-0.19.tar.gz'
+  homepage 'http://www.exiv2.org'
+  md5 'f52fb75a2cb7512f1484deab76473e13'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
