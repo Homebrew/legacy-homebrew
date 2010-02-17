43cf2d1cebd5eb61e943e6d6eaf07a288fd4f563
Joshua Peek <josh@joshpeek.com>
2010-02-17 17:43:48 -0600

    Upgrade hub to v0.3.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Hub <Formula
-  url 'http://github.com/defunkt/hub/tarball/v0.3.0'
+  url 'http://github.com/defunkt/hub/tarball/v0.3.2'
   homepage 'http://github.com/defunkt/hub'
-  md5 'f77c39cc69c5e9e909608a7580b0feaf'
+  md5 'e87f8e0c28232acbec75cb7645548def'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
