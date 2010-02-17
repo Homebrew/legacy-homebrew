a6cede5e7f1264ca450ad6f03a854582a8aa9b7e
Martin Kuehl <martin.kuehl@gmail.com>
2010-02-17 15:10:26 +0100

    Updated Formula: global 5.8
    Also remove erroneous call to `Pathname#rm`.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/global.rb
+++ Library/Formula/global.rb
@@ -1,15 +1,17 @@
 require 'formula'
 
 class Global <Formula
-  @url='http://ftp.gnu.org/gnu/global/global-5.6.tar.gz'
+  @url='http://tamacom.com/global/global-5.8.tar.gz'
   @homepage='http://www.gnu.org/software/global/'
-  @md5='cc1f79cb4f62ab4b4c8b5e8a68c51f5e'
+  @md5='7ba2efb55269615b2722cca36aced2cb'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
     
     # we copy these in already
-    (share+'gtags').rm %w[README COPYING LICENSE INSTALL ChangeLog AUTHORS]
+    Dir.chdir(share+'gtags') do
+      FileUtils.rm %w[README COPYING LICENSE INSTALL ChangeLog AUTHORS]
+    end
   end
 end
