ca19912e0b5a8fa9327fc0f0f9b044f7a4e1cc68
Adam Vandenberg <flangy@gmail.com>
2010-02-19 09:48:24 -0800

    Update GraphViz to 2.26.3

--- Library/Formula/graphviz.rb
+++ Library/Formula/graphviz.rb
@@ -1,12 +1,15 @@
 require 'formula'
 
 class Graphviz <Formula
-  url 'http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.24.0.tar.gz'
+  url 'http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.26.3.tar.gz'
+  md5 '6f45946fa622770c45609778c0a982ee'
   homepage 'http://graphviz.org/'
-  md5 '806a30dbc3f8deb219216f35be0e7a40'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-qartz", "--disable-python", "--disable-perl"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-qartz",
+                          "--disable-python", "--disable-perl"
     system "make install"
   end
 end
