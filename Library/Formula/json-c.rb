3ee2969ede6c036515e4373cd0a557fae738f7ef
Michael Stephens <me@mikej.st>
2010-02-20 09:23:03 -0500

    json-c 0.9
    A JSON implementation in C.
    
    Signed-off-by: David Höppner <0xffea@gmail.com>

---
+++ Library/Formula/json-c.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class JsonC <Formula
+  url 'http://oss.metaparadigm.com/json-c/json-c-0.9.tar.gz'
+  homepage 'http://oss.metaparadigm.com/json-c/'
+  md5 '3a13d264528dcbaf3931b0cede24abae'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
