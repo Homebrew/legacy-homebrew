0634c1c7c3049558051203c6d383c59484b1577b
Max Howell <max@methylblue.com>
2010-02-20 11:53:51 +0000

    ENV['CXX'] is no longer set, use ENV.cxx

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -9,11 +9,8 @@ class Parrot <Formula
   depends_on 'pcre'
 
   def install
-    confargs = %W[--prefix=#{prefix} --debugging=0 --without-opengl]
-    # the arguments Parrot tries to pass to gcc-4.2 on 10.5 don't compile
-    confargs << "--cc=#{ENV['CC']}" if MACOS_VERSION >= 10.6
-
-    system "perl", "Configure.pl", *confargs
+    system "perl", "Configure.pl", "--prefix=#{prefix}", "--debugging=0",
+                                   "--without-opengl", "--cc=#{ENV.cc}"
     system "make"
     system "make install"
 
