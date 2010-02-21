cc67c9eb423d83057e4f826324eff55e3a1f5192
Keenan Brock <keenan@thebrocks.net>
2010-02-21 04:45:50 -0500

    ogmtools now creates man pages in correct directory ogmtools now builds dvdxchap (to extract dvd chapter information) added libdvdread dependency for dvdxchap
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/libdvdread.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Libdvdread <Formula
+  url 'http://www.dtek.chalmers.se/groups/dvd/dist/libdvdread-0.9.7.tar.gz'
+  homepage 'http://www.dtek.chalmers.se/groups/dvd/'
+  md5 '078788c9241ae16763529e1235502337'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--mandir=#{man}"
+    system "make install"
+  end
+end
