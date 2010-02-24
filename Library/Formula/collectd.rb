76a9a58e183a5d28ae08c99f7b6b886a5c4cbe54
Adam Vandenberg <flangy@gmail.com>
2010-02-23 22:30:36 -0800

    Update collectd to not strip symbols.

--- Library/Formula/collectd.rb
+++ Library/Formula/collectd.rb
@@ -4,6 +4,10 @@ class Collectd <Formula
   url 'http://collectd.org/files/collectd-4.9.1.tar.bz2'
   homepage 'http://collectd.org/'
   md5 '5753496651c8c84afaea1fe290876bfc'
+  
+  def skip_clean? path
+    true
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
