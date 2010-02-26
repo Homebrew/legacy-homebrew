5e9d73ba5509e6c2517e21871cfa0e771a981111
mystix <xerokill@gmail.com>
2010-02-27 00:44:49 +0800

    Updated Groovy formula to 1.7.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    I went ahead and dropped the @s and reordered the delete
    to happen before install.
    
    Also, the "version" was redundant.

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -1,13 +1,12 @@
 require 'formula'
 
 class Groovy <Formula
-  @url='http://dist.groovy.codehaus.org/distributions/groovy-binary-1.7.0.zip'
-  @homepage='http://groovy.codehaus.org/'
-  @version='1.7.0'
-  @sha256='7f08ce19c757ddf8b14f5dcf909048719c82348584aa707bbe0a65a9dd9c6afc'
+  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-1.7.1.zip'
+  md5 '65f522de612ca56e815992fffbf10d12'
+  homepage 'http://groovy.codehaus.org/'
 
   def install
+    rm_f Dir["bin/*.bat"]
     prefix.install %w[bin conf lib]
-    FileUtils.rm_f Dir["#{bin}/*.bat"]
   end
 end
