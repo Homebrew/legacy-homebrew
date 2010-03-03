48a51fa55ec940427dac5fc364296fbe05ccc2bf
Mike Arthur <mike@mikearthur.co.uk>
2010-03-03 00:03:40 +0000

    Update Oxygen icons to 4.4.1.

--- Library/Formula/oxygen-icons.rb
+++ Library/Formula/oxygen-icons.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class OxygenIcons <Formula
-  url 'ftp://ftp.kde.org/pub/kde/stable/4.4.0/src/oxygen-icons-4.4.0.tar.bz2'
+  url 'ftp://ftp.kde.org/pub/kde/stable/4.4.1/src/oxygen-icons-4.4.1.tar.bz2'
   homepage 'http://www.oxygen-icons.org/'
-  md5 'fbcd429cc822cb88a815d97a4e66be4d'
+  md5 '9e91b94f2e743d5dc0bd740ed0acb025'
 
   depends_on 'cmake'
 
