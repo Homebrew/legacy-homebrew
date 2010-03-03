50454060f58425efde1f49fbe63dba55b5909eae
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2010-03-03 16:57:19 -0500

    add trash folder patch and hcache support to mutt
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mutt.rb
+++ Library/Formula/mutt.rb
@@ -4,17 +4,25 @@ class Mutt <Formula
   @url='ftp://ftp.mutt.org/mutt/devel/mutt-1.5.20.tar.gz'
   @homepage='http://www.mutt.org/'
   @md5='027cdd9959203de0c3c64149a7ee351c'
+  
+  depends_on 'tokyo-cabinet'
 
   def options
     [
-      ['--sidebar-patch', "Apply sidebar (folder list) patch"]
+      ['--sidebar-patch', "Apply sidebar (folder list) patch"],
+      ['--trash-patch', "Apply trash folder patch"]
     ]
   end
 
   def patches
+    patches = []
     if ARGV.include? '--sidebar-patch'
-      'http://lunar-linux.org/~tchan/mutt/patch-1.5.20.sidebar.20090619.txt'
+      patches << 'http://lunar-linux.org/~tchan/mutt/patch-1.5.20.sidebar.20090619.txt'
     end
+    if ARGV.include? '--trash-patch'
+      patches << 'http://trac.macports.org/raw-attachment/ticket/20412/patch-1.5.20.cd.trash_folder.diff'
+    end
+    patches
   end
 
   def install
@@ -30,6 +38,7 @@ class Mutt <Formula
           "--enable-imap",
           "--enable-smtp",
           "--enable-pop",
+          "--enable-hcache",
 
           # This is just a trick to keep 'make install' from trying to chgrp
           # the mutt_dotlock file (which we can't do if we're running as an
