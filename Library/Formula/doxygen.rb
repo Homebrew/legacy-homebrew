6d7f04987028b00b4b3b050e3bf2031232ca66e1
David Höppner <0xffea@gmail.com>
2010-03-04 18:40:27 +0100

    doxygen: update version (Austin Ziegler)

--- Library/Formula/doxygen.rb
+++ Library/Formula/doxygen.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class Doxygen <Formula
-  url 'http://ftp.stack.nl/pub/users/dimitri/doxygen-1.6.2.src.tar.gz'
+  url 'http://ftp.stack.nl/pub/users/dimitri/doxygen-1.6.3.src.tar.gz'
   homepage 'http://www.doxygen.org/'
-  md5 '70260101ef60952cb99484700241c99e'
+  md5 '2d6ea20a9d850d94321cee78bab7bb87'
 
   def install
     system "./configure", "--prefix", "#{prefix}"
     inreplace "Makefile" do |s|
       # Path of man1 relative to already given prefix
-      s.change_make_var! 'MAN1DIR', 'share/man/man1'
+      s.change_make_var! 'MAN1DIR', man1
     end
     system "make"
     system "make install"
