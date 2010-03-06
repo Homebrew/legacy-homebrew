83cc5851a2af7669859ce139b2dd87c9aa4f6ea2
Chris Wanstrath <chris@ozmm.org>
2010-03-05 18:57:36 -0800

    hub 1.0.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Hub <Formula
-  url 'http://github.com/defunkt/hub/tarball/v0.3.2'
+  url 'http://github.com/defunkt/hub/tarball/v1.0.1'
   homepage 'http://github.com/defunkt/hub'
-  md5 'e87f8e0c28232acbec75cb7645548def'
+  md5 'c838f7419173cc646a5b355307c5c1c7'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
