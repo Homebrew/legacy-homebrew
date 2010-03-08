96a495d2e198ed86812533eb15ccd100eb9d0483
Adam Vandenberg <flangy@gmail.com>
2010-03-07 20:46:36 -0800

    hub 1.0.2

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Hub <Formula
-  url 'http://github.com/defunkt/hub/tarball/v1.0.1'
+  url 'http://github.com/defunkt/hub/tarball/v1.0.2'
   homepage 'http://github.com/defunkt/hub'
-  md5 'c838f7419173cc646a5b355307c5c1c7'
+  md5 '4e31316783e53ba200580d6557bb9314'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
