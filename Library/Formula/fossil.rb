309ad25df9132afd7554da48b839c49a987a7516
Martin Kühl <martin.kuehl@gmail.com>
2010-03-12 12:31:01 +0100

    Updated formula: fossil 20100308141844
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/fossil.rb
+++ Library/Formula/fossil.rb
@@ -1,10 +1,9 @@
 require 'formula'
 
 class Fossil <Formula
-	
-  @url='http://www.fossil-scm.org/download/fossil-src-20091220213451.tar.gz'
-  @homepage='http://www.fossil-scm.org/'
-  @md5='152d94b378eacc1d8151ec3385800ac2'
+  url 'http://www.fossil-scm.org/download/fossil-src-20100308141844.tar.gz'
+  homepage 'http://www.fossil-scm.org/'
+  md5 'ba555a2bb337bf7548169e39b4c84dab'
 
   def install
     system "make"
