c308413af8f8fce07bc3651d36bf79556a40d2c0
Adam Vandenberg <flangy@gmail.com>
2010-03-13 09:40:22 -0800

    Homepage for swftools was wrong, wrong, wrong.

--- Library/Formula/swftools.rb
+++ Library/Formula/swftools.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Swftools <Formula
   url 'http://www.swftools.org/swftools-0.9.0.tar.gz'
-  homepage 'http://www.swftools.com'
+  homepage 'http://www.swftools.org'
   md5 '946e7c692301a332745d29140bc74e55'
 
   depends_on 'jpeg'
