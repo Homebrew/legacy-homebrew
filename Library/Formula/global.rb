df4e6b8b34f1745ebfb6c6e67b3236af76d9d730
Adam Vandenberg <flangy@gmail.com>
2010-03-16 11:39:18 -0700

    Update global to 5.8.1

--- Library/Formula/global.rb
+++ Library/Formula/global.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Global <Formula
-  url 'http://tamacom.com/global/global-5.8.tar.gz'
+  url 'http://tamacom.com/global/global-5.8.1.tar.gz'
   homepage 'http://www.gnu.org/software/global/'
-  md5 'f6015f420614dd02082abecd49a4d666'
+  md5 '9c357098e42c9ba32776ccd6b549d85d'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
