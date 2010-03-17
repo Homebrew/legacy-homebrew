b56fafbc8ad69f224b37b831f4727fdf8dc6d2da
Adam Vandenberg <flangy@gmail.com>
2010-03-16 22:33:13 -0700

    Split out some Formula tests.

--- Library/Homebrew/test/test_bucket.rb
+++ Library/Homebrew/test/test_bucket.rb
@@ -15,11 +15,6 @@ class MockFormula <Formula
   end
 end
 
-class MostlyAbstractFormula <Formula
-  @url=''
-  @homepage = 'http://example.com/'
-end
-
 class TestZip <Formula
   def initialize
     zip=HOMEBREW_CACHE.parent+'test-0.1.zip'
@@ -27,14 +22,6 @@ class TestZip <Formula
     @url="file://#{zip}"
     @homepage = 'http://example.com/'
     super 'testzip'
-  end
-end
-
-class TestBallOverrideBrew <Formula
-  def initialize
-    super "foo"
-  end
-  def brew
   end
 end
 
@@ -52,15 +39,6 @@ class BeerTasting < Test::Unit::TestCase
     end
   end
 
-  def test_prefix
-    nostdout do
-      TestBall.new.brew do |f|
-        assert_equal File.expand_path(f.prefix), (HOMEBREW_CELLAR+f.name+'0.1').to_s
-        assert_kind_of Pathname, f.prefix
-      end
-    end
-  end
-  
   FOOBAR='foo-bar'
   def test_formula_funcs
     classname=Formula.class_s(FOOBAR)
@@ -87,17 +65,6 @@ class BeerTasting < Test::Unit::TestCase
     assert_not_nil Formula.factory(FOOBAR)
   end
   
-  def test_cant_override_brew
-    assert_raises(RuntimeError) { TestBallOverrideBrew.new }
-  end
-  
-  def test_abstract_formula
-    f=MostlyAbstractFormula.new
-    assert_equal '__UNKNOWN__', f.name
-    assert_raises(RuntimeError) { f.prefix }
-    nostdout { assert_raises(RuntimeError) { f.brew } }
-  end
-
   def test_zip
     nostdout { assert_nothing_raised { TestZip.new.brew {} } }
   end
@@ -209,14 +176,6 @@ class BeerTasting < Test::Unit::TestCase
     assert_equal '0.1', foo1.version
   end
   
-  def test_class_naming
-    assert_equal 'ShellFm', Formula.class_s('shell.fm')
-    assert_equal 'Fooxx', Formula.class_s('foo++')
-    assert_equal 'SLang', Formula.class_s('s-lang')
-    assert_equal 'PkgConfig', Formula.class_s('pkg-config')
-    assert_equal 'FooBar', Formula.class_s('foo_bar')
-  end
-      
   def test_ENV_options
     require 'extend/ENV'
     ENV.extend(HomebrewEnvExtension)
