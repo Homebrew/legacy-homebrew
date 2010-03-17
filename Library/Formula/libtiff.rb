c8592e2788944700ed8296ed0d2ce8bb276484ef
Adam Vandenberg <flangy@gmail.com>
2010-03-17 09:54:37 -0700

    Updated homepage and download for libtiff.

--- Library/Formula/libtiff.rb
+++ Library/Formula/libtiff.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libtiff <Formula
-  @url='ftp://ftp.remotesensing.org/libtiff/tiff-3.9.2.tar.gz'
-  @homepage='http://www.libtiff.org/'
-  @md5='93e56e421679c591de7552db13384cb8'
+  url 'http://download.osgeo.org/libtiff/tiff-3.9.2.tar.gz'
+  homepage 'http://www.remotesensing.org/libtiff/'
+  md5 '93e56e421679c591de7552db13384cb8'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--mandir=#{man}"
