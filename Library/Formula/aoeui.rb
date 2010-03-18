abd78ea252c8775a7ff1796a25ce1fcbe2e25a8c
Martin Kühl <martin.kuehl@gmail.com>
2010-03-18 09:06:36 +0100

    Fix HEAD support for aoeui
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/aoeui.rb
+++ Library/Formula/aoeui.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Aoeui <Formula
   url 'http://aoeui.googlecode.com/files/aoeui-1.4.tgz'
-  head 'svn://http://aoeui.googlecode.com/svn/trunk/'
+  head 'http://aoeui.googlecode.com/svn/trunk/'
   homepage 'http://aoeui.googlecode.com/'
   md5 '8df4615fdf854838fe6c8ca773d0a6ea'
 
