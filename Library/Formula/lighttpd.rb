a2ca5777cfbcd1d1656a33eb632c780d98b30d25
kret <andreskee@gmail.com>
2010-03-21 18:43:23 +0100

    Updated lighttpd to version 1.4.26.
    Signed-off-by: David Höppner <0xffea@gmail.com>

--- Library/Formula/lighttpd.rb
+++ Library/Formula/lighttpd.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Lighttpd <Formula
-  url 'http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.25.tar.bz2'
-  md5 '2027c49fb46530e45338c5e2da13c02f'
+  url 'http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.26.tar.bz2'
+  md5 'a682c8efce47a2f4263a247ba0813c9b'
   homepage 'http://www.lighttpd.net/'
 
   depends_on 'pkg-config'
