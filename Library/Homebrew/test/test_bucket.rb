7700d03550ea9cbea693e36ff38bc5d4400fa6c2
Adam Vandenberg <flangy@gmail.com>
2010-03-20 23:13:25 -0700

    Move ENV tests to separate file.

--- Library/Homebrew/test/test_bucket.rb
+++ Library/Homebrew/test/test_bucket.rb
@@ -175,20 +175,4 @@ class BeerTasting < Test::Unit::TestCase
     assert_equal 'foo-0.1', foo1.stem
     assert_equal '0.1', foo1.version
   end
-  
-  def test_ENV_options
-    require 'extend/ENV'
-    ENV.extend(HomebrewEnvExtension)
-
-    ENV.gcc_4_0
-    ENV.gcc_4_2
-    ENV.O3
-    ENV.minimal_optimization
-    ENV.no_optimization
-    ENV.libxml2
-    ENV.x11
-    ENV.enable_warnings
-    assert !ENV.cc.empty?
-    assert !ENV.cxx.empty?
-  end
 end
