015cdf2b413fa65a92ffee2522a2fd785284f1d4
Barry Mitchelson <barry.mitchelson@gmail.com>
2010-03-22 21:19:12 +0000

    Ugraded tmux to version 1.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class Tmux <Formula
-  url 'http://downloads.sourceforge.net/tmux/tmux-1.1.tar.gz'
-  md5 'faf2fc52ac3ae63d899f6fece2c112cd'
+  url 'http://downloads.sourceforge.net/tmux/tmux-1.2.tar.gz'
+  md5 '748fbe7bb5f86812e19bd6005ff21a5a'
   homepage 'http://tmux.sourceforge.net'
+
+  depends_on 'libevent'
 
   def install
     ENV['PREFIX'] = "#{prefix}"
