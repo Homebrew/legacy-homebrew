0c3cb507548d3bd91cd88a21abda6079a4c20b0a
Adam Vandenberg <flangy@gmail.com>
2010-03-25 16:13:50 -0700

    Update protobuf-c to 0.13; need -j1 (on Mac Pro).

--- Library/Formula/protobuf-c.rb
+++ Library/Formula/protobuf-c.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class ProtobufC <Formula
-  url 'http://protobuf-c.googlecode.com/files/protobuf-c-0.12.tar.gz'
+  url 'http://protobuf-c.googlecode.com/files/protobuf-c-0.13.tar.gz'
   homepage 'http://code.google.com/p/protobuf-c/'
-  md5 'fefe81642f1e5565eb8a661e597b5bf7'
+  sha1 '3144f260b736855cea2984731c2ba1647d40dfe0'
 
   depends_on 'protobuf'
 
   def install
+    ENV.j1
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
