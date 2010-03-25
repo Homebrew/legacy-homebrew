78b7f308ef60832467b7ec51cf92b63b917f1042
Augie Fackler <durin42@gmail.com>
2010-03-24 22:17:25 -0500

    Update pango to latest version.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    * Updated it further still.

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Pango <Formula
-  url 'http://ftp.gnome.org/pub/gnome/sources/pango/1.26/pango-1.26.2.tar.bz2'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/pango/1.28/pango-1.28.0.tar.bz2'
   homepage 'http://www.pango.org/'
-  md5 'f30212b8833af3ce5c82121dc309e3d0'
+  sha256 '68480485b714e3570a58c270add9e9785fa78068f7410949b478e8a9d3f5bc40'
 
-  depends_on 'glib'
   depends_on 'pkg-config'
+  depends_on 'glib'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--with-x"
