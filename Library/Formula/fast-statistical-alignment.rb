f0072d9a1bc4f3fbc806b2db35258ef311ef5cef
Ben Blackburne <bpb@lysozyme.net>
2010-03-26 16:31:17 +0000

    FSA: Fast Statistical Alignment
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/fast-statistical-alignment.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class FastStatisticalAlignment <Formula
+  url 'http://downloads.sourceforge.net/project/fsa/fsa-1.15.3.tar.gz'
+  homepage 'http://fsa.sourceforge.net/'
+  md5 '9c03a6b48c3a276f6233dc5715ed3d0a'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
