8ac083027694ef673efef568d29cc20df1ab7ac8
Johan Oskarsson <johan@oskarsson.nu>
2010-03-27 15:47:49 +0100

    Added support for Thrift 0.2.0

--- Library/Formula/thrift.rb
+++ Library/Formula/thrift.rb
@@ -7,12 +7,15 @@ require 'formula'
 class Thrift <Formula
   homepage 'http://incubator.apache.org/thrift/'
   head 'http://svn.apache.org/repos/asf/incubator/thrift/trunk'
+  version '0.2.0'
+  url 'http://apache.dataphone.se/incubator/thrift/0.2.0-incubating/thrift-0.2.0-incubating.tar.gz'
+  md5 '9958c57c402c02171ba0bcc96183505c'
 
   depends_on 'boost'
   
   def install
     FileUtils.cp "/usr/X11/share/aclocal/pkg.m4", "aclocal"
-    system "./bootstrap.sh"
+    system "./bootstrap.sh" if version == 'HEAD'
     system "./configure","--disable-debug","--without-java",
                          "--prefix=#{prefix}","--libdir=#{lib}",
                          # rationale: this can be installed with easy_install
