e37f4354d3eb097cd004919959d40a2de606f792
David Höppner <0xffea@gmail.com>
2010-03-31 20:56:01 +0200

    New formula kes
    Kes is a fork of the es shell created by Paul Haahr and Byron Rakitzis. It is
    based on the rc command interpreter, part of the Plan 9 operating system.

---
+++ Library/Formula/kes.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Kes <Formula
+  head 'git://github.com/epilnivek/kes.git'
+  homepage 'http://github.com/epilnivek/kes'
+
+  def install
+    system "make"
+    bin.install 'es'
+    man1.install 'doc/es.1'
+  end
+end
