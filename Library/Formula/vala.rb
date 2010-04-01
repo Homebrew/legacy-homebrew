c5c38c10db94252c32849d52ba924034887d26be
Adam Vandenberg <flangy@gmail.com>
2010-04-01 09:58:37 -0700

    Update to vala 0.8.0

--- Library/Formula/vala.rb
+++ Library/Formula/vala.rb
@@ -2,15 +2,17 @@ require 'formula'
 
 class Vala <Formula
   head 'git://git.gnome.org/vala'
-  url 'http://download.gnome.org/sources/vala/0.7/vala-0.7.10.tar.bz2'
+  url 'http://download.gnome.org/sources/vala/0.8/vala-0.8.0.tar.bz2'
   homepage 'http://live.gnome.org/Vala'
-  md5 '6e807644f500d6605a5ee4e1612dd187'
+  md5 '36d7970325293059e05e21283f9d3ab2'
 
-  depends_on 'glib'
+  depends_on 'pkg-config'
   depends_on 'gettext'
+  depends_on 'glib'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make" # Single step fails to compile for 0.8.0
     system "make install"
   end
 end
