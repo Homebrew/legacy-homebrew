490158741db5dc92d0f451e52fd1c28acb0c781f
Luke Amdor <luke.amdor@gmail.com>
2010-04-04 20:02:05 -0500

    Updated sbt to 0.7.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Sbt <Formula
-  JAR = 'sbt-launch-0.7.0.jar'
+  JAR = 'sbt-launch-0.7.2.jar'
   url "http://simple-build-tool.googlecode.com/files/#{JAR}"
   homepage 'http://code.google.com/p/simple-build-tool'
-  md5 '19275aa32369683c6a92de9394da822b'
+  md5 '9d02c8d4720e38492262a8b0266b077e'
 
   def install
     (bin+'sbt').write <<-EOS
