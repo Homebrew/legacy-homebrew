087cea41422b814cb0ad675c588c723c53816ebc
Adam Vandenberg <flangy@gmail.com>
2010-04-06 12:14:40 -0700

    Update formulas to use :using.

--- Library/Formula/osm2pgsql.rb
+++ Library/Formula/osm2pgsql.rb
@@ -2,16 +2,11 @@ require 'formula'
 require 'date'
 
 class Osm2pgsql <Formula
-  head 'http://svn.openstreetmap.org/applications/utils/export/osm2pgsql/'
+  head 'http://svn.openstreetmap.org/applications/utils/export/osm2pgsql/', :using => :svn
   homepage 'http://wiki.openstreetmap.org/wiki/Osm2pgsql'
 
   def install
     system "make"
     bin.install "osm2pgsql"
   end
-  
-  def download_strategy
-    SubversionDownloadStrategy
-  end
-  
 end
