d33b2644c555af1ff4beca581d89222973d7742c
Zachary Voase <zacharyvoase@me.com>
2010-04-06 14:37:32 +0000

    Added formula for HTML-XML-utils.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/html-xml-utils.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class HtmlXmlUtils <Formula
+  url 'http://www.w3.org/Tools/HTML-XML-utils/html-xml-utils-5.5.tar.gz'
+  homepage 'http://www.w3.org/Tools/HTML-XML-utils/'
+  md5 '28c58add86e35a60e602a029c8e2f04b'
+
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
