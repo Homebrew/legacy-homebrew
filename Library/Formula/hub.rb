10d2bbf52ffc6c669909fdd74e26e7fb26b373b9
Adam Vandenberg <flangy@gmail.com>
2010-04-07 10:42:05 -0700

    Update hub to 1.1.0

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Hub <Formula
-  url 'http://github.com/defunkt/hub/tarball/v1.0.2'
+  url 'http://github.com/defunkt/hub/raw/gh-pages/standalone'
   homepage 'http://github.com/defunkt/hub'
-  md5 '4e31316783e53ba200580d6557bb9314'
+  version '1.1.0'
+  md5 '4f5e4dec0663fb33aa3e8e46edbfce5a'
 
   def install
-    system "rake", "install", "prefix=#{prefix}"
+    bin.install 'standalone' => 'hub'
   end
 end
