06203ba908d7f25d0abfc47d0ab7df740b59e806
Adam Vandenberg <flangy@gmail.com>
2010-04-06 22:41:51 -0700

    Update resty formula.

--- Library/Formula/resty.rb
+++ Library/Formula/resty.rb
@@ -1,14 +1,15 @@
 require 'formula'
 
-# resty is a bash wrapper around curl for testing HTTP/REST APIs.
-
 class Resty <Formula
   head 'git://github.com/micha/resty.git'
   homepage 'http://github.com/micha/resty'
 
   def install
     system "mv README.markdown README"
-    # Note: pp depends on perl JSON module, which we don't install for you.
-    bin.install %w[pp resty]
+    bin.install %w[pp resty pypp]
+  end
+
+  def caveats
+    "The Perl printy-printer (pp) depends on JSON from CPAN."
   end
 end
