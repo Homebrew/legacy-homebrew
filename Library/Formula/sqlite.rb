8dd2d463502e37a692d69eb8c314cf751e4884d6
Barry Sears <barry.sears@gmail.com>
2010-04-07 22:41:07 -0400

    Modified sqlite formula. Added option for R*-Tree module
    Signed-off-by: David Höppner <0xffea@gmail.com>
    
    * white space

--- Library/Formula/sqlite.rb
+++ Library/Formula/sqlite.rb
@@ -5,7 +5,14 @@ class Sqlite <Formula
   md5 '8f1e86b3909a27f8122b0981afd16fcd'
   homepage 'http://www.sqlite.org/'
 
+  def options
+    [
+      ["--with-rtree", "Enables the R*Tree index module"]
+    ]
+  end
+
   def install
+    ENV.append "CFLAGS", "-DSQLITE_ENABLE_RTREE=1" if ARGV.include? "--with-rtree"
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking"
     system "make install"
