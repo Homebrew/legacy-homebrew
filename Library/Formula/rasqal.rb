027c1276a3cf6f1a409013d7f9fd6c4fe1b7a808
David Höppner <0xffea@gmail.com>
2010-04-09 17:37:11 +0200

    rasqal: version update to 0.9.19

--- Library/Formula/rasqal.rb
+++ Library/Formula/rasqal.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Rasqal <Formula
-  url 'http://download.librdf.org/source/rasqal-0.9.17.tar.gz'
+  url 'http://download.librdf.org/source/rasqal-0.9.19.tar.gz'
   homepage 'http://librdf.org/rasqal/'
-  md5 'ee65a1c55da27d95f50d1e667b98d1d0'
+  md5 '9bc1b40ffe1bdc794887d845d153bd4e'
 
   depends_on 'raptor'
 
