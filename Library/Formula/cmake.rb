9f586170baa1bf51b4c1318c4835f590ad568cea
David Höppner <0xffea@gmail.com>
2010-04-09 11:04:47 +0200

    cmake: update version to 2.8.1

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Cmake <Formula
-  url 'http://www.cmake.org/files/v2.8/cmake-2.8.0.tar.gz'
-  md5 '3b3679b8a6afaedc38a8c15dd7ff4fcf'
+  url 'http://www.cmake.org/files/v2.8/cmake-2.8.1.tar.gz'
+  md5 'feadc2e5ebbfed0efc90178583503725'
   homepage 'http://www.cmake.org/'
 
   def install
