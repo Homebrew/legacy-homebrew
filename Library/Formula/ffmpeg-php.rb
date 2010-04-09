00233f0abe4300f3fbb54859e854b9424602167a
Bob Fanger <bfanger@gmail.com>
2010-04-09 21:08:03 +0200

    ffmpeg-php - a php extension for handling movie and audio files
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    * Reformatted

---
+++ Library/Formula/ffmpeg-php.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class FfmpegPhp <Formula
+  homepage 'http://ffmpeg-php.sourceforge.net'
+  head 'http://ffmpeg-php.svn.sourceforge.net/svnroot/ffmpeg-php/trunk/ffmpeg-php'
+
+  depends_on 'ffmpeg'
+
+  def install
+    system "phpize"
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--with-ffmpeg=#{HOMEBREW_PREFIX}"
+    system "make"
+    prefix.install 'modules/ffmpeg.so'    
+  end
+
+  def caveats
+   <<-END_CAVEATS
+ * Add the following line to php.ini:
+    extension="#{prefix}/ffmpeg.so"
+ * Restart your webserver.
+ * Write a PHP page that calls "phpinfo();"
+ * Load it in a browser and look for the info on the ffmpeg module.
+ * If you see it, you have been successful!
+    END_CAVEATS
+  end
+end
