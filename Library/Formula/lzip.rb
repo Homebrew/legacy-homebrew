63f6322af6865dd964eefd9e451ee3a3261e71c5
David Höppner <0xffea@gmail.com>
2010-04-10 15:19:46 +0200

    lzip: version update to 1.10

--- Library/Formula/lzip.rb
+++ Library/Formula/lzip.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Lzip <Formula
-  url 'http://mirrors.igsobe.com/nongnu/lzip/lzip-1.9.tar.gz'
+  url 'http://mirrors.igsobe.com/nongnu/lzip/lzip-1.10.tar.gz'
   homepage 'http://www.nongnu.org/lzip/lzip.html'
-  md5 '9e1d7f4db5b953e9f75b8500ebe0d4c0'
+  md5 '84879f20450a69a94e125a67f4724d12'
 
   def install
     system "./configure", "--prefix=#{prefix}", "CXX=#{ENV.cxx}"
