4685ba994d11048e9e688e642e1056f602e1337f
Martin Kühl <martin.kuehl@gmail.com>
2010-04-12 01:01:45 +0200

    New Formula: highlight
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    * Fix etc folder, must have trailing slash for .conf to install correctly.

---
+++ Library/Formula/highlight.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Highlight <Formula
+  url 'http://www.andre-simon.de/zip/highlight-2.16.tar.bz2'
+  homepage 'http://www.andre-simon.de/doku/highlight/en/highlight.html'
+  sha1 'b5fed14bb1a973fe134dd2133766bb86fdc7494e'
+
+  def install
+    conf = etc+'highlight/'
+    system "make", "PREFIX=#{prefix}", "conf_dir=#{conf}"
+    system "make", "PREFIX=#{prefix}", "conf_dir=#{conf}", "install"
+  end
+end
