7adcf94ab5c31c27c4e54865dd74c1de2eb80793
Adam Vandenberg <flangy@gmail.com>
2010-04-12 12:19:08 -0700

    Download nspr over normal HTTP, not HTTPS.

--- Library/Formula/nspr.rb
+++ Library/Formula/nspr.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Nspr <Formula
-  @url='https://ftp.mozilla.org/pub/mozilla.org/nspr/releases/v4.7.6/src/nspr-4.7.6.tar.gz'
-  @homepage='http://www.mozilla.org/projects/nspr/'
-  @md5='c78384602b4b466081a55025446641db'
+  url 'http://ftp.mozilla.org/pub/mozilla.org/nspr/releases/v4.7.6/src/nspr-4.7.6.tar.gz'
+  homepage 'http://www.mozilla.org/projects/nspr/'
+  md5 'c78384602b4b466081a55025446641db'
 
   def install
     require 'hardware'
