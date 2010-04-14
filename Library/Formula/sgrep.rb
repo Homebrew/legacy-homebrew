8939721fc06b9af44ad51ca6f98042b8ce5c40bb
Martin Kühl <martin.kuehl@gmail.com>
2010-04-15 01:31:57 +0200

    New Formula: sgrep
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/sgrep.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Sgrep <Formula
+  url 'ftp://ftp.cs.helsinki.fi/pub/Software/Local/Sgrep/sgrep-1.92a.tar.gz'
+  homepage 'http://www.cs.helsinki.fi/u/jjaakkol/sgrep.html'
+  md5 '99eb1ed515648f653fc7be45e0896378'
+
+  def install
+    system "./configure",
+      "--prefix=#{prefix}",
+      "--mandir=#{man}",
+      "--datadir=#{share}/sgrep"
+    system "make install"
+  end
+end
