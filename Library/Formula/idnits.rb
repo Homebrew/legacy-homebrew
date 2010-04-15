dbfa7be440a5de21d364a41625a4aeac3e096067
Lars <lars.eggert@gmx.net>
2010-04-15 10:13:06 +0300

    Updated formulas: idnits, rfcmarkup, xml2rfc

--- Library/Formula/idnits.rb
+++ Library/Formula/idnits.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Idnits <Formula
-  url 'http://tools.ietf.org/tools/idnits/idnits-2.12.00.tgz'
+  url 'http://tools.ietf.org/tools/idnits/idnits-2.12.02.tgz'
   homepage 'http://tools.ietf.org/tools/idnits/'
-  md5 '54a4bbcf6e9afa454d608afa60cc7e5b'
+  md5 '9570404dc6fad568e289ca6eeac76b6f'
 
   depends_on 'aspell'
 
