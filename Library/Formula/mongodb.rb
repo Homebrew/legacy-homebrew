da5ea7cd378db8c11930524b392e60e30c868f5a
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2010-04-15 10:36:48 +0900

    updated mongodb-1.4.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -7,13 +7,13 @@ class Mongodb <Formula
   aka :mongo
 
   if Hardware.is_64_bit? and not ARGV.include? '--32bit'
-    url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.4.0.tgz'
-    md5 'c1e7fa4a566a050810ec00c746f40b4b'
-    version '1.4.0-x86_64'
+    url 'http://downloads.mongodb.org/osx/mongodb-osx-x86_64-1.4.1.tgz'
+    md5 'aaaccadb17e32dc476ab0d0aacae0e0d'
+    version '1.4.1-x86_64'
   else
-    url 'http://downloads.mongodb.org/osx/mongodb-osx-i386-1.4.0.tgz'
-    md5 '37bc07e09c6be6dbc4289bec54c0b120'
-    version '1.4.0-i386'
+    url 'http://downloads.mongodb.org/osx/mongodb-osx-i386-1.4.1.tgz'
+    md5 '8f144824d986e13f4a2b7c46cf75c1c6'
+    version '1.4.1-i386'
   end
 
   def skip_clean? path
