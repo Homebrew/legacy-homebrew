8cd7fb951d2b20e099c3ceffd41f0e921dd5acde
Adam Vandenberg <flangy@gmail.com>
2010-04-19 11:32:58 -0700

    Make libidn optional in wget.
    wget will find and use libidn if it is present, which uses gettext.
    This dependency was not captured in the wget formula.
    
    To keep things simpler, the user must now opt-in to libdin by brewing
    with "--enable-iri". Without this flag, we tell configure not to look
    for libidn.
    
    Fixes #1105.

--- Library/Formula/wget.rb
+++ Library/Formula/wget.rb
@@ -1,12 +1,20 @@
 require 'formula'
 
+# iri support requires libidn, which pulls in getteext, so we
+# disable this by default.
+
 class Wget <Formula
   homepage 'http://www.gnu.org/software/wget/'
   url 'http://ftp.gnu.org/gnu/wget/wget-1.12.tar.bz2'
   md5 '308a5476fc096a8a525d07279a6f6aa3'
 
+  depends_on "libidn" if ARGV.include? "--enable-iri"
+
   def install
-    system "./configure", "--disable-debug", "--prefix=#{prefix}"
+    args = ["--disable-debug", "--prefix=#{prefix}"]
+    args << "--disable-iri" unless ARGV.include? "--enable-iri"
+
+    system "./configure", *args
     system "make install"
   end
 end
