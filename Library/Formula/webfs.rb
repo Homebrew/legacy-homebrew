d899589ad79b176f6a446b760b3a21149eeb0eae
Adam Vandenberg <flangy@gmail.com>
2010-04-19 09:17:05 -0700

    webfs formula from zimbatm

---
+++ Library/Formula/webfs.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Webfs <Formula
+  url 'http://dl.bytesex.org/releases/webfs/webfs-1.21.tar.gz'
+  homepage 'http://linux.bytesex.org/misc/webfs.html'
+  md5 '6dc125fe160479404147e7bbfc781dbc'
+
+  def patches
+    {:p0 => "http://svn.macports.org/repository/macports/trunk/dports/www/webfs/files/patch-ls.c"}
+  end
+
+  def install
+    ENV["prefix"]=prefix
+    system "make install mimefile=/etc/apache2/mime.types"
+  end
+end
