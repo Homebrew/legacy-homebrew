04c6fc957bb8560641710fbf64ac5179d9ea8bd3
Markus Groß <mgross@informatik.uni-bremen.de>
2010-04-20 22:52:06 +0200

    New formula libmpc (actually it is called mpc, but this conflicts with the existing mpc)
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/libmpc.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Libmpc < Formula
+  url 'http://multiprecision.org/mpc/download/mpc-0.8.1.tar.gz'
+  homepage 'http://multiprecision.org'
+  md5 '5b34aa804d514cc295414a963aedb6bf'
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
