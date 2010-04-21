3d6aa2ea586d665a73a46e5a71973733dceddff8
Martin Kühl <martin.kuehl@gmail.com>
2010-04-21 23:59:43 +0200

    New formula: tre
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/tre.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Tre <Formula
+  url 'http://laurikari.net/tre/tre-0.8.0.tar.bz2'
+  homepage 'http://laurikari.net/tre/'
+  md5 'b4d3232593dadf6746f4727bdda20b41'
+
+  def install
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}"
+    system "make install"
+  end
+end
