afd7be36d11abd9e5a6b9037d498ee0dc0bad786
Tony Sokhon <tonyskn@gmail.com>
2010-04-24 12:20:11 +0200

    New formula: GNU httptunnel v3.0.5 (stable)
    httptunnel creates a bidirectional virtual data connection
    tunnelled in HTTP requests.
    
    http://www.nocrew.org/software/httptunnel.html
    Signed-off-by: David Höppner <0xffea@gmail.com>
    
    * use man var

---
+++ Library/Formula/httptunnel.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Httptunnel <Formula
+  url 'http://www.nocrew.org/software/httptunnel/httptunnel-3.0.5.tar.gz'
+  homepage 'http://www.nocrew.org/software/httptunnel.html'
+  md5 '2a710f377c82ab4eb201458f7b9f99c5'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make install"
+  end
+end
