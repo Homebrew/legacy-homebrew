0b96161d0d5700b8c3a3b0162d253c6b52ead77c
Max Howell <max@methylblue.com>
2010-04-24 12:01:44 +0100

    libspotify 0.0.4
    Split out from the framework using magic.

---
+++ Library/Formula/libspotify.rb
@@ -0,0 +1,46 @@
+require 'formula'
+
+class Libspotify <Formula
+  url 'http://developer.spotify.com/download/libspotify/libspotify-0.0.4-darwin.zip'
+  version '0.0.4'
+  homepage 'http://developer.spotify.com/en/libspotify/overview/'
+  md5 'a97552130df26b1aa2228adcf8d4210b'
+
+  def inc
+    include + 'libspotify'
+  end
+  def pc
+    lib+'pkgconfig'+'libspotify.pc'
+  end
+
+  def install
+    prefix.install 'share'
+    inc.install "libspotify.framework/Versions/#{version}/Headers/api.h"
+    lib.install "libspotify.framework/Versions/#{version}/libspotify" => 'libspotify.0.0.4.dylib'
+    doc.install Dir['doc/*']
+
+    cd lib
+    ln_s "libspotify.0.0.4.dylib", "libspotify.dylib"
+
+    system "install_name_tool",
+           "-id",
+           "#{HOMEBREW_PREFIX}/lib/libspotify.#{version}.dylib",
+           "libspotify.dylib"
+
+    pc.write pc_content
+  end
+
+  def pc_content; <<-EOS.undent
+    prefix=#{HOMEBREW_PREFIX}
+    exec_prefix=${prefix}
+    libdir=${exec_prefix}/lib
+    includedir=${prefix}/include
+
+    Name: libspotify
+    Description: Spotify client library
+    Version: #{version}
+    Libs: -L${libdir} -lspotify
+    Cflags: -I${includedir}
+    EOS
+  end
+end
