a4dcce657da59fa03bec0320548e29cef5569255
Adam Vandenberg <flangy@gmail.com>
2010-04-26 09:52:46 -0700

    Update (keg-only) libxml2 to 2.7.7.

--- Library/Formula/libxml2.rb
+++ Library/Formula/libxml2.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class Libxml2 <Formula
-  url 'ftp://xmlsoft.org/libxml2/libxml2-2.7.6.tar.gz'
+  url 'ftp://xmlsoft.org/libxml2/libxml2-2.7.7.tar.gz'
   homepage 'http://xmlsoft.org'
-  md5 '7740a8ec23878a2f50120e1faa2730f2'
+  md5 '9abc9959823ca9ff904f1fbcf21df066'
 
   def keg_only?
     :provided_by_osx
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make"
     ENV.j1
     system "make install"
