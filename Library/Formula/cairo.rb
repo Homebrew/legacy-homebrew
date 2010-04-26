161ba1022c8e54fa1adaa11e311a40ef675cc988
Adam Vandenberg <flangy@gmail.com>
2010-04-26 09:29:38 -0700

    Keg-only Cairo for Leopard.

---
+++ Library/Formula/cairo.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Cairo <Formula
+  url 'http://cairographics.org/releases/cairo-1.8.10.tar.gz'
+  homepage 'http://cairographics.org/'
+  md5 'b60a82f405f9400bbfdcf850b1728d25'
+
+  depends_on 'pkg-config'
+  depends_on 'libpng'
+  depends_on 'pixman'
+
+  # Comes with Snow Leopard, but not Leopard
+  def keg_only?
+    :provied_by_osx
+  end
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-x"
+    system "make install"
+  end
+end
