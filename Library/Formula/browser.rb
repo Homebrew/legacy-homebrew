a993dd90037f02ebee45450b4a469f52e9be063b
Kenneth Reitz <me@kennethreitz.com>
2010-04-26 11:48:18 -0400

    New forumla for defunkts Browswer script
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    * Simplfied

---
+++ Library/Formula/browser.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class Browser <GithubGistFormula
+  url 'http://gist.github.com/raw/318247/bcbcf6c5fb730b4f508c633cce56ba17828825c8/browser'
+  homepage 'http://gist.github.com/318247/'
+  md5 '8bdad2260651ea88d4f868071ef5b604'
+end
