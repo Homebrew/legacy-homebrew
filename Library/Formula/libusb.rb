32efc0eb56771f21b7c01f0e9dc5ed32d794af54
Adam Vandenberg <flangy@gmail.com>
2010-04-27 11:52:57 -0700

    Update libusb to 1.0.7

--- Library/Formula/libusb.rb
+++ Library/Formula/libusb.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libusb <Formula
-  url 'http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.6/libusb-1.0.6.tar.bz2'
-  homepage 'http://libusb.sourceforge.net'
-  md5 '818c7c02112a53e0c182666ee83f2057'
+  url 'http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.7/libusb-1.0.7.tar.bz2'
+  homepage 'http://www.libusb.org/'
+  md5 '31660f3433ba40c57be31b8a7d709a7d'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
