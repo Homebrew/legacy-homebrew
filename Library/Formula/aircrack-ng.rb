ae1bb72a1ab33847031b8429dd80f4472b9f42c7
Mike Danko <mike@l4m3.com>
2010-04-27 09:26:31 -0400

    updated aircrack-ng to 1.1. features and major security fixes
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/aircrack-ng.rb
+++ Library/Formula/aircrack-ng.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class AircrackNg <Formula
-  url 'http://download.aircrack-ng.org/aircrack-ng-1.0.tar.gz'
-  md5 'dafbfaf944ca9d523fde4bae86f0c067'
+  url 'http://download.aircrack-ng.org/aircrack-ng-1.1.tar.gz'
+  md5 'f7a24ed8fad122c4187d06bfd6f998b4'
   homepage 'http://aircrack-ng.org/'
   
   def install
