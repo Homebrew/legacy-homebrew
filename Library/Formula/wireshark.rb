d7c09f7d6d65046c71c0944a9e8860a08f3a906e
Adam Vandenberg <flangy@gmail.com>
2010-04-27 08:32:57 -0700

    Update wireshark to 1.2.7.

--- Library/Formula/wireshark.rb
+++ Library/Formula/wireshark.rb
@@ -1,17 +1,20 @@
 require 'formula'
 
 class Wireshark <Formula
-  @url='http://media-2.cacetech.com/wireshark/src/wireshark-1.0.10.tar.bz2'
-  @md5='facf1eef2f126a9d45e5c30702dae12b'
-  @homepage='http://www.wireshark.org'
+  url 'http://media-2.cacetech.com/wireshark/src/wireshark-1.2.7.tar.bz2'
+  md5 'cff6b18aa47a7e3eb973efe62c676b50'
+  homepage 'http://www.wireshark.org'
 
+  depends_on 'gnutls' => :optional
+  depends_on 'pcre' => :optional
   depends_on 'glib'
 
   def install
-    system "./configure", "--disable-debug",
-                          "--prefix=#{prefix}",
+    system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
                           "--disable-wireshark" # actually just disables the GTK GUI
+    system "make"
+    ENV.j1 # Install failed otherwise.
     system "make install"
   end
   
