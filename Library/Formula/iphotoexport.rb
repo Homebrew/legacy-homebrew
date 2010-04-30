a7ad466bc23952c34aa47d187cb32cea59af5ee9
Michel Rasschaert <michel.rasschaert@gmail.com>
2010-04-30 23:09:26 +0200

    New formula : iphotoexport
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    * Updated to 1.6.4

---
+++ Library/Formula/iphotoexport.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Iphotoexport <Formula
+  url 'http://iphotoexport.googlecode.com/files/iphotoexport-1.6.4.zip'
+  homepage 'http://code.google.com/p/iphotoexport/'
+  sha1 '50fa0916cf9689efdfd33cd4680424234b4e9023'
+
+  depends_on 'exiftool'
+  
+  def install
+    unzip_dir = @name+'-'+@version
+    # Change hardcoded exiftool path
+    inreplace "#{unzip_dir}/tilutil/exiftool.py", "/usr/bin/exiftool", "exiftool"
+    
+    prefix.install Dir[unzip_dir+'/*']
+    bin.mkpath
+    ln_s prefix+'iphotoexport.py', bin+'iphotoexport'
+  end
+end
