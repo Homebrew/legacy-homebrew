b7ae3fe183da304fcbb6c212cc66e7db755b7996
Adam Vandenberg <flangy@gmail.com>
2010-05-02 17:09:27 -0700

    Remove explicit versions from GitHub tarballs

--- Library/Formula/growlnotify.rb
+++ Library/Formula/growlnotify.rb
@@ -1,7 +1,6 @@
 require 'formula'
 
 class Growlnotify <Formula
-  version '1.2'
   url 'http://github.com/indirect/growlnotify/tarball/v1.2'
   md5 'b7f4436ab3916d3d1072467cecf7e4ee'
   homepage 'http://growl.info/extras.php#growlnotify'
