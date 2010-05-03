70aed406a1dd3c629edb878cd6fd6cc2a0a13fbb
Adam Vandenberg <flangy@gmail.com>
2010-05-03 10:40:24 -0700

    Updates to GNU Smalltalk
    * Add build-time gawk dependency
    * Add notes about 32-bit optional dependencies
    
    Fixes #1306.

--- Library/Formula/gnu-smalltalk.rb
+++ Library/Formula/gnu-smalltalk.rb
@@ -2,20 +2,28 @@ require 'formula'
 
 # References:
 # * http://smalltalk.gnu.org/wiki/building-gst-guides
+#
+# Note that we build 32-bit, which means that 64-bit
+# optional dependencies will break the build. You may need
+# to "brew unlink" these before installing GNU Smalltalk and
+# "brew link" them afterwards:
+# * gdbm
 
 class GnuSmalltalk <Formula
   url 'ftp://ftp.gnu.org/gnu/smalltalk/smalltalk-3.2.tar.gz'
   homepage 'http://smalltalk.gnu.org/'
   sha1 'd951714c4fc7d91d06bdc33c20905885e5d2b25f'
 
-  # gmp is an optional dep, but doesn't compile on 10.5
-  # depends_on 'gmp' => :optional
+  depends_on 'gawk' # Needed to build
+  # depends_on 'gmp' => :optional # 32/64 built build problems
 
   def install
     # Codegen problems with LLVM
     ENV.gcc_4_2
+
     # 64-bit version doesn't build, so force 32 bits.
     ENV.m32
+
     ENV['FFI_CFLAGS'] = '-I/usr/include/ffi'
     system "./configure", "--prefix=#{prefix}", "--disable-debug", 
                           "--disable-dependency-tracking",
