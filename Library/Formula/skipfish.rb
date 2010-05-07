b92b575b8892fc2ee4419dff1ae9b437a577f8bb
Ali Asad Lotia <lotia@umich.edu>
2010-05-07 14:14:18 -0400

    Update skipfish to 1.34b.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/skipfish.rb
+++ Library/Formula/skipfish.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Skipfish <Formula
-  url 'http://skipfish.googlecode.com/files/skipfish-1.33b.tgz'
+  url 'http://skipfish.googlecode.com/files/skipfish-1.34b.tgz'
   homepage 'http://code.google.com/p/skipfish/'
-  md5 '7ee6a67e84e03804a5ef0270d2ca7abe'
+  md5 'b988a2d8ddbef7c3cbecbbafd2ec7269'
 
   depends_on 'libidn'
 
