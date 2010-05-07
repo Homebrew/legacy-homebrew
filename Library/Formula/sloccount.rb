2e328e02962024d69072a900e8d6eaeb5a070e49
Will Boyce <me@willboyce.com>
2010-05-08 00:26:03 +0100

    patched sloccount with support for Erlang
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/sloccount.rb
+++ Library/Formula/sloccount.rb
@@ -30,3 +30,37 @@ index 0c029f1..8e303e2 100644
  	$(INSTALL) $(EXECUTABLES) $(INSTALL_DIR)
  
  uninstall_programs:
+
+diff --git a/break_filelist b/break_filelist
+index ad2de47..ff854e0 100755
+--- a/break_filelist
++++ b/break_filelist
+@@ -205,6 +205,7 @@ $noisy = 0;            # Set to 1 if you want noisy reports.
+   "hs" => "haskell", "lhs" => "haskell",
+    # ???: .pco is Oracle Cobol
+   "jsp" => "jsp",  # Java server pages
++  "erl" => "erlang",
+ );
+
+
+diff --git a/erlang_count b/erlang_count
+new file mode 100755
+index 0000000..50af97c
+--- /dev/null
++++ b/erlang_count
+@@ -0,0 +1,3 @@
++#!/bin/sh
++
++generic_count '%' $@
+diff --git a/makefile b/makefile
+index 0c029f1..7b446d5 100644
+--- a/makefile
++++ b/makefile
+@@ -95,6 +95,7 @@ EXECUTABLES= \
+    count_extensions \
+    count_unknown_ext \
+    csh_count \
++   erlang_count \
+    exp_count \
+    fortran_count \
+    f90_count \
