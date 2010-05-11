ba5676e3b80e25fa7f252af93a3b560742a3ecb5
Igor Zubkov <igor.zubkov@gmail.com>
2010-05-11 21:35:00 +0300

    update webkit2png to 0.5 from git and add md5 to formula
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/webkit2png.rb
+++ Library/Formula/webkit2png.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Webkit2png <Formula
-  head 'git://github.com/paulhammond/webkit2png.git'
+  url 'http://github.com/paulhammond/webkit2png/tarball/9c4265a82ebfcec200fca8de39fb970e5aae0a3d'
+  version '0.5'
   homepage 'http://www.paulhammond.org/webkit2png/'
-  md5 ''
+  sha1 '1112d3f7e5fac5e1bfef11f38626d09536957310'
 
   def install
     bin.install 'webkit2png'
