7abef33894da5772bb29b366e41cd4db3ee633e3
Julian Stahnke <mail@julianstahnke.com>
2010-05-12 16:32:18 +0200

    Elastic search version 0.6.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/elasticsearch.rb
+++ Library/Formula/elasticsearch.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Elasticsearch < Formula
-  url 'http://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.5.1.zip'
+  url 'http://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.6.0.zip'
   homepage 'http://www.elasticsearch.com'
-  md5 'c438c0d18bef8d2efd2d801fa872a3fa'
+  md5 'e3657a1970695c3d043341484b338af3'
 
   def install
     FileUtils.rm_f Dir["bin/*.bat"]
