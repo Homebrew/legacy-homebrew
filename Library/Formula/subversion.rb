c846cb80afc4a99855196928b152edd93743d8da
Augie Fackler <durin42@gmail.com>
2010-05-14 13:33:37 -0500

    Subversion: flag for building Python bindings
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    * Added caveats and cleaned up a bit.

--- Library/Formula/subversion.rb
+++ Library/Formula/subversion.rb
@@ -1,5 +1,6 @@
 require 'formula'
 
+def build_python?; ARGV.include? "--python"; end
 def build_java?; ARGV.include? "--java"; end
 def build_universal?; ARGV.include? '--universal'; end
 
@@ -14,7 +15,7 @@ class Subversion <Formula
   url 'http://subversion.tigris.org/downloads/subversion-1.6.11.tar.bz2'
   md5 '75419159b50661092c4137449940b5cc'
   homepage 'http://subversion.apache.org/'
-  
+
   aka 'svn'
 
   # On Snow Leopard, build a new neon. For Leopard, the deps above include this.
@@ -23,6 +24,7 @@ class Subversion <Formula
   def options
     [
       ['--java', 'Build Java bindings.'],
+      ['--python', 'Build Python bindings.'],
       ['--universal', 'Build as a Universal Intel binary.']
     ]
   end
@@ -79,6 +81,11 @@ class Subversion <Formula
     system "make"
     system "make install"
 
+    if build_python?
+      system "make swig-py"
+      system "make install-swig-py"
+    end
+
     if build_java?
       ENV.j1 # This build isn't parallel safe
       system "make javahl"
@@ -87,11 +94,23 @@ class Subversion <Formula
   end
 
   def caveats
-    if build_java?
-      <<-EOS.undent
-        You may need to link the Java bindings into the Java Extensions folder:
-          sudo ln -s #{HOMEBREW_PREFIX}/lib/libsvnjavahl-1.dylib /Library/Java/Extensions
+    s = ""
+
+    if build_python?
+      s += <<-EOS.undent
+        You may need to add the Python bindings to your PYTHONPATH from:
+          #{HOMEBREW_PREFIX}/lib/svn-python
+
       EOS
     end
+
+    if build_java?
+      s += <<-EOS.undent
+        You may need to link the Java bindings into the Java Extensions folder:
+          sudo ln -s #{HOMEBREW_PREFIX}/lib/libsvnjavahl-1.dylib /Library/Java/Extensions
+
+      EOS
+    end
+    return s
   end
 end
