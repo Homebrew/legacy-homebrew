37a511c4c36a2d3bbee4fe335d23f66eb34a73e8
Julian Stahnke <mail@julianstahnke.com>
2010-05-15 16:41:55 +0200

    ElasticSearch 0.7.0
    Signed-off-by: David Höppner <0xffea@gmail.com>

--- Library/Formula/elasticsearch.rb
+++ Library/Formula/elasticsearch.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Elasticsearch < Formula
-  url 'http://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.6.0.zip'
+  url 'http://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.7.0.zip'
   homepage 'http://www.elasticsearch.com'
-  md5 'e3657a1970695c3d043341484b338af3'
+  md5 '6f71e1306d7fba1b1fc1c3d48641feb9'
 
   def install
     FileUtils.rm_f Dir["bin/*.bat"]
