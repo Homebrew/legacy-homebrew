00fa1a3b39724df5e16b6a58da6111124daaeb3b
David Höppner <0xffea@gmail.com>
2010-05-16 13:23:14 +0200

    dosbox: version update to 0.74

--- Library/Formula/dosbox.rb
+++ Library/Formula/dosbox.rb
@@ -1,12 +1,10 @@
 require 'formula'
 require 'hardware'
 
-# Install script heavily borrowed from Fink package:
-#  http://pdb.finkproject.org/pdb/package.php/dosbox
 class Dosbox <Formula
-  url 'http://downloads.sourceforge.net/project/dosbox/dosbox/0.73/dosbox-0.73.tar.gz'
+  url 'http://downloads.sourceforge.net/project/dosbox/dosbox/0.74/dosbox-0.74.tar.gz'
   homepage 'http://www.dosbox.com/'
-  md5 '0823a11242db711ac3d6ebfff6aff572'
+  md5 'b9b240fa87104421962d14eee71351e8'
   
   depends_on 'sdl'
   depends_on 'sdl_net'
@@ -16,27 +14,11 @@ class Dosbox <Formula
     ENV.libpng
     ENV.fast
     
-    which_darwin=`uname -r|cut -f1 -d.`
-    
-    # 64-bit CPU detection is broken in ./configure
-    if MACOS_VERSION >= 10.6 and Hardware.is_64_bit?
-      which_cpu = 'x86_64'
-    else
-      which_cpu = 'i386'
-    end
-    
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--disable-sdltest",
-                          "--enable-core-inline",
-                          "--build=#{which_cpu}-apple-darwin#{which_darwin}"
-    
-    # This should be a patch - add missing header.
-    inreplace "src/gui/midi_coreaudio.h",
-      "#include <AudioToolbox/AUGraph.h>",
-      "#include <AudioToolbox/AUGraph.h>\n#include <CoreServices/CoreServices.h>"
-    
+                          "--enable-core-inline"
     system "make"
     
     bin.install 'src/dosbox'
