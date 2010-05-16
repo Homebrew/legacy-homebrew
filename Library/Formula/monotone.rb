4662abc3ff960739641a4a7bda8c17459c5aa8a5
David Höppner <0xffea@gmail.com>
2010-05-16 12:20:05 +0200

    monotone: update version to 0.47

--- Library/Formula/monotone.rb
+++ Library/Formula/monotone.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Monotone <Formula
-  url 'http://www.monotone.ca/downloads/0.46/monotone-0.46.tar.gz'
+  url 'http://www.monotone.ca/downloads/0.47/monotone-0.47.tar.gz'
   homepage 'http://www.monotone.ca/'
-  md5 '405286223efcf375cd022dabf2f1c845'
+  md5 '21da9c44a197f2e5e379a5bb4e42797e'
 
   depends_on 'pkg-config'
   depends_on 'gettext'
