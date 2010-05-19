abf262f82a35b5b552fca48df16e7e0607769ea2
Jack Chu <kamuigt@gmail.com>
2010-05-19 17:20:54 -0400

    Update grails forumla to 1.3.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Grails <Formula
-  url 'http://dist.codehaus.org/grails/grails-1.3.0.zip'
+  url 'http://dist.codehaus.org/grails/grails-1.3.1.zip'
   homepage 'http://grails.org'
-  md5 '0117efe1cbff4e0bb4fe3eb05e37a0c0'
+  md5 'eae157394eeaa704015756a7613eae50'
 
   def install
     rm_f Dir["bin/*.bat"]
