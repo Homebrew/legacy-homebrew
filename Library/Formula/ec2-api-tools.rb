c227f4f4357b787f75acca7e53e8f240ddac1173
Adam Vandenberg <flangy@gmail.com>
2010-05-19 20:14:41 -0700

    Clean up ec2-api-tools
    Modernize the brew and clean up the instructions.

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -1,55 +1,45 @@
 require 'formula'
 
 class Ec2ApiTools <Formula
-  @homepage='http://developer.amazonwebservices.com/connect/entry.jspa?externalID=351'
-  @url='http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.3-51254.zip'
-  @md5='4644d3984009f576b1c34c6d60086e60'
-  
+  homepage 'http://developer.amazonwebservices.com/connect/entry.jspa?externalID=351'
+  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.3-51254.zip'
+  md5 '4644d3984009f576b1c34c6d60086e60'
+
   def patches
     # (From http://gist.github.com/200283) Gets rid of the
     # "[Deprecated] Xalan: org.apache.xml.res.XMLErrorResources_en_US"
     # messages that the tools spew on 1.3-41620 under Snow Leopard
     DATA
   end
-  
+
   def install
-    # Nothing to be done but copying things into place
-    FileUtils.rm Dir['bin/*\.cmd']
-    
-    (prefix+bin).install Dir['bin/ec2-*']
-    prefix.install 'lib'
+    rm Dir['bin/*.cmd'] # Remove Windows versions
+    bin.install Dir['bin/ec2-*'] # Install commands to bin
+    prefix.install 'lib' # Put the .jars in prefix/lib
   end
-  
+
   def caveats
-    return <<-EOS
-Before you can utilize the EC2 API tools, you must export several environment
-variables to your $SHELL. The easiest way to do this is to add them to your
-dotfiles. If you're running the `bash` shell (the default), you'll want to add
-them to `~/.bash_profile`. If this is the case, run `nano ~/.bash_profile` at
-a terminal to edit said file. zsh users will want to edit `~/.zprofile`
-instead.
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL
+      and download your X.509 certificate and private key from Amazon Web Services.
 
-    export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home/"
-    export EC2_HOME="#{prefix}/"
+      Your certificate and private key are available at:
+      http://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key
 
-However, you're still not ready to use the tools. You need to download your
-X.509 certificate and private key from Amazon Web Services. These files are
-available at the following URL:
+      Download two ".pem" files, one starting with `pk-`, and one starting with `cert-`.
+      You need to put both into a folder in your home directory, `~/.ec2`.
 
-http://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key
+      To export the needed variables, add them to your dotfiles.
+       * On Bash, add them to `~/.bash_profile`.
+       * On Zsh, add them to `~/.zprofile` instead.
 
-You should download two `.pem` files, one starting with `pk-`, and one
-starting with `cert-`. You need to put both into a folder in your home
-directory, `~/.ec2`, and then add the following to your profile file:
-
-    export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
-    export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
-
+      export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home/"
+      export EC2_HOME="#{prefix}/"
+      export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
+      export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
     EOS
   end
-  
 end
-
 
 __END__
 diff --git i/bin/ec2-cmd w/bin/ec2-cmd
