2118bc2f9bf31b185a81b2c28d38cef9e277c297
David Höppner <0xffea@gmail.com>
2010-05-22 13:10:37 +0200

    New formula zzuf
    zzuf is a transparent application input fuzzer. Its purpose is to find bugs in applications
    by corrupting their user-contributed data (which more than often comes from untrusted sources
    on the Internet). It works by intercepting file and network operations and changing random
    bits in the program’s input. zzuf’s behaviour is deterministic, making it easier to reproduce
    bugs.

---
+++ Library/Formula/zzuf.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Zzuf <Formula
+  url 'http://caca.zoy.org/files/zzuf/zzuf-0.13.tar.gz'
+  homepage 'http://caca.zoy.org/wiki/zzuf'
+  md5 '74579c429f9691f641a14f408997d42d'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
