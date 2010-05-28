5ab6da3f799dd44af06565aa8a18977ccc205b41
Yacin Nadji <yacin@gatech.edu>
2010-05-27 23:14:01 -0400

    added md5 to libconfig
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libconfig.rb
+++ Library/Formula/libconfig.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libconfig <Formula
   url 'http://www.hyperrealm.com/libconfig/libconfig-1.4.5.tar.gz'
   homepage 'http://www.hyperrealm.com/libconfig/'
-  md5 ''
+  md5 'f2219e1b2501e7296a7d3e971c63666a'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
