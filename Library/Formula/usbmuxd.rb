65ff87b6835906ca54c4e5e6dc715842b850759f
Edward George <edwardgeorge@gmail.com>
2010-06-02 20:58:47 +0100

    update usbmuxd to 1.0.4
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/usbmuxd.rb
+++ Library/Formula/usbmuxd.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Usbmuxd <Formula
-  url 'http://marcansoft.com/uploads/usbmuxd/usbmuxd-1.0.0-rc2.tar.bz2'
+  url 'http://marcansoft.com/uploads/usbmuxd/usbmuxd-1.0.4.tar.bz2'
   homepage 'http://marcansoft.com/blog/iphonelinux/usbmuxd/'
-  md5 '996d5ec74fafae9d2fca0c0d7a75ba71'
+  md5 '450c72273dd1dcc1d0fcfc7138122d54'
 
   depends_on 'libusb'
   depends_on 'cmake'
