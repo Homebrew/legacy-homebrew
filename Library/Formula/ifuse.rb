475b97ff784d4c4885102f7be5bfd0ea4ed13a90
Edward George <edwardgeorge@gmail.com>
2010-06-03 00:05:44 +0100

    Added formula: ifuse 1.0.0
    ifuse is a fuse filesystem that allows you to mount the filesystem of an
    iPhone/iPod Touch.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/ifuse.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Ifuse <Formula
+  url 'http://www.libimobiledevice.org/downloads/ifuse-1.0.0.tar.bz2'
+  homepage 'http://www.libimobiledevice.org/'
+  md5 '325d58abe182afa95187e6c55f2bba5f'
+
+  depends_on 'pkg-config'
+  depends_on 'glib'
+  depends_on 'libimobiledevice'
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def caveats
+    <<-EOS.undent
+    This depends on the MacFUSE installation from http://code.google.com/p/macfuse/
+    MacFUSE must be installed prior to installing this formula.
+    EOS
+  end
+end
