1a3cf9d440beb3ccc3936dcc4290435c01a7ff3c
Reid Burke <me@reidburke.com>
2010-06-07 15:59:12 -0700

    Install all of the npm man pages & fix "npm help"
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/npm.rb
+++ Library/Formula/npm.rb
@@ -8,29 +8,33 @@ class Npm <Formula
 
   depends_on 'node'
 
-  def install
-    # install all the required libs in libexec
-    libexec.install %w[cli.js npm.js package.json doc lib man scripts]
-
-    # install man pages
-    man1.mkpath
-    ln_s libexec+'man/npm.1', man1+'npm.1'
-
-    # install the wrapper executable
-    (bin+"npm").write executable
-  end
-
-  def caveats
-    <<-EOS.undent
-      npm is still under heavy development.
-      Hopefully this package will help keep people up to date.
-    EOS
-  end
-
   def executable
     <<-EOS
 #!/bin/sh
 exec "#{libexec}/cli.js" "$@"
     EOS
+  end
+
+  def install
+    doc.install Dir["doc/*"]
+    prefix.install ["LICENSE", "README.md"]
+
+    # install all the required libs in libexec so `npm help` will work
+    libexec.install Dir["*"]
+
+    # add "npm-" prefix to man pages link them into the libexec man pages
+    man1.mkpath
+    Dir.chdir libexec+"man" do
+      Dir["*"].each do |file|
+        if file == "npm.1"
+          ln_s "#{libexec}/man/#{file}", man1
+        else
+          ln_s "#{libexec}/man/#{file}", "#{man1}/npm-#{file}"
+        end
+      end
+    end
+
+    # install the wrapper executable
+    (bin+"npm").write executable
   end
 end
