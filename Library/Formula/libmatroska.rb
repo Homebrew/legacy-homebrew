0666a64781b6433d4568d0ed34942b5eb6df573a
Dominic Evans <oldmanuk@gmail.com>
2010-06-07 08:56:23 +0100

    Update libmatroska to 1.0.0

--- Library/Formula/libmatroska.rb
+++ Library/Formula/libmatroska.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Libmatroska <Formula
   # This is the official source, but it's frequently down. Use the mktoolnix mirror instead.
-  # url 'http://dl.matroska.org/downloads/libmatroska/libmatroska-0.8.1.tar.bz2'
-  url 'http://www.bunkus.org/videotools/mkvtoolnix/sources/libmatroska-0.8.1.tar.bz2'
+  # url 'http://dl.matroska.org/downloads/libmatroska/libmatroska-1.0.0.tar.bz2'
+  url 'http://www.bunkus.org/videotools/mkvtoolnix/sources/libmatroska-1.0.0.tar.bz2'
   homepage 'http://www.matroska.org/'
-  md5 '2ceb8235e5189fe4e79ec25ebd97b56b'
+  md5 '4efcf3945b17a6f0320cf1aaa326a4de'
 
   depends_on 'libebml'
 
