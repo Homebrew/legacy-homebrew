dd90bc39a09cbb472939735f2681e8701f327af8
Adam Vandenberg <flangy@gmail.com>
2010-06-10 19:06:30 -0700

    Fix ack md5

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -3,6 +3,6 @@ require 'formula'
 class Ack <ScriptFileFormula
   version '1.92'
   url "http://github.com/petdance/ack/raw/f5556c48eb46100e1733f5a21b45a00f6c190061/ack"
-  md5 '7175438fbcb43405f50d7ffda701eeb3'
+  md5 'c7285e66d033249a0b25de2b472b00ee'
   homepage 'http://betterthangrep.com/'
 end
