8b05fd2781d29281549ec2fe8485977c4f196798
Douglas Creager <douglas.creager@redjack.com>
2010-06-10 21:45:37 -0400

    SiLK 2.3.1
    SiLK, the System for Internet-Level Knowledge, is a collection of
    traffic analysis tools developed by the CERT Network Situational
    Awareness Team (CERT NetSA) to facilitate security analysis of large
    networks.  The SiLK tool suite supports the efficient collection,
    storage, and analysis of network flow data, enabling network security
    analysts to rapidly query large historical traffic data sets.  SiLK is
    ideally suited for analyzing traffic on the backbone or border of a
    large, distributed enterprise or mid-sized ISP.
    
    A SiLK installation consists of two categories of applications: the
    packing system and the analysis suite.  The packing system collects
    IPFIX, NetFlow v9, or NetFlow v5 and converts the data into a more
    space efficient format, recording the packed records into
    service-specific binary flat files.  The analysis suite consists of
    tools which read these flat files and perform various query
    operations, ranging from per-record filtering to statistical analysis
    of groups of records.  The analysis tools interoperate using pipes,
    allowing a user to develop a relatively sophisticated query from a
    simple beginning.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/silk.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Silk <Formula
+  url 'http://tools.netsa.cert.org/releases/silk-2.3.1.tar.gz'
+  homepage 'http://tools.netsa.cert.org/silk/'
+  md5 'f49ca6da333e5d579347af858e6a1b83'
+
+  depends_on 'glib'
+  depends_on 'libfixbuf'
+  depends_on 'yaf'
+
+  def install
+    system "./configure",
+        "--disable-dependency-tracking",
+        "--prefix=#{prefix}",
+        "--mandir=#{man}",
+        "--enable-ipv6",
+        "--enable-data-rootdir=#{var}/silk"
+    system "make"
+    system "make install"
+
+    (var+"silk").mkpath
+  end
+end
