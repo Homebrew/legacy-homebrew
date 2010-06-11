c72fb19aa8d0f16f2f712604aed51a5a1b7cdaef
Douglas Creager <douglas.creager@redjack.com>
2010-06-11 00:03:27 -0400

    Poco 1.3.6p2
    The POCO C++ Libraries (POCO stands for POrtable COmponents) are open
    source C++ class libraries that simplify and accelerate the
    development of network-centric, portable applications in C++.  The
    libraries integrate perfectly with the C++ Standard Library and fill
    many of the functional gaps left open by it.  Their modular and
    efficient design and implementation makes the POCO C++ Libraries
    extremely well suited for embedded development, an area where the C++
    programming language is becoming increasingly popular, due to its
    suitability for both low-level (device I/O, interrupt handlers, etc.)
    and high-level object-oriented development.  Of course, the POCO C++
    Libraries are also ready for enterprise-level challenges.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/poco.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Poco <Formula
+  url 'http://downloads.sourceforge.net/project/poco/sources/poco-1.3.6/poco-1.3.6p2-all.tar.bz2'
+  homepage 'http://pocoproject.org/'
+  md5 '8f1a6c3511764167d39f1950da3fcb37'
+  version '1.3.6p2'
+
+  def install
+    system "./configure",
+        "--disable-debug",
+        "--disable-dependency-tracking",
+        "--omit=Data/MySQL,Data/ODBC",
+        "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
