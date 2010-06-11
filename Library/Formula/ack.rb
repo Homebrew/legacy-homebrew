8871219c1e4289090d9fb359a2eaf3862c9637b0
Dominic Evans <oldmanuk@gmail.com>
2010-06-11 08:07:47 +0100

    Fix ack md5 and blog->raw

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ack <ScriptFileFormula
   version '1.92'
-  url "http://github.com/petdance/ack/blob/079b049b7240c2960a8ff811b2857eba462ad803/ack"
-  md5 'c71bac8eb03af2cc3e51bf0c787755d8'
+  url "http://github.com/petdance/ack/raw/079b049b7240c2960a8ff811b2857eba462ad803/ack"
+  md5 '7db577145ceba9f6cc5fddc3e8198342'
   homepage 'http://betterthangrep.com/'
 end
