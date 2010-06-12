0da053fb9723a5d3fadde66c6e0b9f2a7108239d
Mark Imbriaco <mark@imbriaco.com>
2010-06-11 23:34:18 -0400

    Update for node.js 0.1.98.
    * gnutls is not necessary, node uses libssl.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,14 +1,12 @@
 require 'formula'
 
 class Node <Formula
-  url 'http://nodejs.org/dist/node-v0.1.97.tar.gz'
+  url 'http://nodejs.org/dist/node-v0.1.98.tar.gz'
   head 'git://github.com/ry/node.git'
   homepage 'http://nodejs.org/'
-  md5 '4e80b775f9417cc8305fdec34646d8fb'
+  md5 'd8a75cb5c18ce20e0206ced95a8c1544'
 
   aka 'node.js'
-  
-  depends_on 'gnutls' => :recommended
   
   def skip_clean? path
     # TODO: at some point someone should tweak this so it only skips clean
