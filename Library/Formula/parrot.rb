7d7c2da6f8e954d45a950b289524eb42398d2f9e
uasi <uasi@99cm.org>
2010-06-16 00:17:56 +0900

    Update parrot to 2.5.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -2,22 +2,23 @@ require 'formula'
 
 class Parrot <Formula
   head 'bzr://https://launchpad.net/parrot/trunk'
-  url 'ftp://ftp.parrot.org/pub/parrot/releases/devel/2.4.0/parrot-2.4.0.tar.gz'
+  url 'ftp://ftp.parrot.org/pub/parrot/releases/devel/2.5.0/parrot-2.5.0.tar.gz'
   homepage 'http://www.parrot.org/'
-  md5 '596c62109f80e1c4b6a5919553dc801b'
+  md5 'c64fe3a619afbda92e92d8df93b0d05d'
 
-  depends_on 'pcre'
+  depends_on 'icu4c' => :optional
+  depends_on 'pcre' => :optional
 
   def install
     system "perl", "Configure.pl", "--prefix=#{prefix}", "--debugging=0",
                                    "--without-opengl", "--cc=#{ENV.cc}"
+
     system "make"
     system "make install"
 
     l = %x{otool -L #{bin}/parrot}[/\S*blib\/lib\S*/]
-
-    %w{parrot parrot-nqp parrot-prove parrot_config parrot_debugger
-       parrot_nci_thunk_gen pbc_disassemble pbc_dump pbc_merge pbc_to_exe
+    %w{ops2c parrot parrot-nqp parrot-prove parrot_config parrot_debugger
+      parrot_nci_thunk_gen pbc_disassemble pbc_dump pbc_merge pbc_to_exe
     }.each do |fn|
       system "install_name_tool -change #{l} #{lib}/libparrot.dylib #{bin+fn}"
     end
