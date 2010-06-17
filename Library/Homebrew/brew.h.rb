8bd0f736eab629f000740ea93e9b9fd08df7ed0f
Adam Vandenberg <flangy@gmail.com>
2010-06-17 11:20:23 -0700

    filter out aliases when real name was found

--- Library/Homebrew/brew.h.rb
+++ Library/Homebrew/brew.h.rb
@@ -403,10 +403,16 @@ def search_brews text
   require "formula"
   formulae = Formulary.names with_aliases=true
   if text =~ /^\/(.*)\/$/
-    return formulae.grep(Regexp.new($1))
+    results = formulae.grep(Regexp.new($1))
   else
     search_term = Regexp.escape(text || "")
-    return formulae.grep(/.*#{search_term}.*/)
+    results = formulae.grep(/.*#{search_term}.*/)
+  end
+
+  # Filter out aliases when the full name was also found
+  aliases = Formulary.get_aliases
+  return results.select do |r|
+    aliases[r] == nil or not (results.include? aliases[r])
   end
 end
 
