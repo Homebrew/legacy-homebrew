292625792c231e5c120664873fe1a427a886b5f0
Lin Jen-Shin <godfat@godfat.org>
2010-06-17 20:39:49 +0800

    Formula: add ucl and upx (which depends on ucl)

---
+++ Library/Formula/upx.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Upx <Formula
+  url 'http://upx.sourceforge.net/download/upx-3.05-src.tar.bz2'
+  homepage 'http://upx.sourceforge.net'
+  md5 '1f0ca94c8c26a816402274dd7e628334'
+
+  depends_on 'ucl'
+
+  def install
+    system "make all"
+    system "src/upx.out src/upx.out"
+    bin.install "src/upx.out" => "upx"
+  end
+end
