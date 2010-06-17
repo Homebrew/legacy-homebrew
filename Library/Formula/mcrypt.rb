1af2ef1e9aaaa16d9896d7008f964f89817cdcc9
Chris Hoffman <cehoffman@gmail.com>
2010-06-17 09:00:18 -0500

    Fix warning on man page installation for mcrypt
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mcrypt.rb
+++ Library/Formula/mcrypt.rb
@@ -1,12 +1,15 @@
 require 'formula'
 
 class Mcrypt <Formula
-  @url='ftp://mirror.internode.on.net/pub/gentoo/distfiles/libmcrypt-2.5.8.tar.gz'
-  @homepage='http://mcrypt.sourceforge.net'
-  @md5='0821830d930a86a5c69110837c55b7da'
+  url 'ftp://mirror.internode.on.net/pub/gentoo/distfiles/libmcrypt-2.5.8.tar.gz'
+  homepage 'http://mcrypt.sourceforge.net'
+  md5 '0821830d930a86a5c69110837c55b7da'
+
+  aka 'libmcrypt'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    ENV.universal_binary if ARGV.include? "--universal"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--mandir=#{man}"
     system "make install"
   end
 end
