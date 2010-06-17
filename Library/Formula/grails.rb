d81ecb3ed6f21f9cfe6ecee31cb65de3d0e7a8d9
Michael Lai <myllai@gmail.com>
2010-06-17 11:23:39 -0700

    Updated grails formula to 1.3.2.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Grails <Formula
-  url 'http://dist.codehaus.org/grails/grails-1.3.1.zip'
+  url 'http://dist.codehaus.org/grails/grails-1.3.2.zip'
   homepage 'http://grails.org'
-  md5 'eae157394eeaa704015756a7613eae50'
+  md5 '59586a39bf0b96e6c0689a3cffab9813'
 
   def install
     rm_f Dir["bin/*.bat"]
