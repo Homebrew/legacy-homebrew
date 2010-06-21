c508c8d087b7bdae27efdb595eb8dbddc4b79c53
Andrew White <andyw@pixeltrix.co.uk>
2010-06-21 13:22:26 +0100

    Upgrade to Ragel 6.6
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ragel.rb
+++ Library/Formula/ragel.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Ragel <Formula
-  url 'http://www.complang.org/ragel/ragel-6.5.tar.gz'
+  url 'http://www.complang.org/ragel/ragel-6.6.tar.gz'
   homepage 'http://www.complang.org/ragel/'
-  md5 'bb152087079ad7a545dcdc955b752301'
+  md5 '5c4366369f4934adc02bd71dc1a4ee1f'
 
   def install
     system "./configure", 
