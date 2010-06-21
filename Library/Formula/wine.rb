ecb2e154f506812060f1d7296b7e73bf378264d4
Adam Vandenberg <flangy@gmail.com>
2010-06-21 09:56:22 -0700

    Update to Wine 1.1.44
    * Don't use mpg123 on 10.6/64-bit

--- Library/Formula/wine.rb
+++ Library/Formula/wine.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Wine <Formula
-  url 'http://downloads.sourceforge.net/project/wine/Source/wine-1.1.42.tar.bz2'
-  sha1 'ea932f19528a22eacc49f16100dbf2251cb4ad5c'
+  url 'http://downloads.sourceforge.net/project/wine/Source/wine-1.1.44.tar.bz2'
+  sha1 '60f11693161b28ff9814949f2b6bbccee1d07a2c'
   homepage 'http://www.winehq.org/'
   head 'git://source.winehq.org/git/wine.git'
 
@@ -20,19 +20,25 @@ EOS
     fails_with_llvm
     ENV.x11
 
-    # Make sure we build 32bit version, because Wine64 is not fully functional yet
+    # Build 32-bit; Wine doesn't support 64-bit host builds on OS X.
     build32 = "-arch i386 -m32"
 
     ENV["LIBS"] = "-lGL -lGLU"
     ENV.append "CFLAGS", build32
     ENV.append "CXXFLAGS", "-D_DARWIN_NO_64_BIT_INODE"
-    ENV.append "LDFLAGS", [build32, "-framework CoreServices", "-lz", "-lGL -lGLU"].join(' ')
+    ENV.append "LDFLAGS", "#{build32} -framework CoreServices -lz -lGL -lGLU"
     ENV.append "DYLD_FALLBACK_LIBRARY_PATH", "/usr/X11/lib"
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--disable-win16"
+    args = [ "--prefix=#{prefix}", "--disable-win16" ]
+
+    # Building a universal mpg123 is non-trivial, so skip for now.
+    args << "--without-mpg123" if Hardware.is_64_bit? and MACOS_VERSION >= 10.6
+
+    system "./configure", *args
     system "make install"
+
+    # Don't need Gnome desktop support
+    rm_rf share+'applications'
 
     # Use a wrapper script, so rename wine to wine.bin
     # and name our startup script wine
