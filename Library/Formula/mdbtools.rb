a4c710c8c7ad27e1c1fd4117e2d373bd660c22e6
chorn <chorn@chorn.com>
2010-06-22 12:36:10 -0400

    Added mdbtools
    * gawk is used to generate the manpages.

---
+++ Library/Formula/mdbtools.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Mdbtools <Formula
+  homepage 'http://sourceforge.net/projects/mdbtools/'
+  # Last stable release won't build on OS X, but HEAD from CVS does.
+  head "cvs://:pserver:anonymous@mdbtools.cvs.sourceforge.net:/cvsroot/mdbtools:mdbtools"
+
+  depends_on 'pkg-config'
+  depends_on 'glib'
+  depends_on 'gawk' => :optional # To generate docs
+
+  def install
+    inreplace 'autogen.sh', 'libtool', 'glibtool'
+
+    system "NOCONFIGURE='yes' ACLOCAL_FLAGS='-I#{HOMEBREW_PREFIX}/share/aclocal' ./autogen.sh"
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make install"
+  end
+end
