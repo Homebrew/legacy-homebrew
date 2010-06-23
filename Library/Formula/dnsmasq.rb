b2d9cbd41220ceda67b14c590b97330820f317fe
Jeffrey Hardy <packagethief@gmail.com>
2010-06-23 00:00:26 -0400

    Updated formula: dnsmasq. Version bump to 2.55
    See http://www.thekelleys.org.uk/dnsmasq/CHANGELOG for details.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dnsmasq.rb
+++ Library/Formula/dnsmasq.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Dnsmasq <Formula
-  @url='http://www.thekelleys.org.uk/dnsmasq/dnsmasq-2.52.tar.gz'
+  @url='http://www.thekelleys.org.uk/dnsmasq/dnsmasq-2.55.tar.gz'
   @homepage='http://www.thekelleys.org.uk/dnsmasq/doc.html'
-  @md5='1bb32fffdb4f977ead607802b5d701d0'
+  @md5='b093d7c6bc7f97ae6fd35d048529232a'
 
   def install
     ENV.deparallelize
