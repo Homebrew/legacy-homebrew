51e96c0e384e0a93f511bff46e9050ac9c17f66c
Jannis Leidel <jannis@leidel.info>
2010-06-23 12:41:03 +0200

    Updated Cherokee formula to 1.0.4.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/cherokee.rb
+++ Library/Formula/cherokee.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Cherokee <Formula
-  url 'http://www.cherokee-project.com/download/1.0/1.0.2/cherokee-1.0.2.tar.gz'
+  url 'http://www.cherokee-project.com/download/1.0/1.0.4/cherokee-1.0.4.tar.gz'
   homepage 'http://www.cherokee-project.com/'
-  md5 'a6c8bdcca3e64349d537d519f09915ba'
+  md5 '24874b465abe6611ef2f2c145a840cb2'
 
   depends_on 'gettext'
 
@@ -11,7 +11,7 @@ class Cherokee <Formula
   skip_clean "var/log"
   skip_clean "var/lib/cherokee/graphs/images"
 
-  def caveats 
+  def caveats
   <<-EOS.undent
     Cherokee is setup to run with your (#{ENV['USER']}) permissions as part of the www group
     on port 80. This can be changed in the cherokee-admin but be aware the new user will need
@@ -21,7 +21,7 @@ class Cherokee <Formula
         sudo cp #{prefix}/org.cherokee.webserver.plist /System/Library/LaunchDaemons
         sudo launchctl load -w /System/Library/LaunchDaemons/org.cherokee.webserver.plist
 
-    If this is an upgrade and you already have the org.cherokee.webserver.plist loaded: 
+    If this is an upgrade and you already have the org.cherokee.webserver.plist loaded:
         sudo launchctl unload -w /System/Library/LaunchDaemons/org.cherokee.webserver.plist
         sudo cp #{prefix}/org.cherokee.webserver.plist /System/Library/LaunchDaemons
         sudo launchctl load -w /System/Library/LaunchDaemons/org.cherokee.webserver.plist
@@ -32,7 +32,7 @@ class Cherokee <Formula
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking",
            "--with-wwwuser=#{ENV['USER']}", "--with-wwwgroup=www"
     system "make install"
-    
+
     prefix.install "org.cherokee.webserver.plist"
   end
 end
