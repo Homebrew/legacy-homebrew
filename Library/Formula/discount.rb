be4c1ad005a93e1b56ace5c8b17949d36494ba0b
Peter Aronoff <telemachus@arpinum.org>
2010-06-28 06:56:44 -0400

    Update discount to 1.6.6
    From [discount's website][1.6.6]:
    
      * version 1.6.6 repairs two defects, one in the markdown compiler and one in
      `theme`:
    
        1. In `theme`, I needed to take into account the source filename
           might not have an extension when I'm making the `.html` filename.
           The old behavior was to look for a dot and put the `.html` after
           that, but _I didn't check to see if there was actually a dot there_
           before appending the `.html`!
    
           This did not work out too well if there was no dot.
    
        2. In the markdown parser, I wasn't handling escapes of the
           open square bracket inside a `[]()` construct.
           So a link like
    	   [foo\[and\]bar](does not work properly!)
           would not parse because
           my square bracket matcher would look for an additional `]`
           to match the `\[` inside the `[]` part.
    
    [1.6.6]: http://www.pell.portland.or.us/~orc/Code/discount/#1.6.6

--- Library/Formula/discount.rb
+++ Library/Formula/discount.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Discount <Formula
-  url 'http://github.com/Orc/discount/tarball/v1.6.5'
+  url 'http://github.com/Orc/discount/tarball/v1.6.6'
   homepage 'http://www.pell.portland.or.us/~orc/Code/markdown/'
-  md5 '85824f81b67649fb466fa469da5b5262'
+  md5 'c05cb804bdf5013aea2e816e5d2233d7'
 
   def install
     system "./configure.sh", "--prefix=#{prefix}", "--mandir=#{man}", "--enable-dl-tag", "--enable-pandoc-header", "--enable-superscript", "--relaxed-emphasis", "--enable-div", "--enable-alpha-list"
