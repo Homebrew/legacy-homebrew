79ca32df8d730900f441762e94b8a8b0dbf5fa4b
Adam Vandenberg <flangy@gmail.com>
2010-06-30 11:27:08 -0700

    Update ImageMagick to 6.6.2-10 and add smoke test

--- Library/Formula/imagemagick.rb
+++ Library/Formula/imagemagick.rb
@@ -20,8 +20,8 @@ def x11?
 end
 
 class Imagemagick <Formula
-  url 'ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.2-9.tar.bz2'
-  md5 '5353f5ae075d14888455f0802225bf29'
+  url 'ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.2-10.tar.bz2'
+  md5 '5eb2b6e2752d5420bd8789936d1eabcd'
   homepage 'http://www.imagemagick.org'
 
   depends_on 'jpeg'
@@ -76,5 +76,9 @@ class Imagemagick <Formula
             unless ghostscript_fonts? or ghostscript_srsly?
     return nil if s.empty?
     return s
+  end
+
+  def test
+    system "identify", "/Library/Application Support/Apple/iChat Icons/Flags/Argentina.gif"
   end
 end
