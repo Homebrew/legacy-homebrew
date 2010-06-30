808376f7442aa47540f7fc98435a8351ccec54a1
Sean Cribbs <seancribbs@gmail.com>
2010-06-30 09:04:17 -0400

    Update berkeley-db formula.

--- Library/Formula/berkeley-db.rb
+++ Library/Formula/berkeley-db.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class BerkeleyDb <Formula
-  @url='http://download.oracle.com/berkeley-db/db-4.8.24.tar.gz'
+  @url='http://download.oracle.com/berkeley-db/db-5.0.21.tar.gz'
   @homepage='http://www.oracle.com/technology/products/berkeley-db/index.html'
-  @md5='147afdecf438ff99ade105a5272db158'
+  @md5='9a749fd2e98fe15840493ddc34cc66d8'
 
   aka 'db'
 
@@ -11,15 +11,15 @@ class BerkeleyDb <Formula
     # BerkeleyDB dislikes parallel builds
     ENV.deparallelize
     ENV.O3 # takes an hour or more with link time optimisation
-    
+
     # BerkeleyDB requires you to build everything from the build_unix subdirectory
     Dir.chdir 'build_unix' do
       system "../dist/configure", "--disable-debug",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--enable-cxx",
-                          "--enable-java"
-                          
+                                  "--prefix=#{prefix}",
+                                  "--mandir=#{man}",
+                                  "--enable-cxx",
+                                  "--enable-java"
+
       system "make install"
 
       # use the standard docs location
