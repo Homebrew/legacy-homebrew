a33d396b2ba2f6eec7b6fe32b2cb5db3543ceb6d
Taybin Rutkin <taybin@penguinsounds.org>
2010-07-03 21:26:45 -0400

    add formula for SLV2
    - lv2 helper library
      http://drobilla.net/software/slv2/

---
+++ Library/Formula/slv2.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Slv2 < Formula
+  url 'http://download.drobilla.net/slv2-0.6.6.tar.bz2'
+  homepage 'http://drobilla.net/software/slv2'
+  md5 'b8e8e0e82dd04324fcd533e8acd1ce85'
+
+  depends_on 'pkg-config'
+  depends_on 'lv2core'
+  depends_on 'redland'
+  depends_on 'jack' => :optional
+
+  def install
+    system "./waf configure --prefix=#{prefix} --lv2dir=#{share}/lv2"
+    system "./waf build"
+    system "./waf install"
+  end
+end
