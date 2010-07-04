f1bfa5136821db53516034cce698476e5c33e0d8
Taybin Rutkin <taybin@penguinsounds.org>
2010-07-03 21:03:31 -0400

    add formula for lrdf
    - Lightweight RDF, depends on raptor

---
+++ Library/Formula/lrdf.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Lrdf <Formula
+  url 'http://sourceforge.net/projects/lrdf/files/liblrdf/0.4.0/liblrdf-0.4.0.tar.gz/download'
+  homepage 'http://lrdf.sourceforge.net'
+  version '0.4.0'
+  md5 '327a5674f671c4b360c6353800226877'
+
+  depends_on 'raptor'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
