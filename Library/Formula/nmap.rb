f237163c68e255732ab92d7ce528c8f48b011d30
Adam Vandenberg <flangy@gmail.com>
2010-07-03 22:37:48 -0700

    nmap - tighten up formula

--- Library/Formula/nmap.rb
+++ Library/Formula/nmap.rb
@@ -1,18 +1,16 @@
 require 'formula'
 
 class Nmap <Formula
-  @url='http://nmap.org/dist/nmap-5.21.tar.bz2'
-  @homepage='http://nmap.org/5/'
-  @md5='f77fa51d89ab27d35e5cd87bb086b858'
+  url 'http://nmap.org/dist/nmap-5.21.tar.bz2'
+  homepage 'http://nmap.org/5/'
+  md5 'f77fa51d89ab27d35e5cd87bb086b858'
 
   def install
     fails_with_llvm
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}", 
-                          "--without-zenmap"
-    
+
+    system "./configure", "--prefix=#{prefix}", "--without-zenmap"
     system "make"                      
     system "make install" # seperate steps required otherwise the build fails
   end
-
 end
