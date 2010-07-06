125cf228e5eb5832e55c63bd8923c717c8358aba
Adam Vandenberg <flangy@gmail.com>
2010-07-06 07:27:39 -0700

    Add explicit version to scons

--- Library/Formula/scons.rb
+++ Library/Formula/scons.rb
@@ -4,6 +4,7 @@ class Scons <Formula
   url 'http://prdownloads.sourceforge.net/scons/scons-2.0.0.final.0.tar.gz'
   homepage 'http://www.scons.org'
   md5 'c2e4c2700cac507caa93d4a3adbbf56f'
+  version '2.0.0'
 
   def install
     man1.install gzip('scons-time.1')
