68ee0f0156197d9d7dbf6b9c60df2f938b83b3a5
Adam Vandenberg <flangy@gmail.com>
2010-07-06 13:14:19 -0700

    Update formula to use Debian version style
    * Debian downloads have ".orig" in the tarball name. Now that
      Pathname supports this style, remove explicit versions from some brews.

--- Library/Formula/urlview.rb
+++ Library/Formula/urlview.rb
@@ -4,7 +4,7 @@ class Urlview <Formula
   url 'http://ftp.de.debian.org/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz'
   homepage 'http://packages.debian.org/unstable/misc/urlview'
   md5 '4f253c82b59136181b1f0bf218ef059d'
-  
+
   def patches
     "http://ftp.de.debian.org/debian/pool/main/u/urlview/urlview_0.9-18.1.diff.gz"
   end
@@ -14,13 +14,10 @@ class Urlview <Formula
     inreplace 'urlview.c',
       '#define DEFAULT_COMMAND "/etc/urlview/url_handler.sh %s"',
       '#define DEFAULT_COMMAND "open %s"'
-    configure_args = [
-        "--prefix=#{prefix}",
-        "--disable-debug",
-        "--disable-dependency-tracking",
-    ]
-    system "./configure", *configure_args
-    man1.mkpath()
+
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    man1.mkpath
     system "make install"
   end
 end
