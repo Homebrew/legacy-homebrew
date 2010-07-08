5825cb3460826bb029fa0d440837114481f9b977
Devin Weaver <suki@tritarget.org>
2010-07-08 12:59:22 -0400

    New Formula: wtf
        The wtf utility displays the expansion of the acronyms specified on
        the command line.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/wtf.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Wtf <Formula
+  url 'http://mirror.aarnet.edu.au/pub/FreeBSD/ports/distfiles/wtf-20080926.tar.gz'
+  homepage 'http://cvsweb.netbsd.org/bsdweb.cgi/src/games/wtf/'
+  md5 '5f3ad46d90a71d30ecf281dbd58e9d20'
+
+  def install
+    inreplace %w[wtf wtf.6], "/usr/share", share
+    bin.install "wtf"
+    (man+"man6").install "wtf.6"
+    (share+"misc").install %w[acronyms acronyms.comp]
+  end
+end
