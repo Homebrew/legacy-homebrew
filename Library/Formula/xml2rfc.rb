661da19e55400e5f5420d43f589e9c14e0730f69
Adam Vandenberg <flangy@gmail.com>
2010-07-09 12:37:42 -0700

    Clean up xml2rfc formula.

--- Library/Formula/xml2rfc.rb
+++ Library/Formula/xml2rfc.rb
@@ -2,20 +2,11 @@ require 'formula'
 
 class Xml2rfc <Formula
   url 'http://xml.resource.org/authoring/xml2rfc-1.35.tgz'
-  md5 '7ffb973fee55318b1bd0fd77a903d2e6'
-  head 'https://svn.tools.ietf.org/svn/tools/xml2rfc/trunk'
   homepage 'http://xml.resource.org/'
+  md5 '7ffb973fee55318b1bd0fd77a903d2e6'
 
-  # http://github.com/mxcl/homebrew/issues/#issue/87
-  depends_on :subversion if MACOS_VERSION < 10.6 and ARGV.include? '--HEAD'
-
-  def download_strategy
-    if ARGV.include? '--HEAD'
-      SubversionDownloadStrategy
-    else
-      CurlDownloadStrategy
-    end
-  end
+  head 'http://svn.tools.ietf.org/svn/tools/xml2rfc/trunk',
+    :using => StrictSubversionDownloadStrategy
 
   def install
     %w[xml2rfc xml2sgml].each do |f|
