0a5fc89f0734e90e80e3819508285847ed09c523
Adam Vandenberg <flangy@gmail.com>
2010-07-10 14:05:19 -0700

    Update libiconv with extra encodings.
    Add Mac-specific encodings to libiconv.

--- Library/Formula/libiconv.rb
+++ Library/Formula/libiconv.rb
@@ -2,11 +2,25 @@ require 'formula'
 
 class Libiconv <Formula
   url 'http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.13.1.tar.gz'
-  md5 '7ab33ebd26687c744a37264a330bbe9a'
   homepage 'http://www.gnu.org/software/libiconv/'
+  md5 '7ab33ebd26687c744a37264a330bbe9a'
+
+  def keg_only?
+    :provided_by_osx
+  end
+
+  def patches
+    { :p1 => [
+      'http://svn.macports.org/repository/macports/trunk/dports/textproc/libiconv/files/patch-Makefile.devel',
+      'http://svn.macports.org/repository/macports/trunk/dports/textproc/libiconv/files/patch-utf8mac.diff',
+      'http://gist.github.com/raw/355571/2fac6d8b7471e4787748f6335ff062908ec0fdc2/gistfile1.txt']
+    }
+  end
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug"
+    ENV.j1
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}", "--enable-extra-encodings"
+    system "make -f Makefile.devel"
     system "make install"
   end
 end
