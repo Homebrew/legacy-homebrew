3add05601f9698972dbcbc671bbe64f2c99a94b6
Adam Vandenberg <flangy@gmail.com>
2010-07-13 14:20:52 -0700

    Add command "brew --env"
    "brew --env" will set up a build environment and then dump certain ENV
    variables (CC, CXX, LD, CFLAGS, CXXFLAGS, MAKEFLAGS).
    
    If any of CC, CXX, LD are symlinks, now also output the target compiler.
    (Typically these will be symlinks from eg /usr/bin/cc to /usr/bin/gcc-4.2).
    
    This is a diagnostic command which may be merged into --config, turned
    into an external command, or removed if it doesn't turn out to be useful.

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -254,7 +254,19 @@ end
 def dump_build_env env
   puts "\"--use-llvm\" was specified" if ARGV.include? '--use-llvm'
 
-  %w[ CC CXX LD CFLAGS CXXFLAGS CPPFLAGS LDFLAGS MACOSX_DEPLOYMENT_TARGET MAKEFLAGS PKG_CONFIG_PATH
+  %w[ CC CXX LD ].each do |k|
+    value = env[k]
+    if value
+      results = value
+      if File.exists? value and File.symlink? value
+        target = Pathname.new(value)
+        results += " => #{target.dirname+target.readlink}"
+      end
+      puts "#{k}: #{results}"
+    end
+  end
+
+  %w[ CFLAGS CXXFLAGS CPPFLAGS LDFLAGS MACOSX_DEPLOYMENT_TARGET MAKEFLAGS PKG_CONFIG_PATH
       HOMEBREW_DEBUG HOMEBREW_VERBOSE HOMEBREW_USE_LLVM HOMEBREW_SVN ].each do |k|
     value = env[k]
     puts "#{k}: #{value}" if value
