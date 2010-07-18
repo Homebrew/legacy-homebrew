ca56a9b819b7fae0ef813c0a387b28a2ef147795
Richard Hurt <rnhurt@gmail.com>
2010-07-18 09:18:21 -0400

    small cleanup

--- Library/Formula/geany.rb
+++ Library/Formula/geany.rb
@@ -5,12 +5,10 @@ class Geany <Formula
   homepage 'http://www.geany.org/Main/HomePage'
   md5 '727cec2936846850bb088b476faad5f2'
 
-# depends_on 'cmake'
   depends_on 'gettext' 
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
-#   system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 end
