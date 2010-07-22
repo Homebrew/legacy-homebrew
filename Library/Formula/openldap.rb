4b42de8148a2faa5a1389a2dc9fbb8ca141d87f0
Adam Vandenberg <flangy@gmail.com>
2010-07-21 17:00:47 -0700

    Minor update for openldap

--- Library/Formula/openldap.rb
+++ Library/Formula/openldap.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Openldap <Formula
-  url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.21.tgz'
+  url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.23.tgz'
   homepage 'http://www.openldap.org/software/'
-  md5 'e7128c57b2bacd940e8906057c94ff26'
+  md5 '90150b8c0d0192e10b30157e68844ddf'
 
  depends_on 'berkeley-db'
 
