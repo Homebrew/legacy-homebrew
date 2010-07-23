7658f85864506c54f33214ee265c8e77e3d72b18
Lin Jen-Shin <godfat@godfat.org>
2010-07-23 17:44:00 +0800

    [hs-cabal-install.rb] Added haskell cabal install package manager

---
+++ Library/Formula/hs-cabal-install.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class HsCabalInstall <Formula
+  url 'http://hackage.haskell.org/packages/archive/cabal-install/0.8.2/cabal-install-0.8.2.tar.gz'
+  homepage 'http://hackage.haskell.org/package/cabal-install'
+  md5 '4abd0933dff361ff69ee9288a211e4e1'
+
+  aka 'cabal'
+  aka 'hs-cabal'
+
+  depends_on 'ghc'
+  depends_on 'hs-http'
+  depends_on 'hs-network'
+  depends_on 'hs-zlib'
+
+  def install
+    system "runhaskell Setup.hs configure --prefix #{prefix} --libdir #{prefix}/lib/haskell"
+    system 'runhaskell Setup.hs build'
+    system 'runhaskell Setup.hs install'
+  end
+end
