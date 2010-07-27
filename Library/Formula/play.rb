aa76115e680d4ed8f9c01e60cf2e9278a80dc55d
Adam Vandenberg <flangy@gmail.com>
2010-07-27 09:45:05 -0700

    Update play to 1.0.3.1

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Play <Formula
-  url 'http://download.playframework.org/releases/play-1.0.3.zip'
+  url 'http://download.playframework.org/releases/play-1.0.3.1.zip'
   homepage 'http://www.playframework.org/'
-  md5 '25d3197c83cc60bde9281d519fae0735'
+  md5 'b704cef8f2d8a68088ed0f80a4c4abe9'
 
   def install
     rm Dir['*.bat']
