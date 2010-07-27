538ae7d73fcc95ca21db56bc96115926016f2c86
Alexis Hildebrandt <afh@surryhill.net>
2010-07-28 00:55:54 +0200

    Update Formula: ledger
    Remove obsolete utfcpp sub-formula for ledger head builds,
    since it's now being referenced as a git submodule.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ledger.rb
+++ Library/Formula/ledger.rb
@@ -1,14 +1,5 @@
 require 'formula'
 
-class Utf8cpp <Formula
-  url 'http://sourceforge.net/projects/utfcpp/files/utf8cpp_2x/Release%202.2.4/utf8_v2_2_4.zip/download'
-  homepage 'http://utfcpp.sourceforge.net/'
-  md5 'b30e1f7087aeca5ad38f7966aaf5f739'
-  # Use version 2.2.4 even for ledger HEAD install
-  version '2.2.4'
-  #head 'http://github.com/jwiegley/utfcpp/'
-end
-
 class Ledger <Formula
   url 'ftp://ftp.newartisans.com/pub/ledger/ledger-2.6.2.tar.gz'
   md5 'b2e6fa98e7339d1e130b1ea9af211c0f'
@@ -26,8 +17,6 @@ class Ledger <Formula
     unless 'HEAD' == @version
       system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     else
-      utfcpp = Pathname.new(Dir.pwd)+'lib/utfcpp'
-      Utf8cpp.new.brew { utfcpp.install Dir['*'] }
       # gmp installs x86_64 only
       inreplace 'acprep', "'-arch', 'i386', ", "" if Hardware.is_64_bit?
       system "./acprep -j#{Hardware.processor_count} opt make -- --prefix=#{prefix}"
