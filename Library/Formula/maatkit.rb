d0b92d3f97b7a3defcd94e334dc9d75077797b33
kevinbin <hongbin119@gmail.com>
2010-07-28 01:09:04 +0800

    update maatkit to version 6652
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/maatkit.rb
+++ Library/Formula/maatkit.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Maatkit <Formula
-  url 'http://maatkit.googlecode.com/files/maatkit-6070.tar.gz'
+  url 'http://maatkit.googlecode.com/files/maatkit-6652.tar.gz'
   homepage 'http://code.google.com/p/maatkit/'
-  md5 '313e7b01c0a718a84055112d0060e45e'
+  md5 'b21a7ee121aa207279761121e00d589a'
 
   depends_on 'DBD::mysql' => :perl
 
