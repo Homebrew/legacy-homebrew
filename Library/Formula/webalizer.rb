e787e7d7b992ab7f97527e9cf5460d14c7e2f533
Richard Hurt <rnhurt@gmail.com>
2010-07-27 20:31:50 -0400

    New Formula: webalizer

---
+++ Library/Formula/webalizer.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Webalizer <Formula
+  url 'ftp://ftp.mrunix.net/pub/webalizer/webalizer-2.21-02-src.tar.bz2'
+  homepage 'http://www.mrunix.net/webalizer/'
+  md5 '29af2558a5564493df654b6310b152e7'
+  version '2.21-02'
+
+  depends_on 'libpng'
+  depends_on 'libgd'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+#   system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
