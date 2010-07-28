8b87addd576ec0487f48c11c2eba309b44a60348
Mark A. Matienzo <mark@matienzo.org>
2010-07-27 20:13:26 -0400

    New Formula: bcwipe
    The BCWipe software is intended to give you a confidence that your deleted
    files cannot be recovered by an intruder. BCWipe repeatedly overwrites special
    patterns to the files to be destroyed. BCWipe for UNIX offers several wiping
    schemas: U.S. DoD 5200.22M standard, German BCI/VSITR 7-pass scheme, U.S. DoE
    3-pass scheme, Bruce Schneier's 7-pass scheme, Peter Gutmann's 35-pass scheme,
    as well as user defined schemes.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/bcwipe.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Bcwipe <Formula
+  url 'http://www.jetico.com/linux/BCWipe-1.9-6.tar.gz'
+  homepage 'http://www.jetico.com/linux/bcwipe-help/wu_intro.htm'
+  md5 'abe1ddf92284d585acf43d7e2d8b1593'
+
+  def install
+    ENV.gcc_4_0_1
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
