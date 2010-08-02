68dd49de7e449b97221dc089e5f92137df4286d0
David Höppner <0xffea@gmail.com>
2010-08-02 13:59:26 +0200

    pcre: update version 8.10

--- Library/Formula/pcre.rb
+++ Library/Formula/pcre.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Pcre <Formula
-  url 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.02.tar.bz2'
+  url 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.10.tar.bz2'
   homepage 'http://www.pcre.org/'
-  md5 '27948c1b5f5c1eabc23cba1ebe4c316f'
+  md5 '780867a700e9d4e4b9cb47aa5453e4b2'
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -12,6 +12,7 @@ class Pcre <Formula
                           "--enable-unicode-properties",
                           "--enable-pcregrep-libz",
                           "--enable-pcregrep-libbz2"
+    system "make test"
     system "make install"
   end
 end
