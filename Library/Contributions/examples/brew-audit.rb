f207fd0ef587d76b32e70a02903293c3affc727f
Adam Vandenberg <flangy@gmail.com>
2010-08-07 15:23:13 -0700

    brew audit - trailing whitespace detection

--- Library/Contributions/examples/brew-audit.rb
+++ Library/Contributions/examples/brew-audit.rb
@@ -8,24 +8,31 @@ end
 
 ff.each do |f|
   text = ""
+  problems = []
+
   File.open(f.path, "r") { |afile| text = afile.read }
 
-  problems = []
-  if /# depends_on 'cmake'/ =~ text
+  if text =~ /# depends_on 'cmake'/
     problems << " * Commented cmake support found."
   end
 
-  if /\?use_mirror=/ =~ text
+  if text =~ /\?use_mirror=/
     problems << " * Remove 'use_mirror' from url."
   end
 
   # 2 (or more, if in an if block) spaces before depends_on, please
-  if /^\ ?depends_on/ =~ text
+  if text =~ /^\ ?depends_on/
     problems << " * Check indentation of 'depends_on'."
   end
 
-  if /(#\{\w+\s*\+\s*['"][^}]+\})/ =~ text
+  if text =~ /(#\{\w+\s*\+\s*['"][^}]+\})/
     problems << " * Try not to concatenate paths in string interpolation:\n   #{$1}"
+  end
+
+  # Don't complain about spaces in patches
+  split_patch = (text.split("__END__")[0]).strip()
+  if split_patch =~ /[ ]+$/
+    problems << " * Trailing whitespace was found."
   end
 
   aliases = Formula.aliases
