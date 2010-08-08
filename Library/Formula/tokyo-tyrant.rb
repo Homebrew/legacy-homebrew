8b7a062be72c24a8eec85bd1773654ad41af7d8c
Adam Vandenberg <flangy@gmail.com>
2010-08-07 17:22:30 -0700

    Update urls/versions for tokyo/kyoto formulae.

--- Library/Formula/tokyo-tyrant.rb
+++ Library/Formula/tokyo-tyrant.rb
@@ -1,17 +1,21 @@
 require 'formula'
 
 class TokyoTyrant <Formula
-  url 'http://1978th.net/tokyotyrant/tokyotyrant-1.1.40.tar.gz'
-  homepage 'http://1978th.net/tokyotyrant/'
-  md5 'cc9b7f0c6764d37700ab43d29a5c6048'
+  url 'http://fallabs.com/tokyotyrant/tokyotyrant-1.1.41.tar.gz'
+  homepage 'http://fallabs.com/tokyotyrant/'
+  md5 'a47e58897bd1cbbac173d5a66cc32ae3'
 
   depends_on 'tokyo-cabinet'
   depends_on 'lua' unless ARGV.include? "--no-lua"
 
+  def options
+    [["--no-lua", "Disable Lua support (and don't force Lua install.)"]]
+  end
+
   def install
     args = ["--prefix=#{prefix}"]
     args << "--enable-lua" unless ARGV.include? "--no-lua"
-    
+
     system "./configure", *args
     system "make"
     system "make install"
