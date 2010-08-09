7eec288d9f9561cc3e7d26cf984b18504da14df0
David Höppner <0xffea@gmail.com>
2010-08-09 13:11:05 +0200

    atomicparsley: md5 did change (why?) #fixes2106

--- Library/Formula/atomicparsley.rb
+++ Library/Formula/atomicparsley.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Atomicparsley <Formula
   url 'http://bitbucket.org/wez/atomicparsley/get/0.9.3.tar.bz2'
   homepage 'http://bitbucket.org/wez/atomicparsley/overview/'
-  md5 'a405fc4b7029ad1ea104dba82dff4b63'
+  md5 '5ab12e2e8b201f3341a37628973722e0'
 
   def install
     system "./autogen.sh"
