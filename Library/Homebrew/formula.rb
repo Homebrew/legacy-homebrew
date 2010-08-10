2a77cee10ed11343c7eca228c681ff4ddd481564
Adam Vandenberg <flangy@gmail.com>
2010-08-09 21:33:30 -0700

    fails_with_llvm will switch to GCC 4.2 by default

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -291,6 +291,16 @@ class Formula
     opoo "LLVM was requested, but this formula is reported as not working with LLVM:"
     puts msg
     puts "Tested with LLVM build #{build}" unless build == nil
+    puts
+
+    if ARGV.force?
+      puts "Continuing anyway. If this works, let us know so we can update the\n"+
+           "formula to remove the warning."
+    else
+      puts "Continuing with GCC 4.2 instead.\n"+
+           "(Use `brew install --force ...` to force use of LLVM.)"
+      ENV.gcc_4_2
+    end
   end
 
 protected
