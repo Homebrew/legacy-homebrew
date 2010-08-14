6870e96998d5ce71cc944027fc860d226df170b2
Stein Magnus Jodal <stein.magnus@jodal.no>
2010-08-14 13:27:55 +0200

    Update gst-ffmpeg to 0.10.11
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gst-ffmpeg.rb
+++ Library/Formula/gst-ffmpeg.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class GstFfmpeg <Formula
-  url 'http://gstreamer.freedesktop.org/src/gst-ffmpeg/gst-ffmpeg-0.10.10.tar.bz2'
+  url 'http://gstreamer.freedesktop.org/src/gst-ffmpeg/gst-ffmpeg-0.10.11.tar.bz2'
   homepage 'http://gstreamer.freedesktop.org/'
-  md5 '447292deff5f3748444e6a5fba41da29'
+  md5 '0d23197ba7ac06ea34fa66d38469ebe5'
 
   depends_on 'gst-plugins-base'
+  # ffmpeg is bundled with gst-ffmpeg
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
