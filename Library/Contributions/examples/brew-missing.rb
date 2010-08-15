4931b6561308c4ff23e2c7c9d8a8001bbfba7e27
Adam Vandenberg <flangy@gmail.com>
2010-08-15 15:23:43 -0700

    Tweak brew missing

--- Library/Contributions/examples/brew-missing.rb
+++ Library/Contributions/examples/brew-missing.rb
@@ -11,11 +11,8 @@ def main
       f_deps = FormulaInstaller.expand_deps(f).collect{|g| g.name}.uniq
       next if f_deps.empty?
 
-      missing_deps = []
-      f_deps.each do |dep_name|
-        unless Formula.factory(dep_name).installed? or outdated.include?(dep_name)
-          missing_deps << dep_name
-        end
+      missing_deps = f_deps.reject do |dep_name|
+        Formula.factory(dep_name).installed? or outdated.include?(dep_name)
       end
 
       puts "#{f.name}: #{missing_deps.join(', ')}" unless missing_deps.empty?
