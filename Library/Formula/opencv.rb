273d642c99ccf294749c01d3502a9b9d85cf099f
Christopher Denter <dennda -O- the-space-station -o- com>
2010-08-15 20:28:43 +0200

    opencv: Use revision 3478.
    Seems to work good. Memory leak from 2.1.0 is fixed. Some later revisions don't compile on OSX, though
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/opencv.rb
+++ Library/Formula/opencv.rb
@@ -1,10 +1,14 @@
 require 'formula'
 
 class Opencv <Formula
-  # Use head, don't install 2.1.0 due to a *massive* memory leak:
+  # Don't use stable 2.1.0 due to a massive memory leak:
   # https://code.ros.org/trac/opencv/ticket/253
-  head 'https://code.ros.org/svn/opencv/trunk/opencv', :using => :svn
+  url 'https://code.ros.org/svn/opencv/trunk/opencv', :using => :svn, :revision => '3478'
+  version "2.1.1-pre"
   homepage 'http://opencv.willowgarage.com/wiki/'
+
+  # NOTE: Head builds past the revision above may break on OS X
+  head 'https://code.ros.org/svn/opencv/trunk/opencv', :using => :svn
 
   depends_on 'cmake'
   depends_on 'pkg-config'
