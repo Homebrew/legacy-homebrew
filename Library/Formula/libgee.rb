01a16731587a3f6f88f1882c3ddaf3a5f4ba86f2
David Höppner <0xffea@gmail.com>
2010-08-23 15:16:10 +0200

    libgee: update version to 0.5.2

--- Library/Formula/libgee.rb
+++ Library/Formula/libgee.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libgee <Formula
-  url 'http://download.gnome.org/sources/libgee/0.5/libgee-0.5.1.tar.bz2'
+  url 'http://download.gnome.org/sources/libgee/0.5/libgee-0.5.2.tar.bz2'
   homepage 'http://live.gnome.org/Libgee'
-  md5 '59789b5b266beadfb8b51bf2c96211b3'
+  md5 'fc5a36eb5f61154a1456cbb8b1798e41'
 
   depends_on 'vala'
 
