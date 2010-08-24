b46eb3bfcfcaba307eb027e0e95e60602860244e
Markus Heberling <markus@tisoft.de>
2010-08-24 06:45:51 +0200

    swig 2.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/swig.rb
+++ Library/Formula/swig.rb
@@ -1,16 +1,14 @@
 require 'formula'
 
 class Swig <Formula
-  url 'http://downloads.sourceforge.net/swig/swig-1.3.40.tar.gz'
+  url 'http://downloads.sourceforge.net/swig/swig-2.0.0.tar.gz'
   homepage 'http://www.swig.org/'
-  md5 '2df766c9e03e02811b1ab4bba1c7b9cc'
+  md5 '36ee2d9974be46a9f0a36460af928eb9'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug", "--disable-dependency-tracking",
-                          # turns prefix/share/swig/1.3.40 into prefix/share/swig
-                          # as versioned dirs are redundant with Homebrew
-                          "--with-swiglibdir=#{share}/swig"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
     system "make install"
   end
 end
