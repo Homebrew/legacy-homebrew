f139326a26c0245a187d09bfdd483dedfc158ed9
Robert Boone <robo4288@gmail.com>
2010-09-01 07:44:32 -0700

    Update to the August build of rakudo-star

--- Library/Formula/rakudo-star.rb
+++ Library/Formula/rakudo-star.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class RakudoStar <Formula
-  url 'http://cloud.github.com/downloads/rakudo/star/rakudo-star-2010.07.tar.gz'
+  url 'http://cloud.github.com/downloads/rakudo/star/rakudo-star-2010.08.tar.gz'
   homepage 'http://rakudo.org/'
   md5 '3be6d0f4f9d8d1143b6c25768e8a7342'
 
