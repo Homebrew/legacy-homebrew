a392985b0034c3df8405111ff0ade7edf610e060
Reid Burke <me@reidburke.com>
2010-09-01 17:23:05 -0700

    Patch mspdebug to talk to a TI LaunchPad.

--- Library/Formula/mspdebug.rb
+++ Library/Formula/mspdebug.rb
@@ -10,10 +10,42 @@ class Mspdebug <Formula
   depends_on 'libusb-compat'
   depends_on 'libelf'
 
+  def patches
+    # Without this patch, mspdebug hangs when trying
+    # to open a connection to a TI LaunchPad.
+    # see: http://e2e.ti.com/support/microcontrollers/msp43016-bit_ultra-low_power_mcus/f/166/p/18554/212659.aspx#212659
+    DATA
+  end
+
   def install
     ENV["PREFIX"] = prefix
-    # Don't assume MacPorts:
-    inreplace "Makefile", "/opt", "/usr"
+    inreplace "Makefile", "/opt", "/usr" # Don't assume MacPorts. (May not be necessary.)
     system "make install"
   end
+
+  def caveats
+    <<-EOS.undent
+        If you're using a RF2500-like device like the TI LaunchPad, you must
+        install a codeless kext for the device to be recognized by mspdebug.
+
+        More information is available at http://mspdebug.sourceforge.net/download.html.
+    EOS
+  end
 end
+
+__END__
+diff --git a/rf2500.c b/rf2500.c
+index ac27dcd..70a69b8 100644
+--- a/rf2500.c
++++ b/rf2500.c
+@@ -212,10 +212,5 @@ transport_t rf2500_open(const char *devpath)
+ 		return NULL;
+ 	}
+ 
+-	/* Flush out lingering data */
+-	while (usb_bulk_read(tr->handle, USB_FET_IN_EP,
+-			     buf, sizeof(buf),
+-			     100) >= 0);
+-
+ 	return (transport_t)tr;
+ }
