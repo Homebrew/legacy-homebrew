7008975179324c6621a98849ed3b55195d9eae45
Nicholas Young <nicholas@nicholaswyoung.com>
2010-09-05 14:58:54 -0500

    Update libtool formula.

--- Library/Formula/libtool.rb
+++ Library/Formula/libtool.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libtool <Formula
   url 'http://ftp.gnu.org/gnu/libtool/libtool-2.2.8.tar.gz'
   homepage 'http://www.gnu.org/software/libtool/'
-  md5 '217e9ec3e3cbdf891cbd4772140964a1'
+  md5 'cad2a7188242bc8dbab0645532ae3d6f'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
