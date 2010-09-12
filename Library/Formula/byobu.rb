8f4042438dc42af08825bdcecb8b2878af667658
John Griffin <johnog@gmail.com>
2010-09-12 17:17:00 +0100

    Add screen enhancement byobu from https://launchpad.net/byobu

---
+++ Library/Formula/byobu.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Byobu <Formula
+  url 'http://launchpad.net/byobu/trunk/3.4/+download/byobu_3.4.orig.tar.gz'
+  homepage 'http://launchpad.net/byobu'
+  md5 '694c1663f178517b995ec9ca574fc463'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
