6e95b9c7af6ce0a1a3b22b957d270be8c35adcf3
Leandro Silva <leandrodoze@gmail.com>
2010-09-15 02:29:48 -0300

    really changed the caveats

--- Library/Formula/mono.rb
+++ Library/Formula/mono.rb
@@ -22,12 +22,17 @@ class Mono <Formula
         http://www.go-mono.com/mono-downloads/download.html
 
     After installation, if you want to make it the default installation to the system, you
-    can do this by:
-        cd /Library/Frameworks/Mono.framework/Versions
-        sudo ln -s #{prefix} brew
-        sudo rm Current
-        sudo ln -s brew Current
-        sudo ln -s /Library/Frameworks/Mono.framework/Libraries/mono /usr/lib/mono
+    can do this by setting these environment variables:
+      MONO_PREFIX=#{prefix}
+      GNOME_PREFIX=/usr
+      export LD_LIBRARY_PATH=$MONO_PREFIX/lib:$LD_LIBRARY_PATH
+      export C_INCLUDE_PATH=$MONO_PREFIX/include:$GNOME_PREFIX/include
+      export ACLOCAL_PATH=$MONO_PREFIX/share/aclocal
+      export PKG_CONFIG_PATH=$MONO_PREFIX/lib/pkgconfig:$GNOME_PREFIX/lib/pkgconfig
+      export PATH=$MONO_PREFIX/bin:$PATH
+      
+    It doesn't affect you system. When you want rollback to previous Mono, just remove (or change)
+    these environment variables.
     EOS
   end
 end
