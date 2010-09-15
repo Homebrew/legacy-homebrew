1f6191ea6b78aa69bf8ce39a427060e3c557ef18
Georg Kreimer <gkreimer@gkreimer.local>
2010-09-15 12:01:04 +0200

    Updating ImageMagick formula to 6.6.4.2

--- Library/Formula/imagemagick.rb
+++ Library/Formula/imagemagick.rb
@@ -24,8 +24,8 @@ def x11?
 end
 
 class Imagemagick <Formula
-  url 'ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.4-0.tar.bz2'
-  md5 '7d302986298855b0d5cbdd73d3dacc15'
+  url 'ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.4-2.tar.bz2'
+  md5 '0ecd0a4a3667d723bb7b1114515fcc95'
   homepage 'http://www.imagemagick.org'
 
   depends_on 'jpeg'
