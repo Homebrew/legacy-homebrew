6111a9d5a290fe53983460c5d22011a1bf64576d
Lee Packham <lpackham@leenux.org.uk>
2010-09-16 16:56:47 +0100

    Remove commented out line

--- Library/Formula/tf.rb
+++ Library/Formula/tf.rb
@@ -12,7 +12,6 @@ class Tf <Formula
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 end
