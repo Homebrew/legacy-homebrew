e5df1828ad18f008844706639170a8051cd33302
Charlie Sharpsteen <source@sharpsteen.net>
2010-09-20 17:46:08 -0700

    Altered SIP formula to install from Mercurial repos
    Riverbank has a nasty habit of pulling old tarballs whenever new releases are
    made.  New releases are made fairly often--- this leads to a state where the
    PyQT and SIP formulae are chronically broken due to outdated tarball links.
    
    This changeset patches the SIP formula to install from the Mercurial repository
    for SIP located at:
    
      http://www.riverbankcomputing.co.uk/hg/sip
    
    By default this formula installs an archived tarball from a tag corresponding to
    the released version of SIP.  Tarballs are fetched using HTTP.
    
    If the user has Mercurial installed, bleeding-edge versions can be installed by
    specifying the --HEAD switch.

--- Library/Formula/sip.rb
+++ Library/Formula/sip.rb
@@ -1,16 +1,21 @@
 require 'formula'
 
-# Note: this project doesn't save old releases, so it breaks often as
-# downloads disappear.
-
 class Sip <Formula
-  url 'http://www.riverbankcomputing.co.uk/static/Downloads/sip4/sip-4.11.1.tar.gz'
+  url 'http://www.riverbankcomputing.co.uk/hg/sip/archive/4.11.1.tar.gz'
+  md5 'dbafd7101a4e7caee6f529912a1356e5'
+  head 'http://www.riverbankcomputing.co.uk/hg/sip'
   homepage 'http://www.riverbankcomputing.co.uk/software/sip'
-  md5 'd32ae4dfea42ebf37419a0ec28b597de'
+
+  if ARGV.include? "--HEAD"
+    def download_strategy
+      MercurialDownloadStrategy
+    end
+  end
 
   def install
     # Force building against System python, because we need a Framework build.
     # See: http://github.com/mxcl/homebrew/issues/issue/930
+    system "/usr/bin/python", "build.py", "prepare"
     system "/usr/bin/python", "configure.py",
                               "--destdir=#{lib}/python",
                               "--bindir=#{bin}",
