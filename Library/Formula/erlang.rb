797f014cc4cfd9f4a5c2ca79a6930796d9a91bd8
Adam Vandenberg <flangy@gmail.com>
2010-09-22 12:40:52 -0700

    erlang - add llvm # that fails

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -34,7 +34,7 @@ class Erlang <Formula
 
   def install
     ENV.deparallelize
-    fails_with_llvm "see http://github.com/mxcl/homebrew/issues/issue/120"
+    fails_with_llvm "See http://github.com/mxcl/homebrew/issues/issue/120", :build => 2326
 
     # If building from GitHub, this step is required (but not for tarball downloads.)
     system "./otp_build autoconf" if File.exist? "otp_build"
