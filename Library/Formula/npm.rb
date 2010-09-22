58c9eb3b2f852f4fab3ada11a59a907e6588db30
Adam Vandenberg <flangy@gmail.com>
2010-09-22 09:36:41 -0700

    revert npm to 0.2.1; someone fix for 0.2.2 thx

--- Library/Formula/npm.rb
+++ Library/Formula/npm.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Npm <Formula
-  url 'http://github.com/isaacs/npm/tarball/v0.2.2'
+  url 'http://github.com/isaacs/npm/tarball/v0.2.1'
   homepage 'http://github.com/isaacs/npm'
-  md5 'c61aaff07b6b66c3f4db26076dc0ec2d'
+  md5 '345410d5d2ad3fb85316649b0d731144'
   head 'git://github.com/isaacs/npm.git'
 
   depends_on 'node'
