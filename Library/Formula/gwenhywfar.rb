9fb0f57663dea75c89d6287003ce33026392fcbd
Jens Abraham <Jens_Abraham@gmx.de>
2010-09-22 18:33:06 +0200

    updated gwenhywfar formula to version 4.0.1 (builds without guis)

--- Library/Formula/gwenhywfar.rb
+++ Library/Formula/gwenhywfar.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gwenhywfar <Formula
-  url 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=31&file=01&dummy=gwenhywfar-3.11.3.tar.gz'
+  url 'http://www.aquamaniac.de/sites/download/download.php?package=01&release=54&file=01&dummy=gwenhywfar-4.0.1.tar.gz'
   homepage 'http://gwenhywfar.sourceforge.net/'
-  md5 '9ab62d881a0f39d4b07ea0badff7201f'
+  md5 '513ea7b5b22edf512fa7d825ef544954'
 
   depends_on 'pkg-config'
   depends_on 'gettext'
@@ -11,7 +11,7 @@ class Gwenhywfar <Formula
 
   def install
     fails_with_llvm "llvm results in a sigsegfault during compile"
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--with-guis=''", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
 end
