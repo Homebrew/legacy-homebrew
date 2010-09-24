500967397d13ffb1324110c87a97d4c812056a12
Tianyi Cui <tianyicui@gmail.com>
2010-09-24 22:45:47 +0800

    grep: bump version to 2.7

--- Library/Formula/grep.rb
+++ Library/Formula/grep.rb
@@ -2,26 +2,21 @@ require 'formula'
 
 class Grep <Formula
   homepage 'http://www.gnu.org/software/grep/'
-  url 'http://mirror.its.uidaho.edu/pub/gnu/grep/grep-2.5.4.tar.bz2'
-  md5 '5650ee2ae6ea4b39e9459d7d0585b315'
-
-  depends_on 'gettext'
-  depends_on 'pcre'
+  url 'http://ftp.gnu.org/gnu/grep/grep-2.7.tar.gz'
+  md5 'e848f07e3e79aa7899345d17c7e4115e'
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--disable-nls",
                           "--prefix=#{prefix}",
                           "--infodir=#{info}",
                           "--mandir=#{man}"
 
-    # Configure gives me 2 copies of -lpcre and no -lintl, so fix that
+    # Configure gives me no -liconv, so fix that
     inreplace "src/Makefile" do |s|
-      s.change_make_var! "LIBS", "-lintl -lpcre"
+      s.change_make_var! "LIBS", "-liconv -lpcre"
     end
 
-    system "make"
     system "make install"
   end
 end
