bb3bd9dac74d6304ee4ebec23a3493a50fb1440c
Jon Parise <jon@indelible.org>
2010-09-25 00:15:58 -0700

    protobuf: optionally install the Python package
    Adding a new --install-python option that installs the Python package that's
    included with the protobuf distribution.  This requires a Homebrew-built
    Python to work because we use its Cellar'ed site-packages directory to host
    the google.protobuf (.egg) package.
    
    The setup.py script also requires setuptools or distribute to be installed in
    order to run.  I haven't included an explcit runtime check for those, however.

--- Library/Formula/protobuf.rb
+++ Library/Formula/protobuf.rb
@@ -5,6 +5,12 @@ class Protobuf <Formula
   sha1 'db0fbdc58be22a676335a37787178a4dfddf93c6'
   homepage 'http://code.google.com/p/protobuf/'
 
+  def options
+      [
+        ["--install-python", "Install Python protobuf package (requires Homebrew-built Python)"],
+      ]
+  end
+
   def install
     fails_with_llvm
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
@@ -12,5 +18,18 @@ class Protobuf <Formula
                           "--with-zlib"
     system "make"
     system "make install"
+
+    if ARGV.include? "--install-python"
+      python = Formula.factory("python")
+      unless python.installed?
+        onoe "The \"--install-python\" option requires a Homebrew-built Python."
+        exit 99
+      end
+
+      cd 'python' do
+        # Note: setup.py currently requires setuptools (or distribute)
+        system "#{python.bin}/python", "setup.py", "install"
+      end
+    end
   end
 end
