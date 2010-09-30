3e7a07b9c2642a08691a1260986de62e97ff4f84
MinRK <benjaminrk@gmail.com>
2010-09-30 01:20:45 -0700

    zeromq universal build with "--universal"
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/zeromq.rb
+++ Library/Formula/zeromq.rb
@@ -5,14 +5,43 @@ class Zeromq <Formula
   homepage 'http://www.zeromq.org/'
   md5 'eadda72ecc5bcfa50a521436e6d92252'
 
+  def options
+    [['--universal', 'Build as a Universal Intel binary.']]
+  end
+
+  def build_fat
+    # make 32-bit
+    arch = "-arch i386"
+    system "CFLAGS=\"$CFLAGS #{arch}\" CXXFLAGS=\"$CXXFLAGS #{arch}\" ./configure --disable-dependency-tracking --prefix=#{prefix}"
+    system "make"
+    system "mv src/.libs src/libs-32"
+    system "make clean"
+
+    # make 64-bit
+    arch = "-arch x86_64"
+    system "CFLAGS=\"$CFLAGS #{arch}\" CXXFLAGS=\"$CXXFLAGS #{arch}\" ./configure --disable-dependency-tracking --prefix=#{prefix}"
+    system "make"
+    system "mv src/.libs/libzmq.0.dylib src/.libs/libzmq.64.dylib"
+
+    # merge UB
+    system "lipo", "-create", "src/libs-32/libzmq.0.dylib", "src/.libs/libzmq.64.dylib", "-output", "src/.libs/libzmq.0.dylib"
+  end
+
   def install
     fails_with_llvm "Compiling with LLVM gives a segfault while linking."
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+
+    if ARGV.include? '--universal'
+      build_fat
+    else
+      system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    end
+
     system "make install"
   end
 
   def caveats; <<-EOS.undent
-    To install the zmq gem on Snow Leopard, on a 64-bit machine:
+    To install the zmq gem on 10.6 with the system Ruby on a 64-bit machine,
+    you may need to do:
       $ ARCHFLAGS="-arch x86_64" gem install zmq -- --with-zmq-dir=#{HOMEBREW_PREFIX}
     EOS
   end
