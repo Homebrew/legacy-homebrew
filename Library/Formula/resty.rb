9867a32415d6256d268bd81f411e91bba1b3c3a6
Adam Vandenberg <flangy@gmail.com>
2010-09-30 13:31:34 -0700

    resty - make files +x and update caveats

--- Library/Formula/resty.rb
+++ Library/Formula/resty.rb
@@ -7,9 +7,15 @@ class Resty <Formula
   def install
     system "mv README.markdown README"
     bin.install %w[pp resty pypp]
+    system "chmod", "a+x", "#{bin}/*"
   end
 
-  def caveats
-    "The Perl printy-printer (pp) depends on JSON from CPAN."
+  def caveats; <<-EOS.undent
+    The Python printy-printer (pypp) uses the json module, available in
+    Python 2.6 and newer.
+
+    The Perl printy-printer (pp) depends on JSON from CPAN:
+      cpan JSON
+    EOS
   end
 end
