d9137b16fbb7385138260eb93b74ddfa7046b062
Adam Vandenberg <flangy@gmail.com>
2010-09-30 12:01:01 -0700

    gd - don't use patch for .pc file

--- Library/Formula/gd.rb
+++ Library/Formula/gd.rb
@@ -8,38 +8,28 @@ class Gd <Formula
 
   depends_on 'jpeg' => :recommended
 
-  def patches
-    DATA
-  end
-
   def install
     ENV.libpng
-    inreplace "gdlib.pc", "PREFIX", prefix
     system "./configure", "--prefix=#{prefix}", "--with-freetype=/usr/X11"
     system "make install"
-    (lib+'pkgconfig').install "gdlib.pc"
+    (lib+'pkgconfig/gdlib.pc').write pkg_file
+  end
+
+  def pkg_file; <<-EOF
+prefix=#{prefix}
+exec_prefix=${prefix}
+libdir=/${exec_prefix}/lib
+includedir=/${prefix}/include
+bindir=/${prefix}/bin
+ldflags=  -L/${prefix}/lib
+
+Name: gd
+Description: A graphics library for quick creation of PNG or JPEG images
+Version: 2.0.36RC1
+Requires:
+Libs: -L${libdir} -lgd
+Libs.private: -lXpm -lX11 -ljpeg -lfontconfig -lfreetype -lpng12 -lz -lm
+Cflags: -I${includedir}
+EOF
   end
 end
-
-__END__
-diff --git a/gdlib.pc b/gdlib.pc
-new file mode 100644
-index 0000000..91fe011
---- /dev/null
-+++ b/gdlib.pc
-@@ -0,0 +1,15 @@
-+prefix=PREFIX
-+exec_prefix=${prefix}
-+libdir=/${exec_prefix}/lib
-+includedir=/${prefix}/include
-+bindir=/${prefix}/bin
-+ldflags=  -L/${prefix}/lib
-+
-+
-+Name: gd
-+Description: A graphics library for quick creation of PNG or JPEG images
-+Version: 2.0.36RC1
-+Requires:
-+Libs: -L${libdir} -lgd
-+Libs.private: -lXpm -lX11 -ljpeg -lfontconfig -lfreetype -lpng12 -lz -lm
-+Cflags: -I${includedir}
