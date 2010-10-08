ac08ae369bbcfe8e098ee999c7517193b4e96be0
Scott Beamer <sbeamer@eecs.berkeley.edu>
2010-10-08 16:00:23 -0700

    fixed merge of libxml2

--- Library/Formula/libxml2.rb
+++ Library/Formula/libxml2.rb
@@ -8,12 +8,8 @@ class Libxml2 <Formula
   keg_only :provided_by_osx
 
   def install
-<<<<<<< HEAD
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-threads"
-=======
     fails_with_llvm "Undefined symbols when linking", :build => "2326"
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
->>>>>>> 09a02645fcf1aa3313f631174016e36917539a6e
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-threads"
     system "make"
     ENV.j1
     system "make install"
