6c351d3fb30d96bd28e59cc57305368947d0a886
Mark A. Matienzo <mark@matienzo.org>
2010-10-10 13:48:20 -0400

    Update formula: afflib
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/afflib.rb
+++ Library/Formula/afflib.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Afflib <Formula
-  url 'http://afflib.org/downloads/afflib-3.6.2.tar.gz'
+  url 'http://afflib.org/downloads/afflib-3.6.3.tar.gz'
   homepage 'http://afflib.org'
-  md5 'c30468c762c8e06776410ca8fb56b782'
+  md5 'b81b5b126cb9c45137bf1ad5d09c99b8'
 
-  depends_on 'expat'
+  depends_on 'expat' => :optional
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
