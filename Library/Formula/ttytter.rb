ad661e5c9f83d85406b0092cd79daae54b258e8f
Tobias Steinhoff <tobias@stonehope.de>
2010-10-11 21:15:09 +0200

    updated ttytter to 1.1.6 and added some notes how to use the new readline feature

--- Library/Formula/ttytter.rb
+++ Library/Formula/ttytter.rb
@@ -1,11 +1,22 @@
 require 'formula'
 
 class Ttytter <ScriptFileFormula
-  url 'http://www.floodgap.com/software/ttytter/dist1/1.1.05.txt'
+  url 'http://www.floodgap.com/software/ttytter/dist1/1.1.06.txt'
   homepage 'http://www.floodgap.com/software/ttytter/'
-  md5 '354aa87ed02105b800a8dcb9e658f8dd'
+  md5 'cfb9a2b9334c194ace2d695bf01a3025'
+  
+  
 
   def install
-    bin.install '1.1.05.txt' => 'ttytter'
+    bin.install '1.1.06.txt' => 'ttytter'
+  end  
+      def caveats; <<-EOS
+    To take full advantage of the new readline feature you have to install readline:
+    brew install readline
+    and the Perl Module Term::ReadLine::TTYtter 
+    use cpan -i Term::ReadLine::TTYtter
+    or if you brewed cpanminus use
+    cpanm Term::ReadLine::TTYtter
+    EOS
   end
 end
