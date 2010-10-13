e3b94f5a41c0db7ad9762cd35f7c7d7f68b07a09
Tobias Steinhoff <tobias@stonehope.de>
2010-10-13 08:57:55 +0200

    Updated ttytter

--- Library/Formula/ttytter.rb
+++ Library/Formula/ttytter.rb
@@ -5,18 +5,17 @@ class Ttytter <ScriptFileFormula
   homepage 'http://www.floodgap.com/software/ttytter/'
   md5 'cfb9a2b9334c194ace2d695bf01a3025'
   
-  
-
   def install
     bin.install '1.1.06.txt' => 'ttytter'
   end  
-      def caveats; <<-EOS
-    To take full advantage of the new readline feature you have to install readline:
-    brew install readline
-    and the Perl Module Term::ReadLine::TTYtter 
-    use cpan -i Term::ReadLine::TTYtter
-    or if you brewed cpanminus use
-    cpanm Term::ReadLine::TTYtter
+  
+  def caveats; <<-EOS
+     To take full advantage of the new readline feature you have to install readline:
+     brew install readline
+     and the Perl Module Term::ReadLine::TTYtter 
+     use cpan -i Term::ReadLine::TTYtter
+     or if you brewed cpanminus use
+     cpanm Term::ReadLine::TTYtter
     EOS
   end
 end
