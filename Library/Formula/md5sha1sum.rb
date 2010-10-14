5b22a4ab546433069134b077f044b872a52c82de
Jean Boussier <jean.boussier@feedback20.com>
2010-10-14 18:50:35 +0200

    fix: download tarball from a mirror

--- Library/Formula/md5sha1sum.rb
+++ Library/Formula/md5sha1sum.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Md5sha1sum <Formula
-  url 'http://www.microbrew.org/tools/md5sha1sum/md5sha1sum-0.9.5.tar.gz'
+  url 'http://download.openpkg.org/components/cache/md5sha1sum/md5sha1sum-0.9.5.tar.gz'
   homepage 'http://www.microbrew.org/tools/md5sha1sum/'
   md5 'a75c5e21071ffa66fad1001de040517a'
 
