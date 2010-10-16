013e7e2a941a49ee116708a34993987b4085b25e
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2010-10-16 14:43:45 -0400

    added arss
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/arss.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Arss <Formula
+  url 'http://downloads.sourceforge.net/project/arss/arss/0.2.3/arss-0.2.3-src.tar.gz'
+  homepage 'http://arss.sourceforge.net/'
+  md5 '7a349ac00dd2732e70043254b4183255'
+
+  depends_on 'cmake' => :build
+  depends_on 'fftw'
+
+  def install
+    Dir.chdir "src" do
+      system "cmake . #{std_cmake_parameters}"
+      system "make install"
+    end
+  end
+end
