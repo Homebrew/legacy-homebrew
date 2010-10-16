00f2dacc865649c47875e4d675b7fca64832dfd2
Jonathan Abourbih <jon.abourbih@gmail.com>
2010-10-16 14:11:47 +0100

    Updated with dependencies for swi-prolog formula

--- Library/Formula/swi-prolog.rb
+++ Library/Formula/swi-prolog.rb
@@ -4,16 +4,23 @@ class SwiProlog <Formula
   url 'http://www.swi-prolog.org/download/stable/src/pl-5.10.1.tar.gz'
   homepage 'http://www.swi-prolog.org/'
   md5 '9168a2c872d2130467c3e74b80ed3ee0'
+  depends_on 'readline'
+  depends_on 'gmp'
+  depends_on 'jpeg'
+  depends_on 'pkg-config'
+  depends_on 'fontconfig'
+  depends_on 'ncursesw'
+  depends_on 'libmcrypt'
+  depends_on 'gawk'
 
-  depends_on "gmp"
-  depends_on "readline"
+  # depends_on 'cmake'
 
   def install
-    # doesn't pick up gmp otherwise
-    ENV.append "CFLAGS", ENV['CPPFLAGS']
-
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--with-world", "--x-includes=/usr/X11/include",
+                          "--x-libraries=/usr/X11/lib", "--mandir=#{man}"
     system "make"
+    system "make check"
     system "make install"
   end
 end
