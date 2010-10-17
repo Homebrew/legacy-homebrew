ae253b3b753a65e9469f7ce37686777d2d87a78e
Tojek Anselm <tojek.anselm@yahoo.com>
2010-10-17 08:52:14 -0700

    sqlite --universal option
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/sqlite.rb
+++ Library/Formula/sqlite.rb
@@ -7,10 +7,12 @@ class Sqlite <Formula
 
   def options
     [["--with-rtree", "Enables the R*Tree index module"]]
+    [["--universal", "Build a universal binary."]]
   end
 
   def install
     ENV.append "CFLAGS", "-DSQLITE_ENABLE_RTREE=1" if ARGV.include? "--with-rtree"
+    ENV.universal_binary if ARGV.include? "--universal"
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking"
     system "make install"
