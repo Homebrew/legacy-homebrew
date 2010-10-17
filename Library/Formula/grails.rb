9ae2ee602d8c031eb81c9985b260fbfc8fab5e73
Adam Vandenberg <flangy@gmail.com>
2010-10-16 18:16:47 -0700

    grails 1.3.5

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Grails <Formula
-  url 'http://dist.codehaus.org/grails/grails-1.3.4.zip'
+  url 'http://dist.codehaus.org/grails/grails-1.3.5.zip'
   homepage 'http://grails.org'
-  md5 'd4c968cf4869cf7afdaae0b17c85beba'
+  md5 'c1323fb99b6d173754e9034f3971e2ca'
 
   def install
     rm_f Dir["bin/*.bat"]
