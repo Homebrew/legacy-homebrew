89b7619a33a2811c10a337ddf95529630ad2b523
pascalbertrand <pascal@pascalbertrand.com>
2010-10-17 18:48:35 +0200

    gmp - Allow creating 32bit build on Snow Leopard
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
     * Note that building 32-bit on Snow Leopard will cause other
       64-bit brews to fail to find gmp.

--- Library/Formula/gmp.rb
+++ Library/Formula/gmp.rb
@@ -7,23 +7,21 @@ class Gmp <Formula
 
   def options
     [
-      ["--skip-check", "Do not run 'make check' to verify libraries. (Not recommended.)"],
-      ["--32-bit", "Force 32-bit on Leopard on 64-bit machines."]
+      ["--32-bit", "Force 32-bit."],
+      ["--skip-check", "Do not run 'make check' to verify libraries."]
     ]
   end
 
   def install
-    fails_with_llvm "On OS X 10.6, some tests fail under LLVM"
+    fails_with_llvm "Tests fail to compile; missing references in 'llvm bitcode in libtests.a(misc.o)'."
 
     args = ["--prefix=#{prefix}", "--infodir=#{info}", "--enable-cxx"]
 
-    if MACOS_VERSION == 10.5
-      if Hardware.is_32_bit? or ARGV.include? "--32-bit"
-        ENV.m32
-        args << "--host=none-apple-darwin"
-      else
-        ENV.m64
-      end
+    if Hardware.is_32_bit? or ARGV.include? "--32-bit"
+      ENV.m32
+      args << "--host=none-apple-darwin"
+    else
+      ENV.m64
     end
 
     system "./configure", *args
