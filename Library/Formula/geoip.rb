a02ce4966dfd78752c6861571306dbf6d68c00d7
Tojek Anselm <tojek.anselm@yahoo.com>
2010-10-17 08:52:14 -0700

    geoip --universal option
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/geoip.rb
+++ Library/Formula/geoip.rb
@@ -5,7 +5,12 @@ class Geoip <Formula
   homepage 'http://www.maxmind.com/app/c'
   md5 'cb14b1beeb40631a12676b11ca0c309a'
 
+  def options
+    [["--universal", "Build a universal binary."]]
+  end
+
   def install
+    ENV.universal_binary if ARGV.include? "--universal"
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
