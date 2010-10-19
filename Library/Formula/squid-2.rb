7167e50301421634acfca8ec4bcac47f634aa865
Michael Brunton-Spall <michael.brunton-spall@guardian.co.uk>
2010-10-19 13:03:58 +0100

    Add formula for squid-2.7
    The current squid formula is 3.2 only, which does not yet have feature parity with squid-2.7, so a squid-2 package seems appropriate.

---
+++ Library/Formula/squid-2.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Squid2 <Formula
+  url 'http://www.squid-cache.org/Versions/v2/2.7/squid-2.7.STABLE9.tar.gz'
+  homepage 'http://www.squid-cache.org/'
+  md5 '7d3b8b0bdda3ae56e438d4a95a97d3b3'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
