5ad901e37e2697961d04ad3fe9d3a0e28671373c
Adam Vandenberg <flangy@gmail.com>
2010-10-20 19:28:46 -0700

    brew-doctor - also check /usr/local/etc

--- Library/Homebrew/brew_doctor.rb
+++ Library/Homebrew/brew_doctor.rb
@@ -222,6 +222,23 @@ def check_access_include
   end
 end
 
+def check_access_etc
+  etc_folder = HOMEBREW_PREFIX+'etc'
+  return unless etc_folder.exist?
+
+  unless etc_folder.writable?
+    puts <<-EOS.undent
+      #{etc_folder} isn't writable.
+      This can happen if you "sudo make install" software that isn't managed
+      by Homebrew. If a brew tries to write a file to this folder, the install
+      will fail during the link step.
+
+      You should probably `chown` #{etc_folder}
+
+    EOS
+  end
+end
+
 def check_usr_bin_ruby
   if /^1\.9/.match RUBY_VERSION
     puts <<-EOS.undent
@@ -563,6 +580,7 @@ def brew_doctor
     check_access_share_locale
     check_access_share_man
     check_access_include
+    check_access_etc
     check_user_path
     check_which_pkg_config
     check_pkg_config_paths
