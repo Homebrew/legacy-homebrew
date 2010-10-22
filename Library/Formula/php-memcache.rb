62f619a9010c0f5611e1138476b152b559ce469b
Michael LeBarron <michael@lebarron.org>
2010-10-22 11:47:34 -0400

    Formula updated: php-memcache
    Just code prettiness regarding indentation

--- Library/Formula/php-memcache.rb
+++ Library/Formula/php-memcache.rb
@@ -22,13 +22,13 @@ class PhpMemcache <Formula
   end
 
   def caveats; <<-EOS.undent
-  To use this software:
-  * Add the following line to php.ini:
-    zend_extension="#{prefix}/memcache.so"
-  * Restart your webserver.
-  * Write a PHP page that calls "phpinfo();"
-  * Load it in a browser and look for the info on the memcache module.
-  * If you see it, you have been successful!
-  EOS
+    To use this software:
+    * Add the following line to php.ini:
+      zend_extension="#{prefix}/memcache.so"
+    * Restart your webserver.
+    * Write a PHP page that calls "phpinfo();"
+    * Load it in a browser and look for the info on the memcache module.
+    * If you see it, you have been successful!
+    EOS
   end
 end
