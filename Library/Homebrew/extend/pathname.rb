bc1d3edf340aa132abb564c2646b46871f377f26
Michael Dippery <mdippery@gmail.com>
2010-10-22 13:54:05 -0400

    Don't list .DS_Store files in `brew info`

--- Library/Homebrew/extend/pathname.rb
+++ Library/Homebrew/extend/pathname.rb
@@ -92,7 +92,7 @@ class Pathname
 
   def abv
     out=''
-    n=`find #{to_s} -type f | wc -l`.to_i
+    n=`find #{to_s} -type f ! -name .DS_Store | wc -l`.to_i
     out<<"#{n} files, " if n > 1
     out<<`/usr/bin/du -hd0 #{to_s} | cut -d"\t" -f1`.strip
   end
