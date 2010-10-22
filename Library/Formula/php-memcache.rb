437aa8cba861948e113196ef33b644a58d433e1f
Michael LeBarron <michael@lebarron.org>
2010-10-22 11:42:31 -0400

    Formula updated: php-memcache
    Comments and better code standards

--- Library/Formula/php-memcache.rb
+++ Library/Formula/php-memcache.rb
@@ -9,25 +9,26 @@ class PhpMemcache <Formula
 
   def install
     Dir.chdir "memcache-#{version}" do
+      # For Zend Server compatibility
       ENV.universal_binary
+
       system "phpize"
       system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}"
+                            "--prefix=#{prefix}",
                             "--enable-memcache"
       system "make"
       prefix.install 'modules/memcache.so'
     end
   end
 
-    def caveats
-        <<-EOS
+  def caveats; <<-EOS.undent
   To use this software:
-   * Add the following line to php.ini:
-      zend_extension="#{prefix}/memcache.so"
-   * Restart your webserver.
-   * Write a PHP page that calls "phpinfo();"
-   * Load it in a browser and look for the info on the memcache module.
-   * If you see it, you have been successful!
-        EOS
-    end
+  * Add the following line to php.ini:
+    zend_extension="#{prefix}/memcache.so"
+  * Restart your webserver.
+  * Write a PHP page that calls "phpinfo();"
+  * Load it in a browser and look for the info on the memcache module.
+  * If you see it, you have been successful!
+  EOS
+  end
 end
