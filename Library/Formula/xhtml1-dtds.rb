ce79f32239f7e7cc1a067007c86717db06076f27
Justin Clift <jclift@redhat.com>
2010-10-25 17:11:05 +1100

    Updated formula: xhtml1-dtds
    Changed the target directory creation, plus file copying, to use a
    nicer Object Oriented approach, as suggested by Adam V. :)

--- Library/Formula/xhtml1-dtds.rb
+++ Library/Formula/xhtml1-dtds.rb
@@ -6,19 +6,28 @@ class Xhtml1Dtds <Formula
   sha256 '148e9eccb5c91222b5f0c07359174ca0df1bd74834a2bae4c6e57c10136a2067'
 
   def install
-    # Create the needed directory structure for the XHTML1 DTDs
-    mkdir_p "#{share}/xhtml1-dtds-1.0/"
+    # The XHTML1 DTDs need to be placed in a directory, after extracting
+    # them from the w3's downloadable tarball.  Then we set up an XML
+    # catalog file, so other packages can use them
 
-    # Copy the DTD files into place
-    cp %w(DTD/xhtml1.dcl DTD/xhtml1-frameset.dtd), "#{share}/xhtml1-dtds-1.0/"
-    cp %w(DTD/xhtml1-strict.dtd DTD/xhtml1-transitional.dtd), "#{share}/xhtml1-dtds-1.0/"
-    cp %w(DTD/xhtml-lat1.ent DTD/xhtml.soc), "#{share}/xhtml1-dtds-1.0/"
-    cp %w(DTD/xhtml-special.ent DTD/xhtml-symbol.ent), "#{share}/xhtml1-dtds-1.0/"
+    # First create a variable containing the name of the directory for
+    # installing things in
+    xhtml_dir = share+"xhtml1-dtds-1.0"
 
-    # Create the XML catalog file
+    # Use the new variable to create the directory
+    xhtml_dir.mkpath
+
+    # Copy the XHTML1 DTD files into the directory
+    Dir.chdir "DTD" do
+      xhtml_dir.install %w(xhtml1.dcl xhtml1-frameset.dtd xhtml1-strict.dtd
+                           xhtml1-transitional.dtd xhtml-lat1.ent xhtml.soc
+                           xhtml-special.ent xhtml-symbol.ent)
+    end
+
+    # Create a brand new XML catalog file
     system "xmlcatalog --noout --create #{share}/xhtml1-dtds-1.0/catalog.xml"
 
-    # Create each of the needed entries in the XML catalog file
+    # Manipulate the new XML catalog file, creating each of the needed entries
     system "xmlcatalog --noout -add public '-//W3C//DTD XHTML 1.0 Strict//EN' 'xhtml1-strict.dtd' #{share}/xhtml1-dtds-1.0/catalog.xml"
     system "xmlcatalog --noout -add public '-//W3C//DTD XHTML 1.0 Transitional//EN' 'xhtml1-transitional.dtd' #{share}/xhtml1-dtds-1.0/catalog.xml"
     system "xmlcatalog --noout -add public '-//W3C//DTD XHTML 1.0 Frameset//EN' 'xhtml1-frameset.dtd' #{share}/xhtml1-dtds-1.0/catalog.xml"
