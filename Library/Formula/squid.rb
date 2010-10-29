f816827dedd4b54b661aded8fcf2b60d46c7bd4b
Will Tisdale <will@frantic-net.co.uk>
2010-10-29 11:35:04 +0100

    Update Squid to 3.1.9
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/squid.rb
+++ Library/Formula/squid.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Squid <Formula
-  url 'http://www.squid-cache.org/Versions/v3/3.1/squid-3.1.7.tar.bz2'
+  url 'http://www.squid-cache.org/Versions/v3/3.1/squid-3.1.9.tar.bz2'
   homepage 'http://www.squid-cache.org/'
-  md5 '83e7aabc1b5bb5b7c83f6dc2f32ca418'
+  md5 '13e78641aa8ee1056299c68a80b656d6'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
