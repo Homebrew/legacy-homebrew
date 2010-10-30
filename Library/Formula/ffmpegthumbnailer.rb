fed1c574e1bfc370725f01b183f9b7b936820b68
Pavel Zavyalov (transferred) <motto@pavel-zavyalovs-mac-mini.local>
2010-10-31 01:41:12 +0400

    initial release

---
+++ Library/Formula/ffmpegthumbnailer.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Ffmpegthumbnailer <Formula
+  url 'http://ffmpegthumbnailer.googlecode.com/files/ffmpegthumbnailer-2.0.4.tar.gz'
+  homepage 'http://code.google.com/p/ffmpegthumbnailer/'
+  md5 '83b43130e29a26126a50705a011004be'
+
+  depends_on 'jpeg'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--enable-jpeg", "--with-jpeg",
+                          "--prefix=#{prefix}"
+    # system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
