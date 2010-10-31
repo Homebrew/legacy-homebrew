aa97c491ffc7fbeed1c9470c65b627dff7e80f8f
Severin Smith <info@smallbutdigital.com>
2010-10-30 21:58:47 -0400

    New Forumla for gerbv, a fantastic Gerber Viewer
    Gerbv, http://gerbv.gpleda.org/index.html, is a simple graphical viewer for Gerber file which runs under GTK+.
    Gerber files are used to mill PCB and there is no other such tool available for mac/*nix.

---
+++ Library/Formula/gerbv.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Gerbv <Formula
+  url 'http://downloads.sourceforge.net/project/gerbv/gerbv/gerbv-2.4.0/gerbv-2.4.0.tar.gz'
+  homepage 'http://gerbv.gpleda.org/'
+  md5 '56431417df2d246db87e225783097d75'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gtk+'
+  depends_on 'cairo' if MACOS_VERSION < 10.6
+
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                  "--disable-update-desktop-database", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
