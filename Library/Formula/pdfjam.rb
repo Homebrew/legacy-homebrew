3181af54f7c3be321336796cb9b10d6ca0fb59c2
Pierre Riteau <priteau@gmail.com>
2010-10-31 11:02:23 +0100

    PDFjam version is actually 2.06, not 206
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pdfjam.rb
+++ Library/Formula/pdfjam.rb
@@ -4,6 +4,7 @@ class Pdfjam <Formula
   url 'http://www2.warwick.ac.uk/fac/sci/statistics/staff/academic/firth/software/pdfjam/pdfjam_206.tgz'
   homepage 'http://www2.warwick.ac.uk/fac/sci/statistics/staff/academic/firth/software/pdfjam'
   md5 '8113cae5d43359708be9e7e9d6df999d'
+  version '2.06'
 
   def install
     bin.install Dir['bin/*']
