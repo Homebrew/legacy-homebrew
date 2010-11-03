bf78e2671830b57c52c800905345f8c3483f09a3
Michael Dippery <mdippery@gmail.com>
2010-11-03 14:08:41 -0400

    coq: Remove unnecessary string concatenation

--- Library/Formula/coq.rb
+++ Library/Formula/coq.rb
@@ -15,7 +15,7 @@ class Coq < Formula
       exit 1
     end
     arch = Hardware.is_64_bit? ? "x86_64" : "i386"
-    camlp5_lib = Formula.factory('camlp5').lib+'ocaml'+'camlp5'
+    camlp5_lib = Formula.factory('camlp5').lib+'ocaml/camlp5'
     system "./configure", "-prefix", prefix,
                           "-mandir", man,
                           "-camlp5dir", camlp5_lib,
