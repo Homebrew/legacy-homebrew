27beb9bddb97073e832f8a7b59ceacac9797e347
Andre Arko <andre@arko.net>
2010-11-05 02:23:26 -0700

    Update gist to a fork that works with HTTPS-only Github

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
-class Gist <Formula
-  url 'http://github.com/defunkt/gist/tarball/v1.2.1'
+class Gist < Formula
+  url 'http://github.com/indirect/gist/tarball/v1.3.0'
   homepage 'http://github.com/defunkt/gist'
-  md5 'f89e4d059e35041acc215808a8d59cd5'
+  md5 '8fc9a5e7261611d39b56e1b9d4b48474'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
