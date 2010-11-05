c2d09cfdaa987b11d30fed4d691ed869de6b6f05
Andre Arko <andre@arko.net>
2010-11-05 02:23:26 -0700

    gist - HTTPS-only Github & multi-file gists
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
     * Ownership of this scrpit has passed to [indirect], so he's
       the new upstream maintainer.

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gist <Formula
-  url 'http://github.com/defunkt/gist/tarball/v1.2.1'
+  url 'http://github.com/indirect/gist/tarball/v1.4.0'
   homepage 'http://github.com/defunkt/gist'
-  md5 'f89e4d059e35041acc215808a8d59cd5'
+  md5 '014c3c8f02b7b46a0ea986da29dd9f18'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
