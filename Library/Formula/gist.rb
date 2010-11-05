d777f82546268a4b590ba0c9310b153c35835c5c
Andre Arko <andre@arko.net>
2010-11-05 13:47:31 -0700

    moar better gist with multifiles and - to force stdin

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gist < Formula
-  url 'http://github.com/indirect/gist/tarball/v1.3.0'
+  url 'http://github.com/indirect/gist/tarball/v1.4.0'
   homepage 'http://github.com/defunkt/gist'
-  md5 '8fc9a5e7261611d39b56e1b9d4b48474'
+  md5 '014c3c8f02b7b46a0ea986da29dd9f18'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
