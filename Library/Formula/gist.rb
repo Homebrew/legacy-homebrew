cee86965e43e1d840a98d750cfeb3221f05ea48e
Adam Vandenberg <flangy@gmail.com>
2010-11-05 19:39:42 -0700

    Change 'gist' homepage too

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Gist <Formula
   url 'http://github.com/indirect/gist/tarball/v1.4.0'
-  homepage 'http://github.com/defunkt/gist'
+  homepage 'http://github.com/indirect/gist'
   md5 '014c3c8f02b7b46a0ea986da29dd9f18'
 
   def install
