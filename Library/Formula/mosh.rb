99d5aae34e97bfaee14d4afb80b31ab69ffa5555
okuoku <mjt@cltn.org>
2010-11-06 22:24:18 +0900

    New formula: mosh
    mosh R6RS Scheme interpreter.
    Known to fail with LLVM, GCC>4.0 required.

--- Library/Formula/mosh.rb
+++ Library/Formula/mosh.rb
@@ -1,19 +1,19 @@
 require 'formula'
 
 class Mosh <Formula
-  url 'http://storage.osdev.info/pub/idmjt/diaryimage/1011/mosh-0.2.6rc13p.tar.gz'
+  url 'http://mosh-scheme.googlecode.com/files/mosh-0.2.6p.tar.gz'
   homepage 'http://mosh.monaos.org'
-  md5 '99c97151d93556490245d33fac3769c2'
+  md5 'e41e38a4a09614392c6e2eb850143724'
   version '0.2.6'
-
-  ENV.gcc_4_2 if MACOS_VERSION < 10.6
 
   depends_on 'gmp'
   depends_on 'oniguruma'
 
   def install
     fails_with_llvm
-    system "./configure", "--disable-debug", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    ENV.gcc_4_2
+    system "./configure", "--disable-debug", "--prefix=#{prefix}", 
+           "--disable-dependency-tracking"
     system "make"
     system "make install"
   end
