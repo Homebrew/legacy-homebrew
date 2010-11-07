d6f02b5a2c990814a0517fc07694062ccfb6c7e6
Adam Vandenberg <flangy@gmail.com>
2010-11-07 11:25:20 -0800

    Allow underscore in github version numbers.

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -2,7 +2,6 @@ require 'formula'
 
 class Ack < Formula
   url "https://github.com/petdance/ack/tarball/1.93_02"
-  version '1.93_02'
   md5 'b468ce41a949fd957dc9b6aee74782e9'
   homepage 'http://betterthangrep.com/'
 
