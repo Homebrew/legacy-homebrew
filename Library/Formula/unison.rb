3336e57fc381d062c9923935d3938133fad9b20f
Lukas Toth <lukas@lukastoth.com>
2010-11-09 12:17:15 -0500

    Patch unison to compile with OCaml 3.12; add head
    Added an inline patch backported from the unison 3.40 tree to make it compile
    with the current Homebrew version of OCaml. The patch will no longer apply
    for versions greater or equal to 3.40. Also, added the SVN trunk repo as
    head. The fsmonitor.py executable should be installed for the head version.

--- Library/Formula/unison.rb
+++ Library/Formula/unison.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Unison < Formula
   url 'http://www.seas.upenn.edu/~bcpierce/unison//download/releases/stable/unison-2.32.52.tar.gz'
+  head 'https://webdav.seas.upenn.edu/svn/unison/trunk/src/', :using => :svn
   homepage 'http://www.cis.upenn.edu/~bcpierce/unison/'
   md5 '0701f095c1721776a0454b94607eda48'
 
@@ -11,6 +12,30 @@ class Unison < Formula
     ENV.j1
     ENV.delete "CFLAGS" # ocamlopt reads CFLAGS but doesn't understand common options
     system "make UISTYLE=text"
-    bin.install 'unison'
+    %w(unison fsmonitor.py).each do |f|
+      bin.install f if File.exist? f
+    end
+  end
+
+  def patches
+    # fixes Unison 2.32.52 to compile with OCaml 3.12 (because of changes in
+    # Map). This was merged into Unison 3.40 at
+    # https://webdav.seas.upenn.edu/viewvc/unison/branches/2.40/src/update.mli?r1=435&r2=457&pathrev=457
+    DATA unless ARGV.build_head?
   end
 end
+
+__END__
+diff --git a/update.mli b/update.mli
+index dc1e018..c99c704 100644
+--- a/update.mli
++++ b/update.mli
+@@ -1,7 +1,7 @@
+ (* Unison file synchronizer: src/update.mli *)
+ (* Copyright 1999-2009, Benjamin C. Pierce (see COPYING for details) *)
+ 
+-module NameMap : Map.S with type key = Name.t
++module NameMap : MyMap.S with type key = Name.t                                                                        
+ 
+ type archive =
+     ArchiveDir of Props.t * archive NameMap.t
