ef48741108b9ab2557f43c057df0079d351fec25
Kevin Williams <kevwil@gmail.com>
2010-11-10 14:31:21 -0700

    update mongrel2 to 1.4 release
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mongrel2.rb
+++ Library/Formula/mongrel2.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Mongrel2 <Formula
-  url 'http://mongrel2.org/static/downloads/mongrel2-1.3.tar.bz2'
+  url 'http://mongrel2.org/static/downloads/mongrel2-1.4.tar.bz2'
   head 'fossil://http://mongrel2.org:44445/'
   homepage 'http://mongrel2.org/'
-  md5 'dfc19b26e93d707c68fcff2d72a0d03f'
+  md5 'd5b917c7cb2fd0527f6824edda37cefb'
 
   depends_on 'zeromq'
 
