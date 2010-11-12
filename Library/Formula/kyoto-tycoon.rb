9bb8d332b981df5634742a61f3c8de7453736f51
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-11-12 18:14:57 +0900

    new formula: kyoto-tycoon
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/kyoto-tycoon.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class KyotoTycoon <Formula
+  url 'http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.7.tar.gz'
+  homepage 'http://fallabs.com/kyototycoon/'
+  md5 'c6ba2b75e6f675c4fbe5d50b3f26143d'
+
+  depends_on 'lua' unless ARGV.include? "--no-lua"
+  depends_on 'kyoto-cabinet'
+
+  def options
+    [["--no-lua", "Disable Lua support (and don't force Lua install.)"]]
+  end
+
+  def install
+    args = ["--prefix=#{prefix}"]
+    args << "--enable-lua" unless ARGV.include? "--no-lua"
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+  end
+end
