1ae8e949efaf5293a41e9063a747a867a243ff9b
Adam Vandenberg <flangy@gmail.com>
2010-11-12 21:05:35 -0800

    Move string extensions

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -169,36 +169,13 @@ def archs_for_command cmd
   archs.extend(ArchitectureListExtension)
 end
 
-# String extensions added by inreplace below.
-module HomebrewInreplaceExtension
-  # Looks for Makefile style variable defintions and replaces the
-  # value with "new_value", or removes the definition entirely.
-  def change_make_var! flag, new_value
-    new_value = "#{flag}=#{new_value}"
-    gsub! Regexp.new("^#{flag}[ \\t]*=[ \\t]*(.*)$"), new_value
-  end
-  # Removes variable assignments completely.
-  def remove_make_var! flags
-    flags.each do |flag|
-      # Also remove trailing \n, if present.
-      gsub! Regexp.new("^#{flag}[ \\t]*=(.*)$\n?"), ""
-    end
-  end
-  # Finds the specified variable
-  def get_make_var flag
-    m = match Regexp.new("^#{flag}[ \\t]*=[ \\t]*(.*)$")
-    return m[1] if m
-    return nil
-  end
-end
-
 def inreplace path, before=nil, after=nil
   [*path].each do |path|
     f = File.open(path, 'r')
     s = f.read
 
     if before == nil and after == nil
-      s.extend(HomebrewInreplaceExtension)
+      s.extend(StringInreplaceExtension)
       yield s
     else
       s.gsub!(before, after)
