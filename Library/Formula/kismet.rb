611f2eac4acefb5231e767cfd3556be473231961
Ian Gallagher <crash@neg9.org>
2010-11-14 19:14:53 -0800

    Update Kismet brew to 2010-07-R1  * Upgrade to current version of Kismet  * Add functionality to automatically configure kismet.conf to use first wireless interface

--- Library/Formula/kismet.rb
+++ Library/Formula/kismet.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Kismet <Formula
-  url 'http://www.kismetwireless.net/code/kismet-2010-01-R1.tar.gz'
-  version '2010-01-R1'
+  url 'http://www.kismetwireless.net/code/kismet-2010-07-R1.tar.gz'
+  version '2010-07-R1'
   homepage 'http://www.kismetwireless.net'
-  md5 'a6d6edcf65d5bb2cb5de6472bcc16f19'
+  sha256 'b1bae7a97e7a904bf620f285aa0d62ebc1fd3b54b671fbca125405036f949e80'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--sysconfdir=#{etc}"
@@ -14,14 +14,34 @@ class Kismet <Formula
     inreplace "Makefile", "-o $(INSTUSR) -g $(MANGRP)", ""
 
     system "make install"
+    
+    # Configure the interface for your systems, requires small plist gem
+    require 'rubygems'
+    system "gem install plist" if not Gem.available?('plist')
+    addInterfacesToConfig()
   end
 
   def caveats
     <<-EOS.undent
-      Read #{doc}/README and edit #{etc}/kismet.conf
+      Read http://www.kismetwireless.net/documentation.shtml and edit #{etc}/kismet.conf as needed.
 
-      Set source=darwin,en1,airport_extreme
-      and replace your_user_here in suiduser with your username
+      - SUID Root functionality does not work, you will have to run this as root, e.g. via `sudo`. Do so at your own risk.
+      - This version can be configured interactively when it is run (listen interface, etc).
+      - Automatically configured to use first AirPort interface found
     EOS
+  end
+  
+  def addInterfacesToConfig
+    Gem.clear_paths
+    require 'plist'
+    file = IO.popen('/usr/sbin/system_profiler -detailLevel mini -xml SPNetworkDataType', mode='r')
+    sysprof_data = Plist::parse_xml(file)
+    airport_interface = sysprof_data[0]['_items'].find_all { |item| item['_name'] == 'AirPort' }[0]['interface']
+
+    kismet_ncsources = "ncsource=%s:name=AirPort" % airport_interface
+    open("#{etc}/kismet.conf", 'a') { |f|
+      f.puts "# Source interface (auto-added via homebrew installation)"
+      f.puts kismet_ncsources
+    }
   end
 end
