3a93bb8d333498de3d611a7e08ad3662500b376c
Markus Hubig <mhubig@gmail.com>
2010-11-15 13:38:57 +0100

    gpsim v0.25.0 - Simulator for PIC microcontrollers

---
+++ Library/Formula/gpsim.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Gpsim <Formula
+  url 'http://sourceforge.net/projects/gpsim/files/gpsim/0.25.0/gpsim-0.25.0.tar.gz'
+  homepage 'http://gpsim.sourceforge.net/'
+  md5 '36e2aeac30fad773f5fb934c867b42b7'
+
+  depends_on 'popt'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--disable-gui", "--disable-shared", "--prefix=#{prefix}"
+    system "make all"
+    system "make install"
+  end
+end
