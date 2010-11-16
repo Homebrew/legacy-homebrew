7603c451c064440844e4d5532c876b958bd6852f
Max Klinger <max@fs.lmu.de>
2010-11-16 17:46:03 +0100

    adding newsbeuter

---
+++ Library/Formula/stfl.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Stfl <Formula
+  url 'http://www.clifford.at/stfl/stfl-0.20.tar.gz'
+  homepage 'http://www.clifford.at'
+  md5 '905e0b8f81fe1b5c95b8d78f56df966b'
+
+	depends_on 'ncursesw'
+
+  def patches
+	  { :p0 => "http://svn.macports.org/repository/macports/trunk/dports/devel/stfl/files/patch-Makefile.diff" }
+  end
+  
+	def install
+		inreplace 'Makefile.cfg','/usr/local',"#{prefix}"
+    system "make install"
+  end
+end
