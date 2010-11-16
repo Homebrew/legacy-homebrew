813aa4e761a50450d6ae2dd5094a5ec2fccbdc2a
Max Klinger <max@fs.lmu.de>
2010-11-16 23:44:39 +0100

    appeased audit

--- Library/Formula/newsbeuter.rb
+++ Library/Formula/newsbeuter.rb
@@ -4,19 +4,19 @@ class Newsbeuter <Formula
   url 'http://www.newsbeuter.org/downloads/newsbeuter-2.2.tar.gz'
   homepage 'http://www.newsbeuter.org'
   md5 '2add1dfe8d3684e67ab75f0c5172c705'
-	
-	depends_on 'gettext'
-	depends_on 'stfl'
-	depends_on 'pkg-config'
-	depends_on 'sqlite'
+
+  depends_on 'gettext'
+  depends_on 'stfl'
+  depends_on 'pkg-config'
+  depends_on 'sqlite'
 
   def install
-		gettext = Formula.factory('gettext')
-		inreplace 'Makefile','/usr/local',"#{prefix}"
+    gettext = Formula.factory('gettext')
+    inreplace 'Makefile','/usr/local',"#{prefix}"
     system "./config.sh"
- 		ENV.append "CXXFLAGS", "-I#{gettext.include}" 
- 		ENV.append "LDFLAGS", "-L#{gettext.lib}" 
-		system "make"
+    ENV.append "CXXFLAGS", "-I#{gettext.include}"
+    ENV.append "LDFLAGS", "-L#{gettext.lib}"
+    system "make"
     system "make install"
   end
 end
--- Library/Formula/stfl.rb
+++ Library/Formula/stfl.rb
@@ -5,14 +5,14 @@ class Stfl <Formula
   homepage 'http://www.clifford.at'
   md5 '905e0b8f81fe1b5c95b8d78f56df966b'
 
-	depends_on 'ncursesw'
+  depends_on 'ncursesw'
 
   def patches
-	  { :p0 => "http://svn.macports.org/repository/macports/trunk/dports/devel/stfl/files/patch-Makefile.diff" }
+    { :p0 => "http://svn.macports.org/repository/macports/trunk/dports/devel/stfl/files/patch-Makefile.diff" }
   end
-  
-	def install
-		inreplace 'Makefile.cfg','/usr/local',"#{prefix}"
+
+  def install
+    inreplace 'Makefile.cfg','/usr/local',"#{prefix}"
     system "make install"
   end
 end
