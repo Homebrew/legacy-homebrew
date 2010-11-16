9cd05791ac41481dbc2d7f6836bfb04e20bcac7f
aktowns <ashleyis@me.com>
2010-11-16 16:45:22 +1100

    Updated libmodplug with md5sum

--- Library/Formula/libmodplug.rb
+++ Library/Formula/libmodplug.rb
@@ -3,14 +3,11 @@ require 'formula'
 class Libmodplug <Formula
   url 'http://downloads.sourceforge.net/project/modplug-xmms/libmodplug/0.8.8.1/libmodplug-0.8.8.1.tar.gz'
   homepage 'http://modplug-xmms.sourceforge.net/'
-  md5 ''
-
-  # depends_on 'cmake'
+  md5 'f7fa53a60c650024ff51cca88341776b'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 end
