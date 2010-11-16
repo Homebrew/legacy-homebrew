4ac3001690a84e606f848180784b77d6660a9861
Riobard <me@riobard.com>
2010-11-16 01:07:55 -0330

    Updated JSMin formula to use GitHub source

--- Library/Formula/jsmin.rb
+++ Library/Formula/jsmin.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Jsmin <Formula
-  url 'http://www.crockford.com/javascript/jsmin.c'
+  url 'https://github.com/douglascrockford/JSMin/raw/master/jsmin.c'
   homepage 'http://www.crockford.com/javascript/jsmin.html'
   md5 '8847fd99576468d6c9e76420da0b6b55'
   version '2008-08-03'
