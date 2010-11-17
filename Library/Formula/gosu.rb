21b0dc1c82b673a56f5f7b1225ab231e2c023686
cleiby <carl.leiby@gmail.com>
2010-11-17 11:59:40 -0500

    Updated gosu for a point release

--- Library/Formula/gosu.rb
+++ Library/Formula/gosu.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Gosu <Formula
-  url 'http://gosu-lang.org/downloads/gosu-0.7.0-C.zip'
-  version '0.7.0-C'
+  url 'http://gosu-lang.org/downloads/gosu-0.7.0.1-C.zip'
+  version '0.7.0.1-C'
   homepage 'http://gosu-lang.org/'
-  md5 '08e48e57813ecb9967109e4d4dea618e'
+  md5 '3ea42cdc403ee57397a9559f6a95be67'
 
   def install
     mv "bin/gosu.sh", "bin/gosu"
