707294e6972d8a919cb185d5f42111c180d2a1f6
Matthew L Daniel <mdaniel@gmail.com>
2010-11-19 23:28:34 +0100

    Fixes the `audit` error by removing whitespace.
    I know this is low-hanging fruit, but it's also distracting to not
    have a clean running `brew audit`.
    
    It apparently was just an extra indentation, which I removed.

--- Library/Formula/libbs2b.rb
+++ Library/Formula/libbs2b.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class Libbs2b <Formula
-  url 'http://downloads.sourceforge.net/project/bs2b/libbs2b/3.1.0/libbs2b-3.1.0.tar.gz'
+  version '3.1.0'
+  url "http://downloads.sourceforge.net/project/bs2b/libbs2b/#{@version}/libbs2b-#{@version}.tar.gz"
   homepage 'http://bs2b.sourceforge.net/'
   md5 '2c3351f1785364107aabc44f67d33d84'
 
   depends_on 'pkg-config' => :build
   depends_on 'libsndfile'
-  
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--disable-static",
