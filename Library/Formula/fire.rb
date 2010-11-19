2977cd2c7c1d1512b376d1cf16b88856b145e3a1
Tianyi Cui <tianyicui@gmail.com>
2010-11-19 14:15:24 +0800

    fire: use https github url
    Sorry about this not fixed previously. Not sure why.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/fire.rb
+++ Library/Formula/fire.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Fire <Formula
   head 'git://github.com/AzizLight/fire.git'
-  homepage 'http://github.com/AzizLight/fire'
+  homepage 'https://github.com/AzizLight/fire'
 
   def install
     bin.install "fire"
