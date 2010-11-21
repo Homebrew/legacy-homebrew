054ca6d5a0dbae05e53271263717a08dd31d4959
Mike McQuaid <mike@mikemcquaid.com>
2010-11-21 13:41:33 +0000

    Fix libdvdread whitespace (using tabs now).

--- Library/Formula/libdvdread.rb
+++ Library/Formula/libdvdread.rb
@@ -1,16 +1,15 @@
 require 'formula'
 
 class Libdvdread <Formula
-  url 'http://www.dtek.chalmers.se/groups/dvd/dist/libdvdread-0.9.7.tar.gz'
-  homepage 'http://www.dtek.chalmers.se/groups/dvd/'
-  md5 '078788c9241ae16763529e1235502337'
+	url 'http://www.dtek.chalmers.se/groups/dvd/dist/libdvdread-0.9.7.tar.gz'
+	homepage 'http://www.dtek.chalmers.se/groups/dvd/'
+	md5 '078788c9241ae16763529e1235502337'
 
-  depends_on 'libdvdcss' => :optional
+	depends_on 'libdvdcss' => :optional
 
-  def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--mandir=#{man}",
-	                        "--with-libdvdcss"
-    system "make install"
-  end
+	def install
+		system "./configure", "--disable-debug", "--disable-dependency-tracking",
+			   "--prefix=#{prefix}", "--mandir=#{man}", "--with-libdvdcss"
+		system "make install"
+	end
 end
