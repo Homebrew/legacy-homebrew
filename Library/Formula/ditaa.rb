2602f7c7aeb4d9364f64651ef266876dd77c7c56
Antti Rasinen <ars@iki.fi>
2010-11-22 18:50:57 +0200

    New formula: ditaa

---
+++ Library/Formula/ditaa.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Ditaa <Formula
+  url 'http://downloads.sourceforge.net/project/ditaa/ditaa/0.9/ditaa0_9.zip'
+  homepage 'http://ditaa.sourceforge.net/'
+  # head 'https://ditaa.svn.sourceforge.net/svnroot/ditaa'
+  md5 '23f2e5ede60ef7763309c08addca071a'
+
+  def jar
+    'ditaa0_9.jar'
+  end
+
+  def script
+<<-EOS
+#!/bin/sh
+# A wrapper for ditaa.
+
+java -jar #{prefix}/#{jar} "$@"
+EOS
+  end
+
+  def install
+    prefix.install jar
+    (bin+'ditaa').write script
+  end
+end
