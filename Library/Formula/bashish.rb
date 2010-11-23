c6e1c77f5ed332cba8292646858ecb2869979ffa
David Höppner <0xffea@gmail.com>
2010-11-23 17:24:53 +0100

    New formula bashish
    Bashish is a theme enviroment for text terminals. It can change colors,
    font, transparency and background image on a per-application basis.
    Additionally Bashish supports prompt changing on common shells such as
    bash, zsh and tcsh.

---
+++ Library/Formula/bashish.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Bashish <Formula
+  url 'http://freefr.dl.sourceforge.net/project/bashish/bashish/2.2.4/bashish-2.2.4.tar.gz'
+  homepage 'http://bashish.sourceforge.net/'
+  md5 '0661cc0040878e921ac7a31b071d20e0'
+
+  depends_on 'dialog'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
