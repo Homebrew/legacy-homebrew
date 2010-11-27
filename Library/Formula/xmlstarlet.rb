908a28305b179c82e97ba678ba762f6fe6804176
Matthew L Daniel <mdaniel@gmail.com>
2010-11-27 11:57:43 +0100

    Version bump to 1.0.3 and fixup for configure patching.
    The configure patching is the road to ruin; better to attack the outcome text
    which we know because brew doesn't provide libxml2 and libxslt (which would
    cause them to live in a different path).
    
    Ultimately, the answer is not to sed xmlstarlet but to submit an upstream
    patch to support non-static builds. I'll take the lead on that.

--- Library/Formula/xmlstarlet.rb
+++ Library/Formula/xmlstarlet.rb
@@ -1,19 +1,27 @@
 require 'formula'
 
 class Xmlstarlet <Formula
-  url 'http://downloads.sourceforge.net/project/xmlstar/xmlstarlet/1.0.1/xmlstarlet-1.0.1.tar.gz'
-  md5 '8deb71834bcdfb4443c258a1f0042fce'
+  url 'http://downloads.sourceforge.net/project/xmlstar/xmlstarlet/1.0.3/xmlstarlet-1.0.3.tar.gz'
+  md5 'ec9a39540b402affcb99a07f0a03d92a'
   homepage 'http://xmlstar.sourceforge.net/'
 
   def install
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--disable-dependency-tracking"
     # thanks, xmlstarlet but OS X doesn't have the static versions
-    inreplace 'configure' do |s|
-      s.gsub! '${LIBXML_PREFIX}/lib/libxml2.a', '-lxml2'
-      s.gsub! '${LIBXSLT_PREFIX}/lib/libxslt.a', '-lxslt'
-      s.gsub! '${LIBXSLT_PREFIX}/lib/libexslt.a', '-lexslt'
+    # easier to replace the resulting paths than to track configure madness
+    # we know they are in /usr/lib because brew won't install libxml2 et al.
+    inreplace 'Makefile' do |s|
+      s.gsub! '/usr/lib/libxml2.a', '-lxml2'
+      s.gsub! '/usr/lib/libxslt.a', '-lxslt'
+      s.gsub! '/usr/lib/libexslt.a', '-lexslt'
+      s.gsub! '-DLIBXML_STATIC', ''
     end
-
-    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--disable-debug", "--disable-dependency-tracking"
+    inreplace 'src/Makefile' do |s|
+      s.gsub! '/usr/lib/libxml2.a', '-lxml2'
+      s.gsub! '/usr/lib/libxslt.a', '-lxslt'
+      s.gsub! '/usr/lib/libexslt.a', '-lexslt'
+      s.gsub! '-DLIBXML_STATIC', ''
+    end
     system "make"
     system "make install"
   end
