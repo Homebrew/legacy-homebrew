498a00dcc0666e3d80620a4f906b03c5c3159af4
Yasuharu NAKANO <ynak@jggug.org>
2010-12-01 00:26:19 +0900

    New Formula: ascii
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/ascii.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Ascii <Formula
+  url 'http://www.catb.org/~esr/ascii/ascii-3.10.tar.gz'
+  homepage 'http://www.catb.org/~esr/'
+  version '3.10'
+  md5 '9dcae3ab8b097efd16a24c5d20e0044e'
+
+  def install
+    system "make"
+    bin.install "ascii"
+  end
+end
