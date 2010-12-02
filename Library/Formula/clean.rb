5199a89d2fd6244a07f6ee1a4d46ed7a2bd7d4c4
Bertrand Marron <me@tusbar.net>
2010-12-02 12:17:25 +0100

    Added new formula for clean (Nifty Clean).
    The clean utility searches  through the filesystem for "temporary files"
    left behind by editors and the like which can be deleted safely.

---
+++ Library/Formula/clean.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Clean <Formula
+  url 'http://downloads.sourceforge.net/project/clean/clean/3.4/clean-3.4.tar.bz2'
+  homepage 'http://clean.sourceforge.net/'
+  md5 '7edc1f9c7c1fc33298fec329cf5dba01'
+
+  def install
+    system '/usr/bin/bsdmake'
+
+    bin.install 'clean'
+    man1.install 'clean.1'
+  end
+end
