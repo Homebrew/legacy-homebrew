adccd3849fa66e8ed7ed6c5d93f305c2235cc952
Thomas Gustafsson <tgu@mac.com>
2010-12-02 08:53:11 +0100

    changed from cvs HEAD to latest stable version 2.8p4

--- Library/Formula/owfs.rb
+++ Library/Formula/owfs.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Owfs <Formula
-  head 'cvs://:pserver:anonymous@owfs.cvs.sourceforge.net:/cvsroot/owfs:owfs'
+  url 'http://downloads.sourceforge.net/project/owfs/owfs/2.8p4/owfs-2.8p4.tar.gz'
+  version '2.8p4'
   homepage 'http://owfs.org/'
-  md5 ''
+  md5 'beccd8765184b2abea0a3f28dc466ea3'
 
   depends_on 'libusb-compat'
-
+  
   def install
     system "autoreconf -ivf"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
