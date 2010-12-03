4d3e22f6da83f2fddeaebb31d71d73bee9a24a86
Keith Fitzgerald <kfitzgerald@gmail.com>
2010-12-03 13:18:35 -0500

    ec2-api-tools actually not required

--- Library/Formula/cloud-watch.rb
+++ Library/Formula/cloud-watch.rb
@@ -8,8 +8,6 @@ class CloudWatch <AmazonWebServicesFormula
   homepage 'http://developer.amazonwebservices.com/connect/entry.jspa?externalID=2534&categoryID=251'
   url      'http://ec2-downloads.s3.amazonaws.com/CloudWatch-2010-08-01.zip'
   md5      '2ad0bc02b1ff16e541936f3676e7e847'
-  
-  depends_on 'ec2-api-tools'
 
   def install
     standard_install
