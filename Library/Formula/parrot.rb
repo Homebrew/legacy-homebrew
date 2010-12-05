8ab68f3a0a3dfbca968413c70aa87342ba43195f
Tomoki Aonuma <uasi@99cm.org>
2010-12-06 00:26:29 +0900

    parrot - fix HEAD URL
    Parrot is now hosted on GitHub.

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Parrot <Formula
-  head 'bzr://https://launchpad.net/parrot/trunk'
+  head 'git://github.com/parrot/parrot.git'
   url 'ftp://ftp.parrot.org/pub/parrot/releases/supported/2.9.1/parrot-2.9.1.tar.gz'
   homepage 'http://www.parrot.org/'
   md5 '5f68803d2a9f7488459337f0628ad8fc'
