818debde62d1520fe959577a9487bb3a7a688876
Mike McQuaid <mike@mikemcquaid.com>
2010-12-08 21:13:49 +0000

    Fix English in orpie comment.

--- Library/Formula/orpie.rb
+++ Library/Formula/orpie.rb
@@ -9,10 +9,9 @@ class Orpie <Formula
   depends_on 'objective-caml'
 
   def install
-    # OCAMLOPT=/usr/bin/false prevents configure from finding ocaml.opt,
-    # so orpie is built and runs as bytecode. Otherwise the build
-    # fails with "undefined symbol" errrors. This is similar to,
-    # but more elegant than, the macports Makefile.in patch.
+    # OCAMLOPT=/usr/bin/false prevents configure from finding ocaml.opt
+    # so orpie is built and runs as bytecode otherwise the build
+    # fails with "undefined symbol" errors.
     # This mechanism still works if /usr/bin/false doesn't exist,
     # although the build output will be uglier in that scenario.
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
