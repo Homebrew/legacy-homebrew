34a078aa5477c1a0706e36016d5f4db1309243a0
Bastien Dejean <fuyumugen@free.fr>
2010-12-09 22:19:42 +0100

    changed home

--- Library/Formula/picolisp.rb
+++ Library/Formula/picolisp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Picolisp <Formula
   url 'http://www.software-lab.de/picoLisp-3.0.4.tgz'
-  homepage 'http://www.software-lab.de/down.html'
+  homepage 'http://www.software-lab.de/doc/ref.html'
   md5 '02dcb487eb276939cea827b7a1384ce9'
 
   def install
