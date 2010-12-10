bd7bc7aed18eaf2b8868c9c3e676e1cad90f6645
Steven Kallstrom <steven@smallacts.com>
2010-12-09 23:52:52 -0600

    New Formula: lxsplit
    lxSplit is a simple tool for splitting files and joining the splitted files on unix-like platforms, such as Linux, FreeBSD, OpenBSD, etc. It is fully compatible with the HJSplit utility which is available for other operating systems. Splitting is done without compression.

---
+++ Library/Formula/lxsplit.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Lxsplit <Formula
+  url 'http://downloads.sourceforge.net/lxsplit/lxsplit-0.2.4.tar.gz'
+  homepage 'http://lxsplit.sourceforge.net/'
+  md5 'ed21a08c167c08d4d81c820782947cb1'
+
+  def install
+    system "make"
+    bin.install 'lxsplit'
+  end
+end
