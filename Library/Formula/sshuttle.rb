d9b500c67846da752c8ead0ce81dfd8f025c1d32
Adam Vandenberg <flangy@gmail.com>
2010-12-11 10:25:06 -0800

    sshuttle 0.43 (and HEAD)
    Transparent proxy server that works as a poor man's VPN. Forwards over ssh.
    Doesn't require admin access. Works with Linux and MacOS, now including 10.6.

---
+++ Library/Formula/sshuttle.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Sshuttle <Formula
+  url 'https://github.com/apenwarr/sshuttle/tarball/sshuttle-0.43'
+  homepage 'https://github.com/apenwarr/sshuttle'
+  md5 '590352aa7cbaad90c8f46dab64b829f4'
+
+  head 'git://github.com/apenwarr/sshuttle.git'
+
+  def install
+    libexec.install Dir['*']
+    (bin+'sshuttle').write <<-EOS.undent
+      #!/bin/bash
+      exec #{libexec}/main.py "$@"
+    EOS
+  end
+end
