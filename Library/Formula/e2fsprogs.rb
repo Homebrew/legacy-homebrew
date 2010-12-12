de1aae94a230b2aa9d90cb7eb82e21afd79cfb0d
Adam Vandenberg <flangy@gmail.com>
2010-12-11 16:53:43 -0800

    e2fsprogs 1.41.12

--- Library/Formula/e2fsprogs.rb
+++ Library/Formula/e2fsprogs.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class E2fsprogs <Formula
-  url 'http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/1.41.9/e2fsprogs-1.41.9.tar.gz'
+  url 'http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/1.41.12/e2fsprogs-1.41.12.tar.gz'
   homepage 'http://e2fsprogs.sourceforge.net/'
-  md5 '52f60a9e19a02f142f5546f1b5681927'
+  md5 '1b24a21fc0c2381ef420961cbfec733f'
 
   keg_only "This brew installs several commands which override OS X-provided file system commands."
 
