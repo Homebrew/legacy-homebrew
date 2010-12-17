58dfea692842f873c90f2de78849b202fc96fc62
Andrew Marshall <andrew@johnandrewmarshall.com>
2010-12-17 00:07:14 -0500

    Update s-lang to v2.2.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/s-lang.rb
+++ Library/Formula/s-lang.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class SLang <Formula
-  url 'ftp://space.mit.edu/pub/davis/slang/v2.2/slang-2.2.2.tar.bz2'
+  url 'ftp://space.mit.edu/pub/davis/slang/v2.2/slang-2.2.3.tar.bz2'
   homepage 'http://www.s-lang.org/'
-  md5 '974437602a781cfe92ab61433dd16d03'
+  md5 '17e1864de999ae9535a9f7350a010427'
 
   depends_on 'pcre' => :optional
   depends_on 'oniguruma' => :optional
