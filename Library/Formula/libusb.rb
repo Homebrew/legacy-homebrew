d957ee150f3e892041e6a748a1420cf59ab7b22b
Opticka <opticka1@vis22.ioo.ucl.ac.uk>
2010-12-20 18:34:11 +0000

    made libusb and liblabjack universal

--- Library/Formula/libusb.rb
+++ Library/Formula/libusb.rb
@@ -4,8 +4,17 @@ class Libusb <Formula
   url 'http://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.8/libusb-1.0.8.tar.bz2'
   homepage 'http://www.libusb.org/'
   md5 '37d34e6eaa69a4b645a19ff4ca63ceef'
+  
+  def options
+     [["--universal", "Build a universal binary."]]
+   end
 
   def install
+    if ARGV.include? '--universal'
+      ENV['CFLAGS'] += " -arch i386 -arch x86_64"
+      ENV['CXX_OPT'] = "-arch i386 -arch x86_64"
+      ENV['ARCHFLAGS'] = "-arch i386 -arch x86_64"
+    end
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
