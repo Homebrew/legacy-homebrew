94e76469444d862f38e6a4f0804bc9eec4817cbf
Oleg Oshmyan <chortos@inbox.lv>
2010-12-21 21:06:00 +0200

    New formula: icoutils

---
+++ Library/Formula/icoutils.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Icoutils <Formula
+  url 'http://savannah.nongnu.org/download/icoutils/icoutils-0.29.1.tar.bz2'
+  homepage 'http://www.nongnu.org/icoutils/'
+  md5 'b58f375e0f8731595e8d0ecdc3a0acb9'
+
+  depends_on 'libpng'
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-rpath",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
