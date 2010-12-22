70903d242ea009a91422952afec597f25c64fbde
Aku Kotkavuo <aku@hibana.net>
2010-12-22 20:46:13 +0200

    Update Rubinius to 1.2.0.

--- Library/Formula/rubinius.rb
+++ Library/Formula/rubinius.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Rubinius < Formula
-  url 'http://asset.rubini.us/rubinius-1.1.1-20101116.tar.gz'
-  version '1.1.1'
+  url 'http://asset.rubini.us/rubinius-1.2.0-20101221.tar.gz'
+  version '1.2.0'
   homepage 'http://rubini.us/'
-  md5 'b39f618eeba37c3aff215da8bca55fd7'
+  md5 '4284c2660f1f648942de35d4fc871f70'
   head 'git://github.com/evanphx/rubinius.git'
 
   # Do not strip binaries, or else it fails to run.
