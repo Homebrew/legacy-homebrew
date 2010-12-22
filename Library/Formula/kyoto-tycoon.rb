c4535abb65a3d7b21918d8288b6297867bc76623
Hideo Kimura <hide@hide-k.net>
2010-12-22 12:26:14 +0900

    update kyoto-tycoon to 0.9.18

--- Library/Formula/kyoto-tycoon.rb
+++ Library/Formula/kyoto-tycoon.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class KyotoTycoon <Formula
-  url 'http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.12.tar.gz'
+  url 'http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.18.tar.gz'
   homepage 'http://fallabs.com/kyototycoon/'
-  md5 '8a92ed5a1dcd6e3f0217d0a4901b6c5d'
+  md5 '756d898867000f01565468be998c4e8f'
 
   depends_on 'lua' unless ARGV.include? "--no-lua"
   depends_on 'kyoto-cabinet'
@@ -19,5 +19,6 @@ class KyotoTycoon <Formula
     system "./configure", *args
     system "make"
     system "make install"
+    Dir["#{libexec}/*"].each { |f| ln_s f, lib }
   end
 end
