3e7a5219b15d1b26469f326f77b167dd09773581
Mathew Oakes <Mathew.Oakes@memechine.org>
2010-12-28 19:19:24 +1100

    had missed fontconfig dependency

--- Library/Formula/abiword.rb
+++ Library/Formula/abiword.rb
@@ -14,6 +14,7 @@ class Abiword <Formula
   depends_on 'pango'
   depends_on 'imagemagick'
   depends_on 'libpng'
+  depends_on 'fontconfig'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
