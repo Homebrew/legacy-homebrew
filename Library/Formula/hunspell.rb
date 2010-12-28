5310cad9ec85cc12269b33cde8cc5b79ccb8450e
Vidar Gundersen <vibrog@github.com>
2010-12-28 21:14:08 +0100

    Build Hunspell as a stand-alone spellchecker

--- Library/Formula/hunspell.rb
+++ Library/Formula/hunspell.rb
@@ -5,8 +5,12 @@ class Hunspell <Formula
   homepage 'http://hunspell.sourceforge.net/'
   md5 '5ef2dc1026660d0ffb7eae7b511aee23'
 
+  depends_on 'readline'
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+           "--with-ui", "--with-readline",
+           "--prefix=#{prefix}"
     system "make"
     ENV.deparallelize
     system "make install"
