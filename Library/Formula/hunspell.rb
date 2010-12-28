e9aa1238f6f76363fc723c85bd7454459391e8ba
Mike McQuaid <mike@mikemcquaid.com>
2010-12-28 14:10:49 +0000

    Update hunspell to 1.2.12.

--- Library/Formula/hunspell.rb
+++ Library/Formula/hunspell.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Hunspell <Formula
-  url 'http://downloads.sourceforge.net/hunspell/hunspell-1.2.8.tar.gz'
+  url 'http://downloads.sourceforge.net/hunspell/hunspell-1.2.12.tar.gz'
   homepage 'http://hunspell.sourceforge.net/'
-  md5 '1177af54a09e320d2c24015f29c3a93e'
+  md5 '5ef2dc1026660d0ffb7eae7b511aee23'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make"
+    ENV.deparallelize
     system "make install"
   end
 end
