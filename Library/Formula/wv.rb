e1ae87cdc48966f6ecc2ad00a952bd92c4069a06
Mathew Oakes <Mathew.Oakes@memechine.org>
2010-12-28 12:47:27 +1100

    dependency for abiword

---
+++ Library/Formula/wv.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Wv <Formula
+  url 'http://www.paldo.org/paldo/sources/wv-1.0/wv-1.2.2.tar.bz2'
+  homepage 'http://www.paldo.org/index-section-packages-page-main-releaseid-124623.html'
+  md5 '31df6739e624a4af6c937e7732dcf647'
+
+  depends_on 'glib'
+  depends_on 'libgsf'
+  depends_on 'libpng'
+  depends_on 'libwmf'
+  depends_on 'libxml2'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
