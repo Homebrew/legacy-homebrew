a4033f9e025a0c8c540286d08fed6c58b1a1078b
Fabiano Sobreira <fabiano@sobreira.eti.br>
2010-12-29 22:14:20 -0200

    Grails need to be installed in libexec to avoid library conflicts with other Java applications

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -6,7 +6,28 @@ class Grails <Formula
   md5 '56fc68a118ca9c65e2c4391c3226a120'
 
   def install
+    # we don't need these
     rm_f Dir["bin/*.bat"]
-    prefix.install Dir['*']
+
+    # everything goes into libexec except the license and informational files
+    prefix.install %w[LICENSE README INSTALL]
+    libexec.install Dir["*"]
+
+    # creates symbolic links for the grails binaries
+    bin.mkpath
+    grails_binaries.each do |f|
+      next unless File.extname(f).empty?
+      ln_s f, bin+File.basename(f)
+    end
+  end
+
+  def grails_binaries
+    Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats; <<-EOS.undent
+    Create a GRAILS_HOME environment variable that points to:
+      #{libexec}
+    EOS
   end
 end
