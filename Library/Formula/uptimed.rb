cc6e2a651b84516fb51351a651628ad00feb0152
Adam Vandenberg <flangy@gmail.com>
2010-12-29 21:48:49 -0800

    uptimed 0.3.12

---
+++ Library/Formula/uptimed.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Uptimed <Formula
+  url 'http://podgorny.cz/uptimed/releases/uptimed-0.3.12.tar.bz2'
+  homepage 'http://podgorny.cz/moin/Uptimed'
+  md5 'c523d6434b672107ea00559bb38eb050'
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+
+    # Per MacPorts
+    inreplace "Makefile", "/var/spool/uptimed", "#{var}/uptimed"
+    inreplace "libuptimed/urec.h", "/var/spool", var
+    inreplace "etc/uptimed.conf-dist", "/var/run", "#{var}/uptimed"
+    system "make install"
+  end
+end
