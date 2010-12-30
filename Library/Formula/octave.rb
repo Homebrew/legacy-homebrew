bc71125b40f0862c48bdc801d7026b54a18d69f1
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2010-12-31 00:01:38 +0100

    Add all conservative options to configure
    read ./configure --help and add every --without-XXX possible and most
    conservative options

--- Library/Formula/octave.rb
+++ Library/Formula/octave.rb
@@ -5,19 +5,40 @@ class Octave <Formula
   homepage 'http://www.gnu.org/software/octave'
   md5 '90c39fa9e241ad2e978bcee4682a2ba9'
 
-  # depends_on 'gnu-sed'
+  depends_on 'gnu-sed'
+  #depends_on 'gawk'
+  depends_on 'readline'
+  #depends_on 'hdf5'
+  #depends_on 'fftw'
+  #depends_on 'curl'
+  depends_on 'gfortran'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-                          "--without-x"
-                          "--without-framework-carbon"
-                          "--without-hdf5"
-                          "--without-curl"
-                          "--without-glpk"
-                          "--without-blas"
-                          "--without-lapack"
+                          "--prefix=#{prefix}",
+                          "--without-x",
+                          "--without-framework-carbon",
+                          "--enable-readline",
+						  "--without-zlib",
+                          "--without-hdf5",
+                          "--without-fftw",
+						  "--without-glpk",
+                          "--without-curl",
+						  "--without-framework-opengl",
+                          "--without-blas",
+                          "--without-lapack",
+						  "--without-qrupdate",
+						  "--without-amd",
+						  "--without-umfpack",
+						  "--without-colamd",
+						  "--without-ccolamd",
+						  "--without-cholmod",
+						  "--without-cxsparse",
+						  "--without-arpack"
 
+    ENV.deparallelize
+	ENV["F77"] = "#{HOMEBREW_PREFIX}/bin/gfortran"
+	ENV["FFLAGS"] = ENV["CFLAGS"]
     system "make install"
   end
 end
