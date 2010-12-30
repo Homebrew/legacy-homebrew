6541915c108f471c562ddc31c20996430bfe6883
Mathias Laurin <Mathias.Laurin+github.com@gmail.com>
2010-12-31 00:25:27 +0100

    Add patch to lo-specfun.cc found on Macports
    lo-specfun.cc does not compile. Their is a patch on Macports that should
    fix it. The patch has now been copied after the __END__ of the formula.

--- Library/Formula/octave.rb
+++ Library/Formula/octave.rb
@@ -13,32 +13,58 @@ class Octave <Formula
   #depends_on 'curl'
   depends_on 'gfortran'
 
+  def patches
+    # comment
+    DATA
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--without-x",
                           "--without-framework-carbon",
                           "--enable-readline",
-						  "--without-zlib",
+                          "--without-zlib",
                           "--without-hdf5",
                           "--without-fftw",
-						  "--without-glpk",
+                          "--without-glpk",
                           "--without-curl",
-						  "--without-framework-opengl",
+                          "--without-framework-opengl",
                           "--without-blas",
                           "--without-lapack",
-						  "--without-qrupdate",
-						  "--without-amd",
-						  "--without-umfpack",
-						  "--without-colamd",
-						  "--without-ccolamd",
-						  "--without-cholmod",
-						  "--without-cxsparse",
-						  "--without-arpack"
+                          "--without-qrupdate",
+                          "--without-amd",
+                          "--without-umfpack",
+                          "--without-colamd",
+                          "--without-ccolamd",
+                          "--without-cholmod",
+                          "--without-cxsparse",
+                          "--without-arpack"
 
     ENV.deparallelize
-	ENV["F77"] = "#{HOMEBREW_PREFIX}/bin/gfortran"
-	ENV["FFLAGS"] = ENV["CFLAGS"]
+    ENV["F77"] = "#{HOMEBREW_PREFIX}/bin/gfortran"
+    ENV["FFLAGS"] = ENV["CFLAGS"]
+    # append -ff2c to FFLAGS if using blas
+    # There is a bug in lo-specfun.cc which prevents from compiling, 
+    # a patch is available from Macports
     system "make install"
   end
 end
+
+__END__
+
+--- liboctave/lo-specfun.cc
++++ liboctave/lo-specfun.cc
+@@ -25,6 +25,12 @@
+ #include <config.h>
+ #endif
+ 
++#if !defined (_REENTRANT)
++#define _REENTRANT
++#endif
++#include <cmath>
++#undef _REENTRANT
++
+ #include "Range.h"
+ #include "CColVector.h"
+ #include "CMatrix.h"
