7a9d980adcac307f49bb86fd1044b8887e04b4ec
Jonas Pfenniger <jonas@pfenniger.name>
2010-12-31 22:19:03 +0000

    brew up: star installed formulae
    When running `brew up`, if a mentioned formulae is also installed,
    it will get a wildcard start at the end.
    It makes it easier to see if any installed formulae is impacted.
    
    utils.rb: #puts_columns cols argument was useless, thus removed
              added the list of elements to star as second argument.

--- Library/Homebrew/cmd/update.rb
+++ Library/Homebrew/cmd/update.rb
@@ -29,11 +29,12 @@ class RefreshBrew
   UPDATED_EXAMPLE  = %r{^\s+#{example_regexp}}
   DELETED_EXAMPLE  = %r{^\s+delete mode \d+ #{example_regexp}$}
 
-  attr_reader :added_formulae, :updated_formulae, :deleted_formulae, :initial_revision
+  attr_reader :added_formulae, :updated_formulae, :deleted_formulae, :installed_formulae
   attr_reader :added_examples, :updated_examples, :deleted_examples
+  attr_reader :initial_revision
 
   def initialize
-    @added_formulae, @updated_formulae, @deleted_formulae = [], [], []
+    @added_formulae, @updated_formulae, @deleted_formulae, @installed_formulae = [], [], [], []
     @added_examples, @updated_examples, @deleted_examples = [], [], []
     @initial_revision = self.current_revision
   end
@@ -73,6 +74,9 @@ class RefreshBrew
     @added_examples.sort!
     @updated_examples.sort!
     @deleted_examples.sort!
+    @installed_formulae = HOMEBREW_CELLAR.children.
+      select{ |pn| pn.directory? }.
+      map{ |pn| pn.basename.to_s }.sort
 
     output.strip != GIT_UP_TO_DATE
   end
@@ -117,12 +121,12 @@ class RefreshBrew
     ## Deleted Formulae
     if deleted_formulae?
       ohai "The following formulae were removed:"
-      puts_columns deleted_formulae
+      puts_columns deleted_formulae, installed_formulae
     end
     ## Updated Formulae
     if pending_formulae_changes?
       ohai "The following formulae were updated:"
-      puts_columns updated_formulae
+      puts_columns updated_formulae, installed_formulae
     else
       puts "No formulae were updated."
     end
