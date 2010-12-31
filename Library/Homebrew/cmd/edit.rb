e210e38ed93b9a71b65b406af3126f93735eb857
Adam Vandenberg <flangy@gmail.com>
2010-12-31 11:00:15 -0800

    Use + instead of /.

--- Library/Homebrew/cmd/edit.rb
+++ Library/Homebrew/cmd/edit.rb
@@ -6,14 +6,14 @@ module Homebrew extend self
       # EDITOR isn't a good fit here, we need a GUI client that actually has
       # a UI for projects, so apologies if this wasn't what you expected,
       # please improve it! :)
-      exec 'mate', HOMEBREW_REPOSITORY/"bin/brew",
-                   HOMEBREW_REPOSITORY/'README.md',
-                   HOMEBREW_REPOSITORY/".gitignore",
-              *Dir[HOMEBREW_REPOSITORY/"Library/*"]
+      exec 'mate', HOMEBREW_REPOSITORY+"bin/brew",
+                   HOMEBREW_REPOSITORY+'README.md',
+                   HOMEBREW_REPOSITORY+".gitignore",
+              *Dir[HOMEBREW_REPOSITORY+"Library/*"]
     else
       # Don't use ARGV.formulae as that will throw if the file doesn't parse
       paths = ARGV.named.map do |name|
-        HOMEBREW_REPOSITORY/"Library/Formula/#{Formula.caniconical_name name}.rb"
+        HOMEBREW_REPOSITORY+"Library/Formula/#{Formula.caniconical_name name}.rb"
       end
       unless ARGV.force?
         paths.each do |path|
