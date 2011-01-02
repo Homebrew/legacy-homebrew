d8f560ce2e88541b0d8b35b476bc5460eb651d45
Ian <ian@Hobbes.local>
2011-01-02 12:28:17 +0100

    add --universal option to exodriver formula

--- Library/Formula/exodriver.rb
+++ Library/Formula/exodriver.rb
@@ -5,20 +5,28 @@ class Exodriver <Formula
   homepage 'http://labjack.com/support/linux-and-mac-os-x-drivers'
   md5 'ec227347ccaf0772d175d3a2cf557105'
   version '2.0-0-98cf1f8'
-
+  
   head 'https://github.com/labjack/exodriver.git', :using => :git
 
   depends_on 'libusb'
 
+  def options
+    [["--universal", "Build a universal binary."]]
+  end
+
   def install
     cd 'liblabjackusb'
     mv 'Makefile.MacOSX', 'Makefile'
-
+    
     inreplace 'Makefile' do |s|
       s.change_make_var! 'DESTINATION', lib
       s.change_make_var! 'HEADER_DESTINATION', include
+      if ARGV.include? '--universal'
+          s.gsub! /^CFLAGS \+\= \-fPIC \-g \-Wall$/, '#CFLAGS += -fPIC -g -Wall'
+          s.gsub! /^#CFLAGS \+\= \-fPIC \-g \-Wall \-arch i386 \-arch x86_64$/, 'CFLAGS += -fPIC -g -Wall -arch i386 -arch x86_64'
+      end
     end
-
+    
     system "make"
     system "make install"
   end
