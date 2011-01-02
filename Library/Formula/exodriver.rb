7f4e12c698507ce5f7901cdec37fcc799dff5c3d
Ian <ian@Hobbes.local>
2011-01-02 12:26:52 +0100

    revert before proper commit

--- Library/Formula/exodriver.rb
+++ Library/Formula/exodriver.rb
@@ -9,21 +9,11 @@ class Exodriver <Formula
   head 'https://github.com/labjack/exodriver.git', :using => :git
 
   depends_on 'libusb'
-  
-  def options
-     [["--universal", "Build a universal binary."]]
-   end
 
   def install
     cd 'liblabjackusb'
-    
-    if ARGV.include? '--universal'
-      system "sed 's/^CFLAGS += -fPIC -g -Wall$/#CFLAGS += -fPIC -g -Wall/' <Makefile.MacOSX >temp"
-      system "sed 's/^#CFLAGS += -fPIC -g -Wall -arch i386 -arch x86_64$/CFLAGS += -fPIC -g -Wall -arch i386 -arch x86_64/' <temp >Makefile.MacOSX"
-    end
-    
     mv 'Makefile.MacOSX', 'Makefile'
-    
+
     inreplace 'Makefile' do |s|
       s.change_make_var! 'DESTINATION', lib
       s.change_make_var! 'HEADER_DESTINATION', include
