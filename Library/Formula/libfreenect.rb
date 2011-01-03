3c3e82e6821fc75553928c12a99cf0b0b754acfd
Gijs Molenaar <gijsmolenaar@gmail.com>
2011-01-03 16:11:37 +0100

    switched to my own fork so i can compile it with opencv 2.2

--- Library/Formula/libfreenect.rb
+++ Library/Formula/libfreenect.rb
@@ -1,18 +1,19 @@
 require 'formula'
 
 class Libfreenect <Formula
-  url 'https://github.com/OpenKinect/libfreenect/tarball/master'
+  url 'https://github.com/gijzelaerr/libfreenect/tarball/master'
   version 'master'
   homepage 'http://openkinect.org'
-  md5 ''
+  md5 'dd0c93cd8d7672ce383e1670b787fcae'
   
   depends_on 'libusb-freenect'
   depends_on 'cmake'
+  depends_on 'opencv'
 
   def install
     mkdir "build"
     cd "build"
-    system "cmake .. #{std_cmake_parameters}"
+    system "cmake .. #{std_cmake_parameters} -DBUILD_CV=ON"
     system "make install"
   end
 end
