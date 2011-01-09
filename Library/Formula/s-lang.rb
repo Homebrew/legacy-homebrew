6e1c247479bf3e48a0df23f73877feaa5cd001fb
Ed Hickey <bassnode@gmail.com>
2011-01-08 18:55:22 -0800

    Fixed dead URL

--- Library/Formula/s-lang.rb
+++ Library/Formula/s-lang.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class SLang <Formula
-  url 'ftp://space.mit.edu/pub/davis/slang/v2.2/slang-2.2.2.tar.bz2'
+  url 'ftp://space.mit.edu/pub/davis/slang/v2.2/OLD/slang-2.2.2.tar.bz2'
   homepage 'http://www.s-lang.org/'
   md5 '974437602a781cfe92ab61433dd16d03'
 
