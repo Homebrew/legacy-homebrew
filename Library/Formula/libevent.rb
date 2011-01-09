0e2effc4c20974253e71ae8565fd31cc1df625e9
Austin Ziegler <austin@halostatue.ca>
2011-01-09 12:59:56 -0500

    Fixed libevent pull location.
    Monkey.org was down today so I couldn't install something that depended
    on libevent. I noticed that there was a source-based build environment
    at sourceforge, so I looked for the download on sourceforge. The recipe
    has been modified to download from sourceforge rather than Monkey.org.
    
    Changing libevent to libevent 2.0.
    
    0. libevent 1.4 and 2.0 can't be cleanly installed side-by-side as it's
       always "libevent" not "libevent" and "libevent2".
    1. tmux can use libevent 2.0, not just 1.4.
    2. llvm compile appeared to work without the error identified in the 1.4
       version.
    
    Update libevent URL to Sourceforge for reliability
    
    Closes #3846.

--- Library/Formula/libevent.rb
+++ Library/Formula/libevent.rb
@@ -1,14 +1,13 @@
 require 'formula'
 
 class Libevent <Formula
-  url "http://www.monkey.org/~provos/libevent-1.4.14b-stable.tar.gz"
+  version '2.0.10'
+  url 'http://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-2.0.10-stable.tar.gz/download'
   homepage 'http://www.monkey.org/~provos/libevent/'
-  md5 'a00e037e4d3f9e4fe9893e8a2d27918c'
+  md5 'a37401d26cbbf28185211d582741a3d4'
   head 'git://levent.git.sourceforge.net/gitroot/levent/levent'
 
   def install
-    fails_with_llvm "Undefined symbol '_current_base' reported during linking.", :build => 2326
-
     ENV.j1 # Needed for Mac Pro compilation
     system "./autogen.sh" if ARGV.build_head?
     system "./configure", "--prefix=#{prefix}"
