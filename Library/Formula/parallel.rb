9423d4e791d9e6a7d55aa9729792985126ed8af9
Jonathan Palardy <jonathan.palardy@gmail.com>
2011-01-10 21:15:46 -0500

    bump version to 20101222
    * update URL
    * update md5
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Parallel <Formula
-  url 'http://ftp.gnu.org/gnu/parallel/parallel-20101202.tar.bz2'
+  url 'http://ftp.gnu.org/gnu/parallel/parallel-20101222.tar.bz2'
   homepage 'http://savannah.gnu.org/projects/parallel/'
-  md5 '66d5c7825a05a1c641555868c9de955a'
+  md5 '7d531b835fa7ba2975a3d3cdf13aae4c'
 
   def install
     system "./configure", "--prefix=#{prefix}"
