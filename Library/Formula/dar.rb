dc7e7fd1211a2b514bd249c8c1a4029d44ceb9a3
Kaspar Schiess <kaspar.schiess@geo.uzh.ch>
2011-01-12 15:48:37 +0100

    + dar, the Disk ARchive
    Version 2.3.10 from http://dar.linux.free.fr/.

---
+++ Library/Formula/dar.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Dar <Formula
+  url 'http://downloads.sourceforge.net/project/dar/dar/2.3.10/dar-2.3.10.tar.gz'
+  homepage 'http://dar.linux.free.fr/doc/index.html'
+  md5 'f134276bb9dc761dbb318c5511e65833'
+
+  depends_on 'gettext'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", '--mandir=/usr/local/share'
+    system "make install"
+  end
+end
