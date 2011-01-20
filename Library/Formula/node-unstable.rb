731aa1cda5cea8b9dcd4ccd0b5f8222faf4c3c94
Chris <chris@superbilk.org>
2011-01-20 23:27:36 +0100

    added support for node.js unstable, currently v0.3.5

---
+++ Library/Formula/node-unstable.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class NodeUnstable <Formula
+  url 'http://nodejs.org/dist/node-v0.3.5.tar.gz'
+  head 'git://github.com/ry/node.git'
+  homepage 'http://nodejs.org/'
+  md5 '9ff8ce8f84c2bc693078b6e3538b31bf'
+
+  # Stripping breaks dynamic loading
+  skip_clean :all
+
+  def options
+    [["--debug", "Build with debugger hooks."]]
+  end
+
+  def install
+    fails_with_llvm
+
+    inreplace 'wscript' do |s|
+      s.gsub! '/usr/local', HOMEBREW_PREFIX
+      s.gsub! '/opt/local/lib', '/usr/lib'
+    end
+
+    args = ["--prefix=#{prefix}"]
+    args << "--debug" if ARGV.include? '--debug'
+
+    system "./configure", *args
+    system "make install"
+  end
+
+  def caveats
+    "Please add #{HOMEBREW_PREFIX}/lib/node to your NODE_PATH environment variable to have node libraries picked up."
+  end
+end
