6e640bfbceadcdd5585e2cf35faea0c1f6e21abe
Jerome Andrieux <jerome.andrieux@af83.com>
2011-01-25 09:20:07 +0100

    Fixed typo and removed version tag as it should be detected from the URL

--- Library/Formula/terminus-font.rb
+++ Library/Formula/terminus-font.rb
@@ -4,13 +4,12 @@ class TerminusFont <Formula
   url 'http://sourceforge.net/projects/terminus-font/files/terminus-font-4.32/terminus-font-4.32.tar.gz'
   homepage 'http://terminus-font.sourceforge.net/'
   md5 '9b74047edcc236a7d4af8abf966c3e7c'
-  version '4.32'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
 						  "--x11dir=#{share}/fonts"
     system "make"
     system "make install fontdir"
-	system "mkfontdir #{share}/fonts"
+    system "mkfontdir #{share}/fonts"
   end
 end
