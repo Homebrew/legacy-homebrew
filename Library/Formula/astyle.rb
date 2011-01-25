7976dd21de8d42d79e1c5953b0dbede91a137b59
Jorge Luis Mendez <jorge@jorgemendez.me>
2011-01-24 20:57:23 -0500

    Updated astyle formula to 2.01

--- Library/Formula/astyle.rb
+++ Library/Formula/astyle.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Astyle <Formula
-  url 'http://downloads.sourceforge.net/sourceforge/astyle/astyle_1.24_macosx.tar.gz'
-  md5 '9b63dadac58e867f14b3894befbdc9b3'
+  url 'http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.01/astyle_2.01_macosx.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fastyle%2Ffiles%2Fastyle%2Fastyle%25202.01%2F&ts=1295918029&use_mirror=softlayer', :using => CurlDownloadStrategy
+  md5 'f81408554bf93ea4ad4feb008a76202c'
   homepage 'http://astyle.sourceforge.net/'
+  version '2.01'
 
   def install
     Dir.chdir 'src' do
