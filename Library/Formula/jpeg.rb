5fed391beaaff5052a46493e2742adc51e941a40
Justin Clift <jclift@redhat.com>
2011-01-26 22:16:00 +1100

    Updated formula: jpeg
    Updated for new upstream release.

--- Library/Formula/jpeg.rb
+++ Library/Formula/jpeg.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Jpeg <Formula
-  url 'http://www.ijg.org/files/jpegsrc.v8b.tar.gz'
-  version '8b'
-  md5 'e022acbc5b36cd2cb70785f5b575661e'
+  url 'http://www.ijg.org/files/jpegsrc.v8c.tar.gz'
+  version '8c'
+  sha256 'edfc0b3e004b2fe58ffeeec89f96e3a3c28972c46725ec127d01edf8a1cc7c9a'
   homepage 'http://www.ijg.org'
 
   def install
