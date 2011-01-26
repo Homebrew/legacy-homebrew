e745778afa7af5e6b06043eb90d2f244938a7d1d
Matthias Bauer <moeffju@gmail.com>
2011-01-26 17:00:43 +0100

    Fix pngcrush URL
    The current release of pngcrush is always put into a folder named
    00-{version} so it shows up on top of SourceForge's sorting.
    That means that as soon as a new version is released, all old links
    break! Case in point: URL contained 00-1.7.13, but when 1.7.14 was
    released, that became 00-1.7.14 and the old directory became 1.7.13.

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pngcrush <Formula
   homepage 'http://pmt.sourceforge.net/pngcrush/'
-  url 'http://downloads.sourceforge.net/project/pmt/pngcrush/00-1.7.13/pngcrush-1.7.13.tar.bz2'
-  md5 'c0816dfc9318d7325008608d321a9228'
+  url 'http://downloads.sourceforge.net/project/pmt/pngcrush/00-1.7.14/pngcrush-1.7.14.tar.bz2'
+  md5 '56481356267dfdc1d1b30e2dd812f4b0'
 
   def install
     # use our CFLAGS, LDFLAGS, CC, and LD
