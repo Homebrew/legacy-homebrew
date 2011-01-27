bb4a7e82953f52113920a68acabe00f43127dcf4
Second Planet <contact@secondplanetanimation.com>
2011-01-26 20:00:56 -0500

    fixed formula synfig-studio

--- Library/Formula/synfig-studio.rb
+++ Library/Formula/synfig-studio.rb
@@ -1,12 +1,15 @@
 require 'formula'
 
-class Synfigstudio <Formula
+class SynfigStudio <Formula
   url 'https://downloads.sourceforge.net/project/synfig/synfigstudio/0.62.02/synfigstudio-0.62.02.tar.gz'
   homepage 'http://synfig.org'
   md5 ''
 
   depends_on 'etl'
   depends_on 'synfig'
+  depends_on 'gtkmm'
+  depends_on 'gtk+'
+  depends_on 'gettext'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
