a677e3033092a4c17709072bc10e1291d3a867eb
Takeshi Ohishi <tks.ohishi@gmail.com>
2011-01-26 18:34:27 -0800

    Add a option to support WebDAV module
    Closes #4035.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -18,7 +18,8 @@ class Nginx < Formula
 
   def options
     [
-      ['--with-passenger', "Compile with support for Phusion Passenger module"]
+      ['--with-passenger', "Compile with support for Phusion Passenger module"],
+      ['--with-webdav',    "Compile with support for WebDAV module"]
     ]
   end
 
@@ -40,6 +41,7 @@ class Nginx < Formula
             "--conf-path=#{etc}/nginx/nginx.conf", "--pid-path=#{var}/run/nginx.pid",
             "--lock-path=#{var}/nginx/nginx.lock"]
     args << passenger_config_args if ARGV.include? '--with-passenger'
+    args << "--with-http_dav_module" if ARGV.include? '--with-webdav'
 
     system "./configure", *args
     system "make install"
