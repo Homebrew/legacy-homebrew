2b5b500b68e9d45faec3d59998da75c623438a3c
Mcklaren <mcklaren@gmail.com>
2011-01-28 23:37:15 +0100

    Updated libqrencode to version 3.1.1

--- Library/Formula/libqrencode.rb
+++ Library/Formula/libqrencode.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libqrencode <Formula
-  url 'http://megaui.net/fukuchi/works/qrencode/qrencode-3.1.0.tar.gz'
+  url 'http://megaui.net/fukuchi/works/qrencode/qrencode-3.1.1.tar.gz'
   homepage 'http://megaui.net/fukuchi/works/qrencode/index.en.html'
-  md5 'fc300af948b36b2197ede0d131c12e21'
+  md5 'd97f67cbefaf577e6c15923f3cc57b6a'
 
   depends_on 'pkg-config' => :build
 
