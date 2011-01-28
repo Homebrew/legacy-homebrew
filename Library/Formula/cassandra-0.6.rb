385f05dae0a6c4d5a1534b4594d428afcf713b56
jhorman <jhorman@gmail.com>
2011-01-28 16:55:49 -0500

    Re-adds Cassandra 0.6 formula
    Cassandra 0.6.8 and 0.7.0 differ wildly. Many users will not want to, or cannot, upgrade. This commit adds a new formula for the Cassandra 0.6 branch. The idea being that cassandra-0.6.rb can track the 0.6 line, and cassandra.rb can track the 0.7 line.

---
+++ Library/Formula/cassandra-0.6.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class Cassandra06 < Formula
+  url 'http://www.mirrorservice.org/sites/ftp.apache.org//cassandra/0.6.8/apache-cassandra-0.6.8-bin.tar.gz'
+  homepage 'http://cassandra.apache.org'
+  md5 'a1a15b00e78fd3148c37d45900e08829'
+
+  def install
+    (var+"lib/cassandra").mkpath
+    (var+"log/cassandra").mkpath
+    (etc+"cassandra").mkpath
+
+    inreplace "conf/storage-conf.xml", "/var/lib/cassandra", "#{var}/lib/cassandra"
+    inreplace "conf/log4j.properties", "/var/log/cassandra", "#{var}/log/cassandra"
+
+    inreplace "bin/cassandra.in.sh" do |s|
+      s.gsub! "cassandra_home=`dirname $0`/..", "cassandra_home=#{prefix}"
+      # Store configs in etc, outside of keg
+      s.gsub! "CASSANDRA_CONF=$cassandra_home/conf", "CASSANDRA_CONF=#{etc}/cassandra"
+      # Jars installed to prefix, no longer in a lib folder
+      s.gsub! "$cassandra_home/lib/*.jar", "$cassandra_home/*.jar"
+    end
+
+    rm Dir["bin/*.bat"]
+
+    (etc+"cassandra").install Dir["conf/*"]
+    prefix.install Dir["*.txt"] + Dir["{bin,interface,javadoc}"]
+    prefix.install Dir["lib/*.jar"]
+  end
+end
