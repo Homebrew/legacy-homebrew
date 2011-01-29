45746b3097475c75c6d3eed48d4f6e11e19eb987
burningTyger <m8r-0478r31@safetymail.info>
2011-01-29 21:01:12 +0100

    ncftp 3.2.5
    3.2.4 was removed on ncftp server
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ncftp.rb
+++ Library/Formula/ncftp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ncftp <Formula
   homepage 'http://www.ncftp.com'
-  url 'ftp://ftp.ncftp.com/ncftp/ncftp-3.2.4-src.tar.gz'
-  md5 '418f3e660d33c8ae98e8a3dd80fb4e1c'
+  url 'ftp://ftp.ncftp.com/ncftp/ncftp-3.2.5-src.tar.gz'
+  md5 'cbcc1eb766e5798e31257abd8f52823b'
 
   def install
     system "./configure", "--disable-universal", "--disable-precomp",
