5eca966551ffecec1a0f406ffcad99dede61ed2b
Christian Höltje <docwhat@gerf.org>
2011-02-03 23:55:59 -0500

    Added formula for html2text
    html2text converts html markup to something you can view as plain text.
    This is very useful when installed *before* installing lesspipe (lesspipe
    figures out what is available at compile time).
    
    Closes #4121.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/html2text.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Html2text <Formula
+  url 'http://www.mbayer.de/html2text/downloads/html2text-1.3.2a.tar.gz'
+  homepage 'http://www.mbayer.de/html2text/'
+  md5 '6097fe07b948e142315749e6620c9cfc'
+
+  def install
+    inreplace 'configure',
+              'for i in "CC" "g++" "cc" "$CC"; do',
+              'for i in "g++"; do'
+
+    system "./configure"
+    system "make all"
+
+    bin.install "html2text"
+    man1.install "html2text.1.gz"
+    man5.install "html2textrc.5.gz"
+  end
+end
