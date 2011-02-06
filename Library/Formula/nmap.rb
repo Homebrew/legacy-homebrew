fdb45d135b17bf3642176a12041f15773084fc51
javallone <jeff.avallone@gmail.com>
2011-02-06 13:26:23 -0500

    Updating nmap formula to support using openssl installed via Homebrew

--- Library/Formula/nmap.rb
+++ Library/Formula/nmap.rb
@@ -5,11 +5,20 @@ class Nmap <Formula
   homepage 'http://nmap.org/5/'
   md5 'a4df96e52cb52a1bbe76caace5f21388'
 
+  depends_on "openssl" if ARGV.include? "--brewed-openssl"
+
+  def options
+    [["--brewed-openssl", "Use OpenSSL installed via Homebrew (fixes compilation errors on Leopard)"]]
+  end
+
   def install
     fails_with_llvm
     ENV.deparallelize
 
-    system "./configure", "--prefix=#{prefix}", "--without-zenmap"
+    args = ["--prefix=#{prefix}", "--without-zenmap"]
+    args << "--with-openssl=" + Formula.factory('openssl').prefix.to_s if ARGV.include? "--brewed-openssl"
+
+    system "./configure", *args
     system "make" # seperate steps required otherwise the build fails
     system "make install"
   end
