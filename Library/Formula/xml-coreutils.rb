fe8970279095673a26258065565903d801e2bdc3
Christian Romney <cromney@pointslope.com>
2011-02-07 14:13:42 -0500

    Fixed xml-coreutils homepage

--- Library/Formula/xml-coreutils.rb
+++ Library/Formula/xml-coreutils.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class XmlCoreutils <Formula
   url 'http://www.lbreyer.com/gpl/xml-coreutils-0.8a.tar.gz'
-  homepage 'http://www.lbreyer.com/gpl/xml-coreutils-0.8a.tar.gz'
+  homepage 'http://www.lbreyer.com/xml-coreutils.html'
   md5 '2a5fa1f1feffad6be2f8af7661408268'
 
   depends_on 'slang'
