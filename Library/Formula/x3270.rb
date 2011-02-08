c2f8c0720849eb3704781450483cc818d87838d8
Josa Gesell <email@josagesell.de>
2011-02-08 16:31:30 +0100

    Updated formula for x3270
    Added options:
    --with-c3270 :   Include c3270, which is a curses-based version of x3270
    --with-s3270 :   Include s3270, which is a displayless version for writing
                     screen-scraping scripts
    --with-tcl3270 : Include tcl3270, which is similar to s3270, but integrated
                     with Tcl
    --with-pr3287 :  Include pr3287, which is for printer emulation

--- Library/Formula/x3270.rb
+++ Library/Formula/x3270.rb
@@ -5,12 +5,60 @@ class X3270 <Formula
   homepage 'http://x3270.bgp.nu/'
   md5 '01d6d3809a457e6f6bd3731642e0c02d'
   version '3.3.11ga6'
-
+  
+  def options
+    [
+      ["--with-c3270",   "Include c3270, which is a curses-based version of x3270"],
+      ["--with-s3270",   "Include s3270, which is a displayless version for writing screen-scraping scripts"],
+      ["--with-tcl3270", "Include tcl3270, which is similar to s3270, but integrated with Tcl"],
+      ["--with-pr3287",  "Include pr3287, which is for printer emulation"]
+    ]
+  end
+  
+  
   def install
+  	ohai 'x3270'
     Dir.chdir 'x3270-3.3' do
       system "./configure", 	"--prefix=#{prefix}"
       system "make"
       system "make install"
+      system "make install.man";
+    end
+    if ARGV.include? "--with-c3270"
+      ohai 'c3270'
+      Dir.chdir 'c3270-3.3' do
+        system "./configure", 	"--prefix=#{prefix}"
+        system "make"
+        system "make install"
+        system "make install.man";
+      end
+    end
+    if ARGV.include? "--with-pr3287"
+      ohai 'pr3287'
+      Dir.chdir 'pr3287-3.3' do
+        system "./configure", 	"--prefix=#{prefix}"
+        system "make"
+        system "make install"
+        system "make install.man";
+      end
+    end
+    if ARGV.include? "--with-s3270"
+      ohai 's3270'
+      Dir.chdir 's3270-3.3' do
+        system "./configure", 	"--prefix=#{prefix}"
+        system "make"
+        system "make install"
+        system "make install.man";
+      end
+    end
+    if ARGV.include? "--with-tcl3270"
+      ohai 'tcl3270'
+      Dir.chdir 'tcl3270-3.3' do
+        system "./configure", 	"--prefix=#{prefix}"
+        system "make"
+        system "make install"
+        system "make install.man";
+      end
     end
   end
 end
