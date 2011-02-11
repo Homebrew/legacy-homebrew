a952e69a5b83f9abddd061c3092986e805f4026f
Bruno Aguirre <yo@brunoaguirre.com>
2011-02-11 15:58:54 -0200

    Added fixes from adamv

--- Library/Formula/pit.rb
+++ Library/Formula/pit.rb
@@ -2,15 +2,13 @@ require 'formula'
 
 class Pit < Formula
   url 'https://download.github.com/michaeldv-pit-0.1.0-5-g3e35117.tar.gz'
+  version '0.1.0-5'
   homepage 'http://github.com/michaeldv/pit'
   md5 '9c4aa317ded18b32fb299c2fd4424ef8'
 
-  def create_install_dir
-    system "mkdir -p #{prefix}/bin"
-  end
-
   def install
-    create_install_dir
+    bin.mkpath
+    (prefix+'bin').mkpath
 
     system "make"
     system "make install PREFIX=#{prefix}"
