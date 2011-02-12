5f5571d65757dee09f0b1cc90302c2566a2b4e46
Second Planet <contact@secondplanetanimation.com>
2011-02-12 15:55:06 -0500

    updated formula - fossil

--- Library/Formula/fossil.rb
+++ Library/Formula/fossil.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Fossil <Formula
-  url 'http://www.fossil-scm.org/download/fossil-src-20101207133137.tar.gz'
+  url 'http://www.fossil-scm.org/download/fossil-src-20110208125237.tar.gz'
   head 'fossil://http://www.fossil-scm.org/'
   homepage 'http://www.fossil-scm.org/'
-  md5 '3da8ef69f7865598edcfc48063f6f464'
+  md5 '274c1cf4db0d57b5af9a86b3bb74c63e'
 
   def install
     system "make"
