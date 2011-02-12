4f081d2ffab1f949701de0593128b12170e67a0d
Bertrand Marron <me@tusbar.net>
2011-02-12 23:59:46 +0100

    Updated feh formula.
    Made the formula less likely to break on upgrades by replacing the patch
    by environment variables set on install.
    Also removed the dependency on libpng.
    
    Closes #4221.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/feh.rb
+++ Library/Formula/feh.rb
@@ -6,27 +6,12 @@ class Feh <Formula
   md5 '3b2354d78a882ce02b429bbe053467a2'
 
   depends_on 'giblib' => :build
-  depends_on 'libpng' => :build
-
-  def patches
-    DATA
-  end
 
   def install
+    ENV.append "LDFLAGS", "-L/usr/X11/lib"
+    ENV.append "CFLAGS", "-I/usr/X11/include"
+
     system "make"
     system "make", "PREFIX=#{prefix}", "install"
   end
 end
-
-__END__
-Patch to add X11 library path.
-
-diff --git a/config.mk b/config.mk
-index 6a5b8b3..e301ff1 100644
---- a/config.mk
-+++ b/config.mk
-@@ -31,3 +31,4 @@ CFLAGS += ${xinerama} -DPREFIX=\"${PREFIX}\" \
-        -DPACKAGE=\"${PACKAGE}\" -DVERSION=\"${VERSION}\"
- 
- LDLIBS += -lm -lpng -lX11 -lImlib2 -lgiblib ${xinerama_ld}
-+LDFLAGS += -L/usr/X11/lib
