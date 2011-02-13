12ac1f3c4ff68a9aa8bddb609839791aa9c8beb6
Max Howell <max@methylblue.com>
2011-02-13 12:24:12 +0000

    Install redo manpages

--- Library/Formula/redo.rb
+++ Library/Formula/redo.rb
@@ -1,5 +1,10 @@
 require 'formula'
 
+class RedoDocs <Formula
+  head 'https://github.com/apenwarr/redo.git', {:using => :git, :branch => 'man' }
+  version 'foo'
+end
+
 class Redo <Formula
   version '0.06'
   url "https://github.com/apenwarr/redo/zipball/redo-#{version}"
@@ -9,5 +14,10 @@ class Redo <Formula
   def install
     ENV['PREFIX'] = prefix
     system "./redo install"
+    rm share/:doc/:redo/'README.md' # lets not have two copies
+
+    RedoDocs.new('redodocs').brew do |formula|
+      man1.install Dir['*']
+    end
   end
 end
