e949c66632233251fb8df76fd6faaca905ea45a3
Giulio Eulisse <giulio.eulisse@cern.ch>
2011-02-14 19:52:10 +0100

    Misc fixes.
    * Use patches and official repository rather than a fork.
    * Fix typo.

--- Library/Formula/ninja.rb
+++ Library/Formula/ninja.rb
@@ -1,13 +1,18 @@
 require 'formula'
 
 class Ninja <Formula
-  head 'git://github.com/alexgartrell/ninja.git'
+  head 'git://github.com/martine/ninja.git'
   homepage 'https://github.com/martine/ninja'
   md5 ''
 
   def install
     system "./bootstrap.sh"
     bin.mkpath
-    bin.install "ninha"
+    bin.install "ninja"
+  end
+  
+  def patches
+    ["https://github.com/alexgartrell/ninja/commit/27e38cf5fbdf1b88831c64f053312eeb1f172d64.diff",
+     "https://github.com/alexgartrell/ninja/commit/7c14d4d3b3e3abc2907037ede5131a221eca2ad3.diff"]
   end
 end
