85a7f621c6c9368b4bf687fdd9579ea000da8276
Martin Kühl <martin.kuehl@gmail.com>
2011-02-16 18:50:51 +0100

    Updated formula: `guile` version 2.0.0
    Guile 2.0.0 has been released and is the current stable version.
    Head is changed to use the git repository instead.

--- Library/Formula/guile.rb
+++ Library/Formula/guile.rb
@@ -1,15 +1,10 @@
 require 'formula'
 
 class Guile < Formula
-  url 'ftp://ftp.gnu.org/gnu/guile/guile-1.8.7.tar.gz'
-  head 'ftp://alpha.gnu.org/gnu/guile/guile-1.9.15.tar.gz'
+  url 'ftp://ftp.gnu.org/gnu/guile/guile-2.0.0.tar.gz'
+  sha1 'e266b79b17a04a98c438e9d5dabb5210fb368d77'
+  head 'git://git.sv.gnu.org/guile.git'
   homepage 'http://www.gnu.org/software/guile/'
-
-  if ARGV.build_head?
-    sha1 '0b1cdd5f0506dcda3ed85324b2a0ff510920ee76'
-  else
-    sha1 '24cd2f06439c76d41d982a7384fe8a0fe5313b54'
-  end
 
   depends_on 'pkg-config' => :build
   depends_on 'libffi'
