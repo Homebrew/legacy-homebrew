f9705ec14881921e762b52f99d5d9daef26f5e19
Laurent Gaches <laurent.gaches@gmail.com>
2011-02-17 14:12:04 +0100

    Updated Formula grails 1.3.7

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Grails <Formula
-  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-1.3.6.zip'
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-1.3.7.zip'
   homepage 'http://grails.org'
-  md5 '56fc68a118ca9c65e2c4391c3226a120'
+  md5 'ea7cbfecdcf30c861f8faa8552ce3b46'
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
