5931d65c865362c94b56ec6949e76abc3539a467
Matthew L Daniel <mdaniel@gmail.com>
2011-02-17 23:13:25 +0100

    Version bump and minor build fix for xmlstarlet.
    The ``src`` directory no longer contains a Makefile, so
    the search and replace was removed for it, but otherwise
    it was just a version bump.

--- Library/Formula/xmlstarlet.rb
+++ Library/Formula/xmlstarlet.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Xmlstarlet <Formula
-  url 'http://downloads.sourceforge.net/project/xmlstar/xmlstarlet/1.0.3/xmlstarlet-1.0.3.tar.gz'
-  md5 'ec9a39540b402affcb99a07f0a03d92a'
+  url 'http://downloads.sourceforge.net/project/xmlstar/xmlstarlet/1.0.5/xmlstarlet-1.0.5.tar.gz'
+  md5 '2a7e5333051fb0b38647cd1e94e81050'
   homepage 'http://xmlstar.sourceforge.net/'
 
   def install
@@ -11,12 +11,6 @@ class Xmlstarlet <Formula
     # easier to replace the resulting paths than to track configure madness
     # we know they are in /usr/lib because brew won't install libxml2 et al.
     inreplace 'Makefile' do |s|
-      s.gsub! '/usr/lib/libxml2.a', '-lxml2'
-      s.gsub! '/usr/lib/libxslt.a', '-lxslt'
-      s.gsub! '/usr/lib/libexslt.a', '-lexslt'
-      s.gsub! '-DLIBXML_STATIC', ''
-    end
-    inreplace 'src/Makefile' do |s|
       s.gsub! '/usr/lib/libxml2.a', '-lxml2'
       s.gsub! '/usr/lib/libxslt.a', '-lxslt'
       s.gsub! '/usr/lib/libexslt.a', '-lexslt'
