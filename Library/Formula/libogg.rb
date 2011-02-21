179e156f52471d9fae344fe4de5d201494115abe
Stein Magnus Jodal <stein.magnus@jodal.no>
2011-02-21 21:36:11 +0100

    Upgrade libogg to 1.2.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libogg.rb
+++ Library/Formula/libogg.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libogg <Formula
   homepage 'http://www.xiph.org/ogg/'
-  url 'http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz'
-  md5 'c95b73759acfc30712beef6ce4e88efa'
+  url 'http://downloads.xiph.org/releases/ogg/libogg-1.2.2.tar.gz'
+  md5 '5a9fcabc9a1b7c6f1cd75ddc78f36c56'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
