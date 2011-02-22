6012ec8decc4943dd2ed417ee64784822e7653f4
shedd <sheddr@sheddtech.com>
2011-02-22 00:26:14 +0000

    adding additional options to mysql_install_db instructions these options should resolve problems that users have had with the mysql system db not being created properly. see: http://blog.shedd.us/resolving-the-mysql-homebrew-install-issue/

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -1,20 +1,18 @@
 require 'formula'
 
 class Mysql <Formula
-  homepage 'http://dev.mysql.com/doc/refman/5.1/en/'
-  url 'http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.54.tar.gz'
-  md5 '2a0f45a2f8b5a043b95ce7575796a30b'
+  homepage 'http://dev.mysql.com/doc/refman/5.5/en/'
+  url 'http://ftp.sunet.se/pub/unix/databases/relational/mysql/Downloads/MySQL-5.5/mysql-5.5.9.tar.gz'
+  md5 '701c0c44b7f1c2300adc0dc45729f903'
 
   depends_on 'readline'
+  depends_on 'cmake' => :build
 
   def options
     [
       ['--with-tests', "Keep tests when installing."],
       ['--with-bench', "Keep benchmark app when installing."],
-      ['--with-embedded', "Build the embedded server."],
-      ['--client-only', "Only install client tools, not the server."],
-      ['--universal', "Make mysql a universal binary"],
-      ['--with-utf8-default', "Set the default character set to utf8"]
+      ['--universal', "Make mysql a universal binary"]
     ]
   end
 
@@ -25,43 +23,27 @@ class Mysql <Formula
   def install
     fails_with_llvm "https://github.com/mxcl/homebrew/issues/issue/144"
 
-    # See: http://dev.mysql.com/doc/refman/5.1/en/configure-options.html
-    # These flags may not apply to gcc 4+
-    ENV['CXXFLAGS'] = ENV['CXXFLAGS'].gsub "-fomit-frame-pointer", ""
-    ENV['CXXFLAGS'] += " -fno-omit-frame-pointer -felide-constructors"
+    args = [
+      ".",
+      "-DCMAKE_INSTALL_PREFIX='#{prefix}'",
+      "-DMYSQL_DATADIR='#{var}/mysql/data'",
+      "-DINSTALL_MANDIR='#{man}'",
+      "-DWITH_SSL=yes",
+      "-DDEFAULT_CHARSET='utf8'",
+      "-DDEFAULT_COLLATION='utf8_general_ci'",
+      "-DSYSCONFDIR='#{HOMEBREW_PREFIX}/etc'"]
+
+    args << "-DWITH_UNIT_TESTS=OFF" if not ARGV.include? '--with-tests'
+    args << "-DINSTALL_SQLBENCHDIR=" if not ARGV.include? '--with-bench'
 
     # Make universal for bindings to universal applications
-    ENV.universal_binary if ARGV.include? '--universal'
+    args << "-DCMAKE_OSX_ARCHITECTURES='ppc;i386'" if ARGV.include? '--universal'
 
-    configure_args = [
-      "--without-docs",
-      "--without-debug",
-      "--disable-dependency-tracking",
-      "--prefix=#{prefix}",
-      "--localstatedir=#{var}/mysql",
-      "--sysconfdir=#{etc}",
-      "--with-plugins=innobase,myisam",
-      "--with-extra-charsets=complex",
-      "--with-ssl",
-      "--without-readline", # Confusingly, means "use detected readline instead of included readline"
-      "--enable-assembler",
-      "--enable-thread-safe-client",
-      "--enable-local-infile",
-      "--enable-shared",
-      "--with-partition"]
+    system "cmake", *args
 
-    configure_args << "--without-server" if ARGV.include? '--client-only'
-    configure_args << "--with-embedded-server" if ARGV.include? '--with-embedded'
-    configure_args << "--with-charset=utf8" if ARGV.include? '--with-utf8-default'
+    system "make"
 
-    system "./configure", *configure_args
     system "make install"
-
-    ln_s "#{libexec}/mysqld", bin
-    ln_s "#{share}/mysql/mysql.server", bin
-
-    (prefix+'mysql-test').rmtree unless ARGV.include? '--with-tests' # save 66MB!
-    (prefix+'sql-bench').rmtree unless ARGV.include? '--with-bench'
 
     (prefix+'com.mysql.mysqld.plist').write startup_plist
   end
@@ -69,8 +51,12 @@ class Mysql <Formula
   def caveats; <<-EOS.undent
     Set up databases with:
         unset TMPDIR
-        mysql_install_db
-
+        cd #{prefix}
+        scripts/mysql_install_db --basedir=#{prefix} --user=mysql --tmpdir=/tmp
+        
+    Running the mysql_install_db command with the user and tmpdir option will
+        ensure that there is no issue creating your system databases.
+    
     If this is your first install, automatically load on login with:
         cp #{prefix}/com.mysql.mysqld.plist ~/Library/LaunchAgents
         launchctl load -w ~/Library/LaunchAgents/com.mysql.mysqld.plist
@@ -86,7 +72,7 @@ class Mysql <Formula
         plist with a version specific program argument.
 
     Or start manually with:
-        mysql.server start
+        mysqld_safe &
     EOS
   end
 
@@ -112,31 +98,3 @@ class Mysql <Formula
     EOPLIST
   end
 end
-
-
-__END__
---- old/scripts/mysqld_safe.sh	2009-09-02 04:10:39.000000000 -0400
-+++ new/scripts/mysqld_safe.sh	2009-09-02 04:52:55.000000000 -0400
-@@ -383,7 +383,7 @@
- fi
- 
- USER_OPTION=""
--if test -w / -o "$USER" = "root"
-+if test -w /sbin -o "$USER" = "root"
- then
-   if test "$user" != "root" -o $SET_USER = 1
-   then
-diff --git a/scripts/mysql_config.sh b/scripts/mysql_config.sh
-index efc8254..8964b70 100644
---- a/scripts/mysql_config.sh
-+++ b/scripts/mysql_config.sh
-@@ -132,7 +132,8 @@ for remove in DDBUG_OFF DSAFEMALLOC USAFEMALLOC DSAFE_MUTEX \
-               DEXTRA_DEBUG DHAVE_purify O 'O[0-9]' 'xO[0-9]' 'W[-A-Za-z]*' \
-               'mtune=[-A-Za-z0-9]*' 'mcpu=[-A-Za-z0-9]*' 'march=[-A-Za-z0-9]*' \
-               Xa xstrconst "xc99=none" AC99 \
--              unroll2 ip mp restrict
-+              unroll2 ip mp restrict \
-+              mmmx 'msse[0-9.]*' 'mfpmath=sse' w pipe 'fomit-frame-pointer' 'mmacosx-version-min=10.[0-9]'
- do
-   # The first option we might strip will always have a space before it because
-   # we set -I$pkgincludedir as the first option
