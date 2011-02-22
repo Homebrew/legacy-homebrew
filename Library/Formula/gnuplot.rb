54436d888f3b7b2f4232c6dc1bcd35bf695a032a
Lars <lars.eggert@gmx.net>
2011-02-22 15:13:19 +0200

    gnuplot 4.4.2

--- Library/Formula/gnuplot.rb
+++ Library/Formula/gnuplot.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gnuplot <Formula
-  url 'http://downloads.sourceforge.net/project/gnuplot/gnuplot/4.4.0/gnuplot-4.4.0.tar.gz'
+  url 'http://downloads.sourceforge.net/project/gnuplot/gnuplot/4.4.2/gnuplot-4.4.2.tar.gz'
   homepage 'http://www.gnuplot.info'
-  md5 'e708665bd512153ad5c35252fe499059'
+  md5 'a4f0dd89f9b9334890464f687ddd9f50'
 
   depends_on 'pkg-config' => :build
   depends_on 'readline'
