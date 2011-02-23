e6f5409cecb7479afe47e0a89a56afc14917f8ab
Carlos Borroto <carlos.borroto@gmail.com>
2011-02-23 18:06:26 -0500

    New formula:   Library/Formula/bedtools.rb

---
+++ Library/Formula/bedtools.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Bedtools <Formula
+  url 'http://bedtools.googlecode.com/files/BEDTools.v2.11.2.tar.gz'
+  homepage 'http://code.google.com/p/bedtools/'
+  md5 'f2d2b7e9ac4e92da7e0c339835a19ae4'
+
+  def install
+    system "make clean"
+    system "make all"
+    bin.install Dir['bin/*']
+  end
+end
