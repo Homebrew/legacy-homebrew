a3f3f4de96f2e936c0a822951e15e253fc6e1f55
JR Boyens <jr.boyens@chacha.com>
2011-02-26 08:59:45 -0500

    Fix whitespace on the fantom formula

--- Library/Formula/fantom.rb
+++ Library/Formula/fantom.rb
@@ -6,8 +6,8 @@ class Fantom <Formula
   md5 '5f5b7fbaae4e2cf76ded359c57530af2'
 
   def install
-	rm_f Dir["bin/*.exe", "lib/dotnet/*"]
-	rm_rf Dir["examples", "src"]
+    rm_f Dir["bin/*.exe", "lib/dotnet/*"]
+    rm_rf Dir["examples", "src"]
     libexec.install Dir['*']
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |f|
