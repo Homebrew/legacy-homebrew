13a566383e447df42d3d84972250fc595d708b69
Martin Brunthaler <martin@brunthaler.net>
2011-02-27 21:03:05 +0100

    Applied feedback from mikemcquaid

--- Library/Formula/nss.rb
+++ Library/Formula/nss.rb
@@ -1,33 +1,23 @@
 require 'formula'
 
 class Nss <Formula
-  version '3.12.9'
-  url "http://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_12_9_RTM/src/nss-#{version}.tar.gz"
+  url "http://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_3_12_9_RTM/src/nss-3.12.9.tar.gz"
   homepage 'http://www.mozilla.org/projects/security/pki/nss/'
   md5 'bd32f183ca28440c1744650be31a9ccc'
 
-  depends_on "sqlite"
   depends_on "nspr"
 
-  def patches
-    base = "http://trac.macports.org/export/75867/trunk/dports/net/nss/files"
-    {
-      :p0 => ["#{base}/patch-UNIX.mk.diff", "#{base}/patch-Darwin.mk.diff"]
-    }
-  end
-
   def install
+    # NSS build fails with parallel calls to make
     ENV.deparallelize
+
+    # Apply patches with inreplace
     Dir.chdir "mozilla/security" do
       inreplace ["coreconf/Darwin.mk", "coreconf/UNIX.mk", "nss/cmd/platlibs.mk",
                  "nss/lib/smime/config.mk", "nss/lib/ssl/config.mk"] do |s|
         s.gsub!('@executable_path', "#{lib}/nss")
-        s.gsub!('@@PREFIX@@/lib/nspr', "#{HOMEBREW_PREFIX}/lib")
-        s.gsub!('@@PREFIX@@/include/nspr', "#{HOMEBREW_PREFIX}/include/nspr")
-        s.gsub!('@@PREFIX@@', "#{prefix}")
-        s.gsub!('= cc\$', "= #{ENV.cc}")
-        s.gsub!('= c++\$', "= #{ENV.cxx}")
-        s.gsub!('= ranlib\$', "= /usr/bin/ranlib")
+        s.gsub!('DEFINES    += -DDEBUG -UNDEBUG -DDEBUG_$(USERNAME)',
+                "DEFINES    += -DDEBUG -UNDEBUG -DDEBUG_$(USERNAME) -I#{HOMEBREW_PREFIX}/include/nspr")
       end
     end
 
@@ -39,19 +29,24 @@ class Nss <Formula
     system "make -C mozilla/security/dbm #{args}"
     system "make -C mozilla/security/nss #{args}"
 
-    bin.mkdir
-    lib.mkdir
+    # We need to use system cp here as Mozilla cross-links all files into
+    # the dist hierarchy and Homebrew's Pathname.install moves the symlink
+    # into the keg rather than copying the referenced file.
     include.mkdir
-    (include+"nss").mkdir
+    include_target = include + "nss"
+    include_target.mkdir
+    Dir["mozilla/dist/public/nss/*"].each { |file| cp file, include_target }
+    Dir["mozilla/dist/public/dbm/*"].each { |file| cp file, include_target }
 
-    # We want to prefix all binaries with "nss-"
+    lib.mkdir
+    Dir["mozilla/dist/Darwin*/lib/*.dylib"].each { |file| cp file, lib }
+    Dir["mozilla/dist/Darwin*/lib/libcrmf.a"].each { |file| cp file, lib }
+
+    # We want to prefix all binaries with "nss-" to be sure that nothing
+    # collides with other binaries. We also need to use cp here 
+    bin.mkdir
     Dir["mozilla/dist/Darwin*/bin/*"].each do |file|
       cp file, "#{bin}/nss-" + File.basename(file)
     end
-
-    Dir["mozilla/dist/public/nss/*"].each { |file| cp file, include+"nss" }
-    Dir["mozilla/dist/public/dbm/*"].each { |file| cp file, include+"nss" }
-    Dir["mozilla/dist/Darwin*/lib/*.dylib"].each { |file| cp file, lib }
-    Dir["mozilla/dist/Darwin*/lib/libcrmf.a"].each { |file| cp file, lib }
   end
 end
