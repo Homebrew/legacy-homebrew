1e28f4a204eaf92905277aa1af2ec081a1f60f3e
David J. Weller-Fahy <dave@weller-fahy.com>
2011-02-27 22:18:15 -0800

    Add t-prot Formula
    'TOFU' protection script designed to be used as a filter for mail user
    agents (initially mutt).

---
+++ Library/Formula/t-prot.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class TProt <Formula
+  url 'http://www.escape.de/~tolot/mutt/t-prot/downloads/t-prot-2.101.tar.gz'
+  homepage 'http://www.escape.de/~tolot/mutt/'
+  md5 '9c2bec775c230b7532d0a4e17972d271'
+
+  depends_on 'Getopt::Long' => :perl
+
+  def install
+    bin.install 't-prot'
+    man1.install 't-prot.1'
+    (share + "doc/#{name}-#{version}").install Dir['*']
+#    (share + "doc/#{name}-#{version}/contrib").install Dir['contrib/*']
+#    (share + "doc/#{name}-#{version}").install ['ChangeLog', 'README', 'TODO']
+  end
+end
