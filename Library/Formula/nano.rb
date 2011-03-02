fe189008ece4110cc86f5df277c447839bf93470
Ville Korhonen <ville@xd.fi>
2011-03-02 02:17:18 +0200

    New formula: GNU nano (2.2.6)

---
+++ Library/Formula/nano.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Nano <Formula
+  url 'http://www.nano-editor.org/dist/v2.2/nano-2.2.6.tar.gz'
+  homepage 'http://www.nano-editor.org/'
+  md5 '03233ae480689a008eb98feb1b599807'
+
+  # depends_on 'cmake'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    # system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
