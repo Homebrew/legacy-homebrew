d98477307acf91e18e0d2d5126849172c5d71121
Barry Allard <barry@barryallard.name>
2011-03-02 07:21:18 -0800

    Metasploit formula: moved svn to head, added official tarball and updated version (tested).

--- Library/Formula/metasploit.rb
+++ Library/Formula/metasploit.rb
@@ -1,9 +1,13 @@
 require 'formula'
 
 class Metasploit <Formula
-  url "https://www.metasploit.com/svn/framework3/trunk/", :using => :svn, :revision => "9321"
-  version "3.4.0"
+  url "http://updates.metasploit.com/data/releases/framework-3.5.2.tar.bz2"
+  head "https://www.metasploit.com/svn/framework3/trunk/", :using => :svn, :revision => "HEAD"
   homepage 'http://www.metasploit.com/framework/'
+
+  if not ARGV.build_head?
+    @sha1 = "981ebf88e0bb65c65293c44185f8c4229137607f"
+  end
 
   def install
     libexec.install Dir["msf*",'data','external','lib','modules','plugins','scripts','test','tools']
