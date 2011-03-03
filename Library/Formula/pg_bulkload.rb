6ddff7d3a01152e200b41ecadf203a03112e9758
Konstantin Käfer <github@kkaefer.com>
2011-03-03 13:33:08 -0500

    pg_bulkload

---
+++ Library/Formula/pg_bulkload.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class PgBulkload <Formula
+  url 'http://pgfoundry.org/frs/download.php/2906/pg_bulkload-3.0.1.tar.gz'
+  homepage 'http://pgbulkload.projects.postgresql.org/'
+  md5 '1903498b38124b8c23ec6faa1b8e79ba'
+
+  depends_on 'postgresql'
+
+  def install
+    system "make USE_PGXS=1 install"
+  end
+
+  def caveats
+    return <<-EOS
+To register pg_bulkload with a database, run:
+    psql -f `pg_config --sharedir`/contrib/pg_bulkload.sql database_name
+EOS
+  end
+end
