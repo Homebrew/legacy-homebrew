9b36e8194c3e88e07aa7b55601f906562eea22e5
Alexis Hildebrandt <afh@surryhill.net>
2011-03-03 21:29:10 +0100

    Fix xcodebuild based formulae
    Add SYMROOT for xcodebuild in case the Xcode defaults
    PBXApplicationwideBuildSettings specify a custom build location

--- Library/Formula/imagesnap.rb
+++ Library/Formula/imagesnap.rb
@@ -6,7 +6,7 @@ class Imagesnap <Formula
   md5 'eddd65d04782cc7538c009cf8a6f7568'
 
   def install
-    system "xcodebuild -project ImageSnap.xcodeproj"
+    system "xcodebuild -project ImageSnap.xcodeproj SYMROOT=build"
     bin.install "build/Release/imagesnap"
   end
 end
