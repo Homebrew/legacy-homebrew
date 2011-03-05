a4896f0525ffab7a9da93b34ae2ffacac0555a25
Aku Kotkavuo <aku@hibana.net>
2011-03-05 11:23:24 +0200

    Update node.js to 0.4.2 and fix git repo
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Node <Formula
-  url 'http://nodejs.org/dist/node-v0.4.1.tar.gz'
-  head 'git://github.com/ry/node.git'
+  url 'http://nodejs.org/dist/node-v0.4.2.tar.gz'
+  head 'git://github.com/joyent/node.git'
   homepage 'http://nodejs.org/'
-  md5 '9566bdbd05c18cc2bbe1fa0fba60dd0a'
+  md5 '9e9e791e125f6a601ebc663dc99c72a8'
 
   # Stripping breaks dynamic loading
   skip_clean :all
