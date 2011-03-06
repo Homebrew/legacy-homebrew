8a08355729db8295db1e53af02904c36e0135217
Adam Vandenberg <flangy@gmail.com>
2011-03-05 21:31:35 -0800

    apache activemq 5.4.2

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Activemq <Formula
-  url 'http://www.reverse.net/pub/apache/activemq/apache-activemq/5.4.1/apache-activemq-5.4.1-bin.tar.gz'
+  url 'http://www.reverse.net/pub/apache/activemq/apache-activemq/5.4.2/apache-activemq-5.4.2-bin.tar.gz'
   homepage 'http://activemq.apache.org/'
-  md5 '9cac63ecfd93852cda8dec35e5feb56a'
+  md5 '804c1a4c01b80321a743648add1fdd06'
 
   skip_clean 'libexec/webapps/admin/WEB-INF/jsp'
 
