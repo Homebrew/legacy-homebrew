056465846ee0e50ec10d36afb8aae303e6915935
Alexis Hildebrandt <afh@surryhill.net>
2011-03-07 11:01:48 +0100

    Update Formula: openssl
    Bump to version 1.0.0d

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Openssl <Formula
-  url 'http://www.openssl.org/source/openssl-0.9.8o.tar.gz'
-  version '0.9.8o'
+  url 'http://www.openssl.org/source/openssl-1.0.0d.tar.gz'
+  version '1.0.0d'
   homepage 'http://www.openssl.org'
-  md5 '63ddc5116488985e820075e65fbe6aa4'
+  md5 '40b6ea380cc8a5bf9734c2f8bf7e701e'
 
   keg_only :provided_by_osx
 
