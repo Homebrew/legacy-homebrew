2b2c2a16a1658b9e883ef16a38d8f73be47d60de
Carlos Borroto <carlos.borroto@gmail.com>
2011-03-07 00:08:13 -0500

    samtools 0.1.13
    OS X patch no longer needed, as issue was fixed upstream.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/samtools.rb
+++ Library/Formula/samtools.rb
@@ -1,14 +1,11 @@
 require 'formula'
 
 class Samtools <Formula
-  url 'http://sourceforge.net/projects/samtools/files/samtools/0.1.12/samtools-0.1.12a.tar.bz2'
+  url 'http://sourceforge.net/projects/samtools/files/samtools/0.1.13/samtools-0.1.13.tar.bz2'
   homepage 'http://samtools.sourceforge.net/'
-  md5 '500ae32fb431de3940e0e955744b9b36'
+  md5 '2e66b94639f90d10cc935bb145ef71f3'
 
   def install
-    # -s is needed on OS X here.
-    inreplace ["Makefile", "bcftools/Makefile"], "$(AR)", "$(AR) -s"
-
     system "make"
     system "make razip"
 
