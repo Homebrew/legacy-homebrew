fad53cec67ab93a209fd1a5a01fc0be29a6b9317
Tomoki Aonuma <uasi@99cm.org>
2011-03-08 19:32:07 +0900

    New formula: gauche
    Gauche is an R5RS Scheme implementation.

---
+++ Library/Formula/gauche.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Gauche <Formula
+  url 'http://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.1.tgz'
+  homepage 'http://practical-scheme.net/gauche/'
+  md5 '6134e9c16aef2bc9fd3fa7e8fbebdd10'
+
+  def install
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--enable-multibyte=utf-8",
+      "--prefix=#{prefix}"
+    system "make"
+    system "make check"
+    system "make install"
+  end
+end
