fe190bb17b65b953f27f82c5fb8be984670e266a
Drew Hess <dhess-src@bothan.net>
2011-03-07 23:04:30 -0800

    mplayer: don't prompt for ffmpeg git pull in configure
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -8,6 +8,12 @@ class Mplayer <Formula
   depends_on 'pkg-config' => :build
   depends_on 'yasm' => :optional
 
+  def patches
+    # configure prompts the user to pull ffmpeg from git.
+    # Don't do that.
+    DATA
+  end
+
   def install
     # Do not use pipes, per bug report
     # https://github.com/mxcl/homebrew/issues#issue/622
@@ -28,3 +34,18 @@ class Mplayer <Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index ef60340..0b24e73 100755
+--- a/configure
++++ b/configure
+@@ -48,8 +48,6 @@ if test -e ffmpeg/mp_auto_pull ; then
+ fi
+ 
+ if ! test -e ffmpeg ; then
+-  echo "No FFmpeg checkout, press enter to download one with git or CTRL+C to abort"
+-  read tmp
+   if ! git clone --depth 1 git://git.videolan.org/ffmpeg.git ffmpeg ; then
+     rm -rf ffmpeg
+     echo "Failed to get a FFmpeg checkout"
