67148b391376ca63a20011f8848736a9cbdad146
Charlie Sharpsteen <source@sharpsteen.net>
2011-03-09 01:53:29 -0800

    Taproom class and brew-tap subcommands
    New class named Taproom. Instances of taproom represent folders where
    repositories ("Breweries") are checked out. The list of available repositories
    is generated from the fork network of a "founding brewery". Adamv's
    homebrew-alt repository is being used as the prototype founder.
    
    The "founding brewery" concept will probably need to be refactored away someday
    in the name of generalization, but right now it serves as a useful boundary by
    restricting the list of possible repositories to a well-known quantity.
    
    Also added support for what will eventually be the dispatch method of
    `brew-tap` subcommands. Currently implemented:
    
      - list: Prints out a "menu" that lists the available "breweries"
    
      - update: Refreshes the menu.

--- Library/Contributions/examples/brew-tap.rb
+++ Library/Contributions/examples/brew-tap.rb
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-require 'rubygems'
+require 'time'
+require 'yaml'
 
-# HTTParty is used because it provides a nice set of functions for querying the
+require 'rubygems'
+# HTTParty is used because it provides a nice abstraction for querying the
 # GitHub API and marshaling the JSON response into ruby objects. However, only
 # a couple of API calls are ever made---so HTTParty could probably be ditched
 # in favor of a system call to curl with no great loss in exchange for removing
@@ -20,19 +22,57 @@ this using the HTTParty gem, but that gem could not be imported.
   exit 1
 end
 
+
+TAPROOM = HOMEBREW_PREFIX + 'Library' + 'Taproom'
+FOUNDING_BREWERY = {:owner => 'adamv', :name => 'homebrew-alt'}
+
+
+module Homebrew extend self
+  def tap_dispatch taproom
+    # Main entry point. Responsible for dispatching calls to brew-tap
+    # subcommands. Modeled after the dispatch method of brew it's self.
+    cmd = ARGV.shift
+    if cmd == 'list'
+      Homebrew.tap_list taproom
+    elsif cmd == 'update'
+      Homebrew.tap_update taproom
+    end
+  end
+
+  def tap_list taproom
+    # Lists repositories available for tapping
+    menu = taproom.menu
+    ohai "Available breweries:\n"
+    menu[:breweries].each do |brewery|
+      puts <<-EOS.undent
+      #{brewery.brew_id}:
+        Name: #{brewery.name}
+        Brewmaster: #{brewery.owner}
+
+      EOS
+    end
+    ohai "Menu last updated: #{menu[:menu_updated]}"
+  end
+
+  def tap_update taproom
+    # Update list of available repositories and perform `brew update`-like
+    # operations on them
+    taproom.update_menu
+  end
+end
+
 class Brewery
-  # A Brewery is a class that represents a GitHub repository that contains
-  # Homebrew formulae.
+  # A Brewery represents a GitHub repository containing Homebrew formulae.
   include HTTParty
   base_uri 'github.com/api/v2/json/repos/show'
   format :json
 
   attr_reader :owner, :name, :brew_id
 
-  def initialize owner, name, brew_id = nil
-    @owner = owner
-    @name = name
-    @brew_id = brew_id || [owner, name].join('-')
+  def initialize args
+    @owner = args[:owner]
+    @name = args[:name]
+    @brew_id = args[:brew_id] || [owner, name].join('-')
   end
 
   def to_hash
@@ -61,21 +101,23 @@ class Brewery
   # These methods all hit the GitHub API. Therefore, they should only be
   # invoked through the memoized accessors defined above.
   def get_network
-    puts "Fetching #{@owner}'s #{@name} network."
+    ohai "Fetching #{@owner}'s #{@name} network"
     network = self.class.get("/#{@owner}/#{@name}/network").parsed_response['network']
     network.map do |repo|
       # We don't need all the information returned in the network array.
       # Abstract the important bits into new Brewery objects.
-      Brewery.new repo['owner'], repo['name']
+      brewery = Brewery.new :owner => repo['owner'], :name => repo['name']
     end
   end
 
   def get_branches
-    puts "Fetching #{@owner}'s #{@name} branch list."
+    ohai "Fetching #{@owner}'s #{@name} branch list."
     self.class.get("/#{@owner}/#{@name}/branches").parsed_response['branches']
   end
 
   def get_default_branch
+    # TODO: Decide if this method is necessary or not.
+    #
     # Unfortunately, GitHub has not implemented this natively in their API...
     # yet. They keep saying they will and the last response was something along
     # the lines of "Ooops, that was supposed to be in the last update". So
@@ -89,4 +131,46 @@ class Brewery
                                    # with it.)
   end
 end
+
+
+class Taproom
+  # The Taproom manages the offerings of several breweries.
+  attr_reader :path, :menu_path, :founder
+
+  def initialize path, founder
+    if not File.directory?(path)
+      Dir.mkdir path # Or die? Needz moar error checking!
+    end
+
+    @path = path
+    @menu_path = path + 'menu.yml'
+
+    # The fork network of the "founding brewery" is used to generate the menu
+    # available at this taproom. Not the most generalized, future-proof way to
+    # do it, but we have to start somewhere.
+    @founder = founder
+  end
+
+  def menu
+    @menu ||= get_menu
+  end
+
+  def get_menu
+    if not File.exists? @menu_path
+      update_menu
+    end
+
+    menu = YAML.load_file @menu_path
+    menu[:breweries] = menu[:breweries].map {|b| Brewery.new b}
+
+    return menu
+  end
+
+  def update_menu
+    menu = {:menu_updated => Time.now, :breweries => @founder.network.map {|b| b.to_hash}}
+    File.open(@menu_path, 'w') {|file| file.write(menu.to_yaml)}
+  end
+end
+
+Homebrew.tap_dispatch Taproom.new(TAPROOM, Brewery.new(FOUNDING_BREWERY))
 
