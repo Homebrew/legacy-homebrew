fed6d74758ef1329720a79bd5d81b6551e4599b5
Adam Vandenberg <flangy@gmail.com>
2011-03-12 17:43:53 -0800

    Fix inheritance for ScriptFileFormula.

--- Library/Formula/repo.rb
+++ Library/Formula/repo.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Repo <ScriptFileFormula
+class Repo < ScriptFileFormula
   homepage 'http://source.android.com/download/using-repo'
   head 'http://android.git.kernel.org/repo'
 end
