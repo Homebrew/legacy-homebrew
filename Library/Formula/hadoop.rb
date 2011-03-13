32db930933b447a146f03c9b255cd90df8a202f5
Adam Vandenberg <flangy@gmail.com>
2011-03-13 10:10:09 -0700

    hadoop 0.21.0

--- Library/Formula/hadoop.rb
+++ Library/Formula/hadoop.rb
@@ -1,31 +1,37 @@
 require 'formula'
 
 class Hadoop < Formula
-  url 'http://www.carfab.com/apachesoftware/hadoop/core/hadoop-0.20.2/hadoop-0.20.2.tar.gz'
+  url 'http://www.gtlib.gatech.edu/pub/apache/hadoop/core/hadoop-0.21.0/hadoop-0.21.0.tar.gz'
   homepage 'http://hadoop.apache.org/common/'
-  md5 '8f40198ed18bef28aeea1401ec536cb9'
+  md5 'ec0f791f866f82a7f2c1319a54f4db97'
 
   def shim_script target
     <<-EOS.undent
     #!/bin/bash
-    exec #{libexec}/bin/#{target} $*
+    exec #{libexec}/bin/#{target} $@
     EOS
   end
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install %w[bin conf contrib lib webapps]
+    libexec.install %w[bin conf lib webapps]
     libexec.install Dir['*.jar']
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |b|
       n = Pathname.new(b).basename
       (bin+n).write shim_script(n)
     end
+
+    inreplace "#{libexec}/conf/hadoop-env.sh",
+      "# export JAVA_HOME=/usr/lib/j2sdk1.6-sun",
+      "export JAVA_HOME=$(/usr/libexec/java_home)"
   end
 
-  def caveats
-    <<-EOS.undent
-      $JAVA_HOME must be set for Hadoop commands to work.
+  def caveats; <<-EOS.undent
+    In Hadoop's config file:
+      #{libexec}/conf/hadoop-env.sh
+    $JAVA_HOME has been set to be the output of:
+      /usr/libexec/java_home
     EOS
   end
 end
