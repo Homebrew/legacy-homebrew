6530586046b6a850c50f2d7d8aa275202ae63dcb
Max Howell <max@methylblue.com>
2011-03-13 12:52:41 +0000

    Replace create --cache with create --no-fetch
    Because since 0.8 we do the opposite, ie. download by default.

--- Library/Homebrew/cmd/create.rb
+++ Library/Homebrew/cmd/create.rb
@@ -85,7 +85,7 @@ class FormulaCreator
       puts "Version detected as #{version}."
     end
 
-    unless ARGV.include? "--no-md5" and version
+    unless ARGV.include? "--no-fetch" and version
       strategy = detect_download_strategy url
       @md5 = strategy.new(url, name, version, nil).fetch.md5 if strategy == CurlDownloadStrategy
     end
