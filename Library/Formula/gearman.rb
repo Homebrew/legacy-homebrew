24f4ad5e25aaa609932720c50efb1d80883fa561
Clint Shryock <clint@ctshryock.com>
2011-03-16 07:37:55 -0500

    Gearman 0.18
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gearman.rb
+++ Library/Formula/gearman.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Gearman < Formula
-  url 'http://launchpad.net/gearmand/trunk/gearman-0.16/+download/gearmand-0.16.tar.gz'
+  url 'http://launchpad.net/gearmand/trunk/0.18/+download/gearmand-0.18.tar.gz'
   homepage 'http://gearman.org/'
-  md5 '40b37df7525345021c72bfd81cccca20'
+  md5 '07374f1d9990925e41527276f13a0628'
 
   depends_on 'libevent'
+  depends_on 'boost'
 
   def install
     system "./configure", "--prefix=#{prefix}"
