af1168c6706274a10f8df8bb7ed23c13529d2938
Romuald Brunet <romuald@chivil.com>
2011-03-16 16:08:30 +0100

    ushare) removed gettext dependency

--- Library/Formula/ushare.rb
+++ Library/Formula/ushare.rb
@@ -5,7 +5,6 @@ class Ushare < Formula
   homepage 'http://ushare.geexbox.org/'
   md5 '5bbcdbf1ff85a9710fa3d4e82ccaa251'
 
-  depends_on 'gettext'
   depends_on 'libupnp'
   depends_on 'libdlna'
 
@@ -17,15 +16,14 @@ class Ushare < Formula
 
   def install
     # Need to explicitly add gettext here.
-    ENV.append 'LDFLAGS', "-lintl"
-
     inreplace 'configure', /config.h/, 'src/config.h'
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--enable-dlna",
                           "--with-libupnp-dir=#{HOMEBREW_PREFIX}",
                           "--with-libdlna-dir=#{HOMEBREW_PREFIX}",
-                          "--disable-strip"
+                          "--disable-strip",
+                          "--disable-nls"
     system "make install"
     man1.install "src/ushare.1"
   end
