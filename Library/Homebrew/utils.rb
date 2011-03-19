cb3649b9557b7af1485180ceab263c4b4f35ad9e
Adam Vandenberg <flangy@gmail.com>
2011-03-19 09:49:17 -0700

    Add methods to ArchitectureListExtension

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -145,6 +145,15 @@ module ArchitectureListExtension
   def universal?
     self.include? :i386 and self.include? :x86_64
   end
+
+  def remove_ppc!
+    self.delete :ppc7400
+    self.delete :ppc64
+  end
+
+  def as_arch_flags
+    self.collect{ |a| "-arch #{a}" }.join(' ')
+  end
 end
 
 # Returns array of architectures that the given command or library is built for.
