16b7bb9a163ad6bbfc2c7af18591ee859c04c6ea
Birger J. Nordølum <birgerjn@gmail.com>
2011-03-20 16:20:16 +0100

    vimpager: 1.4.3

--- Library/Formula/vimpager.rb
+++ Library/Formula/vimpager.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
 class Vimpager < Formula
-  url 'http://www.vim.org/scripts/download_script.php?src_id=14694'
-  version '1.4'
   homepage 'http://www.vim.org/scripts/script.php?script_id=1723'
-  md5 'b2c4c978c826876e11bf5453eb19c2a7'
+  url 'http://www.vim.org/scripts/download_script.php?src_id=15255'
+  version '1.4.3'
+  sha1 '9fada08a4f47a851e0919b4fb98dda6212b8c8eb'
 
   def install
-    bin.install 'download_script.php?src_id=14694' => 'vimpager'
+    bin.install 'download_script.php?src_id=15255' => 'vimpager'
   end
 
   def caveats; <<-EOS.undent
