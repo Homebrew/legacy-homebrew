ca35c7db9b4148d2b44998a456b4bd00a524fbf0
Juxi Leitner <juxi.leitner@gmail.com>
2011-03-21 12:07:17 +0100

    sdf

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -1,9 +1,12 @@
 require 'formula'
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 class Tig <Formula
   url 'http://jonas.nitro.dk/tig/releases/tig-0.16.2.tar.gz'
 =======
+=======
+>>>>>>> 0fcb6d84066603c90f68a35dddd3d709ceb82070
 class Tig < Formula
   url 'http://jonas.nitro.dk/tig/releases/tig-0.17.tar.gz'
 >>>>>>> 0fcb6d84066603c90f68a35dddd3d709ceb82070
