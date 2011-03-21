3b7e4204bb1d5247491d30d4890852cd4283fea1
shedd <sheddr@sheddtech.com>
2011-03-21 22:19:05 +0000

    restoring end per audit

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -96,3 +96,31 @@ class Mysql <Formula
     EOPLIST
   end
 end
+
+
+__END__
+--- old/scripts/mysqld_safe.sh  2009-09-02 04:10:39.000000000 -0400
++++ new/scripts/mysqld_safe.sh  2009-09-02 04:52:55.000000000 -0400
+@@ -383,7 +383,7 @@
+ fi
+
+ USER_OPTION=""
+-if test -w / -o "$USER" = "root"
++if test -w /sbin -o "$USER" = "root"
+ then
+   if test "$user" != "root" -o $SET_USER = 1
+   then
+diff --git a/scripts/mysql_config.sh b/scripts/mysql_config.sh
+index efc8254..8964b70 100644
+--- a/scripts/mysql_config.sh
++++ b/scripts/mysql_config.sh
+@@ -132,7 +132,8 @@ for remove in DDBUG_OFF DSAFEMALLOC USAFEMALLOC DSAFE_MUTEX \
+               DEXTRA_DEBUG DHAVE_purify O 'O[0-9]' 'xO[0-9]' 'W[-A-Za-z]*' \
+               'mtune=[-A-Za-z0-9]*' 'mcpu=[-A-Za-z0-9]*' 'march=[-A-Za-z0-9]*' \
+               Xa xstrconst "xc99=none" AC99 \
+-              unroll2 ip mp restrict
++              unroll2 ip mp restrict \
++              mmmx 'msse[0-9.]*' 'mfpmath=sse' w pipe 'fomit-frame-pointer' 'mmacosx-version-min=10.[0-9]'
+ do
+   # The first option we might strip will always have a space before it because
+   # we set -I$pkgincludedir as the first option
