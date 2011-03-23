1415d115d6fbbb70f16c3eaf68a2c35fc717a4fe
NanoTech <nanotech008@gmail.com>
2011-03-23 14:57:59 -0600

    polipo: update git url, use Homebrew's INFODIR, enable IPv6

--- Library/Formula/polipo.rb
+++ Library/Formula/polipo.rb
@@ -3,12 +3,12 @@ require 'formula'
 class Polipo < Formula
   url 'http://freehaven.net/~chrisd/polipo/polipo-1.0.4.tar.gz'
   homepage 'http://www.pps.jussieu.fr/~jch/software/polipo/'
-  head 'git://git.torproject.org/git/polipo'
+  head 'git://git.wifi.pps.jussieu.fr/polipo'
   md5 'defdce7f8002ca68705b6c2c36c4d096'
 
   def install
     cache_root = (var + "cache/polipo").mkpath
-    make_opts = "PREFIX=#{prefix} LOCAL_ROOT=#{share}/polipo/www DISK_CACHE_ROOT=#{cache_root} MANDIR=#{man}"
+    make_opts = "PREFIX=#{prefix} LOCAL_ROOT=#{share}/polipo/www DISK_CACHE_ROOT=#{cache_root} MANDIR=#{man} INFODIR=#{info} PLATFORM_DEFINES=-DHAVE_IPv6"
     system "make all #{make_opts}"
     system "make install #{make_opts}"
   end
