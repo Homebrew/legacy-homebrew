6d8aa2ddbf75bd01af9c1b929d2e2ba7d7755d20
Drew Hess <dhess-src@bothan.net>
2011-03-26 17:05:36 -0400

    google-app-engine: update to 1.4.2.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/google-app-engine.rb
+++ Library/Formula/google-app-engine.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class GoogleAppEngine < Formula
-  url 'http://googleappengine.googlecode.com/files/google_appengine_1.4.0.zip'
+  url 'http://googleappengine.googlecode.com/files/google_appengine_1.4.2.zip'
   homepage 'http://code.google.com/appengine/'
-  sha1 '8b5bac0ba85e98160fa0fada0c3e957326732947'
+  sha1 '132d50710b1337169981cd78914d36df29aea722'
 
   def install
     cd '..'
