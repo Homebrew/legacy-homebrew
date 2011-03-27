687e35c78512c192b167ee3b3457dd7a8aa66cd7
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-03-27 11:56:53 -0700

    update to scala 2.9.0.RC1

--- Library/Formula/scala.rb
+++ Library/Formula/scala.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class ScalaDocs < Formula
   homepage 'http://www.scala-lang.org/'
-  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.8.1.final-devel-docs.tgz'
-  version '2.8.1'
-  md5 'afd5c7d3073bd735a25cfc4ed61f3543'
+  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.9.0.RC1-devel-docs.tgz'
+  version '2.9.0.RC1'
+  md5 '995cfa4c3cc75b2245f9f377faa3b092'
 end
 
 class Scala < Formula
   homepage 'http://www.scala-lang.org/'
-  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.8.1.final.tgz'
-  version '2.8.1'
-  md5 '4fa66742341b5c9f6877ce64d409cb92'
+  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.9.0.RC1.tgz'
+  version '2.9.0.RC1'
+  md5 '2e646dd3cad7df3c6a39f203b1848302'
 
   def options
     [['--with-docs', 'Also install library documentation']]
