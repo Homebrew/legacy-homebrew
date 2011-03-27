44fa3e61c94f5c9fdce2dcda7b1cae0865a6a922
Joachim Beckers <joachim@jbeckers.be>
2011-03-27 16:50:28 +0200

    fix --with-tests build option

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -12,7 +12,7 @@ class Mysql < Formula
 
   def options
     [
-      ['--with-tests', "Keep tests when installing."],
+      ['--with-tests', "Build with unit tests."],
       ['--with-embedded', "Build the embedded server."],
       ['--with-bench', "Keep benchmark app when installing."],
       ['--universal', "Make mysql a universal binary"]
@@ -34,7 +34,10 @@ class Mysql < Formula
       "-DDEFAULT_COLLATION='utf8_general_ci'",
       "-DSYSCONFDIR='#{HOMEBREW_PREFIX}/etc'"]
 
+    # To enable unit testing at build, we need to download the unit testing suite
     args << "-DWITH_UNIT_TESTS=OFF" if not ARGV.include? '--with-tests'
+    args << "-DENABLE_DOWNLOADS=ON" if ARGV.include? '--with-tests'
+    
     args << "-DINSTALL_SQLBENCHDIR=" if not ARGV.include? '--with-bench'
 
     # Build the embedded server
