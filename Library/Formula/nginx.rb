ffba3e47d4c7ca498e5495a579d1c94a69185ceb
Dennis Krupenik <dennis@krupenik.com>
2011-03-28 00:18:36 +0300

    nginx: head updated to 0.9.6
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Nginx < Formula
   url 'http://nginx.org/download/nginx-0.8.54.tar.gz'
-  head 'http://nginx.org/download/nginx-0.9.5.tar.gz'
+  head 'http://nginx.org/download/nginx-0.9.6.tar.gz'
   homepage 'http://nginx.org/'
 
   if ARGV.build_head?
-    @md5='955960482bf55b537ad0db5cca8fd61a'
+    md5 'bb2dbacd4e66c4c578e0ac44b2f006b5'
   else
-    @md5='44df4eb6a22d725021288c570789046f'
+    md5 '44df4eb6a22d725021288c570789046f'
   end
 
   depends_on 'pcre'
@@ -42,9 +42,13 @@ class Nginx < Formula
   end
 
   def install
-    args = ["--prefix=#{prefix}", "--with-http_ssl_module", "--with-pcre",
-            "--conf-path=#{etc}/nginx/nginx.conf", "--pid-path=#{var}/run/nginx.pid",
+    args = ["--prefix=#{prefix}",
+            "--with-http_ssl_module",
+            "--with-pcre",
+            "--conf-path=#{etc}/nginx/nginx.conf",
+            "--pid-path=#{var}/run/nginx.pid",
             "--lock-path=#{var}/nginx/nginx.lock"]
+
     args << passenger_config_args if ARGV.include? '--with-passenger'
     args << "--with-http_dav_module" if ARGV.include? '--with-webdav'
 
