3ac99ae9164af405cfa02ca15b1054a0dd80e7d2
Dennis Krupenik <dennis@krupenik.com>
2011-03-28 00:18:36 +0300

    nginx: updated to 0.9.6

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Nginx < Formula
   url 'http://nginx.org/download/nginx-0.8.54.tar.gz'
-  head 'http://nginx.org/download/nginx-0.9.5.tar.gz'
+  head 'http://nginx.org/download/nginx-0.9.6.tar.gz'
   homepage 'http://nginx.org/'
 
   if ARGV.build_head?
-    @md5='955960482bf55b537ad0db5cca8fd61a'
+    @md5='bb2dbacd4e66c4c578e0ac44b2f006b5'
   else
     @md5='44df4eb6a22d725021288c570789046f'
   end
