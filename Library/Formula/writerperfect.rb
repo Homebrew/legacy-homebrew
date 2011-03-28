31616ebaabd25b1311c288df8394c75b6f41ba97
Benjamin Fleischer <github@benjaminfleischer.com>
2011-03-28 14:11:01 -0500

    Adds writerperfect Formula, can convert wpd to odt with wpd2odt
    writerperfect is a *nix binary wpd2odt that can convert a Wordperfect
    wpd document to an Open Office / LibreOffice odt document from the
    command line. http://libwpd.sourceforge.net/index.html
    
    (requires updated formulas for dependencies libwpd and libwpg)

---
+++ Library/Formula/writerperfect.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Writerperfect < Formula
+  url 'http://downloads.sourceforge.net/libwpd/writerperfect-0.8.0.tar.bz2'
+  md5 'cb55b682737ee99cff9c632f0b360372'
+  homepage 'http://libwpd.sourceforge.net/'
+
+  depends_on "libwpg"
+  depends_on "libwpd"
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "make install"
+  end
+end
+
