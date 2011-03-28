5091649076b539a6965d364804d8dbcfa618cda1
Aku <aku@hibana.net>
2011-03-28 12:37:52 +0300

    Update GHC to v7.0.3.

--- Library/Formula/ghc.rb
+++ Library/Formula/ghc.rb
@@ -1,42 +1,43 @@
 require 'formula'
+require 'hardware'
 
 class Ghc < Formula
+  def options
+    [
+      ['--i386', 'Install 32-bit version of GHC (default).'],
+      ['--x86_64', 'Install 64-bit version of GHC (experimental).']
+    ]
+  end
+
   homepage 'http://haskell.org/ghc/'
-  url "http://darcs.haskell.org/download/dist/6.12.3/GHC-6.12.3-i386.pkg"
-  version '6.12.3'
-  md5 '58399e3af68f50a23a847bdfe3de5aca'
+  version '7.0.3'
+  if not ARGV.include? '--x86_64'
+    url "http://www.haskell.org/ghc/dist/7.0.3/ghc-7.0.3-i386-apple-darwin.tar.bz2"
+    md5 '649912037de756cf4570f84e5d53cf9c'
+  else
+    url "http://www.haskell.org/ghc/dist/7.0.3/ghc-7.0.3-x86_64-apple-darwin.tar.bz2"
+    md5 '8a514a022ce21b8672f00054244faf26'
+  end
 
   # Avoid stripping the Haskell binaries & libraries.
   # See: http://hackage.haskell.org/trac/ghc/ticket/2458
   skip_clean ['bin', 'lib']
 
-  def replace_all foo, bar
-    # Find all text files containing foo and replace it with bar
-    files = `/usr/bin/grep -lsIR #{foo} .`.split
-    inreplace files, foo, bar
+  def install
+    if ARGV.include? '--x86_64' and !Hardware.is_64_bit?
+      onoe "The x86_64 version is only for 64-bit hardware."
+      exit 1
+    end
+
+    system "./configure --prefix=#{prefix}"
+    system "make install"
   end
 
-  def install
-    short_version = version.split('.').first(2).join('')
+  def caveats; <<-EOS.undent
+    32-bit version of GHC is installed by default, as the x84_64 version is
+    labelled experimental. Override with --x86_64.
 
-    # Extract files from .pax.gz
-    system '/bin/pax -f ghc.pkg/Payload -p p -rz'
-    cd "GHC.framework/Versions/#{short_version}/usr"
-
-    # Fix paths
-    replace_all "/Library/Frameworks/GHC.framework/Versions/#{short_version}/usr/lib/ghc-#{version}", "#{lib}/ghc"
-    replace_all "/Library/Frameworks/GHC.framework/Versions/#{short_version}/usr", prefix
-
-    prefix.install ['bin', 'share']
-
-    # Remove version from lib folder
-    lib.install "lib/ghc-#{version}" => 'ghc'
-
-    # Fix ghc-asm Perl reference
-    inreplace "#{lib}/ghc/ghc-asm", "#!/opt/local/bin/perl", "#!/usr/bin/env perl"
-
-    # Regenerate GHC package cache
-    rm "#{lib}/ghc/package.conf.d/package.cache"
-    system "#{bin}/ghc-pkg", 'recache', '--package-conf', "#{lib}/ghc/package.conf.d"
+    This brew is for GHC only; you might also be interested in haskell-platform.
+    EOS
   end
 end
