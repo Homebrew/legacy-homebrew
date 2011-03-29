941db842e3405cee07b6838066c6c75fb3a24c27
Luc Heinrich <luc@honk-honk.com>
2011-03-29 14:31:16 +0200

    New formula: tiff2png 0.91.

---
+++ Library/Formula/tiff2png.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Tiff2png < Formula
+  url 'ftp://ftp.simplesystems.org/pub/libpng/png/applications/tiff2png-0.91.tar.gz'
+  homepage 'http://www.libpng.org/pub/png/apps/tiff2png.html'
+  md5 'b5db7add863c5cf469197aa327c0b202'
+
+  depends_on 'libtiff'
+  depends_on 'libpng'
+  depends_on 'jpeg'
+
+  def install
+    libtiff = Formula.factory('libtiff')
+    libpng  = Formula.factory('libpng')
+    libjpeg = Formula.factory('jpeg')
+
+    inreplace 'Makefile.unx' do |s|
+        s.remove_make_var! 'DEBUGFLAGS'
+        s.change_make_var! 'CC', ENV.cc
+        s.change_make_var! 'LIBTIFF', libtiff.lib
+        s.change_make_var! 'TIFFINC', libtiff.include
+        s.change_make_var! 'LIBJPEG', libjpeg.lib
+        s.change_make_var! 'LIBPNG', libpng.lib
+        s.change_make_var! 'PNGINC', libpng.include
+        s.change_make_var! 'ZLIB', '/usr/lib'
+    end
+
+    system 'make -f Makefile.unx'
+    bin.install('tiff2png')
+  end
+end
