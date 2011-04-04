d18dad812f4d0389b8ec3d67881c1a278e1868a3
deepsweet <kir@soulshine.in>
2011-04-04 23:03:59 +0300

    xmlrpc-c 1.16.34 update

--- Library/Formula/xmlrpc-c.rb
+++ Library/Formula/xmlrpc-c.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class XmlrpcC < Formula
-  url 'http://downloads.sourceforge.net/sourceforge/xmlrpc-c/xmlrpc-c-1.06.39.tgz'
-  md5 'ec62fb15cca83ffeaf6a8d53b5ec8614'
+  url 'http://downloads.sourceforge.net/sourceforge/xmlrpc-c/xmlrpc-c-1.16.34.tgz'
+  md5 '41347f56aca5806e81164d3470812918'
   homepage 'http://xmlrpc-c.sourceforge.net/'
 
   def install
