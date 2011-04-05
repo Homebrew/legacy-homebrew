7665fe4b5fbcc3940104f6230d33184f28baee4f
Birger J. Nordølum <birgerjn@gmail.com>
2011-04-05 12:21:47 +0200

    Git Extras 0.4.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
 class GitExtras < Formula
-  url 'git://github.com/visionmedia/git-extras.git', :tag => '0.3.0'
-  version '0.3.0'
-  head 'git://github.com/visionmedia/git-extras.git', :branch => 'master'
-
   homepage 'https://github.com/visionmedia/git-extras'
+  url 'https://github.com/visionmedia/git-extras/tarball/0.4.0'
+  md5 '9dc9b45d44a82b3117c34429bc78d7a5'
+  head 'git://github.com/visionmedia/git-extras.git', :branch => 'master'
 
   def install
     inreplace 'Makefile', '/usr/local', prefix
