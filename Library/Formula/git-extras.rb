4c5ee0badb706acafcffd3f18cc7cd9efd00ae67
Birger J. Nordølum <birgerjn@gmail.com>
2011-04-05 12:21:47 +0200

    Git Extras 0.4.0

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
 class GitExtras < Formula
-  url 'git://github.com/visionmedia/git-extras.git', :tag => '0.3.0'
-  version '0.3.0'
-  head 'git://github.com/visionmedia/git-extras.git', :branch => 'master'
-
   homepage 'https://github.com/visionmedia/git-extras'
+  url 'git://github.com/visionmedia/git-extras.git', :tag => '0.4.0'
+  version '0.4.0'
+  head 'git://github.com/visionmedia/git-extras.git', :branch => 'master'
 
   def install
     inreplace 'Makefile', '/usr/local', prefix
