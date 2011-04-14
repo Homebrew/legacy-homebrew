d698a61b54ce4f744398033eb254713cda4a393a
Jack Nagel <jacknagel@gmail.com>
2011-04-13 21:46:01 -0500

    Update gdb to 7.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gdb.rb
+++ Library/Formula/gdb.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
-class Gdb <Formula
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.1.tar.bz2'
+class Gdb < Formula
+  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.2.tar.bz2'
   homepage 'http://www.gnu.org/software/gdb/'
-  md5 '21dce610476c054687b52770d2ddc657'
+  md5 '64260e6c56979ee750a01055f16091a5'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
