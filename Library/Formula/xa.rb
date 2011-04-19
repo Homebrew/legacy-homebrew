63307f5957507064c945eb73aa1858a406effc0c
Matthew Callis <matthew.callis@gmail.com>
2011-04-18 21:56:07 -0700

    Adding xa (xa65) portable cross-assembler.

---
+++ Library/Formula/xa.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Xa < Formula
+  url 'http://www.floodgap.com/retrotech/xa/dists/xa-2.3.5.tar.gz'
+  homepage 'http://www.floodgap.com/retrotech/xa/'
+  md5 'edd15aa8674fb86225faf34e56d5cab2'
+
+  def install
+    inreplace 'Makefile' do |s|
+      s.gsub! /\/usr\/local/, prefix
+    end
+
+    system "make"
+    system "make install"
+  end
+end
