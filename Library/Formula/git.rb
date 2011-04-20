7eb51116a5027141c5b7889fe6c054d569d340a4
Matt Di Pasquale <my@fake-email.com>
2011-04-19 23:04:31 -0400

    Git formula: tidy, clarify & add comments & output
    Comments: add more, fix punctuation, make concise
    Output: add instructions for Git bash completion

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -16,21 +16,23 @@ class Git < Formula
   homepage 'http://git-scm.com'
 
   def install
-    # if these things are installed, tell git build system to not use them
+    # If these things are installed, tell git build system to not use them.
     ENV['NO_FINK']='1'
     ENV['NO_DARWIN_PORTS']='1'
-    # If local::lib is used you get a 'Only one of PREFIX or INSTALL_BASE can be given' error
+    # Fix issue #333: https://github.com/mxcl/homebrew/issues/333
+    # Unset PERL_MM_OPT. Else, those who use local::lib get error:
+    # "Only one of PREFIX or INSTALL_BASE can be given."
     ENV['PERL_MM_OPT']='';
-    # build verbosely so we can debug better
+    # Build verbosely, so we can debug better.
     ENV['V']='1'
 
     inreplace "Makefile" do |s|
-      s.remove_make_var! %w{CFLAGS LDFLAGS}
+      s.remove_make_var! %w{CFLAGS LDFLAGS} # set by Homebrew in ENV.rb
     end
 
     system "make", "prefix=#{prefix}", "install"
 
-    # Install the git bash completion file.
+    # Install the Git bash completion file.
     # Put it into the Cellar so that it gets upgraded along with git upgrades.
     (prefix+'etc/bash_completion.d').install 'contrib/completion/git-completion.bash'
 
@@ -40,9 +42,9 @@ class Git < Formula
     # Install all other contrib files to share/contrib
     (share).install 'contrib'
 
-    # these files are exact copies of the git binary, so like the contents
-    # of libexec/git-core lets hard link them
-    # I am assuming this is an overisght by the git devs
+    # These files are exact copies of the git binary, so like the contents
+    # of libexec/git-core lets hard link them.
+    # I'm assuming this is an overisght by the git devs.
     git_md5 = (bin+'git').md5
     %w[git-receive-pack git-upload-archive].each do |fn|
       fn = bin + fn
@@ -51,14 +53,20 @@ class Git < Formula
       fn.make_link bin+'git'
     end
 
-    # we could build the manpages ourselves, but the build process depends
-    # on many other packages, and is somewhat crazy, this way is easier
+    # Download prebuilt manpages. Much faster & easier than building ourselves.
     GitManuals.new.brew { man.install Dir['*'] }
     GitHtmldocs.new.brew { (share+'doc/git-doc').install Dir['*'] }
   end
 
   def caveats; <<-EOS.undent
     Bash completion and emacs support have been installed.
+
+    For bash completion, add the following lines to your ~/.bash_profile file:
+
+    # Enable Bash tab-completion of Git commands, branch & file names, etc.
+    if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
+     	source `brew --prefix`/etc/bash_completion.d/git-completion.bash
+    fi
 
     The rest of the "contrib" folder has been copied to:
       #{share}/contrib
