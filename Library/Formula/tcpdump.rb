a35ddca219db6ff22e51fe6f7d5af821d6773d48
Mathieu Rhéaume <ddrmanxbxfr@gmail.com>
2011-04-20 01:54:44 -0400

    Added tcpdump 4.1.1 formula

---
+++ Library/Formula/tcpdump.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Tcpdump < Formula
+  url 'http://www.tcpdump.org/release/tcpdump-4.1.1.tar.gz'
+  homepage 'http://www.tcpdump.org'
+  md5 'd0dd58bbd6cd36795e05c6f1f74420b0'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
