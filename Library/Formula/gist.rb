2c421a129110e38bf189dbe2088cc1196ab563f9
Jack Nagel <jacknagel@gmail.com>
2011-04-21 16:00:06 -0500

    gist 2.0.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gist < Formula
-  url 'https://github.com/defunkt/gist/tarball/v2.0.1'
+  url 'https://github.com/defunkt/gist/tarball/v2.0.2'
   homepage 'https://github.com/defunkt/gist'
-  md5 'f6c8ef18f8019876cb54c9bfa3d4865b'
+  md5 'f43e0b5fd8cc3d0edab6aec840adf0b5'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
