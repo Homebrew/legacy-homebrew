9a2434691c0a20e9291c05140dc60f8758d7104c
Rupert Bedford <rupertpkb@gmail.com>
2011-04-23 20:27:56 +0100

    Changed the homepage

--- Library/Formula/ortp.rb
+++ Library/Formula/ortp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ortp < Formula
   url 'http://download.savannah.gnu.org/releases/linphone/ortp/sources/ortp-0.16.5.tar.gz'
-  homepage 'http://download.savannah.gnu.org/releases/linphone/ortp/docs/'
+  homepage 'http://www.linphone.org/eng/documentation/dev/ortp.html'
   md5 '94546901d14b85f97342f4ecf39489b1'
 
   def install
