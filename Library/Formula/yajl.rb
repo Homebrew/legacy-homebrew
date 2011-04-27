5307f453545031330fbfd9547d7a43083ebc8bde
Birger J. Nordølum <birgerjn@gmail.com>
2011-04-27 18:09:28 +0200

    YAJL 2.0

--- Library/Formula/yajl.rb
+++ Library/Formula/yajl.rb
@@ -2,13 +2,8 @@ require 'formula'
 
 class Yajl < Formula
   homepage 'http://lloyd.github.com/yajl/'
-  url 'http://github.com/lloyd/yajl/tarball/1.0.11'
-  md5 '5b60f4d59b3b1fb42d7808d08460fb12'
-
-  def patches
-    # All YAJL releases so far have an rpath bug, though its fixed in upstream git:
-    "https://github.com/lloyd/yajl/commit/a31c4d0f9ad90b4b58508702fd877bb35039067e.patch"
-  end
+  url 'http://github.com/lloyd/yajl/tarball/2.0.0'
+  sha256 '752033679036ca32cd938bc73d89096f74d51b049aeb5131ee0d47152b6363d5'
 
   # Configure uses cmake, even though it looks like we're
   # just using autotools below.
