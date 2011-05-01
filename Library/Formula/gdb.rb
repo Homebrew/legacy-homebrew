12cccbf29b385ad9d3548d86cf61e0d186fbd502
Jack Nagel <jacknagel@gmail.com>
2011-04-30 21:20:20 -0500

    Add caveats to gdb and fix Python support
    User-installed versions of gdb have to be code signed; this caveat
    points users to more information about this. Also, gdb has problems
    building against a Homebrew-installed Python. This patch forces it
    to build against the system Python.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gdb.rb
+++ Library/Formula/gdb.rb
@@ -6,8 +6,22 @@ class Gdb < Formula
   md5 '64260e6c56979ee750a01055f16091a5'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    args = ["--prefix=#{prefix}",
+            "--disable-debug",
+            "--disable-dependency-tracking",
+            "--with-python=/usr"]
+
+    system "./configure", *args
     system "make"
     system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    In order to make this build of gdb useful, you may have to code sign
+    the binary. In order to do this, you must create a certificate in your
+    System keychain. For more information, see:
+
+    http://sourceware.org/gdb/wiki/BuildingOnDarwin
+    EOS
   end
 end
