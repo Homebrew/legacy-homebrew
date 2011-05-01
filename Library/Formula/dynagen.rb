7f79cbdea14e9ea40342e253f4adb7a866339b2e
Andre Sencioles Vitorio Oliveira <andre@bcp.net.br>
2011-05-01 16:55:37 -0300

    New Formula: dynagen
    Dynagen is a front-end for use with the Dynamips Cisco router emulator.
    It uses an INI-like configuration file to provision Dynamips emulator
    networks.
    It takes care of specifying the right port adapters, generating and matching
    up those pesky NIO descriptors, specifying bridges, frame-relay, ATM switches,
    etc.
    It also provides a management CLI for listing devices, suspending and
    reloading instances, determining and managing idle-pc values, performing
    packet captures, etc.

---
+++ Library/Formula/dynagen.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Dynagen <Formula
+  url 'http://sourceforge.net/projects/dyna-gen/files/dynagen%20source%20_%20Linux/dynagen%200.11.0/dynagen-0.11.0.tar.gz'
+  homepage 'http://www.dynagen.org/'
+  md5 '3f88b3449b17096dca84d007f0b91b3f'
+
+  depends_on 'dynamips'
+
+  def install
+  end
+end
