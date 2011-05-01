76504975292332fcd6646c2e3f5bee921a717ce4
Timothée Peignier <timothee.peignier@tryphon.org>
2011-05-01 09:55:27 +0200

    us pypye 1.5 for 64 bits and pypy 1.4.1 for 32 bits

--- Library/Formula/pypy.rb
+++ Library/Formula/pypy.rb
@@ -2,10 +2,16 @@ require 'formula'
 require 'hardware'
 
 class Pypy < Formula
-  url 'http://pypy.org/download/pypy-1.5-osx64.tar.bz2'
-  md5 'b1417916bc01ebb9f95c666f5e397fb5'
+  if MacOS.prefer_64_bit?
+    url 'http://pypy.org/download/pypy-1.5-osx64.tar.bz2'
+    md5 'b1417916bc01ebb9f95c666f5e397fb5'
+    version '1.5.0'
+  else
+    url 'http://pypy.org/download/pypy-1.4.1-osx.tar.bz2'
+    md5 '8584c4e8c042f5b661fcfffa0d9b8a25'
+    version '1.4.1'
+  end
   homepage 'http://pypy.org/'
-  version '1.5.0'
 
   def install
     prefix.install ["bin", "lib-python", "lib_pypy"]
