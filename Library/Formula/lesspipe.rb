f56ebc3ac6ac4124a5570d3bdf4187e28c539274
Ches Martin <ches@whiskeyandgrits.net>
2011-05-06 13:33:41 +0700

    lesspipe: add option to enable syntax highlighting
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/lesspipe.rb
+++ Library/Formula/lesspipe.rb
@@ -5,7 +5,15 @@ class Lesspipe < Formula
   url 'http://www-zeuthen.desy.de/~friebel/unix/less/lesspipe-1.71.tar.gz'
   md5 '6d921dc4ce9809d405cb8d694ac7cbbd'
 
+  def options
+    [['--syntax-highlighting', 'Enable syntax highlighting']]
+  end
+
   def install
+    if ARGV.include? '--syntax-highlighting'
+      inreplace 'configure', %q{$ifsyntax = "\L$ifsyntax";}, %q{$ifsyntax = "\Ly";}
+    end
+
     system "./configure", "--prefix=#{prefix}", "--default"
     man1.mkpath
     system "make install"
