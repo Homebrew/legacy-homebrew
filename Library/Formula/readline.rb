9dd88f3f9b31c0ac460d2f8033bd2845fe464a3d
bin hong <hongbin119@gmail.com>
2011-05-06 12:13:00 +0800

    change readline download addr http://ftp.gnu.org/gnu/readline/

--- Library/Formula/readline.rb
+++ Library/Formula/readline.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Readline < Formula
   homepage 'http://tiswww.case.edu/php/chet/readline/rltop.html'
-  url 'ftp://ftp.cwru.edu/pub/bash/readline-6.2.tar.gz'
-  sha256 '79a696070a058c233c72dd6ac697021cc64abd5ed51e59db867d66d196a89381'
+  url 'http://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz'
+  sha256 '67948acb2ca081f23359d0256e9a271c'
   version '6.2.1'
 
   keg_only <<-EOS
@@ -14,7 +14,7 @@ EOS
 
   def patches
     {:p0 => [
-        "ftp://ftp.cwru.edu/pub/bash/readline-6.2-patches/readline62-001",
+        "http://ftp.gnu.org/gnu/readline/readline-6.2-patches/readline62-001",
       ]}
   end
 
