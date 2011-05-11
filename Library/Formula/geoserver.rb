10a51dbdba9c67f3f2561b25c12cbab1dc07b1e9
Martin Plöger <martin.ploeger@web.de>
2011-05-11 20:29:02 +0200

    refactored a bit

--- Library/Formula/geoserver.rb
+++ Library/Formula/geoserver.rb
@@ -21,26 +21,26 @@ class Geoserver < Formula
 
   def caveats
     s = "You can start/stop the geoserver via 'geoserver_startup.sh'/'geoserver_shutdown.sh'."
-    s << "\nConsider using the '--with-geowebcache' parameter to install the optional cache." unless ARGV.include? '--with-geowebcache'
+    s << "\nConsider using '--with-geowebcache' to install the optional cache." unless ARGV.include? '--with-geowebcache'
     s
   end
 
   def install
     rm_f Dir['bin/*.bat']
     prefix.install Dir['*.txt']
-    unless File.exists? var + name
+    unless File.exists? var + name + 'data_dir'
       (var + name).install Dir['data_dir']
     else
-      ohai 'Existing data directory found!', "Directory #{var + name} already exists. Skipping install of data_dir."
+      ohai "Directory #{var + name + 'data_dir'} already exists!", 'Skipping install of data_dir!'
       rm_rf Dir['data_dir']
     end
     libexec.install Dir['*']
-    Dir["#{libexec}/bin/*"].each do |file|
-      (bin + ('geoserver_' + File.basename(file))).write <<-EOS
-#!/bin/sh
-export GEOSERVER_DATA_DIR=#{var}/#{name}/data_dir
-export GEOSERVER_HOME=#{libexec}
-. #{file}
+    Dir[libexec + 'bin/*'].each do |file|
+      (bin + ('geoserver_' + File.basename(file))).write <<-EOS.undent
+        #!/bin/sh
+        export GEOSERVER_DATA_DIR=#{var + name}/data_dir
+        export GEOSERVER_HOME=#{libexec}
+        . #{file}
       EOS
     end
     Geowebcache.new.brew { (libexec + 'webapps/geowebcache').install Dir['*'] } if ARGV.include? '--with-geowebcache'
