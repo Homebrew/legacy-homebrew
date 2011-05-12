4ac8e2dc847690785785c0b4cd0ccca484b1112a
Lars <lars.eggert@gmx.net>
2011-05-12 10:43:50 -0400

    htmlcompressor 1.3.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/htmlcompressor.rb
+++ Library/Formula/htmlcompressor.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class Htmlcompressor < Formula
-  url 'http://htmlcompressor.googlecode.com/files/htmlcompressor-1.3.jar'
+  url 'http://htmlcompressor.googlecode.com/files/htmlcompressor-1.3.1.jar'
   homepage 'http://code.google.com/p/htmlcompressor/'
-  md5 '68296830cb99e4348284ea88602ed06b'
+  md5 'e0a1fde8210d3197e87bffa6e0d9ee0d'
 
   def install
-    libexec.install "htmlcompressor-1.3.jar"
+    libexec.install "htmlcompressor-1.3.1.jar"
     (bin+'htmlcompressor').write <<-EOS.undent
       #!/bin/sh
-      java -jar "#{libexec}/htmlcompressor-1.3.jar" $@
+      java -jar "#{libexec}/htmlcompressor-1.3.1.jar" $@
     EOS
   end
 end
