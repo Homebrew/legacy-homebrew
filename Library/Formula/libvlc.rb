557c884d7532baa8226dee0d95598ba46a19cf0d
Lee Hambley <lee.hambley@gmail.com>
2011-05-12 11:21:51 +0200

    Update formula for VLC 1.1.10

--- Library/Formula/libvlc.rb
+++ Library/Formula/libvlc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libvlc <Formula
 
-  url       'http://downloads.sourceforge.net/project/vlc/1.1.7/vlc-1.1.7.tar.bz2'
+  url       'http://downloads.sourceforge.net/project/vlc/1.1.10/vlc-1.1.10.tar.bz2'
   homepage  'http://www.videolan.org'
-  md5       '932ce1fa4afa54b56ac0ccc0655667b6'
+  md5       '066cb94b1e3aa848d828121354d6be4d'
 
   def install
     system 'cd extras/contrib && ./bootstrap ' + (snow_leopard_64? ? 'x86_64-apple-darwin10' : nil)
