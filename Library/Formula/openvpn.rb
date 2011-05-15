2a28d33fc66fd827416f60989c4cd6bf9ba23e92
Birger J. Nordølum <birgerjn@gmail.com>
2011-05-15 14:03:51 +0200

    OpenVPN 2.2.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/openvpn.rb
+++ Library/Formula/openvpn.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Openvpn < Formula
-  url 'http://swupdate.openvpn.net/community/releases/openvpn-2.1.4.tar.gz'
   homepage 'http://openvpn.net/'
-  md5 '96a11868082685802489254f03ff3bde'
+  url 'http://build.openvpn.net/downloads/releases/openvpn-2.2.0.tar.gz'
+  sha256 '71ebeaaba1641e6d1454ae0f10f986002e5c6afdcd09da659122e3e5c74a2567'
 
   depends_on 'lzo' => :recommended
 
