bb4ef0380f61b196b7a78e641f1d0131ce13c4be
Brian Riddle <brian.riddle@tv4.se>
2011-05-16 13:44:07 +0200

    analog 6.0
    initial commit of analog a access log analizer.

---
+++ Library/Formula/analog.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Analog < Formula
+  url 'http://analog.cx/analog-6.0.tar.gz'
+  homepage 'http://analog.cx'
+  md5 '743d03a16eb8c8488205ae63cdb671cd'
+
+  def install
+    system "make"
+    bin.install "analog"
+    etc.install "analog.cfg"
+    doc.install Dir["docs/*"]
+    #not sure if this is the best way
+    #to handle this man page
+    cp "analog.man", "analog.1"
+    man1.install "analog.1"
+  end
+end
