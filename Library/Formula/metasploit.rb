37155e6e8cec0250e7f5f9f43b397a80c581e917
Franz Bettag <franz@bett.ag>
2011-05-17 19:30:00 +0200

    Metasploit 3.7.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/metasploit.rb
+++ Library/Formula/metasploit.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Metasploit < Formula
-  url "https://www.metasploit.com/svn/framework3/trunk/", :using => :svn, :revision => "9321"
-  head "https://www.metasploit.com/svn/framework3/trunk/", :using => :svn
-  version "3.4.0"
+  url "http://updates.metasploit.com/data/releases/framework-3.7.1.tar.bz2"
   homepage 'http://www.metasploit.com/framework/'
+  sha1 '9a104bcacc8ecf683f5df16613da19076c81413c'
+
+  head "https://www.metasploit.com/svn/framework3/trunk/", :using => :svn
 
   def install
     libexec.install Dir["msf*",'data','external','lib','modules','plugins','scripts','test','tools']
