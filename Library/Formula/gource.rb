128571df7b0632229afeb663339fa171ba286fac
Robin Ramael <robin.ramael@gmail.com>
2011-05-17 18:31:04 +0200

    Updated gource to 0.34

--- Library/Formula/gource.rb
+++ Library/Formula/gource.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gource < Formula
   homepage 'http://code.google.com/p/gource/'
-  url 'git://github.com/acaudwell/Gource.git', :tag => "ae14ffc6135b4cf0a89"
-  version "0.33"
+  url 'git://github.com/acaudwell/Gource.git', :tag => "7730617ba28b9e6d72e9"
+  version "0.34"
   head 'git://github.com/acaudwell/Gource.git'
 
   depends_on 'pkg-config' => :build
