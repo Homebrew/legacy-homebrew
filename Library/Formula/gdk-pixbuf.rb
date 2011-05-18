fb80122f0abaada6a19c79ed060f2a761e6f4975
Penn Su <pennsuaccount@gmail.com>
2011-05-18 22:19:49 +0800

    Revert "Revert "Fixed the issue#5658, my last pull request was erroneous so just ignore it.""
    This reverts commit 3fa54aed83443484e774af7a9653de8d6de995d3.

--- Library/Formula/gdk-pixbuf.rb
+++ Library/Formula/gdk-pixbuf.rb
@@ -9,8 +9,10 @@ class GdkPixbuf < Formula
   depends_on 'jasper'
 
   def install
-    # added the missing path for PKGCONFIG to find glib-2.0.pc
-    system "export PKG_CONFIG_PATH=#{lib}/pkgconfig:$PKG_CONFIG_PATH"
+    # added the missing path for PKGCONFIG to find glib-2.0.pc, unfortunately
+      # I don't know the way to specify the prefix path for another formula,
+      # this is a bit hack-ish way of fixing it.
+    ENV['PKG_CONFIG_PATH'] = "#{HOMEBREW_PREFIX}/Cellar/glib/2.28.6/lib/pkgconfig"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-libjasper",
