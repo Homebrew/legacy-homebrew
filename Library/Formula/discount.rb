d8c85f3f525f5d91fc675931c1634571936b896f
Jack Nagel <jacknagel@gmail.com>
2011-05-19 04:20:04 -0500

    discount 2.0.9
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/discount.rb
+++ Library/Formula/discount.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Discount < Formula
-  url 'https://github.com/Orc/discount/tarball/v2.0.8'
+  url 'https://github.com/Orc/discount/tarball/v2.0.9'
   homepage 'http://www.pell.portland.or.us/~orc/Code/markdown/'
-  md5 '36b3abee3f3c7b370555f353b80857df'
+  md5 '3190e8f3d4a607537faa4ccc3d92ea6d'
 
   def install
     system "./configure.sh", "--prefix=#{prefix}", "--mandir=#{man}",
