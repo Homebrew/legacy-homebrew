82790f8cfdebb16dde07a013c11e3f1feac47f90
Jack Chen (chendo) <me@chen.do>
2011-05-20 10:31:14 +1000

    Using a more explicit url for filepath tarball

--- Library/Formula/filepath.rb
+++ Library/Formula/filepath.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Filepath < Formula
   version '0.3'
-  url 'https://github.com/chendo/filepath/tarball/v0.3'
+  url 'https://github.com/downloads/chendo/filepath/filepath-0.3.tar.gz'
   homepage 'http://github.com/chendo/filepath'
   md5 '74d03212ca4915d82b4a0c7f77bf10ac'
 
