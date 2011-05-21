4e435701da3aa3e3da955f1925f6677177f98993
Sebastian Staudt <koraktor@gmail.com>
2011-05-21 09:36:15 +0200

    git-extras 0.5.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitExtras < Formula
   homepage 'https://github.com/visionmedia/git-extras'
-  url 'https://github.com/visionmedia/git-extras/tarball/0.4.1'
-  sha1 'fdc105a9437296d207eb981ff23440ce8f115034'
+  url 'https://github.com/visionmedia/git-extras/tarball/0.5.1'
+  sha1 'faaeb466154cff9021d19aa4cb4f080ac9ee6d9f'
   head 'git://github.com/visionmedia/git-extras.git', :branch => 'master'
 
   def install
