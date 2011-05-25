fc95e077be664249bdb88ed9d15f620251143c2c
Adam Vandenberg <flangy@gmail.com>
2011-05-24 22:25:17 -0700

    redis 2.2.8

--- Library/Formula/redis.rb
+++ Library/Formula/redis.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Redis < Formula
-  url 'http://redis.googlecode.com/files/redis-2.2.7.tar.gz'
+  url 'http://redis.googlecode.com/files/redis-2.2.8.tar.gz'
   head 'git://github.com/antirez/redis.git'
   homepage 'http://redis.io/'
-  md5 '86937a61b9788d1d06fbfbd2bff03916'
+  md5 '106af5e3d4646588fd27be499227e14c'
 
   fails_with_llvm "Fails with \"reference out of range from _linenoise\""
 
