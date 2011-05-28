267ff4ffa1259e5f28012987b491b809a78b75f6
Ronny Haryanto <ronny@haryan.to>
2011-05-28 16:21:51 +1000

    jpegoptim 1.2.4
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/jpegoptim.rb
+++ Library/Formula/jpegoptim.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Jpegoptim < Formula
-  url 'http://www.kokkonen.net/tjko/src/jpegoptim-1.2.3.tar.gz'
+  url 'http://www.kokkonen.net/tjko/src/jpegoptim-1.2.4.tar.gz'
   homepage 'http://www.kokkonen.net/tjko/projects.html'
-  md5 '36afa60f8baac825935e215eb19e41e0'
+  md5 '40e8e627181f524ad29717c5b07cd442'
 
   depends_on 'jpeg'
 
