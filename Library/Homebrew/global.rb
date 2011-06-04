ce4370b5477d3218ee47819c0b207e48be117f01
Dan Loewenherz <dloewenherz@gmail.com>
2011-06-03 21:11:47 -0700

    Add HOMEBREW_CACHE environmental variable to dynamically set cache directory

--- Library/Homebrew/global.rb
+++ Library/Homebrew/global.rb
@@ -10,7 +10,8 @@ ARGV.extend(HomebrewArgvExtension)
 HOMEBREW_VERSION = '0.8'
 HOMEBREW_WWW = 'http://mxcl.github.com/homebrew/'
 
-HOMEBREW_CACHE = if Process.uid == 0
+HOMEBREW_CACHE = ENV['HOMEBREW_CACHE']
+HOMEBREW_CACHE = if not defined? HOMEBREW_CACHE and Process.uid == 0
   # technically this is not the correct place, this cache is for *all users*
   # so in that case, maybe we should always use it, root or not?
   Pathname.new("/Library/Caches/Homebrew")
