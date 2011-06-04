eef60f6be7206216ba229852fc3db322de195ac8
Ilya Strukov <iley@iley.ru>
2011-06-04 23:50:30 +0400

    add formula: ftjam

---
+++ Library/Formula/ftjam.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Jam < Formula
+  url 'http://sourceforge.net/projects/freetype/files/ftjam/2.5.2/ftjam-2.5.2.tar.bz2/download'
+  homepage 'http://www.freetype.org/jam/'
+  md5 'e61304b370ba06f68082f0219a196576'
+
+  def install
+    system "./configure --prefix=#{prefix}"
+    system 'make'
+    system 'make install'
+  end
+end
