030ceb67eb47460936079048a10a17e912cc0df7
Sudish Joseph <sudish@gmail.com>
2011-06-04 03:48:09 -0400

    Deparallelize the build of the 'bind' subdirectory.
    It breaks with make -j8.
    
    Replace all uses of the '/' method with '+'.

--- Library/Formula/isc-dhcp.rb
+++ Library/Formula/isc-dhcp.rb
@@ -8,24 +8,24 @@ class IscDhcp < Formula
 
   def install
     # use one dir under var for all runtime state.
-    dhcpd_dir = var/'dhcpd'
+    dhcpd_dir = var+'dhcpd'
 
     # change the locations of various files to suite homebrew.
     # we pass these in through CFLAGS since some cannot be changed
     # via configure args.
     path_opts = {
-      '_PATH_DHCPD_CONF'    => etc/'dhcpd.conf',
-      '_PATH_DHCLIENT_CONF' => etc/'dhclient.conf',
-      '_PATH_DHCPD_DB'      => dhcpd_dir/'dhcpd.leases',
-      '_PATH_DHCPD6_DB'     => dhcpd_dir/'dhcpd6.leases',
-      '_PATH_DHCLIENT_DB'   => dhcpd_dir/'dhclient.leases',
-      '_PATH_DHCLIENT6_DB'  => dhcpd_dir/'dhclient6.leases',
-      '_PATH_DHCPD_PID'     => dhcpd_dir/'dhcpd.pid',
-      '_PATH_DHCPD6_PID'    => dhcpd_dir/'dhcpd6.pid',
-      '_PATH_DHCLIENT_PID'  => dhcpd_dir/'dhclient.pid',
-      '_PATH_DHCLIENT6_PID' => dhcpd_dir/'dhclient6.pid',
-      '_PATH_DHCRELAY_PID'  => dhcpd_dir/'dhcrelay.pid',
-      '_PATH_DHCRELAY6_PID' => dhcpd_dir/'dhcrelay6.pid',
+      '_PATH_DHCPD_CONF'    => etc+'dhcpd.conf',
+      '_PATH_DHCLIENT_CONF' => etc+'dhclient.conf',
+      '_PATH_DHCPD_DB'      => dhcpd_dir+'dhcpd.leases',
+      '_PATH_DHCPD6_DB'     => dhcpd_dir+'dhcpd6.leases',
+      '_PATH_DHCLIENT_DB'   => dhcpd_dir+'dhclient.leases',
+      '_PATH_DHCLIENT6_DB'  => dhcpd_dir+'dhclient6.leases',
+      '_PATH_DHCPD_PID'     => dhcpd_dir+'dhcpd.pid',
+      '_PATH_DHCPD6_PID'    => dhcpd_dir+'dhcpd6.pid',
+      '_PATH_DHCLIENT_PID'  => dhcpd_dir+'dhclient.pid',
+      '_PATH_DHCLIENT6_PID' => dhcpd_dir+'dhclient6.pid',
+      '_PATH_DHCRELAY_PID'  => dhcpd_dir+'dhcrelay.pid',
+      '_PATH_DHCRELAY6_PID' => dhcpd_dir+'dhcrelay6.pid',
     }
     path_opts.each do |symbol,path|
       ENV.append 'CFLAGS', "-D#{symbol}='\"#{path}\"'"
@@ -37,13 +37,24 @@ class IscDhcp < Formula
       "--disable-dependency-tracking"
     ]
 
-    system "./configure", *config_opts
-    system "make"
-    system "make install"
+    system './configure', *config_opts
+
+    # the 'bind' subdirectory doesn't like overly parallel builds
+    # so build it sequentially. deparallelizing the whole build
+    # can be slow.
+    makeflags = ENV['MAKEFLAGS']
+    ENV.deparallelize
+    system 'make -C bind'
+    ENV['MAKEFLAGS'] = makeflags
+
+    # build everything else
+    inreplace 'Makefile', 'SUBDIRS = bind', 'SUBDIRS = '
+    system 'make'
+    system 'make install'
 
     # rename all the installed sample etc/* files so they don't clobber
     # any existing config files at symlink time.
-    Dir.open(prefix/'etc') do |dir|
+    Dir.open(prefix+'etc') do |dir|
       dir.each do |f|
         file = "#{dir.path}/#{f}"
         File.rename(file, "#{file}.sample") if File.stat(file).file?
