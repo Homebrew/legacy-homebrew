48e063d70cedf8a7ed587653aa04b024f13613fc
Adam Vandenberg <flangy@gmail.com>
2011-06-05 12:52:32 -0700

    Metasploit: allow in-place updates

--- Library/Formula/metasploit.rb
+++ Library/Formula/metasploit.rb
@@ -7,9 +7,21 @@ class Metasploit < Formula
 
   head "https://www.metasploit.com/svn/framework3/trunk/", :using => :svn
 
+  # Metasploit's tarball comes with a full .svn checkout.
+  # Don't clean these folders, so users can "svn up" to update
+  # metasploit in-place, which apparently is standard for this project.
+  skip_clean :all
+
   def install
     libexec.install Dir["msf*",'data','external','lib','modules','plugins','scripts','test','tools']
     bin.mkpath
     Dir["#{libexec}/msf*"].each {|f| ln_s f, bin}
+  end
+
+  def caveats; <<-EOS.undent
+    Metasploit can be updated in-place by doing:
+      cd `brew --prefix metasploit`/libexec/
+      svn up
+    EOS
   end
 end
