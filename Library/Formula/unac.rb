8fc96d6fff6c7d3f2637bc203a8b07ae5a951269
Marcus <derencius@gmail.com>
2011-06-08 03:01:15 -0300

    Comment about the patch used on this formula

--- Library/Formula/unac.rb
+++ Library/Formula/unac.rb
@@ -6,6 +6,9 @@ class Unac < Formula
   md5 '884d057e644d3f26ec503b0780fbcb2b'
 
   def patches
+    # I wrote this patch to fix compiling by removing long strings that supposed to be comments on the first lines of unactest1.c. 
+    # Also unactest1.c seems to be just a test file. This patch then would not have impact on the utility.
+    # Author: marcus derencius (github.com/derencius)
     "https://gist.github.com/raw/992290/34b3ad99904001efc509ab72c5b1478d25a0dd0a/a.patch"
   end
 
