7bf66b9f0c3c9b44fea4085c882564c5d4b364f8
Felix Schulze <code@felixschulze.de>
2011-06-10 20:13:05 +0200

    highlight 3.5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/highlight.rb
+++ Library/Formula/highlight.rb
@@ -1,13 +1,15 @@
 require 'formula'
 
 class Highlight < Formula
-  url 'http://www.andre-simon.de/zip/highlight-2.16.tar.bz2'
+  url 'http://www.andre-simon.de/zip/highlight-3.5.tar.bz2'
   homepage 'http://www.andre-simon.de/doku/highlight/en/highlight.html'
-  sha1 'b5fed14bb1a973fe134dd2133766bb86fdc7494e'
+  sha1 '0e57b45103b6f471a96f987f98dc5e09fed138b5'
+
+  depends_on 'boost'
+  depends_on 'lua'
 
   def install
-    conf = etc+'highlight/'
-    system "make", "PREFIX=#{prefix}", "conf_dir=#{conf}"
-    system "make", "PREFIX=#{prefix}", "conf_dir=#{conf}", "install"
+    system "make", "PREFIX=#{prefix}", "conf_dir=#{etc}/highlight"
+    system "make", "PREFIX=#{prefix}", "conf_dir=#{etc}/highlight", "install"
   end
 end
