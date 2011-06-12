a064229f776a5eaab6c24ceafb250ef10a41c01e
Greisberger Christophe <greisby@gmail.com>
2011-06-12 13:41:01 +0200

    New formula: frei0r

---
+++ Library/Formula/frei0r.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Frei0r < Formula
+  url 'ftp://ftp.dyne.org/frei0r/releases/frei0r-plugins-1.3.tar.gz'
+  homepage 'http://frei0r.dyne.org/'
+  md5 'a2eb63feeeb0c5cf439ccca276cbf70c'
+
+  # depends_on 'cmake'
+  depends_on 'opencv'
+  depends_on 'gavl'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    # system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
