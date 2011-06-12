75c5e30f039a14693d118ae173144874d31df34f
Jason L. Shiffer <jshiffer@zerotao.org>
2011-06-12 10:26:53 -0400

    Initial robodoc formula
    Includes formula for 4.49.41 as well as a head formula referencing
    github.

---
+++ Library/Formula/robodoc.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Robodoc < Formula
+  url 'http://rfsber.home.xs4all.nl/Robo/robodoc-4.99.41.tar.gz'
+  md5 '986ff954e0ba5a9c407384fc4b05303d'
+  homepage 'http://rfsber.home.xs4all.nl/Robo/robodoc.html'
+  head 'git://github.com/gumpu/ROBODoc'
+
+  def install
+   if ARGV.build_head?
+       system "autoreconf", "-f", "-i"
+   end
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
