44bbb9bfb0f9abe5cd429f4fa847983b48f8855a
Greisberger Christophe <greisby@gmail.com>
2011-06-12 15:22:27 +0200

    New formula: libdc1394

---
+++ Library/Formula/libdc1394.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Libdc1394 < Formula
+  url 'http://sourceforge.net/projects/libdc1394/files/libdc1394-2/libdc1394-2.1.3.tar.gz'
+  homepage 'http://damien.douxchamps.net/ieee1394/libdc1394/'
+  md5 'd8b2cbfae1b329fdeaa638da80427334'
+
+  # depends_on 'cmake'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    # system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
