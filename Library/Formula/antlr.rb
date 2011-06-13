aabe2d84370f8e9bed56bb1192706088bd863207
Ryan Govostes <rgovostes@gmail.com>
2011-06-13 16:37:24 -0700

    antlr 3.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/antlr.rb
+++ Library/Formula/antlr.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Antlr < Formula
-  version '3.2'
-  url "http://www.antlr.org/download/antlr-#{version}.jar"
+  version '3.3'
+  url "http://www.antlr.org/download/antlr-#{version}-complete.jar"
   homepage 'http://www.antlr.org/'
-  md5 'ee7dc3fb20cf3e9efd871e297c0d532b'
+  md5 '238becce7da69f7be5c5b8a65558cf63'
 
   def install
-    prefix.install "antlr-#{version}.jar"
+    prefix.install "antlr-#{version}-complete.jar"
     #Add an executable shell-script
-    (bin + "antlr-#{version}").write "#!/bin/sh\njava -jar #{prefix}/antlr-#{version}.jar $*"
+    (bin + "antlr-#{version}").write "#!/bin/sh\njava -jar #{prefix}/antlr-#{version}-complete.jar $*"
   end
 end
