b6cb5c8e9010be6aba6380fe0bed04fdc32cf349
Adam Vandenberg <flangy@gmail.com>
2011-06-13 15:18:46 -0700

    mutt: clean up formula a bit

--- Library/Formula/mutt.rb
+++ Library/Formula/mutt.rb
@@ -18,8 +18,7 @@ class Mutt < Formula
   end
 
   def patches
-    # Fix unsubscribe malformed folder
-    p = Array.new
+    p = []
 
     if ARGV.include? '--sidebar-patch'
       p << 'https://raw.github.com/nedos/mutt-sidebar-patch/master/mutt-sidebar.patch'
@@ -48,17 +47,13 @@ class Mutt < Formula
             # This is just a trick to keep 'make install' from trying to chgrp
             # the mutt_dotlock file (which we can't do if we're running as an
             # unpriviledged user)
-            "--with-homespool=.mbox"
-      ]
+            "--with-homespool=.mbox"]
+    args << "--with-slang" if ARGV.include? '--with-slang'
 
     if ARGV.include? '--enable-debug'
       args << "--enable-debug"
     else
       args << "--disable-debug"
-    end
-
-    if ARGV.include? '--with-slang'
-      args << "--with-slang"
     end
 
     system "./configure", *args
