b35fc29723ca726000e06e3ed502b7c2e831cead
Adam Vandenberg <flangy@gmail.com>
2011-06-13 17:33:34 -0700

    antlr: match Homebrew style guide

--- Library/Formula/antlr.rb
+++ Library/Formula/antlr.rb
@@ -1,14 +1,16 @@
 require 'formula'
 
 class Antlr < Formula
+  url "http://www.antlr.org/download/antlr-3.3-complete.jar"
   version '3.3'
-  url "http://www.antlr.org/download/antlr-#{version}-complete.jar"
   homepage 'http://www.antlr.org/'
   md5 '238becce7da69f7be5c5b8a65558cf63'
 
   def install
-    prefix.install "antlr-#{version}-complete.jar"
-    #Add an executable shell-script
-    (bin + "antlr-#{version}").write "#!/bin/sh\njava -jar #{prefix}/antlr-#{version}-complete.jar $*"
+    prefix.install "antlr-3.3-complete.jar"
+    (bin+"antlr-3.3").write <<-EOS.undent
+    #!/bin/sh
+    java -jar #{prefix}/antlr-3.3-complete.jar "$@"
+    EOS
   end
 end
