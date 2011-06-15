0c656d53ee4c7ebfebda4dfae7e3808ae7c95d3d
Kashif Rasul <kashif@nomad-labs.com>
2011-06-15 01:51:59 -0700

    updated potrace to 1.9

--- Library/Formula/potrace.rb
+++ Library/Formula/potrace.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Potrace < Formula
-  url 'http://potrace.sourceforge.net/download/potrace-1.8.tar.gz'
+  url 'http://potrace.sourceforge.net/download/potrace-1.9.tar.gz'
   homepage 'http://potrace.sourceforge.net'
-  md5 'e73b45565737d64011612704dd4d9f86'
+  md5 'ef973e7ec9c2e5b3e19e8dfeaa7524c3'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
