015272288fdb64b2c6d44a5fb6acde006f0ef424
sowawa ( Keisuke SOGAWA ) <keisuke.sogawa@gmail.com>
2011-06-16 10:54:03 +0900

    Updated clamav to version 0.97.1

--- Library/Formula/clamav.rb
+++ Library/Formula/clamav.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Clamav < Formula
-  url 'http://downloads.sourceforge.net/clamav/clamav-0.97.tar.gz'
+  url 'http://downloads.sourceforge.net/clamav/clamav-0.97.1.tar.gz'
   homepage 'http://www.clamav.net/'
-  md5 '605ed132b2f8e89df11064adea2b183b'
+  md5 '4f3fb74ff1098c25eef8e10721478c29'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
