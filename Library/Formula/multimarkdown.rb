6ee63a3e004f2971d188d8fbd2ff2fea908d8faf
Dipen Lad <dipen.lad@gmail.com>
2011-06-16 10:23:46 +0100

    update to multimarkdown 3.0

--- Library/Formula/multimarkdown.rb
+++ Library/Formula/multimarkdown.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Multimarkdown < Formula
-  url 'https://github.com/fletcher/peg-multimarkdown.git', :tag => "3.0b11"
+  url 'https://github.com/fletcher/peg-multimarkdown.git', :tag => "3.0"
   homepage 'http://fletcherpenney.net/multimarkdown/'
   version "3.0b11"
   head 'https://github.com/fletcher/peg-multimarkdown.git'
