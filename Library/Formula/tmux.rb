0835ad198639eb235351b34a86d79c4af102761d
Bryan Cribbs <bryan.cribbs@gmail.com>
2011-06-15 19:25:52 -0500

    tmux: also install bash completion
    The tmux distribution contains a bash completion script
    in examples/bash_completion_tmux.sh.  Install it as 'tmux'.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -11,11 +11,17 @@ class Tmux < Formula
     ENV['PREFIX'] = prefix
     system "./configure"
 
-    inreplace "GNUmakefile" do |s|
-      # Put docs in the right place
-      s.gsub! "man/man1", "share/man/man1"
-    end
+    # Put man pages in the right place
+    inreplace "GNUmakefile", "man/man1", "share/man/man1"
 
     system "make install"
+    # Install bash completion scripts for use with bash-completion
+    (prefix+'etc/bash_completion.d').install "examples/bash_completion_tmux.sh" => 'tmux'
+  end
+
+  def caveats; <<-EOS.undent
+    Bash completion script was installed to:
+      #{etc}/bash_completion.d/tmux
+    EOS
   end
 end
