05443266b6daf0b3cc92a160271118a8d1a460ac
Adam Vandenberg <flangy@gmail.com>
2011-06-16 20:16:58 -0700

    mongo-php: clean up version

--- Library/Formula/mongo-php.rb
+++ Library/Formula/mongo-php.rb
@@ -4,15 +4,13 @@ class MongoPhp < Formula
   url 'http://pecl.php.net/get/mongo-1.1.4.tgz'
   homepage 'http://pecl.php.net/package/mongo'
   md5 '22f1e25690589f6d80d5ed29e56644eb'
-  version '1.1.4'
 
   def install
     Dir.chdir "mongo-#{version}" do
       system "phpize"
       system "./configure", "--prefix=#{prefix}"
       system "make"
-
-      prefix.install %w(modules/mongo.so)
+      prefix.install "modules/mongo.so"
     end
   end
 
