ea390f00cea4227ddd796f7c83e58f5ab106fdc2
Charlie Sharpsteen <source@sharpsteen.net>
2011-06-19 16:38:32 -0700

    libgit2 0.13.0
    Version 0.13.0 also dropped support for the Waf buildsystem, so this update
    switches to CMake
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libgit2.rb
+++ Library/Formula/libgit2.rb
@@ -1,16 +1,19 @@
 require 'formula'
 
 class Libgit2 < Formula
-  url 'https://github.com/libgit2/libgit2/zipball/v0.12.0'
-  md5 '70073b25de4cca873cf565f81023f426'
+  url 'https://github.com/libgit2/libgit2/zipball/v0.13.0'
+  md5 'd9e5bb792bc6d7aeb0c4ea90aac074e1'
   homepage 'http://libgit2.github.com/'
 
   head 'https://github.com/libgit2/libgit2.git', :branch => 'master'
 
+  depends_on 'cmake' => :build
+
   def install
-    system "./waf", "configure", "--prefix=#{prefix}"
-    system "./waf", "build-static"
-    system "./waf", "build-shared"
-    system "./waf", "install"
+    mkdir 'build'
+    Dir.chdir 'build' do
+      system "cmake .. #{std_cmake_parameters} -DBUILD_TESTS=NO"
+      system "make install"
+    end
   end
 end
