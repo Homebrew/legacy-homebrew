23a37cbb1dba47aadea6ecd8a1e3459f9d84b354
Montgomery Kosma <mkosma@gmail.com>
2011-06-21 20:17:46 -0700

    Install the Ruby API for MaxMind's GeoIP C library
    Replaces functionality of MacPorts rb-net-geoip
    Huzzah for my first recipe ever.  MK

---
+++ Library/Formula/net-geoip.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+# install Ruby API for Maxmind GeoIP C library
+#
+# (as an alternative to MacPorts rb-net-geoip)
+
+class NetGeoip < Formula
+  depends_on 'geoip'
+
+  url 'http://geolite.maxmind.com/download/geoip/api/ruby/net-geoip-0.07.tar.gz'
+  homepage 'http://www.maxmind.com/app/ruby'
+  md5 '7cdc43b9382af14ddf4dd1295f4a402c'
+
+  def install
+    system "ruby extconf.rb"
+    system "make"
+    system "make install"
+  end
+end
