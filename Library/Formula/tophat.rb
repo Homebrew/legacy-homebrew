c987c7c3af7c43640b9f648a54e27cfe637256b7
Carlos Borroto <carlos.borroto@gmail.com>
2011-06-23 10:45:38 -0400

    tophat 1.3.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tophat.rb
+++ Library/Formula/tophat.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Tophat < Formula
-  url 'http://tophat.cbcb.umd.edu/downloads/tophat-1.3.0.tar.gz'
+  url 'http://tophat.cbcb.umd.edu/downloads/tophat-1.3.1.tar.gz'
   homepage 'http://tophat.cbcb.umd.edu/'
-  md5 'e56f3ff42d1bb8a60489044da7b5421f'
+  md5 'c6e39c7935fd6c7b43a80db3a179a537'
 
   depends_on 'samtools'
 
