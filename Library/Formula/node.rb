e3eb03d2b5232a134c1a65be57c05865eb05e503
João <mail@joaomoreno.com>
2011-06-29 14:44:37 +0200

    node.js 0.4.9
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Node < Formula
-  url 'http://nodejs.org/dist/node-v0.4.8.tar.gz'
+  url 'http://nodejs.org/dist/node-v0.4.9.tar.gz'
   head 'https://github.com/joyent/node.git'
   homepage 'http://nodejs.org/'
-  md5 '22c9f69370069fe81678592cc8ae48f1'
+  md5 'e525b8b99d949bf2f031bc262c138e96'
 
   # Leopard OpenSSL is not new enough, so use our keg-only one
   depends_on 'openssl' if MacOS.leopard?
