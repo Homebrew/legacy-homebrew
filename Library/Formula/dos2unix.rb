85fee5a3a120ed0dbdb388c03380fef27b636116
Justin Hahn <jhahn@rbmtechnologies.com>
2011-06-29 11:47:52 -0400

    Upgrade dos2unix to release 5.3, and eliminate the now duplicate unix2dos formula.

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -1,16 +1,20 @@
 require 'formula'
 
 class Dos2unix < Formula
-  url 'http://sourceforge.net/projects/dos2unix/files/dos2unix/3.2/dos2unix-3.2.tar.gz'
-  md5 'db3902feba8a4bae26e9a1bd27c7ce53'
+  url 'http://sourceforge.net/projects/dos2unix/files/dos2unix/5.3/dos2unix-5.3.tar.gz'
+  md5 '0db30704f5b68c5a0aeaff9b8d7012e8'
   homepage 'http://dos2unix.sourceforge.net/'
-
+  
+  depends_on 'gettext'
+  
   def install
-    # we don't use the Makefile as it doesn't optimize
-    system "#{ENV.cc} #{ENV.cflags} dos2unix.c -o dos2unix"
-
-    bin.install %w[dos2unix]
-    ln_sf bin+'dos2unix', bin+'mac2unix'
-    man1.install %w[mac2unix.1 dos2unix.1]
+    # Load gettext and set the appropriate environment variables to help
+    # dos2unix find the gettext library.
+    gettext = Formula.factory("gettext")
+    ENV.append 'CFLAGS', "-I#{gettext.include}"
+    ENV.append 'LDFLAGS', "-lintl "
+    
+    # Set the appropriate prefix and compilation variables.
+    system "make", "prefix=#{prefix}", "CFLAGS=#{ENV['CFLAGS']}", "LDFLAGS=#{ENV['LDFLAGS']}", "install"
   end
 end
