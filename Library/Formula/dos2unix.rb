c64b8e5071e4d3b5f09af75664a42d94cbd4b00c
Justin Hahn <jhahn@rbmtechnologies.com>
2011-06-30 10:32:06 -0400

    Update dos2unix v5.3 to make NLS optional
    This makes the NLS support for dos2unix optional (disabled by default).

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -4,17 +4,27 @@ class Dos2unix < Formula
   url 'http://sourceforge.net/projects/dos2unix/files/dos2unix/5.3/dos2unix-5.3.tar.gz'
   md5 '0db30704f5b68c5a0aeaff9b8d7012e8'
   homepage 'http://dos2unix.sourceforge.net/'
-  
-  depends_on 'gettext'
-  
+
+  depends_on "gettext" if ARGV.include? "--enable-nls"
+
+  def options
+    [["--enable-nls", "Enable NLS support."]]
+  end
+
   def install
-    # Load gettext and set the appropriate environment variables to help
-    # dos2unix find the gettext library.
-    gettext = Formula.factory("gettext")
-    ENV.append 'CFLAGS', "-I#{gettext.include}"
-    ENV.append 'LDFLAGS', "-lintl "
-    
-    # Set the appropriate prefix and compilation variables.
-    system "make", "prefix=#{prefix}", "CFLAGS=#{ENV['CFLAGS']}", "LDFLAGS=#{ENV['LDFLAGS']}", "install"
+    args = ["prefix=#{prefix}"]
+
+    if ARGV.include? "--enable-nls"
+      gettext = Formula.factory("gettext")
+      ENV.append 'CFLAGS', "-I#{gettext.include}"
+      ENV.append 'LDFLAGS', "-lintl "
+      args += ["CFLAGS=#{ENV['CFLAGS']}", "LDFLAGS=#{ENV['LDFLAGS']}"]
+    else
+      args += ["ENABLE_NLS="]
+    end
+
+    args << "install"
+
+    system "make", *args
   end
 end
