58275d8086e24a31f0d83ab9188fcafe12151206
Jon Cooper <joncooper@carbonfive.com>
2011-07-01 11:18:54 -0700

    arpwatch 2.1a15 Patch by joncooper to correctly make a directory & fix the /usr/bin/install invocation on OS X

--- Library/Formula/arpwatch.rb
+++ Library/Formula/arpwatch.rb
@@ -5,6 +5,7 @@ class Arpwatch < Formula
   homepage 'http://http://ee.lbl.gov/'
   md5 'cebfeb99c4a7c2a6cee2564770415fe7'
 
+  # Patch by joncooper to correctly make a directory & fix the /usr/bin/install invocation on OS X
   def patches
     'https://raw.github.com/gist/1058745/b72113730c0bdf0acb2966101937765584d98d08/arpwatch-2.1a15.darwin.patch'
   end
