51cefb3bfe72974fc09a0ea71f173130f90ac321
Tobias Steinhoff <tobias@stonehope.de>
2011-07-01 06:58:37 +0200

    ttytter 1.2.00
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ttytter.rb
+++ Library/Formula/ttytter.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Ttytter < ScriptFileFormula
-  url 'http://www.floodgap.com/software/ttytter/dist1/1.1.12.txt'
+  url 'http://www.floodgap.com/software/ttytter/dist1/1.2.00.txt'
   homepage 'http://www.floodgap.com/software/ttytter/'
-  md5 '43385b93f42ae8892eff0ddc99903898'
+  md5 '82841292bc27d9297e2a6d907be2cd83'
 
   def install
-    bin.install '1.1.12.txt' => 'ttytter'
+    bin.install '1.2.00.txt' => 'ttytter'
   end
 
   def caveats; <<-EOS.undent
