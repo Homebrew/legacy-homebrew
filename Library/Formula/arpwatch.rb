af756bb25b786640895439ed1dde89a49158662e
Jon Cooper <joncooper@carbonfive.com>
2011-07-01 09:24:11 -0700

    arpwatch 2.1a15

---
+++ Library/Formula/arpwatch.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Arpwatch < Formula
+  url 'ftp://ftp.ee.lbl.gov/arpwatch-2.1a15.tar.gz'
+  homepage 'http://http://ee.lbl.gov/'
+  md5 'cebfeb99c4a7c2a6cee2564770415fe7'
+
+  def patches
+    'https://raw.github.com/gist/1058745/b72113730c0bdf0acb2966101937765584d98d08/arpwatch-2.1a15.darwin.patch'
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
