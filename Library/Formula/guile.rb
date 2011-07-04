59fc2cc69f5d979335ba08b4372e0206f4f4619a
Brett Hoerner <brett@bretthoerner.com>
2011-07-04 08:04:25 -0500

    Update Guile to 2.0.2

--- Library/Formula/guile.rb
+++ Library/Formula/guile.rb
@@ -3,10 +3,10 @@ require 'formula'
 class Guile < Formula
   homepage 'http://www.gnu.org/software/guile/'
   url 'ftp://ftp.gnu.org/gnu/guile/guile-1.8.7.tar.gz'
-  head 'ftp://ftp.gnu.org/pub/gnu/guile/guile-2.0.1.tar.gz'
+  head 'ftp://ftp.gnu.org/pub/gnu/guile/guile-2.0.2.tar.gz'
 
   if ARGV.build_head?
-    sha1 '3276a826f17a5edc0baf2eedc4821a771824141b'
+    sha1 '1943fd22417ce1e51babbdcd7681e66a794a8da3'
   else
     sha1 '24cd2f06439c76d41d982a7384fe8a0fe5313b54'
   end
