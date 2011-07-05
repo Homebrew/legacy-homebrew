e8f908ef6abc5a5ef57c9ec659afc65e179b45a6
Joshua B. Griffith <j.griffith@facilitrak.com>
2011-07-05 17:40:34 -0500

    Change qi dependencies to from SBCL to CLISP for stability

--- Library/Formula/qi.rb
+++ Library/Formula/qi.rb
@@ -5,12 +5,12 @@ class Qi < Formula
   homepage 'http://www.lambdassociates.org/'
   md5 '3a0b5c56d0f107f80f5bca11b82a4d59'
 
-  depends_on 'sbcl'
+  depends_on 'clisp'
 
   def install
-    system "cd Lisp; sbcl --load 'install.lsp'"
-    system "echo \"#!/bin/bash\nsbcl --core #{bin}/Qi.core $*\" > qi"
+    system "cd Lisp; clisp install.lsp"
+    system "echo \"#!/bin/bash\nclisp -M #{bin}/Qi.mem $*\" > qi"
     system "chmod 755 qi"
-    bin.install ['Lisp/Qi.core', 'qi']
+    bin.install ['Lisp/Qi.mem', 'qi']
   end
 end
