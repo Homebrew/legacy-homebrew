9a93494564ae709112eb511b9050ca690f70c1db
Joshua B. Griffith <j.griffith@facilitrak.com>
2011-07-05 19:16:39 -0500

    Add SBCL option

--- Library/Formula/qi.rb
+++ Library/Formula/qi.rb
@@ -5,12 +5,27 @@ class Qi < Formula
   homepage 'http://www.lambdassociates.org/'
   md5 '3a0b5c56d0f107f80f5bca11b82a4d59'
 
-  depends_on 'clisp'
+  def options
+    [['--SBCL', 'Use SBCL instead of CLISP']]
+  end
+
+  if ARGV.include? '--SBCL'
+    depends_on 'sbcl'
+  else
+    depends_on 'clisp'
+  end
 
   def install
-    system "cd Lisp; clisp install.lsp"
-    system "echo \"#!/bin/bash\nclisp -M #{bin}/Qi.mem $*\" > qi"
+    if ARGV.include? '--SBCL'
+      system "cd Lisp; sbcl --load 'install.lsp'"
+      system "echo \"#!/bin/bash\nsbcl --core #{prefix}/Qi.core $*\" > qi"
+      prefix.install ['Lisp/Qi.core']
+    else
+      system "cd Lisp; clisp install.lsp"
+      system "echo \"#!/bin/bash\nclisp -M #{prefix}/Qi.mem $*\" > qi"
+      prefix.install ['Lisp/Qi.mem']
+    end
     system "chmod 755 qi"
-    bin.install ['Lisp/Qi.mem', 'qi']
+    bin.install ['qi']
   end
 end
