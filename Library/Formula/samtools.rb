004efdca386fe76eac593136e8926bb5df3700fb
Carlos Borroto <carlos.borroto@gmail.com>
2011-07-07 09:55:34 -0400

    samtools 1.1.17 and add install bcftools
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/samtools.rb
+++ Library/Formula/samtools.rb
@@ -1,19 +1,21 @@
 require 'formula'
 
 class Samtools < Formula
-  url 'http://sourceforge.net/projects/samtools/files/samtools/0.1.16/samtools-0.1.16.tar.bz2'
+  url 'http://sourceforge.net/projects/samtools/files/samtools/0.1.17/samtools-0.1.17.tar.bz2'
   homepage 'http://samtools.sourceforge.net/'
-  md5 'ae26c63a783f95a45ddf8a109c9d9a5d'
+  md5 '9549f7a6bba894766df9c3dd18d61f0b'
   head 'https://samtools.svn.sourceforge.net/svnroot/samtools/trunk/samtools'
 
   def install
     system "make"
     system "make razip"
+    system "cd bcftools; make"
 
-    bin.install %w{samtools razip}
+    bin.install %w{samtools razip bcftools/bcftools}
     (share+'samtools').install %w{examples misc}
+    (share+'samtools/misc').install %w{bcftools/vcfutils.pl}
     lib.install 'libbam.a'
     (include+'bam').install Dir['*.h']
-    man1.install 'samtools.1'
+    man1.install %w{samtools.1}
   end
 end
