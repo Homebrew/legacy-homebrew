8aec273272d154450af79bce7378c0b46ed4ff8a
Adam Vandenberg <flangy@gmail.com>
2011-07-07 10:37:35 -0700

    chicken: update for xcode 4

--- Library/Formula/chicken.rb
+++ Library/Formula/chicken.rb
@@ -6,9 +6,11 @@ class Chicken < Formula
   homepage 'http://www.call-cc.org/'
   head 'git://code.call-cc.org/chicken-core'
 
+  fails_with_llvm "See http://lists.gnu.org/archive/html/chicken-users/2010-12/msg00158.html"
+
   def install
     ENV.deparallelize
-    args = ["PREFIX=#{prefix}", "PLATFORM=macosx"]
+    args = ["PREFIX=#{prefix}", "PLATFORM=macosx", "C_COMPILER=#{ENV.cc}"] # Chicken uses a non-standard var. for this
     args << "ARCH=x86-64" if MacOS.prefer_64_bit?
     system "make", *args
     system "make", "install", *args
