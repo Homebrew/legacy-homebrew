5452fed2b1364f3b5f7d9d70ae04a362cfd48819
David Wooldridge (z0mbix) <zombie@zombix.org>
2011-07-09 00:44:22 +0100

    Added a formula for shmux

---
+++ Library/Formula/shmux.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Shmux < Formula
+  url 'http://web.taranis.org/shmux/dist/shmux-1.0.2.tgz'
+  homepage 'http://web.taranis.org/shmux/'
+  md5 '4ab5c46b4154cbeab54bdc0036bd9140'
+
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
