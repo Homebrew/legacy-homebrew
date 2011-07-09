3e390c3780ecc6efce6ec931bc685778858c79c3
Jonathan Schleifer <js@webkeks.org>
2011-07-09 03:15:56 +0200

    Update objfw.

--- Library/Formula/objfw.rb
+++ Library/Formula/objfw.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Objfw < Formula
-  url 'https://webkeks.org/objfw/downloads/objfw-0.5.2.tar.gz'
+  url 'https://webkeks.org/objfw/downloads/objfw-0.5.3.tar.gz'
   homepage 'https://webkeks.org/objfw/'
-  md5 'c4302df7637c812344b7b2f531ccf7d1'
+  sha256 '9a5db85312086d69a08999347fe1fd3052c4b2f2cdbd146d849d263fdd74a798'
 
   def install
     system "xcodebuild -target ObjFW"
