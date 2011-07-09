db40d52cf3450cbb68f3244a8afa7f7ccda01589
aimee daniells <aimeedaniells@googlemail.com>
2011-07-09 19:16:49 +0100

    Selenium chrome driver 14.0.813.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/chromedriver.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Chromedriver < Formula
+  version '14.0.813.0'
+  url 'http://chromium.googlecode.com/files/chromedriver_mac_14.0.813.0.zip'
+  homepage 'http://seleniumhq.wordpress.com/2011/07/07/new-chromedriver/'
+  sha1 'b5f375000cb1e292cbe35966550075f9f28d50e5'
+
+  def install
+    bin.install 'chromedriver'
+  end
+end
