daea3dd7681229b572fb3370477bee3e8bf33a14
Jason Kim <jason@512k.org>
2011-07-09 20:26:58 -0700

    dialog 1.1.20110707
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dialog.rb
+++ Library/Formula/dialog.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Dialog < Formula
   url 'ftp://invisible-island.net/dialog/dialog.tar.gz'
   homepage 'http://invisible-island.net/dialog/'
-  md5 '2eaa82055b86006e205d2c3f74d9b800'
-  version '1.1.20110302'
+  md5 '34d01aaacbb2932b77774e6c1eec8d2a'
+  version '1.1.20110707'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
