234c1099d98a239967f865ac62fd4f2185ff04f8
posativ <info@posativ.org>
2011-07-10 11:09:44 +0200

    make lltag.rb brew conform

--- Library/Formula/lltag.rb
+++ Library/Formula/lltag.rb
@@ -2,10 +2,8 @@ require 'formula'
 
 class Lltag < Formula
   url 'http://download.gna.org/lltag/lltag-0.14.3.tar.bz2'
-  homepage ''
+  homepage 'http://home.gna.org/lltag/'
   md5 'fdd0fdb9a0228bbc8b6bad6199dd4500'
-
-  # depends_on 'cmake'
 
   def install
     system "make"
