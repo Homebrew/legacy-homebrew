f3ff6859367576a6d9c907d417285d5e8a03d1f2
Jack Nagel <jacknagel@gmail.com>
2011-07-11 21:02:24 -0500

    tmux 1.5
    A laundry list of updates, including a revamped build system.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -1,20 +1,18 @@
 require 'formula'
 
 class Tmux < Formula
-  url 'http://sourceforge.net/projects/tmux/files/tmux/tmux-1.4/tmux-1.4.tar.gz'
-  md5 '0bfc7dd9a5bab192406167589c716a21'
+  url 'http://sourceforge.net/projects/tmux/files/tmux/tmux-1.5/tmux-1.5.tar.gz'
+  md5 '3d4b683572af34e83bc8b183a8285263'
   homepage 'http://tmux.sourceforge.net'
 
   depends_on 'libevent'
 
   def install
-    ENV['PREFIX'] = prefix
-    system "./configure"
-
-    # Put man pages in the right place
-    inreplace "GNUmakefile", "man/man1", "share/man/man1"
-
+    ENV.append "LDFLAGS", '-lresolv'
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--sysconfdir=#{etc}"
     system "make install"
+
     # Install bash completion scripts for use with bash-completion
     (prefix+'etc/bash_completion.d').install "examples/bash_completion_tmux.sh" => 'tmux'
   end
