5788a6652ecb04c2946d0469aaa6e936421d1b1b
Felix Schulze <code@felixschulze.de>
2011-07-14 22:59:31 +0200

    android-ndk r6

--- Library/Formula/android-ndk.rb
+++ Library/Formula/android-ndk.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class AndroidNdk < Formula
-  url 'http://dl.google.com/android/ndk/android-ndk-r5b-darwin-x86.tar.bz2'
+  url 'http://dl.google.com/android/ndk/android-ndk-r6-darwin-x86.tar.bz2'
   homepage 'http://developer.android.com/sdk/ndk/index.html#overview'
-  md5 '019a14622a377b3727ec789af6707037'
-  version 'r5b'
+  md5 'a154905e49a6246abd823b75b6eda738'
+  version 'r6'
 
   depends_on 'android-sdk'
 
