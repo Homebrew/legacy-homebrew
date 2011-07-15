9a324e5a82622cee569371322fe77ab76b437127
Marv Cool <marv@hostin.is>
2011-07-15 14:30:16 +0200

    hydra 6.5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/hydra.rb
+++ Library/Formula/hydra.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Hydra < Formula
-  url 'http://www.thc.org/releases/hydra-6.4-src.tar.gz'
+  url 'http://www.thc.org/releases/hydra-6.5-src.tar.gz'
   homepage 'http://www.thc.org/thc-hydra/'
-  md5 '4592be3ef50edd608fd066e5ac8c46a1'
+  md5 '69a5afbbcbe3b1fdd31f9bf616480336'
 
   def install
     system "./configure", "--prefix=#{prefix}"
