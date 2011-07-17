e7e147613261c3f471438764a2d40a1ab3713867
Marv Cool <marv@hostin.is>
2011-07-17 22:16:35 +0200

    added first patch to 6.5. Making it 6.5+diff1

--- Library/Formula/hydra.rb
+++ Library/Formula/hydra.rb
@@ -4,6 +4,18 @@ class Hydra < Formula
   url 'http://www.thc.org/releases/hydra-6.5-src.tar.gz'
   homepage 'http://www.thc.org/thc-hydra/'
   md5 '69a5afbbcbe3b1fdd31f9bf616480336'
+  version "6.5+diff1"
+
+  def patches
+    # this patch is provided by the original authors of this software!
+    # Quote from their website:
+    #INFORMATION FOR VERSION 6.5:
+    #        As the next version will be v7.0 which will get a rewrite of the main function,
+    #        it will take some months for the next release.
+    #        For important issues there will be diff patches provided here for 6.5
+    #        PATCH: The following diff patches two issues in the http-form module:
+    { :p0 => "http://www.thc.org/thc-hydra/hydra-6.5-fix.diff" }
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
