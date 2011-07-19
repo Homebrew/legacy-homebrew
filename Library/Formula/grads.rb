156ddf5456c4b217baf4635df087e442d6ec9e04
Ryan Senkbeil <rsenk330@gmail.com>
2011-07-19 13:33:08 -0500

    Added formula for GrADS

---
+++ Library/Formula/grads.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class GradsSupplementary < Formula
+  url 'ftp://grads.iges.org/grads/data2.tar.gz'
+  md5 'cacf16d75f53c876ff18bd4f8100fa66'
+end
+
+class Grads < Formula
+  url 'ftp://iges.org/grads/2.0/grads-2.0.a9-bin-darwin9.8-intel.tar.gz'
+  homepage 'http://www.iges.org/grads/grads.html'
+  md5 '9c9f054aa2f96562fc49771f6364aeab'
+  version '2.0a9'
+
+  def install
+    bin.install ['bin/bufrscan', 'bin/grads', 'bin/grib2scan', 'bin/gribmap', 'bin/gribscan', 'bin/gxeps', 'bin/gxps', 'bin/gxtran', 'bin/stnmap', 'bin/wgrib']
+
+    # Install the required supplementary files
+    GradsSupplementary.new.brew{ (lib+'grads').install Dir['*'] }
+  end
+end
