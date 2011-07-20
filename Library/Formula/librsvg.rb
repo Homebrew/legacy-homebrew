f99c835a5d83082acb983b7d977870580cb50a10
Johannes Schönberger <johannes.schoenberger@tum.de>
2011-07-20 07:08:35 +0200

    librsvg 2.34.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/librsvg.rb
+++ Library/Formula/librsvg.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Librsvg < Formula
-  url 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.32/librsvg-2.32.1.tar.gz'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.34/librsvg-2.34.0.tar.gz'
   homepage 'http://librsvg.sourceforge.net/'
-  md5 'd7a242ca43e33e1b63d3073f9d46a6a8'
+  md5 '3bf6472d65e15cd13230f886da88e913'
 
   depends_on 'gtk+'
   depends_on 'libcroco'
