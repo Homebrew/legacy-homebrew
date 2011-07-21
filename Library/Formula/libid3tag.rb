9b4a7c1ae9d493c0fadd618a3e4cfe9862d14c21
iXô <ixo@kawaiyume.net>
2011-07-21 20:45:23 +0200

    libid3tag: add patches to correct memory leaks
    libid3tag has some serious memory leaks which are unlikely to be
    corrected upstream, since the original project hasn't updated since
    2004. These Gentoo patches resolve the issues.
    
    Closes #6517.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/libid3tag.rb
+++ Library/Formula/libid3tag.rb
@@ -26,6 +26,28 @@ Cflags: -I${includedir}
     Formula.factory('mad').homepage
   end
 
+  # Fixes serious memory leaks; see https://bugs.launchpad.net/mixxx/+bug/403586
+  def patches
+    base = "http://mirror.ovh.net/gentoo-portage/media-libs/libid3tag/files/0.15.1b/"
+    # patch for utf-16 (memory leaks)
+    { :p1 => ["libid3tag-0.15.1b-utf16.patchlibid3tag-0.15.1b-utf16.patch",
+              "libid3tag-0.15.1b-unknown-encoding.patch",
+              "libid3tag-0.15.1b-compat.patch",
+              "libid3tag-0.15.1b-file-write.patch",
+              ].map { |file_name| "#{base}/#{file_name}" },
+
+      # typedef for 64-bit long + buffer overflow
+      :p0 => ["libid3tag-0.15.1b-64bit-long.patch",
+              "libid3tag-0.15.1b-fix_overflow.patch",
+              "libid3tag-0.15.1b-tag.patch",
+             ].map { |file_name| "#{base}/#{file_name}" },
+
+      # corrects "a cappella" typo
+      :p2 => ["libid3tag-0.15.1b-a_capella.patch",
+             ].map { |file_name| "#{base}/#{file_name}" }
+    }
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
