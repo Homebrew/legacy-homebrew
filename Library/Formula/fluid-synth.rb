3f60ffef7b3ccc7030480e4480d5658a577281e3
Adam Vandenberg <flangy@gmail.com>
2011-07-21 09:00:22 -0700

    fluidsynth: update homepage

--- Library/Formula/fluid-synth.rb
+++ Library/Formula/fluid-synth.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class FluidSynth < Formula
   url 'http://sourceforge.net/projects/fluidsynth/files/fluidsynth-1.1.3/fluidsynth-1.1.3.tar.gz'
-  homepage 'http://fluidsynth.resonance.org/trac'
+  homepage 'http://www.fluidsynth.org/'
   md5 '0d3e3cc770b4da413010dfb7dfdce9c8'
 
   depends_on 'pkg-config' => :build
