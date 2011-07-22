55f9d1240a62d68715841d2bc2d7df6c011de35b
Trevor Wennblom <trevor@well.com>
2011-07-22 13:52:45 -0500

    resolve #6546 - NoMethodError: undefined method `each' for "FLAG":String

--- Library/Homebrew/extend/string.rb
+++ Library/Homebrew/extend/string.rb
@@ -22,6 +22,7 @@ module StringInreplaceExtension
 
   # Removes variable assignments completely.
   def remove_make_var! flags
+    flags = [flags] if !flags.kind_of? Array
     flags.each do |flag|
       # Also remove trailing \n, if present.
       gsub! Regexp.new("^#{flag}[ \\t]*=(.*)$\n?"), ""
