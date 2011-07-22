39942f84a4d97f4dc22b03fd341be8b652a21b10
Carlos Perez <carlos_perez@darkoperator.com>
2011-07-21 22:53:05 -0400

    Updateted John formula to latest version of John with the latest jumbo patch

--- Library/Formula/john.rb
+++ Library/Formula/john.rb
@@ -1,13 +1,9 @@
 require 'formula'
 
 class John < Formula
-  url 'http://www.openwall.com/john/g/john-1.7.3.4.tar.bz2'
+  url 'http://www.openwall.com/john/g/john-1.7.8-jumbo-2.tar.bz2'
   homepage 'http://www.openwall.com/john/'
-  md5 '2f2310c49961c3edea6f92b8dcd45ff4'
-
-  def patches
-    { :p0 => DATA }
-  end
+  md5 'e1f382181249163a3e0468014e7c779e'
 
   fails_with_llvm
 
@@ -26,16 +22,3 @@ class John < Formula
   end
 end
 
-
-__END__
---- src/john.c.orig	2010-01-01 22:58:55.000000000 -0500
-+++ src/john.c	2010-01-01 22:59:11.000000000 -0500
-@@ -249,7 +249,7 @@ static void john_init(char *name, int ar
- 		cfg_init(CFG_PRIVATE_ALT_NAME, 1);
- #endif
- 		cfg_init(CFG_FULL_NAME, 1);
--		cfg_init(CFG_ALT_NAME, 0);
-+		cfg_init(CFG_ALT_NAME, 1);
- 	}
- 
- 	status_init(NULL, 1);
