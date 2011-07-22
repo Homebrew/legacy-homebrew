d28504bc84d0f1198c652a8e90a6d4875a93d001
Trever Miller <trever@phasorburn.com>
2011-07-22 13:50:57 -0700

    Fix home page.

--- Library/Formula/rcs.rb
+++ Library/Formula/rcs.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Rcs < Formula
   url 'ftp://ftp.cs.purdue.edu/pub/RCS/rcs-5.7.tar.Z'
   version '5.7'
-  homepage 'http://www.gnu.org/s/rcs/'
+  homepage 'http://www.cs.purdue.edu/homes/trinkle/RCS/'
   md5 '423282f0edb353296d9f3498ab683abf'
 
   def install
