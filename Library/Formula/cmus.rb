3eb42fc0f5f50a50cb596b2bac697245a7a8e62a
Mark Szymanski <mrman208@me.com>
2011-07-24 11:22:58 -0500

    cmus 2.4.2

--- Library/Formula/cmus.rb
+++ Library/Formula/cmus.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Cmus < Formula
-  url 'http://downloads.sourceforge.net/cmus/cmus-v2.4.1.tar.bz2'
+  url 'http://downloads.sourceforge.net/cmus/cmus-v2.4.2.tar.bz2'
   homepage 'http://cmus.sourceforge.net/'
-  md5 '7932bbada04bc9a273c332a323fc5704'
+  md5 'f3ed7f14db20344ad7386aef48b98a4c'
 
   depends_on 'pkg-config' => :build
   depends_on 'libao'
