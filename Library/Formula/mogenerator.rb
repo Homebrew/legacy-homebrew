8438f3bac5480fd2cf09a2629812319e06ca191e
Nathan de Vries <nathan@atnan.com>
2011-07-25 02:59:37 +1000

    Mogenerator 1.23.
    * Install the Mogenerator default templates & provide instructions for
      copying them into "~/Library/Application Support/mogenerator".
    
    * Force the use of Clang.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mogenerator.rb
+++ Library/Formula/mogenerator.rb
@@ -1,13 +1,30 @@
 require 'formula'
 
 class Mogenerator < Formula
-  url 'https://github.com/rentzsch/mogenerator/tarball/1.22'
+  url 'https://github.com/rentzsch/mogenerator/tarball/1.23'
   homepage 'http://rentzsch.github.com/mogenerator/'
-  md5 '72084ea17995f3ef9faa1ce9379a5ca3'
+  md5 'cb6bcbb1fe8303a89e8ee27b789ac8ed'
   head "https://github.com/rentzsch/mogenerator.git"
 
   def install
+    ENV.clang
     system "xcodebuild -target mogenerator -configuration Release SYMROOT=symroot OBJROOT=objroot"
     bin.install "symroot/Release/mogenerator"
+
+    # Install default demplates
+    (prefix+'templates').install Dir['templates/*.motemplate']
+  end
+
+  def caveats; <<-EOS.undent
+     A default set of Mogenerator templates have been installed to:
+       #{prefix}/templates
+
+     If you haven't written your own templates, you may wish to copy these for
+     your own use:
+
+       mkdir -p "#{ENV['HOME']}/Library/Application Support/mogenerator"
+       cp #{prefix}/templates/*.motemplate "#{ENV['HOME']}/Library/Application Support/mogenerator/"
+
+    EOS
   end
 end
