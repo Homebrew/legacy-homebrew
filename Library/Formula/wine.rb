cc722c32b20900458c74551baca40762b85e119d
Adam Vandenberg <flangy@gmail.com>
2011-07-24 17:27:35 -0700

    wine: move --devel to 1.3.25

--- Library/Formula/wine.rb
+++ Library/Formula/wine.rb
@@ -4,8 +4,8 @@ class Wine < Formula
   homepage 'http://www.winehq.org/'
 
   if ARGV.flag? '--devel'
-    url 'http://downloads.sourceforge.net/project/wine/Source/wine-1.3.21.tar.bz2'
-    sha256 '6c089df0d164c442a5620f7b5eee48ab21fd746a75793731cc3a028b20df57a0'
+    url 'http://downloads.sourceforge.net/project/wine/Source/wine-1.3.25.tar.bz2'
+    sha256 'f5525a966efd2f973c9a0fd6391d0d3e5817432e59598fe47c494b240d7e1caa'
   else
     url 'http://downloads.sourceforge.net/project/wine/Source/wine-1.2.3.tar.bz2'
     sha256 '3fd8d3f2b466d07eb90b8198cdc9ec3005917a4533db7b8c6c69058a2e57c61f'
