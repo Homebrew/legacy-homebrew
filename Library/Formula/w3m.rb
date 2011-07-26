ce12231ac8f09d9e470d52ebbc63143aa3390bf8
Jin Choi <jsc@alum.mit.edu>
2011-07-26 03:05:26 -0400

    w3m: patch for API change in bdw-gc
    Current version of w3m segfaults due to an old version of bdw-gc. With
    the working alpha version, an API change causes a compilation
    failure. This patch fixes that and remains compatible with the old
    API.

--- Library/Formula/w3m.rb
+++ Library/Formula/w3m.rb
@@ -15,4 +15,26 @@ class W3m < Formula
                           "--disable-image"
     system "make install"
   end
+
+  def patches
+    # GC_set_warn_proc no longer returns old proc
+    DATA
+  end
 end
+
+__END__
+diff --git a/main.c b/main.c
+index fed8c55..55fe3f1 100644
+--- a/main.c
++++ b/main.c
+@@ -842,7 +842,8 @@ main(int argc, char **argv, char **envp)
+     mySignal(SIGPIPE, SigPipe);
+ #endif
+ 
+-    orig_GC_warn_proc = GC_set_warn_proc(wrap_GC_warn_proc);
++    orig_GC_warn_proc = GC_get_warn_proc();
++    GC_set_warn_proc(wrap_GC_warn_proc);
+     err_msg = Strnew();
+     if (load_argc == 0) {
+ 	/* no URL specified */
+
