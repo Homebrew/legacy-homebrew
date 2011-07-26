2fa795852d6806734a348c1c7730513b96fb93cf
Jack Nagel <jacknagel@gmail.com>
2011-07-26 16:02:10 -0500

    GNU Parallel 20110722
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Parallel < Formula
-  url 'http://ftp.gnu.org/gnu/parallel/parallel-20110522.tar.bz2'
+  url 'http://ftp.gnu.org/gnu/parallel/parallel-20110722.tar.bz2'
   homepage 'http://savannah.gnu.org/projects/parallel/'
-  md5 '83c12186d740698cddb3795420d0e1f8'
+  md5 'fddfaad6e944e7d6609bf110f9a562cc'
 
   def install
     system "./configure", "--prefix=#{prefix}"
