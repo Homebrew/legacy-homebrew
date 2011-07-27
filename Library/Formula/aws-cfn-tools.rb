e4e104dec1ae38f3b8cfeda3eab51705190c8e90
Pat Downey <pat.downey@gmail.com>
2011-07-27 09:45:17 +0200

    AWS CloudFormation 1.0.8
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/aws-cfn-tools.rb
+++ Library/Formula/aws-cfn-tools.rb
@@ -6,8 +6,8 @@ require "#{File.dirname __FILE__}/ec2-api-tools.rb"
 class AwsCfnTools < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/AWS-CloudFormation/2555753788650372'
   url 'https://s3.amazonaws.com/cloudformation-cli/AWSCloudFormation-cli.zip'
-  version '1.0.7'
-  md5 '0ba857ef6beca08c20f5d57a31585098'
+  version '1.0.8'
+  md5 'f47dd2331d8f608d6bee508e24268a67'
 
   depends_on 'ec2-api-tools'
 
