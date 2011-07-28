e7ce99cd20822071406c97e05d3f624de6a8df11
Andy Fowler <andy@andyfowler.com>
2011-07-28 19:03:11 -0400

    libmemcached 0.50 (0.51 is currently broken, see <https://bugs.launchpad.net/libmemcached/+bug/817289>)

--- Library/Formula/libmemcached.rb
+++ Library/Formula/libmemcached.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libmemcached < Formula
-  url 'http://download.tangent.org/libmemcached-0.43.tar.gz'
+  url 'http://launchpad.net/libmemcached/1.0/0.50/+download/libmemcached-0.50.tar.gz'
   homepage 'http://libmemcached.org'
-  md5 'f6940255a1889871ef3a29f430370950'
+  md5 'c8627014a37cd821cf93317b8de6f9f8'
 
   depends_on 'memcached'
 
