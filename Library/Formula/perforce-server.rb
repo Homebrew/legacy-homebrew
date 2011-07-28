9c5053314ff01595877c64bd2a73248cb17b3afb
Robert Shaw <robert914@me.com>
2011-07-28 15:04:01 -0700

    perforce-server 2010.2.322263
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/perforce-server.rb
+++ Library/Formula/perforce-server.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class PerforceServer < Formula
-  url 'http://filehost.perforce.com/perforce/r10.1/bin.darwin80u/p4d'
+  url 'http://filehost.perforce.com/perforce/r10.2/bin.darwin90u/p4d'
   homepage 'http://www.perforce.com/'
-  md5 '27660df45ac5008891e17868063de6b7'
-  version '2010.1.278506'
+  md5 'ec899caca75736d85a19ab99e9e37fdd'
+  version '2010.2.322263'
 
   def install
     bin.install 'p4d'
