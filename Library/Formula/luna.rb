4066695df5f3720915568f13a7be00da828dce4a
Chris Ledet <chris@ledet.io>
2011-07-27 23:15:07 -0400

    added luna programming language formula

---
+++ Library/Formula/luna.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Luna < Formula
+  url 'https://github.com/visionmedia/luna/tarball/master'
+  head 'https://github.com/visionmedia/luna'
+  homepage 'http://visionmedia.github.com/luna/'
+  md5 '3b1b2b59fbf5a86d3273e865e1882206'
+  version 'pre0.0.1'
+
+  def install
+    system "make"
+    mv 'luna', prefix
+  end
+end
