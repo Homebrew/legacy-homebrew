928e757e2c6e78cdc068c678d97df99f2c055b13
moumar <moumar@MacBook-Pro-de-moumar.local>
2011-07-28 17:13:58 +0200

    sphinx: support for libstemmer compilation as an option

--- Library/Formula/sphinx.rb
+++ Library/Formula/sphinx.rb
@@ -14,8 +14,19 @@ class Sphinx < Formula
     args << "--with-pgsql" if `/usr/bin/which pg_config`.size > 0
     args << "--without-mysql" if `/usr/bin/which mysql`.size <= 0
 
+    if ARGV.include? '--with-libstemmer'
+      args << "--with-libstemmer"
+      system("curl http://snowball.tartarus.org/dist/libstemmer_c.tgz | tar xzf -")
+    end
+
     system "./configure", *args
     system "make install"
+  end
+
+  def options
+    [
+      ['--with-libstemmer', "Compile with libstemmer"]
+    ]
   end
 
   def caveats
