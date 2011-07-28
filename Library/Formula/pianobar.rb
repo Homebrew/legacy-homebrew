cebee37989f2c29cc6b4532083009c190756cf77
Jin Yi <razamatan@hotmail.com>
2011-07-28 13:06:00 -0700

    fix pianobar formula again.  commented for future.

--- Library/Formula/pianobar.rb
+++ Library/Formula/pianobar.rb
@@ -20,6 +20,17 @@ class Pianobar < Formula
     # Force GCC into c99 mode
     ENV.append 'CFLAGS', "-std=c99"
 
+    # PLEASE RETAIN THIS CODEBLOCK - begin
+    # Enable 64-bit builds if needed
+    w_flag = MacOS.prefer_64_bit? ? "-W64" : ""
+    # Help non-default install paths
+    lib_path = HOMEBREW_PREFIX.to_s == "/usr/local" ? "" : "-I#{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib"
+
+    inreplace "Makefile" do |s|
+      s.gsub! "-O2 -DNDEBUG", "-O2 -DNDEBUG #{w_flag} #{lib_path}"
+    end
+    # PLEASE RETAIN THIS CODEBLOCK - end
+
     system "make", "PREFIX=#{prefix}"
     system "make", "install", "PREFIX=#{prefix}"
 
