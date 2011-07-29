d3fadd5aacc89eece99557fe6e638b915ef7849d
Max Howell <max@methylblue.com>
2011-07-29 14:54:42 +0100

    Don't mention the sbin PATH issue unless files are there
    Because the new installer will always create /usr/local/sbin. Because we must keep /usr/local root:wheel unless we want to be considered bad citizens.

--- Library/Homebrew/cmd/doctor.rb
+++ Library/Homebrew/cmd/doctor.rb
@@ -370,7 +370,7 @@ def check_user_path
   end
 
   # Don't complain about sbin not being in the path if it doesn't exist
-  if (HOMEBREW_PREFIX+'sbin').exist?
+  if (HOMEBREW_PREFIX+'sbin').children.count > 0
     unless seen_prefix_sbin
       puts <<-EOS.undent
         Some brews install binaries to sbin instead of bin, but Homebrew's
