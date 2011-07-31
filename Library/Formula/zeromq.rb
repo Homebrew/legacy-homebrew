851861a36ff3974452a333ef99578e6110ae3c60
Adam Vandenberg <flangy@gmail.com>
2011-07-30 22:20:09 -0700

    zeromq 2.1.7: 2.1.8 was moved back to rc status

--- Library/Formula/zeromq.rb
+++ Library/Formula/zeromq.rb
@@ -5,10 +5,10 @@ def pgm_flags
 end
 
 class Zeromq < Formula
-  url 'http://download.zeromq.org/zeromq-2.1.8.tar.gz'
+  url 'http://download.zeromq.org/zeromq-2.1.7.tar.gz'
   head 'https://github.com/zeromq/libzmq.git'
   homepage 'http://www.zeromq.org/'
-  md5 '2d234b1bc32a9e2c905b58f505d23ffa'
+  md5 '7d3120f8a8fb913a7e55c57c6eb024f3'
 
   fails_with_llvm "Compiling with LLVM gives a segfault while linking."
 
