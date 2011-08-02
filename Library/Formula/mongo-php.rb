ed78166b6cc167b407a0b99b6644377b162d5a6b
Osman Üngür <osmanungur@gmail.com>
2011-08-02 14:24:32 +0300

    mongo-php 1.2.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mongo-php.rb
+++ Library/Formula/mongo-php.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class MongoPhp < Formula
-  url 'http://pecl.php.net/get/mongo-1.1.4.tgz'
+  url 'http://pecl.php.net/get/mongo-1.2.2.tgz'
   homepage 'http://pecl.php.net/package/mongo'
-  md5 '22f1e25690589f6d80d5ed29e56644eb'
+  md5 'b589a922222bfe5a13e5b18359e87437'
 
   def install
     Dir.chdir "mongo-#{version}" do
