4d421b347f65442b05ba63f268608bdb7c9b663d
ptrv <mail@petervasil.net>
2011-08-06 18:30:48 +0200

    glfw 2.7.1

--- Library/Formula/glfw.rb
+++ Library/Formula/glfw.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Glfw < Formula
-  url 'http://downloads.sourceforge.net/project/glfw/glfw/2.7/glfw-2.7.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/glfw/glfw/2.7.1/glfw-2.7.1.tar.bz2'
   homepage 'http://www.glfw.org/'
-  md5 'cc024566d78279c6af728ba2812c9107'
+  md5 '1cf551916124fccfc303fa4e50080f91'
 
   def install
     ENV.prepend 'PREFIX', "#{prefix}"
