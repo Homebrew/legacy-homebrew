0af55db431657c1ddea60e0fcd2f9c71cdb4b807
Ruben Fonseca <root@cpan.org>
2011-08-06 12:08:38 +0100

    libvpx 0.9.7
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libvpx.rb
+++ Library/Formula/libvpx.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Libvpx < Formula
-  url 'http://webm.googlecode.com/files/libvpx-v0.9.6.tar.bz2'
-  sha1 'a3522bd2b73d52381ba767ded1cbf4760e9cc6f8'
+  url 'http://webm.googlecode.com/files/libvpx-v0.9.7.tar.bz2'
+  sha1 '639596df7182a93db83f61af8f5bb5b6a13dcf63'
   homepage 'http://www.webmproject.org/code/'
 
   depends_on 'yasm' => :build
