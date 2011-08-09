6517007aa9f8d9579894f9ee2e2b0c43d9b09dcb
Ajay Gopinathan <ajay@gopinathan.net>
2011-08-09 12:40:06 +0200

    Cleaned up indentation

--- Library/Formula/cvxopt.rb
+++ Library/Formula/cvxopt.rb
@@ -12,20 +12,19 @@ class Cvxopt < Formula
 		'src/setup.py'
 	end
 
+	def install
+		inreplace setup_file, "BUILD_GLPK = 0", "BUILD_GLPK = 1"
+		inreplace setup_file, "GLPK_LIB_DIR = '/usr/lib'", "GLPK_LIB_DIR = '#{HOMEBREW_PREFIX}/lib'"
+		inreplace setup_file, "GLPK_INC_DIR = '/usr/include'", "GLPK_INC_DIR = '#{HOMEBREW_PREFIX}/include'"
+		inreplace  'src/setup.py', "ATLAS_LIB_DIR = '/usr/lib'", "ATLAS_LIB_DIR = '/System/Library/Frameworks/vecLib.framework'"
+		system "cd src && python setup.py install --home=#{HOMEBREW_PREFIX}"
+	end
 
-  def install
-	  inreplace setup_file, "BUILD_GLPK = 0", "BUILD_GLPK = 1"
-	  inreplace setup_file, "GLPK_LIB_DIR = '/usr/lib'", "GLPK_LIB_DIR = '#{HOMEBREW_PREFIX}/lib'"
-	  inreplace setup_file, "GLPK_INC_DIR = '/usr/include'", "GLPK_INC_DIR = '#{HOMEBREW_PREFIX}/include'"
-	  inreplace  'src/setup.py', "ATLAS_LIB_DIR = '/usr/lib'", "ATLAS_LIB_DIR = '/System/Library/Frameworks/vecLib.framework'"
-	  system "cd src && python setup.py install --home=#{HOMEBREW_PREFIX}"
-  end
-
-  def caveats
-	  the_caveat = <<-EOS
+	def caveats
+		the_caveat = <<-EOS
 	  Ensure that homebrew python packages can be seen by Python:
-	       export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/python:$PYTHONPATH
-	  EOS
-	  return the_caveat
-  end
+	  	export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/python:$PYTHONPATH
+				EOS
+		return the_caveat
+	end
 end
