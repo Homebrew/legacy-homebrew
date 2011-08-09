16472ac80707cd50809329d2da1811c582d0af8c
Chris Ledet <chris@ledet.io>
2011-08-08 22:59:50 -0400

    s3cmd 1.0.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/s3cmd.rb
+++ Library/Formula/s3cmd.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class S3cmd < Formula
-  url 'http://downloads.sourceforge.net/project/s3tools/s3cmd/1.0.0/s3cmd-1.0.0.tar.gz'
+  url 'http://downloads.sourceforge.net/project/s3tools/s3cmd/1.0.1/s3cmd-1.0.1.tar.gz'
   homepage 'http://s3tools.org/s3cmd'
-  md5 'e82f0246479015ce50a09d8d4ada8429'
+  md5 'dc62becc03a3e6100843611ebe2707c2'
 
   def install
     libexec.install Dir['s3cmd', 'S3']
