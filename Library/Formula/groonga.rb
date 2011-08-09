87cb2a55d0d589937d93ef32e84a63c41b5616f4
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-08-09 12:54:00 +0900

    groonga 1.2.4
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Groonga < Formula
-  url 'http://groonga.org/files/groonga/groonga-1.0.6.tar.gz'
+  url 'http://packages.groonga.org/source/groonga/groonga-1.2.4.tar.gz'
   homepage 'http://groonga.org/'
-  md5 'a278e72801a68acf374cbc309d73ba34'
+  md5 '58faebee3055f849e01be953eb4a6b7f'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--with-zlib"
