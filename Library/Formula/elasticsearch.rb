6e1fea5ceb102ae507bf66fada53bb8e8b3dda9d
Jasper van Wanrooy <jasper@vanwanrooy.net>
2011-08-09 15:05:51 +0200

    Upgrade the elastic search library to the latest upgrade.

--- Library/Formula/elasticsearch.rb
+++ Library/Formula/elasticsearch.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Elasticsearch < Formula
-  url 'https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.17.2.tar.gz'
+  url 'https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.17.4.tar.gz'
   homepage 'http://www.elasticsearch.org'
-  md5 '457846ead97d76b9549c71c8e630bc54'
+  md5 '28312a3cc61572d27b061a597c048172'
 
   def install
     # Remove Windows files
