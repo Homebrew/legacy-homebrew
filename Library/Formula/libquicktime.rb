ddeea569c58c3bef9c8dcbf2e134b957e4a36001
Steve Marshall <steve@nascentguruism.com>
2011-08-10 00:13:00 +0100

    Upgrade libquicktime to 1.2.3 to fix ffmpeg 0.8 incompatibility.

--- Library/Formula/libquicktime.rb
+++ Library/Formula/libquicktime.rb
@@ -1,18 +1,9 @@
 require 'formula'
 
 class Libquicktime < Formula
-  url 'http://downloads.sourceforge.net/project/libquicktime/libquicktime/1.1.5/libquicktime-1.1.5.tar.gz'
+  url 'http://downloads.sourceforge.net/project/libquicktime/libquicktime/1.2.3/libquicktime-1.2.3.tar.gz'
   homepage 'http://libquicktime.sourceforge.net/'
-  md5 '0fd45b3deff0317c2f85a34b1b106acf'
-
-  def patches
-      # http://bugs.gentoo.org/show_bug.cgi?id=294488
-      #
-      # Previous version jpeg automatically set dinfo.do_fancy_downsampling to FALSE.
-      # Newer versions (since 7) of media-libs/jpeg do not do that anymore and the
-      # program must do it explicitly
-      DATA
-  end
+  md5 '9a82a1546408605ea8337b3a7c78786e'
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
@@ -31,15 +22,3 @@ class Libquicktime < Formula
     system "make install"
   end
 end
-
-__END__
---- libquicktime-1.1.5/plugins/mjpeg/libmjpeg.c	2010-01-15 05:41:17.000000000 +1100
-+++ libquicktime-1.1.5/plugins/mjpeg/libmjpeg.c-2	2010-04-07 23:33:46.000000000 +1000
-@@ -785,6 +785,7 @@
-   result->jpeg_compress.input_components = 3;
-   result->jpeg_compress.in_color_space = JCS_RGB;
-   jpeg_set_quality(&(result->jpeg_compress), mjpeg->quality, 0);
-+  result->jpeg_compress.do_fancy_downsampling = FALSE;
- 
-   if(mjpeg->use_float) 
-     result->jpeg_compress.dct_method = JDCT_FLOAT;
