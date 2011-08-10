2d9eabe12aa879f7cdca9bcdc2571bece6766d39
Misty De Meo <mistydemeo@gmail.com>
2011-08-10 14:13:24 -0500

    xmp 3.4.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xmp.rb
+++ Library/Formula/xmp.rb
@@ -1,9 +1,16 @@
 require 'formula'
 
 class Xmp < Formula
-  url 'http://downloads.sourceforge.net/project/xmp/xmp/3.3.0/xmp-3.3.0.tar.gz'
+  url 'http://downloads.sourceforge.net/project/xmp/xmp/3.4.0/xmp-3.4.0.tar.gz'
   homepage 'http://xmp.sourceforge.net'
-  md5 '0ac15cdb68cf0a08f418d37b4c1843bd'
+  md5 '8d18f1340e46278f7006c4d6df385e4b'
+
+  def patches
+    # fixes compilation error with GCC 4.2
+    # can be removed in the next release
+    # http://sourceforge.net/mailarchive/message.php?msg_id=27928353
+    "http://downloads.sourceforge.net/project/xmp/xmp/3.4.0/xmp-3.4.0-ununsed-but-set-variable-gcc-warning.patch"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
