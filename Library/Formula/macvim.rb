0824705a5846d1b021cfdd22d5a58d087b39aefc
Sean McCullough <mcculloughsean@gmail.com>
2011-08-11 15:50:11 -0500

    Added warning to not install macvim if you find 1.9.2

--- Library/Formula/macvim.rb
+++ Library/Formula/macvim.rb
@@ -8,14 +8,14 @@ class Macvim < Formula
   head 'https://github.com/b4winckler/macvim.git', :branch => 'master'
 
   def options
-  [
-    # Building custom icons fails for many users, so off by default.
-    ["--custom-icons", "Try to generate custom document icons."],
-    ["--with-cscope", "Build with Cscope support."],
-    ["--with-envycoder", "Build with Envy Code R Bold font."],
-    ["--override-system-vim", "Override system vim."],
-    ["--enable-clipboard", "Enable System clipboard handling in the terminal."]
-  ]
+    [
+      # Building custom icons fails for many users, so off by default.
+      ["--custom-icons", "Try to generate custom document icons."],
+      ["--with-cscope", "Build with Cscope support."],
+      ["--with-envycoder", "Build with Envy Code R Bold font."],
+      ["--override-system-vim", "Override system vim."],
+      ["--enable-clipboard", "Enable System clipboard handling in the terminal."]
+    ]
   end
 
   depends_on 'cscope' if ARGV.include? '--with-cscope'
@@ -34,54 +34,58 @@ class Macvim < Formula
     ENV['ARCHFLAGS'] = "-arch #{arch}"
 
     args = ["--with-macsdk=#{MACOS_VERSION}",
-           "--with-features=huge",
-           "--with-tlib=ncurses",
-           "--enable-multibyte",
-           "--with-macarchs=#{arch}",
-           "--enable-perlinterp",
-           "--enable-pythoninterp",
-           "--enable-rubyinterp",
-           "--enable-tclinterp"]
+      "--with-features=huge",
+        "--with-tlib=ncurses",
+        "--enable-multibyte",
+        "--with-macarchs=#{arch}",
+      "--enable-perlinterp",
+        "--enable-pythoninterp",
+        "--enable-rubyinterp",
+        "--enable-tclinterp"]
 
-    args << "--enable-cscope" if ARGV.include? "--with-cscope"
-    args << "--enable-clipboard" if ARGV.include? "--enable-clipboard"
+      args << "--enable-cscope" if ARGV.include? "--with-cscope"
+      args << "--enable-clipboard" if ARGV.include? "--enable-clipboard"
 
-    system "./configure", *args
+      unless ARGV.include? "--custom-icons"
+        inreplace "src/MacVim/icons/Makefile", "$(MAKE) -C makeicns", ""
+        inreplace "src/MacVim/icons/make_icons.py", "dont_create = False", "dont_create = True"
+      end
 
-    unless ARGV.include? "--custom-icons"
-      inreplace "src/MacVim/icons/Makefile", "$(MAKE) -C makeicns", ""
-      inreplace "src/MacVim/icons/make_icons.py", "dont_create = False", "dont_create = True"
-    end
+      if ARGV.include? "--with-envycoder"
+        # Font download location has changed.
+        # This is fixed in MacVim trunk, but not in the stable tarball.
+        inreplace "src/MacVim/icons/Makefile",
+          "http://download.damieng.com/latest/EnvyCodeR",
+          "http://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip"
+      else
+        # Remove the font from the build dependencies
+        inreplace "src/MacVim/icons/Makefile",
+          '$(OUTDIR)/MacVim-generic.icns: make_icons.py vim-noshadow-512.png loadfont.so Envy\ Code\ R\ Bold.ttf',
+          "$(OUTDIR)/MacVim-generic.icns: make_icons.py vim-noshadow-512.png loadfont.so"
+      end
 
-    if ARGV.include? "--with-envycoder"
-      # Font download location has changed.
-      # This is fixed in MacVim trunk, but not in the stable tarball.
-      inreplace "src/MacVim/icons/Makefile",
-        "http://download.damieng.com/latest/EnvyCodeR",
-        "http://download.damieng.com/fonts/original/EnvyCodeR-PR7.zip"
-    else
-      # Remove the font from the build dependencies
-      inreplace "src/MacVim/icons/Makefile",
-        '$(OUTDIR)/MacVim-generic.icns: make_icons.py vim-noshadow-512.png loadfont.so Envy\ Code\ R\ Bold.ttf',
-        "$(OUTDIR)/MacVim-generic.icns: make_icons.py vim-noshadow-512.png loadfont.so"
-    end
+      if `ruby -v` =~ /1\.9\.2/  
+        onoe "Can't install MacVim with ruby 1.9.2. Please change your environment to use 1.8.7"
+        raise "Can't install MacVim using ruby 1.9.2"
+      else
+        ohai "Using Ruby that isn't 1.9"
+      end
+      system "make"
 
-    system "make"
+      prefix.install "src/MacVim/build/Release/MacVim.app"
+      inreplace "src/MacVim/mvim", /^# VIM_APP_DIR=\/Applications$/,
+        "VIM_APP_DIR=#{prefix}"
+      bin.install "src/MacVim/mvim"
 
-    prefix.install "src/MacVim/build/Release/MacVim.app"
-    inreplace "src/MacVim/mvim", /^# VIM_APP_DIR=\/Applications$/,
-              "VIM_APP_DIR=#{prefix}"
-    bin.install "src/MacVim/mvim"
-
-    # Create MacVim vimdiff, view, ex equivalents
-    executables = %w[mvimdiff mview mvimex]
-    executables += %w[vi vim vimdiff view vimex] if ARGV.include? "--override-system-vim"
-    executables.each {|f| ln_s bin+'mvim', bin+f}
+      # Create MacVim vimdiff, view, ex equivalents
+      executables = %w[mvimdiff mview mvimex]
+      executables += %w[vi vim vimdiff view vimex] if ARGV.include? "--override-system-vim"
+      executables.each {|f| ln_s bin+'mvim', bin+f}
   end
 
   def caveats; <<-EOS.undent
     MacVim.app installed to:
-      #{prefix}
+    #{prefix}
 
     To link the application to a normal Mac OS X location:
         brew linkapps
