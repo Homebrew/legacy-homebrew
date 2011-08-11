9a7dae41514686489f783d1da03b537bf3136fc3
Paul Phillips <paulp@improving.org>
2011-08-11 09:22:37 -0700

    ascii: updated homepage
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ascii.rb
+++ Library/Formula/ascii.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ascii < Formula
   url 'http://www.catb.org/~esr/ascii/ascii-3.10.tar.gz'
-  homepage 'http://www.catb.org/~esr/'
+  homepage 'http://www.catb.org/~esr/ascii/'
   md5 '9dcae3ab8b097efd16a24c5d20e0044e'
 
   def install
