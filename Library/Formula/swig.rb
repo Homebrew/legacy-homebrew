53ef962fdc12dbe9d7876581bd53be09cf09cc78
m <mfox@cavium.com>
2011-08-11 19:34:47 -0700

    swig formula
    I resurected this one. It was removed as a duplicate because supposedly
    OSX comes with a version of swig. OSX Lion does not seem to have this
    anymore.

---
+++ Library/Formula/swig.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Swig < Formula
+  url 'http://sourceforge.net/swig/swig-2.0.4.tar.gz'
+  homepage 'http://www.swig.org/'
+  md5 '4319c503ee3a13d2a53be9d828c3adc0'
+  depends_on 'pcre'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+      "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
