b9c4a382a832ddc8521d6f49d471436407a1ff57
Adam Vandenberg <flangy@gmail.com>
2011-08-13 11:34:09 -0700

    Chromedriver: fix audit warning

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -1,7 +1,6 @@
 require 'formula'
 
 class Chromedriver < Formula
-  version '14.0.813.0'
   url 'http://chromium.googlecode.com/files/chromedriver_mac_14.0.813.0.zip'
   homepage 'http://seleniumhq.wordpress.com/2011/07/07/new-chromedriver/'
   sha1 'b5f375000cb1e292cbe35966550075f9f28d50e5'
