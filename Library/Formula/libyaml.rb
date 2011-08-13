6155e6b345f3de5b12a6c91b8000efa1c3b65833
William Woodall <wjwwood@gmail.com>
2011-08-13 18:29:17 -0500

    Cleaned up the libyaml to have a --universal option rather than automatically making it universal.  This is ready for a pull request.

--- Library/Formula/libyaml.rb
+++ Library/Formula/libyaml.rb
@@ -4,11 +4,23 @@ class Libyaml < Formula
   url 'http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz'
   homepage 'http://pyyaml.org/wiki/LibYAML'
   md5 '36c852831d02cf90508c29852361d01b'
-
+  
+  def options
+    [
+      ["--universal", "Build for both 32 & 64 bit Intel."],
+    ]
+  end
+  
   def install
-    ENV['CFLAGS'] = "-arch i386 -arch x86_64"
-    ENV['LDFLAGS'] = "-arch i386 -arch x86_64"
-    system './configure', "--disable-dependency-tracking", "--prefix=#{prefix}"
+    if ARGV.build_universal?
+      ENV['CFLAGS'] = "-arch i386 -arch x86_64"
+      ENV['LDFLAGS'] = "-arch i386 -arch x86_64"
+      args << "--disable-dependency-tracking"
+    end
+    
+    args << "--prefix=#{prefix}"
+    
+    system './configure', *args
     system "make install"
   end
 end
