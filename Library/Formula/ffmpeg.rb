4408f08a9b196f47c7f6de7870c62aa7b529d674
João <mail@joaomoreno.com>
2011-08-14 23:38:01 +0100

    Fixed ffmpeg in Lion

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -32,10 +32,13 @@ class Ffmpeg < Formula
     args << "--enable-libvpx" if Formula.factory('libvpx').installed?
     args << "--enable-libxvid" if Formula.factory('xvid').installed?
 
-    # Enable alternate compilers
-    args << "--cc=clang" if ENV.use_clang?
-    args << "--cc=llvm-gcc" if ENV.use_llvm?
-    args << "--cc=gcc" if ENV.use_gcc?
+    if MacOS.lion?
+      args << "--cc=clang"
+    else
+      args << "--cc=clang" if ENV.use_clang?
+      args << "--cc=llvm-gcc" if ENV.use_llvm?
+      args << "--cc=gcc" if ENV.use_gcc?
+    end
 
     # For 32-bit compilation under gcc 4.2, see:
     # http://trac.macports.org/ticket/20938#comment:22
