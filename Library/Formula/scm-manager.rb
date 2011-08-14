e194a44150bbb5ecf761914b8d79b5944552d12d
Sebastian Sdorra <s.sdorra@ostfalia.de>
2011-08-14 15:16:42 +0200

    scm-manager 1.13
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/scm-manager.rb
@@ -0,0 +1,81 @@
+require 'formula'
+
+class ScmManagerCliClient < Formula
+  homepage 'http://www.scm-manager.org'
+  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/clients/scm-cli-client/1.13/scm-cli-client-1.13-jar-with-dependencies.jar'
+  version '1.13'
+  md5 '3d79ea68075ab0f8891aaf591f87224b'
+end
+
+# 1.14 gives me "Bad CPU type in executable" in the jvsc-darwin executable when starting the server
+# - @adamv
+class ScmManager < Formula
+  homepage 'http://www.scm-manager.org'
+  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/scm-server/1.13/scm-server-1.13-app.tar.gz'
+  version '1.13'
+  md5 'd2923425425cd233539c6750ae54889d'
+
+  skip_clean :all
+
+  def install
+    rm_rf Dir['bin/*.bat']
+
+    libexec.install Dir['*']
+
+    (bin/'scm-server').write <<-EOS.undent
+      #!/bin/bash
+      BASEDIR="#{libexec}"
+      REPO="#{libexec}/lib"
+      "#{libexec}/bin/scm-server" "$@"
+    EOS
+    chmod 0755, bin/'scm-server'
+
+    tools = libexec/'tools'
+    ScmManagerCliClient.new.brew { tools.install Dir['*'] }
+
+    scmCliClient = bin+'scm-cli-client'
+    scmCliClient.write <<-EOS.undent
+      #!/bin/bash
+      java -jar "#{tools}/scm-cli-client-1.14-jar-with-dependencies.jar" "$@"
+    EOS
+    chmod 0755, scmCliClient
+
+    plist_path.write startup_plist
+    plist_path.chmod 0644
+  end
+
+  def caveats; <<-EOS.undent
+    If this is your first install, automatically load on login with:
+        mkdir -p ~/Library/LaunchAgents
+        cp #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    If this is an upgrade and you already have the #{plist_path.basename} loaded:
+        launchctl unload -w ~/Library/LaunchAgents/#{plist_path.basename}
+        cp #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    Or start manually:
+      scm-server start
+    EOS
+  end
+
+def startup_plist; <<-EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>#{bin}/scm-server</string>
+      <string>start</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+  </dict>
+</plist>
+EOS
+  end
+end
