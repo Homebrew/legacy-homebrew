1f2f80d90dca5c2b873e2647f64097af65d68faf
guns <self@sungpae.com>
2011-08-14 18:39:09 -0500

    feh 1.14.2
    Fixes compilation on Lion against libpng 1.5
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/feh.rb
+++ Library/Formula/feh.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Feh < Formula
-  url 'http://feh.finalrewind.org/feh-1.11.2.tar.bz2'
+  url 'http://feh.finalrewind.org/feh-1.14.2.tar.bz2'
   homepage 'http://freshmeat.net/projects/feh'
-  md5 '3b2354d78a882ce02b429bbe053467a2'
+  md5 '5a64a93ae00f2f4189a6119e835d4452'
 
   depends_on 'giblib' => :build
 
