dbc3a24a25183d99a18a632ddafe56577f0bae83
David Shere <david.e.shere@gmail.com>
2011-08-14 17:55:54 -0700

    avra 1.3.0

--- Library/Formula/avra.rb
+++ Library/Formula/avra.rb
@@ -6,9 +6,9 @@ class Avra < Formula
   md5 'd5d48369ceaa004c4ca09f61f69b2c84'
 
   def install
-        system "touch NEWS ChangeLog"   # build fails if these don't exist
-        system "cd src; ./bootstrap"
-        system "cd src; ./configure --prefix=#{prefix}"
-        system "cd src; make install"
+	system "touch NEWS ChangeLog"	# build fails if these don't exist
+	system "cd src; ./bootstrap"
+	system "cd src; ./configure --prefix=#{prefix}"
+	system "cd src; make install"
   end
 end
