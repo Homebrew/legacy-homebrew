1c6bbf95254b29ccc4a226324da8e6bf988e6968
Sven Axelsson <sven@fronter.com>
2011-08-16 11:43:20 +0200

    asciidoc: add --HEAD
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/asciidoc.rb
+++ Library/Formula/asciidoc.rb
@@ -3,9 +3,14 @@ require 'formula'
 class Asciidoc < Formula
   url 'http://downloads.sourceforge.net/project/asciidoc/asciidoc/8.6.5/asciidoc-8.6.5.tar.gz'
   md5 '9247724283501ec1cfb27d5eae9e5eaf'
+  head 'https://code.google.com/p/asciidoc/', :using => :hg
   homepage 'http://www.methods.co.nz/asciidoc'
 
   def install
+    if ARGV.build_head? and not File.exists? "./configure"
+      ohai "Creating configure file"
+      system "autoconf"
+    end
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make install"
   end
