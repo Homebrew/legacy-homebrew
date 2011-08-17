53827038a3b4eb083a274ab3a89fc1789d0c8d99
JuanPablo <jpabloaj@gmail.com>
2011-08-16 21:17:29 -0400

    git-home formula : homepage added

--- Library/Formula/git-home.rb
+++ Library/Formula/git-home.rb
@@ -2,11 +2,8 @@ require 'formula'
 
 class GitHome < Formula
   url 'https://github.com/juanpabloaj/git-home/tarball/v0.1'
-  homepage ''
+  homepage 'https://github.com/juanpabloaj/git-home'
   md5 '3e5ab7b6516e33660c404e75a9d81d82'
-
-  # depends_on 'cmake'
-
   def install
 	  bin.install "git-home"
   end
