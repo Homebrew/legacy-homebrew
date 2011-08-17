562bfeb79ea05f85818461d551feef23178588f7
Max Howell <max@methylblue.com>
2011-08-17 12:30:21 +0100

    Only error out for `sudo brew install`
    Note this only happens if Homebrew is *not* installed sudo. Which is most of the time true.
    
    Fixes #6899

--- Library/Homebrew/cmd/install.rb
+++ Library/Homebrew/cmd/install.rb
@@ -9,6 +9,12 @@ module Homebrew extend self
       raise "No available formula for #{name}\n#{msg}" if msg
     end unless ARGV.force?
 
+    if Process.uid.zero? and not File.stat(HOMEBREW_BREW_FILE).uid.zero?
+      # note we only abort if Homebrew is *not* installed as sudo and the user
+      # calls brew as root. The fix is to chown brew to root.
+      abort "Cowardly refusing to `sudo brew install'"
+    end
+
     install_formulae ARGV.formulae
   end
 
