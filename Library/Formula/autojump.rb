3a15a69e21e1d5b1f2d0e10595c91b89fdbcb28c
Evan Shelhamer <shelhamer@imaginarynumber.net>
2011-08-19 08:03:56 -0400

    autojump v16
    Includes fixes for Lion.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/autojump.rb
+++ Library/Formula/autojump.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Autojump < Formula
-  url 'https://github.com/downloads/joelthelion/autojump/autojump_v15.tar.gz'
+  url 'https://github.com/downloads/joelthelion/autojump/autojump_v16.tar.gz'
   homepage 'https://github.com/joelthelion/autojump/wiki'
-  md5 'a4a36d774ccb27cbcc9dcc6e2528632b'
+  md5 '2ac11b916911fd35e0da3f5cbee45f14'
 
   head 'https://github.com/joelthelion/autojump.git'
 
