cd4e639b9f572d97df2ddbf43365383bad0b123c
Adam Vandenberg <flangy@gmail.com>
2011-08-19 09:36:33 -0700

    wget 1.12: 1.13 is now 404
    This reverts commit 4d39ab424fad311b8cc03ea0e9eb3029ca6af91c.

--- Library/Formula/wget.rb
+++ Library/Formula/wget.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wget < Formula
   homepage 'http://www.gnu.org/software/wget/'
-  url 'http://ftp.gnu.org/gnu/wget/wget-1.13.tar.gz'
-  md5 'd3698837e6a9567ca9be10f115989940'
+  url 'http://ftp.gnu.org/gnu/wget/wget-1.12.tar.bz2'
+  md5 '308a5476fc096a8a525d07279a6f6aa3'
 
   depends_on "openssl" if MacOS.leopard?
   depends_on "libidn" if ARGV.include? "--enable-iri"
@@ -12,11 +12,15 @@ class Wget < Formula
     [["--enable-iri", "Enable iri support."]]
   end
 
+  def patches
+    # Fixes annoying TLS Subject Alternative Name bug encountered especially when using GitHub
+    # Remove when 1.12.1 is released.
+    # See https://savannah.gnu.org/bugs/?23934
+    "http://savannah.gnu.org/file/wget-1.12-subjectAltNames.diff?file_id=18828"
+  end
+
   def install
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--with-ssl=openssl",
-            "--prefix=#{prefix}"]
+    args = ["--disable-debug", "--prefix=#{prefix}"]
     args << "--disable-iri" unless ARGV.include? "--enable-iri"
 
     system "./configure", *args
