fae8e716bc9e91aa498767ede4fb464f2f9adb1f
Jari Bakken <jari.bakken@gmail.com>
2011-08-20 15:40:23 +0200

    chromedriver 14.0.813.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Chromedriver < Formula
   url 'http://chromium.googlecode.com/files/chromedriver_mac_14.0.836.0.zip'
-  homepage 'http://seleniumhq.wordpress.com/2011/07/07/new-chromedriver/'
+  homepage 'http://code.google.com/p/selenium/wiki/ChromeDriver'
   sha1 '2b292bb47e26286fc714c3b9a74904800a6acc97'
 
   def install
