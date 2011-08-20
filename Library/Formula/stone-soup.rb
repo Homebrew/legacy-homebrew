5fdde2a77110fa965acbacc16471bff58a5b4b71
Mario Lassnig <mario@lassnig.net>
2011-08-20 12:49:42 +0200

    added tiles option, fixed XCode4 build on 10.6

--- Library/Formula/stone-soup.rb
+++ Library/Formula/stone-soup.rb
@@ -8,9 +8,41 @@ class StoneSoup < Formula
   # Keep empty folders for save games and such
   skip_clean :all
 
+  def options
+    [['--with-tiles', 'compile the tiles version']]
+  end
+
   def install
     ENV.x11
     Dir.chdir "source"
-    system "make", "prefix=#{prefix}", "SAVEDIR=saves/", "DATADIR=data/", "TILES=y", "install"
+
+    if ARGV.include? '--with-tiles'
+      # works out of the box
+      system "make", "prefix=#{prefix}", "SAVEDIR=saves/", "DATADIR=data/", "TILES=y", "install"
+    else
+      # if XCode 4 is installed on Snow Leopard, then forcing the framework breaks the build, so remove it.
+      # Xcode3 should be fine out of the box.
+      if File.directory? "/Developer-old"
+        inreplace "makefile" do |s|
+          s.gsub!(
+                  "CC = $(GCC) $(CFLAGS_ARCH) -isysroot $(SDKROOT) -mmacosx-version-min=$(SDK_VER)",
+                  "CC = $(GCC) $(CFLAGS_ARCH)"
+                  )
+          s.gsub!(
+                  "CXX = $(GXX) $(CFLAGS_ARCH) -isysroot $(SDKROOT) -mmacosx-version-min=$(SDK_VER)",
+                  "CXX = $(GXX) $(CFLAGS_ARCH)"
+                  )
+          s.gsub!(
+                  "DEPCC = $(GCC) $(or $(CFLAGS_DEPCC_ARCH),$(CFLAGS_ARCH)) -isysroot $(SDKROOT) -mmacosx-version-min=$(SDK_VER)",
+                  "DEPCC = $(GCC) $(or $(CFLAGS_DEPCC_ARCH),$(CFLAGS_ARCH))"
+                  )
+          s.gsub!(
+                  "DEPCXX = $(GXX) $(or $(CFLAGS_DEPCC_ARCH),$(CFLAGS_ARCH)) -isysroot $(SDKROOT) -mmacosx-version-min=$(SDK_VER)",
+                  "DEPCXX = $(GXX) $(or $(CFLAGS_DEPCC_ARCH),$(CFLAGS_ARCH))"
+                  )
+        end
+        system "make", "prefix=#{prefix}", "SAVEDIR=saves/", "DATADIR=data/", "install"
+      end
+    end
   end
 end
