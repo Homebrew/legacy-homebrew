49b1a766c9a61507fc78b87d58d012731c59ec17
Tony <zearin@gonk.net>
2011-08-21 19:50:46 -0300

    Allow `brew upgrade` to upgrade specified formulae (or all formulae if none given). I hacked this together with pretty much no Ruby experience, but it tests okay for me. Feel free to improve any sub-standard code. :P

--- Library/Contributions/examples/brew-upgrade.rb
+++ Library/Contributions/examples/brew-upgrade.rb
@@ -4,4 +4,20 @@
 require 'cmd/outdated'
 require 'cmd/install'
 
-Homebrew.install_formulae Homebrew.outdated_brews.map{ |_keg, name, _version| Formula.factory name }
+
+if ARGV.named.empty?  
+  # Upgrade all outdated formulae if none are specified on the command line
+  Homebrew.install_formulae Homebrew.outdated_brews.map{ |_keg, name, _version| Formula.factory name }
+
+else
+  # Upgrade only specified formulae
+  (ARGV.formulae & Homebrew.outdated_brews).each do |f|
+    Hombrew.install_formula f
+  end
+
+  # Notify anything already up-to-date
+  unless (ARGV.formulae - Homebrew.outdated_brews).empty?
+    ohai "Already up-to-date:", (ARGV.formulae - Homebrew.outdated_brews)
+  end
+  
+end
