1a8f7b50f47434abfb2f6772aadb42b89d1199aa
Max Howell <max@methylblue.com>
2011-08-21 14:29:48 +0100

    Specify that they are symlinks

--- Library/Homebrew/cmd/link.rb
+++ Library/Homebrew/cmd/link.rb
@@ -2,7 +2,7 @@ module Homebrew extend self
   def link
     ARGV.kegs.each do |keg|
       print "Linking #{keg}... "
-      puts "#{keg.link} links created"
+      puts "#{keg.link} symlinks created"
     end
   end
 end
