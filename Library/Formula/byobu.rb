cd1c3a749f86e4ebedbcc13ba5dfa31a0bd81db3
Justin Mayer <entrop@gmail.com>
2011-08-22 11:20:47 -0700

    byobu 4.30
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/byobu.rb
+++ Library/Formula/byobu.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Byobu < Formula
-  url 'http://launchpad.net/byobu/trunk/4.17/+download/byobu_4.17.orig.tar.gz'
+  url 'http://launchpad.net/byobu/trunk/4.30/+download/byobu_4.30.orig.tar.gz'
   homepage 'http://launchpad.net/byobu'
-  md5 '9f6921e870706df00c95df0c891925b9'
+  md5 'fc2852d1d1cf68282af0a24a680b87f6'
 
   def install
     system "./configure", "--prefix=#{prefix}"
