24fc5f282d6005130b870babafa74a18a09a04b3
Jan de Muijnck-Hughes <j.demuijnckhughes@googlemail.com>
2011-08-22 20:50:47 +0100

    Rebased branch to fix clean commit, added update for PBC.
    My branch of homebrew was 'corrupt'. Rebased to latest version, and included update for PBC.

--- Library/Formula/pbc.rb
+++ Library/Formula/pbc.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Pbc <Formula
+class Pbc < Formula
   url 'http://crypto.stanford.edu/pbc/files/pbc-0.5.12.tar.gz'
   homepage 'http://crypto.stanford.edu/pbc/'
   md5 '4f3f017b5709fcc46b58d1ebc6b30e3f'
