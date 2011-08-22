a8f1a2acccd865fa406610b185648aae8744137c
Alex Conner <aconner@nframe.com>
2011-08-22 13:07:46 -0400

    FreeTDS 0.91
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/freetds.rb
+++ Library/Formula/freetds.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Freetds < Formula
-  url 'http://ibiblio.org/pub/Linux/ALPHA/freetds/stable/freetds-0.82.tar.gz'
+  url 'http://ibiblio.org/pub/Linux/ALPHA/freetds/stable/freetds-0.91.tar.gz'
   homepage 'http://www.freetds.org/'
-  md5 '3df6b2e83fd420e90f1becbd1162990a'
+  md5 'b14db5823980a32f0643d1a84d3ec3ad'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--with-tdsver=8.0", "--enable-msdblib", "--mandir=#{man}"
