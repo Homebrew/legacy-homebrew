3acb5fa379de89799c8d50ab77ca683714bf8694
Jeff Hodges <jeff@somethingsimilar.com>
2011-08-22 19:23:00 -0700

    jsonpp 1.0.0

---
+++ Library/Formula/jsonpp.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class Jsonpp < Formula
+  url 'http://github.com/jmhodges/jsonpp/downloads/jsonpp-1.0.0-osx-x86_64.zip'
+  homepage 'http://jmhodges.github.com/jsonpp/'
+  md5 '2cd5faf6f11466b587a4ab744de13a16'
+
+  def install
+    system "cp jsonpp #{bin}"
+  end
+end
