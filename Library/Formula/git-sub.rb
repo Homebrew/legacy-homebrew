8ebdb03b7c392e02cf72232701fcf75529ca97e3
Greg Osuri <gosuri@gmail.com>
2011-08-22 17:08:48 -0700

    git-sub 0.0.3

--- Library/Formula/git-sub.rb
+++ Library/Formula/git-sub.rb
@@ -5,8 +5,6 @@ class GitSub < Formula
   homepage 'https://github.com/gosuri/git-sub'
   md5 '33bc0d0017de284ccb85f775649a0a90'
 
-  # depends_on 'cmake'
-
   def install
     system "make"
     system "make install PREFIX=#{prefix}"
