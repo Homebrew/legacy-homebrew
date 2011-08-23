be5a10bc050ca61d8ae7adba64ab53966c4c0eb5
Greg Osuri <gosuri@gmail.com>
2011-08-22 22:30:00 -0700

    git-sub 0.0.4

--- Library/Formula/git-sub.rb
+++ Library/Formula/git-sub.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class GitSub < Formula
-  url 'https://github.com/gosuri/git-sub/tarball/0.0.3'
-  homepage 'https://github.com/gosuri/git-sub'
-  md5 '33bc0d0017de284ccb85f775649a0a90'
 
-  # depends_on 'cmake'
+  homepage  'https://github.com/gosuri/git-sub'
+  head      'git://github.com/gosuri/git-sub.git'
+  url       'https://github.com/gosuri/git-sub/tarball/0.0.4'
+  md5       'bc7d8be1c1afb517291282c905a41a7c'
 
   def install
     system "make"
