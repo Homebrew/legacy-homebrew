4a02966a70c8d04893af637e46eeee04cf5725d6
Philipp Suter <suter.philipp@gmail.com>
2011-08-23 22:53:05 +0200

    Changed CMinpack according to feedback

--- Library/Formula/cminpack.rb
+++ Library/Formula/cminpack.rb
@@ -5,10 +5,9 @@ class Cminpack < Formula
   homepage 'http://devernay.free.fr/hacks/cminpack/cminpack.html'
   md5 '3573b33d498cc1bf3787a86efbd12c3a'
 
-  depends_on 'cmake'
+  depends_on 'cmake' => :build
 
   def install
-    # system "./configure", "--disable-debug", "--disable-dependency-tracking","--prefix=#{prefix}"
     system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
