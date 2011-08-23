121262ed48948caae1ead31b49257f25455d2ee4
Adimir Colen <adimircolen@gmail.com>
2011-08-23 20:17:05 -0300

    add suport for sam2p command

---
+++ Library/Formula/sam2p.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Sam2p < Formula
+  url 'http://sam2p.googlecode.com/files/sam2p-0.49.tar.gz'
+  homepage ''
+  md5 '72387e3e3897580fb5ae439713e75177'
+
+  # depends_on 'cmake'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--enable-lzw", "--enable-gif"
+    # system "cmake . #{std_cmake_parameters}"
+    system "make install"
+  end
+end
