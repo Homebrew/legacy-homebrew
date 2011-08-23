9b8a5be8e2305ff1cadfd32320cb64f099da3266
Philipp Suter <suter.philipp@gmail.com>
2011-08-23 22:36:59 +0200

    Changed FLANN to follow homebrew standards.

---
+++ Library/Formula/flann.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Flann < Formula
+  url 'http://people.cs.ubc.ca/~mariusm/uploads/FLANN/flann-1.6.11-src.zip'
+  homepage 'http://www.cs.ubc.ca/~mariusm/index.php/FLANN/FLANN'
+  md5 '5fd889b9f3777aa6e0d05b2546d25eb5'
+
+  depends_on 'cmake'
+  depends_on 'HDF5'
+  depends_on 'GTest'
+  def install
+    # system "./configure", "--disable-debug", "--disable-dependency-tracking","--prefix=#{prefix}"
+    system "cmake . #{std_cmake_parameters} -DBUILD_MATLAB_BINDINGS=OFF"
+    system "make install"
+  end
+end
