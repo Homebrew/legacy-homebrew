241148df53f66568762a6aac52d7108679d39ad9
Adimir Colen <adimircolen@gmail.com>
2011-08-24 19:41:59 -0300

    fixed issues in brew audit sam2p

--- Library/Formula/sam2p.rb
+++ Library/Formula/sam2p.rb
@@ -2,14 +2,13 @@ require 'formula'
 
 class Sam2p < Formula
   url 'http://sam2p.googlecode.com/files/sam2p-0.49.tar.gz'
-  homepage ''
+  homepage 'http://sam2p.googlecode.com'
   md5 '72387e3e3897580fb5ae439713e75177'
 
-  # depends_on 'cmake'
+  depends_on 'cmake'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--enable-lzw", "--enable-gif"
-    # system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 end
