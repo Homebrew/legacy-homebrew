bee97120594045e5dba084d7c313c6d8a142cd9f
Peter Morelli <pmorelli@gmail.com>
2011-08-23 23:28:10 -0700

    play 1.2.3
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Play < Formula
-  url 'http://download.playframework.org/releases/play-1.2.2.zip'
+  url 'http://download.playframework.org/releases/play-1.2.3.zip'
   homepage 'http://www.playframework.org/'
-  md5 'd50f34bacf02f1ebb90eb13920b14346'
+  md5 '75822b1ec443239a4467147a94882442'
 
   def install
     rm_rf 'python' # we don't need the bundled Python for windows
