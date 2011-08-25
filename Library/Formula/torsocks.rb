59fa7020e67c417cdfc1352e4895c8554b134efe
Colby Olson <me@colbyolson.com>
2011-08-24 22:01:43 -0700

    Added Formula torsocks1.1

--- Library/Formula/torsocks.rb
+++ Library/Formula/torsocks.rb
@@ -5,12 +5,11 @@ class Torsocks < Formula
   homepage 'http://code.google.com/p/torsocks/'
   md5 '1704fd009ed1a1c1dc9c6b72305a5449'
 
-  # depends_on 'cmake'
+  depends_on 'tor'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 end
