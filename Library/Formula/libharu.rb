7b39b06374fe6f7e1a260c371755c80a08e8a8b2
Daniel Berjón <asuranceturix@gmail.com>
2011-08-25 16:43:28 +0200

    Libharu: fixed compilation on Lion
    Apparently Lion ships with libpng 1.5, which breaks compilation of libharu 2.2.1. Applied patch to fix it before next release.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/libharu.rb
+++ Library/Formula/libharu.rb
@@ -5,6 +5,15 @@ class Libharu < Formula
   url 'http://libharu.org/files/libharu-2.2.1.tar.bz2'
   md5 '4febd7e677b1c5d54db59a608b84e79f'
 
+  def patches
+    # Fixes compilation against LibPNG 1.5. Can be removed on next release.
+    # Based on a commit in the LibHaru repository which does not apply cleanly
+    # due to a missing CHANGES file:
+    #
+    #     https://github.com/tony2001/libharu/commit/e5bf8b0.patch
+    "http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/media-libs/libharu/files/libharu-2.2.1-libpng-1.5.patch?revision=1.1"
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
