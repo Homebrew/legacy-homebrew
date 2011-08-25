661894f7703c19beeec0882b01aaff945a5b3705
Drew Stephens <drew@dinomite.net>
2011-08-25 13:30:18 -0400

    Create formula for JSCoverage
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

---
+++ Library/Formula/jscoverage.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Jscoverage < Formula
+  url 'http://siliconforks.com/jscoverage/download/jscoverage-0.5.1.tar.bz2'
+  homepage 'http://siliconforks.com/jscoverage/'
+  md5 'a70d79a6759367fbcc0bcc18d6866ff3'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    bin.install('jscoverage');
+  end
+end
