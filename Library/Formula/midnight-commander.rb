ddad9a6a0b8ece03834fb2c808e95ca16bbe6588
Jack Nagel <jacknagel@gmail.com>
2011-08-26 11:08:39 -0500

    midnight-commander: use CurlUnsafeDownloadStrategy
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -1,16 +1,8 @@
 require 'formula'
 
-class MidnightCommanderDownloadStrategy < CurlDownloadStrategy
-  # midnight-commander.org uses a self-signed cert, which is
-  # silly and trips curl up
-  def _fetch
-    curl '-k', @url, '-o', @tarball_path
-  end
-end
-
 class MidnightCommander < Formula
   url 'http://www.midnight-commander.org/downloads/mc-4.7.5.3.tar.bz2',
-      :using => MidnightCommanderDownloadStrategy
+      :using => CurlUnsafeDownloadStrategy
   homepage 'http://www.midnight-commander.org/'
   sha256 '0875b4c745d771b65ab71d0a295f2e6e91297121411a82068e2879f39a7628b6'
 
