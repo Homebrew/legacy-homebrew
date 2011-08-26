979f2760724269361f087b7f535c9d58ff1a562d
Max Howell <max@methylblue.com>
2011-08-26 16:53:16 +0100

    Built with LLVM-2335 on Lion
    I presume this means it will also work on 10.6 etc. as long as the LLVM version is new enough. But we may be proved wrong.

--- Library/Formula/pcre.rb
+++ Library/Formula/pcre.rb
@@ -5,7 +5,7 @@ class Pcre < Formula
   homepage 'http://www.pcre.org/'
   md5 'f14a9fef3c92f3fc6c5ac92d7a2c7eb3'
 
-  fails_with_llvm "Bus error in ld on SL 10.6.4"
+  fails_with_llvm "Bus error in ld on SL 10.6.4", :build => 2326
 
   def options
     [["--universal", "Build a universal binary."]]
