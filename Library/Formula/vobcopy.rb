d1249435e73ec994dbe5d8596fbaf23079b85019
miku <martin.czygan@gmail.com>
2011-08-27 15:06:38 +0200

    updated vobcopy homepage
    previous homepage http://vobcopy.org yields a 403 Forbidden

--- Library/Formula/vobcopy.rb
+++ Library/Formula/vobcopy.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Vobcopy < Formula
   url 'http://vobcopy.org/download/vobcopy-1.2.0.tar.bz2'
-  homepage 'http://vobcopy.org'
+  homepage 'http://freshmeat.net/projects/vobcopy'
   md5 '88f735ccd051093ff40dab4597bc586e'
 
   depends_on 'libdvdread'
