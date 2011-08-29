4abe84f4f65a617d22aa681d19873cec2a5140fd
Brian Gyss <brian.gyss@gmail.com>
2011-08-29 14:37:59 -0700

    Updated flann formula based on comments.
    This formula is updated based on a number of comments and requests.

--- Library/Formula/flann.rb
+++ Library/Formula/flann.rb
@@ -5,19 +5,36 @@ class Flann < Formula
   homepage 'http://www.cs.ubc.ca/~mariusm/index.php/FLANN/FLANN'
   md5 '5fd889b9f3777aa6e0d05b2546d25eb5'
 
-  depends_on 'cmake'
-  depends_on 'octave'
-  depends_on 'gtest'
+  depends_on 'cmake' => :build
+  depends_on 'gtest' => :build
   depends_on 'hdf5'
-  depends_on 'numpy' => :python
 
-  def install
-    system "cmake . #{std_cmake_parameters}"
-    system "make install"
+  def options
+    [
+      ['--python', "Enable python bindings."],
+      ['--matlab', "Enable matlab/octave bindings."]
+  ]
   end
 
-  def test
-    # this will fail we won't accept that, make it test the program works!
-    system "/usr/bin/false"
+  def install
+    args = std_cmake_parameters.split
+    if ARGV.include? '--matlab'
+      args << "-DBUILD_MATLAB_BINDINGS:BOOL=ON"
+    else
+      args << "-DBUILD_MATLAB_BINDINGS:BOOL=OFF"
+    end
+    
+    if ARGV.include? '--python'
+      args << "-DBUILD_PYTHON_BINDINGS:BOOL=ON"
+    else
+      args << "-DBUILD_PYTHON_BINDINGS:BOOL=OFF"
+    end
+    
+    Dir.mkdir 'build'
+    args << ".."
+    Dir.chdir 'build' do
+      system "cmake", *args
+      system "make install"
+    end
   end
 end
