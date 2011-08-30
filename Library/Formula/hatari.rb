6c1539f24e493514dfbf8a781dc56d5a90287a92
Trevor Wennblom <trevor@well.com>
2011-08-30 15:49:02 -0500

    hatari 1.5.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/hatari.rb
+++ Library/Formula/hatari.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Hatari < Formula
-  url 'http://download.berlios.de/hatari/hatari-1.4.0.tar.bz2'
+  url 'http://download.berlios.de/hatari/hatari-1.5.0.tar.bz2'
   homepage 'http://hatari.berlios.de/'
-  md5 '2f30e5c9e146ee92e3f2f5ae1cef3673'
+  md5 '16277cff73ec3a342b87b7b7ea3932f4'
 
   depends_on 'cmake' => :build
   depends_on 'sdl'
