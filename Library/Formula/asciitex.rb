8906f88efe553bd1a971c48e1831ecc32b282f0d
Rui Pereira <rui.pereira@gmail.com>
2011-08-30 13:48:46 +0200

    New formula: asciiTeX
    asciiTeX is an ASCII equation renderer with a syntax
    similar to LaTeX.

---
+++ Library/Formula/asciitex.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Asciitex < Formula
+  url 'http://downloads.sourceforge.net/project/asciitex/asciiTeX-0.21.tar.gz'
+  homepage 'http://asciitex.sourceforge.net'
+  md5 'b894d924880a5659d9637f30ed5a3bb0'
+  version '0.21'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-gtk"
+    inreplace 'Makefile', 'man/asciiTeX_gui.1', ''
+    system "make install"
+    prefix.install 'EXAMPLES'
+  end
+
+  def test
+    system "asciiTeX -f #{prefix}/EXAMPLES"
+  end
+end
