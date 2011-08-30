cd6c68be0ec3e3ea082909f1876442ead5b0c035
Chris Deigan <chris@deigan.id.au>
2011-08-30 13:22:02 +1000

    Initial ggobi formula

---
+++ Library/Formula/ggobi.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Ggobi < Formula
+  url 'http://www.ggobi.org/downloads/ggobi-2.1.9.tar.bz2'
+  homepage 'http://www.ggobi.org/'
+  md5 'b579861f157dfc6c5669604859352eb4'
+
+  depends_on 'gtk+'
+  depends_on 'glib'
+  depends_on 'atk'
+  depends_on 'cairo'
+  depends_on 'fontconfig'
+  depends_on 'pango'
+  depends_on 'gettext'
+
+  def install
+	 inreplace 'src/texture.c', 'psort', 'p_sort'
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "make check"
+  end
+end
