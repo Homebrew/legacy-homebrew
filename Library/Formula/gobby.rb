519162455d76ba590ab7d44e8ffdb3fe22c321d3
Marnen Laibow-Koser <marnen@marnen.org>
2011-08-30 11:34:16 -0400

    gobby 0.4.94
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/gobby.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Gobby < Formula
+  homepage 'http://gobby.0x539.de'
+  url 'http://releases.0x539.de/gobby/gobby-0.4.94.tar.gz'
+  md5 '899d68ebe1af67c5a45b49328b86e23e'
+
+  head 'git://git.0x539.de/git/gobby.git'
+  
+  depends_on 'pkg-config' => :build
+  depends_on 'gtkmm'
+  depends_on 'libgsasl'
+  depends_on 'libxml++'
+  depends_on 'gtksourceview'
+  depends_on 'obby'
+  depends_on 'gettext'
+  depends_on 'hicolor-icon-theme'
+  depends_on 'libinfinity'
+
+  def install
+    ENV.x11
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
