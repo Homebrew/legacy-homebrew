64b616b9d6d54f176b6c8aeba56fc67a555ef5e4
David Cox <david.daniel.cox@gmail.com>
2011-08-30 22:52:36 -0400

    Removed cmake dependency (for the sake of minimalism), and specified the dos2unix dependency as build only

--- Library/Formula/anttweakbar.rb
+++ Library/Formula/anttweakbar.rb
@@ -5,12 +5,12 @@ class Anttweakbar < Formula
   homepage 'http://www.antisphere.com/Wiki/tools:anttweakbar'
   md5 '5aaee2ea2bca03a258f8dad494828d7e'
 
-  depends_on 'cmake'
-  depends_on 'dos2unix'
+  depends_on 'dos2unix' => :build
 
   def install
-    system "cmake . #{std_cmake_parameters}"
-    system "make install"
+    system "cd src; make -f Makefile.osx"
+    lib.install('./lib/libAntTweakBar.dylib')
+    include.install('./include/AntTweakBar.h')
   end
 
   def patches
@@ -18,7 +18,11 @@ class Anttweakbar < Formula
     # in order to apply a patch
     system "dos2unix `find src/ -type f`"
 
-    # add build infrastructure (cmake), and patch a few 10.7 issues
-    "https://raw.github.com/gist/1163244/fc972c607502fdc088e765a9b6ecde0122566777/atb_patch.diff"
+    # patch two small issues -- one related to 10.7 compatibility, one related
+    # to missing symbols due to an (apparently) erroneous 'extern "C"' wrapper
+  'https://raw.github.com/gist/1182684/8afd29c2bef6c410f1886d297012af752c0d0ac3/atb_10_7_patch_minimal.diff'
   end
 end
+
+
+
