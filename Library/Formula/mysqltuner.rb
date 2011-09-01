98d41b11144d91aa7ea91a273b38775b73bf2269
Bernhard Essl <me@bernhardessl.com>
2011-09-01 16:39:44 +0200

    added new formula for mysqltuner

---
+++ Library/Formula/mysqltuner.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Mysqltuner < ScriptFileFormula
+  url 'http://mysqltuner.pl/mysqltuner.pl'
+  homepage 'http://mysqltuner.pl'
+  md5 'fa1dd0eb8d90893ae6f1f22e0a9adfe1'
+  version '1.2.0'
+end
