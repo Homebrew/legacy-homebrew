3c4bed875810cf6cd7f0419d29001ba1d1a933de
Adam Vandenberg <flangy@gmail.com>
2011-09-01 10:08:21 -0700

    Edit port/fink warning

--- Library/Homebrew/cmd/install.rb
+++ Library/Homebrew/cmd/install.rb
@@ -56,10 +56,9 @@ module Homebrew extend self
 
   def check_macports
     if MacOS.macports_or_fink_installed?
-      opoo "It appears you have Macports or Fink installed"
+      opoo "It appears you have Macports or Fink installed."
       puts "Software installed with other package managers causes known problems for"
-      puts "Homebrew. If formula fail to build uninstall Macports/Fink and reinstall any"
-      puts "affected formula."
+      puts "Homebrew. If a formula fails to build, uninstall Macports/Fink try again."
     end
   end
 
