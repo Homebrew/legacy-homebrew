869b89b64339299b14c9c8f01bdd6d6709018573
Jack Nagel <jacknagel@gmail.com>
2011-09-02 01:13:28 -0500

    tests: fix failing test_a_basic_install
    The test_a_basic_install was failing because it expected
    libexec.children.length to be 1, when in fact there were two files in
    the untarred libexec: NOOP, and the Mac OS X resource fork file ._NOOP.
    
    That file is now removed, and the test passes.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Homebrew/test/test_checksums.rb
+++ Library/Homebrew/test/test_checksums.rb
@@ -20,7 +20,7 @@ class ChecksumTests < Test::Unit::TestCase
   
   def test_md5
     valid_md5 = Class.new(TestBall) do
-      @md5='71aa838a9e4050d1876a295a9e62cbe6'
+      @md5='a04e0a201dcf2e5758ad351f7eea3ddc'
     end
     
     good_checksum valid_md5
@@ -36,7 +36,7 @@ class ChecksumTests < Test::Unit::TestCase
 
   def test_sha1
     valid_sha1 = Class.new(TestBall) do
-      @sha1='6ea8a98acb8f918df723c2ae73fe67d5664bfd7e'
+      @sha1='027afeac65cfb27c15fc372122e6b2be2c14daba'
     end
     
     good_checksum valid_sha1
@@ -52,7 +52,7 @@ class ChecksumTests < Test::Unit::TestCase
 
   def test_sha256
     valid_sha256 = Class.new(TestBall) do
-      @sha256='ccbf5f44743b74add648c7e35e414076632fa3b24463d68d1f6afc5be77024f8'
+      @sha256='b537438cdd7519dc645d034d7e01f0bcdf908758901f26550e6bf65b496c4474'
     end
     
     good_checksum valid_sha256
