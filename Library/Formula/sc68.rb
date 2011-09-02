b7191753ce7f34be9df343c1eb53a014355cbb47
Misty De Meo <mistydemeo@gmail.com>
2011-09-01 21:24:22 -0500

    SC68 2.2.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/sc68.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Sc68 < Formula
+  url 'http://downloads.sourceforge.net/project/sc68/sc68/2.2.1/sc68-2.2.1.tar.gz'
+  homepage 'http://sc68.atari.org/project.html'
+  md5 '84aa948f76274361f7e78c3563951eff'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--infodir=#{info}"
+    system "make install"
+  end
+
+  def test
+    # SC68 ships with a sample module; test attempts to print its metadata
+    system "#{bin}/info68 #{share}/sc68/Sample/About-Intro.sc68 -C ': ' -N -L"
+  end
+end
