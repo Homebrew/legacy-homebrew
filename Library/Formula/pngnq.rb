2f7f96a33c78b60d85f2ccd0739648308b021bfc
Chad Remesch <chad@remesch.com>
2011-09-01 22:56:59 -0400

    pngnq: fix compilation on Lion
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pngnq.rb
+++ Library/Formula/pngnq.rb
@@ -7,6 +7,14 @@ class Pngnq < Formula
 
   depends_on 'pkg-config' => :build
 
+  # Fixes compilation on OSX Lion
+  # png.h on Lion does not, in fact, include zlib.h
+  # See: http://sourceforge.net/tracker/?func=detail&aid=3353513&group_id=213072&atid=1024252
+  # See: http://sourceforge.net/tracker/?func=detail&aid=3402960&group_id=213072&atid=1024252
+  def patches
+    DATA
+  end
+
   def install
     ENV.x11 # for libpng
     system "./configure", "--disable-dependency-tracking",
@@ -14,3 +22,18 @@ class Pngnq < Formula
     system "make install"
   end
 end
+
+
+__END__
+diff --git a/src/rwpng.c b/src/rwpng.c
+index aaa21fc..5324afe 100644
+--- a/src/rwpng.c
++++ b/src/rwpng.c
+@@ -31,6 +31,7 @@
+
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <zlib.h>
+
+ #include "png.h"        /* libpng header; includes zlib.h */
+ #include "rwpng.h"      /* typedefs, common macros, public prototypes */
