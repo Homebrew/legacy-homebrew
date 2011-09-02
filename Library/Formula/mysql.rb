a572cf498ad952349dc3b371b854149bb3f762c7
Max Howell <max@methylblue.com>
2011-09-02 11:15:29 +0100

    Don't fails_with_llvm if MacOS.lion?
    Not sure how I failed so badly at logic. This should be rewritten so that the build info has a tested MacOS version too. However it seems unlikely that the same LLVM versions on different platforms would have different compile errors. So we'll risk it, and it'll be informative for us too.

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -9,8 +9,7 @@ class Mysql < Formula
   depends_on 'readline'
   depends_on 'pidof'
 
-  fails_with_llvm "https://github.com/mxcl/homebrew/issues/issue/144",
-                  :build => 2326 if MacOS.lion?
+  fails_with_llvm "https://github.com/mxcl/homebrew/issues/issue/144", :build => 2326
 
   skip_clean :all # So "INSTALL PLUGIN" can work.
 
