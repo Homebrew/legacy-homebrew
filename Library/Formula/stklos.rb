7d14bcd44cd4eae69256f49f062e539614f84d08
Jack Nagel <jacknagel@gmail.com>
2011-09-04 22:12:18 -0500

    stklos: mark as fails_with_llvm, update to 1.01
    Should fix #7423.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/stklos.rb
+++ Library/Formula/stklos.rb
@@ -1,15 +1,19 @@
 require 'formula'
 
 class Stklos < Formula
-  url 'http://www.stklos.net/download/stklos-1.00.tar.gz'
+  url 'http://www.stklos.net/download/stklos-1.01.tar.gz'
   homepage 'http://www.stklos.net/'
-  md5 '3fd4809205871b65882be9b9f7e17090'
+  md5 '2c370627c3abd07c30949b2ee7d3d987'
 
   depends_on 'gmp'
   depends_on 'pcre'
 
+  # it fails with LLVM while building its internal bdw-gc,
+  # for the same reason as bdw-gc itself.
+  fails_with_llvm "LLVM gives an unsupported inline asm error."
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
   end
