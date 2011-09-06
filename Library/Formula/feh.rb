ed1433f3bc2720dfba24e18d1e98787473974a5b
guns <self@sungpae.com>
2011-09-06 14:03:13 -0500

    feh: fix missing fonts issue, update to 1.16
    Building feh without PREFIX=... leads to errors about missing fonts,
    since the builtin font paths are hardcoded relative to the PREFIX.
    
    The prefix must be supplied during build, and perhaps during install
    (which must be done separately since there is no build dependency on
    installation).
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/feh.rb
+++ Library/Formula/feh.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class Feh < Formula
-  url 'http://feh.finalrewind.org/feh-1.14.2.tar.bz2'
+  url 'http://feh.finalrewind.org/feh-1.16.tar.bz2'
   homepage 'http://freshmeat.net/projects/feh'
-  md5 '5a64a93ae00f2f4189a6119e835d4452'
+  md5 'd8583e8dde2f383dc9a8dfc28bf6b348'
 
   depends_on 'giblib' => :build
 
   def install
     ENV.x11
 
-    system "make"
+    system "make", "PREFIX=#{prefix}"
     system "make", "PREFIX=#{prefix}", "install"
   end
 end
