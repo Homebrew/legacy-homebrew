3b612df44c8eca09d5d35b453f3b14982d08fbc9
BEN ENGLISCH <EMAILBEN145@gmail.com>
2011-09-06 01:26:59 -0500

    make cacaview less useless

--- Library/Formula/libcaca.rb
+++ Library/Formula/libcaca.rb
@@ -8,6 +8,7 @@ class Libcaca < Formula
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
+  depends_on 'imlib2'
 
   fails_with_llvm "unsupported inline asm: input constraint with a matching output constraint of incompatible type!"
 
@@ -21,7 +22,6 @@ class Libcaca < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
-                          "--disable-imlib2",
                           "--disable-doc",
                           "--disable-slang",
                           "--disable-java",
