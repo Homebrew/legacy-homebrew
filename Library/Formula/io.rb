cffefe8f975ae56abb5b163787c3e545c28cf548
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-09-06 16:02:08 -0400

    Use latest stable version of Io.

--- Library/Formula/io.rb
+++ Library/Formula/io.rb
@@ -1,32 +1,20 @@
 require 'formula'
 
 class Io < Formula
-  head 'https://github.com/stevedekorte/io.git'
+  head 'https://github.com/stevedekorte/io.git', :tag => '2010.06.06'
   homepage 'http://iolanguage.com/'
 
-  depends_on 'cmake' => :build
   depends_on 'libsgml'
   depends_on 'ossp-uuid'
   depends_on 'libevent'
 
-  # Either CMake doesn't detect OS X's png include path correctly,
-  # or there's an issue with io's build system; force the path in
-  # so we can build.
-  def patches
-    DATA
-  end
-
   def install
-    opoo "IO fails to build often!"
-    puts "There is no stable revision: https://github.com/mxcl/homebrew/issues/7399"
-
     ENV.j1
-    mkdir 'io-build'
 
-    Dir.chdir 'io-build' do
-      system "cmake .. #{std_cmake_parameters}"
-      system "make install"
-    end
+    system "make vm"
+    system "make install"
+    system "make port"
+    system "make install"
 
     rm_f Dir['docs/*.pdf']
     doc.install Dir['docs/*']
@@ -34,18 +22,3 @@ class Io < Formula
     prefix.install 'license/bsd_license.txt' => 'LICENSE'
   end
 end
-
-__END__
-diff --git a/addons/Image/CMakeLists.txt b/addons/Image/CMakeLists.txt
-index a65693d..2166f1b 100644
---- a/addons/Image/CMakeLists.txt
-+++ b/addons/Image/CMakeLists.txt
-@@ -22,7 +22,7 @@ if(PNG_FOUND AND TIFF_FOUND AND JPEG_FOUND)
- 	add_definitions(-DBUILDING_IMAGE_ADDON)
- 
- 	# Additional include directories
--	include_directories(${PNG_INCLUDE_DIR} ${TIFF_INCLUDE_DIR} ${JPEG_INCLUDE_DIR})
-+	include_directories("/usr/X11/include" ${PNG_INCLUDE_DIR} ${TIFF_INCLUDE_DIR} ${JPEG_INCLUDE_DIR})
- 
- 	# Generate the IoImageInit.c file.
- 	# Argument SHOULD ALWAYS be the exact name of the addon, case is
