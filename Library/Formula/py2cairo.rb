23f417cfc423922bf5ff038c4f1dbdfdf75331d6
John Harrison <ash@greaterthaninfinity.com>
2011-09-07 19:12:17 -0500

    Updating the way py2cairo is configured.

--- Library/Formula/py2cairo.rb
+++ Library/Formula/py2cairo.rb
@@ -8,14 +8,10 @@ class Py2cairo < Formula
   depends_on 'native-cairo'
 
   def install
-    # TODO: figure out if this is the "right way" to do this
-    ENV['ARCHFLAGS'] = "-arch x86_64"
+    # Python extensions default to universal but cairo may not be universal
+    ENV['ARCHFLAGS'] = "-arch x86_64" unless ARGV.build_universal?
     system "./waf", "configure", "--prefix=#{prefix}", "--nopyc", "--nopyo"
     system "./waf", "install"
   end
 
-  def test
-    # this will fail we won't accept that, make it test the program works!
-    system "/usr/bin/false"
-  end
 end
