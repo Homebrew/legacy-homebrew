e7c5aa0b8d062d7608926ddfcd3ffcbd8262e6dd
Charlie Sharpsteen <source@sharpsteen.net>
2011-09-11 13:06:05 -0700

    Return Formula objects from `outdated_brews`
    Previously, `outdated_brews` returned a list of lists---each containing three
    components of a Formula object:
    
      - f.rack
      - f.name
      - f.version
    
    Frequently more information is required which necessitates back-casting from
    `name` to a Formula object---simpy returning formula objects removes this step.

--- Library/Homebrew/cmd/upgrade.rb
+++ Library/Homebrew/cmd/upgrade.rb
@@ -14,24 +14,23 @@ module Homebrew extend self
     outdated = if ARGV.named.empty?
       Homebrew.outdated_brews
     else
-      ARGV.formulae.map do |f|
+      ARGV.formulae.each do |f|
         raise "#{f} already upgraded" if f.installed?
         raise "#{f} not installed" unless f.rack.exist? and not f.rack.children.empty?
-        [f.rack, f.name, f.version]
       end
     end
 
     if outdated.length > 1
       oh1 "Upgrading #{outdated.length} outdated package#{outdated.length.plural_s}, with result:"
-      puts outdated.map{ |_, name, version| "#{name} #{version}" } * ", "
+      puts outdated.map{ |f| "#{f.name} #{f.version}" } * ", "
     end
 
-    outdated.each do |rack, name, version|
-      installer = FormulaInstaller.new(Formula.factory(name))
+    outdated.each do |f|
+      installer = FormulaInstaller.new f
       installer.show_header = false
-      oh1 "Upgrading #{name}"
+      oh1 "Upgrading #{f.name}"
       installer.install
-      Keg.new("#{rack}/#{version}").unlink
+      Keg.new("#{f.rack}/#{f.version}").unlink
       installer.caveats
       installer.finish # includes link step
     end
