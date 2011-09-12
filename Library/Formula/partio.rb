bd1c00fff38f221b81dd807d8286d5abb09b5107
Shuen Huei Guan <drake.guan@gmail.com>
2011-09-12 19:13:26 +0800

    Add Partio - A library for particle IO and manipulation.

---
+++ Library/Formula/partio.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Partio < Formula
+  head 'https://github.com/wdas/partio.git'
+  homepage 'https://github.com/wdas/partio'
+
+  # depends_on 'cmake'
+  depends_on 'scons' => :build
+  depends_on 'doxygen' => :build
+
+  def install
+      system "scons", "-j", "#{Hardware.processor_count}", 
+                      "--prefix", "#{prefix}"
+
+      bin.install Dir['bin/*']
+      include.install Dir['include/*']
+      lib.install Dir['lib/*']
+  end
+end
