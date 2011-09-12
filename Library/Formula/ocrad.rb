177d37ff3235f1e4904d087dacf86b1a45e19e42
Jack Nagel <jacknagel@gmail.com>
2011-09-12 11:31:30 -0500

    Use automatic mirror selection for GNU downloads
    It seems that the main GNU download site has issues in some places
    outside the U.S., so we'll use the provided "ftpmirror.gnu.org" to pick
    a nearby mirror.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ocrad.rb
+++ Library/Formula/ocrad.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Ocrad < Formula
-  url 'http://ftp.gnu.org/gnu/ocrad/ocrad-0.20.tar.gz'
+  url 'http://ftpmirror.gnu.org/ocrad/ocrad-0.20.tar.gz'
   homepage 'http://www.gnu.org/software/ocrad/'
   md5 '47040630580dbc75ce16f4a4fabede3f'
 
