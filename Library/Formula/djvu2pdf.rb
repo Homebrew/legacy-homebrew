82f6baded7dcabcfb2cecb5a4e9484df68898880
Parker Kuivila <ParkerKuivila@gmail.com>
2011-09-12 13:19:52 -0400

    Added formula: djvu2pdf

--- Library/Formula/djvu2pdf.rb
+++ Library/Formula/djvu2pdf.rb
@@ -1,20 +1,21 @@
 require 'formula'
 
 class Djvu2pdf < Formula
-  url 'http://0x2a.at/site/projects/djvu2pdf/djvu2pdf-0.9.1.tar.gz'
+  url 'http://0x2a.at/site/projects/djvu2pdf/djvu2pdf-0.9.2.tar.gz'
   homepage 'http://0x2a.at/s/projects/djvu2pdf'
-  md5 '47a77ec1693c352dda1cd7ced3f50c1c'
+  md5 'af93c29a857a014f86dffcff6c773ef1'
 
   depends_on 'ghostscript'
   depends_on 'djvulibre'
 
   def install
-    system "chmod +x djvu2pdf && mv djvu2pdf /usr/local/bin"
-    #system "mv djvu2pdf.1.gz #{prefix}/share/man"
+    system "chmod +x djvu2pdf"
+    bin.install 'djvu2pdf'
+    man1.install 'djvu2pdf.1.gz'
   end
 
   def test
     # this will fail we won't accept that, make it test the program works!
-    system "/usr/bin/false"
+    system "#{bin}/djvu2pdf -h"
   end
 end
