205da2ca7c3c0b74658b48d098e86b01b753c09d
Matt Cegielka <matt@mudbrick.org>
2011-09-13 18:39:18 +0100

    Update imapsync Formula: New homepage and explicit Perl module dependencies

--- Library/Formula/imapsync.rb
+++ Library/Formula/imapsync.rb
@@ -2,8 +2,11 @@ require 'formula'
 
 class Imapsync < Formula
   url 'https://fedorahosted.org/released/imapsync/imapsync-1.456.tgz'
-  homepage 'http://freshmeat.net/projects/imapsync/'
+  homepage 'http://ks.lamiral.info/imapsync/'
   md5 'e9ea9ab5eba11cfe1c62ae9be1d9d7ae'
+
+  depends_on 'Mail::IMAPClient' => :perl
+  depends_on 'Authen::NTLM'     => :perl
 
   def install
     system 'perl', '-c', 'imapsync'
@@ -11,12 +14,5 @@ class Imapsync < Formula
     bin.install 'imapsync'
     man1.install 'imapsync.1'
   end
-
-  def caveats; <<-EOS.undent
-      imapsyny requires the Mail::IMAPClient and Authen::NTLM modules from CPAN:
-          cpan -i Mail::IMAPClient Authen::NTLM
-    EOS
-  end
-
 
 end
