a15d024ec2ba37400a88ed77deb98a94b8f9a811
Willem Deconinck <ir.willem.deconinck@gmail.com>
2011-09-13 08:22:44 +0200

    zoltan: fixes to formula

--- Library/Formula/zoltan.rb
+++ Library/Formula/zoltan.rb
@@ -9,35 +9,27 @@ class Zoltan < Formula
   def options
     [
       ["--with-scotch", "Enable PT-Scotch support"],
-
-      # Zoltan does not support parmetis version 4 yet.
-      # ["--with-parmetis", "Enable Parmetis support"],
     ]
   end
 
   depends_on 'cmake' => :build
   depends_on 'scotch'   if ARGV.include? '--with-scotch'
 
-  # Zoltan does not support parmetis version 4 yet.
-  # depends_on 'parmetis' if ARGV.include? '--with-parmetis'
-
   def install
 
     mkdir 'zoltan-build'
 
-    args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}",
-            "-DCMAKE_BUILD_TYPE=Release",
-            "-DTPL_ENABLE_MPI:BOOL=ON",
-            "-DTrilinos_ENABLE_Fortran:BOOL=OFF",
-            "-DTrilinos_ENABLE_EXAMPLES:BOOL=OFF",
-            "-DTrilinos_VERBOSE_CONFIGURE:BOOL=OFF",
-            "-DTrilinos_ENABLE_ALL_PACKAGES:BOOL=OFF",
-            "-DTrilinos_ENABLE_Zoltan:BOOL=ON",
-            "-DZoltan_ENABLE_EXAMPLES:BOOL=OFF",
-            "-DZoltan_ENABLE_TESTS:BOOL=OFF"
-            ]
-
-    args << "-DZoltan_ENABLE_ParMETIS:BOOL=ON" if ARGV.include? '--with-parmetis'
+    args = std_cmake_parameters.split
+    args.concat [
+      "-DTPL_ENABLE_MPI:BOOL=ON",   
+      "-DTrilinos_ENABLE_Fortran:BOOL=OFF", 
+      "-DTrilinos_ENABLE_EXAMPLES:BOOL=OFF",
+      "-DTrilinos_VERBOSE_CONFIGURE:BOOL=OFF",  
+      "-DTrilinos_ENABLE_ALL_PACKAGES:BOOL=OFF",
+      "-DTrilinos_ENABLE_Zoltan:BOOL=ON",   
+      "-DZoltan_ENABLE_EXAMPLES:BOOL=OFF",
+      "-DZoltan_ENABLE_TESTS:BOOL=OFF"
+    ]
     args << "-DZoltan_ENABLE_Scotch:BOOL=ON"   if ARGV.include? '--with-scotch'
 
     Dir.chdir 'zoltan-build' do
