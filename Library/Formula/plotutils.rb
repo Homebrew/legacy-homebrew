d92803b20dfca90b2cb63fca084ae8948c24874f
Jack Nagel <jacknagel@gmail.com>
2011-09-13 23:39:29 -0500

    plotutils: fix audit warning
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/plotutils.rb
+++ Library/Formula/plotutils.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Plotutils < Formula
-  url 'ftp://mirrors.kernel.org/gnu/plotutils/plotutils-2.6.tar.gz'
+  url 'http://ftpmirror.gnu.org/plotutils/plotutils-2.6.tar.gz'
   homepage 'http://www.gnu.org/software/plotutils/'
   md5 'c08a424bd2438c80a786a7f4b5bb6a40'
 
