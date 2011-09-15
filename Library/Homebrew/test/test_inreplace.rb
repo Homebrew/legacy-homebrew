0cf92eaa6a71ef335e43e56d8d150a8f090ed3ce
Charlie Sharpsteen <source@sharpsteen.net>
2011-09-15 14:12:33 -0700

    brew tests: Leading whitespace tests for inreplace

--- Library/Homebrew/test/test_inreplace.rb
+++ Library/Homebrew/test/test_inreplace.rb
@@ -43,9 +43,29 @@ class InreplaceTest < Test::Unit::TestCase
     assert_equal "OTHER=def\nFLAG=def\nFLAG2=abc", s1
   end
 
+  def test_change_make_var_leading_space
+    # Sometimes variables have leading whitespace --- this should be preserved,
+    # especially in the case of tabs which are significant to make.
+    s1="OTHER=def\n  FLAG = abc\n\tFLAG = abc\nFLAG2=abc"
+    s1 = s1.lines.map do |l|
+      l.extend(StringInreplaceExtension)
+      l.change_make_var! "FLAG", "def"
+      return l
+    end
+    assert_equal "OTHER=def\n  FLAG=def\n\tFLAG=def\nFLAG2=abc", s1.join
+  end
+
   def test_remove_make_var
     # Replace flag
     s1="OTHER=def\nFLAG = abc\nFLAG2 = def"
+    s1.extend(StringInreplaceExtension)
+    s1.remove_make_var! "FLAG"
+    assert_equal "OTHER=def\nFLAG2 = def", s1
+  end
+
+  def test_remove_make_var_leading_space
+    # Sometimes variables have leading whitespace.
+    s1="OTHER=def\n  FLAG = abc\n\tFLAG = abc\nFLAG2 = def"
     s1.extend(StringInreplaceExtension)
     s1.remove_make_var! "FLAG"
     assert_equal "OTHER=def\nFLAG2 = def", s1
@@ -57,5 +77,12 @@ class InreplaceTest < Test::Unit::TestCase
     s1.extend(StringInreplaceExtension)
     s1.remove_make_var! ["FLAG", "FLAG2"]
     assert_equal "OTHER=def\nOTHER2=def", s1
+  end
+
+  def test_get_make_var_leading_space
+    # Get value of FLAG.
+    s1="OTHER=def\n  FLAG = abc\nFLAG2 = def"
+    s1.extend(StringInreplaceExtension)
+    assert_equal 'abc', s1.get_make_var('FLAG')
   end
 end
