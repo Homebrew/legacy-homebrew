600b2d9728e27bc0809d77ee808833a2d0c02274
Misty De Meo <mistydemeo@gmail.com>
2011-09-15 22:20:10 -0500

    Migrate versioned tarballs to '--devel'
    Formulae with development branches that have a version number and a
    stable md5 now use a `url` statement, not `head`, and are installed
    using `--devel` instead of `--HEAD`. This lets them take advantage of
    version numbering for `brew upgrade`.

--- Library/Formula/fltk.rb
+++ Library/Formula/fltk.rb
@@ -1,20 +1,21 @@
 require 'formula'
 
 class Fltk < Formula
-  url 'http://ftp2.easysw.com/pub/fltk/1.3.0/fltk-1.3.0-source.tar.gz'
   homepage 'http://www.fltk.org/'
-  head 'http://ftp.easysw.com/pub/fltk/snapshots/fltk-1.3.x-r9013.tar.bz2'
-  if ARGV.build_head?
-    md5 '9c5eb9eb8642be56cb68e7c5b1c98611'
+
+  unless ARGV.flag? '--devel'
+  url 'http://ftp2.easysw.com/pub/fltk/1.3.0/fltk-1.3.0-source.tar.gz'
+  md5 '44d5d7ba06afdd36ea17da6b4b703ca3'
   else
-    md5 '44d5d7ba06afdd36ea17da6b4b703ca3'
+  url 'http://ftp.easysw.com/pub/fltk/snapshots/fltk-1.3.x-r9013.tar.bz2'
+  md5 '9c5eb9eb8642be56cb68e7c5b1c98611'
   end
 
   depends_on 'jpeg'
 
   def install
     ENV.libpng
-    system "autoconf" if ARGV.build_head?
+    system "autoconf" if ARGV.flag? '--devel'
 
     system "./configure", "--prefix=#{prefix}", "--enable-threads"
     system "make install"
