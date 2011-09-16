3c059d96814a9fc73ac38f185503e49423ca1241
Pete Deffendol <pete@mdsc.com>
2011-09-15 23:15:17 -0600

    MapServer: updated to 6.0.1
    Also removes a patch that is no longer required.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/mapserver.rb
+++ Library/Formula/mapserver.rb
@@ -1,23 +1,17 @@
 require 'formula'
 
 class Mapserver < Formula
-  url 'http://download.osgeo.org/mapserver/mapserver-6.0.0.tar.gz'
+  url 'http://download.osgeo.org/mapserver/mapserver-6.0.1.tar.gz'
   homepage 'http://mapserver.org/'
-  md5 '5bcb1a6fb4a743e9f069466fbdf4ab76'
+  md5 'b96287449dcbca9a2fcea3a64905915a'
 
   depends_on 'gd'
   depends_on 'proj'
   depends_on 'gdal'
-  depends_on 'libagg'
-
-  def patches
-    # http://trac.osgeo.org/mapserver/ticket/3877 (patch from the 6.0 release branch)
-    { :p4 => "http://trac.osgeo.org/mapserver/changeset/11714?format=diff&new=11714" }
-  end
 
   def install
     system "./configure", "--prefix=#{prefix}", "--with-png=/usr/X11",
-           "--with-proj", "--with-gdal", "--with-agg"
+           "--with-proj", "--with-gdal"
     system "make"
     bin.install "mapserv"
   end
