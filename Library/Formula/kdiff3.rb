8d1ef2d33b39d086a8431581db51c5a36311ded8
Bob Fanger <bfanger@gmail.com>
2011-09-17 18:51:45 +0200

    Updated version to 0.9.96

--- Library/Formula/kdiff3.rb
+++ Library/Formula/kdiff3.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Kdiff3 < Formula
-  url 'http://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.95/kdiff3-0.9.95.tar.gz'
+  url 'http://downloads.sourceforge.net/project/kdiff3/kdiff3/0.9.96/kdiff3-0.9.96.tar.gz'
   homepage 'http://kdiff3.sourceforge.net/'
-  md5 '652a98bf79ba762a8a646d4a0fddb323'
+  md5 '46b96befddf3448a3cb673f018c6f6f9'
 
   depends_on 'qt'
 
