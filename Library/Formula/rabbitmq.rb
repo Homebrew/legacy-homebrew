417019cd4ee4200dd879a4c62e448ad904d265f5
Ealden Esto E. Escanan <ealden@gmail.com>
2011-09-18 09:15:58 +0800

    RabbitMQ 2.6.1
    2.6.1 fixes a bug in 2.6.0 that causes the broker to fail to restart on
    reboot in certain operating systems.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rabbitmq < Formula
   homepage 'http://www.rabbitmq.com'
-  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.0/rabbitmq-server-2.6.0.tar.gz'
-  md5 'e4d9b6792b556f0c145cf02f1430f0b3'
+  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v2.6.1/rabbitmq-server-2.6.1.tar.gz'
+  md5 'c8b86f2e2a86502932b54e9ced2eb63c'
 
   depends_on 'erlang'
   depends_on 'simplejson' => :python if MacOS.leopard?
