1ad93847f7497641cd302a6065828476ccf41945
Charlie Sharpsteen <source@sharpsteen.net>
2011-09-19 08:51:54 -0700

    mirror support: Mirrors for kernel.org software
    - All Debian software is now mirrored by ftp.us.debian.org.
    
    - LibFFI is mirrored by ftp.sourceware.org.
    
    - All kernel.org software is now mirrored by mirrorservice.org.
    
    Fixes #7478.

--- Library/Formula/sl.rb
+++ Library/Formula/sl.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Sl < Formula
   url 'http://mirrors.kernel.org/debian/pool/main/s/sl/sl_3.03.orig.tar.gz'
+  mirror 'http://ftp.us.debian.org/debian/pool/main/s/sl/sl_3.03.orig.tar.gz'
   homepage 'http://packages.debian.org/source/stable/sl'
   md5 'd0d997b964bb3478f7f4968eee13c698'
 
