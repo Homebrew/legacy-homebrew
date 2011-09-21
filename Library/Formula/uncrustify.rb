cd6ad2757cd620222cd3fafdbb47142561d0a0e5
Adam Lickel <adam@lickel.com>
2011-09-21 11:00:15 -0700

    uncrustify 0.58
    Closes #7743.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/uncrustify.rb
+++ Library/Formula/uncrustify.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Uncrustify < Formula
-  url 'http://downloads.sourceforge.net/project/uncrustify/uncrustify/uncrustify-0.57/uncrustify-0.57.tar.gz'
+  url 'http://downloads.sourceforge.net/project/uncrustify/uncrustify/uncrustify-0.58/uncrustify-0.58.tar.gz'
   head 'https://github.com/bengardner/uncrustify.git'
   homepage 'http://uncrustify.sourceforge.net/'
-  md5 'df597bf0380d2a0039023719a9fdb6c5'
+  md5 '9fa275570a4422aa1cb90fbbdde79a76'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
