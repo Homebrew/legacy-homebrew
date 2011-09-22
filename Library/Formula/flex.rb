d14f44c3d7c108382298e69a5e7b5c7a91e168fc
Matt Fichman <matt.fichman@gmail.com>
2011-09-22 00:50:51 -0700

    Added GNU bison parser generator

--- Library/Formula/flex.rb
+++ Library/Formula/flex.rb
@@ -13,11 +13,6 @@ class Flex < Formula
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test flex`. Remove this comment before submitting
-    # your pull request!
-    system "false"
+    system "flex --version"
   end
 end
