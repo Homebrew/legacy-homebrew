07db3eda2e19ddbd8c04c350628eb07780917dec
lordB8r <jpbader@remachines.com>
2011-09-22 09:47:45 -0500

    edited formula for latest update 2011.09.22...first commit so not sure if i borked it or not.

--- Library/Formula/pianobar.rb
+++ Library/Formula/pianobar.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Pianobar < Formula
-  url 'https://github.com/PromyLOPh/pianobar/tarball/2011.01.24'
-  version '2011.01.24'
+  url 'https://github.com/PromyLOPh/pianobar/tarball/2011.09.22'
+  version '2011.09.22'
   homepage 'https://github.com/PromyLOPh/pianobar/'
   md5 '97ee5b39e5e9006f6139db2787f346a0'
 
