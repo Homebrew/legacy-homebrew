94c4104e50a95c111710ab7bc52cc2f7417db712
Bram Swenson <bram.swenson@akqa.com>
2011-09-23 19:39:21 -0700

    added node-unstable to track the unstable branch of node

---
+++ Library/Formula/node-unstable.rb
@@ -0,0 +1,37 @@
+require 'formula'
+
+class NodeUnstable < Formula
+  url 'http://nodejs.org/dist/v0.5.7/node-v0.5.7.tar.gz'
+  head 'https://github.com/joyent/node.git'
+  homepage 'http://nodejs.org/'
+  md5 'a4e09aa7d6ffec29729f2887b705b2fc'
+
+  # Leopard OpenSSL is not new enough, so use our keg-only one
+  depends_on 'openssl' if MacOS.leopard?
+
+  fails_with_llvm :build => 2326
+
+  # Stripping breaks dynamic loading
+  skip_clean :all
+
+  def options
+    [["--debug", "Build with debugger hooks."]]
+  end
+
+  def install
+    inreplace 'wscript' do |s|
+      s.gsub! '/usr/local', HOMEBREW_PREFIX
+      s.gsub! '/opt/local/lib', '/usr/lib'
+    end
+
+    args = ["--prefix=#{prefix}"]
+    args << "--debug" if ARGV.include? '--debug'
+
+    system "./configure", *args
+    system "make install"
+  end
+
+  def caveats
+    "Please add #{HOMEBREW_PREFIX}/lib/node_modules to your NODE_PATH environment variable to have node libraries picked up."
+  end
+end
