5a959cd619a56149526c6a8ba53bfb065e2c49dd
Carl Hörberg <carl.hoerberg@gmail.com>
2011-09-28 13:28:08 +0300

    Upgraded Postgres to 9.1.1

--- Library/Formula/postgresql.rb
+++ Library/Formula/postgresql.rb
@@ -3,8 +3,8 @@ require 'hardware'
 
 class Postgresql < Formula
   homepage 'http://www.postgresql.org/'
-  url 'http://ftp9.us.postgresql.org/pub/mirrors/postgresql/source/v9.0.4/postgresql-9.0.4.tar.bz2'
-  md5 '80390514d568a7af5ab61db1cda27e29'
+  url 'http://ftp9.us.postgresql.org/pub/mirrors/postgresql/source/v9.1.1/postgresql-9.1.1.tar.bz2'
+  md5 '061a9f17323117c9358ed60f33ecff78'
 
   depends_on 'readline'
   depends_on 'libxml2' if MacOS.leopard? # Leopard libxml is too old
