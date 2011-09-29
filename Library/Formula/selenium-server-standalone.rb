b9ae536c4c2084c1dab0630f60dcf1f2389fc9ab
Paul Kmiec <paul.kmiec@appfolio.com>
2011-09-29 10:37:20 -0700

    upgrade to selenium 2.7.0

--- Library/Formula/selenium-server-standalone.rb
+++ Library/Formula/selenium-server-standalone.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class SeleniumServerStandalone < Formula
-  url 'http://selenium.googlecode.com/files/selenium-server-standalone-2.5.0.jar'
+  url 'http://selenium.googlecode.com/files/selenium-server-standalone-2.7.0.jar'
   homepage 'http://seleniumhq.org/'
-  md5 'c8aabf2fa0b26dec5134aa06eb54ac34'
+  md5 'a997d346758a31a4f42a79f42d94be1d'
 
   def install
-    prefix.install "selenium-server-standalone-2.5.0.jar"
+    prefix.install "selenium-server-standalone-2.7.0.jar"
     (prefix + "selenium-server-standalone.plist").write plist_file
   end
 
@@ -24,7 +24,7 @@ If this is an upgrade and you already have the selenium-server-standalone.plist 
   launchctl load -w ~/Library/LaunchAgents/selenium-server-standalone.plist
 
 Or start it manually with:
-  java -jar #{prefix}/selenium-server-standalone-2.5.0.jar -p 4444
+  java -jar #{prefix}/selenium-server-standalone-2.7.0.jar -p 4444
 EOS
   end
 
@@ -44,7 +44,7 @@ EOS
         <array>
                 <string>/usr/bin/java</string>
                 <string>-jar</string>
-                <string>#{prefix}/selenium-server-standalone-2.5.0.jar</string>
+                <string>#{prefix}/selenium-server-standalone-2.7.0.jar</string>
                 <string>-port</string>
                 <string>4444</string>
         </array>
