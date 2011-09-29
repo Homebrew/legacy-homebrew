366ac38d507529386d7d5b0b01ffa41b56cd195f
Chris Van Horne <chris.vanhorne@gmail.com>
2011-09-28 17:02:04 -0700

    pari 2.5.0

--- Library/Formula/pari.rb
+++ Library/Formula/pari.rb
@@ -1,12 +1,39 @@
 require 'formula'
 
+def use_gmp?
+  ARGV.include? '--with-gmp'
+end
+
+def use_readline?
+  ARGV.include? '--with-readline'
+end
+
 class Pari < Formula
-  url 'http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.3.5.tar.gz'
+  url 'http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.5.0.tar.gz'
   homepage 'http://pari.math.u-bordeaux.fr/'
-  md5 '6077c6db56fdd32e39a06a9bf320e1f7'
+  md5 '0b595a1345679ff482785a686c863e9f'
+
+  depends_on 'readline' if use_readline?
+  depends_on 'gmp' if use_gmp?
+
+  def options
+    [
+      ['--with-readline', 'Build with GNU readline support.'],
+      ['--with-gmp', 'Build wth GNU MP support.']
+    ]
+  end
 
   def install
-    system "./Configure", "--prefix=#{prefix}"
+    ENV.x11
+    readline = Formula.factory 'readline'
+    gmp = Formula.factory 'gmp'
+
+    args = ["--prefix=#{prefix}"]
+    args << "--with-readline=#{readline.prefix}" if use_readline?
+    args << "--with-gmp=#{gmp.prefix}" if use_gmp?
+
+    system "./Configure", *args
+    system "make gp"
     system "make install"
   end
 end
