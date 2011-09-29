9b36a142b3d7c00fcaed6d6e600993835f65104a
Robert Shaw <robert914@me.com>
2011-09-29 16:11:36 -0700

    libebml 1.2.2
    Closes #7900.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libebml.rb
+++ Library/Formula/libebml.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
 class Libebml < Formula
-  # This is the official source, but it's frequently down. Use the mktoolnix mirror instead.
-  # url 'http://dl.matroska.org/downloads/libebml/libebml-1.2.1.tar.bz2'
-  url 'http://www.bunkus.org/videotools/mkvtoolnix/sources/libebml-1.2.1.tar.bz2'
+  url 'http://dl.matroska.org/downloads/libebml/libebml-1.2.2.tar.bz2'
+  mirror 'http://www.bunkus.org/videotools/mkvtoolnix/sources/libebml-1.2.2.tar.bz2'
   homepage 'http://www.matroska.org/'
-  md5 '13c5a10e260e703d3a7f003fdc995183'
+  md5 '726cc2bd1a525929ff35ff9854c0ebab'
 
   def install
     system 'cp -r make/linux make/darwin'
