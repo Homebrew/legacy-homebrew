2b5e49726682b9f8891421122549e87555b5edbf
Nihad Abbasov <narkoz.2008@gmail.com>
2011-09-30 11:20:33 +0600

    bump lsof to 4.85

--- Library/Formula/lsof.rb
+++ Library/Formula/lsof.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
 class Lsof < Formula
-  url 'ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.84.tar.bz2'
-  md5 'a09326df500ef7e4550af546868338d6'
+  url 'ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.85.tar.bz2'
+  md5 '102ee2081172bbe76dccaa6cceda8573'
   homepage 'http://people.freebsd.org/~abe/'
 
   def install
-    system "tar xf lsof_4.84_src.tar"
-    Dir.chdir "lsof_4.84_src" do
+    system "tar xf lsof_4.85_src.tar"
+    Dir.chdir "lsof_4.85_src" do
       mv "00README", "../README"
       system "./Configure -n darwin"
       system "make"
