5007ea6788f81f1aa6559eb357888cefd5c0ec7b
Artur Adib <arturadib@gmail.com>
2011-09-29 21:02:56 -0300

    Edited Library/Formula/closure_linter.rb via GitHub

--- Library/Formula/closure_linter.rb
+++ Library/Formula/closure_linter.rb
@@ -2,8 +2,10 @@ require 'formula'
 
 class ClosureLinter < Formula
   url 'http://closure-linter.googlecode.com/files/closure_linter-2.3.2b.tar.gz'
-  homepage 'http://code.google.com/closure/utilities/docs/linter_howto.html'
+  homepage 'http://code.google.com/closure/utilities/'
   md5 '432d27bad92067f3a0a63f898982508d'
+
+  depends_on 'python'
 
   def install
     system "python", "setup.py", "install",
