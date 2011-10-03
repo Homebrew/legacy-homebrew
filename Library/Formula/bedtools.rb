8a90f65b15953e8267dba15a555ab78dac0feea6
Carlos Borroto <carlos.borroto@gmail.com>
2011-10-03 16:10:10 -0400

    bedtools 2.13.3

--- Library/Formula/bedtools.rb
+++ Library/Formula/bedtools.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Bedtools < Formula
-  url 'http://bedtools.googlecode.com/files/BEDTools.v2.13.2.tar.gz'
+  url 'http://bedtools.googlecode.com/files/BEDTools.v2.13.3.tar.gz'
   homepage 'http://code.google.com/p/bedtools/'
-  md5 'cd3671eda69da312237523beadefbd52'
+  md5 '4119b0e84d49b0a7425070c82b4d1920'
   head 'https://github.com/arq5x/bedtools.git'
 
   def install
