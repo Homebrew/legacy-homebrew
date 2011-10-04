4e8441f428830d17b68dfa7e4f73585b2cba168b
Rodrigo Kochenburger <divoxx@gmail.com>
2011-10-04 17:58:57 -0300

    Updating rbenv and ruby-build formulas to use latest versions

--- Library/Formula/rbenv.rb
+++ Library/Formula/rbenv.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Rbenv < Formula
-  url 'https://github.com/sstephenson/rbenv/tarball/v0.2.1'
+  url 'https://github.com/sstephenson/rbenv/tarball/v0.2.0'
   homepage 'https://github.com/sstephenson/rbenv'
-  md5 '4b2ca757c7dcc6384a49d8947b97c4ed'
+  md5 'c26a72aa07ecf6cf51f1517f2c70a9dd'
 
   head 'https://github.com/sstephenson/rbenv.git'
 
--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -4,11 +4,9 @@ class RubyBuild < Formula
   url 'https://github.com/sstephenson/ruby-build/tarball/v20110928'
   homepage 'https://github.com/sstephenson/ruby-build'
   md5 '2f3cd506d0bb7a7e2cb6bbb36c81fdca'
-
-  head 'https://github.com/sstephenson/ruby-build.git'
+  version 'v20110928'
 
   def install
-    ENV['PREFIX'] = prefix
-    system "./install.sh"
+    system "PREFIX=\"#{prefix}\" ./install.sh"
   end
 end
