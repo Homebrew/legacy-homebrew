0cc34ccac4995be11c91836304767c9fa4d34294
Nibbles 2bits <nibbles@speakeasy.net>
2011-10-05 19:46:42 -0700

    fluidsynth: Update to 1.1.5
    This updates fluidsynth to 1.1.5 and adds a test function that
    outputs the version information. The --enable-double flag turns
    on double precision DSP rather than floats.  I figure we can spare
    the horses.  This software compiles without error on x86_64 10.6.8
    using llvm-2335.9, gcc-4.2.1, and clang-2.0 from XCode-4.0.2.
    There are no make checks. It creates native static and shared.

--- Library/Formula/fluid-synth.rb
+++ Library/Formula/fluid-synth.rb
@@ -1,16 +1,22 @@
 require 'formula'
 
 class FluidSynth < Formula
-  url 'http://sourceforge.net/projects/fluidsynth/files/fluidsynth-1.1.3/fluidsynth-1.1.3.tar.gz'
+  url 'http://sourceforge.net/projects/fluidsynth/files/fluidsynth-1.1.5/fluidsynth-1.1.5.tar.gz'
   homepage 'http://www.fluidsynth.org/'
-  md5 '0d3e3cc770b4da413010dfb7dfdce9c8'
+  sha1 '2f98696ca0a6757684f0a881bf92b3149536fdf2'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'libsndfile' => :optional
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "prefix=#{prefix}", "--enable-double"
     system "make install"
+  end
+
+  def test
+    system "#{HOMEBREW_PREFIX}/bin/fluidsynth -V"
+    oh1 "The test was successful"
   end
 end
