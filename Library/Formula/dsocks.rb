8428da7adb6cccf576851e9b1355d4ca3a659cd8
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-10-07 08:45:08 +0200

    Fix the dsocks formula to use ENV.cc and ENV.cflags

--- Library/Formula/dsocks.rb
+++ Library/Formula/dsocks.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Dsocks < Formula
-  url 'http://monkey.org/~dugsong/dsocks/dsocks-1.7.tar.gz'
+  url 'http://dsocks.googlecode.com/files/dsocks-1.7.tar.gz'
   homepage 'http://monkey.org/~dugsong/dsocks/'
   md5 'b2c87206e3f526bdab8e990a0168ab6c'
 
   def install
-    system "llvm-gcc", "-O3", "-shared", "-o", "libdsocks.dylib", "dsocks.c", "atomicio.c", "-lresolv"
+    system "#{ENV.cc} #{ENV.cflags} -shared -o libdsocks.dylib dsocks.c atomicio.c -lresolv"
     inreplace "dsocks.sh", "/usr/local", HOMEBREW_PREFIX
 
     lib.install "libdsocks.dylib"
