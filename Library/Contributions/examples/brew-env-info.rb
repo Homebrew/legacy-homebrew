b45958f39436f9c21f2a6b89b3ac7a035a25cde4
John Harrison <ash@greaterthaninfinity.com>
2011-10-07 23:35:19 -0500

    Adding a command to generate the env information for linking aginst a library. This is useful for `keg_only` formula and for generating the correct -I and -L information for any formula including its depndencies.
    Signed-off-by: William Woodall <wjwwood@gmail.com>

---
+++ Library/Contributions/examples/brew-env-info.rb
@@ -0,0 +1,47 @@
+require 'formula'
+require 'extend/ENV'
+require 'hardware'
+
+module Homebrew extend self
+  def env_info
+    ENV.extend(HomebrewEnvExtension)
+    ENV.setup_build_environment
+    ARGV.formulae.each do |f|
+      if f.keg_only?
+        ENV.prepend 'LDFLAGS', "-L#{f.lib}"
+        ENV.prepend 'CPPFLAGS', "-I#{f.include}"
+        ENV.prepend 'PATH', "#{f.bin}", ':'
+        ENV.prepend 'PKG_CONFIG_PATH', f.lib+'pkgconfig', ':'
+      end
+      f.recursive_deps.uniq.each do |dep|
+        dep = Formula.factory dep
+        if dep.keg_only?
+          ENV.prepend 'LDFLAGS', "-L#{dep.lib}"
+          ENV.prepend 'CPPFLAGS', "-I#{dep.include}"
+          ENV.prepend 'PATH', "#{dep.bin}", ':'
+          ENV.prepend 'PKG_CONFIG_PATH', dep.lib+'pkgconfig', ':'
+        end
+      end
+    end
+    %w[ CC CXX LD ].each do |k|
+      value = ENV[k]
+      if value
+        results = value
+        if File.exists? value and File.symlink? value
+          target = Pathname.new(value)
+          results = target.realpath
+        end
+        puts "#{k}=#{results}"
+      end
+    end
+
+    %w[ CFLAGS CXXFLAGS CPPFLAGS LDFLAGS MACOSX_DEPLOYMENT_TARGET MAKEFLAGS PKG_CONFIG_PATH
+        HOMEBREW_DEBUG HOMEBREW_VERBOSE HOMEBREW_USE_CLANG HOMEBREW_USE_GCC HOMEBREW_USE_LLVM
+        HOMEBREW_SVN ].each do |k|
+      value = ENV[k]
+      puts "#{k}=#{value}" if value
+    end
+  end
+end
+
+Homebrew.env_info
