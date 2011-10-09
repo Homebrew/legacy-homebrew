5a6f8158c4d9952aadf1b6da2151d10f87a96b79
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2011-10-09 14:36:15 +0200

    libass: update to 0.10.0
    Signed-off-by: Oleg Oshmyan <chortos@inbox.lv>

--- Library/Formula/libass.rb
+++ Library/Formula/libass.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Libass < Formula
-  url 'http://libass.googlecode.com/files/libass-0.9.13.tar.gz'
+  url 'http://libass.googlecode.com/files/libass-0.10.0.tar.gz'
   homepage 'http://code.google.com/p/libass/'
-  md5 '006f48a9831c00914f05f830592f532c'
+  sha1 '82bfda2b78f74cf75a4dd0283d090ad1a71a697f'
 
   depends_on 'pkg-config' => :build
+  depends_on 'fribidi'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
