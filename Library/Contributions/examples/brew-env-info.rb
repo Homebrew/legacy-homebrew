d394e378896bcd08004fcc3f374b0bd11dec7ce8
John Harrison <ash@greaterthaninfinity.com>
2011-10-08 21:31:29 -0500

    Adding in a --help command
    Signed-off-by: William Woodall <wjwwood@gmail.com>

--- Library/Contributions/examples/brew-env-info.rb
+++ Library/Contributions/examples/brew-env-info.rb
@@ -4,6 +4,41 @@ require 'hardware'
 
 module Homebrew extend self
   def env_info
+    if ARGV.include? "--help"
+      puts <<-EOS
+brew env-info
+
+NAME
+  brew env-info - for printing enviorment flags of a given formula.
+
+SYNPOSIS
+  brew env-info FORMULA...            Prints the enviorment variables
+                                      appripriate for building and linking
+                                      against all of the formula you pass it.
+
+DESCRIPTION
+  brew env-info is a tool for generating build flags for packages managed by
+  brew. This command will print any settings needed to build against the
+  formula you pass the command.
+
+EXAMPLES
+  Here is an example of how to use brew env-info.
+
+    brew env-info libiconv cairo       Prints the enviorment variables
+                                       appripriate for building against
+                                       libiconv and cairo.
+
+  If your project uses a Makefile for example you could use the following:
+
+    brew env-info libiconv cairo | xargs env make
+
+  Most importantly, this will export CPPFLAGS and LDFLAGS with the correct
+  paths for building against both libiconv and cairo, even if they are in
+  kegs outside of the normal path structure.
+EOS
+      return
+    end
+
     ENV.extend(HomebrewEnvExtension)
     ENV.setup_build_environment
     ARGV.formulae.each do |f|
@@ -35,9 +70,9 @@ module Homebrew extend self
       end
     end
 
-    %w[ CFLAGS CXXFLAGS CPPFLAGS LDFLAGS MACOSX_DEPLOYMENT_TARGET MAKEFLAGS PKG_CONFIG_PATH
-        HOMEBREW_DEBUG HOMEBREW_VERBOSE HOMEBREW_USE_CLANG HOMEBREW_USE_GCC HOMEBREW_USE_LLVM
-        HOMEBREW_SVN ].each do |k|
+    %w[ CFLAGS CXXFLAGS CPPFLAGS LDFLAGS MACOSX_DEPLOYMENT_TARGET MAKEFLAGS
+        PKG_CONFIG_PATH HOMEBREW_DEBUG HOMEBREW_VERBOSE HOMEBREW_USE_CLANG
+        HOMEBREW_USE_GCC HOMEBREW_USE_LLVM HOMEBREW_SVN PATH ].each do |k|
       value = ENV[k]
       puts "#{k}=\"#{value}\"" if value
     end
