d5a04c9306899f92ad8665970bfa13053b0dfd84
Paul Ingles <paul@oobaloo.co.uk>
2011-10-13 20:18:40 +0100

    Add missing homepage

--- Library/Formula/msgpack-rpc.rb
+++ Library/Formula/msgpack-rpc.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class MsgpackRpc < Formula
   url 'http://msgpack.org/releases/cpp/msgpack-rpc-0.3.1.tar.gz'
-  homepage ''
+  homepage 'https://github.com/msgpack/msgpack-rpc'
   md5 'e5e8f3631ac83d20cdf65fa0d7bfee21'
   
   depends_on 'mpio'
