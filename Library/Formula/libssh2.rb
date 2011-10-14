ba338261136a607b13355f569e0c85e4136ad860
Gian Basagre <gcbasagre@gmail.com>
2011-10-15 01:04:55 +1300

    libssh2: Update to 1.3.0
    Closes #8116.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/libssh2.rb
+++ Library/Formula/libssh2.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Libssh2 < Formula
-  url 'http://www.libssh2.org/download/libssh2-1.2.8.tar.gz'
+  url 'http://www.libssh2.org/download/libssh2-1.3.0.tar.gz'
   homepage 'http://www.libssh2.org/'
   md5 '1b236563b75d1b1f0d483352dc5918cf'
 
