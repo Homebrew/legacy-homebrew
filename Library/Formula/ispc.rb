81fcfa9815d81c8441df6b5ad27242c3765dd160
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-10-14 11:46:46 -0400

    ispc 1.2.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/ispc.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Ispc < Formula
+  homepage 'http://ispc.github.com'
+  url 'https://github.com/downloads/ispc/ispc/ispc-v1.2.2-osx.tar.gz'
+  version '1.2.2'
+  md5 '21f1fe1aabe47ae280e0dc92d50d564f'
+
+  def install
+    bin.install 'ispc'
+  end
+
+  def test
+    system "#{bin}/ispc -v"
+  end
+end
