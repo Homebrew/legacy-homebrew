2faef3685d6179fd833c530f5119d0563fe28541
px <px@ns1.net>
2011-10-14 23:12:26 -0700

    almost ready

--- Library/Formula/python-progressbar.rb
+++ Library/Formula/python-progressbar.rb
@@ -11,11 +11,6 @@ class PythonProgressbar < Formula
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test progressbar`. Remove this comment before submitting
-    # your pull request!
-    system "false"
+  system "true"
   end
 end
