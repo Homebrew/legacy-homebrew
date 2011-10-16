742443c2b4a3f1435576e5b86ccc3b5a6cf7bd69
Marc <xerokill@gmail.com>
2011-10-16 14:01:13 +0800

    Updated formula: jsvc 1.0.7 (tested on a fresh install of OSX Lion, as well as OSX 10.7.2)

--- Library/Formula/jsvc.rb
+++ Library/Formula/jsvc.rb
@@ -1,12 +1,26 @@
 require 'formula'
 
 class Jsvc < Formula
-  version '1.0.5'
+  version '1.0.7'
   homepage 'http://commons.apache.org/daemon/jsvc.html'
-  url "http://archive.apache.org/dist/commons/daemon/binaries/#{version}/darwin/commons-daemon-#{version}-bin-darwin-universal.tar.gz"
-  md5 '1d5f4b81e5ac18da0d58af421bc3c139'
+  url 'http://apache.oss.eznetsols.org//commons/daemon/source/commons-daemon-1.0.7-native-src.tar.gz'
+  md5 '044e996449b6b59a8cd5da87575a0007'
+
+  def patches
+    # updated sample startup scripts based on OSX note from http://commons.apache.org/daemon/jsvc.html:
+    # "Use -jvm server because default client JVM is not present for all architectures."
+    'https://raw.github.com/gist/1290534/samples.patch'
+  end
 
   def install
+    ENV.append "CFLAGS", "-m64"
+    ENV.append "LDFLAGS", "-m64"
+
+    prefix.install %w{ NOTICE.txt LICENSE.txt RELEASE-NOTES.txt unix/samples }
+
+    cd 'unix'
+    system './configure'
+    system 'make'
     bin.install 'jsvc'
   end
 end
