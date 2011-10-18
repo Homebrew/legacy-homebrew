ba80ae285031b87c2324cb994c6974b2a75bb66b
Lukas Sabota <LTsmooth42@gmail.com>
2011-10-17 20:08:46 -0400

    added Formula for fceux 2.1.5

---
+++ Library/Formula/fceux.rb
@@ -0,0 +1,49 @@
+require 'formula'
+
+class Fceux < Formula
+  url 'http://downloads.sourceforge.net/fceultra/fceux-2.1.5.src.tar.bz2'
+  homepage 'http://fceux.com'
+  md5 'e8b20e62bbbb061b1a59d51b47c827bd'
+  
+  depends_on 'scons'
+  depends_on 'sdl'
+  depends_on 'libzip'
+  depends_on 'gtk+' unless ARGV.include? "--no-gtk"
+
+  # broken right now
+  def options
+    [
+      ['--no-gtk', "Don't build with Gtk+ support"]
+    ]
+  end
+  
+  def patches
+    # fixes compilation errors on osx -- patch data at tail
+    DATA
+  end
+
+  def install
+    if ARGV.include? "--no-gtk"
+      system "sed \"s/\\(GTK.\\+ \\)1/\\10/g\" SConstruct > SConstruct"
+    end
+    system "cat SConstruct"
+    system "scons"
+    system "mkdir -p #{prefix}/bin"
+    system "install -m755 ./src/fceux #{prefix}/bin/"
+  end
+end
+
+### patch data
+__END__
+t a/src/drivers/sdl/SConscript b/src/drivers/sdl/SConscript
+index 9c7247c..7a9f297 100644
+--- a/src/drivers/sdl/SConscript
++++ b/src/drivers/sdl/SConscript
+@@ -1,3 +1,6 @@
++Import('env')
++env.ParseConfig('pkg-config --cflags --libs x11')
++Export('env')
+ my_list =  Split("""
+ input.cpp
+ config.cpp
+# config.cpp
