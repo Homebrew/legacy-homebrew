c2e14515593135817b86b95577c3c9b2e23902e5
William R. Otte <wotte@dre.vanderbilt.edu>
2011-10-19 19:43:56 -0400

    Integrated suggestion from jacknagel for installing to java dir

--- Library/Formula/xalan-j.rb
+++ Library/Formula/xalan-j.rb
@@ -5,14 +5,12 @@ class XalanJ < Formula
   homepage 'http://xml.apache.org/xalan-j'
   md5 'fc805051f0fe505c7a4b1b5c8db9b9e3'
 
-  def java; prefix+'share/java' end
-
   depends_on 'xerces-j'
 
   def install
     system "/usr/bin/ant all docs javadocs"
 
-    java.install Dir['build/*.jar']
+    (share+'java').install Dir['build/*.jar']
     doc.install Dir['build/docs/*']
   end
 end
