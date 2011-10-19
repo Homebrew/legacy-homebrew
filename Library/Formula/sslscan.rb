d6762db317341935007928b4cefde0bc3585d8bf
Greg Turnquist <greg.turnquist@springsource.com>
2011-10-19 14:02:41 -0500

    sslscan: Create new formula to build sslscan and install and its man page.
    sslscan is a utility used to analyze security settings that pertain to SSL.
    The tarball is very simple and only requires a simple patch to the Makefile
    to include libcrypto. It comes with a man page that gets installed into man1.
    Installation checks that sslscan was correctly installed, verifying the
    entire build process.

--- Library/Formula/sslscan.rb
+++ Library/Formula/sslscan.rb
@@ -17,11 +17,6 @@ class Sslscan < Formula
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test sslscan`. Remove this comment before submitting
-    # your pull request!
     system "sslscan"
   end
 end
