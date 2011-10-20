dbb620658235031fed1d277b7396f97c0855ce62
Osman Üngür <osmanungur@gmail.com>
2011-10-20 13:09:52 +0300

    mongo-php 1.2.6
    Closes #8203.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/mongo-php.rb
+++ Library/Formula/mongo-php.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class MongoPhp < Formula
-  url 'http://pecl.php.net/get/mongo-1.2.4.tgz'
+  url 'http://pecl.php.net/get/mongo-1.2.6.tgz'
   homepage 'http://pecl.php.net/package/mongo'
-  md5 '307b9e5863873e49079910906470d950'
+  md5 'b471f3d9309c2caa52ea90122042d3f4'
 
   def install
     Dir.chdir "mongo-#{version}" do
