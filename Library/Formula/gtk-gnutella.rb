f0b08276fa4421adaee1b40c01065282306cb65a
Ismael Vilas Boas <ismael.vb@gmail.com>
2011-10-21 04:17:55 +0100

    New Formula: Frama-C
    Created a formula for frama-c (maybe need some changes to allow compile
    without GUI)
    updated dependency gtk-gnutella
    created dependency libgnomecanvas

--- Library/Formula/gtk-gnutella.rb
+++ Library/Formula/gtk-gnutella.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class GtkGnutella < Formula
-  url 'http://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/0.96.8/gtk-gnutella-0.96.8.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/0.97.1/gtk-gnutella-0.97.1.tar.bz2'
   homepage 'http://gtk-gnutella.sourceforge.net/en/?page=news'
   md5 'a9424951fec62cc9f19f2086dc5137b5'
 
