1bf0f672ef86dcadb031a9b9f62a866ee1fecfef
Adrian Petrescu <apetresc@gmail.com>
2011-10-21 22:38:32 -0400

    Using HTTP mirror for xboard sources.
    Addresses issues with mxcl/homebrew#8232.

--- Library/Formula/xboard.rb
+++ Library/Formula/xboard.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Xboard < Formula
-  url 'ftp://ftp.gnu.org/gnu/xboard/xboard-4.5.3.tar.gz'
+  url 'http://ftpmirror.gnu.org/xboard/xboard-4.5.3.tar.gz'
   homepage 'http://www.gnu.org/software/xboard/'
   md5 '48a623643fc6ad2b3d1963165bca76dc'
 
@@ -14,7 +14,6 @@ class Xboard < Formula
             "--x-lib=/usr/X11/lib",
             "--disable-zippy"]
 
-#   system "./autogen.sh"  ## Not needed for release tarballs
     system "./configure", *args
     system "make"
     system "make install"
