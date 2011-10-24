fc6c2adf6fc3c5340f7437c3954432c8e3a85981
Tryn Mirell <tryn@mirell.org>
2011-10-23 21:58:57 -0500

    link-grammar 4.7.4
    The Link Grammar Parser is a syntactic parser of English (and other
    languages as well), based on link grammar, an original theory of English
    syntax. Given a sentence, the system assigns to it a syntactic
    structure, which consists of a set of labelled links connecting pairs of
    words. The parser also produces a "constituent" (Penn tree-bank style
    phrase tree) representation of a sentence (showing noun phrases, verb
    phrases, etc.). The RelEx extension provides dependency-parse output.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/link-grammar.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class LinkGrammar < Formula
+  url 'http://www.abisource.com/downloads/link-grammar/4.7.4/link-grammar-4.7.4.tar.gz'
+  homepage 'http://www.abisource.com/projects/link-grammar/'
+  md5 'e90e702a953641713a1292db20677bd2'
+
+  depends_on 'pkg-config' => :build
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "link-parser", "--version"
+  end
+end
