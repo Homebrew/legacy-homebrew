b6db050f5f43177155ebe8308a27a809527d4508
Henry Mercer <enovav@me.com>
2011-10-24 15:22:02 +0200

    Updating htmlcompressor formula for htmlcompressor v1.5.2

--- Library/Formula/htmlcompressor.rb
+++ Library/Formula/htmlcompressor.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class Htmlcompressor < Formula
-  url 'http://htmlcompressor.googlecode.com/files/htmlcompressor-1.4.2.jar'
+  url 'http://htmlcompressor.googlecode.com/files/htmlcompressor-1.5.2.jar'
   homepage 'http://code.google.com/p/htmlcompressor/'
-  md5 '2afe7aba064c619a761541ab466cca28'
+  md5 '91575c89c83d0563dcf2aad409e4748f'
 
   def install
-    libexec.install "htmlcompressor-1.4.2.jar"
+    libexec.install "htmlcompressor-1.5.2.jar"
     (bin+'htmlcompressor').write <<-EOS.undent
       #!/bin/sh
-      java -jar "#{libexec}/htmlcompressor-1.4.2.jar" $@
+      java -jar "#{libexec}/htmlcompressor-1.5.2.jar" $@
     EOS
   end
 end
