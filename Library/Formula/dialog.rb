5b32a97a28872f9e6c0a9562da97ff4240473dd4
Tryn Mirell <tryn@mirell.org>
2011-10-23 23:55:33 -0500

    Update dialog to v1.1.20111020

--- Library/Formula/dialog.rb
+++ Library/Formula/dialog.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Dialog < Formula
   url 'ftp://invisible-island.net/dialog/dialog.tar.gz'
   homepage 'http://invisible-island.net/dialog/'
-  md5 '34d01aaacbb2932b77774e6c1eec8d2a'
-  version '1.1.20110707'
+  md5 '15dd2ce9f537c0400f600d1a383aa0a3'
+  version '1.1.20111020'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
