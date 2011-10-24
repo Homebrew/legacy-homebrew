8483682dfeb685c682f19d0bf9a6a27f3b217a4c
David Turnbull <david@broodax.net>
2011-10-25 09:02:44 +1100

    pulledpork homepage url

--- Library/Formula/pulledpork.rb
+++ Library/Formula/pulledpork.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Pulledpork < Formula
   url 'http://pulledpork.googlecode.com/files/pulledpork-0.6.1.tar.gz'
-  homepage ''
+  homepage 'http://code.google.com/p/pulledpork/'
   md5 'a35c5c89d1f631ade1a2cd4e5c3a8778'
 
   depends_on 'Switch' => :perl
