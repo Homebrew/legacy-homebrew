6fb5628066d855a39b4555758f995b8fdc196bb4
Jack Nagel <jacknagel@gmail.com>
2011-10-25 17:25:46 -0500

    libtasn1 2.10
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libtasn1 < Formula
-  url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-2.9.tar.gz'
+  url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-2.10.tar.gz'
   homepage 'http://www.gnu.org/software/libtasn1/'
-  md5 'f4f4035b84550100ffeb8ad4b261dea9'
+  md5 'ef80c227d0dcdc2940fbc58faf8e0cf1'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
