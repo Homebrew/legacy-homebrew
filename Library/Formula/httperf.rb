7b016e1dec7113940bdc11454d663413512d5ea5
Jay Levitt <jay@jay.fm>
2011-10-25 17:33:31 -0400

    httperf: support --enable-debug option
    The --enable-debug configure-time option builds httperf with
    runtime support for the --debug=N or -d=N flags, which show
    details about the requests and responses.  N is a number; the
    higher the number, the more detail you get.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/httperf.rb
+++ Library/Formula/httperf.rb
@@ -5,8 +5,14 @@ class Httperf < Formula
   homepage 'http://code.google.com/p/httperf/'
   md5 '2968c36b9ecf3d98fc1f2c1c9c0d9341'
 
+  def options
+    [['--enable-debug', 'build with support for the --debug=N option']]
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    debug = ARGV.include?('--enable-debug') ? '--enable-debug' : '--disable-debug'
+
+    system "./configure", "--prefix=#{prefix}", debug, "--disable-dependency-tracking"
     system "make install"
   end
 end
