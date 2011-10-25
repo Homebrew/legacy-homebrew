e1c62cb7349d124f1f9f4ca9753e4cada23a6263
Jean-Olivier Irisson <irisson@normalesup.org>
2011-10-25 19:32:56 +0200

    New formula for Swatchbooker 0.7.3
    Swatchbooker is a python program to view, convert and edit swatches
    (colours mostly at the moment). It comprises command line tools and a
    Qt GUI, hence the pyqt dependency. It requires little-cms to convert
    colours and little-cms hence needs to be compiled with python bindings.

---
+++ Library/Formula/swatchbooker.rb
@@ -0,0 +1,40 @@
+require 'formula'
+
+class Swatchbooker < Formula
+  url 'http://launchpad.net/swatchbooker/trunk/0.7.3/+download/SwatchBooker-0.7.3.tar.gz'
+  homepage 'http://www.selapa.net/swatchbooker/'
+  md5 'd5c2d6d58679b233e2c298381b164acd'
+
+  depends_on 'lcms'
+  depends_on 'pil'
+  depends_on 'pyqt'
+
+  def install
+    # Tell launching shell scipts where the python code actually is
+    inreplace "data/swatchbooker" do |s|
+      s.gsub! "/usr/lib",
+              "#{HOMEBREW_PREFIX}/lib"
+    end
+    inreplace "data/sbconvert" do |s|
+      s.gsub! "/usr/lib",
+              "#{HOMEBREW_PREFIX}/lib"
+    end
+    inreplace "data/sbconvertor" do |s|
+      s.gsub! "/usr/lib",
+              "#{HOMEBREW_PREFIX}/lib"
+    end
+
+    system "python", "setup.py", "build"
+    system "python", "setup.py", "install", "--prefix=#{prefix}"
+  end
+
+  def caveats; <<-EOS
+For the graphical user interface to work, PyQT requires that you amend your PYTHONPATH like so:
+    export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/python:$PYTHONPATH
+EOS
+  end
+
+  def test
+    system "swatchbooker"
+  end
+end
