312dde7dba69a0195f7ae517ed6d1ec89b42ebfb
Victor Bilyk <victorbilyk@gmail.com>
2011-10-28 01:42:31 +0700

    added patch source and description

--- Library/Formula/winexe.rb
+++ Library/Formula/winexe.rb
@@ -6,6 +6,14 @@ class Winexe < Formula
   md5 '48325521ddc40d14087d1480dc83d51e'
 
   def patches
+    # This patch removes second definition of event context, which *should* break the build virtually everywhere,  
+    # but for some reason it only breaks it on OS X.
+    #
+    # There are several instructions over the Internet on how to build winexe for OS X, all of them
+    # share the same patch, so original author is unknown. One of the sources:
+    #
+    # http://miskstuf.tumblr.com/post/6840077505/winexe-1-00-linux-macos-windows-7-finally-working 
+    #
     {:p1 => 'https://raw.github.com/gist/1294786/winexe.patch'}
   end
 
