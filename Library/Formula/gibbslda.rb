657e151738d000c9a25f3a43dc5458abbecf9b17
Chris Lowis <chris.lowis@gmail.com>
2011-10-27 13:28:16 +0100

    rename the formula to "gibbslda" from "gibbs-lda"

--- Library/Formula/gibbslda.rb
+++ Library/Formula/gibbslda.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class GibbsLda < Formula
+class Gibbslda < Formula
   url 'http://downloads.sourceforge.net/project/gibbslda/GibbsLDA%2B%2B/0.2/GibbsLDA%2B%2B-0.2.tar.gz?r=&ts=1314109304'
   homepage 'http://gibbslda.sourceforge.net/'
   md5 'd87d35be6dc8b37afc515f6237ba2ccb'
