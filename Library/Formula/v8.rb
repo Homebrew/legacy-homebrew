14ad4af18ee76bfb9703cf836edb0aaef2752ad9
muddletoes <muddletoes@hotmail.com>
2011-10-28 12:49:06 -0400

    Revert ff9cffb7245908dc3f545df8e6569aa841422747^..HEAD

--- Library/Formula/v8.rb
+++ Library/Formula/v8.rb
@@ -6,51 +6,21 @@ class V8 < Formula
 
   depends_on 'scons' => :build
 
-  def caveats
-    s = "For build options see:\n  brew options v8"
-
-    return s
-  end
-
-
-  def options
-    [
-     ['--debug',"Include debugging information and assertions in the V8 binary."],
-     ['--no-snapshots',"Disable 'snapshots' (saved program states) in V8."],
-     ['--shared-libraries',"Build shared rather than static libraries."],
-     ['--developer-shell',"Build the V8 JavaScript development shell."]
-    ]
-  end
-  
   def install
-
     arch = Hardware.is_64_bit? ? 'x64' : 'ia32'
 
-    args = [
-            "-j #{Hardware.processor_count}",
-            "arch=#{arch}",
-            "visibility=default",
-            "console=readline",
-            (ARGV.include? '--debug')?'mode=debug':'mode=release',
-            (ARGV.include? '--no-snapshots')?'snapshot=off':'snapshot=on',
-            (ARGV.include? '--shared-libraries')?'library=shared':'library=static',
-            (ARGV.include? '--developer-shell')?'d8':'sample=shell'
-            ]
-
-
-
-
-    system "scons", *args
-
+    system "scons", "-j #{ENV.make_jobs}",
+                    "arch=#{arch}",
+                    "mode=release",
+                    "snapshot=on",
+                    "library=shared",
+                    "visibility=default",
+                    "console=readline",
+                    "sample=shell"
 
     include.install Dir['include/*']
     lib.install Dir['libv8.*']
-
-    if ARGV.include? '--developer-shell'
-      bin.install 'd8' => 'v8'
-    else
-      bin.install 'shell' => 'v8'
-    end
+    bin.install 'shell' => 'v8'
 
     system "install_name_tool -change libv8.dylib #{lib}/libv8.dylib #{bin}/v8"
   end
