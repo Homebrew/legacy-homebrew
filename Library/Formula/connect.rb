2fdc089082c90ca5e9f7c186b487dbfb3eaff8a5
Tony Sokhon <tonyskn@gmail.com>
2011-10-31 00:28:10 +0100

    Fix 'connect' formula url

--- Library/Formula/connect.rb
+++ Library/Formula/connect.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Connect < Formula
-  url 'http://www.taiyo.co.jp/~gotoh/ssh/connect.c'
+  url 'http://www.meadowy.org/~gotoh/ssh/connect.c'
   version '1.100'
   homepage 'http://bent.latency.net/bent/git/goto-san-connect-1.85/src/connect.html'
   md5 '5165e2fcd2cf58899f34878fe6b447c6'
