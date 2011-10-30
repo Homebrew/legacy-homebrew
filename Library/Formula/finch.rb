b3ed649d07f3294d86c1dd4a2a218415fbbeaac4
Ryan <rsully.dev+git@me.com>
2011-10-30 12:32:09 -0400

    Updated finch formula (new version) along with glib+dbus support
    I originally was creating a new pidgin formulua (minus the gui), but I think updating the finch formula and simply adding the extra dependencies is the better way to go. This will setup a dev enviornment for libpurple and glib (besides installing the functional finch client).

--- Library/Formula/finch.rb
+++ Library/Formula/finch.rb
@@ -2,23 +2,26 @@ require 'formula'
 
 # Finch is a command-line-only IM client from Pidgin
 class Finch < Formula
+  url 'http://voxel.dl.sourceforge.net/project/pidgin/Pidgin/2.10.0/pidgin-2.10.0.tar.bz2'
   homepage 'http://developer.pidgin.im/wiki/Using%20Finch'
-  url 'http://sourceforge.net/projects/pidgin/files/Pidgin/2.7.11/pidgin-2.7.11.tar.bz2'
-  sha256 'a24e2c3118bd47983955d398c9cf5543c12e95527cdf7de9d273a6e92f9d160b'
+  md5 'e1453c9093c4f32beec19abd14069a3f'
 
   depends_on 'pkg-config' => :build
+  depends_on 'intltool'
+  depends_on 'libiconv'
   depends_on 'libidn'
   depends_on 'gettext'
   depends_on 'glib'
+  depends_on 'dbus'
+  depends_on 'dbus-glib'
   depends_on 'intltool'
   depends_on 'gnutls'
+  depends_on 'nss' # Extra for some SSL stuff, probably unrequired
 
   def install
     # To get it to compile, had to configure without support for:
     #   * Sametime (meanwhile)
     #   * Bonjour (avahi)
-    #   * Communicating with other programs (d-bus)
-    #   * Perl scripting
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-gtkui",
@@ -26,8 +29,12 @@ class Finch < Formula
                           "--disable-vv",
                           "--disable-meanwhile",
                           "--disable-avahi",
-                          "--disable-dbus",
-                          "--disable-perl"
+                          "--disable-nm",
+                          "-with-dbus-services=/usr/local/share/dbus-1/"
     system "make install"
+  end
+  
+  def test
+    system "finch --version" # Just to make sure something runs without actually using finch
   end
 end
