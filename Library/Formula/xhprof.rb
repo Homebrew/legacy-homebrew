356311e1aa912d0b4e5fc3f8216b4e06d5fa8f4b
Valentin Sushkov <sushkov.valentin@gmail.com>
2011-11-03 18:51:37 +0400

    Added xhprof formula
    XHProf is a function-level hierarchical profiler for PHP and has a
    simple HTML based user interface.

---
+++ Library/Formula/xhprof.rb
@@ -0,0 +1,45 @@
+require 'formula'
+
+class Xhprof < Formula
+  url 'https://nodeload.github.com/facebook/xhprof/tarball/master'
+  homepage 'https://nodeload.github.com/facebook/xhprof/tarball/master'
+  md5 'c02c5e5477a232677e8c9ebab8aed66a'
+  version '2011.07.17'
+
+  def install
+    p Dir.pwd
+    Dir.chdir "extension" do
+      system "phpize"
+      php_config = `which php-config`
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}",
+                            "--with-php-config=#{php_config}"
+      system "make"
+      prefix.install 'modules/xhprof.so'
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    To use this software:
+      * Add the following line to php.ini:
+        [xhprof]
+        extension="#{prefix}/xhprof.so"
+        xhprof.output_dir="/var/tmp/xhprof"
+      * Make the /var/tmp/xhprof folder writable
+      * Restart your webserver.
+      * Write a PHP page that calls "phpinfo();"
+      * Load it in a browser and look for the info on the xdebug module.
+        If you see it, you have been successful!
+      * Read this: http://techportal.ibuildings.com/2009/12/01/profiling-with-xhprof/
+    EOS
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just
+    # replace "false" with the main program this formula installs, but
+    # it'd be nice if you were more thorough. Test the test with
+    # `brew test xhprof`. Remove this comment before submitting
+    # your pull request!
+    system "false"
+  end
+end
