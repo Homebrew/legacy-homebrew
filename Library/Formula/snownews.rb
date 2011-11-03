aa05992ecebc2121532d4a61a29fa4c5f9f27bcd
Jack Nagel <jacknagel@gmail.com>
2011-11-02 22:41:25 -0500

    paperkey: run 'make check'
    Short and sweet, and since we're dealing with PGP keys here it's a good
    idea to make sure we've compiled correctly.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>
    
    Added Formula for Snownews, a console RSS reader.
    
    added depends item for gettext.

---
+++ Library/Formula/snownews.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Snownews < Formula
+  url 'https://kiza.kcore.de/media/software/snownews/snownews-1.5.12.tar.gz'
+  homepage 'https://kiza.kcore.de/media/software/snownews'
+  md5 '80da8943fc5aa96571924aec0087d4c0'
+
+  depends_on 'gettext'
+
+  def install
+    system "./configure", "--disable-nls", "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "snownews"
+  end
+end
