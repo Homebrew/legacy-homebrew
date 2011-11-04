b25164a4b8a7268676aaaeab2161da63b5d77da1
Trung Lê <joneslee85@gmail.com>
2011-11-04 12:41:52 +1100

    Added open-cubic-player formula

---
+++ Library/Formula/ocp.rb
@@ -0,0 +1,43 @@
+require "formula"
+
+class Ocp < Formula
+  head "git://git.code.sf.net/p/opencubicplayer/code", :using => :git
+  homepage "http://sourceforge.net/p/opencubicplayer/home/"
+
+  #depends_on "binutils" => :build
+  depends_on "mad" unless ARGV.include? "--without-mad"
+  depends_on "flac" unless ARGV.include? "--without-flac"
+  depends_on "adplug" if ARGV.include? "--with-adplug"
+
+  def options
+    [
+      ["--without-mad", "disable mad mpeg audio support"],
+      ["--without-flac", "disable FLAC support"],
+      ["--with-adplug", "enable adplug support"],
+    ]
+  end
+
+  def install
+    args = ["--prefix=#{prefix}",
+            "--disable-debug",
+            "--disable-dependency-tracking",
+            "--without-x11"]
+
+    if ARGV.include? "--without-mad"
+      args << "--without-mad"
+    elsif ARGV.include? "--without-flac"
+      args << "--without-flac"
+    elsif ARGV.include? "--with-adplug"
+      args << "--with-adplug"
+    end
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+  end
+
+  def caveats
+    "For build options see:\n  brew options ocp\n\n"
+  end
+
+end
