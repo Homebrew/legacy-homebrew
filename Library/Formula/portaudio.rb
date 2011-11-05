f32d085f349a8694ec6b1e8818acf35bbba92e3f
Liam Staskawicz <lstask@gmail.com>
2011-11-05 14:58:36 -0700

    portaudio: update to latest stable candidate. much simpler build config. tested on OS X 10.7.2 Xcode 4.1

--- Library/Formula/portaudio.rb
+++ Library/Formula/portaudio.rb
@@ -1,30 +1,11 @@
 require 'formula'
 
 class Portaudio < Formula
-  url 'http://www.portaudio.com/archives/pa_stable_v19_20071207.tar.gz'
+  url 'http://www.portaudio.com/archives/pa_stable_candidate_v19_20111026.tgz'
   homepage 'http://www.portaudio.com'
-  md5 'd2943e4469834b25afe62cc51adc025f'
-
-  depends_on 'pkg-config' => :build
-
-  fails_with_llvm :build => 2334
-
-  def options
-    [["--universal", "Build a universal binary."]]
-  end
-
-  # Use the MacPort patches that fix compiling against newer OS X SDKs
-  def patches
-    {:p0 => [
-      "https://trac.macports.org/export/77586/trunk/dports/audio/portaudio/files/patch-configure",
-      "https://trac.macports.org/export/77586/trunk/dports/audio/portaudio/files/patch-src__hostapi__coreaudio__pa_mac_core.c",
-      "https://trac.macports.org/export/77586/trunk/dports/audio/portaudio/files/patch-src__common__pa_types.h"
-    ]}
-  end
+  md5 'a8340787619a72aab945e6d324ed80a3'
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
-
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
 
