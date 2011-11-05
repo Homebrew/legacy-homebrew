22776eca7c98490e8a237bd62f9142ef6eecd587
Oleg Oshmyan <chortos@inbox.lv>
2011-11-05 04:31:24 +0000

    New formula: libkate 0.4.1

---
+++ Library/Formula/libkate.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Libkate < Formula
+  url 'http://libkate.googlecode.com/files/libkate-0.4.1.tar.gz'
+  homepage 'http://code.google.com/p/libkate/'
+  sha1 '87fd8baaddb7120fb4d20b0a0437e44ea8b6c9d8'
+
+  depends_on 'libogg' => :recommended
+
+  def install
+    ENV.libpng
+    # GCC 4.2 segfaults during compilation
+    ENV.llvm if ENV.compiler == :gcc
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
