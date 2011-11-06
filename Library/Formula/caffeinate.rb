cde44a8821493501a657e98771ed4f4f577cd524
Takuya Tozawa <takuya.tozawa@gmail.com>
2011-11-07 05:39:14 +0900

    Fixed url protocol

--- Library/Formula/caffeinate.rb
+++ Library/Formula/caffeinate.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Caffeinate < Formula
-  url 'git://github.com/uasi/caffeinate.git', :tag => 'f6fca5076c2e09a0fd6863fff3f51f2d80beabab'
+  url 'https://github.com/uasi/caffeinate.git', :tag => 'f6fca5076c2e09a0fd6863fff3f51f2d80beabab'
   homepage 'https://github.com/uasi/caffeinate'
   md5 'c16adae4e018b98f57ca3898907bdd30'
   version '1.0'
