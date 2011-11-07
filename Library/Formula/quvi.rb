081972d9b21b6c098acb1b1ec74e76c908fe0edd
Nibbles 2bits <nibbles@speakeasy.net>
2011-11-06 22:49:50 -0800

    quvi: Update to 0.4.0
    This updates the formula to quvi-0.4.0.
    It depends on its library, libquvi, which is a new formula.
    This compiles and tests without error using Clang-2.0.139 and
    llvm-2335.9 on Snow Leopard using XCode-4.0.2. It also compiles
    on Lion using XCode-4.2 (4D502) using both compilers.
    
    EDIT: tested against Lion
    EDIT: removed the explanation about a separate libquvi-scripts
    EDIT: tested against the updated libquvi.
    EDIT: added a def test that prints versions for all 3 components.

--- Library/Formula/quvi.rb
+++ Library/Formula/quvi.rb
@@ -1,19 +1,21 @@
 require 'formula'
 
 class Quvi < Formula
-  url 'http://sourceforge.net/projects/quvi/files/0.2/quvi-0.2.18.tar.bz2'
-  sha1 '42a1a0a949ddb5d3eaec91cddd21ed34a5b1e259'
+  url 'http://downloads.sourceforge.net/project/quvi/0.4/quvi/quvi-0.4.0.tar.bz2'
+  sha1 '79784b47223d77188ba2cc2c905d7fb530d8c71b'
   homepage 'http://quvi.sourceforge.net/'
 
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
   depends_on 'lua'
+  depends_on 'libquvi'
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--enable-smut",
-                          "--enable-broken"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
+  end
+
+  def test
+    system "#{HOMEBREW_PREFIX}/bin/quvi --version"
   end
 end
