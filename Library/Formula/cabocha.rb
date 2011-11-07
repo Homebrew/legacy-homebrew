1c67d7d35a7a194bb17db575cd19b82b99edaaf1
Oame <oame@oameya.com>
2011-11-07 22:25:15 +0900

    optimized version number
    Signed-off-by: Oame <oame@oameya.com>

--- Library/Formula/cabocha.rb
+++ Library/Formula/cabocha.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Cabocha < Formula
   url 'http://cabocha.googlecode.com/files/cabocha-0.60.tar.gz'
   homepage 'http://code.google.com/p/cabocha/'
-  version '0.60'
+  version '0.6'
   md5 '3399873faab9b252fd57e1d48fa1285d'
 
   depends_on 'mecab'
