a446d0c8f61b51a6bf9c2e90c631e60fe8100ce7
Carsten Dreesbach <carsten.dreesbach@opower.com>
2011-11-06 19:56:45 -0500

    Adding dependencies, fixing test to not exit with 1 and adding a hint
    Added the pkg-config dependency as suggested by jacknagel.
    Fixed the test (by adding version command) to not exit with 1.
    Added a hint to the user to enable XTEST in X server.

--- Library/Formula/xdotool.rb
+++ Library/Formula/xdotool.rb
@@ -5,11 +5,18 @@ class Xdotool < Formula
   homepage 'http://www.semicomplete.com/projects/xdotool/'
   md5 '62d0c2158bbaf882a1cf580421437b2f'
 
+  depends_on 'pkg-config' => :build
+
   def install
     system "make", "PREFIX=#{prefix}", "INSTALLMAN=#{man}", "install"
+
+    ohai "You will probably want to enable XTEST in your X11 server now by running:\n"
+    ohai "defaults write org.x.X11 enable_test_extensions -boolean true\n"
+    ohai "(see http://stackoverflow.com/questions/1264210/does-mac-x11-have-the-xtest-extension"
+    ohai "for the source of this useful hint - thx to Nicholas Riley for providing that)"
   end
 
   def test
-    system "xdotool"
+    system "xdotool version"
   end
 end
