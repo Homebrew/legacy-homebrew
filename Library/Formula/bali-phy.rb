d7aa9b1a6fd73377feac2f1e44cfda49279d027c
Nibbles 2bits <nibbles@speakeasy.net>
2011-11-06 23:55:36 -0800

    bali-phy: update to 2.1.1, enable cairo
     - Update bali-phy to 2.1.1.
     - Build oos as directed in the docs.
     - Add --enable-cairo flag to build the rest of the programs.
    
    The compiles and runs without error on Snow Leopard using clang, and
    llvm from XCode-4.0.2.
    
    Closes #8491.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/bali-phy.rb
+++ Library/Formula/bali-phy.rb
@@ -1,14 +1,23 @@
 require 'formula'
 
 class BaliPhy < Formula
-  url 'http://www.biomath.ucla.edu/msuchard/bali-phy/bali-phy-2.1.0.tar.gz'
+  url 'http://www.biomath.ucla.edu/msuchard/bali-phy/bali-phy-2.1.1.tar.gz'
   homepage 'http://www.biomath.ucla.edu/msuchard/bali-phy/'
-  md5 'ce5d96d464c3a8957caf6683f5b4ee2c'
+  sha1 'e72073a1c5b05c797668e476bfd8517594f074e6'
 
   depends_on 'gsl'
 
+# fails_with_clang
+#     io.H:25:31: error: use of undeclared identifier 'push_back'
+#       void operator()(const T& t){push_back(t);}
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-    system "make install"
+    # docs say build oos
+    mkdir 'macbuild'
+    Dir.chdir 'macbuild' do
+      system "../configure", "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}", "--enable-cairo"
+      system "make install"
+    end
   end
 end
