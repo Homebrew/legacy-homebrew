7aabc49cd099aaf0db1db4b140038ed3b86366c5
Drarok Ithaqua <github@catchall.drarok.com>
2011-11-08 12:40:40 +0000

    Follow symlink chain to get real gcc path. Fix the regexp match for llvm in path.

--- Library/Homebrew/extend/ENV.rb
+++ Library/Homebrew/extend/ENV.rb
@@ -133,9 +133,16 @@ module HomebrewEnvExtension
     remove_from_cflags '-O4'
     @compiler = :gcc
 
+    # Check the symlink path... There's no File.realpath on Ruby 1.8.7.
+    was_link = File.symlink? self['CC']
+
+    # Loop until it's no longer a symlink.
+    while (File.symlink? self['CC']) do
+        self['CC'] = File.readlink(self['CC'])
+    end
+
     raise "GCC could not be found" if args[:force] and not File.exist? ENV['CC'] \
-                                   or (File.symlink? ENV['CC'] \
-                                   and File.readlink(ENV['CC']) =~ /llvm/)
+                                   or (was_link and ENV['CC'] =~ /llvm/)
   end
   alias_method :gcc_4_2, :gcc
 
