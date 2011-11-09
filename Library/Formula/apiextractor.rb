38711f06bcac8027d73b5a05335ee295a895c1fe
Charlie Sharpsteen <source@sharpsteen.net>
2011-11-09 12:12:55 -0800

    PySide: Update to 1.0.8
    Also updates dependencies:
    
      - apiextractor
      - generatorrunner
      - shiboken

--- Library/Formula/apiextractor.rb
+++ Library/Formula/apiextractor.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Apiextractor < Formula
   homepage 'http://www.pyside.org/docs/apiextractor'
-  url 'http://pyside.org/files/apiextractor-0.10.7.tar.bz2'
-  md5 '539be815e7fb09064fefcdb21ad44965'
+  url 'http://pyside.org/files/apiextractor-0.10.8.tar.bz2'
+  md5 'ddf42bdd6becae602feacf8a1b190425'
 
   depends_on 'cmake' => :build
   depends_on 'qt'
