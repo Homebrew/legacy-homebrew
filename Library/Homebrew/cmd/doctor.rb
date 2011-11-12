0852cccff96e9bcd53cdd880a232897f66f32246
Jack Nagel <jacknagel@gmail.com>
2011-11-11 20:12:41 -0600

    doctor: remove outdated XCode version reference
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Homebrew/cmd/doctor.rb
+++ Library/Homebrew/cmd/doctor.rb
@@ -197,7 +197,7 @@ def check_gcc_versions
   if MacOS.xcode_version == nil
       puts <<-EOS.undent
         We couldn't detect any version of Xcode.
-        If you downloaded Xcode 4.1 from the App Store, you may need to run the installer.
+        If you downloaded Xcode from the App Store, you may need to run the installer.
 
       EOS
   elsif MacOS.xcode_version < "4.0"
