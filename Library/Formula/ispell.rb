b2b4e3d2e2ceefb0f813c838c58cb60f6380ae41
Sean Escriva <sean.escriva@gmail.com>
2011-11-13 17:54:49 -0800

    New formula: ispell 3.3.02

---
+++ Library/Formula/ispell.rb
@@ -0,0 +1,40 @@
+require 'formula'
+
+class Ispell < Formula
+  url 'http://www.lasr.cs.ucla.edu/geoff/tars/ispell-3.3.02.tar.gz'
+  homepage 'http://lasr.cs.ucla.edu/geoff/ispell.html'
+  md5 '12087d7555fc2b746425cd167af480fe'
+
+  def skip_clean? path
+    true
+  end
+
+  # a little tricky since it doesn't use a configure script
+  def install
+    ENV.deparallelize
+    ENV.no_optimization
+    cp "local.h.macos", "local.h"
+    chmod 0644, "local.h"
+    inreplace "local.h" do |s|
+      s.gsub! '/usr/local', "#{prefix}"
+      s.gsub! '/man/man', '/share/man/man'
+    end
+    chmod 0644, "correct.c"
+    inreplace "correct.c", "getline", "getline_ispell"
+    system "make config.sh"
+    chmod 0644, "config.sh"
+    inreplace "config.sh" do |s|
+      s.gsub! '/usr/share/dict', "#{share}/dict"
+      s.gsub! /yacc/, "/usr/bin/yacc"
+    end
+    lib.mkdir
+    system "/usr/bin/make all"
+    system "/usr/bin/make install"
+  end
+
+  def test
+    %w{ buildhash defmt-c defmt-sh findaffix icombine ijoin ispell munchlist tryaffix}.each do |f|
+      system "test -x #{bin}/" + f
+    end
+  end
+end
