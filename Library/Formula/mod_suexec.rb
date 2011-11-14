7365ffd4f47611a09efbcf238ae1c7866ba1e647
Lifepillar <github@lifepillar.com>
2011-11-14 19:32:35 +0100

    Use environment variables to customize the configuration.

--- Library/Formula/mod_suexec.rb
+++ Library/Formula/mod_suexec.rb
@@ -7,7 +7,11 @@ class ModSuexec < Formula
   md5 '4504934464c5ee51018dbafa6d99810d'
 
   def install
-    suexec_docroot   = '/Library/WebServer'
+    suexec_userdir   = ENV['SUEXEC_USERDIR']  || 'Sites'
+    suexec_docroot   = ENV['SUEXEC_DOCROOT']  || '/Library/WebServer'
+    suexec_uidmin    = ENV['SUEXEC_UIDMIN']   || '500'
+    suexec_gidmin    = ENV['SUEXEC_GIDMIN']   || '20'
+    suexec_safepath  = ENV['SUEXEC_SAFEPATH'] || '/usr/local/bin:/usr/bin:/bin:/opt/local/bin'
     logfile          = '/private/var/log/apache2/suexec_log'
     begin
       suexecbin = `/usr/sbin/apachectl -V`.match(/SUEXEC_BIN="(.+)"/)[1]
@@ -18,12 +22,12 @@ class ModSuexec < Formula
       "--enable-suexec=shared",
       "--with-suexec-bin=#{suexecbin}",
       "--with-suexec-caller=_www",
-      "--with-suexec-userdir=Sites",
+      "--with-suexec-userdir=#{suexec_userdir}",
       "--with-suexec-docroot=#{suexec_docroot}",
-      "--with-suexec-uidmin=500",
-      "--with-suexec-gidmin=20",
+      "--with-suexec-uidmin=#{suexec_uidmin.to_i}",
+      "--with-suexec-gidmin=#{suexec_gidmin.to_i}",
       "--with-suexec-logfile=#{logfile}",
-      "--with-suexec-safepath='/usr/local/bin:/usr/bin:/bin:/opt/local/bin'"
+      "--with-suexec-safepath=#{suexec_safepath}"
     system "make"
     libexec.install 'modules/generators/.libs/mod_suexec.so'
     include.install 'modules/generators/mod_suexec.h'
@@ -47,6 +51,10 @@ class ModSuexec < Formula
 
       Please, be sure to understand the security implications of suexec
       by carefully reading http://httpd.apache.org/docs/current/suexec.html.
+
+      This formula will use the values of the following environment
+      variables, if set: SUEXEC_DOCROOT, SUEXEC_USERDIR, SUEXEC_UIDMIN,
+      SUEXEC_GIDMIN, SUEXEC_SAFEPATH.
     EOS
   end
 
