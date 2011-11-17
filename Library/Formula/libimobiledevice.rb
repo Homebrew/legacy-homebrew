5a6efbf0ac858dec30495854b6739061d2c9b08a
Ingo Richter <ingo.richter@gmail.com>
2011-11-16 19:02:00 -0800

    - added head formula - fix the m4 problem with the missing pkg.m4 by adding the path to aclocal

--- Library/Formula/libimobiledevice.rb
+++ Library/Formula/libimobiledevice.rb
@@ -2,8 +2,10 @@ require 'formula'
 
 class Libimobiledevice < Formula
   url 'http://www.libimobiledevice.org/downloads/libimobiledevice-1.1.1.tar.bz2'
+  md5 'aba5c6030c99fd1022b9c7488bce7b30'
+  head 'http://cgit.sukimashita.com/libimobiledevice.git', :using => :git
+  version '1.1.1'
   homepage 'http://www.libimobiledevice.org/'
-  md5 'cdc13037e822d9ac2e109536701d153a'
 
   depends_on 'pkg-config' => :build
   depends_on 'libtasn1'
@@ -13,7 +15,12 @@ class Libimobiledevice < Formula
   depends_on 'gnutls'
 
   def install
+    # fix the m4 problem with the missing pkg.m4
+    aclocalDefault = `/usr/bin/aclocal --print-ac-dir`
+    inreplace "autogen.sh", "aclocal -I m4", "aclocal -I m4 -I#{aclocalDefault.strip} -I #{HOMEBREW_PREFIX}/share/aclocal"
+
     ENV.prepend "CFLAGS", "-I#{HOMEBREW_PREFIX}/include"
+    system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--without-swig"
