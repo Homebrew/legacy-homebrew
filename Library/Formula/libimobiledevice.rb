6ce789ccf95f9fef5c3dd3fc4fde62487ccbfa01
Ingo Richter <ingo.richter@gmail.com>
2011-11-17 16:30:22 -0800

    - removed the version - modification of the autogen.sh only in the case that HEAD is build

--- Library/Formula/libimobiledevice.rb
+++ Library/Formula/libimobiledevice.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Libimobiledevice < Formula
   url 'http://www.libimobiledevice.org/downloads/libimobiledevice-1.1.1.tar.bz2'
-  md5 'aba5c6030c99fd1022b9c7488bce7b30'
+  md5 'cdc13037e822d9ac2e109536701d153a'
   head 'http://cgit.sukimashita.com/libimobiledevice.git', :using => :git
-  version '1.1.1'
   homepage 'http://www.libimobiledevice.org/'
 
   depends_on 'pkg-config' => :build
@@ -15,12 +14,15 @@ class Libimobiledevice < Formula
   depends_on 'gnutls'
 
   def install
-    # fix the m4 problem with the missing pkg.m4
-    aclocalDefault = `/usr/bin/aclocal --print-ac-dir`
-    inreplace "autogen.sh", "aclocal -I m4", "aclocal -I m4 -I#{aclocalDefault.strip} -I #{HOMEBREW_PREFIX}/share/aclocal"
+    if ARGV.build_head?
+      # fix the m4 problem with the missing pkg.m4
+      aclocalDefault = `/usr/bin/aclocal --print-ac-dir`
+      inreplace "autogen.sh", "aclocal -I m4", "aclocal -I m4 -I#{aclocalDefault.strip} -I #{HOMEBREW_PREFIX}/share/aclocal"
 
-    ENV.prepend "CFLAGS", "-I#{HOMEBREW_PREFIX}/include"
-    system "./autogen.sh"
+      ENV.prepend "CFLAGS", "-I#{HOMEBREW_PREFIX}/include"
+      system "./autogen.sh"
+    end
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--without-swig"
