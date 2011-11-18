59727bbb1feda617c66abb702342e3b13001b29b
Xiyue Deng <manphiz@gmail.com>
2011-11-18 05:53:50 -0800

    boost: Update to 1.48.0
    Closes #8657.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -2,10 +2,12 @@ require 'formula'
 
 class Boost < Formula
   homepage 'http://www.boost.org'
-  url 'http://downloads.sourceforge.net/project/boost/boost/1.47.0/boost_1_47_0.tar.bz2'
-  md5 'a2dc343f7bc7f83f8941e47ed4a18200'
-  bottle 'https://downloads.sourceforge.net/project/machomebrew/Bottles/boost-1.47.0-bottle.tar.gz'
-  bottle_sha1 '4f3834fb471c3fac20c649bc4081ddde991e4b3b'
+  url 'http://downloads.sourceforge.net/project/boost/boost/1.48.0/boost_1_48_0.tar.bz2'
+  md5 'd1e9a7a7f532bb031a3c175d86688d95'
+
+  # Bottle built on 10.7.2 using XCode 4.2
+  bottle 'https://downloads.sourceforge.net/project/machomebrew/Bottles/boost-1.48.0-bottle.tar.gz'
+  bottle_sha1 'd5edc2714c4182081d0399f0a0278672e1c85410'
 
   def options
     [
