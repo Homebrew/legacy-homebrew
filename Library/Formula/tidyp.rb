785151bd29dd448f6d1f0e37bce7aacb8c520bf9
Nicola Tuveri <nic.tuv@gmail.com>
2011-11-19 14:16:14 +0100

    Tidyp: add manpage
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tidyp.rb
+++ Library/Formula/tidyp.rb
@@ -1,5 +1,10 @@
 require 'formula'
 
+class TidypManual < Formula
+  url "https://github.com/petdance/tidyp/raw/6a6c85bc9cb089e343337377f76127d01dd39a1c/htmldoc/tidyp1.xsl"
+  md5 "5f90f914e69bdff1663a6772c6d9ca3d"
+end
+
 class Tidyp < Formula
   url 'http://github.com/downloads/petdance/tidyp/tidyp-1.04.tar.gz'
   homepage 'http://tidyp.com/'
@@ -9,5 +14,13 @@ class Tidyp < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
+
+    # Use the newly brewed Tidyp to generate the manual
+    TidypManual.new.brew do
+      system "#{bin}/tidyp -xml-help > tidyp1.xml"
+      system "#{bin}/tidyp -xml-config > tidyp-config.xml"
+      system "/usr/bin/xsltproc tidyp1.xsl tidyp1.xml |/usr/bin/gzip >tidyp.1.gz"
+      man1.install 'tidyp.1.gz'
+    end
   end
 end
