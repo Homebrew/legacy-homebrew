4787fe9d85d2e9e43f67cc8c287e70eac96dfc85
Matt Sanford <matt@mzsanford.com>
2011-11-20 10:07:03 -0800

    Re-add the 'head' reference and update the version.
    Version was incorrect and the 'head' had been removed on accident.

--- Library/Formula/libcld.rb
+++ Library/Formula/libcld.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Libcld < Formula
+  head 'git://github.com/mzsanford/cld.git', :tag => 'v0.1.1'
   url 'https://github.com/mzsanford/cld/tarball/v0.1.1'
   homepage 'https://github.com/mzsanford/cld'
   md5 'd8913e2acf594ea98d8988d1718cb732'
-  version '0.1.0'
+  version '0.1.1'
 
 
   def install
