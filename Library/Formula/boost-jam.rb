a09c3a6497ca063fffebe795edb5e685ef79066e
Jonathan Grochowski <jongrocho@gmail.com>
2011-11-20 06:11:44 -0800

    Make boost-jam compatible with boost-build-v2
    By default bjam looks in /usr/share/boost-build for its configuration
    files. This patch makes bjam compatible with the brew version of
    Boost.Build. If boost-build-v2 is installed, bjam will use it's
    boost-build directory, otherwise it will search for a manually install
    Build.Boost and let bjam use its defaults to use it. If it can't find
    either of those, it alerts the user and offers a few remediation paths.

--- Library/Formula/boost-jam.rb
+++ Library/Formula/boost-jam.rb
@@ -6,6 +6,24 @@ class BoostJam < Formula
   md5 'f790e022d658db38db5cc4aeeccad3f1'
 
   def install
+    if Formula.factory('boost-build-v2').installed?
+      ohai 'Boost.Build installed by brew, using it.'
+      inreplace [ 'Jambase', 'jambase.c' ], '/usr/share/boost-build', Formula.factory('boost-build-v2').share+'boost-build'
+    elsif ENV['BOOST_BUILD_PATH'] and File.exists?(File.join ENV['BOOST_BUILD_PATH'], 'boost-build.jam') or
+        ENV['BOOST_PATH'] and File.exists?(File.join ENV['BOOST_PATH'], 'boost-build.jam')
+      ohai 'Boost.Build installed manually, using it.'
+    elsif File.exists? '/usr/share/boost-build/boost-build.jam'
+      ohai 'Boost.Build installed in /usr/share/boost-build, using it.'
+    else
+      opoo <<-EOS.undent
+        Boost.Build not installed. Either install it with
+          brew install boost-build-v2
+        and reinstall boost-jam, or install Boost.Build manually.
+        If not installed to /usr/share/boost-build, set
+        BOOST_BUILD_PATH or BOOST_PATH to the installation directory.
+      EOS
+    end
+
     system "./build.sh"
     bin.install Dir["bin.macos*/bjam"]
   end
