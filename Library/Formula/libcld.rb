35f1b52fad5c007f1cbc45d35a38ea152d7bf8e3
Matt Sanford <matt@mzsanford.com>
2011-11-19 19:30:55 -0800

    Update from 'head' to 'url' for formula source location
    Changed from git head to tarball url and updated the md5 accordingly.

--- Library/Formula/libcld.rb
+++ Library/Formula/libcld.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libcld < Formula
-  head 'git://github.com/mzsanford/cld.git', :tag => 'v0.1.1'
+  url 'https://github.com/mzsanford/cld/tarball/v0.1.1'
   homepage 'https://github.com/mzsanford/cld'
-  md5 '03ea94f74f9a01bbabbc93259c1eaac001ce7ca8'
+  md5 'd8913e2acf594ea98d8988d1718cb732'
   version '0.1.0'
 
 
