73d58f723f5cad546690ef1e743530645c66c53e
Ryan Johnston <ryanj@ryanj.org>
2011-11-21 15:35:00 -0700

    added depends item for gettext.

--- Library/Formula/snownews.rb
+++ Library/Formula/snownews.rb
@@ -5,6 +5,8 @@ class Snownews < Formula
   homepage 'https://kiza.kcore.de/media/software/snownews'
   md5 '80da8943fc5aa96571924aec0087d4c0'
 
+  depends_on 'gettext'
+
   def install
     system "./configure", "--disable-nls", "--prefix=#{prefix}"
     system "make install"
