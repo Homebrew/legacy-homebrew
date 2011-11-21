696d8605b30fba86f6a08fe920f246d35f6608a9
Juan Sebastian Casallas <casallas@iastate.edu>
2011-11-21 10:23:54 +0100

    Add --only-blahtex option to blahtexml
    -Some libraries only need blahtex like Instill or Maruku
    -It isn't worth building xerces-c (needed for blahtexml only) when you only need blahtex
    -caveats now explains that you get both blahtex and blahtexml by default

--- Library/Formula/blahtexml.rb
+++ Library/Formula/blahtexml.rb
@@ -5,12 +5,28 @@ class Blahtexml < Formula
   homepage 'http://gva.noekeon.org/blahtexml/'
   md5 'ed790599223c2f8f6d205be8988882de'
 
-  depends_on 'xerces-c'
+  depends_on 'xerces-c' unless ARGV.include? '--blahtex-only'
+
+  def options
+    [
+      ['--blahtex-only', "Build only blahtex, not blahtexml"]
+    ]
+  end
+
 
   def install
-    system "/usr/bin/make blahtex-mac"
+    system "make blahtex-mac"
     bin.install('blahtex')
-    system "/usr/bin/make blahtexml-mac"
-    bin.install('blahtexml')
+    unless ARGV.include? '--blahtex-only'
+      system "make blahtexml-mac"
+      bin.install('blahtexml')
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    This formula installs both blahtex and blahtexml.
+    If you only want to install blahtex, then do:
+      brew install blahtexml --blahtex-only
+    EOS
   end
 end
