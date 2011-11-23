cc9dfe28cd454e7ecebae6e629c1c34271c25b4f
eregon <eregontp@gmail.com>
2011-11-23 12:34:46 +0100

    most: the patch is unneeded, configure has a --with-slang=DIR argument

--- Library/Formula/most.rb
+++ Library/Formula/most.rb
@@ -7,48 +7,9 @@ class Most < Formula
 
   depends_on 's-lang'
 
-  def patches; DATA end
-
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--with-slang=#{HOMEBREW_PREFIX}",
+                          "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
 end
-
-
-# most looks in a few set directories for slang, so at least add our prefix
-__END__
-diff --git a/configure b/configure
-index f24b2f1..9ac109c 100755
---- a/configure
-+++ b/configure
-@@ -9936,13 +9936,8 @@ echo "$as_me: error: --with-slanginc requres a value" >&2;}
-             $jd_prefix_incdir \
-             /usr/local/slang/include \
-             /usr/local/include/slang \
--  	  /usr/local/include \
--  	  /usr/include/slang \
--  	  /usr/slang/include \
--  	  /usr/include \
--  	  /opt/include/slang \
--  	  /opt/slang/include \
--  	  /opt/include"
-+	  /usr/local/include \
-+	  `brew --prefix`/include"
- 
-        for X in $lib_include_dirs
-        do
-@@ -9965,12 +9960,7 @@ echo "$as_me: error: --with-slanginc requres a value" >&2;}
-             /usr/local/lib \
-             /usr/local/lib/slang \
-             /usr/local/slang/lib \
--  	  /usr/lib \
--  	  /usr/lib/slang \
--  	  /usr/slang/lib \
--  	  /opt/lib \
--  	  /opt/lib/slang \
--  	  /opt/slang/lib"
-+	  `brew --prefix`/lib"
- 
-        case "$host_os" in
-          *darwin* )
