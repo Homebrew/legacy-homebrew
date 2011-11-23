a433469ffa3f2c5245a10164937555c63903b63f
eregon <eregontp@gmail.com>
2011-11-23 12:58:04 +0100

    add HEAD build for most

--- Library/Formula/most.rb
+++ Library/Formula/most.rb
@@ -4,6 +4,7 @@ class Most < Formula
   url 'ftp://space.mit.edu/pub/davis/most/most-5.0.0a.tar.bz2'
   homepage 'http://www.jedsoft.org/most/'
   md5 '4c42abfc8d3ace1b0e0062ea021a5917'
+  head 'git://git.jedsoft.org/git/most.git'
 
   depends_on 's-lang'
 
