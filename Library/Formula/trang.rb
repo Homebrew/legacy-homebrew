29f71122419458b26ba0f10916eebb1f9cb6b94f
Trevor Muñoz <trevor.munoz@gmail.com>
2011-11-24 18:42:31 -0700

    Trang 20091111
    Trang is a schema converter for XML. Trang can convert between
    RelaxNG (XML syntax), RelaxNG Compact, XML DTDs, W3C XML
    Schemas.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/trang.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Trang < Formula
+  homepage 'http://code.google.com/p/jing-trang/'
+  url 'http://jing-trang.googlecode.com/files/trang-20091111.zip'
+  md5 '9d31799b948c350850eb9dd14e5b832d'
+
+  def wrapper target; <<-EOS.undent
+    #!/bin/bash
+    java -jar #{libexec}/trang.jar "$@"
+    EOS
+  end
+
+  def install
+    libexec.install Dir["*"]
+    (bin+'trang').write wrapper('trang')
+  end
+end
