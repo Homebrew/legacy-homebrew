8fcbbc7d0272923d3e0caac4de07ffce59357589
Jack Nagel <jacknagel@gmail.com>
2011-11-26 12:10:10 -0600

    doctor: remove terminal width check for curl
    This ended up being noise for some users. We need a better set of steps
    to trigger the bug in order to create a good doctor check; patches
    welcome from users who are encountering this issue.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Homebrew/cmd/doctor.rb
+++ Library/Homebrew/cmd/doctor.rb
@@ -800,21 +800,6 @@ def check_git_version
   end
 end
 
-def check_terminal_width
-  # http://sourceforge.net/tracker/?func=detail&atid=100976&aid=3435710&group_id=976
-  if `tput cols`.chomp.to_i > 262
-    puts <<-EOS.undent
-      Your terminal width is greater than 262 columns.
-
-      This can trigger a segfault in some versions of curl, which may cause
-      downloads to appear to fail.
-
-      You may want to adjust your terminal size.
-
-    EOS
-  end
-end
-
 module Homebrew extend self
   def doctor
     old_stdout = $stdout
@@ -860,7 +845,6 @@ module Homebrew extend self
       check_git_status
       check_for_leopard_ssl
       check_git_version
-      check_terminal_width
     ensure
       $stdout = old_stdout
     end
