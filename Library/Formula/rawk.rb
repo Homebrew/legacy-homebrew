95bc25a9753d407a50542910ccea61a48f57761c
Kyle Isom <coder@kyleisom.net>
2011-11-27 22:36:41 +0300

    add rawk, a static site generator

---
+++ Library/Formula/rawk.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Rawk < Formula
+  homepage 'http://rawk.brokenlcd.net'
+  url 'http://downloads.sourceforge.net/project/rawk-sh/rawk-1.0.tgz'
+  md5 'aa1c4a5c742ae561302fe76b192f1244'
+
+  def install
+    system "make PREFIX=#{prefix}"
+  end
+
+  def remove
+    system "make remove PREFIX=#{prefix}"
+  end
+
+  def test
+    system "false"
+  end
+end
