44845d99d975d02c12ed24264ecabc820b240496
Adam Vandenberg <flangy@gmail.com>
2011-11-27 10:16:52 -0800

    ETL 0.4.14

--- Library/Formula/etl.rb
+++ Library/Formula/etl.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Etl < Formula
-  url 'https://downloads.sourceforge.net/project/synfig/ETL/0.04.13/ETL-0.04.13.tar.gz'
+  url 'http://downloads.sourceforge.net/project/synfig/ETL/0.04.14/ETL-0.04.14.tar.gz'
   homepage 'http://synfig.org'
-  md5 'd52253adeb219d2fa554cbd744dfff29'
+  md5 '7cb91905cbe07af327340aeba2226c06'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
