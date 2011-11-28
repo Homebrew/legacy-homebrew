2cab1397a1e5a6906ce694a4c643f67f48747d8a
Pieter Nicolai <pnicolai@lavabit.com>
2011-11-28 15:59:39 +0100

    lame 3.99.3
    Update LAME to 3.99.3 (stable / minor bugfix)
    Changes: Fix for tracker item [ 3441349 ] --tg does not handle genre number when adding unicode tag.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/lame.rb
+++ Library/Formula/lame.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Lame < Formula
   homepage 'http://lame.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/sourceforge/lame/lame-3.99.2.tar.gz'
-  md5 'd049cc1c6b2be16805d5765cc0b23737'
+  url 'http://downloads.sourceforge.net/sourceforge/lame/lame-3.99.3.tar.gz'
+  md5 '5ad31e33e70455eb3a7b79a5dd934fce'
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}", "--enable-nasm"
