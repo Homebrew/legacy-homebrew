0d60ed0d9a58f7d111b3e6d82ba04039fbd96fde
tizoc <utizoc@gmail.com>
2011-11-28 10:24:16 -0200

    chibi-scheme: add HEAD
    Closes #8839.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/chibi-scheme.rb
+++ Library/Formula/chibi-scheme.rb
@@ -4,6 +4,7 @@ class ChibiScheme < Formula
   url 'http://chibi-scheme.googlecode.com/files/chibi-scheme-0.5.1.tgz'
   homepage 'http://code.google.com/p/chibi-scheme/'
   md5 'c29c4721cd31d6bcafd061dfcf622f46'
+  head 'https://code.google.com/p/chibi-scheme/', :using => :hg
 
   def install
     # "make" and "make install" must be done separately
