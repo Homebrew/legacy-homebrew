2253ce3221d11f5460f25c50ed81e8f8cb037b69
tizoc <utizoc@gmail.com>
2011-11-28 00:28:00 -0200

    chibi-scheme 0.5.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/chibi-scheme.rb
+++ Library/Formula/chibi-scheme.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class ChibiScheme < Formula
-  url 'http://chibi-scheme.googlecode.com/files/chibi-scheme-0.5.tgz'
+  url 'http://chibi-scheme.googlecode.com/files/chibi-scheme-0.5.1.tgz'
   homepage 'http://code.google.com/p/chibi-scheme/'
-  md5 'b7a14bf7827b43c83b56eaaa9254e5e3'
+  md5 'c29c4721cd31d6bcafd061dfcf622f46'
 
   def install
     # "make" and "make install" must be done separately
