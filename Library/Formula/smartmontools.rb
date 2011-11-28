74aa29af474a087706094f51847366bc6664c7b5
Jack Nagel <jacknagel@gmail.com>
2011-11-28 14:53:12 -0600

    smartmontools 5.42
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/smartmontools.rb
+++ Library/Formula/smartmontools.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Smartmontools < Formula
-  url 'http://downloads.sourceforge.net/project/smartmontools/smartmontools/5.41/smartmontools-5.41.tar.gz'
+  url 'http://downloads.sourceforge.net/project/smartmontools/smartmontools/5.42/smartmontools-5.42.tar.gz'
   homepage 'http://sourceforge.net/apps/trac/smartmontools/'
-  md5 '4577886bea79d4ff12bd9a7d323ce692'
+  md5 '4460bf9a79a1252ff5c00ba52cf76b2a'
 
   def install
     (var+'run').mkpath
