48f6bf21418686eb8d882212aa614fa2ce4c3d0b
Tobias Steinhoff <tobias@stonehope.de>
2011-11-29 07:10:15 +0100

    ttytter: Update to 1.2.4
    Closes #8862.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/ttytter.rb
+++ Library/Formula/ttytter.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Ttytter < ScriptFileFormula
-  url 'http://www.floodgap.com/software/ttytter/dist1/1.2.02.txt'
+  url 'http://www.floodgap.com/software/ttytter/dist1/1.2.04.txt'
   homepage 'http://www.floodgap.com/software/ttytter/'
-  md5 '09a4748e2f00a79e2f84e37a42d56076'
+  md5 '6c4b7d42350d4e9511ffc909bd3b4846'
 
   def install
-    bin.install '1.2.02.txt' => 'ttytter'
+    bin.install '1.2.04.txt' => 'ttytter'
   end
 
   def caveats; <<-EOS.undent
