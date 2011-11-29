90abb3321d8d5f4bbde915803e10186e14006819
Ingo Richter <ingo.richter@gmail.com>
2011-11-28 23:37:58 -0800

    - call autoreconf in order to generate a proper configure script

--- Library/Formula/ideviceinstaller.rb
+++ Library/Formula/ideviceinstaller.rb
@@ -14,10 +14,10 @@ class Ideviceinstaller < Formula
 
   def install
     # fix the m4 problem with the missing pkg.m4
-    aclocalDefault = `/usr/bin/aclocal --print-ac-dir`
-    inreplace "autogen.sh", "aclocal -I m4", "aclocal -I m4 -I#{aclocalDefault.strip} -I #{HOMEBREW_PREFIX}/share/aclocal"
+    ENV['LIBTOOLIZE'] = "/usr/bin/glibtoolize"
+    ENV['ACLOCAL'] = "/usr/bin/aclocal -I m4 -I #{HOMEBREW_PREFIX}/share/aclocal"
 
-    system "./autogen.sh"
+    system "autoreconf -ivf"
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
