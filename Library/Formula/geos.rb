3c0fe43b18d6d7ee368ebc26094af023f3e086ea
Nibbles 2bits <nibbles@speakeasy.net>
2011-11-28 17:24:58 -0800

    geos: Fix LLVM compiler errors
    geos 3.3.1 fails_with_llvm due to missing symbols that get optimized out of
    libgeom. This patches the makefile for that library so all objects are compiled
    at -O1 rather than -O3.
    
    Closes #8858.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/geos.rb
+++ Library/Formula/geos.rb
@@ -5,14 +5,14 @@ class Geos < Formula
   homepage 'http://trac.osgeo.org/geos/'
   sha1 '4f89e62c636dbf3e5d7e1bfcd6d9a7bff1bcfa60'
 
-  def skip_clean? path
-    path.extname == '.la'
-  end
-
-  fails_with_llvm "Some symbols are missing during link step."
-
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    # fixes compile error: missing symbols being optimized out using llvm.
+    if ENV.compiler == :llvm then
+      inreplace 'src/geom/Makefile.in', 'CFLAGS = @CFLAGS@', 'CFLAGS = @CFLAGS@ -O1'
+      inreplace 'src/geom/Makefile.in', 'CXXFLAGS = @CXXFLAGS@', 'CXXFLAGS = @CXXFLAGS@ -O1'
+    end
+
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
 end
