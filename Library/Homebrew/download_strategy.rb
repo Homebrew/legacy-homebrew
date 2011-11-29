9ab78e0b5b20d5da61f9f2921ac82a81636cb8dd
Colin Hebert <hebert.colin@gmail.com>
2011-11-29 20:33:20 +0100

    download_strategy.ry: Fix CVSDownloadStrategy
    The FileUtils.cp_r function with a wildcard doesn't copy hidden files (starting with a dot)
    Instead of copying everything selected by *, select all the folder content

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -412,7 +412,7 @@ class CVSDownloadStrategy < AbstractDownloadStrategy
   end
 
   def stage
-    FileUtils.cp_r Dir[@co+"*"], Dir.pwd
+    FileUtils.cp_r Dir[@co+"{.}"], Dir.pwd
 
     require 'find'
     Find.find(Dir.pwd) do |path|
