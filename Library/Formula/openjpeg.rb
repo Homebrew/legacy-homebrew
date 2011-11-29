cc34bbeaf47fb799fd3ebc8f49e04f98e55f45ab
Nibbles 2bits <nibbles@speakeasy.net>
2011-11-28 16:22:56 -0800

    openjpeg fix missing zlib.h on Lion
    Apple removed zlib.h from png.h on Lion, causing missing symbols.
    This patches codec/convert.c to include zlib.h.  They fixed the
    problem upstream in HEAD.  This compiles on Lion and SL using
    every compiler from XCode-4.2 and 4.0.2.

--- Library/Formula/openjpeg.rb
+++ Library/Formula/openjpeg.rb
@@ -8,12 +8,26 @@ class Openjpeg < Formula
 
   head 'http://openjpeg.googlecode.com/svn/trunk/'
 
+  # Head can have an optional dep on little-cms2 for which there is a pull request,
+  # but it's unlikely to be pulled as there is a policy against multiple versions.
+  # If the user adds little-cms2 themselves, installing head will find and use it,
+  # without any changes to the formula.
   depends_on 'cmake' => :build
-  depends_on 'little-cms' => :optional
   depends_on 'libtiff'
+  depends_on 'little-cms' => :optional
 
   def install
-    system "cmake . #{std_cmake_parameters}"
-    system "make install"
+
+    # Fixes missing symbols, as Apple removed zlib.h from png.h on Lion.
+    if MacOS.lion? and not ARGV.build_head?
+      inreplace 'codec/convert.c', '#include <png.h>',
+                                   "#include <png.h>\n#include <zlib.h>"
+    end
+
+    Dir.mkdir 'macbuild'
+    Dir.chdir 'macbuild' do
+      system "cmake #{std_cmake_parameters} .."
+      system "make install"
+    end
   end
 end
