e885803f036d9fe1202d669102f0996eab9a6f8a
Pieter Nicolai <pnicolai@lavabit.com>
2011-11-30 12:07:16 +0100

    qrencode 3.2.0
    Update QRencode to 3.2.0
    Patches for issues #6447 and #6843 are now contained in this release.
    Building is now thread-safe. Changed url, homepage and use sha1.
    
    Closes #8888.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/qrencode.rb
+++ Library/Formula/qrencode.rb
@@ -1,25 +1,18 @@
 require 'formula'
 
 class Qrencode < Formula
-  url 'http://megaui.net/fukuchi/works/qrencode/qrencode-3.1.1.tar.gz'
-  homepage 'http://megaui.net/fukuchi/works/qrencode/index.en.html'
-  md5 'd97f67cbefaf577e6c15923f3cc57b6a'
+  url 'http://fukuchi.org/works/qrencode/qrencode-3.2.0.tar.gz'
+  homepage 'http://fukuchi.org/works/qrencode/index.html.en'
+  sha1 '1c9cf02cc8e79dddc7238cad64b0bf3c48e94210'
 
   depends_on 'pkg-config' => :build
 
   def install
     ENV.x11 # For libpng
 
-    # This may be removable in the next official release.
-    # See: https://github.com/fukuchi/libqrencode/issues/3
-    unless ARGV.build_head?
-      inreplace 'configure', "libpng12", "libpng"
-    end
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
-    ENV.j1 # Install isn't parallel-safe
     system "make install"
   end
 end
