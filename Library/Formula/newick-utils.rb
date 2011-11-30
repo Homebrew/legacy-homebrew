f2684805fbab943bd92dae3403fee4d205a14673
Lance Hepler <nlhepler@gmail.com>
2011-11-30 13:16:49 -0800

    newick-utils 1.6
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/newick-utils.rb
+++ Library/Formula/newick-utils.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class NewickUtils < Formula
-  url 'http://cegg.unige.ch/pub/newick-utils-1.5.0.tar.gz'
+  url 'http://cegg.unige.ch/pub/newick-utils-1.6.tar.gz'
   homepage 'http://cegg.unige.ch/newick_utils'
-  md5 '5f65b0fe30bf9389297616358f01fac7'
+  md5 'ed0bf50f03196cfef3809f5a76804624'
 
   def patches
     # don't bother testing nw_gen, it's known to fail on MacOSX
@@ -26,6 +26,6 @@ end
 __END__
 --- a/tests/test_nw_gen.sh
 +++ b/tests/test_nw_gen.sh
-@@ -106,0 +107,2 @@
+@@ -138,0 +139,2 @@
 +pass=TRUE
 +
