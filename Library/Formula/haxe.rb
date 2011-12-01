eccbd1a8f728ae7b0f829d77f5ee1d40344b0cf9
Justin Donaldson <donaldson@bigml.com>
2011-12-01 10:00:59 -0800

    haxe 2.08
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/haxe.rb
+++ Library/Formula/haxe.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Haxe < Formula
-  url 'http://haxe.org/file/haxe-2.07-osx.tar.gz'
-  version '2.07'
+  url 'http://haxe.org/file/haxe-2.08-osx.tar.gz'
+  version '2.08'
   homepage 'http://haxe.org/'
-  sha1 '0958a13077aedef1b304d4b43b6eb5d6041bdf88'
+  sha1 'e8758ea9155bf27606348d02240c2af1fecee67b'
 
   def install
     bin.install %w(haxe haxedoc haxelib)
@@ -14,7 +14,7 @@ class Haxe < Formula
   def caveats; <<-EOS.undent
     HaXe needs to know how to find its standard library so add this to your
     shell profile:
-      export HAXE_LIBRARY_PATH="`brew --prefix`/share/haxe/std"'
+      export HAXE_LIBRARY_PATH="$(brew --prefix)/share/haxe/std"
     EOS
   end
 end
