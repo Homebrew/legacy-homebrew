34350456a099e0e1d6871d443270ac5a15cb9ace
John Earles <jearles@gmail.com>
2011-12-01 07:01:26 -0500

    Upgrade node to 0.6.3 (includes --without-npm)
    Updated node to 0.6.3.  Added patch to allow --without-npm option.
    Added ohai message and caveat regarding npm installation.

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Node < Formula
-  url 'http://nodejs.org/dist/v0.6.2/node-v0.6.2.tar.gz'
+  url 'http://nodejs.org/dist/v0.6.3/node-v0.6.3.tar.gz'
   head 'https://github.com/joyent/node.git'
   homepage 'http://nodejs.org/'
-  md5 '875431b2c2e3d0ebf5a676b3d96bf766'
+  md5 'e9c72081c2a1141128e53f84dcba3f0e'
 
   # Leopard OpenSSL is not new enough, so use our keg-only one
   depends_on 'openssl' if MacOS.leopard?
@@ -18,13 +18,21 @@ class Node < Formula
     [["--debug", "Build with debugger hooks."]]
   end
 
+  # apply temporary patch until 0.6.4 is available
+  # see: https://github.com/mxcl/homebrew/pull/8784
+  def patches
+    "https://github.com/joyent/node/commit/3ca0517.patch"
+  end 
+
   def install
     inreplace 'wscript' do |s|
       s.gsub! '/usr/local', HOMEBREW_PREFIX
       s.gsub! '/opt/local/lib', '/usr/lib'
     end
 
-    args = ["--prefix=#{prefix}"]
+    ohai "Homebrew is skipping npm install..."
+    
+    args = ["--prefix=#{prefix}", "--without-npm"]
     args << "--debug" if ARGV.include? '--debug'
 
     # v0.6 appears to have a bug in parallel building
@@ -36,6 +44,9 @@ class Node < Formula
   end
 
   def caveats
-    "Please add #{HOMEBREW_PREFIX}/lib/node_modules to your NODE_PATH environment variable to have node libraries picked up."
+    <<-EOS.undent
+        Homebrew has NOT installed npm.  Please use 'curl http://npmjs.org/install.sh | sh' to install.
+        Please add #{HOMEBREW_PREFIX}/lib/node_modules to your NODE_PATH environment variable to have node libraries picked up.
+    EOS
   end
 end
