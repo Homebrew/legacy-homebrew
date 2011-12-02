dcb42edda07fd9f98d2770559eef5baf04f53efd
Nibbles 2bits <nibbles@speakeasy.net>
2011-12-01 21:35:24 -0800

    xmoto: upgrade to 0.5.9 include zlib.h ENV.x11 OpenGL
    This patches tim_png.cpp to include zlib.h on Lion.  This
    builds and runs well on Lion and Snow Lep with all compilers.
    
    EDIT: Upgrade to 0.5.9.  Add ENV.x11 to pick up png and set
    CFLAGS and LDFLAGS, enable apple-opengl-framework, remove
    appends that are now satisfied.
    
    This would fix #8883

--- Library/Formula/xmoto.rb
+++ Library/Formula/xmoto.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Xmoto < Formula
-  url 'http://download.tuxfamily.org/xmoto/xmoto/0.5.7/xmoto-0.5.7-src.tar.gz'
+  url 'http://download.tuxfamily.org/xmoto/xmoto/0.5.9/xmoto-0.5.9-src.tar.gz'
   homepage 'http://xmoto.tuxfamily.org/'
-  md5 'c4b8477412445e114843b3b6163937f3'
+  sha1 '07757accce78151dc8873bef8270df0e56196772'
 
+  depends_on 'libxml2'
   depends_on 'gettext'
   depends_on 'jpeg'
   depends_on 'sdl'
@@ -15,9 +16,15 @@ class Xmoto < Formula
   depends_on 'ode'
 
   def install
-    ENV.append "CPPFLAGS", "-I#{include} -I/usr/X11/include"
-    ENV.append "LDFLAGS", "-framework OpenGL -framework Cocoa -L#{lib} -L/usr/X11/lib -lstdc++ -lSDLmain -lSDL -lSDL_mixer -lpng"
-    system "./configure", "--prefix=#{prefix}", "--with-internal-xdg=1"
+    # The ENV.x11 picks up libpng and sets CFLAGS and LDFLAGS
+    ENV.x11
+    # Fixes missing symbols due to png.h v1.5 not including zlib.h
+    if MacOS.lion? then
+      inreplace 'src/image/tim_png.cpp',
+                  '#include <string.h>',
+                  "#include <string.h>\n#include <zlib.h>"
+    end
+    system "./configure", "--prefix=#{prefix}", "--with-internal-xdg=1", "--with-apple-opengl-framework"
     system "make install"
   end
 end
