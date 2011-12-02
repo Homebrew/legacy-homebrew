f897bebee85a3b27d1c5f21bec4bf5995ace95ac
Phil Harvey <philharveyx@gmail.com>
2011-12-01 21:02:46 -0600

    Apache Cassandra 1.0.5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/cassandra.rb
+++ Library/Formula/cassandra.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Cassandra < Formula
-  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/1.0.3/apache-cassandra-1.0.3-bin.tar.gz'
+  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/1.0.5/apache-cassandra-1.0.5-bin.tar.gz'
   homepage 'http://cassandra.apache.org'
-  md5 '87ab7f116296e4be993a767531b73efa'
+  sha1 'e9b06bd3d9173babfc406593d586599eb96c72e6'
 
   def install
     (var+"lib/cassandra").mkpath
