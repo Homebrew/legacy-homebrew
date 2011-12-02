ef5509ef9614763b38a13e59ce1db9f0d8760071
Jack Nagel <jacknagel@gmail.com>
2011-12-02 14:36:34 -0600

    git 1.7.8
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -1,18 +1,18 @@
 require 'formula'
 
 class GitManuals < Formula
-  url 'http://git-core.googlecode.com/files/git-manpages-1.7.7.4.tar.gz'
-  sha1 'cb21e55ae793865453c165a0e666348f2db8c740'
+  url 'http://git-core.googlecode.com/files/git-manpages-1.7.8.tar.gz'
+  sha1 '93315f7f51d7f27d3e421c9b0d64afa27f3d16df'
 end
 
 class GitHtmldocs < Formula
-  url 'http://git-core.googlecode.com/files/git-htmldocs-1.7.7.4.tar.gz'
-  sha1 '6012cb017a04ded85c48ca5510f741e98c02f671'
+  url 'http://git-core.googlecode.com/files/git-htmldocs-1.7.8.tar.gz'
+  sha1 '2734079e22a0a6e3e78779582be9138ffc7de6f7'
 end
 
 class Git < Formula
-  url 'http://git-core.googlecode.com/files/git-1.7.7.4.tar.gz'
-  sha1 '5b6920989480a37ec65977e756b24961578795dd'
+  url 'http://git-core.googlecode.com/files/git-1.7.8.tar.gz'
+  sha1 '7453e737e008f7319a5eca24a9ef3c5fb1f13398'
   homepage 'http://git-scm.com'
 
   depends_on 'pcre' if ARGV.include? '--with-pcre'
