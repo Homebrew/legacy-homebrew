6ecb1aade5cf991ab2dc3f590222abeb3816657c
Adam Guthrie <asguthrie@gmail.com>
2011-12-03 20:55:09 +0000

    byobu 4.51

--- Library/Formula/byobu.rb
+++ Library/Formula/byobu.rb
@@ -1,12 +1,38 @@
 require 'formula'
 
 class Byobu < Formula
-  url 'http://launchpad.net/byobu/trunk/4.30/+download/byobu_4.30.orig.tar.gz'
+  url 'http://launchpad.net/byobu/trunk/4.51/+download/byobu_4.51.orig.tar.gz'
   homepage 'http://launchpad.net/byobu'
-  md5 'fc2852d1d1cf68282af0a24a680b87f6'
+  md5 '4786cbff4149f3eb9b8815474a63af2e'
+  depends_on 'gnu-sed'
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
+  end
+
+  def caveats
+    version_match = '##VERSION_NUMBER##'
+    prefix_match = prefix.sub(version, version_match)
+
+    <<-EOS.undent
+    You will need to set BYOBU_PREFIX to Homebrew's install path before 
+    running byobu.
+
+    Something like:
+
+      echo 'export BYOBU_PREFIX=`brew --prefix`' >> $HOME/.profile
+
+    If upgrading from 4.30 or earlier, you may need to tweak the files in your 
+    $HOME/.byobu directory to remove hardcoded references to this 
+    formula's prefix.
+
+    Something like, replacing #{version_match} with your previously installed 
+    version number before running:
+
+      find $HOME/.byobu -type f -print0 | \\
+        xargs -0 sed -e 's@#{prefix_match}@#{HOMEBREW_PREFIX}@g' -i ''
+
+    EOS
   end
 end
