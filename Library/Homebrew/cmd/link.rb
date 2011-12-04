e70fbcf504b6c01a86ebd79e66a05e59ac13ba47
Dylan Smith <Dylan.Smith@shopify.com>
2011-12-03 22:53:00 -0500

    Allow linking with multiple keg versions installed.

--- Library/Homebrew/cmd/link.rb
+++ Library/Homebrew/cmd/link.rb
@@ -1,6 +1,17 @@
 module Homebrew extend self
   def link
-    ARGV.kegs.each do |keg|
+    require 'keg'
+    kegs = ARGV.formulae.map do |f|
+      path = f.installed_prefix
+      unless path.directory?
+        dirs = f.rack.children.select(&:directory?) rescue []
+        raise NoSuchKegError.new(name) if dirs.length == 0
+        raise MultipleVersionsInstalledError.new(name) if dirs.length > 1
+        path = dirs.first
+      end
+      keg = Keg.new path
+    end
+    kegs.each do |keg|
       print "Linking #{keg}... "
       begin
         puts "#{keg.link} symlinks created"
