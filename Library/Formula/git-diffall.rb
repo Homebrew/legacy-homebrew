8ac42980d46b0789b2f2ec3e011b2b69fec6bac4
Matt McClure <matt.mcclure@mapmyfitness.com>
2011-12-04 21:43:41 -0500

    Uses the explicit commit hash of today's head commit.

--- Library/Formula/git-diffall.rb
+++ Library/Formula/git-diffall.rb
@@ -1,24 +1,16 @@
 require 'formula'
 
 class GitDiffall < Formula
-  url 'https://github.com/thenigan/git-diffall.git'
-  head 'https://github.com/thenigan/git-diffall.git'
-  version 'head'
+  url 'https://github.com/thenigan/git-diffall/tarball/ac986bb784873044e81a4b5e0a6353793bed8acc'
+  version 'ac986bb784873044e81a4b5e0a6353793bed8acc'
   homepage 'https://github.com/thenigan/git-diffall'
-  md5 ''
-
-  # depends_on 'git'
+  md5 '5610232d3b014c55ed964537917f79ee'
 
   def install
     bin.install('git-diffall')
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test git-diffall`. Remove this comment before submitting
-    # your pull request!
     system "test -x /usr/local/bin/git-diffall"
   end
 end
