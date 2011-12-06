abce5d546a3cd2b8357c6010ddf45d4e75232aba
Ingo Richter <ingo.richter@gmail.com>
2011-12-06 00:19:27 -0800

    - fix the issue with changed md5 checksum with each download. Now doanloading from /downloads/ instead from /snapshots/

--- Library/Formula/ideviceinstaller.rb
+++ Library/Formula/ideviceinstaller.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Ideviceinstaller < Formula
-  url 'http://cgit.sukimashita.com/ideviceinstaller.git/snapshot/ideviceinstaller-1.0.0.tar.gz'
-  md5 '7f35a8bb0e620db23931af3fff816bef'
+  url 'http://cgit.sukimashita.com/ideviceinstaller.git/downloads/ideviceinstaller-1.0.0.tar.gz'
+  md5 '85bca8024d385c40fef477f33fefab5d'
   head 'http://cgit.sukimashita.com/ideviceinstaller.git', :using => :git
   homepage 'http://www.sukimashita.com/'
+  version '1.0.0'
 
   depends_on 'pkg-config' => :build
   depends_on 'libimobiledevice'
@@ -14,10 +15,11 @@ class Ideviceinstaller < Formula
 
   def install
     # fix the m4 problem with the missing pkg.m4
-    ENV['LIBTOOLIZE'] = "/usr/bin/glibtoolize"
-    ENV['ACLOCAL'] = "/usr/bin/aclocal -I m4 -I #{HOMEBREW_PREFIX}/share/aclocal"
 
-    system "autoreconf -ivf"
+    aclocalDefault = `/usr/bin/aclocal --print-ac-dir`
+    inreplace "autogen.sh", "aclocal -I m4", "aclocal -I m4 -I#{aclocalDefault.strip} -I #{HOMEBREW_PREFIX}/share/aclocal"
+
+    system "./autogen.sh"
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
