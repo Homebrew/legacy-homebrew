05989d8fc3ae87794287e9352696c2b3486d7aa9
Robin Heggelund Hansen <skinneyz89@gmail.com>
2011-12-07 07:03:59 +0100

    Updated vala to 0.14.0

--- Library/Formula/vala.rb
+++ Library/Formula/vala.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Vala < Formula
+  version '0.14.0'
   head 'git://git.gnome.org/vala'
-  url 'http://download.gnome.org/sources/vala/0.11/vala-0.11.7.tar.bz2'
+  url 'http://download.gnome.org/sources/vala/0.14/vala-0.14.0.tar.bz2'
   homepage 'http://live.gnome.org/Vala'
-  md5 '5515f32552ee45ed5c7541c119009caa'
+  sha256 '4cbca602e2e2a09803ae33fd7324219bc2c611db5a62a52e733e7d8806acb6f5'
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
