ef97e18ed97e4a02ff3a6459138bee79dd97fd47
Alexandre Fiori <fiorix@gmail.com>
2011-12-07 16:28:56 -0500

    patch for libav 0.7 support

--- Library/Formula/mediatomb.rb
+++ Library/Formula/mediatomb.rb
@@ -5,8 +5,13 @@ class Mediatomb < Formula
   homepage 'http://mediatomb.cc'
   md5 'e927dd5dc52d3cfcebd8ca1af6f0d3c2'
 
+  # This is for libav 0.7 support.
+  # See https://bugs.launchpad.net/ubuntu/+source/mediatomb/+bug/784431 for details.
+  def patches
+    "https://launchpadlibrarian.net/71985647/libav_0.7_support.patch"
+  end
+  
   def install
-    system "curl https://launchpadlibrarian.net/71985647/libav_0.7_support.patch|patch -p1"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
