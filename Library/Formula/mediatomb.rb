9416a3f19b0626de58bf17d155b13c580a65c398
Alexandre Fiori <fiorix@gmail.com>
2011-12-07 01:36:31 -0500

    Bugfix for supporting current libav. See https://bugs.launchpad.net/ubuntu/+source/mediatomb/+bug/784431 for details.

--- Library/Formula/mediatomb.rb
+++ Library/Formula/mediatomb.rb
@@ -6,6 +6,7 @@ class Mediatomb < Formula
   md5 'e927dd5dc52d3cfcebd8ca1af6f0d3c2'
 
   def install
+    system "curl https://launchpadlibrarian.net/71985647/libav_0.7_support.patch|patch -p1"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
