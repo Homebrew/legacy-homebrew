d5fb16e7572550b8dbe33cfcc9de653b6d18e609
Teddy Brown <teddy@chemicalfusion.net>
2011-12-07 15:26:52 -0500

    Fix moon-buggy formula

--- Library/Formula/moon-buggy.rb
+++ Library/Formula/moon-buggy.rb
@@ -5,18 +5,10 @@ class MoonBuggy < Formula
   homepage 'http://www.seehuhn.de/pages/moon-buggy'
   md5 '4da97ea40eca686f6f8b164d8b927e38'
 
-<<<<<<< HEAD
-  def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-=======
-  # depends_on 'cmake'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake . #{std_cmake_parameters}"
->>>>>>> f55499592e6da49f9cb5df09e9348d026aa34fc7
     system "make install"
   end
 
