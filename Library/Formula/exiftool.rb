5c137c2a898998c37d542615eccd6ff1abd3a09e
Misty De Meo <mistydemeo@gmail.com>
2011-12-08 09:25:34 -0600

    exiftool 8.72
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/exiftool.rb
+++ Library/Formula/exiftool.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Exiftool < Formula
-  url 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-8.71.tar.gz'
+  url 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-8.72.tar.gz'
   homepage 'http://www.sno.phy.queensu.ca/~phil/exiftool/index.html'
-  md5 '9bc316b3270d95f069188c93f79bcd58'
+  md5 '9ed54dc1b981188e34013911ce4e24f5'
 
   def install
     system "perl", "Makefile.PL"
