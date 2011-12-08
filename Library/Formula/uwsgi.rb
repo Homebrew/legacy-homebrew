39026fa103e03596e988f83291631551f692d016
Krzysztof Urbaniak <urban@fail.pl>
2011-12-09 00:21:39 +0100

    uwsgi 0.9.9.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Uwsgi < Formula
-  url 'http://projects.unbit.it/downloads/uwsgi-0.9.9.2.tar.gz'
+  url 'http://projects.unbit.it/downloads/uwsgi-0.9.9.3.tar.gz'
   homepage 'http://projects.unbit.it/uwsgi/'
-  md5 'fafecf7dc86a36f003d2fadaa44483f1'
+  md5 'bbf4ac40901fb9d26c8286e7637b77f5'
 
   def install
     # Find the arch for the Python we are building against.
