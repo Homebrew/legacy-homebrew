96596d6a76eb11870bcb103e74b7ce19333314d5
Daniel Berjón <asuranceturix@gmail.com>
2011-12-08 19:18:05 +0100

    libmpdclient 2.6
    Closes #9057.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libmpdclient.rb
+++ Library/Formula/libmpdclient.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libmpdclient < Formula
-  url 'http://downloads.sourceforge.net/project/musicpd/libmpdclient/2.5/libmpdclient-2.5.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/musicpd/libmpdclient/2.6/libmpdclient-2.6.tar.bz2'
   homepage 'http://mpd.wikia.com/wiki/ClientLib:libmpdclient'
-  sha1 '4e3c0925c92c27ddcb13113adc7ebe6dc975abc6'
+  sha1 '00d88152c0e8599f56d01625da41b55d3a43c264'
 
   def options
     [[ '--universal', 'Build a universal library.' ]]
