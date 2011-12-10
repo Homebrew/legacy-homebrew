d628e96761956b1b4123fcc8d162f3c354e16688
Rui Pereira <rui.pereira@gmail.com>
2011-12-10 17:17:24 +0100

    gv 3.7.3.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gv.rb
+++ Library/Formula/gv.rb
@@ -1,20 +1,15 @@
 require 'formula'
 
 class Gv < Formula
-  url 'http://ftpmirror.gnu.org/gv/gv-3.7.2.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gv/gv-3.7.2.tar.gz'
+  url 'http://ftpmirror.gnu.org/gv/gv-3.7.3.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/gv/gv-3.7.3.tar.gz'
   homepage 'http://www.gnu.org/s/gv/'
-  md5 'eb47d465755b7291870af66431c6f2e1'
+  md5 '98ae3e9ce338b64ba5ab622389c5960e'
 
   depends_on 'ghostscript'
   depends_on 'xaw3d'
 
   skip_clean 'share/gv/safe-gs-workdir'
-
-  def patches
-    # apply MacPorts typedef patch
-    { :p0 => "https://trac.macports.org/export/83941/trunk/dports/print/gv/files/patch-src-callbacks.c.diff" }
-  end
 
   def install
     ENV.x11
