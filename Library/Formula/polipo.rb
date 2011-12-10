0da8b86495db88b6b2e90eae8a4e15a2a5597bb6
Peter Hollows <peter.hollows@gmail.com>
2011-12-11 02:27:20 +1100

    polipo: Add sample launchd plist
    Add sample launchd plist and instructions in
    caveat for automatic startup/shutdown.

--- Library/Formula/polipo.rb
+++ Library/Formula/polipo.rb
@@ -12,5 +12,39 @@ class Polipo < Formula
     make_opts = "PREFIX=#{prefix} LOCAL_ROOT=#{share}/polipo/www DISK_CACHE_ROOT=#{cache_root} MANDIR=#{man} INFODIR=#{info} PLATFORM_DEFINES=-DHAVE_IPv6"
     system "make all #{make_opts}"
     system "make install #{make_opts}"
+
+    (prefix + 'fr.jussieu.pps.polipo.plist').write startup_plist
+    (prefix + 'fr.jussieu.pps.polipo.plist').chmod 0644
+  end
+
+  def startup_plist
+    return <<-EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>fr.jussieu.pps.polipo</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <true/>
+    <key>UserName</key>
+    <string>#{`whoami`.chomp}</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>#{bin}/polipo</string>
+    </array>
+  </dict>
+</plist>
+    EOPLIST
+  end
+
+  def caveats; <<-EOS.undent
+    You can start polipo automatically on login with:
+      mkdir -p ~/Library/LaunchAgents
+      cp #{prefix}/fr.jussieu.pps.polipo.plist ~/Library/LaunchAgents/
+      launchctl load -w ~/Library/LaunchAgents/fr.jussieu.pps.polipo.plist
+    EOS
   end
 end
