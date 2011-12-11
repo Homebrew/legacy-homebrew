9948598e5262ead9304e8705c000223e86791401
Walter McGinnis <walter@katipo.co.nz>
2011-12-11 19:42:33 +1300

    Taking out unnecessary depends_on libxml and comment.

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -1,14 +1,10 @@
 require 'formula'
 
-# currently the dependency on brew's version of libxml2 is necessary
-# to avoid this: https://gist.github.com/1458566
-
 class Yaz < Formula
   url 'http://ftp.indexdata.dk/pub/yaz/yaz-4.2.22.tar.gz'
   homepage 'http://www.indexdata.com/yaz'
   md5 'b28ecda4073a950a1f915829475e5238'
 
-  depends_on 'libxml2'
   depends_on 'pkg-config' => :build
 
   def install
