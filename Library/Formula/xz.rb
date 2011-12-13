97cb210f43d3959779b7109c0770b21eb51d5952
Douglas Creager <douglas.creager@redjack.com>
2011-12-13 17:02:46 -0500

    xz: add --universal
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -5,7 +5,12 @@ class Xz < Formula
   url 'http://tukaani.org/xz/xz-5.0.3.tar.bz2'
   sha256 '5a11b9e17bfcda62319c5a8c4a2062dc81607a316d3f6adff89422d81ec1eae9'
 
+  def options
+    [["--universal", "Build a universal binary."]]
+  end
+
   def install
+    ENV.universal_binary if ARGV.build_universal?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
