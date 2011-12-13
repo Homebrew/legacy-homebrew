8b8f4e51775459af9c9928ade028e1d0037f7a67
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-12-13 21:54:20 +0100

    yarp 2.3.14
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yarp.rb
+++ Library/Formula/yarp.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Yarp < Formula
-  url 'http://downloads.sourceforge.net/yarp0/yarp-2.3.8.tar.gz'
-  md5 '5a932488be65aa4318f5c7f4aaf9ff50'
+  url 'http://downloads.sourceforge.net/yarp0/yarp-2.3.14.tar.gz'
+  md5 '4ee659e31abe915e3453062f994aabd7'
   homepage 'http://yarp.it'
   head 'https://yarp0.svn.sourceforge.net/svnroot/yarp0/trunk/yarp2'
 
@@ -11,9 +11,11 @@ class Yarp < Formula
   depends_on 'ace'
   depends_on 'gsl'
   depends_on 'gtk+'
+  depends_on 'sqlite'
+  depends_on 'readline'
 
   def install
-    system "cmake . #{std_cmake_parameters} -DCREATE_LIB_MATH=TRUE -DCREATE_GUIS=TRUE -DCREATE_YARPSERVER3=TRUE"
+    system "cmake . #{std_cmake_parameters} -DCREATE_LIB_MATH=TRUE -DCREATE_GUIS=TRUE -DCREATE_YMANAGER=TRUE -DYARP_USE_SYSTEM_SQLITE=TRUE"
     system "make install"
   end
 end
