03ece61dd5a153f9306f937a63017dd2e137d1df
brada <brad.atcheson@gmail.com>
2011-12-12 22:34:05 -0800

    adol-c: style fixes to formula

--- Library/Formula/adol-c.rb
+++ Library/Formula/adol-c.rb
@@ -5,7 +5,7 @@ class AdolC < Formula
   homepage 'https://projects.coin-or.org/ADOL-C'
   head 'https://projects.coin-or.org/svn/ADOL-C/trunk/', :using => :svn
   md5 '5fe149865b47f77344ff910702da8b99'
-  # HEAD contains bugfix causing NaN to appear in 2.2.1
+  # HEAD contains bugfix for NaNs appearing in 2.2.1
   # http://answerpot.com/showthread.php?2997935-sparse_jac+return+unreasonable+NaN's
 
   depends_on 'aardvark_shell_utils' => :build  # for realpath
@@ -17,9 +17,8 @@ class AdolC < Formula
         s.gsub! "lib64", "lib"
     end
     system "./configure", "--prefix=#{prefix}", "--enable-sparse",
-                          "--with-colpack="+`brew --prefix colpack`
+                          "--with-colpack=#{HOMEBREW_PREFIX}"
     system "make install"
-    # use "brew -v install adol-c" to see the test results
     system "make test"
   end
 end
