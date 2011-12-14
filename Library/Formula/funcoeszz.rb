0b31e583fca622491e4bcb5f0312019363c4644f
Jayson Reis <santosdosreis@gmail.com>
2011-12-13 22:21:31 -0200

    fixed link

--- Library/Formula/funcoeszz.rb
+++ Library/Formula/funcoeszz.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Funcoeszz < Formula
   url 'http://funcoeszz.googlecode.com/files/funcoeszz-10.12.sh'
-  homepage 'http://funcoeszz.googlecode.com/f'
+  homepage 'http://funcoeszz.googlecode.com/'
   md5 '93f69412f97a38ecb80493279440b5f0'
   version '10.12'
 
