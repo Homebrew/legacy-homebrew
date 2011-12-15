6f523bdefd19b85d507a3611af35385653a188c1
Lars-Olof Moilanen <lars.olof.moilanen@gmail.com>
2011-12-15 23:02:32 +0100

    shairport formula =)

---
+++ Library/Formula/shairport.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Shairport < Formula
+  head 'https://github.com/albertz/shairport.git'
+  homepage 'https://github.com/albertz/shairport'
+
+  depends_on 'libao'
+
+  def install
+    #system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    #                      "--prefix=#{prefix}"
+    system "mkdir #{bin}"
+    inreplace 'Makefile' do |s|
+      s.change_make_var! 'prefix', prefix
+      s.gsub! 'install -D', 'install'
+    end
+    system "make install"
+  end
+
+  def test
+    system "shairport --help"
+  end
+end
