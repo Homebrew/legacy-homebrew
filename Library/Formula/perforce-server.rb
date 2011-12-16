9326c193fe754b86db89027946194f90521ccde4
Robert Shaw <robert914@me.com>
2011-12-16 13:37:26 -0800

    perforce-server: update to v2011.1.370818
    Closes #9145.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/perforce-server.rb
+++ Library/Formula/perforce-server.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class PerforceServer < Formula
-  url 'http://filehost.perforce.com/perforce/r10.2/bin.darwin90u/p4d'
+  url 'http://filehost.perforce.com/perforce/r11.1/bin.darwin90u/p4d'
   homepage 'http://www.perforce.com/'
-  md5 '4603399cbbb0c128a24742ac7fcd14d9'
-  version '2010.2.347035'
+  md5 'e431cca5a3ee56815794b682db4adad3'
+  version '2011.1.370818'
 
   def install
     bin.install 'p4d'
