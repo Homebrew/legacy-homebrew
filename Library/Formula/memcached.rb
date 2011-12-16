cb0cb40213a8363d2ef91b713abced259f14d4f9
Eric DeLabar <edelabar@gmail.com>
2011-12-16 15:04:05 -0500

    Added --enable-sasl-pwdb configure parameter to enable SASL2 authentication on an OSX Lion (10.7) machine.  Details: http://code.google.com/p/memcached/wiki/ReleaseNotes145

--- Library/Formula/memcached.rb
+++ Library/Formula/memcached.rb
@@ -10,12 +10,14 @@ class Memcached < Formula
   def options
     [
       ["--enable-sasl", "Enable SASL support -- disables ASCII protocol!"],
+      ["--enable-sasl-pwdb", "Enable SASL with memcached's own plain text password db support -- disables ASCII protocol!"],
     ]
   end
 
   def install
     args = ["--prefix=#{prefix}"]
     args << "--enable-sasl" if ARGV.include? "--enable-sasl"
+    args << "--enable-sasl-pwdb" if ARGV.include? "--enable-sasl-pwdb"
 
     system "./configure", *args
     system "make install"
