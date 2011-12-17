544b184263300647b8353a1d01b4ec4f111ec010
Sid Karunaratne <sakaru@gmail.com>
2011-12-17 23:12:24 +0000

    added patch to fix compilation errors caused by deprecated constants, switched to bz2 archive

--- Library/Formula/gtksourceview.rb
+++ Library/Formula/gtksourceview.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gtksourceview < Formula
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gtksourceview/2.11/gtksourceview-2.11.2.tar.gz'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gtksourceview/2.11/gtksourceview-2.11.2.tar.bz2'
   homepage 'http://projects.gnome.org/gtksourceview/'
-  md5 '7c4bbdc1d1628932362b4f222e80afd4'
+  sha256 'f9594d8f18f6bcc3d72da4051636d7a8b4d3f6d29d6827309b262c9483644994'
 
   depends_on 'pkg-config' => :build
   depends_on 'intltool'
@@ -14,5 +14,10 @@ class Gtksourceview < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
+  end
+  
+  def patches
+  # fixes deprecated constants
+  "https://raw.github.com/gist/1491725/5f86e4d4e2245f01876ada1023d55e0366abd5dc/gtksourceview-const.patch"
   end
 end
