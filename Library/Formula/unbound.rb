ea33135c4e3469fef0ee9e1d67eb1c13d14b5303
Anand Buddhdev <arhbkb@gmail.com>
2011-12-21 14:08:47 +0100

    unbound 1.4.14
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/unbound.rb
+++ Library/Formula/unbound.rb
@@ -1,15 +1,14 @@
 require 'formula'
 
 class Unbound < Formula
-  url 'http://www.unbound.net/downloads/unbound-1.4.13.tar.gz'
+  url 'http://www.unbound.net/downloads/unbound-1.4.14.tar.gz'
   homepage 'http://www.unbound.net'
-  md5 '7e3b27dee2b97640dd2e1783253317ab'
+  md5 'cd69fdaaa6af01ea0b6fbc59802f74ba'
 
   depends_on 'ldns'
 
   def install
-    system "./configure", "--disable-gost", "--disable-sha2", "--with-ssl-optional",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-gost", "--prefix=#{prefix}"
     system "make install"
   end
 end
