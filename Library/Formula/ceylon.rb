dcb64c8303b5226ef4705b23f70d65a596bde81d
Julien Ponge <julien.ponge@gmail.com>
2011-12-22 19:10:24 +0100

    Formula for the Ceylon language.
    Signed-off-by: Julien Ponge <julien.ponge@gmail.com>

---
+++ Library/Formula/ceylon.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Ceylon < Formula
+  url 'http://ceylon-lang.org/download/dist/1_0_Milestone1'
+  homepage 'http://ceylon-lang.org/'
+  md5 '627ebfc52fc9ba93fc63df59f8309509'
+
+  def install
+    rm_f Dir["bin/*.bat"]
+
+    doc.install Dir['doc/*']
+    libexec.install Dir['*']
+
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each { |f| ln_s f, bin }
+  end
+end
