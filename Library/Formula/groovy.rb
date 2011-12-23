94fb0e3e486bffe141806ce87fd6af7a19b876f4
Ealden Esto E. Escanan <ealden@gmail.com>
2011-12-24 01:13:57 +0800

    Groovy 1.8.5
    Closes #9256.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Groovy < Formula
   homepage 'http://groovy.codehaus.org/'
-  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-1.8.4.zip'
-  md5 '3ecfe663fcdc1d9b54b16e5eabe54c90'
+  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-1.8.5.zip'
+  md5 '0c8759563be35e26fcebff240de4ca21'
 
   def install
     rm_f Dir["bin/*.bat"]
