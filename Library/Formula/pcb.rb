412eba049704db11735087b2f2c9c1a2f5fd5288
Dmitry Nedospasov <dmitry@nedos.net>
2011-12-26 13:37:18 +0100

    pcb: update to latest snapshot

--- Library/Formula/pcb.rb
+++ Library/Formula/pcb.rb
@@ -1,15 +1,18 @@
 require 'formula'
 
 class Pcb < Formula
-  url 'http://geda.seul.org/dist/pcb-20100929.tar.gz'
+  url 'wget http://downloads.sourceforge.net/project/pcb/pcb/pcb-20110918/pcb-20110918.tar.gz'
   homepage 'http://pcb.gpleda.org/'
-  md5 '4c71f5d1c40ad65539957748b88eb863'
+  sha1 '53ca27797d4db65a068b56f157e3ea6c5c29051f'
 
   depends_on 'gtk+'
   depends_on 'gd'
   depends_on 'gettext'
   depends_on 'd-bus'
   depends_on 'intltool'
+  depends_on 'gtkglext'
+
+  def patches; DATA; end
 
   def install
     # Help configure find libraries
@@ -33,3 +36,18 @@ class Pcb < Formula
     "This software runs under X11."
   end
 end
+
+__END__
+diff --git a/src/hid/common/hidgl.c b/src/hid/common/hidgl.c
+index 21edb5d..7101bef 100644
+--- a/src/hid/common/hidgl.c
++++ b/src/hid/common/hidgl.c
+@@ -66,6 +66,8 @@
+ #include <dmalloc.h>
+ #endif
+ 
++// compatibility typedef
++typedef GLvoid (*_GLUfuncptr)();
+ 
+ triangle_buffer buffer;
+ float global_depth = 0;
