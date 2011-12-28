bda45d86470dd5a36d9053e62496076ab9c22c01
Jack Nagel <jacknagel@gmail.com>
2011-12-28 15:21:36 -0600

    git: remove hardlink cruft
    I'm not sure if this was every really necessary, but at least in current
    git, these things are already hardlinks to the main git binary. From a
    non-Homebrew build:
    
        $ ls -i1
        53142021 git
        53142026 git-cvsserver
        53142021 git-receive-pack
        53142025 git-shell
        53142021 git-upload-archive
        53142022 git-upload-pack
        53142116 gitk
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -58,17 +58,6 @@ class Git < Formula
     # Some people like the stuff in the contrib folder
     (share+'git').install 'contrib'
 
-    # These files are exact copies of the git binary, so like the contents
-    # of libexec/git-core lets hard link them.
-    # I am assuming this is an overisght by the git devs.
-    git_md5 = (bin+'git').md5
-    %w[git-receive-pack git-upload-archive].each do |fn|
-      fn = bin + fn
-      next unless git_md5 == fn.md5
-      fn.unlink
-      fn.make_link bin+'git'
-    end
-
     # We could build the manpages ourselves, but the build process depends
     # on many other packages, and is somewhat crazy, this way is easier.
     GitManuals.new.brew { man.install Dir['*'] }
