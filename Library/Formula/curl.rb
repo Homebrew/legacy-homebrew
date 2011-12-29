259346315253d5902003b97a814828669052fcbc
Jack Nagel <jacknagel@gmail.com>
2011-12-28 19:52:22 -0600

    curl 7.23.1
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Curl < Formula
   homepage 'http://curl.haxx.se/'
-  url 'http://curl.haxx.se/download/curl-7.22.0.tar.bz2'
-  sha256 'bda0da862322b122784f5a9d7a65efdd99a6061292cfb8e9357e0c67c03e5112'
+  url 'http://curl.haxx.se/download/curl-7.23.1.tar.bz2'
+  sha256 '7d8c8cef8f244118245c37853cb175bd95c156ff453730ec2c88189b05a7c06e'
 
   keg_only :provided_by_osx,
             "The libcurl provided by Leopard is too old for CouchDB to use."
