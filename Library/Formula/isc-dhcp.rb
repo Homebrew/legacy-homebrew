c7d5b76b0eefe3c0622c6b8a4e9d102bd43cdaca
Adam Vandenberg <flangy@gmail.com>
2011-12-31 08:48:51 -0800

    isc-dhcp: fix version

--- Library/Formula/isc-dhcp.rb
+++ Library/Formula/isc-dhcp.rb
@@ -3,6 +3,7 @@ require 'formula'
 class IscDhcp < Formula
   url 'ftp://ftp.isc.org/isc/dhcp/4.2.3-P1/dhcp-4.2.3-P1.tar.gz'
   homepage 'http://www.isc.org/software/dhcp'
+  version '4.2.3-P1'
   sha256 '505c5754526ba92669bfd64daa8b529c6694c4528b45c3f35743c6fd4bd72897'
 
   def install
