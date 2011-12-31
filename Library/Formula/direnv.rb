f38fac66581cdaeb99d007f2c941adf72321022b
Jonas Pfenniger <jonas@pfenniger.name>
2011-12-31 22:34:25 +0100

    direnv v0.1.109
    Closes #9352.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/direnv.rb
+++ Library/Formula/direnv.rb
@@ -1,18 +1,14 @@
 require 'formula'
 
 class Direnv < Formula
-  homepage 'https://github.com/zimbatm/direnv'
-  url 'https://github.com/zimbatm/direnv/tarball/v0.1.81'
-  md5 '9429548fbf48ec56ff1d11bf601ed649'
+  homepage 'http://www.direnv.net'
+  url 'https://github.com/zimbatm/direnv/tarball/v0.1.109'
+  md5 'a3ddab0323037533be3b942683188742'
 
   head 'https://github.com/zimbatm/direnv.git'
 
   def install
-    # App and support files live in libexec
-    libexec.install Dir['libexec/*']
-    # Symlink into bin
-    bin.mkpath
-    ln_s libexec+'direnv', bin+'direnv'
+    system "make install DESTDIR=#{prefix}"
   end
 
   def caveats
