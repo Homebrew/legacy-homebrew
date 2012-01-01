fa31edbc0c9dd4601792c22825f39a1edea87713
Hiroaki Nakamura <hnakamur@gmail.com>
2012-01-01 13:19:57 +0900

    Update yaws to 1.92.
    Also add a 32-bit only option.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yaws.rb
+++ Library/Formula/yaws.rb
@@ -2,16 +2,26 @@ require 'formula'
 
 class Yaws < Formula
   homepage 'http://yaws.hyber.org'
-  url 'http://yaws.hyber.org/download/yaws-1.90.tar.gz'
-  md5 'c2aae5a4e01ad47d44955551c9a67333'
+  url 'http://yaws.hyber.org/download/yaws-1.92.tar.gz'
+  md5 'd0c05d2041df79089f7de5d8437ee34b'
 
   depends_on 'erlang'
 
   def options
-    [["--with-yapp", "Build and install yaws applications"]]
+    [
+      ["--with-yapp", "Build and install yaws applications"],
+      ['--32-bit', "Build 32-bit only."]
+    ]
   end
 
   def install
+    if ARGV.build_32_bit?
+      %w{ CFLAGS LDFLAGS }.each do |compiler_flag|
+        ENV.remove compiler_flag, "-arch x86_64"
+        ENV.append compiler_flag, "-arch i386"
+      end
+    end
+
     Dir.chdir 'yaws' do
       system "./configure", "--prefix=#{prefix}"
       system "make install"
