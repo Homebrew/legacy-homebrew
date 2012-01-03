10a821d1996509938d22df9e86962d147216b48e
Andreas Steinel <A.Steinel@gmail.com>
2012-01-03 01:56:27 +0100

    MPC als MPC2 eingebaut

---
+++ Library/Formula/mpc2.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Mpc < Formula
+  url 'ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz'
+  md5 '5b34aa804d514cc295414a963aedb6bf'
+
+  depends_on 'gmp'
+  depends_on 'mpfr'
+
+  def install
+    #ENV.gcc_4_2
+
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--enable-checking=release",
+      "--with-gmp=#{HOMEBREW_PREFIX}", "--with-mpfr=#{HOMEBREW_PREFIX}", "--with-mpc=#{HOMEBREW_PREFIX}" #, "--program-suffix=-4.6.1"
+
+    system "echo '#define GMP_RNDA MPFR_RNDA' >> config.h"
+    system "make install"
+  end
+end
