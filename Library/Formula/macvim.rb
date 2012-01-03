e5e847d45392d43f7d8a19edfd9f080b15911b2e
Martin Jagusch <async@mj.io>
2012-01-03 13:02:07 +0100

    MacVim 7.3-64
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/macvim.rb
+++ Library/Formula/macvim.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Macvim < Formula
   homepage 'http://code.google.com/p/macvim/'
-  url 'https://github.com/b4winckler/macvim/tarball/snapshot-63'
-  version '7.3-63'
-  md5 '6abd828216e4ee37d78538cf6f4a7af0'
+  url 'https://github.com/b4winckler/macvim/tarball/snapshot-64'
+  version '7.3-64'
+  md5 '5bdc0bc618b3179130f846f8d0f81283'
   head 'https://github.com/b4winckler/macvim.git', :branch => 'master'
 
   def options
