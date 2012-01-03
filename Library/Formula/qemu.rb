34ed9ce525847ea78df8f7a12353eaf2d6966d61
joaohenriques <joaopedrohenriques@gmail.com>
2012-01-03 21:35:43 +0000

    qemu 1.0

--- Library/Formula/qemu.rb
+++ Library/Formula/qemu.rb
@@ -1,17 +1,23 @@
 require 'formula'
 
 class Qemu < Formula
-  url 'http://wiki.qemu.org/download/qemu-0.15.0.tar.gz'
+  url 'http://wiki.qemu.org/download/qemu-1.0.tar.gz'
   homepage 'http://www.qemu.org/'
-  md5 'dbc55b014bcd21b98e347f6a90f7fb6d'
+  md5 'a64b36067a191451323b0d34ebb44954'
 
   depends_on 'jpeg'
   depends_on 'gnutls'
-
+  depends_on 'glib'
+ 
   fails_with_llvm "Segmentation faults occur at run-time with LLVM"
 
+  def patches
+    "https://raw.github.com/gist/1556648/softfloat-Avoid-uint16-type-conflict-on-Darwin"
+  end
+  
   def install
     system "./configure", "--prefix=#{prefix}",
+                          "--enable-cocoa",
                           "--disable-darwin-user",
                           "--enable-cocoa",
                           "--disable-bsd-user",
