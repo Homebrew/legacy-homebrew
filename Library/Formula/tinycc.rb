492878adf691dd38a8b16051595b8b5526e9efc3
Alexandre Fiori <fiorix@gmail.com>
2012-01-03 19:24:17 -0500

    tcc port to osx

--- Library/Formula/tinycc.rb
+++ Library/Formula/tinycc.rb
@@ -1,44 +1,17 @@
 require 'formula'
 
 class Tinycc < Formula
-  url 'http://download.savannah.nongnu.org/releases/tinycc/tcc-0.9.25.tar.bz2'
+  # This is a modified version of tcc, which generate MACH-O binaries as opposed
+  # to the original tcc which can only generate ELF.
+  # It was posted in tcc's mailing list, and the patch is over 78k lines.
+  # http://lists.gnu.org/archive/html/tinycc-devel/2011-08/msg00030.html
+  url 'https://gd.meizo.com/tcc-0.9.25-osx.tar.gz'
+  version '0.9.25'
   homepage 'http://bellard.org/tcc/'
-  md5 '991c2a1986cce15f03ca6ddc86ea5f43'
-
-  def patches
-    DATA
-  end
+  md5 'acc601e60286c1f4ed60ca14c3ba740d'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make install"
   end
 end
-
-# Make it compile in OSX
-# Patch was found here: http://stackoverflow.com/questions/3712902/problems-compiling-tcc-on-os-x
-__END__
-diff -u a/libtcc.c b/libtcc.c
---- a/libtcc.c	2012-01-03 12:13:47.000000000 -0500
-+++ b/libtcc.c	2012-01-03 12:14:39.000000000 -0500
-@@ -1508,11 +1508,18 @@
-     int i;
- 
-     if (level == 0) {
--        /* XXX: only support linux */
-+#ifdef __DARWIN_UNIX03
-+        *paddr = uc->uc_mcontext->__ss.__rip;
-+#else
-         *paddr = uc->uc_mcontext.gregs[REG_RIP];
-+#endif
-         return 0;
-     } else {
-+#ifdef __DARWIN_UNIX03
-+        fp = uc->uc_mcontext->__ss.__rbp;
-+#else
-         fp = uc->uc_mcontext.gregs[REG_RBP];
-+#endif
-         for(i=1;i<level;i++) {
-             /* XXX: check address validity with program info */
-             if (fp <= 0x1000)
-
