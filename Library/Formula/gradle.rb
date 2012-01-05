d2a8cbbb1b6b9fe57402b31169b162387a39ab24
Ealden Esto E. Escanan <ealden@gmail.com>
2012-01-06 00:47:04 +0800

    Gradle 1.0-milestone-7
    Closes #9439.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>
    Closes #9439.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gradle < Formula
   homepage 'http://www.gradle.org/'
-  version '1.0-milestone-6'
-  url 'http://repo.gradle.org/gradle/distributions/gradle-1.0-milestone-6-bin.zip'
-  md5 '85b6c8662b71c7033ce359912f241616'
+  version '1.0-milestone-7'
+  url 'http://repo.gradle.org/gradle/distributions/gradle-1.0-milestone-7-bin.zip'
+  md5 '3622dbf2f2ef30c5abf5d8b94e709e13'
 
   def install
     rm_f Dir["bin/*.bat"]
