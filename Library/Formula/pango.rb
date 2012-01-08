2d29472550b72c73f17802946dbcfeebc5d954c8
Jack Nagel <jacknagel@gmail.com>
2012-01-08 13:12:11 -0600

    pango: move back to 1.28.4
    1.29.x is the development series; we'll stay on the stable 1.28 series
    until 1.30.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pango < Formula
   homepage 'http://www.pango.org/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/pango/1.29/pango-1.29.4.tar.bz2'
-  sha256 'f15deecaecf1e9dcb7db0e4947d12b5bcff112586434f8d30a5afd750747ff2b'
+  url 'http://ftp.gnome.org/pub/gnome/sources/pango/1.28/pango-1.28.4.tar.bz2'
+  sha256 '7eb035bcc10dd01569a214d5e2bc3437de95d9ac1cfa9f50035a687c45f05a9f'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
