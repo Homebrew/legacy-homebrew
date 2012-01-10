e9afef8b43035d15b54cb1f7402ef0ded508a446
Alex-P. Natsios <drakevr@linuxteam.cs.teilar.gr>
2012-01-10 10:03:25 +0200

    embryo 1.1.0

--- Library/Formula/embryo.rb
+++ Library/Formula/embryo.rb
@@ -1,19 +1,15 @@
 require 'formula'
 
 class Embryo < Formula
-  url 'http://download.enlightenment.org/releases/embryo-1.0.0.tar.gz'
+  url 'http://download.enlightenment.org/releases/embryo-1.1.0.tar.gz'
   homepage 'http://trac.enlightenment.org/e/wiki/Embryo'
-  md5 '2d6269c931656d5714197e508b144f18'
+  md5 'aded5754ee7f586e3a0631e0fa3abcc8'
   head 'http://svn.enlightenment.org/svn/e/trunk/embryo/', :using => :svn
 
   depends_on 'pkg-config' => :build
   depends_on 'eina'
 
   def install
-    # hack to allow building with current trunk, will be made obsolete after 1.1
-    # is released: http://comments.gmane.org/gmane.comp.window-managers.enlightenment.devel/30780
-    inreplace 'configure.ac', 'm4_define([v_mic], [999])', 'm4_define([v_mic], [99])' if ARGV.build_head?
-    inreplace 'configure.ac', 'eina >= 1.0.999', 'eina >= 1.0.99' if ARGV.build_head?
     system "./autogen.sh" if ARGV.build_head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
