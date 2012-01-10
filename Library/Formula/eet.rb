f61de686100ea93fbdca453b12724304915319b4
Alex-P. Natsios <drakevr@linuxteam.cs.teilar.gr>
2012-01-10 10:02:12 +0200

    eet 1.5.0

--- Library/Formula/eet.rb
+++ Library/Formula/eet.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Eet < Formula
-  url 'http://download.enlightenment.org/releases/eet-1.4.1.tar.gz'
+  url 'http://download.enlightenment.org/releases/eet-1.5.0.tar.gz'
   homepage 'http://trac.enlightenment.org/e/wiki/Eet'
-  md5 '88d126fce01dc1330a1e798d9063aba1'
+  md5 'f6fd734fbf6a2852abf044a2e1a8cabf'
   head 'http://svn.enlightenment.org/svn/e/trunk/eet/', :using => :svn
 
   depends_on 'pkg-config' => :build
@@ -12,9 +12,6 @@ class Eet < Formula
   depends_on 'lzlib'
 
   def install
-    # hack to allow building with current trunk, will be made obsolete after 1.1
-    # is released: http://comments.gmane.org/gmane.comp.window-managers.enlightenment.devel/30780
-    inreplace 'configure.ac', 'm4_define([v_mic], [999])', 'm4_define([v_mic], [99])' if ARGV.build_head?
     system "./autogen.sh" if ARGV.build_head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
