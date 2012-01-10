323e2859bcd8dc6c5bfd89b9f7f27bb34f7802fe
darron <darron@nonfiction.ca>
2012-01-09 17:24:01 -0700

    ec2-api-tools 1.5.2.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -41,8 +41,8 @@ end
 
 class Ec2ApiTools < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/351'
-  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.5.0.1.zip'
-  md5 '578025392f7dcfbf0570d2bfa9c06533'
+  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.5.2.3.zip'
+  md5 'fa1e050b35ce9e7f4a2017f251c5e2bb'
 
   def install
     standard_install
