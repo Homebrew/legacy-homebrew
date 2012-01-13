db6cc7f591beff75126957ef845ee7e5b9c2c8ac
Adam Vandenberg <flangy@gmail.com>
2012-01-12 20:23:20 -0800

    Jenkins 1.447

--- Library/Formula/jenkins.rb
+++ Library/Formula/jenkins.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Jenkins < Formula
-  url 'http://mirrors.jenkins-ci.org/war/1.443/jenkins.war', :using => :nounzip
+  url 'http://mirrors.jenkins-ci.org/war/1.447/jenkins.war', :using => :nounzip
   head 'https://github.com/jenkinsci/jenkins.git'
-  version '1.443'
-  md5 'd004192da6c2061c11f7e6cd17603489'
+  version '1.447'
+  md5 '1d7abb4fe6de96c48f0ef1a99edb5879'
   homepage 'http://jenkins-ci.org'
 
   def install
