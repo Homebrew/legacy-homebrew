256194f7bac205ec20d33b914645d991da8fbc29
Nicolas Despres <nicolas.despres@gmail.com>
2012-01-15 03:23:51 +0100

    cmake: Use all cmake-provided third-party libraries.
    This is the default when bootstrapping CMake (see ./bootstrap --help).
    CMake is totally self-contained and thus we prevent conflict with other
    libraries such as expat.

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -8,15 +8,6 @@ class Cmake < Formula
   bottle_sha1 '8f4731fa17bf96afa2cdbfa48aaf6020a9836e3f'
 
   def install
-    # A framework-installed expat will be detected and mess things up.
-    if File.exist? "/Library/Frameworks/expat.framework"
-      opoo "/Library/Frameworks/expat.framework detected"
-      puts <<-EOS.undent
-        This will be picked up by CMake's build system and likey cause the
-        build to fail, trying to link to a 32-bit version of expat.
-        You may need to move this file out of the way for this brew to work.
-      EOS
-    end
 
     if ENV['GREP_OPTIONS'] == "--color=always"
       opoo "GREP_OPTIONS is set to '--color=always'"
@@ -27,8 +18,6 @@ class Cmake < Formula
     end
 
     system "./bootstrap", "--prefix=#{prefix}",
-                          "--system-libs",
-                          "--no-system-libarchive",
                           "--datadir=/share/cmake",
                           "--docdir=/share/doc/cmake",
                           "--mandir=/share/man"
