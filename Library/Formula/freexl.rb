601dd8103f5f02f18798c21aab0466481636f677
Adam Vandenberg <flangy@gmail.com>
2012-01-16 21:30:46 -0800

    freexl: fix md5

--- Library/Formula/freexl.rb
+++ Library/Formula/freexl.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Freexl < Formula
   homepage 'https://www.gaia-gis.it/fossil/freexl/index'
   url 'http://www.gaia-gis.it/gaia-sins/freexl-1.0.0b.tar.gz'
-  md5 '5a9c422ed1af7487b626889f3cace2dd'
+  md5 'e2e1c59eb2883d825782dfe8fe244207'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
