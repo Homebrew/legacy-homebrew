85fe46a8dd7ad97fc752f16f33a6e74e92092d0e
Jason Voegele <jason@jvoegele.com>
2012-01-17 16:32:41 -0500

    Correct URL for vorbisgain 0.37.

--- Library/Formula/vorbisgain.rb
+++ Library/Formula/vorbisgain.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Vorbisgain < Formula
-  url 'http://sjeng.org/ftp/vorbis/vorbisgain-0.36.zip'
+  url 'http://sjeng.org/ftp/vorbis/vorbisgain-0.37.tar.gz'
   homepage 'http://sjeng.org/vorbisgain.html'
   md5 '850b05a7b2b0ee67edb5a27b8c6ac3a2'
   version '0.37'
