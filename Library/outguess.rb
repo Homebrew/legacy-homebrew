86ee7c97484a01c0b5d5636266124756aba2b499
ake <peaceunlimited@gmail.com>
2012-01-17 18:33:55 +0700

    New formula: outguess 0.2

---
+++ Library/outguess.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Outguess < Formula
+  homepage 'http://www.outguess.org/'
+  url 'http://www.outguess.org/outguess-0.2.tar.gz'
+  mirror 'http://www.mirrors.wiretapped.net/security/steganography/outguess/outguess-0.2.tar.gz'
+  md5 '321f23dc0badaba4350fa66b59829064'
+
+  def install
+    args = ["--disable-debug",
+            "--disable-dependency-tracking",
+            "--prefix=#{prefix}",
+            "--sysconfdir=#{etc}",
+            "--mandir=#{man}"
+            ]
+
+    system "./configure", *args
+    system "make"
+    bin.install "outguess"
+    man1.install "outguess.1"
+  end
+end
