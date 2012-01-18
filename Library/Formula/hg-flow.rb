b20934d4d72e3c7f39a329052633236737db395b
Jamie Sa <james.sa.2013@gmail.com>
2012-01-18 12:03:03 +0800

    downgrade to v0.3 latest stable tag and pu Jack's feedback in place.

--- Library/Formula/hg-flow.rb
+++ Library/Formula/hg-flow.rb
@@ -1,24 +1,24 @@
 require 'formula'
 
 class HgFlow < Formula
-  url 'https://bitbucket.org/yinwm/hgflow/get/develop.tar.gz'
-  version 'v0.4'
+  url 'https://bitbucket.org/yinwm/hgflow/get/v0.3.tar.gz'
+  version 'v0.3'
   homepage 'https://bitbucket.org/yinwm/hgflow/wiki/Home'
-  md5 '4c818c98c81cb01e6a1b5a38138c1a84'
-
-  depends_on 'mercurial'
+  head "http://bitbucket.org/yinwm/hgflow", :using => :hg
+  md5 '0c3305bc349be31da76ef82460019d09'
 
   def install
     prefix.install Dir['*']
-    system <<-HEREDOC.undent
-      echo "
-      [extensions]
-      hgflow = /usr/local/Cellar/hg-flow/v0.4/src/hgflow/hgflow.py" >> ~/.hgrc
-      HEREDOC
   end
 
   def caveats; <<-EOS.undent
-    Check your ~/.hgrc for [extionsions]
+    1. Put following lines into your ~/.hgrc
+    2. Restart your shell and try "hg flow".
+    3. For more information go to http://bitbucket.org/yinwm/hgflow
+
+    [extensions]
+    hgflow = #{HOMEBREW_PREFIX}/Cellar/#{name}/#{version}/src/hgflow/hgflow.py
+    
     EOS
   end
 end
