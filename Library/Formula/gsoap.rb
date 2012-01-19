32f056114f2a7218592627bdaa05ecf9d76a48b4
Olli Helenius <liff@iki.fi>
2012-01-19 22:40:02 +0000

    Add gSOAP formula, version 2.8.6
    Closes #6210.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/gsoap.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Gsoap < Formula
+  url 'http://downloads.sourceforge.net/project/gsoap2/gSOAP/gsoap_2.8.6.zip'
+  homepage 'http://www.cs.fsu.edu/~engelen/soap.html'
+  md5 'c0b962c6216bcf59255dc4288783252f'
+
+  def install
+    ENV.deparallelize
+    system './configure', "--prefix=#{prefix}"
+    system 'make install'
+  end
+
+  def test
+    system "soapcpp2 -v"
+  end
+end
