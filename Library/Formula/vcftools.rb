62337f339d97b677679a55b45b4c347b7c683544
Lance Parsons <lparsons@princeton.edu>
2012-01-20 19:02:26 -0500

    vcftools 0.1.8
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/vcftools.rb
+++ Library/Formula/vcftools.rb
@@ -1,15 +1,9 @@
 require 'formula'
 
 class Vcftools < Formula
-  url 'http://downloads.sourceforge.net/project/vcftools/vcftools_0.1.7.tar.gz'
+  url 'http://downloads.sourceforge.net/project/vcftools/vcftools_0.1.8.tar.gz'
   homepage 'http://vcftools.sourceforge.net/index.html'
-  md5 'd3e68027a7fe40d3f8cb28c3006c7248'
-
-  def patches
-    # Install Perl modules to /lib/perl5/site_perl and ensure VcfStats.pm is installed
-    # This is fixed in vcf source tree, will not be needed after version 0.1.7
-    DATA
-  end
+  md5 '9895d3f30f2dac1eb01dd774557384de'
 
   def install
     system "make install PREFIX=#{prefix} CPP=#{ENV.cxx}"
@@ -26,32 +20,3 @@ class Vcftools < Formula
     EOS
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 39c042b..055afd0 100644
---- a/Makefile
-+++ b/Makefile
-@@ -17,7 +17,7 @@ ifndef PREFIX
-     export PREFIX = $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
- endif
- export BINDIR = ${PREFIX}/bin
--export MODDIR = ${PREFIX}/lib
-+export MODDIR = ${PREFIX}/lib/perl5/site_perl
- 
- DIRS = cpp perl
- install:
-
-diff --git a/perl/Makefile b/perl/Makefile
-index 222b75d..2e8b49f 100644
---- a/perl/Makefile
-+++ b/perl/Makefile
-@@ -1,7 +1,7 @@
- 
- BIN = vcf-compare fill-aa vcf-annotate vcf-merge vcf-isec vcf-stats vcf-to-tab fill-an-ac \
-     vcf-query vcf-convert vcf-subset vcf-validator vcf-concat vcf-sort
--MOD = FaSlice.pm Vcf.pm
-+MOD = FaSlice.pm Vcf.pm VcfStats.pm
- 
- install:
- 	    @for i in $(BIN); do cp $(CURDIR)/$$i $(BINDIR)/$$i; done; \
