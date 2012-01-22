7971d87528d2483eeefe063fd08cb4df27250108
Devon Bryant <devonbryant@gmail.com>
2012-01-21 20:15:38 -0700

    Create formula for Argtable ANSI C command line parser

---
+++ Library/Formula/argtable2.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Argtable2 < Formula
+  url 'http://sourceforge.net/projects/argtable/files/argtable/argtable-2.13/argtable2-13.tar.gz'
+  homepage 'http://argtable.sourceforge.net/'
+  md5 '156773989d0d6406cea36526d3926668'
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
