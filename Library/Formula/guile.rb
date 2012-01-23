e5433cf7f8b6d7458e875e11d22eb7e8dec9def6
Misty De Meo <mistydemeo@gmail.com>
2012-01-22 22:32:15 -0600

    Add `devel` to the DSL, + stable and bottle blocks
    This commit adds a `devel` entry to the DSL, allowing formulae to
    specify an unstable branch.
    
    `devel` takes a block, which should contain standard `url` and `md5`
    fields (and `version`, if necessary). This must come after the standard
    DSL fields.
    
    This commit also migrates over all formulae currently using `devel` to
    the new syntax, as well as formulae which used `head` for non-VCS urls.
    
    The new syntax is also available for `stable` and `bottle`. `stable` is
    an option alongside the old syntax. `bottle` replaces the old syntax.
    
    Note that the @stable ivar in Formula has been renamed to @standard,
    and the @bottle ivar has been renamed to @bottle_url.
    
    Closes #9735.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/guile.rb
+++ Library/Formula/guile.rb
@@ -3,13 +3,12 @@ require 'formula'
 class Guile < Formula
   homepage 'http://www.gnu.org/software/guile/'
   url 'http://ftpmirror.gnu.org/guile/guile-1.8.7.tar.gz'
+  sha1 '24cd2f06439c76d41d982a7384fe8a0fe5313b54'
   mirror 'http://ftp.gnu.org/gnu/guile/guile-1.8.7.tar.gz'
-  head 'http://ftpmirror.gnu.org/guile/guile-2.0.2.tar.gz'
 
-  if ARGV.build_head?
+  devel do
+    url 'http://ftpmirror.gnu.org/guile/guile-2.0.2.tar.gz'
     sha1 '1943fd22417ce1e51babbdcd7681e66a794a8da3'
-  else
-    sha1 '24cd2f06439c76d41d982a7384fe8a0fe5313b54'
   end
 
   depends_on 'pkg-config' => :build
