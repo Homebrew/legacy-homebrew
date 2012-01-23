f5d9da821c9e58266b777c2f1744b2880ba1acce
Mitch Amiano <mamiano@nc.rr.com>
2012-01-23 15:58:43 -0500

    Update git-hg to use depends_on for mercurial and python mercurial module

--- Library/Formula/git-hg.rb
+++ Library/Formula/git-hg.rb
@@ -3,13 +3,11 @@ require 'formula'
 class GitHg < Formula
   head 'https://github.com/offbytwo/git-hg.git'
   homepage 'http://offbytwo.com/git-hg/'
+  
+  depends_on 'mercurial'
+  depends_on 'mercurial' => :python
 
   def install
-    unless `/usr/bin/which hg`.size > 0
-      puts "You may need to install Mercurial before using this software:"
-      puts "  brew install mercurial"
-    end
-
     prefix.install Dir['*']
   end
 end
