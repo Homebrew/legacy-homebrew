636a921ef5367d0b7aeb649a376bcb06124815ee
Jocelyn Mallon <jocelyn.e.mallon@gmail.com>
2012-01-24 00:16:03 -0800

    pngcrush 1.7.24
    Update to latest version, 1.7.24, compiled fine on 10.7.2 x86_64

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pngcrush < Formula
   homepage 'http://pmt.sourceforge.net/pngcrush/'
-  url 'http://downloads.sourceforge.net/project/pmt/pngcrush/1.7.17/pngcrush-1.7.17.tar.bz2'
-  md5 '80F5EBBE7D15C58B077EDAE7738F08AD'
+  url 'http://sourceforge.net/projects/pmt/files/pngcrush/1.7.24/pngcrush-1.7.24.tar.bz2'
+  md5 '9F29BD4BC05AE1415E5AD10241798794'
 
   def install
     # use our CFLAGS, LDFLAGS, CC, and LD
