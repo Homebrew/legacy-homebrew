8de401f4422975173c3bde66b1f0a12ae81c64a2
Ingo Richter <ingo.richter@gmail.com>
2012-01-25 15:10:26 -0800

    libimobiledevice: add --head
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libimobiledevice.rb
+++ Library/Formula/libimobiledevice.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class Libimobiledevice < Formula
-  url 'http://www.libimobiledevice.org/downloads/libimobiledevice-1.1.1.tar.bz2'
   homepage 'http://www.libimobiledevice.org/'
+  url 'http://www.libimobiledevice.org/downloads/libimobiledevice-1.1.1.tar.bz2'
   md5 'cdc13037e822d9ac2e109536701d153a'
+
+  head 'http://cgit.sukimashita.com/libimobiledevice.git', :using => :git
 
   depends_on 'pkg-config' => :build
   depends_on 'libtasn1'
@@ -13,7 +15,17 @@ class Libimobiledevice < Formula
   depends_on 'gnutls'
 
   def install
-    ENV.prepend "CFLAGS", "-I#{HOMEBREW_PREFIX}/include"
+    if ARGV.build_head?
+      # fix the m4 problem with the missing pkg.m4
+      ENV['LIBTOOLIZE'] = "/usr/bin/glibtoolize"
+      ENV['ACLOCAL'] = "/usr/bin/aclocal -I m4 -I #{HOMEBREW_PREFIX}/share/aclocal"
+      ENV.prepend "CFLAGS", "-I#{HOMEBREW_PREFIX}/include"
+
+      system "autoreconf -ivf"
+    end
+
+    ENV.append_to_cflags "-std=gnu89" if ENV.compiler == :clang
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--without-swig"
