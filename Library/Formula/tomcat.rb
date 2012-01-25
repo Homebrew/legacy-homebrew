9f3389bfdbb59a67e22e3ef9fa5b13207a458724
Ralph Rooding <ralph.rooding@kabisa.nl>
2012-01-25 09:40:24 +0100

    Tomcat 7.0.25
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Tomcat < Formula
-  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.23/bin/apache-tomcat-7.0.23.tar.gz'
+  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.25/bin/apache-tomcat-7.0.25.tar.gz'
   homepage 'http://tomcat.apache.org/'
-  md5 'd8dcd5fb07dd1769d571fdabade9cc68'
+  md5 '2aa59d23555d641b20efad4aed86b693'
 
   skip_clean :all
 
