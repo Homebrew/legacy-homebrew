ada68287eaeab4985b485fe3df12c002d81f38ec
Jack Nagel <jacknagel@gmail.com>
2012-01-25 23:00:23 -0600

    node: use new devel syntax
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,17 +1,15 @@
 require 'formula'
 
 class Node < Formula
-  if ARGV.build_devel?
-    url 'http://nodejs.org/dist/v0.7.0/node-v0.7.0.tar.gz'
-    md5 '5aceaa4e7aa43d78f87005f468dd6e3a'
-  else
-    url 'http://nodejs.org/dist/v0.6.8/node-v0.6.8.tar.gz'
-    md5 '9fd7baa2d27b848c3134e6ae35bb87b2'
-  end
-
+  url 'http://nodejs.org/dist/v0.6.8/node-v0.6.8.tar.gz'
+  md5 '9fd7baa2d27b848c3134e6ae35bb87b2'
+  homepage 'http://nodejs.org/'
   head 'https://github.com/joyent/node.git'
 
-  homepage 'http://nodejs.org/'
+  devel do
+    url 'http://nodejs.org/dist/v0.7.0/node-v0.7.0.tar.gz'
+    md5 '5aceaa4e7aa43d78f87005f468dd6e3a'
+  end
 
   # Leopard OpenSSL is not new enough, so use our keg-only one
   depends_on 'openssl' if MacOS.leopard?
