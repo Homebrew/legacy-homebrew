8bf5ec4204893fec0ab72ca2a23e18d01f8aa934
Jack Nagel <jacknagel@gmail.com>
2012-01-26 00:16:37 -0600

    cpanminus: add stable tarball
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/cpanminus.rb
+++ Library/Formula/cpanminus.rb
@@ -1,6 +1,8 @@
 require 'formula'
 
 class Cpanminus < Formula
+  url 'https://github.com/miyagawa/cpanminus/tarball/1.5007'
+  sha1 'd0bbf766a7250253138381833af152895f8115b7'
   head 'https://github.com/miyagawa/cpanminus.git'
   homepage 'https://github.com/miyagawa/cpanminus'
 
