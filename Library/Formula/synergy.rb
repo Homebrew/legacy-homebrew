e5ba00f086c903f0ae9c1bb38d929c1f4fb84a94
Jack Nagel <jacknagel@gmail.com>
2012-01-25 23:58:04 -0600

    synergy: use devel instead of HEAD
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/synergy.rb
+++ Library/Formula/synergy.rb
@@ -1,21 +1,19 @@
 require 'formula'
 
 class Synergy < Formula
+  # Newer 1.3.x versions have a critical bug on OS X:
+  # http://code.google.com/p/synergy-plus/issues/detail?id=47
+  # Do not bump this to 1.3.7 or newer until that issue is resolved.
+  url 'http://synergy.googlecode.com/files/synergy-1.3.6p2-MacOSX-Universal.zip'
+  sha1 '5b9336fb553db17bd109e3f2bca517af18ed3863'
+  version '1.3.6p2'
+  homepage 'http://synergy-foss.org'
 
-  if ARGV.build_head?
+  devel do
     url 'http://synergy.googlecode.com/files/synergy-1.4.3-MacOSX106-Universal.zip'
     sha1 'aa60fa6ac975dd22dc095d6cd9f3dc755b91bb64'
     version '1.4.3'
-  else
-    # Newer 1.3.x versions have a critical bug on OS X:
-    # http://code.google.com/p/synergy-plus/issues/detail?id=47
-    # Do not bump this to 1.3.7 or newer until that issue is resolved.
-    url 'http://synergy.googlecode.com/files/synergy-1.3.6p2-MacOSX-Universal.zip'
-    sha1 '5b9336fb553db17bd109e3f2bca517af18ed3863'
-    version '1.3.6p2'
   end
-
-  homepage 'http://synergy-foss.org'
 
   def install
     bin.install 'synergyc'
