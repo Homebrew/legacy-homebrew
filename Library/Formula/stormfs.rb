2bc56082483352e9742b48355005c2e5a7bf123e
Ben LeMasurier <ben.lemasurier@gmail.com>
2012-01-27 16:21:32 -0700

    updates for relaeses < Lion

--- Library/Formula/stormfs.rb
+++ Library/Formula/stormfs.rb
@@ -3,11 +3,12 @@ require 'formula'
 class Stormfs < Formula
   url 'https://github.com/downloads/benlemasurier/stormfs/stormfs-0.01.tar.gz'
   homepage 'https://github.com/benlemasurier/stormfs'
-  md5 '14796c891d0dd431d807add766f69e39'
+  md5 '99939ae58cb25bf081d769f6f1cf9180'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'fuse4x'
+  depends_on 'curl' if MacOS.leopard?
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
