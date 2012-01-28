3e015719223bf242ac9f151802445e91e0409307
Adam Vandenberg <flangy@gmail.com>
2012-01-28 12:50:34 -0800

    boost-jam: fix homepage

--- Library/Formula/boost-jam.rb
+++ Library/Formula/boost-jam.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class BoostJam < Formula
   url 'http://downloads.sourceforge.net/project/boost/boost-jam/3.1.18/boost-jam-3.1.18.tgz'
-  homepage 'http://www.boost.org/doc/tools/build/doc/html/jam/usage.html'
+  homepage 'http://www.boost.org/boost-build2/doc/html/bbv2/jam.html'
   md5 'f790e022d658db38db5cc4aeeccad3f1'
 
   def install
