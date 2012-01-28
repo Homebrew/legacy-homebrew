0e47222f0045584afb0bebea201c4318cb13f439
Robert Sanders <robert@zeevex.com>
2012-01-28 12:11:18 -0500

    Kawa 1.11
    Kawa scheme, a JVM-based scheme interpreter
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/kawa.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Kawa < Formula
+  url 'ftp://ftp.gnu.org/pub/gnu/kawa/kawa-1.11.jar'
+  homepage 'http://www.gnu.org/software/kawa/'
+  md5 'eee96e13a329513a2f705ee03bacef63'
+
+  def install
+    prefix.install "kawa-#{version}.jar"
+    (bin+'kawa').write <<-EOS.undent
+      #!/bin/sh
+      KAWA_HOME="#{prefix}"
+      java -jar "$KAWA_HOME/kawa-#{version}.jar"
+    EOS
+  end
+end
