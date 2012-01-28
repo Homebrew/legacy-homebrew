f93eafcfd3c3deed89704f71e1892e90bbbda442
John Earles <jearles@gmail.com>
2012-01-27 20:46:01 -0500

    node 0.6.9
    Closes #9823.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Node < Formula
-  url 'http://nodejs.org/dist/v0.6.8/node-v0.6.8.tar.gz'
-  md5 '9fd7baa2d27b848c3134e6ae35bb87b2'
+  url 'http://nodejs.org/dist/v0.6.9/node-v0.6.9.tar.gz'
+  md5 'c2d2aee123a141ba8431855f1d9c8200'
   homepage 'http://nodejs.org/'
   head 'https://github.com/joyent/node.git'
 
