3f74b7442c97aeadbda6d0bad4f967703cb67ffb
Cory <cmburget@ncsu.edu>
2012-01-29 11:46:55 -0500

    Changed homepage to claylabs.com

--- Library/Formula/clay.rb
+++ Library/Formula/clay.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Clay < Formula
   url 'https://github.com/jckarter/clay/tarball/v0.1.0'
-  homepage 'https://github.com/jckarter/clay/'
+  homepage 'http://claylabs.com/clay/'
   md5 'fa34b4ecc0ab13047f802b9aac6d850b'
   version '0.1.0'
   head 'git://github.com/jckarter/clay.git'
