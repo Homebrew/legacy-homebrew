a8445fa84361228922e915074260c901d203aab5
dinkypumpkin <dinkypumpkin@gmail.com>
2012-01-30 11:30:14 +0000

    ffmpeg: Remove version string generation for HEAD builds

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -74,40 +74,12 @@ class Ffmpeg < Formula
       end
     end
 
-    write_version_file if ARGV.build_head?
-
     system "make install"
 
     if ARGV.include? "--with-tools"
       system "make alltools"
       bin.install Dir['tools/*'].select {|f| File.executable? f}
     end
-  end
-
-  # Makefile expects to run in git repo and generate a version number
-  # with 'git describe' command (see version.sh) but Homebrew build
-  # runs in temp copy created via git checkout-index, so 'git describe'
-  # does not work. Work around by writing VERSION file in build directory
-  # to be picked up by version.sh.  Note that VERSION file will already
-  # exist in release versions, so this only applies to git HEAD builds.
-  def write_version_file
-    return if File.exists?("VERSION")
-    git_tag = "UNKNOWN"
-    Dir.chdir(cached_download) do
-      ver = `./version.sh`.chomp
-      if not $?.success? or ver == "UNKNOWN"
-        # fall back to git
-        ver = `git describe --tags --match N --always`.chomp
-        if not $?.success?
-          opoo "Could not determine build version from git repository - set to #{git_tag}"
-        else
-          git_tag = "git-#{ver}"
-        end
-      else
-        git_tag = ver
-      end
-    end
-    File.open("VERSION","w") {|f| f.puts git_tag}
   end
 
 end
