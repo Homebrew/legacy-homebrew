a470383d92ef074a536363f8292e1465a45a68ee
James Blanding <ilikepi+github@gmail.com>
2012-01-30 18:32:38 -0500

    httrack 3.44.4

--- Library/Formula/httrack.rb
+++ Library/Formula/httrack.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Httrack < Formula
   url 'http://download.httrack.com/cserv.php3?File=httrack.tar.gz'
   homepage 'http://www.httrack.com/'
-  md5 '7aaf56913388adfdba506c346c2ca020'
-  version '3.44.1'
+  md5 '614b7124b887c543fc10545b994b2814'
+  version '3.44.4'
 
   def install
     ENV.deparallelize
