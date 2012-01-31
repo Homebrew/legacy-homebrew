e21477f3d0bd4f9f0d38c70ddc0af0521306c68c
Jack Nagel <jacknagel@gmail.com>
2012-01-30 19:09:17 -0600

    lcms and lcms2: add optional deps on jpeg and tiff
    These things are picked up if they are installed, and as they are pretty
    lightweight we may as well mark them as optional deps.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/little-cms2.rb
+++ Library/Formula/little-cms2.rb
@@ -5,6 +5,9 @@ class LittleCms2 < Formula
   homepage 'http://www.littlecms.com/'
   md5 '327348d67c979c88c2dec59a23a17d85'
 
+  depends_on 'jpeg' => :optional
+  depends_on 'libtiff' => :optional
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
