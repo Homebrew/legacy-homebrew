295defd3d4bb8ba804d1604b7122bce52d5de1e7
Sijawusz Pur Rahnama <sijawusz@gmail.com>
2012-02-01 17:24:49 +0100

    Couple of fixes as suggested by @jacknagel.

--- Library/Formula/fwknop.rb
+++ Library/Formula/fwknop.rb
@@ -5,16 +5,13 @@ class Fwknop < Formula
   homepage 'http://www.cipherdyne.org/fwknop/'
   md5 'b2ee477140d9e92466c9c6f267442625'
 
-  # depends_on 'cmake' => :build
-
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 
   def test
-    system "fwknop --version"
+    system "#{bin}/fwknop --version"
   end
 end
