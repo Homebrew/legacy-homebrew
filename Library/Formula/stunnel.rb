46934e14ff8698965929ebe67b5c3be3db928aef
Jason Cooper <skawaii@gmail.com>
2012-02-01 14:56:53 -0500

    stunnel: properly created self-signed cert
    Closes #9926.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/stunnel.rb
+++ Library/Formula/stunnel.rb
@@ -35,14 +35,41 @@ end
 
 __END__
 diff --git a/tools/stunnel.cnf b/tools/stunnel.cnf
-index 0c00347..f53668f 100644
+index 0c00347..d011421 100644
 --- a/tools/stunnel.cnf
 +++ b/tools/stunnel.cnf
-@@ -5,6 +5,7 @@ default_bits                    = 2048
+@@ -5,27 +5,15 @@ default_bits                    = 2048
  encrypt_key                     = no
  distinguished_name              = req_dn
  x509_extensions                 = cert_type
 +prompt                          = no
 
  [ req_dn ]
- countryName = Country Name (2 letter code)
+-countryName = Country Name (2 letter code)
+-countryName_default             = PL
+-countryName_min                 = 2
+-countryName_max                 = 2
+-
+-stateOrProvinceName             = State or Province Name (full name)
+-stateOrProvinceName_default     = Mazovia Province
+-
+-localityName                    = Locality Name (eg, city)
+-localityName_default            = Warsaw
+-
+-organizationName                = Organization Name (eg, company)
+-organizationName_default        = Stunnel Developers
+-
+-organizationalUnitName          = Organizational Unit Name (eg, section)
+-organizationalUnitName_default  = Provisional CA
+-
+-0.commonName                    = Common Name (FQDN of your server)
+-0.commonName_default            = localhost
++countryName                     = PL
++stateOrProvinceName              = Mazovia Province
++localityName                     = Warsaw
++organizationName                 = Stunnel Developers
++organizationalUnitName           = Provisional CA
++0.commonName                     = localhost
+
+ # To create a certificate for more than one name uncomment:
+ # 1.commonName                  = DNS alias of your server
