50fa3ddcf0ffcffef7b4fe6b9eed8e69030e94ae
Sebastian Staudt <koraktor@gmail.com>
2012-02-03 10:56:24 +0100

    go-app-engine-32: Fix formula class name
    Closes #9952.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/go-app-engine-32.rb
+++ Library/Formula/go-app-engine-32.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class GoAppEngine64 < Formula
+class GoAppEngine32 < Formula
   url 'http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_386-1.6.2.zip'
   homepage 'http://code.google.com/appengine/docs/go/overview.html'
   sha1 '004aebd7e6252ce3e84b89096009dcd22dadf5fe'
