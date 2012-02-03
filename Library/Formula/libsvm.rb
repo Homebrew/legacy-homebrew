6413d900d7f9b1e1673d6caf086bf1d9822a0acd
Dougal Sutherland <dougal@gmail.com>
2012-02-03 18:02:21 -0500

    libsvm: install library
    Closes #9958.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libsvm.rb
+++ Library/Formula/libsvm.rb
@@ -6,7 +6,15 @@ class Libsvm < Formula
   md5 '44d2a3a611280ecd0d66aafe0d52233e'
 
   def install
+    inreplace 'Makefile', '-soname', '-install_name'
+    inreplace 'Makefile', 'libsvm.so.$(SHVER)', 'libsvm.$(SHVER).dylib'
+
     system "make"
+    system "make lib"
+    ln_s 'libsvm.2.dylib', 'libsvm.dylib'
+
     bin.install ['svm-scale', 'svm-train', 'svm-predict']
+    lib.install ['libsvm.2.dylib', 'libsvm.dylib']
+    include.install ['svm.h']
   end
 end
