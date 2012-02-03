a8ec0897d466500d3df85f6a38235538ef8e3e60
Berik Visschers <berik.visschers@xaton.com>
2012-02-03 11:57:44 +0100

    - Remove comments - Use bin.install instead of make

--- Library/Formula/fmscripts.rb
+++ Library/Formula/fmscripts.rb
@@ -5,18 +5,14 @@ class Fmscripts < Formula
   homepage 'http://soft.vub.ac.be/soft/'
   md5 '54b5ed94c89acd309effd37187414593'
 
-  # depends_on 'cmake' => :build
-
   def install
-    system "make"
+    bin.install ['fmdiff', 'fmdiff3', 'fmmerge', 'fmresolve']
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test fmscripts`. Remove this comment before submitting
-    # your pull request!
-    system "fmdiff"
+    system "/usr/local/bin/fmdiff"
+    system "/usr/local/bin/fmdiff3"
+    system "/usr/local/bin/fmmerge"
+    system "/usr/local/bin/fmresolve"
   end
 end
