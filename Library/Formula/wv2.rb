056e7d0af1fbbcd120003f2c8344f00497a998f3
Jack Nagel <jacknagel@gmail.com>
2012-02-02 18:55:36 -0600

    wv2: remove template test method
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/wv2.rb
+++ Library/Formula/wv2.rb
@@ -15,13 +15,4 @@ class Wv2 < Formula
                           "--prefix=#{prefix}"
     system "make install"
   end
-
-  def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test wv2`. Remove this comment before submitting
-    # your pull request!
-    system "false"
-  end
 end
