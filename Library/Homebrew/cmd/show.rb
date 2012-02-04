50c50cc44231c9dc8d794082239e1497a25a3e8b
Sebastian Staudt <koraktor@gmail.com>
2012-02-04 11:38:12 +0100

    Cleanup show command
    This requires #9969 to be merged first.

--- Library/Homebrew/cmd/show.rb
+++ Library/Homebrew/cmd/show.rb
@@ -2,17 +2,12 @@ module Homebrew extend self
   def show
     require 'formula'
     cd HOMEBREW_REPOSITORY
-    formulae = ARGV.send(:downcased_unique_named).map do |name|
-      begin
-        Formula.factory name
-      rescue FormulaUnavailableError
-      end
-    end.compact
+    formulae = ARGV.formulae
+    revs = ARGV.named - formulae.map(&:name)
+    revs = %w{HEAD} if formulae.empty? && revs.empty?
     formulae.each do |f|
       system 'git', 'log', '-1', '-p', *(ARGV.options_only + ['--', f.path])
     end
-    revs = ARGV.named - formulae.map(&:name)
-    revs = ['HEAD'] if formulae.empty? && revs.empty?
     revs.each do |r|
       system 'git', 'show', *ARGV.options_only << r
     end
