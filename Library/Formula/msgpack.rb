09b444f80d7b24088e0f42604f59d10cee415ef6
Adam Vandenberg <flangy@gmail.com>
2012-02-04 09:45:57 -0800

    msgpack 0.5.7

--- Library/Formula/msgpack.rb
+++ Library/Formula/msgpack.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Msgpack < Formula
   homepage 'http://msgpack.org/'
-  url 'http://msgpack.org/releases/cpp/msgpack-0.5.6.tar.gz'
-  sha256 '4d4a2b50955e35b6b8e92247ee079467b3294db2378d5bc73e83c9bbe184545b'
+  url 'http://msgpack.org/releases/cpp/msgpack-0.5.7.tar.gz'
+  sha256 '7c203265cf14a4723820e0fc7ac14bf4bad5578f7bc525e9835c70cd36e7d1b8'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
