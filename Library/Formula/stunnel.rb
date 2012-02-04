1ad7aba82a55f81eff6511eb50affff50c23134b
Trevor Wennblom <trevor@well.com>
2012-02-04 02:24:54 -0600

    stunnel 4.52
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/stunnel.rb
+++ Library/Formula/stunnel.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Stunnel < Formula
-  url 'ftp://ftp.stunnel.org/stunnel/archive/4.x/stunnel-4.50.tar.gz'
+  url 'ftp://ftp.stunnel.org/stunnel/archive/4.x/stunnel-4.52.tar.gz'
   homepage 'http://www.stunnel.org/'
-  md5 'd68b4565294496a8bdf23c728a679f53'
+  md5 'f5e713dda0e8efa659f372832ecd0c2c'
 
   # This patch installs a bogus .pem in lieu of interactive cert generation.
+  # - additionally stripping carriage-returns
   def patches
     DATA
   end
@@ -35,16 +36,22 @@ end
 
 __END__
 diff --git a/tools/stunnel.cnf b/tools/stunnel.cnf
-index 0c00347..d011421 100644
+index d8c3174..5ad26e0 100644
 --- a/tools/stunnel.cnf
 +++ b/tools/stunnel.cnf
-@@ -5,27 +5,15 @@ default_bits                    = 2048
- encrypt_key                     = no
- distinguished_name              = req_dn
- x509_extensions                 = cert_type
-+prompt                          = no
-
- [ req_dn ]
+@@ -1,42 +1,30 @@
+-# OpenSSL configuration file to create a server certificate
+-# by Michal Trojnara 1998-2012
+-
+-[ req ]
+-# the default key length is secure and quite fast - do not change it
+-default_bits                    = 2048
+-# comment out the next line to protect the private key with a passphrase
+-encrypt_key                     = no
+-distinguished_name              = req_dn
+-x509_extensions                 = cert_type
+-
+-[ req_dn ]
 -countryName = Country Name (2 letter code)
 -countryName_default             = PL
 -countryName_min                 = 2
@@ -64,12 +71,46 @@ index 0c00347..d011421 100644
 -
 -0.commonName                    = Common Name (FQDN of your server)
 -0.commonName_default            = localhost
+-
+-# To create a certificate for more than one name uncomment:
+-# 1.commonName                  = DNS alias of your server
+-# 2.commonName                  = DNS alias of your server
+-# ...
+-# See http://home.netscape.com/eng/security/ssl_2.0_certificate.html
+-# to see how Netscape understands commonName.
+-
+-[ cert_type ]
+-nsCertType                      = server
+-
++# OpenSSL configuration file to create a server certificate
++# by Michal Trojnara 1998-2012
++
++[ req ]
++# the default key length is secure and quite fast - do not change it
++default_bits                    = 2048
++# comment out the next line to protect the private key with a passphrase
++encrypt_key                     = no
++distinguished_name              = req_dn
++x509_extensions                 = cert_type
++prompt                          = no
++
++[ req_dn ]
 +countryName                     = PL
-+stateOrProvinceName              = Mazovia Province
-+localityName                     = Warsaw
-+organizationName                 = Stunnel Developers
-+organizationalUnitName           = Provisional CA
-+0.commonName                     = localhost
-
- # To create a certificate for more than one name uncomment:
- # 1.commonName                  = DNS alias of your server
++stateOrProvinceName             = Mazovia Province
++localityName                    = Warsaw
++organizationName                = Stunnel Developers
++organizationalUnitName          = Provisional CA
++0.commonName                    = localhost
++
++# To create a certificate for more than one name uncomment:
++# 1.commonName                  = DNS alias of your server
++# 2.commonName                  = DNS alias of your server
++# ...
++# See http://home.netscape.com/eng/security/ssl_2.0_certificate.html
++# to see how Netscape understands commonName.
++
++[ cert_type ]
++nsCertType                      = server
++
+--
+1.7.9
