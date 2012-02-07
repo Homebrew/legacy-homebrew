4976acfafd555e9897e4fed90d0eae9f5de79671
Douglas Creager <douglas.creager@redjack.com>
2012-02-07 18:45:56 -0500

    Install ragel user's guide

--- Library/Formula/ragel.rb
+++ Library/Formula/ragel.rb
@@ -1,5 +1,10 @@
 require 'formula'
 
+class RagelUserGuide < Formula
+  url 'http://www.complang.org/ragel/ragel-guide-6.7.pdf'
+  sha1 '6f3483fea075941c989ac37e6c49afabc7e181c0'
+end
+
 class Ragel < Formula
   url 'http://www.complang.org/ragel/ragel-6.7.tar.gz'
   homepage 'http://www.complang.org/ragel/'
@@ -8,5 +13,8 @@ class Ragel < Formula
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
+
+    # Install the prebuilt PDF documentation
+    RagelUserGuide.new.brew { doc.install Dir['*.pdf'] }
   end
 end
