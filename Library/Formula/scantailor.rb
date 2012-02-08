6a369c0b8ebc7a6b3b17b8f2e257d3e1f4563455
Misty De Meo <mistydemeo@gmail.com>
2012-02-07 17:21:38 -0800

    scantailor 0.9.11

--- Library/Formula/scantailor.rb
+++ Library/Formula/scantailor.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Scantailor < Formula
   homepage 'http://scantailor.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/scantailor/scantailor/0.9.10/scantailor-0.9.10.tar.gz'
-  md5 'f962c93a2d63b449fa3f6612ade3b028'
+  url 'http://downloads.sourceforge.net/project/scantailor/scantailor/0.9.11/scantailor-0.9.11.tar.gz'
+  md5 '15984c8828ecb2de542ac94e3c41a810'
 
   devel do
     url 'http://downloads.sourceforge.net/project/scantailor/scantailor-devel/enhanced/scantailor-enhanced-20110907.tar.gz'
