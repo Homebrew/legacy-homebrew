be9c7f31963443b89345bf9ba94c237886ae247e
Jack Nagel <jacknagel@gmail.com>
2012-02-07 19:30:08 -0600

    pango: apply patch from MacPorts
    Other formula that use pango with glib 2.30.x have issues when they see
    the deprecated G_CONST_RETURN; this fix should already be present in
    1.29.x.
    
    Also take the opportunity to reorder the formula a bit.
    
    Fixes #10032.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -8,18 +8,10 @@ class Pango < Formula
   devel do
     url 'http://ftp.gnome.org/pub/gnome/sources/pango/1.29/pango-1.29.4.tar.bz2'
     sha256 'f15deecaecf1e9dcb7db0e4947d12b5bcff112586434f8d30a5afd750747ff2b'
-
-    # Fixes font size rendering in lion. See the following post for details
-    # http://web.me.com/aschweiz/Website/Blog/Entries/2011/10/6_CoreText_vs._FontConfig.html
-    def patches
-      "http://web.me.com/aschweiz/Website/Blog/Entries/2011/10/6_CoreText_vs._FontConfig_files/pango-1.29.3-coretext.patch"
-    end if MacOS.lion?
   end
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
-
-  fails_with_llvm "Undefined symbols when linking", :build => "2326"
 
   if MacOS.leopard?
     depends_on 'fontconfig' # Leopard's fontconfig is too old.
@@ -30,6 +22,24 @@ class Pango < Formula
     #
     #   https://trac.macports.org/ticket/30370
     depends_on 'cairo'
+  end
+
+  fails_with_llvm "Undefined symbols when linking", :build => "2326"
+
+  def patches
+    p = {}
+    unless ARGV.build_devel?
+      # Some things that depend on pango and glib 2.30.x have issues with the deprecated
+      # G_CONST_RETURN. Shouldn't be an issue with 1.29.x.
+      p[:p0] = "https://trac.macports.org/export/89719/trunk/dports/x11/pango/files/patch-G_CONST_RETURN.diff"
+    end
+
+    if ARGV.build_devel? and MacOS.lion?
+      # Fixes font size rendering in lion. See the following post for details
+      # http://web.me.com/aschweiz/Website/Blog/Entries/2011/10/6_CoreText_vs._FontConfig.html
+      p[:p1] = "http://web.me.com/aschweiz/Website/Blog/Entries/2011/10/6_CoreText_vs._FontConfig_files/pango-1.29.3-coretext.patch"
+    end
+    return p
   end
 
   def install
