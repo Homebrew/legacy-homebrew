d446722e385b1e1d81e99279ba43f1db9c04397c
Alan Harper <alan@aussiegeek.net>
2012-02-08 16:07:14 +1100

    Put depends_on in correct place in jigdo formula

--- Library/Formula/jigdo.rb
+++ Library/Formula/jigdo.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Jigdo < Formula
-  depends_on 'berkeley-db'
   url 'http://atterer.org/sites/atterer/files/2009-08/jigdo/jigdo-0.7.3.tar.bz2'
   homepage 'http://atterer.org/jigdo/'
   md5 'bbc1d9ac750bb34e0748f0b57157cc72'
+
+  depends_on 'berkeley-db'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--mandir=#{man}",
