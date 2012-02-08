c89f2220d05590f312577e532e81bbdfa3225b7d
Jack Nagel <jacknagel@gmail.com>
2012-02-07 20:05:18 -0600

    msgpack: fails with LLVM on Lion
    msgpack fails to compile with even the latest LLVM, at least on Lion. I
    can build with LLVM on Snow Leopard.
    
    For reference, MacPorts forces clang on this one.
    
    Closes #10006.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/msgpack.rb
+++ Library/Formula/msgpack.rb
@@ -5,6 +5,9 @@ class Msgpack < Formula
   url 'http://msgpack.org/releases/cpp/msgpack-0.5.7.tar.gz'
   sha256 '7c203265cf14a4723820e0fc7ac14bf4bad5578f7bc525e9835c70cd36e7d1b8'
 
+  # fails with the latest LLVM, but only on Lion?
+  fails_with_llvm "Compilation fails" if MacOS.lion?
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
