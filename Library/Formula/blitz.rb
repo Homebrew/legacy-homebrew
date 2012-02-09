f58aad7f12621f05b07784b818ac20cf1ba16404
Rike-Benjamin Schuppner <rikebs@debilski.de>
2012-02-09 21:47:44 +0100

    blitz++: Add mercurial HEAD
    Closes #10076.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/blitz.rb
+++ Library/Formula/blitz.rb
@@ -4,8 +4,11 @@ class Blitz < Formula
   url 'http://downloads.sourceforge.net/project/blitz/blitz/Blitz%2B%2B%200.9/blitz-0.9.tar.gz'
   homepage 'http://oonumerics.org/blitz'
   md5 '031df2816c73e2d3bd6d667bbac19eca'
+  head 'http://blitz.hg.sourceforge.net:8000/hgroot/blitz/blitz', :using => :hg
 
   def install
+    system "/usr/bin/autoreconf", "-fi" if ARGV.build_head?
+
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--infodir=#{info}",
                           "--enable-shared",
