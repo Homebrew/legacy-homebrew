61d780a4f3380ef7305d2ae14aaa5699b7edf0d2
Jack Nagel <jacknagel@gmail.com>
2012-02-09 11:01:50 -0600

    tig: install html docs
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -1,14 +1,15 @@
 require 'formula'
 
 class Tig < Formula
-  url 'http://jonas.nitro.dk/tig/releases/tig-0.18.tar.gz'
   homepage 'http://jonas.nitro.dk/tig/'
+  url 'http://jonas.nitro.dk/tig/releases/tig-0.18.tar.gz'
   md5 '4fa9e33c5daa76b6fed11e068405356f'
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
     system "make install-doc-man"
+    system "make install-doc-html"
 
     (prefix+'etc/bash_completion.d').install 'contrib/tig-completion.bash'
   end
