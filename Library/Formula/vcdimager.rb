c7636f0cde5fb65ee8947f24a348f3de26fb7698
Adam Vandenberg <flangy@gmail.com>
2012-02-09 22:32:04 -0800

    vcdimager: add missing md5

--- Library/Formula/vcdimager.rb
+++ Library/Formula/vcdimager.rb
@@ -4,7 +4,7 @@ class Vcdimager < Formula
   homepage 'http://www.gnu.org/software/vcdimager/'
   url 'http://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz'
-  md5 ''
+  md5 '3af22978fd79c79d5fda6513b6811145'
 
   depends_on 'pkg-config' => :build
   depends_on 'libcdio'
