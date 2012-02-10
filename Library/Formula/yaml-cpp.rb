15be50cc2e36d09b3232d5727031debb2538c75e
Adam Vandenberg <flangy@gmail.com>
2012-02-09 18:56:25 -0800

    yaml-cpp 0.3.0

--- Library/Formula/yaml-cpp.rb
+++ Library/Formula/yaml-cpp.rb
@@ -1,19 +1,14 @@
 require 'formula'
 
 class YamlCpp < Formula
-  url 'http://yaml-cpp.googlecode.com/files/yaml-cpp-0.2.5.tar.gz'
+  url 'http://yaml-cpp.googlecode.com/files/yaml-cpp-0.3.0.tar.gz'
   homepage 'http://code.google.com/p/yaml-cpp/'
-  md5 'b17dc36055cd2259c88b2602601415d9'
+  sha1 '28766efa95f1b0f697c4b4a1580a9972be7c9c41'
 
   depends_on 'cmake' => :build
   depends_on 'libyaml'
 
   def install
-
-    # Fix for building on 10.6
-    # See: http://code.google.com/p/yaml-cpp/issues/detail?id=68 (Fixed in revision r363)
-    inreplace 'src/token.h', '<ios>', '<iostream>'
-
     system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
