923b201e758496b676dc0819d0df01f64b4264bf
Adam Vandenberg <flangy@gmail.com>
2012-02-09 21:30:54 -0800

    tnef 1.4.8

--- Library/Formula/tnef.rb
+++ Library/Formula/tnef.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Tnef < Formula
-  url 'http://downloads.sourceforge.net/project/tnef/tnef/v1.4.7/tnef-1.4.7.tar.gz'
-  md5 '00978a8ad8cc79a1ee605172f882ebe9'
   homepage 'http://sourceforge.net/projects/tnef/'
+  url 'http://downloads.sourceforge.net/project/tnef/tnef/tnef-1.4.8.tar.gz'
+  md5 'cc6443ac5f30913394c0c16ae7941e4f'
 
   def install
     system "./configure", "--prefix=#{prefix}"
