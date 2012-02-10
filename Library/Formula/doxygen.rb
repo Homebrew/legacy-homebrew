7b325a83ad11257b6923be512f3538cb332cc158
Jack Nagel <jacknagel@gmail.com>
2012-02-09 21:21:22 -0600

    doxygen: avoid inreplace
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/doxygen.rb
+++ Library/Formula/doxygen.rb
@@ -1,19 +1,19 @@
 require 'formula'
 
 class Doxygen < Formula
+  homepage 'http://www.doxygen.org/'
   url 'http://ftp.stack.nl/pub/users/dimitri/doxygen-1.7.6.1.src.tar.gz'
   sha1 '6203d4423d12315f1094b56a4d7393347104bc4a'
-  homepage 'http://www.doxygen.org/'
+
   head 'https://doxygen.svn.sourceforge.net/svnroot/doxygen/trunk'
 
   def install
     system "./configure", "--prefix", prefix
-    inreplace "Makefile" do |s|
-      # Path of man1 relative to already given prefix
-      s.change_make_var! 'MAN1DIR', 'share/man/man1'
-    end
-
-    system "make"
-    system "make install"
+    system "make", "CC=#{ENV.cc}",
+                   "CXX=#{ENV.cxx}",
+                   "CFLAGS=#{ENV.cflags}",
+                   "CXXFLAGS=#{ENV.cflags}"
+    # MAN1DIR, relative to the given prefix
+    system "make", "MAN1DIR=share/man/man1", "install"
   end
 end
