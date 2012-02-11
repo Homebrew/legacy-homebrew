0987c60b65d5a425a777a5b5a467839217a72c84
François-Xavier Thomas <fx.thomas@gmail.com>
2012-02-11 02:19:14 +0100

    asymptote 2.15
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/asymptote.rb
+++ Library/Formula/asymptote.rb
@@ -3,9 +3,9 @@ require 'formula'
 def TeX_installed?; return `which latex`.chomp != ''; end
 
 class Asymptote < Formula
-  url 'http://downloads.sourceforge.net/asymptote/asymptote-2.13.src.tgz'
   homepage 'http://asymptote.sourceforge.net/'
-  md5 '7ef34150583d6fd617eae23803a3ead3'
+  url 'http://downloads.sourceforge.net/asymptote/asymptote-2.15.src.tgz'
+  md5 '1adb969a4d7b17a3ae98728d1956bd77'
 
   depends_on 'readline'
   depends_on 'bdw-gc'
