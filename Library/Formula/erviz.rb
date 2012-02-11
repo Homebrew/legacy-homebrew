f953fec16efde21620fc1ec5424fc24bc1a9b4d9
Adam Vandenberg <flangy@gmail.com>
2012-02-11 12:17:16 -0800

    erviz: fix quoting and simplify

--- Library/Formula/erviz.rb
+++ Library/Formula/erviz.rb
@@ -8,11 +8,11 @@ class Erviz < Formula
   depends_on 'graphviz'
 
   def install
-    libexec.install %w(core cui).map { |n| "_setup_/common/bin/erviz-#{n}-1.0.6.jar" }
+    libexec.install Dir["_setup_/common/bin/*.jar"]
 
     (bin+'erviz').write <<-EOS.undent
       #!/bin/sh
-      java -Duser.language=en -Duser.country=US -cp "#{libexec}/erviz-cui-1.0.6.jar:#{libexec}/erviz-core-1.0.6.jar" jp.gr.java_conf.simply.erviz.cui.Main $@
+      java -Duser.language=en -Duser.country=US -cp "#{libexec}/erviz-cui-1.0.6.jar:#{libexec}/erviz-core-1.0.6.jar" jp.gr.java_conf.simply.erviz.cui.Main "$@"
     EOS
   end
 end
