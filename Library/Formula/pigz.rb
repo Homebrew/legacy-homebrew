4f1afa5454d43b3e7c91077bcc83262e0570313b
Adam Vandenberg <flangy@gmail.com>
2012-02-10 20:31:07 -0800

    pigz: style updates

--- Library/Formula/pigz.rb
+++ Library/Formula/pigz.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class Pigz < Formula
-  url 'http://www.zlib.net/pigz/pigz-2.2.3.tar.gz'
   homepage 'http://www.zlib.net/pigz/'
+  url 'http://www.zlib.net/pigz/pigz-2.2.3.tar.gz'
   md5 '8330a6c6a3e5f1954687aaba4b973a6f'
 
   def install
     system "make"
-    bin.install ["pigz", "unpigz"]
-    man1.install ["pigz.1"]
+    bin.install "pigz", "unpigz"
+    man1.install "pigz.1"
     ln_s 'pigz.1', man1+'unpigz.1'
   end
 end
