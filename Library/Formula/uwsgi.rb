d1a9b78f34b916e364121254e5b4668de9428fb7
Ali Asad Lotia <ali.asad.lotia@gmail.com>
2012-02-11 18:27:25 +0000

    uwsgi 1.0.3
    Closes #10111.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Uwsgi < Formula
-  url 'http://projects.unbit.it/downloads/uwsgi-1.0.2.1.tar.gz'
+  url 'http://projects.unbit.it/downloads/uwsgi-1.0.3.tar.gz'
   homepage 'http://projects.unbit.it/uwsgi/'
-  md5 '41648cb886c0d1e31a672bad2b45f4c9'
+  md5 'dfbfd8776b9653f4e618aa64c6a7c740'
 
   def install
     # Find the arch for the Python we are building against.
