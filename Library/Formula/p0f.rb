69212ca7ade1efeeb65cd40c2772b394687fe829
Adam Vandenberg <flangy@gmail.com>
2012-02-10 20:47:40 -0800

    p0f: style updates

--- Library/Formula/p0f.rb
+++ Library/Formula/p0f.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class P0f < Formula
-  url 'http://lcamtuf.coredump.cx/p0f3/releases/p0f-3.03b.tgz'
   homepage 'http://lcamtuf.coredump.cx/p0f3/'
+  url 'http://lcamtuf.coredump.cx/p0f3/releases/p0f-3.03b.tgz'
   md5 '034d068deb68badfbede6dcc89cc80cf'
 
   def install
     inreplace "config.h", "p0f.fp", "#{etc}/p0f/p0f.fp"
     system "./build.sh"
-    sbin.install ["p0f"]
-    (etc+"p0f").install ['p0f.fp']
+    sbin.install "p0f"
+    (etc+"p0f").install "p0f.fp"
   end
 end
