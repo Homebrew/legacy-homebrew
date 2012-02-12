cc409fb8415a0502619dad0bab31358df6f31186
Nibbles 2bits <nibbles@speakeasy.net>
2012-02-12 15:05:11 -0800

    pyqt 4.9.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pyqt.rb
+++ Library/Formula/pyqt.rb
@@ -4,9 +4,9 @@ require 'formula'
 # downloads disappear.
 
 class Pyqt < Formula
-  url 'http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-mac-gpl-4.9.tar.gz'
+  url 'http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-mac-gpl-4.9.1.tar.gz'
   homepage 'http://www.riverbankcomputing.co.uk/software/pyqt'
-  md5 'ef3e49bc7f2e45aa52b551781e034ccf'
+  sha1 '6c0dbf0edb9a0f07fb3ed95f6c3b4b5d0458dbe7'
 
   depends_on 'sip'
   depends_on 'qt'
