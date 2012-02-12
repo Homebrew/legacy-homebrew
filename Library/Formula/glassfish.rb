543ea4d03cc75ca033a78b4d9336f9e7f8496601
Adam Vandenberg <flangy@gmail.com>
2012-02-12 11:15:30 -0800

    glassfish: use install_symlink

--- Library/Formula/glassfish.rb
+++ Library/Formula/glassfish.rb
@@ -1,20 +1,18 @@
 require 'formula'
 
 class Glassfish < Formula
-  url 'http://download.java.net/glassfish/3.1.1/release/glassfish-3.1.1.zip'
   homepage 'http://glassfish.org/'
+  url 'http://download.java.net/glassfish/3.1.1/release/glassfish-3.1.1.zip'
   md5 'bf92c2c99b3d53b83bbc8c7e2124a897'
 
   skip_clean :all
 
   def install
     rm_rf Dir['bin/*.bat']
+
     libexec.install Dir['*']
-    bin.mkpath
-    Dir["#{libexec}/bin/*"].each do |f|
-      ln_s f, bin
-      chmod 0755, (bin + File.basename(f))
-    end
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
     inreplace "#{libexec}/bin/asadmin" do |s|
       s.change_make_var! 'AS_INSTALL', "#{libexec}/glassfish"
     end
