6a8c437b273e829ac76c6c462c59aaf31c3b52de
Adam Vandenberg <flangy@gmail.com>
2012-02-12 11:15:53 -0800

    gradle: use install_symlink

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -2,14 +2,13 @@ require 'formula'
 
 class Gradle < Formula
   homepage 'http://www.gradle.org/'
-  version '1.0-milestone-7'
   url 'http://repo.gradle.org/gradle/distributions/gradle-1.0-milestone-7-bin.zip'
+  version '1.0-milestone-7'
   md5 '3622dbf2f2ef30c5abf5d8b94e709e13'
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin lib]
-    bin.mkpath
-    ln_s libexec+('bin/gradle'), bin
+    bin.install_symlink libexec+'bin/gradle'
   end
 end
