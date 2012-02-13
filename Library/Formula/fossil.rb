2ca5c58caaaf74c52be4867dc72152b73689ed3f
HelgeG <helgeg@helgegudmundsen.com>
2012-02-13 13:32:40 +0000

    Fossil formula: Fixed url to point to correct source path for v1.21.

--- Library/Formula/fossil.rb
+++ Library/Formula/fossil.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Fossil < Formula
   homepage 'http://www.fossil-scm.org/'
-  url 'http://www.fossil-scm.org/download/fossil-src-20111213135356.tar.gz'
+  url 'http://www.fossil-scm.org/download/fossil-src-20111021125253.tar.gz'
   version '1.21'
   md5 'd3bd7d3bf60b523578f37315cd8a8f12'
   head 'fossil://http://www.fossil-scm.org/'
