9dc733cfa9d2658195782b79a4d314b8f1e7f35c
Dan Frankowski <dan@code42.com>
2012-02-13 14:50:53 -0600

    trying to get out of this merge hell

--- Library/Formula/tkdiff.rb
+++ Library/Formula/tkdiff.rb
@@ -1,16 +1,15 @@
 require 'formula'
 
 class Tkdiff < Formula
-  url 'http://downloads.sourceforge.net/project/tkdiff/tkdiff/4.2/tkdiff-4.2.tar.gz'
   homepage 'http://tkdiff.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/tkdiff/tkdiff/4.2/tkdiff-4.2.tar.gz'
   md5 'efc19226416afe3e98e4a29c112cfc3e'
 
   def install
-    bin.install('tkdiff')
+    bin.install 'tkdiff'
   end
 
   def test
-    # Test that the executable runs
     system "#{bin}/tkdiff --help"
   end
 end
