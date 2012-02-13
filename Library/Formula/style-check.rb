59f3f70844a1a2aa693d19b614c91011fa0f286e
Jack Nagel <jacknagel@gmail.com>
2012-02-12 23:39:09 -0600

    style-check: avoid inreplace
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/style-check.rb
+++ Library/Formula/style-check.rb
@@ -1,17 +1,14 @@
 require 'formula'
 
 class StyleCheck < Formula
-  url 'http://www.cs.umd.edu/~nspring/software/style-check-0.14.tar.gz'
   homepage 'http://www.cs.umd.edu/~nspring/software/style-check-readme.html'
+  url 'http://www.cs.umd.edu/~nspring/software/style-check-0.14.tar.gz'
   md5 'b88b0632b80abf9c8aaa2c5f2c3e2934'
 
   def install
-    inreplace "Makefile" do |s|
-      s.change_make_var! 'PREFIX', prefix
-      s.change_make_var! 'SYSCONFDIR', (etc+'style-check.d')
-    end
-    inreplace "style-check.rb", '/etc/style-check.d/', (etc+'style-check.d/')
-
-    system "make install"
+    inreplace "style-check.rb", '/etc/style-check.d/', etc+'style-check.d/'
+    system "make", "PREFIX=#{prefix}",
+                   "SYSCONFDIR=#{etc}/style-check.d",
+                   "install"
   end
 end
