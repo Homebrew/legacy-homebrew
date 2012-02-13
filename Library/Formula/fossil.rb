ac41b88a8bea1944b09eaf25565ca6dffac44884
Jack Nagel <jacknagel@gmail.com>
2012-02-13 13:47:11 -0600

    fossil: fix checksum
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/fossil.rb
+++ Library/Formula/fossil.rb
@@ -3,8 +3,9 @@ require 'formula'
 class Fossil < Formula
   homepage 'http://www.fossil-scm.org/'
   url 'http://www.fossil-scm.org/download/fossil-src-20111213135356.tar.gz'
+  md5 'b81c13dc145804911c0b941c804522d6'
   version '1.21'
-  md5 'd3bd7d3bf60b523578f37315cd8a8f12'
+
   head 'fossil://http://www.fossil-scm.org/'
 
   def install
