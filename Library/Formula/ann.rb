5134e5ca4b1e0560ffb92161edd494195443602a
Adam Vandenberg <flangy@gmail.com>
2012-02-12 21:12:02 -0800

    ann: fix tests

--- Library/Formula/ann.rb
+++ Library/Formula/ann.rb
@@ -1,17 +1,18 @@
 require 'formula'
 
 class Ann < Formula
-  url 'http://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.zip'
   homepage 'http://www.cs.umd.edu/~mount/ANN/'
+  url 'http://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.zip'
   md5 '31267ffbe4e6d04768b3ec21763e9343'
 
   def install
     system "make", "macosx-g++"
-    bin.install Dir["bin/*"]
-    prefix.install ["lib","sample","doc","include"]
+    prefix.install "bin", "lib", "sample", "doc", "include"
   end
 
   def test
-    system "#{bin}/ann_sample", "-df", "#{prefix}/sample/data.pts", "-qf", "query.pts"
+    Dir.chdir "#{prefix}/sample" do
+      system "#{bin}/ann_sample", "-df", "data.pts", "-qf", "query.pts"
+    end
   end
 end
