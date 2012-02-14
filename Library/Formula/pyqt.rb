77e1b8d2ef644bc918a7c168a1f9b7fbf23f09cd
Lincoln Mullen <lincoln@lincolnmullen.com>
2012-02-14 10:24:01 -0500

    fixed URL to source code which was returning a 404

--- Library/Formula/pyqt.rb
+++ Library/Formula/pyqt.rb
@@ -4,7 +4,7 @@ require 'formula'
 # downloads disappear.
 
 class Pyqt < Formula
-  url 'http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-mac-gpl-4.9.1.tar.gz'
+  url 'http://www.riverbankcomputing.co.uk/static/Downloads/PyQt3/PyQt-mac-gpl-4.9.1.tar.gz'
   homepage 'http://www.riverbankcomputing.co.uk/software/pyqt'
   sha1 '6c0dbf0edb9a0f07fb3ed95f6c3b4b5d0458dbe7'
 
