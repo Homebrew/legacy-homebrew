fb5afa5fa64bf5ee4b7151d91d541b6b301c8404
Adam Vandenberg <flangy@gmail.com>
2012-02-13 21:35:10 -0800

    id3tool: style update

--- Library/Formula/id3tool.rb
+++ Library/Formula/id3tool.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Id3tool < Formula
-  url 'http://nekohako.xware.cx/id3tool/id3tool-1.2a.tar.gz'
   homepage 'http://nekohako.xware.cx/id3tool/'
+  url 'http://nekohako.xware.cx/id3tool/id3tool-1.2a.tar.gz'
   md5 '061185562c0d0e6327406d9fc2f194b2'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
     system "make install"
   end
 end
