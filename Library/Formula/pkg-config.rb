cea77acc826fd6474d6833f0605949da9d126a6c
Jack Nagel <jacknagel@gmail.com>
2012-02-14 17:39:37 -0600

    pkg-config: add share/pkgconfig to pc-path
    According to the pkg-config docs, share/pkgconfig is also a valid
    location for pc files, and a few packages in Homebrew insist on sticking
    them there. Rather than patch these packages individually, just add
    HOMEBREW_PREFIX/share/pkgconfig to the default pc file search path.
    
    Fixes #10182.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -15,6 +15,7 @@ class PkgConfig < Formula
 
     paths = %W[
         #{HOMEBREW_PREFIX}/lib/pkgconfig
+        #{HOMEBREW_PREFIX}/share/pkgconfig
         /usr/local/lib/pkgconfig
         /usr/lib/pkgconfig
         /usr/X11/lib/pkgconfig
