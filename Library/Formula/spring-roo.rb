1bbc7bf6f9ca4b2a91a0dae39ac1facbe16728f8
Daniel Geske <dgeske@horngroup.com>
2012-02-14 16:55:40 -0500

    Spring Roo 1.2.1
    "The Spring Roo team is pleased to announce the availability
    1.2.1.RELEASE.  This is the first maintenance release for 1.2 and
    includes fixes for a number of issues in multi-module Maven support and
    JSF/PrimeFaces.  It also has  performance enhancements and has been
    updated to support the recent release of PrimeFaces 3.1 and Google App
    Engine 1.6.2.  Please note that to keep the distribution size small we
    have not included the reference documentation in the zip for this and
    future releases. The documentation, as always,  can be found on the
    Spring Roo site."
    
    Closes #10184.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/spring-roo.rb
+++ Library/Formula/spring-roo.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class SpringRoo < Formula
-  url 'http://s3.amazonaws.com/dist.springframework.org/release/ROO/spring-roo-1.2.0.RELEASE.zip'
-  version '1.2.0'
   homepage 'http://www.springsource.org/spring-roo'
-  sha1 '3fa1fccd9e69d3b5c83f5bb70f1f3d852178d400'
+  url 'http://s3.amazonaws.com/dist.springframework.org/release/ROO/spring-roo-1.2.1.RELEASE.zip'
+  sha1 'aa2ffb42d9fd1e5456767635974eef60e6a3001b'
+  version '1.2.1'
 
   def install
     rm Dir["bin/*.bat"]
     File.rename "bin/roo.sh", "bin/roo"
-    prefix.install %w[annotations bin bundle conf docs legal samples]
+    prefix.install %w[annotations bin bundle conf legal samples]
   end
 end
