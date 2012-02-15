ffa8348af773e5d20f24846a35563f57f2734284
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2012-02-15 21:25:07 +0000

    ice: updated ice formula to compile Java and Python libraries

--- Library/Formula/ice.rb
+++ Library/Formula/ice.rb
@@ -1,29 +1,40 @@
 require 'formula'
 
 class Ice < Formula
-  url 'http://www.zeroc.com/download/Ice/3.4/Ice-3.4.1.tar.gz'
+
+  url 'http://www.zeroc.com/download/Ice/3.4/Ice-3.4.2.tar.gz'
+  md5 'e97672eb4a63c6b8dd202d0773e19dc7'
   homepage 'http://www.zeroc.com'
-  md5 '3aae42aa47dec74bb258c1a1b2847a1a'
 
   depends_on 'berkeley-db'
   depends_on 'mcpp'
+  # other dependencies listed for Ice are for additional utilities not compiled
 
   def patches
-    # Patch for Ice-3.4.1 to work with Berkely DB 5.X.
-    "http://gist.github.com/raw/459204/44183ae997afb8ec19148fec498a11d67b5ae8bf/Ice-3.4.1-db5.patch"
+    # Patch for Ice-3.4.2 to work with Berkely DB 5.X rather than 4.X
+    "https://raw.github.com/gist/1619052/5be2a4bed2d4f1cf41ce9b95141941a252adaaa2/Ice-3.4.2-db5.patch"
+  end
+
+  def site_package_dir
+    "#{which_python}/site-packages"
+  end
+
+  def which_python
+    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
   end
 
   def options
     [
       ['--doc', 'Install documentation'],
-      ['--demo', 'Build demos']
+      ['--demo', 'Build demos'],
+      ['--java', 'Build java library'],
+      ['--python', 'Build python library']
     ]
   end
 
   def install
     ENV.O2
     inreplace "cpp/config/Make.rules" do |s|
-      s.gsub! "#OPTIMIZE", "OPTIMIZE"
       s.gsub! "/opt/Ice-$(VERSION)", prefix
       s.gsub! "/opt/Ice-$(VERSION_MAJOR).$(VERSION_MINOR)", prefix
     end
@@ -32,6 +43,7 @@ class Ice < Formula
     wb = 'config src include'
     wb += ' doc' if ARGV.include? '--doc'
     wb += ' demo' if ARGV.include? '--demo'
+
     inreplace "cpp/Makefile" do |s|
       s.change_make_var! "SUBDIRS", wb
     end
@@ -44,5 +56,33 @@ class Ice < Formula
       system "make"
       system "make install"
     end
+
+    if ARGV.include? '--java'
+      Dir.chdir "java" do
+        system "ant ice-jar"
+        Dir.chdir "lib" do
+          lib.install ['Ice.jar', 'ant-ice.jar']
+        end
+      end
+    end
+
+    if ARGV.include? '--python'
+
+      inreplace "py/config/Make.rules" do |s|
+        s.gsub! "/opt/Ice-$(VERSION)", prefix
+        s.gsub! "/opt/Ice-$(VERSION_MAJOR).$(VERSION_MINOR)", prefix
+      end
+
+      Dir.chdir "py" do
+        system "make"
+        system "make install"
+      end
+
+      # install python bits
+      Dir.chdir "#{prefix}/python" do
+        (lib + site_package_dir).install Dir['*']
+      end
+    end
+
   end
 end
