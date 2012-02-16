5c2d7204ee308763517abcc08ac80232b62f6686
Silvio Henrique Ferreira <shferreira@me.com>
2012-02-16 17:39:45 -0200

    treecc 0.3.10
    Closes #10238.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/treecc.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Treecc < Formula
+  url 'http://download.savannah.gnu.org/releases/dotgnu-pnet/treecc-0.3.10.tar.gz'
+  homepage 'http://gnu.org/software/dotgnu/treecc/treecc.html'
+  md5 'def09f2132f87d6a38a0718e2f14ee61'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    bin.install "treecc"
+  end
+
+  def test
+    system "#{bin}/treecc -v"
+  end
+end
