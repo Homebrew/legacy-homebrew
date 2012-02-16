8bbf66628e80f22218f8fe50b1f2e9c1a0d2f970
Jack Nagel <jacknagel@gmail.com>
2012-02-15 22:21:20 -0600

    Fix broken checksums
    Both of these were picked up by the checks added in 641d22f. While
    lowercase is the convention used across Homebrew, I wonder if the audit
    check should be more forgiving; after all, uppercase letters are valid.
    And the checksum validation code does an uppercase comparison.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gtkwave.rb
+++ Library/Formula/gtkwave.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Gtkwave < Formula
   homepage 'http://gtkwave.sourceforge.net/'
   url 'http://gtkwave.sourceforge.net/gtkwave-3.3.31.tar.gz'
-  md5 'brary/Caches/Homebrew/gtkwave-3.'
+  md5 'bbe65da9010a0037f9b5d0dd9da63e30'
 
   depends_on 'gtk+'
   depends_on 'xz'
