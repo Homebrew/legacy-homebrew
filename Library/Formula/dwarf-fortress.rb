8694b43678feda9089e1ade19ac99e712e630871
Misty De Meo <mistydemeo@gmail.com>
2012-02-15 19:16:57 -0600

    dwarf-fortress 0.34.01
    Also removes the patch which is no longer necessary.

--- Library/Formula/dwarf-fortress.rb
+++ Library/Formula/dwarf-fortress.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class DwarfFortress < Formula
-  url 'http://www.bay12games.com/dwarves/df_31_25_osx.tar.bz2'
+  url 'http://www.bay12games.com/dwarves/df_34_01_osx.tar.bz2'
   homepage 'http://www.bay12games.com/dwarves/'
-  md5 '673b098b8b9c07b4c6be507dba8c7657'
-  version '0.31.25'
+  md5 '3364e147de0bf02e375ba1e5b70c4dee'
+  version '0.34.01'
 
   def script; <<-EOS.undent
     #!/bin/sh
@@ -14,9 +14,6 @@ class DwarfFortress < Formula
   end
 
   def install
-    # Fixes: http://www.bay12games.com/dwarves/mantisbt/view.php?id=4103#c18417
-    inreplace 'df', 'DYLD_FALLBACK_', 'DYLD_' if MacOS.lion?
-
     (bin + 'dwarffortress').write script
 
     rm_rf 'sdl' # only contains a readme
