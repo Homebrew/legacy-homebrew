a51e0eba0c4595c69ce22d1e0d4175a7c747e522
Adam Vandenberg <flangy@gmail.com>
2012-02-16 20:40:45 -0800

    cowpatty: make md5 lowercase to fix audit warning

--- Library/Formula/cowpatty.rb
+++ Library/Formula/cowpatty.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Cowpatty < Formula
-  url 'http://www.willhackforsushi.com/code/cowpatty/4.3/cowpatty-4.3.tgz'
   homepage 'http://www.willhackforsushi.com/Cowpatty.html'
-  md5 'DECCAC0763A05EF7014107D347BF9190'
+  url 'http://www.willhackforsushi.com/code/cowpatty/4.3/cowpatty-4.3.tgz'
+  md5 'deccac0763a05ef7014107d347bf9190'
 
   def install
     inreplace "Makefile", "/usr/local/", "#{prefix}/"
