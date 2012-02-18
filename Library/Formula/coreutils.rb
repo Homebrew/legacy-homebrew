7777983fb821702c60668a272dbf9b428711d0f1
Jack Nagel <jacknagel@gmail.com>
2012-02-17 18:56:36 -0600

    Bite the bullet and declare xz as a build-time dep
    Determining build-time deps as part of the download strategy is probably
    a Homebrew 2 topic, so avoid the "you need to install xz" warning by
    declaring xz as a dep where used.
    
    The "don't use xz if there are alternatives" policy is still in effect,
    though.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/coreutils.rb
+++ Library/Formula/coreutils.rb
@@ -17,6 +17,8 @@ class Coreutils < Formula
   mirror 'http://ftp.gnu.org/gnu/coreutils/coreutils-8.15.tar.xz'
   sha256 '837eb377414eae463fee17d0f77e6d76bed79b87bc97ef0c23887710107fd49c'
 
+  depends_on 'xz' => :build
+
   def install
     system "./configure", "--prefix=#{prefix}", "--program-prefix=g"
     system "make install"
