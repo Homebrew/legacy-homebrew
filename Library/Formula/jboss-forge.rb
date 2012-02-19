e968aaa4fbe818a2955851027fec7341b8ebb7db
Brice Dutheil <brice.dutheil@gmail.com>
2012-02-19 18:29:07 +0100

    Updating Jboss Forge version from 1.0.0.Beta3 to 1.0.0.CR2

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class JbossForge < Formula
   homepage 'http://jboss.org/forge'
-  version '1.0.0.Beta3'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=1.0.0.Beta3&e=zip'
-  md5 '870855a1ff9417c5cfcdad9c3c683ccb'
+  version '1.0.0.CR2'
+  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=1.0.0.CR2&e=zip'
+  md5 'b667e58d5c9f6e514403ca3efafbd8a5'
 
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin modules jboss-modules.jar]
-    prefix.install %w{ licence.txt readme.txt }
+    prefix.install %w{ LICENSE.txt README.txt }
     bin.mkpath
     ln_s libexec+('bin/forge'), bin
   end
