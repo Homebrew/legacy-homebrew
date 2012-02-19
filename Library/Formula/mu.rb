98306886bfbe54d5994855a6788bb8bd0639c29a
Michael Ströck <michael@stroeck.com>
2012-02-19 12:39:13 +0100

    Update mu to 0.9.8.1.

--- Library/Formula/mu.rb
+++ Library/Formula/mu.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Mu < Formula
-  url 'http://mu0.googlecode.com/files/mu-0.9.7.tar.gz'
-  sha1 '8641d579a770d59124b72433712841736d326ca6'
+  url 'http://mu0.googlecode.com/files/mu-0.9.8.1.tar.gz'
+  sha1 '13c504e7feb07aee76554acc3dfe386289aee07b'
   homepage 'http://www.djcbsoftware.nl/code/mu/'
-  head 'git://gitorious.org/mu/old.git'
+  head 'https://github.com/djcb/mu.git'
 
   depends_on 'gettext'
   depends_on 'glib'
