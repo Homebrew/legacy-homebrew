fcc43dbee9d4ba182adae16e95ce4e71ccf0f511
Tim Mecking <tim@mecking.net>
2012-02-20 16:01:04 +0100

    New formula: GPTFDISK 0.8.2
    The new formula for version 0.8.2 of gptfdisk as requested:
    https://github.com/mxcl/homebrew/pull/9629#issuecomment-4046939

--- Library/Formula/gptfdisk.rb
+++ Library/Formula/gptfdisk.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gptfdisk < Formula
-  url 'http://sourceforge.net/projects/gptfdisk/files/gptfdisk/0.8.1/gptfdisk-0.8.1.tar.gz'
   homepage 'http://www.rodsbooks.com/gdisk/'
-  md5 'f556297cce00ef096de11d504d85b9a9'
+  url 'http://sourceforge.net/projects/gptfdisk/files/gptfdisk/0.8.2/gptfdisk-0.8.2.tar.gz'
+  md5 'a6a5beb047d65c0cda4eacc6b5625a19'
 
   depends_on 'popt'
   depends_on 'icu4c'
@@ -24,10 +24,10 @@ class Gptfdisk < Formula
   end
 end
 __END__
-diff -u a/Makefile.mac b/Makefile.mac
---- a/Makefile.mac	2012-01-17 00:38:25.000000000 +0100
-+++ b/Makefile.mac	2012-01-17 01:19:21.000000000 +0100
-@@ -1,8 +1,4 @@
+diff -u a/Makefile.mac b/Makefile.mac 
+--- a/Makefile.mac	2012-01-22 20:44:28.000000000 +0100
++++ b/Makefile.mac	2012-02-20 14:06:04.000000000 +0100
+@@ -1,11 +1,6 @@
 -CC=gcc
 -CXX=g++
  CFLAGS=-O2 -D_FILE_OFFSET_BITS=64 -g
@@ -35,23 +35,28 @@ diff -u a/Makefile.mac b/Makefile.mac
 -#CXXFLAGS=-O2 -Wall -D_FILE_OFFSET_BITS=64 -I /usr/local/include -I/opt/local/include -g
  LIB_NAMES=crc32 support guid gptpart mbrpart basicmbr mbr gpt bsd parttypes attributes diskio diskio-unix
  MBR_LIBS=support diskio diskio-unix basicmbr mbrpart
- #LIB_SRCS=$(NAMES:=.cc)
-@@ -15,15 +11,15 @@
+-#LIB_SRCS=$(NAMES:=.cc)
+ LIB_OBJS=$(LIB_NAMES:=.o)
+ MBR_LIB_OBJS=$(MBR_LIBS:=.o)
+ LIB_HEADERS=$(LIB_NAMES:=.h)
+@@ -14,17 +9,14 @@
+ all:	gdisk sgdisk cgdisk fixparts
  
  gdisk:	$(LIB_OBJS) gpttext.o gdisk.o
- #	$(CXX) $(LIB_OBJS) gpttext.o gdisk.o -o gdisk
+-#	$(CXX) $(LIB_OBJS) gpttext.o gdisk.o -o gdisk
 -	$(CXX) $(LIB_OBJS) -L/usr/lib -licucore gpttext.o gdisk.o -o gdisk
 +	$(CXX) $(LIB_OBJS) $(LDFLAGS) -licuuc -licuio gpttext.o gdisk.o -o gdisk
  
  cgdisk: $(LIB_OBJS) cgdisk.o gptcurses.o
- #	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o $(LDFLAGS) -lncurses -o sgdisk
+-#	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o $(LDFLAGS) -lncurses -o sgdisk
 -	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o $(LDFLAGS) -licucore -lncurses -o cgdisk
 +	$(CXX) $(LIB_OBJS) cgdisk.o gptcurses.o $(LDFLAGS) -licuuc -licuio -lncurses -o cgdisk
  
  sgdisk: $(LIB_OBJS) gptcl.o sgdisk.o
- #	$(CXX) $(LIB_OBJS) gptcl.o sgdisk.o -L/sw/lib -lpopt -o sgdisk
+-#	$(CXX) $(LIB_OBJS) gptcl.o sgdisk.o -L/sw/lib -lpopt -o sgdisk
 -	$(CXX) $(LIB_OBJS) gptcl.o sgdisk.o -L/sw/lib -licucore -lpopt -o sgdisk
+-
 +	$(CXX) $(LIB_OBJS) gptcl.o sgdisk.o $(LDFLAGS) -licuuc -licuio -lpopt -o sgdisk
- 
++	
  fixparts: $(MBR_LIB_OBJS) fixparts.o
  	$(CXX) $(MBR_LIB_OBJS) fixparts.o $(LDFLAGS) -o fixparts
