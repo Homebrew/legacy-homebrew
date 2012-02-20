be27b66c570ab61fbda152cfd98a82ad96d507ce
Jeanmonod David <david.jeanmonod@gmail.com>
2012-02-20 07:11:21 +0100

    Add a in replace action to adapt the path

--- Library/Formula/play2.rb
+++ Library/Formula/play2.rb
@@ -6,7 +6,15 @@ class Play2 < Formula
   md5 '8319bdefbad86c9f58d71b24c029a668'
 
   def install
+
+    # remove windows command file
     rm Dir['play.bat'] # remove windows command file
+
+    # Update the local folder var, as the current script doesn't allow
+    #  to be reference by relative symlinks
+    # More info here: https://play.lighthouseapp.com/projects/82401/tickets/43-play-shell-script-doesnt-work-when-symlinked
+    inreplace 'play', 'dir=`dirname $PRG`', "dir=#{prefix}/libexec"
+
     libexec.install Dir['*']
     bin.install_symlink libexec+'play'
   end
