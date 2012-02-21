87f52a7f221801f15a72543cae76a0b152f9ee0d
Martin Wilhelmi <mnin@mnin.de>
2012-02-21 11:13:39 +0100

    don't use clang 3.1 for bwm-ng
    Closes #10365.
    
    Signed-off-by: Max Howell <max@methylblue.com>

--- Library/Formula/bwm-ng.rb
+++ Library/Formula/bwm-ng.rb
@@ -6,6 +6,11 @@ class BwmNg < Formula
   md5 'd3a02484fb7946371bfb4e10927cebfb'
 
   def install
+    if MacOS.default_compiler == :clang
+      # auto-retest next clang version, submit patch if fails!
+      ENV.llvm if MacOS.clang_version.to_f <= 3.1
+    end
+
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
