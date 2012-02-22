e27443907da9937842adc6b38b87eda49be84cbc
Michael Koziarski <michael@koziarski.com>
2012-02-23 09:30:21 +1300

    Upgrade to riak 1.1.0

--- Library/Formula/riak.rb
+++ Library/Formula/riak.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Riak < Formula
   homepage 'http://wiki.basho.com/Riak.html'
-  url 'http://downloads.basho.com/riak/riak-1.0.3/riak-1.0.3.tar.gz'
-  md5 '433d2cb0c6eba0b6d374d9d37f7f54fe'
+  url 'http://downloads.basho.com/riak/riak-1.1.0/riak-1.1.0.tar.gz'
+  md5 'b240b920eb037e751fbd22ede6ebba61'
 
   head 'https://github.com/basho/riak.git'
 
@@ -14,9 +14,6 @@ class Riak < Formula
   skip_clean 'libexec/data/ring'
 
   depends_on 'erlang'
-
-  # Enable use of Erlang R14B04; fixed in the upstream 1.1.0 pre-release.
-  def patches; DATA; end
 
   def install
     ENV.deparallelize
@@ -38,17 +35,3 @@ class Riak < Formula
     man1.install Dir["doc/man/man1/*"]
   end
 end
-
-__END__
-diff --git a/rebar.config b/rebar.config
-index ee81bfc..31d2fae 100644
---- a/rebar.config
-+++ b/rebar.config
-@@ -1,6 +1,6 @@
- {sub_dirs, ["rel"]}.
- 
--{require_otp_vsn, "R14B0[23]"}.
-+{require_otp_vsn, "R14B0[234]"}.
- 
- {cover_enabled, true}.
- 
