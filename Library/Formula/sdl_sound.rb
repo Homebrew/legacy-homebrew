5b8c4ec91d9a79c6c6703362fe6f188ccd96d587
Nibbles 2bits <nibbles@speakeasy.net>
2012-02-21 22:56:22 -0800

    sdl_sound fix to handle missing autotools in XCode-4.3
    One dep and HEAD can't be built in the absence of autoreconf and
    aclocal.  This adds a conditional to the libmikmod dep so that
    it only is a dep if autoreconf and aclocal exist.  It adds another
    conditional to building HEAD, and bails out of the install if the
    required autotools are missing.  This builds and checks on SL and
    Lion using all five compilers from XCode-4.3 and XCode-4.0.2.
    
    EDIT: checks now for autorecon, aclocal, libtool.

--- Library/Formula/sdl_sound.rb
+++ Library/Formula/sdl_sound.rb
@@ -1,5 +1,9 @@
 require 'formula'
 
+def autotools_exist?
+  `which aclocal` != '' and `which autoreconf` != '' and `which libtool` != ''
+end
+
 class SdlSound < Formula
   url 'http://icculus.org/SDL_sound/downloads/SDL_sound-1.0.3.tar.gz'
   homepage 'http://icculus.org/SDL_sound/'
@@ -9,13 +13,25 @@ class SdlSound < Formula
   depends_on 'pkg-config' => :build
   depends_on 'sdl'
   depends_on 'flac' => :optional
-  depends_on 'libmikmod' => :optional
+  depends_on 'libmikmod' if autotools_exist?
   depends_on 'libogg' => :optional
   depends_on 'libvorbis' => :optional
   depends_on 'speex' => :optional
   depends_on 'physfs' => :optional
 
   def install
+    # Bail out if the tools to build head don't exist. Those tools
+    # can come from either the system or Homebrew.
+    if ARGV.build_head? and not autotools_exist?
+      onoe <<-EOS.undent
+        Can't build head without autoreconf and aclocal.  Those tools
+               are not part of XCode-4.3 or the Command Line Tools.
+                 * autoconf: is a part of adamv/homebrew-alt on github
+                 * aclocal:  is a part of automake. Check homebrew-alt\n
+      EOS
+      Process.exit
+    end
+
     system "./bootstrap" if ARGV.build_head?
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
