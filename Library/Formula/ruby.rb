baffa5ec1ec82a004469f4037630d5194269618c
Gerald Boersma <gerald.boersma@gmail.com>
2012-02-22 10:18:37 -0800

    Updated for Ruby 1.9.3 patch 125.

--- Library/Formula/ruby.rb
+++ Library/Formula/ruby.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Ruby < Formula
-  url 'http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.bz2'
+  url 'http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.bz2'
   homepage 'http://www.ruby-lang.org/en/'
   head 'http://svn.ruby-lang.org/repos/ruby/trunk/', :using => :svn
-  sha256 'ca8ba4e564fc5f98b210a5784e43dfffef9471222849e46f8e848b37e9f38acf'
+  sha256 'c67a59443052b5a9219eb4cee3892bdfbc6f250f0c8e214e02256a4cc7ef5526'
 
   depends_on 'readline'
   depends_on 'libyaml'
