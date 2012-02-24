fdcb1a3c931d31e574765536f361f80d1c5e69ce
jmoore <josh@glencoesoftware.com>
2012-02-24 09:24:06 +0100

    mcpp: fix handling of // comments
    While trying to get zeroc-ice34 to compile on 10.7, it
    was necessary to apply the clang patches from [1]. Building
    this outside of homebrew works fine, but fails under homebrew
    with a segmentation fault of slice2py.
    
    Suggestion from [1] was to apply the mcpp patch from [2]
    (a tarball). The patch has already been accepted to MacPorts
    and FreeBSD, but the patch from [3] does not apply cleanly.
    
    Instead, I uploaded it to gist.github.com (see [4]).
    
    [1]: http://www.zeroc.com/forums/patches/5647-patch-compiling-ice-clang-gcc4-7-a.html
    [2]: http://www.zeroc.com/download/Ice/3.4/ThirdParty-Sources-3.4.1.tar.gz
    [3]: http://www.freebsd.org/cgi/query-pr.cgi?pr=155934
    [4]: https://raw.github.com/gist/1899036/960203cf482f1cde771c09ccd78ba91069ab9162/patch.mcpp.2.7.2
    
    Signed-off-by: jmoore <josh@glencoesoftware.com>

--- Library/Formula/mcpp.rb
+++ Library/Formula/mcpp.rb
@@ -13,5 +13,22 @@ class Mcpp < Formula
 
   def patches
     "https://gist.github.com/raw/668341/e50827c8d9e8452befcab64bd8800b16d1f66d0e/mcpp-fix-stpcpy.patch"
+    "https://raw.github.com/gist/1899036/960203cf482f1cde771c09ccd78ba91069ab9162/patch.mcpp.2.7.2"
   end
+
+  def test
+      # Checks that the output from mcpp -C (with comments) is:
+      #
+      #   #line 1 "<stdin>"
+      #   //comment
+      #   test;
+      #
+      # and not:
+      #
+      #   #line 1 "<stdin>"
+      #   //commenttest;
+      #
+      system 'echo "test; //comment" | mcpp -C | wc -l | grep -q 3'
+  end
+
 end
