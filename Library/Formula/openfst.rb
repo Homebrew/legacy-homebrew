a2b3267688c2f6295eff0068149e4790e9504f8a
Brenton Partridge <bapartridge@gmail.com>
2012-02-25 00:43:00 -0500

    OpenFST: upgrade to 1.2.10

--- Library/Formula/openfst.rb
+++ Library/Formula/openfst.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Openfst < Formula
-  url 'http://openfst.cs.nyu.edu/twiki/pub/FST/FstDownload/openfst-1.1.tar.gz'
+  url 'http://openfst.cs.nyu.edu/twiki/pub/FST/FstDownload/openfst-1.2.10.tar.gz'
   homepage 'http://www.openfst.org/'
-  md5 '7491c12e0878ab594cc14ae88103c486'
+  md5 '2c73dca4cbfe3850b9b7f6988249c870'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
