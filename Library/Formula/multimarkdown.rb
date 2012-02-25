062172f556c13354bded325b88634de0e64831d0
janrito <janrito+github@gmail.com>
2012-02-25 13:39:14 +0000

    reverted change

--- Library/Formula/multimarkdown.rb
+++ Library/Formula/multimarkdown.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Multimarkdown < Formula
   homepage 'http://fletcherpenney.net/multimarkdown/'
   url 'https://github.com/fletcher/peg-multimarkdown/tarball/3.5'
-  md5 'fa4e93b38f2e1fec37d5a9441daa10a6'
+  md5 '3ba5725c96b1c12f5c2419be9753bf80'
   head 'https://github.com/fletcher/peg-multimarkdown.git', :branch => 'development'
 
   def install
