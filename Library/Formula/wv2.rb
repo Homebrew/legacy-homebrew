ee2c3abdc07a20a5fac07fb298a73d0e2330fb9c
Camillo Lugaresi <camillo.lugaresi@gmail.com>
2012-02-25 01:39:12 -0600

    Remove spurious libiconv dependencies
    OS X ships with iconv. glib failed to detect it, and this snowballed
    into a few formulas requiring a superfluous duplicate install of
    libiconv. All of the formulas in this commit compile without the
    libiconv dependency.
    
    jn:
    The libiconv shipped with 10.5, 10.6, and 10.7 is version 1.11, so there
    shouldn't be any cases of "10.x needs a newer libiconv but 10.y does
    not".
    
    In any case, we'll wait a week or so just in case any weird issues crop
    up, and if not, then we can remove the libiconv formula and blacklist
    it.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/wv2.rb
+++ Library/Formula/wv2.rb
@@ -1,18 +1,17 @@
 require 'formula'
 
 class Wv2 < Formula
-  url 'http://downloads.sourceforge.net/project/wvware/wv2/0.3.1/wv2-0.3.1.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/wvware/wv2-0.4.2.tar.bz2'
   homepage 'http://wvware.sourceforge.net/'
-  md5 '4a20200141cb1299055f2bf13b56989d'
+  md5 '850ed8e44a84e7bf0610747827018cbc'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'libgsf'
-  depends_on 'libiconv'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    ENV.append 'LDFLAGS', '-liconv -lgobject-2.0' # work around broken detection
+    system "cmake #{std_cmake_parameters} ."
     system "make install"
   end
 end
