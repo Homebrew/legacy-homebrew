69e0c09ba28aee983cbd06d0bc825d74d5b45e7a
Camillo Lugaresi <camillo.lugaresi@gmail.com>
2012-02-24 23:57:00 -0600

    Wine 1.4
    Finally, a new stable release. With this we can remove some old cruft
    from the formula. Also updating winetricks.
    
    Support for --devel is kept because we expect development releases to
    continue at the usual brisk pace.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/winetricks.rb
+++ Library/Formula/winetricks.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Winetricks < ScriptFileFormula
   homepage 'http://code.google.com/p/winetricks/'
-  url 'http://winetricks.googlecode.com/svn-history/r782/trunk/src/winetricks', :using => :curl
-  version '20120503'
+  url 'http://winetricks.googlecode.com/svn-history/r795/trunk/src/winetricks', :using => :curl
+  version '20120308'
   # this is the version stated in the file, but note that it is not always updated
 
   head 'http://winetricks.googlecode.com/svn/trunk/src/winetricks', :using => :curl
@@ -12,7 +12,7 @@ class Winetricks < ScriptFileFormula
 
   # Don't provide an md5 for the HEAD build
   unless ARGV.build_head?
-    sha256 '9d0c519811f63807832af747fa65147aacdafe17be4a567fc716af4d87a80070'
+    sha256 'c6df4d7a8c0d4218507edd12607cec677378d6cf7371324d9cd4a07c923e1bae'
   end
 
   def caveats; <<-EOS.undent
