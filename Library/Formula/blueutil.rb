2b662a63dd206ada14083d718df68e2b0bb122a5
Adam Vandenberg <flangy@gmail.com>
2012-02-26 07:36:40 -0800

    Blueutil 1.0.0

--- Library/Formula/blueutil.rb
+++ Library/Formula/blueutil.rb
@@ -1,8 +1,11 @@
 require 'formula'
 
 class Blueutil < Formula
-  head 'https://github.com/toy/blueutil.git'
   homepage 'https://github.com/toy/blueutil'
+  url 'https://github.com/toy/blueutil/tarball/v1.0.0'
+  sha1 '64df692f32e920590746ca9d7dc0ea19c3b5c909'
+
+  head 'https://github.com/toy/blueutil.git'
 
   def install
     # Set to build with SDK=macosx10.6, but it doesn't actually need 10.6
