b534a12c12f980dac5a90f06c8336b782489ff19
Linus Unnebäck <linus@folkdatorn.se>
2012-02-27 17:35:32 +0100

    --devel support for supertux

--- Library/Formula/supertux.rb
+++ Library/Formula/supertux.rb
@@ -12,16 +12,33 @@ class Supertux < Formula
   depends_on 'libogg'
   depends_on 'libvorbis'
 
+  devel do
+    url 'http://download.berlios.de/supertux/supertux-0.3.3.tar.bz2'
+    md5 'f3f803e629ee51a9de0b366a036e393d'
+    depends_on 'cmake' => :build
+    depends_on 'glew'
+    depends_on 'boost'
+  end
+
   def patches
     # Patch from macports port
     # https://trac.macports.org/browser/trunk/dports/games/supertux/Portfile
-    { :p0 => DATA }
+    { :p0 => DATA } if not ARGV.build_devel?
   end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make install"
+    if ARGV.build_devel?
+      system "cmake . #{std_cmake_parameters} -DINSTALL_SUBDIR_BIN=bin -DINSTALL_SUBDIR_SHARE=share/supertux2"
+      system "make"
+      bin.install ['supertux2']
+      share.mkpath
+      (share + 'supertux2').mkpath
+      (share + 'supertux2').install Dir['data/*']
+    else
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}"
+      system "make install"
+    end
   end
 
   def test
