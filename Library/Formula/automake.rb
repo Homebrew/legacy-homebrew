0b9bf5999cfd45ce6ce0506bea4b1810690e9e4b
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2012-02-27 10:04:27 +0100

    automake: update to 1.11.3
    Closes #10520.
    
    Signed-off-by: Max Howell <max@methylblue.com>

--- Library/Formula/automake.rb
+++ Library/Formula/automake.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Automake < Formula
   homepage 'http://www.gnu.org/software/automake/'
-  url 'http://ftpmirror.gnu.org/automake/automake-1.11.tar.gz'
-  md5 'fab0bd2c3990a6679adaf9eeac0c6d2a'
+  url 'http://ftpmirror.gnu.org/automake/automake-1.11.3.tar.gz'
+  md5 '93ecb319f0365cb801990b00f658d026'
 
   depends_on "autoconf"
 
