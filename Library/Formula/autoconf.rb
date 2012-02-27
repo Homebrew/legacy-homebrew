983bacd9c4e94629f5a1723708f992d22f638624
Max Howell <max@methylblue.com>
2012-02-27 12:20:49 +0000

    Force autoreconf to use glibtoolize and not libtoolize

--- Library/Formula/autoconf.rb
+++ Library/Formula/autoconf.rb
@@ -5,6 +5,11 @@ class Autoconf < Formula
   homepage 'http://www.gnu.org/software/autoconf'
   md5 'c3b5247592ce694f7097873aa07d66fe'
 
+  def patches
+    # force autoreconf to look for and use our glibtoolize
+    DATA
+  end
+
   if MacOS.xcode_version.to_f < 4.3 or File.file? "/usr/bin/autoconf"
     keg_only "Xcode (up to and including 4.2) provides (a rather old) Autoconf."
   end
@@ -18,3 +23,19 @@ class Autoconf < Formula
     system "#{HOMEBREW_PREFIX}/bin/autoconf --version"
   end
 end
+
+
+__END__
+diff --git a/bin/autoreconf.in b/bin/autoreconf.in
+index 192c8fa..70a1d72 100644
+--- a/bin/autoreconf.in
++++ b/bin/autoreconf.in
+@@ -112,7 +112,7 @@ my $autoheader = $ENV{'AUTOHEADER'} || '@bindir@/@autoheader-name@';
+ my $autom4te   = $ENV{'AUTOM4TE'}   || '@bindir@/@autom4te-name@';
+ my $automake   = $ENV{'AUTOMAKE'}   || 'automake';
+ my $aclocal    = $ENV{'ACLOCAL'}    || 'aclocal';
+-my $libtoolize = $ENV{'LIBTOOLIZE'} || 'libtoolize';
++my $libtoolize = $ENV{'LIBTOOLIZE'} || 'glibtoolize';
+ my $autopoint  = $ENV{'AUTOPOINT'}  || 'autopoint';
+ my $make       = $ENV{'MAKE'}       || 'make';
+ 
