efcbbc0180b6496dcf8412e49142e32aa58f81c9
Pavel "Uznick" Shvedov <u@uznick.com>
2012-02-28 04:29:08 +0400

    brew audit fixes

--- Library/Formula/ctpp2.rb
+++ Library/Formula/ctpp2.rb
@@ -1,13 +1,12 @@
 require 'formula'
 
 
-class Ctpp2 <Formula
+class Ctpp2 < Formula
   url 'http://ctpp.havoc.ru/download/ctpp2-2.8.tar.gz'
   homepage 'http://ctpp.havoc.ru'
   md5 'f9474c6dae89b7c514a997d505c32e76'
-  version '2.8'
 
-  depends_on 'cmake'
+  depends_on 'cmake' => :build
 
   def install
     system "cmake #{std_cmake_parameters} ."
