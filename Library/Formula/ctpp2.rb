51ac6297446f2b94bbaf29cfa3d47a1eb41144d0
Pavel "Uznick" Shvedov <u@uznick.com>
2012-02-28 04:26:10 +0400

    std_cmake_parameters, correct intendation and remove extra trash

--- Library/Formula/ctpp2.rb
+++ Library/Formula/ctpp2.rb
@@ -2,18 +2,16 @@ require 'formula'
 
 
 class Ctpp2 <Formula
-    url 'http://ctpp.havoc.ru/download/ctpp2-2.8.tar.gz'
-    homepage 'http://ctpp.havoc.ru'
-    md5 'f9474c6dae89b7c514a997d505c32e76'
-    version '2.8'
+  url 'http://ctpp.havoc.ru/download/ctpp2-2.8.tar.gz'
+  homepage 'http://ctpp.havoc.ru'
+  md5 'f9474c6dae89b7c514a997d505c32e76'
+  version '2.8'
 
-	depends_on 'cmake'
+  depends_on 'cmake'
 
-	def install
-    	system "cmake ."
-	    system "make"
-	    system "make install"
-	end
+  def install
+    system "cmake #{std_cmake_parameters} ."
+  end
 
 end
 
