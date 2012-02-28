9a0d3c8f17d9bd470b5c5f9d206ca8e18c1db9b4
George Song <george@55minutes.com>
2012-02-27 21:11:33 -0800

    Simplified installation of hg bash_completion

--- Library/Formula/mercurial.rb
+++ Library/Formula/mercurial.rb
@@ -25,9 +25,7 @@ class Mercurial < Formula
     ENV.minimal_optimization
 
     # install the completion script
-    bc_dir = prefix + 'etc/bash_completion.d'
-    bc_dir.install 'contrib/bash_completion'
-    mv bc_dir + 'bash_completion', bc_dir + 'hg-completion.bash'
+    (prefix + 'etc/bash_completion.d').install 'contrib/bash_completion' => 'hg-completion.bash'
 
     # Force the binary install path to the Cellar
     inreplace "Makefile",
