c276e457dfa18bd4ebecb8e0565a307cbfd2992f
Kensuke Nagae <kyanny@gmail.com>
2012-02-29 10:58:59 +0900

    weechat: options to enable script language plugins
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/weechat.rb
+++ Library/Formula/weechat.rb
@@ -10,6 +10,14 @@ class Weechat < Formula
   depends_on 'cmake' => :build
   depends_on 'gnutls'
 
+  def options
+    [
+      ["--enable-ruby", "Enable Ruby module"],
+      ["--enable-perl", "Enable Perl module"],
+      ["--enable-python", "Enable Python module"],
+    ]
+  end
+
   def install
     # Remove all arch flags from the PERL_*FLAGS as we specify them ourselves.
     # This messes up because the system perl is a fat binary with 32, 64 and PPC
@@ -22,12 +30,29 @@ class Weechat < Formula
       %Q{\n  STRING(REGEX REPLACE "#{archs}" "" PERL_CFLAGS "${PERL_CFLAGS}")} +
       %Q{\n  STRING(REGEX REPLACE "#{archs}" "" PERL_LFLAGS "${PERL_LFLAGS}")}
 
+    args = []
+
+    if ARGV.include? "--enable-ruby"
+      args << "-DENABLE_RUBY=ON"
+    else
+      args << "-DENABLE_RUBY=OFF"
+    end
+
+    if ARGV.include? "--enable-perl"
+      args << "-DENABLE_PERL=ON"
+    else
+      args << "-DENABLE_PERL=OFF"
+    end
+
+    if ARGV.include? "--enable-python"
+      args << "-DENABLE_PYTHON=ON"
+    else
+      args << "-DENABLE_PYTHON=OFF"
+    end
+
     # -DPREFIX has to be specified because weechat devs enjoy being non-standard
-    # Compiling langauge module doesn't work. Feel free to add options to enable these.
     system "cmake", "-DPREFIX=#{prefix}",
-                    "-DENABLE_RUBY=OFF",
-                    "-DENABLE_PERL=OFF",
-                    "-DENABLE_PYTHON=OFF",
+                    args.join(" "),
                     std_cmake_parameters,
                     "."
     system "make install"
