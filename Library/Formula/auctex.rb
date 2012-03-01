6a473b6b39b84dd3aeb6f310da92df9489eac95a
Adam Vandenberg <flangy@gmail.com>
2012-02-29 20:42:09 -0800

    auctex: more style nitpicks

--- Library/Formula/auctex.rb
+++ Library/Formula/auctex.rb
@@ -1,26 +1,23 @@
 require 'formula'
 
 class Auctex < Formula
+  homepage 'http://ftp.gnu.org/pub/gnu/auctex'
   url 'http://ftpmirror.gnu.org/auctex/auctex-11.86.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/auctex/auctex-11.86.tar.gz'
-  homepage 'http://ftp.gnu.org/pub/gnu/auctex'
   md5 '6bc33a67b6ac59db1aa238f3693b36d2'
 
   def options
-    [
-     ['--with-emacs=/full/path/to/emacs>', "Force a different emacs"]
-    ]
+    [['--with-emacs=</full/path/to/emacs>', "Force a different emacs"]]
   end
 
   def which_emacs
-    emacs = `which emacs`.chomp
     # check arguments for a different emacs
     ARGV.each do |a|
       if a.index('--with-emacs')
-        emacs = a.sub('--with-emacs=', '')
+        return a.sub('--with-emacs=', '')
       end
     end
-    return emacs
+    return `which emacs`.chomp
   end
 
   def install
@@ -34,13 +31,14 @@ class Auctex < Formula
       exit 1
     end
 
-    brew_lispdir = share + 'emacs/site-lisp'
-    brew_texmf = share + 'texmf'
     # configure fails if the texmf dir is not there yet
+    brew_texmf = share + 'texmf'
     brew_texmf.mkpath
 
-    system "./configure", "--prefix=#{prefix}", "--with-texmf-dir=#{brew_texmf}",
-                          "--with-emacs=#{which_emacs}", "--with-lispdir=#{brew_lispdir}"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-texmf-dir=#{brew_texmf}",
+                          "--with-emacs=#{which_emacs}",
+                          "--with-lispdir=#{share}/emacs/site-lisp"
 
     system "make"
     ENV.deparallelize # Needs a serialized install
@@ -53,24 +51,26 @@ class Auctex < Formula
     # be by default in the load-path
     if which_emacs.index("#{HOMEBREW_PREFIX}/bin")
       dot_emacs = <<-EOS
-        (require 'tex-site)
+      (require 'tex-site)
       EOS
     else
       dot_emacs = <<-EOS
-        (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
-        (require 'tex-site)
+      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
+      (require 'tex-site)
       EOS
     end
 
     <<-EOS.undent
-    * texmf files installed into
-        #{HOMEBREW_PREFIX}/share/texmf/
-      you can add it to your TEXMFHOME using:
-        sudo tlmgr conf texmf TEXMFHOME "~/Library/texmf:#{HOMEBREW_PREFIX}/share/texmf"
+    texmf files installed into:
+      #{HOMEBREW_PREFIX}/share/texmf/
 
-    * Emacs package installed into
-        #{HOMEBREW_PREFIX}/share/emacs/site-lisp
-      to activate add the following to your .emacs:
+    You can add it to your TEXMFHOME using:
+      sudo tlmgr conf texmf TEXMFHOME "~/Library/texmf:#{HOMEBREW_PREFIX}/share/texmf"
+
+    Emacs package installed into:
+      #{HOMEBREW_PREFIX}/share/emacs/site-lisp
+
+    To activate, add the following to your .emacs:
 #{dot_emacs}
     EOS
   end
