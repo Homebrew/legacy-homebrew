43d9d5f053aff3be9aac1dc0adc9eed215d1f62c
Mathieu Jobin <somekool@gmail.com>
2012-03-03 10:54:29 -0500

    Formula file for kgpg, this should not require anymore details.

---
+++ Library/Formula/kgpg.rb
@@ -0,0 +1,7 @@
+require 'base_kde_formula'
+
+class Kgpg < BaseKdeFormula
+  url 'http://download.kde.org/stable/4.8.0/src/kgpg-4.8.0.tar.bz2'
+  homepage 'http://utils.kde.org/projects/kgpg/'
+  md5 '26213dc56299768a5e9eb7d525a833ab'
+end
