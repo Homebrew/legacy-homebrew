655a35508f88efd914f6005c7b453f73b160fea6
Mathieu Jobin <somekool@gmail.com>
2012-03-03 11:02:08 -0500

    minor version bump

--- Library/Formula/ptlib.rb
+++ Library/Formula/ptlib.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Ptlib < Formula
-  url 'http://ftp.gnome.org/pub/gnome/sources/ptlib/2.10/ptlib-2.10.1.tar.bz2'
+  url 'http://ftp.gnome.org/pub/gnome/sources/ptlib/2.10/ptlib-2.10.2.tar.bz2'
   homepage 'http://wiki.ekiga.org/'
-  md5 '35dfe5e21dcf96ad1d4b87099819ff1a'
+  md5 '58cfea61b460ae9e4ac41cc877d00173'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
