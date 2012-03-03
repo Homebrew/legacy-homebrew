0f40756924e8a8ff224c3e4040f8a346b029563b
Leon <leon@radley.se>
2012-03-03 10:54:29 +0100

    ssh-copy-rsub

---
+++ Library/Formula/ssh-copy-rsub.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class SshCopyRsub < GithubGistFormula
+  homepage 'https://gist.github.com/1957476'
+  url 'https://raw.github.com/gist/1957476'
+  md5 '67e12ad319df1687c54eab4b24d0b064'
+end
