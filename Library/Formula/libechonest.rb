78d24870940b23b07cee2056f4036dec43471742
Mathieu Jobin <somekool@gmail.com>
2012-03-04 08:56:17 -0500

    strip down to use my BaseKdeFormula

--- Library/Formula/libechonest.rb
+++ Library/Formula/libechonest.rb
@@ -1,16 +1,9 @@
-require 'formula'
+require 'base_kde_formula'
 
-class Libechonest < Formula
+class Libechonest < BaseKdeFormula
   url 'http://pwsp.cleinias.com/libechonest-1.1.8.tar.bz2'
   homepage 'https://projects.kde.org/projects/playground/libs/libechonest'
   sha1 'ce79da389979e7deca2858b1d677312f027b6264'
-
-  depends_on 'cmake' => :build
   depends_on 'qt'
   depends_on 'qjson'
-
-  def install
-    system "cmake . #{std_cmake_parameters}"
-    system "make install"
-  end
 end
