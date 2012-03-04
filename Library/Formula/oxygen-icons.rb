8ebafedece843c62b182fa0823dc8d8ed45ae3d2
Mathieu Jobin <somekool@gmail.com>
2012-03-04 09:51:01 -0500

    update to 4.8.0 and strip down to use my BaseKdeFormula

--- Library/Formula/oxygen-icons.rb
+++ Library/Formula/oxygen-icons.rb
@@ -1,14 +1,7 @@
-require 'formula'
+require 'base_kde_formula'
 
-class OxygenIcons < Formula
-  url 'ftp://ftp.kde.org/pub/kde/stable/4.7.4/src/oxygen-icons-4.7.4.tar.bz2'
+class OxygenIcons < BaseKdeFormula
+  url 'ftp://ftp.kde.org/pub/kde/stable/4.8.0/src/oxygen-icons-4.8.0.tar.bz2'
   homepage 'http://www.oxygen-icons.org/'
-  md5 '1ab8d750781249d84becca7f7eb988ed'
-
-  depends_on 'cmake' => :build
-
-  def install
-    system "cmake . #{std_cmake_parameters}"
-    system "make install"
-  end
+  md5 '2ae26ba235f207eb29677637c1d059a7'
 end
