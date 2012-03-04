9a0974db4daf7296509436325b0a4d826591c007
Mathieu Jobin <somekool@gmail.com>
2012-03-04 09:43:22 -0500

    merge changes from nebiros

--- Library/Formula/kdelibs.rb
+++ Library/Formula/kdelibs.rb
@@ -18,5 +18,42 @@ class Kdelibs < BaseKdeFormula
   depends_on 'd-bus'
   depends_on 'qt'
   depends_on 'libdbusmenu-qt'
+  depends_on 'aspell' => :optional
+  depends_on 'enchant' => :optional
 
+  def patches
+    # macports patches.
+    {:p1 => [
+      "http://svn.macports.org/repository/macports/trunk/dports/kde/kdelibs4/files/add-bundles-to-path.patch",
+      "http://svn.macports.org/repository/macports/trunk/dports/kde/kdelibs4/files/default-kde4-xdg-menu-prefix.patch",
+      "http://svn.macports.org/repository/macports/trunk/dports/kde/kdelibs4/files/workaround-kdeinit4-crash.patch",
+      "http://svn.macports.org/repository/macports/trunk/dports/kde/kdelibs4/files/patch-cmake-modules-FindKDE4-Internal.cmake.diff",
+      "http://svn.macports.org/repository/macports/trunk/dports/kde/kdelibs4/files/patch-cmake-modules-FindPhonon.cmake.diff",
+      "http://svn.macports.org/repository/macports/trunk/dports/kde/kdelibs4/files/patch-cmake-modules-FindQt4.cmake.diff"
+    ]}
+  end
+  
+  def extra_cmake_args
+    "-DWITH_ENCHANT=ON -DWITH_Soprano=ON -DWITH_Avahi=OFF -DWITH_HSPELL=OFF -DWITH_FAM=OFF"
+  end
+
+  def install
+    opoo "Compile qt formula first with: --with-qtdbus and --with-qt3support flags."
+    puts <<-EOS.undent
+      Enable QtDBus module (--with-qtdbus) and Qt3Support module (--with-qt3support)
+      in qt formula first to avoid errors in kdevplatform and kdevelop formulas.
+    EOS
+    default_install
+  end
+
+  def caveats; <<-EOS.undent
+      First add /usr/local path to your $KDEDIRS in your ~/.bash_profile file.
+        export KDEDIRS="$KDEDIRS:$HOME/Library/Preferences/KDE:/usr/local"
+  
+      Now remember to run kbuildsycoca4 command, if you have some issues with kdevelop
+      formula or kate formula about mime types, try to update mime types:
+        $ update-mime-database /usr/local/share/mime
+        $ kbuildsycoca4 --noincremental
+    EOS
+  end
 end
