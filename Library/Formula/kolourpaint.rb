80f1d4632b60ee2b762d8b5310cb62f64096606a
Mathieu Jobin <somekool@gmail.com>
2012-03-04 08:55:11 -0500

    update to 4.8.0 and strip down to use my BaseKdeFormula

--- Library/Formula/kolourpaint.rb
+++ Library/Formula/kolourpaint.rb
@@ -1,26 +1,10 @@
-require 'formula'
+require 'base_kde_formula'
 
-class Kolourpaint < Formula
-  url 'http://download.kde.org/stable/4.7.4/src/kolourpaint-4.7.4.tar.bz2'
+class Kolourpaint < BaseKdeFormula
+  url 'http://download.kde.org/stable/4.8.0/src/kolourpaint-4.8.0.tar.bz2'
   homepage 'http://www.kolourpaint.org/'
-  md5 'fff4e25a8f5158944997b81552dac776'
+  md5 '3b5f5f0d008a1e19e08bcea85ed33dde'
 
-  depends_on 'cmake' => :build
-  depends_on 'automoc4' => :build
   depends_on 'kdelibs'
   depends_on 'kde-runtime'
-
-  def install
-    #ENV.x11
-    mkdir 'build'
-    cd 'build'
-    kdelibs = Formula.factory 'kdelibs'
-    system "cmake .. -DCMAKE_INSTALL_PREFIX=#{kdelibs.prefix} -DBUILD_doc=FALSE -DBUNDLE_INSTALL_DIR=#{bin}"
-    system "make install"
-  end
-
-  def caveats; <<-EOS.undent
-    Remember to run brew linkapps.
-    EOS
-  end
 end
