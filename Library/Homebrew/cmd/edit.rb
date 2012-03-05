f38b98fbf95f3cee0407f122a20a477aa4518eb6
Max Howell <max@methylblue.com>
2012-03-05 16:02:46 +0000

    `brew edit adamv/alt/ruby` works
    The reason it didn't work is it doesn't use ARGV.formulae because it needs to be able to edit stuff that doesn't parse. Most other stuff already works because we are awesome.

--- Library/Homebrew/cmd/edit.rb
+++ Library/Homebrew/cmd/edit.rb
@@ -17,7 +17,12 @@ module Homebrew extend self
     else
       # Don't use ARGV.formulae as that will throw if the file doesn't parse
       paths = ARGV.named.map do |name|
-        HOMEBREW_REPOSITORY+"Library/Formula/#{Formula.canonical_name name}.rb"
+        name = Formula.canonical_name name
+        if name.include? '/'
+          Pathname.new(name)
+        else
+          HOMEBREW_REPOSITORY+"Library/Formula/#{name}.rb"
+        end
       end
       unless ARGV.force?
         paths.each do |path|
