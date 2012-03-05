ccd9ba4feb5f47c33e42a93ba9fd052b53c92baa
rhysd <u033263f@ecs.cmc.osaka-u.ac.jp>
2012-03-06 08:28:15 +0900

    fixed some issues. #10705
    Thank you, Jack Nagel. I fixed some issues he pointed.
    
     - remove comments
     - change indentation to 2sp
     - use :optional for optional dependencies
     - change url. now url points at a stable zipball

--- Library/Formula/pficommon.rb
+++ Library/Formula/pficommon.rb
@@ -1,74 +1,67 @@
 require 'formula'
 
-# This is a general purpose C++ library provided by Preferred Infrastructure.
-
 class Pficommon < Formula
-    homepage 'http://pfi.github.com/pficommon/'
-    url 'https://github.com/pfi/pficommon.git', :using => :git
-    version '1.3.1'
+  homepage 'http://pfi.github.com/pficommon/'
+  url 'https://github.com/pfi/pficommon/zipball/v1.3.1.0'
+  md5 'e53e92351ddf157784fe791b778dfe8d'
+  version '1.3.1'
 
-    # add optional dependency
-    ARGV.each do |arg|
-        if arg =~ /^--([a-z]+)$/
-            depends_on $1
-        end
+  head 'git://github.com/pfi/pficommon.git'
+
+  depends_on 'mysql' => :optional
+  depends_on 'postgresql' => :optional
+  depends_on 'msgpack' => :optional
+  depends_on 'fcgi' => :optional
+  depends_on 'imagemagick' => :optional
+  depends_on 'gtest' => :optional
+
+  def options
+    [
+      ['--mysql', 'database library with mysql'],
+      ['--postgresql', 'database library with postgresql'],
+      ['--msgpack', 'library using msgpack'],
+      ['--fcgi', 'library for fcgi'],
+      ['--imagemagick', 'visualization with imagemagick'],
+      ['--gtest', 'execute unit test using gtest']
+    ]
+  end
+
+  def install
+
+    args = [
+      "--prefix=#{prefix}"
+    ]
+
+    if not ( ARGV.include?('--mysql') or ARGV.include?('--postgresql') )
+      args << '--disable-database'
     end
 
-    def options
-        [
-            ['--mysql', 'database library with mysql'],
-            ['--postgresql', 'database library with postgresql'],
-            ['--msgpack', 'library using msgpack'],
-            ['--fcgi', 'library for fcgi'],
-            ['--gtest', 'execute unit test using gtest']
-        ]
+    if ARGV.include? '--fcgi'
+      fcgi = Formula.factory 'fcgi'
+      args << "--with-fcgi=#{fcgi.prefix}"
+    else
+      args << '--disable-fcgi'
     end
 
-    def install
-
-        args = [
-            "--prefix=#{prefix}",
-            "--disable-magickpp",
-            "--oldincludedir=/usr/local/include"
-        ]
-
-        if not ( ARGV.include?('--mysql') or ARGV.include?('--postgresql') )
-            args << '--disable-database'
-        end
-
-        if ARGV.include? '--fcgi'
-            fcgi = Formula.factory 'fcgi'
-            args << "--with-fcgi=#{fcgi.prefix}"
-        else
-            args << '--disable-fcgi'
-        end
-
-        if ARGV.include? '--msgpack'
-            msgpack = Formula.factory 'msgpack'
-            args << "--with-msgpack=#{msgpack.prefix}"
-        end
-
-        # check is executed when library is built and installed
-        if ARGV.include? '--gtest'
-            args << '--checkall'
-        end
-
-        # system "./waf configure", *args
-        # system "./waf build -j4"
-        # system "./waf install"
-        system "./configure", *args
-        system "make"
-        system "make install"
-
-        # install everything
-        prefix.install Dir['*']
+    if ARGV.include? '--msgpack'
+      msgpack = Formula.factory 'msgpack'
+      args << "--with-msgpack=#{msgpack.prefix}"
     end
 
-    def caveats
-        <<-EOS.undent
-        This is a general purpose C++ library provided by Preferred Infrastructure ( http://preferred.jp ).
-        LICENSE: New BSD License
-        EOS
+    unless ARGV.include? '--imagemagick'
+      args << "--disable-magickpp"
     end
+
+    # check is executed when library is built and installed
+    if ARGV.include? '--gtest'
+      args << '--checkall'
+    end
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+
+    prefix.install Dir['*']
+  end
 
 end
