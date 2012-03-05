3bd69f256fd816ebe6717f8bbf14fa044aa9aa7a
Jack Nagel <jacknagel@gmail.com>
2012-03-04 20:47:42 -0600

    haproxy 1.4.19
    Add missing dep on pcre, closing #10684.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/haproxy.rb
+++ Library/Formula/haproxy.rb
@@ -2,8 +2,10 @@ require 'formula'
 
 class Haproxy < Formula
   homepage 'http://haproxy.1wt.eu'
-  url 'http://haproxy.1wt.eu/download/1.4/src/haproxy-1.4.17.tar.gz'
-  md5 '7f82319fdb53956b14f2088b654be341'
+  url 'http://haproxy.1wt.eu/download/1.4/src/haproxy-1.4.19.tar.gz'
+  md5 '41392d738460dbf99295fd928031c6a4'
+
+  depends_on 'pcre'
 
   def install
     # We build generic since the Makefile.osx doesn't appear to work
