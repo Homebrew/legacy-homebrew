153885df2641ee3a0e5f8a9029b1e120972b33ed
Clay McClure <clay@daemons.net>
2012-03-06 12:27:04 -0500

    sphinx: use llvm
    The changes outlined in https://github.com/mxcl/homebrew/issues/10016
    allow sphinx to build on OS X Lion 10.7.3 with XCode 4.3.
    
    I wrote the configure patch to address upstream issue:
    
      http://sphinxsearch.com/bugs/view.php?id=1123
    
    Some discussion of the fix can be found here:
    
      https://github.com/mxcl/homebrew/pull/10698
    
    Also removes the unknown configure option --disable-debug.
    
    Fixes #10016.

--- Library/Formula/sphinx.rb
+++ Library/Formula/sphinx.rb
@@ -19,12 +19,18 @@ class Sphinx < Formula
   fails_with_llvm "ld: rel32 out of range in _GetPrivateProfileString from /usr/lib/libodbc.a(SQLGetPrivateProfileString.o)",
     :build => 2334
 
+  # Patch the configure script to run under clang, preventing the error:
+  #   configure: error: Gcc version error. Minspec is 3.4
+  # Reported to sphinx upstream: http://sphinxsearch.com/bugs/view.php?id=1123
+  # Some discussion here: https://github.com/mxcl/homebrew/issues/10016
+  # and here: https://github.com/mxcl/homebrew/pull/10698
+  def patches; DATA; end
+
   def install
     lstem = Pathname.pwd+'libstemmer_c'
     Libstemmer.new.brew { lstem.install Dir['*'] }
 
     args = ["--prefix=#{prefix}",
-            "--disable-debug",
             "--disable-dependency-tracking",
             "--localstatedir=#{var}"]
 
@@ -34,6 +40,15 @@ class Sphinx < Formula
     # configure script won't auto-select PostgreSQL
     args << "--with-pgsql" if which 'pg_config'
     args << "--without-mysql" unless which 'mysql'
+
+    # Sphinx 2.0.3 does not build under clang 3.1. It fails with:
+    #   sphinxexpr.cpp:1799:11: error: use of undeclared identifier 'ExprEval'
+    # Some discussion here: https://github.com/mxcl/homebrew/issues/10016
+    # and here: https://github.com/mxcl/homebrew/pull/10698
+    # FIXME This should be replaced with fails_with_clang once available
+    if ENV.compiler == :clang
+      ENV.llvm
+    end
 
     system "./configure", *args
     system "make install"
@@ -59,3 +74,18 @@ class Sphinx < Formula
     EOS
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index aebac75..82d6d05 100755
+--- a/configure
++++ b/configure
+@@ -4361,7 +4361,7 @@ cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+ 
+ #ifdef __GNUC__
+ #if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ > 3)
+-void main() {}
++int main() {}
+ #else
+ syntax error
+ #endif
