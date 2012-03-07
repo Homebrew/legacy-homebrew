5508cd4542aeccfae639d6df790a528a5d8d50b2
Samuel Maftoul <samuel.maftoul@gmail.com>
2012-03-07 22:39:48 +0100

    fixed sed ; added caveats and plist

--- Library/Formula/seeks.rb
+++ Library/Formula/seeks.rb
@@ -23,7 +23,49 @@ class Seeks < Formula
                           "--prefix=#{prefix}"
     system "make"
     system "make install"
-    system "sed -i -e 's/^#\(activated-plugin httpserv\)$/\1/' #{prefix}/etc/config"
-    system "sed -i -e 's/^# automatic-proxy-disable 1$/automatic-proxy-disable 0/' #{prefix}/etc/config"
+    system "sed -i -e 's/#\\(activated-plugin httpserv\\)/\\1/' #{prefix}/etc/seeks/config"
+    system "sed -i -e 's/^# automatic-proxy-disable 1$/automatic-proxy-disable 0/' #{prefix}/etc/seeks/config"
+
+    plist_path.write startup_plist
+    plist_path.chmod 0644
+
+  end
+
+  def caveats; <<-EOS.undent
+    You can enable seeks to automatically load on login with:
+
+      mkdir -p ~/Library/LaunchAgents
+      cp "#{plist_path}" ~/Library/LaunchAgents/
+      launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    Or start it manually with:
+      #{bin}/seeks
+    EOS
+  end
+
+
+  def startup_plist
+    return <<-EOPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <true/>
+    <key>UserName</key>
+    <string>#{`whoami`.chomp}</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>#{HOMEBREW_PREFIX}/bin/seeks</string>
+    </array>
+    <key>WorkingDirectory</key>
+    <string>#{HOMEBREW_PREFIX}</string>
+  </dict>
+</plist>
+    EOPLIST
   end
 end
