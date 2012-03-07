c4363af4bc446bf3e064710f14f2cf76ee1aa63d
paul cannon <paul@datastax.com>
2012-03-07 14:02:44 -0600

    cassandra: update to 1.0.8

--- Library/Formula/cassandra.rb
+++ Library/Formula/cassandra.rb
@@ -1,14 +1,17 @@
 require 'formula'
 
 class Cassandra < Formula
-  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/1.0.7/apache-cassandra-1.0.7-bin.tar.gz'
+  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/1.0.8/apache-cassandra-1.0.8-bin.tar.gz'
   homepage 'http://cassandra.apache.org'
-  sha1 '8ae535821b29eead5b7dc7788a0e66ecf1ac267e'
+  sha1 '49647719d4813b879857073315ce0e8b67eca15a'
+
+  depends_on 'cql' => :python
 
   def install
     (var+"lib/cassandra").mkpath
     (var+"log/cassandra").mkpath
     (etc+"cassandra").mkpath
+    (share+"cassandra").mkpath
 
     inreplace "conf/cassandra.yaml", "/var/lib/cassandra", "#{var}/lib/cassandra"
     inreplace "conf/log4j-server.properties", "/var/log/cassandra", "#{var}/log/cassandra"
@@ -30,14 +33,11 @@ class Cassandra < Formula
     (etc+"cassandra").install Dir["conf/*"]
     prefix.install Dir["*.txt"] + Dir["{bin,interface,javadoc,pylib,lib/licenses}"]
     prefix.install Dir["lib/*.jar"]
+    (bin+'cqlshrc.sample').chmod 0644
+    mv [bin+'cqlshrc.sample', bin+'cassandra.in.sh', bin+'stop-server'], share+"cassandra"
 
     plist_path.write startup_plist
     plist_path.chmod 0644
-  end
-
-  def patches
-    # let cqlsh know where to find cqlshlib
-    DATA
   end
 
   def caveats; <<-EOS.undent
@@ -45,13 +45,6 @@ class Cassandra < Formula
       mkdir -p ~/Library/LaunchAgents
       cp #{plist_path} ~/Library/LaunchAgents/
       launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
-
-    If you plan to use the CQL shell (cqlsh), you will need the Python CQL library
-    installed. Since Homebrew prefers using pip for Python packages, you can
-    install that using:
-
-      pip install cql
-
     EOS
   end
 
@@ -82,18 +75,3 @@ class Cassandra < Formula
     EOPLIST
   end
 end
-
-__END__
-diff --git a/bin/cqlsh b/bin/cqlsh
-index 0f02a0c..f7fb469 100755
---- a/bin/cqlsh
-+++ b/bin/cqlsh
-@@ -47,7 +47,7 @@ import ConfigParser
- 
- # cqlsh should run correctly when run out of a Cassandra source tree,
- # out of an unpacked Cassandra tarball, and after a proper package install.
--cqlshlibdir = os.path.join(os.path.dirname(__file__), '..', 'pylib')
-+cqlshlibdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'pylib')
- if os.path.isdir(cqlshlibdir):
-     sys.path.insert(0, cqlshlibdir)
- 
