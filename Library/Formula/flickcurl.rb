a50d5b2e4afbe0dba0e4fb01cb6baa9a70af94ea
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2012-03-07 00:36:50 -0500

    New Formula: flickcurl 1.22
    flickcurl is a C library and a set of utility programs for the Flickr API.

---
+++ Library/Formula/flickcurl.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Flickcurl < Formula
+  homepage 'http://librdf.org/flickcurl/'
+  url 'http://download.dajobe.org/flickcurl/flickcurl-1.22.tar.gz'
+  md5 '33106156f9a9e538b5787f92db717f5d'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'libxml2'  => :build
+  depends_on 'curl' => :build
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "/usr/local/Cellar/flickcurl/1.22/bin/flickcurl -h"
+  end
+end
