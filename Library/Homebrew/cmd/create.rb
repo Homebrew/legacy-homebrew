c5a21ea1acdecc79cdb15abba0aaabe393f343f5
Adam Vandenberg <flangy@gmail.com>
2012-03-07 18:52:21 +0100

    Use sha1 as the default checksum for new formulae
    See: https://github.com/mxcl/homebrew/pull/12587

--- Library/Homebrew/cmd/create.rb
+++ Library/Homebrew/cmd/create.rb
@@ -57,7 +57,7 @@ end
 
 class FormulaCreator
   attr :url
-  attr :md5
+  attr :sha1
   attr :name, true
   attr :path, true
   attr :mode, true
@@ -89,7 +89,7 @@ class FormulaCreator
 
     unless ARGV.include? "--no-fetch" and version
       strategy = detect_download_strategy url
-      @md5 = strategy.new(url, name, version, nil).fetch.md5 if strategy == CurlDownloadStrategy
+      @sha1 = strategy.new(url, name, version, nil).fetch.sha1 if strategy == CurlDownloadStrategy
     end
 
     path.write ERB.new(template, nil, '>').result(binding)
@@ -104,7 +104,7 @@ class FormulaCreator
     class #{Formula.class_s name} < Formula
       homepage ''
       url '#{url}'
-      md5 '#{md5}'
+      sha1 '#{sha1}'
 
     <% if mode == :cmake %>
       depends_on 'cmake' => :build
