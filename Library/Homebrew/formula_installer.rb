ff63fddd48fcbb8dd5f9c69d6789b7bc4503066f
Max Howell <max@methylblue.com>
2012-03-07 11:16:27 +0000

    Stop supporting `brew install --force'
    Rationale: it breaks stuff. You should not be able to install over something already installed *and* linked. brew now prompts you to unlink the existing linked keg first.
    
    I came to fix this because I discovered that `brew install foo` works without --force provided foo's formula version is different to that which is installed and linked. I'm not sure if this was intentionally broken, but it led to at least two tickets I found where people were getting crazy issues trying to install over the top of already installed previous versions.
    
    So I also fixed a whole category of other issues, mostly by moving error handling into FormulaInstaller. Now error's can be caught in Homebrew.install and handled on a formula-by-formula basis. I will next port this behaviour to upgrade.

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -16,10 +16,40 @@ class FormulaInstaller
     @show_header = true
     @ignore_deps = ARGV.include? '--ignore-dependencies' || ARGV.interactive?
     @install_bottle = !ARGV.build_from_source? && ff.bottle_up_to_date?
+
+    check_install_sanity
+  end
+
+  def check_install_sanity
+    if f.installed?
+      raise CannotInstallFormulaError, "#{f}-#{f.version} already installed"
+    end
+
+    # Building head-only without --HEAD is an error
+    if not ARGV.build_head? and f.standard.nil?
+      raise CannotInstallFormulaError, <<-EOS.undent
+        #{f} is a head-only formula
+        Install with `brew install --HEAD #{f.name}
+      EOS
+    end
+
+    # Building stable-only with --HEAD is an error
+    if ARGV.build_head? and f.unstable.nil?
+      raise CannotInstallFormulaError, "No head is defined for #{f.name}"
+    end
   end
 
   def install
-    raise FormulaAlreadyInstalledError, f if f.installed? and not ARGV.force?
+    # not in initialize so upgrade can unlink the active keg before calling this
+    # function but after instantiating this class so that it can avoid having to
+    # relink the active keg if possible (because it is slow).
+    if f.linked_keg.directory?
+      # some other version is already installed *and* linked
+      raise CannotInstallFormulaError, <<-EOS.undent
+        #{f}-#{f.linked_keg.realpath.basename} already installed
+        To install this version, first `brew unlink #{f}'
+      EOS
+    end
 
     unless ignore_deps
       f.check_external_deps
