578a4c5277d46ea0a0cb11596c6a7cf88da8326c
John Knight <bug@knightlabs.com>
2012-03-07 11:12:24 -0500

    Correct Play to Play2 class.

--- Library/Formula/play2.rb
+++ Library/Formula/play2.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
-class Play < Formula
+class Play2 < Formula
   homepage 'http://www.playframework.org/'
-  url 'http://download.playframework.org/releases/play-1.2.4.zip'
-  md5 'ec8789f8cc02927ece536d102f5e649e'
+  url 'http://download.playframework.org/releases/play-2.0-RC4.zip'
+  md5 '98a9bbe198ca75146dfac813af5cfdbd'
 
   def install
     rm_rf 'python' # we don't need the bundled Python for windows
