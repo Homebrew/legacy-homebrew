7045a8695c6dce7576d207f552d8b2e383997d38
Anatol Pomozov <anatol.pomozov@gmail.com>
2012-03-07 14:52:30 -0800

    fuse4x: Update to 0.9.0
    Closes #10768.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/fuse4x.rb
+++ Library/Formula/fuse4x.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fuse4x < Formula
   homepage 'http://fuse4x.org/'
-  url 'https://github.com/fuse4x/fuse.git', :tag => "fuse4x_0_8_14"
-  version "0.8.14"
+  url 'https://github.com/fuse4x/fuse.git', :tag => "fuse4x_0_9_0"
+  version "0.9.0"
 
   depends_on 'gettext'
   depends_on 'fuse4x-kext'
@@ -19,6 +19,7 @@ class Fuse4x < Formula
 
     system "autoreconf", "--force", "--install"
 
+    ENV.append_to_cflags "-D_DARWIN_USE_64_BIT_INODE" # force 64bit inodes on 10.5. On 10.6+ this is no-op.
     system "./configure", "--disable-dependency-tracking", "--disable-debug", "--disable-static", "--prefix=#{prefix}"
     system "make install"
   end
