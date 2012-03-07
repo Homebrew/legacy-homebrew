dd268454923f697697f00fd2be2fdec624ea3b0a
John Knight <bug@knightlabs.com>
2012-03-07 12:07:27 -0500

    Added 'devel' block for Play 2.x release candidate builds.
    Since Play 2.x is still in RC, replacing the 1.2.4 release with a
    development build is undesirable at this point, but there's still a need
    to leverage the 2.x framework in certain cases.

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -5,8 +5,13 @@ class Play < Formula
   url 'http://download.playframework.org/releases/play-1.2.4.zip'
   md5 'ec8789f8cc02927ece536d102f5e649e'
 
+  devel do
+    url 'http://download.playframework.org/releases/play-2.0-RC4.zip'
+    md5 '98a9bbe198ca75146dfac813af5cfdbd'
+  end
+  
   def install
-    rm_rf 'python' # we don't need the bundled Python for windows
+    rm_rf 'python' unless ARGV.build_devel? # we don't need the bundled Python for windows, and has been removed in the RC builds
     rm Dir['*.bat']
     libexec.install Dir['*']
     bin.install_symlink libexec+'play'
