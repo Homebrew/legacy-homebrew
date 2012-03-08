7317ef0eadca63e962d4196b0ba02ca6307904e5
Luke Campbell <luke.s.campbell@gmail.com>
2012-03-08 10:01:52 -0500

    Update Library/Formula/lua.rb

--- Library/Formula/lua.rb
+++ Library/Formula/lua.rb
@@ -29,7 +29,7 @@ class Lua < Formula
 
   def install
     # Apply patch-level 2
-    curl "https://raw.github.com/gist/2001138/2dce9786968d5e518682d2564fa41d2ea284dd50/patch-lua-.5.1.4-3", "-O"
+    curl "https://trac.macports.org/export/90538/trunk/dports/lang/lua/files/patch-lua-5.1.4-3", "-O"
     safe_system '/usr/bin/patch', '-d', 'src', '-i', '../patch-lua-5.1.4-3'
     # we could use the patches method if it supported additional arguments (-d in our case)
 
