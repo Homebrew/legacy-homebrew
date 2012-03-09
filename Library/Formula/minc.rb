a28958c319e7fd24d2f3a0dc49af0ca046b29e34
Ulrik Landberg Stephansen <ulrikls@gmail.com>
2012-03-09 18:51:39 +0100

    Update MINC to 2.1.12
    Updates MINC to 2.1.12, as well as homepage and HEAD build.
    Tested on Mac OS 10.7.3, Xcode 4.3.1
    
    Source is now hosted on https://github.com/BIC-MNI/minc
    
    MINC does not build with clang, I'm not sure how to specify this in the
    formula.

--- Library/Formula/minc.rb
+++ Library/Formula/minc.rb
@@ -1,17 +1,23 @@
 require 'formula'
 
 class Minc < Formula
-  url 'https://github.com/downloads/andrewjanke/minc/minc-2.1.10.tar.gz'
+  url 'https://github.com/downloads/BIC-MNI/minc/minc-2.1.12.tar.gz'
   homepage 'http://en.wikibooks.org/wiki/MINC'
-  md5 '64032918928393ee6e1da7e8af3433d8'
+  md5 '48ccd7dbc52a9301301f5abc370c3f8c'
 
-  head 'https://github.com/andrewjanke/minc.git'
+  head 'https://github.com/BIC-MNI/minc.git'
 
   #fails_with_clang "Throws 'non-void function 'miget_real_value_hyperslab' should return a value' error during build.", :build => 318
 
   depends_on 'netcdf'
 
+  if MacOS.xcode_version >= "4.3"
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   def install
+    system "autoreconf", "--force", "--instal"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
