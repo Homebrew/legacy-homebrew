4200ac442b52c12eec72e2de82815433b4535b27
Adam Vandenberg <flangy@gmail.com>
2012-03-10 14:29:37 -0800

    xmlrpc-c 1.16.40

--- Library/Formula/xmlrpc-c.rb
+++ Library/Formula/xmlrpc-c.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class XmlrpcC < Formula
   homepage 'http://xmlrpc-c.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/sourceforge/xmlrpc-c/xmlrpc-c-1.16.39.tgz'
-  md5 'e88c9ee202890d726405b3bdfb00cfaf'
+  url 'http://downloads.sourceforge.net/sourceforge/xmlrpc-c/xmlrpc-c-1.16.40.tgz'
+  md5 '0b5c026d48c21937261d90bdadda7248'
 
   def install
     ENV.deparallelize
-    # choosing --enable-libxml2-backend to lose some weight and not statically
-    # link in expat
-    # NOTE seemingly it isn't possible to build dylibs with this thing
-    system "./configure", "--disable-debug", "--enable-libxml2-backend", "--prefix=#{prefix}"
+    # --enable-libxml2-backend to lose some weight and not statically link in expat
+    system "./configure", "--disable-debug",
+                          "--enable-libxml2-backend",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
