709b8077ad5fd03ab75532dc0772b9a83d0fcc77
Duncan Mak <duncan@a-chinaman.com>
2012-03-11 01:49:30 -0500

    scala 2.9.1-1
    Closes #10858.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/scala.rb
+++ Library/Formula/scala.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class ScalaDocs < Formula
   homepage 'http://www.scala-lang.org/'
-  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.9.1.final-devel-docs.tgz'
-  version '2.9.1'
-  md5 '88668b400ec61c8b043e288ddc62b8b8'
+  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.9.1-1-devel-docs.tgz'
+  version '2.9.1-1'
+  md5 '6bfdd990c379c1b2c87335c89f6c444c'
 end
 
 class ScalaCompletion < Formula
@@ -16,9 +16,9 @@ end
 
 class Scala < Formula
   homepage 'http://www.scala-lang.org/'
-  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.9.1.final.tgz'
-  version '2.9.1'
-  md5 '1a06eacc7f59f279bf1700c98d5bf19d'
+  url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.9.1-1.tgz'
+  version '2.9.1-1'
+  md5 'bde2427b3f56e9c5ccb86a4376ac0d93'
 
   def options
     [['--with-docs', 'Also install library documentation']]
