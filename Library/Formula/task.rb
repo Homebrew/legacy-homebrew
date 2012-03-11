d77457ca52e8188545c32fdc50675f2e17315eb1
Jake Bell <jake@theunraveler.com>
2012-03-11 18:55:07 -0500

    task --devel 2.0.0.rc1
    Closes #10869.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -7,9 +7,24 @@ class Task < Formula
 
   skip_clean :all
 
+  devel do
+    url 'http://www.taskwarrior.org/download/task-2.0.0.RC1.tar.gz'
+    md5 '42333fa5a0ab3a6b058146182cbb7b1a'
+    version '2.0.0.rc1'
+  end
+
+  if ARGV.build_devel?
+    depends_on "cmake" => :build
+  end
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    if ARGV.build_devel?
+      system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+    else
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+    end
+
     system "make install"
     (etc+'bash_completion.d').install 'scripts/bash/task_completion.sh'
   end
