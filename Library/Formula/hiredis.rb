f3da147de22a77bd8b76c74b207b049f2c13ec60
Adam Vandenberg <flangy@gmail.com>
2012-03-11 14:39:02 -0700

    hiredis: fix audit warning

--- Library/Formula/hiredis.rb
+++ Library/Formula/hiredis.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
 class Hiredis < Formula
-  url 'https://github.com/antirez/hiredis/tarball/v0.10.0'
-  head 'https://github.com/antirez/hiredis.git'
   homepage 'https://github.com/antirez/hiredis'
-  version '0.10.0'
+  url 'https://github.com/antirez/hiredis/tarball/v0.10.0'
   sha1 'a54dd2b31cb39bc05bf88538c688bf50f8c6c9c7'
+  head 'https://github.com/antirez/hiredis.git'
 
   def install
     # Architecture isn't detected correctly on 32bit Snow Leopard without help
