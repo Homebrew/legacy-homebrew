1c6ebbfaf0d1e969232508cab3ddec624dcf95fe
Adam Vandenberg <flangy@gmail.com>
2012-03-11 16:27:04 -0700

    ushare: document patch

--- Library/Formula/ushare.rb
+++ Library/Formula/ushare.rb
@@ -1,14 +1,15 @@
 require 'formula'
 
 class Ushare < Formula
-  url 'http://ushare.geexbox.org/releases/ushare-1.1a.tar.bz2'
   homepage 'http://ushare.geexbox.org/'
+  url 'http://ushare.geexbox.org/releases/ushare-1.1a.tar.bz2'
   md5 '5bbcdbf1ff85a9710fa3d4e82ccaa251'
 
   depends_on 'gettext'
   depends_on 'libupnp'
   depends_on 'libdlna'
 
+  # Correct "OPTFLAGS" to "CFLAGS"
   def patches
     { :p0 =>
       "https://trac.macports.org/export/89267/trunk/dports/net/ushare/files/patch-configure.diff"
@@ -16,7 +17,7 @@ class Ushare < Formula
   end
 
   def install
-    # Need to explicitly add gettext here.
+    # Need to explicitly add intl and gettext here.
     gettext = Formula.factory("gettext")
     ENV.append 'LDFLAGS', "-lintl"
     ENV.append 'CFLAGS', "-I#{gettext.include}"
