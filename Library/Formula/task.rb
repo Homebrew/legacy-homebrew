abc4441f5be2382ba14ffbdd505627eeb8f271c7
Adam Vandenberg <flangy@gmail.com>
2012-03-11 16:12:04 -0700

    task: style nits

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Task < Formula
-  url 'http://www.taskwarrior.org/download/task-1.9.4.tar.gz'
   homepage 'http://www.taskwarrior.org/'
+  url 'http://www.taskwarrior.org/download/task-1.9.4.tar.gz'
   md5 '0c5d9dedb1ead69590af895d16708070'
 
   skip_clean :all
@@ -11,8 +11,6 @@ class Task < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
-
-    # Install the bash completion file
     (etc+'bash_completion.d').install 'scripts/bash/task_completion.sh'
   end
 end
