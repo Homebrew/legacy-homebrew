595d2b91038915b363c35a6628b9a5fed45840cf
Adam Vandenberg <flangy@gmail.com>
2012-03-10 17:20:43 -0800

    unshield: remove unused configure switch

--- Library/Formula/unshield.rb
+++ Library/Formula/unshield.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Unshield < Formula
-  url 'http://downloads.sourceforge.net/project/synce/Unshield/0.6/unshield-0.6.tar.gz'
   homepage 'http://www.synce.org/oldwiki/index.php/Unshield'
+  url 'http://downloads.sourceforge.net/project/synce/Unshield/0.6/unshield-0.6.tar.gz'
   md5 '31a829192a255160d1f71cda4c865c9c'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
