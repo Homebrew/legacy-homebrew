79fbd62ac398a1184384a1efdab6f87d243be506
Adam Vandenberg <flangy@gmail.com>
2012-03-10 17:06:42 -0800

    vala: update comment

--- Library/Formula/vala.rb
+++ Library/Formula/vala.rb
@@ -13,8 +13,8 @@ class Vala < Formula
   depends_on 'glib'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
-    system "make" # Single step fails to compile for 0.8.0
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make" # Fails to compile as a single step
     system "make install"
   end
 
