74467d354ec38bb582ba642237a295c34cb655e9
Jack Nagel <jacknagel@gmail.com>
2012-03-12 17:51:27 -0500

    glib: add test
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -89,4 +89,27 @@ class Glib < Formula
 
     (share+'gtk-doc').rmtree
   end
+
+  def test
+    mktemp do
+      (Pathname.pwd/'test.c').write <<-EOS.undent
+        #include <string.h>
+        #include <glib.h>
+
+        int main(void)
+        {
+            gchar *result_1, *result_2;
+            char *str = "string";
+
+            result_1 = g_convert(str, strlen(str), "ASCII", "UTF-8", NULL, NULL, NULL);
+            result_2 = g_convert(result_1, strlen(result_1), "UTF-8", "ASCII", NULL, NULL, NULL);
+
+            return (strcmp(str, result_2) == 0) ? 0 : 1;
+        }
+        EOS
+      system "clang", "-o", "test", "test.c",
+        *`pkg-config --cflags --libs glib-2.0`.split
+      system "./test"
+    end
+  end
 end
