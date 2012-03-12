1ad0ec3dacb9a7b8c339412d9ae92d6f63d30d77
Adam Vandenberg <flangy@gmail.com>
2012-03-12 06:51:02 -0700

    pigz 2.2.4
    Also use our CC and CFLAGS.

--- Library/Formula/pigz.rb
+++ Library/Formula/pigz.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Pigz < Formula
   homepage 'http://www.zlib.net/pigz/'
-  url 'http://www.zlib.net/pigz/pigz-2.2.3.tar.gz'
-  md5 '8330a6c6a3e5f1954687aaba4b973a6f'
+  url 'http://www.zlib.net/pigz/pigz-2.2.4.tar.gz'
+  md5 '9df2a3c742524446fa4e797c17e8fd85'
 
   def install
-    system "make"
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
     bin.install "pigz", "unpigz"
     man1.install "pigz.1"
     ln_s 'pigz.1', man1+'unpigz.1'
