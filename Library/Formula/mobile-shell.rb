ebbc505903b25df5c0bd6075ed1520e00b3a3688
seph <seph@directionless.org>
2012-03-13 12:30:01 -0400

    update to 1.0

--- Library/Formula/mobile-shell.rb
+++ Library/Formula/mobile-shell.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MobileShell < Formula
   homepage 'http://mosh.mit.edu/'
-  url 'https://github.com/downloads/keithw/mosh/mosh-0.98c.tar.gz'
-  md5 'ea7b11726d64b9125f40e5bf9cbd4921'
+  url 'https://github.com/downloads/keithw/mosh/mosh-1.0.tar.gz'
+  md5 'c53723f8579789adf535215dfd5e562a'
 
   head 'https://github.com/keithw/mosh.git'
 
