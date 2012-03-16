b52077ed99562659ef783d36e3ca9e622a3cdc31
jmoore <josh@glencoesoftware.com>
2012-03-16 08:29:23 +0100

    Patch mcpp for compiling zeroc-ice34 on 10.7
    Combined version of gh-10280 and gh-10443

--- Library/Formula/mcpp.rb
+++ Library/Formula/mcpp.rb
@@ -5,13 +5,34 @@ class Mcpp < Formula
   homepage 'http://mcpp.sourceforge.net/'
   md5 '512de48c87ab023a69250edc7a0c7b05'
 
+  def patches
+    # patch to support ZeroC Ice, taken from Ice-3.4.2 third party source archive.
+    # http://www.zeroc.com/download.html#src
+    #
+    # this subsumes the previous patch from this formula
+    "https://raw.github.com/gist/f8580f14d689307a2601/8103cf09204859d5ef536a917474391893dcd120/gistfile1.diff"
+  end
+
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--enable-mcpplib"
     system "make install"
   end
 
-  def patches
-    "https://gist.github.com/raw/668341/e50827c8d9e8452befcab64bd8800b16d1f66d0e/mcpp-fix-stpcpy.patch"
+  def test
+      # Checks that the output from mcpp -C (with comments) is:
+      #
+      #   #line 1 "<stdin>"
+      #   //comment
+      #   test;
+      #
+      # and not:
+      #
+      #   #line 1 "<stdin>"
+      #   //commenttest;
+      #
+      system 'echo "test; //comment" | mcpp -C | wc -l | grep -q 3'
   end
+
 end
