6deb62a3adc47bb4ab8f96069c9320c578ac4ef4
Max Howell <max@methylblue.com>
2012-03-16 12:59:27 +0000

    HOMEBREW_LIBRARY
    I got fed up writing HOMEBREW_REPOSITORY/"Library" and also correcting mistakes when I did HOMEBREW_PREFIX instead.

--- Library/Homebrew/cmd/untap.rb
+++ Library/Homebrew/cmd/untap.rb
@@ -3,15 +3,15 @@ require 'cmd/tap' # for tap_args
 module Homebrew extend self
   def untap
     user, repo = tap_args
-    tapd = HOMEBREW_PREFIX/"Library/Taps/#{user}-#{repo}"
+    tapd = HOMEBREW_LIBRARY/"Taps/#{user}-#{repo}"
 
     raise "No such tap!" unless tapd.directory?
 
-    gitignores = (HOMEBREW_PREFIX/"Library/Formula/.gitignore").read.split rescue []
+    gitignores = (HOMEBREW_LIBRARY/"Formula/.gitignore").read.split rescue []
 
     tapd.find_formula do |pn|
       bn = pn.basename.to_s
-      pn = HOMEBREW_REPOSITORY/"Library/Formula"/bn
+      pn = HOMEBREW_LIBRARY/"Formula/#{bn}"
       pn.delete if pn.symlink? and pn.realpath.to_s =~ %r[^#{tapd.realpath}]
       gitignores.delete(bn)
     end
