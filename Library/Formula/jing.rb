9905512af6f4daaad105792423c33f5de69ddc39
Adam Vandenberg <flangy@gmail.com>
2012-03-16 21:59:28 -0700

    fix some wrapper scripts

--- Library/Formula/jing.rb
+++ Library/Formula/jing.rb
@@ -5,14 +5,11 @@ class Jing < Formula
   url 'http://jing-trang.googlecode.com/files/jing-20091111.zip'
   md5 '13eef193921409a1636377d1efbf9843'
 
-  def wrapper target; <<-EOS.undent
-    #!/bin/bash
-    java -jar "#{libexec}/bin/jing.jar" "$@"
-    EOS
-  end
-
   def install
     libexec.install Dir["*"]
-    (bin+'jing').write wrapper('jing')
+    (bin+'jing').write <<-EOS.undent
+      #!/bin/bash
+      java -jar "#{libexec}/bin/jing.jar" "$@"
+    EOS
   end
 end
