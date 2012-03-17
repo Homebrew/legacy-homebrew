d5fb1042e92726fb96fad1dad46b8f84d6d4d4f2
poppen <poppen.jp@gmail.com>
2012-03-17 14:41:57 +0900

    duply 1.5.5.5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/duply.rb
+++ Library/Formula/duply.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Duply < Formula
-  url 'http://sourceforge.net/projects/ftplicity/files/duply%20(simple%20duplicity)/1.5.x/duply_1.5.4.tgz'
   homepage 'http://duply.net'
-  md5 '07ca9266d2abec31f2aa19e23f912dac'
+  url 'http://sourceforge.net/projects/ftplicity/files/duply%20%28simple%20duplicity%29/1.5.x/duply_1.5.5.5.tgz'
+  md5 'fdc05d1731dc8d0de88b7e17a1095506'
 
   depends_on 'duplicity'
 
