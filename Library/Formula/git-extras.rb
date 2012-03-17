5e73c94821507cd07f0a77b4f1db9a6b62d74b04
Doc Walker <2-718@wvfans.net>
2012-03-17 05:33:22 -0500

    Update git-extras (1.4.0 to 1.5.0)

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitExtras < Formula
   homepage 'https://github.com/visionmedia/git-extras'
-  url 'https://github.com/visionmedia/git-extras/tarball/1.4.0'
-  sha1 '9471ae1df77e561980983e2ae7e7d1f6b74e95bc'
+  url 'https://github.com/visionmedia/git-extras/tarball/1.5.0'
+  sha1 '60e0ad00b046d76b3c111d9cc3d02aa4191e450a'
 
   head 'https://github.com/visionmedia/git-extras.git', :branch => 'master'
 
