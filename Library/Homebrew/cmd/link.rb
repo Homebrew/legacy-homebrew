20bb4eaa452100ccf0fe9b11a60f0574b884cdc1
Max Howell <max@methylblue.com>
2012-03-19 00:15:40 +0000

    Better error when linking fails
    Refs http://stackoverflow.com/questions/9762943
    
    The system ln no longer outputs anything. Though the user can force its output with a --verbose of course. So in cases where it's not the usual of: not writable or existing file, we can ask the user to run with --verbose. I don't particularly like hiding its output, but it just confused the error IMO since it is creating a relative symlink the output was weird every time I've seen it in tickets.
    
    I made a print wrapper so that the brew-link output doesn't get mucked up if an exception is thrown.

--- Library/Homebrew/cmd/link.rb
+++ Library/Homebrew/cmd/link.rb
@@ -1,4 +1,5 @@
 module Homebrew extend self
+
   def link
     raise KegUnspecifiedError if ARGV.named.empty?
 
@@ -9,9 +10,30 @@ module Homebrew extend self
     end
 
     ARGV.kegs.each do |keg|
-      print "Linking #{keg}... "
-      puts if ARGV.verbose?
-      puts "#{keg.link} symlinks created"
+      print "Linking #{keg}... " do
+        puts if ARGV.verbose?
+        puts "#{keg.link} symlinks created"
+      end
     end
   end
+
+  private
+
+  # Allows us to ensure a puts happens before the block exits so that if say,
+  # an exception is thrown, its output starts on a new line.
+  def print str, &block
+    Kernel.print str
+    puts_capture = Class.new do
+      def self.puts str
+        $did_puts = true
+        Kernel.puts str
+      end
+    end
+
+    puts_capture.instance_eval &block
+
+  ensure
+    puts unless $did_puts
+  end
+
 end
