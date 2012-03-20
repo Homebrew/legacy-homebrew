4c08d25fc87a7ab08a5a02f6e633f139bea3e45c
nibbles 2bits <nibbles@speakeasy.net>
2012-03-19 21:07:02 -0700

    clhep 2.1.2.2
    Upgrade to clhep 2.1.2.2.  Fixes compile error with clang.
    Builds in parallel.  Remove ENV.j1.  Passes `make test` on Lion
    and Snow Leopard with five compilers from Xcode-4.3.1 and 4.0.2.
    
    Closes #11080.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/clhep.rb
+++ Library/Formula/clhep.rb
@@ -2,16 +2,14 @@ require 'formula'
 
 class Clhep < Formula
   homepage 'http://proj-clhep.web.cern.ch/proj-clhep/'
-  url 'http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.1.1.0.tgz'
-  md5 'f8acb50a9cdb9ac8937fecfcb741ba10'
+  url 'http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.1.2.2.tgz'
+  sha1 '374340e316c192c211db36c4cd3f29407cc75318'
 
   depends_on 'cmake' => :build
 
   def install
     mkdir 'clhep-build' do
-      system "cmake ../CLHEP  #{std_cmake_parameters} -DCMAKE_PREFIX_PATH=#{prefix}"
-      ENV.j1
-      system "make"
+      system "cmake #{std_cmake_parameters} -DCMAKE_PREFIX_PATH=#{prefix} ../CLHEP"
       system "make install"
     end
   end
