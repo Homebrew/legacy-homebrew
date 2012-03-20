cbfc25e1cb50fe571e9310153eeada4ade3c392c
Chris Wanstrath <chris@ozmm.org>
2012-03-20 12:39:26 -0700

    gist 3.0.1
    Closes #11095.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gist < Formula
   homepage 'https://github.com/defunkt/gist'
-  url 'https://github.com/defunkt/gist/tarball/v2.0.3'
-  md5 '0b99159635df2083651d2ff9db8abb25'
+  url 'https://github.com/defunkt/gist/tarball/v3.0.1'
+  md5 '88c3fdb6c1503c36942b62f8684c1798'
   head 'https://github.com/defunkt/gist.git'
 
   def install
