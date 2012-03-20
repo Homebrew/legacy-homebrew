59ecc70562b09aa02a6d0ee83453d8f163a3ca0a
Andrea Francia <me@andreafrancia.it>
2012-03-20 18:19:03 +0100

    Add the ren commmand.

---
+++ Library/Formula/ren.rb
@@ -0,0 +1,20 @@
+require 'formula'
+require 'tmpdir'
+
+class Ren < Formula
+  homepage 'http://pdb.finkproject.org/pdb/package.php/ren'
+  url 'http://www.ibiblio.org/pub/Linux/utils/file/ren-1.0.tar.gz'
+  md5 '245453453a8bd1c0bf7d03880b97d632'
+
+  def install
+    system "make"
+    system "install -d -m 0755 #{prefix}/bin #{prefix}/share/man/man1"
+    system "install -m 0755 ren #{prefix}/bin"
+    system "install -m 0644 ren.1 #{prefix}/share/man/man1"
+  end
+
+  def test
+    system 'ren'
+  end
+
+end
