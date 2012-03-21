7d0cfd0766c5a4558b884748af6137a5230ae739
Adam Vandenberg <flangy@gmail.com>
2012-03-20 20:22:27 -0700

    dpkg: mark as fails-with-clang

--- Library/Formula/dpkg.rb
+++ Library/Formula/dpkg.rb
@@ -13,6 +13,12 @@ class Dpkg < Formula
   def patches; DATA; end
 
   def install
+    # FIXME This should be replaced with fails_with_clang once available
+    if ENV.compiler == :clang
+      opoo "Formula will not build with Clang, using LLVM."
+      ENV.llvm
+    end
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-compiler-warnings",
