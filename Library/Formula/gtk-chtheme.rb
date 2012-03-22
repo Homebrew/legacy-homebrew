e232602489bb6d37beb956272179d1d0dfd8769b
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2012-03-22 09:39:48 +0100

    Add changes as suggested by comments on pull request: https://github.com/mxcl/homebrew/pull/10719

--- Library/Formula/gtk-chtheme.rb
+++ Library/Formula/gtk-chtheme.rb
@@ -5,6 +5,7 @@ class GtkChtheme < Formula
   url 'http://plasmasturm.org/code/gtk-chtheme/gtk-chtheme-0.3.1.tar.bz2'
   md5 'f688053bf26dd6c4f1cd0bf2ee33de2a'
 
+  depends_on 'pkg-config' => :build
   depends_on 'cairo' 
   depends_on 'gdk-pixbuf' 
   depends_on 'gettext' 
@@ -13,20 +14,15 @@ class GtkChtheme < Formula
   depends_on 'pango'
 
   def install
-    # ENV.x11 # if your formula requires any X11 headers
-    # ENV.j1  # if your formula's build system can't parallelize
-
+    # Unfortunately chtheme relies on some deprecated functionality
+    # we need to disable errors for it to compile properly
     inreplace 'Makefile', '-DGTK_DISABLE_DEPRECATED', ''
-    inreplace 'Metadata' do |s|
-      s.change_make_var! "PREFIX", HOMEBREW_PREFIX
-    end
-    system "make install" # if this fails, try separate make/make install steps
+
+#    inreplace 'Metadata' do |s|
+#      s.change_make_var! "PREFIX", HOMEBREW_PREFIX
+#    end
+#    system "make install" # if this fails, try separate make/make install steps
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
-  def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test gtk-chtheme`.
-    system "false"
-  end
 end
