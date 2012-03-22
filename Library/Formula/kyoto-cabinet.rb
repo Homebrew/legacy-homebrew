16ca0d34fbfaeb38653648aca17f8448e7fba139
Pedro Borges <pdhborges@gmail.com>
2012-03-22 14:57:20 +0000

    Add patch to build with clang
    Currently clang doesn't generate instructions for atomic operations
    with non-integer arguments.

--- Library/Formula/kyoto-cabinet.rb
+++ Library/Formula/kyoto-cabinet.rb
@@ -5,6 +5,14 @@ class KyotoCabinet < Formula
   homepage 'http://fallabs.com/kyotocabinet/'
   sha1 '345358259ec4e58b5986b5d6fa8f82dfe2816c37'
 
+  def patches
+    p = []
+    if ENV.compiler == :clang
+      p << 'https://raw.github.com/gist/9b24c8fffc234d75b732/f910b0cffd4758312af06974482622e0a9b8b37a/lock.patch'
+    end
+    p
+  end
+
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make" # Separate steps required
