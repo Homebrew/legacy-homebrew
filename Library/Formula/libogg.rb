a1448209678571ee3194f73fc0c3131865214670
richo <richo@psych0tik.net>
2012-03-23 01:24:32 +1100

    libogg, libvorbis: Add --HEAD
    Closes #11146.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/libogg.rb
+++ Library/Formula/libogg.rb
@@ -5,7 +5,22 @@ class Libogg < Formula
   url 'http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz'
   md5 '0a7eb40b86ac050db3a789ab65fe21c2'
 
+  head 'http://svn.xiph.org/trunk/ogg', :using => :svn
+
+  if ARGV.build_head?
+    depends_on "automake" => :build
+
+    if MacOS.xcode_version >= "4.3"
+      depends_on "libtool" => :build
+      depends_on "autoconf" => :build
+    end
+  end
+
   def install
+    if ARGV.build_head?
+      system "./autogen.sh"
+    end
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
