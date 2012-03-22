52d244a70ff393e260eb2b15c1a6bf8c8f9c193c
Dominique Orban <dominique.orban@gmail.com>
2012-03-22 13:12:52 -0400

    Update Adol-C formula
    Update to version 2.3.0 and add option for sparse derivatives.

--- Library/Formula/adol-c.rb
+++ Library/Formula/adol-c.rb
@@ -1,13 +1,32 @@
 require 'formula'
 
+# Are sparse derivatives requested?
+def enable_sparse?; ARGV.include? '--enable-sparse'; end
+
 class AdolC < Formula
-  url 'http://www.coin-or.org/download/source/ADOL-C/ADOL-C-2.2.1.tgz'
+  url 'http://www.coin-or.org/download/source/ADOL-C/ADOL-C-2.3.0.tgz'
   homepage 'http://www.coin-or.org/projects/ADOL-C.xml'
-  md5 '5fe149865b47f77344ff910702da8b99'
+  md5 'c06013e6023ac9c7066738a84b9dafa5'
+
+  if enable_sparse?
+    # As of version 2.3.0, Colpack no longer has to be downloaded to a subfolder
+    # of Adol-C. But we need recent versions of automake and autoconf.
+    depends_on 'colpack'
+    depends_on 'automake'
+    depends_on 'autoconf'
+  end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+
+    args = ["--disable-debug",
+            "--disable-dependency-tracking",
+            "--prefix=#{prefix}"]
+
+    if enable_sparse?
+      args = args + ["--enable-sparse"]
+    end
+
+    system "./configure", *args
     system "make install"
   end
 
