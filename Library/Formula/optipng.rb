381788295ec4535a2e61069c231dd7c6c651514c
Tim Douglas <timdoug@bu.mp>
2012-03-21 19:46:29 -0700

    optipng 0.7.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/optipng.rb
+++ Library/Formula/optipng.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Optipng < Formula
-  url 'http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7/optipng-0.7.tar.gz'
+  url 'http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.1/optipng-0.7.1.tar.gz'
   homepage 'http://optipng.sourceforge.net/'
-  md5 '9b2f526ce79ea779c7004c7964ee8bcc'
+  md5 'b6181d566998ad489397b985ebfc4a03'
 
   def install
     system "./configure", "--with-system-zlib", "--prefix=#{prefix}", "--mandir=#{man}"
