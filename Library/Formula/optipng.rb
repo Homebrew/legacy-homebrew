26e475d854dd8c0b40daaa3c5e76639fd9088e76
Adam Vandenberg <flangy@gmail.com>
2012-03-21 20:23:47 -0700

    optipng: style nits

--- Library/Formula/optipng.rb
+++ Library/Formula/optipng.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Optipng < Formula
-  url 'http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.1/optipng-0.7.1.tar.gz'
   homepage 'http://optipng.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.1/optipng-0.7.1.tar.gz'
   md5 'b6181d566998ad489397b985ebfc4a03'
 
   def install
-    system "./configure", "--with-system-zlib", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "./configure", "--with-system-zlib",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
     system "make install"
   end
 end
