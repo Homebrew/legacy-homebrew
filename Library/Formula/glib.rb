dba31527cdd9592944c01cdf9c8ce34fe4074983
Jonathan Schleifer <js@webkeks.org>
2012-03-26 17:22:05 +0200

    Update glib to 2.32.0.

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -4,8 +4,8 @@ def build_tests?; ARGV.include? '--test'; end
 
 class Glib < Formula
   homepage 'http://developer.gnome.org/glib/'
-  url 'ftp://ftp.gnome.org/pub/gnome/sources/glib/2.30/glib-2.30.3.tar.xz'
-  sha256 'e6cbb27c71c445993346e785e8609cc75cea2941e32312e544872feba572dd27'
+  url 'ftp://ftp.gnome.org/pub/gnome/sources/glib/2.32/glib-2.32.0.tar.xz'
+  sha256 'cde9d9f25ed648069c547e323897ad9379974e1f936b4477fa51bcf1bb261ae4'
 
   depends_on 'xz' => :build
   depends_on 'gettext'
@@ -15,12 +15,9 @@ class Glib < Formula
 
   def patches
     { :p0 => %W[
-      https://trac.macports.org/export/87537/trunk/dports/devel/glib2/files/patch-configure.diff
-      https://trac.macports.org/export/87537/trunk/dports/devel/glib2/files/patch-glib-2.0.pc.in.diff
       https://trac.macports.org/export/87537/trunk/dports/devel/glib2/files/patch-glib_gunicollate.c.diff
       https://trac.macports.org/export/87537/trunk/dports/devel/glib2/files/patch-gi18n.h.diff
       https://trac.macports.org/export/87537/trunk/dports/devel/glib2/files/patch-gio_xdgmime_xdgmime.c.diff
-      https://trac.macports.org/export/87537/trunk/dports/devel/glib2/files/patch-gio_gdbusprivate.c.diff
       ]}
   end
 
@@ -43,12 +40,6 @@ class Glib < Formula
 
     args << "--disable-debug" unless build_tests?
 
-    # MacPorts puts "@@PREFIX@@" in patches and does inreplace on the files,
-    # so we must follow suit if we use their patches
-    inreplace ['gio/xdgmime/xdgmime.c', 'gio/gdbusprivate.c'] do |s|
-      s.gsub! '@@PREFIX@@', HOMEBREW_PREFIX
-    end
-
     # glib and pkg-config <= 0.26 have circular dependencies, so we should build glib without pkg-config
     # The pkg-config dependency can be eliminated if certain env variables are set
     # Note that this *may* need to be updated if any new dependencies are added in the future
@@ -61,10 +52,6 @@ class Glib < Formula
     ENV['LIBFFI_LIBS'] = '-lffi'
 
     system "./configure", *args
-
-    # Fix for 64-bit support, from MacPorts
-    curl "https://trac.macports.org/export/87537/trunk/dports/devel/glib2/files/config.h.ed", "-O"
-    system "ed - config.h < config.h.ed"
 
     system "make"
     # Suppress a folder already exists warning during install
