96f565925f64afd1cba707eb23b9ecf209d24478
Filip Krikava <filip.krikava@i3s.unice.fr>
2012-03-26 12:20:45 +0200

    Set LLVM as default compiler and added note about tests from simgrid developers

--- Library/Formula/simgrid.rb
+++ Library/Formula/simgrid.rb
@@ -10,18 +10,34 @@ class Simgrid < Formula
   depends_on 'graphviz'
 
   def install
-    system "cmake . #{std_cmake_parameters}"
+    # FIXME This should be replaced with fails_with_clang once available
+    if ENV.compiler == :clang
+      opoo "Formula will not build with Clang, using LLVM."
+      ENV.llvm
+    end
+
+    system "cmake . -Denable_debug=on -Denable_compile_optimizations=off #{std_cmake_parameters}"
     system "make install"
   end
 
   def test
     # In the 3.6.2 following tests seems to be failing on OSX Lion
+    # 
+    #  21 - tesh-gras-dd-r-little32-4 (SEGFAULT)
     # 105 - msg-chord-raw-parallel (Failed)
     # 124 - gras-pmm-sg-64-raw (Failed)
+    # 134 - simdag-test-simdag (SEGFAULT)
     # 139 - tracing-ms (Failed)
-    # 156 - testall (OTHER_FAULT)
+    # 156 - testall (SEGFAULT)
+    #
+    # According to simgrid developers this is a known issues that should
+    # not compromise the functinality of the library itself.
+    # Once they are resolved, I will update this formula.
+    # More details:
+    # http://lists.gforge.inria.fr/pipermail/simgrid-user/2012-March/002870.html
+    #
     #system "make check"
-    #system "ctest"
+
     system "false"
   end
 end
