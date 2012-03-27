fc1bcd7eb627849bd6a46d1837727912eb80d90f
Jean-Olivier Irisson <irisson@normalesup.org>
2012-03-27 14:41:07 +0200

    Update hub to 1.8.4

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Hub < Formula
-  url 'https://github.com/defunkt/hub/tarball/v1.8.3'
+  url 'https://github.com/defunkt/hub/tarball/v1.8.4'
   homepage 'https://github.com/defunkt/hub#readme'
   head 'https://github.com/defunkt/hub.git'
-  md5 '8775c2312ba2b175775a81e9b0bf5e24'
+  md5 '1ca041817c014d2d3a5267f2aed4157b'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
