6beb01b1464be5efe7d919c58db73aa4b26f2bdf
Fábio M. Costa <fabiomcosta@gmail.com>
2012-03-27 19:50:12 -0300

    Requires SnowLeopard or newer version. got snippet from jstalk's formula: https://github.com/mxcl/homebrew/blob/master/Library/Formula/jstalk.rb

--- Library/Formula/phantomjs.rb
+++ Library/Formula/phantomjs.rb
@@ -1,9 +1,21 @@
 require 'formula'
 
+class NeedsSnowLeopardOrNewer < Requirement
+  def satisfied?
+    MacOS.snow_leopard?
+  end
+
+  def message
+    "phantomjs requires Mac OS X 10.6 (Snow Leopard) or newer."
+  end
+end
+
 class Phantomjs < Formula
   url "http://phantomjs.googlecode.com/files/phantomjs-1.5.0-macosx-static.zip"
   homepage 'http://www.phantomjs.org/'
   sha1 'b87152ce691e7ed1937d30f86bc706a408d47f64'
+  
+  depends_on NeedsSnowLeopardOrNewer.new
 
   def install
     bin.install "bin/phantomjs"
