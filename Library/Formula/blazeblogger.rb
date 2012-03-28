87db11b12061b2d68d1bf00d8aa4e42e1648595a
Kyle Isom <coder@kyleisom.net>
2012-03-28 12:35:42 +0300

    add blazeblogger, a simple static CMS engine

---
+++ Library/Formula/blazeblogger.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Blazeblogger < Formula
+  homepage 'http://blaze.blackened.cz/'
+  url 'http://blazeblogger.googlecode.com/files/blazeblogger-1.2.0.tar.gz'
+  md5 'a126ea460e389f73d0f0fb45164bdaf9'
+
+  def install
+    system "make prefix=#{prefix} compdir=#{prefix} install"
+  end
+
+  def test
+    # testing blazeblogger is hard, we'd have to test creating a repo
+    system "true"
+  end
+
+end
