a9a0cab4767b57ada5d7caa24dfe02643cb3f880
Thomas Strathmann <thomas@pdp7.org>
2012-03-28 18:31:02 +0200

    makeicns 1.4.9

---
+++ Library/Formula/makeicns.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Makeicns < Formula
+  url 'https://bitbucket.org/mkae/makeicns/downloads/makeicns-1.4.9.tar.bz2'
+  head 'https://bitbucket.org/mkae/makeicns', :using => :hg
+  md5 '795ec620f15bfcebb5246319c1a96eab'
+  homepage 'http://bitbucket.org/mkae/makeicns'
+
+  def install
+    system "make"
+    bin.install "makeicns"
+  end
+
+  def test
+    system "makeicns"
+  end
+end
