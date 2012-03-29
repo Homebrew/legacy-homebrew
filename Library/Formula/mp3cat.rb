257e90ce16d02a0b0391cafba42a3a9ae98dcd70
Ajai Khattri <ajai@everyscreenmedia.com>
2012-03-29 14:35:47 -0400

    Cleaned up comments. Removed test. Now installs additional binaries.

--- Library/Formula/mp3cat.rb
+++ Library/Formula/mp3cat.rb
@@ -1,30 +1,12 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Mp3cat < Formula
   homepage 'http://tomclegg.net/mp3cat'
   url 'http://tomclegg.net/software/mp3cat-0.4.tar.gz'
   md5 '0aa75af15c57b13aa7858092b79f3a61'
 
-  #depends_on 'cmake' => :build
-
   def install
-    # ENV.x11 # if your formula requires any X11 headers
-    # ENV.j1  # if your formula's build system can't parallelize
-
-    #system "./configure", "--disable-debug", "--disable-dependency-tracking",
-    #                      "--prefix=#{prefix}"
-    #system "cmake . #{std_cmake_parameters}"
     system "make"
-	bin.install('mp3cat')
-  end
-
-  def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test mp3cat`.
-    system "false"
+	bin.install('mp3cat','mp3log','mp3log-conf','mp3dirclean','mp3http','mp3stream-conf')
   end
 end
