8c1a6f194a49af7de92174bcd07bc218e6dca69a
Andrea Lattuada <utaaal@gmail.com>
2012-03-30 14:06:46 +0200

    Mozilla Rust 0.2
    Closes #11309.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rust.rb
+++ Library/Formula/rust.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Rust < Formula
-  url 'http://dl.rust-lang.org/dist/rust-0.1.tar.gz'
+  url 'http://dl.rust-lang.org/dist/rust-0.2.tar.gz'
   homepage 'http://www.rust-lang.org/'
-  md5 '80b655bcceaf2192c502a692c8c1eb20'
+  md5 '47be90f952ec01c3088af58be78fd618'
 
   def install
     system "./configure", "--prefix=#{prefix}"
