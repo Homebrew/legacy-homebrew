baa0e0b629df8b5ec1a876ec3937469ae8ed2174
Hao Song <baritono.tux@gmail.com>
2012-03-30 21:45:02 +0800

    create formula for MPFI, the multi-precision interval arithmetic library based on MPFR

---
+++ Library/Formula/mpfi.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Mpfi < Formula
+  homepage 'http://perso.ens-lyon.fr/nathalie.revol/software.html'
+  url 'https://gforge.inria.fr/frs/download.php/30130/mpfi-1.5.1.tar.gz'
+  md5 '2787d2fab9ba7fc5b171758e84892fb5'
+
+  depends_on 'gmp'
+  depends_on 'mpfr'
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+  def test
+    system "make check"
+  end
+end
