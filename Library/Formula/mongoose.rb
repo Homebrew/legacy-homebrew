2e83f0a1fb13dabee319b34b26ea75afd02326d9
Nate Drake <ndrake@gmail.com>
2012-03-30 21:57:12 -0300

    Mongoose 3.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mongoose.rb
+++ Library/Formula/mongoose.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Mongoose < Formula
   homepage 'http://code.google.com/p/mongoose/'
-  url 'http://mongoose.googlecode.com/files/mongoose-2.11.tgz'
-  md5 'f6985da7513d354cc18b21b7670d23c1'
+  url 'http://mongoose.googlecode.com/files/mongoose-3.1.tgz'
+  md5 'e718fc287b4eb1bd523be3fa00942bb0'
 
   def install
-    system "make mac"
+    system "make", "mac", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
     bin.install "mongoose"
     man1.install "mongoose.1"
   end
