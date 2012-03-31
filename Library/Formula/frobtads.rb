fa08d70b6ba8bedc97d1e7faa02c7eb88165ce7d
Justin de Vesine <ziz@ziz.org>
2012-03-30 21:34:47 -0600

    frobtads 1.1

--- Library/Formula/frobtads.rb
+++ Library/Formula/frobtads.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Frobtads < Formula
   homepage 'http://www.tads.org/frobtads.htm'
-  url 'http://www.tads.org/frobtads/frobtads-0.13.tar.gz'
-  md5 'b6f25787b9ff7b89931d765046c68642'
+  url 'http://www.tads.org/frobtads/frobtads-1.1.tar.gz'
+  md5 '2d29d1449bac6f23b6c5f522209069a1'
+  head 'git://git.assembla.com/frobtads.git'
 
   def install
     system "./configure", "--disable-dependency-tracking",
