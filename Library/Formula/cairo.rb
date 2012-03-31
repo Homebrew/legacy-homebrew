c7bb5b02ab2c05e900babcf87e9fe33d8ae75961
Birger J. Nordølum <birgerjn@gmail.com>
2012-03-31 13:31:23 +0200

    Cairo 1.12.2
    Also added changes of #12047.
    
    Closes #11286.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/cairo.rb
+++ Library/Formula/cairo.rb
@@ -2,11 +2,13 @@ require 'formula'
 
 class Cairo < Formula
   homepage 'http://cairographics.org/'
-  url 'http://www.cairographics.org/releases/cairo-1.10.2.tar.gz'
-  sha1 'ccce5ae03f99c505db97c286a0c9a90a926d3c6e'
+  url 'http://cairographics.org/releases/cairo-1.12.2.tar.xz'
+  sha256 'b786bc4a70542bcb09f2d9d13e5e6a0c86408cbf6d1edde5f0de807eecf93f96'
 
   depends_on 'pkg-config' => :build
+  depends_on 'xz'=> :build
   depends_on 'pixman'
+  depends_on :x11
 
   keg_only :provided_by_osx,
             "The Cairo provided by Leopard is too old for newer software to link against."
@@ -15,21 +17,18 @@ class Cairo < Formula
     [['--universal', 'Build a universal library']]
   end
 
-  # Fixes a build error with llvm, 'lto could not merge'.  Fixes a build error
-  # when brewing universal, cannot use 'lto-bc' with multiple -arch options.
   # Fixes a build error with clang & universal, where a function was implicit.
-  # Not reported upstream because we are using an old version, Cairo-1.10.2.
-  # cf. issues #12923 and #10400
   def patches; DATA; end
 
   def install
-    ENV.universal_binary if ARGV.include? '--universal'
+    ENV.universal_binary if ARGV.build_universal?
+
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --with-x
-    ]
-    args << '--enable-xcb' unless MacOS.leopard?
+      --with-x]
+
+    args << '--enable-xcb=no' if MacOS.leopard?
 
     system "./configure", *args
     system "make install"
@@ -37,9 +36,11 @@ class Cairo < Formula
 end
 
 __END__
---- a/configure	2010-12-25 06:22:57.000000000 -0800
-+++ b/configure	2012-06-19 22:39:49.000000000 -0700
-@@ -17224,7 +17224,7 @@
+diff --git a/configure b/configure
+index b75757d..1230da2 100755
+--- a/configure
++++ b/configure
+@@ -17939,7 +17939,7 @@ CAIRO_NONPKGCONFIG_LIBS="$LIBS"
  
  MAYBE_WARN="-Wall -Wextra \
  -Wold-style-definition -Wdeclaration-after-statement \
@@ -48,12 +49,3 @@ __END__
  -Wnested-externs -Wpointer-arith -Wwrite-strings \
  -Wsign-compare -Wstrict-prototypes -Wmissing-prototypes \
  -Wpacked -Wswitch-enum -Wmissing-format-attribute \
-@@ -17236,7 +17236,7 @@
- MAYBE_WARN="$MAYBE_WARN -erroff=E_ENUM_TYPE_MISMATCH_ARG \
- 			-erroff=E_ENUM_TYPE_MISMATCH_OP"
- 
--MAYBE_WARN="$MAYBE_WARN -fno-strict-aliasing -fno-common -flto"
-+MAYBE_WARN="$MAYBE_WARN -fno-strict-aliasing -fno-common"
- 
- MAYBE_WARN="$MAYBE_WARN -Wp,-D_FORTIFY_SOURCE=2"
- 
