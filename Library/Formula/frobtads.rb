128b3de1034cba932583db2ad6f127a552374124
Adam Vandenberg <flangy@gmail.com>
2012-03-30 22:03:04 -0700

    frobtads 0.13

--- Library/Formula/frobtads.rb
+++ Library/Formula/frobtads.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Frobtads < Formula
-  url 'http://www.tads.org/frobtads/frobtads-0.13.tar.gz'
   homepage 'http://www.tads.org/frobtads.htm'
+  url 'http://www.tads.org/frobtads/frobtads-0.13.tar.gz'
   md5 'b6f25787b9ff7b89931d765046c68642'
 
   def install
