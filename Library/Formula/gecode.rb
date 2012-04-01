dd395873721c7e119a2de9359186c5a7d07b654f
nibbles 2bits <nibbles@speakeasy.net>
2012-04-01 14:31:55 -0700

    gecode: remove fails_with, disable examples
    Gecode was updated to 3.7.2 in 672c5ab20b77 which fixed the error with
    clang. Geocode passes make check.
    
    * Remove fails_with block.
    * Remove unrecognized configure option for dependency-tracking
    * Remove --disable-debug option.  It is the defualt.
    * Add --disable-examples option.  This stops two dozen example
    binaries from filling /usr/local/bin that have common names like
    golf, sudoku, grocery, and partition.
    
    Closes #11374.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gecode.rb
+++ Library/Formula/gecode.rb
@@ -5,14 +5,8 @@ class Gecode < Formula
   homepage 'http://www.gecode.org/'
   md5 '8d505801f5730bd1b639fb2213b24919'
 
-  fails_with :clang do
-    build 318
-    cause "error: reference to non-static member function must be called"
-  end
-
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-examples"
     system "make install"
   end
 end
