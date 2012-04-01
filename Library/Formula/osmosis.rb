74e430cab7f465d89faceb3f14de84c7b21f01ae
Christoph Bünte <info@christophbuente.de>
2012-04-01 15:55:32 +0200

    Update osmosis to 0.40.1

--- Library/Formula/osmosis.rb
+++ Library/Formula/osmosis.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Osmosis < Formula
   homepage 'http://wiki.openstreetmap.org/wiki/Osmosis'
-  url 'http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-0.39.zip'
-  md5 'd3668ec3cf713246929de1b2f75eadef'
+  url 'http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-0.40.1.zip'
+  md5 '2baf3eb87647a81b163fb53928253209'
 
   def patches
     # need to adjust home dir for a clean install
@@ -25,5 +25,5 @@ __END__
  MYAPP_HOME=`cd "$MYAPP_HOME" && pwd`
 +MYAPP_HOME="$MYAPP_HOME/libexec"
  cd "$saveddir"
- 
+
  # Build up the classpath of required jar files via classworlds launcher.
