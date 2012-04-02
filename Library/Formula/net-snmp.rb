bccd6ed90aadbb228704e2cd3ffedf92a8a591fc
nibbles 2bits <nibbles@speakeasy.net>
2012-04-01 23:30:41 -0700

    net-snmp - fix compile error on Lion
    net-snmp-5.7.1 is missing a darwin11.h header that can be copied
    from the existing darwin10.h, if the 10s are changed to 11s.
    Configure also warns that mibII/icmp is required but disabled.
    
    * remove the configure flag --with-out-mib-modules=mibII/icmp
    * if lion, cp the darwin10.h to darwin11.h
    * patch the darwin11.h to replace the 10s with 11s.
    
    This builds again on Lion without error.  The patch has been
    reported upstream, and its url is in the formula comments.

--- Library/Formula/net-snmp.rb
+++ Library/Formula/net-snmp.rb
@@ -5,15 +5,46 @@ class NetSnmp < Formula
   url 'http://sourceforge.net/projects/net-snmp/files/net-snmp/5.7.1/net-snmp-5.7.1.tar.gz'
   md5 'c95d08fd5d93df0c11a2e1bdf0e01e0b'
 
+  def patches
+    # Fixes compile error on Lion, missing header darwin11.h
+    # The patch is reported upstream and does not exist in HEAD as of 2012-03-30.
+    # https://sourceforge.net/tracker/?func=detail&aid=3514049&group_id=12694&atid=312694
+    if MacOS.lion?
+      DATA if header_created?
+    end
+  end
+
   def install
+
     system "./configure", "--prefix=#{prefix}",
                           "--with-persistent-directory=#{var}/db/net-snmp",
                           "--with-defaults",
                           "--without-rpm",
                           "--with-mib-modules=host ucd-snmp/diskio",
-                          "--with-out-mib-modules=mibII/icmp",
                           "--without-kmem-usage"
     system "make"
     system "make install"
   end
+
+  def header_created?
+    cp 'include/net-snmp/system/darwin10.h', 'include/net-snmp/system/darwin11.h'
+    return TRUE
+  end
 end
+
+__END__
+--- a/include/net-snmp/system/darwin11.h
++++ b/include/net-snmp/system/darwin11.h
+@@ -44,9 +44,9 @@
+ /*
+  * This section defines Mac OS X 10.5 (and later) specific additions.
+  */
+-#define darwin 10
+-#ifndef darwin10
+-#   define darwin10 darwin
++#define darwin 11
++#ifndef darwin11
++#   define darwin11 darwin
+ #endif
+ 
+ /*
