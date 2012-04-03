143ae0018dd87652103a75a7a44fc241a21437f1
Jack Nagel <jacknagel@gmail.com>
2012-04-03 13:11:13 -0500

    hiredis: style nits
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/hiredis.rb
+++ Library/Formula/hiredis.rb
@@ -4,6 +4,7 @@ class Hiredis < Formula
   homepage 'https://github.com/antirez/hiredis'
   url 'https://github.com/antirez/hiredis/tarball/v0.10.0'
   sha1 'a54dd2b31cb39bc05bf88538c688bf50f8c6c9c7'
+
   head 'https://github.com/antirez/hiredis.git'
 
   fails_with :clang do
@@ -18,7 +19,6 @@ class Hiredis < Formula
     # Architecture isn't detected correctly on 32bit Snow Leopard without help
     ENV["OBJARCH"] = MacOS.prefer_64_bit? ? "-arch x86_64" : "-arch i386"
 
-    system "make PREFIX=#{prefix}"
-    system "make install PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 end
