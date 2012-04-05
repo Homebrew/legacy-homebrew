d0ee1a410a9989bdb6737bfe0975c74fadab48fe
Nils Homer <nilshomer@gmail.com>
2012-04-04 23:18:28 -0400

    tmap 0.3.7

---
+++ Library/Formula/tmap.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Tmap < Formula
+  homepage 'http://github.com/iontorrent/TMAP'
+  url 'http://github.com/iontorrent/TMAP/tarball/tmap.0.3.7'
+  md5 '2283f15ca05c27c534cef22850711bba'
+  head 'git://github.com/iontorrent/TMAP.git', :using => :git
+
+  def install
+    system "sh autogen.sh"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "tmap"
+  end
+end
