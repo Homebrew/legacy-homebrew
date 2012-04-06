cb94753b88b75c61cd780e37a5bfd28e7d076ed3
Jack Nagel <jacknagel@gmail.com>
2012-04-05 21:09:24 -0500

    Introduce new formula specs

--- Library/Homebrew/formula_support.rb
+++ Library/Homebrew/formula_support.rb
@@ -1,9 +1,9 @@
 require 'download_strategy'
 
+class SoftwareSpec
+  attr_reader :checksum, :mirrors, :specs, :strategy
 
-# Defines a URL and download method for a stable or HEAD build
-class SoftwareSpecification
-  attr_reader :url, :specs, :using
+  CHECKSUM_TYPES = [:md5, :sha1, :sha256].freeze
 
   VCS_SYMBOLS = {
     :bzr     => BazaarDownloadStrategy,
@@ -16,34 +16,122 @@ class SoftwareSpecification
     :svn     => SubversionDownloadStrategy,
   }
 
-  def initialize url, specs=nil
-    raise "No url provided" if url.nil?
-    @url = url
+  # Detect which type of checksum is being used, or nil if none
+  def checksum_type
+    @checksum_type ||= CHECKSUM_TYPES.detect do |type|
+      instance_variable_defined?("@#{type}")
+    end
+  end
+
+  def has_checksum?
+    (checksum_type and self.send(checksum_type)) || false
+  end
+
+  # Was the version defined in the DSL, or detected from the URL?
+  def explicit_version?
+    @explicit_version || false
+  end
+
+  # Returns a suitable DownloadStrategy class that can be
+  # used to retrieve this software package.
+  def download_strategy
+    return detect_download_strategy(@url) if @strategy.nil?
+
+    # If a class is passed, assume it is a download strategy
+    return @strategy if @strategy.kind_of? Class
+
+    detected = VCS_SYMBOLS[@strategy]
+    raise "Unknown strategy #{@strategy} was requested." unless detected
+    return detected
+  end
+
+  # The methods that follow are used in the block-form DSL spec methods
+  CHECKSUM_TYPES.each do |cksum|
+    class_eval %Q{
+      def #{cksum}(val=nil)
+        val.nil? ? @#{cksum} : @#{cksum} = val
+      end
+    }
+  end
+
+  def url val=nil, specs=nil
+    return @url if val.nil?
+    @url = val
     if specs.nil?
-      @using = nil
+      @strategy = nil
     else
-      # Get download strategy hint, if any
-      @using = specs.delete :using
-      # The rest of the specs are for source control
+      @strategy = specs.delete :using
       @specs = specs
     end
   end
 
-  # Returns a suitable DownloadStrategy class that can be
-  # used to retreive this software package.
-  def download_strategy
-    return detect_download_strategy(@url) if @using.nil?
-
-    # If a class is passed, assume it is a download strategy
-    return @using if @using.kind_of? Class
-
-    detected = VCS_SYMBOLS[@using]
-    raise "Unknown strategy #{@using} was requested." unless detected
-    return detected
+  def version val=nil
+    unless val.nil?
+      @version = val
+      @explicit_version = true
+    end
+    @version ||= Pathname.new(@url).version
+    return @version
   end
 
-  def detect_version
-    Pathname.new(@url).version
+  def mirror val, specs=nil
+    @mirrors ||= []
+    @mirrors << { :url => val, :specs => specs }
+  end
+end
+
+class HeadSoftwareSpec < SoftwareSpec
+  def initialize
+    super
+    @version = 'HEAD'
+    @checksum = nil
+  end
+
+  def verify_download_integrity fn
+    return
+  end
+end
+
+class Bottle < SoftwareSpec
+  attr_writer :url
+  attr_reader :revision
+
+  def initialize
+    @revision = 0
+    @strategy = CurlBottleDownloadStrategy
+  end
+
+  # Checksum methods in the DSL's bottle block optionally take
+  # a Hash, which indicates the platform the checksum applies on.
+  CHECKSUM_TYPES.each do |cksum|
+    class_eval %Q{
+      def #{cksum}(val=nil)
+        @#{cksum} ||= Hash.new
+        case val
+        when nil
+          @#{cksum}[MacOS.cat]
+        when String
+          @#{cksum}[:lion] = val
+        when Hash
+          key, value = val.shift
+          @#{cksum}[value] = key
+        end
+      end
+    }
+  end
+
+  def url val=nil
+    val.nil? ? @url : @url = val
+  end
+
+  # Used in the bottle DSL to set @revision, but acts as an
+  # as accessor for @version to preserve the interface
+  def version val=nil
+    if val.nil?
+      return @version ||= Pathname.new(@url).version
+    else
+      @revision = val
+    end
   end
 end
 
