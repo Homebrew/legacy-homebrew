d28b45cf48819227d0b4c762be779836c16f1469
mazy learns <tepidtenacity@gmail.com>
2012-04-05 20:25:19 -0700

    Fixed quotes

--- Library/Formula/watch.rb
+++ Library/Formula/watch.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Watch < Formula
-  homepage `http://procps.sourceforge.net/`
+  homepage 'http://procps.sourceforge.net/'
   url 'http://procps.sourceforge.net/procps-3.2.8.tar.gz'
-  md5 `9532714b6846013ca9898984ba4cd7e0`
+  md5 '9532714b6846013ca9898984ba4cd7e0'
 
   def patches
     # from debian
