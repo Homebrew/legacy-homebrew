5ed1927ce36603cfb4dd89377ebaf2ca0e831c80
Dave Rice <dericed@yahoo.com>
2012-04-05 21:30:43 -0300

    mediainfo 0.7.55
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/media-info.rb
+++ Library/Formula/media-info.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MediaInfo < Formula
   homepage 'http://mediainfo.sourceforge.net'
-  url 'http://downloads.sourceforge.net/mediainfo/MediaInfo_CLI_0.7.54_GNU_FromSource.tar.bz2'
-  md5 '91aefa130e98cd639452a4011578d761'
+  url 'http://downloads.sourceforge.net/mediainfo/MediaInfo_CLI_0.7.55_GNU_FromSource.tar.bz2'
+  md5 'ce02b3074e545f20c4a2c5c900b083a3'
 
   depends_on 'pkg-config' => :build
 
