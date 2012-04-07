e5870c53537257f1eee1c971d6be32217b23cbb8
Desert Crystal <dave@desertcrystal.com>
2012-04-08 06:32:01 +1000

    osslsigncode 1.4

---
+++ Library/Formula/osslsigncode.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Osslsigncode < Formula
+  homepage 'http://sourceforge.net/projects/osslsigncode/'
+  url 'http://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.4.tar.gz'
+  md5 '018b12b3efc4a5250dd3977c2bada3cd'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+
+end
