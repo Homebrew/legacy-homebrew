19ee16bdab079a958f7281a74e09ac72e0b2a3e1
Thomas Bonfort <thomas.bonfort@gmail.com>
2012-04-09 14:02:19 +0200

    update mapserver formula for HEAD/6.2
    - disable GD support by default
    - add missing dependencies: jpeg,giflib
    - default build enables gdal and postgis
    - optional support for cairo, ogr, gd
    - update install method => make install
    - caveats does not mention php if php not enabled

--- Library/Formula/mapserver.rb
+++ Library/Formula/mapserver.rb
@@ -4,19 +4,32 @@ class Mapserver < Formula
   homepage 'http://mapserver.org/'
   url 'http://download.osgeo.org/mapserver/mapserver-6.0.2.tar.gz'
   md5 'd831c905b1b0df7ac09a80c3f9387374'
+  head 'https://github.com/mapserver/mapserver.git'
 
-  depends_on 'gd'
+  depends_on 'gd' if not ARGV.build_head?
   depends_on 'proj'
-  depends_on 'gdal'
+  depends_on 'jpeg'
+
+  #we want gdal support if ogr was requested, unless gdal was explicitely disabled
+  depends_on 'gdal' if ARGV.include? '--with-ogr' or not ARGV.include? '--without-gdal'
 
   depends_on 'geos' if ARGV.include? '--with-geos'
-  depends_on 'postgresql' if ARGV.include? '--with-postgresql' and not MacOS.lion?
+  depends_on 'giflib' if not ARGV.build_head? or ARGV.include? '--with-gif'
+  depends_on 'cairo' if ARGV.include? '--with-cairo'
+
+  #postgis support enabled by default
+  depends_on 'postgresql' if not MacOS.lion? and not ARGV.include? '--without-postgresql'
 
   def options
     [
+      ["--with-gd", "Build support for the aging GD renderer"],
+      ["--with-ogr", "Build support for OGR vector access"],
       ["--with-geos", "Build support for GEOS spatial operations"],
       ["--with-php", "Build PHP MapScript module"],
-      ["--with-postgresql", "Build support for PostgreSQL as a data source"]
+      ["--with-gif", "Enable support for gif symbols"],
+      ["--with-cairo", "Enable support for cairo SVG and PDF output"],
+      ["--without-postgresql", "Disable support for PostgreSQL as a data source"],
+      ["--without-gdal", "Disable support for GDAL raster access"]
     ]
   end
 
@@ -24,15 +37,18 @@ class Mapserver < Formula
     args = [
       "--prefix=#{prefix}",
       "--with-proj",
-      "--with-gdal",
-      "--with-ogr",
       "--with-png=/usr/X11"
     ]
 
+    args.push "--with-gd" if ARGV.include? '--with-gd' or not ARGV.build_head?
+    args.push "--with-gdal" unless ARGV.include? '--without-gdal'
+    args.push "--without-gif" if ARGV.build_head? and not ARGV.include? '--with-gif'
+    args.push "--with-ogr" if ARGV.include? '--with-ogr'
     args.push "--with-geos" if ARGV.include? '--with-geos'
+    args.push "--with-cairo" if ARGV.include? '--with-cairo'
     args.push "--with-php=/usr/include/php" if ARGV.include? '--with-php'
 
-    if ARGV.include? '--with-postgresql'
+    unless ARGV.include? '--without-postgresql'
       if MacOS.lion? # Lion ships with PostgreSQL libs
         args.push "--with-postgis"
       else
@@ -46,31 +62,41 @@ class Mapserver < Formula
   def patches
     # Fix clang compilation issue, remove on future release
     # See http://trac.osgeo.org/mapserver/changeset/12809
-    DATA
+    DATA if not ARGV.build_head?
   end
 
   def install
     ENV.x11
     system "./configure", *configure_args
     system "make"
-    bin.install %w(mapserv shp2img legend shptree shptreevis
+    if not ARGV.build_head?
+      bin.install %w(mapserv shp2img legend shptree shptreevis
         shptreetst scalebar sortshp mapscriptvars tile4ms
         msencrypt mapserver-config)
+    else
+      system "make install"
+    end
+         
 
     if ARGV.include? '--with-php'
       prefix.install %w(mapscript/php/php_mapscript.so)
     end
   end
 
-  def caveats; <<-EOS.undent
-    The Mapserver CGI executable is #{prefix}/mapserv
-
-    If you built the PHP option:
-      * Add the following line to php.ini:
-        extension="#{prefix}/php_mapscript.so"
-      * Execute "php -m"
-      * You should see MapScript in the module list
+  def caveats
+    s = <<-EOS
+The Mapserver CGI executable is #{prefix}/mapserv
     EOS
+    if ARGV.include? '--with-php'
+      s << <<-EOS
+If you built the PHP option:
+  * Add the following line to php.ini:
+    extension="#{prefix}/php_mapscript.so"
+  * Execute "php -m"
+  * You should see MapScript in the module list
+      EOS
+    end
+    return s
   end
 end
 
