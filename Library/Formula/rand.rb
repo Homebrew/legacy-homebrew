5ef59140110491af69b604effff94c13d48284dc
Erik Greenwald <erik@brlcad.org>
2012-04-10 10:31:57 -0400

    Added a formula for rand

---
+++ Library/Formula/rand.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Rand < Formula
+  homepage ''
+  url 'http://www.elfga.com/~erik/files/rand-1.8.tar.bz2'
+  md5 'b05fb52321811a4d120faa297ee1e375'
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make install"
+  end
+
+  def test
+    system "true"
+  end
+end
