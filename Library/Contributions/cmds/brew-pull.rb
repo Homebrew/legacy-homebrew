Homebrew/legacy-homebrew

commit c27a575eb6b2dfc0d662aaab2cf9d754647fbadd

brew-pull: add support for pulling from taps

Library/Contributions/cmds/brew-pull.rb
@@ -3,24 +3,33 @@
 
 require 'utils'
 
+def tap arg
+  match = arg.match(%r[homebrew-(\w+)/])
+  match[1] if match
+end
+
 if ARGV.empty?
   onoe 'This command requires at least one argument containing a URL or pull request number'
 end
-
-Dir.chdir HOMEBREW_REPOSITORY
 
 ARGV.named.each do|arg|
   if arg.to_i > 0
     url = 'https://github.com/mxcl/homebrew/pull/' + arg
   else
     # This regex should work, if it's too precise, feel free to fix it.
-    url_match = arg.match 'https:\/\/github.com\/\w+\/homebrew\/(pull\/(\d+)|commit\/\w{4,40})'
+    url_match = arg.match 'https:\/\/github.com\/\w+\/homebrew(-\w+)?\/(pull\/(\d+)|commit\/\w{4,40})'
     unless url_match
       ohai 'Ignoring URL:', "Not a GitHub pull request or commit: #{arg}"
       next
     end
 
     url = url_match[0]
+  end
+
+  if tap url
+    Dir.chdir HOMEBREW_REPOSITORY/"Library/Taps/homebrew-#{tap url}"
+  else
+    Dir.chdir HOMEBREW_REPOSITORY
   end
 
   # GitHub provides commits'/pull-requests' raw patches using this URL.
@@ -65,7 +74,7 @@ ARGV.named.each do|arg|
     `git diff #{revision}.. --name-status`.each_line do |line|
       status, filename = line.split
       # Don't try and do anything to removed files.
-      if (status == 'A' or status == 'M') and filename.include? '/Formula/'
+      if (status == 'A' or status == 'M') and filename.include? '/Formula/' or tap url
         formula = File.basename(filename, '.rb')
         ohai "Installing #{formula}"
         # Not sure if this is the best way to install?

commit 0e33e0e1882622209fa725112acf7bb57454b9d5

brew-pull: Fix detecting issue #
Fixes a regression introduced in c27a575eb6b2dfc0d662aaab2cf9d754647fbadd
This is why I wish 1.8.x had named captures.

Library/Contributions/cmds/brew-pull.rb
@@ -53,7 +53,7 @@ ARGV.named.each do|arg|
 
   safe_system 'git', *patch_args
 
-  issue = arg.to_i > 0 ? arg.to_i : url_match[2]
+  issue = arg.to_i > 0 ? arg.to_i : url_match[3]
   if issue and not ARGV.include? '--clean'
     ohai "Patch closes issue ##{issue}"
     message = `git log HEAD^.. --format=%B`