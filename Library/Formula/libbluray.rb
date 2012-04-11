5deec9ba71902296eaedcd28a167aac4d3270052
Alex Zinchenko <admloki@gmail.com>
2012-04-12 01:48:41 +0400

    Updated libbluray formula according to recommendations

--- Library/Formula/libbluray.rb
+++ Library/Formula/libbluray.rb
@@ -7,27 +7,14 @@ class Libbluray < Formula
   mirror 'ftp://ftp.videolan.org/pub/videolan/libbluray/0.2.2/libbluray-0.2.2.tar.bz2'
   md5 'cb3254de43276861ea6b07c603f4651c'
 
-  depends_on 'openssl' if ARGV.include? '--with-openssl'
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
-
-  def options
-    [
-      ['--with-openssl', 'install OpenSSL to satisfy dependency']
-    ]
+  if MacOS.xcode_version >= "4.3"
+    depends_on 'automake' => :build
+    depends_on 'libtool' => :build
   end
 
   def install
-    unless Formula.factory("openssl").installed?
-      opoo 'Re-install formula with \'--with-openssl\' option if getting troubles.'
-    end
-
-    ohai 'Bootstrapping...'
-    quiet_system "./bootstrap"
-
-    config_args = ["--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"]
-
-    system "./configure", *config_args
+    system "./bootstrap"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
