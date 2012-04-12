f1fd6e80e4d71a6a2ffd5b547f0dd909596db231
Leonhardt Wille <wille@emma-mobil.de>
2012-04-12 05:24:02 +0200

    added SVN HEAD for libgphoto2 and gphoto2

--- Library/Formula/gphoto2.rb
+++ Library/Formula/gphoto2.rb
@@ -4,7 +4,8 @@ class Gphoto2 < Formula
   url 'http://downloads.sourceforge.net/project/gphoto/gphoto/2.4.11/gphoto2-2.4.11.tar.bz2'
   homepage 'http://gphoto.org/'
   md5 '2635075f702b40eb2e95a80658bd4773'
-
+  head 'https://gphoto.svn.sourceforge.net/svnroot/gphoto/branches/libgphoto2-2_4/gphoto2'
+  
   depends_on 'pkg-config' => :build
   depends_on 'jpeg'
   depends_on 'libgphoto2'
--- Library/Formula/libgphoto2.rb
+++ Library/Formula/libgphoto2.rb
@@ -4,7 +4,8 @@ class Libgphoto2 < Formula
   homepage 'http://www.gphoto.org/proj/libgphoto2/'
   url 'http://downloads.sourceforge.net/project/gphoto/libgphoto/2.4.13/libgphoto2-2.4.13.tar.bz2'
   md5 'd20a32fe2bb7d802a6a8c3b6f7f97c5e'
-
+  
+  head "https://gphoto.svn.sourceforge.net/svnroot/gphoto/branches/libgphoto2-2_4/libgphoto2"
   depends_on 'pkg-config' => :build
   depends_on 'libusb-compat'
   depends_on 'libexif' => :optional
