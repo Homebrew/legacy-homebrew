393d59e6ef75b8368768573e14f7329f9f4ea762
Frizlab <fload@me.com>
2012-04-14 01:56:06 +0200

    babl: Update to 0.1.10
    Closes #11635.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/babl.rb
+++ Library/Formula/babl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Babl < Formula
   homepage 'http://www.gegl.org/babl/'
-  url 'ftp://ftp.gimp.org/pub/babl/0.1/babl-0.1.6.tar.bz2'
-  md5 'dc960981a5ec5330fc1c177be9f59068'
+  url 'ftp://ftp.gtk.org/pub/babl/0.1/babl-0.1.10.tar.bz2'
+  md5 '9e1542ab5c0b12ea3af076a9a2f02d79'
 
   head 'git://git.gnome.org/babl'
 
@@ -14,7 +14,9 @@ class Babl < Formula
   end
 
   def patches
-    # Fixes an error when compiling with clang. See:
+    # There are two patches.
+    # The first one changes an include <values.h> (deleted on Mac OS X) to <limits.h>
+    # The second one fixes an error when compiling with clang. See:
     # https://trac.macports.org/browser/trunk/dports/graphics/babl/files/clang.patch
     { :p0 => DATA }
   end
@@ -34,25 +36,60 @@ class Babl < Formula
 end
 
 __END__
-Index: extensions/sse-fixups.c
-===================================================================
---- extensions/sse-fixups.c.orig 2011-06-28 20:01:39.000000000 -0700
-+++ extensions/sse-fixups.c 2011-06-28 20:01:29.000000000 -0700
-@@ -21,7 +21,7 @@
- 
- #include "config.h"
- 
--#if defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
-+#if !defined(__clang__) && defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
- 
- #include <stdint.h>
- #include <stdlib.h>
-@@ -173,7 +173,7 @@
- int
- init (void)
- {
--#if defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
-+#if !defined(__clang__) && defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
- 
-   Babl *rgbaF_linear = babl_format_new (
-     babl_model ("RGBA"),
+diff -cr babl.ori/babl-palette.c babl/babl-palette.c
+*** babl.ori/babl-palette.c	Sat Apr 14 01:31:40 2012
+--- babl/babl-palette.c	Sat Apr 14 01:32:15 2012
+***************
+*** 19,25 ****
+  #include <stdlib.h>
+  #include <string.h>
+  #include <stdio.h>
+! #include <values.h>
+  #include <assert.h>
+  #include "config.h"
+  #include "babl-internal.h"
+--- 19,25 ----
+  #include <stdlib.h>
+  #include <string.h>
+  #include <stdio.h>
+! #include <limits.h>
+  #include <assert.h>
+  #include "config.h"
+  #include "babl-internal.h"
+diff -cr extensions.ori/sse-fixups.c extensions/sse-fixups.c
+*** extensions.ori/sse-fixups.c	Sat Apr 14 01:31:40 2012
+--- extensions/sse-fixups.c	Sat Apr 14 01:33:44 2012
+***************
+*** 21,27 ****
+  
+  #include "config.h"
+  
+! #if defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
+  
+  #include <stdint.h>
+  #include <stdlib.h>
+--- 21,27 ----
+  
+  #include "config.h"
+  
+! #if !defined(__clang__) && defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
+  
+  #include <stdint.h>
+  #include <stdlib.h>
+***************
+*** 177,183 ****
+  int
+  init (void)
+  {
+! #if defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
+  
+    const Babl *rgbaF_linear = babl_format_new (
+      babl_model ("RGBA"),
+--- 177,183 ----
+  int
+  init (void)
+  {
+! #if !defined(__clang__) && defined(__GNUC__) && (__GNUC__ >= 4) && defined(USE_SSE) && defined(USE_MMX)
+  
+    const Babl *rgbaF_linear = babl_format_new (
+      babl_model ("RGBA"),
