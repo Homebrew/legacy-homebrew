b2859e368897e166bfff10860b46c661bb3e9857
Marcel Asio <marcel@asio.se>
2012-04-15 12:12:54 +0200

    A script to manage multiple git repos in same dir
    Movein is a script for managing multiple git repositories checked out
    to same directory, useful for managing dot-files of different classes
    for instance separating shell config from X11 config in different
    repositories.

---
+++ Library/Formula/movein.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Movein < Formula
+  homepage 'http://stew.vireo.org/movein/'
+  url 'http://stew.vireo.org/movein/downloads/movein-1.3.tar.gz'
+  md5 'bd86f4360fb91e69168920d416a9f9d9'
+  depends_on 'mr'
+
+  def install
+    bin.install ['movein']
+    man1.install gzip('movein')
+  end
+
+  def test
+    system "ls #{bin}/movein"
+  end
+end
