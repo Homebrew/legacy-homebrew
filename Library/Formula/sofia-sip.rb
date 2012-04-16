01b90572f298646e221e4f29c09bbf28124d7835
Thomas William <thomas.william@gwtonline-fb.de>
2012-04-16 09:03:56 +0200

    made the requested changes to the formula

--- Library/Formula/sofia-sip.rb
+++ Library/Formula/sofia-sip.rb
@@ -1,10 +1,12 @@
 require 'formula'
+
 class SofiaSip < Formula
-  depends_on 'glib'
-  depends_on 'gettext'
   homepage 'http://sofia-sip.sourceforge.net/'
   url 'http://downloads.sourceforge.net/project/sofia-sip/sofia-sip/1.12.11/sofia-sip-1.12.11.tar.gz'
   md5 'f3582c62080eeecd3fa4cd5d4ccb4225'
+
+  depends_on 'glib'
+  depends_on 'gettext'
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -14,7 +16,7 @@ class SofiaSip < Formula
 
   def test
     #bin: addrinfo    localinfo   sip-date    sip-dig     sip-options stunc
-    system "localinfo"
-    system "sip-date"
+    system "#{bin}/localinfo"
+    system "#{bin}/sip-date"
   end
 end
