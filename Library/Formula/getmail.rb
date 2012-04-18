72eac5b430e3a71ace5e2d7cd5713a6f6e74650c
nibbles 2bits <nibbles@speakeasy.net>
2012-04-17 21:05:59 -0700

    getmail 4.26.0 & build with python
    Upgrade getmail to version 4.26.0 and build it with python.
    Apparently getmail says it requires python, and all its documents
    say to build it using `python setup.py install`.  Remove all the
    manual install steps.  Building it as instructed puts the files
    in the correctly versioned `site-packages` and puts the correct
    interpreter line at the top of the scripts, and combines all the
    scripts into an egg, and installs the docs in the right location.

--- Library/Formula/getmail.rb
+++ Library/Formula/getmail.rb
@@ -1,15 +1,11 @@
 require 'formula'
 
 class Getmail < Formula
-  url 'http://pyropus.ca/software/getmail/old-versions/getmail-4.25.0.tar.gz'
   homepage 'http://pyropus.ca/software/getmail/'
-  md5 'ec0be67bc1e472c13201c1e3a0c35e66'
+  url 'http://pyropus.ca/software/getmail/old-versions/getmail-4.26.0.tar.gz'
+  sha1 'b350c98db9883365444c59e06ffc6672c68fa0f7'
 
   def install
-    scripts = %w[ getmail getmail_fetch getmail_maildir getmail_mbox ]
-    libexec.install 'getmailcore'
-    libexec_scripts = libexec.install scripts
-    bin.install_symlink libexec_scripts
-    man1.install Dir['docs/*.1']
+    system 'python', 'setup.py', 'install', "--prefix=#{prefix}"
   end
 end
