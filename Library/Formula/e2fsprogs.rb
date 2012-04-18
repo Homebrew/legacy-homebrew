921abababff1480c238105f7518c045194e6fde5
Will Ross <paxswill@gmail.com>
2012-04-18 02:53:28 -0400

    e2fsprogs: add HEAD
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/e2fsprogs.rb
+++ Library/Formula/e2fsprogs.rb
@@ -4,6 +4,7 @@ class E2fsprogs < Formula
   url 'http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/1.41.12/e2fsprogs-1.41.12.tar.gz'
   homepage 'http://e2fsprogs.sourceforge.net/'
   md5 '1b24a21fc0c2381ef420961cbfec733f'
+  head 'https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git', :using => :git
 
   keg_only "This brew installs several commands which override OS X-provided file system commands."
 
