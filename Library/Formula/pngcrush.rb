0698df1695ee34e169aa1be8cff909a69a234c1f
Jocelyn Mallon <jocelyn.e.mallon@gmail.com>
2012-04-18 01:11:44 -0700

    bump pngcrush to v1.7.27
    Starting with 1.7.26, pngcrush uses zlib 1.2.6, and requires additional CFLAGS to build, the flags were copied and pasted straight from the 1.7.27 makefile for statically linked.

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Pngcrush < Formula
   homepage 'http://pmt.sourceforge.net/pngcrush/'
-  url 'http://downloads.sourceforge.net/project/pmt/pngcrush/1.7.25/pngcrush-1.7.25.tar.bz2'
-  md5 'C891A418419EFA07AA513DB602D8F403'
+  url 'http://downloads.sourceforge.net/project/pmt/pngcrush/1.7.27/pngcrush-1.7.27.tar.bz2'
+  md5 '582AB2B4C262B8837CC2D30BF7D14F33'
 
   def install
     system "make", "CC=#{ENV.cc}",
                    "LD=#{ENV.cc}",
-                   "CFLAGS=#{ENV.cflags}",
+                   "CFLAGS=#{ENV.cflags} -I. -funroll-loops -fomit-frame-pointer -Wall -Wshadow -DZ_SOLO",
                    "LDFLAGS=#{ENV.ldflags}"
     bin.install 'pngcrush'
   end
