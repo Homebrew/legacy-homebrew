74def3a82809a15368cb6c7e0ab4b454367bb3d6
Christian Höltje <docwhat@gerf.org>
2012-04-18 11:21:06 -0400

    Added vim syntax support
    This creates a standard vimfiles configuration for
    the tmux syntax files. Instructions for use are
    included in the caveat blurb.

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -23,12 +23,29 @@ class Tmux < Formula
     system "make install"
 
     # Install bash completion scripts for use with bash-completion
-    (prefix+'etc/bash_completion.d').install "examples/bash_completion_tmux.sh" => 'tmux'
+    (etc+'bash_completion.d').install "examples/bash_completion_tmux.sh" => 'tmux'
+
+    # Install vim syntax file as a plugin.
+    (Pathname.pwd+'examples/ftdetect-tmux.vim').open('w') do |f|
+      f.print <<-EOS.undent
+        " Created by HomeBrew
+        au BufNewFile,BufRead .tmux.conf*,tmux.conf* set filetype=tmux
+      EOS
+    end
+    (share+'vim/vimfiles/ftdetect').install "examples/ftdetect-tmux.vim" => "tmux.vim"
+    (share+'vim/vimfiles/syntax').install "examples/tmux.vim"
+
   end
 
   def caveats; <<-EOS.undent
     Bash completion script was installed to:
       #{etc}/bash_completion.d/tmux
+
+    If you use Apple's vim and want syntax highlighting for
+    your .tmux.conf then add this to your .vimrc:
+        set runtimepath+=/usr/local/share/vim/vimfiles/
+
+    Syntax highlighting should work automatically with the vim from homedir-dupes.
     EOS
   end
 
