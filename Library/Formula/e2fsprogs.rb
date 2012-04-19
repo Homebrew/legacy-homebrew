57b7bef0109e2baf494d6e26316a3493b841e3b3
Adam Vandenberg <flangy@gmail.com>
2012-04-18 19:26:08 -0700

    e2fsprogs: also install libs

--- Library/Formula/e2fsprogs.rb
+++ Library/Formula/e2fsprogs.rb
@@ -1,17 +1,34 @@
 require 'formula'
 
 class E2fsprogs < Formula
-  url 'http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.42.2/e2fsprogs-1.42.2.tar.gz'
   homepage 'http://e2fsprogs.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.42.2/e2fsprogs-1.42.2.tar.gz'
   md5 '04f4561a54ad0419248316a00c016baa'
   head 'https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git', :using => :git
 
   keg_only "This brew installs several commands which override OS X-provided file system commands."
+
+  depends_on 'pkg-config' => :build
+
+  def patches
+    # MacPorts patch to compile libs correctly.
+    p = {:p0 => [
+      "https://trac.macports.org/export/92117/trunk/dports/sysutils/e2fsprogs/files/patch-lib__Makefile.darwin-lib"
+    ]}
+
+    # MacPorts patch to allow compilation on Leopard.
+    if MacOS.leopard?
+      p[:p0] << "https://trac.macports.org/export/92117/trunk/dports/sysutils/e2fsprogs/files/patch-lib-ext2fs-inline.c.diff"
+    end
+
+    p
+  end
 
   def install
     ENV.append_to_cflags "--std=gnu89 -Wno-return-type" if ENV.compiler == :clang
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
+    system "make install-libs"
   end
 end
