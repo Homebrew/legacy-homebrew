3300395471e1d12761e1889070206a81d92f1f94
Mike McQuaid <mike@mikemcquaid.com>
2012-04-19 15:21:39 -0500

    Add and correctly find erlang bottle.
    Fixes #11780.

--- Library/Homebrew/extend/pathname.rb
+++ Library/Homebrew/extend/pathname.rb
@@ -214,8 +214,10 @@ class Pathname
     return $1 if $1
 
     # eg. foobar4.5.1
-    /((\d+\.)*\d+)$/.match stem
-    return $1 if $1
+    unless /^erlang-/.match basename
+      /((\d+\.)*\d+)$/.match stem
+      return $1 if $1
+    end
 
     # eg foobar-4.5.0-bin
     /-((\d+\.)+\d+[abc]?)[-._](bin|dist|stable|src|sources?)$/.match stem
