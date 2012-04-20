0b63d2496dab1be6fc2c65c903c07c7bfba890b0
Sebastian Staudt <koraktor@gmail.com>
2012-04-20 18:57:19 +0200

    gource: Update to 0.38
    Gource now requires boost and glm.
    
    Closes #11782.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/gource.rb
+++ Library/Formula/gource.rb
@@ -2,16 +2,19 @@ require 'formula'
 
 class Gource < Formula
   homepage 'http://code.google.com/p/gource/'
-  url 'http://gource.googlecode.com/files/gource-0.37.tar.gz'
-  sha1 '87d88b5cbf312e160e55582f8e187c87b3df097d'
+  url 'http://gource.googlecode.com/files/gource-0.38.tar.gz'
+  sha1 '78f8c2064114313851f53b657d12db28abb89fae'
   head 'https://github.com/acaudwell/Gource.git'
 
+  depends_on 'glm' => :build
   depends_on 'pkg-config' => :build
-  depends_on 'sdl'
-  depends_on 'sdl_image'
+
+  depends_on 'boost'
+  depends_on 'glew'
   depends_on 'jpeg'
   depends_on 'pcre'
-  depends_on 'glew'
+  depends_on 'sdl'
+  depends_on 'sdl_image'
 
   if ARGV.build_head? and MacOS.xcode_version >= "4.3"
     depends_on "automake" => :build
