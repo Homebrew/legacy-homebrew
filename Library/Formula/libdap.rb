7a9d86fbd3ca62f3568f3d12fd40b250ed859334
Charlie Sharpsteen <source@sharpsteen.net>
2012-04-19 17:00:13 -0700

    LibDAP: Update to 3.11.2

--- Library/Formula/libdap.rb
+++ Library/Formula/libdap.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libdap < Formula
   homepage 'http://www.opendap.org'
-  url 'http://www.opendap.org/pub/source/libdap-3.11.1.tar.gz'
-  md5 '985b5b4f86394eea45a467c2100e9ec5'
+  url 'http://www.opendap.org/pub/source/libdap-3.11.2.tar.gz'
+  md5 'ab8ab83332525e048802200ea0552619'
 
   depends_on 'pkg-config' => :build
 
