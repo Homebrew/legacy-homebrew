5a653eb577fc1bb770af98d9519d03ffeac295b5
Paul Welch <welch.paulallen@gmail.com>
2012-04-20 11:12:03 -0400

    curl 7.25.0
    Closes #11778.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Curl < Formula
   homepage 'http://curl.haxx.se/'
-  url 'http://curl.haxx.se/download/curl-7.24.0.tar.bz2'
-  sha256 'ebdb111088ff8b0e05b1d1b075e9f1608285e8105cc51e21caacf33d01812c16'
+  url 'http://curl.haxx.se/download/curl-7.25.0.tar.bz2'
+  sha256 'cfb29139dfeb2449bf8a89963c3aed7d87f034013f05564cc0330b435c90b491'
 
   keg_only :provided_by_osx,
             "The libcurl provided by Leopard is too old for CouchDB to use."
