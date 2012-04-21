5821d24f94331eecd6a9b4a75a300cd0850d3918
Jack Nagel <jacknagel@gmail.com>
2012-04-20 22:02:41 -0500

    libnet 1.1.6
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libnet.rb
+++ Library/Formula/libnet.rb
@@ -1,29 +1,12 @@
 require 'formula'
 
 class Libnet < Formula
-  url "https://github.com/sam-github/libnet/tarball/libnet-1.1.4"
-  md5 "0cb6c04063c1db37c91af08c76d25134"
-  head 'https://github.com/sam-github/libnet.git'
   homepage 'https://github.com/sam-github/libnet'
-
-  if MacOS.xcode_version >= "4.3"
-    # when and if the tarball provides configure, remove autogen.sh and these deps
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  url 'http://sourceforge.net/projects/libnet-dev/files/libnet-1.1.6.tar.gz'
+  sha1 'dffff71c325584fdcf99b80567b60f8ad985e34c'
 
   def install
-    cd 'libnet'
-    inreplace "autogen.sh", "libtoolize", "glibtoolize"
-    system "./autogen.sh"
-
-    unless MacOS.leopard?
-      cp "/usr/share/libtool/config/config.guess", "."
-      cp "/usr/share/libtool/config/config.sub", "."
-    end
-
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-    touch 'doc/man/man3/libnet.3'
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
 end
