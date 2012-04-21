2bd8a6933e538e00b1b360648709b92086af738c
Pepijn de Vos <pepijndevos@gmail.com>
2012-04-21 21:04:41 +0200

    pygtkglext formula

---
+++ Library/Formula/pygtkglext.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class Pygtkglext < Formula
+  homepage 'http://projects.gnome.org/gtkglext/download.html#pygtkglext'
+  url 'http://downloads.sourceforge.net/gtkglext/pygtkglext-1.1.0.tar.gz'
+  md5 'dfbe2ceb05db9265a7d94b209fa8ad97'
+
+  depends_on 'pygtk'
+  depends_on 'pygobject'
+
+  def install
+    system "export", "PYGTK_CODEGEN=\"`which pygobject-codegen-2.0`\""
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    mktemp do
+      (Pathname.pwd+'test.py').write <<-EOS.undent
+        #!/usr/bin/env python
+        import pygtk
+        pygtk.require('2.0')
+        import gtk.gtkgl
+      EOS
+      system "chmod +x test.py"
+      system "./test.py"
+    end
+  end
+end
