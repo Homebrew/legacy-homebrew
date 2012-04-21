2cd0203c216ecb66cd11a61f390d391134689a09
Adam Vandenberg <flangy@gmail.com>
2012-04-21 09:33:03 -0700

    Google-Sparsehash 1.12

--- Library/Formula/google-sparsehash.rb
+++ Library/Formula/google-sparsehash.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class GoogleSparsehash < Formula
-  url 'http://google-sparsehash.googlecode.com/files/sparsehash-1.11.tar.gz'
   homepage 'http://code.google.com/p/google-sparsehash/'
-  sha1 '9bd33e0336420058ff96873f7651e8e18aaea056'
+  url 'http://google-sparsehash.googlecode.com/files/sparsehash-1.12.tar.gz'
+  sha1 '6c6da5d03b6b71ba69cf056087a94b5f01048782'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make check"
     system "make install"
   end
