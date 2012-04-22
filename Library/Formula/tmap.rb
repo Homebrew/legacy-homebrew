26035e15f3c967e69c4dc56c0b9caea0bc715ca5
Nils Homer <nilshomer@gmail.com>
2012-04-22 14:14:08 -0700

    * adding missing line

--- Library/Formula/tmap.rb
+++ Library/Formula/tmap.rb
@@ -4,6 +4,7 @@ class Tmap < Formula
   homepage 'http://github.com/iontorrent/TMAP'
   url 'http://github.com/iontorrent/TMAP/tarball/tmap.0.3.7'
   md5 '2283f15ca05c27c534cef22850711bba'
+
   head 'git://github.com/iontorrent/TMAP.git', :using => :git
 
   def install
