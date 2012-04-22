0561d0f97f348bee58cf99cd1332257daf540d81
aas <alexander.ashurkov@gmail.com>
2012-04-22 23:45:46 +0600

    Midnight Commander 4.8.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class MidnightCommander < Formula
-  url 'http://www.midnight-commander.org/downloads/mc-4.8.0.tar.bz2',
-      :using => CurlUnsafeDownloadStrategy
   homepage 'http://www.midnight-commander.org/'
-  sha256 'dbf077b318c13fc6d465dc67bd43958f067b9ff7e21041975bd14927dfa31b52'
+  url 'http://www.midnight-commander.org/downloads/mc-4.8.3.tar.bz2',
+      :using => CurlUnsafeDownloadStrategy
+  sha256 '445f286652b85c3e8e87839bad64c28ad2dc80661778571a0b59c2b920ef60ac'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
