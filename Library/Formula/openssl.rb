3821d98f0e2f9cfd4ea5171f530723dd480c760f
Birger J. Nordølum <birgerjn@gmail.com>
2012-04-23 10:09:56 +0200

    OpenSSL 1.0.1a
    [jn: no-asm is to fix compilation on 64-bit; perhaps it is not needed on
    32-bit, but I didn't test it and I don't think its that important.]
    
    Closes #11821.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Openssl < Formula
-  homepage 'http://www.openssl.org'
-  url 'http://www.openssl.org/source/openssl-1.0.1.tar.gz'
-  sha1 'a6476d33fd38c2e7dfb438d1e3be178cc242c907'
+  homepage 'http://openssl.org'
+  url 'http://openssl.org/source/openssl-1.0.1a.tar.gz'
+  sha256 'd3487e09d891c772cf946273a3bb0ca47479e7941be6d822274320e7cfcc361b'
 
   keg_only :provided_by_osx,
     "The OpenSSL provided by OS X is too old for some software."
@@ -12,6 +12,7 @@ class Openssl < Formula
     args = %W[./Configure
                --prefix=#{prefix}
                --openssldir=#{etc}/openssl
+               no-asm
                zlib-dynamic
                shared
              ]
