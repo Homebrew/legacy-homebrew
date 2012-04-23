b4f02a686dde979625fdb521957184a3908d8237
Gildas Cuisinier <gildas.cuisinier@gcuisinier.net>
2012-04-23 11:30:17 +0200

    Update CRaSH formula to 1.0.0

--- Library/Formula/crash.rb
+++ Library/Formula/crash.rb
@@ -3,9 +3,9 @@ require 'formula'
 
 class Crash < Formula
   homepage 'http://vietj.github.com/crash/'
-  url 'http://crsh.googlecode.com/files/crsh-1.0.0-cr2.tar.gz'
-  md5 '3dece12afa54044b575a7a54d04ac12d'
-  version '1.0.0-cr2'
+  url 'http://crsh.googlecode.com/files/crsh-1.0.0.tar.gz'
+  md5 '568e43affde5feca3e4c8b20a53fad95'
+  version '1.0.0'
 
   def install
     # Install 
