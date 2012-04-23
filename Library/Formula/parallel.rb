8bcc9f9c7e4d891e3a84486315df0dbc3aeb1d0d
nibbles 2bits <nibbles@speakeasy.net>
2012-04-23 14:37:22 -0700

    parallel 20120422
    Upgrade parallel to version 20120422.
    
    Closes #11839.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Parallel < Formula
   homepage 'http://savannah.gnu.org/projects/parallel/'
-  url 'http://ftpmirror.gnu.org/parallel/parallel-20120322.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20120322.tar.bz2'
-  md5 'ceb12fd5eac4a9d2ca18ffebe970b0be'
+  url 'http://ftpmirror.gnu.org/parallel/parallel-20120422.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20120422.tar.bz2'
+  sha1 '1f0e9bdca7271fa6932a570980b8a3b335b2eaa5'
 
   def install
     system "./configure", "--prefix=#{prefix}"
