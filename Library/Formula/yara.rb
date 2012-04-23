35d7feb061892ad316f89dc3d4d8222c4ef08f73
Patrick McLaughlin <psm14case@gmail.com>
2012-04-23 10:31:39 -0400

    Update yara to 1.6

--- Library/Formula/yara.rb
+++ Library/Formula/yara.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Yara < Formula
   homepage 'http://code.google.com/p/yara-project/'
-  url 'http://yara-project.googlecode.com/files/yara-1.4.tar.gz'
-  md5 'ecc744a67482dc9d717936ccd69dc39f'
+  url 'http://yara-project.googlecode.com/files/yara-1.6.tar.gz'
+  md5 'c54fe284181df90e0520810797821287'
 
   depends_on 'pcre'
 
