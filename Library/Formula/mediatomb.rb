079a966530879640ef8cf04283e11b255394c725
Adam Vandenberg <flangy@gmail.com>
2012-04-24 09:10:16 -0700

    mediatomb: fails with clang

--- Library/Formula/mediatomb.rb
+++ Library/Formula/mediatomb.rb
@@ -12,6 +12,18 @@ class Mediatomb < Formula
     "https://launchpadlibrarian.net/71985647/libav_0.7_support.patch"
   end
 
+  fails_with :clang do
+    build 318
+    cause <<-EOS.undent
+      In file included from ../src/content_manager.cc:45:
+      In file included from ../src/content_manager.h:36:
+      In file included from ../src/storage.h:40:
+      In file included from ../src/hash.h:47:
+      ../src/hash/dbr_hash.h:127:15: error: use of undeclared identifier 'search'
+              if (! search(key, &slot))
+    EOS
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
