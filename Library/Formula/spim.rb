4047c6f2316f55a03d154674452834411ce9c389
Patrick O'Doherty <p@trickod.com>
2012-04-24 00:21:59 -0700

    Updated the SPIM package

--- Library/Formula/spim.rb
+++ Library/Formula/spim.rb
@@ -2,24 +2,22 @@ require 'formula'
 
 class Spim < Formula
   homepage 'http://pages.cs.wisc.edu/~larus/spim.html'
-  url 'http://www.cs.wisc.edu/~larus/SPIM/spim.tar.gz'
+  url 'http://patrickod.com/code/spim.tar.gz'
   version '8.0'
-  md5 '146558e8256f2b7577fb825fdc76a04f'
+  md5 'cf3b40b9bc6eea3a7173b7c6bf834ce6'
 
   def install
-    cd 'spim' do
-      inreplace "Makefile" do |s|
-        s.change_make_var! "BIN_DIR", bin
-        s.change_make_var! "EXCEPTION_DIR", libexec
-        s.change_make_var! "MAN_DIR", man1
-      end
-
-      system "make"
-      system "make install"
-      system "make install-man"
-      system "make test"
-
-      mv "#{man1}/spim.man", "#{man1}/spim.1"
+    inreplace "Makefile" do |s|
+      s.change_make_var! "BIN_DIR", bin
+      s.change_make_var! "EXCEPTION_DIR", libexec
+      s.change_make_var! "MAN_DIR", man1
     end
+
+    system "make"
+    system "make install"
+    system "make install-man"
+    system "make test"
+
+    mv "#{man1}/spim.man", "#{man1}/spim.1"
   end
 end
