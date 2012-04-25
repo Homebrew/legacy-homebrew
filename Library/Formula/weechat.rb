03a49c08b19adef065923e3de27b992d3ed8784d
nibbles 2bits <nibbles@speakeasy.net>
2012-04-24 18:04:53 -0700

    weechat: Fix args and bindings
    * Craft the `args` using std_cmake_parameters.split.
    * Simplify how the options adjust the args using build defaults.
    * Support user installed python with the usual vtk.rb method for
    finding the python libs and headers.
    * Patch FindPython.cmake to not query LINKFORSHARED.
    * Add the standard PYTHONPATH caveat and which_python function.
    * Only patch for perl if not HEAD.  Fixed upstream recently.
    * Use latest FindRuby.cmake module from the Cellar.
    * Add FindPackageHandleStandardArgs.cmake needed by FindRuby.
    
    These changes fix how the previous commit malformed the args,
    allowing the args to have the desired effect, where previously
    they were not put into the array correctly, causing -Wno-dev
    for example to be be ignored.
    
    This fixes the python bindings by patching FindPython.cmake
    because it tries to use a Python variable, LINKFORSHARED,
    that is only crafted for Python's Makefiles, not for external
    programs to query, as it contains a non-existent path.  The
    fix is needed when Python is built as a framework, not loose.
    
    This adds the glue in use by vtk.rb to locate the Python libs
    and Python headers that could be in various places.
    
    The inreplace for Perl has been fixed upstream.  So add a
    conditional to that code so that it only applies to stable.
    
    Let's try the most recent FindRuby.cmake from cmake-2.8.8 to see
    if it can find the Ruby headers and libraries correctly.  This is
    discussed in the issue below, and an issue has been opened with
    the rbenv developers on GitHub to verify that the values found
    by FindRuby are accurate.
    
    Neither the Python patch nor the Ruby inreplace has been reported
    to the weechat developers until the Ruby issue is solved.
    
    This would fix #11835

--- Library/Formula/weechat.rb
+++ Library/Formula/weechat.rb
@@ -18,43 +18,100 @@ class Weechat < Formula
     ]
   end
 
+  def patches
+    # Weechat uses a Python variable that's not designed for external progs.
+    # LINKFORSHARED is used by Python's Makefiles internally and doesn't point
+    # to a directory that exists.  This is unreported at this time but will be
+    # filed with the weechat developers along with the Ruby hacks being tested.
+    DATA if python? and python_framework?
+  end
+
   def install
+    # Weechat has problems finding Ruby Libraries and Includes, but it uses
+    # an outdated FindRuby.cmake.  cp the Homebrew one and try that.
+    if ruby? then
+      modules = "#{Formula.factory('cmake').share}/cmake/Modules"
+      cp "#{modules}/FindPackageHandleStandardArgs.cmake", './cmake'
+      cp "#{modules}/FindRuby.cmake", './cmake'
+    end
+
     # Remove all arch flags from the PERL_*FLAGS as we specify them ourselves.
     # This messes up because the system perl is a fat binary with 32, 64 and PPC
-    # compiles, but our deps don't have that.
-    archs = ['-arch ppc', '-arch i386', '-arch x86_64'].join('|')
-
-    inreplace  "src/plugins/scripts/perl/CMakeLists.txt",
-      'IF(PERL_FOUND)',
-      'IF(PERL_FOUND)' +
-      %Q{\n  STRING(REGEX REPLACE "#{archs}" "" PERL_CFLAGS "${PERL_CFLAGS}")} +
-      %Q{\n  STRING(REGEX REPLACE "#{archs}" "" PERL_LFLAGS "${PERL_LFLAGS}")}
-
-    args = []
-
-    if ARGV.include? "--enable-ruby"
-      args << "-DENABLE_RUBY=ON"
-    else
-      args << "-DENABLE_RUBY=OFF"
-    end
-
-    if ARGV.include? "--enable-perl"
-      args << "-DENABLE_PERL=ON"
-    else
-      args << "-DENABLE_PERL=OFF"
-    end
-
-    if ARGV.include? "--enable-python"
-      args << "-DENABLE_PYTHON=ON"
-    else
-      args << "-DENABLE_PYTHON=OFF"
+    # compiles, but our deps don't have that.  This is fixed in HEAD.
+    # Remove at version 0.3.8.
+    unless ARGV.build_head? then
+      archs = ['-arch ppc', '-arch i386', '-arch x86_64'].join('|')
+      inreplace  "src/plugins/scripts/perl/CMakeLists.txt",
+        'IF(PERL_FOUND)',
+        'IF(PERL_FOUND)' +
+        %Q{\n  STRING(REGEX REPLACE "#{archs}" "" PERL_CFLAGS "${PERL_CFLAGS}")} +
+        %Q{\n  STRING(REGEX REPLACE "#{archs}" "" PERL_LFLAGS "${PERL_LFLAGS}")}
     end
 
     # -DPREFIX has to be specified because weechat devs enjoy being non-standard
-    system "cmake", "-DPREFIX=#{prefix}",
-                    args.join(" "),
-                    std_cmake_parameters,
-                    "."
-    system "make install"
+    args = std_cmake_parameters.split + %W[ -DPREFIX=#{prefix} ]
+    args << '-DENABLE_RUBY=OFF' unless ruby?
+    args << '-DENABLE_PERL=OFF' unless perl?
+
+    if python?
+      if python_framework?
+        args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
+        args << "-DPYTHON_INCLUDE_PATH='#{python_prefix}/Headers'"
+      else
+        python_version = `python-config --libs`.match('-lpython(\d+\.\d+)').captures.at(0)
+        python_lib = "#{python_prefix}/lib/libpython#{python_version}"
+        args << "-DPYTHON_INCLUDE_PATH='#{python_prefix}/include/python#{python_version}'"
+        if File.exists? "#{python_lib}.a"
+          args << "-DPYTHON_LIBRARY='#{python_lib}.a'"
+        else
+          args << "-DPYTHON_LIBRARY='#{python_lib}.dylib'"
+        end
+      end
+    else
+      args << '-DENABLE_PYTHON=OFF'
+    end
+
+    args << '.'
+    system "cmake", *args
+    system 'make install'
+  end
+
+  def caveats; <<-EOS.undent
+    If you build weechat with --enable-python, then amend your PYTHONPATH like so:
+      export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages:$PYTHONPATH
+    EOS
+  end
+
+  def python?; ARGV.include? '--enable-python'; end
+  def ruby?; ARGV.include? '--enable-ruby'; end
+  def perl?; ARGV.include? '--enable-perl'; end
+
+  def python_prefix
+    python_prefix = `python-config --prefix`.strip
+  end
+
+  def python_framework?
+    # Python was compiled with --framework:
+    File.exist? "#{python_prefix}/Python"
+  end
+
+  def which_python
+    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
   end
 end
+
+__END__
+--- a/cmake/FindPython.cmake	2012-01-08 02:15:27.000000000 -0800
++++ b/cmake/FindPython.cmake	2012-04-29 18:55:03.000000000 -0700
+@@ -49,10 +49,7 @@
+     OUTPUT_VARIABLE PYTHON_POSSIBLE_LIB_PATH
+     )
+ 
+-  EXECUTE_PROCESS(
+-    COMMAND ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import *; print(get_config_var('LINKFORSHARED'))"
+-    OUTPUT_VARIABLE PYTHON_LFLAGS
+-    )
++  SET(PYTHON_LFLAGS "-u _PyMac_Error")
+ 
+   # remove the new lines from the output by replacing them with empty strings
+   STRING(REPLACE "\n" "" PYTHON_INC_DIR "${PYTHON_INC_DIR}")
