bc290caf5a60ee26540224c5404d0deb9d610413
Simon Head <simon@cern.ch>
2012-04-25 19:52:16 +0100

    xrootd 3.1.1
    XRootD provides tools and ways to build high performance scalable
    data repositories.
    
    Closes #11879.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/xrootd.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Xrootd < Formula
+  homepage 'http://xrootd.slac.stanford.edu/index.html'
+  url 'http://xrootd.slac.stanford.edu/download/v3.1.1/xrootd-3.1.1.tar.gz'
+  md5 '6466b12a99aed3f8ea0b56b5b3ace093'
+
+  depends_on 'cmake' => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_parameters
+      system "make install"
+    end
+  end
+
+  def test
+    system "#{bin}/xrootd"
+  end
+end
