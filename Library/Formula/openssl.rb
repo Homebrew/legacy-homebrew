ec0025342449dd8b6f2039d5e401b2df78f365ee
Birger J. Nordølum <birgerjn@gmail.com>
2012-04-25 10:56:35 +0200

    OpenSSL: Fix regression in 1.0.1a for Mac OS 64-bit
    Closes #11869.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -8,11 +8,17 @@ class Openssl < Formula
   keg_only :provided_by_osx,
     "The OpenSSL provided by OS X is too old for some software."
 
+  def patches
+    # Fixed regression with Mac OS X and 64-bit.
+    # http://rt.openssl.org/Ticket/Display.html?id=2797
+    # Can be removed with next release. Please check first.
+    DATA
+  end
+
   def install
     args = %W[./Configure
                --prefix=#{prefix}
                --openssldir=#{etc}/openssl
-               no-asm
                zlib-dynamic
                shared
              ]
@@ -27,3 +33,18 @@ class Openssl < Formula
     system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl"
   end
 end
+
+__END__
+diff --git i/crypto/evp/e_rc4_hmac_md5.c w/crypto/evp/e_rc4_hmac_md5.c
+index 3f32b25..e65380d 100644
+--- i/crypto/evp/e_rc4_hmac_md5.c
++++ w/crypto/evp/e_rc4_hmac_md5.c
+@@ -103,7 +103,8 @@ static int rc4_hmac_md5_init_key(EVP_CIPHER_CTX *ctx,
+ #if	!defined(OPENSSL_NO_ASM) &&	( \
+ 	defined(__x86_64)	|| defined(__x86_64__)	|| \
+ 	defined(_M_AMD64)	|| defined(_M_X64)	|| \
+-	defined(__INTEL__)		)
++	defined(__INTEL__)		) && \
++	!(defined(__APPLE__) && defined(__MACH__))
+ #define	STITCHED_CALL
+ #endif
