b31af54bc6d1e7efbd333a05d23d8062f552ecc9
Peter Aronoff <telemachus@arpinum.org>
2012-04-25 14:39:49 -0400

    Upgrade mu to 0.9.8.3

--- Library/Formula/mu.rb
+++ Library/Formula/mu.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Mu < Formula
-  url 'http://mu0.googlecode.com/files/mu-0.9.7.tar.gz'
-  sha1 '8641d579a770d59124b72433712841736d326ca6'
+  url 'http://mu0.googlecode.com/files/mu-0.9.8.3.tar.gz'
+  sha1 'e66396783b9424cb7bf740bc309bd1361f399d16'
   homepage 'http://www.djcbsoftware.nl/code/mu/'
   head 'git://gitorious.org/mu/old.git'
 
