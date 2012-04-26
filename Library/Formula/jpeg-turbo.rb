ee51c0b9556fe22f4bbb5ed03d3f94fff7d6c2d5
Rui Pereira <rui.pereira@gmail.com>
2012-04-26 10:44:51 +0200

    libjpeg-turbo 1.2.0
    libjpeg-turbo is a derivative of libjpeg that uses
    SIMD instructions (MMX, SSE2, NEON) to accelerate
    baseline JPEG compression and decompression on
    x86, x86-64, and ARM systems.
    
    Closes #11894.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/jpeg-turbo.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class JpegTurbo < Formula
+  homepage 'http://www.libjpeg-turbo.org/'
+  url 'http://downloads.sourceforge.net/project/libjpeg-turbo/1.2.0/libjpeg-turbo-1.2.0.tar.gz'
+  sha1 '4ab00afc9a8d54cd2e7a67aacb9c49e01a8bccac'
+
+  depends_on 'nasm' => :build if MacOS.prefer_64_bit?
+
+  keg_only "libjpeg-turbo is not linked to prevent conflicts with the standard libjpeg."
+
+  def install
+    args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
+    args << "--host=x86_64-apple-darwin" if MacOS.prefer_64_bit?
+    system "./configure", *args
+    system "make install"
+  end
+
+  def test
+    mktemp do
+      system "#{bin}/jpegtran -crop 500x500+200+500 -transpose -perfect -outfile test.jpg /System/Library/CoreServices/DefaultDesktop.jpg"
+      system "/usr/bin/qlmanage", "-p", "test.jpg"
+    end
+  end
+end
