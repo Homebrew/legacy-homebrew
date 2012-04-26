f83d5cda6ec3164fa227734d8650cc50031039ad
Anatol Pomozov <anatol.pomozov@gmail.com>
2012-04-25 17:26:12 -0700

    Revert "fuse4x: update to 0.10.0"
    0.10.0 contains a kernel panic. It is going to be fixed in 0.10.1.
    
    This reverts commit 8a0932f320208db56c10d1d16fff31c22e3be87e.
    
    Closes #11888.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/fuse4x.rb
+++ Library/Formula/fuse4x.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Fuse4x < Formula
   homepage 'http://fuse4x.org/'
-  url 'https://github.com/fuse4x/fuse/tarball/fuse4x_0_10_0'
-  md5 '10bacfd8318714de72a95e8baf62d6cd'
-  version "0.10.0"
+  # NOTE: Version 0.10 contains a nasty race condition. Wait for 0.10.1
+  url 'https://github.com/fuse4x/fuse.git', :tag => "fuse4x_0_9_0"
+  version "0.9.0"
 
   depends_on "automake" => :build
   depends_on "libtool" => :build
