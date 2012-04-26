b1cb1c362216a463c91bde029fe68cea9facd90e
Ryan Schlesinger <ryan@instanceinc.com>
2012-04-26 10:13:17 -0700

    mobile-shell 1.2 + HEAD
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mobile-shell.rb
+++ Library/Formula/mobile-shell.rb
@@ -2,14 +2,16 @@ require 'formula'
 
 class MobileShell < Formula
   homepage 'http://mosh.mit.edu/'
-  url 'https://github.com/downloads/keithw/mosh/mosh-1.1.3.tar.gz'
-  md5 'de507695b6f67523bc81596f97220a9a'
+  url 'https://github.com/downloads/keithw/mosh/mosh-1.2.tar.gz'
+  md5 '2822792d362cc1539278ca3d3afb279a'
 
   head 'https://github.com/keithw/mosh.git'
 
+  # Needs new autoconf for correct AC_C_RESTRICT macro
+  # See: https://github.com/keithw/mosh/issues/241
+  depends_on 'autoconf' => :build if ARGV.build_head?
   depends_on 'pkg-config' => :build
   depends_on 'protobuf'
-  depends_on 'boost'
 
   def install
     system "./autogen.sh" if ARGV.build_head?
