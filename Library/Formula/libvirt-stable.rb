8e60ddecf3237572d7f87208e5229a55aa2ab5af
Justin Clift <jclift@redhat.com>
2012-04-27 12:21:47 +1000

    New formula: libvirt-stable
    Upstream libvirt has moved to a new release process, with both a
    stable release (this formula), and it's regular roughly-monthly
    leading edge release (the existing libvirt formula).

---
+++ Library/Formula/libvirt-stable.rb
@@ -0,0 +1,62 @@
+require 'formula'
+
+class LibvirtStable < Formula
+  homepage 'http://www.libvirt.org'
+  url 'ftp://libvirt.org/libvirt/libvirt-0.9.11.2.tar.gz'
+  sha256 '75cf3514014ba57657550bd64c7cc2de069ee295ddb26fd9230672813859a427'
+
+  depends_on "gnutls"
+  depends_on "yajl"
+
+  if MacOS.leopard?
+    # Definitely needed on Leopard, but not on Snow Leopard.
+    depends_on "readline"
+    depends_on "libxml2"
+  end
+
+  fails_with :llvm do
+    build 2326
+    cause "Undefined symbols when linking"
+  end
+
+  def options
+    [['--without-libvirtd', 'Build only the virsh client and development libraries.']]
+  end
+
+  def install
+    args = ["--prefix=#{prefix}",
+            "--localstatedir=#{var}",
+            "--mandir=#{man}",
+            "--sysconfdir=#{etc}",
+            "--with-esx",
+            "--with-init-script=none",
+            "--with-remote",
+            "--with-test",
+            "--with-vbox",
+            "--with-vmware",
+            "--with-yajl",
+            "--without-qemu"]
+
+    args << "--without-libvirtd" if ARGV.include? '--without-libvirtd'
+
+    system "./configure", *args
+
+    # Compilation of docs doesn't get done if we jump straight to "make install"
+    system "make"
+    system "make install"
+
+    # Update the SASL config file with the Homebrew prefix
+    inreplace "#{etc}/sasl2/libvirt.conf" do |s|
+      s.gsub! "/etc/", "#{HOMEBREW_PREFIX}/etc/"
+    end
+
+    # If the libvirt daemon is built, update its config file to reflect
+    # the Homebrew prefix
+    unless ARGV.include? '--without-libvirtd'
+      inreplace "#{etc}/libvirt/libvirtd.conf" do |s|
+        s.gsub! "/etc/", "#{HOMEBREW_PREFIX}/etc/"
+        s.gsub! "/var/", "#{HOMEBREW_PREFIX}/var/"
+      end
+    end
+  end
+end
