69ef3a5e3e67ca2f30abdf8e471fe9398cb8e3c3
nibbles 2bits <nibbles@speakeasy.net>
2012-04-28 13:58:17 -0700

    getxbook 0.9
    Upgrade getxbook to version 0.9.
    Remove the patch for clang.  The issue is fixed in 0.9.
    Tested with llvm and clang.
    
    Closes #11929.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/getxbook.rb
+++ Library/Formula/getxbook.rb
@@ -2,31 +2,10 @@ require 'formula'
 
 class Getxbook < Formula
   homepage 'http://njw.me.uk/software/getxbook/'
-  url 'http://njw.me.uk/software/getxbook/getxbook-0.8.tar.bz2'
-  sha1 'd08d082cd6481302c102fcd7c373258737b2bd3d'
-
-  def patches
-    # Remove -Werror, fixes compilation with clang.  Not an issue for llvm-gcc.
-    #    error: too many arguments in call to 'getpagelist'
-    # Reported upstream via email to bug-getxbook@njw.me.uk (no tracker).
-    # This issue still exists in HEAD as of 19 APR 2012.
-    DATA
-  end
+  url 'http://njw.me.uk/software/getxbook/getxbook-0.9.tar.bz2'
+  sha1 '833b37fd121b24090ce59424886571be4bedf6c2'
 
   def install
     system "make", "CC=#{ENV.cc}", "PREFIX=#{prefix}", "install"
   end
 end
-
-__END__
---- a/config.mk	2012-02-14 14:09:56.000000000 -0800
-+++ b/config.mk	2012-04-19 23:24:22.000000000 -0700
-@@ -6,7 +6,7 @@
- PREFIX = /usr/local
- MANPREFIX = $(PREFIX)/share/man
- 
--CFLAGS = -std=c99 -pedantic -Wall -Wextra -Werror -g -D_POSIX_C_SOURCE=200112L \
-+CFLAGS = -std=c99 -pedantic -Wall -Wextra -g -D_POSIX_C_SOURCE=200112L \
-          -DVERSION=\"$(VERSION)\"
- 
- W32TCLKIT = tclkit-8.5.9-win32.upx.exe
