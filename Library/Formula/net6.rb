ecc95eaf09ab6b3b4aa45c44718443f9970c4f28
Jack Nagel <jacknagel@gmail.com>
2012-04-28 17:05:14 -0500

    net6: fix audit warning
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/net6.rb
+++ Library/Formula/net6.rb
@@ -4,7 +4,7 @@ class Net6 < Formula
   url 'http://releases.0x539.de/net6/net6-1.3.12.tar.gz'
   homepage 'http://gobby.0x539.de'
   md5 '506776416d8aea2b9ea13a81f9145383'
-  
+
   depends_on 'pkg-config' => :build
   depends_on 'gnutls'
   depends_on 'libsigc++'
