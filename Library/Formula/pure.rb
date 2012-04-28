1455a59a5c32f630c77cf6045a9ec2619304d3af
Jack Nagel <jacknagel@gmail.com>
2012-04-28 18:14:41 -0500

    pure: fix docs install
    Fixes #11889.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pure.rb
+++ Library/Formula/pure.rb
@@ -1,11 +1,15 @@
 require 'formula'
 
+class PureDocs < Formula
+  url 'http://pure-lang.googlecode.com/files/pure-docs-0.53.tar.gz'
+  sha1 'c0ad274d344d982e65b15b63daf3a267c50ab186'
+end
+
 class Pure < Formula
   homepage 'http://code.google.com/p/pure-lang/'
   url 'http://pure-lang.googlecode.com/files/pure-0.53.tar.gz'
   sha1 '67f1394c06d885b79fc824283f0f7551a6fb2641'
 
-  depends_on 'wget'
   depends_on 'llvm'
   depends_on 'gmp'
   depends_on 'readline'
@@ -19,6 +23,7 @@ class Pure < Formula
     system "make"
     system "make check"
     system "make install"
-    system "make install-docs"
+
+    PureDocs.new.brew { system "make", "prefix=#{prefix}", "install" }
   end
 end
