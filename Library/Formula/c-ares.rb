a327e22251abf326dfa901afcdcbc8bff7da64e8
nibbles 2bits <nibbles@speakeasy.net>
2012-04-28 14:05:50 -0700

    c-ares 1.8.0
    Upgrade c-ares to version 1.8.0.
    Add `--disable-dependency-tracking` and `--disable-debug`
    
    Closes #11930.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/c-ares.rb
+++ Library/Formula/c-ares.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class CAres < Formula
-  url 'http://c-ares.haxx.se/download/c-ares-1.7.5.tar.gz'
   homepage 'http://c-ares.haxx.se/'
-  md5 '800875fc23cd8e1924d8af9172ed33e7'
+  url 'http://c-ares.haxx.se/download/c-ares-1.8.0.tar.gz'
+  sha1 'be886ef6a4238b237d865342477df751cab9757d'
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}",
+                          '--disable-dependency-tracking',
+                          '--disable-debug'
     system "make install"
   end
 end
