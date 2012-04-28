44b01219eff906428780034b14dc0aefbd7f601f
Jack Nagel <jacknagel@gmail.com>
2012-04-28 17:16:35 -0500

    dc3dd: move patch inline
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/dc3dd.rb
+++ Library/Formula/dc3dd.rb
@@ -5,13 +5,12 @@ class Dc3dd < Formula
   url 'http://downloads.sourceforge.net/project/dc3dd/dc3dd/7.1.0/dc3dd-7.1.614.tar.gz'
   md5 'b6c4ec16e7f539b17224d7f334f8396e'
 
-  # remove explicit dependency on automake 1.10.1
-  def patches
-    'https://raw.github.com/gist/1159050/611c91779ba90edc77b8489e006c96a8e0078462/dc3dd-automake.patch'
-  end
+  # Remove explicit dependency on automake 1.10.1
+  def patches; DATA; end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--infodir=#{info}"
     system "make"
@@ -20,3 +19,18 @@ class Dc3dd < Formula
     prefix.install %w[README Options_Reference.txt Sample_Commands.txt]
   end
 end
+
+__END__
+diff --git a/configure.ac b/configure.ac
+index 5d1870a..b32639c 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -32,7 +32,7 @@ AC_CONFIG_SRCDIR(src/dc3dd.c)
+ AC_CONFIG_AUX_DIR(build-aux)
+ AC_CONFIG_HEADERS([lib/config.h:lib/config.hin])
+ 
+-AM_INIT_AUTOMAKE([1.10.1])
++AM_INIT_AUTOMAKE
+ 
+ AC_PROG_CC_STDC
+ AM_PROG_CC_C_O
