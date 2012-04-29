a529047c5ea7d9b41b455c3437e033bbbee40d8b
Christian Höltje <docwhat@gerf.org>
2012-04-29 03:03:19 -0400

    task: install completion into site-functions
    `_task` needs to be installed into `share/zsh/site-functions`
    not `share/zsh/functions`.
    
    Files in `share/zsh/functions` must not change unless zsh is
    upgraded and zsh is restarted.  `site-functions` can change during a zsh
    process's lifetime.
    
    Closes #11939.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -13,6 +13,15 @@ class Task < Formula
     system "cmake #{std_cmake_parameters}"
     system "make install"
     (etc+'bash_completion.d').install 'scripts/bash/task_completion.sh'
-    (share+'zsh/functions').install   'scripts/zsh/_task'
+    (share+'zsh/site-functions').install   'scripts/zsh/_task'
+  end
+
+  def caveats; <<-EOS.undent
+    Bash completion has been installed to:
+      #{etc}/bash_completion.d
+
+    zsh completion has been installed to:
+      #{HOMEBREW_PREFIX}/share/zsh/site-functions
+    EOS
   end
 end
