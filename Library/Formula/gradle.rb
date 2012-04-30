9f5954135fde76f80fcb16069ebe1c8dae0a693f
Ealden Esto E. Escanan <ealden@gmail.com>
2012-04-30 14:37:32 +0800

    Gradle 1.0-RC-3
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gradle < Formula
   homepage 'http://www.gradle.org/'
-  url 'http://services.gradle.org/distributions/gradle-1.0-rc-2-bin.zip'
-  md5 '60c5255b0bdc73ae88a1e7ffb9034ecb'
-  version '1.0-rc-2'
+  url 'http://services.gradle.org/distributions/gradle-1.0-rc-3-bin.zip'
+  md5 '1a32e467930c05b3dca56a9543634511'
+  version '1.0-rc-3'
 
   def install
     rm_f Dir["bin/*.bat"]
