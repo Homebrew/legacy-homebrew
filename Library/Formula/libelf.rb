55e02291c196f118d3216396488570ea69dc071f
nibbles 2bits <nibbles@speakeasy.net>
2012-05-01 01:50:06 -0700

    libelf: fix for clang, separate make step
    Libelf fails with clang-3.1 (build 318) unless the `make` step is
    separate from `make install`.  Add that command, adjust configure
    line to not go past column 80, adjust the `homepage` to use the
    English url, deploy non-interpolated strings.
    
    Fixes #11943

--- Library/Formula/libelf.rb
+++ Library/Formula/libelf.rb
@@ -1,12 +1,15 @@
 require 'formula'
 
 class Libelf < Formula
+  homepage 'http://www.mr511.de/software/english.html'
   url 'http://www.mr511.de/software/libelf-0.8.13.tar.gz'
-  homepage 'http://www.mr511.de/software/'
   md5 '4136d7b4c04df68b686570afa26988ac'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system './configure', "--prefix=#{prefix}",
+                          '--disable-debug',
+                          '--disable-dependency-tracking'
+    system 'make'  # Fails with clang-3.1.318 unless separate make steps.
+    system 'make install'
   end
 end
