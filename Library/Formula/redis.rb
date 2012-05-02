8fba0e4f485d9e1ef209004ee0edb2f34dab130a
Chad Catlett <chad@catlett.info>
2012-05-02 06:16:24 -0500

    redis 2.4.13
    Closes #12005.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/redis.rb
+++ Library/Formula/redis.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Redis < Formula
   homepage 'http://redis.io/'
-  url 'http://redis.googlecode.com/files/redis-2.4.12.tar.gz'
-  md5 '9187084a4986f4422d2d6a3fffed70f4'
+  url 'http://redis.googlecode.com/files/redis-2.4.13.tar.gz'
+  sha256 'b6e057b6b5cd5946f8b46b2427401400686ed4f6c4af446eb1f0ec170d3f87be'
 
   head 'https://github.com/antirez/redis.git', :branch => 'unstable'
 
