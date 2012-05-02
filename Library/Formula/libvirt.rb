81db8088ad8f1da826f006378ad611d789ea20a5
Justin Clift <jclift@redhat.com>
2012-05-02 14:44:35 +1000

    Updated formula: libvirt
    Changed to using http downloads, so upstream probject can better
    track download numbers.

--- Library/Formula/libvirt.rb
+++ Library/Formula/libvirt.rb
@@ -3,13 +3,13 @@ require 'formula'
 class Libvirt < Formula
   homepage 'http://www.libvirt.org'
   version "0.9.11.3"
-  url 'ftp://libvirt.org/libvirt/libvirt-0.9.11.3.tar.gz'
+  url 'http://libvirt.org/sources/stable_updates/libvirt-0.9.11.3.tar.gz'
   sha256 'aa73b329d2f6eb200991b9dc378d4636c15cd2f95ca224995d01b45257584fa2'
 
   # Latest (roughly) monthly release.
   devel do
     version "0.9.11"
-    url 'ftp://libvirt.org/libvirt/libvirt-0.9.11.tar.gz'
+    url 'http://libvirt.org/sources/libvirt-0.9.11.tar.gz'
     sha256 'ce98fe435f83e109623a021b1f714fe806c3ab556d0780ce959cf75c98766062'
   end
 
