20782c6fe757eb5ef395da00710400e12e6b6cc3
Sandeep Pathivada <pathivas@tcd.ie>
2012-05-02 14:26:51 +0100

    moved depends_on below md5 and added new lines as requested.

--- Library/Formula/byobu.rb
+++ Library/Formula/byobu.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Byobu < Formula
-  depends_on 'coreutils'
   url 'http://launchpad.net/byobu/trunk/5.17/+download/byobu_5.17.orig.tar.gz'
   homepage 'http://launchpad.net/byobu'
   md5 '50551374a67dbf77349b0b453044ed6c'
 
+  depends_on 'coreutils'
+  
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
