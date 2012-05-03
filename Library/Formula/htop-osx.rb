c9a157ec485c880f09d31078e8dcdf9fbca2b39b
Mike McQuaid <mike@mikemcquaid.com>
2012-05-03 10:49:45 +0800

    htop-osx: add setuid caveats
    From discussion in #11727.

--- Library/Formula/htop-osx.rb
+++ Library/Formula/htop-osx.rb
@@ -12,4 +12,15 @@ class HtopOsx < Formula
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install", "DEFAULT_INCLUDES='-iquote .'"
   end
+
+  def caveats; <<-EOS.undent
+    htop-osx requires root privileges to correctly display all running processes.
+    You can either run the program via `sudo` or set the setuid bit:
+
+      sudo chown root:wheel #{bin}/htop
+      sudo chmod u+s #{bin}/htop
+
+    You should be certain that you trust any software you granting root privileges.
+    EOS
+  end
 end
