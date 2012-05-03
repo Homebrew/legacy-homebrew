c10d33486b785b578a6594ac7a14204129879972
LI Daobing <lidaobing@gmail.com>
2012-05-03 21:20:39 +0800

    new Formula: cconv
    Simplified-Traditional Chinese translate tool. With character and word
    translation support.

---
+++ Library/Formula/cconv.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Cconv < Formula
+  homepage 'http://code.google.com/p/cconv/'
+  url 'http://cconv.googlecode.com/files/cconv-0.6.2.tar.gz'
+  md5 'f61de90861951d86f3d08e75603cd286'
+
+  def patches
+    # fix link with iconv: http://code.google.com/p/cconv/issues/detail?id=18
+    "https://raw.github.com/gist/2585471/5adce03cc4245bd598828422f3f1279c39dfa281/gistfile1.txt"
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+    rm "#{include}/unicode.h"
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test cconv`.
+    system "false"
+  end
+end
