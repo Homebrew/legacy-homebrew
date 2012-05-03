22819d5946a53595aca4f42e083a8e97121b62cd
Charlie Sharpsteen <source@sharpsteen.net>
2012-05-03 15:45:31 -0700

    ENV.rb: Always return integers from make_jobs
    When referencing `Hardware.processor_count`, `ENV.make_jobs` will return an
    integer. If referencing the environment variable `HOMEBREW_MAKE_JOBS`, it
    returned a string.
    
    Now, the function always returns an integer.
    
    Fixes #12033.

--- Library/Formula/shiboken.rb
+++ Library/Formula/shiboken.rb
@@ -2,16 +2,19 @@ require 'formula'
 
 class Shiboken < Formula
   homepage 'http://www.pyside.org/docs/shiboken'
-  url 'http://pyside.org/files/shiboken-1.1.0.tar.bz2'
-  md5 '9c9d696c8c426fb5abf28a6bd3759558'
+  url 'http://www.pyside.org/files/shiboken-latest.tar.bz2'
+  md5 'fa451b6c4f3e06cce283a84550a96fd2'
 
   depends_on 'cmake' => :build
-  depends_on 'generatorrunner'
+  depends_on 'qt'
 
   def install
     # Building the tests also runs them. Not building and running tests cuts
     # install time in half.
-    system "cmake #{std_cmake_parameters} -DBUILD_TESTS=OFF ."
-    system "make install"
+    mkdir 'build'
+    chdir 'build' do
+      system "cmake .. #{std_cmake_parameters} -DBUILD_TESTS=OFF"
+      system "make install"
+    end
   end
 end
