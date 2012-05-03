10feb017e7bafc96739dd75ac6c97304eab0ec5d
Misty De Meo <mistydemeo@gmail.com>
2012-05-03 09:52:29 -0500

    media-info 0.7.57

--- Library/Formula/media-info.rb
+++ Library/Formula/media-info.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MediaInfo < Formula
   homepage 'http://mediainfo.sourceforge.net'
-  url 'http://downloads.sourceforge.net/mediainfo/MediaInfo_CLI_0.7.56_GNU_FromSource.tar.bz2'
-  md5 '0776e81f0a6f6f580fa636b8f3a70d4d'
+  url 'http://downloads.sourceforge.net/mediainfo/MediaInfo_CLI_0.7.57_GNU_FromSource.tar.bz2'
+  md5 '8dbe5bf7eed774690ca3cf904e6ed9a7'
 
   depends_on 'pkg-config' => :build
 
