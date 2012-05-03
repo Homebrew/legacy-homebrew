8cb637685b78d82505c9282754a5d7cd92cb7a29
nibbles 2bits <nibbles@speakeasy.net>
2012-05-03 16:51:45 -0700

    cairo 1.12.2 & remove fails_with :llvm
    Upgrade cairo to 1.12.2 using the `.xz` tarball.  It saves 20MB.
    Add the build time dep on `xz`.
    Remove the `fails_with :llvm` build 2336.
    Modify the `--enable-xcb` command to fit the new configure args.
    
    Saving 20 MiB of download is worth the new dep on xz.
    Cario builds without error using clang, llvm, and gcc from both
    XCode-4.3.2 and XCode-4.0.2.  This was tested by building pango
    and running `brew test pango` that was designed to exercise cairo.
    Also their configure command has the xcb flag set to auto.  So
    this commit adjusts the args so that xcb is off for Leopard and
    on for everyone else like the previous formula.

--- Library/Formula/cairo.rb
+++ Library/Formula/cairo.rb
@@ -2,19 +2,15 @@ require 'formula'
 
 class Cairo < Formula
   homepage 'http://cairographics.org/'
-  url 'http://www.cairographics.org/releases/cairo-1.10.2.tar.gz'
-  sha1 'ccce5ae03f99c505db97c286a0c9a90a926d3c6e'
+  url 'http://www.cairographics.org/releases/cairo-1.12.2.tar.xz'
+  sha1 'bc2ee50690575f16dab33af42a2e6cdc6451e3f9'
 
   depends_on 'pkg-config' => :build
+  depends_on 'xz' => :build
   depends_on 'pixman'
 
   keg_only :provided_by_osx,
             "The Cairo provided by Leopard is too old for newer software to link against."
-
-  fails_with :llvm do
-    build 2336
-    cause "Throws an 'lto could not merge' error during build."
-  end
 
   def install
     args = %W[
@@ -22,7 +18,7 @@ class Cairo < Formula
       --prefix=#{prefix}
       --with-x
     ]
-    args << '--enable-xcb' unless MacOS.leopard?
+    args << '--enable-xcb=no' if MacOS.leopard?
 
     system "./configure", *args
     system "make install"
