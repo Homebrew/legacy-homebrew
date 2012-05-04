1845d5768baa08d3eaa698ddd66cce8e328b689c
Adam Malcontenti-Wilson <adman.com@gmail.com>
2012-05-05 01:08:48 +1000

    Update cairo to 1.12.0

--- Library/Formula/cairo.rb
+++ Library/Formula/cairo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Cairo < Formula
   homepage 'http://cairographics.org/'
-  url 'http://www.cairographics.org/releases/cairo-1.10.2.tar.gz'
-  sha1 'ccce5ae03f99c505db97c286a0c9a90a926d3c6e'
+  url 'http://www.cairographics.org/releases/cairo-1.12.0.tar.gz'
+  sha1 '63e0d1372a7919956b6d959709dfdf35d3cecc02'
 
   depends_on 'pkg-config' => :build
   depends_on 'pixman'
