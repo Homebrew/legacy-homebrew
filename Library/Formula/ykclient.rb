df0853d314d5ee67d90d4cd425bfca99366af048
Robert Lenhardt <lenhardt.robert@googlemail.com>
2012-05-04 21:54:24 +0200

    ykclient 2.7
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ykclient.rb
+++ Library/Formula/ykclient.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Ykclient < Formula
-  url 'http://yubico-c-client.googlecode.com/files/ykclient-2.3.tar.gz'
+  url 'http://yubico-c-client.googlecode.com/files/ykclient-2.7.tar.gz'
   homepage 'http://code.google.com/p/yubico-c-client/'
-  md5 '7de70fb19e1a1857689afaec74def7fb'
+  md5 'ffaf492498d8d57e87fbbe355d438a91'
 
   def install
     system "./configure", "--prefix=#{prefix}"
