c5e464911eeca1902eb0cb936d615f1c4aea80ef
Adam Vandenberg <flangy@gmail.com>
2012-05-05 12:34:44 -0700

    tiff2png: remove inreplace

--- Library/Formula/tiff2png.rb
+++ Library/Formula/tiff2png.rb
@@ -1,31 +1,26 @@
 require 'formula'
 
 class Tiff2png < Formula
-  url 'ftp://ftp.simplesystems.org/pub/libpng/png/applications/tiff2png/tiff2png-0.91.tar.gz'
   homepage 'http://www.libpng.org/pub/png/apps/tiff2png.html'
+  url 'ftp://ftp.simplesystems.org/pub/libpng/png/applications/tiff2png/tiff2png-0.91.tar.gz'
   md5 'b5db7add863c5cf469197aa327c0b202'
 
   depends_on 'libtiff'
   depends_on 'jpeg'
 
   # libpng 1.5 no longer #includes zlib.h
-  def patches
-    DATA
-  end
+  def patches; DATA; end
 
   def install
-    inreplace 'Makefile.unx' do |s|
-      s.remove_make_var! 'DEBUGFLAGS'
-      s.change_make_var! 'CC', ENV.cc
-      s.change_make_var! 'LIBTIFF', HOMEBREW_PREFIX+"lib"
-      s.change_make_var! 'TIFFINC', HOMEBREW_PREFIX+"include"
-      s.change_make_var! 'LIBJPEG', HOMEBREW_PREFIX+"lib"
-      s.change_make_var! 'LIBPNG', '/usr/X11/lib'
-      s.change_make_var! 'PNGINC', '/usr/X11/include'
-      s.change_make_var! 'ZLIB', '/usr/lib'
-    end
-
-    system 'make -f Makefile.unx'
+    system "make", "-f", "Makefile.unx", "CC=#{ENV.cc}",
+                                         "OPTIMFLAGS=#{ENV.cflags}",
+                                         "LIBTIFF=#{HOMEBREW_PREFIX}/lib",
+                                         "TIFFINC=#{HOMEBREW_PREFIX}/include",
+                                         "LIBJPEG=#{HOMEBREW_PREFIX}/lib",
+                                         "LIBPNG=/usr/X11/lib",
+                                         "PNGINC=/usr/X11/include",
+                                         "ZLIB=/usr/lib",
+                                         "DEBUGFLAGS="
     bin.install 'tiff2png'
   end
 end
