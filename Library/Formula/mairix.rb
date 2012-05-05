41414ee392e22e5c0cb5494cb20750ac1b6bae15
Jack Nagel <jacknagel@gmail.com>
2012-05-05 17:24:13 -0500

    mairix: use HTTPS for GitHub head
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/mairix.rb
+++ Library/Formula/mairix.rb
@@ -3,8 +3,9 @@ require 'formula'
 class Mairix < Formula
   homepage 'http://www.rpcurnow.force9.co.uk/mairix/'
   url 'http://downloads.sourceforge.net/project/mairix/mairix/0.23/mairix-0.23.tar.gz'
-  head 'git://github.com/rc0/mairix.git', :using => :git
   md5 '732cf94e05c779ff1ce4a9e9eb19ab44'
+
+  head 'https://github.com/rc0/mairix.git'
 
   def install
     ENV.j1
