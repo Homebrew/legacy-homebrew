6e323165aaaf67d01c2a68a83c9d4c5ffbdd7e40
Jack Nagel <jacknagel@gmail.com>
2012-05-06 00:55:09 -0500

    maxima: use full path in test
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/maxima.rb
+++ Library/Formula/maxima.rb
@@ -28,7 +28,7 @@ class Maxima < Formula
   end
 
   def test
-    system "maxima --batch-string='run_testsuite(); quit();'"
+    system "#{bin}/maxima --batch-string='run_testsuite(); quit();'"
   end
 end
 
