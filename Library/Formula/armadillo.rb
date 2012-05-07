542dd4d18123102eaa551dfa83975d6df47df483
nibbles 2bits <nibbles@speakeasy.net>
2012-05-07 01:44:44 -0700

    armadillo 3.0.3
    Upgrade armadillo to version 3.0.3.  Compiles with clang and llvm
    from XCode-4.3.2.

--- Library/Formula/armadillo.rb
+++ Library/Formula/armadillo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Armadillo < Formula
   homepage 'http://arma.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/arma/armadillo-3.0.2.tar.gz'
-  md5 '2605d9fda2b8113e4d2afbeb104f953d'
+  url 'http://downloads.sourceforge.net/project/arma/armadillo-3.0.3.tar.gz'
+  sha1 '44e5d48798c6b3ce81c2e51e8d34c332b3d0af99'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
