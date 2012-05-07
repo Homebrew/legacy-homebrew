908217de8aacfdd300a97611e15f997cdf34b04b
Charlie Sharpsteen <source@sharpsteen.net>
2012-05-07 10:49:48 -0700

    libspatialite: Use non-amalgamated version
    The amalgamated version of SpatiaLite, which includes its own copy of SQLite,
    is depreciated and will be discontinued in the near future:
    
      https://groups.google.com/d/msg/spatialite-users/ZQcEx30p1W8/mi5Gvzlht7wJ

--- Library/Formula/libspatialite.rb
+++ Library/Formula/libspatialite.rb
@@ -6,8 +6,8 @@ end
 
 class Libspatialite < Formula
   homepage 'https://www.gaia-gis.it/fossil/libspatialite/index'
-  url 'http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-amalgamation-3.0.1.tar.gz'
-  md5 'df7f0f714c2de1dc2791ddef6e8eaba5'
+  url 'http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-3.0.1.tar.gz'
+  md5 '450d1a0d9da1bd9f770b7db3f2509f69'
 
   def options
     [['--without-freexl', 'Build without support for reading Excel files']]
@@ -15,6 +15,10 @@ class Libspatialite < Formula
 
   depends_on 'proj'
   depends_on 'geos'
+  # Needs SQLite > 3.7.3 which rules out system SQLite on Snow Leopard and
+  # below. Also needs dynamic extension support which rules out system SQLite
+  # on Lion. Finally, RTree index support is required as well.
+  depends_on 'sqlite'
 
   depends_on 'freexl' unless without_freexl?
 
@@ -22,10 +26,13 @@ class Libspatialite < Formula
     # O2 and O3 leads to corrupt/invalid rtree indexes
     # http://groups.google.com/group/spatialite-users/browse_thread/thread/8e1cfa79f2d02a00#
     ENV.Os
+    # Ensure Homebrew's libsqlite is found before the system version.
+    ENV.append 'LDFLAGS', "-L#{HOMEBREW_PREFIX}/lib"
+
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --target=macosx
+      --with-sysroot=#{HOMEBREW_PREFIX}
     ]
     args << '--enable-freexl=no' if without_freexl?
 
