eb44add6c78908038d2f210f0b1d7f8d1dae4b9b
Charlie Sharpsteen <source@sharpsteen.net>
2012-05-07 11:01:27 -0700

    Spatialite-tools: Update to 3.1.0a

--- Library/Formula/spatialite-tools.rb
+++ Library/Formula/spatialite-tools.rb
@@ -2,18 +2,20 @@ require 'formula'
 
 class SpatialiteTools < Formula
   homepage 'https://www.gaia-gis.it/fossil/spatialite-tools/index'
-  url 'http://www.gaia-gis.it/gaia-sins/spatialite-tools-sources/spatialite-tools-3.0.0-stable.tar.gz'
-  md5 'b54f94eb5297c1cff1820c2a35752a9c'
+  url 'http://www.gaia-gis.it/gaia-sins/spatialite-tools-sources/spatialite-tools-3.1.0a.tar.gz'
+  md5 '241f0eb00da1b19c1088d53684c24214'
 
   depends_on 'pkg-config' => :build
+
   depends_on 'libspatialite'
+  depends_on 'readosm'
 
   def install
     # See: https://github.com/mxcl/homebrew/issues/3328
+    ENV.append 'LDFLAGS', "-L#{HOMEBREW_PREFIX}/lib"
     ENV.append 'LDFLAGS', '-liconv'
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--target=macosx"
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
