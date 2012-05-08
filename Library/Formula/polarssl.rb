cb2874e9b4e91f2851e9f4ddb942472f406beb5c
Squeeks <privacymyass@gmail.com>
2012-05-09 04:15:49 +1000

    New Formula: PolarSSL 1.1.3

---
+++ Library/Formula/polarssl.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Polarssl < Formula
+  homepage 'http://polarssl.org/'
+  url 'http://polarssl.org/code/releases/polarssl-1.1.3-gpl.tgz'
+  md5 'fdd367e3b5ab43ed2af8ffbdfaf0fb81'
+
+  depends_on 'cmake' => :build
+
+  def install
+    system "cmake . #{std_cmake_parameters}"
+    system "make"
+    system "make install"
+  end
+
+  def test
+    system "false"
+  end
+end
