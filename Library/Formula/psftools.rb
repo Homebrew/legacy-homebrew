bee8ac2713711fa39a7fa87b4a2f6505f472eafb
Jack Nagel <jacknagel@gmail.com>
2012-05-07 23:23:22 -0500

    psftools: use a subformula to download test file
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/psftools.rb
+++ Library/Formula/psftools.rb
@@ -1,5 +1,10 @@
 require 'formula'
 
+class Pc8x8Font < Formula
+  url 'http://www.zone38.net/font/pc8x8.zip'
+  sha1 '4dbd149bb97b5b4f3464fcbda2387a479b09acb7'
+end
+
 class Psftools < Formula
   url 'http://www.seasip.info/Unix/PSF/psftools-1.0.7.tar.gz'
   homepage 'http://www.seasip.demon.co.uk/Unix/PSF/'
@@ -14,10 +19,10 @@ class Psftools < Formula
   def test
     # The zip file has a fon in it, use fon2fnts to extrat to fnt
     mktemp do
-      curl "http://www.zone38.net/font/pc8x8.zip", "-O"
-      system "/usr/bin/unzip pc8x8.zip"
-      system "#{bin}/fon2fnts pc8x8.fon"
-      raise unless File.exist? "PC8X8_9.fnt"
+      Pc8x8Font.new.brew do
+        system "#{bin}/fon2fnts pc8x8.fon"
+        raise unless File.exist? "PC8X8_9.fnt"
+      end
     end
   end
 end
