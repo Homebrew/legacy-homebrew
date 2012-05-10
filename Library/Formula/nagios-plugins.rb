f6d72f8483e8a66ab13197511692d0b0db23ed4d
Björn Albers <bjoernalbers@googlemail.com>
2012-05-10 12:50:53 +0200

    Update path of nagios-plugins
    Change path from sbin/nagios-plugins to sbin in order to avoid hacks in
    related formulae.
    
    Closes #12181.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/nagios-plugins.rb
+++ Library/Formula/nagios-plugins.rb
@@ -5,31 +5,19 @@ class NagiosPlugins < Formula
   homepage 'http://nagiosplugins.org/'
   md5 '56abd6ade8aa860b38c4ca4a6ac5ab0d'
 
-  def nagios_sbin; sbin+'nagios-plugins'; end
-
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--libexecdir=#{nagios_sbin}"
+                          "--libexecdir=#{sbin}"
     system "make install"
     system "make install-root"
   end
 
   def caveats
     <<-EOS.undent
-    All plugins are accessible under
-
-      #{nagios_sbin}
-
-    and get symlinked to
-
-      #{HOMEBREW_PREFIX}/sbin/nagios-plugins
-
-    so that they don't appear in your search path but still can be found by
-    nagios across version updates.
-
-    Please feel free to update your PATH if you want to use them standalone!
+    All plugins have been installed in:
+      #{HOMEBREW_PREFIX}/sbin
     EOS
   end
 end
