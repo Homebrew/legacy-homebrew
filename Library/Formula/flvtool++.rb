2112ed65aeb9c57be7ad06c1872dbf87c1c4619c
Peter Jaros <peter.a.jaros@gmail.com>
2012-05-10 11:29:33 -0400

    Point flvtool++ to Elbandi's GitHub mirror.
    Facebook no longer provides flvtool++ on its developer site.

--- Library/Formula/flvtool++.rb
+++ Library/Formula/flvtool++.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Flvtoolxx < Formula
-  url 'http://mirror.facebook.net/facebook/flvtool++/flvtool++-1.2.1.tar.gz'
-  homepage 'http://developers.facebook.com/opensource/'
-  md5 'a8c4c578b4c6741a94ca6eb197a01fe1'
+  url 'https://github.com/Elbandi/flvtool-pp/tarball/upstream/1.2.1'
+  version '1.2.1'
+  homepage 'https://github.com/Elbandi/flvtool-pp'
+  md5 'e942a396cad1a557af444450e8281efa'
 
   depends_on 'scons' => :build
   depends_on 'boost'
