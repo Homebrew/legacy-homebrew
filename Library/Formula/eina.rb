d4185d557b549bdf5c25e9a2a665fcc2eb3f2543
Adam Vandenberg <flangy@gmail.com>
2012-05-10 07:10:12 -0700

    Remove redundant :using => :svn

--- Library/Formula/eina.rb
+++ Library/Formula/eina.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Eina < Formula
-  url 'http://download.enlightenment.org/releases/eina-1.1.0.tar.gz'
   homepage 'http://trac.enlightenment.org/e/wiki/Eina'
+  url 'http://download.enlightenment.org/releases/eina-1.1.0.tar.gz'
   md5 'fedb3814427827c1bb777edea3c86298'
-  head 'http://svn.enlightenment.org/svn/e/trunk/eina/', :using => :svn
+
+  head 'http://svn.enlightenment.org/svn/e/trunk/eina/'
 
   depends_on 'pkg-config' => :build
 
