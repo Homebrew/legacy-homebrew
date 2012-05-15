4f6aa55c464baf0f4e646b9658bb7dca3b43261b
nibbles 2bits <nibbles@speakeasy.net>
2012-05-15 08:45:15 -0700

    rasqal 0.9.29
    Upgrade raptor to version 0.9.29 and adjust the location of the
    html docs from gtk-doc to the usual location in share.  For this
    app, the standard helper path `doc` results in `doc/rasqal/rasqal`.
    So `share/doc` is used to eliminate the duplicate `rasqal` path.
    
    Note: The homepage for rasqal mentions an upcoming API change:
    'RDQL support will be removed in the next release (0.9.30)'
    
    Closes #12259.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/rasqal.rb
+++ Library/Formula/rasqal.rb
@@ -1,14 +1,16 @@
 require 'formula'
 
 class Rasqal < Formula
-  url 'http://download.librdf.org/source/rasqal-0.9.28.tar.gz'
   homepage 'http://librdf.org/rasqal/'
-  md5 'a3662b8d9efef9d8ef0a3c182450fba2'
+  url 'http://download.librdf.org/source/rasqal-0.9.29.tar.gz'
+  sha1 'a005556bf62d44a8fa1cc2faf931f78ed4516852'
 
   depends_on 'raptor'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system './configure', "--prefix=#{prefix}",
+                          "--with-html-dir=#{share}/doc",
+                          '--disable-dependency-tracking'
     system "make install"
   end
 end
