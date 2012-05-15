ea5a6733a082a8fa07cb4b1674e422098679a0b7
Keisuke Kawahara <kyohsuke@conafie.jp>
2012-05-15 16:44:49 +0900

    kyoto-tycoon 0.9.55
    Closes #12255.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/kyoto-tycoon.rb
+++ Library/Formula/kyoto-tycoon.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class KyotoTycoon < Formula
-  url 'http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.52.tar.gz'
   homepage 'http://fallabs.com/kyototycoon/'
-  sha1 '448b96e9b0f262c26574ab655ea8ad84f64ffb48'
+  url 'http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.55.tar.gz'
+  sha1 'bd301b3d355788ae89e09a092b1d469f8a69f411'
 
   depends_on 'lua' unless ARGV.include? "--no-lua"
   depends_on 'kyoto-cabinet'
