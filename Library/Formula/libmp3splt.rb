64d863d0af4404bb330a6506acc3a2a18771a738
Jack Nagel <jacknagel@gmail.com>
2012-05-17 18:44:30 -0500

    libmp3splt 0.7.2
     - Remove upstreamed patch
     - Add dep on libtool for 10.5/10.6
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libmp3splt.rb
+++ Library/Formula/libmp3splt.rb
@@ -1,46 +1,30 @@
 require 'formula'
 
 class Libmp3splt < Formula
-  url 'http://downloads.sourceforge.net/project/mp3splt/libmp3splt/0.7.1/libmp3splt-0.7.1.tar.gz'
   homepage 'http://mp3splt.sourceforge.net'
-  md5 '62025951f483334f14f1b9be58162094'
+  url 'http://downloads.sourceforge.net/project/mp3splt/libmp3splt/0.7.2/libmp3splt-0.7.2.tar.gz'
+  md5 '848817d1d980729aec99bbc62caddd76'
+
+  unless MacOS.lion?
+    depends_on 'automake' => :build
+    depends_on 'libtool' => :build
+  end
 
   depends_on 'pkg-config' => :build
-  depends_on 'automake' => :build unless MacOS.lion?
   depends_on 'gettext'
   depends_on 'pcre'
   depends_on 'libid3tag'
   depends_on 'mad'
   depends_on 'libvorbis'
 
-  # autogen.sh calls `libtoolize`, while OS X installs under the name `glibtoolize`
-  # reported upstream at https://sourceforge.net/tracker/?func=detail&aid=3497957&group_id=55130&atid=476061
-  def patches
-    DATA
-  end
-
   def install
-    if !MacOS.lion?
+    unless MacOS.lion?
       system "./autogen.sh"
       system "autoconf"
     end
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
   end
 end
-
-__END__
-diff --git a/autogen.sh b/autogen.sh
-index 68962f2..916b868 100755
---- a/autogen.sh
-+++ b/autogen.sh
-@@ -53,7 +53,7 @@ echo -n "1/6 Running autopoint... " \
- && echo -n "3/6 Running autoheader... " \
- && autoheader && echo "done" \
- && echo -n "4/6 Running libtoolize... " \
--&& libtoolize -c --force && echo "done" \
-+&& glibtoolize -c --force && echo "done" \
- && echo -n "5/6 Running autoconf... " \
- && autoconf && echo "done" \
- && echo -n "6/6 Running automake... " \
