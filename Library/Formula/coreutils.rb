948a36e352c01ea502107a7e77ce0bfc3d98bb36
Jack Nagel <jacknagel@gmail.com>
2012-05-16 19:10:14 -0500

    coreutils 8.17
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/coreutils.rb
+++ Library/Formula/coreutils.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Coreutils < Formula
   homepage 'http://www.gnu.org/software/coreutils'
-  url 'http://ftpmirror.gnu.org/coreutils/coreutils-8.16.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/coreutils/coreutils-8.16.tar.xz'
-  sha256 '2a458fead15d9336f46bb4304cc3eaa6ed9407b9130e7ee2ec533909881d2067'
+  url 'http://ftpmirror.gnu.org/coreutils/coreutils-8.17.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/coreutils/coreutils-8.17.tar.xz'
+  sha256 '4e075a0d238072a5bd079046e1f024dc5e0d9133d43a39c73d0b86b0d1e2c5e5'
 
   depends_on 'xz' => :build
 
