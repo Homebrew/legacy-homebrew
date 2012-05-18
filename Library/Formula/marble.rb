38e2dc6751a375085ec41276f9d9bd6936b1d9ce
Joan <aseques@gmail.com>
2012-05-19 01:34:06 +0200

    Add extra patch to preserve cmake files needed by digikam

--- Library/Formula/marble.rb
+++ Library/Formula/marble.rb
@@ -34,3 +34,20 @@ __END__
        #SET (lib_dir ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/lib)
        SET (data_dir   ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/resources/data)
        SET (plugin_dir   ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/resources/plugins)
+#Patch to set the appropiate path for cmake modules (required for digikam)
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index ebf41b9..86ff26a 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -161,9 +161,11 @@ else(WIN32)
+       #SET (lib_dir ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/lib)
+       SET (data_dir   ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/resources/data)
+       SET (plugin_dir   ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/resources/plugins)
++      set(CMAKE_MODULES_INSTALL_PATH ${CMAKE_ROOT}/Modules)
+     else (QTONLY) # KDE4 on Mac...
+       set(data_dir ${DATA_INSTALL_DIR}/marble/data)
+       set(plugin_dir ${PLUGIN_INSTALL_DIR}/plugins/marble)
++      set(CMAKE_MODULES_INSTALL_PATH ${DATA_INSTALL_DIR}/cmake/modules)
+     endif (QTONLY)
+   else(APPLE) # Linux / bsd etc...
+     if (QTONLY)
