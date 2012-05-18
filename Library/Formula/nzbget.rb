f504656c54ec6ef27468f6a5b68aa4e561ceac23
Simon <bob@yagoo.com>
2012-05-18 12:55:52 +0100

    Updated to 0.8.0

--- Library/Formula/nzbget.rb
+++ Library/Formula/nzbget.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Nzbget < Formula
-  url 'http://downloads.sourceforge.net/project/nzbget/nzbget-stable/0.7.0/nzbget-0.7.0.tar.gz'
+  url 'http://downloads.sourceforge.net/project/nzbget/nzbget-stable/0.8.0/nzbget-0.8.0.tar.gz'
   homepage 'http://sourceforge.net/projects/nzbget/'
-  md5 '27971846aba75f5e312d80dce7edbc5d'
+  md5 'c47f464fe988deeb4fc5381506c1fbe3'
   head 'https://nzbget.svn.sourceforge.net/svnroot/nzbget/trunk', :using => :svn
 
   # Also depends on libxml2 but the one in OS X is fine
