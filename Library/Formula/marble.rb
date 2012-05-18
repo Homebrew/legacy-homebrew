0949a143611afd506d2c9a6f6f45c2295af7dfef
Joan <aseques@gmail.com>
2012-05-19 00:37:07 +0200

    Set the patch inline

--- Library/Formula/marble.rb
+++ Library/Formula/marble.rb
@@ -9,8 +9,7 @@ class Marble < Formula
   depends_on 'qt'
 
   def patches
-    #Patch to disable architecture forcing (https://git.reviewboard.kde.org/r/104942/)
-    "https://gist.github.com/raw/2662935/56cfb27ebc9bee75deabf85bfcaf484112ceef33/gistfile1.txt"
+    DATA
   end
 
   def install
@@ -18,3 +17,20 @@ class Marble < Formula
     system "make install" # if this fails, try separate make/make install steps
   end
 end
+
+__END__
+#Patch to disable architecture forcing (https://git.reviewboard.kde.org/r/104942/
+--- a/CMakeLists.txt	2012-05-12 15:05:14.000000000 +0200
++++ b/CMakeLists.txt	2012-05-12 15:05:21.000000000 +0200
+@@ -155,8 +155,9 @@
+       SET( MACOSX_BUNDLE_VERSION 0.3.0 )
+       SET( MACOSX_BUNDLE_LONG_VERSION_STRING Version 0.3.0 )
+       SET( MACOSX_BUNDLE_BUNDLE_NAME Marble)
+-      #SET( CMAKE_OSX_ARCHITECTURES ppc;i386 ) #Comment out if not universal binary
+-      SET( CMAKE_OSX_ARCHITECTURES i386 ) #Comment out if universal binary
++      if (MACOSX_UNIVERSAL)
++        SET( CMAKE_OSX_ARCHITECTURES ppc;i386 ) #Comment out if not universal binary
++      endif(MACOSX_UNIVERSAL)
+       #SET (lib_dir ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/lib)
+       SET (data_dir   ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/resources/data)
+       SET (plugin_dir   ${CMAKE_INSTALL_PREFIX}/Marble.app/Contents/MacOS/resources/plugins)
