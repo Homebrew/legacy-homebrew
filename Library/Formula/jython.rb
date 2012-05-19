c7692ab6fe1f10aaadd316bf99cb6df170fccee9
Timothée Peignier <timothee.peignier@tryphon.org>
2012-05-19 14:08:21 +0200

    jython --devel 2.7a1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/jython.rb
+++ Library/Formula/jython.rb
@@ -2,19 +2,16 @@ require 'formula'
 
 class Jython < Formula
   homepage 'http://www.jython.org'
-  url "http://downloads.sourceforge.net/project/jython/jython/2.5.2/jython_installer-2.5.2.jar",
-    :using => :nounzip
+  url "http://downloads.sourceforge.net/project/jython/jython/2.5.2/jython_installer-2.5.2.jar"
   sha1 'd4534a691edf40aa1d51723dfe3e22db1e39b432'
 
   devel do
-    url "http://downloads.sourceforge.net/project/jython/jython-dev/2.5.3b1/jython_installer-2.5.3b1.jar",
-      :using => :nounzip
-    version '2.5.3b1'
-    sha1 'bcfc024a93289b2f99bf000fb7666a48fe3d32da'
+    url "http://downloads.sourceforge.net/project/jython/jython-dev/2.7.0a1/jython_installer-2.7a1.jar"
+    sha1 'c2fc5232da2c9de0cca357bf4fa5dc312bbf1294'
   end
 
   def install
     system "java", "-jar", "jython_installer-#{version}.jar", "-s", "-d", libexec
-    bin.install_symlink libexec+'bin/jython'
+    bin.install_symlink libexec/'bin/jython'
   end
 end
