5724d799750f9e7049aabdb4e918b79ac2e7a578
Thomas Johansson <prencher@prencher.dk>
2012-05-19 13:02:30 +0200

    Go App Engine SDK 1.6.5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/go-app-engine-64.rb
+++ Library/Formula/go-app-engine-64.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class GoAppEngine64 < Formula
-  url 'http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_amd64-1.6.4.zip'
+  url 'http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_amd64-1.6.5.zip'
   homepage 'http://code.google.com/appengine/docs/go/overview.html'
-  sha1 '5de646a1d400c191a7227f58098de090926727ea'
+  sha1 '5329cad4f32d106dec5ffdd61debdd81d8d22268'
 
   def install
     cd '..'
     share.install 'google_appengine' => name
     bin.mkpath
-    %w[appcfg.py bulkload_client.py bulkloader.py dev_appserver.py remote_api_shell.py].each do |fn|
+    %w[appcfg.py bulkload_client.py bulkloader.py dev_appserver.py download_appstats.py].each do |fn|
       ln_s share+name+fn, bin
     end
   end
