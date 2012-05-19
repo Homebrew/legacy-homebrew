0125f126a35f196df7d2a12eab496850fd45abac
Adam Vandenberg <flangy@gmail.com>
2012-05-19 10:23:09 -0700

    Hadoop 1.0.3

--- Library/Formula/hadoop.rb
+++ Library/Formula/hadoop.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hadoop < Formula
   homepage 'http://hadoop.apache.org/common/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=hadoop/core/hadoop-1.0.1/hadoop-1.0.1.tar.gz'
-  md5 'e627d9b688c4de03cba8313bd0bba148'
+  url 'http://www.apache.org/dyn/closer.cgi?path=hadoop/core/hadoop-1.0.3/hadoop-1.0.3.tar.gz'
+  md5 'f42d05e5c7bb43f85119546b80296435'
 
   def shim_script target
     <<-EOS.undent
