2390610c1a8f8e6e0a39f1cd1e4eb1fa6e37f1fd
Jack Nagel <jacknagel@gmail.com>
2012-05-21 16:16:53 -0500

    Revert "giflib 4.2.0"
    This reverts commit bad8e1f17ade0d2679de996090e53945fb1c3023.
    
    Due to API changes, 4.2.0 breaks a bunch of stuff; stick with 4.1.6 for
    the time being.

--- Library/Formula/giflib.rb
+++ Library/Formula/giflib.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Giflib < Formula
   homepage 'http://giflib.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/giflib/giflib-4.x/giflib-4.2.0.tar.bz2'
-  sha1 'bc942711f75de7d8539f79be34d69c0d53c381c1'
+  url 'http://downloads.sourceforge.net/project/giflib/giflib-4.x/giflib-4.1.6/giflib-4.1.6.tar.bz2'
+  md5 '7125644155ae6ad33dbc9fc15a14735f'
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
 end
