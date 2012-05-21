4f1643d7df89bc738dad0fcc0b7546b8e96b2967
Frank Denis <github@pureftpd.org>
2012-05-20 23:19:34 -0700

    Update dnscrypt-proxy to the latest version (0.8 -> 0.9.4).

--- Library/Formula/dnscrypt-proxy.rb
+++ Library/Formula/dnscrypt-proxy.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class DnscryptProxy < Formula
-  url 'https://github.com/downloads/opendns/dnscrypt-proxy/dnscrypt-proxy-0.8.tar.gz'
+  url 'https://github.com/downloads/opendns/dnscrypt-proxy/dnscrypt-proxy-0.9.4.tar.gz'
   head 'https://github.com/opendns/dnscrypt-proxy.git', :branch => 'master'
   homepage 'http://www.opendns.com/technology/dnscrypt'
-  md5 '9c573da27aa897f27a564214c92bcddb'
+  md5 'c997fd1322ac5aef8325a718da7d73e2'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
