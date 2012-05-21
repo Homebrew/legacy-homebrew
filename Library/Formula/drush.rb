eba66892cd7082366f32b7c1ab52f945523f6351
Mark Shropshire <marks@knowclassic.com>
2012-05-21 07:28:03 -0400

    drush 5.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/drush.rb
+++ Library/Formula/drush.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Drush < Formula
   homepage 'http://drupal.org/project/drush'
-  url 'http://ftp.drupal.org/files/projects/drush-7.x-5.1.tar.gz'
-  md5 '329c3b9a658bd718e4212c2ac3a930cf'
+  url 'http://ftp.drupal.org/files/projects/drush-7.x-5.2.tar.gz'
+  md5 'eb18dc1640bcf7a9dc776dec5cd0d163'
   head 'git://git.drupal.org/project/drush.git'
 
   def install
