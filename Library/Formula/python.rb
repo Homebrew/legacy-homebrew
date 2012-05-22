3fbcb0b2df3074592ca76c113ba0baf0b311defe
Adam Vandenberg <flangy@gmail.com>
2012-05-21 20:19:21 -0700

    distribute 0.6.27
    subformula of Python, Python 3, PyPy

--- Library/Formula/python.rb
+++ Library/Formula/python.rb
@@ -9,8 +9,8 @@ def as_framework?
 end
 
 class Distribute < Formula
-  url 'http://pypi.python.org/packages/source/d/distribute/distribute-0.6.26.tar.gz'
-  md5 '841f4262a70107f85260362f5def8206'
+  url 'http://pypi.python.org/packages/source/d/distribute/distribute-0.6.27.tar.gz'
+  md5 'ecd75ea629fee6d59d26f88c39b2d291'
 end
 
 class Python < Formula
