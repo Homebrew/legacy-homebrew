49745d25b1cae0d60c281fe0c593ea6034d563c5
Richard Brown <r.brown@lawson-his.co.uk>
2012-05-22 13:30:41 +0100

    uudeview: Fixes compiler warnings when -wreturn-type is on e.g.
    inews.c:195:4: error: non-void function 'append_signature' should return
    a value [-Wreturn-type]

--- Library/Formula/uudeview.rb
+++ Library/Formula/uudeview.rb
@@ -5,6 +5,11 @@ class Uudeview < Formula
   url 'http://www.fpx.de/fp/Software/UUDeview/download/uudeview-0.5.20.tar.gz'
   md5 '0161abaec3658095044601eae82bbc5b'
 
+  def patches
+    #Fixes compiler warnings when -wreturn-type is on
+    "https://raw.github.com/gist/2768732/7091ce65e2c99b5972eedd250745d96d63a00477/fix-return-type-warnings.diff"
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
