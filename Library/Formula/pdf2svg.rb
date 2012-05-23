bf0a6535aaed5cdc2a38452e9b84d9e6f8487f0c
Charlie Sharpsteen <source@sharpsteen.net>
2012-05-23 14:30:02 -0700

    pdf2svg: Depend on a Requirement for Poppler-Glib
    Instead of doing `pkg-config` checks, now use a Requirement that references the
    installation Tab for Poppler.

--- Library/Formula/pdf2svg.rb
+++ Library/Formula/pdf2svg.rb
@@ -1,9 +1,21 @@
 require 'formula'
 
-def have_poppler_glib?
-  # Not using Homebrew's system wrapper because we actually want to see the
-  # exit code of the command.
-  Kernel.system "#{HOMEBREW_PREFIX}/bin/pkg-config", "poppler-glib", "--exists"
+class PopplerGlib < Requirement
+  def satisfied?
+    poppler = Tab.for_formula 'poppler'
+    poppler.installed_with? '--with-glib'
+  end
+
+  def fatal?
+    true
+  end
+
+  def message; <<-EOS.undent
+    pdf2svg requires the Poppler-Glib bindings but Poppler was not installed
+    with support for glib. Please reinstall Poppler using the `--with-glib`
+    option.
+    EOS
+  end
 end
 
 class Pdf2svg < Formula
@@ -14,6 +26,7 @@ class Pdf2svg < Formula
   depends_on "pkg-config" => :build
 
   depends_on "poppler"
+  depends_on PopplerGlib.new
   depends_on "gtk+"
   depends_on "cairo" # Poppler-glib needs a newer cairo than provided by OS X 10.6.x
                      # and pdf2svg needs it to be on PKG_CONFIG_PATH during the build
@@ -24,18 +37,8 @@ class Pdf2svg < Formula
   end
 
   def install
-    unless have_poppler_glib?
-      onoe <<-EOS.undent
-        pkg-config could not find poppler-glib! Please try re-installing
-        Poppler with support for the Glib backend:
-
-            brew uninstall poppler
-            brew install --with-glib poppler
-      EOS
-      exit 1
-    end
-
     ENV.x11
+
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
