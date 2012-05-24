16bc601ce45c15506d44804d20a113542073c4b5
jiayong.ou <jiayong.ou@memonews.com>
2012-05-24 11:37:16 +0200

    remove generated comment

--- Library/Formula/git-publish.rb
+++ Library/Formula/git-publish.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class GitPublish < Formula
   homepage 'http://code.google.com/p/git-publish/'
   url 'http://git-publish.googlecode.com/files/git-publish-1.0.tar.bz2'
