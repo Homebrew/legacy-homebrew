a7ad462a24cd9219a8b8b217b0e0ee5a1fa1055d
Sam Granieri <sam@samgranieri.com>
2012-05-24 19:58:36 -0500

    @jacknagel thanks for the formatting tips

--- Library/Formula/casperjs.rb
+++ Library/Formula/casperjs.rb
@@ -1,9 +1,12 @@
 require 'formula'
+
 class Casperjs < Formula
   homepage 'http://casperjs.org'
-  head 'git://github.com/n1k0/casperjs.git'
   url 'https://github.com/n1k0/casperjs/tarball/0.6.8'
   md5 'cb72d1c5f33528e7020678e06d80352c'
+
+  head 'git://github.com/n1k0/casperjs.git'
+
   depends_on 'phantomjs'
 
   def script; <<-EOS.undent
@@ -19,8 +22,8 @@ class Casperjs < Formula
     inreplace 'bin/bootstrap.js' do |s|
       s.gsub! /'bin', 'usage.txt'/, "'libexec', 'usage.txt'"
     end
-    prefix.install ['docs', 'modules', 'samples','package.json', 'tests', 'CHANGELOG.md', 'LICENSE.md']
-    libexec.install ['bin/casperjs','bin/bootstrap.js', 'bin/usage.txt']
+    prefix.install 'docs', 'modules', 'samples','package.json', 'tests', 'CHANGELOG.md', 'LICENSE.md'
+    libexec.install 'bin/casperjs','bin/bootstrap.js', 'bin/usage.txt'
     (bin+'casperjs').write script
   end
 
