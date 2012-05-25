aa5022bf9c751becf78f57f47d4942baadbbd17b
Jack Nagel <jacknagel@gmail.com>
2012-05-24 20:43:45 -0500

    dnscrypt-proxy: remove broken HEAD
    The GitHub checkout needs an autotools bootstrap. Patches welcome to
    enable it, but removing it until then.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/dnscrypt-proxy.rb
+++ Library/Formula/dnscrypt-proxy.rb
@@ -1,9 +1,8 @@
 require 'formula'
 
 class DnscryptProxy < Formula
-  url 'https://github.com/downloads/opendns/dnscrypt-proxy/dnscrypt-proxy-0.9.4.tar.gz'
-  head 'https://github.com/opendns/dnscrypt-proxy.git', :branch => 'master'
   homepage 'http://www.opendns.com/technology/dnscrypt'
+  url 'https://github.com/downloads/opendns/dnscrypt-proxy/dnscrypt-proxy-0.9.4.tar.gz'
   md5 'c997fd1322ac5aef8325a718da7d73e2'
 
   def install
