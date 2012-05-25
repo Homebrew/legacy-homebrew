c80513395ffdb865c906d097d490765961547ac0
Enderson Maia <endersonmaia@gmail.com>
2012-05-25 10:51:05 -0300

    Adding support for compilation against unixODBC.

--- Library/Formula/freetds.rb
+++ Library/Formula/freetds.rb
@@ -5,8 +5,25 @@ class Freetds < Formula
   homepage 'http://www.freetds.org/'
   md5 'b14db5823980a32f0643d1a84d3ec3ad'
 
+  depends_on "unixodbc" if ARGV.include? "--with-unixodbc"
+
+  def options
+    [['--with-unixodbc', "Compile against unixODBC."]]
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--with-tdsver=7.1", "--mandir=#{man}"
+    args = ["--prefix=#{prefix}",
+            "--with-tdsver=7.0",
+            "--enable-msdblib",
+            "--mandir=#{man}"
+            ]
+    
+    if ARGV.include? "--with-unixodbc"
+      unixodbc_prefix = Formula.factory('unixodbc').prefix
+      args << "--with-unixodbc=#{unixodbc_prefix}"
+    end
+    
+    system "./configure", *args
     system 'make'
     ENV.j1 # Or fails to install on multi-core machines
     system 'make install'
