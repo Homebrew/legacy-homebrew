792a9d021dad6f82eb87e3c08c68056947d90ca6
Justin Hileman <justin@justinhileman.info>
2012-05-25 09:05:55 -0700

    coda-cli 1.0.4
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/coda-cli.rb
+++ Library/Formula/coda-cli.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class CodaCli < Formula
   homepage 'http://justinhileman.info/coda-cli/'
-  url 'https://github.com/bobthecow/coda-cli/zipball/v1.0.3'
-  md5 '706e49cad02472e70b4c361035a852f8'
+  url 'https://github.com/bobthecow/coda-cli/zipball/v1.0.4'
+  md5 'a86ae7e9f2389406d1a8889ecf5c1566'
 
   def install
     bin.install 'coda'
