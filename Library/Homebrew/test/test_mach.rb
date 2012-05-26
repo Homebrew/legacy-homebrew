15cceb6326977421976733e31f01f2d689c95395
Jack Nagel <jacknagel@gmail.com>
2012-05-25 23:44:11 -0500

    Pathname: add Mach-O module
    The MachO module contains methods for learning about Mach-O binaries,
    and can be used where one might normally shell out to file(1).
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ Library/Homebrew/test/test_mach.rb
@@ -0,0 +1,140 @@
+require 'testing_env'
+
+require 'extend/ARGV' # needs to be after test/unit to avoid conflict with OptionsParser
+ARGV.extend(HomebrewArgvExtension)
+
+class MachOPathnameTests < Test::Unit::TestCase
+  def test_fat_dylib
+    pn = Pathname.new("#{TEST_FOLDER}/mach/fat.dylib")
+    assert pn.universal?
+    assert !pn.i386?
+    assert !pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert pn.dylib?
+    assert !pn.mach_o_executable?
+    assert !pn.text_executable?
+    assert pn.arch == :universal
+    assert_match /Mach-O (64-bit )?dynamically linked shared library/,
+      `/usr/bin/file -h '#{pn}'`.chomp
+  end
+
+  def test_i386_dylib
+    pn = Pathname.new("#{TEST_FOLDER}/mach/i386.dylib")
+    assert !pn.universal?
+    assert pn.i386?
+    assert !pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert pn.dylib?
+    assert !pn.mach_o_executable?
+    assert !pn.text_executable?
+    assert_match /Mach-O (64-bit )?dynamically linked shared library/,
+      `/usr/bin/file -h '#{pn}'`.chomp
+  end
+
+  def test_x86_64_dylib
+    pn = Pathname.new("#{TEST_FOLDER}/mach/x86_64.dylib")
+    assert !pn.universal?
+    assert !pn.i386?
+    assert pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert pn.dylib?
+    assert !pn.mach_o_executable?
+    assert !pn.text_executable?
+    assert_match /Mach-O (64-bit )?dynamically linked shared library/,
+      `/usr/bin/file -h '#{pn}'`.chomp
+  end
+
+  def test_mach_o_executable
+    pn = Pathname.new("#{TEST_FOLDER}/mach/a.out")
+    assert pn.universal?
+    assert !pn.i386?
+    assert !pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert !pn.dylib?
+    assert pn.mach_o_executable?
+    assert !pn.text_executable?
+    assert_match /Mach-O (64-bit )?executable/,
+      `/usr/bin/file -h '#{pn}'`.chomp
+  end
+
+  def test_non_mach_o
+    pn = Pathname.new("#{TEST_FOLDER}/tarballs/testball-0.1.tbz")
+    assert !pn.universal?
+    assert !pn.i386?
+    assert !pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert !pn.dylib?
+    assert !pn.mach_o_executable?
+    assert !pn.text_executable?
+    assert pn.arch == :dunno
+    assert_no_match /Mach-O (64-bit )?dynamically linked shared library/,
+      `/usr/bin/file -h '#{pn}'`.chomp
+    assert_no_match /Mach-O [^ ]* ?executable/,
+      `/usr/bin/file -h '#{pn}'`.chomp
+  end
+end
+
+class TextExecutableTests < Test::Unit::TestCase
+  TMPDIR = HOMEBREW_PREFIX/'tmp'
+
+  def setup
+    FileUtils.mkdir_p TMPDIR
+  end
+
+  def test_simple_shebang
+    pn = Pathname.new('foo')
+    pn.write '#!/bin/sh'
+    assert !pn.universal?
+    assert !pn.i386?
+    assert !pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert !pn.dylib?
+    assert !pn.mach_o_executable?
+    assert pn.text_executable?
+    assert_equal [], pn.archs
+    assert pn.arch == :dunno
+    assert_match /text executable/, `/usr/bin/file -h '#{pn}'`.chomp
+  end
+
+  def test_shebang_with_options
+    pn = Pathname.new('bar')
+    pn.write '#! /usr/bin/perl -w'
+    assert !pn.universal?
+    assert !pn.i386?
+    assert !pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert !pn.dylib?
+    assert !pn.mach_o_executable?
+    assert pn.text_executable?
+    assert_equal [], pn.archs
+    assert pn.arch == :dunno
+    assert_match /text executable/, `/usr/bin/file -h '#{pn}'`.chomp
+  end
+
+  def test_malformed_shebang
+    pn = Pathname.new('baz')
+    pn.write '#! '
+    assert !pn.universal?
+    assert !pn.i386?
+    assert !pn.x86_64?
+    assert !pn.ppc7400?
+    assert !pn.ppc64?
+    assert !pn.dylib?
+    assert !pn.mach_o_executable?
+    assert !pn.text_executable?
+    assert_equal [], pn.archs
+    assert pn.arch == :dunno
+    assert_match /text executable/, `/usr/bin/file -h '#{pn}'`.chomp
+  end
+
+  def teardown
+    TMPDIR.rmtree
+  end
+end
