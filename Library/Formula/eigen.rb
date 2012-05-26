fedf54a9d9d416dbc76405da03af169b6df2288b
Adam Vandenberg <flangy@gmail.com>
2012-05-26 09:46:23 -0700

    eigen: fix build type

--- Library/Formula/eigen.rb
+++ Library/Formula/eigen.rb
@@ -10,7 +10,11 @@ class Eigen < Formula
   def install
     ENV.fortran
     mkdir 'eigen-build' do
-      system "cmake", "..", "-Dpkg_config_libdir=#{lib}", *std_cmake_args
+      args = std_cmake_args
+      args.delete '-DCMAKE_BUILD_TYPE=None'
+      args << '-DCMAKE_BUILD_TYPE=Release'
+
+      system "cmake", "..", "-Dpkg_config_libdir=#{lib}", *args
       system "make install"
     end
   end
