3ca7f40f24dbb52576bfc2f548c0aa3b53e06328
Dossy Shiobara <dossy@panoptic.com>
2012-05-27 13:29:53 -0400

    Allow building lame as a universal binary.
    Useful for Audacity and Mixxx, which both want libmp3lame but
    need i386 binaries.

--- Library/Formula/lame.rb
+++ Library/Formula/lame.rb
@@ -5,7 +5,15 @@ class Lame < Formula
   url 'http://downloads.sourceforge.net/sourceforge/lame/lame-3.99.5.tar.gz'
   md5 '84835b313d4a8b68f5349816d33e07ce'
 
+  def options
+    [
+      ["--universal", "Build for both 32 & 64 bit Intel."]
+    ]
+  end
+
   def install
+    ENV.universal_binary if ARGV.build_universal?
+
     system "./configure", "--disable-debug", "--prefix=#{prefix}", "--enable-nasm"
     system "make install"
   end
