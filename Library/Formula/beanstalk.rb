ce13f51c05930304ccda2da562702aa28c2b91ca
Adam Vandenberg <flangy@gmail.com>
2012-05-28 14:03:43 -0700

    beanstalk 1.6

--- Library/Formula/beanstalk.rb
+++ Library/Formula/beanstalk.rb
@@ -2,15 +2,8 @@ require 'formula'
 
 class Beanstalk < Formula
   homepage 'http://kr.github.com/beanstalkd/'
-  url 'https://github.com/downloads/kr/beanstalkd/beanstalkd-1.5.tar.gz'
-  md5 'd75a0a93e6b80b57fea61136f6da57eb'
-
-  # fix cpu use on freebsd and darwin
-  # bug report: https://github.com/kr/beanstalkd/issues/96
-  # Will be in next release
-  def patches
-    "http://github.com/kr/beanstalkd/commit/80da772efeeaabb12893f52a93da74ca9e69206d.patch"
-  end
+  url 'https://github.com/downloads/kr/beanstalkd/beanstalkd-1.6.tar.gz'
+  md5 '844cb7790e2a7ccfc08629dbc2b73178'
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
