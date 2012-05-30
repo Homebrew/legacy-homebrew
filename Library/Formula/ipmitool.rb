7d6910d3c83c81c0ef31eeb9e26fa96b188f7690
Dossy Shiobara <dossy@panoptic.com>
2012-05-30 17:11:48 -0400

    Apply "delloem" patches from CentOS 6.2 SRPM.
    Incorporate some of the Dell-specific patches for ipmitool
    from the CentOS 6.2 SRPM.
    
    Unfortunately, this does not (yet) include all the functionality
    that is available direct from Dell in their modified ipmitool
    RPM.

--- Library/Formula/ipmitool.rb
+++ Library/Formula/ipmitool.rb
@@ -5,6 +5,15 @@ class Ipmitool < Formula
   homepage 'http://ipmitool.sourceforge.net/'
   md5 '1d0da20add7388d64c549f95538b6858'
 
+  def options
+    [["--with-delloem", "Include support for \"delloem\" subcommand"]]
+  end
+
+  def patches
+    # optionally incorporate delloem patches
+    { :p1 => "https://gist.github.com/raw/2838711/dc127edd2ba34e1e801bf2a07afa2e3c81a9817f/ipmitool-1.8.11-delloem.patch" } if ARGV.include? '--with-delloem'
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}",
                           "--disable-debug", "--disable-dependency-tracking"
