892830b02cb8d7db9aba59d9e8990e056a2e17b9
Eli Bishop <ebishop@atlassian.com>
2012-05-30 12:03:44 -0700

    atlassian-plugin-sdk 3.10.4

--- Library/Formula/atlassian-plugin-sdk.rb
+++ Library/Formula/atlassian-plugin-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AtlassianPluginSdk < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
-  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/3.10.3/atlassian-plugin-sdk-3.10.3.tar.gz'
-  md5 '33212e59590dd9aa85fe3618bd3192eb'
+  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/3.10.4/atlassian-plugin-sdk-3.10.4.tar.gz'
+  md5 '77ff1f8f531519bcbed8391f4c394fba'
 
   def install
     # Remove windows files
