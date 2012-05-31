07a4dc88fd699b5efa90da381b869a73001bed2e
aaronlake <aaron.lake@gmail.com>
2012-05-31 09:54:02 -0300

    Update mobile-shell to 1.2.1.

--- Library/Formula/mobile-shell.rb
+++ Library/Formula/mobile-shell.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MobileShell < Formula
   homepage 'http://mosh.mit.edu/'
-  url 'https://github.com/downloads/keithw/mosh/mosh-1.2.tar.gz'
-  md5 '2822792d362cc1539278ca3d3afb279a'
+  url 'https://github.com/downloads/keithw/mosh/mosh-1.2.1.tar.gz'
+  md5 'ec6d224be5b25e521fccf2f6b763b6da'
 
   head 'https://github.com/keithw/mosh.git'
 
