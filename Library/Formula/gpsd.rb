39ba6174e62de47cf3cd3145a1244ce2fa11fde4
Pascal Deschenes <pdeschen@gmail.com>
2012-05-30 21:40:56 -0400

    gpsd: Fixes following pull request comments
    * Remove heading comments/notes
    * Remove version
    * Remove test

--- Library/Formula/gpsd.rb
+++ Library/Formula/gpsd.rb
@@ -1,27 +1,14 @@
 require 'formula'
 
-# Notes:
-#   man pages won't be installed since:
-#     "Neither xsltproc nor xmlto found, documentation cannot be built."
-#
-#   Some build flags had to be changed in order to have a successfull install
-#
-#   Python binding as been disabled
-#
 class Gpsd < Formula
   url 'http://git.savannah.gnu.org/cgit/gpsd.git/snapshot/release-3.5.tar.gz'
   homepage 'https://savannah.nongnu.org/projects/gpsd'
   md5 'a3f88b41c36ee2f2bce6d9ca961688fa'
-  version '3.5'
 
   depends_on 'scons' => :build
 
   def install
     system "scons", "chrpath=False", "python=False", "strip=False", "shared=False", "prefix=#{prefix}/"
     system "scons install"
-  end
-
-  def test
-    system "scons testregress"
   end
 end
