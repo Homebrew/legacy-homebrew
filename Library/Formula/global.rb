88de15a97c55a6f740d9de8f278b614047ec3256
Arne Jørgensen <arne@arnested.dk>
2012-05-31 20:45:28 +0200

    global: Rebuild PHP parser for GNU GLOBAL.
    GNU GLOBAL is distributed with a PHP parser generated with an older
    version of flex(1). That parser has some limitation (see
    http://comments.gmane.org/gmane.comp.gnu.global.bugs/1439).
    
    Added a rebuild of the PHP parser to the install target.

--- Library/Formula/global.rb
+++ Library/Formula/global.rb
@@ -2,11 +2,21 @@ require 'formula'
 
 class Global < Formula
   homepage 'http://www.gnu.org/software/global/'
-  url 'http://ftpmirror.gnu.org/global/global-6.2.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/global/global-6.2.tar.gz'
-  md5 '30f5c6c0f737a3475ec8786ae6e34648'
+  url 'http://ftpmirror.gnu.org/global/global-6.2.4.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/global/global-6.2.4.tar.gz'
+  md5 'c778d4e52a30706db7a02070cce1078c'
+
+  def options
+    [
+      ["--without-rebuilding-php-parser", "Don't rebuild PHP parser; use the parser distributed by GNU GLOBAL."],
+    ]
+  end
 
   def install
+    unless ARGV.include? "--without-rebuilding-php-parser"
+      # Rebuilding the PHP parser, see <http://comments.gmane.org/gmane.comp.gnu.global.bugs/1439>.
+      system "flex -o libparser/php.c libparser/php.l"
+    end
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
 
@@ -14,5 +24,29 @@ class Global < Formula
     cd share+'gtags' do
       rm %w[README COPYING LICENSE INSTALL ChangeLog AUTHORS]
     end
+  end
+
+  def caveats
+    s = <<-EOS.undent
+
+      GNU GLOBAL is distributed with a PHP parser generated with an
+      old version of flex(1). That parser has some limitation (see
+      http://comments.gmane.org/gmane.comp.gnu.global.bugs/1439).
+
+      Installing GNU GLOBAL with Homebrew will therefor rebuild the
+      PHP parser using Mac OS X' newer version of flex(1) which
+      generates a parser without the limit.
+
+      You can install GNU GLOBAL without rebuilding the PHP parser
+      (and instead use the parser distributed by GNU GLOBAL) by using
+      the option '--without-rebuilding-php-parser':
+
+        brew install global --without-rebuilding-php-parser
+
+      Use i.e. if you want to be sure whether a problem is caused by
+      the rebuild parser or some other thing in GNU GLOBAL.
+    EOS
+
+    return s
   end
 end
