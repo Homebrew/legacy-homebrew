75e457eb3979628d43f9b110c9983ab9b73e1dff
Fabio Utzig <fabio@alklever.com>
2012-06-01 10:18:16 -0300

    Fixed url/homepage ordering, commented skip_clean

--- Library/Formula/sigrok-cli.rb
+++ Library/Formula/sigrok-cli.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class SigrokCli < Formula
-  url 'http://downloads.sourceforge.net/project/sigrok/source/sigrok-cli/sigrok-cli-0.3.1.tar.gz'
   homepage 'http://sigrok.org'
+  url 'http://downloads.sourceforge.net/project/sigrok/source/sigrok-cli/sigrok-cli-0.3.1.tar.gz'
   md5 'eed06b1408a969b86b0f6e1aa29ae0cb'
 
   depends_on 'pkg-config' => :build
@@ -10,6 +10,7 @@ class SigrokCli < Formula
   depends_on 'libsigrok'
   depends_on 'libsigrokdecode'
 
+  # Stripping breaks dynamic linking
   skip_clean :all
 
   def install
