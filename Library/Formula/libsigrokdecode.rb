f14b5e021e57a02a4478755627f72f2de73b1d6d
Fabio Utzig <fabio@alklever.com>
2012-05-31 23:38:34 -0300

    Fix url/homepage ordering, removed skip_clean, check for python3 without depends

--- Library/Formula/libsigrokdecode.rb
+++ Library/Formula/libsigrokdecode.rb
@@ -1,15 +1,46 @@
 require 'formula'
 
+class NeedsPython3 < Requirement
+  def satisfied?
+    is_python_version_3? || python3_found?
+  end
+
+  def fatal?
+    true
+  end
+
+  def message; <<-EOS.undent
+    This library depends on Python version 3.
+    EOS
+  end
+
+private
+
+  # check if default python command is version 3
+  def is_python_version_3?
+    return true if `python -c 'import sys;print(sys.version[:1])'`.to_i == 3
+    return false
+  end
+
+  # check if python3 command is found in PATH
+  def python3_found?
+    ENV['PATH'].split(':').each do |path|
+      return true if File.executable? "#{path}/python3"
+    end
+    return false
+  end
+end
+
 class Libsigrokdecode < Formula
-  url 'http://downloads.sourceforge.net/project/sigrok/source/libsigrokdecode/libsigrokdecode-0.1.0.tar.gz'
   homepage 'http://sigrok.org'
+  url 'http://downloads.sourceforge.net/project/sigrok/source/libsigrokdecode/libsigrokdecode-0.1.0.tar.gz'
   md5 '9bc237972f6176ba9dcff057b4e85fd6'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
-  depends_on 'python3'
 
-  skip_clean :all
+  # libsigrokdecode needs python3!
+  depends_on NeedsPython3.new
 
   def install
     system "./configure", "--prefix=#{prefix}"
