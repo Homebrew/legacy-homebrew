0642b9ef47829bc3c2fe1cff50b56d43b2360e70
Alexander Barton <alex@barton.de>
2012-06-03 14:56:30 +0200

    ngIRCd: add launchd plist

--- Library/Formula/ngircd.rb
+++ Library/Formula/ngircd.rb
@@ -17,5 +17,27 @@ class Ngircd < Formula
                           "--with-tcp-wrappers",
                           "--enable-ipv6"
     system "make install"
+
+    system "make -C contrib/MacOSX de.barton.ngircd.plist"
+    inreplace "contrib/MacOSX/de.barton.ngircd.plist",
+      /\/Library\/Logs/, "#{HOMEBREW_PREFIX}/var/log"
+
+    prefix.install "contrib/MacOSX/de.barton.ngircd.plist"
+    (prefix+"de.barton.ngircd.plist").chmod 0644
+    false
+  end
+
+  def caveats; <<-EOCAVEATS
+# Start/Stop ngircd(8) Daemon
+
+If this is your first install, automatically load on login with:
+  sudo cp #{prefix}/de.barton.ngircd.plist /Library/LaunchDaemons/
+  sudo launchctl load -w /Library/LaunchDaemons/de.barton.ngircd.plist
+
+If this is an upgrade and you already have the #{plist_path.basename} loaded:
+  sudo launchctl unload -w /Library/LaunchDaemons/de.barton.ngircd.plist
+  sudo cp #{prefix}/de.barton.ngircd.plist /Library/LaunchDaemons/
+  sudo launchctl load -w /Library/LaunchDaemons/de.barton.ngircd.plist
+EOCAVEATS
   end
 end
