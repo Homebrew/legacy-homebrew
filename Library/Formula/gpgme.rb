b53d9816f6721c1ddc882fb391e3eeec66f584ba
Jack Nagel <jacknagel@gmail.com>
2012-06-03 22:39:53 -0500

    gpgme 1.3.2
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gpgme.rb
+++ Library/Formula/gpgme.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Gpgme < Formula
-  url 'ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-1.3.1.tar.bz2'
   homepage 'http://www.gnupg.org/gpgme.html'
-  sha1 '7d19a95a2239da13764dad7f97541be884ec5a37'
+  url 'ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-1.3.2.tar.bz2'
+  sha1 '5b5ebcc4dad46ced0e436a30f5542577536619c7'
 
   depends_on 'gnupg'
   depends_on 'libgpg-error'
