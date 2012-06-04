69d4980867e8139beba6aed1991e5b5eccaece8e
Andrew Lindesay <apl@lindesay.co.nz>
2012-06-05 11:51:02 +1200

    + changes to the simpledb; use sub-formula for AWS libraries + changes to the simpledb; remove comments

--- Library/Formula/simpledb.rb
+++ Library/Formula/simpledb.rb
@@ -1,14 +1,9 @@
-# [apl 8.may.2012]
-# This formula is to install a third-party tool that allows for command
-# line access to the Amazon Web Services Simple DB service.  The perl
-# script requires additional third-party perl libraries to function. In
-# order for the install process to be self-contained, those libraries
-# are downloaded and then unpacked into the same target directory as
-# the 'simpledb' script.  A proxy-script is generated to launch the
-# 'simpledb' script in such a way that it is able to reference the
-# third party perl libraries.
-
 require 'formula'
+
+class SimpledbAwsLibraries < Formula
+  url 'http://aws-libraries.s3.amazonaws.com/perl/AmazonSimpleDB/2009-04-15/AmazonSimpleDB-2009-04-15-perl-library.zip'
+  md5 "fcfff6ad8cd867df4dabe8b081bc1b0d"
+end
 
 class Simpledb < Formula
   homepage 'http://amazon-simpledb-cli.googlecode.com/'
@@ -18,9 +13,7 @@ class Simpledb < Formula
 
   def install
 
-    system "curl 'http://aws-libraries.s3.amazonaws.com/perl/AmazonSimpleDB/2009-04-15/AmazonSimpleDB-2009-04-15-perl-library.zip' > AmazonSimpleDB-2009-04-15-perl-library.zip"
-    system "unzip 'AmazonSimpleDB-2009-04-15-perl-library.zip'"
-    system "mv AmazonSimpleDB-2009-04-15-perl-library/src perl-lib"
+    SimpledbAwsLibraries.new.brew { mv 'src', buildpath/'perl-lib' }
     system "mv bin/simpledb simpledb-perl"
 
     open("simpledb", 'w') { |f|
@@ -33,8 +26,6 @@ class Simpledb < Formula
     prefix.install 'simpledb-perl'
     prefix.install 'perl-lib'
     bin.install 'simpledb'
-
-    ohai 'To get information about how to use simpledb, execute \'/usr/local/bin/simpledb --help\''
 
   end
 
