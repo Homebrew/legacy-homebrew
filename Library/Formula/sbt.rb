49e7c187c32077214a1f154ce9305ddd97cad773
David M. Lee <dlee@digium.com>
2012-06-04 09:43:37 -0500

    sbt 0.11.3-2
    Closes #12585.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Sbt < Formula
-  url "http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.11.3/sbt-launch.jar"
   homepage 'http://github.com/harrah/xsbt/'
-  version '0.11.3'
-  md5 '653cc42660766e3e046570ec1a94c0c7'
+  url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.11.3-2/sbt-launch.jar'
+  version '0.11.3-2'
+  md5 'b8d0d2cd7000f2de29a2cda65e7809e5'
 
   def install
     (bin+'sbt').write <<-EOS.undent
