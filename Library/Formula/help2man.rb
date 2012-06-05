6e03da68467d640e5f3935bc458856bf944af7f3
Jack Nagel <jacknagel@gmail.com>
2012-06-05 15:12:15 -0500

    help2man 1.40.10
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/help2man.rb
+++ Library/Formula/help2man.rb
@@ -2,16 +2,12 @@ require 'formula'
 
 class Help2man < Formula
   homepage 'http://www.gnu.org/software/help2man/'
-  url 'http://ftpmirror.gnu.org/help2man/help2man-1.40.9.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/help2man/help2man-1.40.9.tar.gz'
-  sha1 '3957023ee890ec391b539236f3f7c5b89b240e3f'
+  url 'http://ftpmirror.gnu.org/help2man/help2man-1.40.10.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/help2man/help2man-1.40.10.tar.gz'
+  sha256 'f7c85b9af84a87d7da912b62dadf426118841750ed1e4598787a54fddaf82b9c'
 
   def install
     system "./configure", "--prefix=#{prefix}"
-
-    # Skip making the "info" files.
-    system "make help2man man"
-    bin.install "help2man"
-    man1.install gzip("help2man.1")
+    system "make install"
   end
 end
