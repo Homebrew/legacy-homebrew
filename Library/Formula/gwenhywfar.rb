7302731725e94022ab4332792a61bd789f57fc61
Alexis Hildebrandt <afh@surryhill.net>
2012-06-06 02:56:30 +0200

    gwenywfar 4.3.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gwenhywfar.rb
+++ Library/Formula/gwenhywfar.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Gwenhywfar < Formula
-  url 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=66&file=01&dummy=gwenhywfar-4.3.2.tar.gz'
   homepage 'http://www.aqbanking.de/'
-  md5 '6fcd39f64e6357321cffeaca470fbe52'
+  url 'http://www.aquamaniac.de/sites/download/download.php?package=01&release=67&file=01&dummy=gwenhywfar-4.3.3.tar.gz'
+  sha1 'c2ba4c45f1eeb379db6c2ae09122c592893f3bd0'
+
   head 'http://devel.aqbanking.de/svn/gwenhywfar/trunk'
 
   depends_on 'pkg-config' => :build
