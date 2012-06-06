c7ef395d663f28ed66cdab60876650cda3069845
Lars Bilke <lars.bilke@ufz.de>
2012-06-06 10:46:19 +0200

    multimarkdown 3.6
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/multimarkdown.rb
+++ Library/Formula/multimarkdown.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
+# url uses git tag to download submodules.
 class Multimarkdown < Formula
   homepage 'http://fletcherpenney.net/multimarkdown/'
-  url 'https://github.com/fletcher/peg-multimarkdown/tarball/3.5'
-  md5 '3ba5725c96b1c12f5c2419be9753bf80'
+  url 'https://github.com/fletcher/peg-multimarkdown.git', :tag => '3.6'
+
   head 'https://github.com/fletcher/peg-multimarkdown.git', :branch => 'development'
 
   def install
