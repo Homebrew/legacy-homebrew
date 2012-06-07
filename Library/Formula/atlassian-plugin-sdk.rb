4d162f1bafaaf91ae26dc5be2744a4fd0fb2b735
Jonathan Doklovic <doklovic@atlassian.com>
2012-06-07 13:30:47 -0700

    atlassian-plugin-sdk 3.11
    Closes #12653.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/atlassian-plugin-sdk.rb
+++ Library/Formula/atlassian-plugin-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AtlassianPluginSdk < Formula
   homepage 'https://developer.atlassian.com/display/DOCS/Atlassian+Plugin+SDK+Documentation'
-  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/3.10.4/atlassian-plugin-sdk-3.10.4.tar.gz'
-  md5 '77ff1f8f531519bcbed8391f4c394fba'
+  url 'https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/atlassian-plugin-sdk/3.11/atlassian-plugin-sdk-3.11.tar.gz'
+  md5 '2899bd68f38f2522a73c6f82f821d541'
 
   def install
     # Remove windows files
