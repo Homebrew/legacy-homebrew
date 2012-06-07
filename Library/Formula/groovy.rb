b06fe17aab4b711b83f557ec38739dc1f2c0e2ea
akafred <github@akafred.com>
2012-06-07 13:04:07 +0200

    groovy: add --devel
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -5,14 +5,20 @@ class Groovy < Formula
   url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-1.8.6.zip'
   md5 'e62d2f9c2c4d528b8a0eb49cdfb389ae'
 
+  devel do
+    url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.0-rc-2.zip'
+    version '2.0.0-rc-2'
+    md5 '13a8277137561596a8ff8bbd15b842b2'
+  end
+
   def install
     # Don't need Windows files.
     # Why are icons in bin?
     rm_f Dir["bin/*.bat","bin/groovy.{icns,ico}"]
 
-    prefix.install %w{ LICENSE.txt NOTICE.txt }
-    libexec.install %w[bin conf lib embeddable]
-
+    prefix.install %w(LICENSE.txt NOTICE.txt)
+    libexec.install %w(bin conf lib embeddable)
+    lib.install "indy" if ARGV.build_devel?
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
