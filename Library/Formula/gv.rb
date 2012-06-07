9dfc31295ba9253ef73ae9cd86ee93591f3bc92a
nibbles 2bits <nibbles@speakeasy.net>
2012-06-07 00:55:54 -0700

    gv 3.7.3.90 + change dep XQuartz
    As reported in 10422, gv would not compile on Lion and possibly SL
    due to problems building Homebrew's `xaw3d`.  That library is a
    part of XQuartz now.
    
    * Upgrade `gv` to version 3.7.3.90 from `alpha.gnu.org` because
    this release is specifically fixed to support the new `libxawd`.
    * Note in the comments to switch back to `ftp.gnu.org` at v3.7.4.
    * Change the dep to a Requirement for XQuartz >= 2.7.2.
    
    Fixes #10422 and #12596

--- Library/Formula/gv.rb
+++ Library/Formula/gv.rb
@@ -1,14 +1,31 @@
 require 'formula'
 
+class XQuartz272Installed < Requirement
+  def satisfied?; MacOS.xquartz_version >= '2.7.2'; end
+  def fatal?; true; end
+  def message; <<-EOS.undent
+
+    Gv needs libxaw3d, which exists in XQuartz >= 2.7.2, but XQuartz was not
+    found on your system.  Because Apple suggests XQuartz for Mt. Lion, it is
+    safe to install, and it works well with Homebrew.  After installing XQuartz:
+      http://xquartz.macosforge.org/landing/
+    and logging out and back in, you will be able to install gv.
+
+    EOS
+  end
+end
+
 class Gv < Formula
-  url 'http://ftpmirror.gnu.org/gv/gv-3.7.3.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gv/gv-3.7.3.tar.gz'
   homepage 'http://www.gnu.org/s/gv/'
-  md5 '98ae3e9ce338b64ba5ab622389c5960e'
+  url 'ftp://alpha.gnu.org/gnu/gv/gv-3.7.3.90.tar.gz'
+  sha1 'd7820c770e595c93b5fe1ea50776ae11d0decfac'
+
+  # Note: Switch back to ftp://ftp.gnu.org/gnu/gv/ @ gv-3.7.4.
+  # This gv version from alpha was released to support libxaw3d >= 1.6.1
 
   depends_on 'ghostscript'
-  depends_on 'xaw3d'
   depends_on :x11
+  depends_on XQuartz272Installed.new
 
   skip_clean 'share/gv/safe-gs-workdir'
 
