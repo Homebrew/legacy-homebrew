916880b20477fe1fa6544fe91ea19c6fa3a5f72f
teenst <teens.twenties@gmail.com>
2012-06-08 03:02:30 +0900

    mecab: fix checksum
    Closes #12646.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/mecab.rb
+++ Library/Formula/mecab.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Mecab < Formula
   homepage 'http://mecab.sourceforge.net/'
   url 'http://mecab.googlecode.com/files/mecab-0.994.tar.gz'
-  sha1 '5fcad4b7ccc2b5f6f37dea9ef6f2ef602bb92d54'
+  sha1 '9d283f9d243b1a58a2845ff60797c11a88b1f926'
 
   def install
     system "./configure", "--disable-dependency-tracking",
