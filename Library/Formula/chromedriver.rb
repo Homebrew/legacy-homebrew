88af97ad895f893cd5ac05d2c9bdffae9ee30c4d
Tatsuro Baba <harakirisoul@gmail.com>
2012-06-07 13:48:38 +0900

    chromedriver 20.0.1133.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Chromedriver < Formula
   homepage 'http://code.google.com/p/chromedriver/'
-  url 'http://chromedriver.googlecode.com/files/chromedriver_mac_19.0.1068.0.zip'
-  sha1 '4a7448555dc61f900c5144d26afbfe99749433de'
+  url 'http://chromedriver.googlecode.com/files/chromedriver_mac_20.0.1133.0.zip'
+  sha1 '00013555f4383dee3308d180eb868ca27e865179'
 
   def install
     bin.install 'chromedriver'
