434e8eb2293caca90baa29bbe225059ada21998f
nibbles 2bits <nibbles@speakeasy.net>
2012-06-07 00:55:54 -0700

    gv 3.7.3.90 + change dep XQuartz
    As reported in 10422, gv would not compile on Lion and possibly SL
    due to problems building Homebrew's `xaw3d`.  That library is a
    part of XQuartz now.
    
    * Upgrade `gv` to version 3.7.3.90 from `alpha.gnu.org` because
    this release is specifically fixed to support the new `libxawd`.
    * Note in the comments to switch back to `ftp.gnu.org` at v3.7.4.
    * Change the dep to a Requirement for XQuartz >= 2.7.2.
    
    Fixes #10422 and #12596
    
    Closes #12637.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gv.rb
+++ Library/Formula/gv.rb
@@ -1,14 +1,15 @@
 require 'formula'
 
 class Gv < Formula
-  url 'http://ftpmirror.gnu.org/gv/gv-3.7.3.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gv/gv-3.7.3.tar.gz'
   homepage 'http://www.gnu.org/s/gv/'
-  md5 '98ae3e9ce338b64ba5ab622389c5960e'
+  url 'ftp://alpha.gnu.org/gnu/gv/gv-3.7.3.90.tar.gz'
+  sha1 'd7820c770e595c93b5fe1ea50776ae11d0decfac'
+
+  # Note: Switch back to ftp://ftp.gnu.org/gnu/gv/ @ gv-3.7.4.
+  # This gv version from alpha was released to support libxaw3d >= 1.6.1
 
   depends_on 'ghostscript'
-  depends_on 'xaw3d'
-  depends_on :x11
+  depends_on :x11 => '2.7.2'
 
   skip_clean 'share/gv/safe-gs-workdir'
 
