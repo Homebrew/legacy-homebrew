dd9a14cc485055eddb465fc2ae65651336913f03
nibbles 2bits <nibbles@speakeasy.net>
2012-06-08 13:11:03 -0700

    mplayer 1.1
    Upgrade mplayer to version 1.1.
    Add configure switches so we use our compiler, fixes clang error.
    Add build time dep on `xz`.
    Add `ENV.O1` to stop symbols being optimized out, fixes llvm error.
    Remove fails with llvm that was fixed by `ENV.O1`.
    Add `fails_with :clang` build 211 for a 32bit inline asm error.
    Remove the patch for Lion because it's fixed in this version.
    
    Mplayer runs well using all five compilers on 64bit Snow Leopard
    and Lion using XCode-4.3.2, 4.2, and 4.0.2, but it fails on 32bit
    SL due an inline asm error using clang.  Can't win them all.
    
    Fixes #12434 and #7827 -- two birds, one commit!
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -2,27 +2,24 @@ require 'formula'
 
 class Mplayer < Formula
   homepage 'http://www.mplayerhq.hu/'
-  url 'ftp://ftp.mplayerhq.hu/MPlayer/releases/MPlayer-1.0rc4.tar.bz2'
-  md5 '1699c94de39da9c4c5d34e8f58e418f0'
+  url 'http://www.mplayerhq.hu/MPlayer/releases/MPlayer-1.1.tar.xz'
+  sha1 '913a4bbeab7cbb515c2f43ad39bc83071b2efd75'
 
   head 'svn://svn.mplayerhq.hu/mplayer/trunk', :using => StrictSubversionDownloadStrategy
 
   depends_on 'pkg-config' => :build
   depends_on 'yasm' => :build
+  depends_on 'xz' => :build
 
-  fails_with :llvm do
-    cause "Missing symbols error while linking"
-  end
+  fails_with :clang do
+    build 211
+    cause 'Inline asm errors during compile on 32bit Snow Leopard.'
+  end unless MacOS.prefer_64_bit?
 
   def patches
-    # When building from SVN HEAD, configure prompts the user to pull FFmpeg
-    # from git.  Don't do that.
-    if ARGV.build_head?
-      DATA
-    elsif `uname -r` =~ /^11\./
-      # Lion requires the following diff (which is already fixed in svn trunk)
-      "https://raw.github.com/gist/1105164/704c64c97ab7ffcc3e5f69b1c5d4fb0850f572c1/vd_mpng.c.diff"
-    end
+    # When building SVN, configure prompts the user to pull FFmpeg from git.
+    # Don't do that.
+    DATA if ARGV.build_head?
   end
 
   def install
@@ -30,12 +27,18 @@ class Mplayer < Formula
     # * https://github.com/mxcl/homebrew/issues/622
     # * http://trac.macports.org/browser/trunk/dports/multimedia/mplayer-devel/Portfile
     # (B) Any kind of optimisation breaks the build
+    # (C) It turns out that ENV.O1 fixes link errors with llvm.
     ENV['CFLAGS'] = ''
     ENV['CXXFLAGS'] = ''
+    ENV.O1 if ENV.compiler == :llvm
 
     # we disable cdparanoia because homebrew's version is hacked to work on OS X
     # and mplayer doesn't expect the hacks we apply. So it chokes.
-    system './configure', "--prefix=#{prefix}", "--disable-cdparanoia"
+    # Specify our compiler to stop ffmpeg from defaulting to gcc.
+    system './configure', "--prefix=#{prefix}",
+                          "--cc=#{ENV.cc}",
+                          "--host-cc=#{ENV.cc}",
+                          "--disable-cdparanoia"
     system "make"
     system "make install"
   end
