6634f149d773e418cfc82a720a32a922c05191cf
Charlie Sharpsteen <source@sharpsteen.net>
2012-06-07 18:31:57 -0700

    librasterlite: Update to 1.1c

--- Library/Formula/librasterlite.rb
+++ Library/Formula/librasterlite.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Librasterlite < Formula
   homepage 'https://www.gaia-gis.it/fossil/librasterlite/index'
-  url 'http://www.gaia-gis.it/gaia-sins/librasterlite-sources/librasterlite-1.1b.tar.gz'
-  md5 '3100a552fa776ad1124ba00657760dea'
+  url 'http://www.gaia-gis.it/gaia-sins/librasterlite-sources/librasterlite-1.1c.tar.gz'
+  sha1 'c54f38ef2974bc92410e2c2196088af14bd9c21a'
 
   depends_on "libgeotiff"
   depends_on "libspatialite"
