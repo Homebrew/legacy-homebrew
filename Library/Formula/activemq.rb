a6d7acd6ad3a279ec855f0861ed6cfd0dc9a3e77
Adam Vandenberg <flangy@gmail.com>
2012-06-08 07:02:08 -0700

    ActiveMQ 5.6.0

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Activemq < Formula
   homepage 'http://activemq.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=activemq/apache-activemq/5.5.1/apache-activemq-5.5.1-bin.tar.gz'
-  md5 '3e10c163c5e3869a9720d47849a5ae29'
+  url 'http://www.apache.org/dyn/closer.cgi?path=activemq/apache-activemq/5.6.0/apache-activemq-5.6.0-bin.tar.gz'
+  sha1 '3205f5fcd61cf8bc7122b2f65ce22f68b2ab487a'
 
   skip_clean 'libexec/webapps/admin/WEB-INF/jsp'
 
