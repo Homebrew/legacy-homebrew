6a5134817a5c835cdf040c0c928b7ebac875464b
Charlie Sharpsteen <source@sharpsteen.net>
2012-06-07 18:29:01 -0700

    freexl: Update to 1.0.0c

--- Library/Formula/freexl.rb
+++ Library/Formula/freexl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Freexl < Formula
   homepage 'https://www.gaia-gis.it/fossil/freexl/index'
-  url 'http://www.gaia-gis.it/gaia-sins/freexl-sources/freexl-1.0.0b.tar.gz'
-  md5 'e2e1c59eb2883d825782dfe8fe244207'
+  url 'http://www.gaia-gis.it/gaia-sins/freexl-sources/freexl-1.0.0c.tar.gz'
+  sha1 'ea16e070ccb70845e73d88b87818805c3d2391f6'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
