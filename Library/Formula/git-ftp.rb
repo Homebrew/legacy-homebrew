3885ae88b86506dca84baf7d84cfc7623a5a263f
René Moser <mail@renemoser.net>
2012-06-08 10:47:25 +0300

    git-ftp 0.7.5

--- Library/Formula/git-ftp.rb
+++ Library/Formula/git-ftp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitFtp < Formula
   homepage 'https://github.com/resmo/git-ftp'
-  url 'https://github.com/resmo/git-ftp/tarball/0.7.4'
-  md5 '6795e657d6948ceac8c6931a36e97c0b'
+  url 'https://github.com/resmo/git-ftp/tarball/0.7.5'
+  md5 '59cafe4c6d8dc8a1b2250d696936ec52'
 
   head 'https://github.com/resmo/git-ftp.git'
 
