28634468c9d15b8a4ba033f679d8711006ad5387
Nick Frolov <nf@mkmks.org>
2012-06-09 17:41:56 +0200

    minisat: add headers and shared libraries to the build minisat-c-bindings: C bindings for minisat's C++ libraries

--- Library/Formula/minisat.rb
+++ Library/Formula/minisat.rb
@@ -1,46 +1,45 @@
 require 'formula'
 
 class Minisat < Formula
+  url 'https://github.com/niklasso/minisat/tarball/master'
+  #url 'https://github.com/niklasso/minisat/tarball/releases/2.2.0'
   homepage 'http://minisat.se'
-  url 'https://github.com/niklasso/minisat/tarball/releases/2.2.0'
-  md5 '2274977506042714811968afca01b68d'
+  #md5 '2274977506042714811968afca01b68d'
+  version 'master'
 
-  # Fixes compilation on mac; in next upstream release. See:
-  # http://groups.google.com/group/minisat/browse_thread/thread/f5b6a180cadbb214
   def patches
-    DATA
+	DATA
   end
 
   def install
-    ENV['MROOT'] = Dir.pwd
-    Dir.chdir 'simp'
-    system "make", "r"
-    bin.install 'minisat_release' => 'minisat'
+	system "cmake CMakeLists.txt"
+	system "make"
+	bin.install 'minisat_simp' => 'minisat'
+	lib.install Dir['libminisat.*']
+	minisat = include + 'minisat'
+	core = minisat + 'core'
+	mtl = minisat + 'mtl'
+	simp = minisat + 'simp'
+	utils = minisat + 'utils'
+	core.install Dir['minisat/core/*.h']
+	mtl.install Dir['minisat/mtl/*.h']
+	simp.install Dir['minisat/simp/*.h']
+	utils.install Dir['minisat/utils/*.h']
   end
+
 end
 
 __END__
-diff --git a/utils/System.cc b/utils/System.cc
-index a7cf53f..feeaf3c 100644
---- a/utils/System.cc
-+++ b/utils/System.cc
-@@ -78,16 +78,17 @@ double Minisat::memUsed(void) {
-     struct rusage ru;
-     getrusage(RUSAGE_SELF, &ru);
-     return (double)ru.ru_maxrss / 1024; }
--double MiniSat::memUsedPeak(void) { return memUsed(); }
-+double Minisat::memUsedPeak(void) { return memUsed(); }
- 
- 
- #elif defined(__APPLE__)
- #include <malloc/malloc.h>
- 
--double Minisat::memUsed(void) {
-+double Minisat::memUsed() {
-     malloc_statistics_t t;
-     malloc_zone_statistics(NULL, &t);
-     return (double)t.max_size_in_use / (1024*1024); }
-+double Minisat::memUsedPeak() { return memUsed(); }
- 
- #else
- double Minisat::memUsed() { 
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index ae4da00..ecbb30f 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -68,7 +68,7 @@ set_target_properties(minisat-lib-shared
+	 VERSION ${MINISAT_VERSION}
+	 SOVERSION ${MINISAT_SOVERSION})
+
+-set_target_properties(minisat_simp       PROPERTIES OUTPUT_NAME "minisat")
++set_target_properties(minisat_simp       PROPERTIES OUTPUT_NAME "minisat_simp")
+
+ #--------------------------------------------------------------------------------------------------
+ # Installation targets:
