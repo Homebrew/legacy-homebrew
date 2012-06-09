81c21de9811367baf385ce0c5fb9d57925c2f46d
Capi Etheriel <barraponto@gmail.com>
2012-06-09 17:11:56 -0300

    lighttpd: add lua option
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/lighttpd.rb
+++ Library/Formula/lighttpd.rb
@@ -1,17 +1,27 @@
 require 'formula'
 
 class Lighttpd < Formula
+  homepage 'http://www.lighttpd.net/'
   url 'http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.30.tar.bz2'
   sha256 '0d795597e4666dbf6ffe44b4a42f388ddb44736ddfab0b1ac091e5bb35212c2d'
-  homepage 'http://www.lighttpd.net/'
 
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
+  depends_on 'lua' if ARGV.include? '--with-lua'
+
+  def options
+    [['--with-lua', 'Include Lua scripting support for mod_magnet']]
+  end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--with-openssl", "--with-ldap"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-openssl
+      --with-ldap
+    ]
+    args << "--with-lua" if ARGV.include? '--with-lua'
+    system "./configure", *args
     system "make install"
   end
 end
