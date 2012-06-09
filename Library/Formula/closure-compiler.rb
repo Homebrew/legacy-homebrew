aa086251f8551bf57b1b0674bedbca9146acc752
oncletom <thomas@oncle-tom.net>
2012-06-09 10:57:09 +0200

    Formula as HEAD-only to fix the latest stable build
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/closure-compiler.rb
+++ Library/Formula/closure-compiler.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class ClosureCompiler < Formula
   homepage 'http://code.google.com/p/closure-compiler/'
-  url 'svn+http://closure-compiler.googlecode.com/svn/trunk/', :revision => '1918'
+  #url 'svn+http://closure-compiler.googlecode.com/svn/trunk/', :revision => '1918'
   version 'r1918'
 
-  head 'svn+http://closure-compiler.googlecode.com/svn/trunk/'
+  head 'svn+http://closure-compiler.googlecode.com/svn/trunk/', :revision => '1918'
 
   def install
     system "ant", "clean"
