a8c55288571cef971169e62c0aeda997a0fa41b8
oncletom <thomas@oncle-tom.net>
2012-06-09 10:26:31 +0200

    Made closure compiler formula working with SVN source
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/closure-compiler.rb
+++ Library/Formula/closure-compiler.rb
@@ -2,14 +2,20 @@ require 'formula'
 
 class ClosureCompiler < Formula
   homepage 'http://code.google.com/p/closure-compiler/'
-  url 'http://closure-compiler.googlecode.com/files/compiler-20120430.tar.gz'
-  sha1 '8adabd3a4307a4168cb050cdcc588e82060d15d3'
+  url 'svn+http://closure-compiler.googlecode.com/svn/trunk/', :revision => 1918
+  version 'r1918'
+
+  url 'svn+http://closure-compiler.googlecode.com/svn/trunk/'
 
   def install
-    libexec.install "compiler.jar"
-    (bin+'closure').write <<-EOS.undent
+    system "ant", "clean"
+    system "ant"
+
+    libexec.install Dir['*']
+
+    (bin+'closure-compiler').write <<-EOS.undent
       #!/bin/bash
-      java -jar "#{libexec}/compiler.jar" "$@"
+      java -jar "#{libexec}/build/compiler.jar" "$@"
     EOS
   end
 end
