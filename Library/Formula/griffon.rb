5e4d288d8830f1c25fffd97e8ab361a0417fc54e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-06-10 14:13:23 -0500

    upgraded to Griffon 1.0.0

--- Library/Formula/griffon.rb
+++ Library/Formula/griffon.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Griffon < Formula
   homepage 'http://griffon.codehaus.org/'
-  url 'http://dist.codehaus.org/griffon/griffon/0.9.x/griffon-0.9.5-bin.tgz'
-  md5 '2f3f579e09c8460e7265e567582a49cc'
+  url 'http://dist.codehaus.org/griffon/griffon/1.0.x/griffon-1.0.0-bin.zip'
+  md5 'cdb95cb96651a5a466ff3fe7b738684e'
 
   def install
     rm_f Dir["bin/*.bat"]
