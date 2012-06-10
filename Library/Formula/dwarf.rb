04c6dfb0431da4e210a63ab936a97d65cd929c03
nibbles 2bits <nibbles@speakeasy.net>
2012-06-10 13:55:33 -0700

    dwarf 0.3.0
    Upgrade dwarf to version 0.3.0
    Add a `fails_with :clang` block and comments indicating the bug
    report upstream.  It works well using llvm on Lion.

--- Library/Formula/dwarf.rb
+++ Library/Formula/dwarf.rb
@@ -1,16 +1,23 @@
 require 'formula'
 
 class Dwarf < Formula
-  url 'http://dwarf-ng.googlecode.com/files/dwarf-0.2.tar.gz'
-  head 'http://code.autistici.org/svn/dwarf/trunk'
   homepage 'http://code.autistici.org/trac/dwarf'
-  md5 '70dce54fe268af3368b9340e3ad73142'
+  url 'http://dwarf-ng.googlecode.com/files/dwarf-0.3.0.tar.gz'
+  sha1 '19a69424bd208741a325a4fc0e791a516c3bc8bc'
+
+  head 'http://code.autistici.org/svn/dwarf/trunk'
 
   depends_on 'readline'
 
+  # There's a subtle error in C99 handling of stdint.h and inttypes.h.
+  # Reported upstream: http://code.autistici.org/trac/dwarf/ticket/8
+  fails_with :clang do
+    build 318
+    cause "error: unknown type name 'intmax_t'"
+  end
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking'
     system "make install"
   end
 end
