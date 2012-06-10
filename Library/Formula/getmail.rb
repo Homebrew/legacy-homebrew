0b4a080b23a6696890bc932fdb7977defdb7e935
Peter Aronoff <telemachus@arpinum.org>
2012-06-10 07:22:33 -0400

    Bump getmail to 4.27.0

--- Library/Formula/getmail.rb
+++ Library/Formula/getmail.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Getmail < Formula
   homepage 'http://pyropus.ca/software/getmail/'
-  url 'http://pyropus.ca/software/getmail/old-versions/getmail-4.26.0.tar.gz'
-  md5 'a45622b355b649071a337c661533453a'
+  url 'http://pyropus.ca/software/getmail/old-versions/getmail-4.27.0.tar.gz'
+  md5 '7cab772a2ffb8db0a7d7a249d0336d2a'
 
   def install
     scripts = %w[ getmail getmail_fetch getmail_maildir getmail_mbox ]
