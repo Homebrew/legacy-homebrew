45292615ab0862e4d3bbb8e2b05767aa8369ead3
Adam Vandenberg <flangy@gmail.com>
2012-06-10 11:04:27 -0700

    ipmitool: fails with clang

--- Library/Formula/ipmitool.rb
+++ Library/Formula/ipmitool.rb
@@ -1,13 +1,19 @@
 require 'formula'
 
 class Ipmitool < Formula
-  url 'http://downloads.sourceforge.net/project/ipmitool/ipmitool/1.8.11/ipmitool-1.8.11.tar.bz2'
   homepage 'http://ipmitool.sourceforge.net/'
-  md5 '1d0da20add7388d64c549f95538b6858'
+  url 'http://downloads.sourceforge.net/project/ipmitool/ipmitool/1.8.11/ipmitool-1.8.11.tar.bz2'
+  sha1 '9f6667c3d47ca56f8c0803ea7849ed375133cb72'
+
+  fails_with :clang do
+    cause 'error: non-void functions should return a value'
+  end
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}",
-                          "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
     system "make install"
   end
 end
