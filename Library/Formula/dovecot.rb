0a2d58443d66acaba4310a3725f0ce3cf96f1ab5
nibbles 2bits <nibbles@speakeasy.net>
2012-06-10 13:38:30 -0700

    dovecot 2.1.6
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dovecot.rb
+++ Library/Formula/dovecot.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dovecot < Formula
   homepage 'http://dovecot.org/'
-  url 'http://www.dovecot.org/releases/2.1/dovecot-2.1.3.tar.gz'
-  md5 'a0e25243862c61de6274cf7d682a76ec'
+  url 'http://www.dovecot.org/releases/2.1/dovecot-2.1.6.tar.gz'
+  sha1 '88bde8a8110646ef1a5b594337a21964e5b35850'
 
   def install
     system "./configure", "--disable-dependency-tracking",
