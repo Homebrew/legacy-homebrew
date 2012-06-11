c4e55d3b1d20892a332a45e104bcf8f7995f0e56
Adam Vandenberg <flangy@gmail.com>
2012-06-10 20:34:37 -0700

    ranger 1.5.4

--- Library/Formula/ranger.rb
+++ Library/Formula/ranger.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ranger < Formula
   homepage 'http://ranger.nongnu.org/'
-  url 'http://nongnu.org/ranger/ranger-1.5.3.tar.gz'
-  md5 'ab90a2aa4d77cacbd47f45ed6b7c58f2'
+  url 'http://nongnu.org/ranger/ranger-1.5.4.tar.gz'
+  sha1 '6cf42596e18e45edcfaa90e8c9601934fcaee047'
 
   head 'git://git.savannah.nongnu.org/ranger.git'
 
