c3691d73cb9adeeb3f89c5406bc2e97848187177
Jonathan Beezley <jon.beezley@gmail.com>
2012-06-11 09:50:28 -0600

    Patch OUI update script to not check for root permissions.  Fixes #12752.

--- Library/Formula/aircrack-ng.rb
+++ Library/Formula/aircrack-ng.rb
@@ -5,6 +5,11 @@ class AircrackNg < Formula
   md5 'f7a24ed8fad122c4187d06bfd6f998b4'
   homepage 'http://aircrack-ng.org/'
 
+  def patches
+    # Remove root requirement from OUI update script.  Fixes #12752.
+    DATA
+  end
+
   def install
     # Force i386, otherwise you get errors:
     #  sha1-sse2.S:190:32-bit absolute addressing is not supported for x86-64
@@ -24,8 +29,38 @@ class AircrackNg < Formula
   end
 
   def caveats;  <<-EOS.undent
-    Run `airodump-ng-oui-update` as root (or with sudo) to install or update
-    the Airodump-ng OUI file.
+    Run `airodump-ng-oui-update` install or update the Airodump-ng OUI file.
     EOS
   end
 end
+
+__END__
+--- a/scripts/airodump-ng-oui-update
++++ b/scripts/airodump-ng-oui-update
+@@ -7,25 +7,6 @@
+ OUI_PATH="/usr/local/etc/aircrack-ng"
+ AIRODUMP_NG_OUI="${OUI_PATH}/airodump-ng-oui.txt"
+ OUI_IEEE="${OUI_PATH}/oui.txt"
+-USERID=""
+-
+-
+-# Make sure the user is root
+-if [ x"`which id 2> /dev/null`" != "x" ]
+-then
+-	USERID="`id -u 2> /dev/null`"
+-fi
+-
+-if [ x$USERID = "x" -a x$UID != "x" ]
+-then
+-	USERID=$UID
+-fi
+-
+-if [ x$USERID != "x" -a x$USERID != "x0" ]
+-then
+-	echo Run it as root ; exit ;
+-fi
+-
+ 
+ if [ ! -d "${OUI_PATH}" ]; then
+ 	mkdir -p ${OUI_PATH}
+
