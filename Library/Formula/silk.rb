9bc9c4668a587c4d3a39ba44153bb4ab114297e3
nibbles 2bits <nibbles@speakeasy.net>
2012-06-11 21:28:08 -0700

    silk 2.4.7
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/silk.rb
+++ Library/Formula/silk.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Silk < Formula
-  url 'http://tools.netsa.cert.org/releases/silk-2.4.0.tar.gz'
   homepage 'http://tools.netsa.cert.org/silk/'
-  md5 '3c12579712e2f49b07e56055a209d20a'
+  url 'http://tools.netsa.cert.org/releases/silk-2.4.7.tar.gz'
+  sha1 '3c12579712e2f49b07e56055a209d20a'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
