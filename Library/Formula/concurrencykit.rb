b344540e60eeb849ff36d19f88a44a497409ae72
Adam Vandenberg <flangy@gmail.com>
2012-06-11 21:05:47 -0700

    ConcurrencyKit 0.2.2

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -1,13 +1,15 @@
 require 'formula'
 
 class Concurrencykit < Formula
-  url 'http://concurrencykit.org/releases/ck-0.1.4.tar.gz'
   homepage 'http://concurrencykit.org'
-  md5 '11f6f2145d12c48fd17845b39f492ceb'
+  url 'http://concurrencykit.org/releases/ck-0.2.2.tar.gz'
+  sha1 '1c57819935f2a24128e521387e58587557de93c9'
+
   head 'git://git.concurrencykit.org/ck.git'
 
   def install
     system "./configure", "--prefix=#{prefix}"
+    system "make", "CC=#{ENV.cc}"
     system "make install"
   end
 end
