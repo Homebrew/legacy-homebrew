ce2e22b3a85f77d53c8c8d50d86011223e1e022f
samueljohn <github@samueljohn.de>
2012-06-13 01:06:43 +0200

    bind: 9.9.1-p2
    - Fix Xcode-only specific path for open-ssl.
    - Add ENV.libxml2
    
    Closes #14201.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/bind.rb
+++ Library/Formula/bind.rb
@@ -2,19 +2,34 @@ require 'formula'
 
 class Bind < Formula
   homepage 'http://www.isc.org/software/bind/'
-  url 'ftp://ftp.isc.org/isc/bind9/9.8.1-P1/bind-9.8.1-P1.tar.gz'
-  version '9.8.1-p1'
-  sha256 '867fdd52d3436c6ab6d357108d7f9eaaf03f1422652e6e61c742816ff7f87929'
+  url 'ftp://ftp.isc.org/isc/bind9/9.9.1-P2/bind-9.9.1-P2.tar.gz'
+  version '9.9.1-p2'
+  sha1 '449b12c32682b5bef64c7b53cd0fc0c6b731c8a7'
 
   depends_on "openssl" if MacOS.leopard?
 
   def install
-      ENV['STD_CDEFINES']='-DDIG_SIGCHASE=1'
-      system "./configure", "--prefix=#{prefix}",
-                            "--enable-threads",
-                            "--enable-ipv6",
-                            "--with-openssl"
-      system "make"
-      system "make install"
+    ENV.libxml2
+    # libxml2 appends one inc dir to CPPFLAGS but bind ignores CPPFLAGS
+    ENV.append 'CFLAGS', ENV['CPPFLAGS']
+
+    ENV['STD_CDEFINES'] = '-DDIG_SIGCHASE=1'
+
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-threads",
+      "--enable-ipv6",
+    ]
+
+    # For Xcode-only systems we help a bit to find openssl.
+    # If CLT.installed?, it evaluates to "/usr", which works.
+    args << "--with-openssl=#{MacOS.sdk_path.to_s}/usr" unless MacOS.leopard?
+
+    system "./configure", *args
+
+    # From the bind9 README: "Do not use a parallel 'make'."
+    ENV.deparallelize
+    system "make"
+    system "make install"
   end
 end
