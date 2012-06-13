4d0815419c98015a289f11ce73c3848939a0dc24
Cody Boisclair <cody@zone38.net>
2012-06-13 12:50:57 -0400

    Use latest rpm2cpio version from FreeBSD Ports

--- Library/Formula/rpm2cpio.rb
+++ Library/Formula/rpm2cpio.rb
@@ -1,8 +1,14 @@
 require 'formula'
 
 class Rpm2cpio < ScriptFileFormula
-  url 'http://www.iagora.com/~espel/rpm2cpio'
-  homepage 'http://www.iagora.com/~espel/rpm2cpio'
-  md5 '4840560d114b469ebf5a3eea690319fa'
-  version '0.1'
+  url 'http://www.freebsd.org/cgi/cvsweb.cgi/~checkout~/ports/archivers/rpm2cpio/files/rpm2cpio?rev=1.4'
+  homepage 'http://www.freebsd.org/cgi/cvsweb.cgi/ports/archivers/rpm2cpio/'
+  md5 '27f19cf1b0b05cfb3256b6b1781378f2'
+  version '1.4'
+  
+  depends_on 'xz'
+  
+  def install
+    bin.install 'rpm2cpio?rev=1.4' => 'rpm2cpio'
+  end
 end
