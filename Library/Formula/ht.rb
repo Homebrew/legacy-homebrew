498205de284e3aca75dad5f3ce1635c1bfdd162c
Jonathan Schleifer <js@webkeks.org>
2012-06-13 19:55:02 +0200

    ht 2.0.20
    Needs a patch now in order to link ncurses instead of ncursesw, as
    upstream switched to link ncursesw.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ht.rb
+++ Library/Formula/ht.rb
@@ -1,11 +1,16 @@
 require 'formula'
 
 class Ht < Formula
-  url 'http://downloads.sourceforge.net/project/hte/ht-source/ht-2.0.19.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/hte/ht-source/ht-2.0.20.tar.bz2'
   homepage 'http://hte.sf.net/'
-  sha1 '08cc1d82b74ea646e308e7b9629b03b5b79ca419'
+  sha256 '4aa162f10a13e60859bef1f04c6529f967fdfd660ae421ee25eab1fbabcd1ed0'
 
   depends_on 'lzo'
+
+  # Use ncurses instead of ncursesw
+  def patches
+    DATA
+  end
 
   def install
     system "chmod +x ./install-sh"
@@ -14,3 +19,18 @@ class Ht < Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index 0bef387..0e47404 100755
+--- a/configure
++++ b/configure
+@@ -5738,7 +5738,7 @@ if test "x$ac_cv_header_ncurses_h" = xyes; then :
+ _ACEOF
+ 
+ $as_echo "#define CURSES_HDR <ncurses.h>" >>confdefs.h
+- CURSES_LIB=ncursesw
++ CURSES_LIB=ncurses
+ fi
+ 
+ done
