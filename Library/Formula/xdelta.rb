02eb474a044806d304e2512475327fee26648fa3
Adam Vandenberg <flangy@gmail.com>
2012-06-13 09:13:04 -0700

    xdelta fails with clang

--- Library/Formula/xdelta.rb
+++ Library/Formula/xdelta.rb
@@ -1,9 +1,14 @@
 require 'formula'
 
 class Xdelta < Formula
-  url 'http://xdelta.googlecode.com/files/xdelta3.0.0.tar.gz'
   homepage 'http://xdelta.org'
+  url 'http://xdelta.googlecode.com/files/xdelta3.0.0.tar.gz'
   sha1 'c9e54fd8dbd9f2e77ead17be9d00e0b8af109024'
+
+  fails_with :clang do
+    build 318
+    cause "Undefined symbols for architecture x86_64: \"_xd3_source_eof\""
+  end
 
   def install
     system "make"
