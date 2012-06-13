2ee837172f4e6472840214dbb9f30067bfbf1acf
Karsten Sperling <mail@ksperling.net>
2012-06-13 17:04:19 +1200

    auto-scaling 1.0.61
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/auto-scaling.rb
+++ Library/Formula/auto-scaling.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class AutoScaling < AmazonWebServicesFormula
-  version  '1.0.49.1'
   homepage 'http://aws.amazon.com/developertools/2535'
-  url      'http://ec2-downloads.s3.amazonaws.com/AutoScaling-2011-01-01.zip'
-  md5      '9d94f533743c5e9c65dd76f69666e739'
+  url 'http://ec2-downloads.s3.amazonaws.com/AutoScaling-2011-01-01.zip'
+  version  '1.0.61.0'
+  sha1 ''
 
   def install
     standard_install
