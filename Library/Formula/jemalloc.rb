15b2ff438077247329cc4d2f581b741d6b6caf34
nibbles 2bits <nibbles@speakeasy.net>
2012-06-14 02:23:45 -0700

    jemalloc 3.0.0
    Upgreade Jemalloc to version 3.0.0.
    Compiles on Lion with clang and llvm from XCode-4.3.3.
    
    Closes #12837.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/jemalloc.rb
+++ Library/Formula/jemalloc.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Jemalloc < Formula
-  url 'http://www.canonware.com/download/jemalloc/jemalloc-2.2.2.tar.bz2'
   homepage 'http://www.canonware.com/jemalloc/download.html'
-  md5 '65b2b3f68c9d229246a67f5a4d29ba83'
+  url 'http://www.canonware.com/download/jemalloc/jemalloc-3.0.0.tar.bz2'
+  sha1 '65a66bd1b54ffdd56f5024b45df19d40d6e6f9dd'
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
