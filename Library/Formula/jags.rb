26c897dc80ec001dd3ba3a963e00be661b5f9fb8
nibbles 2bits <nibbles@speakeasy.net>
2012-06-14 02:07:13 -0700

    jags 3.2.0
    Upgrade jags to version 3.2.0.
    Remove the `chmod` command because the binary is already 755.
    Tested on Lion with clang and llvm from XCode-4.3.3.
    
    Closes #12835.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/jags.rb
+++ Library/Formula/jags.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Jags < Formula
-  url 'http://sourceforge.net/projects/mcmc-jags/files/JAGS/3.x/Source/JAGS-3.1.0.tar.gz'
   homepage 'http://www-fis.iarc.fr/~martyn/software/jags/'
-  md5 '1f84163a3e5114d30d7832fc26e54acf'
+  url 'http://sourceforge.net/projects/mcmc-jags/files/JAGS/3.x/Source/JAGS-3.2.0.tar.gz'
+  sha1 '020df50df98e066b261db77b061d8d8d354bf72b'
 
   def install
     ENV.fortran
@@ -11,6 +11,5 @@ class Jags < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
-    system "chmod +x #{bin}/jags"
   end
 end
