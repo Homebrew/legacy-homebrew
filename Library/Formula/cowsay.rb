12011ee470cd222c003639c49460d0d9a9b62828
Misty De Meo <mistydemeo@gmail.com>
2012-06-14 22:50:06 -0500

    cowsay: new sha1
    cowsay returns after a brief absence with a new hash.
    
    Closes #12840.

--- Library/Formula/cowsay.rb
+++ Library/Formula/cowsay.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Cowsay < Formula
   url 'http://www.nog.net/~tony/warez/cowsay-3.03.tar.gz'
   homepage 'http://www.nog.net/~tony/warez/cowsay.shtml'
-  md5 'b29169797359420dadb998079021a494'
+  sha1 'e44dec32d2a462ed87f5e419237d6f236b87efe0'
 
   def install
     system "/bin/sh", "install.sh", prefix
