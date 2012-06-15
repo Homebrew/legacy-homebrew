5d21e1e24609a3e5ada81612fc30c2a86e6c6233
Jack Nagel <jacknagel@gmail.com>
2012-06-15 14:45:57 -0500

    Add comment about error pipe (mis)behavior
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Homebrew/build.rb
+++ Library/Homebrew/build.rb
@@ -32,6 +32,12 @@ at_exit do
     # can be inconvenient for the user. But we need to be safe.
     system "/usr/bin/sudo -k"
 
+    # The main Homebrew process expects to eventually see EOF on the error
+    # pipe in FormulaInstaller#build. However, if any child process fails to
+    # terminate (i.e, fails to close the descriptor), this won't happen, and
+    # the installer will hang. Set close-on-exec to prevent this.
+    # Whether it is *wise* to launch daemons from formulae is a separate
+    # question altogether.
     if ENV['HOMEBREW_ERROR_PIPE']
       require 'fcntl'
       IO.new(ENV['HOMEBREW_ERROR_PIPE'].to_i, 'w').fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
