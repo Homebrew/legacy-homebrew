4d63aef5334e3d7a916113e6fa35f2e8ba376543
nibbles 2bits <nibbles@speakeasy.net>
2012-06-14 22:06:21 -0700

    yaz 4.2.33
    Upgrade yaz to version 4.2.33.
    Tested with clang and llvm from XCode-4.3.3.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Yaz < Formula
-  url 'http://ftp.indexdata.dk/pub/yaz/yaz-4.2.28.tar.gz'
   homepage 'http://www.indexdata.com/yaz'
-  md5 '6c75d0448b729a013706748c7a8c3f7f'
+  url 'http://ftp.indexdata.dk/pub/yaz/yaz-4.2.33.tar.gz'
+  sha1 '439b3098c7598d704dd65474ebf454a77ee9b27e'
 
   depends_on 'pkg-config' => :build
 
