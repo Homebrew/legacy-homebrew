a09096ce9705e55b61187cb455dbe9841c0fe0ba
samueljohn <github@samueljohn.de>
2012-06-15 11:43:50 +0200

    lua: Using our LDFLAGS
    This is needed for Xcode-only installations, since the LDFLAGS matter.
    
    Closes #12856.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/lua.rb
+++ Library/Formula/lua.rb
@@ -39,6 +39,7 @@ class Lua < Formula
     inreplace 'src/Makefile' do |s|
       s.remove_make_var! 'CC'
       s.change_make_var! 'CFLAGS', "#{ENV.cflags} $(MYCFLAGS)"
+      s.change_make_var! 'MYLDFLAGS', ENV['LDFLAGS']
     end
 
     # Fix path in the config header
