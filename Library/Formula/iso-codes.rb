781a67449919940e47a5f12d5bc8cc69622fc69e
nibbles 2bits <nibbles@speakeasy.net>
2012-06-16 04:24:04 -0700

    iso-codes 3.35
    Upgrade iso-codes to verion 3.35.
    Remove the PATH append for gettext bin.  It's done automatically.
    Tested on Lion with cland and llvm from XCode-4.3.3.
    
    Closes #12878.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/iso-codes.rb
+++ Library/Formula/iso-codes.rb
@@ -1,16 +1,13 @@
 require 'formula'
 
 class IsoCodes < Formula
-  url 'http://ftp.us.debian.org/debian/pool/main/i/iso-codes/iso-codes_3.28.orig.tar.bz2'
   homepage 'http://pkg-isocodes.alioth.debian.org/'
-  md5 'f84dda8dcf7ffdcbe64c82b7c2165bf8'
+  url 'http://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.35.tar.bz2'
+  sha1 'ae9cfc7a17ff16ddaca9a925dab826789b571b61'
 
   depends_on 'gettext' => :build
 
   def install
-    # Add keg-only gettext bin to path
-    ENV.append 'PATH', Formula.factory('gettext').bin
-
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
