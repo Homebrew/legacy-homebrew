fd8fc16d0bbd577fd103e48f16e7d15266026e60
nibbles 2bits <nibbles@speakeasy.net>
2012-06-16 04:18:53 -0700

    ipv6calc 0.93.1
    Upgrade ipv6calc to version 0.93.1.
    Tested on Lion with cland and llvm from XCode-4.3.3.
    
    Closes #12876.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ipv6calc.rb
+++ Library/Formula/ipv6calc.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Ipv6calc < Formula
-  url 'ftp://ftp.bieringer.de/pub/linux/IPv6/ipv6calc/ipv6calc-0.90.0.tar.gz'
   homepage 'http://www.deepspace6.net/projects/ipv6calc.html'
-  md5 'c1a654214058fdac5f8c2ba6a196e2b8'
+  url 'ftp://ftp.deepspace6.net/pub/ds6/sources/ipv6calc/ipv6calc-0.93.1.tar.gz'
+  sha1 '8cd23ed0bee03d79c70662e64ab4c7aae1219439'
 
   def install
+    # This needs --mandir, otherwise it tries to install to /share/man/man8.
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make install"
   end
