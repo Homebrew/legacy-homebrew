e027d0c414cfbfb69ec71b42162bed30ff8658b8
nibbles 2bits <nibbles@speakeasy.net>
2012-06-16 04:16:47 -0700

    ipbt r9487
    Upgrade ipbt to version r9487.
    Remove the `--disable-debug` that's unrecognized.
    Tested on Lion with clang and llvm from XCode-4.3.3.
    
    Closes #12875.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ipbt.rb
+++ Library/Formula/ipbt.rb
@@ -2,13 +2,12 @@ require 'formula'
 
 class Ipbt < Formula
   homepage 'http://www.chiark.greenend.org.uk/~sgtatham/ipbt/'
-  url 'http://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-r9253.tar.gz'
-  version 'r9253'
-  md5 '4af1feec39b95b437b669e3f1e46d7ef'
+  url 'http://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-r9487.tar.gz'
+  sha1 'ff0b306dae14ed6c693331c995410ad8dfc28d02'
+  version 'r9487'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking'
     system "make install"
   end
 
