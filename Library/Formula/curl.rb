f6959d43968884c580c6fe5e6370d48fa1be1ee2
Adam Vandenberg <flangy@gmail.com>
2012-06-17 10:02:48 -0700

    curl 7.26.0

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Curl < Formula
   homepage 'http://curl.haxx.se/'
-  url 'http://curl.haxx.se/download/curl-7.25.0.tar.bz2'
-  sha256 'cfb29139dfeb2449bf8a89963c3aed7d87f034013f05564cc0330b435c90b491'
+  url 'http://curl.haxx.se/download/curl-7.26.0.tar.bz2'
+  sha256 'fced262f16eb6bfcdcea15e04a7905ffcb5ff04b14a19ca35b9df86d6720d26a'
 
   keg_only :provided_by_osx,
             "The libcurl provided by Leopard is too old for CouchDB to use."
