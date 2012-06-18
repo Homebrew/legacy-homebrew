f9a32f4c6f83a06184cb495787458bf13989fcee
Tamas Nepusz <ntamas@gmail.com>
2012-06-18 15:58:59 +0200

    igraph 0.6
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/igraph.rb
+++ Library/Formula/igraph.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Igraph < Formula
   homepage 'http://igraph.sourceforge.net'
-  url 'http://downloads.sourceforge.net/sourceforge/igraph/igraph-0.5.4.tar.gz'
-  md5 '47963ece64fe5f793e154e238bc6c3c3'
+  url 'http://downloads.sourceforge.net/sourceforge/igraph/igraph-0.6.tar.gz'
+  sha1 'd9a6e7c0462b5948ebfbf9f1d2685141f5867fe3'
 
   # GMP is optional, and doesn't have a universal build
   depends_on 'gmp' => :optional unless ARGV.build_universal?
@@ -12,16 +12,11 @@ class Igraph < Formula
     [["--universal", "Build a universal binary."]]
   end
 
-  # Fix for llvm-gcc. This is already merged in upstream and
-  # will not be required for igraph >= 0.5.5
-  def patches
-    "https://raw.github.com/gist/1209951/e337ad8c2d8cb613872e5381a99f411d314576a1/igraph-0.5.4_llvm-gcc.patch"
-  end
-
   def install
     ENV.universal_binary if ARGV.build_universal?
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
   end
