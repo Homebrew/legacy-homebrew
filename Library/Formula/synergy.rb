6fcf26714d61c79a47e6c64372dcfd511f23ebc6
Adam Vandenberg <flangy@gmail.com>
2012-06-18 10:38:47 -0700

    synergy: remove .dmg --devel version
    Homebrew doesn't know what to do with .dmg files, and they
    should probably be installed manually anyway.

--- Library/Formula/synergy.rb
+++ Library/Formula/synergy.rb
@@ -12,19 +12,6 @@ class Synergy < Formula
     sha1 'cedf8dc0f5f1d95b967fabcd4ed9bfc0f72840e6'
   end
 
-  devel do
-    version '1.4.8'
-    if MacOS.lion?
-      url 'http://synergy.googlecode.com/files/synergy-1.4.8-MacOSX107-x86_64.dmg'
-      sha1 '8c3f0326d74c935c20cc83c79dda20677d9b5814'
-    elsif MacOS.snow_leopard?
-      url 'http://synergy.googlecode.com/files/synergy-1.4.8-MacOSX106-i386.dmg'
-      sha1 '1de78afdc838b1c0ef21e36d996b18ff6019b191'
-    else
-      opoo "No --devel for this version of OS X."
-    end
-  end
-
   def install
     bin.install 'synergyc', 'synergys'
   end
