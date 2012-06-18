487cd815cf36f2d95bf6d574c40bced4ab5e22fa
Drew Stephens <drew@dinomite.net>
2012-06-18 16:47:23 -0400

    Incorporate adamv's comments on pull request
    https://github.com/dinomite/homebrew/commit/fadd974df648e34c522e69e5770b21d4c7036544#commitcomment-1472680

--- Library/Formula/whirr.rb
+++ Library/Formula/whirr.rb
@@ -2,14 +2,11 @@ require 'formula'
 
 class Whirr < Formula
   homepage 'http://whirr.apache.org/'
-  url 'http://www.carfab.com/apachesoftware/whirr/whirr-0.7.1/whirr-0.7.1.tar.gz'
+  url 'http://www.apache.org/dyn/closer.cgi?path=whirr/whirr-0.7.1/whirr-0.7.1.tar.gz'
   sha1 '15772fd7bf35cbc1c50023f4a22bcbb1cd1f80c9'
 
   def install
-      lib.install Dir['lib/*']
-      bin.install Dir['bin/whirr']
-  end
-
-  def test
+    lib.install Dir['lib/*']
+    bin.install Dir['bin/whirr']
   end
 end
