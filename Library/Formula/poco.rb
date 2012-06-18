7b539ff9ea38facfc26cf7e7b0db51ddfab941c9
Adam Vandenberg <flangy@gmail.com>
2012-06-18 12:20:31 -0700

    poco 1.4.3p1

--- Library/Formula/poco.rb
+++ Library/Formula/poco.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Poco < Formula
   homepage 'http://pocoproject.org/'
-  url 'http://downloads.sourceforge.net/project/poco/sources/poco-1.4.1/poco-1.4.1p1-all.tar.bz2'
-  md5 '5b35baa1bf7ee4b20437b8950a1c5012'
+  url 'http://downloads.sourceforge.net/project/poco/sources/poco-1.4.3/poco-1.4.3p1-all.tar.gz'
+  sha1 '893b26bdd2adee36d489ce1412bf67d5035f5b47'
 
   def install
     arch = Hardware.is_64_bit? ? 'Darwin64': 'Darwin'
