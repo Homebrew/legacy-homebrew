52a33de3da50213b1bd006ee01bcab079c92e74e
nibbles 2bits <nibbles@speakeasy.net>
2012-06-18 09:59:19 -0700

    qrencode 3.3.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/qrencode.rb
+++ Library/Formula/qrencode.rb
@@ -2,14 +2,13 @@ require 'formula'
 
 class Qrencode < Formula
   homepage 'http://fukuchi.org/works/qrencode/index.html.en'
-  url 'http://fukuchi.org/works/qrencode/qrencode-3.3.0.tar.gz'
-  sha1 '7e7143b2a2b925d77f2d9bae8135c1b7f027af8e'
+  url 'http://fukuchi.org/works/qrencode/qrencode-3.3.1.tar.gz'
+  sha1 'a5a99f47f4215ec17e74999fdb893a4e542f790c'
 
   depends_on 'pkg-config' => :build
 
   def install
     ENV.x11 # For libpng
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
