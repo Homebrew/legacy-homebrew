86a2cdec08155cbc496257073ec6d368ef4bfdff
Tamas Nepusz <ntamas@gmail.com>
2012-06-18 15:58:59 +0200

    igraph: update to 0.6

--- Library/Formula/igraph.rb
+++ Library/Formula/igraph.rb
@@ -2,20 +2,14 @@ require 'formula'
 
 class Igraph < Formula
   homepage 'http://igraph.sourceforge.net'
-  url 'http://downloads.sourceforge.net/sourceforge/igraph/igraph-0.5.4.tar.gz'
-  md5 '47963ece64fe5f793e154e238bc6c3c3'
+  url 'http://downloads.sourceforge.net/sourceforge/igraph/igraph-0.6.tar.gz'
+  md5 '1590c524b7849ecf9b25705b746756e2'
 
   # GMP is optional, and doesn't have a universal build
   depends_on 'gmp' => :optional unless ARGV.build_universal?
 
   def options
     [["--universal", "Build a universal binary."]]
-  end
-
-  # Fix for llvm-gcc. This is already merged in upstream and
-  # will not be required for igraph >= 0.5.5
-  def patches
-    "https://raw.github.com/gist/1209951/e337ad8c2d8cb613872e5381a99f411d314576a1/igraph-0.5.4_llvm-gcc.patch"
   end
 
   def install
