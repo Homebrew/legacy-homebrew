dc5e4ef9387a46f26ff9c84360defa55a824ad14
nibbles 2bits <nibbles@speakeasy.net>
2012-06-18 09:12:16 -0700

    qemu 1.1.0-1
    Upgrade qemu to version 1.1.0-1.
    Add `--disable-sdl`.  Let it use the CoreAudio backend instead.
    Add a flag to specify the build target based on `prefer_64_bit?`
    Add a `cause` to the `fails_with :clang` for future reference.
    Tested on Lion with llvm from XCode-4.3.3.

--- Library/Formula/qemu.rb
+++ Library/Formula/qemu.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Qemu < Formula
   homepage 'http://www.qemu.org/'
-  url 'http://wiki.qemu.org/download/qemu-1.1.0.tar.bz2'
-  sha256 '927f498eff8dce2334de9338cae9e3a7c63bd472c85451235c45de5029140fc0'
+  url 'http://wiki.qemu.org/download/qemu-1.1.0-1.tar.bz2'
+  sha256 '1e566f8cbc33e5fb7d5f364c0fd1cdde9e921e647223b5d7ae7e5f95544b258d'
 
   depends_on 'jpeg'
   depends_on 'gnutls'
@@ -11,15 +11,26 @@ class Qemu < Formula
 
   fails_with :clang do
     build 318
+    cause 'Compile error: global register variables are not supported'
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--cc=#{ENV.cc}",
-                          "--host-cc=#{ENV.cc}",
-                          "--enable-cocoa",
-                          "--disable-bsd-user",
-                          "--disable-guest-agent"
+    # Disable the sdl backend. Let it use CoreAudio instead.
+    args = %W[
+      --prefix=#{prefix}
+      --cc=#{ENV.cc}
+      --host-cc=#{ENV.cc}
+      --enable-cocoa
+      --disable-bsd-user
+      --disable-guest-agent
+      --disable-sdl
+    ]
+    if MacOS.prefer_64_bit?
+      args << '--target-list=x86_64-softmmu'
+    else
+      args << '--target-list=i386-softemu'
+    end
+    system "./configure", *args
     system "make install"
   end
 end
