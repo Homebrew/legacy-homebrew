75bc18c2f317e78f3d6cf1dd30636d1cd1dc66e5
John Benediktsson <mrjbq7@gmail.com>
2012-06-19 08:59:44 -0700

    gdbm: add universal option.
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gdbm.rb
+++ Library/Formula/gdbm.rb
@@ -6,7 +6,12 @@ class Gdbm < Formula
   mirror 'http://ftp.gnu.org/gnu/gdbm/gdbm-1.10.tar.gz'
   md5 '88770493c2559dc80b561293e39d3570'
 
+  def options
+    [["--universal", "Build a universal binary."]]
+  end
+
   def install
+    ENV.universal_binary if ARGV.build_universal?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
