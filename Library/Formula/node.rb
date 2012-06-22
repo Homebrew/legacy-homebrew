4de0c5b5163631970be04b89f8177a7312a7389e
Adam Vandenberg <flangy@gmail.com>
2012-06-21 22:53:44 -0700

    node: fix devel

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -8,8 +8,8 @@ class Node < Formula
   head 'https://github.com/joyent/node.git'
 
   devel do
-    url 'http://nodejs.org/dist/v0.7.11/node-v0.7.12.tar.gz'
-    sha1 '036ab6662287901db9d85df93a5d2a25'
+    url 'http://nodejs.org/dist/v0.7.12/node-v0.7.12.tar.gz'
+    sha1 'cb570abacbf4eb7e23c3d2620d00dd3080d9c19d'
   end
 
   # Leopard OpenSSL is not new enough, so use our keg-only one
