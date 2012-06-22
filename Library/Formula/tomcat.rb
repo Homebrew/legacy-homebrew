3293f97435394dc4aab5cd139fcb90ef1e4bc3db
Adam Vandenberg <flangy@gmail.com>
2012-06-21 22:52:55 -0700

    tomcat: fix hash

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Tomcat < Formula
   homepage 'http://tomcat.apache.org/'
   url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.28/bin/apache-tomcat-7.0.28.tar.gz'
-  sha1 '5f2b0c19ab413f2a98683d360b7a092d03e9595b'
+  sha1 '1bf21f2121eb0dc234cd092ec1d9c92416c0e4b1'
 
   skip_clean :all
 
