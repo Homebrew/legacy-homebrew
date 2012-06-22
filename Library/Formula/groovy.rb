1417c77f4686de39e5c1cb4ff431ec710fa08036
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-06-22 12:28:13 -0500

    Groovy devel 2.0.0.rc-4
    Closes #12972.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -3,12 +3,12 @@ require 'formula'
 class Groovy < Formula
   homepage 'http://groovy.codehaus.org/'
   url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-1.8.6.zip'
-  md5 'e62d2f9c2c4d528b8a0eb49cdfb389ae'
+  sha1 '470fd9b2ea4f2cf405b8a71d92e7a8a4753b1a8c'
 
   devel do
-    url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.0-rc-2.zip'
-    version '2.0.0-rc-2'
-    md5 '13a8277137561596a8ff8bbd15b842b2'
+    url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.0-rc-4.zip'
+    version '2.0.0-rc-4'
+    sha1 '1bfb33e14736bad6c99ba08014c16c89f5bd100a'
   end
 
   def install
