c88203c12203559388b14d91a475ed562cd6bd93
Jack Nagel <jacknagel@gmail.com>
2012-06-25 19:33:42 -0500

    parallel 20120622
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Parallel < Formula
   homepage 'http://savannah.gnu.org/projects/parallel/'
-  url 'http://ftpmirror.gnu.org/parallel/parallel-20120522.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20120522.tar.bz2'
-  sha1 'bc8118949f900c05ef232c6946c4ebfa779cafed'
+  url 'http://ftpmirror.gnu.org/parallel/parallel-20120622.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20120622.tar.bz2'
+  sha1 'b184a55674ce10e8bf7c65790c2806552eb79577'
 
   def install
     system "./configure", "--prefix=#{prefix}"
