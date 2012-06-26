bd46463b2a2da87eaf8e1d7d6a244ccb9865bedc
John Knight <bug@knightlabs.com>
2012-06-26 08:57:24 -0400

    Updated formula: Play 2.0.2
    Updated play.rb formula to reflect GA of playframework 2.0.2

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -2,29 +2,16 @@ require 'formula'
 
 class Play < Formula
   homepage 'http://www.playframework.org/'
-  url 'http://download.playframework.org/releases/play-2.0.1.zip'
-  md5 'f860f006a713a6c9949c4c3f3efc33fb'
+  url 'http://download.playframework.org/releases/play-2.0.2.zip'
+  md5 'd1408ffca1f35cc89167e0717dd31069'
 
-  devel do
-    url 'http://download.playframework.org/releases/play-2.0-RC4.zip'
-    md5 '98a9bbe198ca75146dfac813af5cfdbd'
-  end
-  
   def install
-<<<<<<< HEAD
-    rm_rf 'python' unless ARGV.build_devel? # we don't need the bundled Python for windows, and has been removed in the RC builds
-    rm Dir['*.bat']
-=======
     rm Dir['*.bat'] # remove windows' bat files
->>>>>>> master
     libexec.install Dir['*']
     inreplace libexec+"play" do |s|
       s.gsub! "$dir/", "$dir/../libexec/"
       s.gsub! "dir=`dirname $PRG`", "dir=`dirname $0` && dir=$dir/`dirname $PRG`"
     end
     bin.install_symlink libexec+'play'
-    
-    # For some reason, play is attempting to unfuck things if the play script is a symlink.
-    inreplace "#{libexec}/play", "dir=`dirname $PRG`", "dir=#{libexec}" if ARGV.build_devel?
   end
 end
