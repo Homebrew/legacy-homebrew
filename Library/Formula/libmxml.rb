45ea56f4b6a135659d3be65281758a47a6e61045
Adam Vandenberg <flangy@gmail.com>
2012-06-27 12:19:32 -0700

    libmxml 2.7
    Also fix hard-coded /Developer path.

--- Library/Formula/libmxml.rb
+++ Library/Formula/libmxml.rb
@@ -1,15 +1,19 @@
 require 'formula'
 
 class Libmxml < Formula
-  url 'http://ftp.easysw.com/pub/mxml/2.6/mxml-2.6.tar.gz'
   homepage 'http://www.minixml.org/'
-  md5 '68977789ae64985dddbd1a1a1652642e'
+  url 'http://ftp.easysw.com/pub/mxml/2.7/mxml-2.7.tar.gz'
+  sha1 'a3bdcab48307794c297e790435bcce7becb9edae'
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--enable-shared",
                           "--prefix=#{prefix}"
+
+    # Makefile hard-codes the path to /Developer
+    inreplace "Makefile", "/Developer/usr/bin/docsetutil", MacOS.locate('docsetutil')
+
     system "make"
     system "make install"
   end
