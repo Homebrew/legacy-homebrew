2ac12cf69a261960da126a3a0bf442579aa5ed3f
Adam Vandenberg <flangy@gmail.com>
2012-06-27 11:44:57 -0700

    libgphoto2 2.4.14

--- Library/Formula/libgphoto2.rb
+++ Library/Formula/libgphoto2.rb
@@ -2,15 +2,16 @@ require 'formula'
 
 class Libgphoto2 < Formula
   homepage 'http://www.gphoto.org/proj/libgphoto2/'
-  url 'http://downloads.sourceforge.net/project/gphoto/libgphoto/2.4.13/libgphoto2-2.4.13.tar.bz2'
-  md5 'd20a32fe2bb7d802a6a8c3b6f7f97c5e'
+  url 'http://downloads.sourceforge.net/project/gphoto/libgphoto/2.4.14/libgphoto2-2.4.14.tar.bz2'
+  sha1 'c932f44d51e820245ff3394ee01a5e9df429dfef'
 
   depends_on 'pkg-config' => :build
   depends_on 'libusb-compat'
   depends_on 'libexif' => :optional
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
