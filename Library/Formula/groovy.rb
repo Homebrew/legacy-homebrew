278a37d241733c9fd76518fb77ec3a4419a34515
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-06-28 10:59:38 -0500

    Groovy 2.0.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -2,14 +2,8 @@ require 'formula'
 
 class Groovy < Formula
   homepage 'http://groovy.codehaus.org/'
-  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-1.8.6.zip'
-  sha1 '470fd9b2ea4f2cf405b8a71d92e7a8a4753b1a8c'
-
-  devel do
-    url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.0-rc-4.zip'
-    version '2.0.0-rc-4'
-    sha1 '1bfb33e14736bad6c99ba08014c16c89f5bd100a'
-  end
+  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.0.zip'
+  sha1 '6ae42581500e157759a56129d215c0a870cd2a4d'
 
   def install
     # Don't need Windows files.
