8e7cf15f01eb3354e451b3f5ad2cbc8c695afeb9
nibbles 2bits <nibbles@speakeasy.net>
2012-06-28 23:14:21 -0700

    rakudo-star 2012.05
    Upgrade rakudo-star to version 2012.05.
    Adjust `CPPFLAGS` to correctly locate the libffi includes.
    Add `ENV.j1` to stop intermittent race conditions when compiling.
    Add code to move the man pages into share because there's no
    configure option for that.
    
    Tested on Lion using clang and llvm from XCode-4.3.3.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rakudo-star.rb
+++ Library/Formula/rakudo-star.rb
@@ -2,17 +2,25 @@ require 'formula'
 
 class RakudoStar < Formula
   homepage 'http://rakudo.org/'
-  url 'https://github.com/downloads/rakudo/star/rakudo-star-2012.01.tar.gz'
-  sha256 '2bfa055c6d3b6060917fb45561d1346fef518912aaf69af361f54dd3f9ec903c'
+  url 'https://github.com/downloads/rakudo/star/rakudo-star-2012.05.tar.gz'
+  sha256 '4bb1cee56e28e2c26948eefe1ae141373191b9b8cff334baa5aa295382e02b9a'
 
   depends_on 'gmp' => :optional
   depends_on 'icu4c' => :optional
   depends_on 'pcre' => :optional
+  depends_on 'libffi'
 
   def install
+    libffi = Formula.factory("libffi")
+    ENV.remove 'CPPFLAGS', "-I#{libffi.include}"
+    ENV.prepend 'CPPFLAGS', "-I#{libffi.lib}/libffi-3.0.11/include"
+
+    ENV.j1  # An intermittent race condition causes random build failures.
     system "perl", "Configure.pl", "--prefix=#{prefix}", "--gen-parrot"
     system "make"
     system "make install"
+    # move the man pages out of the top level into share.
+    mv "#{prefix}/man", share
   end
 
   def caveats; <<-EOS
