12234407acc750f42d2cd52b834bc8d02b0b96ec
Adam Vandenberg <flangy@gmail.com>
2012-06-29 09:58:43 -0700

    libpst 0.6.54

--- Library/Formula/libpst.rb
+++ Library/Formula/libpst.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class Libpst < Formula
-  url 'http://www.five-ten-sg.com/libpst/packages/libpst-0.6.53.tar.gz'
   homepage 'http://www.five-ten-sg.com/libpst/'
-  md5 'e030d3128562ac189c2400dedec36b86'
+  url 'http://www.five-ten-sg.com/libpst/packages/libpst-0.6.54.tar.gz'
+  sha1 'a4713b87e330556acb0786b4efa975a3c739cd84'
 
   depends_on 'boost'
 
   def install
     system "./configure", "--prefix=#{prefix}",
-                          "--mandir=#{man}",
+                          # "--mandir=#{man}",
                           "--with-boost-python=boost_python-mt"
     system "make install"
   end
