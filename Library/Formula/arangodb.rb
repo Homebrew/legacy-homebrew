92c308ef0df15870ad46ee110829eb25358448ee
Frank Celler <f.celler@triagens.de>
2012-06-30 14:52:16 +0300

    ArangoDB 1.0.alpha3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/arangodb.rb
+++ Library/Formula/arangodb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Arangodb < Formula
   homepage 'http://www.arangodb.org/'
-  url "https://github.com/triAGENS/ArangoDB/zipball/v1.0.alpha2"
-  sha1 'd6e90221dcc02252ae9bb66085a797d495be5bc4'
+  url "https://github.com/triAGENS/ArangoDB/zipball/v1.0.alpha3"
+  sha1 '1e63607f1d98edaa02e36e664ebb770ace0bbda7'
 
   head "https://github.com/triAGENS/ArangoDB.git"
 
@@ -11,18 +11,22 @@ class Arangodb < Formula
   depends_on 'v8'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
                           "--disable-relative",
                           "--disable-all-in-one",
                           "--enable-mruby",
-                          "--disable-debug",
-                          "--disable-dependency-tracking",
                           "--datadir=#{share}",
                           "--localstatedir=#{var}"
 
     system "make install"
 
-    (var+'arango').mkpath
+    (var+'arangodb').mkpath
+    (var+'log/arangodb').mkpath
+
+    plist_path.write startup_plist
+    plist_path.chmod 0644
   end
 
   def caveats; <<-EOS.undent
@@ -31,11 +35,46 @@ class Arangodb < Formula
 
       https://github.com/triAGENS/ArangoDB/issues
 
-    To start the ArangoDB server, run:
+    If this is your first install, automatically load on login with:
+        mkdir -p ~/Library/LaunchAgents
+        cp #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    If this is an upgrade and you already have the #{plist_path.basename} loaded:
+        launchctl unload -w ~/Library/LaunchAgents/#{plist_path.basename}
+        cp #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+
+    To start the ArangoDB server manually, run:
         /usr/local/sbin/arangod
 
     To start the ArangoDB shell, run:
         arangosh
+    EOS
+  end
+
+  def startup_plist
+    return <<-EOS
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>KeepAlive</key>
+    <true/>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>#{HOMEBREW_PREFIX}/sbin/arangod</string>
+      <string>-c</string>
+      <string>#{etc}/arangodb/arangod.conf</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>UserName</key>
+    <string>#{`whoami`.chomp}</string>
+  </dict>
+</plist>
     EOS
   end
 end
