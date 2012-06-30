56be220b3207fcf9b1ecfb1e268362ed574e792f
Wei Dai <x@wei23.net>
2012-06-29 20:39:56 -0700

    Fasd 0.7.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/fasd.rb
+++ Library/Formula/fasd.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fasd < Formula
   homepage 'https://github.com/clvv/fasd'
-  url 'https://github.com/clvv/fasd/tarball/0.5.5'
-  sha1 '8ce380ab9e04b4cc14e79557fbb0e7b0ef4b61f7'
+  url 'https://github.com/clvv/fasd/tarball/0.7.0'
+  sha1 'afc45b3c9dd29b20dbe80629d0774de4e5c1fc2b'
 
   def install
     bin.install 'fasd'
