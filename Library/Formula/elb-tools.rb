14bed21024f71ffa633ff98e872696773255e652
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2012-07-01 17:22:07 -0300

    Correction: homepage url
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/elb-tools.rb
+++ Library/Formula/elb-tools.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class ElbTools < AmazonWebServicesFormula
-  homepage 'http://developer.amazonwebservices.com/connect/entry.jspa?categoryID=251&externalID=2536'
+  homepage 'http://aws.amazon.com/developertools/2536'
   url 'http://ec2-downloads.s3.amazonaws.com/ElasticLoadBalancing.zip'
   version '1.0.17.0'
   sha1 '0546f9dde4a98e2371cf7a03b419bab09254f99c'
