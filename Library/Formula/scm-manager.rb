3eaf85cb39a4a0d3261c19fe1265a6a6f08027e2
Sebastian Sdorra <s.sdorra@ostfalia.de>
2012-07-01 16:49:18 +0200

    scm-manager 1.17
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/scm-manager.rb
+++ Library/Formula/scm-manager.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class ScmManagerCliClient < Formula
   homepage 'http://www.scm-manager.org'
-  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/clients/scm-cli-client/1.16/scm-cli-client-1.16-jar-with-dependencies.jar'
-  version '1.16'
-  md5 'eb8b13be5bd101343a0e9f81a7490fb9'
+  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/clients/scm-cli-client/1.17/scm-cli-client-1.17-jar-with-dependencies.jar'
+  version '1.17'
+  md5 'c54bfac927defd1c62e102293d468972'
 end
 
 class ScmManager < Formula
   homepage 'http://www.scm-manager.org'
-  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/scm-server/1.16/scm-server-1.16-app.tar.gz'
-  version '1.16'
-  md5 '2a6433ec4ce630966b18ac16b8220f56'
+  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/scm-server/1.17/scm-server-1.17-app.tar.gz'
+  version '1.17'
+  md5 '05fda4b81218f54159c24804e61a9487'
 
   skip_clean :all
 
