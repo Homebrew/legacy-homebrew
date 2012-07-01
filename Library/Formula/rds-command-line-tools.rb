fa4a31028826486686846ac6d70cb89ad0b407fb
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2012-07-01 16:28:13 -0300

    Updated RDS Command Line Toolkit to 1.8.002
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rds-command-line-tools.rb
+++ Library/Formula/rds-command-line-tools.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RdsCommandLineTools < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/2928'
-  url      'http://s3.amazonaws.com/rds-downloads/RDSCli-1.6.001.zip'
-  md5      '7625dea485b3eaa7c28c57bb32d591ff'
+  url      'http://s3.amazonaws.com/rds-downloads/RDSCli-1.8.002.zip'
+  md5      'e695e43a1bd3b124f729ffd98959ecf9'
 
   def install
     standard_install
