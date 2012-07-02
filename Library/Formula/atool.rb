fce54e7a23a0b476f93b8635e6bb7975a95bb5a0
Misty De Meo <mistydemeo@gmail.com>
2012-07-01 21:37:11 -0500

    atool: no longer requires gnu-sed
    Fixes #13151.

--- Library/Formula/atool.rb
+++ Library/Formula/atool.rb
@@ -5,12 +5,8 @@ class Atool < Formula
   url 'http://savannah.nongnu.org/download/atool/atool-0.39.0.tar.gz'
   md5 'e0aa006decbc6484b9dba2879f1bb9f0'
 
-  depends_on "gnu-sed"
-
   def install
     system "./configure", "--prefix=#{prefix}"
-    # OS X sed doesn't work; use GNU sed
-    inreplace 'Makefile', 'sed -r', '#{HOMEBREW_PREFIX}/bin/gsed'
     system "make install"
   end
 end
