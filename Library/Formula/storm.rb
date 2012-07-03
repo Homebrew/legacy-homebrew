67521a4810edf397a3ef56b09851ec19b8c935a9
Simon Pantzare <simon@pewpewlabs.com>
2012-07-03 09:34:16 +0200

    Storm 0.7.1 --> 0.7.4

--- Library/Formula/storm.rb
+++ Library/Formula/storm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Storm < Formula
   homepage 'https://github.com/nathanmarz/storm/wiki'
-  url 'https://github.com/downloads/nathanmarz/storm/storm-0.7.1.zip'
-  md5 '83dc4d18439ab4e5da230e7e66f98a7a'
+  url 'https://github.com/downloads/nathanmarz/storm/storm-0.7.4.zip'
+  md5 '4d00cc826443fc2fc76514f3b688d656'
 
   def install
     libexec.install Dir['*']
