7c857b6bbff3520e6f77fffa20c9c39d0d4b73e9
Alexander Surma <surma@surmair.de>
2012-07-04 00:09:03 +0200

    wrk-trello: Write wrk formula

---
+++ Library/Formula/wrk-trello.rb
@@ -0,0 +1,29 @@
+require 'formula'
+
+class WrkTrello < Formula
+  homepage 'https://github.com/blangel/wrk'
+  url 'https://github.com/downloads/blangel/wrk/wrk.tar'
+  sha1 '61f278d49fb44a09e7be884f99ef22c58d2b525c'
+  version '1.0'
+
+
+  def install
+    bin.install 'bin/wrk'
+    # wrk script expects the jars to be inside the lib folder, so
+    # `lib` inside `libexec` it is.
+    libexec.install 'lib'
+  end
+
+  def caveats
+    <<-EOS.undent
+    You should set WRK_HOME and PLY_HOME to
+    #{libexec}
+
+    To get your token go here:
+    https://trello.com/1/authorize?key=8d56bbd601877abfd13150a999a840d0&name=Wrk&expiration=never&response_type=token&scope=read,write
+    and save it to ~/.wrk/token
+    (Start `wrk` for more information)
+    EOS
+  end
+end
+
