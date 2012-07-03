71847b6ae9334e94a7158148e16a7d0a68b9b131
Misty De Meo <mistydemeo@gmail.com>
2012-07-03 09:27:13 -0500

    libgsf 1.14.23

--- Library/Formula/libgsf.rb
+++ Library/Formula/libgsf.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Libgsf < Formula
-  url 'http://ftp.gnome.org/pub/GNOME/sources/libgsf/1.14/libgsf-1.14.21.tar.bz2'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/libgsf/1.14/libgsf-1.14.23.tar.xz'
   homepage 'http://projects.gnome.org/gnumeric/'
-  sha256 'eef0a9d6eca4e6af6c16b208947e3c958c428b94d22792bdd0b80c08a4b301db'
+  sha256 'bfc1c6178f5319d5e6d854c380ce26542f9a103a5ff31c9d25a834e0be52fb17'
 
+  depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
   depends_on 'glib'
