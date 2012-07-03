648e8c0052b819251bfaaa77483950ce3dc3c908
Alexander Surma <surma@surmair.de>
2012-07-04 00:38:46 +0200

    wrk-trello: Wrap script for WRK_HOME

--- Library/Formula/wrk-trello.rb
+++ Library/Formula/wrk-trello.rb
@@ -6,17 +6,21 @@ class WrkTrello < Formula
   sha1 '61f278d49fb44a09e7be884f99ef22c58d2b525c'
   version '1.0'
 
+  def script; <<-EOS.undent
+    #!/bin/sh
+
+    export WRK_HOME=#{libexec}
+    #{libexec}/bin/wrk
+    EOS
+  end
 
   def install
     libexec.install Dir['*']
-    bin.install_symlink "#{libexec}/bin/wrk"
+    (bin+'wrk').write script
   end
 
   def caveats
     <<-EOS.undent
-    You should set WRK_HOME to
-    #{libexec}
-
     To get your token go here:
     https://trello.com/1/authorize?key=8d56bbd601877abfd13150a999a840d0&name=Wrk&expiration=never&response_type=token&scope=read,write
     and save it to ~/.wrk/token
