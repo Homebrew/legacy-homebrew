c45be8a7a0dcff64ee623dea8081ee64b791027c
Ben Couldrey <ben@unixben.com>
2012-07-05 14:54:38 +1000

    OpenConnect 4.03
    * now includes vpnc-script
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/openconnect.rb
+++ Library/Formula/openconnect.rb
@@ -1,15 +1,38 @@
 require 'formula'
 
+class VpncScript < Formula
+  url 'http://git.infradead.org/users/dwmw2/vpnc-scripts.git/blob_plain/HEAD:/vpnc-script'
+  md5 '7a51184f883bba826615e85853e6d30a'
+end
+
 class Openconnect < Formula
-  url 'ftp://ftp.infradead.org/pub/openconnect/openconnect-2.26.tar.gz'
   homepage 'http://www.infradead.org/openconnect.html'
-  md5 'e3c7605fed128efe39c2eb9400af6765'
+  url 'ftp://ftp.infradead.org/pub/openconnect/openconnect-4.03.tar.gz'
+  sha1 '2d071ea714481a201ff1659e2041c56d140475d5'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gettext'
 
   def install
-    inreplace 'Makefile' do |s|
-      s.gsub! '$(DESTDIR)/usr/bin', "$(DESTDIR)#{bin}"
-      s.gsub! '$(DESTDIR)/usr/libexec', "$(DESTDIR)#{libexec}"
-    end
+    VpncScript.new.brew { etc.install Dir['*'] }
+    chmod 0755, "#{etc}/vpnc-script"
+
+    args = %W[
+      --prefix=#{prefix}
+      --sbindir=#{bin}
+      --localstatedir=#{var}
+      --with-vpnc-script=#{etc}/vpnc-script
+    ]
+
+    system "./configure", *args
     system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    OpenConnect requires the use of a TUN/TAP driver.
+
+    You can download one at http://tuntaposx.sourceforge.net/
+    and install it prior to running OpenConnect.
+    EOS
   end
 end
