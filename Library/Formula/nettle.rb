e6f454d3c29bb189e215508cc7a693801040067e
Jack Nagel <jacknagel@gmail.com>
2012-07-07 12:41:01 -0500

    nettle 2.5
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/nettle.rb
+++ Library/Formula/nettle.rb
@@ -1,25 +1,16 @@
 require 'formula'
 
 class Nettle < Formula
-  url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.4.tar.gz'
   homepage 'http://www.lysator.liu.se/~nisse/nettle/'
-  md5 '450be8c4886d46c09f49f568ad6fa013'
+  url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.5.tar.gz'
+  sha256 '4aa142e908b0053082d7729fc0155dc90dbe71c8ef5f50f30034805ed5c92c72'
 
   depends_on 'gmp'
 
-  def patches
-    # patch from MacPorts to sort out really ugly dylib mess
-    { :p0 => "https://trac.macports.org/export/85828/trunk/dports/devel/nettle/files/patch-configure.diff" }
-  end
-
   def install
-    ENV.universal_binary
-    ENV['DYLD_LIBRARY_PATH'] = lib # otherwise 'make check' fails
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--enable-shared",
-                          "--disable-assembler"
+                          "--enable-shared"
     system "make"
     system "make install"
     system "make check"
