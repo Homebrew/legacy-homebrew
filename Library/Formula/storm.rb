7ee45ee752e1775d46ea9d5b7f60fbbf94d027c4
Adam Vandenberg <flangy@gmail.com>
2012-07-06 19:49:51 -0700

    storm: use sha1 and simplify install

--- Library/Formula/storm.rb
+++ Library/Formula/storm.rb
@@ -3,10 +3,10 @@ require 'formula'
 class Storm < Formula
   homepage 'https://github.com/nathanmarz/storm/wiki'
   url 'https://github.com/downloads/nathanmarz/storm/storm-0.7.4.zip'
-  md5 '4d00cc826443fc2fc76514f3b688d656'
+  sha1 '1ee4ec917c52a49d8cc11b4d2e8b41b79c98f97a'
 
   def install
     libexec.install Dir['*']
-    bin.install_symlink Dir["#{libexec}/bin/storm"]
+    bin.install_symlink libexec/"bin/storm"
   end
 end
