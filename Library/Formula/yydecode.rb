036bb26724aa7515e579c028bf02be67e99ec543
James Fargher <proglottis@nothing.co.nz>
2012-07-08 14:33:47 +1200

    yydecode 0.2.10

---
+++ Library/Formula/yydecode.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Yydecode < Formula
+  homepage 'http://yydecode.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/yydecode/yydecode/0.2.10/yydecode-0.2.10.tar.gz'
+  sha1 '27b1e6a3c6f8fa92a487a517fdb05c4e3e0c454a'
+
+  depends_on 'gettext'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make install"
+  end
+
+  def test
+    system "yydecode --version"
+  end
+end
