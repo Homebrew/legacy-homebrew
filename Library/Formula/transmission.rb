32da4ef11bf98f381209d0699da70b1076eef367
Chad Catlett <chad@catlett.info>
2012-07-10 08:09:04 -0500

    Transmission 2.60
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/transmission.rb
+++ Library/Formula/transmission.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Transmission < Formula
   homepage 'http://www.transmissionbt.com/'
-  url 'http://download.transmissionbt.com/files/transmission-2.52.tar.bz2'
-  sha1 'b9cb4d2cf50e8a476dab638d60ec9bee38b04d49'
+  url 'http://download.transmissionbt.com/files/transmission-2.60.tar.bz2'
+  sha1 '1dfc64ff56f954d37b3cca4aa067f067e0fa9744'
 
   depends_on 'pkg-config' => :build # So it will find system libcurl
   depends_on 'libevent'
