13749049d14cadfb9f1e15bcc771e75b7a7b41e1
SHIMADA Koji <koji.shimada@enishi-tech.com>
2012-07-13 16:56:37 +0900

    Use patche on upstream

--- Library/Formula/cutter.rb
+++ Library/Formula/cutter.rb
@@ -12,7 +12,7 @@ class Cutter < Formula
 
   def patches
     # see https://github.com/mxcl/homebrew/pull/11163#issuecomment-4689357
-    DATA
+    "https://github.com/clear-code/cutter/commit/46f985153f465ae22ce8acdde5ec95c3d4361c83.diff"
   end
 
   def install
@@ -23,57 +23,3 @@ class Cutter < Formula
     system "make install"
   end
 end
-
-__END__
---- a/cppcutter/cppcut-assertions-helper.h	2011-12-31 02:02:52.000000000 -0800
-+++ b/cppcutter/cppcut-assertions-helper.h	2012-03-25 23:57:16.000000000 -0700
-@@ -54,24 +54,6 @@
-                       const char *expression_expected,
-                       const char *expression_actual);
- 
--    template <class Type> void assert_equal(const Type *expected,
--                                            const Type *actual,
--                                            const char *expression_expected,
--                                            const char *expression_actual)
--    {
--        assert_equal_reference(expected, actual,
--                               expression_expected, expression_actual);
--    };
--
--    template <class Type> void assert_equal(const Type& expected,
--                                            const Type& actual,
--                                            const char *expression_expected,
--                                            const char *expression_actual)
--    {
--        assert_equal_reference(expected, actual,
--                               expression_expected, expression_actual);
--    };
--
-     template <class Type> void assert_equal_reference(
-         const Type& expected, const Type& actual,
-         const char *expression_expected, const char *expression_actual)
-@@ -95,6 +77,24 @@
-         }
-     }
- 
-+    template <class Type> void assert_equal(const Type *expected,
-+                                            const Type *actual,
-+                                            const char *expression_expected,
-+                                            const char *expression_actual)
-+    {
-+        assert_equal_reference(expected, actual,
-+                               expression_expected, expression_actual);
-+    };
-+
-+    template <class Type> void assert_equal(const Type& expected,
-+                                            const Type& actual,
-+                                            const char *expression_expected,
-+                                            const char *expression_actual)
-+    {
-+        assert_equal_reference(expected, actual,
-+                               expression_expected, expression_actual);
-+    };
-+
-     CPPCUT_DECL
-     void assert_not_equal(char *expected, char *actual,
-                           const char *expression_expected,
