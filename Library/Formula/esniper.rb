01939192710f852a581720d15ecb2465bbe61243
Thomas Pani <thomas.pani@gmail.com>
2012-07-14 20:39:54 +0200

    esniper 2.27

--- Library/Formula/esniper.rb
+++ Library/Formula/esniper.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Esniper < Formula
-  url 'http://downloads.sourceforge.net/project/esniper/esniper/2.26.0/esniper-2-26-0.tgz'
+  url 'http://downloads.sourceforge.net/project/esniper/esniper/2.27.0/esniper-2-27-0.tgz'
   homepage 'http://sourceforge.net/projects/esniper/'
-  md5 '5ea4ae725691fd2178abebb2ba3ee516'
+  md5 '673a014f78402c2af1f2d42b3990ed9d'
+  version '2.27'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
