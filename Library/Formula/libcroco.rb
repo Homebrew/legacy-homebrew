e848732b31d3ce578af3eb97442d08cd670df6f3
nibbles 2bits <nibbles@speakeasy.net>
2012-07-14 13:50:36 -0700

    libcroco 0.6.5
    Add `--disable-Bsymbolic` to stop an unrecognized flag link error.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libcroco.rb
+++ Library/Formula/libcroco.rb
@@ -1,16 +1,19 @@
 require 'formula'
 
 class Libcroco < Formula
-  url 'ftp://ftp.gnome.org/pub/GNOME/sources/libcroco/0.6/libcroco-0.6.2.tar.bz2'
-  md5 '1429c597aa4b75fc610ab3a542c99209'
   homepage 'http://www.freespiders.org/projects/libcroco/'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/libcroco/0.6/libcroco-0.6.5.tar.xz'
+  sha256 '2c6959c3644e889264a61c35ddf17401c86943681d4fe3c1682ecd9acabda7e3'
 
   depends_on 'pkg-config' => :build
+  depends_on 'xz' => :build
   depends_on 'glib'
   depends_on 'intltool'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-    system "make install"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--disable-Bsymbolic"
+    system 'make install'
   end
 end
