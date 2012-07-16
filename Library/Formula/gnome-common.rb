f6d20be545a266404de642ee8c7c08d94ee23103
nibbles 2bits <nibbles@speakeasy.net>
2012-07-15 20:50:46 -0700

    gnome-common 3.4.0.1
    Upgrade gnome-common to version 3.4.0.1.
    Add a dep on `xz`.
    Add a patch to help `gnome-autogen.sh` to accept automake-1.12
    as being greater than 1.09.
    Tested on Lion with clang and llvm from XCode-4.3.3.

--- Library/Formula/gnome-common.rb
+++ Library/Formula/gnome-common.rb
@@ -1,9 +1,17 @@
 require 'formula'
 
 class GnomeCommon < Formula
-  url 'http://ftp.gnome.org/pub/gnome/sources/gnome-common/2.28/gnome-common-2.28.0.tar.gz'
   homepage 'http://git.gnome.org/browse/gnome-common/'
-  md5 'c85414eb4538961973f9f68fcdb96c58'
+  url 'http://ftp.gnome.org/pub/gnome/sources/gnome-common/3.4/gnome-common-3.4.0.1.tar.xz'
+  sha256 '3d92a5d1dae550c409f644d87a4bba17342e14fb11ce8b1e58757ce35f3a46f7'
+
+  depends_on 'xz' => :build
+
+  def patches
+    # Make gnome-autogen.sh accept our automake-1.12.x as >= 1.9.
+    # Remove when at version >= 3.4.0.2
+    'http://git.gnome.org/browse/gnome-common/patch/?id=1fed4ee7015b89a1ac4c4a535aeb753e820e4970'
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
