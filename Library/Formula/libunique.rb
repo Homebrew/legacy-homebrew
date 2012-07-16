d03023a908c04659bca573433b53c023d283c98f
nibbles 2bits <nibbles@speakeasy.net>
2012-07-15 20:48:32 -0700

    libunique 3.0.2
    Upgrade libunique to version 3.0.2.
    Add a flag to disable gtk docs.
    Change the gtk dep to gtk+3.
    Tested with clang and llvm from XCode-4.3.3.
    
    Closes #13421.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libunique.rb
+++ Library/Formula/libunique.rb
@@ -2,21 +2,23 @@ require 'formula'
 
 class Libunique < Formula
   homepage 'http://live.gnome.org/LibUnique'
-  url 'ftp://ftp.gnome.org/pub/GNOME/sources/libunique/1.1/libunique-1.1.6.tar.bz2'
-  sha256 'e5c8041cef8e33c55732f06a292381cb345db946cf792a4ae18aa5c66cdd4fbb'
-  head 'git://git.gnome.org/unique'
+  url 'ftp://ftp.gnome.org/pub/GNOME/sources/libunique/3.0/libunique-3.0.2.tar.bz2'
+  sha256 '50269a87c7aabf1e25f01b3bbb280133138ffd7b6776289894c614a4b6ca968d'
 
   depends_on 'pkg-config' => :build
   depends_on 'dbus-glib'
-  depends_on 'gtk+'
+  depends_on 'gtk+3'
+  depends_on :x11
 
   def install
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--enable-introspection=no",
-            "--enable-maintainer-flags=no",
-            "--prefix=#{prefix}"]
-
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-introspection
+      --disable-maintainer-flags
+      --disable-gtk-doc-html
+      --prefix=#{prefix}
+    ]
     system "./configure", *args
     system "make install"
   end
