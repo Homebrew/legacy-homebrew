4237ea1c2c4c10a4c5288d11c0f0311e97f30473
Jack Nagel <jacknagel@gmail.com>
2012-07-17 12:50:54 -0500

    readline 6.2.4
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/readline.rb
+++ Library/Formula/readline.rb
@@ -5,7 +5,7 @@ class Readline < Formula
   url 'http://ftpmirror.gnu.org/readline/readline-6.2.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz'
   sha256 '79a696070a058c233c72dd6ac697021cc64abd5ed51e59db867d66d196a89381'
-  version '6.2.2'
+  version '6.2.4'
 
   keg_only <<-EOS
 OS X provides the BSD libedit library, which shadows libreadline.
@@ -16,7 +16,9 @@ EOS
   def patches
     {:p0 => [
         "http://ftpmirror.gnu.org/readline/readline-6.2-patches/readline62-001",
-        "http://ftpmirror.gnu.org/readline/readline-6.2-patches/readline62-002"
+        "http://ftpmirror.gnu.org/readline/readline-6.2-patches/readline62-002",
+        "http://ftpmirror.gnu.org/readline/readline-6.2-patches/readline62-003",
+        "http://ftpmirror.gnu.org/readline/readline-6.2-patches/readline62-004"
       ]}
   end
 
