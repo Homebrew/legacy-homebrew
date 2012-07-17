8176e36ea8e39e524364e1e5c09f8a81310bf475
Adam Vandenberg <flangy@gmail.com>
2012-07-17 09:40:40 -0700

    tcpflow 1.2.7

--- Library/Formula/tcpflow.rb
+++ Library/Formula/tcpflow.rb
@@ -2,11 +2,8 @@ require 'formula'
 
 class Tcpflow < Formula
   homepage 'https://github.com/simsong/tcpflow'
-  url 'https://github.com/downloads/simsong/tcpflow/tcpflow-1.2.6.tar.gz'
-  sha1 '4267c491cd5f944f8deba727aa7870ced1ad2224'
-
-  # Patch from MacPorts
-  def patches; DATA; end
+  url 'https://github.com/downloads/simsong/tcpflow/tcpflow-1.2.7.tar.gz'
+  sha1 'e7e71a34afb4d557ebe80e2d589f00d4afd38be4'
 
   def copy_libtool_files!
     if MacOS.xcode_version >= "4.3"
@@ -28,20 +25,3 @@ class Tcpflow < Formula
     system "make install"
   end
 end
-
-__END__
---- a/src/tcpip.cpp  
-+++ b/src/tcpip.cpp
-@@ -541,6 +541,12 @@ unsigned int tcpdemux::get_max_fds(void)
- 	    perror("getrlimit");
- 	    exit(1);
- 	}
-+ 
-+#if defined(__APPLE__)
-+	if (limit.rlim_max > OPEN_MAX) {
-+		limit.rlim_max = OPEN_MAX;
-+	}
-+#endif
- 
- 	/* set the current to the maximum or specified value */
- 	if (max_desired_fds) limit.rlim_cur = max_desired_fds;
