570380dba12678247c8bca7dd9621c00894ff131
limed <limed@mozilla.com>
2012-07-17 17:10:28 -0700

    Path for trash patch and pgp_verbose_mime was pointing to a url that no longer exists. Updated the URL in the formula to point to the newer location on Debians' website
    Have tested it out and the compile worked

--- Library/Formula/mutt.rb
+++ Library/Formula/mutt.rb
@@ -22,10 +22,10 @@ class Mutt < Formula
   def patches
     urls = [
       ['--sidebar-patch', 'https://raw.github.com/nedos/mutt-sidebar-patch/master/mutt-sidebar.patch'],
-      ['--trash-patch', 'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-5/features/trash-folder'],
+      ['--trash-patch', 'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.1/features/trash-folder'],
       ['--ignore-thread-patch', 'http://ben.at.tanjero.com/patches/ignore-thread-1.5.21.patch'],
       ['--pgp-verbose-mime-patch',
-          'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-5/features-old/patch-1.5.4.vk.pgp_verbose_mime'],
+          'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.1/features-old/patch-1.5.4.vk.pgp_verbose_mime'],
     ]
 
     p = []
