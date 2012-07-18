be69a1f09cd3c851ae953191b3f32a623bdb4913
Jack Nagel <jacknagel@gmail.com>
2012-07-18 03:22:00 -0500

    Automatically append shell completion caveats
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -158,6 +158,22 @@ class FormulaInstaller
       check_infopages
       check_m4
     end
+
+    keg = Keg.new(f.prefix)
+
+    if keg.completion_installed? :bash
+      ohai 'Caveats', <<-EOS.undent
+        Bash completion has been installed to:
+          #{HOMEBREW_PREFIX}/etc/bash_completion.d
+        EOS
+    end
+
+    if keg.completion_installed? :zsh
+      ohai 'Caveats', <<-EOS.undent
+        zsh completion has been installed to:
+          #{HOMEBREW_PREFIX}/share/zsh/site-functions
+        EOS
+    end
   end
 
   def finish
