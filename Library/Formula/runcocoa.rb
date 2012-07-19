47029abe778b2334a2a90ad38a7c38b14c81a5d2
Hugo Ferreira <hugo@ferreira.cc>
2012-07-19 02:18:05 +0200

    runcocoa 20120108
    - Make commands for gcc and llvm-gcc version agnostic
    - Do not hardcode the path of gcc and llvm-gcc
    - Added '-nomain' parameter to add your own main routine, for more advanced uses (eg. adding classes)
    
    Closes #13483.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/runcocoa.rb
+++ Library/Formula/runcocoa.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Runcocoa < Formula
   homepage 'https://github.com/michaeltyson/Commandline-Cocoa'
-  url 'https://github.com/michaeltyson/Commandline-Cocoa/tarball/e64b3666bca94c501c88'
-  md5 'bc396ac6ccab09e2f978b81771f13e3b'
-  version 'a'
+  url 'https://github.com/michaeltyson/Commandline-Cocoa/tarball/834f73b4b5d0d2be0d336c9869973f5f0db55949'
+  md5 'ae9f074333e9b9e21c346064dc03428c'
+  version '20120108'
 
   def install
     bin.install 'runcocoa.sh' => 'runcocoa'
