93a28f5fc2f5d40a838bd9c55d1cc6c6d4b4e992
Misty De Meo <mistydemeo@gmail.com>
2012-07-18 22:19:47 -0500

    runcocoa: use sha1

--- Library/Formula/runcocoa.rb
+++ Library/Formula/runcocoa.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Runcocoa < Formula
   homepage 'https://github.com/michaeltyson/Commandline-Cocoa'
   url 'https://github.com/michaeltyson/Commandline-Cocoa/tarball/834f73b4b5d0d2be0d336c9869973f5f0db55949'
-  md5 'ae9f074333e9b9e21c346064dc03428c'
+  sha1 '76f95d2b0f893ff635e0ff2487ffc26846d995f3'
   version '20120108'
 
   def install
