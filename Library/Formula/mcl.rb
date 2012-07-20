bd94d157bd56561f5486fbe0d99338a2865612ed
Tamas Nepusz <ntamas@gmail.com>
2012-07-20 17:16:32 +0200

    mcl 12-135

--- Library/Formula/mcl.rb
+++ Library/Formula/mcl.rb
@@ -1,12 +1,17 @@
 require 'formula'
 
 class Mcl < Formula
-  version '10-201'
-  url 'http://micans.org/mcl/src/mcl-10-201.tar.gz'
+  version '12-135'
+  url 'http://micans.org/mcl/src/mcl-12-135.tar.gz'
   homepage 'http://micans.org/mcl'
-  md5 '9e8aecb9011560b9c3e55fe0f2d1e791'
+  md5 '99bb9a5ef5f54749d695a7319c1cb40c'
 
   def install
+    # Force the compiler to run in C89 mode because one of the source
+    # files uses "restrict" as a variable name and this is a restricted
+    # keyword in C99
+    ENV.append_to_cflags '-std=c89'
+
     bin.mkpath
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
