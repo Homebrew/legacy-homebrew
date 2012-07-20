4fabe0b7ca2a8599eef310b3b1e96bc2a01d93e2
Will Ross <wross@cmf.nrl.navy.mil>
2012-07-20 13:22:27 -0400

    p0f: Update to version 3.05b

--- Library/Formula/p0f.rb
+++ Library/Formula/p0f.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class P0f < Formula
   homepage 'http://lcamtuf.coredump.cx/p0f3/'
-  url 'http://lcamtuf.coredump.cx/p0f3/releases/p0f-3.03b.tgz'
-  md5 '034d068deb68badfbede6dcc89cc80cf'
+  url 'http://lcamtuf.coredump.cx/p0f3/releases/p0f-3.05b.tgz'
+  md5 'edbc4b135b2646db3227a441268fd2e2'
 
   def install
     inreplace "config.h", "p0f.fp", "#{etc}/p0f/p0f.fp"
