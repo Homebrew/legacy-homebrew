6a32c9369004d89c9bffeff2aaf2226ecfcab4da
Rianol Jou <ar3sti@gmail.com>
2012-07-23 13:29:31 +0800

    tmux: Correct japanese sonant mark display
    Closes #13544.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -15,12 +15,17 @@ class Tmux < Formula
     depends_on :libtool
   end
 
-  # This patch adds the implementation of osdep_get_cwd for Darwin platform,
-  # so that tmux can get current working directory correctly under Mac OS.
-  # NOTE: it applies to 1.6 only, and should be removed when 1.7 is out.
-  #       (because it has been merged upstream)
   def patches
-   DATA if build.stable?
+    # Fix for Japanese characters. See:
+    #   http://sourceforge.net/tracker/?func=detail&aid=3566884&group_id=200378&atid=973264
+    p = ['http://sourceforge.net/tracker/download.php?group_id=200378&atid=973264&file_id=453002&aid=3566884']
+    # This patch adds the implementation of osdep_get_cwd for Darwin platform,
+    # so that tmux can get current working directory correctly under Mac OS.
+    # NOTE: it applies to 1.6 only, and should be removed when 1.7 is out.
+    #       (because it has been merged upstream)
+    p << DATA if build.stable?
+
+    p
   end
 
   def install
