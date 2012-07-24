3be4092b1ff95963d5ab21baa26ef8de943b973c
taskwarrior <support@taskwarrior.org>
2012-07-24 02:25:58 +0200

    task 2.1.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Task < Formula
   homepage 'http://www.taskwarrior.org/'
-  url 'http://www.taskwarrior.org/download/task-2.0.0.tar.gz'
-  sha1 'dc587363fbdc1dcac7f7e07b1bccfd1fb56b2435'
+  url 'http://www.taskwarrior.org/download/task-2.1.0.tar.gz'
+  sha1 'fd1120db56bf44e0d84191d5dce29a6d9d872a8a'
 
   depends_on "cmake" => :build
 
@@ -12,7 +12,7 @@ class Task < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make install"
-    (etc+'bash_completion.d').install 'scripts/bash/task_completion.sh'
+    (etc+'bash_completion.d').install 'scripts/bash/task.sh'
     (share+'zsh/site-functions').install   'scripts/zsh/_task'
   end
 
