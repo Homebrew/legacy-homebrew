b51e88c06a41e3e0e4017d4c38db8e86b1b863cb
Gints Murans <gm@gm.lv>
2012-07-25 00:03:16 +0300

    New version of FreeImage

--- Library/Formula/freeimage.rb
+++ Library/Formula/freeimage.rb
@@ -9,10 +9,10 @@ class FreeimageHttpDownloadStrategy < CurlDownloadStrategy
 end
 
 class Freeimage < Formula
-  url 'http://downloads.sourceforge.net/project/freeimage/Source%20Distribution/3.15.1/FreeImage3151.zip',
+  url 'http://downloads.sourceforge.net/project/freeimage/Source%20Distribution/3.15.3/FreeImage3153.zip',
         :using => FreeimageHttpDownloadStrategy
-  version '3.15.1'
-  md5 '450d2ff278690b0d1d7d7d58fad083cc'
+  version '3.15.3'
+  md5 '6c305b4086500ff8541c596ea6fc7cd7'
   homepage 'http://sf.net/projects/freeimage'
 
   def options
@@ -33,9 +33,11 @@ class Freeimage < Formula
 end
 
 __END__
---- old/Makefile.gnu	2010-12-06 23:37:20.000000000 -0800
-+++ new/Makefile.gnu	2011-10-19 13:42:59.000000000 -0700
-@@ -5,8 +5,9 @@
+diff --git a/Makefile.fip b/Makefile.fip
+index 1c2dee4..5ca28bd 100755
+--- a/Makefile.fip
++++ b/Makefile.fip
+@@ -5,8 +5,9 @@ include fipMakefile.srcs
  
  # General configuration variables:
  DESTDIR ?= /
@@ -47,86 +49,28 @@ __END__
  
  # Converts cr/lf to just lf
  DOS2UNIX = dos2unix
-@@ -27,11 +28,10 @@
+@@ -27,8 +28,8 @@ endif
  
- TARGET  = freeimage
+ TARGET  = freeimageplus
  STATICLIB = lib$(TARGET).a
 -SHAREDLIB = lib$(TARGET)-$(VER_MAJOR).$(VER_MINOR).so
 -LIBNAME	= lib$(TARGET).so
--VERLIBNAME = $(LIBNAME).$(VER_MAJOR)
-+SHAREDLIB = lib$(TARGET).$(VER_MAJOR).$(VER_MINOR).dylib
++SHAREDLIB = lib$(TARGET)-$(VER_MAJOR).$(VER_MINOR).dylib
 +LIBNAME	= lib$(TARGET).dylib
+ VERLIBNAME = $(LIBNAME).$(VER_MAJOR)
  HEADER = Source/FreeImage.h
--
-+LIBTOOL ?= libtool
- 
- 
- default: all
-@@ -40,7 +40,7 @@
+ HEADERFIP = Wrapper/FreeImagePlus/FreeImagePlus.h
+@@ -40,7 +41,7 @@ all: dist
  
  dist: FreeImage
  	cp *.a Dist
 -	cp *.so Dist
 +	cp *.dylib Dist
  	cp Source/FreeImage.h Dist
+ 	cp Wrapper/FreeImagePlus/FreeImagePlus.h Dist
  
- dos2unix:
-@@ -58,16 +58,14 @@
-	$(AR) r $@ $(MODULES)
- 
- $(SHAREDLIB): $(MODULES)
--	$(CC) -s -shared -Wl,-soname,$(VERLIBNAME) $(LDFLAGS) -o $@ $(MODULES) $(LIBRARIES)
-+	$(CXX) -dynamiclib -install_name $(LIBNAME) -current_version $(VER_MAJOR).$(VER_MINOR) -compatibility_version $(VER_MAJOR) $(LDFLAGS) -o $@ $(MODULES)
-
- install:
-	install -d $(INCDIR) $(INSTALLDIR)
--	install -m 644 -o root -g root $(HEADER) $(INCDIR)
--	install -m 644 -o root -g root $(STATICLIB) $(INSTALLDIR)
--	install -m 755 -o root -g root $(SHAREDLIB) $(INSTALLDIR)
--	ln -sf $(SHAREDLIB) $(INSTALLDIR)/$(VERLIBNAME)
--	ln -sf $(VERLIBNAME) $(INSTALLDIR)/$(LIBNAME)	
--	ldconfig
-+	install -m 644 $(HEADER) $(INCDIR)
-+	install -m 644 $(STATICLIB) $(INSTALLDIR)
-+	install -m 755 $(SHAREDLIB) $(INSTALLDIR)
-+	ln -sf $(SHAREDLIB) $(INSTALLDIR)/$(LIBNAME)
-
- clean:
-	rm -f core Dist/*.* u2dtmp* $(MODULES) $(STATICLIB) $(SHAREDLIB) $(LIBNAME)
---- old/Makefile.fip	2011-10-19 11:20:03.000000000 -0700
-+++ new/Makefile.fip	2011-10-19 11:24:01.000000000 -0700
-@@ -5,8 +5,9 @@
-
- # General configuration variables:
- DESTDIR ?= /
--INCDIR ?= $(DESTDIR)/usr/include
--INSTALLDIR ?= $(DESTDIR)/usr/lib
-+PREFIX ?= /usr/local
-+INCDIR ?= $(DESTDIR)$(PREFIX)/include
-+INSTALLDIR ?= $(DESTDIR)$(PREFIX)/lib
-
- # Converts cr/lf to just lf
- DOS2UNIX = dos2unix
-@@ -28,7 +29,7 @@
- TARGET  = freeimageplus
- STATICLIB = lib$(TARGET).a
- SHAREDLIB = lib$(TARGET)-$(VER_MAJOR).$(VER_MINOR).so
--LIBNAME	= lib$(TARGET).so
-+LIBNAME	= lib$(TARGET).dylib
- VERLIBNAME = $(LIBNAME).$(VER_MAJOR)
- HEADER = Source/FreeImage.h
- HEADERFIP = Wrapper/FreeImagePlus/FreeImagePlus.h
-@@ -40,7 +41,7 @@
-
- dist: FreeImage
-	cp *.a Dist
--	cp *.so Dist
-+	cp *.dylib Dist
-	cp Source/FreeImage.h Dist
-	cp Wrapper/FreeImagePlus/FreeImagePlus.h Dist
-
-@@ -59,14 +60,15 @@
-	$(AR) r $@ $(MODULES)
+@@ -59,14 +60,15 @@ $(STATICLIB): $(MODULES)
+ 	$(AR) r $@ $(MODULES)
  
  $(SHAREDLIB): $(MODULES)
 -	$(CC) -s -shared -Wl,-soname,$(VERLIBNAME) $(LDFLAGS) -o $@ $(MODULES) $(LIBRARIES)
@@ -143,6 +87,68 @@ __END__
 +	install -m 644 $(STATICLIB) $(INSTALLDIR)
 +	install -m 755 $(SHAREDLIB) $(INSTALLDIR)
 +	ln -s $(SHAREDLIB) $(INSTALLDIR)/$(LIBNAME)
+ 
+ clean:
+ 	rm -f core Dist/*.* u2dtmp* $(MODULES) $(STATICLIB) $(SHAREDLIB) $(LIBNAME)
+diff --git a/Makefile.gnu b/Makefile.gnu
+index e6007ae..3336285 100755
+--- a/Makefile.gnu
++++ b/Makefile.gnu
+@@ -5,8 +5,9 @@ include Makefile.srcs
+ 
+ # General configuration variables:
+ DESTDIR ?= /
+-INCDIR ?= $(DESTDIR)/usr/include
+-INSTALLDIR ?= $(DESTDIR)/usr/lib
++PREFIX ?= /usr/local
++INCDIR ?= $(DESTDIR)$(PREFIX)/include
++INSTALLDIR ?= $(DESTDIR)$(PREFIX)/lib
+ 
+ # Converts cr/lf to just lf
+ DOS2UNIX = dos2unix
+@@ -27,11 +28,10 @@ endif
+ 
+ TARGET  = freeimage
+ STATICLIB = lib$(TARGET).a
+-SHAREDLIB = lib$(TARGET)-$(VER_MAJOR).$(VER_MINOR).so
+-LIBNAME	= lib$(TARGET).so
+-VERLIBNAME = $(LIBNAME).$(VER_MAJOR)
++SHAREDLIB = lib$(TARGET).$(VER_MAJOR).$(VER_MINOR).dylib
++LIBNAME	= lib$(TARGET).dylib
+ HEADER = Source/FreeImage.h
+-
++LIBTOOL ?= libtool
+ 
+ 
+ default: all
+@@ -40,7 +40,7 @@ all: dist
+ 
+ dist: FreeImage
+ 	cp *.a Dist
+-	cp *.so Dist
++	cp *.dylib Dist
+ 	cp Source/FreeImage.h Dist
+ 
+ dos2unix:
+@@ -58,16 +58,14 @@ $(STATICLIB): $(MODULES)
+ 	$(AR) r $@ $(MODULES)
+ 
+ $(SHAREDLIB): $(MODULES)
+-	$(CC) -s -shared -Wl,-soname,$(VERLIBNAME) $(LDFLAGS) -o $@ $(MODULES) $(LIBRARIES)
++	$(CXX) -dynamiclib -install_name $(LIBNAME) -current_version $(VER_MAJOR).$(VER_MINOR) -compatibility_version $(VER_MAJOR) $(LDFLAGS) -o $@ $(MODULES)
+ 
+ install:
+ 	install -d $(INCDIR) $(INSTALLDIR)
+-	install -m 644 -o root -g root $(HEADER) $(INCDIR)
+-	install -m 644 -o root -g root $(STATICLIB) $(INSTALLDIR)
+-	install -m 755 -o root -g root $(SHAREDLIB) $(INSTALLDIR)
+-	ln -sf $(SHAREDLIB) $(INSTALLDIR)/$(VERLIBNAME)
+-	ln -sf $(VERLIBNAME) $(INSTALLDIR)/$(LIBNAME)	
+-#	ldconfig
++	install -m 644 $(HEADER) $(INCDIR)
++	install -m 644 $(STATICLIB) $(INSTALLDIR)
++	install -m 755 $(SHAREDLIB) $(INSTALLDIR)
++	ln -sf $(SHAREDLIB) $(INSTALLDIR)/$(LIBNAME)
  
  clean:
  	rm -f core Dist/*.* u2dtmp* $(MODULES) $(STATICLIB) $(SHAREDLIB) $(LIBNAME)
