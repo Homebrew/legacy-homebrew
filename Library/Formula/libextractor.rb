ab798c9cdeb730f7285f0b5dc791bb277e3c75b0
Adam Vandenberg <flangy@gmail.com>
2012-07-25 07:56:15 -0700

    libextractor: add pkg-config dep and comment libtool dep

--- Library/Formula/libextractor.rb
+++ Library/Formula/libextractor.rb
@@ -6,7 +6,12 @@ class Libextractor < Formula
   mirror 'http://ftp.gnu.org/gnu/libextractor/libextractor-0.6.2.tar.gz'
   md5 '4b2af1167061430d58a101d5dfc6b4c7'
 
+  # The version of libtool on Lion is new enough:
+  # $ /usr/bin/libtool -V
+  # Apple Inc. version cctools-822
+  # But compilation fails on Snow Leopard without a newer one.
   depends_on 'libtool' => :build unless MacOS.lion?
+  depends_on 'pkg-config' => :build
 
   def install
     ENV.deparallelize
