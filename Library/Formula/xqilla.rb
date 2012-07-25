507e8180ceef3d02abfec35944bfd28f597b2326
Brett Koonce <koonce@gmail.com>
2012-07-25 18:36:40 -0500

    xqilla 2.3.0
    Closes #13597.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/xqilla.rb
+++ Library/Formula/xqilla.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Xqilla < Formula
-  url 'http://downloads.sourceforge.net/project/xqilla/xqilla/2.2.4/XQilla-2.2.4.tar.gz'
   homepage 'http://xqilla.sourceforge.net/'
-  md5 'a00672133d06772f54f18d0fda304c02'
+  url 'http://downloads.sourceforge.net/project/xqilla/xqilla/2.3.0/XQilla-2.3.0.tar.gz'
+  sha1 'facfd0134652cef7ebbc3c54d61fc248c1610c95'
 
   depends_on 'xerces-c'
 
