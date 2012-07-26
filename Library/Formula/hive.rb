f43791eb757edf68062de68f17f45d8e29f615c7
Brett Koonce <koonce@gmail.com>
2012-07-26 09:34:31 -0500

    hive 0.9
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/hive.rb
+++ Library/Formula/hive.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hive < Formula
   homepage 'http://hive.apache.org'
-  url 'http://www.apache.org/dyn/closer.cgi?path=hive/hive-0.8.0/hive-0.8.0-bin.tar.gz'
-  md5 '9aca92b683da8955aca3beb5a438d2f9'
+  url 'http://www.apache.org/dyn/closer.cgi?path=hive/hive-0.9.0/hive-0.9.0-bin.tar.gz'
+  sha1 '8c8086ff9ec5de5c661d98d926bbedac62c28a61'
 
   depends_on 'hadoop'
 
