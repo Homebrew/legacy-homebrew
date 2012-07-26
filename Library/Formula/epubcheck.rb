6009cece1935c5ec128439a21ade3cb6dc0a4004
DJManas <sourek.petr@gmail.com>
2012-07-26 22:45:29 +0300

    Update Library/Formula/epubcheck.rb
    Only added --devel to install version 3.0 beta 5. I am sorry this is my first commitment and I might do some wrong things ;(

--- Library/Formula/epubcheck.rb
+++ Library/Formula/epubcheck.rb
@@ -5,12 +5,25 @@ class Epubcheck < Formula
   url 'http://epubcheck.googlecode.com/files/epubcheck-1.2.zip'
   sha1 '86036eadad8408070791b3da368958239ed8a410'
 
+  devel do
+    url 'http://epubcheck.googlecode.com/files/epubcheck-3.0b5.zip'
+    sha1 'd5f9d60733f587946fc853f561ca2f13e3b8f83d'
+  end
+
   def install
-    libexec.install "epubcheck-1.2.jar", "lib"
-    (bin/'epubcheck').write <<-EOS.undent
-      #!/bin/sh
-      java -jar "#{libexec}/epubcheck-1.2.jar" "$@"
-    EOS
+    if ARGV.build_devel?
+      libexec.install "epubcheck-3.0b5.jar", "lib"
+      (bin/'epubcheck').write <<-EOS.undent
+        #!/bin/sh
+        java -jar "#{libexec}/epubcheck-3.0b5.jar" "$@"
+      EOS
+    else
+      libexec.install "epubcheck-1.2.jar", "lib"
+      (bin/'epubcheck').write <<-EOS.undent
+        #!/bin/sh
+        java -jar "#{libexec}/epubcheck-1.2.jar" "$@"
+      EOS
+    end
   end
 
   def test
@@ -19,7 +32,7 @@ class Epubcheck < Formula
         epubcheck file.epub
 
       The reply should look like:
-        Epubcheck Version 1.2
+        Epubcheck Version 1.2 or 3.0b5
 
         No errors or warnings detected
     EOS
