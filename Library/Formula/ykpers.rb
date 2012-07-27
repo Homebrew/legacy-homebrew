825a4c367505f6b2a2667456cd46890c49e2d42d
Brett Koonce <koonce@gmail.com>
2012-07-27 00:57:00 -0500

    ykpers 1.7.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ykpers.rb
+++ Library/Formula/ykpers.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ykpers < Formula
   homepage 'http://code.google.com/p/yubikey-personalization/'
-  url 'http://yubikey-personalization.googlecode.com/files/ykpers-1.6.4.tar.gz'
-  sha1 '491af38ed3d91548a4d470b2aa3abf88927effef'
+  url 'http://yubikey-personalization.googlecode.com/files/ykpers-1.7.0.tar.gz'
+  sha1 'de325107c1d6e9d3a61199cad5eeac69b98a36d7'
 
   depends_on 'libyubikey'
 
