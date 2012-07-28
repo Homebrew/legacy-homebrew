3e79919225b569685c6a96c25d0d997b1ff45b80
Brett Koonce <koonce@gmail.com>
2012-07-28 16:08:07 -0500

    ykclient 2.8
    Closes #13693.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ykclient.rb
+++ Library/Formula/ykclient.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Ykclient < Formula
-  url 'http://yubico-c-client.googlecode.com/files/ykclient-2.7.tar.gz'
   homepage 'http://code.google.com/p/yubico-c-client/'
-  md5 'ffaf492498d8d57e87fbbe355d438a91'
+  url 'http://yubico-c-client.googlecode.com/files/ykclient-2.8.tar.gz'
+  sha1 '91aefae12d7e13360d8c03ccd66ef1d26d1b04f9'
 
   def install
     system "./configure", "--prefix=#{prefix}"
