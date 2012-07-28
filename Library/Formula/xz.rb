3c05ab2fd9d969fe9948e56deda288f473cd9fb7
Brett Koonce <koonce@gmail.com>
2012-07-28 16:39:12 -0500

    xz 5.0.4
    Closes #13701.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Xz < Formula
   homepage 'http://tukaani.org/xz/'
-  url 'http://tukaani.org/xz/xz-5.0.3.tar.bz2'
-  sha256 '5a11b9e17bfcda62319c5a8c4a2062dc81607a316d3f6adff89422d81ec1eae9'
+  url 'http://tukaani.org/xz/xz-5.0.4.tar.bz2'
+  sha256 '5cd9b060d3a1ad396b3be52c9b9311046a1c369e6062aea752658c435629ce92'
 
   def options
     [["--universal", "Build a universal binary."]]
