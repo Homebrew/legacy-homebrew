9f77d355a9ee2d4a4696ade3ca1870776473c0b8
Willem Deconinck <wdeconinck@me.com>
2012-07-29 12:43:16 +0200

    parmetis 4.0.2

--- Library/Formula/parmetis.rb
+++ Library/Formula/parmetis.rb
@@ -1,14 +1,28 @@
 require 'formula'
 
 class Parmetis < Formula
-  url 'http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.tar.gz'
+  url 'http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.2.tar.gz'
   homepage 'http://glaros.dtc.umn.edu/gkhome/metis/parmetis/overview'
-  md5 '856dbbd43c12d97cb6a0dc583e6361d5'
+  md5 '0912a953da5bb9b5e5e10542298ffdce'
 
   depends_on 'cmake' => :build
+  depends_on MPIDependency.new(:cc,:cxx)
 
   def install
-    system "make", "config", "prefix=#{prefix}"
-    system 'make install'
+    # Install Metis
+    Dir.chdir 'metis' do
+      # Wrapper around a CMake configuration
+      system "make" , "config" , "shared=1" , "prefix=#{prefix}"
+      system 'make install'
+    end    
+
+    # Install ParMETIS
+    # This flag is set when using clang. But as the mpi wrappers might be wrapping llvm,
+    # and CMake says it can't compile with this, remove it.
+    ENV.remove_from_cflags /-march=native/
+
+    # Wrapper around a CMake configuration
+    system "make" , "config" , "shared=1" , "cc=#{ENV['MPICC']}" , "cxx=#{ENV['MPICXX']}" , "prefix=#{prefix}"
+    system 'make install'    
   end
 end
