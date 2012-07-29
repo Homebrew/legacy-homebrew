20a5fb6eb8ef80788e88705bcf61e6caa8e734f7
Brett Koonce <koonce@gmail.com>
2012-07-29 09:55:04 -0500

    webp 0.1.99
    Closes #13717.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/webp.rb
+++ Library/Formula/webp.rb
@@ -1,15 +1,16 @@
 require 'formula'
 
 class Webp < Formula
-  url 'http://webp.googlecode.com/files/libwebp-0.1.3.tar.gz'
   homepage 'http://code.google.com/speed/webp/'
-  md5 '254d4670e14e9ed881f0536b006ab336'
+  url 'http://webp.googlecode.com/files/libwebp-0.1.99.tar.gz'
+  sha1 '20c4d471601c44c9dcabe116eca3573a0aab5289'
 
   depends_on :x11
   depends_on 'jpeg'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
