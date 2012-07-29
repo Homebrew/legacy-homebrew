e9352491c8b8b08288ab4b87cf52c78eab9d2d01
Adam Vandenberg <flangy@gmail.com>
2012-07-29 09:23:57 -0700

    cpptest: style update

--- Library/Formula/cpptest.rb
+++ Library/Formula/cpptest.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class Cpptest < Formula
-  url 'http://downloads.sourceforge.net/project/cpptest/cpptest/cpptest-1.1.1/cpptest-1.1.1.tar.gz'
   homepage 'http://cpptest.sourceforge.net/'
-  md5 'b50379402d69d40417add19ef88f9938'
+  url 'http://downloads.sourceforge.net/project/cpptest/cpptest/cpptest-1.1.1/cpptest-1.1.1.tar.gz'
+  sha1 '5c791746ecb0f18a5e4f15f8a57bf9e433399ebb'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
   end
