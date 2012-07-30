940af788e1e568bd5c76fd4a5847c25ba0de6cad
Misty De Meo <mistydemeo@gmail.com>
2012-07-30 11:59:00 -0300

    xmlrpc-c: Fix curl #include on 10.8
    curl/types.h has been deprecated for many years, and is an empty file
    in the 10.6 and 10.7 versions of curl. 10.8 upgraded to a version of
    curl which has dropped it entirely, breaking some software which still
    #included the deprecreated header. This patch backports an upstream fix
    which removes the unnecessary #include.
    
    Fixes #13540.

--- Library/Formula/xmlrpc-c.rb
+++ Library/Formula/xmlrpc-c.rb
@@ -5,6 +5,15 @@ class XmlrpcC < Formula
   url 'http://downloads.sourceforge.net/sourceforge/xmlrpc-c/xmlrpc-c-1.16.42.tgz'
   sha1 '7a71fabc652c2848a7226605432a2e420a02dff0'
 
+  def patches
+    # Disable including deprecated curl/types.h, which is missing on 10.8
+    # On 10.6 and 10.7 it's empty, so could probably patch unconditionally
+    # see http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=636457#10
+    if MacOS.mountain_lion?
+      "http://xmlrpc-c.svn.sourceforge.net/viewvc/xmlrpc-c/stable/lib/curl_transport/xmlrpc_curl_transport.c?r1=2115&r2=2150&view=patch"
+    end
+  end
+
   def install
     ENV.deparallelize
     # --enable-libxml2-backend to lose some weight and not statically link in expat
