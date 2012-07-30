f0794528a6ffaf51a2b4f0d6feae195d5d859403
Brett Koonce <koonce@gmail.com>
2012-07-30 17:39:33 -0500

    pdnsd 1.2.9a
    Closes #13795.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pdnsd.rb
+++ Library/Formula/pdnsd.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Pdnsd < Formula
-  url 'http://members.home.nl/p.a.rombouts/pdnsd/releases/pdnsd-1.2.8-par.tar.gz'
-  version '1.2.8-par'
   homepage 'http://members.home.nl/p.a.rombouts/pdnsd/'
-  md5 '779c5d19576e561fbf2455de435e5597'
+  url 'http://members.home.nl/p.a.rombouts/pdnsd/releases/pdnsd-1.2.9a-par.tar.gz'
+  version '1.2.9a-par'
+  sha1 '55d8fd71e2f291a3ead53c8097729fa95264bea9'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
