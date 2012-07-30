3ba580ab0a240260a10774ba43efd130f1226df3
Brett Koonce <koonce@gmail.com>
2012-07-30 11:28:40 -0500

    objective-caml 4.00
    i removed the old devel option and stripped the bottle code

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -2,21 +2,10 @@ require 'formula'
 
 class ObjectiveCaml < Formula
   homepage 'http://caml.inria.fr/ocaml/index.en.html'
-  url 'http://caml.inria.fr/pub/distrib/ocaml-3.12/ocaml-3.12.1.tar.bz2'
-  sha1 '29b44117b116b1a5bc54a8b4514af483793a769f'
+  url 'http://caml.inria.fr/pub/distrib/ocaml-4.00/ocaml-4.00.0.tar.bz2'
+  sha1 '22c0ae84c0049e75ec1577d5c1a26c293bb740b3'
 
   head 'http://caml.inria.fr/svn/ocaml/trunk', :using => :svn
-
-  devel do
-    url 'http://caml.inria.fr/pub/distrib/ocaml-4.00/ocaml-4.00.0+beta2.tar.bz2'
-    sha1 '75fedc849cc1cbfe620f0258c0f6c8214467efb7'
-    version '4.00.0+beta2'
-  end
-
-  bottle do
-    sha1 'e5e28c74b859b8bb15a11f7f2a7a33608671b1b9' => :snowleopard
-    sha1 'f32709be6cba5639a3f7185835963d630d6f8b59' => :lion
-  end
 
   # Don't strip symbols, so dynamic linking doesn't break.
   skip_clean :all
