e12687b7d36c4549ce4cdbb57644712b38844371
Brett Koonce <koonce@gmail.com>
2012-07-31 16:42:28 -0500

    nesc 1.3.4

--- Library/Formula/nesc.rb
+++ Library/Formula/nesc.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Nesc < Formula
-  url 'http://downloads.sourceforge.net/project/nescc/nescc/v1.3.3/nesc-1.3.3.tar.gz'
   homepage 'http://nescc.sourceforge.net/'
-  md5 'f48a8b316de3f75ef1074c5585681f91'
+  url 'http://downloads.sourceforge.net/project/nescc/nescc/v1.3.4/nesc-1.3.4.tar.gz'
+  sha1 '360148ca99a88b628bcd4490ad42a9466490bf4e'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
