3fe2d85f3002acf370164f7cb47a8a0842c787db
Constantime Kim <elegantcoder@gmail.com>
2012-07-31 13:22:12 +0900

    serf: universal support
    Fixes #13226.
    Closes #13813.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/serf.rb
+++ Library/Formula/serf.rb
@@ -5,7 +5,12 @@ class Serf < Formula
   url 'http://serf.googlecode.com/files/serf-1.1.0.tar.bz2'
   sha1 '231af70b7567a753b49df4216743010c193884b7'
 
+  def options
+    [['--universal', 'Builds a universal binary.']]
+  end
+
   def install
+    ENV.universal_binary if ARGV.build_universal?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
