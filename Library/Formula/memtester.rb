84a4a29a3058dfdf365afa78cc11fca8582f0aca
Brett Koonce <koonce@gmail.com>
2012-08-01 15:20:26 -0500

    memtester 4.3.0
    Closes #13861.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/memtester.rb
+++ Library/Formula/memtester.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Memtester < Formula
-  url 'http://pyropus.ca/software/memtester/old-versions/memtester-4.2.2.tar.gz'
   homepage 'http://pyropus.ca/software/memtester/'
-  md5 '0118616cc8860c6b85ef6a1281da1783'
+  url 'http://pyropus.ca/software/memtester/old-versions/memtester-4.3.0.tar.gz'
+  sha1 'eff0e3020a7750bd5452b051526ad6439e92b9cd'
 
   def install
     inreplace "Makefile" do |s|
