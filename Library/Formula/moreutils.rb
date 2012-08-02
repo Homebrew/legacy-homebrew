b579aa8bbd4b2ff1113b746192901fb32a6e7690
Brett Koonce <koonce@gmail.com>
2012-08-01 21:39:06 -0500

    moreutils 0.47

--- Library/Formula/moreutils.rb
+++ Library/Formula/moreutils.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Moreutils < Formula
   homepage 'http://packages.debian.org/unstable/utils/moreutils'
-  url 'http://mirrors.kernel.org/debian/pool/main/m/moreutils/moreutils_0.46.tar.gz'
-  mirror 'http://ftp.us.debian.org/debian/pool/main/m/moreutils/moreutils_0.46.tar.gz'
-  md5 '658f9cc6b6dcc0b50dcff714e13b22dc'
+  url 'http://mirrors.kernel.org/debian/pool/main/m/moreutils/moreutils_0.47.tar.gz'
+  mirror 'http://ftp.us.debian.org/debian/pool/main/m/moreutils/moreutils_0.47.tar.gz'
+  sha1 '0459608fdb37bc59605d7a476717693afd133e00'
 
   def install
     # Building the man pages requires DocBook, so we skip them.
