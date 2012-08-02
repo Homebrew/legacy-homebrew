dc70b1af6481aabe723fc9ac83a1fd2960d96a09
Jon Buckley <jon@jbuckley.ca>
2012-08-02 11:00:22 -0400

    Update opus-tools to v0.1.4
    This release fixes several resampling bugs and greatly increases resampler performance. It also provides working unicode filename and tag support for Windows (Unicode worked for mac and *nix previously).

--- Library/Formula/opus-tools.rb
+++ Library/Formula/opus-tools.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class OpusTools < Formula
   homepage 'http://www.opus-codec.org'
-  url 'http://downloads.xiph.org/releases/opus/opus-tools-0.1.3.tar.gz'
-  sha1 '82f9f2cbabfbfac9bea4354a043a0fcaf2e03a47'
+  url 'http://downloads.xiph.org/releases/opus/opus-tools-0.1.4.tar.gz'
+  sha1 'b4030cd65afa57340b5ae25fe3467808ca1454cf'
 
   head 'https://git.xiph.org/opus-tools.git'
 
