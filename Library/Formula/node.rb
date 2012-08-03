b9a4c011531ed943f13427930ddb84862bc558d5
Juan C. Müller <jcmuller@gmail.com>
2012-08-03 17:33:31 -0400

    node: use https for npm install location
    Closes #13924.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -34,7 +34,7 @@ class Node < Formula
     <<-EOS.undent
       Homebrew has NOT installed npm. We recommend the following method of
       installation:
-        curl http://npmjs.org/install.sh | sh
+        curl https://npmjs.org/install.sh | sh
 
       After installing, add the following path to your NODE_PATH environment
       variable to have npm libraries picked up:
