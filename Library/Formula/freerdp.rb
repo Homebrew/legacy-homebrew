2d4000fe58b877455dfa57940360cfacc6f54713
Mike McQuaid <mike@mikemcquaid.com>
2012-08-03 20:54:43 +0100

    freerdp: cleanup and fix Mountain Lion build.
    Fixes #13353.

--- Library/Formula/freerdp.rb
+++ Library/Formula/freerdp.rb
@@ -3,22 +3,26 @@ require 'formula'
 class Freerdp < Formula
   homepage 'http://www.freerdp.com/'
   url 'https://github.com/FreeRDP/FreeRDP/tarball/1.0.1'
-  md5 '1282189a87893bf196da20382e45f6c1'
+  sha1 '93a7ffaa0e0942f3446810777154bf78053cc66c'
 
   head 'https://github.com/FreeRDP/FreeRDP.git'
 
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
 
-  # Fixes clang build problems
-  # Already upstream, check for removal on 1.1 release:
-  # https://github.com/FreeRDP/FreeRDP/pull/544
   def patches
-    'https://github.com/bmiklautz/FreeRDP/commit/1d32894775edd1bacdbcb4b6c3e129841b637374.patch'
+    [
+      # Fixes clang build problems
+      # Already upstream, check for removal on 1.1 release:
+      # https://github.com/FreeRDP/FreeRDP/pull/544
+      'https://github.com/mikemcquaid/FreeRDP/commit/1d3289.patch',
+      # Fixes Mountain Lion build problem
+      'https://github.com/mikemcquaid/FreeRDP/commit/e32f9e.patch'
+    ]
   end
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", "-DWITH_X11=OFF", *std_cmake_args
     system "make install"
   end
 end
