53d5bfb44e8644eff1693b2a734f079d10b53043
Mike McQuaid <mike@mikemcquaid.com>
2012-08-04 10:48:23 +0100

    Use LC_CTYPE rather than LANG for most specific ML sed fix.
    Fix #11728, fix #12890, fix #13653, fix #13734, fix #13787, fix #13818,
    fix #13939.

--- Library/Homebrew/extend/ENV.rb
+++ Library/Homebrew/extend/ENV.rb
@@ -12,7 +12,7 @@ module HomebrewEnvExtension
     if MacOS.mountain_lion?
       # Fix issue with sed barfing on unicode characters on Mountain Lion.
       delete('LC_ALL')
-      self['LANG']="C"
+      self['LC_CTYPE']="C"
 
       # Mountain Lion no longer ships a few .pcs; make sure we pick up our versions
       prepend 'PKG_CONFIG_PATH',
