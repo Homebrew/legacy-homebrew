e190eabe5e928759e4a6b409386c510957fdb008
Brett Koonce <koonce@gmail.com>
2012-08-03 21:15:57 -0500

    ispc 1.3.0
    Closes #13932.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ispc.rb
+++ Library/Formula/ispc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Ispc < Formula
   homepage 'http://ispc.github.com'
-  url 'https://github.com/downloads/ispc/ispc/ispc-v1.2.2-osx.tar.gz'
-  version '1.2.2'
-  md5 '21f1fe1aabe47ae280e0dc92d50d564f'
+  url 'https://github.com/downloads/ispc/ispc/ispc-v1.3.0-osx.tar.gz'
+  version '1.3.0'
+  sha1 'ec60f9e08405b6160b98aefd8cb4f9f7ee07c12e'
 
   def install
     bin.install 'ispc'
