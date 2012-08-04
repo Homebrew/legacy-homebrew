bc276abfb2d0c2f3b35adb633bc40a0071a04a89
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2012-08-04 22:58:30 +0200

    emboss 6.5.7
    Closes #13965.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/emboss.rb
+++ Library/Formula/emboss.rb
@@ -1,12 +1,16 @@
 require 'formula'
 
 class Emboss < Formula
-  url 'ftp://emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.4.0.tar.gz'
   homepage 'http://emboss.sourceforge.net/'
-  md5 '54993a22064222b01bd4fc8086f7684d'
+  url 'ftp://emboss.open-bio.org/pub/EMBOSS/EMBOSS-6.5.7.tar.gz'
+  sha1 '907231eafe07917ae0bf9c5da2e7cdc3e9bae03a'
+
+  depends_on 'pkg-config' => :build
+  depends_on :x11
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
