602888a537bbc17243b264070996f7ae1efe9c9e
Brett Koonce <koonce@gmail.com>
2012-08-03 20:56:17 -0500

    ical-buddy 1.8.4

--- Library/Formula/ical-buddy.rb
+++ Library/Formula/ical-buddy.rb
@@ -5,20 +5,13 @@ require 'formula'
 
 class IcalBuddy < Formula
   homepage 'http://hasseg.org/icalBuddy/'
-  url 'http://hasseg.org/git-public/icalBuddy.git', :tag => 'v1.8.2'
-  version '1.8.2'
+  url 'http://hasseg.org/icalBuddy/1.8.4/icalBuddy-v1.8.4.zip', :tag => 'v1.8.4'
+  sha1 '40e11d937869a5cd90b958271043a7efac9d21bb'
+  version '1.8.4'
 
   head 'http://hasseg.org/git-public/icalBuddy.git'
 
   def install
-    arch = MacOS.prefer_64_bit? ? "x86_64" : "i386"
-    sdk = MACOS_VERSION
-
-    inreplace "Makefile" do |s|
-      s.gsub! "-arch i386 -arch ppc $(ARCH_64BIT)", "-arch #{arch}"
-      s.gsub! "-mmacosx-version-min=10.5", "-mmacosx-version-min=#{sdk}"
-    end
-
     system "make icalBuddy icalBuddy.1 icalBuddyLocalization.1 icalBuddyConfig.1"
 
     bin.install "icalBuddy"
