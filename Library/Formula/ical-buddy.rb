5a8d754c2ef05d536085de7224003b08217ff9e3
Brett Koonce <koonce@gmail.com>
2012-08-03 20:56:17 -0500

    ical-buddy 1.8.4
    Closes #13929.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ical-buddy.rb
+++ Library/Formula/ical-buddy.rb
@@ -1,26 +1,14 @@
 require 'formula'
 
-# NOTE: Using git-repo instead of zip package as the latter does not
-#       include source files.
-
 class IcalBuddy < Formula
   homepage 'http://hasseg.org/icalBuddy/'
-  url 'http://hasseg.org/git-public/icalBuddy.git', :tag => 'v1.8.2'
-  version '1.8.2'
+  url 'http://hasseg.org/icalBuddy/1.8.4/icalBuddy-v1.8.4.zip'
+  sha1 '40e11d937869a5cd90b958271043a7efac9d21bb'
 
   head 'http://hasseg.org/git-public/icalBuddy.git'
 
   def install
-    arch = MacOS.prefer_64_bit? ? "x86_64" : "i386"
-    sdk = MACOS_VERSION
-
-    inreplace "Makefile" do |s|
-      s.gsub! "-arch i386 -arch ppc $(ARCH_64BIT)", "-arch #{arch}"
-      s.gsub! "-mmacosx-version-min=10.5", "-mmacosx-version-min=#{sdk}"
-    end
-
     system "make icalBuddy icalBuddy.1 icalBuddyLocalization.1 icalBuddyConfig.1"
-
     bin.install "icalBuddy"
     man1.install Dir["*.1"]
   end
