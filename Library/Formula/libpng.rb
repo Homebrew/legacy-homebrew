2d5d36a905fef08340e46d23fe6f0cba698cd269
Mike McQuaid <mike@mikemcquaid.com>
2012-08-04 10:21:33 +0100

    libpng: don't depend on all of X11

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -5,8 +5,7 @@ class Libpng < Formula
   url 'http://downloads.sf.net/project/libpng/libpng15/1.5.12/libpng-1.5.12.tar.gz'
   sha1 'c329f3a9b720d7ae14e8205fa6e332236573704b'
 
-  keg_only :provided_by_osx,
-            "TODO: Which software depends on this newer version of libpng?"
+  keg_only :provided_by_osx if MacOS.x11_installed?
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
