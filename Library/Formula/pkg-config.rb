ce24dc8b86e4481443571bc7481542f73b1cb7a3
Mihail-samoylov <mihail.samoylov@gmail.com>
2012-08-05 00:05:32 +0700

    pkg-config: add a mirror
    The official download is offline from time to time. See:
    http://serverfault.com/questions/413591/brew-install-imagemagick-curl-fails
    
    Closes #13952.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -3,6 +3,7 @@ require 'formula'
 class PkgConfig < Formula
   homepage 'http://pkgconfig.freedesktop.org'
   url 'http://pkgconfig.freedesktop.org/releases/pkg-config-0.27.tar.gz'
+  mirror 'http://fossies.org/unix/privat/pkg-config-0.27.tar.gz'
   sha256 '79a6b43ee6633c9e6cc03eb1706370bb7a8450659845b782411f969eaba656a4'
 
   depends_on 'gettext'
