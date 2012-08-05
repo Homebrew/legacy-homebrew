f0e6f0e2358e3f0914e2333b1e5580496fc2598b
Bart Schuller <schuller@lunatech.com>
2012-08-05 20:12:28 +0200

    New sbt shell script, new upstream version
    This is the official sbt, the shell script is official too.
    The previous formula was based on a homebrew-specific shell script.

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -1,27 +1,52 @@
 require 'formula'
 
 class Sbt < Formula
-  homepage 'http://github.com/harrah/xsbt/'
-  url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.11.3-2/sbt-launch.jar'
-  version '0.11.3-2'
-  md5 'b8d0d2cd7000f2de29a2cda65e7809e5'
+  homepage 'http://www.scala-sbt.org/'
+  url 'http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.0/sbt.tgz'
+  version '0.12.0'
+  md5 'b167319b91b2c37031d8761b5b28fa76'
+
+  def patches
+    # add realpath, call it so we find the files in libexec
+    # See https://github.com/sbt/sbt-launcher-package/issues/15
+    DATA
+  end
 
   def install
-    (bin+'sbt').write <<-EOS.undent
-      #!/bin/sh
-      test -f ~/.sbtconfig && . ~/.sbtconfig
-      exec java -Xmx512M ${SBT_OPTS} -jar #{libexec}/sbt-launch.jar "$@"
-    EOS
-
-    libexec.install Dir['*']
+    libexec.install 'bin/sbt'
+    libexec.install 'bin/sbt-launch-lib.bash'
+    libexec.install 'bin/sbt-launch.jar'
+    bin.install_symlink libexec+'sbt'
   end
 
-  def caveats;  <<-EOS.undent
-    You can use $SBT_OPTS to pass additional JVM options to SBT.
-    For convenience, this can specified in `~/.sbtconfig`.
-
-    For example:
-        SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M"
-    EOS
-  end
 end
+__END__
+diff --git a/bin/sbt b/bin/sbt
+index a791e2a..d00cd1c 100755
+--- a/bin/sbt
++++ b/bin/sbt
+@@ -1,6 +1,10 @@
+ #!/usr/bin/env bash
+ 
+-. $(dirname $0)/sbt-launch-lib.bash
++realpath () {
++  python -c 'import os,sys;print os.path.realpath(sys.argv[1])' $1
++}
++
++. $(dirname $(realpath $0))/sbt-launch-lib.bash
+ 
+ 
+ declare -r noshare_opts="-Dsbt.global.base=project/.sbtboot -Dsbt.boot.directory=project/.boot -Dsbt.ivy.home=project/.ivy"
+diff --git a/bin/sbt-launch-lib.bash b/bin/sbt-launch-lib.bash
+index 3bfd078..2fbf99b 100755
+--- a/bin/sbt-launch-lib.bash
++++ b/bin/sbt-launch-lib.bash
+@@ -29,7 +29,7 @@ dlog () {
+ }
+ 
+ jar_file () {
+-  echo "$(dirname $0)/sbt-launch.jar"
++  echo "$(dirname $(realpath $0))/sbt-launch.jar"
+ }
+ 
+ acquire_sbt_jar () {
