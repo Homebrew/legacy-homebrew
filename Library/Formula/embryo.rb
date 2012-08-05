782652ce441598a7186dee9b7a4adc6857fd2a5b
Brett Koonce <koonce@gmail.com>
2012-08-04 22:22:33 -0500

    embryo 1.2.0
    Closes #13971.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/embryo.rb
+++ Library/Formula/embryo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Embryo < Formula
   homepage 'http://trac.enlightenment.org/e/wiki/Embryo'
-  url 'http://download.enlightenment.org/releases/embryo-1.1.0.tar.gz'
-  sha1 'f17bd3af565ada4ed5bbae1818c871b33c0897b8'
+  url 'http://download.enlightenment.org/releases/embryo-1.2.0.tar.gz'
+  sha1 '5e392a41ead0d696512b02f436eeae8747ddfbcb'
 
   head 'http://svn.enlightenment.org/svn/e/trunk/embryo/'
 
