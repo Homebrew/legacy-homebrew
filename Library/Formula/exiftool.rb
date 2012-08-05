5b7e53f23e34dd113a3adab3a8d332533d25f541
Brett Koonce <koonce@gmail.com>
2012-08-04 22:27:43 -0500

    ExifTool 8.99
    Closes #13978.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/exiftool.rb
+++ Library/Formula/exiftool.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Exiftool < Formula
   homepage 'http://www.sno.phy.queensu.ca/~phil/exiftool/index.html'
-  url 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-8.97.tar.gz'
-  sha1 'd763fa7ba28e8a52f00570d5624a325eb22bcc6a'
+  url 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-8.99.tar.gz'
+  sha1 '45b79b2d2e93c926e8be3aaa6474f60b05daac14'
 
   def install
     system "perl", "Makefile.PL"
