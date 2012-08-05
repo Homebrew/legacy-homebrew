4f8f50f9475286de7a0852bf7587c9b62771fdb9
Brett Koonce <koonce@gmail.com>
2012-08-04 22:26:49 -0500

    exodriver 2.5.1
    Closes #13974.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/exodriver.rb
+++ Library/Formula/exodriver.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Exodriver < Formula
   homepage 'http://labjack.com/support/linux-and-mac-os-x-drivers'
-  url 'https://github.com/labjack/exodriver/tarball/v2.5.0'
-  md5 '85e8782d81ecd49012bbcf51c72d9ef8'
+  url 'https://github.com/labjack/exodriver/tarball/v2.5.1'
+  sha1 '4c4ab59f84492fe65bc8e1785831a8a22b952690'
 
   head 'https://github.com/labjack/exodriver.git'
 
