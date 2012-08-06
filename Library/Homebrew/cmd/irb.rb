3533bc658f78e1fc52e11c1441ba59bba89a42c5
Max Howell <mxcl@me.com>
2012-08-06 14:41:55 -0400

    New command `brew irb`
    Works rather damn well.
    
    I considered `brew -i`, but we aren't a shell.

---
+++ Library/Homebrew/cmd/irb.rb
@@ -0,0 +1,11 @@
+module Homebrew extend self
+  def irb
+    if ARGV.include? "--help"
+      puts "Formula.factory('ace').installed?"
+    else
+      ohai "Interactive Homebrew Shell"
+      puts "Example commands available with: brew irb --help"
+      exec "irb", "-I#{HOMEBREW_REPOSITORY}/Library/Homebrew", "-rglobal"
+    end
+  end
+end
