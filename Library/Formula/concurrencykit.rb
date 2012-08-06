c2850c07f2715fb0a3c6a9b43d36dc4ad08db996
Brett Koonce <koonce@gmail.com>
2012-08-06 17:16:15 -0500

    concurrencykit 0.2.6
    Closes #14015.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Concurrencykit < Formula
   homepage 'http://concurrencykit.org'
-  url 'http://concurrencykit.org/releases/ck-0.2.2.tar.gz'
-  sha1 '1c57819935f2a24128e521387e58587557de93c9'
+  url 'http://concurrencykit.org/releases/ck-0.2.6.tar.gz'
+  sha1 '875f2692bcf151ba38a245d908f9f933e81e65cb'
 
   head 'git://git.concurrencykit.org/ck.git'
 
