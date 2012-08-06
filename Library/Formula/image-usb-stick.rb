fae4c9bc20a5e2dc10601f6c94dc78fbaab172ea
dejay <chipsolderer@gmail.com>
2012-08-06 17:11:06 -0600

    New formula: image-usb-stick

--- Library/Formula/image-usb-stick.rb
+++ Library/Formula/image-usb-stick.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class ImageUsbStick < ScriptFileFormula
-  homepage 'https://github.com/abock/image-usb-stick'
-  url 'https://raw.github.com/abock/image-usb-stick/master/image-usb-stick'
+  homepage 'https://github.com/abock/image-usb-stick/'
+  url 'https://raw.github.com/abock/image-usb-stick/b6f48c97d244ef6e3321e72ffa15f4e008637e66/image-usb-stick'
   sha1 '08d0050616f823239fc30ca669878de0fe1a73de'
 end
