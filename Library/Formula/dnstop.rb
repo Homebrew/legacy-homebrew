24d0de4b3fde592398a80c6aad36630d4d6b9ed6
Brett Koonce <koonce@gmail.com>
2012-08-05 20:55:31 -0500

    dnstop 20120611
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dnstop.rb
+++ Library/Formula/dnstop.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Dnstop < Formula
-  url 'http://dns.measurement-factory.com/tools/dnstop/src/dnstop-20110502.tar.gz'
   homepage 'http://dns.measurement-factory.com/tools/dnstop/index.html'
-  md5 '28cb54f3780b27e15df8924235e4e37b'
+  url 'http://dns.measurement-factory.com/tools/dnstop/src/dnstop-20120611.tar.gz'
+  sha1 'a5fb7e9d307488f2f6aaaa0291b9c7c187f68fd9'
 
   def install
     system "./configure", "--prefix=#{prefix}"
