ebef8f0f50891906bece769b7917d1598d442f62
nibbles 2bits <nibbles@speakeasy.net>
2012-08-06 10:32:15 -0700

    bdw-gc: add `fails_with :clang`
    bdw-gc-7.2c fails to pass make check with clang-4.0 on ML.
    Add a `fails_with :clang` block because it will otherwise get
    a segfault: 11.
    
    Fixes #13940

--- Library/Formula/bdw-gc.rb
+++ Library/Formula/bdw-gc.rb
@@ -5,6 +5,11 @@ class BdwGc < Formula
   url 'http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc-7.2c.tar.gz'
   sha1 '18c5b1aa9289a12fead3ceeda8fdc81f4ed08964'
 
+  def fails_with :clang
+    build 421
+    cause 'Segfault 11 during make check'
+  end
+
   def options
     [['--universal', 'Make a 32/64-bit Intel build.']]
   end
