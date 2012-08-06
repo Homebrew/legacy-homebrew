b2d464bfbe7e620c818f9add5d4405a3a5c8cfa3
Sébastien Gross <sebchezwamorg>
2012-08-06 22:34:32 +0200

    Update to contacts 1.1a-3 to support UTF8 strings.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- Library/Formula/contacts.rb
+++ Library/Formula/contacts.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Contacts < Formula
-  url 'https://github.com/dhess/contacts/tarball/v1.1a'
-  version '1.1a'
+  url 'https://github.com/dhess/contacts/tarball/master'
+  version '1.1a-3'
   homepage 'http://www.gnufoo.org/contacts/contacts.html'
-  sha1 '2e5c8b4d9302aa6d3652a559155a95d74b6ffb6c'
+  sha1 '644f449f0d01ddf62236ce82240f4757921cca17'
 
   def install
     system "make"
