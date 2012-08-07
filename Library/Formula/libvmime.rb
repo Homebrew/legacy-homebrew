c2e7c407a7ec911f04eb3fdc2913483378bc59c5
zeeMonkeez <git@lessjunkmorefunk.de>
2012-08-07 14:42:44 +0100

    Fix formula for libvmime
    * give cause for fails_with
    * reorder depends_on
    * no array for arguments of configure
    * remove whitespace

--- Library/Formula/libvmime.rb
+++ Library/Formula/libvmime.rb
@@ -5,20 +5,19 @@ class Libvmime < Formula
   url 'http://downloads.sourceforge.net/project/vmime/vmime/0.9/libvmime-0.9.1.tar.bz2'
   sha1 '3e8dd8855e423db438d465777efeb523c4abb5f3'
 
-  depends_on "libgsasl"
   depends_on "pkg-config" => :build
+  depends_on "libgsasl"
   depends_on "gnutls"
   fails_with :clang do
      build 318
+     cause <<-EOS.undent
+     ../vmime/base.hpp:255:12: error: use 'template' keyword to treat
+     'dynamicCast' as a dependent template name
+     EOS
   end
   def install
-    args = %W[--prefix=#{prefix}
-      LIBS=-liconv
-      --disable-debug]
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}", "LIBS=-liconv", "--disable-debug"
     system "make"
     system "make install"
   end
-
 end
