9cb607f0fef3abb8813497c1781a9c855b5fc0c0
Gavin M. Roy <gmr@meetme.com>
2012-08-07 18:23:13 -0400

    Update to 2.8.5

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rabbitmq < Formula
   homepage 'http://www.rabbitmq.com'
-  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.4/rabbitmq-server-generic-unix-2.8.4.tar.gz'
-  sha1 '75275083648fd9243c5bc212530355c76821c120'
+  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.5/rabbitmq-server-generic-unix-2.8.5.tar.gz'
+  sha1 '5762e40e2a9b79f12bf22f2f366feaa1'
 
   depends_on 'erlang'
   depends_on 'simplejson' => :python if MacOS.leopard?
