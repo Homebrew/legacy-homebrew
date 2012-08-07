f71f39c6ec57423f7803eed4585992b545d66be1
Brett Koonce <koonce@gmail.com>
2012-08-07 00:33:55 -0500

    CppUTest v3.1

--- Library/Formula/cpputest.rb
+++ Library/Formula/cpputest.rb
@@ -2,8 +2,12 @@ require 'formula'
 
 class Cpputest < Formula
   homepage 'http://www.cpputest.org/'
-  url 'http://downloads.sourceforge.net/project/cpputest/cpputest/v2.3/CppUTest-v2.3.zip'
-  md5 '0546bf6d0f1513842cfa781255dcbdda'
+  url 'http://sourceforge.net/projects/cpputest/files/cpputest/v3.1/CppUTest-v3.1.zip'
+  sha1 '8ff6b764a9ca6202582ae0c94545f56b921f39d5'
+
+  fails_with :clang do
+    build 412
+  end
 
   def install
     system "make"
