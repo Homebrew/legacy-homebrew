ff2ae37ecb9b39acf530e1c9c386a905548a52b3
Adam Vandenberg <flangy@gmail.com>
2012-08-08 22:13:02 -0700

    berkeley-db: use options dsl

--- Library/Formula/berkeley-db.rb
+++ Library/Formula/berkeley-db.rb
@@ -5,18 +5,17 @@ class BerkeleyDb < Formula
   url 'http://download.oracle.com/berkeley-db/db-5.3.21.tar.gz'
   sha1 '32e43c4898c8996750c958a90c174bd116fcba83'
 
-  def options
-    [['--without-java', 'Compile without Java support.']]
-  end
+  option 'without-java', 'Compile without Java support.'
 
   def install
     # BerkeleyDB dislikes parallel builds
     ENV.deparallelize
 
     args = ["--disable-debug",
-            "--prefix=#{prefix}", "--mandir=#{man}",
+            "--prefix=#{prefix}",
+            "--mandir=#{man}",
             "--enable-cxx"]
-    args << "--enable-java" unless ARGV.include? "--without-java"
+    args << "--enable-java" unless build.include? "--without-java"
 
     # BerkeleyDB requires you to build everything from the build_unix subdirectory
     cd 'build_unix' do
