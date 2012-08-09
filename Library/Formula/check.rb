b015b13238481a80bf5bccea88cd67b56da67e5f
Adam Vandenberg <flangy@gmail.com>
2012-08-08 22:01:34 -0700

    check: use options dsl

--- Library/Formula/check.rb
+++ Library/Formula/check.rb
@@ -1,16 +1,14 @@
 require 'formula'
 
 class Check < Formula
-  url 'http://downloads.sourceforge.net/project/check/check/0.9.8/check-0.9.8.tar.gz'
   homepage 'http://check.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/check/check/0.9.8/check-0.9.8.tar.gz'
   md5 '5d75e9a6027cde79d2c339ef261e7470'
 
-  def options
-    [["--universal", "Build a universal binary."]]
-  end
+  option :universal
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
