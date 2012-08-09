bd24da5b76a846a9b004e4832d01cb45d9d1671b
Adam Vandenberg <flangy@gmail.com>
2012-08-08 22:07:31 -0700

    babl: use options dsl

--- Library/Formula/babl.rb
+++ Library/Formula/babl.rb
@@ -9,9 +9,7 @@ class Babl < Formula
 
   depends_on 'pkg-config' => :build
 
-  def options
-    [["--universal", "Builds a universal binary"]]
-  end
+  option :universal
 
   def patches
     # There are two patches.
@@ -22,7 +20,7 @@ class Babl < Formula
   end
 
   def install
-    if ARGV.build_universal?
+    if build.universal?
       ENV.universal_binary
       if ENV.compiler == :gcc
         opoo 'Compilation may fail at babl-cpuaccel.c using gcc for a universal build'
