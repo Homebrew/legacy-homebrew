5924c8c014acf110cb9d136bc7b39cd6cefe8744
Brandon Bennett <bennetb@gmail.com>
2012-08-10 17:56:35 -0600

    Add dependency for libtool for libslax and juise

--- Library/Formula/juise.rb
+++ Library/Formula/juise.rb
@@ -5,9 +5,10 @@ class Juise < Formula
   url 'http://juise.googlecode.com/files/juise-0.3.10.tar.gz'
   sha1 '1e3c0f5e98ee3f499b5180512cc98e294c551fde'
 
+  depends_on 'libtool'
+  depends_on 'libslax'
   depends_on 'libxml2' if MacOS.version <= 10.7 # Lion's libxml is too old. Need > 2.7.8
   depends_on 'libxslt' if MacOS.version <= 10.7 # Lion's libxslt is too old. Need > 1.1.26
-  depends_on 'libslax'
 
   def install
     ENV.libxml2 if MacOS.mountain_lion?
