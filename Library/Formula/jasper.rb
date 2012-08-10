ebbde821e258b2a4ca3444f3191ff11285f6bb42
Adam Vandenberg <flangy@gmail.com>
2012-08-09 22:12:30 -0700

    jasper: use options dsl

--- Library/Formula/jasper.rb
+++ Library/Formula/jasper.rb
@@ -5,15 +5,13 @@ class Jasper < Formula
   url 'http://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip'
   sha1 '9c5735f773922e580bf98c7c7dfda9bbed4c5191'
 
+  option :universal
+
   depends_on 'jpeg'
 
   fails_with :llvm do
     build 2326
     cause "Undefined symbols when linking"
-  end
-
-  def options
-    [["--universal", "Build a universal binary."]]
   end
 
   # The following patch fixes a bug (still in upstream as of jasper 1.900.1)
@@ -23,7 +21,7 @@ class Jasper < Formula
   def patches; DATA; end
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--enable-shared",
