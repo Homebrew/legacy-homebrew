f3b20ca05a6c8e95c4001ee2212889ab8b7a56cd
Brandon Bennett <bennetb@gmail.com>
2012-08-10 17:48:44 -0600

    Correctly update dependencies for libslax and juise

--- Library/Formula/libslax.rb
+++ Library/Formula/libslax.rb
@@ -5,8 +5,8 @@ class Libslax < Formula
   url 'https://libslax.googlecode.com/files/libslax-0.11.23.tar.gz'
   sha1 '0be3d52f8e9f0b048b2816086d5fa3688b0d8364'
 
-  depends_on 'libxml2' if MacOS.version >= 10.7  # Lion's libxml is too old. Need > 2.7.8
-  depends_on 'libxslt' if MacOS.version >= 10.7 # Lion's libxslt is too old. Need > 1.1.26
+  depends_on 'libxml2' if MacOS.version <= 10.7  # Lion's libxml is too old. Need > 2.7.8
+  depends_on 'libxslt' if MacOS.version <= 10.7 # Lion's libxslt is too old. Need > 1.1.26
 
   def install
     ENV.libxml2 if MacOS.mountain_lion?
