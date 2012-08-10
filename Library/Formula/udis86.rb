0f227264773fd34259ad92bb360648ed8726eefc
Adam Vandenberg <flangy@gmail.com>
2012-08-09 21:49:40 -0700

    udis86: use options dsl

--- Library/Formula/udis86.rb
+++ Library/Formula/udis86.rb
@@ -1,17 +1,16 @@
 require 'formula'
 
 class Udis86 < Formula
+  homepage 'http://udis86.sourceforge.net'
   url 'http://downloads.sourceforge.net/udis86/udis86-1.7.tar.gz'
   md5 'e279108e10f774e6c3af83caa18f5dc3'
-  homepage 'http://udis86.sourceforge.net'
 
-  def options
-    [["--universal", "Build a universal binary."]]
-  end
+  option :universal
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
-    system "./configure", "--enable-shared", "--prefix=#{prefix}"
+    ENV.universal_binary if build.universal?
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-shared"
     system "make"
     system "make install"
   end
