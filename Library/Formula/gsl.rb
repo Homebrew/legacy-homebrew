7ce827e36d20c815c6753f8a8700779d7ab315ed
Adam Vandenberg <flangy@gmail.com>
2012-08-09 21:55:01 -0700

    gsl: use options dsl

--- Library/Formula/gsl.rb
+++ Library/Formula/gsl.rb
@@ -1,19 +1,18 @@
 require 'formula'
 
 class Gsl < Formula
+  homepage 'http://www.gnu.org/software/gsl/'
   url 'http://ftpmirror.gnu.org/gsl/gsl-1.15.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/gsl/gsl-1.15.tar.gz'
-  homepage 'http://www.gnu.org/software/gsl/'
   md5 '494ffefd90eef4ada678c306bab4030b'
 
-  def options
-    [["--universal", "Build a universal binary."]]
-  end
+  option :universal
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
 
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make" # A GNU tool which doesn't support just make install! Shameful!
     system "make install"
   end
