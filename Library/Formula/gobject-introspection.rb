f170a6717b6cf5e4c2731567d8085d39b9c83e33
Adam Vandenberg <flangy@gmail.com>
2012-08-09 21:53:39 -0700

    gobject-introspection: use options dsl

--- Library/Formula/gobject-introspection.rb
+++ Library/Formula/gobject-introspection.rb
@@ -5,17 +5,15 @@ class GobjectIntrospection < Formula
   url 'http://ftp.gnome.org/pub/gnome/sources/gobject-introspection/1.32/gobject-introspection-1.32.1.tar.xz'
   sha256 '44f3fb933f76e4728818cc360cb5f9e2edcbdf9bc8a8f9aded99b3e3ef5cb858'
 
+  option :universal
+
   depends_on 'pkg-config' => :build
   depends_on 'xz' => :build
   depends_on 'glib'
   depends_on 'libffi'
 
-  def options
-    [["--universal", "Build universal binaries"]]
-  end
-
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           # Tests require (at least) cairo, disable them.
