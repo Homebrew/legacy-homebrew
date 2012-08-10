b3f4bbbc9ff1bcbdee1681668647fee7b0b1063b
Brandon Bennett <bennetb@gmail.com>
2012-08-10 16:11:11 -0600

    juise 0.3.10
    JUISE stands for JUNOS User Interface Scripting Environment which
    takes the abilities provided by the scripting facility of JUNOS
    and moves it into the open source world, where a script can run on
    a remote box, accessing JUNOS resources over the NETCONF (or
    JUNOScript) API. Initially this will be an excellent environment
    for creating and debugging scripts, but for many users, it may
    become their "normal" scripting environment.

---
+++ Library/Formula/juise.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Juise < Formula
+  homepage 'http://code.google.com/p/juise/'
+  url 'http://juise.googlecode.com/files/juise-0.3.10.tar.gz'
+  sha1 '1e3c0f5e98ee3f499b5180512cc98e294c551fde'
+
+  depends_on 'libxml2' if MacOS.lion? # Lion's libxml is too old. Need > 2.7.8
+  depends_on 'libxslt' if MacOS.lion? # Lion's libxslt is too old. Need > 1.1.26
+  depends_on 'libslax'
+
+  def install
+    ENV.libxml2 if MacOS.mountain_lion?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
