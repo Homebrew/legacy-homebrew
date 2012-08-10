7f48191e1f64cb1b801fc8d0c888b9e25066c634
Adam Vandenberg <flangy@gmail.com>
2012-08-09 21:39:38 -0700

    xz: use options dsl

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -5,12 +5,10 @@ class Xz < Formula
   url 'http://tukaani.org/xz/xz-5.0.4.tar.bz2'
   sha256 '5cd9b060d3a1ad396b3be52c9b9311046a1c369e6062aea752658c435629ce92'
 
-  def options
-    [["--universal", "Build a universal binary."]]
-  end
+  option :universal
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
