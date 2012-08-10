836c8b050395f32bbd85963ccb41e75cf84fc466
Adam Vandenberg <flangy@gmail.com>
2012-08-09 21:51:17 -0700

    gegl: use options dsl

--- Library/Formula/gegl.rb
+++ Library/Formula/gegl.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class Gegl < Formula
-  url 'ftp://ftp.gimp.org/pub/gegl/0.2/gegl-0.2.0.tar.bz2'
   homepage 'http://www.gegl.org/'
+  url 'ftp://ftp.gimp.org/pub/gegl/0.2/gegl-0.2.0.tar.bz2'
   md5 '32b00002f1f1e316115c4ed922e1dec8'
+
+  option :universal
 
   depends_on 'babl'
   depends_on 'glib'
@@ -11,18 +13,17 @@ class Gegl < Formula
   depends_on 'intltool' => :build
   depends_on 'pkg-config' => :build
 
-  def options
-  [
-    ["--universal", "Builds a universal binary"],
-  ]
-  end
-
   def install
     # ./configure breaks when optimization is enabled with llvm
     ENV.no_optimization if ENV.compiler == :llvm
 
-    argv = ["--disable-docs", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"]
-    if ARGV.build_universal?
+    argv = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --disable-docs
+    ]
+    if build.universal?
       ENV.universal_binary
       # ffmpeg's formula is currently not universal-enabled
       argv << "--without-libavformat"
