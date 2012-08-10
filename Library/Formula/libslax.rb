aec789ba0f56856412fbbe9097301069bb069cd1
Brandon Bennett <bennetb@gmail.com>
2012-08-10 16:07:59 -0600

    libslax 0.11.23
    SLAX is an alternative syntax for XSLT which is tailored for
    readability and familiarity, following the style of C and Perl.
    Programming constructs and XPath expressions are moved from the XML
    elements and attributes used in XSLT to first class language
    constructs. The overabundance of angle brackets and quotes is
    replaced by the parentheses and curly braces which programmers have
    enjoyed since the birth of "C".
    
    SLAX provides the binary 'slaxproc' which is useful for executing
    slax or checking for syntax errors.  Slaxproc can also be used to
    convert between slax and xlst and visa-versa as well as debugging
    and profiling making is a very valible tool when scripting with
    slax.

---
+++ Library/Formula/libslax.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Libslax < Formula
+  homepage 'https://code.google.com/p/libslax/'
+  url 'https://libslax.googlecode.com/files/libslax-0.11.23.tar.gz'
+  sha1 '0be3d52f8e9f0b048b2816086d5fa3688b0d8364'
+
+  depends_on 'libxml2' if MacOS.lion? # Lion's libxml is too old. Need > 2.7.8
+  depends_on 'libxslt' if MacOS.lion? # Lion's libxslt is too old. Need > 1.1.26
+
+  def install
+    ENV.libxml2 if MacOS.mountain_lion?
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
