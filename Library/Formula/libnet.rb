957c6b9f2dbc363fd91ecf09f7dc2e66722ca9ab
Adam Vandenberg <flangy@gmail.com>
2012-08-10 09:28:22 -0700

    libnet: fix raw sockets
    Closes #12062.

--- Library/Formula/libnet.rb
+++ Library/Formula/libnet.rb
@@ -5,8 +5,22 @@ class Libnet < Formula
   url 'http://sourceforge.net/projects/libnet-dev/files/libnet-1.1.6.tar.gz'
   sha1 'dffff71c325584fdcf99b80567b60f8ad985e34c'
 
+  # MacPorts does an autoreconf to get raw sockets working
+  depends_on :autoconf
+  depends_on :libtool
+
+  # Fix raw sockets support
+  def patches
+    {:p0 =>
+     "https://trac.macports.org/export/95336/trunk/dports/net/libnet11/files/patch-configure.in.diff"
+    }
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "autoreconf --force --install"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    inreplace "src/libnet_link_bpf.c", "#include <net/bpf.h>", "" # Per MacPorts
     system "make install"
   end
 end
