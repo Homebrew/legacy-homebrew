697ac84c213e5a9f52207e16a65b37d87b2bd143
Mike McQuaid <mike@mikemcquaid.com>
2012-08-11 15:29:04 +0100

    pidof: add bottles.

--- Library/Formula/pidof.rb
+++ Library/Formula/pidof.rb
@@ -6,6 +6,12 @@ class Pidof < Formula
   sha1 '150ff344d7065ecf9bc5cb3c2cc83eeda8d31348'
   version '0.1.4'
 
+  bottle do
+    sha1 '77c1d4049ffb4ec5eabb25c04ac916c2a74b7fc3' => :mountainlion
+    sha1 '55132e9ded5e165405777ba4a5b97b41ca476fc9' => :lion
+    sha1 'e43a932c884f17e71b00ac3b76c44bd51af285fe' => :snowleopard
+  end
+
   def install
     system "make", "all", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
     man1.install gzip("pidof.1")
