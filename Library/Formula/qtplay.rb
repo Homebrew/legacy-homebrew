697a407629d492a7c74b2bdb4b73f9c360678840
nibbles 2bits <nibbles@speakeasy.net>
2012-08-11 16:38:43 -0700

    New formula: qtplay
    Qtplay is a command line interface audio player using Quicktime.
    It's just one file to compile, and it works well on Mt. Lion.
    Tested with clang and llvm from XCode-4.4.1.
    
    Qtplay will be a dep in the new Solfege formula.
    
    Closes #8775

---
+++ Library/Formula/qtplay.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Qtplay < Formula
+  homepage 'https://sites.google.com/site/rainbowflight2/'
+  url 'https://sites.google.com/site/rainbowflight2/qtplay1.3.1.tar.gz'
+  sha1 'fd7394675c972377a48c2ff8e0a774853c0be6a3'
+
+  def install
+    system ENV.cc, "qtplay.c", "-arch", "i386", "-framework", "QuickTime", "-framework", "Carbon", "-o", "qtplay"
+    bin.install 'qtplay'
+    man1.install 'qtplay.1'
+    prefix.install 'Readme.rtf'
+  end
+
+  def test
+    system "#{bin}/qtplay", "--help"
+  end
+end
