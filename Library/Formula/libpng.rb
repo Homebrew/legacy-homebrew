d51098a4d9a9fc15bc66be4aeb44056261def921
Mike McQuaid <mike@mikemcquaid.com>
2012-08-11 16:02:16 +0100

    libpng: add bottles.

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -7,6 +7,12 @@ class Libpng < Formula
 
   keg_only :provided_by_osx if MacOS::X11.installed?
 
+  bottle do
+    sha1 '83c6be83e86404f41982e5e1e6877924fe737bdf' => :mountainlion
+    sha1 '9a86cc5cec4cb19bd04c7c1e93595d96ebcde66f' => :lion
+    sha1 '3ba3f991b61afcaf0f369da89443738443d4effe' => :snowleopard
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
