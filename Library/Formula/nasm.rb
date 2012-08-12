62396412ff6370c3831745b3b0fe8f9c2306a6f8
Jack Nagel <jacknagel@gmail.com>
2012-08-12 12:57:47 -0500

    nasm: use options DSL
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/nasm.rb
+++ Library/Formula/nasm.rb
@@ -5,12 +5,10 @@ class Nasm < Formula
   url 'http://www.nasm.us/pub/nasm/releasebuilds/2.10.03/nasm-2.10.03.tar.bz2'
   sha256 '3babec15086fed1d00495e7c412848fd135cad70faa811738cb35da46d98974c'
 
-  def options
-    [[ '--universal', 'Build a universal binary' ]]
-  end
+  option :universal
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}"
     system "make everything"
     system "make install_everything"
