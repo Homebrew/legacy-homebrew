3c5457fccf847f94cdd98e6212f33b0369428184
Jack Nagel <jacknagel@gmail.com>
2012-08-13 09:36:11 -0500

    x3270: use options DSL
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/x3270.rb
+++ Library/Formula/x3270.rb
@@ -5,14 +5,10 @@ class X3270 < Formula
   url 'http://sourceforge.net/projects/x3270/files/x3270/3.3.12ga10/suite3270-3.3.12ga10-src.tgz'
   sha1 '582bb7f3f3523e08404cc8eff80860efd7a79e1b'
 
-  def options
-    [
-      ["--with-c3270",   "Include c3270 (curses-based version)"],
-      ["--with-s3270",   "Include s3270 (displayless version)"],
-      ["--with-tcl3270", "Include tcl3270 (integrated with Tcl)"],
-      ["--with-pr3287",  "Include pr3287 (printer emulation)"]
-    ]
-  end
+  option 'with-c3270', 'Include c3270 (curses-based version)'
+  option 'with-s3270', 'Include s3270 (displayless version)'
+  option 'with-tcl3270', 'Include tcl3270 (integrated with Tcl)'
+  option 'with-pr3287', 'Include pr3287 (printer emulation)'
 
   def make_directory(directory)
     cd directory do
@@ -25,9 +21,9 @@ class X3270 < Formula
 
   def install
     make_directory 'x3270-3.3'
-    make_directory 'c3270-3.3'   if ARGV.include? "--with-c3270"
-    make_directory 'pr3287-3.3'  if ARGV.include? "--with-pr3287"
-    make_directory 's3270-3.3'   if ARGV.include? "--with-s3270"
-    make_directory 'tcl3270-3.3' if ARGV.include? "--with-tcl3270"
+    make_directory 'c3270-3.3' if build.include? "with-c3270"
+    make_directory 'pr3287-3.3' if build.include? "with-pr3287"
+    make_directory 's3270-3.3' if build.include? "with-s3270"
+    make_directory 'tcl3270-3.3' if build.include? "with-tcl3270"
   end
 end
