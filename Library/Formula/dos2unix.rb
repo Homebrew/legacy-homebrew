4b2bcd577018e29098d0c8ec045db54f8e5ef4e1
Jack Nagel <jacknagel@gmail.com>
2012-08-13 09:36:30 -0500

    dos2unix: always use gettext
    gettext is now bottled on three of the platforms we support, so remove
    the hackery and just depend on it.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -5,28 +5,16 @@ class Dos2unix < Formula
   url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.1.tar.gz'
   sha1 '4f07a16ab3c875cd668e8d9ac3845c6dedce2980'
 
-  depends_on "gettext" if ARGV.include? "--enable-nls"
-
-  def options
-    [["--enable-nls", "Enable NLS support."]]
-  end
+  depends_on 'gettext'
 
   def install
-    args = ["prefix=#{prefix}"]
-
-    if ARGV.include? "--enable-nls"
-      gettext = Formula.factory("gettext")
-      args << "CFLAGS_OS=-I#{gettext.include}"
-      args << "LDFLAGS_EXTRA=-L#{gettext.lib} -lintl"
-    else
-      args << "ENABLE_NLS="
-    end
-
-    args << "CC=#{ENV.cc}"
-    args << "CPP=#{ENV.cc}"
-    args << "CFLAGS=#{ENV.cflags}"
-    args << "install"
-
-    system "make", *args
+    gettext = Formula.factory("gettext")
+    system "make", "prefix=#{prefix}",
+                   "CC=#{ENV.cc}",
+                   "CPP=#{ENV.cc}",
+                   "CFLAGS=#{ENV.cflags}",
+                   "CFLAGS_OS=-I#{gettext.include}",
+                   "LDFLAGS_EXTRA=-L#{gettext.lib} -lintl",
+                   "install"
   end
 end
