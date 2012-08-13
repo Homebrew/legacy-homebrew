db9bd9cb20de3c4f56a35280399e52a43f066c4d
Mark A. Matienzo <mark@matienzo.org>
2012-08-13 13:29:18 -0400

    zzuf: add OS X-specific memory patch

--- Library/Formula/zzuf.rb
+++ Library/Formula/zzuf.rb
@@ -5,6 +5,11 @@ class Zzuf < Formula
   homepage 'http://caca.zoy.org/wiki/zzuf'
   md5 '74579c429f9691f641a14f408997d42d'
 
+  def patches
+  	# Fix OS X-specific bug in zzuf 0.13; see https://trac.macports.org/ticket/29157
+  	{ :p3 => 'https://trac.macports.org/export/78051/trunk/dports/security/zzuf/files/patch-src-libzzuf-lib--mem.c.diff'}
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
