7889d0f5f82fbabcbc67d92cb3dd85e0a6a7e742
Jack Nagel <jacknagel@gmail.com>
2012-08-13 09:57:00 -0500

    gnu-units: use options DSL
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gnu-units.rb
+++ Library/Formula/gnu-units.rb
@@ -6,13 +6,11 @@ class GnuUnits < Formula
   mirror 'http://ftp.gnu.org/gnu/units/units-2.00.tar.gz'
   sha1 '6da9ea78ff0dc21bc43cf1809c530e61d9394ce0'
 
-  def options
-    [['--default-names', "Do NOT prepend 'g' to the binary; will override system utils."]]
-  end
+  option 'default-names', "Do not prepend 'g' to the binary"
 
   def install
     args = ["--prefix=#{prefix}"]
-    args << "--program-prefix=g" unless ARGV.include? '--default-names'
+    args << "--program-prefix=g" unless build.include? 'default-names'
 
     system "./configure", *args
     system "make install"
