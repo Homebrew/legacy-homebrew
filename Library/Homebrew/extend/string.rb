03e1e4c49be50f7aff58980aa5a4e3b3be08ff15
Max Howell <mxcl@me.com>
2012-08-14 11:23:30 -0400

    Improve libiconv diagnostic and suggestion
    This is coming up a lot at the moment, so here is an attempt to be more helpful.

--- Library/Homebrew/extend/string.rb
+++ Library/Homebrew/extend/string.rb
@@ -3,6 +3,18 @@ class String
     gsub(/^.{#{slice(/^ +/).length}}/, '')
   end
 
+  # eg:
+  #   if foo then <<-EOS.undent_________________________________________________________72
+  #               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+  #               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+  #               minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+  #               ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+  #               voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+  #               sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+  #               mollit anim id est laborum.
+  #               EOS
+  alias_method :undent_________________________________________________________72, :undent
+
   unless String.method_defined?(:start_with?)
     def start_with? prefix
       prefix = prefix.to_s
