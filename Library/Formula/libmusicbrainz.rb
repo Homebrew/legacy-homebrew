a0cea14a5ed7e768aea3e9a68c6e5ce6ed1dbc8b
Roberto Nibali <rnibali@gmail.com>
2012-08-15 01:58:42 +0200

    Upgrade of libmusicbrainz to version 5.0.1 as found on: https://github.com/mxcl/homebrew/wiki/Outdated-Formulae

--- Library/Formula/libmusicbrainz.rb
+++ Library/Formula/libmusicbrainz.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Libmusicbrainz < Formula
   homepage 'http://musicbrainz.org/doc/libmusicbrainz'
-  url 'https://github.com/downloads/musicbrainz/libmusicbrainz/libmusicbrainz-4.0.1.tar.gz'
-  md5 '3fe4e869f69dc83a4427b5d5c52580ef'
+  url 'https://github.com/downloads/metabrainz/libmusicbrainz/libmusicbrainz-5.0.1.tar.gz'
+  md5 'a0406b94c341c2b52ec0fe98f57cadf3'
 
   depends_on 'neon'
   depends_on 'cmake' => :build
 
   def install
     neon = Formula.factory("neon")
-    neon_args %W[-DNEON_LIBRARIES:FILEPATH=#{neon.lib}/libneon.dylib
+    neon_args = %W[-DNEON_LIBRARIES:FILEPATH=#{neon.lib}/libneon.dylib
                  -DNEON_INCLUDE_DIR:PATH=#{neon.include}/neon]
     system "cmake", ".", *(std_cmake_args + neon_args)
     system "make install"
