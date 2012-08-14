570539119104e3255c085e11969cfe159960368b
John Benediktsson <mrjbq7@gmail.com>
2012-08-13 18:44:58 -0700

    gts: add --universal option
    Closes #14165.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gts.rb
+++ Library/Formula/gts.rb
@@ -5,10 +5,13 @@ class Gts < Formula
   url 'http://downloads.sourceforge.net/project/gts/gts/0.7.6/gts-0.7.6.tar.gz'
   md5 '9f710aefd2ed9b3cc1b1216171fc5a8a'
 
+  option :universal
+
   depends_on 'gettext'
   depends_on 'glib'
 
   def install
+    ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
 
