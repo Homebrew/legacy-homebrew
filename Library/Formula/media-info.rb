bf137a753ceb716c1ff3b0a6d54b898a89c89ae5
Jack Nagel <jacknagel@gmail.com>
2012-08-14 11:31:29 -0500

    Adjust formula versions
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/media-info.rb
+++ Library/Formula/media-info.rb
@@ -3,6 +3,7 @@ require 'formula'
 class MediaInfo < Formula
   homepage 'http://mediainfo.sourceforge.net'
   url 'http://downloads.sourceforge.net/mediainfo/MediaInfo_CLI_0.7.59_GNU_FromSource.tar.bz2'
+  version '0.7.59'
   sha1 'ac7fde6e4d8ae5f127675e74c005cd7b5ddc9b6a'
 
   depends_on 'pkg-config' => :build
