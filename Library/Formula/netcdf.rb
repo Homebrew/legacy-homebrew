7f418ca41bce924708fd9c79b72999123854bbb6
John Foster <john.foster@utsa.edu>
2012-08-14 09:08:57 -0500

    NetCDF LargeFiles variant
    This is the NetCDF C libraries, but with the LargeFiles variant changes
    incorportated in order to work with ExodusII libraries.

--- Library/Formula/netcdf.rb
+++ Library/Formula/netcdf.rb
@@ -1,48 +1,21 @@
 require 'formula'
 
-def fortran?
-  ARGV.include? '--enable-fortran'
-end
-
-def no_cxx?
-  ARGV.include? '--disable-cxx'
-end
-
-class NetcdfCXX < Formula
-  homepage 'http://www.unidata.ucar.edu/software/netcdf'
-  url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx4-4.2.tar.gz'
-  sha1 '59628c9f06c211a47517fc00d8b068da159ffa9d'
-end
-
-class NetcdfFortran < Formula
-  homepage 'http://www.unidata.ucar.edu/software/netcdf'
-  url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-fortran-4.2.tar.gz'
-  sha1 'f1887314455330f4057bc8eab432065f8f6f74ef'
-end
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Netcdf < Formula
   homepage 'http://www.unidata.ucar.edu/software/netcdf'
-  url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-4.2.1.tar.gz'
-  sha1 'dfb6b10ef8dd20e785efa5e29b448383090f144d'
+  url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-4.2.1.1.tar.gz'
+  sha1 '76631cb4e6b767c224338415cf6e5f5ff9bd1238'
 
   depends_on 'hdf5'
 
-  def options
-    [
-      ['--enable-fortran', 'Compile Fortran bindings'],
-      ['--disable-cxx', "Don't compile C++ bindings"]
-    ]
+  def patches
+      # makes appropriate changes for "LargeFile format" required by ExodusII
+      DATA
   end
 
   def install
-    if fortran?
-      ENV.fortran
-      # fix for ifort not accepting the --force-load argument, causing
-      # the library libnetcdff.dylib to be missing all the f90 symbols.
-      # http://www.unidata.ucar.edu/software/netcdf/docs/known_problems.html#intel-fortran-macosx
-      # https://github.com/mxcl/homebrew/issues/13050
-      ENV['lt_cv_ld_force_load'] = 'no' if ENV['FC'] == 'ifort'
-    end
 
     common_args = %W[
       --disable-dependency-tracking
@@ -50,27 +23,34 @@ class Netcdf < Formula
       --enable-static
       --enable-shared
     ]
-
+  
     args = common_args.clone
     args.concat %w[--enable-netcdf4 --disable-doxygen]
 
-    system './configure', *args
-    system 'make install'
+    system "./configure", *args
+    system "make install"
 
-    # Add newly created installation to paths so that binding libraries can
-    # find the core libs.
-    ENV.prepend 'PATH', bin, ':'
-    ENV.prepend 'CPPFLAGS', "-I#{include}"
-    ENV.prepend 'LDFLAGS', "-L#{lib}"
-
-    NetcdfCXX.new.brew do
-      system './configure', *common_args
-      system 'make install'
-    end unless no_cxx?
-
-    NetcdfFortran.new.brew do
-      system './configure', *common_args
-      system 'make install'
-    end if fortran?
   end
+
 end
+
+__END__
+diff --git a/include/netcdf.h b/include/netcdf.h
+index 8ccfb0f..0353b29 100644
+--- a/include/netcdf.h
++++ b/include/netcdf.h
+@@ -189,11 +189,11 @@ created with the ::NC_CLASSIC_MODEL flag.
+ As a rule, NC_MAX_VAR_DIMS <= NC_MAX_DIMS.
+ */
+ /**@{*/
+-#define NC_MAX_DIMS	1024	
++#define NC_MAX_DIMS	65536
+ #define NC_MAX_ATTRS	8192	
+-#define NC_MAX_VARS	8192	
++#define NC_MAX_VARS	524288
+ #define NC_MAX_NAME	256	
+-#define NC_MAX_VAR_DIMS	1024 /**< max per variable dimensions */
++#define NC_MAX_VAR_DIMS	8 /**< max per variable dimensions */
+ /**@}*/
+ 
+ /** This is the max size of an SD dataset name in HDF4 (from HDF4 documentation).*/
