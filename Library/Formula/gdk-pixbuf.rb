5e1604befa51a002365863fef332c8b3c3577028
Birger J. Nordølum <birgerjn@gmail.com>
2012-08-15 19:50:03 +0200

    gdk-pixbuf 2.26.2
    Closes #14214.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gdk-pixbuf.rb
+++ Library/Formula/gdk-pixbuf.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GdkPixbuf < Formula
   homepage 'http://gtk.org'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.26/gdk-pixbuf-2.26.1.tar.xz'
-  sha256 'a60af12b58d9cc15ba4c680c6730ce5d38e8d664af1d575a379385b94b4ec7ba'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.26/gdk-pixbuf-2.26.2.tar.xz'
+  sha256 '3555521050e30e2fa090bfe650910c14649b6ab0acb35a5a7eaf90fe694f1403'
 
   option :universal
 
