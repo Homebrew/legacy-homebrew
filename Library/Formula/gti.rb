204998d4f468d2689da4739f50b5421223734f23
Rob Hanlon <rob@mediapiston.com>
2012-08-15 17:48:02 -0700

    Added gti formula.
    From the homepage:
    
    "It's a program that displays a badly made ASCII-art animation to punish
    you for your typing error - and after that magically launches the
    command you meant to launch.
    
    The code is available under an MIT-like license on github - though of
    course anybody can write his or her own version in a couple of minutes.
    
    This whole thing is heavily inspired by sl, which displays a steam
    locomotive. However, gti is actually nicer than sl as it at least
    executes git after the animation. :-)"

---
+++ Library/Formula/gti.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Gti < Formula
+  homepage 'http://r-wos.org/hacks/gti'
+  url 'https://github.com/rwos/gti/tarball/v1.0.3'
+  sha1 '53ccd11466af6b859e5ec2ceec30f39bdc50c451'
+  head 'git://github.com/rwos/gti.git'
+
+  def install
+    system 'make', "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
+    bin.install 'gti'
+  end
+end
