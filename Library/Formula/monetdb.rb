e73ac9c078fac23161570fb1b32ade73a9e2c610
Steffen Hausmann <dont@beatyourspider.de>
2012-08-16 21:09:37 +0200

    MonetDB 11.11.5
    Disable support for the jaql query language, which is beta-quality and
    fails to build.
    
    Closes #14239.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/monetdb.rb
+++ Library/Formula/monetdb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Monetdb < Formula
   homepage 'http://www.monetdb.org/'
-  url 'http://dev.monetdb.org/downloads/sources/Apr2012-SP2/MonetDB-11.9.7.tar.bz2'
-  sha1 '50029d88fc1be2fcd54085943120bd9e5aecd2ca'
+  url 'http://dev.monetdb.org/downloads/sources/Jul2012/MonetDB-11.11.5.tar.bz2'
+  sha1 'f0961abd7f6c467deb4cc540dbfe304f50944ba3'
 
   head 'http://dev.monetdb.org/hg/MonetDB', :using => :hg
 
@@ -18,6 +18,7 @@ class Monetdb < Formula
                           "--enable-assert=no",
                           "--enable-optimize=yes",
                           "--enable-testing=no",
+                          "--disable-jaql",
                           "--without-rubygem"
     system "make install"
   end
