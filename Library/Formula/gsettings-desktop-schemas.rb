a3f4d5ca41f596aa558b236fd4bcf559fcc9ce67
Chris Koenig <chrisjameskoenig@gmail.com>
2012-08-16 13:52:53 -0500

    New formula: gsettings-desktop-schemas
    Intended to be used by glib-networking, coming in a later commit.

---
+++ Library/Formula/gsettings-desktop-schemas.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class GsettingsDesktopSchemas < Formula
+  homepage 'http://ftp.gnome.org/pub/GNOME/sources/gsettings-desktop-schemas/'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gsettings-desktop-schemas/3.5/gsettings-desktop-schemas-3.5.4.tar.xz'
+  sha256 '0f5686683841a9d5a2d4e1dc60392d69cf2b2920614a5d504bb2cac5daea1df2'
+
+  depends_on 'xz' => :build
+  depends_on 'gettext'
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+end
