aed63fe8161ed7b417d2aef14b7b13fccb2943d4
Misty De Meo <mistydemeo@gmail.com>
2012-08-17 08:19:09 -0500

    texi2html 1.70
    texi2html 5.0 was causing problems; this rolls it back to 1.70, which
    is the same version as shipped on Lion before it was removed.

--- Library/Formula/texi2html.rb
+++ Library/Formula/texi2html.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Texi2html < Formula
   homepage 'http://www.nongnu.org/texi2html/'
-  url 'http://download.savannah.gnu.org/releases/texi2html/texi2html-5.0.tar.bz2'
-  sha1 '20072444ce814d0e74fd7e467d1506908f8c889c'
+  url 'http://download.savannah.gnu.org/releases/texi2html/texi2html-1.70.tar.gz'
+  sha1 'b3472fb5ef80824177a56a1a37983c13497b23be'
 
   keg_only :provided_by_osx unless MacOS.mountain_lion?
 
@@ -16,6 +16,6 @@ class Texi2html < Formula
   end
 
   def test
-    system "texi2html", "--help"
+    system "#{bin}/texi2html", "--help"
   end
 end
