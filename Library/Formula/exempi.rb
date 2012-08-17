61f14741afb2d8f121c0a62df94dd0f56ab581e6
Misty De Meo <mistydemeo@gmail.com>
2012-08-17 14:10:16 -0500

    exempt: style nits
    A few style changes to bring it in line with current style:
    
    * Moved homepage above url
    * Use sha1 instead of md5

--- Library/Formula/exempi.rb
+++ Library/Formula/exempi.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Exempi < Formula
-  url 'http://libopenraw.freedesktop.org/download/exempi-2.2.0.tar.bz2'
   homepage 'http://libopenraw.freedesktop.org/wiki/Exempi'
-  md5 'f46d96975613593ee17aaf48cd350228'
+  url 'http://libopenraw.freedesktop.org/download/exempi-2.2.0.tar.bz2'
+  sha1 '8c90ee42fef86890e4850c3562f8044f9cd66cfb'
 
   depends_on 'boost'
 
