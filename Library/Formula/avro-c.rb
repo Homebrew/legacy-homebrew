8a62ca9d72c984d21a7253a901395ebf80b671c1
Max Howell <mxcl@me.com>
2012-08-17 18:42:14 -0400

    avro-c conflicts with xz

--- Library/Formula/avro-c.rb
+++ Library/Formula/avro-c.rb
@@ -5,6 +5,9 @@ class AvroC < Formula
   url 'http://www.apache.org/dyn/closer.cgi?path=avro/avro-1.6.3/c/avro-c-1.6.3.tar.gz'
   md5 'af3f15605b47d02719706e5d67de8e75'
 
+  # probably should be an optional dep
+  conflicts_with 'xz'
+
   depends_on 'cmake' => :build
 
   def install
