2ee96e76af27981d7556518601d5ba1268ff29cd
Charlie Sharpsteen <source@sharpsteen.net>
2012-08-18 16:06:57 -0700

    libwps: Update to 0.2.7

--- Library/Formula/libwps.rb
+++ Library/Formula/libwps.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libwps < Formula
   homepage 'http://libwps.sourceforge.net'
-  url 'http://sourceforge.net/projects/libwps/files/libwps/libwps-0.2.2/libwps-0.2.2.tar.bz2'
-  md5 '29721a16f25967d59969d5f0ae485b4a'
+  url 'http://sourceforge.net/projects/libwps/files/libwps/libwps-0.2.7/libwps-0.2.7.tar.bz2'
+  sha1 'ccd0b0083a367f293be85f7cea5ba9a8a4355d5c'
 
   depends_on 'pkg-config' => :build
   depends_on 'libwpd'
