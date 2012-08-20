d6d45410fdce19a70ab0ac30970d17e37fb44052
Mark Shropshire <mdshrops@shropnet.net>
2012-08-19 22:32:48 -0400

    Updated drush to 5.7

--- Library/Formula/drush.rb
+++ Library/Formula/drush.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Drush < Formula
   homepage 'http://drupal.org/project/drush'
-  url 'http://ftp.drupal.org/files/projects/drush-7.x-5.6.tar.gz'
-  sha1 '85eb48940833b3acd57927f94c6fd2b35e521c31'
+  url 'http://ftp.drupal.org/files/projects/drush-7.x-5.7.tar.gz'
+  sha1 '2f96f5ea655eb8810a8087c5e7a6f807'
 
   head 'git://git.drupal.org/project/drush.git'
 
