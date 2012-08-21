08e8a92e332deab10d944fb6c6ac57d9a3263693
Max Howell <mxcl@me.com>
2012-08-20 23:53:47 -0400

    conflicts_with message: recommend unlink
    Yes uninstall works, but lets not push people towards that, they can presumably guess that, in the rare case they'd prefer that option.

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -651,10 +651,11 @@ private
       #{formula} cannot be installed alongside #{name.downcase}.
       EOS
       message << "This is because #{opts[:because]}\n" if opts[:because]
-      if !ARGV.force? then message << <<-EOS.undent
-      Please `brew unlink` or `brew uninstall` #{formula} before continuing.
-      To install anyway, use:
-        brew install --force
+      unless ARGV.force? then message << <<-EOS.undent
+        Please `brew unlink #{formula}` before continuing. Unlinking removes
+        the formula's symlinks from #{HOMEBREW_PREFIX}. You can link the
+        formula again after the install finishes. You can --force this install
+        but the build may fail or cause obscure side-effects in the end-binary.
         EOS
       end
 
