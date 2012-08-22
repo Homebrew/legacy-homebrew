00326ca3cd6d82b7f53af3d8583d04d4d1c79a7e
Max Howell <mxcl@me.com>
2012-08-22 09:31:47 -0400

    Fix lastfmfpclient compilation with clang
    NOTE lastfm_fplib is the exact same formula! We need to solve deletion/renames and remove one of them.
    
    Fixes #13485.

--- Library/Formula/lastfmfpclient.rb
+++ Library/Formula/lastfmfpclient.rb
@@ -12,7 +12,18 @@ class Lastfmfpclient < Formula
   depends_on 'mad'
   depends_on 'libsamplerate'
 
+  def inreplace_fix
+    # This project was made on Windows (LOL), patches against Windows
+    # line-endings fail for some reason, so we will inreplace instead.
+    # Fixes compile with clang failure due to entirely missing variable, how
+    # the fuck did GCC ever compile this?!
+    inreplace 'fplib/src/FloatingAverage.h',
+      'for ( int i = 0; i < size; ++i )',
+      'for ( int i = 0; i < m_values.size(); ++i )'
+  end
+
   def install
+    inreplace_fix
     system "cmake", ".", *std_cmake_args
     system "make install"
   end
