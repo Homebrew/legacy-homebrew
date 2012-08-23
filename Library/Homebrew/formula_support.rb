c99a6bb8e05bf3789919f441cc05ada8da0fd68e
Mike McQuaid <mike@mikemcquaid.com>
2012-08-23 09:49:08 -0700

    Fix keg-only on pre-Mountain Lion formulae.

--- Library/Homebrew/formula_support.rb
+++ Library/Homebrew/formula_support.rb
@@ -133,7 +133,7 @@ class KegOnlyReason
     @reason = reason
     @explanation = explanation
     @valid = case @reason
-      when :when_xquartz_installed then MacOS::XQuartz.installed?
+      when :provided_pre_mountain_lion then MacOS.version < :mountain_lion
       else true
       end
   end
@@ -150,8 +150,8 @@ class KegOnlyReason
 
       #{@explanation}
       EOS
-    when :when_xquartz_installed then <<-EOS.undent
-      XQuartz provides this software.
+    when :provided_pre_mountain_lion then <<-EOS.undent
+      Mac OS X already provides this software in versions before Mountain Lion.
 
       #{@explanation}
       EOS
