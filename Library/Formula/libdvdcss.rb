a409351d9351a687041cc550eedb2bf75aac7bba
Mike McQuaid <mike@mikemcquaid.com>
2012-08-22 22:55:28 -0700

    libdvdcss: use sha1 rather than md5.

--- Library/Formula/libdvdcss.rb
+++ Library/Formula/libdvdcss.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Libdvdcss < Formula
-  url 'http://download.videolan.org/pub/libdvdcss/1.2.11/libdvdcss-1.2.11.tar.bz2'
-  md5 'd25d906c3f9007ccd91b1efb909f93e7'
-  head 'svn://svn.videolan.org/libdvdcss/trunk'
   homepage 'http://www.videolan.org/developers/libdvdcss.html'
+  url 'http://download.videolan.org/pub/libdvdcss/1.2.11/libdvdcss-1.2.11.tar.bz2'
+  sha1 '55d75d603071aa4bbcd7a7dcfa63c52dd0e8a104'
+  head 'svn://svn.videolan.org/libdvdcss/trunk'
 
   def install
     system "./bootstrap" if ARGV.build_head?
