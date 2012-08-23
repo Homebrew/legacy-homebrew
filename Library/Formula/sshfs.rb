7fe42b26b1c43925937c6fd64c855fd3d71e6a73
Adam Vandenberg <flangy@gmail.com>
2012-08-23 15:15:33 -0700

    sshfs: style nits

--- Library/Formula/sshfs.rb
+++ Library/Formula/sshfs.rb
@@ -3,8 +3,7 @@ require 'formula'
 class Sshfs < Formula
   homepage 'http://fuse.sourceforge.net/sshfs.html'
   url 'https://github.com/fuse4x/sshfs/tarball/sshfs_2_4_0'
-  md5 'c9ea547b9684ec4d85437393a2731322'
-  version '2.4.0'
+  sha1 '37a9a49a3042fad43f168f25454b56060a48e210'
 
   depends_on :automake
   depends_on :libtool
