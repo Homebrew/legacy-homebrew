e801fca85a43366b92f97292f1bd48e079e2bf0e
Jack Nagel <jacknagel@gmail.com>
2012-08-25 14:54:22 -0500

    pkg-config 0.27.1
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -2,17 +2,9 @@ require 'formula'
 
 class PkgConfig < Formula
   homepage 'http://pkgconfig.freedesktop.org'
-  url 'http://pkgconfig.freedesktop.org/releases/pkg-config-0.27.tar.gz'
-  mirror 'http://fossies.org/unix/privat/pkg-config-0.27.tar.gz'
-  sha256 '79a6b43ee6633c9e6cc03eb1706370bb7a8450659845b782411f969eaba656a4'
-
-  depends_on 'gettext'
-
-  bottle do
-    sha1 '52e1a98740cc834f4b29ee31923812914461f815' => :mountainlion
-    sha1 'ebeb434ee288ac7c96cfa09eee98434fe810edff' => :lion
-    sha1 'b72a6f5078ee917a28c1e6c9948db23701f4dd18' => :snowleopard
-  end
+  url 'http://pkgconfig.freedesktop.org/releases/pkg-config-0.27.1.tar.gz'
+  mirror 'http://fossies.org/unix/privat/pkg-config-0.27.1.tar.gz'
+  sha256 '4f63d0df3035101b12949250da5231af49e3c3afcd8fb18554fa7c3cb92d8c17'
 
   def install
     paths = %W[
@@ -35,11 +27,5 @@ class PkgConfig < Formula
     system "make"
     system "make check"
     system "make install"
-
-    # Fix some bullshit.
-    # pkg-config tries to install glib's m4 macros, which will conflict with
-    # an actual glib install. See:
-    # https://bugs.freedesktop.org/show_bug.cgi?id=52031
-    rm Dir["#{share}/aclocal/g*.m4"]
   end
 end
