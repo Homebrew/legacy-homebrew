8837423a5a9643cf1b2e824c9a31cc72a78c516b
Mike McQuaid <mike@mikemcquaid.com>
2012-08-25 11:31:57 -0700

    Rename tab 'built_bottle' to 'build_as_bottle'.

--- Library/Homebrew/tab.rb
+++ Library/Homebrew/tab.rb
@@ -12,7 +12,7 @@ class Tab < OpenStruct
     Tab.new :used_options => args.used_options(f),
             :unused_options => args.unused_options(f),
             :tabfile => f.prefix + "INSTALL_RECEIPT.json",
-            :built_bottle => !!args.build_bottle?,
+            :built_as_bottle => !!args.build_bottle?,
             :tapped_from => f.tap
   end
 
@@ -34,7 +34,7 @@ class Tab < OpenStruct
       rescue FormulaUnavailableError
         Tab.new :used_options => [],
                 :unused_options => [],
-                :built_bottle => false,
+                :built_as_bottle => false,
                 :tapped_from => ""
       end
     end
@@ -62,7 +62,7 @@ class Tab < OpenStruct
   def self.dummy_tab f
     Tab.new :used_options => [],
             :unused_options => f.build.as_flags,
-            :built_bottle => false,
+            :built_as_bottle => false,
             :tapped_from => ""
   end
 
@@ -78,7 +78,7 @@ class Tab < OpenStruct
     MultiJson.encode({
       :used_options => used_options,
       :unused_options => unused_options,
-      :built_bottle => built_bottle,
+      :built_as_bottle => built_as_bottle,
       :tapped_from => tapped_from
     })
   end
