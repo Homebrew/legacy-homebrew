f69f4fda2f572c4e5830a07191cba540503999bd
Mike McQuaid <mike@mikemcquaid.com>
2012-08-25 13:18:16 -0700

    pkg-config: add bottles.

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -6,6 +6,12 @@ class PkgConfig < Formula
   mirror 'http://fossies.org/unix/privat/pkg-config-0.27.1.tar.gz'
   sha256 '4f63d0df3035101b12949250da5231af49e3c3afcd8fb18554fa7c3cb92d8c17'
 
+  bottle do
+    sha1 '42935c12d2f0496f63bbba4b94c2c02a09035bf0' => :mountainlion
+    sha1 'dd791f33f599972d8c95fba908bf8485c46a772d' => :lion
+    sha1 '3f1f7c324e277c8774e045ffced8966086c237df' => :snowleopard
+  end
+
   def install
     paths = %W[
         #{HOMEBREW_PREFIX}/lib/pkgconfig
