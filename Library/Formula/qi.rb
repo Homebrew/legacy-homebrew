1bf9cd4a977971c558186060494d957ff2323e72
Adam Vandenberg <flangy@gmail.com>
2012-08-25 23:35:10 -0700

    qi: use new dsl

--- Library/Formula/qi.rb
+++ Library/Formula/qi.rb
@@ -1,22 +1,20 @@
 require 'formula'
 
 class Qi < Formula
-  url 'http://www.lambdassociates.org/Download/QiII1.07.zip'
   homepage 'http://www.lambdassociates.org/'
+  url 'http://www.lambdassociates.org/Download/QiII1.07.zip'
   md5 '3a0b5c56d0f107f80f5bca11b82a4d59'
 
-  def options
-    [['--SBCL', 'Use SBCL instead of CLISP']]
-  end
+  option 'SBCL', 'Use SBCL instead of CLISP'
 
-  if ARGV.include? '--SBCL'
+  if build.include? 'SBCL'
     depends_on 'sbcl'
   else
     depends_on 'clisp'
   end
 
   def install
-    if ARGV.include? '--SBCL'
+    if build.include? 'SBCL'
       cd 'Lisp' do
         system "sbcl", "--load", "install.lsp"
       end
