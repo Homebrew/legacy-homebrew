3be5ee5e4dbd02d38e1d6132f3eb378028284bfb
Daniel Luz <dev@mernen.com>
2012-08-26 00:28:54 -0300

    rust: fix compilation for Mountain Lion (clang 4.0)

--- Library/Formula/rust.rb
+++ Library/Formula/rust.rb
@@ -11,9 +11,15 @@ class Rust < Formula
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--enable-clang"
     system "make"
     system "make install"
+  end
+
+  def patches
+    # fix for Mountain Lion's clang 4.0
+    # should be part of next release (commit 50f2db4)
+    DATA
   end
 
   def test
@@ -22,3 +28,18 @@ class Rust < Formula
     system "#{bin}/cargo"
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index 06bddcc..040bae9 100755
+--- a/configure
++++ b/configure
+@@ -400,7 +400,7 @@ then
+                       | cut -d ' ' -f 2)
+ 
+     case $CFG_CLANG_VERSION in
+-        (3.0svn | 3.0 | 3.1)
++        (3.0svn | 3.0 | 3.1 | 4.0)
+         step_msg "found ok version of CLANG: $CFG_CLANG_VERSION"
+         CFG_C_COMPILER="clang"
+         ;;
