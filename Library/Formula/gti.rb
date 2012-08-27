60e9ed90a029b52249586d8ff618abf6952664f1
Adam Vandenberg <flangy@gmail.com>
2012-08-26 22:55:12 -0700

    gti: fix audit warning

--- Library/Formula/gti.rb
+++ Library/Formula/gti.rb
@@ -5,7 +5,7 @@ class Gti < Formula
   url 'https://github.com/rwos/gti/tarball/v1.0.3'
   sha1 '53ccd11466af6b859e5ec2ceec30f39bdc50c451'
 
-  head 'git://github.com/rwos/gti.git'
+  head 'https://github.com/rwos/gti.git'
 
   def install
     system 'make', "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
