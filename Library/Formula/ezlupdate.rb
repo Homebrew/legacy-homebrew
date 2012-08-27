71e56b4b29023394e39bbde9801f6005e964618c
Adam Vandenberg <flangy@gmail.com>
2012-08-26 22:22:48 -0700

    ezlupdate: version is detected

--- Library/Formula/ezlupdate.rb
+++ Library/Formula/ezlupdate.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Ezlupdate < Formula
   homepage 'http://ezpedia.org/ez/ezlupdate'
   url 'https://github.com/downloads/ezsystems/ezpublish/ezpublish_community_project-2011.10-with_ezc.tar.bz2'
-  version '2011.10'
   md5 'd40cbcf714c1071ffb6ee2c1e4975282'
 
   depends_on 'qt'
