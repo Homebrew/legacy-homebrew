808d790c18fa79f732d6ca145b59ae24a44a2ad7
Steve Gury <stevegury@twitter.com>
2012-08-28 15:24:01 -0700

    Update ocaml to 4.0.0

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -1,26 +1,11 @@
 require 'formula'
 
 class ObjectiveCaml < Formula
-  homepage 'http://caml.inria.fr/ocaml/index.en.html'
-  url 'http://caml.inria.fr/pub/distrib/ocaml-3.12/ocaml-3.12.1.tar.bz2'
-  sha1 '29b44117b116b1a5bc54a8b4514af483793a769f'
+  homepage 'http://www.ocaml-lang.org'
+  url 'http://caml.inria.fr/pub/distrib/ocaml-4.00/ocaml-4.00.0.tar.gz'
+  sha1 '9653e76dd14f0fbb750d7b438415890ab9fe2f4e'
 
   head 'http://caml.inria.fr/svn/ocaml/trunk', :using => :svn
-
-  devel do
-    url 'http://caml.inria.fr/pub/distrib/ocaml-4.00/ocaml-4.00.0+beta2.tar.bz2'
-    sha1 '75fedc849cc1cbfe620f0258c0f6c8214467efb7'
-    version '4.00.0+beta2'
-  end
-
-  bottle do
-    sha1 'e5e28c74b859b8bb15a11f7f2a7a33608671b1b9' => :snowleopard
-    sha1 'f32709be6cba5639a3f7185835963d630d6f8b59' => :lion
-  end
-
-  def patches
-    "http://caml.inria.fr/mantis/file_download.php?file_id=723&type=bug"
-  end
 
   # Don't strip symbols, so dynamic linking doesn't break.
   skip_clean :all
