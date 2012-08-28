f7b4ce056e9df713e087b0cd15ce09af4793285a
Jack Nagel <jacknagel@gmail.com>
2012-08-27 22:08:13 -0500

    Simplify `brew outdated` even further
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Homebrew/cmd/outdated.rb
+++ Library/Homebrew/cmd/outdated.rb
@@ -5,7 +5,7 @@ module Homebrew extend self
   def outdated
     outdated_brews do |f|
       if $stdout.tty? and not ARGV.flag? '--quiet'
-        versions = f.rack.subdirs.map { |d| Keg.new(d) }.map { |keg| keg.basename }
+        versions = f.rack.subdirs.map { |d| Keg.new(d).version }
         puts "#{f.name} (#{versions*', '} < #{f.version})"
       else
         puts f.name
@@ -14,11 +14,8 @@ module Homebrew extend self
   end
 
   def outdated_brews
-    HOMEBREW_CELLAR.subdirs.reject { |d| d.subdirs.empty? }.map do |rack|
-      f = Formula.factory(rack.basename.to_s) rescue nil
-      next if f.nil?
-
-      kegs = rack.subdirs.map { |d| Keg.new(d) }
+    Formula.installed.map do |f|
+      kegs = f.rack.subdirs.map { |d| Keg.new(d) }
       unless kegs.any? { |keg| keg.version >= f.version }
         yield f if block_given?
         f
