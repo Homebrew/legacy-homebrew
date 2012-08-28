bcb2ae6dc841217f68dcb40f2e34cb31fefa0d6e
Dmitry Nedospasov <dmitry@nedos.net>
2012-08-28 19:49:58 +0200

    avra: build environment depends on automake
    Closes #14505.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/avra.rb
+++ Library/Formula/avra.rb
@@ -5,6 +5,8 @@ class Avra < Formula
   url 'http://sourceforge.net/projects/avra/files/1.3.0/avra-1.3.0.tar.bz2'
   md5 'd5d48369ceaa004c4ca09f61f69b2c84'
 
+  depends_on :automake
+
   def install
     # build fails if these don't exist
     system "touch NEWS ChangeLog"
