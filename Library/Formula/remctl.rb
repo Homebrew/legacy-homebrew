0bdf4960cbee035ff566aa79e7c9754260f278d9
Jack Nagel <jacknagel@gmail.com>
2012-08-28 10:30:00 -0500

    remctl: add dep on PCRE
    Fixes #14495.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/remctl.rb
+++ Library/Formula/remctl.rb
@@ -5,8 +5,12 @@ class Remctl < Formula
   url 'http://archives.eyrie.org/software/kerberos/remctl-3.2.tar.gz'
   sha1 'f49c287c29b6b289995b8907edc6ecba4c298c99'
 
+  depends_on 'pcre'
+
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-pcre=#{HOMEBREW_PREFIX}"
     system "make install"
   end
 
