b5b6b6183ed4902f1ba37ebc4757dba019b39061
Adam Vandenberg <flangy@gmail.com>
2012-08-27 21:59:26 -0700

    kyoto-tycoon: use new dsl

--- Library/Formula/kyoto-tycoon.rb
+++ Library/Formula/kyoto-tycoon.rb
@@ -5,16 +5,14 @@ class KyotoTycoon < Formula
   url 'http://fallabs.com/kyototycoon/pkg/kyototycoon-0.9.56.tar.gz'
   sha1 'e5433833e681f8755ff6b9f7209029ec23914ce6'
 
-  depends_on 'lua' unless ARGV.include? "--no-lua"
-  depends_on 'kyoto-cabinet'
+  option "no-lua", "Disable Lua support"
 
-  def options
-    [["--no-lua", "Disable Lua support (and don't force Lua install.)"]]
-  end
+  depends_on 'lua' unless build.include? "no-lua"
+  depends_on 'kyoto-cabinet'
 
   def install
     args = ["--prefix=#{prefix}"]
-    args << "--enable-lua" unless ARGV.include? "--no-lua"
+    args << "--enable-lua" unless build.include? "no-lua"
 
     system "./configure", *args
     system "make"
