e7f1fc55c41b0d24d3438f616cf19555ad5e66f4
Adam Vandenberg <flangy@gmail.com>
2012-08-30 12:50:28 -0700

    ngspice: add X11 dep

--- Library/Formula/ngspice.rb
+++ Library/Formula/ngspice.rb
@@ -3,19 +3,20 @@ require 'formula'
 class Ngspice < Formula
   homepage 'http://ngspice.sourceforge.net/'
   url 'http://downloads.sourceforge.net/project/ngspice/ng-spice-rework/24/ngspice-24.tar.gz'
-  md5 'e9ed7092da3e3005aebd892996b2bd5f'
+  sha1 'a6bb0e65c1c07d48f1d3e4df44fc17f6262952eb'
 
-  def options
-    [["--without-xspice", "Build without x-spice extensions"]]
-  end
+  option "without-xspice", "Build without x-spice extensions"
+
+  depends_on :x11
 
   def install
-    args = ["--disable-debug",
-            "--disable-dependency-tracking",
-            "--prefix=#{prefix}",
-            "--with-editline=yes",
-            "--enable-x"]
-    args << "--enable-xspice" unless ARGV.include? "--without-xspice"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-editline=yes
+      --enable-x
+    ]
+    args << "--enable-xspice" unless build.include? "without-xspice"
 
     system "./configure", *args
     system "make install"
