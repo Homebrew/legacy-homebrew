4ef158906312451f267821e287a2a38e8ee7343e
Berk D. Demir <bdd@mindcast.org>
2012-08-30 14:48:29 -0700

    spiped 1.2.0
    This release adds ``spipe'', a client to spiped which encrypts a single
    connection and as the unencrypted end of that connection uses the
    standard input and output.  In other words, spipe acts like 'netcat' or
    'openssl s_client'.
    
    A practical use of spipe as an OpenSSH ProxyCommand to create hidden in
    plain sight SSH services is explained at its authors blog.
    
    Closes #14576.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/spiped.rb
+++ Library/Formula/spiped.rb
@@ -2,12 +2,18 @@ require 'formula'
 
 class Spiped < Formula
   homepage 'http://www.tarsnap.com/spiped.html'
-  url 'http://www.tarsnap.com/spiped/spiped-1.1.0.tgz'
-  sha256 'b727b902310d217d56c07d503c4175c65387ff07c9cd50a24584903faf9f3dc3'
+  url 'https://www.tarsnap.com/spiped/spiped-1.2.0.tgz'
+  sha256 '46fb94da62bf1f074513047519f2d4f5faf103e5b8555e14a375a42b74508a6b'
 
   depends_on :bsdmake
 
+  def patches
+    # - Include ``stdint.h'' for ``uint8_t'' definition.
+    # - Remove unconditional ``librt'' from LDADD.
+    "https://raw.github.com/gist/3541617/e212734f3ff105f636ea6e225ad7c9d5cb0ce347/spiped-1.2.0-macosx-mountain-lion.patch"
+  end
+
   def install
-    system "bsdmake", "LDADD=-lcrypto", "BINDIR_DEFAULT=#{bin}", "install"
+    system "bsdmake", "BINDIR_DEFAULT=#{bin}", "install"
   end
 end
