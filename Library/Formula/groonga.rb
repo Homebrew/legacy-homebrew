e5a5e79b6e26c8c84f39aed6fc71f517f098ca03
Adam Vandenberg <flangy@gmail.com>
2012-08-31 08:49:54 -0700

    groonga: add missing deps
    Closes #14588.

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -5,6 +5,8 @@ class Groonga < Formula
   url 'http://packages.groonga.org/source/groonga/groonga-2.0.6.tar.gz'
   sha1 'a012a86d03e193f2ab029de2f1e09aa5f0e52bcf'
 
+  depends_on 'pkg-config' => :build
+  depends_on 'pcre'
   depends_on 'msgpack'
 
   def install
