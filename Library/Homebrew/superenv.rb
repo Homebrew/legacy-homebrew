3a637c2a92f96c89b9971a26a7d6a5d429599eab
Max Howell <mxcl@me.com>
2012-08-31 10:26:22 -0400

    Raise if sdk_path is nil for nclt
    This situation should be impossible now (in that, we should detect sdk_path provided we also detected nclt), so if it happens somehow raise in such a way that the user will be encouraged to report the bug.

--- Library/Homebrew/superenv.rb
+++ Library/Homebrew/superenv.rb
@@ -40,6 +40,7 @@ class << ENV
 
   def setup_build_environment
     reset
+    check
     ENV['LD'] = 'cc'
     ENV['MAKEFLAGS'] ||= "-j#{determine_make_jobs}"
     ENV['PATH'] = determine_path
@@ -52,6 +53,10 @@ class << ENV
     ENV['CMAKE_INCLUDE_PATH'] = determine_cmake_include_path
     ENV['ACLOCAL_PATH'] = determine_aclocal_path
     ENV['VERBOSE'] = '1' if ARGV.verbose?
+  end
+
+  def check
+    raise if MacSystem.xcode43_without_clt? and MacOS.sdk_path.nil?
   end
 
   def universal_binary
