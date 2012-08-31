06c9e24505c96dc28055a1b75e0de272697bf0d9
Arild Shirazi <as4@eshirazi.com>
2012-08-30 23:13:11 -0500

    Adding a new formula for git-flow-clone, which provides a convenience method for cloning a Gitflow repository.

--- Library/Formula/git-flow-clone.rb
+++ Library/Formula/git-flow-clone.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class GitFlowClone < Formula
   homepage 'https://github.com/ashirazi/git-flow-clone'
-  url 'https://github.com/ashirazi/git-flow-clone/tarball/master', :tag => '0.1.0'
-  sha1 '039361d7ec4ff6076daf18f0c8bf704d113824b3'
-  version '0.1.0'
+  url 'https://github.com/ashirazi/git-flow-clone.git', :tag => '0.1.1'
+  sha1 'a5df4bdee240a16013fbc5969f834382f6135226'
+  version '0.1.1'
   depends_on 'git-flow'
 
   def install
+    puts "prefix is #{prefix}"
     system "make", "prefix=#{prefix}", "install"
   end
 
