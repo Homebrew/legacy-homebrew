716e0b34582a3da1d8d709a2a5d6f22b5e56e0b9
nibbles 2bits <nibbles@speakeasy.net>
2012-08-31 10:16:19 -0700

    sam2p 0.49.1
    Upgrade sam2p to version 0.49.1.
    Add `--disable-debug`
    Add `fails_with :clang`
    Works well with superenv.
    
    Fixes #14592
    
    Closes #14599.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/sam2p.rb
+++ Library/Formula/sam2p.rb
@@ -1,13 +1,20 @@
 require 'formula'
 
 class Sam2p < Formula
-  url 'http://sam2p.googlecode.com/files/sam2p-0.49.tar.gz'
   homepage 'http://code.google.com/p/sam2p/'
-  md5 '72387e3e3897580fb5ae439713e75177'
+  url 'http://sam2p.googlecode.com/files/sam2p-0.49.1.tar.gz'
+  sha1 '5922e6029e2ed4e524066080476e02ddfdbcc18e'
+
+  fails_with :clang do
+    build 421
+    cause "treating 'c' input as 'c++' when in C++ mode, this behavior is deprecated"
+  end
 
   def install
-    system "./configure", "--enable-lzw", "--enable-gif",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--enable-lzw",
+                          "--enable-gif"
     system "make"
 
     bin.install "sam2p"
