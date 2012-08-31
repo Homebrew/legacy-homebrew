ce8c49b142069cfea9303ff4d5c8bcea9b74bffe
Max Howell <mxcl@me.com>
2012-08-30 22:54:24 -0400

    String.f convenience function for brew irb
    eg. "ack".f

--- Library/Homebrew/cmd/irb.rb
+++ Library/Homebrew/cmd/irb.rb
@@ -1,13 +1,21 @@
+require 'formula'
+require 'keg'
+require 'irb'
+
+class String
+  def f
+    Formula.factory(self)
+  end
+end
+
 module Homebrew extend self
   def irb
     if ARGV.include? "--help"
       puts "Formula.factory('ace').installed?"
+      puts "ack.f # => instance of the Ack formula"
     else
       ohai "Interactive Homebrew Shell"
       puts "Example commands available with: brew irb --help"
-      require 'formula'
-      require 'keg'
-      require 'irb'
       IRB.start
     end
   end
