62b180d58f2cc4dc4aae9882eadb7fdb20efbfb3
Adam Vandenberg <flangy@gmail.com>
2012-09-01 13:26:50 -0700

    parrot --devel 4.7.0

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -5,6 +5,11 @@ class Parrot < Formula
   url 'ftp://ftp.parrot.org/pub/parrot/releases/supported/4.6.0/parrot-4.6.0.tar.bz2'
   sha256 '31d48e348eab418d5d9d9f9bb24d628763ff90c608d21b1944c227b6938a69d1'
 
+  devel do
+    url 'ftp://ftp.parrot.org/pub/parrot/releases/devel/4.7.0/parrot-4.7.0.tar.bz2'
+    sha256 '0ab6a6ce1a20191d1d143e792ff916fcd37fc024'
+  end
+
   head 'https://github.com/parrot/parrot.git'
 
   depends_on 'gmp' => :optional
