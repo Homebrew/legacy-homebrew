6bcb2c61a10396bac0d43bb29962b3757f7ebcc6
Jack Nagel <jacknagel@gmail.com>
2012-09-01 12:41:41 -0500

    help2man 1.40.12
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/help2man.rb
+++ Library/Formula/help2man.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Help2man < Formula
   homepage 'http://www.gnu.org/software/help2man/'
-  url 'http://ftpmirror.gnu.org/help2man/help2man-1.40.11.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/help2man/help2man-1.40.11.tar.gz'
-  sha256 '34ad76638facd75d5bbc771a6827d1cbc43c435ae64321fb5a9d7b339a861835'
+  url 'http://ftpmirror.gnu.org/help2man/help2man-1.40.12.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/help2man/help2man-1.40.12.tar.gz'
+  sha256 'dec8e6e3f570b745a8484d7a4229135f1cf1c5257adf1c590cd094f7d591e4e4'
 
   def install
     # install is not parallel safe
