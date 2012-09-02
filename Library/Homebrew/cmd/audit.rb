8792bc25bf4c637e920a9f3d9ddf035c085216f7
Mike McQuaid <mike@mikemcquaid.com>
2012-09-02 01:25:36 -0700

    brew-audit: recommend SHA1 over MD5.

--- Library/Homebrew/cmd/audit.rb
+++ Library/Homebrew/cmd/audit.rb
@@ -233,6 +233,8 @@ class FormulaAuditor
         when :sha256 then 64
         end
 
+      problem "md5 is broken, deprecated: use sha1 instead" if cksum.hash_type == :md5
+
       if cksum.empty?
         problem "#{cksum.hash_type} is empty"
       else
