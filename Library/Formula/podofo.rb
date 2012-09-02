4c749289ffeaad9b0cb1118420013b6ea94936d6
Mike McQuaid <mike@mikemcquaid.com>
2012-09-02 01:09:50 -0700

    podofo: fix SHA1 and X11 dependency.

--- Library/Formula/podofo.rb
+++ Library/Formula/podofo.rb
@@ -5,13 +5,12 @@ class Podofo < Formula
   url 'http://downloads.sourceforge.net/podofo/podofo-0.9.1.tar.gz'
   sha1 'd04c26b93dcf5f82a8dd90e02df6de95fb98ef47'
 
-  depends_on 'pkg-config' => :build
   depends_on 'cmake' => :build
-
-  depends_on :x11
+  depends_on :libpng
+  depends_on :freetype
+  depends_on :fontconfig
   depends_on 'jpeg'
   depends_on 'libtiff'
-  depends_on 'lua'
 
   def install
     mkdir 'build' do
