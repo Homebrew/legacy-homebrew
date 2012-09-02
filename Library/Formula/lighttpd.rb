9c2b9e729b6cc7c6a9c00644a14641293383ec8e
Adam Vandenberg <flangy@gmail.com>
2012-09-02 11:51:43 -0700

    lighttpd: use new dsl

--- Library/Formula/lighttpd.rb
+++ Library/Formula/lighttpd.rb
@@ -5,13 +5,11 @@ class Lighttpd < Formula
   url 'http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.31.tar.bz2'
   sha256 '5209e7a25d3044cb21b34d6a2bb3a6f6c216ba903ea486a803d070582e5e26ac'
 
+  option 'with-lua', 'Include Lua scripting support for mod_magnet'
+
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
-  depends_on 'lua' if ARGV.include? '--with-lua'
-
-  def options
-    [['--with-lua', 'Include Lua scripting support for mod_magnet']]
-  end
+  depends_on 'lua' => :optional if build.include? 'with-lua'
 
   def install
     args = %W[
@@ -20,7 +18,7 @@ class Lighttpd < Formula
       --with-openssl
       --with-ldap
     ]
-    args << "--with-lua" if ARGV.include? '--with-lua'
+    args << "--with-lua" if build.include? 'with-lua'
     system "./configure", *args
     system "make install"
   end
