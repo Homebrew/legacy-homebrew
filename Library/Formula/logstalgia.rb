f58a1c34c4e74ca9e687912edfe4bda51ca6e700
Mike McQuaid <mike@mikemcquaid.com>
2012-09-02 01:05:50 -0700

    logstalgia: fix SHA1, build.*? and X11 dependency.

--- Library/Formula/logstalgia.rb
+++ Library/Formula/logstalgia.rb
@@ -7,15 +7,16 @@ class Logstalgia < Formula
 
   head 'https://github.com/acaudwell/Logstalgia.git'
 
-  depends_on 'pkg-config' => :build
   depends_on 'sdl'
-  depends_on 'sdl_image'
+  depends_on :freetype
+  depends_on 'pkg-config' => :build
   depends_on 'ftgl'
+  depends_on :libpng
   depends_on 'jpeg'
+  depends_on 'sdl_image'
   depends_on 'pcre'
-  depends_on :x11
 
-  if ARGV.build_head?
+  if build.head?
     depends_on :automake
     depends_on :libtool
   end
@@ -25,7 +26,7 @@ class Logstalgia < Formula
     ENV.append "CXXFLAGS", "-I#{HOMEBREW_PREFIX}/include"
 
     # Handle building head.
-    system "autoreconf -f -i" if ARGV.build_head?
+    system "autoreconf -f -i" if build.head?
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
