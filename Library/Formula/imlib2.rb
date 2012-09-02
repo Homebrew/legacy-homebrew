62f7f2646f50b6e14a4bc90d187778f5bbc20d49
Mike McQuaid <mike@mikemcquaid.com>
2012-09-02 00:59:54 -0700

    imlib2: fix SHA1, test and X11 dependency.

--- Library/Formula/imlib2.rb
+++ Library/Formula/imlib2.rb
@@ -6,12 +6,23 @@ class Imlib2 < Formula
   sha1 'af86a2c38f4bc3806db57e64e74dc9814ad474a0'
 
   depends_on 'pkg-config' => :build
-  depends_on :x11 # for Freetype
+  depends_on :freetype
+  depends_on :libpng => :recommended
+  depends_on 'jpeg' => :recommended
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--enable-amd64=no"
+                          "--enable-amd64=no",
+                          "--without-x"
     system "make install"
+  end
+
+  def test
+    mktemp do
+      system "#{bin}/imlib2_conv", \
+        "/System/Library/Frameworks/SecurityInterface.framework/Versions/A/Resources/Key_Large.png", \
+        "imlib2_test.jpg"
+    end
   end
 end
