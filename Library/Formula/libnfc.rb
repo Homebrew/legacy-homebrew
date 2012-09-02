be0f1bfb1abd9889730240e535fd56533eda680d
Adam Vandenberg <flangy@gmail.com>
2012-09-02 11:42:54 -0700

    libnfc: use new dsl

--- Library/Formula/libnfc.rb
+++ Library/Formula/libnfc.rb
@@ -1,21 +1,22 @@
 require 'formula'
 
 class Libnfc < Formula
-  url 'http://libnfc.googlecode.com/files/libnfc-1.5.1.tar.gz'
   homepage 'http://www.libnfc.org/'
-  md5 '81e3e59496060dc495c95844654a8038'
+  url 'http://libnfc.googlecode.com/files/libnfc-1.5.1.tar.gz'
+  sha1 'aebd4a126f2268d06ade445cd6cf2b3c59256f51'
 
   depends_on 'libusb-compat'
 
-  def options
-    [['--with-pn532_uart', 'Enable PN532 UART support']]
-  end
+  option 'with-pn532_uart', 'Enable PN532 UART support'
 
   def install
-    args = ["--disable-debug", "--disable-dependency-tracking",
-            "--prefix=#{prefix}"]
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
 
-    if ARGV.include? '--with-pn532_uart'
+    if build.include? 'with-pn532_uart'
       args << "--enable-serial-autoprobe"
       args << "--with-drivers=pn532_uart"
     end
