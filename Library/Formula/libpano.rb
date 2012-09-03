9b116a93e72c413513e5712fb2de2e2da8e2f4d6
Adam Vandenberg <flangy@gmail.com>
2012-09-03 14:45:52 -0700

    libpano: remove unused configure switch

--- Library/Formula/libpano.rb
+++ Library/Formula/libpano.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libpano < Formula
+  homepage 'http://panotools.sourceforge.net/'
   url 'http://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.18/libpano13-2.9.18.tar.gz'
   version '13-2.9.18'
-  homepage 'http://panotools.sourceforge.net/'
   sha1 '23849bdbdfc9176a2b53d157e58bd24aa0e7276e'
 
   depends_on :libpng
@@ -11,7 +11,7 @@ class Libpano < Formula
   depends_on 'libtiff'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
     system "make install"
