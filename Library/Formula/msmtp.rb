6bda972a8290c2cc1d5d320ebbb6e0c3c2778aa6
Adam Vandenberg <flangy@gmail.com>
2012-09-03 13:51:13 -0700

    msmtp: use new dsl

--- Library/Formula/msmtp.rb
+++ Library/Formula/msmtp.rb
@@ -5,15 +5,16 @@ class Msmtp < Formula
   url 'http://downloads.sourceforge.net/project/msmtp/msmtp/1.4.28/msmtp-1.4.28.tar.bz2'
   sha1 '3fd44b30e8f4ae071b2a5a205d6007f3465fa970'
 
+  option 'with-macosx-keyring', "Support Mac OS X Keyring"
+
   depends_on 'pkg-config' => :build
 
-  def options
-    [['--with-macosx-keyring', "Support Mac OS X Keyring"]]
-  end
-
   def install
-    args = [ "--disable-dependency-tracking", "--prefix=#{prefix}" ]
-    args << "--with-macosx-keyring" if ARGV.include? '--with-macosx-keyring'
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+    args << "--with-macosx-keyring" if build.include? 'with-macosx-keyring'
 
     system "./configure", *args
     system "make install"
