df366d4b1bf98d482acb1fcb54f69666494388ff
Adam Vandenberg <flangy@gmail.com>
2012-09-03 20:12:48 -0700

    htmlcompressor: use new dsl

--- Library/Formula/htmlcompressor.rb
+++ Library/Formula/htmlcompressor.rb
@@ -1,25 +1,23 @@
 require 'formula'
 
 class Htmlcompressor < Formula
-  url 'http://htmlcompressor.googlecode.com/files/htmlcompressor-1.5.3.jar'
   homepage 'http://code.google.com/p/htmlcompressor/'
+  url 'http://htmlcompressor.googlecode.com/files/htmlcompressor-1.5.3.jar'
   sha1 '57db73b92499e018b2f2978f1c7aa7b1238c7a39'
 
-  def options
-    [
-      ['--yuicompressor', "Use YUICompressor for JS/CSS compression."],
-    ]
-  end
+  option 'yuicompressor', "Use YUICompressor for JS/CSS compression"
 
-  depends_on "yuicompressor" if ARGV.include? '--yuicompressor'
+  depends_on "yuicompressor" => :optional if build.include? 'yuicompressor'
 
   def install
     libexec.install "htmlcompressor-#{version}.jar"
+
     (bin+'htmlcompressor').write <<-EOS.undent
       #!/bin/sh
-      java -jar "#{libexec}/htmlcompressor-#{version}.jar" $@
+      java -jar "#{libexec}/htmlcompressor-#{version}.jar" "$@"
     EOS
-    if ARGV.include? '--yuicompressor'
+
+    if build.include? 'yuicompressor'
       yui = Formula.factory('yuicompressor')
       yui_jar = "yuicompressor-#{yui.version}.jar"
       ln_s "#{yui.libexec}/#{yui_jar}", "#{libexec}/#{yui_jar}"
