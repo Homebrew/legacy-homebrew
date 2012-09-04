99c2194a89c09885059b7d5e9c33bda93f897b19
nibbles 2bits <nibbles@speakeasy.net>
2012-09-04 11:45:43 -0700

    libgtextutils 0.6.1
    Upgrade libgtextutils to version 0.6.1. Ok with Clang on 10.8

--- Library/Formula/libgtextutils.rb
+++ Library/Formula/libgtextutils.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libgtextutils < Formula
   homepage 'http://hannonlab.cshl.edu/fastx_toolkit/'
-  url 'http://hannonlab.cshl.edu/fastx_toolkit/libgtextutils-0.6.tar.bz2'
-  sha1 '0e3ad375249d7f5236d18285e8a84141e118b119'
+  url 'http://hannonlab.cshl.edu/fastx_toolkit/libgtextutils-0.6.1.tar.bz2'
+  sha1 'dbf1714be75511feae3313904a7449a1f680bc23'
 
   def install
     system "./configure", "--disable-debug",
