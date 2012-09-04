Homebrew/legacy-homebrew

commit 3c5ca25cad4a19b72006dffe1c782a58f4e8154b

Batch convert MD5 formula to SHA1.
Closes #14653.

Library/Formula/scala.rb
@@ -3,20 +3,20 @@ require 'formula'
 class ScalaDocs < Formula
   homepage 'http://www.scala-lang.org/'
   url 'http://www.scala-lang.org/downloads/distrib/files/scala-docs-2.9.2.zip'
-  md5 '48d9102789bf696d3d8621fb24ed47da'
+  sha1 '806fc1d91bda82d6a584172d7742531386ae68fb'
 end
 
 class ScalaCompletion < Formula
   homepage 'http://www.scala-lang.org/'
   url 'https://raw.github.com/scala/scala-dist/27bc0c25145a83691e3678c7dda602e765e13413/completion.d/2.9.1/scala'
   version '2.9.1'
-  md5 '40cb02604b718fd0977a12d932b9e693'
+  sha1 '806fc1d91bda82d6a584172d7742531386ae68fb'
 end
 
 class Scala < Formula
   homepage 'http://www.scala-lang.org/'
   url 'http://www.scala-lang.org/downloads/distrib/files/scala-2.9.2.tgz'
-  md5 'bc1f68ce3f989347231f2e23784deee9'
+  sha1 '806fc1d91bda82d6a584172d7742531386ae68fb'
 
   option 'with-docs', 'Also install library documentation'
 

commit 00129358bac4a559a195b5ccd006db43274876bb

change 3c5ca25 broke the scala formula by using the same sha1 sum for all scala artifacts that get installed. This change fixes that by providing the correct sha1 sums.

Library/Formula/scala.rb
@@ -3,14 +3,14 @@ require 'formula'
 class ScalaDocs < Formula
   homepage 'http://www.scala-lang.org/'
   url 'http://www.scala-lang.org/downloads/distrib/files/scala-docs-2.9.2.zip'
-  sha1 '806fc1d91bda82d6a584172d7742531386ae68fb'
+  sha1 'b49ef500314d968ddbd683b64628925a747f35e5'
 end
 
 class ScalaCompletion < Formula
   homepage 'http://www.scala-lang.org/'
   url 'https://raw.github.com/scala/scala-dist/27bc0c25145a83691e3678c7dda602e765e13413/completion.d/2.9.1/scala'
   version '2.9.1'
-  sha1 '806fc1d91bda82d6a584172d7742531386ae68fb'
+  sha1 'e2fd99fe31a9fb687a2deaf049265c605692c997'
 end
 
 class Scala < Formula