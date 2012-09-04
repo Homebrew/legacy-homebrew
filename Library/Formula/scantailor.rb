32367afa1ae673cdf897c09e9440291c8d7aff72
Misty De Meo <mistydemeo@gmail.com>
2012-09-04 09:45:06 -0500

    scantailor enhanced-20120812

--- Library/Formula/scantailor.rb
+++ Library/Formula/scantailor.rb
@@ -6,9 +6,9 @@ class Scantailor < Formula
   sha1 '21ec03317ca2b278179693237eaecd962ee0263b'
 
   devel do
-    url 'http://downloads.sourceforge.net/project/scantailor/scantailor-devel/enhanced/scantailor-enhanced-20111213.tar.gz'
-    version 'enhanced-20111213'
-    sha1 'a5fc7cd8802d6898275f07811d51952cafa8e7ea'
+    url 'http://downloads.sourceforge.net/project/scantailor/scantailor-devel/enhanced/scantailor-enhanced-20120812.tar.bz2'
+    version 'enhanced-20120812'
+    sha1 'e3535d6e21a1844cf83eb2b23469fb6d90c070a9'
   end
 
   depends_on 'cmake' => :build
