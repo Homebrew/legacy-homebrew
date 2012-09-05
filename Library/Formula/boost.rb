1acb25ccdf51419f00de9ede1ca0e1f7a2393c64
Nikita Kakuev <nikita.kakuev@gmail.com>
2012-09-06 01:03:29 +0400

    boost: add option to use system layout.
    Closes #14749.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -4,6 +4,10 @@ def needs_universal_python?
   build.universal? and not build.include? "without-python"
 end
 
+def boost_layout
+  (build.include? "use-system-layout") ? "system" : "tagged"
+end
+
 class UniversalPython < Requirement
   def message; <<-EOS.undent
     A universal build was requested, but Python is not a universal build
@@ -37,6 +41,7 @@ class Boost < Formula
   option 'without-python', 'Build without Python'
   option 'with-icu', 'Build regexp engine with icu support'
   option 'with-c++11', 'Compile using Clang, std=c++11 and stdlib=libc++' if MacOS.version >= :lion
+  option 'use-system-layout', 'Use system layout instead of tagged'
 
   depends_on UniversalPython.new if needs_universal_python?
   depends_on "icu4c" if build.include? "with-icu"
@@ -96,7 +101,7 @@ class Boost < Formula
             "--libdir=#{lib}",
             "-d2",
             "-j#{ENV.make_jobs}",
-            "--layout=tagged",
+            "--layout=#{boost_layout}",
             "--user-config=user-config.jam",
             "threading=multi",
             "install"]
