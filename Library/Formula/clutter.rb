abeeb0dd4060e381d32c3cb86a4c59c57b9dcb6d
Jack Nagel <jacknagel@gmail.com>
2012-09-05 15:56:30 -0500

    Use HTTP for gnome URLs
    c.f. #10377.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/clutter.rb
+++ Library/Formula/clutter.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Clutter < Formula
   homepage 'http://clutter-project.org/'
-  url 'http://download.gnome.org/sources/clutter/1.10/clutter-1.10.8.tar.xz'
+  url 'http://ftp.gnome.org/pub/gnome/sources/clutter/1.10/clutter-1.10.8.tar.xz'
   sha256 '7c08c2deff62c134c1a3d18e04dcb6fbae4bbc541c800638b9fc3d71fe4a64bf'
 
   depends_on 'pkg-config' => :build
