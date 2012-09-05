c7167221e350382cf7cb81663a986f63c911a9e1
Alastair <alastair.andrew@gmail.com>
2012-09-05 21:56:07 +0200

    Added in the sha1 for the package
    Amend the formula to include the sha1 value generated by brew fetch

--- Library/Formula/chktex.rb
+++ Library/Formula/chktex.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Chktex < Formula
   url 'http://download.savannah.gnu.org/releases/chktex/chktex-1.7.1.tar.gz'
   homepage 'http://www.nongnu.org/chktex/'
-  #sha1 'b42b6a69e17373760c9653cce0add6ffc741312b'
+  sha1 'ed94b96ed8ce65fb1cef1b5fc019045c5b2dd8a8'
 
   def install
     # Seriously, don't pause and show ASCII art
