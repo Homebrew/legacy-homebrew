ed7e5115a7581d081b6af684711c2e758ef9db49
Adam Vandenberg <flangy@gmail.com>
2012-09-05 20:59:43 -0700

    mobile-shell: use new dsl

--- Library/Formula/mobile-shell.rb
+++ Library/Formula/mobile-shell.rb
@@ -9,12 +9,12 @@ class MobileShell < Formula
 
   # Needs new autoconf for correct AC_C_RESTRICT macro
   # See: https://github.com/keithw/mosh/issues/241
-  depends_on 'autoconf' => :build if ARGV.build_head?
+  depends_on 'autoconf' => :build if build.head?
   depends_on 'pkg-config' => :build
   depends_on 'protobuf'
 
   def install
-    system "./autogen.sh" if ARGV.build_head?
+    system "./autogen.sh" if build.head?
 
     # Upstream prefers O2:
     # https://github.com/keithw/mosh/blob/master/README.md
