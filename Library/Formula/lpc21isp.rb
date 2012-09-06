411c940f49dea543f0fc88be4e3406b98f4c4c21
Pedro Vanzella <pedro@pedrovanzella.com>
2012-09-05 23:22:53 -0300

    updated lpc21isp to version 1.83

--- Library/Formula/lpc21isp.rb
+++ Library/Formula/lpc21isp.rb
@@ -1,12 +1,15 @@
 require 'formula'
 
 class Lpc21isp < Formula
-  url 'http://sourceforge.net/projects/lpc21isp/files/lpc21isp/1.79/lpc21isp_179.zip'
-  version '1.79'
+  url 'http://sourceforge.net/projects/lpc21isp/files/lpc21isp/1.83/lpc21isp_183.tar.gz'
+  version '1.83'
   homepage 'http://lpc21isp.sourceforge.net/'
-  md5 '67c1e2a4cebccadd6fb8ea39faf8c89e'
+  md5 '4b437a6d6e718afa6d182f0c18f5363f'
 
   def install
+	
+	# Can't statically link on OSX, so we'll remove that from the Makefile
+	inreplace 'Makefile', "CFLAGS	+= -Wall -static", "CFLAGS	+= -Wall" 
     system "make"
     bin.install ["lpc21isp"]
   end
