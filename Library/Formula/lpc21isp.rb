fd21b4af1d8c1f806930c548598746e15170683c
Pedro Vanzella <pedro@pedrovanzella.com>
2012-09-06 10:33:14 -0300

    updated to comply with new house rules

--- Library/Formula/lpc21isp.rb
+++ Library/Formula/lpc21isp.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Lpc21isp < Formula
-  url 'http://sourceforge.net/projects/lpc21isp/files/lpc21isp/1.83/lpc21isp_183.tar.gz'
-  version '1.83'
   homepage 'http://lpc21isp.sourceforge.net/'
-  md5 '4b437a6d6e718afa6d182f0c18f5363f'
+  url 'http://sourceforge.net/projects/lpc21isp/files/lpc21isp/1.83/lpc21isp_183.tar.gz'
+  sha1 'b251105cec7a0a65f271c233c94e0fe3fab42082'
+  version '1.83'
 
   def install
 	
