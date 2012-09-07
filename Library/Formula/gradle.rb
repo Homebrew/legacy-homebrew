9c9752b1b138b7de3f1628787e1f711d77fc9965
Gasol Wu <gasol.wu@gmail.com>
2012-09-08 01:34:50 +0800

    gradle devel 1.2-rc1
    Closes #14791.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -5,6 +5,12 @@ class Gradle < Formula
   url 'http://services.gradle.org/distributions/gradle-1.1-bin.zip'
   sha1 '938b2a4e40fd2fd701c5138edbcbab54b9b7f85e'
 
+  devel do
+    url 'http://services.gradle.org/distributions/gradle-1.2-rc-1-bin.zip'
+    sha1 '3e64ca9c8cbf1f03da4d5c20a42cb46464d0a820'
+    version '1.2-rc1'
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin lib]
