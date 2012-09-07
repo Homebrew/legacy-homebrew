d5760357d408980f8bda2ac55f9231308723cc71
Jack Nagel <jacknagel@gmail.com>
2012-09-06 20:24:08 -0500

    mpfr 3.1.1-p2
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/mpfr.rb
+++ Library/Formula/mpfr.rb
@@ -3,11 +3,16 @@ require 'formula'
 class Mpfr < Formula
   homepage 'http://www.mpfr.org/'
   url 'http://www.mpfr.org/mpfr-3.1.1/mpfr-3.1.1.tar.bz2'
+  version '3.1.1-p2'
   sha256 '7b66c3f13dc8385f08264c805853f3e1a8eedab8071d582f3e661971c9acd5fd'
 
   depends_on 'gmp'
 
   option '32-bit'
+
+  def patches
+    "http://www.mpfr.org/mpfr-current/allpatches"
+  end
 
   def install
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
