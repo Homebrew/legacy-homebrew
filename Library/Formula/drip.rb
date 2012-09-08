ae223c9d6eecdad7ad6b389ed8f1080dd60bfff4
Justin Balthrop <git@justinbalthrop.com>
2012-09-08 10:06:40 -0700

    update drip to 0.1.4. use make to compile C code

--- Library/Formula/drip.rb
+++ Library/Formula/drip.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Drip < Formula
   homepage 'https://github.com/flatland/drip'
-  url 'https://github.com/flatland/drip/tarball/0.1.3'
-  sha1 '12e818bc1eca23a9eb09c033023aedf35c646f41'
+  url 'https://github.com/flatland/drip/tarball/0.1.4'
+  sha1 '564ebb5971d0366fe5e19da074b2bdf3494ac655'
 
   def install
-    system 'bin/drip'
+    system 'make'
     libexec.install %w{ bin src Makefile }
     bin.install_symlink libexec/'bin/drip'
   end
