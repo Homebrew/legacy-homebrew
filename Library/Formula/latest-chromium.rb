9d0915b0a51133d5c0931c6df92ed3bd9358c37d
Teo Ljungberg <teo.ljungberg@gmail.com>
2012-09-08 10:45:01 +0200

    new formula: installs the latest chromium release

---
+++ Library/Formula/latest-chromium.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class LatestChromium < GithubGistFormula
+  homepage 'https://gist.github.com/228538'
+  url 'https://raw.github.com/gist/228538/8a0500b3af4c46200f8c16e0ed5a1b653c2e79cb/nightly-chromium'
+  sha1 '8e1892f8cd22da54ada61ceb8112456fa36155b5'
+end
