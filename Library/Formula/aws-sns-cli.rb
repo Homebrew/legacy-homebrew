3467dbfe7643e46d6caedd8550eae10194ac204f
Adam Vandenberg <flangy@gmail.com>
2012-09-09 09:50:03 -0700

    aws-sns-cli: don't install 'service' to bin

--- Library/Formula/aws-sns-cli.rb
+++ Library/Formula/aws-sns-cli.rb
@@ -8,7 +8,15 @@ class AwsSnsCli < AmazonWebServicesFormula
   sha1 'fcb6f651275b88f6225ed94f51568936ccba9e6d'
 
   def install
-    standard_install
+    rm Dir['bin/*.cmd'] # Remove Windows versions
+
+    # There is a "service" binary, which of course will conflict with any number
+    # other brews that have a generically named tool. So don't just blindly
+    # install bin to the prefix.
+    jars = prefix/'jars'
+    jars.install "bin", "lib"
+    system "chmod +x #{jars}/bin/*"
+    bin.install_symlink Dir["#{jars}/bin/sns-*"]
   end
 
   def caveats
