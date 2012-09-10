9e132182c38fbf35a07bcf489376a63b5776f5cb
Ming-Hsien Tsai <mhtsai208@gmail.com>
2012-09-10 23:43:38 +0800

    Add a formula for Ssreflect.
    Ssreflect is an extension of Coq. It provides convenient notations,
    several mathematical components, and other general purpose features.
    The famous Four Colour Theorem was proved in Coq with the Ssreflect
    extension.

---
+++ Library/Formula/ssreflect.rb
@@ -0,0 +1,32 @@
+require 'formula'
+
+class Ssreflect < Formula
+  homepage 'http://www.msr-inria.inria.fr/Projects/math-components'
+  url 'http://www.msr-inria.inria.fr/Projects/math-components/ssreflect-1.4-coq8.4.tar.gz'
+  version '1.4'
+  sha1 'c9e678a362973b202a5d90d2abf6436fa1ab4dcf'
+
+  depends_on 'objective-caml'
+  depends_on 'coq'
+
+  def install
+    ENV.j1
+
+    # Enable static linking.
+    inreplace 'Make' do |s|
+      s.gsub! /#\-custom/, '-custom'
+      s.gsub! /#SSRCOQ/, 'SSRCOQ'
+    end
+
+    args = ["COQBIN=#{HOMEBREW_PREFIX}/bin/",
+            "COQLIBINSTALL=lib/coq/user-contrib",
+            "COQDOCINSTALL=doc/coq",
+            "DSTROOT=#{prefix}/"]
+    system "make", *args
+    system "make", "install", *args
+    bin.install 'bin/ssrcoq.byte', 'bin/ssrcoq'
+    mkdir_p "#{share}/ssreflect"
+    cp "pg-ssr.el", "#{share}/ssreflect"
+  end
+
+end
