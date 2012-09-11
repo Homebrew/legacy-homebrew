854ba620621ed94cff69f848399fc839356a7478
Daniel Serodio <dserodio@ig.com>
2012-09-11 10:49:58 -0300

    Groovy 2.0.2
    Closes #14866.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Groovy < Formula
   homepage 'http://groovy.codehaus.org/'
-  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.1.zip'
-  sha1 '8368651e4c96e2ab272d2a0623861c817d947316'
+  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.2.zip'
+  sha1 '024104492f110b4395d6efd40207f8726468c6b4'
 
   def install
     # Don't need Windows files.
