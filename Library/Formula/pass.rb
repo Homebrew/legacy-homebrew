7e6325d839a5589dc2440bbd667a7358f4a7c0cf
Theo Belaire <theo.belaire+github@gmail.com>
2012-09-11 16:20:12 -0400

    Bumped version to 1.2.1
    The OSX version of mktemp was not accepting certain GNU arguments.
    This is now fixed.
    
    Closes #14864.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/pass.rb
+++ Library/Formula/pass.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pass < Formula
   homepage 'http://zx2c4.com/projects/password-store'
-  url 'http://git.zx2c4.com/password-store/snapshot/password-store-1.2.tar.xz'
-  sha256 '09b5ca5250be8137d4153b4c38659cedd33e2eeec832751a93add2c3ec434903'
+  url 'http://git.zx2c4.com/password-store/snapshot/password-store-1.2.1.tar.xz'
+  sha256 'e75c49db92288cb41913905f9cdfbae6cfb8db09f9d5537242ca436c811042fa'
   head 'http://git.zx2c4.com/password-store', :using => :git
 
   depends_on 'xz' => :build
