5d10557388e82e7e04d90a3743547bc453e304a3
Ming-Hsien Tsai <mhtsai208@gmail.com>
2012-09-11 11:32:08 +0800

    Add an option to install HTML documents.

--- Library/Formula/ssreflect.rb
+++ Library/Formula/ssreflect.rb
@@ -9,6 +9,13 @@ class Ssreflect < Formula
   depends_on 'objective-caml'
   depends_on 'coq'
 
+  option 'with-doc', 'Install HTML documents'
+
+  def patches
+    # Fix an ill-formatted ocamldoc comment.
+    DATA
+  end
+
   def install
     ENV.j1
 
@@ -20,12 +27,34 @@ class Ssreflect < Formula
 
     args = ["COQBIN=#{HOMEBREW_PREFIX}/bin/",
             "COQLIBINSTALL=lib/coq/user-contrib",
-            "COQDOCINSTALL=doc/coq",
+            "COQDOCINSTALL=share/doc",
             "DSTROOT=#{prefix}/"]
     system "make", *args
     system "make", "install", *args
+    if build.include? 'with-doc'
+      system "make", "-f", "Makefile.coq", "html", *args
+      system "make", "-f", "Makefile.coq", "mlihtml", *args
+      system "make", "-f", "Makefile.coq", "install-doc", *args
+    end
     bin.install 'bin/ssrcoq.byte', 'bin/ssrcoq'
     (share/'ssreflect').install "pg-ssr.el"
   end
 
 end
+
+
+__END__
+diff --git a/src/ssrmatching.mli b/src/ssrmatching.mli
+index fd2e835..1d9d15b 100644
+--- a/src/ssrmatching.mli
++++ b/src/ssrmatching.mli
+@@ -77,7 +77,7 @@ val interp_cpattern :
+     pattern
+ 
+ (** The set of occurrences to be matched. The boolean is set to true
+- *  to signal the complement of this set (i.e. {-1 3}) *)
++ *  to signal the complement of this set (i.e. \{-1 3\}) *)
+ type occ = (bool * int list) option
+ 
+ (** Substitution function. The [int] argument is the number of binders
+
