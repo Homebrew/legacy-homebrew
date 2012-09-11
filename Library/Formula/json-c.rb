7955181227a8530bbc9d870e75761ad968306700
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2012-09-11 11:52:14 -0500

    json-c: update homepage
    Closes #14858.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/json-c.rb
+++ Library/Formula/json-c.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class JsonC < Formula
+  homepage 'https://github.com/json-c/json-c/wiki'
   url 'http://oss.metaparadigm.com/json-c/json-c-0.9.tar.gz'
-  homepage 'http://oss.metaparadigm.com/json-c/'
   sha1 'daaf5eb960fa98e137abc5012f569b83c79be90f'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
