0109dde82da333560ad94ff780babfe502c9e6ea
Andrey Popp <8mayday@gmail.com>
2012-09-12 10:37:45 +0400

    opam: A package manager for OCaml

---
+++ Library/Formula/opam.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Opam < Formula
+  homepage 'https://github.com/OCamlPro/opam'
+  url 'https://github.com/OCamlPro/opam/tarball/0.6.0'
+  sha1 '71aa753f37a60debe45a623bc2266a1262e9c305'
+
+  depends_on "objective-caml"
+  depends_on "wget"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+  def test
+    system "opam --help"
+  end
+end
