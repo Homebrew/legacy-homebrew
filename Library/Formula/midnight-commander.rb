587b03e07387af9a766296f7c5f664174d56eb6b
restorer <restorer@zame-dev.org>
2012-09-12 15:39:20 +0300

    Fix encoding issues (double question marks instead of UTF-8 characters) plus update to version 4.8.5 Confirmed to work on OSX 10.6 (with XCode 4.2 for Show Leopard) and OSX 10.7

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -2,15 +2,20 @@ require 'formula'
 
 class MidnightCommander < Formula
   homepage 'http://www.midnight-commander.org/'
-  url 'http://www.midnight-commander.org/downloads/mc-4.8.4.tar.bz2',
+  url 'https://www.midnight-commander.org/downloads/mc-4.8.5.tar.bz2',
       :using => CurlUnsafeDownloadStrategy
-  sha256 '917f32a6ce0b8e3ec893bb18af73e64c246d79066322041608352623a579234b'
+  sha256 '92c58ad388360fe39f4affd66596a3f9c21dff19f001c5fb05f0349bb0049d4b'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 's-lang'
 
   def install
+    # Fix encoding issues (double question marks instead of UTF-8 characters)
+    # Confirmed to work on OSX 10.6 (with XCode 4.2 for Show Leopard) and OSX 10.7
+    ENV.remove "LDFLAGS", "-L/usr/local/opt/gettext/lib"
+    ENV.remove "CPPFLAGS", "-I/usr/local/opt/gettext/include"
+
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--without-x",
