d84ab0a91ae24804fc4c11b2453f126d9750f050
Jack Nagel <jacknagel@gmail.com>
2012-09-11 21:49:52 -0500

    pango: needs newer fontconfig on Leopard
    Fixes #14850.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -10,8 +10,13 @@ class Pango < Formula
   depends_on 'pkg-config' => :build
   depends_on 'xz' => :build
   depends_on 'glib'
-  depends_on :fontconfig
   depends_on :x11 unless build.include? 'without-x'
+
+  if MacOS.version == :leopard
+    depends_on 'fontconfig'
+  else
+    depends_on :fontconfig
+  end
 
   # The Cairo library shipped with Lion contains a flaw that causes Graphviz
   # to segfault. See the following ticket for information:
