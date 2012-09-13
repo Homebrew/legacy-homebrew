c1e8049ba759b2e1ad05d8f11c0b899b7979b18f
Jack Nagel <jacknagel@gmail.com>
2012-09-13 12:55:42 -0500

    synfigstudio: remove skip_clean :all
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/synfigstudio.rb
+++ Library/Formula/synfigstudio.rb
@@ -5,8 +5,6 @@ class Synfigstudio < Formula
   url 'http://downloads.sourceforge.net/project/synfig/synfigstudio/0.63.05/synfigstudio-0.63.05.tar.gz'
   sha1 '3e8f590b2b1b3cfdef7ecc898fcef3c9254f21e2'
 
-  skip_clean :all # So modules will load
-
   depends_on 'gettext'
   depends_on 'libsigc++'
   depends_on 'gtkmm'
