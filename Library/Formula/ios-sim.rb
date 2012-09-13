725ea8996abdccc5f2fbd2597585b8558983991c
Shazron Abdullah <shazron@apache.org>
2012-09-12 17:28:17 -0700

    ios-sim 1.5.1
    Closes #14898.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ios-sim.rb
+++ Library/Formula/ios-sim.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class IosSim < Formula
   homepage 'https://github.com/phonegap/ios-sim'
-  url 'https://github.com/phonegap/ios-sim/tarball/1.5'
-  sha1 '3f9095e091602cf0c9a447d35a2558622a87d2c1'
+  url 'https://github.com/phonegap/ios-sim/tarball/1.5.1'
+  sha1 'ccacb1cec833a766c0960b7f400330a5d0664cbd'
 
   def install
     system "rake", "install", "prefix=#{prefix}"
