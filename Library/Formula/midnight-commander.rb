fcfdebd80c976aae217bdd0444119d8c3689e7e9
restorer <restorer@zame-dev.org>
2012-09-13 10:38:07 +0300

    Enable internal gettext

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -13,10 +13,12 @@ class MidnightCommander < Formula
   def install
     # Fix encoding issues (double question marks instead of UTF-8 characters)
     # Confirmed to work on OSX 10.6 (with XCode 4.2 for Show Leopard) and OSX 10.7
+    # 1. Remove references to external gettext
     ENV.remove "LDFLAGS", "-L/usr/local/opt/gettext/lib"
     ENV.remove "CPPFLAGS", "-I/usr/local/opt/gettext/include"
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--with-included-gettext", # 2. Enable internal gettext
                           "--prefix=#{prefix}",
                           "--without-x",
                           "--with-screen=slang"
