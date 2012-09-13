249d839f93e85b2f823db29d6e911dd2732799dc
Jack Nagel <jacknagel@gmail.com>
2012-09-13 12:55:42 -0500

    polyml: remove skip_clean :all
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/polyml.rb
+++ Library/Formula/polyml.rb
@@ -5,9 +5,6 @@ class Polyml < Formula
   homepage 'http://www.polyml.org'
   sha1 'a037cd8cf4ce4a43b685b0e0d80f3f1e20d7c9fa'
 
-  # Or dynamic linking breaks
-  skip_clean :all
-
   def install
     # for whatever reason, the configure script fails to find c++ if CXX is defined.
     # this overrides configure so that it won't check for c++ and will assume it exists.
