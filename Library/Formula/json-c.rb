ec83ce56e108735b818cd42fd878e4182f036122
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2012-09-13 20:19:39 -0500

    json-c: version 0.10
    json-c has a new version.

--- Library/Formula/json-c.rb
+++ Library/Formula/json-c.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class JsonC < Formula
   homepage 'https://github.com/json-c/json-c/wiki'
-  url 'http://oss.metaparadigm.com/json-c/json-c-0.9.tar.gz'
-  sha1 'daaf5eb960fa98e137abc5012f569b83c79be90f'
+  url 'https://github.com/json-c/json-c/tarball/json-c-0.10'
+  sha1 '309781f680a298f886c1e11afd5783ff81f5dbde'
 
   def install
     system "./configure", "--disable-dependency-tracking",
