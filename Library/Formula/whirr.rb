61ab56712c24dba35c3fcccb42430ca542f95c85
Adam Vandenberg <flangy@gmail.com>
2012-09-14 07:30:05 -0700

    whirr 0.8.0

--- Library/Formula/whirr.rb
+++ Library/Formula/whirr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Whirr < Formula
   homepage 'http://whirr.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=whirr/whirr-0.7.1/whirr-0.7.1.tar.gz'
-  sha1 '15772fd7bf35cbc1c50023f4a22bcbb1cd1f80c9'
+  url 'http://www.apache.org/dyn/closer.cgi?path=whirr/whirr-0.8.0/whirr-0.8.0.tar.gz'
+  sha1 '44056fee1fd0e83a7257772d1a20faeae8f5cd1d'
 
   def shim_script target
     <<-EOS.undent
