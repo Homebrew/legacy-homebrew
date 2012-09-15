65c03e6a162e7a35e984da54f9b1d1ed8d08fcc0
Sean Goller <sean@goller.net>
2012-09-15 16:19:56 -0700

    Update libmusicbrainz formula to latest version.

---
+++ Library/Formula/libmusicbrainz.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Libmusicbrainz < Formula
+  homepage 'http://musicbrainz.org/doc/libmusicbrainz'
+  url 'https://github.com/downloads/metabrainz/libmusicbrainz/libmusicbrainz-5.0.1.tar.gz'
+  sha1 'd4823beeca3faf114756370dc7dd6e3cd01d7e4f'
+
+  depends_on 'cmake' => :build
+  depends_on 'neon'
+
+  def install
+    neon = Formula.factory("neon")
+    neon_args = %W[-DNEON_LIBRARIES:FILEPATH=#{neon.lib}/libneon.dylib
+                 -DNEON_INCLUDE_DIR:PATH=#{neon.include}/neon]
+    system "cmake", ".", *(std_cmake_args + neon_args)
+    system "make install"
+  end
+
+  def test
+    system "pkg-config", "--libs", "libmusicbrainz5"
+  end
+
+end
