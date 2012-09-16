79a8d0a0eeeae64daa59366b260aecb5768cebf8
Tobias Steinhoff <tobias@stonehope.de>
2012-09-16 08:30:36 +0200

    ttytter 2.0.3
    Closes #14961.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ttytter.rb
+++ Library/Formula/ttytter.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Ttytter < ScriptFileFormula
   homepage 'http://www.floodgap.com/software/ttytter/'
-  url 'http://www.floodgap.com/software/ttytter/dist2/2.0.02.txt'
-  sha1 '2fef9f67979584e1175eada166e3680b6c806492'
+  url 'http://www.floodgap.com/software/ttytter/dist2/2.0.03.txt'
+  sha1 'ddffb936b08cf3fa67fc121fb258a6f4e088fd2f'
 
   def install
-    bin.install '2.0.02.txt' => 'ttytter'
+    bin.install '2.0.03.txt' => 'ttytter'
   end
 
   def caveats; <<-EOS.undent
