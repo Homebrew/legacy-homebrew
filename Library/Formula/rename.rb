121593645df3da9c003852f2d7263b6cc446abd7
Brett Nakashima <brettnak@gmail.com>
2012-09-20 15:47:12 -0700

    Update version in Library/Formula/rename.rb

--- Library/Formula/rename.rb
+++ Library/Formula/rename.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Rename < Formula
   url 'http://plasmasturm.org/code/rename/rename', :using => :nounzip
-  version '0.1.3'
+  version '1.100'
   homepage 'http://plasmasturm.org/code/rename'
   sha1 '2077cdb11878ffeaefa32063e29af87d8ad7a596'
 
