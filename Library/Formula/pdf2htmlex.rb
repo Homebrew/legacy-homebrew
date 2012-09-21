4c15d192450bc7a906a3e4310003e18b0e57fca0
Jamie Ly <jamie@bricksimple.com>
2012-09-21 06:03:25 -0400

    Used the downloads URL generated by the tag and updated the sha appropriately. Also moved head below sha.

--- Library/Formula/pdf2htmlex.rb
+++ Library/Formula/pdf2htmlex.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Pdf2htmlex < Formula
   homepage 'http://coolwanglu.github.com/pdf2htmlEX/'
-  url 'https://github.com/coolwanglu/pdf2htmlEX.git', :tag => 'v0.3.1'
+  url 'https://github.com/coolwanglu/pdf2htmlEX/tarball/v0.3.1'
+  sha1 '1ad2d9d1db10ce10721ffb38cbc692da32662bfe'
+
   head 'https://github.com/coolwanglu/pdf2htmlEX.git'
-  version 'v0.3.1'
-  sha1 '325901f101b66e8b4959e3619dfe42a0068c0b09'
 
   depends_on 'cmake' => :build
   depends_on 'fontforge'
