9beab9e836cc988d318e23f77bda387acd912cd7
Tomas Lin <tomaslin@gmail.com>
2012-09-22 21:13:27 +0200

    Update Groovy to 2.0.4

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Groovy < Formula
   homepage 'http://groovy.codehaus.org/'
-  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.2.zip'
-  sha1 '024104492f110b4395d6efd40207f8726468c6b4'
+  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.0.4.zip'
+  sha1 '5e6fc699a91c508a33cbedf61b170ed64469fe79'
 
   def install
     # Don't need Windows files.
