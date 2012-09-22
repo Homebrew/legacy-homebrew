2e2d8e252d52c37b47a9b57afebdb6dab0212ce0
Max Howell <mxcl@me.com>
2012-09-21 23:50:03 -0400

    liblastfm-0.3.3.1
    A fake release, but I maintain this and will tag the next proper release 0.3.4.
    
    Remove fails with clang.
    
    Closes #15008.

--- Library/Formula/liblastfm.rb
+++ Library/Formula/liblastfm.rb
@@ -2,18 +2,13 @@ require 'formula'
 
 class Liblastfm < Formula
   homepage 'https://github.com/mxcl/liblastfm/'
-  url 'https://github.com/mxcl/liblastfm/tarball/0.3.3'
-  sha1 'f2e9705c9c2cbeaa14f46da9bd35ab36fe710392'
+  url 'https://github.com/mxcl/liblastfm/tarball/e380c7f03f4b2417db87372df733606f4a153c53'
+  version '0.3.3.1'
+  sha1 'c68691b63a383f2a53a5894b99f165ffed4ea0db'
 
   depends_on 'qt'
   depends_on 'fftw'
   depends_on 'libsamplerate'
-
-  # See issue #12886.  Reported in: https://github.com/mxcl/liblastfm/issues/14
-  fails_with :clang do
-    build 318
-    cause 'error: reference to non-static member function must be called'
-  end
 
   def install
     system "./configure", "--release", "--prefix", prefix
