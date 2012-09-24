5fbb8f5e04a4de05ccf6e05f65e30a1a06aeb174
Max Howell <mxcl@me.com>
2012-09-24 09:46:42 -0400

    Work even when RUBYLIB='-'
    Apparently setting RUBYLIB to '-' causes the library path to be unset. So we need to set our own library path in our scripts.
    
    Fixes Homebrew/homebrew-versions/#47.

--- Library/ENV/libsuperenv.rb
+++ Library/ENV/libsuperenv.rb
@@ -1,6 +1,9 @@
 # Yes, a good deal of this could be imported from Homebrew-proper
 # But Homebrew-proper is dog-slow currently, and I didn't want every cc
-# instantiation to be slower be a tangible amount.
+# instantiation to be slower by a tangible amount.
+
+# https://github.com/Homebrew/homebrew-versions/issues/47
+$:.unshift "/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8"
 
 class String
   def directory?; File.directory? self end
