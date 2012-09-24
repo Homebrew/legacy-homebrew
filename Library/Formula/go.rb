10d5fddd4d543352bbee499d0210f20e15db9573
Timothée Peignier <timothee.peignier@tryphon.org>
2012-09-24 21:15:07 +0200

    go 1.0.3
    Closes #15086.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/go.rb
+++ Library/Formula/go.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Go < Formula
   homepage 'http://golang.org'
-  url 'http://go.googlecode.com/files/go1.0.2.src.tar.gz'
-  version '1.0.2'
-  sha1 '408bb361df8c34b1bba41383812154e932907526'
+  url 'http://go.googlecode.com/files/go1.0.3.src.tar.gz'
+  version '1.0.3'
+  sha1 '1a67293c10d6c06c633c078a7ca67e98c8b58471'
 
   head 'http://go.googlecode.com/hg/'
 
