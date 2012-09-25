4a8e8f3fd23e8fdbe3eaf505a7c3898843d29ba6
Carlos Yakimov <logicode.ve@gmail.com>
2012-09-25 10:20:04 -0530

    Incorrect sha1

--- Library/Formula/auto-scaling.rb
+++ Library/Formula/auto-scaling.rb
@@ -4,7 +4,7 @@ class AutoScaling < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/2535'
   url 'http://ec2-downloads.s3.amazonaws.com/AutoScaling-2011-01-01.zip'
   version  '1.0.61.1'
-  sha1 '70fe87756f37dcfc412b6013d71e687dee6cb1c5'
+  sha1 '70138664fc4a76c965660b86e7be959a44281c40'
 
   def install
     standard_install
