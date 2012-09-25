c9a301d2b952c4dc822ad42c8676197249f8f20c
Frank Denis <github@pureftpd.org>
2012-09-25 15:58:47 +0200

    Fix dnscrypt-proxy formula after @adamv review

--- Library/Formula/dnscrypt-proxy.rb
+++ Library/Formula/dnscrypt-proxy.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class DnscryptProxy < Formula
+  homepage 'http://dnscrypt.org'
   url 'https://github.com/downloads/opendns/dnscrypt-proxy/dnscrypt-proxy-1.1.0.tar.gz'
   head 'https://github.com/opendns/dnscrypt-proxy.git', :branch => 'master'
-  homepage 'http://dnscrypt.org'
   sha256 '73c1042f6ba68dedd89ab518c319f5e46b3536a3c49e697ef9ba504601b26c71'
 
   if build.head?
@@ -14,16 +14,15 @@ class DnscryptProxy < Formula
   option "plugins", "Support plugins and install example plugins."
 
   def install
-    if build.head?
-      system "autoreconf", "-if"
-    end
+    system "autoreconf", "-if" if build.head?
 
+    configure_args = [ "--prefix=#{prefix}", "--disable-dependency-tracking" ]
     if build.include? "plugins"
-      system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking",
-      "--enable-plugins", "--enable-relaxed-plugins-permissions", "--enable-plugins-root"
-    else
-      system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+      configure_args << "--enable-plugins"
+      configure_args << "--enable-relaxed-plugins-permissions"
+      configure_args << "--enable-plugins-root"
     end
+    system "./configure", *configure_args
     system "make install"
   end
 end
