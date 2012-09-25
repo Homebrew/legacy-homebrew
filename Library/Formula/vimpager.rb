5ea8017340511865158c33a1365d260058b665ea
Simon Olofsson <simon@olofsson.de>
2012-09-25 23:22:08 +0200

    vimpager 1.7.4

--- Library/Formula/vimpager.rb
+++ Library/Formula/vimpager.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vimpager < Formula
   homepage 'https://github.com/rkitover/vimpager'
-  url 'https://github.com/rkitover/vimpager/tarball/1.7.1'
-  sha1 '4e44e3af13e4ca927a2d9fcda15b3ecb958dbd06'
+  url 'https://github.com/rkitover/vimpager/tarball/1.7.4'
+  sha1 '7a94beba5ed08b5156d98deba2523f0b7d51bb35'
   head 'https://github.com/rkitover/vimpager.git'
 
   def install
