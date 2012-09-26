b350c24ad10b714ae453073fb5d1445682e68bd9
Yan Zhou <zhouyan@me.com>
2012-09-26 17:35:25 +0200

    Armadillo 3.4.2
    Closes #15135.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/armadillo.rb
+++ Library/Formula/armadillo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Armadillo < Formula
   homepage 'http://arma.sourceforge.net/'
-  url 'http://sourceforge.net/projects/arma/files/armadillo-3.4.1.tar.gz'
-  sha1 '16f11ee6e7a545581ab239e7f826e14d7d1c2e3a'
+  url 'http://sourceforge.net/projects/arma/files/armadillo-3.4.2.tar.gz'
+  sha1 'de3d2a6005d2433f0630f4ef0885a0a8712968dd'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
