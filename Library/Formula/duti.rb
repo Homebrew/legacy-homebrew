164148c28e90e6131a5b5b422fbf4401f0e2f790
Andrew Mortensen <admorten@umich.edu>
2012-09-26 11:53:37 -0400

    Use 1.5.1 release imported from SF.net instead of 1.5.1 tag tarball.
    Eliminate autoconf dependency.

--- Library/Formula/duti.rb
+++ Library/Formula/duti.rb
@@ -2,14 +2,12 @@ require 'formula'
 
 class Duti < Formula
   homepage 'http://duti.org/'
-  url 'https://github.com/fitterhappier/duti/tarball/duti-1.5.1'
-  sha1 '30f1f3274030fe511668c42b5d63115e9ad21751'
+  url 'https://github.com/downloads/fitterhappier/duti/duti-1.5.1.tar.gz'
+  sha1 'ac199f936180a3ac62100ae9a31e107a45330557'
   head 'https://github.com/fitterhappier/duti.git'
 
-  depends_on :autoconf
 
   def install
-    system "autoconf"
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
