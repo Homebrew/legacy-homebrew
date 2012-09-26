e8eed1c0e115f61f5ee97709566ff94627f05442
Adam Vandenberg <flangy@gmail.com>
2012-09-25 20:20:04 -0700

    vice: update deps

--- Library/Formula/vice.rb
+++ Library/Formula/vice.rb
@@ -5,8 +5,11 @@ class Vice < Formula
   url "http://www.zimmers.net/anonftp/pub/cbm/crossplatform/emulators/VICE/vice-2.3.tar.gz"
   sha1 '5e7e1a375a4ca8c4895dc1552162955fdffce296'
 
+  depends_on 'pkg-config' => :build
   depends_on 'jpeg'
   depends_on :libpng
+  depends_on 'giflib' => :optional
+  depends_on 'lame' => :optional
 
   def remove_unused_icons
     Pathname.glob libexec+'*.app' do |d|
@@ -24,10 +27,13 @@ class Vice < Formula
 
   def install
     # Use Cocoa instead of X
+    # Use a static lame, otherwise Vice is hard-coded to look in
+    # /opt for the library.
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-cocoa",
-                          "--without-x"
+                          "--without-x",
+                          "--enable-static-lame"
     system "make"
     system "make bindist"
     prefix.install Dir['vice-macosx-*/*']
