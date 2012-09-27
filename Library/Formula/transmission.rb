4983160f64fd902b9a2d227efb85e2d808cb2a74
royhodgman <royhodgman@yahoo.com>
2012-09-27 07:53:39 -0400

    update transmission to 2.71
    This updates transmission to version 2.71.
    
    [Release notes can be found here](https://trac.transmissionbt.com/wiki/Changes#version-2.71 "Transmission 2.71 release notes").

--- Library/Formula/transmission.rb
+++ Library/Formula/transmission.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Transmission < Formula
   homepage 'http://www.transmissionbt.com/'
-  url 'http://download.transmissionbt.com/files/transmission-2.61.tar.bz2'
-  sha1 '7df170ecee6e62766859dca6ae0cf4e89c1ea99f'
+  url 'http://download.transmissionbt.com/files/transmission-2.71.tar.bz2'
+  sha1 '78dbd49fff06e6562e3e8861da8af54069285fc9'
 
   depends_on 'pkg-config' => :build # So it will find system libcurl
   depends_on 'libevent'
