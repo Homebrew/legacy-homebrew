1ae4086a7659b90af38b799cb71aec6b760a23ef
Yoji SHIDARA <dara@shidara.net>
2012-09-29 18:03:41 +0900

    groonga-2.0.7

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Groonga < Formula
   homepage 'http://groonga.org/'
-  url 'http://packages.groonga.org/source/groonga/groonga-2.0.6.tar.gz'
-  sha1 'a012a86d03e193f2ab029de2f1e09aa5f0e52bcf'
+  url 'http://packages.groonga.org/source/groonga/groonga-2.0.7.tar.gz'
+  sha1 '577f609c785803f90b65e2defa23247a5a256623'
 
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
