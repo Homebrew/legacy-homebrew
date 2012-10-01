29e3109f095a19a1b4c29a706b328aa369a7856d
Jake McGinty <mcginty@illinois.edu>
2012-10-01 18:08:52 -0500

    Missing dependency in Formula/ettercap.rb
    Failed to compile on OS X 10.8 Mountain Lion without cairo being installed first, making it a dependency.

--- Library/Formula/ettercap.rb
+++ Library/Formula/ettercap.rb
@@ -8,6 +8,7 @@ class Ettercap < Formula
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
   depends_on 'libnet'
+  depends_on 'cairo'
 
   # Stripping breaks plugin support
   skip_clean 'bin'
