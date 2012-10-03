f2345bd43e907ee521f7a28b3bccf9338e04eac9
Geoff Greer <geoff@greer.fm>
2012-10-02 18:23:11 -0700

    The Silver Searcher 0.12
    Closes #15263.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/the_silver_searcher.rb
+++ Library/Formula/the_silver_searcher.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class TheSilverSearcher < Formula
   homepage 'https://github.com/ggreer/the_silver_searcher'
-  url 'https://github.com/ggreer/the_silver_searcher/tarball/0.11.1'
-  sha1 '73e46e45ad911ef697f53b51764d5306e61b0538'
+  url 'https://github.com/ggreer/the_silver_searcher/tarball/0.12'
+  sha1 '8b19f76fd81d7c2db22567127cf2cf8d6302d43a'
 
   head 'https://github.com/ggreer/the_silver_searcher.git'
 
