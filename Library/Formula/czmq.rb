e6728df5966e97574fd17d4d23aa7039c409ce22
Jay Han <jayhan@google.com>
2012-10-08 22:08:47 -0700

    cqmz: Add universal option
    so that both 32-bit and 64-bit libraries get installed.
    
    Closes #15366.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/czmq.rb
+++ Library/Formula/czmq.rb
@@ -5,9 +5,12 @@ class Czmq < Formula
   url 'http://download.zeromq.org/czmq-1.2.0.tar.gz'
   sha1 '826830df72443edb4646283cd5b490683af6df2a'
 
+  option :universal
+
   depends_on 'zeromq'
 
   def install
+    ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
