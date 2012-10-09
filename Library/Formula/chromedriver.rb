7935640759decd6de4a3e3be5388e5fc0321213e
Thong Kuah <kuahyeow@gmail.com>
2012-10-09 14:36:25 +1300

    chromedriver 23.0.1240.0

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Chromedriver < Formula
   homepage 'http://code.google.com/p/chromedriver/'
-  url 'http://chromedriver.googlecode.com/files/chromedriver_mac_21.0.1180.4.zip'
-  sha1 'ea6f2f45c835d3413fde3a7b08e5e3e4db6dc3f9'
+  url 'https://chromedriver.googlecode.com/files/chromedriver_mac_23.0.1240.0.zip'
+  sha1 '20a21cca0a4f0a1e25ee88e1f0bd53acbdba4cc6'
 
   def install
     bin.install 'chromedriver'
