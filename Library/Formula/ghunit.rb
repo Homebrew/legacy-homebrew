79f814f8e78a46107243cd73936b78fb976dabd1
GWStuartClift <Stuart.Clift@GWPLC.com>
2012-10-10 09:27:34 +0100

    Added Xcode dependency, removed redundant test and installs to prefix/Frameworks

--- Library/Formula/ghunit.rb
+++ Library/Formula/ghunit.rb
@@ -1,6 +1,9 @@
 require 'formula'
 
 class Ghunit < Formula
+
+  depends_on :xcode
+
   homepage 'https://github.com/gabriel/gh-unit'
   url 'https://github.com/gabriel/gh-unit/tarball/0.5.5'
   sha1 '19da294c5c6c8b8d9b425032219387248b87bbca'
@@ -8,11 +11,10 @@ class Ghunit < Formula
   def install
     system "make -C Project-iOS"
     system "make -C Project-MacOSX"
-    include.install "Project-iOS/build/Framework/GHUnitIOS.framework", "Project-MacOSX/build/Release/GHUnit.framework"
+    frameworks = prefix+"Frameworks"
+    frameworks.mkdir
+    frameworks.install "Project-iOS/build/Framework/GHUnitIOS.framework"
+    frameworks.install "Project-MacOSX/build/Release/GHUnit.framework"
   end
 
-  def test
-    system "test -e #{include}/GHUnitIOS.framework"
-    system "test -e #{include}/include/GHUnit.framework"
-  end
 end
