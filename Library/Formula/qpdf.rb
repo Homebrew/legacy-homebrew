549e38c60034a84f29fa084ae1986a2de9beacd6
Ankit Solanki <ankit.solanki@gmail.com>
2012-10-11 16:04:41 +0530

    qpdf: Update to 3.0.2
    The patches specified in the formula no longer seem necessary, it
    compiles fine without them.
    
    Closes #15405.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/qpdf.rb
+++ Library/Formula/qpdf.rb
@@ -1,18 +1,11 @@
 require 'formula'
 
 class Qpdf < Formula
-  url 'http://downloads.sourceforge.net/project/qpdf/qpdf/2.3.1/qpdf-2.3.1.tar.gz'
+  url 'http://downloads.sourceforge.net/project/qpdf/qpdf/3.0.2/qpdf-3.0.2.tar.gz'
   homepage 'http://qpdf.sourceforge.net/'
-  sha1 '043cceeb74a702d4e6a6a83b0211d99f28383655'
+  sha1 '0cab59b27c9adf4067ffc002db1d9262e219c364'
 
   depends_on 'pcre'
-
-  def patches
-    # Fix call to depreciated PCRE function. Can probably be removed on next
-    # release. Upstream issue:
-    #   http://sourceforge.net/tracker/?func=detail&aid=3489349&group_id=224196&atid=1060899
-    'http://sourceforge.net/tracker/download.php?group_id=224196&atid=1060899&file_id=436172&aid=3489349'
-  end
 
   def install
     # find Homebrew's libpcre
