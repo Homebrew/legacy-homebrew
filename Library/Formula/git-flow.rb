c40cff570b905b5e4a6b283edc703f7b84bd0001
Jack Nagel <jacknagel@gmail.com>
2012-10-12 15:12:48 -0500

    git-flow: fix missing checksum
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/git-flow.rb
+++ Library/Formula/git-flow.rb
@@ -3,7 +3,7 @@ require 'formula'
 class GitFlowCompletion < Formula
   homepage 'https://github.com/bobthecow/git-flow-completion'
   url 'https://github.com/bobthecow/git-flow-completion/tarball/0.4.1.0'
-  sha1 ''
+  sha1 'c3d09e9d9e6a268d0587e31d30d6a20ca8c36800'
 
   head 'https://github.com/bobthecow/git-flow-completion.git', :branch => 'develop'
 end
