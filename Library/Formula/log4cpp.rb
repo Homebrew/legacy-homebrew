d383ab561a4a525f959ff1dbc1a6ba5b63dc41d0
Bradley Bell <bradleyb@uw.edu>
2012-10-13 03:03:51 -0700

    update download path

--- Library/Formula/log4cpp.rb
+++ Library/Formula/log4cpp.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Log4cpp < Formula
-  url 'http://downloads.sourceforge.net/project/log4cpp/log4cpp-1.0.x%20%28current%29/log4cpp-1.0/log4cpp-1.0.tar.gz'
+  url 'http://downloads.sourceforge.net/project/log4cpp/log4cpp-1.0.x%20%28old%29/log4cpp-1.0/log4cpp-1.0.tar.gz'
   homepage 'http://log4cpp.sourceforge.net/'
   sha1 'bdb2fa7a8c8af738d79156bf9f6191875cdba5cc'
 
