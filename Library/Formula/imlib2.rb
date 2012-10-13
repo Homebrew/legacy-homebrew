59a2f11372c302621497384f3146a4fa35c1fa0d
Mike McQuaid <mike@mikemcquaid.com>
2012-10-13 16:11:53 +0100

    imlib2: default to X11 support on.
    References #15325, #15390.

--- Library/Formula/imlib2.rb
+++ Library/Formula/imlib2.rb
@@ -5,11 +5,11 @@ class Imlib2 < Formula
   url 'http://downloads.sourceforge.net/project/enlightenment/imlib2-src/1.4.5/imlib2-1.4.5.tar.bz2'
   sha1 'af86a2c38f4bc3806db57e64e74dc9814ad474a0'
 
-  option "with-x", "Build with X support"
+  option "without-x", "Build without X support"
 
   depends_on :freetype
   depends_on :libpng => :recommended
-  depends_on :x11 if MacOS::X11.installed? or build.include? "with-x"
+  depends_on :x11 if MacOS::X11.installed? or not build.include? "without-x"
   depends_on 'pkg-config' => :build
   depends_on 'jpeg' => :recommended
 
@@ -19,7 +19,7 @@ class Imlib2 < Formula
       --prefix=#{prefix}
       --enable-amd64=no
     ]
-    args << "--without-x" unless build.include? "with-x"
+    args << "--without-x" if build.include? "without-x"
 
     system "./configure", *args
     system "make install"
