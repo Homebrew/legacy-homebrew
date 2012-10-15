e959ee1fc0b47a435405fabf4c22429a8c6cb19f
nibbles 2bits <nibbles@speakeasy.net>
2012-10-15 11:53:34 -0700

    pgtap 0.90.0
    - Upgrade to 0.90.0
    - Change the skip clean to 'share' to stop a warning.
    - The `skip_clean` is needed. It installs nothing to its prefix.
    - Change the `satisfied?` PGS test to whether HB PGS is installed.
    - Prepend HB PSG bin to PATH so it finds the right pg-config

--- Library/Formula/pgtap.rb
+++ Library/Formula/pgtap.rb
@@ -12,7 +12,7 @@ class PostgresqlInstalled < Requirement
     EOS
   end
   def satisfied?
-    which 'pg_config'
+    Formula.factory('postgresql').installed?
   end
   def fatal?
     true
@@ -20,16 +20,16 @@ class PostgresqlInstalled < Requirement
 end
 
 class Pgtap < Formula
-  url 'http://pgfoundry.org/frs/download.php/2701/pgtap-0.24.tar.bz2'
   homepage 'http://pgtap.org'
-  sha1 '8a23fe62e1e476731076a588cb628fe9f1a028b1'
+  url 'http://pgfoundry.org/frs/download.php/3183/pgtap-0.90.0.tar.bz2'
+  sha1 '707a483cfe5c954732b548cb15abc73de115215b'
 
   depends_on PostgresqlInstalled.new
 
-  skip_clean :all
+  skip_clean 'share'
 
   def install
+    ENV.prepend 'PATH', Formula.factory('postgresql').bin, ':'
     system "make install"
-    bin.install %w(bbin/pg_prove bbin/pg_tapgen)
   end
 end
