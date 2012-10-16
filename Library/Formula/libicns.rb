f89a696cd4aaca895bd8e7566695f8c4f070466e
Damien Pollet <damien.pollet@gmail.com>
2012-10-17 00:37:40 +0200

    New formula: libicns 0.8.1
    Library dealing with OS X icon file format, and a couple conversion
    utilities: icns2png & png2icns.

--- Library/Formula/libicns.rb
+++ Library/Formula/libicns.rb
@@ -1,21 +1,33 @@
 require 'formula'
 
 class Libicns < Formula
-  homepage 'http://icns.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/icns/libicns-0.8.1.tar.gz'
-  sha256 '335f10782fc79855cf02beac4926c4bf9f800a742445afbbf7729dab384555c2'
+  homepage 'http://icns.sourceforge.net'
+  url 'http://sourceforge.net/projects/icns/files/libicns-0.8.1.tar.gz'
+  sha1 '6e8a27326dad93133b9575acfedb6fa7a7694bf2'
+  head 'https://icns.svn.sourceforge.net/svnroot/icns'
 
-  depends_on 'jasper'
-  depends_on :libpng
+  option 'with-jasper', 'Build with JasPer rather than OpenJPEG'
+
+  depends_on 'libpng'
+  depends_on 'openjpeg' unless build.include? 'with-jasper'
+  depends_on 'jasper'       if build.include? 'with-jasper'
 
   def install
-    # Fix for libpng 1.5 on Lion, may not be needed in head version of libicns
-    inreplace 'icnsutils/png2icns.c', 'png_set_gray_1_2_4_to_8', 'png_set_expand_gray_1_2_4_to_8'
-
-    ENV.universal_binary # Also build 32-bit so Wine can use it
-
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
+    (share/name).install Dir['samples/test*']
+  end
+
+  def test
+    mktemp do
+      %w{ test1.icns test2.rsrc test3.bin }.each do |testfile|
+        system "#{bin}/icns2png", '-l', "#{share}/#{name}/#{testfile}"
+        system "#{bin}/icns2png", '-x', "#{share}/#{name}/#{testfile}"
+      end
+      # test converting back on #1 (only one that comes back bit-identical)
+      system "#{bin}/png2icns", "test1.icns", "test1_128x128x32.png"
+      system "cmp", "test1.icns", "#{share}/#{name}/test1.icns"
+    end
   end
 end
