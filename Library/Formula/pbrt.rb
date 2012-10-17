797067d6fddc2fc831c4dce8934bc7892ea46a65
Sihui Xu <small.mimi@gmail.com>
2012-10-17 22:59:17 +0800

    New formula: Pbrt 2.0.334.

---
+++ Library/Formula/pbrt.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Pbrt < Formula
+  homepage 'http://pbrt.org/'
+  url 'https://github.com/mmp/pbrt-v2/tarball/2.0.334'
+  version '2.0.334'
+  sha1 'a5d1324f3ab9072bd6a88fee3cbc0c153bbb50eb'
+
+  depends_on "openexr"
+
+  def install
+    cd "src" do
+      system "make"
+
+      cd "bin" do
+        bin.install "bsdftest", "exravg", "exrdiff", "pbrt"
+      end
+    end
+  end
+end
