a0b3d7f75a597649b20e3a92d560a392ee4b0513
Frizlab <fload@me.com>
2012-10-19 17:48:46 +0200

    plowshare: add caveat about X11 support for imagemagick
    Closes #15550.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/plowshare.rb
+++ Library/Formula/plowshare.rb
@@ -24,6 +24,15 @@ class Plowshare < Formula
     ENV["PREFIX"] = prefix
     system "bash setup.sh install"
   end
+
+  def caveats; <<-EOS.undent
+    The default installation of imagemagick does not enable
+    X11 support. plowshare uses the display command which does
+    not work if X11 support is not enabled. To enable:
+      brew remove imagemagick
+      brew install imagemagick --with-x
+    EOS
+  end
 end
 
 
