0198f3db4ce2ae54e9c10ed0a33b946a37b76087
Bradley Bell <bradleyb@uw.edu>
2012-10-19 15:00:34 -0700

    add release candidate version of log4cpp

--- Library/Formula/log4cpp.rb
+++ Library/Formula/log4cpp.rb
@@ -5,6 +5,11 @@ class Log4cpp < Formula
   homepage 'http://log4cpp.sourceforge.net/'
   sha1 'bdb2fa7a8c8af738d79156bf9f6191875cdba5cc'
 
+  devel do
+    url 'http://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1rc3.tar.gz'
+    sha1 '24dc2eda5b1cee7af58a42924f61cd44c88f066a'
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
