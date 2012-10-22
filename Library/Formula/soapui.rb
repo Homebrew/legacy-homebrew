1bbc27c996af28c0a55cf5678879327208a2294c
Lukas Marek <lukas.marek@gmail.com>
2012-10-22 16:32:54 +0200

    SOAPUI_HOME is set by patching soapui.sh

--- Library/Formula/soapui.rb
+++ Library/Formula/soapui.rb
@@ -6,28 +6,32 @@ class Soapui < Formula
   version '4.5.1'
   sha1 'b2386e22259d9b8746e0f57fe03a61af16461f6d'
 
-	def install
-	    libexec.install Dir['*']
-	    bin.install_symlink Dir["#{libexec}/bin/*.sh"]
-	    system "chmod +x " "#{libexec}/bin/*.sh"	    
-	end
-
-	def test
-	    system "#{bin}/soapui", "-e", "puts 'hello'"
-  	end
-
-  	def caveats;
-    <<-EOS.undent
-        Add the following into your $HOME/.bashrc or $HOME/.bash_profile or your shell's equivalent:
-
-        ########################################################################################        
-        # export SOAPUI_HOME                      => reflects location of SoapUI        
-        ########################################################################################
-        export SOAPUI_HOME=#{libexec}
-    EOS
+  def install
+      libexec.install Dir['*']
+      bin.install_symlink Dir["#{libexec}/bin/*.sh"]
+      system "chmod +x " "#{libexec}/bin/*.sh"      
   end
 
+  def test
+      system "#{bin}/soapui", "-e", "puts 'hello'"
+    end
 
+  def patches
+    # fixes SOAPUI_HOME
+    DATA
+  end
 end
 
-
+__END__
+diff --git a/bin/soapui.sh b/bin/soapui.sh
+index a77a3de..1f681b1 100644
+--- a/bin/soapui.sh
++++ b/bin/soapui.sh
+@@ -21,7 +21,7 @@ esac
+ if [ "x$SOAPUI_HOME" = "x" ];
+ then
+     # get the full path (without any relative bits)
+-    SOAPUI_HOME=`cd $DIRNAME/..; pwd`
++    SOAPUI_HOME=$HOMEBREW_PREFIX/soapui/4.5.1/libexec
+ fi
+ export SOAPUI_HOME
