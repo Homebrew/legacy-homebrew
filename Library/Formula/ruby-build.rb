4184c671a07a4917d8f6c05c8c7f0883c9b53c62
Erik Michaels-Ober <sferik@gmail.com>
2012-10-22 13:44:11 -0700

    ruby-build 20121022
    Added a definition for JRuby 1.7.0
    
    Closes #15608.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyBuild < Formula
   homepage 'https://github.com/sstephenson/ruby-build'
-  url 'https://github.com/sstephenson/ruby-build/tarball/v20121020'
-  sha1 '62d2ed8995c02e5d8e3f55f0db381b4e3d44d310'
+  url 'https://github.com/sstephenson/ruby-build/tarball/v20121022'
+  sha1 '925b8eee613076f0b36bd9612a1dfb2abe03ff3f'
 
   head 'https://github.com/sstephenson/ruby-build.git'
 
