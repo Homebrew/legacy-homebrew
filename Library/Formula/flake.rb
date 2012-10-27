89b5b5fac1dc4bb962126af585c3d9bd80dc2b29
Katherine Whitlock <toroidalcode@gmail.com>
2012-10-27 15:47:09 -0400

    Altered flake.rb to match Homebrew standards

--- Library/Formula/flake.rb
+++ Library/Formula/flake.rb
@@ -1,10 +1,8 @@
 require 'formula'
 
-
 class Flake < Formula
-  my_version = 0.11
   homepage 'http://flake-enc.sourceforge.net'
-  url "http://downloads.sourceforge.net/project/flake-enc/flake/#{my_version}/flake-#{my_version}.tar.bz2"
+  url "http://downloads.sourceforge.net/project/flake-enc/flake/0.11/flake-0.11.tar.bz2"
   sha1 '2dd2276c1f1ba36abb1c305185efeced06abca62'
 
   def install
