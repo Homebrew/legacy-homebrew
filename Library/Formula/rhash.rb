fcec7f1ca92b95ebeafaf83e8579176dac5ba4a4
Michael Bashkirov <bashmish@gmail.com>
2012-10-28 00:44:39 +0400

    rhash: fix install path

--- Library/Formula/rhash.rb
+++ Library/Formula/rhash.rb
@@ -6,7 +6,7 @@ class Rhash < Formula
   sha1 '83c0e74a39a7824f430ef24c107e3474831d0acf'
 
   def install
-    system 'make', 'install', "PREFIX=#{prefix}",
+    system 'make', 'install', "PREFIX=",
                               "DESTDIR=#{prefix}",
                               "CC=#{ENV.cc}"
   end
