64608adca28b66e08a4ae268411656bee3cfda25
Adam Vandenberg <flangy@gmail.com>
2012-10-28 14:25:50 -0700

    tophat: fix whitespace in patch.
    Fixes #15726.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tophat.rb
+++ Library/Formula/tophat.rb
@@ -31,29 +31,29 @@ __END__
 @@ -4854,15 +4854,15 @@
        exit(0);
      }
-
+   
 -  std::set<Junction, skip_count_lt> vseg_juncs[num_threads];
 +  std::set<Junction, skip_count_lt> vseg_juncs[1];
    std::set<Junction, skip_count_lt> cov_juncs;
    std::set<Junction, skip_count_lt> butterfly_juncs;
-
+   
    std::set<Junction> juncs;
-
+   
 -  std::set<Deletion> vdeletions[num_threads];
 -  std::set<Insertion> vinsertions[num_threads];
 -  FusionSimpleSet vfusions[num_threads];
 +  std::set<Deletion> vdeletions[1];
 +  std::set<Insertion> vinsertions[1];
 +  FusionSimpleSet vfusions[1];
-
+   
    RefSequenceTable rt(sam_header, true);
-
+   
 --- a/src/tophat_reports.cpp	2012-10-18 10:43:09.000000000 -0700
 +++ b/src/tophat_reports.cpp	2012-10-27 22:39:31.000000000 -0700
 @@ -2290,11 +2290,11 @@
-	num_threads = 1;
+ 	num_threads = 1;
      }
-
+ 
 -  JunctionSet vjunctions[num_threads];
 -  InsertionSet vinsertions[num_threads];
 -  DeletionSet vdeletions[num_threads];
@@ -64,13 +64,13 @@ __END__
 +  DeletionSet vdeletions[1];
 +  FusionSet vfusions[1];
 +  Coverage vcoverages[1];
-
+   
    vector<boost::thread*> threads;
    for (int i = 0; i < num_threads; ++i)
 @@ -2420,10 +2420,10 @@
      fprintf(stderr, "Warning: %lu small overhang junctions!\n", (long unsigned int)small_overhangs);
    */
-
+ 
 -  JunctionSet vfinal_junctions[num_threads];
 -  InsertionSet vfinal_insertions[num_threads];
 -  DeletionSet vfinal_deletions[num_threads];
@@ -79,6 +79,6 @@ __END__
 +  InsertionSet vfinal_insertions[1];
 +  DeletionSet vfinal_deletions[1];
 +  FusionSet vfinal_fusions[1];
-
+ 
    for (int i = 0; i < num_threads; ++i)
      {
