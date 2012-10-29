4f3da6c77d934db6ad41d70a1bd5c71ee2d579a9
Brett Koonce <koonce@gmail.com>
2012-10-28 20:37:34 -0500

    MonetDB 11.13.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/monetdb.rb
+++ Library/Formula/monetdb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Monetdb < Formula
   homepage 'http://www.monetdb.org/'
-  url 'http://dev.monetdb.org/downloads/sources/Jul2012-SP2/MonetDB-11.11.11.tar.bz2'
-  sha1 'ee053afcfcb2551856f78076eabfb250535d397f'
+  url 'http://dev.monetdb.org/downloads/sources/Oct2012/MonetDB-11.13.3.tar.bz2'
+  sha1 'b414d85f49463b0d46da1275bc91f166c302d50d'
 
   head 'http://dev.monetdb.org/hg/MonetDB', :using => :hg
 
