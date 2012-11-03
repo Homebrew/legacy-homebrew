38546671b099bd5c84935ff43c3c25ee2c1905fd
Guilherme Brondani Torri <guitorri@gmail.com>
2012-11-03 13:34:10 +0100

    New formula gtk-mac-integration
    Add formula for gtk-mac-integration package. Including simple test.

---
+++ Library/Formula/gtk-mac-integration.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class GtkMacIntegration < Formula
+  homepage 'https://live.gnome.org/GTK+/OSX'
+  url 'ftp://ftp.gnome.org/pub/gnome/sources/gtk-mac-integration/2.0/gtk-mac-integration-2.0.1.tar.xz'
+  sha1 '9d939a2e4fb6c0ab4fe3d544ac712a152451249a'
+  head 'git://git.gnome.org/gtk-mac-integration'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gtk+'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+
+    system "make install"
+
+    prefix.install "src/.libs/test-integration"
+    (prefix + "src").install "src/testui.xml"
+  end
+
+  def test
+    system "cd #{prefix} && ./test-integration"
+  end
+end
