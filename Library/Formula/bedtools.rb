6d07f9770014506f6ad489014dd07e6e409b442a
Adam Vandenberg <flangy@gmail.com>
2012-11-04 08:27:06 -0800

    bedtools: style nits

--- Library/Formula/bedtools.rb
+++ Library/Formula/bedtools.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class Bedtools < Formula
-  url 'http://bedtools.googlecode.com/files/BEDTools.v2.17.0.tar.gz'
   homepage 'http://code.google.com/p/bedtools/'
+  url 'http://bedtools.googlecode.com/files/BEDTools.v2.17.0.tar.gz'
   sha1 '1b1de3c35394a423f9ad98a9957a8853b426a578'
+
   head 'https://github.com/arq5x/bedtools.git'
 
   def install
     system "make all"
-    bin.install Dir['bin/*']
+    prefix.install 'bin'
   end
 end
