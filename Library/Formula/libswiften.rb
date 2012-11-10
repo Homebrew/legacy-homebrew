b093fa87cd82088a2184a3b3ee6a885ebba0a589
Remko Tronçon <git@el-tramo.be>
2012-11-10 11:17:43 +0100

    libswiften 1.0
    Swiften is a robust, high-quality, standards-compliant, cross-platform,
    and performant C++ library for implementing XMPP applications. Swiften
    is used as the back-end library for the Swift Instant Messaging
    client.

---
+++ Library/Formula/libswiften.rb
@@ -0,0 +1,40 @@
+require 'formula'
+
+class Libswiften < Formula
+  homepage 'http://swift.im/swiften'
+
+  url 'http://swift.im/downloads/releases/swift-1.0/swift-1.0.tar.gz'
+  sha1 '9f3780e5dca32c0e0a0ad04e36a3f6214400a1ca'
+
+  head "git://swift.im/swift"
+
+  devel do
+    url 'http://swift.im/downloads/releases/swift-2.0beta2/swift-2.0beta2.tar.gz'
+    version '2.0beta2'
+    sha1 '1ad1967f5d1928d1e7ce9a76877912b779e20c8f'
+  end
+
+  depends_on 'scons' => :build
+  depends_on 'libidn'
+  depends_on 'boost'
+
+  def install
+    system "scons",
+        "-j #{ENV.make_jobs}",
+        "V=1", "optimize=1",
+        "debug=0",
+        "allow_warnings=1",
+        "swiften_dll=1",
+        "boost_includedir=#{HOMEBREW_PREFIX}/opt/boost/include",
+        "boost_libdir=#{HOMEBREW_PREFIX}/opt/boost/lib",
+        "libidn_includedir=#{HOMEBREW_PREFIX}/opt/libidn/include",
+        "libidn_libdir=#{HOMEBREW_PREFIX}/opt/libidn/lib",
+        "SWIFTEN_INSTALLDIR=#{prefix}",
+        prefix
+    man1.install 'Swift/Packaging/Debian/debian/swiften-config.1' unless build.stable?
+  end
+
+  def test
+    system "#{bin}/swiften-config"
+  end
+end
