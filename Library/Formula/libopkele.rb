f6f926a972368fe3e449c275bd40f9faff7b25c5
Denis Sukhonin <d.sukhonin@gmail.com>
2012-11-12 01:31:26 +0400

    Fixes.
    * Use SHA1.
    * Remove OpenSSL dependency.
    * Put a reason to fails_with.

--- Library/Formula/libopkele.rb
+++ Library/Formula/libopkele.rb
@@ -3,13 +3,21 @@ require 'formula'
 class Libopkele < Formula
   url 'http://kin.klever.net/dist/libopkele-2.0.4.tar.bz2'
   homepage 'http://kin.klever.net/libopkele/'
-  md5 '47a7efbdd2c9caaaa8e4360eb2beea21'
-  head 'git://github.com/hacker/libopkele.git'
+  sha1 '0c403d118efe6b4ee4830914448078c0ee967757'
+  head 'https://github.com/hacker/libopkele.git'
 
-  depends_on 'openssl'
   depends_on 'pkg-config' => :build
 
-  fails_with :clang
+  fails_with :clang do
+    build 2336
+    cause <<-EOS.undent
+      In file included from discovery.cc:5:
+      ../include/opkele/discovery.h:24:11: error: use of undeclared identifier 'insert'
+                              return insert(typename map_type::value_type(priority,d))->second;
+                                     ^
+                                     this->
+    EOS
+  end if !build.head?
 
   if build.head?
     depends_on :automake
