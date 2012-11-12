45263d92bd1ebeb46e1e99b465f0db552721be8e
Adam Vandenberg <flangy@gmail.com>
2012-11-12 06:57:53 -0800

    jmeter: use binary download
    Fixes #15993.

--- Library/Formula/jmeter.rb
+++ Library/Formula/jmeter.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Jmeter < Formula
   homepage 'http://jmeter.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=jmeter/source/apache-jmeter-2.8_src.tgz'
+  url 'http://www.apache.org/dyn/closer.cgi?path=jmeter/binaries/apache-jmeter-2.8.tgz'
   sha1 'e18cac4ab2b73dfcfe5d11e857905fa31c638563'
 
   def install
