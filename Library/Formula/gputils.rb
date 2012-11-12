31ae4b456f027498d95fca3acda4b528a781fe15
Brett Koonce <koonce@gmail.com>
2012-11-12 15:01:19 -0600

    gputils 0.14.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gputils.rb
+++ Library/Formula/gputils.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gputils < Formula
   homepage 'http://gputils.sourceforge.net/'
-  url 'http://sourceforge.net/projects/gputils/files/gputils/0.14.2/gputils-0.14.2.tar.gz'
-  sha1 '65253319f2e105eb17a8d5b9a77454f213ca5efe'
+  url 'http://sourceforge.net/projects/gputils/files/gputils/0.14.3/gputils-0.14.3.tar.gz'
+  sha1 '182da2eed6671cef4f2120a06913fe42e2e85110'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
