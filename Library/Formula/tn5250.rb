46026ab9ef0af2e1fefe4ca68444fd50d24c7579
Tjerk Smits <tjerk.smits@gmail.com>
2012-11-13 19:08:21 +0100

    OS 10.7+ patch
    Added patch for OS X 10.7 and 10.8

--- Library/Formula/tn5250.rb
+++ Library/Formula/tn5250.rb
@@ -16,4 +16,34 @@ class Tn5250 < Formula
   def test
     system "false"
   end
+  
+  def patches
+    DATA
+  end
 end
+
+__END__
+
+--- tn5250-0.17.4/curses/cursesterm.c 2008-11-21 01:12:20.000000000 -0700
++++ cursesterm.c 2012-07-28 11:45:04.000000000 -0600
+@@ -22,6 +22,9 @@
+#define _TN5250_TERMINAL_PRIVATE_DEFINED
+#include "tn5250-private.h"
+#include "cursesterm.h"
++#ifdef __APPLE__
++#include <term.h>
++#endif
+
+#ifdef USE_CURSES
+
+
+--- tn5250-0.17.4/curses/tn5250.c 2008-11-21 01:12:21.000000000 -0700
++++ tn5250.c 2012-07-28 11:37:10.000000000 -0600
+@@ -19,6 +19,7 @@
+*/
+
+#include "tn5250-private.h"
++#include "cursesterm.h"
+
+Tn5250Session *sess = NULL;
+Tn5250Stream *stream = NULL;
