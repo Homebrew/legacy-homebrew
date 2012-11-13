5aacebc1ecc0911accc13ad419274cec8a264633
Stefan <hydra_github@me.com>
2012-11-13 04:53:46 +0100

    libtasn1 3.0
    Closes #16004.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -2,12 +2,11 @@ require 'formula'
 
 class Libtasn1 < Formula
   homepage 'http://www.gnu.org/software/libtasn1/'
-  url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-2.14.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/libtasn1/libtasn1-2.14.tar.gz'
-  sha1 '22f9e0b15f870c8e03ac9cc1ead969d4d84eb931'
+  url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-3.0.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/libtasn1/libtasn1-3.0.tar.gz'
+  sha1 '0ce12f8b0460ae6eabf2a608506dbd337bf78a71'
 
   def install
-    ENV.universal_binary
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
