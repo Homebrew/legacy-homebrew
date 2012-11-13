214a619a061c6f2afceb9f1ea53332084a52d3f1
Stefan <hydra_github@me.com>
2012-11-13 04:54:16 +0100

    Update gnutls to 3.1.4
    Closes #16009.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gnutls.rb
+++ Library/Formula/gnutls.rb
@@ -2,16 +2,15 @@ require 'formula'
 
 class Gnutls < Formula
   homepage 'http://www.gnu.org/software/gnutls/gnutls.html'
-  url 'http://ftpmirror.gnu.org/gnutls/gnutls-3.1.3.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/gnutls/gnutls-3.1.3.tar.xz'
-  sha256 'fcb236c663489d3dba5a3f41486810f3889eb4508403ebeeb58b79f0b34bce39'
+  url 'http://ftpmirror.gnu.org/gnutls/gnutls-3.1.4.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/gnutls/gnutls-3.1.4.tar.xz'
+  sha256 'f27d92cdca8a4f4406e58c91e90e9ce1c6f23d1bbeddf864be789b99b0ef7d70'
 
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'libtasn1'
   depends_on 'p11-kit'
   depends_on 'nettle'
-  depends_on 'gmp'
 
   fails_with :llvm do
     build 2326
@@ -19,10 +18,7 @@ class Gnutls < Formula
   end
 
   def install
-    ENV.append 'LDFLAGS', '-ltasn1' # find external libtasn1
-
     system "./configure", "--disable-dependency-tracking",
-                          "--disable-guile",
                           "--disable-static",
                           "--prefix=#{prefix}"
     system "make install"
