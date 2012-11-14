09472d512ebb7506c8cdfa6b36097ae60a71056d
Gary Wolfman <g_wolfman@icloud.com>
2012-11-13 20:02:29 -0500

    Add c++11 support for boost.rb, but also a similar version of boost 1.51 (upstream of 1.52 is broken under c++11)
    The commit consists of two formula files.  boost.rb adds c++11 support as simply as possible by including a 'with-c++11' option which will append the appropriate toolset, cxxflags and link flags for the c++11 std and libc++.  The formula also changes the call to ./bjam with a call to ./b2.  The two utilities are identical, but the boost project now has a preference for b2 as the proper name for the boost-build system, due to the massive divergence boost-build now has from Perforce Jam.  Unfortunately, an upstream bug in boost 1.52 makes it impossible to build boost 1.52 using c++11; multiple constexpr errors result which cause the build to fail.  Therefore, an equivalent formula for boost 1.51 is also provided as boost151.rb.  This formula will correctly build boost 1.51 using c++11 and libc++ and therefore provides a libc++ compliant version of boost for those who require it.  Eventually, when the constexpr bug is fixed upstream, boost151.rb could be deprecated and removed.

--- Library/Formula/boost151.rb
+++ Library/Formula/boost151.rb
@@ -17,17 +17,17 @@ class UniversalPython < Requirement
   end
 end
 
-class Boost < Formula
+class Boost151 < Formula
   homepage 'http://www.boost.org'
-  url 'http://downloads.sourceforge.net/project/boost/boost/1.52.0/boost_1_52_0.tar.bz2'
-  sha1 'cddd6b4526a09152ddc5db856463eaa1dc29c5d9'
+  url 'http://downloads.sourceforge.net/project/boost/boost/1.51.0/boost_1_51_0.tar.bz2'
+  sha1 '52ef06895b97cc9981b8abf1997c375ca79f30c5'
 
   head 'http://svn.boost.org/svn/boost/trunk'
 
   bottle do
-    sha1 'a4e733fe67c15b7bfe500b0855d84616152f7042' => :mountainlion
-    sha1 'dd94aac5f03fb553c1c0e393fbd346748b0bc524' => :lion
-    sha1 '5fae01afa7e5c6e2d29ec32a24324fdaa14cf594' => :snowleopard
+    sha1 'd1f4cb36278adb7d86b221bcfc63619ec3022fdb' => :mountainlion
+    sha1 '46dd00df6343295bceae54b040cceb1d3714fe15' => :lion
+    sha1 '474aed3845ceaf26e0eeb3175ad3e2c4f3bca942' => :snowleopard
   end
 
   env :userpaths
