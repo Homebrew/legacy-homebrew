d27efcc43ec301dabb73e9077b70fc22ee3a9b9c
Will Ross <paxswill@gmail.com>
2012-11-13 19:37:43 -0500

    Update the Android SDK/NDK to 4.2 versions

--- Library/Formula/android-ndk.rb
+++ Library/Formula/android-ndk.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class AndroidNdk < Formula
   homepage 'http://developer.android.com/sdk/ndk/index.html'
-  url 'http://dl.google.com/android/ndk/android-ndk-r8b-darwin-x86.tar.bz2'
-  version 'r8b'
-  sha1 '497ba01810c881c0655c9b0759d529adf9e8cf52'
+  url 'http://dl.google.com/android/ndk/android-ndk-r8c-darwin-x86.tar.bz2'
+  version 'r8c'
+  sha1 'fcdea88142f3e124285a16d6989b02747fd0cfde'
 
   depends_on 'android-sdk'
 
