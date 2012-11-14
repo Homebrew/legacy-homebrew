a10622baef9c953cd05a0a77bf99df5207133fdc
Tjerk Smits <tjerk.smits@gmail.com>
2012-11-14 21:30:58 +0100

    Update tn5250 fix

--- Library/Formula/tn5250.rb
+++ Library/Formula/tn5250.rb
@@ -3,7 +3,11 @@ require 'formula'
 class Tn5250 < Formula
   homepage 'http://tn5250.sourceforge.net/'
   url 'http://sourceforge.net/projects/tn5250/files/tn5250/0.17.4/tn5250-0.17.4.tar.gz'
-  md5 'd1eb7c5a2e15cd2f43a1c115e2734153'
+  sha1 '2c84f03f798fd5095009d6798d1e6c0b29e48a75'
+
+  def patches
+    DATA
+  end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
@@ -11,15 +15,7 @@ class Tn5250 < Formula
     system "make"
     system "make install" 
     system "make clean"
-  end
-
-  def test
-    system "false"
-  end
-  
-  def patches
-    DATA
-  end
+  end  
 end
 
 __END__
