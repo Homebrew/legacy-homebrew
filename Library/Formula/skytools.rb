130e9f2c3b39f58b058bab72f424c108b2fb0ee2
Stefan <hydra_github@me.com>
2012-11-16 09:17:09 +0100

    update skytools to 3.1.2

--- Library/Formula/skytools.rb
+++ Library/Formula/skytools.rb
@@ -2,10 +2,11 @@ require 'formula'
 
 class Skytools < Formula
   homepage 'http://pgfoundry.org/projects/skytools/'
-  url 'http://pgfoundry.org/frs/download.php/3321/skytools-3.1.tar.gz'
-  sha1 'f31fb7096f160fb959f8a217cbea529da04b277e'
+  url 'http://pgfoundry.org/frs/download.php/3390/skytools-3.1.2.tar.gz'
+  sha1 '47fef7abb4fab55b8987bff839a88afc4f4c717e'
 
-  depends_on :postgresql
+  # Works only with homebrew postgres: https://github.com/mxcl/homebrew/issues/16024
+  depends_on 'postgresql'
 
   def install
     system "./configure", "--prefix=#{prefix}"
