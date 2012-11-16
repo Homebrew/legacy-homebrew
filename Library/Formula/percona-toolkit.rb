d9be1cde0a598b65f60f1c7e7a868ee515452080
Gergo Erdosi <gergo@gergoerdosi.com>
2012-11-16 09:26:52 +0100

    Percona Toolkit 2.1.6
    Closes #16105.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/percona-toolkit.rb
+++ Library/Formula/percona-toolkit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class PerconaToolkit < Formula
   homepage 'http://www.percona.com/software/percona-toolkit/'
-  url 'http://www.percona.com/redir/downloads/percona-toolkit/2.1.5/percona-toolkit-2.1.5.tar.gz'
-  sha1 '611052414a89db71ec493fe725d650112c6034fb'
+  url 'http://www.percona.com/redir/downloads/percona-toolkit/2.1.6/percona-toolkit-2.1.6.tar.gz'
+  sha1 '75fed9c2bc9d658a99869a1c8f81c8a1e8999e58'
 
   depends_on 'Time::HiRes' => :perl
   depends_on 'DBD::mysql' => :perl
