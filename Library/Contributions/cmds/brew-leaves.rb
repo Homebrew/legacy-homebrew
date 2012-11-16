1f3300bef2b93cf650b86491db33933a4ba1c845
Jake Bell <jake@theunraveler.com>
2012-11-16 13:33:50 +0100

    Fixing brew-leaves command when formula has no "deps" method

--- Library/Contributions/cmds/brew-leaves.rb
+++ Library/Contributions/cmds/brew-leaves.rb
@@ -7,7 +7,7 @@ require 'formula'
 def get_used_by
   used_by = {}
   Formula.each do |f|
-    next if f.deps == nil
+    next if !f.respond_to?(:deps) || f.deps == nil
 
     f.deps.each do |dep|
       _deps = used_by[dep.to_s] || []
