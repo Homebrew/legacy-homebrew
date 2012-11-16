647e22d52c08bda992fbe49e88868835075978ec
Brett Koonce <koonce@gmail.com>
2012-11-16 16:14:14 -0600

    ktoblzcheck 1.40
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ktoblzcheck.rb
+++ Library/Formula/ktoblzcheck.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ktoblzcheck < Formula
   homepage 'http://ktoblzcheck.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/ktoblzcheck/ktoblzcheck-1.39.tar.gz'
-  sha1 'b88ae800ed4cf7a1b8547eb0104fc6663334716d'
+  url 'http://downloads.sourceforge.net/project/ktoblzcheck/ktoblzcheck-1.40.tar.gz'
+  sha1 '6bd54cdbeef15db63885a434362e45b7ac80c995'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
