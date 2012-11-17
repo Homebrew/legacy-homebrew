a86011a4f19b4d0d952bc8add1840cbbd0aa505e
Forrest Xu <forrest.xu@gmail.com>
2012-11-17 19:07:04 +0800

    task-spooler 0.7.3
    Closes #16118.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/task-spooler.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class TaskSpooler < Formula
+  homepage 'http://vicerveza.homeunix.net/~viric/soft/ts/'
+  url 'http://vicerveza.homeunix.net/~viric/soft/ts/ts-0.7.3.tar.gz'
+  sha1 '33b9321d4f48a2c2685a8240db4e00c0e69ef9bc'
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
