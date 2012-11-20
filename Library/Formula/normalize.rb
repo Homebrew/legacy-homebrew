975a7dbe5124d597d5d66964f5f20a9933bb44cf
Bernhard Wagner <gitcommit@bernhardwagner.net>
2012-11-21 00:20:11 +0100

    https://github.com/mxcl/homebrew/pull/16101#issuecomment-10563376

--- Library/Formula/normalize.rb
+++ Library/Formula/normalize.rb
@@ -5,9 +5,16 @@ class Normalize < Formula
   url 'http://savannah.nongnu.org/download/normalize/normalize-0.7.7.tar.gz'
   sha1 '1509ca998703aacc15f6098df58650b3c83980c7'
 
+	depends_on 'mad' unless build.include? 'without-mad'
+
+	option 'without-mad', 'Compile without MP3 support'
+
   def install
+		args = %W[--disable-debug --disable-dependency-tracking --prefix=#{prefix} --mandir=#{man}]
+		args << "--without-mad" if build.include? "without-mad"
+
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--mandir=#{man}"
+                          "--prefix=#{prefix}", "--mandir=#{man}", *args
     system "make install"
   end
 
