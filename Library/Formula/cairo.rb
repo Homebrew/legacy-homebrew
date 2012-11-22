485f7940f6fad77ed37c7f867b8dc844f43fba2b
Sebastian Haas <sehaas@deebas.com>
2012-11-22 13:27:10 +0100

    update cairo to 1.12.8 and remove obsolete patch

--- Library/Formula/cairo.rb
+++ Library/Formula/cairo.rb
@@ -5,10 +5,10 @@ require 'formula'
 
 class Cairo < Formula
   homepage 'http://cairographics.org/'
-  url 'http://cairographics.org/releases/cairo-1.12.6.tar.xz'
-  mirror 'http://ftp-nyc.osuosl.org/pub/gentoo/distfiles/cairo-1.12.6.tar.xz'
-  mirror 'ftp://anduin.linuxfromscratch.org/BLFS/conglomeration/cairo/cairo-1.12.6.tar.xz'
-  sha256 '64b0cb89ef45972427f65bc5974e1c2e060bececdea1785fc9b7b6c7f1f42ae1'
+  url 'http://cairographics.org/releases/cairo-1.12.8.tar.xz'
+  mirror 'http://ftp-nyc.osuosl.org/pub/gentoo/distfiles/cairo-1.12.8.tar.xz'
+  mirror 'ftp://anduin.linuxfromscratch.org/BLFS/conglomeration/cairo/cairo-1.12.8.tar.xz'
+  sha256 '8fbb6fc66117ab4100bad830cb4479497e53c6f3facb98bf05c8d298554ebdd9'
 
   keg_only :provided_pre_mountain_lion
 
@@ -23,11 +23,6 @@ class Cairo < Formula
   depends_on 'xz'=> :build
   depends_on 'glib' unless build.include? 'without-x'
   depends_on :x11 unless build.include? 'without-x'
-
-  # See: http://lists.cairographics.org/archives/cairo/2012-October/023694.html
-  def patches
-    DATA if MacOS.version <= :snow_leopard
-  end
 
   def install
     ENV.universal_binary if build.universal?
@@ -49,17 +44,3 @@ class Cairo < Formula
     system "make install"
   end
 end
-
-__END__
-diff --git a/src/cairo-xlib-surface-shm.c b/src/cairo-xlib-surface-shm.c	
-index 08169f2..6a1752d 100644
---- a/src/cairo-xlib-surface-shm.c
-+++ b/src/cairo-xlib-surface-shm.c
-@@ -51,7 +51,7 @@
- #include <X11/Xlibint.h>
- #include <X11/Xproto.h>
- #include <X11/extensions/XShm.h>
--#include <X11/extensions/shmproto.h>
-+#include <X11/extensions/shmstr.h>
- #include <sys/ipc.h>
- #include <sys/shm.h>
