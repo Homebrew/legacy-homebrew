2640d0d087b0d5a50d2a3ab5ab4eb925da28a66d
William Ting <william.h.ting@gmail.com>
2012-11-22 12:10:52 -0600

    autojump 21.0.5
    Closes #16196.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/autojump.rb
+++ Library/Formula/autojump.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Autojump < Formula
   homepage 'https://github.com/joelthelion/autojump#name'
-  url 'https://github.com/downloads/joelthelion/autojump/autojump_v21.0.3.tar.gz'
-  sha1 'b81308d6ff50680cb74795195e93427557a938ad'
+  url 'https://github.com/downloads/joelthelion/autojump/autojump_v21.0.5.tar.gz'
+  sha1 'df2f7acd1b88e278fa0cace4db3b4a8fa01e4c23'
 
   head 'https://github.com/joelthelion/autojump.git'
 
@@ -21,7 +21,7 @@ class Autojump < Formula
     Add the following line to your ~/.bash_profile or ~/.zshrc file (and remember
     to source the file to update your current session):
 
-    [[ -f `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
+    [[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
 
     EOS
   end
