7cb5c0d3d9a0e91ae7e155f945edeecab2478800
Janusz Harkot <janusz.harkot@gmail.com>
2012-11-25 07:38:30 +0100

    limmemcached: 1.0.14 - sha1 instead md5

--- Library/Formula/libmemcached.rb
+++ Library/Formula/libmemcached.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libmemcached < Formula
   homepage 'http://libmemcached.org'
   url 'https://launchpad.net/libmemcached/1.0/1.0.14/+download/libmemcached-1.0.14.tar.gz'
-  md5 '3b074b9e23e07fe8fbb85ba5f952f62c'
+  sha1 'fe19ffa2bdfe2108f14019bb76adffb7a3bb7eee'
 
   depends_on 'memcached'
 
