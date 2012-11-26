2d07167c18dc2e12365b8751c5bc1766dbb389d9
Michele Damiano Torelli <me@mdtorelli.it>
2012-11-26 16:21:38 +0100

    play --devel 2.1-RC1
    Closes #16243.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -5,6 +5,12 @@ class Play < Formula
   url 'http://download.playframework.org/releases/play-2.0.4.zip'
   sha1 '98cacf40aa2099e01051a2f0b94187dd2fbb729a'
 
+  devel do
+    url 'http://download.playframework.org/releases/play-2.1-RC1.zip'
+    sha1 '71b03af81173f7b5e62eb620438bbb4b29e0c29f'
+    version '2.1-RC1'
+  end
+
   def install
     rm Dir['*.bat'] # remove windows' bat files
     libexec.install Dir['*']
