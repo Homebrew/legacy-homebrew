4b962eff3cddff6f67079c35fe3d14eb35b3b6b0
Zhao Cai <caizhaoff@gmail.com>
2012-11-26 12:56:55 -0500

    xtitle: directly install without rename

--- Library/Formula/xtitle.rb
+++ Library/Formula/xtitle.rb
@@ -1,16 +1,13 @@
 require 'formula'
 
 class Xtitle < Formula
-  homepage 'http://www.cs.indiana.edu/~kinzler/xtitle'
+  homepage 'http://www.cs.indiana.edu/~kinzler/xtitle/'
   url 'http://www.cs.indiana.edu/~kinzler/xtitle/xtitle-1.0.2.tgz'
   version '1.0.2'
   sha1 '0322cf93316a066bcba098b250821bcadaaedc02'
 
   def install
-    File.rename "xtitle.sh", "xtitle"
-    File.rename "xtitle.man", "xtitle.1"
-  
-    bin.install 'xtitle'
-    man1.install 'xtitle.1'
+    bin.install "xtitle.sh" => "xtitle"
+    man1.install "xtitle.man" => "xtitle.1"
   end
 end
