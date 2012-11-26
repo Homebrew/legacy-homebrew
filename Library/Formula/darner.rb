0286c1bfcfdec72b380a07bb1b79e47350b1df54
Douglas F Shearer <dougal.s@gmail.com>
2012-11-26 12:19:34 +0000

    Added a startup plist for Darner Formula.

--- Library/Formula/darner.rb
+++ Library/Formula/darner.rb
@@ -15,5 +15,64 @@ class Darner < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make install"
+
+    # Create the data directory.
+    FileUtils.mkdir_p "#{var}/darner/"
+
+    # Create the log directory.
+    FileUtils.mkdir_p "#{var}/log/darner/"
+  end
+
+  def caveats
+    <<-EOS.undent
+    If this is your first install, automatically load Darner on login with:
+        mkdir -p ~/Library/LaunchAgents
+        ln -nfs #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -wF ~/Library/LaunchAgents/#{plist_path.basename}
+
+    If this is an upgrade and you already have the #{plist_path.basename} loaded:
+        launchctl unload -w ~/Library/LaunchAgents/#{plist_path.basename}
+        ln -nfs #{plist_path} ~/Library/LaunchAgents/
+        launchctl load -wF ~/Library/LaunchAgents/#{plist_path.basename}
+
+    To stop the Darner daemon:
+        launchctl unload -wF ~/Library/LaunchAgents/#{plist_path.basename}
+
+    You'll find the Darner log here:
+        open #{var}/log/darner/darner.log
+
+    To start Darner manually:
+        darner -d #{var}/darner
+
+    EOS
+  end
+
+  def startup_plist
+    <<-PLIST.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>KeepAlive</key>
+          <true/>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{HOMEBREW_PREFIX}/bin/darner</string>
+            <string>-d</string>
+            <string>#{var}/darner</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/darner/darner.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/darner/darner.log</string>
+        </dict>
+      </plist>
+    PLIST
   end
 end
