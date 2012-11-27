0eb0c031a8cc9b33552495ea7ff5f7de9b73f1af
Stefan <hydra_github@me.com>
2012-11-27 14:04:25 +0100

    update gnutls to 3.1.5

--- Library/Formula/gnutls.rb
+++ Library/Formula/gnutls.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gnutls < Formula
   homepage 'http://www.gnu.org/software/gnutls/gnutls.html'
-  url 'http://ftpmirror.gnu.org/gnutls/gnutls-3.1.4.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/gnutls/gnutls-3.1.4.tar.xz'
-  sha256 'f27d92cdca8a4f4406e58c91e90e9ce1c6f23d1bbeddf864be789b99b0ef7d70'
+  url 'http://ftpmirror.gnu.org/gnutls/gnutls-3.1.5.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/gnutls/gnutls-3.1.5.tar.xz'
+  sha256 '2f095984dd9cbbf4dce417ecd81640fe47732322d025062b2c9a189d022de0d3'
 
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
@@ -14,14 +14,14 @@ class Gnutls < Formula
 
   fails_with :llvm do
     build 2326
-    cause "Undefined symbols when linking"
+    cause 'Undefined symbols when linking'
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-static",
+    system './configure', '--disable-dependency-tracking',
+                          '--disable-static',
                           "--prefix=#{prefix}"
-    system "make install"
+    system 'make install'
 
     # certtool shadows the OS X certtool utility
     mv bin+'certtool', bin+'gnutls-certtool'
