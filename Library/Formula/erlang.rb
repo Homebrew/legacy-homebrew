cb73cccc42d28268d6a0578749cd8688152e6f40
Jan Lehnardt <jan@apache.org>
2012-11-28 20:40:39 +0100

    erlang R15B03
    Closes #16294.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>
    
    WIP

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -1,38 +1,32 @@
 require 'formula'
 
 class ErlangManuals < Formula
-  url 'http://erlang.org/download/otp_doc_man_R15B02.tar.gz'
-  sha1 'e50cc887b36b0b2f158a87fa5b21cb2b2c6679b0'
+  url 'http://erlang.org/download/otp_doc_man_R15B03.tar.gz'
+  sha1 'a0ea6cc389b208c2a75ffb0027e143afc33c40b3'
 end
 
 class ErlangHtmls < Formula
-  url 'http://erlang.org/download/otp_doc_html_R15B02.tar.gz'
-  sha1 'b2ef425fe5aa9f4fff7afaa9b8204c45357eaa89'
+  url 'http://erlang.org/download/otp_doc_html_R15B03.tar.gz'
+  sha1 'ff866b07d217f48ee6253fdbed956be5ebf3cb9d'
 end
 
 class ErlangHeadManuals < Formula
-  url 'http://erlang.org/download/otp_doc_man_R15B02.tar.gz'
-  sha1 'e50cc887b36b0b2f158a87fa5b21cb2b2c6679b0'
+  url 'http://erlang.org/download/otp_doc_man_R15B03.tar.gz'
+  sha1 'a0ea6cc389b208c2a75ffb0027e143afc33c40b3'
 end
 
 class ErlangHeadHtmls < Formula
-  url 'http://erlang.org/download/otp_doc_html_R15B02.tar.gz'
-  sha1 'b2ef425fe5aa9f4fff7afaa9b8204c45357eaa89'
+  url 'http://erlang.org/download/otp_doc_html_R15B03.tar.gz'
+  sha1 'ff866b07d217f48ee6253fdbed956be5ebf3cb9d'
 end
 
 class Erlang < Formula
   homepage 'http://www.erlang.org'
   # Download tarball from GitHub; it is served faster than the official tarball.
-  url 'https://github.com/erlang/otp/tarball/OTP_R15B02'
-  sha1 '540d0d0a006082a8bc3e1fc239f2043fee015967'
+  url 'https://github.com/erlang/otp/tarball/OTP_R15B03'
+  sha1 '2497d3a0d04b55cae83f81974286440648993309'
 
   head 'https://github.com/erlang/otp.git', :branch => 'dev'
-
-  bottle do
-    sha1 '94cbe622b817e8a5bd7797b615aad5e47c5d8660' => :mountainlion
-    sha1 'ec5b4749668c95ad55410c0316390046ee576895' => :lion
-    sha1 '10b0aa609354c07938ac936578c9d1f12a4249ba' => :snowleopard
-  end
 
   # remove the autoreconf if possible
   depends_on :automake
