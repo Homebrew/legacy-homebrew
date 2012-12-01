0a2c58163c470ae812dc9f17c05936b6c91e904b
Brett Koonce <koonce@gmail.com>
2012-11-30 22:32:56 -0600

    duply 1.5.9
    Closes #16332.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/duply.rb
+++ Library/Formula/duply.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Duply < Formula
   homepage 'http://duply.net'
-  url 'http://sourceforge.net/projects/ftplicity/files/duply%20%28simple%20duplicity%29/1.5.x/duply_1.5.8.tgz'
-  sha1 '0b8925fc34a51642b752e550cf57a044c61bfa57'
+  url 'http://sourceforge.net/projects/ftplicity/files/duply%20%28simple%20duplicity%29/1.5.x/duply_1.5.9.tgz'
+  sha1 '5b9277b3f20ff88cfb045069580b6a3e9d382c83'
 
   depends_on 'duplicity'
 
