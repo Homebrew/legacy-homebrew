038ef3fd1754631c51e0b75b32f84bda969085af
nulltask <nulltask@gmail.com>
2012-12-02 02:20:44 +0900

    mon 1.1.1

---
+++ Library/Formula/mon.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Mon < Formula
+  homepage 'https://github.com/visionmedia/mon'
+  url 'https://github.com/visionmedia/mon/archive/1.1.1.tar.gz'
+  version '1.1.1'
+  sha1 '7dde195a394d47d7b5f0c69583d5ade6e8a82ef4'
+
+  def install
+    system "make install"
+  end
+end
