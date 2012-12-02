d0f9e12dfb275ba9ee5f23184526240246213cf9
John Wiegley <johnw@fpcomplete.com>
2012-12-02 01:16:41 -0600

    Finished recipe for prooftree

--- Library/Formula/prooftree.rb
+++ Library/Formula/prooftree.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Prooftree < Formula
   homepage 'http://askra.de/software/prooftree'
   url 'http://askra.de/software/prooftree/releases/prooftree-0.10.tar.gz'
@@ -12,17 +9,12 @@ class Prooftree < Formula
   depends_on 'lablgtk'
 
   def install
-    # ENV.j1  # if your formula's build system can't parallelize
-
     system "./configure", "--prefix", "#{prefix}"
     system "make"
     system "make install"
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test prooftree`.
-    system "false"
+    system "prooftree -help"
   end
 end
