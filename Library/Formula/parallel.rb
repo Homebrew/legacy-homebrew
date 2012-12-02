437c5d69b5239eadd660034d6810d1286ff90062
Brett Koonce <koonce@gmail.com>
2012-12-02 16:31:29 -0600

    parallel 20121122
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Parallel < Formula
   homepage 'http://savannah.gnu.org/projects/parallel/'
-  url 'http://ftpmirror.gnu.org/parallel/parallel-20121022.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20121022.tar.bz2'
-  sha256 '13626899b50f884d635e0bd21684e87e51d0f235e760f11b8df4a605e0fa03c8'
+  url 'http://ftpmirror.gnu.org/parallel/parallel-20121122.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20121122.tar.bz2'
+  sha256 '0315336141612ba2ec1f76e6c8c58a72f4531777c96b79b91ef64b3980be584f'
 
   def install
     system "./configure", "--prefix=#{prefix}"
