d3cf2de5aaab8b111c7327ff0db653e6d93dcfa6
gnzlbg <gonzalobg88@gmail.com>
2012-12-03 21:06:43 +0100

    Upgrade hdf5 version and enable parallel hdf5
    - EDIT: upgrade hdf5 to version 1.8.10
    - EDIT: added error messages for incompatible options
    - ADD: enable parallel hdf5 support

--- Library/Formula/hdf5.rb
+++ Library/Formula/hdf5.rb
@@ -2,14 +2,17 @@ require 'formula'
 
 class Hdf5 < Formula
   homepage 'http://www.hdfgroup.org/HDF5'
-  url 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.9/src/hdf5-1.8.9.tar.bz2'
-  sha1 '7d5e5e8caa5970c65e70a5b4ad6787efe0bf70bb'
+  url 'http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.10/src/hdf5-1.8.10.tar.bz2'
+  sha1 '867a91b75ee0bbd1f1b13aecd52e883be1507a2c'
 
   depends_on 'szip'
+  depends_on MPIDependency.new(:cc, :cxx, :f77, :f90)
 
-  # TODO - warn that these options conflict
   option 'enable-fortran', 'Compile Fortran bindings'
-  option 'enable-threadsafe', 'Trade performance and C++ or Fortran support for thread safety'
+  option 'enable-threadsafe', 'Thread safety. (incompatible with enable-cxx)'
+  option 'enable-parallel', 'Enable parallel hdf5 (incompatible with enable-cxx)'
+  option 'enable-cxx', 'Compile C++ bindings (incompatible with enable-parallel and enable-threadsafe)'
+  option 'test', 'Verify the build with make check'
 
   def install
     args = %W[
@@ -24,17 +27,38 @@ class Hdf5 < Formula
       --enable-shared=yes
     ]
 
-    if build.include? 'enable-threadsafe'
-      args.concat %w[--with-pthread=/usr --enable-threadsafe]
-    else
-      args << '--enable-cxx'
-      if build.include? 'enable-fortran'
-        args << '--enable-fortran'
-        ENV.fortran
-      end
+    if (build.include? 'enable-threadsafe') && (build.include? 'enable-cxx')
+      onoe "Incompatible options selected:",
+      "enable-threadsafe is not compatible with enable-cxx."
     end
 
-    system "./configure", *args
+    if (build.include? 'enable-parallel') && (build.include? 'enable-cxx')
+      onoe "Incompatible options selected:",
+      "enable-parallel is not compatible with enable-cxx."
+    end
+
+    args << '--enable-parallel' if build.include? 'enable-parallel'
+    args << '--enable-cxx' if build.include? 'enable-cxx'
+
+    if build.include? 'enable-threadsafe'
+      args.concat %w[--with-pthread=/usr --enable-threadsafe]
+    end
+
+    if build.include? 'enable-fortran'
+      args << '--enable-fortran'
+      ENV.fortran
+    end
+
+    if build.include? 'enable-parallel'
+      ENV['CC'] = "#{HOMEBREW_PREFIX}/bin/mpicc"
+      ENV['CXX'] = "#{HOMEBREW_PREFIX}/bin/mpicxx"
+      ENV['FC'] = "#{HOMEBREW_PREFIX}/bin/mpif90" if build.include? 'enable-fortran'
+      system './configure', *args
+    else
+      system "./configure", *args
+    end
+
+    system 'make check' if build.include? 'test'
     system "make install"
   end
 end
