52e5d7349c5539976830a183b71d1194f86d2294
Adam Vandenberg <flangy@gmail.com>
2012-12-02 18:12:27 -0800

    youtube-dl: use tagged github download
    Fixes #16363.

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -2,12 +2,11 @@ require 'formula'
 
 class YoutubeDl < Formula
   homepage 'http://rg3.github.com/youtube-dl/'
-  url 'https://github.com/rg3/youtube-dl/raw/2012.11.29/youtube-dl'
-  sha256 'e5c209765a366515845fde5da9d7022ad97c29e56b4611a4dc06f5d198377991'
-  version '2012.11.29'
+  url 'https://github.com/rg3/youtube-dl/archive/2012.11.29.tar.gz'
+  sha1 '2d13c271ef17781354f0372ca3a5ec6209892625'
 
   def install
-    system "make", "PREFIX=#{prefix}"
+    system "make", "youtube-dl", "PREFIX=#{prefix}"
     bin.install 'youtube-dl'
   end
 end
