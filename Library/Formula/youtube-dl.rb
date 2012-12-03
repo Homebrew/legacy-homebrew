7244b0bba144c3a9cd6a1b55d2c9c1f1feeb6e6a
Adam Vandenberg <flangy@gmail.com>
2012-12-02 16:33:42 -0800

    youtube-dl 2012.11.29

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class YoutubeDl < Formula
   homepage 'http://rg3.github.com/youtube-dl/'
-  url 'https://github.com/rg3/youtube-dl/tarball/2012.09.27/youtube-dl'
-  sha256 '45e88c1a5b81e633bddd43d5363e7ade92af2eeb534c37a5170f4b68d73605ea'
-  version '2012.09.27'
+  url 'https://github.com/rg3/youtube-dl/raw/2012.11.29/youtube-dl'
+  sha256 'e5c209765a366515845fde5da9d7022ad97c29e56b4611a4dc06f5d198377991'
+  version '2012.11.29'
 
   def install
     system "make", "PREFIX=#{prefix}"
