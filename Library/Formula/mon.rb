e22a86072985177fb9c2aec76ad006d11e9fbac0
Karl Böhlmark <karl.bohlmark@edgeware.tv>
2012-12-03 17:05:49 +0100

    Use homebrew prefix when installing

--- Library/Formula/mon.rb
+++ Library/Formula/mon.rb
@@ -7,6 +7,7 @@ class Mon < Formula
   sha1 '7dde195a394d47d7b5f0c69583d5ade6e8a82ef4'
 
   def install
-    system "make install"
+  	FileUtils.mkdir_p "#{prefix}/bin"
+    system "make install PREFIX=#{prefix}"
   end
 end
