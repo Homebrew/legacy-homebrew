34e2d089cebbfa3a0c16a3c11c971d18095c46b3
ashirazi <as4@eshirazi.com>
2012-12-02 22:46:53 -0500

    Update Library/Formula/git-flow-clone.rb
    Fix download link for submission.
    Stylistic improvements.

--- Library/Formula/git-flow-clone.rb
+++ Library/Formula/git-flow-clone.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GitFlowClone < Formula
   homepage 'https://github.com/ashirazi/git-flow-clone'
-  url 'https://github.com/ashirazi/git-flow-clone.git', :tag => '0.1.2'
+  url 'https://github.com/ashirazi/git-flow-clone/archive/0.1.2.zip'
   sha1 'dd3d31069f74e946c2ea3d7953d3652bd3c34cfb'
-  version '0.1.2'
+  
   depends_on 'git-flow'
 
   def install
