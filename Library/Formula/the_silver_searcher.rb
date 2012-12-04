fe56eed1d921e0c65c117c248f9c349d6be30a5b
Zearin <zearin@gonk.net>
2012-12-04 12:39:25 -0500

    Update  to 0.13.1

--- Library/Formula/the_silver_searcher.rb
+++ Library/Formula/the_silver_searcher.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class TheSilverSearcher < Formula
   homepage 'https://github.com/ggreer/the_silver_searcher'
-  url 'https://github.com/ggreer/the_silver_searcher/tarball/0.7.2'
-  sha1 '48c33fa25ded5c446c0fca8d32b10872aee38e70'
+  url 'https://github.com/downloads/ggreer/the_silver_searcher/the_silver_searcher-0.13.1.tar.gz'
+  sha1 '47128b0a19c70b263b0f448e052d6d63443fcadf'
 
   head 'https://github.com/ggreer/the_silver_searcher.git'
 
@@ -15,7 +15,7 @@ class TheSilverSearcher < Formula
 
   def install
     # Stable tarball does not include pre-generated configure script
-    system "aclocal -I /usr/local/share/aclocal"
+    system "aclocal -I #{HOMEBREW_PREFIX}/share/aclocal"
     system "autoconf"
     system "autoheader"
     system "automake --add-missing"
