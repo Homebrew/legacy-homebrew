433e6aaa14832e5b2479bbac5030097745aa885c
Rafe Colton <r.colton@modcloth.com>
2012-12-07 16:27:27 -0500

    Updating version to more accurate 'master'.
    Also adding note to this effect in install.

--- Library/Formula/cpp-netlib.rb
+++ Library/Formula/cpp-netlib.rb
@@ -4,15 +4,15 @@ class CppNetlib < Formula
   homepage 'http://cpp-netlib.github.com/latest/index.html'
   url 'https://nodeload.github.com/cpp-netlib/cpp-netlib/tar.gz/master'
   sha1 'dac3d9d1efa130edddecdea9a64e1b46e5afe2f8'
-  version '0.9.4'
+  version 'master'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
   depends_on 'openssl'
 
   def install
-    #make note that this is using master instead of 0.9.4
-    #check cmake version
+    ohai "Note: This formula is installing cpp-netlib from master instead of version 0.9.4 due to OSX-related fix in master"
+
     build_dir = Dir.pwd
     Dir.chdir "#{prefix}" do
       system "cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ #{build_dir}"
