e04e899370bc6e64e2897af9a2d678e9f704ebcc
Rafe Colton <r.colton@modcloth.com>
2012-12-07 16:19:28 -0500

    [Issue #16316] Adding formual for cpp-netlib
    Initial commit.

---
+++ Library/Formula/cpp-netlib.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class CppNetlib < Formula
+  homepage 'http://cpp-netlib.github.com/latest/index.html'
+  url 'https://nodeload.github.com/cpp-netlib/cpp-netlib/tar.gz/master'
+  sha1 'dac3d9d1efa130edddecdea9a64e1b46e5afe2f8'
+  version '0.9.4'
+
+  depends_on 'cmake' => :build
+  depends_on 'boost'
+  depends_on 'openssl'
+
+  def install
+    #make note that this is using master instead of 0.9.4
+    #check cmake version
+    build_dir = Dir.pwd
+    Dir.chdir "#{prefix}" do
+      system "cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ #{build_dir}"
+      system 'make'
+    end
+  end
+
+  def test
+    Dir.chdir "#{prefix}" do
+      system 'make test'
+    end
+  end
+end
