9b591ea814071258ab296724716bdc3ae7b9c6d5
chrisatkin <contact@chrisatk.in>
2012-12-09 15:09:59 +0000

    Added pplatex formula
    LaTeX is known to produce really nice document layouts. But it is also known to produce quite some line noise on the command line. This tool parses the log-output of latex and pdflatex and prints only warnings and errors in an humanely readable format.
    The code is based on the LaTeX output parser of Kile (also used by TexMakerX), with some modifications and bugfixes.

---
+++ Library/Formula/pplatex.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Pplatex < Formula
+  homepage 'http://www.stefant.org/web/projects/software/pplatex.html'
+  url 'http://dl.dropbox.com/u/12697903/pplatex/pplatex-1.0-rc1-src.tar.gz'
+  #version '1.0'
+  sha1 'd437c64a8263eeb45ded4f57df8cce29080a92d0'
+
+  depends_on 'scons' => :build
+  depends_on 'pcre'
+
+  def install
+    system 'scons'
+    bin.install('bin/pplatex')
+    bin.install('bin/ppdflatex')
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test pplatex`.
+    system "false"
+  end
+end
