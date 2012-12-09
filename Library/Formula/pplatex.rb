5772cba3ca1631e21b3d6b71b6cfea777d2671a3
chrisatkin <contact@chrisatk.in>
2012-12-09 15:55:25 +0000

    Removed autogenerated comments

--- Library/Formula/pplatex.rb
+++ Library/Formula/pplatex.rb
@@ -1,8 +1,4 @@
 require 'formula'
-
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Pplatex < Formula
   homepage 'http://www.stefant.org/web/projects/software/pplatex.html'
   url 'http://dl.dropbox.com/u/12697903/pplatex/pplatex-1.0-rc1-src.tar.gz'
@@ -19,9 +15,6 @@ class Pplatex < Formula
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test pplatex`.
     system "pplatex -h"
   end
 end
