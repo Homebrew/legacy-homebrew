ceee028da9a6018d0f0d3e1ef64ffb68bcd3c64f
futaya <futaya@cpan.org>
2012-12-10 14:12:11 +0900

    Update Library/Formula/dialog.rb

--- Library/Formula/dialog.rb
+++ Library/Formula/dialog.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Dialog < Formula
   homepage 'http://invisible-island.net/dialog/'
   url 'ftp://invisible-island.net/dialog/dialog-1.1-20120215.tgz'
+  mirror 'http://distfiles.macports.org/dialog/dialog-1.1-20120215.tgz'
   sha1 '0d8a07e064c6d4f9cc7d9cb21c4609dc4a19537e'
 
   def install
