75e66c1b1ec874fdbb28735b9de868dbb2ecb74b
Thomas Dunton <thomas.dunton@gmail.com>
2012-12-10 22:18:25 +0000

    Upgrade to Gromacs 4.6 - Scala Life 2012.
    Upgrade Gromacs to the Scala Life pre-release of version 4.6.

--- Library/Formula/gromacs.rb
+++ Library/Formula/gromacs.rb
@@ -2,23 +2,26 @@ require 'formula'
 
 class Gromacs < Formula
   homepage 'http://www.gromacs.org/'
-  url 'ftp://ftp.gromacs.org/pub/gromacs/gromacs-4.5.5.tar.gz'
-  sha1 'ce4b4f9a0453dd2ffea72f28ea0bc7bb7a72f479'
+  url 'http://www.scalalife.eu/system/files/Gromacs-4.6-ScalaLife2012.tgz'
+  sha1 '9f2d7b6bd6ffaee8ffd421d891785e682fabf3f2'
 
   option 'enable-mpi', "Enables MPI support"
   option 'enable-double',"Enables double precision"
 
   depends_on :x11
   depends_on 'fftw'
+  depends_on 'cmake'
 
   def install
-    args = ["--prefix=#{prefix}"]
-    args << "--enable-mpi" if build.include? 'enable-mpi'
-    args << "--enable-double" if build.include? 'enable-double'
+    #args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}"]
+    #args << "--enable-mpi" if build.include? 'enable-mpi'
+    #args << "--enable-double" if build.include? 'enable-double'
 
-    system "./configure", *args
-    system "make"
-    ENV.j1
+    system "mkdir build"
+    system "cd build"
+    system "cmake ../gromacs-4.6-ScalaLife-120831 -DCMAKE_INSTALL_PREFIX=#{prefix} -DGMX_GPU=OFF -DGMX_OPENMP=OFF"
+    system "make -j2"
+    #ENV.j2
     system "make install"
   end
 end
