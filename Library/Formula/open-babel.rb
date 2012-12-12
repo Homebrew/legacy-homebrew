b91c2c1a4b293b57f4bc9d1eb4454f0a13553be6
Agilio Padua <agilio.padua@gmail.com>
2012-12-12 19:39:23 +0100

    Added option to build Java language bindings

--- Library/Formula/open-babel.rb
+++ Library/Formula/open-babel.rb
@@ -1,25 +1,28 @@
 require 'formula'
 
-class Openbabel < Formula
+class OpenBabel < Formula
   homepage 'http://www.openbabel.org'
   url 'http://sourceforge.net/projects/openbabel/files/openbabel/2.3.2/openbabel-2.3.2.tar.gz'
   sha1 'b8831a308617d1c78a790479523e43524f07d50d'
 
-  option 'build-gui',       'Build the Graphical User Interface'
-  option 'png',             'Support PNG depiction'
-  option 'python-bindings', 'Compile Python language bindings'
+  option 'gui',    'Build the Graphical User Interface'
+  option 'png',    'Support PNG depiction'
+  option 'python', 'Compile Python language bindings'
+  option 'java',   'Compile Java language bindings'
 
   depends_on 'cmake' => :build
   depends_on 'wxmac' if build.include? 'build-gui'
   depends_on 'cairo' if build.include? 'png'
   depends_on 'eigen' if build.include? 'python-bindings'
+  depends_on 'eigen' if build.include? 'java-bindings'
 
   def install
     args = %W[ -DCMAKE_INSTALL_PREFIX=#{prefix} ]
-    args << '-DPYTHON_BINDINGS=ON' if build.include? 'python-bindings'
-    args << '-DBUILD_GUI=ON' if build.include? 'build-gui'
-    args << '-DCAIRO_INCLUDE_DIRS=/usr/local/include/cairo '\
-            '-DCAIRO_LIBRARIES=/usr/local/lib/libcairo.dylib' if build.include? 'png'
+    args << "-DPYTHON_BINDINGS=ON" if build.include? 'python'
+    args << "-DJAVA_BINDINGS=ON" if build.include? 'java'
+    args << "-DBUILD_GUI=ON" if build.include? 'gui'
+    args << "-DCAIRO_INCLUDE_DIRS=#{include}/cairo "\
+            "-DCAIRO_LIBRARIES=#{lib}/libcairo.dylib" if build.include? 'png'
 
     system "mkdir ../build"
     system "cd ../build"
@@ -30,10 +33,9 @@ class Openbabel < Formula
 
   def caveats; <<-EOS.undent
     Language bindings will be installed to the same location as the
-    Open Babel libraries (/usr/local/lib).
-    To prepare to use the bindings, add the install directory to the
-    front of the appropriate environment variable:
-    PYTHONPATH for Python.
+    Open Babel libraries. To prepare to use the bindings, prepend the
+    install directory to the appropriate environment variable:
+    PYTHONPATH for Python, CLASSPATH for Java.
     EOS
   end
 end
