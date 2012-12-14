a57d38d26b46cc78fab0bb4ce0aca319aad4dff9
Ian Will <icwill@gmail.com>
2012-12-14 12:19:15 -0500

    Updated cmake dependency per homebrew conventions
     - Explicitly listed the build category
     - Listed cmake dependency first

--- Library/Formula/openjpeg.rb
+++ Library/Formula/openjpeg.rb
@@ -7,10 +7,10 @@ class Openjpeg < Formula
 
   head 'http://openjpeg.googlecode.com/svn/trunk/'
 
+  depends_on 'cmake' => :build
   depends_on 'little-cms2'
   depends_on 'libtiff'
   depends_on :libpng
-  depends_on 'cmake'
 
   def install
     system "cmake", ".", *std_cmake_args
