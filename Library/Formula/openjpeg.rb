151663dc148780ad57c05c2c850dc9463b349db0
Ian Will <ian.will@nrl.navy.mil>
2012-12-14 09:19:43 -0500

    Updated openjpeg to version 2.0.0

--- Library/Formula/openjpeg.rb
+++ Library/Formula/openjpeg.rb
@@ -2,17 +2,21 @@ require 'formula'
 
 class Openjpeg < Formula
   homepage 'http://www.openjpeg.org/'
-  url 'http://openjpeg.googlecode.com/files/openjpeg-1.5.1.tar.gz'
-  sha1 '1b0b74d1af4c297fd82806a9325bb544caf9bb8b'
+  url 'http://openjpeg.googlecode.com/files/openjpeg-2.0.0.tar.gz'
+  sha1 '0af78ab2283b43421458f80373422d8029a9f7a7'
+  #url 'http://openjpeg.googlecode.com/files/openjpeg-1.5.1.tar.gz'
+  #sha1 '1b0b74d1af4c297fd82806a9325bb544caf9bb8b'
 
   head 'http://openjpeg.googlecode.com/svn/trunk/'
 
   depends_on 'little-cms2'
   depends_on 'libtiff'
   depends_on :libpng
+  depends_on 'cmake'
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "cmake", ".", *std_cmake_args
+    system "make"
     system "make install"
   end
 end
