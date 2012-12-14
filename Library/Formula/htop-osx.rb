c60f7c07085478c6717397fbed6103195065e28e
Thomas Kupper <thomas.kupper@gmail.com>
2012-12-14 13:37:18 +0100

    Make htop-osx run on my rMBP 10.8.2 and iMac 10.7.5
    Small adjustments in the Formula to prevent it from crashing

--- Library/Formula/htop-osx.rb
+++ Library/Formula/htop-osx.rb
@@ -2,12 +2,23 @@ require 'formula'
 
 class HtopOsx < Formula
   homepage 'https://github.com/max-horvath/htop-osx'
+  version '0.8.2.1'
   url 'https://github.com/max-horvath/htop-osx/tarball/0.8.2.1-2012-04-18'
   sha1 '90975472c683e59a6476e215ae5cb768d86659a8'
+
+  binname = 'htop'
 
   depends_on :autoconf
   depends_on :automake
   depends_on :libtool
+
+  if build.include? 'env=super'
+     onoe "#{binname} crashes in procedure BarMeterMode_draw (Meter.c) if using super environment"
+     exit 1
+  end
+
+  env :std
+  ohai 'compiling using standard environment...'
 
   def install
     # Otherwise htop will segfault when resizing the terminal
