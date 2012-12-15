5298623f4648b80967305c300299afb18c7a2575
Shannon Skipper <shannonskipper@gmail.com>
2012-12-15 15:29:04 -0800

    update formula to chruby version 0.2.5

--- Library/Formula/chruby.rb
+++ Library/Formula/chruby.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Chruby < Formula
 
   homepage 'https://github.com/postmodern/chruby#readme'
-  url 'https://github.com/postmodern/chruby/archive/v0.2.4.tar.gz'
-  sha1 'c89d4a4732d63515b1948a3d0dd0e3a2d4ed1057'
+  url 'https://github.com/postmodern/chruby/archive/v0.2.5.tar.gz'
+  sha1 '0b4819754f320ea32a66aaae91a4e426dcc264fc'
   head 'https://github.com/postmodern/chruby.git'
 
   def install
