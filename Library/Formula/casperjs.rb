85d5789cf59a979aceb12656a8d7bb090de60a8b
Ruud Kamphuis <ruudk@mphuis.com>
2012-12-15 12:25:12 +0100

    CasperJS 1.0.0-RC6
    Closes #16586.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/casperjs.rb
+++ Library/Formula/casperjs.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Casperjs < Formula
   homepage 'http://www.casperjs.org/'
-  url 'https://github.com/n1k0/casperjs/zipball/1.0.0-RC4'
-  sha1 '6211c5014d549e64e7aef751a63f15cc65c2bab6'
-  version '1.0.0-RC4'
+  url 'https://github.com/n1k0/casperjs/zipball/1.0.0-RC6'
+  sha1 '08059268523eac0ba8d70b387d5573e18e7ad414'
+  version '1.0.0-RC6'
 
   head 'https://github.com/n1k0/casperjs.git'
 
