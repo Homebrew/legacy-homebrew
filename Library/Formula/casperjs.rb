d4aa856ae0957f70d9dec036e13ae03d7727f2f6
Ruud Kamphuis <ruudk@mphuis.com>
2012-12-15 12:28:04 +0100

    Correct SHA

--- Library/Formula/casperjs.rb
+++ Library/Formula/casperjs.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Casperjs < Formula
   homepage 'http://www.casperjs.org/'
   url 'https://github.com/n1k0/casperjs/zipball/1.0.0-RC6'
-  sha1 '0995830a73e75a3d75f08d721f17b39a4cf4c4d4'
+  sha1 '08059268523eac0ba8d70b387d5573e18e7ad414'
   version '1.0.0-RC6'
 
   head 'https://github.com/n1k0/casperjs.git'
