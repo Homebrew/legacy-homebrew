79f64d1d4831f7d20cac00533f208691ba3d8a21
Samuel John <github@SamuelJohn.de>
2012-12-17 10:50:39 +0100

    sbcl 1.1.2
    - SbclBootstrapBinaries updated to the latest available 1.1.0
    
    Closes #16618.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/sbcl.rb
+++ Library/Formula/sbcl.rb
@@ -1,22 +1,22 @@
 require 'formula'
 
 class SbclBootstrapBinaries < Formula
-  url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.0.55/sbcl-1.0.55-x86-darwin-binary.tar.bz2'
-  sha1 '8ea71938c40a6dccfe2d43a86e9b115f4428a218'
-  version "1.0.55"
+  url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.1.0/sbcl-1.1.0-x86-64-darwin-binary.tar.bz2'
+  sha1 'ed2069e124027c43926728c48d604efbb4e33950'
+  version "1.1.0"
 end
 
 class Sbcl < Formula
   homepage 'http://www.sbcl.org/'
-  url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.1.1/sbcl-1.1.1-source.tar.bz2'
-  sha1 '87e8c37a2811e10d044074ec8bfc79918c66f2d8'
+  url 'http://downloads.sourceforge.net/project/sbcl/sbcl/1.1.2/sbcl-1.1.2-source.tar.bz2'
+  sha1 'b562c67d689abf8e0dffcd42d11617062ab52633'
 
   head 'git://sbcl.git.sourceforge.net/gitroot/sbcl/sbcl.git'
 
   bottle do
-    sha1 '8d568b7db2feecdb281805583f7ea3a7d104d6ac' => :mountainlion
-    sha1 'f6a50ffac61f5bb4ff398791b5a93f32f8983a25' => :lion
-    sha1 'e718dfaf3b7aa4b2be8e62d5274d59a0fedfc24b' => :snowleopard
+    # sha1 '' => :mountainlion
+    # sha1 '' => :lion
+    # sha1 '' => :snowleopard
   end
 
   fails_with :llvm do
