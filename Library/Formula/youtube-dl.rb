e5cc7cb096b8b1d1b10e8cb988811997c3a14337
Ian Hill <ianhill@buymeasoda.com>
2012-12-17 17:43:29 +1000

    youtube-dl: Update the download url
    Closes #16612.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class YoutubeDl < Formula
   homepage 'http://rg3.github.com/youtube-dl/'
-  url 'https://github.com/rg3/youtube-dl/archive/2012.12.11.tar.gz'
-  sha1 '317904b43096ff01d6c6bbb62676912cd546809f'
+  url 'https://github.com/downloads/rg3/youtube-dl/youtube-dl.2012.12.11.tar.gz'
+  sha1 '9087a85afb80b0c2e2d9f667b638b66a85686938'
 
   def install
     system "make", "youtube-dl", "PREFIX=#{prefix}"
