8ab052971a0543f8332eac5b75b1bc3d7dd229f9
ttjsmits <tjerk.smits@gmail.com>
2012-12-18 22:14:03 +0100

    Update Library/Formula/tn5250.rb
    Fixed "Segmentation fault: 11" issue on OS X

--- Library/Formula/tn5250.rb
+++ Library/Formula/tn5250.rb
@@ -5,8 +5,8 @@ class Tn5250 < Formula
   url 'http://sourceforge.net/projects/tn5250/files/tn5250/0.17.4/tn5250-0.17.4.tar.gz'
   sha1 '2c84f03f798fd5095009d6798d1e6c0b29e48a75'
 
-  # Fix Segmentation fault: 11 issue on OS X
   def patches
+    # Fix Segmentation fault: 11 issue on OS X
     DATA
   end
 
@@ -14,32 +14,37 @@ class Tn5250 < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
-    system "make install" 
-  end  
+    system "make install"
+  end
 end
 
 __END__
 
---- tn5250-0.17.4/curses/cursesterm.c 2008-11-21 01:12:20.000000000 -0700
-+++ cursesterm.c 2012-07-28 11:45:04.000000000 -0600
+diff --git a/curses/cursesterm.c b/curses/cursesterm.c
+index bf20f05..b9c394c 100644
+--- a/curses/cursesterm.c
++++ b/curses/cursesterm.c
 @@ -22,6 +22,9 @@
-#define _TN5250_TERMINAL_PRIVATE_DEFINED
-#include "tn5250-private.h"
-#include "cursesterm.h"
+ #define _TN5250_TERMINAL_PRIVATE_DEFINED
+ #include "tn5250-private.h"
+ #include "cursesterm.h"
 +#ifdef __APPLE__
 +#include <term.h>
 +#endif
-
-#ifdef USE_CURSES
-
-
---- tn5250-0.17.4/curses/tn5250.c 2008-11-21 01:12:21.000000000 -0700
-+++ tn5250.c 2012-07-28 11:37:10.000000000 -0600
+ 
+ #ifdef USE_CURSES
+ 
+diff --git a/curses/tn5250.c b/curses/tn5250.c
+index 30a1627..67392b1 100644
+--- a/curses/tn5250.c
++++ b/curses/tn5250.c
 @@ -19,6 +19,7 @@
-*/
-
-#include "tn5250-private.h"
+  */
+ 
+ #include "tn5250-private.h"
 +#include "cursesterm.h"
+ 
+ Tn5250Session *sess = NULL;
+ Tn5250Stream *stream = NULL;
 
-Tn5250Session *sess = NULL;
-Tn5250Stream *stream = NULL;
+
