ae99efe326490faacaa921a6cffe38c3582dae91
somekool <somekool@gmail.com>
2012-12-19 16:03:17 -0500

    devel package for log4cpp

--- Library/Formula/log4cpp.rb
+++ Library/Formula/log4cpp.rb
@@ -5,6 +5,11 @@ class Log4cpp < Formula
   url 'http://sourceforge.net/projects/log4cpp/files/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1.tar.gz'
   sha1 '6003105dd11b1fe6f0f88b5bc42c86cccd78d5ae'
 
+  devel do
+    url 'http://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1rc3.tar.gz'
+    sha1 '24dc2eda5b1cee7af58a42924f61cd44c88f066a'
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
