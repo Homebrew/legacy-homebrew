e55c94f6ff95993c2fc008e3689a60c8da9758ac
Dimitri Krassovski <dimitri@wix.com>
2012-12-19 12:59:03 +0200

    gcutil 1.5.0
    Closes #16650.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/gcutil.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Gcutil < Formula
+  homepage 'https://developers.google.com/compute/docs/gcutil/'
+  url 'https://google-compute-engine-tools.googlecode.com/files/gcutil-1.5.0.tar.gz'
+  sha1 '785290893ab722f2a1510f37982e80e52f9f043a'
+
+  def install
+    libexec.install 'gcutil', 'lib'
+    bin.install_symlink libexec/"gcutil"
+  end
+
+  def test
+    system "#{bin}/gcutil", "version"
+  end
+end
