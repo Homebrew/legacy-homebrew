32a2f2c46abca782d8a846678042f40a0f2eab14
somekool <somekool@gmail.com>
2012-12-20 10:03:31 -0500

    update soprano to 2.8.0

--- Library/Formula/soprano.rb
+++ Library/Formula/soprano.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Soprano < Formula
   homepage 'http://soprano.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/soprano/Soprano/2.7.5/soprano-2.7.5.tar.bz2'
-  md5 '9d881ce405354da4f7c2eeee386e2859'
+  url 'http://downloads.sourceforge.net/project/soprano/Soprano/2.8.0/soprano-2.8.0.tar.bz2'
+  md5 '273c3403aeb6d8a43e78a4887f50a385'
 
   depends_on 'cmake' => :build
   depends_on 'qt'
