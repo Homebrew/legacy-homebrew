829260c9ca4499dc2adaea297825d9d64990c522
somekool <somekool@gmail.com>
2012-12-20 10:11:13 -0500

    update calligra-2.5.4

--- Library/Formula/calligra.rb
+++ Library/Formula/calligra.rb
@@ -2,8 +2,8 @@ require 'base_kde_formula'
 
 class Calligra < BaseKdeFormula
   homepage 'http://www.calligra-suite.org/'
-  url 'http://download.kde.org/stable/calligra-latest/calligra-2.4.2.tar.bz2'
-  sha1 'd44a6b321b0b90122b86e074785546f4a2b0d066'
+  url 'http://download.kde.org/stable/calligra-latest/calligra-2.5.4.tar.bz2'
+  md5 '54f4677a9b00eec5951fa2386ba271a1'
   depends_on 'kdelibs'
   # ENV.x11 # if your formula requires any X11 headers
   # ENV.j1  # if your formula's build system can't parallelize
