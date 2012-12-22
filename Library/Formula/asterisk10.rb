5eed9b7153a1071a149b4a25894cb019832e93d9
Moshe Brevda <mbrevda@gmail.com>
2012-12-22 20:00:11 +0200

    Asterisk 10
    adding a homebrew formula for asterisk 10
    
    Corrected naming of formula class
    
    Cleaned up options; remove fails_on; added patch for 100% cpu usage

---
+++ Library/Formula/asterisk10.rb
@@ -0,0 +1,48 @@
+require 'formula'
+
+class Asterisk10 < Formula
+  homepage 'http://asterisk.org'
+  url 'http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-10.11.0.tar.gz'
+  sha1 'a678774bd6cb90fb4cefe115eb91a30228410288'
+  depends_on 'openssl'
+  depends_on 'libxml2'
+  depends_on 'sqlite'
+  env :std
+  option 'make-samples', 'Install the sample config files. NOTE: WILL OVERWRITE ANY EXISTING CONFIGS!'
+  
+  def install
+    ENV['CC'] = 'gcc'
+    ENV['CXX'] = 'g++'
+    ENV['CXXFLAGS'] = ""
+    ENV['CFLAGS'] = ''
+    ENV['LDFLAGS'] = ''
+    ENV['CPPFLAGS'] = ''
+    system "./configure", 
+            "--prefix=#{prefix}", 
+            "--without-netsnmp", 
+            "--with-ssl=/usr/local/opt/openssl/"
+    inreplace 'Makefile.rules', '-O6', '-Os'
+
+    system "make"
+    system "make install"
+    system "make samples" if build.include? 'make-samples'
+  end
+  def patches; DATA; end
+  def test
+    system asterisk
+  end
+end
+
+__END__
+##https://issues.asterisk.org/jira/browse/ASTERISK-17163
+--- ./pbx/pbx_spool.c	2012-12-23 10:49:01.000000000 +0200
++++ ./pbx/pbx_spool.c	2012-12-23 10:48:43.000000000 +0200
+@@ -53,6 +53,11 @@
+ #include "asterisk/module.h"
+ #include "asterisk/utils.h"
+ #include "asterisk/options.h"
++/* Force filesystem polling on OSX */
++#ifdef __APPLE__
++    #undef HAVE_KQUEUE
++    #undef HAVE_INOTIFY
++#endif
