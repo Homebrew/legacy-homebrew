6830867a38d519fdb14cfdf205109cc77cb94ed7
Evgeny Mandrikov <mandrikov@gmail.com>
2012-12-22 13:23:57 +0100

    Update Formula Sonar 3.4

--- Library/Formula/sonar.rb
+++ Library/Formula/sonar.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sonar < Formula
   homepage 'http://www.sonarsource.org'
-  url 'http://dist.sonar.codehaus.org/sonar-3.3.2.zip'
-  md5 'a76abb38c8eb1aaf277d685d496348f4'
+  url 'http://dist.sonar.codehaus.org/sonar-3.4.zip'
+  md5 'cda81914c8059dd4cbb06e7c8284d78d'
 
   def install
     # Delete native bin directories for other systems
