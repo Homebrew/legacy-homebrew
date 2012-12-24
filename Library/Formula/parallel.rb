a3b3d8de4255cd1094d346eec79195804c0b87c4
Jack Nagel <jacknagel@gmail.com>
2012-12-24 11:41:46 -0600

    parallel and moreutils conflict
    Closes #16719.

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -6,6 +6,9 @@ class Parallel < Formula
   mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20121222.tar.bz2'
   sha256 '0ce96ad4e36734baae7ce6c8d99d004810fbfdf5209d6f86d5b5fc9a92dc17f8'
 
+  conflicts_with 'moreutils',
+    :because => "both install a 'parallel' executable."
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
