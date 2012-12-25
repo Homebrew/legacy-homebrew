eba8b5e002a2d0488570516c07d41f3b2d09a070
Jake Kaufman <jakeman@foursquare.com>
2012-12-25 14:09:13 -0600

    Rust 0.5
    Also carry patch to fix showstopper repl bug
    
    Closes #16744.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rust.rb
+++ Library/Formula/rust.rb
@@ -2,12 +2,18 @@ require 'formula'
 
 class Rust < Formula
   homepage 'http://www.rust-lang.org/'
-  url 'http://dl.rust-lang.org/dist/rust-0.4.tar.gz'
-  sha256 '150685f07e4d605cadf9fba25b05e9cc1b009364dd744131cf4230d64981d093'
+  url 'http://dl.rust-lang.org/dist/rust-0.5.tar.gz'
+  sha256 'd326d22707f0562d669c11efbc33ae812ddbf76ab78f07087fc5beb095a8928a'
 
   fails_with :clang do
     build 318
     cause "cannot initialize a parameter of type 'volatile long long *' with an rvalue of type 'int *'"
+  end
+
+  # Fix repl showstopper bug; can be removed for 0.6.
+  def patches
+    [ "https://github.com/gifnksm/rust/commit/9bf87bbf66227c132283ae59720f919601de9a56.patch",
+    "https://github.com/gifnksm/rust/commit/3ee1d3ebb81de199fc630a86933ac18c0a869482.patch" ]
   end
 
   def install
