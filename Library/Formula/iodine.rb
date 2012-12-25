fc38d820ac63dc44d68bf88f94cdc4d1f2be425b
Filippo Valsorda <filippo.valsorda@gmail.com>
2012-12-25 03:14:56 +0100

    Apply Mountain Lion patch to iodine
    The inreplace was failing hard;
    this applies the e1e438497a83dbe6800212a0e5cb632907d1b3d9 patch by @zschoche for Mountain Lion compatibility.
    It has gone unheard upstream yarrick/iodine#1 and iodine does not work on 10.8 without.

--- Library/Formula/iodine.rb
+++ Library/Formula/iodine.rb
@@ -5,12 +5,13 @@ class Iodine < Formula
   homepage 'http://code.kryo.se/iodine/'
   sha1 '4fa9a248b8a84df8a727a5d749e669e58136edca'
 
-  def install
-    unless MacOS.version == :leopard
-      inreplace ["src/common.c", "src/dns.c", "src/iodine.c", "src/iodined.c"],
-        "arpa/nameser8_compat", "arpa/nameser_compat"
-    end
+  def patches
+    # Mountain Lion fix by @zschoche
+    # https://github.com/yarrick/iodine/pull/1
+    "https://github.com/zschoche/iodine/commit/e1e438497a83dbe6800212a0e5cb632907d1b3d9.patch"
+  end
 
+  def install
     system "make", "install", "prefix=#{prefix}"
   end
 end
