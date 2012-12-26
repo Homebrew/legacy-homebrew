b4870a0d7ac9956603545c401d97adefb91eba83
Matt Oakes <matt@ribot.co.uk>
2012-12-26 18:03:21 +0000

    libnfc: add patch for OS X
    Closes #16758.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libnfc.rb
+++ Library/Formula/libnfc.rb
@@ -5,9 +5,17 @@ class Libnfc < Formula
   url 'http://libnfc.googlecode.com/files/libnfc-1.6.0-rc1.tar.gz'
   sha1 'bbff76269120c3a531eb96b7ceb96fd36c0071a1'
 
+  option 'with-pn532_uart', 'Enable PN532 UART support'
+
+  depends_on 'pkg-config' => :build
   depends_on 'libusb-compat'
 
-  option 'with-pn532_uart', 'Enable PN532 UART support'
+  # Fixes the lack of MIN macro in sys/param.h on OS X which causes the formula not to compile
+  # Reported upstream:
+  # https://groups.google.com/forum/?fromgroups=#!topic/libnfc-devel/K0cwIdPuqJg
+  def patches
+    DATA
+  end
 
   def install
     args = %W[
@@ -25,3 +33,25 @@ class Libnfc < Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/libnfc/nfc-internal.h b/libnfc/nfc-internal.h
+index ec9e2fc..41797b2 100644
+--- a/libnfc/nfc-internal.h
++++ b/libnfc/nfc-internal.h
+@@ -33,6 +33,15 @@
+
+ #include "log.h"
+
++// Patch to compile on OS X
++// Tested on OS X Mountain Lion
++#ifndef MAX
++#define MAX(a,b) (((a) > (b)) ? (a) : (b))
++#endif
++#ifndef MIN
++#define MIN(a,b) (((a) < (b)) ? (a) : (b))
++#endif
++
+ /**
+  * @macro HAL
+  * @brief Execute corresponding driver function if exists.
