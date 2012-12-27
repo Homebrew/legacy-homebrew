4d2319ed7b43a05ad9db720cdad20de9ab065867
Erik Michaels-Ober <sferik@gmail.com>
2012-12-27 08:31:34 -0800

    ruby-build 20121227
    * Added a definition for Ruby 1.9.3-p362
    * Added a definition for Ruby 1.8.7-p371
    * Moved the default ruby-build mirror from GitHub Downloads to Amazon
      CloudFront.
    
    Closes #16765.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyBuild < Formula
   homepage 'https://github.com/sstephenson/ruby-build'
-  url 'https://github.com/sstephenson/ruby-build/tarball/v20121204'
-  sha1 '1c29e308e46a180bbd54f380ee50934aa1f78b72'
+  url 'https://github.com/sstephenson/ruby-build/tarball/v20121227'
+  sha1 'db33874fdb2f260fcbcc2202364e5d3cfc261fe6'
 
   head 'https://github.com/sstephenson/ruby-build.git'
 
