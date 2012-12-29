dab30622c94e6054ba182e9a576633218caab3d7
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2012-12-29 15:31:54 -0300

    JQ version 1.2 upgrade

--- Library/Formula/jq.rb
+++ Library/Formula/jq.rb
@@ -2,9 +2,12 @@ require 'formula'
 
 class Jq < Formula
   homepage 'http://stedolan.github.com/jq/'
-  url 'https://github.com/stedolan/jq/archive/jq-1.1.tar.gz'
-  sha1 '555c9b2d9852376092be556bc0649154d5b5c2bf'
+  url 'https://github.com/stedolan/jq/tarball/a05904b2267d53f9a305d010facd6f307a9d5373'
+  version 1.2
+  sha1 '816c4926966b702bf5bd57789dbee89a16532b82'
   head 'https://github.com/stedolan/jq.git'
+
+  depends_on 'bison'
 
   def install
     system "make"
