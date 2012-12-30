2025fa00d6197eb12d431b9eef5ea7cb023ca7f7
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2012-12-30 09:31:53 -0300

    MySQL 5.5.29 withou bottle

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -2,15 +2,9 @@ require 'formula'
 
 class Mysql < Formula
   homepage 'http://dev.mysql.com/doc/refman/5.5/en/'
-  url 'http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.28.tar.gz/from/http://cdn.mysql.com/'
-  version '5.5.28'
-  sha1 '7b029e61db68866eeea0bec40d47fcdced30dd36'
-
-  bottle do
-    sha1 'a4389fb4c6e77d43b166e29ce1ebf9d9e193bb11' => :mountainlion
-    sha1 '9f0da89543cd96d6352ac6ede0cb2dfd156ea7c1' => :lion
-    sha1 '440103cef7733865f8ceed83a86242648b357ec2' => :snowleopard
-  end
+  url 'http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.29.tar.gz/from/http://cdn.mysql.com/'
+  version '5.5.29'
+  sha1 '40e26b193b6ece86ce97896c0c9c524d479e37be'
 
   depends_on 'cmake' => :build
   depends_on 'pidof' unless MacOS.version >= :mountain_lion
