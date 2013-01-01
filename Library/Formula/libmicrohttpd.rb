6a1e76799a01e5fcb39951e75c46a4e4ce6cca1e
Brett Koonce <koonce@gmail.com>
2012-12-31 22:50:39 -0600

    libmicrohttpd 0.9.24
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libmicrohttpd.rb
+++ Library/Formula/libmicrohttpd.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libmicrohttpd < Formula
   homepage 'http://www.gnu.org/software/libmicrohttpd/'
-  url 'http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.22.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.22.tar.gz'
-  sha1 '95d2934448ea2f6beff985f312b4502ea1563d2a'
+  url 'http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.24.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.24.tar.gz'
+  sha1 '9fe31e1ecf81bf9eae4819d9e93e2e76d483f02b'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
