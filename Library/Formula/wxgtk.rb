497be962236a0cb366a7c04a3664d9e413df6978
Joan Valduvieco Llopart <jvalduvieco@gmail.com>
2013-01-02 00:18:03 +0100

    Add --with-wxgtk to use wxGTK and be able to use erlang's graphic tools like observer in Mountain Lion. Erlang depends on wxWidgets 2.8 API as erlang graphic tools use it. In Mountain Lion WxWidgets 2.8 do not compile because Carbon has been dropped from XCode 4.4. To solve this issue WxGTK can be used as it has the same API as WxWidgets 2.8 and use X11 instead of Carbon.

---
+++ Library/Formula/wxgtk.rb
@@ -0,0 +1,38 @@
+require 'formula'
+
+class Wxgtk < Formula
+  homepage 'http://www.wxwidgets.org'
+  url "http://downloads.sourceforge.net/project/wxwindows/2.8.12/wxGTK-2.8.12.tar.gz"
+  sha1 '56cc7f6622dd6f2fecd43fc210dc1c6cb155b37f'
+
+  keg_only 'This formula conflicts  with wxwidgets 2.9 and by now is only used by erlang'
+
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on 'gtk+'
+
+  def install
+
+    args=[
+      "--with-libpng",
+      "--with-opengl",
+      "--with-libjpeg",
+      "--with-libtiff",
+      "--with-freetype",
+      "--with-zlib",
+      "--enable-unicode",
+      "--disable-debug",
+      "--prefix=#{prefix}",
+      "--with-gtk"
+    ]
+    ENV.append 'LDFLAGS', '-lX11 -lGL -lGLU'
+
+    system "./configure", *args
+
+    system "make install"
+
+    cd "contrib" do
+      system "make"
+      system "make install"
+    end
+  end
+end
