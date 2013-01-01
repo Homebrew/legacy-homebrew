e8ad1ddd855a34ff1704064d5b99188d6ccab8a6
Brett Koonce <koonce@gmail.com>
2013-01-01 08:59:14 -0600

    pngquant 1.8.1

--- Library/Formula/pngquant.rb
+++ Library/Formula/pngquant.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pngquant < Formula
   homepage 'http://pngquant.org/'
-  url 'https://github.com/pornel/improved-pngquant/tarball/1.7.3'
-  sha1 'f6c22528428ee2806fa611aa536443306e45542f'
+  url 'https://github.com/pornel/improved-pngquant/tarball/1.8.1'
+  sha1 'c29e14e9d554b0906259bb2f51954661cf336084'
 
   head 'https://github.com/pornel/improved-pngquant.git'
 
