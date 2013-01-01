5a3632b0f9e1e87caf3c8d2e20d0db977d9eea8c
Misty De Meo <mistydemeo@gmail.com>
2013-01-01 17:38:25 -0400

    cdrtools: use smake instead of make
    cdrtools won't build its binaries using plain make.

--- Library/Formula/cdrtools.rb
+++ Library/Formula/cdrtools.rb
@@ -5,7 +5,9 @@ class Cdrtools < Formula
   url 'ftp://ftp.berlios.de/pub/cdrecord/cdrtools-3.00.tar.gz'
   sha1 '2cd7d1725e0da2267b7a033cc744295d6e2bc6b9'
 
+  depends_on 'smake' => :build
+
   def install
-    system "make", "INS_BASE=#{prefix}", "INS_RBASE=#{prefix}", "install"
+    system "smake", "INS_BASE=#{prefix}", "INS_RBASE=#{prefix}", "install"
   end
 end
