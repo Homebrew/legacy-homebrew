e1f6ff071ec7d398963618253fc0e1bb192992cd
Christian Hofstaedtler <christian@hofstaedtler.name>
2013-01-03 00:05:56 +0100

    freerdp: fix HEAD builds
    Closes #16851.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/freerdp.rb
+++ Library/Formula/freerdp.rb
@@ -17,9 +17,20 @@ class Freerdp < Formula
       'https://github.com/FreeRDP/FreeRDP/commit/1d3289.patch',
       'https://github.com/FreeRDP/FreeRDP/commit/e32f9e.patch'
     ]
-  end
+  end unless build.head?
 
   def install
+    if build.head? then
+      # workaround for out-of-git clone tree build
+      inreplace 'cmake/GetGitRevisionDescription.cmake',
+        'set(GIT_PARENT_DIR "${CMAKE_SOURCE_DIR}")',
+        "set(GIT_PARENT_DIR \"#{cached_download}\")"
+
+      inreplace 'cmake/GetGitRevisionDescription.cmake',
+        'WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"',
+        "WORKING_DIRECTORY \"#{cached_download}\""
+    end
+
     system "cmake", ".", *std_cmake_args
     system "make install"
   end
