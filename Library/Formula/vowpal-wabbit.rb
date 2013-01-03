b8ac4e3e33347b95c030f9b1c70cf5768cf880bd
Adam Vandenberg <flangy@gmail.com>
2013-01-03 12:45:52 -0800

    vowpal wabbit 7.1

--- Library/Formula/vowpal-wabbit.rb
+++ Library/Formula/vowpal-wabbit.rb
@@ -2,23 +2,27 @@ require 'formula'
 
 class VowpalWabbit < Formula
   homepage 'https://github.com/JohnLangford/vowpal_wabbit'
-  url 'https://github.com/JohnLangford/vowpal_wabbit/tarball/v7.0'
-  sha1 '1960f9b8423ce13d6c0f29e3c23feeb2d52f2918'
+  url 'https://github.com/JohnLangford/vowpal_wabbit/tarball/v7.1'
+  sha1 'a2a8241654d79fd2c0b14a3907d98f7e87705ba8'
 
   head 'git://github.com/JohnLangford/vowpal_wabbit.git'
 
-  depends_on :libtool
-  depends_on :automake
+  if build.head?
+    depends_on :libtool
+    depends_on :automake
+  end
+
   depends_on 'boost' => :build
 
   def install
-    # 7.0 tarball doesn't include autogen files
-    inreplace 'autogen.sh' do |s|
-      s.gsub! 'libtoolize', 'glibtoolize'
-      s.gsub! '/usr/share/aclocal', "#{HOMEBREW_PREFIX}/share/aclocal"
+    if build.head?
+      inreplace 'autogen.sh' do |s|
+        s.gsub! 'libtoolize', 'glibtoolize'
+        s.gsub! '/usr/share/aclocal', "#{HOMEBREW_PREFIX}/share/aclocal"
+      end
+      system "./autogen.sh"
     end
 
-    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
