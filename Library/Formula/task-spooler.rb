f3824d7df474c27fe505912034d6214740f3dfc1
Jack Nagel <jacknagel@gmail.com>
2013-01-02 23:36:18 -0600

    moreutils and task-spooler conflict
    Fixes #16850.

--- Library/Formula/task-spooler.rb
+++ Library/Formula/task-spooler.rb
@@ -5,6 +5,9 @@ class TaskSpooler < Formula
   url 'http://vicerveza.homeunix.net/~viric/soft/ts/ts-0.7.3.tar.gz'
   sha1 '33b9321d4f48a2c2685a8240db4e00c0e69ef9bc'
 
+  conflicts_with 'moreutils',
+    :because => "both install a 'ts' executable."
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
