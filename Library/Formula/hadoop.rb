85701ffc753e57b53353fbd7c76c39af07662148
Guido Serra aka Zeph <guido.serra@rocket-internet.de>
2013-01-03 15:36:49 +0100

    getting in line with mattbornski/homebrew/commit/d217a4e69ea21b0bc4cd81252bcd55ed9f26026e

--- Library/Formula/hadoop.rb
+++ Library/Formula/hadoop.rb
@@ -4,21 +4,14 @@ class Hadoop < Formula
   homepage 'http://hadoop.apache.org/common/'
   url 'http://www.apache.org/dyn/closer.cgi?path=hadoop/core/hadoop-2.0.2-alpha/hadoop-2.0.2-alpha.tar.gz'
   sha1 '44206b2d4b657a8efa47dbe78725c58233e17a9d'
+  version '2.0.2'
+
+  depends_on 'protobuf'
 
   def install
-    rm_f Dir["bin/*.bat"]
-    libexec.install %w[etc bin sbin lib libexec share]
-    libexec.install Dir['*.jar']
+    libexec.install %w[bin lib libexec share]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
     # But don't make rcc visible, it conflicts with Qt
     (bin/'rcc').unlink
-  end
-
-  def caveats; <<-EOS.undent
-    In Hadoop's config file:
-      #{libexec}/etc/hadoop/hadoop-env.sh
-    $JAVA_HOME has been set to be the output of:
-      /usr/libexec/java_home
-    EOS
   end
 end
