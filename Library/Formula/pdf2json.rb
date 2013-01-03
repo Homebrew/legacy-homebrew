b306f1c2bdc3801c43fb7fd13069ec38b6c16e41
Rui Pereira <rui.pereira@gmail.com>
2013-01-03 15:39:24 +0100

    pdf2json: remove unused dependency
    Only install the pdf2json binary.
    
    Closes #16871.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pdf2json.rb
+++ Library/Formula/pdf2json.rb
@@ -5,14 +5,12 @@ class Pdf2json < Formula
   url 'http://pdf2json.googlecode.com/files/pdf2json-0.61.tar.gz'
   sha1 'd7bfcf89ab82741ed014d3499fe7505d7168686d'
 
-  depends_on 'xpdf'
-
   def install
     system "./configure", "--prefix=#{prefix}"
     # Fix manpage install location. See:
     # http://code.google.com/p/pdf2json/issues/detail?id=2
     inreplace "Makefile", "/man/", "/share/man/"
     system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}"
-    system "make install"
+    bin.install 'src/pdf2json'
   end
 end
