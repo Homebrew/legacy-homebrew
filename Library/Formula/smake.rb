c547881687a063775ef402d91fd56e3609b9217b
Adam Vandenberg <flangy@gmail.com>
2013-01-04 13:39:51 -0800

    smake: style nit

--- Library/Formula/smake.rb
+++ Library/Formula/smake.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Smake < Formula
-  url 'ftp://ftp.berlios.de/pub/smake/smake-1.2.2.tar.bz2'
   homepage 'http://cdrecord.berlios.de/private/smake.html'
+  url 'ftp://ftp.berlios.de/pub/smake/smake-1.2.2.tar.bz2'
   sha1 'abd5a99a6fe588afa31246f3d686c686c75bb953'
 
   # smake silently fails to build its binaries in superenv
