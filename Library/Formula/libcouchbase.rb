ce3771d07121d8bfb06cb17601017d1216e5e19f
Sergey Avseyev <sergey.avseyev@gmail.com>
2013-01-04 23:15:29 +0300

    libcouchbase 2.0.2
    Closes #16895.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libcouchbase < Formula
   homepage 'http://couchbase.com/develop/c/current'
-  url 'http://packages.couchbase.com/clients/c/libcouchbase-2.0.1.tar.gz'
-  sha1 '51df1b3099a42cdcb8bf6b46d802791828d7e1d7'
+  url 'http://packages.couchbase.com/clients/c/libcouchbase-2.0.2.tar.gz'
+  sha1 'e1f6149d2fc05e3e8be568469baed5c7234c48f3'
 
   option 'with-libev-plugin', 'Build libev IO plugin (will pull libev dependency)'
   option 'without-libevent-plugin', 'Do not build libevent plugin (will remove libevent dependency)'
