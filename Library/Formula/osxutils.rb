93e68885b890e1e6da8c6c01e45f34126a0faaa7
Adam Vandenberg <flangy@gmail.com>
2013-01-05 10:57:35 -0800

    osxutils: fix audit

--- Library/Formula/osxutils.rb
+++ Library/Formula/osxutils.rb
@@ -5,7 +5,7 @@ class Osxutils < Formula
   url 'https://github.com/vasi/osxutils/archive/v1.8.tar.gz'
   sha1 'c3d20da36ecfae3abff07f482a572b3680a12b6d'
 
-  head 'git://github.com/vasi/osxutils.git'
+  head 'https://github.com/vasi/osxutils.git'
 
   def install
     system 'make'
