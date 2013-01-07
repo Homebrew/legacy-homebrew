97db918edbdecc98bf8240f55c082fa9312fad27
Adam Vandenberg <flangy@gmail.com>
2013-01-07 14:09:12 -0800

    Revert "libmagic 5.12"
    This reverts commit e2e69aff7507c2c56b54f36aa4ec1faea47e4061.
    
    This fails to compile on Snow Leopard. Reported upstream:
    http://bugs.gw.com/view.php?id=230
    
    Closes #16943.

--- Library/Formula/libmagic.rb
+++ Library/Formula/libmagic.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libmagic < Formula
   homepage 'http://www.darwinsys.com/file/'
-  url 'ftp://ftp.astron.com/pub/file/file-5.12.tar.gz'
-  mirror 'http://fossies.org/unix/misc/file-5.12.tar.gz'
-  sha1 '782db8a2b37ff8ceada9d19c416eaf6c5b8297d4'
+  url 'ftp://ftp.astron.com/pub/file/file-5.11.tar.gz'
+  mirror 'http://fossies.org/unix/misc/file-5.11.tar.gz'
+  sha1 'df8ffe8759ec8cd85a98dc98e858563ea2555f64'
 
   def install
     system "./configure", "--disable-dependency-tracking",
