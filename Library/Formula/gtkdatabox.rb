0ebabb66ba004eadd679133f04dabe7bdbf19d13
Adam Vandenberg <flangy@gmail.com>
2013-01-07 21:01:48 -0800

    gtkdatabox 0.9.2.0

--- Library/Formula/gtkdatabox.rb
+++ Library/Formula/gtkdatabox.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gtkdatabox < Formula
   homepage 'http://sourceforge.net/projects/gtkdatabox/'
-  url 'http://downloads.sourceforge.net/project/gtkdatabox/gtkdatabox/0.9.1.3/gtkdatabox-0.9.1.3.tar.gz'
-  sha1 '02380eeb755b885856253a1a71dd3e8109c201df'
+  url 'http://downloads.sourceforge.net/project/gtkdatabox/gtkdatabox/0.9.2.0/gtkdatabox-0.9.2.0.tar.gz'
+  sha1 'a2cb25c1aa1b817283a3da9598d6d1d6e702d58f'
 
   depends_on 'pkg-config' => :build
   depends_on 'gtk+'
