70edc171c12a26dd9f1234199edc6e1d6f099482
Kyle Sexton <ks@mocker.org>
2013-01-09 10:17:15 -0600

    Initial libntlm package, useful for getting mstmp to talk to Exchange with ntlm authentication

---
+++ Library/Formula/libntlm.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Libntlm < Formula
+  homepage 'http://www.nongnu.org/libntlm/'
+  url 'http://www.nongnu.org/libntlm/releases/libntlm-1.3.tar.gz'
+  version '1.3'
+  sha1 '5dd798d5fb9a75656225052aa88ceb9befbbd4a0'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "ls #{HOMEBREW_PREFIX}/lib/libntlm.a"
+  end
+end
