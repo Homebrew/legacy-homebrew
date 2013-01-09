c35339871d6b07b944dcfe047ca0558fc8f60dce
Luke Carrier <hey@lukecarrier.me>
2013-01-09 13:57:54 +0000

    GTK Theme Switch: 2.1.0

---
+++ Library/Formula/gtk-theme-switch.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class GtkThemeSwitch < Formula
+  homepage 'http://freecode.com/projects/gtkthemeswitch'
+  url 'http://ftp.debian.org/debian/pool/main/g/gtk-theme-switch/gtk-theme-switch_2.1.0.orig.tar.gz'
+  sha1 'f42cd43df5ed671e9eb9c180de944f77e1bdc879'
+
+  depends_on 'intltool'
+  depends_on 'gettext'
+  depends_on 'gtk+'
+  depends_on 'hicolor-icon-theme'
+
+  def install
+    system 'make', "PREFIX=#{prefix}", 'install'
+  end
+
+  def test
+    system 'gtk-theme-switch2'
+  end
+end
