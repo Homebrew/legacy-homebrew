0a856704a0c2674583e82169eb498d4845f36f0f
Adam Vandenberg <flangy@gmail.com>
2013-01-09 16:24:08 -0800

    byobu 5.24
    Closes #16968.

--- Library/Formula/byobu.rb
+++ Library/Formula/byobu.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Byobu < Formula
   homepage 'http://launchpad.net/byobu'
-  url 'https://launchpad.net/byobu/trunk/5.21/+download/byobu_5.21.orig.tar.gz'
-  sha1 '81ed721b643addf84c7aba10f173c27dc69391bd'
+  url 'https://launchpad.net/byobu/trunk/5.24/+download/byobu_5.24.orig.tar.gz'
+  sha1 'aa149862d70d8ad1b7b0507193669f24dc15ba99'
 
   depends_on 'coreutils'
   depends_on 'gnu-sed' # fails with BSD sed
