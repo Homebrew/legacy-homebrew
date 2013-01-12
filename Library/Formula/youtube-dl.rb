40280c697979364fa22937c607803bf3fdebce0a
Jaime Marquínez Ferrándiz <jaimemf93@gmail.com>
2013-01-12 21:01:24 +0100

    youtube-dl 2013.01.12
    Closes #17033.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class YoutubeDl < Formula
   homepage 'http://rg3.github.com/youtube-dl/'
-  url 'http://youtube-dl.org/downloads/2013.01.11/youtube-dl-2013.01.11.tar.gz'
-  sha1 '6a670631bea4bccb5242e5a7bcba38758dec60c9'
+  url 'http://youtube-dl.org/downloads/2013.01.12/youtube-dl-2013.01.12.tar.gz'
+  sha1 '52e3337bebe8aa9aca78a64aa254ca4d37830310'
 
   def install
     system "make", "youtube-dl", "PREFIX=#{prefix}"
