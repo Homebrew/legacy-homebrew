915066e466f1eaf23eb4827377df19a1a0dd4fff
Luke Carrier <hey@lukecarrier.me>
2013-01-12 23:25:10 +0000

    GTK Theme Switch; depend on pkg-config, clean up

--- Library/Formula/gtk-theme-switch.rb
+++ Library/Formula/gtk-theme-switch.rb
@@ -5,10 +5,11 @@ class GtkThemeSwitch < Formula
   url 'http://ftp.debian.org/debian/pool/main/g/gtk-theme-switch/gtk-theme-switch_2.1.0.orig.tar.gz'
   sha1 'f42cd43df5ed671e9eb9c180de944f77e1bdc879'
 
-  depends_on 'intltool'
   depends_on 'gettext'
   depends_on 'gtk+'
   depends_on 'hicolor-icon-theme'
+  depends_on 'intltool'
+  depends_on 'pkg-config'
 
   def install
     system 'make', "PREFIX=#{prefix}", 'install'
