82c6fd433741f15c291e7ecbed84af1e39ec9b68
Daniel Widerin <daniel@widerin.net>
2013-01-14 14:55:34 +0100

    Update Library/Formula/python.rb
    Allow --enable-unicode=ucs4 compile option

--- Library/Formula/python.rb
+++ Library/Formula/python.rb
@@ -41,6 +41,7 @@ class Python < Formula
   option 'quicktest', 'Run `make quicktest` after the build'
   option 'with-brewed-openssl', "Use Homebrew's openSSL instead of the one from OS X"
   option 'with-poll', 'Enable select.poll, which is not fully implemented on OS X (http://bugs.python.org/issue5154)'
+  option 'with-ucs4', 'Enable unicode ucs4 support for python. (http://docs.python.org/2/c-api/unicode.html)'
 
   # --with-dtrace relies on CLT as dtrace hard-codes paths to /usr
   option 'with-dtrace', 'Experimental DTrace support (http://bugs.python.org/issue13405)' if MacOS::CLT.installed?
@@ -87,6 +88,7 @@ class Python < Formula
 
     args << '--without-gcc' if ENV.compiler == :clang
     args << '--with-dtrace' if build.include? 'with-dtrace'
+    args << '--enable-unicode=ucs4' if build.include? 'with-ucs4'
 
     distutils_fix_superenv(args)
     distutils_fix_stdenv
