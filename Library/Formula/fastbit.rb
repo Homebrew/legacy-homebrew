b25be1d5a83d91f39f8657d8958bb1ac9014ed6a
Don Spaulding <donspauldingii@gmail.com>
2013-01-15 14:55:23 -0600

    Update to 1.3.5

--- Library/Formula/fastbit.rb
+++ Library/Formula/fastbit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fastbit < Formula
   homepage 'https://sdm.lbl.gov/fastbit/'
-  url 'https://codeforge.lbl.gov/frs/download.php/400/fastbit-ibis1.3.4.tar.gz'
-  sha1 'ba1f55942a684f49a75631116492968db17e11d3'
+  url 'https://codeforge.lbl.gov/frs/download.php/401/fastbit-ibis1.3.5.tar.gz'
+  sha1 '3a01de4474b88cd042cafea41d73f3ecf87be747'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
