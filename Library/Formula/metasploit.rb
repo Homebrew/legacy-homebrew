418ea296e7bc03ccce7523fa6e9f42b9ecfedf6e
jvdp <jvdplas@gmail.com>
2013-01-16 23:01:06 +0100

    metasploit 4.5.0 Update 2013010902

--- Library/Formula/metasploit.rb
+++ Library/Formula/metasploit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Metasploit < Formula
   homepage 'http://www.metasploit.com/framework/'
-  url 'http://downloads.metasploit.com/data/releases/framework-4.5.0.tar.bz2'
-  sha1 '13c3e4ef5265ecb5b6303d39ca1347982b782886'
+  url 'https://github.com/rapid7/metasploit-framework.git', :using => :git, :tag => '2013010902'
+  version '4.5.0-2013010902'
 
   head "https://github.com/rapid7/metasploit-framework.git", :using => :git
 
