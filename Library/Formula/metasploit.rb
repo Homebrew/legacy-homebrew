e2bcdf673c445f82946ba81dbd82b8fe25f68422
jvdp <jvdplas@gmail.com>
2013-01-16 22:47:58 +0100

    metasploit: new head url and remove caveat (no longer accurate)

--- Library/Formula/metasploit.rb
+++ Library/Formula/metasploit.rb
@@ -5,22 +5,10 @@ class Metasploit < Formula
   url 'http://downloads.metasploit.com/data/releases/framework-4.5.0.tar.bz2'
   sha1 '13c3e4ef5265ecb5b6303d39ca1347982b782886'
 
-  head "https://www.metasploit.com/svn/framework3/trunk/", :using => :svn
-
-  # Metasploit's tarball comes with a full .svn checkout.
-  # Don't clean these folders, so users can "svn up" to update
-  # metasploit in-place, which apparently is standard for this project.
-  skip_clean :all
+  head "https://github.com/rapid7/metasploit-framework.git", :using => :git
 
   def install
     libexec.install Dir['.svn','armitage','HACKING',"msf*",'data','documentation','external','lib','modules','plugins','scripts','test','tools']
     bin.install_symlink Dir["#{libexec}/msf*","#{libexec}/armitage"]
-  end
-
-  def caveats; <<-EOS.undent
-    Metasploit can be updated in-place by doing:
-      cd `brew --prefix metasploit`/libexec/
-      svn up
-    EOS
   end
 end
