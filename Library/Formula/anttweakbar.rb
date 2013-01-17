b5c7d15ef197c1a4b009d6c534090aea812d062d
Brett Koonce <koonce@gmail.com>
2013-01-17 10:03:39 -0600

    AntTweakBar 1.15
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/anttweakbar.rb
+++ Library/Formula/anttweakbar.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Anttweakbar < Formula
   homepage 'http://www.antisphere.com/Wiki/tools:anttweakbar'
-  url 'http://www.antisphere.com/Tools/AntTweakBar/AntTweakBar_114.zip'
-  version '1.14'
-  sha1 'b6405082efb6d6f26b3856a1b12c4e520055390e'
+  url 'http://www.antisphere.com/Tools/AntTweakBar/AntTweakBar_115.zip'
+  version '1.15'
+  sha1 'fcc5069c00001e4954e2c465042f591df167c40b'
 
   def install
     cd 'src' do
