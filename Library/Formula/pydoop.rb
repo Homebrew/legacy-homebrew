39fdc3d3445e13787d7d0e1a4b44ab86b084e023
simleo <simleo@crs4.it>
2013-01-17 16:48:24 +0100

    fixed pydoop formula according to adamv's comments

--- Library/Formula/pydoop.rb
+++ Library/Formula/pydoop.rb
@@ -3,7 +3,6 @@ require 'formula'
 class JdkInstalled < Requirement
   def message; <<-EOS.undent
     A JDK is required.
-
     You can get the official Oracle installers from:
     http://www.oracle.com/technetwork/java/javase/downloads/index.html
     EOS
@@ -36,24 +35,55 @@ class JavaHome < Requirement
 end
 
 class Pydoop < Formula
-  homepage ''
+  homepage 'http://http://pydoop.sourceforge.net/'
   url 'http://sourceforge.net/projects/pydoop/files/Pydoop-0.8/pydoop-0.8.1.tar.gz'
-  version '0.8.1'
   sha1 '9ef27551b1a16ef0454b903306cf05408ca91e7c'
 
   depends_on JdkInstalled.new
   depends_on JavaHome.new
   depends_on 'boost'
-  depends_on 'hadoop'
+  unless(ENV["HADOOP_HOME"])
+    depends_on 'hadoop'
+  end
 
   def install
-    ENV.append 'HADOOP_HOME', '/usr/local/Cellar/hadoop/1.1.1/libexec'
+    unless(ENV["HADOOP_HOME"])
+      ohai "HADOOP_HOME is unset. Using brew version"
+      ENV.append 'HADOOP_HOME', Formula.factory('hadoop').libexec
+    end
     ENV.append 'BOOST_PYTHON', 'boost_python-mt'
     system 'python', 'setup.py', 'build'
-    system 'python', 'setup.py', 'install', '--user'
+
+    # In order to install into the Cellar, the dir must exist and be in the
+    # PYTHONPATH.
+    temp_site_packages = lib/which_python/'site-packages'
+    mkdir_p temp_site_packages
+    ENV['PYTHONPATH'] = temp_site_packages
+    args = [
+      "--no-user-cfg",
+      "--verbose",
+      "install",
+      "--force",
+      "--install-scripts=#{share}/python",
+      "--install-lib=#{temp_site_packages}",
+      "--install-data=#{share}",
+      "--install-headers=#{include}",
+      "--record=installed-files.txt"
+    ]
+    system "python", "-s", "setup.py", *args
+
+    prefix.install %w[test examples]
   end
 
-  def test
-    system "false"
+  def which_python
+    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
   end
+
+  def caveats; <<-EOS.undent
+    This formula installs pydoop against whatever Python is first in your path.
+    This Python needs to have either setuptools or distribute installed or
+    the build will fail.
+    EOS
+  end
+
 end
