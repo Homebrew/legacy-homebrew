cbb32f3fcfc6b7cce46f77d82b78011fab85b905
Ian White <ian@impressiver.com>
2013-01-16 18:51:10 -0800

    Updated gist url and sha1

--- Library/Formula/django-completion.rb
+++ Library/Formula/django-completion.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class DjangoCompletion < GithubGistFormula
-  url 'https://gist.github.com/raw/4552835/e810c4075bbcdd4c4c0dd727901948fcf5f308b6/django_bash_completion'
+  url 'https://gist.github.com/raw/4552835/47a0d6983b08b66081695d6a8683be280b05a61f/django_bash_completion'
   homepage 'https://gist.github.com/4552835'
-  sha1 'd1aa4da325b24fd6cecbf3cd8e5e2d983932dd1f'
+  sha1 'c1b0eab397bfd09bbb197298fd0a7808d983b750'
 
   def install
     (prefix+'etc/bash_completion.d').install Dir['*']
