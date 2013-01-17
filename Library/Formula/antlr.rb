81d77b6dce9dc1e8f949964b1e8614c172bdf28f
Brett Koonce <koonce@gmail.com>
2013-01-17 10:03:30 -0600

    antlr 3.5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/antlr.rb
+++ Library/Formula/antlr.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Antlr < Formula
   homepage 'http://www.antlr.org/'
-  url "http://www.antlr.org/download/antlr-3.4-complete.jar"
-  sha1 '5cab59d859caa6598e28131d30dd2e89806db57f'
+  url 'http://www.antlr.org/download/antlr-3.5-complete.jar'
+  sha1 'f552792c4cdceb8c02fa1354e773349763267ab9'
 
   def install
-    prefix.install "antlr-3.4-complete.jar"
-    bin.write_jar_script prefix/"antlr-3.4-complete.jar", "antlr-3.4"
+    prefix.install "antlr-3.5-complete.jar"
+    bin.write_jar_script prefix/"antlr-3.5-complete.jar", "antlr-3.4"
   end
 end
