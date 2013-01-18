97e66f35345e9ca8e6deec492db5a28061320dac
richo <richo@psych0tik.net>
2013-01-18 14:47:20 +1100

    chicken 4.8.0.1
    Closes #17157.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/chicken.rb
+++ Library/Formula/chicken.rb
@@ -1,14 +1,16 @@
 require 'formula'
 
 class Chicken < Formula
-  url 'http://code.call-cc.org/releases/4.8.0/chicken-4.8.0.tar.gz'
-  sha1 '5068929f02d8a4fcb8fde13e4ddefb0bcb7142a6'
   homepage 'http://www.call-cc.org/'
+  url 'http://code.call-cc.org/releases/4.8.0/chicken-4.8.0.1.tar.gz'
+  sha1 '3db001f2533af9b50399384f50a02ed67f74c4d7'
+
   head 'git://code.call-cc.org/chicken-core'
 
   def install
     ENV.deparallelize
-    args = ["PREFIX=#{prefix}", "PLATFORM=macosx", "C_COMPILER=#{ENV.cc}"] # Chicken uses a non-standard var. for this
+    # Chicken uses a non-standard var. for this
+    args = ["PREFIX=#{prefix}", "PLATFORM=macosx", "C_COMPILER=#{ENV.cc}"]
     args << "ARCH=x86-64" if MacOS.prefer_64_bit?
     system "make", *args
     system "make", "install", *args
