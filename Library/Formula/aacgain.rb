72e5901dfdc71b8b4b586051ac765c75f91f92c2
Adam Vandenberg <flangy@gmail.com>
2013-01-20 09:31:19 -0800

    Revert "aacgain 1.9"
    This reverts commit 6f7ecf4077efe2826751e137973da8bbcccaf890.
    
    Closes #17209.

--- Library/Formula/aacgain.rb
+++ Library/Formula/aacgain.rb
@@ -2,7 +2,11 @@ require 'formula'
 
 class Aacgain < Formula
   homepage 'http://aacgain.altosdesign.com/'
-  url 'http://aacgain.altosdesign.com/alvarez/aacgain-1.9.tar.bz2'
+  # This server will autocorrect a 1.9 url back to this 1.8 tarball.
+  # The 1.9 version mentioned on the website is pre-release, so make
+  # sure 1.9 is actually out before updating.
+  # See: https://github.com/mxcl/homebrew/issues/16838
+  url 'http://aacgain.altosdesign.com/alvarez/aacgain-1.8.tar.bz2'
   sha1 '331039c4231e4d85ae878795ce3095dd96dcbfdb'
 
   def install
