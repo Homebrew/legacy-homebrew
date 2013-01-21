615a203d35579686c8922d579e475192691f4220
Adam Vandenberg <flangy@gmail.com>
2013-01-21 06:18:45 -0800

    rhash: add Lion requirement
    Closes #17132.

--- Library/Formula/rhash.rb
+++ Library/Formula/rhash.rb
@@ -1,9 +1,23 @@
 require 'formula'
 
+class LionOrNewer < Requirement
+  fatal true
+
+  def satisfied?
+    MacOS.version >= :lion
+  end
+  def message
+    "rhash requires `wcsdup` which isn't in the SDK before Lion."
+  end
+end
+
+
 class Rhash < Formula
   homepage 'http://rhash.anz.ru/'
   url 'http://downloads.sourceforge.net/project/rhash/rhash/1.2.10/rhash-1.2.10-src.tar.gz'
   sha1 '130f55faf3f13760ef0ab6a25e52db5052064c63'
+
+  depends_on LionOrNewer.new
 
   def install
     system 'make', 'install', "PREFIX=",
