c52ce5a3656bc694ca450d51da5999797b272609
tokuhirom <tokuhirom@gmail.com>
2013-01-21 14:12:01 +0900

    plenv 1.1.0
    Closes #17220.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/plenv.rb
+++ Library/Formula/plenv.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Plenv < Formula
   homepage 'https://github.com/tokuhirom/plenv'
-  url 'https://github.com/tokuhirom/plenv/tarball/1.0.8'
-  sha1 '9babaeb2af77609a00ccdbda5a22213d7c3529dc'
+  url 'https://github.com/tokuhirom/plenv/tarball/1.1.0'
+  sha1 'b7f2d72022fa8db36fe8cb9b4f3d351a845b1d34'
 
   head 'https://github.com/tokuhirom/plenv.git'
 
