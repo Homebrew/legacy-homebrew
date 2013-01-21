22cd5fafbf4f8e2a67a886537122f6b226f9c331
Bryan McLemore <kaelten@gmail.com>
2013-01-20 22:00:43 -0600

    Adding a recipie for cgminer.

---
+++ Library/Formula/cgminer.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Cgminer < Formula
+  homepage 'https://github.com/ckolivas/cgminer'
+  url 'https://github.com/ckolivas/cgminer/archive/v2.10.4.zip'
+  sha1 '026e03f35f23c26417d09a3288f7933969668001'
+
+  depends_on 'automake' => :build
+  depends_on 'curl' => :build
+  depends_on 'c-ares' => :build
+  depends_on 'libusb' => :build
+  depends_on 'pkg-config' => :build
+
+  def install
+    ENV['NOCONFIGURE'] = '1'
+
+    system "./autogen.sh"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--enable-cpumining"
+    system "make install"
+  end
+end
