8a642f38a1542cdc8eb7603ef53654a012c76085
Alex Burka <aburka@seas.upenn.edu>
2013-01-21 18:50:43 -0500

    libtermkey: uses libtool
    Closes #17241.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libtermkey.rb
+++ Library/Formula/libtermkey.rb
@@ -5,6 +5,8 @@ class Libtermkey < Formula
   url 'http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.13.tar.gz'
   sha1 'f0c2ead4185095e5c87c51b1a721e211ea08bae6'
 
+  depends_on :libtool => :build
+
   def install
     system "make", "PREFIX=#{prefix}", "LIBTOOL=glibtool"
     system "make", "install", "PREFIX=#{prefix}", "LIBTOOL=glibtool"
