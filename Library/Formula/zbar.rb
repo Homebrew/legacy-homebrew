4b95006ed0338de18bbb5705dbc88db108f70dbd
Adam Vandenberg <flangy@gmail.com>
2013-01-21 06:23:22 -0800

    zbar: depends on :x11

--- Library/Formula/zbar.rb
+++ Library/Formula/zbar.rb
@@ -5,6 +5,7 @@ class Zbar < Formula
   url 'http://downloads.sourceforge.net/project/zbar/zbar/0.10/zbar-0.10.tar.bz2'
   sha1 '273b47c26788faba4325baecc34063e27a012963'
 
+  depends_on :x11
   depends_on 'pkg-config' => :build
   depends_on 'jpeg'
   depends_on 'imagemagick'
