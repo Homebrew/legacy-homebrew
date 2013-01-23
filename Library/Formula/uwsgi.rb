0113a5139199f05bc500a602a2311a6e91b5766d
Krzysztof Urbaniak <urban@fail.pl>
2013-01-23 01:06:35 +0100

    uwsgi 1.4.4
    Closes #17261.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -19,8 +19,8 @@ end
 
 class Uwsgi < Formula
   homepage 'http://projects.unbit.it/uwsgi/'
-  url 'http://projects.unbit.it/downloads/uwsgi-1.2.6.tar.gz'
-  sha1 '61996a4bc7d745dc3ed849c78310c4e1c5c70ee1'
+  url 'http://projects.unbit.it/downloads/uwsgi-1.4.4.tar.gz'
+  sha1 'efaff6ebaa39bc015771bdecbf73373e1d1eceed'
 
   depends_on UniversalPcre.new
   depends_on 'pcre'
