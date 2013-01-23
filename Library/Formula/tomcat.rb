db50e5be3360db36c4dd3f43b4ce24c5e640b704
Christian Moritz <chrmoritz@gmail.com>
2013-01-23 23:03:20 +0100

    tomcat 7.0.35
    Closes #17272.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tomcat < Formula
   homepage 'http://tomcat.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.34/bin/apache-tomcat-7.0.34.tar.gz'
-  sha1 '881569860855458ed57c967e3cb10632ad951549'
+  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.35/bin/apache-tomcat-7.0.35.tar.gz'
+  sha1 '62a50fcd4c4144971003d3d2688129ae33cce3e6'
 
   def install
     # Remove Windows scripts
