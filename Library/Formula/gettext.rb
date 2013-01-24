029bb2ea17b7166617774be08cb9e2463601f896
Birger J. Nordølum <birgerjn@gmail.com>
2013-01-24 13:06:14 +0100

    gettext 0.18.2
    Removed one of the patches, as done over at MacPorts.

--- Library/Formula/gettext.rb
+++ Library/Formula/gettext.rb
@@ -2,32 +2,27 @@ require 'formula'
 
 class Gettext < Formula
   homepage 'http://www.gnu.org/software/gettext/'
-  url 'http://ftpmirror.gnu.org/gettext/gettext-0.18.1.1.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gettext/gettext-0.18.1.1.tar.gz'
-  sha1 '5009deb02f67fc3c59c8ce6b82408d1d35d4e38f'
+  url 'http://ftpmirror.gnu.org/gettext/gettext-0.18.2.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/gettext/gettext-0.18.2.tar.gz'
+  sha256 '516a6370b3b3f46e2fc5a5e222ff5ecd76f3089bc956a7587a6e4f89de17714c'
 
   keg_only "OS X provides the BSD gettext library and some software gets confused if both are in the library path."
 
-  bottle do
-    sha1 'd1ad5ad15bfe8fe813ee37e5d6b514fc79924b9a' => :mountainlion
-    sha1 'c75fdb192f1b49c9e7e2039c66e24f60f26bc027' => :lion
-    sha1 'b8958544542fc160b4c74db5d83cb441d12741c7' => :snowleopard
-  end
+#  bottle do
+#    sha1 'd1ad5ad15bfe8fe813ee37e5d6b514fc79924b9a' => :mountainlion
+#    sha1 'c75fdb192f1b49c9e7e2039c66e24f60f26bc027' => :lion
+#    sha1 'b8958544542fc160b4c74db5d83cb441d12741c7' => :snowleopard
+#  end
 
   option :universal
   option 'with-examples', 'Keep example files'
 
   def patches
-    # Patch to allow building with Xcode 4; safe for any compiler.
-    p = {:p0 => ['https://trac.macports.org/export/79617/trunk/dports/devel/gettext/files/stpncpy.patch']}
-
     unless build.include? 'with-examples'
       # Use a MacPorts patch to disable building examples at all,
       # rather than build them and remove them afterwards.
-      p[:p0] << 'https://trac.macports.org/export/79183/trunk/dports/devel/gettext/files/patch-gettext-tools-Makefile.in'
+      {:p0 => ['https://trac.macports.org/export/102008/trunk/dports/devel/gettext/files/patch-gettext-tools-Makefile.in']}
     end
-
-    return p
   end
 
   def install
