55602982a434eeac44543ac4bac375a63e160650
Adam Vandenberg <flangy@gmail.com>
2013-01-23 20:57:30 -0800

    clpbar: fix audit warning

--- Library/Formula/clpbar.rb
+++ Library/Formula/clpbar.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Clpbar < Formula
   homepage 'http://clpbar.sourceforge.net/'
   url 'http://sourceforge.net/projects/clpbar/files/clpbar/bar-1.11.1/bar_1.11.1.tar.gz'
-  version '1.11.1'
   sha1 '112d54cec7ebe85aacc6c2ef7d55e297264d1dd5'
 
   def install
