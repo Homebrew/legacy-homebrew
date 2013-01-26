807087937dc73ae1622f971bbb7c67e8510dd63d
Jack Nagel <jacknagel@gmail.com>
2013-01-26 17:21:48 -0600

    inspircd: use new optional deps

--- Library/Formula/inspircd.rb
+++ Library/Formula/inspircd.rb
@@ -7,24 +7,19 @@ class Inspircd < Formula
 
   head 'https://github.com/inspircd/inspircd.git', :branch => 'insp20'
 
-  option 'without-gnutls', 'Disable the GnuTLS module'
-  option 'with-openssl', 'Enable the OpenSSL module'
-  option 'with-pcre', 'Enable the PCRE module'
-  option 'with-tre', 'Enable the TRE module'
-
   depends_on 'pkg-config' => :build
-  depends_on 'gnutls' => :recommended unless build.include? 'without-gnutls'
-  depends_on 'libgcrypt' unless build.include? 'without-gnutls'
-  depends_on 'openssl' if build.include? 'with-openssl'
-  depends_on 'pcre' if build.include? 'with-pcre'
-  depends_on 'tre' if build.include? 'with-tre'
+  depends_on 'gnutls' => :recommended
+  depends_on 'libgcrypt' unless build.without? 'gnutls'
+  depends_on 'openssl' => :optional
+  depends_on 'pcre' => :optional
+  depends_on 'tre' => :optional
 
   def install
     modules = []
-    modules << 'm_ssl_gnutls.cpp' unless build.include? 'without-gnutls'
-    modules << 'm_ssl_openssl.cpp' if build.include? 'with-openssl'
-    modules << 'm_regex_pcre.cpp' if build.include? 'with-pcre'
-    modules << 'm_regex_tre.cpp' if build.include? 'with-tre'
+    modules << 'm_ssl_gnutls.cpp' unless build.without? 'gnutls'
+    modules << 'm_ssl_openssl.cpp' if build.with? 'openssl'
+    modules << 'm_regex_pcre.cpp' if build.with? 'pcre'
+    modules << 'm_regex_tre.cpp' if build.with? 'tre'
 
     system './configure', "--enable-extras=#{modules.join(',')}" unless modules.empty?
     system './configure', "--prefix=#{prefix}", "--with-cc=#{ENV.cc}"
