b1755b966cf9e774051f5184fae69f0790531136
Stefan <hydra_github@me.com>
2013-01-26 13:26:30 +0100

    jemalloc 3.3.0
    Closes #17316.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/jemalloc.rb
+++ Library/Formula/jemalloc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jemalloc < Formula
   homepage 'http://www.canonware.com/jemalloc/download.html'
-  url 'http://www.canonware.com/download/jemalloc/jemalloc-3.2.0.tar.bz2'
-  sha1 'e5204872e74d3ee75eaa51c6dc39731d611883f3'
+  url 'http://www.canonware.com/download/jemalloc/jemalloc-3.3.0.tar.bz2'
+  sha1 'e426b0b8e4f8ef073b90358cb7603e7cbccde895'
 
   def install
     system './configure', '--disable-debug', "--prefix=#{prefix}"
