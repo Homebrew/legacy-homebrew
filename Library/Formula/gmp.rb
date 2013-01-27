a84a217d7c2178b73e96d9148b3f50b5d44763a4
bathtub <bathtubdev@gmail.com>
2013-01-27 11:49:30 -0800

    GMP 5.1.0. Completely new build strategy; builds universal every time.
    This is the first piece of "code" I have ever contributed to a project.
    So I wanted to do it right. Lots more to come!
    
    ~~~~ The Bathtub

--- Library/Formula/gmp.rb
+++ Library/Formula/gmp.rb
@@ -1,38 +1,116 @@
 require 'formula'
-
 class Gmp < Formula
-  homepage 'http://gmplib.org/'
-  url 'http://ftpmirror.gnu.org/gmp/gmp-5.0.5.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gmp/gmp-5.0.5.tar.bz2'
-  sha256 '1f588aaccc41bb9aed946f9fe38521c26d8b290d003c5df807f65690f2aadec9'
+                                                                             ###
+ homepage 'http://gmplib.org/' # <-: GNU mirrors no longer host GMP releases:  #
+ #  "The gmplib.org site now is the sole source for current GMP releases."     #
+ url 'ftp://ftp.gmplib.org/pub/gmp-5.1.0/gmp-5.1.0.tar.bz2'# I found a mirror  #
+ mirror 'http://fossies.org/unix/misc/gmp-5.1.0.tar.bz2' # <--: here for now.  #
+ sha256 'dfd9aba98fe5caa54a715b4584c7d45eb0ee0c8be9a3181164ad2fad5eefc796'     #
 
-  option '32-bit'
+ ###
+ # Passing any of the following arguments to configure *will* cause failures:
+ # enable-nails , enable-fat, enable-profiling=prof|gprof, enable-assert
+ # Those options exposed below are to make debugging easier. for easy debugging. 
+ # Generally, don't touch. The same goes for anything unlisted.
 
-  def install
-    # Reports of problems using gcc 4.0 on Leopard
-    # https://github.com/mxcl/homebrew/issues/issue/2302
-    # Also force use of 4.2 on 10.6 in case a user has changed the default
-    # Do not force if xcode > 4.2 since it does not have /usr/bin/gcc-4.2 as default
-    # FIXME convert this to appropriate fails_with annotations
-    ENV.gcc if MacOS::Xcode.provides_gcc?
+ option '32-bit', 'Manually specify 32-bit ABI'                              ###
+ option 'enable-assert', 'Enable assertion checking. Not reccommended.'        #
+ option 'disable-cxx', 'Disable C++ support.'                                  #
+ option 'disable-assembly', 'Disable assembly code optimization. Much slower.' #
+ option 'disable-fft', 'Disable fast Fourier transforms for multiplication.'   #
+ option 'disable-shared', 'Do not build dylibs.'                               #
+ option 'enable-static', 'Build static libraries (as well). No need usually.'  #
+ option 'enable-old-fft-full', 'Full of old farts. If thats what you want.'    #
 
-    args = %W[--prefix=#{prefix} --enable-cxx]
+ def install
+  args = ["--prefix=#{prefix}"] 
+  args << '--enable-cxx'      unless build.include? 'disable-cxx'
+  args << '--disable-static'  unless build.include? 'enable-static'
 
-    # Build 32-bit where appropriate, and help configure find 64-bit CPUs
-    # see: http://gmplib.org/macos.html
-    if MacOS.prefer_64_bit? and not build.build_32_bit?
-      ENV.m64
-      args << "--build=x86_64-apple-darwin"
-    else
-      ENV.m32
-      args << "--build=none-apple-darwin"
-    end
+  args << '--disable-shared'      if build.include? 'disable-shared'
+  args << '--enable-assert'       if build.include? 'enable-assert'
+  args << '--enable-old-fft-full' if build.include? 'enable-old-fft-full'
+  args << '--disable-assembly'    if build.include? 'disable-assembly'
+  args << '--disable-fft'         if build.include? 'disable-fft' 
+  
+  ###
+  # The previous issues listed herein should't occur provided that the following
+  # strictures are adhered to: first, do not try to optimize. The whole idea
+  # behind GMP is to help on big hard math stuff by using the strengths of your
+  # individual CPU, with as much of the C implementation replaced with assembly
+  # code as possible. It needs to be able to accurately recognize your platform.
+  # So no flags, except: you MUST tell the toolchain the proper architecture to
+  # match the target ABI.
 
-    system "./configure", *args
-    system "make"
-    ENV.j1 # Doesn't install in parallel on 8-core Mac Pro
-    # Upstream implores users to always run the test suite
-    system "make check"
-    system "make install"
-  end
-end
+  ###
+  # We'll then bake in -m32 where needed the second time, roll our own fat
+  # binary with lipo. A Note: specifying "--build=none-apple-darwin" as per
+  # the previous configuration is precicly the opposite of the goal here
+  # exactly *not* what we want to do; it clobbers any possible optimizations
+  # and, in the case of "none", literally forces it to use *only* C code.
+
+  ENV.deparallelize # is, unfortuately, needed. The problem is somewhere between
+                    # autoenv and libtool, though who's but it is, hard to say.
+                    # It chokes up at link time and complains about a duplicate
+                    # directory. But this is a good enough workaround.
+
+  if MacOS.prefer_64_bit? && !build.build_32_bit?
+
+     ENV.m64; ENV.append 'ABI', '64'
+
+     system './configure', *args
+     system 'make'
+     system 'make -C tune speed'
+     system "make am--refresh"
+     system './configure', *args
+     system 'make'
+     system 'make check'
+     system 'make install'
+
+     system 'mv', "#{prefix}", "#{Pathname.pwd}/64"
+     system 'make distclean'
+     
+     ENV.delete 'ABI'                                                        end
+
+   ENV.m32
+   ENV.append 'ABI', '32'
+
+   system './configure', *args
+   system 'make'
+   system 'make -C tune speed'
+   system 'make am--refresh'
+   system './configure', *args
+   system 'make'
+   system 'make check'
+   system 'make install'
+
+   unless build.build_32_bit? || !MacOS.prefer_64_bit?
+   
+    system 'lipo',   "-create", "#{prefix}/lib/libgmp.10.dylib",
+                                "#{Pathname.pwd}/64/lib/libgmp.10.dylib",
+                     "-output", "#{prefix}/lib/libgmp.10.dylib"
+    
+    system 'lipo',   '-create', "#{prefix}/lib/libgmpxx.4.dylib",
+                                "#{Pathname.pwd}/64/lib/libgmpxx.4.dylib",
+                     '-output', "#{prefix}/lib/libgmpxx.4.dylib"
+                          
+    if build.include? 'enable-static'
+     system 'lipo',  '-create', "#{prefix}/lib/libgmp.a",
+                                "#{Pathname.pwd}/64/lib/libgmp.a",
+                     '-output', "#{prefix}/lib/libgmp.a"
+    
+     system 'lipo',  '-create', "#{prefix}/lib/libgmpxx.a",
+                                "#{Pathname.pwd}/64/lib/libgmpxx.a",
+                     '-output', "#{prefix}/lib/libgmpxx.a"                   end
+                                                                             end
+   if build.include? 'enable-static'
+      system 'strip', '-x',     "#{prefix}/lib/libgmp.a",
+                                "#{prefix}/lib/libgmpxx.a"
+      system 'lipo', '-info',   "#{prefix}/lib/libgmp.a"
+      system 'lipo', '-info',   "#{prefix}/lib/libgmpxx.a"
+                                                                             end
+      system 'strip', '-x',     "#{prefix}/lib/libgmp.10.dylib",
+                                "#{prefix}/lib/libgmpxx.4.dylib"
+      system 'lipo', '-info',   "#{prefix}/lib/libgmp.10.dylib"
+      system 'lipo', '-info',   "#{prefix}/lib/libgmpxx.4.dylib"
+end                                                                          end
