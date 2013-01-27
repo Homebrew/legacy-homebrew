2792cb6b76ca68a571b8abadc95a52b1b9de5f65
Adam Vandenberg <flangy@gmail.com>
2013-01-27 14:49:06 -0800

    rhash: style nits

--- Library/Formula/rhash.rb
+++ Library/Formula/rhash.rb
@@ -16,7 +16,7 @@ class Rhash < Formula
   url 'http://downloads.sourceforge.net/project/rhash/rhash/1.2.10/rhash-1.2.10-src.tar.gz'
   sha1 '130f55faf3f13760ef0ab6a25e52db5052064c63'
 
-  depends_on LionOrNewer.new
+  depends_on LionOrNewer
 
   def install
     system 'make', 'install', "PREFIX=",
