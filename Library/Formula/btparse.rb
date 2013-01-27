b81cd3cf8c089cef0c5340f4de8fb4e68f8438b2
Adam Vandenberg <flangy@gmail.com>
2013-01-26 18:00:03 -0800

    btparse: style nits

--- Library/Formula/btparse.rb
+++ Library/Formula/btparse.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Btparse < Formula
-  url 'http://www.gerg.ca/software/btOOL/btparse-0.34.tar.gz'
   homepage 'http://www.gerg.ca/software/btOOL/'
+  url 'http://www.gerg.ca/software/btOOL/btparse-0.34.tar.gz'
   sha1 'cfc2a5364b99121843dae4fd4ce1b01326a7a0bb'
 
   def install
