745602baafa12aed3d6ee5badcc694f88f547247
Adam Vandenberg <flangy@gmail.com>
2013-01-26 17:47:38 -0800

    bcrypt: style nits

--- Library/Formula/bcrypt.rb
+++ Library/Formula/bcrypt.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Bcrypt < Formula
-  url 'http://bcrypt.sourceforge.net/bcrypt-1.1.tar.gz'
   homepage 'http://bcrypt.sourceforge.net'
+  url 'http://bcrypt.sourceforge.net/bcrypt-1.1.tar.gz'
   sha1 'fd4c7c83fdc560f143bb0e0a8c9fb7aa57e69698'
 
   def install
