f906587867990bfd40c9054fc32456a0829cb23f
Adam Vandenberg <flangy@gmail.com>
2013-01-27 15:42:43 -0800

    unixodbc: style nits

--- Library/Formula/unixodbc.rb
+++ Library/Formula/unixodbc.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Unixodbc < Formula
-  url 'http://www.unixodbc.org/unixODBC-2.3.1.tar.gz'
   homepage 'http://www.unixodbc.org/'
+  url 'http://www.unixodbc.org/unixODBC-2.3.1.tar.gz'
   sha1 '815cbc4f34e1a6d95daf3a5ab74e6ed3a586aad7'
 
   def install
