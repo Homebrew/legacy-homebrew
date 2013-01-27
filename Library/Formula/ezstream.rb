3c076e61349c1ea258162621e43cdd30ad72f6c9
Adam Vandenberg <flangy@gmail.com>
2013-01-27 15:05:39 -0800

    ezstream: style nits

--- Library/Formula/ezstream.rb
+++ Library/Formula/ezstream.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Ezstream < Formula
-  url 'http://downloads.xiph.org/releases/ezstream/ezstream-0.5.6.tar.gz'
   homepage 'http://www.icecast.org/ezstream.php'
+  url 'http://downloads.xiph.org/releases/ezstream/ezstream-0.5.6.tar.gz'
   sha1 'f9d3ee5a2f81d156685ffbfc59b556c63afcca83'
 
   depends_on 'libvorbis'
