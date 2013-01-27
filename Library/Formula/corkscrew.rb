caf101493384c3d3909cacfa0fcc16ec3a4d2cfe
Adam Vandenberg <flangy@gmail.com>
2013-01-26 18:22:19 -0800

    corkscrew: style nits

--- Library/Formula/corkscrew.rb
+++ Library/Formula/corkscrew.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Corkscrew < Formula
-  url 'http://www.agroman.net/corkscrew/corkscrew-2.0.tar.gz'
   homepage 'http://www.agroman.net/corkscrew/'
+  url 'http://www.agroman.net/corkscrew/corkscrew-2.0.tar.gz'
   sha1 '8bdb4c0dc71048136c721c33229b9bf795230b32'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--host=apple"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--host=apple"
     system "make install"
   end
 end
