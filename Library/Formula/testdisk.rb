96e64fdb03cd0497d97bb52d1354cc63a0cf248f
Adam Vandenberg <flangy@gmail.com>
2013-01-27 19:20:41 -0800

    testdisk: style nits

--- Library/Formula/testdisk.rb
+++ Library/Formula/testdisk.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Testdisk < Formula
-  url 'http://www.cgsecurity.org/testdisk-6.13.tar.bz2'
   homepage 'http://www.cgsecurity.org/wiki/TestDisk'
+  url 'http://www.cgsecurity.org/testdisk-6.13.tar.bz2'
   sha1 'b08ace0257e3e437b6fc140360d75807ca4d46ae'
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
