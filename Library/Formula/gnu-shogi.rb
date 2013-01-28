76162f7c34033fc2a6364cd52076e1363ed1d944
Adam Vandenberg <flangy@gmail.com>
2013-01-27 18:55:58 -0800

    gnu-shogi: style nits

--- Library/Formula/gnu-shogi.rb
+++ Library/Formula/gnu-shogi.rb
@@ -1,23 +1,23 @@
 require 'formula'
 
 class GnuShogi < Formula
-  url 'http://www.cs.caltech.edu/~mvanier/hacking/gnushogi/gnushogi-1.3.2.tar.gz'
   homepage 'http://www.cs.caltech.edu/~mvanier/hacking/gnushogi/gnushogi.html'
+  url 'http://www.cs.caltech.edu/~mvanier/hacking/gnushogi/gnushogi-1.3.2.tar.gz'
   sha1 '6135ec3eb84917d0dc2cab9ac982818ed41aad87'
 
   depends_on :x11
 
   def patches
-      # gcc 4.2.1 flags the following error
-      # In file included from attacks.c:32:
-      # gnushogi.h:144: error: conflicting types for ‘setlinebuf’
-      # /usr/include/stdio.h:351: error: previous declaration of ‘setlinebuf’ was here
-      #
-      # My patch fixes this by changing the declared return type of setlinebuf in
-      # gnushogi.h to int to match up with stdio.h's declaration
-      # It was returning void anyway, so unless something errantly relies on it not
-      # leaving something in the return register, this shouldn't break anything
-      DATA
+    # gcc 4.2.1 flags the following error
+    # In file included from attacks.c:32:
+    # gnushogi.h:144: error: conflicting types for ‘setlinebuf’
+    # /usr/include/stdio.h:351: error: previous declaration of ‘setlinebuf’ was here
+    #
+    # My patch fixes this by changing the declared return type of setlinebuf in
+    # gnushogi.h to int to match up with stdio.h's declaration
+    # It was returning void anyway, so unless something errantly relies on it not
+    # leaving something in the return register, this shouldn't break anything
+    DATA
   end
 
   def install
