0eb112db8a28598db2f7043000874ba7aee45786
Adam Vandenberg <flangy@gmail.com>
2013-01-27 21:08:10 -0800

    tabbed: style nit

--- Library/Formula/tabbed.rb
+++ Library/Formula/tabbed.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Tabbed < Formula
-  url 'http://dl.suckless.org/tools/tabbed-0.4.tar.gz'
   homepage 'http://tools.suckless.org/tabbed'
+  url 'http://dl.suckless.org/tools/tabbed-0.4.tar.gz'
   sha1 '7529360b088df30b66f05aa960712f1feda46e91'
 
   head 'http://hg.suckless.org/tabbed'
