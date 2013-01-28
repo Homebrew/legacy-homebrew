6d617056090c5f36a77a7ebb3ba76883cdd8e5cd
Adam Vandenberg <flangy@gmail.com>
2013-01-27 21:25:36 -0800

    scalpel: style nits

--- Library/Formula/scalpel.rb
+++ Library/Formula/scalpel.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Scalpel < Formula
-  url 'http://www.digitalforensicssolutions.com/Scalpel/scalpel-2.0.tar.gz'
   homepage 'http://www.digitalforensicssolutions.com/Scalpel/'
+  url 'http://www.digitalforensicssolutions.com/Scalpel/scalpel-2.0.tar.gz'
   sha1 '4cc1164c75471c75f7dfa91c81ccef7eb15142f1'
-  depends_on 'tre'
 
+  depends_on 'tre'
 
   def install
     system "./configure", "--disable-dependency-tracking",
