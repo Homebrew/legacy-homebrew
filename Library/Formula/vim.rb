191a38e4f29c91c58902c778f828abb7fef90e31
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2013-01-28 13:33:36 +0800

    vim 7.3.785
    Closes #17373.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -4,8 +4,8 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-783'
-  version '7.3.783'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-785'
+  version '7.3.785'
 
   head 'https://vim.googlecode.com/hg/'
 
