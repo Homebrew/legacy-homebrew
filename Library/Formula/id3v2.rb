3aad7d087be0ad8a4fc0d5dc08a7ee35a468c4fd
Adam Vandenberg <flangy@gmail.com>
2013-01-27 20:50:40 -0800

    id3v2: style nits

--- Library/Formula/id3v2.rb
+++ Library/Formula/id3v2.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Id3v2 < Formula
-  url 'http://downloads.sourceforge.net/project/id3v2/id3v2/0.1.11/id3v2-0.1.11.tar.gz'
   homepage 'http://id3v2.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/id3v2/id3v2/0.1.11/id3v2-0.1.11.tar.gz'
   sha1 'ca825d851ca0c6a5783af107dc6baa7aa93f0bad'
 
   depends_on 'id3lib'
