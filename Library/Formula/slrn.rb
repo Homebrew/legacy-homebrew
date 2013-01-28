66ebdcca3817bbd77758b14577716ff87a3dc2fa
Adam Vandenberg <flangy@gmail.com>
2013-01-27 21:21:09 -0800

    slrn: style nits

--- Library/Formula/slrn.rb
+++ Library/Formula/slrn.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Slrn < Formula
-  url 'http://sourceforge.net/projects/slrn/files/slrn/slrn-1.0.1.tar.gz'
   homepage 'http://slrn.sourceforge.net/'
+  url 'http://sourceforge.net/projects/slrn/files/slrn/slrn-1.0.1.tar.gz'
   sha1 '9ad41ec3894d2b6b1ae8f158e994a8f138540baa'
 
   depends_on 's-lang'
