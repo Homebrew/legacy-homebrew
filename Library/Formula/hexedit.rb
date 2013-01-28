5628c52831e3b7ee9797d4e1e44d02ce01ec7529
Adam Vandenberg <flangy@gmail.com>
2013-01-27 19:26:09 -0800

    hexedit: style nits

--- Library/Formula/hexedit.rb
+++ Library/Formula/hexedit.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class Hexedit < Formula
-  url 'http://rigaux.org/hexedit-1.2.12.src.tgz'
   homepage 'http://rigaux.org/hexedit.html'
+  url 'http://rigaux.org/hexedit-1.2.12.src.tgz'
   sha1 'fee89e390945045fe6b74b5f07600a8e664c8b21'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--mandir=#{man}"
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
     system "make install"
   end
 end
