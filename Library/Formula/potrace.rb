2a7cd9d726aab997654072e85562b14541d82d69
Adam Vandenberg <flangy@gmail.com>
2013-01-27 21:36:41 -0800

    potrace: style nits

--- Library/Formula/potrace.rb
+++ Library/Formula/potrace.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Potrace < Formula
-  url 'http://potrace.sourceforge.net/download/potrace-1.10.tar.gz'
   homepage 'http://potrace.sourceforge.net'
+  url 'http://potrace.sourceforge.net/download/potrace-1.10.tar.gz'
   sha1 'e6e8b64d85d30b4695d197a79187ac98c2b236f9'
 
   def install
