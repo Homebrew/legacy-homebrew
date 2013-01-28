4d223b548dc14c83cec0cb8f1dda9f6e0c6f1123
Adam Vandenberg <flangy@gmail.com>
2013-01-27 21:29:52 -0800

    re2c: style nits

--- Library/Formula/re2c.rb
+++ Library/Formula/re2c.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Re2c < Formula
-  url 'http://downloads.sourceforge.net/project/re2c/re2c/0.13.5/re2c-0.13.5.tar.gz'
   homepage 'http://re2c.org'
+  url 'http://downloads.sourceforge.net/project/re2c/re2c/0.13.5/re2c-0.13.5.tar.gz'
   sha1 '3d334efab53a4a051a2b189b49a849e13523b6c0'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
