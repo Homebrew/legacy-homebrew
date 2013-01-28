a734ef7f17938600ef167ae32b72b58b9262f57c
Jack Nagel <jacknagel@gmail.com>
2013-01-28 11:22:48 -0600

    LanguageModuleDependency: call super() to generate tags list
    Fixes #17379.

--- Library/Homebrew/requirements.rb
+++ Library/Homebrew/requirements.rb
@@ -8,6 +8,7 @@ class LanguageModuleDependency < Requirement
     @language = language
     @module_name = module_name
     @import_name = import_name || module_name
+    super()
   end
 
   satisfy { quiet_system(*the_test) }
