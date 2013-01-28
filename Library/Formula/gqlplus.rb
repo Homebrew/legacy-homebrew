1134e375e17f810630b81eff7313e4f40193d9a2
Adam Vandenberg <flangy@gmail.com>
2013-01-27 18:57:31 -0800

    gqlplus: style nits

--- Library/Formula/gqlplus.rb
+++ Library/Formula/gqlplus.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Gqlplus < Formula
-  url 'http://downloads.sourceforge.net/project/gqlplus/gqlplus/1.12/gqlplus-1.12.tar.gz'
   homepage 'http://gqlplus.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/gqlplus/gqlplus/1.12/gqlplus-1.12.tar.gz'
   sha1 '95bb0085555af94b48bad924d9e7027c9bc3cb93'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make"
     bin.install "gqlplus"
   end
