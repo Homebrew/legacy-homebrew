c6433328271771af4a200f0d79990b343509c4e0
Adam Vandenberg <flangy@gmail.com>
2013-01-28 21:25:46 -0800

    libhid: style nits

--- Library/Formula/libhid.rb
+++ Library/Formula/libhid.rb
@@ -1,15 +1,17 @@
 require 'formula'
 
 class Libhid < Formula
-  url 'http://alioth.debian.org/frs/download.php/1958/libhid-0.2.16.tar.gz'
   homepage 'http://libhid.alioth.debian.org/'
+  url 'http://alioth.debian.org/frs/download.php/1958/libhid-0.2.16.tar.gz'
   sha1 '9a25fef674e8f20f97fea6700eb91c21ebbbcc02'
 
   depends_on 'libusb'
   depends_on 'libusb-compat'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}", "--disable-swig"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--disable-swig"
 
     system "make install"
   end
