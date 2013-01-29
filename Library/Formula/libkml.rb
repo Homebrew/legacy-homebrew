f9c8e53f870de19d1ab0062716a996de4bb9de5b
Adam Vandenberg <flangy@gmail.com>
2013-01-28 21:26:32 -0800

    libkml: style nits

--- Library/Formula/libkml.rb
+++ Library/Formula/libkml.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Libkml < Formula
-  url 'http://libkml.googlecode.com/files/libkml-1.2.0.tar.gz'
   homepage 'http://code.google.com/p/libkml/'
+  url 'http://libkml.googlecode.com/files/libkml-1.2.0.tar.gz'
   sha1 '3fa5acdc2b2185d7f0316d205002b7162f079894'
 
   head 'http://libkml.googlecode.com/svn/trunk/', :using => :svn
