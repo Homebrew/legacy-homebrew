5830d0268e5fedb278f1a1e0e7ab1282bfe4d3da
Adam Vandenberg <flangy@gmail.com>
2013-01-28 20:43:11 -0800

    montage: style nits

--- Library/Formula/montage.rb
+++ Library/Formula/montage.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Montage < Formula
-  url 'http://montage.ipac.caltech.edu/download/Montage_v3.3.tar.gz'
   homepage 'http://montage.ipac.caltech.edu'
+  url 'http://montage.ipac.caltech.edu/download/Montage_v3.3.tar.gz'
   sha1 'c8db5b9018c96e19b584d45758819e892b453d6d'
 
   def install
@@ -11,8 +11,9 @@ class Montage < Formula
   end
 
   def caveats; <<-EOS.undent
-    Montage is under the Caltech/JPL non-exclusive, non-commercial software licence agreement
-    available at http://montage.ipac.caltech.edu/docs/download.html
+    Montage is under the Caltech/JPL non-exclusive, non-commercial software
+    licence agreement available at:
+      http://montage.ipac.caltech.edu/docs/download.html
     EOS
   end
 end
