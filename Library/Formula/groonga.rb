594e8080c22bd6f615592d3155fe14a571a6140e
Yoji SHIDARA <dara@shidara.net>
2013-01-29 12:38:07 +0900

    groonga 2.1.2
    Closes #17396.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Groonga < Formula
   homepage 'http://groonga.org/'
-  url 'http://packages.groonga.org/source/groonga/groonga-2.1.1.tar.gz'
-  sha1 'd0ef04bf6b61dac6911308f5d9fa08bec888d765'
+  url 'http://packages.groonga.org/source/groonga/groonga-2.1.2.tar.gz'
+  sha1 '3b189a3a188eef22149b08ed625368c5cd15c363'
 
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
