2d14f3f407ae1b82040699ac71eeea6fd911c183
Adam Vandenberg <flangy@gmail.com>
2013-01-28 21:20:55 -0800

    libpuzzle: style nits

--- Library/Formula/libpuzzle.rb
+++ Library/Formula/libpuzzle.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Libpuzzle < Formula
-  url 'http://download.pureftpd.org/pub/pure-ftpd/misc/libpuzzle/releases/libpuzzle-0.11.tar.bz2'
   homepage 'http://libpuzzle.pureftpd.org/project/libpuzzle'
+  url 'http://download.pureftpd.org/pub/pure-ftpd/misc/libpuzzle/releases/libpuzzle-0.11.tar.bz2'
   sha1 'a3352c67fd61eab33d5a03c214805b18723d719e'
 
   depends_on 'gd'
