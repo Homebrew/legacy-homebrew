1c7ba800a0a87381420467a34fca64326232d44c
Adam Vandenberg <flangy@gmail.com>
2013-01-28 21:20:14 -0800

    libopennet: style nits

--- Library/Formula/libopennet.rb
+++ Library/Formula/libopennet.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Libopennet < Formula
-  url 'http://www.rkeene.org/files/oss/libopennet/libopennet-0.9.9.tar.gz'
   homepage 'http://www.rkeene.org/oss/libopennet'
+  url 'http://www.rkeene.org/files/oss/libopennet/libopennet-0.9.9.tar.gz'
   sha1 'd15c698498401ec6036646eaf19914117d6f5c56'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
     system "make"
     system "make install"
   end
