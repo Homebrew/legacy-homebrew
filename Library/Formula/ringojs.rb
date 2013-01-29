3204d0d7e5baec3dd9e86bba615ab73a7cfdc5ef
Adam Vandenberg <flangy@gmail.com>
2013-01-28 19:40:47 -0800

    ringojs: style nits

--- Library/Formula/ringojs.rb
+++ Library/Formula/ringojs.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Ringojs < Formula
-  url 'https://github.com/downloads/ringo/ringojs/ringojs-0.8.tar.gz'
   homepage 'http://ringojs.org'
+  url 'https://github.com/downloads/ringo/ringojs/ringojs-0.8.tar.gz'
   sha1 '28fd76fce28b41e2abcbe27a8b1731744d340e94'
 
   def install
