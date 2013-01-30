cf4c3902c9a95d2e1738e01d436743defde274cf
Andrew Childs <lorne@cons.org.nz>
2013-01-31 02:40:04 +1300

    Remove opensp, duplicate of open-sp

--- Library/Formula/libofx.rb
+++ Library/Formula/libofx.rb
@@ -5,7 +5,7 @@ class Libofx < Formula
   url 'http://sourceforge.net/projects/libofx/files/libofx/0.9.5/libofx-0.9.5.tar.gz'
   sha1 '7e5245d68a0f3f7efad2fd809b2afbbff6ba0e73'
 
-  depends_on 'opensp'
+  depends_on 'open-sp'
 
   def install
     system "./configure", "--disable-dependency-tracking",
