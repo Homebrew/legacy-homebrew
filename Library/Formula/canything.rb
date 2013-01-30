e4dc83fb1a8293d1eb160b9c50aaea96ff7983ed
handlename <nagata@handlena.me>
2013-01-30 14:33:08 +0900

    some fix according to habit of homebrew

--- Library/Formula/canything.rb
+++ Library/Formula/canything.rb
@@ -1,16 +1,12 @@
 require 'formula'
 
 class Canything < Formula
+  homepage 'https://github.com/keiji0/canything'
   url 'https://github.com/keiji0/canything/archive/0.1.tar.gz'
   sha1 '55a8174d630df0bc6cf3f182cd075c4c0bae6eec'
-  homepage 'https://github.com/keiji0/canything'
-  version '0.1'
 
   def install
     system "make"
-    system "mkdir -p #{prefix}/bin"
-    system "cp LICENSE #{prefix}"
-    system "cp README #{prefix}"
-    system "mv canything #{prefix}/bin"
+    bin.install 'canything'
   end
 end
