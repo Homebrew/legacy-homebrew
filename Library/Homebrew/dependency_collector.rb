6a1944b2f314ba98155ae7a6512c76f2916c9dee
Jack Nagel <jacknagel@gmail.com>
2013-01-29 22:52:10 -0600

    Create proxy classes for "partial" X11 dependencies
    When a formula's dependency tree contains more than one X11 dependency,
    they are de-duplicated by comparing the min_version attribute. However,
    this can result in broken dependency trees if one of the X11Dependency
    objects was actually specified as e.g. `:libpng`.
    
    In practice, this only matters when one or more of the dependencies has
    additional metadata that makes it distinct from the rest, i.e. an
    :optional or :recommended tag.
    
    To combat this, make these special, "partial" X11 dependencies instances
    of different classes so that they are not de-duped.
    
    It will still be necessary, at the time when requirements are expanded
    by the installer, to de-duplicate any remaining X11 dependencies after
    applying the optional/recommended filters in order to avoid duplicated
    modifications to the environment (as ENV.x11 is not idempotent).
    
    c.f. #17369.

--- Library/Homebrew/dependency_collector.rb
+++ Library/Homebrew/dependency_collector.rb
@@ -77,24 +77,18 @@ private
     when :autoconf, :automake, :bsdmake, :libtool
       # Xcode no longer provides autotools or some other build tools
       Dependency.new(spec.to_s, tag) unless MacOS::Xcode.provides_autotools?
-    when :libpng, :freetype, :pixman, :fontconfig, :cairo
+    when *X11Dependency::Proxy::PACKAGES
       if MacOS.version >= :mountain_lion
         Dependency.new(spec.to_s, tag)
       else
-        X11Dependency.new(spec.to_s, tag)
+        X11Dependency::Proxy.for(spec.to_s, tag)
       end
-    when :x11
-      X11Dependency.new(spec.to_s, tag)
-    when :xcode
-      XcodeDependency.new(tag)
-    when :mysql
-      MysqlInstalled.new(tag)
-    when :postgresql
-      PostgresqlInstalled.new(tag)
-    when :tex
-      TeXInstalled.new(tag)
-    when :clt
-      CLTDependency.new(tag)
+    when :x11        then X11Dependency.new(spec.to_s, tag)
+    when :xcode      then XcodeDependency.new(tag)
+    when :mysql      then MysqlInstalled.new(tag)
+    when :postgresql then PostgresqlInstalled.new(tag)
+    when :tex        then TeXInstalled.new(tag)
+    when :clt        then CLTDependency.new(tag)
     else
       raise "Unsupported special dependency #{spec}"
     end
