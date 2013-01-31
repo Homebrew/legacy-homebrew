8329e172a26f514ed54bedc2389dbc5e660cff03
Alexander Surma <surma@surmair.de>
2013-01-31 15:52:01 +0100

    ivy 2.3.0-rc2
    Closes #17464.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ivy.rb
+++ Library/Formula/ivy.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Ivy < Formula
   homepage 'http://ant.apache.org/ivy/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=ant/ivy/2.3.0-rc1/apache-ivy-2.3.0-rc1-bin.tar.gz'
-  sha1 '6cdb7b8131ca3b13b0b6b43988f3ab92da2de6e7'
+  url 'http://www.apache.org/dyn/closer.cgi?path=ant/ivy/2.3.0-rc2/apache-ivy-2.3.0-rc2-bin.tar.gz'
+  sha1 '12a2461ba801642ff44fa1b39c6a04d39b06552a'
 
   def install
     libexec.install Dir['ivy*']
     doc.install Dir['doc/*']
-    bin.write_jar_script libexec/'ivy-2.3.0-rc1.jar', 'ivy', '$JAVA_OPTS'
+    bin.write_jar_script libexec/'ivy-2.3.0-rc2.jar', 'ivy', '$JAVA_OPTS'
   end
 end
