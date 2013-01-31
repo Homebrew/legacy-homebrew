2854aec4776c18af56165ad45218f75b01d9453c
Etienne Laurin <atnnn@rethinkdb.com>
2013-01-31 14:01:58 -0800

    rethinkdb: fix dependencies, simplify build options and add head
    Closes #17478.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rethinkdb.rb
+++ Library/Formula/rethinkdb.rb
@@ -5,18 +5,21 @@ class Rethinkdb < Formula
   url 'https://github.com/rethinkdb/rethinkdb/archive/v1.3.2.tar.gz'
   sha1 '8956087fb98f32fa5a320e696e28b239afe03ab4'
 
-  depends_on 'v8'
+  head 'https://github.com/rethinkdb/rethinkdb.git'
+
   depends_on 'node' => :build
   depends_on 'protobuf' => :build
+  depends_on 'boost' => :build
+  depends_on 'v8'
   depends_on 'less' => :node
   depends_on 'coffee-script' => :node
-  depends_on LanguageModuleDependency.new(:ruby, 'ruby_protobuf', 'protobuf/compiler/compiler')
+  depends_on 'handlebars' => :node if build.head?
 
   def install
-    system "cd src; make DEBUG=0 FORCEVERSION=1 RETHINKDB_VERSION=#{version} PREFIX=#{prefix} WEBRESDIR=#{share}/rethinkdb/web"
+    cd "src" do
+      system "make DEBUG=0 WEBRESDIR=#{share}/rethinkdb/web"
+    end
     bin.install 'build/release/rethinkdb'
-
-    (share/'rethinkdb').mkpath
     share.install 'build/release/web' => 'rethinkdb'
   end
 end
