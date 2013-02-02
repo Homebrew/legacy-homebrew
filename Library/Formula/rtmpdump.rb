c688cdbd5ff19960cb964b28abd6e07038959af1
Charlie Sharpsteen <source@sharpsteen.net>
2013-02-02 10:24:54 -0800

    rtmpdump: Set version number
    The version parser was going with `2.4+20111222.git4e06e21.orig`.

--- Library/Formula/rtmpdump.rb
+++ Library/Formula/rtmpdump.rb
@@ -4,6 +4,7 @@ class Rtmpdump < Formula
   homepage 'http://rtmpdump.mplayerhq.hu'
   url 'http://ftp.de.debian.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20111222.git4e06e21.orig.tar.gz'
   sha1 '16f7e7470939ce8801e7d499345fa7d8f195c636'
+  version '2.4'
 
   head 'git://git.ffmpeg.org/rtmpdump'
 
