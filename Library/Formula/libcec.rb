1aa4dc463118eebcac5c626732e3e584ca769c8e
Jack Nagel <jacknagel@gmail.com>
2013-02-01 23:44:58 -0600

    libcec: autotools are build deps by default

--- Library/Formula/libcec.rb
+++ Library/Formula/libcec.rb
@@ -5,9 +5,9 @@ class Libcec < Formula
   url 'https://github.com/Pulse-Eight/libcec/archive/libcec-2.0.5-repack.zip'
   sha1 '94b2c94439156c13499620777bfe03900016879a'
 
-  depends_on :autoconf => :build
-  depends_on :automake => :build
-  depends_on :libtool => :build
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
   depends_on 'pkg-config' => :build
 
   def install
