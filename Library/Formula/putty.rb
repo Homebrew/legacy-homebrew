fa97d06e43105897bb84cf84f1dd2c3a3204e56b
DocumentRoot <browserleaks@gmail.com>
2013-02-02 19:58:47 +0400

    Update Library/Formula/putty.rb
    added putty gui depends_on 'gtk+'

--- Library/Formula/putty.rb
+++ Library/Formula/putty.rb
@@ -5,19 +5,18 @@ class Putty < Formula
   url 'http://the.earth.li/~sgtatham/putty/0.62/putty-0.62.tar.gz'
   sha1 '5898438614117ee7e3704fc3f30a3c4bf2041380'
 
+  depends_on 'gtk+'
+
   def install
-    # use the unix build to make all PuTTY command line tools
+    # use the unix build to make all PuTTY gui and command line tools
     cd "unix" do
-      # disable GTK upon configure
-      system "./configure", "--prefix=#{prefix}",
-                            "--disable-gtktest",
-                            "--with-gtk-prefix=/dev/null"
-      system "make", "VER=-DRELEASE=#{version}", "all-cli"
+      system "./configure", "--prefix=#{prefix}"
+      system "make", "VER=-DRELEASE=#{version}", "all"
       # install manually
-      bin.install %w{ plink pscp psftp puttygen }
+      bin.install %w{ pterm putty puttytel plink pscp psftp puttygen }
     end
     cd "doc" do
-      man1.install %w{ plink.1 pscp.1 psftp.1 puttygen.1 }
+      man1.install %w{ pterm.1 putty.1 puttytel.1 plink.1 pscp.1 psftp.1 puttygen.1 }
     end
   end
 end
