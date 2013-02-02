b7ad902863a4c17c6c28ace0a83c76036f89fa10
Jack Nagel <jacknagel@gmail.com>
2013-02-01 23:45:05 -0600

    spiped: autotools are build deps by default

--- Library/Formula/spiped.rb
+++ Library/Formula/spiped.rb
@@ -5,7 +5,7 @@ class Spiped < Formula
   url 'https://www.tarsnap.com/spiped/spiped-1.2.2.tgz'
   sha256 'a9eb4681e4ccd5d86b8a2d4e16785db8ba10d8a9f7f732485511fd4b92dff1ec'
 
-  depends_on :bsdmake => :build
+  depends_on :bsdmake
 
   def install
     system "bsdmake", "BINDIR_DEFAULT=#{bin}", "install"
