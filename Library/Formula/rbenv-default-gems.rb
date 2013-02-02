7d6f0ead8392824e8168ab5f4d51a5f10766f6e7
Jason Sallis <jason@sall.is>
2013-02-02 14:39:49 -0800

    add rbenv-default-gems formula w/ upgraded ruby-build dependency

---
+++ Library/Formula/rbenv-default-gems.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class RbenvDefaultGems < Formula
+  homepage 'https://github.com/sstephenson/rbenv-default-gems'
+  url 'https://github.com/sstephenson/rbenv-default-gems/tarball/v1.0.0'
+  sha1 '31789539ad0e69c2b9e2ec0da1d9acb8fba8b2bc'
+
+  head 'https://github.com/sstephenson/rbenv-default-gems.git'
+
+  depends_on 'rbenv'
+  depends_on 'ruby-build'
+
+  def install
+    prefix.install Dir['*']
+    ln_sf opt_prefix, "#{HOMEBREW_PREFIX}/var/lib/rbenv/plugins/#{name}"
+  end
+end
