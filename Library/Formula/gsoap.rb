871c700a11dce2f186557aa8be2366ebbc18214d
Brett Koonce <koonce@gmail.com>
2013-02-01 20:10:56 -0600

    gsoap 2.8.13
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gsoap.rb
+++ Library/Formula/gsoap.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gsoap < Formula
   homepage 'http://www.cs.fsu.edu/~engelen/soap.html'
-  url 'http://sourceforge.net/projects/gsoap2/files/gSOAP/gsoap_2.8.12.zip'
-  sha1 '6ff652dbee1b28a21c11ddcca64128fd647a992e'
+  url 'http://sourceforge.net/projects/gsoap2/files/gSOAP/gsoap_2.8.13.zip'
+  sha1 '8236bf4c214755d77ceed7e581baf0142c2034fe'
 
   def install
     ENV.deparallelize
