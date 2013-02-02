4a7b0be2141523fdfa36cf26d9bbdc1d11d86540
Ben Hoskings <ben@hoskings.net>
2013-02-02 20:53:20 +1100

    Shell out with #system calls in 'rubinius'.
    Fixes 'wrong number of arguments (14 for 0)' that
    the #ruby calls were generating.

--- Library/Formula/rubinius.rb
+++ Library/Formula/rubinius.rb
@@ -17,18 +17,18 @@ class Rubinius < Formula
     # Set to stop Rubinius messing with our prefix.
     ENV["RELEASE"] = "1"
 
-    ruby "./configure",
-         "--skip-system", # download and use the prebuilt LLVM
-         "--bindir", bin,
-         "--prefix", prefix,
-         "--includedir", "#{include}/rubinius",
-         "--libdir", lib,
-         "--mandir", man, # For completeness; no manpages exist yet.
-         "--gemsdir", "#{lib}/rubinius/gems"
+    system "./configure",
+           "--skip-system", # download and use the prebuilt LLVM
+           "--bindir", bin,
+           "--prefix", prefix,
+           "--includedir", "#{include}/rubinius",
+           "--libdir", lib,
+           "--mandir", man, # For completeness; no manpages exist yet.
+           "--gemsdir", "#{lib}/rubinius/gems"
 
     ohai "config.rb", File.open('config.rb').to_a if ARGV.debug? or ARGV.verbose?
 
-    ruby "-S", "rake", "install"
+    system "rake", "install"
 
     # Remove conflicting command aliases
     bin.children.select(&:symlink?).each(&:unlink)
