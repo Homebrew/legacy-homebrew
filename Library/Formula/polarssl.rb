7a4a808d062ef8fba43a53eced22a28c047c87e2
Birger J. Nordølum <birgerjn@gmail.com>
2013-02-02 02:50:51 +0100

    polarssl 1.2.4
    Closes #17517.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/polarssl.rb
+++ Library/Formula/polarssl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Polarssl < Formula
   homepage 'http://polarssl.org/'
-  url 'http://polarssl.org/code/releases/polarssl-1.2.3-gpl.tgz'
-  sha1 'b47b3db19c4487c5930eaec23dda0dbd24851146'
+  url 'http://polarssl.org/code/releases/polarssl-1.2.4-gpl.tgz'
+  sha1 '75690f234392e2d663abd66750a9e87ee627e3e8'
 
   depends_on 'cmake' => :build
 
