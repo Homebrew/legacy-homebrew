f166b83fee048524ae611ab642091444c89bc08b
Brett Koonce <koonce@gmail.com>
2013-02-01 20:11:31 -0600

    libcsv 3.0.3
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libcsv.rb
+++ Library/Formula/libcsv.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libcsv < Formula
   homepage 'http://sourceforge.net/projects/libcsv/'
-  url 'http://downloads.sourceforge.net/project/libcsv/libcsv/libcsv-3.0.2/libcsv-3.0.2.tar.gz'
-  sha1 '253f23ecedcfdc5b3e4884458d77d6806c07a48d'
+  url 'http://downloads.sourceforge.net/project/libcsv/libcsv/libcsv-3.0.3/libcsv-3.0.3.tar.gz'
+  sha1 '2f637343c3dfac80559595f519e8f78f25acc7c1'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
