70c63b0427e65d9350a83ab8410057b7f90ad771
Jack Nagel <jacknagel@gmail.com>
2013-02-03 14:39:47 -0600

    geeqie: intltool is a build-time dep

--- Library/Formula/geeqie.rb
+++ Library/Formula/geeqie.rb
@@ -7,8 +7,8 @@ class Geeqie < Formula
 
   depends_on :x11
   depends_on 'pkg-config' => :build
+  depends_on 'intltool' => :build
   depends_on 'gettext'
-  depends_on 'intltool'
   depends_on 'gtk+'
   depends_on 'imagemagick' => :recommended
   depends_on 'exiv2' => :recommended
