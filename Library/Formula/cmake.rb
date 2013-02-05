15cbe3383d875d2e0304580c737d38e7b95db78e
Stefan <hydra_github@me.com>
2013-02-05 04:23:20 +0100

    cmake 2.8.10.2
    Closes #17594.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -22,14 +22,8 @@ end
 
 class Cmake < Formula
   homepage 'http://www.cmake.org/'
-  url 'http://www.cmake.org/files/v2.8/cmake-2.8.10.1.tar.gz'
-  sha1 'ff536d0592a0433ef3610f1861886712b99979a5'
-
-  bottle do
-    sha1 '1a43a9a7f05216c9dc2458bca6aaa80c4a6cfc5b' => :mountainlion
-    sha1 '31856bbd662ca47c325761fc7040e43f9a635c64' => :lion
-    sha1 'ff80d9bb064fcec2e268896ede95532f99c6cfb6' => :snowleopard
-  end
+  url 'http://www.cmake.org/files/v2.8/cmake-2.8.10.2.tar.gz'
+  sha1 '2d868ccc3f9f2aa7c2844bd0a4609d5313edaaec'
 
   depends_on NoExpatFramework
 
