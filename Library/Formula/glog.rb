7c50303e0b13b3aae8789a5ed7cecf77b914af55
Brett Koonce <koonce@gmail.com>
2013-02-05 07:56:54 -0600

    glog 0.3.3
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/glog.rb
+++ Library/Formula/glog.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Glog < Formula
   homepage 'http://code.google.com/p/google-glog/'
-  url 'http://google-glog.googlecode.com/files/glog-0.3.2.tar.gz'
-  sha1 '94e641e50afd03c574af6a55084e94a347c911d7'
+  url 'http://google-glog.googlecode.com/files/glog-0.3.3.tar.gz'
+  sha1 'ed40c26ecffc5ad47c618684415799ebaaa30d65'
 
   depends_on 'gflags'
 
