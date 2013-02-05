1a11407498b7a9d9420d4333b8a2df9d9647993b
Stefan <hydra_github@me.com>
2013-02-05 03:52:36 +0100

    nsd 3.2.15

--- Library/Formula/nsd.rb
+++ Library/Formula/nsd.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Nsd < Formula
   homepage 'http://www.nlnetlabs.nl/projects/nsd/'
-  url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-3.2.13.tar.gz'
-  sha1 '2cb44f75e9686fd73c7ee9765857a36a8fe5bca9'
+  url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-3.2.15.tar.gz'
+  sha1 'e31a81ab7877422b34e1f163f9509cd93f395664'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
