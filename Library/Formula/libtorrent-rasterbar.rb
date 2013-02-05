d3b37e9dbf00d29c7aefff7b050530514ce70f7b
Stefan <hydra_github@me.com>
2013-02-05 05:37:07 +0100

    libtorrent-rasterbar 0.16.7
    Closes #17597.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class LibtorrentRasterbar < Formula
   homepage 'http://www.rasterbar.com/products/libtorrent/'
-  url 'https://libtorrent.googlecode.com/files/libtorrent-rasterbar-0.16.6.tar.gz'
-  sha1 'baf95f53be99a90c4bb1488a72e9d625684bbbd2'
+  url 'https://libtorrent.googlecode.com/files/libtorrent-rasterbar-0.16.7.tar.gz'
+  sha1 '94b132a17d8f6a5345937731343398be8d31f170'
 
   depends_on 'boost'
 
