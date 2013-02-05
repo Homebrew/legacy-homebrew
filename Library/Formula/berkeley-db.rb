9785083794981dfa692172e1cea6497aa30b7bbe
Jack Nagel <jacknagel@gmail.com>
2013-02-05 09:23:35 -0600

    berkeley-db: patch for build under Xcode 4.6
    Fixes #17596.

--- Library/Formula/berkeley-db.rb
+++ Library/Formula/berkeley-db.rb
@@ -7,6 +7,9 @@ class BerkeleyDb < Formula
 
   option 'with-java', 'Compile with Java support.'
 
+  # Fix build under Xcode 4.6
+  def patches; DATA; end
+
   def install
     # BerkeleyDB dislikes parallel builds
     ENV.deparallelize
@@ -28,3 +31,27 @@ class BerkeleyDb < Formula
     end
   end
 end
+
+__END__
+diff --git a/src/dbinc/atomic.h b/src/dbinc/atomic.h
+index 096176a..561037a 100644
+--- a/src/dbinc/atomic.h
++++ b/src/dbinc/atomic.h
+@@ -144,7 +144,7 @@ typedef LONG volatile *interlocked_val;
+ #define	atomic_inc(env, p)	__atomic_inc(p)
+ #define	atomic_dec(env, p)	__atomic_dec(p)
+ #define	atomic_compare_exchange(env, p, o, n)	\
+-	__atomic_compare_exchange((p), (o), (n))
++	__atomic_compare_exchange_db((p), (o), (n))
+ static inline int __atomic_inc(db_atomic_t *p)
+ {
+ 	int	temp;
+@@ -176,7 +176,7 @@ static inline int __atomic_dec(db_atomic_t *p)
+  * http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
+  * which configure could be changed to use.
+  */
+-static inline int __atomic_compare_exchange(
++static inline int __atomic_compare_exchange_db(
+ 	db_atomic_t *p, atomic_value_t oldval, atomic_value_t newval)
+ {
+ 	atomic_value_t was;
