177f436376c90a4e4f218d7fa315cd51c2a09533
Mike McQuaid <mike@mikemcquaid.com>
2013-02-05 21:30:43 -0800

    rbenv-default-gems: fix caveats
    References #17545.

--- Library/Formula/rbenv-default-gems.rb
+++ Library/Formula/rbenv-default-gems.rb
@@ -17,7 +17,7 @@ class RbenvDefaultGems < Formula
 
   def caveats; <<-EOS.undent
     If you are using Homebrew's directories for `rbenv` rather than ~/.rbenv
-    place the `default-gems` file in #{opt_prefix}/.
+    place the `default-gems` file in #{HOMEBREW_PREFIX+'opt/rbenv'}/.
     EOS
   end
 end
