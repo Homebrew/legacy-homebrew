9fa52b850cc35d38b476f56a839a0a643ae7e74c
Brian Martin <brianmartin@gmail.com>
2013-02-06 11:10:48 -0500

    tbb: update broken download link
    Closes #17631.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/tbb.rb
+++ Library/Formula/tbb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tbb < Formula
   homepage 'http://www.threadingbuildingblocks.org/'
-  url 'http://threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb41_20121003oss_src.tgz'
-  sha1 '072333068fa278635582846eaaa22d51c0100e09'
+  url 'http://threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb41_20121003oss_src_1.tgz'
+  sha1 '9ecf581dbf98f7828d83b4f1b47f6f1140fbb759'
   version '4.1u1'
 
   fails_with :clang do
