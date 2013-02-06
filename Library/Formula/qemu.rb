0550650b8add5a47b0cfbe5db607b76359ee7e26
Xiyue Deng <manphiz@gmail.com>
2013-02-05 22:56:13 -0800

    qemu 1.3.1
    Closes #17623.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/qemu.rb
+++ Library/Formula/qemu.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Qemu < Formula
   homepage 'http://www.qemu.org/'
-  url 'http://wiki.qemu-project.org/download/qemu-1.3.0.tar.bz2'
-  sha1 'ed56e8717308a56f51a6ed4c18a4335e5aacae83'
+  url 'http://wiki.qemu-project.org/download/qemu-1.3.1.tar.bz2'
+  sha1 '5a3ef5273b3f39418e90680b144001f97d27c2c2'
+
   head 'git://git.qemu-project.org/qemu.git', :using => :git
 
   depends_on 'jpeg'
@@ -11,20 +12,8 @@ class Qemu < Formula
   depends_on 'glib'
   depends_on 'pixman'
 
-  def patches
-    # This patch fixes the semaphore fallback code for block devices,
-    # as OS X does not implement sem_timedwait() & Co.
-    #
-    # It has not been merged to the 1.3.x stable branch yet.
-    #
-    # See https://bugs.launchpad.net/qemu/+bug/1087114
-    if not build.head? then
-      { :p1 => "https://github.com/qemu/qemu/commit/a795ef8dcb8cbadffc996c41ff38927a97645234.diff"}
-    end
-  end
-
   def install
-    # Disable the sdl backend. Let it use CoreAudio instead.
+    # Disable the sdl backend; use CoreAudio instead.
     args = %W[
       --prefix=#{prefix}
       --cc=#{ENV.cc}
