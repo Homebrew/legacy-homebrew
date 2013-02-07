c5851bb07062f15bb46e8d9eaaf48a2ea2b34294
Adam Vandenberg <flangy@gmail.com>
2013-02-06 20:43:54 -0800

    notmuch: add --emacs option
    Closes #15895.

--- Library/Formula/notmuch.rb
+++ Library/Formula/notmuch.rb
@@ -1,10 +1,27 @@
 require 'formula'
 
+class NewEnoughEmacs < Requirement
+  fatal true
+
+  def satisfied?
+    `emacs --version`.split("\n")[0] =~ /GNU Emacs (\d+)\./
+    major_version = ($1 || 0).to_i
+    major_version >= 23
+  end
+
+  def message
+    "Emacs support requires at least Emacs 23."
+  end
+end
+
 class Notmuch < Formula
   homepage 'http://notmuchmail.org'
   url 'http://notmuchmail.org/releases/notmuch-0.14.tar.gz'
   sha1 'ad1ef9c2d29cfb0faab7837968d11325dee404bd'
 
+  option "emacs", "Install emacs support."
+
+  depends_on NewEnoughEmacs if build.include? "emacs"
   depends_on 'xapian'
   depends_on 'talloc'
   depends_on 'gmime'
@@ -15,8 +32,13 @@ class Notmuch < Formula
   end
 
   def install
-    # requires a newer emacs than OS X provides, so disable the bindings
-    system "./configure", "--prefix=#{prefix}", "--without-emacs"
+    args = ["--prefix=#{prefix}"]
+    if build.include? "emacs"
+      args << "--with-emacs"
+    else
+      args << "--without-emacs"
+    end
+    system "./configure", *args
     system "make install"
   end
 end
