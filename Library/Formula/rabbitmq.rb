0d7c339c991500b13369e63eb3258c323044a046
Peter Eisentraut <peter@eisentraut.org>
2013-02-08 12:53:42 -0500

    rabbitmq: Add missing build dependencies
    It requires autoconf, automake, libtool to build.

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -5,6 +5,9 @@ class Rabbitmq < Formula
   url 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.0.2/rabbitmq-server-generic-unix-3.0.2.tar.gz'
   sha1 '83e3d01cf1d901b72b2110751405c53c4f50eb16'
 
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
   depends_on 'erlang'
   depends_on 'simplejson' => :python if MacOS.version == :leopard
 
