0466f9726a5281aaa187258848cf0c539f913ee5
Shaun Jackman <sjackman@gmail.com>
2013-02-09 13:20:08 -0800

    graphviz 2.30.0

--- Library/Formula/graphviz.rb
+++ Library/Formula/graphviz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Graphviz < Formula
   homepage 'http://graphviz.org/'
-  url 'http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.28.0.tar.gz'
-  sha1 '4725d88a13e071ee22e632de551d4a55ca08ee7d'
+  url 'http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.30.0.tar.gz'
+  sha1 '596c1ddf60c5428c5c4700d10f3ea86534195311'
 
   env :std
 
@@ -31,7 +31,7 @@ class Graphviz < Formula
 
   def patches
     {:p0 =>
-      "https://trac.macports.org/export/78507/trunk/dports/graphics/graphviz-gui/files/patch-project.pbxproj.diff",
+      "https://trac.macports.org/export/101998/trunk/dports/graphics/graphviz/files/patch-project.pbxproj.diff",
      :p1 => DATA}
   end
 
@@ -85,35 +85,8 @@ class Graphviz < Formula
   end
 end
 
-# fix build on platforms without /usr/lib/libltdl.a (i.e., Lion)
-# http://www.graphviz.org/mantisbt/view.php?id=2109
-# fixed in upstream development version 2.29
-# second part of DATA patch fixes quartz plugin build, may not be in upstream yet
+# fix quartz plugin build, may not be in upstream yet
 __END__
-diff --git a/lib/gvc/Makefile.in b/lib/gvc/Makefile.in
-index 2d345a0..67183f2 100644
---- a/lib/gvc/Makefile.in
-+++ b/lib/gvc/Makefile.in
-@@ -41,8 +41,7 @@ host_triplet = @host@
- @WITH_WIN32_TRUE@am__append_1 = -O0
- @WITH_ORTHO_TRUE@am__append_2 = $(top_builddir)/lib/ortho/libortho_C.la
- @WITH_ORTHO_TRUE@am__append_3 = $(top_builddir)/lib/ortho/libortho_C.la
--@ENABLE_LTDL_TRUE@am__append_4 = $(LIBLTDL) $(LIBLTDL_LDFLAGS)
--@ENABLE_LTDL_TRUE@am__append_5 = $(LIBLTDL)
-+@ENABLE_LTDL_TRUE@am__append_4 = @LIBLTDL@ $(LIBLTDL_LDFLAGS)
- subdir = lib/gvc
- DIST_COMMON = $(noinst_HEADERS) $(pkginclude_HEADERS) \
- 	$(srcdir)/Makefile.am $(srcdir)/Makefile.in \
-@@ -87,8 +86,7 @@ am__installdirs = "$(DESTDIR)$(libdir)" "$(DESTDIR)$(man3dir)" \
- 	"$(DESTDIR)$(pkgincludedir)"
- LTLIBRARIES = $(lib_LTLIBRARIES) $(noinst_LTLIBRARIES)
- am__DEPENDENCIES_1 =
--@ENABLE_LTDL_TRUE@am__DEPENDENCIES_2 = $(am__DEPENDENCIES_1) \
--@ENABLE_LTDL_TRUE@	$(am__DEPENDENCIES_1)
-+@ENABLE_LTDL_TRUE@am__DEPENDENCIES_2 = $(am__DEPENDENCIES_1)
- am__DEPENDENCIES_3 = $(top_builddir)/lib/pack/libpack_C.la \
- 	$(top_builddir)/lib/xdot/libxdot_C.la \
- 	$(top_builddir)/lib/common/libcommon_C.la $(am__append_2) \
 diff --git a/plugin/quartz/Makefile.in b/plugin/quartz/Makefile.in
 index 20ec9c6..dbeb46b 100644
 --- a/plugin/quartz/Makefile.in
