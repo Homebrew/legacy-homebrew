a95243685893b9117bb7fd24bfd760e2d05c4db0
Adam Vandenberg <flangy@gmail.com>
2013-02-09 13:08:27 -0800

    uudeview: patch to compile with clang

--- Library/Formula/uudeview.rb
+++ Library/Formula/uudeview.rb
@@ -5,9 +5,9 @@ class Uudeview < Formula
   url 'http://www.fpx.de/fp/Software/UUDeview/download/uudeview-0.5.20.tar.gz'
   sha1 '2c6ab7d355b545218bd0877d598bd5327d9fd125'
 
-  fails_with :clang do
-    build 425
-    cause "inews.c:195:4: error: non-void function 'append_signature' should return a value [-Wreturn-type]"
+  # Fix function signatures (for clang)
+  def patches
+    {:p0 => "https://trac.macports.org/export/102865/trunk/dports/mail/uudeview/files/inews.c.patch" }
   end
 
   def install
