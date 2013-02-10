39341849716e33b5e95cebfb26c3df4bfaa833f1
Jack Nagel <jacknagel@gmail.com>
2013-02-09 23:53:29 -0600

    sylpheed: depends on pkg-config

--- Library/Formula/sylpheed.rb
+++ Library/Formula/sylpheed.rb
@@ -5,6 +5,7 @@ class Sylpheed < Formula
   url 'http://sylpheed.sraoss.jp/sylpheed/v3.3/sylpheed-3.3.0.tar.bz2'
   sha1 '07b7dd7f0b58065b6f8db7a3b443240aade7da6a'
 
+  depends_on 'pkg-config' => :build
   depends_on :x11
   depends_on 'gpgme'
   depends_on 'gtk+'
