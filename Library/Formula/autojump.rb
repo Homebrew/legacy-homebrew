ad5d917ef6450752af6dc996418f49f59b5ff302
Evan Shelhamer <shelhamer@imaginarynumber.net>
2013-02-10 13:00:48 -0800

    autojump 21.4.2
    Closes #17742.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/autojump.rb
+++ Library/Formula/autojump.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Autojump < Formula
   homepage 'https://github.com/joelthelion/autojump#name'
-  url 'https://github.com/joelthelion/autojump/archive/release-v21.3.0.tar.gz'
-  sha1 'fb9db92fca7c760c94e27f200dc0b2e1773ca150'
+  url 'https://github.com/joelthelion/autojump/archive/release-v21.4.2.tar.gz'
+  sha1 '6876838c570542a8af90b5850d5e17c8d656c50a'
 
   head 'https://github.com/joelthelion/autojump.git'
 
