b91c0dac6a866107b33356c5c37685dd3b426a7c
Adam Vandenberg <flangy@gmail.com>
2013-02-11 15:23:07 -0800

    solfege uses pkg-config
    Closes #17775.

--- Library/Formula/solfege.rb
+++ Library/Formula/solfege.rb
@@ -7,6 +7,7 @@ class Solfege < Formula
   sha1 '0b83f351e90aeb9267f76d8dda0638dc50682226'
 
   depends_on 'xz' => :build
+  depends_on 'pkg-config' => :build
   depends_on 'gettext'      => :build
   depends_on 'pygtk'        => :recommended
   depends_on 'qtplay'       => :recommended
