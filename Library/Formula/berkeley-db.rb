4ca2411ef25bda39916b8b2b60cf9faf9630069e
Adam Vandenberg <flangy@gmail.com>
2013-02-10 17:33:14 -0800

    berkeley-db: add --enable-sql option
    Closes #15166.

--- Library/Formula/berkeley-db.rb
+++ Library/Formula/berkeley-db.rb
@@ -6,19 +6,24 @@ class BerkeleyDb < Formula
   sha1 '32e43c4898c8996750c958a90c174bd116fcba83'
 
   option 'with-java', 'Compile with Java support.'
+  option 'enable-sql', 'Compile with SQL support.'
 
   # Fix build under Xcode 4.6
+  # Double-underscore names are reserved, and __atomic_compare_exchange is now
+  # a built-in, so rename this to something non-conflicting.
   def patches; DATA; end
 
   def install
     # BerkeleyDB dislikes parallel builds
     ENV.deparallelize
-
-    args = ["--disable-debug",
-            "--prefix=#{prefix}",
-            "--mandir=#{man}",
-            "--enable-cxx"]
+    args = %W[
+      --disable-debug
+      --prefix=#{prefix}
+      --mandir=#{man}
+      --enable-cxx
+    ]
     args << "--enable-java" if build.include? "with-java"
+    args << "--enable-sql" if build.include? "enable-sql"
 
     # BerkeleyDB requires you to build everything from the build_unix subdirectory
     cd 'build_unix' do
@@ -27,7 +32,7 @@ class BerkeleyDb < Formula
 
       # use the standard docs location
       doc.parent.mkpath
-      mv prefix+'docs', doc
+      mv prefix/'docs', doc
     end
   end
 end
