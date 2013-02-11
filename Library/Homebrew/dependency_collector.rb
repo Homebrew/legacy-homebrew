0eacf9b6831cc49ab2cce3e3f9b4823679b62369
Jack Nagel <jacknagel@gmail.com>
2013-02-10 19:25:22 -0600

    Shrink DependencyCollector#parse_symbol_spec and add tests

--- Library/Homebrew/dependency_collector.rb
+++ Library/Homebrew/dependency_collector.rb
@@ -76,13 +76,9 @@ private
     case spec
     when :autoconf, :automake, :bsdmake, :libtool
       # Xcode no longer provides autotools or some other build tools
-      Dependency.new(spec.to_s, [:build, *tag]) unless MacOS::Xcode.provides_autotools?
+      autotools_dep(spec, tag)
     when *X11Dependency::Proxy::PACKAGES
-      if MacOS.version >= :mountain_lion
-        Dependency.new(spec.to_s, tag)
-      else
-        X11Dependency::Proxy.for(spec.to_s, tag)
-      end
+      x11_dep(spec, tag)
     when :cairo, :pixman
       # We no longer use X11 psuedo-deps for cairo or pixman,
       # so just return a standard formula dependency.
@@ -95,6 +91,20 @@ private
     when :clt        then CLTDependency.new(tag)
     else
       raise "Unsupported special dependency #{spec}"
+    end
+  end
+
+  def x11_dep(spec, tag)
+    if MacOS.version >= :mountain_lion
+      Dependency.new(spec.to_s, tag)
+    else
+      X11Dependency::Proxy.for(spec.to_s, tag)
+    end
+  end
+
+  def autotools_dep(spec, tag)
+    unless MacOS::Xcode.provides_autotools?
+      Dependency.new(spec.to_s, [:build, *tag])
     end
   end
 end
