768b1038e0b394642bcf5e1e7a499273d80a3aea
Francisco Souza <f@souza.cc>
2013-02-14 20:08:24 -0200

    vim: version 7.3.820

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -4,8 +4,8 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-816'
-  version '7.3.816'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-820'
+  version '7.3.820'
 
   head 'https://vim.googlecode.com/hg/'
 
