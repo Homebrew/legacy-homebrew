a512978b3724d58a3159fce2a840b6702f643d6f
Aaron Bell <aarontbell@gmail.com>
2013-02-14 09:36:54 -0800

    perforce: fix checksum
    Closes #17841.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/perforce.rb
+++ Library/Formula/perforce.rb
@@ -6,7 +6,7 @@ class Perforce < Formula
   if MacOS.prefer_64_bit?
     url 'http://filehost.perforce.com/perforce/r12.2/bin.darwin90x86_64/p4'
     version '2012.2.536738-x86_64'
-    sha1 '4a0469279283204bbce510a849c3ffac6597c58d'
+    sha1 '6ecdbd5559ea4d04cddf81d76afe71e5cbe2baee'
   else
     url 'http://filehost.perforce.com/perforce/r12.2/bin.darwin90x86/p4'
     version '2012.2.536738-x86'
