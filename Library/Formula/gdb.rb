bca08f0a1d7685a0dd70f20479963c6806d59b5f
David Holm <dholmster@gmail.com>
2013-02-17 00:34:33 +0100

    Support using the homebrewed installation of Python with GDB
    Adds a flag, --with-brewed-python, which will build GDB with the
    Homebrewed Python installation instead of the system one. This requires
    Python to be built with --universal so a check for this has been added
    as well.
    
    Closes #153.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- Library/Formula/gdb.rb
+++ Library/Formula/gdb.rb
@@ -1,5 +1,18 @@
 require 'formula'
 
+class UniversalBrewedPython < Requirement
+  satisfy { archs_for_command("python").universal? }
+
+  def message; <<-EOS.undent
+    A build of GDB using a brewed Python was requested, but Python is not
+    a universal build.
+
+    GDB requires Python to be built as a universal binary or it will fail
+    if attempting to debug a 32-bit binary on a 64-bit host.
+    EOS
+  end
+end
+
 class Gdb < Formula
   homepage 'http://www.gnu.org/software/gdb/'
   url 'http://ftpmirror.gnu.org/gdb/gdb-7.6.tar.bz2'
@@ -7,13 +20,25 @@ class Gdb < Formula
   sha1 'b64095579a20e011beeaa5b264fe23a9606ee40f'
 
   depends_on 'readline'
+  if build.include? 'with-brewed-python'
+    depends_on UniversalBrewedPython
+  end
+
+  option 'with-brewed-python', 'Use the Homebrew version of Python'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--with-python=/usr",
-                          "--with-system-readline"
+    args = ["--prefix=#{prefix}",
+            "--disable-debug",
+            "--disable-dependency-tracking",
+            "--with-system-readline"]
+
+    if build.include? 'with-brewed-python'
+      args << "--with-python=#{HOMEBREW_PREFIX}"
+    else
+      args << "--with-python=/usr"
+    end
+
+    system "./configure", *args
     system "make"
     system "make install"
   end
