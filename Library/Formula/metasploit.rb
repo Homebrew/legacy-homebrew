501c4235fd8cec3ca946c60bb044193100ed1de4
Austin Seipp <aseipp@pobox.com>
2013-02-16 01:34:32 -0600

    Metasploit 4.5.0-2013021301
    Closes #17878.
    
    Signed-off-by: Austin Seipp <aseipp@pobox.com>
    Closes #17878.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/metasploit.rb
+++ Library/Formula/metasploit.rb
@@ -2,11 +2,10 @@ require 'formula'
 
 class Metasploit < Formula
   homepage 'http://www.metasploit.com/framework/'
-  url 'https://github.com/rapid7/metasploit-framework/archive/2013010902.tar.gz'
-  version '4.5.0-2013010902'
-  sha1 '2bcde6e6209e428fb18195c381597d462b00acf5'
-
   head 'https://github.com/rapid7/metasploit-framework.git'
+  url 'https://github.com/rapid7/metasploit-framework/archive/2013021301.tar.gz'
+  version '4.5.0-2013021301'
+  sha1 '63934228ec316ca6c2313b151eab6e3bd91f5508'
 
   def install
     libexec.install Dir["msf*"]
