c7732bc57cbdb0a2c9cbcdb1093fab308fff3568
Jacob Appelbaum <jacob@appelbaum.net>
2013-02-16 11:30:28 -0800

    use a newer upstream version that understands a custom prefix

--- Library/Formula/makedepend.rb
+++ Library/Formula/makedepend.rb
@@ -3,10 +3,11 @@ require 'formula'
 class Makedepend < Formula
   homepage 'https://www.github.com/ioerror/makedepend/'
   head 'https://www.github.com/ioerror/makedepend.git', :using => :git,  :branch => 'master'
-  url 'https://www.github.com/ioerror/makedepend.git', :using => :git,  :branch => 'master', tag: => '0.1'
+  url 'https://github.com/ioerror/makedepend/archive/0.2.tar.gz'
+  sha1 '85b3b2a53b401b8bd5be19443960acb1367d3055'
 
   def install
-    system "make install"
+    system "DESTDIR=#{prefix} make install"
   end
 
   def test
