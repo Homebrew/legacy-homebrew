5bbedf725a59365eaed164eec307a519fd3f4698
Adam Vandenberg <flangy@gmail.com>
2013-02-17 11:16:30 -0800

    vim 7.3.822

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -4,8 +4,8 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-820'
-  version '7.3.820'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-822'
+  version '7.3.822'
 
   head 'https://vim.googlecode.com/hg/'
 
