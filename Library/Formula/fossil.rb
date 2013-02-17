54ab0e3f9d1097d4cdb6391b1e5331690d3ec0d8
Jack Nagel <jacknagel@gmail.com>
2013-02-16 18:29:33 -0600

    fossil 1.25

--- Library/Formula/fossil.rb
+++ Library/Formula/fossil.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Fossil < Formula
   homepage 'http://www.fossil-scm.org/'
-  url 'http://www.fossil-scm.org/download/fossil-src-20121022124804.tar.gz'
-  sha1 '9130ae9f17299b490e45dc25c20be81b140ea9cb'
-  version '1.24'
+  url 'http://www.fossil-scm.org/download/fossil-src-20130216000435.tar.gz'
+  sha1 '16bf2d05ec62cd704b2cbc2bb9388de2a1b5ac97'
+  version '1.25'
 
   head 'fossil://http://www.fossil-scm.org/'
 
