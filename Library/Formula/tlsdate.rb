22bff113303c13442c9d93ed361951f9a2fb937d
Jacob Appelbaum <jacob@appelbaum.net>
2013-02-16 22:46:32 -0800

    Update after review of pull request from adamv

--- Library/Formula/tlsdate.rb
+++ Library/Formula/tlsdate.rb
@@ -3,9 +3,8 @@ require 'formula'
 class Tlsdate < Formula
   homepage 'https://www.github.com/ioerror/tlsdate/'
   url 'https://github.com/ioerror/tlsdate/archive/tlsdate-0.0.5.tar.gz'
-  version '0.0.5'
-  sha1sum 'b933d777c5d54ac4a5824f749d819ca884a1a2a9'
-  head 'https://www.github.com/ioerror/tlsdate.git', :using => :git, :branch => 'master'
+  sha1 'b933d777c5d54ac4a5824f749d819ca884a1a2a9'
+  head 'https://www.github.com/ioerror/tlsdate.git', :branch => 'master'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
