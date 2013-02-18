615f69684e556e528dd94dd58b6c6342ea50d2f9
Mike McQuaid <mike@mikemcquaid.com>
2013-02-18 10:37:38 +0000

    pkg-config 0.28 bottles

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -6,6 +6,12 @@ class PkgConfig < Formula
   mirror 'http://fossies.org/unix/privat/pkg-config-0.28.tar.gz'
   sha256 '6b6eb31c6ec4421174578652c7e141fdaae2dabad1021f420d8713206ac1f845'
 
+  bottle do
+    sha1 'fe503c105b952d04863931da96f03b0339d7fbb0' => :mountain_lion
+    sha1 'd483a4ed0eca160fb5c69d3a0a4011c3393ee2a3' => :lion
+    sha1 'e5859bdb284d4810057fd8cace202c60af40885f' => :snow_leopard
+  end
+
   def install
     paths = %W[
         #{HOMEBREW_PREFIX}/lib/pkgconfig
