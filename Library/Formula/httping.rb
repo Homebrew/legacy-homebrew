33ccd80e0ff7da60354fc4dd1d28513d55645f9a
David Pärsson <david@parsson.se>
2013-02-19 17:15:28 +0100

    httping 1.5.8
    Closes #17948.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/httping.rb
+++ Library/Formula/httping.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Httping < Formula
   homepage 'http://www.vanheusden.com/httping/'
-  url 'http://www.vanheusden.com/httping/httping-1.5.6.tgz'
-  sha1 '146d114ff6359556ae5f720964aa091b39dbb506'
+  url 'http://www.vanheusden.com/httping/httping-1.5.8.tgz'
+  sha1 'd736194395aed0493c4b7e8b7c2a01fe513d17be'
 
   def patches
     # fixes conflicting definitions of strdup()
