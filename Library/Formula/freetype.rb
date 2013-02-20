d31c5bbc60d6f9f896e15f24d900eb120d12035f
Stefan <hydra_github@me.com>
2013-02-20 11:06:55 +0100

    freetype 2.4.11
    Closes #17965.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/freetype.rb
+++ Library/Formula/freetype.rb
@@ -2,17 +2,12 @@ require 'formula'
 
 class Freetype < Formula
   homepage 'http://www.freetype.org'
-  url 'http://downloads.sf.net/project/freetype/freetype2/2.4.10/freetype-2.4.10.tar.gz'
-  sha1 '44dba26ff965b1cd1c62e480fdefaeca62ed33da'
+  url 'http://downloads.sf.net/project/freetype/freetype2/2.4.11/freetype-2.4.11.tar.gz'
+  sha1 'a8373512281f74a53713904050e0d71c026bf5cf'
 
   keg_only :provided_pre_mountain_lion
 
   option :universal
-
-  bottle do
-    # Included with X11 so no bottle needed before Mountain Lion.
-    sha1 '02121cf64c189e61117dc9bef3de856f296761af' => :mountainlion
-  end
 
   def install
     ENV.universal_binary if build.universal?
