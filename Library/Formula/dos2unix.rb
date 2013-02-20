e776424eae0170b1f59fd127989ef0028f98f1c5
Brett Koonce <koonce@gmail.com>
2013-02-19 18:18:51 -0600

    dos2unix 6.0.3

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dos2unix < Formula
   homepage 'http://waterlan.home.xs4all.nl/dos2unix.html'
-  url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.2.tar.gz'
-  sha1 '270a07b281ce293414641221623a5c4fc9540252'
+  url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.3.tar.gz'
+  sha1 '2c9ae9177fcb0d8b63d141f73a75c928c38ace01'
 
   depends_on 'gettext'
 
