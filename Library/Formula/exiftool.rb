acd934129de763ccf5402138712dd5b449e506ac
Misty De Meo <mistydemeo@gmail.com>
2013-02-21 16:15:27 -0600

    exiftool 9.20

--- Library/Formula/exiftool.rb
+++ Library/Formula/exiftool.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Exiftool < Formula
   homepage 'http://www.sno.phy.queensu.ca/~phil/exiftool/index.html'
-  url 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-9.18.tar.gz'
-  sha1 'f8c5d59d28e8d237adcdd8ce4cbc7be843c92021'
+  url 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-9.20.tar.gz'
+  sha1 '0fca652b7a96df5c32b82692c4d0cba4b104d559'
 
   def install
     system "perl", "Makefile.PL"
