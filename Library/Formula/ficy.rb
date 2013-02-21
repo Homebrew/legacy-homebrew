b5aa587ae7f3c48156d2e4106ee87cd8d99e2fa1
Christopher Thielen <cthielen@gmail.com>
2013-02-21 14:43:28 -0800

    fIcy formula up to standards: comments removed, proper usage of prefix.install

--- Library/Formula/ficy.rb
+++ Library/Formula/ficy.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Ficy < Formula
   homepage 'http://www.thregr.org/~wavexx/software/fIcy/'
   url 'http://www.thregr.org/~wavexx/software/fIcy/releases/fIcy-1.0.18.tar.gz'
@@ -12,13 +9,10 @@ class Ficy < Formula
 
   def install
     system "make"
-    system "cp fIcy fPls fResync #{prefix}"
+    prefix.install 'fIcy', 'fPls', 'fResync'
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test fIcy`.
     system "fIcy"
     system "fPls"
     system "fResync"
