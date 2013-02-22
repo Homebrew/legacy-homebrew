d88fe2fefa84aadc3f688cf1bb7633865068dcc4
Keith Schulze <keith.schulze@gmail.com>
2013-02-23 01:14:39 +1100

    dart 18717
    Updating to the latest release of the dart-sdk (18717)

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -4,14 +4,14 @@ class Dart < Formula
   homepage 'http://www.dartlang.org/'
 
   if MacOS.prefer_64_bit?
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/18115/dartsdk-macos-64.zip'
-    sha1 '3cd57696a33e34cc2f33e1bb529c4bd4b59ebe6c'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/18717/dartsdk-macos-64.zip'
+    sha1 '2ef8841fdf21c29f97813863c4c20d3952789e36'
   else
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/18115/dartsdk-macos-32.zip'
-    sha1 '01c7f8022681e65814ed5e7751ad1a4dba686a04'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/18717/dartsdk-macos-32.zip'
+    sha1 '360e7eea55b8adda3c2635df964d75664eb82ad2'
   end
 
-  version '18115'
+  version '18717'
 
   def install
     libexec.install Dir['*']
