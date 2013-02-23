4654abeada657d9ef3afcd0a8e0143858312d56b
Luke Campbell <luke.s.campbell@gmail.com>
2013-02-22 23:26:19 -0500

    Removes comments/whitespace and fixes build
    The three dependencies required => :build
    
    Audit still says "trailing whitespace"

--- Library/Formula/libgswteos-10.rb
+++ Library/Formula/libgswteos-10.rb
@@ -1,34 +1,24 @@
 require 'formula'
-
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 #
 # TEOS-10 V3.0 GSW Oceanographic Toolbox in C
 # 
-
 class Libgswteos10 < Formula
   homepage ''
   url 'https://github.com/lukecampbell/gsw-teos/tarball/v3.0r3'
   homepage 'http://www.teos-10.org/'
   version '3.0'
   sha1 'aa58bdfbe92d40504713cd9b8ed39f59c8d04627'
-
-  depends_on 'autoconf'
-  depends_on 'automake'
-  depends_on 'libtool'
-
-
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
   def install
-
     system "bash ./autogen.sh"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
     system "make install" # if this fails, try separate make/make install steps
   end
-
   def test
-    system "#{prefix}/bin/gsw_check_functions"
+    system "#{bin}/gsw_check_functions"
   end
 end
-
