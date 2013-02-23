0a40fe630ccf8f7391b4ab5c4320a7dfa3af0cec
Jacob Appelbaum <jacob@appelbaum.net>
2013-02-22 19:01:09 -0800

    This fixes builds --HEAD for a known compiler issue

--- Library/Formula/go.rb
+++ Library/Formula/go.rb
@@ -13,6 +13,11 @@ class Go < Formula
   option 'cross-compile-all', "Build the cross-compilers and runtime support for all supported platforms"
   option 'cross-compile-common', "Build the cross-compilers and runtime support for darwin, linux and windows"
 
+  fails_with :clang do
+    build 425
+    cause "clang: error: no such file or directory: 'libgcc.a'"
+  end
+
   def install
     # install the completion scripts
     (prefix/'etc/bash_completion.d').install 'misc/bash/go' => 'go-completion.bash'
