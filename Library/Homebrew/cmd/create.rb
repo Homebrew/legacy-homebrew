9e49a9a8ba19c63aec9142352e5b786c26a5c5fc
Mike McQuaid <mike@mikemcquaid.com>
2013-02-23 08:42:34 +0000

    brew create: use test DSL

--- Library/Homebrew/cmd/create.rb
+++ Library/Homebrew/cmd/create.rb
@@ -146,7 +146,9 @@ class FormulaCreator
         system "make install" # if this fails, try separate make/make install steps
       end
 
-      def test
+      test do
+        # `test do` will create, run in and delete a temporary directory.
+        #
         # This test will fail and we won't accept that! It's enough to just replace
         # "false" with the main program this formula installs, but it'd be nice if you
         # were more thorough. Run the test with `brew test #{name}`.
