3913ed57fcac534998d6e9965e6d53a1b24b0113
Rob Hoelz <rob@hoelz.ro>
2013-02-25 17:58:00 +0100

    Rakudo Star 2013.02
    Closes #18079.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rakudo-star.rb
+++ Library/Formula/rakudo-star.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RakudoStar < Formula
   homepage 'http://rakudo.org/'
-  url 'http://rakudo.org/downloads/star/rakudo-star-2013.01.tar.gz'
-  sha256 'dc1e7d0d5c8156f5399da968eaac7f8b0166b8067309e372372e6a43fd3e802b'
+  url 'http://rakudo.org/downloads/star/rakudo-star-2013.02.tar.gz'
+  sha256 '99c9b033065c2f8e4e847182677b71cbc1f7ba5f509069d3c4c5edf606e9d92a'
 
   depends_on 'gmp' => :optional
   depends_on 'icu4c' => :optional
