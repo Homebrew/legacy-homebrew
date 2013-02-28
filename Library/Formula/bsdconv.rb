e47116db3d4541667391b06ccb8980032d60a65f
Lin Jen-Shin <godfat@godfat.org>
2013-02-28 11:34:01 +0800

    bsdconv 9.1
    Closes #18129.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/bsdconv.rb
+++ Library/Formula/bsdconv.rb
@@ -1,19 +1,20 @@
+# encoding: utf-8
 require 'formula'
 
 class Bsdconv < Formula
   homepage 'https://github.com/buganini/bsdconv'
-  url 'https://github.com/buganini/bsdconv/tarball/9.0'
-  sha1 '5b82f097575c9bbb0a1b809846c23f582da72bef'
+  url 'https://github.com/buganini/bsdconv/tarball/9.1'
+  sha1 '282fa6f2bcb6ef3b73f19ef269ecc917edcde729'
 
   head 'https://github.com/buganini/bsdconv.git'
 
   def install
-    ENV.j1 # Library must be built before the codec tables are generated
     system "make", "PREFIX=#{prefix}"
     system "make", "PREFIX=#{prefix}", "install"
   end
 
   def test
-    system "#{bin}/bsdconv"
+    printf = 'printf "\263\134\245\134\273\134"'
+    system "test `#{printf} | #{bin}/bsdconv big5:utf-8` = '許功蓋'"
   end
 end
