5ec779e73bba837aefdc0813e14df81f87be276c
Oliver Erxleben <oliver.erxleben@me.com>
2013-02-28 19:59:34 +0100

    Update adobe-ari-sdk to latest version 3.6
    Checksum failed for 3.5, I updated 

--- Library/Formula/adobe-air-sdk.rb
+++ Library/Formula/adobe-air-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AdobeAirSdk < Formula
   homepage 'http://www.adobe.com/products/air/sdk/'
-  url 'http://airdownload.adobe.com/air/mac/download/3.4/AdobeAIRSDK.tbz2'
-  sha1 '3bc90f619d9f1620187538b6f591675c7d6011d5'
+  url 'http://download.macromedia.com/air/mac/download/3.6/AIRSDK_Compiler.tbz2'
+  sha1 '669fa27502df8939a513ced7636b974e1e753302'
 
   def startup_script name
     (bin+name).write <<-EOS.undent
