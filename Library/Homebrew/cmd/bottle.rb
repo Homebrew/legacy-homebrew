82ecb69a53166c8c2fa0e507ac32c86241b66343
Mike McQuaid <mike@mikemcquaid.com>
2013-02-28 22:14:59 +0000

    Add non-/usr/local bottles support.
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Homebrew/cmd/bottle.rb
+++ Library/Homebrew/cmd/bottle.rb
@@ -20,12 +20,22 @@ module Homebrew extend self
 
     HOMEBREW_CELLAR.cd do
       ohai "Bottling #{f.name} #{f.version}..."
+      bottle_relocatable = !quiet_system(
+        'grep', '--recursive', '--quiet', '--max-count=1',
+                HOMEBREW_CELLAR, "#{f.name}/#{f.version}")
+      cellar = nil
+      if bottle_relocatable
+        cellar = ':any'
+      elsif HOMEBREW_CELLAR.to_s != '/usr/local/Cellar'
+        cellar = "'#{HOMEBREW_CELLAR}'"
+      end
       # Use gzip, faster to compress than bzip2, faster to uncompress than bzip2
       # or an uncompressed tarball (and more bandwidth friendly).
       safe_system 'tar', 'czf', bottle_path, "#{f.name}/#{f.version}"
       sha1 = bottle_path.sha1
       puts "./#{filename}"
       puts "bottle do"
+      puts "  cellar #{cellar}" if cellar
       puts "  revision #{bottle_revision}" if bottle_revision > 0
       puts "  sha1 '#{sha1}' => :#{MacOS.cat}"
       puts "end"
