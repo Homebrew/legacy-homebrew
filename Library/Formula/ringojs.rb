11f1b7b950323c1422e3df3607d0f527eba20c77
herrernst <herr.ernst@gmail.com>
2013-03-01 14:34:12 +0100

    don't remove empty packages dir
    prevent removal of 'packages' dir, it is needed if you want install packages later

--- Library/Formula/ringojs.rb
+++ Library/Formula/ringojs.rb
@@ -4,6 +4,7 @@ class Ringojs < Formula
   homepage 'http://ringojs.org'
   url 'http://ringojs.org/downloads/ringojs-0.9.tar.gz'
   sha1 '1b0b7efcad323d5dd7ce3b1dbdfc079914e8713a'
+  skip_clean 'libexec/packages'
 
   def install
     rm Dir['bin/*.cmd']
