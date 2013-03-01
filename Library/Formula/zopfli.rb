ee9ff009a2394b92a69dda95c0261d22c318fd71
Christopher Lord <christopherlord@gmail.com>
2013-02-28 22:46:40 -0700

    remove some boilerplate comments

--- Library/Formula/zopfli.rb
+++ Library/Formula/zopfli.rb
@@ -14,11 +14,6 @@ class Zopfli < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test zopfli`.
     system "zopfli"
   end
 end
