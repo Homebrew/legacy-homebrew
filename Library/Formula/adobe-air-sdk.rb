2a4099fe962d0ba4ce9884c7fae82a736be687ea
bjosip <josip@infinum.hr>
2013-03-01 10:37:47 +0100

    Adobe AIR SDK: Fix SHA1 for v3.5.0.1060

--- Library/Formula/adobe-air-sdk.rb
+++ Library/Formula/adobe-air-sdk.rb
@@ -3,7 +3,7 @@ require 'formula'
 class AdobeAirSdk < Formula
   homepage 'http://www.adobe.com/products/air/sdk/'
   url 'http://airdownload.adobe.com/air/mac/download/3.5/AdobeAIRSDK.tbz2'
-  sha1 'a23ffc39d836a3e6ffc260fccb724f1649389ae9'
+  sha1 'b407b57d98370a3e6a66d446bb48f6b99dff3c3e'
 
   def install
     libexec.install Dir['*']
