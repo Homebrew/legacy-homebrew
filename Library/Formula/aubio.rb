7e24eee9ede9708cff1e1e740756104f099b0202
Erik Kallen <info@erikkallen.nl>
2013-03-01 14:53:03 +0100

    Added formula for aubio library
    Added the formula for the aubio library. Built sucessfully on OSX 10.8.2

---
+++ Library/Formula/aubio.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Aubio < Formula
+  homepage 'http://aubio.org/'
+  url 'http://aubio.org/pub/aubio-0.3.2.tar.gz'
+  sha1 '8ef7ccbf18a4fa6db712a9192acafc9c8d080978'
+  depends_on 'pkg-config' => :build
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    ENV.j1
+    system "make install"
+  end
+end
