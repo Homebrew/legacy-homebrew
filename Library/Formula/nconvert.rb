35b5da1a0a5b6e8bf18cc34c3b4063da06f573cf
Johanns Gregorian <io@jsani.com>
2013-03-02 01:43:42 -0800

    Removed comments + added version number

--- Library/Formula/nconvert.rb
+++ Library/Formula/nconvert.rb
@@ -1,13 +1,12 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Nconvert < Formula
   homepage 'http://www.xnview.com/en/nconvert.html'
   url 'http://download.xnview.com/NConvert-macosx.tgz'
   sha1 '11b09a62c1c7f3feeb53718e46a70652074e077b'
 
+  version '6.17'
+  
   def install
     bin.install 'nconvert'
   end
