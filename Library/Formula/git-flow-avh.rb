2f181b5cee6df12b6fe478c4ebd1a114fffbfc85
Justin de Vesine <ziz@ziz.org>
2013-03-04 16:16:17 -0700

    git-flow-avh: add petervanderdoes' git-flow-completion
    Closes #17809.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/git-flow-avh.rb
+++ Library/Formula/git-flow-avh.rb
@@ -1,5 +1,13 @@
 require 'formula'
 
+class GitFlowAvhCompletion < Formula
+  homepage 'https://github.com/petervanderdoes/git-flow-completion'
+  url 'https://github.com/petervanderdoes/git-flow-completion/tarball/0.4.2'
+  sha1 '0a36ae6fda83b6ba0251f4eea3a957f94f5467b7'
+
+  head 'https://github.com/petervanderdoes/git-flow-completion.git', :branch => 'develop'
+end
+
 class GitFlowAvh < Formula
   homepage 'https://github.com/petervanderdoes/gitflow'
   url 'https://github.com/petervanderdoes/gitflow/archive/1.5.0.tar.gz'
@@ -13,6 +21,11 @@ class GitFlowAvh < Formula
 
   def install
     system "make", "prefix=#{prefix}", "install"
+
+    GitFlowAvhCompletion.new('git-flow-avh-completion').brew do
+      (prefix+'etc/bash_completion.d').install "git-flow-completion.bash"
+      (share+'zsh/site-functions').install "git-flow-completion.zsh"
+    end
   end
 
   def caveats; <<-EOS.undent
