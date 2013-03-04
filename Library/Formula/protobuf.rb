73a29a98606231b15c763ba734c2310a901910d2
Joshua Foster <jhawk28@gmail.com>
2013-03-04 16:15:59 -0500

    protobuf 2.5.0
    Closes #18241.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/protobuf.rb
+++ Library/Formula/protobuf.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Protobuf < Formula
   homepage 'http://code.google.com/p/protobuf/'
-  url 'http://protobuf.googlecode.com/files/protobuf-2.4.1.tar.bz2'
-  sha1 'df5867e37a4b51fb69f53a8baf5b994938691d6d'
+  url 'http://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2'
+  sha1 '62c10dcdac4b69cc8c6bb19f73db40c264cb2726'
 
   option :universal
 
