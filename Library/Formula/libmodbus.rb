1ec9960067296d971542d51ee1c297fcc80c0a55
Aaron Huslage <huslage@tethr.org>
2013-03-05 13:25:18 -0500

    libmodbus 3.0.3

--- Library/Formula/libmodbus.rb
+++ Library/Formula/libmodbus.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Libmodbus < Formula
   homepage 'http://libmodbus.org'
   url 'https://github.com/downloads/stephane/libmodbus/libmodbus-3.0.3.tar.gz'
@@ -14,12 +11,4 @@ class Libmodbus < Formula
     system "make install"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test libmodbus`.
-    system "false"
-  end
-end
+ end
