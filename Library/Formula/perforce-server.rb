4a58b0d37cbf3e6ddc2ee71752001673a1a249a6
Katherine G. Pe <katz@bridgeutopiaweb.com>
2013-03-05 19:01:06 +0800

    Fix Perforce server installation error: SHA1 mismatch

--- Library/Formula/perforce-server.rb
+++ Library/Formula/perforce-server.rb
@@ -6,7 +6,7 @@ class PerforceServer < Formula
   if MacOS.prefer_64_bit?
     url 'http://filehost.perforce.com/perforce/r12.2/bin.darwin90x86_64/p4d'
     version '2012.2.551823-x86_64'
-    sha1 'df326fc5d88bdd9ab4a6fe77a86f7100b0935958'
+    sha1 'e404c431df55d382b16d3f61a50261735865300f'
   else
     url 'http://filehost.perforce.com/perforce/r12.2/bin.darwin90x86/p4d'
     version '2012.2.551823-x86'
