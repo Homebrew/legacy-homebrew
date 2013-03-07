61e066009e569920a3113eed63b0ea7d56aba377
Tyler Brock <tyler.brock@gmail.com>
2013-03-07 13:28:42 -0500

    mongodb-2.4.0-rc2

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -22,9 +22,9 @@ class Mongodb < Formula
   version '2.2.3-x86_64'
 
   devel do
-    url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.4.0-rc1.tgz'
-    sha1 'faf65dc63560e100584cca0d2866b1c4e755fdb6'
-    version '2.4.0-rc1-x86_64'
+    url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.4.0-rc2.tgz'
+    sha1 '651e8b59c38367e4038449104c6e884f8216ac2c'
+    version '2.4.0-rc2-x86_64'
   end
 
   depends_on SixtyFourBitRequired
