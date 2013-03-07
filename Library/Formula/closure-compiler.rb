04c3dcc10e07d512a9d7f47aab46af7317c07da1
Ian Lancaster <ian@bitomics.com>
2013-03-06 22:26:46 -0500

    closure-compiler 20130227
    Switched HEAD to git and url to latest release.

--- Library/Formula/closure-compiler.rb
+++ Library/Formula/closure-compiler.rb
@@ -2,11 +2,12 @@ require 'formula'
 
 class ClosureCompiler < Formula
   homepage 'http://code.google.com/p/closure-compiler/'
-  # Use an SVN download to get the externals as well
-  url 'svn+http://closure-compiler.googlecode.com/svn/trunk/', :revision => '2388'
-  version '20121212'
+  # switched to archive over tags from git
+  url 'https://closure-compiler.googlecode.com/files/compiler-20130227.tar.gz'
+  sha1 '0becbcb3b4f23162f4df50d2996e1a0765f75b75'
+  version '20130227'
 
-  head 'svn+http://closure-compiler.googlecode.com/svn/trunk/'
+  head 'https://code.google.com/p/closure-compiler/', :using => :git
 
   def install
     system "ant", "clean"
