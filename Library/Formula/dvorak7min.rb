e9035a2c39b17d600c113ad640b90aeb6b88eb0c
Yurii Samsoniuk <mr.sigito@gmail.com>
2013-03-10 02:08:01 +0200

    Added new source url.
    Changed source root on debian doxygen site.

--- Library/Formula/dvorak7min.rb
+++ Library/Formula/dvorak7min.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Dvorak7min < Formula
-  homepage 'https://github.com/yaychris/dvorak7min'
-  url 'https://github.com/yaychris/dvorak7min/archive/700f80569e1267ae42c8349bc5b6337387c6b5d0.tar.gz'
+  homepage 'http://dvorak7min.sourcearchive.com/'
+  url 'http://dvorak7min.sourcearchive.com/downloads/1.6.1-13.1/dvorak7min_1.6.1.orig.tar.gz'
   version '1.6.1'
   sha1 '83f7ec4eba3fa33cf0547a8614ee02e50ff21c81'
 
