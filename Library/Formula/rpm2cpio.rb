ab0c0cb1bf7fd7e64e6d23b5d638eeb0409c0088
Jack Nagel <jacknagel@gmail.com>
2013-03-10 14:47:57 -0500

    rpm2cpio: adjust version to match upstream

--- Library/Formula/rpm2cpio.rb
+++ Library/Formula/rpm2cpio.rb
@@ -3,11 +3,9 @@ require 'formula'
 class Rpm2cpio < Formula
   homepage 'http://svnweb.freebsd.org/ports/head/archivers/rpm2cpio/'
   url 'http://svnweb.freebsd.org/ports/head/archivers/rpm2cpio/files/rpm2cpio?revision=259745'
-  # cvs2svn:cvs-rev	1.4
-  version '1.4'
+  version '1.3'
   sha1 '7bd6e848eed9444a4dacf9759cc195ca56ab19b2'
 
-  # Uses xz and lzma tools at runtime
   depends_on 'xz'
 
   def install
