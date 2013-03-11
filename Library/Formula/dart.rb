3fc8dee4fdd2e0c7005be8039921a40573a1867b
Yehor Lvivski <lvivski@gmail.com>
2013-03-11 14:24:54 +0200

    dart 19425
    Closes #18392.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -4,14 +4,14 @@ class Dart < Formula
   homepage 'http://www.dartlang.org/'
 
   if MacOS.prefer_64_bit?
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/18717/dartsdk-macos-64.zip'
-    sha1 '2ef8841fdf21c29f97813863c4c20d3952789e36'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/19425/dartsdk-macos-64.zip'
+    sha1 '7eb284106b7ace99963c30d68f292227ff785363'
   else
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/18717/dartsdk-macos-32.zip'
-    sha1 '360e7eea55b8adda3c2635df964d75664eb82ad2'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/19425/dartsdk-macos-32.zip'
+    sha1 'e19497d322603276e46bb489860f1f51a45b04ad'
   end
 
-  version '18717'
+  version '19425'
 
   def install
     libexec.install Dir['*']
