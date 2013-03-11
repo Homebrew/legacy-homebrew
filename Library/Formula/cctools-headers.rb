2f3b109750bc93fa07124846a8c36c63aef7128b
Misty De Meo <mistydemeo@gmail.com>
2013-03-10 19:33:21 -0500

    cctools-headers 836

---
+++ Library/Formula/cctools-headers.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+# ld64 requires an updated mach/machine.h to build
+class XnuHeaders < Formula
+  url 'http://opensource.apple.com/tarballs/xnu/xnu-2050.18.24.tar.gz'
+  sha1 '3a2a0b3629cb215b17aca3bb365b8b10b8b408fe'
+end
+
+# These headers are newer than the version of cctools we provide
+class CctoolsHeaders < Formula
+  homepage 'http://opensource.apple.com/'
+  url 'http://www.opensource.apple.com/tarballs/cctools/cctools-836.tar.gz'
+  sha1 'fe2aab3f527adf6c775462ac045699a150dc7f82'
+
+  keg_only :provided_by_osx
+
+  def install
+    # only supports DSTROOT, not PREFIX
+    inreplace "include/Makefile", "/usr/include", "/include"
+    system "make", "installhdrs", "DSTROOT=#{prefix}", "RC_ProjectSourceVersion=#{version}"
+    # installs some headers we don't need to DSTROOT/usr/local/include
+    (prefix/'usr').rmtree
+
+    XnuHeaders.new.brew {(include/'mach').install 'osfmk/mach/machine.h'}
+  end
+end
