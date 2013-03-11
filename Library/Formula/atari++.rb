eaaf1cab460b1de2567ab599ca3a56ea72cc1551
Jochen Schäfer <jochen@joschs-robotics.de>
2013-03-11 22:48:23 +0100

    Rearranged things, so that SDL is now recommended and X11 is optional. Secondly, you have to select at least one Frontend.

--- Library/Formula/atari++.rb
+++ Library/Formula/atari++.rb
@@ -5,20 +5,25 @@ class Atarixx < Formula
   url 'http://www.xl-project.com/download/atari++_1.71.tar.gz'
   sha1 '6d18494068bf491077cff32de514a1118bb133b1'
 
-  depends_on :x11 => :recommended
-  depends_on 'sdl' => :optional
+  depends_on :x11 => :optional
+  depends_on 'sdl' => :recommended
   option 'with-curses'
   
   def install
     options = []
     options << "--disable-CURSES" unless build.include? 'with-curses'
     options << "--disable-SDL" unless build.include? 'with-sdl'
-    options << "--disable-X11" if build.include? 'without-x11'
+    options << "--disable-X11" unless build.include? 'with-x11'
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    if options.length == 3
+      onoe "At least choose one frontend of SDL (recommended), X11 or Curses"
+      exit
+    else
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", options
-    system "make"
-    system "make install" # if this fails, try separate make/make install steps
+      system "make"
+      system "make install" # if this fails, try separate make/make install steps
+    end
   end
 
   def test
