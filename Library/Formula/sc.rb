260505dcbf709e341ea239943113e1f1d55f3311
Daniel Bretoi <daniel@bretoi.com>
2013-03-11 19:01:44 -0700

    remove comments. spaces instead of tabs

--- Library/Formula/sc.rb
+++ Library/Formula/sc.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Sc < Formula
   homepage 'http://www.boutell.com/lsm/lsmbyid.cgi/000293'
   url 'ftp://metalab.unc.edu/pub/Linux/apps/financial/spreadsheet/sc-7.16.tar.gz'
@@ -16,16 +13,13 @@ class Sc < Formula
 
     inreplace  "Makefile", 'prefix=/usr', "prefix=#{prefix}"
     inreplace  "Makefile", 'install: $(EXDIR)/$(name) $(EXDIR)/$(name)qref $(EXDIR)/p$(name) \
-	 $(LIBDIR)/tutorial $(MANDIR)/$(name).$(MANEXT) \
-	 $(MANDIR)/p$(name).$(MANEXT)', 'install: $(EXDIR)/$(name) $(EXDIR)/$(name)qref $(EXDIR)/p$(name) \
-	 $(LIBDIR)/tutorial'
+   $(LIBDIR)/tutorial $(MANDIR)/$(name).$(MANEXT) \
+   $(MANDIR)/p$(name).$(MANEXT)', 'install: $(EXDIR)/$(name) $(EXDIR)/$(name)qref $(EXDIR)/p$(name) \
+   $(LIBDIR)/tutorial'
     system "make install" # if this fails, try separate make/make install steps
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test sc`.
     system "sc"
   end
 end
