52e0cdefd55718711fedf2123e3d1c025057ac5e
n0rmrx <n0rmrx@0xbot.com>
2013-03-12 14:50:58 +0800

    solr 4.2.0
    Closes #18419.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/solr.rb
+++ Library/Formula/solr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Solr < Formula
   homepage 'http://lucene.apache.org/solr/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.1.0/solr-4.1.0.tgz'
-  sha1 '89cce8c7c8d68df20f009a7c04ede423d6fa7c84'
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.2.0/solr-4.2.0.tgz'
+  sha1 '3525a424be3c7f5de27ea1ab60b267cc0670b6c4'
 
   def script; <<-EOS.undent
     #!/bin/sh
