fb846110b32f70de43a32810b25d96e5af97be30
Alexander Pakulov <a.pakulov@gmail.com>
2013-03-14 22:47:01 +0300

    Formula for jmxtrans

---
+++ Library/Formula/jmxtrans.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Jmxtrans < Formula
+  homepage 'https://github.com/jmxtrans/jmxtrans'
+  url 'https://github.com/downloads/jmxtrans/jmxtrans/jmxtrans-20121016-151320-36564abc7e.zip'
+  sha1 '2b2dc4727de16a30e8b7ac84b9bb4cb7b1ea4a6f'
+  version '20121016'
+
+  def install
+    prefix.install_metafiles
+    libexec.install Dir['*']
+
+    (libexec+'jmxtrans.sh').chmod 0755
+    bin.install_symlink "#{libexec}/jmxtrans.sh" => "jmxtrans"
+  end
+end
