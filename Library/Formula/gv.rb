7958e72e9b61ed3d00f1d8e56128329d16cad6fc
Jack Nagel <jacknagel@gmail.com>
2013-03-18 13:59:04 -0500

    gv: depends on pkg-config

--- Library/Formula/gv.rb
+++ Library/Formula/gv.rb
@@ -6,6 +6,7 @@ class Gv < Formula
   mirror 'http://ftp.gnu.org/gnu/gv/gv-3.7.4.tar.gz'
   sha1 'd5bc11a37136dff69248f943a632544a4036b63f'
 
+  depends_on 'pkg-config' => :build
   depends_on 'ghostscript' => 'with-x11'
   depends_on :x11 => '2.7.2'
 
