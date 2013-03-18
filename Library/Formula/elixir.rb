fb1b067b41792603d6ed486901635a2571eda441
Adam Vandenberg <flangy@gmail.com>
2013-03-17 22:31:08 -0700

    elixir: fix checksum
    Closes #18553.

--- Library/Formula/elixir.rb
+++ Library/Formula/elixir.rb
@@ -19,8 +19,8 @@ end
 
 class Elixir < Formula
   homepage 'http://elixir-lang.org/'
-  url  'https://github.com/elixir-lang/elixir/tarball/v0.8.1'
-  sha1 '6805ffbfc75652b23952512fc86188db91c7d9c6'
+  url  'https://github.com/elixir-lang/elixir/archive/v0.8.1.tar.gz'
+  sha1 'cb88d5c8c4543a71463fed3060f300d8933a9760'
 
   head 'https://github.com/elixir-lang/elixir.git'
 
