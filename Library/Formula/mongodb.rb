4e36c7c8c9acc184542ac3eb1a4abc6f7496276c
Shawn Jonnet <sjonnet19@gmail.com>
2013-03-18 12:05:35 -0300

    MongoDB 2.4.0
    Closes #18562.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -17,15 +17,9 @@ end
 
 class Mongodb < Formula
   homepage 'http://www.mongodb.org/'
-  url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.2.3.tgz'
-  sha1 '6b81469374eb8d1b209fcdd8111d4e654573d095'
-  version '2.2.3-x86_64'
-
-  devel do
-    url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.4.0-rc3.tgz'
-    sha1 '9ce2dc8b5fe8354131403c151167eb052df24a1f'
-    version '2.4.0-rc3-x86_64'
-  end
+  url 'http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.4.0.tgz'
+  sha1 'def7102b6f8198134beee6ddacee5ea057651ef3'
+  version '2.4.0-x86_64'
 
   depends_on SixtyFourBitRequired
 
