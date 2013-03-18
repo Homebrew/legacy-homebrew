9b218a4faee2a1bb50e2bb1599ec0f3058f03763
Lee Trout <lee@leetrout.com>
2013-03-18 20:47:24 -0300

    RethinkDB depends on boost
    Closes #18577.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rethinkdb.rb
+++ Library/Formula/rethinkdb.rb
@@ -5,6 +5,7 @@ class Rethinkdb < Formula
   url 'http://download.rethinkdb.com/dist/rethinkdb-1.4.0.tgz'
   sha1 'cc4f75f92aace746e5ea5fce68c0e831f6327890'
 
+  depends_on 'boost'
   depends_on 'v8'
   depends_on 'protobuf' => :build
 
