1fac6164aaf84242cdc310c6e05f98150871363d
Yehor Lvivski <lvivski@gmail.com>
2013-03-19 18:54:04 +0200

    dart 20193
    Closes #18591.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -4,14 +4,14 @@ class Dart < Formula
   homepage 'http://www.dartlang.org/'
 
   if MacOS.prefer_64_bit?
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/19425/dartsdk-macos-64.zip'
-    sha1 '7eb284106b7ace99963c30d68f292227ff785363'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/20193/dartsdk-macos-64.zip'
+    sha1 '8da75b3e6c44c47224a07fa670d2a9fb77326013'
   else
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/19425/dartsdk-macos-32.zip'
-    sha1 'e19497d322603276e46bb489860f1f51a45b04ad'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/20193/dartsdk-macos-32.zip'
+    sha1 'ebe196d40e4c8757897da8d5c195957bb25af635'
   end
 
-  version '19425'
+  version '20193'
 
   def install
     libexec.install Dir['*']
