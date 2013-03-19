1e40ba6b6b71ed786ecf086144b21c34606b3fc2
Jack Nagel <jacknagel@gmail.com>
2013-03-19 14:12:02 -0500

    erlang: use archive URL

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -23,8 +23,8 @@ end
 class Erlang < Formula
   homepage 'http://www.erlang.org'
   # Download tarball from GitHub; it is served faster than the official tarball.
-  url 'https://github.com/erlang/otp/tarball/OTP_R15B03-1'
-  sha1 '5ba866722de79956b06966c232490d32bb7ba0a6'
+  url 'https://github.com/erlang/otp/archive/OTP_R15B03-1.tar.gz'
+  sha1 '7843070f5d325f95ef13022fc416b22b6b14120d'
 
   head 'https://github.com/erlang/otp.git', :branch => 'dev'
 
