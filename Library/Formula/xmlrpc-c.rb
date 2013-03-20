7343ffc453cc680ac472969dc992c85d8e70f426
Chris Lucas <chris@chrisjlucas.com>
2013-03-20 01:41:55 -0700

    xmlrpc-c 1.25.21
    note: patch is no longer needed as of 1.25.08
    
    Closes #18603.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/xmlrpc-c.rb
+++ Library/Formula/xmlrpc-c.rb
@@ -2,15 +2,8 @@ require 'formula'
 
 class XmlrpcC < Formula
   homepage 'http://xmlrpc-c.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/sourceforge/xmlrpc-c/xmlrpc-c-1.16.44.tgz'
-  sha1 '181c54ac860698ed7cf4e3814c6f3c6dd46b2ce1'
-
-  def patches
-    # Backport patch for deprecated curl/types.h include, which is gone on 10.8
-    # On 10.6 and 10.7 it's empty, so could probably patch unconditionally
-    # see http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=636457#10
-    DATA if MacOS.version >= :mountain_lion
-  end
+  url 'http://svn.code.sf.net/p/xmlrpc-c/code/stable', :revision => 2452
+  version '1.25.21'
 
   def install
     ENV.deparallelize
@@ -18,43 +11,9 @@ class XmlrpcC < Formula
     system "./configure", "--disable-debug",
                           "--enable-libxml2-backend",
                           "--prefix=#{prefix}"
+
+    # xmlrpc-config.h cannot be found if only calling make install
+    system "make"
     system "make install"
   end
 end
-
-__END__
---- stable/lib/curl_transport/curltransaction.c 2011/03/26 19:32:28 2115
-+++ stable/lib/curl_transport/curltransaction.c 2011/07/09 17:47:44 2150
-@@ -17,7 +17,9 @@
- #include "version.h"
- 
- #include <curl/curl.h>
-+#ifdef NEED_CURL_TYPES_H
- #include <curl/types.h>
-+#endif
- #include <curl/easy.h>
- 
- #include "curlversion.h"
---- stable/lib/curl_transport/curlmulti.c 2011/07/09 17:41:18 2149
-+++ stable/lib/curl_transport/curlmulti.c 2011/07/09 17:47:44 2150
-@@ -19,7 +19,9 @@
- #endif
- 
- #include <curl/curl.h>
-+#ifdef NEED_CURL_TYPES_H
- #include <curl/types.h>
-+#endif
- #include <curl/easy.h>
- #include <curl/multi.h>
- 
---- stable/lib/curl_transport/xmlrpc_curl_transport.c 2011/07/09 17:41:18 2149
-+++ stable/lib/curl_transport/xmlrpc_curl_transport.c 2011/07/09 17:47:44 2150
-@@ -85,7 +85,9 @@
- #include "xmlrpc-c/time_int.h"
- 
- #include <curl/curl.h>
-+#ifdef NEED_CURL_TYPES_H
- #include <curl/types.h>
-+#endif
- #include <curl/easy.h>
- #include <curl/multi.h>
