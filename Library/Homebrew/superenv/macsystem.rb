06935cd77d28a4447ea33384263d23076e4a518a
Samuel John <github@SamuelJohn.de>
2013-03-20 18:20:18 +0100

    Better superenv support for Xcode elsewhere
    - The Library/ENV/4.3/xcrun shim now respects
      ENV['DEVELOPER_DIR'] instead assuming the location
      of /Applications/Xcode.app/Contents/Developer.
    - The env var DEVELOPER_DIR is set if it is not
      already. So, during superenv this var is always set
      and we no longer have to care about people with unset
      or wrongly set xcode-select stuff. This has been
      a major PITA in the past.
    - determine_developer_dir (which is used to set
      the DEVELOPER_DIR var) now uses MacOS::Xcode.prefix
      which is proven and very capable and uses splotlight
      correctly.
    - Replace (and remove) MacSystem.xcode43_developer_dir
      with Xcode.prefix
    
    Closes #18618

--- Library/Homebrew/superenv/macsystem.rb
+++ Library/Homebrew/superenv/macsystem.rb
@@ -1,7 +1,7 @@
 # new code because I don't really trust the Xcode code now having researched it more
 module MacSystem extend self
   def xcode_clt_installed?
-    File.executable? "/usr/bin/clang" and File.executable? "/usr/bin/lldb"
+    File.executable? "/usr/bin/clang" and File.executable? "/usr/bin/lldb" and File.executable? "/usr/bin/make"
   end
 
   def xcode43_without_clt?
@@ -11,16 +11,6 @@ module MacSystem extend self
   def x11_prefix
     @x11_prefix ||= %W[/opt/X11 /usr/X11
       #{MacOS.sdk_path}/usr/X11].find{|path| File.directory? "#{path}/include" }
-  end
-
-  def xcode43_developer_dir
-    @xcode43_developer_dir ||=
-      tst(ENV['DEVELOPER_DIR']) ||
-      tst(`xcode-select -print-path 2>/dev/null`) ||
-      tst("/Applications/Xcode.app/Contents/Developer") ||
-      MacOS.mdfind("com.apple.dt.Xcode").find{|path| tst(path) }
-    raise unless @xcode43_developer_dir
-    @xcode43_developer_dir
   end
 
   private
