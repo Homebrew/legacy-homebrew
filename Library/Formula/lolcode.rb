af59424d1073bc141e87c15ff97fdcee47aefc6d
Douglas Soares de Andrade <contato@douglasandrade.com>
2013-03-20 21:29:13 -0300

    lolcode: fix checksum
    Closes #18624.
    
    [jn: switch to archive URL]
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/lolcode.rb
+++ Library/Formula/lolcode.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Lolcode < Formula
   homepage 'http://www.icanhaslolcode.org/'
-  url 'https://github.com/justinmeza/lci/tarball/v0.10.3'
-  sha1 'c3e4b2b0b83a5e257c2e0e3b613c83c3cae4d084'
+  url 'https://github.com/justinmeza/lci/archive/v0.10.3.tar.gz'
+  sha1 '05f08be0e2eecc6e774b656e614bd48ef12481a7'
 
   head 'https://github.com/justinmeza/lolcode.git'
 
