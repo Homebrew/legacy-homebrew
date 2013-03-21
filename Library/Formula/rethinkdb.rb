8b4ef0030ac4d876c3c60e78fae42eacea839dea
Etienne Laurin <atnnn@rethinkdb.com>
2013-03-21 15:58:25 -0700

    RethinkDB 1.4.1
    Closes #18653.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/rethinkdb.rb
+++ Library/Formula/rethinkdb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rethinkdb < Formula
   homepage 'http://www.rethinkdb.com/'
-  url 'http://download.rethinkdb.com/dist/rethinkdb-1.4.0.tgz'
-  sha1 'cc4f75f92aace746e5ea5fce68c0e831f6327890'
+  url 'http://download.rethinkdb.com/dist/rethinkdb-1.4.1.tgz'
+  sha1 '456effad3e1acb6afe57da3bed65ba4e0fa0a11c'
 
   depends_on 'boost'
   depends_on 'v8'
