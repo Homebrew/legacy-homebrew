fc0a2a1f53b21ad58a3b3418190709932a2d9ce0
Jack Nagel <jacknagel@gmail.com>
2013-03-22 00:09:17 -0500

    harfbuzz: remove autogen
    There is no documentation as to why this is necessary, it builds fine
    for me without it, and MacPorts does not do it, so I'm removing it. It
    can be added back with comments if it breaks for someone.

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -6,11 +6,6 @@ class Harfbuzz < Formula
   sha256 'c653f70a275e98d109a9f1271373a6e80978c97298d723cb3f370351852f9da5'
 
   depends_on 'pkg-config' => :build
-
-  # Requires newer autotools on all platforms
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
   depends_on 'glib'
   depends_on 'icu4c' => :recommended
 
@@ -18,7 +13,6 @@ class Harfbuzz < Formula
   depends_on 'fontconfig'
 
   def install
-    system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
