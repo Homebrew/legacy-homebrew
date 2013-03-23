a8a484e781b82f69dd1f53aa0924a8088bfafc17
Stian Eikeland <stian@eikeland.se>
2013-03-23 20:45:34 +0100

    tinc 1.0.20
    Closes #18684.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/tinc.rb
+++ Library/Formula/tinc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tinc < Formula
   homepage 'http://www.tinc-vpn.org'
-  url 'http://www.tinc-vpn.org/packages/tinc-1.0.19.tar.gz'
-  sha1 '3ae326a060a88f5a9d2a1b8fdab26671b691f8ab'
+  url 'http://www.tinc-vpn.org/packages/tinc-1.0.20.tar.gz'
+  sha1 '3120b19d78e1981c0aaeae7519d81bdb657cb926'
 
   depends_on 'lzo'
 
