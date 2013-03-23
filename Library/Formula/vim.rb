7834f840569a31aeb2d9673f9dd655fb78e6f405
Francisco Souza <f@souza.cc>
2013-03-22 23:36:21 -0300

    vim 7.3.875
    Closes #18674.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -4,8 +4,8 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-874'
-  version '7.3.874'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-875'
+  version '7.3.875'
 
   head 'https://vim.googlecode.com/hg/'
 
