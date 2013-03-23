eb8b01bd6aabe3d7169bc0e08a5c52e5efadbe38
Aubin Paul <git@outlyer.org>
2013-03-23 16:08:53 -0400

    Fixes for broken compilation of mplayer on Mac OS X > 10.6 as well as an updated download strategy so that HEAD snapshots have revision numbers instead of "UNKNOWN"
    The upstream HEAD version of configure fails to build on x86_64/Darwin for recent versions of clang. The fixes include:
    - using -Wl,-pie to fix linking at the end
    - using -DPIC with YASM to allow ffmpeg to compile properly
    - --mdynamic-no-pic breaks compilation due to security features in newer versions of Mac OS X
    - Overloading the SVN strategy to have a snapshot version written so that mplayer doesn't show up without a version number.

--- Library/Formula/mplayer.rb
+++ Library/Formula/mplayer.rb
@@ -1,11 +1,20 @@
 require 'formula'
 
+class MplayerHeadDownloadStrategy < StrictSubversionDownloadStrategy
+  def stage
+    quiet_safe_system @@svn, 'export', '--force', @co, Dir.pwd
+    snapshot_file = File.join(Dir.pwd,'snapshot_version')
+    checkout = @co
+    quiet_safe_system ("svnversion #{checkout} > #{snapshot_file}")
+  end
+end
+
 class Mplayer < Formula
   homepage 'http://www.mplayerhq.hu/'
   url 'http://www.mplayerhq.hu/MPlayer/releases/MPlayer-1.1.tar.xz'
   sha1 '913a4bbeab7cbb515c2f43ad39bc83071b2efd75'
 
-  head 'svn://svn.mplayerhq.hu/mplayer/trunk', :using => StrictSubversionDownloadStrategy
+  head 'svn://svn.mplayerhq.hu/mplayer/trunk', :using => MplayerHeadDownloadStrategy
 
   option 'with-x', 'Build with X11 support'
   option 'without-osd', 'Build without OSD'
@@ -83,3 +92,30 @@ index a1fba5f..5deaa80 100755
      if ! git clone --depth 1 git://source.ffmpeg.org/ffmpeg.git ffmpeg ; then
          rm -rf ffmpeg
          echo "Failed to get a FFmpeg checkout"
+@@ -1729,7 +1729,6 @@
+ _getch=getch2.c
+ 
+ if darwin; then
+-  extra_cflags="-mdynamic-no-pic $extra_cflags"
+   _timer=timer-darwin.c
+ fi
+ 
+@@ -2771,8 +2770,8 @@
+     extra_ldflags="$extra_ldflags -pie"
+     relocatable=yes
+     res_comment="non-PIC"
+-  elif x86_64 && cflag_check -fpie -pie ; then
+-    extra_ldflags="$extra_ldflags -fpie -pie"
++  elif x86_64 && cflag_check -fpie -Wl,-pie; then
++    extra_ldflags="$extra_ldflags -fpie -Wl,-pie"
+     extra_cflags="$extra_cflags -fpie"
+     relocatable=yes
+     res_comment="fast PIC"
+@@ -2847,8 +2846,7 @@
+ echocheck "PIC"
+ def_pic='#define CONFIG_PIC 0'
+ pic=no
+-cpp_condition_check '' 'defined(__PIC__) || defined(__pic__) || defined(PIC)' &&
+-  pic=yes && extra_cflags="$extra_cflags -DPIC" && def_pic='#define CONFIG_PIC 1'
++pic=yes && extra_cflags="$extra_cflags -DPIC" && def_pic='#define CONFIG_PIC 1'
+ echores $pic
