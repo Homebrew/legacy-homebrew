e527a029c3f7decb4c880296e693e793913cf4b2
Adam Vandenberg <flangy@gmail.com>
2013-03-24 16:46:24 -0700

    libmtp uses pkg-config

--- Library/Formula/libmtp.rb
+++ Library/Formula/libmtp.rb
@@ -5,6 +5,7 @@ class Libmtp < Formula
   url 'http://sourceforge.net/projects/libmtp/files/libmtp/1.1.4/libmtp-1.1.4.tar.gz'
   sha1 '07f6b64e4d3c4966fab8d6d507d90eb2e6dff538'
 
+  depends_on "pkg-config" => :build
   depends_on "libusb-compat"
   depends_on "libgcrypt" => :optional
 
