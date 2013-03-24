04e4b1b71261924a3daf6848624d787987276737
Jack Nagel <jacknagel@gmail.com>
2013-03-24 14:32:24 -0500

    aria2 1.16.5

--- Library/Formula/aria2.rb
+++ Library/Formula/aria2.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Aria2 < Formula
   homepage 'http://aria2.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/aria2/stable/aria2-1.16.4/aria2-1.16.4.tar.bz2'
-  sha1 '302885fae5809e5bd62abd4cdb9592bb5b47c4a1'
+  url 'http://downloads.sourceforge.net/project/aria2/stable/aria2-1.16.5/aria2-1.16.5.tar.bz2'
+  sha1 '04ce2ae2f6500f6bb49c926e414ddc9c1a6059c3'
 
   depends_on 'pkg-config' => :build
   depends_on 'gnutls'
