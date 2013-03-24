78f3fe18580a297b347fe997f1b7e26c2869877d
Daniel Lee Harple <dlh@harple.com>
2013-03-24 17:58:15 -0400

    Homebrew::log can now take a block. This makes it simpler to implement a command that uses an external git repository browser (gitk, gitx, etc.).

---
+++ Library/Contributions/cmd/brew-gitk.rb
@@ -0,0 +1,17 @@
+def gitk(path)
+  # Calling 'gitk' directly doesn't open Wish in the foreground, so we use
+  # 'open' instead. This requires us to set the following env variables, because
+  # Wish.app doesn't inherit our current working directory.
+  toplevel = `git rev-parse --show-toplevel`.chomp
+  ENV["GIT_DIR"] = "#{toplevel}/.git"
+  ENV["GIT_WORK_TREE"] = toplevel
+  system "open",
+         "/System/Library/Frameworks/Tk.framework/Resources/Wish.app",
+         "--args",
+         `which gitk`.chomp,
+         *ARGV.options_only + ["--", path]
+end
+
+Homebrew.log do |path|
+  gitk(path)
+end
--- Library/Homebrew/cmd/log.rb
+++ Library/Homebrew/cmd/log.rb
@@ -1,11 +1,15 @@
 module Homebrew extend self
   def log
+    path = "."
     if ARGV.named.empty?
       cd HOMEBREW_REPOSITORY
-      exec "git", "log", *ARGV.options_only
     else
       path = ARGV.formulae.first.path.realpath
       cd path.dirname # supports taps
+    end
+    if block_given?
+      yield path
+    else
       exec "git", "log", *ARGV.options_only + [path]
     end
   end
