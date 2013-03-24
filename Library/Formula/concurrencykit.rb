5f1505c165779bad412db9ad77786dc0a3d2fdc6
Francisco Souza <f@souza.cc>
2013-03-24 01:06:10 -0300

    concurrencykit 0.2.15
    Closes #18694.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Concurrencykit < Formula
   homepage 'http://concurrencykit.org'
-  url 'http://concurrencykit.org/releases/ck-0.2.13.tar.gz'
-  sha1 'fd350384baa7f6263a2cc9d9d43f69f0dd3c89d0'
+  url 'http://concurrencykit.org/releases/ck-0.2.15.tar.gz'
+  sha1 'eea1aef0178f4d728164b3b2dda8ebededc62832'
 
   head 'git://git.concurrencykit.org/ck.git'
 
