54c0b85fe8997245b45a3ab6613651698aebe9de
Yehor Lvivski <lvivski@gmail.com>
2013-03-25 21:31:47 +0200

    dart 20444

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -4,14 +4,14 @@ class Dart < Formula
   homepage 'http://www.dartlang.org/'
 
   if MacOS.prefer_64_bit?
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/20193/dartsdk-macos-64.zip'
-    sha1 '8da75b3e6c44c47224a07fa670d2a9fb77326013'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/20444/dartsdk-macos-64.zip'
+    sha1 'e1676b070532e96fc408c80de1193ed0dea1ff1b'
   else
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/20193/dartsdk-macos-32.zip'
-    sha1 'ebe196d40e4c8757897da8d5c195957bb25af635'
+    url 'https://gsdview.appspot.com/dart-editor-archive-integration/20444/dartsdk-macos-32.zip'
+    sha1 'df7de6ecdfd758642c8cf551c4bc20a000fce4c2'
   end
 
-  version '20193'
+  version '20444'
 
   def install
     libexec.install Dir['*']
