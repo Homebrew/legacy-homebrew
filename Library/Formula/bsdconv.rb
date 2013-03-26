6a692d05470eab35bdf6c45ad06a4a4c45ae8c35
Jack Nagel <jacknagel@gmail.com>
2013-03-26 01:20:14 -0500

    bsdconv: use archive URL

--- Library/Formula/bsdconv.rb
+++ Library/Formula/bsdconv.rb
@@ -4,8 +4,8 @@ require 'formula'
 
 class Bsdconv < Formula
   homepage 'https://github.com/buganini/bsdconv'
-  url 'https://github.com/buganini/bsdconv/tarball/9.1'
-  sha1 '282fa6f2bcb6ef3b73f19ef269ecc917edcde729'
+  url 'https://github.com/buganini/bsdconv/archive/9.1.tar.gz'
+  sha1 '0c76fa6ea9f5922a705826f95a99c36dd951845a'
 
   head 'https://github.com/buganini/bsdconv.git'
 
