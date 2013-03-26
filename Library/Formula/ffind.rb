82a4847267f5151ae19b10e487825f75e0e135f1
Jack Nagel <jacknagel@gmail.com>
2013-03-26 01:20:21 -0500

    ffind: use archive URL

--- Library/Formula/ffind.rb
+++ Library/Formula/ffind.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ffind < Formula
   homepage 'https://github.com/sjl/friendly-find'
-  url 'https://github.com/sjl/friendly-find/tarball/v0.3.1'
-  sha1 '891207067c0d8715b48e91a52ff815aa73d7e139'
+  url 'https://github.com/sjl/friendly-find/archive/v0.3.1.tar.gz'
+  sha1 'c729a7ab2ae215d37a3e7cc0f16de24be1c965f6'
 
   conflicts_with 'sleuthkit',
     :because => "both install a 'ffind' executable."
