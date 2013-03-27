b2e4eb8d851461b3c40af9dd66320b89264b21d7
Fedor Bezrukov <Fedor.Bezrukov@gmail.com>
2013-03-27 11:52:02 +0100

    Fixed inkscape

--- Library/Formula/inkscape.rb
+++ Library/Formula/inkscape.rb
@@ -19,15 +19,34 @@ class Inkscape < Formula
   depends_on 'cairomm'
   depends_on 'pango'
   depends_on :x11
+  depends_on 'poppler'
+  depends_on 'libpng'
+
 
   fails_with :clang
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--enable-lcms",
-                          "--disable-poppler-cairo"
+                          "--enable-lcms"
     system "make install"
+  end
+
+  def caveats; <<-EOS.undent
+    From: http://wiki.inkscape.org/wiki/index.php/Frequently_asked_questions#Copying_and_pasting_in_Inkscape_creates_pixellated_images_instead_of_copying_the_vector_objects
+
+    Starting with XQuartz 2.3.2, X11 has some functionality to
+    exchange the content of the clipboard with OS X. But it currently
+    does not know how to deal with vector images so it just captures
+    the screen, i.e. creates a bitmap copy, and then pastes that. You
+    need to deactivate this functionality in X11 preferences >
+    Pasteboard: uncheck "Update Pasteboard when CLIPBOARD
+    changes". However, this will also prevent copying text from any
+    X11 application to Mac OS X ones. It will not prevent copying text
+    from OS X to X11.
+
+    Install 'brew install pstoedit' if you want to use
+    Extensions->Render->LaTeX Formula.
   end
 
   def test
