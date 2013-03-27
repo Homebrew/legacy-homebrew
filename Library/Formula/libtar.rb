2e76d96ff880e689d65290efc1e2dc42f5c0e53a
Steven Peters <scpeters@osrfoundation.org>
2013-03-27 12:12:57 -0700

    libtar: new formula
    libtar provides a C interface to tar files. It installs include and
    library files as well as an executable called libtar, which provides
    similar functionality to tar and gnu-tar. I think there should be
    no conflicts since the executable has a different name.

---
+++ Library/Formula/libtar.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Libtar < Formula
+  homepage 'http://www.feep.net/libtar/'
+  url 'ftp://ftp.feep.net/pub/software/libtar/libtar-1.2.11.tar.gz'
+  sha1 '9611f23024b0e89aad1cfea301122186b3c160f8'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
