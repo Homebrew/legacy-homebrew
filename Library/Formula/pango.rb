0708199c9365a2d3ddd61114226a97e5e41eef77
Jack Nagel <jacknagel@gmail.com>
2013-03-28 10:40:35 -0500

    pango 1.34

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pango < Formula
   homepage 'http://www.pango.org/'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/pango/1.32/pango-1.32.6.tar.xz'
-  sha256 '8e9a3eadebf30a31640f2b3ae0fb455cf92d10d1cad246d0ffe72ec595905174'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/pango/1.34/pango-1.34.0.tar.xz'
+  sha256 '105da1efe019018a9de81681d88c9ace83d2111509ab625c4be9d124a1fdce27'
 
   option 'without-x', 'Build without X11 support'
 
@@ -27,13 +27,6 @@ class Pango < Formula
   fails_with :llvm do
     build 2326
     cause "Undefined symbols when linking"
-  end
-
-  # Fixes hanging of inkscape, already upstream for next version.
-  # https://bugs.launchpad.net/inkscape/+bug/1114392
-  # http://git.gnome.org/browse/pango/commit/?id=10cc6557ba24239570ee9a7bcaa0a553aae9df95
-  def patches
-    DATA
   end
 
   def install
@@ -60,77 +53,3 @@ class Pango < Formula
     system "#{bin}/pango-querymodules", "--version"
   end
 end
-__END__
-diff --git a/pango/pangoft2-render.c b/pango/pangoft2-render.c
-index 60cf432..42923f4 100644
---- a/pango/pangoft2-render.c
-+++ b/pango/pangoft2-render.c
-@@ -210,7 +210,7 @@ pango_ft2_font_render_glyph (PangoFont *font,
-       return box;
-     }
-
--  face = pango_fc_font_lock_face (PANGO_FC_FONT (font));
-+  face = pango_ft2_font_get_face (font);
-
-   if (face)
-     {
-@@ -231,8 +231,6 @@ pango_ft2_font_render_glyph (PangoFont *font,
-       rendered->bitmap_left = face->glyph->bitmap_left;
-       rendered->bitmap_top = face->glyph->bitmap_top;
-
--      pango_fc_font_unlock_face (PANGO_FC_FONT (font));
--
-       if (G_UNLIKELY (!rendered->bitmap.buffer)) {
-         g_slice_free (PangoFT2RenderedGlyph, rendered);
-	return NULL;
-@@ -242,8 +240,6 @@ pango_ft2_font_render_glyph (PangoFont *font,
-     }
-   else
-     {
--      pango_fc_font_unlock_face (PANGO_FC_FONT (font));
--
- generic_box:
-       return  pango_ft2_font_render_box_glyph (PANGO_UNKNOWN_GLYPH_WIDTH,
-					       PANGO_UNKNOWN_GLYPH_HEIGHT,
-diff --git a/pango/pangoft2.c b/pango/pangoft2.c
-index cbe1d21..dc6db5d 100644
---- a/pango/pangoft2.c
-+++ b/pango/pangoft2.c
-@@ -424,13 +424,12 @@ pango_ft2_font_get_kerning (PangoFont *font,
- static FT_Face
- pango_ft2_font_real_lock_face (PangoFcFont *font)
- {
--  return pango_fc_font_lock_face (font);
-+  return pango_ft2_font_get_face ((PangoFont *)font);
- }
-
- static void
--pango_ft2_font_real_unlock_face (PangoFcFont *font)
-+pango_ft2_font_real_unlock_face (PangoFcFont *font G_GNUC_UNUSED)
- {
--  pango_fc_font_unlock_face (font);
- }
-
- static gboolean
-@@ -500,18 +499,12 @@ pango_ft2_font_get_coverage (PangoFont     *font,
- PangoGlyph
- pango_ft2_get_unknown_glyph (PangoFont *font)
- {
--  PangoFcFont *fc_font = PANGO_FC_FONT (font);
--  FT_Face face;
--  PangoGlyph glyph;
--
--  face = pango_fc_font_lock_face (fc_font);
-+  FT_Face face = pango_ft2_font_get_face (font);
-   if (face && FT_IS_SFNT (face))
-     /* TrueType fonts have an 'unknown glyph' box on glyph index 0 */
--    glyph = 0;
-+    return 0;
-   else
--    glyph = PANGO_GLYPH_EMPTY;
--  pango_fc_font_unlock_face (fc_font);
--  return glyph;
-+    return PANGO_GLYPH_EMPTY;
- }
-
- typedef struct
