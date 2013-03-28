a048e50b4077f7423b06e25c5e1008ea38c3da1d
youpy <youpy@buycheapviagraonlinenow.com>
2013-03-28 21:07:01 +0900

    add formula for nicovideo-dl

---
+++ Library/Formula/nicovideo-dl.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class NicovideoDl < Formula
+  homepage 'http://sourceforge.jp/projects/nicovideo-dl/'
+  url 'http://dl.sourceforge.jp/nicovideo-dl/56304/nicovideo-dl-0.0.20120212.tar.gz'
+  sha1 '19f92570e01bd19a5a980e67985c0821e0af6ad5'
+
+  def install
+    bin.install 'nicovideo-dl'
+  end
+
+  def test
+    system "#{bin}/nicovideo-dl",  "-v"
+  end
+end
