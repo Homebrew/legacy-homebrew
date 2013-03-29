4cd60ac5ff2a24759879a5e2387b901dd4d0b7ae
Adam Vandenberg <flangy@gmail.com>
2013-03-29 06:42:14 -0700

    jsmin 2013-02-25
    Closes #18787.

--- Library/Formula/jsmin.rb
+++ Library/Formula/jsmin.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Jsmin < Formula
   homepage 'http://www.crockford.com/javascript/jsmin.html'
-  url 'https://github.com/douglascrockford/JSMin/archive/8f62fe05856935ddcd49e364502ed98c4cf555b8.tar.gz'
-  version '2008-08-03'
-  sha1 '7909b2574255ebd2df46f847e2ec9ef60a56933d'
+  url 'https://github.com/douglascrockford/JSMin/archive/67754f619d0562f583dc5e869d2c05c0af21aca9.tar.gz'
+  version '2013-02-25'
+  sha1 '71633539862e0ab68ed74a92127304794ec8bfa9'
 
   def install
     system 'cc jsmin.c -o jsmin'
