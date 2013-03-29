7b3e7c25ea9ec7f0f271c625957a6a1d49ef5c21
Adam Vandenberg <flangy@gmail.com>
2013-03-29 06:33:01 -0700

    tomcat 7.0.39
    Closes #18819.

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -2,8 +2,11 @@ require 'formula'
 
 class Tomcat < Formula
   homepage 'http://tomcat.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.37/bin/apache-tomcat-7.0.37.tar.gz'
-  sha1 'c720975d6ebefa071eb94fdb27f13f53e8884811'
+  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.39/bin/apache-tomcat-7.0.39.tar.gz'
+  sha1 'a4dbca1ffa8f67cf9c985426ee019973638f4666'
+
+  # Keep log folders
+  skip_clean 'libexec'
 
   def install
     # Remove Windows scripts
