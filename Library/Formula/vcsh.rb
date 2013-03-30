c64a2a8fe19c300dd99af8cca665bd17fad64c90
Corey Quinn <corey@sequestered.net>
2013-03-30 16:53:46 -0700

    vcsh 1.1
     - add man page
    
    Closes #18853.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/vcsh.rb
+++ Library/Formula/vcsh.rb
@@ -2,14 +2,15 @@ require 'formula'
 
 class Vcsh < Formula
   homepage 'https://github.com/RichiH/vcsh'
-  url 'https://github.com/RichiH/vcsh/archive/v1.0-1.tar.gz'
-  version '1.0-1'
-  sha1 '23a72bc495fce299ee52ac98470f808a0a9a002f'
+  url 'https://github.com/RichiH/vcsh/archive/v1.1-homebrew.tar.gz'
+  version '1.1'
+  sha1 '2e51f6930528f668536478958205092a595b81c9'
 
   depends_on 'mr'
 
   def install
     bin.install 'vcsh'
+    man1.install 'vcsh.1'
   end
 
   test do
