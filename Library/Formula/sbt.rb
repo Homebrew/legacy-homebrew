7022bd8082f1b6300f786c6e0e32ecea14471448
Kashif Rasul <kashif.rasul@gmail.com>
2013-03-30 13:25:44 +0100

    sbt 0.12.3
    Closes #18837.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Sbt < Formula
   homepage 'http://www.scala-sbt.org'
-  url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.12.2/sbt-launch.jar'
-  version '0.12.2'
-  sha1 'ef6213597489edd8d1955086924f488949dd4975'
+  url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.12.3/sbt-launch.jar'
+  version '0.12.3'
+  sha1 '38d15379d20a8e8113e59285ff8a1e52b01b98b6'
 
   devel do
     url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.0-M1/sbt-launch.jar'
