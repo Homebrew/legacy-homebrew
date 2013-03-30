6a6da08cb4e599144b30115edcffabbc3c6b818f
Brett Koonce <koonce@gmail.com>
2013-03-30 11:27:12 -0500

    parrot (devel) 5.1.0
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -6,8 +6,8 @@ class Parrot < Formula
   sha256 '40c7176059e4462c722511a29450a4b80867a8459e273b602fbeaac2a22457f9'
 
   devel do
-    url 'ftp://ftp.parrot.org/pub/parrot/releases/devel/4.11.0/parrot-4.11.0.tar.bz2'
-    sha256 '0632ad0ca9557df391088bddbbc2608a4503adb5679dcbe62c065a73d4ce49de'
+    url 'ftp://ftp.parrot.org/pub/parrot/releases/devel/5.1.0/parrot-5.1.0.tar.bz2'
+    sha256 'af26c2fcc806505ec516ebb013bdd37b218633f5fe63faaa6b843ffe55e0135e'
   end
 
   head 'https://github.com/parrot/parrot.git'
