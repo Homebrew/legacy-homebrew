08ede3cf47a2469519a1fdbf122e7d7ab9ce15c0
Paul Hammond <paul@paulhammond.org>
2013-03-30 20:46:02 -0700

    webkit2png 0.6
    Closes #18744.

--- Library/Formula/webkit2png.rb
+++ Library/Formula/webkit2png.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Webkit2png < Formula
   homepage 'http://www.paulhammond.org/webkit2png/'
-  url 'https://github.com/paulhammond/webkit2png/archive/9c4265a82ebfcec200fca8de39fb970e5aae0a3d.tar.gz'
-  version '0.5'
-  sha1 '43765c86306e63b6d9d5a7c904598f3834b98b35'
+  url 'https://github.com/paulhammond/webkit2png/archive/v0.6.tar.gz'
+  sha1 '647bbdee40358e98c40542cd441cc0b84af75e36'
 
   def install
     bin.install 'webkit2png'
