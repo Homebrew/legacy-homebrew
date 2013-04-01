e83e1fb100b2fca0029e8865e15714c075d2a101
Jack Nagel <jacknagel@gmail.com>
2013-03-31 21:44:03 -0500

    libmicrohttpd 0.9.26

--- Library/Formula/libmicrohttpd.rb
+++ Library/Formula/libmicrohttpd.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libmicrohttpd < Formula
   homepage 'http://www.gnu.org/software/libmicrohttpd/'
-  url 'http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.25.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.25.tar.gz'
-  sha1 'da24fe9572f83e67959b0e32e15d261fcbf46bc1'
+  url 'http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.26.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.26.tar.gz'
+  sha1 'b6869fa5274d846b6325f205c023d7e2bd31f7f5'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
