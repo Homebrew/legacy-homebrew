9f33ef65fd4f2f0ac67d52880752ec75e9268305
Adam Vandenberg <flangy@gmail.com>
2013-04-01 07:14:45 -0700

    cocot: audit warnings

--- Library/Formula/cocot.rb
+++ Library/Formula/cocot.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Cocot < Formula
   homepage 'http://vmi.jp/software/cygwin/cocot.html'
   url 'https://github.com/vmi/cocot/archive/cocot-1.1-20120313.tar.gz'
-  version '1.1-20120313'
   sha1 'ffc36f56e47c22a963ef48cb67e60b0337b58bc6'
+
   head 'https://github.com/vmi/cocot.git', :branch => 'master'
 
   def install
