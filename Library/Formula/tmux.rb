0a273daa097302eee5ea636c683f88b8bf04ad21
Lance Parsons <lparsons@genomics.princeton.edu>
2013-04-02 16:59:32 -0400

    tmux: patch for Snow Leopard
    Closes #18915.
    Fixes #18776.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -15,6 +15,12 @@ class Tmux < Formula
     depends_on :libtool
   end
 
+  def patches
+    # Fixes installation failure on Snow Leopard
+    # http://sourceforge.net/mailarchive/forum.php?thread_name=CAJfQvvc2QDU%3DtXWb-sc-NK0J8cgnDRMDod6CNKO1uYqu%3DY5CXg%40mail.gmail.com&forum_name=tmux-users
+    DATA
+  end
+
   def install
     system "sh", "autogen.sh" if build.head?
 
@@ -32,3 +38,29 @@ class Tmux < Formula
   end
 end
 
+__END__
+diff --git a/osdep-darwin.c b/osdep-darwin.c
+index 23de9d5..b5efe84 100644
+--- a/osdep-darwin.c
++++ b/osdep-darwin.c
+@@ -33,17 +33,17 @@ struct event_base	*osdep_event_init(void);
+ char *
+ osdep_get_name(int fd, unused char *tty)
+ {
+-	struct proc_bsdshortinfo	bsdinfo;
++	struct proc_bsdinfo bsdinfo;
+	pid_t				pgrp;
+	int				ret;
+
+	if ((pgrp = tcgetpgrp(fd)) == -1)
+		return (NULL);
+
+-	ret = proc_pidinfo(pgrp, PROC_PIDT_SHORTBSDINFO, 0,
++	ret = proc_pidinfo(pgrp, PROC_PIDTBSDINFO, 0,
+	    &bsdinfo, sizeof bsdinfo);
+-	if (ret == sizeof bsdinfo && *bsdinfo.pbsi_comm != '\0')
+-		return (strdup(bsdinfo.pbsi_comm));
++	if (ret == sizeof bsdinfo && *bsdinfo.pbi_comm != '\0')
++		return (strdup(bsdinfo.pbi_comm));
+	return (NULL);
+ }
