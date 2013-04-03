46005c50ff7290f10618ec3fa929ba8df70fd71b
Adam Vandenberg <flangy@gmail.com>
2013-04-03 07:40:53 -0700

    jenkins 1.509

--- Library/Formula/jenkins.rb
+++ Library/Formula/jenkins.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Jenkins < Formula
   homepage 'http://jenkins-ci.org'
-  url 'http://mirrors.jenkins-ci.org/war/1.508/jenkins.war'
-  sha1 '9b4a9b7402b5b3fda301d4ee3696d2a8e6151b0a'
+  url 'http://mirrors.jenkins-ci.org/war/1.509/jenkins.war'
+  sha1 'e2a68b57e049ce0440da13309d300721862eaf2b'
+
   head 'https://github.com/jenkinsci/jenkins.git'
 
   def install
