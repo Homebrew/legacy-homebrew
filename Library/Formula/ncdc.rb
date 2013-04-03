05535388e0f0ab907325f7c1490ad89102b10798
Tobias Stehle <stehle.t@gmail.com>
2013-04-03 17:29:45 +0200

    ncdc 1.16.1
    Closes #18933.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ncdc.rb
+++ Library/Formula/ncdc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ncdc < Formula
   homepage 'http://dev.yorhel.nl/ncdc'
-  url 'http://dev.yorhel.nl/download/ncdc-1.15.tar.gz'
-  sha1 '2070d5e24201079445be15845a5f36cba874c0a5'
+  url 'http://dev.yorhel.nl/download/ncdc-1.16.1.tar.gz'
+  sha1 '3bdbf8c58a95eab2dc318bd853be991343f88fe9'
 
   depends_on 'glib'
   depends_on 'sqlite'
