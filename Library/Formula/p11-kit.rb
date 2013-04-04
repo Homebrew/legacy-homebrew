8157601571b66543b7ed3b8fbdc5e553be0ebd77
Jack Nagel <jacknagel@gmail.com>
2013-04-04 18:38:10 -0500

    p11-kit 0.18.0

--- Library/Formula/p11-kit.rb
+++ Library/Formula/p11-kit.rb
@@ -2,22 +2,39 @@ require 'formula'
 
 class P11Kit < Formula
   homepage 'http://p11-glue.freedesktop.org'
-  url 'http://p11-glue.freedesktop.org/releases/p11-kit-0.14.tar.gz'
-  sha256 '7a5e561b8b4c6e25ed7a89ef36c8127437c8f18bd86fe4cd41d899c5c7def6d3'
+  url 'http://p11-glue.freedesktop.org/releases/p11-kit-0.18.0.tar.gz'
+  sha256 '9ebcdcf57b7686b92146cf475cb2b66cdf3757f6e62d8e77c39dae89ffb43e31'
 
   option :universal
+
+  depends_on 'pkg-config' => :build
+  depends_on 'libtasn1'
+
+  def patches; DATA end
 
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--without-trust-paths"
     system "make"
-
-    # Bug workaround: https://bugs.freedesktop.org/show_bug.cgi?id=57714
-    mv 'tests/.libs/mock-one.so', 'tests/.libs/mock-one.dylib'
-
     system "make check"
     system "make install"
   end
 end
+
+__END__
+diff --git a/p11-kit/tests/test-init.c b/p11-kit/tests/test-init.c
+index 7df4be9..557d0c2 100644
+--- a/p11-kit/tests/test-init.c
++++ b/p11-kit/tests/test-init.c
+@@ -274,7 +274,7 @@ test_load_and_initialize (CuTest *tc)
+ 	CK_RV rv;
+ 	int ret;
+ 
+-	rv = p11_kit_load_initialize_module (BUILDDIR "/.libs/mock-one" SHLEXT, &module);
++	rv = p11_kit_load_initialize_module (BUILDDIR "/.libs/mock-one.so", &module);
+ 	CuAssertTrue (tc, rv == CKR_OK);
+ 	CuAssertTrue (tc, module != NULL);
+ 
