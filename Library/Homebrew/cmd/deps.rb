7e81e1eaf5bd643644b5d19b49b345f69d572286
Colin Dean <git@cad.cx>
2013-04-07 19:09:22 -0400

    add installed status indicator for deps command
    * display a green checkmark or red x indicating installed?
    * simply highlight dep to show status if `HOMEBREW_NO_EMOJI` is set
    * do nothing if stdout is not a tty

--- Library/Homebrew/cmd/deps.rb
+++ Library/Homebrew/cmd/deps.rb
@@ -22,7 +22,7 @@ module Homebrew extend self
     else
       raise FormulaUnspecifiedError if ARGV.named.empty?
       all_deps = deps_for_formulae(ARGV.formulae, !ARGV.one?)
-      all_deps.sort! unless mode.topo_order?
+      all_deps.sort! {|a,b| a.to_s <=> b.to_s} unless mode.topo_order?
       puts all_deps
     end
   end
@@ -35,17 +35,18 @@ module Homebrew extend self
       deps = f.deps.default
       reqs = f.requirements
     end
-    deps.map(&:name) + reqs.to_a.map do |r|
+    #deps.map(&:name) + reqs.to_a.map do |r|
+    deps + reqs.to_a.map do |r|
       r.class.default_formula if r.default_formula?
     end.compact
   end
 
   def deps_for_formulae(formulae, recursive=false)
-    formulae.map {|f| deps_for_formula(f, recursive) }.inject(&:&)
+    formulae.map {|f| decorate_dependencies deps_for_formula(f, recursive) }.inject(&:&)
   end
 
   def puts_deps(formulae)
-    formulae.each { |f| puts "#{f.name}: #{deps_for_formula(f)*' '}" }
+    formulae.each { |f| puts "#{f.name}: #{decorate_dependencies(deps_for_formula(f))*' '}" }
   end
 
   def puts_deps_tree(formulae)
@@ -65,5 +66,36 @@ module Homebrew extend self
       puts "|  "*(level-1)+"|- "+dep.to_s
       recursive_deps_tree(Formula.factory(dep.to_s), level+1)
     end
+  end
+
+  #old one
+  #def recursive_deps_tree f, level
+    #f.deps.each do |dep|
+    #  puts "> "*level+decorate_dependencies([dep])*''
+    #  recursive_deps_tree(Formula.factory(dep), level+1)
+   #end
+  #end
+
+  def decorate_dependency dep
+    return dep if !$stdout.tty?
+    #necessary for 1.8.7 unicode handling since many installs are on 1.8.7
+    checkmark = Tty.green + ["2714".hex].pack("U*") + Tty.reset
+    cross_out = Tty.red + ["2718".hex].pack("U*") + Tty.reset
+
+    if ENV['HOMEBREW_NO_EMOJI']
+      "%s%s%s" % [(dep.installed? ? Tty.green : Tty.red), dep, Tty.reset]
+    else
+      "%s %s" % [dep, (dep.installed? ? checkmark : cross_out)]
+    end
+
+  end
+
+  def decorate_dependencies dependencies
+    return dependencies if !$stdout.tty?
+    deps_status = dependencies.collect do |dep|
+      decorate_dependency dep
+    end
+    #this version returns an array because some methods that call it expect
+    #a string. those just do array*', ' to get a string.
   end
 end
