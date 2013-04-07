f3c49df0a33dfd95848959bb6a8ace6c180eecd0
Jack Nagel <jacknagel@gmail.com>
2013-04-06 21:34:41 -0500

    Update github URLs

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class YoutubeDl < Formula
-  homepage 'http://rg3.github.com/youtube-dl/'
+  homepage 'http://rg3.github.io/youtube-dl/'
   url 'http://youtube-dl.org/downloads/2013.03.29/youtube-dl-2013.03.29.tar.gz'
   sha1 '7dd68fcee929296199c5a38460182880012e3cd6'
 
