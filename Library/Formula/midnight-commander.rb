f287d9a71cc42685407caf11f9fd77470eee7eb4
aas <alexander.ashurkov@gmail.com>
2013-04-07 17:20:49 +0600

    Midnight Commander 4.8.8
    Closes #19025.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class MidnightCommander < Formula
   homepage 'http://www.midnight-commander.org/'
-  url 'http://www.midnight-commander.org/downloads/mc-4.8.7.tar.bz2',
+  url 'http://www.midnight-commander.org/downloads/mc-4.8.8.tar.bz2',
       :using => CurlUnsafeDownloadStrategy
-  sha256 '4e9c45925b47650dac58d7556a89c3e9b4275e48976b6c13b63c2f8eed3a383b'
+  sha256 '7b5e6f90e6709d1c1bcb4a2bf6d2a62b9494adef3ff4325ffdb3551c29e42a1c'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
