e17f09680d723377b985445a1ddcbae88679496f
Stefan <hydra_github@me.com>
2013-04-08 01:22:13 +0200

    json-c 0.11

--- Library/Formula/json-c.rb
+++ Library/Formula/json-c.rb
@@ -2,16 +2,15 @@ require 'formula'
 
 class JsonC < Formula
   homepage 'https://github.com/json-c/json-c/wiki'
-  url 'https://github.com/downloads/json-c/json-c/json-c-0.10.tar.gz'
-  sha1 'f90f643c8455da21d57b3e8866868a944a93c596'
+  url 'https://github.com/json-c/json-c/archive/json-c-0.11-20130402.tar.gz'
+  version '0.11'
+  sha1 '1910e10ea57a743ec576688700df4a0cabbe64ba'
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
 
-    # The Makefile forgets to install this header. This is fixed upstream and
-    # can be pulled on the next release.
-    (include/'json').install 'json_object_iterator.h'
+    ENV.deparallelize
+    system "make install"
   end
 end
