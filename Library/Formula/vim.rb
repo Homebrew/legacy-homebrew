13ffd1b94f9c0522c61078583efdc4d3101034c3
Francisco Souza <f@souza.cc>
2013-04-06 23:43:32 -0300

    vim 7.3.884
    Closes #19019.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -4,8 +4,8 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-882'
-  version '7.3.882'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-884'
+  version '7.3.884'
 
   head 'https://vim.googlecode.com/hg/'
 
