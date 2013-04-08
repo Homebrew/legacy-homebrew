ef4557bf498f599c2996ed491b47ccd1b5c0b641
Xin Sun <Sun.Simpson@gmail.com>
2013-04-08 22:38:53 +0800

    Enable AVX support for SIMD speedup with clang compiler and on Sandy Bridge or newer platforms.

--- Library/Formula/fftw.rb
+++ Library/Formula/fftw.rb
@@ -14,6 +14,11 @@ class Fftw < Formula
             "--enable-threads",
             "--disable-dependency-tracking"]
 
+    simd_args = ["--enable-sse2"]
+    if ENV.compiler == :clang and Hardware::CPU.avx?
+      simd_args << "--enable-avx"
+    end
+        
     if build.include? "with-fortran"
       ENV.fortran
     else
@@ -21,18 +26,16 @@ class Fftw < Formula
     end
 
     # single precision
-    # enable-sse only works with single
-    system "./configure", "--enable-single",
-                          "--enable-sse",
-                          *args
+    # enable-sse2 works for both single and double precisions
+    system "./configure", "--enable-single", *(args + simd_args)
     system "make install"
 
     # clean up so we can compile the double precision variant
     system "make clean"
 
     # double precision
-    # enable-sse2 only works with double precision (default)
-    system "./configure", "--enable-sse2", *args
+    # enable-sse2 works for both single and double precisions
+    system "./configure", *(args + simd_args)
     system "make install"
 
     # clean up so we can compile the long-double precision variant
