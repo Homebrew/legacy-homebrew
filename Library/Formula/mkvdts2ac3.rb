2008e473645f07cdf53886e0ba04b7856a0118dc
Deniz Türkoglu <turkoglu.deniz@gmail.com>
2013-04-09 00:03:18 +0300

    Remove redundant version from mkvdts2ac3

--- Library/Formula/mkvdts2ac3.rb
+++ Library/Formula/mkvdts2ac3.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Mkvdts2ac3 < Formula
   homepage 'https://github.com/JakeWharton/mkvdts2ac3'
   url 'https://github.com/JakeWharton/mkvdts2ac3/archive/1.6.0.tar.gz'
-  version '1.6.0'
   sha1 'e427eb6875d935dc228c42e99c3cd19c7ceaa322'
 
   depends_on 'mkvtoolnix'
