13e0db9632bd1cae8a7d521c7097df725cb928f0
Adam Vandenberg <flangy@gmail.com>
2013-04-07 21:06:42 -0700

    hbase 0.94.6.1

--- Library/Formula/hbase.rb
+++ Library/Formula/hbase.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hbase < Formula
   homepage 'http://hbase.apache.org'
-  url 'http://www.apache.org/dyn/closer.cgi?path=hbase/hbase-0.94.6/hbase-0.94.6.tar.gz'
-  sha1 '4a73120818b5595e926de3fc578519b62e60bb63'
+  url 'http://www.apache.org/dyn/closer.cgi?path=hbase/hbase-0.94.6.1/hbase-0.94.6.1.tar.gz'
+  sha1 'f97a91da6b82036b9a28da2d67884c8799537e65'
 
   depends_on 'hadoop'
 
