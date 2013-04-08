e7a88028f8fe2b8d6182270d219f18359448e100
clay richardson <c.richardson917@gmail.com>
2013-04-07 22:46:20 -0700

    aws-elasticbeanstalk: symlinks, patch setup.rb
    removed *.cmd files in ./api/bin/
    symlink ./api/bin/*
    symlink ./api/lib/*
    added require 'pathname' gem to setup.rb
    patched setup.rb to fix problems with relative paths and symlinks

--- Library/Formula/aws-elasticbeanstalk.rb
+++ Library/Formula/aws-elasticbeanstalk.rb
@@ -10,8 +10,58 @@ class AwsElasticbeanstalk < AmazonWebServicesFormula
     rm_rf Dir['eb/windows']
     rm_rf Dir['eb/linux']
     rm_rf Dir['AWSDevTools/Windows']
+    rm_rf Dir['api/bin/*.cmd']
 
     libexec.install %w{AWSDevTools api eb}
     bin.install_symlink libexec/"eb/macosx/python2.7/eb"
+    bin.install_symlink Dir[libexec/'api/bin/*']
+  end
+
+  def patches
+      # fixes problems with relative paths and symlinks
+      DATA
   end
 end
+
+__END__
+
+diff --git a/api/bin/setup.rb b/api/bin/setup.rb
+index 39c8895..ebc67d8 100644
+--- a/api/bin/setup.rb
++++ b/api/bin/setup.rb
+@@ -13,11 +13,31 @@
+ # ANY KIND, either express or implied. See the License for the specific
+ # language governing permissions and limitations under the License.
+ #
+-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
++require 'pathname'
++
++this_file = Pathname.new(__FILE__).realpath
++this_directory = File.dirname(this_file)
++
++$:.unshift(
++  File.absolute_path(
++    File.join(
++      this_directory,
++      '..',
++      'lib'
++    )
++  )
++)
+ 
+ require 'aws/elasticbeanstalk'
+ 
+-ca_bundle_file = File.join(File.dirname(__FILE__), '..', 'ca-bundle.crt')
++ca_bundle_file = File.absolute_path(
++  File.join(
++    this_directory,
++    '..',
++    'ca-bundle.crt'
++  )
++)
++
+ abort "CA Bundle required" unless File.exists?(ca_bundle_file)
+ 
+ config = {
+
+
