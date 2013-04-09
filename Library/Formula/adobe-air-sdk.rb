647d2c61f03b8b9313d3fb03ae430df83edfb838
Kyle Fritz <kyle.p.fritz@gmail.com>
2013-04-09 10:59:42 -0300

    rm empty line whitespace

--- Library/Formula/adobe-air-sdk.rb
+++ Library/Formula/adobe-air-sdk.rb
@@ -4,7 +4,7 @@ class AdobeAirSdk < Formula
   homepage 'http://www.adobe.com/products/air/sdk/'
   url 'http://airdownload.adobe.com/air/mac/download/3.7/AdobeAIRSDK.tbz2'
   sha1 'adde88ee22315792d945d05d5eb83eea8b7a808a'
-  
+
   def install
     libexec.install Dir['*']
     bin.write_exec_script libexec/'bin/adl'
