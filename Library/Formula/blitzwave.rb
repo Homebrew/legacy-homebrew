69389ba0d3c2bd40bc987e4f4b813b43b796d9e7
clubphily <suter.philipp@gmail.com>
2013-04-09 21:58:27 +0200

    removed comments and added test

--- Library/Formula/blitzwave.rb
+++ Library/Formula/blitzwave.rb
@@ -1,8 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Blitzwave < Formula
   homepage 'http://oschulz.github.io/blitzwave'
   url 'https://github.com/downloads/oschulz/blitzwave/blitzwave-0.7.1.tar.gz'
@@ -13,15 +10,14 @@ class Blitzwave < Formula
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test blitzwave`.
-    system "false"
+    cd "#{prefix}/src" do
+      system "WaveletDecomp-test"
+      system "arrayTools-test"
+      system "Wavelet-test"
+    end
   end
 end
