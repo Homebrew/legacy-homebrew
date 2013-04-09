c472dbeabb3f56e8a9a283511f8addfd55210512
Kyle Fritz <kyle.p.fritz@gmail.com>
2013-04-09 10:19:02 -0300

    adobe-air-sdk 3.7
    Closes #19076.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/adobe-air-sdk.rb
+++ Library/Formula/adobe-air-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AdobeAirSdk < Formula
   homepage 'http://www.adobe.com/products/air/sdk/'
-  url 'http://airdownload.adobe.com/air/mac/download/3.6/AdobeAIRSDK.tbz2'
-  sha1 'c43b0b100a46b7f304a1b213664b3bf573721b85'
+  url 'http://airdownload.adobe.com/air/mac/download/3.7/AdobeAIRSDK.tbz2'
+  sha1 'adde88ee22315792d945d05d5eb83eea8b7a808a'
 
   def install
     libexec.install Dir['*']
