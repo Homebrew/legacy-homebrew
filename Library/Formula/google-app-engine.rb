d55d34f376dc1372fa821ee7c5ffe305898eae33
Vincent <zakora@gmail.com>
2013-04-11 23:33:03 +0300

    Fix google-app-engine
    Add "_python_runtime.py" to the file list.
    
    If this file is omitted then google app engine can't run in development mode and raise the following error:
    /usr/bin/python: can't open file '/usr/local/bin/_python_runtime.py': [Errno 2] No such file or directory
    
    Closes #19139.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/google-app-engine.rb
+++ Library/Formula/google-app-engine.rb
@@ -10,7 +10,7 @@ class GoogleAppEngine < Formula
     share.install 'google_appengine' => name
     bin.mkpath
     %w[
-      api_server.py appcfg.py bulkload_client.py bulkloader.py dev_appserver.py download_appstats.py endpointscfg.py gen_protorpc.py google_sql.py old_dev_appserver.py remote_api_shell.py
+      _python_runtime.py api_server.py appcfg.py bulkload_client.py bulkloader.py dev_appserver.py download_appstats.py endpointscfg.py gen_protorpc.py google_sql.py old_dev_appserver.py remote_api_shell.py
     ].each do |fn|
       ln_s share+name+fn, bin
     end
