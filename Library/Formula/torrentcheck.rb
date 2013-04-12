2bb3d1199f8432c740a76a12712c724a1cab1552
Jonathan Wilkins <jwilkins@bitland.net>
2013-04-12 05:01:49 -0700

    torrentcheck 1.00
    Closes #19147.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/torrentcheck.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Torrentcheck < Formula
+  homepage 'http://torrentcheck.sourceforge.net/'
+  url 'http://sourceforge.net/projects/torrentcheck/files/torrentcheck-1.00.zip'
+  sha256 'a839f9ac9669d942f83af33db96ce9902d84f85592c99b568ef0f5232ff318c5'
+
+  def install
+    inreplace "torrentcheck.c", "#include <malloc.h>", ""
+    system ENV.cc, ENV.cflags, "torrentcheck.c", "sha1.c", "-o", "torrentcheck"
+    bin.install 'torrentcheck'
+  end
+
+  def test
+    system "#{bin}/torrentcheck"
+  end
+end
