649ed49279569fd0d88f6a63e20a60ee00883214
Rajeev Jain <rajeeja@gmail.com>
2013-04-14 03:27:42 -0500

    MOAB: Mesh-Oriented datA Base, depends on netcdf, hdf5 and othersLibrary/Formula/moab.rb

--- Library/Formula/netcdf.rb
+++ Library/Formula/netcdf.rb
@@ -1,15 +1,17 @@
 require 'formula'
 
+def fortran?
+  ARGV.include? '--enable-fortran'
+end
+
+def no_cxx?
+  ARGV.include? '--disable-cxx'
+end
+
 class NetcdfCXX < Formula
   homepage 'http://www.unidata.ucar.edu/software/netcdf'
   url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx4-4.2.tar.gz'
   sha1 '59628c9f06c211a47517fc00d8b068da159ffa9d'
-end
-
-class NetcdfCXX_compat < Formula
-  homepage 'http://www.unidata.ucar.edu/software/netcdf'
-  url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-cxx-4.2.tar.gz'
-  sha1 'bab9b2d873acdddbdbf07ab35481cd0267a3363b'
 end
 
 class NetcdfFortran < Formula
@@ -20,17 +22,20 @@ end
 
 class Netcdf < Formula
   homepage 'http://www.unidata.ucar.edu/software/netcdf'
-  url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-4.2.1.1.tar.gz'
-  sha1 '76631cb4e6b767c224338415cf6e5f5ff9bd1238'
+  url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-4.2.1.tar.gz'
+  sha1 'dfb6b10ef8dd20e785efa5e29b448383090f144d'
 
   depends_on 'hdf5'
 
-  option 'enable-fortran', 'Compile Fortran bindings'
-  option 'disable-cxx', "Don't compile C++ bindings"
-  option 'enable-cxx-compat', 'Compile C++ bindings for compatibility'
+  def options
+    [
+      ['--enable-fortran', 'Compile Fortran bindings'],
+      ['--disable-cxx', "Don't compile C++ bindings"]
+    ]
+  end
 
   def install
-    if build.include? 'enable-fortran'
+    if fortran?
       ENV.fortran
       # fix for ifort not accepting the --force-load argument, causing
       # the library libnetcdff.dylib to be missing all the f90 symbols.
@@ -61,16 +66,11 @@ class Netcdf < Formula
     NetcdfCXX.new.brew do
       system './configure', *common_args
       system 'make install'
-    end unless build.include? 'disable-cxx'
-
-    NetcdfCXX_compat.new.brew do
-      system './configure', *common_args
-      system 'make install'
-    end if build.include? 'enable-cxx-compat'
+    end unless no_cxx?
 
     NetcdfFortran.new.brew do
       system './configure', *common_args
       system 'make install'
-    end if build.include? 'enable-fortran'
+    end if fortran?
   end
 end
