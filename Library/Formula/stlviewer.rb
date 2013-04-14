543b11b5a816cb0752f35b75a1e03d4ca80422ae
Tobi Lehman <tobi.lehman@gmail.com>
2013-04-14 10:07:28 -0700

    Get stable 0.1 release

--- Library/Formula/stlviewer.rb
+++ Library/Formula/stlviewer.rb
@@ -2,11 +2,8 @@ require 'formula'
 
 class Stlviewer < Formula
   homepage 'https://github.com/vishpat/stlviewer#readme'
-  url 'https://github.com/vishpat/stlviewer/archive/master.zip'
-  sha1 'fc8e8afd3e26fc194da1a4abd17dd3e971d81b61'
-  # There is no versioning scheme, so I am using
-  # version 1.<number of commits in master>
-  version '1.14'
+  url 'https://github.com/vishpat/stlviewer/archive/0.1.zip'
+  sha1 'e2b062a96512e657ab09c2524876bc2601b74cb4'
 
   def install
     system "./compile.py"
