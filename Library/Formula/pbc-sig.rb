ddaa7305043197c45f127e5324ff0d2a6ef241f3
Brian Buchanan <bwb@holo.org>
2013-04-16 08:39:59 -0700

    pbc_sig 0.0.8

--- Library/Formula/pbc-sig.rb
+++ Library/Formula/pbc-sig.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class PbcSig < Formula
   homepage 'http://crypto.stanford.edu/pbc/sig/'
-  url 'http://crypto.stanford.edu/pbc/sig/files/pbc_sig-0.0.7.tar.gz'
-  sha1 'f15e4b721f95d073dfbe1a4687c490b82f7ac176'
+  url 'http://crypto.stanford.edu/pbc/sig/files/pbc_sig-0.0.8.tar.gz'
+  sha1 '718d86bca56c5e11ed87967770cb0d27533cd751'
 
   depends_on 'pbc'
 
