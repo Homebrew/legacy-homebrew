c547f8f715e878b711e21e022fc1a3103a0a5a24
Brian Buchanan <bwb@holo.org>
2013-04-16 08:39:53 -0700

    pbc 0.5.13

--- Library/Formula/pbc.rb
+++ Library/Formula/pbc.rb
@@ -2,15 +2,10 @@ require 'formula'
 
 class Pbc < Formula
   homepage 'http://crypto.stanford.edu/pbc/'
-  url 'http://crypto.stanford.edu/pbc/files/pbc-0.5.12.tar.gz'
-  sha1 '6fc0815a3e7766958365df4495247049d1bf968c'
+  url 'http://crypto.stanford.edu/pbc/files/pbc-0.5.13.tar.gz'
+  sha1 'f3e040a7f6648444feb77adffad514b5616aa2c9'
 
   depends_on 'gmp'
-
-  fails_with :clang do
-    build 421
-    cause "Clang does not support nested functions"
-  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
