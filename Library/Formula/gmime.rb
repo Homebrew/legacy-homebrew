be433a09ff7c4d1166aa0719ed22fd7e38125b3d
Aleh Suprunovich <br@ahlamon.org>
2013-04-16 08:29:42 +0300

    gmime 2.6.15
    Signed-off-by: Aleh Suprunovich <br@ahlamon.org>

--- Library/Formula/gmime.rb
+++ Library/Formula/gmime.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gmime < Formula
   homepage 'http://spruce.sourceforge.net/gmime/'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gmime/2.6/gmime-2.6.9.tar.xz'
-  sha256 '5ebb60a35fa5c0789fe10f6e4af5040cb7bc4a707a8a962fbd57b4fc5595c0b9'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gmime/2.6/gmime-2.6.15.tar.xz'
+  sha256 'b4c2a0b99b82063387cd750a38421ebaa0636f339e67984a84371bcb697dc99a'
 
   depends_on 'pkg-config' => :build
   depends_on 'xz' => :build
