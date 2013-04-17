db3d912c0f522f4829b0065dfab55dffc920f733
Shaun Jackman <sjackman@gmail.com>
2013-04-17 10:10:01 -0700

    Disable m32 and m64 on non-OSX systems. Closes #8

--- Library/Homebrew/extend/ENV/std.rb
+++ Library/Homebrew/extend/ENV/std.rb
@@ -283,10 +283,12 @@ module Stdenv
   end
 
   def m64
+    return unless MACOS
     append_to_cflags '-m64'
     append 'LDFLAGS', "-arch #{Hardware::CPU.arch_64_bit}"
   end
   def m32
+    return unless MACOS
     append_to_cflags '-m32'
     append 'LDFLAGS', "-arch #{Hardware::CPU.arch_32_bit}"
   end
