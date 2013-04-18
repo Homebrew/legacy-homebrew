c227351a8eb26a33e5c92805fb1d19722e57d5f9
Jochen Schäfer <jochen@joschs-robotics.de>
2013-04-18 08:23:11 +0200

    X11 is now a necessary dependency.

--- Library/Formula/atari++.rb
+++ Library/Formula/atari++.rb
@@ -5,7 +5,7 @@ class Atarixx < Formula
   url 'http://www.xl-project.com/download/atari++_1.71.tar.gz'
   sha1 '6d18494068bf491077cff32de514a1118bb133b1'
 
-  depends_on :x11 => :optional
+  depends_on :x11
   depends_on 'sdl' => :recommended
   option 'with-curses'
   
@@ -13,17 +13,11 @@ class Atarixx < Formula
     options = []
     options << "--disable-CURSES" unless build.include? 'with-curses'
     options << "--disable-SDL" unless build.include? 'with-sdl'
-    options << "--disable-X11" unless build.include? 'with-x11'
 
-    if options.length == 3
-      onoe "At least choose one frontend of SDL (recommended), X11 or Curses"
-      exit
-    else
-      system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", options
-      system "make"
-      system "make install" # if this fails, try separate make/make install steps
-    end
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                        "--prefix=#{prefix}", options
+    system "make"
+    system "make install" # if this fails, try separate make/make install steps
   end
 
   def test
