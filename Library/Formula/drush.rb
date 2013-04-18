f9a5c3fd046ad58de5e223c1871e6288cb939c91
Eric J. Duran <eric.duran7@gmail.com>
2013-04-18 14:07:43 -0300

    removing extra line break

--- Library/Formula/drush.rb
+++ Library/Formula/drush.rb
@@ -5,7 +5,6 @@ class Drush < Formula
   url 'http://ftp.drupal.org/files/projects/drush-7.x-5.9.tar.gz'
   sha1 '12533dbc7a18f1fef79a1853a8fdb88171f4fed8'
 
-
   head 'git://git.drupal.org/project/drush.git', :branch => '8.x-6.x'
 
   def install
