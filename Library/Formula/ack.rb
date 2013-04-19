896581db08afa26f8bd9e1b565d2c0a047a93517
Mike McQuaid <mike@mikemcquaid.com>
2013-04-20 00:34:36 +0100

    Revert "ack 2.02"
    This reverts commit 8f670fec0d54f1f976095bfcc836211d0d0758e7.

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -1,14 +1,16 @@
 require 'formula'
 
 class Ack < Formula
-  homepage 'http://beyondgrep.com/'
-  url 'https://github.com/petdance/ack2/archive/2.02.tar.gz'
-  sha1 'bad4f48b0aa0e93cc5f526324837d9bce49bd7ac'
+  homepage 'http://betterthangrep.com/'
+  url 'https://github.com/petdance/ack/archive/1.96.tar.gz'
+  sha1 '5d53b2b6f285e222a8459730495d6b07c692edd6'
 
   def install
     system 'pod2man', 'ack', 'ack.1'
     man1.install 'ack.1'
     bin.install 'ack'
+    bash_completion.install 'etc/ack.bash_completion.sh'
+    zsh_completion.install 'etc/ack.zsh_completion' => '_ack'
   end
 
   test do
