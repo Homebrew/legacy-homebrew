97158d82ab11dc5266b40c1223e90dc904b6063c
Jack Nagel <jacknagel@gmail.com>
2013-04-20 14:10:01 -0500

    harfbuzz 0.9.16

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Harfbuzz < Formula
   homepage 'http://www.freedesktop.org/wiki/Software/HarfBuzz'
-  url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.15.tar.bz2'
-  sha256 '214f919c2c998eb1316dd1a14a13adf27460e706a709488777076babf7fbbf1d'
+  url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.16.tar.bz2'
+  sha256 '6da7e032f705d8a5c91487cd296628d64975672a5b0c1704829cf2285072c92b'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
