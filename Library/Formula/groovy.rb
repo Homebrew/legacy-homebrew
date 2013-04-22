2c85de1259402fdbfe0f87df948ea5f130c0e1f7
Christian Höltje <docwhat@gerf.org>
2013-04-21 21:46:22 -0400

    groovy: added --invokedyamic flag
    Groovy can take advantage of Java 1.7's invokedynamic support
    if you swap out the jars with the `-indy` versions.

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -5,10 +5,20 @@ class Groovy < Formula
   url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.1.3.zip'
   sha1 'fa41789263ca6c7af145a3e5f568f264b4fd7a12'
 
+  option 'invokedynamic', "Install the InvokeDynamic version of Groovy (only works with Java 1.7+)"
+
   def install
     # Don't need Windows files.
     # Why are icons in bin?
     rm_f Dir["bin/*.bat","bin/groovy.{icns,ico}"]
+
+    if build.include? 'invokedynamic'
+      Dir['indy/*.jar'].each do |src_path|
+        dst_file = File.basename(src_path, '-indy.jar') + '.jar'
+        dst_path = File.join('lib', dst_file)
+        mv src_path, dst_path
+      end
+    end
 
     prefix.install_metafiles
     libexec.install %w(bin conf lib embeddable)
