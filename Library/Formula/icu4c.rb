b6c2b0b32a5b93d805a7863c7c187c745c01988e
Xiyue Deng <manphiz@gmail.com>
2013-04-22 05:19:34 -0700

    icu4c: 51.1
    * Update hashes and drop old bottles.
    * Enable build with Clang as [1] shouldn't be happening again.
    
    [1] https://github.com/mxcl/homebrew/issues/16142
    
    Closes #19354.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/icu4c.rb
+++ Library/Formula/icu4c.rb
@@ -2,24 +2,13 @@ require 'formula'
 
 class Icu4c < Formula
   homepage 'http://site.icu-project.org/'
-  url 'http://download.icu-project.org/files/icu4c/50.1/icu4c-50_1-src.tgz'
-  version '50.1'
-  sha1 '9a3369c00a8be8eff935d2893849ad2eb246c0ef'
-
-  bottle do
-    revision 2
-    sha1 '34c2ab788c5ca698c1902d3d6c38db0461f8b100' => :mountain_lion
-    sha1 '899afa2267843f7204583884885f2c05f8189ddc' => :lion
-    sha1 'a54cbdd33dbdcb0fd8ed2441580e91e8ff114640' => :snow_leopard
-  end
+  url 'http://download.icu-project.org/files/icu4c/51.1/icu4c-51_1-src.tgz'
+  version '51.1'
+  sha1 '7905632335e3dcd6667224da0fa087b49f9095e9'
 
   keg_only "Conflicts; see: https://github.com/mxcl/homebrew/issues/issue/167"
 
   option :universal
-
-  fails_with :clang do
-    cause "Icu will turn on C++11 mode when built with clang, which causes incompatibilities."
-  end
 
   def install
     ENV.universal_binary if build.universal?
