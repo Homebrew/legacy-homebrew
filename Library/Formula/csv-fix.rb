fc7d5aaa0666cc814a4d50f2e4e71692cee5b5c5
Turadg Aleahmad <turadg@aleahmad.net>
2013-04-22 23:02:17 -0400

    provide a HEAD source for CSVfix to get v1.5
    The project is up to version 1.5 but for some reason hasn't updated
    its source tarball since v1.2. This brew goes straight to the source.

--- Library/Formula/csv-fix.rb
+++ Library/Formula/csv-fix.rb
@@ -5,9 +5,10 @@ class CsvFix < Formula
   url 'http://csvfix.googlecode.com/files/csvfix_src_120.zip'
   version '1.20'
   sha1 'ec0b6d491429d908a9208d6e474591f30dec654c'
+  head 'https://code.google.com/p/csvfix', :using => :hg
 
   def install
-    system "make lin"
+    system "make mac || make lin"
     bin.install 'csvfix/bin/csvfix'
   end
 end
