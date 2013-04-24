85c10215c447538461d9047d4375a31a3e05f7b6
Andrew Janke <janke@pobox.com>
2013-04-24 03:41:24 -0400

    fio: fix "error: Define GOLDEN_RATIO_PRIME"
    Let fio's own CFLAGS settings stand instead of clearing them in the formula.
    
    The new 2.0.15 fio build was breaking because the Makefile changed around since
    2.0.7, and is now using CFLAGS to pass necessary macros and "-include" options
    to the build. The formula was crushing it with "CFLAGS=", which broke the build.
    
    Also turn on "V=1" to get normal full output from make, showing cc command lines,
    instead of the terse "CC foo.o" output, which makes it harder to diagnose.
    
    Fixes #19380.
    Closes #19423.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/fio.rb
+++ Library/Formula/fio.rb
@@ -6,10 +6,12 @@ class Fio < Formula
   sha1 '3b672f19ef37d0f4d733dc78820a5e4a735b9a7f'
 
   def install
+    system "./configure"
+    # fio's CFLAGS passes vital stuff around, and crushing it will break the build
     system "make", "prefix=#{prefix}",
                    "mandir=#{man}",
                    "CC=#{ENV.cc}",
-                   "CFLAGS=#{ENV.cflags}",
+                   "V=true", # get normal verbose output from fio's makefile
                    "install"
   end
 end
