3a33d2033f370ade3233eef63fad28d71d032293
Brett Koonce <koonce@gmail.com>
2013-04-23 23:10:05 -0500

    libwps 0.2.8

--- Library/Formula/libwps.rb
+++ Library/Formula/libwps.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libwps < Formula
   homepage 'http://libwps.sourceforge.net'
-  url 'http://sourceforge.net/projects/libwps/files/libwps/libwps-0.2.7/libwps-0.2.7.tar.bz2'
-  sha1 'ccd0b0083a367f293be85f7cea5ba9a8a4355d5c'
+  url 'http://sourceforge.net/projects/libwps/files/libwps/libwps-0.2.8/libwps-0.2.8.tar.bz2'
+  sha1 '6af0f161a39198f6a509aa6360398b46c94bd56c'
 
   depends_on 'pkg-config' => :build
   depends_on 'libwpd'
