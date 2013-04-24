a5b5e6c41c2d286e0abbfade8eedd2fca83d0b74
Brett Koonce <koonce@gmail.com>
2013-04-23 23:14:03 -0500

    rnv 1.7.11

--- Library/Formula/rnv.rb
+++ Library/Formula/rnv.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rnv < Formula
   homepage 'http://freshmeat.net/projects/rnv'
-  url 'http://downloads.sourceforge.net/project/rnv/Sources/1.7.10/rnv-1.7.10.tar.bz2'
-  sha1 'f85fa4b8da41758e51f8192b1c0974f62c53a242'
+  url 'http://downloads.sourceforge.net/project/rnv/Sources/1.7.11/rnv-1.7.11.tar.bz2'
+  sha1 '1a4475cbb6ff6da9e98cd8cceae7356e9a9f72b4'
 
   depends_on 'expat'
 
