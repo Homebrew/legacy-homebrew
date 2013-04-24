7ce3715bf629f43a87f78b6e8cf4071dd8c96968
Jack Nagel <jacknagel@gmail.com>
2013-04-24 14:26:09 -0500

    atkmm 2.22.7

--- Library/Formula/atkmm.rb
+++ Library/Formula/atkmm.rb
@@ -2,9 +2,10 @@ require 'formula'
 
 class Atkmm < Formula
   homepage 'http://www.gtkmm.org'
-  url 'http://ftp.acc.umu.se/pub/GNOME/sources/atkmm/2.22/atkmm-2.22.6.tar.bz2'
-  sha256 '923d0d638aba70c7f5feff1fc296e0546978afbb049f404b77d97d90dde4e88f'
+  url 'http://ftp.acc.umu.se/pub/GNOME/sources/atkmm/2.22/atkmm-2.22.7.tar.xz'
+  sha256 'bfbf846b409b4c5eb3a52fa32a13d86936021969406b3dcafd4dd05abd70f91b'
 
+  depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'atk'
   depends_on 'glibmm'
