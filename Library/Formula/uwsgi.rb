9fbf741d2d568ccaf91395f7f5f472e5ef8c5bda
Krzysztof Urbaniak <urban@fail.pl>
2013-04-24 09:44:45 +0200

    uwsgi 1.9.7

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Uwsgi < Formula
   homepage 'http://projects.unbit.it/uwsgi/'
-  url 'http://projects.unbit.it/downloads/uwsgi-1.9.5.tar.gz'
-  sha1 '3f37d61ebce10d2ec0380553022f6e6cb05e03d9'
+  url 'http://projects.unbit.it/downloads/uwsgi-1.9.7.tar.gz'
+  sha1 '735a25963398a696f6814aea21a40f93a3a69ce0'
 
   depends_on 'pcre'
   depends_on 'libyaml'
