4f372eb67dd7afa16f9de9c228211c0afc38ca53
Misty De Meo <mistydemeo@gmail.com>
2013-04-25 18:22:57 -0500

    sub2srt: new url
    Fixes #19438.

--- Library/Formula/sub2srt.rb
+++ Library/Formula/sub2srt.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Sub2srt < Formula
-  homepage 'http://www.robelix.com/sub2srt/'
-  url 'http://www.robelix.com/sub2srt/download/sub2srt-0.5.3.tar.gz'
-  sha1 'eaecadc4caaaacddcd0e13202be722f13706acd4'
+  homepage 'https://github.com/robelix/sub2srt'
+  url 'https://github.com/robelix/sub2srt/archive/0.5.3.tar.gz'
+  sha1 'd0d1ed31adde5f3e6f117ca4e5a80b206d92b93f'
 
   def install
     bin.install 'sub2srt'
