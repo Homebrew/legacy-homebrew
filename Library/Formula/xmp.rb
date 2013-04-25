28d48b450866f2f9fe2127b7905bcd9c511354fa
Misty De Meo <mistydemeo@gmail.com>
2013-04-25 11:07:37 -0500

    xmp 4.0.3

--- Library/Formula/xmp.rb
+++ Library/Formula/xmp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Xmp < Formula
   homepage 'http://xmp.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/xmp/xmp/4.0.2/xmp-4.0.2.tar.gz'
-  sha1 '32c2d5884cd94cfbc442095200d625b9f7ec6d2d'
+  url 'http://downloads.sourceforge.net/project/xmp/xmp/4.0.3/xmp-4.0.3.tar.gz'
+  sha1 '1d33cedfc2f56c6c7dc1688a2d226083bc523e2e'
   head 'git://git.code.sf.net/p/xmp/xmp-cli'
 
   depends_on :autoconf if build.head?
@@ -15,10 +15,10 @@ class Xmp < Formula
     system "make install"
 
     # install the included demo song
-    share.install "08_sad_song.it" unless build.head?
+    share.install "ub-name.it" unless build.head?
   end
 
   def test
-    system "#{bin}/xmp", "--load-only", share/"08_sad_song.it"
+    system "#{bin}/xmp", "--load-only", share/"ub-name.it"
   end
 end
