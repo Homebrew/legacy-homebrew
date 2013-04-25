3e3fd8088efd87edd865bf4a4bc6b3d6981d50e5
Adam Vandenberg <flangy@gmail.com>
2013-04-25 12:33:14 -0700

    uwsgi 1.9.8
    Closes #19433.

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Uwsgi < Formula
   homepage 'http://projects.unbit.it/uwsgi/'
-  url 'http://projects.unbit.it/downloads/uwsgi-1.9.7.tar.gz'
-  sha1 '735a25963398a696f6814aea21a40f93a3a69ce0'
+  url 'http://projects.unbit.it/downloads/uwsgi-1.9.8.tar.gz'
+  sha1 '41f6b838228d980a60d99c412a5ad16b4ba9bfe4'
 
   depends_on 'pcre'
   depends_on 'libyaml'
