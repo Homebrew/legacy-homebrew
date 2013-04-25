6ea75a44b332097e8235801ff5ed7156b44cb348
Misty De Meo <mistydemeo@gmail.com>
2013-04-25 10:56:53 -0500

    libxmp: new HEAD url

--- Library/Formula/libxmp.rb
+++ Library/Formula/libxmp.rb
@@ -4,7 +4,7 @@ class Libxmp < Formula
   homepage 'http://xmp.sourceforge.net'
   url 'http://downloads.sourceforge.net/project/xmp/libxmp/4.0.4/libxmp-4.0.4.tar.gz'
   sha1 'b8078f9565eba4ad540b7ac3dc4c98e6369bd760'
-  head 'git://xmp.git.sourceforge.net/gitroot/xmp/xmp'
+  head 'git://git.code.sf.net/p/xmp/libxmp'
 
   depends_on :autoconf if build.head?
 
