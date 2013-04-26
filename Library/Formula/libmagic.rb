70eae74390c216afad628b96ea23467d8a35cc52
Roger Seagle <roger.seagle@gmail.com>
2013-04-26 14:04:18 -0400

    Updated libmagic to install python bindings. Updated binwalk to use latest version.

--- Library/Formula/libmagic.rb
+++ Library/Formula/libmagic.rb
@@ -6,6 +6,8 @@ class Libmagic < Formula
   mirror 'http://fossies.org/unix/misc/file-5.13.tar.gz'
   sha1 '927651df90ead6b3e036e243109137c7d42c4fb6'
 
+  option "with-python", "Build Python bindings."
+
   # Fixed upstream, should be in next release
   # See http://bugs.gw.com/view.php?id=230
   def patches; DATA; end if MacOS.version < :lion
@@ -19,6 +21,32 @@ class Libmagic < Formula
     # Don't dupe this system utility
     rm bin/"file"
     rm man1/"file.1"
+
+    # Check for building python bindings
+    build_python = build.include? "with-python"
+    cd "python" do
+      # In order to install into the Cellar, the dir must exist and be in the PYTHONPATH.
+      temp_site_packages = lib/which_python/'site-packages'
+      mkdir_p temp_site_packages
+      ENV['PYTHONPATH'] = temp_site_packages
+
+      args = [
+        "--no-user-cfg",
+        "--verbose",
+        "install",
+        "--force",
+        "--install-scripts=#{bin}",
+        "--install-lib=#{temp_site_packages}",
+        "--install-data=#{share}",
+        "--install-headers=#{include}",
+      ]
+      system "python", "setup.py", *args
+    end if build_python
+
+  end
+
+ def which_python
+    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
   end
 end
 
