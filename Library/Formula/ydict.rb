7bb361fe634e88b0dc99a84f81cd8e5c46d84763
Chen Wen <pokkys@gmail.com>
2013-04-27 01:10:46 +0800

    rename ydict to ydict.py and make a wrapper script to set the default locate.

--- Library/Formula/ydict.rb
+++ Library/Formula/ydict.rb
@@ -5,10 +5,14 @@ class Ydict < Formula
   url 'http://ydict.googlecode.com/files/ydict-1.2.6.tar.gz'
   sha1 '08eb840c31964fb80c5f9af54a26e2bd6e622d6b'
 
-  def install
-    bin.install 'ydict'
+  def ydict_wrapper; <<-EOS.undent
+    #!/bin/sh
+    LC_ALL=en_US.UTF-8 "#{bin}/ydict.py" "$@"
+    EOS
   end
-  def caveats
-    "Please set your LC_ALL to UTF-8, ex: 'export LC_ALL=en_US.UTF-8'"
+
+  def install
+    bin.install 'ydict' => 'ydict.py'
+    (bin/'ydict').write(ydict_wrapper)
   end
 end
