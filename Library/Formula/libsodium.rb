e3b06e713ccbf37c1f9ce48a323a8b9516e9112c
Frank Denis <github@pureftpd.org>
2013-04-28 13:06:16 -0700

    libsodium 0.4.1
    Closes #19495.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libsodium.rb
+++ Library/Formula/libsodium.rb
@@ -2,10 +2,13 @@ require 'formula'
 
 class Libsodium < Formula
   homepage 'https://github.com/jedisct1/libsodium/'
-  url 'http://download.dnscrypt.org/libsodium/releases/libsodium-0.3.tar.gz'
-  sha1 '3d486dc40e7cbd9542d00088d3b73a719d7746fa'
+  url 'http://download.dnscrypt.org/libsodium/releases/libsodium-0.4.1.tar.gz'
+  sha256 '65756c7832950401cc0e6ee0e99b165974244e749f40f33d465f56447bae8ce3'
+
+  option :universal
 
   def install
+    ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make check"
