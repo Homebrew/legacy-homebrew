3460a6f3ea4ad43282c55fdb7ced3562f18c1c15
David Albert <davidbalbert@gmail.com>
2013-04-28 14:45:13 -0400

    Gdb 7.6
    Closes #179.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gdb.rb
+++ Library/Formula/gdb.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gdb < Formula
   homepage 'http://www.gnu.org/software/gdb/'
-  url 'http://ftpmirror.gnu.org/gdb/gdb-7.5.1.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gdb/gdb-7.5.1.tar.bz2'
-  sha1 'd04c832698ac470a88788e719d19ca7c1d4d803d'
+  url 'http://ftpmirror.gnu.org/gdb/gdb-7.6.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gdb/gdb-7.6.tar.bz2'
+  sha1 'b64095579a20e011beeaa5b264fe23a9606ee40f'
 
   depends_on 'readline'
 
@@ -16,21 +16,6 @@ class Gdb < Formula
                           "--with-system-readline"
     system "make"
     system "make install"
-  end
-
-  def patches
-    [
-      # Support for new Mach-O load commands backported from trunk. This should
-      # be unnecessary in GDB 7.6.
-      "https://raw.github.com/gist/4197567/dad208436d69ba591200098c91a94fa9e1cdc160/loadcommands.patch",
-
-      # This is a suplemental patch that fixes a small bug in the above patch.
-      # It's not in trunk yet, but it has been submitted upstream, okayed, and
-      # hopefully a version of it will be committed by the time 7.6 comes out.
-      # If you're updating this formula for 7.6, email davidbalbert@gmail.com,
-      # and I'll let you know if the change has made it into the release.
-      "https://raw.github.com/gist/4197567/e53221895da6bf0e8b307db6e774c24f543db1c3/startaddress.patch"
-    ]
   end
 
   def caveats; <<-EOS.undent
