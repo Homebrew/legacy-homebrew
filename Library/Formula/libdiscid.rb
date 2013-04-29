9dc2f62d7771fcdf3b21a248683a262d46e5ae54
Anton Rieder <aried3r@gmail.com>
2013-04-29 22:26:39 +0200

    libdiscid 0.5.0
    Closes #19519.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libdiscid.rb
+++ Library/Formula/libdiscid.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libdiscid < Formula
   homepage 'http://musicbrainz.org/doc/libdiscid'
-  url 'http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-0.4.1.tar.gz'
-  sha1 '256f5d4d7fcfb99a6bee92971bfd9f6da96e9639'
+  url 'http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-0.5.0.tar.gz'
+  sha1 'e046be72fca56bc1e5dce3568055331b58ee4dfc'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
