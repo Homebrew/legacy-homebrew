32943b46a0106e13478ee2cb4f4eea96cc90505a
Yoji SHIDARA <dara@shidara.net>
2013-04-29 16:23:09 +0900

    kytea 0.4.5
    Closes #19501.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/kytea.rb
+++ Library/Formula/kytea.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Kytea < Formula
   homepage 'http://www.phontron.com/kytea/'
-  url 'http://www.phontron.com/kytea/download/kytea-0.4.2.tar.gz'
-  sha1 'd43c9712819f112e5b2077eba16926c506cf6387'
+  url 'http://www.phontron.com/kytea/download/kytea-0.4.5.tar.gz'
+  sha1 'af0228203f509f576f5a2dcf2c0cb44e2f36aa72'
 
   def install
     system "./configure", "--disable-dependency-tracking",
