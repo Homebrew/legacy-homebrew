7edd348aafdb8a00b6753ed12cc777084a8b6514
Thijs Vermeir <thijsvermeir@gmail.com>
2013-04-29 13:08:51 +0200

    gst-libav 1.0.7
    Closes #19505.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gst-libav.rb
+++ Library/Formula/gst-libav.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GstLibav < Formula
   homepage 'http://gstreamer.freedesktop.org'
-  url 'http://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.0.6.tar.xz'
-  mirror 'http://ftp.osuosl.org/pub/blfs/svn/g/gst-libav-1.0.6.tar.xz'
-  sha256 '8ab222a52bf7482e913f2c9a4f490cda8f8ed1acfbc429f27451b0558b08044d'
+  url 'http://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.0.7.tar.xz'
+  mirror 'http://ftp.osuosl.org/pub/blfs/svn/g/gst-libav-1.0.7.tar.xz'
+  sha256 'aefa679d14e7a6558673cfbf401b9c01f1903bb52e5dc08332e9001d25a7ba7a'
 
   depends_on "pkg-config" => :build
   depends_on "xz" => :build
