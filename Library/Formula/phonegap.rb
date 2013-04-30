a662d22ca4f1675ec77c30ef8728722f0de3c3fe
Luis Abreu <lmjabreu@gmail.com>
2013-05-01 00:02:40 +0200

    Updated phonegap.rb
    * replaced .zip with .tar.gz
    * removed redundant explicit version
    * renamed phonegap-ios to phonegap-ios-create

--- Library/Formula/phonegap.rb
+++ Library/Formula/phonegap.rb
@@ -2,15 +2,14 @@ require 'formula'
 
 class Phonegap < Formula
   homepage 'http://phonegap.com'
-  url 'https://github.com/phonegap/phonegap/archive/2.6.0.zip'
-  version '2.6.0'
-  sha1 'b4171edc30444fed9350aa2605aa81916d8e73f8'
+  url 'https://github.com/phonegap/phonegap/archive/2.6.0.tar.gz'
+  sha1 '1b3d435953f8aa1a26b18516ca3a967aa6b722c1'
   head 'https://github.com/phonegap/phonegap.git'
 
   def install
     libexec.install Dir['*']
     # create symlinks for phonegap-ios*
-    bin.install_symlink "#{libexec}/lib/ios/bin/create" => "phonegap-ios"
+    bin.install_symlink "#{libexec}/lib/ios/bin/create" => "phonegap-ios-create"
     bin.install_symlink "#{libexec}/lib/ios/bin/autotest" => "phonegap-ios-autotest"
     bin.install_symlink "#{libexec}/lib/ios/bin/cordova_plist_to_config_xml" => "phonegap-ios-plist_to_config_xml"
     bin.install_symlink "#{libexec}/lib/ios/bin/diagnose_project" => "phonegap-ios-diagnose_project"
