fb09087bc1e4cc1224e83e3c5312a4d4074c539d
Jon Ege Ronnenberg <jon.ronnenberg@gmail.com>
2013-05-01 02:46:59 +0300

    httrack: Update to 3.47.7
    Closes #19548.
    
    Signed-off-by: Charlie Sharpsteen <source@sharpsteen.net>

--- Library/Formula/httrack.rb
+++ Library/Formula/httrack.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Httrack < Formula
   homepage 'http://www.httrack.com/'
-  url 'http://download.httrack.com/httrack-3.47.6.tar.gz'
-  sha1 'bdf726de2b0838c9823dc4e77f4fb4791374ba44'
+  url 'http://download.httrack.com/httrack-3.47.7.tar.gz'
+  sha1 '324ead01e873c5fdff7305ba9194d920300ba136'
 
   def install
     ENV.deparallelize
