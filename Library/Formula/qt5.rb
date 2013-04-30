0c94c06ec03f2dfd40ccddf9c658d6dc101ba801
Mike McQuaid <mike@mikemcquaid.com>
2013-04-30 18:32:51 +0100

    qt5 5.0.2
    Closes #19271

--- Library/Formula/qt5.rb
+++ Library/Formula/qt5.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Qt5 < Formula
   homepage 'http://qt-project.org/'
-  url 'http://releases.qt-project.org/qt5/5.0.1/single/qt-everywhere-opensource-src-5.0.1.tar.gz'
-  sha1 'fda04435b1d4069dc189ab4d22ed7a36fe6fa3e9'
+  url 'http://releases.qt-project.org/qt5/5.0.2/single/qt-everywhere-opensource-src-5.0.2.tar.gz'
+  sha1 '7df93ca2cc5274f0cef72ea71f06feed2594b92f'
 
   head 'git://gitorious.org/qt/qt5.git', :branch => 'master'
 
@@ -21,12 +21,6 @@ class Qt5 < Formula
   depends_on "d-bus" if build.include? 'with-qtdbus'
   depends_on "mysql" if build.include? 'with-mysql'
   depends_on "jpeg"
-
-  def patches
-    # http://qt.gitorious.org/qt/qtbase/commit/655ba5?format=patch
-    # Inlined to fix paths.
-    DATA
-  end
 
   def install
     args = ["-prefix", prefix,
@@ -111,37 +105,3 @@ class Qt5 < Formula
     EOS
   end
 end
-
-__END__
-From 655ba5755696df8e2594bca9f7696ab621f5afc3 Mon Sep 17 00:00:00 2001
-From: Gabriel de Dietrich <gabriel.dedietrich@digia.com>
-Date: Tue, 5 Feb 2013 13:39:33 +0100
-Subject: [PATCH] Cocoa QPA: Fix compilation error
-
-The error appeared with latest clang as of Feb. 5, 2013.
-
-Apple LLVM version 4.2 (clang-425.0.24) (based on LLVM 3.2svn)
-Target: x86_64-apple-darwin12.2.0
-
-Change-Id: I8df8cccc941ac03a7a997bdd5afe095b7b6f65d3
-Reviewed-by: Frederik Gladhorn <frederik.gladhorn@digia.com>
----
- src/plugins/platforms/cocoa/qcocoawindow.h |    3 ++-
- 1 files changed, 2 insertions(+), 1 deletions(-)
-
-diff --git a/qtbase/src/plugins/platforms/cocoa/qcocoawindow.h b/qtbase/src/plugins/platforms/cocoa/qcocoawindow.h
-index 3b5be0a..324a43c 100644
---- a/qtbase/src/plugins/platforms/cocoa/qcocoawindow.h
-+++ b/qtbase/src/plugins/platforms/cocoa/qcocoawindow.h
-@@ -49,7 +49,8 @@
-
- #include "qcocoaglcontext.h"
- #include "qnsview.h"
--class QT_PREPEND_NAMESPACE(QCocoaWindow);
-+
-+QT_FORWARD_DECLARE_CLASS(QCocoaWindow)
-
- @interface QNSWindow : NSWindow {
-     @public QCocoaWindow *m_cocoaPlatformWindow;
---
-1.7.1
