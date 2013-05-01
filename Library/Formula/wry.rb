ec6dd3553d91bca17a2d8c0c047bf2c11db0936c
Rob Warner <rwarner@grailbox.com>
2013-05-01 19:26:32 -0400

    Add sha1

--- Library/Formula/wry.rb
+++ Library/Formula/wry.rb
@@ -13,6 +13,7 @@ end
 class Wry < Formula
   homepage 'http://grailbox.com/wry/'
   url 'https://github.com/hoop33/wry/archive/v1.4.1.tar.gz'
+  sha1 '303c02c21b1046c92ca37e584a3a77cfe37381b2'
   head 'https://github.com/hoop33/wry.git'
 
   depends_on LionOrNewer
