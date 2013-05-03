633c94583d111032e961d9d7e0fc507fda3ad10f
Andrew Griffiths <andrew.john.griffiths@gmail.com>
2013-05-04 04:04:52 +1000

    added xmlindent (http://xmlindent.sourceforge.net/)

---
+++ Library/Formula/xmlindent.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Xmlindent < Formula
+  homepage 'http://xmlindent.sourceforge.net/'
+  version '0.2.17'
+  url 'http://aarnet.dl.sourceforge.net/project/xmlindent/xmlindent/0.2.17/xmlindent-0.2.17.tar.gz'
+  sha1 '024d5c27ae7fd63b012e663c437b3ec46f12b789'
+
+  def install
+    inreplace 'Makefile', '-lfl', '-ll'
+    inreplace 'indent.c', 'assert(strlen(yytext) == 1);', 'if (strlen(yytext) != 1)  return;'
+         
+    system "make" 
+
+    bin.mkpath
+    bin.install 'xmlindent'
+
+    man.mkpath
+    man.install('xmlindent.1')
+  end
+
+  def test
+    system "xmlindent"
+  end
+end
