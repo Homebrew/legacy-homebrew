855c9655774f1b3f6b4cad4bc17f3ceea68d851c
Mike McQuaid <mike@mikemcquaid.com>
2013-05-03 17:53:15 +0100

    icu4c: 51.1 bottles

--- Library/Formula/icu4c.rb
+++ Library/Formula/icu4c.rb
@@ -6,6 +6,12 @@ class Icu4c < Formula
   version '51.1'
   sha1 '7905632335e3dcd6667224da0fa087b49f9095e9'
 
+  bottle do
+    sha1 '6b5b4ab5704cc2a8b17070a087c7f9594466cf1d' => :mountain_lion
+    sha1 'a555b051a65717e1ca731eec5743969d8190a9f8' => :lion
+    sha1 'bcb1ab988f67c3d48fb7c5829153c136c16c059b' => :snow_leopard
+  end
+
   keg_only "Conflicts; see: https://github.com/mxcl/homebrew/issues/issue/167"
 
   option :universal
