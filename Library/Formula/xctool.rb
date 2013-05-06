b1ba5ba187a1fe8f33889dbea580c16da4935bd3
Gordon Fontenot <gordon@thoughtbot.com>
2013-05-06 17:23:37 -0300

    xctool: add --HEAD
    Closes #19633.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xctool.rb
+++ Library/Formula/xctool.rb
@@ -4,6 +4,7 @@ class Xctool < Formula
   homepage 'https://github.com/facebook/xctool'
   url 'https://github.com/facebook/xctool/archive/v0.1.0.tar.gz'
   sha1 'f5cf21d14f26127cea6b6b069fcc2c7387c41af6'
+  head 'https://github.com/facebook/xctool.git'
 
   depends_on :xcode
 
