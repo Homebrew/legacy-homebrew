46941be352958eccec42d04894534b1c05f357a3
Daniel Lee Harple <dlh@harple.com>
2013-05-07 14:07:25 -0400

    Pass a User-Agent when fetching data from the GitHub API
    See <http://developer.github.com/v3/#user-agent-required>.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Contributions/cmd/brew-ls-taps.rb
+++ Library/Contributions/cmd/brew-ls-taps.rb
@@ -1,8 +1,7 @@
-require 'open-uri'
 require 'vendor/multi_json'
 
 begin
-  open "https://api.github.com/legacy/repos/search/homebrew" do |f|
+  GitHub.open "https://api.github.com/legacy/repos/search/homebrew" do |f|
     MultiJson.decode(f.read)["repositories"].each do |repo|
       if repo['name'] =~ /^homebrew-(\S+)$/
         puts tap = if repo['username'] == "Homebrew"
