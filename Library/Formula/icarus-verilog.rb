d24bb834cf0af27cef652c67a8fb04ba504c9fb3
Adam Vandenberg <flangy@gmail.com>
2013-05-06 19:52:25 -0700

    verilog 0.9.6
    Closes #19643.

--- Library/Formula/icarus-verilog.rb
+++ Library/Formula/icarus-verilog.rb
@@ -2,13 +2,8 @@ require 'formula'
 
 class IcarusVerilog < Formula
   homepage 'http://iverilog.icarus.com/'
-  url 'ftp://icarus.com/pub/eda/verilog/v0.9/verilog-0.9.5.tar.gz'
-  sha1 '3a69cb935ab562882a07a52904f3cba74aed2229'
-
-  devel do
-    url 'ftp://ftp.icarus.com/pub/eda/verilog/snapshots/verilog-20120501.tar.gz'
-    sha1 '313ab0f5dc4d198bd4687daaf2e54749c67558b3'
-  end
+  url 'ftp://icarus.com/pub/eda/verilog/v0.9/verilog-0.9.6.tar.gz'
+  sha1 'd81f586b801a2d897ba8c35971d660b960220ed4'
 
   def install
     # Fixes an assertion when XCode-4.4 tries to link with clang or llvm-gcc.
