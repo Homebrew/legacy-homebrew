f883222509a7e7b120c9d9899ad714f232117b2b
Luke Petre <lpetre@gmail.com>
2013-05-08 05:10:05 +0100

    perforce-server: add plist
    Closes #19673.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/perforce-server.rb
+++ Library/Formula/perforce-server.rb
@@ -15,5 +15,36 @@ class PerforceServer < Formula
 
   def install
     bin.install 'p4d'
+    (var+"p4root").mkpath
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_prefix}/bin/p4d</string>
+        <string>-p</string>
+        <string>1666</string>
+        <string>-r</string>
+        <string>#{var}/p4root</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+      <key>WorkingDirectory</key>
+      <string>#{var}/p4root</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/p4d.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/p4d.log</string>
+    </dict>
+    </plist>
+    EOS
   end
 end
