1f56b26adab006b57c59723e360cd59ae9bcc7ff
Jack Nagel <jacknagel@gmail.com>
2013-05-09 14:19:23 -0500

    moreutils 0.48

--- Library/Formula/moreutils.rb
+++ Library/Formula/moreutils.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Moreutils < Formula
   homepage 'http://packages.debian.org/unstable/utils/moreutils'
-  url 'http://mirrors.kernel.org/debian/pool/main/m/moreutils/moreutils_0.47.tar.gz'
-  mirror 'http://ftp.us.debian.org/debian/pool/main/m/moreutils/moreutils_0.47.tar.gz'
-  sha1 '0459608fdb37bc59605d7a476717693afd133e00'
+  url 'http://mirrors.kernel.org/debian/pool/main/m/moreutils/moreutils_0.48.tar.gz'
+  mirror 'http://ftp.us.debian.org/debian/pool/main/m/moreutils/moreutils_0.48.tar.gz'
+  sha1 '023893342ce68d877f4a8eb14142bbd7fd3706ee'
 
   conflicts_with 'parallel',
     :because => "both install a 'parallel' executable."
