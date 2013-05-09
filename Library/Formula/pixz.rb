5cd1b2ef9db16e7d1a28aa0f2defed4c29268067
Jeff Field <jeff.field@gmail.com>
2013-05-09 15:50:12 -0400

    New formula: pixz
    Pixz (pronounced 'pixie') is a parallel, indexing version of XZ. This
    formula successfully builds on OS X 10.8.3. It requires libarchive.
    Added an option (--with-docs, hat tip to whoever did the git-cola
    formula as well as the helpful folks in IRC) which will build and
    install a manpage from the asciidoc source the author included.
    Unfortunately, building the docs requires the installation of 2 other
    packages, so I decided to keep it optional.
    
    Closes #19698.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ Library/Formula/pixz.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class Pixz < Formula
+  homepage 'https://github.com/vasi/pixz'
+  url 'http://sourceforge.net/projects/pixz/files/pixz-1.0.2.tgz'
+  sha1 '953b2b55504ba349f1e7e47bdfcd4165ba206827'
+
+  head 'https://github.com/vasi/pixz.git'
+
+  option 'with-docs', 'Build man pages using asciidoc and DocBook'
+
+  depends_on 'libarchive'
+  depends_on 'xz'
+
+  if build.with? 'docs'
+    depends_on 'asciidoc' => :build
+    depends_on 'docbook' => :build
+  end
+
+  def install
+    system 'make'
+    bin.install 'pixz'
+
+    if build.with? 'docs'
+      ENV['XML_CATALOG_FILES'] = "#{HOMEBREW_PREFIX}/etc/xml/catalog"
+      system 'a2x --doctype manpage --format manpage pixz.1.asciidoc'
+      man1.install 'pixz.1'
+    end
+  end
+
+  test do
+    system "#{bin}/pixz"
+  end
+end
