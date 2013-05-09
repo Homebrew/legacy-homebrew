6588433735c716f0aee7a07bd5099e16eddf7cec
Dustin Kirkland <dustin.kirkland@gmail.com>
2013-05-09 12:18:14 -0500

    New upstream release of Byobu 5.41, fix some Mac issues

--- Library/Formula/byobu.rb
+++ Library/Formula/byobu.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Byobu < Formula
   homepage 'http://byobu.co'
-  url 'https://launchpad.net/byobu/trunk/5.39/+download/byobu_5.39.orig.tar.gz'
-  sha1 '65b22a3c6bfa7e6a7ddb6838fc95dcb217522493'
+  url 'https://launchpad.net/byobu/trunk/5.41/+download/byobu_5.41.orig.tar.gz'
+  sha1 'fdf4e6384936def0dd45485b5b32e20d366a3cbc'
 
   depends_on 'coreutils'
   depends_on 'gnu-sed' # fails with BSD sed
