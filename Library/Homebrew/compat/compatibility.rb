9b7c228bdf9fd59b7b6021b52a2f67a90c5b5571
Jack Nagel <jacknagel@gmail.com>
2013-05-09 23:46:30 -0500

    Remove ancient global methods from compat

--- Library/Homebrew/compat/compatibility.rb
+++ Library/Homebrew/compat/compatibility.rb
@@ -1,54 +1,5 @@
-## Compatibility layer introduced in 0.8 (refactor)
-
-# maybe never used by anyone, but alas it must continue to exist
-def versions_of(keg_name)
-  `/bin/ls #{HOMEBREW_CELLAR}/#{keg_name}`.collect { |version| version.strip }.reverse
-end
-
-def dump_config
-  require 'cmd/--config'
-  Homebrew.__config
-end
-
-def dump_build_env env
-  require 'cmd/--env'
-  Homebrew.dump_build_env env
-end
-
-def default_cc
-  MacOS.default_cc
-end
-
-def gcc_42_build
-  MacOS.gcc_42_build_version
-end
-
-alias :gcc_build :gcc_42_build
-
-def gcc_40_build
-  MacOS.gcc_40_build_version
-end
-
-def llvm_build
-  MacOS.llvm_build_version
-end
-
 def x11_installed?
   MacOS::X11.installed?
-end
-
-def macports_or_fink_installed?
-  MacOS.macports_or_fink_installed?
-end
-
-def outdated_brews
-  require 'cmd/outdated'
-  Homebrew.outdated_brews
-end
-
-def search_brews text
-  require 'cmd/search'
-  Homebrew.search_brews text
 end
 
 def snow_leopard_64?
