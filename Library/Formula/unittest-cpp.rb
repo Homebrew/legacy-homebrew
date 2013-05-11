78e53b202180a970b12b25a739145dd5293483f8
Simon Walker <s.r.walker101@googlemail.com>
2013-05-11 14:51:45 +0100

    unittest-cpp 1.4
    Closes #19732.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/unittest-cpp.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class UnittestCpp < Formula
+  homepage 'http://unittest-cpp.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/unittest-cpp/UnitTest++/1.4/unittest-cpp-1.4.zip'
+  sha1 'dad944159e2e135aea74039987490eaaee00f2ad'
+
+  def install
+    system "make"
+
+    # Install the headers
+    include.install Dir['src/*.h']
+    include.install 'src/Posix'
+
+    # Install the compiled library
+    lib.install 'libUnitTest++.a'
+
+    # Install the documentation
+    doc.install 'docs/UnitTest++.html'
+  end
+end
