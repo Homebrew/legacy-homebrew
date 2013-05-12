581aaa3bc2536eca39b7eaf28a88eb863592f7c9
Jon Ege Ronnenberg <jon.ronnenberg@gmail.com>
2013-05-12 06:01:27 +0300

    httrack: use stable download URL
    Closes #19742.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/httrack.rb
+++ Library/Formula/httrack.rb
@@ -2,7 +2,10 @@ require 'formula'
 
 class Httrack < Formula
   homepage 'http://www.httrack.com/'
-  url 'http://download.httrack.com/httrack-3.47.11.tar.gz'
+  # url: Always use mirror.httrack.com when you link to a new version
+  # of HTTrack, as link to download.httrack.com will break on next
+  # HTTrack update.
+  url 'http://mirror.httrack.com/historical/httrack-3.47.11.tar.gz'
   sha1 '237fca7a9c5c63b53c5d0134dac6e5714422384c'
 
   def install
