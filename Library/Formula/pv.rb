4a0f1ab130c3d02304cf808cd4afaabf45504344
Adam Vandenberg <flangy@gmail.com>
2013-05-12 09:16:51 -0700

    pv 1.4.6

--- Library/Formula/pv.rb
+++ Library/Formula/pv.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pv < Formula
   homepage 'http://www.ivarch.com/programs/pv.shtml'
-  url 'http://www.ivarch.com/programs/sources/pv-1.3.9.tar.bz2'
-  sha1 '9ed32eb0c441f9699acd4091e096ca2f4bb6b5ea'
+  url 'http://www.ivarch.com/programs/sources/pv-1.4.6.tar.bz2'
+  sha1 'c8f66859a7e1d314f1160e3b8fc0e26cf5cdea11'
 
   depends_on 'gettext'
 
