0afd04245be15d201cf34607989e389ed0a2b096
Alexis Hildebrandt <afh@surryhill.net>
2013-05-13 20:06:08 +0200

    ledger: Clean-up dependencies, use ninja for build
    Change urls to point to ledger organization on github
    
    Closes #19777.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ledger.rb
+++ Library/Formula/ledger.rb
@@ -5,26 +5,30 @@ class Ledger < Formula
   url 'https://github.com/downloads/ledger/ledger/ledger-2.6.3.tar.gz'
   sha1 '5b8e7d8199acb116f13720a5a469fff1f14b4041'
 
-  head 'https://github.com/jwiegley/ledger.git', :branch => 'master'
+  head 'https://github.com/ledger/ledger.git', :branch => 'master'
 
   option 'debug', 'Build with debugging symbols enabled'
   option 'python', 'Enable Python support'
 
-  depends_on 'gettext'
   depends_on 'boost'
-  depends_on 'mpfr'
-  depends_on 'gmp'
-  depends_on 'pcre'
-  depends_on 'expat'
-  depends_on 'cmake' if build.head?
-  depends_on 'libofx' => :optional unless build.head?
+  if build.head?
+    depends_on 'cmake' => :build
+    depends_on 'ninja' => :build
+    depends_on 'mpfr'
+    depends_on 'gmp'
+  else
+    depends_on 'gettext'
+    depends_on 'pcre'
+    depends_on 'expat'
+    depends_on 'libofx' => :optional
+  end
 
   def install
     # find Homebrew's libpcre
     ENV.append 'LDFLAGS', "-L#{HOMEBREW_PREFIX}/lib"
 
     if build.head?
-      args = [((build.include? 'debug') ? 'debug' : 'opt'), "make", "-j#{ENV.make_jobs}", "--output=build"]
+      args = [((build.include? 'debug') ? 'debug' : 'opt'), "make", "-N", "-j#{ENV.make_jobs}", "--output=build"]
       args << '--python' if build.include? 'python'
       system "./acprep", *args
       system "cmake", "-D", "CMAKE_INSTALL_PREFIX=#{prefix}", "-P", "build/cmake_install.cmake"
