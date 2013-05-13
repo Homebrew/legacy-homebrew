e580c81d62ab343c79e2ba0fb5517c4ca341a5e0
Adam Vandenberg <flangy@gmail.com>
2013-05-13 09:37:50 -0700

    pari 2.5.4

--- Library/Formula/pari.rb
+++ Library/Formula/pari.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pari < Formula
   homepage 'http://pari.math.u-bordeaux.fr/'
-  url 'http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.5.3.tar.gz'
-  sha1 'de79eee7ae017a495dc0c648b6f7d5a4f6c7a522'
+  url 'http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.5.4.tar.gz'
+  sha1 '471226fd86fea0ad1f236249a49bdaee16aa34bf'
 
   depends_on 'readline'
   depends_on :x11
