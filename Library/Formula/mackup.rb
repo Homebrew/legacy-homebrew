1bc3916d19902655685d1bd30fa1b48004aa62c2
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2013-05-13 23:59:15 -0500

    Updated the url to include the latest version

--- Library/Formula/mackup.rb
+++ Library/Formula/mackup.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Mackup < Formula
   homepage 'https://github.com/lra/mackup'
-  url 'https://raw.github.com/lra/mackup/0.1/mackup.py'
+  url 'https://raw.github.com/lra/mackup/0.3.2/mackup.py'
   sha1 '0d83270a6826cca2231b486282be2a0b219a0d78'
 
   def install
