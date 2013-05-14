aae83e75a5629da48785b8b952e22ebb19200614
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2013-05-14 00:19:33 -0500

    Updated the SHA1 and added a version number

--- Library/Formula/mackup.rb
+++ Library/Formula/mackup.rb
@@ -3,7 +3,8 @@ require 'formula'
 class Mackup < Formula
   homepage 'https://github.com/lra/mackup'
   url 'https://raw.github.com/lra/mackup/0.3.2/mackup.py'
-  sha1 '0d83270a6826cca2231b486282be2a0b219a0d78'
+  sha1 'ab158808d9443847274d38b995e9bb55f5bcb92a'
+  version '0.3.2'
 
   def install
     bin.install "mackup.py" => "mackup"
