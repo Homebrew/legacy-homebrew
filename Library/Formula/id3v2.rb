362be803900781f06e4d344d87118efb80bde6dd
Alex Bukin <abukin@csu.fullerton.edu>
2013-05-14 09:36:01 -0700

    update id3v2 to 0.1.12

--- Library/Formula/id3v2.rb
+++ Library/Formula/id3v2.rb
@@ -2,17 +2,12 @@ require 'formula'
 
 class Id3v2 < Formula
   homepage 'http://id3v2.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/id3v2/id3v2/0.1.11/id3v2-0.1.11.tar.gz'
-  sha1 'ca825d851ca0c6a5783af107dc6baa7aa93f0bad'
+  url 'http://downloads.sourceforge.net/project/id3v2/id3v2/0.1.12/id3v2-0.1.12.tar.gz'
+  sha1 '8f42153b2f53098c221da2e8fe42170e727cd9ad'
 
   depends_on 'id3lib'
 
   def install
-    inreplace 'Makefile' do |s|
-      s.gsub! 'c++', ENV.cxx
-      s.gsub! '/man/man1', '/share/man/man1'
-    end
-
     bin.mkpath
     man1.mkpath
     system "make", "install", "PREFIX=#{prefix}"
