49bb996906a55c6417d2c90a12ae12874a975296
Rob Warner <rwarner@grailbox.com>
2013-05-14 19:05:48 -0400

    wry 1.5
    Closes #19823.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/wry.rb
+++ Library/Formula/wry.rb
@@ -12,8 +12,8 @@ end
 
 class Wry < Formula
   homepage 'http://grailbox.com/wry/'
-  url 'https://github.com/hoop33/wry/archive/v1.4.1.tar.gz'
-  sha1 '303c02c21b1046c92ca37e584a3a77cfe37381b2'
+  url 'https://github.com/hoop33/wry/archive/v1.5.tar.gz'
+  sha1 '56f03ff07b19b2b5489d024b60c1b1d23f347961'
 
   head 'https://github.com/hoop33/wry.git'
 
