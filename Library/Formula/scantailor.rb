924ccc973c23ae824bd48988aeb4c87b6f8a33c3
Jack Nagel <jacknagel@gmail.com>
2013-05-14 20:14:28 -0500

    scantailor: use custom Version subclass

--- Library/Formula/scantailor.rb
+++ Library/Formula/scantailor.rb
@@ -1,13 +1,33 @@
 require 'formula'
 
 class Scantailor < Formula
+  class Version < ::Version
+    def enhanced?
+      to_a[0].to_s == "enhanced"
+    end
+
+    def <=>(other)
+      other = self.class.new(other)
+      if enhanced? && other.enhanced?
+        super
+      elsif enhanced?
+        1
+      elsif other.enhanced?
+        -1
+      else
+        super
+      end
+    end
+  end
+
   homepage 'http://scantailor.sourceforge.net/'
   url 'http://downloads.sourceforge.net/project/scantailor/scantailor/0.9.11/scantailor-0.9.11.tar.gz'
+  version '0.9.11' => Version
   sha1 '21ec03317ca2b278179693237eaecd962ee0263b'
 
   devel do
     url 'http://downloads.sourceforge.net/project/scantailor/scantailor-devel/enhanced/scantailor-enhanced-20120812.tar.bz2'
-    version 'enhanced-20120812'
+    version 'enhanced-20120812' => Version
     sha1 'e3535d6e21a1844cf83eb2b23469fb6d90c070a9'
   end
 
