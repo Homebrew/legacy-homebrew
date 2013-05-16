5f82dc90108af58ae85466e9be9abc66769d78d6
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2013-05-16 22:57:25 +0200

    fontconfig: add patch for in memory fonts bugs
    Closes #19846.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/fontconfig.rb
+++ Library/Formula/fontconfig.rb
@@ -12,14 +12,26 @@ class Fontconfig < Formula
   depends_on :freetype
   depends_on 'pkg-config' => :build
 
-  # Patch adapted from Macports patch for 2.9.0 defines sizeof based on __LP64__
-  # Fixes universal builds but seems groovy enough to apply in all cases.
-  # https://trac.macports.org/browser/trunk/dports/graphics/fontconfig/files/patch-check-arch-at-runtime.diff
-  def patches; DATA; end
+  def patches
+    [
+      # Patch adapted from Macports patch for 2.9.0 defines sizeof based on __LP64__
+      # Fixes universal builds but seems groovy enough to apply in all cases.
+      # https://trac.macports.org/browser/trunk/dports/graphics/fontconfig/files/patch-check-arch-at-runtime.diff
+      DATA,
+
+      # Patch copied over from Fedora to correct a bug with in memory fonts
+      # that breaks Firefox, libass and a lot of other sofware
+      # See https://github.com/mxcl/homebrew/issues/19312 for details.
+      # NOTE: This will probably be fixed in next fontconfig.
+      'http://pkgs.fedoraproject.org/cgit/fontconfig.git/plain/fontconfig-fix-woff.patch?id=e669d0170be4492cd966114122ad5281ec2276de'
+    ]
+  end
 
   def install
     ENV.universal_binary if build.universal?
-    system "./configure", "--disable-dependency-tracking", "--with-add-fonts=/Library/Fonts,~/Library/Fonts", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--with-add-fonts=/Library/Fonts,~/Library/Fonts",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
