3e186a5cf25584041fc77f625cfd3a0f45c81d43
googolmo <googolmo@gmail.com>
2013-05-17 21:33:39 +0800

    add shadowsocks-libev 1.2

---
+++ Library/Formula/shadowsocks-libev.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class ShadowsocksLibev < Formula
+  homepage 'https://github.com/madeye/shadowsocks-libev'
+  url 'https://github.com/madeye/shadowsocks-libev.git'
+  version '1.2'
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test shadowsocks`.
+    system "false"
+  end
+end
