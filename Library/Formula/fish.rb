3a86b452ca94060768e443b09057c66bc736110a
ridiculousfish <corydoras@ridiculousfish.com>
2013-05-17 00:56:56 -0700

    fish 2.0
    Replaces fish 1.x. fishfish recipe is left alone for now, but is a
    conflict and should be uninstalled first.
    
    Closes #19887.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/fish.rb
+++ Library/Formula/fish.rb
@@ -2,34 +2,22 @@ require 'formula'
 
 class Fish < Formula
   homepage 'http://fishshell.com'
-  url 'http://downloads.sourceforge.net/project/fish/fish/1.23.1/fish-1.23.1.tar.bz2'
-  sha1 '3a6a5d0cfff348e5f9b1e7cd771865fd1dcd802e'
+  url 'http://fishshell.com/files/2.0.0/fish-2.0.0.tar.gz'
+  sha1 '2d28553e2ff975f8e5fed6b266f7a940493b6636'
 
-  head 'git://gitorious.org/fish-shell/fish-shell.git'
+  head 'https://github.com/fish-shell/fish-shell.git'
 
   # Indeed, the head build always builds documentation
   depends_on 'doxygen' => :build if build.head?
-  depends_on :autoconf if build.head?
-  depends_on 'readline'
+  depends_on :autoconf
 
   skip_clean 'share/doc'
 
   conflicts_with "fishfish"
 
-  def patches
-    p = []
-
-    unless build.head?
-      # Reduces the timeout in select_try() from 5s to 10ms.
-      # The old timeout would cause fish to frequently freeze for a 5
-      # second period.
-      p << "http://gitorious.org/fish-shell/fish-shell/commit/6b8e7b16f6d4e11e168e3ce2effe2d8f0a53b184?format=patch"
-    end
-  end
-
   def install
-    system "autoconf" if build.head?
-    system "./configure", "--prefix=#{prefix}", "--without-xsel"
+    system "autoconf"
+    system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 
