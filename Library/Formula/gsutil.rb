c68f738f5d36a17dc5ac91573504fb335a29a92d
Nat Welch <nat@natwelch.com>
2013-05-17 10:20:01 -0600

    Fix gsutil formula to use a version that has all of the dependencies.

--- Library/Formula/gsutil.rb
+++ Library/Formula/gsutil.rb
@@ -1,14 +1,19 @@
 require 'formula'
 
 # This is a pure Python formula, but Google suck and won't provide it in PYPI.
-# So fearlessly we elect to maintain the formula. References:
+# So fearlessly we elect to maintain the formula. 
+#
+# Note: We can not use the github tag download version because it doesn't 
+# include the third_party directory.
+#
+# References:
 # https://github.com/mxcl/homebrew/pull/7484
 # https://github.com/mxcl/homebrew/issues/2560
 
 class Gsutil < Formula
   homepage 'https://github.com/GoogleCloudPlatform/gsutil'
-  url 'https://github.com/GoogleCloudPlatform/gsutil/archive/v3.29.tar.gz'
-  sha1 'ae224258bcd1aa14c7dd5106766ec48640f815c8'
+  url 'http://storage.googleapis.com/pub/gsutil_3.9.tar.gz'
+  sha1 '33e728bc5d6d26c81737446843ffa0f9a5dbc8b5'
 
   def install
     libexec.install Dir["*"]
