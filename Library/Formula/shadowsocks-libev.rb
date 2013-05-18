6fd6eca8722df4b91e62ef06835eefa129a2f022
googolmo <googolmo@gmail.com>
2013-05-18 10:58:50 +0800

    add caveats

--- Library/Formula/shadowsocks-libev.rb
+++ Library/Formula/shadowsocks-libev.rb
@@ -1,27 +1,38 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class ShadowsocksLibev < Formula
   homepage 'https://github.com/madeye/shadowsocks-libev'
   url 'https://github.com/madeye/shadowsocks-libev.git'
   version '1.2'
 
   def install
-    # ENV.j1  # if your formula's build system can't parallelize
-
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
 
+  def caveats; <<-EOS.undent
+    usage:
+        ss-local -s server_host -p server_port -l local_port -k password
+        [-m encrypt_method] [-f pid_file] [-t timeout] [-c config_file]
+
+        ss-redir -s server_host -p server_port -l local_port -k password
+        [-m encrypt_method] [-f pid_file] [-t timeout] [-c config_file]
+
+        ss-server -s server_host -p server_port -k password
+        [-m encrypt_method] [-f pid_file] [-t timeout] [-c config_file]
+    options:
+        encrypt_method:         table, rc4
+        pid_file:               valid path to the pid file
+        timeout:                socket timeout in senconds
+        config_file:            json format config file
+    notes:
+        ss-redir provides a transparent proxy function and only works on the
+        Linux platform with iptables.
+    EOS
+    end
+
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test shadowsocks`.
     system "false"
   end
 end
