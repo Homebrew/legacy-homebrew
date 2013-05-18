8046b5e21e74fc856f72176b875deea7c096c7b4
Misty De Meo <mistydemeo@gmail.com>
2013-05-18 16:58:55 -0500

    xmp 4.0.5

--- Library/Formula/xmp.rb
+++ Library/Formula/xmp.rb
@@ -2,11 +2,12 @@ require 'formula'
 
 class Xmp < Formula
   homepage 'http://xmp.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/xmp/xmp/4.0.4/xmp-4.0.4.tar.gz'
-  sha1 '2eb9750790d660b014fad75d5331b4d05553302b'
+  url 'http://downloads.sourceforge.net/project/xmp/xmp/4.0.5/xmp-4.0.5.tar.gz'
+  sha1 '3de0292afc8c0e28b3f2f9328b28bc19d0fda9d1'
   head 'git://git.code.sf.net/p/xmp/xmp-cli'
 
   depends_on :autoconf if build.head?
+  depends_on 'pkg-config' => :build
   depends_on 'libxmp'
 
   def install
