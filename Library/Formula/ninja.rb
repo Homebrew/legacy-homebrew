8e7bd0c9c7764d401ac27dfcd373386fbef9ba5a
Markus Herb <mail@markusherb.de>
2013-05-18 10:38:14 +0200

    ninja 1.3.2
    Closes #19906.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ninja.rb
+++ Library/Formula/ninja.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ninja < Formula
   homepage 'https://github.com/martine/ninja'
-  url 'https://github.com/martine/ninja/archive/v1.1.0.tar.gz'
-  sha1 'ce37677326997c70da4fe49c1e948aba5b77317f'
+  url 'https://github.com/martine/ninja/archive/v1.3.2.tar.gz'
+  sha1 'c91b0df9bd4d3700695e77c31baaef74f6496152'
 
   def install
     system "./bootstrap.py"
