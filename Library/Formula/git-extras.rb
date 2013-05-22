1f86e13ff2b3119096cbe7c256bffaf7dbfb0946
Brett Koonce <koonce@gmail.com>
2013-05-21 21:06:14 -0500

    git-extras 1.8.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitExtras < Formula
   homepage 'https://github.com/visionmedia/git-extras'
-  url 'https://github.com/visionmedia/git-extras/archive/1.7.0.tar.gz'
-  sha1 'c314b947f285d92c2a227c7d73f46bca258e5111'
+  url 'https://github.com/visionmedia/git-extras/archive/1.8.0.tar.gz'
+  sha1 '5a52d3e247a5134c8e2bf89b339a204a7f4ad8e0'
 
   head 'https://github.com/visionmedia/git-extras.git', :branch => 'master'
 
