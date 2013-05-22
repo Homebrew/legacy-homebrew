f47b084de9b9cce41f34695b00a9e94752319d29
Misty De Meo <mistydemeo@gmail.com>
2013-05-22 10:03:02 -0500

    libxmp 4.1.4

--- Library/Formula/libxmp.rb
+++ Library/Formula/libxmp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libxmp < Formula
   homepage 'http://xmp.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/xmp/libxmp/4.1.3/libxmp-4.1.3.tar.gz'
-  sha1 '1d8a2629e05abc45bf19f7f488a6c0368aa38bd7'
+  url 'http://downloads.sourceforge.net/project/xmp/libxmp/4.1.4/libxmp-4.1.4.tar.gz'
+  sha1 'cb4b54ff2a24cf19d336c8360612788028727aaf'
   head 'git://git.code.sf.net/p/xmp/libxmp'
 
   depends_on :autoconf if build.head?
