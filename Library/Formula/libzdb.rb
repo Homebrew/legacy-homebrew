2695c5dfd5157c759501b1405a6cfee544079e09
Brett Koonce <koonce@gmail.com>
2013-05-23 22:23:36 -0500

    libzdb 2.11.2

--- Library/Formula/libzdb.rb
+++ Library/Formula/libzdb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libzdb < Formula
   homepage 'http://tildeslash.com/libzdb/'
-  url 'http://tildeslash.com/libzdb/dist/libzdb-2.11.1.tar.gz'
-  sha1 'c6fd44a779e7e0c112ebd7b30a1ee08f86feb41e'
+  url 'http://tildeslash.com/libzdb/dist/libzdb-2.11.2.tar.gz'
+  sha1 'a1f848dcf666566d7a481e68fdd6ad58268023f0'
 
   option 'without-sqlite',     "Compile without SQLite support"
   option 'without-postgresql', "Compile without PostgreSQL support"
