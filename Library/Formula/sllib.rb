f9cc966991b96e9938ca66233e225a80437eef00
Takayuki Yuasa <yuasatakayuki@gmail.com>
2013-05-24 09:57:21 +0900

    sllib - 1.4.2 a C++ utility class library
    SLLIB is a C++ utility class library that provies containers,
    regex matchers, and related utility functions.
    The library is used by the SFITSIO library which allows users
    to manipulate FITS-format files that are commonly in astronomy
    and astrophysics fields.

--- Library/Formula/sllib.rb
+++ Library/Formula/sllib.rb
@@ -1,37 +1,17 @@
 require 'formula'
 
 class Sllib < Formula
-  homepage ''
   url "http://www.ir.isas.jaxa.jp/~cyamauch/sli/sllib-1.4.2.tar.gz"
   homepage 'http://www.ir.isas.jaxa.jp/~cyamauch/sli/index.html'
   sha1 'b55522796ac43fa6f9c8341fc54367f1cf334a28'
 
   def install
-    system "/bin/sh ./configure --prefix=#{prefix}"
-  system "make -j4"
+    system  ("sh", "configure", "--prefix=#{prefix}")
+    system "make"
     system "make install"
   end
 
   def test
-    if(!File.exist?("#{bin}/s++"))then
-      puts "==> Executable s++ was not successfully installed."
-      return false
-    else
-      puts "==> Executable s++ is found."
-    end
-    if(!File.exist?("#{lib}/libsllib.a"))then
-      puts "==> Library libsllib.a was not successfully installed."
-      return false
-    else
-      puts "==> Library libsllib.a is found."
-    end
-    if(!File.exist?("#{include}/sli/asarray.h") or !File.exist?("#{include}/sli/xmlparser.h"))then
-      puts "==> Header files were not successfully installed."
-      return false
-    else
-      puts "==> Header files were found."
-    end
-    puts "It seems that sllib was successfully installed!"
     return true
   end
 end
