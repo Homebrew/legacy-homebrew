8749e46df29657f0112d55966e2f519a95b4eaab
Brett Koonce <koonce@gmail.com>
2013-05-23 22:24:41 -0500

    patchutils 0.3.3
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/patchutils.rb
+++ Library/Formula/patchutils.rb
@@ -2,8 +2,10 @@ require 'formula'
 
 class Patchutils < Formula
   homepage 'http://cyberelk.net/tim/software/patchutils/'
-  url 'http://cyberelk.net/tim/data/patchutils/stable/patchutils-0.3.2.tar.bz2'
-  sha1 '00c9d41318240bfae93843abd442adbdc8c4b568'
+  url 'http://cyberelk.net/tim/data/patchutils/stable/patchutils-0.3.3.tar.xz'
+  sha1 '89d3f8a454bacede1b9a112b3a13701ed876fcc1'
+
+  depends_on 'xz'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
