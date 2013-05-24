64329bbb275e57e2487b64a6853097ca5c26f5f3
Takayuki Yuasa <yuasatakayuki@gmail.com>
2013-05-24 09:57:09 +0900

    sfitsio - 1.4.2 tool for manipulating FITS files
    SFITSIO is a C++ class library that allows users to manipulate
    FITS files in a object-oriented manner. The FITS format is a
    structured file format mainly developed by NASA and is widely
    used to store and ananlyze astronomical and astrphysical data
    such as telescope image or large binary table containing photon
    event list. SFITSIO is a well-maintained library package, and
    has been widely commissioned in multiple space observatory
    missions and groud telescope projects.

--- Library/Formula/sfitsio.rb
+++ Library/Formula/sfitsio.rb
@@ -1,7 +1,6 @@
 require 'formula'
 
 class Sfitsio < Formula
-  homepage ''
   url "http://www.ir.isas.jaxa.jp/~cyamauch/sli/sfitsio-1.4.2.tar.gz"
   homepage 'http://www.ir.isas.jaxa.jp/~cyamauch/sli/index.html#SFITSIO'
   sha1 '916db23bca3848118b268faa1ed756742aef4746'
@@ -9,25 +8,12 @@ class Sfitsio < Formula
   depends_on 'sllib'
 
   def install
-    system "sh ./configure --prefix=#{prefix}"
-    system "make -j4"
+    system  ("sh", "configure", "--prefix=#{prefix}")
+    system "make"
     system "make install"
   end
 
   def test
-    if(!File.exist?("#{lib}/libsfitsio.a"))then
-      puts "==> Library libsfitsio.a was not successfully installed."
-      return false
-    else
-      puts "==> Library libsfitsio.a is found."
-    end
-    if(!File.exist?("#{include}/sli/fits.h") or !File.exist?("#{include}/sli/fitscc.h"))then
-      puts "==> Header files were not successfully installed."
-      return false
-    else
-      puts "==> Header files were found."
-    end
-    puts "It seems that sfitsio was successfully installed!"
     return true
   end
 end
