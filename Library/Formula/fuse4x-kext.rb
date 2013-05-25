e81c0047e58505a0daae5bb76c35b4155ac94746
Mike McQuaid <mike@mikemcquaid.com>
2013-05-25 15:58:38 +0100

    fuse4x-kext: update bottles.

--- Library/Formula/fuse4x-kext.rb
+++ Library/Formula/fuse4x-kext.rb
@@ -6,13 +6,11 @@ class Fuse4xKext < Formula
   sha1 '4222c14b38325d9e41fb0925d2681dda3e73e861'
 
   bottle do
-    # Bottle provided for Lion and newer since the Command Line Tools cannot
-    # compile things that use `xcodebuild`. Actual compilation takes ~10
-    # seconds so there is no need to bottle this for earlier systems.
-    revision 2
-
-    sha1 '66e546c4d8b590b0c67584b73a6731757a5d87fb' => :mountainlion
-    sha1 '08c877f8764d755e0574083ffc981105e3913a27' => :lion
+    cellar :any
+    revision 3
+    sha1 'f571d853f081883d943a6e8c1b12753c96fa4b28' => :mountain_lion
+    sha1 'd64873c1c3afa179929651ce2c08cd6f893b421f' => :lion
+    sha1 '759f3fadd80b79080a21e5cce3afe000bf410701' => :snow_leopard
   end
 
   def install
