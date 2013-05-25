077a1c17c4bb1657cb9cc856768026ba2f6ae425
Nolic <git@nolic.fr>
2013-05-25 12:59:51 +0200

    LanguageTool 2.1
    - Use now the standalone languagetool package
    
    - Add additional languagetool-server and languagetool-gui exec scripts

--- Library/Formula/languagetool.rb
+++ Library/Formula/languagetool.rb
@@ -2,11 +2,19 @@ require 'formula'
 
 class Languagetool < Formula
   homepage 'http://www.languagetool.org/'
-  url 'http://www.languagetool.org/download/LanguageTool-2.1.oxt'
-  sha1 '73310a2400378ea2fd302a6654df32a5b471aa3e'
+  url 'http://www.languagetool.org/download/LanguageTool-2.1.zip'
+  sha1 'a9ce558e42710dfd97a43928e0917dec4cab8762'
+
+  def server_script server_jar; <<-EOS.undent
+    #!/bin/bash
+    exec java -cp #{server_jar} org.languagetool.server.HTTPServer "$@"
+    EOS
+  end
 
   def install
     libexec.install Dir["*"]
-    bin.write_jar_script libexec/'LanguageTool.jar', 'languagetool'
+    bin.write_jar_script libexec/'languagetool-commandline.jar', 'languagetool'
+    (bin+'languagetool-server').write server_script(libexec/'languagetool-server.jar')
+    bin.write_jar_script libexec/'languagetool-standalone.jar', 'languagetool-gui'
   end
 end
