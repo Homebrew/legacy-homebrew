fa3ca345b0041c35efde1efc6d71cff5419740b1
Daniel Sloof <goapsychadelic@gmail.com>
2013-05-25 14:33:57 +0100

    cmake 2.8.11
    Closes #19894.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -22,14 +22,8 @@ end
 
 class Cmake < Formula
   homepage 'http://www.cmake.org/'
-  url 'http://www.cmake.org/files/v2.8/cmake-2.8.10.2.tar.gz'
-  sha1 '2d868ccc3f9f2aa7c2844bd0a4609d5313edaaec'
-
-  bottle do
-    sha1 '0bdd4bfd4a094d3fbacbe33d0968161a0f24d665' => :mountain_lion
-    sha1 '340f321eed8fd6b423980ecf9bed90646fc9331c' => :lion
-    sha1 'c23c8c6124c1d59817301678cabad9517966d899' => :snow_leopard
-  end
+  url 'http://www.cmake.org/files/v2.8/cmake-2.8.11.tar.gz'
+  sha1 '7b7961402ec71d84052734da1bed2b28e6a2c4af'
 
   depends_on NoExpatFramework
 
