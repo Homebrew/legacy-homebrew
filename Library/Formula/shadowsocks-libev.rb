60cbcd43e2ae676283ab8a45b3256cacbfb92100
googolmo <googolmo@gmail.com>
2013-05-26 20:58:44 +0800

    update git tag

--- Library/Formula/shadowsocks-libev.rb
+++ Library/Formula/shadowsocks-libev.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class ShadowsocksLibev < Formula
   homepage 'https://github.com/madeye/shadowsocks-libev'
-  url 'https://github.com/madeye/shadowsocks-libev.git', :using => :git, :revision => '8fe783e434adc7bb0949855c31a7dabb257b0bda'
+  url 'https://github.com/madeye/shadowsocks-libev.git', :using => :git, :revision => '5b7f057663898b91e174b9440864f51474cc780d'
   version '1.3'
 
   def install
