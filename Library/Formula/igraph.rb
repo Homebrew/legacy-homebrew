b69d63652a2752e124a0e3326194008fac488e6d
Thierry Moisan <thierry.moisan@gmail.com>
2013-05-27 16:49:44 -0400

    Add gmp dependancy since the configure script looks for it

--- Library/Formula/igraph.rb
+++ Library/Formula/igraph.rb
@@ -5,6 +5,8 @@ class Igraph < Formula
   url 'http://downloads.sourceforge.net/project/igraph/C%20library/0.6.5/igraph-0.6.5.tar.gz'
   sha1 'f1605c5592e8bf3c97473f7781e77b6608448f78'
 
+  depends_on 'gmp' => :build
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
