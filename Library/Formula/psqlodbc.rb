06b0590724f0307934b24e132ac3f005ab309312
Paul Ingles <paul@oobaloo.co.uk>
2013-05-27 20:40:58 +0100

    add unixodbc dependency for compilation

--- Library/Formula/psqlodbc.rb
+++ Library/Formula/psqlodbc.rb
@@ -4,6 +4,8 @@ class Psqlodbc < Formula
   homepage 'http://www.postgresql.org/'
   url 'http://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.01.0200.tar.gz'
   sha1 '9ecee7c370ec6a0d87791490dea01723436a4e2b'
+  
+  depends_on "unixodbc"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
