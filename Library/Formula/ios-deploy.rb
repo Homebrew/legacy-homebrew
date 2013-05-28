2fc05df2b51f7dd519ff77384e2c09a6a7e8711b
Shazron Abdullah <shazron@apache.org>
2013-05-28 15:43:49 -0700

    Updated ios-deploy 1.0 sha1 checksum

--- Library/Formula/ios-deploy.rb
+++ Library/Formula/ios-deploy.rb
@@ -3,7 +3,7 @@ require 'formula'
 class IosDeploy < Formula
   homepage 'https://github.com/phonegap/ios-deploy'
   url 'https://github.com/phonegap/ios-deploy/archive/1.0.tar.gz'
-  sha1 '1745f32cbf5d97ac9c8642fa13c75296b5609162'
+  sha1 'a41bb14096792c84b634c8828d01ec540e2d8793'
 
   def install
     system "make"
