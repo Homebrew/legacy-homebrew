8bcd79b46591c191f782434d2808b8ca638eeaa7
Adam Vandenberg <flangy@gmail.com>
2013-05-29 11:12:22 -0700

    bind: fix audit

--- Library/Formula/bind.rb
+++ Library/Formula/bind.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Bind < Formula
   homepage 'http://www.isc.org/software/bind/'
   url 'ftp://ftp.isc.org/isc/bind9/9.9.3/bind-9.9.3.tar.gz'
-  version '9.9.3'
   sha1 '0d770a68ccdb98db7930951cf380d5c7ed9b5b67'
 
   depends_on "openssl" if MacOS.version == :leopard
