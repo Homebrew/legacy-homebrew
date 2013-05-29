60b0aca68b972dc3a8c728508a299fe4c5a8b463
havenwood <shannonskipper@gmail.com>
2013-05-28 19:59:50 -0700

    chruby 0.3.5
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/chruby.rb
+++ Library/Formula/chruby.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Chruby < Formula
   homepage 'https://github.com/postmodern/chruby#readme'
-  url 'https://github.com/postmodern/chruby/archive/v0.3.4.tar.gz'
-  sha1 '28445cd75f514b299f6b2f2bf123793d8739a99b'
+  url 'https://github.com/postmodern/chruby/archive/v0.3.5.tar.gz'
+  sha1 'a08254910d18c20fe979f6551bd0797e287f1e5e'
 
   head 'https://github.com/postmodern/chruby.git'
 
