b6dbef5cf07a7b1ad566c41667d9d65f21299610
Adam Vandenberg <flangy@gmail.com>
2013-05-29 10:26:30 -0700

    Make nimrod usable
    Closes #20078.

--- Library/Formula/nimrod.rb
+++ Library/Formula/nimrod.rb
@@ -7,9 +7,27 @@ class Nimrod < Formula
 
   head 'https://github.com/Araq/Nimrod.git'
 
+  # Install to libexec until an upstream fix appears for
+  # https://github.com/Araq/Nimrod/issues/459
   def install
     system "/bin/sh", "./build.sh"
     inreplace 'install.sh', '$1/nimrod', '$1'
-    system "/bin/sh", "./install.sh", prefix
+    system "/bin/sh", "./install.sh", libexec
+  end
+
+  test do
+    (testpath/'hello.nim').write <<-EOS.undent
+      echo("Hi!")
+    EOS
+    system "#{libexec}/bin/nimrod compile --run hello.nim"
+  end
+
+  def caveats; <<-EOS.undent
+    Nimrod has been installed to #{libexec}.
+    The compiler will currently fail to find system.nim if called through a
+    symlink. To compile nim files, specify the full path to the compiler:
+
+      #{libexec}/bin/nimrod compile --run hello.nim
+    EOS
   end
 end
