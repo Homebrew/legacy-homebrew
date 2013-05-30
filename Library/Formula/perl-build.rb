ac3c3a77c8448aa326bfc769c40733063426c0db
tokuhirom <tokuhirom@gmail.com>
2013-05-30 17:48:20 +0900

    Added perl-build 1.02

---
+++ Library/Formula/perl-build.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class PerlBuild < Formula
+  homepage 'https://github.com/tokuhirom/Perl-Build'
+  url 'https://github.com/tokuhirom/Perl-Build/archive/1.02.tar.gz'
+  sha1 '30043e08e1bebbf9b41341bae816e7957d6167f3'
+
+  head 'https://github.com/tokuhirom/perl-build.git'
+
+  def install
+    prefix.install 'bin'
+    prefix.install 'perl-build' => 'bin/perl-build'
+  end
+end
