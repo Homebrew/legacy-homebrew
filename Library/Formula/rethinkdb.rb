a9f10f54ae8a87f51ae401228b6a0a3c697d7b24
Mojo Talantikite <mojo.talantikite@gmail.com>
2013-05-31 17:01:35 -0400

    RethinkDB: add launchd plist
    Closes #20186.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rethinkdb.rb
+++ Library/Formula/rethinkdb.rb
@@ -13,4 +13,36 @@ class Rethinkdb < Formula
     system "make"
     system "make install-osx"
   end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+          <string>#{opt_prefix}/bin/rethinkdb</string>
+          <string>-d</string>
+          <string>#{var}/rethinkdb</string>
+          <string>--bind</string>
+          <string>all</string>
+          <string>--http-port</string>
+          <string>8080</string>
+      </array>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/rethinkdb/rethinkdb.log</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/rethinkdb/rethinkdb.log</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
 end
