5d4dcd14ea8c90f1822d21da0ee699bd45055de0
Adam Vandenberg <flangy@gmail.com>
2013-05-30 20:53:04 -0700

    libgetdata 0.8.4

--- Library/Formula/libgetdata.rb
+++ Library/Formula/libgetdata.rb
@@ -2,46 +2,24 @@ require 'formula'
 
 class Libgetdata < Formula
   homepage 'http://getdata.sourceforge.net/'
-  url 'http://sourceforge.net/projects/getdata/files/getdata/0.8.3/getdata-0.8.3.tar.bz2'
-  sha1 '6db067fd3d1c40cf436cb73c26285c3dfeb902ff'
+  url 'http://sourceforge.net/projects/getdata/files/getdata/0.8.4/getdata-0.8.4.tar.bz2'
+  sha1 'fe50cc6a0a0be719a6ce06acc3beea19fcda13ce'
 
-  option 'lzma',        'Build with LZMA compression support'
-  option 'zzip',        'Build with zzip compression support'
-  option 'with-fortran','Build Fortran 77 bindings'
+  option 'with-fortran', 'Build Fortran 77 bindings'
+  option 'lzma', 'Build with LZMA compression support'
+  option 'zzip', 'Build with zzip compression support'
 
-  depends_on 'xz'          if build.include? 'lzma'
-  depends_on 'libzzip'     if build.include? 'zzip'
+  depends_on 'xz' if build.include? 'lzma'
+  depends_on 'libzzip' if build.include? 'zzip'
 
   def install
-
     ENV.fortran if build.with? 'fortran'
-
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
-
     system "./configure", *args
+    system "make"
     system "make", "install"
   end
-
-  def patches
-    # Fixes include_dirs array breaking strict.  Patch submitted upstream.
-    DATA
-  end
 end
-
-__END__
-diff --git a/bindings/perl/Build.PL.in b/bindings/perl/Build.PL.in
-index d4e5491..ee20e9c 100644
---- a/bindings/perl/Build.PL.in
-+++ b/bindings/perl/Build.PL.in
-@@ -42,7 +42,7 @@ my $build = $class->new(
-   dist_version_from => "GetData.pm",
-   extra_compiler_flags => ['@DEFS@', '-I@top_builddir@/src'],
-   extra_linker_flags => ['-L@top_builddir@/src/.libs/', '-lgetdata'],
--  include_dirs => '@top_srcdir@/src',
-+  include_dirs => ['@top_srcdir@/src'],
-   license => 'lgpl',
-   module_name => "GetData",
-   pm_files => { 'GetData.pm' => 'lib/GetData.pm' },
