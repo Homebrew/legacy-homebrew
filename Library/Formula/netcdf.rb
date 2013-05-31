19ab5373a125d60a0746452e6b7a386bd58b4d0a
Li Dong <dongli@lasg.iap.ac.cn>
2013-05-31 14:47:17 +0800

    Add 'enable-parallel-with-hdf5' option for netcdf
    NetCDF library implements parallel I/O through parallel-netcdf (in
    classic model) or hdf5 (in netcdf4 model). Because parallel-netcdf
    formula is not formally in Homebrew, I add an option as in title for
    compiling parallel I/O for netcdf through hdf5.
    
    Signed-off-by: Li Dong <dongli@lasg.iap.ac.cn>

--- Library/Formula/netcdf.rb
+++ Library/Formula/netcdf.rb
@@ -23,11 +23,13 @@ class Netcdf < Formula
   url 'http://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-4.2.1.1.tar.gz'
   sha1 '76631cb4e6b767c224338415cf6e5f5ff9bd1238'
 
+  # FIXME: If with 'enable-parallel-with-hdf5' option, hdf5 must be installed with 'enable-parallel' option.
   depends_on 'hdf5'
 
   option 'enable-fortran', 'Compile Fortran bindings'
   option 'disable-cxx', "Don't compile C++ bindings"
   option 'enable-cxx-compat', 'Compile C++ bindings for compatibility'
+  option 'enable-parallel-with-hdf5', 'Compile parallel I/O bindings through hdf5'
 
   def install
     if build.include? 'enable-fortran'
@@ -45,6 +47,32 @@ class Netcdf < Formula
       --enable-static
       --enable-shared
     ]
+
+    if build.include? 'enable-parallel-with-hdf5'
+      common_args.concat %w[--enable-parallel-tests]
+      ENV['MPICC'] = "#{HOMEBREW_PREFIX}/bin/mpicc"
+      ENV['MPICXX'] = "#{HOMEBREW_PREFIX}/bin/mpicxx"
+      ENV['MPIF77'] = "#{HOMEBREW_PREFIX}/bin/mpif77"
+      ENV['MPIF90'] = "#{HOMEBREW_PREFIX}/bin/mpif90"
+      # CC and CXX must be set, if not we will encounter the following error when running
+      # NetCDF Fortran codes with parallel I/O:
+      #
+      #   Program received signal SIGSEGV: Segmentation fault - invalid memory reference.
+      #
+      ENV['CC'] = "#{HOMEBREW_PREFIX}/bin/mpicc"
+      ENV['CXX'] = "#{HOMEBREW_PREFIX}/bin/mpicxx"
+      # F77 and FC must also be set to gfortran or any other compiler without MPI wrap, or
+      # we will encounter:
+      #
+      #   dyld: lazy symbol binding failed: Symbol not found: _nf_create_par_
+      #
+      if ENV['FC'] == ''
+        ENV['F77'] = "gfortran"
+        ENV['FC'] = "gfortran"
+      else
+        ENV['F77'] = ENV['FC']
+      end
+    end
 
     args = common_args.clone
     args.concat %w[--enable-netcdf4 --disable-doxygen]
