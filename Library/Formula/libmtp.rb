84918ebbb0deb48c1fc5abe74afa9d75e81c14d4
Adam Vandenberg <flangy@gmail.com>
2013-05-31 13:50:13 -0700

    libmtp: does not compile on Snow Leopard if libgcrypt is linked

--- Library/Formula/libmtp.rb
+++ Library/Formula/libmtp.rb
@@ -1,13 +1,26 @@
 require 'formula'
 
+class NoGcrypt < Requirement
+  satisfy(:build_env => false) { !Formula.factory("libgcrypt").installed? }
+
+  def message; <<-EOS.undent
+    This software can fail to compile when libgcrypt is installed.
+    You may need to try:
+      brew unlink libgcrypt
+      brew install libmtp
+      brew link libgcrypt
+    EOS
+  end
+end
+
 class Libmtp < Formula
   homepage 'http://libmtp.sourceforge.net/'
   url 'http://sourceforge.net/projects/libmtp/files/libmtp/1.1.6/libmtp-1.1.6.tar.gz'
   sha1 'f9e55c75399fc5f4deabcdfa58e1b01b2e6e3283'
 
+  depends_on NoGcrypt
   depends_on "pkg-config" => :build
   depends_on "libusb-compat"
-  depends_on "libgcrypt" => :optional
 
   def install
     system "./configure", "--disable-dependency-tracking",
