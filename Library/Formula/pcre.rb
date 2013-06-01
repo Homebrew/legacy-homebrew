a2bd701c23731c2fa2011fd9243b08fa12814fba
Jack Nagel <jacknagel@gmail.com>
2013-06-01 10:34:10 -0500

    pcre 8.33

--- Library/Formula/pcre.rb
+++ Library/Formula/pcre.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Pcre < Formula
   homepage 'http://www.pcre.org/'
-  url 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.32.tar.bz2'
-  mirror 'http://downloads.sourceforge.net/project/pcre/pcre/8.32/pcre-8.32.tar.bz2'
-  sha256 'a913fb9bd058ef380a2d91847c3c23fcf98e92dc3b47cd08a53c021c5cde0f55'
+  url 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.33.tar.bz2'
+  mirror 'http://downloads.sourceforge.net/project/pcre/pcre/8.33/pcre-8.33.tar.bz2'
+  sha256 'c603957a4966811c04af5f6048c71cfb4966ec93312d7b3118116ed9f3bc0478'
 
   option :universal
 
@@ -21,7 +21,8 @@ class Pcre < Formula
                           "--enable-utf8",
                           "--enable-unicode-properties",
                           "--enable-pcregrep-libz",
-                          "--enable-pcregrep-libbz2"
+                          "--enable-pcregrep-libbz2",
+                          "--enable-jit"
     system "make test"
     system "make install"
   end
