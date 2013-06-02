49f5eefb4baab31a8bb6fdc8bab849779f534e25
Paul Ingles <paul@oobaloo.co.uk>
2013-06-02 08:19:10 +0100

    psqlodbc 9.01.0200: update formula to use option for libiodbc and unixodbc dependencies;

--- Library/Formula/psqlodbc.rb
+++ Library/Formula/psqlodbc.rb
@@ -4,26 +4,29 @@ class Psqlodbc < Formula
   homepage 'http://psqlodbc.projects.pgfoundry.org/'
   url 'http://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.01.0200.tar.gz'
   sha1 '9ecee7c370ec6a0d87791490dea01723436a4e2b'
-  
-  depends_on "postgresql"
-  depends_on "unixodbc" => :optional
-  depends_on "libiodbc" => [:optional, 'with-iodbc']
+
+  option 'with-unixodbc', "Whether unixODBC driver manager is used"
+  option 'with-iodbc', "Whether iDOCB driver manager is used"
+  option 'disable-unicode', "Build without Unicode support"
+
+  depends_on "unixodbc" if build.with? "unixodbc"
+  depends_on "libiodbc" if build.with? "iodbc"
 
   def install
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}", "--enable-pthreads"]
-    args << "--with-libpq=#{Formula.factory('postgresql').lib}"
-    
+
     if build.include? "disable-unicode"
       args << "--disable-unicode"
     end
-    
-    if build.include? "with-iodbc"
+
+    if build.with? "iodbc"
       args << "--with-iodbc=#{Formula.factory('libiodbc').prefix}"
     end
-    if build.include? "with-unixodbc"
+
+    if build.with? "unixodbc"
       args << "--with-unixodbc=#{Formula.factory('unixodbc').prefix}"
     end
-    
+
     system "./configure", *args
 
     system "make"
