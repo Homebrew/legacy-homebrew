5ca5a2a8309fcb45ba8bd9f4109fe14b9c7b23f7
Adam Vandenberg <flangy@gmail.com>
2013-06-02 11:52:43 -0700

    botan: simplify
    * don't need to_s on ENV.compiler
    * soname inreplace no longer needed

--- Library/Formula/botan.rb
+++ Library/Formula/botan.rb
@@ -11,12 +11,9 @@ class Botan < Formula
     args = ["--prefix=#{prefix}"]
     args << "--cpu=x86_64" if MacOS.prefer_64_bit?
     args << "--enable-debug" if build.include? "enable-debug"
-    # The --cc option needs "clang" or "gcc" and not the full path.
-    args << "--cc=#{ENV.compiler.to_s}"
+    args << "--cc=#{ENV.compiler}"
 
     system "./configure.py", *args
-    # "-soname" can lead to issues like https://github.com/mxcl/homebrew/issues/11972
-    inreplace "Makefile", "-Wl,-soname", "-dynamiclib -install_name "
     # A hack to force them use our CFLAGS. MACH_OPT is empty in the Makefile
     # but used for each call to cc/ld.
     system "make", "install", "MACH_OPT=#{ENV.cflags}"
