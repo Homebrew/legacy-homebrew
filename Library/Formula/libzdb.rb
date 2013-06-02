572c8f3c073b990fd949b5bdb3b7e43f5dc16ab6
Jack Nagel <jacknagel@gmail.com>
2013-06-02 17:13:06 -0500

    libzdb: use recommended deps

--- Library/Formula/libzdb.rb
+++ Library/Formula/libzdb.rb
@@ -5,22 +5,18 @@ class Libzdb < Formula
   url 'http://tildeslash.com/libzdb/dist/libzdb-2.11.2.tar.gz'
   sha1 'a1f848dcf666566d7a481e68fdd6ad58268023f0'
 
-  option 'without-sqlite',     "Compile without SQLite support"
-  option 'without-postgresql', "Compile without PostgreSQL support"
-  option 'without-mysql',      "Compile without MySQL support"
-
-  depends_on :postgresql unless build.include? 'without-postgresql'
-  depends_on :mysql unless build.include? 'without-mysql'
-  depends_on 'sqlite' unless build.include? 'without-sqlite'
+  depends_on :postgresql => :recommended
+  depends_on :mysql => :recommended
+  depends_on 'sqlite' => :recommended
 
   def install
     args = ["--disable-debug",
             "--disable-dependency-tracking",
             "--prefix=#{prefix}"]
 
-    args << "--without-sqlite"     if build.include? 'without-sqlite'
-    args << "--without-mysql"      if build.include? 'without-mysql'
-    args << "--without-postgresql" if build.include? 'without-postgresql'
+    args << "--without-postgresql" if build.without? 'postgresql'
+    args << "--without-mysql" if build.without? 'mysql'
+    args << "--without-sqlite" if build.without? 'sqlite'
 
     system "./configure", *args
     system "make install"
