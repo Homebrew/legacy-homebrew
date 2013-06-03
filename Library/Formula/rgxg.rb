fa1897255ad3dff37bd22117b8f5389775ca2dea
Igmar Palsenberg <igmar@palsenberg.com>
2013-06-03 12:33:10 +0200

    rgxg: 0.1 - Added formula

---
+++ Library/Formula/rgxg.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Rgxg < Formula
+  homepage 'http://http://rgxg.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/rgxg/rgxg/rgxg-0.1.tar.gz'
+  sha1 'c57ad6daae9bc1b706a1b641d8a3c3be5c9ae743'
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    system "false"
+  end
+end
