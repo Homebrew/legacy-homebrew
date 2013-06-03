fd308f2bb04ca6ae3795d145b195426246bab909
Igmar Palsenberg <igmar@palsenberg.com>
2013-06-03 12:49:08 +0200

    Removed comment

--- Library/Formula/rgxg.rb
+++ Library/Formula/rgxg.rb
@@ -6,8 +6,6 @@ class Rgxg < Formula
   sha1 'c57ad6daae9bc1b706a1b641d8a3c3be5c9ae743'
 
   def install
-    # ENV.j1  # if your formula's build system can't parallelize
-
     system "./configure", "--prefix=#{prefix}"
     system "make", "install" # if this fails, try separate make/make install steps
   end
