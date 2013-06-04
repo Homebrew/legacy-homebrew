e62ec648f06942210a7f2c4c19e83c7310a248d8
Adam Vandenberg <flangy@gmail.com>
2013-06-03 21:06:11 -0700

    latex2html: fix indents

--- Library/Formula/latex2html.rb
+++ Library/Formula/latex2html.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class Latex2html < Formula
-    homepage 'http://ctan.uib.no/help/Catalogue/entries/latex2html.html'
-    url 'http://ctan.uib.no/support/latex2html/latex2html-2012.tgz'
-    sha1 '54b42d3fb812b0bf3d25bbde7e0ea2daf84e69ff'
+  homepage 'http://ctan.uib.no/help/Catalogue/entries/latex2html.html'
+  url 'http://ctan.uib.no/support/latex2html/latex2html-2012.tgz'
+  sha1 '54b42d3fb812b0bf3d25bbde7e0ea2daf84e69ff'
 
-    depends_on 'netpbm'
-    depends_on 'ghostscript' => :optional
+  depends_on 'netpbm'
+  depends_on 'ghostscript' => :optional
 
-    def install
-      system "./configure", "--prefix=#{prefix}", "--without-mktexlsr"
-      system "make install"
-    end
+  def install
+    system "./configure", "--prefix=#{prefix}", "--without-mktexlsr"
+    system "make install"
+  end
 end
