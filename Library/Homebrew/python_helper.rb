dfaa197bc0ebca1d03d367915af64bf64844ddfa
Samuel John <github@SamuelJohn.de>
2013-06-04 21:10:39 +0200

    Python is less verbose with "brew: Using python.."
    Now it is only shown for an `python do ... end` block
    and not for ordinary python.site_packages or other
    methods.

--- Library/Homebrew/python_helper.rb
+++ Library/Homebrew/python_helper.rb
@@ -48,7 +48,7 @@ def python_helper(options={:allowed_major_versions => [2, 3]}, &block)
       # Now is the time to set the site_packages to the correct value
       py.site_packages = lib/py.xy/'site-packages'
       if block_given?
-        puts "brew: Python block (#{py.binary})..." if ARGV.verbose?
+        puts "brew: Python block (#{py.binary})..." if ARGV.verbose? && ARGV.debug?
         require 'superenv'
         # Ensure env changes are only temporary by using `with_build_environment`
         ENV.with_build_environment do
@@ -71,8 +71,7 @@ def python_helper(options={:allowed_major_versions => [2, 3]}, &block)
           res
         end
       else
-        puts "brew: Using #{py.binary}" if ARGV.verbose?
-        # We return here with intention, because no block_given?
+        # We return the first available python, because no block_given?
         return py
       end
     end
