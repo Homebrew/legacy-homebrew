e81db021db64ddd9ae95e648ead5ee94660c3efb
Andrew Tian <andrew@turboroot.com>
2013-06-04 17:56:24 -0500

    gcutil 1.8.1
    Closes #20272.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gcutil.rb
+++ Library/Formula/gcutil.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gcutil < Formula
   homepage 'https://developers.google.com/compute/docs/gcutil/'
-  url 'https://google-compute-engine-tools.googlecode.com/files/gcutil-1.8.0.tar.gz'
-  sha1 '03ebba7ea4569a4e8f853a6226ec51999f7064b5'
+  url 'https://google-compute-engine-tools.googlecode.com/files/gcutil-1.8.1.tar.gz'
+  sha1 '746f2d30f2bd380c9c459c0ece16937caf032b8e'
 
   def install
     libexec.install 'gcutil', 'lib'
