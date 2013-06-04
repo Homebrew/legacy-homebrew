0b82b6baabe2cf8f643fcfecb431454f2741ea46
Janusz Harkot <jh@blueice.pl>
2013-06-04 20:03:00 +0200

    Add option to use alternative *SSL providers, suggest to use curl-ca-bundle to have working version of curl and libcurl

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -12,13 +12,18 @@ class Curl < Formula
   option 'with-libmetalink', 'Build with Metalink support'
   option 'with-ares', 'Build with C-Ares async DNS support'
   option 'with-ssl', 'Build with Homebrew OpenSSL instead of the system version'
+  option 'with-gnutls', 'Build with GnuTLS'
+  option 'with-polarssl', 'Build with PolarSSL'
   option 'with-darwinssl', 'Build with Secure Transport for SSL support'
 
   depends_on 'pkg-config' => :build
+  depends_on 'curl-ca-bundle' => :recommended
   depends_on 'libssh2' if build.with? 'ssh'
   depends_on 'libmetalink' if build.with? 'libmetalink'
   depends_on 'c-ares' if build.with? 'ares'
   depends_on 'openssl' if build.with? 'ssl'
+  depends_on 'gnutls' if build.with? 'gnutls'
+  depends_on 'polarssl' if build.with? 'polarssl'
 
   def install
     args = %W[
@@ -27,11 +32,15 @@ class Curl < Formula
       --prefix=#{prefix}
     ]
 
+    args << "--with-ca-bundle=#{Formula.factory("curl-ca-bundle").opt_prefix}/share/ca-bundle.crt" if build.with? 'curl-ca-bundle'
     args << "--with-libssh2" if build.with? 'ssh'
     args << "--with-libmetalink" if build.with? 'libmetalink'
     args << "--enable-ares=#{Formula.factory("c-ares").opt_prefix}" if build.with? 'ares'
     args << "--with-ssl=#{Formula.factory("openssl").opt_prefix}" if build.with? 'ssl'
     args << "--with-darwinssl" if build.with? 'darwinssl'
+    args << "--without-ssl" if build.with? 'gnutls' or build.with? 'polarssl'
+    args << "--with-gnutls" if build.with? 'gnutls'
+    args << "--with-polarssl" if build.with? 'polarssl'
 
     system "./configure", *args
     system "make install"
