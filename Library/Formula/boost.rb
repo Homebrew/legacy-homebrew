805e22566caee8f0e1c6e51faedae0be4f73597d
Mike McQuaid <mike@mikemcquaid.com>
2013-06-04 13:52:17 +0100

    boost: don't use bottle if python brew installed.
    Closes #20124.

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -43,6 +43,12 @@ class Boost < Formula
     cause "Dropped arguments to functions when linking with boost"
   end
 
+  def pour_bottle?
+    # Don't use the bottle if there is a Homebrew python installed as users
+    # will probably want to link against that instead.
+    not Formula.factory('python').installed?
+  end
+
   def install
     # Adjust the name the libs are installed under to include the path to the
     # Homebrew lib directory so executables will work when installed to a
