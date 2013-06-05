35b934018f385743f44dbbdded2178622d0de02a
adriancole <adrian.f.cole@gmail.com>
2013-06-04 23:19:30 -0700

    update to denominator 1.1.4 which include config file support
    Closes #20282.
    
    Signed-off-by: Samuel John <github@SamuelJohn.de>

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -4,10 +4,10 @@ class Denominator < Formula
   # nounzip as this is a single executable file; it is a shell script wrapper around
   # a java jar file
   homepage 'https://github.com/Netflix/denominator/tree/master/denominator-cli'
-  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/1.1.3/denominator?direct',
+  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/1.1.4/denominator?direct',
     :using  => :nounzip
-  version '1.1.3'
-  sha1 '97da0f22a2eb2854339677f5576b8995d81b2ad5'
+  version '1.1.4'
+  sha1 '1613e16a5eeeab089f6322343103e5ecf77b8238'
 
   def install
     bin.install "denominator?direct" => "denominator"
