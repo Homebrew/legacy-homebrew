35cea0ba4241f92630fc979462b03a2361bb488f
Doug Goldstein <doug.goldstein@pikewerks.com>
2013-06-06 14:12:33 -0500

    libvirt: Add python bindings (:recommended)
    With the new python dependency support in Homebrew, libvirt can now
    build Python bindings.
    
    Closes #20311.
    
    Signed-off-by: Samuel John <github@SamuelJohn.de>

--- Library/Formula/libvirt.rb
+++ Library/Formula/libvirt.rb
@@ -11,6 +11,7 @@ class Libvirt < Formula
   depends_on 'gnutls'
   depends_on 'libgcrypt'
   depends_on 'yajl'
+  depends_on :python => :recommended
 
   if MacOS.version == :leopard
     # Definitely needed on Leopard, but not on Snow Leopard.
@@ -37,7 +38,8 @@ class Libvirt < Formula
             "--with-yajl",
             "--without-qemu"]
 
-    args << "--without-libvirtd" if build.include? 'without-libvirtd'
+    args << "--without-libvirtd" if build.without? 'libvirtd'
+    args << "--without-python" if build.wihtout? 'python'
 
     system "./configure", *args
 
