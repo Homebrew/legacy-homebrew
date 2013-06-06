450e06c6f7184581f996f971adc56663150b7567
Adam Vandenberg <flangy@gmail.com>
2013-06-06 16:54:52 -0700

    Warn when overriding ScriptFileFormula#install
    ScriptFileFormula's use is that it installs whatever was downloaded
    to bin; if the install is overridden, there's no benefit over deriving
    directly from Formula.

--- Library/Homebrew/formula_specialties.rb
+++ Library/Homebrew/formula_specialties.rb
@@ -5,6 +5,14 @@ class ScriptFileFormula < Formula
   def install
     bin.install Dir['*']
   end
+
+  def self.method_added method
+    super method
+    case method
+    when :install
+      opoo "#{name}: if you are overriding ScriptFileFormula#install, use a Formula instead"
+    end
+  end
 end
 
 # See browser for an example
