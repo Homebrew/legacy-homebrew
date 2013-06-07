70439203ab41183880cb5879918644c2f226dc44
Adam Vandenberg <flangy@gmail.com>
2013-06-06 17:03:44 -0700

    Use ScriptFileFormula
    Switch these to use ScriptFileFormula, since they no longer
    need to munge the installed filenames.

--- Library/Formula/rpm2cpio.rb
+++ Library/Formula/rpm2cpio.rb
@@ -1,14 +1,10 @@
 require 'formula'
 
-class Rpm2cpio < Formula
+class Rpm2cpio < ScriptFileFormula
   homepage 'http://svnweb.freebsd.org/ports/head/archivers/rpm2cpio/'
   url 'http://svnweb.freebsd.org/ports/head/archivers/rpm2cpio/files/rpm2cpio?revision=259745'
   version '1.3'
   sha1 '7bd6e848eed9444a4dacf9759cc195ca56ab19b2'
 
-  depends_on 'xz'
-
-  def install
-    bin.install 'rpm2cpio?revision=259745' => 'rpm2cpio.pl'
-  end
+  depends_on 'xz' # runtime dependency
 end
