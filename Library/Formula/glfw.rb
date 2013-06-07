692943e5f26b51d53d5f576a8f41e75a27fad500
Hector Dearman <hector.dearman@gmail.com>
2013-06-07 03:55:01 +0100

    glfw: Add the universal flag to glfw
    Let users pass glfw the universal flag in the standard way.

--- Library/Formula/glfw.rb
+++ Library/Formula/glfw.rb
@@ -5,7 +5,10 @@ class Glfw < Formula
   url 'http://downloads.sourceforge.net/project/glfw/glfw/2.7.7/glfw-2.7.7.tar.bz2'
   sha1 'fce3baff5ae8ca8583fe91e63a23c3aad593b016'
 
+  option :universal
+
   def install
+    ENV.universal_binary if build.universal?
     system 'make', "PREFIX=#{prefix}", 'cocoa-dist-install'
   end
 end
