75ae2f0857361b5f88ea1d26e4b0deb04745e4dc
Jack Nagel <jacknagel@gmail.com>
2013-06-07 12:12:07 -0500

    automake 1.13.3

--- Library/Formula/automake.rb
+++ Library/Formula/automake.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Automake < Formula
   homepage 'http://www.gnu.org/software/automake/'
-  url 'http://ftpmirror.gnu.org/automake/automake-1.13.2.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/automake/automake-1.13.2.tar.gz'
-  sha1 '72ee9fcd180c54fd7c067155d85fa071a99c3ea3'
+  url 'http://ftpmirror.gnu.org/automake/automake-1.13.3.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/automake/automake-1.13.3.tar.gz'
+  sha1 'f46864c28755842d6d789793b7359a079c1491ae'
 
   # Always needs a newer autoconf, even on Snow Leopard.
   depends_on 'autoconf'
