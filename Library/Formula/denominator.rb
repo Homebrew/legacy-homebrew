40e33d869c0684e71abf92998647d98e3a1a4456
adriancole <adrian.f.cole@gmail.com>
2013-06-07 11:50:15 -0700

    upgrade denominator to 1.2.0: supports providers that allow duplicate zone names such as route53 and rackspace

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -4,10 +4,10 @@ class Denominator < Formula
   # nounzip as this is a single executable file; it is a shell script wrapper around
   # a java jar file
   homepage 'https://github.com/Netflix/denominator/tree/master/denominator-cli'
-  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/1.1.4/denominator?direct',
+  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/1.2.0/denominator?direct',
     :using  => :nounzip
-  version '1.1.4'
-  sha1 '1613e16a5eeeab089f6322343103e5ecf77b8238'
+  version '1.2.0'
+  sha1 '9331a1eb9c4c68c40b621e0b92d7e1e471a56bae'
 
   def install
     bin.install "denominator?direct" => "denominator"
