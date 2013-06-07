e83a9bec485e218dd449df93c5564c834fde1a2a
Samuel John <github@SamuelJohn.de>
2013-06-07 17:52:03 +0200

    youtube-dl: Python 2.x and 3.x support

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -5,10 +5,16 @@ class YoutubeDl < Formula
   url 'http://youtube-dl.org/downloads/2013.05.23/youtube-dl-2013.05.23.tar.gz'
   sha1 '68e70bf1f285536e84e4fdf71c3d27fefd830612'
 
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
+
   def install
-    system "make", "youtube-dl", "PREFIX=#{prefix}"
-    bin.install 'youtube-dl'
-    man1.install 'youtube-dl.1'
+    python do
+      system python, "setup.py", "install", "--prefix=#{prefix}",
+                                            "--record=installed.txt",
+                                            "--single-version-externally-managed"
+      cp bin/'youtube-dl', bin/"youtube-dl#{python.version.major}"
+    end
     bash_completion.install 'youtube-dl.bash-completion'
   end
 
