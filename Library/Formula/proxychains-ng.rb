016180a5096c9a61bfdeef371d01f3a829363614
rofl0r <retnyg@gmx.net>
2013-06-08 13:16:43 +0200

    proxychains-ng: add head tag pointing to git repo

--- Library/Formula/proxychains-ng.rb
+++ Library/Formula/proxychains-ng.rb
@@ -3,6 +3,7 @@ require 'formula'
 class ProxychainsNG < Formula
   homepage 'https://sourceforge.net/projects/proxychains-ng'
   url 'http://surfnet.dl.sourceforge.net/project/proxychains-ng/proxychains-4.5.tar.bz2'
+  head 'git://github.com/rofl0r/proxychains.git'
   sha1 'b7d85c6dfae83ec50a92c232f674231192f1ca33'
 
   def install
