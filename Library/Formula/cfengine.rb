f296d845f36b05735f5043168b4b22e669aa5ed6
Adam Vandenberg <flangy@gmail.com>
2013-06-07 17:18:50 -0700

    cfengine: use opt_prefix for pcre

--- Library/Formula/cfengine.rb
+++ Library/Formula/cfengine.rb
@@ -5,12 +5,12 @@ class Cfengine < Formula
   url 'https://cfengine.com/source-code/download?file=cfengine-3.4.4.tar.gz'
   sha1 'f99174fcb358a263b1a1090668d7ba2ba849bbc1'
 
-  depends_on 'tokyo-cabinet'
   depends_on 'pcre'
+  depends_on 'tokyo-cabinet'
 
   def install
     # Find our libpcre
-    ENV.append 'LDFLAGS', "-L#{HOMEBREW_PREFIX}/lib"
+    ENV.append 'LDFLAGS', "-L#{Formula.factory('pcre').opt_prefix}/lib"
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
