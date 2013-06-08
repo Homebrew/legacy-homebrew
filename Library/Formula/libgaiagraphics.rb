53edd22e157ab0a8f56e7bc51a62531011714a57
Joachim LeBlanc <demosdemon@gmail.com>
2013-06-07 20:58:42 -0500

    libspatialite 4.1.0 libgaiagraphics, librasterlite
    * Gdal: Use includes from brewed sqlite
    * spatialite-tools 4.1.0
    * librasterlite 1.1g
    * libgaiagraphics 0.5
    * Update to use new style `build.with?`
    
    Closes #20341.
    
    Signed-off-by: Samuel John <github@SamuelJohn.de>

--- Library/Formula/libgaiagraphics.rb
+++ Library/Formula/libgaiagraphics.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libgaiagraphics < Formula
   homepage 'https://www.gaia-gis.it/fossil/libgaiagraphics/index'
-  url 'http://www.gaia-gis.it/gaia-sins/gaiagraphics-sources/libgaiagraphics-0.4b.tar.gz'
-  sha1 'd045ad6d22db9e67ba410a62b9398c337786fe53'
+  url 'http://www.gaia-gis.it/gaia-sins/gaiagraphics-sources/libgaiagraphics-0.5.tar.gz'
+  sha1 'db9eaef329fc650da737c71aac6136088fcb6549'
 
   depends_on 'pkg-config' => :build
   depends_on 'libgeotiff'
