49571920692aea23f3eac89543cb4e92a38e05cf
Adam Vandenberg <flangy@gmail.com>
2013-06-08 09:02:17 -0700

    MySQL: allow compilation with Xcode-only setups
    Closes #20185.

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -42,6 +42,12 @@ class Mysql < Formula
   end
 
   def install
+    # Don't hard-code the libtool path. See:
+    # https://github.com/mxcl/homebrew/issues/20185
+    inreplace "cmake/libutils.cmake",
+      "COMMAND /usr/bin/libtool -static -o ${TARGET_LOCATION}",
+      "COMMAND libtool -static -o ${TARGET_LOCATION}"
+
     # Build without compiler or CPU specific optimization flags to facilitate
     # compilation of gems and other software that queries `mysql-config`.
     ENV.minimal_optimization
