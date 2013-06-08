6a71ddf4b7c054ed7b4c9996a7a82c44c7a6015c
darron froese <darron@froese.org>
2013-06-08 14:43:44 -0600

    Create exenv.rb
    Was missing - appears to work.

---
+++ Library/Formula/exenv.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Exenv < Formula
+  homepage 'https://github.com/mururu/exenv'
+  url 'https://github.com/mururu/exenv/archive/master.zip'
+  sha1 '2a796bca35136e2abc24a5fe2868dec3858edd3c'
+  version '0.1.0'
+  head 'https://github.com/mururu/exenv.git'
+
+  def install
+    inreplace 'libexec/exenv', '/usr/local', HOMEBREW_PREFIX
+    prefix.install Dir['*']
+  end
+
+  def caveats; <<-EOS.undent
+    To use Homebrew's directories rather than ~/.exenv add to your profile:
+      export EXENV_ROOT=#{var}/exenv
+
+    To enable shims and autocompletion add to your profile:
+      if which exenv > /dev/null; then eval "$(exenv init -)"; fi
+    EOS
+  end
+end
