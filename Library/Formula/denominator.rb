1f5ac59309b936161e6c730778205b3cdf2078c4
adriancole <adrian.f.cole@gmail.com>
2013-06-08 07:55:07 -0700

    upgrade to denominator 1.2.1: better error message when listing zones and the -Z arg doesn't match what's on the server.

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -3,10 +3,10 @@ require 'formula'
 class Denominator < ScriptFileFormula
   # This is a shell script wrapping a java jar; download as single file using nounzip
   homepage 'https://github.com/Netflix/denominator/tree/master/denominator-cli'
-  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/1.2.0/denominator?direct',
+  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/1.2.1/denominator?direct',
     :using  => :nounzip
-  version '1.2.0'
-  sha1 '9331a1eb9c4c68c40b621e0b92d7e1e471a56bae'
+  version '1.2.1'
+  sha1 '2d9b9248bdff1f5c09a19bf6d71f7f28ef3e1306'
 
   test do
     system "#{bin}/denominator", "help"
