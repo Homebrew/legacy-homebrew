647091e9bfb33da3c8d3c2633bb0fce5c3d89c21
Yurii Samsoniuk <mr.sigito@gmail.com>
2013-06-11 23:38:20 +0300

    Updated shasum.

--- Library/Formula/dvorak7min.rb
+++ Library/Formula/dvorak7min.rb
@@ -4,7 +4,7 @@ class Dvorak7min < Formula
   homepage 'http://dvorak7min.sourcearchive.com/'
   url 'http://dvorak7min.sourcearchive.com/downloads/1.6.1-13.1/dvorak7min_1.6.1.orig.tar.gz'
   version '1.6.1'
-  sha1 '83f7ec4eba3fa33cf0547a8614ee02e50ff21c81'
+  sha1 'e531ce2e6c64a4867f5bb7e03d64f8fbb0ce8707'
 
   def install
     system "make"
