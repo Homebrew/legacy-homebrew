40578e7eb50a1340c1342dfa5bcf2f9e581f0409
Adam Vandenberg <flangy@gmail.com>
2013-06-11 07:47:12 -0700

    proxychains-ng: fix url

--- Library/Formula/proxychains-ng.rb
+++ Library/Formula/proxychains-ng.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class ProxychainsNg < Formula
   homepage 'https://sourceforge.net/projects/proxychains-ng'
-  url 'http://surfnet.dl.sourceforge.net/project/proxychains-ng/proxychains-4.5.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/proxychains-ng/proxychains-4.5.tar.bz2'
   sha1 'b7d85c6dfae83ec50a92c232f674231192f1ca33'
 
   head 'https://github.com/rofl0r/proxychains-ng.git'
