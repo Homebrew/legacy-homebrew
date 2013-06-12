453a5811505f331cde7f53ad48d10f31424751f3
Jack Nagel <jacknagel@gmail.com>
2013-06-12 09:02:20 -0500

    install: remove overcautious array manipulation

--- Library/Homebrew/cmd/install.rb
+++ Library/Homebrew/cmd/install.rb
@@ -18,7 +18,8 @@ module Homebrew extend self
       end
     end unless ARGV.force?
 
-    install_formulae ARGV.formulae
+    perform_preinstall_checks
+    ARGV.formulae.each { |f| install_formula(f) }
   end
 
   def check_ppc
@@ -67,16 +68,6 @@ module Homebrew extend self
     check_xcode
     check_macports
     check_cellar
-  end
-
-  def install_formulae formulae
-    formulae = [formulae].flatten.compact
-    unless formulae.empty?
-      perform_preinstall_checks
-      formulae.each do |f|
-        install_formula(f)
-      end
-    end
   end
 
   def install_formula f
