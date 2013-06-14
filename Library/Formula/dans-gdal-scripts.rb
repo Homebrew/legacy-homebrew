838e057bf045bae6ec4d851ddacb2fac6c7debaf
Dayne Broderson <broderson@gmail.com>
2013-06-13 19:56:07 -0800

    applying mods recommened by homebrew devs in pull request

--- Library/Formula/dans-gdal-scripts.rb
+++ Library/Formula/dans-gdal-scripts.rb
@@ -6,21 +6,21 @@ class DansGdalScripts < Formula
   sha1 '021d8dfa2b636c20be4e91dea69fefda1618b53e'
   version '0.22'
 
+  depends_on :autoconf
   depends_on 'automake'
-  depends_on 'autoconf'
   depends_on 'boost'
   depends_on 'gdal'
   depends_on :libtool
 
   def install
-    (buildpath/'m4').mkpath # or autogen fails
+    (buildpath/'m4').mkpath
     system "./autogen.sh"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    `gdal_list_corners`.split[0] == "Usage:" # ghetto test but it is a start
+    `gdal_list_corners`.split[0] == "Usage:" 
   end
 end
