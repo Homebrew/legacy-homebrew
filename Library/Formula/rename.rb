9d987fa55f435b70303ea7d2ca557042aa55248e
Brett Koonce <koonce@gmail.com>
2013-06-14 13:36:45 -0500

    rename v1.600
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rename.rb
+++ Library/Formula/rename.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rename < Formula
   homepage 'http://plasmasturm.org/code/rename'
-  url 'https://github.com/ap/rename/archive/v1.500.tar.gz'
-  sha1 'a1315f57191cf78191b23cd20ebfe3afe7056c7e'
+  url 'https://github.com/ap/rename/archive/v1.600.tar.gz'
+  sha1 'a7946ce3602e3810aaa70300674ccb26832634ed'
 
   def install
     system 'pod2man', 'rename', 'rename.1'
