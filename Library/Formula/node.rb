bc690baa03b25ec17a865a8b3e79369737a9aa97
Mario Uher <uher.mario@gmail.com>
2013-06-14 10:02:36 +0200

    node v0.10.11

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -31,8 +31,8 @@ end
 
 class Node < Formula
   homepage 'http://nodejs.org/'
-  url 'http://nodejs.org/dist/v0.10.10/node-v0.10.10.tar.gz'
-  sha1 'c21643a57b1ec9eca56e6deac22fe075616e0e9c'
+  url 'http://nodejs.org/dist/v0.10.11/node-v0.10.11.tar.gz'
+  sha1 '4f646bb3418d1c177ce28cdbc61e71de35b38aea'
 
   devel do
     url 'http://nodejs.org/dist/v0.11.2/node-v0.11.2.tar.gz'
