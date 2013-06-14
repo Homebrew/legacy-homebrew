76020aa51f6245c0c1c050aaea134997b46534c9
Dayne Broderson <broderson@gmail.com>
2013-06-13 20:01:44 -0800

    using symbols as recommended, let brew autodetect version

--- Library/Formula/dans-gdal-scripts.rb
+++ Library/Formula/dans-gdal-scripts.rb
@@ -4,13 +4,12 @@ class DansGdalScripts < Formula
   homepage 'https://github.com/gina-alaska/dans-gdal-scripts/'
   url 'https://github.com/gina-alaska/dans-gdal-scripts/archive/v0.22.zip'
   sha1 '021d8dfa2b636c20be4e91dea69fefda1618b53e'
-  version '0.22'
 
   depends_on :autoconf
-  depends_on 'automake'
+  depends_on :automake
+  depends_on :libtool
   depends_on 'boost'
   depends_on 'gdal'
-  depends_on :libtool
 
   def install
     (buildpath/'m4').mkpath
