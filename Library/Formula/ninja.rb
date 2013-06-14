2d0a3ef09a6ac0d828aa87ab970fd3acffb52543
Brett Koonce <koonce@gmail.com>
2013-06-14 13:36:33 -0500

    ninja v1.3.4

--- Library/Formula/ninja.rb
+++ Library/Formula/ninja.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ninja < Formula
   homepage 'https://github.com/martine/ninja'
-  url 'https://github.com/martine/ninja/archive/v1.3.3.tar.gz'
-  sha1 '1e27d85316a267afa5e98ed62b660395ab5b9851'
+  url 'https://github.com/martine/ninja/archive/v1.3.4.tar.gz'
+  sha1 'e6ac7d49b2b5913956ad6740c8612981183808af'
 
   depends_on :python
 
