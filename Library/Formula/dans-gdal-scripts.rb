da41e215e90aec672fb4aadf02d67b75dda7a1ba
Dayne Broderson <broderson@gmail.com>
2013-06-13 19:49:04 -0800

    pointing at official repository for v0.22 after getting thumbs up from Dan

--- Library/Formula/dans-gdal-scripts.rb
+++ Library/Formula/dans-gdal-scripts.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class DansGdalScripts < Formula
   homepage 'https://github.com/gina-alaska/dans-gdal-scripts/'
-  url 'https://github.com/dayne/dans-gdal-scripts/archive/v0.22.zip'
+  url 'https://github.com/gina-alaska/dans-gdal-scripts/archive/v0.22.zip'
   sha1 '021d8dfa2b636c20be4e91dea69fefda1618b53e'
   version '0.22'
 
