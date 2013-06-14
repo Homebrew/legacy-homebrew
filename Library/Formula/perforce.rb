50cca7e9274375d4c88fa1799b01d785388fad16
Károly Lőrentey <karoly@lorentey.hu>
2013-06-14 14:10:46 +0200

    p4: 2013.1
    Closes #20496.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/perforce.rb
+++ Library/Formula/perforce.rb
@@ -4,13 +4,13 @@ class Perforce < Formula
   homepage 'http://www.perforce.com/'
 
   if MacOS.prefer_64_bit?
-    url 'http://filehost.perforce.com/perforce/r12.2/bin.darwin90x86_64/p4'
-    version '2012.2.631250-x86_64'
-    sha1 'bd294fae92a0b9e450000a7e6a793aec9839aa58'
+    url 'http://filehost.perforce.com/perforce/r13.1/bin.darwin90x86_64/p4'
+    version '2013.1.610569-x86_64'
+    sha1 '8a8b349dc698699b125185e285303a4c855c4cd2'
   else
-    url 'http://filehost.perforce.com/perforce/r12.2/bin.darwin90x86/p4'
-    version '2012.2.631250-x86'
-    sha1 '38fba9371be46e5b163291d826efb1eca94d046f'
+    url 'http://filehost.perforce.com/perforce/r13.1/bin.darwin90x86/p4'
+    version '2013.1.610569-x86'
+    sha1 '78998ee6875056bd1a367a5aea9d1d76ff2e7c1f'
   end
 
   def install
