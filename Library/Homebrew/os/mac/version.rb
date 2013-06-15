3e4e6c6551344cec577861b6fe29e640fef80ee3
Jack Nagel <jacknagel@gmail.com>
2013-06-14 19:10:14 -0500

    Pass these as strings, they get converted anyway

--- Library/Homebrew/os/mac/version.rb
+++ Library/Homebrew/os/mac/version.rb
@@ -4,11 +4,11 @@ module MacOS
   class Version < ::Version
     def <=>(other)
       v = case other
-          when :mountain_lion then 10.8
-          when :lion          then 10.7
-          when :snow_leopard  then 10.6
-          when :leopard       then 10.5
-          when :tiger         then 10.4
+          when :mountain_lion then "10.8"
+          when :lion          then "10.7"
+          when :snow_leopard  then "10.6"
+          when :leopard       then "10.5"
+          when :tiger         then "10.4"
           else other.to_s
           end
       super(Version.new(v))
