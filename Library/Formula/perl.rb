dcea6f262710fba600fbc7f1d4eab36906658817
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2013-06-17 00:32:36 +0800

    Remove caveats and enable threads support by default.

--- Library/Formula/perl.rb
+++ Library/Formula/perl.rb
@@ -12,7 +12,7 @@ class Perl < Formula
     sha1 'e2a73a07629267f28ec537b01ad847164e2d07df'
   end
 
-  option 'use-threads', 'Enable Perl threads'
+  option 'disable-threads', 'Disable Perl threads support'
   option 'skip-test', 'Skip `make test` step'
 
   def install
@@ -25,20 +25,12 @@ class Perl < Formula
         '-Duselargefiles',
     ]
 
-    args << '-Dusethreads' if build.include? 'use-threads'
+    args << '-Dusethreads' unless build.include? 'disable-threads'
     args << '-Dusedevel' if build.devel? or build.head?
 
     system './Configure', *args
     system "make"
     system "make test" unless build.include? 'skip-test'
     system "make install"
-  end
-
-  def caveats
-    unless build.include? 'use-threads' then <<-EOS.undent
-      Builds without threads by default. Use --use-threads to build with threads.
-      Builds with `make test` by default. Use --skip-test to skip `make test` step.
-      EOS
-    end
   end
 end
