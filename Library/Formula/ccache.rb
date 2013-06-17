eeac1176a29f7ced089445cbe81b24ef2801ff44
Reed Riley <john.reed.riley@gmail.com>
2013-06-17 12:33:27 -0700

    ccache: added symlinks for more compiler versions (fixes #20561)

--- Library/Formula/ccache.rb
+++ Library/Formula/ccache.rb
@@ -22,10 +22,12 @@ class Ccache < Formula
     libexec.mkpath
 
     %w[
+      clang
+      clang++
       cc
-      gcc gcc2 gcc3 gcc-3.3 gcc-4.0 gcc-4.2
-      c++ c++3 c++-3.3 c++-4.0 c++-4.2
-      g++ g++2 g++3 g++-3.3 g++-4.0 g++-4.2
+      gcc gcc2 gcc3 gcc-3.3 gcc-4.0 gcc-4.2 gcc-4.3 gcc-4.4 gcc-4.5 gcc-4.6 gcc-4.7 gcc-4.8 gcc-4.9
+      c++ c++3 c++-3.3 c++-4.0 c++-4.2 c++-4.3 c++-4.4 c++-4.5 c++-4.6 c++-4.7 c++-4.8 c++-4.9
+      g++ g++2 g++3 g++-3.3 g++-4.0 g++-4.2 g++-4.3 g++-4.4 g++-4.5 g++-4.6 g++-4.7 g++-4.8 g++-4.9
     ].each do |prog|
       ln_s bin+"ccache", libexec + prog
     end
