013bc013e0286a88f8a96d0023d4336c2f2a3044
Adam Vandenberg <flangy@gmail.com>
2013-06-17 18:04:31 -0700

    matlab2tikz 0.3.3

--- Library/Formula/matlab2tikz.rb
+++ Library/Formula/matlab2tikz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Matlab2tikz < Formula
   homepage 'https://github.com/nschloe/matlab2tikz'
-  url 'https://github.com/nschloe/matlab2tikz/archive/0.3.2.tar.gz'
-  sha1 '026762e58b51a14c91921baf556cd5d349c8b8d1'
+  url 'https://github.com/nschloe/matlab2tikz/archive/0.3.3.tar.gz'
+  sha1 'eb0a0991d34543e133256a17c6fc6f96112c8c5a'
 
   head 'https://github.com/nschloe/matlab2tikz.git'
 
