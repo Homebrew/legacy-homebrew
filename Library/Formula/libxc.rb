80baa0c826dce053f4975dcbc5390b6c702b9bb0
rpmbuild <rpmbuild@macmini01.fysik.dtu.dk>
2013-06-19 22:44:30 +0200

    LibXC: based on #10939 , uses clang instead of gcc
    Based on https://github.com/mxcl/homebrew/pull/10939
    Updated to libxc-2.0.1, sha1 instead of md5, and clang instead of gcc

---
+++ Library/Formula/libxc.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Libxc < Formula
+  homepage 'http://www.tddft.org/programs/octopus/wiki/index.php/Libxc'
+  url 'http://www.tddft.org/programs/octopus/down.php?file=libxc/libxc-2.0.1.tar.gz'
+  sha1 'f87a83fd1fbc2595902bef9c0f957ef3b00fef31'
+
+  def install
+    ENV.fortran
+    system "./configure", "FCCPP=clang -E -C -ansi",
+           "CC=clang", "CFLAGS=-pipe",
+           "--prefix=#{prefix}", "--enable-shared"
+    system "make install"
+  end
+end
