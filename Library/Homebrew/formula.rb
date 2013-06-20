78308c1577cb0212d04b8677cf6f8739ca58d9de
Jack Nagel <jacknagel@gmail.com>
2013-06-20 16:25:58 -0500

    Use a Set for skip_clean_paths

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -789,10 +789,9 @@ class Formula
         return
       end
 
-      @skip_clean_paths ||= []
       paths.each do |p|
         p = p.to_s unless p == :la # Keep :la in paths as a symbol
-        @skip_clean_paths << p unless @skip_clean_paths.include? p
+        skip_clean_paths << p
       end
     end
 
@@ -801,7 +800,7 @@ class Formula
     end
 
     def skip_clean_paths
-      @skip_clean_paths or []
+      @skip_clean_paths ||= Set.new
     end
 
     def keg_only reason, explanation=nil
