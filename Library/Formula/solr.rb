74884935a42ed8b4abd7bc2c20ecbb255f90f357
Yago Riveiro <yago.riveiro@gmail.com>
2013-06-20 17:17:04 +0100

    Solr 4.3.1

--- Library/Formula/solr.rb
+++ Library/Formula/solr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Solr < Formula
   homepage 'http://lucene.apache.org/solr/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.3.0/solr-4.3.0.tgz'
-  sha1 '8c62319d7eb0a42a223888d98aa9f5e1472bd7af'
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.3.1/solr-4.3.1.tgz'
+  sha1 '193954d743b5fa4d652764d20db17f81fd3a5c76'
 
   def script; <<-EOS.undent
     #!/bin/sh
