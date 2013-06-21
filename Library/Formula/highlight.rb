b88b8b2a4a467d04957062d9308d3e2eb2f81182
BiZoNiX <bizonix@me.com>
2013-06-21 21:04:09 +0300

    highlight 3.14
    Closes #20669.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/highlight.rb
+++ Library/Formula/highlight.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Highlight < Formula
   homepage 'http://www.andre-simon.de/doku/highlight/en/highlight.html'
-  url 'http://www.andre-simon.de/zip/highlight-3.12.tar.bz2'
-  sha1 '4d96b4d91ff08268a338847b9ea408d741b91573'
+  url 'http://www.andre-simon.de/zip/highlight-3.14.tar.bz2'
+  sha1 '02dd278367e9029baeb396cd6af77f11ceb731c5'
 
   depends_on 'pkg-config' => :build
   depends_on 'boost'
@@ -12,8 +12,7 @@ class Highlight < Formula
   def install
     inreplace "src/makefile" do |s|
       s.change_make_var! "CXX", ENV.cxx
-      s.gsub! /^(CFLAGS):=.*$/, "\\1 = #{ENV.cppflags}"
-      s.gsub! /^(LUA_LIBS)=(.*)$/, "\\1 = #{ENV.ldflags} \\2"
+      s.gsub! /lua5.1/, "lua"
     end
 
     conf_dir = etc+'highlight/' # highlight needs a final / for conf_dir
