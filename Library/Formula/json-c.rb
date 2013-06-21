1a35cc8e349147744d66d2663faa6e2edbea43a0
Kashif Rasul <kashif.rasul@gmail.com>
2013-06-21 17:22:57 +0200

    fix download url

--- Library/Formula/json-c.rb
+++ Library/Formula/json-c.rb
@@ -2,7 +2,8 @@ require 'formula'
 
 class JsonC < Formula
   homepage 'https://github.com/json-c/json-c/wiki'
-  url 'https://github.com/downloads/json-c/json-c/json-c-0.11.tar.gz'
+  version '0.11'
+  url 'https://github.com/json-c/json-c/archive/json-c-0.11-20130402.tar.gz'
   sha1 '1910e10ea57a743ec576688700df4a0cabbe64ba'
 
   def install
