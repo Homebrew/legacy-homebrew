8edab3d8bd53b09a2e7057373801c9f1073ac34b
Gary V. Vaughan <gary@gnu.org>
2013-06-22 14:48:32 +0700

    lua-5.2.2
    * Upgrade lua Formula to latest 5.2.2 release.
    * There is no :universal option in homebrew/versions/lua52.lua, so
      I removed it here too.
    * There is no .pc file shipped with 5.2.2, so I removed the edits
      to it from the Formula.

--- Library/Formula/lua.rb
+++ Library/Formula/lua.rb
@@ -1,18 +1,15 @@
 require 'formula'
 
 class Lua < Formula
-  # 5.2 is not fully backwards compatible, and breaks e.g. luarocks.
-  # It is available in Homebrew-versions for the time being.
   homepage 'http://www.lua.org/'
-  url 'http://www.lua.org/ftp/lua-5.1.5.tar.gz'
-  sha1 'b3882111ad02ecc6b972f8c1241647905cb2e3fc'
+  url 'http://www.lua.org/ftp/lua-5.2.2.tar.gz'
+  sha1 '0857e41e5579726a4cb96732e80d7aa47165eaf5'
 
   fails_with :llvm do
     build 2326
     cause "Lua itself compiles with LLVM, but may fail when other software tries to link."
   end
 
-  option :universal
   option 'with-completion', 'Enables advanced readline support'
   option 'without-sigaction', 'Revert to ANSI signal instead of improved POSIX sigaction'
 
@@ -21,48 +18,37 @@ class Lua < Formula
     # Be sure to build a dylib, or else runtime modules will pull in another static copy of liblua = crashy
     # See: https://github.com/mxcl/homebrew/pull/5043
     # Also, take care of versioned file suffixes to support parallel installation with other releases
-    p << 'https://gist.github.com/gvvaughan/5832455/raw/ad53d564bb0abb68a7cd9f27b4ef82e2bb2bd3ae/lua-5.1-homebrew.diff'
+    p << 'https://gist.github.com/gvvaughan/5832054/raw/1f1d726cced4f23719de4aef24d965d257d6bbba/lua-5.2-homebrew.diff'
     # sigaction provided by posix signalling power patch from
     # http://lua-users.org/wiki/LuaPowerPatches
     unless build.without? 'sigaction'
-      p << 'http://lua-users.org/files/wiki_insecure/power_patches/5.1/sig_catch.patch'
+      p << 'http://lua-users.org/files/wiki_insecure/power_patches/5.2/lua-5.2.2-sig_catch.patch'
     end
     # completion provided by advanced readline power patch from
     # http://lua-users.org/wiki/LuaPowerPatches
     if build.with? 'completion'
-      p << 'http://luajit.org/patches/lua-5.1.4-advanced_readline.patch'
+      p << 'http://luajit.org/patches/lua-5.2.0-advanced_readline.patch'
     end
     p
   end
 
   def install
-    ENV.universal_binary if build.universal?
-
     # Use our CC/CFLAGS to compile.
     inreplace 'src/Makefile' do |s|
       s.remove_make_var! 'CC'
-      s.change_make_var! 'CFLAGS', "#{ENV.cflags} $(MYCFLAGS)"
+      s.change_make_var! 'CFLAGS', "#{ENV.cflags} -DLUA_COMPAT_ALL $(SYSCFLAGS) $(MYCFLAGS)"
       s.change_make_var! 'MYLDFLAGS', ENV.ldflags
-      s.sub! 'MYCFLAGS_VAL', "-fno-common -DLUA_USE_LINUX"
     end
 
     # Fix path in the config header
     inreplace 'src/luaconf.h', '/usr/local', HOMEBREW_PREFIX
 
-    # Fix paths in the .pc
-    inreplace 'etc/lua.pc' do |s|
-      s.gsub! "prefix= /usr/local", "prefix=#{HOMEBREW_PREFIX}"
-      s.gsub! "INSTALL_MAN= ${prefix}/man/man1", "INSTALL_MAN= ${prefix}/share/man/man1"
-    end
-
-    # this ensures that this symlinking for lua starts at lib/lua/5.1 and not
+    # this ensures that this symlinking for lua starts at lib/lua/5.2 and not
     # below that, thus making luarocks work
     (HOMEBREW_PREFIX/"lib/lua"/version.to_s.split('.')[0..1].join('.')).mkpath
 
     system "make", "macosx", "INSTALL_TOP=#{prefix}", "INSTALL_MAN=#{man1}"
     system "make", "install", "INSTALL_TOP=#{prefix}", "INSTALL_MAN=#{man1}"
-
-    (lib+"pkgconfig").install 'etc/lua.pc'
   end
 end
 
