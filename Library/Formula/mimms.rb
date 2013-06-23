55ad0743f352fe2679be4ed97c0f807de6384dcf
stefanschmidt <stefan.schmidt@gmx.net>
2013-06-24 00:28:10 +0200

    Simplified Python usage

---
+++ Library/Formula/mimms.rb
@@ -0,0 +1,57 @@
+require 'formula'
+
+class Mimms < Formula
+  homepage 'http://savannah.nongnu.org/projects/mimms/'
+  url 'https://launchpad.net/mimms/trunk/3.2/+download/mimms-3.2.tar.bz2'
+  sha1 'e8ddf9905919e7eb6602e2c9547ba537e2f69d88'
+
+  depends_on :python
+  depends_on 'libmms'
+
+  # Switch shared library loading to Mach-O naming convention (.dylib)
+  # Fix installation path for man page to $(brew --prefix)/share/man
+  def patches
+     DATA
+  end
+
+  def install
+    python do
+      system python, "setup.py", "install", "--prefix=#{prefix}"
+    end
+  end
+
+  def caveats
+    python.standard_caveats if python
+  end
+
+  test do
+    system "mimms"
+  end
+end
+__END__
+diff --git a/libmimms/libmms.py b/libmimms/libmms.py
+index fb59207..ac42ba4 100644
+--- a/libmimms/libmms.py
++++ b/libmimms/libmms.py
+@@ -23,7 +23,7 @@ exposes the mmsx interface, since this one is the most flexible.
+ 
+ from ctypes import *
+ 
+-libmms = cdll.LoadLibrary("libmms.so.0")
++libmms = cdll.LoadLibrary("libmms.0.dylib")
+ 
+ # opening and closing the stream
+ libmms.mmsx_connect.argtypes = [c_void_p, c_void_p, c_char_p, c_int]
+
+diff --git a/setup.py b/setup.py
+index cfeb678..f6f92f0 100644
+--- a/setup.py
++++ b/setup.py
+@@ -10,6 +10,6 @@ setup(
+   packages=['libmimms'],
+   scripts=['mimms'],
+   data_files=[
+-    ('share/man/man1', ['mimms.1'])
++    ('man/man1', ['mimms.1'])
+     ]
+   )
