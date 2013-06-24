dcf7234d47f4063d452c71227f60aa63f7615627
chdiza <chdiza@gmail.com>
2013-06-24 14:05:46 -0400

    msmtp: Delete unnecessary option code
    Msmtp will enable support for the OS X Keychain by default on Macs.  No need to ask for it.

--- Library/Formula/msmtp.rb
+++ Library/Formula/msmtp.rb
@@ -5,18 +5,17 @@ class Msmtp < Formula
   url 'http://downloads.sourceforge.net/project/msmtp/msmtp/1.4.31/msmtp-1.4.31.tar.bz2'
   sha1 'c0edce1e1951968853f15209c8509699ff9e9ab5'
 
-  option 'with-macosx-keyring', "Support Mac OS X Keyring"
-
   depends_on 'pkg-config' => :build
+
+  # msmtp enables OS X Keychain support by default, so no need to ask for it.
 
   def install
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
-    args << "--with-macosx-keyring" if build.include? 'with-macosx-keyring'
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
   end
 end
