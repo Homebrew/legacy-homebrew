e2b10438666f41366f596fe762b2455f5761bcd8
Gvozden Neskovic <neskovic@gmail.com>
2013-06-25 16:19:53 +0300

    isl 0.12
    Closes #20767.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/isl.rb
+++ Library/Formula/isl.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Isl < Formula
   homepage 'http://www.kotnet.org/~skimo/isl/'
-  url 'http://www.kotnet.org/~skimo/isl/isl-0.11.2.tar.bz2'
-  mirror 'ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/isl/isl-0.11.2.tar.bz2'
-  sha1 'ca2c93a58e899379d39f2956b2299c62e3975018'
+  url 'http://www.kotnet.org/~skimo/isl/isl-0.12.tar.bz2'
+  mirror 'ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/isl/isl-0.12.tar.bz2'
+  sha1 'f694b741530676cd1ea27a6437bd090c69e61455'
 
   head 'http://repo.or.cz/w/isl.git'
 
@@ -13,5 +13,8 @@ class Isl < Formula
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
+
+    # move gdb helper to proper location
+    (share+'gdb/auto-load').install Dir[lib+'*-gdb.py']
   end
 end
