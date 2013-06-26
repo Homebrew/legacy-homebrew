ba588de3c10653722b6ccf2035668b37c88ec613
Michael Klishin <michael@defprotocol.org>
2013-06-26 16:29:51 +0400

    RabbitMQ 3.1.3
    Includes an important fix for the management UI plugin.

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rabbitmq < Formula
   homepage 'http://www.rabbitmq.com'
-  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.2/rabbitmq-server-generic-unix-3.1.2.tar.gz'
-  sha1 '382187e7c40fba1c40963f12172c0f572c1655b9'
+  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.3/rabbitmq-server-generic-unix-3.1.3.tar.gz'
+  sha1 '4de0b2fed97733cd7e5ea7a29267fac5c5ca97ae'
 
   depends_on 'erlang'
   depends_on 'simplejson' => :python if MacOS.version == :leopard
