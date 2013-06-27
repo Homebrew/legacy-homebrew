2317c82be3479f7406640c9e3cf256f86a0d520b
Jack Feser <jack.feser@gmail.com>
2013-06-27 17:24:20 -0500

    openctm: add formula
    Add formula for the OpenCTM compressed triangle mesh format.  Contains a
    shared library and bindings for Python and Pascal.  Aalso ontains a
    viewer for openctm files and a converter for multiple other formats.

---
+++ Library/Formula/openctm.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Openctm < Formula
+  homepage 'http://openctm.sourceforge.net'
+  url 'http://sourceforge.net/projects/openctm/files/OpenCTM-1.0.3/OpenCTM-1.0.3-src.tar.bz2'
+  sha1 '7acfda356b3fa5393d993001ffeaa0e6aa2f65da'
+
+  depends_on 'gtk+'
+
+  def install
+    build_cmd = []
+
+    system "make", "-f", "Makefile.macosx"
+
+    system "mkdir", "-p", lib, include, bin, man1
+    system "make", "LIBDIR=#{lib}",
+                   "INCDIR=#{include}",
+                   "BINDIR=#{bin}",
+                   "MAN1DIR=#{man1}",
+                   "-f", "Makefile.macosx", "install"
+  end
+
+  test do
+    system "#{bin}/ctmconv"
+  end
+end
