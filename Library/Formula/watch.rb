c56ceea13e47b8fc02649a86e0b4bc5dffc19ebc
Coby Chapple <coby@github.com>
2013-06-27 10:08:39 +0100

    :skull: version line, it’s detected from the tarball

--- Library/Formula/watch.rb
+++ Library/Formula/watch.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Watch < Formula
   homepage 'https://github.com/whit537/watch'
   url 'https://github.com/whit537/watch/archive/0.3.0.tar.gz'
-  version '0.3.0'
   sha1 '1b355d16d729d5fe4c0ce90ec92b45946cfa4a5a'
 
   def install
