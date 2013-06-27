045c736142b5c814b5c751f448b4f486f3881276
Sander Bouwhuis <sander@enrise.com>
2013-06-27 14:39:55 +0200

    specify git tag

--- Library/Formula/hubflow.rb
+++ Library/Formula/hubflow.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Hubflow < Formula
   homepage 'http://datasift.github.io/gitflow/'
-  url 'https://github.com/datasift/gitflow.git', :revision => 'd4e8de95acf88195051015df13158d755174c322'
+  url 'https://github.com/datasift/gitflow.git', :tag => '1.5.1'
   version '1.5.1'
 
   def install
