c5e08ac577cfdcb53f73792435aa5355db5842a8
Coby Chapple <coby@github.com>
2013-06-27 11:08:13 +0100

    take a stab at using the procps-ng version, this will likely fail

--- Library/Formula/watch.rb
+++ Library/Formula/watch.rb
@@ -2,10 +2,25 @@ require 'formula'
 
 class Watch < Formula
   homepage 'https://github.com/whit537/watch'
-  url 'https://github.com/whit537/watch/archive/0.3.0.tar.gz'
-  sha1 '1b355d16d729d5fe4c0ce90ec92b45946cfa4a5a'
+  url 'http://gitorious.org/procps/procps/archive-tarball/v3.3.0'
+  version '0.3.0'
+  sha1 'e51631aeaa005d99080d0a3621ffb13f36fc1d96'
+
+  depends_on "pkg-config" => :build
+  depends_on "automake"   => :build
+  depends_on "autoconf"   => :build
+  depends_on "libtool"    => :build
 
   def install
+    system "rm -rf autom4te.cache"
+    system "glibtoolize"
+    system "aclocal", "-I", "m4"
+    system "autoconf"
+    system "autoheader"
+    system "automake", "--add-missing"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "watch"
     bin.install "watch"
     man1.install "watch.1"
   end
