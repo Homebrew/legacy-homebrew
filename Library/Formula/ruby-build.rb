4e8b56da964bc7eb68e3d5602ce682b58e658f2e
Mitsutaka Mimura <takkanm@gmail.com>
2013-06-28 13:10:40 +0900

    fix sha1

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,7 +3,7 @@ require 'formula'
 class RubyBuild < Formula
   homepage 'https://github.com/sstephenson/ruby-build'
   url 'https://github.com/sstephenson/ruby-build/archive/v20130628.tar.gz'
-  sha1 'c34c1c2ecff95320b902ab59742284bddec3fe8f'
+  sha1 'a26df829fa089807c30422a2df04348b3b4ec1b5'
 
   head 'https://github.com/sstephenson/ruby-build.git'
 
