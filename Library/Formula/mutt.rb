1400312c601e87330013df743b4ae67b8f6d82cf
Peter Aronoff <telemachus@arpinum.org>
2013-06-28 19:01:01 -0400

    mutt: Add confirm-attachment patch
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/mutt.rb
+++ Library/Formula/mutt.rb
@@ -17,6 +17,7 @@ class Mutt < Formula
   option "with-slang", "Build against slang instead of ncurses"
   option "with-ignore-thread-patch", "Apply ignore-thread patch"
   option "with-pgp-verbose-mime-patch", "Apply PGP verbose mime patch"
+  option "with-confirm-attachment-patch", "Apply confirm attachment patch"
 
   depends_on 'tokyo-cabinet'
   depends_on 'slang' => :optional
@@ -34,6 +35,7 @@ class Mutt < Formula
       ['with-ignore-thread-patch', 'https://gist.github.com/mistydemeo/5522742/raw/1439cc157ab673dc8061784829eea267cd736624/ignore-thread-1.5.21.patch'],
       ['with-pgp-verbose-mime-patch',
           'http://patch-tracker.debian.org/patch/series/dl/mutt/1.5.21-6.2/features-old/patch-1.5.4.vk.pgp_verbose_mime'],
+      ['with-confirm-attachment-patch', 'https://gist.github.com/tlvince/5741641/raw/c926ca307dc97727c2bd88a84dcb0d7ac3bb4bf5/mutt-attach.patch'],
     ]
 
     if build.with? "ignore-thread-patch" and build.with? "sidebar-patch"
