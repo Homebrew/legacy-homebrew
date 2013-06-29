8896ac462d723d23f37be273a2c78eb9741ad5ee
Jack Nagel <jacknagel@gmail.com>
2013-06-28 20:31:55 -0500

    dub: remove redundant :revision spec

--- Library/Formula/dub.rb
+++ Library/Formula/dub.rb
@@ -5,7 +5,7 @@ class Dub < Formula
   url  'https://github.com/rejectedsoftware/dub/archive/v0.9.15.tar.gz'
   sha1 '408216dae221ca19220f91f9081b2f8e0d8a5484'
 
-  head 'https://github.com/rejectedsoftware/dub.git', :revision => 'HEAD'
+  head 'https://github.com/rejectedsoftware/dub.git'
 
   depends_on 'pkg-config' => :build
   depends_on 'dmd'  => :build
