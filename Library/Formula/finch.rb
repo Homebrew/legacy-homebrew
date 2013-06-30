668434759694999fa42e84385565afc79a931934
Adam Vandenberg <flangy@gmail.com>
2013-06-30 15:07:33 -0700

    finch no longer needs libgcrypt to configure

--- Library/Formula/finch.rb
+++ Library/Formula/finch.rb
@@ -7,30 +7,21 @@ class Finch < Formula
 
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
-  depends_on 'libidn'
   depends_on 'gettext'
   depends_on 'glib'
   depends_on 'gnutls'
-  # guntls used to use libgcrypt, and the configure script links this
-  # library when testing for gnutls, so include it as a build-time
-  # dependency. See:
-  # https://github.com/mxcl/homebrew/issues/17129
-  depends_on 'libgcrypt' => :build
+  depends_on 'libidn'
 
   def install
-    # To get it to compile, had to configure without support for:
-    #   * Sametime (meanwhile)
-    #   * Bonjour (avahi)
-    #   * Communicating with other programs (d-bus)
-    #   * Perl scripting
+    # Builds the UI
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--disable-gtkui",
-                          "--disable-gstreamer",
-                          "--disable-vv",
-                          "--disable-meanwhile",
                           "--disable-avahi",
                           "--disable-dbus",
+                          "--disable-gstreamer",
+                          "--disable-gtkui",
+                          "--disable-meanwhile",
+                          "--disable-vv",
                           "--disable-perl"
     system "make install"
   end
