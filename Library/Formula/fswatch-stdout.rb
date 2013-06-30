cadb1e8505de55f6ab878a03f6112ed4b38f7c76
Marshall Levin <mlevin@meadhall.com>
2013-06-30 02:21:22 -0400

    fswatch-stdout - removed test
    you can't test it that way :-)

--- Library/Formula/fswatch-stdout.rb
+++ Library/Formula/fswatch-stdout.rb
@@ -1,6 +1,7 @@
 require 'formula'
 
 class FswatchStdout < Formula
+
   homepage 'https://github.com/unphased/fswatch'
   url 'https://github.com/unphased/fswatch', :revision => 'c6bdb93e1072f7184ccc10da63100b0e0c1cd800'
   version '2013-06-22'
@@ -13,7 +14,4 @@ class FswatchStdout < Formula
     bin.install "fswatch-stdout"
   end
 
-  def test
-    system "#{bin}/fswatch-stdout"
-  end
 end
