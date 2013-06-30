d834bfeaf73ca95287422c5a3f0a71fc88231e1f
Marshall Levin <mlevin@navigationarts.com>
2013-06-29 23:43:57 -0400

    fswatch - new formula
    Added fswatch, a handy osx analog of ionotify to watch for filesystem changes and then execute a script.

---
+++ Library/Formula/fswatch.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Fswatch < Formula
+  homepage 'https://github.com/alandipert/fswatch'
+  url 'https://github.com/alandipert/fswatch.git', :revision => 'a6ff7fde6f1775a40a8517aa48958d9ff23a120f'
+  version '2012-11-16'
+
+  head 'https://github.com/alandipert/fswatch.git', :branch => 'master'
+
+  def install
+    system "make all"
+    bin.install "fswatch"
+  end
+
+  def test
+    system "#{bin}/fswatch"
+  end
+end
