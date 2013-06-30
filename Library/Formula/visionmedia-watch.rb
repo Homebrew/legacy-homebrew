bb7b850d5775cb47195205bb3dfa6fa912df7601
Adam Vandenberg <flangy@gmail.com>
2013-06-30 08:49:04 -0700

    watch and visionmedia-watch conflict

--- Library/Formula/visionmedia-watch.rb
+++ Library/Formula/visionmedia-watch.rb
@@ -3,8 +3,11 @@ require 'formula'
 class VisionmediaWatch < Formula
   homepage 'https://github.com/visionmedia/watch'
   url 'https://github.com/visionmedia/watch/archive/0.2.1.tar.gz'
-  head 'https://github.com/visionmedia/watch.git'
   sha1 '752006185eadb34a0ec3bdbea259c27359d886d0'
+
+  head 'https://github.com/visionmedia/watch.git'
+
+  conflicts_with 'watch'
 
   def install
     bin.mkdir
--- Library/Formula/watch.rb
+++ Library/Formula/watch.rb
@@ -3,8 +3,10 @@ require 'formula'
 class Watch < Formula
   homepage 'http://procps.sourceforge.net/'
   url 'http://procps.sourceforge.net/procps-3.2.8.tar.gz'
+  version '0.2.0' # watch command itself is version 0.2.0
   sha1 'a0c86790569dec26b5d9037e8868ca907acc9829'
-  version '0.2.0'
+
+  conflicts_with 'visionmedia-watch'
 
   def install
     system "make", "watch", "PKG_LDFLAGS=-Wl"
