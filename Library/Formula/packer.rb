e4e1ad76e296d348bfb08adb4fc0cc458d6f8e47
Joel Moss <joel@developwithstyle.com>
2013-06-30 10:38:57 +0100

    Updated to 0.1.2

--- Library/Formula/packer.rb
+++ Library/Formula/packer.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Packer < Formula
   homepage 'http://www.packer.io/'
-  version '0.1.1'
-  url 'https://github.com/mitchellh/packer/archive/v0.1.1.tar.gz'
-  sha1 '6f6b061498df5e43a982be766e59588b1fe0c915'
+  version '0.1.2'
+  url 'https://github.com/mitchellh/packer/archive/v0.1.2.tar.gz'
+  sha1 '8198f2a1e9971b2134092a31c0b4812d78dcf0af'
   head 'https://github.com/mitchellh/packer.git'
 
   depends_on 'go' => :build
