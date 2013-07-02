bd95630817daaa56ed12acd8d61a310ad92151ae
Paul Morton <pmorton@biaprotect.com>
2013-07-02 11:16:01 -0700

    Add Source Build

--- Library/Formula/packer.rb
+++ Library/Formula/packer.rb
@@ -5,8 +5,21 @@ class Packer < Formula
   homepage 'http://www.packer.io/'
   url 'https://dl.bintray.com/mitchellh/packer/0.1.3_darwin_amd64.zip?direct'
   sha1 '2177c221cc439e8df3ddeada1feb1287727aa1a7'
+  head 'https://github.com/mitchellh/packer.git'
+  
+  if build.head?
+    depends_on 'go'
+    depends_on 'mercurial'
+  end
 
   def install
-    bin.install Dir['*']
+    if build.head?
+      ENV['GOPATH'] = File.expand_path('./go')
+      system 'make'
+      bin.install Dir['bin/*']
+    else
+      bin.install Dir['*']
+    end
   end
+
 end
