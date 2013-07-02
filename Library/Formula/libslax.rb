8651d51004544588ea6c2d9650c3dae12bc1ce99
Brandon Bennett <bennetb@gmail.com>
2013-07-02 09:48:47 -0600

    libslax 0.16.6
    Closes #20964.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libslax.rb
+++ Library/Formula/libslax.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libslax < Formula
   homepage 'http://www.libslax.org/'
-  url 'https://github.com/Juniper/libslax/archive/0.16.2.tar.gz'
-  sha1 'a2270ec7e67c540889894b732e7d4387592c7500'
+  url 'https://github.com/Juniper/libslax/archive/0.16.6.tar.gz'
+  sha1 'e902566ddb4b2285fa2143712cf0260feb73e2fe'
 
   head 'https://github.com/Juniper/libslax.git'
 
@@ -11,11 +11,9 @@ class Libslax < Formula
   depends_on 'libtool'  => :build
 
   # Need newer versions of these libraries
-  if MacOS.version <= :lion
-    depends_on 'libxml2'
-    depends_on 'libxslt'
-    depends_on 'curl'
-  end
+  depends_on 'libxml2'
+  depends_on 'libxslt'
+  depends_on 'curl' if MacOS.version <= :lion
 
   def install
     system "sh ./bin/setup.sh"
