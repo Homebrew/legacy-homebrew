7e403bafebafab79ffeedb745eda7770306792f8
Jeff Hodges <jeff@somethingsimilar.com>
2013-07-03 02:24:56 -0700

    correct sha1

--- Library/Formula/jsonpp.rb
+++ Library/Formula/jsonpp.rb
@@ -4,7 +4,7 @@ class Jsonpp < Formula
   homepage 'http://jmhodges.github.io/jsonpp/'
   url 'https://github.com/jmhodges/jsonpp/releases/v1.2.0/715/jsonpp-1.2.0-osx-x86_64.zip'
   version '1.2.0'
-  sha1 '7aec425f3604d8c8e20f597d13df5bc5d9044a5f'
+  sha1 '422d5b2cefa92923d2fbef9afe1324d72134509e'
 
   def install
     bin.install 'jsonpp'
