9f54288198ef814d07b3054a382e6600e7b3613b
Pete Gadomski <pete.gadomski@gmail.com>
2013-07-06 11:16:22 -0600

    Add head option to PDAL formula
    PDAL is hosted on github and sometimes folks might want to build it from
    the latest development version.

--- Library/Formula/pdal.rb
+++ Library/Formula/pdal.rb
@@ -4,6 +4,7 @@ class Pdal < Formula
   homepage 'http://pointcloud.org'
   url 'https://github.com/PDAL/PDAL/archive/0.1.0.tar.gz'
   sha1 '74f3f5d8b58c52b7eb2afe260a0020b7c4a6906e'
+  head 'https://github.com/PDAL/PDAL.git'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
