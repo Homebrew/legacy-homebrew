b41f9b745bdb05a4521d662bb44a01ca0e58f2f3
Xiyue Deng <manphiz@gmail.com>
2013-07-06 14:56:15 -0700

    Winetricks: Update to 20130629 release, r1026
    Closes #21045.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/winetricks.rb
+++ Library/Formula/winetricks.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Winetricks < ScriptFileFormula
   homepage 'http://code.google.com/p/winetricks/'
-  url 'http://winetricks.googlecode.com/svn-history/r913/trunk/src/winetricks', :using => :curl
+  url 'http://winetricks.googlecode.com/svn-history/r1026/trunk/src/winetricks', :using => :curl
   # since the version stated in the field is seldom updated, we append the revision number
-  version '20120912-r913'
-  sha256 'b7b9a53ca507cec75efbd263c5dad1aaa80f8751469a62d9e68ed44e58050d3c'
+  version '20130629-r1026'
+  sha256 'd0906097e6bd9b3f52715abb2e8d6ae70d20184d46ba004fa9f0bd537e786c1d'
 
   head 'http://winetricks.googlecode.com/svn/trunk/src/winetricks', :using => :curl
 
