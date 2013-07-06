70a821775fbf855e3e23f1df94c22a8cef6631bd
Jason Webb <bigjasonwebb@gmail.com>
2013-07-05 22:27:45 -0600

    play 2.1.2
    Closes #21033.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -2,15 +2,10 @@ require 'formula'
 
 class Play < Formula
   homepage 'http://www.playframework.org/'
-  url 'http://downloads.typesafe.com/play/2.1.1/play-2.1.1.zip'
-  sha1 '3b1a0c3ca23dde36113d935b85d7f4d45f05923b'
+  url 'http://downloads.typesafe.com/play/2.1.2/play-2.1.2.zip'
+  sha1 'b6d8716a54219b40ffc678656c4763e360258bf7'
 
   head 'https://github.com/playframework/Play20.git'
-
-  devel do
-    url 'http://downloads.typesafe.com/play/2.1.2-RC1/play-2.1.2-RC1.zip'
-    sha1 '61e1ee829ea5bec826dc4237ebb6dcab3e47feb7'
-  end
 
   def install
     rm Dir['*.bat'] # remove windows' bat files
