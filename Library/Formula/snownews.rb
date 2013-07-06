777ae9032cb0347f28e5a7a6bbe9dc7474869507
Benjamin Weber <one@bwe.im>
2013-07-06 18:39:08 +0200

    snownews: x11 dependency removed, indentation fixed, inserted patch comment

--- Library/Formula/snownews.rb
+++ Library/Formula/snownews.rb
@@ -5,11 +5,10 @@ class Snownews < Formula
   url 'https://kiza.eu/media/software/snownews/snownews-1.5.12.tar.gz'
   sha1 'b3addaac25c2c093aa5e60b8b89e50e7d7450bcf'
 
-  depends_on :x11 # if your formula requires any X11/XQuartz components
   depends_on 'gettext' => :build
 
   def patches
-        "http://gist.github.com/benjaminweb/5936137/raw"
+    "http://gist.github.com/benjaminweb/5936137/raw" #fixes zlib linking
   end
 
   def install
