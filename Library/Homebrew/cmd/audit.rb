3e1eab105a97259d8c12c39407decdb415564c38
Adam Vandenberg <flangy@gmail.com>
2013-07-07 11:56:11 -0700

    audit: skip an audit for mongodb

--- Library/Homebrew/cmd/audit.rb
+++ Library/Homebrew/cmd/audit.rb
@@ -463,8 +463,10 @@ class FormulaAuditor
       problem "No double 'without': Use `build.without? '#{$1}'` to check for \"--without-#{$1}\""
     end
 
-    if line =~ /ARGV\.(?!(debug\?|verbose\?|value[\(\s]))/
-      problem "Use build instead of ARGV to check options"
+    unless f.name == 'mongodb' # Mongo writes out a Ruby script that uses ARGV
+      if line =~ /ARGV\.(?!(debug\?|verbose\?|value[\(\s]))/
+        problem "Use build instead of ARGV to check options"
+      end
     end
 
     if line =~ /def options/
