55015606215d08b71dc9c7cea707e644c98e6620
Brandon Bennett <bennetb@gmail.com>
2013-07-07 10:50:45 -0600

    juise 0.5.1

--- Library/Formula/juise.rb
+++ Library/Formula/juise.rb
@@ -1,26 +1,25 @@
 require 'formula'
 
 class Juise < Formula
-  homepage 'http://code.google.com/p/juise/'
-  url 'http://juise.googlecode.com/files/juise-0.3.21.tar.gz'
-  sha1 '1d58b182ce60edc275f6574bc72c128a799438e0'
+  homepage 'https://github.com/Juniper/juise/wiki'
+  url 'https://github.com/Juniper/juise/archive/0.5.1.tar.gz'
+  sha1 '7996f67b00d5b787d03d27258c6560f4ef3b1cf6'
 
+  head 'https://github.com/Juniper/juise.git'
+
+  depends_on 'automake' => :build
   depends_on 'libtool'  => :build
   depends_on 'libslax'
   depends_on 'libssh2'
   depends_on 'pcre'
-
-  # Need newer versions of these libraries
-  if MacOS.version <= :lion
-    depends_on 'libxml2'
-    depends_on 'libxslt'
-    depends_on 'curl'
-  end
+  depends_on 'sqlite'
 
   def install
+    system "sh ./bin/setup.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--with-libssh2-prefix=#{HOMEBREW_PREFIX}"
+                          "--with-libssh2-prefix=#{HOMEBREW_PREFIX}",
+                          "--with-sqlite3-prefix=#{Formula.factory('sqlite').opt_prefix}"
     system "make install"
   end
 end
