c5542ddeb0cb3a2c3a922c4ffe5bfae7327a00df
D1plo1d <d1plo1d@d1plo1d.com>
2013-07-08 00:22:26 -0400

    Updating the tegh installer per adamv's suggestions.

--- Library/Formula/tegh.rb
+++ Library/Formula/tegh.rb
@@ -1,16 +1,18 @@
 require 'formula'
 
 class Tegh < Formula
-  url 'https://github.com/D1plo1d/tegh.git', :tag => '0.2.0'
-  version '0.2.0'
   homepage 'https://github.com/D1plo1d/tegh'
+  url 'https://github.com/D1plo1d/tegh.git', :tag => '0.2.0'
   head 'https://github.com/D1plo1d/tegh.git', :branch => 'develop'
 
   depends_on 'node'
 
   def install
     system "npm", "install"
-    prefix.install Dir['*']
+    prefix.install 'package.json'
+    prefix.install Dir['src']
+    prefix.install Dir['node_modules']
+    bin.install Dir['bin/tegh']
   end
 
   test do
