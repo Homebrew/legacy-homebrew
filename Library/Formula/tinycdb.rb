254197a082962ee201773ebf4fd9b1d9b95a15e7
Louis-Philippe Gauthier <lpgauth@gmail.com>
2013-07-09 23:54:24 -0400

    Remove version + Add mandir

--- Library/Formula/tinycdb.rb
+++ Library/Formula/tinycdb.rb
@@ -3,11 +3,10 @@ require 'formula'
 class Tinycdb < Formula
   homepage 'http://www.corpit.ru/mjt/tinycdb.html'
   url 'http://www.corpit.ru/mjt/tinycdb/tinycdb-0.78.tar.gz'
-  version '0.78'
   sha1 'ade42ee1e7c56f66a63cb933206c089b9983adba'
 
   def install
     system "make"
-    system "make install prefix=#{prefix}"
+    system "make install prefix=#{prefix} mandir=#{man}"
   end
 end
