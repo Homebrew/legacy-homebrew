bc3e4ef033482f1cf5c05741bfc21c1ff14fb44c
David James <david@bluemontlabs.com>
2013-07-11 16:58:38 -0400

    riak 1.4.0
    32 bit support dropped. See:
    https://github.com/basho/riak/issues/352

--- Library/Formula/riak.rb
+++ Library/Formula/riak.rb
@@ -1,21 +1,15 @@
 require 'formula'
 
 class Riak < Formula
-  homepage 'http://wiki.basho.com/Riak.html'
+  homepage 'http://docs.basho.com/riak/latest/tutorials/installation/Installing-on-Mac-OS-X/'
 
-  if Hardware.is_64_bit? and not build.build_32_bit?
-    url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.3/1.3.1/osx/10.6/riak-1.3.1-osx-x86_64.tar.gz'
-    version '1.3.1-x86_64'
-    sha256 'e7b4db2273ef3fc0ba76f37468ff040d94a9bdac90ed129e226e483b19451414'
-  else
-    url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.3/1.3.1/osx/10.6/riak-1.3.1-osx-i386.tar.gz'
-    version '1.3.1-i386'
-    sha256 '1169ddcbc1a613c734f3df7350b416e84c667faaae73b647d2fa696aa45bc085'
+  if Hardware.is_64_bit?
+    url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.0/osx/10.8/riak-1.4.0-OSX-x86_64.tar.gz'
+    version '1.4.0-x86_64'
+    sha256 '4830b9e00b520b8494264017bfc2ff10753fd51b9e013bafd3054e29e5be1443'
   end
 
   skip_clean 'libexec'
-
-  option '32-bit'
 
   def install
     libexec.install Dir['*']
