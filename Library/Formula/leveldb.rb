970c671e4b4e90542ec02de2ef3e30b2fa4bff7c
Brett Koonce <koonce@gmail.com>
2013-07-12 17:04:49 -0500

    leveldb 1.12.0
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/leveldb.rb
+++ Library/Formula/leveldb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Leveldb < Formula
   homepage 'https://code.google.com/p/leveldb/'
-  url 'https://leveldb.googlecode.com/files/leveldb-1.11.0.tar.gz'
-  sha1 '401914f2e6465ed008d92a11f4013a28fdf5f240'
+  url 'https://leveldb.googlecode.com/files/leveldb-1.12.0.tar.gz'
+  sha1 'e7b84f239949920bfc2d7d1074ec6a6fa8869e23'
 
   depends_on 'snappy' => :build
 
@@ -13,8 +13,8 @@ class Leveldb < Formula
     include.install "include/leveldb"
     bin.install 'leveldbutil'
     lib.install 'libleveldb.a'
-    lib.install 'libleveldb.dylib.1.11' => 'libleveldb.1.11.dylib'
-    lib.install_symlink lib/'libleveldb.1.11.dylib' => 'libleveldb.dylib'
-    lib.install_symlink lib/'libleveldb.1.11.dylib' => 'libleveldb.1.dylib'
+    lib.install 'libleveldb.dylib.1.12' => 'libleveldb.1.12.dylib'
+    lib.install_symlink lib/'libleveldb.1.12.dylib' => 'libleveldb.dylib'
+    lib.install_symlink lib/'libleveldb.1.12.dylib' => 'libleveldb.1.dylib'
   end
 end
