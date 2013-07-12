e17f79fb769a5adbb48251517e3cb623d55b2d87
Denis <denieldfu@gmail.com>
2013-07-12 15:58:10 +0300

    Midnight Commander 4.8.9
    Closes #21167.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class MidnightCommander < Formula
   homepage 'http://www.midnight-commander.org/'
-  url 'http://www.midnight-commander.org/downloads/mc-4.8.8.tar.bz2',
+  url 'http://www.midnight-commander.org/downloads/mc-4.8.9.tar.bz2',
       :using => CurlUnsafeDownloadStrategy
-  mirror 'http://fossies.org/linux/misc/mc-4.8.8.tar.bz2'
-  sha256 '7b5e6f90e6709d1c1bcb4a2bf6d2a62b9494adef3ff4325ffdb3551c29e42a1c'
+  mirror 'http://fossies.org/linux/misc/mc-4.8.9.tar.bz2'
+  sha256 '7e184cf5ed3ff8b93fcbfff16608c1444920f523beda2fcc586878d619ea11da'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
