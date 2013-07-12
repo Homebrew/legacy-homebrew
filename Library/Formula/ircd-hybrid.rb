59ef60396a2a96e260757217d2f911f00e56a097
Brett Koonce <koonce@gmail.com>
2013-07-12 18:42:55 -0500

    ircd-hybrid 8.1.4
    Closes #21181.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ircd-hybrid.rb
+++ Library/Formula/ircd-hybrid.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class IrcdHybrid < Formula
   homepage 'http://www.ircd-hybrid.org/'
-  url 'http://downloads.sourceforge.net/project/ircd-hybrid/ircd-hybrid/ircd-hybrid-8.1.1/ircd-hybrid-8.1.1.tgz'
-  sha1 'efdafab2ec56e9e1854df5bd0e01bf5f5afca00e'
+  url 'http://downloads.sourceforge.net/project/ircd-hybrid/ircd-hybrid/ircd-hybrid-8.1.4/ircd-hybrid-8.1.4.tgz'
+  sha1 '33945a0cdb6c208c5c7413cf696890dfd1b3b147'
 
   # ircd-hybrid needs the .la files
   skip_clean :la
