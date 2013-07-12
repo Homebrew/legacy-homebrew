c66a55af805b52bb7a183aa7fa626ee7732f573d
Ryan Jennings <c0der78@gmail.com>
2013-07-11 17:02:30 -0700

    don't like the idea of a test binary lying around - changing to run tests on build

--- Library/Formula/igloo.rb
+++ Library/Formula/igloo.rb
@@ -4,16 +4,15 @@ class Igloo < Formula
   homepage 'http://igloo-testing.org'
   url 'https://github.com/joakimkarlsson/igloo/archive/igloo.1.0.0.tar.gz'
   sha1 'f6b538862e4b83e1a389b27fd79f1b89ff1f3c76'
-  depends_on 'cmake' => :build
+  option 'skip-tests', 'Skip performing a self test on build'
+  depends_on 'cmake' => :build unless build.include? 'skip-tests'
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make"
+    unless build.include? 'skip-tests'
+      system "cmake", ".", *std_cmake_args
+      system "make"
+      system "bin/igloo-tests"
+    end
     include.install "igloo"
-    bin.install "bin/igloo-tests" => "igloo-tests"
-  end
-
-  test do
-    system "igloo-tests"
   end
 end
