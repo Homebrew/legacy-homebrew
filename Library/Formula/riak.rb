49577dad3307364751cdcf9783e45f4ad45d1cf3
Michael Erb <erbmicha@gmail.com>
2013-07-13 00:48:16 -0700

    riak 1.4.0

--- Library/Formula/riak.rb
+++ Library/Formula/riak.rb
@@ -4,28 +4,29 @@ class Riak < Formula
   homepage 'http://wiki.basho.com/Riak.html'
 
   if Hardware.is_64_bit? and not build.build_32_bit?
-    url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.3/1.3.1/osx/10.6/riak-1.3.1-osx-x86_64.tar.gz'
-    version '1.3.1-x86_64'
-    sha256 'e7b4db2273ef3fc0ba76f37468ff040d94a9bdac90ed129e226e483b19451414'
+    url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.0/osx/10.8/riak-1.4.0-OSX-x86_64.tar.gz'
+    version '1.4.0-x86_64'
+    sha256 '4830b9e00b520b8494264017bfc2ff10753fd51b9e013bafd3054e29e5be1443'
   else
-    url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.3/1.3.1/osx/10.6/riak-1.3.1-osx-i386.tar.gz'
-    version '1.3.1-i386'
-    sha256 '1169ddcbc1a613c734f3df7350b416e84c667faaae73b647d2fa696aa45bc085'
+    ohai "There is no pre-compiled 32-bit version anymore. Go to http://docs.basho.com/riak/latest/tutorials/installation/Installing-on-Mac-OS-X/#From-Source and compile from source."
   end
 
-  skip_clean 'libexec'
+  depends_on :arch => :x86_64
 
-  option '32-bit'
+  # patch scripts to work with homebrew symlinking
+  def patches
+    [
+      "https://gist.github.com/erbmicha/5970803/raw", # env.sh patch
+      "https://gist.github.com/erbmicha/5970790/raw", # riak patch
+      "https://gist.github.com/erbmicha/5972538/raw", # search-cmd patch
+      "https://gist.github.com/erbmicha/5972526/raw", # riak-admin patch
+      "https://gist.github.com/erbmicha/5972531/raw", # riak-debug patch
+    ]
+  end
 
   def install
-    libexec.install Dir['*']
-
-    # The scripts don't dereference symlinks correctly.
-    # Help them find stuff in libexec. - @adamv
-    inreplace Dir["#{libexec}/bin/*"] do |s|
-      s.change_make_var! "RUNNER_SCRIPT_DIR", "#{libexec}/bin"
-    end
-
+    prefix.install Dir['*']
+    rm Dir.glob("#{bin}/*.orig") # get rid of the patch backups before symlinking to bin
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 end
