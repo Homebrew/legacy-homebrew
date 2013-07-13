15c98d1d7505056ce274b317d13eecdf24c29490
Brett Koonce <koonce@gmail.com>
2013-07-13 07:47:04 -0500

    libusbx 1.0.16

--- Library/Formula/libusbx.rb
+++ Library/Formula/libusbx.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libusbx < Formula
   homepage 'http://libusbx.org'
-  url 'http://downloads.sourceforge.net/project/libusbx/releases/1.0.15/source/libusbx-1.0.15.tar.bz2'
-  sha1 '1ca868f775093b0109d9240cb3ccd36367764dc6'
+  url 'http://downloads.sourceforge.net/project/libusbx/releases/1.0.16/source/libusbx-1.0.16.tar.bz2'
+  sha1 'ec164f02e6732c373e5a24be6b33a59142435615'
 
   head 'https://github.com/libusbx/libusbx.git'
 
