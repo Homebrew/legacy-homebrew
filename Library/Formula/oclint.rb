2dbc7124779ec4eb2376347c1c3e791035b7b2ef
Ryuichi Saito <ryuichi@ryuichisaito.com>
2013-07-12 20:32:52 -0500

    Build from source code instead

--- Library/Formula/oclint.rb
+++ Library/Formula/oclint.rb
@@ -1,25 +1,105 @@
 require 'formula'
 
+class Clang < Formula
+  homepage 'http://llvm.org/'
+  url 'http://llvm.org/releases/3.3/cfe-3.3.src.tar.gz'
+  sha1 'ccd6dbf2cdb1189a028b70bcb8a22509c25c74c8'
+  head 'http://llvm.org/git/clang.git'
+end
+
+class CompilerRT < Formula
+  homepage 'http://llvm.org/'
+  url 'http://llvm.org/releases/3.3/compiler-rt-3.3.src.tar.gz'
+  sha1 '745386ec046e3e49742e1ecb6912c560ccd0a002'
+  head 'http://llvm.org/git/compiler-rt.git'
+end
+
+class Llvm < Formula
+  homepage 'http://llvm.org/'
+  url 'http://llvm.org/releases/3.3/llvm-3.3.src.tar.gz'
+  sha1 'c6c22d5593419e3cb47cbcf16d967640e5cce133'
+  head 'http://llvm.org/git/llvm.git'
+end
+
+class OCLintXcodebuild < Formula
+  head 'https://github.com/oclint/oclint-xcodebuild.git'
+end
+
+class OCLintJSONCompilationDatabase < Formula
+  head 'https://github.com/oclint/oclint-json-compilation-database.git'
+end
+
 class Oclint < Formula
   homepage 'http://oclint.org'
-  url 'http://archives.oclint.org/releases/0.7/oclint-0.7-x86_64-apple-darwin-10.tar.gz'
+  url 'http://archives.oclint.org/releases/0.7/oclint-0.7-src.tar.gz'
   version '0.7'
-  sha1 '867751f9e1b73515c22a014b22592b31c92f81bb'
+  sha1 '6621d62764a27a4e9c12e0877bb3a927005aaaae'
+  head 'https://github.com/oclint/oclint.git'
 
-  devel do
-    url 'http://archives.oclint.org/nightly/oclint-0.8.dev.d78537d-x64_64-apple-darwin-12.tar.gz'
-    version '0.8.dev.d78537d'
-    sha1 '03ab470d0638087c42994ea12161178d1f9db47f'
-  end
+  depends_on 'cmake' => :build
+
+  option 'with-debug', 'Build in debug mode'
 
   def install
-    lib.install Dir['lib/clang']
-    lib.install Dir['lib/oclint']
-    bin.install Dir['bin/*']
+    Llvm.new("llvm").brew do
+      llvm_dir.install Dir['*']
+    end
+
+    Clang.new("clang").brew do
+      clang_dir.install Dir['*']
+    end
+
+    CompilerRT.new("compiler-rt").brew do
+      compiler_rt_dir.install Dir['*']
+    end
+
+    if build.head?
+      OCLintJSONCompilationDatabase.new("oclint-json-compilation-database").brew do
+        (buildpath/'oclint-json-compilation-database').install Dir['*']
+      end
+
+      OCLintXcodebuild.new("oclint-xcodebuild").brew do
+        (buildpath/'oclint-xcodebuild').install Dir['*']
+      end
+    end
+
+    args = []
+    args << "release" unless build.with? 'debug'
+
+    cd oclint_script_dir do
+      system './buildClang.sh', *args
+      system './buildAll.sh', *args
+    end
+
+    cd oclint_release_dir do
+      lib.install Dir['lib/clang']
+      lib.install Dir['lib/oclint']
+      bin.install Dir['bin/*']
+    end
   end
 
   def test
     system "echo \"int main() { return 0; }\" > #{prefix}/test.m"
     system "#{bin}/oclint #{prefix}/test.m -- -c"
+  end
+
+  def oclint_script_dir
+    buildpath/'oclint-scripts'
+  end
+
+  def oclint_release_dir
+    buildpath/'build/oclint-release'
+  end
+
+  def llvm_dir
+    buildpath/'llvm'
+  end
+
+  def clang_dir
+    llvm_dir/'tools/clang'
+  end
+
+  def compiler_rt_dir
+    llvm_dir/'projects/compiler-rt'
   end
 end
