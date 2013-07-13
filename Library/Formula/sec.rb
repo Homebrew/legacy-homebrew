f18733d48b2e4ebd607a716f09dfc750d701b372
Brett Koonce <koonce@gmail.com>
2013-07-13 07:50:13 -0500

    sec 2.7.4
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/sec.rb
+++ Library/Formula/sec.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sec < Formula
   homepage 'http://simple-evcorr.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/simple-evcorr/sec/2.7.3/sec-2.7.3.tar.gz'
-  sha1 '926747fb34e45597d24cf6b7a44b9d23be01f7d0'
+  url 'http://downloads.sourceforge.net/project/simple-evcorr/sec/2.7.4/sec-2.7.4.tar.gz'
+  sha1 'a19d100d0340116bb227e835cb822f8c76d6f243'
 
   def install
     bin.install 'sec'
