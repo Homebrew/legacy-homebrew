0d7a3aa6ae8d89f48f5041636f474e7db869df82
Brett Koonce <koonce@gmail.com>
2013-07-13 07:47:25 -0500

    liquibase 3.0.2

--- Library/Formula/liquibase.rb
+++ Library/Formula/liquibase.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Liquibase < Formula
   homepage 'http://liquibase.org'
-  url 'http://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.0.1-bin.tar.gz'
-  sha1 'a84ee01dc3987d8a710b56baca8e6ebe2e837c75'
+  url 'http://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.0.2-bin.tar.gz'
+  sha1 'c48193ea228b07b0c42109692c8ddb4981863044'
 
   def install
     rm_f Dir['*.bat']
@@ -20,5 +20,9 @@ class Liquibase < Formula
       You should set the environment variable LIQUIBASE_HOME to
         #{libexec}
     EOS
+  end
+
+  test do
+    system "#{bin}/liquibase", "--version"
   end
 end
