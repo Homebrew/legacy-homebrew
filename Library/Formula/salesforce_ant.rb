886b6046438fe7b7cbecbf3d7e150d3c66c261e4
Ryan Brainard <ryan.brainard@gmail.com>
2013-07-13 22:10:06 -0700

    Change salesforce_ant formula to not depend on ant formula

--- Library/Formula/salesforce_ant.rb
+++ Library/Formula/salesforce_ant.rb
@@ -5,14 +5,18 @@ class SalesforceAnt < Formula
   url 'https://login.salesforce.com/dwnld/SfdcAnt/salesforce_ant_28.0.zip'
   sha1 '99c4e2aaec3d266dbbf15a7714a3a733604319ab'
 
-  depends_on 'ant'
-
   def install
     libexec.install 'ant-salesforce.jar'
-    system "ln -sf /usr/local/opt/salesforce_ant/libexec/ant-salesforce.jar /usr/local/opt/ant/libexec/lib/ant-salesforce.jar"
   end
-
+ 
+  def caveats; <<-EOS.undent
+    To complete the installation, symlink ant-salesforce.jar into your ANT library directory:
+      mkdir -p $HOME/.ant/lib
+      ln -s #{HOMEBREW_PREFIX}/opt/#{name}/libexec/ant-salesforce.jar $HOME/.ant/lib/ant-salesforce.jar
+    EOS
+  end
+ 
   test do
-    system "ant -diagnostics | grep -E 'ant-salesforce.jar \\([1-9]\\d+ bytes\\)'"
+    system "jar tf #{HOMEBREW_PREFIX}/opt/#{name}/libexec/ant-salesforce.jar | grep 'com/salesforce/antlib.xml'"
   end
 end
