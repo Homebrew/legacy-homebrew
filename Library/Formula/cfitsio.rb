5c13647af81ad7e301ee829a64047be1541f58db
Brett Koonce <koonce@gmail.com>
2013-07-16 16:27:09 -0500

    cfitsio 3.350
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/cfitsio.rb
+++ Library/Formula/cfitsio.rb
@@ -8,9 +8,9 @@ end
 
 class Cfitsio < Formula
   homepage 'http://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html'
-  url 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3340.tar.gz'
-  sha1 '9b6f61bbc0528662134d05f9d84dfe88db9df9b2'
-  version '3.340'
+  url 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3350.tar.gz'
+  sha1 '4870380013d089e1e9b8994d74f15482decffc1c'
+  version '3.350'
 
   option 'with-examples', "Compile and install example programs"
 
