84b75fe1d8240420186f3ed39037c7921849ec1b
Alexandre Laborie <alaborie@gmail.com>
2013-07-16 14:44:24 +1200

    Fixed expected sha1.

--- Library/Formula/glfw.rb
+++ Library/Formula/glfw.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Glfw < Formula
   homepage 'http://www.glfw.org/'
   url 'https://github.com/glfw/glfw/archive/3.0.1.tar.gz'
-  sha1 'ad73150e884687b7b413ec3abf5b0cc2c5c0df51'
+  sha1 '6bdad407a050b792c85ccf127a1a25ee576a6e43'
 
   option :universal
 
