2ea155d027dfe85e89ade535201b95d0d414bf2d
Brett Koonce <koonce@gmail.com>
2013-07-15 22:39:46 -0500

    cpanminus 1.6923

--- Library/Formula/cpanminus.rb
+++ Library/Formula/cpanminus.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Cpanminus < Formula
   homepage 'https://github.com/miyagawa/cpanminus'
-  url 'https://github.com/miyagawa/cpanminus/archive/1.6919.tar.gz'
-  sha1 '2a63b32cef4173aea0bebe78b44c390d66247a95'
+  url 'https://github.com/miyagawa/cpanminus/archive/1.6923.tar.gz'
+  sha1 '8be50b449a966656ccc973270a24572ebc5c3706'
 
   head 'https://github.com/miyagawa/cpanminus.git'
 
