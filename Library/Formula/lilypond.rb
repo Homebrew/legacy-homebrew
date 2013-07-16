8297e5f7ad4b396b19c02f0a4ace1de0a0ca3e02
yrammos <yannis.rammos@me.com>
2013-07-17 00:38:09 +0300

    lilypond devel 2.17.22
    Closes #21261.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/lilypond.rb
+++ Library/Formula/lilypond.rb
@@ -14,8 +14,8 @@ class Lilypond < Formula
   sha1 '1eb3b0e5c117a8669dba19ab28f933351e51e39a'
 
   devel do
-    url 'http://download.linuxaudio.org/lilypond/source/v2.17/lilypond-2.17.21.tar.gz'
-    sha1 '4c208ae1569edb59a598219277eaf6a4a1258205'
+    url 'http://download.linuxaudio.org/lilypond/source/v2.17/lilypond-2.17.22.tar.gz'
+    sha1 'c5433cf6ce202685fd841c97161607b9ee03229e'
   end
 
   env :std
