696dee9ba32d884941ed29352fe0e5b53b4eb961
Mike McQuaid <mike@mikemcquaid.com>
2013-07-17 15:17:52 -0700

    qt, qt5: don't depend on png, jpeg.
    Closes #21255.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/qt5.rb
+++ Library/Formula/qt5.rb
@@ -16,15 +16,12 @@ class Qt5 < Formula
   option 'with-mysql', 'Enable MySQL plugin'
   option 'developer', 'Compile and link Qt with developer options'
 
-  depends_on :libpng
-
   depends_on "d-bus" if build.include? 'with-qtdbus'
   depends_on "mysql" => :optional
-  depends_on "jpeg"
 
   def install
     args = ["-prefix", prefix,
-            "-system-libpng", "-system-zlib",
+            "-system-zlib",
             "-confirm-license", "-opensource"]
 
     unless MacOS::CLT.installed?
@@ -32,7 +29,6 @@ class Qt5 < Formula
       ENV.append 'CXXFLAGS', "-I#{MacOS.sdk_path}/System/Library/Frameworks/CoreFoundation.framework/Headers"
     end
 
-    args << "-L#{Formula.factory('jpeg').opt_prefix}/lib" << "-I#{Formula.factory('jpeg').opt_prefix}/include"
     args << "-L#{MacOS::X11.prefix}/lib" << "-I#{MacOS::X11.prefix}/include" if MacOS::X11.installed?
 
     args << "-plugin-sql-mysql" if build.with? 'mysql'
