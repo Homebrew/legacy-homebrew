5b3bfadd58164f3c8e26295e06d3b4be790d957a
Samy Al Bahra <sbahra@repnop.org>
2013-07-18 03:09:51 -0400

    concurrencykit 0.2.20
    Includes support for enabling pointer packing for significant
    performance gains and Intel TSX. Also includes support for running
    regression suite from homebrew.

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -2,14 +2,22 @@ require 'formula'
 
 class Concurrencykit < Formula
   homepage 'http://concurrencykit.org'
-  url 'http://concurrencykit.org/releases/ck-0.2.19.tar.gz'
-  sha1 '9fd2caa481004be12961c06d5fc510db3692fca5'
+  url 'http://concurrencykit.org/releases/ck-0.2.20.tar.gz'
+  sha1 'bc99f50a99061b1a1310fee7200591c35fcbcc36'
 
   head 'git://git.concurrencykit.org/ck.git'
 
+  option 'enable-pointer-packing', 'Build with pointer-packing enabled'
+  option 'enable-rtm',             'Build with support for Intel TSX'
+  option 'run-check',              'Run regression suite (takes long time)'
+
   def install
-    system "./configure", "--prefix=#{prefix}"
+    args = [ "--prefix=#{prefix}" ]
+    args << '--enable-pointer-packing' if build.include? 'enable-pointer-packing'
+    args << '--enable-rtm'             if build.include? 'enable-rtm'
+    system "./configure", *args
     system "make", "CC=#{ENV.cc}"
     system "make install"
+    system "make check" if build.include? 'run-check'
   end
 end
