454bbcd00476a5e9af3262eb6a98f4115d5ff5a7
Jan Stępień <jstepien@users.sourceforge.net>
2013-07-18 19:25:06 +0200

    ponysay 3.0.1

---
+++ Library/Formula/ponysay.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Ponysay < Formula
+  homepage 'http://terse.tk/ponysay/'
+  url 'https://github.com/erkin/ponysay/archive/3.0.1.tar.gz'
+  sha1 'bb867de2cf20a4bc454143d214c8968a0bdbe715'
+
+  depends_on :python3
+  depends_on "coreutils"
+
+  def install
+    system "./setup.py",
+           "--freedom=partial",
+           "--prefix=#{prefix}",
+           "--cache-dir=#{prefix}/var/cache",
+           "--sysconf-dir=#{prefix}/etc",
+           "install"
+  end
+end
