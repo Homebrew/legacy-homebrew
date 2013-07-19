a31383cd4bca1fd2cf43c4d984183a8d7cd2d0fd
Morton Jonuschat <yabawock@gmail.com>
2013-07-19 10:24:21 +0200

    Make libvpx compile on OSX 10.9 (Mavericks)

--- Library/Formula/libvpx.rb
+++ Library/Formula/libvpx.rb
@@ -56,3 +56,30 @@ __END__
             perl -p -e "s;.*?([a-z0-9_/]+.${sfx}).*;\1;")
  #" restore editor state
  for inc in ${includes}; do
+
+--- a/configure	2012-05-09 01:14:00.000000000 +0200
++++ b/configure	2013-07-19 10:10:02.000000000 +0200
+@@ -111,6 +111,7 @@
+ all_platforms="${all_platforms} x86-darwin10-gcc"
+ all_platforms="${all_platforms} x86-darwin11-gcc"
+ all_platforms="${all_platforms} x86-darwin12-gcc"
++all_platforms="${all_platforms} x86-darwin13-gcc"
+ all_platforms="${all_platforms} x86-linux-gcc"
+ all_platforms="${all_platforms} x86-linux-icc"
+ all_platforms="${all_platforms} x86-os2-gcc"
+@@ -123,6 +124,7 @@
+ all_platforms="${all_platforms} x86_64-darwin10-gcc"
+ all_platforms="${all_platforms} x86_64-darwin11-gcc"
+ all_platforms="${all_platforms} x86_64-darwin12-gcc"
++all_platforms="${all_platforms} x86_64-darwin13-gcc"
+ all_platforms="${all_platforms} x86_64-linux-gcc"
+ all_platforms="${all_platforms} x86_64-linux-icc"
+ all_platforms="${all_platforms} x86_64-solaris-gcc"
+@@ -134,6 +136,7 @@
+ all_platforms="${all_platforms} universal-darwin10-gcc"
+ all_platforms="${all_platforms} universal-darwin11-gcc"
+ all_platforms="${all_platforms} universal-darwin12-gcc"
++all_platforms="${all_platforms} universal-darwin13-gcc"
+ all_platforms="${all_platforms} generic-gnu"
+
+ # all_targets is a list of all targets that can be configured
