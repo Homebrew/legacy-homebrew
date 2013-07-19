8121c6e8f4a7125d05cf0caba24527a62dcdf853
Adam Vandenberg <flangy@gmail.com>
2013-07-19 11:12:08 -0700

    little-cms: add --universal

--- Library/Formula/little-cms.rb
+++ Library/Formula/little-cms.rb
@@ -5,11 +5,14 @@ class LittleCms < Formula
   url 'http://downloads.sourceforge.net/project/lcms/lcms/1.19/lcms-1.19.tar.gz'
   sha1 'd5b075ccffc0068015f74f78e4bc39138bcfe2d4'
 
+  option :universal
+
   depends_on :python => :optional
   depends_on 'jpeg' => :optional
   depends_on 'libtiff' => :optional
 
   def install
+    ENV.universal_binary if build.universal?
     args = ["--disable-debug", "--prefix=#{prefix}"]
     args << "--with-python" if build.with? "python"
 
