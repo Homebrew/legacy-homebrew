b29fd77291a55dcd0e8a4156771efff7859324f2
Morton Jonuschat <yabawock@gmail.com>
2013-07-19 10:24:21 +0200

    libvpx: enable compilation on Mavericks
    Closes #21322.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libvpx.rb
+++ Library/Formula/libvpx.rb
@@ -13,9 +13,12 @@ class Libvpx < Formula
 
   # Fixes build error on ML, discussed in:
   # https://github.com/mxcl/homebrew/issues/12567
-  # yasm: FATAL: unable to open include file `asm_enc_offsets.asm'.  Reported to:
+  # yasm: FATAL: unable to open include file `asm_enc_offsets.asm'.
+  # Reported to:
   # https://groups.google.com/a/webmproject.org/group/webm-discuss/browse_thread/thread/39d1166feac1061c
   # Not yet in HEAD as of 20 JUN 2012.
+  # Add Mavericks as a comple target, upstream in:
+  # http://git.chromium.org/gitweb/?p=webm/libvpx.git;a=commitdiff;h=fe4a52077f076fff4f3024373af21600afbc6df7
   def patches; DATA; end
 
   def install
@@ -56,3 +59,30 @@ __END__
             perl -p -e "s;.*?([a-z0-9_/]+.${sfx}).*;\1;")
  #" restore editor state
  for inc in ${includes}; do
+
+--- a/configure	2012-05-09 01:14:00.000000000 +0200
++++ b/configure	2013-07-19 10:10:02.000000000 +0200
+@@ -111,6 +111,7 @@
+ all_platforms="${all_platforms} x86-darwin10-gcc"
+ all_platforms="${all_platforms} x86-darwin11-gcc"
+ all_platforms="${all_platforms} x86-darwin12-gcc"
++all_platforms="${all_platforms} x86-darwin13-gcc"
+ all_platforms="${all_platforms} x86-linux-gcc"
+ all_platforms="${all_platforms} x86-linux-icc"
+ all_platforms="${all_platforms} x86-os2-gcc"
+@@ -123,6 +124,7 @@
+ all_platforms="${all_platforms} x86_64-darwin10-gcc"
+ all_platforms="${all_platforms} x86_64-darwin11-gcc"
+ all_platforms="${all_platforms} x86_64-darwin12-gcc"
++all_platforms="${all_platforms} x86_64-darwin13-gcc"
+ all_platforms="${all_platforms} x86_64-linux-gcc"
+ all_platforms="${all_platforms} x86_64-linux-icc"
+ all_platforms="${all_platforms} x86_64-solaris-gcc"
+@@ -134,6 +136,7 @@
+ all_platforms="${all_platforms} universal-darwin10-gcc"
+ all_platforms="${all_platforms} universal-darwin11-gcc"
+ all_platforms="${all_platforms} universal-darwin12-gcc"
++all_platforms="${all_platforms} universal-darwin13-gcc"
+ all_platforms="${all_platforms} generic-gnu"
+
+ # all_targets is a list of all targets that can be configured
