e3f8303e476af5243e30df7ee7b555b35a69509c
Christophe Robin <crobin@nekoo.com>
2013-07-20 23:09:02 +0900

    Update to libuv 11.6, now builds with gyp

--- Library/Formula/libuv.rb
+++ Library/Formula/libuv.rb
@@ -2,19 +2,28 @@ require 'formula'
 
 class Libuv < Formula
   homepage 'https://github.com/joyent/libuv'
-  url 'https://github.com/joyent/libuv/archive/v0.10.7.tar.gz'
-  sha1 '6bb4fef7849a2d357589859dd83fd1ad897a21b2'
+  url 'https://github.com/joyent/libuv/archive/v0.11.6.tar.gz'
+  sha1 'a429d644fa972a1685b105c839f92762dca64ea5'
 
   head 'https://github.com/joyent/libuv.git', :branch => 'master'
 
   option :universal
 
+  depends_on :python => :build
+  depends_on 'git' => :build
+
   def install
     ENV.universal_binary if build.universal?
 
-    system 'make', 'libuv.dylib'
+    system 'mkdir', '-p', 'build'
+    system 'git', 'clone', 'https://git.chromium.org/external/gyp.git', 'build/gyp'
+    system './gyp_uv', '-Dlibrary=shared_library'
+    cd 'out' do
+      ENV['BUILDTYPE'] = 'Release'
+      system 'make'
+    end
 
     include.install Dir['include/*']
-    lib.install 'libuv.dylib'
+    lib.install 'out/Release/libuv.dylib'
   end
 end
