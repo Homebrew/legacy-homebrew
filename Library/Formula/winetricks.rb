f0b8efcc3950b6e18701d3957cee166ff9866c0d
Xiyue Deng <manphiz@gmail.com>
2013-07-19 17:46:32 -0700

    winetricks: update to 20130707 revision 1034.

--- Library/Formula/winetricks.rb
+++ Library/Formula/winetricks.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Winetricks < ScriptFileFormula
   homepage 'http://code.google.com/p/winetricks/'
-  url 'http://winetricks.googlecode.com/svn-history/r1026/trunk/src/winetricks', :using => :curl
+  url 'http://winetricks.googlecode.com/svn-history/r1034/trunk/src/winetricks', :using => :curl
   # since the version stated in the field is seldom updated, we append the revision number
-  version '20130629-r1026'
-  sha256 'd0906097e6bd9b3f52715abb2e8d6ae70d20184d46ba004fa9f0bd537e786c1d'
+  version '20130707-r1034'
+  sha256 '00eb1a454815fa31061f762a819e2c4ae95b14c6507a9fff5485bc9f07719da5'
 
   head 'http://winetricks.googlecode.com/svn/trunk/src/winetricks', :using => :curl
 
