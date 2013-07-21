59bf921bcc680490e98bd6952385fb15411fde4b
Jonas <pewpew@fastmail.fm>
2013-07-21 21:30:10 +0200

    changed git-encrypt branch to develop

--- Library/Formula/git-encrypt.rb
+++ Library/Formula/git-encrypt.rb
@@ -2,10 +2,7 @@ require 'formula'
 
 class GitEncrypt < Formula
   homepage 'https://github.com/shadowhand/git-encrypt'
-  url 'https://github.com/shadowhand/git-encrypt/archive/0.3.0.tar.gz'
-  sha1 '24ce80398c003c082a21402eddf9e73387b6f885'
-
-  head 'https://github.com/shadowhand/git-encrypt.git', :branch => 'master'
+  head 'https://github.com/shadowhand/git-encrypt.git', :branch => 'develop'
 
   def install
     bin.install 'gitcrypt'
