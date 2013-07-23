e9c3271c391f745d12fc9f2bf7b1da3bab0f3482
Roger Leigh <r.leigh@dundee.ac.uk>
2013-07-23 14:16:37 +0100

    ice: Build python bindings
    This required a small patch to py/config/Make.rules.Darwin
    to remove the use of OSX 10.8-specific paths so that it will
    build on >= 10.6.

--- Library/Formula/ice.rb
+++ Library/Formula/ice.rb
@@ -39,6 +39,10 @@ class Ice < Formula
       system "make"
       system "make install"
     end
+    cd "py" do
+      system "make"
+      system "make install"
+    end
   end
 end
 
@@ -84,3 +88,17 @@ diff -urN Ice-3.5.0.original/cpp/config/Make.rules.Darwin Ice-3.5.0/cpp/config/M
  
  #
  # QT is used only for the deprecated IceGrid and IceStorm SQL plugins
+--- Ice-3.5.0.original/py/config/Make.rules.Darwin	2013-03-11 15:19:47.000000000 +0000
++++ Ice-3.5.0/py/config/Make.rules.Darwin	2013-07-23 13:41:42.000000000 +0100
+@@ -27,9 +27,9 @@
+     PYTHON_LIBS		= -F$(patsubst %/Python.framework/Versions/,%,$(dir $(PYTHON_HOME))) -framework Python
+ else
+     XCODE_PATH  = $(shell xcode-select --print-path)
+-    SDKS_DIR    = $(XCODE_PATH)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk
+-    PYTHON_HOME	= $(SDKS_DIR)/System/Library/Frameworks/Python.framework/Versions/Current
++    PYTHON_HOME	= /System/Library/Frameworks/Python.framework/Versions/Current
+     PYTHON_LIBS	= -framework Python
++
+ endif
+ 
+ PYTHON_INCLUDE_DIR	= $(PYTHON_HOME)/include/$(PYTHON_VERSION)
