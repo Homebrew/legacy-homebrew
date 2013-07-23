4fa8dd326367322d0ac3270d441cb2b0d5f5126e
Misty De Meo <mistydemeo@gmail.com>
2013-07-23 11:31:39 -0700

    contacts: only needs CLT on 10.8

--- Library/Formula/contacts.rb
+++ Library/Formula/contacts.rb
@@ -9,10 +9,10 @@ class Contacts < Formula
   version '1.1a-3'
   sha1 '79526dd96e5b5297daaae6327c79de9366f94c87'
 
-  # Yes, it requires both Xcode *and* the CLT to build;
+  # Yes, it requires both Xcode *and* the CLT to build on 10.8 and newer;
   # see https://github.com/mxcl/homebrew/issues/21408
   depends_on :xcode
-  depends_on :clt
+  depends_on :clt if MacOS.version > :lion
 
   def install
     system "make"
