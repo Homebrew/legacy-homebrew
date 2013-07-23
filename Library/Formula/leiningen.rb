a5adc0d97dfb71bcfdd6c790676ce74775ecad38
Hao Hong <agate.hao@gmail.com>
2013-07-23 17:57:19 +0800

    Update leiningen.rb
    skip self-install if lein-jar already exists.

--- Library/Formula/leiningen.rb
+++ Library/Formula/leiningen.rb
@@ -1,15 +1,18 @@
 require 'formula'
 
 class Leiningen < Formula
+  VERSION  = '2.2.0'
+  LEIN_JAR = "#{ENV['HOME']}/.lein/self-installs/leiningen-#{VERSION}-standalone.jar"
+  
   homepage 'https://github.com/technomancy/leiningen'
-  url 'https://github.com/technomancy/leiningen/archive/2.2.0.tar.gz'
+  url "https://github.com/technomancy/leiningen/archive/#{VERSION}.tar.gz"
   sha1 '0ca7e4ea68b490171d869bd5cc3912feba8d7ee9'
 
   head 'https://github.com/technomancy/leiningen.git'
 
   def install
     bin.install "bin/lein"
-    system "#{bin}/lein", "self-install"
+    system "#{bin}/lein", "self-install" unless File.exist?(LEIN_JAR)
     bash_completion.install 'bash_completion.bash' => 'lein-completion.bash'
     zsh_completion.install 'zsh_completion.zsh' => '_lein'
   end
