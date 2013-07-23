c84445a54b67e709abaeec7f20de5aeb95418fa6
Misty De Meo <mistydemeo@gmail.com>
2013-07-23 11:22:26 -0700

    contacts: depends_on both Xcode and CLT
    Fixes #21408.

--- Library/Formula/contacts.rb
+++ Library/Formula/contacts.rb
@@ -9,6 +9,11 @@ class Contacts < Formula
   version '1.1a-3'
   sha1 '79526dd96e5b5297daaae6327c79de9366f94c87'
 
+  # Yes, it requires both Xcode *and* the CLT to build;
+  # see https://github.com/mxcl/homebrew/issues/21408
+  depends_on :xcode
+  depends_on :clt
+
   def install
     system "make"
     bin.install "build/Deployment/contacts"
