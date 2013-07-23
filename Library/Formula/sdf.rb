0134d65d4e3eb71742ad734c741418fd89e31ea1
Brandon Bloom <brandon@brandonbloom.name>
2013-07-22 20:00:20 -0400

    sdf: depends on pkg-config
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/sdf.rb
+++ Library/Formula/sdf.rb
@@ -5,6 +5,7 @@ class Sdf < Formula
   url 'ftp://ftp.strategoxt.org/pub/stratego/StrategoXT/strategoxt-0.17/sdf2-bundle-2.4.tar.gz'
   sha1 'b9be75d56503e7f06fcc9cc543303bae123c0845'
 
+  depends_on 'pkg-config' => :build
   depends_on 'aterm'
 
   def install
