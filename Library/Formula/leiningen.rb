23d901c494550b3ee286892aaf68b63bb09e2abf
agate <agate.hao@gmail.com>
2013-07-24 09:39:22 +0800

    re-inline version

--- Library/Formula/leiningen.rb
+++ Library/Formula/leiningen.rb
@@ -1,11 +1,10 @@
 require 'formula'
 
 class Leiningen < Formula
-  VERSION  = '2.2.0'
-  LEIN_JAR = "#{ENV['HOME']}/.lein/self-installs/leiningen-#{VERSION}-standalone.jar"
+  LEIN_JAR = "#{ENV['HOME']}/.lein/self-installs/leiningen-2.2.0-standalone.jar"
   
   homepage 'https://github.com/technomancy/leiningen'
-  url "https://github.com/technomancy/leiningen/archive/#{VERSION}.tar.gz"
+  url "https://github.com/technomancy/leiningen/archive/2.2.0.tar.gz"
   sha1 '0ca7e4ea68b490171d869bd5cc3912feba8d7ee9'
 
   head 'https://github.com/technomancy/leiningen.git'
