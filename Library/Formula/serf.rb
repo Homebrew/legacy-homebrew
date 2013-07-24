c163700853cd18fcfed7d2a77d04539a1d9b36ef
Clemens Gruber <clemensgru@gmail.com>
2013-07-25 00:48:50 +0200

    serf 1.3.0
    Closes #21427.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/serf.rb
+++ Library/Formula/serf.rb
@@ -2,22 +2,18 @@ require 'formula'
 
 class Serf < Formula
   homepage 'http://code.google.com/p/serf/'
-  url 'http://serf.googlecode.com/files/serf-1.2.1.tar.bz2'
-  sha1 'f65fbbd72926c8e7cf0dbd4ada03b0d226f461fd'
+  url 'http://serf.googlecode.com/files/serf-1.3.0.tar.bz2'
+  sha1 '14ed3e1dc195016a548499b3831f3df6b2501d27'
 
   option :universal
 
   depends_on :libtool
   depends_on 'sqlite'
-
-  def apr_bin
-    superbin or "/usr/bin"
-  end
+  depends_on 'scons' => :build
 
   def install
     ENV.universal_binary if build.universal?
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-apr=#{apr_bin}"
-    system "make install"
+    system "scons", "PREFIX=#{prefix}"
+    system "scons install"
   end
 end
