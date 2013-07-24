ed10c9e2bb6e6c19c4b6e89ff8e4497ffa284282
Daniel Sloof <goapsychadelic@gmail.com>
2013-07-24 15:55:43 +0200

    Swap mirrors for xz
    Upstream is being hammered by Homebrew users. Would like to have less traffic.

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Xz < Formula
   homepage 'http://tukaani.org/xz/'
-  url 'http://tukaani.org/xz/xz-5.0.5.tar.gz'
-  mirror 'http://fossies.org/linux/misc/xz-5.0.5.tar.gz'
+  url 'http://fossies.org/linux/misc/xz-5.0.5.tar.gz'
+  mirror 'http://tukaani.org/xz/xz-5.0.5.tar.gz'
   sha256 '5dcffe6a3726d23d1711a65288de2e215b4960da5092248ce63c99d50093b93a'
 
   option :universal
