f0afc298c43520e40681eb93b73b21a86851acfc
Patrick Li <patrick@satellitemedia.co.nz>
2013-07-26 11:30:36 +1200

    neon: add --with-brewed-openssl
    Closes #21448.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/neon.rb
+++ Library/Formula/neon.rb
@@ -5,22 +5,30 @@ class Neon < Formula
   url 'http://www.webdav.org/neon/neon-0.29.6.tar.gz'
   sha1 'ae1109923303f67ed3421157927bc4bc29c58961'
 
-  depends_on 'pkg-config' => :build
-
   keg_only :provided_by_osx,
             "Compiling newer versions of Subversion on 10.6 require this newer neon."
 
   option :universal
+  option 'with-brewed-openssl', 'Include OpenSSL support via Homebrew'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'openssl' if build.with? 'brewed-openssl'
 
   def install
     ENV.universal_binary if build.universal?
     ENV.enable_warnings
-    system "./configure", "--disable-debug",
-                          "--prefix=#{prefix}",
-                          "--enable-shared",
-                          "--disable-static",
-                          "--disable-nls",
-                          "--with-ssl"
+    args = [
+      "--disable-debug",
+      "--prefix=#{prefix}",
+      "--enable-shared",
+      "--disable-static",
+      "--disable-nls",
+      "--with-ssl",
+    ]
+    if build.with? 'brewed-openssl'
+      args << "--with-libs=" + Formula.factory('openssl').opt_prefix.to_s
+    end
+    system "./configure", *args
     system "make install"
   end
 end
