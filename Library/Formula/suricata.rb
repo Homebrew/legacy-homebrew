ab1bc1082084abd963c9508eeae3021816ef2119
smiledawgg <smiledawgg@me.com>
2013-07-26 10:57:53 -0400

    suricata 1.4.5
    Closes #21463.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/suricata.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Suricata < Formula
+  homepage 'http://suricata-ids.org'
+  url 'http://www.openinfosecfoundation.org/download/suricata-1.4.5.tar.gz'
+  sha1 '723e111d19376dd352df4b50f3ee635a5328b01e'
+
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
+  depends_on 'pkg-config' => :build
+  depends_on 'libnet'
+  depends_on 'libyaml'
+  depends_on 'pcre'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
