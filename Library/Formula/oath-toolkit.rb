7dfdd6df2a262351bdfb51c9bbad1f2cd52caed3
Stefan <hydra_github@me.com>
2013-07-28 18:32:03 +0200

    oath-toolkit 2.4.0
    Closes #21504.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/oath-toolkit.rb
+++ Library/Formula/oath-toolkit.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class OathToolkit < Formula
   homepage 'http://www.nongnu.org/oath-toolkit/'
-  url 'http://download.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.0.2.tar.gz'
-  mirror 'http://download-mirror.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.0.2.tar.gz'
-  sha1 '3672924a9d9c3373ec62a6b79e9aa1e846c4d178'
+  url 'http://download.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.4.0.tar.gz'
+  mirror 'http://download-mirror.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.4.0.tar.gz'
+  sha1 '89d2cd30dd401a3f6973ec3c2b26f1cb737764a7'
 
   depends_on 'pkg-config' => :build
   depends_on 'libxmlsec1'
