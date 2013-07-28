59fd3b86f1b3b1d2ff55ba0adf286db83eaa38ae
Stefan <hydra_github@me.com>
2013-07-28 18:02:30 +0200

    polarssl 1.2.8
    Closes #21498.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/polarssl.rb
+++ Library/Formula/polarssl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Polarssl < Formula
   homepage 'http://polarssl.org/'
-  url 'https://polarssl.org/code/releases/polarssl-1.2.7-gpl.tgz'
-  sha256 'd64c2d1247f93cdeb884bd3706dfddffc636634bbf81d3772af750d5b5191328'
+  url 'https://polarssl.org/code/releases/polarssl-1.2.8-gpl.tgz'
+  sha1 'a3e69d4e9302529c5006dcb7d8ecab9c99488036'
 
   depends_on 'cmake' => :build
 
