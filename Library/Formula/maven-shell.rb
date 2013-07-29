cfe3c374cadb9aa252f49c2e24b31333c6484d37
Jan Jungnickel <jj@sgm-media.com>
2013-07-29 12:23:41 +0200

    maven-shell 1.1.0
    Closes #21519.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/maven-shell.rb
+++ Library/Formula/maven-shell.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MavenShell < Formula
   homepage 'http://shell.sonatype.org/'
-  url 'http://repo1.maven.org/maven2/org/sonatype/maven/shell/dist/mvnsh-assembly/1.0.1/mvnsh-assembly-1.0.1-bin.tar.gz'
-  sha1 'c8c7487689c53c1140d45d830c9e6d7d39f942c4'
+  url 'http://repo1.maven.org/maven2/org/sonatype/maven/shell/dist/mvnsh-assembly/1.1.0/mvnsh-assembly-1.1.0-bin.tar.gz'
+  sha1 '0aa91e810be695952f56e7369c00d9e4bf1a3ead'
 
   def install
     # Remove windows files.
