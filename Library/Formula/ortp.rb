0a430a198f32b7e092ab3eaa8e2a3da9799cf33c
Stefan <hydra_github@me.com>
2013-07-29 12:49:01 +0200

    ortp 0.22.0

--- Library/Formula/ortp.rb
+++ Library/Formula/ortp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ortp < Formula
   homepage 'http://www.linphone.org/eng/documentation/dev/ortp.html'
-  url 'http://download.savannah.gnu.org/releases/linphone/ortp/sources/ortp-0.20.0.tar.gz'
-  sha1 '9c66fb9512134869d4d2eb7766b6a8c5e4da1cc7'
+  url 'http://download.savannah.gnu.org/releases/linphone/ortp/sources/ortp-0.22.0.tar.gz'
+  sha1 '3f4712307ceba27c6498abd2090f411e02084dbd'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
