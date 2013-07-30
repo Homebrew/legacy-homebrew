a3424a36ee86a902412c32bc9327fe922e60d45f
Adam Vandenberg <flangy@gmail.com>
2013-07-30 09:29:20 -0700

    aws-elasticbeanstalk: fix audit

--- Library/Formula/aws-elasticbeanstalk.rb
+++ Library/Formula/aws-elasticbeanstalk.rb
@@ -4,7 +4,6 @@ class AwsElasticbeanstalk < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/code/6752709412171743'
   url 'https://s3.amazonaws.com/elasticbeanstalk/cli/AWS-ElasticBeanstalk-CLI-2.5.1.zip'
   sha1 'ad55ba660ff6852742ec1066abba506378049078'
-  version '2.5.1'
 
   def install
     # Remove versions for other platforms.
