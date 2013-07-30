4001ff1dfbaae4bdd4c1b5152101469bc4d841e2
Maggie Zhou <emmbeezee@gmail.com>
2013-07-30 18:03:48 -0400

    Change test to take in a valid input "-u" to test install.

--- Library/Formula/midicsv.rb
+++ Library/Formula/midicsv.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Midicsv < Formula
-  homepage 'http://www.fourmilab.ch/webtools/midicsv/'
+  homepage 'http://www.fourmilab.ch/webtools/midicsv'
   url 'http://www.fourmilab.ch/webtools/midicsv/midicsv-1.1.tar.gz'
   sha1 '1f34b6b874c26652ec4791701e5bfdccbbb35370'
 
@@ -17,6 +17,6 @@ class Midicsv < Formula
   end
 
   test do
-    system "midicsv"
-  end
+    system "midicsv -u"
+   end
 end
