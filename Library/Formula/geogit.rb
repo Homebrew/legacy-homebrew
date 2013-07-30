cd0b5165313e21103c798149568744a314582ca6
Adam Vandenberg <flangy@gmail.com>
2013-07-30 10:30:35 -0700

    geogit 0.3.0

--- Library/Formula/geogit.rb
+++ Library/Formula/geogit.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Geogit < Formula
   homepage 'http://www.geogit.org'
-  url 'http://downloads.sourceforge.net/project/geogit/geogit-0.2.0/geogit-cli-app-0.2.0.zip'
-  sha1 'd57e4a67a644c15f5415c5c73259a9efb5a0595a'
+  url 'http://downloads.sourceforge.net/project/geogit/geogit-0.3.0/geogit-cli-app-0.3.0.zip'
+  sha1 'c01976d4592da0184e5459c264c115577c084ae3'
 
   def install
     bin.install "bin/geogit"
     bin.install "bin/geogit-console"
     prefix.install "repo"
+    man1.install Dir['man/*']
   end
 end
