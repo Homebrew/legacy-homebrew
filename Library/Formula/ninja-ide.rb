dfb44b99958d120a4e03d194fabb1aa73c8f2afa
Jeroen <osscca.git@gmail.com>
2013-07-31 17:47:55 +0200

    Corrections to the formula in order to import macfsevents.

--- Library/Formula/ninja-ide.rb
+++ Library/Formula/ninja-ide.rb
@@ -5,27 +5,20 @@ class NinjaIde < Formula
   url 'https://github.com/ninja-ide/ninja-ide/archive/v2.3.tar.gz'
   sha1 '64ccbbf8521a8fbef43c3d57cf616b7f8b466460'
 
-  depends_on :python => :recommended
-  depends_on :python3 => :optional
+  depends_on :python
+  depends_on 'fsevents' => :python
   depends_on 'pyqt'
 
   def install
     python do
       system python, "setup.py", "install", "--prefix=#{prefix}"
-    end
 
-    if python
-      system "pip install --upgrade macfsevents"
-    end
-
-    if python3
-      system "pip3 install --upgrade macfsevents"
     end
   end
 
   def caveats
     <<-EOS.undent
-      Run Ninja-IDE by typing `ninja-ide`
+      You can run Ninja-IDE by typing `ninja-ide`
     EOS
   end
 
