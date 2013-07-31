33d8b9b9bf7ea746f540a37a8f18c3f32b2635a3
Adam Vandenberg <flangy@gmail.com>
2013-07-31 08:26:58 -0700

    hercules 3.09
    Closes #21560.

--- Library/Formula/hercules.rb
+++ Library/Formula/hercules.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Hercules < Formula
-  homepage 'http://www.hercules-390.org/'
-  url 'http://www.hercules-390.org/hercules-3.07.tar.gz'
-  sha1 'd0b2e543dd66ee43576e5a5faff8f4cc061cffb4'
+  homepage 'http://www.hercules-390.eu/'
+  url 'http://downloads.hercules-390.eu/hercules-3.09.tar.gz'
+  sha1 '5cbe89ec214de00b90e3077c27db636a7d2607f5'
 
   depends_on 'gawk'
 
