19acab2180d1bd35d2bb0969ecd278784895068c
Adam Vandenberg <flangy@gmail.com>
2013-07-31 10:11:46 -0700

    fb-client 1.1.4

--- Library/Formula/fb-client.rb
+++ Library/Formula/fb-client.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class FbClient < Formula
   homepage 'https://paste.xinu.at'
-  url 'https://paste.xinu.at/data/client/fb-1.1.3.tar.gz'
-  sha1 'e18605950735c6bd5e48636d7e7893235206e9cf'
+  url 'https://paste.xinu.at/data/client/fb-1.1.4.tar.gz'
+  sha1 '03483b5cdda9d27121941ddd10ffd20967f3f63b'
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
