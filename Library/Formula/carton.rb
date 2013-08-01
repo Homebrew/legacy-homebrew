7bd4682a859bc3cbbac7a12409f8353013fa9c0a
Josh Tilles <MerelyAPseudonym@gmail.com>
2013-08-01 18:11:30 -0400

    Remove manual setting of version.

--- Library/Formula/carton.rb
+++ Library/Formula/carton.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Carton < Formula
   homepage 'https://github.com/rejeep/carton#readme'
   url 'https://github.com/rejeep/carton/archive/v0.3.1.tar.gz'
-  version '0.3.1'
   sha1 'd8c76e6db32d319f95bfec1abad4627c144dd9df'
   head 'https://github.com/rejeep/carton.git'
 
