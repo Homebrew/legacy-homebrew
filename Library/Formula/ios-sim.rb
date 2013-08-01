b2df3db929590d78ac94fd4eac2d54b41f06d2ff
Jan Berkel <jan.berkel@gmail.com>
2013-08-01 14:02:43 +0200

    Testing 1.8.1

--- Library/Formula/ios-sim.rb
+++ Library/Formula/ios-sim.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class IosSim < Formula
   homepage 'https://github.com/phonegap/ios-sim'
-  url 'https://github.com/phonegap/ios-sim/archive/1.8.2.tar.gz'
-  sha1 '4328b3c8e6b455631d52b7ce5968170c9769eb1e'
+  url 'https://github.com/phonegap/ios-sim/archive/1.8.1.tar.gz'
+  sha1 '888466262bceaeaefc2658c0d932096877a805dd'
 
   def install
     rake "install", "prefix=#{prefix}"
