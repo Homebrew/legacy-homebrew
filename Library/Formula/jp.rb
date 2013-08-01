28fa5736736474d52dce70fded004591eabc4c20
Baptiste Fontaine <batifon@yahoo.fr>
2013-08-01 19:19:08 +0200

    jp v0.1 added (not a binary).

---
+++ Library/Formula/jp.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Jp < Formula
+  homepage 'http://www.paulhammond.org/jp/'
+  url 'https://github.com/paulhammond/jp/archive/v0.1.tar.gz'
+  sha1 'c4cc3f484d06154c8a6e5402e8ce5e0b570a2b33'
+
+  depends_on 'go' => :build
+
+  def install
+    ENV['GOPATH'] = pwd
+    mkdir_p 'src'
+    mv 'jp', 'src/jp'
+    `go get jp`
+    bin.install 'bin/jp'
+  end
+
+  test do
+    json = '{"foo":[1,2],"home":"brew"}'
+    File.open('data.json', 'w') { |f| f.write json }
+    `jp data.json`.gsub(/\s+/, '') == json
+  end
+end
