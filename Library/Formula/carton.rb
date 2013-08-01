a121f6596487bf33ed8885f8b00cc395dc3bbae6
Josh Tilles <MerelyAPseudonym@gmail.com>
2013-08-01 18:09:56 -0400

    Adhere to conventions about `head` placement.

--- Library/Formula/carton.rb
+++ Library/Formula/carton.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Carton < Formula
   homepage 'https://github.com/rejeep/carton#readme'
-  head 'git://github.com/rejeep/carton'
   url 'https://github.com/rejeep/carton/archive/v0.3.1.tar.gz'
   version '0.3.1'
   sha1 'd8c76e6db32d319f95bfec1abad4627c144dd9df'
+  head 'https://github.com/rejeep/carton.git'
 
   # `package.el` is only included by default in Emacs >= 24
   depends_on 'emacs'
