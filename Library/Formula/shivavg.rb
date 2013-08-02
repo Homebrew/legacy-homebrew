3ca9170a0b240813602e428672b3b4bc63fcdc61
Ben Swift <benjamin.j.swift@gmail.com>
2013-08-02 16:02:44 +1000

    ShivaVG 0.2.1
    Closes #21605.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/shivavg.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Shivavg < Formula
+  homepage 'http://sourceforge.net/projects/shivavg/'
+  url 'http://downloads.sourceforge.net/project/shivavg/ShivaVG/0.2.1/ShivaVG-0.2.1.zip'
+  sha1 'f018c9d525f6cc65703bd1310662aca68e04e5d3'
+
+  depends_on :automake
+  depends_on :autoconf
+  depends_on :libtool
+
+  def install
+    system "autoreconf", "-ivf"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-example-all=no"
+    system "make", "install"
+  end
+end
