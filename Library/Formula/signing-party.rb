e1816a1f7cedc3c3c0acdb34d54ba9bb2f6ed2db
Alessio Caiazza <nolith@abisso.org>
2013-08-03 11:09:09 +0200

    signing-party: gpgparticipants: not working on OSX
    ```data``` on OSX behave differently from the linux version.
    
    Closes #21628.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/signing-party.rb
+++ Library/Formula/signing-party.rb
@@ -20,6 +20,12 @@ class SigningParty < Formula
   depends_on 'Text::Iconv' => :perl
   depends_on 'GnuPG::Interface' => :perl
 
+  # gpgparticipants data on OS X behaves differently from linux version
+  # https://github.com/mxcl/homebrew/pull/21628
+  def patches
+    DATA
+  end
+
   def install
     # gpgdir and gpgwrap are not included as they have their own homepages
     # springraph is not included because it depends on the 'GD' perl module
@@ -90,3 +96,18 @@ class SigningParty < Formula
     end
   end
 end
+
+__END__
+diff --git a/gpgparticipants/gpgparticipants b/gpgparticipants/gpgparticipants
+index 4dd06e8..ea76aff 100755
+--- a/gpgparticipants/gpgparticipants
++++ b/gpgparticipants/gpgparticipants
+@@ -29,7 +29,7 @@ title=$(echo "$5"|tr a-z A-Z|sed 's/\(.\)/\1 /g')
+ exec > "$output"
+ 
+ # Date of event
+-LANG=C date --date="$date" +"%A, %B %e, %Y;  %H:%M"
++LANG=C date -j -f "%Y%m%d %H%M" "$date" +"%A, %B %e, %Y;  %H:%M"
+ # Organiser contact
+ printf "%80s\n\n\n" "$org"
+ # Title
