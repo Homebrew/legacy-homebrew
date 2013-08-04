c0de4f8a5e0f0d959b09110fc048b86ac5a653e4
Keisuke Takahashi <keithseahus@gmail.com>
2013-08-04 16:29:52 +0900

    Fix dependency on ant against failure in \"brew readall\" test on Jenkins.

--- Library/Formula/voltdb.rb
+++ Library/Formula/voltdb.rb
@@ -11,7 +11,7 @@ class Voltdb < Formula
   url 'https://github.com/VoltDB/voltdb/archive/voltdb-3.4.tar.gz'
   sha1 '9a45ee70b99ea32c0c919b786ba528677db3d28'
 
-  depends_on 'ant' => :build
+  depends_on 'ant'
   depends_on :python
   depends_on 'valgrind' => :optional
 
