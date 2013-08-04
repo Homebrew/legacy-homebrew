246af303eee40dc3f431479b97e4f1541fd2440d
Tijs Zwinkels - TinkerTank <tijs@tinkertank.eu>
2013-08-04 13:53:01 +0100

    Fix error when downloading: curl: (35) error:14077458:SSL routines:SSL23_GET_SERVER_HELLO:reason(1112) Issue and fix very similar to https://github.com/mxcl/homebrew/issues/20991

--- Library/Formula/easy-git.rb
+++ Library/Formula/easy-git.rb
@@ -1,8 +1,17 @@
 require 'formula'
 
+# Force use of SSL3
+# Similar to https://github.com/mxcl/homebrew/issues/20991
+class CurlSSL3DownloadStrategy < CurlDownloadStrategy
+  def _fetch
+    curl @url, '-3', '-C', downloaded_size, '-o', @temporary_path
+  end
+end
+
 class EasyGit < Formula
   homepage 'http://people.gnome.org/~newren/eg/'
-  url 'http://people.gnome.org/~newren/eg/download/1.7.3/eg'
+  url 'https://people.gnome.org/~newren/eg/download/1.7.3/eg',
+    :using => CurlSSL3DownloadStrategy
   version "1.7.3"
   sha1 'd17165c20ea1b3887f1f81ec6d1217727b817409'
 
