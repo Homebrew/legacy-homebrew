1ef2e3e752104df53d09aad0530880bb2577824f
Jack Nagel <jacknagel@gmail.com>
2013-08-05 11:34:17 -0500

    gd: make deps explicit

--- Library/Formula/gd.rb
+++ Library/Formula/gd.rb
@@ -19,10 +19,36 @@ class Gd < Formula
     cause "Undefined symbols when linking"
   end
 
+  def png_prefix
+    MacOS.version >= :mountain_lion ? HOMEBREW_PREFIX/"opt/libpng" : MacOS::X11.prefix
+  end
+
+  def freetype_prefix
+    MacOS.version >= :mountain_lion ? HOMEBREW_PREFIX/"opt/freetype" : MacOS::X11.prefix
+  end
+
   def install
     ENV.universal_binary if build.universal?
     args = %W{--disable-dependency-tracking --prefix=#{prefix}}
-    args << "--without-freetype" unless build.with? 'freetype'
+
+    if build.with? "libpng"
+      args << "--with-png=#{png_prefix}"
+    else
+      args << "--without-png"
+    end
+
+    if build.with? "freetype"
+      args << "--with-freetype=#{freetype_prefix}"
+    else
+      args << "--without-freetype"
+    end
+
+    if build.with? "jpeg"
+      args << "--with-jpeg=#{Formula.factory("jpeg").opt_prefix}"
+    else
+      args << "--without-jpeg"
+    end
+
     system "./configure", *args
     system "make install"
   end
