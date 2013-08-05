c7ece7733a4409ab867b6c471c353dcae29dc316
Greg Turnquist <gturnquist@gopivotal.com>
2013-08-05 13:14:04 -0500

    Patch formula based on feedback

--- Library/Formula/spring-boot-cli.rb
+++ Library/Formula/spring-boot-cli.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class SpringBootCli < Formula
   homepage 'https://github.com/SpringSource/spring-boot'
+  url 'https://repo.springsource.org/milestone/org/springframework/boot/spring-boot-cli/0.5.0.M1/spring-boot-cli-0.5.0.M1.jar'
   version '0.5.0.M1'
-  url 'https://repo.springsource.org/milestone/org/springframework/boot/spring-boot-cli/%s/spring-boot-cli-%s.jar' % [version, version]
   sha1 'd620af87d6a253a4577118c018c320b8f299a399'
 
   def install
-    libexec.install('spring-boot-cli-%s.jar' % version)
-    File.open('spring', 'w') { |file|
-      file.write("#!/bin/sh\n")
-      file.write("java -jar /usr/local/Cellar/spring-boot-cli/%s/libexec/spring-boot-cli-%s.jar \$*\n" % [version, version])
-    }
+    libexec.install 'spring-boot-cli-0.5.0.M1.jar'
+    (buildpath/'spring').write <<-EOS.undent
+      #!/bin/sh
+      java -jar #{libexec}/spring-boot-cli-0.5.0.M1.jar $*
+    EOS
     bin.install('spring')
   end
 
