827285af12ffdf6e66ab1a89be54daa4e055c760
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2013-08-05 15:36:53 +0100

    yafc: 1.2.5  new formula added
    Added formula for YAFC (Yet Another FTP Client) 1.2.5
    
    There is an old PR for yafc which never got merged, I've hopefully
    avoided the problems that were raised with that one. Old PR:
    https://github.com/mxcl/homebrew/pull/19871

---
+++ Library/Formula/yafc.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Yafc < Formula
+  homepage 'http://www.yafc-ftp.com/'
+  url 'http://www.yafc-ftp.com/upload/yafc-1.2.5.tar.xz'
+  sha1 'b8156689f92a5d9b684180394f1763500a25c1a5'
+
+  depends_on 'readline'  # we get a compilation error with system readline
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+           "--with-readline="+Formula.factory('readline').opt_prefix
+    system "make", "install"
+  end
+
+  test do
+    # Prints version information on stdout and exits with success
+    system "yafc -V"
+  end
+end
