2f8ad0b187e8c39d6eaa4ec6afc8036050673099
James Deaton <jdeaton@gmail.com>
2013-08-05 15:50:51 -0500

    lnav 0.5.0
    Adds new formula for lnav, the Logfile Navigator, a curses-
    based tool for viewing and analyzing log files.
    
    Closes #21682.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/lnav.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Lnav < Formula
+  homepage 'http://tstack.github.io/lnav/'
+  url 'http://lnav.org/downloads/lnav-0.5.0.tar.bz2'
+  sha1 '8d68cb8b46878b2a54e9004a1f4994b07772b8b0'
+
+  head 'https://github.com/tstack/lnav.git'
+
+  depends_on 'readline'
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
