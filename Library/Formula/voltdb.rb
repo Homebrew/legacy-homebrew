75bdce3efc97cb597a55527e4e856701cd192155
Keisuke Takahashi <keithseahus@gmail.com>
2013-08-06 01:04:15 +0900

    Fix some insufficient codes.

--- Library/Formula/voltdb.rb
+++ Library/Formula/voltdb.rb
@@ -1,17 +1,10 @@
 require 'formula'
 
-class Formula
-  def tools; prefix+'tools' end
-  def voltdb; prefix+'voltdb' end
-end
-
 class Voltdb < Formula
   homepage 'https://github.com/VoltDB/voltdb'
   head 'https://github.com/VoltDB/voltdb.git'
   url 'https://github.com/VoltDB/voltdb/archive/voltdb-3.4.tar.gz'
   sha1 '9a45ee70b99ea32c0c919b786ba528677db3d284'
-
-  depends_on :python
 
   def install
     system 'ant'
@@ -22,15 +15,12 @@ class Voltdb < Formula
     end
     ln_s lib/'voltdb/python', lib/'python'
     ln_s lib/'voltdb/extension', lib/'extension'
-    tools.install Dir['tools/*']
-    voltdb.install Dir['voltdb/*']
+    prefix.install 'tools'
+    prefix.install 'voltdb'
     doc.install Dir['doc/*']
     prefix.install 'version.txt'
 
-    file_to_mod = prefix/'bin/voltdb'
-    system("chmod +w #{file_to_mod}")
-    system("sed -i '' 's/VOLTDB_LIB=\$VOLTDB_HOME\\/lib/VOLTDB_LIB=\$VOLTDB_HOME\\/lib\\/voltdb/' #{file_to_mod}")
-    system("chmod -w #{file_to_mod}")
+    inreplace prefix/'bin/voltdb', /VOLTDB_LIB=\$VOLTDB_HOME\/lib/, 'VOLTDB_LIB=$VOLTDB_HOME/lib/voltdb'
   end
 
   test do
