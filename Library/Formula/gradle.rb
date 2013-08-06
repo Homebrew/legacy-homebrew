45d598ccb381e5347d73e3ba1a6f83ae5d1416de
seanjreilly <seanjreilly@gmail.com>
2013-08-06 13:10:33 +0100

    Gradle 1.7
    Closes #21695.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -2,14 +2,8 @@ require 'formula'
 
 class Gradle < Formula
   homepage 'http://www.gradle.org/'
-  url 'http://services.gradle.org/distributions/gradle-1.6-bin.zip'
-  sha1 '631650e2b0aa8dea45f94a999ecab850d0f07370'
-
-  devel do
-    url 'http://services.gradle.org/distributions/gradle-1.7-rc-2-bin.zip'
-    sha1 'c2117683acbfaaa5eb7a1a93220f6290f8835f2f'
-    version '1.7-rc2'
-  end
+  url 'http://services.gradle.org/distributions/gradle-1.7-bin.zip'
+  sha1 '0cbadad3ced2ba5c53bbb7e6ec3e779c1cd3966c'
 
   def install
     libexec.install %w[bin lib]
