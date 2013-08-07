514d9146250c56d45ae08795b32297ff3a827f25
Jack Nagel <jacknagel@gmail.com>
2013-08-06 19:24:19 -0500

    MacSystem now just wraps MacOS

--- Library/Homebrew/superenv/macsystem.rb
+++ Library/Homebrew/superenv/macsystem.rb
@@ -1,10 +1,8 @@
-# new code because I don't really trust the Xcode code now having researched it more
-module MacSystem extend self
+module MacSystem
+  extend self
+
   def xcode_clt_installed?
-    # TODO: handle 10.9 CLT which has headers under:
-    # /Library/Developer/CommandLineTools/usr/include
-    File.executable? "/usr/bin/clang" and File.executable? "/usr/bin/lldb" and
-      File.executable? "/usr/bin/make" and File.directory? "/usr/include"
+    MacOS::CLT.installed?
   end
 
   def xcode43_without_clt?
@@ -12,7 +10,6 @@ module MacSystem extend self
   end
 
   def x11_prefix
-    @x11_prefix ||= %W[/opt/X11 /usr/X11
-      #{MacOS.sdk_path}/usr/X11].find{|path| File.directory? "#{path}/include" }
+    MacOS::X11.prefix
   end
 end
