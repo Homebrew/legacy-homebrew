cb04e3fe806d73aabb5432f7fc967a9521e3fe56
Nikolaus Wittenstein <nikolaus.wittenstein@gmail.com>
2013-08-07 12:26:57 -0400

    czmq: add HEAD option
    Closes #21730.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/czmq.rb
+++ Library/Formula/czmq.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Czmq < Formula
   homepage 'http://czmq.zeromq.org/'
+  head 'https://github.com/zeromq/czmq.git'
   url 'http://download.zeromq.org/czmq-1.4.1.tar.gz'
   sha1 '8ddb485e9d53bca6bb703c850be40b8da70c4d74'
 
@@ -9,8 +10,19 @@ class Czmq < Formula
 
   depends_on 'zeromq'
 
+  if build.head?
+    depends_on 'autoconf'
+    depends_on 'automake'
+    depends_on 'libtool'
+  end
+
   def install
     ENV.universal_binary if build.universal?
+
+    if build.head?
+      system "./autogen.sh"
+    end
+
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
