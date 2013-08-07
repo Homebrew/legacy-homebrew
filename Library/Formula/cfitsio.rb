1793fc17fe80728d2c61825bdce0ebdb94d1d3b5
Adam Vandenberg <flangy@gmail.com>
2013-08-06 22:28:49 -0700

    cfitsio: use resource

--- Library/Formula/cfitsio.rb
+++ Library/Formula/cfitsio.rb
@@ -1,11 +1,5 @@
 require 'formula'
 
-class CfitsioExamples < Formula
-  url 'http://heasarc.gsfc.nasa.gov/docs/software/fitsio/cexamples/cexamples.zip'
-  version '2012.09.24'
-  sha1 '668ffa9a65a66c9f1d7f4241867e1e8adf653231'
-end
-
 class Cfitsio < Formula
   homepage 'http://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html'
   url 'ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/cfitsio3350.tar.gz'
@@ -14,20 +8,24 @@ class Cfitsio < Formula
 
   option 'with-examples', "Compile and install example programs"
 
+  resource 'examples' do
+    url 'http://heasarc.gsfc.nasa.gov/docs/software/fitsio/cexamples/cexamples.zip'
+    version '2012.09.24'
+    sha1 '668ffa9a65a66c9f1d7f4241867e1e8adf653231'
+  end if build.with? 'examples'
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make shared"
     system "make install"
 
-    if build.include? 'with-examples'
+    if build.with? 'examples'
       system "make fpack funpack"
       bin.install 'fpack', 'funpack'
 
-      # fetch, compile and install examples programs
-      CfitsioExamples.new.brew do
-        Dir['*.c'].each do |f|
-          # compressed_fits.c does not work (obsolete function call)
-          next if f == 'compress_fits.c'
+      resource('examples').stage do
+        # compressed_fits.c does not work (obsolete function call)
+        Dir['*.c'].reject{|f| f == 'compress_fits.c'}.each do |f|
           system ENV.cc, f, "-I#{include}", "-L#{lib}", "-lcfitsio", "-lm", "-o", "#{bin}/#{f.sub('.c', '')}"
         end
       end
