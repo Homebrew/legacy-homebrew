3c8e3c7400d020b39b02c7d5a42c5e23adb7ae6e
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2013-08-07 10:46:36 +0300

    clojurescript recipe

---
+++ Library/Formula/clojurescript.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Clojurescript < Formula
+  homepage 'https://github.com/clojure/clojurescript'
+  url 'https://github.com/clojure/clojurescript/archive/r1853.tar.gz'
+  sha1 '19e07e266ea189d791bfe06507e4560483b8f0d6'
+
+  head "https://github.com/clojure/clojurescript.git"
+
+  def install
+    system "./script/bootstrap"
+    prefix.install Dir['*'] 
+    [bin/'cljsc', prefix/'script/repl', prefix/'script/repljs', prefix/'script/browser-repl'].map { | file | set_env(file) }
+    [prefix/'script/repljs', prefix/'script/browser-repl'].map { | file | bin.install(file) }
+  end
+
+  test do
+    system "cljsc"
+  end
+
+  private
+
+  def set_env(file)
+    lines = IO.readlines(file)
+    lines.insert(1, "CLOJURESCRIPT_HOME=#{prefix}")
+    File.open(file, 'w') do |file|
+      file.puts lines
+    end
+  end
+
+end
