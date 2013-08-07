58086c9be5a067606a8033efbdcf53521f063dd7
Adam Vandenberg <flangy@gmail.com>
2013-08-06 21:18:22 -0700

    leiningen: use resource

--- Library/Formula/leiningen.rb
+++ Library/Formula/leiningen.rb
@@ -1,10 +1,5 @@
 require 'formula'
 
-class LeiningenJar < Formula
-  url 'https://leiningen.s3.amazonaws.com/downloads/leiningen-2.3.2-standalone.jar'
-  sha1 'ed6f93be75c796408544042cfd26699d45b49725'
-end
-
 class Leiningen < Formula
   homepage 'https://github.com/technomancy/leiningen'
   url 'https://github.com/technomancy/leiningen/archive/2.3.2.tar.gz'
@@ -12,12 +7,19 @@ class Leiningen < Formula
 
   head 'https://github.com/technomancy/leiningen.git'
 
+  resource 'jar' do
+    url 'https://leiningen.s3.amazonaws.com/downloads/leiningen-2.3.2-standalone.jar'
+    sha1 'ed6f93be75c796408544042cfd26699d45b49725'
+  end
+
   def install
-    LeiningenJar.new.brew { libexec.install "leiningen-#{version}-standalone.jar" }
+    libexec.install resource('jar')
+
     # bin/lein autoinstalls and autoupdates, which doesn't work too well for us
     inreplace "bin/lein-pkg" do |s|
       s.change_make_var! 'LEIN_JAR', libexec/"leiningen-#{version}-standalone.jar"
     end
+
     bin.install "bin/lein-pkg" => 'lein'
     bash_completion.install 'bash_completion.bash' => 'lein-completion.bash'
     zsh_completion.install 'zsh_completion.zsh' => '_lein'
