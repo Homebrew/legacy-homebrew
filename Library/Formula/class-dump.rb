e9c39c6bdd3be9614a7e341e63ce5cd5d67ed5f3
Adam Vandenberg <flangy@gmail.com>
2013-08-07 10:16:36 -0700

    class-dump: remove skip-clean

--- Library/Formula/class-dump.rb
+++ Library/Formula/class-dump.rb
@@ -2,12 +2,9 @@ require 'formula'
 
 class ClassDump < Formula
   homepage 'http://www.codethecode.com/projects/class-dump/'
+  head 'https://github.com/nygard/class-dump.git'
   url 'http://www.codethecode.com/download/class-dump-3.4.tar.bz2'
   sha1 'bc6d9542af201028ae980b9d0497b491ce98227f'
-
-  head 'https://github.com/nygard/class-dump.git'
-
-  skip_clean "bin/class-dump"
 
   def install
     bin.install 'class-dump'
