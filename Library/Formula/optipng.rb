b95c31827f6ddfb8e5ed3ddc7104ac956b58b98f
Denis Denisov <denieldfu@gmail.com>
2013-08-08 14:18:02 +0300

    optipng: --HEAD
    Add allow --HEAD version download

--- Library/Formula/optipng.rb
+++ Library/Formula/optipng.rb
@@ -4,6 +4,7 @@ class Optipng < Formula
   homepage 'http://optipng.sourceforge.net/'
   url 'http://downloads.sourceforge.net/project/optipng/OptiPNG/optipng-0.7.4/optipng-0.7.4.tar.gz'
   sha1 'dcde17501cfb7a425b6cc23cfed482bbfd4867bb'
+  head 'http://optipng.hg.sourceforge.net/hgweb/optipng/optipng', :using => :hg 
 
   def install
     system "./configure", "--with-system-zlib",
