14a315e3580a70238278e9b55cf0404f87d315ad
Samuel John <github@SamuelJohn.de>
2013-08-08 11:57:51 +0200

    Don't hard-code PYTHONPATH for `brew test`
    This rules out any possibility to test Python 3.x software as the
    PYTHONPATH is used by any Python and `brew test pyqt --with-python3`
    fails because it picks up the `pyqt` module fomr Python 2.x's
    site-packages. Upstream tells us not to set the PYTHONPATH and then
    starting another (major) python version.

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -508,8 +508,6 @@ class Formula
   def test
     require 'test/unit/assertions'
     extend(Test::Unit::Assertions)
-    # TODO: move this into PythonInstalled.
-    ENV['PYTHONPATH'] = PythonInstalled.new.global_site_packages
     ret = nil
     mktemp do
       @testpath = Pathname.pwd
