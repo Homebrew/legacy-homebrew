9cdeb45d7bd50c608646cef61e23f3c898b05e58
Jeffrey Stedfast <fejj@gnome.org>
2013-08-08 16:32:47 -0400

    gmime 2.6.17
    Closes #21768.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gmime.rb
+++ Library/Formula/gmime.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gmime < Formula
   homepage 'http://spruce.sourceforge.net/gmime/'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gmime/2.6/gmime-2.6.15.tar.xz'
-  sha256 'b4c2a0b99b82063387cd750a38421ebaa0636f339e67984a84371bcb697dc99a'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gmime/2.6/gmime-2.6.17.tar.xz'
+  sha256 '22d49e57c404a0a68d1ac3426c8f23a39185a4b2d569de03bece07db03d1202f'
 
   depends_on 'pkg-config' => :build
   depends_on 'xz' => :build
@@ -14,6 +14,8 @@ class Gmime < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--enable-largefile",
+                          "--disable-introspection",
+                          "--disable-vala",
                           "--disable-mono",
                           "--disable-glibtest"
     system "make install"
