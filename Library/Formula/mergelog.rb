4ea4a8671f144153eb4a99a752ced15fd32f761a
Ingmar Hupp <commits@sdfjkl.org>
2013-08-08 13:46:13 +0100

    mergelog 4.5
    Closes #21754.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/mergelog.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Mergelog < Formula
+  homepage 'http://mergelog.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/mergelog/mergelog-4.5.tar.gz'
+  sha1 'bc9bdfddc561301e417a2de949ce3c4203b54c94'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/mergelog", "/dev/null"
+  end
+end
