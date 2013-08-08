054ddcda8ae48498bb65ecfea3057853215361cc
Dmitry Nedospasov <dmitry@nedos.net>
2013-08-08 18:00:31 +0200

    libftdi 1.0
    Closes #21758.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libftdi.rb
+++ Library/Formula/libftdi.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Libftdi < Formula
   homepage 'http://www.intra2net.com/en/developer/libftdi'
-  url "http://www.intra2net.com/en/developer/libftdi/download/libftdi-0.20.tar.gz"
-  sha1 '4bc6ce70c98a170ada303fbd00b8428d8a2c1aa2'
+  url 'http://www.intra2net.com/en/developer/libftdi/download/libftdi1-1.0.tar.bz2'
+  sha1 '5be76cfd7cd36c5291054638f7caf4137303386f'
 
+  depends_on 'cmake' => :build
   depends_on 'libusb-compat'
 
   def install
     mkdir 'libftdi-build' do
-      system "../configure", "--prefix=#{prefix}"
-      system "make"
+      system "cmake", "..", *std_cmake_args
       system "make install"
     end
   end
