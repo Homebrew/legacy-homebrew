c7e4955b907361d7d21a6c2703c809f1582fd800
Samuel John <github@SamuelJohn.de>
2013-08-08 12:08:50 +0200

    python: Harden build by unsetting PYTHONPATH

--- Library/Formula/python.rb
+++ Library/Formula/python.rb
@@ -56,6 +56,7 @@ class Python < Formula
     # Unset these so that installing pip and setuptools puts them where we want
     # and not into some other Python the user has installed.
     ENV['PYTHONHOME'] = nil
+    ENV['PYTHONPATH'] = nil
 
     args = %W[
              --prefix=#{prefix}
