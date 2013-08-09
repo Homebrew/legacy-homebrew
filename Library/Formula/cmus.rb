6302f0dead477d0536275a40f80f59c0b91d8840
Adam Vandenberg <flangy@gmail.com>
2013-08-09 07:16:35 -0700

    cmus: remove CFLAGS code

--- Library/Formula/cmus.rb
+++ Library/Formula/cmus.rb
@@ -18,13 +18,6 @@ class Cmus < Formula
   depends_on 'libcue'
 
   def install
-    # We add this to CPPFLAGS and LDFLAGS in ENV, but cmus doesn't
-    # pick up on that. Adding this patch because I am too lazy to
-    # send a patch upstream to respect CPPFLAGS - @adamv
-    unless HOMEBREW_PREFIX.to_s == '/usr/local'
-      ENV.append 'CFLAGS', "-isystem #{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib"
-    end
-
     system "./configure", "prefix=#{prefix}", "mandir=#{man}"
     system "make install"
   end
