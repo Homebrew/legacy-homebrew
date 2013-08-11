739f75612b0db9405dc0a792e4d11144bee54df5
Mike Fiedler <miketheman@gmail.com>
2013-08-11 12:40:57 -0400

    tlassemble: use correct version url

--- Library/Formula/tlassemble.rb
+++ Library/Formula/tlassemble.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Tlassemble < Formula
   homepage 'http://www.dayofthenewdan.com/projects/tlassemble/'
-  url 'https://github.com/dbridges/cocoa-tlassemble/archive/master.zip'
+  url 'https://github.com/dbridges/cocoa-tlassemble/archive/v1.0.zip'
   sha1 'afb9e809051de7022ee9e7b8649b2ef3abab70d7'
   version '1.0'
 
