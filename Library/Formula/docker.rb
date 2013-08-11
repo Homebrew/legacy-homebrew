ca64be1c97aac1f479cbd3f1cca206eed05dc988
Wes Morgan <cap10morgan@gmail.com>
2013-08-10 19:10:35 -0600

    add docker formula

---
+++ Library/Formula/docker.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Docker < Formula
+  homepage 'http://docker.io/'
+  url 'https://github.com/dotcloud/docker/archive/v0.5.2.tar.gz'
+  sha1 'd8961d57a8de950ec30975ff9fa675f4da3bc27e'
+
+  depends_on 'go' => :build
+
+  def install
+    system "make", "all"
+    bin.install("bin/docker")
+  end
+
+  def caveats; <<-EOF.undent
+    Docker is a client / server system, but the server doesn't (yet) run on
+    Mac OS X. You can, however, run the client and point it at a Docker
+    daemon running inside a Linux VM or remote host.
+
+    To point the Docker client at a remote daemon instance:
+        docker -H=tcp://docker.host:4243 [command]
+    EOF
+  end
+
+  test do
+    system "docker"
+  end
+end
