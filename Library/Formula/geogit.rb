e8e1460d03e28a04231e263d4b39825937de8df0
Matthew Vincent <matt@averails.com>
2013-08-11 12:02:55 -0700

    GeoGit 0.4.0
    Closes #21822.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/geogit.rb
+++ Library/Formula/geogit.rb
@@ -2,13 +2,11 @@ require 'formula'
 
 class Geogit < Formula
   homepage 'http://www.geogit.org'
-  url 'http://downloads.sourceforge.net/project/geogit/geogit-0.3.0/geogit-cli-app-0.3.0.zip'
-  sha1 'c01976d4592da0184e5459c264c115577c084ae3'
+  url 'http://downloads.sourceforge.net/project/geogit/geogit-0.4.0/geogit-cli-app-0.4.0.zip'
+  sha1 '41d87374e96a765da5ddab928b4cef0756ad339c'
 
   def install
-    bin.install "bin/geogit"
-    bin.install "bin/geogit-console"
+    bin.install "bin/geogit", "bin/geogit-console"
     prefix.install "repo"
-    man1.install Dir['man/*']
   end
 end
