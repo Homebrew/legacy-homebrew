3ff4cd865b0d4d314b0913eced83295a6b6ea1c3
Brett Koonce <koonce@gmail.com>
2013-08-10 19:20:40 -0500

    gsl 1.16
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gsl.rb
+++ Library/Formula/gsl.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gsl < Formula
   homepage 'http://www.gnu.org/software/gsl/'
-  url 'http://ftpmirror.gnu.org/gsl/gsl-1.15.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gsl/gsl-1.15.tar.gz'
-  sha1 'd914f84b39a5274b0a589d9b83a66f44cd17ca8e'
+  url 'http://ftpmirror.gnu.org/gsl/gsl-1.16.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/gsl/gsl-1.16.tar.gz'
+  sha1 '210af9366485f149140973700d90dc93a4b6213e'
 
   option :universal
 
