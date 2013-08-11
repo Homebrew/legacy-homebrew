728f735a8e97c521b46ad11f52bfb6017bda1df5
Mike Fiedler <miketheman@gmail.com>
2013-08-11 07:39:56 -0400

    adds tlassemble, time lapse assembler cli tool

---
+++ Library/Formula/tlassemble.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Tlassemble < Formula
+  homepage 'http://www.dayofthenewdan.com/projects/tlassemble/'
+  url 'https://github.com/dbridges/cocoa-tlassemble/archive/master.zip'
+  sha1 'afb9e809051de7022ee9e7b8649b2ef3abab70d7'
+  version '1.0'
+
+  def install
+    system 'make'
+    bin.install 'tlassemble'
+  end
+
+  test do
+    system "tlassemble --help | grep 'tlassemble 1.0'"
+  end
+end
