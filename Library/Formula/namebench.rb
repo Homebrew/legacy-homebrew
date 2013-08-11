34ce1c14f4c806ff42c839114c503a3c0ac61bb4
Nik Cubrilovic <nikcub@gmail.com>
2013-08-12 00:25:12 +1000

    formula for namebench

---
+++ Library/Formula/namebench.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Namebench < Formula
+  homepage 'https://code.google.com/p/namebench/'
+  url 'https://namebench.googlecode.com/files/namebench-1.3.1-source.tgz'
+  sha1 '2e6ca5a4f20512cb967c5ac43b023cc38c271131'
+
+  depends_on :python
+
+  def install
+    python do
+      system python, "setup.py", "install", "--prefix=#{prefix}", "--install-data=#{prefix}/bin"
+      
+    end
+  end
+
+end
