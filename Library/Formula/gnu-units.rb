ed86749da22ac84d0c71e6371f55abae59ceaae5
Brett Koonce <koonce@gmail.com>
2013-08-10 19:20:22 -0500

    (gnu) units 2.02
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gnu-units.rb
+++ Library/Formula/gnu-units.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GnuUnits < Formula
   homepage 'http://www.gnu.org/software/units/'
-  url 'http://ftpmirror.gnu.org/units/units-2.01.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/units/units-2.01.tar.gz'
-  sha1 '80e7f1a2e70769bfac93702924871843b85f12d4'
+  url 'http://ftpmirror.gnu.org/units/units-2.02.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/units/units-2.02.tar.gz'
+  sha1 'e460371dc97034d17ce452e6b64991f7fd2d1409'
 
   option 'default-names', "Do not prepend 'g' to the binary"
 
