e790ad67dbe31f4eb911bf899bc5eb0d60e68772
Matthew Vincent <matt@averails.com>
2013-08-11 12:02:55 -0700

    Updating GeoGit to 0.4.0

--- Library/Formula/geogit.rb
+++ Library/Formula/geogit.rb
@@ -2,13 +2,14 @@ require 'formula'
 
 class Geogit < Formula
   homepage 'http://www.geogit.org'
-  url 'http://downloads.sourceforge.net/project/geogit/geogit-0.3.0/geogit-cli-app-0.3.0.zip'
-  sha1 'c01976d4592da0184e5459c264c115577c084ae3'
+  url 'http://downloads.sourceforge.net/project/geogit/geogit-0.4.0/geogit-cli-app-0.4.0.zip'
+  sha1 '41d87374e96a765da5ddab928b4cef0756ad339c'
 
   def install
     bin.install "bin/geogit"
     bin.install "bin/geogit-console"
     prefix.install "repo"
-    man1.install Dir['man/*']
+    # The 0.4.0 is missing the man package, this may be fixed in a 0.4.1 or later release.
+    # man1.install Dir['man/*']
   end
 end
