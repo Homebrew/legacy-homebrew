305d28f9bf463fd6887b9277504063ac50e18d5a
Denis Denisov <denieldfu@gmail.com>
2013-08-11 14:07:02 +0300

    axel: add --HEAD

--- Library/Formula/axel.rb
+++ Library/Formula/axel.rb
@@ -4,6 +4,7 @@ class Axel < Formula
   homepage 'http://freshmeat.net/projects/axel/'
   url 'http://alioth.debian.org/frs/download.php/file/3016/axel-2.4.tar.bz2'
   sha1 '9e212e2890a678ccb2ab48f575a659a32d07b1a9'
+  head 'https://github.com/emiraga/axel.git', :branch => 'release1'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--debug=0", "--i18n=0"
