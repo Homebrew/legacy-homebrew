bfe19180b0d0a93c9e4760c6cadbd9ca17a2f91c
Misty De Meo <mistydemeo@gmail.com>
2013-08-11 23:06:24 -0700

    leiningen 2.3.0

--- Library/Formula/leiningen.rb
+++ Library/Formula/leiningen.rb
@@ -1,22 +1,22 @@
 require 'formula'
 
 class LeiningenJar < Formula
-  url 'https://leiningen.s3.amazonaws.com/downloads/leiningen-2.2.0-standalone.jar', :using => :nounzip
-  sha1 '694c01251f71954e9d1d7003b42dee1b3a393191'
+  url 'https://leiningen.s3.amazonaws.com/downloads/leiningen-2.3.0-standalone.jar', :using => :nounzip
+  sha1 '17d7347a8bee5ee34c6191ded0af0f8d6b348319'
 end
 
 class Leiningen < Formula
   homepage 'https://github.com/technomancy/leiningen'
-  url 'https://github.com/technomancy/leiningen/archive/2.2.0.tar.gz'
-  sha1 '0ca7e4ea68b490171d869bd5cc3912feba8d7ee9'
+  url 'https://github.com/technomancy/leiningen/archive/2.3.0.tar.gz'
+  sha1 'f19921fb05b5313a6ea654602e015ca9ed8ece9b'
 
   head 'https://github.com/technomancy/leiningen.git'
 
   def install
-    LeiningenJar.new.brew { libexec.install 'leiningen-2.2.0-standalone.jar' }
+    LeiningenJar.new.brew { libexec.install "leiningen-#{version}-standalone.jar" }
     # bin/lein autoinstalls and autoupdates, which doesn't work too well for us
     inreplace "bin/lein-pkg" do |s|
-      s.change_make_var! 'LEIN_JAR', libexec/'leiningen-2.2.0-standalone.jar'
+      s.change_make_var! 'LEIN_JAR', libexec/"leiningen-#{version}-standalone.jar"
     end
     bin.install "bin/lein-pkg" => 'lein'
     bash_completion.install 'bash_completion.bash' => 'lein-completion.bash'
