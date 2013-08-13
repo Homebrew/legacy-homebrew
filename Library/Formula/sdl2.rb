a3f49bcfdb7b065773e4b0c5e992b3809120d607
Jørgen P. Tjernø <jorgen@valvesoftware.com>
2013-08-13 12:25:17 -0700

    sdl2: Update to correct HEAD info.

--- Library/Formula/sdl2.rb
+++ Library/Formula/sdl2.rb
@@ -5,7 +5,7 @@ class Sdl2 < Formula
   url 'http://www.libsdl.org/release/SDL2-2.0.0.tar.gz'
   sha1 'a907eb5203abad6649c1eae0120d96c0a1931350'
 
-  head 'http://hg.libsdl.org/SDL', :branch => 'SDL-2.0', :using => :hg
+  head 'http://hg.libsdl.org/SDL', :using => :hg
 
   if build.head?
     depends_on :automake
