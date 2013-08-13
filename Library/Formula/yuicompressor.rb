09a83eeecd7cba13bc645f49533adc89a9efb7a9
Dennis Luxen <luxen@kit.edu>
2013-08-13 10:29:17 +0200

    yuicompressor 2.4.8
    Closes #21864.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yuicompressor.rb
+++ Library/Formula/yuicompressor.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Yuicompressor < Formula
   homepage 'http://yuilibrary.com/projects/yuicompressor'
-  url 'http://yui.zenfs.com/releases/yuicompressor/yuicompressor-2.4.7.zip'
-  sha1 '64d209cae769cee2b89217ba226543001c59d27b'
+  url 'https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.zip'
+  sha1 '178e265570c8161e8074c7ca19896abb05e76c1f'
 
   def install
-    libexec.install "build/yuicompressor-#{version}.jar"
+    libexec.install "yuicompressor-#{version}.jar"
     bin.write_jar_script libexec/"yuicompressor-#{version}.jar", "yuicompressor"
   end
 end
