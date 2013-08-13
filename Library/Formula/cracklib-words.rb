1757da90451f9a00bff87bd6f5ae4b7c1c161825
Jack Nagel <jacknagel@gmail.com>
2013-08-12 19:01:04 -0500

    cracklib-words: use regular download strategy

--- Library/Formula/cracklib-words.rb
+++ Library/Formula/cracklib-words.rb
@@ -2,14 +2,12 @@ require 'formula'
 
 class CracklibWords < Formula
   homepage 'http://cracklib.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/cracklib/cracklib-words/2008-05-07/cracklib-words-20080507.gz',
-    :using => :nounzip
+  url 'http://downloads.sourceforge.net/project/cracklib/cracklib-words/2008-05-07/cracklib-words-20080507.gz'
   sha1 'e0cea03e505e709b15b8b950d56cb493166607da'
 
   depends_on 'cracklib'
 
   def install
-    system "gzip", "-d", "cracklib-words-#{version}.gz"
     share.install "cracklib-words-#{version}" => "cracklib-words"
     system "/bin/sh", "-c", "#{HOMEBREW_PREFIX}/sbin/cracklib-packer < #{share}/cracklib-words"
   end
