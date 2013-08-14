8598f1213182ef1e322b0e275bb3629026bd0570
Mike McQuaid <mike@mikemcquaid.com>
2013-08-14 08:57:07 +0100

    jenkins 1.527

--- Library/Formula/jenkins.rb
+++ Library/Formula/jenkins.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jenkins < Formula
   homepage 'http://jenkins-ci.org'
-  url 'http://mirrors.jenkins-ci.org/war/1.526/jenkins.war'
-  sha1 '2ef7a07af52cdb0c9f1c53792e97b32c3d217931'
+  url 'http://mirrors.jenkins-ci.org/war/1.527/jenkins.war'
+  sha1 '9dde2e29dc76e8e3b94f18da1bd290f658303212'
 
   head 'https://github.com/jenkinsci/jenkins.git'
 
