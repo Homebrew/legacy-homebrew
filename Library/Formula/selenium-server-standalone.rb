b6438e7551e26e6131326080f39ff4d8c3e4f15c
Adam Vandenberg <flangy@gmail.com>
2013-08-13 20:45:45 -0700

    selenium-server-standalone 2.35.0
    Closes #21875.

--- Library/Formula/selenium-server-standalone.rb
+++ Library/Formula/selenium-server-standalone.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class SeleniumServerStandalone < Formula
   homepage 'http://seleniumhq.org/'
-  url 'http://selenium.googlecode.com/files/selenium-server-standalone-2.34.0.jar'
-  sha1 '27b6173d12081f479efe1da59d568255c34869f0'
+  url 'http://selenium.googlecode.com/files/selenium-server-standalone-2.35.0.jar'
+  sha1 'd27ff0f4ff5c06f63ad9113f448f37e6a00147d1'
 
   def install
     prefix.install "selenium-server-standalone-#{version}.jar"
@@ -16,26 +16,26 @@ class SeleniumServerStandalone < Formula
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
     <dict>
-            <key>Label</key>
-            <string>#{plist_name}</string>
-            <key>RunAtLoad</key>
-            <true/>
-            <key>KeepAlive</key>
-            <false/>
-            <key>ProgramArguments</key>
-            <array>
-                    <string>/usr/bin/java</string>
-                    <string>-jar</string>
-                    <string>#{prefix}/selenium-server-standalone-#{version}.jar</string>
-                    <string>-port</string>
-                    <string>4444</string>
-            </array>
-            <key>ServiceDescription</key>
-            <string>Selenium Server</string>
-            <key>StandardErrorPath</key>
-            <string>/var/log/selenium/selenium-error.log</string>
-            <key>StandardOutPath</key>
-            <string>/var/log/selenium/selenium-output.log</string>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <false/>
+      <key>ProgramArguments</key>
+      <array>
+        <string>/usr/bin/java</string>
+        <string>-jar</string>
+        <string>#{prefix}/selenium-server-standalone-#{version}.jar</string>
+        <string>-port</string>
+        <string>4444</string>
+      </array>
+      <key>ServiceDescription</key>
+      <string>Selenium Server</string>
+      <key>StandardErrorPath</key>
+      <string>/var/log/selenium/selenium-error.log</string>
+      <key>StandardOutPath</key>
+      <string>/var/log/selenium/selenium-output.log</string>
     </dict>
     </plist>
     EOS
