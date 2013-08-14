d497be425ab6367b43ed9093763f50ac463b90cd
Steffen Matthischke <steffen.matthischke@gmail.com>
2013-08-14 03:27:18 +0200

    unrar --devel 5.0.10
    Add devel block for unrar 5.0.10 (UNRAR 5.00 beta 8) which supports unraring
    of rar 5 archives.
    
    Closes #21882.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/unrar.rb
+++ Library/Formula/unrar.rb
@@ -5,8 +5,19 @@ class Unrar < Formula
   url 'http://www.rarlab.com/rar/unrarsrc-4.2.4.tar.gz'
   sha1 '1cc29603fb4e4df16a3aa9bfc7da1afaf0923259'
 
+  devel do
+    url 'http://www.rarlab.com/rar/unrarsrc-5.0.10.tar.gz'
+    sha1 'd48c245a58193c373fd2633f40829dcdda33b387'
+  end
+
   def install
-    system "make --makefile makefile.unix"
+
+    if build.stable?
+      system "make --makefile makefile.unix"
+    else
+      system "make"
+    end
+
     bin.install 'unrar'
     prefix.install 'license.txt' => 'COPYING'
     prefix.install 'readme.txt' => 'README'
