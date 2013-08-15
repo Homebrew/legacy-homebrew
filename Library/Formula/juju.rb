912c31201a1f4a9c3af720ac44a530580370cc46
Rodrigo Chacon <rochacon@gmail.com>
2013-08-15 01:30:32 -0300

    juju: add --devel
    Closes #22005.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -6,6 +6,11 @@ class Juju < Formula
   version "1.12.0-1"
   sha1 "b552919f5f4ed5a34885a2a6a8a4a0d7be485267"
 
+  devel do
+    url "https://launchpad.net/juju-core/trunk/1.13.1/+download/juju-core_1.13.1.tar.gz"
+    sha1 "1c1346378763d6aa469c434c1467b8ea4f8530be"
+  end
+
   depends_on 'go' => :build
 
   def install
