f79271fee0119c9dcbb74651de913c2c376d76fd
Adam Vandenberg <flangy@gmail.com>
2013-08-15 09:37:53 -0700

    getmail: simplify slightly

--- Library/Formula/getmail.rb
+++ Library/Formula/getmail.rb
@@ -7,9 +7,9 @@ class Getmail < Formula
   sha1 'c4ebc38d17f9a2ed2516e5070e300b0e160b0aaa'
 
   def install
-    scripts = %w( getmail getmail_fetch getmail_maildir getmail_mbox )
-    libexec.install scripts, 'getmailcore'
-    bin.install_symlink Dir["#{libexec}/*"] - ["#{libexec}/getmailcore"]
+    libexec.install %w( getmail getmail_fetch getmail_maildir getmail_mbox )
+    bin.install_symlink Dir["#{libexec}/*"]
+    libexec.install 'getmailcore'
     man1.install Dir['docs/*.1']
   end
 end
