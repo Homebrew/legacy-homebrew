4c943342d2dd89a2a06fb7b02f76dc25f2506b73
Keisuke Takahashi <keithseahus@gmail.com>
2013-08-16 23:46:45 +0900

    VoltDB 3.5

--- Library/Formula/voltdb.rb
+++ Library/Formula/voltdb.rb
@@ -3,20 +3,18 @@ require 'formula'
 class Voltdb < Formula
   homepage 'https://github.com/VoltDB/voltdb'
   head 'https://github.com/VoltDB/voltdb.git'
-  url 'https://github.com/VoltDB/voltdb/archive/voltdb-3.4.tar.gz'
-  sha1 '9a45ee70b99ea32c0c919b786ba528677db3d284'
+  url 'https://github.com/VoltDB/voltdb/archive/voltdb-3.5.tar.gz'
+  sha1 'fb765f087e2faf95b8187182a88d4058f8bfa5e2'
 
   def install
     system 'ant'
 
-    inreplace 'bin/voltdb', /VOLTDB_LIB=\$VOLTDB_HOME\/lib/, 'VOLTDB_LIB=$VOLTDB_HOME/lib/voltdb'
-
-    Dir['lib/*'].each do |f| (lib/'voltdb').install f end
-    ln_s lib/'voltdb/python', lib/'python'
+    Dir['lib/*'].each do |f| libexec.install f end
+    lib.install_symlink Dir["#{libexec}/*"]
     prefix.install 'bin', 'tools', 'voltdb', 'version.txt', 'doc'
   end
 
   test do
-    assert_equal "3.4\n", File.read("#{prefix}/version.txt")
+    assert_equal "3.5\n", File.read("#{prefix}/version.txt")
   end
 end
