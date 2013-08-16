97ca745b2674a7d27452ae91ae9d16e3fc92b1c4
Keisuke Takahashi <keithseahus@gmail.com>
2013-08-16 21:42:38 +0900

    Update the formula into voltdb-3.5

--- Library/Formula/voltdb.rb
+++ Library/Formula/voltdb.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Voltdb < Formula
   homepage 'https://github.com/VoltDB/voltdb'
   head 'https://github.com/VoltDB/voltdb.git'
-  url 'https://github.com/VoltDB/voltdb/archive/voltdb-3.4.tar.gz'
-  sha1 '9a45ee70b99ea32c0c919b786ba528677db3d284'
+  url 'https://github.com/VoltDB/voltdb/archive/voltdb-3.5.tar.gz'
+  sha1 'fb765f087e2faf95b8187182a88d4058f8bfa5e2'
 
   def install
     system 'ant'
@@ -17,6 +17,6 @@ class Voltdb < Formula
   end
 
   test do
-    assert_equal "3.4\n", File.read("#{prefix}/version.txt")
+    assert_equal "3.5\n", File.read("#{prefix}/version.txt")
   end
 end
