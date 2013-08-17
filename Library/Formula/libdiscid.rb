9a215b43e38c925c4bb4ce9e0bea72ddf5cd179e
Brett Koonce <koonce@gmail.com>
2013-08-17 18:49:59 -0500

    libdiscid 0.5.2

--- Library/Formula/libdiscid.rb
+++ Library/Formula/libdiscid.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libdiscid < Formula
   homepage 'http://musicbrainz.org/doc/libdiscid'
-  url 'http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-0.5.0.tar.gz'
-  sha1 'e046be72fca56bc1e5dce3568055331b58ee4dfc'
+  url 'http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-0.5.2.tar.gz'
+  sha1 '445f2e1dc9bdf7effffdcc913424958b7cef1ae7'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
