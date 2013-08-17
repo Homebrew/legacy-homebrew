02a102e4949636d26a375dc3a4a04b2cb1ec3081
Brett Koonce <koonce@gmail.com>
2013-08-17 18:49:25 -0500

    fwknop 2.5.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/fwknop.rb
+++ Library/Formula/fwknop.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fwknop < Formula
   homepage 'http://www.cipherdyne.org/fwknop/'
-  url 'http://www.cipherdyne.org/fwknop/download/fwknop-2.0.3.tar.gz'
-  sha1 '13f0e5d3762d2ebd09183581f0fb0f6329835671'
+  url 'http://www.cipherdyne.org/fwknop/download/fwknop-2.5.1.tar.bz2'
+  sha1 '65bed25e9d7f4a7ccc3f15cab35b95a6f7b21873'
 
   # needed for gpg support
   depends_on 'gpgme' => :optional
