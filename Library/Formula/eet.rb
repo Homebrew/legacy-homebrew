9e19d004677160a4685698e61043bca94af25e02
Brett Koonce <koonce@gmail.com>
2013-08-17 18:48:12 -0500

    eet 1.7.8
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/eet.rb
+++ Library/Formula/eet.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Eet < Formula
   homepage 'http://trac.enlightenment.org/e/wiki/Eet'
-  url 'http://download.enlightenment.org/releases/eet-1.7.7.tar.gz'
-  sha1 'd3f756321147b26c89b69ece8c6115ba0f68490e'
+  url 'http://download.enlightenment.org/releases/eet-1.7.8.tar.gz'
+  sha1 'd68e17fb155954c7d524cdba98244c09ae511513'
 
   head 'http://svn.enlightenment.org/svn/e/trunk/eet/'
 
