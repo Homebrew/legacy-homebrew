f80e1f457eead0c6db9ae5b7ee984ceffecc8243
Brett Koonce <koonce@gmail.com>
2013-08-17 18:50:55 -0500

    xpa 2.1.15
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xpa.rb
+++ Library/Formula/xpa.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Xpa < Formula
   homepage 'http://hea-www.harvard.edu/RD/xpa/'
-  url 'http://hea-www.harvard.edu/saord/download/xpa/xpa-2.1.14.tar.gz'
-  sha1 '927afc7beb90999cf63f75810d34a0ffead5f401'
+  url 'http://hea-www.harvard.edu/saord/download/xpa/xpa-2.1.15.tar.gz'
+  sha1 '52f5cc2925f5ee6e642602d4405a240b32ade455'
 
   depends_on :x11
 
