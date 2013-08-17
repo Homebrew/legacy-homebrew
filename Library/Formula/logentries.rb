c73261b93451f66f20e5d4501f12885bc7abd1bd
Brett Koonce <koonce@gmail.com>
2013-08-16 21:23:59 -0500

    logentries v1.2.14
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/logentries.rb
+++ Library/Formula/logentries.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Logentries < Formula
   homepage 'https://logentries.com/doc/agent/'
-  url 'https://github.com/logentries/le/archive/v1.2.12.tar.gz'
-  sha1 '7dc003bee3c3169fc88a34aa21cdc3e606e194df'
+  url 'https://github.com/logentries/le/archive/v1.2.14.tar.gz'
+  sha1 'd0b8073c7d5cae990b186bacf1bb2e2d39c544a5'
 
   def install
     bin.install 'le'
