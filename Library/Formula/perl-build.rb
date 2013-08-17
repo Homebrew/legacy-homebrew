368737587849eb4667cb84e49d5f560198aa4eda
Brett Koonce <koonce@gmail.com>
2013-08-16 21:24:45 -0500

    Perl-Build 1.04

--- Library/Formula/perl-build.rb
+++ Library/Formula/perl-build.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class PerlBuild < Formula
   homepage 'https://github.com/tokuhirom/Perl-Build'
-  url 'https://github.com/tokuhirom/Perl-Build/archive/1.03.tar.gz'
-  sha1 'dda816ad6445d82b4be144dc426d33853a8b5344'
+  url 'https://github.com/tokuhirom/Perl-Build/archive/1.04.tar.gz'
+  sha1 '1adad66a7d18f009e77674565ee57fb34194914a'
 
   head 'https://github.com/tokuhirom/perl-build.git'
 
