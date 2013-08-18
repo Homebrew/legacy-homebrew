0d499de38a9c40987fd506321379a06585fffac1
Kashif Rasul <kashif.rasul@gmail.com>
2013-08-18 12:47:00 +0200

    geos 3.4.1
    Closes #21978.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/geos.rb
+++ Library/Formula/geos.rb
@@ -2,20 +2,19 @@ require 'formula'
 
 class Geos < Formula
   homepage 'http://trac.osgeo.org/geos'
-  url 'http://download.osgeo.org/geos/geos-3.3.8.tar.bz2'
-  sha1 '1743e09f37eb75d85283a684a5765c4f44d035fa'
+  url 'http://download.osgeo.org/geos/geos-3.4.1.tar.bz2'
+  sha1 '0fee633694049192726149f83c47fef4d73c7468'
 
   option :universal
 
-  def install
-    ENV.universal_binary if build.universal?
-    # fixes compile error: missing symbols being optimized out using llvm.
-    if ENV.compiler == :llvm then
-      inreplace 'src/geom/Makefile.in', 'CFLAGS = @CFLAGS@', 'CFLAGS = @CFLAGS@ -O1'
-      inreplace 'src/geom/Makefile.in', 'CXXFLAGS = @CXXFLAGS@', 'CXXFLAGS = @CXXFLAGS@ -O1'
-    end
+  depends_on 'cmake' => :build
 
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+  def install
+    if build.universal?
+      ENV.universal_binary
+      ENV['CMAKE_OSX_ARCHITECTURES'] = Hardware::CPU.universal_archs.as_cmake_arch_flags
+    end
+    system "cmake", ".", *std_cmake_args
     system "make install"
   end
 end
