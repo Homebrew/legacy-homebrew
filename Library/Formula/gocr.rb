edb0e8c9074f7109682cd7be2b337de63ca07347
Matt Swain <m.swain@me.com>
2013-08-19 14:19:38 +0100

    gocr 0.50: Added option to build library
    Using option '--with-lib' will run 'make libs' which will ensure the library and headers are installed. Required a minor patch to the makefile.

--- Library/Formula/gocr.rb
+++ Library/Formula/gocr.rb
@@ -5,6 +5,14 @@ class Gocr < Formula
   url 'http://www-e.uni-magdeburg.de/jschulen/ocr/gocr-0.50.tar.gz'
   sha1 '2018ddf7be1c95dcc12f63f7ac40ad98da06f8a4'
 
+  option 'with-lib', 'Install library and headers'
+
+  depends_on 'netpbm' => :optional
+
+  def patches
+    "https://gist.github.com/mcs07/6229210/raw" if build.include? 'with-lib'
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
@@ -15,6 +23,12 @@ class Gocr < Formula
       s.change_make_var! 'mandir', '/share/man'
     end
 
+    system "make libs" if build.include? 'with-lib'
     system "make install"
+
+  end
+
+  test do
+    system "#{bin}/gocr -h"
   end
 end
