717055014ed0e9120517d3586838efb3df89cc29
tyrion-lannister <liedacil.ae@gmail.com>
2013-08-19 12:55:57 +0200

    libcppa: 0.7.1 - Disabling context switch option

--- Library/Formula/libcppa.rb
+++ Library/Formula/libcppa.rb
@@ -8,20 +8,16 @@ class Libcppa < Formula
   depends_on 'cmake' => :build
 
   option 'with-opencl', 'Build with OpenCL actors'
-  option 'disable-context-switching', 'Compile libcppa without context-switching actors even if Boost.Context is available'
 
   def install
     args = %W[
       --prefix=#{prefix}
       --build-static
+      --disable-context-switching
     ]
 
     if build.with? 'opencl'
       args << "--with-opencl"
-    end
-
-    if build.include? 'disable-context-switching'
-      args << "--disable-context-switching"
     end
 
     system "./configure", *args
