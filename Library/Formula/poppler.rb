753f323da50cebcde2875a4f2e7aa7587a94c27b
Ed Porras <digressed@icloud.com>
2013-08-20 17:31:25 +0200

    added build dependency on xz

--- Library/Formula/poppler.rb
+++ Library/Formula/poppler.rb
@@ -14,6 +14,7 @@ class Poppler < Formula
   option 'with-glib', 'Build Glib backend'
 
   depends_on 'pkg-config' => :build
+  depends_on 'xz' => :build
 
   depends_on :fontconfig
   depends_on 'openjpeg'
