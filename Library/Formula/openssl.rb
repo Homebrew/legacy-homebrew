34d27d027220345bf8a2e8d128b7f6e93378feb9
Jack Nagel <jacknagel@gmail.com>
2013-08-20 12:11:59 -0500

    openssl: avoid miscompilations under stdenv with clang and -O3

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -19,6 +19,11 @@ class Openssl < Formula
 
     if MacOS.prefer_64_bit?
       args << "darwin64-x86_64-cc" << "enable-ec_nistp_64_gcc_128"
+
+      # -O3 is used under stdenv, which results in test failures when using clang
+      inreplace 'Configure',
+        %{"darwin64-x86_64-cc","cc:-arch x86_64 -O3},
+        %{"darwin64-x86_64-cc","cc:-arch x86_64 -Os}
     else
       args << "darwin-i386-cc"
     end
