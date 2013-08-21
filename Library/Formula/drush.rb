c0e4b2211af7eeb45aa4721ccaa364e6b2c8835b
Albert Volkman <albert@27squared.com>
2013-08-20 20:28:34 -0400

    drush 6.0

--- Library/Formula/drush.rb
+++ Library/Formula/drush.rb
@@ -2,10 +2,9 @@ require 'formula'
 
 class Drush < Formula
   homepage 'http://drupal.org/project/drush'
-  url 'http://ftp.drupal.org/files/projects/drush-8.x-6.0-rc4.tar.gz'
-  sha1 '66a96f3540e9502b86385b1c62358b839410ac92'
-
-  head 'git://git.drupal.org/project/drush.git', :branch => '8.x-6.x'
+  head 'https://github.com/drush-ops/drush.git'
+  url 'https://github.com/drush-ops/drush/archive/6.0.0.tar.gz'
+  sha1 '6ff849b5d0807c4c4a39afeed28b9aa2dbc6de5e'
 
   def install
     libexec.install Dir['*']
