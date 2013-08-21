f287dcf1f6ceae285fb26af8ce818bf92261d0fa
Adam Vandenberg <flangy@gmail.com>
2013-08-20 21:32:29 -0700

    drush: fix sha1

--- Library/Formula/drush.rb
+++ Library/Formula/drush.rb
@@ -4,7 +4,7 @@ class Drush < Formula
   homepage 'http://drupal.org/project/drush'
   head 'https://github.com/drush-ops/drush.git'
   url 'https://github.com/drush-ops/drush/archive/6.0.0.tar.gz'
-  sha1 '6ff849b5d0807c4c4a39afeed28b9aa2dbc6de5e'
+  sha1 'de8b4065f179f6ddfd9c8911c2f818f81ab1f4a4'
 
   def install
     libexec.install Dir['*']
