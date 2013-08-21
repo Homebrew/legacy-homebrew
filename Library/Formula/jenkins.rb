c8a4d5ca76368a60439a20771272eeea37e321ee
Victor Baca <baca.victor@heb.com>
2013-08-21 14:44:52 -0500

    jenkins 1.528
    Closes #22054.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/jenkins.rb
+++ Library/Formula/jenkins.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jenkins < Formula
   homepage 'http://jenkins-ci.org'
-  url 'http://mirrors.jenkins-ci.org/war/1.527/jenkins.war'
-  sha1 '9dde2e29dc76e8e3b94f18da1bd290f658303212'
+  url 'http://mirrors.jenkins-ci.org/war/1.528/jenkins.war'
+  sha1 '2ba68851285c1462fcaa24b4c1b9d6efa7c921c5'
 
   head 'https://github.com/jenkinsci/jenkins.git'
 
