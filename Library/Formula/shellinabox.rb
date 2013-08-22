f3b438c1faac65e3b2cf88a540084bd7505fa90f
Guy Rozendorn <guy@rzn.co.il>
2013-08-22 19:29:36 +0300

    Update shellinabox.rb

--- Library/Formula/shellinabox.rb
+++ Library/Formula/shellinabox.rb
@@ -5,15 +5,12 @@ class Shellinabox < Formula
   url 'https://shellinabox.googlecode.com/files/shellinabox-2.14.tar.gz'
   sha1 '9e01f58c68cb53211b83d0f02e676e0d50deb781'
 
-
   def install
-
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    system "shellinaboxd --version"
+    system "#{bin}/shellinaboxd", "--version"
   end
 end
