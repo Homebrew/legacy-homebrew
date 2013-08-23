3d6d5b086747127bd7124d82cdfd80126537fcb4
Adrià Planas <adriaplanas@liquidcodeworks.com>
2013-08-23 18:39:05 +0200

    Update to 0.8

--- Library/Formula/mongo-c.rb
+++ Library/Formula/mongo-c.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MongoC < Formula
   homepage 'http://docs.mongodb.org/ecosystem/drivers/c/'
-  url 'https://s3.amazonaws.com/drivers.mongodb.org/c/mongo-c-driver-0.7.1.zip'
-  sha1 '4409af06513102c269e327c4a34f5121b244f61a'
+  url 'https://github.com/mongodb/mongo-c-driver/zipball/v0.8'
+  sha1 'af602a051af7ae4fad32e6d98fb62bd331cd5465'
 
   def install
     system "make"
