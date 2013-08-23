67840c89d7e3d655e811f48f24f6d930d2a2d9c1
Adrià Planas <adriaplanas@liquidcodeworks.com>
2013-08-23 20:14:20 +0200

    Fix to follow naming conventions

--- Library/Formula/mongo-c.rb
+++ Library/Formula/mongo-c.rb
@@ -5,6 +5,11 @@ class MongoC < Formula
   url 'https://github.com/mongodb/mongo-c-driver/archive/v0.8.zip'
   sha1 'af602a051af7ae4fad32e6d98fb62bd331cd5465'
 
+  def patches
+    # Fix to follow naming conventions
+    "https://gist.github.com/planas/6321873/raw"
+  end
+
   def install
     system "make"
     system "make install DESTDIR='' PREFIX=#{prefix}"
