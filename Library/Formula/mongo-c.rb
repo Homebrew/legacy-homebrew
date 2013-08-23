67d2c458595c6f6dbe1fd0ffaee253624c8a782a
Adrià Planas <adriaplanas@liquidcodeworks.com>
2013-08-23 19:02:43 +0200

    Better off use archive url

--- Library/Formula/mongo-c.rb
+++ Library/Formula/mongo-c.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class MongoC < Formula
   homepage 'http://docs.mongodb.org/ecosystem/drivers/c/'
-  url 'https://github.com/mongodb/mongo-c-driver/zipball/v0.8'
+  url 'https://github.com/mongodb/mongo-c-driver/archive/v0.8.zip'
   sha1 'af602a051af7ae4fad32e6d98fb62bd331cd5465'
 
   def install
