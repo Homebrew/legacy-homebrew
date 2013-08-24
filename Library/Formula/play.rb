d5dd9ac23d3196ac96b54f5086e45beeb547fffa
keith lloyd <forestofrain@gmail.com>
2013-08-24 14:25:51 -0600

    play: change HEAD repo
    Added check to not apply relative symlink hack to play file for 2.2.x(HEAD and devel)
    
    Closes #22111.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -2,10 +2,9 @@ require 'formula'
 
 class Play < Formula
   homepage 'http://www.playframework.org/'
+  head 'https://github.com/playframework/playframework.git'
   url 'http://downloads.typesafe.com/play/2.1.3/play-2.1.3.zip'
   sha1 'f399da960980edc929011c07ef64ee868eca8a9f'
-
-  head 'https://github.com/playframework/Play20.git'
 
   devel do
     url 'http://downloads.typesafe.com/play/2.2.0-M2/play-2.2.0-M2.zip'
@@ -14,12 +13,23 @@ class Play < Formula
   end
 
   def install
-    rm Dir['*.bat'] # remove windows' bat files
-    libexec.install Dir['*']
-    inreplace libexec+"play" do |s|
-      s.gsub! "$dir/", "$dir/../libexec/"
-      s.gsub! "dir=`dirname $PRG`", "dir=`dirname $0` && dir=$dir/`dirname $PRG`"
+    system "./framework/build", "publish-local" if build.head?
+
+    # remove Windows .bat files
+    rm Dir['*.bat']
+    rm Dir["#{buildpath}/**/*.bat"] if build.head?
+
+    # apply workaround for relative symlink, remove block when stable version reaches 2.2.x.
+    # https://github.com/playframework/playframework/issues/1516
+    # https://github.com/playframework/playframework/pull/777
+    if build.stable?
+      inreplace buildpath/"play" do |s|
+        s.gsub! "$dir/", "$dir/../libexec/"
+        s.gsub! "dir=`dirname $PRG`", "dir=`dirname $0` && dir=$dir/`dirname $PRG`"
+      end
     end
-    bin.install_symlink libexec+'play'
+
+    libexec.install Dir['*']
+    bin.install_symlink libexec/'play'
   end
 end
