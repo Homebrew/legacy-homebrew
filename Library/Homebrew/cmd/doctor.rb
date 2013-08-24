21b154daa382996e1258673d4a045edd55ef8ab0
Dan Hughes <dev.ldhughes@gmail.com>
2013-08-24 10:02:40 -0400

    Changes to work under Linux systems.
    Change gzip, mktemp and compression code to work under Linux systems.

--- Library/Homebrew/cmd/doctor.rb
+++ Library/Homebrew/cmd/doctor.rb
@@ -660,7 +660,7 @@ def check_for_multiple_volumes
   real_cellar = HOMEBREW_CELLAR.realpath
 
   tmp_prefix = ENV['HOMEBREW_TEMP'] || '/tmp'
-  tmp = Pathname.new `/usr/bin/mktemp -d #{tmp_prefix}/homebrew-brew-doctor-XXXX`.strip
+  tmp = Pathname.new with_system_path { `mktemp -d #{tmp_prefix}/homebrew-brew-doctor-XXXX` }.strip
   real_temp = tmp.realpath.parent
 
   where_cellar = volumes.which real_cellar
