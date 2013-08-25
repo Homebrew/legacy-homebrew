25957f8a650d038a15287b04d4beb05d00c317ae
Chen, Xiaoqiang <feuvan@gmail.com>
2013-08-25 12:19:16 +0800

    Revert ce39c3f..1281b6e
    This rolls back to commit ce39c3f37ce3bfea10e54aebd7df7c22a013ffcb.

--- Library/Formula/rdesktop.rb
+++ Library/Formula/rdesktop.rb
@@ -1,51 +1,15 @@
 require 'formula'
 
 class Rdesktop < Formula
-	homepage 'http://www.rdesktop.org/'
-	url 'http://downloads.sourceforge.net/project/rdesktop/rdesktop/1.8.0/rdesktop-1.8.0.tar.gz'
-	sha1 '2d39a41d29ad1ad2509d1e343a2817a3c7d666de'
+  homepage 'http://www.rdesktop.org/'
+  url 'http://downloads.sourceforge.net/project/rdesktop/rdesktop/1.7.1/rdesktop-1.7.1.tar.gz'
+  sha1 'c718d0f49948a964c7ef8424b8ade73ecce3aba3'
 
-	depends_on :x11
+  depends_on :x11
 
-	def install
-		args = ["--prefix=#{prefix}", "--disable-credssp", "--disable-smartcard", "--with-openssl=#{MacOS.sdk_path}/usr"]
-		args << "--x-includes=/opt/X11/include" if MacOS.version >= :mavericks
-		system "./configure", *args
-		system "make install"
-	end
-
-	def patches
-		DATA
-	end
+  def install
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
 end
-
-__END__
-diff --git a/tcp.c b/tcp.c
-index a541e45..af1ac45 100644
---- a/tcp.c
-+++ b/tcp.c
-@@ -318,7 +318,9 @@ tcp_tls_connect(void)
- 		}
- 
- 		options = 0;
-+#ifdef SSL_OP_NO_COMPRESSION
- 		options |= SSL_OP_NO_COMPRESSION;
-+#endif
- 		options |= SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS;
- 		SSL_CTX_set_options(g_ssl_ctx, options);
- 	}
-
-diff --git a/tcp.c b/tcp.c
-index af1ac45..943a655 100644
---- a/tcp.c
-+++ b/tcp.c
-@@ -193,7 +193,7 @@ tcp_recv(STREAM s, uint32 length)
- 	int rcvd = 0, ssl_err;
- 
- 	if (g_network_error == True)
--		return;
-+		return NULL;
- 
- 	if (s == NULL)
- 	{
-
