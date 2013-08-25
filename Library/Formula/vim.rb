0abc30170d5fc474fbe9a32bf6f1b026b4260fb0
David Larson <dplarson@ucsd.edu>
2013-08-25 01:12:05 -0700

    vim: switch back to tracking debian-unstable

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -2,11 +2,9 @@ require 'formula'
 
 class Vim < Formula
   homepage 'http://www.vim.org/'
-  # Get the base 7.4 tarball from Vim.org. But once patches start again, go
-  # back to tracking Debian unstable here:
-  # http://ftp.de.debian.org/debian/pool/main/v/vim/
-  url 'http://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2'
-  sha1 '601abf7cc2b5ab186f40d8790e542f86afca86b7'
+  # This package tracks debian-unstable: http://packages.debian.org/unstable/vim
+  url 'http://ftp.de.debian.org/debian/pool/main/v/vim/vim_7.4.000.orig.tar.gz'
+  sha1 '7cdb5eb06fe134b3a2fe7914959e1d571c76c2db'
 
   head 'https://vim.googlecode.com/hg/'
 
