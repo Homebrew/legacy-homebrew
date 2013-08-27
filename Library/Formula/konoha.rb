4e2dbef4336679663d3b3b3d6a8c4290daded22f
Samuel John <github@SamuelJohn.de>
2013-08-27 15:17:54 +0200

    konoha: --HEAD and adding mecab dep for 10.8+
    because 10.7 is the last SDK that has mecab.h.

--- Library/Formula/konoha.rb
+++ Library/Formula/konoha.rb
@@ -5,6 +5,8 @@ class Konoha < Formula
   url 'https://github.com/konoha-project/konoha3/archive/v0.1.tar.gz'
   sha1 '85ee3527c9a2db2874280e506c09aab8809a6ad5'
 
+  head 'https://github.com/konoha-project/konoha3.git'
+
   option 'tests', 'Verify the build with make test (1 min)'
 
   depends_on 'cmake' => :build
@@ -12,15 +14,11 @@ class Konoha < Formula
   depends_on 'pcre'
   depends_on 'json-c'
   depends_on 'sqlite'
+  depends_on 'mecab' if MacOS.version >= :mountain_lion
   depends_on :python # for python glue code
 
   def install
     args = std_cmake_args + ['..']
-    # mecab is no longer in the 10.8 SDK but superenv does not (yet) support
-    # switching to an older SDK. And if we add the include path so that
-    # `mecab.h` is found, we get more errors in mecab_glue.c:
-    #     error: no member named 'id' in 'struct mecab_node_t'
-    args << "-DHAVE_LIBMECAB=OFF" if MacOS.version >= :mountain_lion
     mkdir 'build' do
       system 'cmake', *args
       system 'make'
