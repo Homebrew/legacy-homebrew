49d554e33e56644bba6f0a92a8766d32bd1e6af0
Gvozden Neskovic <neskovic@gmail.com>
2013-08-27 14:21:14 +0200

    mpir: 2.6 - multiprecision integer (bignum) library
    New formula. MPIR is an open source multiprecision integer (bignum)
    library forked from the GMP (GNU Multi Precision) project. It consists
    of much code from past GMP releases, in combination with much original
    contributed code.

---
+++ Library/Formula/mpir.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Mpir < Formula
+  homepage 'http://www.mpir.org/'
+
+  url 'http://www.mpir.org/mpir-2.6.0.tar.bz2'
+  sha1 '28a91eb4d2315a9a73dc39771acf2b99838b9d72'
+
+  def install
+    ENV.deparallelize
+
+    system "./configure", "--prefix=#{prefix}"
+
+    system "make"
+    system "make", "install"
+  end
+
+  fails_with :clang do
+    build 425
+    cause <<-EOS.undent
+      Configure message:
+      checking compiler cc -O2 -fomit-frame-pointer ... no, gcc-4.3.2 on 64bit is bad,
+      try -O1 or -fno-strict-aliasing for the flags.
+    EOS
+  end
+end
