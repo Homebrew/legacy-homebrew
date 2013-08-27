f11ac53369d4ade66e7afd402354a14482a532ee
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-08-27 02:19:47 +0200

    Update SBT to 0.13.0

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Sbt < Formula
   homepage 'http://www.scala-sbt.org'
-  url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.12.4/sbt-launch.jar'
-  version '0.12.4'
-  sha1 '701af98879a5c2d89c089d69e96e5d1c3bcfafaa'
+  url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.0/sbt-launch.jar'
+  version '0.13.0'
+  sha1 'b78a29b8db7ae0fc90a7890a84a4617975e1d450'
 
   devel do
     url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.0-RC5/sbt-launch.jar'
