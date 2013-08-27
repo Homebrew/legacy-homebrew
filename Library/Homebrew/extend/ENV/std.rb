9977ff8c604b2e2924be06ca3ba4ca6cd67a9d33
Jack Nagel <jacknagel@gmail.com>
2013-08-26 22:11:29 -0500

    Simplify condition

--- Library/Homebrew/extend/ENV/std.rb
+++ Library/Homebrew/extend/ENV/std.rb
@@ -71,8 +71,7 @@ module Stdenv
     # Add lib and include etc. from the current macosxsdk to compiler flags:
     macosxsdk MacOS.version
 
-    # For Xcode 4.3 (*without* the "Command Line Tools for Xcode") compiler and tools inside of Xcode:
-    if not MacOS::CLT.installed? and MacOS::Xcode.installed? and MacOS::Xcode.version >= "4.3"
+    if MacOS::Xcode.without_clt?
       # Some tools (clang, etc.) are in the xctoolchain dir of Xcode
       append_path 'PATH', "#{MacOS.xctoolchain_path}/usr/bin" if MacOS.xctoolchain_path
       # Others are now at /Applications/Xcode.app/Contents/Developer/usr/bin
