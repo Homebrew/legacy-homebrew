b0ef2dbbf8395ba9a7bacca0b434e922f1964df3
Fred Potter <fpotter@fb.com>
2013-08-27 17:08:28 -0700

    xctool 0.1.9
    Closes #22160.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xctool.rb
+++ Library/Formula/xctool.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Xctool < Formula
   homepage 'https://github.com/facebook/xctool'
-  url 'https://github.com/facebook/xctool/archive/v0.1.7.tar.gz'
-  sha1 'd835f1a34d7d11278664162d4361a0bb03941e5d'
+  url 'https://github.com/facebook/xctool/archive/v0.1.9.tar.gz'
+  sha1 'b82539ff47329feb38ab9e5980e5ecdd4501d144'
   head 'https://github.com/facebook/xctool.git'
 
   depends_on :xcode
