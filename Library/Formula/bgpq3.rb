6fcbb8d938e6217d6858d33ee69c169e788800a2
Job Snijders <job@instituut.net>
2013-08-28 19:03:07 +0200

    bgpq3 0.1.19 + small Makefile.in patch (has been reported upstream)

---
+++ Library/Formula/bgpq3.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Bgpq3 < Formula
+  homepage 'http://snar.spb.ru/prog/bgpq3/'
+  url 'http://snar.spb.ru/prog/bgpq3/bgpq3-0.1.19.tgz'
+  sha1 '41a2afaeffb12e43048ca8771c6cc6e6392e0da5'
+  
+  def patches
+    "https://gist.github.com/job/6368404/raw"
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/bgpq3", "AS-ANY"
+  end
+end
