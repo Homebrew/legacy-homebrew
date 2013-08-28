fa1b5947664be81caaad72958c5ce602ee87c0c4
Job Snijders <job@instituut.net>
2013-08-28 20:45:51 +0200

    fixed paths for mandir in similar patch for bin dir

--- Library/Formula/bgpq3.rb
+++ Library/Formula/bgpq3.rb
@@ -6,7 +6,7 @@ class Bgpq3 < Formula
   sha1 '41a2afaeffb12e43048ca8771c6cc6e6392e0da5'
 
   def patches
-    # Upstream has been informed of this patch through email  
+    # Upstream has been informed of this patch through email
     DATA
   end
 
@@ -20,17 +20,21 @@ class Bgpq3 < Formula
   end
 end
 
-
 __END__
 diff --git a/Makefile.in b/Makefile.in
-index c2d7e96..0c44bae 100644
+index c2d7e96..afec780 100644
 --- a/Makefile.in
 +++ b/Makefile.in
-@@ -29,6 +29,7 @@ clean:
+@@ -29,9 +29,10 @@ clean:
  	rm -rf *.o *.core core.* core
  
  install: bgpq3
-+	if test ! -d @prefix@/bin; then mkdir -p @prefix@/bin; fi
++	if test ! -d @prefix@/bin ; then mkdir -p @prefix@/bin ; fi
  	${INSTALL} -c -s -m 755 bgpq3 @bindir@
- 	if test ! -d @prefix@/man/man8 ; then mkdir -p @prefix@/man/man8 ; fi
- 	${INSTALL} -m 644 bgpq3.8 @prefix@/man/man8
+-	if test ! -d @prefix@/man/man8 ; then mkdir -p @prefix@/man/man8 ; fi
+-	${INSTALL} -m 644 bgpq3.8 @prefix@/man/man8
++	if test ! -d @mandir@/man/man8 ; then mkdir -p @mandir@/man/man8 ; fi
++	${INSTALL} -m 644 bgpq3.8 @mandir@/man/man8
+ 
+ depend: 
+ 	makedepend -- $(CFLAGS) -- $(SRCS)
