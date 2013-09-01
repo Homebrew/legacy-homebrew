bba9e5519a5fe1b02d73cf5d86ed3ce05a9cc8a1
Erik Michaels-Ober <sferik@gmail.com>
2013-09-01 13:32:25 +0100

    ruby-build 20130901
    Closes #22246.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyBuild < Formula
   homepage 'https://github.com/sstephenson/ruby-build'
-  url 'https://github.com/sstephenson/ruby-build/archive/v20130806.tar.gz'
-  sha1 'cd038bfb6ccdf7f3e8c284450a29326eacb3f1c6'
+  url 'https://github.com/sstephenson/ruby-build/archive/v20130901.tar.gz'
+  sha1 '0ade3bd4435b83d61c022c1b7cea2517032a30e2'
 
   head 'https://github.com/sstephenson/ruby-build.git'
 
