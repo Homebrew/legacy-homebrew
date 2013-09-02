eb38091917ef1f7de2979d18b2ab3643b5193721
Markus Cisler <m@kuchen.io>
2013-09-02 23:47:26 +0200

    fb-client 1.3.0

--- Library/Formula/fb-client.rb
+++ Library/Formula/fb-client.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class FbClient < Formula
   homepage 'https://paste.xinu.at'
-  url 'https://paste.xinu.at/data/client/fb-1.1.4.tar.gz'
+  url 'https://paste.xinu.at/data/client/fb-1.3.0.tar.gz'
   sha1 '03483b5cdda9d27121941ddd10ffd20967f3f63b'
 
   conflicts_with 'findbugs',
