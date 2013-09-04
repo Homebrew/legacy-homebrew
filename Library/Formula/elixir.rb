ac2b419471e9fb1c07b430900072fb090352323d
Jack Nagel <jacknagel@gmail.com>
2013-09-04 18:34:12 -0500

    elixir: erlang R16 is in core now

--- Library/Formula/elixir.rb
+++ Library/Formula/elixir.rb
@@ -1,29 +1,5 @@
 require 'formula'
 
-class ErlangInstalled < Requirement
-  fatal true
-
-  satisfy {
-    erl = which('erl') and begin
-      `#{erl} -noshell -eval 'io:fwrite("~s~n", [erlang:system_info(otp_release)]).' -s erlang halt | grep -q '^R1[6789]'`
-      $?.exitstatus == 0
-    end
-  }
-
-  def message; <<-EOS.undent
-    Erlang R16 is required to install.
-
-    You can install this with:
-      brew tap homebrew/versions
-      brew unlink erlang
-      brew install erlang-r16
-
-    Or you can use an official installer from:
-      http://www.erlang.org/
-    EOS
-  end
-end
-
 class Elixir < Formula
   homepage 'http://elixir-lang.org/'
   url  'https://github.com/elixir-lang/elixir/archive/v0.10.2.tar.gz'
@@ -31,9 +7,7 @@ class Elixir < Formula
 
   head 'https://github.com/elixir-lang/elixir.git'
 
-  depends_on ErlangInstalled
-
-  env :userpaths
+  depends_on 'erlang'
 
   def install
     system "make"
