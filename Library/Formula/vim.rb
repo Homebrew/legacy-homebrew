817ef43786aabbc816eb3b7a3989a6563fa41e2b
cmar <cmar@customink.com>
2013-09-05 15:23:38 -0400

    vim 7.4.027
    Closes #22344.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -2,11 +2,8 @@ require 'formula'
 
 class Vim < Formula
   homepage 'http://www.vim.org/'
-  # Get the base 7.4 tarball from Vim.org. But once patches start again, go
-  # back to tracking Debian unstable here:
-  # http://ftp.de.debian.org/debian/pool/main/v/vim/
-  url 'http://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2'
-  sha1 '601abf7cc2b5ab186f40d8790e542f86afca86b7'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-4-027'
+  version '7.4.027'
 
   head 'https://vim.googlecode.com/hg/'
 
