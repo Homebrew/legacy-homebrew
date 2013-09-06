624ef12af9ee34a04e01503f4a4c00be06b24b71
Serguey Parkhomovsky <xindigo@gmail.com>
2013-09-06 09:49:42 -0700

    chicken 4.8.0.4
    Closes #22358.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/chicken.rb
+++ Library/Formula/chicken.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Chicken < Formula
   homepage 'http://www.call-cc.org/'
-  url 'http://code.call-cc.org/releases/4.8.0/chicken-4.8.0.3.tar.gz'
-  sha1 '90ce759d3b8a2cb53b2409c1e90277d380069440'
+  url 'http://code.call-cc.org/releases/4.8.0/chicken-4.8.0.4.tar.gz'
+  sha1 '35fe59da04041a7b98d018e5ebb223d491ae57c4'
 
   head 'git://code.call-cc.org/chicken-core'
 
