99fae495dfde8c499833895cd1b1878aef174eab
Mike McQuaid <mike@mikemcquaid.com>
2013-09-07 12:17:39 +0100

    nexus: replace patch with inreplace.

--- Library/Formula/nexus.rb
+++ Library/Formula/nexus.rb
@@ -6,13 +6,12 @@ class Nexus < Formula
   version '2.6.2-01'
   sha1 '046c4b9ed27e4d05eec1be80b676f4ee25ba352a'
 
-  # Put the sonatype-work directory in the var directory, to persist across version updates
-  def patches
-    DATA
-  end
-
   def install
     rm_f Dir['bin/*.bat']
+    # Put the sonatype-work directory in the var directory, to persist across version updates
+    inreplace "nexus-#{version}/conf/nexus.properties",
+      'nexus-work=${bundleBasedir}/../sonatype-work/nexus',
+      "nexus-work=#{var}/nexus"
     libexec.install Dir["nexus-#{version}/*"]
     bin.install_symlink libexec/'bin/nexus'
   end
@@ -47,16 +46,3 @@ class Nexus < Formula
     EOS
   end
 end
-
-__END__
-diff --git a/nexus-2.6.2-01/conf/nexus.properties b/nexus-2.6.2-01/conf/nexus.properties
-index df89251..23b536b 100644
---- a/nexus-2.6.2-01/conf/nexus.properties
-+++ b/nexus-2.6.2-01/conf/nexus.properties
-@@ -22,5 +22,5 @@ nexus-webapp=${bundleBasedir}/nexus
- nexus-webapp-context-path=/nexus
-
- # Nexus section
--nexus-work=${bundleBasedir}/../sonatype-work/nexus
-+nexus-work=HOMEBREW_PREFIX/var/nexus
- runtime=${bundleBasedir}/nexus/WEB-INF
