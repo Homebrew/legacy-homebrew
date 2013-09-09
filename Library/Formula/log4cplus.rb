37d2a3b4daf21f3662614bed570f1be861af98c1
Bozo Dragojevic <bozzo.dragojevic@gmail.com>
2013-09-09 12:35:29 +0200

    log4cplus: 1.0.4.3 - compiles with new toolchain
    version 1.0.4 doesn't compile for me. switching to 10.4.3 helps

--- Library/Formula/log4cplus.rb
+++ Library/Formula/log4cplus.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Log4cplus < Formula
-  url 'http://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.0.4/log4cplus-1.0.4.tar.bz2'
+  url 'http://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.0.4/log4cplus-1.0.4.3.tar.bz2'
   homepage 'http://log4cplus.sourceforge.net/'
-  sha1 'b8ca1b01b23788ac04f25a7bdaaaca7e366c7312'
+  sha1 '917d244f7f3d58a5fff35e3eef7fff9c74e9409b'
 
   def install
     system "./configure", "--disable-dependency-tracking",
