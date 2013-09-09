10f3948b02dd4b3bda6b3fb3bff3e4d0f5564393
Brett Koonce <koonce@gmail.com>
2013-09-09 16:16:38 -0500

    appengine-java-sdk 1.8.4
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/app-engine-java-sdk.rb
+++ Library/Formula/app-engine-java-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AppEngineJavaSdk < Formula
   homepage 'https://developers.google.com/appengine/docs/java/overview'
-  url 'http://googleappengine.googlecode.com/files/appengine-java-sdk-1.8.3.zip'
-  sha1 '3fa3fcb10cf4dd7602393d2f25ab5b905026b2a5'
+  url 'http://googleappengine.googlecode.com/files/appengine-java-sdk-1.8.4.zip'
+  sha1 'ca7077793a1c7c4d078110416635c095eecde3f9'
 
   def install
     rm Dir['bin/*.cmd']
