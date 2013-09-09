b29cc17cd0d95d95d515bf88cc265d95fd0c1a48
Brett Koonce <koonce@gmail.com>
2013-09-09 16:16:00 -0500

    go-appengine (amd64) 1.8.4
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/go-app-engine-64.rb
+++ Library/Formula/go-app-engine-64.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GoAppEngine64 < Formula
   homepage 'http://code.google.com/appengine/docs/go/overview.html'
-  url 'http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_amd64-1.8.3.zip'
-  sha1 'cbb3e63aeb17f0ed5edac28d8f447d5ecbfcbd9c'
+  url 'http://googleappengine.googlecode.com/files/go_appengine_sdk_darwin_amd64-1.8.4.zip'
+  sha1 'c69ce3e86364fa3f775fffe7cdac9e97e6f9ca53'
 
   def install
     cd '..'
