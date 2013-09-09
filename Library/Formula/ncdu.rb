ccf411184f858f8bced7454841feb14eac6fe4eb
Tim Carey-Smith <tim@spork.in>
2013-09-10 11:36:30 +1200

    ncdu 1.10
    Add HEAD version of ncdu
    
    Closes #22425.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ncdu.rb
+++ Library/Formula/ncdu.rb
@@ -2,10 +2,15 @@ require 'formula'
 
 class Ncdu < Formula
   homepage 'http://dev.yorhel.nl/ncdu'
-  url 'http://dev.yorhel.nl/download/ncdu-1.9.tar.gz'
-  sha1 '27cb5464b192db8ffdf0a894fe51d29985348eb0'
+  url 'http://dev.yorhel.nl/download/ncdu-1.10.tar.gz'
+  sha1 'cf3b5fbb5b69cbae5425bfff2660ac3d8224a605'
+  head 'git://g.blicky.net/ncdu.git'
+
+  depends_on :automake if build.head?
+  depends_on :autoconf if build.head?
 
   def install
+    system "autoreconf", "-i" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
