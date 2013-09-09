908b17a80953e0afd8fc64ff1e98000d1df87e84
Darin Morrison <darinmorrison+git@gmail.com>
2013-09-09 06:09:06 -0600

    cabal-install: update to 1.18.0.1, sandbox build.
    Use sandboxing support rather than ~/.ghc for build.
    
    Closes #22416.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cabal-install.rb
+++ Library/Formula/cabal-install.rb
@@ -2,17 +2,24 @@ require 'formula'
 
 class CabalInstall < Formula
   homepage 'http://www.haskell.org/haskellwiki/Cabal-Install'
-  url 'http://hackage.haskell.org/packages/archive/cabal-install/1.16.0.2/cabal-install-1.16.0.2.tar.gz'
-  sha1 'd85cba298302b9c426fedd6d8e4c892343ca70ea'
+  url 'http://hackage.haskell.org/packages/archive/cabal-install/1.18.0.1/cabal-install-1.18.0.1.tar.gz'
+  sha1 'ac403d580bd399d682e5d8f4fd8d6d07c03622d9'
 
   depends_on 'ghc'
 
   conflicts_with 'haskell-platform'
 
   def install
-    ENV['PREFIX'] = "#{prefix}"
-    ENV.delete('VERBOSE')
+    # use a temporary package database instead of ~/.cabal or ~/.ghc
+    pkg_db = "#{Dir.pwd}/package.conf.d"
+    system 'ghc-pkg', 'init', pkg_db
+    ENV['EXTRA_CONFIGURE_OPTS'] = "--package-db=#{pkg_db}"
+    ENV['PREFIX'] = Dir.pwd
+    inreplace 'bootstrap.sh', 'list --global',
+      'list --global --no-user-package-db'
+
     system 'sh', 'bootstrap.sh'
+    bin.install "bin/cabal"
     bash_completion.install 'bash-completion/cabal'
   end
 end
