ecff27cc2c5dcb0cfdfa673cb581428a5b38c33d
Alex Regueiro <alex@noldorin.com>
2013-09-11 04:03:43 +0100

    pdfmarks 0.1 (new formula)

---
+++ Library/Formula/pdfmarks.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Pdfmarks < Formula
+  homepage 'https://bitbucket.org/alexreg/pdfmarks'
+  url 'https://bitbucket.org/alexreg/pdfmarks/get/0.1.tar.gz'
+  sha1 '8897430451ae0d46e5a507710f9c3f42af5c8d90'
+
+  def install
+    inreplace 'pdfmarks' do |s|
+      s.gsub! "source \"common\"", "source \"#{libexec}/common\""
+      s.gsub! "\"pdfmarks.ps\"", "\"#{share}/pdfmarks.ps\""
+    end
+
+    share.install('pdfmarks.ps')
+    libexec.install('common')
+    bin.install('pdfmarks')
+  end
+
+  test do
+    system "#{bin}/pdfmarks", "-h"
+  end
+end
