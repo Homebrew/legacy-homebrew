351287df73ee4e256a8503267d67f34b70683a65
Misty De Meo <mistydemeo@gmail.com>
2013-09-10 23:08:17 -0700

    Move CompilerSelector logic into build env setup
    This moves the CompilerSelector fails_with logic into the build
    environment setup, making the compiler selection available before
    performing actions that depends on knowing what the compiler is, e.g.
    setting up PATH.
    
    ENV.setup_build_environment now optionally takes a Formula argument
    to provide the information necessary to do the fails_with, and the new
    ENV.validate_cc! extracts the fails_with logic from Build.install.

--- Library/Homebrew/extend/ENV/super.rb
+++ Library/Homebrew/extend/ENV/super.rb
@@ -54,16 +54,18 @@ module Superenv
     delete('CLICOLOR_FORCE') # autotools doesn't like this
   end
 
-  def setup_build_environment
+  def setup_build_environment(formula=nil)
     reset
+
     self.cc  = 'cc'
     self.cxx = 'c++'
+    self['HOMEBREW_CC'] = determine_cc
+    validate_cc!(formula) unless formula.nil?
     self['DEVELOPER_DIR'] = determine_developer_dir
     self['MAKEFLAGS'] ||= "-j#{determine_make_jobs}"
     self['PATH'] = determine_path
     self['PKG_CONFIG_PATH'] = determine_pkg_config_path
     self['PKG_CONFIG_LIBDIR'] = determine_pkg_config_libdir
-    self['HOMEBREW_CC'] = determine_cc
     self['HOMEBREW_CCCFG'] = determine_cccfg
     self['HOMEBREW_BREW_FILE'] = HOMEBREW_BREW_FILE
     self['HOMEBREW_SDKROOT'] = "#{MacOS.sdk_path}" if MacOS::Xcode.without_clt?
@@ -99,20 +101,7 @@ module Superenv
     # s - apply fix for sed's Unicode support
     # a - apply fix for apr-1-config path
 
-    # Homebrew's apple-gcc42 will be outside the PATH in superenv,
-    # so xcrun may not be able to find it
-    if self['HOMEBREW_CC'] == 'gcc-4.2'
-      apple_gcc42 = begin
-        Formulary.factory('apple-gcc42')
-      rescue Exception # in --debug, catch bare exceptions too
-        nil
-      end
-      append_path('PATH', apple_gcc42.opt_prefix/'bin') if apple_gcc42
-    end
-
-    if ENV['HOMEBREW_CC'] =~ GNU_GCC_REGEXP
-      warn_about_non_apple_gcc($1)
-    end
+    warn_about_non_apple_gcc($1) if ENV['HOMEBREW_CC'] =~ GNU_GCC_REGEXP
   end
 
   def universal_binary
@@ -141,6 +130,24 @@ module Superenv
     paths += deps.map{|dep| "#{HOMEBREW_PREFIX}/opt/#{dep}/bin" }
     paths << MacOS::X11.bin if x11?
     paths += %w{/usr/bin /bin /usr/sbin /sbin}
+
+    # Homebrew's apple-gcc42 will be outside the PATH in superenv,
+    # so xcrun may not be able to find it
+    if self['HOMEBREW_CC'] == 'gcc-4.2'
+      apple_gcc42 = begin
+        Formulary.factory('apple-gcc42')
+      rescue Exception # in --debug, catch bare exceptions too
+        nil
+      end
+      paths << apple_gcc42.opt_prefix/'bin' if apple_gcc42
+    end
+
+    if self['HOMEBREW_CC'] =~ GNU_GCC_REGEXP
+      gcc_name = 'gcc' + $1.delete('.')
+      gcc = Formulary.factory(gcc_name)
+      paths << gcc.opt_prefix/'bin'
+    end
+
     paths.to_path_s
   end
 
