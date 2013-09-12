camillol/homebrew

rabbitmq-c: use resource

Library/Formula/rabbitmq-c.rb
@@ -1,10 +1,5 @@
 require 'formula'
 
-class RabbitmqCodegen < Formula
-  url 'https://github.com/rabbitmq/rabbitmq-codegen/archive/rabbitmq_v3_1_0.tar.gz'
-  sha1 '5b82a3887586804a712aa2c5eeff4a59e89537b7'
-end
-
 class RabbitmqC < Formula
   homepage 'https://github.com/alanxz/rabbitmq-c'
   url 'https://github.com/alanxz/rabbitmq-c/archive/rabbitmq-c-v0.3.0.zip'
@@ -20,14 +15,16 @@ class RabbitmqC < Formula
   depends_on 'rabbitmq'
   depends_on 'simplejson' => :python if MacOS.version <= :leopard
 
+  resource 'codegen' do
+    url 'https://github.com/rabbitmq/rabbitmq-codegen/archive/rabbitmq_v3_1_0.tar.gz'
+    sha1 '5b82a3887586804a712aa2c5eeff4a59e89537b7'
+  end
+
   def install
     ENV.universal_binary if build.universal?
-
-    RabbitmqCodegen.new.brew { (buildpath/"codegen").install Dir["*"] }
-
+    (buildpath/'codegen').install resource('codegen')
     system "autoreconf", "-i"
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
-
 end

Homebrew/legacy-homebrew

Library/Formula/rabbitmq-c.rb
@@ -1,10 +1,5 @@
 require 'formula'
 
-class RabbitmqCodegen < Formula
-  url 'https://github.com/rabbitmq/rabbitmq-codegen/archive/rabbitmq_v3_1_5.tar.gz'
-  sha1 '08b0415364e517e033e83c0b033820b62713a0bf'
-end
-
 class RabbitmqC < Formula
   homepage 'https://github.com/alanxz/rabbitmq-c'
   url 'https://github.com/alanxz/rabbitmq-c/archive/v0.4.0.tar.gz'
@@ -21,14 +16,16 @@ class RabbitmqC < Formula
   depends_on 'rabbitmq'
   depends_on 'simplejson' => :python if MacOS.version <= :leopard
 
+  resource 'codegen' do
+    url 'https://github.com/rabbitmq/rabbitmq-codegen/archive/rabbitmq_v3_1_5.tar.gz'
+    sha1 '08b0415364e517e033e83c0b033820b62713a0bf'
+  end
+
   def install
     ENV.universal_binary if build.universal?
-
-    RabbitmqCodegen.new.brew { (buildpath/"codegen").install Dir["*"] }
-
+    (buildpath/'codegen').install resource('codegen')
     system "autoreconf", "-i"
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
-
 end