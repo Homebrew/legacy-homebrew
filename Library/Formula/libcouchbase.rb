c272c52e0fead6f75bbce60cbe69b87ed7a3c9d5
Sergey Avseyev <sergey.avseyev@gmail.com>
2013-09-12 08:39:24 -0700

    libcouchbase: allow universal builds

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -5,6 +5,7 @@ class Libcouchbase < Formula
   url 'http://packages.couchbase.com/clients/c/libcouchbase-2.1.3.tar.gz'
   sha1 '460e6b8744b3d5634c5284ff4c690c207c61f9a6'
 
+  option :universal
   option 'with-libev-plugin', 'Build libev IO plugin (will pull libev dependency)'
   option 'without-libevent-plugin', 'Do not build libevent plugin (will remove libevent dependency)'
 
@@ -12,12 +13,19 @@ class Libcouchbase < Formula
   depends_on 'libevent' unless build.include?('without-libevent-plugin')
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--disable-examples",
-                          "--disable-tests", # don't download google-test framework
-                          "--disable-couchbasemock"
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}",
+      "--disable-examples",
+      "--disable-tests", # don't download google-test framework
+      "--disable-couchbasemock"
+    ]
+    if build.universal?
+      args.push("--enable-fat-binary")
+      ENV.universal_binary
+    end
+    system "./configure", *args
     system "make install"
   end
 
