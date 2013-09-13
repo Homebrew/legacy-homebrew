de18648916fe47462c4c95c6f64f103a4ac42be6
Jeff Hann <jeff.hann@teamspace.ca>
2013-09-13 09:23:31 -0300

    correcting URL and SHA1 key to reflect a tag release. Correcting line spacing and indentation.

--- Library/Formula/cownet.rb
+++ Library/Formula/cownet.rb
@@ -2,19 +2,15 @@ require 'formula'
 
 class Cownet < Formula
   homepage 'http://obihann.github.io/Cownet/'
-  url 'https://github.com/obihann/Cownet.git'
+  url 'https://github.com/obihann/Cownet/archive/0.2.0-beta.tar.gz'
   version '0.2.0'
-  sha1 '63beadee3d28baaba71c689454a051eb94fcdff6'
+  sha1 '16a7b8e563d086a1b5078ce15f7f8688e2a59572'
+
   depends_on :python
   depends_on "figlet"
   depends_on "cowsay"
 
   def install
-      system python, "setup.py", "install", "--home=#{prefix}"
-  end
-
-  test do
-      system 'false'
+    system python, "setup.py", "install", "--home=#{prefix}"
   end
 end
-
