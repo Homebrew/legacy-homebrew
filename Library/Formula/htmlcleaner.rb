943f5a4fdae2af2220f6f6c92fda15ae4f27b0aa
Jens Erat <email@jenserat.de>
2013-09-14 00:38:00 +0200

    htmlcleaner: 2.6.1 - HTML to well-formed XML
    Fix package based on comments.

--- Library/Formula/htmlcleaner.rb
+++ Library/Formula/htmlcleaner.rb
@@ -2,15 +2,11 @@ require 'formula'
 
 class Htmlcleaner < Formula
   homepage 'http://htmlcleaner.sourceforge.net/index.php'
-  url 'http://downloads.sourceforge.net/projects/htmlcleaner/files/htmlcleaner/htmlcleaner%20v2.6.1/htmlcleaner-2.6.1.zip'
-  version '2.6.1'
+  url 'http://downloads.sourceforge.net/project/htmlcleaner/htmlcleaner/htmlcleaner%20v2.6.1/htmlcleaner-2.6.1.zip'
   sha1 '4c5d2049c7048dc6632a17ec15e21ab0fcf510b9'
 
   def install
-    prefix.install "htmlcleaner-2.6.1.jar"
-    (bin+"htmlcleaner").write <<-EOS.undent
-      #!/bin/sh
-      java -jar "#{prefix}/htmlcleaner-2.6.1.jar" "$@"
-    EOS
+    libexec.install "htmlcleaner-#{version}.jar"
+    bin.write_jar_script libexec/"htmlcleaner-#{version}.jar", "htmlcleaner"
   end
 end
