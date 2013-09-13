b612481bdced2198ad9a6513a9b145d9c26d1d4a
Jeff Hann <jeff.hann@teamspace.ca>
2013-09-13 12:10:32 -0300

    fixing incorrect version number

--- Library/Formula/archey.rb
+++ Library/Formula/archey.rb
@@ -4,7 +4,7 @@ class Archey < Formula
   homepage 'http://obihann.github.io/archey-osx/'
   url 'https://github.com/obihann/archey-osx/archive/1.1.tar.gz'
   sha1 '52da445c38c08415caa8ba93d28bbfe7b4ac1003'
-  version "1.0"
+  version "1.1"
 
   def install
     system "mkdir #{bin}"
