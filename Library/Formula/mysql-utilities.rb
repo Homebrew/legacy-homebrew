b09d50f09a93f361b2d89faec9be436105c1e967
lathropd <daniel.lathrop@gmail.com>
2013-09-13 15:00:05 -0500

    mysql-utilities formula code clean-up suggested by adamv

--- Library/Formula/mysql-utilities.rb
+++ Library/Formula/mysql-utilities.rb
@@ -8,39 +8,18 @@ require 'formula'
 class MysqlUtilities < Formula
   homepage 'http://dev.mysql.com/doc/workbench/en/mysql-utilities.html'
   url 'http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-utilities-1.3.5.tar.gz/from/http://cdn.mysql.com/'
+  version '1.3.5'
   sha1 '1b0fe9eb6cb4f96388f87188abe4b0b7be7efd99'
 
-  version '1.3.5'
   depends_on 'sphinx'
   depends_on :python => ['distutils','sphinx','jinja2']
   depends_on :python => ['mysql.connector' => 'mysql-connector']
+
   def install
     system python, "setup.py", "install"
   end
 
   test do
     system "mysqlauditadmin --help"
-    system "mysqlauditgrep --help"
-    system "mysqldbcompare --help"
-    system "mysqldbcopy --help"
-    system "mysqldbexport --help"
-    system "mysqldbimport --help"
-    system "mysqldiff --help"
-    system "mysqldiskusage --help"
-    system "mysqlfailover --help"
-    system "mysqlfrm --help"
-    system "mysqlindexcheck --help"
-    system "mysqlmetagrep --help"
-    system "mysqlprocgrep --help"
-    system "mysqlreplicate --help"
-    system "mysqlrpladmin --help"
-    system "mysqlrplcheck --help"
-    system "mysqlrplshow --help"
-    system "mysqlserverclone --help"
-    system "mysqlserverinfo --help"
-    system "mysqluc --help"
-    system "mysqluserclone --help"
-
-
   end
 end
