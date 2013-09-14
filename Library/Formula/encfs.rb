d7a57f87d4eb306c14d6737824c7243646d86d7d
Lifepillar <github@lifepillar.com>
2013-09-14 12:48:52 +0300

    Add an option to encfs to build with external OSXFUSE.
    This is meant as a temporary workaround as long as OSXFUSE
    does not fix their build system so that Homebrew can have
    an osxfuse formula. See also:
    
        https://github.com/mxcl/homebrew/issues/17274

--- Library/Formula/encfs.rb
+++ Library/Formula/encfs.rb
@@ -1,5 +1,19 @@
 require 'formula'
 
+class OSXFuseRequirement < Requirement
+  fatal true
+  satisfy { Pathname('/usr/local/include/osxfuse').exist? }
+
+  def message; <<-EOS.undent
+    To build this formula with --without-fuse4x, you must first
+    install OSXFUSE. Since there is no formula for OSXFUSE yet,
+    you should download the OSXFUSE package here:
+
+      http://osxfuse.github.io
+    EOS
+  end
+end
+
 class Encfs < Formula
   homepage 'http://www.arg0.net/encfs'
   url 'http://encfs.googlecode.com/files/encfs-1.7.4.tgz'
@@ -9,10 +23,18 @@ class Encfs < Formula
   depends_on 'gettext'
   depends_on 'boost'
   depends_on 'rlog'
-  depends_on 'fuse4x'
+  option 'without-fuse4x', 'Build without fuse4x support (requires OSXFUSE)'
+  depends_on 'fuse4x' => :recommended
+  depends_on OSXFuseRequirement if build.without? 'fuse4x'
 
   def install
-    inreplace "configure", "-lfuse", "-lfuse4x"
+    if build.without? 'fuse4x'
+      ENV['CPPFLAGS'] = '-I/usr/local/include/osxfuse'
+      libfuse = 'osxfuse'
+    else
+      libfuse = 'fuse4x'
+    end
+    inreplace "configure", "-lfuse", "-l#{libfuse}"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-boost=#{HOMEBREW_PREFIX}"
@@ -21,7 +43,8 @@ class Encfs < Formula
   end
 
   def caveats; <<-EOS.undent
-    Make sure to follow the directions given by `brew info fuse4x-kext`
+    Unless you build using --without-fuse4x, make sure
+    to follow the directions given by `brew info fuse4x-kext`
     before trying to use a FUSE-based filesystem.
     EOS
   end
