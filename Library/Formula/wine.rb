94da3d75acdc1b0ff82bbdd1f09de1132c7ba032
Link0306 <hungryjosh@yahoo.com>
2013-09-15 15:09:18 -0400

    Update wine.rb
    Updates to a newer version of the patch that fixes things in a simpler way (http://bugs.winehq.org/show_bug.cgi?id=34188#c7).
    
    Also makes this patch only run in the stable version of wine as it has already been implemented in the devel version.

--- Library/Formula/wine.rb
+++ Library/Formula/wine.rb
@@ -60,9 +60,11 @@ class Wine < Formula
     cause 'error: invalid operand for instruction lretw'
   end
 
-  def patches
-    # http://bugs.winehq.org/show_bug.cgi?id=34188
-    ['http://bugs.winehq.org/attachment.cgi?id=45477']
+  if build.stable?
+  	def patches
+      # http://bugs.winehq.org/show_bug.cgi?id=34188
+      ['http://bugs.winehq.org/attachment.cgi?id=45507']
+  	end
   end
 
   # the following libraries are currently not specified as dependencies, or not built as 32-bit:
