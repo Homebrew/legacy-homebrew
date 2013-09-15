db71c1e94d8c985d886ff25735722ffd6c9dbefb
Adam Vandenberg <flangy@gmail.com>
2013-09-15 11:52:33 -0700

    ctags: inline patch
    Old URL was 404, and patch is small enough to inline.
    
    Closes #22559.

--- Library/Formula/ctags.rb
+++ Library/Formula/ctags.rb
@@ -2,16 +2,15 @@ require 'formula'
 
 class Ctags < Formula
   homepage 'http://ctags.sourceforge.net/'
+  head 'https://ctags.svn.sourceforge.net/svnroot/ctags/trunk'
   url 'http://downloads.sourceforge.net/ctags/ctags-5.8.tar.gz'
   sha1 '482da1ecd182ab39bbdc09f2f02c9fba8cd20030'
 
-  head 'https://ctags.svn.sourceforge.net/svnroot/ctags/trunk'
-
   depends_on :autoconf if build.head?
 
+  # fixes http://sourceforge.net/tracker/?func=detail&aid=3247256&group_id=6556&atid=106556
   def patches
-    # fixes http://sourceforge.net/tracker/?func=detail&aid=3247256&group_id=6556&atid=106556
-    { :p2 => "https://raw.github.com/gist/4010022/8d0697dc87a40e65011e2192439609c17578c5be/ctags.patch" }
+    { :p2 => DATA }
   end
 
   def install
@@ -40,3 +39,41 @@ class Ctags < Formula
     EOS
   end
 end
+
+__END__
+diff -ur a/ctags-5.8/read.c b/ctags-5.8/read.c
+--- a/ctags-5.8/read.c	2009-07-04 17:29:02.000000000 +1200
++++ b/ctags-5.8/read.c	2012-11-04 16:19:27.000000000 +1300
+@@ -18,7 +18,6 @@
+ #include <string.h>
+ #include <ctype.h>
+ 
+-#define FILE_WRITE
+ #include "read.h"
+ #include "debug.h"
+ #include "entry.h"
+diff -ur a/ctags-5.8/read.h b/ctags-5.8/read.h
+--- a/ctags-5.8/read.h	2008-04-30 13:45:57.000000000 +1200
++++ b/ctags-5.8/read.h	2012-11-04 16:19:18.000000000 +1300
+@@ -11,12 +11,6 @@
+ #ifndef _READ_H
+ #define _READ_H
+ 
+-#if defined(FILE_WRITE) || defined(VAXC)
+-# define CONST_FILE
+-#else
+-# define CONST_FILE const
+-#endif
+-
+ /*
+ *   INCLUDE FILES
+ */
+@@ -95,7 +89,7 @@
+ /*
+ *   GLOBAL VARIABLES
+ */
+-extern CONST_FILE inputFile File;
++extern inputFile File;
+ 
+ /*
+ *   FUNCTION PROTOTYPES
