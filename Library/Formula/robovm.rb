55619ce146d6bfc6cfe494382a79e284e6c37c87
Sam Pullara <spullara@yahoo.com>
2013-09-15 11:00:10 -0700

    remove some extraneous comments and dependencies

--- Library/Formula/robovm.rb
+++ Library/Formula/robovm.rb
@@ -1,16 +1,10 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Robovm < Formula
   homepage 'http://www.robovm.org'
   url 'http://download.robovm.org/robovm-0.0.4.tar.gz'
   version '0.0.4'
   sha1 'cd64444948b8ec48dccce6e1077be5c25c34ba04'
-
-  # depends_on 'cmake' => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
     prefix.install_metafiles
