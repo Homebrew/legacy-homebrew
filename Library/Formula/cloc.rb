ffa947081d1511924de4a79b19ba3e8c93babcf7
Brett Koonce <koonce@gmail.com>
2013-09-15 18:10:48 -0500

    cloc 1.60
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/cloc.rb
+++ Library/Formula/cloc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Cloc < Formula
   homepage 'http://cloc.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/cloc/cloc/v1.58/cloc-1.58.pl'
-  sha1 'bf2b4478343da50d0eea83ad0ed3f126ff210d39'
+  url 'http://downloads.sourceforge.net/project/cloc/cloc/v1.60/cloc-1.60.pl'
+  sha1 'd002e85b7deb988e4f88eaf3f9697cf011ec0c8a'
 
   def install
     bin.install "cloc-#{version}.pl" => "cloc"
