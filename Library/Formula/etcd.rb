91b364ee938f1ae33bebb846023237e1db7a37d6
Stian Eikeland <stian@eikeland.se>
2013-09-16 13:06:27 +0200

    Etcd 0.1.1 - added
    Etcd is a high-available key/value shared configuration store.

---
+++ Library/Formula/etcd.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Etcd < Formula
+  homepage 'http://github.com/coreos/etcd'
+  url 'https://github.com/coreos/etcd/archive/v0.1.1.tar.gz'
+  sha1 '60f39379b7f916a7e3e87fdefc1104cd2330c1ae'
+  head 'https://github.com/coreos/etcd.git'
+
+  depends_on 'go' => :build
+
+  def install
+    ENV['GOPATH'] = buildpath
+    system "./build"
+    bin.install 'etcd'
+  end
+end
