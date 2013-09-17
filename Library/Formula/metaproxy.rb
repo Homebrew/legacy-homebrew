0de0003405f6c8a33f414f7ad2a3a220e3c26691
Brett Koonce <koonce@gmail.com>
2013-09-17 12:11:44 -0500

    metaproxy 1.3.62
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/metaproxy.rb
+++ Library/Formula/metaproxy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Metaproxy < Formula
   homepage 'http://www.indexdata.com/metaproxy'
-  url 'http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.3.60.tar.gz'
-  sha1 '30e72d9d9b09b8c924a260c35896f17b61ee5acc'
+  url 'http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.3.62.tar.gz'
+  sha1 'ceeb395d95a1d2ae2a266cc7d65be4e0569a8d9b'
 
   depends_on 'pkg-config' => :build
   depends_on 'yazpp'
