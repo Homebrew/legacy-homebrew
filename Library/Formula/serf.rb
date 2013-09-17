0a780bc3fc978876be2192450f5eaf7bd2540d71
Clemens Gruber <clemensgru@gmail.com>
2013-09-17 23:01:41 +0200

    serf 1.3.1
    Also add an important dylib patch to only use the major version as install name
    
    Closes #22636.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/serf.rb
+++ Library/Formula/serf.rb
@@ -2,22 +2,37 @@ require 'formula'
 
 class Serf < Formula
   homepage 'http://code.google.com/p/serf/'
-  url 'http://serf.googlecode.com/files/serf-1.2.1.tar.bz2'
-  sha1 'f65fbbd72926c8e7cf0dbd4ada03b0d226f461fd'
+  url 'http://serf.googlecode.com/files/serf-1.3.1.tar.bz2'
+  sha1 'b8c8e12e7163d7bacf9be0ea4aaa7b8c32e8c72c'
 
   option :universal
 
   depends_on :libtool
   depends_on 'sqlite'
+  depends_on 'scons' => :build
 
-  def apr_bin
-    Superenv.bin or "/usr/bin"
+  # Patch to use only the major version of the dylib
+  # Details: https://code.google.com/p/serf/source/detail?r=2161
+  def patches
+    { :p0 => DATA }
   end
 
   def install
     ENV.universal_binary if build.universal?
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-apr=#{apr_bin}"
-    system "make install"
+    system "scons", "PREFIX=#{prefix}"
+    system "scons install"
   end
 end
+
+__END__
+--- SConstruct~ 2013-08-15 11:13:20.000000000 +0200
++++ SConstruct 2013-09-17 22:52:50.000000000 +0200
+@@ -401,7 +401,7 @@
+   # to a path in the sandbox. The shared library install name (id) should be the
+   # final targat path.
+   install_shared_path = install_shared[0].abspath
+-  target_install_shared_path = os.path.join(libdir, lib_shared[0].name)
++  target_install_shared_path = os.path.join(libdir, '%s.dylib' % LIBNAME)
+   env.AddPostAction(install_shared, ('install_name_tool -id %s %s'
+                                      % (target_install_shared_path,
+                                         install_shared_path)))
