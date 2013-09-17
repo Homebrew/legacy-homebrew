79ed86ded92a0c9a941747e3b5d0bfb33cc1b91d
Mike Prentice <mprentice@gmail.com>
2013-09-16 22:04:01 -0400

    cdlabelgen - fix build and test commands
    Remove patch, instead set prefix in make install command.
    
    Create man1 directory before running make install.
    
    Use full path to binary in test, cdlabelgen -> #{bin}/cdlabelgen

--- Library/Formula/cdlabelgen.rb
+++ Library/Formula/cdlabelgen.rb
@@ -8,33 +8,12 @@ class Cdlabelgen < Formula
   depends_on 'jpeg2ps' => :recommended
 
   def install
-    system "make", "install"
-  end
-
-  def patches
-    # fix Makefile to install to /usr/local instead of /usr
-    DATA
+    system "mkdir", "-p", man1
+    system "make", "install", "BASE_DIR=#{prefix}"
   end
 
   test do
-    system "cdlabelgen -c TestTitle --output-file testout.eps"
+    system "#{bin}/cdlabelgen -c TestTitle --output-file testout.eps"
     File.file?("testout.eps")
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index c7a3977..63c72b4 100644
---- a/Makefile
-+++ b/Makefile
-@@ -10,8 +10,8 @@ ZIPVERSION = 430
- # Change these to locations you need, also
- # remember to edit cdlabelgen and its @where_is_the_template as needed.
-
--BASE_DIR   = /usr
--# BASE_DIR   = /usr/local
-+# BASE_DIR   = /usr
-+BASE_DIR   = /usr/local
- # BASE_DIR   = /opt
- BIN_DIR   = $(BASE_DIR)/bin
- LIB_DIR   = $(BASE_DIR)/share/cdlabelgen
