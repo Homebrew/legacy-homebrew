cb337975dd7a50dc234256bfb41bb12ac1a1d0f4
Mike Prentice <mprentice@gmail.com>
2013-09-16 22:50:55 -0400

    jpeg2ps - fix build and test commands
    Remove patch, instead set variables in make install command:
    BINDIR=#{bin}, MANDIR=#{man}
    
    Create needed directories first before running make install.
    
    Change test to try to find local .jpg file in /Library.
    
    Fix test call from jpeg2ps to #{bin}/jpeg2ps.

--- Library/Formula/jpeg2ps.rb
+++ Library/Formula/jpeg2ps.rb
@@ -5,33 +5,16 @@ class Jpeg2ps < Formula
   url 'http://www.pdflib.com/fileadmin/pdflib/products/more/jpeg2ps/jpeg2ps-1.9.tar.gz'
   sha1 '2fc2701c7c00ba17b051ebcc7b9c8059eb6614f9'
 
-  def patches
-    # fix Makefile to install to share/man instead of man
-    DATA
-  end
-
   def install
-    system "make", "install"
+    system "mkdir", "-p", bin
+    system "mkdir", "-p", man
+    system "make", "install", "BINDIR=#{bin}", "MANDIR=#{man}"
   end
 
   test do
-    system "wget https://upload.wikimedia.org/wikipedia/en/b/bd/Test.jpg"
-    system "jpeg2ps Test.jpg > Test.eps"
-    File.file?("Test.eps")
+    # not terribly robust
+    testfile = `find /Library -name '*.jpg' | head -n 1`
+    testfile.strip!
+    system "#{bin}/jpeg2ps", testfile
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 1586b82..817a173 100644
---- a/Makefile
-+++ b/Makefile
-@@ -45,7 +45,7 @@ DOSDISTFILES = $(DISTFILES) jpeg2ps.exe
- BINDIR = /usr/local/bin
- 
- # Location where to install the manual page.
--MANDIR = /usr/local/man/man1
-+MANDIR = /usr/local/share/man/man1
- 
- CONVFILES = \
-        jpeg2ps.c psimage.h readjpeg.c asc85ec.c getopt.c       \
