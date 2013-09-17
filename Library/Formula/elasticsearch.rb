36b7f4a19859351a012fb7ea8782174719b40a5d
Bruno Miranda <bruno@bopia.com>
2013-09-17 12:48:12 -0700

    elasticsearch 0.90.5
    Closes #22632.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/elasticsearch.rb
+++ Library/Formula/elasticsearch.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Elasticsearch < Formula
   homepage 'http://www.elasticsearch.org'
-  url 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.tar.gz'
-  sha1 '24843192bee3afd19f5a958800e896153dbc3569'
+  url 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.tar.gz'
+  sha1 '8027a4ae1bef6876c7651b1590607c8ff6108820'
 
   head 'https://github.com/elasticsearch/elasticsearch.git'
 
