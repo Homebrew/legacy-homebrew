e5a69433f66686364783d81a782db66789c37556
Alex Regueiro <alex@noldorin.com>
2013-09-17 16:45:21 +0100

    cclive: fixed small syntax error

--- Library/Formula/cclive.rb
+++ Library/Formula/cclive.rb
@@ -20,6 +20,7 @@ class Cclive < Formula
     depends_on 'libtool'
     depends_on 'glibmm' => :build if build.head?
     depends_on 'asciidoc' => :build if build.head?
+  end
   depends_on 'boost149'
   depends_on 'quvi'
   depends_on 'pcre'
