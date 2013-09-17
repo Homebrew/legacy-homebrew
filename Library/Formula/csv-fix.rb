2f6a2b06cac3721c4b00b5b420f0bb717c25d799
Matthew Caruana Galizia <m@m.cg>
2013-09-17 15:11:21 -0600

    Fix sha1

--- Library/Formula/csv-fix.rb
+++ Library/Formula/csv-fix.rb
@@ -4,7 +4,7 @@ class CsvFix < Formula
   homepage 'http://code.google.com/p/csvfix/'
   url 'https://csvfix.googlecode.com/archive/version-1.3.zip'
   version '1.3'
-  sha1 '8a0303e5c43062cf348e992c52058bd125cb16a8'
+  sha1 '76f959c29944fa915a37a3d8cf478149ebd83036'
 
   def install
     system "make lin"
