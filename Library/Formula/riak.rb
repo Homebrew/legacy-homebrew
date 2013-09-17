e5a9c8c4a5f7df030d1127818203bbbd909b1a3b
Brett Koonce <koonce@gmail.com>
2013-09-17 12:12:20 -0500

    riak 1.4.2
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/riak.rb
+++ Library/Formula/riak.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Riak < Formula
   homepage 'http://wiki.basho.com/Riak.html'
-  url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.1/osx/10.8/riak-1.4.1-OSX-x86_64.tar.gz'
-  version '1.4.1'
-  sha256 'ebe68fb9fa2ee87636d2e8bb38d3a8e805c61edbd329fdf648e68933aae79668'
+  url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.2/osx/10.8/riak-1.4.2-OSX-x86_64.tar.gz'
+  version '1.4.2'
+  sha256 '2accc58a0ea2f7bd3edc31c934edb0bff6a1535994607fd6cec9c6bbefcf2abf'
 
   depends_on :macos => :mountain_lion
   depends_on :arch => :x86_64
