fd02c75f091dfd57c0195f560008c7792ba8e923
Pierre Riteau <priteau@ci.uchicago.edu>
2013-09-17 21:06:31 -0500

    Fix homepage URL (404)

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Denominator < ScriptFileFormula
   # This is a shell script wrapping a java jar; download as single file using nounzip
-  homepage 'https://github.com/Netflix/denominator/tree/master/denominator-cli'
+  homepage 'https://github.com/Netflix/denominator/tree/master/cli'
   url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/4.1.0/denominator?direct',
     :using  => :nounzip
   version '4.1.0'
