148a746ddd177edc08e4ce31708cecca91f2fcf9
Mike McQuaid <mike@mikemcquaid.com>
2013-09-18 12:36:43 +0100

    Bottles: use options hash for install_bottle?

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -30,8 +30,9 @@ class FormulaInstaller
     check_install_sanity
   end
 
-  def pour_bottle? warn=false
-    tab.used_options.empty? && options.empty? && install_bottle?(f, warn)
+  def pour_bottle? install_bottle_options={:warn=>false}
+    tab.used_options.empty? && options.empty? && \
+      install_bottle?(f, install_bottle_options)
   end
 
   def check_install_sanity
@@ -167,7 +168,7 @@ class FormulaInstaller
     @poured_bottle = false
 
     begin
-      if pour_bottle? true
+      if pour_bottle? :warn => true
         pour
         @poured_bottle = true
         tab = Tab.for_keg f.prefix
