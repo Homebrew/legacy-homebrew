fab833ad4f368eee9672d437169f09cdacfae8b1
Alec Thomas <alec@swapoff.org>
2013-09-19 18:04:57 -0400

    OnDir 0.2.3
    Closes #22693.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/ondir.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Ondir < Formula
+  homepage 'http://swapoff.org/ondir.html'
+  url 'http://swapoff.org/files/ondir/ondir-0.2.3.tar.gz'
+  sha1 '372962799612d925c1edd6eaca774dc971438bbe'
+
+  def install
+    system "make"
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+end
