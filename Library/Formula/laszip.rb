e7f622b5207237fe51a72c1c15cc86af6d4326e5
Adam Vandenberg <flangy@gmail.com>
2013-09-19 07:56:53 -0700

    laszip 2.2.0

--- Library/Formula/laszip.rb
+++ Library/Formula/laszip.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Laszip < Formula
   homepage 'http://www.laszip.org/'
-  url 'https://github.com/LASzip/LASzip/archive/2.0.2.tar.gz'
-  sha1 '42c27cf3beff50f53e51d4a1b921e3b9f3b0faf7'
+  url 'https://github.com/LASzip/LASzip/archive/v2.2.0.tar.gz'
+  sha1 '911881d7698642f6c201a70cab62a55a337b5627'
   head 'https://github.com/LASzip/LASzip.git'
 
   depends_on 'cmake' => :build
