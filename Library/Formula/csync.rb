ece93e33a888a9e6115f626a57390eb7de68b6c5
Jack Nagel <jacknagel@gmail.com>
2013-09-20 16:22:30 -0500

    csync: use spec deps

--- Library/Formula/csync.rb
+++ Library/Formula/csync.rb
@@ -2,9 +2,21 @@ require 'formula'
 
 class Csync < Formula
   homepage 'http://www.csync.org/'
-  head 'git://git.csync.org/projects/csync.git'
-  url 'http://www.csync.org/files/csync-0.49.9.tar.gz'
-  sha1 'fd7df6c13aa6fc6de74cb48c2ac35ad11f6d895d'
+
+  stable do
+    url 'http://www.csync.org/files/csync-0.49.9.tar.gz'
+    sha1 'fd7df6c13aa6fc6de74cb48c2ac35ad11f6d895d'
+
+    depends_on 'log4c'
+    depends_on 'samba'
+  end
+
+  head do
+    url 'git://git.csync.org/projects/csync.git'
+    # Log4c and libsmbclient are optional in HEAD.
+    depends_on 'log4c' => :optional
+    depends_on 'samba' => :optional
+  end
 
   depends_on 'check' => :build
   depends_on 'cmake' => :build
@@ -13,15 +25,6 @@ class Csync < Formula
   depends_on 'iniparser'
   depends_on 'sqlite'
   depends_on 'libssh' => :optional
-
-  if build.head?
-    # Log4c and libsmbclient are optional in HEAD.
-    depends_on 'log4c' => :optional
-    depends_on 'samba' => :optional
-  else
-    depends_on 'log4c'
-    depends_on 'samba'
-  end
 
   depends_on :macos => :lion
 
