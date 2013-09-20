e2e8f2711afea08db5e19c95a5da076b161cc0f3
Jack Nagel <jacknagel@gmail.com>
2013-09-20 10:15:51 -0500

    blitz: use spec deps

--- Library/Formula/blitz.rb
+++ Library/Formula/blitz.rb
@@ -5,9 +5,9 @@ class Blitz < Formula
   url 'http://downloads.sourceforge.net/project/blitz/blitz/Blitz++%200.10/blitz-0.10.tar.gz'
   sha1 '7e157ec22ed2d261e896b7de4e8e8d3bf7d780e2'
 
-  head 'http://blitz.hg.sourceforge.net:8000/hgroot/blitz/blitz', :using => :hg
+  head do
+    url 'http://blitz.hg.sourceforge.net:8000/hgroot/blitz/blitz', :using => :hg
 
-  if build.head?
     depends_on :automake
     depends_on :libtool
   end
