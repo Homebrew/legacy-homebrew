9b9803764204de1b02dd42ac086e332ec7920983
Jack Nagel <jacknagel@gmail.com>
2013-09-20 10:15:52 -0500

    eet: use spec deps

--- Library/Formula/eet.rb
+++ Library/Formula/eet.rb
@@ -5,9 +5,9 @@ class Eet < Formula
   url 'http://download.enlightenment.org/releases/eet-1.7.8.tar.gz'
   sha1 'd68e17fb155954c7d524cdba98244c09ae511513'
 
-  head 'http://svn.enlightenment.org/svn/e/trunk/eet/'
+  head do
+    url 'http://svn.enlightenment.org/svn/e/trunk/eet/'
 
-  if build.head?
     depends_on :automake
     depends_on :libtool
   end
