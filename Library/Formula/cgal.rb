6ce524a9ac89960a591db7f5d950977242649c7a
Arkadi Tolkun <destman@gmail.com>
2013-09-20 09:55:48 +0300

    cgal: compile with c++11 (--with-c++11)
    Also added patch to resolve problems with Constrained_triangulation_2.

--- Library/Formula/cgal.rb
+++ Library/Formula/cgal.rb
@@ -8,6 +8,7 @@ class Cgal < Formula
   option 'imaging', "Build ImageIO and QT compoments of CGAL"
   option 'with-eigen3', "Build with Eigen3 support"
   option 'with-lapack', "Build with LAPACK support"
+  option 'with-c++11', 'Compile using Clang, std=c++11 and stdlib=libc++' if MacOS.version >= :lion
 
   depends_on 'cmake' => :build
   depends_on 'boost'
@@ -16,6 +17,10 @@ class Cgal < Formula
 
   depends_on 'qt' if build.include? 'imaging'
   depends_on 'eigen' if build.include? 'with-eigen3'
+
+  def patches
+    { :p0 => DATA }
+  end
 
   def install
     args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}",
@@ -31,8 +36,46 @@ class Cgal < Formula
     if build.include? 'with-lapack'
       args << "-DWITH_LAPACK=ON"
     end
+    if build.include? 'with-c++11'
+      args << "-DCGAL_CFG_NO_CPP0X_ARRAY=1"
+      args << "-DCGAL_CFG_NO_TR1_ARRAY=1"
+      args << "-DCGAL_CFG_NO_VARIADIC_TEMPLATES=1"
+      args << "CXX=clang++ -std=c++11 -stdlib=libc++"
+    end
+
     args << '.'
     system "cmake", *args
     system "make install"
   end
 end
+
+__END__
+--- include/CGAL/Constrained_triangulation_2.h	2012-01-17 21:01:00.000000000 +0100
++++ include/CGAL/Constrained_triangulation_2.h	2012-04-16 11:35:58.859492000 +0200
+@@ -541,7 +541,7 @@
+ 
+   list_ab.push_back(Edge(lf, lf->index(current_face)));
+   list_ba.push_front(Edge(rf, rf->index(current_face)));
+-  intersected_faces.push_front(current_face);
++  intersected_faces.push_front(current_face.handle());
+ 
+   // initcd
+   previous_face=current_face; 
+@@ -574,7 +574,7 @@
+       }
+       else {
+ 	lf= current_face->neighbor(i2);
+-	intersected_faces.push_front(current_face);
++	intersected_faces.push_front(current_face.handle());
+ 	if (orient == LEFT_TURN) 
+ 	  list_ab.push_back(Edge(lf, lf->index(current_face)));
+ 	else // orient == RIGHT_TURN
+@@ -590,7 +590,7 @@
+     
+   // last triangle 
+   vi = current_vertex;
+-  intersected_faces.push_front(current_face);
++  intersected_faces.push_front(current_face.handle());
+   lf= current_face->neighbor(cw(ind));
+   list_ab.push_back(Edge(lf, lf->index(current_face))); 
+   rf= current_face->neighbor(ccw(ind));
