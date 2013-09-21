5197dc7a9740aa6cae6d3ff6a72326a4e34204ef
seanjreilly <seanjreilly@gmail.com>
2013-09-20 18:54:06 -0600

    gradle-1.8-rc2

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -6,9 +6,9 @@ class Gradle < Formula
   sha1 '0cbadad3ced2ba5c53bbb7e6ec3e779c1cd3966c'
 
   devel do
-    url 'http://services.gradle.org/distributions/gradle-1.8-rc-1-bin.zip'
-    sha1 'd43e0d53f054549dc84ccc95f1e7e9460d07472e'
-    version '1.8-rc1'
+    url 'http://services.gradle.org/distributions/gradle-1.8-rc-2-bin.zip'
+    sha1 '85c048ecf773560e9a6032ed845f225098a4da43'
+    version '1.8-rc2'
   end
 
   def install
