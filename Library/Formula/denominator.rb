1c66ac5557ce1c1aff6af208ae8b7858ca9db511
adriancole <adrian.f.cole@gmail.com>
2013-09-21 11:41:32 -0700

    denominator 4.3.0
    Closes #22733.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -3,10 +3,10 @@ require 'formula'
 class Denominator < ScriptFileFormula
   # This is a shell script wrapping a java jar; download as single file using nounzip
   homepage 'https://github.com/Netflix/denominator/tree/master/cli'
-  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/4.1.0/denominator?direct',
+  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/4.3.0/denominator?direct',
     :using  => :nounzip
-  version '4.1.0'
-  sha1 '3c25e0ecdc9021dc01908871deb90cc46883d7c0'
+  version '4.3.0'
+  sha1 '44ce57b254792fff953a006b34aef3df359721bc'
 
   test do
     system "#{bin}/denominator", "help"
