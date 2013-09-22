ee52fb69ec427fafb6540cb17cb97e3ffe48ade6
Jack Nagel <jacknagel@gmail.com>
2013-09-21 20:47:13 -0500

    libopkele: use spec deps

--- Library/Formula/libopkele.rb
+++ Library/Formula/libopkele.rb
@@ -5,7 +5,12 @@ class Libopkele < Formula
   url 'http://kin.klever.net/dist/libopkele-2.0.4.tar.bz2'
   sha1 '0c403d118efe6b4ee4830914448078c0ee967757'
 
-  head 'https://github.com/hacker/libopkele.git'
+  head do 
+    url 'https://github.com/hacker/libopkele.git'
+
+    depends_on :automake
+    depends_on :libtool
+  end
 
   depends_on 'pkg-config' => :build
 
@@ -15,12 +20,7 @@ class Libopkele < Formula
       In file included from discovery.cc:5:
       ../include/opkele/discovery.h:24:11: error: use of undeclared identifier 'insert'
     EOS
-  end if !build.head?
-
-  if build.head?
-    depends_on :automake
-    depends_on :libtool
-  end
+  end unless build.head?
 
   def install
     system "./autogen.bash" if build.head?
