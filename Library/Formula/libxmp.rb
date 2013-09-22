727d3b1867da493e76910909b7db37c094b4a0e0
Jack Nagel <jacknagel@gmail.com>
2013-09-21 20:47:24 -0500

    libxmp: use spec deps

--- Library/Formula/libxmp.rb
+++ Library/Formula/libxmp.rb
@@ -4,9 +4,11 @@ class Libxmp < Formula
   homepage 'http://xmp.sourceforge.net'
   url 'http://downloads.sourceforge.net/project/xmp/libxmp/4.1.5/libxmp-4.1.5.tar.gz'
   sha1 'f50ff6d98e9c21c8fb5e29a8c9e5677983285d90'
-  head 'git://git.code.sf.net/p/xmp/libxmp'
 
-  depends_on :autoconf if build.head?
+  head do
+    url 'git://git.code.sf.net/p/xmp/libxmp'
+    depends_on :autoconf
+  end
 
   def install
     system "autoconf" if build.head?
