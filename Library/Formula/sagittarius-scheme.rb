373f92f2285a8eabd037ad13bed0d767e4a6ffc0
KOMATSU Seiji <skomatsu.comutt@gmail.com>
2013-09-22 13:29:29 +0900

    Fixed patch part (spaces are incorrect)

--- Library/Formula/sagittarius-scheme.rb
+++ Library/Formula/sagittarius-scheme.rb
@@ -49,8 +49,8 @@ diff --git a/ext/crypto/libtomcrypt-1.17/CMakeLists.txt b/ext/crypto/libtomcrypt
 --- a/ext/crypto/libtomcrypt-1.17/CMakeLists.txt
 +++ b/ext/crypto/libtomcrypt-1.17/CMakeLists.txt
 @@ -8,7 +8,7 @@
- #
-
+ # 
+ 
  # reset c flags
 -IF (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
 +IF (CMAKE_C_COMPILER_ID STREQUAL "Clang" OR CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
@@ -61,9 +61,9 @@ diff --git a/ext/ffi/CMakeLists.txt b/ext/ffi/CMakeLists.txt
 --- a/ext/ffi/CMakeLists.txt
 +++ b/ext/ffi/CMakeLists.txt
 @@ -2,7 +2,22 @@
- #
+ # 
  # Build file for regex
-
+ 
 -CHECK_INCLUDE_FILE(ffi.h HAVE_FFI_H)
 +IF (APPLE)
 +  IF (FFI_LIBRARY_DIR)
@@ -85,7 +85,7 @@ diff --git a/ext/ffi/CMakeLists.txt b/ext/ffi/CMakeLists.txt
    SET(LIB_FFI_FOUND TRUE)
  ELSE()
 @@ -14,7 +29,11 @@
-
+ 
  IF (LIB_FFI_FOUND)
    #CHECK_FUNCTION_EXISTS(ffi_prep_cif_var HAVE_FFI_PREP_CIF_VAR)
 -  FIND_LIBRARY(FFI ffi)
@@ -108,7 +108,7 @@ diff --git a/ext/zlib/CMakeLists.txt b/ext/zlib/CMakeLists.txt
 +  CHECK_LIBRARY_EXISTS(${ZLIB_LIBRARIES} inflateReset2 "" HAVE_ZLIB_INFLATE_RESET2)
 +  MESSAGE(STATUS "HAVE_ZLIB_INFLATE_RESET2 = ${HAVE_ZLIB_INFLATE_RESET2}")
 +ENDIF()
-
+ 
 -IF (NOT ${ZLIB_FOUND})
 +IF (NOT ZLIB_FOUND OR NOT HAVE_ZLIB_INFLATE_RESET2)
  #  IF (MSVC)
@@ -118,21 +118,21 @@ diff --git a/ext/zlib/CMakeLists.txt b/ext/zlib/CMakeLists.txt
      IF (NOT EXISTS ${HAS_ZLIB_ARCHIVE})
        MESSAGE(STATUS "donwloading zlib")
        FILE(
--   DOWNLOAD "http://zlib.net/${USED_ZLIB_VERSION}.tar.gz"
+-  	DOWNLOAD "http://zlib.net/${USED_ZLIB_VERSION}.tar.gz"
 -                 "${CMAKE_CURRENT_BINARY_DIR}/zlib.tar.gz"
--   SHOW_PROGRESS)
+-  	SHOW_PROGRESS)
 +        DOWNLOAD "http://zlib.net/${USED_ZLIB_VERSION}.tar.gz"
 +        "${CMAKE_CURRENT_BINARY_DIR}/zlib.tar.gz"
 +        SHOW_PROGRESS)
        EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E tar xzf
--   ${CMAKE_CURRENT_BINARY_DIR}/zlib.tar.gz
--   WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
--   )
+-	${CMAKE_CURRENT_BINARY_DIR}/zlib.tar.gz
+-	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+-	)
 +        ${CMAKE_CURRENT_BINARY_DIR}/zlib.tar.gz
 +        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
 +      IF(UNIX OR CYGWIN OR MINGW)
 +        # Default generated CMakeLists.txt does not include correct source directory
-+        EXECUTE_PROCESS(COMMAND sh -c "patch -p3 < ${PROJECT_SOURCE_DIR}/ext/zlib/zlib-CMakeLists.txt.patch"
++        EXECUTE_PROCESS(COMMAND patch -f -p3 -i ${PROJECT_SOURCE_DIR}/ext/zlib/zlib-CMakeLists.txt.patch
 +          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
 +      ENDIF()
        FILE(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/${USED_ZLIB_VERSION}/zconf.h)
@@ -156,14 +156,14 @@ new file mode 100644
 --- /dev/null
 +++ b/ext/zlib/zlib-CMakeLists.txt.patch
 @@ -0,0 +1,11 @@
-+--- build/ext/zlib/zlib-1.2.8.org/CMakeLists.txt   2013-04-29 07:57:10.000000000 +0900
-++++ build/ext/zlib/zlib-1.2.8/CMakeLists.txt   2013-09-22 11:33:03.000000000 +0900
++--- build/ext/zlib/zlib-1.2.8.org/CMakeLists.txt	2013-04-29 07:57:10.000000000 +0900
+++++ build/ext/zlib/zlib-1.2.8/CMakeLists.txt	2013-09-22 11:33:03.000000000 +0900
 +@@ -83,7 +83,7 @@
-+       ${ZLIB_PC} @ONLY)
-+ configure_file(   ${CMAKE_CURRENT_SOURCE_DIR}/zconf.h.cmakein
-+       ${CMAKE_CURRENT_BINARY_DIR}/zconf.h @ONLY)
++ 		${ZLIB_PC} @ONLY)
++ configure_file(	${CMAKE_CURRENT_SOURCE_DIR}/zconf.h.cmakein
++ 		${CMAKE_CURRENT_BINARY_DIR}/zconf.h @ONLY)
 +-include_directories(${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_SOURCE_DIR})
 ++include_directories(${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
-+
-+
++ 
++ 
 + #============================================================================
