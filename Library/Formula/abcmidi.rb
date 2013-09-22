3d2651669a34fd4b9bdee456fe2d4daec98c014e
siadat <siadat@gmail.com>
2013-09-22 15:24:51 +0330

    Update abcmidi formula
    The old url returns a 404 error.
    
    Closes #22742.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- Library/Formula/abcmidi.rb
+++ Library/Formula/abcmidi.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Abcmidi < Formula
   homepage 'http://www.ifdo.ca/~seymour/runabc/top.html'
-  url 'http://www.ifdo.ca/~seymour/runabc/abcMIDI-2013-06-07.zip'
-  version '2013-06-07'
-  sha1 '31563dd1362d1619614571f277c3a8128984659e'
+  url 'http://www.ifdo.ca/~seymour/runabc/abcMIDI-2013-09-15.zip'
+  version '2013-09-15'
+  sha1 '974cfbc10d04e6b6872e361e70a308c1b78e47d9'
 
   def install
     # configure creates a "Makefile" file. A "makefile" file already exist in
