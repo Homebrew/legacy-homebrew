f2f70fce201d4be197a4aecc7894e60cd2ca2f2e
Jack Nagel <jacknagel@gmail.com>
2013-09-21 20:47:16 -0500

    libsodium: use spec deps

--- Library/Formula/libsodium.rb
+++ Library/Formula/libsodium.rb
@@ -5,15 +5,15 @@ class Libsodium < Formula
   url 'http://download.dnscrypt.org/libsodium/releases/libsodium-0.4.2.tar.gz'
   sha256 '1a7901cdd127471724e854a8eb478247dc0ca67be549345c75fc6f2d4e05ed39'
 
-  head 'https://github.com/jedisct1/libsodium.git'
+  head do
+    url 'https://github.com/jedisct1/libsodium.git'
 
-  option :universal
-
-  if build.head?
     depends_on 'libtool' => :build
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
   end
+
+  option :universal
 
   def install
     ENV.universal_binary if build.universal?
