7548fd1da3fe0837e86afb9200f2851b69e7367d
Adam Vandenberg <flangy@gmail.com>
2013-09-23 07:33:32 -0700

    libtiff: url/mirror

--- Library/Formula/libtiff.rb
+++ Library/Formula/libtiff.rb
@@ -2,7 +2,8 @@ require 'formula'
 
 class Libtiff < Formula
   homepage 'http://www.remotesensing.org/libtiff/'
-  url 'http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz'
+  url 'ftp://ftp.remotesensing.org/pub/libtiff/tiff-4.0.3.tar.gz'
+  mirror 'http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz'
   sha256 'ea1aebe282319537fb2d4d7805f478dd4e0e05c33d0928baba76a7c963684872'
 
   option :universal
