af21527f587b1b62a4d5bcb672887cf30f2cfb73
Adam Vandenberg <flangy@gmail.com>
2013-09-22 22:21:14 -0700

    ejdb 1.1.24

--- Library/Formula/ejdb.rb
+++ Library/Formula/ejdb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ejdb < Formula
   homepage 'http://ejdb.org/'
-  url 'https://github.com/Softmotions/ejdb/archive/v1.1.23.tar.gz'
-  sha1 '00ca2c150b2988747ad7b15148c2d624253026c5'
+  url 'https://github.com/Softmotions/ejdb/archive/v1.1.24.tar.gz'
+  sha1 'b743aef55a39a64bd4ee0e94fa58e2c90e63db02'
 
   def install
     cd 'tcejdb' do
