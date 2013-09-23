565244df06e010e3b90d7c731857668aa3697247
Sven-S. Porst <ssp-web@earthlingsoft.net>
2013-09-23 23:46:13 +0200

    yaz 4.2.68
    Closes #22769.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Yaz < Formula
   homepage 'http://www.indexdata.com/yaz'
-  url 'http://ftp.indexdata.dk/pub/yaz/yaz-4.2.65.tar.gz'
-  sha1 'f12cfd31cc215ff8df1a9490835e7af52f80bfc3'
+  url 'http://ftp.indexdata.dk/pub/yaz/yaz-4.2.68.tar.gz'
+  sha1 '5e45020db2b1fa462844bc3787f6756f2fe84186'
 
   depends_on 'pkg-config' => :build
 
