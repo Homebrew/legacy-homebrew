0eb8f14e096fc3834e182d372ca5d3d1d30b265a
Bas Vodde <basv@odd-e.com>
2013-09-23 11:14:58 +0800

    CppUTest 3.5
    Closes #22748.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/cpputest.rb
+++ Library/Formula/cpputest.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Cpputest < Formula
   homepage 'http://www.cpputest.org/'
-  url 'https://github.com/cpputest/cpputest/archive/v3.4.tar.gz'
+  url 'hhttps://github.com/cpputest/cpputest/archive/v3.4.tar.gz'
   sha1 'ecd53f5b1a92a1f2291249ce69f544392f5a8462'
 
   def install
