35787df5ef9d0a69f1efdb7afba49025977a8df8
adriancole <adrian.f.cole@gmail.com>
2013-09-23 09:54:18 -0700

    denominator 4.3.2

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -3,10 +3,10 @@ require 'formula'
 class Denominator < ScriptFileFormula
   # This is a shell script wrapping a java jar; download as single file using nounzip
   homepage 'https://github.com/Netflix/denominator/tree/master/cli'
-  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/4.3.0/denominator?direct',
+  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/4.3.2/denominator?direct',
     :using  => :nounzip
-  version '4.3.0'
-  sha1 '44ce57b254792fff953a006b34aef3df359721bc'
+  version '4.3.2'
+  sha1 '9cdc9d1f3121512eb3bdb0ee93540cce30926702'
 
   test do
     system "#{bin}/denominator", "help"
