8be73cafc841e503c580f04e963107fd1405a188
Kevin Sjöberg <kev.sjoberg@gmail.com>
2013-09-24 22:11:01 +0200

    m 0.0.1

---
+++ Library/Formula/m.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class M < Formula
+  homepage 'https://github.com/KevinSjoberg/m'
+  url 'https://github.com/KevinSjoberg/m/archive/v0.0.1.tar.gz'
+  version '0.0.1'
+  sha1 '24898e60f333c125e19800acbff5e97b1010444d'
+
+  def install
+    system "mkdir -p #{prefix}/share/m && cp m #{prefix}/share/m/"
+  end
+
+  def caveats; <<-EOS.undent
+    Make sure to add the following in your ~/.bashrc.
+
+      source #{prefix}/share/m/m
+    EOS
+  end
+end
