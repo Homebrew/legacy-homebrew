29745d36117667c430ab03f753528dbf1f9a93ba
Joe Block <jpb@unixorn.net>
2013-09-24 07:39:18 -0700

    git-cal 0.9 (new formula)
    git-cal is a simple script to view a commits calendar for a git
    repository (similar to github contributions calendar) on the
    command line.
    
    Closes #22785.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/git-cal.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class GitCal < Formula
+  homepage 'https://github.com/k4rthik/git-cal'
+  url 'https://github.com/k4rthik/git-cal/archive/v0.9.tar.gz'
+  sha1 'dd4027e367382a8593cab4212d2c7882a7b37680'
+
+  def install
+    bin.install 'git-cal'
+  end
+
+  test do
+		# git-cal fails when run outside of a git repo, so
+		# switch to #{bin} before testing it.
+    system "cd #{bin}; #{bin}/git-cal"
+  end
+end
