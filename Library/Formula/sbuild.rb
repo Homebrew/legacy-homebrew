a93447d337bb255aeeca27f2d560707f3cb0a076
Adam Vandenberg <flangy@gmail.com>
2013-09-24 22:45:58 -0700

    sbuild 0.6.0

--- Library/Formula/sbuild.rb
+++ Library/Formula/sbuild.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sbuild < Formula
   homepage 'http://sbuild.tototec.de/sbuild/projects/sbuild/wiki'
-  url 'http://sbuild.tototec.de/sbuild/attachments/download/57/sbuild-0.4.0-dist.zip'
-  sha1 'f206a97c810d925f2bd06bc463c55d5cd7483ca5'
+  url 'http://sbuild.tototec.de/sbuild/attachments/download/75/sbuild-0.6.0-dist.zip'
+  sha1 '21a75d6ff08d56d470da047d73a96ebddc8eee49'
 
   def install
     libexec.install Dir['*']
