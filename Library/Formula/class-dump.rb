1dbf527324167e01231722307d25a24164811c2b
Zachary Waldowski <zwaldowski@gmail.com>
2013-09-25 12:39:26 -0400

    Install class-dump stable from Git nygard/class-dump

--- Library/Formula/class-dump.rb
+++ Library/Formula/class-dump.rb
@@ -2,16 +2,11 @@ require 'formula'
 
 class ClassDump < Formula
   homepage 'http://www.codethecode.com/projects/class-dump/'
+  url 'https://github.com/nygard/class-dump.git', :tag => '3.4'
   head 'https://github.com/nygard/class-dump.git'
-  url 'http://www.codethecode.com/download/class-dump-3.4.tar.bz2'
-  sha1 'bc6d9542af201028ae980b9d0497b491ce98227f'
 
   def install
-    if build.head?
-      system "xcodebuild", "-configuration", "Release", "SYMROOT=build", "PREFIX=#{prefix}", "ONLY_ACTIVE_ARCH=YES"
-      bin.install "build/Release/class-dump"
-    else
-      bin.install 'class-dump'
-    end
+    system "xcodebuild", "-configuration", "Release", "SYMROOT=build", "PREFIX=#{prefix}", "ONLY_ACTIVE_ARCH=YES"
+    bin.install "build/Release/class-dump"
   end
 end
