6049055666828929b9973d26e1959ba5e779ea3c
Jack Nagel <jacknagel@gmail.com>
2013-09-25 09:50:38 -0500

    glib-networking 2.38.0

--- Library/Formula/glib-networking.rb
+++ Library/Formula/glib-networking.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GlibNetworking < Formula
   homepage 'https://launchpad.net/glib-networking'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/glib-networking/2.36/glib-networking-2.36.2.tar.xz'
-  sha256 '2108d55b0af3eea56ce256830bcaf1519d6337e0054ef2eff80f2c0ef0eb23f9'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/glib-networking/2.38/glib-networking-2.38.0.tar.xz'
+  sha256 'a43eacbf721b475cf6ba0cd2eab02a332014f71a4c41d0b44bd7bbf8ed1f840d'
 
   depends_on 'pkg-config' => :build
   depends_on 'xz' => :build
