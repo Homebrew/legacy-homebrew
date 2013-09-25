23c8314106a81a9e90dd3541ac4002e5118c13c2
Michelangelo De Simone <micdesim@gmail.com>
2013-09-25 14:49:49 -0700

    Fix to ./configure script.

--- Library/Formula/zlib.rb
+++ Library/Formula/zlib.rb
@@ -5,6 +5,10 @@ class Zlib < Formula
   url 'http://zlib.net/zlib-1.2.8.tar.gz'
   sha1 'a4d316c404ff54ca545ea71a27af7dbc29817088'
 
+  def patches
+    {:p1 => "https://gist.github.com/michelangelo/6706342/raw/6fa96feaed48ff76f55fa51a99177fce033a2318/configure.patch"}
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
