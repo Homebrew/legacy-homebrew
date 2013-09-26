c7733f42f73964b4b0ef1ff0cac8d4d4e9530780
Christopher Moeller <cmoel@me.com>
2013-09-25 23:20:56 -0700

    Adds woman shell command
    `woman` is a `man` replacement. It does all the same things as `man`.

---
+++ Library/Formula/woman.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class Woman < GithubGistFormula
+  homepage 'https://gist.github.com/cmoel/6710450'
+  url 'https://gist.github.com/cmoel/6710450/raw/4fdf87b1b898372d43c57b889e69da5c4de4f55c/woman'
+  sha1 '41f98ed06a41ad24e650303e67e2df3f3ac47588'
+end
