e72c4b422f5c521b286655844249b8e9dba2ef68
Adam Malcontenti-Wilson <adman.com@gmail.com>
2013-09-26 18:31:32 +1000

    gobject-introspection 1.38.0

--- Library/Formula/gobject-introspection.rb
+++ Library/Formula/gobject-introspection.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GobjectIntrospection < Formula
   homepage 'http://live.gnome.org/GObjectIntrospection'
-  url 'http://ftp.gnome.org/pub/gnome/sources/gobject-introspection/1.34/gobject-introspection-1.34.1.tar.xz'
-  sha256 'bf40470c863dbb292ec52d1e84495e9334ea954e3a0ee59d6ff5f8161ea53abd'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gobject-introspection/1.38/gobject-introspection-1.38.0.tar.xz'
+  sha256 '3575e5d353c17a567fdf7ffaaa7aebe9347b5b0eee8e69d612ba56a9def67d73'
 
   option :universal
 
