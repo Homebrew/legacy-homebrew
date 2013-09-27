839d0aab2c8817a2c09be9b2cfada4e0b4dd21d0
Mike McQuaid <mike@mikemcquaid.com>
2013-09-27 19:34:54 +0100

    aws-sns-cli: fix SHA-1.

--- Library/Formula/aws-sns-cli.rb
+++ Library/Formula/aws-sns-cli.rb
@@ -5,7 +5,7 @@ class AwsSnsCli < AmazonWebServicesFormula
   url 'http://sns-public-resources.s3.amazonaws.com/SimpleNotificationServiceCli-2010-03-31.zip'
   # The version in the tarball is the API version; this is the tool version
   version '2012-03-31'
-  sha1 'b7b2c1dc0e604b3fb4056845eeea03a51bc6cd7b'
+  sha1 '192bd9e682f2b27a3c10f207f7a85c65dcaae471'
 
   def install
     rm Dir['bin/*.cmd'] # Remove Windows versions
