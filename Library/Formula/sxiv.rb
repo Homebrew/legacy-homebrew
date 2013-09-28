a5042d38094bf7d50cede9faf54205681fb2c2df
Brett Koonce <koonce@gmail.com>
2013-09-28 00:49:10 -0500

    sxiv 1.1.1

--- Library/Formula/sxiv.rb
+++ Library/Formula/sxiv.rb
@@ -2,30 +2,17 @@ require 'formula'
 
 class Sxiv < Formula
   homepage 'https://github.com/muennich/sxiv'
-  url 'https://github.com/downloads/muennich/sxiv/sxiv-1.0.tar.gz'
-  sha1 'e29e33c38fd2a6c8a2cb3b270776859328aa8e0e'
+  url 'https://github.com/muennich/sxiv/archive/v1.1.1.tar.gz'
+  sha1 'a87a6940936cc1d14c54d5ffe7814980b5511f07'
 
   head 'https://github.com/muennich/sxiv.git'
 
   depends_on :x11
   depends_on 'imlib2'
-  depends_on 'giflib' => :optional
-  depends_on 'libexif' => :optional
-
-  # Makefile uses GNU install
-  def patches; DATA; end
+  depends_on 'giflib'
 
   def install
     system "make", "config.h"
-
-    if build.with? "giflib"
-      inreplace "config.h", "#define GIF_SUPPORT  0", "#define GIF_SUPPORT  1"
-    end
-
-    if build.with? "libexif"
-      inreplace "config.h", "#define EXIF_SUPPORT 0", "#define EXIF_SUPPORT 1"
-    end
-
     system "make", "PREFIX=#{prefix}", "install"
   end
 
@@ -33,19 +20,3 @@ class Sxiv < Formula
     system "#{bin}/sxiv", "-v"
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index 32c644f..0cd8019 100644
---- a/Makefile
-+++ b/Makefile
-@@ -52,7 +52,8 @@ dist: clean
- 
- install: all
- 	@echo "installing executable file to $(DESTDIR)$(PREFIX)/bin"
--	@install -D -m 755 sxiv $(DESTDIR)$(PREFIX)/bin/sxiv
-+	@install -d $(DESTDIR)$(PREFIX)/bin
-+	@install -m 755 sxiv $(DESTDIR)$(PREFIX)/bin/sxiv
- 	@echo "installing manual page to $(DESTDIR)$(MANPREFIX)/man1"
- 	@mkdir -p $(DESTDIR)$(MANPREFIX)/man1
- 	@sed "s/VERSION/$(VERSION)/g" sxiv.1 > $(DESTDIR)$(MANPREFIX)/man1/sxiv.1
