a2bc063b531cb8896aac9deab931b64c073dd4be
Brett Koonce <koonce@gmail.com>
2013-09-27 22:20:43 -0500

    ipmitool 1.8.13
    Closes #22894.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ipmitool.rb
+++ Library/Formula/ipmitool.rb
@@ -2,14 +2,18 @@ require 'formula'
 
 class Ipmitool < Formula
   homepage 'http://ipmitool.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/ipmitool/ipmitool/1.8.12/ipmitool-1.8.12.tar.bz2'
-  sha1 'b895564db1196e891b60d2ab4f6d0bf5499c3453'
-
-  # Project uses -Wno-unused-result and -Wno-packed-bitfield-compat, which were
-  # introduced in gcc-4.4 and don't work on Apple gcc or llvm-gcc
-  def patches; DATA; end
+  url 'http://downloads.sourceforge.net/project/ipmitool/ipmitool/1.8.13/ipmitool-1.8.13.tar.bz2'
+  sha1 '22254a2b814c8cd323866a4dd835e390521c1dfa'
 
   def install
+    # fix build errors w/ clang
+    inreplace 'include/ipmitool/ipmi_user.h', 'HAVE_PRAGMA_PACK', 'DISABLE_PRAGMA_PACK'
+    # undefined non-posix symbols
+    inreplace 'src/plugins/serial/serial_basic.c', 'IUCLC', '0'
+    inreplace 'src/plugins/serial/serial_basic.c', 'TCFLSH', '0'
+    inreplace 'src/plugins/serial/serial_terminal.c', 'IUCLC', '0'
+    inreplace 'src/plugins/serial/serial_terminal.c', 'TCFLSH', '0'
+
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -17,18 +21,3 @@ class Ipmitool < Formula
     system "make install"
   end
 end
-
-__END__
-diff --git a/configure b/configure
-index bdf6dae..f5b8e8d 100755
---- a/configure
-+++ b/configure
-@@ -5030,7 +5030,7 @@ fi
- done
- 
- 
--CFLAGS="$CFLAGS -fno-strict-aliasing -Wreturn-type -Wno-unused-result -Wno-packed-bitfield-compat"
-+CFLAGS="$CFLAGS -fno-strict-aliasing -Wreturn-type"
- 
- case `pwd` in
-   *\ * | *\  *)
