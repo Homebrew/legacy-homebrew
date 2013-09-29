6f52e6a6fe1a1c044bb1db44b0fce9d64794e2de
Adam Vandenberg <flangy@gmail.com>
2013-09-28 21:01:26 -0700

    mercurial: add mirror
    Closes #22901.

--- Library/Formula/mercurial.rb
+++ Library/Formula/mercurial.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Mercurial < Formula
   homepage 'http://mercurial.selenic.com/'
   url 'http://mercurial.selenic.com/release/mercurial-2.7.1.tar.gz'
+  mirror 'http://fossies.org/linux/misc/mercurial-2.7.1.tar.gz'
   sha1 '3c2b75421a1f141305a773711b637f81020b89a1'
 
   head 'http://selenic.com/repo/hg', :using => :hg
