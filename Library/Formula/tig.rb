791560cbf0e3aa7cb09599a29f64e3bf73aca791
Piotr Banasik <piotr.banasik@gmail.com>
2013-09-30 12:01:23 -0700

    tig 1.2.1
    Closes #22946.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -2,15 +2,17 @@ require 'formula'
 
 class Tig < Formula
   homepage 'http://jonas.nitro.dk/tig/'
-  url 'http://jonas.nitro.dk/tig/releases/tig-1.2.tar.gz'
-  sha1 '38bff28a205b94623ad0c087a3073c986002dc83'
+  url 'http://jonas.nitro.dk/tig/releases/tig-1.2.1.tar.gz'
+  sha1 '5755bae7342debf94ef33973e0eaff6207e623dc'
+
+  depends_on "asciidoc"
+  depends_on "xmlto"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
     system "make install"
     system "make install-doc-man"
 
-    doc.install Dir['*.html']
     bash_completion.install 'contrib/tig-completion.bash'
   end
 end
