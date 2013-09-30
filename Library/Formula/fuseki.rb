6307852f48a524f082b603c9b771755fbc0e7301
Ash Moran <ash.moran@patchspace.co.uk>
2013-09-30 10:45:55 +0100

    LaunchAgent for fuseki

--- Library/Formula/fuseki.rb
+++ Library/Formula/fuseki.rb
@@ -15,10 +15,29 @@ class Fuseki < Formula
 
     # Write the installation path into the wrapper shell script
     inreplace 'fuseki-server', /export FUSEKI_HOME=.+/, "export FUSEKI_HOME=\"#{prefix}\""
+    inreplace 'fuseki-server', /^exec java\s+(.+)/, "exec java -Dlog4j.configuration=file:#{etc/'fuseki.log4j.properties'} \\1"
+
+    # Use file logging instead of STDOUT logging
+    (var/'log/fuseki').mkpath
+    inreplace 'log4j.properties', /^log4j\.rootLogger=.+/, '### \0'
+    inreplace 'log4j.properties', /^log4j\.appender\.stdlog.+/, '### \0'
+    inreplace 'log4j.properties', /^## (log4j\.rootLogger=.+)/, '\1'
+    inreplace 'log4j.properties', /^## (log4j\.appender\.FusekiFileLog.+)/, '\1'
+    inreplace 'log4j.properties', /^log4j.appender.FusekiFileLog.File=.+/,
+                                  "log4j.appender.FusekiFileLog.File=#{(var/'log/fuseki/fuseki.log')}"
+    etc.install 'log4j.properties' => 'fuseki.log4j.properties'
 
     # Move binaries into place
     bin.install 'fuseki-server'
     bin.install Dir["s-*"]
+
+    unless File.exists?(etc/'fuseki.ttl')
+      etc.cp 'config.ttl' => 'fuseki.ttl'
+      ohai "The sample config.ttl config file has been copied to #{etc/'fuseki.ttl'}"
+    end
+
+    # Create a location for dataset files, in case we're being used in LaunchAgent mode
+    (var/'fuseki').mkpath
 
     # Copy across everything else
     prefix.install Dir["*"]
@@ -26,6 +45,8 @@ class Fuseki < Formula
 
   def caveats; <<-EOS.undent
     Quick-start guide:
+
+    * In-memory operation
 
       1. Start the server:
           fuseki-server --update --mem /ds
@@ -35,6 +56,45 @@ class Fuseki < Formula
 
       3. Import some sample data into the store:
           s-put http://localhost:3030/ds/data default #{prefix}/Data/books.ttl
+
+    * LaunchAgent configuration
+
+      1. Edit #{etc/'fuseki.ttl'} to configure your services and datasets
+         (a directory #{var/'fuseki'} has been created for dataset folders)
+
+      2. Follow the LaunchAgent instructions below
+
+    Logging:
+
+      Logging config is stored in #{etc/'fuseki.log4j.properties'}
+      Currently this file will be overwritten if you re-install or upgrade Fuseki.
+
+      To follow the logs, run:
+        tail -f #{var/'log/fuseki/fuseki.log'}
+    EOS
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <false/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_prefix}/bin/fuseki-server</string>
+          <string>--config</string>
+          <string>/usr/local/etc/fuseki.ttl</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+      </dict>
+    </plist>
     EOS
   end
 
