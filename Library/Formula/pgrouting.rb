95e0ae820d299c465ab474faa3f921d802c23d55
Thomas Cioppettini <thomas.cioppettini@gmail.com>
2013-10-01 01:50:32 -0400

    Upgrade pgrouting to v2.0.0

--- Library/Formula/pgrouting.rb
+++ Library/Formula/pgrouting.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Pgrouting < Formula
   homepage 'http://www.pgrouting.org'
-  url "https://github.com/pgRouting/pgrouting/archive/v2.0.0-rc1.tar.gz"
-  version "2.0.0-rc1"
-  sha1 "481077067c754a92f82dabaeaa2a9140881bda25"
+  url "https://github.com/pgRouting/pgrouting/archive/v2.0.0.tar.gz"
+  version "2.0.0"
+  sha1 "51ad76917e40fd25113af78abd7504017b47e488"
 
   depends_on 'cmake' => :build
   depends_on 'boost'
