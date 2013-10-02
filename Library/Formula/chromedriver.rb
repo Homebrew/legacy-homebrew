887431a33fe521b582564d4127d72690eee83ebe
Kevin Moore <kevin@thinkpixellab.com>
2013-10-02 12:04:21 -0300

    chromedriver - fixed version

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -4,6 +4,7 @@ class Chromedriver < Formula
   homepage 'http://code.google.com/p/chromedriver/'
   url 'http://chromedriver.storage.googleapis.com/2.4/chromedriver_mac32.zip'
   sha1 'b78a808c083fe17215069c9d0ff0e6b6455d8964'
+  version '2.4'
 
   def install
     bin.install 'chromedriver'
