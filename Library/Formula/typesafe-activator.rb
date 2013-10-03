94182aaafd312107c5ff9cbc1c2f21f7a3e4c940
Jason Womack <jason.womack@gmail.com>
2013-10-03 14:57:22 -0600

    Removed head conditionals and renamed formula Activator->TypesafeActivator

--- Library/Formula/typesafe-activator.rb
+++ Library/Formula/typesafe-activator.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
-class Activator < Formula
+class TypesafeActivator < Formula
   homepage 'http://typesafe.com/activator'
   url 'http://downloads.typesafe.com/typesafe-activator/1.0.0/typesafe-activator-1.0.0.zip'
   sha1 '9bbe02637e15f401fab28fcfbb038c43c17fbeba'
 
   def install
-    system "./framework/build", "publish-local" if build.head?
+    system "./framework/build", "publish-local"
 
     # remove Windows .bat files
     rm Dir['*.bat']
-    rm Dir["#{buildpath}/**/*.bat"] if build.head?
+    rm Dir["#{buildpath}/**/*.bat"]
 
     prefix.install_metafiles
 
