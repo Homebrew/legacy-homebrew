b0fc15579a67eade24638a235a8bf86f07c52ba6
Mike McQuaid <mike@mikemcquaid.com>
2013-10-03 09:55:52 +0100

    cabal-install: add bottles.

--- Library/Formula/cabal-install.rb
+++ Library/Formula/cabal-install.rb
@@ -5,6 +5,13 @@ class CabalInstall < Formula
   url 'http://hackage.haskell.org/package/cabal-install-1.18.0.2/cabal-install-1.18.0.2.tar.gz'
   sha1 '2d1f7a48d17b1e02a1e67584a889b2ff4176a773'
 
+  bottle do
+    cellar :any
+    sha1 '6479272e2dcc61c0e240fb396baac1de96026655' => :mountain_lion
+    sha1 '72bc77c328690f89ce722fc7d7885fa91b0bf331' => :lion
+    sha1 '4f4ea54fb256df47bbbba5a8a21e5b3540300f69' => :snow_leopard
+  end
+
   depends_on 'ghc'
 
   conflicts_with 'haskell-platform'
