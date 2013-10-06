c45c39ed83ac016bd99440471c83d5c72ea43fa0
D1plo1d <d1plo1d@d1plo1d.com>
2013-10-06 00:21:19 -0400

    Adding a CuraEngine formula.

---
+++ Library/Formula/curaengine.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Curaengine < Formula
+  homepage ''
+  url 'https://github.com/Ultimaker/CuraEngine/archive/13.06.3.tar.gz'
+  head 'https://github.com/Ultimaker/CuraEngine.git'
+  sha1 '74b6053fd1fbfd74eee9f0958d2cbf0cb295bf24'
+
+  def install
+    ENV.deparallelize
+    system "make"
+    bin.install 'curaengine'
+    system "mv #{bin}/CuraEngine #{bin}/curaengine"
+  end
+
+  test do
+    system "curaengine"
+  end
+end
