638c73d8ec6b4177e4ae5722b60e2dd30d95d420
Adam Malcontenti-Wilson <adman.com@gmail.com>
2013-10-06 20:56:55 -0500

    clutter 1.14.4
    Closes #22843.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/clutter.rb
+++ Library/Formula/clutter.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Clutter < Formula
   homepage 'https://wiki.gnome.org/Clutter'
-  url 'http://ftp.gnome.org/pub/gnome/sources/clutter/1.12/clutter-1.12.2.tar.xz'
-  sha256 '27a8c4495099ea33de39c2d9a911a2c9e00ffa4dcc8f94fafedbcc752c0ddf13'
+  url 'http://ftp.gnome.org/pub/gnome/sources/clutter/1.14/clutter-1.14.4.tar.xz'
+  sha256 'c996d91fff6fff24d9e23dcd545439ebc6b999fb1cf9ee44c28ca54c49c0ee1c'
 
   option 'without-x', 'Build without X11 support'
 
