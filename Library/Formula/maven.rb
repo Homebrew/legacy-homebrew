54b6e539894b309f476167417ec422948b25950a
Sebastian Roth <sebastian.roth@gmail.com>
2013-10-07 09:53:45 +0800

    maven: 3.1.1
    Closes #23084.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/maven.rb
+++ Library/Formula/maven.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Maven < Formula
   homepage 'http://maven.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.1.0/binaries/apache-maven-3.1.0-bin.tar.gz'
-  sha1 'af0867027f0907631c1f85ecf668f74c08f5d5e9'
+  url 'http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz'
+  sha1 '630eea2107b0742acb315b214009ba08602dda5f'
 
   def install
     # Remove windows files
