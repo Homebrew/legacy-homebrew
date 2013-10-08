c3937ffce586e9fc0463baaa00349ffd60a04794
Mike McQuaid <mike@mikemcquaid.com>
2013-10-08 08:23:40 +0100

    gmp: remove broken bottles.

--- Library/Formula/gmp.rb
+++ Library/Formula/gmp.rb
@@ -6,12 +6,6 @@ class Gmp < Formula
   mirror 'http://ftp.gnu.org/gnu/gmp/gmp-5.1.3.tar.bz2'
   sha1 'b35928e2927b272711fdfbf71b7cfd5f86a6b165'
 
-  bottle do
-    cellar :any
-    sha1 'ce121187fc35199c2657c126e079c273dee5865a' => :mountain_lion
-    sha1 '766fa63518250395e686b581af1f2f00892b1519' => :lion
-  end
-
   option '32-bit'
 
   def install
