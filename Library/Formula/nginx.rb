8b3292a2ff9b9d7794d5384494ba5a0c59b820cb
Misty De Meo <mistydemeo@gmail.com>
2013-10-09 23:17:47 -0700

    ncrack: disable openssl header check
    Fixes #23160.

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Nginx < Formula
   homepage 'http://nginx.org/'
-  url 'http://nginx.org/download/nginx-1.4.3.tar.gz'
-  sha1 '8d0c34c84ce6dd8ba4442889e8f2599044c90930'
+  url 'http://nginx.org/download/nginx-1.4.2.tar.gz'
+  sha1 '8f006dc773840b6624a137a584ff8850d5155e3f'
 
   devel do
-    url 'http://nginx.org/download/nginx-1.5.6.tar.gz'
-    sha1 '24d84eccc7fecd98d19330ef708d3ea29031abc7'
+    url 'http://nginx.org/download/nginx-1.5.5.tar.gz'
+    sha1 '8526d41cdabcd40d4ffa5ae12c8a2cc325255431'
   end
 
   head 'http://hg.nginx.org/nginx/', :using => :hg
