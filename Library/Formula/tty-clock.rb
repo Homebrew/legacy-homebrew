5894845dbe7320470333167e2203082b4e5c7621
phySi0 <ha.alamin@gmail.com>
2013-10-10 01:16:02 +0100

    Added tty-clock formula

---
+++ Library/Formula/tty-clock.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class TtyClock < Formula
+  homepage 'https://github.com/xorg62/tty-clock/tree/d9875dca06a995faf56a5816abbc5c7ba8c45970'
+  url 'https://github.com/xorg62/tty-clock.git', :using => :git, :revision => 'd9875dca06a995faf56a5816abbc5c7ba8c45970'
+  sha1 'd9875dca06a995faf56a5816abbc5c7ba8c45970'
+
+  def install
+	inreplace "Makefile", "INSTALLPATH = /usr/local/bin/", "INSTALLPATH = #{prefix}/bin/"
+	system "make"
+	system "make install"
+  end
+
+  test do
+    system "tty-clock"
+  end
+end
