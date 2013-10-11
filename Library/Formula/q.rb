c20794756ed75b3fc8c490e2508d6ce6fae5915a
Stuart Carnie <stuart.carnie@gmail.com>
2013-10-11 10:30:17 -0700

    Changes per Adam

--- Library/Formula/q.rb
+++ Library/Formula/q.rb
@@ -1,12 +1,10 @@
-# vim:ts=2 sw=2 expandtab
-
 require 'formula'
 
 class Q < Formula
   homepage 'https://github.com/harelba/q'
-  url 'https://github.com/harelba/q/archive/fbef1ce50c6079f845a2efbcc6c7734ffc7855af.tar.gz'
-  sha1 '4e8bb4867256b89756ada8f6774a46f7a37aafa8'
-  version 'master'
+  url 'https://github.com/stuartcarnie/q/archive/1.0.tar.gz'
+  sha1 '0916d5909f43cac6a9c1dfaf1e81b4ff83363f81'
+  version '1.0'
 
   def install
     bin.install 'q'
