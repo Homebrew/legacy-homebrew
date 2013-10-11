9eee74896ed403e8b03a0572634e8148f8cc5a77
Stuart Carnie <stuart.carnie@gmail.com>
2013-10-11 14:27:18 -0700

    Remove version attribute

--- Library/Formula/q.rb
+++ Library/Formula/q.rb
@@ -4,7 +4,6 @@ class Q < Formula
   homepage 'https://github.com/harelba/q'
   url 'https://github.com/harelba/q/archive/1.0.tar.gz'
   sha1 '0916d5909f43cac6a9c1dfaf1e81b4ff83363f81'
-  version '1.0'
 
   def install
     bin.install 'q'
