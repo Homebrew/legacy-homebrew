d2b9f766ebc999aa452a4b084b3aad29dce65085
Stuart Carnie <stuart.carnie@gmail.com>
2013-10-11 14:08:52 -0700

    q: use official repo
    Closes #23213.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/q.rb
+++ Library/Formula/q.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Q < Formula
   homepage 'https://github.com/harelba/q'
-  url 'https://github.com/stuartcarnie/q/archive/1.0.tar.gz'
+  url 'https://github.com/harelba/q/archive/1.0.tar.gz'
   sha1 '0916d5909f43cac6a9c1dfaf1e81b4ff83363f81'
-  version '1.0'
 
   def install
     bin.install 'q'
