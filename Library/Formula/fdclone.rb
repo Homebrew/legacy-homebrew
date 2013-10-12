e1f4b824c3819f8bef7d3bc30853008d7345f906
Ryo Okamoto <ryo@aquahill.net>
2013-10-12 15:27:10 +0900

    fdclone: 3.01a (new formula)
    FDclone is a file & directory maintenance tool. It is a clone
    of "FD" for MS-DOS, made by Mr. A.Idei in 1989.
    
    homepage: http://hp.vector.co.jp/authors/VA012337/soft/fd/

---
+++ Library/Formula/fdclone.rb
@@ -0,0 +1,29 @@
+require 'formula'
+
+class Fdclone < Formula
+  homepage 'http://hp.vector.co.jp/authors/VA012337/soft/fd/'
+  url 'http://hp.vector.co.jp/authors/VA012337/soft/fd/FD-3.01a.tar.gz'
+  sha1 '5d4f90ccaee67fadcc5d776f90bbe2fd760c4cdd'
+  version '3.01a'
+
+  depends_on 'nkf'
+
+  def install
+    system "make", "PREFIX=#{prefix}", "all"
+    system "make", "MANTOP=#{man}", "install"
+
+    %w(README FAQ HISTORY LICENSES TECHKNOW ToAdmin).each do |file|
+      system "nkf", "-w", "--overwrite", file
+      prefix.install "#{file}.eng" => file
+      prefix.install file => "#{file}.ja"
+    end
+
+    share.install "_fdrc" => "fd2rc.dist"
+  end
+
+  def caveats; <<-EOS.undent
+    To install the initial config file:
+        install -c -m 0644 #{share}/fd2rc.dist ~/.fd2rc
+    EOS
+  end
+end
