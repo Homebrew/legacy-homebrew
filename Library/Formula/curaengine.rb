1979d5cc12f1c740b1269f557b4f7f826a73f735
Rob Gilson <thatotherdude@gmail.com>
2013-10-12 02:06:46 -0400

    Adding a homepage to curaengine and patching it for case sensitive environments

--- Library/Formula/curaengine.rb
+++ Library/Formula/curaengine.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Curaengine < Formula
-  homepage ''
+  homepage 'https://github.com/Ultimaker/CuraEngine'
   url 'https://github.com/Ultimaker/CuraEngine/archive/13.06.3.tar.gz'
   head 'https://github.com/Ultimaker/CuraEngine.git'
   sha1 '74b6053fd1fbfd74eee9f0958d2cbf0cb295bf24'
@@ -9,7 +9,7 @@ class Curaengine < Formula
   def install
     ENV.deparallelize
     system "make"
-    bin.install 'curaengine'
+    bin.install 'CuraEngine'
     system "mv #{bin}/CuraEngine #{bin}/curaengine"
   end
 
