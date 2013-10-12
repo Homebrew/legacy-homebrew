770b9d71b0e18c1a2d9dbe87c48e095c79d4604d
mashriq <yukotan+mashriq@gmail.com>
2013-10-12 17:36:52 +0900

    gcutil 1.8.4 to 1.9.0

--- Library/Formula/gcutil.rb
+++ Library/Formula/gcutil.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gcutil < Formula
   homepage 'https://developers.google.com/compute/docs/gcutil/'
-  url 'https://google-compute-engine-tools.googlecode.com/files/gcutil-1.8.4.tar.gz'
-  sha1 'cf5d094df8fa847befad98d8b3507091822a798e'
+  url 'https://google-compute-engine-tools.googlecode.com/files/gcutil-1.9.0.tar.gz'
+  sha1 'e058f6b07f3438c8c54f8b3b8ba11a41c5fe0121'
 
   def install
     libexec.install 'gcutil', 'lib'
