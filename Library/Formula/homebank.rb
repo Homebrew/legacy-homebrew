15c8d51a6be43b5183e7ef4268a225dfbeef8902
Mike McQuaid <mike@mikemcquaid.com>
2013-10-13 10:38:20 +0100

    homebank 4.5.4

--- Library/Formula/homebank.rb
+++ Library/Formula/homebank.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Homebank < Formula
   homepage 'http://homebank.free.fr'
-  url 'http://homebank.free.fr/public/homebank-4.5.2.tar.gz'
-  sha1 'a9caa11edfd15f4dbe2e2957a11026d0b71aea24'
+  url 'http://homebank.free.fr/public/homebank-4.5.4.tar.gz'
+  sha1 '0d896a95963a5748216b98062b4e15aa5c94bb85'
 
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
