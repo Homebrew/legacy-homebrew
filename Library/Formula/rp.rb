ebd4888a6d56f02328bccfc667eef606e353f823
tlk <jeremieboutoille@gmail.com>
2013-10-13 17:27:51 +0200

    New Formula : rp

---
+++ Library/Formula/rp.rb
@@ -0,0 +1,46 @@
+require 'formula'
+
+
+class Rp < Formula
+  homepage 'http://0vercl0k.github.io/rp/'
+  version '1.0'
+  head 'https://github.com/0vercl0k/rp.git'
+  url 'https://github.com/0vercl0k/rp/archive/master.zip'
+  sha1 '665d6b174f225bfa0946dd4bdb6239482561cdf9'
+
+  depends_on 'cmake' => :build
+
+  def patches
+    # In order to have the same binary name in 32 and 64 bits.
+    DATA
+  end
+
+  def install
+    system "mkdir", "build"
+    cd 'build' do
+      system "cmake", ".."
+      system "make"
+    end
+    bin.install 'bin/rp-osx'
+  end
+
+end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 79d576b..34c2afa 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -36,12 +36,10 @@ set(RP_NAME "${RP_NAME}-${RP_PLAT}")
+ 
+ if(CMAKE_SIZEOF_VOID_P EQUAL 8 AND NOT(optX86BUILD))
+     set(FLAG_CXX "-m64")
+-    set(RP_NAME "${RP_NAME}-x64")
+     set(BEA_LIBRARY "BeaEngine.x64.${RP_PLAT}.${EXTENSION_LIBRARY}")
+     set(ARGTABLE_LIBRARY "argtable2.x64.${RP_PLAT}.${EXTENSION_LIBRARY}")
+ else()
+     set(FLAG_CXX "-m32")
+-    set(RP_NAME "${RP_NAME}-x86")
+     set(BEA_LIBRARY "BeaEngine.x86.${RP_PLAT}.${EXTENSION_LIBRARY}")
+     set(ARGTABLE_LIBRARY "argtable2.x86.${RP_PLAT}.${EXTENSION_LIBRARY}")
+ endif(CMAKE_SIZEOF_VOID_P EQUAL 8 AND NOT(optX86BUILD))
