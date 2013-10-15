e6b1c12806819ea8ee8267e948d75dc4b6d3be6c
Stefan <humdedum@icloud.com>
2013-10-15 17:33:42 +0200

    polarssl 1.3.1

--- Library/Formula/polarssl.rb
+++ Library/Formula/polarssl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Polarssl < Formula
   homepage 'http://polarssl.org/'
-  url 'https://polarssl.org/code/releases/polarssl-1.2.8-gpl.tgz'
-  sha1 'a3e69d4e9302529c5006dcb7d8ecab9c99488036'
+  url 'https://polarssl.org/download/polarssl-1.3.1-gpl.tgz'
+  sha1 'b33856a1b2f736b18a49a20d48986bce6b3133f5'
 
   depends_on 'cmake' => :build
 
