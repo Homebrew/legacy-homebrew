500ba28aa04e7d048c2826659ba192038d84b5c4
Tobias Hieta <tobias@plexapp.com>
2013-10-16 15:15:11 +0100

    cmake 2.8.12
    Closes #23309.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -22,17 +22,10 @@ end
 
 class Cmake < Formula
   homepage 'http://www.cmake.org/'
-  url 'http://www.cmake.org/files/v2.8/cmake-2.8.11.2.tar.gz'
-  sha1 '31f217c9305add433e77eff49a6eac0047b9e929'
+  url 'http://www.cmake.org/files/v2.8/cmake-2.8.12.tar.gz'
+  sha1 '93c93d556e702f8c967acf139fd716268ce69f39'
 
   head 'http://cmake.org/cmake.git'
-
-  bottle do
-    cellar :any
-    sha1 '024d5263bce0f7f36bde4579ce6fc9be9d55fd72' => :mountain_lion
-    sha1 'bfcc7c9925aea56bd5ce883ed8ca391c27144551' => :lion
-    sha1 '22a1369e2ed4b4a4113621b9df6fd75b162e35fb' => :snow_leopard
-  end
 
   depends_on NoExpatFramework
 
