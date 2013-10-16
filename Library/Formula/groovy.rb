dc46a32312407552f03f9345c2ff42fb8e44718a
魏涛 <undancer@users.noreply.github.com>
2013-10-16 14:15:28 +0800

    groovy 2.1.8
    Closes #23300.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Groovy < Formula
   homepage 'http://groovy.codehaus.org/'
-  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.1.7.zip'
-  sha1 '930dbfc1ab4ec036bd798bbd93f2741c944997db'
+  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.1.8.zip'
+  sha1 '6525d9866d52901ee66ae4eddc504dc2265da948'
 
   option 'invokedynamic', "Install the InvokeDynamic version of Groovy (only works with Java 1.7+)"
 
