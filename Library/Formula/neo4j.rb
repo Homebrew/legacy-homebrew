b240557d8651b23e820e122618284649b637f36a
Michal Svab <michal.svab@gamesys.co.uk>
2013-10-16 10:12:39 +0100

    Update devel version of Neo4j to 2.0.0-M6

--- Library/Formula/neo4j.rb
+++ Library/Formula/neo4j.rb
@@ -7,9 +7,9 @@ class Neo4j < Formula
   version 'community-1.9.4-unix'
 
   devel do
-    url 'http://dist.neo4j.org/neo4j-community-2.0.0-M05-unix.tar.gz'
-    sha1 '30a1b1a93ad6923cbce3563779faff23f25a100c'
-    version 'community-2.0.0-M05-unix'
+    url 'http://dist.neo4j.org/neo4j-community-2.0.0-M06-unix.tar.gz'
+    sha1 'a39ebc5476ace229e4ad5c901238a2e24a6ef0d7'
+    version 'community-2.0.0-M06-unix'
   end
 
   def install
