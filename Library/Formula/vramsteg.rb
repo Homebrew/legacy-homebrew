6fe3a4282ff01d3d5e594df98782aa709647ef55
Ayed <bekacho.php@gmail.com>
2013-10-17 07:49:47 +0300

    Vramsteg 1.0.1
    Vramsteg is a CLI progress bar that can be used from any script language.
    It supports color, labels, percentage completion, elapsed time and estimates.
    
    Closes #23326.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/vramsteg.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Vramsteg < Formula
+  homepage 'http://tasktools.org/projects/vramsteg.html'
+  url 'http://taskwarrior.org/download/vramsteg-1.0.1.tar.gz'
+  sha1 'bbc9f54e6e10b3e82dbbac6275e2e611d752e85d'
+
+  depends_on 'cmake' => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
