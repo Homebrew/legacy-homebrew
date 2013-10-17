fa610625a14fb282a168ce6af55111c1c0bc9060
Ayed <bekacho.php@gmail.com>
2013-10-17 08:01:17 +0300

    Added test

--- Library/Formula/vramsteg.rb
+++ Library/Formula/vramsteg.rb
@@ -13,11 +13,6 @@ class Vramsteg < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test vramsteg`.
-    system "false"
+    system "vramsteg"
   end
 end
