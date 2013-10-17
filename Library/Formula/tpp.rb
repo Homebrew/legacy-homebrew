39d6453608fdff6c91ad1c0e8ce8c90e121b9a73
phySi0 <ha.alamin@gmail.com>
2013-10-17 23:56:15 +0100

    Moved patches and resources above install and removed square brackets around .install command arguments

--- Library/Formula/tpp.rb
+++ Library/Formula/tpp.rb
@@ -7,28 +7,6 @@ class Tpp < Formula
 
   depends_on 'figlet' => :optional
 
-  def install
-    bin.install "tpp.rb" => "tpp"
-    share.install ['contrib', 'examples']
-    man1.install ['doc/tpp.1']
-    doc.install ['README', 'CHANGES', 'DESIGN', 'COPYING', 'THANKS', 'README.de']
-    resource('ncurses-ruby').stage do
-      inreplace 'extconf.rb', '$CFLAGS  += " -g"', '$CFLAGS  += " -g -DNCURSES_OPAQUE=0"'
-      system "ruby", "extconf.rb"
-      system "make"
-      system "mkdir", "ncurses-ruby"
-      system "mv", "lib/ncurses_sugar.rb", "ncurses-ruby/ncurses_sugar.rb"
-      system "mv", "ncurses_bin.bundle", "ncurses-ruby"
-      lib.install ['ncurses-ruby']
-      share.install 'README' => '#{share}/doc/ncurses-ruby/README'
-      share.install 'Changes' => '#{share}/doc/ncurses-ruby/Changes'
-      share.install 'VERSION' => '#{share}/doc/ncurses-ruby/VERSION'
-      share.install 'TODO' => '#{share}/doc/ncurses-ruby/TODO'
-      share.install 'COPYING' => '#{share}/doc/ncurses-ruby/COPYING'
-      share.install 'THANKS' => '#{share}/doc/ncurses-ruby/THANKS'
-    end
-  end
-
   resource 'ncurses-ruby' do
     url 'http://switch.dl.sourceforge.net/project/ncurses-ruby.berlios/ncurses-ruby-1.3.1.tar.bz2'
     mirror 'http://freefr.dl.sourceforge.net/project/ncurses-ruby.berlios/ncurses-ruby-1.3.1.tar.bz2'
@@ -37,6 +15,28 @@ class Tpp < Formula
 
   def patches
     DATA
+  end
+
+  def install
+    bin.install "tpp.rb" => "tpp"
+    share.install 'contrib', 'examples'
+    man1.install 'doc/tpp.1'
+    doc.install 'README', 'CHANGES', 'DESIGN', 'COPYING', 'THANKS', 'README.de'
+    resource('ncurses-ruby').stage do
+      inreplace 'extconf.rb', '$CFLAGS  += " -g"', '$CFLAGS  += " -g -DNCURSES_OPAQUE=0"'
+      system "ruby", "extconf.rb"
+      system "make"
+      system "mkdir", "ncurses-ruby"
+      system "mv", "lib/ncurses_sugar.rb", "ncurses-ruby/ncurses_sugar.rb"
+      system "mv", "ncurses_bin.bundle", "ncurses-ruby"
+      lib.install 'ncurses-ruby'
+      share.install 'README' => '#{share}/doc/ncurses-ruby/README',
+                    'Changes' => '#{share}/doc/ncurses-ruby/Changes',
+                    'VERSION' => '#{share}/doc/ncurses-ruby/VERSION',
+                    'TODO' => '#{share}/doc/ncurses-ruby/TODO',
+                    'COPYING' => '#{share}/doc/ncurses-ruby/COPYING',
+                    'THANKS' => '#{share}/doc/ncurses-ruby/THANKS'
+    end
   end
 
   test do
