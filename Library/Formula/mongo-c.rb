56eaca9c7890bd4c819a2d61b767c140ca37426d
Brett Koonce <koonce@gmail.com>
2013-10-18 18:54:44 -0500

    mongo-c v0.8.1
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mongo-c.rb
+++ Library/Formula/mongo-c.rb
@@ -2,14 +2,8 @@ require 'formula'
 
 class MongoC < Formula
   homepage 'http://docs.mongodb.org/ecosystem/drivers/c/'
-  url 'https://github.com/mongodb/mongo-c-driver/archive/v0.8.zip'
-  sha1 'f21924cea0011ad71309f86f1358d082d855aa79'
-
-  # Reported upstream:
-  # https://github.com/mxcl/homebrew/pull/22096
-  def patches
-    "https://gist.github.com/planas/6321873/raw"
-  end
+  url 'https://github.com/mongodb/mongo-c-driver/archive/v0.8.1.zip'
+  sha1 '38ae6a6273bbf11e24f696a65a3ea3901e456126'
 
   def install
     system "make"
