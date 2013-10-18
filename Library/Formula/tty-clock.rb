15e1c41e866872d0c331c0a9114b489747e2cd51
phySi0 <ha.alamin@gmail.com>
2013-10-18 14:08:46 +0100

    Changed URL to tagged release and modified test to be non-blocking

--- Library/Formula/tty-clock.rb
+++ Library/Formula/tty-clock.rb
@@ -2,6 +2,8 @@ require 'formula'
 
 class TtyClock < Formula
   homepage 'https://github.com/xorg62/tty-clock'
+  url 'https://github.com/xorg62/tty-clock/archive/v0.1.tar.gz'
+  sha1 '5350a9c6c391f8a87fd8f467ca3aeb932585e69e'
   head 'https://github.com/xorg62/tty-clock.git'
 
   def install
@@ -11,6 +13,6 @@ class TtyClock < Formula
   end
 
   test do
-    system "tty-clock"
+    system "#{bin}/tty-clock -i"
   end
 end
