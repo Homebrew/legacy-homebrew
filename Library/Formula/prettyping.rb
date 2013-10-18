bbf36a31baec5dd313ae2b0ed978b3e651c2e0b5
Alan Pearce <alan@bulbstudios.com>
2013-10-18 22:00:37 +0100

    Add formula for Prettyping shell script

---
+++ Library/Formula/prettyping.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Prettyping < ScriptFileFormula
+  homepage 'https://bitbucket.org/denilsonsa/small_scripts/overview'
+  url 'https://bitbucket.org/denilsonsa/small_scripts/raw/d1b6136cabcdd6a700a9ea5149487d93c5992cc5/prettyping.sh'
+  sha1 'f329930dafec34313e60e48c5a31ccead0f972ea'
+  version 'd1b6136'
+end
