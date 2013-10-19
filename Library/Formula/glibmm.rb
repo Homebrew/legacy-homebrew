f387641e28bc8144c2354dbc5e98a948ef77f163
Jack Nagel <jacknagel@gmail.com>
2013-10-19 16:08:09 -0500

    glibmm: add upstream patch to fix build

--- Library/Formula/glibmm.rb
+++ Library/Formula/glibmm.rb
@@ -10,9 +10,19 @@ class Glibmm < Formula
   depends_on 'libsigc++'
   depends_on 'glib'
 
+  depends_on 'automake' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'libtool' => :build
+  depends_on 'mm-common' => :build
+
+  # Upstream patch to fix build error
+  # Remove patch, autotools deps and call to autogen.sh at next version
+  def patches
+    "https://git.gnome.org/browse/glibmm/patch/?id=c619be2fadae1b5a87151db0b0adddf55584084a"
+  end
+
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./autogen.sh", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
 end
