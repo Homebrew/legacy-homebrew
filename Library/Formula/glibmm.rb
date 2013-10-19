a255aa5ec265388158354ec8fccaa6167971d560
Jack Nagel <jacknagel@gmail.com>
2013-10-19 13:49:53 -0500

    glibmm 2.38.0

--- Library/Formula/glibmm.rb
+++ Library/Formula/glibmm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Glibmm < Formula
   homepage 'http://www.gtkmm.org/'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.36/glibmm-2.36.2.tar.xz'
-  sha256 'f15b65bf5740257be03c69f2f5ae71436ac3011ebeb02274b2b99b796c6837cf'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.38/glibmm-2.38.0.tar.xz'
+  sha256 'f37bab6bedb7b68045e356feca9e27760a5ce50d95df07156656a0e1deabc402'
 
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
