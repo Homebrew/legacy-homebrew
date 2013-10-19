dc51c2d4638d75e9f49f68cb8412eb589d4babe5
Brett Koonce <koonce@gmail.com>
2013-10-19 18:33:39 -0500

    yaz 5.0.2
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Yaz < Formula
   homepage 'http://www.indexdata.com/yaz'
-  url 'http://ftp.indexdata.dk/pub/yaz/yaz-5.0.1.tar.gz'
-  sha1 '44c5c2cbbb318039620a2c7ca385932d0c03e389'
+  url 'http://ftp.indexdata.dk/pub/yaz/yaz-5.0.2.tar.gz'
+  sha1 '587bd687e063de26546fd643b541be81842a8989'
 
   depends_on 'pkg-config' => :build
 
