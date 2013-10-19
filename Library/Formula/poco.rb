e40f5f91bfef1758b6c35c22d26f756e14b56587
Brett Koonce <koonce@gmail.com>
2013-10-19 18:32:54 -0500

    poco 1.4.6p2
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/poco.rb
+++ Library/Formula/poco.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Poco < Formula
   homepage 'http://pocoproject.org/'
-  url 'http://pocoproject.org/releases/poco-1.4.6/poco-1.4.6p1-all.tar.bz2'
-  sha1 'c667c97b57d3f56b2884237a43d701b63e2be1f5'
-  version '1.4.6p1-all'
+  url 'http://pocoproject.org/releases/poco-1.4.6/poco-1.4.6p2-all.tar.bz2'
+  sha1 '36f9c7cc806eb9ba8fea11b25f2392c37d8f904d'
+  version '1.4.6p2-all'
 
   devel do
     url 'http://pocoproject.org/releases/poco-1.5.1/poco-1.5.1-all.tar.bz2'
