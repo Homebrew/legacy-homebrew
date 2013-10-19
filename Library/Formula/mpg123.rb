999b3ae23c6eccbe8ec9bd5371d0c8769343c0c5
Brett Koonce <koonce@gmail.com>
2013-10-19 13:17:23 -0500

    mpg123 1.16.0
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mpg123.rb
+++ Library/Formula/mpg123.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Mpg123 < Formula
   homepage 'http://www.mpg123.de/'
-  url 'http://downloads.sourceforge.net/project/mpg123/mpg123/1.15.4/mpg123-1.15.4.tar.bz2'
-  mirror 'http://mpg123.orgis.org/download/mpg123-1.15.4.tar.bz2'
-  sha1 'f39d927bcf7abf4f9d857f10cd97c8ceccaffbfa'
+  url 'http://downloads.sourceforge.net/project/mpg123/mpg123/1.16.0/mpg123-1.16.0.tar.bz2'
+  mirror 'http://mpg123.orgis.org/download/mpg123-1.16.0.tar.bz2'
+  sha1 '73105629f8d4b9426ec9fe93455a8271a96c4ae4'
 
   def install
     args = ["--disable-debug", "--disable-dependency-tracking",
