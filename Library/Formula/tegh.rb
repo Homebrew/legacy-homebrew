1a5e7b0c68d48b05f7627a76f513402b3fed67c7
D1plo1d <d1plo1d@d1plo1d.com>
2013-10-21 00:25:58 -0400

    Updating tegh HEAD to use develop

--- Library/Formula/tegh.rb
+++ Library/Formula/tegh.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Tegh < Formula
   homepage 'https://github.com/D1plo1d/tegh'
-  head 'https://github.com/D1plo1d/tegh.git'
+  head 'https://github.com/D1plo1d/tegh.git', :branch => 'develop'
   url 'https://s3.amazonaws.com/tegh_binaries/0.3.0/tegh-0.3.0-brew.tar.gz'
   sha1 'ffdffdf566c99db683a806c2357517d71b5fc5ca'
 
