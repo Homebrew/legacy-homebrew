d41a6ab3d343333de99fd58491cd93b7cba8e969
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2013-10-21 15:29:38 -0400

    Remove patches entirely as they are not recommended by upstream Quassel or Qt and do not affect ML. If testing of the PR breaks SL or Lion, this can be reverted.

--- Library/Formula/quassel.rb
+++ Library/Formula/quassel.rb
@@ -10,36 +10,8 @@ class Quassel < Formula
   depends_on 'cmake' => :build
   depends_on 'qt'
 
-  def patches
-    DATA
-  end
-
   def install
     system "cmake", ".", *std_cmake_args
     system "make install"
   end
 end
-
-__END__
---- a/src/qtui/chatscene.cpp	2012-03-20 13:39:20.000000000 -0700
-+++ b/src/qtui/chatscene.cpp	2012-06-20 10:06:44.000000000 -0700
-@@ -34,7 +34,7 @@
- #endif
- 
- #ifdef HAVE_WEBKIT
--#  include <QWebView>
-+#  include <QtWebKit/QWebView>
- #endif
- 
- #include "chatitem.h"
---- a/src/qtui/webpreviewitem.cpp	2012-03-20 13:39:20.000000000 -0700
-+++ b/src/qtui/webpreviewitem.cpp	2012-06-20 10:08:51.000000000 -0700
-@@ -25,6 +25,6 @@
- #include <QGraphicsProxyWidget>
- #include <QPainter>
--#include <QWebView>
--#include <QWebSettings>
-+#include <QtWebKit/QWebView>
-+#include <QtWebKit/QWebSettings>
- 
- WebPreviewItem::WebPreviewItem(const QUrl &url)
