1f2b4b66c36db131f0471ae95523b99bd181f7c1
Sam Crang <sam.crang@gmail.com>
2013-10-21 19:29:49 +0100

    Upgrade to version 0.3.0 of Bats.

--- Library/Formula/bats.rb
+++ Library/Formula/bats.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Bats < Formula
   homepage 'https://github.com/sstephenson/bats'
-  url 'https://github.com/sstephenson/bats/archive/v0.2.0.tar.gz'
-  sha1 'c2e67181e2bd6f89f9e40d4cad43c0fe2c1df19a'
+  url 'https://github.com/sstephenson/bats/archive/v0.3.0.tar.gz'
+  sha1 'a40959400a288c9527e131409f6eb6bf3183c9ae'
   head 'https://github.com/sstephenson/bats.git'
 
   def install
