975acc00ef8b6a240d2fa793c99518c00ca21f1b
Ernie Hershey <github@ernie.org>
2013-10-21 17:33:37 -0400

    mongodb: 2.4.7 (stable) and 2.5.3 (devel)
    Closes #23427.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mongodb < Formula
   homepage 'http://www.mongodb.org/'
-  url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.6.tar.gz'
-  sha1 '32066d405f3bed175c9433dc4ac455c2e0091b53'
+  url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.7.tar.gz'
+  sha1 'abef63992fe12e4e68a7d9de01d8d8eaa8705c9a'
 
   bottle do
     revision 2
@@ -13,15 +13,15 @@ class Mongodb < Formula
   end
 
   devel do
-    url 'http://downloads.mongodb.org/src/mongodb-src-r2.5.2.tar.gz'
-    sha1 'e6b0aa35ea78e6bf9d7791a04810a4db4d69decc'
+    url 'http://downloads.mongodb.org/src/mongodb-src-r2.5.3.tar.gz'
+    sha1 '8fbd7f6f2a55092ae0e461ee0f5a4a7f738d40c9'
   end
 
   head 'https://github.com/mongodb/mongo.git'
 
   def patches
-    # Fix Clang v8 build failure.
-    'https://github.com/mongodb/mongo/commit/be4bc7.patch'
+    # Fix osx_min_verson issues with clang
+    'https://github.com/mongodb/mongo/commit/978af9.patch' if build.devel?
   end
 
   depends_on 'scons' => :build
