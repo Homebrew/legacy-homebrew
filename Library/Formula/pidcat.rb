e41034280a46997d81254a28733669d9e3661b60
Tyler Chesley <tchesley@control4.com>
2013-10-21 14:20:55 -0600

    pidcat: 1.4.0 - script to highlight android logcat output for your process
    Simple formula to checkouts Jack Wharton's pidcat.py script, make it executable and put it on the path for easy use.

--- Library/Formula/pidcat.rb
+++ Library/Formula/pidcat.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pidcat < Formula
   homepage 'https://github.com/JakeWharton/pidcat'
-  url 'https://github.com/JakeWharton/pidcat.git', :branch => 'master'
-  version '1.0.0'
+  url 'https://github.com/JakeWharton/pidcat.git'
+  version '1.4.0'
 
   def install
     bin.install('pidcat.py')
