4b05d8e06cab1c9a31225b513d60d2edf6e1bac0
Mike McQuaid <mike@mikemcquaid.com>
2013-10-23 22:15:21 +0100

    test_utils: fix Mavericks arch test.

--- Library/Homebrew/test/test_utils.rb
+++ Library/Homebrew/test/test_utils.rb
@@ -15,18 +15,23 @@ class UtilTests < Test::Unit::TestCase
     return unless File.exist? '/usr/bin/svn'
 
     archs = archs_for_command '/usr/bin/svn'
-    if `sw_vers -productVersion` =~ /10\.(\d+)/ and $1.to_i >= 7
+    if `sw_vers -productVersion` =~ /10\.(\d+)/ and $1.to_i >= 9
+      assert_equal 1, archs.length
+      assert archs.include?(:x86_64)
+    elsif `sw_vers -productVersion` =~ /10\.(\d+)/ and $1.to_i >= 7
       assert_equal 2, archs.length
       assert archs.include?(:x86_64)
+      assert archs.include?(:i386)
     elsif `sw_vers -productVersion` =~ /10\.(\d+)/ and $1.to_i == 6
       assert_equal 3, archs.length
       assert archs.include?(:x86_64)
+      assert archs.include?(:i386)
       assert archs.include?(:ppc7400)
     else
       assert_equal 2, archs.length
+      assert archs.include?(:i386)
       assert archs.include?(:ppc7400)
     end
-    assert archs.include?(:i386)
   end
 
 end
