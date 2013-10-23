19d8328e2b55bac96da35129d56f9a0b0f82e99a
Alexis Hildebrandt <afh@surryhill.net>
2013-10-23 10:58:06 +0200

    gwenhywfar devel 4.8.0beta
    Closes #23465.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gwenhywfar.rb
+++ Library/Formula/gwenhywfar.rb
@@ -4,8 +4,12 @@ class Gwenhywfar < Formula
   homepage 'http://www.aqbanking.de/'
   url 'http://www.aquamaniac.de/sites/download/download.php?package=01&release=67&file=01&dummy=gwenhywfar-4.3.3.tar.gz'
   sha1 'c2ba4c45f1eeb379db6c2ae09122c592893f3bd0'
-
   head 'http://devel.aqbanking.de/svn/gwenhywfar/trunk'
+
+  devel do
+    url 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=71&file=01&dummy=gwenhywfar-4.8.0beta.tar.gz'
+    sha1 '13b02c2485d0ddb33eaa391d295addd96cdb3071'
+  end
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
