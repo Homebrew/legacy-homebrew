90dec4281370e58ca1ff5d3e43e693ede52cc4eb
Leif Middelschulte <leif.middelschulte@gmail.com>
2013-10-24 20:55:39 +0200

    Add back GStreamer 0.10

---
+++ Library/Formula/gst-plugins-bad010.rb
@@ -0,0 +1,35 @@
+require 'formula'
+
+class GstPluginsBad010 < Formula
+  homepage 'http://gstreamer.freedesktop.org/'
+  url 'http://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-0.10.23.tar.bz2'
+  sha256 '0eae7d1a1357ae8377fded6a1b42e663887beabe0e6cc336e2ef9ada42e11491'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gettext'
+  depends_on 'gst-plugins-base'
+
+  # These optional dependencies are based on the intersection of
+  # gst-plugins-bad-0.10.21/REQUIREMENTS and Homebrew formulae
+  depends_on 'dirac' => :optional
+  depends_on 'libdvdread' => :optional
+  depends_on 'libmms' => :optional
+
+  # These are not mentioned in REQUIREMENTS, but configure look for them
+  depends_on 'libexif' => :optional
+  depends_on 'faac' => :optional
+  depends_on 'faad2' => :optional
+  depends_on 'libsndfile' => :optional
+  depends_on 'schroedinger' => :optional
+  depends_on 'rtmpdump' => :optional
+
+  def install
+    ENV.append "CFLAGS", "-no-cpp-precomp -funroll-loops -fstrict-aliasing"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-sdl"
+    system "make"
+    system "make install"
+  end
+end
---
+++ Library/Formula/gstreamer010.rb
@@ -0,0 +1,51 @@
+require 'formula'
+
+class Gstreamer010 < Formula
+  homepage 'http://gstreamer.freedesktop.org/'
+  url 'http://gstreamer.freedesktop.org/src/gstreamer/gstreamer-0.10.36.tar.bz2'
+  sha256 'e556a529e0a8cf1cd0afd0cab2af5488c9524e7c3f409de29b5d82bb41ae7a30'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gettext'
+  depends_on 'glib'
+
+  # Fix sed version detection for 10.8
+  # Reported and fixed upstream:
+  # https://bugzilla.gnome.org/show_bug.cgi?id=680428
+  def patches
+    DATA
+  end
+
+  def install
+    # Look for plugins in HOMEBREW_PREFIX/lib/gstreamer-0.10 instead of
+    # HOMEBREW_PREFIX/Cellar/gstreamer/0.10/lib/gstreamer-0.10, so we'll find
+    # plugins installed by other packages without setting GST_PLUGIN_PATH in
+    # the environment.
+    inreplace "configure", 'PLUGINDIR="$full_var"',
+      "PLUGINDIR=\"#{HOMEBREW_PREFIX}/lib/gstreamer-0.10\""
+
+    ENV.append "CFLAGS", "-funroll-loops -fstrict-aliasing -fno-common"
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-introspection=no"
+    system "make"
+    system "make install"
+  end
+end
+
+__END__
+diff --git a/configure b/configure
+index 0af896d..20e6576 100755
+--- a/configure
++++ b/configure
+@@ -21304,7 +21304,7 @@ fi
+   fi
+ 
+         flex_min_version=2.5.31
+-  flex_version=`$FLEX_PATH --version | head -n 1 | sed 's/^.* //' | sed 's/[a-zA-Z]*$//' | cut -d' ' -f1`
++  flex_version=`$FLEX_PATH --version | head -n 1 | awk '{print $2'}`
+   { $as_echo "$as_me:${as_lineno-$LINENO}: checking flex version $flex_version >= $flex_min_version" >&5
+ $as_echo_n "checking flex version $flex_version >= $flex_min_version... " >&6; }
+   if perl -w <<EOF
