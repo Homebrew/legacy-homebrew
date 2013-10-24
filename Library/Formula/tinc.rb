5e59e67f370f0d8a0b4325ea5f4ac23725b4cece
Stian Eikeland <stian@eikeland.se>
2013-10-24 13:48:03 +0200

    tinc 1.0.23
    Closes #23525.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/tinc.rb
+++ Library/Formula/tinc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tinc < Formula
   homepage 'http://www.tinc-vpn.org'
-  url 'http://www.tinc-vpn.org/packages/tinc-1.0.22.tar.gz'
-  sha1 '4a61b5b5f2e31bda5225b1224880d25524d093e7'
+  url 'http://www.tinc-vpn.org/packages/tinc-1.0.23.tar.gz'
+  sha1 '840dca0cc1d28a3e408f463693ef766c72d8bc90'
 
   depends_on 'lzo'
 
