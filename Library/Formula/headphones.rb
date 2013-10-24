fbb740ee8ede05295a635d22a4d434cb8f9cc389
Bas Spaans <bas.spaans@gmail.com>
2013-10-24 19:11:10 +0200

    Re-added homepage

--- Library/Formula/headphones.rb
+++ Library/Formula/headphones.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Headphones < Formula
-  homepage ''
+  homepage 'https://github.com/rembo10/headphones'
   url 'https://github.com/rembo10/headphones/archive/v0.1.2.zip'
   version '0.1.2'
   sha1 'd02b4ba4b513e318abf08d72c5b8b9ed4e35de3a'
