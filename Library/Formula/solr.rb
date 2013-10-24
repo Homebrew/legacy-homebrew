7ff8e195768e428a63780816c672a08623dde4a7
Mark A. Matienzo <mark@matienzo.org>
2013-10-24 13:26:07 -0400

    Solr 4.5.1
    Closes #23538.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/solr.rb
+++ Library/Formula/solr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Solr < Formula
   homepage 'http://lucene.apache.org/solr/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.5.0/solr-4.5.0.tgz'
-  sha1 '0b1d4a9b6714b80db7a83f30077d97f05d38af3f'
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.5.1/solr-4.5.1.tgz'
+  sha1 '019153f18c28b2b4d966496438940e21a7f63daa'
 
   def script; <<-EOS.undent
     #!/bin/sh
