b929ee05d67bc348b981694e59d2af10f6338aae
seanjreilly <seanjreilly@gmail.com>
2013-10-24 12:07:34 +0100

    gradle 1.9-rc1
    Closes #23523.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -5,6 +5,12 @@ class Gradle < Formula
   url 'http://services.gradle.org/distributions/gradle-1.8-bin.zip'
   sha1 'f14299582a1ab6c1293a43697ecda4b2673e34b1'
 
+  devel do
+    url 'http://services.gradle.org/distributions/gradle-1.9-rc-1-bin.zip'
+    sha1 '9867a3ab08e40dcf5997319c2d262d7a4dc1bc0a'
+    version '1.9-rc1'
+  end
+
   def install
     libexec.install %w[bin lib]
     bin.install_symlink libexec+'bin/gradle'
