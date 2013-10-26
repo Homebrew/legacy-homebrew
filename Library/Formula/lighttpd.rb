e4d514a8b183fea62a892e8cd9efb689f9336c5e
Brett Koonce <koonce@gmail.com>
2013-10-25 20:38:07 -0500

    lighttpd 1.4.33
    Closes #23614.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/lighttpd.rb
+++ Library/Formula/lighttpd.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Lighttpd < Formula
   homepage 'http://www.lighttpd.net/'
-  url 'http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.32.tar.bz2'
-  sha256 '60691b2dcf3ad2472c06b23d75eb0c164bf48a08a630ed3f308f61319104701f'
+  url 'http://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.33.tar.bz2'
+  sha256 '2ff2324658c0f90e7d39afd40f08f11ca230903b9019c31a2bbecd8f087f235e'
 
   option 'with-lua', 'Include Lua scripting support for mod_magnet'
 
   depends_on 'pkg-config' => :build
+  depends_on 'automake' => :build
   depends_on 'pcre'
   depends_on 'lua' => :optional
   depends_on 'libev' => :optional
@@ -33,6 +34,9 @@ class Lighttpd < Formula
 
     args << "--with-lua" if build.with? 'lua'
     args << "--with-libev" if build.with? 'libev'
+
+    # fixed upstream, should be in next release: http://redmine.lighttpd.net/issues/2517
+    inreplace 'src/Makefile.am', '$(LDAP_LIB)', '$(SSL_LIB) $(LDAP_LIB)'
 
     system "./configure", *args
     system "make install"
