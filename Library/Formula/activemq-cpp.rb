90d7070ea3f49e1566d54eae4779eec301113a3f
Adam Vandenberg <flangy@gmail.com>
2013-10-26 10:51:17 -0700

    activemq-cpp 3.8.1

--- Library/Formula/activemq-cpp.rb
+++ Library/Formula/activemq-cpp.rb
@@ -1,14 +1,14 @@
 require 'formula'
 
 class ActivemqCpp < Formula
-  url 'http://apache.dattatec.com/activemq/activemq-cpp/source/activemq-cpp-library-3.7.1-src.tar.bz2'
   homepage 'http://activemq.apache.org/cms/index.html'
-  sha1 'e70167a0dc4f6c645c1e16a9c4e3fd95d208adf5'
+  url 'http://apache.dattatec.com/activemq/activemq-cpp/source/activemq-cpp-library-3.8.1-src.tar.bz2'
+  sha1 'd82d6c647d507ef4a44e28665f68def7a79e5b1d'
 
   depends_on 'autogen' => :build
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
 
 
   def install
