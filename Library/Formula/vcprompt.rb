6465db7d25960a8fecc17bdfc20655d755016467
Robert Gieseke <rob.g@web.de>
2013-10-26 18:27:51 +0200

    vcprompt 1.1
    Closes #23632.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/vcprompt.rb
+++ Library/Formula/vcprompt.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vcprompt < Formula
   homepage 'https://bitbucket.org/gward/vcprompt'
-  url 'https://bitbucket.org/gward/vcprompt/downloads/vcprompt-1.0.tar.gz'
-  sha1 'a393d06a3b6030a822af53c985b6a170265ee901'
+  url 'https://bitbucket.org/gward/vcprompt/downloads/vcprompt-1.1.tar.gz'
+  sha1 '6982e3ecc0c677546da1e067984c0e25c3f9261c'
 
   head 'hg://https://bitbucket.org/gward/vcprompt'
 
