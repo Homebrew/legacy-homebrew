390e3aa4b2f3d03ebd1cab6b9e21de99f80fe920
Gregory Pakosz <gpakosz@visionobjects.com>
2013-10-26 12:36:31 +0200

    peg 0.1.13
    Closes #23626.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/peg.rb
+++ Library/Formula/peg.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Peg < Formula
   homepage 'http://piumarta.com/software/peg/'
-  url 'http://piumarta.com/software/peg/peg-0.1.9.tar.gz'
-  sha1 '40a3dd06264fef8aaaedcc88f198c0641bf4190d'
+  url 'http://piumarta.com/software/peg/peg-0.1.13.tar.gz'
+  sha1 'ecf33104907ed18339395874245a6a0bd2b4f515'
 
   def install
     system "make all"
     bin.install %w[peg leg]
-    man1.install gzip("peg.1")
+    man1.install gzip("src/peg.1")
   end
 end
