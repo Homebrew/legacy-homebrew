27ab8bde4a41821f2a27d3717e53372097fb9a88
Mike McQuaid <mike@mikemcquaid.com>
2013-10-28 10:34:34 +0000

    automake: add bottles.

--- Library/Formula/automake.rb
+++ Library/Formula/automake.rb
@@ -6,6 +6,12 @@ class Automake < Formula
   mirror 'http://ftp.gnu.org/gnu/automake/automake-1.14.tar.gz'
   sha1 '648f7a3cf8473ff6aa433c7721cab1c7fae8d06c'
 
+  bottle do
+    sha1 'd4b453a2c8d0f4c0cefa499a8f658448d781225e' => :mavericks
+    sha1 'bbbd4cc22501df3e747596d4bfd02cba987fc852' => :mountain_lion
+    sha1 '8ee87f97dc533fd1b86a46dff0b05d831070c6cb' => :lion
+  end
+
   # Always needs a newer autoconf, even on Snow Leopard.
   depends_on 'autoconf'
 
