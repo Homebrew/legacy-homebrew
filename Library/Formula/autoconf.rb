c72b814982944fb63eaa478298e0c31be984d496
Mike McQuaid <mike@mikemcquaid.com>
2013-10-28 10:34:25 +0000

    autoconf: add bottles.

--- Library/Formula/autoconf.rb
+++ Library/Formula/autoconf.rb
@@ -6,6 +6,12 @@ class Autoconf < Formula
   mirror 'http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz'
   sha1 '562471cbcb0dd0fa42a76665acf0dbb68479b78a'
 
+  bottle do
+    sha1 '2b69898b2827740e5983e25c7f20c0328201b256' => :mavericks
+    sha1 'ec30045d8fe4be10858b66d59f029fb19fe63b5e' => :mountain_lion
+    sha1 'e7d6d88e762996c2fb96238f7d9e48e6d0feaeba' => :lion
+  end
+
   if MacOS::Xcode.provides_autotools? or File.file? "/usr/bin/autoconf"
     keg_only "Xcode (up to and including 4.2) provides (a rather old) Autoconf."
   end
