e8768d2400aff4aae88dc54bdd1709fe224de66d
phySi0 <ha.alamin@gmail.com>
2013-10-28 07:03:12 +0000

    Added Cap'n Proto formula

---
+++ Library/Formula/capnp.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Capnp < Formula
+  homepage 'http://kentonv.github.io/capnproto/'
+  url 'http://capnproto.org/capnproto-c++-0.3.0.tar.gz'
+  sha1 '26152010298db40687bf1b18ff6a438986289a44'
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "-j6", "check"
+	system "make", "install"
+  end
+
+  test do
+    system "capnp", "--version"
+  end
+end
