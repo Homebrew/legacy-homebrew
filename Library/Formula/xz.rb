2a0060c92385a89bccaeb501491e6246af78233d
Mike McQuaid <mike@mikemcquaid.com>
2013-10-28 11:19:31 +0000

    xz: add bottles.

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -8,6 +8,12 @@ class Xz < Formula
   mirror 'http://tukaani.org/xz/xz-5.0.5.tar.gz'
   sha256 '5dcffe6a3726d23d1711a65288de2e215b4960da5092248ce63c99d50093b93a'
 
+  bottle do
+    sha1 '08b71836cce456f716a9415c1565eba5f172c735' => :mavericks
+    sha1 '9cc36f16730d913cfa6644eeaff6e5091a8e9602' => :mountain_lion
+    sha1 '4a181308ba15a63f7876d8b3852136288af78d28' => :lion
+  end
+
   option :universal
 
   def install
