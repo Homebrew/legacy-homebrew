a8a3690207303dffa9ef72f54eaa20c076458944
Jose Luis Loya <jl.loya@gmail.com>
2013-10-27 23:39:12 -0500

    sqlmap: 0.9 - Automatic SQL injection and database penetration tool
    Formula updated to use the stable version 0.9. Stills has the head option
    in case anyone wants to install the bleeding-edge version.

--- Library/Formula/sqlmap.rb
+++ Library/Formula/sqlmap.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Sqlmap < Formula
   homepage 'http://sqlmap.org/'
-  url 'http://github.com/sqlmapproject/sqlmap/tarball/master'
-  sha1 'fb769f7b08e2e9fbaa7cdce8637c8c5879048a40'
-  version '1.0-dev'
+  url 'https://github.com/sqlmapproject/sqlmap/archive/0.9.tar.gz'
+  sha1 '25d7c13fc6e8bb55a1b4d9ba60a7ebd558ad0374'
   head 'https://github.com/sqlmapproject/sqlmap.git'
 
   depends_on :python
@@ -20,8 +19,12 @@ class Sqlmap < Formula
   end
   def install
     unzip_dir = prefix + "#{name}-#{version}/"
-    inreplace "lib/parse/cmdline.py", '"\"%s\"" % _ if " " in _ else _)', ''
-    inreplace "lib/parse/cmdline.py", 'usage = "%s%s [options]" % ("python " if not IS_WIN else "", \\', 'usage = "sqlmap [options]"'
+    if build.head?
+      inreplace "lib/parse/cmdline.py", '"\"%s\"" % _ if " " in _ else _)', ''
+      inreplace "lib/parse/cmdline.py", 'usage = "%s%s [options]" % ("python " if not IS_WIN else "", \\', 'usage = "sqlmap [options]"'
+    else
+      inreplace "lib/parse/cmdline.py", 'usage = "python %s [options]" % sys.argv[0]', 'usage = "sqlmap [options]"'
+    end
     unzip_dir.install Dir['*']
     bin.install_symlink unzip_dir+"sqlmap.py" => 'sqlmap.py'
     bin.install_symlink unzip_dir+"sqlmap.py" => 'sqlmap'
