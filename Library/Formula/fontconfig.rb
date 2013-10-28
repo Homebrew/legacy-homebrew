88b5625091c6a51b3efdd7cf630f869fe2af1d78
Mike McQuaid <mike@mikemcquaid.com>
2013-10-28 11:00:44 +0000

    fontconfig: add bottles.

--- Library/Formula/fontconfig.rb
+++ Library/Formula/fontconfig.rb
@@ -5,6 +5,12 @@ class Fontconfig < Formula
   url 'http://fontconfig.org/release/fontconfig-2.10.95.tar.bz2'
   sha1 'f9f4a25b730a9c56f951db6fec639ddeeb92a9d4'
 
+  bottle do
+    # Included with X11 so no bottle needed before Mountain Lion.
+    sha1 '8429234eb972b93a02dba0925d23c10c1705da77' => :mavericks
+    sha1 '8f5835b617380b08d2b38e1f0ee4ee976daa34c5' => :mountain_lion
+  end
+
   keg_only :provided_pre_mountain_lion
 
   option :universal
