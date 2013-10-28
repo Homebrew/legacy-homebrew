ce70a56e31f41091ffbe0f9beabd03e02773f924
onewheelskyward <andrew.kreps@gmail.com>
2013-10-28 14:23:57 -0700

    siege 3.0.5
    Closes #23714.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/siege.rb
+++ Library/Formula/siege.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Siege < Formula
   homepage 'http://www.joedog.org/index/siege-home'
-  url "http://www.joedog.org/pub/siege/siege-3.0.4.tar.gz"
-  sha256 '7d1c47d73347b0c618a711c0d3da638ac3cf3eccf78df420627dae7ea8907f97'
+  url 'http://www.joedog.org/pub/siege/siege-3.0.5.tar.gz'
+  sha256 '283e624fd802775bf6eb8832c4f76dad6692aa1f3efa98db1ae2ddaba651ca99'
 
   def install
     # To avoid unnecessary warning due to hardcoded path, create the folder first
