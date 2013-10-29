20faffe6897c30e87509f5375399f79adcdfe4d4
phySi0 <ha.alamin@gmail.com>
2013-10-29 19:08:38 +0000

    Should fix Cap'n Proto for those who have an old Clang version

--- Library/Formula/capnp.rb
+++ Library/Formula/capnp.rb
@@ -5,13 +5,25 @@ class Capnp < Formula
   url 'http://capnproto.org/capnproto-c++-0.3.0.tar.gz'
   sha1 '26152010298db40687bf1b18ff6a438986289a44'
 
-  depends_on :macos => :mountain_lion
+  resource 'clang' do
+    # "As of this writing, Mac OSX 10.8 with Xcode 4.6 command-line tools is not quite good enough to compile Cap’n Proto. The included version of GCC is ancient. The included version of Clang – which mysteriously advertises itself as version 4.2 – was actually cut from LLVM SVN somewhere between versions 3.1 and 3.2; it is not sufficient to build Cap’n Proto."
+    # http://kentonv.github.io/capnproto/install.html
+    # Old formula worked on ML test-bot reports, so I'm guessing this is only for older Xcode installations
+    url 'http://llvm.org/releases/3.2/clang+llvm-3.2-x86_64-apple-darwin11.tar.gz'
+    sha1 'ae98cce94e335cb409edab95f0b7170af6c0278b'
+  end unless MacOS.clang_version.to_f >= 5.0
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    args = ["--disable-debug", "--disable-dependency-tracking", "--disable-silent-rules", "--prefix=#{prefix}"]
+    unless MacOS.clang_version.to_f >= 5.0
+      resource('clang').stage do
+        system "ln", "-s",
+               "/usr/lib/c++", "lib/c++"
+        clang_dir = pwd
+        args << "CXX=#{clang_dir}/bin/clang++"
+      end
+    end
+    system "./configure", *args
     system "make", "-j6", "check"
     system "make", "install"
   end
