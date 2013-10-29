95d42d6ad494475eae7acbb29862c1fa5fcdc537
Ryan Sobol <contact@ryansobol.com>
2013-10-29 21:50:02 +0800

    gitio: 1.0.0 - Shorten a github.com URL to a git.io URL
    Usage: gitio URL [code]
    
    Examples:
      $ gitio https://gist.github.com/ryansobol/7214372
      http://git.io/EgwCBg
    
      $ gitio gist.github.com/ryansobol/7214372
      http://git.io/EgwCBg
    
      $ gitio https://github.com/ryansobol ryansobol
      http://git.io/ryansobol

---
+++ Library/Formula/gitio.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class Gitio < GithubGistFormula
+  url 'https://gist.github.com/ryansobol/7214372/raw/5bb571d3640a6b04fdca159b9e1ff8440493ae14/gitio'
+  sha1 'f9e2a180100687931084fb0b6734c3c8e9612089'
+  version '1.0.0'
+  homepage 'https://gist.github.com/ryansobol/7214372'
+end
