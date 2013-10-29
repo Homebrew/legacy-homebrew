21f3411b9e0a1d9e74630671098dbd3dcd0c7da3
Jack Nagel <jacknagel@gmail.com>
2013-10-28 20:04:35 -0500

    ragel: add patch to fix compilation with recent clang
    Closes #23394.

--- Library/Formula/ragel.rb
+++ Library/Formula/ragel.rb
@@ -10,9 +10,30 @@ class Ragel < Formula
     sha1 'e57ee7f740dd395d4d5330949594a02c91ad0308'
   end
 
+  def patches
+    # Fix compilation with clang
+    # http://www.complang.org/pipermail/ragel-users/2013-October/003007.html
+    DATA
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
     doc.install resource('pdf')
   end
 end
+
+__END__
+diff --git a/ragel/javacodegen.cpp b/ragel/javacodegen.cpp
+index adff67e..2152b35 100644
+--- a/ragel/javacodegen.cpp
++++ b/ragel/javacodegen.cpp
+@@ -1184,7 +1184,7 @@ std::ostream &JavaTabCodeGen::ARRAY_ITEM( string item, bool last )
+ {
+ 	item_count++;
+ 
+-	out << setw(5) << setiosflags(ios::right) << item;
++	out << setw(5) << std::setiosflags(ios::right) << item;
+ 	
+ 	if ( !last ) {
+ 		if ( item_count % SAIIC == 0 ) {
