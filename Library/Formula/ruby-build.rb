0eda1fcd9185f478cee20835cadb0828b6c671ef
Mislav Marohnić <mislav.marohnic@gmail.com>
2013-10-30 02:48:15 +0100

    ruby-build v20131030
    Closes #23755.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyBuild < Formula
   homepage 'https://github.com/sstephenson/ruby-build'
-  url 'https://github.com/sstephenson/ruby-build/archive/v20131028.tar.gz'
-  sha1 '4fa33cbcc5f84f2e7b4ec160fb3fd5119ad11404'
+  url 'https://github.com/sstephenson/ruby-build/archive/v20131030.tar.gz'
+  sha1 '64a3728d9bc9e81361036b87b843522b7bd152f8'
 
   head 'https://github.com/sstephenson/ruby-build.git'
 
