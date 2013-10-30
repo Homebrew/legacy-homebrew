8b0fe163f346e1fbdd81139b3140e45727b29e48
Pierre FILSTROFF <pfilstroff@gmail.com>
2013-10-30 12:07:43 +0100

    Fixed cpp-netlib formula and remove test method

--- Library/Formula/cpp-netlib.rb
+++ Library/Formula/cpp-netlib.rb
@@ -4,7 +4,6 @@ class CppNetlib < Formula
   homepage 'http://cpp-netlib.org'
   url 'https://github.com/cpp-netlib/cpp-netlib/archive/cpp-netlib-0.10.1.tar.gz'
   sha1 'aadfb5f039586d47404a2e473548244e9eeaa1f9'
-  version '0.10.1'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
@@ -15,15 +14,10 @@ class CppNetlib < Formula
     cxxflags = "-ftemplate-depth=256"
 
     system "CXXFLAGS=\"" + cxxflags + "\" cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ."
-    system 'make -j4'
+    system 'make'
 
-    # Install libraries
+    # Install
     lib.install Dir["libs/network/src/libcppnetlib-*.a"]
-  end
-
-  def test
-    Dir.chdir "#{prefix}" do
-      system 'make test'
-    end
+    include.install "boost"
   end
 end
