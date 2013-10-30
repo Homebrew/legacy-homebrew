3215a5db1f3a1a3ae3fdcce7aadc61fd2c3837a0
jlhonora <jlhonora@ing.puc.cl>
2013-10-30 17:51:26 -0300

    Corrected multiple homebrew errors
    Corrected the following errors with previous pull request
    commit. All pointed by 'adamv':
    
      * Fixed `brew audit` suggestions
      * Added a tarball instead of specific commits
      * No parentheses in calls, 'ala ruby'
      * Removed test

--- Library/Formula/lsusb.rb
+++ Library/Formula/lsusb.rb
@@ -1,21 +1,15 @@
 require 'formula'
 
 class Lsusb < Formula
-  homepage 'https://github.com/jlhonora/homebrew'
-  url 'https://raw.github.com/jlhonora/lsusb/fdf36d393881d0ba479988a8e890afdb6544753b/lsusb'
-  sha1 '7603057721c16af33eb48e4b631d4a595b13dba0'
-  version '0.1'
+  homepage 'https://github.com/jlhonora/lsusb'
+  url 'https://raw.github.com/jlhonora/lsusb/master/lsusb-0.1.tar.gz'
+  sha1 '46556b26ed9b6086cf724bdb4e573ff4de16517c'
+
   head 'https://github.com/jlhonora/lsusb.git'
-  
-  depends_on 'wget'
 
   def install
-    system "wget https://raw.github.com/jlhonora/lsusb/master/man/lsusb.1"
-    bin.install('lsusb')
-    man1.install('lsusb.1')
+    bin.install 'lsusb'
+    man1.install 'man/lsusb.1'
   end
 
-  test do
-    system "true"
-  end
 end
