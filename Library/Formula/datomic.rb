13354afee48c8b9421e2fb3e5ca909dbf3947dde
Kofi Jedamzik <kofi@jedamzik.net>
2013-10-30 18:44:55 +0100

    datomic 0.8.4254
    Closes #23794.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/datomic.rb
+++ Library/Formula/datomic.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Datomic < Formula
   homepage 'http://www.datomic.com/'
-  url "http://downloads.datomic.com/0.8.4218/datomic-free-0.8.4218.zip"
-  sha1 'e7de3c55900b6caae7b66b62581d6c53e9993858'
+  url 'https://my.datomic.com/downloads/free/0.8.4254'
+  sha1 '6398d53e89e942b8e7fbf495ba0c7c1bff977a3d'
+  version '0.8.4254'
 
   def write_libexec_alias *script_names
     script_names.each do |script_name|
@@ -25,6 +26,10 @@ class Datomic < Formula
       You may need to set JAVA_HOME:
         export JAVA_HOME="$(/usr/libexec/java_home)"
       All commands have been installed with the prefix 'datomic-'.
+
+      We agreed to the Datomic Free Edition License for you:
+        http://www.datomic.com/datomic-free-edition-license.html
+      If this is unacceptable you should uninstall.
     EOS
   end
 end
