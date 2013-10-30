8a4a4df02c329cb9dc754fc7581a63e41771337c
jlhonora <jlhonora@ing.puc.cl>
2013-10-30 16:53:11 -0300

    Added man page
    Added documentation for the lsusb command

--- Library/Formula/lsusb.rb
+++ Library/Formula/lsusb.rb
@@ -5,10 +5,14 @@ class Lsusb < Formula
   url 'https://raw.github.com/jlhonora/lsusb/fdf36d393881d0ba479988a8e890afdb6544753b/lsusb'
   sha1 '7603057721c16af33eb48e4b631d4a595b13dba0'
   version '0.1'
+  head 'https://github.com/jlhonora/lsusb.git'
+  
+  depends_on 'wget'
 
   def install
-    system "chmod +x lsusb"
-    system "cp lsusb /usr/local/bin"
+    system "wget https://raw.github.com/jlhonora/lsusb/master/man/lsusb.1"
+    bin.install('lsusb')
+    man1.install('lsusb.1')
   end
 
   test do
