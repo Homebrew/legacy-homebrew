05be896735797c8ca35ba045874a913ec65a4812
Nikolay Kolev <nikolaynkolev@gmail.com>
2013-10-30 13:39:07 -0700

    tup: switch from fuse4x to osxfuse
    Closes #23816.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/tup.rb
+++ Library/Formula/tup.rb
@@ -7,7 +7,7 @@ class Tup < Formula
   head 'https://github.com/gittup/tup.git'
 
   depends_on 'pkg-config' => :build
-  depends_on 'fuse4x'
+  depends_on 'osxfuse'
 
   def install
     ENV['TUP_LABEL'] = version
@@ -21,7 +21,7 @@ class Tup < Formula
   end
 
   def caveats; <<-EOS.undent
-    Make sure to follow the directions given by `brew info fuse4x-kext`
+    Make sure to follow the directions given by `brew info osxfuse`
     before using 'tup' build tool.
     EOS
   end
