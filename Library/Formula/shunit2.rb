954b2960e5171ae652bbeed390f3647f5b8d1124
Marcos D. Sánchez <marcosdsanchez@gmail.com>
2013-10-30 12:15:49 -0300

    shunit2 2.1.6
    Closes #23786.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/shunit2.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Shunit2 < Formula
+  homepage 'https://code.google.com/p/shunit2/'
+  url 'https://shunit2.googlecode.com/files/shunit2-2.1.6.tgz'
+  sha1 '9cd0e1834b221c378c2f8a6f0baf10410e53680f'
+
+  def install
+    bin.install 'src/shunit2'
+  end
+
+  test do
+    system "#{bin}/shunit2"
+  end
+end
