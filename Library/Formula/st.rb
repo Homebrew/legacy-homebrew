3fc98fa74c38f25ea2638ee1edb61dbc036d45aa
Adam Vandenberg <flangy@gmail.com>
2013-10-31 08:01:24 -0700

    st 1.1.1

--- Library/Formula/st.rb
+++ Library/Formula/st.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class St < Formula
   homepage 'https://github.com/nferraz/st'
-  url 'https://github.com/nferraz/st/archive/v1.0.8.tar.gz'
-  sha1 'cb85e1fba35aeeafe347088ccae910f9072bf91e'
+  url 'https://github.com/nferraz/st/archive/v1.1.1.tar.gz'
+  sha1 'ad7fd987b8c986a9ff80a1f65faad7ce6fbb56fa'
 
   def install
     system "perl", "Makefile.PL", "PREFIX=#{prefix}"
