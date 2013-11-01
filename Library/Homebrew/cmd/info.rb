40b43c25e86340b98fcffbcf21acf7f39029109f
Habib Alamin <ha.alamin@gmail.com>
2013-11-01 05:08:39 +0000

    Added check to see if match was found before printing description

--- Library/Homebrew/cmd/info.rb
+++ Library/Homebrew/cmd/info.rb
@@ -100,7 +100,10 @@ module Homebrew extend self
     formula_home_res = Net::HTTP.get_response URI(f.homepage)
     if formula_home_res.is_a? Net::HTTPSuccess
       pkg_mgr_desc_class = /(<([[:alpha:]]+)[[[:alnum:]]="[[:blank:]]]* class="pkg_mgr_desc".*?>)(.*?)(<\/\2>)/m
-      desc = formula_home_res.body.match(pkg_mgr_desc_class).captures[2]
+      desc = formula_home_res.body.match(pkg_mgr_desc_class)
+      if desc.kind_of? MatchData
+        desc = desc.captures[2]
+      end
       if desc.kind_of? String
         convert_html_tags!(desc, f.homepage)
         ohai "Description"
