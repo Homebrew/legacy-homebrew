94ff5b63497f505fb07e25031bb4a1ad9d13fca5
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-01 15:09:19 +0800

    Fix homepage of formula

--- Library/Formula/remotebox.rb
+++ Library/Formula/remotebox.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Remotebox < Formula
-  homepage 'https://nobgoblin.org.uk/'
+  homepage 'https://knobgoblin.org.uk/'
   url 'http://knobgoblin.org.uk/downloads/RemoteBox-1.6.tar.bz2'
   sha1 '3e73e722f1bfddae04b9465d28b1958d2caf0ee0'
 
