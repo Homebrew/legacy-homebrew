09295890b00db7bf914306701948f5d3c8de8dc3
Jason Morrison <jason.p.morrison@gmail.com>
2013-11-01 14:48:48 -0700

    chromedriver 2.5
    Closes #23876.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Chromedriver < Formula
   homepage 'http://code.google.com/p/chromedriver/'
-  url 'http://chromedriver.storage.googleapis.com/2.4/chromedriver_mac32.zip'
-  sha1 'b78a808c083fe17215069c9d0ff0e6b6455d8964'
-  version '2.4'
+  url 'http://chromedriver.storage.googleapis.com/2.5/chromedriver_mac32.zip'
+  sha1 '2eee598714762e49051460f769be0053e15c6c3e'
+  version '2.5'
 
   def install
     bin.install 'chromedriver'
