5fd98c1cb8d6a6ea1cc210ddecf1a0592cb4f00a
Mike McQuaid <mike@mikemcquaid.com>
2013-11-01 11:19:23 -0700

    python_dependency: fix includes location on 10.9.
    Fixes #23837.
    Fixes #23866.

--- Library/Homebrew/requirements/python_dependency.rb
+++ Library/Homebrew/requirements/python_dependency.rb
@@ -142,8 +142,8 @@ class PythonDependency < Requirement
     if brewed?
       # Homebrew since a long while only supports frameworked python
       HOMEBREW_PREFIX/"opt/#{python}/Frameworks/Python.framework/Versions/#{version.major}.#{version.minor}"
-    elsif from_osx?
-      # Python on OS X has been stripped off its includes (unless you install the CLT), therefore we use the MacOS.sdk.
+    elsif from_osx? and MacOS.version < :mavericks
+      # Python on OS X before Mavericks has been stripped off its includes (unless you install the CLT), therefore we use the MacOS.sdk.
       Pathname.new("#{MacOS.sdk_path}/System/Library/Frameworks/Python.framework/Versions/#{version.major}.#{version.minor}")
     else
       # What Python knows about itself
