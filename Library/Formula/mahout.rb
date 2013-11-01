ac5165b46e73bbd6076dec79ff04daf2eb2406bf
Michael Andrews <michael@socialcast.com>
2013-11-01 10:19:04 -0700

    mahout:0.8 - address code review

--- Library/Formula/mahout.rb
+++ Library/Formula/mahout.rb
@@ -4,13 +4,10 @@ class Mahout < Formula
   homepage 'http://mahout.apache.org/'
   head 'http://svn.apache.org/repos/asf/mahout/trunk'
   url 'http://apache.cs.utah.edu/mahout/0.8/mahout-distribution-0.8.tar.gz'
-  sha1 '666fe711603670c316c4c9f4f5580a9d0caf2dff'
+  sha1 '67669fa4a8969a8b8c6ebb94fa7f5aeae96e9119'
+
   depends_on 'maven' => :build
   depends_on 'hadoop'
-
-  bottle do
-    sha1 '9dea60acc861eaacb31e980c4be01616f4761f44' => :mavericks
-  end
 
   def shim_script target
     <<-EOS.undent
@@ -20,14 +17,24 @@ class Mahout < Formula
   end
 
   def install
-    system 'chmod 755 ./bin'
-    system 'mvn -DskipTests clean install'
+    if build.head?
+      system 'chmod 755 ./bin'
+      system 'mvn -DskipTests clean install'
+    end
+
     libexec.install %w[bin]
-    libexec.install Dir['buildtools/target/*.jar']
-    libexec.install Dir['core/target/*.jar']
-    libexec.install Dir['examples/target/*.jar']
-    libexec.install Dir['math/target/*.jar']
+
+    if build.head?
+      libexec.install Dir['buildtools/target/*.jar']
+      libexec.install Dir['core/target/*.jar']
+      libexec.install Dir['examples/target/*.jar']
+      libexec.install Dir['math/target/*.jar']
+    else
+      libexec.install Dir['*.jar']
+    end
+
     bin.mkpath
+
     Dir["#{libexec}/bin/*"].each do |b|
       n = Pathname.new(b).basename
       (bin+n).write shim_script(n)
