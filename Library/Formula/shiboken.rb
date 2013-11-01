8aa473712cceb70f08127732077aeae34a6dbf4c
Brian Jensen <Jensen.J.Brian@gmail.com>
2013-11-01 17:51:20 +0100

    shiboken 1.2.1, fix build issue with Mavericks
    Closes #23867.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/shiboken.rb
+++ Library/Formula/shiboken.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Shiboken < Formula
   homepage 'http://www.pyside.org/docs/shiboken'
-  url 'https://download.qt-project.org/official_releases/pyside/shiboken-1.2.0.tar.bz2'
-  mirror 'https://distfiles.macports.org/py-shiboken/shiboken-1.2.0.tar.bz2'
-  sha1 '03866dbdfa34078b2d9d35f4b6d83aa65e292e3f'
+  url 'http://download.qt-project.org/official_releases/pyside/shiboken-1.2.1.tar.bz2'
+  mirror 'https://distfiles.macports.org/py-shiboken/shiboken-1.2.1.tar.bz2'
+  sha1 'f310ac163f3407109051ccebfd192bc9620e9124'
 
   head 'git://gitorious.org/pyside/shiboken.git'
 
@@ -12,6 +12,13 @@ class Shiboken < Formula
   depends_on :python => :recommended
   depends_on :python3 => :optional
   depends_on 'qt'
+
+  def patches
+    # This fixes issues with libc++ and its lack of the tr1 namespace.
+    # Upstream ticket: https://bugreports.qt-project.org/browse/PYSIDE-200
+    # Patch is currently under code review at: https://codereview.qt-project.org/#change,69324
+    DATA
+  end
 
   def install
     # This block will be run for each python (2.x and 3.x if requested)!
@@ -54,3 +61,26 @@ class Shiboken < Formula
     end
   end
 end
+
+__END__
+diff --git a/ext/sparsehash/google/sparsehash/sparseconfig.h b/ext/sparsehash/google/sparsehash/sparseconfig.h
+index 44a4dda..5073639 100644
+--- a/ext/sparsehash/google/sparsehash/sparseconfig.h
++++ b/ext/sparsehash/google/sparsehash/sparseconfig.h
+@@ -13,6 +13,16 @@
+     #define HASH_NAMESPACE stdext
+     /* The system-provided hash function including the namespace. */
+     #define SPARSEHASH_HASH  HASH_NAMESPACE::hash_compare
++/* libc++ does not implement the tr1 namespce, instead the
++ * equivalient functionality is placed in namespace std,
++ * so use when it targeting such systems (OS X 10.7 onwards) */
++#elif defined(_LIBCPP_VERSION)
++    /* the location of the header defining hash functions */
++    #define HASH_FUN_H <functional>
++    /* the namespace of the hash<> function */
++    #define HASH_NAMESPACE std
++    /* The system-provided hash function including the namespace. */
++    #define SPARSEHASH_HASH HASH_NAMESPACE::hash
+ #else
+     /* the location of the header defining hash functions */
+     #define HASH_FUN_H <tr1/functional>
