befa85f67eac9e68d2bd82b110025290e73662fd
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-11-01 10:52:01 +0100

    passenger 4.0.23
    Closes #23855.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/passenger.rb
+++ Library/Formula/passenger.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Passenger < Formula
   homepage 'https://www.phusionpassenger.com/'
-  url 'http://s3.amazonaws.com/phusion-passenger/releases/passenger-4.0.21.tar.gz'
-  sha1 '66b3b2b85283df2b7875d4dcc044dc1afb28a58b'
+  url 'http://s3.amazonaws.com/phusion-passenger/releases/passenger-4.0.23.tar.gz'
+  sha1 '09a76232adb2361fccd27634eede3e3af2e53ab4'
   head 'https://github.com/phusion/passenger.git'
 
   depends_on :macos => :lion
