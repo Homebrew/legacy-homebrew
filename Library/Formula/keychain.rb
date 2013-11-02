0cf6d390eabe729a0d16bd9daa551b232445613f
Steve Jabour <steve@jabour.me>
2013-11-02 12:55:15 -0600

    Updated Keychain Formula to Build from Repository I've updated the Keychain formula so that it builds from the offical Funtoo Github repository. This morning, the Funtoo website continually responded for requests to the archive with a 404.
    This change should be more reliable going forward.

--- Library/Formula/keychain.rb
+++ Library/Formula/keychain.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Keychain < Formula
   homepage 'http://www.funtoo.org/wiki/Keychain'
-  url 'https://github.com/funtoo/keychain.git', :using => :git,
+  url 'https://github.com/funtoo/keychain.git',
     :revision => '7bca870a1f920820adf0e4fa319b30b7e4985ed3'
   version '2.7.1'
   head 'https://github.com/funtoo/keychain.git'
