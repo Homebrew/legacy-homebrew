06fba7b3f4549183469d9ffa59d453915bdcb461
Lifepillar <github@lifepillar.com>
2013-11-02 21:55:31 +0100

    Remove patch for Ledger formula.
    The patch has been pushed upstream, so it is not needed any longer.

--- Library/Formula/ledger.rb
+++ Library/Formula/ledger.rb
@@ -26,12 +26,6 @@ class Ledger < Formula
   depends_on 'gmp'
   depends_on :python => :optional
 
-  # Fix HEAD to be compatible with Clang and libc++ on Mavericks.
-  # See: http://article.gmane.org/gmane.comp.finance.ledger.general/5268
-  if build.head? and MacOS.version >= :mavericks
-    def patches; DATA end
-  end
-
   def install
     opoo "Homebrew: Sorry, python bindings for --HEAD seem not to install. Help us fixing this!" if build.with? 'python'
 
@@ -65,31 +59,3 @@ class Ledger < Formula
     end
   end
 end
-
-__END__
-diff --git a/src/account.h b/src/account.h
-index daeee03..b726fac 100644
---- a/src/account.h
-+++ b/src/account.h
-@@ -310,7 +310,7 @@ void put_account(property_tree::ptree& pt, const account_t& acct,
-
- //simple struct added to allow std::map to compare accounts in the accounts report
- struct account_compare {
--  bool operator() (const account_t& lhs, const account_t& rhs){
-+  bool operator() (const account_t& lhs, const account_t& rhs) const {
-     return (lhs.fullname().compare(rhs.fullname()) < 0);
-   }
- };
-diff --git a/src/commodity.h b/src/commodity.h
-index a1988a3..d521d80 100644
---- a/src/commodity.h
-+++ b/src/commodity.h
-@@ -353,7 +353,7 @@ void put_commodity(property_tree::ptree& pt, const commodity_t& comm,
-
- //simple struct to allow std::map to compare commodities names
- struct commodity_compare {
--  bool operator() (const commodity_t* lhs, const commodity_t* rhs){
-+  bool operator() (const commodity_t* lhs, const commodity_t* rhs) const {
-     return (lhs->symbol().compare(rhs->symbol()) < 0);
-   }
- };
