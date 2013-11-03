bba16f0df034d1a605ffa0fe92810d4a687bc3f1
Jack Nagel <jacknagel@gmail.com>
2013-11-03 11:45:26 -0600

    Remove --use-gcc and friends from completion scripts

--- Library/Contributions/brew_fish_completion.fish
+++ Library/Contributions/brew_fish_completion.fish
@@ -137,9 +137,6 @@ complete -c brew -s f -l force -n '__fish_complete_brew_command force' -d "Insta
 complete -c brew -s i -l interactive -n '__fish_complete_brew_command install' -d "Open a subshell to install manually"
 complete -c brew -l git -n '__fish_complete_brew_command install' -d 'Create a git repo (useful for making patches)'
 complete -c brew -l fresh -n '__fish_complete_brew_command install' -d "Do not reuse options from previous installs"
-complete -c brew -l use-clang -n '__fish_complete_brew_command install' -d "Attempt to compile using Clang"
-complete -c brew -l use-llvm -n '__fish_complete_brew_command install' -d "Attempt to compile using LLVM"
-complete -c brew -l use-gcc -n '__fish_complete_brew_command install' -d "Attempt to compile using GCC"
 complete -c brew -l build-from-source -n '__fish_complete_brew_command install' -d "Compile from source even if a bottle is provided"
 complete -c brew -l devel -n '__fish_complete_brew_command install' -d "Install the development version"
 complete -c brew -l HEAD -n '__fish_complete_brew_command install' -d "Install the HEAD version"
