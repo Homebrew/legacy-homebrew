bd7f22c186154b8596445dcbe9ad7a99fef41033
Erik Thorselius <erik.thorslius@gmail.com>
2013-11-04 20:56:29 +0100

    Formula for worklog script.
    Worklog is a little program that you can log your time on different tasks.
    
    Changed to use mauvm release.
    Use the bin.install rename syntax.
    Followed ruby format.

---
+++ Library/Formula/worklog.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Worklog < Formula
+  homepage 'https://github.com/mauvm/worklog'
+  url 'https://raw.github.com/mauvm/worklog/v0.1/worklog.py'
+  sha1 'f035f7c9ab16318d72509e684e8a0ca5723403cc'
+  version '0.1'
+
+  def install
+    bin.install 'worklog.py' => 'worklog'
+  end
+end
