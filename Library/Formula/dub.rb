81ed32de822f205f4b35e41c33592c6781ff3d62
Brett Koonce <koonce@gmail.com>
2013-11-05 08:38:01 -0600

    dub v0.9.19
    Closes #23997.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/dub.rb
+++ Library/Formula/dub.rb
@@ -2,13 +2,15 @@ require 'formula'
 
 class Dub < Formula
   homepage 'http://registry.vibed.org/'
-  url  'https://github.com/rejectedsoftware/dub/archive/v0.9.18.tar.gz'
-  sha1 'a848d5b461eb95acc604d82de154922d3892888b'
+  url  'https://github.com/rejectedsoftware/dub/archive/v0.9.19.tar.gz'
+  sha1 'dcf880029190180a1a4a4753237c0eb164941c98'
 
   head 'https://github.com/rejectedsoftware/dub.git'
 
   depends_on 'pkg-config' => :build
   depends_on 'dmd'  => :build
+
+  def patches; DATA; end
 
   def install
     system "./build.sh"
@@ -19,3 +21,26 @@ class Dub < Formula
     system "#{bin}/dub; true"
   end
 end
+
+__END__
+diff --git a/build.sh b/build.sh
+index dce1766..eacc765 100755
+--- a/build.sh
++++ b/build.sh
+@@ -16,11 +16,11 @@ fi
+ LIBS=`pkg-config --libs libcurl 2>/dev/null || echo "-lcurl"`
+
+ # fix for modern GCC versions with --as-needed by default
+-if [ "$DC" = "dmd" ]; then
+-	LIBS="-l:libphobos2.a $LIBS"
+-elif [ "$DC" = "ldmd2" ]; then
+-	LIBS="-lphobos-ldc $LIBS"
+-fi
++# if [ "$DC" = "dmd" ]; then
++#     LIBS="-l:libphobos2.a $LIBS"
++# elif [ "$DC" = "ldmd2" ]; then
++#     LIBS="-lphobos-ldc $LIBS"
++# fi
+
+ # adjust linker flags for dmd command line
+ LIBS=`echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g'`
