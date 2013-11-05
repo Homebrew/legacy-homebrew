40ea11a166c5108da0b06ba6e965125fe92f869e
Dominique Orban <dominique.orban@gmail.com>
2013-11-05 18:33:26 -0500

    open-mpi: don't vend libevent headers
    Do not install the libevent header files that ship with Open-MPI.
    This resolves a conflict with libevent.
    
    Closes #24013.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/open-mpi.rb
+++ Library/Formula/open-mpi.rb
@@ -10,10 +10,15 @@ class OpenMpi < Formula
   option :cxx11
 
   conflicts_with 'mpich2', :because => 'both install mpi__ compiler wrappers'
-  conflicts_with 'libevent', :because => 'both install same set of header files'
   conflicts_with 'lcdf-typetools', :because => 'both install same set of binaries.'
 
   depends_on :fortran unless build.include? 'disable-fortran'
+
+  def patches
+    # Do not install the libevent header files.
+    # See http://www.open-mpi.org/community/lists/users/2013/11/22900.php
+    DATA
+  end
 
   def install
     ENV.cxx11 if build.cxx11?
@@ -46,3 +51,25 @@ class OpenMpi < Formula
     bin.write_jar_script libexec/'vtsetup.jar', 'vtsetup.jar'
   end
 end
+
+__END__
+
+diff --git a/opal/mca/event/libevent2021/libevent/include/Makefile.in b/opal/mca/event/libevent2021/libevent/include/Makefile.in
+index 99fb60b..5f7cad0 100644
+--- a/opal/mca/event/libevent2021/libevent/include/Makefile.in
++++ b/opal/mca/event/libevent2021/libevent/include/Makefile.in
+@@ -281,10 +281,10 @@ EVENT2_EXPORT = \
+ 	event2/util.h
+ 
+ EXTRA_SRC = $(EVENT2_EXPORT)
+-@INSTALL_LIBEVENT_TRUE@nobase_include_HEADERS = $(EVENT2_EXPORT)
+-@INSTALL_LIBEVENT_TRUE@nobase_nodist_include_HEADERS = ./event2/event-config.h
+-@INSTALL_LIBEVENT_FALSE@noinst_HEADERS = $(EVENT2_EXPORT)
+-@INSTALL_LIBEVENT_FALSE@nodist_noinst_HEADERS = ./event2/event-config.h
++#@INSTALL_LIBEVENT_TRUE@nobase_include_HEADERS = $(EVENT2_EXPORT)
++#@INSTALL_LIBEVENT_TRUE@nobase_nodist_include_HEADERS = ./event2/event-config.h
++#@INSTALL_LIBEVENT_FALSE@noinst_HEADERS = $(EVENT2_EXPORT)
++#@INSTALL_LIBEVENT_FALSE@nodist_noinst_HEADERS = ./event2/event-config.h
+ all: all-am
+ 
+ .SUFFIXES:
