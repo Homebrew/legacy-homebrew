b4b0ddf7de42402a3c3ae1eacf46b3b4b63cf97f
Bohdan Turkynewych <tb0hdan@gmail.com>
2013-11-05 12:32:05 +0200

    Added OpenSSL dependency for Monit

--- Library/Formula/monit.rb
+++ Library/Formula/monit.rb
@@ -5,10 +5,15 @@ class Monit < Formula
   url 'http://mmonit.com/monit/dist/monit-5.6.tar.gz'
   sha256 '38e09bd8b39abc59e6b9a9bb7a78f7eac2b02a92f4de1f3a6dc24e84dfedae0d'
 
+  # OpenSSL support
+  depends_on "openssl" => :build
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--localstatedir=#{var}/monit",
-                          "--sysconfdir=#{etc}/monit"
+                          "--sysconfdir=#{etc}/monit",
+                          "--with-ssl-incl-dir=#{HOMEBREW_PREFIX}/opt/openssl/include",
+                          "--with-ssl-lib-dir=#{HOMEBREW_PREFIX}/opt/openssl/lib"
     system "make install"
   end
 
