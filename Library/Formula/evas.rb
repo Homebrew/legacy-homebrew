57cd9a392aaa4e708fba8a210e5e99f56254cd6b
Sebastian Roth <sebastian.roth@gmail.com>
2013-11-06 22:48:27 +0800

    make `brew audit evas` pass

--- Library/Formula/evas.rb
+++ Library/Formula/evas.rb
@@ -6,7 +6,7 @@ class Evas < Formula
   sha1 'ce71de058896e80c8f1822d967a6dcee01a1c9ac'
 
   option 'with-docs', 'Install development libraries/headers and HTML docs'
-  
+
   head do
     url 'http://svn.enlightenment.org/svn/e/trunk/evas/'
 
@@ -22,16 +22,16 @@ class Evas < Formula
   depends_on 'fribidi'
   depends_on 'harfbuzz'
   depends_on 'doxygen' if build.include? 'with-docs'
-  
+
   def install
     system "./autogen.sh" if build.head?
-    
+
     args = ["--prefix=#{prefix}", "--disable-dependency-tracking"]
-    
+
     args << "--with-doxygen-file=#{HOMEBREW_PREFIX}/bin/doxygen" if build.include? 'with-docs'
-    
+
     system "./configure", *args
-                                                   
+
     system "make install"
     system "make install-doc" if build.include? 'with-docs'
   end
