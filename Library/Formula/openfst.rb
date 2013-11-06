2be2de372a1db52e79f6fd6abd52db9a307fd3e6
Kyle Gorman <kylebgorman@gmail.com>
2013-11-05 16:48:22 -0800

    OpenFst and OpenGrm-Ngram
    more style

--- Library/Formula/openfst.rb
+++ Library/Formula/openfst.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Openfst < Formula
-  homepage 'http://www.openfst.org/'
   url 'http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.3.4.tar.gz'
+  homepage 'http://www.openfst.org/'
   sha1 '21972c05896b2154a3fa1bdca5c9a56350194b38'
 
   option 'with-far', 'enable finite state transducer archive support needed for OpenGrm'
@@ -13,7 +13,6 @@ class Openfst < Formula
       ENV.append 'CXXFLAGS', "-I#{MacOS.sdk_path}/usr/include/c++/4.2.1"
       ENV.append 'LIBS', "#{MacOS.sdk_path}/usr/lib/libstdc++.dylib"
     end
-
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
     args << "--enable-far" if build.with? 'far'
     args << "--enable-pdt" if build.with? 'pdt'
--- Library/Formula/opengrm-ngram.rb
+++ Library/Formula/opengrm-ngram.rb
@@ -1,19 +1,17 @@
 require 'formula'
 
 class OpengrmNgram < Formula
-  depends_on 'openfst' => 'with-far'
-
-  homepage 'http://openfst.cs.nyu.edu/twiki/bin/view/GRM/NGramLibrary'
   url 'http://openfst.cs.nyu.edu/twiki/pub/GRM/NGramDownload/opengrm-ngram-1.1.0.tar.gz'
+  homepage 'http://openfst.cs.nyu.edu/twiki/bin/view/GRM/NGramLibrary'
   sha1 'a2ceeaf6ac129b66d2682d76a20388cf1d4b8c31'
 
-  def install
+  depends_on 'openfst' => 'with-far'
 
+  def install
     if MacOS.version > :mountain_lion
       ENV.append 'CXXFLAGS', "-I#{MacOS.sdk_path}/usr/include/c++/4.2.1"
       ENV.append 'LIBS', "#{MacOS.sdk_path}/usr/lib/libstdc++.dylib"
     end
-
     system "./configure", "--disable-dependency-tracking", 
                           "--prefix=#{prefix}"
     system "make install"
