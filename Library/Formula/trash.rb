e233dadfaf2ac0b1edb5800ebf575b0c448efb10
Adam Vandenberg <flangy@gmail.com>
2013-11-05 22:11:41 -0800

    add trash/osxutils conflict

--- Library/Formula/trash.rb
+++ Library/Formula/trash.rb
@@ -5,6 +5,8 @@ class Trash < Formula
   url 'http://github.com/hasseg/trash/archive/v0.8.3.zip'
   sha1 'de3489f7dbfe03dc490a3c7da22dba90998af3d1'
 
+  conflicts_with 'osxutils', :because => 'both install a trash binary'
+
   def install
     system "make"
     system "make docs"
