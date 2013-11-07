3270a64368df7c4b5a97edf29b79cf1798b5173c
Adam Vandenberg <flangy@gmail.com>
2013-11-07 06:55:47 -0800

    jam/ftjam conflict

--- Library/Formula/jam.rb
+++ Library/Formula/jam.rb
@@ -5,6 +5,8 @@ class Jam < Formula
   url 'ftp://ftp.perforce.com/jam/jam-2.5.zip'
   sha1 '794a3f4483315c6b9f010f03b592646d3815328c'
 
+  conflicts_with 'ftjam', :because => 'both install a `jam` binary'
+
   def install
     system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
     bin.install "bin.macosx/jam", "bin.macosx/mkjambase"
