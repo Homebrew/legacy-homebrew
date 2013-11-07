698efc9f1049ee019656512fb7f2fa8ad04d8a91
Jack Nagel <jacknagel@gmail.com>
2013-11-06 22:56:10 -0600

    cfengine: fix compilation on 10.9
    Fixes #23560.

--- Library/Formula/cfengine.rb
+++ Library/Formula/cfengine.rb
@@ -12,10 +12,12 @@ class Cfengine < Formula
   depends_on 'libxml2' if MacOS.version < :mountain_lion
 
   def patches
-    # Fix compilation errors. Both changes are merged upstream already
-    # https://github.com/cfengine/core/pull/947
-    # https://github.com/cfengine/core/commit/d03fcc2d38a4db0c79386aaef30597102bf45853
-    DATA
+    # Upstream patches for OS X compilation
+    %w{
+      https://github.com/cfengine/core/commit/f748a005b39a7aafd554e41a528b2216e28dce92.patch
+      https://github.com/cfengine/core/commit/d03fcc2d38a4db0c79386aaef30597102bf45853.patch
+      https://github.com/cfengine/core/commit/228f27002018a82b339ddfe6a5510a24128ce0ab.patch
+    }
   end
 
   def install
@@ -33,37 +35,3 @@ class Cfengine < Formula
     system "#{bin}/cf-agent", "-V"
   end
 end
-
-__END__
-diff --git a/cf-agent/verify_environments.c b/cf-agent/verify_environments.c
-index afb84ad..c59b599 100644
---- a/cf-agent/verify_environments.c
-+++ b/cf-agent/verify_environments.c
-@@ -282,13 +282,13 @@ static void VerifyEnvironments(EvalContext *ctx, Attributes a, Promise *pp)
-     {
-     case cfv_virt_vbox:
-     case cfv_virt_test:
--        VerifyVirtDomain(hyper_uri, envtype, a, pp);
-+        VerifyVirtDomain(ctx, hyper_uri, envtype, a, pp);
-         break;
-     case cfv_virt_xen_net:
-     case cfv_virt_kvm_net:
-     case cfv_virt_esx_net:
-     case cfv_virt_test_net:
--        VerifyVirtNetwork(hyper_uri, envtype, a, pp);
-+        VerifyVirtNetwork(ctx, hyper_uri, envtype, a, pp);
-         break;
-     default:
-         break;
-diff --git a/cf-agent/verify_files_utils.c b/cf-agent/verify_files_utils.c
-index 21f195c..ec4ba0d 100644
---- a/cf-agent/verify_files_utils.c
-+++ b/cf-agent/verify_files_utils.c
-@@ -2271,7 +2271,6 @@ static void VerifyCopiedFileAttributes(EvalContext *ctx, const char *src, const
-         if (!CopyFileExtendedAttributesDisk(src, dest))
-         {
-             cfPS(ctx, LOG_LEVEL_INFO, PROMISE_RESULT_FAIL, pp, attr, "Could not preserve extended attributes (ACLs and security contexts) on file '%s'", dest);
--            return NULL;
-         }
-     }
- }
