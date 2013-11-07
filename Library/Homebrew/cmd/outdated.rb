937da3eb408aca896fd062bd90e0a2c201fb38c7
Võ Anh Duy <voanhduy1512@live.com>
2013-11-07 22:13:56 +0700

    rollback outdated

--- Library/Homebrew/cmd/outdated.rb
+++ Library/Homebrew/cmd/outdated.rb
@@ -3,15 +3,11 @@ require 'keg'
 
 module Homebrew extend self
   def outdated
-    if outdated_brews.empty?
-      puts "Already up-to-date."
-    else
-      outdated_brews do |f, versions|
-        if $stdout.tty? and not ARGV.flag? '--quiet'
-          puts "#{f.name} (#{versions*', '} < #{f.version})"
-        else
-          puts f.name
-        end
+    outdated_brews do |f, versions|
+      if $stdout.tty? and not ARGV.flag? '--quiet'
+        puts "#{f.name} (#{versions*', '} < #{f.version})"
+      else
+        puts f.name
       end
     end
   end
