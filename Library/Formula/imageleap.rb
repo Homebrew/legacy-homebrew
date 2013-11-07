0e51d77133135aba4294d2ade95cbe85a68a8ec5
Dayton Nolan <daytonn@gmail.com>
2013-11-07 13:15:42 -0600

    Fix failing install test

--- Library/Formula/imageleap.rb
+++ Library/Formula/imageleap.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Imageleap < Formula
   homepage 'https://github.com/daytonn/imageleap'
-  url 'https://github.com/daytonn/imageleap/archive/v1.0.6.tar.gz'
-  sha1 'fa570157e205859208b3a2521d2b33e152cb4fc7'
+  url 'https://github.com/daytonn/imageleap/archive/v1.0.7.tar.gz'
+  sha1 'b5dfa3fae32ebdb4019292eda99d07389a9ed328'
 
   def install
     system "PREFIX='#{prefix}'"
