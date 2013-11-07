bfc0f91fd3721d8b8241a1232af9bae2c317f4fd
Jack Nagel <jacknagel@gmail.com>
2013-11-07 01:30:30 -0600

    loudmouth 1.5.0-20121201

--- Library/Formula/loudmouth.rb
+++ Library/Formula/loudmouth.rb
@@ -1,21 +1,15 @@
 require 'formula'
 
 class Loudmouth < Formula
-  homepage 'http://www.loudmouth-project.org/'
-  url 'http://mcabber.com/files/loudmouth-1.4.3-20111204.tar.bz2'
-  version '1.4.3.111204'
-  sha1 '38010a74d28fa06624b7461e515aec47c0ff140e'
+  homepage 'http://mcabber.com'
+  url 'http://mcabber.com/files/loudmouth-1.5.0-20121201.tar.bz2'
+  version '1.5.0.20121201'
+  sha1 '502963c3068f7033bb21d788918c1e5cd14f386e'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'gnutls'
   depends_on 'libidn'
-
-  # Fix compilation against newer glib. See:
-  # https://github.com/mxcl/homebrew/issues/12240
-  def patches
-    DATA
-  end
 
   def install
     system "./configure", "--disable-debug",
@@ -24,20 +18,3 @@ class Loudmouth < Formula
     system "make install"
   end
 end
-
-
-__END__
-diff --git a/loudmouth/lm-error.c b/loudmouth/lm-error.c
-index 103aaaf..74d3315 100644
---- a/loudmouth/lm-error.c
-+++ b/loudmouth/lm-error.c
-@@ -25,7 +25,7 @@
-  */
- 
- #include <config.h>
--#include <glib/gerror.h>
-+#include <glib.h>
- #include "lm-error.h"
- 
- GQuark
-
