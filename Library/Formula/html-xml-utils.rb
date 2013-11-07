81e74a6e3439c5041e1280a6137c33459df6b2c0
Jack Nagel <jacknagel@gmail.com>
2013-11-07 13:05:23 -0600

    html-xml-utils: fix build with clang

--- Library/Formula/html-xml-utils.rb
+++ Library/Formula/html-xml-utils.rb
@@ -5,18 +5,9 @@ class HtmlXmlUtils < Formula
   url 'http://www.w3.org/Tools/HTML-XML-utils/html-xml-utils-6.4.tar.gz'
   sha1 '6dc6ace41a4c3f692f79b16107d50d0e80d0ee40'
 
-  fails_with :clang do
-    build 425
-    cause <<-EOS.undent
-      Undefined symbols for architecture x86_64:
-        "_min", referenced from:
-            _write_index_item in hxindex.o
-      EOS
-  end
-
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    ENV.append 'CFLAGS', '-std=gnu89'
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
     ENV.j1 # install is not thread-safe
     system "make install"
