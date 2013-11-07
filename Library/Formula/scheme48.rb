b1c57827a1e33d7a52813820e7cb9b2cc85b7e34
Adam Vandenberg <flangy@gmail.com>
2013-11-06 21:09:20 -0800

    scsh/scheme48 conflict
    Closes #23059.

--- Library/Formula/scheme48.rb
+++ Library/Formula/scheme48.rb
@@ -5,6 +5,8 @@ class Scheme48 < Formula
   url 'http://s48.org/1.9/scheme48-1.9.tgz'
   sha1 '8fd78d328a8c39ecd848e849ade094c30c2bb4ba'
 
+  conflicts_with 'scsh', :because => 'both install include/scheme48.h'
+
   def install
     ENV.deparallelize
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
