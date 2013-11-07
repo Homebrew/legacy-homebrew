538766a52a25336bdf50ca85f463bb16ab9313bd
Adam Vandenberg <flangy@gmail.com>
2013-11-06 20:52:03 -0800

    clusterit/couchdb-lucene conflict

--- Library/Formula/clusterit.rb
+++ Library/Formula/clusterit.rb
@@ -5,6 +5,8 @@ class Clusterit < Formula
   url 'http://downloads.sourceforge.net/project/clusterit/clusterit/clusterit-2.5/clusterit-2.5.tar.gz'
   sha1 '65d36116665179dd16029ac53182fde92d868020'
 
+  conflicts_with 'couchdb-lucene', :because => 'both install a `run` binary'
+
   depends_on :x11
 
   def install
