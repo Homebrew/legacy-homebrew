be2d909fce93bf58be734b04c6c32caabaa2dc0f
Sun Liwen <sunliwen@gmail.com>
2013-11-07 22:43:07 +0800

    activemq 5.9.0
    Closes #24057.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Activemq < Formula
   homepage 'http://activemq.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=activemq/apache-activemq/5.8.0/apache-activemq-5.8.0-bin.tar.gz'
-  sha1 '426150ee260d3ab57e9c8674944ca7d171646056'
+  url 'http://www.apache.org/dyn/closer.cgi?path=activemq/apache-activemq/5.9.0/apache-activemq-5.9.0-bin.tar.gz'
+  sha1 'f82be4b8fcac37ad0e5598db7f62083806e85d85'
 
   skip_clean 'libexec/webapps/admin/WEB-INF/jsp'
 
