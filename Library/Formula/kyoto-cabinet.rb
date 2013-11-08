f35997a51ca3dc424fc4613db949dc87dadeb73d
Dan Crosta <dcrosta@late.am>
2013-11-08 09:38:38 -0500

    make kyoto-cabinet and kyoto-tycoon build with clang on mavericks

--- Library/Formula/kyoto-cabinet.rb
+++ Library/Formula/kyoto-cabinet.rb
@@ -18,4 +18,22 @@ class KyotoCabinet < Formula
     system "make" # Separate steps required
     system "make install"
   end
+
+  def patches
+    DATA
+  end
 end
+
+
+__END__
+--- a/kccommon.h  2013-11-08 09:27:37.000000000 -0500
++++ b/kccommon.h  2013-11-08 09:27:47.000000000 -0500
+@@ -82,7 +82,7 @@
+ using ::snprintf;
+ }
+
+-#if __cplusplus > 199711L || defined(__GXX_EXPERIMENTAL_CXX0X__) || defined(_MSC_VER)
++#if __cplusplus > 199711L || defined(__GXX_EXPERIMENTAL_CXX0X__) || defined(_MSC_VER) || defined(_LIBCPP_VERSION)
+
+ #include <unordered_map>
+ #include <unordered_set>
