cb6d8adb85e218c55432057bb16634e1f23f45d7
Yehor Lvivski <lvivski@gmail.com>
2013-11-08 22:43:13 +0200

    dart 30107
    Closes #24095.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -4,14 +4,14 @@ class Dart < Formula
   homepage 'http://www.dartlang.org/'
 
   if MacOS.prefer_64_bit?
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/29341/dartsdk-macos-64.zip'
-    sha1 '4deec77f9b6a3cd495115384d2e5806baaca0516'
+    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/30107/sdk/dartsdk-macos-x64-release.zip'
+    sha1 'bb1e6c1f4f75f37bbca767127dba95d3a051e72d'
   else
-    url 'https://gsdview.appspot.com/dart-editor-archive-integration/29341/dartsdk-macos-32.zip'
-    sha1 'fe0fa1e27ecff186a2a8616f6eecbf10649776b7'
+    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/30107/sdk/dartsdk-macos-ia32-release.zip'
+    sha1 '772041f056154a740b8608a8723e8e14a328be6f'
   end
 
-  version '29341'
+  version '30107'
 
   def install
     libexec.install Dir['*']
@@ -27,15 +27,11 @@ class Dart < Formula
 
   test do
     (testpath/'sample.dart').write <<-EOS.undent
-      import 'dart:io';
       void main() {
-        Options opts = new Options();
-        for (String arg in opts.arguments) {
-          print(arg);
-        }
+        print(r"test message");
       }
     EOS
 
-    assert_equal "test\nmessage\n", `#{bin}/dart sample.dart test message`
+    assert_equal "test message\n", `#{bin}/dart sample.dart`
   end
 end
