de7e44e3cc3d36de8ae21ae819ca794eeea4b80b
Jack Nagel <jacknagel@gmail.com>
2013-11-09 00:37:00 -0600

    lftp: allow superenv
    This seems to be fixed.

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -6,9 +6,6 @@ class Lftp < Formula
   mirror 'ftp://ftp.cs.tu-berlin.de/pub/net/ftp/lftp/lftp-4.4.10.tar.bz2'
   sha1 '0fc98664e572256f280a5b57d279d3b415a87fce'
 
-  # https://github.com/mxcl/homebrew/issues/18749
-  env :std
-
   depends_on 'pkg-config' => :build
   depends_on 'readline'
   depends_on 'gnutls'
