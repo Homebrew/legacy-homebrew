99c8d17f683483a9d14dcdc4556a7d0430eef8db
Bulat Shakirzyanov <bulat@twilio.com>
2013-11-08 21:46:09 -0800

    incorporate fixes

--- Library/Formula/mesos.rb
+++ Library/Formula/mesos.rb
@@ -1,8 +1,29 @@
 require 'formula'
 
+class Java7Requirement < Requirement
+  fatal true
+
+  def java_home_cmd
+    "/usr/libexec/java_home -v '1.7'"
+  end
+
+  satisfy :build_env => false do
+    system *java_home_cmd.split(" ")
+    $?.success?
+  end
+
+  def message; <<-EOS.undent
+    Couldn't locate JDK7, here is how to fix it:
+
+      1. Download and install JDK7 from http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html
+      2. Verify that it has been installed by running #{java_home_cmd}
+      3. Re-run brew install mesos
+    EOS
+  end
+end
+
 class Mesos < Formula
   homepage 'http://mesos.apache.org'
-  version '0.14.1'
   url 'http://www.webhostingjams.com/mirror/apache/mesos/0.14.1/mesos-0.14.1.tar.gz'
   sha1 '0b8e7ebd9c8a28f073b955f7229c5a28ee2d7120'
 
@@ -14,6 +35,18 @@ class Mesos < Formula
     cause 'multiple configure and compile errors'
   end
 
+  depends_on Java7Requirement
+
+  def install
+    system "./configure", "--disable-debug",
+                           "--disable-dependency-tracking",
+                           "--disable-silent-rules",
+                           "--prefix=#{prefix}"
+
+    system "make"
+    system "make", "install"
+  end
+
   def caveats
     <<-EOS.undent
       Mesos doesn't install on clang or apple-gcc-4.2.
@@ -23,32 +56,5 @@ class Mesos < Formula
         1. brew install gcc46
         2. brew install mesos --cc=gcc-4.6
     EOS
-  end
-
-  def install
-    java_home = `/usr/libexec/java_home -v '1.7'`.chomp
-    unless $?.success?
-      puts <<-MSG
-
-Couldn't locate JDK7, here is how to fix it:
-
-  1. Download and install JDK7 from http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html
-  2. Verify that it has been installed by running /usr/libexec/java_home -v '1.7'
-  3. Re-run brew install mesos
-
-      MSG
-    end
-
-    system "./configure", "--disable-debug",
-                           "--disable-dependency-tracking",
-                           "--disable-silent-rules",
-                           "--prefix=#{prefix}"
-
-    system "make"
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    system "test", "-x", "#{bin}/mesos-local"
   end
 end
