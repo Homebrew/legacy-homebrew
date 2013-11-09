1192d712d4cdb6798e3f9123c7ff6e3a77140b40
xaizek <xaizek@openmailbox.org>
2013-11-09 21:13:32 +0200

    vifm 0.7.6
    Lion dependency is removed as vifm doesn't use `wcscasecmp` and
    `wcsncasecmp` functions anymore.
    
    Closes #24129.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/vifm.rb
+++ Library/Formula/vifm.rb
@@ -2,17 +2,8 @@ require 'formula'
 
 class Vifm < Formula
   homepage 'http://vifm.sourceforge.net/index.html'
-  url 'http://downloads.sourceforge.net/project/vifm/vifm/vifm-0.7.5.tar.bz2'
-  sha1 '202b369b45d741e32a50084d902c4dcc33014915'
-
-  # This actually depends on Xcode 4.2 or newer, not Lion per se, as it will
-  # work on Snow Leopard running Xcode 4.2. This software uses the string
-  # functions `wcscasecmp` and `wcsncasecmp`, which were not present in
-  # earlier compiler sets.
-  # Note that someone could add implementations of these to str.c in this
-  # software and likely get it to compile across versions, if someone had the
-  # energy to attempt this.
-  depends_on :macos => :lion
+  url 'http://downloads.sourceforge.net/project/vifm/vifm/vifm-0.7.6.tar.bz2'
+  sha1 '2c9a57ec80a0c389c2807b5e86f25c2b8dd1c0cd'
 
   def install
     system "./configure", "--disable-dependency-tracking",
