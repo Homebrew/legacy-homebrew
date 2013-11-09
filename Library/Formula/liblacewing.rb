eacb7b0624cf9a6d44366b7ae8e36224e4c73187
James McLaughlin <james@lacewing-project.org>
2013-11-09 18:39:24 +0000

    liblacewing 0.5.4

--- Library/Formula/liblacewing.rb
+++ Library/Formula/liblacewing.rb
@@ -3,14 +3,11 @@ require 'formula'
 class Liblacewing < Formula
   homepage 'http://lacewing-project.org/'
   head 'https://github.com/udp/lacewing.git'
-  url 'https://github.com/udp/lacewing/archive/0.5.3.tar.gz'
-  sha1 'dd408716ef14630ef6636aa47dd1150c44615e8d'
-
-  # Use a newer OpenSSL to get SPDY support
-  depends_on 'openssl'
+  url 'https://github.com/udp/lacewing/archive/0.5.4.tar.gz'
+  sha1 '078486a4dcd6ce33c2c881954c5dc82843411ac9'
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-ssl", "--disable-spdy"
     system "make"
     system "make install"
   end
