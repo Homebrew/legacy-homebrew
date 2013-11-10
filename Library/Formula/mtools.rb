5560aff3366ddc112b01a5a5bedd623fb172d93c
Adam Vandenberg <flangy@gmail.com>
2013-11-09 22:43:35 -0800

    mtools/multimarkdown conflict

--- Library/Formula/mtools.rb
+++ Library/Formula/mtools.rb
@@ -6,6 +6,8 @@ class Mtools < Formula
   mirror 'http://ftp.gnu.org/gnu/mtools/mtools-4.0.17.tar.gz'
   sha1 'eebfab51148c4ab20a6aca3cea8057da5a11bdc8'
 
+  conflicts_with 'multimarkdown', :because => 'both install `mmd` binaries'
+
   depends_on :x11 => :optional
 
   def install
