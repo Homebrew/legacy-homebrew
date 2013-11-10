e0c29b13699663e13e0376716abb16ff74db001c
Adam Vandenberg <flangy@gmail.com>
2013-11-09 22:43:35 -0800

    mtools/multimarkdown conflict

--- Library/Formula/multimarkdown.rb
+++ Library/Formula/multimarkdown.rb
@@ -2,17 +2,16 @@ require 'formula'
 
 class Multimarkdown < Formula
   homepage 'http://fletcherpenney.net/multimarkdown/'
-
-  # Use the tag instead of the tarball to get submodules
+  head 'https://github.com/fletcher/MultiMarkdown-4.git', :branch => 'master'
+  # Use git tag instead of the tarball to get submodules
   url 'https://github.com/fletcher/MultiMarkdown-4.git', :tag => '4.3.1'
 
-  head 'https://github.com/fletcher/MultiMarkdown-4.git', :branch => 'master'
+  conflicts_with 'mtools', :because => 'both install `mmd` binaries'
 
   def install
     ENV.append 'CFLAGS', '-g -O3 -include GLibFacade.h'
     system "make"
-    bin.install 'multimarkdown'
-    bin.install Dir['scripts/*']
+    bin.install 'multimarkdown', Dir['scripts/*']
     prefix.install 'Support'
   end
 
