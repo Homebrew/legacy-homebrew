3d856061aab617f68f99e96258bd80bcd5189b96
Brett Koonce <koonce@gmail.com>
2013-11-09 23:22:22 -0600

    proxytunnel: disable strlcat/strlcpy on mavericks
    Fixes #24138.
    Closes #24144.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/proxytunnel.rb
+++ Library/Formula/proxytunnel.rb
@@ -6,6 +6,12 @@ class Proxytunnel < Formula
   sha1 '51d816125bb9e9bca267d35f861000eb0fa9d80b'
 
   def install
+    if MacOS.version >= :mavericks
+      # http://trac.macports.org/browser/trunk/dports/net/proxytunnel/Portfile
+      inreplace ["proxytunnel.h", "setproctitle.c", "strlcat.c", "strzcat.c"], "strlcat", "strlcat_disable"
+      inreplace ["proxytunnel.h", "setproctitle.c", "strlcpy.c"], "strlcpy", "strlcpy_disable"
+    end
+
     system "make"
     bin.install "proxytunnel"
     man1.install "proxytunnel.1"
