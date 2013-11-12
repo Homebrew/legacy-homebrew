b3768ec000321df2162f6c4d29a9485e2225bdca
Jack Nagel <jacknagel@gmail.com>
2013-11-11 19:05:47 -0600

    orc 0.4.18

--- Library/Formula/orc.rb
+++ Library/Formula/orc.rb
@@ -2,15 +2,10 @@ require 'formula'
 
 class Orc < Formula
   homepage 'http://code.entropywave.com/projects/orc/'
-  url 'http://code.entropywave.com/download/orc/orc-0.4.17.tar.gz'
-  sha1 '5cb7b3225a23bc4a5771a62e9c94a90d21609632'
+  url 'http://code.entropywave.com/download/orc/orc-0.4.18.tar.gz'
+  sha1 '0119bec2291c878a7953cb041dd080856c986ccf'
 
   def install
-    # Fix compiling on 32 bit systems. See:
-    # https://trac.macports.org/ticket/26881
-    # https://github.com/mxcl/homebrew/issues/8848
-    ENV["CFLAGS"] = "-Xarch_i386 -O1" #if Hardware.is_32_bit?
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-gtk-doc"
