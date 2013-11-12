6bc272a1d49160c2a852a8f7cb55bcd88992c1be
blob8108 <blob8108@gmail.com>
2013-11-12 11:55:18 +0000

    SmartSim: 1.2.1 - digital logic simulator.
    Remove failing test; switch to an older version of the package.

--- Library/Formula/smartsim.rb
+++ Library/Formula/smartsim.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Smartsim < Formula
   homepage 'http://smartsim.org.uk'
-  url 'https://github.com/ashleynewson/SmartSim.git', :revision => '7bab4542f5'
-  version '1.3.2'
+  url 'https://github.com/ashleynewson/SmartSim/archive/v1.2.1.tar.gz'
+  sha1 'ee3e5b4b4d7615ebbd9a1e944d0e506e697e7da0'
+  head 'https://github.com/ashleynewson/smartsim'
 
   depends_on :x11
-  depends_on 'gtk+3'
+  depends_on 'gtk+'
   depends_on 'librsvg'
   depends_on 'libxml2'
   depends_on 'glib'
-  depends_on 'cairo'
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
 
@@ -20,9 +20,5 @@ class Smartsim < Formula
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
-  end
-
-  test do
-    system "false"
   end
 end
