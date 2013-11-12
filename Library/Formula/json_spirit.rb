407b2a9f6807d6a008faf17dbce4c17979af884b
Francisco Facioni <fran6co@gmail.com>
2013-11-12 16:43:32 -0300

    json_spirit 4.07
    Closes #24227.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/json_spirit.rb
+++ Library/Formula/json_spirit.rb
@@ -2,14 +2,17 @@ require 'formula'
 
 class JsonSpirit < Formula
   homepage 'http://www.codeproject.com/KB/recipes/JSON_Spirit.aspx'
-  url 'https://uwe-arzt.de/files/json_spirit_v4.04.zip'
-  sha1 '5e92f0b337c43104faaf23f082d4c6763986bdd1'
+  url 'https://github.com/png85/json_spirit/archive/json_spirit-4.07.zip'
+  sha1 'e7055cb8fd596fc89b73e6898d8162a56fd80ec7'
 
   depends_on 'boost'
   depends_on 'cmake' => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
+    args = std_cmake_args
+    args << "-DBUILD_STATIC_LIBRARIES=ON"
+
+    system "cmake", *args
     system "make install"
   end
 end
