3d135708241f9951567f1e911a93d79eb7799c87
Robert Lillack <rob@burningsoda.com>
2013-11-13 21:38:08 +0100

    monoberry: update to 0.2.0

--- Library/Formula/monoberry.rb
+++ Library/Formula/monoberry.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Monoberry < Formula
   homepage 'http://burningsoda.com/software/monoberry/'
-  url 'https://github.com/roblillack/monoberry/releases/download/0.1.0/monoberry-0.1.0.tgz'
-  sha1 '733598dd918afd0f34947e9b0db66d25f9f4619e'
+  url 'https://github.com/roblillack/monoberry/releases/download/0.2.0/monoberry-0.2.0.tgz'
+  sha1 '223c886cc8ed79dd8e21c77760b78f659ede5b8c'
 
   def install
     (share/'monoberry').install "target/lib", "target/target", "target/tool"
