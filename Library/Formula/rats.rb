96d66402e8590d15967a03571d2cddd9828dfd28
denji <denji@localhost>
2013-11-13 06:14:00 +0200

    rat: clear code

--- Library/Formula/rats.rb
+++ Library/Formula/rats.rb
@@ -5,18 +5,14 @@ class Rats < Formula
   url 'https://rough-auditing-tool-for-security.googlecode.com/files/rats-2.3.tar.gz'
   sha1 '02283f2a5f0482f09c7e33d2aa84d68c48a8ec5a'
 
-  #depends_on 'expat' => :optional
-
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--infodir=#{info}"
-    #                      "--with-expat-lib=path",
-    #                      "--with-expat-include=path"
     system "make install"
   end
 
   test do
-    system "rats"
+    system "#{bin}/rats"
   end
 end
