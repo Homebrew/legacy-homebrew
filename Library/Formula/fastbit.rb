04aabea62ed51169ae3c471700c828eed57a4339
Adam Vandenberg <flangy@gmail.com>
2013-11-12 22:18:03 -0800

    fastbit/pam_yubico conflict

--- Library/Formula/fastbit.rb
+++ Library/Formula/fastbit.rb
@@ -5,7 +5,8 @@ class Fastbit < Formula
   url 'https://codeforge.lbl.gov/frs/download.php/409/fastbit-ibis1.3.8.tar.gz'
   sha1 '3e0feed7932d34be49ca41fdb689f55b0466a28a'
 
-  conflicts_with 'salt', :because => 'both install `include/filter.h`'
+  conflicts_with 'fastbit', :because => 'Both install `include/util.h`'
+  conflicts_with 'salt', :because => 'Both install `include/filter.h`'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
