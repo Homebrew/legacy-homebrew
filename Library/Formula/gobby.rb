6db58c5a26a75ca23d7067365f6674f78d12bbfe
Adam Vandenberg <flangy@gmail.com>
2013-11-13 21:33:43 -0800

    Redact libgsasl
    The gsasl formula is a superset, so just carry one not both.

--- Library/Formula/gobby.rb
+++ Library/Formula/gobby.rb
@@ -10,7 +10,7 @@ class Gobby < Formula
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
   depends_on 'gtkmm'
-  depends_on 'libgsasl'
+  depends_on 'gsasl'
   depends_on 'libxml++'
   depends_on 'gtksourceview'
   depends_on 'gettext'
