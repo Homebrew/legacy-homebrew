0fffa420f6679aa7fddf1bdf7116caafa0ae81b4
Colin Wood <cwood06@gmail.com>
2013-11-13 21:28:47 -0500

    Package URL changed
    It looks like the package url is down including the home page. Not sure if the owner removed it or what. However, fedora does have a tar file that will install.

--- Library/Formula/axel.rb
+++ Library/Formula/axel.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Axel < Formula
   homepage 'http://axel.alioth.debian.org'
-  url 'http://alioth.debian.org/frs/download.php/file/3016/axel-2.4.tar.bz2'
-  sha1 '9e212e2890a678ccb2ab48f575a659a32d07b1a9'
+  url 'http://pkgs.fedoraproject.org/repo/pkgs/axel/axel-2.4.tar.gz/a2a762fce0c96781965c8f9786a3d09d/axel-2.4.tar.gz'
+  sha1 '6d89a7ce797ddf4c23a210036d640d013fe843ca'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--debug=0", "--i18n=0"
