f18af7d8d8997137abfe1135bf1e65b999a4c59b
Yehor Lvivski <lvivski@gmail.com>
2013-11-14 12:49:43 +0200

    dart 1.0 release (rev30188)
    Closes #24291.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -4,14 +4,14 @@ class Dart < Formula
   homepage 'http://www.dartlang.org/'
 
   if MacOS.prefer_64_bit?
-    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/30107/sdk/dartsdk-macos-x64-release.zip'
-    sha1 'bb1e6c1f4f75f37bbca767127dba95d3a051e72d'
+    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/30188/sdk/dartsdk-macos-x64-release.zip'
+    sha1 '1488243e69a12194625a8ef483cfd3614f7e14f0'
   else
-    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/30107/sdk/dartsdk-macos-ia32-release.zip'
-    sha1 '772041f056154a740b8608a8723e8e14a328be6f'
+    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/30188/sdk/dartsdk-macos-ia32-release.zip'
+    sha1 '59b7119ad7db5823c5959c57bd0943a7428430db'
   end
 
-  version '30107'
+  version '30188'
 
   def install
     libexec.install Dir['*']
