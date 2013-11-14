2614d9a793a899820bd19129b949a6311abb6e15
Pavel Penkov <ebonfortress@gmail.com>
2013-11-14 12:47:27 +0400

    giter8 0.6.0
    Closes #24287.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/giter8.rb
+++ Library/Formula/giter8.rb
@@ -4,7 +4,7 @@ class Giter8 < Formula
   homepage 'https://github.com/n8han/giter8'
   url "http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.0/sbt-launch.jar"
   sha1 'b78a29b8db7ae0fc90a7890a84a4617975e1d450'
-  version '0.5.3'
+  version '0.6.0'
 
   def exec_script
      <<-EOS
@@ -16,7 +16,7 @@ exec java -Xmx512M -jar #{prefix}/sbt-launch.jar @giter8.launchconfig "$@"
   def config_script
     <<-EOS
 [app]
-  version: 0.5.3
+  version: 0.6.0
   org: net.databinder.giter8
   name: giter8
   class: giter8.Giter8
