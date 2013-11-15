3bb666b3d04dd26e11a15367764edfdd441a087f
Jack Nagel <jacknagel@gmail.com>
2013-11-15 17:17:29 -0600

    sispmctl: fix dependency

--- Library/Formula/sispmctl.rb
+++ Library/Formula/sispmctl.rb
@@ -5,12 +5,11 @@ class Sispmctl < Formula
   url 'http://downloads.sourceforge.net/project/sispmctl/sispmctl/sispmctl-3.1/sispmctl-3.1.tar.gz'
   sha1 'f89d2820ca48794b80df81309910299dbc1278e1'
 
-  depends_on 'libusb'
+  depends_on 'libusb-compat'
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-
     system "make install"
   end
 end
