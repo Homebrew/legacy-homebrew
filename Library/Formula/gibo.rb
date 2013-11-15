71690c806e36812b5fadde131286a9799b1c234e
Simon Whitaker <simon@goosoftware.co.uk>
2013-11-15 09:12:29 +0000

    gibo 1.0.2
    Closes #24340.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gibo.rb
+++ Library/Formula/gibo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gibo < Formula
   homepage 'https://github.com/simonwhitaker/gibo'
-  url 'https://github.com/simonwhitaker/gibo/archive/1.0.1.tar.gz'
-  sha1 '06923dcdfd8052e7cfe2d4806b51d1b78b229bfa'
+  url 'https://github.com/simonwhitaker/gibo/archive/1.0.2.tar.gz'
+  sha1 '58c809c99841975a00fbdc540af4b3c478b95c7b'
 
   def install
     bin.install "gibo"
