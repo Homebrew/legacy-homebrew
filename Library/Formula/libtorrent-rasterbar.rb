c1787d1b5573f64ff26a5b3200386b283ca226ef
Brett Koonce <koonce@gmail.com>
2013-11-15 13:41:12 -0600

    libtorrent-rasterbar 0.16.12
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class LibtorrentRasterbar < Formula
   homepage 'http://www.rasterbar.com/products/libtorrent/'
-  url 'https://libtorrent.googlecode.com/files/libtorrent-rasterbar-0.16.11.tar.gz'
-  sha1 'accf70b2ddc5ab048a35c6dc3ef20718a67833f5'
+  url 'https://libtorrent.googlecode.com/files/libtorrent-rasterbar-0.16.12.tar.gz'
+  sha1 'cd326fd2609c3d00c25dabdd441a0704efbf0207'
 
   depends_on 'pkg-config' => :build
   depends_on 'boost'
