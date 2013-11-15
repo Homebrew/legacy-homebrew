93f8379116ea5c0786e01099f498f7e2f5bf564c
Jonathan Barronville <jonathan@belairlabs.com>
2013-11-14 21:53:58 -0500

    `option` goes below `url`

--- Library/Formula/adobe-air-sdk.rb
+++ Library/Formula/adobe-air-sdk.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class AdobeAirSdk < Formula
   homepage 'http://adobe.com/products/air/sdk'
-  option 'with-compiler', 'Grab the version with the new compiler (for non-Flex users).'
   sha1 '715da9ad8f3bc7a61dcc54835084cbc7b9a92d66'
   url 'http://airdownload.adobe.com/air/mac/download/3.9/AdobeAIRSDK.tbz2'
+  option 'with-compiler', 'Grab the version with the new compiler (for non-Flex users).'
 
   if build.with? 'compiler'
     sha1 '1334fad165bab05f3abe0579ed1776e58c8da43e'
