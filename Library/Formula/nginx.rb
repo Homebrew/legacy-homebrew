6e10e574090db324981b6451b14f393e2e189206
Matt Robenolt <m@robenolt.com>
2013-11-15 09:36:39 -0800

    Add support for compiling nginx with the mail module

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -19,6 +19,7 @@ class Nginx < Formula
   option 'with-debug', 'Compile with support for debug log'
   option 'with-spdy', 'Compile with support for SPDY module'
   option 'with-gunzip', 'Compile with support for gunzip module'
+  option 'with-mail', 'Compile with support for mail module'
 
   depends_on 'pcre'
   depends_on 'passenger' => :optional
@@ -80,6 +81,7 @@ class Nginx < Formula
     args << "--with-debug" if build.include? 'with-debug'
     args << "--with-http_spdy_module" if build.include? 'with-spdy'
     args << "--with-http_gunzip_module" if build.include? 'with-gunzip'
+    args << "--with-mail" if build.include? 'with-mail'
 
     if build.head?
       system "./auto/configure", *args
