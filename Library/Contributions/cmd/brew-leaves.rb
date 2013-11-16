9433b0e1b921e28543796caf9f98ef2bf5451aef
Jack Nagel <jacknagel@gmail.com>
2013-11-16 14:10:23 -0600

    Make brew-leaves respect optional deps

--- Library/Contributions/cmd/brew-leaves.rb
+++ Library/Contributions/cmd/brew-leaves.rb
@@ -4,11 +4,26 @@
 
 require 'formula'
 require 'set'
+require 'tab'
 
+installed = Formula.installed
 deps_of_installed = Set.new
 
-Formula.installed.each do |f|
-  deps_of_installed.merge f.deps.map(&:name)
-end.each do |f|
+installed.each do |f|
+  deps = []
+
+  f.deps.each do |dep|
+    if dep.optional? || dep.recommended?
+      tab = Tab.for_formula(f)
+      deps << dep.name if tab.with?(dep.name)
+    else
+      deps << dep.name
+    end
+  end
+
+  deps_of_installed.merge(deps)
+end
+
+installed.each do |f|
   puts f.name unless deps_of_installed.include? f.name
 end
