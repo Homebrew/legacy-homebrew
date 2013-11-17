cd2ef55e797ad71ea837a5ad1b5cb764e4db18d8
Mike McQuaid <mike@mikemcquaid.com>
2013-11-17 09:41:41 +0000

    brew-bundle: cleanup path handling.

--- Library/Contributions/cmd/brew-bundle.rb
+++ Library/Contributions/cmd/brew-bundle.rb
@@ -4,28 +4,31 @@
 #
 # Looks for a Brewfile and runs each line as a brew command.
 #
-# brew bundle              # Looks for "./brewfile"
-# brew bundle path/to/dir  # Looks for "./path/to/dir/brewfile"
+# brew bundle              # Looks for "./Brewfile"
+# brew bundle path/to/dir  # Looks for "./path/to/dir/Brewfile"
 # brew bundle path/to/file # Looks for "./path/to/file"
 #
 # For example, given a Brewfile with the following contents:
-#
 # tap foo/bar
 # install spark
 #
 # Running `brew bundle` will run the commands `brew tap foo/bar`
 # and `brew install spark`.
 
-if ARGV.empty? then
-	path = 'brewfile'
-else
-	path = ARGV[0]
-	if File.directory? path then
-		path = path + '/brewfile'
-	end
+path = 'Brewfile'
+error = ' in current directory'
+
+if ARGV.first
+  if File.directory? ARGV.first
+    path = "#{ARGV.first}/#{path}"
+    error = " in '#{ARGV.first}'"
+  else
+    path = ARGV.first
+    error = " at '#{ARGV.first}'"
+  end
 end
 
-raise "No such Brewfile: #{path}" unless File.exist? path
+raise "Cannot find Brewfile#{error}" unless File.exist? path
 
 File.readlines(path).each do |line|
   command = line.chomp
