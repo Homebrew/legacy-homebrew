d1cd236e75572711fce4613b9c4316b679cb8c12
Adam Vandenberg <flangy@gmail.com>
2013-11-17 10:34:19 -0800

    add lib/dtrace to non-owned directories

--- Library/Homebrew/keg.rb
+++ Library/Homebrew/keg.rb
@@ -139,6 +139,7 @@ class Keg < Pathname
       # pkg-config database gets explicitly created
       when 'pkgconfig' then :mkpath
       # lib/language folders also get explicitly created
+      when 'dtrace' then :mkpath
       when /^gdk-pixbuf/ then :mkpath
       when 'ghc' then :mkpath
       when 'lua' then :mkpath
