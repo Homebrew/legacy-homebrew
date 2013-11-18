99763da586e93f68114f2702a9b97ce089efc20c
Jack Nagel <jacknagel@gmail.com>
2013-11-18 09:47:26 -0600

    gtksourceview3 3.10.1

--- Library/Formula/gtksourceview3.rb
+++ Library/Formula/gtksourceview3.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gtksourceview3 < Formula
   homepage 'http://projects.gnome.org/gtksourceview/'
-  url 'http://ftp.acc.umu.se/pub/gnome/sources/gtksourceview/3.10/gtksourceview-3.10.0.tar.xz'
-  sha1 '96136aa8b0f133e49269d4c71341fceff32898f7'
+  url 'http://ftp.acc.umu.se/pub/gnome/sources/gtksourceview/3.10/gtksourceview-3.10.1.tar.xz'
+  sha1 '195da95964eedf16355c97f1ca5ae99c24ca398b'
 
   depends_on :x11
   depends_on 'pkg-config' => :build
