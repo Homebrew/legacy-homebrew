55a4d60bb8ecf88686e3896177be030348b71869
Jack Nagel <jacknagel@gmail.com>
2013-11-18 10:16:53 -0600

    mg 20131118

--- Library/Formula/mg.rb
+++ Library/Formula/mg.rb
@@ -2,18 +2,13 @@ require 'formula'
 
 class Mg < Formula
   homepage 'http://homepage.boetes.org/software/mg/'
-  url 'http://homepage.boetes.org/software/mg/mg-20130922.tar.gz'
-  sha1 'ddd461ebae8df3c016359956348329fd04906195'
-  depends_on 'bsdmake' => :build
+  url 'http://homepage.boetes.org/software/mg/mg-20131118.tar.gz'
+  sha1 '61f0d6ef2fd36acc51fa560aa67d4eccd3a6c2b9'
+
   depends_on 'clens'
 
-  def patches
-    { :p0 => 'https://gist.github.com/jasperla/7264123/raw'}
-  end
-
   def install
-    ENV.j1
-    system "bsdmake"
+    system "make"
     bin.install "mg"
     doc.install "tutorial"
   end
