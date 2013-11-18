160e4597d28d92cf2f627d0e25477f298fd27ce0
seanjreilly <seanjreilly@gmail.com>
2013-11-18 12:11:31 +0000

    gradle 1.9-rc4
    Closes #24430.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -6,9 +6,9 @@ class Gradle < Formula
   sha1 'f14299582a1ab6c1293a43697ecda4b2673e34b1'
 
   devel do
-    url 'http://services.gradle.org/distributions/gradle-1.9-rc-3-bin.zip'
-    sha1 '9c4dc0aaddd20f2a5a9ba6dd7367033ec975ab2c'
-    version '1.9-rc3'
+    url 'http://services.gradle.org/distributions/gradle-1.9-rc-4-bin.zip'
+    sha1 '18967c087f0109a1d5971faf3c31e83cd8a5d938'
+    version '1.9-rc4'
   end
 
   def install
