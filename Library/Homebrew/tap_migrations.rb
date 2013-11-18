951d019e5de0366d8eeaacdab4885dd06ebf8792
Adam Vandenberg <flangy@gmail.com>
2013-11-18 07:18:52 -0800

    iopt moved to homebrew-science
    Closes #22945.

--- Library/Homebrew/tap_migrations.rb
+++ Library/Homebrew/tap_migrations.rb
@@ -1,5 +1,6 @@
 TAP_MIGRATIONS = {
   'octave' => 'homebrew/science',
   'grads' => 'homebrew/binary',
-  'denyhosts' => 'homebrew/boneyard'
+  'denyhosts' => 'homebrew/boneyard',
+  'ipopt' => 'homebrew/science'
 }
