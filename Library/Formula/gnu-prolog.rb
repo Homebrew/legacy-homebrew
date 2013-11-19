f0b8a7aeec88eed41df076f590740995ba645282
Jack Nagel <jacknagel@gmail.com>
2013-11-19 12:05:10 -0600

    gnu-prolog: fix build with clang

--- Library/Formula/gnu-prolog.rb
+++ Library/Formula/gnu-prolog.rb
@@ -5,16 +5,18 @@ class GnuProlog < Formula
   url 'http://gprolog.univ-paris1.fr/gprolog-1.4.4.tar.gz'
   sha1 '658b0efa5d916510dcddbbd980d90bc4d43a6e58'
 
-  fails_with :clang do
-    cause "Fatal Error: Segmentation Violation"
+  # Upstream patch:
+  # http://sourceforge.net/p/gprolog/code/ci/784b3443a0a2f087c1d1e7976739fa517efe6af6
+  def patches
+    "https://gist.github.com/jacknagel/7549696/raw/3078eef282ca141c95a0bf74396f4248bbe34775/gprolog-clang.patch"
   end
 
   def install
-    ENV.j1 # won't make in parallel
     cd 'src' do
       system "./configure", "--prefix=#{prefix}", "--with-doc-dir=#{doc}"
       system "make"
-      system "make install-strip"
+      ENV.deparallelize
+      system "make", "install"
     end
   end
 end
