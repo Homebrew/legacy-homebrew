af7eb2bd2395a7e273b7a12a3802d74e2cebff01
Brett Koonce <koonce@gmail.com>
2013-11-19 17:57:33 -0600

    gptfdisk 0.8.8
    Closes #24514.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/gptfdisk.rb
+++ Library/Formula/gptfdisk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gptfdisk < Formula
   homepage 'http://www.rodsbooks.com/gdisk/'
-  url 'http://downloads.sourceforge.net/project/gptfdisk/gptfdisk/0.8.7/gptfdisk-0.8.7.tar.gz'
-  sha1 'a134cdf28d0130bc0d3e459c0098b9109d9d3fb6'
+  url 'http://downloads.sourceforge.net/project/gptfdisk/gptfdisk/0.8.8/gptfdisk-0.8.8.tar.gz'
+  sha1 '11f66ec2a67920368f7afe0c01a1cb8c3f2c527a'
 
   depends_on 'popt'
   depends_on 'icu4c'
