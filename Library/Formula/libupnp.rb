96f6a081fc585da486505b1a1f4b75cc1eaa40b8
Brett Koonce <koonce@gmail.com>
2013-11-19 08:58:26 -0600

    libupnp 1.6.19
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libupnp.rb
+++ Library/Formula/libupnp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libupnp < Formula
   homepage 'http://pupnp.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/pupnp/pupnp/libUPnP%201.6.18/libupnp-1.6.18.tar.bz2'
-  sha1 '7dc7bd2fa74a0f9c65b5fcc4c65b8faa6cf02007'
+  url 'http://downloads.sourceforge.net/project/pupnp/pupnp/libUPnP%201.6.19/libupnp-1.6.19.tar.bz2'
+  sha1 'ee9e16ff42808521b62b7fc664fc9cba479ede88'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
