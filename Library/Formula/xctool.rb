14ff2985749270a96d8b96c74ef3be4d88b9bf5d
Adam Vandenberg <flangy@gmail.com>
2013-11-18 22:19:42 -0800

    xctool: fix head build
    Closes #24454

--- Library/Formula/xctool.rb
+++ Library/Formula/xctool.rb
@@ -10,7 +10,11 @@ class Xctool < Formula
   depends_on :macos => :lion
 
   def install
-    system "./build.sh 'XT_INSTALL_ROOT=#{libexec}'"
+    if build.head?
+      system "./scripts/build.sh 'XT_INSTALL_ROOT=#{libexec}'"
+    else
+      system "./build.sh 'XT_INSTALL_ROOT=#{libexec}'"
+    end
     bin.install_symlink "#{libexec}/bin/xctool"
   end
 
