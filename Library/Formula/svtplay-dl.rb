6f0dac69c96d055480a125d852801432b46a81f3
Brett Koonce <koonce@gmail.com>
2013-11-19 08:05:49 -0600

    svtplay-dl 0.9.2013.11.15
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/svtplay-dl.rb
+++ Library/Formula/svtplay-dl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class SvtplayDl < Formula
   homepage 'https://github.com/spaam/svtplay-dl'
-  url 'https://github.com/spaam/svtplay-dl/archive/0.9.2013.10.14.tar.gz'
-  sha1 'ab7017d699ae963729ff43055600ca4bca21213d'
+  url 'https://github.com/spaam/svtplay-dl/archive/0.9.2013.11.15.tar.gz'
+  sha1 '62e5d3e9c3fbdf3f27c230500c8790c187b240d5'
 
   depends_on 'rtmpdump'
 
