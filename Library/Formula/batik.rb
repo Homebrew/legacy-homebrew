c89ecef909994bd7eaca2e4103ade67d01ad4a28
Chris Osterwood <osterwood@gmail.com>
2013-11-18 21:58:53 -0500

    batik 1.7
    Creation of a formula for Batik SVG renderer, part of the apache.org project.
    
    Closes #24472.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/batik.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Batik < Formula
+  homepage 'https://xmlgraphics.apache.org/batik/'
+  url 'http://www.us.apache.org/dist/xmlgraphics/batik/batik-1.7.zip'
+  sha1 '69d974ab0a4e13cbdd649fa96785776563b73fcf'
+
+  def install
+    doc.install Dir['docs/*']
+    libexec.install 'lib', Dir['*.jar']
+
+    bin.write_jar_script libexec/'batik-rasterizer.jar', 'batik-rasterizer'
+    bin.write_jar_script libexec/'batik.jar', 'batik'
+  end
+end
