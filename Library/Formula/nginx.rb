118c261fbb8d7276b8f6b55888c6f860c24a234f
Marc Qualie <marc@marcqualie.com>
2013-11-19 16:05:09 +0000

    nginx 1.4.4 + 1.5.7
    Closes #24491.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Nginx < Formula
   homepage 'http://nginx.org/'
-  url 'http://nginx.org/download/nginx-1.4.3.tar.gz'
-  sha1 '8d0c34c84ce6dd8ba4442889e8f2599044c90930'
+  url 'http://nginx.org/download/nginx-1.4.4.tar.gz'
+  sha1 '304d5991ccde398af2002c0da980ae240cea9356'
 
   devel do
-    url 'http://nginx.org/download/nginx-1.5.6.tar.gz'
-    sha1 '24d84eccc7fecd98d19330ef708d3ea29031abc7'
+    url 'http://nginx.org/download/nginx-1.5.7.tar.gz'
+    sha1 '4dd04c73c3081277fe9c98c4a386c8baf956f5ca'
   end
 
   head 'http://hg.nginx.org/nginx/', :using => :hg
