c5432cdfe51429ebf5be386bd400aacd066ebfc2
Brett Koonce <koonce@gmail.com>
2013-11-19 08:04:56 -0600

    matlab2tikz 0.4.3
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/matlab2tikz.rb
+++ Library/Formula/matlab2tikz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Matlab2tikz < Formula
   homepage 'https://github.com/nschloe/matlab2tikz'
-  url 'https://github.com/nschloe/matlab2tikz/archive/0.4.2.tar.gz'
-  sha1 '70ff455c6171aaee729301619ce4069c97d4342f'
+  url 'https://github.com/nschloe/matlab2tikz/archive/0.4.3.tar.gz'
+  sha1 '6a2e81785e6fa0ba1681dfd5d3c159d08ec91c03'
 
   head 'https://github.com/nschloe/matlab2tikz.git'
 
