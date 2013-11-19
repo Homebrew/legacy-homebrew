ac10532a57df58d37a8cef0e4c2335d5e9853a1d
Brett Koonce <koonce@gmail.com>
2013-11-19 11:42:40 -0600

    scamper 20130824
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/scamper.rb
+++ Library/Formula/scamper.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Scamper < Formula
-  homepage 'http://www.wand.net.nz/scamper/'
-  url 'http://www.wand.net.nz/scamper/scamper-cvs-20111202c.tar.gz'
-  sha1 'eb0dc6bf13a0d568f65e079ad39be53c3e8457d9'
+  homepage 'http://www.caida.org/tools/measurement/scamper/'
+  url 'http://www.caida.org/tools/measurement/scamper/code/scamper-cvs-20130824.tar.gz'
+  sha1 'c3d5c5bb28fd39020a13d9d7be25e4e5ac0cb05e'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
