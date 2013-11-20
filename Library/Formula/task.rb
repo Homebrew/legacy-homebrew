6137c45ee104a2cec7f45ca7ef30695cfa9377fa
Benjamin Weber <one@bwe.im>
2013-11-20 17:51:47 +0100

    Added link to HEAD

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -4,6 +4,7 @@ class Task < Formula
   homepage 'http://www.taskwarrior.org/'
   url 'http://www.taskwarrior.org/download/task-2.2.0.tar.gz'
   sha1 '70656deb48a460f95370c885e388b475475f64eb'
+  head 'http://tasktools.org/task.git'
 
   depends_on "cmake" => :build
 
