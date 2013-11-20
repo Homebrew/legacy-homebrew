08d542e3a8b6efa56f866d528edf45229261730a
Benjamin Weber <one@bwe.im>
2013-11-20 17:57:30 +0100

    Added HEAD

--- Library/Formula/taskd.rb
+++ Library/Formula/taskd.rb
@@ -4,6 +4,7 @@ class taskd < Formula
   homepage 'http://taskwarrior.org'
   url 'http://taskwarrior.org/download/taskd-1.0.0.beta2.tar.gz'
   sha1 'e74f4004db571338f48094d28c7f1c39e772745b'
+  head 'http://tasktools.org/taskd.git'
 
   depends_on 'cmake' => :build
   depends_on 'gnutls' => :build
@@ -13,9 +14,5 @@ class taskd < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
-  end
-
-  test do
-    system "false"
   end
 end
