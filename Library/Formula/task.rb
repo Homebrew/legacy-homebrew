629b9b88f2420ab3fc4c5c2389a409fa18706d39
Benjamin Weber <one@bwe.im>
2013-11-20 23:16:22 +0100

    Added task HEAD

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class TaskwarriorDownloadStrategy < GitDownloadStrategy
-  # We need to make a local clone so we can't use "--depth 1"
+  # CMakeLists.txt requires presence of .git to generate commit.h (version information), otherwise make fails.
   def support_depth?
     false
   end
@@ -11,7 +11,7 @@ class Task < Formula
   homepage 'http://www.taskwarrior.org/'
   url 'http://www.taskwarrior.org/download/task-2.2.0.tar.gz'
   sha1 '70656deb48a460f95370c885e388b475475f64eb'
-  head 'git://tasktools.org/task.git', #:branch => :develop,
+  head 'git://tasktools.org/task.git', :branch => :"2.3.0",
   :using => TaskwarriorDownloadStrategy
 
   depends_on "cmake" => :build
@@ -24,6 +24,6 @@ class Task < Formula
   end
 
   test do
-    system "#{bin}/task", "diagnostics"
+    system "#{bin}/task", "--version"
   end
 end
