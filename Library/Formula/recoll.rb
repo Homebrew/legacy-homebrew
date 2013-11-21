82db4e401126cffd7968ae7c085ca70f7d5a402b
Jean-Francois Dockes <jf@dockes.org>
2013-11-21 15:01:20 +0100

    Update formula to recoll 1.19.9 which hopefully does not need patches and shoul compile with cxx11

--- Library/Formula/recoll.rb
+++ Library/Formula/recoll.rb
@@ -2,19 +2,11 @@ require 'formula'
 
 class Recoll < Formula
   homepage 'http://www.recoll.org'
-  url 'http://www.recoll.org/recoll-1.19.7.tar.gz'
-  sha1 'f7521966f5d128867cca0cfb73d999b87eed6a9f'
+  url 'http://www.recoll.org/recoll-1.19.9.tar.gz'
+  sha1 'ab662057082eb889b196653c328440ed95bf9ac4'
 
   depends_on 'xapian'
   depends_on 'qt'
-
-  def patches
-    { :p2 => [
-        "https://bitbucket.org/medoc/recoll/commits/1e6e8586ee1f058f58fef5ebba5ae49d8267bee1/raw/",
-      "https://bitbucket.org/medoc/recoll/commits/65a7041abbe6e64f6597d5cf27690e91014a85d4/raw/",
-      "https://bitbucket.org/medoc/recoll/commits/79dcd7fa03797b6b51720025b3c3276395523c38/raw/"
-    ]}
-  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
