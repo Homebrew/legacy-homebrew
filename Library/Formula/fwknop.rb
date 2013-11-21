fab119b2ff4130e78ac74457696b4afc1ab32571
Adam Vandenberg <flangy@gmail.com>
2013-11-20 21:29:34 -0800

    fwknop: remove comments

--- Library/Formula/fwknop.rb
+++ Library/Formula/fwknop.rb
@@ -2,19 +2,15 @@ require 'formula'
 
 class Fwknop < Formula
   homepage 'http://www.cipherdyne.org/fwknop/'
+  head 'https://github.com/mrash/fwknop.git'
   url 'https://github.com/mrash/fwknop/archive/2.5.1-1.tar.gz'
   version '2.5.1-1'
   sha1 '3f6c43b91ab555d7652b81c16bccfc8049eb0f92'
 
-  head 'https://github.com/mrash/fwknop.git'
-
-  # needed for running autogen.sh script in GitHub release tarball
-  # as well as when building from HEAD
   depends_on :automake
   depends_on :autoconf
   depends_on :libtool
 
-  # needed for gpg support
   depends_on 'gpgme' => :optional
 
   def install
