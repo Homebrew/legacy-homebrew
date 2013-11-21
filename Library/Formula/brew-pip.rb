7205635c88e3fa9b867bd48dbb64ae32af69cb4c
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-21 14:58:05 +0800

    Update brew-pip 0.4.0

--- Library/Formula/brew-pip.rb
+++ Library/Formula/brew-pip.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class BrewPip < Formula
-  homepage 'https://github.com/josh/brew-pip'
-  url 'https://github.com/josh/brew-pip/archive/v0.1.2.tar.gz'
-  sha1 '49fbd82ceb601e98999cbd28c106a7c26ff16a2b'
+  homepage 'https://github.com/edavis/brew-pip'
+  url 'https://github.com/edavis/brew-pip/archive/v0.4.0.tar.gz'
+  sha1 'd977a6d80709b3525f1b773e08ce3515315ea1d4'
 
   def install
     bin.install 'bin/brew-pip'
