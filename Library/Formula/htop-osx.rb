d629fd08eca56ad9266b62f000f9e24ed5dac6eb
Mike McQuaid <mike@mikemcquaid.com>
2013-11-21 13:48:19 +0000

    htop-osx 0.8.2.2
    Closes #24563.

--- Library/Formula/htop-osx.rb
+++ Library/Formula/htop-osx.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class HtopOsx < Formula
   homepage 'https://github.com/max-horvath/htop-osx'
-  url 'https://github.com/max-horvath/htop-osx/archive/0.8.2.1-2013-03-31.tar.gz'
-  version '0.8.2.1'
-  sha1 '9c4bbe8517b59ca2ead8fedd4b8b24452f2ec55e'
+  url 'https://github.com/max-horvath/htop-osx/archive/0.8.2.2.tar.gz'
+  sha1 '17c56fe5efe81cf6b0f4c13a958fa7e4d8591b23'
 
   depends_on :autoconf
   depends_on :automake
