c58323e4d71a64d7ff7d42b5cf02b5dc67caa341
dewey <mail@notmyhostna.me>
2013-11-22 16:51:56 +0100

    ghi: 0.9.0.20131120
    Closes #24589.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ghi.rb
+++ Library/Formula/ghi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ghi < Formula
   homepage 'https://github.com/stephencelis/ghi'
-  url 'https://github.com/stephencelis/ghi/archive/0.9.0.20130912.zip'
-  sha1 'c08baf9e6ac69131ee683725a27fc66dd0907a47'
+  url 'https://github.com/stephencelis/ghi/archive/0.9.0.20131120.tar.gz'
+  sha1 '7d1b245f22f78338ec3c5d1696af80b1437c3c7a'
 
   def install
     bin.install "ghi"
