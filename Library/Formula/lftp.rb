4055a34a55647d692f99f3b3a93890966cb2fd54
Christopher Eby <kreed@kreed.org>
2013-11-22 17:46:44 -0600

    lftp: add --with-brewed-openssl
    Closes #24602.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -7,10 +7,12 @@ class Lftp < Formula
   sha1 '56cefb9aa683acfa5c5713d530f594085ea7b149'
 
   option 'with-gnutls', "Use GnuTLS instead of the default OpenSSL"
+  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
 
   depends_on 'pkg-config' => :build
   depends_on 'readline'
   depends_on 'gnutls' => :optional
+  depends_on 'openssl' if build.with? 'brewed-openssl'
 
   def install
     # Bus error
