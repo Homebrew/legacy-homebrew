96516595f6f9e27b4f2255d06c691466f13e4617
Shannon Skipper <shannonskipper@gmail.com>
2013-11-22 15:06:56 -0800

    ruby-install 0.3.2
    ### 0.3.2 / 2013-11-22
    
    * Exit normally when `--no-reinstall` is specified and the ruby has already
      been installed. (@cbandy)
    * `fetch()` can now strip trailing tabs as well as spaces.
    
    #### ruby
    
    * Added version aliases for `2` and `1`.
    * Added versions 1.9.3-p484, 2.0.0-p353 and 2.1.0-preview2 for CVE-2013-4164.
    
    #### jruby
    
    * Added versions 1.7.6, 1.7.7 and 1.7.8.
    
    #### rubinius
    
    * Drop support for installing 2.0.0, due to multiple bugs.
    * Added versions 2.1.1, 2.2.0 and 2.2.1.
    
    Closes #24599.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ruby-install.rb
+++ Library/Formula/ruby-install.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyInstall < Formula
   homepage 'https://github.com/postmodern/ruby-install#readme'
-  url 'https://github.com/postmodern/ruby-install/archive/v0.3.1.tar.gz'
-  sha1 'e7e8a8b7e7138c6d364367a161dbb5a0a2bceda8'
+  url 'https://github.com/postmodern/ruby-install/archive/v0.3.2.tar.gz'
+  sha1 '5561df9bb5d73677cf5193d27ec130f52c4e4755'
 
   head 'https://github.com/postmodern/ruby-install.git'
 
