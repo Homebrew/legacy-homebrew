bab815c30471d93b56f6b4f242f7ded6a08f47e6
Curtis Hovey <sinzui.is@gmail.com>
2013-11-23 10:59:01 -0500

    juju: add bash completion
    Closes #24610.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -17,6 +17,19 @@ class Juju < Formula
     args.insert(1, "-v") if ARGV.verbose?
     system "go", *args
     bin.install 'bin/juju'
+    (bash_completion/'juju-completion.bash').write <<-EOS.undent
+    _juju()
+    {
+        local cur prev options files targets
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        prev="${COMP_WORDS[COMP_CWORD-1]}"
+        actions=$(juju help commands 2>/dev/null | awk '{print $1}')
+        COMPREPLY=( $( compgen -W "${actions}" -- ${cur} ) )
+        return 0
+    }
+    complete -F _juju juju
+    EOS
   end
 
   def test
