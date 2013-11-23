bf57a3630accf84bc4bc497a88fcbcce904466de
Jack Nagel <jacknagel@gmail.com>
2013-11-22 20:58:47 -0600

    libmatroska 1.4.1

--- Library/Formula/libmatroska.rb
+++ Library/Formula/libmatroska.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libmatroska < Formula
   homepage 'http://www.matroska.org/'
-  url 'http://dl.matroska.org/downloads/libmatroska/libmatroska-1.4.0.tar.bz2'
-  mirror 'http://www.bunkus.org/videotools/mkvtoolnix/sources/libmatroska-1.4.0.tar.bz2'
-  sha256 '1b6d02e75cdbfb6d282dcf2a902a259c3075404885d5e8063e6652a2b3f6c11b'
+  url 'http://dl.matroska.org/downloads/libmatroska/libmatroska-1.4.1.tar.bz2'
+  mirror 'http://www.bunkus.org/videotools/mkvtoolnix/sources/libmatroska-1.4.1.tar.bz2'
+  sha256 '086f21873e925679babdabf793c3bb85c353d0cd79423543a3355e08e8a4efb7'
 
   depends_on 'libebml'
 
