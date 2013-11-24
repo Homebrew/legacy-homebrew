0467663c572c16d59cd8648e9790205ac7bd210e
Nicholas Sivo <nick@kogir.com>
2013-11-23 16:44:27 -0800

    libmaxminddb: Add head.

--- Library/Formula/libmaxminddb.rb
+++ Library/Formula/libmaxminddb.rb
@@ -4,11 +4,16 @@ class Libmaxminddb < Formula
   homepage 'https://github.com/maxmind/libmaxminddb'
   url 'https://github.com/maxmind/libmaxminddb/releases/download/0.5.2/libmaxminddb-0.5.2.tar.gz'
   sha1 'db7618a97c222cab0a0ba2fb8439abcd1465f10c'
+  head 'https://github.com/maxmind/libmaxminddb.git'
 
   option :universal
 
   def install
     ENV.universal_binary if build.universal?
+
+    if build.head?
+      system "./bootstrap"
+    end
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
