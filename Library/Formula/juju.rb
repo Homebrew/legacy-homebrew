53e465ab7f91d7e3b8eb1afa2b6d9ee61049908a
Curtis Hovey <sinzui.is@gmail.com>
2013-11-24 08:40:55 -0500

    Simplified the instll of juju bash completions.

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -11,33 +11,25 @@ class Juju < Formula
     cause "clang: error: no such file or directory: 'libgcc.a'"
   end
 
-  def install_completion
-    script = <<-END_SCRIPT
-      _juju()
-      {
-          local cur prev options files targets
-          COMPREPLY=()
-          cur="${COMP_WORDS[COMP_CWORD]}"
-          prev="${COMP_WORDS[COMP_CWORD-1]}"
-          actions=$(juju help commands 2>/dev/null | awk '{print $1}')
-          COMPREPLY=( $( compgen -W "${actions}" -- ${cur} ) )
-          return 0
-      }
-      complete -F _juju juju
-    END_SCRIPT
-    File.open('./juju-completion.bash', 'wb') do |file|
-      file.write(script)
-    end
-    bash_completion.install 'juju-completion.bash'
-  end
-
   def install
     ENV['GOPATH'] = buildpath
     args = %w(install launchpad.net/juju-core/cmd/juju)
     args.insert(1, "-v") if ARGV.verbose?
     system "go", *args
     bin.install 'bin/juju'
-    self.install_completion
+    (bash_completion/'juju-completion.bash').write <<-EOS.undent
+    _juju()
+    {
+        local cur prev options files targets
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        prev="${COMP_WORDS[COMP_CWORD-1]}"
+        actions=$(juju help commands 2>/dev/null | awk '{print $1}')
+        COMPREPLY=( $( compgen -W "${actions}" -- ${cur} ) )
+        return 0
+    }
+    complete -F _juju juju
+    EOS
   end
 
   def test
