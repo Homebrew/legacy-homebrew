ecdc0d23e2b7d6dd618285a9c64336b487aded3a
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2013-11-25 21:34:15 -0200

    mackup 0.5.3
    Closes #24669.
    
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
    Closes #24669.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/mackup.rb
+++ Library/Formula/mackup.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mackup < Formula
   homepage 'https://github.com/lra/mackup'
-  url 'https://github.com/lra/mackup/archive/0.5.2.tar.gz'
-  sha1 '4b5fcd010f256dd8c67fe7a6ed9da2b77d162d09'
+  url 'https://github.com/lra/mackup/archive/0.5.3.tar.gz'
+  sha1 '85b5d8fd54da99c3dfb13f420b6eedcddfc56b20'
 
   def install
     bin.install "mackup.py" => "mackup"
