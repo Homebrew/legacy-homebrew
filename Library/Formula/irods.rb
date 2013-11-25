c7b3e787726e0261b647d3b3791e248967ea3e7e
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-11-25 15:08:55 +0100

    Fixing on code review by @mikemcquaid

--- Library/Formula/irods.rb
+++ Library/Formula/irods.rb
@@ -2,8 +2,7 @@ require 'formula'
 
 class Irods < Formula
   homepage 'https://www.irods.org'
-  version '3.3'
-  url "https://github.com/irods/irods/archive/#{version}.tar.gz"
+  url "https://github.com/irods/irods/archive/3.3.tar.gz"
   sha1 '4279445098ead7c3c1149141588f62fda4e017bc'
 
   conflicts_with 'sleuthkit', :because => 'both install `ils`'
@@ -13,26 +12,26 @@ class Irods < Formula
   depends_on 'fuse4x' if build.include? 'with-fuse'
 
   def install
-	chdir 'iRODS'
-	system "./scripts/configure"
+    chdir 'iRODS'
+    system "./scripts/configure"
 	
-	# include PAM authentication by default
+    # include PAM authentication by default
     inreplace 'config/config.mk', '# PAM_AUTH = 1', 'PAM_AUTH = 1'
     inreplace 'config/config.mk', '# USE_SSL = 1', 'USE_SSL = 1'
     
-	system "make"
+    system "make"
 
     bin.install Dir['clients/icommands/bin/*'].select {|f| File.executable? f}
 
 	# patch in order to use fuse4x
     if build.include? 'with-fuse'
-      inreplace 'config/config.mk', '# IRODS_FS = 1', 'IRODS_FS = 1'
-      inreplace 'config/config.mk', "fuseHomeDir=#{HOMEBREW_PREFIX}"
-      chdir 'clients/fuse' do
-        inreplace 'Makefile', 'lfuse', 'lfuse4x'
-        system "make"
-      end
-      bin.install Dir['clients/fuse/bin/*'].select {|f| File.executable? f}
+        inreplace 'config/config.mk', '# IRODS_FS = 1', 'IRODS_FS = 1'
+        inreplace 'config/config.mk', 'fuseHomeDir=/home/mwan/adil/fuse-2.7.0', "fuseHomeDir=#{HOMEBREW_PREFIX}"
+        chdir 'clients/fuse' do
+            inreplace 'Makefile', 'lfuse', 'lfuse4x'
+            system "make"
+        end
+        bin.install Dir['clients/fuse/bin/*'].select {|f| File.executable? f}
     end
   end
 
