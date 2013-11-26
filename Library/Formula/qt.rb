133d1e19276c326adfed05bf3a5040207f752380
Mike McQuaid <mike@mikemcquaid.com>
2013-11-26 08:11:00 +0000

    qt: use 4.8 branch snapshot for stable on 10.9.
    Would be nice if this could be done another way but I think it's better
    than making users use --HEAD until Qt fixes their mess (in January):
    http://permalink.gmane.org/gmane.comp.lib.qt.devel/13812
    
    Closes #24674.
    
    Signed-off-by: BrewTestBot <brew-test-bot@googlegroups.com>

--- Library/Formula/qt.rb
+++ Library/Formula/qt.rb
@@ -2,24 +2,34 @@ require 'formula'
 
 class Qt < Formula
   homepage 'http://qt-project.org/'
-  url 'http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz'
-  sha1 '745f9ebf091696c0d5403ce691dc28c039d77b9e'
+  if MacOS.version < :mavericks
+    url 'http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz'
+    sha1 '745f9ebf091696c0d5403ce691dc28c039d77b9e'
+  else
+    # This is a snapshot of the current qt-4.8 branch. It's been used by a
+    # bunch of people to get Qt working on Mavericks and 4.8.5 needs too many
+    # patches to compile any time soon (January-ish):
+    # http://permalink.gmane.org/gmane.comp.lib.qt.devel/13812
+    url 'https://github.com/qtproject/qt/archive/157da36977213237939df14608753bb3ec280f0b.tar.gz'
+    sha1 '145b8eb6a6c2ccc1cc58ddcb03a1d33b153e0c15'
+    # It would be nice if this was a real version number but unfortunately
+    # that will mess with the bottles.
+    version '4.8.5'
+
+    resource 'libWebKitSystemInterfaceMavericks' do
+      url 'http://trac.webkit.org/export/157771/trunk/WebKitLibraries/libWebKitSystemInterfaceMavericks.a'
+      sha1 'fc5ebf85f637f9da9a68692df350e441c8ef5d7e'
+      version '157771'
+    end
+  end
+
+  head 'git://gitorious.org/qt/qt.git', :branch => '4.8'
 
   bottle do
     revision 2
     sha1 'b361f521d413409c0e4397f2fc597c965ca44e56' => :mountain_lion
     sha1 'dcf218f912680031de7ce6d7efa021e499caea78' => :lion
     sha1 '401f2362ad9a22245a206729954dba731a1cdb52' => :snow_leopard
-  end
-
-  head do
-    url 'git://gitorious.org/qt/qt.git', :branch => '4.8'
-
-    resource 'libWebKitSystemInterfaceMavericks' do
-      url 'http://trac.webkit.org/export/157771/trunk/WebKitLibraries/libWebKitSystemInterfaceMavericks.a'
-      sha1 'fc5ebf85f637f9da9a68692df350e441c8ef5d7e'
-      version '157771'
-    end if MacOS.version >= :mavericks
   end
 
   option :universal
@@ -73,12 +83,6 @@ class Qt < Formula
   end
 
   def install
-    # Must be built with --HEAD on Mavericks at the moment
-    raise <<-EOS.undent if MacOS.version == :mavericks and not build.head?
-      On Mavericks, you must install Qt HEAD:
-        brew install qt --HEAD
-      EOS
-
     ENV.universal_binary if build.universal?
     ENV.append "CXXFLAGS", "-fvisibility=hidden"
 
