5128c3d1281ea75bf40f2a7d41100db0a98c7acf
Adam Vandenberg <flangy@gmail.com>
2013-11-26 23:06:58 -0800

    open-mesh: fix build

--- Library/Formula/open-mesh.rb
+++ Library/Formula/open-mesh.rb
@@ -11,6 +11,8 @@ class OpenMesh < Formula
   depends_on 'qt'
   depends_on 'glew'
 
+  def patches; DATA; end
+
   def install
     mkdir 'openmesh-build' do
       system "cmake -DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=Release .."
@@ -22,3 +24,17 @@ class OpenMesh < Formula
     system "#{bin}/mconvert", "-help"
   end
 end
+
+__END__
+diff --git a/src/OpenMesh/Tools/Utils/getopt.h b/src/OpenMesh/Tools/Utils/getopt.h
+index df0d7e0..af6bd55 100644
+--- a/src/OpenMesh/Tools/Utils/getopt.h
++++ b/src/OpenMesh/Tools/Utils/getopt.h
+@@ -21,6 +21,7 @@ OPENMESHDLLEXPORT extern int getopt(int nargc, char * const *nargv, const char *
+ 
+ #  endif
+ #else
++#include <unistd.h>
+ #  include <getopt.h>
+ #endif
+ 
