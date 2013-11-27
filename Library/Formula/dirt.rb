8ed698ae97b421d43c42aacfba3225b89233d5e6
Simon Fransson <simon@dessibelle.se>
2013-11-27 13:06:57 +0100

    dirt 1.0 (new formula)
    An unimpressive thingie for playing bits of samples with some level of
    accuracy. Dirt is a software sampler, which listens for
    OpenSoundControl messages on port 7771. Mainly used with Tidal
    (http://yaxu.org/tidal/). Released as free software under the terms of
    the GNU Public License version 3.0 and later.
    
    (c) Alex McLean 2013
    Released under the GNU Public Licence version 3
    
    Closes #24707.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/dirt.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Dirt < Formula
+  homepage 'https://github.com/yaxu/Dirt'
+  head 'https://github.com/yaxu/Dirt.git'
+  url 'https://github.com/yaxu/Dirt/archive/1.0.tar.gz'
+  sha1 '853d6a80bf77ebceabd25411ea01568d9acb3362'
+
+  depends_on 'jack'
+  depends_on 'liblo'
+
+  def install
+    system "make", "DESTDIR=#{prefix}", "install"
+  end
+
+  test do
+    system "#{bin}/dirt", "-h"
+  end
+end
