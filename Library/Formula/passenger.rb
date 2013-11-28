bb706d122640f8180fc81c9b00217278b6232c86
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-11-28 12:18:10 +0100

    passenger 4.0.26
    Closes #24733.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/passenger.rb
+++ Library/Formula/passenger.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Passenger < Formula
   homepage 'https://www.phusionpassenger.com/'
-  url 'http://s3.amazonaws.com/phusion-passenger/releases/passenger-4.0.25.tar.gz'
-  sha1 'e830f46fd3e666017ae3c441efdfe36cc575364e'
+  url 'http://s3.amazonaws.com/phusion-passenger/releases/passenger-4.0.26.tar.gz'
+  sha1 'd41bbf70bd41d6bf34f2a582b129c3ea0b44c345'
   head 'https://github.com/phusion/passenger.git'
 
   depends_on :macos => :lion
