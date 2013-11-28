6303cccdaed0903bb1890353362d2fc25ebab91a
denji <denji@localhost>
2013-11-29 01:45:42 +0200

    Back

--- Library/Formula/radare2.rb
+++ Library/Formula/radare2.rb
@@ -11,26 +11,11 @@ class Radare2 < Formula
   depends_on 'libmagic'
   depends_on 'gmp'
   depends_on 'lua'
-  depends_on "openssl" if MacOS.version <= :leopard
 
   def install
-    args = [
-      "--prefix=#{prefix}",
-      "--with-ostype=darwin",
-    ]
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-ostype=darwin"
 
-    if MacOS.version > :leopard
-      # For Xcode-only systems we help a bit to find openssl.
-      # If CLT.installed?, it evaluates to "/usr", which works.
-      args << "--with-openssl=#{MacOS.sdk_path}/usr"
-    else
-      args << "--with-openssl=#{Formula.factory('openssl').opt_prefix}"
-    end
-
-    system "./configure", *args
-
-    # "Do not use a parallel 'make'."
-    ENV.deparallelize
     system "make"
     system "make install"
   end
