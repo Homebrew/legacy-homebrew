92be3a71e4ba286fd8a067bf5041eb17be6170bd
Adam Vandenberg <flangy@gmail.com>
2013-11-27 20:20:55 -0800

    voms 2.0.11

--- Library/Formula/voms.rb
+++ Library/Formula/voms.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Voms < Formula
   homepage 'https://github.com/italiangrid/voms'
-  url 'https://github.com/italiangrid/voms/archive/2_0_8.tar.gz'
-  sha1 'f8c442318636a29da9e2cf8933fa46249b337a05'
+  url 'https://github.com/italiangrid/voms/archive/2_0_11.tar.gz'
+  sha1 'dabc2ebe01d01052f5b3849cb04f4947a38d99dd'
 
   depends_on :autoconf
   depends_on :automake
