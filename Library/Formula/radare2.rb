fc0ae736136c338cd98b707e00e36b9c22a0fbb7
denji <denji@localhost>
2013-11-28 21:36:45 +0200

    Test build

--- Library/Formula/radare2.rb
+++ Library/Formula/radare2.rb
@@ -7,13 +7,31 @@ class Radare2 < Formula
 
   head 'https://github.com/radare/radare2.git'
 
+  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+
   depends_on 'libewf'
   depends_on 'libmagic'
   depends_on 'gmp'
   depends_on 'lua'
+  depends_on "openssl" if MacOS.version <= :leopard or build.with?('brewed-openssl')
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    args = [
+      "--prefix=#{prefix}",
+      "--with-ostype=darwin",
+    ]
+
+  	if build.with? 'brewed-openssl'
+  	  args << "--with-openssl=#{Formula.factory('openssl').opt_prefix}"
+    elsif MacOS.version > :leopard
+      # For Xcode-only systems we help a bit to find openssl.
+      # If CLT.installed?, it evaluates to "/usr", which works.
+      args << "--with-openssl=#{MacOS.sdk_path}/usr"
+    end
+    system "./configure", *args
+
+    # "Do not use a parallel 'make'."
+    ENV.deparallelize
     system "make"
     system "make install"
   end
