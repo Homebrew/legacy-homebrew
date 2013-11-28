33b9254d20815e6dedb0e6b6701289a06a7edb6d
Adam Vandenberg <flangy@gmail.com>
2013-11-28 11:22:00 -0800

    aimage: change homepage

--- Library/Formula/aimage.rb
+++ Library/Formula/aimage.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Aimage < Formula
-  homepage 'http://afflib.org/archives/tag/aimage'
+  homepage 'http://www.forensicswiki.org/wiki/Aimage'
   url 'http://pkgs.fedoraproject.org/repo/pkgs/aimage/aimage-3.2.5.tar.gz/07a11d653cdd1d7a5aefe4d99cdbd408/aimage-3.2.5.tar.gz'
   sha1 '8f3136e9ad3204fb4c91149b3f01a32cc83e0371'
 
