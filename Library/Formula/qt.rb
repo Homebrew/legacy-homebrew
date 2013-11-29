9958bd96d0d4c0acf1af341abad132a9bfde8168
Mike McQuaid <mike@mikemcquaid.com>
2013-11-29 11:44:26 +0000

    qt: update patches needed for HEAD.

--- Library/Formula/qt.rb
+++ Library/Formula/qt.rb
@@ -45,41 +45,48 @@ class Qt < Formula
   odie 'qt: --with-debug-and-release is no longer supported' if build.include? 'with-debug-and-release'
 
   def patches
-    # Patches to fix compilation on Mavericks (http://github.com/mxcl/homebrew/pull/23793)
-    return unless MacOS.version >= :mavericks
+    p = []
+    if MacOS.version >= :mavericks
+      # Patches to fix compilation on Mavericks (http://github.com/mxcl/homebrew/pull/23793)
+      unless build.head?
+        p += [
+          # Change Iff4d919d: Added a patch to let the WebKit's QNetworkReplyHandler.cpp compile at Mac OS X 10.9 Mavericks
+          # (https://codereview.qt-project.org/#change,70438)
+          'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/ec13ef2a8b4adc7b9695e6d49876d826f89802ae/Change_Iff4d919d',
+          # Change Ied51c868: Added a patch to let the WebKit's qgraphicswebview.cpp compile at Mac OS X 10.9 Mavericks
+          # (https://codereview.qt-project.org/#change,70439)
+          'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/8834e194a0f4e0c99ef64064f6a86ddcb617f444/Change_Ied51c868',
+          # Change Ic6330613: Added a patch to let the WebKit's NotificationPresenterClientQt.cpp compile at Mac OS X 10.9 Mavericks
+          # (https://codereview.qt-project.org/#change,70440)
+          'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/9ee8460814204faa5cf5b1317fba5d1b14a563eb/Change_Ic6330613',
+          # Change I2ad84441: Added a patch to let the WebKit's .pro file find the lib for Mavericks. This is needed to compile at Mac OS X 10.9 Mavericks
+          # (https://codereview.qt-project.org/#change,70442)
+          'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/f73ea1979f4595fc463b2deb77987b389748e289/Change_I2ad84441',
+          # Change I4c697a87: Added a patch to let the WebKit's platform/Timer.h compile at Mac OS X 10.9 Mavericks
+          # (https://codereview.qt-project.org/#change,70443)
+          'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/9d5305f6bb01cf445893d09bf399097a53706d6a/Change+I4c697a87',
+          # Change I31ad9a7a: Added a patch to let the WebKit's platform/Timer.cpp compile at Mac OS X 10.9 Mavericks
+          # (https://codereview.qt-project.org/#change,70444)
+          'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/2f9a348e575f63d435c3d32a9c70c4c2d687542c/Change_I31ad9a7a',
+          # Change Ieb30c115: Backported fix for WebKit libc++ support on OS X Mavricks
+          # (https://codereview.qt-project.org/#change,70929)
+          'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/ebdc1fbf8d1b9a65e797124fb64b709a7d71107d/Change_Ieb30c115',
+        ]
+      end
 
-    [
-      # Change Change I8fd619af: Added a patch to let the CLucene's FieldCachImpl.cpp compile at Mac OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70437)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/6d2597c4c61cca04ed56472fd1fd793798526ce6/Change_I8fd619af',
-      # Change Iff4d919d: Added a patch to let the WebKit's QNetworkReplyHandler.cpp compile at Mac OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70438)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/ec13ef2a8b4adc7b9695e6d49876d826f89802ae/Change_Iff4d919d',
-      # Change Ied51c868: Added a patch to let the WebKit's qgraphicswebview.cpp compile at Mac OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70439)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/8834e194a0f4e0c99ef64064f6a86ddcb617f444/Change_Ied51c868',
-      # Change Ic6330613: Added a patch to let the WebKit's NotificationPresenterClientQt.cpp compile at Mac OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70440)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/9ee8460814204faa5cf5b1317fba5d1b14a563eb/Change_Ic6330613',
-      # Change I2ad84441: Added a patch to let the WebKit's .pro file find the lib for Mavericks. This is needed to compile at Mac OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70442)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/f73ea1979f4595fc463b2deb77987b389748e289/Change_I2ad84441',
-      # Change I4c697a87: Added a patch to let the WebKit's platform/Timer.h compile at Mac OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70443)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/9d5305f6bb01cf445893d09bf399097a53706d6a/Change+I4c697a87',
-      # Change I31ad9a7a: Added a patch to let the WebKit's platform/Timer.cpp compile at Mac OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70444)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/2f9a348e575f63d435c3d32a9c70c4c2d687542c/Change_I31ad9a7a',
-      # Change Ieb30c115: Backported fix for WebKit libc++ support on OS X Mavricks
-      # (https://codereview.qt-project.org/#change,70929)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/ebdc1fbf8d1b9a65e797124fb64b709a7d71107d/Change_Ieb30c115',
-      # Change Iaedaff7c: Enable building with clang / libc++ on OS X 10.9 Mavericks
-      # (https://codereview.qt-project.org/#change,70930)
-      'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/cc0a38d67cb36b650a275af3825731ce1f2ba35c/Change_Iaedaff7c',
-      # Change I04e1471d: Return the correct system font on OS X Mavericks.
-      # (https://codereview.qt-project.org/#change,62261)
-      'https://gist.github.com/mhemeryck/7487365/raw/adc0ba7a9b33b113ab3d15f545082703a51d3ccd/Change_I04e1471d',
-    ]
+      p += [
+        # Change Change I8fd619af: Added a patch to let the CLucene's FieldCachImpl.cpp compile at Mac OS X 10.9 Mavericks
+        # (https://codereview.qt-project.org/#change,70437)
+        'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/6d2597c4c61cca04ed56472fd1fd793798526ce6/Change_I8fd619af',
+        # Change Iaedaff7c: Enable building with clang / libc++ on OS X 10.9 Mavericks
+        # (https://codereview.qt-project.org/#change,70930)
+        'https://gist.github.com/jensenb/aafb2c2d1e0fcce2994f/raw/cc0a38d67cb36b650a275af3825731ce1f2ba35c/Change_Iaedaff7c',
+        # Change I04e1471d: Return the correct system font on OS X Mavericks.
+        # (https://codereview.qt-project.org/#change,62261)
+        'https://gist.github.com/mhemeryck/7487365/raw/adc0ba7a9b33b113ab3d15f545082703a51d3ccd/Change_I04e1471d',
+      ]
+    end
+    p
   end
 
   def install
