7d23253fd3c889336ec7a51d9f5d804aadce874b
Adam Vandenberg <flangy@gmail.com>
2013-11-30 09:58:00 -0800

    fribidi 0.19.5

--- Library/Formula/fribidi.rb
+++ Library/Formula/fribidi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fribidi < Formula
   homepage 'http://fribidi.org/'
-  url 'http://fribidi.org/download/fribidi-0.19.2.tar.gz'
-  sha1 '3889469d96dbca3d8522231672e14cca77de4d5e'
+  url 'http://fribidi.org/download/fribidi-0.19.5.tar.bz2'
+  sha1 '58445266df185f7e5109f356c0261d41db39182a'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
