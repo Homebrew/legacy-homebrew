2905c9676bdd9fec1a55a3fc81ad96d160a9b9d8
Guillaume Hain <zedtux@zedroot.org>
2013-11-30 12:18:05 +0100

    jags: update homepage.
    Looks like the new homepage is now http://mcmc-jags.sourceforge.net.
    
    Closes #24801.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/jags.rb
+++ Library/Formula/jags.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Jags < Formula
-  homepage 'http://www-fis.iarc.fr/~martyn/software/jags/'
+  homepage 'http://mcmc-jags.sourceforge.net'
   url 'http://downloads.sourceforge.net/project/mcmc-jags/JAGS/3.x/Source/JAGS-3.3.0.tar.gz'
   sha1 '79a50baaf1e2b2e7673d477e830963b49aad2a6c'
 
