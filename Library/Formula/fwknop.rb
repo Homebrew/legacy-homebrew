88aeb5e908cdf354a6298791c298e9bbd91b42a8
Tim Heckman <tim+code@timheckman.net>
2013-11-29 22:55:58 -0800

    remove version declaration

--- Library/Formula/fwknop.rb
+++ Library/Formula/fwknop.rb
@@ -4,7 +4,6 @@ class Fwknop < Formula
   homepage 'http://www.cipherdyne.org/fwknop/'
   head 'https://github.com/mrash/fwknop.git'
   url 'https://github.com/mrash/fwknop/archive/2.5.1.1.tar.gz'
-  version '2.5.1.1'
   sha1 '47adc5734dfc84f9ab6e2ba8a5097132519cab8a'
 
   depends_on :automake
