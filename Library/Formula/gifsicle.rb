d0eaec0636eacd8f190b763e480cc2548a34bd74
Adam Vandenberg <flangy@gmail.com>
2013-11-30 10:53:17 -0800

    gifsicle 1.77

--- Library/Formula/gifsicle.rb
+++ Library/Formula/gifsicle.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gifsicle < Formula
   homepage 'http://www.lcdf.org/gifsicle/'
-  url 'http://www.lcdf.org/gifsicle/gifsicle-1.70.tar.gz'
-  sha1 'f5017c7e3298108f2063a926f83392c66f0652ae'
+  url 'http://www.lcdf.org/gifsicle/gifsicle-1.77.tar.gz'
+  sha1 '29ac22a9aa1a22ed85e4df90668c72d08d89c8b0'
 
   depends_on :x11
 
