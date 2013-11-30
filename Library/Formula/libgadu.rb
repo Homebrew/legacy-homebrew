390ab4d7497ab53e6afdfd95a272dec0e6b1431a
Guillaume Hain <zedtux@zedroot.org>
2013-11-30 10:30:13 +0100

    libgadu: fix homepage 404.
    Closes #24796.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libgadu.rb
+++ Library/Formula/libgadu.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Libgadu < Formula
-  homepage 'http://toxygen.net/libgadu/'
+  homepage 'http://libgadu.net/'
   url 'http://toxygen.net/libgadu/files/libgadu-1.11.2.tar.gz'
   sha1 '0e13836416b49212d5f17a74d8c2e72c5f915238'
 
