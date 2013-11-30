5554935b99f5bfedf1f58e7ddaf52124c285bf88
razasyedh <razasyedh@gmail.com>
2013-11-30 02:54:10 -0500

    ipbt: fix checksum
    Closes #24795.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ipbt.rb
+++ Library/Formula/ipbt.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ipbt < Formula
   homepage 'http://www.chiark.greenend.org.uk/~sgtatham/ipbt/'
   url 'http://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-r9487.tar.gz'
-  sha1 '6938c792994473cff2e3cdb44edc957664e6cc83'
+  sha1 'c4ce632afcefd9d1537567e17e76645f5f4482e8'
   version 'r9487'
 
   def install
