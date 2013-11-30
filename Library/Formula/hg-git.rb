04a6280fbfbcce80ddb5ebe640458c01105e4bf5
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-30 11:46:33 +0800

    hg-git 0.4.0

---
+++ Library/Formula/hg-git.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class HgGit < Formula
+  homepage 'https://bitbucket.org/durin42/hg-git/overview'
+  url 'https://bitbucket.org/durin42/hg-git/get/0.4.0.tar.bz2'
+  sha1 'b7dcf179d6c41fe284cb4dd070376de157d82ab9'
+  head 'ssh://hg@bitbucket.org/durin42/hg-git', :using => :hg
+
+  depends_on :python
+  depends_on "hg" => :recommended
+
+  def install
+    prefix.install 'hggit/'
+  end
+  
+  def caveats; <<-EOS.undent
+    To enable hg-git, add the following lines to your ~/.hgrc
+       [extensions]
+       hggit = #{prefix}/hggit
+    
+    More details at https://bitbucket.org/durin42/hg-git
+    EOS
+  end
+end
