1f7a98afeb464b140ac95e65cd4f7e0316ad61d3
Jack Nagel <jacknagel@gmail.com>
2013-12-01 16:50:48 -0600

    gtk+3 3.10.5

--- Library/Formula/gtk+3.rb
+++ Library/Formula/gtk+3.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gtkx3 < Formula
   homepage 'http://gtk.org/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.10/gtk+-3.10.4.tar.xz'
-  sha256 '4fd411fc6377d969d7f8e99c14d078822e8dbe940a417be299e456a3d2c4a52b'
+  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.10/gtk+-3.10.5.tar.xz'
+  sha256 '3191f6d8e8cdf0bac1a8ed6388acc67a15acf5e276161c9b93a305938d29ccc7'
 
   depends_on :x11 => '2.5' # needs XInput2, introduced in libXi 1.3
   depends_on 'pkg-config' => :build
