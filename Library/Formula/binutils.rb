c812333630723da601f70becd81cb6d9caa41f57
Stefan <humdedum@icloud.com>
2013-12-03 00:21:38 +0100

    binutils 2.24
    Closes #24885.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -2,15 +2,9 @@ require 'formula'
 
 class Binutils < Formula
   homepage 'http://www.gnu.org/software/binutils/binutils.html'
-  url 'http://ftpmirror.gnu.org/binutils/binutils-2.23.2.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.gz'
-  sha1 'c3fb8bab921678b3e40a14e648c89d24b1d6efec'
-
-  # Fix compilation with clang. Reported upstream:
-  # http://sourceware.org/bugzilla/show_bug.cgi?id=15728
-  def patches
-    DATA
-  end
+  url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
+  sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
 
   def install
     system "./configure", "--disable-debug",
@@ -28,23 +22,3 @@ class Binutils < Formula
     system "make install"
   end
 end
-
-__END__
---- a/include/cgen/basic-ops.h	2009-10-23 18:17:08.000000000 -0600
-+++ b/include/cgen/basic-ops.h	2013-07-10 14:21:44.000000000 -0600
-@@ -295,11 +295,11 @@
-    significant and word number 0 is the most significant word.
-    ??? May also wish an endian-dependent version.  Later.  */
- 
--QI SUBWORDSIQI (SI, int);
--HI SUBWORDSIHI (SI, int);
--QI SUBWORDDIQI (DI, int);
--HI SUBWORDDIHI (DI, int);
--SI SUBWORDDISI (DI, int);
-+static QI SUBWORDSIQI (SI, int);
-+static HI SUBWORDSIHI (SI, int);
-+static QI SUBWORDDIQI (DI, int);
-+static HI SUBWORDDIHI (DI, int);
-+static SI SUBWORDDISI (DI, int);
- 
- #ifdef SEMOPS_DEFINE_INLINE
