e551def30e40b5421b911f75caddc68314ac2dd0
Todd Persen <todd.persen@gmail.com>
2013-12-02 17:17:11 -0500

    InfluxDB 0.3.2

--- Library/Formula/influxdb.rb
+++ Library/Formula/influxdb.rb
@@ -2,14 +2,8 @@ require "formula"
 
 class Influxdb < Formula
   homepage "http://influxdb.org"
-  url "http://get.influxdb.org/src/influxdb-0.3.0.tar.gz"
-  sha1 "057e5ac9a53f1c516f920d9f5c158b4998f669c6"
-
-  bottle do
-    sha1 'b9bd4fb3404f11457522db197d47862e354848a5' => :mavericks
-    sha1 '0baf4e253800bf95524ddd5bae79f54e66dcf18e' => :mountain_lion
-    sha1 'b3c0a6b7e668c57e78f92d13a3f89e9dc4c1d1da' => :lion
-  end
+  url "http://get.influxdb.org/influxdb-0.3.2.src.tar.gz"
+  sha1 "bb0c2a2fdc0db2e7b13ba5611c22b833e313b986"
 
   depends_on "leveldb"
   depends_on "protobuf" => :build
@@ -20,7 +14,11 @@ class Influxdb < Formula
   def install
     ENV["GOPATH"] = buildpath
 
-    system "go build src/server/server.go"
+    flex = Formula.factory('flex').bin/"flex"
+    bison = Formula.factory('bison').bin/"bison"
+
+    system "./configure --with-flex=#{flex} --with-bison=#{bison}"
+    system "make build"
 
     inreplace "config.json.sample" do |s|
       s.gsub! "/tmp/influxdb/development/db", "#{var}/influxdb/data"
