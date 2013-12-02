6d9a091bf376143d8e7ce6cf874add123cfd71fe
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-03 00:59:13 +0800

    Add comment to explain why env :std is required

--- Library/Formula/crosstool-ng.rb
+++ Library/Formula/crosstool-ng.rb
@@ -15,6 +15,7 @@ class CrosstoolNg < Formula
   depends_on 'binutils'
   depends_on 'libelf'
 
+  # Avoid superenv to prevent https://github.com/mxcl/homebrew/pull/10552#issuecomment-9736248
   env :std
 
   def patches
