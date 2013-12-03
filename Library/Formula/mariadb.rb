6fa0d30f5166a69fcccdc14c80d738251ce46a7e
Dan <dan@wadoku.de>
2013-12-03 18:11:34 +0100

    mariadb: document issues handled by patches

--- Library/Formula/mariadb.rb
+++ Library/Formula/mariadb.rb
@@ -32,7 +32,12 @@ class Mariadb < Formula
   def patches
     if build.devel?
       [
+	# Prevent name collision leading to compilation failure. See:
+	# issue #24489, upstream: https://mariadb.atlassian.net/browse/MDEV-5314
         'https://gist.github.com/makigumo/7735363/raw/mariadb-10.0.6.mac.patch',
+	# Cherry-picked from upstream.
+	# http://bazaar.launchpad.net/~maria-captains/maria/10.0-base/revision/3941#support-files/CMakeLists.txt
+	# Resolved in 10.0.7, see: https://mariadb.atlassian.net/browse/MDEV-5314
         'https://gist.github.com/makigumo/7735363/raw/mariadb-10.0.6.mac.2.patch'
       ]
     end
