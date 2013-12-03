4bbe77df140bf56f9d30d965f5c963b916644890
Mark Simpson <verdammelt@gmail.com>
2013-12-02 23:26:24 -0500

    compface 1.5.2
    Closes #24892.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ Library/Formula/compface.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class Compface < Formula
+  homepage 'http://freecode.com/projects/compface'
+  url 'http://ftp.xemacs.org/pub/xemacs/aux/compface-1.5.2.tar.gz'
+  sha1 '72dad8774b3301a1562bdbd5b3c5536ebf86a03d'
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+
+    system "make", "install"
+  end
+
+  test do
+    system bin/"uncompface"
+  end
+end
