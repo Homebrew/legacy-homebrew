951ac585ae8c5e3b9cb97013a4cd91773ff234e6
chris williams <chilliams@gmail.com>
2013-12-02 19:30:49 -0500

    plt-racket 5.3.6

--- Library/Formula/plt-racket.rb
+++ Library/Formula/plt-racket.rb
@@ -4,8 +4,8 @@ class PltRacket < Formula
   homepage 'http://racket-lang.org/'
   # Use GitHub tarball as the release tarball doesn't have
   # everything needed for building on OS X
-  url 'https://github.com/plt/racket/archive/v5.2.tar.gz'
-  sha1 '313425909ff8d956c3e99d0a2b5b3cb12d0f98ad'
+  url 'https://github.com/plt/racket/archive/v5.3.6.tar.gz'
+  sha1 '6b0e7a11bb3ae6480b99db346e5b503a97539e6b'
 
   def install
     cd 'src' do
