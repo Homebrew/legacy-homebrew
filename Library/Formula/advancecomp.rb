7a8349b394df10b84aa89e41f3d87fa5e4b3eb19
Michael Ford <fanquake@users.noreply.github.com>
2013-12-03 18:36:31 +0800

    advancecomp 1.18
    Closes #24898.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/advancecomp.rb
+++ Library/Formula/advancecomp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Advancecomp < Formula
   homepage 'http://advancemame.sourceforge.net/comp-readme.html'
-  url 'http://downloads.sourceforge.net/advancemame/advancecomp-1.17.tar.gz'
-  sha1 '9197bc424d111575501221710301b44764150e85'
+  url 'http://downloads.sourceforge.net/advancemame/advancecomp-1.18.tar.gz'
+  sha1 'e5b00dc75cd6db4dfb1886968baf26a3a450ef7e'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
