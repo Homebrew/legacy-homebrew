f611fdb0f0e3cc09632fb5db2b79f7698e0c257d
Mark Simpson <verdammelt@gmail.com>
2013-12-02 23:26:24 -0500

    Formula for compface/uncompface.

---
+++ Library/Formula/compface.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Compface < Formula
+  homepage 'http://freecode.com/projects/compface'
+  url 'http://ftp.xemacs.org/pub/xemacs/aux/compface-1.5.2.tar.gz'
+  sha1 '72dad8774b3301a1562bdbd5b3c5536ebf86a03d'
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "uncompface"
+  end
+end
