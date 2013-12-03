1b2a84eb2b2f5aead8aaf3422775e5f2d064eae9
Peter Monks <pmonks@gmail.com>
2013-12-02 16:32:30 -0800

    multitail 6.0
    Closes #24889.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/multitail.rb
+++ Library/Formula/multitail.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Multitail < Formula
   homepage 'http://www.vanheusden.com/multitail/download.html'
-  url 'http://www.vanheusden.com/multitail/multitail-5.2.12.tgz'
-  sha1 '3d4979b8be1daaf99e4fd983366e0131aa3d0d3b'
+  url 'http://www.vanheusden.com/multitail/multitail-6.0.tgz'
+  sha1 'fed0d3c5f03ff022f8f2ae90c0027312c3ff70ce'
 
   def install
     ENV['DESTDIR'] = prefix
