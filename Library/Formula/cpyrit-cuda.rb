add207b69117ab4305b19ad96559258c546b0b36
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-04 01:16:07 -0600

    libdnet built with python

--- Library/Formula/cpyrit-cuda.rb
+++ Library/Formula/cpyrit-cuda.rb
@@ -35,7 +35,7 @@ class CpyritCuda < Formula
     url 'https://pyrit.googlecode.com/files/cpyrit-cuda-0.4.0.tar.gz'
     sha1 '6481b1d104fc8a1753d50d517b99638782171a08'
 
-    depends_on 'libnet'
+    depends_on 'libnet' => '--with-python'
     depends_on 'scapy' => :python
     depends_on :python
 
