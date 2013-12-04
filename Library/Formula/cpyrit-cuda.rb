96dfaa3e3df6e33b94e94c0ace7c4d84c5482487
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-03 20:42:04 -0600

    change nvidia depends_on

--- Library/Formula/cpyrit-cuda.rb
+++ Library/Formula/cpyrit-cuda.rb
@@ -5,7 +5,7 @@ class CpyritCuda < Formula
     url 'https://pyrit.googlecode.com/files/cpyrit-cuda-0.4.0.tar.gz'
     sha1 '6481b1d104fc8a1753d50d517b99638782171a08'
 
-    depends_on :nvidiacuda
+    depends_on NvidiaCudaRequirement
     depends_on 'libnet'
     depends_on 'scapy'
     depends_on :python
