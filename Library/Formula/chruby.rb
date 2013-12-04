3032d8ab68515f91501523a98e88ac93b92918e4
Shannon Skipper <shannonskipper@gmail.com>
2013-12-04 13:59:40 -0800

    chruby 0.3.8
    ### 0.3.8 / 2013-12-04
    
    #### chruby.sh
    
    * Remove trailing slashes from ruby directories when iterating over `RUBIES`.
      (@halostatue)
    * Ensure all temporary variables are local or unset.
    
    #### auto.sh
    
    * Ensure that `chruby_auto` can read `.ruby-version` files that do not end with
      a new-line. (@hosiawak)
    
    #### scripts/setup.sh
    
    * Install ruby-install 0.3.3.
    
    #### scripts/bug_report.sh
    
    * Print `$HOME`, `$RUBY_AUTO_VERSION`.
    * Print `trap -p`, `$preexec_functions` and `$precmd_functions`.
    * Print env variables even when they are empty.
    
    Closes #24941.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/chruby.rb
+++ Library/Formula/chruby.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Chruby < Formula
   homepage 'https://github.com/postmodern/chruby#readme'
-  url 'https://github.com/postmodern/chruby/archive/v0.3.7.tar.gz'
-  sha1 '237903096f874f40749520d267fe35e2e3222bb3'
+  url 'https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz'
+  sha1 '320d13bacafeae72631093dba1cd5526147d03cc'
 
   head 'https://github.com/postmodern/chruby.git'
 
@@ -18,7 +18,7 @@ class Chruby < Formula
 
     By default chruby will search for Rubies installed into /opt/rubies/ or
     ~/.rubies/. For non-standard installation locations, simply set the RUBIES
-    variable:
+    variable after loading chruby.sh:
 
       RUBIES=(
         /opt/jruby-1.7.0
