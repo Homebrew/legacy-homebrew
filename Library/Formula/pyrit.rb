d44d887171f0ab87bd2a40354d9b84bfb3d75d08
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-03 21:12:40 -0600

    use python tap for scapy add openssl

--- Library/Formula/pyrit.rb
+++ Library/Formula/pyrit.rb
@@ -6,7 +6,9 @@ class Pyrit < Formula
   sha1 '3041c0d593c21df88f41416be872eca0997cf22b'
 
   depends_on :python
-  depends_on "libdnet"
+  depends_on 'libdnet'
+  depends_on 'openssl'
+  depends_on 'scapy' => :python
 
   def install
     python do
