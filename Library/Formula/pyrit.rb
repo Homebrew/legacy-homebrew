e66173d313999f98f961aa836dd7c9fdcf7ab179
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-03 20:13:27 -0600

    change to pypcap

--- Library/Formula/pyrit.rb
+++ Library/Formula/pyrit.rb
@@ -6,7 +6,7 @@ class Pyrit < Formula
   sha1 '3041c0d593c21df88f41416be872eca0997cf22b'
 
   depends_on :python
-  depends_on "pylibpcap"
+  depends_on "pypcap"
   depends_on "libdnet"
 
   def install
