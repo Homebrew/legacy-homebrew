b10bc18ab16e0ae4102fc2361e9fce48b44da2d5
David Groulx <david@sandbendersoftware.com>
2013-12-04 22:39:35 -0500

    Install serf 0.2.1

--- Library/Formula/serf.rb
+++ Library/Formula/serf.rb
@@ -1,38 +1,18 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
-class 021DarwinAmd < Formula
+class Serf < Formula
   homepage 'http://serfdom.io'
-  url 'https://dl.bintray.com/mitchellh/serf/0.2.1_darwin_amd64.zip'
-  sha1 '530df1e78423d22e6f087a1cfd886a9376cbebd3'
+  version '0.2.1'
 
-  # depends_on 'cmake' => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  if Hardware.is_64_bit? 
+    url 'https://dl.bintray.com/mitchellh/serf/0.2.1_darwin_amd64.zip'
+    sha256 'e6cf1d071431efbfcfdc9dab19cef17df6703b692022892ef469796410ff3cfb'
+  else
+    url 'https://dl.bintray.com/mitchellh/serf/0.2.1_darwin_386.zip'
+    sha256 'ebd6f8f66ed10b43a7db469d2310fcbf253cd9c08b7e61bec4155cddc383c0bd'
+  end 
 
   def install
-    # ENV.j1  # if your formula's build system can't parallelize
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test 0.2.1_darwin_amd`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "--version"`.
-    system "false"
+    bin.install Dir['*']
   end
 end
