7ad291d55e77cbbb40c5a42c788c44c53c2a4639
seanjreilly <seanjreilly@gmail.com>
2013-12-05 16:06:15 +0000

    groovy 2.2.1
    Closes #24963.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -2,14 +2,8 @@ require 'formula'
 
 class Groovy < Formula
   homepage 'http://groovy.codehaus.org/'
-  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.2.0.zip'
-  sha1 'e4c80b14fd21b168c1682b26f117e6427f9e712f'
-
-  devel do
-    url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.2.0-rc-3.zip'
-    sha1 '4db741568aa6ab1ba333f40f4e25aa63653faa76'
-    version '2.2.0-rc3'
-  end
+  url 'http://dist.groovy.codehaus.org/distributions/groovy-binary-2.2.1.zip'
+  sha1 'a366e74fe0da4e4eab98f6e6b1b996a06fd37046'
 
   option 'invokedynamic', "Install the InvokeDynamic version of Groovy (only works with Java 1.7+)"
 
