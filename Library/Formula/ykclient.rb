e690be79abe32c4f6670c7353908d5c90830a64c
Yann Hodique <yann.hodique@gmail.com>
2013-12-05 10:06:53 -0800

    add pkg-config dependency

--- Library/Formula/ykclient.rb
+++ Library/Formula/ykclient.rb
@@ -5,6 +5,8 @@ class Ykclient < Formula
   url 'http://yubico.github.io/yubico-c-client/releases/ykclient-2.12.tar.gz'
   sha1 '518ce53ba9ef61a619f9150778f19fad23014a9c'
 
+  depends_on 'pkg-config' => :build
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
