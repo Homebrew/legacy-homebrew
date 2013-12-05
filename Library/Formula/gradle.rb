3a3a7feca22a58057de05e31ffc025def7e30c6c
seanjreilly <seanjreilly@gmail.com>
2013-12-05 16:30:03 +0000

    gradle 1.10-rc-1
    Closes #24964.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -5,6 +5,12 @@ class Gradle < Formula
   url 'http://services.gradle.org/distributions/gradle-1.9-bin.zip'
   sha1 'e35f498e7e52d2ee3a12d313eb4c3d7805e6a346'
 
+  devel do
+    url 'http://services.gradle.org/distributions/gradle-1.10-rc-1-bin.zip'
+    sha1 '3f55ac6595e94675ff4d568108d643cf6348f747'
+    version '1.10-rc-1'
+  end
+
   def install
     libexec.install %w[bin lib]
     bin.install_symlink libexec+'bin/gradle'
