a0d860ed29e653206c9e5c639560a69cb1be57b2
Steffen Matthischke <steffen.matthischke@gmail.com>
2013-12-05 16:01:40 +0100

    unrar 5.0.14
    Closes #24960.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/unrar.rb
+++ Library/Formula/unrar.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Unrar < Formula
   homepage 'http://www.rarlab.com'
-  url 'http://www.rarlab.com/rar/unrarsrc-5.0.13.tar.gz'
-  sha1 '411644d3d31fa021163eab66751d4f4fa9bce8c7'
+  url 'http://www.rarlab.com/rar/unrarsrc-5.0.14.tar.gz'
+  sha1 'f6826b330668698fca227b3331e2bab564932d7e'
 
   def install
     system "make"
