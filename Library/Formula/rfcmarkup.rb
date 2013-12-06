530395ff3e46da6c93c4add5ead4585b26241f8f
Adam Vandenberg <flangy@gmail.com>
2013-12-05 22:53:34 -0800

    rfcmarkup 1.106

--- Library/Formula/rfcmarkup.rb
+++ Library/Formula/rfcmarkup.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rfcmarkup < Formula
   homepage 'http://tools.ietf.org/tools/rfcmarkup/'
-  url 'http://tools.ietf.org/tools/rfcmarkup/rfcmarkup-1.101.tgz'
-  sha1 'e0c6822185b5b2d73dc72e169d87fab9c50dc2a8'
+  url 'http://tools.ietf.org/tools/rfcmarkup/rfcmarkup-1.106.tgz'
+  sha1 '1df2fbaea768ab5390c83c2f6185670a429d0051'
 
   def install
     bin.install "rfcmarkup"
