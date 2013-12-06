790b078359d023f9250e8e31662836a4d4fdb160
Eric Radman <ericshane@eradman.com>
2013-12-06 13:31:51 -0500

    - Remove header noise - chmod 644 - Use version scanned from URL

--- Library/Formula/entr.rb
+++ Library/Formula/entr.rb
@@ -1,12 +1,8 @@
-#!/usr/bin/env ruby
-#-*-mode: Ruby; coding: utf-8;-*-
-
 require 'formula'
 
 class Entr < Formula
   url 'http://entrproject.org/code/entr-2.2.tar.gz'
   homepage 'https://bitbucket.org/eradman/entr'
-  version '2.2'
   sha1 '71eedf5d9397a08a231f0ab400f5aeec4f77571b'
 
   def install
