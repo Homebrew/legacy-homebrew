a869da6e274ea8988b304dcf8015c5518227bede
Stefan <humdedum@icloud.com>
2013-12-06 15:27:29 +0100

    polarssl 1.3.2
    Closes #24994.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/polarssl.rb
+++ Library/Formula/polarssl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Polarssl < Formula
   homepage 'http://polarssl.org/'
-  url 'https://polarssl.org/download/polarssl-1.3.1-gpl.tgz'
-  sha1 'b33856a1b2f736b18a49a20d48986bce6b3133f5'
+  url 'https://polarssl.org/download/polarssl-1.3.2-gpl.tgz'
+  sha1 '2e671bb1bc60ce00db2352be63c4ea38ad2c8b03'
 
   depends_on 'cmake' => :build
 
