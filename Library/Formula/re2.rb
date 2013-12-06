c5abf40afbb48007928dd8c7dd0eb97cdbbe25ad
ipfans <ipfanscn@gmail.com>
2013-12-06 17:45:38 +0800

    re2 fix for osx 10.9

--- Library/Formula/re2.rb
+++ Library/Formula/re2.rb
@@ -6,7 +6,17 @@ class Re2 < Formula
   url 'https://re2.googlecode.com/files/re2-20131024.tgz'
   sha1 '90a356fb205c5004cc4f08e45e02994b898b592a'
 
+  def patch
+    if MacOS.version >= :mavericks
+      [
+        "https://gist.github.com/ipfans/7821015/raw/e32a604c6cb23b091de9f8139e7cd503dad08147/re2-mavericks"
+      ]
+    end
+
   def install
+    if MacOS.version >= :mavericks
+      ENV.append "CXXFLAGS", "-std=c++11 -DUSE_CXX0X"
+    end
     system "make", "install", "prefix=#{prefix}"
   end
 end
