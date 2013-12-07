97902f8fe9bd9f1c374b52df0d95741bc61743ff
Adam Vandenberg <flangy@gmail.com>
2013-12-06 20:21:24 -0800

    parallel 20131122

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Parallel < Formula
   homepage 'http://savannah.gnu.org/projects/parallel/'
-  url 'http://ftpmirror.gnu.org/parallel/parallel-20131022.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20131022.tar.bz2'
-  sha256 '93280c7a03c18b07685157af36f2dfc82bbc59828acbdfe86ed27171a442b6f9'
+  url 'http://ftpmirror.gnu.org/parallel/parallel-20131122.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20131122.tar.bz2'
+  sha256 'ef5e0af618cd71c2a2d96ab3aa800ca44e6fab830092176db4b3468b747e29d0'
 
   conflicts_with 'moreutils',
     :because => "both install a 'parallel' executable."
