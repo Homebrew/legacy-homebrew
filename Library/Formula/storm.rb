b0083dad218fa8c7309ee0a3092c9a6465870123
Paul Bergeron <paul.d.bergeron@gmail.com>
2013-12-06 23:32:43 -0800

    Storm 0.9.0.1
    Closes #25019.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/storm.rb
+++ Library/Formula/storm.rb
@@ -2,13 +2,8 @@ require 'formula'
 
 class Storm < Formula
   homepage 'https://github.com/nathanmarz/storm/wiki'
-  url 'https://dl.dropbox.com/u/133901206/storm-0.8.2.zip'
-  sha1 'abb533c90f5b9eeaafb9136670091fc894fce169'
-
-  devel do
-    url 'https://dl.dropboxusercontent.com/s/p5wf0hsdab5n9kn/storm-0.9.0-rc2.zip'
-    sha1 '1fc891d29259a431bcd297cbcc24888152cc25bb'
-  end
+  url 'https://dl.dropboxusercontent.com/s/dj86w8ojecgsam7/storm-0.9.0.1.zip'
+  sha1 '230abcc15a9e1358442429fd1c856dc12a3be920'
 
   def install
     libexec.install Dir['*']
