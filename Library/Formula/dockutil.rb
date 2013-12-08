5312d19ee96c1e542f8c43990749ff302dadc50d
James Hart <hjhart@gmail.com>
2013-12-08 09:53:22 -0800

    Added formula for dockutil 1.1.4

---
+++ Library/Formula/dockutil.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Dockutil < Formula
+  homepage 'https://github.com/kcrawford/dockutil'
+  url 'https://raw.github.com/kcrawford/dockutil/master/scripts/dockutil'
+  sha1 'c54a9208b47795d0453f8f36d2056a866f8b9187'
+  version '1.1.4'
+
+  depends_on :python
+
+  resource 'plistlib' do
+    url 'https://pypi.python.org/packages/source/p/plist/plist-0.2.tar.gz'
+    sha1 'eac8a0d71a20515f955101429101b3b9f445f809'
+  end
+
+  def install
+    path = File.join(prefix, 'bin')
+    mkdir path
+    cp 'dockutil', path
+  end
+
+  test do
+    assert_equal "1.1.4", `#{bin}/dockutil --version`.strip
+  end
+end
