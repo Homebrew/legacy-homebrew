d75a5760d730495f1c12c0c9d20524e55642fc71
Adam Vandenberg <flangy@gmail.com>
2013-12-08 21:08:08 -0800

    t1utils 1.38

--- Library/Formula/t1utils.rb
+++ Library/Formula/t1utils.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class T1utils < Formula
   homepage 'http://www.lcdf.org/type/'
-  url 'http://www.lcdf.org/type/t1utils-1.37.tar.gz'
-  sha256 '42bdce77aaf12b33ca6d193e01a2d2c0012f755435a6d25921f94733ee61cec3'
+  url 'http://www.lcdf.org/type/t1utils-1.38.tar.gz'
+  sha1 'a97ba119a2e376db49d9d4911472c0033e2fece8'
 
   def install
     system "./configure", "--disable-dependency-tracking",
