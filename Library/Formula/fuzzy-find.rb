e61dd11bb8e80250f75b972b56a8322e6baaf709
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2013-12-09 21:24:26 +0100

    fuzzy-find 0.6.0
    Closes #25091.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/fuzzy-find.rb
+++ Library/Formula/fuzzy-find.rb
@@ -2,15 +2,16 @@ require 'formula'
 
 class FuzzyFind < Formula
   homepage "https://github.com/silentbicycle/ff"
-  url "https://github.com/silentbicycle/ff/archive/v0.5-first-form.tar.gz"
-  version "0.5"
-  sha1 "698526ae2c725763da0990a4112632ac4b3194c8"
+  url "https://github.com/silentbicycle/ff/archive/v0.6-flag-features.tar.gz"
+  version "0.6.0"
+  sha1 "13429471e6b51613361128e4d31f2e62013e2c2b"
 
   head 'https://github.com/silentbicycle/ff.git'
 
   def install
     system "make"
     bin.install "ff"
+    man1.install "ff.1"
   end
 
   test do
