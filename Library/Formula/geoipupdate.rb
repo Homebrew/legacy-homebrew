5f057985e6a765054308cc8bf95a09b8e9c6c3bc
Jack Nagel <jacknagel@gmail.com>
2013-12-09 12:19:24 -0600

    geoipupdate: use head block

--- Library/Formula/geoipupdate.rb
+++ Library/Formula/geoipupdate.rb
@@ -4,9 +4,10 @@ class Geoipupdate < Formula
   homepage 'https://github.com/maxmind/geoipupdate'
   url 'https://github.com/maxmind/geoipupdate/releases/download/v2.0.0/geoipupdate-2.0.0.tar.gz'
   sha1 'd3c90ad9c9ad5974e8a5a30c504e7827978ddea7'
-  head 'https://github.com/maxmind/geoipupdate.git'
 
-  if build.head?
+  head do
+    url 'https://github.com/maxmind/geoipupdate.git'
+
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
     depends_on 'libtool' => :build
