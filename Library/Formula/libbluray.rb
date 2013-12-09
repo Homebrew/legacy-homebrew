ca498ad2bc536c6e5d137ddf5e6bc0aa878fe8b0
Bruno R. Barreyra <barreyra@gmail.com>
2013-12-09 10:43:46 -0800

    libbluray: adding freetype support

--- Library/Formula/libbluray.rb
+++ Library/Formula/libbluray.rb
@@ -5,6 +5,10 @@ class Libbluray < Formula
   url 'ftp://ftp.videolan.org/pub/videolan/libbluray/0.4.0/libbluray-0.4.0.tar.bz2'
   sha1 '39984aae77efde2e0917ed7e183ebf612813d7f3'
 
+  def patches
+    DATA
+  end
+
   head do
     url 'git://git.videolan.org/libbluray.git'
 
@@ -14,6 +18,7 @@ class Libbluray < Formula
   end
 
   depends_on 'pkg-config' => :build
+  depends_on 'freetype' => :recommended
 
   def install
     ENV.libxml2
@@ -24,3 +29,19 @@ class Libbluray < Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/src/libbluray/decoders/textst_render.c b/src/libbluray/decoders/textst_render.c
+index 780b640..ffcb1bd 100644
+--- a/src/libbluray/decoders/textst_render.c
++++ b/src/libbluray/decoders/textst_render.c
+@@ -30,7 +30,7 @@
+ #ifdef HAVE_FT2
+ #include <ft2build.h>
+ #include FT_FREETYPE_H
+-#include <freetype/ftsynth.h>
++#include FT_SYNTHESIS_H
+ #endif
+ 
+ #include "textst_render.h"
+
