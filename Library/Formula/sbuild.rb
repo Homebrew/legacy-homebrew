f1b8b9bacc60335c8fb1566a3b3ccf2746e259cf
Tobias Roeser <le.petit.fou@web.de>
2013-12-09 14:29:52 +0100

    sbuild.rb: Version bump to SBuild 0.7.0

--- Library/Formula/sbuild.rb
+++ Library/Formula/sbuild.rb
@@ -2,13 +2,15 @@ require 'formula'
 
 class Sbuild < Formula
   homepage 'http://sbuild.tototec.de/sbuild/projects/sbuild/wiki'
-  url 'http://sbuild.tototec.de/sbuild/attachments/download/75/sbuild-0.6.0-dist.zip'
-  sha1 '21a75d6ff08d56d470da047d73a96ebddc8eee49'
+  url 'http://sbuild.tototec.de/sbuild/attachments/download/82/sbuild-0.7.0-dist.zip'
+  sha1 '02e593136d5c24269ea9243e88ee7701cb50b120'
 
   def install
     libexec.install Dir['*']
     system "chmod +x #{libexec}/bin/sbuild"
+    system "chmod +x #{libexec}/bin/sbuild-debug"
     bin.install_symlink libexec/"bin/sbuild"
+    bin.install_symlink libexec/"bin/sbuild-debug"
   end
 
   test do
