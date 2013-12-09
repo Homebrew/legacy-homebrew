0444efc40e8a0694160f0941d9775c17bb2c13e4
George Gastaldi <gegastaldi@gmail.com>
2013-12-09 17:15:12 -0200

    jbossforge 1.4.3.Final
    Closes #25088.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class JbossForge < Formula
-  homepage 'http://jboss.org/forge'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=1.3.1.Final&e=zip'
-  version '1.3.1.Final'
-  sha1 'e693734821045e4c5dd9872bf8e36bfbd2891130'
+  homepage 'http://forge.jboss.org/'
+  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=1.4.3.Final&e=zip'
+  version '1.4.3.Final'
+  sha1 '1c6a9491cfd6eee2c4484bf350acd3515231ffad'
 
   def install
     rm_f Dir["bin/*.bat"]
