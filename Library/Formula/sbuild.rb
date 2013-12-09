814a2f8b5647cfd3c995f1505536a8e582d70664
Tobias Roeser <le.petit.fou@web.de>
2013-12-09 14:29:52 +0100

    sbuild 0.7.0
    Closes #25075.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/sbuild.rb
+++ Library/Formula/sbuild.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Sbuild < Formula
   homepage 'http://sbuild.tototec.de/sbuild/projects/sbuild/wiki'
-  url 'http://sbuild.tototec.de/sbuild/attachments/download/75/sbuild-0.6.0-dist.zip'
-  sha1 '21a75d6ff08d56d470da047d73a96ebddc8eee49'
+  url 'http://sbuild.tototec.de/sbuild/attachments/download/82/sbuild-0.7.0-dist.zip'
+  sha1 '02e593136d5c24269ea9243e88ee7701cb50b120'
 
   def install
     libexec.install Dir['*']
