3374782033edf9db9f4d40422995335c7b575afe
mhemeryck <martijn.hemeryck@soundtalks.com>
2013-12-10 15:52:02 +0100

    adapted according to `brew audit`:
      - removed trailing space
      - removed redundant version number

--- Library/Formula/cfv.rb
+++ Library/Formula/cfv.rb
@@ -3,9 +3,8 @@ require 'formula'
 class Cfv < Formula
   homepage 'http://cfv.sourceforge.net/'
   url 'http://downloads.sourceforge.net/project/cfv/cfv/1.18.3/cfv-1.18.3.tar.gz'
-  version '1.18.3'
   sha1 '8f3361fb9c13fe2645f1df8c177c61459f50e846'
-  
+
   depends_on :python
 
   def install
