3e60c4ccbc734752c13b1c1c5a0cf23b8760d417
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-10 11:45:15 +0800

    Remove coreutils dependency > - Instead of using grealpath from coreutils, use inreplace to insert HOMEBREW #{prefix} path > - Make maven dependency conditional, if head

--- Library/Formula/gremlin.rb
+++ Library/Formula/gremlin.rb
@@ -4,15 +4,14 @@ class Gremlin < Formula
   homepage 'http://gremlin.tinkerpop.com'
   url 'http://tinkerpop.com/downloads/gremlin/gremlin-groovy-2.4.0.zip'
   sha1 'ee7979e2a0a773a41ca998d81d3712193d5f5403'
-  head 'https://github.com/tinkerpop/gremlin.git', :branch => 'master'
-  depends_on "maven" => :build
-  depends_on "coreutils"
-
-  def patches
-    DATA
+  head do
+    url 'https://github.com/tinkerpop/gremlin.git', :branch => 'master'
+    depends_on "maven" => :build
   end
 
   def install
+    inreplace 'bin/gremlin.sh', '`dirname $0`/..', prefix
+    inreplace 'bin/gremlin.sh', '/lib/', '/libexec/'
     if build.head?
       system 'mvn', 'clean', 'install'
     else
@@ -22,16 +21,3 @@ class Gremlin < Formula
     end
   end
 end
-
-__END__
-diff --git a/bin/gremlin.sh b/bin/gremlin.sh
---- a/bin/gremlin.sh
-+++ b/bin/gremlin.sh
-@@ -5,7 +5,7 @@
-     CP=$( echo `dirname $0`/../lib/*.jar . | sed 's/ /;/g')
-     ;;
-   *)
--    CP=$( echo `dirname $0`/../lib/*.jar . | sed 's/ /:/g')
-+    CP=$( echo `dirname $(grealpath $0)`/../libexec/*.jar . | sed 's/ /:/g')
- esac
- #echo $CP
