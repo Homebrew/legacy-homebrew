e6f086c5f930ca9e6020a01fefb20aadfb289d1d
Adam Vandenberg <flangy@gmail.com>
2013-12-09 19:26:11 -0800

    discount 2.1.7

--- Library/Formula/discount.rb
+++ Library/Formula/discount.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Discount < Formula
   homepage 'http://www.pell.portland.or.us/~orc/Code/discount/'
-  url 'http://www.pell.portland.or.us/~orc/Code/discount/discount-2.1.6.tar.bz2'
-  sha1 'a7461731613d0e2f88dd19af9feb860c191e8234'
+  url 'http://www.pell.portland.or.us/~orc/Code/discount/discount-2.1.7.tar.bz2'
+  sha1 '517bcf7409d8c02b3e57f51264b2e110f8a03120'
 
   conflicts_with 'markdown',
     :because => 'both discount and markdown ship a `markdown` executable.'
