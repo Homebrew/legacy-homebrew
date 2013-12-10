7728a1c76f662ae92121218728dfd38a41edaecc
James Hart <hjhart@gmail.com>
2013-12-10 00:10:24 -0800

    Using latest commit as tar archive

--- Library/Formula/dockutil.rb
+++ Library/Formula/dockutil.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dockutil < Formula
   homepage 'https://github.com/kcrawford/dockutil'
-  url 'https://raw.github.com/kcrawford/dockutil/master/scripts/dockutil'
-  sha1 'c54a9208b47795d0453f8f36d2056a866f8b9187'
+  url 'https://github.com/kcrawford/dockutil/archive/4ec75cf3d4dcd3ca3923525c6d659e40baf174b9.tar.gz'
+  sha1 'bfb8db52d30dc254a76a34d8434d66b2bf8792e7'
   version '1.1.4'
 
   depends_on :python
@@ -13,7 +13,6 @@ class Dockutil < Formula
     sha1 'eac8a0d71a20515f955101429101b3b9f445f809'
   end
 
-
   def install
     install_args = [ "setup.py", "install", "--prefix=#{libexec}" ]
 
@@ -21,7 +20,7 @@ class Dockutil < Formula
       resource('plistlib').stage { system python, *install_args }
     end
 
-    bin.install 'dockutil'
+    bin.install 'scripts/dockutil'
   end
 
   test do
