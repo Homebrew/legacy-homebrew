329eaaacebc28cf61e7c75b6672e7ac7681a55da
Lee Hanxue <hanxue.developer@gmail.com>
2013-12-10 11:34:00 +0800

    Change maven dependency for head only

--- Library/Formula/rexster.rb
+++ Library/Formula/rexster.rb
@@ -4,9 +4,10 @@ class Rexster < Formula
   homepage 'http://rexster.tinkerpop.com'
   url 'http://tinkerpop.com/downloads/rexster/rexster-server-2.4.0.zip'
   sha1 '9fb8d7ac4c797f44bed03d9309479841b5430e50'
-  head 'https://github.com/tinkerpop/rexster.git', :branch => 'master'
-  depends_on "maven" => :build
-
+  head do
+    url 'https://github.com/tinkerpop/rexster.git', :branch => 'master'
+    depends_on "maven" => :build
+  end
   def patches
     DATA
   end
