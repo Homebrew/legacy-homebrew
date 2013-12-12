4e2477ff1898523e7c7d9f25f91ae40f6b297f9d
Jack Nagel <jacknagel@gmail.com>
2013-12-12 15:42:31 -0600

    Combine superenv tests

--- Library/Homebrew/test/test_ENV.rb
+++ Library/Homebrew/test/test_ENV.rb
@@ -138,7 +138,24 @@ end
 class SuperenvTests < Test::Unit::TestCase
   include SharedEnvTests
 
+  attr_reader :env, :bin
+
   def setup
     @env = {}.extend(Superenv)
+    @bin = HOMEBREW_REPOSITORY/"Library/ENV/#{MacOS::Xcode.version}"
+    bin.mkpath
+  end
+
+  def test_bin
+    assert_equal bin, Superenv.bin
+  end
+
+  def test_initializes_deps
+    assert_equal [], env.deps
+    assert_equal [], env.keg_only_deps
+  end
+
+  def teardown
+    bin.rmtree
   end
 end
