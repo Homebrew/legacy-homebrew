715f9bac5e036843f177f5779da7e14b61610c41
Dennis Luxen <info@project-osrm.org>
2013-12-12 16:20:21 -0500

    libstxx 1.4.0
    fix compilation on 10.7 and 10.8
    
    apply patch on old OS X versions, further include fixes for 10.7/10.8

--- Library/Formula/libstxxl.rb
+++ Library/Formula/libstxxl.rb
@@ -2,51 +2,21 @@ require 'formula'
 
 class Libstxxl < Formula
   homepage 'http://stxxl.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/stxxl/stxxl/1.3.1/stxxl-1.3.1.tar.gz'
-  sha1 '5fba2bb26b919a07e966b2f69ae29aa671892a7d'
+  url 'https://github.com/stxxl/stxxl/archive/1.4.0.tar.gz'
+  sha1 '57230314bf136e477d6d96d0f68030af1f652278'
 
   # issue has been rectified in upstream and future 1.4.0 release
-  patch :DATA if MacOS.version >= :mavericks
+  depends_on 'cmake' => :build
+
+  patch do
+    url "https://github.com/stxxl/stxxl/commit/d23dd29b9492061a8da2201ab2585914b66bb9c3.diff"
+    sha1 "af0e84a74f20047def5a76146057f3c176f12845"
+  end
 
   def install
-    ENV['COMPILER'] = ENV.cxx
-    if MacOS.version >= :mavericks
-      inreplace 'make.settings.gnu' do |s|
-        s.gsub! /USE_MACOSX.*no/, 'USE_MACOSX ?= yes#'
-        s.gsub! /#STXXL_SPECIFIC\s*\+=.*$/, 'STXXL_SPECIFIC += -std=c++0x'
-      end
+    mkdir "build" do
+      system "cmake", "..", "-DCMAKE_BUILD_TYPE=Release"
+      system "make install"
     end
-    system "make", "config_gnu", "USE_MACOSX=yes"
-    system "make", "library_g++", "USE_MACOSX=yes"
-
-    prefix.install 'include'
-    lib.install 'lib/libstxxl.a'
   end
 end
-
-__END__
-Index: utils/mlock.cpp
-===================================================================
---- stxxl-1.3.1/utils/mlock.cpp (revision 3229)
-+++ stxxl-1.3.1/utils/mlock.cpp (working copy)
-@@ -18,6 +18,9 @@
- #include <iostream>
- #include <sys/mman.h>
-
-+#include <chrono>
-+#include <thread>
-+
- int main(int argc, char ** argv)
- {
-     if (argc == 2) {
-@@ -28,8 +31,9 @@
-                 c[i] = 42;
-             if (mlock(c, M) == 0) {
-                 std::cout << "mlock(, " << M << ") successful, press Ctrl-C to finish" << std::endl;
-+                std::chrono::seconds duration(86400);
-                 while (1)
--                    sleep(86400);
-+                    std::this_thread::sleep_for(duration);
-             } else {
-                 std::cerr << "mlock(, " << M << ") failed!" << std::endl;
-                 return 1;
