ec686d9fb67c8deff0c0001351636eede5a6dcca
Butch Anton <butch@butch.net>
2013-12-13 11:09:41 -0600

    Passing -D_FORTIFY_SOURCE=0 disables buffer overflow protection.

--- Library/Formula/hercules.rb
+++ Library/Formula/hercules.rb
@@ -10,8 +10,8 @@ class Hercules < Formula
   depends_on 'gawk'
 
   def install
-    # For newer compilers, we need to use _FORTIFY_SOURCE=0 to do proper
-    # bound checking.
+    # For newer compilers, we need to use _FORTIFY_SOURCE=0 to disable
+    # buffer overflow protection.
     ENV.append 'CPPFLAGS', "-D_FORTIFY_SOURCE=0" if MacOS.version >= :mavericks
 
     # Since Homebrew optimizes for us, tell Hercules not to.
