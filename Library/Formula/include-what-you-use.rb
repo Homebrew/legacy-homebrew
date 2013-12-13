d5111680a0ee8e69fd2fbc1fd241fb2ca884ec0b
Daniel Russel <drussel@gmail.com>
2013-12-12 20:43:02 -0800

    provide different link
    curl succeeds in downloading the link for me, however I don't have too much confidence.

--- Library/Formula/include-what-you-use.rb
+++ Library/Formula/include-what-you-use.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class IncludeWhatYouUse < Formula
   homepage 'https://code.google.com/p/include-what-you-use/'
-  url 'https://doc-00-9o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/63i6dla0uj8qk3q6nbbd9m5b1rjkkrmk/1385755200000/18138411108955853598/*/0ByBfuBCQcURXQktsT3ZjVmZtWkU?h=16653014193614665626&e=download'
+  url 'https://doc-00-9o-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/82e24tdlmk93f5s0cpudtjq9e68ka4lg/1386907200000/18138411108955853598/*/0ByBfuBCQcURXQktsT3ZjVmZtWkU?h=16653014193614665626&amp;e=download'
   version '3.3'
   sha1 'f49dd094300b648543e3510ebd8101dbab23056f'
 
