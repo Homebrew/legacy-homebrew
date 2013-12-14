1e86bd262d7ad887412453bf3f21a4bd87f51b58
Mike McQuaid <mike@mikemcquaid.com>
2013-12-14 18:15:12 +0000

    doctor: check for migrated origin Git remote.
    Closes #25211.

--- Library/Homebrew/cmd/doctor.rb
+++ Library/Homebrew/cmd/doctor.rb
@@ -775,9 +775,9 @@ def check_git_origin
       Without a correctly configured origin, Homebrew won't update
       properly. You can solve this by adding the Homebrew remote:
         cd #{HOMEBREW_REPOSITORY}
-        git remote add origin https://github.com/mxcl/homebrew.git
+        git remote add origin https://github.com/Homebrew/homebrew.git
       EOS
-    elsif origin !~ /mxcl\/homebrew(\.git)?$/ then <<-EOS.undent
+    elsif origin !~ /Homebrew\/homebrew(\.git)?$/ then <<-EOS.undent
       Suspicious git origin remote found.
 
       With a non-standard origin, Homebrew won't pull updates from
@@ -786,7 +786,7 @@ def check_git_origin
 
       Unless you have compelling reasons, consider setting the
       origin remote to point at the main repository, located at:
-        https://github.com/mxcl/homebrew.git
+        https://github.com/Homebrew/homebrew.git
       EOS
     end
   end
