ace01812e06d306084f5584e03b575ec0b34aba1
Jack Nagel <jacknagel@gmail.com>
2013-12-14 10:18:55 -0600

    libsmi: use https download
    Closes #25182.

--- Library/Formula/libsmi.rb
+++ Library/Formula/libsmi.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Libsmi < Formula
   homepage 'http://www.ibr.cs.tu-bs.de/projects/libsmi/'
-  url 'ftp://ftp.ibr.cs.tu-bs.de/pub/local/libsmi/libsmi-0.4.8.tar.gz'
+  url 'https://www.ibr.cs.tu-bs.de/projects/libsmi/download/libsmi-0.4.8.tar.gz'
   sha1 '77c512ccbdd29667d152398b0dcde533aed57b49'
 
   def install
