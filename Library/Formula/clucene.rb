bf7c8f91118b28332d8b33043a6c97cb801e5436
Leo Franchi <lfranchi@kde.org>
2013-12-13 21:09:31 -0500

    clucene: fix --HEAD build
    Closes #25191.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/clucene.rb
+++ Library/Formula/clucene.rb
@@ -15,10 +15,14 @@ class Clucene < Formula
       # Fix libpthread dependencies in OS X 10.9
       # Based on MacPorts patches: http://trac.macports.org/ticket/40899
       # Reported upstream: http://sourceforge.net/p/clucene/bugs/219/
-      {:p0 => [
-        'https://gist.github.com/tlvince/7934499/raw/d0859996dbda8f4cf643d091ae6b491f0a64da59/CLucene-LuceneThreads.h.diff',
-        'https://gist.github.com/tlvince/7935339/raw/fd78b1ada278eaf1904e1437efa0f2a1265041a9/CLucene-config-repl_tchar.h.diff'
-      ]}
+      if build.head?
+        'https://gist.github.com/lfranchi/7954811/raw/828176c01a8f2c1c11eff43bf6773242955dabab/CLucene-HEAD-mavericks.patch'
+      else
+        {:p0 => [
+          'https://gist.github.com/tlvince/7934499/raw/d0859996dbda8f4cf643d091ae6b491f0a64da59/CLucene-LuceneThreads.h.diff',
+          'https://gist.github.com/tlvince/7935339/raw/fd78b1ada278eaf1904e1437efa0f2a1265041a9/CLucene-config-repl_tchar.h.diff'
+        ]}
+      end
     end
   end
 
