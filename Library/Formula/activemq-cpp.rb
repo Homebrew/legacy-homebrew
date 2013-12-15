c302d7f0b5ffca77301c9b35cbca07bca491354f
Brett Koonce <koonce@gmail.com>
2013-12-15 12:02:42 -0600

    activemq-cpp 3.8.2
    Closes #25232.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/activemq-cpp.rb
+++ Library/Formula/activemq-cpp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class ActivemqCpp < Formula
   homepage 'http://activemq.apache.org/cms/index.html'
-  url 'http://apache.dattatec.com/activemq/activemq-cpp/source/activemq-cpp-library-3.8.1-src.tar.bz2'
-  sha1 'd82d6c647d507ef4a44e28665f68def7a79e5b1d'
+  url 'http://apache.dattatec.com/activemq/activemq-cpp/source/activemq-cpp-library-3.8.2-src.tar.bz2'
+  sha1 '6fcaeb278f2d359343ccef4b5814f0092a4d7b54'
 
   depends_on 'autogen' => :build
   depends_on :autoconf
