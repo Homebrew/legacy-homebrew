9ef89672731056f308eca0f757ff7e2e4a6804b4
yanniks <kontakt@yanniks.de>
2013-12-15 13:50:53 +0100

    daq 2.0.1
    Closes #25227.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/daq.rb
+++ Library/Formula/daq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Daq < Formula
   homepage 'http://www.snort.org/'
-  url 'http://www.snort.org/dl/snort-current/daq-1.1.1.tar.gz'
-  sha1 '60d3ccb3fb34a0dfe9f9747a3ade45f130716337'
+  url 'http://www.snort.org/dl/snort-current/daq-2.0.1.tar.gz'
+  sha1 '63431274e868195a8c4efb064b204aa5a151d387'
 
   def install
     system "./configure", "--disable-dependency-tracking",
