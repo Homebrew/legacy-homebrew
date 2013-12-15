8887b463d556a66cd6c1b5261b606cffdead469a
Brett Koonce <koonce@gmail.com>
2013-12-15 12:26:45 -0600

    xorriso 1.3.4
    Closes #25233.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xorriso.rb
+++ Library/Formula/xorriso.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Xorriso < Formula
   homepage 'http://www.gnu.org/software/xorriso/'
-  url 'http://ftpmirror.gnu.org/xorriso/xorriso-1.3.2.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/xorriso/xorriso-1.3.2.tar.gz'
-  sha1 'e897e994ed01aa13070481adccfdf2a97044bdd4'
+  url 'http://ftpmirror.gnu.org/xorriso/xorriso-1.3.4.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/xorriso/xorriso-1.3.4.tar.gz'
+  sha1 '2478393074b821c26dbadd294158b858054d5038'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
