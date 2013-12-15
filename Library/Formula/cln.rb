7a444824e1ef8902dde2909ff65070a3805b56f2
Adam Vandenberg <flangy@gmail.com>
2013-12-15 13:19:39 -0800

    cln: patch for clang

--- Library/Formula/cln.rb
+++ Library/Formula/cln.rb
@@ -5,7 +5,12 @@ class Cln < Formula
   url 'http://www.ginac.de/CLN/cln-1.3.3.tar.bz2'
   sha1 '11c56780eb83ed54f2ad1ecef7f0dc0f609c426d'
 
-  depends_on "gmp"
+  depends_on 'gmp'
+
+  # Patch for Clang from MacPorts
+  def patches
+    "https://trac.macports.org/export/114806/trunk/dports/math/cln/files/patch-clang.diff"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}",
