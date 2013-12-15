80ff574e2796ff8f63d2bcb3e08aa0b099f37d90
Mike Barrett <mike182uk@gmail.com>
2013-12-15 20:37:15 +0000

    Library/Formula/rbenv-communal-gems.rb
    set version to 1.0.1

--- Library/Formula/rbenv-communal-gems.rb
+++ Library/Formula/rbenv-communal-gems.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RbenvCommunalGems < Formula
   homepage 'https://github.com/tpope/rbenv-communal-gems'
-  url 'https://github.com/tpope/rbenv-communal-gems/archive/cbc18bc791b3a25a6b1df27e256d02b9695bb537.tar.gz'
-  sha1 'e101b157bec6299c07f52adc3f2073b98cd27c79'
+  url 'https://github.com/tpope/rbenv-communal-gems/archive/v1.0.1.tar.gz'
+  sha1 '39360238e89f2d723409c3de2a4132f561de7bb9'
 
   depends_on 'rbenv'
 
