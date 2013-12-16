fe900f9580fea05e75ced1457debc804c0c3a871
David Paniz <davidpaniz@gmail.com>
2013-12-16 19:28:48 -0200

    PredictionIO v0.6.4 - First formula for PredictionIO - #25209

---
+++ Library/Formula/predictionio.rb
@@ -0,0 +1,35 @@
+require 'formula'
+
+class Predictionio < Formula
+  homepage 'http://prediction.io/'
+  url 'http://download.prediction.io/PredictionIO-0.6.4.zip'
+  sha1 '29422514f5f71eaa8bcf7e689083471f1ba93ca3'
+
+  depends_on 'mongodb'
+  depends_on 'hadoop'
+
+  def install
+    rm_f Dir["bin/*.bat"]
+
+    libexec.install Dir['*']
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+
+    mv "#{bin}/start-all.sh", "#{bin}/predictionio-start-all.sh"
+    mv "#{bin}/stop-all.sh", "#{bin}/predictionio-stop-all.sh"
+
+    system "#{libexec}/bin/setup-vendors.sh"
+    system "#{libexec}/bin/setup.sh"
+  end
+
+  def caveats; <<-EOS.undent
+    Before use, you must create a user. Run:
+      #{bin}/user
+
+    After that, PredictionIO is ready.
+    Start using:
+      predictionio-start-all.sh
+    Stop using:
+      predictionio-stop-all.sh
+    EOS
+  end
+end
