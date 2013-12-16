b36dd50bc3cf849195a97ae34b1e74d56e9c0875
Bryce Chidester <bryce@cobryce.com>
2013-12-16 09:42:37 -0800

    texapp 0.6.6
    Closes #25261.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/texapp.rb
+++ Library/Formula/texapp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Texapp < Formula
   homepage 'http://www.floodgap.com/software/texapp/'
-  url 'http://www.floodgap.com/software/texapp/dist0/0.6.05.txt'
-  sha1 '146ab03cf669e1c73fed577c7b5f6922b44fb55b'
+  url 'http://www.floodgap.com/software/texapp/dist0/0.6.06.txt'
+  sha1 '5dbf1caf40bd70bc1ed121601c384bf9823ee1e4'
 
   def install
     bin.install "#{version}.txt" => "texapp"
