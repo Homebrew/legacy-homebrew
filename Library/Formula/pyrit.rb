48035e0630f546327ba804c988f04f2f6d3e9e33
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-16 04:37:51 -0600

    Instructions to install scapy from tap

--- Library/Formula/pyrit.rb
+++ Library/Formula/pyrit.rb
@@ -1,5 +1,19 @@
 require 'formula'
 
+class ScapyRequirement < Requirement
+  build true
+  fatal true
+
+  satisfy { which 'scapy' }
+
+  def message
+    <<-EOS.undent
+      To use this formula you need to install scapy.
+      brew install samueljohn/python/scapy
+    EOS
+  end
+end
+
 class Pyrit < Formula
   homepage 'https://code.google.com/p/pyrit/'
   url 'https://pyrit.googlecode.com/files/pyrit-0.4.0.tar.gz'
@@ -7,7 +21,7 @@ class Pyrit < Formula
 
   depends_on :python
   depends_on 'libdnet' => 'with-python'
-  depends_on 'scapy' => :python
+  depends_on ScapyRequirement
 
   def install
     python do
