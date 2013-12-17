1a129dbeb763e663693432934c543655d0de1202
Brett Koonce <koonce@gmail.com>
2013-12-17 01:24:56 -0600

    gsoap 2.8.17
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gsoap.rb
+++ Library/Formula/gsoap.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gsoap < Formula
   homepage 'http://www.cs.fsu.edu/~engelen/soap.html'
-  url 'http://downloads.sourceforge.net/project/gsoap2/gSOAP/gsoap_2.8.16.zip'
-  sha1 '8024f03dedef361a5271a7c0b26f961fd278703c'
+  url 'http://downloads.sourceforge.net/project/gsoap2/gSOAP/gsoap_2.8.17.zip'
+  sha1 'acf815bfd8cae1098edd3340880a6f817a170dee'
 
   def install
     ENV.deparallelize
