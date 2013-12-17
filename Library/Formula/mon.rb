59f6bc764c11c6b1896a08bf783852bdbb63cde5
Brett Koonce <koonce@gmail.com>
2013-12-17 10:41:31 -0600

    mon 1.2.3
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mon.rb
+++ Library/Formula/mon.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mon < Formula
   homepage 'https://github.com/visionmedia/mon'
-  url 'https://github.com/visionmedia/mon/archive/1.2.2.tar.gz'
-  sha1 '01af043f44fe8654ce2c82d17ae1664a572064f2'
+  url 'https://github.com/visionmedia/mon/archive/1.2.3.tar.gz'
+  sha1 '9c5013332b6ecccb6368b100e6aee377e35b5bb1'
 
   def install
     system "make"
