3c62a1559b2572c7472d9b55b96a513c3f7d4b70
Alexander Bayandin <a.bayandin@gmail.com>
2013-12-19 10:07:16 +0400

    ansible 1.4.2
    Closes #25325.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ansible.rb
+++ Library/Formula/ansible.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ansible < Formula
   homepage 'http://www.ansibleworks.com/'
-  url 'https://github.com/ansible/ansible/archive/v1.4.1.tar.gz'
-  sha1 '7a63fca5b84901c3fa7b98f1c616df4fb5c26a03'
+  url 'https://github.com/ansible/ansible/archive/v1.4.2.tar.gz'
+  sha1 'f1808f53ba89d0acc8aa9bc7d8b42c5d5715e148'
 
   head 'https://github.com/ansible/ansible.git', :branch => :devel
 
