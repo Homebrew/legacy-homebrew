3bf883de6e72fb9193530f48d9f254fd6edd7f78
Brett Koonce <koonce@gmail.com>
2013-12-18 23:12:56 -0600

    libewf 20131210
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libewf.rb
+++ Library/Formula/libewf.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libewf < Formula
   homepage 'http://code.google.com/p/libewf/'
-  url 'https://googledrive.com/host/0B3fBvzttpiiSMTdoaVExWWNsRjg/libewf-20130416.tar.gz'
-  sha1 'b455412299fd15e7a4f1be670d886f99350bdae4'
+  url 'https://googledrive.com/host/0B3fBvzttpiiSMTdoaVExWWNsRjg/libewf-20131210.tar.gz'
+  sha1 '0b133f39c1f69fe0044a41a502704336e2f54c78'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
