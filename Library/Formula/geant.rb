fe4a087a4f8e8c3fc5d4f89d54043dc2015aa72a
Genie Jhang <geniejhang@majimak.com>
2013-12-20 22:35:06 +0900

    - GEANT4.10.00   --with-qt option   --with-g3tog4 option

--- Library/Formula/geant.rb
+++ Library/Formula/geant.rb
@@ -5,6 +5,8 @@ class Geant < Formula
   url 'http://geant4.cern.ch/support/source/geant4.10.00.tar.gz'
   sha1 'cd91d0b50aab119c85a05c7a44b6fac9168af02d'
 
+  option 'with-g3tog4', 'Use G3toG4 Library'
+
   depends_on 'cmake' => :build
   depends_on :x11 # if your formula requires any X11/XQuartz components
   depends_on 'clhep'
@@ -12,26 +14,22 @@ class Geant < Formula
 
   def install
     mkdir 'geant-build' do
-      if build.with? 'qt'
-        system "cmake", "../",
-                        "-DCMAKE_PREFIX_PATH=#{prefix}",
-                        "-DGEANT4_INSTALL_DATA=ON",
-                        "-DGEANT4_USE_OPENGL_X11=ON",
-                        "-DGEANT4_USE_RAYTRACER_X11=ON",
-                        "-DGEANT4_BUILD_EXAMPLE=ON",
-                        "-DGEANT4_USE_QT=ON",
-                        "-DGEANT4_USE_SYSTEM_CLHEP=ON",
-                        *std_cmake_args
-      else
-        system "cmake", "../",
-                        "-DCMAKE_PREFIX_PATH=#{prefix}",
-                        "-DGEANT4_INSTALL_DATA=ON",
-                        "-DGEANT4_USE_OPENGL_X11=ON",
-                        "-DGEANT4_USE_RAYTRACER_X11=ON",
-                        "-DGEANT4_BUILD_EXAMPLE=ON",
-                        "-DGEANT4_USE_SYSTEM_CLHEP=ON",
-                        *std_cmake_args
-      end
+#                 -DCMAKE_PREFIX_PATH=#{prefix}
+      args = %W[
+                 ../
+                 -DGEANT4_INSTALL_DATA=ON
+                 -DGEANT4_USE_OPENGL_X11=ON
+                 -DGEANT4_USE_RAYTRACER_X11=ON
+                 -DGEANT4_BUILD_EXAMPLE=ON
+                 -DGEANT4_USE_SYSTEM_CLHEP=ON
+               ]
+
+      args << '-DGEANT4_USE_QT=ON' if build.with? 'qt' 
+      args << '-DGEANT4_USE_G3TOG4=ON' if build.with? 'g3tog4'
+
+      args.concat(std_cmake_args)
+
+      system "cmake", *args
       system "make install" # if this fails, try separate make/make install steps
     end
   end
