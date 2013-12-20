80e5c47ab219818c49443143573701a3257dda9e
Misty De Meo <mistydemeo@gmail.com>
2013-12-19 22:04:38 -0800

    qpdf 5.1.0
    Drop patches, adjust pcre link path

--- Library/Formula/qpdf.rb
+++ Library/Formula/qpdf.rb
@@ -2,20 +2,14 @@ require 'formula'
 
 class Qpdf < Formula
   homepage 'http://qpdf.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/qpdf/qpdf/5.0.1/qpdf-5.0.1.tar.gz'
-  sha1 '41a4bd91bfbc2d3585ea229b53bfd1183186b1b3'
+  url 'http://downloads.sourceforge.net/project/qpdf/qpdf/5.1.0/qpdf-5.1.0.tar.gz'
+  sha1 'a2aafad5c49efb62e98e6895bb96ca423179bf43'
 
   depends_on 'pcre'
 
-  # Fix building using Clang and libc++
-  # https://github.com/qpdf/qpdf/issues/19
-  def patches
-    'https://github.com/qpdf/qpdf/pull/21.diff'
-  end
-
   def install
     # find Homebrew's libpcre
-    ENV.append 'LDFLAGS', "-L#{HOMEBREW_PREFIX}/lib"
+    ENV.append 'LDFLAGS', "-L#{Formula.factory('pcre').opt_prefix}/lib"
 
     system "./configure", "--prefix=#{prefix}"
     system "make"
