e1906b3a383318a223f4e0c3efe55dbcc2febbaf
Corey Quinn <corey@sequestered.net>
2013-12-21 03:08:15 -0800

    zsh 5.0.3 has been pulled from the mirrors, reverting to 5.0.2

--- Library/Formula/zsh.rb
+++ Library/Formula/zsh.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Zsh < Formula
   homepage 'http://www.zsh.org/'
-  url 'http://fossies.org/linux/misc/zsh-5.0.3.tar.bz2'
-  mirror 'http://www.zsh.org/pub/zsh-5.0.3.tar.bz2'
-  sha1 'b7a80d2cdb83a871d869cfdf487bda8a18e01ef7'
+  url 'http://downloads.sourceforge.net/project/zsh/zsh/5.0.2/zsh-5.0.2.tar.bz2'
+  sha1 '9f55ecaaae7cdc1495f91237ba2ec087777a4ad9'
 
   depends_on 'gdbm'
   depends_on 'pcre'
