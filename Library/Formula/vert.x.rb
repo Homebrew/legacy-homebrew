6840589f40d3a5e4985c1200420aaf88953c824b
Taro FURUKAWA <tarowin12@mteq.jp>
2013-12-21 11:49:17 +0900

    vert.x 2.1M2
    Closes #25370.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/vert.x.rb
+++ Library/Formula/vert.x.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class VertX < Formula
   homepage 'http://vertx.io/'
-  url 'http://dl.bintray.com/vertx/downloads/vert.x-2.0.2-final.tar.gz'
-  sha1 'fc8cce1d3fca9eeac90e15e6338f83c01ca1d44a'
+  url 'http://dl.bintray.com/vertx/downloads/vert.x-2.1M2.tar.gz'
+  sha1 'eedfe222212287749b01a3dd53cb8224d896ee1a'
+  version '2.1M2'
 
   def install
     rm_f Dir["bin/*.bat"]
