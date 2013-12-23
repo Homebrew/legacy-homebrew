01d2c730ec5417cc201f45bdd22569e7269fa7d9
Gregor Jasny <gregor.jasny@citrix.com>
2013-12-23 10:19:01 +0100

    net-snmp: add devel version (works on 10.9)
    The current stable version does not build on Mavericks
    (see #25097).
    
    Closes #25408.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/net-snmp.rb
+++ Library/Formula/net-snmp.rb
@@ -5,6 +5,12 @@ class NetSnmp < Formula
   url 'http://downloads.sourceforge.net/project/net-snmp/net-snmp/5.7.2/net-snmp-5.7.2.tar.gz'
   sha1 'c493027907f32400648244d81117a126aecd27ee'
 
+  devel do
+    url 'http://downloads.sourceforge.net/project/net-snmp/net-snmp/5.7.3-pre-releases/net-snmp-5.7.3.pre1.tar.gz'
+    version '5.7.3.pre1'
+    sha1 '3420d54a8e78e460c8900bd752b91687fcba7b80'
+  end
+
   def install
     system "./configure", "--disable-debugging",
                           "--prefix=#{prefix}",
