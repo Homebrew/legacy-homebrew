fa18d21a8a80cc2481ed038a2ba448671c6fdc12
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2013-12-24 15:38:12 +0200

    osm2pgrouting 2.0.0: Use inreplace instead of patch
    Editing the CMakeLists.txt file is now performed with
    inreplace instead of applying a patch.

--- Library/Formula/osm2pgrouting.rb
+++ Library/Formula/osm2pgrouting.rb
@@ -10,42 +10,18 @@ class Osm2pgrouting < Formula
   depends_on 'boost'
   depends_on :postgresql
 
-  def patches
+  def install
     # Fixes the default hard-coded /usr/share which the program would be installed in.
     # Instead we supply relative paths, and run cmake with flag -DCMAKE_INSTALL_PREFIX=#{prefix} so that
-    # we get a proper absolute path. This flag is part of the std_cmake_args, so it's not specified explicitly.
-    DATA
-  end
+    # we get a proper path inside prefix.
+    inreplace "CMakeLists.txt" do |s|
+      s.gsub! "/usr/share/osm2pgrouting", "."
+      s.gsub! "/usr/share/bin", "bin"
+    end
 
-  def install
     system "cmake", ".", *std_cmake_args
     system "make"
     system "make", "install"
   end
 
 end
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 6b0402b..50e02c9 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -2,7 +2,7 @@ PROJECT(osm2pgrouting)
- CMAKE_MINIMUM_REQUIRED(VERSION 2.6)
- 
- set (CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake" ${CMAKE_MODULE_PATH})
--set (SHARE_DIR "/usr/share/osm2pgrouting")
-+set (SHARE_DIR ".")
- 
- FIND_PACKAGE(PostgreSQL REQUIRED)
- FIND_PACKAGE(EXPAT REQUIRED)
-@@ -19,7 +19,7 @@ ADD_EXECUTABLE(osm2pgrouting ${SRC})
- TARGET_LINK_LIBRARIES(osm2pgrouting ${PostgreSQL_LIBRARIES} ${EXPAT_LIBRARIES})
- 
- INSTALL(TARGETS osm2pgrouting
--  RUNTIME DESTINATION "/usr/share/bin"
-+  RUNTIME DESTINATION "bin"
- )
- 
- INSTALL(FILES
-
