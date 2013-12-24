30de3d8904f42f181468b3ab4da79548ae25872d
Chris Sandrini <christian.sandrini@me.com>
2013-12-24 19:02:37 +0100

    Remove original comments

--- Library/Formula/therion.rb
+++ Library/Formula/therion.rb
@@ -1,9 +1,5 @@
 require 'formula'
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Therion < Formula
   homepage 'http://therion.speleo.sk'
   url 'http://therion.speleo.sk/downloads/therion-5.3.12.tar.gz'
@@ -33,15 +29,8 @@ class Therion < Formula
         You can find the package at http://mirror.ctan.org/systems/mac/mactex/MacTeX.pkg
     EOS
   end
+
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test therion`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "--version"`.
-    system "false"
+    system "#{bin}/therion", "--version"
   end
 end
