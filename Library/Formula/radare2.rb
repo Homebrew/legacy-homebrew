6e08d19edeb53cb6f6a12126e09c1355e6f02f6f
Christian Barcenas <cbarcen@seas.upenn.edu>
2013-12-24 12:27:35 -0500

    Cleaned up radare2 formula

--- Library/Formula/radare2.rb
+++ Library/Formula/radare2.rb
@@ -5,7 +5,7 @@ class Radare2 < Formula
   url 'http://radare.org/get/radare2-0.9.6.tar.xz'
   sha1 'a12a2de9588d9925d32e1fb2e1942e491c602358'
 
-  head 'https://github.com/radare/radare2.git', :using => :git
+  head 'https://github.com/radare/radare2.git'
 
   depends_on 'libewf'
   depends_on 'libmagic'
