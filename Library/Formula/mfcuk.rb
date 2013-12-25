5f13b6e3be6f46d734fc6da21b20f7205be0d547
soul916 <soul916@hotmail.com>
2013-12-25 13:11:05 +0800

    Add mfcuk 0.3.8 Formula

---
+++ Library/Formula/mfcuk.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Mfcuk < Formula
+  homepage 'http://code.google.com/p/mfcuk/'
+  url 'http://mfcuk.googlecode.com/files/mfcuk-0.3.8.tar.gz'
+  sha1 '2a8259440ac5bed8516c8d771a945b713dacd2bc'
+
+  depends_on 'automake' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'libnfc'
+  depends_on 'libusb'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "mfcuk", "-h"
+  end
+end
