1c3a5560515605da73b66cf2005c0f91dd3db8ff
Shannon Skipper <shannonskipper@gmail.com>
2013-12-25 15:42:32 -0800

    ruby-install 0.3.4
    ### 0.3.4 / 2013-12-25
    
    * Use the `.part` file extension for files being downloaded, to distinguish
      between already downloaded files.
    
    #### ruby
    
    * Added versions for 2.1.0-rc1 and 2.1.0.
    
    #### jruby
    
    * Added version for 1.7.9.
    
    #### rubinius
    
    * Added version for 2.2.2.
    
    Closes #25465.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ruby-install.rb
+++ Library/Formula/ruby-install.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyInstall < Formula
   homepage 'https://github.com/postmodern/ruby-install#readme'
-  url 'https://github.com/postmodern/ruby-install/archive/v0.3.3.tar.gz'
-  sha1 '9a7a5a8890468acf3d507cca5a2bdb9a53383726'
+  url 'https://github.com/postmodern/ruby-install/archive/v0.3.4.tar.gz'
+  sha1 '76b1df95cb056cfe432499a780f6a12ce908b159'
 
   head 'https://github.com/postmodern/ruby-install.git'
 
