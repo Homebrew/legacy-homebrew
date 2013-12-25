46c8c32abb48b1bb3f31d7c2b6b93470357ee275
Genie Jhang <geniejhang@majimak.com>
2013-12-25 13:04:54 +0900

    - required package is added

--- Library/Formula/geant.rb
+++ Library/Formula/geant.rb
@@ -13,6 +13,7 @@ class Geant < Formula
   depends_on :x11
   depends_on 'clhep'
   depends_on 'qt' => :optional
+  depends_on 'xerces-c' => :required if build.with? 'gdml'
 
   def install
     mkdir 'geant-build' do
