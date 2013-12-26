f71f43b73350287fc47f34e39b6a64fb439cb65c
Ruben Fonseca <fonseka@gmail.com>
2013-12-26 08:35:26 +0000

    nettle 2.7.1
    Closes #25470.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/nettle.rb
+++ Library/Formula/nettle.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Nettle < Formula
   homepage 'http://www.lysator.liu.se/~nisse/nettle/'
-  url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.6.tar.gz'
-  sha1 '401f982a0b365e04c8c38c4da42afdd7d2d51d80'
+  url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz'
+  sha1 'e7477df5f66e650c4c4738ec8e01c2efdb5d1211'
 
   depends_on 'gmp'
 
