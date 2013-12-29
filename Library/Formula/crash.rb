451ec0a5e59c79800f4cd8026496fe231cf8978d
Paul Phillips <paulp@improving.org>
2013-12-29 10:31:21 -0800

    crash 1.2.8 and docs
    Closes #25539.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/crash.rb
+++ Library/Formula/crash.rb
@@ -2,12 +2,17 @@ require 'formula'
 
 class Crash < Formula
   homepage 'http://www.crashub.org/'
-  url 'https://crsh.googlecode.com/files/crash-1.2.5.tar.gz'
-  sha1 '8567aa4f16e47aca7041f0e48ebcbc88a038c308'
+  url 'https://crsh.googlecode.com/files/crash-1.2.8.tar.gz'
+  sha1 '12290cd227b20eae07674f8cf0d43ee58732e34b'
+
+  resource 'docs' do
+    url 'https://crsh.googlecode.com/files/crash-1.2.8-docs.tar.gz'
+    sha1 'ea385cf28c9c8fc0512d8809bf511c37a5be8e1a'
+  end
 
   def install
+    doc.install resource('docs')
     libexec.install Dir['crash/*']
-    doc.install Dir['doc/*']
     bin.install_symlink "#{libexec}/bin/crash.sh"
   end
 end
