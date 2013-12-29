cca838dd19057a265d401073c5e1764759e92f44
Paul Phillips <paulp@improving.org>
2013-12-29 10:31:21 -0800

    crash 1.2.8

--- Library/Formula/crash.rb
+++ Library/Formula/crash.rb
@@ -2,12 +2,11 @@ require 'formula'
 
 class Crash < Formula
   homepage 'http://www.crashub.org/'
-  url 'https://crsh.googlecode.com/files/crash-1.2.5.tar.gz'
-  sha1 '8567aa4f16e47aca7041f0e48ebcbc88a038c308'
+  url 'https://crsh.googlecode.com/files/crash-1.2.8.tar.gz'
+  sha1 '12290cd227b20eae07674f8cf0d43ee58732e34b'
 
   def install
     libexec.install Dir['crash/*']
-    doc.install Dir['doc/*']
     bin.install_symlink "#{libexec}/bin/crash.sh"
   end
 end
