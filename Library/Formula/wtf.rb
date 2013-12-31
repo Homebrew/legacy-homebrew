14c89b6e4ee6f5b7f31253be24959c5a3f1b6182
Jinyang Zhou <pandachow19910411@gmail.com>
2013-12-31 04:41:30 -0600

    wtf 20130810
    Closes #25566.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/wtf.rb
+++ Library/Formula/wtf.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wtf < Formula
   homepage 'http://cvsweb.netbsd.org/bsdweb.cgi/src/games/wtf/'
-  url 'http://mirror.aarnet.edu.au/pub/FreeBSD/ports/distfiles/wtf-20111107.tar.gz'
-  sha1 'c5b280cba0c92cf6a80480375bd189a19766e628'
+  url 'http://mirror.aarnet.edu.au/pub/FreeBSD/ports/distfiles/wtf-20130810.tar.gz'
+  sha1 '9ca3e9264941273fc21744f861e583e79899a2ee'
 
   def install
     inreplace %w[wtf wtf.6], "/usr/share", share
