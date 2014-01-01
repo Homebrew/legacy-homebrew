30153a4e6c0dc009b2eed61890776dd7df4f8bd7
Ted Pennings <tedpennings@gmail.com>
2014-01-01 15:50:49 -0500

    youtube-dl 2013.12.26
    Also add a test.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class YoutubeDl < Formula
   homepage 'http://rg3.github.io/youtube-dl/'
-  url 'http://youtube-dl.org/downloads/2013.12.11.2/youtube-dl-2013.12.11.2.tar.gz'
-  sha1 '04a29b9bd69f74fa276546fe997e89bf123aa222'
+  url 'https://yt-dl.org/downloads/2013.12.26/youtube-dl-2013.12.26.tar.gz'
+  sha1 '4cbd9c8ed452dea57dcc5b91469efb2525bd9469'
 
   def install
     system "make", "youtube-dl", "PREFIX=#{prefix}"
@@ -14,5 +14,9 @@ class YoutubeDl < Formula
 
   def caveats
     "To use post-processing options, `brew install ffmpeg`."
+  end
+
+  def test
+    system "#{bin}/youtube-dl", '--simulate', 'http://www.youtube.com/watch?v=he2a4xK8ctk'
   end
 end
