76496e27f390c939bbeabd52f29938c23f6521c5
Sebastian Sdorra <s.sdorra@ostfalia.de>
2014-01-02 08:29:40 +0100

    scm-manager 1.35
    Closes #25594.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/scm-manager.rb
+++ Library/Formula/scm-manager.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class ScmManager < Formula
   homepage 'http://www.scm-manager.org'
-  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/scm-server/1.34/scm-server-1.34-app.tar.gz'
-  version '1.34'
-  sha1 'a24a13fc9ba7c80355f210052e95c4b92eb1f2cf'
+  url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/scm-server/1.35/scm-server-1.35-app.tar.gz'
+  version '1.35'
+  sha1 '9ae7168e3f312dbf75d5ade278dfa0c8176e8dd0'
 
   skip_clean 'libexec/var/log'
 
   resource 'client' do
-    url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/clients/scm-cli-client/1.34/scm-cli-client-1.34-jar-with-dependencies.jar'
-    sha1 '0753518a2d33e51fd7778ae243878e4d3d0a3a90'
+    url 'http://maven.scm-manager.org/nexus/content/repositories/releases/sonia/scm/clients/scm-cli-client/1.35/scm-cli-client-1.35-jar-with-dependencies.jar'
+    sha1 '4695829eb4241ef937fe1b98f016a866c3d3e045'
   end
 
   def install
