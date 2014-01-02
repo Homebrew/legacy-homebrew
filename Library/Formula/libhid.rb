b95d6b2ad21c78eac01bf0be3a8c722cf35cd2f7
Alexander D. Kanevskiy <kad@kad.name>
2014-01-02 19:58:01 +0200

    libhid: fix compilation error on MacOS X 10.9
    ld: library not found for -lIOKit
    
    Closes #25607.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libhid.rb
+++ Library/Formula/libhid.rb
@@ -9,6 +9,9 @@ class Libhid < Formula
   depends_on 'libusb'
   depends_on 'libusb-compat'
 
+  # Fix compilation error on 10.9
+  def patches; DATA; end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -17,3 +20,27 @@ class Libhid < Formula
     system "make install"
   end
 end
+
+__END__
+--- libhid-0.2.16/src/Makefile.am.org	2014-01-02 19:20:33.000000000 +0200
++++ libhid-0.2.16/src/Makefile.am	2014-01-02 19:21:15.000000000 +0200
+@@ -17,7 +17,7 @@ else
+ if OS_DARWIN
+ OS_SUPPORT_SOURCE = darwin.c
+ AM_CFLAGS += -no-cpp-precomp
+-AM_LDFLAGS += -lIOKit -framework "CoreFoundation"
++AM_LDFLAGS += -framework IOKit -framework "CoreFoundation"
+ else
+ OS_SUPPORT =
+ endif
+--- libhid-0.2.16/src/Makefile.in.org	2014-01-02 19:20:35.000000000 +0200
++++ libhid-0.2.16/src/Makefile.in	2014-01-02 19:21:24.000000000 +0200
+@@ -39,7 +39,7 @@ POST_UNINSTALL = :
+ build_triplet = @build@
+ host_triplet = @host@
+ @OS_BSD_FALSE@@OS_DARWIN_TRUE@@OS_LINUX_FALSE@@OS_SOLARIS_FALSE@am__append_1 = -no-cpp-precomp
+-@OS_BSD_FALSE@@OS_DARWIN_TRUE@@OS_LINUX_FALSE@@OS_SOLARIS_FALSE@am__append_2 = -lIOKit -framework "CoreFoundation"
++@OS_BSD_FALSE@@OS_DARWIN_TRUE@@OS_LINUX_FALSE@@OS_SOLARIS_FALSE@am__append_2 = -framework IOKit -framework "CoreFoundation"
+ bin_PROGRAMS = libhid-detach-device$(EXEEXT)
+ subdir = src
+ DIST_COMMON = $(include_HEADERS) $(srcdir)/Makefile.am \
