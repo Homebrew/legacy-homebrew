807d72fa12c27a3b8aa3a8b18a324970430074ba
denji <denji@localhost>
2014-01-03 07:10:53 +0200

    radare2: 0.9.7git and --HEAD

--- Library/Formula/radare2.rb
+++ Library/Formula/radare2.rb
@@ -2,18 +2,34 @@ require 'formula'
 
 class Radare2 < Formula
   homepage 'http://radare.org'
-  url 'http://radare.org/get/radare2-0.9.2.tar.gz'
-  sha1 '7cfa170a59453d57361d730f4c4a1495d36930a8'
+  url 'https://github.com/radare/radare2/archive/4a5165f232.tar.gz'
+  sha1 'd9eece2accc7598906383cadb7334d99c2713134'
+  version '0.9.7git'
+  head 'https://github.com/radare/radare2.git'
 
-  head 'http://radare.org/hg/radare2', :using => :hg
+  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
 
   depends_on 'libewf'
   depends_on 'libmagic'
   depends_on 'gmp'
   depends_on 'lua'
+  depends_on "openssl" if MacOS.version <= :leopard or build.with?('brewed-openssl')
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    args = [
+      "--prefix=#{prefix}",
+      "--with-ostype=darwin",
+    ]
+    if build.with? 'brewed-openssl'
+      args << "--with-openssl=#{Formula.factory('openssl').opt_prefix}"
+    elsif MacOS.version > :leopard
+      # For Xcode-only systems we help a bit to find openssl.
+      # If CLT.installed?, it evaluates to "/usr", which works.
+      args << "--with-openssl=#{MacOS.sdk_path}/usr"
+    end
+
+    system "./configure", *args
+
     system "make"
     system "make install"
   end
