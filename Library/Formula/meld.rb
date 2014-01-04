846de514e1da0e7e812f08e22e1c36f8dcae4486
Mike McQuaid <mike@mikemcquaid.com>
2014-01-04 13:10:52 +0000

    meld: cleanup python usage.

--- Library/Formula/meld.rb
+++ Library/Formula/meld.rb
@@ -14,23 +14,9 @@ class Meld < Formula
   depends_on 'pygobject'
   depends_on 'rarian'
 
-  # TODO: Move this into Library/Homebrew somewhere (see also mitmproxy.rb and ansible.rb).
-  def wrap bin_file, pythonpath
-    bin_file = Pathname.new bin_file
-    libexec_bin = Pathname.new libexec/'bin'
-    libexec_bin.mkpath
-    mv bin_file, libexec_bin
-    bin_file.write <<-EOS.undent
-    #!/bin/sh
-    PYTHONPATH="#{pythonpath}:$PYTHONPATH" "#{libexec_bin}/#{bin_file.basename}" "$@" > /dev/null 2>&1
-    EOS
-  end
-
   def install
     system "make", "prefix=#{prefix}", "install"
 
-    Dir["#{bin}/*"].each do |bin_file|
-      wrap bin_file, python.global_site_packages
-    end
+    bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV['PYTHONPATH'])
   end
 end
