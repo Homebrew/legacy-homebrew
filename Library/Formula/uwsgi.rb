d345c5cb408d98a33aef42daa7f41697eb30624f
Mike McQuaid <mike@mikemcquaid.com>
2014-01-04 13:17:21 +0000

    uwsgi: cleanup python usage.

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -5,16 +5,13 @@ class Uwsgi < Formula
   url 'http://projects.unbit.it/downloads/uwsgi-1.9.18.2.tar.gz'
   sha1 '613328a2da84a7eb3b6f01f892833f2ef8e591ca'
 
-  depends_on :python
   depends_on 'pcre'
   depends_on 'libyaml'
 
   def install
-    python do
-      %w{CFLAGS LDFLAGS}.each { |e| ENV.append e, "-arch #{MacOS.preferred_arch}" }
+    %w{CFLAGS LDFLAGS}.each { |e| ENV.append e, "-arch #{MacOS.preferred_arch}" }
 
-      system python, "uwsgiconfig.py", "--build"
-      bin.install "uwsgi"
-    end
+    system "python", "uwsgiconfig.py", "--build"
+    bin.install "uwsgi"
   end
 end
