849705ab2fb13bf3d3fdfbb333b2d3a027a0caf2
Mike McQuaid <mike@mikemcquaid.com>
2014-01-04 09:14:26 +0000

    Revert "default_compiler: recognize gcc-4.0"
    This reverts commit bedcbb806bb8a991a1317b4ccde8d16b6974cc46.

--- Library/Homebrew/os/mac.rb
+++ Library/Homebrew/os/mac.rb
@@ -104,7 +104,6 @@ module OS
 
     def default_compiler
       case default_cc
-        when /^gcc-4.0/ then :gcc_4_0
         when /^gcc/ then :gcc
         when /^llvm/ then :llvm
         when "clang" then :clang
