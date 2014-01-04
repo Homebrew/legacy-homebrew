cc4b71d97313efa502749c399b46a5bdd7fcba4f
Mike McQuaid <mike@mikemcquaid.com>
2014-01-04 13:06:19 +0000

    glade: cleanup python usage.

--- Library/Formula/glade.rb
+++ Library/Formula/glade.rb
@@ -5,7 +5,6 @@ class Glade < Formula
   url 'http://ftp.gnome.org/pub/GNOME/sources/glade3/3.8/glade3-3.8.4.tar.xz'
   sha256 'c7ae0775b96a400cf43be738b2f836663a505b1458255df9ce83a340057e3d08'
 
-  depends_on :python
   depends_on 'pkg-config' => :build
   depends_on 'xz' => :build
   depends_on 'intltool' => :build
