0015f2c964632ca0aefd6a09e4179d8dea9f57e1
Habib Alamin <ha.alamin@gmail.com>
2014-01-04 14:54:57 +0000

    Now uses meta description HTML tag, which is very common

--- Library/Homebrew/cmd/dscr.rb
+++ Library/Homebrew/cmd/dscr.rb
@@ -31,10 +31,10 @@ module Homebrew extend self
   def dscr_formula f
     formula_home_res = Net::HTTP.get_response URI(f.homepage)
     if formula_home_res.is_a? Net::HTTPSuccess
-      pkg_mgr_desc_class = /(<([[:alpha:]]+)[[[:alnum:]]="[[:blank:]]]* class="pkg_mgr_desc".*?>)(.*?)(<\/\2>)/m
-      desc = formula_home_res.body.match(pkg_mgr_desc_class)
+      desc_meta_class = /(<meta name="description" content=")(.*)("[ ][\/]>)/
+      desc = formula_home_res.body.match(desc_meta_class)
       if desc.kind_of? MatchData
-        desc = desc.captures[2]
+        desc = desc.captures[1]
       end
       if desc.kind_of? String
         convert_html_tags!(desc, f.homepage)
