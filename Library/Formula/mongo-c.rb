3915d0f3b33a203ea2176ffe5c91f2c7db83d54a
Stefan <humdedum@icloud.com>
2014-01-04 20:24:32 +0000

    mongo-c 0.90.0
    Closes #25194.
    Closes #25641.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mongo-c.rb
+++ Library/Formula/mongo-c.rb
@@ -2,11 +2,16 @@ require 'formula'
 
 class MongoC < Formula
   homepage 'http://docs.mongodb.org/ecosystem/drivers/c/'
-  url 'https://github.com/mongodb/mongo-c-driver/archive/v0.8.1.zip'
-  sha1 '38ae6a6273bbf11e24f696a65a3ea3901e456126'
+  url 'https://github.com/mongodb/mongo-c-driver/releases/download/0.90.0/libmongoc-0.90.0.tar.gz'
+  sha1 '15e552b04f3e124fb80e1397e80ddb46fbf36a46'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'libbson'
 
   def install
-    system "make"
-    system "make", "install", "DESTDIR=", "PREFIX=#{prefix}"
+    # https://github.com/mongodb/mongo-c-driver/issues/5
+    inreplace 'configure', 'enable_libclang=yes', 'enable_libclang=no'
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
   end
 end
