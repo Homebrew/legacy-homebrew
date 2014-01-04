47abf17a51f520336240478e8a58fc9225cf9cbe
Matt McClure <matthewlmcclure@gmail.com>
2014-01-04 11:40:42 -0500

    git-integration: Remove superfluous quotes

--- Library/Formula/git-integration.rb
+++ Library/Formula/git-integration.rb
@@ -33,11 +33,11 @@ class GitIntegration < Formula
 
   test do
     system "git", "init"
-    system "git", "commit", "--allow-empty", "-m", "'An initial commit'"
+    system "git", "commit", "--allow-empty", "-m", "An initial commit"
     system "git", "checkout", "-b", "branch-a", "master"
-    system "git", "commit", "--allow-empty", "-m", "'A commit on branch-a'"
+    system "git", "commit", "--allow-empty", "-m", "A commit on branch-a"
     system "git", "checkout", "-b", "branch-b", "master"
-    system "git", "commit", "--allow-empty", "-m", "'A commit on branch-b'"
+    system "git", "commit", "--allow-empty", "-m", "A commit on branch-b"
     system "git", "checkout", "master"
     system "git", "integration", "--create", "integration"
     system "git", "integration", "--add", "branch-a"
