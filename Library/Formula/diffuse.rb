f6d0db3baf711cbc03265492f54aa02f71fcd1e5
Mike McQuaid <mike@mikemcquaid.com>
2014-01-04 13:04:15 +0000

    diffuse: cleanup python usage.

--- Library/Formula/diffuse.rb
+++ Library/Formula/diffuse.rb
@@ -5,16 +5,13 @@ class Diffuse < Formula
   url 'http://downloads.sourceforge.net/project/diffuse/diffuse/0.4.7/diffuse-0.4.7.tar.bz2'
   sha1 '9e3b906b579495027b76dc723b5df28e400d3f26'
 
-  depends_on :python
   depends_on 'pygtk'
 
   def install
-    python do
-      system python, "./install.py",
+    system "python", "./install.py",
                      "--sysconfdir=#{etc}",
                      "--examplesdir=#{share}",
                      "--prefix=#{prefix}"
-    end
   end
 
   def test
