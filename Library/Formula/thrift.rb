54ec779016ce45a2d53211f0a711c2751161c6f6
Andreas Fuchs <asf@boinkor.net>
2014-01-07 13:19:37 -0800

    Thrift: Update to 0.9.1 and fix build on OS X 10.9
    * Enable build on C++11 and add patches to fix clang errors.
    * Disable building tests (otherwise the build fails due to missing
      files)
    * Add the missing dependency to pkg-config on the HEAD build.

--- Library/Formula/thrift.rb
+++ Library/Formula/thrift.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Thrift < Formula
   homepage 'http://thrift.apache.org'
-  url 'http://archive.apache.org/dist/thrift/0.9.0/thrift-0.9.0.tar.gz'
-  sha1 'fefcf4d729bf80da419407dfa028740aa95fa2e3'
+  url 'http://archive.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz'
+  sha1 'dc54a54f8dc706ffddcd3e8c6cd5301c931af1cc'
 
   head do
     url 'https://git-wip-us.apache.org/repos/asf/thrift.git', :branch => "master"
@@ -11,6 +11,7 @@ class Thrift < Formula
     depends_on :autoconf
     depends_on :automake
     depends_on :libtool
+    depends_on 'pkg-config' => :build
   end
 
   option "with-haskell", "Install Haskell binding"
@@ -24,13 +25,21 @@ class Thrift < Formula
 
   # Includes are fixed in the upstream. Please remove this patch in the next version > 0.9.0
   def patches
-    DATA
+    [
+      # Fix issue with C++11 and reserved-user-defined-literal
+      "https://gist.github.com/duedal/7156317/raw/a7edf1de9d092ef5b0a4f3fc3c048e1985248d36/thrifty.patch",
+      # Apply THRIFT-2201 fix from master to 0.9.1 branch (required for clang to compile with C++11 support)
+      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=836d95f9f00be73c6936d407977796181d1a506c",
+      # Tutorial includes both boost and std, so shared_ptr is ambigous with C++11 support enabled
+      "https://gist.github.com/duedal/7156317/raw/9fec9ed82d160f027730ec1790852135dd37ef9f/cpp-tutorial.patch"
+    ]
   end
 
   def install
     system "./bootstrap.sh" if build.head?
 
     exclusions = ["--without-ruby"]
+    exclusions << ["--without-tests"]
 
     exclusions << "--without-python" unless build.with? "python"
     exclusions << "--without-haskell" unless build.include? "with-haskell"
@@ -38,6 +47,8 @@ class Thrift < Formula
     exclusions << "--without-perl" unless build.include? "with-perl"
     exclusions << "--without-php" unless build.include? "with-php"
     exclusions << "--without-erlang" unless build.include? "with-erlang"
+
+    ENV.cxx11
 
     ENV["PY_PREFIX"] = prefix  # So python bindins don't install to /usr!
 
@@ -56,24 +67,10 @@ class Thrift < Formula
       gem install thrift
 
     To install PHP bindings:
-      export PHP_PREFIX=/path/to/homebrew/thrift/0.9.0/php
-      export PHP_CONFIG_PREFIX=/path/to/homebrew/thrift/0.9.0/php_extensions
+      export PHP_PREFIX=/path/to/homebrew/thrift/0.9.1/php
+      export PHP_CONFIG_PREFIX=/path/to/homebrew/thrift/0.9.1/php_extensions
       brew install thrift --with-php
 
     EOS
   end
 end
-__END__
-diff --git a/lib/cpp/src/thrift/transport/TSocket.h b/lib/cpp/src/thrift/transport/TSocket.h
-index ff5e541..65e6aea 100644
---- a/lib/cpp/src/thrift/transport/TSocket.h
-+++ b/lib/cpp/src/thrift/transport/TSocket.h
-@@ -21,6 +21,8 @@
- #define _THRIFT_TRANSPORT_TSOCKET_H_ 1
-
- #include <string>
-+#include <sys/socket.h>
-+#include <arpa/inet.h>
-
- #include "TTransport.h"
- #include "TVirtualTransport.h"
