1f509e70bd2129d395b5da692b39c8a2f1e7a2a5
Mike McQuaid <mike@mikemcquaid.com>
2014-01-07 08:10:31 +0000

    create: always use double quotes.

--- Library/Homebrew/cmd/create.rb
+++ Library/Homebrew/cmd/create.rb
@@ -115,24 +115,24 @@ class FormulaCreator
   end
 
   def template; <<-EOS.undent
-    require 'formula'
+    require "formula"
 
     # Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
     #                #{HOMEBREW_CONTRIB}/example-formula.rb
     # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
     class #{Formula.class_s name} < Formula
-      homepage ''
-      url '#{url}'
+      homepage ""
+      url "#{url}"
     <% unless version.nil? or version.detected_from_url? %>
-      version '#{version}'
+      version "#{version}"
     <% end %>
-      sha1 '#{sha1}'
+      sha1 "#{sha1}"
 
     <% if mode == :cmake %>
-      depends_on 'cmake' => :build
+      depends_on "cmake" => :build
     <% elsif mode.nil? %>
-      # depends_on 'cmake' => :build
+      # depends_on "cmake" => :build
     <% end %>
       depends_on :x11 # if your formula requires any X11/XQuartz components
 
@@ -161,12 +161,12 @@ class FormulaCreator
       test do
         # `test do` will create, run in and delete a temporary directory.
         #
-        # This test will fail and we won't accept that! It's enough to just replace
-        # "false" with the main program this formula installs, but it'd be nice if you
+        # This test will fail and we won"t accept that! It"s enough to just replace
+        # "false" with the main program this formula installs, but it"d be nice if you
         # were more thorough. Run the test with `brew test #{name}`.
         #
         # The installed folder is not in the path, so use the entire path to any
-        # executables being tested: `system "\#{bin}/program", "--version"`.
+        # executables being tested: `system "\#{bin}/program", "do", "something"`.
         system "false"
       end
     end
