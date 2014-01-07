838bfa1b4cd71bf938d3b82981c6138465e78e92
Christian Moritz <chrmoritz@gmail.com>
2014-01-07 01:57:38 +0100

    slimerjs 0.9
    And use xulrunner dependency.
    
    Closes #25276.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/slimerjs.rb
+++ Library/Formula/slimerjs.rb
@@ -2,37 +2,43 @@ require 'formula'
 
 class Slimerjs < Formula
   homepage 'http://www.slimerjs.org'
-  url 'http://download.slimerjs.org/v0.8/0.8.5/slimerjs-0.8.5.zip'
-  sha1 '15ab47922d24806ed826325d7afdf0a18d2b342e'
+  url "http://download.slimerjs.org/v0.9/0.9.0/slimerjs-0.9.0.zip"
+  sha1 "c78cf0961eba2421abfdf5c99d28f6fe8dba7cc8"
 
   head 'https://github.com/laurentj/slimerjs.git'
 
-  devel do
-    url 'https://github.com/laurentj/slimerjs/archive/RELEASE_0.9.1rc1.tar.gz'
-    sha1 '9fb0a3b2e4c44c8d48e9b436127ba2036099b93f'
-    version '0.9.1rc1'
+  if MacOS.version > :snow_leopard
+    option "without-xulrunner", "Build without xulrunner (requires a installed Firefox)"
+    depends_on "xulrunner" => :recommended
   end
 
   def install
     unless build.stable?
-      cd 'src/'
-      system 'zip -r omni.ja chrome/ components/ modules/ defaults/ chrome.manifest -x@package_exclude.lst'
-      libexec.install 'slimerjs.py'
+      cd "src"
+      system "zip", "-r", "omni.ja", "chrome/", "components/", "modules/",
+                    "defaults/", "chrome.manifest", "-x@package_exclude.lst"
     end
-    libexec.install %w[application.ini omni.ja slimerjs]
-    bin.install_symlink libexec/'slimerjs'
+    libexec.install ["application.ini", "omni.ja", "slimerjs", "slimerjs.py"]
+    bin.install_symlink libexec/"slimerjs"
   end
 
   def caveats; <<-EOS.undent
-    You should set the SLIMERJSLAUNCHER environment variable to a installation of
-    Mozilla Firefox (or Mozilla XULRunner).
+    You can set the environment variable SLIMERJSLAUNCHER to a installation of
+    Mozilla Firefox (or Mozilla XULRunner) to use this version with SlimerJS instead
+    of the one installed by Homebrew (this is required if build without xulrunner).
     For a standard Mozilla Firefox installation this would be:
 
       export SLIMERJSLAUNCHER=/Applications/Firefox.app/Contents/MacOS/firefox
 
     Note: If you use SlimerJS with an unstable version of Mozilla Firefox/XULRunner
-    (>24.0) you may have to change the [Gecko]MaxVersion in:
+    (>27.*) you may have to change the [Gecko]MaxVersion in:
       #{libexec}/application.ini
     EOS
+  end
+
+  test do
+    system "#{bin}/slimerjs", "-v"
+    curl "-O", "https://raw.github.com/laurentj/slimerjs/ec1e53a/examples/phantomjs/loadspeed.js"
+    system "#{bin}/slimerjs", "loadspeed.js", "www.google.com"
   end
 end
