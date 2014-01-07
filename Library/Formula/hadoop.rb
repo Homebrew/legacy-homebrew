7a57a310fe770814ac2dac0c941ddcc0661b6c42
Theodore Wong <twong@illumina.com>
2014-01-07 14:23:48 -0800

    Change the 1.0.3 archive URL to point to the Apache archive server

--- Library/Formula/hadoop.rb
+++ Library/Formula/hadoop.rb
@@ -1,17 +1,26 @@
 require 'formula'
 
 class Hadoop < Formula
-  homepage 'http://hadoop.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=hadoop/core/hadoop-1.2.1/hadoop-1.2.1.tar.gz'
-  sha1 'b07b88ca658dc9d338aa84f5c68c809eb7c70964'
+  homepage 'http://hadoop.apache.org/common/'
+  url 'http://archive.apache.org/dist/hadoop/core/hadoop-1.0.3/hadoop-1.0.3.tar.gz'
+  sha1 '5ca6b77e0a600475fae6770c52b47a751f646f9c'
+
+  def shim_script target
+    <<-EOS.undent
+    #!/bin/bash
+    exec "#{libexec}/bin/#{target}" "$@"
+    EOS
+  end
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin conf lib webapps contrib]
     libexec.install Dir['*.jar']
-    bin.write_exec_script Dir["#{libexec}/bin/*"]
-    # But don't make rcc visible, it conflicts with Qt
-    (bin/'rcc').unlink
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |b|
+      n = Pathname.new(b).basename
+      (bin+n).write shim_script(n)
+    end
 
     inreplace "#{libexec}/conf/hadoop-env.sh",
       "# export JAVA_HOME=/usr/lib/j2sdk1.5-sun",
