24cfe61785fa1236fbfe60fc8b05db83c5b15afe
Julien Feltesse <julien.feltesse@gmail.com>
2014-01-07 18:12:17 +0900

    hicolor-icon-theme 0.13
    Closes #25704.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hicolor-icon-theme.rb
+++ Library/Formula/hicolor-icon-theme.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class HicolorIconTheme < Formula
   homepage 'http://icon-theme.freedesktop.org/wiki/HicolorTheme'
-  url 'http://icon-theme.freedesktop.org/releases/hicolor-icon-theme-0.12.tar.gz'
-  sha1 '87368844d1fcef899c3dc4e59f07264340606538'
+  url 'http://icon-theme.freedesktop.org/releases/hicolor-icon-theme-0.13.tar.gz'
+  sha1 '15e30dfcf5e7b53c1a6f9028c30665006abba55c'
 
   def install
     system "./configure", "--prefix=#{prefix}"
