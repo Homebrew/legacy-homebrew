d80fb2184b0dee63567b3f8e4b108815545df3e5
Michael Hood <michaelhood@gmail.com>
2014-01-09 00:46:49 -0800

    tig: add head option
    Closes #25755.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Tig < Formula
   homepage 'http://jonas.nitro.dk/tig/'
+  head 'https://github.com/jonas/tig.git'
   url 'http://jonas.nitro.dk/tig/releases/tig-1.2.1.tar.gz'
   sha1 '5755bae7342debf94ef33973e0eaff6207e623dc'
 
@@ -13,6 +14,7 @@ class Tig < Formula
   end
 
   def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
     system "make install"
     system "make install-doc-man" if build.with? "docs"
