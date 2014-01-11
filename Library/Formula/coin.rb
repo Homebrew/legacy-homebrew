8f1497965ddc850acfd5fe7416deab6014047de9
Johnathan Conley <johnathan.conley@gmail.com>
2014-01-10 23:54:18 -0600

    coin: fix clang compilation.
    Closes #25809.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/coin.rb
+++ Library/Formula/coin.rb
@@ -6,10 +6,16 @@ class Coin < Formula
   sha1 '8e9f05628461963623686d3ec53102214e233dd1'
 
   def install
+    # https://bitbucket.org/Coin3D/coin/issue/47 (fix misspelled test flag)
+    inreplace "configure", '-fno-for-scoping', '-fno-for-scope'
+
+    # https://bitbucket.org/Coin3D/coin/issue/45 (suppress math-undefs)
+    # http://ftp.netbsd.org/pub/pkgsrc/current/pkgsrc/graphics/Coin/patches/patch-include_Inventor_C_base_math-undefs.h
+    inreplace "include/Inventor/C/base/math-undefs.h", "#ifndef COIN_MATH_UNDEFS_H", "#if false"
+
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-framework-prefix=#{lib}"
     system "make install"
   end
-
 end
