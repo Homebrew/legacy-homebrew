c6958dd1e59b790895bc68ca2a6b822e4ad814a8
Andreas Fuchs <asf@boinkor.net>
2014-01-11 12:28:49 -0800

    Exclude 0.9.1-specific patches from devel or HEAD builds

--- Library/Formula/thrift.rb
+++ Library/Formula/thrift.rb
@@ -24,20 +24,24 @@ class Thrift < Formula
   depends_on :python => :optional
 
   def patches
-    [
-      # Apply THRIFT-2201 fix from master to 0.9.1 branch (required for clang to compile with C++11 support)
-      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=836d95f9f00be73c6936d407977796181d1a506c",
-      # Apply THRIFT-667
-      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=12c09f44cb291b1ecc4074cb3a55775b375fa8b2",
-      # Apply THRIFT-1755
-      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=8cd3efe50a42975375e8ff3bc03306d9e4174314",
-      # Apply THRIFT-2045
-      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=282e440c6de219b7b8f32b01cc7eb599f534f33f",
-      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=9f9cd10e813ef574dd5578d78ca26a9088383d3a",
-      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=e957675647d3d7caafe842aa85cbd987e91b21f9",
-      # Apply THRIFT-2229 fix from master to 0.9.1 branch
-      "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=5f2d34e5ab33651059a085525b3adbab6a877e6f"
-    ]
+    # These patches are 0.9.1-specific and can go away once a newer
+    # version is released:
+    unless build.head? or build.devel?
+      [
+        # Apply THRIFT-2201 fix from master to 0.9.1 branch (required for clang to compile with C++11 support)
+        "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=836d95f9f00be73c6936d407977796181d1a506c",
+        # Apply THRIFT-667
+        "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=12c09f44cb291b1ecc4074cb3a55775b375fa8b2",
+        # Apply THRIFT-1755
+        "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=8cd3efe50a42975375e8ff3bc03306d9e4174314",
+        # Apply THRIFT-2045
+        "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=282e440c6de219b7b8f32b01cc7eb599f534f33f",
+        "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=9f9cd10e813ef574dd5578d78ca26a9088383d3a",
+        "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=e957675647d3d7caafe842aa85cbd987e91b21f9",
+        # Apply THRIFT-2229 fix from master to 0.9.1 branch
+        "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=5f2d34e5ab33651059a085525b3adbab6a877e6f"
+      ]
+    end
   end
 
   def install
