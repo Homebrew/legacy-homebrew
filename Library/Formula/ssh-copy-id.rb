2fd47330d0977cd427a96fa2bcd3a1f0d214618c
Ted Pennings <tedpennings@gmail.com>
2014-01-11 14:02:08 -0500

    Add a mirror

--- Library/Formula/ssh-copy-id.rb
+++ Library/Formula/ssh-copy-id.rb
@@ -3,6 +3,7 @@ require 'formula'
 class SshCopyId < Formula
   homepage 'http://www.openssh.com/'
   url 'http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.4p1.tar.gz'
+  mirror 'http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.4p1.tar.gz'
   version '6.4p1'
   sha1 'cf5fe0eb118d7e4f9296fbc5d6884965885fc55d'
 
