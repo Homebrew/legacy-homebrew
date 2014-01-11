860b73538d85c3d787d6aaeda26f3e6a49ad1fb9
Alex Schroeder <alex@gnu.org>
2014-01-12 00:22:58 +0100

    mkhexgrid: depends on boost

--- Library/Formula/mkhexgrid.rb
+++ Library/Formula/mkhexgrid.rb
@@ -5,6 +5,7 @@ class Mkhexgrid < Formula
   url 'http://www.nomic.net/~uckelman/mkhexgrid/releases/mkhexgrid-0.1.1.src.tar.bz2'
   sha1 '3b8e0be3636f90a3217a5e3556bdaf7bb0c49116'
 
+  depends_on 'boost'
   depends_on 'gd' => %w{with-freetype}
 
   def install
