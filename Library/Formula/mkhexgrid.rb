a6f66cbfef17747b810d9d77018c3f4d6334a76a
Alex Schroeder <alex@gnu.org>
2014-01-11 23:30:21 +0100

    mkhexgrid: create hexagonal grids used for strategy games
    The Makefile has the compiler and prefix hardcoded and needs to be
    changed before building. During installation, it also tries to change
    owner and group to 0. In Mac OS 10.9, there's also a namescape
    conflict for exceptions. The Formula handles these issues for you.
    
    The Formula depends on GD with FreeType enabled. This can be a problem
    if you have GD without FreeType installed.

---
+++ Library/Formula/mkhexgrid.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Mkhexgrid < Formula
+  homepage 'http://www.nomic.net/~uckelman/mkhexgrid/'
+  url 'http://www.nomic.net/~uckelman/mkhexgrid/releases/mkhexgrid-0.1.1.src.tar.bz2'
+  sha1 '3b8e0be3636f90a3217a5e3556bdaf7bb0c49116'
+
+  depends_on 'gd' => %w{with-freetype}
+
+  def install
+    inreplace 'Makefile' do |s|
+      s.change_make_var! "DESTDIR", prefix
+      s.change_make_var! "CC", ENV.cc
+      s.gsub! /install (.*) -o 0 -g 0/, 'install \1'
+    end
+    inreplace 'mkhexgrid.cpp' do |s|
+      s.sub! 'catch (exception &e)', 'catch (std::exception &e)'
+    end
+    system "make" # needs to be separate
+    system "make", "install"
+  end
+
+  test do
+    # test the example from the man page (but without inches)
+    system "mkhexgrid", "--output=ps", "--image-width=2448",
+    "--image-height=1584", "--hex-side=36", "--coord-bearing=0",
+    "--coord-dist=22", "--coord-size=8", "--grid-thickness=1",
+    "--coord-font=Helvetica", "--grid-grain=h", "--grid-start=o",
+    "--coord-tilt=-90", "--centered", "-o", "test.ps"
+  end
+end
