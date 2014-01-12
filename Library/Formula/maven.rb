a8d465b32b85bba5cf2fe73350428c039fd56b6d
Ted Pennings <tedpennings@gmail.com>
2014-01-12 11:44:46 -0500

    maven: add a test.
    Closes #25845.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/maven.rb
+++ Library/Formula/maven.rb
@@ -19,4 +19,27 @@ class Maven < Formula
     # file will be found relative to it
     bin.install_symlink Dir["#{libexec}/bin/*"] - ["#{libexec}/bin/m2.conf"]
   end
+
+  test do
+    (testpath/'pom.xml').write <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+        <modelVersion>4.0.0</modelVersion>
+        <groupId>org.homebrew</groupId>
+        <artifactId>maven-test</artifactId>
+        <version>1.0.0-SNAPSHOT</version>
+      </project>
+    EOS
+    (testpath/'src/main/java/org/homebrew/MavenTest.java').write <<-EOS.undent
+      package org.homebrew;
+      public class MavenTest {
+        public static void main(String[] args) {
+          System.out.println("Testing Maven with Homebrew!");
+        }
+      }
+    EOS
+    system "#{bin}/mvn", 'compile'
+  end
+
 end
