7b851cd4435945d0df66f2235e19f1b72da7c34c
jm2dev <jomarcar@gmail.com>
2014-01-12 12:10:27 +0000

    Replacing tar.xz file for zip one.

--- Library/Formula/pass.rb
+++ Library/Formula/pass.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Pass < Formula
   homepage 'http://zx2c4.com/projects/password-store'
-  url 'http://git.zx2c4.com/password-store/snapshot/password-store-1.4.2.tar.xz'
-  sha256 'ac5261b176e2be60b44c5c785dc5770a3e0f459bc6dbc42136679d68d3986e04'
+  url 'http://git.zx2c4.com/password-store/snapshot/password-store-1.4.2.zip'
+  sha256 '7e7ff669117951330436534025e39a07ee358a8932a6908870457f6b7f8702c2'
   head 'http://git.zx2c4.com/password-store', :using => :git
 
-  depends_on 'xz' => :build
+  depends_on 'unzip' => :build
   depends_on 'pwgen'
   depends_on 'tree'
   depends_on 'gnu-getopt'
