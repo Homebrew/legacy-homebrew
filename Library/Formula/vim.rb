a6f56aba8c92e7ffcc1c58445dfbdeb4e7d9447f
左岸 <itaofe@gmail.com>
2014-01-12 15:35:57 +0800

    Update vim.rb

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -3,10 +3,7 @@ require 'formula'
 class Vim < Formula
   homepage 'http://www.vim.org/'
   head 'https://vim.googlecode.com/hg/'
-  # This package tracks debian-unstable: http://packages.debian.org/unstable/vim
-  # url 'http://ftp.debian.org/debian/pool/main/v/vim/vim_7.4.052.orig.tar.gz'
-  # sha1 '216ab69faf7e73e4b86da7f00e4ad3b3cca1fdb8'
-  
+
   url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2'
   sha1 '601abf7cc2b5ab186f40d8790e542f86afca86b7
 
