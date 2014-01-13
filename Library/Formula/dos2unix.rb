6064ce26610d62bf2f3ce58a9182c8c7af238faf
asaph <ace@dontletgo.com>
2014-01-12 20:11:44 -0800

    dos2unix 6.0.4
    Closes #25859.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Dos2unix < Formula
   homepage 'http://waterlan.home.xs4all.nl/dos2unix.html'
-  url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.3.tar.gz'
-  sha1 '2c9ae9177fcb0d8b63d141f73a75c928c38ace01'
+  url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.4.tar.gz'
+  sha1 '93d73148c09908a42dcbf5339312c9aa1f18ba7c'
 
   depends_on 'gettext'
 
