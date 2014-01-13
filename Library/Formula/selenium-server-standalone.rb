444877df173fd772f18ba1850524572767978a65
Mike McQuaid <mike@mikemcquaid.com>
2014-01-13 09:39:04 +0000

    selenium-server-standalone: fix bad plist_options.

--- Library/Formula/selenium-server-standalone.rb
+++ Library/Formula/selenium-server-standalone.rb
@@ -10,7 +10,7 @@ class SeleniumServerStandalone < Formula
     bin.write_jar_script libexec/"selenium-server-standalone-#{version}.jar", "selenium-server"
   end
 
-  plist_options :manual => "#{bin}/selenium-server -p 4444"
+  plist_options :manual => "selenium-server -p 4444"
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
