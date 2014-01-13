f3d1b8dcaa06c85b71702191cc89e504b31d44e8
Alex Regueiro <alex@noldorin.com>
2014-01-13 03:51:55 +0000

    uriparser 0.8.0
    Closes #25858.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/uriparser.rb
+++ Library/Formula/uriparser.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Uriparser < Formula
   homepage 'http://uriparser.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/uriparser/Sources/0.7.9/uriparser-0.7.9.tar.bz2'
-  sha1 'c02d1db3c4e132863b6fc4e4f5e08cec535089d1'
+  url 'http://downloads.sourceforge.net/project/uriparser/Sources/0.8.0/uriparser-0.8.0.tar.bz2'
+  sha1 '4bfe347220b00ff9cd3252e2b784d13e583282fb'
 
   depends_on 'pkg-config' => :build
   depends_on 'cpptest'
