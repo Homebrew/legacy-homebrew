00de0081deb7246085efedbcce3c7dc7ac59adaf
David E. Wheeler <david@justatheory.com>
2014-01-14 12:56:15 -0800

    Also install pg_prove.

--- Library/Formula/pgtap.rb
+++ Library/Formula/pgtap.rb
@@ -8,11 +8,28 @@ class Pgtap < Formula
   head       'https://github.com/theory/pgtap.git'
 
   depends_on :postgresql
+  depends_on :cpanminus
 
   skip_clean 'share'
 
   def install
+    pg_prove
     ENV.prepend_path 'PATH', Formula.factory('postgresql').bin
     system "make install"
   end
+
+  def pg_prove
+    arch  = %x(perl -MConfig -E 'print $Config{archname}')
+    plib  = "#{HOMEBREW_PREFIX}/lib/perl5"
+    ENV['PERL5LIB'] = "#{plib}:#{plib}/#{arch}:#{lib}:#{lib}/#{arch}"
+    ENV.remove_from_cflags(/-march=\w+/)
+    ENV.remove_from_cflags(/-msse\d?/)
+
+    # Install TAP::Parser::SourceHandler::pgTAP.
+    system "cpanm --local-lib '#{prefix}' --notest TAP::Parser::SourceHandler::pgTAP"
+
+    # Remove perllocal.pod, since it just gets in the way of other modules.
+    rm "#{prefix}/lib/perl5/#{arch}/perllocal.pod", :force => true
+  end
+
 end
