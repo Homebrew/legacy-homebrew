24c14b98d951284e9ff9d2bc19f2528b7b64c0ad
Eugeny Klementev <eugeny.klementev@gmail.com>
2014-01-14 11:43:39 +0400

    openrtsp 2014_01_13
    Closes #25892.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/openrtsp.rb
+++ Library/Formula/openrtsp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Openrtsp < Formula
   homepage 'http://www.live555.com/openRTSP'
-  url 'http://www.live555.com/liveMedia/public/live.2014.01.11.tar.gz'
-  sha1 '51acfe56d1dab32b5a1040c9c30f7962d89a7bc6'
+  url 'http://www.live555.com/liveMedia/public/live.2014.01.13.tar.gz'
+  sha1 'e617930d23ef55381613121c2ea921ce59b9ee0e'
 
   option "32-bit"
 
