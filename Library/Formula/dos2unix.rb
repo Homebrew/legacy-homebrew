432a7eeccbf0c71be05b5fe5cba001eab104f705
asaph <ace@dontletgo.com>
2014-01-13 20:57:43 -0800

    dos2unix --devel 6.0.5-beta1
    Closes #25889.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/dos2unix.rb
+++ Library/Formula/dos2unix.rb
@@ -7,6 +7,11 @@ class Dos2unix < Formula
 
   depends_on 'gettext'
 
+  devel do
+    url 'http://waterlan.home.xs4all.nl/dos2unix/dos2unix-6.0.5-beta1.tar.gz'
+    sha1 'bc00e0eb920c1aa02f7c99ccb355837ecce31b03'
+  end
+
   def install
     gettext = Formula.factory("gettext")
     system "make", "prefix=#{prefix}",
