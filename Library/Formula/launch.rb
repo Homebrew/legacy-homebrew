b9bdd8066e8ccd0e5418d4f18e1f9ccdeded467f
Jason P. Scharf <jason.scharf@intergrated.net>
2014-01-14 13:58:44 -0500

    launch 1.2

--- Library/Formula/launch.rb
+++ Library/Formula/launch.rb
@@ -2,20 +2,22 @@ require 'formula'
 
 class Launch < Formula
   homepage 'http://web.sabi.net/nriley/software/'
-  url 'http://web.sabi.net/nriley/software/launch-1.1.tar.gz'
-  sha1 '000d6f1063f1b9091a8b10de90cfa778ed6a6ed1'
+  url 'http://sabi.net/nriley/software/launch-1.2.tar.gz'
+  sha1 '173b75081caf451f3d3a49630bbe5c5b7e7ab17d'
 
-  head 'http://dev.sabi.net/svn/dev/trunk/launch/launch/', :using => :svn
-
-  # Fix compilation on 10.8 and newer, per MacPorts
-  def patches
-    {:p0 => "https://trac.macports.org/export/114086/trunk/dports/aqua/launch/files/patch-main.c.diff"}
-  end
+  head 'https://github.com/nriley/launch.git'
 
   def install
     rm_rf "launch" # We'll build it ourself, thanks.
-    system "#{ENV.cc} -o launch -std=c99 #{ENV.cflags} main.c -framework ApplicationServices"
+    system  "xcodebuild",
+            "-configuration",
+            "Deployment",
+            " SYMROOT=build clean"
+    system  "xcodebuild",
+            "-configuration",
+            "Deployment",
+            "SYMROOT=build"
     man1.install gzip('launch.1')
-    bin.install 'launch'
+    bin.install 'build/Deployment/launch'
   end
 end
