1cb1a098c6fef790c365036df6ed9ee713f73e09
asaph <ace@dontletgo.com>
2014-01-15 00:20:31 -0800

    crash --devel 1.3.0-beta13
    Closes #25915.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/crash.rb
+++ Library/Formula/crash.rb
@@ -5,6 +5,16 @@ class Crash < Formula
   url 'https://crsh.googlecode.com/files/crash-1.2.8.tar.gz'
   sha1 '12290cd227b20eae07674f8cf0d43ee58732e34b'
 
+  devel do
+    url 'https://crsh.googlecode.com/files/crash-1.3.0-beta13.tar.gz'
+    sha1 '4012ff4cc751a261ebfadf01f3b9441b275844a8'
+
+    resource 'docs' do
+      url 'https://crsh.googlecode.com/files/crash-1.3.0-beta13-docs.tar.gz'
+      sha1 '5b0e6da691662a5344ad0029de457297fdcc1df8'
+    end
+  end
+
   resource 'docs' do
     url 'https://crsh.googlecode.com/files/crash-1.2.8-docs.tar.gz'
     sha1 'ea385cf28c9c8fc0512d8809bf511c37a5be8e1a'
