abad809063166c67fac03b3ef89e001c01bcf894
Xavier Lacoste <xavier.lacoste@inria.fr>
2014-01-15 12:02:24 +0100

    moving to 1.7.4rc1

--- Library/Formula/open-mpi.rb
+++ Library/Formula/open-mpi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class OpenMpi < Formula
   homepage 'http://www.open-mpi.org/'
-  url 'http://www.open-mpi.org/software/ompi/v1.7/downloads/openmpi-1.7.3.tar.bz2'
-  sha1 'ce61ee466ac2b21024c7d8dabc4f18676b2f1b76'
+  url 'http://www.open-mpi.org/software/ompi/v1.7/downloads/openmpi-1.7.4rc1.tar.bz2'
+  sha1 'af52f674392553dd57019fdcd717f86e95d03787'
 
   option 'disable-fortran', 'Do not build the Fortran bindings'
   option 'enable-mpi-thread-multiple', 'Enable MPI_THREAD_MULTIPLE'
@@ -13,12 +13,6 @@ class OpenMpi < Formula
   conflicts_with 'lcdf-typetools', :because => 'both install same set of binaries.'
 
   depends_on :fortran unless build.include? 'disable-fortran'
-
-  def patches
-    # Do not install the libevent header files.
-    # See http://www.open-mpi.org/community/lists/users/2013/11/22900.php
-    DATA
-  end
 
   def install
     ENV.cxx11 if build.cxx11?
@@ -52,24 +46,3 @@ class OpenMpi < Formula
   end
 end
 
-__END__
-
-diff --git a/opal/mca/event/libevent2021/libevent/include/Makefile.in b/opal/mca/event/libevent2021/libevent/include/Makefile.in
-index 99fb60b..5f7cad0 100644
---- a/opal/mca/event/libevent2021/libevent/include/Makefile.in
-+++ b/opal/mca/event/libevent2021/libevent/include/Makefile.in
-@@ -281,10 +281,10 @@ EVENT2_EXPORT = \
- 	event2/util.h
- 
- EXTRA_SRC = $(EVENT2_EXPORT)
--@INSTALL_LIBEVENT_TRUE@nobase_include_HEADERS = $(EVENT2_EXPORT)
--@INSTALL_LIBEVENT_TRUE@nobase_nodist_include_HEADERS = ./event2/event-config.h
--@INSTALL_LIBEVENT_FALSE@noinst_HEADERS = $(EVENT2_EXPORT)
--@INSTALL_LIBEVENT_FALSE@nodist_noinst_HEADERS = ./event2/event-config.h
-+#@INSTALL_LIBEVENT_TRUE@nobase_include_HEADERS = $(EVENT2_EXPORT)
-+#@INSTALL_LIBEVENT_TRUE@nobase_nodist_include_HEADERS = ./event2/event-config.h
-+#@INSTALL_LIBEVENT_FALSE@noinst_HEADERS = $(EVENT2_EXPORT)
-+#@INSTALL_LIBEVENT_FALSE@nodist_noinst_HEADERS = ./event2/event-config.h
- all: all-am
- 
- .SUFFIXES:
