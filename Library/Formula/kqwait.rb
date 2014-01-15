1c89f04c199715df75badea0b236baabd517a5e5
Alex Regueiro <alex@noldorin.com>
2014-01-15 15:29:03 +0000

    kqwait: removed leading 'v' from version string

--- Library/Formula/kqwait.rb
+++ Library/Formula/kqwait.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Kqwait < Formula
   homepage 'https://github.com/sschober/kqwait'
   url 'https://github.com/sschober/kqwait.git', :revision => '7dbafc50c9b16eb4b63774411629b4f016fed871'
-  version 'v1.0.3'
+  version '1.0.3'
   sha1 '7b4e762de8867593b21b06908a6ee13dbc1c863a'
 
   head 'https://github.com/sschober/kqwait.git'
