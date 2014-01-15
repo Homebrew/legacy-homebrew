f95c4b57e5a993c6524e95a1f926718ce823ea51
Brett Koonce <koonce@gmail.com>
2014-01-15 11:33:26 -0600

    re2 20140111

--- Library/Formula/re2.rb
+++ Library/Formula/re2.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Re2 < Formula
   homepage 'https://code.google.com/p/re2/'
   head 'https://re2.googlecode.com/hg'
-  url 'https://re2.googlecode.com/files/re2-20131024.tgz'
-  sha1 '90a356fb205c5004cc4f08e45e02994b898b592a'
+  url 'https://re2.googlecode.com/files/re2-20140111.tgz'
+  sha1 'd51b3c2e870291070a1bcad8e5b471ae83e1f8df'
 
   def install
     system "make", "install", "prefix=#{prefix}"
