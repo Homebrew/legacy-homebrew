5e27bcdad30cd371e0d6ce44f89c46cfbf28887c
Brett Koonce <koonce@gmail.com>
2014-01-16 00:50:35 -0600

    imlib2 1.4.6
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/imlib2.rb
+++ Library/Formula/imlib2.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Imlib2 < Formula
   homepage 'http://sourceforge.net/projects/enlightenment/files/'
-  url 'http://downloads.sourceforge.net/project/enlightenment/imlib2-src/1.4.5/imlib2-1.4.5.tar.bz2'
-  sha1 'af86a2c38f4bc3806db57e64e74dc9814ad474a0'
+  url 'http://downloads.sourceforge.net/project/enlightenment/imlib2-src/1.4.6/imlib2-1.4.6.tar.bz2'
+  sha1 '20e111d822074593e8d657ecf8aafe504e9e2967'
 
   option "without-x", "Build without X support"
 
