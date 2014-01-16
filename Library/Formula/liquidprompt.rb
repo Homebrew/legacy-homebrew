d6d9127075930fd4f19de3d8e9650382155877fd
Brad Beyenhof <bbeyenhof@icloud.com>
2014-01-16 17:57:21 +0000

    liquidprompt 1.8
    Move liquidprompt script to share & notify in info.
    
    Closes #25925.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/liquidprompt.rb
+++ Library/Formula/liquidprompt.rb
@@ -2,23 +2,23 @@ require 'formula'
 
 class Liquidprompt < Formula
   homepage 'https://github.com/nojhan/liquidprompt'
-  url 'https://github.com/nojhan/liquidprompt/archive/v_1.7.tar.gz'
-  sha1 'db7f24b20f09480b3491c5250f30f7ccd67ee44e'
+  url 'https://github.com/nojhan/liquidprompt/archive/v_1.8.tar.gz'
+  sha1 'ad481e98b0f6ed251cd65cb01464da151929f081'
 
   def install
-    (share+'liquidprompt').install 'liquidpromptrc-dist'
-    bin.install 'liquidprompt'
+    share.install 'liquidpromptrc-dist'
+    share.install 'liquidprompt'
   end
 
   def caveats; <<-EOS.undent
     Add the following lines to your bash or zsh config (e.g. ~/.bash_profile):
-      if [ -f $(brew --prefix)/bin/liquidprompt ]; then
-        . $(brew --prefix)/bin/liquidprompt
+      if [ -f #{HOMEBREW_PREFIX}/share/liquidprompt ]; then
+        . #{HOMEBREW_PREFIX}/share/liquidprompt
       fi
 
     If you'd like to reconfigure options, you may do so in ~/.liquidpromptrc.
     A sample file you may copy and modify has been installed to
-      #{HOMEBREW_PREFIX}/share/liquidprompt/liquidpromptrc-dist
+      #{HOMEBREW_PREFIX}/share/liquidpromptrc-dist
 
     Don't modify the PROMPT_COMMAND variable elsewhere in your shell config;
     that will break things.
