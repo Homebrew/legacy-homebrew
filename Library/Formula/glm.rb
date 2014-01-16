93eb661af69ed7660c4ba7164f425746368f3077
Brett Koonce <koonce@gmail.com>
2014-01-16 00:50:06 -0600

    glm 0.9.5.1
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/glm.rb
+++ Library/Formula/glm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Glm < Formula
   homepage 'http://glm.g-truc.net/'
-  url 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.4.6/glm-0.9.4.6.zip'
-  sha1 'a909c34c718e9146575717d429ee55647b692d41'
+  url 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.5.1/glm-0.9.5.1.zip'
+  sha1 'e1165e2c1be99e3cfeacbecaa15a2c89f2caf503'
   head 'https://github.com/Groovounet/glm.git'
 
   def install
