3fbafea3784545f96d0cff72e1f9da82caab7e82
Philipp Fehre <philipp.fehre@googlemail.com>
2014-01-16 11:16:29 +0100

    Allow silencing of completion messages patch
    This has been a very annoying limitation for plugins like YouCompleteMe
    which has been patched
    https://groups.google.com/forum/#!topic/vim_dev/WeBBjkXE8H8
    but not been applied upstream yet
    
    https://github.com/Valloric/YouCompleteMe/issues/642 for more details

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -40,6 +40,12 @@ class Vim < Formula
   #
   # Second patch: includes Mac OS X version macros not included by default on 10.9
   # Reported upstream: https://groups.google.com/forum/#!topic/vim_mac/5kVAMSPb6uU
+  #
+  # Third patch: patch for adding option to disable insert completion message
+  # This allows plugins like You-Complete-Me and alike to silence very annoying
+  # messages about user defined completion patterns
+  # https://github.com/Valloric/YouCompleteMe/issues/642
+  # Reported upstream: https://groups.google.com/forum/#!topic/vim_dev/WeBBjkXE8H8
   def patches; DATA; end unless build.head?
 
   def install
@@ -158,3 +164,82 @@ index 78b79c2..54009ab 100644
  /*
   * Macintosh machine-dependent things.
   *
+ 
+
+diff -r 462a4499f9c6 runtime/doc/options.txt
+--- a/runtime/doc/options.txt	Fri Nov 29 14:24:42 2013 +0900
++++ b/runtime/doc/options.txt	Fri Nov 29 18:07:09 2013 +0900
+@@ -6259,6 +6259,9 @@
+ 	  A	don't give the "ATTENTION" message when an existing swap file
+ 		is found.
+ 	  I	don't give the intro message when starting Vim |:intro|.
++	  c	don't give the |ins-completion-menu| message.  For example,
++		"-- XXX completion (YYY)", "match 1 of 2", "The only match",
++		"Pattern not found", "Back at original", etc.
+ 
+ 	This gives you the opportunity to avoid that a change between buffers
+ 	requires you to hit <Enter>, but still gives as useful a message as
+diff -r 462a4499f9c6 src/edit.c
+--- a/src/edit.c	Fri Nov 29 14:24:42 2013 +0900
++++ b/src/edit.c	Fri Nov 29 18:07:09 2013 +0900
+@@ -3878,7 +3878,8 @@
+ 	    ins_compl_free();
+ 	    compl_started = FALSE;
+ 	    compl_matches = 0;
+-	    msg_clr_cmdline();		/* necessary for "noshowmode" */
++	    if (!shortmess(SHM_COMPLETIONMENU))
++		msg_clr_cmdline();	/* necessary for "noshowmode" */
+ 	    ctrl_x_mode = 0;
+ 	    compl_enter_selects = FALSE;
+ 	    if (edit_submode != NULL)
+@@ -5333,7 +5334,8 @@
+ 	    {
+ 		ctrl_x_mode = 0;
+ 		edit_submode = NULL;
+-		msg_clr_cmdline();
++		if (!shortmess(SHM_COMPLETIONMENU))
++		    msg_clr_cmdline();
+ 		return FAIL;
+ 	    }
+ 
+@@ -5594,12 +5596,12 @@
+     showmode();
+     if (edit_submode_extra != NULL)
+     {
+-	if (!p_smd)
++	if (!p_smd && !shortmess(SHM_COMPLETIONMENU))
+ 	    msg_attr(edit_submode_extra,
+ 		    edit_submode_highl < HLF_COUNT
+ 		    ? hl_attr(edit_submode_highl) : 0);
+     }
+-    else
++    else if (!shortmess(SHM_COMPLETIONMENU))
+ 	msg_clr_cmdline();	/* necessary for "noshowmode" */
+ 
+     /* Show the popup menu, unless we got interrupted. */
+diff -r 462a4499f9c6 src/option.h
+--- a/src/option.h	Fri Nov 29 14:24:42 2013 +0900
++++ b/src/option.h	Fri Nov 29 18:07:09 2013 +0900
+@@ -212,7 +212,8 @@
+ #define SHM_SEARCH	's'		/* no search hit bottom messages */
+ #define SHM_ATTENTION	'A'		/* no ATTENTION messages */
+ #define SHM_INTRO	'I'		/* intro messages */
+-#define SHM_ALL		"rmfixlnwaWtToOsAI" /* all possible flags for 'shm' */
++#define SHM_COMPLETIONMENU	'c'	/* completion menu messages */
++#define SHM_ALL		"rmfixlnwaWtToOsAIc" /* all possible flags for 'shm' */
+ 
+ /* characters for p_go: */
+ #define GO_ASEL		'a'		/* autoselect */
+diff -r 462a4499f9c6 src/screen.c
+--- a/src/screen.c	Fri Nov 29 14:24:42 2013 +0900
++++ b/src/screen.c	Fri Nov 29 18:07:09 2013 +0900
+@@ -9704,7 +9704,8 @@
+ 	    }
+ #endif
+ #ifdef FEAT_INS_EXPAND
+-	    if (edit_submode != NULL)		/* CTRL-X in Insert mode */
++	    /* CTRL-X in Insert mode */
++	    if (edit_submode != NULL && !shortmess(SHM_COMPLETIONMENU))
+ 	    {
+ 		/* These messages can get long, avoid a wrap in a narrow
+ 		 * window.  Prefer showing edit_submode_extra. */
