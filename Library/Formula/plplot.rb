69aaee401fce351003187aa58cc24fd26ee85c76
Brett Koonce <koonce@gmail.com>
2014-01-16 00:51:35 -0600

    plplot 5.9.11
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/plplot.rb
+++ Library/Formula/plplot.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Plplot < Formula
   homepage 'http://plplot.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/plplot/plplot/5.9.10%20Source/plplot-5.9.10.tar.gz'
-  sha1 '10dbdfe41d71a9f51e4396729c001ed2baea22fe'
+  url 'http://downloads.sourceforge.net/project/plplot/plplot/5.9.11%20Source/plplot-5.9.11.tar.gz'
+  sha1 'cfe7e8085ca054d484b24598636d4a5dcbf357c6'
 
   depends_on 'cmake' => :build
   depends_on 'pkg-config' => :build
