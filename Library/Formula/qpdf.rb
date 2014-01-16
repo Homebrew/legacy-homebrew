a3ac16f22c1ee0f0f02cd990305295e03c60bded
Brett Koonce <koonce@gmail.com>
2014-01-16 00:52:04 -0600

    qpdf 5.1.1
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/qpdf.rb
+++ Library/Formula/qpdf.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Qpdf < Formula
   homepage 'http://qpdf.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/qpdf/qpdf/5.1.0/qpdf-5.1.0.tar.gz'
-  sha1 'a2aafad5c49efb62e98e6895bb96ca423179bf43'
+  url 'http://downloads.sourceforge.net/project/qpdf/qpdf/5.1.1/qpdf-5.1.1.tar.gz'
+  sha1 'e407a73bbaf6b1681e0952342e6d66a7746045c3'
 
   depends_on 'pcre'
 
