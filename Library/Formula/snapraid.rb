8117c4384cdc11fa07b581a8c101122a4e2e852e
Brett Koonce <koonce@gmail.com>
2014-01-16 00:52:25 -0600

    snapraid 5.2
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/snapraid.rb
+++ Library/Formula/snapraid.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Snapraid < Formula
   homepage 'http://snapraid.sourceforge.net/'
   head 'git://snapraid.git.sourceforge.net/gitroot/snapraid/snapraid'
-  url 'http://downloads.sourceforge.net/project/snapraid/snapraid-5.1.tar.gz'
-  sha1 'c36b91fbe56db4a45b6881f17ad8d39bea0b7fb6'
+  url 'http://downloads.sourceforge.net/project/snapraid/snapraid-5.2.tar.gz'
+  sha1 'b7abe28323eea6fd39152c34e0d26cf8530663db'
 
   def install
     system "./configure", "--prefix=#{prefix}"
