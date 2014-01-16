8068b150e8440986ea6d856c5ace4d15d61dd6fa
Frank Celler <f.celler@triagens.de>
2014-01-16 09:27:54 +0100

    arangodb 1.4.5
    Closes #25947.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/arangodb.rb
+++ Library/Formula/arangodb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Arangodb < Formula
   homepage 'http://www.arangodb.org/'
-  url 'https://www.arangodb.org/repositories/Source/ArangoDB-1.4.4.tar.gz'
-  sha1 'e4b099a47f384761fc6aeddc003c85d11e33f40f'
+  url 'https://www.arangodb.org/repositories/Source/ArangoDB-1.4.5.tar.gz'
+  sha1 '580c21270404727a8847b6acf5250a1fa20ac043'
 
   head "https://github.com/triAGENS/ArangoDB.git", :branch => 'unstable'
 
