62185485d3641275fd44a884e258fb83dcbf4a72
Silas Sewell <silas@sewell.org>
2014-01-17 15:59:02 -0500

    t1lib v5.1.2

---
+++ Library/Formula/t1lib.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class T1lib < Formula
+  homepage 'http://www.t1lib.org/'
+  url 'http://www.ibiblio.org/pub/Linux/libs/graphics/t1lib-5.1.2.tar.gz'
+  sha1 '4b4fc22c8688eefaaa8cfc990f0039f95f4287de'
+
+  def install
+    system './configure', "--prefix=#{prefix}"
+    system 'make', 'without_doc'
+    system 'make', 'install'
+  end
+end
