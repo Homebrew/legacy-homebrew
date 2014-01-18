9d6d7c9fd89653094357704953451edfb15a70d3
Brett Koonce <koonce@gmail.com>
2014-01-18 16:19:37 -0600

    nasm 2.11
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/nasm.rb
+++ Library/Formula/nasm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Nasm < Formula
   homepage 'http://www.nasm.us/'
-  url 'http://www.nasm.us/pub/nasm/releasebuilds/2.10.09/nasm-2.10.09.tar.bz2'
-  sha256 '7141180d3874b5967c6a60191e8d45fba9cc86bd60a4803ad80b6b6b3eac36b9'
+  url 'http://www.nasm.us/pub/nasm/releasebuilds/2.11/nasm-2.11.tar.bz2'
+  sha256 '1ce7e897c67255a195367a60c739a90a0b33a4a73f058f7cda3253bcf975642b'
 
   option :universal
 
