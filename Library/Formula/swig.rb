ec118db0b4d94bada79065344fa79461a5669d81
Florian Franzen <FlorianFranzen@gmail.com>
2014-01-18 07:13:11 +0100

    swig: Added HEAD version support.

--- Library/Formula/swig.rb
+++ Library/Formula/swig.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Swig < Formula
   homepage 'http://www.swig.org/'
+  head 'https://github.com/swig/swig.git'
   url 'http://downloads.sourceforge.net/project/swig/swig/swig-2.0.11/swig-2.0.11.tar.gz'
   sha1 'd3bf4e78824dba76bfb3269367f1ae0276b49df9'
 
@@ -9,8 +10,23 @@ class Swig < Formula
 
   depends_on 'pcre'
 
+  def patches 
+    p = []
+    p << "https://gist.github.com/FloFra/8486763/raw" if build.head?
+    return p
+  end
+
+  depends_on 'automake' => :build if build.head?
+  depends_on 'autoconf' => :build if build.head?
+  depends_on 'yodl' => :build if build.head?
+
   def install
     ENV.universal_binary if build.universal?
+    
+    if build.head?
+      system "./autogen.sh" 
+    end
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
