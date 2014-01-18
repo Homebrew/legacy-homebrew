d21bcdd3a674b4f16370e5b329506c5a91796f85
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2014-01-18 07:26:46 -0200

    Include formula for asciinema (http://asciinema.org/)
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

---
+++ Library/Formula/asciinema.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Asciinema < Formula
+  homepage 'http://asciinema.org/'
+  url 'https://github.com/sickill/asciinema/archive/v0.9.7.tar.gz'
+  sha1 '1f93942163d36ea5ca94f9ee9c68d45c3a213148'
+
+  depends_on :python => "2.7"
+
+  def install
+    system 'make', 'install', "PREFIX=#{prefix}"
+  end
+
+end
