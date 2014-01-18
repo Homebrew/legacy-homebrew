374609cd62463c3416f30d62ca7f4419946fbd57
Marcus André <hello@marcusandre.de>
2014-01-18 21:46:19 +0100

    add mongroup(1) recipe

---
+++ Library/Formula/mongroup.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Mongroup < Formula
+  homepage "https://github.com/jgallen23/mongroup"
+  url "https://github.com/jgallen23/mongroup/archive/0.4.0.tar.gz"
+  sha1 "b6472e325016353afaac04528e3226dc80401e95"
+
+  # add mon(1)
+  depends_on "mon"
+
+  # build
+  def install
+    system "make"
+    bin.install "mongroup"
+  end
+end
