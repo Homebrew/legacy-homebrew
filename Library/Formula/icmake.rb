d6bbfca5c8fd29120b9a1ce3ca4c6c89a81568d1
Florian Franzen <FlorianFranzen@gmail.com>
2014-01-19 03:34:05 +0100

    Fixed icmake formula according to brew build bot requirements.

--- Library/Formula/icmake.rb
+++ Library/Formula/icmake.rb
@@ -1,34 +1,32 @@
 require 'formula'
 
 class Icmake < Formula
-  url 'http://downloads.sourceforge.net/project/icmake/icmake/7.16.00/icmake_7.16.00.orig.tar.gz'
-  head 'https://icmake.svn.sourceforge.net/svnroot/icmake/trunk/icmake', :using => :svn
   homepage 'http://icmake.sourceforge.net/'
+  url 'http://downloads.sourceforge.net/project/icmake/icmake/7.16.00/icmake_7.16.00.orig.tar.gz'
+  head 'https://icmake.svn.sourceforge.net/svnroot/icmake/trunk/icmake'
   sha1 '8e175538dabc40afbb933202166bfdffd5f675c2'
-  version "7.16"
   
   def patches
-    p = []
-    # Fix Icmake's grep usage (with BSD sed) 
-    p << "https://gist.github.com/ColinHebert/1377434/raw"
-    # Fix Lexer
-    p << "https://gist.github.com/ColinHebert/1377507/raw"
-    # Fix Installer
-    p << "https://gist.github.com/ColinHebert/1377533/raw"
-
-    return p
+    [
+      # Fix sed syntax
+      "https://gist.github.com/ColinHebert/1377434/raw/2b9d07272210fdcdd1277abd64fb5d475a652086/conversions.diff",
+      # Removes redundant __STDC_VERSION__ from lexer.c
+      "https://gist.github.com/ColinHebert/1377507/raw/545a60cb39105c80d97e543dc77e00ab7ecbb5ff/lexer.c.diff",
+      # Fix Installer
+       "https://gist.github.com/ColinHebert/1377533/raw/c7cb3f768deafa8c2e393bed1f5d4acd42edc7bd/icm_install.diff",
+    ]
   end
 
   def install
     # Write config file used during build process
-    File.open('INSTALL.im', "w") {|file| file.puts <<-EOS.undent
-      #define BINDIR      "#{bin}"
-      #define SKELDIR     "#{share}"
-      #define MANDIR      "#{man}"
-      #define LIBDIR      "#{lib}"
-      #define CONFDIR     "#{etc + "icmake"}"
-      #define DOCDIR      "#{doc}"
-      #define DOCDOCDIR   "#{doc.dirname() + "icmake-doc"}"
+    File.open('INSTALL.im', 'w') { |file| file.write <<-EOS.undent
+        #define BINDIR      "#{bin}"
+        #define SKELDIR     "#{share}"
+        #define MANDIR      "#{man}"
+        #define LIBDIR      "#{lib}"
+        #define CONFDIR     "#{etc}icmake"
+        #define DOCDIR      "#{doc}"
+        #define DOCDOCDIR   "#{doc.dirname()}icmake-doc"
       EOS
     }
 
