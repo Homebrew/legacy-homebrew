0a731b6d91df5aa34d8b2d2a2aacbbadb30be809
Florian Franzen <FlorianFranzen@gmail.com>
2014-01-19 21:38:41 +0100

    yodl.rb: Reformated formula.

--- Library/Formula/yodl.rb
+++ Library/Formula/yodl.rb
@@ -4,41 +4,34 @@ class Yodl < Formula
   homepage "http://yodl.sourceforge.net/"
   url "http://downloads.sourceforge.net/project/yodl/yodl/3.03.0/yodl_3.03.0.orig.tar.gz"
   sha1 "bf244ab9b14024db98bee417c2055196237fab0b"
-  version "3.03.0"
 
-  option 'with-manual', "Use latex to create an optional manual during build."
-  
+  option "with-manual", "Use latex to create an optional manual during build."
+
   depends_on "icmake" => :build
-  depends_on :tex => 'with-manual'
+  depends_on :tex if build.with? "manual"
 
   def patches
-    'https://gist.github.com/FloFra/8485659/raw'
+    # Disables debugging and removes hardlink to icmake.
+    "https://gist.github.com/FloFra/8510468/raw/e599e9030ab3e39a12295522df489c010a337a3d/yodl_build_mac.patch"
   end
 
   def install
-    inreplace 'INSTALL.im', %-"/usr"-, %-"#{prefix}"-
-    inreplace 'build', '#!/usr/bin/icmake', '#!/usr/bin/env icmake' 
+    inreplace "INSTALL.im", %-"/usr"-, %-"#{prefix}"-
 
-    # Build all
-    system "./build programs"
-    system "./build man"
+    system "./build", "programs"
+    system "./build", "man"
 
-    if build.with? 'manual'
-      system "./build manual"
-    end
+    system "./build", "manual" if build.with? 'manual'
 
-    system "./build macros"
+    system "./build", "macros"
 
-    # Install all
-    system "./build install programs /"
-    system "./build install man /"
 
-    if build.with? 'manual'
-      system "./build install manual /"
-    end
+    system "./build", "install", "programs", "/"
+    system "./build", "install", "man", "/"
 
-    system "./build install macros /"
-    system "./build install docs /"
+    system "./build", "install", "manual", "/" if build.with? 'manual'
+
+    system "./build", "install", "macros", "/"
+    system "./build", "install", "docs", "/"
   end
 end
-
