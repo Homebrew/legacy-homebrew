dd8fad24d7fee06a3edc9774105c8d56e499ac8e
Marcus André <hello@marcusandre.de>
2014-01-19 19:32:38 +0100

    remove comments.

--- Library/Formula/mongroup.rb
+++ Library/Formula/mongroup.rb
@@ -5,10 +5,8 @@ class Mongroup < Formula
   url "https://github.com/jgallen23/mongroup/archive/0.4.0.tar.gz"
   sha1 "b6472e325016353afaac04528e3226dc80401e95"
 
-  # add mon(1)
   depends_on "mon"
 
-  # build
   def install
     system "make", "install"
   end
