d9c3a12e2f2af652e96cb6a284a69b2ba018323e
Markus Klink <justjoheinz@gmail.com>
2014-01-20 12:16:46 +0100

    Updated jenkins and sha1 to version 1.547

--- Library/Formula/jenkins.rb
+++ Library/Formula/jenkins.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Jenkins < Formula
   homepage 'http://jenkins-ci.org'
-  url 'http://mirrors.jenkins-ci.org/war/1.544/jenkins.war'
-  sha1 '3cf4332ce31e0fe1f4ae7391f3400dcb87ef8238'
+  url 'http://mirrors.jenkins-ci.org/war/1.547/jenkins.war'
+  sha1 '951e9f553a37db036a05d3d33750481a23fc40ac'
 
   head 'https://github.com/jenkinsci/jenkins.git'
 
