54b2d47b5af7749a5b7b03c95b2cc92e2e8d3da8
Mike McQuaid <mike@mikemcquaid.com>
2014-01-21 08:48:08 -0800

    zsh: remove caveats for system file modifications.

--- Library/Formula/zsh.rb
+++ Library/Formula/zsh.rb
@@ -46,16 +46,6 @@ class Zsh < Formula
   end
 
   def caveats; <<-EOS.undent
-    To use this build of Zsh as your login shell, add it to /etc/shells.
-
-    If you have administrator privileges, you must fix an Apple miss
-    configuration in Mac OS X 10.7 Lion by renaming /etc/zshenv to
-    /etc/zprofile, or Zsh will have the wrong PATH when executed
-    non-interactively by scripts.
-
-    Alternatively, install Zsh with /etc disabled:
-      brew install --disable-etcdir zsh
-
     Add the following to your zshrc to access the online help:
       unalias run-help
       autoload run-help
