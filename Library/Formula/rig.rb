4e0cff903d5d326a7a5347f62d6277e3e74724be
adnissen <andrew_nissen@yahoo.com>
2014-01-21 12:33:45 -0500

    updated homepage

--- Library/Formula/rig.rb
+++ Library/Formula/rig.rb
@@ -1,14 +1,14 @@
 require "formula"
 
 class Rig < Formula
-  homepage "http://packages.debian.org/wheezy/rig"
-  url "http://ftp.de.debian.org/debian/pool/main/r/rig/rig_1.11.orig.tar.gz"
+  homepage "http://rig.sourceforge.net/"
+  url "http://downloads.sourceforge.net/project/rig/rig/1.11/rig-1.11.tar.gz"
   sha1 "53a26676c35ba0c1112e1906deebb33a41dd5e95"
 
   def install
     system "make"
     bin.install 'rig'
-    (share/'rig').install 'data' 
+    (share/'rig').install 'data'
   end
 
   test do
