bcce9fb72af9a8ca903ca8475732cbdb5a913360
Tilman Potthof <tilmanpotthof@googlemail.com>
2014-01-22 02:27:13 +0100

    HEVEA 2.12
    Closes #26085.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hevea.rb
+++ Library/Formula/hevea.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hevea < Formula
   homepage 'http://hevea.inria.fr/'
-  url 'http://hevea.inria.fr/distri/hevea-2.09.tar.gz'
-  sha1 '7186fccfe84611680a6451fcb9c9d78130f3adf2'
+  url 'http://hevea.inria.fr/distri/hevea-2.12.tar.gz'
+  sha1 '21ca8c366a3fd1030b3cfba3cd2aa16e6baf5c56'
 
   depends_on 'objective-caml'
   depends_on 'ghostscript' => :optional
