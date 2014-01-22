a5d0225aafcb24c5beb23df98d3ce2b73c722c7a
Nobutaka OSHIRO <n-oshiro@nekojarashi.com>
2014-01-22 13:59:36 +0900

    stone: add --with-ssl option

--- Library/Formula/stone.rb
+++ Library/Formula/stone.rb
@@ -5,8 +5,11 @@ class Stone < Formula
   url 'http://www.gcd.org/sengoku/stone/stone-2.3e.tar.gz'
   sha1 '1ed12f29c1f896c1a94237b30201c615af5bb0f8'
 
+  option 'with-ssl', 'SSL support'
+
   def install
-    system "make", "macosx"
+    target = (build.with? 'ssl') ? "macosx-ssl" : "macosx"
+    system "make", target
     bin.install "stone"
   end
 end
