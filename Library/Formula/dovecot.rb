b4d86ad0a5f4307ab6f07415bae759eec6412a0f
Damon Haley <damon.haley@colorado.edu>
2014-01-22 15:16:04 -0700

    working version after running:
    brew install --HEAD clucene

--- Library/Formula/dovecot.rb
+++ Library/Formula/dovecot.rb
@@ -1,11 +1,22 @@
 require 'formula'
 
+
 class Dovecot < Formula
   homepage 'http://dovecot.org/'
-  url 'http://dovecot.org/releases/2.2/dovecot-2.2.2.tar.gz'
-  sha256 '30c98e8f9e40d7397d451a8679359b70031702f205aefbff1a6e27656fb63b9a'
+  url 'http://dovecot.org/releases/2.2/dovecot-2.2.10.tar.gz'
+  mirror 'http://fossies.org/linux/misc/dovecot-2.2.10.tar.gz'
+  sha256 '75592483d40dc4f76cc3b41af40caa4be80478946a699d46846d5d03e4d2e09b'
+
+  # http://snowball.tartarus.org/
+  resource 'stemmer' do
+    url 'http://snowball.tartarus.org/dist/libstemmer_c.tgz'
+    sha1 '69056075b9fa1382e07cec6c32c8e82f3f35677b'
+  end
 
   def install
+    (buildpath/'libstemmer_c').install resource('stemmer')
+
+    ENV.append 'CPPFLAGS', "-I/usr/local/Cellar/clucene/2.3.3.4/lib"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--libexecdir=#{libexec}",
@@ -14,7 +25,6 @@ class Dovecot < Formula
                           "--with-ssl=openssl",
                           "--with-sqlite",
                           "--with-lucene",
-                          "--with-stemmer",
                           "--with-zlib",
                           "--with-bzlib"
     system "make install"
@@ -53,6 +63,3 @@ Source: http://wiki.dovecot.org/LaunchdInstall
     EOS
   end
 end
-
-
-require 'formula'
