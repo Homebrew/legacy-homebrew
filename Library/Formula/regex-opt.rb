079200fb8975a8a90fe9f7c44e91adc81320d4a3
Ryan Davis <ryand-ruby@zenspider.com>
2014-01-22 13:31:21 -0800

    Switched regex-opt to apple-gcc42 to get it to build properly (uses GCC extensions).

--- Library/Formula/regex-opt.rb
+++ Library/Formula/regex-opt.rb
@@ -5,9 +5,17 @@ class RegexOpt < Formula
   url 'http://bisqwit.iki.fi/src/arch/regex-opt-1.2.3.tar.gz'
   sha1 'a3b58af5a173a9b77ede7d0cb01831ae7a315b38'
 
+  depends_on 'apple-gcc42' if MacOS.version >= :mountain_lion
+
   def install
     system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}"
     bin.install 'regex-opt'
+  end
+
+  fails_with :clang do
+    cause <<-EOS.undent
+      regexp-opt uses GCC extensions and must be built with gcc, not clang.
+      EOS
   end
 
   def test
