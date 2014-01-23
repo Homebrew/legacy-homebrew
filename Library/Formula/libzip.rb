67babebada7581b20df42f056a7964b43f5b271f
Duncan Tebbs <duncan.tebbs@gmail.com>
2014-01-23 23:34:30 +0900

    libzip: universal binary support
    Closes #26122.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libzip.rb
+++ Library/Formula/libzip.rb
@@ -5,7 +5,10 @@ class Libzip < Formula
   url 'http://www.nih.at/libzip/libzip-0.11.2.tar.gz'
   sha1 'eeb3b5567fcf3532fa4bcb6440a87c7ad8507d2d'
 
+  option :universal
+
   def install
+    ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "CXX=#{ENV.cxx}",
