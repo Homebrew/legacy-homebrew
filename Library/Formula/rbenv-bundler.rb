4bdb14b6779da71e5d6f1552de4db2f2bab59108
Martin Puppe <mail@mpuppe.de>
2014-01-23 18:24:51 +0100

    rbenv-bundler: add HEAD
    Closes #26125.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/rbenv-bundler.rb
+++ Library/Formula/rbenv-bundler.rb
@@ -4,6 +4,7 @@ class RbenvBundler < Formula
   homepage 'https://github.com/carsomyr/rbenv-bundler'
   url 'https://github.com/carsomyr/rbenv-bundler/archive/0.96.tar.gz'
   sha1 '02339c604c840f24b66523c9faad6ed82500eda1'
+  head 'https://github.com/carsomyr/rbenv-bundler.git'
 
   depends_on 'rbenv'
 
