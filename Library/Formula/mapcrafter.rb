3ea229ad3d4649f16aa044d95a786046b7ef7abd
Thor Erik Lie <thor@otc.nu>
2014-01-23 08:53:17 +0100

    changed mapcrafter to use v 1.1.2 V 1.1.2 includes also 1.1.1 which fixes a mac spesific issue 1.1.1 includes fixes for threads hanging when queue is 0

--- Library/Formula/mapcrafter.rb
+++ Library/Formula/mapcrafter.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mapcrafter < Formula
   homepage 'http://mapcrafter.org'
-  url 'https://github.com/m0r13/mapcrafter/archive/v.1.1.zip'
-  sha1 '125e5559e713a5d7be3c94c575edf8a795043f7f'
+  url 'https://github.com/m0r13/mapcrafter/archive/v.1.1.2.zip'
+  sha1 'd303cf08504fa65c86032cb50fd852f65e92d629'
 
   depends_on 'cmake' => :build
   depends_on 'libpng'
