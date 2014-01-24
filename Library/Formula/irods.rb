0481964b2ef592694e4adcc7d5600367aaf8dbd9
Rui Pereira <rui.pereira@gmail.com>
2014-01-24 10:32:56 +0100

    Fix compilation with osxfuse.

--- Library/Formula/irods.rb
+++ Library/Formula/irods.rb
@@ -28,7 +28,8 @@ class Irods < Formula
       inreplace 'config/config.mk', '# IRODS_FS = 1', 'IRODS_FS = 1'
       inreplace 'config/config.mk', 'fuseHomeDir=/home/mwan/adil/fuse-2.7.0', "fuseHomeDir=#{HOMEBREW_PREFIX}"
       chdir 'clients/fuse' do
-        inreplace 'Makefile', 'lfuse', 'lfuse4x'
+        inreplace 'Makefile', 'lfuse', 'losxfuse'
+        inreplace 'Makefile', '-I$(fuseHomeDir)/include', '-I$(fuseHomeDir)/include/osxfuse'
         system 'make'
       end
       bin.install Dir['clients/fuse/bin/*'].select {|f| File.executable? f}
