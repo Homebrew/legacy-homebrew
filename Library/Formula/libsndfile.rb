c9ca73f28750a5fd13a6f0c3b726da9dcf1609fb
Aleh Suprunovich <br@ahlamon.org>
2014-01-26 01:23:31 +0300

    libsndfile: patch attribution information updated
    Signed-off-by: Aleh Suprunovich <br@ahlamon.org>

--- Library/Formula/libsndfile.rb
+++ Library/Formula/libsndfile.rb
@@ -21,7 +21,10 @@ class Libsndfile < Formula
       # fixed upstream: https://github.com/erikd/libsndfile/commit/d04e1de82ae0af48fd09d5cb09bf21b4ca8d513c
       "https://gist.github.com/metabr/8623583/raw/90966b76c6f52e1293b5303541e1f2d72e2afd08/0001-libsndfile-doesn-t-find-Carbon.h-using-XCode-4.3.patch",
       # libsndfile fails to build with libvorbis 1.3.4
-      # fixed in FINK: http://fink.cvs.sourceforge.net/viewvc/fink/dists/10.7/stable/main/finkinfo/sound/libsndfile1.patch?revision=1.2&view=markup
+      # fixed upstream:
+      # https://github.com/erikd/libsndfile/commit/d7cc3dd0a437cfb087e09c80c0b89dfd3ec80a54
+      # https://github.com/erikd/libsndfile/commit/700ae0e8f358497dd614bcee8e4b93c629209b37
+      # https://github.com/erikd/libsndfile/commit/50d1df56f7f9b90d0fafc618d4947611e9689ae9
       "https://gist.github.com/metabr/8623583/raw/cd3540f4abd8521edf0011ab6dd40888cfadfd7a/0002-libsndfile-fails-to-build-with-libvorbis-1.3.4.patch"
     ]
   end
