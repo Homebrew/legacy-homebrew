3006eee5575bd3a58e5d742bc13abfa974a54960
Mandar Gokhale <mandar.mmg@gmail.com>
2014-01-25 14:53:32 -0500

    Remove generated comments, fix tests
    Add missing / to one test path, and change the --help option to the
    (correct) -help

--- Library/Formula/bittwist.rb
+++ Library/Formula/bittwist.rb
@@ -7,19 +7,11 @@ class Bittwist < Formula
 
   def install
     system "make"
-    system "make", "install"
+    system "make", "install", "prefix=#{prefix}"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test bittwist-macosx`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/bittwist", "--help"
-    system "#{bin}bittwiste", "--help"
+    system "#{bin}/bittwist -help"
+    system "#{bin}/bittwiste -help"
   end
 end
