97ff1b6c23f107131cec28108fa9c01e65d770e4
Aleh Suprunovich <br@ahlamon.org>
2014-01-26 00:11:19 +0300

    libsndfile: fix build with libvorbis 1.3.4
    Closes #26172.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libsndfile.rb
+++ Library/Formula/libsndfile.rb
@@ -6,6 +6,9 @@ class Libsndfile < Formula
   sha1 'e95d9fca57f7ddace9f197071cbcfb92fa16748e'
 
   depends_on 'pkg-config' => :build
+  depends_on :autoconf
+  depends_on :automake
+  depends_on :libtool
   depends_on 'flac'
   depends_on 'libogg'
   depends_on 'libvorbis'
@@ -13,27 +16,24 @@ class Libsndfile < Formula
   option :universal
 
   def patches
-    # libsndfile doesn't find Carbon.h using XCode 4.3:
-    # fixed upstream: https://github.com/erikd/libsndfile/commit/d04e1de82ae0af48fd09d5cb09bf21b4ca8d513c
-    DATA
+    [
+      # libsndfile doesn't find Carbon.h using XCode 4.3:
+      # fixed upstream: https://github.com/erikd/libsndfile/commit/d04e1de82ae0af48fd09d5cb09bf21b4ca8d513c
+      "https://gist.github.com/metabr/8623583/raw/90966b76c6f52e1293b5303541e1f2d72e2afd08/0001-libsndfile-doesn-t-find-Carbon.h-using-XCode-4.3.patch",
+      # libsndfile fails to build with libvorbis 1.3.4
+      # fixed upstream:
+      # https://github.com/erikd/libsndfile/commit/d7cc3dd0a437cfb087e09c80c0b89dfd3ec80a54
+      # https://github.com/erikd/libsndfile/commit/700ae0e8f358497dd614bcee8e4b93c629209b37
+      # https://github.com/erikd/libsndfile/commit/50d1df56f7f9b90d0fafc618d4947611e9689ae9
+      "https://gist.github.com/metabr/8623583/raw/cd3540f4abd8521edf0011ab6dd40888cfadfd7a/0002-libsndfile-fails-to-build-with-libvorbis-1.3.4.patch"
+    ]
   end
 
   def install
     ENV.universal_binary if build.universal?
 
+    system "autoreconf", "-i"
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
 end
-
-__END__
---- a/programs/sndfile-play.c	2011-03-27 22:15:31.000000000 -0700
-+++ b/programs/sndfile-play.c	2012-02-24 20:02:06.000000000 -0800
-@@ -58,7 +58,6 @@
- 	#include 	<sys/soundcard.h>
- 
- #elif (defined (__MACH__) && defined (__APPLE__))
--	#include <Carbon.h>
- 	#include <CoreAudio/AudioHardware.h>
- 
- #elif defined (HAVE_SNDIO_H)
