454b2c82b02d02e6ce2b2db5be96899d496cc585
Benjamin Weber <one@bwe.im>
2014-01-26 19:34:28 +0100

    taskd 1.0.0
    Taskserver 1.0.0 provides basic, task-level, conflict-free
    synchronization between client and server, supporting
    multiple users. This sync capability exceeds the crude file-based
    sync provided by file sharing options.

---
+++ Library/Formula/taskd.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Taskd < Formula
+  homepage 'http://taskwarrior.org'
+  url 'http://taskwarrior.org/download/taskd-1.0.0.tar.gz'
+  sha1 '5a89406a21be1f95ece03674315b35814fe4f037'
+  head 'http://tasktools.org/taskd.git'
+
+  depends_on 'cmake' => :build
+  depends_on 'gnutls'
+  depends_on 'ossp-uuid'
+  depends_on 'readline'
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/taskd", 'diagnostics'
+  end 
+end
