e7d16c0de6391a7a58fbdd9683732acce74ec8f0
Laurent Raufaste <analogue@glop.org>
2014-01-26 04:32:12 -0500

    mackup 0.5.9
    Closes #26180.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mackup.rb
+++ Library/Formula/mackup.rb
@@ -2,11 +2,20 @@ require 'formula'
 
 class Mackup < Formula
   homepage 'https://github.com/lra/mackup'
-  url 'https://github.com/lra/mackup/archive/0.5.8.tar.gz'
-  sha1 'adab98fe9350ce45d4502a44b891dde64375d604'
+  url 'https://github.com/lra/mackup/archive/0.5.9.tar.gz'
+  sha1 '69d13c27b3a2794ddecb415333e783ca45aff055'
+
+  head 'https://github.com/lra/mackup.git'
+
+  depends_on :python
 
   def install
-    bin.install "mackup.py" => "mackup"
-    (share/'mackup').install ".mackup.cfg" => "mackup.cfg.example"
+    bin.install 'bin/mackup'
+    (prefix+'mackup').install 'mackup/__init__.py', 'mackup/main.py'
+    share.install '.mackup.cfg' => 'mackup.cfg.example'
+  end
+
+  def test
+    system "#{bin}/mackup", '-h'
   end
 end
