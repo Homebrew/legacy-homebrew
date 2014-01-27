6e8f3031e136803567357d9c522258a13e3ad3e8
Anand Buddhdev <arhbkb@gmail.com>
2014-01-27 21:27:58 +0100

    NSD: update to version 4.0.1

--- Library/Formula/nsd.rb
+++ Library/Formula/nsd.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Nsd < Formula
   homepage 'http://www.nlnetlabs.nl/projects/nsd/'
-  url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-4.0.0.tar.gz'
-  sha1 'b3ebd669be8e830f62062d12be55242ca41da369'
+  url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-4.0.1.tar.gz'
+  sha1 '788cc290ade7fc6a61fe4391837d3abddbda4df0'
 
-  option 'with-root-server', 'Allow NSD to run as a root server'
+  option 'with-root-server', 'Allow NSD to run as a root name server'
   option 'with-bind8-stats', 'Enable BIND8-like NSTATS & XSTATS'
   option 'with-ratelimit', 'Enable rate limiting'
 
