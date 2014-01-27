56828c3b0515dd555e3331d86e29782d1f40e069
gragana <michael.garrett@gmail.com>
2014-01-27 14:43:00 -0700

    Update kanif.rb
    cleanup changes

--- Library/Formula/kanif.rb
+++ Library/Formula/kanif.rb
@@ -1,10 +1,11 @@
 require "formula"
 
 class Kanif < Formula
-  depends_on 'taktuk'
   homepage "http://http://taktuk.gforge.inria.fr/kanif/"
   url "http://gforge.inria.fr/frs/download.php/26773/kanif-1.2.2.tar.gz"
   sha1 "7a10fe0e74159875f004b6c4a12a0202ff092ce9"
+  
+  depends_on 'taktuk'
 
   def install
     system "./configure", "--disable-debug",
