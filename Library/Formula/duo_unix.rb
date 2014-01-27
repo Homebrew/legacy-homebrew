91ba89559661ee40d1098d634629c87e65866314
Grant Stavely <gstavely@evernote.com>
2014-01-27 10:40:39 -0800

    duo_unix: 1.9.6 (new formula)
    Second-factor authentication via Duo login service.
    
    Duo provides secondary authentication via the Duo authentication
    service, executing the user's login shell or command only if successful.
    
    Closes #26207.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/duo_unix.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class DuoUnix < Formula
+  homepage "https://www.duosecurity.com/docs/duounix"
+  url "https://dl.duosecurity.com/duo_unix-1.9.6.tar.gz"
+  sha1 "02f2d28af55872c278df8b99d2e8503098504583"
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{sbin}/login_duo", "-d", "-c", "#{etc}/login_duo.conf", "-f", "foobar", "echo", "SUCCESS"
+  end
+end
