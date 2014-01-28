1b7f16a6d691492d71f78d16882f7442261b745a
gragana <michael.garrett@gmail.com>
2014-01-28 12:12:48 -0700

    Update taktuk.rb
    changed url from 'https' to 'http'

--- Library/Formula/taktuk.rb
+++ Library/Formula/taktuk.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Taktuk < Formula
   homepage "http://http://taktuk.gforge.inria.fr/"
-  url "https://gforge.inria.fr/frs/download.php/30903/taktuk-3.7.4.tar.gz"
+  url "http://gforge.inria.fr/frs/download.php/30903/taktuk-3.7.4.tar.gz"
   sha1 "947de1e9810316142815df3077e3f629680de564"
 
   def install
