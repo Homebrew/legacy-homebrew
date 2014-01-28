110427999cd667898f028d7c1a86ae5b4f96f83e
Rich Cheng <github@whileyouweregone.co.uk>
2014-01-28 10:01:55 +0000

    trash: update URL
    The author of 'trash' has changed his name on GitHub, so the download
    URL needed to be updated.
    
    Closes #26220.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/trash.rb
+++ Library/Formula/trash.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Trash < Formula
   homepage 'http://hasseg.org/trash/'
-  url 'http://github.com/hasseg/trash/archive/v0.8.3.zip'
+  url 'http://github.com/ali-rantakari/trash/archive/v0.8.3.zip'
   sha1 'de3489f7dbfe03dc490a3c7da22dba90998af3d1'
 
   conflicts_with 'osxutils', :because => 'both install a trash binary'
