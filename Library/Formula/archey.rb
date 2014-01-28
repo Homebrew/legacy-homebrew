dd15e3ce5c31e62c6dc419b932fb214a843876bd
Jeff Hann <jeffhann@gmail.com>
2014-01-28 10:10:35 -0400

    updating archey to fix a error while caculating ram

--- Library/Formula/archey.rb
+++ Library/Formula/archey.rb
@@ -2,12 +2,11 @@ require 'formula'
 
 class Archey < Formula
   homepage 'http://obihann.github.io/archey-osx/'
-  url 'https://github.com/obihann/archey-osx/archive/1.1.tar.gz'
+  url 'https://github.com/obihann/archey-osx/archive/1.2.tar.gz'
   sha1 '52da445c38c08415caa8ba93d28bbfe7b4ac1003'
-  version "1.0"
+  version "1.2"
 
   def install
-    system "mkdir #{bin}"
-    system "cp bin/archey #{bin}/"
+    bin.install 'bin/archey'
   end
 end
