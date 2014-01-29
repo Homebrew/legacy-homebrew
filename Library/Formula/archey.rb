377d137bf187073402623cbc93d6327fa499ed00
Mike McQuaid <mike@mikemcquaid.com>
2014-01-29 16:14:57 +0100

    archey: fix SHA-1.

--- Library/Formula/archey.rb
+++ Library/Formula/archey.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Archey < Formula
   homepage 'http://obihann.github.io/archey-osx/'
   url 'https://github.com/obihann/archey-osx/archive/1.2.tar.gz'
-  sha1 '4c93027246391be2abba7184e5170aeced09eeb9'
+  sha1 '61fe422a37f07a1c24762244a355bf992452dae4'
 
   def install
     bin.install 'bin/archey'
