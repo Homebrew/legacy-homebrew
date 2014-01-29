52aba7cf725160c1c16d9e7bac2b1f92dc6eebdb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-01-29 17:27:04 +0000

    phantomjs: add 1.9.7 bottle.
    Closes #26240.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/phantomjs.rb
+++ Library/Formula/phantomjs.rb
@@ -5,6 +5,13 @@ class Phantomjs < Formula
   url 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-source.zip'
   sha1 '124b017d493d5ccabd22afaf078d0650ac048840'
 
+  bottle do
+    cellar :any
+    sha1 "fb82891b5d63de81a89c7b5b41b8aeba39dec470" => :mavericks
+    sha1 "042dd2341a1140cdcd575e08aa432ed659e3f00e" => :mountain_lion
+    sha1 "575af708d7e5ef6e39e90766ffd2bf97b6c4b51b" => :lion
+  end
+
   def patches
     [
       'https://github.com/ariya/phantomjs/commit/fe6a96.patch'
