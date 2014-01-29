737e6aaee5397d0e878ad0e55ad02127c178a794
Thomas Danielsson <thomas@elajt.se>
2014-01-30 00:20:18 +0100

    subnetcalc: 2.2.0 - IPv4/IPv6 Subnet Calculator
    Installs subnetcalc with optional geoip-support.
    Unupdated repo seem to be located at https://github.com/dreibh/subnetcalc

---
+++ Library/Formula/subnetcalc.rb
@@ -0,0 +1,21 @@
+require "formula"
+
+class Subnetcalc < Formula
+  homepage "http://www.iem.uni-due.de/~dreibh/subnetcalc/"
+  url "http://www.iem.uni-due.de/~dreibh/subnetcalc/download/subnetcalc-2.2.0.tar.gz"
+  sha1 "fbe6411443f4140937a85f051e733efd803ec7b3"
+
+  depends_on 'geoip' => :recommended
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-geoip=no" if build.with? "geoip"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/subnetcalc", "::1"
+  end
+end
