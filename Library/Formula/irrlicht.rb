6ca324537b6317116268f652d9f0bdf3eb6278a1
Pavel Puchkin <i@neoascetic.me>
2014-01-30 14:12:50 +1000

    irrlicht: update sha1
    http://irrlicht.sourceforge.net/2013/12/irrlicht-1-8-1-makefile-version-number-fix/
    
    Closes #26278.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/irrlicht.rb
+++ Library/Formula/irrlicht.rb
@@ -4,7 +4,7 @@ class Irrlicht < Formula
   homepage 'http://irrlicht.sourceforge.net/'
   head 'https://irrlicht.svn.sourceforge.net/svnroot/irrlicht/trunk'
   url 'http://downloads.sourceforge.net/irrlicht/irrlicht-1.8.1.zip'
-  sha1 '47f073dabf329e102628757bde4520a4d1629028'
+  sha1 '231cd2cf2eefe43bde7c40537ece055a3d6e09cb'
 
   depends_on :xcode
 
