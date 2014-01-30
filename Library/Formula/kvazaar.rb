03b90c3581d33c2f475fb5cc337043f06c93cf14
Dirk Gadsden <dirk@esherido.com>
2014-01-29 19:31:38 -0500

    Re-add kvazaar formula (for Homebrew/homebrew#26252)

---
+++ Library/Formula/kvazaar.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Kvazaar < Formula
+  homepage "https://github.com/ultravideo/kvazaar"
+  url "https://github.com/ultravideo/kvazaar/archive/v0.2.0.tar.gz"
+  sha1 "b0f23dc0d421e64183deba8fdcd2347863d711d5"
+
+  depends_on 'yasm' => :build
+
+  def install
+    inreplace 'src/Makefile', 'elf64', 'macho64'
+    inreplace 'src/Makefile', 'elf', 'macho32'
+    inreplace 'src/x64/test64.asm', 'cpuId64', '_cpuId64'
+    cd "src" do
+      system "make"
+    end
+  end
+end
