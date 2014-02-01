55eadf7cc5771c09c943f2445f0d8a814c6c40a6
Matthew L Daniel <mdaniel@gmail.com>
2014-01-31 18:44:51 -0800

    reposurgeon 3.1
    Closes #26320.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/reposurgeon.rb
+++ Library/Formula/reposurgeon.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Reposurgeon < Formula
   homepage 'http://www.catb.org/esr/reposurgeon/'
-  url 'http://www.catb.org/~esr/reposurgeon/reposurgeon-3.0.tar.gz'
-  sha256 '358b72670c645b62a122feccebb660d9f3c98544bf1fc4d159b1b44cb2a45107'
+  url 'http://www.catb.org/~esr/reposurgeon/reposurgeon-3.1.tar.gz'
+  sha256 '34f6ca25ce9ebab477661294dcde943e70621bafb523a0cb28a8b551bb6a532f'
 
   depends_on 'asciidoc'
   depends_on 'xmlto'
