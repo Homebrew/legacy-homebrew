6335ae78e6bb1e86e7f1e34706a24a8bc1520201
Sun Liwen <sunliwen@gmail.com>
2014-02-01 18:51:34 +0800

    parallel 20140122
    Closes #26325.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Parallel < Formula
   homepage 'http://savannah.gnu.org/projects/parallel/'
-  url 'http://ftpmirror.gnu.org/parallel/parallel-20131222.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20131222.tar.bz2'
-  sha256 '6772be4d236de1696c1d7f84eee00f36d0a69da28c845c8730c38816c9eaef21'
+  url 'http://ftpmirror.gnu.org/parallel/parallel-20140122.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/parallel/parallel-20140122.tar.bz2'
+  sha256 'b18339bba65f9b2e287ec934d05c12e8103c2c83bc7f9b3c06f6648ecf15c79f'
 
   conflicts_with 'moreutils',
     :because => "both install a 'parallel' executable."
