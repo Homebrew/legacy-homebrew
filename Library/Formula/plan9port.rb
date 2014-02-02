3881039d64f4eec258c5b8a1c2b40da82e9ad339
David Holm <dholmster@gmail.com>
2014-02-02 10:07:25 +0000

    plan9port 20140123 (new formula)
    Closes #26323.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/plan9port.rb
@@ -0,0 +1,31 @@
+require "formula"
+
+class Plan9port < Formula
+  homepage "http://swtch.com/plan9port/"
+  url "https://plan9port.googlecode.com/files/plan9port-20140123.tgz"
+  sha1 "0b207cdaac9df2dd1ae7f8e156c4ffcc625d594c"
+
+  def install
+    ENV["PLAN9_TARGET"] = libexec
+    system "./INSTALL"
+
+    libexec.install Dir["*"]
+    bin.install_symlink Dir["#{libexec}/bin/9"]
+    Dir["#{libexec}/mac/*.app"].each {|path| mv path, prefix }
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <u.h>
+      #include <libc.h>
+      #include <stdio.h>
+
+      int main(void) {
+        return printf("Hello World\\n");
+      }
+    EOS
+    system "#{bin}/9", "9c", "test.c"
+    system "#{bin}/9", "9l", "-o", "test", "test.o"
+    assert_equal "Hello World\n", `./test`
+  end
+end
