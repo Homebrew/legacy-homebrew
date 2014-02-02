6f623987eef8c31a373a9f5a38b847937a66972b
Jack Nagel <jacknagel@gmail.com>
2014-02-02 13:47:29 -0500

    mpg123 1.18.0

--- Library/Formula/mpg123.rb
+++ Library/Formula/mpg123.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Mpg123 < Formula
   homepage 'http://www.mpg123.de/'
-  url 'http://downloads.sourceforge.net/project/mpg123/mpg123/1.17.0/mpg123-1.17.0.tar.bz2'
-  mirror 'http://mpg123.orgis.org/download/mpg123-1.17.0.tar.bz2'
-  sha1 '8185f06ab94651adb2fc7a1ab860f0fd154b6f80'
+  url 'http://downloads.sourceforge.net/project/mpg123/mpg123/1.18.0/mpg123-1.18.0.tar.bz2'
+  mirror 'http://mpg123.orgis.org/download/mpg123-1.18.0.tar.bz2'
+  sha1 'd6e59eeb972a7d11dfcc7505cf1f3e0ae875f439'
 
   def install
     args = ["--disable-debug", "--disable-dependency-tracking",
