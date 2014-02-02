f794ee0ca4207415937d88c0a399d248cc38670e
Tim Fenney <kodekata@gmail.com>
2014-02-01 19:09:02 -0500

    dcraw 9.20
    Bump version from 9.19. Also, change dependency on little-cms to
    little-cms2.
    
    Closes #26340.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/dcraw.rb
+++ Library/Formula/dcraw.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class Dcraw < Formula
   homepage 'http://www.cybercom.net/~dcoffin/dcraw/'
-  url 'http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-9.19.tar.gz'
-  sha1 '83a85200b0f105a40d23e885f5108bc3b53bdbe4'
+  url 'http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-9.20.tar.gz'
+  sha1 '9b8ca82eaaf6582ffa0f2602e8ca705c722ea87e'
 
   depends_on 'jpeg'
   depends_on 'jasper'
-  depends_on 'little-cms'
+  depends_on 'little-cms2'
 
   def install
     ENV.append_to_cflags "-I#{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib"
-    system "#{ENV.cc} -o dcraw #{ENV.cflags} dcraw.c -lm -ljpeg -llcms -ljasper"
+    system "#{ENV.cc} -o dcraw #{ENV.cflags} dcraw.c -lm -ljpeg -llcms2 -ljasper"
     bin.install 'dcraw'
     man1.install 'dcraw.1'
   end
