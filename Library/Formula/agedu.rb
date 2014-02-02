34f409f72536cb8126ad6e3492efca67e482a518
Kevin Emmett <kjemmett@gmail.com>
2014-02-02 16:23:13 -0500

    agedu r10126
    Closes #26356.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/agedu.rb
+++ Library/Formula/agedu.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Agedu < Formula
   homepage 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/'
-  url 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-r9723.tar.gz'
-  version 'r9723'
-  sha1 '3b6a7c73a033c86a142f6d2bfa9d5d481d96adc5'
+  url 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-r10126.tar.gz'
+  version 'r10126'
+  sha1 'b16e5b68a0c093298bb453fac6abe9a330c9a694'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
