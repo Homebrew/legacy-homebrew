d64b14b2618bb2329e19e154cdaa3596d7735e4f
Shannon Skipper <shannonskipper@gmail.com>
2014-02-03 13:46:45 -0800

    Update url to project's current location.
    Update homepage to new url.

--- Library/Formula/libyaml.rb
+++ Library/Formula/libyaml.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Libyaml < Formula
-  homepage 'http://pyyaml.org/wiki/LibYAML'
-  url 'http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz'
+  homepage 'https://bitbucket.org/xi/libyaml'
+  url 'https://bitbucket.org/xi/libyaml/get/0.1.4.tar.gz'
   sha1 'e0e5e09192ab10a607e3da2970db492118f560f2'
 
   option :universal
