65ab91672db31fe8a56127566970ff16f4ab56b7
archstar <archstar@git.fake>
2014-02-03 15:08:13 -0700

    pebble-sdk 2.0.0

--- Library/Formula/pebble-sdk.rb
+++ Library/Formula/pebble-sdk.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class PebbleSdk < Formula
   homepage 'https://developer.getpebble.com/2/'
-  url 'https://s3.amazonaws.com/assets.getpebble.com/sdk2/PebbleSDK-2.0-BETA7.tar.gz'
-  version '2.0-BETA7'
-  sha1 '26455446e5e3c68fa321a6ac9b644c8dd36a8343'
+  url 'https://s3.amazonaws.com/assets.getpebble.com/sdk2/PebbleSDK-2.0.0.tar.gz'
+  version '2.0.0'
+  sha1 'e06d77d26c8adb20760217b7f68944a5b72bd223'
 
   depends_on 'freetype' => :recommended
   depends_on 'mpfr'
