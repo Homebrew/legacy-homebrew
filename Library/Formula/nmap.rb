14fe36e17f09084d42faee263836270648a2ff39
timothykang <timkang@ucla.edu>
2014-02-03 11:49:33 -0800

    nmap: Add build flag '--without-nmap-update'
    Without SVN installed, running nmap causes it to complain that
    'libsvn_client-1.0.dylib' isn't loaded, which the nmap-update component
    depends on to connect to https://svn.nmap.org/updates to get NSE script
    updates.
    
    Since it looks like https://svn.nmap.org/updates is only accessible to
    primary nmap developers at the moment, build nmap without nmap-update
    (rather than add SVN as a dependency).
    
    Closes #26386.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/nmap.rb
+++ Library/Formula/nmap.rb
@@ -22,6 +22,7 @@ class Nmap < Formula
       --with-libpcre=included
       --with-liblua=included
       --with-openssl=#{Formula.factory("openssl").prefix}
+      --without-nmap-update
       --without-zenmap
       --disable-universal
     ]
