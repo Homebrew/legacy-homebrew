b84e563bc8c1f7c4e0fee4e6c57813e8848c2c7e
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-02-04 07:48:28 +0700

    termrec: 0.17 - switch to github tarball

--- Library/Formula/termrec.rb
+++ Library/Formula/termrec.rb
@@ -2,10 +2,12 @@ require "formula"
 
 class Termrec < Formula
   homepage "http://angband.pl/termrec.html"
-  url "http://angband.pl/git/termrec/", :using => :git, :tag => "0.17"
+  url "https://github.com/kilobyte/termrec/archive/0.17.tar.gz"
+  sha1 "45df1b35b7f236fafd1e4db9c45543501dfde359"
   head "http://angband.pl/git/termrec/", :using => :git
   depends_on "libtool" => :build
   depends_on "automake" => :build
+  depends_on "autoconf" => :build
   depends_on "xz"
 
   def install
