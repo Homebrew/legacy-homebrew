357dda23d7b1659e48a35b0ce0289d6f94c86f7a
Jack Nagel <jacknagel@gmail.com>
2014-02-04 11:07:02 -0500

    gettext: add bottles

--- Library/Formula/gettext.rb
+++ Library/Formula/gettext.rb
@@ -6,6 +6,12 @@ class Gettext < Formula
   mirror 'http://ftp.gnu.org/gnu/gettext/gettext-0.18.3.2.tar.gz'
   sha256 'd1a4e452d60eb407ab0305976529a45c18124bd518d976971ac6dc7aa8b4c5d7'
 
+  bottle do
+    sha1 "b051e525a42aa11242dc80afd19aa914d38b1e4b" => :mavericks
+    sha1 "a1e9a0835d6f2ac2134ac3583e40ac3e4315c5d0" => :mountain_lion
+    sha1 "674f284e9fb6be58df47b788a84eaa5a0c64d195" => :lion
+  end
+
   keg_only "OS X provides the BSD gettext library and some software gets confused if both are in the library path."
 
   option :universal
