5ec0dc650397441005f0a2e1b800fbe9c31a4ee0
Nguyen Anh Quynh <aquynh@gmail.com>
2014-02-04 17:03:34 +0800

    add capstone formula

---
+++ Library/Formula/capstone.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Capstone < Formula
+  homepage 'http://capstone-engine.org'
+  url 'http://capstone-engine.org/download/2.0/capstone-2.0.tgz'
+  sha1 '209cdc69518f754c5d7d07672d8e28cdda9feae7'
+
+  def install
+    system "./make.sh"
+    system "export PREFIX=/usr/local; ./make.sh install"
+  end
+end
