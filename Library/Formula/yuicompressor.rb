5656eede5162eac845e37a8adb5d805f29842a7a
Mike Naberezny <mike@naberezny.com>
2014-02-05 11:53:47 -0800

    yuicompressor: add test
    Closes #26438.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yuicompressor.rb
+++ Library/Formula/yuicompressor.rb
@@ -9,4 +9,16 @@ class Yuicompressor < Formula
     libexec.install "yuicompressor-#{version}.jar"
     bin.write_jar_script libexec/"yuicompressor-#{version}.jar", "yuicompressor"
   end
+
+  test do
+    path = testpath/"test.js"
+    path.write <<-EOS
+      var i = 1;      // foo
+      console.log(i); // bar
+    EOS
+
+    output = `#{bin}/yuicompressor --nomunge --preserve-semi #{path}`.strip
+    assert_equal "var i=1;console.log(i);", output
+    assert_equal 0, $?.exitstatus
+  end
 end
