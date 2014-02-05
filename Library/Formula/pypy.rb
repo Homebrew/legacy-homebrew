b27ff2708076f932782a955c9b5d29fdef5f9983
Mike Goodspeed <mikegoodspeed@gmail.com>
2014-02-04 19:18:19 -0600

    python: setuptools 2.1
    Closes #26418.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pypy.rb
+++ Library/Formula/pypy.rb
@@ -9,8 +9,8 @@ class Pypy < Formula
   depends_on :arch => :x86_64
 
   resource 'setuptools' do
-    url 'https://pypi.python.org/packages/source/s/setuptools/setuptools-2.0.1.tar.gz'
-    sha1 '5283b4dca46d45efd1156713ab51836509646c03'
+    url 'https://pypi.python.org/packages/source/s/setuptools/setuptools-2.1.tar.gz'
+    sha1 '3e4a325d807eb0104e98985e7bd9f1ef86fc2efa'
   end
 
   resource 'pip' do
