1e535de62650bf4f522413ebd448ae3c996a19f2
bismark-foofus <bismark@foofus.net>
2014-02-05 17:12:28 -0600

    Modified formula to include the option of building against Homebrew's OpenSSL.

--- Library/Formula/libssh2.rb
+++ Library/Formula/libssh2.rb
@@ -5,12 +5,22 @@ class Libssh2 < Formula
   url 'http://www.libssh2.org/download/libssh2-1.4.3.tar.gz'
   sha1 'c27ca83e1ffeeac03be98b6eef54448701e044b0'
 
+  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--with-openssl",
-                          "--with-libz"
+    args = [
+      "--prefix=#{prefix}",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--with-libz",
+      "--with-openssl"
+    ]
+
+    if MacOS.version <= :leopard or build.with?('brewed-openssl')
+      args << "--with-libssl-prefix=#{Formula.factory('openssl').opt_prefix}"
+    end
+
+    system "./configure", *args
     system "make install"
   end
 end
