dd64b822a3a61cc621887ae5b7f437c233922535
Ben Chatelain <ben.d.chatelain@kp.org>
2014-02-05 11:54:49 -0700

    burl: 1.0.1 - better curl(1) through augmentation
    Introduce burl formula at v1.0.1. Reawakens #13120

---
+++ Library/Formula/burl.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Burl < Formula
+  homepage "https://github.com/visionmedia/burl"
+  url "https://github.com/visionmedia/burl/archive/1.0.1.zip"
+  sha1 "1c082369f45dd1a9236f7cc078608578e8b06dc1"
+
+  depends_on 'curl' => :optional
+
+  def install
+    bin.install 'bin/burl'
+  end
+
+  test do
+    system "#{bin}/burl", "--help"
+  end
+end
