8fbae7a95d5d9cbbd455673111c2083bd2ae39c5
Alexej Kubarev <alexej.kubarev@accedo.tv>
2014-02-05 22:20:52 +0100

    gradle 1.11-rc-1
    Closes #26446.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -5,6 +5,12 @@ class Gradle < Formula
   url 'http://services.gradle.org/distributions/gradle-1.10-bin.zip'
   sha1 'f911beeff25ebbb7b4af3092dd7f0c93019aef17'
 
+  devel do
+    url 'http://services.gradle.org/distributions/gradle-1.11-rc-1-bin.zip'
+    sha1 'd5c1e564a788faa7fd109a17e0412a86a7b37502'
+    version '1.11-rc-1'
+  end
+
   def install
     libexec.install %w[bin lib]
     bin.install_symlink libexec+'bin/gradle'
