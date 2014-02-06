dc2a55ab0aa36af57dd4a5e49632f1d78c65439b
bismark-foofus <bismark@foofus.net>
2014-02-06 08:30:48 -0600

    Changed method of building against Homebrew's openssl (now default).

--- Library/Formula/libssh2.rb
+++ Library/Formula/libssh2.rb
@@ -5,22 +5,12 @@ class Libssh2 < Formula
   url 'http://www.libssh2.org/download/libssh2-1.4.3.tar.gz'
   sha1 'c27ca83e1ffeeac03be98b6eef54448701e044b0'
 
-  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+  depends_on 'openssl' => :recommended
 
-  def install
-    args = [
-      "--prefix=#{prefix}",
-      "--disable-debug",
-      "--disable-dependency-tracking",
-      "--with-libz",
-      "--with-openssl"
-    ]
-
-    if MacOS.version <= :leopard or build.with?('brewed-openssl')
-      args << "--with-libssl-prefix=#{Formula.factory('openssl').opt_prefix}"
-    end
-
-    system "./configure", *args
-    system "make install"
-  end
+  system "./configure", "--prefix=#{prefix}",
+                        "--disable-debug",
+                        "--disable-dependency-tracking",
+                        "--with-libz",
+                        "--with-openssl"
+  system "make install"
 end
