5408e508c6929c0ce29ec6333330a9171c7dc9fc
Eric Pardee <eric@atlasdigital.tv>
2014-02-05 16:33:55 -0800

    mediainfo 0.7.66
    Closes #26451.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/media-info.rb
+++ Library/Formula/media-info.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class MediaInfo < Formula
   homepage 'http://mediainfo.sourceforge.net'
-  url 'http://mediaarea.net/download/binary/mediainfo/0.7.65/MediaInfo_CLI_0.7.65_GNU_FromSource.tar.bz2'
-  version '0.7.65'
-  sha1 '8b24e118d99cbe5066d96bd3e4f9a23d45fb2cf2'
+  url 'http://mediaarea.net/download/binary/mediainfo/0.7.66/MediaInfo_CLI_0.7.66_GNU_FromSource.tar.bz2'
+  version '0.7.66'
+  sha1 '05c6ba9d7b63be15a4acd441b7c4a0f2f99b0014'
 
   depends_on 'pkg-config' => :build
   # fails to build against Leopard's older libcurl
