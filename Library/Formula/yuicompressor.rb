8b801bad2d7721017ac1984d134b2f62850830f3
asaph <ace@dontletgo.com>
2014-02-06 20:26:53 -0800

    yuicompressor: update homepage
    The previous homepage (http://yuilibrary.com/projects/yuicompressor) redirects to the project's github issues page now.
    http://yui.github.io/yuicompressor/ is the project's current homepage.
    
    Closes #26491.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/yuicompressor.rb
+++ Library/Formula/yuicompressor.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Yuicompressor < Formula
-  homepage 'http://yuilibrary.com/projects/yuicompressor'
+  homepage 'http://yui.github.io/yuicompressor/'
   url 'https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.zip'
   sha1 '178e265570c8161e8074c7ca19896abb05e76c1f'
 
