e91aa2de59d1308a64fc1f2c0ffa969019c4fbe9
malch <malchmih@gmail.com>
2014-02-08 15:18:12 +0400

    datomic 0.9.4532
    Closes #26517.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/datomic.rb
+++ Library/Formula/datomic.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Datomic < Formula
   homepage 'http://www.datomic.com/'
-  url 'https://my.datomic.com/downloads/free/0.9.4470'
-  sha1 '9aadd5c6b19cc012e4028d7f33ae3cadeb2b5a2c'
-  version '0.9.4470'
+  url 'https://my.datomic.com/downloads/free/0.9.4532'
+  sha1 '00656b7cccb582cc0b77eeffd154f27c6a824bea'
+  version '0.9.4532'
 
   def write_libexec_alias *script_names
     script_names.each do |script_name|
