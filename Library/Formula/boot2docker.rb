c395e135f87026b8252b9c1e3b6dfbbfe2b8c86e
Welton Rodrigo <rodrigo@familianascimento.org>
2014-02-09 14:41:19 +0000

    boot2docker 0.5.2 (new formula)
    Closes #26435.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/boot2docker.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Boot2docker < Formula
+  homepage "https://github.com/steeve/boot2docker"
+  url "https://github.com/steeve/boot2docker/archive/v0.5.2.tar.gz"
+  sha1 "487bf0556c97a35fd4af942e2802be0b34c3fd74"
+
+  depends_on "docker"
+
+  def install
+    bin.install "boot2docker"
+  end
+
+  test do
+    system "#{bin}/boot2docker", "info"
+  end
+end
