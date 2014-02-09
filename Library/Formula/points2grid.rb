2b12adeb4b7903bee7fae9c97ccd74471affaaa2
Misty De Meo <mistydemeo@gmail.com>
2014-02-08 18:47:54 -0800

    points2grid 1.2.1

--- Library/Formula/points2grid.rb
+++ Library/Formula/points2grid.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Points2grid < Formula
   homepage 'https://github.com/CRREL/points2grid'
-  url 'https://github.com/CRREL/points2grid/archive/1.1.1.tar.gz'
-  sha1 'f0a7841f1cd804b67bf7ccd15bc5b3bcada975a7'
+  url 'https://github.com/CRREL/points2grid/archive/1.2.1.tar.gz'
+  sha1 'afd1b8ac0086b7ac220a3615e0ec1512161a4628'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
