3455155e2d05346a7230f77f5d8ee5b52911da02
Mike McQuaid <mike@mikemcquaid.com>
2014-02-09 14:32:00 +0000

    docker: add 0.8.0 bottle.

--- Library/Formula/docker.rb
+++ Library/Formula/docker.rb
@@ -5,6 +5,12 @@ class Docker < Formula
   url "https://github.com/dotcloud/docker.git", :tag => "v0.8.0"
   sha1 "1e9362dab2ac2ecb4a1f193a7e72d060000438c3"
 
+  bottle do
+    sha1 "3ff7bceb60e918424e57a07d780d62a61443ae17" => :mavericks
+    sha1 "f27703485378e335e4bde8fa0321087aa94a52a3" => :mountain_lion
+    sha1 "3f054cc6e3f55cf3a14e466d19dde0adc2889dcf" => :lion
+  end
+
   depends_on "go" => :build
 
   def install
