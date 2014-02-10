a62b89d19a026db12c331892de447cdb4cae49e1
Mike McQuaid <mike@mikemcquaid.com>
2014-02-10 18:56:33 +0000

    docker: update packaging based on upstream.

--- Library/Formula/docker.rb
+++ Library/Formula/docker.rb
@@ -11,20 +11,21 @@ class Docker < Formula
     sha1 "3f054cc6e3f55cf3a14e466d19dde0adc2889dcf" => :lion
   end
 
+  def patches
+    "https://github.com/dotcloud/docker/commit/6174ba.patch"
+  end
+
   depends_on "go" => :build
 
   def install
     ENV["DOCKER_GITCOMMIT"] = downloader.cached_location.cd do
       `git rev-parse HEAD`
     end
-    ENV["GOPATH"] = "#{buildpath}/vendor"
-    (buildpath/"vendor/src/github.com/dotcloud").mkpath
-    ln_s buildpath, "vendor/src/github.com/dotcloud/docker"
+    ENV["AUTO_GOPATH"] = "1"
     inreplace "hack/make/dynbinary", "sha1sum", "shasum"
 
     system "hack/make.sh", "dynbinary"
     bin.install "bundles/0.8.0/dynbinary/docker-0.8.0" => "docker"
-    bin.install "bundles/0.8.0/dynbinary/dockerinit-0.8.0" => "dockerinit"
   end
 
   test do
