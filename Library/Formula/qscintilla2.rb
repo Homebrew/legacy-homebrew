a8cbd54118eda07f1579f19694a2c6df17ca1920
David Wales <daviewales@gmail.com>
2014-02-10 22:59:19 +1100

    qscintilla2: Updated formula for Python3, Added QtDesigner Plugin

--- Library/Formula/qscintilla2.rb
+++ Library/Formula/qscintilla2.rb
@@ -5,8 +5,32 @@ class Qscintilla2 < Formula
   url 'http://downloads.sf.net/project/pyqt/QScintilla2/QScintilla-2.8/QScintilla-gpl-2.8.tar.gz'
   sha1 '3edf9d476d4e6af0706a4d33401667a38e3a697e'
 
-  depends_on 'pyqt'
-  depends_on 'sip'
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
+
+  if !Formula.factory("python").installed? && build.with?("python") &&
+     build.with?("python3")
+    odie <<-EOS.undent
+      qscintilla2: You cannot use system Python 2 and Homebrew's Python 3 simultaneously.
+      Either `brew install python` or use `--without-python3`.
+    EOS
+  end
+
+  if build.with? "python3"
+    depends_on "pyqt" => "with-python3"
+  else
+    depends_on "pyqt"
+  end
+
+  def pythons
+    pythons = []
+    ["python", "python3"].each do |python|
+      next if build.without? python
+      version = /\d\.\d/.match `#{python} --version 2>&1`
+      pythons << [python, version]
+    end
+    pythons
+  end
 
   def install
     # On Mavericks we want to target libc++, this requires a unsupported/macx-clang-libc++ flag
@@ -31,15 +55,44 @@ class Qscintilla2 < Formula
     end
 
     cd 'Python' do
-      (share/"sip").mkpath
-      system 'python', 'configure.py', "-o", lib, "-n", include,
-                       "--apidir=#{prefix}/qsci",
-                       "--destdir=#{lib}/python2.7/site-packages/PyQt4",
-                       "--qsci-sipdir=#{share}/sip",
-                       "--pyqt-sipdir=#{HOMEBREW_PREFIX}/share/sip",
-                       "--spec=#{spec}"
-      system 'make'
-      system 'make', 'install'
+      pythons.each do |python, version|
+        (share/"sip").mkpath
+        system python, "configure.py", "-o", lib, "-n", include,
+                         "--apidir=#{prefix}/qsci",
+                         "--destdir=#{lib}/python#{version}/site-packages/PyQt4",
+                         "--qsci-sipdir=#{share}/sip",
+                         "--pyqt-sipdir=#{HOMEBREW_PREFIX}/share/sip",
+                         "--spec=#{spec}"
+        system 'make'
+        system 'make', 'install'
+        system "make", "clean" if pythons.length > 1
+      end
+    end
+
+    # QtDesigner Plugin
+    cd "designer-Qt4Qt5" do
+      ENV.append_path "CPLUS_INCLUDE_PATH", "#{include}"
+      inreplace "designer.pro" do |s|
+        s.gsub! "$$[QT_INSTALL_LIBS]", lib
+        s.gsub! "$$[QT_INSTALL_PLUGINS]", "#{HOMEBREW_PREFIX}/Cellar/qt/4.8.5/plugins"
+      end
+      system "qmake", "designer.pro"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    Pathname("test.py").write <<-EOS.undent
+      import PyQt4.Qsci
+      assert("QsciLexer" in dir(PyQt4.Qsci))
+    EOS
+    pythons.each do |python, version|
+      unless Formula.factory(python).installed?
+        ENV["PYTHONPATH"] = HOMEBREW_PREFIX/"lib/python#{version}/site-packages"
+      end
+      system python, "test.py"
     end
   end
 end
+
