d2017489d95f6f7d4d2d7d4c2d6505b6c865d461
Sebastian Wyder <sebastian.wyder@me.com>
2014-02-10 15:10:15 +0100

    wrk 3.1.0
    Added dependency to OpenSSL because wrk now supports benchmarking SSL/TLS endpoints up to TLSv1.2 and the standard OS X OpenSSL is too old to support TLSv1.2.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>
    
    Closes #26569.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/wrk.rb
+++ Library/Formula/wrk.rb
@@ -2,9 +2,11 @@ require 'formula'
 
 class Wrk < Formula
   homepage 'https://github.com/wg/wrk'
-  url 'https://github.com/wg/wrk/archive/3.0.4.tar.gz'
-  sha1 '55ac8311878f81a6cc9d649da930792e2efb6fe7'
+  url 'https://github.com/wg/wrk/archive/3.1.0.tar.gz'
+  sha1 '3ddc1e591a846b92899a534abf4bc49d2c6cd98e'
   head 'https://github.com/wg/wrk.git'
+
+  depends_on 'openssl'
 
   conflicts_with 'wrk-trello', :because => 'both install `wrk` binaries'
 
@@ -15,6 +17,6 @@ class Wrk < Formula
   end
 
   test do
-    system *%W{#{bin}/wrk -c 1 -t 1 http://www.github.com/}
+    system *%W{#{bin}/wrk -c 1 -t 1 https://github.com/}
   end
 end
