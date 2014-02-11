d28806d085701519200ca27ce369e68c8be1dd0c
Hermiod <nicola.mosco@gmail.com>
2014-02-12 00:20:26 +0100

    libcppa 0.8.1
    Closes #26642.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libcppa.rb
+++ Library/Formula/libcppa.rb
@@ -2,17 +2,14 @@ require 'formula'
 
 class Libcppa < Formula
   homepage 'http://libcppa.blogspot.it'
-  url 'https://github.com/Neverlord/libcppa/archive/V0.7.1.tar.gz'
-  sha1 '0f1f685e94bfa16625370b978ff26deaf799b94e'
+  url 'http://github.com/Neverlord/libcppa/archive/V0.8.1.tar.gz'
+  sha1 'd4f096aae2bb72e254ad6df45edf3fb62370acaa'
 
   depends_on :macos => :lion
   depends_on 'cmake' => :build
 
   option 'with-opencl', 'Build with OpenCL actors'
-
-  def caveats
-      "Libcppa requires a C++11 compliant compiler"
-  end
+  option 'with-examples', 'Build examples'
 
   fails_with :gcc do
     cause 'libcppa requires a C++11 compliant compiler.'
@@ -29,9 +26,8 @@ class Libcppa < Formula
       --disable-context-switching
     ]
 
-    if build.with? 'opencl'
-      args << "--with-opencl"
-    end
+    args << '--with-opencl' if build.with? 'opencl'
+    args << '--no-examples' unless build.with? 'examples'
 
     system "./configure", *args
     system "make"
