77f2c8c29c697735325110a2762ad35373c8e4ff
Mike McQuaid <mike@mikemcquaid.com>
2014-02-11 08:34:02 +0000

    agedu: fix sha1 (again)

--- Library/Formula/agedu.rb
+++ Library/Formula/agedu.rb
@@ -4,7 +4,7 @@ class Agedu < Formula
   homepage 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/'
   url 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-r10126.tar.gz'
   version 'r10126'
-  sha1 'f4d2ea0694ec7813005002fe11a15cc2e26b269f'
+  sha1 '4438ae698b626cba5ce061ed3ae5eefa5bfb1c50'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
