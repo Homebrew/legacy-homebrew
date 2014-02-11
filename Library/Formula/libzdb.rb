44be3b88500050aa0ebccb253c871e946de88596
Brett Koonce <koonce@gmail.com>
2014-02-11 11:44:09 -0600

    libzdb 3.0
    Closes #26630.

--- Library/Formula/libzdb.rb
+++ Library/Formula/libzdb.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libzdb < Formula
   homepage 'http://tildeslash.com/libzdb/'
-  url 'http://tildeslash.com/libzdb/dist/libzdb-2.12.tar.gz'
-  sha1 '4d34be255afddbd025b2f60c5cd11b4389b5c88c'
+  url 'http://tildeslash.com/libzdb/dist/libzdb-3.0.tar.gz'
+  sha1 'bcf14c11cfcd0c05ecc8740f43cd0d6170406dc1'
 
   depends_on :postgresql => :recommended
   depends_on :mysql => :recommended
