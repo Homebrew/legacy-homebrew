6f8f42e07b75e40e1fccca0c8d82edbebcaef350
Alessio Bogon <alessio.bogon@gmail.com>
2014-02-11 16:46:06 +0100

    cputhrottle

---
+++ Library/Formula/cputhrottle.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Cputhrottle < Formula
+  homepage "http://www.willnolan.com/cputhrottle/cputhrottle.html"
+  url "http://www.willnolan.com/cputhrottle/cputhrottle.tar.gz"
+  sha1 "e88c7264cb09399f2e4c54f9d10978ffbb99078b"
+  version "20100515"
+
+  depends_on "boost" => :build
+
+  def install
+    system "make", "all"
+    bin.install('cputhrottle')
+  end
+
+end
