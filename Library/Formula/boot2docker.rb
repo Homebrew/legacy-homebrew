f6cc05287706bc1fb52d4df6aa5ddb3e377840dc
Riobard <me@riobard.com>
2014-02-11 14:00:16 -0330

    boot2docker: make dependency on docker recommended
    Closes #26435.
    Closes #26627.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -5,7 +5,7 @@ class Boot2docker < Formula
   url 'https://github.com/steeve/boot2docker/archive/v0.5.4.tar.gz'
   sha1 '881630491b335230d62a679b8dfbb52c3e249fb8'
 
-  depends_on "docker"
+  depends_on "docker" => :recommended
 
   def install
     bin.install "boot2docker"
