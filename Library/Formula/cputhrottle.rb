7d0bc2c54fd227f896d8b1d80b6ca042a3f692c5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-11 17:06:58 +0000

    cputhrottle: add 20100515 bottle.
    Closes #26623.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cputhrottle.rb
+++ Library/Formula/cputhrottle.rb
@@ -6,6 +6,13 @@ class Cputhrottle < Formula
   sha1 "e88c7264cb09399f2e4c54f9d10978ffbb99078b"
   version "20100515"
 
+  bottle do
+    cellar :any
+    sha1 "3524fce7418022bea4ea10b2c9e9b794a1876201" => :mavericks
+    sha1 "aeb5297fa772631d121d0e480e26e40aa82af5e9" => :mountain_lion
+    sha1 "346f0b9b01e890938afe4571fc810dd8afba5664" => :lion
+  end
+
   depends_on "boost" => :build
 
   def install
