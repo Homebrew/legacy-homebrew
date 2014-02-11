9aa17ce4289fb4f5f6700328bb898ef93bc051b6
Jacob Hayes <jacob.hayes@cerner.com>
2014-02-10 18:49:00 -0600

    [agedu] Update sha1

--- Library/Formula/agedu.rb
+++ Library/Formula/agedu.rb
@@ -4,7 +4,7 @@ class Agedu < Formula
   homepage 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/'
   url 'http://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-r10126.tar.gz'
   version 'r10126'
-  sha1 'a8fc32bd78e79f4c65e8d87140a243dcccda3a8d'
+  sha1 'f4d2ea0694ec7813005002fe11a15cc2e26b269f'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
