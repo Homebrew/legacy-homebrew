93f20f41aeb90d1289b30ad20147e35c79a186d9
Jeffrey Macko <mackoj@users.noreply.github.com>
2014-02-12 18:16:28 +0100

    Update librsvg.rb
    Change mirror link

--- Library/Formula/librsvg.rb
+++ Library/Formula/librsvg.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Librsvg < Formula
   homepage 'https://live.gnome.org/LibRsvg'
-  url 'https://download.gnome.org/sources/librsvg/2.40/librsvg-2.40.1.tar.xz'
+  url 'http://mirror.umd.edu/gnome/sources/librsvg/2.40/librsvg-2.40.1.tar.xz'
   sha256 '8813b4fe776d5e7acbce28bacbaed30ccb0cec3734eb3632c711a16ebe2961d7'
 
   depends_on :x11
