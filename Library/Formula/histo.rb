2bc57d643ccff6fcfadf081ec72f94d3355934cd
Caleb Xu <calebcenter@live.com>
2014-02-12 12:40:34 -0500

    Moved head below url and sha1

--- Library/Formula/histo.rb
+++ Library/Formula/histo.rb
@@ -4,8 +4,8 @@ require "formula"
 class Histo < Formula
   homepage "https://github.com/visionmedia/histo"
   url "https://github.com/visionmedia/histo/archive/0.0.2.tar.gz"
-  head "https://github.com/visionmedia/histo.git"
   sha1 "0eec89b51716cb8a91cf9088590909fdf340662c"
+  head "https://github.com/visionmedia/histo.git"
 
 
   def install
