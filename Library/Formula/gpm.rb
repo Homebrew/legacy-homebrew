a93e0c2fc56b341f9a5091542ab60448dfabb650
elCuervo <yo@brunoaguirre.com>
2014-02-13 10:41:18 -0200

    Adds Go Package Manager

---
+++ Library/Formula/gpm.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Gpm < Formula
+  homepage "https://github.com/pote/gpm"
+  url "https://github.com/pote/gpm/archive/v1.0.0.tar.gz"
+  sha1 "1bceb0af64c821fab01fe9606362d936343d6950"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "gpm version"
+  end
+end
