c5636a177ca6f262b9bb7533aec875be8b83ad33
Adam Vandenberg <flangy@gmail.com>
2014-02-13 19:58:05 -0800

    crm: use gnu89
    Fixes #26707.

--- Library/Formula/crm114.rb
+++ Library/Formula/crm114.rb
@@ -8,6 +8,7 @@ class Crm114 < Formula
   depends_on 'tre'
 
   def install
+    ENV.append 'CFLAGS', '-std=gnu89'
     inreplace 'Makefile', 'LDFLAGS += -static -static-libgcc', ''
     bin.mkpath
     system "make", "prefix=#{prefix}", "install"
