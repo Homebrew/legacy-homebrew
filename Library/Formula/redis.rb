d0a569de51f567f7894ef063fb75361d98a0aba0
Joep Loijens <joep.loijens@gmail.com>
2014-02-14 11:36:28 +0100

    redis 2.8.6
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/redis.rb
+++ Library/Formula/redis.rb
@@ -2,14 +2,8 @@ require 'formula'
 
 class Redis < Formula
   homepage 'http://redis.io/'
-  url 'http://download.redis.io/releases/redis-2.8.5.tar.gz'
-  sha1 'f0eb48609ff66ead3c7f06bbe8a8dd1aa7341b73'
-
-  bottle do
-    sha1 "3670032929123972994cdc29374628311e5fb874" => :mavericks
-    sha1 "9240b55f06576409d8f9e9f6f4d00d67a6861da0" => :mountain_lion
-    sha1 "1ee0860611776df9e9d361df6a2d1ba6d5111cbb" => :lion
-  end
+  url 'http://download.redis.io/releases/redis-2.8.6.tar.gz'
+  sha1 '8680046580c75987961241f2e1e417c242b91a49'
 
   head 'https://github.com/antirez/redis.git', :branch => 'unstable'
 
