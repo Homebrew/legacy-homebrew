277d42f510a605cc6c15e0f972855b3482a432a0
Severen Redwood <severen.redwood@gmail.com>
2014-02-15 01:01:59 +1300

    Added a caveat.
    Added a caveat that notifies the user about a possible error if "export GANT_HOME="/usr/local/opt/gant/libexec" is not entered into their .bashrc/.zshrc

--- Library/Formula/gant.rb
+++ Library/Formula/gant.rb
@@ -14,4 +14,11 @@ class Gant < Formula
     libexec.install %w[bin lib conf]
     bin.install_symlink "#{libexec}/bin/gant"
   end
+
+  def caveats; <<-EOS.undent
+    NOTE: You will recieve errors upon trying to use Gant unless you
+    enter Gant's home directory into your .zshrc/.bashrc
+    To do this paste in: export GANT_HOME="/usr/local/opt/gant/libexec"
+    EOS
+  end
 end
