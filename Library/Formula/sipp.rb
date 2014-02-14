3d684f440c5e3696316dcfce3d3b7f60ca9e2d96
John Dyer <jdyer@tropo.com>
2014-02-14 09:24:32 -0500

    Include pcap support by default, and conform to style guide

--- Library/Formula/sipp.rb
+++ Library/Formula/sipp.rb
@@ -5,17 +5,11 @@ class Sipp < Formula
   url 'http://downloads.sourceforge.net/project/sipp/sipp/3.4/sipp-3.3.990.tar.gz'
   sha1 'b2637cb72556595253bbdd4a68cc974c9ac1d92e'
 
-  option 'pcap',     'Build with PCAP support'
-  option 'openssl',  'Build with OpenSSL support'
-
-  depends_on 'openssl'   if build.include? 'openssl'
+  depends_on 'openssl' => :optional
 
   def install
-    args = []
-
-    args << "--with-pcap" if build.include? 'pcap'
+    args = ["--with-pcap"]
     args << "--with-openssl" if build.include? 'openssl'
-
     system "./configure", *args
     system "make", "DESTDIR=#{prefix}"
     bin.install "sipp"
