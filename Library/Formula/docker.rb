c2be399073e9c7f364ee31282dea600551b07b9d
Mike McQuaid <mike@mikemcquaid.com>
2014-02-14 12:05:27 +0000

    docker: replace inreplace with upstream patch.

--- Library/Formula/docker.rb
+++ Library/Formula/docker.rb
@@ -18,6 +18,7 @@ class Docker < Formula
     [
       "https://github.com/dotcloud/docker/commit/6174ba.patch",
       "https://github.com/dotcloud/docker/commit/f794fb.patch",
+      "https://github.com/dotcloud/docker/commit/32535e.patch",
     ]
   end
 
@@ -26,7 +27,6 @@ class Docker < Formula
   def install
     ENV["GIT_DIR"] = downloader.cached_location/".git"
     ENV["AUTO_GOPATH"] = "1"
-    inreplace "hack/make/dynbinary", "sha1sum", "shasum"
 
     system "hack/make.sh", "dynbinary"
     bin.install "bundles/0.8.0/dynbinary/docker-0.8.0" => "docker"
