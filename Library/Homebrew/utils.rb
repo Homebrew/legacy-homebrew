76a195dff6b966d361b40007f5df7c168e37d705
Jack Nagel <jacknagel@gmail.com>
2014-02-13 23:14:30 -0500

    Use a more accurate method name and drop unhelpful block

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -322,7 +322,7 @@ module GitHub extend self
     issues_matching(name, :state => "open")
   end
 
-  def find_pull_requests query
+  def print_pull_requests_matching(query)
     return if ENV['HOMEBREW_NO_GITHUB_API']
     puts "Searching pull requests..."
 
@@ -339,7 +339,7 @@ module GitHub extend self
       return
     end
 
-    prs.each { |i| yield "#{i["title"]} (#{i["html_url"]})" }
+    prs.each { |i| puts "#{i["title"]} (#{i["html_url"]})" }
   end
 
   def private_repo?(user, repo)
