cf388a9c7053adbafad0db3c3417130461733c41
Brett Koonce <koonce@gmail.com>
2014-02-15 11:25:07 -0600

    ngspice 26

--- Library/Formula/ngspice.rb
+++ Library/Formula/ngspice.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ngspice < Formula
   homepage 'http://ngspice.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/ngspice/ng-spice-rework/25/ngspice-25.tar.gz'
-  sha1 '745c3c32385b7d5c808836e393fe7699f8568860'
+  url 'http://downloads.sourceforge.net/project/ngspice/ng-spice-rework/26/ngspice-26.tar.gz'
+  sha1 '7c043c604b61f76ad1941defeeac6331efc48ad2'
 
   option "with-x", "Build with X support"
   option "without-xspice", "Build without x-spice extensions"
