d59b8c365c74fe78b5d4f0861f82f8218ef52b80
Brett Koonce <koonce@gmail.com>
2014-02-15 11:24:39 -0600

    glm 0.9.5.2

--- Library/Formula/glm.rb
+++ Library/Formula/glm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Glm < Formula
   homepage 'http://glm.g-truc.net/'
-  url 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.5.1/glm-0.9.5.1.zip'
-  sha1 'e1165e2c1be99e3cfeacbecaa15a2c89f2caf503'
+  url 'http://downloads.sourceforge.net/project/ogl-math/glm-0.9.5.2/glm-0.9.5.2.zip'
+  sha1 '647736ff89bde9543ce20c1a4497674f9fbf6ab9'
   head 'https://github.com/Groovounet/glm.git'
 
   def install
