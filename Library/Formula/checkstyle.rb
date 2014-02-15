493931beb275a22fa674b24e62efd54e62d2a247
Mike Naberezny <mike@naberezny.com>
2014-02-15 11:15:34 -0800

    checkstyle: add test
    Closes #26751.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/checkstyle.rb
+++ Library/Formula/checkstyle.rb
@@ -9,4 +9,14 @@ class Checkstyle < Formula
     libexec.install "checkstyle-#{version}-all.jar", "sun_checks.xml"
     bin.write_jar_script libexec/"checkstyle-#{version}-all.jar", "checkstyle"
   end
+
+  test do
+    path = testpath/"foo.java"
+    path.write "public class Foo{ }\n"
+
+    output = `#{bin}/checkstyle -c #{libexec}/sun_checks.xml -r #{path}`
+    errors = output.split("\n").select { |line| line.start_with?(path) }
+    assert errors.include?("#{path}:1:17: '{' is not preceded with whitespace.")
+    assert_equal errors.size, $?.exitstatus
+  end
 end
