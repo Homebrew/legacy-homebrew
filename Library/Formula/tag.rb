e32645be7451ba47f0973f56e5d25615f9d7f0ed
Brett Koonce <koonce@gmail.com>
2014-02-15 12:09:22 -0600

    tag v0.7.5
    Closes #26747.

--- Library/Formula/tag.rb
+++ Library/Formula/tag.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tag < Formula
   homepage 'https://github.com/jdberry/tag/'
-  url 'https://github.com/jdberry/tag/archive/v0.7.3.tar.gz'
-  sha1 'aaab77f51eeacde2fdd75a7c56403c0d620b7970'
+  url 'https://github.com/jdberry/tag/archive/v0.7.5.tar.gz'
+  sha1 'e1075a1068f6a4cb377144e162ee47c063a4a0d6'
   head 'https://github.com/jdberry/tag.git'
 
   depends_on :macos => :mavericks
