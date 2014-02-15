5c156367ed23dd18fd1a7db85f74c85192aaa778
Brett Koonce <koonce@gmail.com>
2014-02-15 12:08:06 -0600

    n 1.2.1

--- Library/Formula/n.rb
+++ Library/Formula/n.rb
@@ -3,8 +3,8 @@ require 'formula'
 class N < Formula
   homepage 'https://github.com/visionmedia/n'
   head 'https://github.com/visionmedia/n.git'
-  url 'https://github.com/visionmedia/n/archive/1.1.0.tar.gz'
-  sha1 '93c7661630310ca38627052be4df47f89285b099'
+  url 'https://github.com/visionmedia/n/archive/1.2.1.tar.gz'
+  sha1 'c8a54313ee8ff43f3b6cdb03f140bc200f548f9f'
 
   def install
     bin.mkdir
