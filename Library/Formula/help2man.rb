bc67c907291145071dd1bf493cb0fffab885c2ec
Brett Koonce <koonce@gmail.com>
2014-02-15 10:14:08 -0600

    help2man 1.44.1
    Closes #26743.

--- Library/Formula/help2man.rb
+++ Library/Formula/help2man.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Help2man < Formula
   homepage 'http://www.gnu.org/software/help2man/'
-  url 'http://ftpmirror.gnu.org/help2man/help2man-1.43.3.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/help2man/help2man-1.43.3.tar.gz'
-  sha256 '67978d118980ebd9f0c60be5db129527900a7b997b9568fc795ba9bdb341d303'
+  url 'http://ftpmirror.gnu.org/help2man/help2man-1.44.1.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/help2man/help2man-1.44.1.tar.xz'
+  sha256 '22de6846771921f455e389cdca07119d7f55b1877685b42dd5bbb9fc1377defb'
 
   def install
     # install is not parallel safe
