22bddba683874696c2f7de607a2b948b0766f305
Brett Koonce <koonce@gmail.com>
2014-02-15 21:06:12 -0600

    antlr 4.2

--- Library/Formula/antlr.rb
+++ Library/Formula/antlr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Antlr < Formula
   homepage 'http://www.antlr.org/'
-  url 'http://www.antlr.org/download/antlr-4.1-complete.jar'
-  sha1 '2f80d904ab786d0616560085d30d402e90b9880a'
+  url 'http://www.antlr.org/download/antlr-4.2-complete.jar'
+  sha1 '2ca46f6c3fee3cde543ec21a800805e9432acab7'
 
   def install
     prefix.install "antlr-#{version}-complete.jar"
