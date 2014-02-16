03547a59064947025531e010fceee5d5a91e024f
Brett Koonce <koonce@gmail.com>
2014-02-15 21:07:03 -0600

    dovecot 2.2.12

--- Library/Formula/dovecot.rb
+++ Library/Formula/dovecot.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Dovecot < Formula
   homepage 'http://dovecot.org/'
-  url 'http://dovecot.org/releases/2.2/dovecot-2.2.10.tar.gz'
-  mirror 'http://fossies.org/linux/misc/dovecot-2.2.10.tar.gz'
-  sha256 '75592483d40dc4f76cc3b41af40caa4be80478946a699d46846d5d03e4d2e09b'
+  url 'http://dovecot.org/releases/2.2/dovecot-2.2.12.tar.gz'
+  mirror 'http://fossies.org/linux/misc/dovecot-2.2.12.tar.gz'
+  sha256 'f3de1cae5665ff202713c1489bc96b4d029760808bf3311d76c173d7f3e38c56'
 
   depends_on 'clucene' => :optional
 
