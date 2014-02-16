837a96f7a10b2306cded3a1fbc418c64cf8b5808
謝致邦 <Yeking@Red54.com>
2014-02-17 04:16:44 +0800

    Add version parameter.

--- Library/Formula/rbenv-bundler.rb
+++ Library/Formula/rbenv-bundler.rb
@@ -3,6 +3,7 @@ require 'formula'
 class RbenvBundler < Formula
   homepage 'https://github.com/carsomyr/rbenv-bundler'
   url 'https://github.com/carsomyr/rbenv-bundler/archive/0aebf227aaf45caf5dd49afbfb5d8970c1506e64.tar.gz'
+  version '0.96.1'
   sha1 '7ecd57ec0ac461da0cbacc0518703be0c058a880'
   head 'https://github.com/carsomyr/rbenv-bundler.git'
 
