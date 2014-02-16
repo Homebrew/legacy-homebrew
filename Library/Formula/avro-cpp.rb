22d7bc617c6f45f56f7f865e73f1014893437c0c
Brett Koonce <koonce@gmail.com>
2014-02-15 21:06:43 -0600

    avro-cpp 1.7.6

--- Library/Formula/avro-cpp.rb
+++ Library/Formula/avro-cpp.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class AvroCpp < Formula
   homepage 'http://avro.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=avro/avro-1.7.5/cpp/avro-cpp-1.7.5.tar.gz'
-  sha1 '8017abbbeb25e092afbffda5acf70df639662190'
+  url 'http://www.apache.org/dyn/closer.cgi?path=avro/avro-1.7.6/cpp/avro-cpp-1.7.6.tar.gz'
+  sha1 '6ef420797e14998d0889b1d6b34cd8d93480a332'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
