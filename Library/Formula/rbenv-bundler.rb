62c23ffa05889fe24fb505af91186d7d59932f31
謝致邦 <Yeking@Red54.com>
2014-02-17 04:38:50 +0800

    Use patch instead of new commit.

--- Library/Formula/rbenv-bundler.rb
+++ Library/Formula/rbenv-bundler.rb
@@ -2,12 +2,15 @@ require 'formula'
 
 class RbenvBundler < Formula
   homepage 'https://github.com/carsomyr/rbenv-bundler'
-  url 'https://github.com/carsomyr/rbenv-bundler/archive/0aebf227aaf45caf5dd49afbfb5d8970c1506e64.tar.gz'
-  version '0.96.1'
-  sha1 '7ecd57ec0ac461da0cbacc0518703be0c058a880'
+  url 'https://github.com/carsomyr/rbenv-bundler/archive/0.96.tar.gz'
+  sha1 '02339c604c840f24b66523c9faad6ed82500eda1'
   head 'https://github.com/carsomyr/rbenv-bundler.git'
 
   depends_on 'rbenv'
+
+  def patches
+    'https://github.com/carsomyr/rbenv-bundler/commit/0aebf2.patch'
+  end
 
   def install
     prefix.install Dir['*']
