f7f8b52400e634fbe690091b8fcbe906070ec7ec
Adam Stankiewicz <sheerun@sher.pl>
2014-02-18 00:49:28 +0100

    Multitail 6.2.1

--- Library/Formula/multitail.rb
+++ Library/Formula/multitail.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Multitail < Formula
   homepage 'http://www.vanheusden.com/multitail/download.html'
-  url 'http://www.vanheusden.com/multitail/multitail-6.0.tgz'
-  sha1 'fed0d3c5f03ff022f8f2ae90c0027312c3ff70ce'
+  url 'http://www.vanheusden.com/multitail/multitail-6.2.1.tgz'
+  sha1 '30bd77c4f542cd6ad6f1a0351f7d9e0a6cef6585'
 
   def install
     ENV['DESTDIR'] = prefix
