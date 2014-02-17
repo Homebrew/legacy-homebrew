01bb68597e40bbb102982b715af7b22f9be8f382
Adam Vandenberg <flangy@gmail.com>
2014-02-16 17:08:02 -0800

    libuvc 0.0.3

--- Library/Formula/libuvc.rb
+++ Library/Formula/libuvc.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Libuvc < Formula
   homepage 'https://github.com/ktossell/libuvc'
-  url 'https://github.com/ktossell/libuvc/archive/v0.0.2.tar.gz'
-  sha1 'e0d57007715eaf36c5ebeb8d348488984c681041'
+  url 'https://github.com/ktossell/libuvc/archive/v0.0.3.tar.gz'
+  sha1 'bd6868772e6e4b9c52431d7731c09df013b6cf43'
 
-  depends_on 'libusb'
   depends_on 'cmake' => :build
+  depends_on 'libusb'
 
   def install
     system "cmake", ".", *std_cmake_args
