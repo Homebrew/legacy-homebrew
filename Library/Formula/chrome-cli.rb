791019657aa8b766a65216b3ee4d6b24a8935aa4
x0l <x0l@users.noreply.github.com>
2014-02-17 23:22:33 +0100

    Create chrome-cli.rb
    Control Google Chrome from the command line.

---
+++ Library/Formula/chrome-cli.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class ChromeCli < Formula
+  homepage "https://github.com/prasmussen/chrome-cli"
+  url "https://github.com/prasmussen/chrome-cli/archive/1.4.0.zip"
+  sha1 "96b03d2e60d5998a8a549af95412253ad7337e7f"
+
+  head "https://github.com/prasmussen/chrome-cli.git"
+  
+  depends_on :xcode
+
+  def install
+    system 'xcodebuild', 'SDKROOT=', 'SYMROOT=build'
+    bin.install 'build/Release/chrome-cli'
+  end
+end
