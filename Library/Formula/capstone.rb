b2fb138d3615f5093c966ec6928c12164c93c51d
Nguyen Anh Quynh <aquynh@gmail.com>
2014-02-19 07:43:20 +0800

    capstone: install to proper lib folder
    Closes #26826.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/capstone.rb
+++ Library/Formula/capstone.rb
@@ -6,6 +6,9 @@ class Capstone < Formula
   sha1 '209cdc69518f754c5d7d07672d8e28cdda9feae7'
 
   def install
+    # Fixed upstream in next version:
+    # https://github.com/aquynh/capstone/commit/dc0d04522fa6ca7222124f390f51eb9e106906f7.diff
+    inreplace 'Makefile', 'lib64', 'lib'
     system "./make.sh"
     ENV["PREFIX"] = prefix
     system "./make.sh", "install"
