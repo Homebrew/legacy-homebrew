eb04f0005b850964b80df6b40c629e815f0f6d77
Jack Nagel <jacknagel@gmail.com>
2014-02-18 17:49:02 -0500

    libxmp: move conditional into method

--- Library/Formula/libxmp.rb
+++ Library/Formula/libxmp.rb
@@ -13,13 +13,15 @@ class Libxmp < Formula
   # Both of these patches have been applied upstream and should be in
   # the next release.
   def patches
-    [
-      # fixes dylib versioning
-      'https://github.com/cmatsuoka/libxmp/commit/c6301dc68e1c1f6c247667df56fda754412cfb48.patch',
-      # fixes replay time of S3M modules
-      'https://github.com/cmatsuoka/libxmp/commit/3368bb9a4583d75e1844fad5caa964045d007179.patch'
-    ]
-  end unless build.head?
+    unless build.head?
+      [
+        # fixes dylib versioning
+        'https://github.com/cmatsuoka/libxmp/commit/c6301dc68e1c1f6c247667df56fda754412cfb48.patch',
+        # fixes replay time of S3M modules
+        'https://github.com/cmatsuoka/libxmp/commit/3368bb9a4583d75e1844fad5caa964045d007179.patch'
+      ]
+    end
+  end
 
   def install
     system "autoconf" if build.head?
