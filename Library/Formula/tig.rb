3551f09f838d2a7deeb9c0ce463e69107217cdb0
Adam Lau <skyros@gmail.com>
2014-02-18 18:55:13 +0800

    tig: patch for better multibyte character support
    Closes #26806.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -13,6 +13,15 @@ class Tig < Formula
     depends_on "xmlto"
   end
 
+  # fixes the problem with displaying/searching multibyte characters
+  # upstream pull request: https://github.com/jonas/tig/issues/99
+  def patches
+  [
+    "https://github.com/spin6lock/tig/commit/159eff692b24aa05f766f76922b0f2515fbbf415.diff",
+    "https://github.com/spin6lock/tig/commit/70cb91e7d210b92ab012d9819bd20ebe92885bd6.diff"
+  ]
+  end
+
   def install
     system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
