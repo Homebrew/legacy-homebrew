0273ba59c18d1a648753fd1333208309eb826520
Jack Nagel <jacknagel@gmail.com>
2014-02-18 16:03:44 -0500

    Use standard curl progress output in verbose mode
    Closes #26818.

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -136,7 +136,10 @@ def curl *args
   curl = Pathname.new '/usr/bin/curl'
   raise "#{curl} is not executable" unless curl.exist? and curl.executable?
 
-  args = [HOMEBREW_CURL_ARGS, HOMEBREW_USER_AGENT, *args]
+  flags = HOMEBREW_CURL_ARGS
+  flags = flags.delete("#") if ARGV.verbose?
+
+  args = [flags, HOMEBREW_USER_AGENT, *args]
   # See https://github.com/Homebrew/homebrew/issues/6103
   args << "--insecure" if MacOS.version < "10.6"
   args << "--verbose" if ENV['HOMEBREW_CURL_VERBOSE']
