0dc0488869f1a8a11b5032ee5a6de3e1a1ebf9d4
Danny Casady <dpcasady@gmail.com>
2014-02-18 12:09:36 -0600

    grails 2.3.6
    Closes #26817.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Grails < Formula
   homepage 'http://grails.org'
-  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.5.zip'
-  sha1 '32a7acbabb22d6d78898f109a3b265ba83589a1b'
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.6.zip'
+  sha1 '9f09b73e7f357ce8bf7938590da0a91cbd849ca1'
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
