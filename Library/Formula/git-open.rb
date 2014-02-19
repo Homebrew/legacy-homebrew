7ee3d7ac13c54051a5caea76d4a83868be9fcd2b
Michael J. Cohen <mjc@kernel.org>
2014-02-19 16:15:59 -0500

    git-open: 1.1 - a commandline utility that allows you to specify a repo name and optionally the author and will then open the matching resource in your web-browser.

---
+++ Library/Formula/git-open.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class GitOpen < Formula
+  homepage "https://github.com/jeffreyiacono/git-open"
+  url "https://github.com/jeffreyiacono/git-open/archive/v1.1.tar.gz"
+  sha1 "86a2a9b67e5fe20779b2ae7ad59a3450c7c72d23"
+
+
+  def install
+    FileUtils.cp("git-open.sh", "git-open")
+    bin.install "git-open"
+  end
+end
