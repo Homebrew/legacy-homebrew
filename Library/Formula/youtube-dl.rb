6c321e7377d76cf89c6b89e595fb5a8679a59b07
Felix Böhm <me@feedic.com>
2014-02-19 13:16:16 +0100

    youtube-dl: add optional rtmpdump dependency
    I just wanted to download a video which required rtmpdump. It would have been nice if homebrew had told me about it in advance.
    
    Closes #26835.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -5,6 +5,8 @@ class YoutubeDl < Formula
   url 'https://yt-dl.org/downloads/2014.02.19.1/youtube-dl-2014.02.19.1.tar.gz'
   sha1 '6d02e9b07ffee6b898349bc2532d0ab9e653f6d4'
 
+  depends_on 'rtmpdump' => :optional
+
   def install
     system "make", "youtube-dl", "PREFIX=#{prefix}"
     bin.install 'youtube-dl'
