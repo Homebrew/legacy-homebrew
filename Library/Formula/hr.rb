4b6cff331201be8e84520ab2f3e18c02c3a82705
Aleksandar Simic <asimic@gmail.com>
2014-02-19 21:32:12 +0000

    hr formula update to 1.1
    Updated hr formula to the latest (1.1) version
    
    Closes #26856.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/hr.rb
+++ Library/Formula/hr.rb
@@ -2,13 +2,14 @@ require 'formula'
 
 class Hr < Formula
   homepage 'https://github.com/LuRsT/hr'
-  url 'https://github.com/LuRsT/hr/archive/1.0.tar.gz'
-  sha1 '64abbc75d4e6cedd8ea4e15396e473298fc5240b'
+  url 'https://github.com/LuRsT/hr/archive/1.1.tar.gz'
+  sha1 '72e0a7836fe5181205de816bb5e0d44be3a8961f'
 
   head 'https://github.com/LuRsT/hr', :branch => 'master'
 
   def install
     bin.install 'hr'
+    man1.install 'hr.1'
   end
 
   test do
