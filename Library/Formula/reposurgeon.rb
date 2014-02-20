8cfb701fc85c96ccf8e5d8f8f5d11979fdef4ff2
Matthew L Daniel <mdaniel@gmail.com>
2014-02-19 20:08:19 -0800

    reposurgeon 3.6
    http://freecode.com/projects/reposurgeon#release_361692
    
    Closes #26854.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/reposurgeon.rb
+++ Library/Formula/reposurgeon.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Reposurgeon < Formula
   homepage 'http://www.catb.org/esr/reposurgeon/'
-  url 'http://www.catb.org/~esr/reposurgeon/reposurgeon-3.4.tar.gz'
-  sha256 '1e7ef243963af6b72924e2064f51bb3d5003bb8c2ae036f905ba5cadab3a461e'
+  url 'http://www.catb.org/~esr/reposurgeon/reposurgeon-3.6.tar.gz'
+  sha256 'c07354319813095f5db6c416fd491eb8efd4d7c67db7161c88fd2c3afe0d5386'
 
   depends_on 'asciidoc'
   depends_on 'xmlto'
