fd358045c4c178581ff5485d163c2dd375863afe
Mark Cornick <mark@cornick.io>
2014-02-20 13:52:13 -0500

    pyenv-pip-rehash 0.0.3

---
+++ Library/Formula/pyenv-pip-rehash.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class PyenvPipRehash < Formula
+  homepage 'https://github.com/yyuu/pyenv-pip-rehash'
+  url 'https://github.com/yyuu/pyenv-pip-rehash/archive/v0.0.3.tar.gz'
+  sha1 'c0d354c7886aed142a46dfd8cf427a7000f40896'
+
+  head 'https://github.com/yyuu/pyenv-pip-rehash.git'
+
+  depends_on 'pyenv'
+
+  def install
+    ENV['PREFIX'] = prefix
+    system "./install.sh"
+  end
+end
