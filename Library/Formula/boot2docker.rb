5c0d76df764e445b0b3df046f09846fc284668a9
Markus Hubig <mhubig@gmail.com>
2014-02-20 12:01:22 +0100

    boot2docker: updates url, add HEAD
    The boot2docker repo has moved from steeve/boot2docker to
    boot2docker/boot2docker. It also adds a HEAD option.
    
    Closes #26857.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -1,9 +1,11 @@
 require "formula"
 
 class Boot2docker < Formula
-  homepage "https://github.com/steeve/boot2docker"
-  url 'https://github.com/steeve/boot2docker/archive/v0.5.4.tar.gz'
+  homepage "https://github.com/boot2docker/boot2docker"
+  url 'https://github.com/boot2docker/boot2docker/archive/v0.5.4.tar.gz'
   sha1 '881630491b335230d62a679b8dfbb52c3e249fb8'
+
+  head "https://github.com/boot2docker/boot2docker.git"
 
   depends_on "docker" => :recommended
 
