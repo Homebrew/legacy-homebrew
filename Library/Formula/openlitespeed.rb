e2735d07a4d6acf2ee7c570897b7b656be971566
denji <denji0k@gmail.com>
2014-02-21 18:09:28 +0200

    fix build.with? and using make install

--- Library/Formula/openlitespeed.rb
+++ Library/Formula/openlitespeed.rb
@@ -18,11 +18,10 @@ class Openlitespeed < Formula
 
   def install
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
-    args << "--enable-debug" if build.include? 'with-debug'
-    args << "--enable-spdy" if build.include? 'with-spdy'
-    args << "--with-openssl=#{Formula.factory('openssl').opt_prefix}" if build.include? 'with-spdy'
+    args << "--enable-debug" if build.with? 'debug'
+    args << "--enable-spdy" if build.with? 'spdy'
+    args << "--with-openssl=#{Formula.factory('openssl').opt_prefix}" if build.with? 'spdy'
     system "./configure", *args
-    system "make"
-    system "make install"
+    system "make", "install"
   end
 end
