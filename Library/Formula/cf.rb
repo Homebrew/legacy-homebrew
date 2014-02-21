ab885b9542297a2a1a0dbdf2eb3fcb238288aa4f
Karen Wang and Tim Jarratt <khwang@pivotallabs.com>
2014-02-20 17:28:26 -0800

    add a formula for cloudfoundry cli

---
+++ Library/Formula/cf.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Cf < Formula
+  homepage 'https://github.com/cloudfoundry/cli'
+  url 'https://github.com/cloudfoundry/cli.git', :tag => 'v6.0.0'
+  version '6.0.0-beta'
+
+  head 'https://github.com/cloudfoundry/cli.git', :branch => 'master'
+
+  depends_on 'go' => :build
+
+  def install
+    system 'bin/build'
+    bin.install 'out/cf'
+  end
+
+  test do
+    system "#{bin}/cf"
+  end
+end
