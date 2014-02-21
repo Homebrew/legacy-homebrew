c9d59a2c21e112d4a480ef2cae113f0026b2f4e5
x0l <x0l@github.com>
2014-02-21 18:48:28 +0100

    Version bump 1.4.0 -> 1.4.1 Added test Requires OSX >= Mountain Lion

--- Library/Formula/chrome-cli.rb
+++ Library/Formula/chrome-cli.rb
@@ -2,15 +2,20 @@ require "formula"
 
 class ChromeCli < Formula
   homepage "https://github.com/prasmussen/chrome-cli"
-  url "https://github.com/prasmussen/chrome-cli/archive/1.4.0.zip"
-  sha1 "96b03d2e60d5998a8a549af95412253ad7337e7f"
+  url "https://github.com/prasmussen/chrome-cli/archive/1.4.1.zip"
+  sha1 "9f44fd5def7f3662b83b90669d95f863722c06eb"
 
   head "https://github.com/prasmussen/chrome-cli.git"
 
   depends_on :xcode
+  depends_on :macos => :mountain_lion # strange error with lion
 
   def install
     system 'xcodebuild', 'SDKROOT=', 'SYMROOT=build'
     bin.install 'build/Release/chrome-cli'
+  end
+
+  test do
+    system "#{bin}/chrome-cli", 'version'
   end
 end
