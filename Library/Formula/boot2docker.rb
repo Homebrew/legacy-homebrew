75801c05baa68c63839513b6a430f4dc7c1cb645
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2014-02-21 22:33:08 +0100

    boot2docker 0.6.0
    Closes #26899.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -2,18 +2,13 @@ require "formula"
 
 class Boot2docker < Formula
   homepage "https://github.com/boot2docker/boot2docker"
-  url 'https://github.com/boot2docker/boot2docker/archive/v0.5.4.tar.gz'
-  sha1 '881630491b335230d62a679b8dfbb52c3e249fb8'
-
   head "https://github.com/boot2docker/boot2docker.git"
+  url 'https://github.com/boot2docker/boot2docker/archive/v0.6.0.tar.gz'
+  sha1 'e90ed901375a345e23685aa8e0f9e3b83db11eff'
 
   depends_on "docker" => :recommended
 
   def install
     bin.install "boot2docker"
-  end
-
-  test do
-    system "#{bin}/boot2docker", "info"
   end
 end
