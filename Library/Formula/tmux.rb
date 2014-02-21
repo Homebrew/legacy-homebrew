2b0e92805b445988f7c1ba9d0fd09b16d2afc087
adnissen <andrew_nissen@yahoo.com>
2014-02-21 08:15:32 -0500

    tmux1.9

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Tmux < Formula
   homepage 'http://tmux.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.8/tmux-1.8.tar.gz'
-  sha1 '08677ea914e1973ce605b0008919717184cbd033'
+  url 'http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.9/tmux-1.9.tar.gz'
+  sha1 '43197e69716a0430a9e856c13df8ceae31783078'
 
   head do
     url 'git://git.code.sf.net/p/tmux/tmux-code'
