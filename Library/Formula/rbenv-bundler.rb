b14d96c15d3c1dcf81649953afe01ec3b0fb6b99
謝致邦 <Yeking@Red54.com>
2014-02-21 12:30:44 +0800

    rbenv-bundler 0.9.7
    Closes #26878.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/rbenv-bundler.rb
+++ Library/Formula/rbenv-bundler.rb
@@ -2,16 +2,11 @@ require 'formula'
 
 class RbenvBundler < Formula
   homepage 'https://github.com/carsomyr/rbenv-bundler'
-  url 'https://github.com/carsomyr/rbenv-bundler/archive/0.96.tar.gz'
-  sha1 '02339c604c840f24b66523c9faad6ed82500eda1'
+  url 'https://github.com/carsomyr/rbenv-bundler/archive/0.97.tar.gz'
+  sha1 'a31e0893465566db8e5c11c555b2ba0d314a869b'
   head 'https://github.com/carsomyr/rbenv-bundler.git'
 
   depends_on 'rbenv'
-
-  def patches
-    # Pending new tag: https://github.com/carsomyr/rbenv-bundler/issues/38
-    "https://github.com/carsomyr/rbenv-bundler/commit/0aebf2.patch"
-  end
 
   def install
     prefix.install Dir['*']
