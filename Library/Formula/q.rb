fa1e0bceb7fec9484add4bf2235283bfe8c00b08
Harel Ben-Attia <harelba@gmail.com>
2014-02-21 11:12:49 -0500

    q 1.1.7
    Closes #26890.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/q.rb
+++ Library/Formula/q.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Q < Formula
   homepage 'https://github.com/harelba/q'
-  url 'https://github.com/harelba/q/archive/1.1.5.tar.gz'
-  sha1 'f64cb6797c73950b28c404389a3efe2d086d081d'
+  url 'https://github.com/harelba/q/archive/1.1.7.tar.gz'
+  sha1 '088a4f267167481ae0c0825ad410c897aeaab9e8'
 
   def install
     bin.install 'q'
