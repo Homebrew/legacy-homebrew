14fc50e4f2f1fa504ebd622726c15958379c3475
Stephen Levine and Tim Jarratt <tjarratt@pivotallabs.com>
2014-02-21 11:19:41 -0800

    Formula style improvements - inreplace + removed 'beta'

--- Library/Formula/cf.rb
+++ Library/Formula/cf.rb
@@ -3,14 +3,14 @@ require 'formula'
 class Cf < Formula
   homepage 'https://github.com/cloudfoundry/cli'
   url 'https://github.com/cloudfoundry/cli.git', :tag => 'v6.0.0'
-  version '6.0.0-beta'
+  version '6.0.0'
 
   head 'https://github.com/cloudfoundry/cli.git', :branch => 'master'
 
   depends_on 'go' => :build
 
   def install
-    system 'sed -i "" -e "s/SHA/homebrew/g" src/cf/app_constants.go'
+    inreplace 'src/cf/app_constants.go', 'SHA', 'homebrew'
     system 'bin/build'
     bin.install 'out/cf'
     doc.install 'LICENSE'
