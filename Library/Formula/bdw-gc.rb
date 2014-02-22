e35c08880833052f97f532ae13ca24b1efa45ba4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-22 19:34:39 +0000

    bdw-gc: add 7.2e bottle.

--- Library/Formula/bdw-gc.rb
+++ Library/Formula/bdw-gc.rb
@@ -5,6 +5,13 @@ class BdwGc < Formula
   url 'http://www.hpl.hp.com/personal/Hans_Boehm/gc/gc_source/gc-7.2e.tar.gz'
   sha1 '3ad593c6d0ed9c0951c21a657b86c55dab6365c8'
 
+  bottle do
+    cellar :any
+    sha1 "267d3e346e5d8a9fff781ee58a7aa3b33e31b3db" => :mavericks
+    sha1 "a92904c2e10891252bb212eb4a438094a36de8c8" => :mountain_lion
+    sha1 "ecc4b3bded24d5a48255c4c4d5295680d5befcf2" => :lion
+  end
+
   option :universal
 
   def install
