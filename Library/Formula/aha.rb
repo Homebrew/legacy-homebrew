e72c27b3ebbc1d6fe0826f28ca01078c40c903ae
Martin Mauch <mauch@crealytics.de>
2014-02-22 19:13:26 +0100

    Add aha ASCII to coloured HTML converter.

---
+++ Library/Formula/aha.rb
@@ -0,0 +1,25 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+
+class Aha < Formula
+  homepage "https://github.com/theZiz/aha"
+  url "https://github.com/theZiz/aha/archive/0.4.7.tar.gz"
+  sha1 "bbf861ea1a98aa72346c2822f1ec40941b84dd53"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "make"
+    bin.install "aha"
+  end
+
+  test do
+    IO.popen("#{bin}/aha", "w+") do |pipe|
+      pipe.write("[35mrain[34mpill[00m")
+      pipe.close_write
+      assert_match /color:purple;">rain.*color:blue;">pill/, pipe.read
+    end
+  end
+end
