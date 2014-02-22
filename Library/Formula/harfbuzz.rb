bd79022f657fab7b794efd3ab11835f70530fcb2
Mike McQuaid <mike@mikemcquaid.com>
2014-02-22 18:38:59 +0000

    harfbuzz: add bottle.

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -5,6 +5,13 @@ class Harfbuzz < Formula
   url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.26.tar.bz2'
   sha256 '5ded7dc3d15fb5c9230d7a224b9256b67b35fde30193b4c3a4c3f6b13110e257'
 
+  bottle do
+    cellar :any
+    sha1 "f070bb22af0d70c2f6d23ed192814b3358f45ac6" => :mavericks
+    sha1 "17ade866edc8b1231f88cfee067f588c17f013db" => :mountain_lion
+    sha1 "1a0cd3cde6b86554a04d9f6fef0d79d0459ff30b" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'cairo'
