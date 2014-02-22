222674a64172246e46e9e327e521f2d90bbdf0a5
Mike McQuaid <mike@mikemcquaid.com>
2014-02-22 22:43:06 +0000

    jemalloc: add bottle.

--- Library/Formula/jemalloc.rb
+++ Library/Formula/jemalloc.rb
@@ -5,6 +5,12 @@ class Jemalloc < Formula
   url 'http://www.canonware.com/download/jemalloc/jemalloc-3.5.0.tar.bz2'
   sha1 '3c6aeed5adbd7267ec7db476f002051143a43ac0'
 
+  bottle do
+    sha1 "3eae83e595a6140685008255b03549f7e5f73c7d" => :mavericks
+    sha1 "00a2f95fe96f2dc85d6d1470e8685ca4c8a822d8" => :mountain_lion
+    sha1 "73958a90f38b8bb5fd83d339106f4ef80d73337d" => :lion
+  end
+
   def install
     system './configure', '--disable-debug', "--prefix=#{prefix}"
     system 'make install'
