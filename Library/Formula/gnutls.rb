148c4446883510d80c53b0dc21b645167e96448b
Mike McQuaid <mike@mikemcquaid.com>
2014-02-22 15:39:17 +0000

    gnutls: add bottle.

--- Library/Formula/gnutls.rb
+++ Library/Formula/gnutls.rb
@@ -6,6 +6,13 @@ class Gnutls < Formula
   mirror 'http://mirrors.dotsrc.org/gcrypt/gnutls/v3.1/gnutls-3.1.18.tar.xz'
   sha1 '360cdb86c1bb6494c27901b5d4c8815b37d5bd4c'
 
+  bottle do
+    cellar :any
+    sha1 "38e1e08bc111bc626e22ca401668efd6a5a42d06" => :mavericks
+    sha1 "bd8a8499a0459ad51b517398535f52fd8c08e411" => :mountain_lion
+    sha1 "9780bf542bba844f4603a9f1ddc0a934cafe0a4d" => :lion
+  end
+
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'libtasn1'
