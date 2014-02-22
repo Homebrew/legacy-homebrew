6ced7e7c6b7aa7b702fe2efe23d8feebaed3aab9
Mike McQuaid <mike@mikemcquaid.com>
2014-02-22 17:01:38 +0000

    brew-test-bot: force dep fetches on cleanup.

--- Library/Contributions/cmd/brew-test-bot.rb
+++ Library/Contributions/cmd/brew-test-bot.rb
@@ -274,7 +274,9 @@ class Test
       test "brew install apple-gcc42"
     end
 
-    test "brew fetch #{dependencies}" unless dependencies.empty?
+    deps_fetch_options = " "
+    deps_fetch_options << " --force" if ARGV.include? '--cleanup'
+    test "brew fetch#{deps_fetch_options} #{dependencies}" unless dependencies.empty?
     formula_fetch_options = " "
     formula_fetch_options << " --build-bottle" unless ARGV.include? '--no-bottle'
     formula_fetch_options << " --force" if ARGV.include? '--cleanup'
