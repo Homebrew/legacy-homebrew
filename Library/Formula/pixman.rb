8365ad59cdbd18e93cb3d83c8c1c21a7ac917769
Mike McQuaid <mike@mikemcquaid.com>
2014-02-22 15:18:15 +0000

    pixman: add bottle.

--- Library/Formula/pixman.rb
+++ Library/Formula/pixman.rb
@@ -5,6 +5,13 @@ class Pixman < Formula
   url 'http://cairographics.org/releases/pixman-0.32.4.tar.gz'
   sha256 '80c7ed420e8a3ae749800241e6347c3d55681296cab71384be7969cd9e657e84'
 
+  bottle do
+    cellar :any
+    sha1 "d4a8df1a31807a916a7d9b457a3835190494f9b1" => :mavericks
+    sha1 "d0b49f5a7a456a8491ec0d4a59113ec89b99bc4e" => :mountain_lion
+    sha1 "fd40b113d86707cab39c3e078c8eddf7db968560" => :lion
+  end
+
   depends_on 'pkg-config' => :build
 
   keg_only :provided_pre_mountain_lion
