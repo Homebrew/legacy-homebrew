21212c9bea68810cbc70f89a26a7f58a22ccee5b
Mike McQuaid <mike@mikemcquaid.com>
2014-02-22 18:39:06 +0000

    pango: add bottle.

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -5,6 +5,12 @@ class Pango < Formula
   url 'http://ftp.gnome.org/pub/GNOME/sources/pango/1.36/pango-1.36.2.tar.xz'
   sha256 'f07f9392c9cf20daf5c17a210b2c3f3823d517e1917b72f20bb19353b2bc2c63'
 
+  bottle do
+    sha1 "58eb78b432c1a6e99d3ef2c6171118c8cce3d569" => :mavericks
+    sha1 "0887ad9879ef06cbcebffb0298957d281cc4b83a" => :mountain_lion
+    sha1 "8cd38f8356eb11db8863555891be34d422f5f7dd" => :lion
+  end
+
   option 'without-x', 'Build without X11 support'
 
   depends_on 'pkg-config' => :build
