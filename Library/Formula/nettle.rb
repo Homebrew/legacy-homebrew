9b2a302f2664020beb14c93ed72226a675ac886c
Mike McQuaid <mike@mikemcquaid.com>
2014-02-22 15:39:08 +0000

    nettle: add bottle.

--- Library/Formula/nettle.rb
+++ Library/Formula/nettle.rb
@@ -5,6 +5,13 @@ class Nettle < Formula
   url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.6.tar.gz'
   sha1 '401f982a0b365e04c8c38c4da42afdd7d2d51d80'
 
+  bottle do
+    cellar :any
+    sha1 "7e05202e46a27cd917080b8cabcfa2f97abebab3" => :mavericks
+    sha1 "da54d410d113c5bbce67d021b0a65cd52ee0cd36" => :mountain_lion
+    sha1 "ae174af411ee2ecc8269ce32f4c64e45871f1f42" => :lion
+  end
+
   depends_on 'gmp'
 
   def install
