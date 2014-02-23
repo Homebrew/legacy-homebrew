20625b11c89d2e86abaa644148d1c8cef322183d
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2014-02-24 01:36:03 +0200

    bottle

--- Library/Formula/grenchman.rb
+++ Library/Formula/grenchman.rb
@@ -5,6 +5,11 @@ class Grenchman < Formula
   url "https://grenchman.s3.amazonaws.com/downloads/grench-0.2.0-mac"
   sha1 "a1468c2e31deeeaf9925c6a3d1ebf9b3aac96521"
 
+  bottle do
+    cellar :any
+    sha1 "a9e9a5fc87da881ee9c004a3468d1eb7fba69156" => :lion
+  end
+
   def install
     bin.install 'grench-0.2.0-mac' => 'grench'
   end
