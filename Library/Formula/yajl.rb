a2dac61930ed0dfaf63def7c7a5c5bd05d3f315f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-23 12:31:08 +0000

    yajl: add 2.0.4 bottle.

--- Library/Formula/yajl.rb
+++ Library/Formula/yajl.rb
@@ -5,6 +5,12 @@ class Yajl < Formula
   url 'https://github.com/lloyd/yajl/archive/2.0.4.tar.gz'
   sha256 '0e78f516dc53ecce7dc073f9a9bb0343186b58ef29dcd1dad74e5e853b216dd5'
 
+  bottle do
+    sha1 "a02d27cc6b98912176bd636ee30aaca464a44ae8" => :mavericks
+    sha1 "1d8cec340ef78502d9e726a96c416c20ee7e85e1" => :mountain_lion
+    sha1 "1e7bbbf4f37b9a251c430b2438f3449cc62d5903" => :lion
+  end
+
   # Configure uses cmake internally
   depends_on 'cmake' => :build
 
