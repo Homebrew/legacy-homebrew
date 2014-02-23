84ec31fd19ed177c0d9e8149b0046c5f17b00e06
David Holm <dholmster@gmail.com>
2014-02-23 17:20:36 +0100

    Install bindings along with radare2

--- Library/Formula/radare2.rb
+++ Library/Formula/radare2.rb
@@ -7,14 +7,43 @@ class Radare2 < Formula
 
   head 'http://radare.org/hg/radare2', :using => :hg
 
-  depends_on 'libewf'
-  depends_on 'libmagic'
-  depends_on 'gmp'
-  depends_on 'lua'
+  depends_on "pkg-config" => :build
+  depends_on "valabind" => :build
+  depends_on "swig" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "libewf"
+  depends_on "libmagic"
+  depends_on "gmp"
+  depends_on "lua"
+
+  def patches
+    # Fixes file name of dynamic libraries so that version goes before .dylib.
+    #  * radare2 pull request #693
+    "https://github.com/radare/radare2/commit/f0dbeb9950c55cdb75a2515b1cf6add4e0f4a591.diff"
+  end
+
+  resource("bindings") do
+    url "http://radare.org/get/radare2-bindings-0.9.7.tar.xz"
+    sha1 "b425e3faeebd3f212e6542a64dafa3b629535e7a"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+
+    resource("bindings").stage do
+      ENV.append_path "PKG_CONFIG_PATH", "#{lib}/pkgconfig"
+
+      # OSX build fix from pull request #18.
+      #  * https://github.com/radare/radare2-bindings/pull/18
+      inreplace "python-config-wrapper" do |s|
+        s.gsub! "\s", "\ "
+      end
+
+      system "./configure", "--prefix=#{prefix}"
+      system "make"
+      system "make", "install", "DESTDIR=#{prefix}"
+    end
   end
 end
