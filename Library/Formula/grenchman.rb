9297bedbd4f81ce5c95ad372e3b2c182d8df7125
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2014-02-24 00:44:27 +0200

    grenchman 0.2 (new formula)

---
+++ Library/Formula/grenchman.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class Grenchman < Formula
+  homepage "http://leiningen.org/grench.html"
+  url "https://grenchman.s3.amazonaws.com/downloads/grench-0.2.0-mac"
+  sha1 "a1468c2e31deeeaf9925c6a3d1ebf9b3aac96521"
+
+  def install
+    File.rename('grench-0.2.0-mac', 'grench') 
+    bin.install('grench')
+  end
+
+end
