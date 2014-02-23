c495626c80dcdaf58a8337c0af9a44facc6d3245
Tim Clem <tim.clem@gmail.com>
2014-02-22 19:12:39 -0500

    yafc: Fix build on 10.9, and fix the bash-completion script.
    Closes #24688

--- Library/Formula/yafc.rb
+++ Library/Formula/yafc.rb
@@ -2,20 +2,47 @@ require 'formula'
 
 class Yafc < Formula
   homepage 'http://www.yafc-ftp.com/'
-  url 'http://www.yafc-ftp.com/upload/yafc-1.2.5.tar.xz'
-  sha1 'b8156689f92a5d9b684180394f1763500a25c1a5'
+  url 'http://www.yafc-ftp.com/upload/yafc-1.3.1.tar.xz'
+  sha1 '63f958446c9a88fed2413e95cc1e8058570f77df'
 
   depends_on 'xz' => :build
   depends_on 'readline'
+
+  def cflags
+    cflags = ENV.cflags.to_s
+    cflags += ' -DHAVE_STRLCPY'
+    cflags
+  end
+
+  def patches
+    DATA
+  end
 
   def install
     readline = Formula["readline"].opt_prefix
     system "./configure", "--prefix=#{prefix}",
                           "--with-readline=#{readline}"
-    system "make", "install"
+    system "make", "install", "CFLAGS=#{cflags}"
   end
 
   test do
     system "#{bin}/yafc", "-V"
   end
 end
+
+__END__
+# The bash-completion script assumes GNU sed and its 'r' option for
+# extended regular expressions. OSX's sed calls this 'E' (they're not
+# entirely compatible, but enough for this use case).
+diff --git a/completion/yafc b/completion/yafc
+index d4f2189..4694d44 100644
+--- a/completion/yafc
++++ b/completion/yafc
+@@ -9,7 +9,7 @@ _yafc()
+     cur=`_get_cword`
+ 
+     if [ $COMP_CWORD -eq 1 ] && [ -f ~/.yafc/bookmarks ]; then
+-        COMPREPLY=( $( compgen -W '$( sed -nre "/machine/ s/.* alias '\''([^'\'']*)'\''/\1/ p" \
++        COMPREPLY=( $( compgen -W '$( sed -nEe "/machine/ s/.* alias '\''([^'\'']*)'\''/\1/ p" \
+             ~/.yafc/bookmarks )' -- "$cur" ) )
+     fi
