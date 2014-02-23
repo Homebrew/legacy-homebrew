09d0a9ca0496525c8c1ebca1fe47ac7830508c1f
Adam Vandenberg <flangy@gmail.com>
2014-02-23 13:15:42 -0800

    latest xcode is 5.0.2
    Closes #26943.

--- Library/Homebrew/os/mac/xcode.rb
+++ Library/Homebrew/os/mac/xcode.rb
@@ -26,12 +26,12 @@ module OS
         when "10.5"         then "3.1.4"
         when "10.6"         then "3.2.6"
         when "10.7"         then "4.6.3"
-        when "10.8"         then "5.0.1"
-        when "10.9"         then "5.0.1"
+        when "10.8"         then "5.0.2"
+        when "10.9"         then "5.0.2"
         else
           # Default to newest known version of Xcode for unreleased OSX versions.
           if MacOS.version > "10.9"
-            "5.0.1"
+            "5.0.2"
           else
             raise "Mac OS X '#{MacOS.version}' is invalid"
           end
