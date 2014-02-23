86660670ebf24617f68b20ea0b096b304cf7bab3
Francisco Souza <f@souza.cc>
2014-02-23 20:27:01 -0300

    concurrencykit 0.3.5
    Closes #26949.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Concurrencykit < Formula
   homepage 'http://concurrencykit.org'
-  url 'http://concurrencykit.org/releases/ck-0.3.4.tar.gz'
-  sha1 'bf32af29ca174f068ed8772aed1647ab3d474264'
+  url 'http://concurrencykit.org/releases/ck-0.3.5.tar.gz'
+  sha1 '4186bd44603e4aec8d63e46d1b8151fdc0ba46ef'
 
   head 'git://git.concurrencykit.org/ck.git'
 
