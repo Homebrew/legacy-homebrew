4c5cedac9aea8110d03f1056ee9e68e2e6ad2523
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-24 11:21:45 +0000

    tmux: add 1.9a bottle.
    Closes #26963.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/tmux.rb
+++ Library/Formula/tmux.rb
@@ -5,6 +5,13 @@ class Tmux < Formula
   url 'http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.9/tmux-1.9a.tar.gz'
   sha1 '815264268e63c6c85fe8784e06a840883fcfc6a2'
 
+  bottle do
+    cellar :any
+    sha1 "258df085ed5fd3ff4374337294641bd057b81ff4" => :mavericks
+    sha1 "3838e790a791d44464df6e7fcd25d8558d864d9c" => :mountain_lion
+    sha1 "4368a7f81267c047050758338eb8f4207da12224" => :lion
+  end
+
   head do
     url 'git://git.code.sf.net/p/tmux/tmux-code'
 
