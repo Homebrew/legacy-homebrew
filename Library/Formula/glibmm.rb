61064c704a20f4c249affa4810ddb7f76997d6a7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-24 15:49:34 +0000

    glibmm: add 2.38.1 bottle.

--- Library/Formula/glibmm.rb
+++ Library/Formula/glibmm.rb
@@ -5,6 +5,12 @@ class Glibmm < Formula
   url 'http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.38/glibmm-2.38.1.tar.xz'
   sha256 '49c925ee1d3c4d0d6cd7492d7173bd6826db51d0b55f458a6496406ae267c4a2'
 
+  bottle do
+    sha1 "b405d3bd887ab8268eafb7d01a7fcb32977a3f97" => :mavericks
+    sha1 "e1b2dc92316aa3c162149987e15bc34806c05248" => :mountain_lion
+    sha1 "a6155e561ab340ba24553ee953f74f917e7fca2c" => :lion
+  end
+
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'libsigc++'
