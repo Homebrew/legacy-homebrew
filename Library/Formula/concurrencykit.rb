b5ba47bdae9caf567f0702f403d5e4ded5a22092
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-24 11:11:34 +0000

    concurrencykit: add 0.4 bottle.
    Closes #26962.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -7,6 +7,13 @@ class Concurrencykit < Formula
 
   head 'git://git.concurrencykit.org/ck.git'
 
+  bottle do
+    cellar :any
+    sha1 "21698717ea6dde053a62253a1ccb2a19c987281a" => :mavericks
+    sha1 "6d952c9718603d3a8794a9884aec020d397c42b3" => :mountain_lion
+    sha1 "3760bbd8cd36fde9952f340cf860800aa2014ddf" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "CC=#{ENV.cc}"
