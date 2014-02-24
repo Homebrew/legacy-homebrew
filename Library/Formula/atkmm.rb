11b2578a11f457146676ef63e723191eb4d37513
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-24 15:49:34 +0000

    atkmm: add 2.22.7 bottle.

--- Library/Formula/atkmm.rb
+++ Library/Formula/atkmm.rb
@@ -5,6 +5,12 @@ class Atkmm < Formula
   url 'http://ftp.acc.umu.se/pub/GNOME/sources/atkmm/2.22/atkmm-2.22.7.tar.xz'
   sha256 'bfbf846b409b4c5eb3a52fa32a13d86936021969406b3dcafd4dd05abd70f91b'
 
+  bottle do
+    sha1 "6c47047d111b4b950e1cb15425365b13b11f6a1b" => :mavericks
+    sha1 "2262b36b562f9f2ea1c024923e738a08145e97df" => :mountain_lion
+    sha1 "eb053cdcba6fc56e0bddde08ac356a18d9bf53b0" => :lion
+  end
+
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'atk'
