021a3a1d058eda88a33f8333aed7bd81c53ad495
Adam Vandenberg <flangy@gmail.com>
2014-02-24 21:26:29 -0800

    ettercap: use Formula[]

--- Library/Formula/ettercap.rb
+++ Library/Formula/ettercap.rb
@@ -19,7 +19,7 @@ class Ettercap < Formula
   end
 
   def install
-    libnet = Formula.factory 'libnet'
+    libnet = Formula['libnet']
 
     args = ['..'] + std_cmake_args + [
       "-DINSTALL_SYSCONFDIR=#{etc}",
