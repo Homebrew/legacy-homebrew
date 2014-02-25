eb985f6ecd8ffc26217149c8862a6ef69e446fae
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-25 18:09:26 +0000

    libmemcached: add 1.0.18 bottle.
    Closes #26998.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libmemcached.rb
+++ Library/Formula/libmemcached.rb
@@ -5,6 +5,13 @@ class Libmemcached < Formula
   url "https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz"
   sha1 "8be06b5b95adbc0a7cb0f232e237b648caf783e1"
 
+  bottle do
+    cellar :any
+    sha1 "abc6b08082ef18c8abb0df901063e88ee21fa82e" => :mavericks
+    sha1 "a4da2082b38e15cd86dffd3f3b3c5b691dee1f90" => :mountain_lion
+    sha1 "5029f4a7f05dd5f727b17617e49b424940c3def0" => :lion
+  end
+
   option "with-sasl", "Build with sasl support"
 
   if build.with? "sasl"
