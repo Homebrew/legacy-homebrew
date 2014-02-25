34b5171e268cc83f382e57342d3ea7f42c068ac9
brad oyler <bradoyler@gmail.com>
2014-02-25 15:20:40 -0500

    add Askwiki formula
    My 1st formula

---
+++ Library/Formula/askwiki.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Askwiki < Formula
+  homepage 'https://github.com/bradoyler/askwiki'
+  url 'https://github.com/bradoyler/askwiki/archive/0.0.1.tar.gz'
+  sha1 '133d18657de36464d0e9eca6cc6289629b6928c1'
+
+  def install
+    bin.install "askwiki"
+    man1.install "man/askwiki.1"
+  end
+
+  test do
+    system "#{bin}/askwiki", "--version"
+  end
+end
