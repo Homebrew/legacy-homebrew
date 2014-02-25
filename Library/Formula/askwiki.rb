6788cdc9df4fe0d371ef7bc1f4d60f4deff59008
brad oyler <bradoyler@gmail.com>
2014-02-25 15:37:18 -0500

    trailing whitespace

--- Library/Formula/askwiki.rb
+++ Library/Formula/askwiki.rb
@@ -4,8 +4,6 @@ class Askwiki < Formula
   homepage 'https://github.com/bradoyler/askwiki'
   url 'https://github.com/bradoyler/askwiki/archive/0.0.1.tar.gz'
   sha1 '133d18657de36464d0e9eca6cc6289629b6928c1'
-  
-  depends_on :x11
 
   def install
     bin.install "askwiki"
