4b021d39ae6eac0968af08fcf8267b8a049ed715
Stefan <humdedum@icloud.com>
2014-02-25 22:36:41 +0100

    stormfs 0.03
    Closes #27009.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/stormfs.rb
+++ Library/Formula/stormfs.rb
@@ -2,33 +2,20 @@ require 'formula'
 
 class Stormfs < Formula
   homepage 'https://github.com/benlemasurier/stormfs'
-  url 'https://github.com/downloads/benlemasurier/stormfs/stormfs-0.01.tar.gz'
-  sha1 'd86bb74beb4b4343b63b3eda3e6bd6f4db982bbb'
+  url 'https://github.com/benlemasurier/stormfs/archive/v0.03.tar.gz'
+  sha1 'bae7aeb409b910dd3844cd930f3ad9427d7e1609'
 
   depends_on 'pkg-config' => :build
+  depends_on :autoconf
+  depends_on :automake
   depends_on 'glib'
   depends_on 'fuse4x'
   depends_on 'curl' if MacOS.version <= :leopard
 
-  def patches; DATA; end
-
   def install
+    system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
   end
 end
-
-__END__
-diff --git a/src/curl.c b/src/curl.c
-index de43059..0b02053 100644
---- a/src/curl.c
-+++ b/src/curl.c
-@@ -23,7 +23,6 @@
- #include <sys/stat.h>
- #include <unistd.h>
- #include <curl/curl.h>
--#include <curl/types.h>
- #include <curl/easy.h>
- #include <pthread.h>
- #include <glib.h>
