405f23759690113060aea7d158d2aadc211a87a4
Adam Vandenberg <flangy@gmail.com>
2014-02-25 07:23:20 -0800

    ledger: use Formula[]

--- Library/Formula/ledger.rb
+++ Library/Formula/ledger.rb
@@ -42,7 +42,7 @@ class Ledger < Formula
       if build.with? 'libofx'
         args << "--enable-ofx"
         # the libofx.h appears to have moved to a subdirectory
-        ENV.append 'CXXFLAGS', "-I#{Formula.factory('libofx').opt_prefix}/include/libofx"
+        ENV.append 'CXXFLAGS', "-I#{Formula["libofx"].opt_prefix}/include/libofx"
       end
       system "./configure", "--disable-debug", "--disable-dependency-tracking",
                             "--prefix=#{prefix}", *args
