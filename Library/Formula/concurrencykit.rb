e0b89fd318bea56a4ecee24dc1b2bd39803f3ea2
Francisco Souza <f@souza.cc>
2014-02-25 20:15:40 -0300

    concurrencykit 0.4.1
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -2,17 +2,10 @@ require 'formula'
 
 class Concurrencykit < Formula
   homepage 'http://concurrencykit.org'
-  url 'http://concurrencykit.org/releases/ck-0.4.tar.gz'
-  sha1 'de7a408528eb780fc5587a2d6b589d6e7af657d1'
+  url 'http://concurrencykit.org/releases/ck-0.4.1.tar.gz'
+  sha1 '53be7f3cc42bf46f409926a8add911bf49f37c20'
 
   head 'git://git.concurrencykit.org/ck.git'
-
-  bottle do
-    cellar :any
-    sha1 "21698717ea6dde053a62253a1ccb2a19c987281a" => :mavericks
-    sha1 "6d952c9718603d3a8794a9884aec020d397c42b3" => :mountain_lion
-    sha1 "3760bbd8cd36fde9952f340cf860800aa2014ddf" => :lion
-  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
