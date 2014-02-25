d32f7ca400a3e768557a716b5fc11def03b0de08
brad oyler <bradoyler@gmail.com>
2014-02-25 15:45:37 -0500

    update sha1

--- Library/Formula/askwiki.rb
+++ Library/Formula/askwiki.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Askwiki < Formula
   homepage 'https://github.com/bradoyler/askwiki'
   url 'https://github.com/bradoyler/askwiki/archive/0.0.1.tar.gz'
-  sha1 '133d18657de36464d0e9eca6cc6289629b6928c1'
+  sha1 '473aa171d98307aa8f1e459f29bc04968bab2206'
 
   def install
     bin.install "askwiki"
