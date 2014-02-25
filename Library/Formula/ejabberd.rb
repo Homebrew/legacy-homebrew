ad9f407daf63141c13cac64a736b0d1f455cddf7
Adam Vandenberg <flangy@gmail.com>
2014-02-24 21:23:24 -0800

    ejabberd: use Formula[]

--- Library/Formula/ejabberd.rb
+++ Library/Formula/ejabberd.rb
@@ -29,7 +29,7 @@ class Ejabberd < Formula
               "--localstatedir=#{var}"]
 
       if MacOS.version <= :leopard
-        openssl = Formula.factory('openssl')
+        openssl = Formula['openssl']
         args << "--with-openssl=#{openssl.prefix}"
       end
 
