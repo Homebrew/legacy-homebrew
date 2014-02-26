2d5025fe56e0560a71fd16233323d79d5ffdb3fd
faraazkhan <faraaz@rationalizeit.us>
2014-02-26 11:04:15 -0500

    update sha

--- Library/Formula/git-subdir.rb
+++ Library/Formula/git-subdir.rb
@@ -3,7 +3,7 @@ require "formula"
 class GitSubdir < Formula
   homepage "https://github.com/andreyvit/git-subdir"
   url "https://github.com/andreyvit/git-subdir/archive/v0.1.tar.gz"
-  sha1 "ac4128b74e8b6a6916d634e7313ee25d80afd579"
+  sha1 "849f55a310f76c523b911c02532700ecb3bca181"
 
   depends_on 'git'
   def install
