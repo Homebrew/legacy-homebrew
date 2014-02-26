e1c8e09b0606a40803332d3897815a851eee5c7f
Faraaz Khan <faraaz@rationalizeit.us>
2014-02-25 22:52:29 -0500

    adding URL to v0.1 of git subdir tarball

--- Library/Formula/git-subdir.rb
+++ Library/Formula/git-subdir.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class GitSubdir < Formula
   homepage "https://github.com/andreyvit/git-subdir"
-  url "git@github.com:andreyvit/git-subdir.git", :using => :git
+  url "https://github.com/andreyvit/git-subdir/archive/v0.1.tar.gz"
   sha1 "ac4128b74e8b6a6916d634e7313ee25d80afd579"
 
   depends_on 'git'
