461ce932c907adc11e05685cd99eaca5fc9591a5
Gabriel Bretschner <software@kanedo.net>
2014-02-26 19:25:01 +0100

    vassh 0.2 Update Now with more substantial test

--- Library/Formula/vassh.rb
+++ Library/Formula/vassh.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Vassh < Formula
   homepage "https://github.com/x-team/vassh"
-  url "https://github.com/x-team/vassh/archive/0.1.tar.gz"
-  sha1 "74e6954e030e0e578390dd16d98a5c7dcaf09208"
+  url "https://github.com/x-team/vassh/archive/0.2.tar.gz"
+  sha1 "b7be90ab0a20f3edebe45dab2f7a048684871068"
 
   def install
     bin.install "vassh.sh", "vasshin", "vassh"
   end
 
   test do
-    system "which vassh"
+    system "vassh -h"
   end
 end
