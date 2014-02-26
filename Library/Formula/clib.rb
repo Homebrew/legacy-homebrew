279d87b65ed9266080a18e448c9e1c95d82b1786
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-26 14:00:28 +0100

    clib prefix installation fixed, test added

--- Library/Formula/clib.rb
+++ Library/Formula/clib.rb
@@ -6,10 +6,12 @@ class Clib < Formula
   sha1 "d840b4259190e1b3ce6cc0970f3e9f659226b9d3"
 
   def install
+    ENV['PREFIX'] = prefix
     system "make", "install"
   end
 
   test do
-    system "clib", "-v"
+    system (bin/"clib"), "install", "-o", "foo-bar", "stephenmathieson/rot13.c"
+    File.directory?("foo-bar")
   end
 end
