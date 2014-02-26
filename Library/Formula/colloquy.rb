755a03cd266cfa9960d2cc3f6c41cb677b122aaa
grecocd <cassiodgreco@gmail.com>
2014-02-26 10:37:28 -0500

    Colloquy: 2.4.2 - IRC Client

--- Library/Formula/colloquy.rb
+++ Library/Formula/colloquy.rb
@@ -1,15 +1,15 @@
 require "formula"
 
 class Colloquy < Formula
-    #version 2.4.2
+  #version 2.4.2
   homepage "http://colloquy.info"
   url "http://colloquy.info/downloads/colloquy-latest.zip"
   sha1 "19892476af32f83685a37af16676b0098ad3bacc"
+  
   depends_on :x11
-
+  
   def install
-     
     system "make", "install"
+    prefix.install "/Applications/Colloquy.app"
   end
 end
-
