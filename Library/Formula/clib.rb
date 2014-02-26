441c46d77ba226c1ccbc2a3a06ed08eeebc2c158
BrewTestBot <brew-test-bot@googlegroups.com>
2014-02-26 13:20:32 +0000

    clib: add 1.0.0 bottle.

--- Library/Formula/clib.rb
+++ Library/Formula/clib.rb
@@ -5,6 +5,13 @@ class Clib < Formula
   url "https://github.com/clibs/clib/archive/1.0.0.tar.gz"
   sha1 "d840b4259190e1b3ce6cc0970f3e9f659226b9d3"
 
+  bottle do
+    cellar :any
+    sha1 "b533a51036f0013496bf8ca8a5039d0a705436f9" => :mavericks
+    sha1 "26d64bc906d6487f1009917f19deb7ad859ccb36" => :mountain_lion
+    sha1 "c963615301797d63ca65e927432173a5fbb33d24" => :lion
+  end
+
   def install
     ENV["PREFIX"] = prefix
     system "make", "install"
