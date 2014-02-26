6b4a86fed531412af141b2928cbfb11dba9e7003
Gabriel Bretschner <software@kanedo.net>
2014-02-26 09:39:59 +0100

    vassh removed trailing whitespace

--- Library/Formula/vassh.rb
+++ Library/Formula/vassh.rb
@@ -4,7 +4,7 @@ class Vassh < Formula
   homepage "https://github.com/x-team/vassh"
   url "https://github.com/x-team/vassh/archive/0.1.tar.gz"
   sha1 "74e6954e030e0e578390dd16d98a5c7dcaf09208"
-  
+
   def install
     bin.install "./vassh.sh"
     bin.install "./vasshin"
