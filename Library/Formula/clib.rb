31a86deab980f6253e539e02727b5f0baa342263
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-26 11:50:11 +0100

    clib v1.0.0

---
+++ Library/Formula/clib.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Clib < Formula
+  homepage "https://github.com/clibs/clib"
+  url "https://github.com/clibs/clib/archive/1.0.0.tar.gz"
+  sha1 "d840b4259190e1b3ce6cc0970f3e9f659226b9d3"
+
+  def install
+    system "make", "install"
+  end
+
+  test do
+    system "clib", "-v"
+  end
+end
