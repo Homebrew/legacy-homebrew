ee4089c6794b9577a39d2257e43bba58bdd0ce9d
Adam Vandenberg <flangy@gmail.com>
2014-02-25 19:48:51 -0800

    subversion 1.8.8

--- Library/Formula/subversion.rb
+++ Library/Formula/subversion.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Subversion < Formula
   homepage 'http://subversion.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=subversion/subversion-1.8.5.tar.bz2'
-  mirror 'http://archive.apache.org/dist/subversion/subversion-1.8.5.tar.bz2'
-  sha1 'd21de7daf37d9dd1cb0f777e999a529b96f83082'
+  url 'http://www.apache.org/dyn/closer.cgi?path=subversion/subversion-1.8.8.tar.bz2'
+  mirror 'http://archive.apache.org/dist/subversion/subversion-1.8.8.tar.bz2'
+  sha1 '8e9f10b7a9704c90e17cfe76fd56e3fe74c01a7a'
 
   bottle do
     sha1 '1022095a741a6fb2c43b28003cecd6d8f220fe1e' => :mavericks
@@ -44,9 +44,7 @@ class Subversion < Formula
   env :userpaths if build.include? 'perl' or build.include? 'ruby'
 
   # 1. Prevent '-arch ppc' from being pulled in from Perl's $Config{ccflags}
-  # 2. Backport r1535610 to help fix #23993.
-  #    See http://subversion.tigris.org/issues/show_bug.cgi?id=4465
-  # 3. Fix #23993 by stripping flags swig can't handle from SWIG_CPPFLAGS
+  # 2. Fix #23993 by stripping flags swig can't handle from SWIG_CPPFLAGS
   def patches
     { :p0 => DATA }
   end
@@ -261,86 +259,8 @@ Patch 1
                   " -I$swig_srcdir/perl/libsvn_swig_perl",
                   " -I$svnlib_srcdir/include",
 
+
 Patch 2
-
-$  svn log -v -r1535610 --diff http://svn.apache.org/repos/asf/subversion/trunk
-------------------------------------------------------------------------
-r1535610 | breser | 2013-10-24 20:22:50 -0600 (Thu, 24 Oct 2013) | 20 lines
-Changed paths:
-   M /subversion/trunk/Makefile.in
-   M /subversion/trunk/build.conf
-   M /subversion/trunk/configure.ac
-
-Filter out -no-cpp-precomp from flags passed to SWIG.
-
-This is necessary since APR for whatever reason leaks the fact that it uses
--no-cpp-precomp on OS X into apr-1-config.  Unfortunately, a lot of versions
-of APR have this in the wild so we just have to deal with it.  If you use clang
-directly you don't see this because we already filter it out of CPPFLAGS.
-
-* Makefile.in
-  (SWIG_CPPFLAGS): New variable, deliberately pulling in EXTRA_CPPFLAGS and
-    not EXTRA_SIWG_CPPFLAGS because it would be harmful to split those
-    (e.g. users wanting to enable a feature that adds an API).
-
-* build.conf
-  (swig-python-opts, swig-perl-opts, swig-ruby-opts): Use SWIG_CPPFLAGS
-    instead of CPPFLAGS.
-
-* configure.acc
-  (SWIG_CPPFLAGS): Add the variable and copy it from the normal CPPFLAGS
-    while filtering out the -no-cpp-precomp.
-
-
-Index: Makefile.in
-===================================================================
---- Makefile.in	(revision 1535609)
-+++ Makefile.in	(revision 1535610)
-@@ -181,6 +181,7 @@
- CPPFLAGS = @CPPFLAGS@ $(EXTRA_CPPFLAGS)
- LDFLAGS = @LDFLAGS@ $(EXTRA_LDFLAGS)
- SWIG_LDFLAGS = @SWIG_LDFLAGS@ $(EXTRA_SWIG_LDFLAGS)
-+SWIG_CPPFLAGS = @SWIG_CPPFLAGS@ $(EXTRA_CPPFLAGS)
-
- COMPILE = $(CC) $(CMODEFLAGS) $(CPPFLAGS) $(CMAINTAINERFLAGS) $(CFLAGS) $(INCLUDES)
- COMPILE_NOWARN = $(CC) $(CMODEFLAGS) $(CPPFLAGS) $(CNOWARNFLAGS) $(CFLAGS) $(INCLUDES)
-Index: build.conf
-===================================================================
---- build.conf	(revision 1535609)
-+++ build.conf	(revision 1535610)
-@@ -88,9 +88,9 @@
-
- bdb-test-scripts =
-
--swig-python-opts = $(CPPFLAGS) -python -classic
--swig-perl-opts = $(CPPFLAGS) -perl -nopm -noproxy
--swig-ruby-opts = $(CPPFLAGS) -ruby
-+swig-python-opts = $(SWIG_CPPFLAGS) -python -classic
-+swig-perl-opts = $(SWIG_CPPFLAGS) -perl -nopm -noproxy
-+swig-ruby-opts = $(SWIG_CPPFLAGS) -ruby
- swig-languages = python perl ruby
- swig-dirs =
-         subversion/bindings/swig/python
-Index: configure.ac
-===================================================================
---- configure.ac	(revision 1535609)
-+++ configure.ac	(revision 1535610)
-@@ -1490,6 +1490,11 @@
-   SVN_STRIP_FLAG(CPPFLAGS, [-no-cpp-precomp ])
- fi
-
-+# Need to strip '-no-cpp-precomp' from CPPFLAGS for SWIG as well.
-+SWIG_CPPFLAGS="$CPPFLAGS"
-+SVN_STRIP_FLAG(SWIG_CPPFLAGS, [-no-cpp-precomp ])
-+AC_SUBST([SWIG_CPPFLAGS])
-+
- dnl Since this is used only on Unix-y systems, define the path separator as '/'
- AC_DEFINE_UNQUOTED(SVN_PATH_LOCAL_SEPARATOR, '/',
-         [Defined to be the path separator used on your local filesystem])
-
-------------------------------------------------------------------------
-
-Patch 3
 
 diff -u configure.ac configure.ac
 --- configure.ac	(working copy)
