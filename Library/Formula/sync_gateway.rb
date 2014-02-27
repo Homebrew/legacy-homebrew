7a5697e0a2e76e741b3b92ca05bff523f1e3b542
Philipp Fehre <philipp.fehre@googlemail.com>
2014-02-27 10:02:02 +0100

    sync_gateway: 1.0-beta2 - couchbase mobile sync
    sync_gateway provides the syncing backend for couchbase mobile and is
    used together with couchbase-lite-ios or android to develop syncing apps
    for iOS or android.
    
    Since the project itself is named sync_gateway I kept the name for the
    formula as well.

---
+++ Library/Formula/sync_gateway.rb
@@ -0,0 +1,27 @@
+require "formula"
+
+class SyncGateway < Formula
+  homepage "http://docs.couchbase.com/sync-gateway"
+  url "https://github.com/couchbase/sync_gateway.git", :tag => "1.0-beta2"
+  head "https://github.com/couchbase/sync_gateway.git", :branch => "master"
+
+  depends_on "go" => :build
+
+  def install
+    if build.head?
+      system "make", "buildit"
+    else
+      ENV["GOBIN"] = "#{Dir.pwd}/bin"
+      system "./go.sh", "install", "-v", "github.com/couchbaselabs/sync_gateway"
+      bin.install("bin/sync_gateway")
+    end
+  end
+
+  test do
+    # test gateway startup and shutdown
+    pid = fork { exec("#{bin}/sync_gateway") }
+    sleep 1
+    Process.kill("SIGINT", pid)
+    Process.wait(pid)
+  end
+end
