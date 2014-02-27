3ac562fd44e7bd660cbc34d8a60d36db27d16764
Adam Vandenberg <flangy@gmail.com>
2014-02-26 21:19:55 -0800

    bagit: use var/bagit/logs for logs
    Closes #27019.

--- Library/Formula/bagit.rb
+++ Library/Formula/bagit.rb
@@ -5,10 +5,11 @@ class Bagit < Formula
   url 'https://github.com/LibraryOfCongress/bagit-java/releases/download/bagit-4.8.1/bagit-4.8.1-bin.zip'
   sha1 'a5f42372dcbe75f44d9181dd8edc8e6f18b68ec9'
 
-  skip_clean 'logs'
-
   def install
-    prefix.install %w{conf logs}
+    inreplace "conf/log4j.properties", "${app.home}/logs", "#{var}/log/bagit"
+    (var/'log/bagit').mkpath
+
+    prefix.install 'conf'
 
     libexec.install Dir['lib/*']
 
