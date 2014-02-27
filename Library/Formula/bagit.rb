471d6083db0625d40eed3b4e74efcb7d34bf10bd
Adam Vandenberg <flangy@gmail.com>
2014-02-26 21:54:04 -0800

    bagit: simplify install to libexec

--- Library/Formula/bagit.rb
+++ Library/Formula/bagit.rb
@@ -6,22 +6,13 @@ class Bagit < Formula
   sha1 'a5f42372dcbe75f44d9181dd8edc8e6f18b68ec9'
 
   def install
-    inreplace "conf/log4j.properties", "${app.home}/logs", "#{var}/log/bagit"
+    # put logs in var, not in the Cellar
     (var/'log/bagit').mkpath
+    inreplace "conf/log4j.properties", "${app.home}/logs", "#{var}/log/bagit"
 
-    prefix.install 'conf'
+    libexec.install Dir['*']
 
-    libexec.install Dir['lib/*']
-
-    # Point to libexec, and move conf file
-    inreplace "bin/bag" do |s|
-      s.gsub! "$APP_HOME/lib", "$APP_HOME/libexec"
-      s.gsub! "/bin/$APP_NAME.classworlds.conf", "/conf/$APP_NAME.classworlds.conf"
-    end
-    inreplace "bin/bag.classworlds.conf", "${app.home}/lib", "${app.home}/libexec"
-
-    bin.install 'bin/bag'
-    (prefix+'conf').install 'bin/bag.classworlds.conf'
+    bin.install_symlink libexec/"bin/bag"
   end
 
   test do
