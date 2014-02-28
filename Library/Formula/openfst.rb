3a4b220d8fac8c56b9e1a46d94e073f4d0b3cd43
Kyle Gorman <gormanky@champ.ohsu.edu>
2014-02-28 12:26:19 -0800

    changed download URL

--- Library/Formula/openfst.rb
+++ Library/Formula/openfst.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Openfst < Formula
   homepage 'http://www.openfst.org/'
-  url 'http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.3.4.tar.gz'
+  url 'http://openfst.cs.nyu.edu/twiki/pub/FST/FstDownload/openfst-1.3.4.tar.gz'
   sha1 '21972c05896b2154a3fa1bdca5c9a56350194b38'
 
   def install
