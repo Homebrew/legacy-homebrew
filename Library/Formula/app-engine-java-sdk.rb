2897f960b8e39098870fa0fb32050b855636a264
Adam Vandenberg <flangy@gmail.com>
2014-02-28 07:36:24 -0800

    appengine java: update homepage

--- Library/Formula/app-engine-java-sdk.rb
+++ Library/Formula/app-engine-java-sdk.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class AppEngineJavaSdk < Formula
-  homepage 'https://developers.google.com/appengine/docs/java/overview'
+  homepage 'https://developers.google.com/appengine/docs/java/gettingstarted/introduction'
   url 'https://commondatastorage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.0.zip'
   sha1 '45fac6746c65641eeaf087c88bc755365c69ef71'
 
