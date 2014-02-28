f8589eba564b2a37fdc27c2520db41e26a6171a5
Adam Vandenberg <flangy@gmail.com>
2014-02-28 07:50:10 -0800

    platypus: fix --HEAD builds
    Closes #27085.

--- Library/Formula/platypus.rb
+++ Library/Formula/platypus.rb
@@ -4,31 +4,40 @@ class Platypus < Formula
   homepage "http://sveinbjorn.org/platypus"
   url "https://github.com/sveinbjornt/Platypus/raw/4.8/Releases/platypus4.8.src.zip"
   sha1 "39d165b9579600cef637b45c70c82307697bb7be"
-  head "https://github.com/sveinbjornt/Platypus", :branch => "master"
+  head "https://github.com/sveinbjornt/Platypus.git", :branch => "master"
 
   depends_on :xcode
 
   def install
-    cd "Platypus 4.8 Source" do
-      if MacOS.version >= :mountain_lion
-        # Platypus wants to use a compiler that isn't shipped with recent versions of XCode.
-        # See https://github.com/Homebrew/homebrew/pull/22618#issuecomment-24898050
-        # and https://github.com/sveinbjornt/Platypus/issues/22
+    # 4.8 tarball has extra __MACOSX folder, so go to the right one
+    # The head tarball only has a single folder in it
+    cd "Platypus 4.8 Source" if build.stable?
 
-        inreplace "Platypus.xcodeproj/project.pbxproj", "GCC_VERSION", "//GCC_VERSION"
-      end
-      xcodebuild "SYMROOT=build", "DSTROOT=#{buildpath}",
-                 "-project", "Platypus.xcodeproj",
-                 "-target", "platypus",
-                 "-target", "ScriptExec",
-                 "clean", "install"
-      man1.install "CommandLineTool/platypus.1"
+    if build.stable? and MacOS.version >= :mountain_lion
+      # Platypus wants to use a compiler that isn't shipped with recent versions of XCode.
+      # See https://github.com/Homebrew/homebrew/pull/22618#issuecomment-24898050
+      # and https://github.com/sveinbjornt/Platypus/issues/22
+
+      inreplace "Platypus.xcodeproj/project.pbxproj", "GCC_VERSION", "//GCC_VERSION"
     end
 
+    xcodebuild "SYMROOT=build", "DSTROOT=#{buildpath}",
+               "-project", "Platypus.xcodeproj",
+               "-target", "platypus",
+               "-target", "ScriptExec",
+               "clean",
+               "install"
+
+    man1.install "CommandLineTool/platypus.1"
+
+    cd buildpath
+
     bin.install "platypus_clt" => "platypus"
+
     cd "ScriptExec.app/Contents" do
       (share/"platypus").install "Resources/MainMenu.nib", "MacOS/ScriptExec"
     end
+
   end
 
   test do
