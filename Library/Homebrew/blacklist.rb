d5189921b76b73e970359153d461aad9772d8db1
Jack Nagel <jacknagel@gmail.com>
2014-02-28 17:20:42 -0600

    Fix libpcap blacklist message

--- Library/Homebrew/blacklist.rb
+++ Library/Homebrew/blacklist.rb
@@ -1,7 +1,10 @@
 def blacklisted? name
   case name.downcase
-  when 'screen', /^rubygems?$/, 'libpcap' then <<-EOS.undent
+  when 'screen', /^rubygems?$/, then <<-EOS.undent
     Apple distributes #{name} with OS X, you can find it in /usr/bin.
+    EOS
+  when 'libpcap' then <<-EOS.undent
+    Apple distributes #{name} with OS X, you can find it in /usr/lib.
     EOS
   when 'libiconv' then <<-EOS.undent
     Apple distributes #{name} with OS X, you can find it in /usr/lib.
