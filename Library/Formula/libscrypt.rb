f59eac7793723eb9d9fefb387c1c67ccf6a4109b
Alex Gaynor <alex.gaynor@gmail.com>
2014-02-27 19:03:15 -0800

    Removed unneeded line

--- Library/Formula/libscrypt.rb
+++ Library/Formula/libscrypt.rb
@@ -6,8 +6,6 @@ class Libscrypt < Formula
   sha1 "27a36e4f7ae35f44a4bb8e8f922883a9323d9b9c"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     system "make", "install", "PREFIX=#{prefix}", "LDFLAGS=", "CFLAGS_EXTRA="
   end
 
