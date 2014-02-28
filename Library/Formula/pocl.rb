daf9d58d5d1734671affa8c6b220d2093214d81d
Mikael Lepistö <mikael.lepisto@vincit.fi>
2014-02-28 23:38:39 +0200

    pocl 0.9: fixed patch to be before install.

--- Library/Formula/pocl.rb
+++ Library/Formula/pocl.rb
@@ -10,6 +10,13 @@ class Pocl < Formula
   depends_on 'llvm' => 'with-clang'
   depends_on :libltdl
 
+  def patches
+    # Check if ndebug flag is required for compiling pocl didn't work on osx
+    # for some reason. Information if bug is fixed is found from
+    # https://github.com/pocl/pocl/issues/59
+    DATA
+  end
+
   def install
     ENV.j1
     system "./configure", "--disable-debug",
@@ -30,13 +37,6 @@ class Pocl < Formula
     system "#{bin}/pocl-standalone -h head.h -o foo.bc foo.cl"
     system "\"#{Formula["llvm"].opt_prefix}/bin/llvm-dis\" < foo.bc | grep foo_workgroup"
     system "pkg-config pocl --modversion | grep #{version}"
-  end
-
-  def patches
-    # Check if ndebug flag is required for compiling pocl didn't work on osx
-    # for some reason. Information if bug is fixed is found from
-    # https://github.com/pocl/pocl/issues/59
-    DATA
   end
 end
 
