5903d0d32acc617c1000dfe204abd035fe434d6e
Jack Nagel <jacknagel@gmail.com>
2014-02-28 11:03:59 -0600

    Remove deprecation notice from system "xcodebuild"
    The new xcodebuild method calls system "xcodebuild" underneath, so this
    notice would always be printed. Instead we will just rely on the audit
    check.
    
    Fixes #27092.

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -565,7 +565,6 @@ class Formula
     ohai "#{cmd} #{pretty_args*' '}".strip
 
     if cmd.to_s.start_with? "xcodebuild"
-      opoo %{system "xcodebuild" is deprecated, use the xcodebuild method instead}
       removed_ENV_variables.update(ENV.remove_cc_etc)
     end
 
