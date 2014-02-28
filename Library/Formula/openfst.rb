12bbd98aea210339696c29add6173723711dee92
Kyle Gorman <gormanky@champ.ohsu.edu>
2014-02-27 17:03:37 -0800

    two style issues fixed
    from @adamv:
    
        Style nit-pick: we prefer to keep --prefix... near the top of the
        options, so add these two new ones under that (and preferably each on a
        separate line.)
    
    from @mistydemeo:
    
        You can use ENV.libstdcxx to force it to use libstdc++ with clang.

--- Library/Formula/openfst.rb
+++ Library/Formula/openfst.rb
@@ -6,13 +6,15 @@ class Openfst < Formula
   sha1 '21972c05896b2154a3fa1bdca5c9a56350194b38'
 
   def install
-    if MacOS.version > :mountain_lion
-      ENV.append 'CPPFLAGS', "-I#{MacOS.sdk_path}/usr/include/c++/4.2.1"
-      ENV.append 'LIBS', "#{MacOS.sdk_path}/usr/lib/libstdc++.dylib"
-    end
-    system "./configure", "--disable-dependency-tracking",
-                          "--enable-far", "--enable-pdt",
-                          "--prefix=#{prefix}"
+    ENV.libstdcxx if ENV.compiler == :clang
+    #if MacOS.version > :mountain_lion
+    #  ENV.append 'CPPFLAGS', "-I#{MacOS.sdk_path}/usr/include/c++/4.2.1"
+    #  ENV.append 'LIBS', "#{MacOS.sdk_path}/usr/lib/libstdc++.dylib"
+    #end
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--enable-far", 
+                          "--enable-pdt"
     system "make install"
   end
 end
