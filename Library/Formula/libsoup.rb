512fe19ffb8d26ce0c49faa1faf45aad9a1ba5ae
Adam Vandenberg <flangy@gmail.com>
2014-03-01 14:04:33 -0800

    libsoup: optional gobject-introspection
    Closes #26844.

--- Library/Formula/libsoup.rb
+++ Library/Formula/libsoup.rb
@@ -8,16 +8,28 @@ class Libsoup < Formula
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
-  depends_on 'glib-networking' # Required at runtime for TLS support
-  depends_on 'gnutls' # Also required for TLS
-  depends_on 'sqlite' # For SoupCookieJarSqlite
+  depends_on 'glib-networking'
+  depends_on 'gnutls'
+  depends_on 'sqlite'
+  depends_on 'gobject-introspection' => :optional
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--without-gnome",
-                          "--disable-tls-check"
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}",
+      "--without-gnome",
+      "--disable-tls-check"
+    ]
+
+    if build.with? "gobject-introspection"
+      args << "--enable-introspection"
+    else
+      args << "--disable-introspection"
+    end
+
+    system "./configure", *args
     system "make install"
   end
 end
