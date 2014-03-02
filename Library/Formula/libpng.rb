6187f41e0b92e1a38e7d33964ce586e0bbe3128e
Adam Vandenberg <flangy@gmail.com>
2014-03-01 18:10:12 -0800

    libpng 1.5.18

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -2,15 +2,14 @@ require 'formula'
 
 class Libpng < Formula
   homepage 'http://www.libpng.org/pub/png/libpng.html'
-  url 'http://sourceforge.net/projects/libpng/files/libpng15/older-releases/1.5.17/libpng-1.5.17.tar.bz2'
-  sha1 '899d660104f3ef5c349c57faad10844b388f8442'
+  url 'http://downloads.sf.net/project/libpng/libpng15/1.5.18/libpng-1.5.18.tar.bz2'
+  sha1 '52fee0139ff96c98a6a0ef6375845ca366a33c94'
 
   bottle do
     cellar :any
-    revision 1
-    sha1 '039e4a626f4cbcbfa14349f86f33663ebc9d20d8' => :mavericks
-    sha1 '6f9f7ad1ea035e897845dc3f4d3909a92664dbb8' => :mountain_lion
-    sha1 '53e282b87f737dc16ddb0700585b5754cec6b913' => :lion
+    sha1 "405736a4cd656764b5bea9ec0b11efb2daddaac0" => :mavericks
+    sha1 "ff19f63cc4e5225f199adb981e0e1d54dede4d2b" => :mountain_lion
+    sha1 "48f94cb7f6a1417f506cc552fd990e7bd2684dec" => :lion
   end
 
   keg_only :provided_pre_mountain_lion
