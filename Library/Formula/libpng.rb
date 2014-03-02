dc7d134ec63795a8b9a80ef61a1ac39b9ef4e505
Theodor Văraru <theo@tvararu.ro>
2014-03-02 03:29:16 +0200

    libpng: update download URL
    Closes #27121.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Libpng < Formula
   homepage 'http://www.libpng.org/pub/png/libpng.html'
-  url 'http://downloads.sf.net/project/libpng/libpng15/1.5.17/libpng-1.5.17.tar.bz2'
+  url 'http://sourceforge.net/projects/libpng/files/libpng15/older-releases/1.5.17/libpng-1.5.17.tar.bz2'
   sha1 '899d660104f3ef5c349c57faad10844b388f8442'
 
   bottle do
