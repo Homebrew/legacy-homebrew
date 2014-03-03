ebc5ccf147968ae546a75e6f41d7bd8cc51b0e27
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-03 21:52:23 +0000

    moreutils: add 0.51 bottle.
    Closes #27170.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/moreutils.rb
+++ Library/Formula/moreutils.rb
@@ -3,6 +3,13 @@ require 'formula'
 class Moreutils < Formula
   homepage 'http://packages.debian.org/unstable/utils/moreutils'
   url 'http://mirrors.kernel.org/debian/pool/main/m/moreutils/moreutils_0.51.tar.gz'
+  bottle do
+    cellar :any
+    sha1 "21fa332179c54179b9581ec1fd5eae00eefbf9da" => :mavericks
+    sha1 "b7b88be54b3cd5659a720dd899ceacd790414e20" => :mountain_lion
+    sha1 "f873a5e0b83b859899c98126871d0fa4bb04b8b9" => :lion
+  end
+
   mirror 'http://ftp.us.debian.org/debian/pool/main/m/moreutils/moreutils_0.51.tar.gz'
   sha1 '374b8c3bea962bbcde4a8158051c570a1fec6811'
 
