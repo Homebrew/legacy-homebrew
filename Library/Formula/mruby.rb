f86c6571365111b794ce3f8a4bfd88117776ebc0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-04 12:32:30 +0000

    mruby: add 1.0.0 bottle.
    Closes #27173.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mruby.rb
+++ Library/Formula/mruby.rb
@@ -7,6 +7,13 @@ class Mruby < Formula
 
   head "https://github.com/mruby/mruby.git"
 
+  bottle do
+    cellar :any
+    sha1 "22bbdac4d6f903b24b1d79d64b311464fdd42161" => :mavericks
+    sha1 "5076d06d254730a052d88b4a9b4b2d1f630a0449" => :mountain_lion
+    sha1 "a88b7302f383dae1f9ca67a3fcbbd603841eec86" => :lion
+  end
+
   depends_on "bison" => :build
 
   def install
