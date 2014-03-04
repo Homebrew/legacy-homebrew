0bc1acb8fde23391364e2d94d932229d87a04157
Ilya Otyutskiy <ilya.otyutskiy@icloud.com>
2014-03-04 13:17:29 +0400

    Add pyenv-virtualenvwrapper v20140122

---
+++ Library/Formula/pyenv-virtualenvwrapper.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class PyenvVirtualenvwrapper < Formula
+  homepage 'https://github.com/yyuu/pyenv-virtualenvwrapper'
+  url 'https://github.com/yyuu/pyenv-virtualenvwrapper/archive/v20140122.tar.gz'
+  sha1 '7233865730c207d6e6660db906be19b88cb3ca3b'
+
+  head 'https://github.com/yyuu/pyenv-virtualenvwrapper.git'
+
+  depends_on 'pyenv'
+
+  def install
+    ENV['PREFIX'] = prefix
+    system "./install.sh"
+  end
+end
