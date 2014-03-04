b23d5fd9cbe52ae7678dfa20b29f13cf3f8d1023
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-04 17:38:18 +0000

    haxe: add 3.1.0 bottle.

--- Library/Formula/haxe.rb
+++ Library/Formula/haxe.rb
@@ -6,6 +6,13 @@ class Haxe < Formula
 
   head 'https://github.com/HaxeFoundation/haxe.git', :branch => 'development'
 
+  bottle do
+    cellar :any
+    sha1 "2d0d03f2a3a1da2bf89473ce495d68d71922d380" => :mavericks
+    sha1 "39f8993c08e35b06a9b57f6ea997c57029898a76" => :mountain_lion
+    sha1 "4fd905adeeab662f5bb547053dce580dcd0936f3" => :lion
+  end
+
   depends_on 'neko'
   depends_on 'objective-caml'
 
