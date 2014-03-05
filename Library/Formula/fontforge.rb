a45cbd42f719a283b96507974dfe34d10e17e157
Sean Leather <sean.leather@gmail.com>
2014-03-05 14:41:08 +0200

    fontforge: Add missing libtool dependency

--- Library/Formula/fontforge.rb
+++ Library/Formula/fontforge.rb
@@ -10,6 +10,7 @@ class Fontforge < Formula
 
   depends_on :autoconf => :build
   depends_on :automake => :build
+  depends_on :libtool => :build
   depends_on 'pkg-config' => :build
 
   depends_on 'gettext'
