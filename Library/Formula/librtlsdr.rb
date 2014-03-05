1e5af55c66678f148aaf5bf9896a17b4381519f9
Adam Vandenberg <flangy@gmail.com>
2014-03-04 21:58:04 -0800

    wip

--- Library/Formula/librtlsdr.rb
+++ Library/Formula/librtlsdr.rb
@@ -5,6 +5,7 @@ class Librtlsdr < Formula
   url "https://github.com/steve-m/librtlsdr/archive/v0.5.3.tar.gz"
   sha1 "f6f20f7b0562a6d3f7b9ff7bff38a15bff175982"
 
+  depends_on "pkg-config" => :build
   depends_on "cmake" => :build
   depends_on "libusb"
 
