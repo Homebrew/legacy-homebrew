1cf19c691967fddb2551f06ba2bf04ae06422b19
Kyle Gorman <gormanky@champ.ohsu.edu>
2014-03-05 10:47:46 -0800

    New formula: opengrm-ngram
    OpenGRM-ngram extends OpenFST (upon which it depends) to provide support
    for general n-gram language modeling and smoothing.

---
+++ Library/Formula/opengrm-ngram.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class OpengrmNgram < Formula
+  homepage 'http://www.openfst.org/twiki/bin/view/GRM/NGramLibrary'
+  url 'http://openfst.cs.nyu.edu/twiki/pub/GRM/NGramDownload/opengrm-ngram-1.1.0.tar.gz'
+  sha1 'a2ceeaf6ac129b66d2682d76a20388cf1d4b8c31'
+
+  depends_on 'openfst'
+
+  def install
+    ENV.libstdcxx if ENV.compiler == :clang && MacOS.version >= :mavericks
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking"
+    system "make install"
+  end
+end
+
