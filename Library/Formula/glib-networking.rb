85c593dcbc9ea9d76e5fe5ea95843ac9e493d757
Jack Nagel <jacknagel@gmail.com>
2014-03-05 21:54:59 -0600

    Increment revisions on gnutls dependents

--- Library/Formula/glib-networking.rb
+++ Library/Formula/glib-networking.rb
@@ -4,6 +4,7 @@ class GlibNetworking < Formula
   homepage 'https://launchpad.net/glib-networking'
   url 'http://ftp.gnome.org/pub/GNOME/sources/glib-networking/2.38/glib-networking-2.38.2.tar.xz'
   sha256 'e8e2fe919d3236169c9bfaec807966ec81b844fc28b11bddc9f8256cfa949fc7'
+  revision 1
 
   depends_on 'pkg-config' => :build
   depends_on 'xz' => :build
