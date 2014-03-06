3aff08c5311afbbc5a0148d05d1ce8c92a1ff22c
Jack Nagel <jacknagel@gmail.com>
2014-03-06 09:28:03 -0600

    libvirt: increment revision for libgcrypt

--- Library/Formula/libvirt.rb
+++ Library/Formula/libvirt.rb
@@ -4,12 +4,12 @@ class Libvirt < Formula
   homepage 'http://www.libvirt.org'
   url 'http://libvirt.org/sources/libvirt-1.2.1.tar.gz'
   sha256 'bc29b5751bf36753c17e2fdbb75e70c7b07df3d9527586d3426e90f5f4abb898'
-  revision 1
+  revision 2
 
   bottle do
-    sha1 "4fbae12166798f0d14c5ce93567e2e02a4b67db7" => :mavericks
-    sha1 "bec4b7b2ab1f45bb091db265d915892d30aba9c2" => :mountain_lion
-    sha1 "b9a52ff49f753b87d7a7ba07eefc8ae68dd1cfd4" => :lion
+    sha1 "0bfaabdc25891e5f01915eba40c9ffc262fc5417" => :mavericks
+    sha1 "93fa00f726556ad96b33f71a4bffb55bac5f0ce4" => :mountain_lion
+    sha1 "b13fe900124c065e247d3a472197cca19d52221a" => :lion
   end
 
   option 'without-libvirtd', 'Build only the virsh client and development libraries'
