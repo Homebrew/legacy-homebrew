45369bb074199f647d080595db2f8428ff588e4c
Kyle Gorman <gormanky@champ.ohsu.edu>
2014-03-05 18:47:16 -0800

    Added tests based on OpenGRM-NGram homepage

--- Library/Formula/opengrm-ngram.rb
+++ Library/Formula/opengrm-ngram.rb
@@ -12,4 +12,18 @@ class OpengrmNgram < Formula
     system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking'
     system 'make install'
   end
+
+  test do
+    # based on the examples at the OpenGRM "NGram quick tour" page...
+    # tests using a tokenized variant of The Importance of Being Earnest
+    system 'curl', 'http://openfst.cs.nyu.edu/twiki/pub/GRM/NGramQuickTour/earnest.txt', '-o', 'e.txt'
+    system 'ngramsymbols', 'e.txt', 'e.syms'
+    system 'farcompilestrings', '-symbols=e.syms', '-keep_symbols=1', 'e.txt', 'e.far'
+    system 'ngramcount', '-order=5', 'e.far', 'e.cnts'
+    system 'ngrammake', 'e.cnts', 'e.mod'
+    system 'ngramshrink', '-method=relative_entropy', 'e.mod', 'e.pru'
+    system 'ngramprint', '--ARPA', 'e.mod'
+    system 'ngraminfo', 'e.mod'
+    system 'ngramrandgen', '--max_sents=1', 'e.mod'
+  end
 end
