00a90e81345146666d62deca45c1356c59d84d05
Thomas Kim Pham <thomas.pham@ithings.ch>
2014-03-07 00:49:07 +0100

    update version

--- Library/Formula/cloud-watch.rb
+++ Library/Formula/cloud-watch.rb
@@ -3,7 +3,7 @@ require 'formula'
 class CloudWatch < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/2534'
   url 'http://ec2-downloads.s3.amazonaws.com/CloudWatch-2010-08-01.zip'
-  version '1.0.13.4'
+  version '1.0.20.0'
   sha1 'c3f5b856b3ff8e1ae06d0ad5db5dd27c214c4881'
 
   def install
