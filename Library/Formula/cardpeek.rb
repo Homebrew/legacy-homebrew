78bda180aa41b03885f9fa71b2e79648a2775c5c
Tarek Belkahia <tarek.belkahia@applidium.com>
2014-03-06 17:21:20 +0100

    cardpeek 0.8.2
    Closes #27270.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/cardpeek.rb
+++ Library/Formula/cardpeek.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Cardpeek < Formula
-  homepage 'https://cardpeek.googlecode.com'
-  url 'https://cardpeek.googlecode.com/files/cardpeek-0.7.2.tar.gz'
-  sha1 '9f774140bbfea2ebdd25f38146d7ebe3b1c0d871'
+  homepage 'http://pannetrat.com/Cardpeek/'
+  url 'http://downloads.pannetrat.com/install/cardpeek-0.8.2.tar.gz'
+  sha1 '7bd532684891b525ae7b98d2ca91d2bb26cd03bf'
 
   head 'http://cardpeek.googlecode.com/svn/trunk/'
 
@@ -11,17 +11,10 @@ class Cardpeek < Formula
   depends_on :autoconf
   depends_on :automake
   depends_on :x11
-  depends_on 'gtk+'
-  depends_on 'lua'
-
-  def patches
-    # Required for 0.7.2, fixed in HEAD. See:
-    # https://code.google.com/p/cardpeek/issues/detail?id=24
-    {:p0 => [
-      "https://cardpeek.googlecode.com/issues/attachment?aid=240000000&name=cardpeek-svn-osx.diff&token=JGVrSd-7Wcyfo98Lny3Y4NVUBcU%3A1373645845242",
-      "https://cardpeek.googlecode.com/issues/attachment?aid=240001000&name=extra_patch.diff&token=mQZWOOGcuPxufd414OgwQjJ505I%3A1373645845242"
-    ]} unless build.head?
-  end
+  depends_on 'gtk+3'
+  depends_on 'curl'
+  depends_on 'glib'
+  depends_on 'homebrew/versions/lua52'
 
   def install
     # always run autoreconf, neeeded to generate configure for --HEAD,
