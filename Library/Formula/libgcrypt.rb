5d981adc275c98e4b7a962709c70636b28c664a5
Jack Nagel <jacknagel@gmail.com>
2014-03-06 09:16:51 -0600

    libgcrypt 1.6.1

--- Library/Formula/libgcrypt.rb
+++ Library/Formula/libgcrypt.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Libgcrypt < Formula
   homepage 'http://gnupg.org/'
-  url 'ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.5.3.tar.bz2'
-  sha1 '2c6553cc17f2a1616d512d6870fe95edf6b0e26e'
+  url 'ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.6.1.tar.bz2'
+  sha1 'f03d9b63ac3b17a6972fc11150d136925b702f02'
 
   bottle do
     cellar :any
@@ -25,12 +25,6 @@ class Libgcrypt < Formula
   fails_with :clang do
     build 77
     cause "basic test fails"
-  end
-
-  def patches
-    if ENV.compiler == :clang and (build.universal? or build.build_32_bit?)
-      { :p0 => "https://trac.macports.org/export/85232/trunk/dports/devel/libgcrypt/files/clang-asm.patch" }
-    end
   end
 
   def install
