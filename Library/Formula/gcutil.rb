eb051642e5c19c9a59d009ff7c858bd334c6b471
Otto Bretz <otto.bretz@gmail.com>
2014-03-06 15:25:24 +0100

    Correct shasum

--- Library/Formula/gcutil.rb
+++ Library/Formula/gcutil.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Gcutil < Formula
   homepage 'https://developers.google.com/compute/docs/gcutil/'
   url 'https://dl.google.com/dl/cloudsdk/release/artifacts/gcutil-1.14.0.zip'
-  sha1 '07c678b6072f9398786a8bc61d2cbc9d34f09c84'
+  sha1 '10f34bfee7e3865cad14ad27caae446aedab9038'
 
   def install
     libexec.install 'gcutil', 'lib'
