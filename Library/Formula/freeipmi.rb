398b1144834a44a175bdb3257402cd84d3775a86
Jack Nagel <jacknagel@gmail.com>
2014-03-06 09:28:03 -0600

    freeipmi: increment revision for libgcrypt

--- Library/Formula/freeipmi.rb
+++ Library/Formula/freeipmi.rb
@@ -4,11 +4,12 @@ class Freeipmi < Formula
   homepage 'http://www.gnu.org/software/freeipmi/'
   url 'http://ftpmirror.gnu.org/freeipmi/freeipmi-1.4.1.tar.gz'
   sha1 'd59fd66e6fe9a58437d55a4bdd96ee431a8348e4'
+  revision 1
 
   bottle do
-    sha1 "352a236f1154284520b0e08da3c509ca9a6f816e" => :mavericks
-    sha1 "a0c6f7d0aa29f2dcd87f28cb53c68e885ad3532a" => :mountain_lion
-    sha1 "529f6c95ef5a2542bbc7fa3788d71c52a29894f7" => :lion
+    sha1 "b98405e8318c8dd990f598b769d1e6a155e3c2a3" => :mavericks
+    sha1 "15d32bf5d1f7f54240fba3012b2222ae1e70e209" => :mountain_lion
+    sha1 "695a73d4e94af49e9a02bf103d16a3e5aa194a51" => :lion
   end
 
   depends_on 'argp-standalone'
