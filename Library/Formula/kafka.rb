206f8142c318a6d5ff077ae5b9490ab5769cfb91
Joshua Griffith <joshua@orgsync.com>
2014-03-06 09:41:19 -0600

    Store logs in var/log/kafka and data in var/lib

--- Library/Formula/kafka.rb
+++ Library/Formula/kafka.rb
@@ -17,13 +17,43 @@ class Kafka < Formula
     # Change default partition count to 1
     inreplace "config/server.properties", "num.partitions=2", "num.partitions=1"
 
+    # Store logs in var/log/kafka
+    logs = (var + 'log/kafka')
+    logs.mkpath
+    ["config/log4j.properties", "config/test-log4j.properties"].each do |f| 
+      inreplace f,
+        "log4j.appender.kafkaAppender.File=logs/server.log",
+        "log4j.appender.kafkaAppender.File=#{logs}/server.log"
+
+      inreplace f,
+        "log4j.appender.stateChangeAppender.File=logs/state-change.log",
+        "log4j.appender.stateChangeAppender.File=#{logs}/state-change.log"
+
+      inreplace f,
+        "log4j.appender.requestAppender.File=logs/kafka-request.log",
+        "log4j.appender.requestAppender.File=#{logs}/kafka-request.log"
+
+      inreplace f,
+        "log4j.appender.controllerAppender.File=logs/controller.log",
+        "log4j.appender.controllerAppender.File=#{logs}/controller.log"
+    end
+
+    # Store data in var/lib
+    data = (var + 'lib')
+    inreplace "config/server.properties",
+      "log.dirs=/tmp/kafka-logs", "log.dirs=#{data}/kafka-logs"
+
+    inreplace "config/zookeeper.properties",
+      "dataDir=/tmp/zookeeper", "dataDir=#{data}/zookeeper"
+
     # Install application
-    libexec.install %w(bin config contrib core examples lib perf project system_test)
+    libexec.install %w(bin config contrib core examples lib perf project
+                       system_test)
     bin.write_exec_script Dir["#{libexec}/bin/*"]
 
     # Symlink configuration files
-    (etc+'kafka').mkpath
-    (etc/'kafka').install_symlink Dir["#{libexec}/config/*"]
+    (etc + 'kafka').mkpath
+    (etc / 'kafka').install_symlink Dir["#{libexec}/config/*"]
   end
 
   def caveats; <<-EOS.undent
