e9fb8a74d03adc967fa512b33c9bcffa204c6dd3
Fabian M <mail.fabianm@gmail.com>
2014-03-07 17:35:49 +0100

    Update ettercap to version 0.8.0

--- Library/Formula/ettercap.rb
+++ Library/Formula/ettercap.rb
@@ -2,32 +2,38 @@ require 'formula'
 
 class Ettercap < Formula
   homepage 'http://ettercap.github.io/ettercap/'
-  url 'https://downloads.sourceforge.net/project/ettercap/ettercap/0.7.6-Locard/ettercap-0.7.6.tar.gz'
-  sha1 '55818952a8c28beb1b650f3ccc9600a2d784a18f'
+  url 'https://github.com/Ettercap/ettercap/archive/534b08bcffd32806d4acbb606128ce6b306451c1.tar.gz'
+  sha1 '39ddf82a9f304cec0b15207f796517675a7c0dd4'
+  version '0.8.0'
+  head 'https://github.com/Ettercap/ettercap.git'
 
+  option 'without-curses', 'Install without curses interface'
+  option 'without-plugins', 'Install without plugins support'
+  option 'with-ipv6', 'Install with IPv6 support'
+  option 'enable-debug', 'Enable debug mode'
+  
   depends_on 'cmake' => :build
   depends_on 'ghostscript' => :build
   depends_on 'pcre'
   depends_on 'libnet'
   depends_on 'curl' # require libcurl >= 7.26.0
-
-  # fixes absence of strndup function on 10.6 and lower; merged upstream
-  def patches
-    if MacOS.version < :lion
-      "https://github.com/Ettercap/ettercap/commit/1692218693ed419465466299c8c76da41c37c945.patch"
-    end
-  end
+  depends_on 'gtk+' => :optional
+  depends_on 'luajit' => :optional
 
   def install
-    libnet = Formula['libnet']
-
-    args = ['..'] + std_cmake_args + [
-      "-DINSTALL_SYSCONFDIR=#{etc}",
-      '-DENABLE_GTK=OFF',
-      "-DHAVE_LIBNET:FILEPATH=#{libnet.opt_prefix}/lib/libnet.dylib"
-    ]
+    args = [".."] + std_cmake_args
+    args << "-DINSTALL_SYSCONFDIR=" + etc
+    args << ("-DCMAKE_BUILD_TYPE=" +  ((build.include? "enable-debug") ? "Debug" : "Release"))
+    args << "-DCMAKE_INSTALL_PREFIX=" + prefix
+    args << "-DENABLE_CURSES=OFF" if build.include? "without-curses"
+    args << "-DENABLE_PLUGINS=OFF" if build.include? "wihtout-plugins"
+    args << "-DENABLE_IPV6=ON" if build.include? "with-ipv6"
+    args << "-DENABLE_GTK=OFF" if build.without? "gtk+"
+    args << "-DENABLE_LUA=ON" if build.with? "luajit"
 
     mkdir "build" do
+      man.mkpath
+
       system "cmake", *args
       system "make install"
     end
