afc3f46d4b2f72ea6f414f8956f2e718297e30bc
Mike McQuaid <mike@mikemcquaid.com>
2014-03-07 18:15:12 +0000

    libxml2: use :python on 10.6.

--- Library/Formula/libxml2.rb
+++ Library/Formula/libxml2.rb
@@ -7,23 +7,17 @@ class Libxml2 < Formula
     url 'ftp://xmlsoft.org/libxml2/libxml2-2.9.1.tar.gz'
     mirror 'http://xmlsoft.org/sources/libxml2-2.9.1.tar.gz'
     sha256 'fd3c64cb66f2c4ea27e934d275904d92cec494a8e8405613780cbc8a71680fdb'
-
-    # 2.9.1 cannot build with Python 2.6: https://github.com/Homebrew/homebrew/issues/20249
-    if MacOS.version <= :snow_leopard
-      depends_on 'python' => :optional
-    else
-      depends_on :python => :recommended
-    end
   end
 
   head do
     url 'https://git.gnome.org/browse/libxml2', :using => :git
 
-    depends_on :python => :recommended # satisfied by Python 2.6+
     depends_on :autoconf
     depends_on :automake
     depends_on :libtool
   end
+
+  depends_on :python => :recommended
 
   keg_only :provided_by_osx
 
