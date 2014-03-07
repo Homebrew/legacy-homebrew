c9eac084b02f601be526d3723a9bee7095be6918
Fabian M <mail.fabianm@gmail.com>
2014-03-07 20:00:25 +0100

    Update ettercap formula to meet guidelines

--- Library/Formula/ettercap.rb
+++ Library/Formula/ettercap.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Ettercap < Formula
   homepage 'http://ettercap.github.io/ettercap/'
-  url 'https://github.com/Ettercap/ettercap/archive/534b08bcffd32806d4acbb606128ce6b306451c1.tar.gz'
-  sha1 '39ddf82a9f304cec0b15207f796517675a7c0dd4'
-  version '0.8.0'
+  url 'https://github.com/Ettercap/ettercap/archive/v0.8.0.tar.gz'
+  sha1 '008fca94bbd67b578699300eb321766cd41fbfff'
+
   head 'https://github.com/Ettercap/ettercap.git'
 
   option 'without-curses', 'Install without curses interface'
@@ -20,6 +20,14 @@ class Ettercap < Formula
   depends_on 'gtk+' => :optional
   depends_on 'luajit' => :optional
 
+  # 0.8.0 won't build without these patches.
+  def patches
+    [
+      "https://github.com/Ettercap/ettercap/commit/4aaaa2fb6a4ea98e110548802ed015694244b90e.patch",
+      "https://github.com/Ettercap/ettercap/commit/33ac95f78e4f6f067e6bc33b8883b3b7daa896f3.patch"
+    ]
+  end
+
   def install
     args = [".."] + std_cmake_args
     args << "-DINSTALL_SYSCONFDIR=" + etc
@@ -32,8 +40,6 @@ class Ettercap < Formula
     args << "-DENABLE_LUA=ON" if build.with? "luajit"
 
     mkdir "build" do
-      man.mkpath
-
       system "cmake", *args
       system "make install"
     end
