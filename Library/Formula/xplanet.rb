854985e4a14fd53c6643f78c3456709dd73294b2
Abe Rajab <blogabe@rajab.us>
2014-03-07 14:31:50 -0800

    xplanet 1.3 (updated dependencies added)
    Adding a "complete" option so that the necessary dependencies installed with Xplanet match Xplanet's default configuration options, namely: PNM support via netpbm, TrueType support via freetype, and PANGOFT2 support via pango.  This will not include JPLs CSPICE library since that is not a current formula.  Assuming theuser has followed the instructions to install and link CSPICE in Homebrew, this updated formula will include support for that as well.

--- Library/Formula/xplanet.rb
+++ Library/Formula/xplanet.rb
@@ -6,12 +6,20 @@ class Xplanet < Formula
   sha1 '7c5208b501b441a0184cbb334a5658d0309d7dac'
 
   option "with-x", "Build for X11 instead of Aqua"
+  option "complete", "Installs the additional dependencies included as part Xplanet's default configuration"
 
   depends_on 'pkg-config' => :build
+  depends_on 'libpng'
   depends_on 'jpeg'
   depends_on 'giflib'
   depends_on 'libtiff'
   depends_on :x11
+
+  if build.include? 'complete'
+    depends_on 'netpbm'
+    depends_on 'freetype'
+    depends_on 'pango'
+  end
 
   def install
     args = %W[
@@ -22,6 +30,12 @@ class Xplanet < Formula
       args << "--with-x"
     else
       args << "--with-aqua" << "--without-x"
+    end
+
+    if build.include? "complete"
+      pnm = Formula["netpbm"].opt_prefix
+      ENV.append 'CPPFLAGS', "-I#{pnm}/include/netpbm"
+      ENV.append 'LDFLAGS', "-L#{pnm}/lib"
     end
 
     system "./configure", *args
