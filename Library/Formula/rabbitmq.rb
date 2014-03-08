b5472cf78163f8e9dd5db1943d7d8ce647f8edd9
mythosil <mythosil@yahoo.co.jp>
2014-03-09 00:35:12 +0900

    rabbitmq-server-3.2.4

--- Library/Formula/rabbitmq.rb
+++ Library/Formula/rabbitmq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rabbitmq < Formula
   homepage 'http://www.rabbitmq.com'
-  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.3/rabbitmq-server-mac-standalone-3.2.3.tar.gz'
-  sha1 'f7bd7fbc5e8f9021204352e02dba806d2064d564'
+  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.4/rabbitmq-server-mac-standalone-3.2.4.tar.gz'
+  sha1 '020351da4c4e75da9f3ed275a4ea199ed2688acd'
 
   depends_on 'simplejson' => :python if MacOS.version <= :leopard
 
