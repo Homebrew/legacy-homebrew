b9197ca2a6aa389c79e81f3d721500f9929494b3
Xu Cheng <xucheng@me.com>
2014-03-09 00:10:41 +0800

    add Python 3 support to Pyside

--- Library/Formula/pyside.rb
+++ Library/Formula/pyside.rb
@@ -8,9 +8,41 @@ class Pyside < Formula
 
   head 'git://gitorious.org/pyside/pyside.git'
 
+  depends_on :python => :recommended
+  depends_on :python3 => :optional
+
+  if !Formula["python"].installed? && build.with?("python") &&
+     build.with?("python3")
+    odie <<-EOS.undent
+      pyside: You cannot use system Python 2 and Homebrew's Python 3 simultaneously.
+      Either `brew install python` or use `--without-python3`.
+    EOS
+  elsif build.without?("python3") && build.without?("python")
+    odie "pyside: --with-python3 must be specified when using --without-python"
+  end
+
   depends_on 'cmake' => :build
-  depends_on 'shiboken'
   depends_on 'qt'
+
+  # if build.with? 'python3'
+  #   if build.without? 'python'
+  #     depends_on 'shiboken' => ['with-python3', 'without-python']
+  #   else
+  #     depends_on 'shiboken' => 'with-python3'
+  #   end
+  # else
+  #   depends_on 'shiboken'
+  # end
+
+  def pythons
+    pythons = []
+    ["python", "python3"].each do |python|
+      next if build.without? python
+      version = /\d\.\d/.match `#{python} --version 2>&1`
+      pythons << [python, version]
+    end
+    pythons
+  end
 
   def patches
     DATA  # Fix moc_qpytextobject.cxx not found (https://codereview.qt-project.org/62479)
@@ -19,23 +51,37 @@ class Pyside < Formula
   def install
     # Add out of tree build because one of its deps, shiboken, itself needs an
     # out of tree build in shiboken.rb.
-    mkdir "macbuild" do
-      qt = Formula["qt"].opt_prefix
-      args = std_cmake_args + %W[
-        -DSITE_PACKAGE=#{lib}/python2.7/site-packages
-        -DALTERNATIVE_QT_INCLUDE_DIR=#{qt}/include
-        -DQT_SRC_DIR=#{qt}/src
-        -DPYTHON_SUFFIX='-python2.7'
-        ..
-      ]
-      system 'cmake', *args
-      system 'make'
-      system 'make', 'install'
+    pythons.each do |python, version|
+      ohai "Install for Python #{version}"
+      mkdir "macbuild#{version}" do
+        qt = Formula["qt"].opt_prefix
+        args = std_cmake_args + %W[
+          -DSITE_PACKAGE=#{lib}/python#{version}/site-packages
+          -DALTERNATIVE_QT_INCLUDE_DIR=#{qt}/include
+          -DQT_SRC_DIR=#{qt}/src
+        ]
+        if version.to_s[0,1] == '2'
+          args << "-DPYTHON_SUFFIX=-python#{version}"
+        else
+          python_suffix=".cpython-#{version.to_s[0,1]}#{version.to_s[2,3]}m"
+          args << "-DPYTHON_SUFFIX=#{python_suffix}"
+          args << "-DUSE_PYTHON3=1"
+        end
+        args << '..'
+        system 'cmake', *args
+        system 'make'
+        system 'make', 'install'
+      end
     end
   end
 
   test do
-    system 'python', '-c', "from PySide import QtCore"
+    pythons.each do |python, version|
+      unless Formula[python].installed?
+        ENV["PYTHONPATH"] = HOMEBREW_PREFIX/"lib/python#{version}/site-packages"
+      end
+      system python, '-c', "from PySide import QtCore"
+    end
   end
 end
 
