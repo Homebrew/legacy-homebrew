5504a38db16751ee924a0e8de1aaabfd2ef34858
Fabian M <mail.fabianm@gmail.com>
2014-03-08 08:55:57 +0100

    Update ettercap to version 0.8.0

--- Library/Formula/ettercap.rb
+++ Library/Formula/ettercap.rb
@@ -2,30 +2,42 @@ require 'formula'
 
 class Ettercap < Formula
   homepage 'http://ettercap.github.io/ettercap/'
-  url 'https://downloads.sourceforge.net/project/ettercap/ettercap/0.7.6-Locard/ettercap-0.7.6.tar.gz'
-  sha1 '55818952a8c28beb1b650f3ccc9600a2d784a18f'
+  url 'https://github.com/Ettercap/ettercap/archive/v0.8.0.tar.gz'
+  sha1 '008fca94bbd67b578699300eb321766cd41fbfff'
 
+  head 'https://github.com/Ettercap/ettercap.git'
+
+  option 'without-curses', 'Install without curses interface'
+  option 'without-plugins', 'Install without plugins support'
+  option 'with-ipv6', 'Install with IPv6 support'
+  option 'enable-debug', 'Enable debug mode'
+  
   depends_on 'cmake' => :build
   depends_on 'ghostscript' => :build
   depends_on 'pcre'
   depends_on 'libnet'
   depends_on 'curl' # require libcurl >= 7.26.0
+  depends_on 'gtk+' => :optional
+  depends_on 'luajit' => :optional
 
-  # fixes absence of strndup function on 10.6 and lower; merged upstream
+  # 0.8.0 won't build without these patches.
   def patches
-    if MacOS.version < :lion
-      "https://github.com/Ettercap/ettercap/commit/1692218693ed419465466299c8c76da41c37c945.patch"
-    end
+    [
+      "https://github.com/Ettercap/ettercap/commit/4aaaa2fb6a4ea98e110548802ed015694244b90e.patch",
+      "https://github.com/Ettercap/ettercap/commit/33ac95f78e4f6f067e6bc33b8883b3b7daa896f3.patch"
+    ]
   end
 
   def install
-    libnet = Formula['libnet'].opt_lib
-
-    args = ['..'] + std_cmake_args + [
-      "-DINSTALL_SYSCONFDIR=#{etc}",
-      '-DENABLE_GTK=OFF',
-      "-DHAVE_LIBNET:FILEPATH=#{libnet}/libnet.dylib"
-    ]
+    args = [".."] + std_cmake_args
+    args << "-DINSTALL_SYSCONFDIR=" + etc
+    args << ("-DCMAKE_BUILD_TYPE=" +  ((build.include? "enable-debug") ? "Debug" : "Release"))
+    args << "-DCMAKE_INSTALL_PREFIX=" + prefix
+    args << "-DENABLE_CURSES=OFF" if build.include? "without-curses"
+    args << "-DENABLE_PLUGINS=OFF" if build.include? "wihtout-plugins"
+    args << "-DENABLE_IPV6=ON" if build.include? "with-ipv6"
+    args << "-DENABLE_GTK=OFF" if build.without? "gtk+"
+    args << "-DENABLE_LUA=ON" if build.with? "luajit" 
 
     mkdir "build" do
       system "cmake", *args
