292a21788a786090ce7d19c7f741accd225292a1
hydrajump <wave@hydrajump.com>
2014-03-09 16:54:52 +0100

    Replaced single quotes with double quotes

--- Library/Formula/awscli.rb
+++ Library/Formula/awscli.rb
@@ -1,87 +1,87 @@
-require 'formula'
+require "formula"
 
 class Awscli < Formula
-  homepage 'https://aws.amazon.com/cli/'
-  url 'https://pypi.python.org/packages/source/a/awscli/awscli-1.3.0.tar.gz'
-  sha1 'c19800087168a93fc04b32b58a3ae5cfb8cc1d97'
+  homepage "https://aws.amazon.com/cli/"
+  url "https://pypi.python.org/packages/source/a/awscli/awscli-1.3.0.tar.gz"
+  sha1 "c19800087168a93fc04b32b58a3ae5cfb8cc1d97"
 
   head do
-    url 'https://github.com/aws/aws-cli.git', :branch => :develop
+    url "https://github.com/aws/aws-cli.git", :branch => :develop
 
-    resource 'botocore' do
-      url 'https://github.com/boto/botocore.git', :branch => :develop
+    resource "botocore" do
+      url "https://github.com/boto/botocore.git", :branch => :develop
     end
 
-    resource 'bcdoc' do
-      url 'https://github.com/boto/bcdoc.git', :branch => :develop
+    resource "bcdoc" do
+      url "https://github.com/boto/bcdoc.git", :branch => :develop
     end
 
-    resource 'jmespath' do
-      url 'https://github.com/boto/jmespath.git', :branch => :develop
+    resource "jmespath" do
+      url "https://github.com/boto/jmespath.git", :branch => :develop
     end
   end
 
   depends_on :python
 
-  resource 'botocore' do
-    url 'https://pypi.python.org/packages/source/b/botocore/botocore-0.34.0.tar.gz'
-    sha1 '67740a07b25816a7ead70376be5946748acef76b'
+  resource "botocore" do
+    url "https://pypi.python.org/packages/source/b/botocore/botocore-0.34.0.tar.gz"
+    sha1 "67740a07b25816a7ead70376be5946748acef76b"
   end
 
-  resource 'bcdoc' do
-    url 'https://pypi.python.org/packages/source/b/bcdoc/bcdoc-0.12.2.tar.gz'
-    sha1 '31b2a714c2803658d9d028c8edf4623fd0daaf18'
+  resource "bcdoc" do
+    url "https://pypi.python.org/packages/source/b/bcdoc/bcdoc-0.12.2.tar.gz"
+    sha1 "31b2a714c2803658d9d028c8edf4623fd0daaf18"
   end
 
-  resource 'six' do
-    url 'https://pypi.python.org/packages/source/s/six/six-1.5.2.tar.gz'
-    sha1 '90128862139a79d10cf213b3baad9c6e2f72fc78'
+  resource "six" do
+    url "https://pypi.python.org/packages/source/s/six/six-1.5.2.tar.gz"
+    sha1 "90128862139a79d10cf213b3baad9c6e2f72fc78"
   end
 
-  resource 'colorama' do
-    url 'https://pypi.python.org/packages/source/c/colorama/colorama-0.2.5.tar.gz'
-    sha1 '87507210c5a7d400b27d23e8dd42734198663d66'
+  resource "colorama" do
+    url "https://pypi.python.org/packages/source/c/colorama/colorama-0.2.5.tar.gz"
+    sha1 "87507210c5a7d400b27d23e8dd42734198663d66"
   end
 
-  resource 'docutils' do
-    url 'https://pypi.python.org/packages/source/d/docutils/docutils-0.11.tar.gz'
-    sha1 '3894ebcbcbf8aa54ce7c3d2c8f05460544912d67'
+  resource "docutils" do
+    url "https://pypi.python.org/packages/source/d/docutils/docutils-0.11.tar.gz"
+    sha1 "3894ebcbcbf8aa54ce7c3d2c8f05460544912d67"
   end
 
-  resource 'rsa' do
-    url 'https://bitbucket.org/sybren/python-rsa/get/version-3.1.2.tar.gz'
-    sha1 '6a7515221e50ee87cfb54cb36e96f2a39df9badd'
+  resource "rsa" do
+    url "https://bitbucket.org/sybren/python-rsa/get/version-3.1.2.tar.gz"
+    sha1 "6a7515221e50ee87cfb54cb36e96f2a39df9badd"
   end
 
   def install
-    ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
+    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
     install_args = [ "setup.py", "install", "--prefix=#{libexec}" ]
 
     if build.head? then
-      resource('jmespath').stage { system "python", *install_args }
+      resource("jmespath").stage { system "python", *install_args }
     end
 
-    resource('botocore').stage { system "python", *install_args }
-    resource('bcdoc').stage { system "python", *install_args }
-    resource('six').stage { system "python", *install_args }
-    resource('colorama').stage { system "python", *install_args }
-    resource('docutils').stage { system "python", *install_args }
-    resource('rsa').stage { system "python", *install_args }
+    resource("botocore").stage { system "python", *install_args }
+    resource("bcdoc").stage { system "python", *install_args }
+    resource("six").stage { system "python", *install_args }
+    resource("colorama").stage { system "python", *install_args }
+    resource("docutils").stage { system "python", *install_args }
+    resource("rsa").stage { system "python", *install_args }
 
     system "python", "setup.py", "install", "--prefix=#{prefix}",
       "--single-version-externally-managed", "--record=installed.txt"
 
     # Install zsh completion
-    zsh_completion.install 'bin/aws_zsh_completer.sh' => '_aws'
+    zsh_completion.install "bin/aws_zsh_completer.sh" => "_aws"
 
     # Install the examples
-    (share+'awscli').install 'awscli/examples'
+    (share+"awscli").install "awscli/examples"
 
-    bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV['PYTHONPATH'])
+    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   def caveats; <<-EOS.undent
-    The 'examples' directory has been installed to:
+    The "examples" directory has been installed to:
       #{HOMEBREW_PREFIX}/share/awscli/examples
 
     Add the following to ~/.bashrc to enable bash completion:
