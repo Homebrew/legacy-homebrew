e35274b1b5a68a65009994de2540524ffc8ae3d6
Fabian M <mail.fabianm@gmail.com>
2014-03-09 03:08:13 +0100

    Update ettercap to version 0.8.0

--- Library/Formula/ettercap.rb
+++ Library/Formula/ettercap.rb
@@ -10,7 +10,7 @@ class Ettercap < Formula
   option 'without-curses', 'Install without curses interface'
   option 'without-plugins', 'Install without plugins support'
   option 'with-ipv6', 'Install with IPv6 support'
-  option 'enable-debug', 'Enable debug mode'
+  option 'with-debug', 'Enable debug mode'
   
   depends_on 'cmake' => :build
   depends_on 'ghostscript' => :build
@@ -20,24 +20,31 @@ class Ettercap < Formula
   depends_on 'gtk+' => :optional
   depends_on 'luajit' => :optional
 
-  # 0.8.0 won't build without these patches.
   def patches
     [
+      # Fixes issue #326: redefinition of 'bpf_program', 'bpf_version', 
+      #  and 'bpf_insn' in ec_send.c on Mac OS X
+      # url: https://github.com/Ettercap/ettercap/issues/326
       "https://github.com/Ettercap/ettercap/commit/4aaaa2fb6a4ea98e110548802ed015694244b90e.patch",
+      # Fixes issue #344: undefined symbol safe_free_mem caused by the previous fix.
+      # url: https://github.com/Ettercap/ettercap/issues/344
       "https://github.com/Ettercap/ettercap/commit/33ac95f78e4f6f067e6bc33b8883b3b7daa896f3.patch"
     ]
   end
 
   def install
-    args = [".."] + std_cmake_args
-    args << "-DINSTALL_SYSCONFDIR=" + etc
-    args << ("-DCMAKE_BUILD_TYPE=" +  ((build.include? "enable-debug") ? "Debug" : "Release"))
-    args << "-DCMAKE_INSTALL_PREFIX=" + prefix
-    args << "-DENABLE_CURSES=OFF" if build.include? "without-curses"
-    args << "-DENABLE_PLUGINS=OFF" if build.include? "wihtout-plugins"
-    args << "-DENABLE_IPV6=ON" if build.include? "with-ipv6"
+    build_type = build.with? "debug" ? "Debug" : "Release" 
+
+    args = std_cmake_args
+    args << "-DINSTALL_SYSCONFDIR=#{etc}"
+    args << ("-DCMAKE_BUILD_TYPE=#{build_type}"
+    args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+    args << "-DENABLE_CURSES=OFF" if build.without? "curses"
+    args << "-DENABLE_PLUGINS=OFF" if build.without? "plugins"
+    args << "-DENABLE_IPV6=ON" if build.with? "ipv6"
     args << "-DENABLE_GTK=OFF" if build.without? "gtk+"
-    args << "-DENABLE_LUA=ON" if build.with? "luajit" 
+    args << "-DENABLE_LUA=ON" if build.with? "luajit"
+    args << ".."
 
     mkdir "build" do
       system "cmake", *args
@@ -45,3 +52,4 @@ class Ettercap < Formula
     end
   end
 end
+
