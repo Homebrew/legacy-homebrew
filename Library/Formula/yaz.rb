57b6af40bfd302ef33fffd06ecd2b0598bd6eab4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-10 09:22:04 +0000

    yaz: add 5.0.18 bottle.
    Closes #27374.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -5,6 +5,13 @@ class Yaz < Formula
   url 'http://ftp.indexdata.dk/pub/yaz/yaz-5.0.18.tar.gz'
   sha1 '2e273f38b90597298ecc9b254a92367158582aa6'
 
+  bottle do
+    cellar :any
+    sha1 "077badcd55b5b7fb3de0dee94792264bdfe8fbb9" => :mavericks
+    sha1 "d61a12ae149b31950eb00cd8dbe21e6bfba6e2aa" => :mountain_lion
+    sha1 "eba1bc0073340ba998081ed8113eb487d8f254db" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'gnutls' => :optional
 
