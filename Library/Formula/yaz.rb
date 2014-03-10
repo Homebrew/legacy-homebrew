6820a4e3d013a2ba6098aee0354ec9b04f988643
Misty De Meo <mistydemeo@gmail.com>
2014-03-10 10:27:24 -0700

    yaz: add test

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 require 'formula'
 
 class Yaz < Formula
@@ -20,5 +21,16 @@ class Yaz < Formula
                           "--prefix=#{prefix}",
                           "--with-xml2"
     system "make install"
+  end
+
+  # This test converts between MARC8, an obscure mostly-obsolete library
+  # text encoding supported by yaz-iconv, and UTF8.
+  test do
+    marc8 = File.open('marc8.txt', 'w') do |f|
+      f.write '$1!0-!L,i$3i$si$Ki$Ai$O!+=(B'
+    end
+
+    result = `"#{bin}/yaz-iconv" -f marc8 -t utf8 marc8.txt`.chomp
+    assert_equal "世界こんにちは！", result
   end
 end
