1a3ea5dc61b2ededd74f8883a373a72962ae2099
Jack Nagel <jacknagel@gmail.com>
2014-03-10 14:56:02 -0500

    Update fetch command for generalized bottle implementation

--- Library/Homebrew/cmd/fetch.rb
+++ Library/Homebrew/cmd/fetch.rb
@@ -17,11 +17,23 @@ module Homebrew extend self
 
     puts "Fetching: #{bucket * ', '}" if bucket.size > 1
     bucket.each do |f|
-      fetch_formula(f)
-      f.resources.each do |r|
-        fetch_resource(r)
+      if fetch_bottle?(f)
+        fetch_formula(f.bottle)
+      else
+        fetch_formula(f)
+        f.resources.each do |r|
+          fetch_resource(r)
+        end
       end
     end
+  end
+
+  def fetch_bottle? f
+    return true if ARGV.force_bottle? && f.bottle
+    return false unless f.bottle && f.pour_bottle?
+    return false if ARGV.build_from_source? || ARGV.build_bottle?
+    return false unless f.bottle.compatible_cellar?
+    return true
   end
 
   def fetch_resource r
