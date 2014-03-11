bfa713582cc11c1d27fa4a6407ac1fa7f75eee86
Alex Gaynor <alex.gaynor@gmail.com>
2014-03-11 09:40:09 -0700

    Use the correct make target

--- Library/Formula/libscrypt.rb
+++ Library/Formula/libscrypt.rb
@@ -6,7 +6,7 @@ class Libscrypt < Formula
   sha1 "ff6b0cda00afcb582ca57699322b94166e5f3ab6"
 
   def install
-    system "make", "install", "PREFIX=#{prefix}", "LDFLAGS=", "CFLAGS_EXTRA="
+    system "make", "install-osx", "PREFIX=#{prefix}", "LDFLAGS=", "CFLAGS_EXTRA="
     system "make", "check", "LDFLAGS=", "CFLAGS_EXTRA="
   end
 end
