16693998805045a76cf66425154234f1e7979141
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-11 22:42:58 +0000

    ngrep: add 1.45 bottle.
    Closes #27448.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ngrep.rb
+++ Library/Formula/ngrep.rb
@@ -5,6 +5,13 @@ class Ngrep < Formula
   url 'https://downloads.sourceforge.net/project/ngrep/ngrep/1.45/ngrep-1.45.tar.bz2'
   sha1 'f26090a6ac607db66df99c6fa9aef74968f3330f'
 
+  bottle do
+    cellar :any
+    sha1 "4954b0c218b60337906b83e39fed7e80252e1890" => :mavericks
+    sha1 "3fa3364523d8027c7222e10f66d6509b6d05bf29" => :mountain_lion
+    sha1 "83a26db0ace21d12c72bb1fbb3a8435c726ff9b7" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
