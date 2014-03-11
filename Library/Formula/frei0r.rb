f1614c140c3e438b01b304e5e2aa0070eba700de
Mike McQuaid <mike@mikemcquaid.com>
2014-03-11 11:16:30 +0000

    frei0r: use brewed autoconf everywhere.
    Closes #27377.

--- Library/Formula/frei0r.rb
+++ Library/Formula/frei0r.rb
@@ -5,7 +5,7 @@ class Frei0r < Formula
   url 'https://files.dyne.org/.xsend.php?file=frei0r/releases/frei0r-plugins-1.4.tar.gz'
   sha1 '7995d06c5412b14fa3b05647084ca9d7a0c84faa'
 
-  depends_on :autoconf
+  depends_on "autoconf"
 
   def install
     system "./configure", "--disable-dependency-tracking",
