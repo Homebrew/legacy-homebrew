46f1141041155393c51d98df9c4a05b447a6b3a3
Alan Pearce <alan@bulbstudios.com>
2014-03-11 15:50:38 +0000

    mysql-connector-c++: use Requirement for MySQL depedency

--- Library/Formula/mysql-connector-c++.rb
+++ Library/Formula/mysql-connector-c++.rb
@@ -5,6 +5,13 @@ class MysqlConnectorCxx < Formula
   url 'http://mysql.he.net/Downloads/Connector-C++/mysql-connector-c++-1.1.3.tar.gz'
   sha1 'b817dccf3a4e340b6a972028ceb7eededaaebd6f'
 
+  class MysqlClient < Requirement
+    fatal true
+    default_formula "mysql"
+
+    satisfy { which 'mysql' }
+  end
+
   depends_on 'cmake' => :build
   depends_on 'boost' => :build
   depends_on 'mysql' => :optional
