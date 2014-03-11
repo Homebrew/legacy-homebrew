92ae76f39a10efb5c83e5478bdc64bf3958dbf40
Joe Grund <grundjoseph@gmail.com>
2014-03-11 12:38:33 -0400

    selenium-server-standalone 2.40.0
    selenium-server-standalone 2.40.0
    
    Closes #27428.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/selenium-server-standalone.rb
+++ Library/Formula/selenium-server-standalone.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class SeleniumServerStandalone < Formula
   homepage 'http://seleniumhq.org/'
-  url 'https://selenium.googlecode.com/files/selenium-server-standalone-2.39.0.jar'
-  sha1 'f2391600481dd285002d04b66916fc4286ff70ce'
+  url 'https://selenium-release.storage.googleapis.com/2.40/selenium-server-standalone-2.40.0.jar'
+  sha1 '165f54ca53e5eefc6f27c26ac03d18bae99a544e'
 
   def install
     libexec.install "selenium-server-standalone-#{version}.jar"
