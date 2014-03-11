b64b9acdd2dba3ec45e8d6498361207c32611194
Alex Gaynor <alex.gaynor@gmail.com>
2014-03-11 08:53:59 -0700

    Updated for libscrypt 1.18

--- Library/Formula/libscrypt.rb
+++ Library/Formula/libscrypt.rb
@@ -2,15 +2,11 @@ require "formula"
 
 class Libscrypt < Formula
   homepage "https://lolware.net/libscrypt.html"
-  url "https://github.com/technion/libscrypt/archive/v1.17.tar.gz"
-  sha1 "27a36e4f7ae35f44a4bb8e8f922883a9323d9b9c"
+  url "https://github.com/technion/libscrypt/archive/v1.18.tar.gz"
+  sha1 "ff6b0cda00afcb582ca57699322b94166e5f3ab6"
 
   def install
     system "make", "install", "PREFIX=#{prefix}", "LDFLAGS=", "CFLAGS_EXTRA="
     system "make", "check", "LDFLAGS=", "CFLAGS_EXTRA="
-    # libscrypt builds a shared object in the non-OS X style (.so.0), so we
-    # rename it to the OS X convention of using .dylib. See
-    # https://github.com/technion/libscrypt/issues/12
-    system "mv", "#{lib}/libscrypt.so.0", "#{lib}/libscrypt.dylib"
   end
 end
