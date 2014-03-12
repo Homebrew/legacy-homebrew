524188e635abfc06e23bdaef4d44880d6dccdae8
tduehr <tduehr@gmail.com>
2014-03-12 12:11:54 -0500

    adds union output option for brew deps

--- Library/Homebrew/cmd/deps.rb
+++ Library/Homebrew/cmd/deps.rb
@@ -8,6 +8,7 @@ module Homebrew extend self
       :tree?       => ARGV.include?('--tree'),
       :all?        => ARGV.include?('--all'),
       :topo_order? => ARGV.include?('-n')
+      :union?      => ARGV.include?('--union')
     )
 
     if mode.installed? && mode.tree?
