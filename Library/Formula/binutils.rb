4a148131cb5f36ecdc566422d95dc16c559b77db
denji <denji0k@gmail.com>
2014-03-12 09:44:40 +0200

    binutils: libiberty: fix --enable-install-libiberty flag

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -6,6 +6,11 @@ class Binutils < Formula
   mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
   sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
 
+  def patches
+    # [PATCH] libiberty: fix --enable-install-libiberty flag
+    'http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo/src/patchsets/binutils/2.24/20_all_libiberty-install.patch'
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
@@ -17,6 +22,7 @@ class Binutils < Formula
                           "--enable-interwork",
                           "--enable-multilib",
                           "--enable-64-bit-bfd",
+                          "--enable-install-libiberty",
                           "--enable-targets=all"
     system "make"
     system "make install"
