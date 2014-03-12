b9afb7b1eae479918dd017c828758c71876b91c4
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2014-03-12 14:54:33 +0900

    cmigemo: add HEAD build
    Closes #27460.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/cmigemo.rb
+++ Library/Formula/cmigemo.rb
@@ -4,12 +4,13 @@ class Cmigemo < Formula
   homepage 'http://www.kaoriya.net/software/cmigemo'
   url 'https://cmigemo.googlecode.com/files/cmigemo-default-src-20110227.zip'
   sha1 '25e279c56d3a8f1e82cbfb3526d1b38742d1d66c'
+  head 'https://github.com/koron/cmigemo.git'
 
   depends_on 'nkf' => :build
 
   # Patch per discussion at: https://github.com/Homebrew/homebrew/pull/7005
   def patches
-    DATA
+    DATA if build.stable?
   end
 
   def install
@@ -17,15 +18,17 @@ class Cmigemo < Formula
     system "./configure", "--prefix=#{prefix}"
     system "make osx"
     system "make osx-dict"
-    cd 'dict' do
-      system "make utf-8"
+    if build.stable?
+      cd 'dict' do
+        system "make utf-8"
+      end
     end
     ENV.j1 # Install can fail on multi-core machines unless serialized
     system "make osx-install"
   end
 
   def caveats; <<-EOS.undent
-    See also https://gist.github.com/457761 to use cmigemo with Emacs.
+    See also https://github.com/emacs-jp/migemo to use cmigemo with Emacs.
     You will have to save as migemo.el and put it in your load-path.
     EOS
   end
