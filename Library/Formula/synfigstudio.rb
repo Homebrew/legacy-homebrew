170d1723c5433ca56a6f4afee909e0f11e7ac52a
Adam Vandenberg <flangy@gmail.com>
2014-03-12 21:40:08 -0700

    synfigstudio: apply candidate patch for Xcode 5
    Closes #25033.

--- Library/Formula/synfigstudio.rb
+++ Library/Formula/synfigstudio.rb
@@ -13,6 +13,12 @@ class Synfigstudio < Formula
   depends_on 'etl'
   depends_on 'synfig'
 
+  def patches
+    # Candidate upstream patch for Xcode 5
+    # http://www.synfig.org/issues/thebuggenie/synfig/issues/504
+    "http://www.synfig.org/issues/thebuggenie/files/show/80"
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
