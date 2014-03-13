9b565f0ababa9dcf657d8eac6bd766749dedf9d5
lalyos <lalyos@yhoo.com>
2014-03-13 21:07:36 +0100

    boot2docker 0.7.0
    Closes #27523.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -3,8 +3,8 @@ require "formula"
 class Boot2docker < Formula
   homepage "https://github.com/boot2docker/boot2docker"
   head "https://github.com/boot2docker/boot2docker.git"
-  url 'https://github.com/boot2docker/boot2docker/archive/v0.6.0.tar.gz'
-  sha1 'e90ed901375a345e23685aa8e0f9e3b83db11eff'
+  url 'https://github.com/boot2docker/boot2docker/archive/v0.7.0.tar.gz'
+  sha1 'd61dc8cdd3b0fd8ce8f8443c4d621f0f87220b0f'
 
   depends_on "docker" => :recommended
 
