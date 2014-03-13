4c4c61cba19aeed0b9140ce044d49873801aa29b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-13 20:14:00 +0000

    s-lang: add 2.2.4 bottle.

--- Library/Formula/s-lang.rb
+++ Library/Formula/s-lang.rb
@@ -6,6 +6,12 @@ class SLang < Formula
   mirror 'http://pkgs.fedoraproject.org/repo/pkgs/slang/slang-2.2.4.tar.bz2/7fcfd447e378f07dd0c0bae671fe6487/slang-2.2.4.tar.bz2'
   sha1 '34e68a993888d0ae2ebc7bc31b40bc894813a7e2'
 
+  bottle do
+    sha1 "312ff6dd9da406831aeeaeee312317f759e0b4a2" => :mavericks
+    sha1 "69df7a58e958882a2a10d8b4f7e170e2de06809b" => :mountain_lion
+    sha1 "7181f19055794dc55e27ad29d89e5d872d5e7e40" => :lion
+  end
+
   depends_on :libpng
   depends_on 'pcre' => :optional
   depends_on 'oniguruma' => :optional
