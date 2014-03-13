22c466b76c3c85eb4f5d0207b2f79442a4d4f1ec
Jack Nagel <jacknagel@gmail.com>
2014-03-13 10:11:00 -0500

    Make FormulaInstaller setup more readable

--- Library/Homebrew/cmd/install.rb
+++ Library/Homebrew/cmd/install.rb
@@ -104,17 +104,17 @@ module Homebrew extend self
 
   def install_formula f
     fi = FormulaInstaller.new(f)
-    fi.options = f.build.used_options
-    fi.ignore_deps = ARGV.ignore_deps? || ARGV.interactive?
-    fi.only_deps = ARGV.only_deps?
-    fi.build_bottle = ARGV.build_bottle?
-    fi.build_from_source = ARGV.build_from_source?
-    fi.force_bottle = ARGV.force_bottle?
-    fi.interactive = ARGV.interactive?
-    fi.interactive &&= :git if ARGV.flag? "--git"
-    fi.verbose = ARGV.verbose?
-    fi.verbose &&= :quieter if ARGV.quieter?
-    fi.debug = ARGV.debug?
+    fi.options             = f.build.used_options
+    fi.ignore_deps         = ARGV.ignore_deps? || ARGV.interactive?
+    fi.only_deps           = ARGV.only_deps?
+    fi.build_bottle        = ARGV.build_bottle?
+    fi.build_from_source   = ARGV.build_from_source?
+    fi.force_bottle        = ARGV.force_bottle?
+    fi.interactive         = ARGV.interactive?
+    fi.interactive       &&= :git if ARGV.flag? "--git"
+    fi.verbose             = ARGV.verbose?
+    fi.verbose           &&= :quieter if ARGV.quieter?
+    fi.debug               = ARGV.debug?
     fi.prelude
     fi.install
     fi.caveats
