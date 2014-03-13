293d28fc61d61dbf097bfd4fa3cb9028ab415fd8
John Claus <johnclaus@gmail.com>
2014-03-13 12:02:34 -0700

    ec2-api-tools 1.6.12.2
    Closes #27519.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ec2ApiTools < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/351'
-  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.6.12.0.zip'
-  sha1 'eb0732df59a2ad2c1ad3fa5839e846b69afad697'
+  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.6.12.2.zip'
+  sha1 '96bea739ee32326c6826fe22ab778320fde841a4'
 
   def install
     standard_install
