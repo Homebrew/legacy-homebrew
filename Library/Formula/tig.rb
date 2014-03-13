f94cdb86efa2327e7a74c095c90a395ed10e6c60
Jack Nagel <jacknagel@gmail.com>
2014-03-13 17:10:48 -0500

    tig: don't apply patches to HEAD

--- Library/Formula/tig.rb
+++ Library/Formula/tig.rb
@@ -16,10 +16,10 @@ class Tig < Formula
   # fixes the problem with displaying/searching multibyte characters
   # upstream pull request: https://github.com/jonas/tig/issues/99
   def patches
-  [
-    "https://github.com/spin6lock/tig/commit/159eff692b24aa05f766f76922b0f2515fbbf415.diff",
-    "https://github.com/spin6lock/tig/commit/70cb91e7d210b92ab012d9819bd20ebe92885bd6.diff"
-  ]
+    [
+      "https://github.com/spin6lock/tig/commit/159eff692b24aa05f766f76922b0f2515fbbf415.diff",
+      "https://github.com/spin6lock/tig/commit/70cb91e7d210b92ab012d9819bd20ebe92885bd6.diff"
+    ] unless build.head?
   end
 
   def install
