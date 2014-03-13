f3c55e60fed89a870fa1ea6764dfd66098eee36a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-13 21:43:32 +0000

    pv: add 1.5.2 bottle.
    Closes #27526.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pv.rb
+++ Library/Formula/pv.rb
@@ -5,6 +5,12 @@ class Pv < Formula
   url 'http://www.ivarch.com/programs/sources/pv-1.5.2.tar.bz2'
   sha1 '0ca480d0ef2400bb9ebe9a992e011a9f004f42b8'
 
+  bottle do
+    sha1 "13db5f53546f554a1a1cef88f763022e0ba08974" => :mavericks
+    sha1 "96bedf90d0688c722e7fe1743230e74713089ef9" => :mountain_lion
+    sha1 "cea67d8b62b936e80dfa80fe7d84706bbd258e29" => :lion
+  end
+
   depends_on 'gettext'
 
   fails_with :llvm do
