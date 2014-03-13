5e76acf7edbc20e3ce915daa98729c5f4169272a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-13 20:13:59 +0000

    sdl2: add 2.0.2 bottle.

--- Library/Formula/sdl2.rb
+++ Library/Formula/sdl2.rb
@@ -5,6 +5,13 @@ class Sdl2 < Formula
   url 'http://libsdl.org/release/SDL2-2.0.2.tar.gz'
   sha1 '304c7cd3dddca98724a3e162f232a8a8f6e1ceb3'
 
+  bottle do
+    cellar :any
+    sha1 "2b6f9e9534325cf8f5c558f11be96cf5b438001b" => :mavericks
+    sha1 "32697d2b975224d24232a12cae5976de70d735c1" => :mountain_lion
+    sha1 "7de4c31f1b527ecdaa9704b8e385b377f5285e6e" => :lion
+  end
+
   head do
     url 'http://hg.libsdl.org/SDL', :using => :hg
 
