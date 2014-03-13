d1256416966edc78c04a225165aefce3a39937f0
blogabe <blogabe@rajab.us>
2014-03-12 22:14:07 -0700

    Update cspice.rb

--- Library/Formula/cspice.rb
+++ Library/Formula/cspice.rb
@@ -4,10 +4,10 @@ class Cspice < Formula
   homepage "http://naif.jpl.nasa.gov/naif/index.html"
   url "http://naif.jpl.nasa.gov/pub/naif/toolkit//C/MacIntel_OSX_AppleC_64bit/packages/cspice.tar.Z"
   sha1 "e5546a72a2d0c7e337850a10d208014efb57d78d"
-  version '64'
+  version "64"
 
   def install
-    cp_r "./", "#{prefix}"
+    prefix.install Dir["*"]
     cp_r "#{lib}/cspice.a", "#{lib}/libcspice.a"
   end
 end
