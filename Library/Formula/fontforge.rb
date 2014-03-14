bcb996bdbf362eebbcc595fb427b7b83613d9e6e
Jack Nagel <jacknagel@gmail.com>
2014-03-13 21:42:11 -0500

    fontforge: use checksummed patches

--- Library/Formula/fontforge.rb
+++ Library/Formula/fontforge.rb
@@ -9,6 +9,12 @@ class Fontforge < Formula
 
     depends_on 'cairo' => :optional
     depends_on 'pango' => :optional
+
+    # Fixes double defined AnchorPoint on Mountain Lion 10.8.2
+    patch do
+      url "https://gist.github.com/rubenfonseca/5078149/raw/98a812df4e8c50d5a639877bc2d241e5689f1a14/fontforge"
+      sha1 "baa7d60f4c6e672180e66438ee675b4ee0fda5ce"
+    end
   end
 
   head do
@@ -42,13 +48,6 @@ class Fontforge < Formula
   fails_with :llvm do
     build 2336
     cause "Compiling cvexportdlg.c fails with error: initializer element is not constant"
-  end
-
-  def patches
-    unless build.head?
-      # Fixes double defined AnchorPoint on Mountain Lion 10.8.2
-      "https://gist.github.com/rubenfonseca/5078149/raw/98a812df4e8c50d5a639877bc2d241e5689f1a14/fontforge"
-    end
   end
 
   def install
