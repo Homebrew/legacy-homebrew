5b9cd02ae52ac2605c760a664850a84df1277478
Jack Nagel <jacknagel@gmail.com>
2014-03-14 10:03:24 -0500

    iodine: use checksummed patches

--- Library/Formula/iodine.rb
+++ Library/Formula/iodine.rb
@@ -5,9 +5,10 @@ class Iodine < Formula
   url 'http://code.kryo.se/iodine/iodine-0.6.0-rc1.tar.gz'
   sha1 '4fa9a248b8a84df8a727a5d749e669e58136edca'
 
-  def patches
-    # 10.8 compatibility; see https://github.com/yarrick/iodine/pull/1
-    "https://github.com/zschoche/iodine/commit/e1e438497a83dbe6800212a0e5cb632907d1b3d9.patch"
+  # 10.8 compatibility; see https://github.com/yarrick/iodine/pull/1
+  patch do
+    url "https://github.com/zschoche/iodine/commit/e1e438497a83dbe6800212a0e5cb632907d1b3d9.patch"
+    sha1 "207140143903db61c78ca1dcdc807ac2314b5aff"
   end
 
   def install
