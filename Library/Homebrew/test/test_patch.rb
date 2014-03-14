df6fc206559060b72a29462b674279231a12509c
Jack Nagel <jacknagel@gmail.com>
2014-03-13 19:51:23 -0500

    Enable new patch implementation with compatibility layer

--- Library/Homebrew/test/test_patch.rb
+++ Library/Homebrew/test/test_patch.rb
@@ -50,3 +50,66 @@ class PatchTests < Test::Unit::TestCase
     assert_raises(ArgumentError) { Patch.create(Object.new) }
   end
 end
+
+class LegacyPatchTests < Test::Unit::TestCase
+  def test_patch_single_string
+    patches = Patch.normalize_legacy_patches("http://example.com/patch.diff")
+    assert_equal 1, patches.length
+    assert_equal :p1, patches.first.strip
+  end
+
+  def test_patch_array
+    patches = Patch.normalize_legacy_patches(
+      %w{http://example.com/patch1.diff http://example.com/patch2.diff}
+    )
+
+    assert_equal 2, patches.length
+    assert_equal :p1, patches[0].strip
+    assert_equal :p1, patches[1].strip
+  end
+
+  def test_p0_hash_to_string
+    patches = Patch.normalize_legacy_patches(
+      :p0 => "http://example.com/patch.diff"
+    )
+
+    assert_equal 1, patches.length
+    assert_equal :p0, patches.first.strip
+  end
+
+  def test_p1_hash_to_string
+    patches = Patch.normalize_legacy_patches(
+      :p1 => "http://example.com/patch.diff"
+    )
+
+    assert_equal 1, patches.length
+    assert_equal :p1, patches.first.strip
+  end
+
+  def test_mixed_hash_to_strings
+    patches = Patch.normalize_legacy_patches(
+      :p1 => "http://example.com/patch1.diff",
+      :p0 => "http://example.com/patch0.diff"
+    )
+    assert_equal 2, patches.length
+    assert_equal 1, patches.select { |p| p.strip == :p0 }.length
+    assert_equal 1, patches.select { |p| p.strip == :p1 }.length
+  end
+
+  def test_mixed_hash_to_arrays
+    patches = Patch.normalize_legacy_patches(
+      :p1 => ["http://example.com/patch10.diff",
+              "http://example.com/patch11.diff"],
+      :p0 => ["http://example.com/patch00.diff",
+              "http://example.com/patch01.diff"]
+    )
+
+    assert_equal 4, patches.length
+    assert_equal 2, patches.select { |p| p.strip == :p0 }.length
+    assert_equal 2, patches.select { |p| p.strip == :p1 }.length
+  end
+
+  def test_nil
+    assert_empty Patch.normalize_legacy_patches(nil)
+  end
+end
