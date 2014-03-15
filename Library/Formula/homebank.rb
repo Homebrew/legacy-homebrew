dab2dade7c03e8d36863e15a31a4324420d38a95
aas <alexander.ashurkov@gmail.com>
2014-03-15 14:13:51 +0600

    Homebank 4.5.6
    Closes #27575.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/homebank.rb
+++ Library/Formula/homebank.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Homebank < Formula
   homepage 'http://homebank.free.fr'
-  url 'http://homebank.free.fr/public/homebank-4.5.5.tar.gz'
-  sha1 '1867c1d1671a3f1ca16f660b8169d5255e440d93'
+  url 'http://homebank.free.fr/public/homebank-4.5.6.tar.gz'
+  sha1 '2026b5dba47ff60d893057d270b07931e126af78'
 
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
