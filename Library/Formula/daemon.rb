40605c954a78e9eef805505267f66b55eb4824bf
Brett Koonce <koonce@gmail.com>
2014-03-15 15:10:17 -0500

    notes

--- Library/Formula/daemon.rb
+++ Library/Formula/daemon.rb
@@ -6,7 +6,8 @@ class Daemon < Formula
   sha1 'fa6298f05f868d54660a7ed70c05fb7a0963a24b'
 
   def patches
-      {:p1 => "https://trac.macports.org/raw-attachment/ticket/42845/daemon-0.6.4-ignore-strlcpy-strlcat.patch"}
+    # fixes for mavericks strlcpy/strlcat: https://trac.macports.org/ticket/42845
+    {:p1 => "https://trac.macports.org/raw-attachment/ticket/42845/daemon-0.6.4-ignore-strlcpy-strlcat.patch"} if MacOS.version == :mavericks
   end
 
   def install
