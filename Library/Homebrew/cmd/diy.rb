7ebea8e54cfabc807e4f92e2bf3c705b24061444
Jack Nagel <jacknagel@gmail.com>
2014-03-15 12:55:14 -0500

    Modernize `brew diy`

--- Library/Homebrew/cmd/diy.rb
+++ Library/Homebrew/cmd/diy.rb
@@ -1,31 +1,35 @@
 module Homebrew extend self
   def diy
+    %w[name version].each do |opt|
+      if ARGV.include? "--set-#{opt}"
+        opoo "--set-#{opt} is deprecated, please use --#{opt}=<#{opt}> instead"
+      end
+    end
+
     path = Pathname.getwd
 
-    version = if ARGV.include? '--set-version'
+    version = ARGV.value "version"
+    version ||= if ARGV.include? "--set-version"
       ARGV.next
     elsif path.version.to_s.empty?
-      raise "Couldn't determine version, try --set-version"
+      raise "Couldn't determine version, set it with --version=<version>"
     else
       path.version
     end
 
-    name = if ARGV.include? '--set-name'
+    name = ARGV.value "name"
+    name ||= if ARGV.include? "--set-name"
       ARGV.next
     else
-      path.basename.to_s =~ /(.*?)-?#{version}/
-      if $1.to_s.empty?
-        path.basename
-      else
-        $1
-      end
+      basename = path.basename.to_s
+      basename[/(.*?)-?#{Regexp.escape(version)}/, 1] || basename
     end
 
     prefix = HOMEBREW_CELLAR/name/version
 
-    if File.file? 'CMakeLists.txt'
+    if File.file? "CMakeLists.txt"
       puts "-DCMAKE_INSTALL_PREFIX=#{prefix}"
-    elsif File.file? 'Makefile.am'
+    elsif File.file? "Makefile.am"
       puts "--prefix=#{prefix}"
     else
       raise "Couldn't determine build system"
