ac23e247f39e25ae4e8de4a5edb06f4aaa2f7e50
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-15 08:25:43 +0000

    homebank: add 4.5.6 bottle.
    Closes #27575.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/homebank.rb
+++ Library/Formula/homebank.rb
@@ -5,6 +5,12 @@ class Homebank < Formula
   url 'http://homebank.free.fr/public/homebank-4.5.6.tar.gz'
   sha1 '2026b5dba47ff60d893057d270b07931e126af78'
 
+  bottle do
+    sha1 "284f95d645b270efd60631cddef64cb6fe5e4a50" => :mavericks
+    sha1 "11047cc6cebe54c5cf79128272cb505701f41776" => :mountain_lion
+    sha1 "f391c15f37190b3c817085eab4ad6a79210da106" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
   depends_on 'gettext'
