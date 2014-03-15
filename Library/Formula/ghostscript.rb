2c6eabd8d589440ff515f2302a2dd61c8425d346
Jack Nagel <jacknagel@gmail.com>
2014-03-14 19:16:33 -0500

    ghostscript: fix patching file that differs between stable and HEAD
    This is fragile but unfortunately it's required.
    
    Fixes #27550.

--- Library/Formula/ghostscript.rb
+++ Library/Formula/ghostscript.rb
@@ -2,8 +2,13 @@ require 'formula'
 
 class Ghostscript < Formula
   homepage 'http://www.ghostscript.com/'
-  url 'http://downloads.ghostscript.com/public/ghostscript-9.10.tar.gz'
-  sha1 '29d6538ae77565c09f399b06455e94e7bcd83d01'
+
+  stable do
+    url 'http://downloads.ghostscript.com/public/ghostscript-9.10.tar.gz'
+    sha1 '29d6538ae77565c09f399b06455e94e7bcd83d01'
+
+    patch :DATA # Uncomment OS X-specific make vars
+  end
 
   bottle do
     sha1 "be9d9be82c03ac8409994fee0cc638d20ceb145c" => :mavericks
@@ -21,6 +26,12 @@ class Ghostscript < Formula
     depends_on :autoconf
     depends_on :automake
     depends_on :libtool
+
+    # Uncomment OS X-specific make vars
+    patch do
+      url "https://gist.githubusercontent.com/jacknagel/9559501/raw/9709b3234cc888d29f717838650d29e7062da917/gs.patch"
+      sha1 "65c99df4f0d57368a086154d34722f5c4b9c84cc"
+    end
   end
 
   option 'with-djvu', 'Build drivers for DjVU file format'
@@ -47,11 +58,6 @@ class Ghostscript < Formula
   resource 'djvu' do
     url 'https://downloads.sourceforge.net/project/djvu/GSDjVu/1.5/gsdjvu-1.5.tar.gz'
     sha1 'c7d0677dae5fe644cf3d714c04b3c2c343906342'
-  end
-
-  # Fix dylib names, per installation instructions
-  def patches
-    DATA
   end
 
   def move_included_source_copies
