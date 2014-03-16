67415d048d6baed77b20152bc23c7dcb03bf1101
Brett Koonce <koonce@gmail.com>
2014-03-16 10:28:43 -0500

    vimpager 1.8.7

--- Library/Formula/vimpager.rb
+++ Library/Formula/vimpager.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vimpager < Formula
   homepage 'https://github.com/rkitover/vimpager'
-  url 'https://github.com/rkitover/vimpager/archive/1.8.5.tar.gz'
-  sha1 '63b6895791dd7263bfb67c574d974d9dda017b1b'
+  url 'https://github.com/rkitover/vimpager/archive/1.8.7.tar.gz'
+  sha1 '796bb69e30a0b87ee65bf29c5ae800a16033f85f'
   head 'https://github.com/rkitover/vimpager.git'
 
   def install
