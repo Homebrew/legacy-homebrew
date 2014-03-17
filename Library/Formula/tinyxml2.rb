b836740134d100e6bfee9d97306b086ab92725ad
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-17 22:18:10 +0000

    tinyxml2: add 2.0.2 bottle.
    Closes #27622.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/tinyxml2.rb
+++ Library/Formula/tinyxml2.rb
@@ -6,6 +6,13 @@ class Tinyxml2 < Formula
   sha1 "c78a4de58540e2a35f4775fd3e577299ebd15117"
   head "https://github.com/leethomason/tinyxml2.git", :branch => "master"
 
+  bottle do
+    cellar :any
+    sha1 "21ac26ad280788da33d3d40b85c51443b8e621bb" => :mavericks
+    sha1 "796b13f7e196897af63cd41e149bea133774a61f" => :mountain_lion
+    sha1 "623579a7a5216e1789f2ef97c6048ccbf349f99b" => :lion
+  end
+
   depends_on "cmake" => :build
 
   def install
