bc8b5bcaf7b3d66d5e5b7021065c5cc42e0d65d8
Jack Nagel <jacknagel@gmail.com>
2014-03-16 21:35:23 -0500

    tcpreplay: use checksummed patches

--- Library/Formula/tcpreplay.rb
+++ Library/Formula/tcpreplay.rb
@@ -5,14 +5,18 @@ class Tcpreplay < Formula
   url 'https://downloads.sourceforge.net/project/tcpreplay/tcpreplay/3.4.4/tcpreplay-3.4.4.tar.gz'
   sha1 '9e4cca81cfbfb919f8759e1a27ce1b3b963ff3b8'
 
-  def patches
-    [
-      # Hard-code use of dylib instead of so
-      DATA,
-      # Use system strlcpy; fixed upstream
-      "https://github.com/synfinatic/tcpreplay/commit/6f45329ba7e6300f07f253032e6feb8a650bea23.patch",
-      "https://github.com/synfinatic/tcpreplay/commit/0983f589a21bbd0d248e9b408bdf5aeacb61ce19.patch",
-    ]
+  # Hard-code use of dylib instead of so
+  patch :DATA
+
+  # Use system strlcpy; fixed upstream
+  patch do
+    url "https://github.com/synfinatic/tcpreplay/commit/6f45329ba7e6300f07f253032e6feb8a650bea23.patch"
+    sha1 "06a8323b7dd8545ea1a629b193b2b1f04535a4a7"
+  end
+
+  patch do
+    url "https://github.com/synfinatic/tcpreplay/commit/0983f589a21bbd0d248e9b408bdf5aeacb61ce19.patch"
+    sha1 "1cb5080a6c032f78b3ac5be28a7e6ac9c4c821da"
   end
 
   def install
