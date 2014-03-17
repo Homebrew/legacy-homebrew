d8cee4fa180630e4c6e67dd544758c8591797d9a
Jack Nagel <jacknagel@gmail.com>
2014-03-17 11:12:54 -0500

    daemon: use checksummed patches

--- Library/Formula/daemon.rb
+++ Library/Formula/daemon.rb
@@ -5,10 +5,11 @@ class Daemon < Formula
   url 'http://libslack.org/daemon/download/daemon-0.6.4.tar.gz'
   sha1 'fa6298f05f868d54660a7ed70c05fb7a0963a24b'
 
-  def patches
-    # fixes for mavericks strlcpy/strlcat: https://trac.macports.org/ticket/42845
-    {:p1 => "https://trac.macports.org/raw-attachment/ticket/42845/daemon-0.6.4-ignore-strlcpy-strlcat.patch"} if MacOS.version == :mavericks
-  end
+  # fixes for mavericks strlcpy/strlcat: https://trac.macports.org/ticket/42845
+  patch do
+    url "https://trac.macports.org/raw-attachment/ticket/42845/daemon-0.6.4-ignore-strlcpy-strlcat.patch"
+    sha1 "8330e7a2df1b8a37b440709c26baf55df251bc56"
+  end if MacOS.version == :mavericks
 
   def install
     system "./config"
