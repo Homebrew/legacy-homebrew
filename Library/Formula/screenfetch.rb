2b19fccf3dbb785b88d456d07eaf356698a83b65
shodan <mega@ioexception.at>
2014-03-17 15:32:20 +0100

    modified sha1 hash

--- Library/Formula/screenfetch.rb
+++ Library/Formula/screenfetch.rb
@@ -3,7 +3,7 @@ require "formula"
 class Screenfetch < Formula
   homepage "http://git.silverirc.com/cgit.cgi/screenfetch.git"
   url "http://git.silverirc.com/cgit.cgi/screenfetch.git/snapshot/screenfetch-3.2.2.tar.gz"
-  sha1 "904b2824246ef5ae93d1ee4cda76b9d74333a27f"
+  sha1 "a520cfcc58cb4c63055dafb7d1df62a967496537"
 
   def install
     system "mv screenfetch-dev screenfetch"
