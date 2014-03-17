4e3b15fa9eb935678e0c59046f786ba50416608d
Jack Nagel <jacknagel@gmail.com>
2014-03-16 21:35:20 -0500

    libquicktime: use checksummed patches

--- Library/Formula/libquicktime.rb
+++ Library/Formula/libquicktime.rb
@@ -15,12 +15,11 @@ class Libquicktime < Formula
 
   # Fixes compilation with ffmpeg 2.x; applied upstream
   # http://sourceforge.net/p/libquicktime/mailman/message/30792767/
-  def patches
-    {
-      :p0 => "http://sourceforge.net/p/libquicktime/mailman/attachment/51812B9E.3090802%40mirriad.com/1/",
-      :p1 => DATA
-    }
+  patch :p0 do
+    url "http://sourceforge.net/p/libquicktime/mailman/attachment/51812B9E.3090802%40mirriad.com/1/"
+    sha1 "58c19548a7ae71fb20ee94ef41fd0c3a967c96c0"
   end
+  patch :DATA
 
   def install
     system "./configure", "--disable-debug",
