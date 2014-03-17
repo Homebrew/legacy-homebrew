fdf95b2d9768cfcf2c434f7e5cf2cce62563d925
Arno Gramatke <arno+github@gramatke.biz>
2014-03-17 16:18:54 +0100

    updated dependency

--- Library/Formula/podofo.rb
+++ Library/Formula/podofo.rb
@@ -7,7 +7,7 @@ class Podofo < Formula
 
   depends_on 'cmake' => :build
   depends_on :libpng
-  depends_on :freetype
+  depends_on 'freetype'
   depends_on :fontconfig
   depends_on 'jpeg'
   depends_on 'libtiff'
