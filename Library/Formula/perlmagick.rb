69cd7099da8db660b267e16d5c7cf732463ad965
blogabe <blogabe@rajab.us>
2014-03-18 09:15:52 -0700

    perlmagick.rb update: cleaning freetype
    Updated freetype include directory to reference X11 first, then Homebrew

--- Library/Formula/perlmagick.rb
+++ Library/Formula/perlmagick.rb
@@ -6,13 +6,12 @@ class Perlmagick < Formula
   sha1 "f234b49a5c570f78d693c04924fe74ddb5595292"
 
   depends_on :freetype
-  depends_on "libxml2"
   depends_on "imagemagick"
 
   def install
+    freetype_inc = "#{MacOS::X11.include}/freetype2/freetype" if MacOS::X11.installed? else "#{Formula["freetype"].opt_prefix}"
     inreplace "Makefile.PL" do |s|
-      s.gsub! "-I/usr/include/libxml2", "-I#{HOMEBREW_PREFIX}/include/libxml2"
-      s.gsub! "-I/usr/include/freetype2", "-I#{HOMEBREW_PREFIX}/include/freetype2"
+      s.gsub! "-I/usr/include/freetype2", "-I#{freetype_inc}"
       s.gsub! "'INSTALLBIN'	=> '/usr/local/bin'", "'INSTALLBIN'	=> '#{bin}'"
       s.gsub! "# 'PREFIX'	=> '/usr/local'", "'PREFIX'	=> '#{prefix}'"
     end
