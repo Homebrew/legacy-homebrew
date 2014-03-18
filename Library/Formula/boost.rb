a7540747b2e32d7cc4f164fd57be0e35e6818512
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2014-03-18 12:07:41 +0100

    boost: bump revision for important patches.
    So people pick up the recent patches from #27436.
    
    See also #27659.

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -16,6 +16,7 @@ class Boost < Formula
   homepage 'http://www.boost.org'
   url 'https://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2'
   sha1 'cef9a0cc7084b1d639e06cd3bc34e4251524c840'
+  revision 1
 
   head 'http://svn.boost.org/svn/boost/trunk'
 
