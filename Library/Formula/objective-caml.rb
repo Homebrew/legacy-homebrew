89c7427574c294e91b4a6bb58ba3ffac5f78cd70
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-18 08:27:01 +0000

    objective-caml: update 4.01.0 bottle.

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -12,10 +12,10 @@ class ObjectiveCaml < Formula
   depends_on :x11 if MacOS::X11.installed?
 
   bottle do
-    revision 2
-    sha1 "27fa5b1bcafd6e3915b9858a757991d3ccc40323" => :mavericks
-    sha1 "f289bb746eafccfc714e194eca4b24ec481e9df6" => :mountain_lion
-    sha1 "e091a997d111e5eb664f95479d84e5799a2284f0" => :lion
+    revision 3
+    sha1 "33e3a1cb87802572e531c801958ca6cdeee4cfbc" => :mavericks
+    sha1 "c418ec76d9f5eba9e1c2151a6e1c89e91da8d212" => :mountain_lion
+    sha1 "7da29c24adfd64772e2c3cca8b8502a1d6ce9fe0" => :lion
   end
 
   # recent versions of clang fail with a hard error if -fno-defer-pop
