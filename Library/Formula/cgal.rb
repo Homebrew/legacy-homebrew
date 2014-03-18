f64f42e0513ee4b4e2e519824c21391dc9ddb7e9
Jack Nagel <jacknagel@gmail.com>
2014-03-18 12:13:30 -0500

    cgal: use patch DSL

--- Library/Formula/cgal.rb
+++ Library/Formula/cgal.rb
@@ -24,10 +24,8 @@ class Cgal < Formula
   depends_on 'qt' if build.include? 'imaging'
   depends_on 'eigen' if build.with? "eigen3"
 
-  def patches
-    # Allows to compile with clang 425: http://goo.gl/y9Dg2y
-    DATA
-  end
+  # Allows to compile with clang 425: http://goo.gl/y9Dg2y
+  patch :DATA
 
   def install
     ENV.cxx11 if build.cxx11?
