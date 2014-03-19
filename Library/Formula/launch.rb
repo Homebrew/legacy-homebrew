1343ced39444f082156c9ac215945ed53bb124ac
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 16:01:17 +0000

    launch: add 1.2.2 bottle.
    Closes #27697.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/launch.rb
+++ Library/Formula/launch.rb
@@ -6,6 +6,13 @@ class Launch < Formula
   url 'http://sabi.net/nriley/software/launch-1.2.2.tar.gz'
   sha1 'd6fabdb495d3395460148bb99341cbf0f1b9d575'
 
+  bottle do
+    cellar :any
+    sha1 "32c11675ab351420dcb35742f66ae8a7ca987acf" => :mavericks
+    sha1 "702b8164d60d13ef3480c98caee053a242193aaf" => :mountain_lion
+    sha1 "1166c77e00378087195ad5273685d839dbb9f305" => :lion
+  end
+
   depends_on :xcode
 
   def install
