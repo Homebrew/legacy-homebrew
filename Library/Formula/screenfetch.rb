ec250cfb242d74d42035dad9d31c1e901a2098ee
Jason P. Scharf <jason.scharf@intergrated.net>
2014-03-19 12:33:14 -0400

    screenfetch --HEAD
     - Added git repo so it could be installed from source.
     - The HEAD version contains a fix for Mac OS X
    
    Closes #27699.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/screenfetch.rb
+++ Library/Formula/screenfetch.rb
@@ -4,6 +4,7 @@ class Screenfetch < Formula
   homepage "http://git.silverirc.com/cgit.cgi/screenfetch.git"
   url "http://git.silverirc.com/cgit.cgi/screenfetch.git/snapshot/screenfetch-3.2.2.tar.bz2"
   sha1 "29ec0d68b2799a946dc75b390d96e5f1b2bb8aaf"
+  head 'git://git.silverirc.com/screenfetch.git', :shallow => false
 
   bottle do
     cellar :any
