0379f6bc3320176bd7fbd35db01b3a2c2602530d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-19 14:07:47 +0000

    snap7: add 1.2.0 bottle.
    Closes #27690.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/snap7.rb
+++ Library/Formula/snap7.rb
@@ -5,6 +5,13 @@ class Snap7 < Formula
   url "https://downloads.sourceforge.net/project/snap7/1.2.0/snap7-full-1.2.0.tar.gz"
   sha1 "49e64997345dfcbb68cecee3e0bc098d96a96407"
 
+  bottle do
+    cellar :any
+    sha1 "530697d2704479af33cbbe51daf5d822206babf4" => :mavericks
+    sha1 "e6d0c563e699f217a9a4a4265fd67c678e51da62" => :mountain_lion
+    sha1 "71df6080c160980de7697395756802765c5cd3bd" => :lion
+  end
+
   def install
     archstr = MacOS.prefer_64_bit? ? "x86_64" : "i386"
     lib.mkpath
