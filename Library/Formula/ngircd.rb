5c5eaab66ed2eeb819ad5e92dd0f61ba4c7084a0
Alexander Barton <alex@barton.de>
2014-03-19 15:00:23 +0100

    ngIRCd: update "url" and "mirror"
    BerliOS is shutting down, so don't use it any more.
    
    Closes #27694.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ngircd.rb
+++ Library/Formula/ngircd.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ngircd < Formula
   homepage 'http://ngircd.barton.de'
-  url 'ftp://ftp.berlios.de/pub/ngircd/ngircd-21.tar.gz'
-  mirror 'http://ngircd.barton.de/pub/ngircd/ngircd-21.tar.gz'
+  url 'http://ngircd.barton.de/pub/ngircd/ngircd-21.tar.gz'
+  mirror 'http://ngircd.mirror.3rz.org/pub/ngircd/ngircd-21.tar.gz'
   sha256 '0edbd41304186e43f9d907a7017b40520cc90c2ce29b1339bdcd7622fffe19a0'
 
   option 'with-iconv', 'Enable character conversion using libiconv.'
