d27a44dbcf45072887d5212be24f431d35e7e8af
Kevin Locke <kevin@kevinlocke.name>
2014-03-19 11:48:10 -0600

    Add support for compiling QuantLib with --c++11
    Although QuantLib does not have any special features/support for C++11,
    it is still useful for developers/administrators who need to link
    against libc++ and to combine QuantLib with other C++11 applications and
    libraries.
    
    Also be sure to depend on boost with c++11 to ensure that QuantLib and
    boost share the same standard library.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- Library/Formula/quantlib.rb
+++ Library/Formula/quantlib.rb
@@ -5,9 +5,16 @@ class Quantlib < Formula
   url 'https://downloads.sourceforge.net/project/quantlib/QuantLib/1.4/QuantLib-1.4.tar.gz'
   sha1 'f31f4651011a8e38e8b2cc6c457760fe61863391'
 
-  depends_on 'boost'
+  option :cxx11
+
+  if build.cxx11?
+    depends_on 'boost' => 'c++11'
+  else
+    depends_on 'boost'
+  end
 
   def install
+    ENV.cxx11 if build.cxx11?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
