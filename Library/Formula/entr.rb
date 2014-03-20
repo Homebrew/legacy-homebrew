c6327e5b5e58f0ce742c6582fcba2d35f6125e22
Eric Radman <ericshane@eradman.com>
2014-03-20 11:00:28 -0400

    entr 2.8

--- Library/Formula/entr.rb
+++ Library/Formula/entr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Entr < Formula
   homepage 'http://entrproject.org/'
-  url 'http://entrproject.org/code/entr-2.6.tar.gz'
-  sha1 'ad0fed4e0311c72b4c2eb70c93b7d1267bd4de4c'
+  url 'http://entrproject.org/code/entr-2.8.tar.gz'
+  sha1 'ec03da66480cc7f8c6130e6f8599e1ae956e9b72'
 
   def install
     ENV['PREFIX'] = prefix
