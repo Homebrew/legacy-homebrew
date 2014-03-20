bea805d9b6ec88ba07110e99a21c471921272693
Joep Loijens <joep.loijens@gmail.com>
2014-03-20 18:53:11 +0100

    ssh-copy-id 6.6p1
    Closes #27739.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ssh-copy-id.rb
+++ Library/Formula/ssh-copy-id.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class SshCopyId < Formula
   homepage 'http://www.openssh.com/'
-  url 'http://ftp.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.5p1.tar.gz'
-  mirror 'http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.5p1.tar.gz'
-  version '6.5p1'
-  sha1 '3363a72b4fee91b29cf2024ff633c17f6cd2f86d'
+  url 'http://ftp.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.6p1.tar.gz'
+  mirror 'http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.6p1.tar.gz'
+  version '6.6p1'
+  sha1 'b850fd1af704942d9b3c2eff7ef6b3a59b6a6b6e'
 
   def install
     bin.install 'contrib/ssh-copy-id'
