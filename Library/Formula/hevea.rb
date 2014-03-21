756a0834100914787d3ac172a2653e41b31aa445
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-21 21:08:16 +0000

    hevea: add 2.13 bottle.
    Closes #27770.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hevea.rb
+++ Library/Formula/hevea.rb
@@ -5,6 +5,12 @@ class Hevea < Formula
   url 'http://hevea.inria.fr/distri/hevea-2.13.tar.gz'
   sha1 '03b35732f880591743ba750948b14efc0a46578b'
 
+  bottle do
+    sha1 "35761f7de7166b14412443c701386581209ac61b" => :mavericks
+    sha1 "ac0e2b98c81f462a7df9a07e2fb38754f922d573" => :mountain_lion
+    sha1 "a86c833a0ffeb1f46917f529895546fdda604261" => :lion
+  end
+
   depends_on 'objective-caml'
   depends_on 'ghostscript' => :optional
 
