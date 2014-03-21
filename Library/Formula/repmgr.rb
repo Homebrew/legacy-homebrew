5e5c4807b58eed32273d941fd02ca32e047086cf
Eugene Jen <ejen@wgen.net>
2014-03-21 00:37:26 -0400

    fix trailing white space problem

--- Library/Formula/repmgr.rb
+++ Library/Formula/repmgr.rb
@@ -5,11 +5,11 @@ class Repmgr < Formula
   url "http://www.repmgr.org/download/repmgr-2.0.tar.gz"
   sha1 "2fa23694b9ee1c9845f026d55995b41c38ab9106"
 
-  depends_on :postgresql 
+  depends_on :postgresql
 
   def install
     system "make", "USE_PGXS=1"
-    system "make", "USE_PGXS=1", "install" 
+    system "make", "USE_PGXS=1", "install"
   end
 
   test do
