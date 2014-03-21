881985f1250f6938ff820952dcf94cd6d718128b
Eugene Jen <ejen@wgen.net>
2014-03-20 22:53:52 -0400

    repmgr 2.0.0:
    Add formula to build repmgr 2.0.0, a replicaion manager from 2ndQuadrant for replications of postgresql. It requires Postgresql 9.1 above

---
+++ Library/Formula/repmgr.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Repmgr < Formula
+  homepage "http://www.repmgr.org/"
+  url "http://www.repmgr.org/download/repmgr-2.0.tar.gz"
+  sha1 "2fa23694b9ee1c9845f026d55995b41c38ab9106"
+
+  depends_on :postgresql 
+
+  def install
+    system "make", "USE_PGXS=1"
+    system "make", "USE_PGXS=1", "install" 
+  end
+
+  test do
+    system "repmgr", "--version"
+    system "repmgrd", "--version"
+  end
+end
