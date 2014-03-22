040ed42fef5579a63c32ffe31deb94b973b33109
David Holm <dholmster@gmail.com>
2014-03-22 13:14:19 +0100

    wine devel 1.7.15
    Closes #27779.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/wine.rb
+++ Library/Formula/wine.rb
@@ -23,8 +23,8 @@ class Wine < Formula
   end
 
   devel do
-    url 'https://downloads.sourceforge.net/project/wine/Source/wine-1.7.14.tar.bz2'
-    sha256 '2df1937e28936ba33e70a42fddcee01097ca0fbdd4dbf2c2f05d8a2ff5263e09'
+    url "https://downloads.sourceforge.net/project/wine/Source/wine-1.7.15.tar.bz2"
+    sha256 "519137adc562d32035fcf7b110d6e65fdd7721af0b176567a2187397c63f1380"
 
     # http://bugs.winehq.org/show_bug.cgi?id=34166
     patch do
