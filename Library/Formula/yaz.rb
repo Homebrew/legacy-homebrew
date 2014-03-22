21c40637212af6fc502f25fa1f237f91847c3b64
Sven-S. Porst <ssp-web@earthlingsoft.net>
2014-03-22 19:10:25 +0100

    yaz 5.0.21
    also consistently use double quotes around strings

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
-require 'formula'
+require "formula"
 
 class Yaz < Formula
-  homepage 'http://www.indexdata.com/yaz'
-  url 'http://ftp.indexdata.dk/pub/yaz/yaz-5.0.20.tar.gz'
-  sha1 '91989f6090b888a5cb4c3157999883d40e10e6bb'
+  homepage "http://www.indexdata.com/yaz"
+  url "http://ftp.indexdata.dk/pub/yaz/yaz-5.0.21.tar.gz"
+  sha1 "cfa3c49060c15f849a04e73227b4e976370bdbd1"
 
   bottle do
     cellar :any
@@ -13,8 +13,8 @@ class Yaz < Formula
     sha1 "f4477ca6a6f7e9018f3fa9cafd8a791fe71feeb7" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gnutls' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "gnutls" => :optional
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -26,8 +26,8 @@ class Yaz < Formula
   # This test converts between MARC8, an obscure mostly-obsolete library
   # text encoding supported by yaz-iconv, and UTF8.
   test do
-    marc8 = File.open('marc8.txt', 'w') do |f|
-      f.write '$1!0-!L,i$3i$si$Ki$Ai$O!+=(B'
+    marc8 = File.open("marc8.txt", "w") do |f|
+      f.write "$1!0-!L,i$3i$si$Ki$Ai$O!+=(B"
     end
 
     result = `"#{bin}/yaz-iconv" -f marc8 -t utf8 marc8.txt`.chomp
