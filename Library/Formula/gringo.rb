d4df2de42832ad66f2e0bc3f6af7a437fb5a9667
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-23 22:45:18 +0000

    gringo: add 4.3.0 bottle.
    Closes #27811.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gringo.rb
+++ Library/Formula/gringo.rb
@@ -5,6 +5,11 @@ class Gringo < Formula
   url 'https://downloads.sourceforge.net/project/potassco/gringo/4.3.0/gringo-4.3.0-source.tar.gz'
   sha1 'dccb55c2c690ebe1f6599a43b6072bfb50eb5e83'
 
+  bottle do
+    cellar :any
+    sha1 "1dc4996fd4469e987a5aab9c205cf157c3460c01" => :mavericks
+  end
+
   depends_on 're2c'  => :build
   depends_on 'scons' => :build
   depends_on 'bison' => :build
