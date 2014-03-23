4914076226d01e5e362091dd5fcc0f6f760c2b25
grecocd <cassiodgreco@gmail.com>
2014-03-23 12:30:59 -0400

    Formula - Shogun Machine Learning Toolbox

---
+++ Library/Formula/shogun.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Shogun < Formula
+  homepage "http://shogun-toolbox.org"
+  url "http://shogun-toolbox.org/archives/shogun/releases/3.2/sources/shogun-3.2.0.tar.bz2"
+  sha1 "442e212a19d0297b4df45a4dfe7deb6312441e54"
+
+  depends_on :x11
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+end
