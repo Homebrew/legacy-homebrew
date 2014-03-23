054acf8c5056af33016fc3b9d63726a6d5949c74
Dominik Moritz <domoritz@gmail.com>
2014-03-23 15:39:42 -0700

    gringo 4.3.0

--- Library/Formula/gringo.rb
+++ Library/Formula/gringo.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gringo < Formula
   homepage 'http://potassco.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/potassco/gringo/4.2.1/gringo-4.2.1-source.tar.gz'
-  sha1 'e27790e7d27c54ebb993d97debc01df5c2a4ddd6'
+  url 'https://downloads.sourceforge.net/project/potassco/gringo/4.3.0/gringo-4.3.0-source.tar.gz'
+  sha1 'dccb55c2c690ebe1f6599a43b6072bfb50eb5e83'
 
   depends_on 're2c'  => :build
   depends_on 'scons' => :build
