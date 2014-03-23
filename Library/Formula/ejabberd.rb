2d3b6b18fb8ea208be9ccecc7f9cb15d12cf907c
conscell <aeinner@gmail.com>
2014-03-23 22:06:37 +0900

    Update to ejabberd 13.12
    made Homebrew OpenSSL support unconditional.

--- Library/Formula/ejabberd.rb
+++ Library/Formula/ejabberd.rb
@@ -5,16 +5,15 @@ class Ejabberd < Formula
   url "http://www.process-one.net/downloads/ejabberd/13.12/ejabberd-13.12.tgz"
   sha1 '3aedb5012fab49181961ff24bad3af581f4b30ee'
 
+  depends_on "openssl"
+  depends_on "erlang"
+  depends_on "libyaml"
+
   option "32-bit"
   option 'with-odbc', "Build with ODBC support"
   option 'with-pgsql', "Build with PostgreSQL support"
   option 'with-mysql', "Build with MySQL support"
-  option 'with-brewed-openssl', "Build with Homebrew OpenSSL instead of the system version"
 
-  depends_on "openssl" if MacOS.version <= :leopard or build.with? "brewed-openssl"
-  depends_on "erlang"
-  depends_on "libyaml"
-  
   def install
     ENV['TARGET_DIR'] = ENV['DESTDIR'] = "#{lib}/ejabberd/erlang/lib/ejabberd-#{version}"
     ENV['MAN_DIR'] = man
@@ -31,19 +30,15 @@ class Ejabberd < Formula
             "--sysconfdir=#{etc}",
             "--localstatedir=#{var}"]
 
-    if MacOS.version <= :leopard or build.with? "brewed-openssl"
-      ENV.prepend "LDFLAGS", "-L#{Formula["openssl"].opt_prefix}/lib"
-      ENV.prepend "CPPFLAGS", "-I#{Formula["openssl"].opt_prefix}/include"
-    end
+    ENV.prepend "LDFLAGS", "-L#{Formula["openssl"].opt_prefix}/lib"
+    ENV.prepend "CPPFLAGS", "-I#{Formula["openssl"].opt_prefix}/include"
 
     args << "--enable-odbc" if build.with? "odbc"
     args << "--enable-pgsql" if build.with? "pgsql"
     args << "--enable-mysql" if build.with? "mysql"
 
     system "./configure", *args
-    system "make"
-    system "make install"
-
+    system "make", "install"
 
     (etc+"ejabberd").mkpath
     (var+"lib/ejabberd").mkpath
