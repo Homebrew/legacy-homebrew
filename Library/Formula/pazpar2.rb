020ab52e24536d85069754597468cb90dcf6fc52
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-24 09:58:07 +0000

    pazpar2: add 1.6.39 bottle.
    Closes #27784.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pazpar2.rb
+++ Library/Formula/pazpar2.rb
@@ -5,6 +5,13 @@ class Pazpar2 < Formula
   url "http://ftp.indexdata.dk/pub/pazpar2/pazpar2-1.6.39.tar.gz"
   sha1 "2be29efecc4f0ed5a6a300638b7ef6577d1e34e4"
 
+  bottle do
+    cellar :any
+    sha1 "c8195a3a96b9278abdc8afba4873715a342455c6" => :mavericks
+    sha1 "de02d686252da5279a58ac85f5d09d9398dbebd2" => :mountain_lion
+    sha1 "e221b78ef5cb0759c4eeb86f1c3bc8a13ad8e44f" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "yaz"
 
