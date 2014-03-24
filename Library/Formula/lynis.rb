409247595555ab6c44a675a46abb898d9b4999a3
Chris Kau <chris.b.kau@gmail.com>
2014-03-24 10:43:16 -0700

    lynis 1.4.7
    Closes #27831.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/lynis.rb
+++ Library/Formula/lynis.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Lynis < Formula
   homepage 'http://www.rootkit.nl/projects/lynis.html'
-  url 'http://cisofy.com/files/lynis-1.4.5.tar.gz'
-  sha1 'c2586439b5d80652c86d44df87e3eb49a06dd31c'
+  url 'http://cisofy.com/files/lynis-1.4.7.tar.gz'
+  sha1 '2c38d0aef90dbe64839478f1f9fdfaec7346f40b'
 
   def install
     inreplace 'lynis' do |s|
