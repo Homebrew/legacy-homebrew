7bcb0a09aada5bfada7fc21d96d8cffd38fd917d
Misty De Meo <mistydemeo@gmail.com>
2014-03-24 08:57:36 -0700

    appledoc 2.2-963

--- Library/Formula/appledoc.rb
+++ Library/Formula/appledoc.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Appledoc < Formula
   homepage 'http://appledoc.gentlebytes.com/'
-  url "https://github.com/tomaz/appledoc/archive/v2.2.tar.gz"
-  sha1 '4ad475ee6bdc2e34d6053c4e384aad1781349f5e'
+  url "https://github.com/tomaz/appledoc/archive/v2.2-963.tar.gz"
+  sha1 '8491dc9ae8fa6bc69da9dcedca601529af3bf4e6'
+  version '2.2-963'
 
   head 'https://github.com/tomaz/appledoc.git', :branch => 'master'
 
@@ -18,6 +19,9 @@ class Appledoc < Formula
                "SYMROOT=build",
                "DSTROOT=build",
                "INSTALL_PATH=/bin",
+               # 2.2-963 no longer actually uses ObjC GC, but will still
+               # try to build with it due to this flag and hence will fail.
+               "GCC_ENABLE_OBJC_GC=unsupported",
                "OTHER_CFLAGS='-DCOMPILE_TIME_DEFAULT_TEMPLATE_PATH=@\"#{prefix}/Templates\"'"
     bin.install "build/bin/appledoc"
     prefix.install "Templates/"
