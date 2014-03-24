12c2b5fb4dc183b0bb38ae5470a377f3452b1737
Jens H. Nielsen <jens.nielsen@ucl.ac.uk>
2014-03-24 11:26:05 +0000

    pyqt5 5.2.1
    Closes #27819.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pyqt5.rb
+++ Library/Formula/pyqt5.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pyqt5 < Formula
   homepage 'http://www.riverbankcomputing.co.uk/software/pyqt/download5'
-  url 'https://downloads.sf.net/project/pyqt/PyQt5/PyQt-5.2/PyQt-gpl-5.2.tar.gz'
-  sha1 'a1c232d34ab268587c127ad3097c725ee1a70cf0'
+  url 'https://downloads.sf.net/project/pyqt/PyQt5/PyQt-5.2.1/PyQt-gpl-5.2.1.tar.gz'
+  sha1 '7d72ec27bfe547223b4b11db8a90603fe4796530'
 
   option 'enable-debug', "Build with debug symbols"
 
