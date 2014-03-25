aa5c5d6a5f320a5621377503b05cf116d5647610
Jack Nagel <jacknagel@gmail.com>
2014-03-25 18:59:47 -0500

    clozure-cl: modernize formula

--- Library/Formula/clozure-cl.rb
+++ Library/Formula/clozure-cl.rb
@@ -9,8 +9,7 @@ class ClozureCl < Formula
   conflicts_with 'cclive', :because => 'both install a ccl binary'
 
   def install
-    # Get rid of all the .svn dirs, that for some reason are
-    # included in the tarball
+    # Get rid of all the .svn directories
     buildpath.find do |path|
       if path.directory? and path.basename.to_s == '.svn'
         rm_rf path
@@ -18,34 +17,16 @@ class ClozureCl < Formula
       end
     end
 
-    # Due to the way ClozureCL is organized, we'll put everything into
-    # a subdirectory, and then link all the necessary scripts to the
-    # locations Homebrew expects
-    ccl_install_dir = prefix + 'ccl'
+    libexec.install Dir["*"]
+    scripts = Dir["#{libexec}/scripts/ccl{,64}"]
 
-    # These scripts allow the user to execute some code or get a REPL
-    ccl_scripts = Dir['./scripts/ccl{,64}']
-
-    # ClozureCL uses the CCL_DEFAULT_DIRECTORY to find its sources. Update
-    # it so it points to the right directory (since the ccl scripts themselves
-    # won't be in the usual location)
-    ccl_scripts.map { |path| Pathname.new(File.expand_path(path)) }.each do |script|
-      inreplace script do |s|
-        s.gsub! /CCL_DEFAULT_DIRECTORY=.+$/, %Q{CCL_DEFAULT_DIRECTORY="#{ccl_install_dir}"}
-      end
+    inreplace scripts do |s|
+      s.gsub! /CCL_DEFAULT_DIRECTORY=.+$/, %Q{CCL_DEFAULT_DIRECTORY="#{libexec}"}
     end
 
-    # Copy everything over to the cellar
-    ccl_install_dir.install Dir['*']
-
-    # Link the wrapper scripts to prefix/bin, where Homebrew can link them properly
-    bin.mkdir
-    ccl_scripts.each do |script|
-      ln ccl_install_dir+script, bin+File.basename(script)
-    end
+    bin.install_symlink scripts
   end
 
-  # Generates a string to test the ccl scripts that can be passed directly to `system'
   def test_ccl(bit = 32)
     ccl = bin + "ccl#{'64' if bit == 64}"
     %Q{#{ccl} -e '(progn (format t "Hello world from #{bit}-bit ClozureCL") (ccl::quit))'}
