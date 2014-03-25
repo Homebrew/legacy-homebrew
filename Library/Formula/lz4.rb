429e6d66be3a5300df7c0974f41d698894d95c71
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-25 08:08:53 +0000

    lz4: update r116 bottle.
    Closes #27846.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/lz4.rb
+++ Library/Formula/lz4.rb
@@ -8,9 +8,9 @@ class Lz4 < Formula
 
   bottle do
     cellar :any
-    sha1 "d81daa2070d46528c4fa70c4eb77ee15fe790687" => :mavericks
-    sha1 "7ea1b15048bdaf49e27ae0b327c4dd501e653bde" => :mountain_lion
-    sha1 "4ed51a6cf8004f234c840887d3b7e75852e2c9cf" => :lion
+    sha1 "64ed763d71baa31fff5d20ea815ddad4e727ef35" => :mavericks
+    sha1 "abd873485e052034929edef016b274e52947655f" => :mountain_lion
+    sha1 "5130288a257b8df4da7538789a38ab53f3bd9a58" => :lion
   end
 
   def install
