23f3a65e8b687dfef995a0c43ecdddc2f77e32ce
Jack Nagel <jacknagel@gmail.com>
2014-03-25 11:33:09 -0500

    ocaml: make site-lib symlink in post-install hook

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -35,10 +35,12 @@ class ObjectiveCaml < Formula
     system "make opt"
     system "make opt.opt"
     system "make", "PREFIX=#{prefix}", "install"
+  end
 
+  def post_install
     # site-lib in the Cellar will be a symlink to the HOMEBREW_PREFIX location,
     # which is mkpath'd by Keg#link when something installs into it
-    ln_s HOMEBREW_PREFIX/"lib/ocaml/site-lib", lib/"ocaml/site-lib"
+    (lib/"ocaml").install_symlink HOMEBREW_PREFIX/"lib/ocaml/site-lib"
   end
 end
 __END__
