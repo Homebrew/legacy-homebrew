aa7539754fb2b62be13a0aaef9c61c5ce5d142e5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-25 15:36:17 +0000

    unbound: add 1.4.21 bottle.

--- Library/Formula/unbound.rb
+++ Library/Formula/unbound.rb
@@ -5,6 +5,12 @@ class Unbound < Formula
   url 'http://www.unbound.net/downloads/unbound-1.4.21.tar.gz'
   sha1 '3ef4ea626e5284368d48ab618fe2207d43f2cee1'
 
+  bottle do
+    sha1 "f012bd189beef2832b3af0b88679660eb092bd55" => :mavericks
+    sha1 "b64749fe75248d14c0164bf4e5e77833b0341f7f" => :mountain_lion
+    sha1 "a3ed34772ca84a0d800e7edc0c676498f4fa541e" => :lion
+  end
+
   depends_on 'ldns'
 
   def install
