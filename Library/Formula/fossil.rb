64beead8a638e28216a73acfedb9b602e4e7f071
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-25 15:48:10 +0000

    fossil: add 1.28 bottle.
    Closes #27858.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/fossil.rb
+++ Library/Formula/fossil.rb
@@ -7,6 +7,13 @@ class Fossil < Formula
   sha1 '9e547a27d2447f12df951e86670da12c7cfbd26a'
   version '1.28'
 
+  bottle do
+    cellar :any
+    sha1 "c4f7800f7a88aee0f5c27dc8ee90e7d67dc90570" => :mavericks
+    sha1 "2b8fa6a214c937ea56e50841614cbcc68375153d" => :mountain_lion
+    sha1 "ad5e8b7cc41ae2a81f5b0ed84110d6e34b56622b" => :lion
+  end
+
   option 'without-json', 'Build without "json" command support.'
   option 'without-tcl', "Build without the tcl-th1 command bridge."
 
