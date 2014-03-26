04d923d8b33fdb4488b18160c1a18b2c6c877e31
Thomas Texier <sharkone@en-mousse.org>
2014-03-26 19:57:26 -0400

    Proper handling of system python vs. brewed python

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -5,24 +5,25 @@ class LibtorrentRasterbar < Formula
   url 'https://downloads.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-0.16.15.tar.gz'
   sha1 '8657a493fb2ee4e6bf55e484deb922d23b65818d'
 
-  option 'with-brewed-python', "Build with Homebrew Python instead of the system version"
-
   depends_on 'pkg-config' => :build
   depends_on 'boost'
   depends_on 'openssl' if MacOS.version <= :snow_leopard # Needs a newer version of OpenSSL on Snow Leopard
-  depends_on 'python' if build.with? "brewed-python"
+  depends_on :python => :recommended
 
   def install
-    args = []
-    args << "PYTHON_EXTRA_LDFLAGS= " if build.with? "brewed-python"
-
     boost = Formula["boost"]
+
+    optional_args = []
+    if build.with? "python"
+      optional_args << "--enable-python-binding"
+      optional_args << "PYTHON_EXTRA_LDFLAGS= "
+    end
+
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-boost=#{boost.opt_prefix}",
-                          "--enable-python-binding",
-                          *args
+                          *optional_args
     system "make install"
   end
 end
