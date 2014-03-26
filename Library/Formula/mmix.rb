df0545129785850bed6fa74844b4d9e0fac1c660
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-26 16:24:24 +0000

    mmix: add 20131017 bottle.

--- Library/Formula/mmix.rb
+++ Library/Formula/mmix.rb
@@ -5,6 +5,13 @@ class Mmix < Formula
   url "http://mmix.cs.hm.edu/src/mmix-20131017.tgz"
   sha1 "75dba738c72fb163302160e745096846a7b8672a"
 
+  bottle do
+    cellar :any
+    sha1 "3203586ea18c63f2332cd7d52e739f87c6bf71c6" => :mavericks
+    sha1 "cc8eeb7fe526992122ddf3a42fc422837b18a0d4" => :mountain_lion
+    sha1 "d1cddd4fff7468c8234f3362d42b2a4bfbd17cee" => :lion
+  end
+
   depends_on "cweb" => :build
 
   def install
