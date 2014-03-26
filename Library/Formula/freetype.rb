8dded45502b142e06daaffa3bba8b7b1f81584ba
Thomas Bouve <crahan@n00.be>
2014-03-26 13:11:13 +0100

    freetype: enable subpixel rendering by default.

--- Library/Formula/freetype.rb
+++ Library/Formula/freetype.rb
@@ -16,10 +16,17 @@ class Freetype < Formula
   keg_only :provided_pre_mountain_lion
 
   option :universal
+  option 'without-subpixel', "Disable sub-pixel rendering (a.k.a. LCD rendering, or ClearType)"
 
   depends_on "libpng"
 
   def install
+    if build.with? "subpixel"
+      inreplace "include/config/ftoption.h",
+          "/* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING */",
+          "#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING"
+    end
+
     ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}", "--without-harfbuzz"
     system "make"
