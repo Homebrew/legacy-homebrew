d383fc44ecf2aef044d8ce835f1ae7670080731b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-26 08:53:08 +0000

    l-smash: add 1.5.2 bottle.
    Closes #27829.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/l-smash.rb
+++ Library/Formula/l-smash.rb
@@ -5,6 +5,13 @@ class LSmash < Formula
   url "https://github.com/l-smash/l-smash.git", :tag => "v1.5.2", :shallow => false
   head "https://github.com/l-smash/l-smash.git"
 
+  bottle do
+    cellar :any
+    sha1 "7e0fda0145b2eceaa950e9cccf84205c9dcaab47" => :mavericks
+    sha1 "0e9a018a8d3f818c42e98153e003018b3ccb6982" => :mountain_lion
+    sha1 "088add54110cd25086fced2086593a8ec46dafb2" => :lion
+  end
+
   def install
     args = ["--prefix=#{prefix}", "--enable-shared"]
 
