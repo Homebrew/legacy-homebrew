b372f7f1460e0e2d2937180780af8f183116f6f0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-26 17:10:53 +0000

    iperf3: add 3.0.2 bottle.
    Closes #24971.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/iperf3.rb
+++ Library/Formula/iperf3.rb
@@ -6,6 +6,13 @@ class Iperf3 < Formula
   sha1 '3dacb887d4ba1b90c9fbb3ec2ae69389d40c01c8'
   head 'https://code.google.com/p/iperf/', :using => :hg
 
+  bottle do
+    cellar :any
+    sha1 "4aa51bb8ab2eeac0f7152924a46bd702d723f094" => :mavericks
+    sha1 "0bec442d6c6d054ee9627c93516022930ea391c2" => :mountain_lion
+    sha1 "66ab71dd56b6f2f539b5ca31ba9a93643d9d647d" => :lion
+  end
+
   depends_on :autoconf
 
   def install
