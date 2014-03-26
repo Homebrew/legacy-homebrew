3f2fbfbd4026e2a74f29aa0d6388fba25d151ce8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-26 16:24:24 +0000

    cweb: add 3.64ad bottle.
    Closes #27870.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cweb.rb
+++ Library/Formula/cweb.rb
@@ -5,6 +5,12 @@ class Cweb < Formula
   url "ftp://ftp.cs.stanford.edu/pub/cweb/cweb-3.64ad.tar.gz"
   sha1 "a9828b66b525d7cf91c57b3c4891168caa4af10a"
 
+  bottle do
+    sha1 "ab57d1005e2c2c9fddd67b4260d37f791deed5c0" => :mavericks
+    sha1 "9b83fb6b84de445f4d58997c710f291b9cbdc696" => :mountain_lion
+    sha1 "953b5d5ae51745f8664ffd5998819d60f1d58189" => :lion
+  end
+
   def install
     ENV.deparallelize
 
