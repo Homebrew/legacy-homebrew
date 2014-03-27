e44509f079922f4f690b185850bf872b6218d76a
Jack Nagel <jacknagel@gmail.com>
2014-03-27 09:35:10 -0500

    Fix conflicting symlink advice
    Closes #27899.

--- Library/Homebrew/extend/pathname.rb
+++ Library/Homebrew/extend/pathname.rb
@@ -298,23 +298,22 @@ class Pathname
       # NOTE only system ln -s will create RELATIVE symlinks
       quiet_system 'ln', '-s', src.relative_path_from(self.dirname), self.basename
       if not $?.success?
-        if self.exist?
+        if symlink? && exist? || symlink?
           raise <<-EOS.undent
             Could not symlink file: #{src.expand_path}
-            Target #{self} already exists. You may need to delete it.
+            Target #{self} already exists as a symlink to #{readlink}.
+            If this file is from another formula, you may need to
+            `brew unlink` it. Otherwise, you may want to delete it.
             To force the link and overwrite all other conflicting files, do:
               brew link --overwrite formula_name
 
             To list all files that would be deleted:
               brew link --overwrite --dry-run formula_name
             EOS
-        # #exist? will return false for symlinks whose target doesn't exist
-        elsif self.symlink?
+        elsif exist?
           raise <<-EOS.undent
             Could not symlink file: #{src.expand_path}
-            Target #{self} already exists as a symlink to #{readlink}.
-            If this file is from another formula, you may need to
-            `brew unlink` it. Otherwise, you may want to delete it.
+            Target #{self} already exists. You may need to delete it.
             To force the link and overwrite all other conflicting files, do:
               brew link --overwrite formula_name
 
