38cb66310049397ef65b9da34a2487b018e0883b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-27 11:36:47 +0000

    gphoto2: add 2.5.4 bottle.
    Closes #27909.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gphoto2.rb
+++ Library/Formula/gphoto2.rb
@@ -5,6 +5,13 @@ class Gphoto2 < Formula
   url 'https://downloads.sourceforge.net/project/gphoto/gphoto/2.5.4/gphoto2-2.5.4.tar.bz2'
   sha1 'c557ee5cdea28f6cff1d2bc1d6ace58be48b65d3'
 
+  bottle do
+    cellar :any
+    sha1 "11a6058dda360a35674e156c324d9973d039ee33" => :mavericks
+    sha1 "8e82208a1cc540b6dca788d0ed1c0b4d0f8ec779" => :mountain_lion
+    sha1 "459af859c8a218d45ecbb36228afde156f3fccab" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'jpeg'
   depends_on 'libgphoto2'
