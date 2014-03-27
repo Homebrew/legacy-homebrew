2913f905899b51a6b06ba55a1964d55ce4a54867
Mike McQuaid <mike@mikemcquaid.com>
2014-03-27 18:26:05 +0000

    Revert "clasp: add 3.0.1 bottle."
    This reverts commit 4aba0b123ec0a28ad4314797415737e58d28ced2.

--- Library/Formula/clasp.rb
+++ Library/Formula/clasp.rb
@@ -5,13 +5,6 @@ class Clasp < Formula
   url 'https://downloads.sourceforge.net/project/potassco/clasp/3.0.1/clasp-3.0.1-source.tar.gz'
   sha1 '23d47997fe1f474785596c17085c32e76ae5d5c1'
 
-  bottle do
-    cellar :any
-    sha1 "5436ef5b91eab2b90c7ceb1b2d5fd3f7f80287f0" => :mavericks
-    sha1 "26681cd8c4d37d1f079866309934aa53be9d4628" => :mountain_lion
-    sha1 "bcd2846a95b709ce1e615103eb1f4f12cf580adb" => :lion
-  end
-
   option 'with-mt', 'Enable multi-thread support'
 
   depends_on 'tbb' if build.with? "mt"
