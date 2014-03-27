6196954c99784947a5c5f6d8a427847d9bc71f94
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-27 12:37:24 +0000

    quantlib: add 1.4 bottle.
    Closes #27708.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/quantlib.rb
+++ Library/Formula/quantlib.rb
@@ -5,6 +5,13 @@ class Quantlib < Formula
   url 'https://downloads.sourceforge.net/project/quantlib/QuantLib/1.4/QuantLib-1.4.tar.gz'
   sha1 'f31f4651011a8e38e8b2cc6c457760fe61863391'
 
+  bottle do
+    cellar :any
+    sha1 "4159c617207a756fc39e11755c8c0c41562ac48d" => :mavericks
+    sha1 "d556348c0d2d44fe2b24167a99c8ac5c3c26bb79" => :mountain_lion
+    sha1 "8fdc268cab283a11967df58489aa6df60c8f8779" => :lion
+  end
+
   option :cxx11
 
   if build.cxx11?
