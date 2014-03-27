43f753b63efff4a449d54fd469b5d6acc4a4ca12
http://jayferd.us/ <jjmadkisson@gmail.com>
2014-03-27 14:15:37 -0700

    use ry 0.5.0 for new installation dirs

--- Library/Formula/ry.rb
+++ Library/Formula/ry.rb
@@ -2,15 +2,18 @@ require "formula"
 
 class Ry < Formula
   homepage "https://github.com/jayferd/ry"
-  url "https://github.com/jayferd/ry/archive/v0.4.2.tar.gz"
-  sha1 "ebce2e822dd62df62af1f6a12701d815bea58ac2"
+  url "https://github.com/jayferd/ry/archive/v0.5.0.tar.gz"
+  sha1 "cce9a6c3a4d19316de00dba422384957c74ccfda"
 
   head "https://github.com/jayferd/ry.git"
 
   depends_on "ruby-build" => :recommended
+  depends_on "bash-completion" => :recommended
 
   def install
     ENV["PREFIX"] = prefix
+    ENV["BASH_COMPLETIONS_DIR"] = etc/"bash_completion.d"
+    ENV["ZSH_COMPLETIONS_DIR"] = share/"zsh/site-functions"
     system "make", "install"
   end
 
