504bd0562e7e2e778c5de962e1eab8e5bbd52a63
Jack Nagel <jacknagel@gmail.com>
2014-03-28 11:11:42 -0500

    libsoup 2.46.0

--- Library/Formula/libsoup.rb
+++ Library/Formula/libsoup.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Libsoup < Formula
   homepage 'http://live.gnome.org/LibSoup'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/libsoup/2.44/libsoup-2.44.2.tar.xz'
-  sha256 'e7e4b5ab74a6c00fc267c9f5963852d28759ad3154dab6388e2d6e1962d598f3'
-  revision 1
+  url 'http://ftp.gnome.org/pub/GNOME/sources/libsoup/2.46/libsoup-2.46.0.tar.xz'
+  sha256 'fa3d5574c1a2df521242e2ca624a2b3057121798cab9f8f40525aa186a7b15a3'
 
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
