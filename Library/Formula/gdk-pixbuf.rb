d714f79a3edef4cea2184355f9b795bd2b241799
Jack Nagel <jacknagel@gmail.com>
2014-03-28 12:35:13 -0500

    gdk-pixbuf 2.30.7

--- Library/Formula/gdk-pixbuf.rb
+++ Library/Formula/gdk-pixbuf.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GdkPixbuf < Formula
   homepage 'http://gtk.org'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.30/gdk-pixbuf-2.30.6.tar.xz'
-  sha256 '43394fa4adfb49fa4d662fdc6f43f7f25a9f914509528f57720b18487b0a7f5a'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.30/gdk-pixbuf-2.30.7.tar.xz'
+  sha256 '0aafc365eab1083a53f15e4828333b968219ffcb1a995ac6289c0147c9ffad06'
 
   bottle do
     sha1 "8e76502fdd8ef8152d8605e36b0ee5ddd68d674c" => :mavericks
