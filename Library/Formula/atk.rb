f828136a6f4bfdb5f964f982bae365309699f0ba
Jack Nagel <jacknagel@gmail.com>
2014-03-28 11:11:43 -0500

    atk 2.12.0

--- Library/Formula/atk.rb
+++ Library/Formula/atk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Atk < Formula
   homepage 'http://library.gnome.org/devel/atk/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/atk/2.10/atk-2.10.0.tar.xz'
-  sha256 '636917a5036bc851d8491194645d284798ec118919a828be5e713b6ecc5b50b0'
+  url 'http://ftp.gnome.org/pub/gnome/sources/atk/2.12/atk-2.12.0.tar.xz'
+  sha256 '48a8431974639c5a59c24fcd3ece1a19709872d5dfe78907524d9f5e9993f18f'
 
   bottle do
     sha1 "f2d6e44d20954bc11b0b031d95c2b8def08227ac" => :mavericks
