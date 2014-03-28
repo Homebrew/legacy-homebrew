c69c28a7e15ef13674204abd1c33dfdf89852ad0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-28 14:34:21 -0500

    gtk+3: add 3.12.0 bottle.

--- Library/Formula/gtk+3.rb
+++ Library/Formula/gtk+3.rb
@@ -5,6 +5,12 @@ class Gtkx3 < Formula
   url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.12/gtk+-3.12.0.tar.xz'
   sha256 'eb69741cd4029b5a1ac9cf04d9de55dcf9e30777a63891750f5d20cc554b6e4b'
 
+  bottle do
+    sha1 "9e2614c2560243bb2739c260b0c883a019321c14" => :mavericks
+    sha1 "2dd7df2a669a1fb41512008160a4f2a383b35028" => :mountain_lion
+    sha1 "6c9814dd0d3eef328823f782da098dc345135491" => :lion
+  end
+
   depends_on :x11 => '2.5' # needs XInput2, introduced in libXi 1.3
   depends_on 'pkg-config' => :build
   depends_on 'glib'
