04496f9c99dfc3e8710c47f6453d5be93dfd2b10
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-28 17:23:57 +0000

    at-spi2-atk: add 2.12.0 bottle.

--- Library/Formula/at-spi2-atk.rb
+++ Library/Formula/at-spi2-atk.rb
@@ -5,6 +5,13 @@ class AtSpi2Atk < Formula
   url 'http://ftp.gnome.org/pub/gnome/sources/at-spi2-atk/2.12/at-spi2-atk-2.12.0.tar.xz'
   sha256 '3b5467c9f169812bc36a4245355b7deedea0a62eb22153df96ced88dcd1c3633'
 
+  bottle do
+    cellar :any
+    sha1 "0332a20b531627abf55f9cea631fcdff17d77963" => :mavericks
+    sha1 "a31be6abe965405218fb8dc0aee7cc3391b48575" => :mountain_lion
+    sha1 "f29c7a7b5b49cddb9ba41b0276e9d2b7f4fd1b31" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'at-spi2-core'
   depends_on 'atk'
