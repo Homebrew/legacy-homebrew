c642902a9260339f83306a0769af482703d5de43
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-28 17:23:57 +0000

    dbus-glib: add 0.102 bottle.

--- Library/Formula/dbus-glib.rb
+++ Library/Formula/dbus-glib.rb
@@ -5,6 +5,12 @@ class DbusGlib < Formula
   url 'http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.102.tar.gz'
   sha1 '58a8955972f6c221461a49f9c541c22e838a5776'
 
+  bottle do
+    sha1 "6ca096253317cc2add57bd5af718924a391ea8f4" => :mavericks
+    sha1 "9fb847d04b08d4f6c17aa8e76e919c899c9c3be1" => :mountain_lion
+    sha1 "411565ac6f2a5b74d110c97955ec9d50a25f1996" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
   depends_on 'glib'
