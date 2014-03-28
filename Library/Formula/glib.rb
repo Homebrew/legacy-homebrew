76de4c919505ba02a01c668046f5a49a9ca05e9b
Jack Nagel <jacknagel@gmail.com>
2014-03-28 11:34:40 -0500

    glib 2.40

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Glib < Formula
-  homepage 'http://developer.gnome.org/glib/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/glib/2.38/glib-2.38.2.tar.xz'
-  sha256 '056a9854c0966a0945e16146b3345b7a82562a5ba4d5516fd10398732aea5734'
+  homepage "http://developer.gnome.org/glib/"
+  url "http://ftp.gnome.org/pub/gnome/sources/glib/2.40/glib-2.40.0.tar.xz"
+  sha256 "0d27f195966ecb1995dcce0754129fd66ebe820c7cd29200d264b02af1aa28b5"
 
   bottle do
     sha1 "4859364747094843599c19a42d7d150f91629f6c" => :mavericks
@@ -34,21 +34,21 @@ class Glib < Formula
   # but needed to fix an assumption about the location of the d-bus machine
   # id file.
   patch do
-    url "https://gist.github.com/jacknagel/6700436/raw/a94f21a9c5ccd10afa0a61b11455c880640f3133/glib-configurable-paths.patch"
-    sha1 "911df7b09452c52ee3e0d269775d546cf7c077d1"
+    url "https://gist.githubusercontent.com/jacknagel/6700436/raw/2d790a8bd0c59ef66835866523988fbf9f680443/glib-configurable-paths.patch"
+    sha1 "7b89ce26c256e43cfdc11bae0c4498ec8529bcd4"
   end
 
   # Fixes compilation with FSF GCC. Doesn't fix it on every platform, due
   # to unrelated issues in GCC, but improves the situation.
   # Patch submitted upstream: https://bugzilla.gnome.org/show_bug.cgi?id=672777
   patch do
-    url "https://gist.github.com/mistydemeo/8c7eaf0940b6b9159779/raw/11b3b1f09d15ccf805b0914a15eece11685ea8a5/gio.diff"
-    sha1 "5afea1a284747d31039449ca970376430951ec55"
+    url "https://gist.githubusercontent.com/jacknagel/9835034/raw/b0388e86f74286f4271f9b0dca8219fdecafd5e3/gio.patch"
+    sha1 "32158fffbfb305296f7665ede6185a47d6f6b389"
   end
 
   patch do
-    url "https://gist.githubusercontent.com/jacknagel/9726139/raw/a3e716034dc082e98f179c9e490910211be1df4c/universal.patch"
-    sha1 "1ce36591ff79bc05eeb89d91f008988e2f4c8cde"
+    url "https://gist.githubusercontent.com/jacknagel/9726139/raw/9d5635480d96d6b5c717d2c0d5d24de38b68ffbd/universal.patch"
+    sha1 "7f38cab550571b39989275362995ade214b44490"
   end if build.universal?
 
   def install
