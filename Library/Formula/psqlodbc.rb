95bf154f21ed0ebd1a39dffea8ea6a6ff534ece0
conscell <aeinner@gmail.com>
2014-03-29 14:28:49 +0900

    PostgreSQL ODBC driver 09.03.0210
    - Replaced an optional OS X Server PostgreSQL dependency with a Requirement and default_formula
    - Added test
    - Fixed brew audit problem

--- Library/Formula/psqlodbc.rb
+++ Library/Formula/psqlodbc.rb
@@ -1,27 +1,35 @@
 require 'formula'
 
+class PostgresqlInstalled < Requirement
+  fatal true
+  default_formula 'postgresql'
+
+  satisfy { which 'pg_config' }
+end
+
 class Psqlodbc < Formula
   homepage 'http://www.postgresql.org/'
   url 'http://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.03.0210.tar.gz'
   sha1 'e1eb147ef0452e1f7b0f9e102dacb5654a580dba'
 
-  option 'with-osxserverpsql', 'Compile with OS X Server PostgreSQL'
-
   depends_on 'unixodbc'
-  depends_on 'postgresql' if build.without? 'osxserverpsql'
+  depends_on 'openssl'
+  depends_on PostgresqlInstalled
 
   def install
     args = %W[
       --prefix=#{prefix}
-      --with-unixodbc=#{Formula.factory('unixodbc').prefix}
+      --with-unixodbc=#{Formula['unixodbc'].prefix}
     ]
-
-    if build.with? 'osxserverpsql'
-      args << "--with-libpq=/Applications/Server.app/Contents/ServerRoot/usr"
-    end
 
     system "./configure", *args
     system "make"
     system "make", "install"
+  end
+
+  test do
+    output = `#{Formula['unixodbc'].bin}/dltest #{lib}/psqlodbcw.so`
+    assert_equal "SUCCESS: Loaded #{lib}/psqlodbcw.so\n", output
+    assert_equal 0, $?.exitstatus
   end
 end
