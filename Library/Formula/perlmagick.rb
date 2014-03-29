92279f2f9ad5d12114a279c7151cce4f6d667753
BrewTestBot <brew-test-bot@googlegroups.com>
2014-03-29 15:24:20 +0000

    perlmagick: add 6.88 bottle.
    Closes #27646.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/perlmagick.rb
+++ Library/Formula/perlmagick.rb
@@ -5,6 +5,12 @@ class Perlmagick < Formula
   url "http://www.imagemagick.org/download/perl/PerlMagick-6.88.tar.gz"
   sha1 "f234b49a5c570f78d693c04924fe74ddb5595292"
 
+  bottle do
+    sha1 "86de9e85e873e3c038a828119875409f1688febb" => :mavericks
+    sha1 "bfa32dda723942a88a2e1d50bba1ebc732c9d198" => :mountain_lion
+    sha1 "ba68b22ae4598361afc9b3cb0730825fc2376ee1" => :lion
+  end
+
   depends_on "freetype"
   depends_on "imagemagick"
 
