ad21e87cc5de38f8bb6386dea8e60595f1f422ac
John Claus <johnclaus@gmail.com>
2014-03-29 23:11:22 -0600

    ec2-api-tools 1.6.13.0

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ec2ApiTools < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/351'
-  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.6.12.0.zip'
+  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.6.13.0.zip'
   sha1 'eb0732df59a2ad2c1ad3fa5839e846b69afad697'
 
   def install
