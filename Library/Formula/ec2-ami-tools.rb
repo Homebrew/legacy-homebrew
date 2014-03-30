29862d57030f23b351a48d1d531d80df00fe8222
John Claus <johnclaus@gmail.com>
2014-03-29 23:13:11 -0600

    ec2-ami-tools 1.5.2

--- Library/Formula/ec2-ami-tools.rb
+++ Library/Formula/ec2-ami-tools.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ec2AmiTools < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/368'
-  url 'http://ec2-downloads.s3.amazonaws.com/ec2-ami-tools-1.4.0.9.zip'
+  url 'http://ec2-downloads.s3.amazonaws.com/ec2-ami-tools-1.5.2.zip'
   sha1 '96583c42b999ce64510fb721af1780719a7f0b34'
 
   def install
