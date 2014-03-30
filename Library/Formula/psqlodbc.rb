a658597303991cf04073d7848bc8d4b27e82e2c5
conscell <aeinner@gmail.com>
2014-03-30 11:38:11 +0900

    PostgreSQL ODBC driver 09.03.0210
    Replaced single quotes with double quotes

--- Library/Formula/psqlodbc.rb
+++ Library/Formula/psqlodbc.rb
@@ -1,26 +1,24 @@
-require 'formula'
+require "formula"
 
 class PostgresqlInstalled < Requirement
   fatal true
-  default_formula 'postgresql'
+  default_formula "postgresql"
 
-  satisfy { which 'pg_config' }
+  satisfy { which "pg_config" }
 end
 
 class Psqlodbc < Formula
-  homepage 'http://www.postgresql.org/'
-  url 'http://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.03.0210.tar.gz'
-  sha1 'e1eb147ef0452e1f7b0f9e102dacb5654a580dba'
+  homepage "http://www.postgresql.org/"
+  url "http://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.03.0210.tar.gz"
+  sha1 "e1eb147ef0452e1f7b0f9e102dacb5654a580dba"
 
-  depends_on 'unixodbc'
-  depends_on 'openssl'
+  depends_on "openssl"
+  depends_on "unixodbc"
   depends_on PostgresqlInstalled
 
   def install
-    args = %W[
-      --prefix=#{prefix}
-      --with-unixodbc=#{Formula['unixodbc'].prefix}
-    ]
+    args = ["--prefix=#{prefix}",
+            "--with-unixodbc=#{Formula['unixodbc'].prefix}"]
 
     system "./configure", *args
     system "make"
