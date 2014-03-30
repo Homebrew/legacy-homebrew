557ef8df05a8ea28ed903f08b2009ebc0e218f5a
Jack Nagel <jacknagel@gmail.com>
2014-03-30 18:00:00 -0500

    Audit gnome URLs, prefer download.gnome.org

--- Library/Homebrew/cmd/audit.rb
+++ Library/Homebrew/cmd/audit.rb
@@ -216,6 +216,10 @@ class FormulaAuditor
     # the rest of the checks apply to mirrors as well
     urls.concat(@specs.map(&:mirrors).flatten)
 
+    urls.grep(%r[^(https?|ftp)://ftp\.gnome]) do |u|
+      problem %{download.gnome.org is preferred for GNOME software (url is #{u}).}
+    end
+
     # Check SourceForge urls
     urls.each do |p|
       # Skip if the URL looks like a SVN repo
