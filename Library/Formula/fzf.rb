d5dfef5069acdd39c7a69343b6147a14ee381cc9
Junegunn Choi <junegunn.c@gmail.com>
2014-03-31 01:07:39 +0900

    fzf 0.8.2

--- Library/Formula/fzf.rb
+++ Library/Formula/fzf.rb
@@ -2,17 +2,21 @@ require "formula"
 
 class Fzf < Formula
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.8.1.tar.gz"
-  sha1 "c77827b6a1ca60eb8273ab20eb97c1f78c375452"
+  url "https://github.com/junegunn/fzf/archive/0.8.2.tar.gz"
+  sha1 "b1f497f4bdedcc317425fcc6daeefed5df03eb0f"
 
   def install
     prefix.install "install", "fzf", "fzf-completion.bash", "fzf-completion.zsh"
     bin.install_symlink prefix/"fzf"
+    (prefix/"plugin").install "plugin/fzf.vim"
   end
 
   def caveats; <<-EOS.undent
     To install useful keybindings and fuzzy completion:
       #{prefix}/install
+
+    To use fzf in Vim, add the following line to your .vimrc:
+      set rtp+=#{prefix}
     EOS
   end
 
