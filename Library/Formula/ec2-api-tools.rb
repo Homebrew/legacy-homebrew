67b0aefc8bfd0b5df70e7b293aa36337f1b11276
John Claus <johnclaus@gmail.com>
2014-03-29 23:29:02 -0600

    New sha1

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ec2ApiTools < AmazonWebServicesFormula
   homepage 'http://aws.amazon.com/developertools/351'
   url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.6.13.0.zip'
-  sha1 'eb0732df59a2ad2c1ad3fa5839e846b69afad697'
+  sha1 '179e9a2111de3cc1fa06629f5faebd3d50d0525c'
 
   def install
     standard_install
