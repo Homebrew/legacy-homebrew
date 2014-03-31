bc11c8ad0ceb9b44e49e055fc499118536eedffe
Arne Jørgensen <arne@arnested.dk>
2014-03-31 21:03:00 +0200

    global 6.2.12

--- Library/Formula/global.rb
+++ Library/Formula/global.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Global < Formula
   homepage 'http://www.gnu.org/software/global/'
-  url 'http://ftpmirror.gnu.org/global/global-6.2.11.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/global/global-6.2.11.tar.gz'
-  sha1 'b0f50213680ec3288988354c34e3b3ae1a42719e'
+  url 'http://ftpmirror.gnu.org/global/global-6.2.12.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/global/global-6.2.12.tar.gz'
+  sha1 '1fc0948ee76185d38733750567a2bdb6b9b07304'
 
   head do
     url 'cvs://:pserver:anonymous:@cvs.savannah.gnu.org:/sources/global:global'
