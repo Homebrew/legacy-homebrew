8a1b4749e467758b32466dc712050bac74f7507c
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2014-03-31 09:24:49 +0200

    pandoc-citeproc: 0.3.0.1

---
+++ Library/Formula/pandoc-citeproc.rb
@@ -0,0 +1,37 @@
+require "formula"
+require "language/haskell"
+
+class PandocCiteproc < Formula
+  include Language::Haskell::Cabal
+
+  homepage "https://github.com/jgm/pandoc-citeproc"
+  url "http://hackage.haskell.org/package/pandoc-citeproc-0.3.0.1/pandoc-citeproc-0.3.0.1.tar.gz"
+  sha1 "958309f9996d563ffba0bd4870bb7f201aac08e8"
+
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
+  depends_on "gmp"
+
+  def install
+    cabal_sandbox do
+      cabal_install_tools "alex", "happy"
+      cabal_install "--only-dependencies", "--constraint=temporary==1.2.0.1"
+      cabal_install "--prefix=#{prefix}"
+    end
+    cabal_clean_lib
+  end
+
+  test do
+    bib = testpath/"test.bib"
+    bib.write <<-EOS.undent
+      @Book{item1,
+      author="John Doe",
+      title="First Book",
+      year="2005",
+      address="Cambridge",
+      publisher="Cambridge University Press"
+      }
+    EOS
+    assert `pandoc-citeproc --bib2yaml #{bib}`.include? "- publisher-place: Cambridge"
+  end
+end
