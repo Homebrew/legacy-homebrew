a0cde13a1513c3ad4f1f72e9e17819bcd6a41a5a
Mike Naberezny <mike@naberezny.com>
2014-04-01 11:57:00 -0700

    visitors: add test

--- Library/Formula/visitors.rb
+++ Library/Formula/visitors.rb
@@ -12,4 +12,13 @@ class Visitors < Formula
     bin.install "visitors"
     man1.install "visitors.1"
   end
+
+  test do
+    IO.popen("#{bin}/visitors - -o text 2>&1", "w+") do |pipe|
+      pipe.puts 'a:80 1.2.3.4 - - [01/Apr/2014:14:28:00 -0400] "GET /1 HTTP/1.1" 200 9 - -'
+      pipe.puts 'a:80 1.2.3.4 - - [01/Apr/2014:14:28:01 -0400] "GET /2 HTTP/1.1" 200 9 - -'
+      pipe.close_write
+      assert pipe.read.include?("Different pages requested: 2")
+    end
+  end
 end
