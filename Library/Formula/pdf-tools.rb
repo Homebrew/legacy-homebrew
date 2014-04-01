f180f6fb652f03f7e413ef3c8818a76a974da9e7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-01 18:41:21 +0100

    pdf-tools: add 0.20 bottle.
    Closes #28019.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pdf-tools.rb
+++ Library/Formula/pdf-tools.rb
@@ -5,6 +5,12 @@ class PdfTools < Formula
   url "https://github.com/axot/pdf-tools/releases/download/v0.20/pdf-tools_v0.20.tgz"
   sha1 "0592f58d8f6dcdc597525ce887abda564eced285"
 
+  bottle do
+    sha1 "424085396d9c3669957be227e3347595fa620b64" => :mavericks
+    sha1 "12acd5174bc22ccaa8d13359a5cdcaee344d83b5" => :mountain_lion
+    sha1 "9275fd218bdc416b140214adb419d8f8201a491f" => :lion
+  end
+
   depends_on "pkg-config" => :build
 
   depends_on :autoconf
