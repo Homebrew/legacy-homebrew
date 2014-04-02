967ba0aa942f27d1b701ffca239bf2c9f7d2a0f6
Mike English <mike.english@atomicobject.com>
2014-04-02 13:30:34 -0400

    Add formula for irssi-otr plugin.

---
+++ Library/Formula/irssi-otr.rb
@@ -0,0 +1,39 @@
+require "formula"
+
+class IrssiOtr < Formula
+  homepage "http://irssi-otr.tuxfamily.org/"
+  url "ftp://download.tuxfamily.org/irssiotr/irssi-otr-0.3.tar.gz"
+  sha1 "5b831dd64e7d84fcda45219b71f017233a6c327b"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on :python => :build
+  depends_on "glib"
+  depends_on "libotr"
+  depends_on "irssi"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      * Irssi's search path for system-wide plugin modules is relative.
+
+      * For Irssi to find irssi-otr, symlink it into your personal config:
+
+      mkdir -p ~/.irssi/modules
+      ln -s #{lib}/irssi/modules/libotr.so ~/.irssi/modules/
+
+      * Documentation:
+
+      #{share}/doc/irssi-otr/README
+    EOS
+    s
+  end
+
+  test do
+    system "test", "-f", "#{lib}/irssi/modules/libotr.so"
+  end
+end
