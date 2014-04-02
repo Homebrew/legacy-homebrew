45a011c4c43e7ad1f75557125980e4d56d29d85c
Collin Allen <commandtab@users.noreply.github.com>
2014-04-02 08:17:19 -0700

    Bump ansible to 1.5.4
    Security fix: https://groups.google.com/forum/#!topic/ansible-project/MUQxiKwSQDc

--- Library/Formula/ansible.rb
+++ Library/Formula/ansible.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ansible < Formula
   homepage 'http://www.ansible.com/home'
-  url 'https://github.com/ansible/ansible/archive/v1.5.3.tar.gz'
+  url 'https://github.com/ansible/ansible/archive/v1.5.4.tar.gz'
   sha1 'a5c2ff6954cc9a6d649827ad9fade76a7e56bf3c'
 
   head 'https://github.com/ansible/ansible.git', :branch => 'devel'
