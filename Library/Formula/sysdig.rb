15c0249c47efb0ad57e5a707991c04a7394f0fa0
Reinaldo Junior <juniorz@gmail.com>
2014-04-03 17:10:44 -0300

    sysdig 0.1.72 (new formula)

---
+++ Library/Formula/sysdig.rb
@@ -0,0 +1,25 @@
+require "formula"
+
+class Sysdig < Formula
+  homepage "http://www.sysdig.org/"
+  url 'https://github.com/draios/sysdig/archive/0.1.72.tar.gz'
+  sha1 'c735c37da82022d22f77490b94f178ad98a1e6f6'
+
+  head 'https://github.com/draios/sysdig.git', :branch => 'development'
+
+  depends_on :macos => :mavericks # depends on clang
+  depends_on "cmake" => :build
+
+  def install
+    ENV.deparallelize
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/sysdig", "-h"
+  end
+end
