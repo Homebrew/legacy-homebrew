d4f47faa2a226b9e35c66ec61ca94e587ebdf84c
Martin Delille <martin@phonations.com>
2014-04-03 12:55:53 +0200

    adding formula for libltc

---
+++ Library/Formula/libltc.rb
@@ -0,0 +1,32 @@
+require "formula"
+
+# Linear (or longitudinal) Timecode (LTC) coder and decoder.
+
+class Libltc < Formula
+  homepage "http://x42.github.io/libltc/"
+  url "https://github.com/x42/libltc/releases/download/v1.1.3/libltc-1.1.3.tar.gz"
+  sha1 "7a5ed324c4a8f87ae1165d8484a153efce18f803"
+
+  patch :DATA
+
+  def install
+    system "./configure"
+    system "make", "install"
+  end
+
+end
+
+__END__
+diff --git a/src/ltc.h b/src/ltc.h
+index d465c4d..65aafaa 100644
+--- a/src/ltc.h
++++ b/src/ltc.h
+@@ -44,7 +44,7 @@
+ # define LTC_BIG_ENDIAN
+ #elif defined _BIG_ENDIAN
+ # define LTC_BIG_ENDIAN
+-#else
++#elif !defined __LITTLE_ENDIAN__
+ # include <endian.h>
+ # if __BYTE_ORDER__ ==  __ORDER_BIG_ENDIAN__
+ #  define LTC_BIG_ENDIAN
