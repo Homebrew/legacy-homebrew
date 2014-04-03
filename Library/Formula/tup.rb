22086f7d021938cfc2d5835036f014e62a101a93
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-03 17:24:32 +0100

    tup: add 0.7.2 bottle.
    Closes #28109.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/tup.rb
+++ Library/Formula/tup.rb
@@ -6,6 +6,13 @@ class Tup < Formula
   sha1 'c0b14a9b7a59c6295ed7339883b21d0f1a8163b3'
   head 'https://github.com/gittup/tup.git'
 
+  bottle do
+    cellar :any
+    sha1 "ea07e07027a25f92959c07844a34e24a62c1929e" => :mavericks
+    sha1 "c7ff37eec01fc7e5049cd5da450b88554f46f2b1" => :mountain_lion
+    sha1 "98255d59e38f854319a60091037fa0f605bcde1f" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'osxfuse'
 
