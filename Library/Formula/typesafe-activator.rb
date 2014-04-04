3ca3d090b0239204e059f1c342b920fbe9d55aba
Renato Cavalcanti <renato@xavena.eu>
2014-04-04 11:20:11 +0200

    typesafe-activator version 1.1.0

--- Library/Formula/typesafe-activator.rb
+++ Library/Formula/typesafe-activator.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class TypesafeActivator < Formula
   homepage 'http://typesafe.com/activator'
-  url 'http://downloads.typesafe.com/typesafe-activator/1.0.13/typesafe-activator-1.0.13.zip'
-  sha1 '34e655823a7931c7f61a978c7d9cbd20881267ac'
+  url 'http://downloads.typesafe.com/typesafe-activator/1.1.0/typesafe-activator-1.1.0.zip'
+  sha1 '1bfc2779c8646f7daac0c3610f77491f65628399'
 
   def install
     # remove Windows .bat files
