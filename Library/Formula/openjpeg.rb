3667390586c73d9d5e7602afb10e9eea455f77d0
David Joy <joy@colorado.edu>
2014-04-04 13:18:29 -0400

    Update openjpeg to 2.0.0
    Switched build system to CMAKE

--- Library/Formula/openjpeg.rb
+++ Library/Formula/openjpeg.rb
@@ -2,18 +2,19 @@ require 'formula'
 
 class Openjpeg < Formula
   homepage 'http://www.openjpeg.org/'
-  url 'https://openjpeg.googlecode.com/files/openjpeg-1.5.1.tar.gz'
-  sha1 '1b0b74d1af4c297fd82806a9325bb544caf9bb8b'
-  revision 1
+  url 'https://downloads.sourceforge.net/project/openjpeg.mirror/openjpeg-2.0.0.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fopenjpeg.mirror%2Ffiles%2F%3Fsource%3Ddlp'
+  sha1 '0af78ab2283b43421458f80373422d8029a9f7a7'
 
   head 'http://openjpeg.googlecode.com/svn/trunk/'
+
+  depends_on "cmake" => :build
 
   depends_on 'little-cms2'
   depends_on 'libtiff'
   depends_on 'libpng'
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "cmake", ".", *std_cmake_args
     system "make install"
   end
 end
