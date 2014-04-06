a3f82db476fd3d3dbaf48acfd1f2f8a075326712
industrie13 <industrie13@gmx.net>
2014-04-06 22:44:53 +0200

    removed subversion dependecy, i18n-de unconditional, removed blank line 24 +30

--- Library/Formula/eric5.rb
+++ Library/Formula/eric5.rb
@@ -9,28 +9,20 @@ class Eric5 < Formula
   depends_on "qscintilla2" => "with-python3"
   depends_on "sip" => "with-python3"
   depends_on "pyqt" => "with-python3"
-  depends_on "subversion" => [:optional, "with-python"]
   depends_on "enchant" => :optional
   option 'with-i18n-de' , "Enable German language support"
 
-  if build.with? 'i18n-de'
-    resource 'i18n-de' do
-      url 'https://downloads.sourceforge.net/project/eric-ide/eric5/stable/5.4.3/eric5-i18n-de-5.4.3.tar.gz'
-      sha1 'a09c1a9aae56f1128400f1ea3b4ca95970a0168d'
-    end
+  resource 'i18n-de' do
+    url 'https://downloads.sourceforge.net/project/eric-ide/eric5/stable/5.4.3/eric5-i18n-de-5.4.3.tar.gz'
+    sha1 'a09c1a9aae56f1128400f1ea3b4ca95970a0168d'
   end
 
   def install
-
     system "python3", "install.py", "install", "--prefix=#{prefix}"
 
     if build.with? 'i18n-de'
       resource('i18n-de').stage { system "python3", "install-i18n.py", "install", "--prefix=#{prefix}" }
     end
-
   end
 
- test do
-   system "#{bin}/eric5", "--version"
-  end
 end
