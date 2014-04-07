cdca9afe815afe1cefcfc204b59e93489362fa04
Jack Nagel <jacknagel@gmail.com>
2014-04-07 13:21:36 -0500

    openssl 1.0.1g

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Openssl < Formula
   homepage 'http://openssl.org'
-  url 'https://www.openssl.org/source/openssl-1.0.1f.tar.gz'
-  mirror 'http://mirrors.ibiblio.org/openssl/source/openssl-1.0.1f.tar.gz'
-  sha256 '6cc2a80b17d64de6b7bac985745fdaba971d54ffd7d38d3556f998d7c0c9cb5a'
+  url 'https://www.openssl.org/source/openssl-1.0.1g.tar.gz'
+  mirror 'http://mirrors.ibiblio.org/openssl/source/openssl-1.0.1g.tar.gz'
+  sha256 '53cb818c3b90e507a8348f4f5eaedb05d8bfe5358aabb508b7263cc670c3e028'
 
   bottle do
     sha1 "2687c0abb5e23d765bbd0024a010e36b05a8939e" => :mavericks
