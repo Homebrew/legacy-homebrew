376b3fe3ce5d731511b757aebc6899716b325395
Alex Regueiro <alex@noldorin.com>
2014-04-09 00:32:28 +0100

    imapsync: stylistic changes to formula
    Closes #28259.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/imapsync.rb
+++ Library/Formula/imapsync.rb
@@ -1,15 +1,15 @@
 require "formula"
 
 class Imapsync < Formula
-  url "https://fedorahosted.org/released/imapsync/imapsync-1.584.tgz"
   homepage "http://ks.lamiral.info/imapsync/"
+  url "https://fedorahosted.org/released/imapsync/imapsync-1.584.tgz"
   sha1 "c1de4558a2379416a9af4064e8922efab08e664d"
+
+  head "https://git.fedorahosted.org/git/imapsync.git"
 
   depends_on "File::Copy::Recursive" => :perl
   depends_on "Mail::IMAPClient" => :perl
   depends_on "Authen::NTLM" => :perl
-
-  head "https://git.fedorahosted.org/git/imapsync.git"
 
   def install
     system "perl", "-c", "imapsync"
@@ -17,5 +17,4 @@ class Imapsync < Formula
     bin.install "imapsync"
     man1.install "imapsync.1"
   end
-
 end
