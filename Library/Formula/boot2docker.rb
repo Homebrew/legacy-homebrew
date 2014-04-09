bb76bb021c1747465af90f539e7b5f1cba2ad2ee
mainyaa <kazuyuki.mori@topgate.ne.jp>
2014-04-09 16:43:29 +0900

    boot2docker 0.8.0
    Closes #28270.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -3,8 +3,8 @@ require "formula"
 class Boot2docker < Formula
   homepage "https://github.com/boot2docker/boot2docker"
   head "https://github.com/boot2docker/boot2docker.git"
-  url 'https://github.com/boot2docker/boot2docker/archive/v0.7.1.tar.gz'
-  sha1 '447ab4bd75d31654f93e546bab20a9ba6e1a4dcd'
+  url 'https://github.com/boot2docker/boot2docker/archive/v0.8.0.tar.gz'
+  sha1 'c6f07db7d3a4952f1508b92f8b32e02e2f316f85'
 
   depends_on "docker" => :recommended
 
