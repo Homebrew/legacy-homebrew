6ed9972d67c6c2401a91febe521a327aa88ed6af
Sam <homebrew@munkynet.org>
2014-04-09 17:16:26 -0400

    Fixes as requested by Homebrew maintainers: - Use double-quotes. - White space/line breaks/line lengths. - Submit patches upstream. Found a better solution for the OpenSSL library location issues: - Pass --with-ssl-dir to configure. - Remove SSLINCLUDE, SSLLIB, etc. from SPECIALS in imap/Makefile. - No longer requires autoconf.

--- Library/Formula/alpine-chappa.rb
+++ Library/Formula/alpine-chappa.rb
@@ -1,61 +1,52 @@
-require 'formula'
+require "formula"
 
 class AlpineChappa < Formula
-  homepage 'http://patches.freeiz.com/alpine/'
-  url 'http://patches.freeiz.com/alpine/release/src/alpine-2.11.tar.xz'
-  sha1 '656556f5d2e5ec7e3680d1760cd02aa3a0072c46'
+  homepage "http://patches.freeiz.com/alpine/"
+  url "http://patches.freeiz.com/alpine/release/src/alpine-2.11.tar.xz"
+  sha1 "656556f5d2e5ec7e3680d1760cd02aa3a0072c46"
 
-  depends_on 'autoconf' => :build
-  depends_on 'openssl'
-  conflicts_with 'alpine'
+  depends_on "openssl"
+
+  conflicts_with "alpine", :because => "both install an `alpine` binary"
 
   def patches
     p = []
-    # Upstream builds are broken on Snow Leopard due to a hack put in
-    # for prior versions of OS X. See:
-    # http://trac.macports.org/ticket/20971
-    p <<  "https://trac.macports.org/export/89747/trunk/dports/mail/alpine/files/alpine-osx-10.6.patch" if MacOS.version >= :snow_leopard
-    # 1. Fix typo "oxs" in configure.ac.
-    # 2. Don't hardcode OpenSSL location in imap Makefile.
-    # 3. #ifdef DEBUG for stray use of global debug level var in web-alpine LDAP module.
+    # Upstream builds are broken on Snow Leopard due to a hack put in for prior
+    # versions of OS X. See: http://trac.macports.org/ticket/20971
+    if MacOS.version >= :snow_leopard
+      p <<  "https://trac.macports.org/export/89747/trunk/dports/mail/alpine/files/alpine-osx-10.6.patch"
+    end
+    # Two patches that fix build problems:
+    # 1. Don't hardcode OpenSSL location in imap Makefile. Homebrew-specific, so
+    #    not submitted upstream. Submitted upstream to chappa@gmx.com.
+    # 2. #ifdef DEBUG for stray use of global debug level var in web-alpine LDAP
+    #    module. Submitted upstream to chappa@gmx.com.
     p << DATA
   end
 
   def install
     ENV.j1
-    system "#{Formula.factory('autoconf').opt_prefix}/bin/autoconf configure.ac > configure"
     system "./configure", "--disable-debug",
-                          "--with-ssl-certs-dir=#{etc}/openssl/certs",
+                          "--with-ssl-dir=#{Formula['openssl'].prefix}",
+                          "--with-ssl-certs-dir=#{etc}/openssl",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
 end
 
 __END__
-diff -ur a/configure.ac b/configure.ac
---- a/configure.ac	2014-02-04 23:45:14.000000000 -0500
-+++ b/configure.ac	2014-02-04 23:45:37.000000000 -0500
-@@ -972,7 +972,7 @@
- 	AC_CHECK_HEADER([security/pam_appl.h],
- 	[
- 	  alpine_PAM="yes"
--	  alpine_c_client_target="oxs"
-+	  alpine_c_client_target="osx"
- 	],
- 	[	AC_CHECK_HEADER([pam/pam_appl.h],
- 		[
 diff -ur alpine-2.11.OLD/imap/Makefile alpine-2.11.NEW/imap/Makefile
 --- alpine-2.11.OLD/imap/Makefile	2013-08-15 00:36:13.000000000 -0400
-+++ alpine-2.11.NEW/imap/Makefile	2014-02-06 10:24:35.000000000 -0500
-@@ -432,7 +432,7 @@
++++ alpine-2.11.NEW/imap/Makefile	2014-04-09 16:37:45.000000000 -0400
+@@ -427,7 +427,7 @@
+ 	$(TOUCH) ip6
+ 	$(BUILD) BUILDTYPE=osx IP=$(IP6) EXTRAAUTHENTICATORS="$(EXTRAAUTHENTICATORS) gss" \
+ 	PASSWDTYPE=pam \
+-	SPECIALS="SSLINCLUDE=/usr/include/openssl SSLLIB=/usr/lib SSLCERTS=/System/Library/OpenSSL/certs SSLKEYS=/System/Library/OpenSSL/private GSSINCLUDE=/usr/include GSSLIB=/usr/lib PAMDLFLAGS=-lpam"
++	SPECIALS="GSSINCLUDE=/usr/include GSSLIB=/usr/lib PAMDLFLAGS=-lpam"
+
  osx:	osxok an
  	$(TOUCH) ip6
- 	$(BUILD) BUILDTYPE=$@ IP=$(IP6) EXTRAAUTHENTICATORS="$(EXTRAAUTHENTICATORS) gss" \
--	SPECIALS="SSLINCLUDE=/usr/include/openssl SSLLIB=/usr/lib SSLCERTS=/System/Library/OpenSSL/certs SSLKEYS=/System/Library/OpenSSL/private GSSINCLUDE=/usr/include GSSLIB=/usr/lib"
-+	SPECIALS="SSLINCLUDE=HOMEBREW_PREFIX/opt/openssl/include/openssl SSLLIB=HOMEBREW_PREFIX/opt/openssl/lib SSLCERTS=HOMEBREW_PREFIX/etc/openssl/certs SSLKEYS=HOMEBREW_PREFIX/etc/openssl/private GSSINCLUDE=/usr/include GSSLIB=/usr/lib"
- 
- osxok:
- 	@$(SH) -c '(test ! -f /usr/include/pam/pam_appl.h ) || make osxwarn'
 diff -urN alpine-2.11.OLD/web/src/alpined.d/alpineldap.c alpine-2.11.NEW/web/src/alpined.d/alpineldap.c
 --- alpine-2.11.OLD/web/src/alpined.d/alpineldap.c	2013-08-15 00:36:01.000000000 -0400
 +++ alpine-2.11.NEW/web/src/alpined.d/alpineldap.c	2014-02-06 11:47:22.000000000 -0500
