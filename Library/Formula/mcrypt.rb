1a2c1a5c165f8d81cea4a1dec27cc1fd2b545ef6
Sergey Polovko <jamel@yandex-team.ru>
2014-04-09 20:53:04 +0400

    use double-quotes

--- Library/Formula/mcrypt.rb
+++ Library/Formula/mcrypt.rb
@@ -1,15 +1,15 @@
-require 'formula'
+require "formula"
 
 class Mcrypt < Formula
-  homepage 'http://mcrypt.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/mcrypt/MCrypt/2.6.8/mcrypt-2.6.8.tar.gz'
-  sha1 '8ae0e866714fbbb96a0a6fa9f099089dc93f1d86'
+  homepage "http://mcrypt.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/mcrypt/MCrypt/2.6.8/mcrypt-2.6.8.tar.gz"
+  sha1 "8ae0e866714fbbb96a0a6fa9f099089dc93f1d86"
 
-  depends_on 'mhash'
+  depends_on "mhash"
 
-  resource 'libmcrypt' do
-    url 'https://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz'
-    sha1 '9a426532e9087dd7737aabccff8b91abf9151a7a'
+  resource "libmcrypt" do
+    url "https://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz"
+    sha1 "9a426532e9087dd7737aabccff8b91abf9151a7a"
   end
 
   option :universal
@@ -23,7 +23,7 @@ class Mcrypt < Formula
   def install
     ENV.universal_binary if build.universal?
 
-    resource('libmcrypt').stage do
+    resource("libmcrypt").stage do
       system "./configure", "--prefix=#{prefix}",
                             "--mandir=#{man}"
       system "make install"
