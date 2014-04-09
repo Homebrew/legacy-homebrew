797e27beb3c79fb06b771565e84751f16e32c618
Mike McQuaid <mike@mikemcquaid.com>
2014-04-09 11:25:30 +0100

    libssh: bump revision for OpenSSL security.

--- Library/Formula/libssh2.rb
+++ Library/Formula/libssh2.rb
@@ -4,6 +4,7 @@ class Libssh2 < Formula
   homepage 'http://www.libssh2.org/'
   url 'http://www.libssh2.org/download/libssh2-1.4.3.tar.gz'
   sha1 'c27ca83e1ffeeac03be98b6eef54448701e044b0'
+  revision 1
 
   bottle do
     cellar :any
