ff9ef4a103de7a79568df9c2a8c3e1b414ede6f4
Adam Vandenberg <flangy@gmail.com>
2014-04-08 20:07:59 -0700

    libbluray: style updates

--- Library/Formula/libbluray.rb
+++ Library/Formula/libbluray.rb
@@ -1,20 +1,20 @@
-require 'formula'
+require "formula"
 
 class Libbluray < Formula
-  homepage 'http://www.videolan.org/developers/libbluray.html'
-  url 'ftp://ftp.videolan.org/pub/videolan/libbluray/0.5.0/libbluray-0.5.0.tar.bz2'
-  sha1 '1a9c61daefc31438f9165e7681c563d0524b2d3e'
+  homepage "http://www.videolan.org/developers/libbluray.html"
+  url "ftp://ftp.videolan.org/pub/videolan/libbluray/0.5.0/libbluray-0.5.0.tar.bz2"
+  sha1 "1a9c61daefc31438f9165e7681c563d0524b2d3e"
 
   head do
-    url 'git://git.videolan.org/libbluray.git'
+    url "git://git.videolan.org/libbluray.git"
 
     depends_on :automake => :build
     depends_on :autoconf => :build
     depends_on :libtool  => :build
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on :freetype => :recommended
+  depends_on "pkg-config" => :build
+  depends_on "freetype" => :recommended
 
   def install
     # https://mailman.videolan.org/pipermail/libbluray-devel/2014-April/001401.html
