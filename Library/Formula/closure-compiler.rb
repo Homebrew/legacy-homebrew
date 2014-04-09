9cf55df1936ec416da69bdd3918bd07bd786b8d4
Adam Vandenberg <flangy@gmail.com>
2014-04-09 07:42:51 -0700

    Revert "closure-compiler: requires Java 7"
    This reverts commit 8bca3b24ea4256444fadc78f67d3a7eb9ad3b63d.

--- Library/Formula/closure-compiler.rb
+++ Library/Formula/closure-compiler.rb
@@ -1,17 +1,5 @@
 require 'formula'
 
-class Java7 < Requirement
-  fatal true
-
-  def satisfied?
-    `java -version`.split("\n")[0] =~ /"1\.7/
-  end
-
-  def message
-    "Java 7 is required."
-  end
-end
-
 class ClosureCompiler < Formula
   homepage 'http://code.google.com/p/closure-compiler/'
   url 'https://code.google.com/p/closure-compiler/', :using => :git, :tag => 'v20140303'
@@ -19,7 +7,6 @@ class ClosureCompiler < Formula
   head 'https://code.google.com/p/closure-compiler/', :using => :git
 
   depends_on :ant
-  depends_on Java7
 
   def install
     system "ant", "clean"
