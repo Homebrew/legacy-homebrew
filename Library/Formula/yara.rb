3360e74875c90efa40ab1e649330ec76f84c5a06
Scott Roberts <sroberts@github.com>
2014-04-10 13:44:10 -0400

    Updated Yara to version 2.1

--- Library/Formula/yara.rb
+++ Library/Formula/yara.rb
@@ -2,21 +2,25 @@ require 'formula'
 
 class Yara < Formula
   homepage 'http://code.google.com/p/yara-project/'
-  url 'https://yara-project.googlecode.com/files/yara-1.7.tar.gz'
-  sha1 '25e90b79275124db3e592ccac1d44a9bba35d7ea'
+  url 'https://github.com/plusvic/yara/archive/2.1.0.tar.gz'
+  sha1 '8289c281a44c933e11de25953f3910fe9f8ee82e'
 
   depends_on 'pcre'
+  depends_on 'libtool'
+  depends_on 'automake'
 
   def install
-    # Use of 'inline' requires gnu89 semantics
+    # Use of 'inline' requires gnu89 semantis
     ENV.append 'CFLAGS', '-std=gnu89' if ENV.compiler == :clang
 
     # find Homebrew's libpcre
     ENV.append 'LDFLAGS', "-L#{Formula["pcre"].opt_lib} -lpcre"
 
+    system "bash build.sh"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+    system "make"
     system "make install"
   end
 end
