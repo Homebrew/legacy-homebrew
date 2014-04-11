ac61040826387b11d0cfa75c7901a24d7e3210e2
Mike Naberezny <mike@naberezny.com>
2014-04-11 16:11:16 -0700

    yara: add test
    Closes #28340.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/yara.rb
+++ Library/Formula/yara.rb
@@ -23,4 +23,28 @@ class Yara < Formula
                           "--prefix=#{prefix}"
     system "make install"
   end
+
+  test do
+    rules = testpath/"commodore.yara"
+    rules.write <<-EOS.undent
+      rule chrout {
+        meta:
+          description = "Calls CBM KERNAL routine CHROUT"
+        strings:
+          $jsr_chrout = {20 D2 FF}
+          $jmp_chrout = {4C D2 FF}
+        condition:
+          $jsr_chrout or $jmp_chrout
+      }
+    EOS
+
+    program = testpath/"zero.prg"
+    File.open(program, "wb") do |f|
+      f.write [0x00, 0xc0, 0xa9, 0x30, 0x4c, 0xd2, 0xff].pack("C*")
+    end
+
+    out = `#{bin}/yara #{rules} #{program}`
+    assert_equal "chrout #{program}\n", out
+    assert_equal 0, $?.exitstatus
+  end
 end
