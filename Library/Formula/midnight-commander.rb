68245a9c04c4fa7ba48e10ce2d5ae4cccf738ffe
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-11 09:34:06 +0100

    midnight-commander: add 4.8.12 bottle.
    Closes #28317.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -6,6 +6,12 @@ class MidnightCommander < Formula
   mirror 'ftp://ftp.osuosl.org/pub/midnightcommander/mc-4.8.12.tar.xz'
   sha256 '5f2fd570a798dc0cc06374adffef8ca403588c4e73dfdf908e9a4311718153fe'
 
+  bottle do
+    sha1 "7b5b58bbf0cbb0dbb5466e9389549de7de7b7e02" => :mavericks
+    sha1 "1c2f103e0e920a41af53fcacc315dc28e5089560" => :mountain_lion
+    sha1 "97a5c81e713a32966a62292252cfba29c4044e76" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'openssl' if MacOS.version <= :leopard
