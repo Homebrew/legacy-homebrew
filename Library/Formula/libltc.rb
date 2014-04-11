a51fba2145c9b0f0cae612b164a4468e3cac04ba
MartinDelille <martin@phonations.com>
2014-04-11 16:17:33 +0200

    add quote, remove end, fix comment

--- Library/Formula/libltc.rb
+++ Library/Formula/libltc.rb
@@ -5,9 +5,9 @@ class Libltc < Formula
   url "https://github.com/x42/libltc/releases/download/v1.1.3/libltc-1.1.3.tar.gz"
   sha1 "7a5ed324c4a8f87ae1165d8484a153efce18f803"
 
-# This patch has been fixed in the master branch not released yet (https://github.com/x42/libltc/commit/b98e5d4094fbbc637fc83fe25d8348e41c325cf8)
+  # This patch has been fixed in the master branch not released yet 
   patch do
-    url https://github.com/x42/libltc/compare/febc9138ed73e5e2ce9be701bfac3b53bb310c66...b98e5d4094fbbc637fc83fe25d8348e41c325cf8.diff
+    url "https://github.com/x42/libltc/compare/febc9138ed73e5e2ce9be701bfac3b53bb310c66...b98e5d4094fbbc637fc83fe25d8348e41c325cf8.diff"
   end
 
   def install
@@ -17,4 +17,3 @@ class Libltc < Formula
 
 end
 
-__END__
