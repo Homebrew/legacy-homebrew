b679bf2c215b29dc2cecfb0cd81c2d34cc02b9cd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-11 09:39:49 +0100

    cloudfoundry-cli: add 6.1.0 bottle.
    Closes #28319.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cloudfoundry-cli.rb
+++ Library/Formula/cloudfoundry-cli.rb
@@ -5,6 +5,12 @@ class CloudfoundryCli < Formula
   head 'https://github.com/cloudfoundry/cli.git', :branch => 'master'
   url 'https://github.com/cloudfoundry/cli.git', :tag => 'v6.1.0'
 
+  bottle do
+    sha1 "5ce0c33f8057cfa5ba85a17fd6683b24158a0571" => :mavericks
+    sha1 "c63fe614f058781e8523b5c1905a2ec0045dc89d" => :mountain_lion
+    sha1 "11aede84b5e4f4656688f587d819e0b8b8cfd19e" => :lion
+  end
+
   depends_on 'go' => :build
 
   def install
