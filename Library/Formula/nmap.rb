e2f6fe9b4bc7cadaa6a8c488193ae233a5a4f1e7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-13 11:06:16 +0100

    nmap: add 6.45 bottle.
    Closes #28368.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/nmap.rb
+++ Library/Formula/nmap.rb
@@ -6,6 +6,12 @@ class Nmap < Formula
   url "http://nmap.org/dist/nmap-6.45.tar.bz2"
   sha1 "3a634d319c57f6d93e5ccb69b45c133690d437e1"
 
+  bottle do
+    sha1 "15741e8fb3f58776fd1245782585d832fe08c1bf" => :mavericks
+    sha1 "76ab79880f2ad3db2f965645aa8665d69442f137" => :mountain_lion
+    sha1 "276181d845c199735c559031acb893b5b27de561" => :lion
+  end
+
   depends_on "openssl"
 
   conflicts_with 'ndiff', :because => 'both install `ndiff` binaries'
