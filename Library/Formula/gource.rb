fbb8c657f4c9fc9e37000b8cb53f47ad2fe7d234
Sebastian Staudt <koraktor@gmail.com>
2014-04-14 20:30:47 +0200

    Update Gource to 0.41

--- Library/Formula/gource.rb
+++ Library/Formula/gource.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gource < Formula
   homepage 'http://code.google.com/p/gource/'
-  url 'https://gource.googlecode.com/files/gource-0.40.tar.gz'
-  sha1 '7af594f84c0ec4c84278a8e9008f83a7a02e97fa'
+  url 'https://github.com/acaudwell/Gource/releases/download/gource-0.41/gource-0.41.tar.gz'
+  sha1 '75aa1e2c5afc2f91b54629d086f2a80bf3b553e5'
 
   head do
     url 'https://github.com/acaudwell/Gource.git'
@@ -23,8 +23,8 @@ class Gource < Formula
   depends_on 'glew'
   depends_on 'jpeg'
   depends_on 'pcre'
-  depends_on 'sdl'
-  depends_on 'sdl_image'
+  depends_on 'sdl2'
+  depends_on 'sdl2_image'
 
   def install
     # For non-/usr/local installs
