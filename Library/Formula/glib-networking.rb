8b87a4628135463f3bdea98ab7679c45f2ea0e94
Jack Nagel <jacknagel@gmail.com>
2014-04-14 18:50:39 -0500

    Stop bottling glib-networking (for now)
    This package installs files into the top-level gio modules directory,
    thus the bottle only installs a bunch of locale files.

--- Library/Formula/glib-networking.rb
+++ Library/Formula/glib-networking.rb
@@ -5,13 +5,6 @@ class GlibNetworking < Formula
   url 'http://ftp.gnome.org/pub/GNOME/sources/glib-networking/2.40/glib-networking-2.40.0.tar.xz'
   sha256 '54e9c2d81171353794d2d2113e1311dfafd889f10993099e447b431c4e800aa4'
 
-  bottle do
-    cellar :any
-    sha1 "1a1d55373f1ec279931a15c0313d7f7c9b7bf0a5" => :mavericks
-    sha1 "e4fc2cae57ba13b9becfefb8869c6e18c62d1dcc" => :mountain_lion
-    sha1 "67dbfa7f9fd9220690fcd3e0eb9f70ffcae5f3a9" => :lion
-  end
-
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
   depends_on 'gettext'
