5773b5f51a4d1b31cabcf298ab6152547a1eb3aa
Sebastian Staudt <koraktor@gmail.com>
2014-04-14 20:30:47 +0200

    gource 0.41
    Closes #28397.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/gource.rb
+++ Library/Formula/gource.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gource < Formula
   homepage 'http://code.google.com/p/gource/'
-  url 'https://gource.googlecode.com/files/gource-0.40.tar.gz'
-  sha1 '7af594f84c0ec4c84278a8e9008f83a7a02e97fa'
+  url 'https://github.com/acaudwell/Gource/releases/download/gource-0.41/gource-0.41.tar.gz'
+  sha1 '75aa1e2c5afc2f91b54629d086f2a80bf3b553e5'
 
   head do
     url 'https://github.com/acaudwell/Gource.git'
@@ -19,14 +19,25 @@ class Gource < Formula
   depends_on 'pkg-config' => :build
   depends_on 'glm' => :build
 
-  depends_on 'boost'
+  if MacOS.version < :mavericks
+    depends_on 'boost' => 'c++11'
+  else
+    depends_on 'boost'
+  end
+
   depends_on 'glew'
   depends_on 'jpeg'
   depends_on 'pcre'
-  depends_on 'sdl'
-  depends_on 'sdl_image'
+  depends_on 'sdl2'
+  depends_on 'sdl2_image'
+
+  needs :cxx11
 
   def install
+    # clang on Mt. Lion will try to build against libstdc++,
+    # despite -std=gnu++0x
+    ENV.libcxx
+
     # For non-/usr/local installs
     ENV.append "CXXFLAGS", "-I#{HOMEBREW_PREFIX}/include"
 
