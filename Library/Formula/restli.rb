4231e82afcbce1266a1331d89f8e9f16cc39e3b2
Joe Betz <jbetz@linkedin.com>
2014-04-15 15:26:24 -0700

    remove whitespace

--- Library/Formula/restli.rb
+++ Library/Formula/restli.rb
@@ -4,7 +4,7 @@ class Restli < Formula
   homepage "http://rest.li/"
   url "http://rest.li/releases/restli-tool/0.0.1/restli-0.0.1.tar.gz"
   sha1 "1b11f3012d112f63250f5721dd82a501b1e42014"
-  
+
   depends_on "giter8"
 
   def install
