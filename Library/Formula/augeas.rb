9c4a4c7e0bfce65cddf621b73ca11807bd2489e1
Charlie Sharpsteen <source@sharpsteen.net>
2014-04-14 22:59:21 -0700

    augeas: Add --HEAD build

--- Library/Formula/augeas.rb
+++ Library/Formula/augeas.rb
@@ -5,11 +5,28 @@ class Augeas < Formula
   url 'http://download.augeas.net/augeas-1.2.0.tar.gz'
   sha1 'ab63548ae5462d7b3dc90e74311b8e566ba22485'
 
+  head do
+    url 'https://github.com/hercules-team/augeas.git'
+
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+    depends_on 'bison' => :build
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libxml2'
   depends_on 'readline'
 
   def install
+    if build.head?
+      # The bootstrap script run by autogen needs to check the state of the
+      # gnulib submodule.
+      ln_s cached_download + '.git', '.git'
+      ln_s cached_download + '.gnulib/.git', '.gnulib/.git'
+
+      system './autogen.sh'
+    end
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
 
