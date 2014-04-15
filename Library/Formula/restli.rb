f90ed03fceb6b0eea05fe48ed63c01170d1d9d4a
Joe Betz <jbetz@linkedin.com>
2014-04-15 16:48:38 -0700

    fix test, make it test something meaningful

--- Library/Formula/restli.rb
+++ Library/Formula/restli.rb
@@ -3,7 +3,7 @@ require "formula"
 class Restli < Formula
   homepage "http://rest.li/"
   url "http://rest.li/releases/restli-tool/0.0.1/restli-0.0.1.tar.gz"
-  sha1 "1b11f3012d112f63250f5721dd82a501b1e42014"
+  sha1 "38078f768fe463f39db2f69f896fa428ea7b4123"
 
   depends_on "giter8"
 
@@ -12,6 +12,7 @@ class Restli < Formula
   end
 
   test do
-    system "#{bin}/restli"
+    (testpath/'input.txt').write('\norg.example\nfortunes\nfortunes\nFortune')
+    system "#{bin}/restli", 'new', '--name=Example'
   end
 end
