cb5e6ed859468382e9916f5a89ce465636db68eb
Josa Gesell <josa@gesell.me>
2014-04-15 08:43:07 +0200

    x3270 3.3.14ga9

--- Library/Formula/x3270.rb
+++ Library/Formula/x3270.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class X3270 < Formula
   homepage 'http://x3270.bgp.nu/'
-  url 'https://downloads.sourceforge.net/project/x3270/x3270/3.3.14ga6/suite3270-3.3.14ga6-src.tgz'
-  sha1 '4788ea7396d4aff04685382e82c440689e8014aa'
+  url 'https://downloads.sourceforge.net/project/x3270/x3270/3.3.14ga9/suite3270-3.3.14ga9-src.tgz'
+  sha1 '84ccfd84b451adf70e38903dd20850c87832b1da'
 
   depends_on :x11
 
