f13e4079bdd7cb2ae921c810a5717b0ec4727f31
Adam Vandenberg <flangy@gmail.com>
2014-04-15 07:44:21 -0700

    aldo: fix use of iterator

--- Library/Formula/aldo.rb
+++ Library/Formula/aldo.rb
@@ -7,8 +7,38 @@ class Aldo < Formula
 
   depends_on 'libao'
 
+  patch :DATA
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
 end
+
+__END__
+diff --git a/src/menu.cc b/src/menu.cc
+index 483b826..092d604 100644
+--- a/src/menu.cc
++++ b/src/menu.cc
+@@ -112,20 +112,17 @@ void Menu::add_item(id_type id, std::string c, Function2 f)
+ 
+ void Menu::add_item_at(unsigned int pos, id_type id, std::string c, Function1 f)
+ {
+-    IT it(&m_its[pos]);
+-    m_its.insert(it, Item(id,c,f) );
++    m_its.insert(m_its.begin()+pos, Item(id,c,f) );
+ }
+ 
+ void Menu::add_item_at(unsigned int pos, id_type id, std::string c, Function2 f)
+ {
+-    IT it(&m_its[pos]);
+-    m_its.insert(it, Item(id,c,f) );
++    m_its.insert(m_its.begin()+pos, Item(id,c,f) );
+ }
+ 
+ void Menu::delete_item_at(unsigned int pos)
+ {
+-    IT it(&m_its[pos]);
+-    m_its.erase(it);
++    m_its.erase(m_its.begin()+pos);
+ }
