96433f6e1555f892816b0163bfd40e2ec47d7f30
Brett Koonce <koonce@gmail.com>
2014-04-15 11:10:46 -0500

    wdiff 1.2.2
    Closes #28425.

--- Library/Formula/wdiff.rb
+++ Library/Formula/wdiff.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Wdiff < Formula
   homepage 'http://www.gnu.org/software/wdiff/'
-  url 'http://ftpmirror.gnu.org/wdiff/wdiff-1.2.1.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/wdiff/wdiff-1.2.1.tar.gz'
-  sha1 '6b5c7c97252cfe607bb1db7db148061be7259a7e'
+  url 'http://ftpmirror.gnu.org/wdiff/wdiff-1.2.2.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/wdiff/wdiff-1.2.2.tar.gz'
+  sha1 'c93b5cb593257d814212e15fc371ff6c6b952c3d'
 
   depends_on 'gettext' => :optional
 
