8cffcfd1c4a5ea05f79944df7ca6622ba07cf5d6
Jack Nagel <jacknagel@gmail.com>
2014-04-15 15:38:31 -0500

    Don't use assert_nothing_raised

--- Library/Homebrew/test/test_utils.rb
+++ Library/Homebrew/test/test_utils.rb
@@ -2,9 +2,7 @@ require 'testing_env'
 
 class UtilTests < Test::Unit::TestCase
   def test_put_columns_empty
-    assert_nothing_raised do
-      # Issue #217 put columns with new results fails.
-      puts_columns []
-    end
+    # Issue #217 put columns with new results fails.
+    puts_columns []
   end
 end
