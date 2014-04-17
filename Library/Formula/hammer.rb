052f3954aafe0f2f5607174e6e29aa44fe832c25
Vyrus <vyrus001@gmail.com>
2014-04-16 19:08:43 -0700

    fixed format issues and made scons work right, also incremented hammer version to rc3

--- Library/Formula/hammer.rb
+++ Library/Formula/hammer.rb
@@ -1,18 +1,16 @@
 require "formula"
 
-class 	Hammer < Formula
-	url "https://github.com/UpstandingHackers/hammer/archive/v1.0.0-rc2.tar.gz"
-	homepage "https://github.com/UpstandingHackers/hammer"
-	sha1 "2a38f8f5107afa52d5698c38b43d30e0f2fa0694"
-	version "1.0.0-rc2"
+class Hammer < Formula
+ url "https://github.com/UpstandingHackers/hammer/archive/v1.0.0-rc3.tar.gz"
+ homepage "https://github.com/UpstandingHackers/hammer"
+ sha1 "3e578d7f0e0a201aea7707c6041e48ec10ed7609"
+ version "1.0.0-rc3"
 
-	depends_on "scons" => :build
+ depends_on "scons" => :build
+ depends_on "pkg-config" => :build
+ depends_on "glib" => :build
 
-	depends_on "pkg-config" => :recomended
-	depends_on "glib" => :recomended
-
-	def install
-    	system "scons"
-    	system "scons install"
-	end
+ def install
+ scons "install"
+ end
 end
