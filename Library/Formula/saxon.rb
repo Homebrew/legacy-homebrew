1b4a81382963a52b533bac2a6ced751ff85d20fe
Brett Koonce <koonce@gmail.com>
2014-04-18 17:23:10 -0500

    saxon 9.5.1.5

--- Library/Formula/saxon.rb
+++ Library/Formula/saxon.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Saxon < Formula
   homepage "http://saxon.sourceforge.net"
-  url 'https://downloads.sourceforge.net/project/saxon/Saxon-HE/9.5/SaxonHE9-5-1-4J.zip'
-  sha1 '10910a1ced9c1a485a6084d4a6f1a6793645a798'
-  version '9.5.1.4'
+  url 'https://downloads.sourceforge.net/project/saxon/Saxon-HE/9.5/SaxonHE9-5-1-5J.zip'
+  sha1 'bb8476866cacb72e5567bdfc246570e7f0986e48'
+  version '9.5.1.5'
 
   def install
     libexec.install Dir["*"]
