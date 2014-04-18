b19990073f5aaa415315c40e4edf7fc6d3869e8d
Martin Joehren <m.joehren@gmail.com>
2014-04-18 11:27:35 +0200

    added HashiCorp consul formula

---
+++ Library/Formula/consul.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+# Consul has multiple components, but as a whole,
+# it is a tool for discovering and configuring services in your infrastructure
+
+class Consul < Formula
+  homepage "http://www.consul.io/"
+  url "https://dl.bintray.com/mitchellh/consul/0.1.0_darwin_amd64.zip"
+  sha1 "08dfd233011e6fe77928b6dd5d0221b5379250ce"
+  version "0.1.0"
+
+  def install
+    bin.install "consul"
+  end
+
+  test do
+    system "#{bin}/consul", "version"
+  end
+end
