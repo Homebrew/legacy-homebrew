dd9b42a66c1659773deddc490760b01654cfffe7
Brett Koonce <koonce@gmail.com>
2014-04-18 17:21:54 -0500

    htmlcleaner 2.8

--- Library/Formula/htmlcleaner.rb
+++ Library/Formula/htmlcleaner.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Htmlcleaner < Formula
   homepage 'http://htmlcleaner.sourceforge.net/index.php'
-  url 'https://downloads.sourceforge.net/project/htmlcleaner/htmlcleaner/htmlcleaner%20v2.7/htmlcleaner-2.7.zip'
-  sha1 'dc9032cbce6dfa2abd2eae060aa146a0d6af9d9f'
+  url 'https://downloads.sourceforge.net/project/htmlcleaner/htmlcleaner/htmlcleaner%20v2.8/htmlcleaner-2.8.zip'
+  sha1 'e4cbfebb306fc0baa95205ba91e452c961eebf85'
 
   def install
     libexec.install "htmlcleaner-#{version}.jar"
