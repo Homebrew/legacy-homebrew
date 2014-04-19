9293e529ece01e2741543d09df371200a7749624
Dennis Martinez <dennis@dennmart.com>
2014-04-18 19:47:03 -0700

    Removing older bottles for memcached 1.4.17

--- Library/Formula/memcached.rb
+++ Library/Formula/memcached.rb
@@ -2,12 +2,6 @@ require 'formula'
 
 class Memcached < Formula
   homepage 'http://memcached.org/'
-
-  bottle do
-    sha1 "9659921b7f83252f74fd71c9d89b0f087a987c39" => :mavericks
-    sha1 "56150a0077821d1758073238e8bac39f0028b6df" => :mountain_lion
-    sha1 "f0b6c864165782512b716f71853f6e24d57fdbda" => :lion
-  end
   url 'http://www.memcached.org/files/memcached-1.4.18.tar.gz'
   sha1 'e550ac63f1accb2c4b8384fd200a79a7e574b364'
 
