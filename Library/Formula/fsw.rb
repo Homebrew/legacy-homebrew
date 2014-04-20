bd2b7a0bc65f45667c5c978218e6140452fc2df5
Adam Vandenberg <flangy@gmail.com>
2014-04-20 10:56:36 -0700

    fsw 1.3.1

--- Library/Formula/fsw.rb
+++ Library/Formula/fsw.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Fsw < Formula
   homepage "http://emcrisostomo.github.io/fsw/"
-  url "https://github.com/emcrisostomo/fsw/releases/download/v1.3.0/fsw-1.3.0.tar.gz"
-  sha1 "9350fc405ca62af5ad00edf1d59ce881018abcb5"
+  url "https://github.com/emcrisostomo/fsw/releases/download/v1.3.1/fsw-1.3.1.tar.gz"
+  sha1 "f9c0f1c75e72f1a29c1e8433ca3f7aa408a94c4e"
 
   def install
-    ENV.append 'CXXFLAGS', '-stdlib=libc++'
+    ENV.append "CXXFLAGS", "-stdlib=libc++"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
