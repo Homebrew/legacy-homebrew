c3585dbb7393bc70a4f84c1a3a541da8342e5b03
Adam Vandenberg <flangy@gmail.com>
2014-04-20 13:23:20 -0700

    wv: style nits

--- Library/Formula/wv.rb
+++ Library/Formula/wv.rb
@@ -1,15 +1,15 @@
-require 'formula'
+require "formula"
 
 class Wv < Formula
-  homepage 'http://wvware.sourceforge.net/'
-  url 'http://abisource.com/downloads/wv/1.2.9/wv-1.2.9.tar.gz'
-  sha1 'db4717a151742dbdb492318f104504a92075543a'
+  homepage "http://wvware.sourceforge.net/"
+  url "http://abisource.com/downloads/wv/1.2.9/wv-1.2.9.tar.gz"
+  sha1 "db4717a151742dbdb492318f104504a92075543a"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'glib'
-  depends_on 'libgsf'
-  depends_on 'libwmf'
-  depends_on 'libpng'
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libgsf"
+  depends_on "libwmf"
+  depends_on "libpng"
 
   def install
     ENV.libxml2
