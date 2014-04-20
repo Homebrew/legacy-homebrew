b02453c0b3329df1dbf915ee06962536422e8900
Alfred E. Heggestad <aeh@db.org>
2014-04-20 16:07:22 +0200

    update libre formula
    updated after review comments from Mike McQuaid
    
    - use double quotes for depends_on
    - add prefix to make invokation

--- Library/Formula/libre.rb
+++ Library/Formula/libre.rb
@@ -5,11 +5,12 @@ class Libre < Formula
   url "http://www.creytiv.com/pub/re-0.4.8.tar.gz"
   sha1 "92bdf13a5e5ba445e1457d3fd6a20bb779c4b4b0"
 
-  depends_on 'openssl'
-  depends_on 'lzlib'
+  depends_on "openssl"
+  depends_on "lzlib"
 
   def install
-    system "make", "install"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
 end
+
