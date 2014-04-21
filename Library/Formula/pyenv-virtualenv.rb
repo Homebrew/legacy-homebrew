faf7a124c9c3f485a84ee8622d1d5df5ce6ffdd5
Xu Cheng <xucheng@me.com>
2014-04-21 22:17:40 +0800

    pyenv-virtualenv: audit style

--- Library/Formula/pyenv-virtualenv.rb
+++ Library/Formula/pyenv-virtualenv.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class PyenvVirtualenv < Formula
-  homepage 'https://github.com/yyuu/pyenv-virtualenv'
-  url 'https://github.com/yyuu/pyenv-virtualenv/archive/v20140421.tar.gz'
-  sha1 '65a82ec70a1de8995d4238fd23becd152e25bdcf'
+  homepage "https://github.com/yyuu/pyenv-virtualenv"
+  url "https://github.com/yyuu/pyenv-virtualenv/archive/v20140421.tar.gz"
+  sha1 "65a82ec70a1de8995d4238fd23becd152e25bdcf"
 
-  head 'https://github.com/yyuu/pyenv-virtualenv.git'
+  head "https://github.com/yyuu/pyenv-virtualenv.git"
 
-  depends_on 'pyenv'
+  depends_on "pyenv"
 
   def install
-    ENV['PREFIX'] = prefix
+    ENV["PREFIX"] = prefix
     system "./install.sh"
   end
 end
