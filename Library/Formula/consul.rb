070875d700eeb42aa76ff3c0b791830f7e8706cc
Nikola Majksner <mike@renheadtechnology.com>
2014-04-22 10:21:13 +0200

    Adding Consul SDA

---
+++ Library/Formula/consul.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class Consul < Formula
+  homepage 'http://www.consul.io/'
+  url 'https://dl.bintray.com/mitchellh/consul/0.1.0_darwin_amd64.zip'
+  sha256 '7989af75d1bbd43a4dff8f440c9a0883a4bcd66a52d94534f94e1ed1ae219681'
+
+  def install
+    bin.install 'consul'
+  end
+end
