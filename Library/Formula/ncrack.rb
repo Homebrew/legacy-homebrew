43e52c3d0f712305fbea183a756d27600a06a256
Jack Nagel <jacknagel@gmail.com>
2014-04-22 17:03:48 -0500

    ncrack: use Homebrew's openssl

--- Library/Formula/ncrack.rb
+++ Library/Formula/ncrack.rb
@@ -5,11 +5,12 @@ class Ncrack < Formula
   url 'http://nmap.org/ncrack/dist/ncrack-0.4ALPHA.tar.gz'
   sha256 'f8bd7e0ef68559490064ec0a5f139b2b9c49aeaf9f6323e080db9ff344c87603'
 
+  depends_on "openssl"
+
   def install
-    # --without-openssl-header-check is necessary because Apple bumps
-    # openssl versions in security updates, but never updates the headers
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--without-openssl-header-check"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
