aca02c25d52fb721de6ea24ff7cefb45af4af5a8
Kyle Gorman <gormanky@ohsu.edu>
2014-04-23 11:18:47 -0700

    Non-trivial tests

--- Library/Formula/opengrm-ngram.rb
+++ Library/Formula/opengrm-ngram.rb
@@ -1,29 +1,38 @@
-require 'formula'
+require "formula"
 
 class OpengrmNgram < Formula
-  homepage 'http://www.openfst.org/twiki/bin/view/GRM/NGramLibrary'
-  url 'http://openfst.cs.nyu.edu/twiki/pub/GRM/NGramDownload/opengrm-ngram-1.1.0.tar.gz'
-  sha1 'a2ceeaf6ac129b66d2682d76a20388cf1d4b8c31'
+  homepage "http://www.openfst.org/twiki/bin/view/GRM/NGramLibrary"
+  url "http://openfst.cs.nyu.edu/twiki/pub/GRM/NGramDownload/opengrm-ngram-1.1.0.tar.gz"
+  sha1 "a2ceeaf6ac129b66d2682d76a20388cf1d4b8c31"
 
-  depends_on 'openfst'
+  depends_on "openfst"
+
+  resource "earnest" do
+    url "http://www.openfst.org/twiki/pub/GRM/NGramQuickTour/earnest.txt"
+    sha1 "122c72f34a41ecb5e21102724938756fdfe349ec"
+  end
 
   def install
     ENV.libstdcxx if ENV.compiler == :clang && MacOS.version >= :mavericks
-    system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking'
-    system 'make install'
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking"
+    system "make install"
   end
 
   test do
-    # based on the examples at the OpenGRM "NGram quick tour" page...
-    # tests using a tokenized variant of The Importance of Being Earnest
-    system 'curl', 'http://openfst.cs.nyu.edu/twiki/pub/GRM/NGramQuickTour/earnest.txt', '-o', 'e.txt'
-    system 'ngramsymbols', 'e.txt', 'e.syms'
-    system 'farcompilestrings', '-symbols=e.syms', '-keep_symbols=1', 'e.txt', 'e.far'
-    system 'ngramcount', '-order=5', 'e.far', 'e.cnts'
-    system 'ngrammake', 'e.cnts', 'e.mod'
-    system 'ngramshrink', '-method=relative_entropy', 'e.mod', 'e.pru'
-    system 'ngramprint', '--ARPA', 'e.mod'
-    system 'ngraminfo', 'e.mod'
-    system 'ngramrandgen', '--max_sents=1', 'e.mod'
+    resource("earnest").stage do
+        fname = "earnest.txt"
+        # tests using normalized The Importance of Being Earnest, based on
+        # examples from the OpenGRM "NGram quick tour" page...
+        system "ngramsymbols", fname, "e.syms"
+        system "farcompilestrings", "-symbols=e.syms",
+                                    "-keep_symbols=1",
+                                    fname, "e.far"
+        system "ngramcount", "-order=5", "e.far", "e.cnts"
+        system "ngrammake", "e.cnts", "e.mod"
+        system "ngramshrink", "-method=relative_entropy", "e.mod", "e.pru"
+        system "ngramprint", "--ARPA", "e.mod"
+        system "ngraminfo", "e.mod"
+    end
   end
 end
