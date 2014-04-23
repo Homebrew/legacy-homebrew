fa2dfbcea4e660bc6ebaf4eabd24b906dc32513c
ryanrms <ryanrms@gmail.com>
2014-04-23 01:20:28 -0600

    mpdviz 0.4.3 (new formula)
    Updated formula to better conform to Ruby Style Guide (change to double quotes)

--- Library/Formula/mpdviz.rb
+++ Library/Formula/mpdviz.rb
@@ -10,12 +10,12 @@ class Mpdviz < Formula
   depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
-    system 'go', 'get', 'github.com/neeee/go-fftw'
-    system 'go', 'get', 'github.com/neeee/pflag'
-    system 'go', 'get', 'github.com/neeee/termbox-go'
-    system 'go', 'build', '-o', 'mpdviz'
-    bin.install 'mpdviz'
+    ENV["GOPATH"] = buildpath
+    system "go", "get", "github.com/neeee/go-fftw"
+    system "go", "get", "github.com/neeee/pflag"
+    system "go", "get", "github.com/neeee/termbox-go"
+    system "go", "build", "-o", "mpdviz"
+    bin.install "mpdviz"
   end
 
 end
