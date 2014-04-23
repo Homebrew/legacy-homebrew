342f61d863cfff2ff53cc747f3d5a40c13739027
Matthew Hawkins <darthmdh@gmail.com>
2014-04-23 20:26:07 +1000

    logstash 1.4.0
    Upgrade logstash to 1.4.0
    
    * added test suite, will test a simple log flow
    * added caveats with link to documentation for beginners
    * passes brew audit

--- Library/Formula/logstash.rb
+++ Library/Formula/logstash.rb
@@ -4,15 +4,23 @@ class Logstash < Formula
   homepage 'http://logstash.net/'
   url 'https://download.elasticsearch.org/logstash/logstash/logstash-1.4.0.tar.gz'
   sha1 '009c9d3d17b781b6ad2cceb776064cda6c6b3957'
-  head 'https://github.com/elasticsearch/logstash.git', :using => :git
 
   def install
-    inreplace "bin/logstash", /^basedir=.*$/, "basedir=#{prefix}"
-    inreplace "bin/logstash.lib.sh", /^basedir=.*$/, "basedir=#{prefix}"
-    prefix.install Dir["*"]
+    inreplace "bin/logstash", /^basedir=.*$/, "basedir=#{libexec}"
+    inreplace "bin/logstash.lib.sh", /^basedir=.*$/, "basedir=#{libexec}"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/logstash"
+  end
+
+  def caveats; <<-EOS.undent
+    Logstash 1.4 emits an unhelpful error if you try to start it sans config.
+    Please read the getting started guide located at:
+    http://logstash.net/docs/latest/tutorials/getting-started-with-logstash
+    EOS
   end
 
   test do
     system "#{bin}/logstash", "--version"
+    system "/bin/echo -n 'hello world' | #{bin}/logstash agent -e 'input { stdin { type => stdin } } output { stdout { codec => rubydebug } }'"
   end
 end
