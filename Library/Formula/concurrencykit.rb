5a071f07df17d6ac75eb1f8b29294d6c813ebe48
Francisco Souza <f@souza.cc>
2014-04-24 17:49:06 -0300

    concurrencykit 0.4.2
    Closes #28685.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Concurrencykit < Formula
   homepage 'http://concurrencykit.org'
-  url 'http://concurrencykit.org/releases/ck-0.4.1.tar.gz'
-  sha1 '53be7f3cc42bf46f409926a8add911bf49f37c20'
+  url 'http://concurrencykit.org/releases/ck-0.4.2.tar.gz'
+  sha1 '70c49d50345c915af2248e7f5223ecf74200dacb'
 
   head 'https://github.com/concurrencykit/ck.git'
 
