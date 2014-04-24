99e5f88ff4d237178bd12e7a2ec82cb424d11f1b
Zac Stewart <zgstewart@gmail.com>
2014-04-24 16:00:36 -0400

    Use double quoted strings instead of single

--- Library/Formula/uru.rb
+++ Library/Formula/uru.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class Uru < Formula
-  homepage 'https://bitbucket.org/jonforums/uru'
-  url 'https://bitbucket.org/jonforums/uru/get/v0.7.4.tar.gz'
-  sha1 '24b91db240e324d2738ad1f19079aed41b9cfdbf'
+  homepage "https://bitbucket.org/jonforums/uru"
+  url "https://bitbucket.org/jonforums/uru/get/v0.7.4.tar.gz"
+  sha1 "24b91db240e324d2738ad1f19079aed41b9cfdbf"
 
-  depends_on 'go' => :build
+  depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
-    system 'go', 'get', '-ldflags', '-s', 'bitbucket.org/jonforums/uru'
-    bin.install 'bin/uru' => 'uru_rt'
+    ENV["GOPATH"] = buildpath
+    system "go", "get", "-ldflags", "-s", "bitbucket.org/jonforums/uru"
+    bin.install "bin/uru" => "uru_rt"
   end
 
   def caveats; <<-EOS.undent
