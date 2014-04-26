dc5c33cdde83074d79d1f114468db27835b87423
Yann Hodique <hodiquey@vmware.com>
2014-04-26 08:25:30 -0700

    Enable timeout support for SSL sockets
    that support was disabled due to a bug in Python 2.3, which hopefully
    nobody uses anymore.
    
    On the downside, without those timeouts, getmail typically gets stuck
    forever when hibernation occurs while the socket is being used.

--- Library/Formula/getmail.rb
+++ Library/Formula/getmail.rb
@@ -6,6 +6,11 @@ class Getmail < Formula
   mirror 'http://fossies.org/linux/misc/getmail-4.46.0.tar.gz'
   sha1 '0e20fcfed6c422e5135304c3728c11c7cee7081a'
 
+  patch do
+    url "https://gist.githubusercontent.com/sigma/11295734/raw/5a7f39d600fc20d7605d3c9e438257285700b32b/ssl_timeout.patch"
+    sha1 "d7242a07c0d4de1890bb8ebd51b55e01e859b302"
+  end
+
   def install
     libexec.install %w( getmail getmail_fetch getmail_maildir getmail_mbox )
     bin.install_symlink Dir["#{libexec}/*"]
