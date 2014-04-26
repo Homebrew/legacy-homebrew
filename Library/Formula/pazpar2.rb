2f5d5ea4c23cba5090b7dd0db3ad7fcd20ef2ceb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-04-26 14:35:32 +0100

    pazpar2: update 1.6.41 bottle.
    Closes #28735.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/pazpar2.rb
+++ Library/Formula/pazpar2.rb
@@ -7,9 +7,9 @@ class Pazpar2 < Formula
 
   bottle do
     cellar :any
-    sha1 "fbcfb7c70a22494468f622e1aa1bb45080558cf9" => :mavericks
-    sha1 "1f7af0f4936fedca7400f36d6223bea69fa96d7d" => :mountain_lion
-    sha1 "2f2489c4108c984420f2a7744d170a7c652dd0b9" => :lion
+    sha1 "491cc42e1f70344396f73270bba9101c487cdfc1" => :mavericks
+    sha1 "45501784c3d44ebdc833047573d8e78c2e495a39" => :mountain_lion
+    sha1 "29aa33b793d41ac2285220a85d252958939a109e" => :lion
   end
 
   depends_on "pkg-config" => :build
