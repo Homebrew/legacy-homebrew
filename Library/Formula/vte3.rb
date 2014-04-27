e7f07d18b2e1e021b471445c7b9827bca8674e76
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-27 10:19:37 +0900

    vte3: use "(double quote) instead of '(single quote)

--- Library/Formula/vte3.rb
+++ Library/Formula/vte3.rb
@@ -1,18 +1,18 @@
-require 'formula'
+require "formula"
 
 class Vte3 < Formula
-  homepage 'http://developer.gnome.org/vte/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/vte/0.32/vte-0.32.2.tar.xz'
-  sha1 'a58569a99ac06a240bdda3ec7353f2626145852d'
+  homepage "http://developer.gnome.org/vte/"
+  url "http://ftp.gnome.org/pub/gnome/sources/vte/0.32/vte-0.32.2.tar.xz"
+  sha1 "a58569a99ac06a240bdda3ec7353f2626145852d"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'intltool' => :build
-  depends_on 'gettext'
-  depends_on 'glib'
-  depends_on 'gtk+3'
-  depends_on 'pygtk'
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gtk+3"
+  depends_on "pygtk"
   depends_on :python => :recommended
-  depends_on 'gobject-introspection'
+  depends_on "gobject-introspection"
 
   def install
     args = [
@@ -25,7 +25,7 @@ class Vte3 < Formula
     if build.with? "python"
       # pygtk-codegen-2.0 has been deprecated and replaced by
       # pygobject-codegen-2.0, but the vte Makefile does not detect this.
-      ENV["PYGTK_CODEGEN"] = Formula["pygobject"].bin/'pygobject-codegen-2.0'
+      ENV["PYGTK_CODEGEN"] = Formula["pygobject"].bin/"pygobject-codegen-2.0"
       args << "--enable-python"
     end
 
