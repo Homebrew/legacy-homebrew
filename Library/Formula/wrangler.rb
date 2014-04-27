c9f09ca3abd6e3bcf266e380a1fb5af6f589a6ed
Jack Nagel <jacknagel@gmail.com>
2014-04-27 14:14:22 -0500

    wrangler: checksum

--- Library/Formula/wrangler.rb
+++ Library/Formula/wrangler.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Wrangler < Formula
   homepage 'http://www.cs.kent.ac.uk/projects/forse/'
   url 'https://github.com/RefactoringTools/wrangler/archive/wrangler1.1.01.tar.gz'
-  sha1 ''
+  sha1 'fe938912ba3d50796da70572aef3e234c5d8f435'
 
   depends_on 'erlang'
 
