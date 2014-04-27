f66aa68813f50ea4cd47819196f62e0f16f0b702
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-27 17:02:02 +0900

    use "(double quote) instead of '(single quote)

--- Library/Formula/json-glib.rb
+++ Library/Formula/json-glib.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class JsonGlib < Formula
-  homepage 'http://live.gnome.org/JsonGlib'
-  url 'http://ftp.gnome.org/pub/gnome/sources/json-glib/1.0/json-glib-1.0.0.tar.xz'
-  sha256 'dbf558d2da989ab84a27e4e13daa51ceaa97eb959c2c2f80976c9322a8f4cdde'
+  homepage "http://live.gnome.org/JsonGlib"
+  url "http://ftp.gnome.org/pub/gnome/sources/json-glib/1.0/json-glib-1.0.0.tar.xz"
+  sha256 "dbf558d2da989ab84a27e4e13daa51ceaa97eb959c2c2f80976c9322a8f4cdde"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'glib'
-  depends_on 'gobject-introspection'
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "gobject-introspection"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
