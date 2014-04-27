92d61fc2f29ed743bee10a7c8e8ce0fd83ce80b4
blogabe <blogabe@rajab.us>
2014-04-27 09:16:55 -0700

    adding gdk-pixbuf
    explicit dependency and post install actions

--- Library/Formula/solfege.rb
+++ Library/Formula/solfege.rb
@@ -6,32 +6,29 @@ class Solfege < Formula
   mirror 'http://ftp.gnu.org/gnu/solfege/solfege-3.20.6.tar.xz'
   sha1 '0b83f351e90aeb9267f76d8dda0638dc50682226'
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gettext'      => :build
-  depends_on 'pygtk'        => :recommended
-  depends_on 'qtplay'       => :recommended
-  depends_on 'librsvg'      => :recommended
-  depends_on 'vorbis-tools' => :recommended
-  depends_on :python
+  depends_on "pkg-config" => :build
+  depends_on "gettext" => :build
+  depends_on "gdk-pixbuf" => "with-update-cache"
+  depends_on "pygtk" => :recommended
+  depends_on "qtplay" => :recommended
+  depends_on "librsvg" => :recommended
+  depends_on "vorbis-tools" => :recommended
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
-    
-    ENV.prepend_create_path "PYTHONPATH", "#{HOMEBREW_PREFIX}/lib/python2.7/site-packages/gtk-2.0"
-    ENV.prepend_create_path "GDK_PIXBUF_MODULEDIR", "#{Dir.glob(File.join ("#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/**", "loaders"))}"
-    sUpdate_Loader="#{Formula["gdk-pixbuf"].bin}/gdk-pixbuf-query-loaders --update-cache"
+    system "make", "install"
 
     inreplace "#{bin}/solfege", "os.path.split(os.path.dirname(os.path.abspath(sys.argv[0])))[0]", "\"#{prefix}\""
 
-    libexec.install "#{bin}"
-    (bin/"solfege").write <<-EOS.undent
-      #!/bin/bash
-      export PYTHONPATH="#{ENV["PYTHONPATH"]}"
-      export GDK_PIXBUF_MODULEDIR="#{ENV["GDK_PIXBUF_MODULEDIR"]}"
-      #{sUpdate_Loader}
-      exec "#{libexec}/bin/solfege" "$@"
-    EOS
+    ENV.prepend_create_path "PYTHONPATH", "#{HOMEBREW_PREFIX}/lib/python2.7/site-packages/gtk-2.0"
+    ENV.prepend_create_path "GDK_PIXBUF_MODULEDIR", "#{Dir.glob(File.join ("#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/**", "loaders"))}"
+
+    bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV["PYTHONPATH"], :GDK_PIXBUF_MODULEDIR => ENV["GDK_PIXBUF_MODULEDIR"])
+  end
+
+  def post_install
+    ENV["GDK_PIXBUF_MODULEDIR"]="#{Dir.glob(File.join ("#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/**", "loaders"))}"
+    system "#{Formula["gdk-pixbuf"].bin}/gdk-pixbuf-query-loaders", "--update-cache"
   end
 
   def caveats
