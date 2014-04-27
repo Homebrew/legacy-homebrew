46db7c315813f0a46a1ab0debc0561df8e4c60c1
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-28 07:36:59 +0900

    json-glib: make --enable-introspection=yes

--- Library/Formula/json-glib.rb
+++ Library/Formula/json-glib.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class JsonGlib < Formula
-  homepage 'http://live.gnome.org/JsonGlib'
-  url 'http://ftp.gnome.org/pub/gnome/sources/json-glib/1.0/json-glib-1.0.0.tar.xz'
-  sha256 'dbf558d2da989ab84a27e4e13daa51ceaa97eb959c2c2f80976c9322a8f4cdde'
+  homepage "http://live.gnome.org/JsonGlib"
+  url "http://ftp.gnome.org/pub/gnome/sources/json-glib/1.0/json-glib-1.0.0.tar.xz"
+  sha256 "dbf558d2da989ab84a27e4e13daa51ceaa97eb959c2c2f80976c9322a8f4cdde"
 
   bottle do
     sha1 "0752cfa2f78c6be59db8eacfd52a46cec2191078" => :mavericks
@@ -11,12 +11,15 @@ class JsonGlib < Formula
     sha1 "90182a996bba98f1ad2b1487c8b1dc3936c17f26" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'glib'
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "gobject-introspection"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-introspection=yes"
     system "make install"
   end
 end
