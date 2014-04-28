15a9d9495629dd9c6cc4d135e958046c3f27e162
Ed Porras <digressed@icloud.com>
2014-04-28 18:39:41 -0400

    killed option to remove splash-backend as requested by @jacknagel

--- Library/Formula/poppler.rb
+++ Library/Formula/poppler.rb
@@ -8,7 +8,6 @@ class Poppler < Formula
   option 'with-qt4', 'Build Qt backend'
   option 'with-glib', 'Build Glib backend' # requires cairo
   option 'with-lcms2', 'Use color management system'
-  option 'without-splash-output', 'Build without Splash output backend'
 
   depends_on 'pkg-config' => :build
 
@@ -48,7 +47,6 @@ class Poppler < Formula
     args << ( build.with?('glib') ? '--enable-poppler-glib' : '--disable-poppler-glib' )
     args << ( build.with?('glib') ? '' : '--disable-cairo-output' )
     args << ( build.with?('lcms2') ? '--enable-cms=lcms2' : '' )
-    args << ( build.without?('splash-output') ? '--disable-splash-output' : '' )
 
     system "./configure", *args
     system "make install"
