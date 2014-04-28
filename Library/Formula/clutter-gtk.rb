db8b334d9b852c6913009c0fab2dbeaa58d59dd3
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-28 23:41:59 +0900

    use "(double quote) instead of '(single quote)

--- Library/Formula/clutter-gtk.rb
+++ Library/Formula/clutter-gtk.rb
@@ -1,23 +1,23 @@
-require 'formula'
+require "formula"
 
 class ClutterGtk < Formula
-  homepage 'https://wiki.gnome.org/Clutter'
-  url 'http://ftp.gnome.org/pub/gnome/sources/clutter-gtk/1.2/clutter-gtk-1.2.2.tar.xz'
-  sha256 '743702bb230519553ca597b585b25af0b8783575fcd09c7fe5bed7dde292893c'
+  homepage "https://wiki.gnome.org/Clutter"
+  url "http://ftp.gnome.org/pub/gnome/sources/clutter-gtk/1.2/clutter-gtk-1.2.2.tar.xz"
+  sha256 "743702bb230519553ca597b585b25af0b8783575fcd09c7fe5bed7dde292893c"
 
-  option 'without-x', 'Build without X11 support'
+  option "without-x", "Build without X11 support"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'glib'
-  depends_on 'gdk-pixbuf'
-  depends_on 'cogl'
-  depends_on 'atk'
-  depends_on 'pango'
-  depends_on 'json-glib'
-  depends_on :x11 => '2.5.1' if build.with? 'x'
-  depends_on 'gtk+3'
-  depends_on 'clutter'
-  depends_on 'gobject-introspection'
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "gdk-pixbuf"
+  depends_on "cogl"
+  depends_on "atk"
+  depends_on "pango"
+  depends_on "json-glib"
+  depends_on :x11 => "2.5.1" if build.with? "x"
+  depends_on "gtk+3"
+  depends_on "clutter"
+  depends_on "gobject-introspection"
 
   def install
     args = %W[
@@ -32,7 +32,7 @@ class ClutterGtk < Formula
       --disable-gtk-doc-html
     ]
 
-    if build.with? 'x'
+    if build.with? "x"
       args.concat %w{
         --with-x --enable-x11-backend=yes
         --enable-gdk-pixbuf=yes
@@ -46,7 +46,7 @@ class ClutterGtk < Formula
       }
     end
 
-    system './configure', *args
-    system 'make install'
+    system "./configure", *args
+    system "make install"
   end
 end
