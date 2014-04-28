1c378a4c165501b56667c7faba8ed4f8956ab2c6
Gordon McShane <gordonmcshane@gmail.com>
2014-04-28 19:12:36 -0400

    putty: Add build from svn for --HEAD
    Closes #28819.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/putty.rb
+++ Library/Formula/putty.rb
@@ -4,18 +4,35 @@ class Putty < Formula
   homepage 'http://www.chiark.greenend.org.uk/~sgtatham/putty/'
   url 'http://the.earth.li/~sgtatham/putty/0.63/putty-0.63.tar.gz'
   sha1 '195c0603ef61082b91276faa8d4246ea472bba3b'
+  head 'svn://svn.tartarus.org/sgt/putty', :using => :svn
 
   depends_on 'pkg-config' => :build
   depends_on 'gtk+' => :optional
 
-  def install
-    cd "unix" do
-      system "./configure", "--prefix=#{prefix}", "--disable-gtktest"
-      system "make", "VER=-DRELEASE=#{version}"
+  if build.head?
+    depends_on "halibut" => :build
+    depends_on :automake
+    depends_on :autoconf
+  end
 
-      bin.install %w{ putty puttytel pterm } if build.with? 'gtk+'
-      bin.install %w{ plink pscp psftp puttygen }
+  def install
+
+    if build.head?
+      system "./mkfiles.pl"
+      system "./mkauto.sh"
+
+      cd "doc" do
+        system "make"
+      end
     end
+
+    system "./configure", "--prefix=#{prefix}", "--disable-gtktest"
+
+    build_version = build.head? ? "svn-#{version}" : version
+    system "make", "VER=-DRELEASE=#{build_version}"
+
+    bin.install %w{ putty puttytel pterm } if build.with? 'gtk+'
+    bin.install %w{ plink pscp psftp puttygen }
 
     cd "doc" do
       man1.install %w{ putty.1 puttytel.1 pterm.1 } if build.with? 'gtk+'
