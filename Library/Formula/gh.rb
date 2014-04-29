9ded0a07f69b87dac8fc9b2a477d14072236916f
Jingwen Owen Ou <jingweno@gmail.com>
2014-04-29 08:03:42 -0700

    Inline SHAs

--- Library/Formula/gh.rb
+++ Library/Formula/gh.rb
@@ -2,19 +2,17 @@ require "formula"
 
 class Gh < Formula
   VERSION = "2.0.0"
-  SOURCE_SHA1 = "ae44a538ca648efe1914d9ffb1af5ab23e2d879e"
-  BOTTLE_SHA1 = "ba504c443f5fd289895e669dffa0eb67cd257add"
 
   homepage "https://github.com/jingweno/gh"
   url "https://github.com/jingweno/gh/archive/v#{VERSION}.zip"
-  sha1 SOURCE_SHA1
+  sha1 "ae44a538ca648efe1914d9ffb1af5ab23e2d879e"
   head "https://github.com/jingweno/gh.git"
 
   bottle do
     root_url "https://github.com/jingweno/gh/releases/download/v#{VERSION}"
     prefix :any
     cellar :any
-    sha1 BOTTLE_SHA1 => :mavericks
+    sha1 "ba504c443f5fd289895e669dffa0eb67cd257add" => :mavericks
   end
 
   depends_on "go" => :build
