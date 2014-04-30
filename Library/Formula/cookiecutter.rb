9e308f305c5ff87b72c1520aa935b4841840a75b
Mike McQuaid <mike@mikemcquaid.com>
2014-04-30 23:36:35 +0100

    cookiecutter: don't depend on :python.
    Use the system Python instead.

--- Library/Formula/cookiecutter.rb
+++ Library/Formula/cookiecutter.rb
@@ -5,7 +5,7 @@ class Cookiecutter < Formula
   url 'https://pypi.python.org/packages/source/c/cookiecutter/cookiecutter-0.7.0.tar.gz'
   sha1 '2685e3288a348a04f5dd2e0f4462ad869e7fdc32'
 
-  depends_on :python
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
     ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
