ba1997e320111cb17b41e2b647d2aab72da3e819
John Kitsantas <proteus@cortexmail.eu>
2014-04-30 04:19:51 +0300

    Fixed SimGrid sha1 hash
    Fixed a problem with the sha1 hash of SimGrid…

--- Library/Formula/simgrid.rb
+++ Library/Formula/simgrid.rb
@@ -3,7 +3,7 @@ require "formula"
 class Simgrid < Formula
   homepage "http://simgrid.gforge.inria.fr/"
   url "http://gforge.inria.fr/frs/download.php/32047/SimGrid-3.10.tar.gz"
-  sha1 "2faad1892ad26d27121a1338e7fccd871056a11a"
+  sha1 "54e3b718d7b07afdefab6c433e11d1a7bf4cb499"
 
   depends_on 'cmake' => :build
   depends_on 'pcre'
