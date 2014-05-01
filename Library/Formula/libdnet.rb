9ef2eed6c7f9ac2e945bf55c8bd660bd1a838e23
BrewTestBot <brew-test-bot@googlegroups.com>
2014-05-01 22:08:51 +0100

    libdnet: add 1.12 bottle.

--- Library/Formula/libdnet.rb
+++ Library/Formula/libdnet.rb
@@ -5,6 +5,12 @@ class Libdnet < Formula
   url 'https://libdnet.googlecode.com/files/libdnet-1.12.tgz'
   sha1 '71302be302e84fc19b559e811951b5d600d976f8'
 
+  bottle do
+    sha1 "6b1bda90b59e20c8a5243d975ae0d948658cd7ff" => :mavericks
+    sha1 "cf3cba764f268c117459cd55a95197d6ff5afd7b" => :mountain_lion
+    sha1 "a3ec593982b554c90c23ac4c9a8192e5683b28ea" => :lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
