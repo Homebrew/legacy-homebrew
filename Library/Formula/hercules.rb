1f083355ac90c6f52d9dea9ab662bfc1b11a6ac0
Ian McAlpine <ian.mcalpine@me.com>
2014-05-02 21:15:14 +0100

    add HEAD build to hercules

--- Library/Formula/hercules.rb
+++ Library/Formula/hercules.rb
@@ -4,15 +4,37 @@ class Hercules < Formula
   homepage 'http://www.hercules-390.eu/'
   url 'http://downloads.hercules-390.eu/hercules-3.10.tar.gz'
   sha1 '10599041c7e5607cf2e7ecc76802f785043e2830'
+  head 'https://github.com/hercules-390/hyperion.git'
 
   skip_clean :la
 
+  if build.head?
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
+
   def install
-    # Since Homebrew optimizes for us, tell Hercules not to.
-    # (It gets it wrong anyway.)
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--enable-optimization=no"
+    if build.head?
+      # bundled autoconf.sh omits --add-missing
+      system "aclocal -I m4 -I autoconf"
+      system "autoheader"
+      system "automake --add-missing"
+      system "autoconf"
+
+      # IPv6 doesn't build on OSX
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}",
+                            "--disable-ipv6"
+                            "--enable-optimization=no"
+    else
+      # Since Homebrew optimizes for us, tell Hercules not to.
+      # (It gets it wrong anyway.)
+      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}",
+                            "--enable-optimization=no"
+    end
+
     system "make"
     system "make install"
   end
