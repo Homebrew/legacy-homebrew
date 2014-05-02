0049a9dacb308d6b6c46bcf5b8b7472348840d54
Luís Marques <luismarques@Feynman.local>
2014-05-02 17:09:11 +0100

    dub: add devel version

--- Library/Formula/dub.rb
+++ Library/Formula/dub.rb
@@ -15,6 +15,11 @@ class Dub < Formula
   sha1 '7752e14f3f5add50b1c7d9138739d72b276e6abe'
 
   head 'https://github.com/rejectedsoftware/dub.git', :using => DubHeadDownloadStrategy
+  
+  devel do
+    url "https://github.com/rejectedsoftware/dub/archive/v0.9.22-beta.2.tar.gz"
+    sha1 "031b525e6d10515a11c227229a47027281142e6d"
+  end
 
   depends_on 'pkg-config' => :build
   depends_on 'dmd'  => :build
