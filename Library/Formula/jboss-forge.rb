71188344f5771dd6330b78ac81c0fd4722ff4ef2
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-05-03 20:14:00 +0200

    jboss-forge 2.5.0

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class JbossForge < Formula
   homepage 'http://forge.jboss.org/'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=1.4.4.Final&e=zip'
-  version '1.4.4.Final'
-  sha1 '7c5218f9ed9c2e4986ee450f3c958d78dc3c09fd'
+  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.5.0.Final&e=zip&c=offline'
+  version '2.5.0.Final'
+  sha1 '035089fff2c7230bea440043598a84cce57933ae'
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install %w{ bin modules jboss-modules.jar }
+    libexec.install %w{ addons bin img lib logging.properties }
     bin.install_symlink libexec/'bin/forge'
   end
 end
