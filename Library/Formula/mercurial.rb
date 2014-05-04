a6fc01853fc44e1b9a59b75d2b639cf119ca0316
Adam Vandenberg <flangy@gmail.com>
2014-05-04 12:45:58 -0700

    mercurial: remove head support
    Closes #26822.

--- Library/Formula/mercurial.rb
+++ Library/Formula/mercurial.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
+# No head build supported; if you need head builds of Mercurial, do so outside
+# of Homebrew.
 class Mercurial < Formula
   homepage 'http://mercurial.selenic.com/'
   url 'http://mercurial.selenic.com/release/mercurial-3.0.tar.gz'
   mirror 'http://fossies.org/linux/misc/mercurial-3.0.tar.gz'
   sha1 'f9648580dd1a6a093fa16d7c28cf5aeefd20f2f0'
-
-  head 'http://selenic.com/repo/hg', :using => :hg
 
   option 'enable-docs', "Build the docs (and require docutils)"
 
@@ -28,17 +28,6 @@ class Mercurial < Formula
     # install the completion scripts
     bash_completion.install 'contrib/bash_completion' => 'hg-completion.bash'
     zsh_completion.install 'contrib/zsh_completion' => '_hg'
-  end
-
-  def caveats
-    if build.head?; <<-EOS.undent
-      To install the --HEAD version of mercurial, you have to:
-        1. `brew install mercurial`  # so brew can use this to fetch sources!
-        2. `brew unlink mercurial`
-        3. `brew install mercurial --HEAD`
-        4. `brew cleanup mercurial`  # to remove the older non-HEAD version
-      EOS
-    end
   end
 
   test do
