8ccde4d11eb60efe0451dbf50f9f1f248df27cd8
Martin Probst <martin@probst.io>
2014-05-04 15:25:09 +0200

    Google App Engine SDKs 1.9.4
    Upgrades App Engine SDKs to 1.9.4 across Java, Python, Go 32/64.
    SHAs verified from Google's download site at https://developers.google.com/appengine/downloads
    
    Closes #28943.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/go-app-engine-32.rb
+++ Library/Formula/go-app-engine-32.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GoAppEngine32 < Formula
   homepage 'http://code.google.com/appengine/docs/go/overview.html'
-  url 'https://commondatastorage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.3.zip'
-  sha1 'df881a99aa2ff7bfec5414f1fc980323a5cb7cc0'
+  url 'https://commondatastorage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.4.zip'
+  sha1 '567e8be4654e150789d262a8b3fda8aa74fc92e2'
 
   def install
     cd '..'
