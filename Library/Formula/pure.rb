63e9a882bea7a34ac108cb315ffe3dbffd34d976
BrewTestBot <brew-test-bot@googlegroups.com>
2014-05-05 04:21:27 +0100

    pure: update 0.60 bottle.

--- Library/Formula/pure.rb
+++ Library/Formula/pure.rb
@@ -5,6 +5,12 @@ class Pure < Formula
   url 'https://bitbucket.org/purelang/pure-lang/downloads/pure-0.60.tar.gz'
   sha1 'ef930868e8ba2b8e1a65c782d8b04828c3d0d255'
 
+  bottle do
+    sha1 "75cfcd8adf10e49d2d1d8277c74ec2151b2047a5" => :mavericks
+    sha1 "017dedafd0e79bd7ae0e782adf8fdbe5a01f9456" => :mountain_lion
+    sha1 "f8fd5804cdf52cb45321fedeb5d4ba42a219a08d" => :lion
+  end
+
   depends_on :automake
   depends_on :libtool
 
