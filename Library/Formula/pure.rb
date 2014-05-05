dbbe5cca708cefcd535246345046636d4fd3a447
Myles Borins <myles@famo.us>
2014-05-05 04:48:54 +0200

    pure 0.60 Update version of pure.  Also added bottle do method to enable bottling of this recipe

--- Library/Formula/pure.rb
+++ Library/Formula/pure.rb
@@ -2,8 +2,14 @@ require 'formula'
 
 class Pure < Formula
   homepage 'http://purelang.bitbucket.org/'
-  url 'https://bitbucket.org/purelang/pure-lang/downloads/pure-0.59.tar.gz'
-  sha1 '22614d77fff9937a53cf513767fdc5e8eeb4aae1'
+  url 'https://bitbucket.org/purelang/pure-lang/downloads/pure-0.60.tar.gz'
+  sha1 'ef930868e8ba2b8e1a65c782d8b04828c3d0d255'
+
+  bottle do
+    sha1 "055c36c18feab70f68a166a5e06d2543c12127d6" => :mavericks
+    sha1 "90e2d4ffa429ad0873d9a9b297c446850c28ddf4" => :mountain_lion
+    sha1 "7ca40658577b399c64523b70b3b708764b5a1d14" => :lion
+  end
 
   depends_on :automake
   depends_on :libtool
@@ -14,8 +20,8 @@ class Pure < Formula
   depends_on 'mpfr'
 
   resource 'docs' do
-    url 'https://bitbucket.org/purelang/pure-lang/downloads/pure-docs-0.59.tar.gz'
-    sha1 '55794cea62dcdec093c37bbf68dba6f2dc2e9f9f'
+    url 'https://bitbucket.org/purelang/pure-lang/downloads/pure-docs-0.60.tar.gz'
+    sha1 '6a5644bc674db8a481c7db5181cf44d6be590645'
   end
 
   def install
