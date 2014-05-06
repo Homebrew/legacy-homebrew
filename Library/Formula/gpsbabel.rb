c6783903491669c9bcde16735260d658752a1677
Johnathan Conley <johnathan.conley@gmail.com>
2014-05-06 08:54:10 -0500

    version 1.5.0

--- Library/Formula/gpsbabel.rb
+++ Library/Formula/gpsbabel.rb
@@ -2,13 +2,14 @@ require 'formula'
 
 class Gpsbabel < Formula
   homepage 'http://www.gpsbabel.org'
-  # revision 4199 is version 1.4.4
-  url 'http://gpsbabel.googlecode.com/svn/trunk/gpsbabel', :revision => '4199'
-  version '1.4.4'
+  # revision 4813 is version 1.5.0
+  url "http://gpsbabel.googlecode.com/svn/trunk/gpsbabel", :revision => "4813"
+  version "1.5.0"
 
   head 'http://gpsbabel.googlecode.com/svn/trunk/gpsbabel'
 
   depends_on 'libusb' => :optional
+  depends_on "qt"
 
   def install
     args = ['--disable-debug', '--disable-dependency-tracking',
