089744b867334b975438c75a7c60feab8fecef9b
gin0606 <kkgn06@gmail.com>
2014-05-06 19:18:16 +0900

    Update Ant to 1.9.4
    update download link and sha1

--- Library/Formula/ant.rb
+++ Library/Formula/ant.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ant < Formula
   homepage 'http://ant.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.3-bin.tar.gz'
-  sha1 '11a0b936fba02f96b8d737d90c610382232ffea6'
+  url 'http://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.4-bin.tar.gz'
+  sha1 '6c41481e8201f6b3f7e216146b95bb6de70208bb'
 
   keg_only :provided_by_osx if MacOS.version < :mavericks
 
