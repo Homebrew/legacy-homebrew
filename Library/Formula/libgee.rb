7b1919f5f45410b01a58ef2fb8745563779c4380
Adam Vandenberg <flangy@gmail.com>
2014-05-06 13:57:11 -0700

    libgee: vala is a build-time dependency

--- Library/Formula/libgee.rb
+++ Library/Formula/libgee.rb
@@ -6,8 +6,8 @@ class Libgee < Formula
   sha1 "ca6531c8ba45cc865f2ce7b93a8b3f96e1c1505e"
 
   depends_on "pkg-config" => :build
+  depends_on "vala" => :build
   depends_on "gobject-introspection"
-  depends_on "vala"
 
   def install
     system "./configure", "--prefix=#{prefix}",
