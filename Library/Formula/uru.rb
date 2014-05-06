6ef1849cefce01c11ab9592ba5905c82a62ae49f
Zac Stewart <zgstewart@gmail.com>
2014-05-06 10:11:24 -0400

    Remove hashmarks from caveats

--- Library/Formula/uru.rb
+++ Library/Formula/uru.rb
@@ -15,14 +15,14 @@ class Uru < Formula
   end
 
   def caveats; <<-EOS.undent
-    # Append to ~/.profile on Ubuntu, or to ~/.zshrc on Zsh
+    Append to ~/.profile on Ubuntu, or to ~/.zshrc on Zsh
     $ echo 'eval "$(uru_rt admin install)"' >> ~/.bash_profile
 
-    # Register a pre-existing "system" ruby already placed on PATH from bash/Zsh
-    # startup configuration files
+    Register a pre-existing "system" ruby already placed on PATH from bash/Zsh
+    startup configuration files
     $ uru_rt admin add system
 
-    # Restart shell
+    Restart shell
     $ exec $SHELL -l
     EOS
   end
