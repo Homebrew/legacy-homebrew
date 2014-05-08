d69ddd960f7542121d939abbcef9844713f585c5
Thomas Kim Pham <thomas.pham@ithings.ch>
2014-05-08 19:15:53 +0200

    boot2docker 0.8.1
    bump version 0.8.1 + sha1 checksum
    
    Closes #29064.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -3,8 +3,8 @@ require "formula"
 class Boot2docker < Formula
   homepage "https://github.com/boot2docker/boot2docker"
   head "https://github.com/boot2docker/boot2docker.git"
-  url 'https://github.com/boot2docker/boot2docker/archive/v0.8.0.tar.gz'
-  sha1 'c6f07db7d3a4952f1508b92f8b32e02e2f316f85'
+  url 'https://github.com/boot2docker/boot2docker/archive/v0.8.1.tar.gz'
+  sha1 'f52d77544ab4ba1462a8f9c0c2b2d214cffb1059'
 
   depends_on "docker" => :recommended
 
