693c84b3fe9c030779201133ce44fb78adc0eb9f
Tim Galeckas <tgaleckas@groupon.com>
2014-05-08 18:07:08 -0500

    fix review concerns

--- Library/Formula/betty.rb
+++ Library/Formula/betty.rb
@@ -1,20 +1,40 @@
 require "formula"
 
+class Ruby19Dependency < Requirement
+  fatal true
+
+  satisfy do
+    `ruby --version` =~ /ruby (\d\.\d).\d/
+    $1.to_f >= 1.9
+  end
+
+  def message
+    "Selecta requires Ruby 1.9 or better."
+  end
+end
+
 class Betty < Formula
   homepage "https://github.com/pickhardt/betty"
-  url "https://github.com/pickhardt/betty.git"
-  sha1 "fed0e108d9a9ceb059428a3601c163d939c80bbc"
+  url "https://github.com/pickhardt/betty/archive/fed0e108d9a9ceb059428a3601c163d939c80bbc.zip"
+  sha1 "478b5a41563a0a6fe7e006c9c79f8d156d14e04b"
   version "0.1.3"
 
+  depends_on Ruby19Dependency
+
   def install
-    prefix.install 'lib', 'main.rb'
-    (bin/'betty').write <<-EOS.undent
+    libexec.install 'lib', 'main.rb'
+    (libexec/'betty').write <<-EOS.undent
       #!/usr/bin/env bash
-      #{prefix/'main.rb'} $@
+      #{libexec/'main.rb'} $@
     EOS
+    (libexec/'betty').chmod 0755
+    bin.write_exec_script libexec/'betty'
   end
 
   test do
-    system bin/'betty'
+    system bin/'betty', 'speech on'
+    system bin/'betty', 'what is your name'
+    system bin/'betty', 'version'
+    system bin/'betty', 'speech off'
   end
 end
