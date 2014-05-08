10fba9a5d2e770f8e4f103302587539130876cfb
Jordi Saludes <jordi.saludes@upc.edu>
2014-05-08 18:55:41 +0200

    Build using cabal-install and language/haskell.

--- Library/Formula/gf.rb
+++ Library/Formula/gf.rb
@@ -1,33 +1,44 @@
 require "formula"
+require "language/haskell"
+
 class Gf < Formula
-    homepage "http://www.grammaticalframework.org/"
+  include Language::Haskell::Cabal
 
-    depends_on 'haskell-platform'
+  homepage "http://www.grammaticalframework.org/"
 
-    stable do
-        url "http://www.grammaticalframework.org/download/gf-3.5.tar.gz"
-        sha1 "9c3d156df73b79c2121a46c6c29c571bfdd619c2"
+  depends_on 'ghc' => :build
+  depends_on 'cabal-install' => :build
+
+  stable do
+    url "http://www.grammaticalframework.org/download/gf-3.5.tar.gz"
+    sha1 "9c3d156df73b79c2121a46c6c29c571bfdd619c2"
+  end
+
+  def install
+    cabal_sandbox do
+    	cabal_install_tools 'alex', 'happy'
+        cabal_install '-f-server'
     end
-    
-    def install
-        system 'cabal', 'install'
-    end
+    cabal_clean_lib
+  end
 
-    test  do
+  def link
+    bin.install_symlink ENV['HOME'] + "/.cabal/bin/gf"
+  end
 
-        system 'gf', '-version'
+  test  do
+    system 'gf', '-version'
 
-        (testpath/"Hello.gf").write <<-EOS.undent
-             abstract Hello = {
-             flags startcat = Greeting ;
-             cat Greeting ; Recipient ;
-             fun
-                Hello : Recipient -> Greeting ;
-                World, Mum, Friends : Recipient ; }
-        EOS
-
-        system 'gf', '--make',
-               testpath/"Hello.gf"
-        assert(File.exists?(testpath/"Hello.pgf"), "No pgf made")
-    end
+    (testpath/"Hello.gf").write <<-EOS.undent
+     abstract Hello = {
+     flags startcat = Greeting ;
+     cat Greeting ; Recipient ;
+     fun
+      Hello : Recipient -> Greeting ;
+      World, Mum, Friends : Recipient ; }
+    EOS
+    system 'gf', '--make',
+    testpath/"Hello.gf"
+    assert(File.exists?(testpath/"Hello.pgf"), "No pgf made")
+  end
 end
