f4677e53d13ae57559a61957554b302fe87fcee9
Mike McQuaid <mike@mikemcquaid.com>
2014-05-08 11:43:11 +0100

    exceptions: fix tap issue URL.
    Closes #29043.

--- Library/Homebrew/exceptions.rb
+++ Library/Homebrew/exceptions.rb
@@ -185,7 +185,7 @@ class BuildError < Homebrew::InstallationError
       puts "#{Tty.red}READ THIS#{Tty.reset}: #{Tty.em}#{ISSUES_URL}#{Tty.reset}"
       if formula.tap?
         user, repo = formula.tap.split '/'
-        tap_issues_url = "https://github.com/#{user}/homebrew-#{repo}/issues"
+        tap_issues_url = "https://github.com/#{user}/#{repo}/issues"
         puts "If reporting this issue please do so at (not Homebrew/homebrew):"
         puts "  #{tap_issues_url}"
       end
