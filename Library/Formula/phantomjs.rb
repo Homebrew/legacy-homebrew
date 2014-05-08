dd2b06dc8dbcbc6befef7114e9d5f6c3dc8ba2a4
Mike McQuaid <mike@mikemcquaid.com>
2014-05-08 13:23:18 +0100

    phantomjs: don't need to disable superenv args.
    Fixed in #28985.

--- Library/Formula/phantomjs.rb
+++ Library/Formula/phantomjs.rb
@@ -20,9 +20,6 @@ class Phantomjs < Formula
   def install
     inreplace 'src/qt/preconfig.sh', '-arch x86', '-arch x86_64' if MacOS.prefer_64_bit?
     args = ['--confirm', '--qt-config']
-    # we have to disable these to avoid triggering Qt optimization code
-    # that will fail in superenv (in --env=std, Qt seems aware of this)
-    args << '-no-3dnow -no-ssse3' if superenv?
     system './build.sh', *args
     bin.install 'bin/phantomjs'
     (share+'phantomjs').install 'examples'
