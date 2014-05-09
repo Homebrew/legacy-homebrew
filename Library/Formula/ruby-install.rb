5244b46b2fa331e59e8d0e2162610ed6d43eea34
Shannon Skipper <shannonskipper@gmail.com>
2014-05-09 06:35:11 -0700

    ruby-install 0.4.3
    Closes #29086.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ruby-install.rb
+++ Library/Formula/ruby-install.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class RubyInstall < Formula
   homepage 'https://github.com/postmodern/ruby-install#readme'
-  url 'https://github.com/postmodern/ruby-install/archive/v0.4.2.tar.gz'
-  sha1 'f5cb5be73f29a943b291e16926bb71af440eefb7'
+  url 'https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz'
+  sha1 'be7dd5ad558102ab812addd3100a91c9812d0317'
 
   head 'https://github.com/postmodern/ruby-install.git'
 
