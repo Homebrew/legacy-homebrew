0d130affc7770e555c4d5d7a75c051f2dfc5a8fd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-05-09 21:13:20 +0100

    hevea: update 2.14 bottle.
    Closes #29094.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/hevea.rb
+++ Library/Formula/hevea.rb
@@ -6,6 +6,9 @@ class Hevea < Formula
   sha1 "78152c83802e34881ce3414072d75bff66facb15"
 
   bottle do
+    sha1 "657486337d169647d9c10afb61516e38ae1bf772" => :mavericks
+    sha1 "0e1fe3dd6b1b21fbf435738f329473c1e6130e38" => :mountain_lion
+    sha1 "4766e826da52e595f679c699c836bcfb141dfdbd" => :lion
   end
 
   depends_on 'objective-caml'
