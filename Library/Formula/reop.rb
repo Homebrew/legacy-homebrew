4179f2dda56326cf199e1c0f69eba7f5bf4454c3
Jonathan Wilkins <jwilkins@bitland.net>
2014-05-08 22:27:22 -0700

    added tagged 1.0.0 release

--- Library/Formula/reop.rb
+++ Library/Formula/reop.rb
@@ -3,7 +3,8 @@ require "formula"
 class Reop < Formula
   homepage "http://www.tedunangst.com/flak/post/reop"
   head 'https://github.com/tedu/reop.git', :using => :git
-
+  url 'https://github.com/tedu/reop/archive/1.0.0.tar.gz'
+  sha256 '8c2bf9a0b66e9a43cbcf3291858a97ccdc62736a378cd98aa3d3fc47f5db3798' 
   depends_on "libsodium"
 
   def install
