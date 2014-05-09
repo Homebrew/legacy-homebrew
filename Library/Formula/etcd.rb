74350ad341e52e5a4c8d2ab90551a48f78914422
Jon Morehouse <morehousej09@gmail.com>
2014-05-09 14:47:23 -0700

    etcd: add plist
    Closes #29098.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/etcd.rb
+++ Library/Formula/etcd.rb
@@ -13,4 +13,29 @@ class Etcd < Formula
     system "./build"
     bin.install 'bin/etcd'
   end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/etcd</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+      </dict>
+    </plist>
+    EOS
+  end
 end
