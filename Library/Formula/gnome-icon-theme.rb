aab4ac80807ffb64b08eacfa01d8a57478d98deb
Adam Vandenberg <flangy@gmail.com>
2014-05-11 12:22:46 -0700

    gnome-icon-theme: use gtk+3
    Because the software being added that depends on this uses Gtk+3,
    use the Gtk+3 icon cache.
    
    Closes #29145.

--- Library/Formula/gnome-icon-theme.rb
+++ Library/Formula/gnome-icon-theme.rb
@@ -4,23 +4,26 @@ class GnomeIconTheme < Formula
   homepage "https://developer.gnome.org"
   url "http://ftp.gnome.org/pub/GNOME/sources/gnome-icon-theme/3.12/gnome-icon-theme-3.12.0.tar.xz"
   sha1 "cc0f0dc55db3c4ca7f2f34564402f712807f1342"
-
-  depends_on "pkg-config" => :build
-  depends_on "gettext" => :build
-  depends_on "gtk+" => :build # for gtk-update-icon-cache
-  depends_on "icon-naming-utils" => :build
-  depends_on "intltool" => :build
+  revision 1
 
   bottle do
     cellar :any
-    sha1 "d0540a403786f3b7a64e9d29f41fc1ea691179c5" => :mavericks
-    sha1 "1ad3bcd28fa71c198154030bb1d8e8bff8d3f868" => :mountain_lion
-    sha1 "43f0035b70fd722ffefb79b7356e0f34a5ba4abf" => :lion
+    sha1 "e9329d81e8c615e78f0c96e62b91008dbc4a74e5" => :mavericks
+    sha1 "84c1e818410a0c5b8b33c6b07932b8a547f69ede" => :mountain_lion
+    sha1 "3bbeb7cee6c7bd1e1b070cc0be276502eee3cc6b" => :lion
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "gettext" => :build
+  depends_on "gtk+3" => :build # for gtk3-update-icon-cache
+  depends_on "icon-naming-utils" => :build
+  depends_on "intltool" => :build
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-icon-mapping",
+                          "GTK_UPDATE_ICON_CACHE=#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache"
     system "make", "install"
   end
 end
