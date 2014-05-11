94874cdd08b4da23e3aaab03e6698ed33cdeb8b5
Adam Vandenberg <flangy@gmail.com>
2014-05-11 10:29:06 -0700

    gnome-icon-theme 3.12.0

--- Library/Formula/gnome-icon-theme.rb
+++ Library/Formula/gnome-icon-theme.rb
@@ -2,15 +2,14 @@ require "formula"
 
 class GnomeIconTheme < Formula
   homepage "https://developer.gnome.org"
-  url "http://ftp.gnome.org/pub/GNOME/sources/gnome-icon-theme/3.10/gnome-icon-theme-3.10.0.tar.xz"
-  sha256 "c65472b5846c67b97fe75200c5180faccd1347a04caa8390fc0ad23de75b36f6"
+  url "http://ftp.gnome.org/pub/GNOME/sources/gnome-icon-theme/3.12/gnome-icon-theme-3.12.0.tar.xz"
+  sha1 "cc0f0dc55db3c4ca7f2f34564402f712807f1342"
 
-  depends_on "xz" => :build
   depends_on "pkg-config" => :build
+  depends_on "gettext" => :build
+  depends_on "gtk+" => :build # for gtk-update-icon-cache
+  depends_on "icon-naming-utils" => :build
   depends_on "intltool" => :build
-  depends_on "gettext"
-  depends_on "gtk+" # for gtk-update-icon-cache
-  depends_on "icon-naming-utils"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
