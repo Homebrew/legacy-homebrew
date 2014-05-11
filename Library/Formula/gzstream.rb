bf444a1fb145ef1ac2b2f9cda9e3371de66a468c
Gabriel Bretschner <software@kanedo.net>
2014-05-11 15:33:07 +0200

    removed zlib dependency
    zlib is provided by mac os x

--- Library/Formula/gzstream.rb
+++ Library/Formula/gzstream.rb
@@ -5,7 +5,6 @@ class Gzstream < Formula
   url "http://www.cs.unc.edu/Research/compgeom/gzstream/gzstream.tgz"
   sha1 "3cdd797c7e5a10408eb664aca9666525fe00652a"
   version "1.5"
-  depends_on "zlib"
 
   def install
     system "make", "clean" # remove precompiled files
