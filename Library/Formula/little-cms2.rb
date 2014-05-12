cb8a06fe6709dddd46c92a09607ffd2fe837a5ad
David Christenson <david@the-david.com>
2014-05-12 05:23:42 -0600

    little-cms2 2.6
    Update to latest stable version.

--- Library/Formula/little-cms2.rb
+++ Library/Formula/little-cms2.rb
@@ -2,15 +2,8 @@ require 'formula'
 
 class LittleCms2 < Formula
   homepage 'http://www.littlecms.com/'
-  url 'https://downloads.sourceforge.net/project/lcms/lcms/2.5/lcms2-2.5.tar.gz'
-  sha1 'bab3470471fc7756c5fbe71be9a3c7d677d2ee7b'
-
-  bottle do
-    cellar :any
-    sha1 "06ec67737332d592670109bac5547d42276557a0" => :mavericks
-    sha1 "57ef9368e594929537805df109031fbe799b3d8a" => :mountain_lion
-    sha1 "79aa4fcb97599f1902b70eebbf3acbbc9936f75e" => :lion
-  end
+  url 'https://downloads.sourceforge.net/project/lcms/lcms/2.6/lcms2-2.6.tar.gz'
+  sha1 'b0ecee5cb8391338e6c281d1c11dcae2bc22a5d2'
 
   depends_on 'jpeg' => :recommended
   depends_on 'libtiff' => :recommended
