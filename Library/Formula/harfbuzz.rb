a5ff982d950ce611c47e4d7bd5f31f3d913aa7bb
David Christenson <david@the-david.com>
2014-05-12 06:26:09 -0600

    Restore bottle block

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -5,6 +5,13 @@ class Harfbuzz < Formula
   url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.28.tar.bz2'
   sha256 'a567f7c58018af0a9751e18641fd8434bfcef8307122dabe47dd652ce8bde048'
 
+  bottle do
+    cellar :any
+    sha1 "27623054bf6ca9f54428f0fe63bc2c2caa726325" => :mavericks
+    sha1 "120db14c8928fc948340ff14ecab0bb814048fac" => :mountain_lion
+    sha1 "7c99b0595e9ea6e44eb346779bd06db1aeb6b49a" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'glib'
   depends_on 'cairo'
