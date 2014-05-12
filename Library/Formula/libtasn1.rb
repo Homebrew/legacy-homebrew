33800344ee351936965ccb540c88f0fdab4a283d
David Christenson <david@the-david.com>
2014-05-12 06:13:45 -0600

    Restore bottle block

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -6,6 +6,13 @@ class Libtasn1 < Formula
   mirror 'http://ftp.gnu.org/gnu/libtasn1/libtasn1-3.5.tar.gz'
   sha1 '7e38f027c765eb5434bf050aef0f20aee45e3420'
 
+  bottle do
+    cellar :any
+    sha1 "ea965f6a6a987c3fd7b687efd24ff1ee7c35f98c" => :mavericks
+    sha1 "86b57809fd4db333e400da028432aa0f7661c3f6" => :mountain_lion
+    sha1 "6d31d4239ce62fb2721e921036d05ada7e68e955" => :lion
+  end
+
   option :universal
 
   def install
