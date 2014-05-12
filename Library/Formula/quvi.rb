075555cb5d4e69197986e0dfb2fbb12c8236f609
David Christenson <david@the-david.com>
2014-05-12 06:40:20 -0600

    quvi 0.9.5
    Update to latest stable release.

--- Library/Formula/quvi.rb
+++ Library/Formula/quvi.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Quvi < Formula
   homepage 'http://quvi.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/quvi/0.4/quvi/quvi-0.4.2.tar.bz2'
-  sha1 'ba67a380785212886089d75f8aa1480d6c05936e'
+  url 'https://downloads.sourceforge.net/project/quvi/0.9/quvi/quvi-0.9.5.tar.xz'
+  sha1 '8040b8fef103350c462ba51fa614cb35a2bc1873'
 
   depends_on 'pkg-config' => :build
   depends_on 'libquvi'
