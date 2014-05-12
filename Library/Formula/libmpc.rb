b1644814de4a68737a23b9dd00a13850eef33705
David Christenson <david@the-david.com>
2014-05-12 05:09:37 -0600

    libmpc 1.0.2
    Update to the latest stable release.

--- Library/Formula/libmpc.rb
+++ Library/Formula/libmpc.rb
@@ -2,16 +2,8 @@ require 'formula'
 
 class Libmpc < Formula
   homepage 'http://multiprecision.org'
-  url 'http://multiprecision.org/mpc/download/mpc-1.0.1.tar.gz'
-  sha1 '8c7e19ad0dd9b3b5cc652273403423d6cf0c5edf'
-
-  bottle do
-    cellar :any
-    revision 1
-    sha1 '491bd8e7535792094846c22c97284c9a9d77eb11' => :mavericks
-    sha1 '129f7b22a326fa10d6ded3aa1059aa4c0b31b673' => :mountain_lion
-    sha1 '533355b4698b5964cad3bca1268a913c5a9a76b2' => :lion
-  end
+  url 'ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz'
+  sha1 '5072d82ab50ec36cc8c0e320b5c377adb48abe70'
 
   depends_on 'gmp'
   depends_on 'mpfr'
