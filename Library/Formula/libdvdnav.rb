7b09d62a8063bfdb76d923438ae4cf0377a9b51b
David Christenson <david@the-david.com>
2014-05-12 05:20:45 -0600

    Remove trailing whitespace

--- Library/Formula/libdvdnav.rb
+++ Library/Formula/libdvdnav.rb
@@ -4,7 +4,7 @@ class Libdvdnav < Formula
   homepage 'http://dvdnav.mplayerhq.hu/'
   url 'http://dvdnav.mplayerhq.hu/releases/libdvdnav-4.2.1.tar.xz'
   sha256 '7fca272ecc3241b6de41bbbf7ac9a303ba25cb9e0c82aa23901d3104887f2372'
-  
+
   head 'git://git.videolan.org/libdvdnav.git'
 
   depends_on 'libdvdread'
