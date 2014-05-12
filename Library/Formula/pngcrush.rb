b93af27033b829622c8645fa35423acc63a72a13
Stian Eikeland <stian@eikeland.se>
2014-05-12 12:58:50 +0200

    pngcrush 1.7.73
    Closes #29175.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -4,8 +4,8 @@ require 'formula'
 # tarballs are routinely removed and upstream won't change this practice.
 class Pngcrush < Formula
   homepage 'http://pmt.sourceforge.net/pngcrush/'
-  url 'https://downloads.sourceforge.net/project/pmt/pngcrush/old-versions/1.7/1.7.71/pngcrush-1.7.71.tar.gz'
-  sha1 '0ffbf485a2d907807bef8d397adceba1a438096b'
+  url 'https://downloads.sourceforge.net/project/pmt/pngcrush/1.7.73/pngcrush-1.7.73.tar.gz'
+  sha1 '49b86afe5a17f58c938e68d2252a6c75408de02b'
 
   def install
     # Required to successfully build the bundled zlib 1.2.6
