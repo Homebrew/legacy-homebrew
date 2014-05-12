c113a3f72552368e81b38b85871fd7e1679b6477
David Christenson <david@the-david.com>
2014-05-12 06:18:02 -0600

    Use preferred GNU mirror, restore bottle block

--- Library/Formula/libmpc.rb
+++ Library/Formula/libmpc.rb
@@ -2,8 +2,17 @@ require 'formula'
 
 class Libmpc < Formula
   homepage 'http://multiprecision.org'
-  url 'ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz'
+  url 'http://ftpmirror.gnu.org/mpc/mpc-1.0.2.tar.gz'
+  mirror 'http://multiprecision.org/mpc/download/mpc-1.0.2.tar.gz'
   sha1 '5072d82ab50ec36cc8c0e320b5c377adb48abe70'
+
+  bottle do
+    cellar :any
+    revision 1
+    sha1 '491bd8e7535792094846c22c97284c9a9d77eb11' => :mavericks
+    sha1 '129f7b22a326fa10d6ded3aa1059aa4c0b31b673' => :mountain_lion
+    sha1 '533355b4698b5964cad3bca1268a913c5a9a76b2' => :lion
+  end
 
   depends_on 'gmp'
   depends_on 'mpfr'
