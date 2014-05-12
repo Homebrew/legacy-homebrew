5eb7be1f67af07d11e265e4daefafcefa85c08cc
David Christenson <david@the-david.com>
2014-05-12 04:16:43 -0600

    gnu-tar 1.27.1
    Update to latest stable release.

--- Library/Formula/gnu-tar.rb
+++ Library/Formula/gnu-tar.rb
@@ -2,15 +2,9 @@ require 'formula'
 
 class GnuTar < Formula
   homepage 'http://www.gnu.org/software/tar/'
-  url 'http://ftpmirror.gnu.org/tar/tar-1.27.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/tar/tar-1.27.tar.gz'
-  sha1 '790cf784589a9fcc1ced33517e71051e3642642f'
-
-  bottle do
-    sha1 "0145c9b2b1b4d54b03afa85514c44dc9755b5afe" => :mavericks
-    sha1 "3816d5a949aada9ed280d524187ab05a4ab28447" => :mountain_lion
-    sha1 "4d9843926d6dc0bb2f19c6e94d537a1f1618986b" => :lion
-  end
+  url 'http://ftpmirror.gnu.org/tar/tar-1.27.1.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/tar/tar-1.27.1.tar.gz'
+  sha1 '192f480cac95b1fbaff11c87a64f7092045b411b'
 
   def install
     args = ["--prefix=#{prefix}", "--mandir=#{man}"]
