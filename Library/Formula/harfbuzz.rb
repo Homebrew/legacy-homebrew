73ae94cb19c898edcd9e1ac08d3aaaeb830e642b
David Christenson <david@the-david.com>
2014-05-12 04:22:32 -0600

    harfbuzz 0.9.28
    Update to latest stable release.

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -2,15 +2,8 @@ require 'formula'
 
 class Harfbuzz < Formula
   homepage 'http://www.freedesktop.org/wiki/Software/HarfBuzz'
-  url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.27.tar.bz2'
-  sha256 '08584ae91c98d5b14d5f2c45b01410a12b030c9397bd73135bd2155297b447db'
-
-  bottle do
-    cellar :any
-    sha1 "27623054bf6ca9f54428f0fe63bc2c2caa726325" => :mavericks
-    sha1 "120db14c8928fc948340ff14ecab0bb814048fac" => :mountain_lion
-    sha1 "7c99b0595e9ea6e44eb346779bd06db1aeb6b49a" => :lion
-  end
+  url 'http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.28.tar.bz2'
+  sha256 'a567f7c58018af0a9751e18641fd8434bfcef8307122dabe47dd652ce8bde048'
 
   depends_on 'pkg-config' => :build
   depends_on 'glib'
