db16b5ec438c8b557d4096dc59395dad1fc0182c
David Christenson <david@the-david.com>
2014-05-12 06:10:52 -0600

    Restore bottle block

--- Library/Formula/little-cms2.rb
+++ Library/Formula/little-cms2.rb
@@ -5,6 +5,13 @@ class LittleCms2 < Formula
   url 'https://downloads.sourceforge.net/project/lcms/lcms/2.6/lcms2-2.6.tar.gz'
   sha1 'b0ecee5cb8391338e6c281d1c11dcae2bc22a5d2'
 
+  bottle do
+    cellar :any
+    sha1 "06ec67737332d592670109bac5547d42276557a0" => :mavericks
+    sha1 "57ef9368e594929537805df109031fbe799b3d8a" => :mountain_lion
+    sha1 "79aa4fcb97599f1902b70eebbf3acbbc9936f75e" => :lion
+  end
+
   depends_on 'jpeg' => :recommended
   depends_on 'libtiff' => :recommended
 
