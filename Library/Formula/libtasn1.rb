8bbf998b0716ebf62982b7cd9c39772725860559
David Christenson <david@the-david.com>
2014-05-12 05:13:49 -0600

    libtasn1 3.5
    Closes #29180.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libtasn1 < Formula
   homepage 'http://www.gnu.org/software/libtasn1/'
-  url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-3.4.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/libtasn1/libtasn1-3.4.tar.gz'
-  sha1 'f0e95f58b3c37405d48b91a585b517a4134586a9'
+  url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-3.5.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/libtasn1/libtasn1-3.5.tar.gz'
+  sha1 '7e38f027c765eb5434bf050aef0f20aee45e3420'
 
   bottle do
     cellar :any
