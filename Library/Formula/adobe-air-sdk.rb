36d4374d948d3da73ba9218d6b3d047167c29f2b
Adam Vandenberg <flangy@gmail.com>
2014-05-13 09:53:06 -0700

    adobe-air-sdk 13
    Closes #29209.
    Closes #29215.

--- Library/Formula/adobe-air-sdk.rb
+++ Library/Formula/adobe-air-sdk.rb
@@ -1,21 +1,24 @@
-require 'formula'
+require "formula"
 
+# Find downloads at:
+# https://helpx.adobe.com/air/kb/archived-air-sdk-version.html
 class AdobeAirSdk < Formula
-  homepage 'http://adobe.com/products/air/sdk'
+  homepage "http://adobe.com/products/air/sdk"
+  version "13.0"
 
-  option 'with-compiler', 'Grab the version with the new compiler (for non-Flex users).'
+  option "with-flex-support", "Do not download the new compiler with the SDK."
 
-  if build.with? 'compiler'
-    sha1 '1334fad165bab05f3abe0579ed1776e58c8da43e'
-    url 'http://airdownload.adobe.com/air/mac/download/3.9/AIRSDK_Compiler.tbz2'
+  if build.without? "flex-support"
+    url "http://airdownload.adobe.com/air/mac/download/13.0/AIRSDK_Compiler.tbz2"
+    sha1 "b3f15e01bb4f2ec4d82151257672156aa49f3ba8"
   else
-    sha1 '715da9ad8f3bc7a61dcc54835084cbc7b9a92d66'
-    url 'http://airdownload.adobe.com/air/mac/download/3.9/AdobeAIRSDK.tbz2'
+    url "http://airdownload.adobe.com/air/mac/download/13.0/AdobeAIRSDK.tbz2"
+    sha1 "dfbdde7e7de31804ad3d74df9f0ba71c5cf5c2ad"
   end
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
   end
 end
