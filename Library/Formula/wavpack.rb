4f4189b9ce90f3208ba5f8862ebde3cb7477f472
David Christenson <david@the-david.com>
2014-05-12 20:37:28 -0600

    wavpack 4.70.0
    Update to latest stable release.

--- Library/Formula/wavpack.rb
+++ Library/Formula/wavpack.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Wavpack < Formula
   homepage 'http://www.wavpack.com/'
-  url 'http://www.wavpack.com/wavpack-4.60.1.tar.bz2'
-  sha1 '003c65cb4e29c55011cf8e7b10d69120df5e7f30'
+  url 'http://www.wavpack.com/wavpack-4.70.0.tar.bz2'
+  sha1 '7bf2022c988c19067196ee1fdadc919baacf46d1'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
