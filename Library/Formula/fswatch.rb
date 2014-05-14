95c58b451f9f9f3d3596addf23806fc09b032a2e
Benny Wong <bdotdub@gmail.com>
2014-05-14 16:43:11 -0400

    fswatch 1.3.2
    Closes #29261.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/fswatch.rb
+++ Library/Formula/fswatch.rb
@@ -2,10 +2,15 @@ require 'formula'
 
 class Fswatch < Formula
   homepage 'https://github.com/alandipert/fswatch'
-  url 'https://github.com/alandipert/fswatch/archive/r0.0.2.tar.gz'
-  sha1 '80dcf0d4af0b28f1b777528050f2e094218cee5f'
+  url 'https://github.com/alandipert/fswatch/archive/v1.3.2.tar.gz'
+  sha1 'af52fd305b80bc5e05217edb1d8cdce02fdb4e91'
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
+    system './autogen.sh'
+    system './configure'
     system 'make'
     bin.install 'fswatch'
   end
