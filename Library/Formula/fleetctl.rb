956e5bc10e4276c6713c5bca915970ccab40e4d1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-05-14 18:29:57 +0100

    fleetctl: update 0.3.2 bottle.
    Closes #29250.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/fleetctl.rb
+++ Library/Formula/fleetctl.rb
@@ -7,9 +7,9 @@ class Fleetctl < Formula
   head 'https://github.com/coreos/fleet.git'
 
   bottle do
-    sha1 "486d83a0cee1d7b5c62b554f5197bd3273a7ce49" => :mavericks
-    sha1 "c62b2821349c898cf03d786b3e75bbb52a291935" => :mountain_lion
-    sha1 "414335309df919f7adf9abe92855d74d63dbb7e5" => :lion
+    sha1 "96b02915f2b3f5ea32ccdb72776c6cac3232678f" => :mavericks
+    sha1 "4156313fb8d0b37718c916a06002bf66f89302f0" => :mountain_lion
+    sha1 "7aa84216b036219dfe2107812161d73cd0036596" => :lion
   end
 
   depends_on 'go' => :build
