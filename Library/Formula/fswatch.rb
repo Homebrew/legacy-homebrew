34789d6df415ed5b5348c814426f938afc98b953
Benny Wong <bdotdub@gmail.com>
2014-05-14 18:37:48 -0400

    Fix checksum and include autoconf / automake

--- Library/Formula/fswatch.rb
+++ Library/Formula/fswatch.rb
@@ -3,9 +3,13 @@ require 'formula'
 class Fswatch < Formula
   homepage 'https://github.com/alandipert/fswatch'
   url 'https://github.com/alandipert/fswatch/archive/v1.3.2.tar.gz'
-  sha1 'c8749d478fb0bfab01fb014bd39b2830e0cdd93e'
+  sha1 'af52fd305b80bc5e05217edb1d8cdce02fdb4e91'
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
+    system './autogen.sh'
     system './configure'
     system 'make'
     bin.install 'fswatch'
