4dea383c21aa2217e19e97022d85e46140697684
David Wales <daviewales@gmail.com>
2014-05-14 17:37:55 +1000

    qt: Add option to disable phonon

--- Library/Formula/qt.rb
+++ Library/Formula/qt.rb
@@ -18,6 +18,7 @@ class Qt < Formula
   option 'with-qt3support', 'Build with deprecated Qt3Support module support'
   option 'with-docs', 'Build documentation'
   option 'developer', 'Build and link with developer options'
+  option 'phonon', 'Build with Qt phonon (default=true)'
 
   depends_on "d-bus" => :optional
   depends_on "mysql" => :optional
@@ -61,6 +62,11 @@ class Qt < Formula
       args << "-qt3support"
     else
       args << "-no-qt3support"
+    end
+
+    if build.without? "phonon"
+      args << "-no-phonon"
+      args << "-no-phonon-backend"
     end
 
     args << "-nomake" << "docs" if build.without? 'docs'
