e485d1780763fea4fc024e08c2f041ccfa4556d8
Adam Vandenberg <flangy@gmail.com>
2014-05-14 20:56:49 -0700

    mysql-proxy 0.8.4

--- Library/Formula/mysql-proxy.rb
+++ Library/Formula/mysql-proxy.rb
@@ -1,15 +1,15 @@
-require 'formula'
+require "formula"
 
 class MysqlProxy < Formula
-  homepage 'http://dev.mysql.com/doc/refman/5.6/en/mysql-proxy.html'
-  url 'http://cdn.mysql.com/Downloads/MySQL-Proxy/mysql-proxy-0.8.3.tar.gz'
-  sha1 'ce6e03e2bdcafecd06a9c3582096dd4eeacdaaa8'
+  homepage "http://dev.mysql.com/doc/refman/5.6/en/mysql-proxy.html"
+  url "http://cdn.mysql.com/Downloads/MySQL-Proxy/mysql-proxy-0.8.4.tar.gz"
+  sha1 "626cea599306f6cfb3a632a024ed034df08cf1b9"
 
   depends_on :mysql
-  depends_on 'pkg-config' => :build
-  depends_on 'glib'
-  depends_on 'libevent'
-  depends_on 'lua'
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libevent"
+  depends_on "lua"
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -19,6 +19,6 @@ class MysqlProxy < Formula
                           "--includedir=#{include}/mysqlproxy"
     system "make install"
     # Copy over the example scripts
-    (share+"mysqlproxy").install Dir['examples/*.lua']
+    (share+"mysqlproxy").install Dir["examples/*.lua"]
   end
 end
