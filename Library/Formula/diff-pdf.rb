ab59ea1e3a4ef68873975e3f2ff2d5900169a62d
Piotr Kuczynski <piotr.kuczynski@here.com>
2014-05-15 17:42:12 +0200

    Style guide compliance

--- Library/Formula/diff-pdf.rb
+++ Library/Formula/diff-pdf.rb
@@ -1,31 +1,32 @@
-require 'formula'
+require "formula"
 
 class DiffPdf < Formula
-  homepage 'http://vslavik.github.io/diff-pdf/'
-  url 'https://github.com/vslavik/diff-pdf/archive/v0.2.tar.gz'
-  sha1 '308ea8e92ac609ca88303dce6a6e8403c6b9f11f'
+  homepage "http://vslavik.github.io/diff-pdf/"
+  url "https://github.com/vslavik/diff-pdf/archive/v0.2.tar.gz"
+  sha1 "308ea8e92ac609ca88303dce6a6e8403c6b9f11f"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'automake' => :build
-  depends_on 'autoconf' => :build
+  depends_on "pkg-config" => :build
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
   depends_on :x11
-  depends_on 'wxmac'
-  depends_on 'cairo'
-  depends_on 'poppler' => 'with-glib'
+  depends_on "wxmac"
+  depends_on "cairo"
+  depends_on "poppler" => "with-glib"
 
   def install
-    system './bootstrap'
+    system "./bootstrap"
 
-    system './configure', '--disable-debug',
-           '--disable-dependency-tracking',
-           '--disable-silent-rules',
+    system "./configure",
+           "--disable-debug",
+           "--disable-dependency-tracking",
+           "--disable-silent-rules",
            "--prefix=#{prefix}"
 
-    system 'make'
-    system 'make', 'install'
+    system "make"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/diff-pdf", '-h'
+    system "#{bin}/diff-pdf", "-h"
   end
 end
