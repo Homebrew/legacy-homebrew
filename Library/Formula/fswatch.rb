cc73e029abc812e642ba8ce21df427c2b1ce6738
Jack Nagel <jacknagel@gmail.com>
2014-05-14 21:23:08 -0500

    fswatch: modernize
     - requires C++11
     - use full autotools build
     - remove broken test

--- Library/Formula/fswatch.rb
+++ Library/Formula/fswatch.rb
@@ -1,21 +1,21 @@
-require 'formula'
+require "formula"
 
 class Fswatch < Formula
-  homepage 'https://github.com/alandipert/fswatch'
-  url 'https://github.com/alandipert/fswatch/archive/v1.3.2.tar.gz'
-  sha1 'af52fd305b80bc5e05217edb1d8cdce02fdb4e91'
+  homepage "https://github.com/alandipert/fswatch"
+  url "https://github.com/alandipert/fswatch/archive/v1.3.2.tar.gz"
+  sha1 "af52fd305b80bc5e05217edb1d8cdce02fdb4e91"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
-  def install
-    system './autogen.sh'
-    system './configure'
-    system 'make'
-    bin.install 'fswatch'
-  end
+  needs :cxx11
 
-  test do
-    system "fswatch 2>&1| grep 'You must specify a directory to watch'"
+  def install
+    ENV.cxx11
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules"
+    system "make", "install"
   end
 end
