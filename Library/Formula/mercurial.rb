6b5f4cd70e275bb5b47f9f0c58b068f3d91207d4
Jack Nagel <jacknagel@gmail.com>
2014-05-15 01:41:38 -0500

    mercurial: docs are bundled with source releases

--- Library/Formula/mercurial.rb
+++ Library/Formula/mercurial.rb
@@ -8,18 +8,8 @@ class Mercurial < Formula
   mirror 'http://fossies.org/linux/misc/mercurial-3.0.tar.gz'
   sha1 'f9648580dd1a6a093fa16d7c28cf5aeefd20f2f0'
 
-  resource "docutils" do
-    url "https://pypi.python.org/packages/source/d/docutils/docutils-0.11.tar.gz"
-    sha1 "3894ebcbcbf8aa54ce7c3d2c8f05460544912d67"
-  end
-
   def install
     ENV.minimal_optimization if MacOS.version <= :snow_leopard
-
-    (buildpath/"doc").install resource("docutils").files("docutils")
-
-    system "make", "doc", "PREFIX=#{prefix}"
-    system "make", "install-doc", "PREFIX=#{prefix}"
 
     system "make", "PREFIX=#{prefix}", "install-bin"
     # Install man pages, which come pre-built in source releases
