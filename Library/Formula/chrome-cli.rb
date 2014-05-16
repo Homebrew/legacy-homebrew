9f21199ffa979a492a305f99c5a134d9e9316312
Adam Vandenberg <flangy@gmail.com>
2014-05-16 13:02:02 -0700

    chrome-cli: add chrome-canary-cli

--- Library/Formula/chrome-cli.rb
+++ Library/Formula/chrome-cli.rb
@@ -8,17 +8,22 @@ class ChromeCli < Formula
   head "https://github.com/prasmussen/chrome-cli.git"
 
   bottle do
-    cellar :any
-    sha1 "d17df1c6d002dbf1009ed604d7affaa6a7c5cfbe" => :mavericks
-    sha1 "4a657ecf95a174d57960538239f0920a285a7e39" => :mountain_lion
   end
 
   depends_on :xcode
   depends_on :macos => :mountain_lion
 
   def install
+    # Release builds
     xcodebuild "SDKROOT=", "SYMROOT=build"
-    bin.install 'build/Release/chrome-cli'
+    bin.install "build/Release/chrome-cli"
+
+    # Canary builds; see:
+    # https://github.com/prasmussen/chrome-cli/issues/15#issuecomment-35202217
+    rm_rf "build"
+    inreplace "chrome-cli/App.m", "com.google.Chrome", "com.google.Chrome.canary"
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/chrome-cli" => "chrome-canary-cli"
   end
 
   test do
