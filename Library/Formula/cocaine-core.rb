cce559c11537760f91d0458c1c0fb4a28db740a8
Evgeny Safronov <division494@gmail.com>
2014-05-16 20:17:38 +0400

    Code style.

--- Library/Formula/cocaine-core.rb
+++ Library/Formula/cocaine-core.rb
@@ -1,24 +1,24 @@
-require 'formula'
+require "formula"
 
 class CocaineCore < Formula
-  homepage 'https://github.com/cocaine/cocaine-core'
-  url 'https://github.com/cocaine/cocaine-core', :using => :git, :tag => '0.11.2.0'
-  sha1 'd045c8586a435e1bb29a3405f43cddc0e651a119'
+  homepage "https://github.com/cocaine/cocaine-core"
+  url "https://github.com/cocaine/cocaine-core", :using => :git, :tag => "0.11.2.0"
+  sha1 "d045c8586a435e1bb29a3405f43cddc0e651a119"
 
   depends_on :macos => :mavericks
-  depends_on 'cmake' => :build
-  depends_on 'boost' => 'c++11'
-  depends_on 'libarchive'
-  depends_on 'libev'
-  depends_on 'libtool' => :build
-  depends_on 'msgpack'
+  depends_on "cmake" => :build
+  depends_on "boost" => "c++11"
+  depends_on "libarchive"
+  depends_on "libev"
+  depends_on "libtool" => :build
+  depends_on "msgpack"
 
   def install
     cmake_args = std_cmake_args
-    cmake_args << '-DCMAKE_CXX_FLAGS=-std=c++11'
-    cmake_args << '-DCMAKE_CXX_FLAGS=-stdlib=libc++'
-    system 'cmake', '.', *cmake_args
-    system 'make', 'install'
+    cmake_args << "-DCMAKE_CXX_FLAGS=-std=c++11"
+    cmake_args << "-DCMAKE_CXX_FLAGS=-stdlib=libc++"
+    system "cmake", ".", *cmake_args
+    system "make", "install"
   end
 
   test do
@@ -48,23 +48,23 @@ class CocaineCore < Formula
         }
       }
     })
-    (testpath/'default.json').write(config)
-    system "mkdir -p #{testpath}/usr/lib/cocaine"
-    system "mkdir -p #{testpath}/var/run/cocaine"
-    system "mkdir -p #{testpath}/var/lib/cocaine"
+    (testpath/"default.json").write(config)
+    (testpath/"usr/lib/cocaine").mkpath
+    (testpath/"var/run/cocaine").mkpath
+    (testpath/"var/lib/cocaine").mkpath
 
     system "cat #{testpath}/default.json"
     system "#{bin}/cocaine-runtime --version"
 
     pid = Process.fork
     if pid.nil?
-      puts 'Starting cocaine-runtime (timeout: 1s) ...'
+      puts "Starting cocaine-runtime (timeout: 1s) ..."
       exec "#{bin}/cocaine-runtime --configuration #{testpath}/default.json"
     else
       Process.detach pid
       sleep 1
       puts "Terminating cocaine-runtime (pid: #{pid}) ..."
-      Process.kill 'TERM', pid
+      Process.kill "TERM", pid
       Process.wait
     end
   end
