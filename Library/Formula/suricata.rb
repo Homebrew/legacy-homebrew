8bff98e3fc08747ba5a64c03ef963548e33d360f
Adam Vandenberg <flangy@gmail.com>
2014-05-15 17:28:25 -0700

    suricata 2.0

--- Library/Formula/suricata.rb
+++ Library/Formula/suricata.rb
@@ -1,18 +1,15 @@
-require 'formula'
+require "formula"
 
 class Suricata < Formula
-  homepage 'http://suricata-ids.org'
-  url 'http://www.openinfosecfoundation.org/download/suricata-1.4.7.tar.gz'
-  sha1 '33eb752ee40e4377e78465d089c5113b7295ce2f'
+  homepage "http://suricata-ids.org"
+  url "http://www.openinfosecfoundation.org/download/suricata-2.0.tar.gz"
+  sha1 "37819a0d6ecb7ebd4201bc32dec40824c145da98"
 
-  depends_on :autoconf
-  depends_on :automake
-  depends_on :libtool
-  depends_on 'pkg-config' => :build
-  depends_on 'libmagic'
-  depends_on 'libnet'
-  depends_on 'libyaml'
-  depends_on 'pcre'
+  depends_on "pkg-config" => :build
+  depends_on "libmagic"
+  depends_on "libnet"
+  depends_on "libyaml"
+  depends_on "pcre"
 
   # Use clang provided strl* functions. Reported upstream:
   # https://redmine.openinfosecfoundation.org/issues/1192
@@ -32,45 +29,46 @@ end
 
 __END__
 diff --git a/src/Makefile.in b/src/Makefile.in
-index 12dc173..d7aeb0e 100644
+index 32cb702..68ef842 100644
 --- a/src/Makefile.in
 +++ b/src/Makefile.in
-@@ -292,7 +292,7 @@ am__suricata_SOURCES_DIST = alert-debuglog.c alert-debuglog.h \
- 	util-runmodes.c util-runmodes.h util-signal.c util-signal.h \
- 	util-spm-bm.c util-spm-bm.h util-spm-bs2bm.c util-spm-bs2bm.h \
- 	util-spm-bs.c util-spm-bs.h util-spm.c util-spm.h util-clock.h \
--	util-strlcatu.c util-strlcpyu.c util-syslog.c util-syslog.h \
-+	util-syslog.c util-syslog.h \
- 	util-threshold-config.c util-threshold-config.h util-time.c \
- 	util-time.h util-unittest.c util-unittest.h \
- 	util-unittest-helper.c util-unittest-helper.h util-validate.h \
-@@ -452,7 +452,6 @@ am_suricata_OBJECTS = alert-debuglog.$(OBJEXT) alert-fastlog.$(OBJEXT) \
- 	util-runmodes.$(OBJEXT) util-signal.$(OBJEXT) \
- 	util-spm-bm.$(OBJEXT) util-spm-bs2bm.$(OBJEXT) \
- 	util-spm-bs.$(OBJEXT) util-spm.$(OBJEXT) \
+@@ -266,7 +266,6 @@ am_suricata_OBJECTS = alert-debuglog.$(OBJEXT) alert-fastlog.$(OBJEXT) \
+ 	util-signal.$(OBJEXT) util-spm-bm.$(OBJEXT) \
+ 	util-spm-bs2bm.$(OBJEXT) util-spm-bs.$(OBJEXT) \
+ 	util-spm.$(OBJEXT) util-storage.$(OBJEXT) \
 -	util-strlcatu.$(OBJEXT) util-strlcpyu.$(OBJEXT) \
  	util-syslog.$(OBJEXT) util-threshold-config.$(OBJEXT) \
  	util-time.$(OBJEXT) util-unittest.$(OBJEXT) \
  	util-unittest-helper.$(OBJEXT) util-affinity.$(OBJEXT) \
-@@ -1322,8 +1321,6 @@ distclean-compile:
- @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-spm-bs.Po@am__quote@
+@@ -862,8 +861,6 @@ util-spm-bs2bm.c util-spm-bs2bm.h \
+ util-spm-bs.c util-spm-bs.h \
+ util-spm.c util-spm.h util-clock.h \
+ util-storage.c util-storage.h \
+-util-strlcatu.c \
+-util-strlcpyu.c \
+ util-syslog.c util-syslog.h \
+ util-threshold-config.c util-threshold-config.h \
+ util-time.c util-time.h \
+@@ -1353,8 +1350,6 @@ distclean-compile:
  @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-spm-bs2bm.Po@am__quote@
  @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-spm.Po@am__quote@
+ @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-storage.Po@am__quote@
 -@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-strlcatu.Po@am__quote@
 -@AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-strlcpyu.Po@am__quote@
  @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-syslog.Po@am__quote@
  @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-threshold-config.Po@am__quote@
  @AMDEP_TRUE@@am__include@ @am__quote@./$(DEPDIR)/util-time.Po@am__quote@
 diff --git a/src/suricata-common.h b/src/suricata-common.h
-index 04543e1..cb7358d 100644
+index 43c76c1..b6010f4 100644
 --- a/src/suricata-common.h
 +++ b/src/suricata-common.h
-@@ -304,8 +304,5 @@ typedef enum PacketProfileDetectId_ {
- #include "util-optimize.h"
+@@ -323,9 +323,6 @@ typedef enum PacketProfileDetectId_ {
  #include "util-path.h"
+ #include "util-conf.h"
  
 -size_t strlcat(char *, const char *src, size_t siz);
 -size_t strlcpy(char *dst, const char *src, size_t siz);
 -
- #endif /* __SURICATA_COMMON_H__ */
- 
+ extern int coverage_unittests;
+ extern int g_ut_modules;
+ extern int g_ut_covered;
