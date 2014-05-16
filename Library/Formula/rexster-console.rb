8e5f5daef329a47fa700aad26afdc4004616eba1
Brennon York <brennon.york@gmail.com>
2014-05-16 08:17:07 -0400

    Rexster Console 2.5.0
    This commit adds the rexster console to the list of homebrew formula. It pulls down the 2.5.0 version
    from their website and copies over the lib/ and doc/ directories into libexec/. Since it is a
    java-based application everything within the libexec/lib/ directory doesn't need built as they are
    java .jar files. The rexster script is copied over into the libexec/bin/ directory and renamed
    'rexster-console' without the version. That same script is then symlinked to the bin/ directory. Last
    an 'inreplace' command is used to change the script to look through symlinks that Homebrew creates to
    find it's /libexec/lib directory. Has a simple test to ensure the system works properly by executing
    the help command.

--- Library/Formula/rexster-console.rb
+++ Library/Formula/rexster-console.rb
@@ -4,10 +4,33 @@ class RexsterConsole < Formula
   homepage "https://github.com/tinkerpop/rexster/wiki"
   url "http://tinkerpop.com/downloads/rexster/rexster-console-2.5.0.zip"
   sha1 "0243908c0ab65baea4b8092bb2b818c597622187"
+  version "2.5.0"
 
   def install
-    prefix.install %w[lib doc]
-    bin.install "bin/rexster-console.sh" => "rexster-console"
+    libexec.install %w[lib doc]
+    (libexec/"ext").mkpath
+    (libexec/"bin").mkpath
+    (libexec/"bin").install "bin/rexster-console.sh" => "rexster-console"
+    bin.install_symlink libexec/"bin/rexster-console"
+ 
+    inreplace "#{libexec}/bin/rexster-console",
+      "CP=\$( echo `dirname \$0`/../lib/*.jar . | sed 's/ /:/g')
+CP=$CP:$( echo `dirname $0`/../ext/*.jar . | sed 's/ /:/g')
+#echo $CP",
+      "# From: http://stackoverflow.com/a/246128
+#   - To resolve finding the directory after symlinks
+SOURCE=\"${BASH_SOURCE[0]}\"
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h \"$SOURCE\" ]; do
+  DIR=\"$( cd -P \"$( dirname \"$SOURCE\" )\" && pwd )\"
+  SOURCE=\"$(readlink \"$SOURCE\")\"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE=\"$DIR/$SOURCE\"
+done
+DIR=\"$( cd -P \"$( dirname \"$SOURCE\" )\" && pwd )\"
+
+CP=$( echo \"$DIR\"/../lib/*.jar . | sed 's/ /:/g')
+CP=$CP:$( echo \"$DIR\"/../ext/*.jar . | sed 's/ /:/g')"
   end
 
   test do
