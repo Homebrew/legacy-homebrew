089b0f48ceef277bb3f7ba46aaf2aade7d15a622
Adam Vandenberg <flangy@gmail.com>
2014-05-16 09:36:58 -0700

    neo4j: simplify URL, modernize

--- Library/Formula/neo4j.rb
+++ Library/Formula/neo4j.rb
@@ -1,23 +1,23 @@
-require 'formula'
+require "formula"
 
 class Neo4j < Formula
-  homepage 'http://neo4j.org'
-  url 'http://download.neo4j.org/artifact?edition=community&version=2.0.3&distribution=tarball&dlid=3981613'
-  sha1 '552e588849acc79acfd2d02c6841e0371a8bf166'
-  version '2.0.3'
+  homepage "http://neo4j.org"
+  url "http://dist.neo4j.org/neo4j-community-2.0.3-unix.tar.gz"
+  sha1 "250b8d8bcb11ea8d9d5bc96cb07390e783c38c28"
+  version "2.0.3"
 
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
 
     # Install jars in libexec to avoid conflicts
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
 
     # Symlink binaries
     bin.install_symlink Dir["#{libexec}/bin/neo4j{,-shell}"]
 
     # Adjust UDC props
-    open("#{libexec}/conf/neo4j-wrapper.conf", 'a') { |f|
+    open("#{libexec}/conf/neo4j-wrapper.conf", "a") { |f|
       f.puts "wrapper.java.additional.4=-Dneo4j.ext.udc.source=homebrew"
 
       # suppress the empty, focus-stealing java gui
