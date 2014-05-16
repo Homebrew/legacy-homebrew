34b075874942249d9cec74ae971dcb73995a4020
Ben Hagen <c.ben.hagen@gmail.com>
2014-05-17 00:06:08 +0200

    fix double-quotes

--- Library/Formula/ltc-tools.rb
+++ Library/Formula/ltc-tools.rb
@@ -6,11 +6,11 @@ class LtcTools < Formula
   sha1 "b126223996b06ac3dd900b322e3ccda9267bcbb3"
   head "https://github.com/x42/ltc-tools.git"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'help2man' => :build
-  depends_on 'libltc'
-  depends_on 'libsndfile'
-  depends_on 'jack'
+  depends_on "pkg-config" => :build
+  depends_on "help2man" => :build
+  depends_on "libltc"
+  depends_on "libsndfile"
+  depends_on "jack"
 
   def install
     system "make", "install"
