cf5d490ae70b010be05c8248e89a8a3e795fc25e
Adam Vandenberg <flangy@gmail.com>
2014-05-17 12:49:04 -0700

    tor: modernize

--- Library/Formula/tor.rb
+++ Library/Formula/tor.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Tor < Formula
-  homepage 'https://www.torproject.org/'
-  url 'https://www.torproject.org/dist/tor-0.2.4.22.tar.gz'
-  sha1 '42349e02c3f6db4e6f2cc52b8a61ea91761ac4d6'
+  homepage "https://www.torproject.org/"
+  url "https://www.torproject.org/dist/tor-0.2.4.22.tar.gz"
+  sha1 "42349e02c3f6db4e6f2cc52b8a61ea91761ac4d6"
 
   bottle do
     sha1 "db30a93edec642fda6226e605371659304da00fa" => :mavericks
@@ -12,18 +12,17 @@ class Tor < Formula
   end
 
   devel do
-    url 'https://www.torproject.org/dist/tor-0.2.5.4-alpha.tar.gz'
-    version '0.2.5.4-alpha'
-    sha1 '6817c103e2e401330823930e1a0dec38e5147ba2'
+    url "https://www.torproject.org/dist/tor-0.2.5.4-alpha.tar.gz"
+    version "0.2.5.4-alpha"
+    sha1 "6817c103e2e401330823930e1a0dec38e5147ba2"
   end
 
-  depends_on 'libevent'
-  depends_on 'openssl'
+  depends_on "libevent"
+  depends_on "openssl"
 
   def install
     # Fix the path to the control cookie.
-    inreplace \
-      'contrib/tor-ctrl.sh',
+    inreplace "contrib/tor-ctrl.sh",
       'TOR_COOKIE="/var/lib/tor/data/control_auth_cookie"',
       'TOR_COOKIE="$HOME/.tor/control_auth_cookie"'
 
@@ -36,7 +35,7 @@ class Tor < Formula
   end
 
   test do
-    system "tor", "--version"
+    system bin/"tor", "--version"
   end
 
   def plist; <<-EOS.undent
