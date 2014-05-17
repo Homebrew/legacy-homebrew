87f6d424cb870a309da08582e649dbebdd1a229e
Yannik Ehlert <yanniks@users.noreply.github.com>
2014-05-17 21:38:06 +0200

    Tor 0.2.4.22, 0.2.5.4
    Closes #29348.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tor.rb
+++ Library/Formula/tor.rb
@@ -2,20 +2,16 @@ require 'formula'
 
 class Tor < Formula
   homepage 'https://www.torproject.org/'
-  url 'https://www.torproject.org/dist/tor-0.2.4.21.tar.gz'
-  sha1 'b93b66e4d5162cefc711cb44f9167ed4799ef990'
-  revision 1
+  url 'https://www.torproject.org/dist/tor-0.2.4.22.tar.gz'
+  sha1 '42349e02c3f6db4e6f2cc52b8a61ea91761ac4d6'
 
   bottle do
-    sha1 "f03d0bf2f97e9520b4ea33c9a15112bdcec49768" => :mavericks
-    sha1 "cc64556bfb03006f029472c2b4541065a174929e" => :mountain_lion
-    sha1 "c612ec254314cf046c292df8416ddf0d8281c4ef" => :lion
   end
 
   devel do
-    url 'https://www.torproject.org/dist/tor-0.2.5.3-alpha.tar.gz'
-    version '0.2.5.3-alpha'
-    sha1 '29784b3f711780cd60fff076f6deb9b1f633fe5c'
+    url 'https://www.torproject.org/dist/tor-0.2.5.4-alpha.tar.gz'
+    version '0.2.5.4-alpha'
+    sha1 '6817c103e2e401330823930e1a0dec38e5147ba2'
   end
 
   depends_on 'libevent'
