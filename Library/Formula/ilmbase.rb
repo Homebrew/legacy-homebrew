95ec7bdff0dd3f27f2548722689c4d5e6fc6d60e
u0hun <lyuyh@daum.net>
2014-05-17 13:45:58 +0900

    ilmbase: add mirror
    Closes #29334.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ilmbase.rb
+++ Library/Formula/ilmbase.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Ilmbase < Formula
   homepage 'http://www.openexr.com/'
   url 'http://download.savannah.gnu.org/releases/openexr/ilmbase-2.1.0.tar.gz'
+  mirror 'http://download-mirror.savannah.gnu.org/releases/openexr/ilmbase-2.1.0.tar.gz'
   sha1 '306d76e7a2ac619c2f641f54b59dd95576525192'
 
   def install
