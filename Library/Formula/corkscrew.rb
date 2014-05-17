2ae1bf9007f0faabaf9bbcc3da5dfe21fe840fc6
Jack Nagel <jacknagel@gmail.com>
2014-05-16 21:46:12 -0500

    corkscrew: copy new config.guess from libtool

--- Library/Formula/corkscrew.rb
+++ Library/Formula/corkscrew.rb
@@ -5,10 +5,13 @@ class Corkscrew < Formula
   url 'http://www.agroman.net/corkscrew/corkscrew-2.0.tar.gz'
   sha1 '8bdb4c0dc71048136c721c33229b9bf795230b32'
 
+  depends_on "libtool" => :build
+
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--host=apple"
-    system "make install"
+    cp Dir["#{Formula["libtool"].opt_share}/libtool/config/config.*"], buildpath
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
   end
 end
