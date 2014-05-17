bb8336ba2d268434d0dec1368e3ea081c9ed01df
Matthew R. Wilson <mwilson@mattwilson.org>
2014-05-16 23:18:29 -0700

    Fix tcl-tk dependency for xcircuit formula

--- Library/Formula/xcircuit.rb
+++ Library/Formula/xcircuit.rb
@@ -5,7 +5,7 @@ class Xcircuit < Formula
   url "http://opencircuitdesign.com/xcircuit/archive/xcircuit-3.7.57.tgz"
   sha1 "29cdb84fb6083c27398a351e0481b310c4f8d44c"
 
-  depends_on "tcl-tk" => "with-x11"
+  depends_on "homebrew/dupes/tcl-tk" => "with-x11"
   depends_on :x11
 
   def install
