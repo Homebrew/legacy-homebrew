c6eba941fc6ee4dfbbbd1e94c1e1c875fac3ea06
David Christenson <david@the-david.com>
2014-05-17 17:21:30 -0600

    rebar 2.3.0
    Closes #29353.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/rebar.rb
+++ Library/Formula/rebar.rb
@@ -1,20 +1,20 @@
-require 'formula'
+require "formula"
 
 class Rebar < Formula
-  homepage 'https://github.com/rebar/rebar'
-  url 'https://github.com/rebar/rebar/archive/2.2.0.tar.gz'
-  sha1 '8b246586383d8809c7681f3e56bfc8d8c3b3757e'
+  homepage "https://github.com/rebar/rebar"
+  url "https://github.com/rebar/rebar/archive/2.3.0.tar.gz"
+  sha1 "4fc7eed4f3210c1547ba5ddfb5d3dc54a6422c37"
 
   head "https://github.com/rebar/rebar.git", :branch => "master"
 
-  depends_on 'erlang'
+  depends_on "erlang"
 
   def install
-    system './bootstrap'
-    bin.install 'rebar'
+    system "./bootstrap"
+    bin.install "rebar"
   end
 
   test do
-    system 'rebar', '--version'
+    system "rebar", "--version"
   end
 end
