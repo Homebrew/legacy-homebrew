f3c10242fb8116ad2ac703085a423a4cb40a099a
Adam Vandenberg <flangy@gmail.com>
2014-05-18 16:23:07 -0700

    saxon: modernize

--- Library/Formula/saxon.rb
+++ Library/Formula/saxon.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class Saxon < Formula
   homepage "http://saxon.sourceforge.net"
-  url 'https://downloads.sourceforge.net/project/saxon/Saxon-HE/9.5/SaxonHE9-5-1-5J.zip'
-  sha1 'bb8476866cacb72e5567bdfc246570e7f0986e48'
-  version '9.5.1.5'
+  url "https://downloads.sourceforge.net/project/saxon/Saxon-HE/9.5/SaxonHE9-5-1-5J.zip"
+  sha1 "bb8476866cacb72e5567bdfc246570e7f0986e48"
+  version "9.5.1.5"
 
   def install
-    libexec.install Dir["*"]
-    bin.write_jar_script libexec/'saxon9he.jar', 'saxon'
+    libexec.install Dir["*.jar"] + ["doc", "notices"]
+    bin.write_jar_script libexec/"saxon9he.jar", "saxon"
   end
 end
