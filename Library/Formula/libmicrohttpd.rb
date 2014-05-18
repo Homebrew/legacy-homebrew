657964e5b47042ad3fffd172fe0e2132e7a25c2f
Brett Koonce <koonce@gmail.com>
2014-05-18 09:56:17 -0700

    libmicrohttpd 0.9.35

--- Library/Formula/libmicrohttpd.rb
+++ Library/Formula/libmicrohttpd.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Libmicrohttpd < Formula
-  homepage 'http://www.gnu.org/software/libmicrohttpd/'
-  url 'http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.34.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.34.tar.gz'
-  sha1 '1122f26fa278556630aaef40a500a9be19bdcfc9'
+  homepage "http://www.gnu.org/software/libmicrohttpd/"
+  url "http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.35.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.35.tar.gz"
+  sha1 "851aacbf46b3145192b07c419c146dd0555fc64f"
 
   bottle do
     cellar :any
