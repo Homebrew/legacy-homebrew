68449a3d00a234960dba19001e6b752164090905
BrewTestBot <brew-test-bot@googlegroups.com>
2014-05-18 19:01:59 +0100

    libgaiagraphics: update 0.5 bottle.

--- Library/Formula/libgaiagraphics.rb
+++ Library/Formula/libgaiagraphics.rb
@@ -6,6 +6,10 @@ class Libgaiagraphics < Formula
   sha1 "db9eaef329fc650da737c71aac6136088fcb6549"
 
   bottle do
+    cellar :any
+    sha1 "2f529e4d286413b9fd423ef8ad10c6addfe6e278" => :mavericks
+    sha1 "d2c56a59614c33dcbd6604363c78f008e43cf22c" => :mountain_lion
+    sha1 "3cd754d017caf49793ae151a977101e87128e09a" => :lion
   end
 
   depends_on "pkg-config" => :build
