7a7d807743f3b04674287fe144079fb71a74d842
Ahmed El Gabri <ahmed@gabri.me>
2014-05-18 11:50:22 -0700

    Update the_silver_searcher

--- Library/Formula/the_silver_searcher.rb
+++ Library/Formula/the_silver_searcher.rb
@@ -3,8 +3,8 @@ require 'formula'
 class TheSilverSearcher < Formula
   homepage 'https://github.com/ggreer/the_silver_searcher'
   head 'https://github.com/ggreer/the_silver_searcher.git'
-  url 'https://github.com/ggreer/the_silver_searcher/archive/0.21.0.tar.gz'
-  sha1 '86503dea202a0eca44a4207a97aa89e1d2353979'
+  url 'https://github.com/ggreer/the_silver_searcher/archive/0.22.0.tar.gz'
+  sha1 '9303d2b8457122fa6f5c69b7058b82b3633c02c2'
 
   bottle do
     cellar :any
