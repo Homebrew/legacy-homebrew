72927b3e9303df5a515ef7b2985055074c2d5a71
Adam Vandenberg <flangy@gmail.com>
2014-05-18 10:24:52 -0700

    libgaiagraphics 0.5
    This is listed as the development version on the homepage, but it is the
    version needed by the current stable version of spatialte-gui.
    
    Nothing else in Homebrew depends on this library, so promote to stable.

--- Library/Formula/libgaiagraphics.rb
+++ Library/Formula/libgaiagraphics.rb
@@ -1,24 +1,23 @@
-require 'formula'
+require "formula"
 
 class Libgaiagraphics < Formula
-  homepage 'https://www.gaia-gis.it/fossil/libgaiagraphics/index'
-  url 'http://www.gaia-gis.it/gaia-sins/gaiagraphics-sources/libgaiagraphics-0.4b.tar.gz'
-  sha1 'd045ad6d22db9e67ba410a62b9398c337786fe53'
-  revision 1
+  homepage "https://www.gaia-gis.it/fossil/libgaiagraphics/index"
+  url "http://www.gaia-gis.it/gaia-sins/gaiagraphics-sources/libgaiagraphics-0.5.tar.gz"
+  sha1 "db9eaef329fc650da737c71aac6136088fcb6549"
 
-  devel do
-    url 'http://www.gaia-gis.it/gaia-sins/gaiagraphics-sources/libgaiagraphics-0.5.tar.gz'
-    sha1 'db9eaef329fc650da737c71aac6136088fcb6549'
+  bottle do
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libgeotiff'
-  depends_on 'jpeg'
-  depends_on 'cairo'
-  depends_on 'libpng'
+  depends_on "pkg-config" => :build
+  depends_on "libgeotiff"
+  depends_on "jpeg"
+  depends_on "cairo"
+  depends_on "libpng"
+  depends_on "proj"
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
