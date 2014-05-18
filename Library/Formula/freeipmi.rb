151b678a2f39ceade90ff610c48a87c3872fac99
Brett Koonce <koonce@gmail.com>
2014-05-18 10:07:55 -0700

    freeipmi 1.4.3

--- Library/Formula/freeipmi.rb
+++ Library/Formula/freeipmi.rb
@@ -1,10 +1,9 @@
-require 'formula'
+require "formula"
 
 class Freeipmi < Formula
-  homepage 'http://www.gnu.org/software/freeipmi/'
-  url 'http://ftpmirror.gnu.org/freeipmi/freeipmi-1.4.1.tar.gz'
-  sha1 'd59fd66e6fe9a58437d55a4bdd96ee431a8348e4'
-  revision 1
+  homepage "http://www.gnu.org/software/freeipmi/"
+  url "http://ftpmirror.gnu.org/freeipmi/freeipmi-1.4.3.tar.gz"
+  sha1 "83d5f54ba98874e3e26c3b4d9ceaf48894667eef"
 
   bottle do
     sha1 "b98405e8318c8dd990f598b769d1e6a155e3c2a3" => :mavericks
@@ -12,19 +11,18 @@ class Freeipmi < Formula
     sha1 "695a73d4e94af49e9a02bf103d16a3e5aa194a51" => :lion
   end
 
-  depends_on 'argp-standalone'
-  depends_on 'libgcrypt'
+  depends_on "argp-standalone"
+  depends_on "libgcrypt"
 
   def install
-    system './configure', "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}"
     # This is a big hammer to disable building the man pages
     # It breaks under homebrew's build system and I'm not sure why
     inreplace "man/Makefile", "install: install-am", "install:"
-    system 'make', 'install'
+    system "make", "install"
   end
 
   test do
     system "#{sbin}/ipmi-fru", "--version"
   end
-
 end
