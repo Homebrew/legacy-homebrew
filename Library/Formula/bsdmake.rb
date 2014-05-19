b6e593a0a7a48db9bee087a98085d616aae67055
Adam Vandenberg <flangy@gmail.com>
2014-05-18 19:28:38 -0700

    bsdmake: quotes

--- Library/Formula/bsdmake.rb
+++ Library/Formula/bsdmake.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Bsdmake < Formula
-  homepage 'http://opensource.apple.com/'
-  url 'http://opensource.apple.com/tarballs/bsdmake/bsdmake-24.tar.gz'
-  sha1 '9ce3c3fc01e0eb47d82827b1eb227eb371fefd5c'
+  homepage "http://opensource.apple.com/"
+  url "http://opensource.apple.com/tarballs/bsdmake/bsdmake-24.tar.gz"
+  sha1 "9ce3c3fc01e0eb47d82827b1eb227eb371fefd5c"
 
   keg_only :provided_until_xcode43
 
@@ -31,9 +31,9 @@ class Bsdmake < Formula
 
   def install
     # Replace @PREFIX@ inserted by MacPorts patches
-    # Use 'prefix' since this is sometimes a keg-only brew
+    # Use "prefix" since this is sometimes a keg-only brew
     # But first replace the X11 path if X11 is installed
-    inreplace 'mk/sys.mk', '@PREFIX@', MacOS::X11.prefix || prefix
+    inreplace "mk/sys.mk", "@PREFIX@", MacOS::X11.prefix || prefix
     inreplace %W[mk/bsd.README
                  mk/bsd.cpu.mk
                  mk/bsd.doc.mk
@@ -42,20 +42,20 @@ class Bsdmake < Formula
                  mk/bsd.port.mk
                  mk/bsd.port.subdir.mk
                  pathnames.h],
-                 '@PREFIX@', prefix
+                 "@PREFIX@", prefix
 
-    inreplace 'mk/bsd.own.mk' do |s|
-      s.gsub! '@INSTALL_USER@', `id -un`.chomp
-      s.gsub! '@INSTALL_GROUP@', `id -gn`.chomp
+    inreplace "mk/bsd.own.mk" do |s|
+      s.gsub! "@INSTALL_USER@", `id -un`.chomp
+      s.gsub! "@INSTALL_GROUP@", `id -gn`.chomp
     end
 
     # See GNUMakefile
-    ENV.append 'CFLAGS', '-D__FBSDID=__RCSID'
-    ENV.append 'CFLAGS', '-mdynamic-no-pic'
+    ENV.append "CFLAGS", "-D__FBSDID=__RCSID"
+    ENV.append "CFLAGS", "-mdynamic-no-pic"
 
     system "make", "-f", "Makefile.dist"
-    bin.install 'pmake' => 'bsdmake'
-    man1.install 'make.1' => 'bsdmake.1'
-    (share/'mk/bsdmake').install Dir['mk/*']
+    bin.install "pmake" => "bsdmake"
+    man1.install "make.1" => "bsdmake.1"
+    (share/"mk/bsdmake").install Dir["mk/*"]
   end
 end
