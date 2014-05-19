225fc1b5a09e1594e0a11ea916a1870121ba30c0
Jordi Saludes <jordi.saludes@upc.edu>
2014-05-19 15:44:47 +0200

    haskell-platform again

--- Library/Formula/gf.rb
+++ Library/Formula/gf.rb
@@ -2,32 +2,30 @@ require "formula"
 class Gf < Formula
     homepage "http://www.grammaticalframework.org/"
 
-    depends_on 'haskell-platform'
+    depends_on "haskell-platform"
 
     stable do
         url "http://www.grammaticalframework.org/download/gf-3.5.tar.gz"
         sha1 "9c3d156df73b79c2121a46c6c29c571bfdd619c2"
     end
-    
+
     def install
-        system 'cabal', 'install'
+      system "cabal", "update"
+      system "cabal", "install"
     end
 
     test  do
-
-        system 'gf', '-version'
-
+      system "gf", "-version"
         (testpath/"Hello.gf").write <<-EOS.undent
-             abstract Hello = {
-             flags startcat = Greeting ;
-             cat Greeting ; Recipient ;
-             fun
-                Hello : Recipient -> Greeting ;
-                World, Mum, Friends : Recipient ; }
+        abstract Hello = {
+        flags startcat = Greeting ;
+        cat Greeting ; Recipient ;
+        fun
+          Hello : Recipient -> Greeting ;
+          World, Mum, Friends : Recipient ; }
         EOS
 
-        system 'gf', '--make',
-               testpath/"Hello.gf"
+      system "gf", "--make", testpath/"Hello.gf"
         assert(File.exists?(testpath/"Hello.pgf"), "No pgf made")
     end
 end
