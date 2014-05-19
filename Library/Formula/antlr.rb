f0190137c40206478075d5d5c6e2d8217a67c614
Christoph Hösler <christoph.hoesler@gmail.com>
2014-05-19 18:57:37 +0200

    antlr: add grun script
    The quick start guide on the antlr website (http://www.antlr.org/)
    uses a TestRig class inside the antlr jar which the authors aliased as
    grun. This alias will get installed as a wrapper script.
    
    Closes #29392.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/antlr.rb
+++ Library/Formula/antlr.rb
@@ -8,5 +8,9 @@ class Antlr < Formula
   def install
     prefix.install "antlr-#{version}-complete.jar"
     bin.write_jar_script prefix/"antlr-#{version}-complete.jar", "antlr4"
+    (bin+"grun").write <<-EOS.undent
+      #!/bin/bash
+      java -classpath #{prefix}/antlr-#{version}-complete.jar:. org.antlr.v4.runtime.misc.TestRig "$@"
+    EOS
   end
 end
