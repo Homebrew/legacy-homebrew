1e5cdf871549e57953e62592320a4592a4a23713
insomniacslk <barberio@84383555c57c.ant.amazon.com>
2014-05-20 15:14:37 +0100

    Changed homepage URL as suggested in https://github.com/Homebrew/homebrew/pull/29406

--- Library/Formula/udptunnel.rb
+++ Library/Formula/udptunnel.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Udptunnel < Formula
-  homepage "http://wiki.leipzig.freifunk.net/Udptunnel"
+  homepage "http://www.cs.columbia.edu/~lennox/udptunnel"
   url "http://www.cs.columbia.edu/~lennox/udptunnel/udptunnel-1.1.tar.gz"
   sha1 "c768097d9bca23d6be35931b010b75a451f34eb8"
 
