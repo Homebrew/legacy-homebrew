bae800ffcc9c9fcf24b0dd41fc2dfb442fe14f1e
Adam Vandenberg <flangy@gmail.com>
2014-05-20 09:52:50 -0700

    valabind: quotes

--- Library/Formula/valabind.rb
+++ Library/Formula/valabind.rb
@@ -1,23 +1,22 @@
-require 'formula'
+require "formula"
 
 class Valabind < Formula
-  homepage 'http://radare.org/'
-  url 'https://github.com/radare/valabind/archive/0.8.0.tar.gz'
-  sha1 'f677110477e14c2e18ac61c56730ab0e51ac450d'
+  homepage "http://radare.org/"
+  head "https://github.com/radare/valabind.git"
+  url "https://github.com/radare/valabind/archive/0.8.0.tar.gz"
+  sha1 "f677110477e14c2e18ac61c56730ab0e51ac450d"
 
-  head 'https://github.com/radare/valabind.git'
-
-  depends_on 'pkg-config' => :build
-  depends_on 'swig' => :run
-  depends_on 'vala'
+  depends_on "pkg-config" => :build
+  depends_on "swig" => :run
+  depends_on "vala"
 
   # Fixes an issue in the vala version detection script.
-  #  * https://github.com/radare/valabind/pull/24
+  # https://github.com/radare/valabind/pull/24
   patch :DATA
 
   def install
-    system 'make'
-    system 'make', 'install', "PREFIX=#{prefix}"
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 end
 
