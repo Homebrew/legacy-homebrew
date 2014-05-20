24af7fe49219fd0b5935fad219ce4e1d0821ea7c
Adam Vandenberg <flangy@gmail.com>
2014-05-20 07:46:43 -0700

    udptunnel: install docs

--- Library/Formula/udptunnel.rb
+++ Library/Formula/udptunnel.rb
@@ -14,6 +14,7 @@ class Udptunnel < Formula
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
+    doc.install "udptunnel.html"
   end
 
   test do
