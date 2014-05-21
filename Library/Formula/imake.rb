d0c91cbd3224af2d6601702e24443aae0c826eda
Jack Nagel <jacknagel@gmail.com>
2014-05-21 18:54:29 -0500

    imake 1.0.7

--- Library/Formula/imake.rb
+++ Library/Formula/imake.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class Imake < Formula
-  homepage 'http://xorg.freedesktop.org'
-  url 'http://xorg.freedesktop.org/releases/individual/util/imake-1.0.6.tar.bz2'
-  sha1 'a54c025d7ac9894b6bc919d13454c6adb12ae140'
+  homepage "http://xorg.freedesktop.org"
+  url "http://xorg.freedesktop.org/releases/individual/util/imake-1.0.7.tar.bz2"
+  sha1 "52e236776133f217d438622034b8603d201a6ec5"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :x11
 
-  resource 'xorg-cf-files' do
-    url 'http://xorg.freedesktop.org/releases/individual/util/xorg-cf-files-1.0.5.tar.bz2'
-    sha1 'ae22eb81d56d018f0b3b149f70965ebfef2385fd'
+  resource "xorg-cf-files" do
+    url "http://xorg.freedesktop.org/releases/individual/util/xorg-cf-files-1.0.5.tar.bz2"
+    sha1 "ae22eb81d56d018f0b3b149f70965ebfef2385fd"
   end
 
   def install
@@ -18,7 +18,7 @@ class Imake < Formula
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
 
-    resource('xorg-cf-files').stage do
+    resource("xorg-cf-files").stage do
       # Fix for different X11 locations.
       inreplace "X11.rules", "define TopXInclude	/**/",
                 "define TopXInclude	-I#{MacOS::X11.include}"
