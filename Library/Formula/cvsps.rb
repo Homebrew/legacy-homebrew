32c2fb33113c612a14c289e25f8d7051903a21d6
Johan Bergström <bugs@bergstroem.nu>
2014-05-21 20:43:39 +1000

    Cvsps: quoting style

--- Library/Formula/cvsps.rb
+++ Library/Formula/cvsps.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class Cvsps < Formula
-  homepage 'http://www.catb.org/~esr/cvsps/'
-  url 'http://www.catb.org/~esr/cvsps/cvsps-3.13.tar.gz'
-  sha1 '29d814f9083e3c51bba1b1fadf6c94ed9c2caa1e'
+  homepage "http://www.catb.org/~esr/cvsps/"
+  url "http://www.catb.org/~esr/cvsps/cvsps-3.13.tar.gz"
+  sha1 "29d814f9083e3c51bba1b1fadf6c94ed9c2caa1e"
 
-  depends_on 'asciidoc'
-  depends_on 'docbook'
+  depends_on "asciidoc"
+  depends_on "docbook"
 
   def install
     # otherwise asciidoc will fail to find docbook
-    ENV['XML_CATALOG_FILES'] = etc/'xml/catalog'
+    ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
 
     system "make", "all", "cvsps.1"
     system "make", "install", "prefix=#{prefix}"
