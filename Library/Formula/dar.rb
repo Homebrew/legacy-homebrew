91affd3649b7bbae8492fb7d0d2083c320a01645
BrewTestBot <brew-test-bot@googlegroups.com>
2014-05-21 18:26:48 +0100

    dar: add 2.4.13 bottle.
    Closes #29134.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/dar.rb
+++ Library/Formula/dar.rb
@@ -5,6 +5,12 @@ class Dar < Formula
   url 'https://downloads.sourceforge.net/project/dar/dar/2.4.13/dar-2.4.13.tar.gz'
   sha1 'b20471ada21cd0cbe4687e7bdd3c2e6f70f5c0d1'
 
+  bottle do
+    sha1 "f940432b6ef7ef6698ff91b11ebb3f345ad56dde" => :mavericks
+    sha1 "87527fd8b9ebede46f237418faff211a789dda8d" => :mountain_lion
+    sha1 "9ba093a21a28e227b05f41a2938d3d18fbdbefac" => :lion
+  end
+
   option 'with-docs', 'build programming documentation (in particular libdar API documentation) and html man page'
   option 'with-libgcrypt', 'enable strong encryption support'
   option 'with-lzo', 'enable lzo compression support'
