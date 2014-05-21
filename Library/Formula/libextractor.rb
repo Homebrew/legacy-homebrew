3297356d8c62a587309f03bfeee419a21ebbd6ec
Adam Vandenberg <flangy@gmail.com>
2014-05-21 10:29:56 -0700

    sptk, libextractor conflict

--- Library/Formula/libextractor.rb
+++ Library/Formula/libextractor.rb
@@ -1,14 +1,16 @@
-require 'formula'
+require "formula"
 
 class Libextractor < Formula
-  homepage 'http://www.gnu.org/software/libextractor/'
-  url 'http://ftpmirror.gnu.org/libextractor/libextractor-1.3.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/libextractor/libextractor-1.3.tar.gz'
-  sha1 '613d0b80e83c79c3e05e073bcda0d0d0bd1f3336'
+  homepage "http://www.gnu.org/software/libextractor/"
+  url "http://ftpmirror.gnu.org/libextractor/libextractor-1.3.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libextractor/libextractor-1.3.tar.gz"
+  sha1 "613d0b80e83c79c3e05e073bcda0d0d0bd1f3336"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :libtool => :run
-  depends_on 'iso-codes' => :optional
+  depends_on "iso-codes" => :optional
+
+  conflicts_with "sptk", :because => "both install `extract`"
 
   def install
     ENV.deparallelize
