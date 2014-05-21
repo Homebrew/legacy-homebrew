d94f814885ddc390f834727f3682baf02168d63e
Adam Vandenberg <flangy@gmail.com>
2014-05-21 07:25:01 -0700

    plantuml: quotes

--- Library/Formula/plantuml.rb
+++ Library/Formula/plantuml.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class Plantuml < Formula
-  homepage 'http://plantuml.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/plantuml/plantuml.7999.jar'
-  sha1 '97215ec19934d15e247ff54e25a77a4483608d04'
+  homepage "http://plantuml.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/plantuml/plantuml.7999.jar"
+  sha1 "97215ec19934d15e247ff54e25a77a4483608d04"
 
-  depends_on 'graphviz'
+  depends_on "graphviz"
 
   def install
     jar = "plantuml.#{version}.jar"
