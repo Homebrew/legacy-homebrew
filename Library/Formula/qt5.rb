473831de0ac74eaf2fc4e2b54ac14ec08d2f20b5
Christian Moritz <chrmoritz@gmail.com>
2014-05-22 01:49:43 +0200

    qt5 5.3.0
    Closes #29472.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/qt5.rb
+++ Library/Formula/qt5.rb
@@ -1,21 +1,21 @@
-require 'formula'
+require "formula"
 
 class Qt5HeadDownloadStrategy < GitDownloadStrategy
   include FileUtils
 
   def stage
     @clone.cd { reset }
-    safe_system 'git', 'clone', @clone, '.'
-    ln_s @clone, 'qt'
-    safe_system './init-repository', '--mirror', "#{Dir.pwd}/"
-    rm 'qt'
+    safe_system "git", "clone", @clone, "."
+    ln_s @clone, "qt"
+    safe_system "./init-repository", "--mirror", "#{Dir.pwd}/"
+    rm "qt"
   end
 end
 
 class Qt5 < Formula
-  homepage 'http://qt-project.org/'
-  url 'http://download.qt-project.org/official_releases/qt/5.2/5.2.1/single/qt-everywhere-opensource-src-5.2.1.tar.gz'
-  sha1 '31a5cf175bb94dbde3b52780d3be802cbeb19d65'
+  homepage "http://qt-project.org/"
+  url "http://download.qt-project.org/official_releases/qt/5.3/5.3.0/single/qt-everywhere-opensource-src-5.3.0.tar.gz"
+  sha1 "edb6a71b9c45c39251f59a999a48a5f552ff07d3"
 
   bottle do
     revision 2
@@ -24,28 +24,24 @@ class Qt5 < Formula
     sha1 "b2362eb20666b961d1d2acec629e4a581aa2b87a" => :lion
   end
 
-  head 'git://gitorious.org/qt/qt5.git', :branch => 'stable',
+  head "git://gitorious.org/qt/qt5.git", :branch => "stable",
     :using => Qt5HeadDownloadStrategy, :shallow => false
 
   keg_only "Qt 5 conflicts Qt 4 (which is currently much more widely used)."
 
   option :universal
-  option 'with-docs', 'Build documentation'
-  option 'developer', 'Build and link with developer options'
+  option "with-docs", "Build documentation"
+  option "developer", "Build and link with developer options"
 
   depends_on "pkg-config" => :build
   depends_on "d-bus" => :optional
   depends_on "mysql" => :optional
 
-  odie 'qt5: --with-qtdbus has been renamed to --with-d-bus' if build.with? "qtdbus"
-  odie 'qt5: --with-demos-examples is no longer supported' if build.with? "demos-examples"
-  odie 'qt5: --with-debug-and-release is no longer supported' if build.with? "debug-and-release"
+  odie "qt5: --with-qtdbus has been renamed to --with-d-bus" if build.with? "qtdbus"
+  odie "qt5: --with-demos-examples is no longer supported" if build.with? "demos-examples"
+  odie "qt5: --with-debug-and-release is no longer supported" if build.with? "debug-and-release"
 
   def install
-    # fixed hardcoded link to plugin dir: https://bugreports.qt-project.org/browse/QTBUG-29188
-    inreplace "qttools/src/macdeployqt/macdeployqt/main.cpp", "deploymentInfo.pluginPath = \"/Developer/Applications/Qt/plugins\";",
-              "deploymentInfo.pluginPath = \"#{prefix}/plugins\";"
-
     ENV.universal_binary if build.universal?
     args = ["-prefix", prefix,
             "-system-zlib",
@@ -58,9 +54,9 @@ class Qt5 < Formula
     # https://bugreports.qt-project.org/browse/QTBUG-34382
     args << "-no-xcb"
 
-    args << "-plugin-sql-mysql" if build.with? 'mysql'
+    args << "-plugin-sql-mysql" if build.with? "mysql"
 
-    if build.with? 'd-bus'
+    if build.with? "d-bus"
       dbus_opt = Formula["d-bus"].opt_prefix
       args << "-I#{dbus_opt}/lib/dbus-1.0/include"
       args << "-I#{dbus_opt}/include/dbus-1.0"
@@ -70,20 +66,20 @@ class Qt5 < Formula
     end
 
     if MacOS.prefer_64_bit? or build.universal?
-      args << '-arch' << 'x86_64'
+      args << "-arch" << "x86_64"
     end
 
     if !MacOS.prefer_64_bit? or build.universal?
-      args << '-arch' << 'x86'
+      args << "-arch" << "x86"
     end
 
-    args << '-developer-build' if build.include? 'developer'
+    args << "-developer-build" if build.include? "developer"
 
     system "./configure", *args
     system "make"
     ENV.j1
     system "make install"
-    if build.with? 'docs'
+    if build.with? "docs"
       system "make", "docs"
       system "make", "install_docs"
     end
