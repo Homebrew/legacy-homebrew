f4d2443bdad7cc7ea3555e7ab7029318c1bb408a
Mike Naberezny <mike@naberezny.com>
2014-05-22 11:19:22 -0700

    fzf: use full path of executable

--- Library/Formula/fzf.rb
+++ Library/Formula/fzf.rb
@@ -22,6 +22,6 @@ class Fzf < Formula
 
   test do
     (testpath/"list").write %w[hello world].join($/)
-    `cat #{testpath}/list | fzf -f wld`.chomp == "world"
+    assert_equal "world", `cat #{testpath}/list | #{bin}/fzf -f wld`.chomp
   end
 end
