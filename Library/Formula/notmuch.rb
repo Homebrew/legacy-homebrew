03c2be2d89c2e29b4631fcd28eb961990b80bf18
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-05-22 10:40:10 -0700

    notmuch 0.18
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/notmuch.rb
+++ Library/Formula/notmuch.rb
@@ -2,18 +2,14 @@ require 'formula'
 
 class Notmuch < Formula
   homepage "http://notmuchmail.org"
-  url "http://notmuchmail.org/releases/notmuch-0.17.tar.gz"
-  sha1 "0fe14140126a0da04754f548edf7e7b135eeec86"
+  url "http://notmuchmail.org/releases/notmuch-0.18.tar.gz"
+  sha1 "bfbcdc340c4b0d544904b3a8ba70be4e819859d9"
 
   depends_on "pkg-config" => :build
   depends_on "emacs" => :optional
   depends_on "xapian"
   depends_on "talloc"
   depends_on "gmime"
-
-  # Fix for mkdir behavior change in 10.9:
-  # http://notmuchmail.org/pipermail/notmuch/2013/016388.html
-  patch :DATA
 
   def install
     args = ["--prefix=#{prefix}"]
@@ -28,25 +24,3 @@ class Notmuch < Formula
     system "make", "V=1", "install"
   end
 end
-
-__END__
-diff --git a/Makefile.local b/Makefile.local
-index 72524eb..c85e09c 100644
---- a/Makefile.local
-+++ b/Makefile.local
-@@ -236,11 +236,11 @@ endif
- quiet ?= $($(shell echo $1 | sed -e s'/ .*//'))
- 
- %.o: %.cc $(global_deps)
--	@mkdir -p .deps/$(@D)
-+	@mkdir -p $(patsubst %/.,%,.deps/$(@D))
- 	$(call quiet,CXX $(CPPFLAGS) $(CXXFLAGS)) -c $(FINAL_CXXFLAGS) $< -o $@ -MD -MP -MF .deps/$*.d
- 
- %.o: %.c $(global_deps)
--	@mkdir -p .deps/$(@D)
-+	@mkdir -p $(patsubst %/.,%,.deps/$(@D))
- 	$(call quiet,CC $(CPPFLAGS) $(CFLAGS)) -c $(FINAL_CFLAGS) $< -o $@ -MD -MP -MF .deps/$*.d
- 
- .PHONY : clean
--- 
-1.8.4.2
