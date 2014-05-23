a8b6eea8569858b580944ccee3613b0c64de695b
Ingmar Vanhassel <ingmar@exherbo.org>
2014-05-23 12:09:49 +0200

    rtorrent: 0.9.4 no longer uses libsigc++

--- Library/Formula/rtorrent.rb
+++ Library/Formula/rtorrent.rb
@@ -9,30 +9,8 @@ class Rtorrent < Formula
   depends_on "libtorrent"
   depends_on "xmlrpc-c" => :optional
 
-  # rtorrent gets a private libsigc++ because libtorrent can only build
-  # under libstdc++, but libsigc++ is too widely used for us to force it
-  # to use libstdc++ globally.
-  # This should be removed once libtorrent is fixed to work under libstdc++.
-  # See https://github.com/mxcl/homebrew/issues/23483
-  resource "libsigcxx" do
-    url "http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.3/libsigc++-2.3.1.tar.xz"
-    sha256 "67d05852b31fdb267c9fdcecd40b046a11aa54d884435e99e3c60dd20cd60393"
-  end
-
   def install
     ENV.libstdcxx if ENV.compiler == :clang
-
-    resource("libsigcxx").stage do
-      system "./configure", "--prefix=#{libexec}/libsigcxx", "--disable-dependency-tracking"
-      system "make"
-      system "make check"
-      system "make install"
-    end
-
-    # Skip pkg-config, which was having trouble looking into rtorrent's libexec
-    ENV["sigc_CFLAGS"] = "-I#{libexec}/libsigcxx/include/sigc++-2.0 \
-      -I#{libexec}/libsigcxx/lib/sigc++-2.0/include"
-    ENV["sigc_LIBS"] = "-L#{libexec}/libsigcxx/lib -lsigc-2.0"
 
     args = ["--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"]
     args << "--with-xmlrpc-c" if build.with? "xmlrpc-c"
