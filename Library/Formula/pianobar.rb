9aded23a41937607a00c689b0ab63d9af73758f5
Adam Vandenberg <flangy@gmail.com>
2014-05-23 11:21:28 -0700

    pianobar: use HEAD block

--- Library/Formula/pianobar.rb
+++ Library/Formula/pianobar.rb
@@ -2,11 +2,14 @@ require "formula"
 
 class Pianobar < Formula
   homepage "https://github.com/PromyLOPh/pianobar/"
-  head "https://github.com/PromyLOPh/pianobar.git"
   url "https://github.com/PromyLOPh/pianobar/archive/2013.09.15.tar.gz"
   sha256 "4b18582eb794def5bf4e7d5de211d1f6c79295edac344928e09072aa9386796c"
   revision 2
 
+  head do
+    url "https://github.com/PromyLOPh/pianobar.git"
+    depends_on "ffmpeg"
+  end
 
   depends_on "pkg-config" => :build
   depends_on "libao"
@@ -15,7 +18,6 @@ class Pianobar < Formula
   depends_on "gnutls"
   depends_on "libgcrypt"
   depends_on "json-c"
-  depends_on "ffmpeg" if build.head?
 
   fails_with :llvm do
     build 2334
