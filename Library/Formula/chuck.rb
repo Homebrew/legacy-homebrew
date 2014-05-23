29bd840bea4feb0a283e09ded97739b3d05172e0
Adam Vandenberg <flangy@gmail.com>
2014-05-23 11:27:49 -0700

    chuck: style

--- Library/Formula/chuck.rb
+++ Library/Formula/chuck.rb
@@ -1,15 +1,13 @@
-require 'formula'
+require "formula"
 
 class Chuck < Formula
-  homepage 'http://chuck.cs.princeton.edu/'
-  url 'http://chuck.cs.princeton.edu/release/files/chuck-1.3.4.0.tgz'
-  sha1 'd32faae2cb60fc81d2716b477cf2d54bc548d9c6'
+  homepage "http://chuck.cs.princeton.edu/"
+  url "http://chuck.cs.princeton.edu/release/files/chuck-1.3.4.0.tgz"
+  sha1 "d32faae2cb60fc81d2716b477cf2d54bc548d9c6"
 
   def install
-    cd "src" do
-      system "make osx"
-      bin.install "chuck"
-    end
-    (share/'chuck').install "examples/"
+    system "make", "-C", "src", "osx"
+    bin.install "src/chuck"
+    (share/"chuck").install "examples"
   end
 end
