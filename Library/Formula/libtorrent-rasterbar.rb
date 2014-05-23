69bbe79f3839396649ec0ad0050a6f2eda6d90e3
Jack Nagel <jacknagel@gmail.com>
2014-05-22 21:20:22 -0500

    libtorrent-rasterbar needs boost-python

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -13,9 +13,14 @@ class LibtorrentRasterbar < Formula
   end
 
   depends_on 'pkg-config' => :build
-  depends_on 'boost'
   depends_on 'openssl' if MacOS.version <= :snow_leopard # Needs a newer version of OpenSSL on Snow Leopard
   depends_on :python => :optional
+
+  if build.with? "python"
+    depends_on "boost" => "with-python"
+  else
+    depends_on "boost"
+  end
 
   def install
     boost = Formula["boost"]
@@ -26,12 +31,9 @@ class LibtorrentRasterbar < Formula
              "--with-boost=#{boost.opt_prefix}" ]
 
     # Build python bindings requires forcing usage of the mt version of boost_python.
-    # Be aware that if using a brewed python, boost will have to be built from source
-    # to ensure that boost_python is linked against the brewed python runtime.
     if build.with? "python"
       args << "--enable-python-binding"
       args << "--with-boost-python=boost_python-mt"
-      args << "PYTHON_EXTRA_LDFLAGS= "
     end
 
     system "./configure", *args
