c439ad67e28fb71bf48f68c79973d67893c16c72
Jack Nagel <jacknagel@gmail.com>
2014-05-24 17:50:40 -0500

    theora: copy new config.guess from libtool

--- Library/Formula/theora.rb
+++ Library/Formula/theora.rb
@@ -1,20 +1,22 @@
-require 'formula'
+require "formula"
 
 class Theora < Formula
-  homepage 'http://www.theora.org/'
-  url 'http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2'
-  sha1 '8dcaa8e61cd86eb1244467c0b64b9ddac04ae262'
+  homepage "http://www.theora.org/"
+  url "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
+  sha1 "8dcaa8e61cd86eb1244467c0b64b9ddac04ae262"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libogg'
-  depends_on 'libvorbis'
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+  depends_on "libogg"
+  depends_on "libvorbis"
 
   def install
+    cp Dir["#{Formula["libtool"].opt_share}/libtool/config/config.*"], buildpath
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-oggtest",
                           "--disable-vorbistest",
                           "--disable-examples"
-    system "make install"
+    system "make", "install"
   end
 end
