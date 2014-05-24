97ea0113fd7a7a07aeceec0c6df7a4eeb1b43673
Xu Cheng <xucheng@me.com>
2014-05-24 15:07:21 +0800

    hub: modernize

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,21 +1,21 @@
-require 'formula'
+require "formula"
 
 class Hub < Formula
-  homepage 'http://hub.github.com/'
-  url 'https://github.com/github/hub/archive/v1.12.1.tar.gz'
-  sha1 '934033bcd17a710a56f33acf099900baf0fe88f3'
-  head 'https://github.com/github/hub.git'
+  homepage "http://hub.github.com/"
+  url "https://github.com/github/hub/archive/v1.12.1.tar.gz"
+  sha1 "934033bcd17a710a56f33acf099900baf0fe88f3"
+  head "https://github.com/github/hub.git"
 
-  option 'without-completions', 'Disable bash/zsh completions'
+  option "without-completions", "Disable bash/zsh completions"
 
   def install
-    ENV['GIT_DIR'] = cached_download/'.git' if build.head?
+    ENV["GIT_DIR"] = cached_download/".git" if build.head?
 
     rake "install", "prefix=#{prefix}"
 
-    if build.with? 'completions'
-      bash_completion.install 'etc/hub.bash_completion.sh'
-      zsh_completion.install 'etc/hub.zsh_completion' => '_hub'
+    if build.with? "completions"
+      bash_completion.install "etc/hub.bash_completion.sh"
+      zsh_completion.install "etc/hub.zsh_completion" => "_hub"
     end
   end
 
@@ -27,7 +27,7 @@ class Hub < Formula
 
   test do
     HOMEBREW_REPOSITORY.cd do
-      assert_equal 'bin/brew', `#{bin}/hub ls-files -- bin`.strip
+      assert_equal "bin/brew", `#{bin}/hub ls-files -- bin`.strip
     end
   end
 end
