2d68dae14b1a1651061abcc073c51672406814df
Adam Vandenberg <flangy@gmail.com>
2014-05-25 07:26:14 -0700

    libgcrypt: quotes

--- Library/Formula/libgcrypt.rb
+++ Library/Formula/libgcrypt.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Libgcrypt < Formula
-  homepage 'http://gnupg.org/'
-  url 'ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.6.1.tar.bz2'
-  sha1 'f03d9b63ac3b17a6972fc11150d136925b702f02'
+  homepage "http://gnupg.org/"
+  url "ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-1.6.1.tar.bz2"
+  sha1 "f03d9b63ac3b17a6972fc11150d136925b702f02"
 
   bottle do
     cellar :any
@@ -12,14 +12,14 @@ class Libgcrypt < Formula
     sha1 "f0c2d69e6552d69fa59342613831b96085ebd85e" => :lion
   end
 
-  depends_on 'libgpg-error'
+  depends_on "libgpg-error"
 
   option :universal
 
-  resource 'config.h.ed' do
-    url 'http://trac.macports.org/export/113198/trunk/dports/devel/libgcrypt/files/config.h.ed'
-    version '113198'
-    sha1 '136f636673b5c9d040f8a55f59b430b0f1c97d7a'
+  resource "config.h.ed" do
+    url "http://trac.macports.org/export/113198/trunk/dports/devel/libgcrypt/files/config.h.ed"
+    version "113198"
+    sha1 "136f636673b5c9d040f8a55f59b430b0f1c97d7a"
   end if build.universal?
 
   def install
@@ -31,13 +31,13 @@ class Libgcrypt < Formula
                           "--with-gpg-error-prefix=#{Formula["libgpg-error"].opt_prefix}"
 
     if build.universal?
-      buildpath.install resource('config.h.ed')
+      buildpath.install resource("config.h.ed")
       system "ed -s - config.h <config.h.ed"
     end
 
     # Parallel builds work, but only when run as separate steps
     system "make"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
   end
 end
