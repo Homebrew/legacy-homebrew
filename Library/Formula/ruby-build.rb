65ad1b537cbfa69a702e783b7a12a98555cd7079
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-05-26 16:34:00 +0700

    ruby-build 20140524
    Closes #29584.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,8 +3,8 @@ require "formula"
 class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  url "https://github.com/sstephenson/ruby-build/archive/v20140517.tar.gz"
-  sha1 "a26aa68f444beaac5ea2c07dcb49803e667bbfae"
+  url "https://github.com/sstephenson/ruby-build/archive/v20140524.tar.gz"
+  sha1 "a44e19bd6eb610d51d3b4d87961b2ea3a7126f3e"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
