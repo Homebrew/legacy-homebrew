2e3ccaae9c0e5c58343852913edc1913922333a0
Adam Vandenberg <flangy@gmail.com>
2014-05-26 09:01:25 -0700

    suomi-malaga-voikko: quotes

--- Library/Formula/suomi-malaga-voikko.rb
+++ Library/Formula/suomi-malaga-voikko.rb
@@ -1,15 +1,14 @@
-require 'formula'
+require "formula"
 
 class SuomiMalagaVoikko < Formula
-  homepage 'http://voikko.puimula.org/'
-  url 'http://www.puimula.org/voikko-sources/suomi-malaga/suomi-malaga-1.16.tar.gz'
-  sha1 '5d9310f8150b8d2b559d5e7c27829e2fd6125dfb'
+  homepage "http://voikko.puimula.org/"
+  url "http://www.puimula.org/voikko-sources/suomi-malaga/suomi-malaga-1.16.tar.gz"
+  sha1 "5d9310f8150b8d2b559d5e7c27829e2fd6125dfb"
 
-  depends_on 'malaga'
+  depends_on "malaga"
 
   def install
     system "make", "voikko"
     system "make", "voikko-install", "DESTDIR=#{lib}/voikko"
   end
-
 end
