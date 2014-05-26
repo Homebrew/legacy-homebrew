45e35ceba205bfdf95f5f55354b444447dec0260
mbcoguno <mbcoguno@users.noreply.github.com>
2014-05-27 00:43:04 +0800

    vim: quotes

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class Vim < Formula
-  homepage 'http://www.vim.org/'
-  head 'https://vim.googlecode.com/hg/'
+  homepage "http://www.vim.org/"
+  head "https://vim.googlecode.com/hg/"
   # This package tracks debian-unstable: http://packages.debian.org/unstable/vim
-  url 'http://ftp.debian.org/debian/pool/main/v/vim/vim_7.4.273.orig.tar.gz'
-  sha1 '87da49006fbea912b5bf5f99cc91030581b43269'
+  url "http://ftp.debian.org/debian/pool/main/v/vim/vim_7.4.273.orig.tar.gz"
+  sha1 "87da49006fbea912b5bf5f99cc91030581b43269"
 
   # We only have special support for finding depends_on :python, but not yet for
   # :ruby, :perl etc., so we use the standard environment that leaves the
@@ -28,15 +28,15 @@ class Vim < Formula
 
   depends_on :python => :recommended
   depends_on :python3 => :optional
-  depends_on 'lua' => :optional
-  depends_on 'luajit' => :optional
-  depends_on 'gtk+' if build.with? 'client-server'
+  depends_on "lua" => :optional
+  depends_on "luajit" => :optional
+  depends_on "gtk+" if build.with? "client-server"
 
-  conflicts_with 'ex-vi',
-    :because => 'vim and ex-vi both install bin/ex and bin/view'
+  conflicts_with "ex-vi",
+    :because => "vim and ex-vi both install bin/ex and bin/view"
 
   def install
-    ENV['LUA_PREFIX'] = HOMEBREW_PREFIX if build.with?('lua')
+    ENV["LUA_PREFIX"] = HOMEBREW_PREFIX if build.with?("lua")
 
     # vim doesn't require any Python package, unset PYTHONPATH.
     ENV.delete('PYTHONPATH')
@@ -54,14 +54,14 @@ class Vim < Formula
 
     opts << "--disable-nls" if build.include? "disable-nls"
 
-    if build.with? 'client-server'
-      opts << '--enable-gui=gtk2'
+    if build.with? "client-server"
+      opts << "--enable-gui=gtk2"
     else
       opts << "--enable-gui=no"
       opts << "--without-x"
     end
 
-    opts << "--with-luajit" if build.with? 'luajit'
+    opts << "--with-luajit" if build.with? "luajit"
 
     # XXX: Please do not submit a pull request that hardcodes the path
     # to ruby: vim can be compiled against 1.8.x or 1.9.3-p385 and up.
