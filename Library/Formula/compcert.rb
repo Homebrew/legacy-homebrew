3871679a10c4c6f92203a03ff6227a9db19bd07c
Jack Nagel <jacknagel@gmail.com>
2014-05-26 14:41:23 -0500

    compcert 2.3pl2

--- Library/Formula/compcert.rb
+++ Library/Formula/compcert.rb
@@ -2,12 +2,13 @@ require "formula"
 
 class Compcert < Formula
   homepage "http://compcert.inria.fr"
-  url "http://compcert.inria.fr/release/compcert-2.1.tgz"
-  sha1 "1ec21e43c3ac778827522d385733e36d24c5f8d4"
+  url "http://compcert.inria.fr/release/compcert-2.3pl2.tgz"
+  sha1 "8d7b9d5b18bd52487686db2acd534c978b1e1421"
+  version "2.3pl2"
 
-  depends_on "coq" => :build
   depends_on "objective-caml" => :build
-  depends_on "camlp5" => :build
+  depends_on "coq" => :build
+  depends_on "menhir" => :build
 
   def install
     ENV.permit_arch_flags
@@ -16,7 +17,7 @@ class Compcert < Formula
     # creates problems since XCode's gcc does not support CFI,
     # but superenv will trick it into using clang which does. This
     # causes problems with the compcert compiler at runtime.
-    inreplace 'configure', '${toolprefix}gcc', "${toolprefix}#{ENV.cc}"
+    inreplace "configure", "${toolprefix}gcc", "${toolprefix}#{ENV.cc}"
 
     system "./configure", "-prefix", prefix, "ia32-macosx"
     system "make", "all"
