e214231df8370660827ca7fef8133ebb889a3760
industrie13 <industrie13@gmx.net>
2014-05-26 23:29:44 +0200

    option went before depends_on use of double-quotes binaries install in prefix

--- Library/Formula/eric5.rb
+++ Library/Formula/eric5.rb
@@ -5,20 +5,21 @@ class Eric5 < Formula
   url "https://downloads.sourceforge.net/project/eric-ide/eric5/stable/5.4.4/eric5-5.4.4.tar.gz"
   sha1 "89f942004ecb1b94bc750bf2e52798275464aad5"
 
+  option "with-i18n-de", "Enable German language support"
+
   depends_on "python3"
   depends_on "qscintilla2" => "with-python3"
   depends_on "sip" => "with-python3"
   depends_on "pyqt" => "with-python3"
   depends_on "enchant" => :optional
-  option 'with-i18n-de' , "Enable German language support"
 
-  resource 'i18n-de' do
+  resource "i18n-de" do
     url 'https://downloads.sourceforge.net/project/eric-ide/eric5/stable/5.4.4/eric5-i18n-de-5.4.4.tar.gz'
     sha1 '6c037322549f49f1ec5a311063fabe4520485289'
   end
 
   def install
-    system "python3", "install.py", "-n #{prefix}"
+    system "python3", "install.py", "-n #{prefix}", "-b #{prefix}/bin"
 
     if build.with? 'i18n-de'
       resource('i18n-de').stage { system "python3", "install-i18n.py", "install", "--prefix=#{prefix}" }
