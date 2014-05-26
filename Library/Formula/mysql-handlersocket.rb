2e1eb1fba1bab25df5240bcb6428ee638367ff7a
Konstantin Kuklin <konstantin.kuklin@gmail.com>
2014-05-26 13:53:59 +0400

    refactored with Jack advices

--- Library/Formula/mysql-handlersocket.rb
+++ Library/Formula/mysql-handlersocket.rb
@@ -1,5 +1,4 @@
-require "formula"
-require "resource"
+require 'formula'
 
 class MysqlHandlersocket < Formula
   homepage 'https://github.com/ahiguti/HandlerSocket-Plugin-for-MySQL'
@@ -7,31 +6,28 @@ class MysqlHandlersocket < Formula
   sha1 '52c145c35ca4cf36f49add6a2f27d92e28e92d38'
 
   depends_on 'mysql'
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
   depends_on 'libtool' => :build
-  depends_on 'automake'
+
 
   def install
-    hs_dir = Dir.pwd
-    mysql_installed_path = Formula["mysql"].prefix
+    mysql = Formula["mysql"]
+    mysql.brew do
+      cd buildpath do
+        system "./autogen.sh"
+        system './configure',
+               "--with-mysql-source=#{mysql.buildpath}",
+               "--with-mysql-bindir=#{mysql.opt_bin}",
+               "--with-mysql-plugindir=#{mysql.opt_lib}/plugin"
 
-    Formula["mysql"].brew do
-      mysql_source = Dir.pwd
-
-      Dir.chdir(hs_dir)
-
-      system "./autogen.sh"
-      system './configure',
-             "--with-mysql-source=#{mysql_source}",
-             "--with-mysql-bindir=#{mysql_installed_path}/bin",
-             "--with-mysql-plugindir=#{mysql_installed_path}/lib/plugin"
-
-      system 'make'
-      system 'make install'
+        system 'make'
+        system 'make install'
+      end
     end
+  end
 
-    puts "!!!!!"
-    puts "Do not forget execute this command to install plugin"
-    puts "mysql -u root -e \"install plugin handlersocket soname 'handlersocket.so'\""
-    puts "!!!!!"
+  def caveats
+    "Do not forget execute this command to install: \nplugin mysql -u root -e \"install plugin handlersocket soname 'handlersocket.so'\""
   end
 end
