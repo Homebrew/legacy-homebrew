b0eea245f4e40bc669f7dce5e439b1fc369870ce
Ralph Giles <giles@mozilla.com>
2014-05-27 13:30:43 -0700

    libogg 1.3.2

--- Library/Formula/libogg.rb
+++ Library/Formula/libogg.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libogg < Formula
-  homepage 'http://www.xiph.org/ogg/'
-  url 'http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz'
-  sha1 '270685c2a3d9dc6c98372627af99868aa4b4db53'
+  homepage 'https://www.xiph.org/ogg/'
+  url 'http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz'
+  sha1 'df7f3977bbeda67306bc2a427257dd7375319d7d'
 
   bottle do
     cellar :any
@@ -13,7 +13,7 @@ class Libogg < Formula
   end
 
   head do
-    url 'http://svn.xiph.org/trunk/ogg'
+    url 'https://svn.xiph.org/trunk/ogg'
 
     depends_on :autoconf
     depends_on :automake
