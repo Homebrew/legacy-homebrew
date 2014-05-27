190c90a7543d58942b336a3807abadbecc238499
Ben Hagen <c.ben.hagen@gmail.com>
2014-05-27 18:41:33 +0200

    fix build for Lion

--- Library/Formula/seexpr.rb
+++ Library/Formula/seexpr.rb
@@ -9,11 +9,9 @@ class Seexpr < Formula
   depends_on "libpng"
   depends_on "doxygen"
 
-  patch do
-    # fix for macosx
-    url "https://github.com/wdas/SeExpr/pull/17.diff"
-    sha1 "78f209dad9f5f6bfb2751ff6b77c8a9d6ddaf8aa"
-  end
+  # fix for macosx
+  # already present in HEAD so it can be removed after version 1.0.1
+  patch :DATA
 
   def install
     mkdir "build" do
@@ -27,4 +25,34 @@ class Seexpr < Formula
     system bin/"asciigraph"
   end
 end
+
+__END__
+diff --git a/src/SeExpr/SeExprFunc.cpp b/src/SeExpr/SeExprFunc.cpp
+index feb6b45..8269b39 100644
+--- a/src/SeExpr/SeExprFunc.cpp
++++ b/src/SeExpr/SeExprFunc.cpp
+@@ -208,7 +208,7 @@ SeExprFunc::getDocString(const char* functionName)
+ 
+ #ifndef SEEXPR_WIN32
+ 
+-#ifdef __APPLE__
++#if defined(__APPLE__) && !defined(__MAC_10_9)
+ static int MatchPluginName(struct dirent* dir)
+ #else
+ static int MatchPluginName(const struct dirent* dir)
+diff --git a/src/SeExpr/SePlatform.h b/src/SeExpr/SePlatform.h
+index 32a6b96..82b0f44 100644
+--- a/src/SeExpr/SePlatform.h
++++ b/src/SeExpr/SePlatform.h
+@@ -40,6 +40,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+     @brief Platform-specific classes, functions, and includes.
+ */
+ 
++#ifdef __APPLE__
++#    include <Availability.h>
++#endif
++
+ // platform-specific includes
+ #if defined(_WIN32) || defined(_WINDOWS) || defined(_MSC_VER)
+ #    ifndef WINDOWS
 
