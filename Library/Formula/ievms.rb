f139713560b89a2be60cdd78c11481d20d304e6f
David Baumgold <david@davidbaumgold.com>
2014-05-28 15:27:17 -0400

    new formula: ievms

---
+++ Library/Formula/ievms.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Ievms < Formula
+  homepage "http://xdissent.github.io/ievms/"
+  url "https://github.com/xdissent/ievms/archive/v0.2.0.tar.gz"
+  sha1 "5cf742663ccce06579968a17bb559606ff4479e2"
+
+  def install
+    bin.install "ievms.sh" => "ievms"
+  end
+
+  test do
+    system "#{bin}/ievms"
+  end
+end
