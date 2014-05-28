6b58e5c9864714c02f94864b95df40fdf1535acc
Jack Nagel <jacknagel@gmail.com>
2014-05-28 10:13:43 -0500

    mpfr 3.1.2-p8

--- Library/Formula/mpfr.rb
+++ Library/Formula/mpfr.rb
@@ -1,11 +1,12 @@
-require 'formula'
+require "formula"
 
 class Mpfr < Formula
-  homepage 'http://www.mpfr.org/'
+  homepage "http://www.mpfr.org/"
   # Upstream is down a lot, so use the GNU mirror + Gist for patches
-  url 'http://ftpmirror.gnu.org/mpfr/mpfr-3.1.2.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.bz2'
-  sha1 '46d5a11a59a4e31f74f73dd70c5d57a59de2d0b4'
+  url "http://ftpmirror.gnu.org/mpfr/mpfr-3.1.2.tar.bz2"
+  mirror "http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.bz2"
+  sha1 "46d5a11a59a4e31f74f73dd70c5d57a59de2d0b4"
+  version "3.1.2-p8"
 
   bottle do
     cellar :any
@@ -15,9 +16,15 @@ class Mpfr < Formula
     sha1 '62c126d1d949cb4d545f44d9c45fe4b0bf276fd4' => :lion
   end
 
-  depends_on 'gmp'
+  # http://www.mpfr.org/mpfr-current/allpatches
+  patch do
+    url "https://gist.githubusercontent.com/jacknagel/7f276cd60149a1ffc9a7/raw/0f2c24423ceda0dae996e2333f395c7115db33ec/mpfr-3.1.2-8.diff"
+    sha1 "047c96dcfb86f010972dedae088a3e67eaaecb8a"
+  end
 
-  option '32-bit'
+  depends_on "gmp"
+
+  option "32-bit"
 
   fails_with :clang do
     build 421
