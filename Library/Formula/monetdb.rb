00a85f0b60626ddb33d2956ef62d9e85f77d15b4
Dimitar Nedev <dnedev@users.noreply.github.com>
2014-05-28 09:29:23 +0200

    Use ssl3 when downloading source MonetDB zip
    Avoid SSL issues under OS X 10.7 and 10.8

--- Library/Formula/monetdb.rb
+++ Library/Formula/monetdb.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Monetdb < Formula
   homepage 'https://www.monetdb.org/'
-  url 'https://dev.monetdb.org/downloads/sources/Jan2014-SP2/MonetDB-11.17.17.zip'
+  url 'https://dev.monetdb.org/downloads/sources/Jan2014-SP2/MonetDB-11.17.17.zip', :using => :ssl3
   sha1 '1f1f668e96a6c38463f6ae7a3821fd05c86a04b3'
 
   head 'http://dev.monetdb.org/hg/MonetDB', :using => :hg
