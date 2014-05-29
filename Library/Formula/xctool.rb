e8c51e5115a8d942c25f794dc8880365b90a0cc5
Christian Legnitto <christianl@fb.com>
2014-05-28 17:27:12 -0700

    Update xctool to 0.1.16
    Not sure if bottles are automatically added later

--- Library/Formula/xctool.rb
+++ Library/Formula/xctool.rb
@@ -7,13 +7,12 @@ end
 
 class Xctool < Formula
   homepage 'https://github.com/facebook/xctool'
-  url 'https://github.com/facebook/xctool/archive/v0.1.15.tar.gz'
-  sha1 'bfa6e02ae0fb90294fe653c55e1a7151877319be'
+  url 'https://github.com/facebook/xctool/archive/v0.1.16.tar.gz'
+  sha1 'c4161403b5f742467b37fd41887fde3d99a5989d'
   head 'https://github.com/facebook/xctool.git'
 
   bottle do
-    sha1 "d10ed0064f04d4f723d09c09868a44418b6ac272" => :mavericks
-    sha1 "0947c080af4b20bcb950b18707bedca3e9a580ae" => :mountain_lion
+    sha1 "a4a9e31f5356ff9a3923da437c2fb5bf4e8a9495" => :mavericks
   end
 
   depends_on :xcode
