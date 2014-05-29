b791d6f5c10d8cb9f173b554fa47c71a7c33172a
Jack Nagel <jacknagel@gmail.com>
2014-05-28 21:15:31 -0500

    readline 6.3.6

--- Library/Formula/readline.rb
+++ Library/Formula/readline.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class Readline < Formula
-  homepage 'http://tiswww.case.edu/php/chet/readline/rltop.html'
-  url 'http://ftpmirror.gnu.org/readline/readline-6.3.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz'
-  sha256 '56ba6071b9462f980c5a72ab0023893b65ba6debb4eeb475d7a563dc65cafd43'
-  version '6.3.5'
+  homepage "http://tiswww.case.edu/php/chet/readline/rltop.html"
+  url "http://ftpmirror.gnu.org/readline/readline-6.3.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz"
+  sha256 "56ba6071b9462f980c5a72ab0023893b65ba6debb4eeb475d7a563dc65cafd43"
+  version "6.3.6"
 
   bottle do
     cellar :any
@@ -27,8 +27,8 @@ EOS
   # Upstream patches can be found in:
   # http://git.savannah.gnu.org/cgit/readline.git
   patch do
-    url "https://gist.githubusercontent.com/jacknagel/8df5735ae9273bf5ebb2/raw/827805aa2927211e7c3d9bb871e75843da686671/readline.diff"
-    sha1 "2d55658a2f01fa14a029b16fea29d20ce7d03b78"
+    url "https://gist.githubusercontent.com/jacknagel/d886531fb6623b60b2af/raw/d6aec221e7369ea152e64c8af83f6b048433dd82/readline-6.3.6.diff"
+    sha1 "774e4d3fe8c22dc715b430343d68beecf6f4e673"
   end
 
   def install
