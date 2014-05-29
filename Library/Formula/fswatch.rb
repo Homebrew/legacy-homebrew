c2f1427e2f6426189e9a793f06772c1ab7b817bf
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-05-30 01:47:53 +0200

    Update fswatch formula for fswatch v. 1.3.3 (fix wrong file).

--- Library/Formula/fswatch.rb
+++ Library/Formula/fswatch.rb
@@ -3,7 +3,7 @@ require "formula"
 class Fswatch < Formula
   homepage "https://github.com/alandipert/fswatch"
   url "https://github.com/alandipert/fswatch/archive/1.3.3.tar.gz"
-  sha1 "1f982d048bdaf759db269e67ddeeb133e756658d"
+  sha1 "b14632eca9618de58f5eadad82342672219902bf"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
