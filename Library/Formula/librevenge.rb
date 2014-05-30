2d3e87bec0620d00ab149873a36444e66d7bef0b
Yue Liu <yue.liu@mail.com>
2014-05-29 23:56:15 -0700

    added formula librevenge

---
+++ Library/Formula/librevenge.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Librevenge < Formula
+  homepage 'http://sourceforge.net/projects/libwpd/files/librevenge/'
+  url 'http://dev-www.libreoffice.org/src/librevenge-0.0.0.tar.bz2'
+  sha1 '25159b8d9cd12b50fbe5b3cbe678bd5620a489a8'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'boost' => :build
+  depends_on 'libtool' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+
+  def install
+    system "./configure", "--without-docs",
+                          "--disable-dependency-tracking",
+                          "--enable-static=no",
+                          "--disable-werror",
+                          "--disable-tests",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
