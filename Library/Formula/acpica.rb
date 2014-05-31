0e53e9c4e06052a50a9393ba5576e25ea432e52f
Adam Vandenberg <flangy@gmail.com>
2014-05-31 15:36:04 -0700

    acpica: update style

--- Library/Formula/acpica.rb
+++ Library/Formula/acpica.rb
@@ -1,11 +1,10 @@
-require 'formula'
+require "formula"
 
 class Acpica < Formula
-  homepage 'https://www.acpica.org/'
-  url 'https://acpica.org/sites/acpica/files/acpica-unix2-20140424.tar.gz'
-  sha1 '6ed59f9099b73dfe901a811c4d5bf333da6fb53f'
-
-  head 'https://github.com/acpica/acpica.git'
+  homepage "https://www.acpica.org/"
+  head "https://github.com/acpica/acpica.git"
+  url "https://acpica.org/sites/acpica/files/acpica-unix2-20140424.tar.gz"
+  sha1 "6ed59f9099b73dfe901a811c4d5bf333da6fb53f"
 
   def install
     ENV.deparallelize
