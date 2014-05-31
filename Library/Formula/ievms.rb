c5aed897c027457e5dd921fc70e840c6945de324
David Baumgold <david@davidbaumgold.com>
2014-05-30 21:40:32 -0400

    ievms: cleanup

--- Library/Formula/ievms.rb
+++ Library/Formula/ievms.rb
@@ -4,9 +4,8 @@ class Ievms < Formula
   homepage "http://xdissent.github.io/ievms/"
   url "https://github.com/xdissent/ievms/archive/v0.2.0.tar.gz"
   sha1 "5cf742663ccce06579968a17bb559606ff4479e2"
+
   depends_on "unar"
-  # also depends on VirtualBox: can be installed with brew-cask:
-  # `brew cask install virtualbox`
 
   def install
     bin.install "ievms.sh" => "ievms"
