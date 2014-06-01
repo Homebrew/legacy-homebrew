aa9f1dcd651e63bb4994b103d1581994d48ab62e
Paul DiPietro <paul.dipietro@me.com>
2014-05-31 22:37:38 -0400

    Remove devel block
    Removed devel block per request due to being older than stable.

--- Library/Formula/sbt.rb
+++ Library/Formula/sbt.rb
@@ -6,12 +6,6 @@ class Sbt < Formula
   version '0.13.5'
   sha1 'f6308bd94bebdd37eb5e2fda732694ce0f34be74'
 
-  devel do
-    url 'http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.5-RC5/sbt-launch.jar'
-    version '0.13.5-RC5'
-    sha1 '490ce7491b2af13913ef8120e421bbe5b7ff63bd'
-  end
-
   def install
     (bin+'sbt').write <<-EOS.undent
       #!/bin/sh
