b83f125c5d470a99a8265c9f4f28f663d060627d
Jack Nagel <jacknagel@gmail.com>
2014-06-01 16:36:31 -0500

    orpie: remove unnecessary configure switch

--- Library/Formula/orpie.rb
+++ Library/Formula/orpie.rb
@@ -1,17 +1,16 @@
-require 'formula'
+require "formula"
 
 class Orpie < Formula
-  homepage 'http://pessimization.com/software/orpie/'
+  homepage "http://pessimization.com/software/orpie/"
   url "http://pessimization.com/software/orpie/orpie-1.5.2.tar.gz"
   sha1 "9786df20fb272fd36f87868bed04cab504602282"
 
-  depends_on 'gsl'
-  depends_on 'objective-caml'
+  depends_on "gsl"
+  depends_on "objective-caml"
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}",
-                          "--mandir=#{man}"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
