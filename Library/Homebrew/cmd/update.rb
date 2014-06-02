d4585342d4f4f99cffa378949cc27bf55d3b7e90
Shaun Jackman <sjackman@gmail.com>
2014-06-02 14:43:26 +0200

    Linuxbrew: The git repository is Homebrew/linuxbrew

--- Library/Homebrew/cmd/update.rb
+++ Library/Homebrew/cmd/update.rb
@@ -78,9 +78,10 @@ module Homebrew extend self
 
   def git_init_if_necessary
     if Dir['.git/*'].empty?
+      repository = if OS.linux? then "linuxbrew" else "homebrew" end
       safe_system "git init"
       safe_system "git config core.autocrlf false"
-      safe_system "git remote add origin https://github.com/Homebrew/homebrew.git"
+      safe_system "git remote add origin https://github.com/Homebrew/#{repository}.git"
       safe_system "git fetch origin"
       safe_system "git reset --hard origin/master"
     end
