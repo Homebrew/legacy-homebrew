2efecaacc553a2331bea3822667bdd1e8ff44d34
David Christenson <david@the-david.com>
2014-06-02 03:08:52 -0600

    Add plist

--- Library/Formula/ejabberd.rb
+++ Library/Formula/ejabberd.rb
@@ -61,4 +61,26 @@ class Ejabberd < Formula
     after 'localhost'.
     EOS
   end
+
+  plist_options :manual => "ejabberd"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_sbin}/ejabberdctl</string>
+        <string>start</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
 end
