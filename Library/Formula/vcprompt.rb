25a7d84c1de3dbbbe1b7a3f38a51c621d36f476c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-02 15:20:15 +0100

    vcprompt: add 1.2.1 bottle.
    Closes #29776.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/vcprompt.rb
+++ Library/Formula/vcprompt.rb
@@ -5,6 +5,13 @@ class Vcprompt < Formula
   url 'https://bitbucket.org/gward/vcprompt/downloads/vcprompt-1.2.1.tar.gz'
   sha1 'fb623e6183b8e5ccbbe5cf7d135a04e727c9b402'
 
+  bottle do
+    cellar :any
+    sha1 "609b05ec156a7b5b154150800dfefd1adcbef0df" => :mavericks
+    sha1 "b20758412be3d8abc8d99a055f34da5c94861280" => :mountain_lion
+    sha1 "f7618134adfd9bd57011eabd66278f8c8918fca0" => :lion
+  end
+
   head do
     url 'hg://https://bitbucket.org/gward/vcprompt'
     depends_on :autoconf
