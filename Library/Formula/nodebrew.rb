1d9ddc48748e910789edbb30660c44fbb37a7e54
oinuma-kazuhiro <oinuma_kazuhiro@cyberagent.co.jp>
2014-06-02 10:53:12 +0900

    New formula "nodebrew"

---
+++ Library/Formula/nodebrew.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class Nodebrew < Formula
+  homepage "https://github.com/hokaccha/nodebrew"
+  url "https://github.com/hokaccha/nodebrew/archive/v0.7.4.tar.gz"
+  sha1 "b60f9048837a15eef2420e9ffbced6d57b753534"
+  head "https://github.com/hokaccha/nodebrew.git"
+
+  def install
+    bin.install "nodebrew" => "nodebrew"
+    system "#{bin}/nodebrew setup_dirs"
+    bash_completion.install "completions/bash/nodebrew-completion" => "nodebrew"
+    zsh_completion.install "completions/zsh/_nodebrew" => "_nodebrew"
+    # root = ENV['NODEBREW_ROOT'] || "#{ENV['HOME']}/.nodebrew"
+    # %w[ default/bin node src ].each do |dir|
+    #   mkdir_p "#{root}/#{dir}"
+    # end
+    # ln_sf "#{root}/default", "#{root}/current"
+  end
+
+  def caveats; <<-EOS.undent
+    Add path:
+      export PATH=$HOME/.nodebrew/current/bin:$PATH
+
+    To use Homebrew's directories rather than ~/.nodebrew add to your profile:
+      export NODEBREW_ROOT=#{var}/nodebrew
+    EOS
+  end
+
+  test do
+    system "#{bin}/nodebrew", 'help'
+  end
+
+end
