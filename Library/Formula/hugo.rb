b6f0e2792f08ae8d22c1d52aca9dc1a4fca019eb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-02 23:01:26 +0100

    hugo: add 0.11 bottle.
    Closes #29765.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hugo.rb
+++ Library/Formula/hugo.rb
@@ -6,6 +6,12 @@ class Hugo < Formula
   url "https://github.com/spf13/hugo.git", :tag => "v0.11"
   sha1 "eb036b11d915bf78d0d679ee14ec065458977e2e"
 
+  bottle do
+    sha1 "71713846fe6611c6059cbbb811c1bad69fe52a8e" => :mavericks
+    sha1 "f5a265c90d90a8d7c072dc62b7a4b51fe7685a6f" => :mountain_lion
+    sha1 "180267e5b98d62605531a3e9ce7e54088f855e08" => :lion
+  end
+
   depends_on "go" => :build
   depends_on "bazaar" => :build
   depends_on :hg => :build
