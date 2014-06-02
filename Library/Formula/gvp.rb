2afa40c1ecc0bbf07242d9e4493f49d1ab46757d
PoTe <pote@tardis.com.uy>
2014-06-02 12:03:43 -0300

    gvp 0.0.4
    Closes #29778.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gvp.rb
+++ Library/Formula/gvp.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Gvp < Formula
   homepage "https://github.com/pote/gvp"
-  url "https://github.com/pote/gvp/archive/v0.0.3.tar.gz"
-  sha1 "46826b8f3b83ba52d629549a5d486525cf633d6e"
+  url "https://github.com/pote/gvp/archive/0.0.4.tar.gz"
+  sha1 "39676c4dd1df4d099cf938122733fb5e2e24c0a0"
 
   bottle do
     cellar :any
@@ -21,7 +21,7 @@ class Gvp < Formula
     assert Kernel.system("#{bin}/gvp init"), "`gvp init` exited with a non-zero status"
     assert File.directory?(".godeps"), "`gvp init` did not create the .godeps directory"
     assert_equal `#{bin}/gvp in 'echo $GOPATH' | grep -v '>> Local GOPATH set.'`.chomp, "#{testpath}/.godeps:#{testpath}", "`gvp in` did not change the GOPATH"
-    assert_equal `#{bin}/gvp in 'echo $GOBIN' | grep -v '>> Local GOPATH set.'`.chomp, "#{testpath}/.godeps/bin:#{ENV["GOBIN"]}", "`gvp in` did not change the GOBIN"
+    assert_equal `#{bin}/gvp in 'echo $GOBIN' | grep -v '>> Local GOPATH set.'`.chomp, "#{testpath}/.godeps/bin", "`gvp in` did not change the GOBIN"
     assert_equal `#{bin}/gvp in 'echo $PATH' | grep -v '>> Local GOPATH set.'`.chomp, "#{testpath}/.godeps/bin:#{ENV["PATH"]}", "`gvp in` did not change the PATH"
   end
 end
