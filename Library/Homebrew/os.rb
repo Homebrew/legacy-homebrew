c047dacee953d1d0354d9c626c471d03821c8696
Shaun Jackman <sjackman@gmail.com>
2014-06-03 16:59:50 +0200

    Linuxbrew: Use xdg-open

--- Library/Homebrew/os.rb
+++ Library/Homebrew/os.rb
@@ -6,4 +6,13 @@ module OS
   def self.linux?
     /linux/i === RUBY_PLATFORM
   end
+
+  # Utilities
+  if OS.mac?
+    PATH_OPEN = "/usr/bin/open"
+  elsif OS.linux?
+    PATH_OPEN = "xdg-open"
+  else
+    raise "Unknown operating system"
+  end
 end
