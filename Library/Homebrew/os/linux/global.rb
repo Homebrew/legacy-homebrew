a09c8f768b4233efdd00cdc059f214a1f7101aa9
Shaun Jackman <sjackman@gmail.com>
2014-06-03 15:11:22 +0200

    Use /var/cache and ~/.cache on Linux

---
+++ Library/Homebrew/os/linux/global.rb
@@ -0,0 +1,3 @@
+# Directories
+HOME_CACHE = Pathname.new("~/.cache/Homebrew").expand_path
+ROOT_CACHE = Pathname.new("/var/cache/Homebrew")
---
+++ Library/Homebrew/os/mac/global.rb
@@ -0,0 +1,3 @@
+# Directories
+HOME_CACHE = Pathname.new("~/Library/Caches/Homebrew").expand_path
+ROOT_CACHE = Pathname.new("/Library/Caches/Homebrew")
