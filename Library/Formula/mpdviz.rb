cac7d84e187a297d68f4405d934822e8ec53046d
Misty De Meo <mistydemeo@gmail.com>
2014-06-02 23:33:19 -0700

    mpdviz: model deps as resources
    Closes #29820.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/mpdviz.rb
+++ Library/Formula/mpdviz.rb
@@ -1,9 +1,25 @@
 require "formula"
+require "language/go"
 
 class Mpdviz < Formula
   homepage "https://github.com/neeee/mpdviz"
   url "http://github.com/neeee/mpdviz/archive/0.4.3.tar.gz"
   sha1 "7923a818155c9fc413e483dcb1b3964ab00e5ded"
+
+  go_resource "github.com/neeee/go-fftw" do
+    url "https://github.com/neeee/go-fftw.git",
+      :revision => "37bfa0d3053b133f7067e9524611a7a963294124"
+  end
+
+  go_resource "github.com/neeee/pflag" do
+    url "https://github.com/neeee/pflag.git",
+      :revision => "3eff50031ed8a4d175401fe12d1c8a24415cbe9f"
+  end
+
+  go_resource "github.com/neeee/termbox-go" do
+    url "https://github.com/neeee/termbox-go.git",
+      :revision => "7d25635285663eedf1aaa26dda7e5b3213045772"
+  end
 
   depends_on "pkg-config" => :build
   depends_on "go" => :build
@@ -11,11 +27,9 @@ class Mpdviz < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    system "go", "get", "github.com/neeee/go-fftw"
-    system "go", "get", "github.com/neeee/pflag"
-    system "go", "get", "github.com/neeee/termbox-go"
+    Language::Go.stage_deps resources, buildpath/"src"
+
     system "go", "build", "-o", "mpdviz"
     bin.install "mpdviz"
   end
-
 end
