0f3f58bb529a56efc754552cb1b576885c7d2e0c
David Lowenfels <david@internautdesign.com>
2014-06-04 12:07:50 -0700

    snd: update to 14.7 - no patch required

--- Library/Formula/snd.rb
+++ Library/Formula/snd.rb
@@ -2,16 +2,14 @@ require "formula"
 
 class Snd < Formula
   homepage "https://ccrma.stanford.edu/software/snd/"
-  url "ftp://ccrma-ftp.stanford.edu/pub/Lisp/snd-14.6.tar.gz"
-  sha1 "da33ba868f74cff9042ddc42acbceffbed017747"
+  url "ftp://ccrma-ftp.stanford.edu/pub/Lisp/snd-14.7.tar.gz"
+  sha1 "14433e0baa87305f783a98fa2161374dfe3a915f"
 
   depends_on :x11
   depends_on "pkg-config" => :build
   depends_on "portaudio"
   depends_on "gtk+"
   depends_on "fftw"
-
-  patch :DATA
 
   def install
     system "ruby tools/make-config-pc.rb > ./ruby.pc"
@@ -29,26 +27,3 @@ class Snd < Formula
   end
 
 end
-
-__END__
-diff --git a/tools/make-config-pc.rb b/tools/make-config-pc.rb
-index 3705ef1..388633e 100755
---- a/tools/make-config-pc.rb
-+++ b/tools/make-config-pc.rb
-@@ -19,6 +19,7 @@ end
- dldflags = CONFIG["DLDFLAGS"] 
- librubyarg = CONFIG["LIBRUBYARG"] 
- libs = CONFIG["LIBS"] 
-+rubylibdir = CONFIG["libdir"]
- 
- print <<OUT 
- Name: Ruby 
-@@ -26,6 +27,6 @@ Description: Object Oriented Script Language
- Version: #{version} 
- URL: http://www.ruby-lang.org 
- Cflags: -I#{rubyhdrdir}/#{arch} -I#{rubyhdrdir} 
--Libs: #{dldflags} #{librubyarg} #{libs} 
-+Libs: -L#{rubylibdir} #{dldflags} #{librubyarg} #{libs} 
- Requires: 
- OUT
-\ No newline at end of file
