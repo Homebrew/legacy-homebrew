deb515fb79a5b23d757fe1ed0ed0fb58173a2c44
Adam Vandenberg <flangy@gmail.com>
2014-06-03 20:09:55 -0700

    unifdef: quotes

--- Library/Formula/unifdef.rb
+++ Library/Formula/unifdef.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Unifdef < Formula
-  homepage 'http://dotat.at/prog/unifdef/'
-  url 'http://dotat.at/prog/unifdef/unifdef-2.10.tar.gz'
-  sha1 '8bc4e4feb914ff4aa164b23230b51d3f526559ac'
-  head 'https://github.com/fanf2/unifdef.git'
+  homepage "http://dotat.at/prog/unifdef/"
+  head "https://github.com/fanf2/unifdef.git"
+  url "http://dotat.at/prog/unifdef/unifdef-2.10.tar.gz"
+  sha1 "8bc4e4feb914ff4aa164b23230b51d3f526559ac"
 
   keg_only :provided_by_osx,
     "The unifdef provided by Xcode cannot compile gevent."
