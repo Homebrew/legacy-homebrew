d0786dc3abfb998bd237d7a70ca0959eb1c54972
Adam Vandenberg <flangy@gmail.com>
2014-06-03 19:28:59 -0700

    byacc: quotes

--- Library/Formula/byacc.rb
+++ Library/Formula/byacc.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Byacc < Formula
-  homepage 'http://invisible-island.net/byacc/byacc.html'
-  url 'ftp://invisible-island.net/byacc/byacc-20140422.tgz'
-  sha1 'aa30cac1e7b3cf9d6d19d6b9653575d56564b213'
+  homepage "http://invisible-island.net/byacc/byacc.html"
+  url "ftp://invisible-island.net/byacc/byacc-20140422.tgz"
+  sha1 "aa30cac1e7b3cf9d6d19d6b9653575d56564b213"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
