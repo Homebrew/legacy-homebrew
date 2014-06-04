cbb02b206659f49acd7a820a8b23720fe2ec792f
Noah Manneschmidt <nmanneschmidt@curse.com>
2014-06-04 16:56:52 -0500

    bitlbee: add plist.

--- Library/Formula/bitlbee.rb
+++ Library/Formula/bitlbee.rb
@@ -48,4 +48,45 @@ class Bitlbee < Formula
     (var+"bitlbee/run").mkpath
     (var+"bitlbee/lib").mkpath
   end
+
+  plist_options :manual => "bitlbee -D"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>OnDemand</key>
+      <true/>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_sbin}/bitlbee</string>
+      </array>
+      <key>ServiceDescription</key>
+      <string>bitlbee irc-im proxy</string>
+      <key>Sockets</key>
+      <dict>
+        <key>Listener</key>
+        <dict>
+          <key>SockFamily</key>
+          <string>IPv4</string>
+          <key>SockProtocol</key>
+          <string>TCP</string>
+          <key>SockServiceName</key>
+          <string>6667</string>
+          <key>SockType</key>
+          <string>stream</string>
+        </dict>
+      </dict>
+      <key>inetdCompatibility</key>
+      <dict>
+        <key>Wait</key>
+        <false/>
+      </dict>
+    </dict>
+    </plist>
+    EOS
+  end
 end
