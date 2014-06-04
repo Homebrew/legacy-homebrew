fe64927596d54cead7d84ac3f54702d803145d78
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-04 19:28:59 +0100

    wry: update 1.8.2 bottle.
    Closes #29774.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/wry.rb
+++ Library/Formula/wry.rb
@@ -9,9 +9,9 @@ class Wry < Formula
 
   bottle do
     cellar :any
-    sha1 "c8994394b33c6458436cab42dddeb7a7b80b3239" => :mavericks
-    sha1 "40f23ed60b0a916ea8caa9ccd5c547897a2ee9d1" => :mountain_lion
-    sha1 "a0c072f531078c12b9160b0ab4deefffaed033ac" => :lion
+    sha1 "01ed3904c9e01e700643609efae8fc0d8714bcc2" => :mavericks
+    sha1 "436083b0ec2addb57a9f042a8fcf99cbfce19618" => :mountain_lion
+    sha1 "1f492a3b41120e67046b84ee9d1d1f25e3897a87" => :lion
   end
 
   depends_on :macos => :lion
