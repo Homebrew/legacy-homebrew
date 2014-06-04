cb26c129163cc3a8657e7127d1c6a28492655e27
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-04 10:24:01 +0100

    metaproxy: add 1.5.3 bottle.
    Closes #29856.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/metaproxy.rb
+++ Library/Formula/metaproxy.rb
@@ -5,6 +5,13 @@ class Metaproxy < Formula
   url "http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.5.3.tar.gz"
   sha1 "8f089512e8ef55cdc3e781f78a6ac335fc4ce334"
 
+  bottle do
+    cellar :any
+    sha1 "40302a5cfecab138701d80cb7080d0078df8e306" => :mavericks
+    sha1 "8ee2a3af239c97321db7f7859fdbf7eb9d76c78f" => :mountain_lion
+    sha1 "98740e4f1059e72d7698bd804ee1e1917f272616" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "yazpp"
   depends_on "boost"
