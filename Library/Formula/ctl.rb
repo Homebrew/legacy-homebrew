2c4c469357ccce28d631bc2fdf7003f53ba31f6e
Ben Hagen <c.ben.hagen@gmail.com>
2014-06-05 23:57:29 +0200

    ctl 1.5.2

--- Library/Formula/ctl.rb
+++ Library/Formula/ctl.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Ctl < Formula
   homepage "https://github.com/ampas/CTL"
-  url "https://github.com/ampas/CTL/archive/ctl-1.5.1.tar.gz"
-  sha1 "f0e611ffe8a5c36e5ef89cc7eab1ff4ab7f97875"
+  url "https://github.com/ampas/CTL/archive/ctl-1.5.2.tar.gz"
+  sha1 "2c4aff2d514eeadfac89c6f041d5ffa2e853a79d"
 
   depends_on "cmake" => :build
   depends_on "libtiff"
