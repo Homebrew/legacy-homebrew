79879bbd000b6a495f76b365ddde8f5e45ceef3a
Evert Pot <me@evertpot.com>
2014-06-05 03:10:23 -0400

    Comments explaining why we don't want --disable-debug.

--- Library/Formula/litmus.rb
+++ Library/Formula/litmus.rb
@@ -6,6 +6,13 @@ class Litmus < Formula
   sha1 '42ad603035d15798facb3be79b1c51376820cb19'
 
   def install
+    # Note that initially this formula also had the --disable-debug option
+    # passed to ./configure.
+    #
+    # This diabled a critical feature. Litmus is a debugging tool, and this
+    # caused all logs to be empty by default.
+    #
+    # See: https://github.com/Homebrew/homebrew/pull/29608
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
