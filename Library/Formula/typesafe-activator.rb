ac472d453f45717f86fcd3a4f3486c7672082399
Matt Harasymczuk <github.com@haras.pl>
2014-06-05 20:45:32 +0200

    Update Typesafe Activator
    1.2.1 -> 1.2.2

--- Library/Formula/typesafe-activator.rb
+++ Library/Formula/typesafe-activator.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class TypesafeActivator < Formula
   homepage 'http://typesafe.com/activator'
-  url 'http://downloads.typesafe.com/typesafe-activator/1.2.1/typesafe-activator-1.2.1.zip'
-  sha1 '736e4a4349dc89994d11654aeca3b8ed32c4916b'
+  url 'http://downloads.typesafe.com/typesafe-activator/1.2.2/typesafe-activator-1.2.2.zip'
+  sha1 '5c51cd5480d0575640032bcd44f58a7b9dbaa929'
 
   def install
     rm Dir["*.bat"] # Remove Windows .bat files
