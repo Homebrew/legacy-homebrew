963ebb89162ff2f24c4e097cb06e02b0ef8cc6ce
Larry Gilbert <larry.gilbert@sleepygiant.com>
2014-06-05 15:01:04 -0700

    tsung: patch for https://support.process-one.net/browse/TSUN-299

--- Library/Formula/tsung.rb
+++ Library/Formula/tsung.rb
@@ -8,6 +8,9 @@ class Tsung < Formula
   depends_on 'erlang'
   depends_on 'gnuplot'
 
+  # Patch for https://support.process-one.net/browse/TSUN-299
+  patch :DATA
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
@@ -15,3 +18,28 @@ class Tsung < Formula
     system "make install"
   end
 end
+__END__
+diff --git a/src/tsung/ts_jabber.erl b/src/tsung/ts_jabber.erl
+index 45b0bf2..5500418 100644
+--- a/src/tsung/ts_jabber.erl
++++ b/src/tsung/ts_jabber.erl
+@@ -181,7 +181,6 @@ presence_bidi(RcvdXml, State)->
+     bidi_resp(subscribed,RcvdXml,SubMatches,State).
+ 
+ starttls_bidi(_RcvdXml, #state_rcv{socket= Socket}=State)->
+-    ssl:start(),
+     {ok, SSL} = ts_ssl:connect(Socket, []),
+     ?LOGF("Upgrading to TLS : ~p",[SSL],?INFO),
+     {nodata, State#state_rcv{socket=SSL,protocol=ts_ssl}}.
+diff --git a/src/tsung/tsung.erl b/src/tsung/tsung.erl
+index 3f9029b..0b47c17 100644
+--- a/src/tsung/tsung.erl
++++ b/src/tsung/tsung.erl
+@@ -39,6 +39,7 @@
+ %%----------------------------------------------------------------------
+ start(_Type, _StartArgs) ->
+ % error_logger:tty(false),
++    ssl:start(),
+     ?LOG("open logfile  ~n",?DEB),
+     LogFileEnc = ts_config_server:decode_filename(?config(log_file)),
+     LogFile = filename:join(LogFileEnc, atom_to_list(node()) ++ ".log"),
