4770d4147291f0c0455e2776c626c8211cf09bf9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-05 08:50:53 +0100

    bindfs: add 1.12.4 bottle.
    Closes #29893.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/bindfs.rb
+++ Library/Formula/bindfs.rb
@@ -5,6 +5,13 @@ class Bindfs < Formula
   url "http://bindfs.org/downloads/bindfs-1.12.4.tar.gz"
   sha1 "c518fdaf4e22472f0b8140c5a338493c444026b6"
 
+  bottle do
+    cellar :any
+    sha1 "6709966a00d8bb146b5bd4e75dc37dc38db7bb88" => :mavericks
+    sha1 "c04026b339551e371fcd5993a9fb4ce332b45af8" => :mountain_lion
+    sha1 "d19667195246207f7b1eb98bae042e54e8dc26ac" => :lion
+  end
+
   head do
     url "https://github.com/mpartel/bindfs.git"
     depends_on "autoconf" => :build
