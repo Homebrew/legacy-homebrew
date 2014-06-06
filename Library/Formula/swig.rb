c484aac34f6959f59bfe298a0fbfc40a6d230665
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-06 13:24:21 +0100

    swig: add 3.0.2 bottle.
    Closes #29934.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/swig.rb
+++ Library/Formula/swig.rb
@@ -5,6 +5,12 @@ class Swig < Formula
   url "https://downloads.sourceforge.net/project/swig/swig/swig-3.0.2/swig-3.0.2.tar.gz"
   sha1 "e695a14acf39b25f3ea2d7303e23e39dfe284e31"
 
+  bottle do
+    sha1 "1736d03e7704234b4729262cdb4077f6004a87b9" => :mavericks
+    sha1 "dd3ddbbfdc77d9d20f4754fde6bc254568e73920" => :mountain_lion
+    sha1 "f08edf42ba3db64e03e90f6bb17d225e0e5218df" => :lion
+  end
+
   option :universal
 
   depends_on "pcre"
