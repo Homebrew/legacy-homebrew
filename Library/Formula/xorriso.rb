0b2bf6ca1b2e25700c3f1378caf63197844d672e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-06 05:54:45 +0100

    xorriso: fix version

--- Library/Formula/xorriso.rb
+++ Library/Formula/xorriso.rb
@@ -5,12 +5,13 @@ class Xorriso < Formula
   url "http://ftpmirror.gnu.org/xorriso/xorriso-1.3.6.pl01.tar.gz"
   mirror "https://ftp.gnu.org/gnu/xorriso/xorriso-1.3.6.pl01.tar.gz"
   sha1 "b9d8f38726993e707c7bb512c73e591644905e9c"
+  version "1.3.6.pl01"
 
   bottle do
     cellar :any
-    sha1 "90dd3a3725745ec28354c9023a586060fd98e657" => :mavericks
-    sha1 "093c903091d37eed55fd4f5401d86cd15f9d3458" => :mountain_lion
-    sha1 "cd35b61fc76fb00f0aef3b12b8468b32b25827c7" => :lion
+    sha1 "be075d21843fa8134448d7c964a74ff5b7ae376a" => :mavericks
+    sha1 "9e9fbc27e1c88493b279a0d6b6429610edf27916" => :mountain_lion
+    sha1 "b135a141f02d669c70427db2bfacff8f7db06ad4" => :lion
   end
 
   def install
