1dd924ec0e7eed3a542b36436e0a8e70280e4ac8
David Christenson <david@the-david.com>
2014-06-05 21:47:26 -0600

    zile 2.4.11
    Update to latest stable release, add pkg-config and help2man
    dependencies, use HTTPS on homepage and mirror, change single quotes
    to double.
    
    Closes #29925.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/zile.rb
+++ Library/Formula/zile.rb
@@ -1,13 +1,15 @@
-require 'formula'
+require "formula"
 
 class Zile < Formula
-  homepage 'http://www.gnu.org/software/zile/'
-  url 'http://ftpmirror.gnu.org/zile/zile-2.4.9.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/zile/zile-2.4.9.tar.gz'
-  sha1 'f233487e2d0ce99d7670832d106d1a2503d4c925'
+  homepage "https://www.gnu.org/software/zile/"
+  url "http://ftpmirror.gnu.org/zile/zile-2.4.11.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/zile/zile-2.4.11.tar.gz"
+  sha1 "ad2efb80031c3a406f8f83ac5d400a38bc297434"
 
-  depends_on 'bdw-gc'
-  depends_on 'gettext'
+  depends_on "pkg-config" => :build
+  depends_on "bdw-gc"
+  depends_on "gettext"
+  depends_on "help2man"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
