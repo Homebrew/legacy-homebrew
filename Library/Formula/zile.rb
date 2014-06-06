ae32b694705c4513fbfdd492d81a8878738b0d42
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-06 04:56:51 +0100

    zile: add 2.4.11 bottle.

--- Library/Formula/zile.rb
+++ Library/Formula/zile.rb
@@ -6,6 +6,12 @@ class Zile < Formula
   mirror "https://ftp.gnu.org/gnu/zile/zile-2.4.11.tar.gz"
   sha1 "ad2efb80031c3a406f8f83ac5d400a38bc297434"
 
+  bottle do
+    sha1 "6112330f840500f201c9903a67003f22c484e458" => :mavericks
+    sha1 "0daeecd50c14a9338527fcfc367f96487d538cd4" => :mountain_lion
+    sha1 "1a32cf3d3c235bba1fd570e7bff81190851ec411" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "bdw-gc"
   depends_on "gettext"
