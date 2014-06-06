26c334fd75b0133f90d5baa9e33ae82d8205fc48
William Woodruff <william@tuffbizz.com>
2014-06-06 17:08:54 -0400

    fixed compiler warnings and program name in error message

--- Library/Formula/ddate.rb
+++ Library/Formula/ddate.rb
@@ -6,8 +6,35 @@ class Ddate < Formula
   version '0.1.0'
   sha1 '41eafa66d577082b95e40a8f40ac054aa8ea45dc'
 
+  patch :DATA
+  patch :p0, :DATA
+
   def install
     system ENV.cc, "ddate.c", "-o", "ddate"
     bin.install 'ddate'
   end
 end
+
+__END__
+diff --git a/ddate.c b/ddate.c
+index 9f0d0d0..f471466 100644
+--- a/ddate.c
++++ b/ddate.c
+@@ -13,6 +13,7 @@
+ #include <time.h>
+ #include <string.h>
+ #include <stdio.h>
++#include <stdlib.h>
+ 
+ struct disc_time
+ {int season; /* 0-4 */
+@@ -39,7 +40,7 @@ main (int argc,char **argv)
+       hastur=makeday(moe,larry,curly);
+     }
+   else if (argc!=1)
+-    { fprintf(stderr,"Syntax: DiscDate [month day year]");
++    { fprintf(stderr,"Syntax: ddate [month day year]\n");
+       exit(1);
+     }
+   else
+
