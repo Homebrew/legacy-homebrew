73c7ca5613f86e393f0ad712e22856b3436db661
William Woodruff <william@tuffbizz.com>
2014-06-07 11:14:49 -0400

    ddate: update with maintained version, manpage.

--- Library/Formula/ddate.rb
+++ Library/Formula/ddate.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
 class Ddate < Formula
-  homepage 'http://www.discordia.ch/Programs/'
-  url 'www.discordia.ch/Programs/ddate.c'
-  version '0.1.0'
-  sha1 '41eafa66d577082b95e40a8f40ac054aa8ea45dc'
+  homepage "https://github.com/bo0ts/ddate"
+  url "https://github.com/bo0ts/ddate/archive/v0.2.2.tar.gz"
+  sha1 "fafb5867c93d7328a1c5e8a0b9726f98362c0c09"
 
   def install
     system ENV.cc, "ddate.c", "-o", "ddate"
-    bin.install 'ddate'
+    bin.install "ddate"
+    man1.install "ddate.1"
   end
 end
