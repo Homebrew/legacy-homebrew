22d4f9284df0aaba51c9e5b4889ab5b28688ea2c
Mort Yao <mort.yao@gmail.com>
2014-06-07 07:08:23 +0200

    faac: Patch libmp4v2 for Linuxbrew
    strcasestr is defined by both libmp4v2 and glibc.
    
    Closes #86

--- Library/Formula/faac.rb
+++ Library/Formula/faac.rb
@@ -12,9 +12,23 @@ class Faac < Formula
     sha1 "88bd2a82586156372015b7a884809a2d14d127cb" => :lion
   end
 
+  patch :DATA
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
   end
 end
+
+__END__
+--- a/common/mp4v2/mpeg4ip.h	2014-08-11 21:47:47.074013710 -0700
++++ b/common/mp4v2/mpeg4ip.h	2014-08-11 21:48:38.278413585 -0700
+@@ -123,7 +123,6 @@
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+-char *strcasestr(const char *haystack, const char *needle);
+ #ifdef __cplusplus
+ }
+ #endif
