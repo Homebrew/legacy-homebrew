f6a79122cdb715aa04982d688cfe5303b4b62c1c
Tomasz Pajor <n@servercrunch.com>
2014-06-08 14:38:20 +0200

    mongo-c 0.96.2
    Closes #29971.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/mongo-c.rb
+++ Library/Formula/mongo-c.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class MongoC < Formula
   homepage 'http://docs.mongodb.org/ecosystem/drivers/c/'
-  url 'https://github.com/mongodb/mongo-c-driver/releases/download/0.96.0/mongo-c-driver-0.96.0.tar.gz'
-  sha1 '2719914366fd95e64e90fc672d7eb18b488e85e6'
+  url 'https://github.com/mongodb/mongo-c-driver/releases/download/0.96.2/mongo-c-driver-0.96.2.tar.gz'
+  sha1 '6a69db4d2e5e2fc68fd2959666802786111c9275'
 
   bottle do
     cellar :any
