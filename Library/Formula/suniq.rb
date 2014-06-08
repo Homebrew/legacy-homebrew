bafd855508b06acb81212212a509e92462f8f50d
CJ <lim@chernjie.com>
2014-06-09 02:56:17 +0800

    Downgrade c++ to 0x from 11;

--- Library/Formula/suniq.rb
+++ Library/Formula/suniq.rb
@@ -2,12 +2,11 @@ require "formula"
 
 class Suniq < Formula
   homepage 'https://github.com/chernjie/suniq'
-  url 'https://github.com/chernjie/suniq/archive/1.2.1.tar.gz'
-  sha1 '4703c6a0c6b3aa6487c50b74617db8399e39f131'
+  url 'https://github.com/chernjie/suniq/archive/1.2.2.tar.gz'
+  sha1 '307156d092480605544296ade8657b3aa81a8601'
 
   depends_on :autoconf
   depends_on :automake
-  depends_on :gcc47
 
   def install
     system "./autogen.sh", "--prefix=#{prefix}"
