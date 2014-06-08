8597f408b3ab0c78aa6e397a1b2960530bd719f6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-08 19:15:24 +0100

    megacmd: add 0.012 bottle.
    Closes #25523.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/megacmd.rb
+++ Library/Formula/megacmd.rb
@@ -6,6 +6,12 @@ class Megacmd < Formula
   sha1 "30690366ef00b64296cd4acca6f842d83dcc9d69"
   head "https://github.com/t3rm1n4l/megacmd.git"
 
+  bottle do
+    sha1 "6ee3f124bf3c9eae280aefb07f2d25af8353a779" => :mavericks
+    sha1 "8292dfa4c648bbe95d1f9752c63ff3dd78104891" => :mountain_lion
+    sha1 "c49c5876032f94ec4e3d2ff71632f347d08191c0" => :lion
+  end
+
   depends_on "go" => :build
 
   def install
