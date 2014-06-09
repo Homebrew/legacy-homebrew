a2f687b0a3fe769c6c711afbcc68cedeff05d5a7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-09 13:47:29 +0100

    gnu-sed: add 4.2.2 bottle.

--- Library/Formula/gnu-sed.rb
+++ Library/Formula/gnu-sed.rb
@@ -6,6 +6,13 @@ class GnuSed < Formula
   mirror 'http://ftp.gnu.org/gnu/sed/sed-4.2.2.tar.bz2'
   sha1 'f17ab6b1a7bcb2ad4ed125ef78948092d070de8f'
 
+  bottle do
+    cellar :any
+    sha1 "4e97d284b3429838d296928a70bb700e38c33453" => :mavericks
+    sha1 "11a59857a6a4e87059a8c52aeaabfb3283be2113" => :mountain_lion
+    sha1 "a60cef42f2169074f1377fdf59dad5b652216d2c" => :lion
+  end
+
   option 'default-names', "Do not prepend 'g' to the binary"
 
   def install
