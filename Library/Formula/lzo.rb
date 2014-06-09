2cd3fb4556b589b93cc677589dbd12442b888087
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-09 13:47:29 +0100

    lzo: add 2.06 bottle.

--- Library/Formula/lzo.rb
+++ Library/Formula/lzo.rb
@@ -5,6 +5,13 @@ class Lzo < Formula
   url 'http://www.oberhumer.com/opensource/lzo/download/lzo-2.06.tar.gz'
   sha256 'ff79e6f836d62d3f86ef6ce893ed65d07e638ef4d3cb952963471b4234d43e73'
 
+  bottle do
+    cellar :any
+    sha1 "39b3bddecc7646bac80d1b12931148932b9ff6b1" => :mavericks
+    sha1 "0a752df8103e1020b2d8d7fa9fa0a8ce59f9cddc" => :mountain_lion
+    sha1 "d88d2c7a55df8acf9cdfe7b3438e04c07e284adc" => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
