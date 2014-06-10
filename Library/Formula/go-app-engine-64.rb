78aac665ac60182c217d926ff8fbc5427344ea32
Brett Koonce <koonce@gmail.com>
2014-06-10 14:53:36 -0700

    go-appengine-sdk (amd64) 1.9.6

--- Library/Formula/go-app-engine-64.rb
+++ Library/Formula/go-app-engine-64.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class GoAppEngine64 < Formula
-  homepage 'http://code.google.com/appengine/docs/go/overview.html'
-  url 'https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.5.zip'
-  sha1 '6784423fdff9bb7cc577b4253d45185190c5c608'
+  homepage "http://code.google.com/appengine/docs/go/overview.html"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.6.zip"
+  sha1 "5c05a76a4e92383c4d8827b2ba9073ead6fb3ac7"
 
   def install
-    cd '..'
-    share.install 'go_appengine' => name
+    cd ".."
+    share.install "go_appengine" => name
     %w[
       api_server.py appcfg.py bulkloader.py bulkload_client.py dev_appserver.py download_appstats.py goapp
     ].each do |fn|
