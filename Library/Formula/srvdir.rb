8c882ffdef386f7b7b366a950a8f9aa4dd5e0f7c
noonan <noonan@twitter.com>
2014-06-10 22:56:47 -0600

    srvdir 1.0 (new forumla)
    adding simple formula for srvdir, no dependency application to spin up server from cmd line

---
+++ Library/Formula/srvdir.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Srvdir < Formula
+  homepage "https://www.srvdir.net/"
+  url "https://dl.srvdir.net/darwin_amd64/srvdir.zip"
+  sha1 "119407ed6abb5ee411a14ec64cfef469926d42e8"
+  version "1.0"
+
+
+  def install
+      bin.install "srvdir"
+  end
+
+end
