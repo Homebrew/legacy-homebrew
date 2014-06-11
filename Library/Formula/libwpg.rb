9c81730c034ffd133ac53c060ec65dc60c9dc083
Yue Liu <yue.liu@mail.com>
2014-06-10 21:05:05 -0700

    update libwpg to 0.3.0

--- Library/Formula/libwpg.rb
+++ Library/Formula/libwpg.rb
@@ -1,19 +1,29 @@
-require 'formula'
+require "formula"
 
 class Libwpg < Formula
   homepage 'http://libwpg.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/libwpg/libwpg/libwpg-0.2.2/libwpg-0.2.2.tar.bz2'
-  sha1 'e9484e795259485ece9a984f60776704d55afeeb'
+  url 'http://dev-www.libreoffice.org/src/libwpg-0.3.0.tar.bz2'
+  sha1 'c8422f9a01e98ff3cb5d64d518e61f6a0bb77551'
 
   depends_on 'pkg-config' => :build
   depends_on 'libwpd'
+  depends_on 'librevenge'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    # Separate steps or install can fail due to folders not existing
-    system "make"
-    ENV.j1
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/'test.cpp').write <<-EOS.undent
+      #include <libwpg/libwpg.h>
+      int main() {
+        return libwpg::WPG_AUTODETECT;
+      }
+    EOS
+    system ENV.cc, "test.cpp",
+                   "-lrevenge-0.0", "-I#{Formula['librevenge'].include}/librevenge-0.0",
+                   "-lwpg-0.3", "-I#{include}/libwpg-0.3"
   end
 end
