25cffcf1e4fa8b2bc8f989d33d8946978a2da9d4
Jack Nagel <jacknagel@gmail.com>
2014-06-10 23:00:03 -0500

    lynis: prefer string interpolation for readability

--- Library/Formula/lynis.rb
+++ Library/Formula/lynis.rb
@@ -7,10 +7,14 @@ class Lynis < Formula
 
   def install
     inreplace 'lynis' do |s|
-      s.gsub! 'tINCLUDE_TARGETS="/usr/local/include/lynis /usr/local/lynis/include /usr/share/lynis/include ./include"', 'tINCLUDE_TARGETS="' + (prefix/'include') + '"'
-      s.gsub! 'tPLUGIN_TARGETS="/usr/local/lynis/plugins /usr/local/share/lynis/plugins /usr/share/lynis/plugins /etc/lynis/plugins ./plugins"', 'tPLUGIN_TARGETS="' + (prefix/'plugins') + '"'
-      s.gsub! 'tDB_TARGETS="/usr/local/share/lynis/db /usr/local/lynis/db /usr/share/lynis/db ./db"', 'tDB_TARGETS="' + (prefix/'db') + '"'
-      s.gsub! 'tPROFILE_TARGETS="/usr/local/etc/lynis/default.prf /etc/lynis/default.prf ./default.prf"', 'tPROFILE_TARGETS="' + (prefix/'default.prf') + '"'
+      s.gsub!'tINCLUDE_TARGETS="/usr/local/include/lynis /usr/local/lynis/include /usr/share/lynis/include ./include"',
+        %{tINCLUDE_TARGETS="#{include}"}
+      s.gsub! 'tPLUGIN_TARGETS="/usr/local/lynis/plugins /usr/local/share/lynis/plugins /usr/share/lynis/plugins /etc/lynis/plugins ./plugins"',
+        %{tPLUGIN_TARGETS="#{prefix}/plugins"}
+      s.gsub! 'tDB_TARGETS="/usr/local/share/lynis/db /usr/local/lynis/db /usr/share/lynis/db ./db"',
+        %{tDB_TARGETS="#{prefix}/db"}
+      s.gsub! 'tPROFILE_TARGETS="/usr/local/etc/lynis/default.prf /etc/lynis/default.prf ./default.prf"',
+        %{tPROFILE_TARGETS="#{prefix}/default.prf"}
     end
 
     prefix.install "db", "dev", "include", "plugins", "default.prf"
