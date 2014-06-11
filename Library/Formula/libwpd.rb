b2f976688fd0136cf20a61cd7d04bd296a5ba82e
Yue Liu <yue.liu@mail.com>
2014-06-10 21:01:18 -0700

    libwpd 0.10.0
    Closes #30158.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libwpd.rb
+++ Library/Formula/libwpd.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Libwpd < Formula
   homepage 'http://libwpd.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/libwpd/libwpd-0.9.9.tar.bz2'
-  sha1 '74d13c4e5137edc78660059257671b5a1f40224d'
+  url "http://dev-www.libreoffice.org/src/libwpd-0.10.0.tar.bz2"
+  sha1 "bbcc6e528a69492fb2b4bbb9a56d385a29efc4c4"
 
   bottle do
     cellar :any
@@ -15,12 +15,23 @@ class Libwpd < Formula
   depends_on 'pkg-config' => :build
   depends_on "glib"
   depends_on "libgsf"
+  depends_on "librevenge"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make"
-    ENV.deparallelize # Needs a serialized install
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <libwpd/libwpd.h>
+      int main() {
+        return libwpd::WPD_OK;
+      }
+    EOS
+    system ENV.cc, "test.cpp",
+                   "-lrevenge-0.0", "-I#{Formula["librevenge"].include}/librevenge-0.0",
+                   "-lwpd-0.10", "-I#{include}/libwpd-0.10"
   end
 end
