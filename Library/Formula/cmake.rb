865742370c48104884eb94464b1183ad571b79c5
Amir Masoud Abdol <amirmasoud.ab@gmail.com>
2014-06-11 19:09:19 +0200

    cmake-3.0.0

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -22,18 +22,12 @@ end
 
 class Cmake < Formula
   homepage 'http://www.cmake.org/'
-  url 'http://www.cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz'
-  sha1 'cca70b307aa32a6a32c72e01fdfcecc84c1c2690'
+  url 'http://www.cmake.org/files/v3.0/cmake-3.0.0.tar.gz'
+  sha1 '590c27894292cfe93848616f383d2f56975a0ffd'
 
   head 'http://cmake.org/cmake.git'
 
-  bottle do
-    cellar :any
-    revision 2
-    sha1 "e1e50cfd9f421b64365a7a2c34e9e6337f9391b7" => :mavericks
-    sha1 "9c60ed323f8752eb257d1505e33d70e4367e4219" => :mountain_lion
-    sha1 "1914f68373cdc8d1c99b4d76e1e1fed85e4303d3" => :lion
-  end
+
 
   depends_on NoExpatFramework
 
