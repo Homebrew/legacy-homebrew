9004908ddb882fdc610def8bf7b9242297755b5b
noonan <noonan@twitter.com>
2014-06-10 22:56:47 -0600

    fix version number and formatting

--- Library/Formula/srvdir.rb
+++ Library/Formula/srvdir.rb
@@ -4,11 +4,10 @@ class Srvdir < Formula
   homepage "https://www.srvdir.net/"
   url "https://dl.srvdir.net/darwin_amd64/srvdir.zip"
   sha1 "119407ed6abb5ee411a14ec64cfef469926d42e8"
-  version "1.0"
-
+  version "0.1"
 
   def install
-      bin.install "srvdir"
+    bin.install "srvdir"
   end
 
 end
