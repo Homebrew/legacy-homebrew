60ede6420f0638266d37a46500a8fb212f0c3a24
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-12 10:40:20 +0100

    goaccess: add 0.8 bottle.
    Closes #30089.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/goaccess.rb
+++ Library/Formula/goaccess.rb
@@ -5,6 +5,12 @@ class Goaccess < Formula
   url 'http://tar.goaccess.io/goaccess-0.8.tar.gz'
   sha1 '8c220e7e65a6e45ae5a2221e241e976d07c23def'
 
+  bottle do
+    sha1 "081452df0eca44ca9d59591985956090087654e8" => :mavericks
+    sha1 "a74a803534ae1050fea03429b11bb298909ab218" => :mountain_lion
+    sha1 "3a327d75218841c39c356f7c03c8a03212920314" => :lion
+  end
+
   option 'enable-geoip', "Enable IP location information using GeoIP"
 
   head do
