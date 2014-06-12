37726a35493cd82ad3c16a30f94f6ab3495e5fe1
Kenji Rikitake <kenji.rikitake@acm.org>
2014-06-12 15:49:04 +0900

    bind 9.10.0-P2

--- Library/Formula/bind.rb
+++ Library/Formula/bind.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Bind < Formula
   homepage 'http://www.isc.org/software/bind/'
-  url 'http://ftp.isc.org/isc/bind9/9.10.0-P1/bind-9.10.0-P1.tar.gz'
-  sha1 '2404524f321107d7654e4c64f4174f6c464debb9'
-  version '9.10.0-P1'
+  url 'http://ftp.isc.org/isc/bind9/9.10.0-P2/bind-9.10.0-P2.tar.gz'
+  sha1 'c57b5825e36933119e9fd6f43e3f52262e7ff4ed'
+  version '9.10.0-P2'
 
   depends_on "openssl"
 
