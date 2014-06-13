b1a99fafe3395b572465bd2c03c68d3a56f63ce5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-13 14:45:11 +0100

    docker: add 1.0.0 bottle.
    Closes #30104.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/docker.rb
+++ Library/Formula/docker.rb
@@ -4,6 +4,12 @@ class Docker < Formula
   homepage "http://docker.io"
   url "https://github.com/dotcloud/docker.git", :tag => "v1.0.0"
 
+  bottle do
+    sha1 "ff9ca100ffcbf521cc4abad2a6a6a9569dd5a52b" => :mavericks
+    sha1 "ec28d7907015be6898bbfee7c8c85b6ee030c6e1" => :mountain_lion
+    sha1 "b9621863233b248d3efe059dcca271ed1769ada6" => :lion
+  end
+
   option "without-completions", "Disable bash/zsh completions"
 
   depends_on "go" => :build
