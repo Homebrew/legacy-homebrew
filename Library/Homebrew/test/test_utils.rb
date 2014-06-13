f75d3e3c9788b5926354c945814766bd7f2c62d5
Jack Nagel <jacknagel@gmail.com>
2014-06-12 22:05:45 -0500

    Use assert_silent

--- Library/Homebrew/test/test_utils.rb
+++ Library/Homebrew/test/test_utils.rb
@@ -3,6 +3,6 @@ require 'testing_env'
 class UtilTests < Homebrew::TestCase
   def test_put_columns_empty
     # Issue #217 put columns with new results fails.
-    puts_columns []
+    assert_silent { puts_columns [] }
   end
 end
