f0ab610bd4aa7133105d84ff37e5798e5783d215
Larry Shaffer <larrys@dakotacarto.com>
2014-06-12 21:59:57 -0600

    spatialindex 1.8.1

--- Library/Formula/spatialindex.rb
+++ Library/Formula/spatialindex.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Spatialindex < Formula
   homepage 'http://libspatialindex.github.io'
-  url 'http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.0.tar.gz'
-  sha1 '490347eaedc543298687dfbdf74853808b0afb3f'
+  url "http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.1.tar.gz"
+  sha1 "c05f8d081316a163aef117b7fd99811539ec421c"
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
