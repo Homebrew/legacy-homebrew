a233b9c368af465477371a78b96d3bab516a51ef
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-06-13 23:55:58 +0100

    Update MD5Deep
    Should fix the whitespace problem declared by the Homebrew build bot.

--- Library/Formula/md5deep.rb
+++ Library/Formula/md5deep.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
-class Md5deep < Formula
+class MD5deep < Formula
   homepage 'http://md5deep.sourceforge.net/'
   url 'https://github.com/jessek/hashdeep/archive/release-4.4.tar.gz'
   sha1 'cb4e313352974299c32bc55fe56396adb74517ef'
+end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
   
   def install
     system "sh bootstrap.sh"
