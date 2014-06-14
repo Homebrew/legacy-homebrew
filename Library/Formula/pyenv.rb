79a87bed0c910fe7a4f31e9308ade8594b254daf
Yamashita Yuu <yamashita@geishatokyo.com>
2014-06-14 14:51:50 +0900

    pyenv 20140614
    Changes:
    
    * pyenv: Change versioning schema (`v0.4.0-YYYYMMDD` -> `vYYYYMMDD`)
    * python-build: Add new PyPy release; pypy-2.3.1, pypy-2.3.1-src
    * python-build: Create symlinks for executables with version suffix
    * python-build: Use SHA2 as default digest algorithm to verify downloaded archives
    * python-build: Update default setuptools version (4.0 -> 4.0.1)
    * python-build: Import recent changes from ruby-build v20140524
    
    Closes #30141.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pyenv.rb
+++ Library/Formula/pyenv.rb
@@ -3,8 +3,8 @@ require "formula"
 class Pyenv < Formula
   homepage "https://github.com/yyuu/pyenv"
   head "https://github.com/yyuu/pyenv.git"
-  url "https://github.com/yyuu/pyenv/archive/v0.4.0-20140602.tar.gz"
-  sha1 "615c425a70c1979908c9a6be81f8b68ec57b7ea0"
+  url "https://github.com/yyuu/pyenv/archive/v20140614.tar.gz"
+  sha1 "5cb6d4a7d6431a7767775fb1107d2afb8059e86a"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
