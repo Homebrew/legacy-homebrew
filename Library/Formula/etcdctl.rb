e64e073d32c934af732a96883a52de6be944b049
Nikola Majksner <majksner@users.noreply.github.com>
2014-06-14 13:24:22 +0200

    etcdctl 0.4.3
    Closes #30149.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/etcdctl.rb
+++ Library/Formula/etcdctl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Etcdctl < Formula
   homepage 'https://github.com/coreos/etcdctl'
-  url 'https://github.com/coreos/etcdctl/archive/v0.3.0.tar.gz'
-  sha1 '3bc5fae34d40c1717e82a7af4ed177c62dbe22af'
+  url 'https://github.com/coreos/etcdctl/archive/v0.4.3.tar.gz'
+  sha1 '6ca73e6d3af5bb8b88a48f349dea9f200a663197'
   head 'https://github.com/coreos/etcdctl.git'
 
   depends_on 'go' => :build
