eec3d65b47163ee6ed5b967660da011758a7c498
Makoto Sasaki <makoto7878@gmail.com>
2014-06-15 05:59:54 +0900

    pyenv 20140615
    Closes #30159.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pyenv.rb
+++ Library/Formula/pyenv.rb
@@ -3,8 +3,8 @@ require "formula"
 class Pyenv < Formula
   homepage "https://github.com/yyuu/pyenv"
   head "https://github.com/yyuu/pyenv.git"
-  url "https://github.com/yyuu/pyenv/archive/v20140614.tar.gz"
-  sha1 "5cb6d4a7d6431a7767775fb1107d2afb8059e86a"
+  url "https://github.com/yyuu/pyenv/archive/v20140615.tar.gz"
+  sha1 "e5caf58319a4ccc393ea8f20378e43989d03deae"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
