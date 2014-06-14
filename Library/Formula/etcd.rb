5cc6945de39cac1574bd784e01ce4ab85e220f6e
Nikola Majksner <majksner@users.noreply.github.com>
2014-06-14 13:22:32 +0200

    etcd 0.4.3
    Closes #30148.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/etcd.rb
+++ Library/Formula/etcd.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Etcd < Formula
   homepage 'https://github.com/coreos/etcd'
-  url 'https://github.com/coreos/etcd/archive/v0.3.0.tar.gz'
-  sha1 '0f191070268f7c87ca044ad23179995b9290e3c0'
+  url 'https://github.com/coreos/etcd/archive/v0.4.3.tar.gz'
+  sha1 '88f44f3a09911fb60fcebcf0ea0f0bbeea2f9bf6'
   head 'https://github.com/coreos/etcd.git'
 
   depends_on 'go' => :build
