965b8e6929f1cbbf794198a42f11d2c19dfd45b9
Yamashita Yuu <yamashita@geishatokyo.com>
2014-06-14 15:06:30 +0900

    pyenv-virtualenv v20140614
    Changes:
    
    * Add `pyenv virtualenv-init` to enable auto-activation feature
    * Create symlinks for executables with version suffix
    
    Closes #30143.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pyenv-virtualenv.rb
+++ Library/Formula/pyenv-virtualenv.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class PyenvVirtualenv < Formula
   homepage "https://github.com/yyuu/pyenv-virtualenv"
-  url "https://github.com/yyuu/pyenv-virtualenv/archive/v20140421.tar.gz"
-  sha1 "65a82ec70a1de8995d4238fd23becd152e25bdcf"
+  url "https://github.com/yyuu/pyenv-virtualenv/archive/v20140614.tar.gz"
+  sha1 "2ac2202f3a8da4126a94a39dd7ba709e696d4a7d"
 
   head "https://github.com/yyuu/pyenv-virtualenv.git"
 
@@ -12,5 +12,11 @@ class PyenvVirtualenv < Formula
   def install
     ENV["PREFIX"] = prefix
     system "./install.sh"
+  end
+
+  def caveats; <<-EOS.undent
+    To enable auto-activation add to your profile:
+      if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+    EOS
   end
 end
