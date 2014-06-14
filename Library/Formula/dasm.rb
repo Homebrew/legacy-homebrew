20d69af20f1c0086e6ba3d79fee95d7c963cfd6d
Nils Berg <berg.nils@gmail.com>
2014-06-14 12:26:39 +0200

    dasm: change download URL and add head URL

--- Library/Formula/dasm.rb
+++ Library/Formula/dasm.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Dasm < Formula
   homepage 'http://dasm-dillon.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/dasm-dillon/dasm-dillon/2.20.11/dasm-2.20.11.tar.gz'
-  sha1 '6c1f0091e88fff8f814a92304286c1875fd64693'
+  url 'https://downloads.sourceforge.net/project/dasm-dillon/dasm-dillon/2.20.11/dasm-2.20.11-2014.03.04-source.tar.gz'
+  sha1 '2ce78b29505b31778b8f796ba4b161b9f890e77e'
+  head 'svn://svn.code.sf.net/p/dasm-dillon/code/trunk'
 
   def install
     system "make"
