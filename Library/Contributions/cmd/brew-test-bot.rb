c487453d78bd07c348657111bbec02cd45f8fb9b
Jack Nagel <jacknagel@gmail.com>
2014-06-15 11:24:24 -0500

    brew-test-bot: use path-limited diff

--- Library/Contributions/cmd/brew-test-bot.rb
+++ Library/Contributions/cmd/brew-test-bot.rb
@@ -247,14 +247,14 @@ class Test
     return unless diff_start_sha1 != diff_end_sha1
     return if @url and not steps.last.passed?
 
-    diff_stat = git "diff", "#{diff_start_sha1}..#{diff_end_sha1}", "--name-status"
+    diff_stat = git "diff-tree", "-r", "--name-status",
+      diff_start_sha1, diff_end_sha1, "--", "Library/Formula"
+
     diff_stat.each_line do |line|
       status, filename = line.split
       # Don't try and do anything to removed files.
-      if (status == 'A' or status == 'M')
-        if filename.match /Formula\/.+\.rb$/
-          @formulae << File.basename(filename, '.rb')
-        end
+      if status == "A" || status == "M"
+        @formulae << File.basename(filename, ".rb")
       end
     end
   end
