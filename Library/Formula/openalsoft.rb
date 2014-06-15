73b2852bdaef553014b5ebb12ca2969866b99d57
Nick Gravelyn <nick@gravelyn.com>
2014-06-15 15:05:19 -0700

    Double quotes instead of single quotes.

--- Library/Formula/openalsoft.rb
+++ Library/Formula/openalsoft.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Openalsoft < Formula
-  homepage 'http://kcat.strangesoft.net/openal.html'
-  url 'http://kcat.strangesoft.net/openal-releases/openal-soft-1.15.1.tar.bz2'
-  sha1 'a0e73a46740c52ccbde38a3912c5b0fd72679ec8'
+  homepage "http://kcat.strangesoft.net/openal.html"
+  url "http://kcat.strangesoft.net/openal-releases/openal-soft-1.15.1.tar.bz2"
+  sha1 "a0e73a46740c52ccbde38a3912c5b0fd72679ec8"
 
   depends_on "cmake" => :build
 
