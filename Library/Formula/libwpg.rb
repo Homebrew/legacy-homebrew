bc4db5d02c6a786093aaeb8ee36fc66104e891d6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-16 01:38:23 +0100

    libwpg: add 0.3.0 bottle.

--- Library/Formula/libwpg.rb
+++ Library/Formula/libwpg.rb
@@ -5,6 +5,13 @@ class Libwpg < Formula
   url 'http://dev-www.libreoffice.org/src/libwpg-0.3.0.tar.bz2'
   sha1 'c8422f9a01e98ff3cb5d64d518e61f6a0bb77551'
 
+  bottle do
+    cellar :any
+    sha1 "50eed06b50a8793c2ef17e739e07bd2d20123236" => :mavericks
+    sha1 "0ec7bbafdeeeb475d0f8b91801df6b14ba5eaf15" => :mountain_lion
+    sha1 "8664b231a414048acf8e1e113178e689338325a7" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libwpd'
   depends_on 'librevenge'
