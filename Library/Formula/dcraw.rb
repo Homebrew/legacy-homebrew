a14215c35507926bfdf122ef0004289f9cd65082
kronicd <kronicd@kronicdnet>
2014-06-16 11:50:58 +0800

    dcraw: altered sha1 due to silent update
    Closes #30185.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/dcraw.rb
+++ Library/Formula/dcraw.rb
@@ -3,7 +3,7 @@ require "formula"
 class Dcraw < Formula
   homepage "http://www.cybercom.net/~dcoffin/dcraw/"
   url "http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-9.21.tar.gz"
-  sha1 "76375e59c55358661de519edfcbc179857d4b371"
+  sha1 "9d5819939e5fe5b4e11952622e2ee7e4e51dd749"
 
   depends_on "jpeg"
   depends_on "jasper"
