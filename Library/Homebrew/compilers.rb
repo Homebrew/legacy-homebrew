47d5deefad28e028f79bd515e3d7808cbdb912cc
Jack Nagel <jacknagel@gmail.com>
2014-06-16 16:07:18 -0500

    Remove unused method alias
    Unlike CompilerFailure, the interface of Compiler is entirely internal,
    so we don't need to maintain compatibility with anything.

--- Library/Homebrew/compilers.rb
+++ Library/Homebrew/compilers.rb
@@ -1,13 +1,4 @@
 class Compiler < Struct.new(:name, :version, :priority)
-  # This is exposed under the `build` name for compatibility, since
-  # `fails_with` continues to use `build` in the public API.
-  # `build` indicates the build number of an Apple compiler.
-  # This is preferred over version numbers since there are often
-  # significant differences within the same version,
-  # e.g. GCC 4.2 build 5553 vs 5666.
-  # Non-Apple compilers don't have build numbers.
-  alias_method :build, :version
-
   # The major version for non-Apple compilers. Used to indicate a compiler
   # series; for instance, if the version is 4.8.2, it would return "4.8".
   def major_version
