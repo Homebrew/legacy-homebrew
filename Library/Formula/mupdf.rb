db9a10ce6054806545e1f02679f7c1c4f1653055
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-17 07:11:33 +0100

    mupdf: add 1.5 bottle.
    Closes #30214.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mupdf.rb
+++ Library/Formula/mupdf.rb
@@ -5,6 +5,13 @@ class Mupdf < Formula
   url 'http://mupdf.com/downloads/mupdf-1.5-source.tar.gz'
   sha1 '628470ed20f9a03c81e90cd5585a31c0fab386ef'
 
+  bottle do
+    cellar :any
+    sha1 "c4e3b26fc0969b29930ee2eb8a76a9eac183f4cc" => :mavericks
+    sha1 "69c2fabdf1c244a978db82927c5f0f68d71783c0" => :mountain_lion
+    sha1 "283487c7f77029e1b6801142a091a19e933964d6" => :lion
+  end
+
   depends_on :macos => :snow_leopard
   depends_on :x11
 
