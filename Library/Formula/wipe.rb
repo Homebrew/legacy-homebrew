79072b91b8bbde3ea46e770c08044056f1cd3141
locolupo <marcel.wolf@me.com>
2014-06-17 20:37:04 +0200

    adding wipe 0.23 to homebrew

---
+++ Library/Formula/wipe.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Wipe < Formula
+  version '0.23'
+  homepage 'http://lambda-diode.com/software/wipe/'
+  url 'https://github.com/locolupo/wipe/archive/master.tar.gz'
+  sha1 'd99a9a448d4911813909c8e0d9fddf448f6b427c'
+
+  def install
+    system "make", "macos"
+    bin.install "wipe"
+  end
+end
