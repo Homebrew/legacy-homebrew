50bcdc0fa289f134377eb0bc603cad946533345e
Brett Koonce <koonce@gmail.com>
2014-06-16 23:49:50 -0700

    liquibase 3.2.0

--- Library/Formula/liquibase.rb
+++ Library/Formula/liquibase.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class Liquibase < Formula
-  homepage 'http://liquibase.org'
-  url 'https://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.1.1-bin.tar.gz'
-  sha1 '48af02bd837eab401236963c5868fc4dae5dfbee'
+  homepage "http://liquibase.org"
+  url "https://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.2.0-bin.tar.gz"
+  sha1 "155b4c9cf9a434bbe98a7a48b9acd46eb6d77f0a"
 
   def install
-    rm_f Dir['*.bat']
-    chmod 0755, 'liquibase'
+    rm_f Dir["*.bat"]
+    chmod 0755, "liquibase"
     prefix.install_metafiles
-    libexec.install Dir['*']
-    bin.install_symlink libexec+'liquibase'
+    libexec.install Dir["*"]
+    bin.install_symlink libexec+"liquibase"
   end
 
   def caveats
