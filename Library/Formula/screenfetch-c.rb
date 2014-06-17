43268e662054af819b20aeedfccbc9e1b06969ef
William Woodruff <william@tuffbizz.com>
2014-06-17 10:04:28 -0400

    fixed indentation, made bin.install array, moved sha1

--- Library/Formula/screenfetch-c.rb
+++ Library/Formula/screenfetch-c.rb
@@ -1,19 +1,14 @@
 require "formula"
 
 class ScreenfetchC < Formula
-    homepage "http://woodruffw.us/realsite/projects/screenfetch-c.html"
-    version "1.2"
-    url "https://github.com/woodruffw/screenfetch-c/archive/1.2-fix2.tar.gz"
-    sha1 "0eee57ecd156ae818efce48f3ca80e706050a8c6"
+  homepage "http://woodruffw.us/realsite/projects/screenfetch-c.html"
+  url "https://github.com/woodruffw/screenfetch-c/archive/1.2-fix2.tar.gz"
+  sha1 "0eee57ecd156ae818efce48f3ca80e706050a8c6"
+  version "1.2"
 
-    def install
-        system "make", "osx"
-        bin.install "screenfetch-c"
-        bin.install "src/detectde"
-        bin.install "src/detectwm"
-        bin.install "src/detectwmtheme"
-        bin.install "src/detectgtk"
-        bin.install "src/detectgpu"
-        man1.install "manpage/screenfetch-c.1"
-    end
+  def install
+    system "make", "osx"
+    bin.install "screenfetch-c", "src/detectde", "src/detectwm", "src/detectwmtheme", "src/detectgtk", "src/detectgpu"
+    man1.install "manpage/screenfetch-c.1"
+  end
 end
