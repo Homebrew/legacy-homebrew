28834163e8ba6819e4574511728ca20db5f3e07b
Grzegorz Dzięgielewski <jabbas@jabbas.eu>
2014-06-17 12:15:42 +0200

    added plist to transmission formula

--- Library/Formula/transmission.rb
+++ Library/Formula/transmission.rb
@@ -35,12 +35,41 @@ class Transmission < Formula
     system "./configure", *args
     system "make" # Make and install in one step fails
     system "make install"
+
+    (var/"transmission").mkpath
   end
 
   def caveats; <<-EOS.undent
     This formula only installs the command line utilities.
     Transmission.app can be downloaded from Transmission's website:
       http://www.transmissionbt.com
+    EOS
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/transmission-daemon</string>
+          <string>--foreground</string>
+          <string>--config-dir</string>
+          <string>#{var}/transmission/</string>
+          <string>--log-info</string>
+          <string>--logfile</string>
+          <string>#{var}/transmission/transmission-daemon.log</string>
+        </array>
+        <key>KeepAlive</key>
+        <dict>
+          <key>NetworkState</key>
+          <true/>
+        </dict>
+      </dict>
+    </plist>
     EOS
   end
 end
