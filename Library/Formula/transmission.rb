d40fe0fc8036aabe6d9ef619c9347e6ba8a8fdc9
Grzegorz Dzięgielewski <jabbas@jabbas.eu>
2014-06-17 12:38:29 +0200

    added plist_options

--- Library/Formula/transmission.rb
+++ Library/Formula/transmission.rb
@@ -46,6 +46,8 @@ class Transmission < Formula
     EOS
   end
 
+  plist_options :startup => true
+
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
