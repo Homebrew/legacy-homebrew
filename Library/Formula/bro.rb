8f7208933d7362675ef4d3ce32c8a3475b3a95d2
Justin Azoff <justin.azoff@gmail.com>
2014-06-17 07:49:03 -0500

    bro 2.3
    Libmagic is no longer a dependency

--- Library/Formula/bro.rb
+++ Library/Formula/bro.rb
@@ -2,12 +2,11 @@ require 'formula'
 
 class Bro < Formula
   homepage 'http://www.bro-ids.org/'
-  url 'http://www.bro-ids.org/downloads/release/bro-2.2.tar.gz'
-  sha1 'cd70c426ca0369f16919cf45ad3222e6287908df'
+  url 'http://www.bro-ids.org/downloads/release/bro-2.3.tar.gz'
+  sha1 '79397be0e351165d44047b044d29b5e6580532cc'
 
   depends_on 'cmake' => :build
   depends_on 'swig' => :build
-  depends_on 'libmagic'
   depends_on 'geoip' => :recommended
 
   def install
