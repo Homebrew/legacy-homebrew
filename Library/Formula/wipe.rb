739f6d84f9866f8993717df4cd80e644da9fc6c9
locolupo <marcel.wolf@me.com>
2014-06-17 21:14:57 +0200

    tagged version/providing a versioned tarball

--- Library/Formula/wipe.rb
+++ Library/Formula/wipe.rb
@@ -3,7 +3,7 @@ require "formula"
 class Wipe < Formula
   version '0.23'
   homepage 'http://lambda-diode.com/software/wipe/'
-  url 'https://github.com/locolupo/wipe/archive/master.tar.gz'
+  url 'https://github.com/locolupo/wipe/archive/0.23.tar.gz'
   sha1 'd99a9a448d4911813909c8e0d9fddf448f6b427c'
 
   def install
