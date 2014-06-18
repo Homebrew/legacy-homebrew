d77f066777e359e6b4efe8479e47b4935b26ca1f
Marcel Wolf <marcel.wolf@me.com>
2014-06-18 13:03:02 +0200

    using double quotes instead of single quotes

--- Library/Formula/wipe.rb
+++ Library/Formula/wipe.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class Wipe < Formula
-  homepage 'http://lambda-diode.com/software/wipe/'
-  url 'https://github.com/locolupo/wipe/archive/0.23.tar.gz'
-  sha1 '3abd8182e37ecf2a4e9fda7a0c88ba98b99ee5ae'
+  homepage "http://lambda-diode.com/software/wipe/"
+  url "https://github.com/locolupo/wipe/archive/0.23.tar.gz"
+  sha1 "3abd8182e37ecf2a4e9fda7a0c88ba98b99ee5ae"
 
   def install
     system "make", "macos"
