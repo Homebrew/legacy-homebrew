67f8919f11b0e7ca5b8987876aa750ceabdf2d4f
Michael Vetter <g.bluehut@gmail.com>
2014-06-18 17:16:23 +0200

    Update play formula: add caveat
    In Play 2.3 the play command has become the activator command. Play has
    been updated to use Activator.
    
    People should use typesafe-activator formula instead.
    
    You can read more about this change at:
    http://www.playframework.com/documentation/2.3.x/Migration23
    and
    http://www.playframework.com/documentation/2.3.x/Highlights23

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -14,4 +14,18 @@ class Play < Formula
     libexec.install Dir['*']
     bin.install_symlink libexec/'play'
   end
+
+  def caveats; <<-EOS.undent
+    In Play 2.3 the play command has become the activator command. Play has been updated to use Activator.
+
+    This formula only installs Play version 2.2.3, if you would like to install the newest versions run:
+    brew remove play
+    brew install typesafe-activator
+
+    You can read more about this change at:
+    http://www.playframework.com/documentation/2.3.x/Migration23
+    and
+    http://www.playframework.com/documentation/2.3.x/Highlights23
+  EOS
+  end
 end
