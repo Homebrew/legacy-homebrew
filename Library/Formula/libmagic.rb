933854b056da7ba8b5818e9d715606864c7be831
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2014-06-18 16:57:57 +0100

    libmagic 5.19
    Closes #30264.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libmagic.rb
+++ Library/Formula/libmagic.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libmagic < Formula
   homepage 'http://www.darwinsys.com/file/'
-  url 'ftp://ftp.astron.com/pub/file/file-5.18.tar.gz'
-  mirror 'http://fossies.org/unix/misc/file-5.18.tar.gz'
-  sha1 '8753e72097f2cc935608e3b1c57c4c4e31bd8d21'
+  url 'ftp://ftp.astron.com/pub/file/file-5.19.tar.gz'
+  mirror 'http://fossies.org/unix/misc/file-5.19.tar.gz'
+  sha1 '0dff09eb44fde1998be79e8d312e9be4456d31ee'
 
   bottle do
     sha1 "051692967234d6fd03b076aa6c2d1fe2a107d10d" => :mavericks
@@ -19,15 +19,6 @@ class Libmagic < Formula
   # Fixed upstream, should be in next release
   # See http://bugs.gw.com/view.php?id=230
   patch :DATA if MacOS.version < :lion
-
-  # Fixed upstream, should be in next release.
-  # See: http://bugs.gw.com/view.php?id=338
-  #      http://bugs.gw.com/view.php?id=341
-  #      http://bugs.gw.com/view.php?id=342
-  patch :p1 do
-    url 'https://gist.githubusercontent.com/kwilczynski/9925996/raw/483a877749c925a8dcc068de8e3ce5b92f5ea826/338-341-342.diff'
-    sha1 'ff6f630bbe699091cb7d8228486f4de67b338fc8'
-  end
 
   def install
     ENV.universal_binary if build.universal?
