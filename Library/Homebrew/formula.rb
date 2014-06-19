88b30a6b17ce891169054ab0d07539b076ebea50
Jack Nagel <jacknagel@gmail.com>
2014-06-18 23:17:49 -0500

    Fix `brew test` when conflicting minitest gems are installed

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -491,8 +491,6 @@ class Formula
   end
 
   def test
-    require 'test/unit/assertions'
-    extend(Test::Unit::Assertions)
     # Adding the used options allows us to use `build.with?` inside of tests
     tab = Tab.for_name(name)
     tab.used_options.each { |opt| build.args << opt unless build.has_opposite_of? opt }
