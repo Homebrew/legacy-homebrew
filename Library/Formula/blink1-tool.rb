f4d7b3aa846a8853fd88b174b7c61304eb4f3f7a
Zak "Ubercow" Kristjanson <ubercow1337@gmail.com>
2014-06-18 20:26:08 -0700

    Add formula for blink1-tool

---
+++ Library/Formula/blink1-tool.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Blink1Tool < Formula
+  homepage "http://thingm.com/products/blink-1.html"
+  url "https://github.com/todbot/blink1/archive/v1.81.tar.gz"
+  sha1 "8fd96bfad3e6781b165848ef59723fd4ceb16bc4"
+
+  def install
+    cd "commandline" do
+      system "make"
+      bin.install "blink1-tool"
+    end
+  end
+
+  test do
+    system "blink1-tool"
+  end
+end
