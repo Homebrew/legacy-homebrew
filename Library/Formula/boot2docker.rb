8ff94a6ee44291a16311ce0ceadef2b650d5d6c8
Markus Hubig <mhubig@gmail.com>
2014-06-19 12:38:23 +0200

    boot2docker 1.0.0
    Updates the boot2docker formula to version 1.0.0. This is the new, Go
    based, boot2docker management utility. It's the official successor of
    the old shell based version!

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -1,14 +1,31 @@
 require "formula"
 
 class Boot2docker < Formula
-  homepage "https://github.com/boot2docker/boot2docker"
-  head "https://github.com/boot2docker/boot2docker.git"
-  url 'https://github.com/boot2docker/boot2docker/archive/v0.9.1.tar.gz'
-  sha1 '543cf1a2d1e29e0cbd34874da4ae966fcbc8d1de'
+  homepage "https://github.com/boot2docker/boot2docker-cli"
+  url "https://github.com/boot2docker/boot2docker-cli.git", :tag => "v1.0.0"
+  head "https://github.com/boot2docker/boot2docker-cli.git", :branch => "master"
+
+  bottle do
+  end
 
   depends_on "docker" => :recommended
+  depends_on "go" => :build
 
   def install
-    bin.install "boot2docker"
+    (buildpath + "src/github.com/boot2docker/boot2docker-cli").install Dir[buildpath/"*"]
+
+    cd "src/github.com/boot2docker/boot2docker-cli" do
+      ENV["GOPATH"] = buildpath
+      system "go", "get", "-d"
+
+      ENV["GIT_DIR"] = cached_download/".git"
+      system "make", "goinstall"
+    end
+
+    bin.install "bin/boot2docker-cli" => "boot2docker"
+  end
+
+  test do
+    system "#{bin}/boot2docker", "version"
   end
 end
