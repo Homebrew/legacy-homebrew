b48d7a7e86112d321913fe41702abd15a9a2d196
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-20 19:45:56 +0100

    blink1: add 1.81 bottle.
    Closes #30275.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/blink1.rb
+++ Library/Formula/blink1.rb
@@ -5,6 +5,13 @@ class Blink1 < Formula
   url "https://github.com/todbot/blink1/archive/v1.81.tar.gz"
   sha1 "8fd96bfad3e6781b165848ef59723fd4ceb16bc4"
 
+  bottle do
+    cellar :any
+    sha1 "3d647127449b9f57d660fd75f0b9cba64463da0b" => :mavericks
+    sha1 "3bfa0403de5aba1f4e2b721bd8561c9b3e50d8c3" => :mountain_lion
+    sha1 "44a769f44423129f2885d6212f8ec089aa9519ee" => :lion
+  end
+
   def install
     cd "commandline" do
       system "make"
