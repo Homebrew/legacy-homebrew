e90dda552024c57da795ad87f3c0b88fa3e08319
Mike McQuaid <mike@mikemcquaid.com>
2014-06-20 19:48:09 +0100

    Revert "formula_installer: bottle against Homebrew Python."
    This reverts commit dc53c331ebb3bf752655a6e2147ea90c38a22e2f.
    
    Closes #30302.
    Closes #30305.

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -58,6 +58,14 @@ class FormulaInstaller
     return true if f.local_bottle_path
     return false unless f.bottle && f.pour_bottle?
 
+    f.requirements.each do |req|
+      next if req.optional? || req.pour_bottle?
+      if install_bottle_options[:warn]
+        ohai "Building source; bottle blocked by #{req} requirement"
+      end
+      return false
+    end
+
     unless f.bottle.compatible_cellar?
       if install_bottle_options[:warn]
         opoo "Building source; cellar of #{f}'s bottle is #{f.bottle.cellar}"
@@ -257,8 +265,7 @@ class FormulaInstaller
             Requirement.prune
           elsif req.build? && dependent != f && install_bottle_for_dep?(dependent, build)
             Requirement.prune
-          elsif req.satisfied? && !(pour_bottle? || build_bottle?) \
-                && !(!req.optional? && req.default_formula?)
+          elsif req.satisfied?
             Requirement.prune
           elsif req.default_formula?
             dep = req.to_dependency
