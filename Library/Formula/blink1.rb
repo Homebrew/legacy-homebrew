f0b19968a9d257d0f97db2eb23014b01200d96a6
Zak "Ubercow" Kristjanson <ubercow1337@gmail.com>
2014-06-20 11:38:03 -0700

    Include libs and rename to blink1
    * Rename from blink1-tool to blink1.
    * Include the libBlink1.dylib and blink1-lib.h files

--- Library/Formula/blink1.rb
+++ Library/Formula/blink1.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Blink1Tool < Formula
+class Blink1 < Formula
   homepage "http://thingm.com/products/blink-1.html"
   url "https://github.com/todbot/blink1/archive/v1.81.tar.gz"
   sha1 "8fd96bfad3e6781b165848ef59723fd4ceb16bc4"
@@ -9,6 +9,8 @@ class Blink1Tool < Formula
     cd "commandline" do
       system "make"
       bin.install "blink1-tool"
+      lib.install "libBlink1.dylib"
+      include.install "blink1-lib.h"
     end
   end
 
