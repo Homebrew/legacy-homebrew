3337d7d64ae1ec7725a971ee3944e2711e14e614
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-20 17:57:59 +0100

    ddate: add 0.2.2 bottle.
    Closes #29958.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ddate.rb
+++ Library/Formula/ddate.rb
@@ -5,6 +5,13 @@ class Ddate < Formula
   url "https://github.com/bo0ts/ddate/archive/v0.2.2.tar.gz"
   sha1 "fafb5867c93d7328a1c5e8a0b9726f98362c0c09"
 
+  bottle do
+    cellar :any
+    sha1 "af6bacba82ac71f204e915ae669b98cd202e79ad" => :mavericks
+    sha1 "8df50f3601d86819e0b491f7508f6267c3252aa0" => :mountain_lion
+    sha1 "7c4699766bea102425380bde8914c95e53fa99ff" => :lion
+  end
+
   def install
     system ENV.cc, "ddate.c", "-o", "ddate"
     bin.install "ddate"
