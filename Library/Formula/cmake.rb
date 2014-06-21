41d773da8bb1eba2fc4d7401eaae975dd0468271
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-06-21 03:10:27 +0100

    Update cmake
    Updates cmake from 2.8.12 to 3.0.0.

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -22,8 +22,8 @@ end
 
 class Cmake < Formula
   homepage 'http://www.cmake.org/'
-  url 'http://www.cmake.org/files/v2.8/cmake-2.8.12.2.tar.gz'
-  sha1 'cca70b307aa32a6a32c72e01fdfcecc84c1c2690'
+  url 'http://www.cmake.org/files/v3.0/cmake-3.0.0.tar.gz'
+  sha1 '4dfd9ee9b829c77175d655f22322f14747f11ad2'
 
   head 'http://cmake.org/cmake.git'
 
