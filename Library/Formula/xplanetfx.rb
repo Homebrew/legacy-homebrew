531080a9bb65e4b96479510949982571a7db0af4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-21 17:45:01 +0100

    xplanetfx: update 2.5.29 bottle.
    Closes #30304.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/xplanetfx.rb
+++ Library/Formula/xplanetfx.rb
@@ -8,9 +8,9 @@ class Xplanetfx < Formula
 
   bottle do
     cellar :any
-    sha1 "1a05c88b0c87baee4d0ef68928546f3eea6cb41e" => :mavericks
-    sha1 "026963b8eb1879177501372940d67a8ac082be4b" => :mountain_lion
-    sha1 "9d5e555007bcb9506f064cd84add23904d0ab9e1" => :lion
+    sha1 "896bb5909553b0b4bd3429ecfb71e543f72ed1c0" => :mavericks
+    sha1 "f6c65e63627c3ab04978b95dc614d723cb8f7809" => :mountain_lion
+    sha1 "7853a1a73196d16e64bdbaa7751efcb3d114af56" => :lion
   end
 
   option "without-perlmagick", "Build without PerlMagick support - used to check cloud map downloads"
