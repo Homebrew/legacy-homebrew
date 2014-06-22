b7c27cfe473176725ad7b03ecc7d9934da88befc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-22 06:25:10 +0100

    libvisio: add 0.1.0 bottle.
    Closes #30354.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libvisio.rb
+++ Library/Formula/libvisio.rb
@@ -5,6 +5,13 @@ class Libvisio < Formula
   url "http://dev-www.libreoffice.org/src/libvisio-0.1.0.tar.xz"
   sha1 "c82e5c7ad25e513c268032cda9febd01b8879504"
 
+  bottle do
+    cellar :any
+    sha1 "5b7af3a34301558c02ba25bf09087d44479427e9" => :mavericks
+    sha1 "19286ead4b765628712f516e4bb161c4b7caf2ff" => :mountain_lion
+    sha1 "6bac7cd8dce1f27c0acfc41e74348333abcb1f43" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
   depends_on "libwpd"
