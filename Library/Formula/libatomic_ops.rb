987c751fba7794a3b25f3ee4d8cb01741a9ad003
Jack Nagel <jacknagel@gmail.com>
2014-06-21 19:50:40 -0500

    libatomic_ops 7.4.2

--- Library/Formula/libatomic_ops.rb
+++ Library/Formula/libatomic_ops.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class LibatomicOps < Formula
-  homepage 'http://www.hpl.hp.com/research/linux/atomic_ops/'
-  url 'http://www.hpl.hp.com/research/linux/atomic_ops/download/libatomic_ops-7.2d.tar.gz'
-  sha1 'ed5bb963648bdfb87fc815a9037114654d164907'
+  homepage "https://github.com/ivmai/libatomic_ops/"
+  url "http://www.ivmaisoft.com/_bin/atomic_ops/libatomic_ops-7.4.2.tar.gz"
+  sha1 "57cd7c64e37fca300bd7b24e3d2f14129b25b376"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
