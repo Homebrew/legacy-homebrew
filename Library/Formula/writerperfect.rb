171a91ff23c45210c4f3150a33fd140d3de673a1
Yue Liu <yue.liu@mail.com>
2014-06-21 23:20:36 -0700

    writerperfect 0.9.1
    Closes #30548.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/writerperfect.rb
+++ Library/Formula/writerperfect.rb
@@ -1,17 +1,27 @@
-require 'formula'
+require "formula"
 
 class Writerperfect < Formula
-  homepage 'http://libwpd.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/libwpd/writerperfect-0.8.1.tar.bz2'
-  sha1 'c48b42f042973923f56d17ce4c1b5024b6e6dcb8'
+  homepage "http://libwpd.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/libwpd/writerperfect/writerperfect-0.9.1/writerperfect-0.9.1.tar.bz2"
+  sha1 "3a71d699dfdcd5a74639c092d62f0566ecd0dce5"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost" => :build
+  depends_on "libodfgen"
+  depends_on "libwps"
   depends_on "libwpg"
   depends_on "libwpd"
+  depends_on "libetonyek" => :optional
+  depends_on "libvisio" => :optional
+  depends_on "libmspub" => :optional
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "echo", "`#{bin}/wpd2odt`"
   end
 end
