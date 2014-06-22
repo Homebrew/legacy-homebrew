dd6d1f3a9783b8bd9bc6659607ae71722745dd1e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-22 06:16:26 +0100

    libetonyek: add 0.1.1 bottle.
    Closes #30352.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libetonyek.rb
+++ Library/Formula/libetonyek.rb
@@ -5,6 +5,13 @@ class Libetonyek < Formula
   url "http://dev-www.libreoffice.org/src/libetonyek-0.1.1.tar.xz"
   sha1 "024fd730b062d1df54de0352323d03d011e59277"
 
+  bottle do
+    cellar :any
+    sha1 "aed0a80bfcfc83748e4e03cf640fe176c0f211d5" => :mavericks
+    sha1 "9ccf399948d3423af1134d541033988dfc3270a0" => :mountain_lion
+    sha1 "a3a53a426861f26ee026676dd0232427bda8f376" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
   depends_on "libwpd"
