56776841d43bcc3a7533332c570069b7eaa3d6e6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-23 12:00:18 +0100

    aubio: add 0.4.1 bottle.
    Closes #30010.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/aubio.rb
+++ Library/Formula/aubio.rb
@@ -5,6 +5,13 @@ class Aubio < Formula
   url 'http://aubio.org/pub/aubio-0.4.1.tar.bz2'
   sha1 '338ec9f633e82c371a370b9727d6f0b86b0ba376'
 
+  bottle do
+    cellar :any
+    sha1 "959a1e4eb7bf72573f565af2ebe2c51eb7a6eef4" => :mavericks
+    sha1 "d530d90d8e39ed5888bd492a3e3a888e21de2dc3" => :mountain_lion
+    sha1 "5dc7cb95da354f22e32f8b754093e82d729cbe9f" => :lion
+  end
+
   option :universal
 
   depends_on :macos => :lion
