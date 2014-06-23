03f62428715ba965d82ad445583fad2ffec77704
Thijs Vermeir <thijsvermeir@gmail.com>
2014-06-23 10:36:00 +0200

    orc 0.4.21

--- Library/Formula/orc.rb
+++ Library/Formula/orc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Orc < Formula
   homepage 'http://cgit.freedesktop.org/gstreamer/orc/'
-  url 'http://gstreamer.freedesktop.org/src/orc/orc-0.4.19.tar.gz'
-  sha1 '6186a6a5faefe6b61d55e5406c7365d69b91c982'
+  url 'http://gstreamer.freedesktop.org/src/orc/orc-0.4.21.tar.xz'
+  sha1 '519a054cd00a52d1819367a510151742051aedc3'
 
   def install
     system "./configure", "--disable-dependency-tracking",
