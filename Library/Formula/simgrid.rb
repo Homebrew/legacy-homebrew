93f65236818c93bee12f9080d55333aa77135896
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-23 21:36:35 +0100

    simgrid: add 3.11.1 bottle.
    Closes #30372.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/simgrid.rb
+++ Library/Formula/simgrid.rb
@@ -5,6 +5,13 @@ class Simgrid < Formula
   url 'http://gforge.inria.fr/frs/download.php/file/33686/SimGrid-3.11.1.tar.gz'
   sha1 'b00585e2ed11d016eff6252384205e1e990f5895'
 
+  bottle do
+    cellar :any
+    sha1 "880b9462dcea8f3f9c4aa4c32e507ef8e7155db5" => :mavericks
+    sha1 "bbaba0f593b4b81f1e1ebd604dc46eaa58ec8568" => :mountain_lion
+    sha1 "7f1cdc6fd8de1a046b15cbc3b8a648e8ccb5775f" => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'boost'
   depends_on 'pcre'
