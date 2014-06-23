65b27d05824600d6bbed9aaf6fd96d0dac49ba6d
Brett Koonce <koonce@gmail.com>
2014-06-22 18:36:10 -0700

    parrot 6.3.0/devel 6.5.0

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -1,24 +1,24 @@
-require 'formula'
+require "formula"
 
 class Parrot < Formula
-  homepage 'http://www.parrot.org/'
-  url 'ftp://ftp.parrot.org/pub/parrot/releases/supported/6.0.0/parrot-6.0.0.tar.bz2'
-  sha256 '6cb9223ee389a36588acf76ad8ac85e2224544468617412b1d7902e5eb8bd39b'
+  homepage "http://www.parrot.org/"
+  url "ftp://ftp.parrot.org/pub/parrot/releases/supported/6.3.0/parrot-6.3.0.tar.bz2"
+  sha256 "8d64df21751770741dac263e621275f04ce7493db6f519e3f4886a085161a80d"
 
   devel do
-    url 'ftp://ftp.parrot.org/pub/parrot/releases/devel/6.1.0/parrot-6.1.0.tar.bz2'
-    sha256 'bb1294ad2a7d5b3c4688fc736fb775e94ecfe35fdc072a2631c2080eb5f366f7'
+    url "ftp://ftp.parrot.org/pub/parrot/releases/devel/6.5.0/parrot-6.5.0.tar.bz2"
+    sha256 "1f45044f8dcfaafef795e93a91c8f4a55dd8347cc0359ce4dcf6f34f7bfff140"
   end
 
-  head 'https://github.com/parrot/parrot.git'
+  head "https://github.com/parrot/parrot.git"
 
-  conflicts_with 'rakudo-star'
+  conflicts_with "rakudo-star"
 
-  depends_on 'gmp' => :optional
-  depends_on 'icu4c' => :optional
-  depends_on 'pcre' => :optional
-  depends_on 'readline' => :optional
-  depends_on 'libffi' => :optional
+  depends_on "gmp" => :optional
+  depends_on "icu4c" => :optional
+  depends_on "pcre" => :optional
+  depends_on "readline" => :optional
+  depends_on "libffi" => :optional
 
   def install
     system "perl", "Configure.pl", "--prefix=#{prefix}",
@@ -29,7 +29,7 @@ class Parrot < Formula
     system "make"
     system "make install"
     # Don't install this file in HOMEBREW_PREFIX/lib
-    rm_rf lib/'VERSION'
+    rm_rf lib/"VERSION"
   end
 
   test do
