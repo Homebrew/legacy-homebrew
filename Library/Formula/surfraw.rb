dc4b8d7b35da7fd573c42cd4539a5881c606799d
BeauJoh <beau@inbeta.org>
2014-06-24 14:20:51 +1000

    surfraw-2.2.9 (updated from 2.2.8)
    Updated surfraw from 2.2.8 to 2.2.9 this was needed as the surfraw
    update was released way back in Dec 10 2013.

--- Library/Formula/surfraw.rb
+++ Library/Formula/surfraw.rb
@@ -1,19 +1,18 @@
-require 'formula'
+require "formula"
 
 class Surfraw < Formula
-  homepage 'http://surfraw.alioth.debian.org/'
-  url 'http://ftp.de.debian.org/debian/pool/main/s/surfraw/surfraw_2.2.8.orig.tar.gz'
-  sha1 '3114cd6e8d64f87b84ed0eff4369bfb0b10f2eb6'
+    url "http://surfraw.alioth.debian.org/dist/surfraw-2.2.9.tar.gz"
+    head 'git://git.debian.org/surfraw/surfraw.git'
+    homepage 'http://surfraw.alioth.debian.org/'
+    sha1 "70bbba44ffc3b1bf7c7c4e0e9f0bdd656698a1c0"
 
-  head 'git://git.debian.org/surfraw/surfraw.git'
-
-  def install
-    system "./prebuild" if build.head?
-    system "./configure", "--prefix=#{prefix}",
-                          "--sysconfdir=#{etc}",
-                          "--with-graphical-browser=open"
-    system "make"
-    ENV.j1 # Install using 1 job, or fails on Mac Pro
-    system "make install"
-  end
+    def install
+        system "./prebuild" if ARGV.build_head?
+        system "./configure", "--prefix=#{prefix}",
+        "--sysconfdir=#{etc}",
+        "--with-graphical-browser=open"
+        system "make"
+        ENV.j1 # Install using 1 job, or fails on Mac Pro
+        system "make install"
+    end
 end
