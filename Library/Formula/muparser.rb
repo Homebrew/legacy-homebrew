7369725731da481d1d4a2ea085bd25702c8c1af2
Stuart Campbell <stuart.i.campbell@gmail.com>
2014-06-25 15:31:58 -0400

    Corrected typo in version number

--- Library/Formula/muparser.rb
+++ Library/Formula/muparser.rb
@@ -4,7 +4,7 @@ class Muparser < Formula
   homepage 'http://muparser.beltoforion.de/'
   url 'https://docs.google.com/uc?export=download&confirm=no_antivirus&id=0BzuB-ydOOoduZjlFOEFRREZrT2s'
   sha1 '3974898052dd9ef350df1860f8292755f78f59df'
-  version '2.23'
+  version '2.2.3'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
