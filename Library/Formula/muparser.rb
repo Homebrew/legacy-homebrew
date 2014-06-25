7f008ff93ae29eaedeb83004ebe36e9d518f95e2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-25 20:40:48 +0100

    muparser: add 2.2.3 bottle.
    Closes #30442.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/muparser.rb
+++ Library/Formula/muparser.rb
@@ -6,6 +6,13 @@ class Muparser < Formula
   sha1 '3974898052dd9ef350df1860f8292755f78f59df'
   version '2.2.3'
 
+  bottle do
+    cellar :any
+    sha1 "5770d673e9eb9f55504eed8828c23508d1140399" => :mavericks
+    sha1 "62a89223d3ae31a0bcc18adb31f5bb9ad570018f" => :mountain_lion
+    sha1 "0e10886b7ca3e5c294f7d8e5913e66889f309175" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
