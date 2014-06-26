80c9bdb48ed44b44c43882be30902d2a76ad12a1
Curtis Hovey <sinzui.is@gmail.com>
2014-06-26 15:14:05 -0400

    juju 1.19.4

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -6,8 +6,8 @@ class Juju < Formula
   sha1 '70ac905e113eedfa08ad8a8acab319b0c7c462cb'
 
   devel do
-    url 'https://launchpad.net/juju-core/trunk/1.19.3/+download/juju-core_1.19.3.tar.gz'
-    sha1 '9ef0ce0d8398e4f0a1ef3888d1204bc54381b16f'
+    url 'https://launchpad.net/juju-core/trunk/1.19.4/+download/juju-core_1.19.4.tar.gz'
+    sha1 'bca2e5d966583797b215afcaf0de0e69d45c48bd'
   end
 
   bottle do
@@ -20,11 +20,19 @@ class Juju < Formula
 
   def install
     ENV['GOPATH'] = buildpath
-    args = %w(install launchpad.net/juju-core/cmd/juju)
+    if build.stable?
+      args = %w(install launchpad.net/juju-core/cmd/juju)
+    else
+      args = %w(install github.com/juju/juju/cmd/juju)
+    end
     args.insert(1, "-v") if ARGV.verbose?
     system "go", *args
     bin.install 'bin/juju'
-    bash_completion.install "src/launchpad.net/juju-core/etc/bash_completion.d/juju-core"
+    if build.stable?
+      bash_completion.install "src/launchpad.net/juju-core/etc/bash_completion.d/juju-core"
+    else
+      bash_completion.install "src/github.com/juju/juju/etc/bash_completion.d/juju-core"
+    end
   end
 
   test do
