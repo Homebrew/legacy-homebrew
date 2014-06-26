bb95bfd0a5dd728b34ef2c8d838c8afeadcf9132
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-27 00:46:02 +0100

    slurm: add 0.4.0 bottle.
    Closes #30467.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/slurm.rb
+++ Library/Formula/slurm.rb
@@ -5,6 +5,13 @@ class Slurm < Formula
   url "https://github.com/mattthias/slurm/archive/upstream/0.4.0.tar.gz"
   sha1 "b50c2245513f1241f78a487504eb5e460aec9a04"
 
+  bottle do
+    cellar :any
+    sha1 "337859641874b00b07d68193b478b3d24f662f18" => :mavericks
+    sha1 "d46703938055e25cf259cb571b89af042328c96c" => :mountain_lion
+    sha1 "be55409fce424e606344f9d904423a7f09cd4caa" => :lion
+  end
+
   depends_on "scons" => :build
 
   # patch to support colour ncurses on OS X
