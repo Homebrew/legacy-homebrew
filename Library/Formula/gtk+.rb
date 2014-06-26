9074493ee64f104c08910bb8a712e80c75d8969b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-26 17:53:14 +0100

    gtk+: update 2.24.24 bottle.

--- Library/Formula/gtk+.rb
+++ Library/Formula/gtk+.rb
@@ -6,6 +6,9 @@ class Gtkx < Formula
   sha256 '12ceb2e198c82bfb93eb36348b6e9293c8fdcd60786763d04cfec7ebe7ed3d6d'
 
   bottle do
+    sha1 "4184a042793c666df9366bc77363abe8347500d4" => :mavericks
+    sha1 "2ba5cee75349a4d87b43e440c04a1e36b4a8f2da" => :mountain_lion
+    sha1 "e7195bd0e896693f940e5000a860be205885fc2c" => :lion
   end
 
   depends_on 'pkg-config' => :build
