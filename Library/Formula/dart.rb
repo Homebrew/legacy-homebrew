b8caa5b7d0d72a3e1ea0be8e2bf7dbd13ba1b192
Brice CHATARD <bchatard@ayaline.com>
2014-06-27 16:15:19 +0100

    Update Dart to version 1.5.1

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -4,14 +4,14 @@ class Dart < Formula
   homepage 'http://www.dartlang.org/'
 
   if MacOS.prefer_64_bit?
-    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/37348/sdk/dartsdk-macos-x64-release.zip'
-    sha1 '5de0801c3140929b9e32d837a7ca0ac4b34422fa'
+    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/37644/sdk/dartsdk-macos-x64-release.zip'
+    sha1 'cce144228fae95b3100f358ebfef41b71b4a8c4e'
   else
-    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/37348/sdk/dartsdk-macos-ia32-release.zip'
-    sha1 'f0a2603e32d4fbc3f5553875f0044705d693cca8'
+    url 'http://storage.googleapis.com/dart-archive/channels/stable/release/37644/sdk/dartsdk-macos-ia32-release.zip'
+    sha1 '09e65b12709c0bc028b89678eb15ca8b26ada8ad'
   end
 
-  version '1.4.3'
+  version '1.5.1'
 
   def install
     libexec.install Dir['*']
