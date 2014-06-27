6ecc88822ee40ad3b9be7d5367c86e6d7e133ba7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-27 20:31:20 +0100

    autogen: add 5.18.2 bottle.
    Closes #30495.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/autogen.rb
+++ Library/Formula/autogen.rb
@@ -6,6 +6,12 @@ class Autogen < Formula
   mirror 'http://ftp.gnu.org/gnu/autogen/rel5.18.2/autogen-5.18.2.tar.gz'
   sha1 'c63a0f567b4ad90c4243efbd2420c51e6b63309a'
 
+  bottle do
+    sha1 "f0f73e326bc3f93b8e9095ed79a7baa50ca2e9b7" => :mavericks
+    sha1 "a4e6e9f7f9b60d18fd43b29c88a857dbab8b3b60" => :mountain_lion
+    sha1 "729f19be0284020f55ebc5b343a9811cde92630c" => :lion
+  end
+
   # Please note, 5.18.2 is not the newest Autogen package.
   # However, 5.18.3 has an unresolved guile issue and should not be updated to.
   # Please do not submit an update to 5.18.3 until this issue (https://github.com/Homebrew/homebrew/pull/30406) is resolved.
