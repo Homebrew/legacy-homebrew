8a01798b105a15b39ddaad676c57bc7c83c39e6b
othree <othree@gmail.com>
2014-06-27 12:00:46 +0800

    Add libr3 formula

---
+++ Library/Formula/libr3.rb
@@ -0,0 +1,50 @@
+require 'formula'
+
+class Libr3 < Formula
+  homepage 'https://github.com/c9s/r3'
+  url 'https://github.com/c9s/r3/archive/1.3.2.tar.gz'
+  sha1 '99837a42b637e32cd634a5fbdaeaf519a2df5dc5'
+
+  option "with-graphviz", "Enable Graphviz functions"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pcre" => :build
+  depends_on "pkg-config" => :build
+  depends_on "graphviz" => :optional
+  depends_on "jemalloc" => :recommended
+
+  head do
+    url 'https://github.com/c9s/r3.git'
+  end
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    system "./autogen.sh"
+    args = %W{
+      --prefix=#{prefix}
+    }
+
+    args << "--enable-graphviz" if build.with? "graphviz"
+    args << "--with-malloc=jemalloc" if build.with? "jemalloc"
+
+    system "./configure", *args
+    system "make install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include "r3.h"
+      int main() {
+          node * n = r3_tree_create(1);
+          r3_tree_free(n);
+          return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-o", "test",
+                  "-lr3", "-I#{include}/r3"
+    system "./test"
+  end
+end
