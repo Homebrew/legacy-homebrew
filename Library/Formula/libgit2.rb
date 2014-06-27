25d4f05dd1bc1d65b796f919fc626f955209b0b1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-27 20:56:46 +0100

    libgit2: add 0.21.0 bottle.

--- Library/Formula/libgit2.rb
+++ Library/Formula/libgit2.rb
@@ -7,6 +7,13 @@ class Libgit2 < Formula
 
   head 'https://github.com/libgit2/libgit2.git', :branch => 'development'
 
+  bottle do
+    cellar :any
+    sha1 "5d71aa92ca16c0e792d134c1b93d4c55fd1fb2c5" => :mavericks
+    sha1 "d62717c452965b9d90e1fb6142d62ff6f47b6403" => :mountain_lion
+    sha1 "36f85d4daa70f51a77e99232167e210b98662169" => :lion
+  end
+
   depends_on 'cmake' => :build
 
   def install
