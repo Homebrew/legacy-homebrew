6c49ca2eea0b730a8cf7f7787cd3915b0c6bd825
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-27 10:40:13 +0100

    r3: add 1.3.2 bottle.

--- Library/Formula/r3.rb
+++ Library/Formula/r3.rb
@@ -5,6 +5,13 @@ class R3 < Formula
   url 'https://github.com/c9s/r3/archive/1.3.2.tar.gz'
   sha1 '99837a42b637e32cd634a5fbdaeaf519a2df5dc5'
 
+  bottle do
+    cellar :any
+    sha1 "16475109c374517ee2e2883a46015622a758ef44" => :mavericks
+    sha1 "75b212352a81faa87c59f6400a98b3946e896ee9" => :mountain_lion
+    sha1 "786d3cfeb350f7b5bc8b17a3d411ac5ef6b8c85f" => :lion
+  end
+
   option :universal
   option "with-graphviz", "Enable Graphviz functions"
 
