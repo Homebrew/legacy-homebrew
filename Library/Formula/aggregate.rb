ff05d816d719e028c63cce24dd174eeb620b557e
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-06-29 00:51:41 +0100

    Aggregate Homepage
    Freecode is no longer being updated, as per their announcement two weeks ago. Consequently, where possible, folks should try to find an alternative to links that point there.

--- Library/Formula/aggregate.rb
+++ Library/Formula/aggregate.rb
@@ -5,6 +5,8 @@ class Aggregate < Formula
   url 'ftp://ftp.isc.org/isc/aggregate/aggregate-1.6.tar.gz'
   sha1 '13420baf4f3f93dbed3c380ab6cca175609c5d7d'
 
+  # Note - Freecode is no longer being updated & an alternative homepage should be found if possible.
+
   conflicts_with 'crush-tools', :because => 'both install an `aggregate` binary'
 
   def install
