c28d0564d09122113ae9385d7be5ebc40f620a47
David Christenson <david@the-david.com>
2014-06-27 21:28:44 -0600

    arping 2.13
    Update to latest stable release, add autotools dependencies, change
    single quotes to double.
    
    Closes #30509.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/arping.rb
+++ Library/Formula/arping.rb
@@ -1,13 +1,16 @@
-require 'formula'
+require "formula"
 
 class Arping < Formula
-  homepage 'https://github.com/ThomasHabets/arping'
-  url 'https://github.com/ThomasHabets/arping/archive/arping-2.12.tar.gz'
-  sha1 '9109330968cc058f735d59d1a81e8cef760969b5'
+  homepage "https://github.com/ThomasHabets/arping"
+  url "https://github.com/ThomasHabets/arping/archive/arping-2.13.tar.gz"
+  sha1 "a253cdfcb83360d4acd5e4fe1d84ed8105a94829"
 
-  depends_on 'libnet'
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libnet"
 
   def install
+    system "./bootstrap.sh"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
