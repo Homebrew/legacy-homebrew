4b4bfe829f6eb2ab1f46aa69627f9bf0c9ea60c8
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-06-29 00:52:19 +0100

    align v1.7.3

--- Library/Formula/align.rb
+++ Library/Formula/align.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Align < Formula
   homepage 'http://www.cs.indiana.edu/~kinzler/align/'
-  url 'http://www.cs.indiana.edu/~kinzler/align/align-1.7.2.tgz'
-  sha1 '6cae78d7df4d0a4aae654c37d68b9501810d9bf0'
+  url 'http://www.cs.indiana.edu/~kinzler/align/align-1.7.3.tgz'
+  sha1 'fca5e53a7a3a95e740e7d12941f4edbee78ca084'
 
   def install
     system 'make', 'install', "BINDIR=#{bin}"
