a75d7acdb6a9d2cbe8eeb414508bca3b4a43c259
David Christenson <david@the-david.com>
2014-06-27 21:04:41 -0600

    libtasn1 4.0
    Update to latest stable release.

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Libtasn1 < Formula
   homepage "https://www.gnu.org/software/libtasn1/"
-  url "http://ftpmirror.gnu.org/libtasn1/libtasn1-3.6.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libtasn1/libtasn1-3.6.tar.gz"
-  sha1 "0b0ac4388ee08f90b90dccfec63e5b026f23cf2c"
+  url "http://ftpmirror.gnu.org/libtasn1/libtasn1-4.0.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.0.tar.gz"
+  sha1 "2187d9b9bfc6b3d15264ca4127e6f53415b87ca8"
 
   bottle do
     cellar :any
