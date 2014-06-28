767dad97c3dae12838cd861c1218cf4149e1d552
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-06-28 21:22:41 +0100

    Activemq
    Version bump

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Activemq < Formula
   homepage 'http://activemq.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=/activemq/5.9.1/apache-activemq-5.9.1-bin.tar.gz'
-  sha1 '03c0730e8de406ad8dc243f5834bf5b74492c5e8'
+  url 'http://www.apache.org/dyn/closer.cgi?path=/activemq/5.10.0/apache-activemq-5.10.0-bin.tar.gz'
+  sha1 '16e2ba71a66b90bbda9d214d620aae1079bbe7e4'
 
   skip_clean 'libexec/webapps/admin/WEB-INF/jsp'
 
