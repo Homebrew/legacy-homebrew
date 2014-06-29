03891be909e80955072d5db9a2235e456e94ba97
Yue Liu <yue.liu@mail.com>
2014-06-29 16:00:52 -0700

    fix libmspub pkgconfig file and url

--- Library/Formula/libmspub.rb
+++ Library/Formula/libmspub.rb
@@ -2,8 +2,9 @@ require "formula"
 
 class Libmspub < Formula
   homepage "http://www.freedesktop.org/wiki/Software/libmspub/"
-  url "http://dev-www.libreoffice.org/src/libmspub-0.1.0.tar.bz2"
+  url "http://dev-www.libreoffice.org/src/libmspub/libmspub-0.1.0.tar.bz2"
   sha1 "4dc9b9c85cb1c30dde071a88edf40a629b6de666"
+  revision 1
 
   bottle do
     cellar :any
@@ -20,6 +21,7 @@ class Libmspub < Formula
   depends_on "librevenge"
 
   def install
+    inreplace "libmspub.pc.in", /^Requires\.private:.*$/, ""
     system "./configure", "--without-docs",
                           "--disable-dependency-tracking",
                           "--enable-static=no",
