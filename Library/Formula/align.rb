60fc8b0b0ee5877379c8ffbbb49860fae3cd8419
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-29 04:47:43 +0100

    align: add 1.7.3 bottle.

--- Library/Formula/align.rb
+++ Library/Formula/align.rb
@@ -5,6 +5,13 @@ class Align < Formula
   url 'http://www.cs.indiana.edu/~kinzler/align/align-1.7.3.tgz'
   sha1 'fca5e53a7a3a95e740e7d12941f4edbee78ca084'
 
+  bottle do
+    cellar :any
+    sha1 "6883eab5710385e8890fb52a8a6ac248f6bb29b7" => :mavericks
+    sha1 "12b51ccae39d06623f7475e289fd6eb6dbb89d7f" => :mountain_lion
+    sha1 "58e739c1df5c04ef37303c822fd4c859bf9e89eb" => :lion
+  end
+
   def install
     system 'make', 'install', "BINDIR=#{bin}"
   end
