eba0fc133c0dcd517cc992e222a3416295e22306
Dan Weeks <dan@danimal.org>
2014-06-29 07:34:36 -0700

    use the official switchaudio-osx repository

--- Library/Formula/switchaudio-osx.rb
+++ Library/Formula/switchaudio-osx.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class SwitchaudioOsx < Formula
-  homepage "https://github.com/FriskyElectrocat/switchaudio-osx/"
-  url "https://github.com/FriskyElectrocat/switchaudio-osx/archive/0.0.1-unofficial.tar.gz"
-  sha1 "7b0dc8569e81d997dd288b4623a9687feecce139"
-  head "https://github.com/FriskyElectrocat/switchaudio-osx.git"
+  homepage "https://github.com/deweller/switchaudio-osx/"
+  url "https://github.com/deweller/switchaudio-osx/archive/1.0.0.tar.gz"
+  sha1 "4b7bae425f4b9ec71b67dc2c4c6f26fd195d623a"
+  head "https://github.com/deweller/switchaudio-osx.git"
 
   depends_on :macos => :lion
   depends_on :xcode => :build
@@ -16,7 +16,7 @@ class SwitchaudioOsx < Formula
                "-verbose"
     prefix.install Dir["build/Release/*"]
     bin.write_exec_script "#{prefix}/SwitchAudioSource"
-    chmod 0755, bin/SwitchAudioSource
+    chmod 0755, bin/"SwitchAudioSource"
   end
 
   test do
