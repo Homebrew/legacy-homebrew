f0777379d9b63003b5d77041e9508b451b15e1c1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-29 01:31:30 +0100

    arping: add 2.13 bottle.

--- Library/Formula/arping.rb
+++ Library/Formula/arping.rb
@@ -5,6 +5,13 @@ class Arping < Formula
   url "https://github.com/ThomasHabets/arping/archive/arping-2.13.tar.gz"
   sha1 "a253cdfcb83360d4acd5e4fe1d84ed8105a94829"
 
+  bottle do
+    cellar :any
+    sha1 "eabc0dbfda6ff745d722a9f9f7ff84db64381ccd" => :mavericks
+    sha1 "38b7d22becfe47fd17ba3dd856fd17f6c90ffbc8" => :mountain_lion
+    sha1 "877c2e5384864515e5861c8d3784d63440097433" => :lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libnet"
