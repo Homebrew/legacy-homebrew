d3c337537e60d30555aa2e98e7106642b125fea7
fbrosson <fbrosson@users.noreply.github.com>
2014-06-29 21:30:32 +0200

    cdrtools: moved the patch defs to fix the formula

--- Library/Formula/cdrtools.rb
+++ Library/Formula/cdrtools.rb
@@ -6,20 +6,6 @@ class Cdrtools < Formula
   stable do
     url 'https://downloads.sourceforge.net/project/cdrtools/cdrtools-3.00.tar.bz2'
     sha1 '6464844d6b936d4f43ee98a04d637cd91131de4e'
-  end
-
-  devel do
-    url 'https://downloads.sourceforge.net/project/cdrtools/alpha/cdrtools-3.01a24.tar.bz2'
-    version '3.01~a24'
-    sha1 'b49b01b6269280336ef3ca89aa41538db3a9b2dc'
-  end
-
-  depends_on 'smake' => :build
-
-  conflicts_with 'dvdrtools',
-    :because => 'both dvdrtools and cdrtools install binaries by the same name'
-
-  stable do
     patch :p0 do
       url 'https://trac.macports.org/export/104091/trunk/dports/sysutils/cdrtools/files/patch-include_schily_sha2.h'
       sha1 '6c2c06b7546face6dd58c3fb39484b9120e3e1ca'
@@ -27,8 +13,16 @@ class Cdrtools < Formula
   end
 
   devel do
+    url 'https://downloads.sourceforge.net/project/cdrtools/alpha/cdrtools-3.01a24.tar.bz2'
+    version '3.01~a24'
+    sha1 'b49b01b6269280336ef3ca89aa41538db3a9b2dc'
     patch :p0, :DATA
   end
+
+  depends_on 'smake' => :build
+
+  conflicts_with 'dvdrtools',
+    :because => 'both dvdrtools and cdrtools install binaries by the same name'
 
   def install
     system "smake", "INS_BASE=#{prefix}", "INS_RBASE=#{prefix}", "install"
