1f9167c106e8ab8238969d489f88180b13e00a6e
Dan Weeks <dan@danimal.org>
2014-06-28 17:25:27 -0700

    more Formula fixes for switchaudio-osx
    - inline the install path variable
    - use double quotes everywhere
    - per @mikemcquaid

--- Library/Formula/switchaudio-osx.rb
+++ Library/Formula/switchaudio-osx.rb
@@ -14,10 +14,9 @@ class SwitchaudioOsx < Formula
                "-target", "SwitchAudioSource",
                "SYMROOT=build",
                "-verbose"
-    prefix.install Dir['build/Release/*']
-    inner_binary = "#{prefix}/SwitchAudioSource"
-    bin.write_exec_script inner_binary
-    chmod 0755, bin/'SwitchAudioSource'
+    prefix.install Dir["build/Release/*"]
+    bin.write_exec_script "#{prefix}/SwitchAudioSource"
+    chmod 0755, bin/SwitchAudioSource
   end
 
   test do
