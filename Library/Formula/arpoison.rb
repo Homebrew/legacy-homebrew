5afe798cf9535daa28b5a9c467b150dd45e66d06
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-29 04:47:43 +0100

    arpoison: add 0.7 bottle.
    Closes #30528.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/arpoison.rb
+++ Library/Formula/arpoison.rb
@@ -5,6 +5,13 @@ class Arpoison < Formula
   url 'http://www.arpoison.net/arpoison-0.7.tar.gz'
   sha1 '14e89b1acbd09874fd389b3cef0dd1a7c7f253ae'
 
+  bottle do
+    cellar :any
+    sha1 "e350e47d43c370aa48cb2bfb944cff1723ab500d" => :mavericks
+    sha1 "3fbfa7b1e1a21d1bdd8c61b2aefb84aca7dc91f6" => :mountain_lion
+    sha1 "465efcfc1778734810c249a71892d062eaf41b45" => :lion
+  end
+
   depends_on 'libnet'
 
   def install
