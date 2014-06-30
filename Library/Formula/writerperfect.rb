eb83d834db0aa3f3b868622b56607fac61ebb28e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-06-30 02:50:54 +0100

    writerperfect: add 0.9.1 bottle.

--- Library/Formula/writerperfect.rb
+++ Library/Formula/writerperfect.rb
@@ -5,6 +5,13 @@ class Writerperfect < Formula
   url "https://downloads.sourceforge.net/project/libwpd/writerperfect/writerperfect-0.9.1/writerperfect-0.9.1.tar.bz2"
   sha1 "3a71d699dfdcd5a74639c092d62f0566ecd0dce5"
 
+  bottle do
+    cellar :any
+    sha1 "f6787986d7acf9d615f128792842e76e5b996c34" => :mavericks
+    sha1 "a75fac916f334c5edd38d8636d25460a9cd8041e" => :mountain_lion
+    sha1 "9d2e62a9eb89f353f236a7e086fcd03829a12bb6" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
   depends_on "libodfgen"
