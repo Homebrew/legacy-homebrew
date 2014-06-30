870c18a7f631e6e48f15a636bd03e4b488204865
Denix <denics@free.fr>
2014-06-30 16:11:19 +0200

    changed following suggestions

--- Library/Formula/iipimage-server.rb
+++ Library/Formula/iipimage-server.rb
@@ -29,28 +29,17 @@ class IipimageServer < Formula
       "--disable-dependency-tracking"
       
     system "make"
-    system "mkdir -p #{HOMEBREW_PREFIX}/Library/WebServer/CGI-Executables/"
-    system "cp src/iipsrv.fcgi #{HOMEBREW_PREFIX}/Library/WebServer/CGI-Executables/"
+    mkdir_p "#{prefix}/Library/WebServer/CGI-Executables/"
+    cp "src/iipsrv.fcgi", "#{prefix}/Library/WebServer/CGI-Executables/"
   end
     
     
   def caveats; <<-EOS.undent
     Note that this formula will install IIPImage Server in your system.
-    In order to be able to use it you must create a file called iipsrv.conf
-    under "/etc/apache2/other/" with your preferred editor:
+    In order to be able to use it you must create /etc/apache2/other/iipsrv.conf
+    Here you can find an example:
     
-      sudo vi /etc/apache2/other/iipsrv.conf
-      
-    containing:
-    
-      LoadModule fastcgi_module /usr/local/Cellar/mod_fastcgi/2.4.6/libexec/mod_fastcgi.so
-        
-      # Create a directory for the iipsrv binary
-      ScriptAlias /fcgi-bin/ "/usr/local/Library/WebServer/CGI-Executables/"
-      # Set the module handler
-      AddHandler fastcgi-script fcgi
-      # Initialise some variables for the FCGI server
-      FastCgiServer /usr/local/Library/WebServer/CGI-Executables/iipsrv.fcgi -initial-env LOGFILE=/tmp/iipsrv.log -initial-env VERBOSITY=5 -initial-env MAX_IMAGE_CACHE_SIZE=10 -initial-env JPEG_QUALITY=75 -initial-env MAX_CVT=3000
+      https://gist.githubusercontent.com/denics/b11d3995ab9fcb55bb53/raw/21f7463e08e600ed89471b7cff4f17e00db40605/iipsrv.conf
 
     Then, restart apache:
       
@@ -62,6 +51,6 @@ EOS
   end
 
   test do
-    system "#{HOMEBREW_PREFIX}/Library/WebServer/CGI-Executables/iipsrv.fcgi"
+    system "#{prefix}/Library/WebServer/CGI-Executables/iipsrv.fcgi"
   end
 end
