4a684c99e2a5754ff89d3d881c9231cc66bf1923
Daniel Rothmaler <daniel.rothmaler@nubix.biz>
2014-06-30 11:31:11 +0200

    update developer version to gradle 2.0 RC2

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -6,9 +6,9 @@ class Gradle < Formula
   sha1 '4fee5af4ce5c1214bdbd5e14ef6adda9929f589f'
 
   devel do
-    url 'https://services.gradle.org/distributions/gradle-2.0-rc-1-bin.zip'
-    sha1 '44be0a1f08b2424cc9a2482d463c06524db3abf3'
-    version '2.0-rc-1'
+    url 'https://services.gradle.org/distributions/gradle-2.0-rc-2-bin.zip'
+    sha1 'b424869ea9f123ba1c2573dc431d49dc50c1dd24'
+    version '2.0-rc-2'
   end
 
   def install
