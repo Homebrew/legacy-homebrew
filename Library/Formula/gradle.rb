855768b23d1d88bd8bdd075f4eeb6204315f9a5b
Mateusz Gajewski <github@wendigo.pl>
2014-07-01 13:08:44 +0200

    Update gradle.rb

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -4,7 +4,6 @@ class Gradle < Formula
   homepage 'http://www.gradle.org/'
   url 'https://downloads.gradle.org/distributions/gradle-2.0-bin.zip'
   sha1 '171d2290257c061a96410297f2596596862a847a'
-  version '2.0'
 
   def install
     libexec.install %w[bin lib]
