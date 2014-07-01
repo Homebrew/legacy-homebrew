ce6b7531f239f8c89aa9bbd2596600282954f7f2
Bulat Shakirzyanov <bulat@twilio.com>
2014-07-01 23:29:32 +0100

    mesos: cleanup formula.
    Closes #30045.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mesos.rb
+++ Library/Formula/mesos.rb
@@ -37,6 +37,24 @@ class Mesos < Formula
   end
 
   test do
-    system "#{sbin}/mesos-master", "--version"
+    require "timeout"
+
+    master = fork do
+      exec "#{sbin}/mesos-master", "--ip=127.0.0.1",
+                                   "--registry=in_memory"
+    end
+    slave = fork do
+      exec "#{sbin}/mesos-slave", "--master=127.0.0.1:5050",
+                                  "--work_dir=#{testpath}"
+    end
+    Timeout::timeout(15) do
+      system "#{bin}/mesos", "execute",
+                             "--master=127.0.0.1:5050",
+                             "--name=execute-touch",
+                             "--command=touch\s#{testpath}/executed"
+    end
+    Process.kill("TERM", master)
+    Process.kill("TERM", slave)
+    system "[ -e #{testpath}/executed ]"
   end
 end
