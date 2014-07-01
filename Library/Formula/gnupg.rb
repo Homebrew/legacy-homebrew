1b40b89a5a85192b1b7c8bed9d642f572cd4528d
David Christenson <david@the-david.com>
2014-06-30 21:17:58 -0600

    gnupg 1.4.18
    Update to latest stable release, use double quotes instead of single.

--- Library/Formula/gnupg.rb
+++ Library/Formula/gnupg.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Gnupg < Formula
-  homepage 'http://www.gnupg.org/'
-  url 'ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.17.tar.bz2'
-  mirror 'http://mirror.switch.ch/ftp/mirror/gnupg/gnupg/gnupg-1.4.17.tar.bz2'
-  sha1 '830c7f749ad92d6577c521addea5e5d920128d42'
+  homepage "http://www.gnupg.org/"
+  url "ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-1.4.18.tar.bz2"
+  mirror "http://mirror.switch.ch/ftp/mirror/gnupg/gnupg/gnupg-1.4.18.tar.bz2"
+  sha1 "41462d1a97f91abc16a0031b5deadc3095ce88ae"
 
   bottle do
     sha1 "4b841286cbcd16c0ae1a039a959dd1d29751072b" => :mavericks
@@ -12,10 +12,10 @@ class Gnupg < Formula
     sha1 "fe3780d0694e1cb01b17bcaadf95b7f3c02b0060" => :lion
   end
 
-  option '8192', 'Build with support for private keys of up to 8192 bits'
+  option "8192", "Build with support for private keys of up to 8192 bits"
 
   def install
-    inreplace 'g10/keygen.c', 'max=4096', 'max=8192' if build.include? '8192'
+    inreplace "g10/keygen.c", "max=4096", "max=8192" if build.include? "8192"
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -25,7 +25,7 @@ class Gnupg < Formula
 
     # we need to create these directories because the install target has the
     # dependency order wrong
-    [bin, libexec/'gnupg'].each(&:mkpath)
+    [bin, libexec/"gnupg"].each(&:mkpath)
     system "make install"
   end
 end
