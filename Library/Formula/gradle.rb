e41d2b832c989b96074e985baa0812e867d7f189
Mateusz Gajewski <github@wendigo.pl>
2014-07-01 10:36:18 +0200

    gradle 2.0
    Closes #30585.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -2,14 +2,8 @@ require 'formula'
 
 class Gradle < Formula
   homepage 'http://www.gradle.org/'
-  url 'https://services.gradle.org/distributions/gradle-1.12-bin.zip'
-  sha1 '4fee5af4ce5c1214bdbd5e14ef6adda9929f589f'
-
-  devel do
-    url 'https://services.gradle.org/distributions/gradle-2.0-rc-2-bin.zip'
-    sha1 'b424869ea9f123ba1c2573dc431d49dc50c1dd24'
-    version '2.0-rc-2'
-  end
+  url 'https://downloads.gradle.org/distributions/gradle-2.0-bin.zip'
+  sha1 '171d2290257c061a96410297f2596596862a847a'
 
   def install
     libexec.install %w[bin lib]
