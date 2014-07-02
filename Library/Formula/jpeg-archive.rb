f7fffb40a0b146c959fc4bc576ce41ac4fe671e3
f-kubotar <f.kubotar@paperboy.co.jp>
2014-07-02 16:03:08 +0900

    using bin.install

--- Library/Formula/jpeg-archive.rb
+++ Library/Formula/jpeg-archive.rb
@@ -1,18 +1,17 @@
-require 'formula'
+require "formula"
 
 class JpegArchive < Formula
-  homepage 'https://github.com/danielgtaylor/jpeg-archive'
-  url 'https://github.com/danielgtaylor/jpeg-archive/archive/1.0.1.tar.gz'
-  sha1 '45d36e74ae795d25e8cbf20d2ccd3ae17d4c6f7b'
+  homepage "https://github.com/danielgtaylor/jpeg-archive"
+  url "https://github.com/danielgtaylor/jpeg-archive/archive/1.0.1.tar.gz"
+  sha1 "45d36e74ae795d25e8cbf20d2ccd3ae17d4c6f7b"
 
-  depends_on 'jpeg-turbo'
+  depends_on "jpeg-turbo"
 
   def install
     system "make"
-    mkdir_p "#{prefix}/bin"
-    %w(jpeg-recompress jpeg-hash jpeg-compare).each do |bin_file|
-      cp bin_file, "#{prefix}/bin/#{bin_file}"
-    end
+    bin.install "./jpeg-recompress"
+    bin.install "./jpeg-hash"
+    bin.install "./jpeg-compare"
   end
 
   test do
