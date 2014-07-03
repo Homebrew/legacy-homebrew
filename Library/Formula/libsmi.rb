b9acca3784d57d20a7a79a2eadd6d54b490fabfb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-03 22:00:26 +0100

    libsmi: add 0.4.8 bottle.

--- Library/Formula/libsmi.rb
+++ Library/Formula/libsmi.rb
@@ -5,6 +5,12 @@ class Libsmi < Formula
   url 'https://www.ibr.cs.tu-bs.de/projects/libsmi/download/libsmi-0.4.8.tar.gz'
   sha1 '77c512ccbdd29667d152398b0dcde533aed57b49'
 
+  bottle do
+    sha1 "85840d6b3e66ecc3ff3c4044942a09cb4cd7bc12" => :mavericks
+    sha1 "b59655ea31657172d78cfe736610ca510f788822" => :mountain_lion
+    sha1 "7e2434d1b0ae815f91c98d09897d71d0a8197a46" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
