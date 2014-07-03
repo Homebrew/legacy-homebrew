492b3d8251bc38496d6c293aaec4be495b74ddd2
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-07-04 00:26:16 +0100

    new formula: onetime
    New formula: onetime. Assists users in creating one-time pads. Still
    actively supported upstream, and has gathered a lot of renewed interest
    thanks to Hak5 covering the tool recently.

---
+++ Library/Formula/onetime.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Onetime < Formula
+  homepage 'http://red-bean.com/onetime/'
+  url 'http://red-bean.com/onetime/onetime-1.81.tar.gz'
+  sha256 '36a83a83ac9f4018278bf48e868af00f3326b853229fae7e43b38d167e628348'
+
+  devel do
+    url "https://github.com/kfogel/OneTime/archive/2.0-beta2.tar.gz"
+    version "2.0.02"
+    sha256 "adf39b670c9066499b8d50c445019f4cca167cdf1fbdbac7e1b779aa75187b44"
+  end
+
+  depends_on :python
+
+  def install
+    inreplace "Makefile", '$(DESTDIR)/usr/bin/', '$(DESTDIR)/usr/local/Cellar/onetime'
+    mkdir "#{prefix}/bin"
+    system "make"
+    system "make install"
+    mv "/usr/local/Cellar/onetime/onetime", "#{prefix}/bin"
+  end
+end
