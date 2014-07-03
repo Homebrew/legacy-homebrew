3bda24f484e5fc92a104fba2806772700cb90b9b
Tomasz Pajor <n@servercrunch.com>
2014-07-03 12:01:34 +0200

    libsodium 0.6.0

--- Library/Formula/libsodium.rb
+++ Library/Formula/libsodium.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class Libsodium < Formula
-  homepage 'https://github.com/jedisct1/libsodium/'
-  url 'https://github.com/jedisct1/libsodium/releases/download/0.5.0/libsodium-0.5.0.tar.gz'
-  sha256 '3ca0a0619199a2adb3449eb7f1bf6e1f4fb2ef8514da9133f7f043b8b5cdf9f0'
+  homepage "https://github.com/jedisct1/libsodium/"
+  url "https://github.com/jedisct1/libsodium/releases/download/0.6.0/libsodium-0.6.0.tar.gz"
+  sha256 "84cdb6bf8ae3384f3ef78636f93bc689df748c1d36f87d4b6ab1e31c2d4dd145"
 
   head do
-    url 'https://github.com/jedisct1/libsodium.git'
+    url "https://github.com/jedisct1/libsodium.git"
 
-    depends_on 'libtool' => :build
-    depends_on 'autoconf' => :build
-    depends_on 'automake' => :build
+    depends_on "libtool" => :build
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
   end
 
   option :universal
