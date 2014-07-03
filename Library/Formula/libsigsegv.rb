26a9f130f3f5e39f35ac34d8ffd718e8a1e58f58
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-03 22:00:26 +0100

    libsigsegv: add 2.10 bottle.

--- Library/Formula/libsigsegv.rb
+++ Library/Formula/libsigsegv.rb
@@ -6,6 +6,13 @@ class Libsigsegv < Formula
   mirror 'http://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.10.tar.gz'
   sha256 '8460a4a3dd4954c3d96d7a4f5dd5bc4d9b76f5754196aa245287553b26d2199a'
 
+  bottle do
+    cellar :any
+    sha1 "9b41705338724002198da13588a26a4a424609f4" => :mavericks
+    sha1 "7684a990f71233442d9ee1d999138dcabc144bb5" => :mountain_lion
+    sha1 "ef78c9e5ee034cdb78f77c2cb130dd578bc9b327" => :lion
+  end
+
   fails_with :llvm do
     build 2336
     cause "Fails make check with LLVM GCC from XCode 4 on Snow Leopard"
