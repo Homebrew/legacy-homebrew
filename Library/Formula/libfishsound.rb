9a35602e451f55a0a28345e7ceec1f00946c901a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-03 20:34:32 +0100

    libfishsound: add 1.0.0 bottle.

--- Library/Formula/libfishsound.rb
+++ Library/Formula/libfishsound.rb
@@ -5,6 +5,13 @@ class Libfishsound < Formula
   url 'http://downloads.xiph.org/releases/libfishsound/libfishsound-1.0.0.tar.gz'
   sha1 '5263dfaa12dce71e30c5d80f871d92869c6b5ce2'
 
+  bottle do
+    cellar :any
+    sha1 "ef415e726cd7f54f1ac7c0a277ef2535ef8be867" => :mavericks
+    sha1 "663fc1ea81771c2e517514670ca8c7bfcb0af184" => :mountain_lion
+    sha1 "86915dd39751989c022a71ea81b6bbc6f7539b53" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libvorbis'
   depends_on 'speex' => :optional
