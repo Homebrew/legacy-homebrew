17913252e3f31f6ea910622fa0b8077f5ffbdde0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-03 20:34:34 +0100

    liblinear: add 1.94 bottle.

--- Library/Formula/liblinear.rb
+++ Library/Formula/liblinear.rb
@@ -5,6 +5,13 @@ class Liblinear < Formula
   url 'http://www.csie.ntu.edu.tw/~cjlin/liblinear/oldfiles/liblinear-1.94.tar.gz'
   sha1 '19678355e6c933b7ec133e07fef77796e50df0d5'
 
+  bottle do
+    cellar :any
+    sha1 "2abdfc1ed089473fdcb303179bcd1462c0a472aa" => :mavericks
+    sha1 "536cd0730f75face8cfa791de405e9b3d092e6d4" => :mountain_lion
+    sha1 "38dd7df4c233bb0ecf07c887be563e00de7004ca" => :lion
+  end
+
   # Fix sonames
   patch :p0 do
     url "https://trac.macports.org/export/94156/trunk/dports/math/liblinear/files/patch-Makefile.diff"
