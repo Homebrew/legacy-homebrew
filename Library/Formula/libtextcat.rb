0475bce41ffc9f9a69139b3cdf25e68f21ff4efe
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-03 22:00:27 +0100

    libtextcat: add 2.2 bottle.

--- Library/Formula/libtextcat.rb
+++ Library/Formula/libtextcat.rb
@@ -5,6 +5,13 @@ class Libtextcat < Formula
   url 'http://software.wise-guys.nl/download/libtextcat-2.2.tar.gz'
   sha1 'e98d7149d6a20fdbb58cc0b79cb5e3f95ae304e4'
 
+  bottle do
+    cellar :any
+    sha1 "690077cff18427cb7d116a36eb3852bb5af86265" => :mavericks
+    sha1 "9a28c9360d649e2f17850667249f8ed428cfce78" => :mountain_lion
+    sha1 "d285c6f6047a4fbeb61f1aa039402f89c4a72fd2" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
