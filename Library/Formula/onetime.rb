cd26c180b9ae52df9033bba4c836deaf55303887
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-04 18:10:38 +0100

    onetime: add 1.81 bottle.
    Closes #30643.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/onetime.rb
+++ Library/Formula/onetime.rb
@@ -5,6 +5,13 @@ class Onetime < Formula
   url "http://red-bean.com/onetime/onetime-1.81.tar.gz"
   sha1 "db8f84963ed7b5831fdf4c19b4494cf7bfd5b753"
 
+  bottle do
+    cellar :any
+    sha1 "efecf649c941cde0917e4660db0ba460f2193554" => :mavericks
+    sha1 "ec09cda341e360b493e28cf05b2ef847caafd90d" => :mountain_lion
+    sha1 "b243466379b50a72945aaa124405a5e350dc9203" => :lion
+  end
+
   devel do
     url "https://github.com/kfogel/OneTime/archive/2.0-beta2.tar.gz"
     version "2.0.02"
