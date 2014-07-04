ab38caba46bd0c35447c86caec1ba7f850ead84d
Larry Shaffer <larrys@dakotacarto.com>
2014-07-03 21:50:52 -0600

    liblas: fix gdal, libgeotiff defined type conflict

--- Library/Formula/liblas.rb
+++ Library/Formula/liblas.rb
@@ -15,6 +15,14 @@ class Liblas < Formula
 
   option 'with-test', 'Verify during install with `make test`'
 
+  # Fix for error of conflicting types for '_GTIFcalloc' between gdal 1.11
+  # and libgeotiff. Commited so can be removed on next stable release.
+  # https://github.com/libLAS/libLAS/issues/33
+  patch do
+    url "https://github.com/libLAS/libLAS/commit/b8799e.diff"
+    sha1 "3d2430327f9bbff9bd1f6d7cba80c7837552204e"
+  end
+
   def install
     mkdir 'macbuild' do
       # CMake finds boost, but variables like this were set in the last
