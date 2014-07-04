cb5b24dff889f75aa606c4dc9bee6f8b77302b60
Jack Nagel <jacknagel@gmail.com>
2014-07-03 19:08:58 -0500

    Revert "glib-networking: add 2.40.1 bottle."
    This reverts commit 2fe175808be1db17559ab2a5676dfcd14f193215.
    
    glib-networking currently installs files outside of its keg, so the
    bottle contains only locale files.

--- Library/Formula/glib-networking.rb
+++ Library/Formula/glib-networking.rb
@@ -1,16 +1,11 @@
 require 'formula'
 
+# This formula installs files directly into the top-level gio modules
+# directory, so it can't be bottled.
 class GlibNetworking < Formula
   homepage 'https://launchpad.net/glib-networking'
   url 'http://ftp.gnome.org/pub/GNOME/sources/glib-networking/2.40/glib-networking-2.40.1.tar.xz'
   sha256 '9fb3e54d049a480afdb814ff7452e7ab67e5d5f607ade230d7713f19922b5a28'
-
-  bottle do
-    cellar :any
-    sha1 "6b1d2dbdd8f9afa8df900dd84ebbedb4b37bc7a9" => :mavericks
-    sha1 "f015f403d8ac4579c2d9547c208832eccd64dde4" => :mountain_lion
-    sha1 "5fc71615593062f8ace776b5eebf6113cd655d1b" => :lion
-  end
 
   depends_on 'pkg-config' => :build
   depends_on 'intltool' => :build
