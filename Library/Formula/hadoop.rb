b1b709e6b1d7afe2bf5b343076e24c5259600dac
Atkins <atkinschang@icloud.com>
2014-07-05 15:44:04 +0800

    hadoop with native library

--- Library/Formula/hadoop.rb
+++ Library/Formula/hadoop.rb
@@ -1,27 +1,61 @@
-require 'formula'
+require "formula"
 
 class Hadoop < Formula
-  homepage 'http://hadoop.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.4.0/hadoop-2.4.0.tar.gz'
-  sha1 'fc94bb5cb6c50a09fd9fd3ed5e134893ccdf1dbe'
+  homepage "http://hadoop.apache.org/"
+  url "http://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.4.1/hadoop-2.4.1.tar.gz"
+  sha1 "f4166392f1c9a4a3602477d85f51976d44d61c27"
+
+  option "with-tests", "Run tests."
+
+  build.with? "tests" and depends_on "maven", "findbugs", "protobuf", "cmake"
+
+  resource "hadoop-2.4.1-src" do
+    url "http://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.4.1/hadoop-2.4.1-src.tar.gz"
+    sha1 "f4166392f1c9a4a3602477d85f51976d44d61c27"
+  end
 
   def install
-    rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
-    libexec.install %w[bin sbin libexec share etc]
-    bin.write_exec_script Dir["#{libexec}/bin/*"]
-    sbin.write_exec_script Dir["#{libexec}/sbin/*"]
-    # But don't make rcc visible, it conflicts with Qt
-    (bin/'rcc').unlink
+    if build.with? "tests"
 
-    inreplace "#{libexec}/etc/hadoop/hadoop-env.sh",
-      "export JAVA_HOME=${JAVA_HOME}",
-      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
-    inreplace "#{libexec}/etc/hadoop/yarn-env.sh",
-      "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
-      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
-    inreplace "#{libexec}/etc/hadoop/mapred-env.sh",
-      "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
-      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+      if !ENV["JAVA_HOME"] or !ENV["PROTOBUF_HOME"] or !ENV["FINDBUGS_HOME"]
+        opoo "WARN! PATH:"
+        opoo "JAVA_HOME: " + (ENV["JAVA_HOME"] ? ENV["JAVA_HOME"] : 'not set')
+        opoo "HADOOP_PROTOC_PATH: " + (ENV["PROTOBUF_HOME"] ? ENV["PROTOBUF_HOME"] : 'not set')
+        opoo "FINDBUGS_HOME: " + (ENV["FINDBUGS_HOME"] ? ENV["FINDBUGS_HOME"] : 'not set')
+      end
+
+      # set maven build project
+      args = ["clean", "package", "-Pdist,docs,src"]
+      (build.with? "test") or (args << )
+
+      # set maven path
+      mvn = "#{HOMEBREW_PREFIX}/bin/mvn"
+      resource("hadoop-2.4.1-src").stage do
+          system mvn, *args
+          (buildpath+"dist").install Dir["hadoop-dist/*"]
+      end
+    else
+      (buildpath+"dist").install Dir["*"]
+    end
+
+    cd "dist" do
+      rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
+      libexec.install %w[bin sbin libexec share etc]
+      bin.write_exec_script Dir["#{libexec}/bin/*"]
+      sbin.write_exec_script Dir["#{libexec}/sbin/*"]
+      # But don't make rcc visible, it conflicts with Qt
+      (bin/'rcc').unlink
+
+      inreplace "#{libexec}/etc/hadoop/hadoop-env.sh",
+        "export JAVA_HOME=${JAVA_HOME}",
+        "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+      inreplace "#{libexec}/etc/hadoop/yarn-env.sh",
+        "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
+        "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+      inreplace "#{libexec}/etc/hadoop/mapred-env.sh",
+        "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
+        "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+      end
   end
 
   def caveats; <<-EOS.undent
