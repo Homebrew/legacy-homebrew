3e1c8c7f5f3b3eeb57b88e6315a67153f3dde0a7
Aitor Guevara <aitorciki@gmail.com>
2014-07-05 17:32:01 +0200

    uwsgi: optionally depend on brewed Python
    uwsgi didn't depend on brewed Python since system's one is enough
    during the build process. If uwsgi is used to serve Python apps, though,
    brewed Python should be an available option to link Python's plugin
    against.
    The added --with-python option will use brewed Python to build the
    plugin if --build-from-source is used.
    
    Closes #30673.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/uwsgi.rb
+++ Library/Formula/uwsgi.rb
@@ -28,6 +28,7 @@ class Uwsgi < Formula
   depends_on "nagios" => :optional
   depends_on "postgresql" => :optional
   depends_on "pypy" => :optional
+  depends_on "python" => :optional
   depends_on "python3" => :optional
   depends_on "rrdtool" => :optional
   depends_on "rsyslog" => :optional
