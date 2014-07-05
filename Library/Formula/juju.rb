54f462f00c0a407aae05066a26aea7db6c801c8d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-05 20:57:54 +0100

    juju: update 1.20.0 bottle.

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -6,10 +6,9 @@ class Juju < Formula
   sha1 'f57547a0221fd3b5eb0b545c233a88999ce0dc78'
 
   bottle do
-    revision 1
-    sha1 "dc0589478813a6825e8921fccac6c3b98708ddfa" => :mavericks
-    sha1 "a66e18adb8834883d83d64be67cd482e06f4a05e" => :mountain_lion
-    sha1 "f7d9d80b43bdfc355d49b1576bbb15335c3330e2" => :lion
+    sha1 "ba09b71468fd53965e2411d1f99c22061b33bc8a" => :mavericks
+    sha1 "900751666032c38f70c58c24da4039333d6a8777" => :mountain_lion
+    sha1 "a95d5de19f5d8676e67fc60c60b296d31049d3c0" => :lion
   end
 
   depends_on 'go' => :build
