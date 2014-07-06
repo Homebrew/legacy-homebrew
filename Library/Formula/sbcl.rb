116f0d8a117763bb49da61f9d602773573ca3936
Aku Kotkavuo <aku.kotkavuo@gmail.com>
2014-07-06 16:47:56 +0300

    SBCL 1.2.1
    Closes #30690.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/sbcl.rb
+++ Library/Formula/sbcl.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Sbcl < Formula
   homepage "http://www.sbcl.org/"
-  url "https://downloads.sourceforge.net/project/sbcl/sbcl/1.2.0/sbcl-1.2.0-source.tar.bz2"
-  sha1 "275804270c2a00304a1da6ffe9764ca29c393133"
+  url "https://downloads.sourceforge.net/project/sbcl/sbcl/1.2.1/sbcl-1.2.1-source.tar.bz2"
+  sha1 "08734c7ade2be7b787e9a48b6934e06bd75572a9"
 
   head "git://sbcl.git.sourceforge.net/gitroot/sbcl/sbcl.git"
 
