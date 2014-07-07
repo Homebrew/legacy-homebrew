8710840b92b7d1253ca821b3b415fe2f7f89f509
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-08 00:54:12 +0100

    makepkg: add 4.1.2 bottle.
    Closes #30685.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/makepkg.rb
+++ Library/Formula/makepkg.rb
@@ -5,6 +5,13 @@ class Makepkg < Formula
   url "ftp://ftp.archlinux.org/other/pacman/pacman-4.1.2.tar.gz"
   sha1 "ed9a40a9b532bc43e48680826d57518134132538"
 
+  bottle do
+    cellar :any
+    sha1 "e3b89d1ae8609703898902b7a847716418366c01" => :mavericks
+    sha1 "9dd3f631e927bb662671a8d6b40b2bf91d29e72e" => :mountain_lion
+    sha1 "ad5e0e8ecf0565352c9ce8ada1fd686918e04b11" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libarchive" => :build
   depends_on "bash"
