83497da39ba77b2aefedfc7b90c7ccf05d08d049
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-07 22:59:34 +0100

    libcouchbase: update 2.3.2 bottle.
    Closes #30721.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -6,9 +6,9 @@ class Libcouchbase < Formula
   sha1 'e0c885e90aa37c2fcd3a3b188fd0bd1638b58276'
 
   bottle do
-    sha1 "a77b5907f6b69ab96464a99b6249db7eafdf8c22" => :mavericks
-    sha1 "062ade4302939f40c2060775a739be1203f49226" => :mountain_lion
-    sha1 "45bd1a2709a57ec640b4f595ca54984027119ff2" => :lion
+    sha1 "5b68f33165ee3fb89323a7f35ab32860108d8c14" => :mavericks
+    sha1 "687f01a5258627f6ec9eb087f227c1a3b0a19493" => :mountain_lion
+    sha1 "dd1e96f09e6bf7d81e8c08603e064bd3d3bc1d3b" => :lion
   end
 
   option :universal
