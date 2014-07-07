98501804df5e5b558c473dae746d09adc49f541d
David Dollar <david@dollar.io>
2014-07-07 09:11:40 -0700

    forego 0.10.0

---
+++ Library/Formula/forego.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Forego < Formula
+  homepage 'https://github.com/ddollar/forego'
+  url 'https://godist.herokuapp.com/projects/ddollar/forego/releases/0.10.0/darwin-amd64/forego'
+  sha1 '1abc482cc301674a884248339aee21b45e150310'
+  head 'https://github.com/ddollar/forego.git'
+
+  def install
+    bin.install "forego"
+  end
+
+  test do
+    (testpath/"Procfile").write("web: echo hello world")
+    system "forego start"
+  end
+
+end
