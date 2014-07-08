24f8893320aedc7ea86dd47bf554b25299bce9fd
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2014-07-08 13:35:36 -0300

    git-extras 1.9.1

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GitExtras < Formula
   homepage 'https://github.com/visionmedia/git-extras'
-  url 'https://github.com/visionmedia/git-extras/archive/1.9.0.tar.gz'
-  sha1 'c4ebdeceaf4cdfde424beef4edca51aa050297f9'
+  url 'https://github.com/visionmedia/git-extras/archive/1.9.1.tar.gz'
+  sha1 '145124710f2f06f17984e335c8b840c90c43eb44'
 
   head 'https://github.com/visionmedia/git-extras.git', :branch => 'master'
 
