86b33db4e29fc42f2ffa70749930ee4a63e697f3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-08 20:24:17 +0100

    dockutil: add 2.0.0 bottle.
    Closes #29136.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/dockutil.rb
+++ Library/Formula/dockutil.rb
@@ -5,6 +5,13 @@ class Dockutil < Formula
   url "https://github.com/kcrawford/dockutil/archive/2.0.0.tar.gz"
   sha1 "8b027a0b1559f12a1c97b4a0bfcadd1566d6e6ba"
 
+  bottle do
+    cellar :any
+    sha1 "ba91d3ab1b64a8a4fd79bde869ecad702793e932" => :mavericks
+    sha1 "de9c71e038429756b4505779bd3f48e13e7384e8" => :mountain_lion
+    sha1 "9696ba0475e52f0969af8dd202290adfee28dc6a" => :lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   resource "plistlib" do
