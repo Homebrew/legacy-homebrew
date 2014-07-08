b0295fe325b994a5a4b51b2379fcf3e9d8fae494
Martin Contento <tinnet@coronam.net>
2014-07-08 15:00:46 +0200

    unpaper: apply ./configure changes suggested by @alessandro-aglietti

--- Library/Formula/unpaper.rb
+++ Library/Formula/unpaper.rb
@@ -6,8 +6,7 @@ class Unpaper < Formula
   sha1 '97068a99d47d1d65030c88d52058c1d5ff7b41d1'
 
   def install
-    system './configure'
-    system 'make'
+    system './configure', '--prefix=#{prefix}', '--disable-debug', '--disable-dependency-tracking'
     system 'make install'
   end
 end
