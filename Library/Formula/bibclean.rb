bc83650136e7cc2baccbd36135e4240a8d56b807
yrammos <yannis.rammos@me.com>
2014-07-09 21:01:43 +0400

    Remove superfluous .

--- Library/Formula/bibclean.rb
+++ Library/Formula/bibclean.rb
@@ -5,8 +5,6 @@ class Bibclean < Formula
   url "http://ftp.math.utah.edu/pub/bibclean/bibclean-2.16.tar.gz"
   sha1 "f6790cd97e7fe05abce06c991d58e96b4b53ad04"
 
-  env :std
-
   def install
     ENV.deparallelize
 
