3d6d670130ac119029dfaff56bc528df3c33fee8
yrammos <yannis.rammos@me.com>
2014-07-09 20:39:22 +0400

    bibclean 2.16 (new formula)
    Build failed with superenv, hence included `env :std`.
    `ENV.deparallelize` was also necessary.
    Two `inreplace` statements insert `mkdir` statements needed on MacOS.
    One `inreplace` statement fixes the `mandir` variable in `Makefile`,
    which is not properly defined by autoconfig despite the `--mandir`
    parameter passed to `./configure`.

---
+++ Library/Formula/bibclean.rb
@@ -0,0 +1,29 @@
+require "formula"
+
+class Bibclean < Formula
+  homepage "http://www.math.utah.edu/~beebe/software/bibtex-bibliography-tools.html"
+  url "http://ftp.math.utah.edu/pub/bibclean/bibclean-2.16.tar.gz"
+  sha1 "f6790cd97e7fe05abce06c991d58e96b4b53ad04"
+
+  env :std
+
+  def install
+    ENV.deparallelize
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+
+    inreplace "Makefile" do |s|
+      s.gsub! /[$][(]CP.*BIBCLEAN.*bindir.*BIBCLEAN[)]/, "mkdir -p $(bindir) && $(CP) $(BIBCLEAN) $(bindir)/$(BIBCLEAN)"
+      s.gsub! /[$][(]CP.*bibclean.*mandir.*bibclean.*manext[)]/, "mkdir -p $(mandir) && $(CP) bibclean.man $(mandir)/bibclean.$(manext)"
+      s.gsub! /mandir.*prefix.*man.*man1/, "mandir = $(prefix)/share/man/man1"
+    end
+
+    system "make", "all", "check", "install"
+  end
+
+  test do
+    system "bibclean"
+  end
+
+end
