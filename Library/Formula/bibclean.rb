6e53b516fa7187b2156bb59df40bcf6b94cc7d4e
yrammos <yannis.rammos@me.com>
2014-07-11 00:50:38 +0400

    Add wrapper script; fix patches.

--- Library/Formula/bibclean.rb
+++ Library/Formula/bibclean.rb
@@ -5,6 +5,14 @@ class Bibclean < Formula
   url "http://ftp.math.utah.edu/pub/bibclean/bibclean-2.16.tar.gz"
   sha1 "f6790cd97e7fe05abce06c991d58e96b4b53ad04"
 
+  # Our wrapper script.
+  def bibclean_wrapper; <<-EOS.undent
+    #!/bin/sh
+
+    export PATH=$PATH:#{share}/bibclean
+    "#{share}/bibclean/bibclean" "$@"
+    EOS
+  end
   def install
     ENV.deparallelize
 
@@ -17,15 +25,28 @@ class Bibclean < Formula
       # requires that the full path to the target already exist.
       s.gsub! /[$][(]CP.*BIBCLEAN.*bindir.*BIBCLEAN[)]/, "mkdir -p $(bindir) && $(CP) $(BIBCLEAN) $(bindir)/$(BIBCLEAN)"
       s.gsub! /[$][(]CP.*bibclean.*mandir.*bibclean.*manext[)]/, "mkdir -p $(mandir) && $(CP) bibclean.man $(mandir)/bibclean.$(manext)"
+
       # Correct `mandir` (man file path) in the Makefile.
       s.gsub! /mandir.*prefix.*man.*man1/, "mandir = $(prefix)/share/man/man1"
-      s.gsub! /install-ini.*uninstall-ini/, "install-ini:  uninstall-ini\n\tmkdir -p $(prefix)/share"
-      # Place all initialization files in $(prefix)/share/, not ./bin/ to comply with standard Unix practice.
-      s.gsub! /[$][(]bindir[)].*bibcleanrc/, "$(prefix)/share/.bibcleanrc"
-      s.gsub! /[$][(]bindir[)].*bibclean.key/, "$(prefix)/share/.bibclean.key"
-      s.gsub! /[$][(]bindir[)].*bibclean.isbn/, "$(prefix)/share/.bibclean.isbn"
+
+      # Place all initialization files in $(prefix)/bibclean/share/ instead of ./bin/ to comply with standard Unix practice.
+      s.gsub! /install-ini.*uninstall-ini/, "install-ini:  uninstall-ini\n\tmkdir -p #{share}/bibclean"
+      s.gsub! /[$][(]bindir[)].*bibcleanrc/, "#{share}/bibclean/.bibcleanrc"
+      s.gsub! /[$][(]bindir[)].*bibclean.key/, "#{share}/bibclean/.bibclean.key"
+      s.gsub! /[$][(]bindir[)].*bibclean.isbn/, "#{share}/bibclean/.bibclean.isbn"
     end
+
+    # Build the binary.
     system "make", "all", "check", "install"
+
+    # Tuck `bin/bibclean` away and create a `bibclean` wrapper script in its place.
+    # The required $PATH adjustments are effected within the script,
+    # so that successful installation does not rely on the user heeding the caveat.
+    mv "#{bin}/bibclean", "#{share}/bibclean/"
+    f = File.new("#{bin}/bibclean", "w")
+    f.write(bibclean_wrapper)
+    f.chmod(775)
+    f.close
   end
 
   test do
@@ -34,12 +55,12 @@ class Bibclean < Formula
 
   def caveats; <<-EOS.undent
     bibclean requires access to several initialization files, with defaults
-    available at #{HOMEBREW_PREFIX}/share. It is strongly recommended to append
-    this path to your $PATH prior to running bibclean:
+    available at #{HOMEBREW_PREFIX}/bibclean/share. The Homebrew build of bibclean
+    is set up to automatically discover any initialization files across
+    $PATH:#{HOMEBREW_PREFIX}/bibclean/share, therefore these defaults are active
+    out of the box without any manual adjustments to $PATH on your part.
 
-        $ export PATH=$PATH:#{HOMEBREW_PREFIX}/share
-
-    For more initialization options, please consult the bibclean man page:
+    For details and initialization options please consult the bibclean man page:
 
         $ man bibclean
     EOS
