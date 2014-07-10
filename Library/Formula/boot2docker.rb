198620835ab28e73cd83f27e48031617cc839f9c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-10 12:34:22 +0100

    boot2docker: update 1.1.1 bottle.

--- Library/Formula/boot2docker.rb
+++ Library/Formula/boot2docker.rb
@@ -6,9 +6,9 @@ class Boot2docker < Formula
   head "https://github.com/boot2docker/boot2docker-cli.git", :branch => "master"
 
   bottle do
-    sha1 "e38bc75129f5a122f6d01bae3d41a13f6d814051" => :mavericks
-    sha1 "e92a3212b05a847cdffbcd8323936196d5b8c626" => :mountain_lion
-    sha1 "aeffb40d92ff6d1aed3c977700929e103b5ad2fb" => :lion
+    sha1 "3bb9ca2e51a8df571763409eb8ee9c371199cc4d" => :mavericks
+    sha1 "79cceff57ba22ef1ddb0e2f75028f215cceb4532" => :mountain_lion
+    sha1 "9e6ee45dade5e24163506e0c55d46c26c99653c3" => :lion
   end
 
   depends_on "docker" => :recommended
