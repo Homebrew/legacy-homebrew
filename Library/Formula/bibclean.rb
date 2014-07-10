5d2c117f5f692a969c6463656a12ddae66e2deb6
yrammos <yannis.rammos@me.com>
2014-07-10 05:45:39 +0400

    Move non-executable files out of bin folder; add caveat.

--- Library/Formula/bibclean.rb
+++ Library/Formula/bibclean.rb
@@ -13,16 +13,36 @@ class Bibclean < Formula
 
     # The following inline patches have been reported upstream.
     inreplace "Makefile" do |s|
+      # Insert `mkdir` statements before `scp` statements because `scp` does not
+      # create non-existing directories in OS X.
       s.gsub! /[$][(]CP.*BIBCLEAN.*bindir.*BIBCLEAN[)]/, "mkdir -p $(bindir) && $(CP) $(BIBCLEAN) $(bindir)/$(BIBCLEAN)"
       s.gsub! /[$][(]CP.*bibclean.*mandir.*bibclean.*manext[)]/, "mkdir -p $(mandir) && $(CP) bibclean.man $(mandir)/bibclean.$(manext)"
+      # Correct the man file path as it appears in the Makefile.
       s.gsub! /mandir.*prefix.*man.*man1/, "mandir = $(prefix)/share/man/man1"
+      s.gsub! /install-ini.*uninstall-ini/, "install-ini:  uninstall-ini\n\tmkdir -p $(prefix)/share"
+      # Place all initialization files to $(prefix)/share/, not ./bin/ to comply with standard Unix practice.
+      s.gsub! /[$][(]bindir[)].*bibcleanrc/, "$(prefix)/share/.bibcleanrc"
+      s.gsub! /[$][(]bindir[)].*bibclean.key/, "$(prefix)/share/.bibclean.key"
+      s.gsub! /[$][(]bindir[)].*bibclean.isbn/, "$(prefix)/share/.bibclean.isbn"
     end
-
     system "make", "all", "check", "install"
   end
 
   test do
     system "bibclean"
+  end
+
+  def caveats; <<-EOS.undent
+    bibclean requires access to several initialization files, with defaults
+    available at #{HOMEBREW_PREFIX}/share. It is strongly recommended to append
+    this path to your $PATH prior to running bibclean:
+
+        $ export PATH=$PATH:#{HOMEBREW_PREFIX}/share
+
+    For more initialization options, please consult the bibclean man page:
+
+        $ man bibclean
+    EOS
   end
 
 end
