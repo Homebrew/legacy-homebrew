f40508fc78a62f8f8439891a08d483b54f500408
Ryan Parrish <ryan.parrish@corelogicllc.com>
2014-07-10 11:02:02 -0400

    iodine 0.7.0
    * Update to 0.7.0 which now includes fixes that previous patches solved
    * depends_on tuntap (previous version did as well, but it wasn't
    included in the formula)
    * Add a basic test (open to suggestions on how to do a better one since
    iodine requires root)

--- Library/Formula/iodine.rb
+++ Library/Formula/iodine.rb
@@ -2,16 +2,15 @@ require 'formula'
 
 class Iodine < Formula
   homepage 'http://code.kryo.se/iodine/'
-  url 'http://code.kryo.se/iodine/iodine-0.6.0-rc1.tar.gz'
-  sha1 '4fa9a248b8a84df8a727a5d749e669e58136edca'
-
-  # 10.8 compatibility; see https://github.com/yarrick/iodine/pull/1
-  patch do
-    url "https://github.com/zschoche/iodine/commit/e1e438497a83dbe6800212a0e5cb632907d1b3d9.diff"
-    sha1 "b3581c211b772a83dc051bac4b3513e0f286279b"
-  end
+  url 'http://code.kryo.se/iodine/iodine-0.7.0.tar.gz'
+  sha1 'f4c49305b6f46a547b160b3bd8c962942d701a63'
+  depends_on 'tuntap'
 
   def install
     system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{sbin}/iodine", "-v"
   end
 end
