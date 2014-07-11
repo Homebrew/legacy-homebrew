dc6433a6f2e69011510fbd1bd5cd0f5baafbafa5
Matt Hauck <matthauck@gmail.com>
2014-07-11 14:26:07 -0700

    exceptions: fix typo
    Closes #30821.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Homebrew/exceptions.rb
+++ Library/Homebrew/exceptions.rb
@@ -179,7 +179,7 @@ class BuildError < Homebrew::InstallationError
       puts
       puts "#{Tty.red}READ THIS#{Tty.reset}: #{Tty.em}#{ISSUES_URL}#{Tty.reset}"
       if formula.tap?
-        tap_issues_url = "https://github.com/#{f.tap}/issues"
+        tap_issues_url = "https://github.com/#{formula.tap}/issues"
         puts "If reporting this issue please do so at (not Homebrew/homebrew):"
         puts "  #{tap_issues_url}"
       end
