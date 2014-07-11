777089e8e7b392edd6e4f3d839cf5f93c5c29fcf
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-11 14:04:21 +0100

    entr: update 2.9 bottle.

--- Library/Formula/entr.rb
+++ Library/Formula/entr.rb
@@ -7,9 +7,9 @@ class Entr < Formula
 
   bottle do
     cellar :any
-    sha1 "b1fe6118d723b9eadbec1bab469eade0943e62ed" => :mavericks
-    sha1 "4768d9865e6b1c20839834ffec69e4697eaee32c" => :mountain_lion
-    sha1 "f7e3bcf2fc31d896f2bb6f62711bed1de2099ed7" => :lion
+    sha1 "088848efb975f72204ab9d5e74b7273b3a1c8bf8" => :mavericks
+    sha1 "31c6173d418ff64a5feb2c4a0d00ac236cde8af7" => :mountain_lion
+    sha1 "aef771a148d1417a0b5fdf2c4e1cd7b1fec89a5f" => :lion
   end
 
   def install
