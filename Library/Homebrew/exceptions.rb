a93128235eaf4af342d1e04a54381b67e77e3095
Shaun Jackman <sjackman@gmail.com>
2014-07-12 09:00:40 -0400

    Linuxbrew: Move ISSUES_URL to os from global
    Closes Homebrew/linuxbrew#103

--- Library/Homebrew/exceptions.rb
+++ Library/Homebrew/exceptions.rb
@@ -177,7 +177,7 @@ class BuildError < Homebrew::InstallationError
   def dump
     if not ARGV.verbose?
       puts
-      puts "#{Tty.red}READ THIS#{Tty.reset}: #{Tty.em}#{ISSUES_URL}#{Tty.reset}"
+      puts "#{Tty.red}READ THIS#{Tty.reset}: #{Tty.em}#{OS::ISSUES_URL}#{Tty.reset}"
       if formula.tap?
         tap_issues_url = "https://github.com/#{formula.tap}/issues"
         puts "If reporting this issue please do so at (not Homebrew/homebrew):"
