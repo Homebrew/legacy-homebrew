ce777cbd2266bfeaa95c90cf66cb99244e9c6f45
José Valim <jose.valim@plataformatec.com.br>
2014-07-12 16:34:44 +0200

    elixir 0.14.3

--- Library/Formula/elixir.rb
+++ Library/Formula/elixir.rb
@@ -26,8 +26,8 @@ end
 
 class Elixir < Formula
   homepage 'http://elixir-lang.org/'
-  url  'https://github.com/elixir-lang/elixir/archive/v0.14.2.tar.gz'
-  sha1 '1f857db32fd2975e8af64d96d9583951c3ea5e11'
+  url  'https://github.com/elixir-lang/elixir/archive/v0.14.3.tar.gz'
+  sha1 '52bace7b7aafed8c10e1a65887a1d375e404cee9'
 
   head 'https://github.com/elixir-lang/elixir.git'
 
