89cadeec201533acd6187a50131ed9347445a0fb
Maurus Cuelenaere <mcuelenaere@gmail.com>
2014-07-13 21:29:55 +0200

    Fix checking out recursive git submodules
    When nested submodules appear in a git repository, the `git submodule foreach
    git checkout-index ..` command would fail because it would checkout at the root
    directory instead of in its parent(s)' folder.
    
    Eg: root/submodule1/submodule2 would be checked out in root/submodule2

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -610,11 +610,12 @@ class GitDownloadStrategy < VCSDownloadStrategy
   end
 
   def update_submodules
-    safe_system 'git', 'submodule', 'update', '--init'
+    safe_system 'git', 'submodule', 'update', '--init', '--recursive'
   end
 
   def checkout_submodules(dst)
-    sub_cmd = "git checkout-index -a -f --prefix=#{dst}/$path/"
+    escaped_clone_path = @clone.to_s.gsub(/\//, '\/')
+    sub_cmd = "git checkout-index -a -f --prefix=#{dst}/${toplevel/#{escaped_clone_path}/}/$path/"
     safe_system 'git', 'submodule', '--quiet', 'foreach', '--recursive', sub_cmd
   end
 end
