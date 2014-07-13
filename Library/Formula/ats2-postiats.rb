32ee652a3485ac32ef0d3f98cbb4b091450fa27f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-14 00:00:21 +0100

    ats2-postiats: add 0.1.0 bottle.
    Closes #30837.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ats2-postiats.rb
+++ Library/Formula/ats2-postiats.rb
@@ -5,6 +5,13 @@ class Ats2Postiats < Formula
   url "https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.1.0/ATS2-Postiats-0.1.0.tgz"
   sha1 "7767db094f7f050edf30518866892b6cd0e2277e"
 
+  bottle do
+    cellar :any
+    sha1 "d02cf136334e7c813a8a7dd041d6b1b24f352a85" => :mavericks
+    sha1 "59786888cee7152fdaf7a27f46d98e667c313339" => :mountain_lion
+    sha1 "b544d4480ad688d9d3f1320d25c5a784a215f49d" => :lion
+  end
+
   depends_on "gmp"
 
   def install
