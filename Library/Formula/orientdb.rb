615e8dd2bcd295750161aee5c621a0de9ff42246
maggiolo00 <enricorisa@Enricos-MacBook-Pro-4.local>
2014-07-14 15:47:03 +0200

    Updated OrientDB to 1.7.5

--- Library/Formula/orientdb.rb
+++ Library/Formula/orientdb.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Orientdb < Formula
   homepage 'http://www.orientdb.org/index.htm'
-  url 'https://orient.googlecode.com/files/orientdb-graphed-1.3.0.tar.gz'
-  sha1 '124e37d2994d2908cc36fd036841b53caeed046c'
+  url 'http://www.orientdb.org/portal/function/portal/download/unknown@unknown.com/-/-/-/-/-/orientdb-community-1.7.5.tar.gz/false/false/mac'
+  version '1.7.5'
+  sha1 '203b31e2615ccedd5c1454a1859ab02cb2f68e1d'
 
-  # Fixing OrientDB init scripts
   patch do
-    url "https://gist.githubusercontent.com/leppert/5468357/raw/f4c926df7c31fe96425f2bf5feb68b52df31ebaf/homebrew.patch"
-    sha1 "1ba6c004d0695fde81053ff010f9d78f757f959b"
-  end
+     url "https://gist.githubusercontent.com/maggiolo00/84835e0b82a94fe9970a/raw/1ed577806db4411fd8b24cd90e516580218b2d53/orientdbsh"
+     sha1 "280284f3a8b6e280e46078b746f8250aa5648979"
+   end
 
   def install
     rm_rf Dir['{bin,benchmarks}/*.{bat,exe}']
