ed2463ae6190d6f7102657010a33d09e4b29c3b5
Michael Paquier <michael@otacoo.com>
2014-07-14 21:50:30 +0900

    ncmpc 0.24
    This is a bug-fix release, with as main changes:
    - Fix for a crash when using "jump"
    - Save keys to path specified by --key-file

--- Library/Formula/ncmpc.rb
+++ Library/Formula/ncmpc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ncmpc < Formula
   homepage 'http://mpd.wikia.com/wiki/Client:Ncmpc'
-  url 'http://www.musicpd.org/download/ncmpc/0/ncmpc-0.23.tar.gz'
-  sha1 '801c9ff84ee091345dd6b33127c1c09e18dac01f'
+  url 'http://www.musicpd.org/download/ncmpc/0/ncmpc-0.24.tar.gz'
+  sha1 '15677a727920afff6485c9da30a4c7a522d4630a'
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
