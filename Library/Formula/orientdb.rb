49929c4ca5d372565bec765227ebec042987e488
maggiolo00 <enricorisa@Enricos-MacBook-Pro-4.local>
2014-07-15 00:19:37 +0200

    fixed indentation for pull request

--- Library/Formula/orientdb.rb
+++ Library/Formula/orientdb.rb
@@ -6,11 +6,12 @@ class Orientdb < Formula
   version '1.7.5'
   sha1 '203b31e2615ccedd5c1454a1859ab02cb2f68e1d'
 
+  # Fixing OrientDB init scripts
   patch do
-     url "https://gist.githubusercontent.com/maggiolo00/84835e0b82a94fe9970a/raw/1ed577806db4411fd8b24cd90e516580218b2d53/orientdbsh"
-     sha1 "280284f3a8b6e280e46078b746f8250aa5648979"
-   end
-
+    url "https://gist.githubusercontent.com/maggiolo00/84835e0b82a94fe9970a/raw/1ed577806db4411fd8b24cd90e516580218b2d53/orientdbsh"
+    sha1 "280284f3a8b6e280e46078b746f8250aa5648979"
+  end
+  
   def install
     rm_rf Dir['{bin,benchmarks}/*.{bat,exe}']
 
