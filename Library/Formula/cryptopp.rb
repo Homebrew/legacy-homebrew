904c4c01f000f0191859ae6732bfb59e44c81309
subtly <subtly>
2014-07-13 18:26:48 -0600

    option to limit vector instructions when compiling on newer CPU and expecting binary to be used on older CPU.

--- Library/Formula/cryptopp.rb
+++ Library/Formula/cryptopp.rb
@@ -6,9 +6,25 @@ class Cryptopp < Formula
   sha1 'ddc18ae41c2c940317cd6efe81871686846fa293'
   version '5.6.2'
 
+  option 'asm', 'Enable ASM optimizations (ignored by -developer)'
+
+  # Mavericks and Mountain Lion support Intel Core 2 Duo processors and 1st-gen
+  # i5 CPU which do not support AVX. Clang defaults vector instructions to
+  # host-cpu, which maybe > SSSE3. Accordingly, ssse3 flag is required to
+  # build binary which supports older CPUs.
+  option 'developer', 'Vector instructions set to SSSE3 for CPU compatibility'
+
   def install
-    # patches welcome to re-enable this on configurations that support it
-    ENV.append 'CXXFLAGS', '-DCRYPTOPP_DISABLE_ASM'
+    cxxflags = ''
+    if !build.include? 'asm'
+      cxxflags = '-DCRYPTOPP_DISABLE_ASM'
+    end
+
+    if build.include? 'developer'
+      cxxflags = '-DCRYPTOPP_DISABLE_ASM -DCLANG_X86_VECTOR_INSTRUCTIONS=ssse3'
+    end
+
+    ENV.append 'CXXFLAGS', cxxflags
 
     system "make", "CXX=#{ENV.cxx}", "CXXFLAGS=#{ENV.cxxflags}"
     lib.install "libcryptopp.a"
