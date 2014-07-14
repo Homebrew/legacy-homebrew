e243d1360c18032401f05ee345dadd664558dbc8
subtly <subtly>
2014-07-13 18:37:33 -0600

    reverted/remove asm option (didn't work on ivybridge i7)

--- Library/Formula/cryptopp.rb
+++ Library/Formula/cryptopp.rb
@@ -6,8 +6,6 @@ class Cryptopp < Formula
   sha1 'ddc18ae41c2c940317cd6efe81871686846fa293'
   version '5.6.2'
 
-  option 'asm', 'Enable ASM optimizations (ignored by -developer)'
-
   # Mavericks and Mountain Lion support Intel Core 2 Duo processors and 1st-gen
   # i5 CPU which do not support AVX. Clang defaults vector instructions to
   # host-cpu, which maybe > SSSE3. Accordingly, ssse3 flag is required to
@@ -15,13 +13,11 @@ class Cryptopp < Formula
   option 'developer', 'Vector instructions set to SSSE3 for CPU compatibility'
 
   def install
-    cxxflags = ''
-    if !build.include? 'asm'
-      cxxflags = '-DCRYPTOPP_DISABLE_ASM'
-    end
-
+    # patches welcome to re-enable ASM on configurations that support it
     if build.include? 'developer'
       cxxflags = '-DCRYPTOPP_DISABLE_ASM -DCLANG_X86_VECTOR_INSTRUCTIONS=ssse3'
+    else
+      cxxflags = '-DCRYPTOPP_DISABLE_ASM'
     end
 
     ENV.append 'CXXFLAGS', cxxflags
