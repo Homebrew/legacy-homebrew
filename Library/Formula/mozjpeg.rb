3124e1760db7c8dc1fc777d909b5d8a7e5d9ffc7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-15 21:21:23 +0100

    mozjpeg: update 2.0.1 bottle.
    Closes #30884.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mozjpeg.rb
+++ Library/Formula/mozjpeg.rb
@@ -9,9 +9,9 @@ class Mozjpeg < Formula
 
   bottle do
     cellar :any
-    sha1 "77a8c981cda61d97804d155484f7ea89a0a729b3" => :mavericks
-    sha1 "bff1e31287c8e23c91d5254b20288f93e0494e2d" => :mountain_lion
-    sha1 "2438fd34c748e6b4ff39ef9f3c66a7864b10b71c" => :lion
+    sha1 "c478f25b3a628e0c643de2eb3e3771660c5a4532" => :mavericks
+    sha1 "0187b56d53c4535f47a88ba45d572a8c5472dac3" => :mountain_lion
+    sha1 "c2d7ce459b09f4ebfa1bdc035e4fddffd1d8df8f" => :lion
   end
 
   depends_on "autoconf" => :build
