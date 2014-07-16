87ab9b604aa73cf9c04321b75abd1b039ea20789
Jack Nagel <jacknagel@gmail.com>
2014-07-15 21:55:14 -0500

    Implement []= on BottleCollector

--- Library/Homebrew/test/test_bottle_collector.rb
+++ Library/Homebrew/test/test_bottle_collector.rb
@@ -7,8 +7,8 @@ class BottleCollectorTests < Homebrew::TestCase
   end
 
   def test_collector_returns_passed_tags
-    @collector.add('foo', :lion)
-    @collector.add('bar', :mountain_lion)
+    @collector[:lion] = "foo"
+    @collector[:mountain_lion] = "bar"
     assert_equal ['bar', :mountain_lion], @collector.fetch_bottle_for(:mountain_lion)
   end
 
@@ -17,19 +17,19 @@ class BottleCollectorTests < Homebrew::TestCase
   end
 
   def test_collector_finds_or_later_tags
-    @collector.add('foo', :lion_or_later)
+    @collector[:lion_or_later] = "foo"
     assert_equal ['foo', :lion_or_later], @collector.fetch_bottle_for(:mountain_lion)
     assert_nil @collector.fetch_bottle_for(:snow_leopard)
   end
 
   def test_collector_prefers_exact_matches
-    @collector.add('foo', :lion_or_later)
-    @collector.add('bar', :mountain_lion)
+    @collector[:lion_or_later] = "foo"
+    @collector[:mountain_lion] = "bar"
     assert_equal ['bar', :mountain_lion], @collector.fetch_bottle_for(:mountain_lion)
   end
 
   def test_collector_finds_altivec_tags
-    @collector.add('foo', :tiger_altivec)
+    @collector[:tiger_altivec] = "foo"
     assert_equal ['foo', :tiger_altivec], @collector.fetch_bottle_for(:tiger_g4)
     assert_equal ['foo', :tiger_altivec], @collector.fetch_bottle_for(:tiger_g4e)
     assert_equal ['foo', :tiger_altivec], @collector.fetch_bottle_for(:tiger_g5)
