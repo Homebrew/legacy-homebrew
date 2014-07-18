e71c9741e0e714de0973de52bfbd16d9a38bff0f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-18 23:20:29 +0100

    telepathy-gabble: add 0.18.3 bottle.

--- Library/Formula/telepathy-gabble.rb
+++ Library/Formula/telepathy-gabble.rb
@@ -5,6 +5,12 @@ class TelepathyGabble < Formula
   url "http://telepathy.freedesktop.org/releases/telepathy-gabble/telepathy-gabble-0.18.3.tar.gz"
   sha1 "1c71c5acf2c506788aa4b1604390f38979d88887"
 
+  bottle do
+    sha1 "39edf6b7b6392eb7ff400798bbb0d304114a55d5" => :mavericks
+    sha1 "e1727fc04c0c2510f646b2c9ab947394cd730ee2" => :mountain_lion
+    sha1 "1084ce8a5c9bf36c544bbe7105310d8410c38a37" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "sqlite"
   depends_on "telepathy-glib"
