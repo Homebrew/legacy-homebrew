8b41f6af064bfc7b0811ca236d52853990962bd1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-18 23:01:56 +0100

    cereal: add 1.0.0 bottle.

--- Library/Formula/cereal.rb
+++ Library/Formula/cereal.rb
@@ -7,6 +7,13 @@ class Cereal < Formula
 
   head "https://github.com/USCiLab/cereal.git", :branch => "develop"
 
+  bottle do
+    cellar :any
+    sha1 "6adcd84314611446fb8926455217e207a47993be" => :mavericks
+    sha1 "5d9e8b45dbe08eaad5fe3088e70036a27d7ff581" => :mountain_lion
+    sha1 "47344b6e499bf21f54f6a59a462a9ada773f8a5e" => :lion
+  end
+
   option "with-tests", "Build and run the test suite"
 
   depends_on "cmake" => :build if build.with? "tests"
