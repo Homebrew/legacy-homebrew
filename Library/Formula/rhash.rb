ca3be47a2ceab73334ee5c7bb1e581a339a9d611
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-18 06:48:15 +0100

    rhash: add 1.3.2 bottle.

--- Library/Formula/rhash.rb
+++ Library/Formula/rhash.rb
@@ -5,6 +5,13 @@ class Rhash < Formula
   url 'https://downloads.sourceforge.net/project/rhash/rhash/1.3.2/rhash-1.3.2-src.tar.gz'
   sha1 '15a997c98da1bc3628e065686866b6167951a44d'
 
+  bottle do
+    cellar :any
+    sha1 "cb5c136e11633ffb2bb28248e61ef468a724a6a5" => :mavericks
+    sha1 "25acb04254d9b202a7a016e89b28898fed48d65a" => :mountain_lion
+    sha1 "0ea791f790338589aea739b56948b126e5e75f1d" => :lion
+  end
+
   # Upstream issue: https://github.com/rhash/RHash/pull/7
   # This patch will need to be in place permanently.
   patch :DATA
