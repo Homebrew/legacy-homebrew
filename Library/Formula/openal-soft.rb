e89cc95584ae4c687d52dfb9863064d67cd01b68
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-19 01:41:05 +0100

    openal-soft: add 1.15.1 bottle.

--- Library/Formula/openal-soft.rb
+++ Library/Formula/openal-soft.rb
@@ -5,6 +5,13 @@ class OpenalSoft < Formula
   url "http://kcat.strangesoft.net/openal-releases/openal-soft-1.15.1.tar.bz2"
   sha1 "a0e73a46740c52ccbde38a3912c5b0fd72679ec8"
 
+  bottle do
+    cellar :any
+    sha1 "44f4eae635fecdc2bebfc971e1d708e660781f1b" => :mavericks
+    sha1 "d360e2481ac65b746f3d5f6b4f5c942a444da2d0" => :mountain_lion
+    sha1 "dd626bbe9640d591867709ac465652f6618d3457" => :lion
+  end
+
   depends_on "cmake" => :build
 
   def install
