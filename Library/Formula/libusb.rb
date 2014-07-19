7822180fd347ab8577e78287e2993bf20cc0f3fc
Brett Koonce <koonce@gmail.com>
2014-07-19 10:14:44 -0700

    libusb 1.0.19

--- Library/Formula/libusb.rb
+++ Library/Formula/libusb.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Libusb < Formula
-  homepage 'http://libusb.info'
-  url 'https://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.18/libusb-1.0.18.tar.bz2'
-  sha256 'c73f5cec45a5de94418da4e151b7232958571926984acfb9bce02b9424e83720'
+  homepage "http://libusb.info"
+  url "https://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.19/libusb-1.0.19.tar.bz2"
+  sha256 "6c502c816002f90d4f76050a6429c3a7e0d84204222cbff2dce95dd773ba6840"
 
   bottle do
     cellar :any
@@ -13,7 +13,7 @@ class Libusb < Formula
   end
 
   head do
-    url 'https://github.com/libusb/libusb.git'
+    url "https://github.com/libusb/libusb.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -21,14 +21,14 @@ class Libusb < Formula
   end
 
   option :universal
-  option 'no-runtime-logging', 'Build without runtime logging functionality'
-  option 'with-default-log-level-debug', 'Build with default runtime log level of debug (instead of none)'
+  option "no-runtime-logging", "Build without runtime logging functionality"
+  option "with-default-log-level-debug", "Build with default runtime log level of debug (instead of none)"
 
   def install
     ENV.universal_binary if build.universal?
 
     args = %W[--disable-dependency-tracking --prefix=#{prefix}]
-    args << "--disable-log" if build.include? 'no-runtime-logging'
+    args << "--disable-log" if build.include? "no-runtime-logging"
     args << "--enable-debug-log" if build.with? "default-log-level-debug"
 
     system "./autogen.sh" if build.head?
