f9229933504b96c766031e304d37d5c6a2edad42
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-19 03:10:55 +0100

    unpaper: add 5.1 bottle.

--- Library/Formula/unpaper.rb
+++ Library/Formula/unpaper.rb
@@ -5,6 +5,13 @@ class Unpaper < Formula
   url "https://www.flameeyes.eu/files/unpaper-5.1.tar.xz"
   sha1 "97068a99d47d1d65030c88d52058c1d5ff7b41d1"
 
+  bottle do
+    cellar :any
+    sha1 "c20298eac668f11a24cb02a61065ad58096623bb" => :mavericks
+    sha1 "dbad33b7762c589639ccf084d33776a55b777593" => :mountain_lion
+    sha1 "1f0bdc89500234a96557d99cd2bcb6cc1372594e" => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
