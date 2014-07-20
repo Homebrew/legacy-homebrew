34a766bb31266e59855eb2ce5a817af761062c4f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-20 21:51:31 +0100

    cgdb: update 0.6.7 bottle.
    Closes #30986.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/cgdb.rb
+++ Library/Formula/cgdb.rb
@@ -6,6 +6,9 @@ class Cgdb < Formula
   sha1 "5e29e306502888dd660a9dd55418e5c190ac75bb"
 
   bottle do
+    sha1 "97d618f51a59e82d00e9957e545cbf8c55430919" => :mavericks
+    sha1 "4d54ccc422b20a5d5a2bb426dab38ed6f0fbb357" => :mountain_lion
+    sha1 "3e2bdb1a3bf2e11741df63c3d13069c844208a2c" => :lion
   end
 
   depends_on "readline"
