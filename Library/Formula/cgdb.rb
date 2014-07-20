56a0231f11f4f2f1492ab7fa2254d5deea30b8cb
Adam Vandenberg <flangy@gmail.com>
2014-07-20 13:14:18 -0700

    cgdb: update style

--- Library/Formula/cgdb.rb
+++ Library/Formula/cgdb.rb
@@ -1,19 +1,17 @@
-require 'formula'
+require "formula"
 
 class Cgdb < Formula
-  homepage 'http://cgdb.github.io/'
-  url 'http://cgdb.me/files/cgdb-0.6.7.tar.gz'
-  sha1 '5e29e306502888dd660a9dd55418e5c190ac75bb'
+  homepage "http://cgdb.github.io/"
+  url "http://cgdb.me/files/cgdb-0.6.7.tar.gz"
+  sha1 "5e29e306502888dd660a9dd55418e5c190ac75bb"
 
-  depends_on 'readline'
+  bottle do
+  end
 
-  # man page for cgdb is only there to point people to the info page where all
-  # of the actual documentation is, so skip cleaning the info to preserve the
-  # documentation
-  skip_clean 'share/info'
+  depends_on "readline"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-readline=#{Formula['readline'].opt_prefix}"
     system "make install"
