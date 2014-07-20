07d937bd0078f37fe3381308146113dd0cf18ce4
Jordi Saludes <jordi.saludes@upc.edu>
2014-07-20 19:30:40 +0200

    gf: Moved depends_on after sha1

--- Library/Formula/gf.rb
+++ Library/Formula/gf.rb
@@ -3,31 +3,31 @@ require "language/haskell"
 
 class Gf < Formula
 
-    homepage "http://www.grammaticalframework.org/"
+  homepage "http://www.grammaticalframework.org/"
 
-    stable do
-        depends_on "haskell-platform"
-        url "http://www.grammaticalframework.org/download/gf-3.5.tar.gz"
-        sha1 "9c3d156df73b79c2121a46c6c29c571bfdd619c2"
-    end
+  stable do
+    url "http://www.grammaticalframework.org/download/gf-3.5.tar.gz"
+    sha1 "9c3d156df73b79c2121a46c6c29c571bfdd619c2"
+    depends_on "haskell-platform"
+  end
 
-    def install
-      system "cabal", "update"
-      system "cabal", "install",
-        "--bindir=#{bin}", "--libdir=#{lib}", "--datadir=#{share}"
-    end
+  def install
+    system "cabal", "update"
+    system "cabal", "install",
+      "--bindir=#{bin}", "--libdir=#{lib}", "--datadir=#{share}"
+  end
 
-    test  do
-        (testpath/"Hello.gf").write <<-EOS.undent
-        abstract Hello = {
-        flags startcat = Greeting ;
-        cat Greeting ; Recipient ;
-        fun
-          Hello : Recipient -> Greeting ;
-          World, Mum, Friends : Recipient ; }
-        EOS
+  test  do
+    (testpath/"Hello.gf").write <<-EOS.undent
+    abstract Hello = {
+    flags startcat = Greeting ;
+    cat Greeting ; Recipient ;
+    fun
+      Hello : Recipient -> Greeting ;
+      World, Mum, Friends : Recipient ; }
+    EOS
 
-      system "gf", "--make", testpath/"Hello.gf"
-        assert(File.exists?(testpath/"Hello.pgf"), "No pgf made")
-    end
+    system "gf", "--make", testpath/"Hello.gf"
+    assert(File.exists?(testpath/"Hello.pgf"), "No pgf made")
+  end
 end
