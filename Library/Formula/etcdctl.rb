36768629afefb934286ecbc3ce624baf50b21631
Brett Koonce <koonce@gmail.com>
2014-07-20 11:48:08 -0700

    etcdctl 0.4.5

--- Library/Formula/etcdctl.rb
+++ Library/Formula/etcdctl.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Etcdctl < Formula
-  homepage 'https://github.com/coreos/etcdctl'
-  url 'https://github.com/coreos/etcdctl/archive/v0.4.4.tar.gz'
-  sha1 '44c88abdd40ff53fd8bb64f41936806b854936fe'
-  head 'https://github.com/coreos/etcdctl.git'
+  homepage "https://github.com/coreos/etcdctl"
+  url "https://github.com/coreos/etcdctl/archive/v0.4.5.tar.gz"
+  sha1 "698704fa04d9308552e42ae7d2bd4ff15c193255"
+  head "https://github.com/coreos/etcdctl.git"
 
   bottle do
     sha1 "ee0471621da897916e0129edc72a3e427fe3a5c8" => :mavericks
@@ -12,11 +12,11 @@ class Etcdctl < Formula
     sha1 "b3191d68fb77a1f6930d4e35ea0d83f7bf87423e" => :lion
   end
 
-  depends_on 'go' => :build
+  depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
+    ENV["GOPATH"] = buildpath
     system "./build"
-    bin.install 'bin/etcdctl'
+    bin.install "bin/etcdctl"
   end
 end
