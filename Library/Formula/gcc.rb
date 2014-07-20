510f86c6722619246ae141ade1d351a11fa074aa
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-07-20 17:45:49 +0100

    gcc: 4.9.1
    Version bump gcc from 4.8.x to 4.9.1 on @jacknagel’s
    [suggestion](https://github.com/Homebrew/homebrew/pull/30872#issuecommen
    t-49511203).
    
    Following the discussion I’ve also killed the ``` HEAD ```, but if
    Mike/Jack/Adam/Misty/etc want that back in I’ll submit another commit
    to re-insert it.

--- Library/Formula/gcc.rb
+++ Library/Formula/gcc.rb
@@ -22,12 +22,9 @@ class Gcc < Formula
   end
 
   homepage "http://gcc.gnu.org"
-  url "http://ftpmirror.gnu.org/gcc/gcc-4.8.3/gcc-4.8.3.tar.bz2"
-  mirror "ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.8.3/gcc-4.8.3.tar.bz2"
-  sha1 "da0a2b9ec074f2bf624a34f3507f812ebb6e4dce"
-  revision 1
-
-  head "svn://gcc.gnu.org/svn/gcc/branches/gcc-4_8-branch"
+  url "http://ftpmirror.gnu.org/gcc/gcc-4.9.1/gcc-4.9.1.tar.bz2"
+  mirror "ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.1/gcc-4.9.1.tar.bz2"
+  sha1 "3f303f403053f0ce79530dae832811ecef91197e"
 
   bottle do
     sha1 "0e4040eb5a667ecacba2b7e7c71a7a868a593386" => :mavericks
