83dec135041e3e5bd7753582490d6655c0a5699d
Brett Koonce <koonce@gmail.com>
2014-07-20 11:48:51 -0700

    fleetctl 0.5.4

--- Library/Formula/fleetctl.rb
+++ Library/Formula/fleetctl.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Fleetctl < Formula
-  homepage 'https://github.com/coreos/fleet'
-  url 'https://github.com/coreos/fleet/archive/v0.5.0.tar.gz'
-  sha1 '90fbbba15a595e707d43f74294b82217bab57b07'
-  head 'https://github.com/coreos/fleet.git'
+  homepage "https://github.com/coreos/fleet"
+  url "https://github.com/coreos/fleet/archive/v0.5.4.tar.gz"
+  sha1 "1fec5e4d23627446bce52eae691cb233ef03e17b"
+  head "https://github.com/coreos/fleet.git"
 
   bottle do
     sha1 "8abefa5ff7b326a0e607857ed9138ea67105f66f" => :mavericks
@@ -12,11 +12,11 @@ class Fleetctl < Formula
     sha1 "8046e086e35a8beb49fab62deb4841687623cc04" => :lion
   end
 
-  depends_on 'go' => :build
+  depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
+    ENV["GOPATH"] = buildpath
     system "./build"
-    bin.install 'bin/fleetctl'
+    bin.install "bin/fleetctl"
   end
 end
