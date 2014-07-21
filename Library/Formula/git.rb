0e8349a0cb3b54d3bb97298e8b031222cba656fb
droid-Q <droid@Droid-rMBP.local>
2014-07-21 10:23:41 +0800

    git update 2.0.2

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Git < Formula
   homepage "http://git-scm.com"
-  url "https://www.kernel.org/pub/software/scm/git/git-2.0.1.tar.gz"
+  url "https://www.kernel.org/pub/software/scm/git/git-2.0.2.tar.gz"
   sha1 "794cba6b2ba2620a08651b9605bac0476804d67e"
 
   head "https://github.com/git/git.git", :shallow => false
@@ -14,12 +14,12 @@ class Git < Formula
   end
 
   resource "man" do
-    url "https://www.kernel.org/pub/software/scm/git/git-manpages-2.0.1.tar.gz"
+    url "https://www.kernel.org/pub/software/scm/git/git-manpages-2.0.2.tar.gz"
     sha1 "d4967cbe1940b7d1e131b8f0c2f609a49c569014"
   end
 
   resource "html" do
-    url "https://www.kernel.org/pub/software/scm/git/git-htmldocs-2.0.1.tar.gz"
+    url "https://www.kernel.org/pub/software/scm/git/git-htmldocs-2.0.2.tar.gz"
     sha1 "156b6ca6b626cbb6f38cbd4810c6ae69c9a4c6d5"
   end
 
