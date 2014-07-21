e9451d0a063d94fe9bac6a2daa83bb07013fbf87
Brett Koonce <koonce@gmail.com>
2014-07-20 18:55:17 -0700

    libuvc 0.0.5

--- Library/Formula/libuvc.rb
+++ Library/Formula/libuvc.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Libuvc < Formula
-  homepage 'https://github.com/ktossell/libuvc'
-  url 'https://github.com/ktossell/libuvc/archive/v0.0.3.tar.gz'
-  sha1 'bd6868772e6e4b9c52431d7731c09df013b6cf43'
+  homepage "https://github.com/ktossell/libuvc"
+  url "https://github.com/ktossell/libuvc/archive/v0.0.5.tar.gz"
+  sha1 "ab2e06d014af6aa72238113666f57405f45d1d18"
 
   bottle do
     sha1 "36d644135909d262346f434be9b470f7be13c149" => :mavericks
@@ -11,8 +11,8 @@ class Libuvc < Formula
     sha1 "3f5104157aa2c1ba31a549b3382119b5c2d0327b" => :lion
   end
 
-  depends_on 'cmake' => :build
-  depends_on 'libusb'
+  depends_on "cmake" => :build
+  depends_on "libusb"
 
   def install
     system "cmake", ".", *std_cmake_args
