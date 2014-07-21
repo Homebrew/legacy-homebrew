76e5822c10aa61cf8d640ee6ed9fba559a999900
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-21 20:20:25 +0100

    gauche: add 0.9.4 bottle.
    Closes #31021.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gauche.rb
+++ Library/Formula/gauche.rb
@@ -5,6 +5,12 @@ class Gauche < Formula
   url 'https://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.4.tgz'
   sha1 '2f0068d19adbc8e7fd3c04ab8e6576d0fac21ad6'
 
+  bottle do
+    sha1 "844ce90625ae0fd6ab27afc965edc7c05e6d283d" => :mavericks
+    sha1 "e039cf0ff8ab34db5053fe2a010822ec3205b5e3" => :mountain_lion
+    sha1 "087b1f85a18485fe2226892d9b96066a027de606" => :lion
+  end
+
   def install
     system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking',
                           '--enable-multibyte=utf-8'
