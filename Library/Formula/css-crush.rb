32ef35d16d470f6b3fc82955d17fe7286307a003
Brett Koonce <koonce@gmail.com>
2014-07-20 19:31:36 -0700

    css-crush 2.2.1

--- Library/Formula/css-crush.rb
+++ Library/Formula/css-crush.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class CssCrush < Formula
-  homepage 'http://the-echoplex.net/csscrush'
-  url 'https://github.com/peteboere/css-crush/archive/v2.1.0.tar.gz'
-  sha1 '3285917dce69420e2822c59e5c25401aaaa5ea64'
+  homepage "http://the-echoplex.net/csscrush"
+  url "https://github.com/peteboere/css-crush/archive/v2.2.1.tar.gz"
+  sha1 "39c721af2765f36d4a2990fe365fb18c3ee41485"
 
   def install
-    libexec.install Dir['*']
-    (bin+'csscrush').write <<-EOS.undent
+    libexec.install Dir["*"]
+    (bin+"csscrush").write <<-EOS.undent
       #!/bin/sh
       php "#{libexec}/cli.php" "$@"
     EOS
