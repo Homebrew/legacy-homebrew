f02bc63a28d0dd1464710f020ea129942c3fb686
Brett Koonce <koonce@gmail.com>
2014-07-20 18:53:06 -0700

    libbson 0.98.0

--- Library/Formula/libbson.rb
+++ Library/Formula/libbson.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Libbson < Formula
   homepage "https://github.com/mongodb/libbson"
-  url "https://github.com/mongodb/libbson/releases/download/0.8.4/libbson-0.8.4.tar.gz"
-  sha1 "698d68defaec7ed67d4f17090ae183aaa47a21df"
+  url "https://github.com/mongodb/libbson/releases/download/0.98.0/libbson-0.98.0.tar.gz"
+  sha1 "3e80019b8896669dc84781fe105438a3ccd2f483"
 
   bottle do
     cellar :any
