f709ac13ac2a746d6f7300de77ec2e10b121bcf0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-21 20:33:07 +0100

    gst-plugins-ugly: add 1.4.0 bottle.
    Closes #31024.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gst-plugins-ugly.rb
+++ Library/Formula/gst-plugins-ugly.rb
@@ -6,6 +6,12 @@ class GstPluginsUgly < Formula
   mirror 'http://ftp.osuosl.org/pub/blfs/svn/g/gst-plugins-ugly-1.4.0.tar.xz'
   sha256 '5314bb60f13d1a7b9c6317df73813af5f3f15a62c7c186b816b0024b5c61744d'
 
+  bottle do
+    sha1 "a82da0b9a9d26d0e5d93c51c02b1855139210a50" => :mavericks
+    sha1 "269dcf70921dc902d887cbaf6165a66eecc4d703" => :mountain_lion
+    sha1 "38d4ff9a96a665496b72e0dcd99a1536b7489d41" => :lion
+  end
+
   head do
     url 'git://anongit.freedesktop.org/gstreamer/gst-plugins-ugly'
 
