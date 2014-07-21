f7c17188487a62b29608a232c7116ed054d3f952
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-07-21 14:52:11 -0400

    gauche 0.9.4
    Q.v. http://practical-scheme.net/gauche/gmemo/?Release%200.9.4

--- Library/Formula/gauche.rb
+++ Library/Formula/gauche.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Gauche < Formula
   homepage 'http://practical-scheme.net/gauche/'
-  url 'https://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.3.3.tgz'
-  sha1 '71d7ca3eceb9adc1de33455c1616cbed89d226f7'
+  url 'https://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.4.tgz'
+  sha1 '2f0068d19adbc8e7fd3c04ab8e6576d0fac21ad6'
 
   def install
     system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking',
