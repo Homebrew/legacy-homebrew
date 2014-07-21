70225c0eee18a6ceee9ab311ecf329be8fdcd6d3
Brett Koonce <koonce@gmail.com>
2014-07-20 19:24:10 -0700

    daemonize 1.7.5

--- Library/Formula/daemonize.rb
+++ Library/Formula/daemonize.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Daemonize < Formula
-  homepage 'http://software.clapper.org/daemonize/'
-  url 'https://github.com/bmc/daemonize/archive/release-1.7.4.tar.gz'
-  sha1 '25465f696108f9e112039c9363aff8c798191789'
+  homepage "http://software.clapper.org/daemonize/"
+  url "https://github.com/bmc/daemonize/archive/release-1.7.5.tar.gz"
+  sha1 "aa0edb1a71a898df0167c2bd426e76c187e19b49"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
