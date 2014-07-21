12dea114fc9300f6a0f714f71f5d4deb1275efba
Brett Koonce <koonce@gmail.com>
2014-07-20 19:22:53 -0700

    coremod 4.2.8

--- Library/Formula/coremod.rb
+++ Library/Formula/coremod.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Coremod < Formula
   homepage "http://xmp.sourceforge.net"
-  url "https://github.com/cmatsuoka/coremod/archive/coremod-4.2.7.tar.gz"
-  sha1 "228ed11a202476aa09ff1ba7b5c4148982a96668"
+  url "https://github.com/cmatsuoka/coremod/archive/coremod-4.2.8.tar.gz"
+  sha1 "583cb3bdc6df583739c5b44a4961ba364e0b0ecd"
 
   head "https://github.com/cmatsuoka/coremod.git"
 
