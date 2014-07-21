d787e5e2c20a653f334248b62f8888daee01f70d
Brett Koonce <koonce@gmail.com>
2014-07-20 18:54:35 -0700

    libsodium 0.6.1

--- Library/Formula/libsodium.rb
+++ Library/Formula/libsodium.rb
@@ -2,9 +2,8 @@ require "formula"
 
 class Libsodium < Formula
   homepage "https://github.com/jedisct1/libsodium/"
-  url "https://github.com/jedisct1/libsodium/releases/download/0.6.0/libsodium-0.6.0.tar.gz"
-  sha256 "84cdb6bf8ae3384f3ef78636f93bc689df748c1d36f87d4b6ab1e31c2d4dd145"
-  revision 1
+  url "https://github.com/jedisct1/libsodium/releases/download/0.6.1/libsodium-0.6.1.tar.gz"
+  sha256 "04ccfeebd23659f3a1f73a828d1b88b984f9c984176f388daf77fe3968cc3694"
 
   bottle do
     cellar :any
@@ -12,9 +11,6 @@ class Libsodium < Formula
     sha1 "34e1cb906d01591db753b44f913bff23d6f24e8c" => :mountain_lion
     sha1 "68d36857985a73fef90caf6e734b5107ba192ea8" => :lion
   end
-
-  # Required to generate the .pc file, but can be removed at the next release
-  depends_on "pkg-config" => :build
 
   head do
     url "https://github.com/jedisct1/libsodium.git"
