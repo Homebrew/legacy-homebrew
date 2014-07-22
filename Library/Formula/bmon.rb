4ba641c851b13575b56f36e786f2d084ec937c62
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-22 07:22:31 +0100

    bmon: add 3.3 bottle.
    Closes #31027.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/bmon.rb
+++ Library/Formula/bmon.rb
@@ -6,6 +6,12 @@ class Bmon < Formula
   url "https://github.com/tgraf/bmon/releases/download/v3.3/bmon-3.3.tar.gz"
   sha1 "f21d7fc70fdc140680f7ef466242d876474ed2aa"
 
+  bottle do
+    sha1 "a5a4f7baad07a9c86ccb0a84b6404cbf9753f204" => :mavericks
+    sha1 "fe23553876a3699ce122e9b4f6ef30ca807104df" => :mountain_lion
+    sha1 "a926a43c20a7f774365d9f27a8d059b1507d709c" => :lion
+  end
+
   head do
     url "https://github.com/tgraf/bmon.git"
     depends_on "autoconf" => :build
