a5df81ee8e0de0906fcd422ddcc7047f25ea55c7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-22 14:10:13 +0100

    hydra-cli: add 1.0 bottle.
    Closes #31031.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hydra-cli.rb
+++ Library/Formula/hydra-cli.rb
@@ -6,6 +6,13 @@ class HydraCli < Formula
   sha1 '15906ca4255839844635f26eab83843f0426b5f9'
   head 'https://github.com/sdegutis/hydra-cli.git'
 
+  bottle do
+    cellar :any
+    sha1 "5b23c9395fb73f77463922c2021350bbca8dd2e1" => :mavericks
+    sha1 "7f645a2a45f2b9cb578e8c117281708b3cfdf83f" => :mountain_lion
+    sha1 "0153b1fce7c2ea3e1726320d53fe2fccea574ef4" => :lion
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
