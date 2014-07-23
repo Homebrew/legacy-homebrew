509885fa00998b6964c349a1d0594f03e2192f5f
Mike Naberezny <mike@naberezny.com>
2014-07-23 14:00:37 -0700

    ume 0.154

--- Library/Formula/ume.rb
+++ Library/Formula/ume.rb
@@ -2,17 +2,15 @@ require 'formula'
 
 class Ume < Formula
   homepage 'http://mamedev.org/'
-  url 'svn://dspnet.fr/mame/trunk', :revision => '29406'
-  version '0.153'
+  url 'svn://dspnet.fr/mame/trunk', :revision => '31397'
+  version '0.154'
 
   head 'svn://dspnet.fr/mame/trunk'
 
-  depends_on :x11
   depends_on 'sdl'
 
   def install
     ENV['MACOSX_USE_LIBSDL'] = '1'
-    ENV['INCPATH'] = "-I#{MacOS::X11.include}"
     ENV['PTR64'] = (MacOS.prefer_64_bit? ? '1' : '0')
 
     system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}", "TARGET=ume"
