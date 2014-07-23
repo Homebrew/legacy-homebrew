2005baef0e2ceafc782ade9d70559d767fbfc4a0
Svend Vanderveken <svend.vanderveken@gmail.com>
2014-07-23 15:10:44 +0200

    adapted start/stop scripts based on pid file + documented usage of spark.executor.uri in local.conf.template

--- bin/server_start.sh
+++ bin/server_start.sh
@@ -45,6 +45,11 @@ fi
 # Pull in other env vars in spark config, such as MESOS_NATIVE_LIBRARY
 . $SPARK_HOME/conf/spark-env.sh
 
+if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE"); then
+   echo 'Job server is already running'
+   exit 1
+fi
+
 if [ -z "$LOG_DIR" ]; then
   echo "LOG_DIR empty; logging will go to /tmp/job-server"
   LOG_DIR=/tmp/job-server
