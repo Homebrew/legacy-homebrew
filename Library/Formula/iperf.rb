d2b895b9b5ffb980902891573170aa156ee6075f
Jack Nagel <jacknagel@gmail.com>
2014-07-22 22:04:11 -0500

    Revert "iperf: add 3.0.5 bottle."
    This reverts commit e65de23461488b19dc62a4dbd59739a16f219fd8.

--- Library/Formula/iperf.rb
+++ Library/Formula/iperf.rb
@@ -5,13 +5,6 @@ class Iperf < Formula
   url 'https://downloads.sourceforge.net/project/iperf/iperf-2.0.5.tar.gz'
   sha1 '7302792dcb1bd7aeba032fef6d3dcc310e4d113f'
 
-  bottle do
-    cellar :any
-    sha1 "ce3cc45d64a0be38f489d96bb9c816e8ef1ded77" => :mavericks
-    sha1 "831b48b0a1eb3822323e637bfb29df1da31df7ff" => :mountain_lion
-    sha1 "a11c989cfaaa79438ca6c77ec9d038a139562692" => :lion
-  end
-
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
