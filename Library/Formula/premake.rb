c2d07273f9e52713fc47948fb4389bdc9144989d
Lucas Berezy <lberezy@users.noreply.github.com>
2014-07-23 23:24:57 +1000

    premake 4.4-beta5 (devel)

--- Library/Formula/premake.rb
+++ Library/Formula/premake.rb
@@ -1,13 +1,20 @@
-require 'formula'
+require "formula"
 
 class Premake < Formula
-  homepage 'http://industriousone.com/premake'
-  url 'https://downloads.sourceforge.net/project/premake/Premake/4.3/premake-4.3-src.zip'
-  sha1 '8f37a3599121580f18b578811162b9b49a2e122f'
+  homepage "http://industriousone.com/premake"
+  url "https://downloads.sourceforge.net/project/premake/Premake/4.3/premake-4.3-src.zip"
+  sha1 "8f37a3599121580f18b578811162b9b49a2e122f"
+
+  devel do
+    url "https://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip"
+    sha1 "02472d4304ed9ff66cde57038c17fbd42a159028"
+  end
 
   def install
-    # Linking against stdc++-static causes a library not found error on 10.7
-    inreplace 'build/gmake.macosx/Premake4.make', '-lstdc++-static ', ''
+    unless build.devel?
+      # Linking against stdc++-static causes a library not found error on 10.7
+      inreplace "build/gmake.macosx/Premake4.make", "-lstdc++-static ", ""
+    end
     system "make -C build/gmake.macosx"
 
     # Premake has no install target, but its just a single file that is needed
