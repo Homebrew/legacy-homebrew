b3049caa9586afcf962836c44920b4065638c519
Kentaro Wada <www.kentaro.wada@gmail.com>
2014-07-24 18:18:29 +0900

    Typing application TRR. modified the installation method not to use 'sudo'

--- Library/Formula/apel.rb
+++ Library/Formula/apel.rb
@@ -4,10 +4,11 @@ class Apel < Formula
   homepage "http://git.chise.org/elisp/apel/"
   url "http://git.chise.org/elisp/dist/apel/apel-10.8.tar.gz"
   sha1 "089c18ae006df093aa2edaeb486bfaead6ac4918"
+  version "10.8"
 
   def install
-    system "make"
-    system "sudo", "make", "install"
+    system "make LISPDIR=\/usr\/local\/share\/emacs\/site-lisp VERSION_SPECIFIC_LISPDIR=\/usr\/local\/share\/emacs\/site-lisp"
+    system "make install LISPDIR=\/usr\/local\/share\/emacs\/site-lisp VERSION_SPECIFIC_LISPDIR=\/usr\/local\/share\/emacs\/site-lisp"
   end
 
 end
