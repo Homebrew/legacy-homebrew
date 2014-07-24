4ea667cc4f45fdc9c6101833810ba838b8531e0c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-24 08:36:23 +0100

    premake: add 4.3 bottle.
    Closes #31064.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/premake.rb
+++ Library/Formula/premake.rb
@@ -5,6 +5,13 @@ class Premake < Formula
   url "https://downloads.sourceforge.net/project/premake/Premake/4.3/premake-4.3-src.zip"
   sha1 "8f37a3599121580f18b578811162b9b49a2e122f"
 
+  bottle do
+    cellar :any
+    sha1 "54ccb106b6abf6c1c76a776cffa82d671ab940a2" => :mavericks
+    sha1 "960b64d517b1290608acc950058a71f9e984ad79" => :mountain_lion
+    sha1 "271358004f8cd52160bfd45d9e58a59bc3fcb75d" => :lion
+  end
+
   devel do
     url "https://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip"
     sha1 "02472d4304ed9ff66cde57038c17fbd42a159028"
