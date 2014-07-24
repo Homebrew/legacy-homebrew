314683557c82537ff652e30149d751daa0c67f6d
Pierre Pagnoux <pagnou_p@epita.fr>
2014-07-24 17:48:19 +0200

    ocamlsdl
    Fixed wrong URL on sourceforge (but do not pass brew audit command).

--- Library/Formula/ocamlsdl.rb
+++ Library/Formula/ocamlsdl.rb
@@ -2,9 +2,7 @@ require "formula"
 
 class Ocamlsdl < Formula
   homepage "http://ocamlsdl.sourceforge.net/"
-  # URL that has been tested:
-  # "http://sourceforge.net/projects/ocamlsdl/files/OCamlSDL/ocamlsdl-0.9.1/ocamlsdl-0.9.1.tar.gz"
-  url "https://downloads.sourceforge.net/projects/ocamlsdl/files/OCamlSDL/ocamlsdl-0.9.1/ocamlsdl-0.9.1.tar.gz"
+  url "http://sourceforge.net/projects/ocamlsdl/files/OCamlSDL/ocamlsdl-0.9.1/ocamlsdl-0.9.1.tar.gz"
   sha1 "2e54f8984b06cede493c3ad29006dde17077a79a"
 
   depends_on "sdl"
@@ -13,7 +11,6 @@ class Ocamlsdl < Formula
   depends_on "sdl_gfx" => :recommended
   depends_on "sdl_ttf" => :recommended
   depends_on "objective-caml"
-
 
   def install
     system "./configure", "--prefix=#{prefix}"
