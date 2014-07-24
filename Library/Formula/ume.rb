e337fd222c3e31919c136624f4349ea59b9a2c14
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-24 06:52:33 +0100

    ume: add 0.154 bottle.
    Closes #31073.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ume.rb
+++ Library/Formula/ume.rb
@@ -7,6 +7,13 @@ class Ume < Formula
 
   head 'svn://dspnet.fr/mame/trunk'
 
+  bottle do
+    cellar :any
+    sha1 "9355c003f65ff30d16dbdd304a340bcb5fb81b61" => :mavericks
+    sha1 "d88f548400574c40e2df081e50ec37c1381edd07" => :mountain_lion
+    sha1 "4a8c91568ef915b0cad541e7b8db71c3545808ef" => :lion
+  end
+
   depends_on 'sdl'
 
   def install
