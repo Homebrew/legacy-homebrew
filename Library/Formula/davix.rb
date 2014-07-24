5b2cf6d2c90e435b8efdb4382edc20b6edb8071e
Jack Nagel <jacknagel@gmail.com>
2014-07-23 21:03:09 -0500

    davix: hoist stable block

--- Library/Formula/davix.rb
+++ Library/Formula/davix.rb
@@ -2,6 +2,9 @@ require "formula"
 
 class Davix < Formula
   homepage "http://dmc.web.cern.ch/projects/davix/home"
+  url "https://git.cern.ch/pub/davix.git", :tag => "R_0_3_4"
+  version "0.3.4"
+
   head "https://git.cern.ch/pub/davix.git"
 
   bottle do
@@ -9,11 +12,6 @@ class Davix < Formula
     sha1 "26b49cc9cc80dfcd4afe3e7cc2ee2621a18e1270" => :mavericks
     sha1 "ecbe105f2e286df23107daf69aa2e1d2ad58b5dd" => :mountain_lion
     sha1 "3b374d8e9dd79c8dbe59d94d8226ef93778e5325" => :lion
-  end
-
-  stable do
-    url "https://git.cern.ch/pub/davix.git", :tag => "R_0_3_4"
-    version "0.3.4"
   end
 
   depends_on "cmake" => :build
