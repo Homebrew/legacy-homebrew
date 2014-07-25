da7bdd3479bb88b315490a7a8330611b81b8ce5b
aleksijuvani <aleksi.juvani@gmail.com>
2014-07-25 20:38:41 +0300

    premake: add HEAD version

--- Library/Formula/premake.rb
+++ Library/Formula/premake.rb
@@ -17,14 +17,48 @@ class Premake < Formula
     sha1 "02472d4304ed9ff66cde57038c17fbd42a159028"
   end
 
-  def install
-    unless build.devel?
-      # Linking against stdc++-static causes a library not found error on 10.7
-      inreplace "build/gmake.macosx/Premake4.make", "-lstdc++-static ", ""
-    end
-    system "make -C build/gmake.macosx"
+  head do
+    url "https://bitbucket.org/premake/premake-dev", :using => :hg
 
-    # Premake has no install target, but its just a single file that is needed
-    bin.install "bin/release/premake4"
+    # An existing premake installation is required to generate the Makefiles
+    resource "premake" do
+      url "https://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip"
+      sha1 "02472d4304ed9ff66cde57038c17fbd42a159028"
+    end
+  end
+
+  def install
+    if build.stable? or build.devel?
+      unless build.devel?
+        # Linking against stdc++-static causes a library not found error on 10.7
+        inreplace "build/gmake.macosx/Premake4.make", "-lstdc++-static ", ""
+      end
+
+      system "make -C build/gmake.macosx"
+
+      # Premake has no install target, but its just a single file that is needed
+      bin.install "bin/release/premake4"
+    end
+
+    if build.head?
+      build_directory = Dir.pwd
+
+      # Build an older version first
+      resource("premake").stage do
+        system "make -C build/gmake.macosx"
+
+        # And use it to generate the Makefiles for the HEAD version
+        premake_directory = Dir.pwd
+
+        Dir.chdir(build_directory) do
+          system File.join(premake_directory, "bin/release/premake4"), "embed"
+          system File.join(premake_directory, "bin/release/premake4"), "gmake"
+        end
+      end
+
+      system "make"
+
+      bin.install "bin/release/premake5"
+    end
   end
 end
