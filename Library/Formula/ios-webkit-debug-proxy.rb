a8a8eee56369c9b87fad03077e2296aa5022a153
Todd Wright <wrightt@google.com>
2014-07-25 17:53:42 -0400

    Restored built-in reqs.

--- Library/Formula/ios-webkit-debug-proxy.rb
+++ Library/Formula/ios-webkit-debug-proxy.rb
@@ -1,23 +1,13 @@
 require 'formula'
 
-class LionOrNewer < Requirement
-  fatal true
-
-  satisfy MacOS.version >= :lion
-
-  def message
-    "ios-webkit-debug-proxy requires Mac OS X 10.7 (Lion) or newer."
-  end
-end
-
 class IosWebkitDebugProxy < Formula
   homepage 'https://github.com/google/ios-webkit-debug-proxy'
   url 'https://github.com/google/ios-webkit-debug-proxy/archive/1.4.tar.gz'
   sha1 'e6d882182fe2fd8f5827a9289545cc7e9ebb25e7'
 
-  depends_on LionOrNewer
-  depends_on :autoconf => :build
-  depends_on :automake => :build
+  depends_on :macos => :lion
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on 'libplist'
   depends_on 'usbmuxd'
   depends_on 'libimobiledevice'
