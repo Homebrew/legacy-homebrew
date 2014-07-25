72a4786536d90a1d233d93f975ba71ecc78f2603
Laurent Arnoud <laurent@spkdev.net>
2014-07-25 13:30:34 +0200

    Use double quotes everywhere

--- Library/Formula/hachoir-metadata.rb
+++ Library/Formula/hachoir-metadata.rb
@@ -7,23 +7,23 @@ class HachoirMetadata < Formula
 
   depends_on :python if MacOS.version <= :snow_leopard
 
-  resource 'hachoir-core' do
+  resource "hachoir-core" do
     url "http://cheeseshop.python.org/packages/source/h/hachoir-core/hachoir-core-1.3.3.tar.gz"
     sha1 "e1d3b5da7d57087c922942b7653cb3b195c7769f"
   end
-  resource 'hachoir-parser' do
+  resource "hachoir-parser" do
     url "http://cheeseshop.python.org/packages/source/h/hachoir-parser/hachoir-parser-1.3.4.tar.gz"
     sha1 "8433e1598b1e8d9404e6978117a203775e68c075"
   end
-  resource 'hachoir-regex' do
+  resource "hachoir-regex" do
     url "http://cheeseshop.python.org/packages/source/h/hachoir-regex/hachoir-regex-1.0.5.tar.gz"
     sha1 "98a3c7e8922f926fdb6c1dec92e093d75712eb3b"
   end
 
   def install
     ENV["PYTHONPATH"] = lib+"python2.7/site-packages"
-    ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
-    ENV.prepend_create_path 'PYTHONPATH', prefix+'lib/python2.7/site-packages'
+    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", prefix+"lib/python2.7/site-packages"
 
     install_args = [ "setup.py", "install", "--prefix=#{libexec}" ]
     res = %w[hachoir-core hachoir-parser hachoir-regex]
@@ -32,7 +32,7 @@ class HachoirMetadata < Formula
     end
 
     system "python", "setup.py", "install", "--prefix=#{prefix}"
-    bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV['PYTHONPATH'])
+    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
