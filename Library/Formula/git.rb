35f027f4bcbb18eb8c5e2b8fdaf851392b705507
droid-Q <droid@Droid-rMBP.local>
2014-07-25 11:27:00 +0800

    git2.0.2 update to git 2.0.3

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Git < Formula
   homepage "http://git-scm.com"
-  url "https://www.kernel.org/pub/software/scm/git/git-2.0.2.tar.gz"
-  sha1 "794cba6b2ba2620a08651b9605bac0476804d67e"
+  url "https://www.kernel.org/pub/software/scm/git/git-2.0.3.tar.gz"
+  sha1 "e580c17045157ccced5c49a6f26aa298a291a89d"
 
   head "https://github.com/git/git.git", :shallow => false
 
@@ -14,13 +14,13 @@ class Git < Formula
   end
 
   resource "man" do
-    url "https://www.kernel.org/pub/software/scm/git/git-manpages-2.0.2.tar.gz"
-    sha1 "d4967cbe1940b7d1e131b8f0c2f609a49c569014"
+    url "https://www.kernel.org/pub/software/scm/git/git-manpages-2.0.3.tar.gz"
+    sha1 "e7ab2fbcb98a3a58cb96a900b9283574f968e6c7"
   end
 
   resource "html" do
-    url "https://www.kernel.org/pub/software/scm/git/git-htmldocs-2.0.2.tar.gz"
-    sha1 "156b6ca6b626cbb6f38cbd4810c6ae69c9a4c6d5"
+    url "https://www.kernel.org/pub/software/scm/git/git-htmldocs-2.0.3.tar.gz"
+    sha1 "736740e95a72a070ae1edf3644665914ced1495c"
   end
 
   option 'with-blk-sha1', 'Compile with the block-optimized SHA1 implementation'
