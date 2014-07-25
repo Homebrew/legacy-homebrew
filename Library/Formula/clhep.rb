f37281622cf7b29daa066b327fe66bffb8ade8ad
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-25 03:07:42 +0100

    clhep: add 2.2.0.1 bottle.
    Closes #31104.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/clhep.rb
+++ Library/Formula/clhep.rb
@@ -5,6 +5,13 @@ class Clhep < Formula
   url "http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.2.0.1.tgz"
   sha1 "88b9d4f39be3ac317e63c2f7ad694ede6e7b7520"
 
+  bottle do
+    cellar :any
+    sha1 "f5f803b03710e3acf529eab111428570e536ebb6" => :mavericks
+    sha1 "610b51552b3864eb26c75b2d242d43c81187d770" => :mountain_lion
+    sha1 "36b000d7ecf82b8c528b1bd2a02d58c39beab1bf" => :lion
+  end
+
   depends_on "cmake" => :build
 
   def install
