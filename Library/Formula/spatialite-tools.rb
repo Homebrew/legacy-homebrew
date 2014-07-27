7c6210eefbb1956eb0087955045fc9dd67388e1b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-27 17:24:32 +0100

    spatialite-tools: add 4.2.0 bottle.
    Closes #31132.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/spatialite-tools.rb
+++ Library/Formula/spatialite-tools.rb
@@ -5,6 +5,13 @@ class SpatialiteTools < Formula
   url "http://www.gaia-gis.it/gaia-sins/spatialite-tools-4.2.0.tar.gz"
   sha1 "de07042afa734b17c840318fa8772466f53f1831"
 
+  bottle do
+    cellar :any
+    sha1 "37e91a6eb19748f4dc385f442b45b35012deb0f7" => :mavericks
+    sha1 "0390c9073ed99007be27114256b9a412592555ab" => :mountain_lion
+    sha1 "f118f37a57fa181b3295653f67bc084e64270d75" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libspatialite'
   depends_on 'readosm'
