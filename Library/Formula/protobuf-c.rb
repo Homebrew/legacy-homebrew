62af75952369b83c4461e569ee8d8c99661a092e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-27 03:20:13 +0100

    protobuf-c: add 1.0.0 bottle.
    Closes #31155.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/protobuf-c.rb
+++ Library/Formula/protobuf-c.rb
@@ -5,6 +5,12 @@ class ProtobufC < Formula
   url 'https://github.com/protobuf-c/protobuf-c/releases/download/v1.0.0/protobuf-c-1.0.0.tar.gz'
   sha1 '6d48eb6a193556262c35526e1ccf209a6fc69684'
 
+  bottle do
+    sha1 "46572020e49936035b3f35194c40f98d90fd8dcd" => :mavericks
+    sha1 "5fe4ec31d10b064fc580ae5958873ca038083a80" => :mountain_lion
+    sha1 "64e31c97b2ef834d6406d128892575706b17cbad" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'protobuf'
 
