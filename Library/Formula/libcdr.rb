1217ffe57774b1d6b9d6380655ab57090adef5fe
luzpaz <luzpaz@users.noreply.github.com>
2014-07-27 13:39:31 -0500

    Update libcdr.rb
    removed trailing whitespaces as per brew audit

--- Library/Formula/libcdr.rb
+++ Library/Formula/libcdr.rb
@@ -1,26 +1,26 @@
 require "formula"
- 
+
 class Libcdr < Formula
   homepage "https://wiki.documentfoundation.org/DLP/Libraries/libcdr"
   url "http://dev-www.libreoffice.org/src/libcdr/libcdr-0.1.0.tar.bz2"
   sha1 "fea63690acea2b9eac5dc6f51232154b7bb41a9e"
- 
+
   #bottle do
   #  cellar :any
   #  sha1 "" => :mavericks
   #  sha1 "" => :mountain_lion
   #  sha1 "" => :lion
   #end
- 
+
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
   depends_on "icu4c"
   depends_on "librevenge"
   depends_on "little-cms2"
   depends_on 'homebrew/dupes/zlib'
- 
+
   needs :cxx11
- 
+
   def install
     ENV.libcxx
     ENV.cxx11
@@ -28,7 +28,7 @@ class Libcdr < Formula
                           "--without-docs"
     system "make", "install"
   end
- 
+
   # test do
   #  (testpath/"test.cpp").write <<-EOS.undent
   #  #include <librevenge-stream/librevenge-stream.h>
