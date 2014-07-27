4d3432f35a8120262571c911d37026de301415d4
Fred Klassen <fklassen@appneta.com>
2014-07-27 12:16:09 -0700

    tcpreplay: point to the new maintainer's home page
    Closes #31165.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/tcpreplay.rb
+++ Library/Formula/tcpreplay.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Tcpreplay < Formula
-  homepage "http://tcpreplay.synfin.net"
+  homepage "http://tcpreplay.appneta.com"
   url "https://downloads.sourceforge.net/project/tcpreplay/tcpreplay/4.0.4/tcpreplay-4.0.4.tar.gz"
   sha1 "f2f266508b06693852bbc5003b20b18da4a1f01b"
 
