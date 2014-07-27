42737795a281be42fde27447ceccc50893e3336d
Austin <asmacdo@gmail.com>
2014-07-27 17:41:18 -0400

    more robust test

--- Library/Formula/doitlive.rb
+++ Library/Formula/doitlive.rb
@@ -8,14 +8,14 @@ class Doitlive < Formula
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
+    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
     system "python", "setup.py", "install", "--prefix=#{libexec}"
 
-    bin.install Dir[libexec/'bin/*']
-    bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV['PYTHONPATH'])
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
-    system "#{bin}/doitlive", "--help"
+    system "#{bin}/doitlive", "themes", "--preview"
   end
 end
