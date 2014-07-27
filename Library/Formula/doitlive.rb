3acd4fd442d2c7e64707247329d9a17d09d3e1b2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-27 22:50:02 +0100

    doitlive: add 2.2.0 bottle.
    Closes #31163.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/doitlive.rb
+++ Library/Formula/doitlive.rb
@@ -5,6 +5,13 @@ class Doitlive < Formula
   url "https://pypi.python.org/packages/source/d/doitlive/doitlive-2.2.0.tar.gz"
   sha1 "00d9f4fa2e25cc6ac14d6d9a1e14c9214c29ff48"
 
+  bottle do
+    cellar :any
+    sha1 "15cee0e3795dc9fe364c21f38e2a79e0adf7fbd8" => :mavericks
+    sha1 "fe98393027e48946b9e9e40fe0f8d5c2d4494ecf" => :mountain_lion
+    sha1 "f8fb5e520dc2287095285d2ceb1fad5498db5a4a" => :lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
