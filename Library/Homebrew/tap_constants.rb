184e4d548d34632c54d82945158103f09503129a
Jack Nagel <jacknagel@gmail.com>
2014-07-26 21:02:23 -0500

    A period is a valid formula name character
    Fixes #31052.

--- Library/Homebrew/tap_constants.rb
+++ Library/Homebrew/tap_constants.rb
@@ -1,7 +1,7 @@
 # match expressions when taps are given as ARGS, e.g. someuser/sometap
 HOMEBREW_TAP_ARGS_REGEX = %r{^([\w-]+)/(homebrew-)?([\w-]+)$}
 # match taps' formula, e.g. someuser/sometap/someformula
-HOMEBREW_TAP_FORMULA_REGEX = %r{^([\w-]+)/([\w-]+)/([\w+-]+)$}
+HOMEBREW_TAP_FORMULA_REGEX = %r{^([\w-]+)/([\w-]+)/([\w+-.]+)$}
 # match taps' directory path, e.g. HOMEBREW_LIBRARY/Taps/someuser/sometap
 HOMEBREW_TAP_DIR_REGEX = %r{#{Regexp.escape(HOMEBREW_LIBRARY.to_s)}/Taps/([\w-]+)/([\w-]+)}
 # match taps' formula path, e.g. HOMEBREW_LIBRARY/Taps/someuser/sometap/someformula
