2a7f662443fa87c92aae977f0c0fd546ccc21483
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-28 08:18:58 +0100

    fpc: add 2.6.4 bottle.
    Closes #31178.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/fpc.rb
+++ Library/Formula/fpc.rb
@@ -5,6 +5,13 @@ class Fpc < Formula
   url "https://downloads.sourceforge.net/project/freepascal/Source/2.6.4/fpc-2.6.4.source.tar.gz"
   sha1 "60eeadf65db25b10b174627457a5799bf0fd0d52"
 
+  bottle do
+    cellar :any
+    sha1 "c77e7a5b6b9fb84b9d90bb4515a8557ccb98a253" => :mavericks
+    sha1 "47f760e84fc84f845718efe4737402e086de705c" => :mountain_lion
+    sha1 "90d3b9d4ad5e3d06efc0108e0b1dbd8e58b18034" => :lion
+  end
+
   resource "bootstrap" do
     url "https://downloads.sourceforge.net/project/freepascal/Bootstrap/2.6.4/universal-macosx-10.5-ppcuniversal.tar.bz2"
     sha1 "1476a19ad7f901868fcbe3dc49e6d46a5865f722"
