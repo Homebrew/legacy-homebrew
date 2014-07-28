cc3641e4d435df6144fbfd219051760c8c6ed1aa
Ben Burkert <ben@benburkert.com>
2014-07-28 10:01:14 -0700

    ngrep: fix ipv6
    Closes #31185.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ngrep.rb
+++ Library/Formula/ngrep.rb
@@ -4,12 +4,19 @@ class Ngrep < Formula
   homepage 'http://ngrep.sourceforge.net/'
   url 'https://downloads.sourceforge.net/project/ngrep/ngrep/1.45/ngrep-1.45.tar.bz2'
   sha1 'f26090a6ac607db66df99c6fa9aef74968f3330f'
+  revision 1
 
   bottle do
     cellar :any
     sha1 "4954b0c218b60337906b83e39fed7e80252e1890" => :mavericks
     sha1 "3fa3364523d8027c7222e10f66d6509b6d05bf29" => :mountain_lion
     sha1 "83a26db0ace21d12c72bb1fbb3a8435c726ff9b7" => :lion
+  end
+
+  # http://sourceforge.net/p/ngrep/bugs/27/
+  patch do
+    url  'https://launchpadlibrarian.net/44952147/ngrep-fix-ipv6-support.patch'
+    sha1 '84ff02f59b4fab8692a3ff2a61b45a4b9d067177'
   end
 
   def install
