57f18ba14e38bd09b810ebc06ea6d1de01036349
Mike McQuaid <mike@mikemcquaid.com>
2014-07-29 10:04:52 +0200

    cabal-install: use double-quotes.

--- Library/Formula/cabal-install.rb
+++ Library/Formula/cabal-install.rb
@@ -1,7 +1,7 @@
-require 'formula'
+require "formula"
 
 class CabalInstall < Formula
-  homepage 'http://www.haskell.org/haskellwiki/Cabal-Install'
+  homepage "http://www.haskell.org/haskellwiki/Cabal-Install"
   url "http://hackage.haskell.org/package/cabal-install-1.20.0.2/cabal-install-1.20.0.2.tar.gz"
   sha1 "e9b3843270b8f5969a4e1205263e59439bc35692"
   revision 1
@@ -13,25 +13,25 @@ class CabalInstall < Formula
     sha1 "14c4103eac763e6e7b2ee480b1daf739c61ad308" => :lion
   end
 
-  depends_on 'ghc'
+  depends_on "ghc"
 
-  conflicts_with 'haskell-platform'
+  conflicts_with "haskell-platform"
 
   def install
     # use a temporary package database instead of ~/.cabal or ~/.ghc
     pkg_db = "#{Dir.pwd}/package.conf.d"
-    system 'ghc-pkg', 'init', pkg_db
-    ENV['EXTRA_CONFIGURE_OPTS'] = "--package-db=#{pkg_db}"
-    ENV['PREFIX'] = Dir.pwd
-    inreplace 'bootstrap.sh', 'list --global',
-      'list --global --no-user-package-db'
+    system "ghc-pkg", "init", pkg_db
+    ENV["EXTRA_CONFIGURE_OPTS"] = "--package-db=#{pkg_db}"
+    ENV["PREFIX"] = Dir.pwd
+    inreplace "bootstrap.sh", "list --global",
+      "list --global --no-user-package-db"
 
-    system 'sh', 'bootstrap.sh'
+    system "sh", "bootstrap.sh"
     bin.install "bin/cabal"
-    bash_completion.install 'bash-completion/cabal'
+    bash_completion.install "bash-completion/cabal"
   end
 
   test do
-    system "#{bin}/cabal", "--config-file=#{testpath}/config", 'info', 'cabal'
+    system "#{bin}/cabal", "--config-file=#{testpath}/config", "info", "cabal"
   end
 end
