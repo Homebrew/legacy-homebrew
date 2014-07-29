bfb929b910ba5125ecd990fd38aa5de5dcece77c
luzpaz <luzpaz@users.noreply.github.com>
2014-07-29 10:11:25 -0500

    Update libcdr.rb
    don't know how whitespaces crept in there. sorry

--- Library/Formula/libcdr.rb
+++ Library/Formula/libcdr.rb
@@ -1,19 +1,19 @@
 require "formula"
- 
+
 class Libcdr < Formula
   homepage "https://wiki.documentfoundation.org/DLP/Libraries/libcdr"
   url "http://dev-www.libreoffice.org/src/libcdr/libcdr-0.1.0.tar.bz2"
   sha1 "fea63690acea2b9eac5dc6f51232154b7bb41a9e"
- 
- 
+
+
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
   depends_on "icu4c"
   depends_on "librevenge"
   depends_on "little-cms2"
- 
+
   needs :cxx11
- 
+
   def install
     ENV.libcxx
     ENV.cxx11
