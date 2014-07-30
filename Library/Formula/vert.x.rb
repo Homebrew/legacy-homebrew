f2d3fb6b8ea119be5897a9633370a31174b340b2
Dave Pringle <dave.pringle@devinfotech.co.uk>
2014-07-30 12:36:13 +0100

    Upgraded Vert.x Formula to 2.1.2

--- Library/Formula/vert.x.rb
+++ Library/Formula/vert.x.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class VertX < Formula
   homepage 'http://vertx.io/'
-  url 'http://dl.bintray.com/vertx/downloads/vert.x-2.1.1.tar.gz'
+  url 'http://dl.bintray.com/vertx/downloads/vert.x-2.1.2.tar.gz'
   sha1 'b11d13e641e8115a8ffeff758626e950382d1a2d'
 
   def install
