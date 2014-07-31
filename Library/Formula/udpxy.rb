7a34a7cae3de9a0b906bc0a6b01ac6694ddb9b5e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-07-31 17:59:57 +0100

    udpxy: add 1.0.23-9 bottle.
    Closes #31255.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/udpxy.rb
+++ Library/Formula/udpxy.rb
@@ -6,6 +6,12 @@ class Udpxy < Formula
   sha1 "4194fc98d51284da48d07c44bbc5bdfa4813a4b8"
   version "1.0.23-9"
 
+  bottle do
+    sha1 "77d4bee071a84cbf4be6505b41a25cad4ac2c5ce" => :mavericks
+    sha1 "7c1859700e919715bf07663bd61564b5cc4b040d" => :mountain_lion
+    sha1 "7f2dfa95dcf0f437c1ddc0197b1fa1599281c68b" => :lion
+  end
+
   # Fix gzip path in Makefile for uname Darwin, this is needed to fix the install task
   # http://sourceforge.net/p/udpxy/patches/4/
   patch :DATA
