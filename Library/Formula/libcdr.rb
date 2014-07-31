4dd9ff2dd6570be8cffdb6ba7c381400a036468c
luzpaz <luzpaz@users.noreply.github.com>
2014-07-31 10:43:42 -0500

    Update libcdr.rb
    Added 
    + @mnin https://github.com/mnin/homebrew/commit/c9cbb6cbc00a82079a70d438667f0048fd70f48b
    + @mikemcquaid https://github.com/Homebrew/homebrew/pull/31162/#discussion_r15633816
    still waiting on @dtardon to help me understand how to tell libcdr where to install via a make flag https://github.com/Homebrew/homebrew/pull/31162/#discussion_r15633822

--- Library/Formula/libcdr.rb
+++ Library/Formula/libcdr.rb
@@ -5,6 +5,12 @@ class Libcdr < Formula
   url "http://dev-www.libreoffice.org/src/libcdr/libcdr-0.1.0.tar.bz2"
   sha1 "fea63690acea2b9eac5dc6f51232154b7bb41a9e"
 
+  bottle do
+    cellar :any
+    sha1 "ceb4998a2eb3214be43b10319385bb207c1f80ff" => :mavericks
+    sha1 "bbc9f84e75033208e7647fc887f27b78b50941eb" => :mountain_lion
+    sha1 "1ff8850e4c9ce77eae40753e3ea5ad8bdf5949bc" => :lion
+  end
 
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
@@ -12,10 +18,8 @@ class Libcdr < Formula
   depends_on "librevenge"
   depends_on "little-cms2"
 
-  needs :cxx11
 
   def install
-    ENV.libcxx
     ENV.cxx11
     system "./configure", "--disable-werror",
                           "--without-docs"
@@ -29,6 +33,6 @@ class Libcdr < Formula
         libcdr::CDRDocument::isSupported(0);
       }
     EOS
-    system ENV.cxx, "test.cpp", "-I#{Formula["librevenge"].include}/librevenge-0.0", "-I#{HOMEBREW_PREFIX}/include/libcdr-0.1", "-lcdr-0.1"
+    system ENV.cxx11, "test.cpp", "-I#{Formula["librevenge"].include}/librevenge-0.0", "-I#{include}", "-lcdr-0.1"
   end
 end
