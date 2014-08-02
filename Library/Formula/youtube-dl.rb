4d5210900fd7857304c02119a457c32946546626
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-02 13:25:16 +0100

    youtube-dl: update 2014.08.02 bottle.
    Closes #31297.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -10,9 +10,9 @@ class YoutubeDl < Formula
 
   bottle do
     cellar :any
-    sha1 "860c65ea577791043d0a5398eb9b8477d48e9298" => :mavericks
-    sha1 "be3894af50e17b15e593281508ae8b3e9523bb1e" => :mountain_lion
-    sha1 "727a22e5cf33ad7955e4f6a14144f97e6e212d32" => :lion
+    sha1 "f3abd53a4d81168b928b225c119e632858cba112" => :mavericks
+    sha1 "b8c98397e782719c49a3715c9437794d72f7b93b" => :mountain_lion
+    sha1 "76b5f07fc14e6274313a17a601216004591bfb59" => :lion
   end
 
   head do
