10d63e802218af3342771ed0537ce78d0fa11137
Brett Koonce <koonce@gmail.com>
2014-08-02 11:44:02 -0700

    go-appengine-sdk (386) 1.9.8

--- Library/Formula/go-app-engine-32.rb
+++ Library/Formula/go-app-engine-32.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GoAppEngine32 < Formula
   homepage "http://code.google.com/appengine/docs/go/overview.html"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.7.zip"
-  sha1 "93bdca30269993f1bdbfba88223d8df3065389be"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.8.zip"
+  sha1 "3768e768b1694ab16968a793b07a4f6d58ea4010"
 
   def install
     cd ".."
