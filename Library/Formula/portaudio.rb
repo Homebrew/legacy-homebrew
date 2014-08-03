2b93c3b07d233cfc43b5afa299ac244cde9041b9
Tomasz Pajor <n@servercrunch.com>
2014-08-03 13:52:00 +0200

    portaudio 19.20140130
    Closes #31313.

--- Library/Formula/portaudio.rb
+++ Library/Formula/portaudio.rb
@@ -1,41 +1,21 @@
-require 'formula'
+require "formula"
 
 class Portaudio < Formula
-  homepage 'http://www.portaudio.com'
+  homepage "http://www.portaudio.com"
+  url "http://www.portaudio.com/archives/pa_stable_v19_20140130.tgz"
+  sha1 "526a7955de59016a06680ac24209ecb6ce05527d"
+  head "https://subversion.assembla.com/svn/portaudio/portaudio/trunk/", :using => :svn
 
-  stable do
-    url 'http://www.portaudio.com/archives/pa_stable_v19_20111121.tgz'
-    sha1 'f07716c470603729a55b70f5af68f4a6807097eb'
-
-    # Fix PyAudio compilation on Lion
-    patch :p0 do
-      url "https://trac.macports.org/export/94150/trunk/dports/audio/portaudio/files/patch-include__pa_mac_core.h.diff"
-      mirror "https://raw.githubusercontent.com/sakuro/macports/9ee0b7dd6045a3967348638a228bc1a658bcaa3f/audio/portaudio/files/patch-include__pa_mac_core.h.diff"
-      sha1 "28188dfb6a5438a050fccb70331751a40e33447d"
-    end if MacOS.version >= :lion
-  end
-
-  head 'https://subversion.assembla.com/svn/portaudio/portaudio/trunk/', :using => :svn
-
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
 
   option :universal
 
-  fails_with :llvm do
-    build 2334
-  end
-
   def install
-    ENV.universal_binary if build.universal?
-
-    args = [ "--prefix=#{prefix}",
-             "--disable-debug",
-             "--disable-dependency-tracking",
-             # portaudio builds universal unless told not to
-             "--enable-mac-universal=#{build.universal? ? 'yes' : 'no'}" ]
-
-    system "./configure", *args
-    system "make install"
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-mac-universal=#{build.universal? ? 'yes' : 'no'}"
+    system "make", "install"
 
     # Need 'pa_mac_core.h' to compile PyAudio
     include.install "include/pa_mac_core.h"
