5eca47e0c3a6af691f48ab68d30fe7a6f1b054e8
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-08-04 00:38:05 +0100

    gnu-tar 1.28

--- Library/Formula/gnu-tar.rb
+++ Library/Formula/gnu-tar.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class GnuTar < Formula
   homepage 'http://www.gnu.org/software/tar/'
-  url 'http://ftpmirror.gnu.org/tar/tar-1.27.1.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/tar/tar-1.27.1.tar.gz'
-  sha1 '192f480cac95b1fbaff11c87a64f7092045b411b'
+  url 'http://ftpmirror.gnu.org/tar/tar-1.28.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/tar/tar-1.28.tar.gz'
+  sha1 'cd30a13bbfefb54b17e039be7c43d2592dd3d5d0'
 
   bottle do
     sha1 "ecc6fc62b93bee896a2bd6e604c934bdb9bdc2dd" => :mavericks
@@ -12,10 +12,23 @@ class GnuTar < Formula
     sha1 "33c07152ade47e3b3c7bfa2d66346a0acc5ff3ef" => :lion
   end
 
+  # autoconf/automake can be removed when the patch is merged upstream.
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  # Fix for xattrs bug causing build failures on OS X:
+  # https://lists.gnu.org/archive/html/bug-tar/2014-08/msg00001.html
+  patch :p1 do
+    url "https://raw.githubusercontent.com/DomT4/patch-mayhem/master/0001-xattrs-fix-bug-in-configure.patch"
+    sha1 "d8d9f9f5862fa1c1468a914e609daaf57accf3d2"
+  end
+
   def install
     args = ["--prefix=#{prefix}", "--mandir=#{man}"]
     args << "--program-prefix=g"
 
+    # autoreconf can be removed when the patch is merged upstream.
+    system "autoreconf"
     system "./configure", *args
     system "make install"
 
