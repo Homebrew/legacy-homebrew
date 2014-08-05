8832bf9b3dbc6069995ed70bb6f768f77915cc9d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-05 12:35:19 +0100

    cppcheck: add 1.66 bottle.
    Closes #31356.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cppcheck.rb
+++ Library/Formula/cppcheck.rb
@@ -7,6 +7,12 @@ class Cppcheck < Formula
 
   head "https://github.com/danmar/cppcheck.git"
 
+  bottle do
+    sha1 "a3b2341260ab7afbb08dbf44e51b32d2edbd6d7c" => :mavericks
+    sha1 "e7290d70f4aee90d644898785eb4eff25fbbcd8b" => :mountain_lion
+    sha1 "ee30f858617e11e68d465d7406f445cf13d6f791" => :lion
+  end
+
   option "no-rules", "Build without rules (no pcre dependency)"
   option "with-gui", "Build the cppcheck gui (requires Qt)"
 
