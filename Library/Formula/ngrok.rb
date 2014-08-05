50bb80d2112eafcf4ae669ca395419ed0d8259b9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-05 06:17:19 +0100

    ngrok: update 1.7 bottle.
    Closes #31348.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/ngrok.rb
+++ Library/Formula/ngrok.rb
@@ -7,6 +7,9 @@ class Ngrok < Formula
   sha1 "4a6baf98a23193c21d7732c6573f53dbbede5033"
 
   bottle do
+    sha1 "71920267073415ccc1ea1305bac6ce51e00d4833" => :mavericks
+    sha1 "e0a622225d38ee57652b84eaffbca95ab43c3ce2" => :mountain_lion
+    sha1 "60b7a16a28265e6e9a9adbac44b3c5069a802d34" => :lion
   end
 
   depends_on "go" => :build
