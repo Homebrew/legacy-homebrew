b64069d01ea14a82aa0817ae14b06d69f022d8c9
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-08-06 23:13:05 +0100

    Openssl 1.0.1i
    The mirror isn’t up yet. I’ll add it back as soon as one is available.

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Openssl < Formula
   homepage 'http://openssl.org'
   url 'https://www.openssl.org/source/openssl-1.0.1i.tar.gz'
-  mirror 'http://mirrors.ibiblio.org/openssl/source/openssl-1.0.1i.tar.gz'
   sha256 '3c179f46ca77069a6a0bac70212a9b3b838b2f66129cb52d568837fc79d8fcc7'
 
   bottle do
