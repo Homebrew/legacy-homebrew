d226a7d1385160eca7ac6a3ad9f715f501e37770
Adam Vandenberg <flangy@gmail.com>
2014-08-05 21:18:07 -0700

    qemu: modernize

--- Library/Formula/qemu.rb
+++ Library/Formula/qemu.rb
@@ -1,21 +1,19 @@
-require 'formula'
+require "formula"
 
 class Qemu < Formula
-  homepage 'http://www.qemu.org/'
-  url 'http://wiki.qemu-project.org/download/qemu-2.1.0.tar.bz2'
-  sha1 'b2829491e4c2f3d32f7bc2860c3a19fb31f5e989'
-  revision 1
+  homepage "http://www.qemu.org/"
+  head "git://git.qemu-project.org/qemu.git"
+  url "http://wiki.qemu-project.org/download/qemu-2.1.0.tar.bz2"
+  sha1 "b2829491e4c2f3d32f7bc2860c3a19fb31f5e989"
 
-  head 'git://git.qemu-project.org/qemu.git'
-
-  depends_on 'pkg-config' => :build
-  depends_on 'libtool' => :build
-  depends_on 'jpeg'
-  depends_on 'gnutls'
-  depends_on 'glib'
-  depends_on 'pixman'
-  depends_on 'vde' => :optional
-  depends_on 'sdl' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+  depends_on "jpeg"
+  depends_on "gnutls"
+  depends_on "glib"
+  depends_on "pixman"
+  depends_on "vde" => :optional
+  depends_on "sdl" => :optional
 
   def install
     args = %W[
@@ -26,10 +24,10 @@ class Qemu < Formula
       --disable-bsd-user
       --disable-guest-agent
     ]
-    args << (build.with?('sdl') ? '--enable-sdl' : '--disable-sdl')
-    args << (build.with?('vde') ? '--enable-vde' : '--disable-vde')
-    args << '--disable-gtk'
-    ENV['LIBTOOL'] = 'glibtool'
+    args << (build.with?("sdl") ? "--enable-sdl" : "--disable-sdl")
+    args << (build.with?("vde") ? "--enable-vde" : "--disable-vde")
+    args << "--disable-gtk"
+    ENV["LIBTOOL"] = "glibtool"
     system "./configure", *args
     system "make", "V=1", "install"
   end
