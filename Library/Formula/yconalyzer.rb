813294ffb30e1700a686c896dcf0b6d347a427dc
Alex Samorukov <samm@os2.kiev.ua>
2014-08-07 18:09:21 +0200

    add link to upstream bugreport

--- Library/Formula/yconalyzer.rb
+++ Library/Formula/yconalyzer.rb
@@ -14,7 +14,8 @@ class Yconalyzer < Formula
     system "make install"
   end
 
-  # Fix build issues issue on OS X/clang
+  # Fix build issues issue on OS X 10.9/clang
+  # Patch reported to upstream - https://sourceforge.net/p/yconalyzer/bugs/3/
   patch :p0, :DATA
 
 end
