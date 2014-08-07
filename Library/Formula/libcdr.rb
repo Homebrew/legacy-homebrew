b4b135500856fff055890c29eea29f45224cf10a
luzpaz <luzpaz@users.noreply.github.com>
2014-08-07 09:05:25 -0500

    Update libcdr.rb

--- Library/Formula/libcdr.rb
+++ Library/Formula/libcdr.rb
@@ -5,12 +5,6 @@ class Libcdr < Formula
   url "http://dev-www.libreoffice.org/src/libcdr/libcdr-0.1.0.tar.bz2"
   sha1 "fea63690acea2b9eac5dc6f51232154b7bb41a9e"
 
-  bottle do
-    cellar :any
-    sha1 "ceb4998a2eb3214be43b10319385bb207c1f80ff" => :mavericks
-    sha1 "bbc9f84e75033208e7647fc887f27b78b50941eb" => :mountain_lion
-    sha1 "1ff8850e4c9ce77eae40753e3ea5ad8bdf5949bc" => :lion
-  end
 
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
@@ -34,6 +28,6 @@ class Libcdr < Formula
         libcdr::CDRDocument::isSupported(0);
       }
     EOS
-    system ENV.cxx11, "test.cpp", "-I#{Formula["librevenge"].include}/librevenge-0.0", "-I#{include}", "-lcdr-0.1"
+    system ENV.cxx11, "-o", "test.cpp", "-I#{Formula["librevenge"].include}/librevenge-0.0", "-I#{include}", "-lcdr-0.1"
   end
 end
