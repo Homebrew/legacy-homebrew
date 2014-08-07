2d40a0f6d8c04e83b0fb081498f67d73eeb6a48b
Brett Koonce <koonce@gmail.com>
2014-08-06 23:41:45 -0700

    protobuf-c 1.0.1
    Closes #31361.

--- Library/Formula/protobuf-c.rb
+++ Library/Formula/protobuf-c.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class ProtobufC < Formula
-  homepage 'https://github.com/protobuf-c/protobuf-c'
-  url 'https://github.com/protobuf-c/protobuf-c/releases/download/v1.0.0/protobuf-c-1.0.0.tar.gz'
-  sha1 '6d48eb6a193556262c35526e1ccf209a6fc69684'
+  homepage "https://github.com/protobuf-c/protobuf-c"
+  url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.0.1/protobuf-c-1.0.1.tar.gz"
+  sha1 "5928059292462aa6696c38857a3ca541a54f1b3f"
 
   bottle do
     sha1 "46572020e49936035b3f35194c40f98d90fd8dcd" => :mavericks
@@ -11,8 +11,8 @@ class ProtobufC < Formula
     sha1 "64e31c97b2ef834d6406d128892575706b17cbad" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'protobuf'
+  depends_on "pkg-config" => :build
+  depends_on "protobuf"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
