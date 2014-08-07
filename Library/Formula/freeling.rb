47d5c5e3457cbfb102b93ebf96bd3ad65e889c2f
Mike McQuaid <mike@mikemcquaid.com>
2014-08-07 19:20:21 +0100

    freeling: cleanup styling.

--- Library/Formula/freeling.rb
+++ Library/Formula/freeling.rb
@@ -1,26 +1,26 @@
-require 'formula'
+require "formula"
 
 class Freeling < Formula
-  homepage 'http://nlp.lsi.upc.edu/freeling/'
-  url 'http://devel.cpl.upc.edu/freeling/downloads/32'
-  version '3.1'
-  sha1 '42dbf7eec6e5c609e10ccc60768652f220d24771'
+  homepage "http://nlp.lsi.upc.edu/freeling/"
+  url "http://devel.cpl.upc.edu/freeling/downloads/32"
+  version "3.1"
+  sha1 "42dbf7eec6e5c609e10ccc60768652f220d24771"
 
-  depends_on 'icu4c'
-  depends_on 'boost' => 'with-icu'
-  depends_on 'libtool' => :build
+  depends_on "icu4c"
+  depends_on "boost" => "with-icu"
+  depends_on "libtool" => :build
 
   def install
-    icu4c = Formula['icu4c']
-    libtool = Formula['libtool']
-    ENV.append 'LDFLAGS', "-L#{libtool.lib}"
-    ENV.append 'LDFLAGS', "-L#{icu4c.lib}"
-    ENV.append 'CPPFLAGS', "-I#{libtool.include}"
-    ENV.append 'CPPFLAGS', "-I#{icu4c.include}"
+    icu4c = Formula["icu4c"]
+    libtool = Formula["libtool"]
+    ENV.append "LDFLAGS", "-L#{libtool.lib}"
+    ENV.append "LDFLAGS", "-L#{icu4c.lib}"
+    ENV.append "CPPFLAGS", "-I#{libtool.include}"
+    ENV.append "CPPFLAGS", "-I#{icu4c.include}"
 
     system "./configure", "--prefix=#{prefix}", "--enable-boost-locale"
 
-    system "make install"
+    system "make", "install"
   end
 
   test do
