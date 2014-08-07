47a71e05534f7f9ea62053e7d450a922ef9eed18
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2014-08-07 21:22:26 +0200

    Added simple test program

--- Library/Formula/libstrophe.rb
+++ Library/Formula/libstrophe.rb
@@ -25,6 +25,28 @@ class Libstrophe < Formula
   end
 
   test do
-    system "ld -dylib -lstrophe"
+    (testpath/'test.c').write <<-EOS.undent
+      #include <strophe.h>
+      #include <assert.h>
+
+      int main(void) {
+        xmpp_ctx_t *ctx;
+        xmpp_log_t *log;
+	
+        xmpp_initialize();
+        log = xmpp_get_default_logger(XMPP_LEVEL_DEBUG);
+	assert(log);
+
+        ctx = xmpp_ctx_new(NULL, log);
+	assert(ctx);
+
+        xmpp_ctx_free(ctx);
+        xmpp_shutdown();
+        return 0;
+      }
+      EOS
+    flags = ["-I#{include}/", "-lstrophe"]
+    system ENV.cc, "-o", "test", "test.c", *(flags + ENV.cflags.to_s.split)
+    system "./test"
   end
 end
