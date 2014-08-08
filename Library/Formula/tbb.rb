9a1ed848bb08bdd6c85a660aee603ba113facc0c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-08 17:39:45 +0100

    tbb: add 4.2.5 bottle.
    Closes #31449.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/tbb.rb
+++ Library/Formula/tbb.rb
@@ -6,6 +6,13 @@ class Tbb < Formula
   sha1 "f50c04a27f5e37c920a03be134dc57ccf909515d"
   version "4.2.5"
 
+  bottle do
+    cellar :any
+    sha1 "1acf91deb5540f1ed153c3ece947146f84e52946" => :mavericks
+    sha1 "0e2799b23f8a7ee446fe49b986d1d8cc308ae755" => :mountain_lion
+    sha1 "626775355972feb9cbcf7a786c109112f36fd746" => :lion
+  end
+
   option :cxx11
 
   def install
