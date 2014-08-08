1c9f8c85769078162c0f935b08b65f04018bc631
Tomasz Pajor <n@servercrunch.com>
2014-08-07 23:12:12 -0500

    gptfdisk: bump revision for icu4c upgrade
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gptfdisk.rb
+++ Library/Formula/gptfdisk.rb
@@ -1,17 +1,18 @@
-require 'formula'
+require "formula"
 
 class Gptfdisk < Formula
-  homepage 'http://www.rodsbooks.com/gdisk/'
-  url 'https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/0.8.10/gptfdisk-0.8.10.tar.gz'
-  sha1 '1708e232220236b6bdf299b315e9bc2205c01ba5'
+  homepage "http://www.rodsbooks.com/gdisk/"
+  url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/0.8.10/gptfdisk-0.8.10.tar.gz"
+  sha1 "1708e232220236b6bdf299b315e9bc2205c01ba5"
+  revision 1
 
-  depends_on 'popt'
-  depends_on 'icu4c'
+  depends_on "popt"
+  depends_on "icu4c"
 
   def install
     system "make -f Makefile.mac"
-    sbin.install 'gdisk', 'cgdisk', 'sgdisk', 'fixparts'
-    man8.install Dir['*.8']
+    sbin.install "gdisk", "cgdisk", "sgdisk", "fixparts"
+    man8.install Dir["*.8"]
   end
 
   test do
