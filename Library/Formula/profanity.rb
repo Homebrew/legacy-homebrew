76eaadaab81972b16c65a18f8545131b8a131186
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-08 18:10:26 +0100

    profanity: add 0.4.2 bottle.
    Closes #31447.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/profanity.rb
+++ Library/Formula/profanity.rb
@@ -6,6 +6,12 @@ class Profanity < Formula
   sha1 "1af17e68a5b6142a996b95168322c88ec4a2720b"
   head "https://github.com/boothj5/profanity.git"
 
+  bottle do
+    sha1 "d0f6236615ac6535042e976856837ccf90cdcc3d" => :mavericks
+    sha1 "22c48eb0d161da4ace8adadee5c251a3a3933eb8" => :mountain_lion
+    sha1 "54b24ff622f05b071084ef53a9bf03c8327e58d9" => :lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
