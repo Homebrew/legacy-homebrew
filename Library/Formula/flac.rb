7c9e0691dcb9a2c2cfcf21936da0c8870fef1806
Mike McQuaid <mike@mikemcquaid.com>
2014-08-08 10:01:32 +0100

    flac: remove flac2mp3.
    It no longer works and it's a separate software project.
    
    Closes #31442.

--- Library/Formula/flac.rb
+++ Library/Formula/flac.rb
@@ -23,7 +23,6 @@ class Flac < Formula
   option :universal
 
   depends_on "pkg-config" => :build
-  depends_on "lame"
   depends_on "libogg" => :optional
 
   fails_with :llvm do
@@ -59,33 +58,5 @@ class Flac < Formula
     end
 
     system "make install"
-    (bin/"flac2mp3").write DATA.read
   end
 end
-
-__END__
-#!/usr/bin/env ruby
-# https://github.com/rmndk/flac2mp3
-
-filename, quality = ARGV[0], ARGV[1]
-abort "Usage: flac2mp3 FLACFILE [V2|V1|V0|320]\nDefault (and recommended) quality is V0." if filename.nil?
-
-qualarg = case quality
-    when "V0","V1","V2" then quality
-    when "320" then "b 320"
-    else "V0"
-end
-
-map = {"TITLE" => "--tt", "ARTIST" => "--ta", "ALBUM" => "--tl", "TRACKNUMBER" => "--tn", "GENRE" => "--tg", "DATE" => "--ty"}
-args = ""
-
-`metaflac --export-tags-to=- "#{filename}"`.each_line do |line|
-    key, value = line.strip.split('=', 2)
-    key.upcase!
-    args << %Q{#{map[key]} "#{value.gsub('"', '\"')}" } if map[key]
-end
-
-basename = File.basename(filename, File.extname(filename))
-
-puts "Encoding #{basename}.mp3"
-exec %Q[flac -sdc "#{filename}" | lame -#{qualarg} #{args} - "#{basename}.mp3"]
