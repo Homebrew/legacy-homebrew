7802085aa42c6e57e86eea9f06f13a0ac62cb7e5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-08 08:26:36 +0100

    protobuf-c: update 1.0.1 bottle.
    Closes #31410.

--- Library/Formula/protobuf-c.rb
+++ Library/Formula/protobuf-c.rb
@@ -6,9 +6,9 @@ class ProtobufC < Formula
   sha1 "5928059292462aa6696c38857a3ca541a54f1b3f"
 
   bottle do
-    sha1 "46572020e49936035b3f35194c40f98d90fd8dcd" => :mavericks
-    sha1 "5fe4ec31d10b064fc580ae5958873ca038083a80" => :mountain_lion
-    sha1 "64e31c97b2ef834d6406d128892575706b17cbad" => :lion
+    sha1 "3e7b3432a5284e9308d0318357ff06ee960e5ed6" => :mavericks
+    sha1 "c1dd644b5f4560a0e70b9bb9085dc50ef0bed98a" => :mountain_lion
+    sha1 "6502775b0cd40d27334dfde490369cbfe7a9fa2a" => :lion
   end
 
   depends_on "pkg-config" => :build
