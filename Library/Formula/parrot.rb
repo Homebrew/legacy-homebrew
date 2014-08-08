b7688b9cc3024596bdd8f9feddab268a8271751d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-07 23:12:13 -0500

    parrot: add 6.6.0_1 bottle.

--- Library/Formula/parrot.rb
+++ Library/Formula/parrot.rb
@@ -7,6 +7,12 @@ class Parrot < Formula
   sha256 "08e9e02db952828f6ab71755be47f99ebc90894378f04d8e4d7f3bc623f79ff5"
   revision 1
 
+  bottle do
+    sha1 "490672e708d62a0c1fc5a38d6326d53f88636c18" => :mavericks
+    sha1 "2ad6a39e1d4f9d655c3b1340b45074aaae49d637" => :mountain_lion
+    sha1 "12a26f049a680a0669a3b43f76b709646b48ce08" => :lion
+  end
+
   conflicts_with "rakudo-star"
 
   depends_on "gmp" => :optional
