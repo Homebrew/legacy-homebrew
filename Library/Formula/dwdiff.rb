a78a1e79cd5e2be945d911a6dd95a0d7d7d16c68
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-07 23:12:12 -0500

    dwdiff: add 2.0.9_1 bottle.

--- Library/Formula/dwdiff.rb
+++ Library/Formula/dwdiff.rb
@@ -6,6 +6,12 @@ class Dwdiff < Formula
   sha1 "01cb2230b9147347bcfd1770898e435e4a57fa25"
   revision 1
 
+  bottle do
+    sha1 "d86c4df0684421132d197d6059ae73c1022b26cc" => :mavericks
+    sha1 "3cde7642d439be392e69a939c8a0f16b30ec84b0" => :mountain_lion
+    sha1 "4e33875ebc44f65716a6588de2a82e52658b9806" => :lion
+  end
+
   depends_on "gettext"
   depends_on "icu4c"
 
