ae7d9168ce2e1f204b6bb2c41f1a94abd7bbfe3e
Tomasz Pajor <n@servercrunch.com>
2014-08-09 20:42:22 +0200

    formula_installer: fix reqs default formulae.
    (Commit message written by Mike McQuaid)
    
    With the change in a0a93f1b3b7b2be9b8a319be91086ffe220f8e32
    unfortunately requirements with default formulae no longer could be
    resolved when the default formulae were not installed. This commit
    fixes this problem.
    
    Closes #31476.
    Closes #31444.
    Closes #30901.
    Closes #31471.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -262,12 +262,14 @@ class FormulaInstaller
           Requirement.prune
         elsif req.build? && dependent != f && install_bottle_for_dep?(dependent, build)
           Requirement.prune
+        elsif req.satisfied? && !(pour_bottle? || build_bottle?)
+          Requirement.prune
         elsif install_requirement_default_formula?(req)
           dep = req.to_dependency
           deps.unshift(dep)
           formulae.unshift(dep.to_formula)
           Requirement.prune
-        elsif req.satisfied?
+        elsif deps.include?(req.to_dependency)
           Requirement.prune
         else
           unsatisfied_reqs[dependent] << req
