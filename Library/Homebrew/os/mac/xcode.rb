659068b933a957a699bf3752d034d5e74167a2b0
Jack Nagel <jacknagel@gmail.com>
2014-08-09 22:00:06 -0500

    Realign case statement

--- Library/Homebrew/os/mac/xcode.rb
+++ Library/Homebrew/os/mac/xcode.rb
@@ -8,13 +8,13 @@ module OS
 
       def latest_version
         case MacOS.version
-        when "10.4"         then "2.5"
-        when "10.5"         then "3.1.4"
-        when "10.6"         then "3.2.6"
-        when "10.7"         then "4.6.3"
-        when "10.8"         then "5.1.1"
-        when "10.9"         then "5.1.1"
-        when "10.10"        then "6.0"
+        when "10.4"  then "2.5"
+        when "10.5"  then "3.1.4"
+        when "10.6"  then "3.2.6"
+        when "10.7"  then "4.6.3"
+        when "10.8"  then "5.1.1"
+        when "10.9"  then "5.1.1"
+        when "10.10" then "6.0"
         else
           # Default to newest known version of Xcode for unreleased OSX versions.
           if MacOS.version > "10.10"
