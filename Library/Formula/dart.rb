aa61363f8298b914b2c408422a0698bd2d7825b5
Seth Ladd <sethladd@gmail.com>
2014-08-10 18:42:15 -0700

    dart: add devel version
    Closes #31503.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/dart.rb
+++ Library/Formula/dart.rb
@@ -13,6 +13,18 @@ class Dart < Formula
 
   version '1.5.8'
 
+  devel do
+    if MacOS.prefer_64_bit?
+      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/38831/sdk/dartsdk-macos-x64-release.zip'
+      sha1 'ed2bc149a6bb5c8b48b8d818269c0be392dc8361'
+    else
+      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/38831/sdk/dartsdk-macos-ia32-release.zip'
+      sha1 'fccba1c2289faaa83b596f7335e107c7b581e2fe'
+    end
+
+    version '1.6.0-dev.7.0'
+  end
+
   def install
     libexec.install Dir['*']
     bin.install_symlink "#{libexec}/bin/dart"
