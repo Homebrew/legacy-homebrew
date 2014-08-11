2fe9b4702a7f987a9aedfb0bcb107efe08f86578
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-11 22:36:08 +0100

    libsecret: add 0.18 bottle.
    Closes #31518.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libsecret.rb
+++ Library/Formula/libsecret.rb
@@ -5,6 +5,12 @@ class Libsecret < Formula
   url "http://ftp.gnome.org/pub/gnome/sources/libsecret/0.18/libsecret-0.18.tar.xz"
   sha1 "af62de3958bbe0ccf59a02101a6704e036378a6f"
 
+  bottle do
+    sha1 "4c39ca74dde90acafc17fc86ffe03559462d2aad" => :mavericks
+    sha1 "c099feb51932a8883ac828b436acbdd154862dc2" => :mountain_lion
+    sha1 "f107bba883485f8ffc18aabd8d6c4e063be0aff0" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'gnu-sed' => :build
   depends_on 'intltool' => :build
