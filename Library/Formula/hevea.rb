7dd4b3f5c34c6d3bf7041ba9d676d3ba95aef077
Jack Nagel <jacknagel@gmail.com>
2014-08-12 00:43:54 -0500

    hevea 2.16
    Closes #31441.

--- Library/Formula/hevea.rb
+++ Library/Formula/hevea.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Hevea < Formula
-  homepage 'http://hevea.inria.fr/'
-  url "http://hevea.inria.fr/distri/hevea-2.14.tar.gz"
-  sha1 "78152c83802e34881ce3414072d75bff66facb15"
+  homepage "http://hevea.inria.fr/"
+  url "http://hevea.inria.fr/distri/hevea-2.16.tar.gz"
+  sha1 "729438f4c37240df7b20053dec18e9ee17d19d46"
 
   bottle do
     sha1 "657486337d169647d9c10afb61516e38ae1bf772" => :mavericks
@@ -11,12 +11,11 @@ class Hevea < Formula
     sha1 "4766e826da52e595f679c699c836bcfb141dfdbd" => :lion
   end
 
-  depends_on 'objective-caml'
-  depends_on 'ghostscript' => :optional
+  depends_on "objective-caml"
+  depends_on "ghostscript" => :optional
 
   def install
-    inreplace 'Makefile', '/usr/local', prefix
     system "make"
-    system "make install"
+    system "make", "PREFIX=#{prefix}", "install"
   end
 end
