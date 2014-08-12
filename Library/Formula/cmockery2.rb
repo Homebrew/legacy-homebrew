98a4857b3a5efb8ad4b06142d6a7d762dcb4512b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-12 15:55:11 +0100

    cmockery2: add 1.3.8 bottle.
    Closes #31515.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cmockery2.rb
+++ Library/Formula/cmockery2.rb
@@ -6,6 +6,13 @@ class Cmockery2 < Formula
   url "https://github.com/lpabon/cmockery2/archive/v1.3.8.tar.gz"
   sha1 "5b5633c9f32b40451b2b94fa47495029f9fad919"
 
+  bottle do
+    cellar :any
+    sha1 "b39492f8fe96daa1008922e4800e3e35b0393103" => :mavericks
+    sha1 "d1925c9d80e45ba7250320ce4786bacd67194fff" => :mountain_lion
+    sha1 "053629d4bb3fc0fe78184c5736e9b0fae64bdcd8" => :lion
+  end
+
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
