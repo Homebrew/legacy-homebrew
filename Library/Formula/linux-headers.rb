acdd7482ce540d4bd60757046cb20fb0fa3f4bf4
Shaun Jackman <sjackman@gmail.com>
2014-08-13 12:24:48 -0700

    linux-headers 3.15.9

--- Library/Formula/linux-headers.rb
+++ Library/Formula/linux-headers.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class LinuxHeaders < Formula
   homepage "http://kernel.org/"
-  url "https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.15.6.tar.xz"
-  sha1 "d8928e10aed8cfc8b16859f0147c2470624372e6"
+  url "https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.15.9.tar.xz"
+  sha1 "c1bed517c0c7ff3514833ec99e1124ab20d9dc1d"
 
   def install
     system "make", "headers_install", "INSTALL_HDR_PATH=#{prefix}"
