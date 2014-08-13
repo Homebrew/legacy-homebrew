f4bfb83262c7760ae9f8d3f67fe8613de595a4e8
Shaun Jackman <sjackman@gmail.com>
2014-08-13 11:19:34 -0700

    patchelf: Use tarball without autotools dependency

--- Library/Formula/patchelf.rb
+++ Library/Formula/patchelf.rb
@@ -2,17 +2,20 @@ require "formula"
 
 class Patchelf < Formula
   homepage "https://nixos.org/patchelf.html"
-  url "https://github.com/NixOS/patchelf/archive/0.8.tar.gz"
-  sha1 "1b1a090fe902285c7b01be9ad2c15af9ff99eced"
+  url "http://nixos.org/releases/patchelf/patchelf-0.8/patchelf-0.8.tar.bz2"
+  sha1 "d0645e9cee6f8e583ae927311c7ce88d29f416fc"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  head do
+    url "https://github.com/NixOS/patchelf.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
 
   def install
     # Fix ./configure: line 4: .: filename argument required
     inreplace "configure.ac", "m4_esyscmd([echo -n $(cat ./version)])", version
 
-    system "./bootstrap.sh"
+    system "./bootstrap.sh" if build.head?
     system "./configure", "--disable-debug",
       "--disable-dependency-tracking",
       "--disable-silent-rules",
