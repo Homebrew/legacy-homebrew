fd93f986da0dbd56edb30dd45f715c3eaf2af5b8
Matthias Votisky <foo.matt@googlemail.com>
2014-08-14 09:01:15 +0200

    ebook-tools 0.2.2 (new formula)

---
+++ Library/Formula/ebook-tools.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class EbookTools < Formula
+  homepage "http://sourceforge.net/projects/ebook-tools"
+  url "https://downloads.sourceforge.net/project/ebook-tools/ebook-tools/0.2.2/ebook-tools-0.2.2.tar.gz"
+  sha1 "1f10bef62c9125cf804366134e486a58308f07ff"
+
+  depends_on "libzip"
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/einfo", "-help"
+  end
+end
