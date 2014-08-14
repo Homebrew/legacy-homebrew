5bd30aef6b42133bb3d38ac965693834d81d38fa
Luca Deri <deri@ntop.org>
2014-08-14 13:42:50 +0200

    Added 0 as trailing version (1.2 -> 1.2.0)

--- Library/Formula/ndpi.rb
+++ Library/Formula/ndpi.rb
@@ -4,7 +4,7 @@ class Ndpi < Formula
   homepage "http://www.ntop.org/products/ndpi/"
   url "https://downloads.sourceforge.net/project/ntop/nDPI/libndpi-1.5.0_r8115.tar.gz"
   sha1 "9fbb97dd3de35670c8f527ac3ccdb1aa50a4da70"
-  version "1.5"
+  version "1.5.0"
 
   depends_on :autoconf => :build
   depends_on :automake => :build
--- Library/Formula/ntopng.rb
+++ Library/Formula/ntopng.rb
@@ -4,7 +4,7 @@ class Ntopng < Formula
   homepage "http://www.ntop.org/products/ntop/"
   url "https://downloads.sourceforge.net/project/ntop/ntopng/ntopng-1.2.0_r8116.tgz"
   sha1 "81189e53aa71084caf11b5462fb752363988fa01"
-  version "1.2"
+  version "1.2.0"
 
   depends_on :autoconf => :build
   depends_on :automake => :build
