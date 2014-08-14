b61a7510a49f6697a79d1b8d654ada1f4877b531
Luca Deri <deri@ntop.org>
2014-08-14 11:55:47 +0200

    Removed double quotes Fixed test step Minor other changes

--- Library/Formula/ndpi.rb
+++ Library/Formula/ndpi.rb
@@ -2,22 +2,22 @@ require "formula"
 
 class Ndpi < Formula
   homepage "http://www.ntop.org/products/ndpi/"
-  url 'https://downloads.sourceforge.net/project/ntop/nDPI/libndpi-1.5.0_r8115.tar.gz'
+  url "https://downloads.sourceforge.net/project/ntop/nDPI/libndpi-1.5.0_r8115.tar.gz"
   sha1 "9fbb97dd3de35670c8f527ac3ccdb1aa50a4da70"
   version "1.5"
 
   depends_on :autoconf => :build
   depends_on :automake => :build
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :libtool => :build
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+    system "make install"
   end
 
   test do
-    system "#{bin}/ndpiReader", "-i", "en0", "-s", "5"
+    system "#{bin}/ndpiReader", "-h"
   end
 
 end
--- Library/Formula/ntopng.rb
+++ Library/Formula/ntopng.rb
@@ -2,34 +2,34 @@ require "formula"
 
 class Ntopng < Formula
   homepage "http://www.ntop.org/products/ntop/"
-  url 'https://downloads.sourceforge.net/project/ntop/ntopng/ntopng-1.2.0_r8116.tgz'
+  url "https://downloads.sourceforge.net/project/ntop/ntopng/ntopng-1.2.0_r8116.tgz"
   sha1 "81189e53aa71084caf11b5462fb752363988fa01"
   version "1.2"
 
   depends_on :autoconf => :build
   depends_on :automake => :build
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :libtool => :build
-  depends_on 'json-glib' => :build
-  depends_on 'json-c' => :build
-  depends_on 'wget' => :build
-  depends_on 'rrdtool' => :build
-  depends_on 'zeromq' => :build
-  depends_on 'gnutls'
-  depends_on 'geoip'
-  depends_on 'sqlite'
-  depends_on 'redis'
-  depends_on 'glib'
+  depends_on "json-glib" => :build
+  depends_on "json-c" => :build
+  depends_on "wget" => :build
+  depends_on "rrdtool" => :build
+  depends_on "zeromq" => :build
+  depends_on "luajit" => :build
+  depends_on "gnutls"
+  depends_on "geoip" => :build
+  depends_on "sqlite" => :build
+  depends_on "redis"
 
   def install
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make","install"
+    system "make install"
   end
 
   test do
-    system "#{bin}/ntopng", "-h"
+    system "#{bin}/ntopng", "--version"
   end
 
 end
