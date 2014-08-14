a75dca65c84b5e36302a32ecf6b28869c52eac52
Pratik Khadloya <tispratik@gmail.com>
2014-08-14 08:28:51 -0700

    Update hbase version
    Hbase 0.93 has been yanked from the downloads url!

--- Library/Formula/hbase.rb
+++ Library/Formula/hbase.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hbase < Formula
   homepage 'http://hbase.apache.org'
-  url 'http://www.apache.org/dyn/closer.cgi?path=hbase/hbase-0.98.3/hbase-0.98.3-hadoop2-bin.tar.gz'
-  sha1 'babbe880afc86783e3e5f659b65b046c1589d0c5'
+  url 'http://www.apache.org/dyn/closer.cgi?path=hbase/hbase-0.98.4/hbase-0.98.4-hadoop2-bin.tar.gz'
+  sha1 '5d235262254a4134d622cc67db8ecd8dcf278d50'
 
   depends_on 'hadoop'
 
