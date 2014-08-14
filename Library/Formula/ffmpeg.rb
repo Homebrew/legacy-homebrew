9feeee66901d6ca44d58e746a45a1b51ce3341f1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-14 07:29:09 +0100

    ffmpeg: update 2.3.2 bottle.
    Closes #31587.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -8,9 +8,9 @@ class Ffmpeg < Formula
   head "git://git.videolan.org/ffmpeg.git"
 
   bottle do
-    sha1 "678d0c8aa724480b4dae58b8cb5799058ec45d4f" => :mavericks
-    sha1 "756846829c78b19e4fbe2f2f1195075c89e5019e" => :mountain_lion
-    sha1 "f6860caa82840e9d9a710458b9f7cdb27bc15a45" => :lion
+    sha1 "abcbf0f0a6a2d1663639b4049c80eec1e0b1c729" => :mavericks
+    sha1 "fe4c529d091d0ab2954e8297ee51c336fa62dc41" => :mountain_lion
+    sha1 "d69a33452d767fe691bdab8dee538e4ed065c83c" => :lion
   end
 
   option "without-x264", "Disable H.264 encoder"
