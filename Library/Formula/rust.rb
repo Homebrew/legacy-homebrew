ed4a55ad08479dbacae1629ff581a26f08444455
Dominyk Tiller <DomT4@users.noreply.github.com>
2014-08-14 23:45:21 +0100

    Rust: Adds https
    Rust has adopted SSL for their download links, so Homebrew can make the switch over now. See [this](https://github.com/rust-lang/rust/issues/16504) Rust Issue for further information.
    
    Closes #31607.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/rust.rb
+++ Library/Formula/rust.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Rust < Formula
   homepage 'http://www.rust-lang.org/'
-  url 'http://static.rust-lang.org/dist/rust-0.11.0.tar.gz'
+  url 'https://static.rust-lang.org/dist/rust-0.11.0.tar.gz'
   sha1 'f849e16e03b474143c575503ae82da7577a2170f'
 
   head 'https://github.com/mozilla/rust.git'
