ec32342a09b05c198c6b11bd9f5152133cd549aa
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-15 12:19:45 +0100

    rabbitmq-c: add 0.5.1 bottle.
    Closes #31585.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/rabbitmq-c.rb
+++ Library/Formula/rabbitmq-c.rb
@@ -7,6 +7,13 @@ class RabbitmqC < Formula
 
   head "https://github.com/alanxz/rabbitmq-c.git"
 
+  bottle do
+    cellar :any
+    sha1 "7637f895726ed8e597c02b616ba7f9a27109da91" => :mavericks
+    sha1 "e9b5682c5fe0d5e5dfec55ce4c3f0957182755b1" => :mountain_lion
+    sha1 "4ae0eb86504082c622c642f3c27f6361d0af4fad" => :lion
+  end
+
   option :universal
   option "without-tools", "Build without command-line tools"
 
