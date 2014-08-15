1011058ea1d82e612b26eb527e27d08bd5081067
Dominik Gehl <dominik.gehl@returnpath.com>
2014-08-15 13:48:27 -0400

    tcpflow: fix URL
    Closes #31622.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/tcpflow.rb
+++ Library/Formula/tcpflow.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Tcpflow < Formula
   homepage 'https://github.com/simsong/tcpflow'
-  url 'http://www.digitalcorpora.org/downloads/tcpflow/tcpflow-1.4.4.tar.gz'
+  url 'http://digitalcorpora.org/downloads/tcpflow/tcpflow-1.4.4.tar.gz'
   sha1 'e4bc5ad08a81a39943bd1c799edefcdee09de784'
 
   head do
