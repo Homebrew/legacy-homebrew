61df041c14a57c32085c57b2f6be9a3df41a18a2
Artur Petrov <ardecvz@gmail.com>
2014-08-17 02:23:06 +0400

    enca 1.15

--- Library/Formula/enca.rb
+++ Library/Formula/enca.rb
@@ -1,13 +1,20 @@
-require 'formula'
+require "formula"
 
 class Enca < Formula
-  homepage 'http://freshmeat.net/projects/enca'
-  url 'http://dl.cihar.com/enca/enca-1.13.tar.gz'
-  sha1 'c6e25ea4f4cc53100bd6dacd62ea22e39151d067'
+  homepage "http://cihar.com/software/enca/"
+  url "http://dl.cihar.com/enca/enca-1.15.tar.gz"
+  sha1 "8a62202521e36d4159cbe6de64c2e7a0ec797e94"
+  head "https://github.com/nijel/enca.git"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    enca = "#{bin}/enca --language=none"
+    assert_match /ASCII/, `#{enca} <<< 'Testing...'`
+    assert_match /UCS-2/, `#{enca} --convert-to=UTF-16 <<< 'Testing...' | #{enca}`
   end
 end
