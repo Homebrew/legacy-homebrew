300a4ee678c62b2e6cda2980be9a84933b81a82d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-16 15:20:53 +0100

    camlistore: add 0.8 bottle.
    Closes #31637.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/camlistore.rb
+++ Library/Formula/camlistore.rb
@@ -6,6 +6,12 @@ class Camlistore < Formula
   url 'https://github.com/bradfitz/camlistore/archive/0.8.tar.gz'
   sha1 '076db79303fe1c62323b9dc0713ef1bfceb286b2'
 
+  bottle do
+    sha1 "0e23421d8dcd222bdaebbd9cdd4027f570e9c76d" => :mavericks
+    sha1 "2833a6aadcb6b11fe31fc7b8adef4a104dd06023" => :mountain_lion
+    sha1 "f72efd9e3c4654a0520b1ecf9991e2a23ea4cdad" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'go' => :build
   depends_on 'sqlite'
