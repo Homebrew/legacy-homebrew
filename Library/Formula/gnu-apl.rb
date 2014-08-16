c1ff030d7dd320e991b86928393090c34cf6d560
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-16 18:14:27 +0100

    gnu-apl: add 1.4 bottle.
    Closes #31641.

--- Library/Formula/gnu-apl.rb
+++ Library/Formula/gnu-apl.rb
@@ -6,6 +6,10 @@ class GnuApl < Formula
   mirror "http://ftp.gnu.org/gnu/apl/apl-1.4.tar.gz"
   sha1 "ee5dab7f7c0f5d79c435a20f3ddcafbda85ac22e"
 
+  bottle do
+    sha1 "e067c7741c9f7e174f0a5f06d99e59fe5b6e930b" => :mavericks
+  end
+
   # GNU Readline is required; libedit won't work.
   depends_on "readline"
   depends_on :macos => :mavericks
