22e35d499dd1fc1b4b919d5c93b2b510e62da3d9
Brett Koonce <koonce@gmail.com>
2014-08-17 09:33:14 -0700

    pdfgrep 1.3.1

--- Library/Formula/pdfgrep.rb
+++ Library/Formula/pdfgrep.rb
@@ -1,14 +1,14 @@
-require 'formula'
+require "formula"
 
 class Pdfgrep < Formula
-  homepage 'http://pdfgrep.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/pdfgrep/1.3.0/pdfgrep-1.3.0.tar.gz'
-  sha1 'cac20afdea7aee1602b2c33c3d8d36ec171c30bc'
+  homepage "http://pdfgrep.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/pdfgrep/1.3.1/pdfgrep-1.3.1.tar.gz"
+  sha1 "8d15760af0803ccea32760d5f68abe4224169639"
 
-  head 'https://git.gitorious.org/pdfgrep/pdfgrep.git'
+  head "https://git.gitorious.org/pdfgrep/pdfgrep.git"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'poppler'
+  depends_on "pkg-config" => :build
+  depends_on "poppler"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
