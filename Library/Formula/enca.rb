e6a617cb479209df21170ab8fd16dc1dc8d76e34
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-17 03:15:28 +0100

    enca: add 1.15 bottle.
    Closes #31646.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/enca.rb
+++ Library/Formula/enca.rb
@@ -6,6 +6,12 @@ class Enca < Formula
   sha1 "8a62202521e36d4159cbe6de64c2e7a0ec797e94"
   head "https://github.com/nijel/enca.git"
 
+  bottle do
+    sha1 "73833b5b16a9d7e0bee09a8a59d222a5a9a7079f" => :mavericks
+    sha1 "f8e7c4152e5b52a0e89a2ba9a7bba63758055e7a" => :mountain_lion
+    sha1 "068d6210c606fc464f18916e2ae0f3ed8ea8e3bf" => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
