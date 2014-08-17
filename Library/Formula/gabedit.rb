568ea8677c82b350ada6db7594bccd27b2bcc17f
Brett Koonce <koonce@gmail.com>
2014-08-17 09:32:06 -0700

    gabedit 2.4.8

--- Library/Formula/gabedit.rb
+++ Library/Formula/gabedit.rb
@@ -1,25 +1,19 @@
-require 'formula'
+require "formula"
 
 class Gabedit < Formula
-  homepage 'http://gabedit.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/gabedit/gabedit/Gabedit240/GabeditSrc240.tar.gz'
-  version '2.4.0'
-  sha1 '9c53590051da363f419a6b746594bf9f9aa92737'
+  homepage "http://gabedit.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/gabedit/gabedit/Gabedit248/GabeditSrc248.tar.gz"
+  version "2.4.8"
+  sha1 "7a48f42c39258471faa0a3942890c16b6290de41"
 
-  devel do
-    url 'https://downloads.sourceforge.net/project/gabedit/GabeditDevloppment/Gabedit247/GabeditSrc247.tar.gz'
-    version '2.4.7'
-    sha1 'fce078aced941742dc01f37b08a0ab4a2df54741'
-  end
-
-  depends_on 'pkg-config' => :build
-  depends_on 'gtk+'
-  depends_on 'gtkglext'
+  depends_on "pkg-config" => :build
+  depends_on "gtk+"
+  depends_on "gtkglext"
 
   def install
     args = []
     args << "OMPLIB=" << "OMPCFLAGS=" if ENV.compiler == :clang
-    system 'make', *args
-    bin.install 'gabedit'
+    system "make", *args
+    bin.install "gabedit"
   end
 end
