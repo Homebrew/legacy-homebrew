d322a6ff5758e83c12e9a75849aef22ae8edf984
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-18 18:41:27 +0100

    ldc: add 0.14.0 bottle.
    Closes #31684.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/ldc.rb
+++ Library/Formula/ldc.rb
@@ -5,6 +5,12 @@ class Ldc < Formula
   url "https://github.com/ldc-developers/ldc/releases/download/v0.14.0/ldc-0.14.0-src.tar.gz"
   sha1 "843c9fb374bb900560f9548d5c4646788f967103"
 
+  bottle do
+    sha1 "824272b3e29e1a5cdf03669099690f5c10021396" => :mavericks
+    sha1 "fd8121391a3528604cfb65da4dbf16ca499b8f76" => :mountain_lion
+    sha1 "fc18d767e69eca7d9a7a5456a5524c7ce1cb3d29" => :lion
+  end
+
   depends_on "cmake" => :build
   depends_on "llvm" => :build
   depends_on "libconfig"
