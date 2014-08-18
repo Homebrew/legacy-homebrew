5a5431e8e82b27a7631ce8cb0a5aa85385d82f90
Jack Nagel <jacknagel@gmail.com>
2014-08-18 01:22:22 -0500

    wine: use spec deps

--- Library/Formula/wine.rb
+++ Library/Formula/wine.rb
@@ -27,6 +27,8 @@ class Wine < Formula
     url "https://downloads.sourceforge.net/project/wine/Source/wine-1.7.24.tar.bz2"
     sha256 "5e9a9f250b6eb703cdc13c6dcfe025958dadddfdd3f8e683f46c2d642b5ec749"
 
+    depends_on "samba" => :optional
+
     # Patch to fix screen-flickering issues. Still relevant on 1.7.23.
     # https://bugs.winehq.org/show_bug.cgi?id=34166
     patch do
@@ -35,7 +37,10 @@ class Wine < Formula
     end
   end
 
-  head "git://source.winehq.org/git/wine.git"
+  head do
+    url "git://source.winehq.org/git/wine.git"
+    depends_on "samba" => :optional
+  end
 
   env :std
 
@@ -55,7 +60,6 @@ class Wine < Formula
   depends_on 'little-cms2'
   depends_on 'libicns'
   depends_on 'libtiff'
-  depends_on "samba" => :optional if !build.stable?
   depends_on 'sane-backends'
   depends_on 'libgsm' => :optional
 
