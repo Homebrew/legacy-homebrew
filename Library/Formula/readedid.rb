c0244e9ab975b9be39b70dc36176a0894aadb3b3
Richard <othercat@gmail.com>
2014-08-18 16:38:28 +0800

    readedid 1.0.1 (new formula)
    `readedid` is an EDID information provider from monitors connected to
    the Mac. Here comes the version 1.0.1.
    
    Since the tradition method which can send terminal command `ioreg` to
    receive the transcode of EDID that can help people to get the
    information needed(such as LCD vendor) was expired on new 13 inch
    Retina Macbook Pro models,I totally rewrite the 10-years-old ReadEDID
    source and bring it workable to modern OSX. I call it `readedid`.

--- Library/Formula/readedid.rb
+++ Library/Formula/readedid.rb
@@ -13,9 +13,9 @@ class Readedid < Formula
    def install
     system "cmake", "-G","Xcode", "./readEDID/"
     xcodebuild "-target readedid SYMROOT=build CONFIGURATION_BUILD_DIR=bin"
-    system "mkdir","/Users/lirichard/Developer/SourceCodes/github/homebrew/Cellar/readedid/1.0.1/bin/"
-    system "cp","bin/Debug/readedid","/Users/lirichard/Developer/SourceCodes/github/homebrew/Cellar/readedid/1.0.1/bin/"
-    #system "cp","doc/readedid.1","/Users/lirichard/Developer/SourceCodes/github/homebrew/share/man/man1/"
+    system "mkdir","/usr/local/Cellar/readedid/1.0.1/bin/"
+    system "cp","bin/Debug/readedid","/usr/local/Cellar/readedid/1.0.1/bin/"
+    #system "cp","doc/readedid.1","/usr/local/Cellar/foo/0.1/share/man/man1/"
    end
    test do
     #system "false"
