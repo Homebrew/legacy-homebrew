e32443de5db057ee8a1d535df903afe4a22bc36d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-19 09:10:32 +0100

    goaccess: update 0.8.3 bottle.
    Closes #31693.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/goaccess.rb
+++ Library/Formula/goaccess.rb
@@ -6,9 +6,9 @@ class Goaccess < Formula
   sha1 "e5ed8a3d6ba2c5b338b5e3e22da8024e7c58ded3"
 
   bottle do
-    sha1 "fa8fb53207de715e9b5898b47d8f0ab536ce3b6d" => :mavericks
-    sha1 "54bb6d73d9e97b678ac8aa9efef411189689054b" => :mountain_lion
-    sha1 "53b5930276abb6f3f2d4bf1bf9f79742dad0cd3d" => :lion
+    sha1 "9c5adcd9d96d92881c2aedecf13e1d42a4d829af" => :mavericks
+    sha1 "380c3c8298fb6c9e77297c708360d0abfc097650" => :mountain_lion
+    sha1 "0e9a7dfd63e7565fddd54a6b7fff13f1e8a81c46" => :lion
   end
 
   option "enable-geoip", "Enable IP location information using GeoIP"
