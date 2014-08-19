4c9389d6d4833d4db4f19afd98b3574f6fc28003
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-19 10:40:54 +0100

    cmus: add 2.6.0 bottle.
    Closes #31694.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cmus.rb
+++ Library/Formula/cmus.rb
@@ -4,6 +4,12 @@ class Cmus < Formula
   homepage "https://cmus.github.io/"
   head "https://github.com/cmus/cmus.git"
 
+  bottle do
+    sha1 "584b513eda4887a001a6a4613160f1181620ba94" => :mavericks
+    sha1 "b7ff43fa726aff8d9e26f7dbf1d2c136cb416910" => :mountain_lion
+    sha1 "5b455c2550115d65edb2b1ee537b0bdbfd62384e" => :lion
+  end
+
   stable do
     url "https://github.com/cmus/cmus/archive/v2.6.0.tar.gz"
     sha1 "aba00eb75335532c0413f7c819c2e2d12fcd4314"
