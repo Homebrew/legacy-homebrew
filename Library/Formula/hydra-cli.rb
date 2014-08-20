235a1174194cf4ff46e21fe2c02352293dcbebfc
Brett Koonce <koonce@gmail.com>
2014-08-19 22:44:52 -0700

    hydra-cli 1.0.1

--- Library/Formula/hydra-cli.rb
+++ Library/Formula/hydra-cli.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class HydraCli < Formula
-  homepage 'https://github.com/sdegutis/hydra-cli'
-  url 'https://github.com/sdegutis/hydra-cli/archive/1.0.tar.gz'
-  sha1 '15906ca4255839844635f26eab83843f0426b5f9'
-  head 'https://github.com/sdegutis/hydra-cli.git'
+  homepage "https://github.com/sdegutis/hydra-cli"
+  url "https://github.com/sdegutis/hydra-cli/archive/1.0.1.tar.gz"
+  sha1 "c71d8c2f5b63a99a030747eb34db49f774234acc"
+  head "https://github.com/sdegutis/hydra-cli.git"
 
   bottle do
     cellar :any
