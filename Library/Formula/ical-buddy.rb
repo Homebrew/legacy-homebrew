b8e04c9411fbb8475cf451fa1ea7bf89d2abc12d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-20 07:50:14 +0100

    ical-buddy: add 1.8.10 bottle.
    Closes #31711.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ical-buddy.rb
+++ Library/Formula/ical-buddy.rb
@@ -6,6 +6,13 @@ class IcalBuddy < Formula
   url 'https://github.com/ali-rantakari/icalBuddy/archive/v1.8.10.tar.gz'
   sha1 'e6d64289f063cc636dd9a517c7fa50a4abe70a14'
 
+  bottle do
+    cellar :any
+    sha1 "23aa4a5dc5375edfa78f0d61c6748dd71e388330" => :mavericks
+    sha1 "0c95d85ea39d4c9d288e1a938e1b5f5e791c29cd" => :mountain_lion
+    sha1 "a856530a9a30a218b2570b3aafb9610ab4297fb8" => :lion
+  end
+
   def install
     args = %W[icalBuddy icalBuddy.1 icalBuddyLocalization.1
       icalBuddyConfig.1 COMPILER=#{ENV.cc}]
