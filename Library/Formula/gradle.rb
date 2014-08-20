5a43487c540b47d6b592b054d4c1b95584a6b426
seanjreilly <seanjreilly@gmail.com>
2014-08-20 12:04:13 +0100

    gradle 2.1-rc-1 (devel)

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -5,6 +5,12 @@ class Gradle < Formula
   url 'https://downloads.gradle.org/distributions/gradle-2.0-bin.zip'
   sha1 '171d2290257c061a96410297f2596596862a847a'
 
+  devel do
+    url 'https://services.gradle.org/distributions/gradle-2.1-rc-1-bin.zip'
+    sha1 '0d912251e781fb44ed23da476bf3b195d6f9cfda'
+    version '2.1-rc-1'
+  end
+
   def install
     libexec.install %w[bin lib]
     bin.install_symlink libexec+'bin/gradle'
