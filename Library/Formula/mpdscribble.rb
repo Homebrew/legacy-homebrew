7c4f3ab80e4e316c00482e40ddc0ec5c5c23cce2
Lukas Stabe <lukas@stabe.de>
2014-08-20 05:33:55 +0200

    mpdscribble: provide launchd plist

--- Library/Formula/mpdscribble.rb
+++ Library/Formula/mpdscribble.rb
@@ -18,4 +18,33 @@ class Mpdscribble < Formula
     The configuration file was placed in #{etc}/mpdscribble.conf
     EOS
   end
+
+  plist_options :manual => "mpdscribble"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/mpdscribble</string>
+            <string>--no-daemon</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/mpdscribble_err.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/mpdscribble.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
 end
