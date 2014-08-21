a31aee762c6479ce7bf65353d156a55d3eb6e069
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-21 17:17:32 +0100

    lv: add 4.51 bottle.
    Closes #31764.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/lv.rb
+++ Library/Formula/lv.rb
@@ -6,6 +6,12 @@ class Lv < Formula
   version '4.51'
   sha1 '1a70299c27aa317a436976a557853858db4dcb5f'
 
+  bottle do
+    sha1 "077f8feee1b78425a38f1775cfa0bba5b5334569" => :mavericks
+    sha1 "08a9356422f94eddb2d767ab167048fb291cede7" => :mountain_lion
+    sha1 "4bc23132a51ab1fb35ec2af15473d1d928c9475d" => :lion
+  end
+
   def patches
     DATA
   end
