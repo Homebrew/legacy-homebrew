e2e7182d9c20f329b4665b9b10573ca672ef80b3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-21 09:13:25 +0100

    ios-sim: add 2.0.1 bottle.

--- Library/Formula/ios-sim.rb
+++ Library/Formula/ios-sim.rb
@@ -6,6 +6,13 @@ class IosSim < Formula
   sha1 "df15d7f49561b92f8d3bf8832db60b83d7ce355e"
   head "https://github.com/phonegap/ios-sim.git"
 
+  bottle do
+    cellar :any
+    sha1 "3f28d1596a8d019ecb4fc70f758190a21e56ebcc" => :mavericks
+    sha1 "9ed5958db90bb227a0f0afc878f4cfb4a835fcca" => :mountain_lion
+    sha1 "4ad30f386778b5e395755109ec50fb725066099c" => :lion
+  end
+
   depends_on :macos => :lion
 
   def install
