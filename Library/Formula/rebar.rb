371de15fb445f687229029bd255fa95338862447
Brett Koonce <koonce@gmail.com>
2014-08-21 00:26:50 -0700

    rebar 2.5.1

--- Library/Formula/rebar.rb
+++ Library/Formula/rebar.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Rebar < Formula
   homepage "https://github.com/rebar/rebar"
-  url "https://github.com/rebar/rebar/archive/2.5.0.tar.gz"
-  sha1 "2c52d3970f0ff10d5c0207ed1e05efbac1bb5a94"
+  url "https://github.com/rebar/rebar/archive/2.5.1.tar.gz"
+  sha1 "cf8d3e33c21f09b826a52a681f5b729559a3280c"
 
   head "https://github.com/rebar/rebar.git", :branch => "master"
 
