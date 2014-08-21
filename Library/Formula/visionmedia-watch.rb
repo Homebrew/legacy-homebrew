e76f6db9879e8e27f5192a1c6e9db2753cd2cf7d
Brett Koonce <koonce@gmail.com>
2014-08-21 00:27:15 -0700

    visionmedia-watch 0.3.1

--- Library/Formula/visionmedia-watch.rb
+++ Library/Formula/visionmedia-watch.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class VisionmediaWatch < Formula
-  homepage 'https://github.com/visionmedia/watch'
-  url 'https://github.com/visionmedia/watch/archive/0.2.1.tar.gz'
-  sha1 '752006185eadb34a0ec3bdbea259c27359d886d0'
+  homepage "https://github.com/visionmedia/watch"
+  url "https://github.com/visionmedia/watch/archive/0.3.1.tar.gz"
+  sha1 "f5dc414eea6c5b079535c843e54ba638bcf0172b"
 
-  head 'https://github.com/visionmedia/watch.git'
+  head "https://github.com/visionmedia/watch.git"
 
-  conflicts_with 'watch'
+  conflicts_with "watch"
 
   def install
     bin.mkdir
