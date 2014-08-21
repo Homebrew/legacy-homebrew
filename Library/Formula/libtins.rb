6273c538baad2bace683a16c770bdcd40ff2d021
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-21 14:58:11 -0500

    libtins: add 3.0 bottle.

--- Library/Formula/libtins.rb
+++ Library/Formula/libtins.rb
@@ -6,6 +6,13 @@ class Libtins < Formula
   sha1 "0e8a31dde9ba9ec55cc4c6d3142fe4e3912598e3"
   head "https://github.com/mfontanini/libtins.git"
 
+  bottle do
+    cellar :any
+    sha1 "1312df6edac4a92790adba7184f73c65aaec376f" => :mavericks
+    sha1 "66dc536e0612a5e66192e8ecb34f2c5e9cb9ed3d" => :mountain_lion
+    sha1 "90b958acd1b71c96d99c0292a22731933a6ee456" => :lion
+  end
+
   option :cxx11
 
   def install
