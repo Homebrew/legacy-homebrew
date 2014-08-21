5f1e1fb9610f968eeb98e5f6d7112441449406e6
Brett Koonce <koonce@gmail.com>
2014-08-21 00:26:08 -0700

    git-encrypt 0.3.2

--- Library/Formula/git-encrypt.rb
+++ Library/Formula/git-encrypt.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class GitEncrypt < Formula
-  homepage 'https://github.com/shadowhand/git-encrypt'
-  url 'https://github.com/shadowhand/git-encrypt/archive/0.3.1.tar.gz'
-  sha1 '2a9a0bff06c0ac1a95961cbf61c3efa1c926a294'
+  homepage "https://github.com/shadowhand/git-encrypt"
+  url "https://github.com/shadowhand/git-encrypt/archive/0.3.2.tar.gz"
+  sha1 "f15eca78cb2ea43bb4f258dea87fe3e6f2dd08b5"
 
-  head 'https://github.com/shadowhand/git-encrypt.git', :branch => 'master'
+  head "https://github.com/shadowhand/git-encrypt.git", :branch => "master"
 
   def install
-    bin.install 'gitcrypt'
+    bin.install "gitcrypt"
   end
 end
