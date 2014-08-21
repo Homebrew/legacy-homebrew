0a5730f109bf9ad81acaee67fa3a0558f941ba1f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-21 09:13:26 +0100

    visionmedia-watch: add 0.3.1 bottle.
    Closes #31753.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/visionmedia-watch.rb
+++ Library/Formula/visionmedia-watch.rb
@@ -7,6 +7,13 @@ class VisionmediaWatch < Formula
 
   head "https://github.com/visionmedia/watch.git"
 
+  bottle do
+    cellar :any
+    sha1 "8491a4f2601bbb56bb071ea7c800d529d97e3b85" => :mavericks
+    sha1 "6fd809bb5216c0e1d75e7ea43c52a55b6bd52f0b" => :mountain_lion
+    sha1 "4e046b6c5fdc152127cb968b53324cc3fe362745" => :lion
+  end
+
   conflicts_with "watch"
 
   def install
