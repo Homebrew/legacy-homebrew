db130ab2d6fbe42d368992d0599bcb2a4c443b3f
Thierry Moisan <thierry.moisan@gmail.com>
2014-08-21 12:43:49 -0400

    Add a test and a revision

--- Library/Formula/git-latexdiff.rb
+++ Library/Formula/git-latexdiff.rb
@@ -1,9 +1,13 @@
 require "formula"
 class GitLatexdiff < Formula
   homepage "https://gitorious.org/git-latexdiff"
-  url "git://gitorious.org/git-latexdiff/git-latexdiff.git"
+  url "git://gitorious.org/git-latexdiff/git-latexdiff.git", :revision => "cdb6c1b"
 
   def install
     system "make", "install"
+  end
+
+  test do
+    system "git", "latexdiff", "-h"
   end
 end
