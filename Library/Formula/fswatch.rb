f773ff58b72bf5c6daed003657d5e0eaaa82a529
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-21 10:01:12 +0100

    fswatch: update 1.4.0 bottle.
    Closes #31756.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/fswatch.rb
+++ Library/Formula/fswatch.rb
@@ -6,9 +6,9 @@ class Fswatch < Formula
   sha1 "12031348b0afcc12105c63271aa57e599fc9ab94"
 
   bottle do
-    sha1 "4f002ffcb2a2c1fa93c1eb1976b12e6eed38304f" => :mavericks
-    sha1 "6d920dae8c0086e7a2cfadd1ed949baee055a208" => :mountain_lion
-    sha1 "3bb0ac74a32cd00429bef19b088687e4ff836307" => :lion
+    sha1 "a86d9f5e6e2c5989a204956f6a1329be597d1255" => :mavericks
+    sha1 "a969777e2fb984d07c4a9d6ddc99b30072885426" => :mountain_lion
+    sha1 "2016e763b3e217e8dc808485309e68f13c20722d" => :lion
   end
 
   needs :cxx11
