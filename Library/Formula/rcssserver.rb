b8b663a9dacbac089cb2c76570f670d40906da43
Takenori KUBO <tknrkb@gmail.com>
2014-08-21 18:50:43 +0200

    Integrate rcsslogplayer and rcssmonitor into rcssserver
    Closes #31768.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/rcssserver.rb
+++ Library/Formula/rcssserver.rb
@@ -12,8 +12,28 @@ class Rcssserver < Formula
     sha1 "ab94f23eae47b48ce2f9ed3d94e727f2d7387289" => :lion
   end
 
+  stable do
+    resource "rcssmonitor" do
+      url "https://downloads.sourceforge.net/sserver/rcssmonitor/15.1.1/rcssmonitor-15.1.1.tar.gz"
+      sha1 "60483838a81acd8ada6d228a86e54faeb019ab10"
+    end
+
+    resource "rcsslogplayer" do
+      url "https://downloads.sourceforge.net/sserver/rcsslogplayer/15.1.1/rcsslogplayer-15.1.1.tar.gz"
+      sha1 "d0b8f8e8a4328398655140e7c019149ab8d9c1c3"
+    end
+  end
+
   head do
     url "svn://svn.code.sf.net/p/sserver/code/rcss/trunk/rcssserver"
+
+    resource "rcssmonitor" do
+      url "svn://svn.code.sf.net/p/sserver/code/rcss/trunk/rcssmonitor_qt4"
+    end
+
+    resource "rcsslogplayer" do
+      url "svn://svn.code.sf.net/p/sserver/code/rcss/trunk/rcsslogplayer"
+    end
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -23,6 +43,7 @@ class Rcssserver < Formula
   depends_on "flex" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
+  depends_on "qt"
 
   def install
     ENV.j1
@@ -43,9 +64,19 @@ class Rcssserver < Formula
 
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
+
+    resources.each do |r|
+      r.stage do
+        system "./bootstrap" if build.head?
+        system "./configure", "--prefix=#{prefix}"
+        system "make", "install"
+      end
+    end
   end
 
   test do
     system "#{bin}/rcssserver help | head -1 | grep 'rcssserver-#{version}'"
+    system "#{bin}/rcsslogplayer --version | tail -1 | grep 'rcsslogplayer Version'"
+    system "#{bin}/rcssmonitor --version | tail -1 | grep 'rcssmonitor Version'"
   end
 end
