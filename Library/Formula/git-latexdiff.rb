1e1350da6befc998c93e854336c0e518c50ee16b
Thierry Moisan <thierry.moisan@gmail.com>
2014-08-21 13:22:42 -0400

    Add a newline after require and switch to bin.install instead of make install

--- Library/Formula/git-latexdiff.rb
+++ Library/Formula/git-latexdiff.rb
@@ -1,10 +1,11 @@
 require "formula"
+
 class GitLatexdiff < Formula
   homepage "https://gitorious.org/git-latexdiff"
   url "git://gitorious.org/git-latexdiff/git-latexdiff.git", :revision => "cdb6c1b"
 
   def install
-    system "make", "install"
+    bin.install "git-latexdiff"
   end
 
   test do
