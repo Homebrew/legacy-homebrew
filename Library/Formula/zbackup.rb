2c2b2c3751d67a601bd542818fac7be5fefb2263
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-22 19:04:08 +0100

    zbackup: add 1.2_1 bottle.
    Closes #31797.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/zbackup.rb
+++ Library/Formula/zbackup.rb
@@ -6,6 +6,13 @@ class Zbackup < Formula
   sha1 'e87dfaeeeea0d59f4af00d3ce248aaabf1a25cb9'
   revision 1
 
+  bottle do
+    cellar :any
+    sha1 "346eb5faf369ed6843c8babca0d6f1dcce7795f9" => :mavericks
+    sha1 "4e2f6e6258feaf7723c40c9c6b66348e0a03bab3" => :mountain_lion
+    sha1 "d9641dcca384fda680615ed3add69ff13e8799a5" => :lion
+  end
+
   depends_on 'cmake' => :build
   depends_on 'openssl'
   depends_on 'protobuf'
