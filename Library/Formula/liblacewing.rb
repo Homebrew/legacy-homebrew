a41c3712661373a61b04246e4e9f7c9dde963a49
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-22 19:04:05 +0100

    liblacewing: add 0.5.4_1 bottle.

--- Library/Formula/liblacewing.rb
+++ Library/Formula/liblacewing.rb
@@ -7,6 +7,13 @@ class Liblacewing < Formula
   sha1 '078486a4dcd6ce33c2c881954c5dc82843411ac9'
   revision 1
 
+  bottle do
+    cellar :any
+    sha1 "581217cd2f70273475b3ea50ab2faafbcf6898f0" => :mavericks
+    sha1 "620f9e799192d74e49a5a2098d0661b69b3f3a14" => :mountain_lion
+    sha1 "39c2c5dfd392344bba96f177b8afc79ab6fc73b8" => :lion
+  end
+
   # Use a newer OpenSSL to get SPDY support
   depends_on 'openssl'
 
