9055f646ee3a6354555d3f16f96ec7b8fe278e55
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-22 19:04:06 +0100

    msmtp: add 1.4.32_1 bottle.

--- Library/Formula/msmtp.rb
+++ Library/Formula/msmtp.rb
@@ -6,6 +6,12 @@ class Msmtp < Formula
   sha1 '03186a70035dbbf7a31272a20676b96936599704'
   revision 1
 
+  bottle do
+    sha1 "17021ef72dbf0c141e352531a2a61923c55a91b7" => :mavericks
+    sha1 "ccc3ff12b2a2bd7299b3fc918eaafa3546a95598" => :mountain_lion
+    sha1 "c028b723e3c4a888b05935a3b5a5e65889e53d7e" => :lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'openssl'
 
