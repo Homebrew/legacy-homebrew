dbd4d5f3eabaf74eb625e5bea273040d5b3fa156
scottsuch <sgorsuch@gmail.com>
2014-08-22 15:01:07 -0700

    Bumps avro-tools to 1.7.7

--- Library/Formula/avro-tools.rb
+++ Library/Formula/avro-tools.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class AvroTools < Formula
   homepage "http://avro.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=avro/avro-1.7.6/java/avro-tools-1.7.6.jar"
-  sha1 "56ebab491e9dce5b1812c3867a8385cd636bb90a"
+  url "http://www.apache.org/dyn/closer.cgi?path=avro/avro-1.7.7/java/avro-tools-1.7.7.jar"
+  sha1 "a2c493c897583892b0423f0c9c732c242cd8816d"
 
   def install
     libexec.install "avro-tools-#{version}.jar"
