c190da12c71fa778bec25cc5e5bd2694d3d07a3f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-22 19:04:06 +0100

    ncrack: add 0.4ALPHA_1 bottle.

--- Library/Formula/ncrack.rb
+++ Library/Formula/ncrack.rb
@@ -6,6 +6,12 @@ class Ncrack < Formula
   sha256 'f8bd7e0ef68559490064ec0a5f139b2b9c49aeaf9f6323e080db9ff344c87603'
   revision 1
 
+  bottle do
+    sha1 "b2931a0cd792bde2eb8582f43bb43243e7542978" => :mavericks
+    sha1 "9a743f6cef362833984f5eb9752b413f7f7960d8" => :mountain_lion
+    sha1 "46c7cd34de5c0725fcee58cf90acfc7902b1335c" => :lion
+  end
+
   depends_on "openssl"
 
   def install
