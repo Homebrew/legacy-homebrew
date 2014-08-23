44da7826b11ceddc0d9117f93111d59611a764a5
Jack Nagel <jacknagel@gmail.com>
2014-08-22 22:55:10 -0500

    Use a set to track failed downloads

--- Library/Homebrew/cmd/fetch.rb
+++ Library/Homebrew/cmd/fetch.rb
@@ -60,19 +60,15 @@ module Homebrew
   private
 
   def retry_fetch? f
-    @fetch_failed ||= {}
-    already_failed = @fetch_failed.fetch(f.name, false)
-
-    if already_failed || !ARGV.include?("--retry")
+    @fetch_failed ||= Set.new
+    if ARGV.include?("--retry") && @fetch_failed.add?(f.name)
+      ohai "Retrying download"
+      f.clear_cache
+      true
+    else
       Homebrew.failed = true
-      return false
+      false
     end
-
-    ohai "Retrying download"
-
-    f.clear_cache
-    @fetch_failed[f.name] = true
-    true
   end
 
   def fetch_fetchable f
