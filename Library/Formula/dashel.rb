48c71f8e78433b8ece5262064553bacb348e7f82
Stéphane Magnenat <stephane@magnenat.net>
2014-08-24 12:50:55 +0200

    Updated upstream

--- Library/Formula/dashel.rb
+++ Library/Formula/dashel.rb
@@ -2,13 +2,10 @@ require "formula"
 
 class Dashel < Formula
   homepage "https://github.com/aseba-community/dashel"
-  url "https://github.com/aseba-community/dashel/archive/1.0.8.tar.gz"
-  sha1 "ea93321250c4bb32c48e7fecf72554a068d64192"
+  url "https://github.com/aseba-community/dashel/archive/1.0.8.1.tar.gz"
+  sha1 "2bb73e05e297fd82067515e57a053e2860b0260e"
 
   depends_on "cmake" => :build
-
-  # fix broken installation path for dashelConfig.cmake
-  patch :DATA
 
   def install
     system "cmake", ".", *std_cmake_args
@@ -20,19 +17,3 @@ class Dashel < Formula
     system "#{share}/test/portlist"
   end
 end
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 4121c0a..a1fda9b 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -73,7 +73,7 @@ if (WIN32 AND NOT CYGWIN)
- 	set(INSTALL_CMAKE_DIR CMake)
- else()
- 	if (APPLE)
--		set(INSTALL_CMAKE_DIR dashel.framework/Resources/CMake/)
-+		set(INSTALL_CMAKE_DIR Frameworks/dashel.framework/Resources/CMake/)
- 	else()
- 		set(INSTALL_CMAKE_DIR share/dashel/CMake)
- 	endif()
-
