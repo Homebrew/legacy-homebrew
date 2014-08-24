fe58588fe5610ae8790812111216e3d65d21a11f
Mike McQuaid <mike@mikemcquaid.com>
2014-08-24 17:09:28 +0100

    osslsigncode: use double-quotes, "make", "install"

--- Library/Formula/osslsigncode.rb
+++ Library/Formula/osslsigncode.rb
@@ -1,10 +1,11 @@
-require 'formula'
+require "formula"
 
 class Osslsigncode < Formula
-  homepage 'http://sourceforge.net/projects/osslsigncode/'
-  url 'https://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.6.tar.gz'
-  sha1 '83c169638c8c1e0122127674cbb73d2e0e6b5bc2'
+  homepage "http://sourceforge.net/projects/osslsigncode/"
+  url "https://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.6.tar.gz"
+  sha1 "83c169638c8c1e0122127674cbb73d2e0e6b5bc2"
   revision 1
+
   bottle do
     cellar :any
     sha1 "0f6af448002b345962fc41199ade20fb16b74891" => :mavericks
@@ -12,12 +13,12 @@ class Osslsigncode < Formula
     sha1 "53019dd5187a15410279c583c2445a38bfe219ba" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'openssl'
-  depends_on 'libgsf' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
+  depends_on "libgsf" => :optional
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
