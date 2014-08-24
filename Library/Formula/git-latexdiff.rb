6e0b86619b4c4f453b66a87584a2b59eb698e6d8
Thierry Moisan <thierry.moisan@gmail.com>
2014-08-24 16:37:28 -0400

    Use the new version tag

--- Library/Formula/git-latexdiff.rb
+++ Library/Formula/git-latexdiff.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class GitLatexdiff < Formula
   homepage "https://gitorious.org/git-latexdiff"
-  url "git://gitorious.org/git-latexdiff/git-latexdiff.git", :revision => "cdb6c1b"
+  url "git://gitorious.org/git-latexdiff/git-latexdiff.git", :tag => "v1.0.0"
 
   def install
     bin.install "git-latexdiff"
