ede95713127c394009c0a4bf58836b0d14411b7d
Mike McQuaid <mike@mikemcquaid.com>
2014-08-24 17:09:09 +0100

    ori: use double-quotes.

--- Library/Formula/ori.rb
+++ Library/Formula/ori.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Ori < Formula
-  homepage 'http://ori.scs.stanford.edu/'
-  url 'https://bitbucket.org/orifs/ori/downloads/ori-0.8.1.tgz'
-  sha1 '3ad31ab14bdb9305423f66cb919fdc445215f3c9'
+  homepage "http://ori.scs.stanford.edu/"
+  url "https://bitbucket.org/orifs/ori/downloads/ori-0.8.1.tgz"
+  sha1 "3ad31ab14bdb9305423f66cb919fdc445215f3c9"
   revision 1
 
   bottle do
@@ -13,12 +13,12 @@ class Ori < Formula
     sha1 "2f653f4b5c98850aef526c724b1005520c562189" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'scons' => :build
-  depends_on 'boost'
-  depends_on 'osxfuse'
-  depends_on 'libevent'
-  depends_on 'openssl'
+  depends_on "pkg-config" => :build
+  depends_on "scons" => :build
+  depends_on "boost"
+  depends_on "osxfuse"
+  depends_on "libevent"
+  depends_on "openssl"
 
   def install
     scons "BUILDTYPE=RELEASE"
