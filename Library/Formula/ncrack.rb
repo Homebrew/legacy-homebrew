19868b5dbc26abc542738f1e7da15057c535707a
Mike McQuaid <mike@mikemcquaid.com>
2014-08-24 17:09:00 +0100

    ncrack: use double-quotes and "make", "install".

--- Library/Formula/ncrack.rb
+++ Library/Formula/ncrack.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Ncrack < Formula
-  homepage 'http://nmap.org/ncrack/'
-  url 'http://nmap.org/ncrack/dist/ncrack-0.4ALPHA.tar.gz'
-  sha256 'f8bd7e0ef68559490064ec0a5f139b2b9c49aeaf9f6323e080db9ff344c87603'
+  homepage "http://nmap.org/ncrack/"
+  url "http://nmap.org/ncrack/dist/ncrack-0.4ALPHA.tar.gz"
+  sha256 "f8bd7e0ef68559490064ec0a5f139b2b9c49aeaf9f6323e080db9ff344c87603"
   revision 1
 
   bottle do
@@ -19,6 +19,6 @@ class Ncrack < Formula
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
