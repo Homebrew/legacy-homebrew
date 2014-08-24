1f65d13533fda4cb255db7bacb87e88b505224ad
Mike McQuaid <mike@mikemcquaid.com>
2014-08-24 17:08:38 +0100

    lynx: use double-quotes and "make", "install".

--- Library/Formula/lynx.rb
+++ Library/Formula/lynx.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Lynx < Formula
-  homepage 'http://lynx.isc.org/release/'
-  url 'http://lynx.isc.org/current/lynx2.8.8rel.2.tar.bz2'
-  version '2.8.8rel.2'
-  sha1 '65bbf95627c88723bbb5880155e5fe01c2753d0c'
+  homepage "http://lynx.isc.org/release/"
+  url "http://lynx.isc.org/current/lynx2.8.8rel.2.tar.bz2"
+  version "2.8.8rel.2"
+  sha1 "65bbf95627c88723bbb5880155e5fe01c2753d0c"
   revision 1
 
   bottle do
@@ -25,10 +25,10 @@ class Lynx < Formula
                           "--with-bzlib",
                           "--with-ssl=#{Formula["openssl"].opt_prefix}",
                           "--enable-ipv6"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/lynx", '-dump', 'http://checkip.dyndns.org'
+    system "#{bin}/lynx", "-dump", "http://checkip.dyndns.org"
   end
 end
