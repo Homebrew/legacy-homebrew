5299770b9f19d33bc394f2adac12300c0c95455c
Mike McQuaid <mike@mikemcquaid.com>
2014-08-24 17:09:54 +0100

    sipp: use double-quotes.

--- Library/Formula/sipp.rb
+++ Library/Formula/sipp.rb
@@ -1,7 +1,7 @@
-require 'formula'
+require "formula"
 
 class Sipp < Formula
-  homepage 'http://sipp.sourceforge.net/'
+  homepage "http://sipp.sourceforge.net/"
   url "https://github.com/SIPp/sipp/archive/v3.4.1.tar.gz"
   sha1 "40a3a7b7549e578997a8f181e557c3cdae39474f"
   revision 1
