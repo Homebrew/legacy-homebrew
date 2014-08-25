5dca4c9c09fdcfaebec6c0a56f3250da380484dd
Stéphane Magnenat <stephane@magnenat.net>
2014-08-25 08:43:45 +0200

    Updated upstream version.

--- Library/Formula/dashel.rb
+++ Library/Formula/dashel.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Dashel < Formula
   homepage "https://github.com/aseba-community/dashel"
-  url "https://github.com/aseba-community/dashel/archive/1.0.8.1.tar.gz"
-  sha1 "2bb73e05e297fd82067515e57a053e2860b0260e"
+  url "https://github.com/aseba-community/dashel/archive/1.0.8.2.tar.gz"
+  sha1 "40db2b76a62c743fbbccc2d6d425cc7479a9dcac"
 
   depends_on "cmake" => :build
 
