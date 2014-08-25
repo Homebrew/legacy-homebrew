b0832737c81b19a7e33c85f7630d9771413b886a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-25 13:31:05 +0100

    truecrack: add 3.5 bottle.

--- Library/Formula/truecrack.rb
+++ Library/Formula/truecrack.rb
@@ -6,6 +6,13 @@ class Truecrack < Formula
   sha1 "21b2a0f2f860ecf401cfc1ac8191638b7410fc64"
   version "3.5"
 
+  bottle do
+    cellar :any
+    sha1 "605d9c2eda636f918413f4ae3d9146bfa5168516" => :mavericks
+    sha1 "a320ed86f967c5b0e430c6506804f5a1574d3143" => :mountain_lion
+    sha1 "67c8b6637db713e7582f6db320fdc1926e7f531b" => :lion
+  end
+
   # Fix missing return value compilation issue
   # https://code.google.com/p/truecrack/issues/detail?id=41
   patch do
