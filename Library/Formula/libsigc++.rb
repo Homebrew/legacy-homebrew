776cdf66dc6f6eeebb31e9fc7e0805b27f975575
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-25 00:01:25 -0500

    libsigc++: update 2.2.11 bottle.

--- Library/Formula/libsigc++.rb
+++ Library/Formula/libsigc++.rb
@@ -6,6 +6,9 @@ class Libsigcxx < Formula
   sha256 '9834045f74f56752c2c6b3cdc195c30ab8314ad22dc8e626d6f67f940f1e4957'
 
   bottle do
+    sha1 "c3c2dd772631225eba25b4df6ce357a41f88af11" => :mavericks
+    sha1 "f8270257d06a6cc5c178c243b3cfa3d44cb4a4c0" => :mountain_lion
+    sha1 "dc6bebf72b9d3eb3d91117e4ccf503edb0b1293c" => :lion
   end
 
   option :cxx11
