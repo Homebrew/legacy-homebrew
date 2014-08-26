d33e44820a2a2ff219830537fa87f11e9b6cefac
David Christenson <david@the-david.com>
2014-08-26 13:06:07 -0600

    x264: use HTTPS links
    Use HTTPS on homepage and git links, change single quotes to double.
    
    Closes #31909.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/x264.rb
+++ Library/Formula/x264.rb
@@ -1,15 +1,15 @@
-require 'formula'
+require "formula"
 
 class X264 < Formula
-  homepage "http://www.videolan.org/developers/x264.html"
+  homepage "https://www.videolan.org/developers/x264.html"
   # the latest commit on the stable branch
-  url "http://git.videolan.org/git/x264.git", :revision => "021c0dc6c95c1bc239c9db78a80dd85fc856a4dd"
+  url "https://git.videolan.org/git/x264.git", :revision => "021c0dc6c95c1bc239c9db78a80dd85fc856a4dd"
   version "r2455"
-  head "http://git.videolan.org/git/x264.git"
+  head "https://git.videolan.org/git/x264.git"
 
   devel do
     # the latest commit on the master branch
-    url "http://git.videolan.org/git/x264.git", :revision => "dd79a61e0e354a432907f2d1f7137b27a12dfce7"
+    url "https://git.videolan.org/git/x264.git", :revision => "dd79a61e0e354a432907f2d1f7137b27a12dfce7"
     version "r2479"
   end
 
@@ -20,9 +20,9 @@ class X264 < Formula
     sha1 "6e5fb8f851796c81a235b18cf88a23d08d743603" => :lion
   end
 
-  depends_on 'yasm' => :build
+  depends_on "yasm" => :build
 
-  option '10-bit', 'Build a 10-bit x264 (default: 8-bit)'
+  option "10-bit", "Build a 10-bit x264 (default: 8-bit)"
   option "with-mp4=", "Select mp4 output: none (default), l-smash or gpac"
 
   case ARGV.value "with-mp4"
@@ -42,7 +42,7 @@ class X264 < Formula
     elsif Formula["gpac"].installed?
       args << "--disable-lsmash"
     end
-    args << "--bit-depth=10" if build.include? '10-bit'
+    args << "--bit-depth=10" if build.include? "10-bit"
 
     # For running version.sh correctly
     buildpath.install_symlink cached_download/".git"
