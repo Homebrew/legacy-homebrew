5394bc1b30015164c3c8f4b816a1feda307c0c85
William Woodruff <william@tuffbizz.com>
2014-08-25 20:00:44 -0400

    rename class to prevent naming conflict

--- Library/Formula/git-hub.rb
+++ Library/Formula/git-hub.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class GitHub < Formula
+class GitHubPlugin < Formula
 	homepage "https://github.com/ingydotnet/git-hub"
 	url "https://github.com/ingydotnet/git-hub/archive/0.1.3.tar.gz"
 	sha1 "743517bc205ed4e3ac6c3077247d7b49ff249e87"
