da8fb60ccea7eed2a40b3d7bf74b8e876b186845
Giovanni Silva <giovanni@atende.info>
2014-08-26 18:50:10 -0300

    Clean up some code in dartium install

--- Library/Formula/dartium.rb
+++ Library/Formula/dartium.rb
@@ -1,10 +1,9 @@
 require 'formula'
 
 class Dartium < Formula
-  homepage "https://www.dartlang.org"  
-  ROOT_URL = "https://storage.googleapis.com/dart-archive/channels"
+  homepage "https://www.dartlang.org"
   release_version = '38663'
-  base_url = "#{ROOT_URL}/stable/release/#{release_version}"
+  base_url = "https://storage.googleapis.com/dart-archive/channels/stable/release/#{release_version}"
   version release_version
   url "#{base_url}/dartium/dartium-macos-ia32-release.zip"
   sha256 "25ecdbf0b80f362611fa76051db2cbff453bf8bd6fecaf2f71f6614d6907198d"
@@ -17,9 +16,8 @@ class Dartium < Formula
   end
 
   def install
-    name = "dartium"
     prefix.install Dir['*']
-    (bin+name).write shim_script "Chromium.app"
+    (bin+"dartium").write shim_script "Chromium.app"
   end
 
   def caveats; <<-EOS.undent
