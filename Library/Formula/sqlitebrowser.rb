0cfbb9bb85ce0e84db5640e10b52ac7e072ec276
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-26 08:02:02 +0100

    sqlitebrowser: add 3.3.0 bottle.
    Closes #31897.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/sqlitebrowser.rb
+++ Library/Formula/sqlitebrowser.rb
@@ -7,6 +7,13 @@ class Sqlitebrowser < Formula
 
   head 'https://github.com/sqlitebrowser/sqlitebrowser.git'
 
+  bottle do
+    cellar :any
+    sha1 "750444aa3b7f137641c34d9bc69610bb1eca74ed" => :mavericks
+    sha1 "29dfedf65d80f50f2a3586a0a4e9e3e40a261b86" => :mountain_lion
+    sha1 "ea3855b388a381a7cf281b1155841df9aa02c687" => :lion
+  end
+
   depends_on 'qt'
   depends_on 'cmake' => :build
   depends_on 'sqlite' => 'with-functions'
