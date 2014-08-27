f875875daa242fe546b5e3e57c30324319bce412
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-28 00:31:08 +0100

    argyll-cms: add 1.6.3 bottle.
    Closes #31931.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/argyll-cms.rb
+++ Library/Formula/argyll-cms.rb
@@ -6,6 +6,13 @@ class ArgyllCms < Formula
   sha1 '0c4d48a6cf6800a8d445bf6cd3c248a40799cf14'
   version '1.6.3'
 
+  bottle do
+    cellar :any
+    sha1 "555b6383cdf85acd91a21dbff6d3aaaa2308c713" => :mavericks
+    sha1 "02cbcdb8abcd0dc2534b8c54b8ebdcdb0ffc92e7" => :mountain_lion
+    sha1 "716adfdabde0d81124ca57388655dc36e9e1c365" => :lion
+  end
+
   depends_on 'jam' => :build
   depends_on 'jpeg'
   depends_on 'libtiff'
