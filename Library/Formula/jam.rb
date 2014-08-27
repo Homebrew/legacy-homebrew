ca4d2a197ab2dc8ac79d87091cd7b8067c896e38
balr0g <balrog032@gmail.com>
2014-08-27 16:48:33 -0400

    Update jam to latest upstream package

--- Library/Formula/jam.rb
+++ Library/Formula/jam.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Jam < Formula
   homepage 'http://www.perforce.com/resources/documentation/jam'
-  url 'ftp://ftp.perforce.com/jam/jam-2.5.zip'
-  sha1 '794a3f4483315c6b9f010f03b592646d3815328c'
+  url 'https://swarm.workshop.perforce.com/projects/perforce_software-jam/download/main/jam-2.6.zip'
+  sha1 'e6d2f909798fad32f3bd7c08699265f82b05b526'
 
   conflicts_with 'ftjam', :because => 'both install a `jam` binary'
 
   def install
-    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
-    bin.install "bin.macosx/jam", "bin.macosx/mkjambase"
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}", "LOCATE_TARGET=bin"
+    bin.install "bin/jam", "bin/mkjambase"
   end
 end
