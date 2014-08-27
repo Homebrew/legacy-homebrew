8e1d22813c746294ab6c565bcf6b1a7b474c1907
Trevor Powell <trevor.powell@morningstar.com>
2014-08-27 09:17:48 -0500

    nvm 0.14.0

--- Library/Formula/nvm.rb
+++ Library/Formula/nvm.rb
@@ -3,8 +3,8 @@ require "formula"
 class Nvm < Formula
   homepage "https://github.com/creationix/nvm"
   head "https://github.com/creationix/nvm.git"
-  url "https://github.com/creationix/nvm/archive/v0.13.1.tar.gz"
-  sha1 "b5331a101e0719c2a17344f498c408dc475a5190"
+  url "https://github.com/creationix/nvm/archive/v0.14.0.tar.gz"
+  sha1 "cfd734392c46eb757634dbea0db5d2cfbd9503ea"
 
   def install
     prefix.install "nvm.sh"
