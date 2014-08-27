bbd951bda11838db045cffa6c0709c430dc54cfd
Misty De Meo <mistydemeo@gmail.com>
2014-08-27 11:57:20 -0700

    httrack: upstream patch to fix build pre-10.7
    Fixes #31789.

--- Library/Formula/httrack.rb
+++ Library/Formula/httrack.rb
@@ -7,6 +7,13 @@ class Httrack < Formula
   url "http://mirror.httrack.com/historical/httrack-3.48.19.tar.gz"
   sha1 "7df386a248444c599948dbc77ed705b101151ed4"
 
+  # Fix building on systems without strnlen; fixed upstream, will be in next release
+  # https://code.google.com/p/httrack/issues/detail?id=54
+  patch :p0 do
+    url "https://gist.githubusercontent.com/mistydemeo/5c50bba4be6c4f53d50c/raw/9465c915719106dbe234380f769c45ae3e118edb/httrack-strnlen.diff"
+    sha1 "206b0c3f00a8274eb01b04fb7f11b0339423332b"
+  end
+
   def install
     ENV.deparallelize
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
