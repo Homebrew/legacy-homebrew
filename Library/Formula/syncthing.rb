163ea06fb09a3d92fbaf440f14e81a567516bc51
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-28 11:31:31 +0100

    syncthing: add 0.9.9 bottle.
    Closes #30035.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/syncthing.rb
+++ Library/Formula/syncthing.rb
@@ -4,6 +4,12 @@ class Syncthing < Formula
   homepage "http://syncthing.net"
   url "https://github.com/calmh/syncthing.git", :tag => "v0.9.9"
 
+  bottle do
+    sha1 "17052e29c6d573f22390de9326a54b7939b84ed2" => :mavericks
+    sha1 "1b11607bc47d0880ba993ffe10d52ef1663ae3a0" => :mountain_lion
+    sha1 "83e4df0d1b04986218acafecb4e408a2e879662d" => :lion
+  end
+
   depends_on "go" => :build
   depends_on :hg => :build
 
