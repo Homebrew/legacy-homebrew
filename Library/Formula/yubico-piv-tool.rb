b73969c42460e539e96e0f9aeb26509179c5a3bb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-28 03:11:31 +0100

    yubico-piv-tool: add 0.1.0 bottle.

--- Library/Formula/yubico-piv-tool.rb
+++ Library/Formula/yubico-piv-tool.rb
@@ -5,6 +5,13 @@ class YubicoPivTool < Formula
   url "http://opensource.yubico.com/yubico-piv-tool/releases/yubico-piv-tool-0.1.0.tar.gz"
   sha1 "bebf01280e0ace0c43ab398a06cc0bc2e7fe2af9"
 
+  bottle do
+    cellar :any
+    sha1 "97d204bea07ee9e00d6f3281a84f94e27b55e174" => :mavericks
+    sha1 "621490567eed24c753f712668308a84880940bcd" => :mountain_lion
+    sha1 "19547ccfa0e742e5258907c28e039d5ed8a7e707" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "openssl"
 
