065391d975c8e26f2acdd346d41569c1ffdcdbac
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-08-28 14:52:53 +0100

    gringo 4.4 & clasp 3.1 & aspcud 1.9
    Closes #31946.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gringo.rb
+++ Library/Formula/gringo.rb
@@ -2,23 +2,14 @@ require 'formula'
 
 class Gringo < Formula
   homepage 'http://potassco.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/potassco/gringo/4.3.0/gringo-4.3.0-source.tar.gz'
-  sha1 'dccb55c2c690ebe1f6599a43b6072bfb50eb5e83'
-
-  bottle do
-    cellar :any
-    sha1 "1dc4996fd4469e987a5aab9c205cf157c3460c01" => :mavericks
-  end
+  url 'https://downloads.sourceforge.net/project/potassco/gringo/4.4.0/gringo-4.4.0-source.tar.gz'
+  sha1 'c39a1c3cfe64b62e39e6abcc8f813e2d1d17251e'
 
   depends_on 're2c'  => :build
   depends_on 'scons' => :build
   depends_on 'bison' => :build
 
   needs :cxx11
-
-  # Fixes missing include; fixed upstream:
-  # http://sourceforge.net/p/potassco/code/8274/tree//trunk/gringo/app/gringo/main.cc?diff=5083e8f9bfc09e133b25ad84:8273
-  patch :p3, :DATA
 
   def install
     # Allow pre-10.9 clangs to build in C++11 mode
@@ -30,15 +21,3 @@ class Gringo < Formula
     bin.install "build/release/gringo", "build/release/clingo"
   end
 end
-
-__END__
---- a/trunk/gringo/app/gringo/main.cc
-+++ b/trunk/gringo/app/gringo/main.cc
-@@ -33,6 +33,7 @@
- #include <gringo/scripts.hh>
- #include <gringo/version.hh>
- #include <gringo/control.hh>
-+#include <climits>
- #include <iostream>
- #include <stdexcept>
- #include <program_opts/application.h>
