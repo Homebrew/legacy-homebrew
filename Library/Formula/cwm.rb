fc32d3b9c31d8de78f1b3d1c5f8030a2ee6f7d8b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-28 16:58:15 +0100

    cwm: add 5.5 bottle.

--- Library/Formula/cwm.rb
+++ Library/Formula/cwm.rb
@@ -5,6 +5,13 @@ class Cwm < Formula
   url "https://github.com/chneukirchen/cwm/archive/v5.5.tar.gz"
   sha1 "0e21a48b4973beb7ddf265bea48b73b0c39a589e"
 
+  bottle do
+    cellar :any
+    sha1 "4794ec9443e9ff59c3af6f85ba0bbac08fad496e" => :mavericks
+    sha1 "2750622d8304cc2a008586e4b7b3cc6bfa82b3e2" => :mountain_lion
+    sha1 "28a681a7185ab875e77eb664679c21751e871855" => :lion
+  end
+
   depends_on :x11
   depends_on "pkg-config" => :build
 
