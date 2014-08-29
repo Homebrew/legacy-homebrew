3951cd96e79223de4c1de30376e053e9f22afff2
Jack Nagel <jacknagel@gmail.com>
2014-08-29 18:54:43 -0500

    android-ndk: use https

--- Library/Formula/android-ndk.rb
+++ Library/Formula/android-ndk.rb
@@ -5,19 +5,19 @@ class AndroidNdk < Formula
   version 'r10'
 
   if MacOS.prefer_64_bit?
-    url "http://dl.google.com/android/ndk/android-ndk64-r10-darwin-x86_64.tar.bz2"
+    url "https://dl.google.com/android/ndk/android-ndk64-r10-darwin-x86_64.tar.bz2"
     sha1 '9ecda655e448d3b249be17f1d66f7e4f9535a3b8'
 
     resource "32bit_target" do
-      url "http://dl.google.com/android/ndk/android-ndk32-r10-darwin-x86_64.tar.bz2"
+      url "https://dl.google.com/android/ndk/android-ndk32-r10-darwin-x86_64.tar.bz2"
       sha1 '4a1d226ee7361b51c02e723b5758e3da536cdf8b'
     end
   else
-    url "http://dl.google.com/android/ndk/android-ndk64-r10-darwin-x86.tar.bz2"
+    url "https://dl.google.com/android/ndk/android-ndk64-r10-darwin-x86.tar.bz2"
     sha1 '0fbfd9b3af17cdab56ac475e1998f53c31fb9c1d'
 
     resource "32bit_target" do
-      url "http://dl.google.com/android/ndk/android-ndk32-r10-darwin-x86.tar.bz2"
+      url "https://dl.google.com/android/ndk/android-ndk32-r10-darwin-x86.tar.bz2"
       sha1 'd38f5fce29e07d8b4deb538ebc2ba1ba4c067afd'
     end
   end
