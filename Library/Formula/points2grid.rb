b2b2fe3a28eb92ceebbf1c575bfcf0282b7d7b0e
Howard Butler <howard@hobu.co>
2014-08-29 14:23:47 -0500

    points2grid 1.3.0
    Closes #31974.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/points2grid.rb
+++ Library/Formula/points2grid.rb
@@ -2,15 +2,17 @@ require 'formula'
 
 class Points2grid < Formula
   homepage 'https://github.com/CRREL/points2grid'
-  url 'https://github.com/CRREL/points2grid/archive/1.2.1.tar.gz'
-  sha1 'afd1b8ac0086b7ac220a3615e0ec1512161a4628'
+  url 'https://github.com/CRREL/points2grid/archive/1.3.0.tar.gz'
+  sha1 'a12a54a70559d9920753256966bd2ce7eade752c'
 
   depends_on 'cmake' => :build
   depends_on 'boost'
+  depends_on 'gdal'
 
   def install
+    args = std_cmake_args + ["-DWITH_GDAL=ON"]
     libexec.install "example.las"
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *args
     system "make install"
   end
 
