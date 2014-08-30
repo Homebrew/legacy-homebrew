79472da86353526f3eec4a1236aef0e9bb9f9d01
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-30 13:44:31 +0100

    sync_gateway: add 1.0.2 bottle.
    Closes #29736.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/sync_gateway.rb
+++ Library/Formula/sync_gateway.rb
@@ -6,6 +6,12 @@ class SyncGateway < Formula
   url "https://github.com/couchbase/sync_gateway.git", :branch => "release/1.0.2"
   version "1.0.2"
 
+  bottle do
+    sha1 "c562b5a600af346900a97ffba3941ec7bf143cc3" => :mavericks
+    sha1 "f0e322874588bc5328ec5ab4880def47026960e4" => :mountain_lion
+    sha1 "b7b9d259e0ed15757f30b2e44881266d9dd2a4de" => :lion
+  end
+
   depends_on "go" => :build
 
   def install
