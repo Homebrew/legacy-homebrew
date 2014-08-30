fafd03439da5db2250561bd35804312b300683ee
Keith Shaw <github@keithws.net>
2014-08-29 18:47:34 -0700

    Replaced patch with inreplace. Updated test to run locally without network.

--- Library/Formula/linklint.rb
+++ Library/Formula/linklint.rb
@@ -8,33 +8,21 @@ class Linklint < Formula
   devel do
     url 'http://linklint.org/download/linklint-2.4.beta.tar.gz'
     sha1 'a159d19b700db52e8a9e2d89a0a8984eb627bf17'
-
-    # original author did not update the version reported with the beta
-    patch :DATA
   end
 
   def install
     mv 'READ_ME.txt', 'README' unless build.devel?
+
+    # fix version number reported by linklint -version in beta
+    # note, upstream is abandoned, so inreplace instead of patch
+    inreplace "linklint-#{version}", "2.3.1", "#{version}" if build.devel?
+
     (share+'doc/linklint').install "README"
     bin.install "linklint-#{version}" => "linklint"
   end
 
   test do
-    system "#{bin}/linklint" "-http" "-host" "brew.sh" "/"
+    (testpath/'index.html').write('<a href="/">Home</a>')
+    system "#{bin}/linklint", "/"
   end
 end
-
-__END__
-diff --git a/linklint-2.4.beta b/linklint-2.4.beta
-index cf3f9a8..0f30eee 100755
---- a/linklint-2.4.beta
-+++ b/linklint-2.4.beta
-@@ -54,7 +54,7 @@
- # 
- #========================================================================
- 
--$version = "2.3.1";
-+$version = "2.4.beta";
- $date    = "June 21, 2001";
- $prog    = "linklint";
- 
