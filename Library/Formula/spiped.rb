cb3b9a34a706621b2852cb0572c3d1eaaabb3eca
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-08-30 08:33:00 +0100

    Spiped 1.4.0
    Version bump from 1.3.1 to 1.4.0 and new OpenSSL dependency.

--- Library/Formula/spiped.rb
+++ Library/Formula/spiped.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Spiped < Formula
-  homepage 'http://www.tarsnap.com/spiped.html'
-  url 'https://www.tarsnap.com/spiped/spiped-1.3.1.tgz'
-  sha256 '8a58a983be460b88ed5a105201a0f0afacb83382208761837a62871dcca42fee'
+  homepage 'https://www.tarsnap.com/spiped.html'
+  url 'https://www.tarsnap.com/spiped/spiped-1.4.0.tgz'
+  sha256 'd8fa13a36905337bec97e507e0689f7bbc9e5426b88d588f3ddd3d6c290dcf5f'
 
   depends_on :bsdmake
+  depends_on 'openssl'
 
   def install
     man1.mkpath
