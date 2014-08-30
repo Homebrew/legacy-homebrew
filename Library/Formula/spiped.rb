42d7f9020f12e344453e9c938669a503bd0c6907
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-30 19:39:00 +0100

    spiped: add 1.4.0 bottle.
    Closes #31982.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/spiped.rb
+++ Library/Formula/spiped.rb
@@ -5,6 +5,13 @@ class Spiped < Formula
   url "https://www.tarsnap.com/spiped/spiped-1.4.0.tgz"
   sha256 "d8fa13a36905337bec97e507e0689f7bbc9e5426b88d588f3ddd3d6c290dcf5f"
 
+  bottle do
+    cellar :any
+    sha1 "a5c02e5820d9e0257c8f7989e411f225bd7059bd" => :mavericks
+    sha1 "bf7b0a89918b04698a793bf503c202a4ef6a883e" => :mountain_lion
+    sha1 "36d6b9cbf1d2128321a60ec487fb6f984347502f" => :lion
+  end
+
   depends_on :bsdmake
   depends_on "openssl"
 
