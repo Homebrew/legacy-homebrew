c387b81cf365f3b86d1f2e53d1fbe584c197336f
Mike McQuaid <mike@mikemcquaid.com>
2014-08-30 10:01:34 +0100

    tarsnap: use double-quotes.

--- Library/Formula/tarsnap.rb
+++ Library/Formula/tarsnap.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Tarsnap < Formula
-  homepage 'https://www.tarsnap.com/'
-  url 'https://www.tarsnap.com/download/tarsnap-autoconf-1.0.35.tgz'
-  sha256 '6c9f6756bc43bc225b842f7e3a0ec7204e0cf606e10559d27704e1cc33098c9a'
+  homepage "https://www.tarsnap.com/"
+  url "https://www.tarsnap.com/download/tarsnap-autoconf-1.0.35.tgz"
+  sha256 "6c9f6756bc43bc225b842f7e3a0ec7204e0cf606e10559d27704e1cc33098c9a"
 
   bottle do
     cellar :any
@@ -12,14 +12,14 @@ class Tarsnap < Formula
     sha1 "a84965928a810a18f8dbac38091e4ab9a9e69214" => :lion
   end
 
-  depends_on 'xz' => :optional
-  depends_on 'openssl'
+  depends_on "xz" => :optional
+  depends_on "openssl"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--enable-sse2",
                           "--prefix=#{prefix}",
                           "--sysconfdir=#{etc}"
-    system "make install"
+    system "make", "install"
   end
 end
