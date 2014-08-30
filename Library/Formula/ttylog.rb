ab1612fa1a79e78bcf901df5bffe1c189f8e93f6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-30 17:35:58 +0100

    ttylog: add 0.25 bottle.

--- Library/Formula/ttylog.rb
+++ Library/Formula/ttylog.rb
@@ -5,6 +5,13 @@ class Ttylog < Formula
   url "https://downloads.sourceforge.net/project/ttylog/ttylog/0.25/ttylog-0.25.tar.gz"
   sha1 "02bb49066d861690439b351f06b0c3bdb203f06b"
 
+  bottle do
+    cellar :any
+    sha1 "5c8e70c0b3a1ad9b5708aa4bcb2fed311d24ead3" => :mavericks
+    sha1 "f5e83d8be3255cede1df0c55f51bfc2bfc4533ff" => :mountain_lion
+    sha1 "5a003888636ca2b2bd252d7f65602a2ac86f540c" => :lion
+  end
+
   depends_on "cmake" => :build
 
   def install
