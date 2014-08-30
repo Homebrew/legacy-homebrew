56955e3a8817b6756b3347c855ef347013f22753
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-30 16:59:42 +0100

    aspcud: add 1.9.0 bottle.

--- Library/Formula/aspcud.rb
+++ Library/Formula/aspcud.rb
@@ -5,6 +5,12 @@ class Aspcud < Formula
   url 'https://downloads.sourceforge.net/project/potassco/aspcud/1.9.0/aspcud-1.9.0-source.tar.gz'
   sha1 'ae77772c2424620b3064d0dfe795c26b1c8aa778'
 
+  bottle do
+    sha1 "f1471e364b9efae91452e8cc76fae1c388920f2d" => :mavericks
+    sha1 "351b927b946fd88ed0f0ac5c74dcaf605fcc919f" => :mountain_lion
+    sha1 "2ce63783516196205d651e86388a9c7707ba783d" => :lion
+  end
+
   depends_on 'boost' => :build
   depends_on 'cmake' => :build
   depends_on 're2c'  => :build
