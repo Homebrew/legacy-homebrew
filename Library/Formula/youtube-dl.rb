7e0e7d1cb057992dc6c74ee627108c089cf719b3
Sergey M․ <dstftw@gmail.com>
2014-08-30 21:44:13 +0700

    youtube-dl 2014.08.29

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -5,8 +5,8 @@ require "formula"
 # https://pypi.python.org/pypi/youtube_dl
 class YoutubeDl < Formula
   homepage "http://rg3.github.io/youtube-dl/"
-  url "https://yt-dl.org/downloads/2014.08.28.2/youtube-dl-2014.08.28.2.tar.gz"
-  sha1 "4860b228793a3906be1abd4e1c7d355b34801f96"
+  url "https://yt-dl.org/downloads/2014.08.29/youtube-dl-2014.08.29.tar.gz"
+  sha1 "9f28a0f1ad389da6abb4e6d6af20a918eb504763"
 
   bottle do
     cellar :any
