e819d6ff5b70d054ade18ac7469a7173521c5c1d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-30 16:59:42 +0100

    clasp: add 3.1.0 bottle.

--- Library/Formula/clasp.rb
+++ Library/Formula/clasp.rb
@@ -5,6 +5,13 @@ class Clasp < Formula
   url 'https://downloads.sourceforge.net/project/potassco/clasp/3.1.0/clasp-3.1.0-source.tar.gz'
   sha1 '57297b641d6900a639e09c2a1c73549707f337b7'
 
+  bottle do
+    cellar :any
+    sha1 "b72378ef1072d758c0b67a5efd2c3aa63ce7b6af" => :mavericks
+    sha1 "dec30107fa4ad5fcf88ebe566c03b88aed00c28d" => :mountain_lion
+    sha1 "47b98da2e221a1387780e3c262b96147efffd61e" => :lion
+  end
+
   option 'with-mt', 'Enable multi-thread support'
 
   depends_on 'tbb' if build.with? "mt"
