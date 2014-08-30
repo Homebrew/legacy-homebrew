a14c8fd6be6c421476b076ebc0bfb0857a944bff
BrewTestBot <brew-test-bot@googlegroups.com>
2014-08-30 16:44:58 +0100

    eigen: add 3.2.2 bottle.

--- Library/Formula/eigen.rb
+++ Library/Formula/eigen.rb
@@ -5,6 +5,13 @@ class Eigen < Formula
   url 'http://bitbucket.org/eigen/eigen/get/3.2.2.tar.bz2'
   sha1 '1e1a85681777314805003db5469d1a00785c58df'
 
+  bottle do
+    cellar :any
+    sha1 "5a64fff74d8c341cfcc36666810531261e0fd7a0" => :mavericks
+    sha1 "28fb90c14195d63016ff9783cb4dd259fab257db" => :mountain_lion
+    sha1 "7d09cae778259591337bf397b061fe7cd516c3a5" => :lion
+  end
+
   head 'https://bitbucket.org/eigen/eigen', :using => :hg
 
   depends_on 'cmake' => :build
