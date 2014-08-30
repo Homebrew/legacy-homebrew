a5f90dd0623ee812930c013413fc7a45dc963e00
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-08-30 08:29:18 +0100

    Scrypt: Added Dep
    Bumps the download link to https as well.

--- Library/Formula/scrypt.rb
+++ Library/Formula/scrypt.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Scrypt < Formula
   homepage 'https://www.tarsnap.com/scrypt.html'
-  url 'http://www.tarsnap.com/scrypt/scrypt-1.1.6.tgz'
+  url 'https://www.tarsnap.com/scrypt/scrypt-1.1.6.tgz'
   sha256 'dfd0d1a544439265bbb9b58043ad3c8ce50a3987b44a61b1d39fd7a3ed5b7fb8'
 
   depends_on 'openssl'
