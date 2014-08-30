7746e53f8a19673f74962b315cddeb638d0b5a7f
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-08-30 08:33:00 +0100

    spiped: depend on Homebrew OpenSSL.
    Temporarily reverting to the previous version whilst I try and figure
    out the compile errors. The OpenSSL dependency is probably an equally
    important part of this PR and I don’t want to keep that back too long
    whilst I try and figure the compile issue out.

--- Library/Formula/spiped.rb
+++ Library/Formula/spiped.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
 class Spiped < Formula
-  homepage 'http://www.tarsnap.com/spiped.html'
+  homepage 'https://www.tarsnap.com/spiped.html'
   url 'https://www.tarsnap.com/spiped/spiped-1.3.1.tgz'
   sha256 '8a58a983be460b88ed5a105201a0f0afacb83382208761837a62871dcca42fee'
 
   depends_on :bsdmake
+  depends_on 'openssl'
 
   def install
     man1.mkpath
