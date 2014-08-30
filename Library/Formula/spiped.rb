c0cfddafc4c26a2ba955360ed041dfe014f75135
Mike McQuaid <mike@mikemcquaid.com>
2014-08-30 10:05:01 +0100

    spiped: use double-quotes.

--- Library/Formula/spiped.rb
+++ Library/Formula/spiped.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Spiped < Formula
-  homepage 'https://www.tarsnap.com/spiped.html'
-  url 'https://www.tarsnap.com/spiped/spiped-1.3.1.tgz'
-  sha256 '8a58a983be460b88ed5a105201a0f0afacb83382208761837a62871dcca42fee'
+  homepage "https://www.tarsnap.com/spiped.html"
+  url "https://www.tarsnap.com/spiped/spiped-1.3.1.tgz"
+  sha256 "8a58a983be460b88ed5a105201a0f0afacb83382208761837a62871dcca42fee"
 
   bottle do
     cellar :any
@@ -13,12 +13,12 @@ class Spiped < Formula
   end
 
   depends_on :bsdmake
-  depends_on 'openssl'
+  depends_on "openssl"
 
   def install
     man1.mkpath
     system "bsdmake", "BINDIR_DEFAULT=#{bin}", "MAN1DIR=#{man1}", "install"
-    doc.install 'spiped/README' => 'README.spiped',
-                'spipe/README' => 'README.spipe'
+    doc.install "spiped/README" => "README.spiped",
+                "spipe/README" => "README.spipe"
   end
 end
