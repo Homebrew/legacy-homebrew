95a9ee998a2f789719f36cf58100e8fde10d04a5
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-08-30 08:26:33 +0100

    scrypt: depend on Homebrew OpenSSL.
    Bumps the homepage to the correct https link, changes the sha1 to
    sha256 to match Tarsnap preferred checksum styling, and added the
    openssl dependency for the same reasons referenced in
    [31978](https://github.com/Homebrew/homebrew/pull/31978).
    
    Also: bumps the download link to https as well.

--- Library/Formula/scrypt.rb
+++ Library/Formula/scrypt.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class Scrypt < Formula
-  homepage 'http://www.tarsnap.com/scrypt.html'
-  url 'http://www.tarsnap.com/scrypt/scrypt-1.1.6.tgz'
-  sha1 '7b1b3feccfe2ff08b5d05752ef1ca7df2ec8646d'
+  homepage 'https://www.tarsnap.com/scrypt.html'
+  url 'https://www.tarsnap.com/scrypt/scrypt-1.1.6.tgz'
+  sha256 'dfd0d1a544439265bbb9b58043ad3c8ce50a3987b44a61b1d39fd7a3ed5b7fb8'
+
+  depends_on 'openssl'
 
   def install
     system "./configure", "--prefix=#{prefix}"
