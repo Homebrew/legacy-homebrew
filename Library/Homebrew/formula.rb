d41706bb93de0b03406f323e1d5306c6a918ac3e
Jack Nagel <jacknagel@gmail.com>
2014-08-29 22:31:55 -0500

    Copy config.log even if the build succeeds

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -260,11 +260,8 @@ class Formula
         # we allow formulae to do anything they want to the Ruby process
         # so load any deps before this point! And exit asap afterwards
         yield self
-      rescue RuntimeError, SystemCallError
-        %w(config.log CMakeCache.txt).each do |fn|
-          (HOMEBREW_LOGS/name).install(fn) if File.file?(fn)
-        end
-        raise
+      ensure
+        (HOMEBREW_LOGS+name).install Dir["config.log", "CMakeCache.txt"]
       end
     end
   end
