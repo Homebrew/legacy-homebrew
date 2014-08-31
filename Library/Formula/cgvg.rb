b815a2cee2dc4d9a51928300f9447ef73e6eafc8
Mike McQuaid <mike@mikemcquaid.com>
2014-08-31 14:34:32 +0100

    cgvg: general cleanup.

--- Library/Formula/cgvg.rb
+++ Library/Formula/cgvg.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Cgvg < Formula
-  homepage 'http://www.uzix.org/cgvg.html'
-  url 'http://www.uzix.org/cgvg/cgvg-1.6.3.tar.gz'
-  sha1 'd5a108e470b6e7bdf7863c540aaf0efc9ddf1335'
+  homepage "http://www.uzix.org/cgvg.html"
+  url "http://www.uzix.org/cgvg/cgvg-1.6.3.tar.gz"
+  sha1 "d5a108e470b6e7bdf7863c540aaf0efc9ddf1335"
 
   bottle do
     cellar :any
@@ -15,6 +15,6 @@ class Cgvg < Formula
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
   end
 end
