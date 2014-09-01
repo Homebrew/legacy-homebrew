4e60811c06ceaf345a4ecd7f134b55f72fdaafad
Adrian Pistol <adinator1999@gmail.com>
2014-09-01 18:38:07 +0200

    Made universal build optional

--- Library/Formula/proxychains-ng.rb
+++ Library/Formula/proxychains-ng.rb
@@ -6,9 +6,18 @@ class ProxychainsNg < Formula
   sha1 '5e5b10009f785434ebdbd7ede5a79efee4e59c5a'
 
   head 'https://github.com/rofl0r/proxychains-ng.git'
+  
+  option :universal, "Build proxychains-ng as an Universal Build."
 
   def install
-    system "./configure", "--fat-binary", "--prefix=#{prefix}", "--sysconfdir=#{prefix}/etc"
+    args=[]
+    if build.universal? then
+      ENV.universal_binary
+      args << "--fat-binary"
+    end
+    args << "--prefix=#{prefix}"
+    args << "--sysconfdir=#{prefix}/etc"
+    system "./configure", *args
     system "make"
     system "make install"
     system "make install-config"
