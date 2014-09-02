0166a450ad6b8e85e25628de9377b3227931ddbb
Mike McQuaid <mike@mikemcquaid.com>
2014-09-02 15:32:57 +0100

    proxychains-ng: general cleanup

--- Library/Formula/proxychains-ng.rb
+++ Library/Formula/proxychains-ng.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class ProxychainsNg < Formula
-  homepage 'https://sourceforge.net/projects/proxychains-ng'
-  url 'https://downloads.sourceforge.net/project/proxychains-ng/proxychains-4.7.tar.bz2'
-  sha1 '5e5b10009f785434ebdbd7ede5a79efee4e59c5a'
+  homepage "https://sourceforge.net/projects/proxychains-ng"
+  url "https://downloads.sourceforge.net/project/proxychains-ng/proxychains-4.7.tar.bz2"
+  sha1 "5e5b10009f785434ebdbd7ede5a79efee4e59c5a"
 
-  head 'https://github.com/rofl0r/proxychains-ng.git'
+  head "https://github.com/rofl0r/proxychains-ng.git"
 
   bottle do
     sha1 "2dec4dda5f1ee8656133141ee50a0a1bcf616c7d" => :mavericks
@@ -16,14 +16,14 @@ class ProxychainsNg < Formula
   option :universal
 
   def install
-    args=["--prefix=#{prefix}", "--sysconfdir=#{prefix}/etc"]
+    args = ["--prefix=#{prefix}", "--sysconfdir=#{prefix}/etc"]
     if build.universal?
       ENV.universal_binary
-      args=args.unshift "--fat-binary"
+      args.unshift "--fat-binary"
     end
     system "./configure", *args
     system "make"
-    system "make install"
-    system "make install-config"
+    system "make", "install"
+    system "make", "install-config"
   end
 end
