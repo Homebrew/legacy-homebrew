d17d4d3dabbe1319fc0ab593f8d03f16bb8d8ea2
Herry <herry13@gmail.com>
2014-09-02 05:51:13 +0100

    rename #{prefix}/bin -> #{bin}

--- Library/Formula/fast-downward.rb
+++ Library/Formula/fast-downward.rb
@@ -4,7 +4,6 @@ class FastDownward < Formula
   homepage "http://fast-downward.org"
   url "http://herry13.github.io/fd/fast-downward-1.0.0.tar.gz"
   sha1 "b84e5e4914a19338be6b5db031b57f80086fd5df"
-  version "1.0.0"
 
   depends_on "cmake" => :build
   depends_on "python"
@@ -29,12 +28,12 @@ class FastDownward < Formula
 
     # install
     system "mkdir #{prefix}/{bin,preprocess,search}"
-    system "cp bin/* #{prefix}/bin"
+    system "cp bin/* #{bin}"
     system "cp -r translate #{prefix}"
     system "cp preprocess/preprocess #{prefix}/preprocess/"
     system "cp search/{downward,downward-release,unitcost} #{prefix}/search/"
-    system "ln -sf #{prefix}/preprocess/preprocess #{prefix}/bin/fast-downward-preprocess"
-    system "ln -sf #{prefix}/search/downward-release #{prefix}/bin/fast-downward-search"
+    system "ln -sf #{prefix}/preprocess/preprocess #{bin}/fast-downward-preprocess"
+    system "ln -sf #{prefix}/search/downward-release #{bin}/fast-downward-search"
   end
 
   test do
