27cbddfc26e95e66e74df8ed449b081840ea9841
Mike McQuaid <mike@mikemcquaid.com>
2014-09-02 15:24:09 +0100

    lv: general cleanup

--- Library/Formula/lv.rb
+++ Library/Formula/lv.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Lv < Formula
-  homepage 'http://www.ff.iij4u.or.jp/~nrt/lv/'
-  url 'http://www.ff.iij4u.or.jp/~nrt/freeware/lv451.tar.gz'
-  version '4.51'
-  sha1 '1a70299c27aa317a436976a557853858db4dcb5f'
+  homepage "http://www.ff.iij4u.or.jp/~nrt/lv/"
+  url "http://www.ff.iij4u.or.jp/~nrt/freeware/lv451.tar.gz"
+  version "4.51"
+  sha1 "1a70299c27aa317a436976a557853858db4dcb5f"
 
   bottle do
     sha1 "077f8feee1b78425a38f1775cfa0bba5b5334569" => :mavericks
@@ -17,15 +17,15 @@ class Lv < Formula
   end
 
   def install
-    cd 'build' do
+    cd "build" do
       system "../src/configure", "--prefix=#{prefix}"
       system "make"
-      bin.install 'lv'
-      bin.install_symlink 'lv' => 'lgrep'
+      bin.install "lv"
+      bin.install_symlink "lv" => "lgrep"
     end
 
-    man1.install 'lv.1'
-    (lib + 'lv').install 'lv.hlp'
+    man1.install "lv.1"
+    (lib+"lv").install "lv.hlp"
   end
 end
 
