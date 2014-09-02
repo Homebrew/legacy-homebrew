5d3c169bbed6980450d903df164a8cb60cbe4fe2
Drew Stephens <drew@dinomite.net>
2014-09-02 14:34:02 -0400

    Employ mikemcquaid's tips

--- Library/Formula/apache-karaf.rb
+++ Library/Formula/apache-karaf.rb
@@ -5,24 +5,9 @@ class ApacheKaraf < Formula
   url "http://mirror.cogentco.com/pub/apache/karaf/2.3.6/apache-karaf-2.3.6.tar.gz"
   sha1 "9a54171ade3c3722a8385b916d9904038af8c13d"
 
-  keg_only "Karaf divines location of its internal dependencies from the invoked executable's location"
-
   def install
-    libexec.install Dir["bin"]
-    libexec.install Dir["data"]
-    libexec.install Dir["demos"]
-    libexec.install Dir["deploy"]
-    libexec.install Dir["etc"]
-    libexec.install Dir["lib"]
-    libexec.install Dir["system"]
-  end
+    libexec.install "bin", "data", "demos", "deploy", "etc", "lib", "system"
 
-  def caveats;
-      msg = <<-EOS.undent
-      Set your KARAF_HOME to
-          #{prefix}/libexec/
-      and then run karaf with
-          #{prefix}/libexec/bin/karaf
-      EOS
+    bin.env_script_all_files(libexec + 'bin', :KARAF_HOME => libexec)
   end
 end
