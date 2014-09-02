34cb27638824e93b09c820d2556214eb01d79705
Mike McQuaid <mike@mikemcquaid.com>
2014-09-02 15:19:39 +0100

    makeself: general cleanup.

--- Library/Formula/makeself.rb
+++ Library/Formula/makeself.rb
@@ -4,15 +4,12 @@ class Makeself < Formula
   homepage "http://www.megastep.org/makeself/"
   url "https://github.com/megastep/makeself/archive/release-2.2.0.tar.gz"
   sha1 "e512745f7aa5becea4f4f85a8dc0aa6fd6ca38aa"
-
-  head 'https://github.com/megastep/makeself.git', :branch => 'master'
+  head "https://github.com/megastep/makeself.git", :branch => "master"
 
   def install
     libexec.install "makeself-header.sh"
-    # moved makeself-header.sh to libexec so change its location in makeself.sh
-    inreplace "makeself.sh" do |f|
-      f.gsub! '`dirname "$0"`', "#{libexec}"
-    end
+    # install makeself-header.sh to libexec so change its location in makeself.sh
+    inreplace "makeself.sh", '`dirname "$0"`', libexec
     bin.install "makeself.sh" => "makeself"
     man1.install "makeself.1"
   end
@@ -20,6 +17,6 @@ class Makeself < Formula
   test do
     touch "testfile"
     system "tar", "cvzf", "testfile.tar.gz", "testfile"
-    system "makeself", ".", "testfile.run", "\"A test file\"", "echo"
+    system "makeself", ".", "testfile.run", '"A test file"', "echo"
   end
 end
