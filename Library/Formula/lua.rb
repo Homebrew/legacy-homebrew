3bd2ac6e3470c7d0137b5471a70a59d0af50c8ca
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-09-03 03:58:45 +0100

    Lua >= Lua52
    Yup. I figured it out. When I did the rebasing earlier and messed that
    up it pulled out the whitespaces in the _END_ patches, and those spaces
    were actually there for a reason and without them… No compile. This
    should fix it. Both of these compile locally again.

--- Library/Formula/lua.rb
+++ Library/Formula/lua.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Lua < Formula
-  homepage "http://www.lua.org/"
-  url "http://www.lua.org/ftp/lua-5.2.3.tar.gz"
-  sha1 "926b7907bc8d274e063d42804666b40a3f3c124c"
+  homepage 'http://www.lua.org/'
+  url 'http://www.lua.org/ftp/lua-5.2.3.tar.gz'
+  sha1 '926b7907bc8d274e063d42804666b40a3f3c124c'
   revision 1
 
   fails_with :llvm do
@@ -25,7 +25,6 @@ class Lua < Formula
   end if build.include? "completion"
 
   def install
-
     # Use our CC/CFLAGS to compile.
     inreplace 'src/Makefile' do |s|
       s.remove_make_var! 'CC'
@@ -36,7 +35,7 @@ class Lua < Formula
     # Fix path in the config header
     inreplace 'src/luaconf.h', '/usr/local', HOMEBREW_PREFIX
 
-    # We need to make our own pc file as Lua no longer ships them.
+    # We ship our own pkg-config file as Lua no longer provide them upstream.
     system "make", "macosx", "INSTALL_TOP=#{prefix}", "INSTALL_MAN=#{man1}"
     system "make", "install", "INSTALL_TOP=#{prefix}", "INSTALL_MAN=#{man1}"
     (lib+"pkgconfig/lua.pc").write pc_file
@@ -67,7 +66,7 @@ class Lua < Formula
     Name: Lua
     Description: An Extensible Extension Language
     Version: 5.2.3
-    Requires: 
+    Requires:
     Libs: -L${libdir} -llua -lm
     Cflags: -I${includedir}
     EOS
@@ -89,14 +88,14 @@ index bd9515f..5940ba9 100644
  
  # Lua version and release.
 @@ -63,6 +63,8 @@ install: dummy
-  cd src && $(INSTALL_DATA) $(TO_INC) $(INSTALL_INC)
-  cd src && $(INSTALL_DATA) $(TO_LIB) $(INSTALL_LIB)
-  cd doc && $(INSTALL_DATA) $(TO_MAN) $(INSTALL_MAN)
-+ ln -s -f liblua.5.2.3.dylib $(INSTALL_LIB)/liblua.5.2.dylib
-+ ln -s -f liblua.5.2.dylib $(INSTALL_LIB)/liblua.dylib
+ 	cd src && $(INSTALL_DATA) $(TO_INC) $(INSTALL_INC)
+ 	cd src && $(INSTALL_DATA) $(TO_LIB) $(INSTALL_LIB)
+ 	cd doc && $(INSTALL_DATA) $(TO_MAN) $(INSTALL_MAN)
++	ln -s -f liblua.5.2.3.dylib $(INSTALL_LIB)/liblua.5.2.dylib
++	ln -s -f liblua.5.2.dylib $(INSTALL_LIB)/liblua.dylib
  
  uninstall:
-  cd src && cd $(INSTALL_BIN) && $(RM) $(TO_BIN)
+ 	cd src && cd $(INSTALL_BIN) && $(RM) $(TO_BIN)
 diff --git a/src/Makefile b/src/Makefile
 index 8c9ee67..7f92407 100644
 --- a/src/Makefile
@@ -105,33 +104,33 @@ index 8c9ee67..7f92407 100644
  
  PLATS= aix ansi bsd freebsd generic linux macosx mingw posix solaris
  
--LUA_A= liblua.a
-+LUA_A= liblua.5.2.3.dylib
- CORE_O=  lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o \
-  lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o \
-  ltm.o lundump.o lvm.o lzio.o
-@@ -56,11 +56,12 @@ o:  $(ALL_O)
- a: $(ALL_A)
+-LUA_A=	liblua.a
++LUA_A=	liblua.5.2.3.dylib
+ CORE_O=	lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o \
+ 	lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o \
+ 	ltm.o lundump.o lvm.o lzio.o
+@@ -56,11 +56,12 @@ o:	$(ALL_O)
+ a:	$(ALL_A)
  
  $(LUA_A): $(BASE_O)
-- $(AR) $@ $(BASE_O)
-- $(RANLIB) $@
-+ $(CC) -dynamiclib -install_name HOMEBREW_PREFIX/lib/liblua.5.2.dylib \
-+   -compatibility_version 5.2 -current_version 5.2.3 \
-+   -o liblua.5.2.3.dylib $^
+-	$(AR) $@ $(BASE_O)
+-	$(RANLIB) $@
++	$(CC) -dynamiclib -install_name HOMEBREW_PREFIX/lib/liblua.5.2.dylib \
++		-compatibility_version 5.2 -current_version 5.2.3 \
++		-o liblua.5.2.3.dylib $^
  
  $(LUA_T): $(LUA_O) $(LUA_A)
-- $(CC) -o $@ $(LDFLAGS) $(LUA_O) $(LUA_A) $(LIBS)
-+ $(CC) -fno-common $(MYLDFLAGS) -o $@ $(LUA_O) $(LUA_A) -L. -llua.5.2.3 $(LIBS)
+-	$(CC) -o $@ $(LDFLAGS) $(LUA_O) $(LUA_A) $(LIBS)
++	$(CC) -fno-common $(MYLDFLAGS) -o $@ $(LUA_O) $(LUA_A) -L. -llua.5.2.3 $(LIBS)
  
  $(LUAC_T): $(LUAC_O) $(LUA_A)
-  $(CC) -o $@ $(LDFLAGS) $(LUAC_O) $(LUA_A) $(LIBS)
+ 	$(CC) -o $@ $(LDFLAGS) $(LUAC_O) $(LUA_A) $(LIBS)
 @@ -106,7 +107,7 @@ linux:
-  $(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_LINUX" SYSLIBS="-Wl,-E -ldl -lreadline"
+ 	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_LINUX" SYSLIBS="-Wl,-E -ldl -lreadline"
  
  macosx:
-- $(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX" SYSLIBS="-lreadline" CC=cc
-+ $(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX -fno-common" SYSLIBS="-lreadline" CC=cc
+-	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX" SYSLIBS="-lreadline" CC=cc
++	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX -fno-common" SYSLIBS="-lreadline" CC=cc
  
  mingw:
-  $(MAKE) "LUA_A=lua52.dll" "LUA_T=lua.exe" \
+ 	$(MAKE) "LUA_A=lua52.dll" "LUA_T=lua.exe" \
