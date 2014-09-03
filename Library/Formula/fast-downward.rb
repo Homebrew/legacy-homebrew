5db337e2b6e138e3f59fb1b8600da17ccab80c22
Herry <herry13@gmail.com>
2014-09-03 13:05:22 +0100

    remove comments; use share; replace single with double quotes

--- Library/Formula/fast-downward.rb
+++ Library/Formula/fast-downward.rb
@@ -3,24 +3,21 @@ require "formula"
 class FastDownward < Formula
   homepage "http://fast-downward.org"
   url "http://herry13.github.io/fd/fast-downward-1.0.0.tar.gz"
-  sha1 "bb319e768dd9ced9735164caf53ac0c2ec182bdc"
+  sha1 "88ef8278760323cd6f8e78ff6fc323c74caa8999"
 
   depends_on "cmake" => :build
   depends_on "flex" => :build
   depends_on "bison" => :build
 
   def install
-    # compile
-    system 'make'
+    system "make"
 
-    # install
     bin.mkpath
-    (prefix/'preprocess').mkpath
-    (prefix/'search').mkpath
-    system 'make', "PREFIX=#{prefix}", 'install'
+    (prefix/"share").mkpath
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
-    system 'fast-downward', 'test/airport-01-domain.pddl', 'test/airport-01-problem.pddl', '--search "lazy_greedy(ff())"'
+    system "fast-downward", "test/airport-01-domain.pddl", "test/airport-01-problem.pddl", "--search 'lazy_greedy(ff())'"
   end
 end
