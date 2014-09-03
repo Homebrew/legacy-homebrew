e824a3b2eb8d5be7593da58582329500711a9597
Herry <herry13@gmail.com>
2014-09-03 11:44:51 +0100

    enable clang support

--- Library/Formula/fast-downward.rb
+++ Library/Formula/fast-downward.rb
@@ -3,19 +3,11 @@ require "formula"
 class FastDownward < Formula
   homepage "http://fast-downward.org"
   url "http://herry13.github.io/fd/fast-downward-1.0.0.tar.gz"
-  sha1 "df0e0565d2774048cf51924182cf69c402c13f8a"
+  sha1 "bb319e768dd9ced9735164caf53ac0c2ec182bdc"
 
   depends_on "cmake" => :build
   depends_on "flex" => :build
   depends_on "bison" => :build
-
-  fails_with :clang do
-    cause "Clang does no recognize 'tr1' library e.g. '#include <tr1/unordered_set>'"
-  end
-
-  fails_with :llvm do
-    cause "it does no recognize 'tr1' library e.g. '#include <tr1/unordered_set>'"
-  end
 
   def install
     # compile
