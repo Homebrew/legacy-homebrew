3a4026aca381f578860ef47c7139dc1ef531ffaa
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-09-04 18:57:11 +0530

    removed hardcoded version from formula

--- Library/Formula/plowshare.rb
+++ Library/Formula/plowshare.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Plowshare < Formula
   homepage 'http://code.google.com/p/plowshare/'
   url 'https://plowshare.googlecode.com/archive/v1.0.5.tar.gz'
-  version 'v1.0.5'
   sha1 ''
 
   head 'https://code.google.com/p/plowshare/', :using => :git
