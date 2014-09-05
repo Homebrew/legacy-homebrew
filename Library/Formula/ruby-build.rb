a738c2dbf840b8959191bd6cb1c1e752a99c41ba
Erik Michaels-Ober <sferik@gmail.com>
2014-09-05 15:39:56 +0200

    ruby-build 20140905

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,9 +3,8 @@ require "formula"
 class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  url "https://github.com/sstephenson/ruby-build/archive/v20140702.tar.gz"
-  sha1 "3b609d6375f1ddcc519c5854ffbb947d3f32ea4e"
-  revision 1
+  url "https://github.com/sstephenson/ruby-build/archive/v20140905.tar.gz"
+  sha1 "e0c92765f1e08f314d6ea48d245f18c0c6753cc0"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
