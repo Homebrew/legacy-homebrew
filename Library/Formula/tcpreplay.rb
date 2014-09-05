808b603d568a9225497046817e717af3eef518dd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-05 18:56:37 +0100

    tcpreplay: add 4.0.5 bottle.

--- Library/Formula/tcpreplay.rb
+++ Library/Formula/tcpreplay.rb
@@ -5,6 +5,13 @@ class Tcpreplay < Formula
   url "https://github.com/appneta/tcpreplay/releases/download/v4.0.5/tcpreplay-4.0.5.tar.gz"
   sha1 "878970d77e1482c9b26ac19eb2d125915a900f9b"
 
+  bottle do
+    cellar :any
+    sha1 "b003d008725719da2af9bd708af9319f2ab43710" => :mavericks
+    sha1 "e62ad60674fe0ceb5bfa65304c00278410bb6881" => :mountain_lion
+    sha1 "9675b87cf528c950d69c8500c78ab059b601f16a" => :lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-debug",
