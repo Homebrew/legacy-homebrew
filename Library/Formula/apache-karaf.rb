0bd75690b87818dbf21fe29faba0b47cb773be73
Drew Stephens <drew@dinomite.net>
2014-09-04 20:11:15 -0400

    Use apache mirror select URL

--- Library/Formula/apache-karaf.rb
+++ Library/Formula/apache-karaf.rb
@@ -2,13 +2,11 @@ require "formula"
 
 class ApacheKaraf < Formula
   homepage "http://karaf.apache.org"
-  url "http://mirror.cogentco.com/pub/apache/karaf/2.3.6/apache-karaf-2.3.6.tar.gz"
+  url "http://www.apache.org/dyn/closer.cgi?path=karaf/2.3.6/apache-karaf-2.3.6.tar.gz"
   sha1 "9a54171ade3c3722a8385b916d9904038af8c13d"
 
   def install
     libexec.install "bin", "data", "demos", "deploy", "etc", "lib", "system"
-
-    bin.env_script_all_files(libexec + 'bin', :KARAF_HOME => libexec)
   end
 
   test do
