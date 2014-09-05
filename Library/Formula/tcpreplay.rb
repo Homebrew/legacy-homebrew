c42efe8d1473dbf0df4057b935c30e0e146202c0
Fred Klassen <fklassen@appneta.com>
2014-09-05 10:30:55 -0700

    Update to release 4.0.5. Point to GitHub instead of SourceForge, although download is available in both locations

--- Library/Formula/tcpreplay.rb
+++ Library/Formula/tcpreplay.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Tcpreplay < Formula
   homepage "http://tcpreplay.appneta.com"
-  url "https://downloads.sourceforge.net/project/tcpreplay/tcpreplay/4.0.4/tcpreplay-4.0.4.tar.gz"
-  sha1 "f2f266508b06693852bbc5003b20b18da4a1f01b"
+  url "https://github.com/appneta/tcpreplay/releases/download/v4.0.5/tcpreplay-4.0.5.tar.gz"
+  sha1 "878970d77e1482c9b26ac19eb2d125915a900f9b"
 
   def install
     system "./configure", "--disable-dependency-tracking",
