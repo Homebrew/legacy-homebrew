e5eb41516193db80dec36bac69cf92f8b78f856f
David Larson <dplarson@ucsd.edu>
2014-09-07 13:09:07 -0500

    the_platinum_searcher 1.7.1
    Closes #31679.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/the_platinum_searcher.rb
+++ Library/Formula/the_platinum_searcher.rb
@@ -1,10 +1,31 @@
 require "formula"
+require "language/go"
 
 class ThePlatinumSearcher < Formula
   homepage "https://github.com/monochromegane/the_platinum_searcher"
-  url "https://github.com/monochromegane/the_platinum_searcher/archive/v1.6.5.tar.gz"
-  sha1 "51658e4825b5f719fb37072da1b5035b5fde5734"
+  url "https://github.com/monochromegane/the_platinum_searcher/archive/v1.7.1.tar.gz"
+  sha1 "5e2d704e0c0d8380c82e55a30b7d0fc749ab0c55"
   head "https://github.com/monochromegane/the_platinum_searcher.git"
+
+  go_resource "github.com/jessevdk/go-flags" do
+    url "https://github.com/jessevdk/go-flags.git",
+      :revision => "7047cf7a8dc6f41e53365420ab62d415055232c6"
+  end
+
+  go_resource "github.com/monochromegane/terminal" do
+    url "https://github.com/monochromegane/terminal.git",
+      :revision => "6d255869fb99937f1f287bd1fe3a034c6c4f68f6"
+  end
+
+  go_resource "github.com/shiena/ansicolor" do
+    url "https://github.com/shiena/ansicolor.git",
+      :revision => "6046e7d18a7698e98846e5d25842e9cf15aecf2c"
+  end
+
+  go_resource "code.google.com/p/go.text" do
+    url "https://code.google.com/p/go.text", :using => :hg,
+      :revision => "46250cb715a27b42c736a5ff2a4e6fa0b2118952"
+  end
 
   depends_on "go" => :build
   depends_on :hg => :build
@@ -17,16 +38,14 @@ class ThePlatinumSearcher < Formula
   end
 
   def install
-    (buildpath + "src/github.com/monochromegane/the_platinum_searcher").install "search"
+    # configure buildpath for local dependencies
+    mkdir_p buildpath/"src/github.com/monochromegane"
+    ln_s buildpath, buildpath/"src/github.com/monochromegane/the_platinum_searcher"
 
     ENV["GOPATH"] = buildpath
+    Language::Go.stage_deps resources, buildpath/"src"
 
-    system "go", "get", "github.com/shiena/ansicolor"
-    system "go", "get", "github.com/monochromegane/terminal"
-    system "go", "get", "github.com/jessevdk/go-flags"
-    system "go", "get", "code.google.com/p/go.text/transform"
-
-    system "go", "build", "-o", "pt"
+    system "go", "build", "-o", "pt", "cmd/pt/main.go"
     bin.install "pt"
   end
 
