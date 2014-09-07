75c8355a168c77d3ca91d69054d2c281905c2032
Gerrit Beine <gerrit.beine@gmx.de>
2014-09-07 19:59:42 +0200

    Use double quotes, removed blank line

--- Library/Formula/omega.rb
+++ Library/Formula/omega.rb
@@ -5,8 +5,8 @@ class Omega < Formula
   url "http://oligarchy.co.uk/xapian/1.2.18/xapian-omega-1.2.18.tar.xz"
   sha1 "9b0060c639ebb53e9b2dd6928019e06d0fd24ced"
 
-  depends_on 'pcre'
-  depends_on 'xapian'
+  depends_on "pcre"
+  depends_on "xapian"
 
   def install
     system "./configure", "--disable-debug",
@@ -20,5 +20,4 @@ class Omega < Formula
     system "#{bin}/omindex", "--db", "./test", "--url", "/", "#{share}/doc/xapian-omega"
     assert File.exist?("./test/flintlock")
   end
-
 end
