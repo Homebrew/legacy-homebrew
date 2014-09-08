b0cf6b7a8199f821b2825d21f57bece71164256f
BestPig <bestpig82@gmail.com>
2014-09-08 21:30:44 +0200

    Upgrade cv to v0.5.1

--- Library/Formula/cv.rb
+++ Library/Formula/cv.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Cv < Formula
   homepage "https://github.com/BestPig/cv"
-  url "https://github.com/BestPig/cv/archive/v0.4.1.tar.gz"
-  sha1 "4621dd0eb2bea2bdbbfa50904fb555a90ff3e446"
+  url "https://github.com/BestPig/cv/archive/v0.5.1.tar.gz"
+  sha1 "8562e4fa4ce47b25567ea027c53d0c01b0bd396d"
 
   def install
     system "make"
