1c7d5a9d1db6d9f9778dde7e2b73807a441f19ff
Luca Deri <deri@ntop.org>
2014-09-08 23:17:19 +0200

    Small changes. In nDPI added -i <test pcap>

--- Library/Formula/ndpi.rb
+++ Library/Formula/ndpi.rb
@@ -3,21 +3,22 @@ require "formula"
 class Ndpi < Formula
   homepage "http://www.ntop.org/products/ndpi/"
   url "https://downloads.sourceforge.net/project/ntop/nDPI/libndpi-1.5.1.tar.gz"
-  sha1 "2932c2d1a50fb4cee39edda78a88df953d996d2b"
+  sha1 "0a6ed585545ab6611f3f0ac9efd9eb36bb5481dd"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "libtool" => :build
+  depends_on "json-c"
 
   def install
     system "./configure","--prefix=#{prefix}"
     system "make"
-    system "make","install"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/ndpiReader","-h"
+    test_pcap = HOMEBREW_LIBRARY/"Homebrew/test/fixtures/test.pcap"
+    system "#{bin}/ndpiReader", "-i", test_pcap
   end
-
 end
--- Library/Formula/ntopng.rb
+++ Library/Formula/ntopng.rb
@@ -3,32 +3,31 @@ require "formula"
 class Ntopng < Formula
   homepage "http://www.ntop.org/products/ntop/"
   url "https://downloads.sourceforge.net/project/ntop/ntopng/ntopng-1.2.1.tgz"
-  sha1 "87764fa3c88965d91e8c0596eba3b3c659a79be4"
+  sha1 "e90a8cc045fb4d65d57d029908a9b029d801490c"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "libtool" => :build
   depends_on "json-glib" => :build
-  depends_on "json-c" => :build
+  depends_on "json-c"
   depends_on "wget" => :build
-  depends_on "rrdtool" => :build
+  depends_on "rrdtool"
   depends_on "zeromq" => :build
   depends_on "luajit" => :build
-  depends_on "gnutls"
-  depends_on "geoip" => :build
-  depends_on "sqlite" => :build
+  depends_on "gnutls" => :build
+  depends_on "geoip"
+  depends_on "sqlite"
   depends_on "redis"
 
   def install
     system "./autogen.sh"
     system "./configure","--prefix=#{prefix}"
     system "make"
-    system "make","install"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/ntopng","--version"
+    system "#{bin}/ntopng", "-h"
   end
-
 end
