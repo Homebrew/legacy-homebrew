e2a9ab0efecd2de06070522ecf5bff1c28a852ea
kbinani <kbinani.bt@gmail.com>
2014-09-09 02:43:52 -0700

    exempi 2.2.2

--- Library/Formula/exempi.rb
+++ Library/Formula/exempi.rb
@@ -2,16 +2,10 @@ require 'formula'
 
 class Exempi < Formula
   homepage 'http://libopenraw.freedesktop.org/wiki/Exempi'
-  url 'http://libopenraw.freedesktop.org/download/exempi-2.2.1.tar.bz2'
-  sha1 '0ca233e81d6f6fef94ac13202ce9a66b584d482e'
+  url 'http://libopenraw.freedesktop.org/download/exempi-2.2.2.tar.bz2'
+  sha1 'c0a0014e18f05aa7fac210c84788ef073718a9d8'
 
   depends_on 'boost'
-
-  # https://bugs.freedesktop.org/show_bug.cgi?id=73058
-  patch do
-    url "http://cgit.freedesktop.org/exempi/patch/?id=75af16b221dca0bf6a9656d5b187c3141d82c200"
-    sha1 "a5ccc2f56ff685a52578ddcca2dc12105347c1d2"
-  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
