36e99ed3e0c1df1c5e60a09cacecc389d9dd814f
Jack Nagel <jacknagel@gmail.com>
2014-09-08 19:23:40 -0500

    autogen 5.18.4

--- Library/Formula/autogen.rb
+++ Library/Formula/autogen.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Autogen < Formula
   homepage 'http://autogen.sourceforge.net'
-  url 'http://ftpmirror.gnu.org/autogen/rel5.18.2/autogen-5.18.2.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/autogen/rel5.18.2/autogen-5.18.2.tar.gz'
-  sha1 'c63a0f567b4ad90c4243efbd2420c51e6b63309a'
+  url 'http://ftpmirror.gnu.org/autogen/rel5.18.4/autogen-5.18.4.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/autogen/rel5.18.4/autogen-5.18.4.tar.xz'
+  sha1 '3d5aa8d99742e92098bb438c684bee5e978a8dd7'
 
   bottle do
     sha1 "f0f73e326bc3f93b8e9095ed79a7baa50ca2e9b7" => :mavericks
@@ -12,24 +12,8 @@ class Autogen < Formula
     sha1 "729f19be0284020f55ebc5b343a9811cde92630c" => :lion
   end
 
-  # Please note, 5.18.2 is not the newest Autogen package.
-  # However, 5.18.3 has an unresolved guile issue and should not be updated to.
-  # Please do not submit an update to 5.18.3 until this issue (https://github.com/Homebrew/homebrew/pull/30406) is resolved.
-  # The ongoing 5.18.3 guile issue has been reported upstream to Autogen's devs.
-
-  depends_on 'guile'
   depends_on 'pkg-config' => :build
-
-  fails_with :clang do
-    build 500
-    cause <<-EOS.undent
-      Clang does not appear to be able to handle variables using the name
-      'noreturn' as it is a new keyword in C++11, but it would normally be
-      used as in [[noreturn]] void foo() and not as a variable name. I'm not
-      sure if this is a compiler bug or what, but GCC handles it fine at
-      the moment.
-    EOS
-  end
+  depends_on 'guile'
 
   def install
     system "./configure", "--disable-debug",
