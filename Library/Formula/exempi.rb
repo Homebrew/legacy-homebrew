4f87d156a4abb53ab20064ff88fe43a5ef6c4f38
Mike McQuaid <mike@mikemcquaid.com>
2014-09-09 12:48:41 +0100

    exempi: general cleanup

--- Library/Formula/exempi.rb
+++ Library/Formula/exempi.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Exempi < Formula
-  homepage 'http://libopenraw.freedesktop.org/wiki/Exempi'
-  url 'http://libopenraw.freedesktop.org/download/exempi-2.2.2.tar.bz2'
-  sha1 'c0a0014e18f05aa7fac210c84788ef073718a9d8'
+  homepage "http://libopenraw.freedesktop.org/wiki/Exempi"
+  url "http://libopenraw.freedesktop.org/download/exempi-2.2.2.tar.bz2"
+  sha1 "c0a0014e18f05aa7fac210c84788ef073718a9d8"
 
   bottle do
     cellar :any
@@ -12,12 +12,12 @@ class Exempi < Formula
     sha1 "9d047f2558f28bfa9e008e4f4ec11939b74ec99d" => :lion
   end
 
-  depends_on 'boost'
+  depends_on "boost"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-boost=#{HOMEBREW_PREFIX}"
-    system "make install"
+    system "make", "install"
   end
 end
