0398644d2b6bcd841884e3acd0bb4e2c02d2090c
Chaitanya Akkineni <cakkineni@gmail.com>
2014-09-09 16:46:52 -0500

    Updating panamax.rb to fix issues outlined in previous pull request.

--- Library/Formula/panamax.rb
+++ Library/Formula/panamax.rb
@@ -1,21 +1,21 @@
 require "formula"
 
+
 class Panamax < Formula
-  homepage "http://www.centurylinklabs.com"
-  url "http://download.panamax.io/installer/pmx-installer-0.1.0.zip"
-  sha1 "56f19e7da78e746c7a3bfcb28dca884942ea4dde"
+  homepage "http://www.panamax.io"
+  url "http://download.panamax.io/installer/panamax-0.2.0.tar.gz"
+  sha1 "5de8a05d835a956f8905edc7073f0a652943328f"
 
   def install
-    cachedir = HOMEBREW_CACHE + "panamax-#{version}.zip"
-    `unzip  -ou #{cachedir}  -d #{ENV['HOME']}/.panamax`
+    system "./configure #{prefix}"
+    system "make", "install"
     bin.install "panamax"
     opoo "If upgrading the Panamax Installer, be sure to run 'panamax reinstall' to ensure compatibility with other Panamax components."
   end
 
   test do
-     installed = File.exist?(ENV['HOME'] + '/.panamax')
-     assert installed
-     assert_equal 0, $?.exitstatus
+    installed = File.exist?("#{prefix}/.panamax")
+    assert installed
+    assert_equal 0, $?.exitstatus
   end
-
 end
