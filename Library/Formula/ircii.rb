4ee5b5bc86bcfccf7f0cb6bbed27941ac6a6e50f
Mike McQuaid <mike@mikemcquaid.com>
2014-09-09 16:57:53 +0100

    ircii: general cleanup.

--- Library/Formula/ircii.rb
+++ Library/Formula/ircii.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Ircii < Formula
-  homepage 'http://www.eterna.com.au/ircii/'
-  url 'http://ircii.warped.com/ircii-20140831.tar.bz2'
-  sha1 'a4d3b3a74a418f99217fe572f6e4c358f1ff3139'
+  homepage "http://www.eterna.com.au/ircii/"
+  url "http://ircii.warped.com/ircii-20140831.tar.bz2"
+  sha1 "a4d3b3a74a418f99217fe572f6e4c358f1ff3139"
 
   bottle do
     sha1 "c95445429da0b3d45122f84cb597ecc707a42939" => :mavericks
@@ -14,12 +14,12 @@ class Ircii < Formula
   depends_on "openssl"
 
   def install
-    ENV.append 'LIBS', '-liconv'
+    ENV.append "LIBS", "-liconv"
     system "./configure", "--prefix=#{prefix}",
                           "--with-default-server=irc.freenode.net",
                           "--enable-ipv6"
     system "make"
     ENV.deparallelize
-    system "make install"
+    system "make", "install"
   end
 end
