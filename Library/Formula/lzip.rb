7333a4bd1807a46124310cc37c1549380db47ca3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-09 14:01:07 +0100

    lzip: add 1.16 bottle.
    Closes #32185.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/lzip.rb
+++ Library/Formula/lzip.rb
@@ -5,6 +5,13 @@ class Lzip < Formula
   url 'http://download.savannah.gnu.org/releases/lzip/lzip-1.16.tar.gz'
   sha1 '5bcefbb788305db7be9748d3c0478156518f1025'
 
+  bottle do
+    cellar :any
+    sha1 "1d027a061ab2107a18bf2819d29d40318eaf106a" => :mavericks
+    sha1 "8a3a453e1cc7d79d3cb6417a93e156edb44f7f9b" => :mountain_lion
+    sha1 "3dbd0d6992d923a23ec3c25c04cb141a7bf3792a" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "CXX=#{ENV.cxx}",
