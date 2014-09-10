92df0d073887cc6405fe1cf22c39e3218e734451
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-10 14:34:00 +0100

    ndpi: add 1.5.1 bottle.

--- Library/Formula/ndpi.rb
+++ Library/Formula/ndpi.rb
@@ -5,6 +5,13 @@ class Ndpi < Formula
   url "https://downloads.sourceforge.net/project/ntop/nDPI/libndpi-1.5.1.tar.gz"
   sha1 "0a6ed585545ab6611f3f0ac9efd9eb36bb5481dd"
 
+  bottle do
+    cellar :any
+    sha1 "81793f54b751d220e7f2bd2389f48e975a84c153" => :mavericks
+    sha1 "2384472ca14acbfa1e8be322f8ed0cd00b7cf608" => :mountain_lion
+    sha1 "b5e612452ecad91acecb4692b2b7fd24ada70b21" => :lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
