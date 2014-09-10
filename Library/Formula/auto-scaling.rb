54e11f51990f139b2c7b5f2dd0d7eb5d4ed3a7a2
Samuel Cochran <sj26@sj26.com>
2014-09-11 07:39:59 +1000

    auto-scaling: correct caveats
    Closes #32219.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- Library/Formula/auto-scaling.rb
+++ Library/Formula/auto-scaling.rb
@@ -11,6 +11,24 @@ class AutoScaling < AmazonWebServicesFormula
   end
 
   def caveats
-    standard_instructions "AWS_AUTO_SCALING_HOME"
+    <<-EOS.undent
+      Before you can use these tools you must populate a file and export some variables to your $SHELL.
+
+      You must create a credential file containing:
+
+      AWSAccessKeyId=<Your AWS Access ID>
+      AWSSecetKey=<Your AWS Secret Key>
+
+      Then to export the needed variables, add them to your dotfiles.
+       * On Bash, add them to `~/.bash_profile`.
+       * On Zsh, add them to `~/.zprofile` instead.
+
+      export JAVA_HOME="$(/usr/libexec/java_home)"
+      export AWS_AUTO_SCALING_HOME="#{libexec}"
+      export AWS_CREDENTIAL_FILE="<Path to credential file>"
+
+      See the website for more details:
+      http://docs.aws.amazon.com/AutoScaling/latest/DeveloperGuide/UsingTheCommandLineTools.html
+    EOS
   end
 end
