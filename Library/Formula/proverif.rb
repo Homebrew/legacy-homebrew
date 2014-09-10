b8e3e701f9b778fe1ee1596d17b7bf9d5e83329e
Benjamin Beurdouche <beurdouche@users.noreply.github.com>
2014-09-10 17:17:34 +0200

    Update proverif.rb
    add version number

--- Library/Formula/proverif.rb
+++ Library/Formula/proverif.rb
@@ -4,6 +4,7 @@ class Proverif < Formula
   homepage "http://prosecco.gforge.inria.fr/personal/bblanche/proverif"
   url "http://prosecco.gforge.inria.fr/personal/bblanche/proverif/proverif1.88pl1.tar.gz"
   sha1 "d03d63d9ad30eaec3c6f60ab187a0da6490000ca"
+  version "1.88.1"
 
   depends_on 'ocaml'
 
