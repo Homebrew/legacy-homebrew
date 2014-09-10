0a672cd668990ff8ae11a226cc3b455a82c51ced
Chaitanya Akkineni <cakkineni@gmail.com>
2014-09-10 15:02:19 -0500

    Updating based on comments from previous submission.

--- Library/Formula/panamax.rb
+++ Library/Formula/panamax.rb
@@ -4,13 +4,15 @@ require "formula"
 class Panamax < Formula
   homepage "http://www.panamax.io"
   url "http://download.panamax.io/installer/panamax-0.2.0.tar.gz"
-  sha1 "5de8a05d835a956f8905edc7073f0a652943328f"
+  sha1 "cd157ba241b1ca6c1eae1e347dbb2e91b324be9c"
 
   def install
-    system "./configure #{prefix}"
+    system "./configure", "#{prefix}"
     system "make", "install"
-    bin.install "panamax"
-    opoo "If upgrading the Panamax Installer, be sure to run 'panamax reinstall' to ensure compatibility with other Panamax components."
+  end
+
+  def caveats
+    "If upgrading the Panamax Installer, be sure to run 'panamax reinstall' to ensure compatibility with other Panamax components."
   end
 
   test do
