4198a9e72fee7e2d9d373ef2a172db3402a42608
Jack Nagel <jacknagel@gmail.com>
2014-09-10 22:42:57 -0500

    libtorrent-rasterbar 1.0.2

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -2,9 +2,8 @@ require "formula"
 
 class LibtorrentRasterbar < Formula
   homepage "http://www.rasterbar.com/products/libtorrent/"
-  url "https://downloads.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-1.0.1.tar.gz"
-  sha1 "98f0117bb716cc6210d5698068aa491bc0ac8ad0"
-  revision 1
+  url "https://downloads.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-1.0.2.tar.gz"
+  sha1 "bfb161825306abbd5c03775e75f5e094ee757a9a"
 
   head do
     url "https://libtorrent.googlecode.com/svn/trunk"
@@ -14,11 +13,6 @@ class LibtorrentRasterbar < Formula
   end
 
   bottle do
-    cellar :any
-    revision 1
-    sha1 "969beb9ebd5d43e9883b099f5e437f124ada4eab" => :mavericks
-    sha1 "7be7e96575fb8e7ec9e52d095d27088379650460" => :mountain_lion
-    sha1 "8146b1dfa41312b545706a9a98cb3cc111c56c89" => :lion
   end
 
   depends_on "pkg-config" => :build
