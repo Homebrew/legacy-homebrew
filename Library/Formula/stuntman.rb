7423501afb7c9e3f8e765a5a82afafc0ea8d2301
Andrew Barnert <abarnert@yahoo.com>
2014-09-10 20:11:15 -0700

    Switch from https to http.
    - The server is apparently not reliably reachable over SSL.

--- Library/Formula/stuntman.rb
+++ Library/Formula/stuntman.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Stuntman < Formula
   homepage "http://www.stunprotocol.org/"
-  url "https://www.stunprotocol.org/stunserver-1.2.7.tgz"
+  url "http://www.stunprotocol.org/stunserver-1.2.7.tgz"
   sha1 "bc315c5f81e5bf9301872d096db8a8ac74089de4"
   head "https://github.com/jselbie/stunserver.git"
 
