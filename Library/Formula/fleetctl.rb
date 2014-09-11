848eed12654487e93130feadb11341e18cd7885a
Brian Waldon <bcwaldon@gmail.com>
2014-09-11 07:38:38 -0700

    fleetctl 0.8.0

--- Library/Formula/fleetctl.rb
+++ Library/Formula/fleetctl.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Fleetctl < Formula
   homepage "https://github.com/coreos/fleet"
-  url "https://github.com/coreos/fleet/archive/v0.7.1.tar.gz"
-  sha1 "78b072d91845c75613a48c8424c27f1422cceb51"
+  url "https://github.com/coreos/fleet/archive/v0.8.0.tar.gz"
+  sha1 "2b45b665513b3ce6812b4f50c48f38891d9c0529"
   head "https://github.com/coreos/fleet.git"
 
   bottle do
