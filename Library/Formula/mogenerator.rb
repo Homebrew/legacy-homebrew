98a7cc944e1881146210821cf8d08ef73d260190
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-11 05:44:58 +0100

    mogenerator: add 1.28 bottle.

--- Library/Formula/mogenerator.rb
+++ Library/Formula/mogenerator.rb
@@ -7,6 +7,13 @@ class Mogenerator < Formula
 
   head 'https://github.com/rentzsch/mogenerator.git'
 
+  bottle do
+    cellar :any
+    sha1 "6a0c63e99ecae49d70b569b5b4507b8352ada961" => :mavericks
+    sha1 "abc1bcf2e7d1ebbb3258023f57e082052061a19c" => :mountain_lion
+    sha1 "fc60b8470f7e5441a599496aa637fd21da83934a" => :lion
+  end
+
   depends_on :xcode => :build
 
   def install
