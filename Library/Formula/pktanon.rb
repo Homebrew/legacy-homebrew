a4c167216064f658dcc8289916d6b3b6ed257ef7
kbinani <kbinani.bt@gmail.com>
2014-09-11 21:30:12 -0500

    pktanon: add a patch to fix build failure
    Closes #32200.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pktanon.rb
+++ Library/Formula/pktanon.rb
@@ -1,12 +1,29 @@
-require 'formula'
+require "formula"
 
 class Pktanon < Formula
-  homepage 'http://www.tm.uka.de/software/pktanon/index.html'
-  url 'http://www.tm.uka.de/software/pktanon/download/pktanon-1.4.0-dev.tar.gz'
-  sha1 '458530c2b167694ee7a29653033e86f0b23e54bd'
+  homepage "http://www.tm.uka.de/software/pktanon/index.html"
+  url "http://www.tm.uka.de/software/pktanon/download/pktanon-1.4.0-dev.tar.gz"
+  sha1 "458530c2b167694ee7a29653033e86f0b23e54bd"
 
-  depends_on 'xerces-c'
-  depends_on 'boost'
+  depends_on "xerces-c"
+  depends_on "boost"
+
+  # fix compile failure caused by undefined function 'sleep'.
+  patch <<-EOS.undent
+    diff --git a/src/Timer.cpp b/src/Timer.cpp
+    index f97be1d..6fb7d53 100644
+    --- a/src/Timer.cpp
+    +++ b/src/Timer.cpp
+    @@ -17,6 +17,7 @@
+     //\r
+     \r
+     #include "Timer.h"\r
+    +#include <unistd.h> // for ::sleep(unsigned int)\r
+     \r
+     Timer::Timer ()\r
+     :	userdata	(NULL),\r
+
+  EOS
 
   def install
     # include the boost system library to resolve compilation errors
