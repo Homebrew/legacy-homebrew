db90e038bbf4ec5ea888f7861bd3a032a8f2bcce
Shaun Jackman <sjackman@gmail.com>
2014-09-11 18:59:32 -0700

    linux-headers: Remove the xz dependency

--- Library/Formula/linux-headers.rb
+++ Library/Formula/linux-headers.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class LinuxHeaders < Formula
   homepage "http://kernel.org/"
-  url "https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.15.9.tar.xz"
-  sha1 "c1bed517c0c7ff3514833ec99e1124ab20d9dc1d"
+  url "https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.15.9.tar.gz"
+  sha1 "068aefd8d7a5ef62e152d6d45228c53bdec4a2cc"
 
   def install
     system "make", "headers_install", "INSTALL_HDR_PATH=#{prefix}"
