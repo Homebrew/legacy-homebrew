2b6ed58ec382b658e6becbc0517335e53ea1a811
Jack Nagel <jacknagel@gmail.com>
2014-09-11 21:30:12 -0500

    pktanon: use inreplace

--- Library/Formula/pktanon.rb
+++ Library/Formula/pktanon.rb
@@ -8,24 +8,11 @@ class Pktanon < Formula
   depends_on "xerces-c"
   depends_on "boost"
 
-  # fix compile failure caused by undefined function 'sleep'.
-  patch <<-EOS.undent
-    diff --git a/src/Timer.cpp b/src/Timer.cpp
-    index f97be1d..6fb7d53 100644
-    --- a/src/Timer.cpp
-    +++ b/src/Timer.cpp
-    @@ -17,6 +17,7 @@
-     //\r
-     \r
-     #include "Timer.h"\r
-    +#include <unistd.h> // for ::sleep(unsigned int)\r
-     \r
-     Timer::Timer ()\r
-     :	userdata	(NULL),\r
-
-  EOS
-
   def install
+    # fix compile failure caused by undefined function 'sleep'.
+    inreplace "src/Timer.cpp", %{#include "Timer.h"\r\n},
+      %{#include "Timer.h"\r\n#include "unistd.h"\r\n}
+
     # include the boost system library to resolve compilation errors
     ENV["LIBS"] = "-lboost_system-mt"
 
