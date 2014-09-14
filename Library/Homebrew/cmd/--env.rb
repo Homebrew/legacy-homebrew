3df80e131bdd12fc37b73d27abc5fb3e69ca424a
Jack Nagel <jacknagel@gmail.com>
2014-09-14 01:26:45 -0500

    Remove unnecessary conditional
    If ARGV.named.empty?, then ARGV.formulae.map(&:name) == [].

--- Library/Homebrew/cmd/--env.rb
+++ Library/Homebrew/cmd/--env.rb
@@ -3,13 +3,10 @@ require "extend/ENV"
 module Homebrew
   def __env
     ENV.activate_extensions!
-
-    if superenv?
-      ENV.deps = ARGV.formulae.map(&:name) unless ARGV.named.empty?
-    end
-
+    ENV.deps = ARGV.formulae.map(&:name) if superenv?
     ENV.setup_build_environment
     ENV.universal_binary if ARGV.build_universal?
+
     if $stdout.tty?
       dump_build_env ENV
     else
