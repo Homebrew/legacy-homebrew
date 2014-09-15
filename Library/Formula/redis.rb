f4a603cfd3baf1934f9c16cf79a234f90be7e05c
Joep Loijens <joep.loijens@gmail.com>
2014-09-15 09:19:17 +0200

    redis 2.8.15

--- Library/Formula/redis.rb
+++ Library/Formula/redis.rb
@@ -2,13 +2,10 @@ require 'formula'
 
 class Redis < Formula
   homepage 'http://redis.io/'
-  url "http://download.redis.io/releases/redis-2.8.14.tar.gz"
-  sha1 "fd0edc045f2b53057d7f6ab38e2fe99086c87e8d"
+  url "http://download.redis.io/releases/redis-2.8.15.tar.gz"
+  sha1 "afc0d753cea68a26038775df2dea75a76e3d0e1d"
 
   bottle do
-    sha1 "ccce44232307e1b943be22831ce058dda0c4affb" => :mavericks
-    sha1 "26a3c474596d83107b745d6c5e436f655ccdc42a" => :mountain_lion
-    sha1 "16db4d79e31274c08e700389f2feb4b2ccf8ee6a" => :lion
   end
 
   head 'https://github.com/antirez/redis.git', :branch => 'unstable'
