2cc793e438771e7d7976cdda425f5e9b0c5e6f8e
Mike McQuaid <mike@mikemcquaid.com>
2014-09-15 10:30:39 +0100

    bash-git-prompt: general cleanup.

--- Library/Formula/bash-git-prompt.rb
+++ Library/Formula/bash-git-prompt.rb
@@ -3,17 +3,18 @@ require "formula"
 class BashGitPrompt < Formula
   homepage "https://github.com/magicmonty/bash-git-prompt"
   url "https://github.com/magicmonty/bash-git-prompt/archive/2.0.tar.gz"
-  head "https://github.com/magicmonty/bash-git-prompt", :using => :git
   sha1 "3c0bc71302b97260cf8d14a1f01be732039365b9"
+  head "https://github.com/magicmonty/bash-git-prompt.git"
 
   def install
-    share.install "./gitprompt.sh", "./gitprompt.fish", "./git-prompt-help.sh", "./git-prompt-colors.sh", "gitstatus.sh", "prompt-colors.sh"
+    share.install "./gitprompt.sh", "./gitprompt.fish", "./git-prompt-help.sh",
+                  "./git-prompt-colors.sh", "gitstatus.sh", "prompt-colors.sh"
     doc.install "README.md"
   end
 
   def caveats; <<-EOS.undent
-    You should add the following to your .bashrc or equivalent:
-    source #{opt_share}/gitprompt.sh
+    You should add the following to your .bashrc (or equivalent):
+      source #{opt_share}/gitprompt.sh
     EOS
   end
 end
