7752b489ece8b6e9ce3eb314572f9d7a7918fb32
unforswearing <notes@theforeverarrow.com>
2014-09-16 02:23:42 -0400

    dued new formula
    updated formulae

--- Library/Formula/dued.rb
+++ Library/Formula/dued.rb
@@ -5,11 +5,4 @@ class Dued < Formula
   url "https://github.com/unforswearing/dued/blob/master/dued.rb.zip"
   sha1 "eed23710121f3945e445947ea43dad8c0dde2df8"
   version "0.1"
-
- def install
-bin.install 'bin/dued'
-end
-test do
-system "#{bin}/dued"
-end
 end
