2ea458c262b37dfe4f81f05f40df33a505d28d5a
Jack Nagel <jacknagel@gmail.com>
2014-09-16 18:36:03 -0500

    libtasn1 4.2

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Libtasn1 < Formula
   homepage "https://www.gnu.org/software/libtasn1/"
-  url "http://ftpmirror.gnu.org/libtasn1/libtasn1-4.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.1.tar.gz"
-  sha1 "a4cdf91b6130d29b5b69dca17a1e85053ac54e7b"
+  url "http://ftpmirror.gnu.org/libtasn1/libtasn1-4.2.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.2.tar.gz"
+  sha1 "d2fe4bf12dbdc4d6765a04abbf8ddaf7e9163afa"
 
   bottle do
     cellar :any
