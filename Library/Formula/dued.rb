f77813bd65c8996b38c7b7b493ea3a8a8b5121d5
unforswearing <notes@theforeverarrow.com>
2014-09-16 02:39:49 -0400

    dued new formula
    update dued
    new sha1 new location

--- Library/Formula/dued.rb
+++ Library/Formula/dued.rb
@@ -1,8 +1,13 @@
 require "formula"
 
 class Dued < Formula
-  homepage "https://github.com/unforswearing/dued"
-  url "https://github.com/unforswearing/dued/blob/master/dued.rb.zip"
-  sha1 "eed23710121f3945e445947ea43dad8c0dde2df8"
+  homepage "https://gist.github.com/unforswearing/2263f5223e46b647f955"
+  url "https://gist.githubusercontent.com/unforswearing/2263f5223e46b647f955/raw/1de9add2f295c3b7bd6c5cadb6b8346916b98558/dued.rbp"
+  sha1 "4ac2474d081e575408e9ae5c8cf0c22dff326481"
   version "0.1"
+
+  def install
+    system "make"
+    system "make", "install"
+  end
 end
