1d02e8a8ca7eecc3e5337ba155c9136d70c0c7ed
unforswearing <notes@theforeverarrow.com>
2014-09-16 02:44:00 -0400

    update
    dued

--- Library/Formula/dued.rb
+++ Library/Formula/dued.rb
@@ -3,7 +3,7 @@ require "formula"
 class Dued < Formula
   homepage "https://gist.github.com/unforswearing/2263f5223e46b647f955"
   url "https://gist.githubusercontent.com/unforswearing/2263f5223e46b647f955/raw/1de9add2f295c3b7bd6c5cadb6b8346916b98558/dued.rbp"
-  sha1 "3943c169390ce09ef2dd060eeaba15ab95f3f620"
+  sha1 "4109335a65cc2deb712c2cf526ff2dd93ef20db6"
   version "0.1"
 
   def install
