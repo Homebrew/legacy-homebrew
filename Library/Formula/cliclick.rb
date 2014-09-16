e16f4f0d1a06bc0e016f91adab926a225f968c72
Carsten Blüm <carsten@bluem.net>
2014-09-16 14:28:50 +0200

    cliclick 3.0.1

--- Library/Formula/cliclick.rb
+++ Library/Formula/cliclick.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Cliclick < Formula
   homepage "http://www.bluem.net/jump/cliclick/"
-  url "https://github.com/BlueM/cliclick/archive/3.0.tar.gz"
-  sha1 "e4984645f771d1e80c0a2efc6c25cb3590a05d94"
+  url "https://github.com/BlueM/cliclick/archive/3.0.1.tar.gz"
+  sha1 "be5e9069b1706da0b49f6a127a1912bb67346213"
 
   def install
     system "make"
