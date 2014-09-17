bbf48f635d80d882d44cbb409d5e3b9a7fc14922
Jack Nagel <jacknagel@gmail.com>
2014-09-16 22:32:18 -0500

    Fix variable shadowing bug in requirement expansion
    Long term we should probably rename the installer's "f" attribute to
    "formula".

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -254,9 +254,9 @@ class FormulaInstaller
 
         if (req.optional? || req.recommended?) && build.without?(req)
           Requirement.prune
-        elsif req.build? && dependent == f && pour_bottle?
+        elsif req.build? && dependent == self.f && pour_bottle?
           Requirement.prune
-        elsif req.build? && dependent != f && install_bottle_for_dep?(dependent, build)
+        elsif req.build? && dependent != self.f && install_bottle_for_dep?(dependent, build)
           Requirement.prune
         elsif install_requirement_default_formula?(req, build)
           dep = req.to_dependency
