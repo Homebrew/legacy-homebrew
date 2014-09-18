00e8474e9f2f4daf19fba4d1e8cd58e14d786c3a
Jack Nagel <jacknagel@gmail.com>
2014-09-18 14:37:47 -0500

    bib-tool: remove unnecessary autoreconf

--- Library/Formula/bib-tool.rb
+++ Library/Formula/bib-tool.rb
@@ -5,13 +5,7 @@ class BibTool < Formula
   url 'http://www.gerd-neugebauer.de/software/TeX/BibTool/BibTool-2.57.tar.gz'
   sha1 'a6e80c86d347a39f3883e552db2dd4deb72b0e86'
 
-  depends_on :autoconf
-  depends_on :automake
-  depends_on :libtool
-
   def install
-    # Needd to pick up the --without-kpathsea argument
-    system "autoreconf", "-fi"
     system "./configure", "--prefix=#{prefix}", "--without-kpathsea"
     system "make"
     system "make install"
