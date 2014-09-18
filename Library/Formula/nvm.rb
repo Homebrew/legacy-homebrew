54dd930fed8ee4af16154e48880f6954845b4c2a
Trevor Powell <trevor.powell@morningstar.com>
2014-09-18 16:53:16 -0500

    nvm 0.16.1
    Closes #32413.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/nvm.rb
+++ Library/Formula/nvm.rb
@@ -3,8 +3,8 @@ require "formula"
 class Nvm < Formula
   homepage "https://github.com/creationix/nvm"
   head "https://github.com/creationix/nvm.git"
-  url "https://github.com/creationix/nvm/archive/v0.16.0.tar.gz"
-  sha1 "0964e91e876a98605d0dbf9145e3e9279dda57a3"
+  url "https://github.com/creationix/nvm/archive/v0.16.1.tar.gz"
+  sha1 "3bd8ce291b52886feeaa31e60e6489aeda350ad1"
 
   def install
     prefix.install "nvm.sh"
