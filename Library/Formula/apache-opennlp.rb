7a2a92b1ae4e0c00f8a55b3cd2ed173a40ed0924
Kevin Barbour <kevinbarbourd@gmail.com>
2014-09-18 11:15:01 -0400

    Replace broken mirror link.

--- Library/Formula/apache-opennlp.rb
+++ Library/Formula/apache-opennlp.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class ApacheOpennlp < Formula
   homepage "http://opennlp.apache.org/"
-  url "http://apache.mirrors.timporter.net//opennlp/opennlp-1.5.3/apache-opennlp-1.5.3-bin.tar.gz"
+  url "http://mirror.symnds.com/software/Apache//opennlp/opennlp-1.5.3/apache-opennlp-1.5.3-bin.tar.gz"
   sha1 "e14b41a4f1f1ae7fd12713bbdd8452b367bfdc9e"
 
   def install
