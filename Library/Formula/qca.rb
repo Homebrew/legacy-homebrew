f46fca6b3574f174456bd2563fe9b1ce622b35f9
Larry Shaffer <larrys@dakotacarto.com>
2014-09-17 23:09:59 -0600

    qca 2.1.0, with Qt plugins

--- Library/Formula/qca.rb
+++ Library/Formula/qca.rb
@@ -1,21 +1,94 @@
-require 'formula'
+require "formula"
 
 class Qca < Formula
-  homepage 'http://delta.affinix.com/qca/'
-  url 'http://delta.affinix.com/download/qca/2.0/qca-2.0.3.tar.bz2'
-  sha1 '9c868b05b81dce172c41b813de4de68554154c60'
+  homepage "http://delta.affinix.com/qca/"
+  url "http://delta.affinix.com/download/qca/2.0/qca-2.1.0.tar.gz"
+  sha1 "2b582b3ccc7e6098cd14d6f52a829ae1539e9cc8"
 
+  head "git://anongit.kde.org/qca.git", :branch => "master"
+
+  option "with-api-docs", "Build Doxygen API documentation"
+  option "without-tests", "Don't build and run tests"
+  option "without-plugins", "Skip building plugins"
+
+  depends_on "cmake" => :build
   depends_on 'qt'
+  depends_on "qt5" => :optional
 
-  # Fix for clang adhering strictly to standard, see:
-  # http://clang.llvm.org/compatibility.html#dep_lookup_bases
-  patch do
-    url "http://quickgit.kde.org/?p=qca.git&a=commitdiff&h=312b69&o=plain"
-    sha1 "f3b1f645e35f46919d9bf9ed6f790619c7d03631"
+  # Plugins (QCA needs at least one plugin to do anything useful)
+  if build.with? "plugins"
+    depends_on "openssl" => :recommended # qca-ossl
+    depends_on "botan" => :optional # qca-botan
+    depends_on "libgcrypt" => :optional # qca-gcrypt
+    depends_on "gnupg" => :optional # qca-gnupg (currently segfaults in tests)
+    depends_on "nss" => :optional # qca-nss
+    depends_on "pkcs11-helper" => :optional # qca-pkcs11
+  end
+
+  if build.with? "api-docs"
+    depends_on "graphviz" => :build
+    depends_on "doxygen" => [:build, "with-dot"]
+  end
+
+  def certs_store
+    prefix/"certs"
+  end
+
+  def root_certs
+    certs_store/"rootcerts.pem"
+  end
+
+  def populate_store
+    # Culled from openssl formula
+    ohai "Populating #{root_certs}"
+    keychains = %w[
+      /Library/Keychains/System.keychain
+      /System/Library/Keychains/SystemRootCertificates.keychain
+    ]
+    certs_store.mkpath
+    rm_f root_certs
+    root_certs.atomic_write `security find-certificate -a -p #{keychains.join(" ")}`
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-tests"
-    system "make install"
+    args = std_cmake_args
+    args << "-DQT4_BUILD=#{build.with?("qt5") ? "OFF" : "ON"}"
+    args << "-DBUILD_TESTS=#{build.with?("tests") ? "ON" : "OFF"}"
+    args << "-DBUILD_PLUGINS=#{build.with?("plugins") ? "auto" : "none"}"
+
+    args << "-DWITH_ossl_PLUGIN=#{build.with?("openssl") ? "YES" : "NO"}"
+    args << "-DWITH_botan_PLUGIN=#{build.with?("botan") ? "YES" : "NO"}"
+    args << "-DWITH_gcrypt_PLUGIN=#{build.with?("libgcrypt") ? "YES" : "NO"}"
+    args << "-DWITH_gnupg_PLUGIN=#{build.with?("gnupg") ? "YES" : "NO"}"
+    args << "-DWITH_nss_PLUGIN=#{build.with?("nss") ? "YES" : "NO"}"
+    args << "-DWITH_pkcs11_PLUGIN=#{build.with?("pkcs11-helper") ? "YES" : "NO"}"
+
+    qca_arch = buildpath/"images/qca-arch.png"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+
+      # Don't block install on test failure, as it may be just one plugin, or
+      # the randomly failing FileWatch test
+      safe_system "make test 2>&1 | tee #{prefix}/make-test.log" if build.with? "tests"
+
+      if build.with? "api-docs"
+        system "make", "doc"
+        doc.install "apidocs/html"
+        (doc/"html").install qca_arch
+      end
+    end
+
+    # Symlink plugins into qt formula prefix
+    # First move plugins out of lib, or segfault on unload of plugin provider instance
+    qca_plugins = "#{prefix}/qca_plugins"
+    mkpath qca_plugins
+    mv "#{lib}/qca/crypto", "#{qca_plugins}/crypto"
+    ln_sf "#{qca_plugins}/crypto", Formula["qt"].prefix/"plugins/"
+  end
+
+  def post_install
+      populate_store
   end
 end
