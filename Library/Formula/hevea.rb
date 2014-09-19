9b2e57a7b6f52085a829d76672631e75ab677173
Mike McQuaid <mike@mikemcquaid.com>
2014-09-19 19:18:00 +0100

    hevea 2.18
    Also: add test, fix prefix.

--- Library/Formula/hevea.rb
+++ Library/Formula/hevea.rb
@@ -2,21 +2,24 @@ require "formula"
 
 class Hevea < Formula
   homepage "http://hevea.inria.fr/"
-  url "http://hevea.inria.fr/distri/hevea-2.16.tar.gz"
-  sha1 "729438f4c37240df7b20053dec18e9ee17d19d46"
-
-  bottle do
-    cellar :any
-    sha1 "e2913f505d600a5a44ea31ee28bd6f80574bbcf0" => :mavericks
-    sha1 "c48beb71f6e9489cdb105dd53f066c09dd26a3ad" => :mountain_lion
-    sha1 "421f60fc322551f1e80a7215475f57c43319165b" => :lion
-  end
+  url "http://hevea.inria.fr/distri/hevea-2.18.tar.gz"
+  sha1 "1fc764a6fc946069b4ca91b29fa1e71c405265d9"
 
   depends_on "objective-caml"
   depends_on "ghostscript" => :optional
 
   def install
+    inreplace "Makefile", "PREFIX=/usr/local", "PREFIX=#{prefix}"
     system "make"
-    system "make", "PREFIX=#{prefix}", "install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.tex").write <<-EOS.undent
+      \\documentclass{article}
+      \\begin{document}
+      \\end{document}
+    EOS
+    system "#{bin}/hevea", "test.tex"
   end
 end
