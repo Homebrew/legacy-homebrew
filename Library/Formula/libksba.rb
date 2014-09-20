fbe6ea29d0a320dca062698b816f4a7e2eb199a7
chdiza <chdiza@gmail.com>
2014-09-20 10:43:02 -0400

    libksba 1.3.1

--- Library/Formula/libksba.rb
+++ Library/Formula/libksba.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libksba < Formula
   homepage 'http://www.gnupg.org/related_software/libksba/index.en.html'
-  url 'ftp://ftp.gnupg.org/gcrypt/libksba/libksba-1.3.0.tar.bz2'
-  mirror 'http://ftp.heanet.ie/mirrors/ftp.gnupg.org/gcrypt/libksba/libksba-1.3.0.tar.bz2'
-  sha1 '241afcb2dfbf3f3fc27891a53a33f12d9084d772'
+  url 'ftp://ftp.gnupg.org/gcrypt/libksba/libksba-1.3.1.tar.bz2'
+  mirror 'http://ftp.heanet.ie/mirrors/ftp.gnupg.org/gcrypt/libksba/libksba-1.3.1.tar.bz2'
+  sha1 '6bfe285dbc3a7b6e295f9389c20ea1cdf4947ee5'
 
   bottle do
     cellar :any
