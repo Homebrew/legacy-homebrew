1b5596392cb99858b9bcf25554eccfc5bc94945e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-20 16:02:37 +0100

    libksba: update 1.3.1 bottle.
    Closes #32451.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/libksba.rb
+++ Library/Formula/libksba.rb
@@ -8,9 +8,9 @@ class Libksba < Formula
 
   bottle do
     cellar :any
-    sha1 "45f55ff042d927fda0d9ae290a509b1ceb285ce6" => :mavericks
-    sha1 "e10515a7634d58e5948c78f378f0690e8e7274ac" => :mountain_lion
-    sha1 "fbe0bd166c4766fcacbbee7df0320b77b8ef6f0b" => :lion
+    sha1 "e3d69fd49c3970da47b084ef6faecf6c1865f15c" => :mavericks
+    sha1 "87668c3f1fe99638c308788a7e0e2769bcd61309" => :mountain_lion
+    sha1 "add30a335e98dcc4a582e2ea94e6221530d3b45f" => :lion
   end
 
   depends_on 'libgpg-error'
