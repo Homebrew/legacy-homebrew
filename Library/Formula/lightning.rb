170ea40e733ecb4e0fc0e3f492b5e8b5978ccf04
Brett Koonce <koonce@gmail.com>
2014-09-20 09:07:10 -0700

    lightning 2.0.5

--- Library/Formula/lightning.rb
+++ Library/Formula/lightning.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Lightning < Formula
-  homepage 'http://www.gnu.org/software/lightning/'
-  url 'http://ftpmirror.gnu.org/lightning/lightning-2.0.4.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/lightning/lightning-2.0.4.tar.gz'
-  sha1 '06982b859dd2322d5bd9e52e0aacebe741a98e6e'
+  homepage "http://www.gnu.org/software/lightning/"
+  url "http://ftpmirror.gnu.org/lightning/lightning-2.0.5.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/lightning/lightning-2.0.5.tar.gz"
+  sha1 "a3b7ba0697690f578bac1d4c991c8e3cbaa33aaf"
 
   def install
     system "./configure","--disable-dependency-tracking", "--prefix=#{prefix}"
