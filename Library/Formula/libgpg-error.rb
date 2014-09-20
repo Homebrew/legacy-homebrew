3d616bab57c4ff3eaeb428a55a90a1833c5f97c4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-20 06:17:40 +0100

    libgpg-error: update 1.16 bottle.

--- Library/Formula/libgpg-error.rb
+++ Library/Formula/libgpg-error.rb
@@ -7,6 +7,10 @@ class LibgpgError < Formula
   sha1 "059c40a2b78c3ac2b4cbec0e0481faba5af332fe"
 
   bottle do
+    cellar :any
+    sha1 "c9d320a135686d2dccef6328a7d9b926ba04db2a" => :mavericks
+    sha1 "5fa28dbea7cf575256f8c7d801f5c162af1b948d" => :mountain_lion
+    sha1 "e934e5ef48e6d87c06324cb5e50637cd979d2bc5" => :lion
   end
 
   option :universal
