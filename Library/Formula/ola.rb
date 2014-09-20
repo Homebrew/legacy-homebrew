170dff02ccd2476eff9f82f3eb2f18e88444be44
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-20 00:03:20 -0500

    ola: update 0.9.1_1 bottle.

--- Library/Formula/ola.rb
+++ Library/Formula/ola.rb
@@ -7,6 +7,9 @@ class Ola < Formula
   revision 1
 
   bottle do
+    sha1 "c2b8f0cdc56866d521557b9827d1285e89e42de8" => :mavericks
+    sha1 "a9b7f59e3e76c2119eca3b20e3193d20ed8218ca" => :mountain_lion
+    sha1 "d2a7b940ab4b72b8a6c061c3c590bcf925907d70" => :lion
   end
 
   option :universal
