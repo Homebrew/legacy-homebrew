533c4acff2a9e7a2ebe1506e1096eed5ef20e433
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-21 01:52:18 +0100

    mikmod: add 3.2.6 bottle.
    Closes #32461.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mikmod.rb
+++ Library/Formula/mikmod.rb
@@ -5,6 +5,12 @@ class Mikmod < Formula
   url "https://downloads.sourceforge.net/project/mikmod/mikmod/3.2.6/mikmod-3.2.6.tar.gz"
   sha1 "55677382ab3e6d5ed35c520669e2cedd395a8ebb"
 
+  bottle do
+    sha1 "81085407e9a27cf49e46cee5ec0d7b6c613cccc1" => :mavericks
+    sha1 "c16208a4e40528c2ef2d70d404b80432bc37f2e2" => :mountain_lion
+    sha1 "fcb7a780fc126c97fdd5c8004785249c6ad5375a" => :lion
+  end
+
   depends_on "libmikmod"
 
   def install
