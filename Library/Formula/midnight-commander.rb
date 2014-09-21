bbf1570864255d82c023bd9a70bace50fd408841
Tomasz Pajor <n@servercrunch.com>
2014-09-21 18:57:51 +0200

    midnight-commander 4.8.13
    Closes #32487.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/midnight-commander.rb
+++ Library/Formula/midnight-commander.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class MidnightCommander < Formula
   homepage 'http://www.midnight-commander.org/'
-  url 'http://ftp.midnight-commander.org/mc-4.8.12.tar.xz'
-  mirror 'ftp://ftp.osuosl.org/pub/midnightcommander/mc-4.8.12.tar.xz'
-  sha256 '5f2fd570a798dc0cc06374adffef8ca403588c4e73dfdf908e9a4311718153fe'
+  url 'http://ftp.midnight-commander.org/mc-4.8.13.tar.xz'
+  mirror 'ftp://ftp.osuosl.org/pub/midnightcommander/mc-4.8.13.tar.xz'
+  sha256 '36d6191a47ec5d89d3788e48846fb620c481816441ff25264add8898d277b657'
 
   bottle do
     sha1 "7b5b58bbf0cbb0dbb5466e9389549de7de7b7e02" => :mavericks
