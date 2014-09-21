3413da87f038b374c00d215b0ce169e58ab2d4c5
Xu Cheng <xucheng@me.com>
2014-09-21 17:06:10 +0800

    keybase 0.6.2

--- Library/Formula/keybase.rb
+++ Library/Formula/keybase.rb
@@ -8,8 +8,8 @@ end
 
 class Keybase < Formula
   homepage "https://keybase.io/"
-  url "https://github.com/keybase/node-client/archive/v0.6.0.tar.gz"
-  sha1 "a94107eea16729f1d1f962f86c1e5769d1f6e494"
+  url "https://github.com/keybase/node-client/archive/v0.6.2.tar.gz"
+  sha1 "709d40cc076578081715c243e1ca5d70d310aeea"
   head "https://github.com/keybase/node-client.git"
 
   depends_on "node"
@@ -21,6 +21,12 @@ class Keybase < Formula
       #!/bin/sh
       export KEYBASE_BIN="#{bin}/keybase"
       exec "#{HOMEBREW_PREFIX}/opt/node/bin/node" "#{libexec}/bin/main.js" "$@"
+    EOS
+  end
+
+  def caveats;<<-EOS.undent if which("gpg2") && !which("gpg")
+      Run below command if you use gpg2 as keybase's backend
+        keybase config gpg gpg2
     EOS
   end
 
