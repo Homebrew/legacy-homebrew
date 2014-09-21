08f81b3c9b5041ed0aea01c62a91f705cf3e6d4b
Jack Nagel <jacknagel@gmail.com>
2014-09-21 17:43:37 -0500

    llvm: fix build on 10.7 and 10.8
    Closes #32495.

--- Library/Formula/llvm.rb
+++ Library/Formula/llvm.rb
@@ -44,7 +44,14 @@ class Llvm < Formula
 
   keg_only :provided_by_osx
 
+  # Apple's libstdc++ is too old to build LLVM
+  fails_with :gcc
+  fails_with :llvm
+
   def install
+    # Apple's libstdc++ is too old to build LLVM
+    ENV.libcxx if ENV.compiler == :clang
+
     if build.with? "python" and build.include? 'disable-shared'
       raise 'The Python bindings need the shared library.'
     end
