50d51db674de7c1d38f0fc53f712ef39689e09b2
Jack Nagel <jacknagel@gmail.com>
2014-09-22 00:12:06 -0500

    plzip 1.2

--- Library/Formula/plzip.rb
+++ Library/Formula/plzip.rb
@@ -1,17 +1,11 @@
-require 'formula'
+require "formula"
 
 class Plzip < Formula
-  homepage 'http://www.nongnu.org/lzip/plzip.html'
-  url 'http://download.savannah.gnu.org/releases/lzip/plzip-1.1.tar.gz'
-  sha1 '22d3bba78b9219e7976372e24773d3abc0ff4563'
+  homepage "http://www.nongnu.org/lzip/plzip.html"
+  url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.2.tar.gz"
+  sha1 "072f3253322ed36f96d452931780eea3dc7ec494"
 
-  depends_on 'lzlib'
-
-  # Stable doesn't build with clang, but 1.2-pre does
-  devel do
-    url 'http://download.savannah.gnu.org/releases/lzip/plzip-1.2-pre1.tar.lz'
-    sha1 '2fbca2f13ff185f1e9824b8fa34ffba79fb7197f'
-  end
+  depends_on "lzlib"
 
   def install
     system "./configure", "--prefix=#{prefix}", "CXX=#{ENV.cxx}",
