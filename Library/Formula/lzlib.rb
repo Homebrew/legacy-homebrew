d32f14948960bb96698d31af18f0ddca59f652e8
Jack Nagel <jacknagel@gmail.com>
2014-09-22 00:10:05 -0500

    lzlib 1.6

--- Library/Formula/lzlib.rb
+++ Library/Formula/lzlib.rb
@@ -1,15 +1,11 @@
-require 'formula'
+require "formula"
 
 class Lzlib < Formula
-  homepage 'http://www.nongnu.org/lzip/lzlib.html'
-  url 'http://download.savannah.gnu.org/releases/lzip/lzlib/lzlib-1.5.tar.gz'
-  sha1 'b89060b72c8357e0d0ca5198d48e97b5650a6d2c'
+  homepage "http://www.nongnu.org/lzip/lzlib.html"
+  url "http://download.savannah.gnu.org/releases/lzip/lzlib/lzlib-1.6.tar.gz"
+  sha1 "4a24e4d17df3fd90f53866ace922c831f26600f6"
 
   bottle do
-    cellar :any
-    sha1 "31e2cdf3e199f746189eefcd0a5c0fa4bc69caba" => :mavericks
-    sha1 "ca854e06e248bf072272ffd723484c4254e0bc89" => :mountain_lion
-    sha1 "4b2b4abb16165cb0931b3ed197851410a2c46684" => :lion
   end
 
   def install
