fdb892375c24d87545dc6825d63e7428cb679ffb
Brett Koonce <koonce@gmail.com>
2014-09-21 18:09:12 -0700

    gsoap 2.8.18

--- Library/Formula/gsoap.rb
+++ Library/Formula/gsoap.rb
@@ -1,14 +1,14 @@
-require 'formula'
+require "formula"
 
 class Gsoap < Formula
-  homepage 'http://www.cs.fsu.edu/~engelen/soap.html'
-  url 'https://downloads.sourceforge.net/project/gsoap2/gSOAP/gsoap_2.8.17.zip'
-  sha1 'd6c483ea2eabade138d71d005300be909be9a274'
+  homepage "http://www.cs.fsu.edu/~engelen/soap.html"
+  url "https://downloads.sourceforge.net/project/gsoap2/gSOAP/gsoap_2.8.18.zip"
+  sha1 "672d81f1b15eb64f2b55f2ba3217be43ae3b197a"
 
   def install
     ENV.deparallelize
-    system './configure', "--prefix=#{prefix}"
-    system 'make install'
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
   end
 
   test do
