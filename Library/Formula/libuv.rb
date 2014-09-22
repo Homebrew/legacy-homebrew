e9baa9c2e50eee7cafaeb67e2f005f9499e79bc5
Jack Nagel <jacknagel@gmail.com>
2014-09-22 00:21:18 -0500

    libuv: fix devel version

--- Library/Formula/libuv.rb
+++ Library/Formula/libuv.rb
@@ -23,7 +23,8 @@ class Libuv < Formula
 
   devel do
     url "https://github.com/joyent/libuv/archive/v1.0.0-rc1.tar.gz"
-    sha1 '5be6817b15980b0453b3da3dafb4beea16e90d8f'
+    sha1 "5be6817b15980b0453b3da3dafb4beea16e90d8f"
+    version "1.0.0-rc1"
 
     depends_on "pkg-config" => :build
     depends_on "automake" => :build
