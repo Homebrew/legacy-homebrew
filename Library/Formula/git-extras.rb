d019b3e1603e142ac6dbb470c9e1c9e252b59575
Brett Koonce <koonce@gmail.com>
2014-09-22 20:49:24 -0700

    git-extras 2.0.0

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class GitExtras < Formula
-  homepage 'https://github.com/visionmedia/git-extras'
-  url 'https://github.com/visionmedia/git-extras/archive/1.9.1.tar.gz'
-  sha1 '145124710f2f06f17984e335c8b840c90c43eb44'
+  homepage "https://github.com/visionmedia/git-extras"
+  url "https://github.com/visionmedia/git-extras/archive/2.0.0.tar.gz"
+  sha1 "1cb12caffc76d3285d60a3b2b169e3ce6040998c"
 
-  head 'https://github.com/visionmedia/git-extras.git', :branch => 'master'
+  head "https://github.com/visionmedia/git-extras.git", :branch => "master"
 
   bottle do
     cellar :any
@@ -15,10 +15,10 @@ class GitExtras < Formula
   end
 
   # Don't take +x off these files
-  skip_clean 'bin'
+  skip_clean "bin"
 
   def install
-    inreplace 'Makefile', %r|\$\(DESTDIR\)(?=/etc/bash_completion\.d)|, '$(DESTDIR)$(PREFIX)'
+    inreplace "Makefile", %r|\$\(DESTDIR\)(?=/etc/bash_completion\.d)|, "$(DESTDIR)$(PREFIX)"
     system "make", "PREFIX=#{prefix}", "install"
   end
 end
