ba8ac3001bcce44a25d590082199d10a90c1aa6d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-23 04:59:59 +0100

    horndis: update 6 bottle.
    Closes #32534.

--- Library/Formula/horndis.rb
+++ Library/Formula/horndis.rb
@@ -7,9 +7,9 @@ class Horndis < Formula
 
   bottle do
     cellar :any
-    sha1 "a62c0ae0ac89d307437735c910db3ad5a2118683" => :mavericks
-    sha1 "c8109415f6ab8ecede83c9614ed561b3072343aa" => :mountain_lion
-    sha1 "e5537e2ba72e129c846d962ab8b45c44878bab5a" => :lion
+    sha1 "e37bc2997a594e67eaeac3c0f8d1ff5f51da7d99" => :mavericks
+    sha1 "1fb6c9910e96a67f34d48260d3d41ebeefb1749a" => :mountain_lion
+    sha1 "af083937c77de9d628d9cf6858296be61846ad63" => :lion
   end
 
   depends_on :xcode => :build
