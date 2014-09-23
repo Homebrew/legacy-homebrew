765c7125ee6adf8474bbbd97f0ea66cd44a83bf8
Phillip Ridlen <phillip@ovenbits.com>
2014-09-23 07:32:45 -0500

    moreutils 0.52
    Closes #32549.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/moreutils.rb
+++ Library/Formula/moreutils.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Moreutils < Formula
   homepage 'http://packages.debian.org/unstable/utils/moreutils'
-  url 'http://mirrors.kernel.org/debian/pool/main/m/moreutils/moreutils_0.51.tar.gz'
-  mirror 'http://ftp.us.debian.org/debian/pool/main/m/moreutils/moreutils_0.51.tar.gz'
-  sha1 '374b8c3bea962bbcde4a8158051c570a1fec6811'
+  url 'http://mirrors.kernel.org/debian/pool/main/m/moreutils/moreutils_0.52.tar.gz'
+  mirror 'http://ftp.us.debian.org/debian/pool/main/m/moreutils/moreutils_0.52.tar.gz'
+  sha1 '32047f935178b490a12c370d8f695f1273dc5895'
 
   bottle do
     revision 1
