ecf1708e3704df4de2235a1f22b34284315529c8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-23 01:48:59 -0500

    libxml++: update 2.36.0 bottle.

--- Library/Formula/libxml++.rb
+++ Library/Formula/libxml++.rb
@@ -6,6 +6,9 @@ class Libxmlxx < Formula
   sha256 "bfdf327bf9ebd12946b7aa6a152045f209d5c9fecd06ebfcdf9b3e7c1af6e2e1"
 
   bottle do
+    sha1 "3a2d850b88b4552b0466250211ea40b5b3816a64" => :mavericks
+    sha1 "ca11491cc3bee2d51184b57bebae0479616eaf4b" => :mountain_lion
+    sha1 "6fe9b9176b1ad77c409d3098eaa9ef4fbefef6b6" => :lion
   end
 
   depends_on "pkg-config" => :build
