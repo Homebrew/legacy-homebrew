fe28fc06cd1d47ba94b0b1677683741953b0fd86
Jack Nagel <jacknagel@gmail.com>
2014-09-23 02:12:29 -0500

    gtk-doc 1.20

--- Library/Formula/gtk-doc.rb
+++ Library/Formula/gtk-doc.rb
@@ -1,25 +1,25 @@
-require 'formula'
+require "formula"
 
 class GtkDoc < Formula
-  homepage 'http://www.gtk.org/gtk-doc/'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/1.18/gtk-doc-1.18.tar.bz2'
-  sha256 'a634d2e93d70468237033c06a17c97f29cf71a35ac5cc01c016324c965d42f73'
+  homepage "http://www.gtk.org/gtk-doc/"
+  url "http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/1.20/gtk-doc-1.20.tar.xz"
+  sha256 "3e6ecf134dbf92a74c24d79848fea3a48e59ab95408a38c6405905d95a293011"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gnome-doc-utils' => :build
-  depends_on 'gettext'
-  depends_on 'glib'
-  depends_on 'docbook'
-  depends_on 'docbook-xsl'
-  depends_on 'libxml2' => 'with-python'
+  depends_on "pkg-config" => :build
+  depends_on "gnome-doc-utils" => :build
+  depends_on "itstool" => :build
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "docbook"
+  depends_on "docbook-xsl"
+  depends_on "libxml2" => "with-python"
 
   def install
-    ENV.append_path 'PYTHONPATH', HOMEBREW_PREFIX/"opt/libxml2/lib/python2.7/site-packages"
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--disable-scrollkeeper",
                           "--with-xml-catalog=#{etc}/xml/catalog"
     system "make"
     system "make install"
