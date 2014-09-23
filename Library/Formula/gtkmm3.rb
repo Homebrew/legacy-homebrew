2baccdfd86c75f29c412c2170d3503bab9914e6d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-23 17:33:52 +0100

    gtkmm3: add 3.14.0 bottle.

--- Library/Formula/gtkmm3.rb
+++ Library/Formula/gtkmm3.rb
@@ -5,6 +5,12 @@ class Gtkmm3 < Formula
   url "http://ftp.gnome.org/pub/GNOME/sources/gtkmm/3.14/gtkmm-3.14.0.tar.xz"
   sha256 "d9f528a62c6ec226fa08287c45c7465b2dce5aae5068e9ac48d30a64a378e48b"
 
+  bottle do
+    sha1 "6d2d49346ac6e7ca5dab76033fbeec1bf54f3af6" => :mavericks
+    sha1 "d87095c7c296f7f5272634230bcfa83dd97c35ba" => :mountain_lion
+    sha1 "205e4f15c2fce7060ef38eac5bcb4c9cc42d5d72" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glibmm"
   depends_on "gtk+3"
