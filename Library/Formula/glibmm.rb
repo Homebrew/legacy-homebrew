ed384395a120bd879a907164586bee278cad9e2f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-23 00:20:56 -0500

    glibmm: update 2.42.0 bottle.

--- Library/Formula/glibmm.rb
+++ Library/Formula/glibmm.rb
@@ -6,6 +6,9 @@ class Glibmm < Formula
   sha256 "985083d97378d234da27a7243587cc0d186897a4b2d3c1286f794089be1a3397"
 
   bottle do
+    sha1 "dfd9e07af7ab9d12f4da8fdfa2022c98681c0868" => :mavericks
+    sha1 "8b1708d725d18c132d3096d5c967999d20840cf1" => :mountain_lion
+    sha1 "4bfe515499a03792b9807ac4ea953ed552131bb7" => :lion
   end
 
   depends_on "pkg-config" => :build
