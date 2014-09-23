ab4a7ec21233678e64a09c2d721faf1fb11524ac
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-23 02:12:29 -0500

    gtk-doc: add 1.20 bottle.

--- Library/Formula/gtk-doc.rb
+++ Library/Formula/gtk-doc.rb
@@ -5,6 +5,12 @@ class GtkDoc < Formula
   url "http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/1.20/gtk-doc-1.20.tar.xz"
   sha256 "3e6ecf134dbf92a74c24d79848fea3a48e59ab95408a38c6405905d95a293011"
 
+  bottle do
+    sha1 "be13f6a01fed97680dfce7c18704885d6cb7ed8f" => :mavericks
+    sha1 "48b5f384aeebd39494794f46ed801f35f5bcfb6e" => :mountain_lion
+    sha1 "0f88dda0c51d73acebc3b2ec1513f67ff7d5c87c" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gnome-doc-utils" => :build
   depends_on "itstool" => :build
