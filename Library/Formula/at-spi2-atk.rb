f2961c935411adf826738395be3c52abb69b5209
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-23 00:20:56 -0500

    at-spi2-atk: update 2.14.0 bottle.

--- Library/Formula/at-spi2-atk.rb
+++ Library/Formula/at-spi2-atk.rb
@@ -6,6 +6,10 @@ class AtSpi2Atk < Formula
   sha256 "56b40ef16d9f1b1630d32addb0cc941372a1e97d8ddafd369f912c7d125688e7"
 
   bottle do
+    cellar :any
+    sha1 "f11701fff9c808d0be86ab0245b7b166e95cf371" => :mavericks
+    sha1 "a2cb17226b9d03779b67f52c42c5668ff3d4e6b2" => :mountain_lion
+    sha1 "690f4136be39fb82537dab0dcb090c87ddd7177d" => :lion
   end
 
   depends_on "pkg-config" => :build
