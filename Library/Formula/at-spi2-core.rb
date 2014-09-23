da03f96a4dacaef02b319d63408e70574e6f1029
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-23 00:20:56 -0500

    at-spi2-core: update 2.14.0 bottle.

--- Library/Formula/at-spi2-core.rb
+++ Library/Formula/at-spi2-core.rb
@@ -6,6 +6,9 @@ class AtSpi2Core < Formula
   sha256 "5807b02e6ef695b52fde7ff26d675dd94a0707df3c42fe7fd224828e092514c8"
 
   bottle do
+    sha1 "81d698ef6592a3dc2f1e34d2fc895d81cd79336b" => :mavericks
+    sha1 "d5513c28dc9326b161840deeb0c148a2d1f3bcd3" => :mountain_lion
+    sha1 "cd3260c7814275528333bfe4cf1e56a7d920cdf8" => :lion
   end
 
   depends_on "pkg-config" => :build
