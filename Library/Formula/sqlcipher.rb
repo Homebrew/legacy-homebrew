8cfeea785df6dbb348ee4bc9b55926710ffa7102
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-09-24 07:12:20 +0100

    Sqlcipher: fts option & openssl dep

--- Library/Formula/sqlcipher.rb
+++ Library/Formula/sqlcipher.rb
@@ -2,16 +2,31 @@ require "formula"
 
 class Sqlcipher < Formula
   homepage "http://sqlcipher.net"
+  head "https://github.com/sqlcipher/sqlcipher.git"
   url "https://github.com/sqlcipher/sqlcipher/archive/v3.1.0.tar.gz"
   sha1 "350a7cbc01690825cb6beca3b9bf6943f71a52de"
 
-  head "https://github.com/sqlcipher/sqlcipher.git"
+  option 'with-fts', 'Build with full-text search enabled'
+
+  depends_on "openssl"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--enable-tempstore=yes",
-                          "CFLAGS=-DSQLITE_HAS_CODEC", "LDFLAGS=-lcrypto",
-                          "--disable-tcl"
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-tempstore=yes
+      --with-crypto-lib=#{Formula["openssl"].opt_prefix}
+      --disable-tcl
+    ]
+
+    if build.with?("fts")
+      args << "CFLAGS=-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_FTS3"
+    else
+      args << "CFLAGS=-DSQLITE_HAS_CODEC"
+    end
+
+    system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
