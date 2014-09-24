ab148e4f44e5192abce317362e66bbe5d673872a
chapmanb <chapmanb@50mail.com>
2014-09-24 13:46:29 -0400

    cmake: Avoid python dependency for Linuxbrew
    On Linux systems, with Linuxbrew, the current cmake build recipe will pull in
    Python and a large number of other system packages that Python depend on. This
    can cause some bad library dependency issues. This small fix skips the Python
    install on these systems. Moved from Homebrew main recipe by request.
    See Homebrew/homebrew#32601

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -34,7 +34,7 @@ class Cmake < Formula
     sha1 "842240c9febb4123918cf62a3cea5ca4207ad860" => :lion
   end
 
-  depends_on :python => :build if MacOS.version <= :snow_leopard
+  depends_on :python => :build if OS.mac? && MacOS.version <= :snow_leopard
 
   resource "sphinx" do
     url "https://pypi.python.org/packages/source/S/Sphinx/Sphinx-1.2.3.tar.gz"
