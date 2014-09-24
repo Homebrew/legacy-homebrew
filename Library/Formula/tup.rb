7c63078b9cad4ba2e9f1183e853823df33ae3bf4
Brett Koonce <koonce@gmail.com>
2014-09-23 18:30:17 -0700

    tup 0.7.3

--- Library/Formula/tup.rb
+++ Library/Formula/tup.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Tup < Formula
-  homepage 'http://gittup.org/tup/'
-  url 'https://github.com/gittup/tup/archive/v0.7.2.tar.gz'
-  sha1 'c0b14a9b7a59c6295ed7339883b21d0f1a8163b3'
-  head 'https://github.com/gittup/tup.git'
+  homepage "http://gittup.org/tup/"
+  url "https://github.com/gittup/tup/archive/v0.7.3.tar.gz"
+  sha1 "e817ab1888b8aab7f784efa7fb90e91c216fae8d"
+  head "https://github.com/gittup/tup.git"
 
   bottle do
     cellar :any
@@ -13,14 +13,14 @@ class Tup < Formula
     sha1 "98255d59e38f854319a60091037fa0f605bcde1f" => :lion
   end
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :osxfuse
 
   def install
-    ENV['TUP_LABEL'] = version
+    ENV["TUP_LABEL"] = version
     system "./build.sh"
-    bin.install 'build/tup'
-    man1.install 'tup.1'
+    bin.install "build/tup"
+    man1.install "tup.1"
   end
 
   test do
