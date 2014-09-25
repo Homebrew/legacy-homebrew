46d6466e04523ccfbaa57b7d939e521d30ad1a9d
theirix <theirix@gmail.com>
2014-09-25 14:45:43 +0400

    Updated to version 0.1.2, added test, fixed quotes

--- Library/Formula/snappystream.rb
+++ Library/Formula/snappystream.rb
@@ -2,17 +2,23 @@ require "formula"
 
 class Snappystream < Formula
   homepage "https://github.com/hoxnox/snappystream"
-  url "https://github.com/hoxnox/snappystream/archive/0.1.tar.gz"
-  sha1 "50d97c8d9899fc63d3ff386d40779f400c912bb4"
+  url "https://github.com/hoxnox/snappystream/archive/0.1.2.tar.gz"
+  sha1 "40ceac66a58659d1827cc7375a72210bb84be9b3"
 
-  head 'https://github.com/hoxnox/snappystream.git'
+  head "https://github.com/hoxnox/snappystream.git"
+
+  option "without-check", "Skip build-time tests (not recommended)"
 
   depends_on "cmake" => :build
-  depends_on 'snappy'
+  depends_on "snappy"
 
   def install
-    system "cmake", ".", *std_cmake_args
+    args = std_cmake_args
+    args << "-DBUILD_TESTS=ON" if build.with? "check"
+
+    system "cmake", ".", *args
+    system "make"
+    system "make", "test" if build.with? "check"
     system "make", "install"
   end
-
 end
