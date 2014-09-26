1ff9048b45bd6a114bf809d26e308688ba04c78c
Shaun Jackman <sjackman@gmail.com>
2014-09-26 13:04:12 -0700

    linux-headers: add 3.15.9 bottle.

--- Library/Formula/linux-headers.rb
+++ Library/Formula/linux-headers.rb
@@ -5,6 +5,11 @@ class LinuxHeaders < Formula
   url "https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.15.9.tar.gz"
   sha1 "068aefd8d7a5ef62e152d6d45228c53bdec4a2cc"
 
+  bottle do
+    cellar :any
+    sha1 "69c321f2cfca43e39891d34b6b088b4b786007e6" => :"x86_64-linux"
+  end
+
   def install
     system "make", "headers_install", "INSTALL_HDR_PATH=#{prefix}"
     rm Dir[prefix/"**/{.install,..install.cmd}"]
