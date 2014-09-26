73e2631f8394c7f6e4ad89b17091fa14759af7ce
Jack Nagel <jacknagel@gmail.com>
2014-09-26 17:36:54 -0500

    bash 4.3.26
    Addresses CVE-2014-7169.

--- Library/Formula/bash.rb
+++ Library/Formula/bash.rb
@@ -8,21 +8,18 @@ class Bash < Formula
 
     mirror "http://ftp.gnu.org/gnu/bash/bash-4.3.tar.gz"
     sha256 "afc687a28e0e24dc21b988fa159ff9dbcf6b7caa92ade8645cc6d5605cd024d4"
-    version "4.3.25"
+    version "4.3.26"
 
     # Vendor the patches. The mirrors are unreliable for getting the patches,
     # and the more patches there are, the more unreliable they get. Upstream
     # patches can be found in: http://git.savannah.gnu.org/cgit/bash.git
     patch do
-      url "https://gist.githubusercontent.com/jacknagel/c1cf23775c774e2b4b6d/raw/a3132f06e6389224f9a27feeb3e143d9d0a3af72/bash-4.3.25.diff"
-      sha1 "6c0484e6230c213dcbd0f2beebc1594ad83fa518"
+      url "https://gist.githubusercontent.com/jacknagel/c1cf23775c774e2b4b6d/raw/2007f21e5c33fbc361afde8ab9d083498fcdc200/bash-4.3.26.diff"
+      sha1 "969270bdc2b57062cde3d0546dbc950bcaffe19a"
     end
   end
 
   bottle do
-    sha1 "bc2656046a3997f7d27fb97f9a2ee9567b57f8c5" => :mavericks
-    sha1 "8c1cb58eac01c5387776afcfde3ed3411fc2120c" => :mountain_lion
-    sha1 "2d8b02fe9a0ae98437b1752e073dbccbbe835de0" => :lion
   end
 
   head "git://git.savannah.gnu.org/bash.git"
