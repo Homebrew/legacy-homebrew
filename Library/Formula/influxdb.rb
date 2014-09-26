a6ed679f4f3ea5da87b5aa2a37ede1527a13df37
Todd Persen <todd.persen@gmail.com>
2014-09-26 18:05:42 -0400

    influxdb 0.8.3

--- Library/Formula/influxdb.rb
+++ Library/Formula/influxdb.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Influxdb < Formula
   homepage "http://influxdb.org"
-  url "http://get.influxdb.org/influxdb-0.7.3.src.tar.gz"
-  sha1 "cc774498036a758661965eb01c45c1941bf0ac10"
+  url "http://get.influxdb.org/with_dependencies/influxdb-0.8.3.src.tar.gz"
+  sha1 "2626bc5e8877ab23db3b121b64f4c5f9c8a2b627"
 
   bottle do
     sha1 "eec1070cf8165525e350ce00c07802a16260a44c" => :mavericks
@@ -12,31 +12,43 @@ class Influxdb < Formula
   end
 
   depends_on "leveldb"
+  depends_on "autoconf" => :build
   depends_on "protobuf" => :build
   depends_on "bison" => :build
   depends_on "flex" => :build
   depends_on "go" => :build
+  depends_on "gawk" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
+    ENV["GOPATH"] = buildpath.parent
+    Dir.chdir File.join(buildpath, "github.com", "influxdb", "influxdb")
 
     flex = Formula["flex"].bin/"flex"
     bison = Formula["bison"].bin/"bison"
 
     system "./configure", "--with-flex=#{flex}", "--with-bison=#{bison}"
-    system "make", "dependencies", "protobuf", "parser"
-    system "go", "build", "daemon"
+
+    system "make", "protobuf"
+    system "make", "parser"
+
+    inreplace "daemon/version.go" do |s|
+      s.gsub! /^(const version).*$/, %Q{\\1 = "0.8.3"}
+      s.gsub! /^(const gitSha).*$/, %Q{\\1 = "fbf9a474055051c64e947f2a071388ee009a08d5"}
+    end
+
+    system "go", "build", "-o", "influxdb", "github.com/influxdb/influxdb/daemon"
 
     inreplace "config.sample.toml" do |s|
       s.gsub! "/tmp/influxdb/development/db", "#{var}/influxdb/data"
       s.gsub! "/tmp/influxdb/development/raft", "#{var}/influxdb/raft"
       s.gsub! "/tmp/influxdb/development/wal", "#{var}/influxdb/wal"
+      s.gsub! "influxdb.log", "#{var}/influxdb/logs/influxdb.log"
       s.gsub! "./admin", "#{opt_share}/admin"
     end
 
-    bin.install "daemon" => "influxdb"
+    bin.install "influxdb" => "influxdb"
     etc.install "config.sample.toml" => "influxdb.conf"
-    share.install "admin"
+    share.install "admin-ui" => "admin"
 
     (var/"influxdb/data").mkpath
     (var/"influxdb/raft").mkpath
