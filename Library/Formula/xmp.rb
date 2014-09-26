025bb43e074b459c1ec746e20793e42f2b072dab
Misty De Meo <mistydemeo@gmail.com>
2014-09-26 10:28:26 -0700

    xmp 4.0.9

--- Library/Formula/xmp.rb
+++ Library/Formula/xmp.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Xmp < Formula
   homepage "http://xmp.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/xmp/xmp/4.0.8/xmp-4.0.8.tar.gz"
-  sha1 "8ea2ba59a9f93ccca904513f374a62f6e450829b"
+  url "https://downloads.sourceforge.net/project/xmp/xmp/4.0.9/xmp-4.0.9.tar.gz"
+  sha1 "f22871d5e5be1a0b08d11a0b3bd28f6723a69ee3"
 
   head do
     url "git://git.code.sf.net/p/xmp/xmp-cli"
