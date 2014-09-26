1e6e850675ed43a0963be1f635b04310cd2be438
Juhani Simola <jsimola@localhost>
2014-09-26 23:51:51 +0300

    libvoikko 3.7.1
    http://voikko.puimula.org/releases.html
    
    Closes #32690.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libvoikko.rb
+++ Library/Formula/libvoikko.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Libvoikko < Formula
-  homepage 'http://voikko.puimula.org/'
-  url 'http://www.puimula.org/voikko-sources/libvoikko/libvoikko-3.7.tar.gz'
-  sha1 '27ad3f72316d3878a0ed7b94a9e855bff66cb81b'
+  homepage "http://voikko.puimula.org/"
+  url "http://www.puimula.org/voikko-sources/libvoikko/libvoikko-3.7.1.tar.gz"
+  sha1 "b6d7ec669bbc33ba5f526f53b1d297f9ee315178"
 
   bottle do
     cellar :any
@@ -12,17 +12,8 @@ class Libvoikko < Formula
     sha1 "2fa799be5ce26948edc2c5c4c7e74c181aee9dc6" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'suomi-malaga-voikko'
-
-  # Fixes compilation issues on OS X 10.9. Both merged upstream:
-  # https://github.com/voikko/corevoikko/pull/5
-  # https://github.com/voikko/corevoikko/pull/6
-  # Note that the upstream commits don't apply cleanly to stable
-  patch do
-    url "https://gist.githubusercontent.com/osimola/7724611/raw/2dcfddaf4bf7c7e9d940edb1b982d5b5e39bc378/libvoikko-3.7-mavericks.patch"
-    sha1 "532d0a3097569c966b2632a5b876d9082b4a2c49"
-  end
+  depends_on "pkg-config" => :build
+  depends_on "suomi-malaga-voikko"
 
   def install
     system "./configure", "--disable-debug",
