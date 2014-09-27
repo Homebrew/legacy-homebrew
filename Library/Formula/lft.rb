8213c114b97c22acf244114e9583d0e5f35d0b5d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-27 19:18:57 +0100

    lft: add 3.73 bottle.

--- Library/Formula/lft.rb
+++ Library/Formula/lft.rb
@@ -5,6 +5,13 @@ class Lft < Formula
   url "http://pwhois.org/dl/index.who?file=lft-3.73.tar.gz"
   sha1 "c5a37bef74d7466d5202ee8136acc3213711252e"
 
+  bottle do
+    cellar :any
+    sha1 "ff1a651b0544691f535bd22199530ea4b53093ba" => :mavericks
+    sha1 "9d04de7da326ee34f836f845bfbae9c6a7b18586" => :mountain_lion
+    sha1 "9e3320656b5181236b9c24de6693de07f47290f8" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
