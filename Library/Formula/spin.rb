c1c6912776f14fc5a4750d2cb7abb06e14d7cfe1
Brett Koonce <koonce@gmail.com>
2014-09-27 10:54:57 -0700

    spin 6.4.0

--- Library/Formula/spin.rb
+++ Library/Formula/spin.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Spin < Formula
   homepage "http://spinroot.com/spin/whatispin.html"
-  url "http://spinroot.com/spin/Src/spin632.tar.gz"
-  version "6.3.2"
-  sha1 "835f4d77e9f4c1c4c697b0d548606a579bd4b5df"
+  url "http://spinroot.com/spin/Src/spin640.tar.gz"
+  version "6.4.0"
+  sha1 "d777255a09970ffdb25b62098c0cf22df4bfeab4"
 
   fails_with :llvm do
     build 2334
