35bf4a3bdbfc14bdcfdd6da363b7f9adce368af3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-27 19:18:57 +0100

    solr: add 4.10.0 bottle.

--- Library/Formula/solr.rb
+++ Library/Formula/solr.rb
@@ -5,6 +5,13 @@ class Solr < Formula
   url "http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.10.0/solr-4.10.0.tgz"
   sha1 "ae47a89f35b5e2a6a4e55732cccc64fb10ed9779"
 
+  bottle do
+    cellar :any
+    sha1 "9c3fe919fd112549b5fbac7b4715a6bcf209b88d" => :mavericks
+    sha1 "ad64b5fe509d2f9da244a52f13d9525259604e2f" => :mountain_lion
+    sha1 "82bc81371673e7e9e2cafae5d9914dd46ec64e84" => :lion
+  end
+
   def script; <<-EOS.undent
     #!/bin/sh
     if [ -z "$1" ]; then
