026b1912fa457c37f6d2f8718523dc29bdcb5c41
Nathan Toone <nathan@toonetown.com>
2014-09-27 17:59:18 -0600

    Add rmate command line utility

---
+++ Library/Formula/rmate.rb
@@ -0,0 +1,12 @@
+require "formula"
+
+class Rmate < Formula
+  homepage "https://github.com/textmate/rmate"
+  url "https://raw.github.com/textmate/rmate/master/bin/rmate"
+  version "1.5.7"
+  sha1 "899e71af9b4e340acca6848cdaffe01a59bb62c0"
+
+  def install
+    bin.install "rmate"
+  end
+end
