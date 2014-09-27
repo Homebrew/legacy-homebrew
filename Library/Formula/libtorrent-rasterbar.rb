79eed07221ce2250791af454c05185a829522574
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-27 07:02:31 +0100

    libtorrent-rasterbar: update 1.0.2_1 bottle.

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -15,9 +15,9 @@ class LibtorrentRasterbar < Formula
 
   bottle do
     cellar :any
-    sha1 "969616cddd409eec6d15d59dcf91cf71d8860d7a" => :mavericks
-    sha1 "8f4d2b6d1cd95bf981afcf9b0f6459149a266cd8" => :mountain_lion
-    sha1 "328c41126de1a7e6dd660ce04bb7c6c154b0cd64" => :lion
+    sha1 "ab6d57a760ab449140fe1343089884e5f7a77c9f" => :mavericks
+    sha1 "6561ef265172991b558e0e13c34b1f3e7fddb253" => :mountain_lion
+    sha1 "72e50533cf48186ca1442c0bbbb5a7d2e6049aee" => :lion
   end
 
   depends_on "pkg-config" => :build
