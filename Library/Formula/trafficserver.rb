1e21f1fbc05a3392db305c7e32c0ac0a7ae10d4b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-27 19:18:57 +0100

    trafficserver: add 4.2.2 bottle.
    Closes #32727.

--- Library/Formula/trafficserver.rb
+++ Library/Formula/trafficserver.rb
@@ -6,6 +6,12 @@ class Trafficserver < Formula
   mirror "http://archive.apache.org/dist/trafficserver/trafficserver-4.2.2.tar.bz2"
   sha1 "8c862f0402f278eb8bca1678b0db0c4fcb0dd565"
 
+  bottle do
+    sha1 "e073851a656cec3a052c9dab8ce7b7658b11dccc" => :mavericks
+    sha1 "9f76a7d1b206220a47a60144f9844f6f30f2921b" => :mountain_lion
+    sha1 "73736e9882f28d2265e923fbb1f5830f3d462bd6" => :lion
+  end
+
   depends_on "pcre"
 
   #remove some amd64 compiler options that fail on Snow Leopard
