a4c48aa6fab44824b57c109218a241d2732f22cd
Brett Koonce <koonce@gmail.com>
2014-09-27 10:53:59 -0700

    dnstop 20140915

--- Library/Formula/dnstop.rb
+++ Library/Formula/dnstop.rb
@@ -1,15 +1,15 @@
-require 'formula'
+require "formula"
 
 class Dnstop < Formula
-  homepage 'http://dns.measurement-factory.com/tools/dnstop/index.html'
-  url 'http://dns.measurement-factory.com/tools/dnstop/src/dnstop-20121017.tar.gz'
-  sha1 '836d9bc118df539b80eb349ca45c946323b13366'
+  homepage "http://dns.measurement-factory.com/tools/dnstop/index.html"
+  url "http://dns.measurement-factory.com/tools/dnstop/src/dnstop-20140915.tar.gz"
+  sha1 "af1567d6b53e8be697b884508a2a3a0edbea5e01"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    bin.install 'dnstop'
-    man8.install 'dnstop.8'
+    bin.install "dnstop"
+    man8.install "dnstop.8"
   end
 
 end
