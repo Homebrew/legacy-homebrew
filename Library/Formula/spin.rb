30369389673c03fc43d333cf5171af6100c2506d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-27 19:18:57 +0100

    spin: add 6.4.0 bottle.

--- Library/Formula/spin.rb
+++ Library/Formula/spin.rb
@@ -6,6 +6,13 @@ class Spin < Formula
   version "6.4.0"
   sha1 "d777255a09970ffdb25b62098c0cf22df4bfeab4"
 
+  bottle do
+    cellar :any
+    sha1 "388caba593c8f6224513d16b7a307358e5745ec8" => :mavericks
+    sha1 "7979b9dc5722eaeebb8e2e98e4a4d0495bedc775" => :mountain_lion
+    sha1 "bd24c1f818b6460e90dada5adc75aa58ef0d5c4c" => :lion
+  end
+
   fails_with :llvm do
     build 2334
   end
