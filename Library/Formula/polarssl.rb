85458e70554d9b7532ab1cdb0b2181addb4a8451
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-28 23:15:12 +0100

    polarssl: add 1.3.8 bottle.

--- Library/Formula/polarssl.rb
+++ Library/Formula/polarssl.rb
@@ -7,6 +7,13 @@ class Polarssl < Formula
 
   head "https://github.com/polarssl/polarssl.git"
 
+  bottle do
+    cellar :any
+    sha1 "d4b03d57816b809d3ed454f338f36c4c586a6c8b" => :mavericks
+    sha1 "921a0256ff3be3e310a9a9d835341a01f9f5a46e" => :mountain_lion
+    sha1 "a2cf94a5c84fade311821c16382777b900c8c272" => :lion
+  end
+
   depends_on "cmake" => :build
 
   conflicts_with "md5sha1sum", :because => "both install conflicting binaries"
