f9d0003ddc451497794abe82634976b4cd768de9
hymced <haveyoumetced@gmail.com>
2014-09-28 23:15:05 +0200

    Update eye-d3.rb

--- Library/Formula/eye-d3.rb
+++ Library/Formula/eye-d3.rb
@@ -5,11 +5,6 @@ class EyeD3 < Formula
   url 'http://eyed3.nicfit.net/releases/eyeD3-0.7.5.tgz'
   sha1 'bcfd0fe14f5fa40f29ca7e7133138a5112f3c270'
   def install
-    # Manually process this file
-    inreplace "src/eyeD3/__init__.py" do |s|
-      s.change_make_var! "eyeD3Version", "\"#{version}\""
-      s.change_make_var! "eyeD3Maintainer", "\"Packaged by Homebrew\""
-    end
 
     libexec.install "src/eyeD3"
     libexec.install "bin/eyeD3" => "eyeD3_script"
