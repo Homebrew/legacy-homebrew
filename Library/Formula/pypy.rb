822d41b1076f65a2b52ff66de772dd66d15bc273
Tim D. Smith <git@tim-smith.us>
2014-09-28 09:59:46 -0700

    pypy: translate, build, and bottle

--- Library/Formula/pypy.rb
+++ Library/Formula/pypy.rb
@@ -2,16 +2,17 @@ require "formula"
 
 class Pypy < Formula
   homepage "http://pypy.org/"
-  url "https://bitbucket.org/pypy/pypy/downloads/pypy-2.4.0-osx64.tar.bz2"
-  sha1 "aa7f9b41d8bfda16239b629cd1b8dc884c2ad808"
-  version "2.4.0"
-  revision 1
+  url "https://bitbucket.org/pypy/pypy/downloads/pypy-2.4.0-src.tar.bz2"
+  sha1 "e2e0bcf8457c0ae5a24f126a60aa921dabfe60fb"
+  revision 2
 
   depends_on :arch => :x86_64
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
 
   resource "setuptools" do
-    url "https://pypi.python.org/packages/source/s/setuptools/setuptools-5.8.tar.gz"
-    sha1 "21e2ee6a61c4f71711956fa99803283f0168041a"
+    url "https://pypi.python.org/packages/source/s/setuptools/setuptools-6.0.2.tar.gz"
+    sha1 "a29a81b7913151697cb15b069844af75d441408f"
   end
 
   resource "pip" do
@@ -19,21 +20,38 @@ class Pypy < Formula
     sha1 "e6cd9e6f2fd8d28c9976313632ef8aa8ac31249e"
   end
 
+  # https://bugs.launchpad.net/ubuntu/+source/gcc-4.2/+bug/187391
+  fails_with :gcc
+
   def install
     # Having PYTHONPATH set can cause the build to fail if another
     # Python is present, e.g. a Homebrew-provided Python 2.x
     # See https://github.com/Homebrew/homebrew/issues/24364
     ENV["PYTHONPATH"] = ""
+    ENV["PYPY_USESSION_DIR"] = buildpath
 
-    rmtree "site-packages"
+    Dir.chdir "pypy/goal" do
+      system "python", buildpath/"rpython/bin/rpython",
+             "-Ojit", "--shared", "--cc", ENV["CC"], "--translation-verbose",
+             "--make-jobs", ENV.make_jobs, "targetpypystandalone.py"
+      system "install_name_tool", "-change", "libpypy-c.dylib", libexec/"lib/libpypy-c.dylib", "pypy-c"
+      system "install_name_tool", "-id", opt_libexec/"lib/libpypy-c.dylib", "libpypy-c.dylib"
+      (libexec/"bin").install "pypy-c" => "pypy"
+      (libexec/"lib").install "libpypy-c.dylib"
+    end
+
+    (libexec/"lib-python").install "lib-python/2.7"
+    libexec.install %w[include lib_pypy]
 
     # The PyPy binary install instructions suggest installing somewhere
     # (like /opt) and symlinking in binaries as needed. Specifically,
     # we want to avoid putting PyPy's Python.h somewhere that configure
     # scripts will find it.
-    libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/pypy"
+    lib.install_symlink libexec/"lib/libpypy-c.dylib"
+  end
 
+  def post_install
     # Post-install, fix up the site-packages and install-scripts folders
     # so that user-installed Python software survives minor updates, such
     # as going from 1.7.0 to 1.7.1.
@@ -46,37 +64,36 @@ class Pypy < Formula
 
     # Tell distutils-based installers where to put scripts
     scripts_folder.mkpath
-    (distutils+"distutils.cfg").write <<-EOF.undent
+    (distutils+"distutils.cfg").atomic_write <<-EOF.undent
       [install]
       install-scripts=#{scripts_folder}
     EOF
 
-    # Install setuptools. The user can then do:
-    # $ easy_install pip
-    # $ pip install --upgrade setuptools
-    # to get newer versions of setuptools outside of Homebrew.
     resource("setuptools").stage { system "#{libexec}/bin/pypy", "setup.py", "install" }
     resource("pip").stage { system "#{libexec}/bin/pypy", "setup.py", "install" }
 
     # Symlinks to easy_install_pypy and pip_pypy
     bin.install_symlink scripts_folder/"easy_install" => "easy_install_pypy"
     bin.install_symlink scripts_folder/"pip" => "pip_pypy"
+
+    # post_install happens after linking
+    %w[easy_install_pypy pip_pypy].each { |e| (HOMEBREW_PREFIX/"bin").install_symlink bin/e }
   end
 
   def caveats; <<-EOS.undent
     A "distutils.cfg" has been written to:
       #{distutils}
-    specifing the install-scripts folder as:
+    specifying the install-scripts folder as:
       #{scripts_folder}
 
     If you install Python packages via "pypy setup.py install", easy_install_pypy,
-    pip_pypy, any provided scripts will go into the install-scripts folder above,
-    so you may want to add it to your PATH *after* the `$(brew --prefix)/bin`
+    or pip_pypy, any provided scripts will go into the install-scripts folder
+    above, so you may want to add it to your PATH *after* #{HOMEBREW_PREFIX}/bin
     so you don't overwrite tools from CPython.
 
-    Setuptools has been installed, so easy_install is available.
-    To update setuptools itself outside of Homebrew:
-        #{scripts_folder}/easy_install pip
+    Setuptools and pip have been installed, so you can use easy_install_pypy and
+    pip_pypy.
+    To update setuptools and pip between pypy releases, run:
         #{scripts_folder}/pip install --upgrade setuptools
 
     See: https://github.com/Homebrew/homebrew/wiki/Homebrew-and-Python
