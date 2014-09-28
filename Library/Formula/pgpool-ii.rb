6aa6974a33e6c3644d5eb58ddb5f433b18ab1393
Jack Nagel <jacknagel@gmail.com>
2014-09-27 19:48:26 -0500

    pgpool-ii 3.2.9

--- Library/Formula/pgpool-ii.rb
+++ Library/Formula/pgpool-ii.rb
@@ -2,16 +2,16 @@ require "formula"
 
 class PgpoolIi < Formula
   homepage "http://www.pgpool.net/mediawiki/index.php/Main_Page"
-  url "http://www.pgpool.net/download.php?f=pgpool-II-3.2.8.tar.gz"
-  sha1 "77ae7f3472294e33837670a45579c72b12e2f50b"
+  url "http://www.pgpool.net/download.php?f=pgpool-II-3.2.9.tar.gz"
+  sha1 "ce5bcaaa46ec3ed6da3aff630329e8ad15cc4ba8"
 
   depends_on :postgresql
 
   # Fix strlcpy conflict. Patch adapted from upstream commit:
   # http://git.postgresql.org/gitweb/?p=pgpool2.git;a=commit;h=95a874ce94abc2859f53ecbd005d3332db423b48
   patch do
-    url "https://gist.githubusercontent.com/jacknagel/10cef3c878cda8788b47/raw/e635efc28b63393aaea0aca896b898136849da07/pgpool.diff"
-    sha1 "b653936ba47a0cea88a8bb45cc462d628498a370"
+    url "https://gist.githubusercontent.com/jacknagel/10cef3c878cda8788b47/raw/f38694935674644baea95316c52b0027a2a3c17f/pgpool.diff"
+    sha1 "510bdfcee4b966dff26f338f33283c20522423e2"
   end
 
   def install
