0b9d89b3ba8925518e114754545e1a24d6112def
Dominyk Tiller <dominyktiller@gmail.com>
2014-09-28 18:51:46 +0100

    Lzo 2.08
    Version bump for Lzo. Fixes CVE-2014-4607.

--- Library/Formula/ht.rb
+++ Library/Formula/ht.rb
@@ -1,17 +1,18 @@
-require 'formula'
+require "formula"
 
 class Ht < Formula
-  homepage 'http://hte.sf.net/'
-  url 'https://downloads.sourceforge.net/project/hte/ht-source/ht-2.0.22.tar.bz2'
-  sha256 'c729d64bf7de440c7b1021d3d6657ccbdb103541b4082a58dca7c8402c773f58'
+  homepage "http://hte.sf.net/"
+  url "https://downloads.sourceforge.net/project/hte/ht-source/ht-2.0.22.tar.bz2"
+  sha256 "c729d64bf7de440c7b1021d3d6657ccbdb103541b4082a58dca7c8402c773f58"
+  revision 1
 
-  depends_on 'lzo'
+  depends_on "lzo"
 
   def install
     system "chmod +x ./install-sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-x11-textmode"
-    system "make install"
+    system "make", "install"
   end
 end
