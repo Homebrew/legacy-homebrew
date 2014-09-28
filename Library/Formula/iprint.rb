64868c40ec1d6d3c1b18ec53886fe90a859437ca
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-28 01:18:41 +0100

    iprint: add 1.3-9 bottle.
    Closes #32619.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/iprint.rb
+++ Library/Formula/iprint.rb
@@ -7,6 +7,13 @@ class Iprint < Formula
   sha1 "1b423107cf6b1e5c2257648047ff91f5c6ac2249"
   version "1.3-9"
 
+  bottle do
+    cellar :any
+    sha1 "05ed7e4f7ab1ecaa578b0660140ca6b802f29beb" => :mavericks
+    sha1 "93a78f64b8b51b89d9a37d94850a4347c37e343a" => :mountain_lion
+    sha1 "d8b7d305d24c836ae7ffb59033bf15edc812acb3" => :lion
+  end
+
   patch do
     url "http://ftp.debian.org/debian/pool/main/i/iprint/iprint_1.3-9.diff.gz"
     sha1 "6164e8e9bd0e08542de34bbf386cd1f0193f17c5"
