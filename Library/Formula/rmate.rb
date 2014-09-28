23526b1c0fb538ecb79b1b46b8560cbebb8cf9ae
Nathan Toone <nathan@toonetown.com>
2014-09-27 18:19:54 -0600

    Point to the versioned URL instead.

--- Library/Formula/rmate.rb
+++ Library/Formula/rmate.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Rmate < Formula
   homepage "https://github.com/textmate/rmate"
-  url "https://raw.github.com/textmate/rmate/master/bin/rmate"
+  url "https://raw.github.com/textmate/rmate/v1.5.7/bin/rmate"
   version "1.5.7"
   sha1 "899e71af9b4e340acca6848cdaffe01a59bb62c0"
 
