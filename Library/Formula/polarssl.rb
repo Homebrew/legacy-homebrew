30c0ebc49f075334e24ae351aa6128735a388733
Dominyk Tiller <dominyktiller@gmail.com>
2014-09-28 22:03:38 +0100

    Polarssl 1.3.8
    Version bump to 1.3.8 & new HEAD.
    
    I’ve also stumbled across one of the strangest conflicts I’ve seen in
    quite some time. For whatever reason, PolarSSL ships with GNU’s ‘Hello’
    program included, and consequently doesn’t link if we already have
    ‘Hello’ installed. Doh.
    
    Closes #32768.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/polarssl.rb
+++ Library/Formula/polarssl.rb
@@ -1,17 +1,21 @@
-require 'formula'
+require "formula"
 
 class Polarssl < Formula
-  homepage 'http://polarssl.org/'
-  url 'https://polarssl.org/download/polarssl-1.3.7-gpl.tgz'
-  sha1 '4bfce7f2e833bead53ecd38098325a784ada5c39'
+  homepage "https://polarssl.org/"
+  url "https://polarssl.org/download/polarssl-1.3.8-gpl.tgz"
+  sha1 "82ed8ebcf3dd53621da5395b796fc0917083691d"
 
-  depends_on 'cmake' => :build
+  head "https://github.com/polarssl/polarssl.git"
 
-  conflicts_with 'md5sha1sum', :because => 'both install conflicting binaries'
+  depends_on "cmake" => :build
+
+  conflicts_with "md5sha1sum", :because => "both install conflicting binaries"
 
   def install
     system "cmake", ".",  *std_cmake_args
     system "make"
-    system "make install"
+    system "make", "install"
+    # Why does PolarSSL ship with GNU's Hello included? Let's remove that.
+    rm "#{bin}/hello"
   end
 end
