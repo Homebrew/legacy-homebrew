f03f866bf0b107bc712c53d9ab1c6270ec29135a
Dominyk Tiller <dominyktiller@gmail.com>
2014-09-28 21:34:20 +0100

    Squashfm 4.3
    This will probably fail the bot, but I wanted to check my debug failure
    locally matched the bot’s.

--- Library/Formula/squashfs.rb
+++ Library/Formula/squashfs.rb
@@ -12,9 +12,11 @@ class Squashfs < Formula
     build 318
   end
 
-  # The instructions for this software say to do this on OS X
-  # Still seems to be required as of 4.3.
-  patch :p0, :DATA
+  # Still required as of 4.3
+  patch do
+    url "https://raw.githubusercontent.com/DomT4/scripts/master/Homebrew_Resources/Squashfs/squashfs.diff"
+    sha1 "8a350de6ee9e898dfbe858510b73ce3bc9b38a3d"
+  end
 
   def install
     cd "squashfs-tools" do
@@ -24,44 +26,3 @@ class Squashfs < Formula
     doc.install %w{ACKNOWLEDGEMENTS CHANGES COPYING INSTALL OLD-READMEs PERFORMANCE.README README README-4.2}
   end
 end
-
-__END__
-
-Originally from some internal notes:
-  "cd squashfs-tools; sed -i.orig 's/\|FNM_EXTMATCH//' $(grep -l FNM_EXTMATCH *)"
-  "cd squashfs-tools; sed -i.orig $'/#include \"unsquashfs.h\"/{i\\\n#include <sys/sysctl.h>\n}' unsquashfs.c"
-
-diff -u squashfs-tools.orig/mksquashfs.c squashfs-tools/mksquashfs.c
---- squashfs-tools.orig/mksquashfs.c	2009-04-05 14:22:48.000000000 -0700
-+++ squashfs-tools/mksquashfs.c	2011-11-17 17:51:31.000000000 -0800
-@@ -3975,7 +3975,7 @@
- 				regexec(path->name[i].preg, name, (size_t) 0,
- 					NULL, 0) == 0 :
- 				fnmatch(path->name[i].name, name,
--					FNM_PATHNAME|FNM_PERIOD|FNM_EXTMATCH) ==
-+					FNM_PATHNAME|FNM_PERIOD) ==
- 					 0;
- 
- 			if(match && path->name[i].paths == NULL) {
-Only in squashfs-tools: mksquashfs.c.orig
-diff -u squashfs-tools.orig/unsquashfs.c squashfs-tools/unsquashfs.c
---- squashfs-tools.orig/unsquashfs.c	2009-04-05 14:23:06.000000000 -0700
-+++ squashfs-tools/unsquashfs.c	2011-11-17 17:51:44.000000000 -0800
-@@ -29,7 +29,7 @@
- #include "compressor.h"
- #include "xattr.h"
- 
--#include <sys/sysinfo.h>
-+#include <sys/sysctl.h>
- #include <sys/types.h>
- 
- struct cache *fragment_cache, *data_cache;
-@@ -1195,7 +1196,7 @@
- 			int match = use_regex ?
- 				regexec(path->name[i].preg, name, (size_t) 0,
- 				NULL, 0) == 0 : fnmatch(path->name[i].name,
--				name, FNM_PATHNAME|FNM_PERIOD|FNM_EXTMATCH) ==
-+				name, FNM_PATHNAME|FNM_PERIOD) ==
- 				0;
- 			if(match && path->name[i].paths == NULL)
- 				/*
