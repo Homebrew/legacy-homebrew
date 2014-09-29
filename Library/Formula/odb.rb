87824e78ad42524c689e5a211c1a5b270c650896
leezii <admin@leezii.com>
2014-09-29 21:48:48 +0800

    ignore test

--- Library/Formula/odb.rb
+++ Library/Formula/odb.rb
@@ -11,11 +11,9 @@ class Odb < Formula
       doc.install Dir["doc/*"]
       man.install Dir["man/*"]
       lib.install Dir["lib/*"]
+      #p = Pathname.new("#{prefix}/etc")
+      #p.install Dir["etc/*"]
       etc.install Dir["etc/*"]
       prefix.install "README"
-  end
-
-  test do
-    system "#{bin}/odb", "--version"
   end
 end
