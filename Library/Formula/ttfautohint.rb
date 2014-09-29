53b77551a256f422b96f8891fde9e34008676c51
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-29 17:54:01 +0100

    ttfautohint: add 1.1 bottle.
    Closes #32788.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ttfautohint.rb
+++ Library/Formula/ttfautohint.rb
@@ -5,6 +5,13 @@ class Ttfautohint < Formula
   url "https://downloads.sourceforge.net/project/freetype/ttfautohint/1.1/ttfautohint-1.1.tar.gz"
   sha1 "db011a93ba15b2ac56ae27e3d8de0696b305ee7b"
 
+  bottle do
+    cellar :any
+    sha1 "35dab86922a01bdb464f3f8e224896a2af96d852" => :mavericks
+    sha1 "0d24c11620451a6e79f50e97ebae1671ef84e283" => :mountain_lion
+    sha1 "f498b23d089d1d59a72e4c9626f8b7d7cdad78f0" => :lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "freetype"
   depends_on "libpng"
