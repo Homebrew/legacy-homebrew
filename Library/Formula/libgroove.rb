dba25215678bc32fafe3ca149679362e4b157858
john muhl <git@johnmuhl.com>
2014-09-29 13:27:36 -0500

    libgroove 4.2.0 (new formula)

---
+++ Library/Formula/libgroove.rb
@@ -0,0 +1,32 @@
+require "formula"
+
+class Libgroove < Formula
+  homepage "https://github.com/andrewrk/libgroove"
+  url "https://github.com/andrewrk/libgroove/archive/4.2.0.tar.gz"
+  sha1 "1d60506bf9c99cefbbe5c2eea24fd0c1c39c39f5"
+
+  depends_on :macos => :mavericks
+  depends_on "cmake" => :build
+  depends_on "chromaprint"
+  depends_on "libav" => ["with-libvorbis"]
+  depends_on "libebur128"
+  depends_on "sdl2"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <groove/groove.h>
+      int main() {
+        groove_init();
+        groove_version();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-lgroove", "-o", "test"
+    system "./test"
+  end
+end
