327a5f5f736ff173d12afca50eecaf540c94d3d1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-29 18:39:39 +0100

    libebur128: add 1.0.2 bottle.
    Closes #32625.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/libebur128.rb
+++ Library/Formula/libebur128.rb
@@ -5,6 +5,13 @@ class Libebur128 < Formula
   url "https://github.com/jiixyj/libebur128/archive/v1.0.2.tar.gz"
   sha1 "b1e2949e6598053edb8aeaf71614a26efcb38bd0"
 
+  bottle do
+    cellar :any
+    sha1 "8ce0f24393cd9fb4e279f7433c5c6f6da103156d" => :mavericks
+    sha1 "193c2934725f5b5e3d2a3c2fae822820eec36716" => :mountain_lion
+    sha1 "b6fa1c43bac3b49a1c6440afa669b600f3c47970" => :lion
+  end
+
   depends_on "cmake" => :build
   depends_on "speex" => [:recommended, "with-libogg"]
 
