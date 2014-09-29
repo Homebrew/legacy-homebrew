639c2ffb40f02018de0f2688a87efe3558764eaf
Thijs Vermeir <thijsvermeir@gmail.com>
2014-09-29 11:41:04 +0200

    libgee 0.16.0

--- Library/Formula/libgee.rb
+++ Library/Formula/libgee.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Libgee < Formula
   homepage "https://wiki.gnome.org/Projects/Libgee"
-  url "http://ftp.gnome.org/pub/GNOME/sources/libgee/0.14/libgee-0.14.0.tar.xz"
-  sha1 "ca6531c8ba45cc865f2ce7b93a8b3f96e1c1505e"
+  url "http://ftp.gnome.org/pub/GNOME/sources/libgee/0.16/libgee-0.16.0.tar.xz"
+  sha1 "d67e718138fb5788d6a1aea7f344e670adb77375"
 
   bottle do
     cellar :any
