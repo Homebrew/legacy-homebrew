0fc6b570a85f5c1d552db2ad6d7b9a480b5ed6ec
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-30 23:04:46 +0100

    glide: add 0.1.0 bottle.
    Closes #32824.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/glide.rb
+++ Library/Formula/glide.rb
@@ -6,6 +6,12 @@ class Glide < Formula
   url "https://github.com/Masterminds/glide/archive/0.1.0.tar.gz"
   sha1 "8323f0a9620d28fa1bc1b8514e2159ed0d27c135"
 
+  bottle do
+    sha1 "6f293dd28cd8648a468df50c47425ab9b44abd57" => :mavericks
+    sha1 "d005e3f36eea13d35e727fc2d89f7a6ef1f6af7d" => :mountain_lion
+    sha1 "f829adcff437950744190b5de6904db7548e81f4" => :lion
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/kylelemons/go-gypsy" do
