bc5662036b3329ec9eeb5fdb430e81438a8eb5a4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-09-30 05:34:59 +0100

    signify-osx: add 1.1 bottle.
    Closes #31196.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/signify-osx.rb
+++ Library/Formula/signify-osx.rb
@@ -6,6 +6,13 @@ class SignifyOsx < Formula
   sha256 "e62649b908b2ae3b8940a452e95b034772cd2856603a196d4a50d78701ed6478"
   head "https://github.com/jpouellet/signify-osx.git"
 
+  bottle do
+    cellar :any
+    sha1 "b327798f0dabb99de3ab4a57dffb9106ab98278b" => :mavericks
+    sha1 "626173becda370cf363c5e1f891c5132be4e22f9" => :mountain_lion
+    sha1 "271dffbae2ffbec4720250934caab7badadbace2" => :lion
+  end
+
   def install
     system "make"
     system "make", "test"
