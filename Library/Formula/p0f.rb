9477096c120bd3e629b54b9b3fe9936c61d7d09e
J. Brandt Buckley <brandt@runlevel1.com>
2014-10-01 13:34:11 -0700

    Update p0f to version 3.0.7b, add test
    - Update p0f to version 3.0.7b.
    - Add a simple test in which p0f reads a pcap file.
    
    In the test we write a simple binary pcap file (included in the recipe base64-encoded) and then attempt to read the file with p0f.

--- Library/Formula/p0f.rb
+++ Library/Formula/p0f.rb
@@ -2,13 +2,23 @@ require 'formula'
 
 class P0f < Formula
   homepage 'http://lcamtuf.coredump.cx/p0f3/'
-  url 'http://lcamtuf.coredump.cx/p0f3/releases/p0f-3.06b.tgz'
-  sha1 'fecc9ed8b79dbf8e4d5ffbd22eda9d850b42c3c3'
+  url 'http://lcamtuf.coredump.cx/p0f3/releases/p0f-3.07b.tgz'
+  sha1 'ae2c4fbcddf2a5ced33abd81992405b93207e7c8'
 
   def install
-    inreplace "config.h", "p0f.fp", "#{etc}/p0f/p0f.fp"
-    system "./build.sh"
-    sbin.install "p0f"
-    (etc+"p0f").install "p0f.fp"
+    inreplace 'config.h', 'p0f.fp', "#{etc}/p0f/p0f.fp"
+    system './build.sh'
+    sbin.install 'p0f'
+    (etc + 'p0f').install 'p0f.fp'
+  end
+
+  test do
+    require 'base64'
+
+    pcap = '1MOyoQIABAAAAAAAAAAAAP//AAAAAAAA92EsVI67DgBEAAAA' \
+           'RAAAAAIAAABFAABAbvpAAEAGAAB/AAABfwAAAcv8Iyjt2/Pg' \
+           'AAAAALAC///+NAAAAgQ/2AEDAwQBAQgKCyrc9wAAAAAEAgAA'
+    (testpath / 'test.pcap').write(Base64.decode64(pcap))
+    system 'p0f', '-r', 'test.pcap'
   end
 end
