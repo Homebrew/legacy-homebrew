edb99852a9405733dceb596db4ce7b61fc27ad32
leezii <admin@leezii.com>
2014-10-01 12:03:37 +0800

    new formula: ODB C++ Object-Relational Mapping(ORM)

---
+++ Library/Formula/odb.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+class Odb < Formula
+  homepage "http://www.codesynthesis.com/products/odb"
+  url "http://www.codesynthesis.com/download/odb/2.3/odb-2.3.0-i686-macosx.tar.bz2"
+  sha1 "7bdde23582d8e6f57d5a2fffd9fa77ab35de5bcc"
+
+  def install
+    bin.install Dir["bin/*"]
+    doc.install Dir["doc/*"]
+    man.install Dir["man/*"]
+    lib.install Dir["lib/*"]
+    (prefix+"etc").install Dir["etc/*"]
+    prefix.install "README"
+  end
+
+  test do
+    system "odb", "--help"
+  end
+
+  def caveats; <<-EOS.undent
+        ODB is undel GPL v2 or NCUEL.
+        Please read http://codesynthesis.com/products/odb/license.xhtml before use.
+        EOS
+  end
+end
