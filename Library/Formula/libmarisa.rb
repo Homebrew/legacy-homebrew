17ba2b018f62b24cb812494445ca7f27557fcdb3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-01 23:04:13 +0100

    libmarisa: add 0.2.4 bottle.
    Closes #32769.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libmarisa.rb
+++ Library/Formula/libmarisa.rb
@@ -5,6 +5,13 @@ class Libmarisa < Formula
   url "https://marisa-trie.googlecode.com/files/marisa-0.2.4.tar.gz"
   sha1 "fb0ed7d993e84dff32ec456a79bd36a00022629d"
 
+  bottle do
+    cellar :any
+    sha1 "7488aa114b3c53ec5b2d11a7ebbc7196de0539a8" => :mavericks
+    sha1 "c45d67cdca754912b77a8b0d9ac39095f3374da1" => :mountain_lion
+    sha1 "3fc68e8e71c8696197f3f9dcb6daa0f98890cc56" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "check"
