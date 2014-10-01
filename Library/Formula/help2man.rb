d1648c5ed9c4a42ffad9777a6342319b1eb92527
Jack Nagel <jacknagel@gmail.com>
2014-10-01 14:35:07 -0500

    help2man 1.46.3

--- Library/Formula/help2man.rb
+++ Library/Formula/help2man.rb
@@ -2,15 +2,11 @@ require "formula"
 
 class Help2man < Formula
   homepage "http://www.gnu.org/software/help2man/"
-  url "http://ftpmirror.gnu.org/help2man/help2man-1.46.2.tar.xz"
-  mirror "http://ftp.gnu.org/gnu/help2man/help2man-1.46.2.tar.xz"
-  sha256 "92191decc8c324c88bfec5e989d13108f22ed135d794bde2b3b802ffe3650311"
+  url "http://ftpmirror.gnu.org/help2man/help2man-1.46.3.tar.xz"
+  mirror "http://ftp.gnu.org/gnu/help2man/help2man-1.46.3.tar.xz"
+  sha256 "a669dc3baf2f9fcfcf9d55d8555d1c234bc3cabd5fbe8e36e0dea4a88c222942"
 
   bottle do
-    cellar :any
-    sha1 "2f432d716f8225e6b698078c65fd44a0588cee74" => :mavericks
-    sha1 "8d2228346994f1f004898126144ea4a4466f9316" => :mountain_lion
-    sha1 "4d903506ba1f27bd570ec3cd4012dcfb3cb569df" => :lion
   end
 
   def install
