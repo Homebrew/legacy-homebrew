ffe19fdf65fbc19377ba5ac6412054492e315d77
Jack Nagel <jacknagel@gmail.com>
2014-10-01 12:08:29 -0500

    erlang: add upstream patch to fix SSL issue

--- Library/Formula/erlang.rb
+++ Library/Formula/erlang.rb
@@ -5,9 +5,18 @@ require 'formula'
 # elixir are compatible.
 class Erlang < Formula
   homepage 'http://www.erlang.org'
-  # Download tarball from GitHub; it is served faster than the official tarball.
-  url "https://github.com/erlang/otp/archive/OTP-17.3.tar.gz"
-  sha1 "655e23a4f98b8ba4976dc417f0e876b40df74a7b"
+
+  stable do
+    # Download tarball from GitHub; it is served faster than the official tarball.
+    url "https://github.com/erlang/otp/archive/OTP-17.3.tar.gz"
+    sha1 "655e23a4f98b8ba4976dc417f0e876b40df74a7b"
+
+    # Upstream patch to fix http://erlang.org/pipermail/erlang-questions/2014-September/081176.html
+    patch do
+      url "https://github.com/erlang/otp/commit/b196730a325cfe74312c3a5f4b1273ba7c705ed6.diff"
+      sha1 "382fcaf4502adaac4f4c00dd46f64adb0ae84212"
+    end
+  end
 
   head 'https://github.com/erlang/otp.git', :branch => 'master'
 
