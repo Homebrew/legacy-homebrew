49b55c6d5bfc91c5231b2a1cf5599a9c84228b3b
Shaun Jackman <sjackman@gmail.com>
2014-10-01 17:36:24 -0700

    glib 2.42.0: Do not use -lintl for Linuxbrew

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -91,7 +91,7 @@ class Glib < Formula
               "Libs: -L${libdir} -lglib-2.0 -L#{gettext}/lib -lintl"
       s.gsub! 'Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include',
               "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include -I#{gettext}/include"
-    end
+    end if OS.mac?
 
     (share+'gtk-doc').rmtree
   end
