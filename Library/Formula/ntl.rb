7b281af55ff1198f754b91cc75bce4bcefe6ef53
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-02 19:12:50 +0100

    ntl: add 6.2.1 bottle.
    Closes #32879.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ntl.rb
+++ Library/Formula/ntl.rb
@@ -5,6 +5,13 @@ class Ntl < Formula
   url 'http://www.shoup.net/ntl/ntl-6.2.1.tar.gz'
   sha1 '3b9ab3bedb0b2e9b5ee322d60745be5caf1c743f'
 
+  bottle do
+    cellar :any
+    sha1 "b55331990e2df310d694d5ae67dc3a55d6f18fd1" => :mavericks
+    sha1 "bd1f0789b9ebefb00eabd5d9de5f5dc2fe08bbc7" => :mountain_lion
+    sha1 "0f7793a186aca6d6ea229b678575a008d3e1911b" => :lion
+  end
+
   def install
     cd "src" do
       system "./configure", "PREFIX=#{prefix}"
