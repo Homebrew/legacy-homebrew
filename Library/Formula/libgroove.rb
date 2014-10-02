e3560a2d053dbfb3c26174eb40494fcbcc886ee3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-02 15:32:12 +0100

    libgroove: add 4.2.0 bottle.
    Closes #32790.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/libgroove.rb
+++ Library/Formula/libgroove.rb
@@ -5,6 +5,11 @@ class Libgroove < Formula
   url "https://github.com/andrewrk/libgroove/archive/4.2.0.tar.gz"
   sha1 "1d60506bf9c99cefbbe5c2eea24fd0c1c39c39f5"
 
+  bottle do
+    cellar :any
+    sha1 "ee1764dec529305eba952244884d880c495762e6" => :mavericks
+  end
+
   depends_on :macos => :mavericks
   depends_on "cmake" => :build
   depends_on "chromaprint"
