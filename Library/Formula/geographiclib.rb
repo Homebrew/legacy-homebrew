27cbc73807befea70e42df517038852f7da9181d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-03 16:33:05 +0100

    geographiclib: update 1.38 bottle.
    Closes #32911.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/geographiclib.rb
+++ Library/Formula/geographiclib.rb
@@ -5,9 +5,10 @@ class Geographiclib < Formula
   sha1 "e854d6d85cc5b1273fa4044828fadd9e1b151fcd"
 
   bottle do
-    sha1 "bfa37887453c33dc76cc1005e3accae3720ea227" => :mavericks
-    sha1 "6daf6c3be855c49c0b2eab5d2c1de502dbeb982c" => :mountain_lion
-    sha1 "7d1d3266f3b3f6398a413a170ee36ca8376a5357" => :lion
+    cellar :any
+    sha1 "f073cfb05702e53278e63e16e639e983f146339e" => :mavericks
+    sha1 "6a29856e399ed02bdfe2ab359acf8b51236a460c" => :mountain_lion
+    sha1 "4dec4fdcf143f0b9343aae0c6cc56d25a8b3e660" => :lion
   end
 
   depends_on "cmake" => :build
