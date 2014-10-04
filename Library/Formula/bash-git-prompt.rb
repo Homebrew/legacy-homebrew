4a154369f0803a45f34bf6416100153291784b3b
Martin Gondermann <magicmonty@pagansoft.de>
2014-10-04 22:14:29 +0200

    bash-git-prompt 2.2
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/bash-git-prompt.rb
+++ Library/Formula/bash-git-prompt.rb
@@ -2,19 +2,23 @@ require "formula"
 
 class BashGitPrompt < Formula
   homepage "https://github.com/magicmonty/bash-git-prompt"
-  url "https://github.com/magicmonty/bash-git-prompt/archive/2.1.tar.gz"
-  sha1 "9bd29dc2aa4859d2f4c0736cb26cc177de9c1274"
+  url "https://github.com/magicmonty/bash-git-prompt/archive/2.2.tar.gz"
+  sha1 "392d430b87639e7f85fde02f2b4a37cb43be9aaf"
   head "https://github.com/magicmonty/bash-git-prompt.git"
 
   def install
-    share.install "./gitprompt.sh", "./gitprompt.fish", "./git-prompt-help.sh",
-                  "./git-prompt-colors.sh", "gitstatus.sh", "prompt-colors.sh"
+    share.install "gitprompt.sh", "gitprompt.fish", "git-prompt-help.sh",
+                  "gitstatus.sh", "prompt-colors.sh"
+
+    (share/"themes").install Dir["themes/*.bgptheme"]
     doc.install "README.md"
   end
 
   def caveats; <<-EOS.undent
     You should add the following to your .bashrc (or equivalent):
-      source #{opt_share}/gitprompt.sh
+      if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+        source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+      fi
     EOS
   end
 end
