bfe8f93ba44878245fa01eeea00f0c70f9f00e1a
Weiyin He <weiyin@gmail.com>
2014-10-05 11:56:10 -0400

    ios-sim 3.0.0
    Closes #32941.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ios-sim.rb
+++ Library/Formula/ios-sim.rb
@@ -2,18 +2,17 @@ require "formula"
 
 class IosSim < Formula
   homepage "https://github.com/phonegap/ios-sim"
-  url "https://github.com/phonegap/ios-sim/archive/2.0.1.tar.gz"
-  sha1 "df15d7f49561b92f8d3bf8832db60b83d7ce355e"
+  url "https://github.com/phonegap/ios-sim/archive/3.0.0.tar.gz"
+  sha1 "e5b5dd2a622549f105935c3f6fc3cce93aba7b45"
   head "https://github.com/phonegap/ios-sim.git"
 
   bottle do
     cellar :any
     sha1 "3f28d1596a8d019ecb4fc70f758190a21e56ebcc" => :mavericks
     sha1 "9ed5958db90bb227a0f0afc878f4cfb4a835fcca" => :mountain_lion
-    sha1 "4ad30f386778b5e395755109ec50fb725066099c" => :lion
   end
 
-  depends_on :macos => :lion
+  depends_on :macos => :mountain_lion
 
   def install
     rake "install", "prefix=#{prefix}"
