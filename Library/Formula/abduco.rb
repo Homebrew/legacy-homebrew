0e41a965b7a2a049167724d20a695ec56abcc056
Colin King <richard.colin.king@gmail.com>
2014-10-05 11:34:57 -0500

    Included upstream patch to fix BSD strip -s behavior vs GNU strip. Safe to remove in Abduco versions after 0.1.

--- Library/Formula/abduco.rb
+++ Library/Formula/abduco.rb
@@ -6,8 +6,14 @@ class Abduco < Formula
   sha1 "063b66d8a9a83ecd5b9501afc86812a06ad79076"
   head "git://repo.or.cz/abduco.git"
 
+  # upstream fix for BSD strip -s behavior compared to GNU
+  # safe to remove in abduco versions after 0.1
+  patch do
+    url "http://repo.or.cz/w/abduco.git/patch/940519"
+    sha1 "45bd29721d0e52d6875a8e3e21c16792a5758be9"
+  end
+
   def install
-    inreplace "Makefile", "strip -s", "strip"
     system "make", "PREFIX=#{prefix}", "install"
   end
 end
