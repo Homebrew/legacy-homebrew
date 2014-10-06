2fe44290ba8ae33e65ca0f2a34598310378d2b32
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-06 01:24:57 +0100

    glide: update 0.2.0 bottle.
    Closes #32958.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/glide.rb
+++ Library/Formula/glide.rb
@@ -7,9 +7,9 @@ class Glide < Formula
   sha1 "a01fc0a56cfcb9070af2e8bbca35da81310b0b21"
 
   bottle do
-    sha1 "6f293dd28cd8648a468df50c47425ab9b44abd57" => :mavericks
-    sha1 "d005e3f36eea13d35e727fc2d89f7a6ef1f6af7d" => :mountain_lion
-    sha1 "f829adcff437950744190b5de6904db7548e81f4" => :lion
+    sha1 "dea3d205ecbfa38f7cfe5e2b79c5ab12af462df9" => :mavericks
+    sha1 "d622cfb237b2e26a63a923f00c7d0af639974219" => :mountain_lion
+    sha1 "7afcc2e6b089b38d8ed38979621fceb457df0cfa" => :lion
   end
 
   depends_on "go" => :build
