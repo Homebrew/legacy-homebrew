6c91a563603c89baaa176a05abc74d956f804d29
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-06 21:17:41 +0100

    ffmpeg: update 2.4.1 bottle.

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -8,6 +8,9 @@ class Ffmpeg < Formula
   head "git://git.videolan.org/ffmpeg.git"
 
   bottle do
+    sha1 "5b84336642ac553973b684f07a0079d1b31f6e37" => :mavericks
+    sha1 "41730850dbf466a075cb97548e693a3ab494a530" => :mountain_lion
+    sha1 "cc04d45926d8595547f7f5501aae500693f230ce" => :lion
   end
 
   option "without-x264", "Disable H.264 encoder"
