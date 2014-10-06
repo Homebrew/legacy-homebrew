089314c12a6fbcc1a88a6b1577a38a15c0044c7e
Vihang D <vihang.dudhalkar@gmail.com>
2014-10-06 15:35:57 +0530

    go-app-engine-64 1.9.12: Fix for Linuxbrew
    Closes #149

--- Library/Formula/go-app-engine-64.rb
+++ Library/Formula/go-app-engine-64.rb
@@ -2,8 +2,16 @@ require "formula"
 
 class GoAppEngine64 < Formula
   homepage "http://code.google.com/appengine/docs/go/overview.html"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.12.zip"
-  sha1 "19f8773e8ccb1d0d28c8475aa8d677eb54137580"
+
+  if OS.mac?
+    url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.12.zip"
+    sha1 "19f8773e8ccb1d0d28c8475aa8d677eb54137580"
+  elsif OS.linux?
+    url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.12.zip"
+    sha1 "400e43a1f8f600a41e9698108cdc9d0fbbf2aadb"
+  else
+    raise "Unknown operating system"
+  end
 
   def install
     cd ".."
