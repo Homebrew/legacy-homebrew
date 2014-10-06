6e783182ab76f2c9e92f1cbdd3922fab3e5a7cfc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-06 19:02:08 +0100

    git-archive-all: add 1.9 bottle.
    Closes #32972.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/git-archive-all.rb
+++ Library/Formula/git-archive-all.rb
@@ -7,6 +7,13 @@ class GitArchiveAll < Formula
 
   head 'https://github.com/Kentzo/git-archive-all.git'
 
+  bottle do
+    cellar :any
+    sha1 "883e8ab977795ea4fb85ac41913e5b3b55a0f25b" => :mavericks
+    sha1 "6d7cd0e9d2ab7e9171585d6f121c1817719fc614" => :mountain_lion
+    sha1 "6833260e3badad055db6da98c6d308345c10a7d1" => :lion
+  end
+
   def install
     system "make", "prefix=#{prefix}", "install"
   end
