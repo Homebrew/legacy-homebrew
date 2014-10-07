23f5335ee09392e59bc3b4bd420878d7e56e115d
john muhl <git@johnmuhl.com>
2014-10-07 09:12:15 -0500

    libgroove 4.2.1
    * Depend on ffmpeg for libavcodec
    * Remove bottle pending dependency resolution issues
    
    Closes #32790.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/libgroove.rb
+++ Library/Formula/libgroove.rb
@@ -2,18 +2,13 @@ require "formula"
 
 class Libgroove < Formula
   homepage "https://github.com/andrewrk/libgroove"
-  url "https://github.com/andrewrk/libgroove/archive/4.2.0.tar.gz"
-  sha1 "1d60506bf9c99cefbbe5c2eea24fd0c1c39c39f5"
-
-  bottle do
-    cellar :any
-    sha1 "ee1764dec529305eba952244884d880c495762e6" => :mavericks
-  end
+  url "https://github.com/andrewrk/libgroove/archive/4.2.1.tar.gz"
+  sha1 "cdcf4109266bb5eb38d62945891d37d907c3ccd3"
 
   depends_on :macos => :mavericks
   depends_on "cmake" => :build
   depends_on "chromaprint"
-  depends_on "libav" => ["with-libvorbis"]
+  depends_on "ffmpeg" => "with-libvorbis"
   depends_on "libebur128"
   depends_on "sdl2"
 
