324a258130b7f6848aba1ab3126fe022eaeb8822
David Christenson <david@the-david.com>
2014-10-06 23:56:12 -0600

    ddrescue 1.19
    Update to latest stable release.

--- Library/Formula/ddrescue.rb
+++ Library/Formula/ddrescue.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Ddrescue < Formula
   homepage "https://www.gnu.org/software/ddrescue/ddrescue.html"
-  url "http://ftpmirror.gnu.org/ddrescue/ddrescue-1.18.1.tar.lz"
-  mirror "https://ftp.gnu.org/gnu/ddrescue/ddrescue-1.18.1.tar.lz"
-  sha1 "97cd3c6c783f3dc5685f0147dc83f3c94fb36b4c"
+  url "http://ftpmirror.gnu.org/ddrescue/ddrescue-1.19.tar.lz"
+  mirror "https://ftp.gnu.org/gnu/ddrescue/ddrescue-1.19.tar.lz"
+  sha1 "5c5bfa4e2d0cb98feb1fd66cb9ea0c8ab2c7d34d"
 
   def install
     system "./configure", "--prefix=#{prefix}",
