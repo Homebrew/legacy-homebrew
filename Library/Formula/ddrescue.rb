7c07f8e1c9db5cf409bd48877f6362616d2da031
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-07 07:07:03 +0100

    ddrescue: add 1.19 bottle.
    Closes #32990.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ddrescue.rb
+++ Library/Formula/ddrescue.rb
@@ -6,6 +6,13 @@ class Ddrescue < Formula
   mirror "https://ftp.gnu.org/gnu/ddrescue/ddrescue-1.19.tar.lz"
   sha1 "5c5bfa4e2d0cb98feb1fd66cb9ea0c8ab2c7d34d"
 
+  bottle do
+    cellar :any
+    sha1 "e3a30bbbd40b51f4ec0d71f215406e0e511f5d04" => :mavericks
+    sha1 "9917eb5ba28f1acb4f02b52b9cab9cb8e7768dfc" => :mountain_lion
+    sha1 "6cca70194ffc701ab511665630db71cdd46425ae" => :lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "CXX=#{ENV.cxx}"
