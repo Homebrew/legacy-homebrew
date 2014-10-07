ef82613aafa75ecc3afa51d4610aaa38c488a410
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-07 18:57:31 +0100

    wxmac 3.0.2
    Closes #32993.

--- Library/Formula/wxmac.rb
+++ Library/Formula/wxmac.rb
@@ -1,10 +1,9 @@
-require 'formula'
+require "formula"
 
 class Wxmac < Formula
   homepage "http://www.wxwidgets.org"
-  url "https://downloads.sourceforge.net/project/wxwindows/3.0.1/wxWidgets-3.0.1.tar.bz2"
-  mirror "https://mirrors.kernel.org/debian/pool/main/w/wxwidgets3.0/wxwidgets3.0_3.0.1.orig.tar.bz2"
-  sha1 "73e58521d6871c9f4d1e7974c6e3a81629fddcf8"
+  url "https://downloads.sourceforge.net/project/wxwindows/3.0.2/wxWidgets-3.0.2.tar.bz2"
+  sha1 "6461eab4428c0a8b9e41781b8787510484dea800"
 
   bottle do
     revision 7
@@ -17,40 +16,20 @@ class Wxmac < Formula
   depends_on "libpng"
   depends_on "libtiff"
 
-  # Patches for 3.0.1 source release to reference the correct dispatch types
-  # regardless of whether built for OS X 10.10 or 10.9 (and earlier...)
-  #
-  # Patch derived from ticket comments and final changesets below:
-  #
-  #  http://trac.wxwidgets.org/ticket/16329
-  #  http://trac.wxwidgets.org/changeset/76744
-  #  http://trac.wxwidgets.org/changeset/76743
-  #
-  # NOTE: Revisit with next upstream release; changes already in upstream trunk
-
-  # Patch Changeset 76744
-  # http://trac.wxwidgets.org/changeset/76744
-  patch :p3 do
-    url "http://trac.wxwidgets.org/changeset/76744?format=diff&new=7674"
-    sha1 "5cd1536d2494ef0b4d21f03799b0ac024572ae31"
-  end
-
-  # Patch Changeset 76743
-  #
-  # This is embedded because all but the last file is patchable with the changeset
-  # checked into the upstream trunk (webview_webkit.mm). The version of that file
-  # in trunk had changed enough that the patch cannot match any of the hunks to
-  # their corresponding line numbers in the 3.0.2 release train source distribution.
+  # Various fixes related to Yosemite. Revisit in next stable release.
+  # Please keep an eye on http://trac.wxwidgets.org/ticket/16329 as well
+  # Theoretically the above linked patch should still be needed, but it isn't. Try to find out why.
   patch :DATA
 
   def install
     # need to set with-macosx-version-min to avoid configure defaulting to 10.5
     # need to enable universal binary build in order to build all x86_64
-    # FIXME I don't believe this is the whole story, surely this can be fixed
-    # without building universal for users who don't need it. - Jack
+    # Jack - I don't believe this is the whole story, surely this can be fixed
+    # without building universal for users who don't need it.
     # headers need to specify x86_64 and i386 or will try to build for ppc arch
     # and fail on newer OSes
-    # https://trac.macports.org/browser/trunk/dports/graphics/wxWidgets30/Portfile#L80
+    # DomT4 - MacPorts seems to have stopped building universal by default? Can we do the same?
+    # https://trac.macports.org/browser/trunk/dports/graphics/wxWidgets-3.0/Portfile#L210
     ENV.universal_binary
     args = [
       "--disable-debug",
@@ -86,16 +65,17 @@ class Wxmac < Formula
     ]
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
   end
 end
 
 __END__
 
-diff -ur a/include/wx/defs.h b/include/wx/defs.h
+diff --git a/include/wx/defs.h b/include/wx/defs.h
+index 397ddd7..d128083 100644
 --- a/include/wx/defs.h
 +++ b/include/wx/defs.h
-@@ -3169,13 +3169,20 @@
+@@ -3169,12 +3169,20 @@ DECLARE_WXCOCOA_OBJC_CLASS(UIImage);
  DECLARE_WXCOCOA_OBJC_CLASS(UIEvent);
  DECLARE_WXCOCOA_OBJC_CLASS(NSSet);
  DECLARE_WXCOCOA_OBJC_CLASS(EAGLContext);
@@ -105,19 +85,19 @@ diff -ur a/include/wx/defs.h b/include/wx/defs.h
  typedef WX_UIView WXWidget;
  typedef WX_EAGLContext WXGLContext;
  typedef WX_NSString* WXGLPixelFormat;
-+typedef WX_UIWebView OSXWebViewPtr; 
  
--#endif
-+#endif 
++typedef WX_UIWebView OSXWebViewPtr;
 +
-+#if wxOSX_USE_COCOA_OR_CARBON 
-+DECLARE_WXCOCOA_OBJC_CLASS(WebView); 
-+typedef WX_WebView OSXWebViewPtr; 
-+#endif 
++#endif
++
++#if wxOSX_USE_COCOA_OR_CARBON
++DECLARE_WXCOCOA_OBJC_CLASS(WebView);
++typedef WX_WebView OSXWebViewPtr;
+ #endif
  
  #endif /* __WXMAC__ */
- 
-diff -ur a/include/wx/html/webkit.h b/include/wx/html/webkit.h
+diff --git a/include/wx/html/webkit.h b/include/wx/html/webkit.h
+index 8700367..f805099 100644
 --- a/include/wx/html/webkit.h
 +++ b/include/wx/html/webkit.h
 @@ -18,7 +18,6 @@
@@ -128,7 +108,7 @@ diff -ur a/include/wx/html/webkit.h b/include/wx/html/webkit.h
  
  // ----------------------------------------------------------------------------
  // Web Kit Control
-@@ -107,7 +106,7 @@
+@@ -107,7 +106,7 @@ private:
      wxString m_currentURL;
      wxString m_pageTitle;
  
@@ -137,10 +117,11 @@ diff -ur a/include/wx/html/webkit.h b/include/wx/html/webkit.h
  
      // we may use this later to setup our own mouse events,
      // so leave it in for now.
-diff -ur a/include/wx/osx/webview_webkit.h b/include/wx/osx/webview_webkit.h
+diff --git a/include/wx/osx/webview_webkit.h b/include/wx/osx/webview_webkit.h
+index 803f8b0..438e532 100644
 --- a/include/wx/osx/webview_webkit.h
 +++ b/include/wx/osx/webview_webkit.h
-@@ -158,7 +158,7 @@
+@@ -158,7 +158,7 @@ private:
      wxWindowID m_windowID;
      wxString m_pageTitle;
  
@@ -149,60 +130,3 @@ diff -ur a/include/wx/osx/webview_webkit.h b/include/wx/osx/webview_webkit.h
  
      // we may use this later to setup our own mouse events,
      // so leave it in for now.
-diff -ur a/src/osx/webview_webkit.mm b/src/osx/webview_webkit.mm
---- a/src/osx/webview_webkit.mm	
-+++ b/src/osx/webview_webkit.mm	
-@@ -442,7 +442,7 @@
-     if ( !m_webView )
-         return;
- 
--    [(WebView*)m_webView goBack];
-+    [m_webView goBack];
- }
- 
- void wxWebViewWebKit::GoForward()
-@@ -450,7 +450,7 @@
-     if ( !m_webView )
-         return;
- 
--    [(WebView*)m_webView goForward];
-+    [m_webView goForward];
- }
- 
- void wxWebViewWebKit::Reload(wxWebViewReloadFlags flags)
-@@ -849,7 +849,7 @@
-     if ( !m_webView )
-         return;
- 
--    [(WebView*)m_webView cut:m_webView];
-+    [m_webView cut:m_webView];
- }
- 
- void wxWebViewWebKit::Copy()
-@@ -857,7 +857,7 @@
-     if ( !m_webView )
-         return;
- 
--    [(WebView*)m_webView copy:m_webView];
-+    [m_webView copy:m_webView];
- }
- 
- void wxWebViewWebKit::Paste()
-@@ -865,7 +865,7 @@
-     if ( !m_webView )
-         return;
- 
--    [(WebView*)m_webView paste:m_webView];
-+    [m_webView paste:m_webView];
- }
- 
- void wxWebViewWebKit::DeleteSelection()
-@@ -873,7 +873,7 @@
-     if ( !m_webView )
-         return;
- 
--    [(WebView*)m_webView deleteSelection];
-+    [m_webView deleteSelection];
- }
- 
- bool wxWebViewWebKit::HasSelection() const
