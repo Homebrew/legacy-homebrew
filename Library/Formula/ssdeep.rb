c922b439b42cd3b1f1a5b0a0fb185b5e375dbd94
Jorge Capmany <jorge@capmany.net>
2014-10-08 17:57:43 +0200

    ssdeep 2.11.1

--- Library/Formula/ssdeep.rb
+++ Library/Formula/ssdeep.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Ssdeep < Formula
   homepage "http://ssdeep.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/ssdeep/ssdeep-2.11/ssdeep-2.11.tar.gz"
-  sha256 "82cc0e06f44127fc5c9c507881951714981da6187cdcfed0158c9167f39effc7"
+  url "https://downloads.sourceforge.net/project/ssdeep/ssdeep-2.11.1/ssdeep-2.11.1.tar.gz"
+  sha256 "a632ac30fca29ad5627e1bf5fae05d9a8873e6606314922479259531e0c19608"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
