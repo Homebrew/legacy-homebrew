f4eb4ea13a66e17ecf36ec906f4489920b20be54
Jack Nagel <jacknagel@gmail.com>
2014-10-09 00:20:15 -0500

    leaves: load tab outside of the inner loop

--- Library/Homebrew/cmd/leaves.rb
+++ Library/Homebrew/cmd/leaves.rb
@@ -9,10 +9,10 @@ module Homebrew
 
     installed.each do |f|
       deps = []
+      tab = Tab.for_formula(f)
 
       f.deps.each do |dep|
         if dep.optional? || dep.recommended?
-          tab = Tab.for_formula(f)
           deps << dep.to_formula.name if tab.with?(dep.to_formula.name)
         else
           deps << dep.to_formula.name
