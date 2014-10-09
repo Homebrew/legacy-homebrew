6eea5c904677a85aae268b781bfcce75a0f0ab36
Stian Eikeland <stian@eikeland.se>
2014-10-09 10:49:17 +0200

    awscli 1.5.1

--- Library/Formula/awscli.rb
+++ Library/Formula/awscli.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Awscli < Formula
   homepage "https://aws.amazon.com/cli/"
-  url "https://pypi.python.org/packages/source/a/awscli/awscli-1.4.3.tar.gz"
-  sha1 "59d5cb117ba1c6071ce4badc61ba48d41263f182"
+  url "https://pypi.python.org/packages/source/a/awscli/awscli-1.5.1.tar.gz"
+  sha1 "784b09ed06d2a414e87960fbe99f67da31a8f676"
 
   bottle do
     cellar :any
@@ -31,8 +31,8 @@ class Awscli < Formula
   depends_on :python if MacOS.version <= :snow_leopard
 
   resource "botocore" do
-    url "https://pypi.python.org/packages/source/b/botocore/botocore-0.63.0.tar.gz"
-    sha1 "41ea1bf85acdd95d7af6f1b705cbe5cf2b4aea02"
+    url "https://pypi.python.org/packages/source/b/botocore/botocore-0.65.0.tar.gz"
+    sha1 "52c8e6888e248ef795e29620b85c61b644c655a3"
   end
 
   resource "bcdoc" do
