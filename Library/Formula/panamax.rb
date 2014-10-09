347856c70d4e9ffc315d53f606517c3d6ccfc815
Chaitanya Akkineni <cakkineni@gmail.com>
2014-10-09 16:35:29 -0500

    Updating to fix issues with build.

--- Library/Formula/panamax.rb
+++ Library/Formula/panamax.rb
@@ -5,7 +5,6 @@ class Panamax < Formula
   homepage "http://www.panamax.io"
   url "http://download.panamax.io/installer/panamax-0.3.0.tar.gz"
   sha1 "d4079e9b5326eb7a5adee581fa246f453bb4ada8"
-
   def install
     system "./configure", "--prefix=#{prefix}", "--var=#{var}/panamax"
     system "make", "install"
@@ -16,14 +15,10 @@ class Panamax < Formula
   end
 
   test do
-    installed = File.exist?("#{prefix}/.panamax")
-    assert installed
-    assert_equal 0, $?.exitstatus
+    assert File.exist?("#{prefix}/.panamax")
+    assert File.exist?("#{var}/panamax")
 
-    installed = File.exist?("#{var}/panamax")
-    assert installed
+    system 'which', 'panamax'
     assert_equal 0, $?.exitstatus
-
-    system "#{prefix}/.panamax/panamax", "--version"
   end
 end
