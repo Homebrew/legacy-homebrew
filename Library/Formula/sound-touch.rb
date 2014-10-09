75124f7a5bb7692f695d82536d8ec032a5969bb6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-09 07:03:36 +0100

    sound-touch: add 1.8.0 bottle.
    Closes #33043.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/sound-touch.rb
+++ Library/Formula/sound-touch.rb
@@ -5,6 +5,13 @@ class SoundTouch < Formula
   url "http://www.surina.net/soundtouch/soundtouch-1.8.0.tar.gz"
   sha256 "3d4161d74ca25c5a98c69dbb8ea10fd2be409ba1a3a0bf81db407c4c261f166b"
 
+  bottle do
+    cellar :any
+    sha1 "ba626eb84b2e8e82b4e2b608534f13800da00994" => :mavericks
+    sha1 "9dbb7296e5b8ea20790d97ee43967614dba9b584" => :mountain_lion
+    sha1 "728d89303b4cd85cd95f1ad625007d164c1f64bb" => :lion
+  end
+
   option "with-integer-samples", "Build using integer samples? (default is float)"
 
   depends_on "autoconf" => :build
