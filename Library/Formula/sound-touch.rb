09d39a3d91d7888914b266bb4f8837e399a97b0c
Roy Shilkrot <roy.shil@gmail.com>
2014-10-09 01:41:55 -0400

    adding an option to sound touch
    adding the option to use integer type samples (rather than the default float) by exposing the ./configure option

--- Library/Formula/sound-touch.rb
+++ Library/Formula/sound-touch.rb
@@ -5,15 +5,20 @@ class SoundTouch < Formula
   url "http://www.surina.net/soundtouch/soundtouch-1.8.0.tar.gz"
   sha256 "3d4161d74ca25c5a98c69dbb8ea10fd2be409ba1a3a0bf81db407c4c261f166b"
 
+  option "with-integer-samples", "Build using integer samples? (default is float)"
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
 
   def install
     system "/bin/sh", "bootstrap"
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    args = ["--disable-dependency-tracking",
+            "--disable-silent-rules",
+            "--prefix=#{prefix}"]
+    args << "--enable-integer-samples" if build.with? "integer-samples"
+
+    system "./configure",*args
     system "make", "install"
   end
 end
