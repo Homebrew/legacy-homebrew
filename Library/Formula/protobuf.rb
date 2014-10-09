0b938d09f5d162bd5872f9b75a027dceb21a0dcb
Tim D. Smith <git@tim-smith.us>
2014-10-08 23:20:47 -0700

    protobuf: build with up-to-date dateutil

--- Library/Formula/protobuf.rb
+++ Library/Formula/protobuf.rb
@@ -21,6 +21,26 @@ class Protobuf < Formula
     build 2334
   end
 
+  resource "dateutil" do
+    url "https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.2.tar.gz"
+    sha1 "fbafcd19ea0082b3ecb17695b4cb46070181699f"
+  end
+
+  resource "pytz" do
+    url "https://pypi.python.org/packages/source/p/pytz/pytz-2014.7.tar.bz2"
+    sha1 "93f461ed9e3fb0e42becf8d7f09647daafc54d66"
+  end
+
+  resource "gflags" do
+    url "https://pypi.python.org/packages/source/p/python-gflags/python-gflags-2.0.tar.gz"
+    sha1 "1529a1102da2fc671f2a9a5e387ebabd1ceacbbf"
+  end
+
+  resource "google.apputils" do
+    url "https://pypi.python.org/packages/source/g/google-apputils/google-apputils-0.4.0.tar.gz"
+    sha1 "76a5757ede82f017c6250b46ce5f369d831ffb8f"
+  end
+
   def install
     # Don't build in debug mode. See:
     # https://github.com/Homebrew/homebrew/issues/9279
@@ -39,12 +59,42 @@ class Protobuf < Formula
     doc.install %w( editors examples )
 
     if build.with? 'python'
+      ENV.prepend_create_path "PYTHONPATH", lib/"python2.7/site-packages"
+      %w[dateutil pytz gflags].each do |r|
+        next if package_installed?("python", r)
+        resource(r).stage do
+          system "python", "setup.py", "install", "--prefix=#{prefix}",
+                 "--single-version-externally-managed", "--record=installed.txt"
+        end
+      end
+      unless package_installed?("python", "google.apputils")
+        resource("google.apputils").stage do
+          # dateutil >= 2 used to be Python 3 only, but it isn't anymore
+          # https://github.com/Homebrew/homebrew/issues/32571
+          # https://code.google.com/p/google-apputils-python/issues/detail?id=6
+          inreplace "setup.py", "python-dateutil>=1.4,<2", "python-dateutil>=1.4"
+          system "python", "setup.py", "install", "--prefix=#{prefix}",
+                 "--single-version-externally-managed", "--record=installed.txt"
+        end
+      end
       chdir 'python' do
         ENV.append_to_cflags "-I#{include}"
         ENV.append_to_cflags "-L#{lib}"
+
+        # Hack around namespace support; google-apputils installs a .pth file
+        # that helps setuptools find google.apputils even though "google" is
+        # a namespace package, not a regular package (i.e. is missing
+        # __init__.py). If this formula installs google-apputils, that
+        # .pth file won't be processed because, although lib/.../site-packages
+        # is in PYTHONPATH, python does not treat it as a special site-packages
+        # directory and therefore does not interpret .pth files.
+        touch lib/"python2.7/site-packages/google/__init__.py"
+
         system 'python', 'setup.py', 'build'
         system 'python', 'setup.py', 'install', '--cpp_implementation', "--prefix=#{prefix}",
                '--single-version-externally-managed', '--record=installed.txt'
+
+        rm lib/"python2.7/site-packages/google/__init__.py"
       end
     end
   end
@@ -53,5 +103,9 @@ class Protobuf < Formula
     Editor support and examples have been installed to:
       #{doc}
     EOS
+  end
+
+  def package_installed? python, module_name
+    quiet_system python, "-c", "import #{module_name}"
   end
 end
