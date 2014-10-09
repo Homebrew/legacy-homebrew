1c8a841dfe5c9b52f1ba4a4231e26a4f604bded8
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-09 22:48:35 +0100

    lua* improvements.
    Fix some issues with luarocks looking for the wrong version and cleanup
    the lua and lua51 formulae.
    
    Closes #32354.

--- Library/Formula/luarocks.rb
+++ Library/Formula/luarocks.rb
@@ -1,19 +1,10 @@
-require 'formula'
+require "formula"
 
 class Luarocks < Formula
   homepage "http://luarocks.org"
-
-  stable do
-    url "http://luarocks.org/releases/luarocks-2.2.0.tar.gz"
-    sha1 "e2de00f070d66880f3766173019c53a23229193d"
-
-    # Remove writability checks in the install script.
-    # Homebrew checks that its install targets are writable, or fails with
-    # appropriate messaging if not. The check that luarocks does has been
-    # seen to have false positives, so remove it.
-    # TODO: better document the false positive cases, or remove this patch.
-    patch :DATA
-  end
+  url "http://luarocks.org/releases/luarocks-2.2.0.tar.gz"
+  sha1 "e2de00f070d66880f3766173019c53a23229193d"
+  revision 1
 
   bottle do
     sha1 "eabd3d0f2bb7979ac831ce948e8d288569d2a0c8" => :mavericks
@@ -23,7 +14,7 @@ class Luarocks < Formula
 
   head "https://github.com/keplerproject/luarocks.git"
 
-  depends_on 'lua'
+  depends_on "lua"
 
   fails_with :llvm do
     cause "Lua itself compiles with llvm, but may fail when other software tries to link."
@@ -31,14 +22,18 @@ class Luarocks < Formula
 
   def install
     # Install to the Cellar, but direct modules to HOMEBREW_PREFIX
+    # Specify where the Lua is to avoid accidental conflict.
+    lua_prefix = Formula["lua"].opt_prefix
+
     args = ["--prefix=#{prefix}",
             "--rocks-tree=#{HOMEBREW_PREFIX}",
-            "--lua-version=5.2",
-            "--sysconfdir=#{etc}/luarocks"]
+            "--sysconfdir=#{etc}/luarocks",
+            "--with-lua=#{lua_prefix}",
+            "--lua-version=5.2"]
 
     system "./configure", *args
-    system "make"
-    system "make install"
+    system "make", "build"
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
@@ -50,45 +45,6 @@ class Luarocks < Formula
   end
 
   test do
-    opoo "Luarocks test script installs 'lpeg'"
-    system "#{bin}/luarocks", "install", "lpeg"
-    system "lua", "-llpeg", "-e", 'print ("Hello World!")'
+    system "#{bin}/luarocks", "install", "say"
   end
 end
-
-__END__
-diff --git a/src/luarocks/fs/lua.lua b/src/luarocks/fs/lua.lua
-index b261905..f396030 100644
---- a/src/luarocks/fs/lua.lua
-+++ b/src/luarocks/fs/lua.lua
-@@ -791,31 +791,7 @@ end
- -- @return boolean or (boolean, string): true on success, false on failure,
- -- plus an error message.
- function fs_lua.check_command_permissions(flags)
--   local root_dir = path.root_dir(cfg.rocks_dir)
--   local ok = true
--   local err = ""
--   for _, dir in ipairs { cfg.rocks_dir, root_dir } do
--      if fs.exists(dir) and not fs.is_writable(dir) then
--         ok = false
--         err = "Your user does not have write permissions in " .. dir
--         break
--      end
--   end
--   local root_parent = dir.dir_name(root_dir)
--   if ok and not fs.exists(root_dir) and not fs.is_writable(root_parent) then
--      ok = false
--      err = root_dir.." does not exist and your user does not have write permissions in " .. root_parent
--   end
--   if ok then
--      return true
--   else
--      if flags["local"] then
--         err = err .. " \n-- please check your permissions."
--      else
--         err = err .. " \n-- you may want to run as a privileged user or use your local tree with --local."
--      end
--      return nil, err
--   end
-+   return true
- end
