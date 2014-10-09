eda661f477c1f01d694e132cd48f51c47f3c0a4a
Seth Pellegrino <seth.pellegrino@jivesoftware.com>
2014-10-09 16:46:32 -0700

    nailgun 0.9.1 (add prefix patch)

--- Library/Formula/nailgun.rb
+++ Library/Formula/nailgun.rb
@@ -10,8 +10,15 @@ class Nailgun < Formula
     sha1 "d57ea0a6f6c1bb1b616c5b3b311b3726c6ff35ad"
   end
 
+  # The makefile is not prefix aware
+  patch :DATA
+
   def install
-    system "make"
+    args = %W[
+      CC=#{ENV.cc}
+      PREFIX=#{prefix}
+    ]
+    system "make", "install", *args
     bin.install "ng"
     resource("nailgun-jar").stage { libexec.install "nailgun-server-#{version}.jar" }
     bin.write_jar_script libexec/"nailgun-server-#{version}.jar", "ng-server", "-server"
@@ -19,10 +26,33 @@ class Nailgun < Formula
 
   test do
     t = Thread.new { system "ng-server 8765" }
-    sleep 0.2 # the server does not begin listening as fast as we can start a background process
+    sleep 1 # the server does not begin listening as fast as we can start a background process
     system "ng", "--nailgun-port", "8765", "ng-version"
     # ng-stop always returns a non-zero exit code even on successful exit
     `ng --nailgun-port 8765 ng-stop || true`
     t.join
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index 21b55e9..e4adfa4 100644
+--- a/Makefile
++++ b/Makefile
+@@ -10,13 +10,15 @@ WIN32_CC=/usr/bin/i586-mingw32msvc-gcc
+ CC=gcc
+ CFLAGS=-Wall -pedantic -s -O3
+ SRCDIR=nailgun-client
++PREFIX=/usr/local
+ 
+ ng: ${SRCDIR}/ng.c
+ 	@echo "Building ng client.  To build a Windows binary, type 'make ng.exe'"
+ 	${CC} ${CFLAGS} -o ng ${SRCDIR}/ng.c
+ 
+ install: ng
+-	install ng /usr/local/bin
++	install -d ${PREFIX}/bin
++	install ng ${PREFIX}/bin
+ 	
+ ng.exe: ${SRCDIR}/ng.c
+ 	${WIN32_CC} -o ng.exe ${SRCDIR}/ng.c -lwsock32 -O3 ${CFLAGS}
