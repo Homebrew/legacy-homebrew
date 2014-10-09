9da66456633a4f4c7f2bd100d19132fa180f4f4b
Seth Pellegrino <seth.pellegrino@jivesoftware.com>
2014-10-09 14:45:57 -0700

    nailgun 0.9.1 (zombie formula)
    As per #21880 nailgun was removed because it was woefully outdated and had moved to a new packaging format.
    This commit brings the nailgun formula back from the boneyard and updates it to 0.9.1.

---
+++ Library/Formula/nailgun.rb
@@ -0,0 +1,27 @@
+require "formula"
+
+class Nailgun < Formula
+  homepage "http://www.martiansoftware.com/nailgun/"
+  url "https://github.com/martylamb/nailgun/archive/nailgun-all-0.9.1.tar.gz"
+  sha1 "75f21504e8a6d41f263560b74c33188e91ec1a27"
+
+  resource "nailgun-jar" do
+    url "http://central.maven.org/maven2/com/martiansoftware/nailgun-server/0.9.1/nailgun-server-0.9.1.jar"
+    sha1 "d57ea0a6f6c1bb1b616c5b3b311b3726c6ff35ad"
+  end
+
+  def install
+    system "make"
+    bin.install "ng"
+    resource("nailgun-jar").stage { libexec.install "nailgun-server-#{version}.jar" }
+    bin.write_jar_script libexec/"nailgun-server-#{version}.jar", "ng-server", "-server"
+  end
+
+  test do
+    system "ng-server 8765 &"
+    sleep(0.1) # the server does not begin listening as fast as
+    system "ng --nailgun-port 8765 ng-version"
+    # ng-stop always returns a non-zero exit code even on successful exit
+    system "ng --nailgun-port 8765 ng-stop || true"
+  end
+end
