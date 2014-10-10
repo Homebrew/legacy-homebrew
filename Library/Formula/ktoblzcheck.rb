c32db7bdaaedbf94d070ac13c1f8c42bd79c3853
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-10 11:05:04 +0100

    ktoblzcheck: add 1.47 bottle.
    Closes #33073.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ktoblzcheck.rb
+++ Library/Formula/ktoblzcheck.rb
@@ -5,6 +5,12 @@ class Ktoblzcheck < Formula
   url "https://downloads.sourceforge.net/project/ktoblzcheck/ktoblzcheck-1.47.tar.gz"
   sha1 "bf099879916d22c9334ef94b4e782afecd319226"
 
+  bottle do
+    sha1 "7190e0fde6f0cbeb76b28981a7a6c497ed766455" => :mavericks
+    sha1 "fcb5cb779e4a70797fc979a99bb9a3a6305607f4" => :mountain_lion
+    sha1 "0627b12d557570f55d8c9d944199c08406d70732" => :lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
