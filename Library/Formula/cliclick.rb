bb8427c48a1a17736b8889c589034426de81b3ea
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-10 10:34:28 +0100

    cliclick: add 3.0.2 bottle.
    Closes #33072.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cliclick.rb
+++ Library/Formula/cliclick.rb
@@ -5,6 +5,13 @@ class Cliclick < Formula
   url "https://github.com/BlueM/cliclick/archive/3.0.2.tar.gz"
   sha1 "7e6dbdf772adf2e36c66dee51bd077a89fe574fa"
 
+  bottle do
+    cellar :any
+    sha1 "a56d6d6c95c5cb7f6641d4f6484e944542aaa6e5" => :mavericks
+    sha1 "b8b97526eb4ff4c38f025a418214e0a166b9758a" => :mountain_lion
+    sha1 "feca3a136c0ff6a464ddfec9e7d4b73f93739a45" => :lion
+  end
+
   def install
     system "make"
     bin.install "cliclick"
