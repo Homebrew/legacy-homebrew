78c39e6a7eb842e4a1bd3ce5aca6ebef08c85ec5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-10 04:16:37 +0100

    libgroove: add 4.2.1 bottle.

--- Library/Formula/libgroove.rb
+++ Library/Formula/libgroove.rb
@@ -5,6 +5,11 @@ class Libgroove < Formula
   url "https://github.com/andrewrk/libgroove/archive/4.2.1.tar.gz"
   sha1 "cdcf4109266bb5eb38d62945891d37d907c3ccd3"
 
+  bottle do
+    cellar :any
+    sha1 "7ebecaca18d390525c2d668df4ca84aad82b5033" => :mavericks
+  end
+
   depends_on :macos => :mavericks
   depends_on "cmake" => :build
   depends_on "ffmpeg" => "with-libvorbis"
