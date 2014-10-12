17b0d8b3fadd9d2b0c2c5d9f7cbfe7a00aceede3
Mitch Tishmack <mitch.tishmack@gmail.com>
2014-10-12 10:18:52 -0500

    Add an option to skip make test in perl formula.

--- Library/Formula/perl.rb
+++ Library/Formula/perl.rb
@@ -17,6 +17,7 @@ class Perl < Formula
   keg_only "OS X ships Perl and overriding that can cause unintended issues"
 
   option "with-dtrace", "Build with DTrace probes"
+  option 'no-test', 'Don\'t run make test'
 
   def install
     args = [
@@ -34,7 +35,7 @@ class Perl < Formula
 
     system "./Configure", *args
     system "make"
-    system "make", "test"
+    system "make", "test" unless build.include? 'no-test'
     system "make", "install"
   end
 
