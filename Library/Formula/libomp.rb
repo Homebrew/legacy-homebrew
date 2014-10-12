a5a14221f8e608db5f8afa41ab53176fa0a5d4c9
Sebastian Stenzel <sebastianstenzel@gmail.com>
2014-10-12 20:59:33 +0200

    support for 32bit, see https://github.com/Homebrew/homebrew/pull/33118#discussion_r18749144

--- Library/Formula/libomp.rb
+++ Library/Formula/libomp.rb
@@ -5,11 +5,19 @@ class Libomp < Formula
   url "https://www.openmprtl.org/sites/default/files/libomp_20140926_oss.tgz"
   sha1 "488ff3874eb5c971523534cb3c987bfb5ce3addb"
 
-  depends_on :arch => :x86_64
+  depends_on :arch => [:x86_64, :intel]
+
+  def intel_arch
+	if MacOS.prefer_64_bit?
+		"mac_32e"
+	else
+		"mac_32"
+	end
+  end
 
   def install
     system "make", "compiler=clang"
     include.install Dir["exports/common/include/*"]
-    lib.install "exports/mac_32e/lib.thin/libiomp5.dylib"
+    lib.install "exports/#{intel_arch}/lib.thin/libiomp5.dylib"
   end
 end
