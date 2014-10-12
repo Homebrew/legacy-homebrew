79042afa2255d4a8b1401abe97ebd834a92995e8
Makoto Sasaki <makoto7878@gmail.com>
2014-10-12 22:15:44 +0900

    pyenv 20141012

--- Library/Formula/pyenv.rb
+++ Library/Formula/pyenv.rb
@@ -3,8 +3,8 @@ require "formula"
 class Pyenv < Formula
   homepage "https://github.com/yyuu/pyenv"
   head "https://github.com/yyuu/pyenv.git"
-  url "https://github.com/yyuu/pyenv/archive/v20141011.tar.gz"
-  sha1 "953a87c8c716ed613af58a610b4c79f57ee9d742"
+  url "https://github.com/yyuu/pyenv/archive/v20141012.tar.gz"
+  sha1 "dd92b2614a05c5459adeccd9a953cf4211f29372"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
