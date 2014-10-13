37618f171035507c4bd7315e3dd0c79a82fc248c
David Christenson <david@the-david.com>
2014-10-13 01:09:17 -0600

    ragel: fix links
    Update homepage and download links to new location, change formatting
    of make command, change single quotes to double.

--- Library/Formula/ragel.rb
+++ Library/Formula/ragel.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class Ragel < Formula
-  homepage 'http://www.complang.org/ragel/'
-  url 'http://www.complang.org/ragel/ragel-6.8.tar.gz'
-  sha1 '95cabbcd52bd25d76c588ddf11e1fd242d7cbcc7'
+  homepage "http://www.colm.net/ragel/"
+  url "http://www.colm.net/wp-content/uploads/2014/10/ragel-6.8.tar.gz"
+  sha1 "95cabbcd52bd25d76c588ddf11e1fd242d7cbcc7"
 
-  resource 'pdf' do
-    url 'http://www.complang.org/ragel/ragel-guide-6.8.pdf'
-    sha1 'e57ee7f740dd395d4d5330949594a02c91ad0308'
+  resource "pdf" do
+    url "http://www.colm.net/wp-content/uploads/2014/10/ragel-guide-6.8.pdf"
+    sha1 "e57ee7f740dd395d4d5330949594a02c91ad0308"
   end
 
   # Fix compilation with recent clang, patch from upstream git repo
@@ -15,8 +15,8 @@ class Ragel < Formula
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
-    doc.install resource('pdf')
+    system "make", "install"
+    doc.install resource("pdf")
   end
 end
 
