246fb9c6b0190fd646a4bc4678980cbbdb2d3ce3
Sebastian Stenzel <sebastianstenzel@gmail.com>
2014-10-13 09:16:54 +0200

    - added test - added fails_with :gcc

--- Library/Formula/libomp.rb
+++ Library/Formula/libomp.rb
@@ -7,6 +7,20 @@ class Libomp < Formula
 
   depends_on :arch => :intel
 
+  fails_with :gcc  do
+    cause "libomp can only be built with clang."
+  end
+
+  fails_with :gcc_4_0 do
+    cause "libomp can only be built with clang."
+  end
+
+  ("4.3".."4.9").each do |n|
+    fails_with :gcc => n do
+      cause "libomp can only be built with clang."
+    end
+  end
+
   def intel_arch
     if MacOS.prefer_64_bit?
       "mac_32e"
@@ -19,5 +33,21 @@ class Libomp < Formula
     system "make", "compiler=clang"
     include.install Dir["exports/common/include/*"]
     lib.install "exports/#{intel_arch}/lib.thin/libiomp5.dylib"
+  end
+
+  test do
+    testfile = <<-EOS.undent
+      #include <omp.h>
+      #include <stdlib.h>
+
+      int main(void) {
+          omp_set_num_threads(2);
+          omp_get_num_threads();
+          return EXIT_SUCCESS;
+      }
+    EOS
+    (testpath/"test.c").write(testfile)
+    system "#{ENV.cc}", "-L#{lib}", "-liomp5", "-I#{include}", "test.c", "-o", "test"
+    system "./test"
   end
 end
