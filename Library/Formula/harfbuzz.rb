1c53272c5074238d6f24fba043e321195b97e6bb
moyogo <moyogo@gmail.com>
2014-10-15 14:54:01 +0100

    harfbuzz: bump revision for icu4c upgrade

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -4,6 +4,7 @@ class Harfbuzz < Formula
   homepage "http://www.freedesktop.org/wiki/Software/HarfBuzz"
   url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.35.tar.bz2"
   sha256 "0aa1a8aba6f502321cf6fef3c9d2c73dde48389c5ed1d3615a7691944c2a06ed"
+  revision 1
 
   bottle do
     cellar :any
