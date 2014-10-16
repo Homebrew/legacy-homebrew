2b4f3842201f4d45f11bf64b5d355a8efc954d30
Jack Nagel <jacknagel@gmail.com>
2014-10-15 21:17:25 -0500

    Exempt libtool and svn from the shadowed headers check

--- Library/Homebrew/formula_cellar_checks.rb
+++ Library/Homebrew/formula_cellar_checks.rb
@@ -104,6 +104,7 @@ module FormulaCellarChecks
   end
 
   def check_shadowed_headers
+    return if f.name == "libtool" || f.name == "subversion"
     return if f.keg_only? || !f.include.directory?
 
     files  = relative_glob(f.include, "**/*.h")
