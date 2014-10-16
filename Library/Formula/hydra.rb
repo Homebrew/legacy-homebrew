e3ab64d635ed850450f20c7f896b75f9b2a76aa8
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-16 21:20:44 +0100

    hydra: added openssl dep
    How didn’t I spot this one before now? Hydra uses OpenSSL. It also uses
    mysql, so that’s now stated. Added a mirror and stylistic changes as
    well.
    
    Closes #33230.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/hydra.rb
+++ Library/Formula/hydra.rb
@@ -1,9 +1,13 @@
-require 'formula'
+require "formula"
 
 class Hydra < Formula
-  homepage 'http://www.thc.org/thc-hydra/'
-  url 'http://www.thc.org/releases/hydra-8.0.tar.gz'
-  sha1 'd1a705985846caf77c291461f391a43457cc76e5'
+  homepage "https://www.thc.org/thc-hydra/"
+  url "https://www.thc.org/releases/hydra-8.0.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/h/hydra/hydra_8.0.orig.tar.gz"
+  sha1 "d1a705985846caf77c291461f391a43457cc76e5"
+  revision 1
+
+  head "https://github.com/vanhauser-thc/thc-hydra"
 
   bottle do
     cellar :any
@@ -13,12 +17,15 @@ class Hydra < Formula
     sha1 "a9682892cb112e109cac81dae9ae123aa2dd38e7" => :lion
   end
 
-  depends_on 'libssh' => :optional
+  depends_on "pkg-config" => :build
+  depends_on :mysql
+  depends_on "libssh" => :optional
+  depends_on "openssl"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     bin.mkpath
-    system "make all install"
+    system "make", "all", "install"
     share.install prefix/"man" # Put man pages in correct place
   end
 end
