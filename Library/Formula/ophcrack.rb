665974ac1a5708544bd351aa72239892779a8c10
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-16 22:54:36 +0100

    ophcrack: added openssl dep
    OpenSSL dependency and style changes. Added a very basic test - Sadly
    it can’t do much more than this without downloading rainbow tables,
    which can take significant time and bandwidth.
    
    Closes #33236.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ophcrack.rb
+++ Library/Formula/ophcrack.rb
@@ -1,16 +1,25 @@
-require 'formula'
+require "formula"
 
 class Ophcrack < Formula
-  homepage 'http://ophcrack.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/ophcrack/ophcrack/3.6.0/ophcrack-3.6.0.tar.bz2'
-  sha1 '8e39b8c013b3f2144b23e33abeeadbb81e4120ca'
+  homepage "http://ophcrack.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/ophcrack/ophcrack/3.6.0/ophcrack-3.6.0.tar.bz2"
+  mirror "https://mirrors.kernel.org/debian/pool/main/o/ophcrack/ophcrack_3.6.0.orig.tar.bz2"
+  sha1 "8e39b8c013b3f2144b23e33abeeadbb81e4120ca"
+  revision 1
+
+  depends_on "openssl"
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-gui",
+                          "--with-libssl=#{Formula["openssl"].opt_prefix}",
                           "--prefix=#{prefix}"
 
     system "make"
     system "make", "-C", "src", "install"
+  end
+
+  test do
+    system "#{bin}/ophcrack", "-h"
   end
 end
