5146befc99a92b6a1407e6356621cb22ddb31392
Jack Nagel <jacknagel@gmail.com>
2014-10-15 23:56:43 -0500

    dmd 2.066.0
    Closes #33198.

--- Library/Formula/dmd.rb
+++ Library/Formula/dmd.rb
@@ -1,53 +1,63 @@
-require 'formula'
+require "formula"
 
 class Dmd < Formula
-  homepage 'http://dlang.org'
-  url 'https://github.com/D-Programming-Language/dmd/archive/v2.065.0.tar.gz'
-  sha1 '15f67e9b088d599c4091f1844676d107e873e850'
+  homepage "http://dlang.org"
+  url "https://github.com/D-Programming-Language/dmd/archive/v2.066.0.tar.gz"
+  sha1 "4ad09f680b249bb10b014b61ccb791a24f7a84c6"
 
   bottle do
-    sha1 "581cd8093ffe9fca493f06996e1d95e538fbb99e" => :mavericks
-    sha1 "3dfec90ecfc970e05efea17d6f786a0a1fbbb322" => :mountain_lion
-    sha1 "68d88b84a7f3407e867a83d093bf649f99830e23" => :lion
   end
 
-  resource 'druntime' do
-    url 'https://github.com/D-Programming-Language/druntime/archive/v2.065.0.tar.gz'
-    sha1 '0118d9386b2d5f006381a5e4802f295132c8717b'
+  resource "druntime" do
+    url "https://github.com/D-Programming-Language/druntime/archive/v2.066.0.tar.gz"
+    sha1 "e1a5a29898127b4775a7b60c2534c3f4fc4387a9"
   end
 
-  resource 'phobos' do
-    url 'https://github.com/D-Programming-Language/phobos/archive/v2.065.0.tar.gz'
-    sha1 '2af606451ee5d651fea91f252e09411714f779df'
+  resource "phobos" do
+    url "https://github.com/D-Programming-Language/phobos/archive/v2.066.0.tar.gz"
+    sha1 "92385871df5883034055a20f8e8da5c398d11dd6"
   end
 
-  resource 'tools' do
-    url 'https://github.com/D-Programming-Language/tools/archive/v2.065.0.tar.gz'
-    sha1 '54b5855599e64d0efbfc1cb21f1a31ef9939f8be'
+  resource "tools" do
+    url "https://github.com/D-Programming-Language/tools/archive/v2.066.0.tar.gz"
+    sha1 "03384e9237c778e9afcecd10e756111fd4f323de"
   end
 
   def install
-    make_args = ["INSTALL_DIR=#{prefix}", "MODEL=#{Hardware::bits}", "-f", "posix.mak"]
+    make_args = ["INSTALL_DIR=#{prefix}", "MODEL=#{Hardware::CPU.bits}", "-f", "posix.mak"]
 
-    system "make", "install", "SYSCONFDIR=#{etc}", "TARGET_CPU=X86", "RELEASE=1", *make_args
+    system "make", "SYSCONFDIR=#{etc}", "TARGET_CPU=X86", "RELEASE=1", *make_args
 
-    share.install prefix/'man'
+    bin.install "src/dmd"
+    prefix.install "samples"
+    man.install Dir["docs/man/*"]
 
-    inreplace bin/'dmd.conf', "DFLAGS=-I%@P%/../../src/phobos -I%@P%/../../src/druntime/import -L-L%@P%/../lib",
-                              "DFLAGS=-I#{prefix}/import -L-L#{lib}"
+    conf = etc/"dmd.conf"
 
-    etc.install bin/'dmd.conf'
+    if conf.exist?
+      inreplace conf, /^DFLAGS=.+$/, "DFLAGS=-I#{include}/d2 -L-L#{lib}"
+    else
+      conf.write <<-EOS.undent
+        [Environment]
+        DFLAGS=-I#{include}/d2 -L-L#{lib}
+        EOS
+    end
 
     make_args.unshift "DMD=#{bin}/dmd"
 
-    (buildpath/'druntime').install resource('druntime')
-    (buildpath/'phobos').install resource('phobos')
+    (buildpath/"druntime").install resource("druntime")
+    (buildpath/"phobos").install resource("phobos")
 
-    system "make", "-C", "druntime", "install", *make_args
-    system "make", "-C", "phobos", "install", "VERSION=#{buildpath}/VERSION", *make_args
+    system "make", "-C", "druntime", *make_args
+    system "make", "-C", "phobos", "VERSION=#{buildpath}/VERSION", *make_args
 
-    resource('tools').stage do
-      inreplace 'posix.mak', 'install: $(TOOLS) $(CURL_TOOLS)', 'install: $(TOOLS)'
+    (include/"d2").install Dir["druntime/import/*"]
+    cp_r ["phobos/std", "phobos/etc"], include/"d2"
+    lib.install Dir["druntime/lib/*", "phobos/**/libphobos2.a"]
+
+
+    resource("tools").stage do
+      inreplace "posix.mak", "install: $(TOOLS) $(CURL_TOOLS)", "install: $(TOOLS)"
       system "make", "install", *make_args
     end
   end
