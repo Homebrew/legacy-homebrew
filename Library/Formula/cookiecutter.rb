e80d5129e1082135745580769d75c269879f11e3
Michael Joseph <michaeljoseph+github@gmail.com>
2014-10-16 23:03:56 +0200

    cookiecutter 0.7.2

--- Library/Formula/cookiecutter.rb
+++ Library/Formula/cookiecutter.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Cookiecutter < Formula
   homepage 'https://github.com/audreyr/cookiecutter'
-  url 'https://pypi.python.org/packages/source/c/cookiecutter/cookiecutter-0.7.1.tar.gz'
-  sha1 '278a615977a4b29cead14cc9ede07a8c7452864a'
+  url 'https://pypi.python.org/packages/source/c/cookiecutter/cookiecutter-0.7.2.tar.gz'
+  sha1 '127b3f755edfb022c4634483666cf5f9d085fa71'
 
   depends_on :python if MacOS.version <= :snow_leopard
 
