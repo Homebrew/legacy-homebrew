ecd55eff483f336a05bfd30d646a52be961019ff
Sebastian Stenzel <sebastianstenzel@gmail.com>
2014-10-17 21:35:05 +0200

    - using tarballs instead of git - checksums

--- Library/Formula/clang-omp.rb
+++ Library/Formula/clang-omp.rb
@@ -2,16 +2,19 @@ require "formula"
 
 class ClangOmp < Formula
   homepage "http://clang-omp.github.io/"
-  url "https://github.com/clang-omp/llvm", :using => :git, :tag => "1013141148"
+  url "https://github.com/clang-omp/llvm/archive/1013141148.tar.gz"
+  sha1 "84b67237aa3a5603143eb22e967b8b35f127bc4e"
 
   depends_on "cmake" => :build
 
   resource "compiler-rt" do
-    url "https://github.com/clang-omp/compiler-rt", :using => :git, :tag => "1013141148"
+    url "https://github.com/clang-omp/compiler-rt/archive/1013141148.tar.gz"
+    sha1 "e85548d5b7d0f99d9699af7d17f2dbac1674b19d"
   end
 
   resource "clang" do
-    url "https://github.com/clang-omp/clang", :using => :git, :branch => "clang-omp", :tag => "1013141148"
+    url "https://github.com/clang-omp/clang/archive/1013141148.tar.gz"
+    sha1 "e066330a9fc699b9c539eeab4090c998b3f78214"
   end
 
   def install
