c8561b1e6ad4e00ea72ace2b1c40a930fcee4062
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-17 09:46:34 +0100

    cookiecutter: add 0.7.2 bottle.
    Closes #33237.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cookiecutter.rb
+++ Library/Formula/cookiecutter.rb
@@ -5,6 +5,13 @@ class Cookiecutter < Formula
   url 'https://pypi.python.org/packages/source/c/cookiecutter/cookiecutter-0.7.2.tar.gz'
   sha1 '127b3f755edfb022c4634483666cf5f9d085fa71'
 
+  bottle do
+    cellar :any
+    sha1 "f0ac7ce389eedcdc2e4b67a5e2fb71747c45adf7" => :mavericks
+    sha1 "c8425e83e01c889d090cc6596b5c9900fe53d4bb" => :mountain_lion
+    sha1 "d20dd767a6fec5a5e305d31eef2f91cf1182ced3" => :lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
