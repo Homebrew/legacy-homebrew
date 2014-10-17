a059918063a0f6df82661c873a342914feaff125
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-17 23:47:19 +0100

    gnutls 3.3.9
    “I’ve just [released gnutls 3.3.9](http://article.gmane.org/gmane.comp.encryption.gpg.gnutls.devel/7728). This is a bug-fix release on the stable branch. With this release the 3.3.x branch becomes the stable branch.”

--- Library/Formula/gnutls.rb
+++ Library/Formula/gnutls.rb
@@ -1,11 +1,13 @@
-require 'formula'
+require "formula"
 
 # GnuTLS has previous, current, and next stable branches, we use current.
+# From 3.4.0 GnuTLS will be permanently disabling SSLv3. Every brew uses will need a revision with that.
+# http://nmav.gnutls.org/2014/10/what-about-poodle.html
 class Gnutls < Formula
-  homepage 'http://gnutls.org'
-  url 'ftp://ftp.gnutls.org/gcrypt/gnutls/v3.2/gnutls-3.2.17.tar.xz'
-  mirror 'http://mirrors.dotsrc.org/gcrypt/gnutls/v3.2/gnutls-3.2.17.tar.xz'
-  sha1 'c48b02912c5dc77b627f1f17dcc05c2be1d59b0f'
+  homepage "http://gnutls.org"
+  url "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-3.3.9.tar.xz"
+  mirror "http://mirrors.dotsrc.org/gcrypt/gnutls/v3.3/gnutls-3.3.9.tar.xz"
+  sha256 "39166de5293a9d30ef1cd0a4d97f01fdeed7d7dbf8db95392e309256edcb13c1"
 
   bottle do
     cellar :any
@@ -15,12 +17,13 @@ class Gnutls < Formula
     sha1 "d5cef16200d281b8ac69957d2f4c0c436b2c766e" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libtasn1'
-  depends_on 'gmp'
-  depends_on 'nettle'
-  depends_on 'guile' => :optional
-  depends_on 'p11-kit' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "xz"
+  depends_on "libtasn1"
+  depends_on "gmp"
+  depends_on "nettle"
+  depends_on "guile" => :optional
+  depends_on "p11-kit" => :optional
 
   fails_with :llvm do
     build 2326
@@ -38,19 +41,20 @@ class Gnutls < Formula
       --prefix=#{prefix}
       --sysconfdir=#{etc}
       --with-default-trust-store-file=#{etc}/openssl/cert.pem
+      --disable-heartbeat-support
     ]
 
-    if build.with? 'guile'
-      args << '--enable-guile'
-      args << '--with-guile-site-dir=no'
+    if build.with? "guile"
+      args << "--enable-guile"
+      args << "--with-guile-site-dir=no"
     end
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
 
     # certtool shadows the OS X certtool utility
-    mv bin+'certtool', bin+'gnutls-certtool'
-    mv man1+'certtool.1', man1+'gnutls-certtool.1'
+    mv bin+"certtool", bin+"gnutls-certtool"
+    mv man1+"certtool.1", man1+"gnutls-certtool.1"
   end
 
   def post_install
