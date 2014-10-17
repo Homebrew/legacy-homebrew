7988a4ad81e050a0117265bdb17001e28a12af70
Michael Joseph <michaeljoseph+github@gmail.com>
2014-10-17 10:11:01 +0200

    Tarball sha, not git sha

--- Library/Formula/cookiecutter.rb
+++ Library/Formula/cookiecutter.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Cookiecutter < Formula
   homepage 'https://github.com/audreyr/cookiecutter'
   url 'https://pypi.python.org/packages/source/c/cookiecutter/cookiecutter-0.7.2.tar.gz'
-  sha1 '58b318de167b8812bcffd94943a6640f4ab92c22'
+  sha1 '127b3f755edfb022c4634483666cf5f9d085fa71'
 
   depends_on :python if MacOS.version <= :snow_leopard
 
