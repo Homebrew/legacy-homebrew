237b5b076a50bc0c3f0e79021a07b4c9b80c04ae
Carbo Kuo <byvoid@byvoid.com>
2014-10-18 18:51:03 +0200

    opencc 1.0.1
    Closes #33318.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/opencc.rb
+++ Library/Formula/opencc.rb
@@ -2,14 +2,17 @@ require 'formula'
 
 class Opencc < Formula
   homepage 'https://github.com/BYVoid/OpenCC'
-  url 'https://opencc.googlecode.com/files/opencc-0.4.3.tar.gz'
-  sha1 '4677e63fa36fb2b92a52c01df2acc83664aaf882'
+  url 'http://dl.bintray.com/byvoid/opencc/opencc-1.0.1.tar.gz'
+  sha1 'e5cdedef5d9d96f5046a0f44f3004611330ded4a'
 
   depends_on 'cmake' => :build
 
+  needs :cxx11
+
   def install
+    ENV.cxx11
     args = std_cmake_args
-    args << '-DENABLE_GETTEXT:BOOL=OFF'
+    args << '-DBUILD_DOCUMENTATION:BOOL=OFF'
     system 'cmake', '.', *args
     system 'make'
     system 'make install'
