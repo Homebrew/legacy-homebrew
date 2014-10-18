6012cad533563cf009949b100488b179c0d553aa
Scott Reeves <cottser@mailworks.org>
2014-10-18 11:38:04 -0400

    virtualhost.sh 1.32
    Closes #33312.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/virtualhost.sh.rb
+++ Library/Formula/virtualhost.sh.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class VirtualhostSh < Formula
   homepage 'https://github.com/virtualhost/virtualhost.sh'
-  url 'https://github.com/virtualhost/virtualhost.sh/archive/1.31.tar.gz'
-  sha1 '25954027dbed14843123bea4efd498cd2abfc4a0'
+  url 'https://github.com/virtualhost/virtualhost.sh/archive/1.32.tar.gz'
+  sha1 '6f62cf860a260db876bd1ce21fbc523c0c0b6ea1'
 
   head 'https://github.com/virtualhost/virtualhost.sh.git'
 
