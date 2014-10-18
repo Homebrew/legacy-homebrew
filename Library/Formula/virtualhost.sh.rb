645af816a042c9d6e6a6c4d027083b3b46916527
Scott Reeves <cottser@mailworks.org>
2014-10-18 14:31:35 -0400

    Fix sha1 for virtualhost.sh 1.32

--- Library/Formula/virtualhost.sh.rb
+++ Library/Formula/virtualhost.sh.rb
@@ -3,7 +3,7 @@ require 'formula'
 class VirtualhostSh < Formula
   homepage 'https://github.com/virtualhost/virtualhost.sh'
   url 'https://github.com/virtualhost/virtualhost.sh/archive/1.32.tar.gz'
-  sha1 '6f62cf860a260db876bd1ce21fbc523c0c0b6ea1'
+  sha1 'dc307937e10c2a5948c59ff2ece6495763415b77'
 
   head 'https://github.com/virtualhost/virtualhost.sh.git'
 
