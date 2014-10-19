6d867cd0c53818e17f2ec13a0a8782336ca2c669
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-19 13:17:31 +0100

    ufraw: add 0.20 bottle.
    Closes #33346.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ufraw.rb
+++ Library/Formula/ufraw.rb
@@ -5,6 +5,12 @@ class Ufraw < Formula
   url 'https://downloads.sourceforge.net/project/ufraw/ufraw/ufraw-0.20/ufraw-0.20.tar.gz'
   sha1 'f2f456c6ec5ab128433502eae05b82a7ed636f3e'
 
+  bottle do
+    sha1 "114f5807e129d36cf97186160ead6003adb5b6fa" => :yosemite
+    sha1 "3604acf00a45b6e569b264d2e482626214731ca8" => :mavericks
+    sha1 "2ee9849f3b51f2702f67a6626539fcae5a9a6f04" => :mountain_lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libpng'
   depends_on 'dcraw'
