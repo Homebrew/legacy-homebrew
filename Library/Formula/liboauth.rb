9ff30cffc7559a4914bde838eebbbda0717815ea
Tim D. Smith <git@tim-smith.us>
2014-10-21 10:04:20 -0700

    liboauth: use Homebrew openssl

--- Library/Formula/liboauth.rb
+++ Library/Formula/liboauth.rb
@@ -4,6 +4,7 @@ class Liboauth < Formula
   homepage 'http://liboauth.sourceforge.net'
   url 'https://downloads.sourceforge.net/project/liboauth/liboauth-1.0.3.tar.gz'
   sha1 '791dbb4166b5d2c843c8ff48ac17284cc0884af2'
+  revision 1
 
   bottle do
     cellar :any
@@ -12,6 +13,8 @@ class Liboauth < Formula
     sha1 "80ef673091befb4c94efc94365271f152ac2fc0e" => :mavericks
     sha1 "f58f7890a6b3165fd4254ef921c0e85eeb73f810" => :mountain_lion
   end
+
+  depends_on "openssl"
 
   def install
     system "./configure", "--disable-dependency-tracking",
