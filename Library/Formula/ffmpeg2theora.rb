0efed547b19d6c792dccc35d551fc4ec22314742
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-21 04:33:38 +0100

    ffmpeg2theora: head fix
    The HEAD has moved (… About 2 years ago). Other changes to reflect the
    new HEAD, style tweaks, and an exceptionally simple test added.

--- Library/Formula/ffmpeg2theora.rb
+++ Library/Formula/ffmpeg2theora.rb
@@ -1,9 +1,20 @@
-require 'formula'
+require "formula"
 
 class Ffmpeg2theora < Formula
-  homepage 'http://v2v.cc/~j/ffmpeg2theora/'
-  url 'http://v2v.cc/~j/ffmpeg2theora/downloads/ffmpeg2theora-0.29.tar.bz2'
-  sha1 '7e78c5ddb8740b33a6ae4c9da76047bd8e662791'
+  homepage "http://v2v.cc/~j/ffmpeg2theora/"
+  revision 1
+
+  stable do
+    url "http://v2v.cc/~j/ffmpeg2theora/downloads/ffmpeg2theora-0.29.tar.bz2"
+    sha1 "7e78c5ddb8740b33a6ae4c9da76047bd8e662791"
+
+    # Fixes build with ffmpeg 2.x by removing use of deprecated constant
+    patch do
+      url "http://git.xiph.org/?p=ffmpeg2theora.git;a=patch;h=d3435a6a83dc656379de9e6523ecf8d565da6ca6"
+      sha1 "5a3e48c386ac077ab58afa6c49631c88f8f85929"
+    end
+  end
+
   bottle do
     cellar :any
     sha1 "7e6d4c59218e8ada99549aa6298565fcdb91a3e7" => :mavericks
@@ -11,27 +22,25 @@ class Ffmpeg2theora < Formula
     sha1 "c5c51465abace7e4b28a8eff91547820c99418b9" => :lion
   end
 
-  revision 1
+  head "git://git.xiph.org/ffmpeg2theora.git"
 
-  head 'http://svn.xiph.org/trunk/ffmpeg2theora'
+  depends_on "pkg-config" => :build
+  depends_on "scons" => :build
+  depends_on "ffmpeg"
+  depends_on "libogg"
+  depends_on "libvorbis"
+  depends_on "theora"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'scons' => :build
-  depends_on 'ffmpeg'
-  depends_on 'libkate' => :optional
-  depends_on 'libogg'
-  depends_on 'libvorbis'
-  depends_on 'theora'
-
-  # Fixes build with ffmpeg 2.x by removing use of deprecated constant
-  patch do
-    url "http://git.xiph.org/?p=ffmpeg2theora.git;a=patch;h=d3435a6a83dc656379de9e6523ecf8d565da6ca6"
-    sha1 "5a3e48c386ac077ab58afa6c49631c88f8f85929"
-  end
+  # ffmpeg2theora can't find our libkate because we don't build liboggkate.
+  # Either fix this here or upstream this issue.
+  # Have removed that optional dependency here temporarily.
 
   def install
     args = ["prefix=#{prefix}", "mandir=PREFIX/share/man"]
-    args << "libkate=1" if Formula['libkate'].installed?
     scons "install", *args
+  end
+
+  test do
+    system "#{bin}/ffmpeg2theora", "--help"
   end
 end
