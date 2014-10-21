8b07f109151afdbe8039102dad61d356962e4923
0xbb <account@bierbaumer.net>
2014-10-21 22:40:44 +0200

    libjson-rpc-cpp 0.4.1
    Closes #33453.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ Library/Formula/libjson-rpc-cpp.rb
@@ -0,0 +1,20 @@
+class LibjsonRpcCpp < Formula
+  homepage "https://github.com/cinemast/libjson-rpc-cpp"
+  url "https://github.com/cinemast/libjson-rpc-cpp/archive/v0.4.1.tar.gz"
+  sha1 "199c5f262c41e51ff1d4f3625fb25543d97e852b"
+
+  depends_on "cmake" => :build
+  depends_on "argtable"
+  depends_on "jsoncpp"
+  depends_on "libmicrohttpd"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/jsonrpcstub", "-h"
+  end
+end
