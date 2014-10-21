64b23de2290ea27014b9e9f44b34b0fbc0d6e282
dwihn0r <dwihn0r@users.noreply.github.com>
2014-10-21 09:06:06 +0200

    osxfuse 2.7.2
    Closes #33424.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/osxfuse.rb
+++ Library/Formula/osxfuse.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Osxfuse < Formula
   homepage "http://osxfuse.github.io"
-  url "https://github.com/osxfuse/osxfuse.git", :tag => "osxfuse-2.7.1"
+  url "https://github.com/osxfuse/osxfuse.git", :tag => "osxfuse-2.7.2"
 
   head "https://github.com/osxfuse/osxfuse.git", :branch => "osxfuse-2"
 
