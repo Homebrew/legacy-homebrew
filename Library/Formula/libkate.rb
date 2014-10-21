119a6efafad8241fbcb0f95aef0b998fc741ae2f
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-21 20:13:57 +0100

    libkate: dependency fixes
    Does anyone actually use this package? I’m amazed nobody has noticed
    it’s been really quite broken for a while. It hasn’t been building the
    ogg libraries because it finds that dependency through pkg-config which
    we hadn’t specified as a dependency, and the only other thing that
    hooks into libkate looks for those ogg libraries and fails to find
    those consequently. This also adds optional dependencies for
    documentation, examples and the GUI for libkate. Libogg also isn’t
    really a recommended dependency, if we’re going with libpng being
    mandatory libogg should be as well.
    
    Added a mirror and a simple test as well.

--- Library/Formula/libkate.rb
+++ Library/Formula/libkate.rb
@@ -1,9 +1,10 @@
-require 'formula'
+require "formula"
 
 class Libkate < Formula
-  homepage 'http://code.google.com/p/libkate/'
-  url 'https://libkate.googlecode.com/files/libkate-0.4.1.tar.gz'
-  sha1 '87fd8baaddb7120fb4d20b0a0437e44ea8b6c9d8'
+  homepage "https://code.google.com/p/libkate/"
+  url "https://libkate.googlecode.com/files/libkate-0.4.1.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/libk/libkate/libkate_0.4.1.orig.tar.gz"
+  sha1 "87fd8baaddb7120fb4d20b0a0437e44ea8b6c9d8"
   revision 1
 
   bottle do
@@ -13,8 +14,15 @@ class Libkate < Formula
     sha1 "8ed93646139c5ec85a9fb9efdab9fa4f161a62d6" => :mountain_lion
   end
 
-  depends_on 'libpng'
-  depends_on 'libogg' => :recommended
+  depends_on "pkg-config" => :build
+  depends_on "doxygen" => :build if build.with? "docs"
+  depends_on "oggz" if build.with? "examples"
+  depends_on "libpng"
+  depends_on "libogg"
+  depends_on "wxmac" => :optional
+
+  option "with-docs", "Build documentation"
+  option "with-examples", "Build example streams"
 
   fails_with :gcc do
     build 5666
@@ -24,7 +32,14 @@ class Libkate < Formula
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
+                          "--enable-shared",
+                          "--enable-static",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/katedec", "-V"
   end
 end
