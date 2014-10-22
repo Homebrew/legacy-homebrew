2364df1cdf605d1914437ccd667d53c98dde55c0
David Christenson <david@the-david.com>
2014-10-21 21:25:14 -0600

    md5sha1sum: use brewed OpenSSL
    Use homebrew's OpenSSL, remove explicit SSLINCPATH, change single quotes
    to double.
    
    Closes #33464.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/md5sha1sum.rb
+++ Library/Formula/md5sha1sum.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Md5sha1sum < Formula
-  homepage 'http://www.microbrew.org/tools/md5sha1sum/'
-  url 'http://www.microbrew.org/tools/md5sha1sum/md5sha1sum-0.9.5.tar.gz'
+  homepage "http://www.microbrew.org/tools/md5sha1sum/"
+  url "http://www.microbrew.org/tools/md5sha1sum/md5sha1sum-0.9.5.tar.gz"
   mirror "http://www.sourcefiles.org/Utilities/Console/M-P/md5sha1sum-0.9.5.tar.gz"
-  sha1 '84a46bfd2b49daa0a601a9c55b7d87c27e19ef87'
+  sha1 "84a46bfd2b49daa0a601a9c55b7d87c27e19ef87"
 
   bottle do
     cellar :any
@@ -13,10 +13,11 @@ class Md5sha1sum < Formula
     sha1 "11bae51a115c14688f7f57cf0903a829f8c28dde" => :lion
   end
 
-  conflicts_with 'polarssl', :because => 'both install conflicting binaries'
+  depends_on "openssl"
+
+  conflicts_with "polarssl", :because => "both install conflicting binaries"
 
   def install
-    ENV["SSLINCPATH"] = "#{MacOS.sdk_path}/usr/include"
     system "./configure", "--prefix=#{prefix}"
     system "make"
     bin.install 'md5sum'
@@ -25,11 +26,11 @@ class Md5sha1sum < Formula
   end
 
   test do
-    (testpath/'file.txt').write('This is a test file with a known checksum')
-    (testpath/'file.txt.sha1').write <<-EOS.undent
+    (testpath/"file.txt").write("This is a test file with a known checksum")
+    (testpath/"file.txt.sha1").write <<-EOS.undent
       52623d47c33ad3fac30c4ca4775ca760b893b963  file.txt
     EOS
-    system "#{bin}/sha1sum", '--check', 'file.txt.sha1'
+    system "#{bin}/sha1sum", "--check", "file.txt.sha1"
   end
 
 end
