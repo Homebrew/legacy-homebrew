b9178583860d02ab825725778e09719552626c3a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-22 15:41:53 +0100

    opus: add 1.1 bottle.

--- Library/Formula/opus.rb
+++ Library/Formula/opus.rb
@@ -5,6 +5,13 @@ class Opus < Formula
   url 'http://downloads.xiph.org/releases/opus/opus-1.1.tar.gz'
   sha1 '35005f5549e2583f5770590135984dcfce6f3d58'
 
+  bottle do
+    cellar :any
+    sha1 "5fb89890cfc8c254a6f13f9657a66da385887b2b" => :yosemite
+    sha1 "8ac2e93276b0df2f69c01d0b727fbff57a637e9e" => :mavericks
+    sha1 "a7e6ed2455861c41e021fec280b6fa28462341a2" => :mountain_lion
+  end
+
   head do
     url 'https://git.xiph.org/opus.git'
 
