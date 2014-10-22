4a45370f133a0ff692857bcdbf05df51a30cc36e
Stuart Gunter <stuart.gunter@gmail.com>
2014-10-22 13:17:20 +0100

    opscenter 5.0.1
    Added dependency on openssl to fix build issues

--- Library/Formula/opscenter.rb
+++ Library/Formula/opscenter.rb
@@ -5,6 +5,8 @@ class Opscenter < Formula
   url "http://downloads.datastax.com/community/opscenter-5.0.1.tar.gz"
   sha1 "ef23fdf2d4034d43e0397d0598311cfcacf0b1bd"
 
+  depends_on "openssl"
+
   def install
     rm_f Dir["bin/*.bat"]
 
