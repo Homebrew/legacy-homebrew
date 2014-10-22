1825b58ee6f025df7811860c43281081c7f8132b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-22 22:45:47 +0100

    lastpass-cli: add 0.3.0 bottle.
    Closes #33486.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/lastpass-cli.rb
+++ Library/Formula/lastpass-cli.rb
@@ -5,6 +5,13 @@ class LastpassCli < Formula
   url "https://github.com/lastpass/lastpass-cli/archive/v0.3.0.tar.gz"
   sha1 "a4491bc5d258899ead6c64d4f97d23af93e03ff9"
 
+  bottle do
+    cellar :any
+    sha1 "2dce40ba5321331bdcc1bfe06ae30c3240dcfbd8" => :yosemite
+    sha1 "952c4e58291145bc4bcfee02e299787f3f9e4d93" => :mavericks
+    sha1 "96f02cd6ce93b14b56a2a75412523624f20cb36e" => :mountain_lion
+  end
+
   depends_on "openssl"
   depends_on "pinentry" => :optional
 
