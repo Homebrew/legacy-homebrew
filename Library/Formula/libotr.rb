267eee8d319e29602675c2dde5e00db965b642be
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-22 06:02:53 +0100

    libotr: add 4.1.0 bottle.

--- Library/Formula/libotr.rb
+++ Library/Formula/libotr.rb
@@ -5,6 +5,13 @@ class Libotr < Formula
   url 'https://otr.cypherpunks.ca/libotr-4.1.0.tar.gz'
   sha1 'df30bc87a7a8f37678dd679d17fa1f9638ea247e'
 
+  bottle do
+    cellar :any
+    sha1 "e3a4f4ed938279c649562da5f47589cd260876af" => :yosemite
+    sha1 "2fddebe58059fe7dcc3bfebd862051c8c7b8f417" => :mavericks
+    sha1 "bbd9c6938ade7c5208c472aba7b65581eb9a210b" => :mountain_lion
+  end
+
   depends_on "libgcrypt"
 
   def install
