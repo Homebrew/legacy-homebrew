9ad3749f43217611dcc774ac3113a98858b0c487
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-22 23:59:40 +0100

    poco: add 1.4.7-all bottle.

--- Library/Formula/poco.rb
+++ Library/Formula/poco.rb
@@ -6,6 +6,13 @@ class Poco < Formula
   sha1 '778bd5fca9d4169a2dd83265bc07123bd0d12273'
   version '1.4.7-all'
 
+  bottle do
+    cellar :any
+    sha1 "5234c064ce6b045c647c2fe144eb4a37312cf7a1" => :yosemite
+    sha1 "23c4636a3c6fc634708999981dce3a37943eb715" => :mavericks
+    sha1 "cddefc836a5b38de7ad852f40f0208b95e1bb063" => :mountain_lion
+  end
+
   devel do
     url 'http://pocoproject.org/releases/poco-1.5.4/poco-1.5.4-all.tar.gz'
     sha1 'd7f14c49472c4f333fe99d43cac021e90d24b024'
