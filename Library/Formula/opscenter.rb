189c8355994d22c664b6d9e304200d6b00f84ffd
Stuart Gunter <stuart.gunter@gmail.com>
2014-10-22 10:52:00 +0100

    opscenter 5.0.1 (new formula)
    Provides a new formula to install version 5.0.1 (currently the latest version) of DataStax OpsCenter

---
+++ Library/Formula/opscenter.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Opscenter < Formula
+  homepage "http://www.datastax.com/docs/opscenter/index"
+  url "http://downloads.datastax.com/community/opscenter-5.0.1.tar.gz"
+  sha1 "ef23fdf2d4034d43e0397d0598311cfcacf0b1bd"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+
+    prefix.install_metafiles
+    libexec.install Dir["*"]
+
+    bin.write_exec_script libexec/"bin/opscenter"
+  end
+end
