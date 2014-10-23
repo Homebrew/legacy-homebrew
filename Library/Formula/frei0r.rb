ce52b9741fac63e2ee7f4b40963c27d4b4da30f3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-23 12:11:39 +0100

    frei0r: add 1.4 bottle.

--- Library/Formula/frei0r.rb
+++ Library/Formula/frei0r.rb
@@ -5,6 +5,13 @@ class Frei0r < Formula
   url 'https://files.dyne.org/.xsend.php?file=frei0r/releases/frei0r-plugins-1.4.tar.gz'
   sha1 '7995d06c5412b14fa3b05647084ca9d7a0c84faa'
 
+  bottle do
+    cellar :any
+    sha1 "dfbdc10c72316e888f6b0ecd3716d57fd7a8d1fc" => :yosemite
+    sha1 "457dc6f5d0786b960715da5ee5f3e426380c34c3" => :mavericks
+    sha1 "2b58483d2b4bb690b852674d3319664c91a62276" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
 
   def install
