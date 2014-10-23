58dfcc1052a8d1c69ae55216cc135a3843cb91b5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-23 05:54:41 +0100

    hardlink: add 0.3.0 bottle.

--- Library/Formula/hardlink.rb
+++ Library/Formula/hardlink.rb
@@ -5,6 +5,13 @@ class Hardlink < Formula
   url "http://jak-linux.org/projects/hardlink/hardlink_0.3.0.tar.xz"
   sha1 "abe9964d104b124b8dbebe6f354691e884f1fa2c"
 
+  bottle do
+    cellar :any
+    sha1 "620848862aa5692f5418c50158ff21ae64cbc3d7" => :yosemite
+    sha1 "1fc61fc57e8bdec0f49302ba75dab3e14b6b663f" => :mavericks
+    sha1 "ba185ba3f4bf55c9a25b2cf9bfd381e392563112" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gnu-getopt"
   depends_on "pcre"
