4204cf0b0bbf53d6a8fc3ff1dc014c123b2dc1d2
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-23 05:48:27 +0100

    hardlink 0.3.0
    Version bump.
    
    Closes #33496.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/hardlink.rb
+++ Library/Formula/hardlink.rb
@@ -2,14 +2,8 @@ require "formula"
 
 class Hardlink < Formula
   homepage "http://jak-linux.org/projects/hardlink/"
-  url "http://jak-linux.org/projects/hardlink/hardlink_0.2.0.tar.gz"
-  sha1 "6ba0fe26bbdf4feac6483550f647b4424e614cb9"
-
-  devel do
-    url "http://jak-linux.org/projects/hardlink/hardlink_0.3.0~rc2.tar.gz"
-    version "0.3.0_rc2"
-    sha1 "0a55a96a79839f668ca392cc0a11fa521044807d"
-  end
+  url "http://jak-linux.org/projects/hardlink/hardlink_0.3.0.tar.xz"
+  sha1 "abe9964d104b124b8dbebe6f354691e884f1fa2c"
 
   depends_on "pkg-config" => :build
   depends_on "gnu-getopt"
