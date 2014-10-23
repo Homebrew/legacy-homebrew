cad52682aa9921afaf9b03b39ec5563e7ab5eaee
leezii <admin@leezii.com>
2014-10-23 22:34:28 +0800

    update odb formula and remove libcutl formula.

--- Library/Formula/odb.rb
+++ Library/Formula/odb.rb
@@ -1,36 +1,42 @@
 require "formula"
 
 class Odb < Formula
-    homepage "http://www.codesynthesis.com/products/odb"
-    url "http://www.codesynthesis.com/download/odb/2.3/odb-2.3.0.tar.gz"
-    sha1 "53c851e3f3724b72d7c7a74c497c50c195729ad1"
+  homepage "http://www.codesynthesis.com/products/odb"
+  url "http://www.codesynthesis.com/download/odb/2.3/odb-2.3.0.tar.gz"
+  sha1 "53c851e3f3724b72d7c7a74c497c50c195729ad1"
 
-    depends_on 'gcc' => :build
-    depends_on 'libcutl' => :build
+  fails_with :clang
+  fails_with :gcc_4_0
+  fails_with :gcc
+  fails_with :llvm
+  
+  depends_on "gcc"
 
-    patch do
-        url "http://codesynthesis.com/~boris/tmp/odb/odb-2.3.0-gcc-4.9.0.patch"
-        sha1 "9d0fe9db9d8667c76cd6a1bb15e911560796b656"
-    end
+  resource "libcutl" do
+    url "http://www.codesynthesis.com/download/libcutl/1.8/libcutl-1.8.1.tar.gz"
+    sha1 "5411892a2959b6164321ebfb6e8e52255786b143"
+  end
+  
+  patch do
+    url "http://codesynthesis.com/~boris/tmp/odb/odb-2.3.0-gcc-4.9.0.patch"
+    sha1 "9d0fe9db9d8667c76cd6a1bb15e911560796b656"
+  end
 
-    def install
-        args = [
-            "CC=gcc-4.9",
-            "CPP=cpp-4.9",
-            "CXX=g++-4.9",
-            "CXXFLAGS=-fno-devirtualize"
-        ]
-        system "./configure", *args
-        system "make", "install"
-    end
+  def install
+    print HOMEBREW_PREFIX.class
+    resource("libcutl").stage{
+      system "./configure", "--prefix=#{prefix}/libcutl"
+      system "make", "install"
+    }
+    ENV.append "CXXFLAGS", "-fno-devirtualize -I#{prefix}/libcutl/include -L#{prefix}/libcutl/lib"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
 
-    test do
-        system "odb", "--help"
-    end
+    #HOMEBREW_PREFIX.install_symlink libexec/"odb.so"
+  end
 
-    def caveats; <<-EOS.undent
-        ODB is undel GPL v2 or NCUEL.
-        Please read http://codesynthesis.com/products/odb/license.xhtml before use.
-        EOS
-    end
+  test do
+    system "odb", "--help"
+  end
+
 end
