9a72a08cc4b6fa3c1f8ba1c5dfa0d35d191c9588
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-23 12:11:39 +0100

    globus-toolkit: add 5.2.5 bottle.

--- Library/Formula/globus-toolkit.rb
+++ Library/Formula/globus-toolkit.rb
@@ -6,6 +6,12 @@ class GlobusToolkit < Formula
   version '5.2.5'
   sha1 '2e39065e0c3970b660e081705915d45640d3c350'
 
+  bottle do
+    sha1 "b0a24a8d66dd36aa0ac48f0ae5dddfd02af22d6c" => :yosemite
+    sha1 "a9e19f50eed7f13d0200c107aabe20751071bc9b" => :mavericks
+    sha1 "1cbbe7a3d08711cbc5665e91558417d7b2028d3a" => :mountain_lion
+  end
+
   depends_on "libtool" => :run
 
   def install
