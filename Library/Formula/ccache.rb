534113795e7ab91b6a80d9191831b7481778e247
Andrei Polushin <polushin@gmail.com>
2014-10-24 12:25:40 +0700

    ccache 3.1.10
    Signed-off-by: Andrei Polushin <polushin@gmail.com>

--- Library/Formula/ccache.rb
+++ Library/Formula/ccache.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Ccache < Formula
   homepage 'http://ccache.samba.org/'
-  url 'http://samba.org/ftp/ccache/ccache-3.1.9.tar.bz2'
-  sha1 'e80a5cb7301e72f675097246d722505ae56e3cd3'
+  url 'http://samba.org/ftp/ccache/ccache-3.1.10.tar.bz2'
+  sha1 'f78154d526b98546c27e5ef480354f3ce021abe2'
 
   head do
     url 'https://github.com/jrosdahl/ccache.git'
