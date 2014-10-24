b74b81293b940c098348491fbf73cefb07fce791
Jack Nagel <jacknagel@gmail.com>
2014-10-23 20:36:56 -0500

    timbl 6.4.6

--- Library/Formula/timbl.rb
+++ Library/Formula/timbl.rb
@@ -1,16 +1,18 @@
-require 'formula'
+require "formula"
 
 class Timbl < Formula
-  homepage 'http://ilk.uvt.nl/timbl/'
-  url 'http://software.ticc.uvt.nl/timbl-6.4.5.tar.gz'
-  sha1 'de8e241dedb1fd413ba885c81f8ffa63a575f419'
+  homepage "http://ilk.uvt.nl/timbl/"
+  url "http://software.ticc.uvt.nl/timbl-6.4.6.tar.gz"
+  sha1 "9ab67a9a10830ba32eca8529accbc167ee5649a2"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libxml2'
-  depends_on 'ticcutils'
+  depends_on "pkg-config" => :build
+  depends_on "libxml2"
+  depends_on "ticcutils"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
   end
 end
