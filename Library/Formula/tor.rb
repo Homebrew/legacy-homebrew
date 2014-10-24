7a370ecbbcceb2d00183eab529e5bb478b737f27
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-24 18:50:45 +0100

    tor: update 0.2.5.10 bottle.
    Closes #33550.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/tor.rb
+++ Library/Formula/tor.rb
@@ -6,9 +6,9 @@ class Tor < Formula
   sha256 "b3dd02a5dcd2ffe14d9a37956f92779d4427edf7905c0bba9b1e3901b9c5a83b"
 
   bottle do
-    sha1 "5456718ac7f890f7afdf41bf9631f1e1dad8dbf3" => :mavericks
-    sha1 "cfe0bec72693402dea5b69268dc077f0c45dadf5" => :mountain_lion
-    sha1 "c46da85cd7b35c1a83a27b3ef88a8a73b99c6072" => :lion
+    sha1 "0bf6ef6985285bac9e67fbc78cef7ebb78844de2" => :yosemite
+    sha1 "6f4d92e5a77e1d3f3da94f1b45e4817c8ccecdf9" => :mavericks
+    sha1 "bae5ecb83486c16256d9d56b284bbf341c8d5a42" => :mountain_lion
   end
 
   depends_on "libevent"
