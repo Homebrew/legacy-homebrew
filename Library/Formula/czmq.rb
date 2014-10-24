88d4ef3cd382435663186818ac52044c033e5c3b
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-24 17:37:26 +0100

    fontforge: general fixes.
    Some fixes for the Fontforge, pending the next imminent stable release.
    Also bumping czmq and zeromq following a discussion [on
    this](https://github.com/fontforge/fontforge/issues/1837) this morning,
    during which I stumbled across a fix by removing the two and
    reinstalling them afresh, so I’m recommending we do that for everyone
    given the two libraries are tiny and it’ll flush out any configuration
    or bottle issues that could be lingering.

--- Library/Formula/czmq.rb
+++ Library/Formula/czmq.rb
@@ -1,29 +1,37 @@
-require 'formula'
+require "formula"
 
 class Czmq < Formula
-  homepage 'http://czmq.zeromq.org/'
-  url 'http://download.zeromq.org/czmq-2.2.0.tar.gz'
-  sha1 '2f4fd8de4cf04a68a8f6e88ea7657d8068f472d2'
+  homepage "http://czmq.zeromq.org/"
+  url "http://download.zeromq.org/czmq-2.2.0.tar.gz"
+  sha1 "2f4fd8de4cf04a68a8f6e88ea7657d8068f472d2"
+  revision 1
 
   head do
-    url 'https://github.com/zeromq/czmq.git'
+    url "https://github.com/zeromq/czmq.git"
 
-    depends_on 'autoconf' => :build
-    depends_on 'automake' => :build
-    depends_on 'libtool' => :build
-    depends_on 'pkg-config' => :build
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
   end
 
   option :universal
 
-  depends_on 'zeromq'
+  depends_on "pkg-config" => :build
+  depends_on "zeromq"
+  depends_on "libsodium" => :recommended
 
   def install
     ENV.universal_binary if build.universal?
+
+    args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
+
+    if build.stable?
+      args << "--with-libsodium" if build.with? "libsodium"
+    end
+
     system "./autogen.sh" if build.head?
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make install"
+    system "./configure", *args
+    system "make", "install"
     rm Dir["#{bin}/*.gsl"]
   end
 
