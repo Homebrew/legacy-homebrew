d6672bea35ce7b13e9e94b437b03c7df6d3ce8e7
chdiza <chdiza@gmail.com>
2014-10-25 02:58:50 -0400

    libgpg-error 1.17

--- Library/Formula/libgpg-error.rb
+++ Library/Formula/libgpg-error.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class LibgpgError < Formula
   homepage "http://www.gnupg.org/"
-  url "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.16.tar.bz2"
-  mirror "http://ftp.heanet.ie/mirrors/ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.16.tar.bz2"
-  sha1 "059c40a2b78c3ac2b4cbec0e0481faba5af332fe"
+  url "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.17.tar.bz2"
+  mirror "http://ftp.heanet.ie/mirrors/ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.17.tar.bz2"
+  sha1 "ba5858b2947e7272dd197c87bac9f32caf29b256"
 
   bottle do
     cellar :any
