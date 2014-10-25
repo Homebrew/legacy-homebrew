265cf86e6fe66370e308e7d8294b1aef22c28322
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-25 19:00:27 +0100

    bro 2.3.1
    Closes #33580.

--- Library/Formula/bro.rb
+++ Library/Formula/bro.rb
@@ -1,9 +1,19 @@
-require 'formula'
+require "formula"
 
 class Bro < Formula
-  homepage 'http://www.bro-ids.org/'
-  url 'http://www.bro-ids.org/downloads/release/bro-2.3.tar.gz'
-  sha1 '79397be0e351165d44047b044d29b5e6580532cc'
+  homepage "https://www.bro.org"
+  head "https://github.com/bro/bro.git"
+
+  stable do
+    url "https://www.bro.org/downloads/release/bro-2.3.1.tar.gz"
+    sha256 "ff32d21e335d2ddb3e2942527c3212de6ead4e7ffd6ac958497fa80e04e60800"
+
+    # Fix for ntohll errrors on Yosemite. Already in HEAD.
+    patch do
+      url "https://github.com/bro/bro/commit/121fcdbb5b9221.diff"
+      sha1 "985cede121964a96fe10c81f2239d1f0c137c517"
+    end
+  end
 
   bottle do
     sha1 "8aa75265faa2f23b73f7b27b7e495d79c60447d7" => :mavericks
@@ -11,12 +21,17 @@ class Bro < Formula
     sha1 "cf5fe821b85cfac5d8e4ebd86df37a7c75cf95cc" => :lion
   end
 
-  depends_on 'cmake' => :build
-  depends_on 'swig' => :build
-  depends_on 'geoip' => :recommended
+  depends_on "cmake" => :build
+  depends_on "swig" => :build
+  depends_on "geoip" => :recommended
+  depends_on "openssl"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "./configure", "--prefix=#{prefix}", "--with-openssl=#{Formula["openssl"].opt_prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/bro", "--version"
   end
 end
