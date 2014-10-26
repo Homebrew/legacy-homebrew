ce8075213b9e6988e3c5d47be7a6a93031600d3c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-26 11:44:15 +0000

    hashcash: add 1.22 bottle.
    Closes #33597.

--- Library/Formula/hashcash.rb
+++ Library/Formula/hashcash.rb
@@ -5,6 +5,12 @@ class Hashcash < Formula
   url "http://hashcash.org/source/hashcash-1.22.tgz"
   sha1 "0fa03c9f266026e505c0ab7b671ad93aef9310de"
 
+  bottle do
+    cellar :any
+    sha1 "c816296851be064a24cfd568acfa485731909877" => :yosemite
+    sha1 "9c6255cf6ca2b0fc0778468119272ecab94308b5" => :mavericks
+  end
+
   def install
     system "make", "install",
                    "PACKAGER=HOMEBREW",
