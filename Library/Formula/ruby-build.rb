222787b45b0225a1b04510e20277132ff2df8f20
Rustam Zagirov <rustam@zagirov.name>
2014-10-27 16:49:46 +0300

    ruby-build 20141027
    update ruby-build to v20141027
    
    Closes #33626.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,8 +3,8 @@ require "formula"
 class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  url "https://github.com/sstephenson/ruby-build/archive/v20141016.tar.gz"
-  sha1 "939d0f4921ea8ed8e559811cf5689be4ad2a7056"
+  url "https://github.com/sstephenson/ruby-build/archive/v20141027.tar.gz"
+  sha1 "bbd58ea04813a16a16aa39ad318245285616929c"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
