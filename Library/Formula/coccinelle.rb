e13e7c368f91e3d1950a1de725d6439ffd273fb5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-27 21:10:33 +0000

    coccinelle: add 1.0.0-rc21 bottle.

--- Library/Formula/coccinelle.rb
+++ Library/Formula/coccinelle.rb
@@ -5,6 +5,12 @@ class Coccinelle < Formula
   url 'http://coccinelle.lip6.fr/distrib/coccinelle-1.0.0-rc21.tgz'
   sha1 'edc008da552eb8f4ef7712fc99b4dc630ab6fb35'
 
+  bottle do
+    sha1 "907762ffd74c58637cfc1968c0812cf324ad8ac1" => :yosemite
+    sha1 "4d2f9a8af8dca8cce0a3e65c44afc70e38fe7218" => :mavericks
+    sha1 "ef567b84ce5bd101e3993e162855e4e88d1f3600" => :mountain_lion
+  end
+
   depends_on "objective-caml"
   depends_on "camlp4"
 
