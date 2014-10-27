8d1ae4ff8747c3bdf64494c27768d084de3f8022
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-27 18:56:09 +0000

    capnp: add 0.4.1 bottle.
    Closes #33620.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/capnp.rb
+++ Library/Formula/capnp.rb
@@ -5,6 +5,13 @@ class Capnp < Formula
   url 'http://capnproto.org/capnproto-c++-0.4.1.tar.gz'
   sha1 '18ce1a404c2bf68e6625e44927bfe6b67186cb15'
 
+  bottle do
+    cellar :any
+    sha1 "e980920f619a0682b1335550844b065894ed43ac" => :yosemite
+    sha1 "37a57c4a451723d7aa63dce453d35c65dce38a6e" => :mavericks
+    sha1 "8d7cc81fe1e7356eea9b422cc59ecbb1cef8f808" => :mountain_lion
+  end
+
   needs :cxx11
   option 'without-shared', 'Disable building shared library variant'
 
