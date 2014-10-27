3f9d3ab0ec5da292a862ce26bb53eb20f0693826
asaka <aisk1988@gmail.com>
2014-10-28 01:11:22 +0800

    bugfix for HOMEBREW_MAKE_JOBS

--- Library/Formula/fibjs.rb
+++ Library/Formula/fibjs.rb
@@ -10,7 +10,7 @@ class Fibjs < Formula
   depends_on "cmake" => :build
 
   def install
-    system "./build", "Release", "-j#{HOMEBREW_MAKE_JOBS}"
+    system "./build", "Release", "-j#{ENV["HOMEBREW_MAKE_JOBS"]}"
     bin.install "bin/Darwin_Release/fibjs"
   end
 
