5d23d962c1c1b0751ec438747a0113512df873ab
Anil Madhavapeddy <anil@recoil.org>
2014-10-27 19:02:08 +0000

    camlp4: add missing version, and add testcase

--- Library/Formula/camlp4.rb
+++ Library/Formula/camlp4.rb
@@ -1,6 +1,7 @@
 require "formula"
 
 class Camlp4 < Formula
+  version '4.02.1+1'
   homepage "https://github.com/ocaml/camlp4"
   url "https://github.com/ocaml/camlp4/archive/4.02.1+1.tar.gz"
   sha1 "7d0f879517887299167f1c3eefa8f4d266d69183"
@@ -17,4 +18,11 @@ class Camlp4 < Formula
     system "make", "all"
     system "make", "install"
   end
+
+  test do
+    (testpath/"foo.ml").write("type t = Homebrew | Rocks")
+    system "#{bin}/camlp4", "-parser", "OCaml", "-printer", "OCamlr", "foo.ml", "-o", (testpath/"foo.ml.out")
+    assert_equal "type t = [ Homebrew | Rocks ];", (testpath/"foo.ml.out").read.strip
+  end
+
 end
