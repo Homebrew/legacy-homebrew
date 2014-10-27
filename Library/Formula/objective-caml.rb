ee28f6fabaddfa45e047fb343ec19e4684a73f4d
Anil Madhavapeddy <anil@recoil.org>
2014-10-27 17:10:01 +0000

    OCaml 4.02: attempt to install camlp4
    This patch is currently broken, see the discussion in #33596

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -4,10 +4,18 @@ class ObjectiveCaml < Formula
   homepage "http://ocaml.org"
   url "http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.1.tar.gz"
   sha1 "6af8c67f2badece81d8e1d1ce70568a16e42313e"
+  revision 1
 
   head "http://caml.inria.fr/svn/ocaml/trunk", :using => :svn
 
-  depends_on :x11 => :recommended
+  option 'without-camlp4', 'Install without camlp4'
+  option 'without-x11', 'Install without the Graphics module'
+  depends_on :x11 => :optional
+
+  resource "camlp4" do
+    url "https://github.com/ocaml/camlp4/archive/4.02.1+1.tar.gz"
+    sha1 "7d0f879517887299167f1c3eefa8f4d266d69183"
+  end
 
   bottle do
     revision 5
@@ -17,17 +25,39 @@ class ObjectiveCaml < Formula
   end
 
   def install
-    system "./configure", "--prefix", HOMEBREW_PREFIX,
-                          "--mandir", man,
-                          "-cc", ENV.cc,
-                          "-with-debug-runtime",
-                          "-aspp", "#{ENV.cc} -c"
+    args = %W[
+      --prefix #{HOMEBREW_PREFIX}
+      --mandir #{man}
+      -cc #{ENV.cc}
+      -with-debug-runtime
+    ]
+    args << "-aspp" << "#{ENV.cc} -c"
+    args << "-no-graph" if build.without?("x11")
+
     ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
+    system "./configure", *args
     system "make world"
     system "make opt"
     system "make opt.opt"
     system "make", "PREFIX=#{prefix}", "install"
     (lib/"ocaml/site-lib").mkpath
+
+    resource('camlp4').stage do
+      # TODO: attempt to set the PATH to the Cellar-installed compiler.
+      # This currently fails since the compiler is a bytecode executable
+      # that expects to find ocamlrun in the `HOMEBREW_PREFIX` and not
+      # `prefix`.  It would be far easier to depend on an installed OCaml.
+      ENV.prepend_path "PATH", bin
+      # TODO: OCAMLLIB overrides the location of the standard library
+      # from the HOMEBREW_PREFIX to the Cellar location.
+      ENV["OCAMLLIB"] = "#{prefix}/lib/ocaml"
+      system "./configure", "--bindir=#{bin}",
+                            "--libdir=#{HOMEBREW_PREFIX}/lib/ocaml",
+                            "--pkgdir=#{HOMEBREW_PREFIX}/lib/ocaml/camlp4"
+      system "make all"
+      system "make install"
+    end if build.with? 'camlp4'
+
   end
 
   def post_install
