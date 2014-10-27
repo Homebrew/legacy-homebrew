e93a33796d90c2fd75852a49684424f688118aaa
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-27 18:21:52 +0000

    yash: add 2.36 bottle.
    Closes #33644.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/yash.rb
+++ Library/Formula/yash.rb
@@ -5,6 +5,13 @@ class Yash < Formula
   url "http://dl.sourceforge.jp/yash/60930/yash-2.36.tar.xz"
   sha1 "da034f893bf34e3851a93e8512f3fb027e59f0f5"
 
+  bottle do
+    cellar :any
+    sha1 "efa6ac2ec264e4efc16394f55839a89753631903" => :yosemite
+    sha1 "803be32d90a8ab8fd2812b342be94316c9727dd8" => :mavericks
+    sha1 "bffcef7392e6ddeccda732aea313ae3ea7f1e36d" => :mountain_lion
+  end
+
   def install
     system "sh", "./configure",
             "--prefix=#{prefix}",
