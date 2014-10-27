05df8213efe044d69e414f7127fb850ccf4849a3
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-27 19:37:11 +0000

    libmongoclient 2.6.5
    Closes #33647

--- Library/Formula/libmongoclient.rb
+++ Library/Formula/libmongoclient.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Libmongoclient < Formula
   homepage "http://www.mongodb.org"
-  url "https://github.com/mongodb/mongo-cxx-driver/archive/legacy-0.0-26compat-2.6.4.tar.gz"
-  sha1 "98510e610c7ad05b238149f6440c16c4c4cd21d9"
+  url "https://github.com/mongodb/mongo-cxx-driver/archive/legacy-0.0-26compat-2.6.5.tar.gz"
+  sha1 "1cc66854eac8cb79457ec68ba24153b321263430"
 
   head "https://github.com/mongodb/mongo-cxx-driver.git", :branch => "26compat"
 
@@ -22,6 +22,10 @@ class Libmongoclient < Formula
   else
     depends_on "boost"
   end
+
+  # Add 10.10 as a recognised OS X version choice.
+  # https://github.com/mongodb/mongo-cxx-driver/pull/185
+  patch :DATA
 
   def install
     ENV.cxx11 if build.cxx11?
@@ -48,3 +52,18 @@ class Libmongoclient < Formula
     scons *args
   end
 end
+
+__END__
+
+diff --git a/SConstruct b/SConstruct
+index ae0f270..24c8522 100644
+--- a/SConstruct
++++ b/SConstruct
+@@ -300,7 +300,7 @@ add_option('propagate-shell-environment',
+            0, False)
+
+ if darwin:
+-    osx_version_choices = ['10.6', '10.7', '10.8', '10.9']
++    osx_version_choices = ['10.6', '10.7', '10.8', '10.9', '10.10']
+     add_option("osx-version-min", "minimum OS X version to support", 1, True,
+                type = 'choice', default = osx_version_choices[0], choices = osx_version_choices)
