9797fb2ec841303e49e2cc28bd6bd89ad70729af
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-27 15:14:19 +0000

    mosquitto: update 1.3.5 bottle.

--- Library/Formula/mosquitto.rb
+++ Library/Formula/mosquitto.rb
@@ -6,10 +6,9 @@ class Mosquitto < Formula
   sha1 "2d30ffbf1c1b310581735e7ea10465e7c310e580"
 
   bottle do
-    revision 1
-    sha1 "5e8dfe80e2ea2af1af0e8ce3b79f59727b9ca82f" => :mavericks
-    sha1 "1b11a70a2c1fe7d9edabc1e3c7419669afafbd6b" => :mountain_lion
-    sha1 "1a36f985225e210c06d1e8d72ddaa7ab13c20337" => :lion
+    sha1 "85ed6685fb8efcf1aa909aaf50da8a843da819e0" => :yosemite
+    sha1 "a505a2071a526c7eb18ce82ca9cd64022f1b0294" => :mavericks
+    sha1 "a533fe55655eb71e3f104d36e52fecaaa1eb8ab0" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
