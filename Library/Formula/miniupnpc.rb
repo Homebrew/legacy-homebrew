9e8d476c23aae5f666fe0cac25abc6704ed71871
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-27 23:30:26 +0000

    miniupnpc 1.9.20141027
    Version bump.
    
    Closes #33660.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/miniupnpc.rb
+++ Library/Formula/miniupnpc.rb
@@ -1,14 +1,11 @@
-require 'formula'
+require "formula"
 
 class Miniupnpc < Formula
-  homepage 'http://miniupnp.tuxfamily.org'
-  url 'http://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-1.8.20131007.tar.gz'
-  sha1 'e2881e9583e90d6706abe0663330f46579efc64a'
+  homepage "http://miniupnp.tuxfamily.org"
+  url "http://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-1.9.20141027.tar.gz"
+  sha1 "b3c8e7f1e8d0a43ee274866730347e9871903ea1"
 
   def install
-    # Reported upstream:
-    # http://miniupnp.tuxfamily.org/forum/viewtopic.php?t=978
-    inreplace 'Makefile', "-Wl,-install_name,$(SONAME)", "-Wl,-install_name,$(INSTALLDIRLIB)/$(SONAME)"
     system "make", "INSTALLPREFIX=#{prefix}", "install"
   end
 end
