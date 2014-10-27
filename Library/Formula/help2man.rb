df2d50e7f114bcd029383d0c5d303a99e6482283
Brett Koonce <koonce@gmail.com>
2014-10-26 19:16:23 -0700

    help2man 1.46.4

--- Library/Formula/help2man.rb
+++ Library/Formula/help2man.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Help2man < Formula
   homepage "http://www.gnu.org/software/help2man/"
-  url "http://ftpmirror.gnu.org/help2man/help2man-1.46.3.tar.xz"
-  mirror "http://ftp.gnu.org/gnu/help2man/help2man-1.46.3.tar.xz"
-  sha256 "a669dc3baf2f9fcfcf9d55d8555d1c234bc3cabd5fbe8e36e0dea4a88c222942"
+  url "http://ftpmirror.gnu.org/help2man/help2man-1.46.4.tar.xz"
+  mirror "http://ftp.gnu.org/gnu/help2man/help2man-1.46.4.tar.xz"
+  sha256 "1ae7f15f53b0cc55b070ae49df2ee5caa942c71529054e157599427bba3c5633"
 
   bottle do
     cellar :any
