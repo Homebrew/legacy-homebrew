88fe4df704722500c5b76e52741ead6c82434d67
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-28 01:38:55 +0000

    miniupnpc: add 1.9.20141027 bottle.

--- Library/Formula/miniupnpc.rb
+++ Library/Formula/miniupnpc.rb
@@ -5,6 +5,13 @@ class Miniupnpc < Formula
   url "http://miniupnp.tuxfamily.org/files/download.php?file=miniupnpc-1.9.20141027.tar.gz"
   sha1 "b3c8e7f1e8d0a43ee274866730347e9871903ea1"
 
+  bottle do
+    cellar :any
+    sha1 "ee9cb081a8ebfda57d568bf7df403703a0b89ef9" => :yosemite
+    sha1 "97d743dbcbd8833d42abc7ed497ce7b86711cffd" => :mavericks
+    sha1 "3eef46a3903bd91d8e1443ac91ce53daa46f0326" => :mountain_lion
+  end
+
   def install
     system "make", "INSTALLPREFIX=#{prefix}", "install"
   end
