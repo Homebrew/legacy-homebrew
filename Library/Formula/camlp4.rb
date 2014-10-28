14e4cb4c6f2714554962e31a9a8ebfc307d2260b
Mike McQuaid <mike@mikemcquaid.com>
2014-10-28 08:09:16 +0000

    camlp4: general cleanup.

--- Library/Formula/camlp4.rb
+++ Library/Formula/camlp4.rb
@@ -1,18 +1,18 @@
 require "formula"
 
 class Camlp4 < Formula
-  version '4.02.1+1'
+  homepage "https://github.com/ocaml/camlp4"
+  url "https://github.com/ocaml/camlp4/archive/4.02.1+1.tar.gz"
+  sha1 "7d0f879517887299167f1c3eefa8f4d266d69183"
+  version "4.02.1+1"
+  head "https://github.com/ocaml/camlp4.git"
+
   bottle do
     cellar :any
     sha1 "0eb13f59de6a620e7663f3e865d0fae8d211abfa" => :yosemite
     sha1 "af24c6a58f16bd15875114bca320db8c3110d88d" => :mavericks
     sha1 "c0c7378b99240a65cec22fa505cc4711413e03ec" => :mountain_lion
   end
-
-  homepage "https://github.com/ocaml/camlp4"
-  url "https://github.com/ocaml/camlp4/archive/4.02.1+1.tar.gz"
-  sha1 "7d0f879517887299167f1c3eefa8f4d266d69183"
-  head "https://github.com/ocaml/camlp4.git"
 
   depends_on "objective-caml"
 
@@ -23,14 +23,15 @@ class Camlp4 < Formula
                           "--libdir=#{HOMEBREW_PREFIX}/lib/ocaml",
                           "--pkgdir=#{HOMEBREW_PREFIX}/lib/ocaml/camlp4"
     system "make", "all"
-    system "make", "install", "LIBDIR=#{prefix}/lib/ocaml",
-                              "PKGDIR=#{prefix}/lib/ocaml/camlp4"
+    system "make", "install", "LIBDIR=#{lib}/ocaml",
+                              "PKGDIR=#{lib}/lib/ocaml/camlp4"
   end
 
   test do
-    (testpath/"foo.ml").write("type t = Homebrew | Rocks")
-    system "#{bin}/camlp4", "-parser", "OCaml", "-printer", "OCamlr", "foo.ml", "-o", (testpath/"foo.ml.out")
-    assert_equal "type t = [ Homebrew | Rocks ];", (testpath/"foo.ml.out").read.strip
+    (testpath/"foo.ml").write "type t = Homebrew | Rocks"
+    system "#{bin}/camlp4", "-parser", "OCaml", "-printer", "OCamlr",
+                            "foo.ml", "-o", testpath/"foo.ml.out"
+    assert_equal "type t = [ Homebrew | Rocks ];",
+                 (testpath/"foo.ml.out").read.strip
   end
-
 end
