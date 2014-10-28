6ba215f498c82ebca11f30f3d3fd948a62dfe862
David Christenson <david@the-david.com>
2014-10-27 20:55:27 -0600

    libtool 2.4.3
    Update to latest stable release, use HTTPS on homepage and mirror,
    remove obsolete patch.
    
    Closes #33668.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libtool.rb
+++ Library/Formula/libtool.rb
@@ -4,10 +4,10 @@ require "formula"
 # This is not the same so as a result we must install this as glibtool.
 
 class Libtool < Formula
-  homepage "http://www.gnu.org/software/libtool/"
-  url "http://ftpmirror.gnu.org/libtool/libtool-2.4.2.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/libtool/libtool-2.4.2.tar.gz"
-  sha1 "22b71a8b5ce3ad86e1094e7285981cae10e6ff88"
+  homepage "https://www.gnu.org/software/libtool/"
+  url "http://ftpmirror.gnu.org/libtool/libtool-2.4.3.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/libtool/libtool-2.4.3.tar.xz"
+  sha1 "7e946bd07b846a8803bf4321e82cd6be8059c0ca"
 
   bottle do
     cellar :any
@@ -22,13 +22,8 @@ class Libtool < Formula
 
   option :universal
 
-  # Allow -stdlib= to pass through to linker
-  # http://git.savannah.gnu.org/gitweb/?p=libtool.git;a=commitdiff;h=8f975a1368594126e37d85511f1f96164e466d93
-  # https://trac.macports.org/ticket/32982
-
-  # Fix interpretation of MACOSX_DEPLOYMENT_TARGET on 10.10
-  # http://article.gmane.org/gmane.comp.gnu.libtool.patches/11730
-  # https://trac.macports.org/changeset/125325
+  # apply upstream patch to respect '--program-prefix'
+  # http://git.savannah.gnu.org/cgit/libtool.git/commit/?id=c77eea5f6c0592423d925131489cc7772e34cf0b
   patch :DATA
 
   def install
@@ -37,7 +32,9 @@ class Libtool < Formula
                           "--prefix=#{prefix}",
                           "--program-prefix=g",
                           "--enable-ltdl-install"
-    system "make install"
+    system "make"
+    ENV.j1
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
@@ -52,58 +49,98 @@ class Libtool < Formula
 end
 
 __END__
-diff --git a/libltdl/config/ltmain.sh b/libltdl/config/ltmain.sh
-index 63ae69d..9ae038c 100644
---- a/libltdl/config/ltmain.sh
-+++ b/libltdl/config/ltmain.sh
-@@ -5851,9 +5851,10 @@ func_mode_link ()
-       # -tp=*                Portland pgcc target processor selection
-       # --sysroot=*          for sysroot support
-       # -O*, -flto*, -fwhopr*, -fuse-linker-plugin GCC link-time optimization
-+      # -stdlib=*            select c++ std lib with clang
-       -64|-mips[0-9]|-r[0-9][0-9]*|-xarch=*|-xtarget=*|+DA*|+DD*|-q*|-m*| \
-       -t[45]*|-txscale*|-p|-pg|--coverage|-fprofile-*|-F*|@*|-tp=*|--sysroot=*| \
--      -O*|-flto*|-fwhopr*|-fuse-linker-plugin)
-+      -O*|-flto*|-fwhopr*|-fuse-linker-plugin|-stdlib=*)
-         func_quote_for_eval "$arg"
- 	arg="$func_quote_for_eval_result"
-         func_append compile_command " $arg"
-diff --git a/configure b/configure
-index a1ef3e3..782d28a 100755
---- a/configure
-+++ b/configure
-@@ -7765,7 +7765,7 @@ $as_echo "$lt_cv_ld_force_load" >&6; }
-       case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
- 	10.0,*86*-darwin8*|10.0,*-darwin[91]*)
- 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
--	10.[012]*)
-+	10.[012][,.]*)
- 	  _lt_dar_allow_undefined='${wl}-flat_namespace ${wl}-undefined ${wl}suppress' ;;
- 	10.*)
- 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
-diff --git a/libltdl/configure b/libltdl/configure
-index f18f272..fef1137 100755
---- a/libltdl/configure
-+++ b/libltdl/configure
-@@ -6978,7 +6978,7 @@ $as_echo "$lt_cv_ld_force_load" >&6; }
-       case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
- 	10.0,*86*-darwin8*|10.0,*-darwin[91]*)
- 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
--	10.[012]*)
-+	10.[012][,.]*)
- 	  _lt_dar_allow_undefined='${wl}-flat_namespace ${wl}-undefined ${wl}suppress' ;;
- 	10.*)
- 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
-diff --git a/libltdl/m4/libtool.m4 b/libltdl/m4/libtool.m4
-index 44e0ecf..4adcf73 100644
---- a/libltdl/m4/libtool.m4
-+++ b/libltdl/m4/libtool.m4
-@@ -1052,7 +1052,7 @@ _LT_EOF
-       case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
- 	10.0,*86*-darwin8*|10.0,*-darwin[[91]]*)
- 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
--	10.[[012]]*)
-+	10.[[012]][[,.]]*)
- 	  _lt_dar_allow_undefined='${wl}-flat_namespace ${wl}-undefined ${wl}suppress' ;;
- 	10.*)
- 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
+diff --git a/Makefile.am b/Makefile.am
+index 77561e1..cd7d61c 100644
+--- a/Makefile.am
++++ b/Makefile.am
+@@ -476,7 +476,7 @@ pkgltdl_files	= COPYING.LIB \
+ 		  ltdl.mk \
+ 		  slist.c
+ 
+-install-data-local: $(lt_Makefile_in)
++install-data-local: $(lt_Makefile_in) install-scripts-local
+ 	@$(NORMAL_INSTALL)
+ ## Don't install over the top of an old pkgdatadir
+ 	-rm -rf '$(DESTDIR)$(pkgdatadir)'/*
+@@ -508,9 +508,14 @@ install-data-local: $(lt_Makefile_in)
+ 	  echo " $(INSTALL_DATA) '$(ltdldir)/$$p' '$(DESTDIR)$(pkgdatadir)/$$p'"; \
+ 	  $(INSTALL_DATA) "$(ltdldir)/$$p" "$(DESTDIR)$(pkgdatadir)/$$p"; \
+ 	done
++	chmod a+x '$(DESTDIR)$(pkgdatadir)/configure'
++
++install-scripts-local: $(lt_Makefile_in)
+ ## Inline helper-scripts for installed libtoolize script
+-	$(SCRIPT_ENV) '$(inline_source)' libtoolize > '$(DESTDIR)$(bindir)/libtoolize';
+-	-chmod a+x '$(DESTDIR)$(pkgdatadir)/configure' '$(DESTDIR)$(bindir)/libtoolize'
++	@p=`echo libtoolize |sed -e '$(transform)'`; \
++	echo " $(SCRIPT_ENV) '$(inline_source)' libtoolize > '$(DESTDIR)$(bindir)/$$p'"; \
++	$(SCRIPT_ENV) '$(inline_source)' libtoolize > "$(DESTDIR)$(bindir)/$$p"; \
++	chmod a+x "$(DESTDIR)$(bindir)/$$p"
+ 
+ 
+ ## ------------- ##
+@@ -592,8 +597,9 @@ uninstall-hook:
+ 	  echo " rm -f '$(DESTDIR)$(aclocaldir)/$$f'"; \
+ 	  rm -f "$(DESTDIR)$(aclocaldir)/$$f"; \
+ 	done
+-	@echo " rm -f '$(DESTDIR)$(bindir)/libtoolize'"; \
+-	rm -f '$(DESTDIR)$(bindir)/libtoolize'
++	@p=`echo libtoolize |sed -e '$(transform)'`; \
++	echo " rm -f '$(DESTDIR)$(bindir)/$$p'"; \
++	rm -f "$(DESTDIR)$(bindir)/$$p"
+ 
+ 
+ ## ----------- ##
+diff --git a/Makefile.in b/Makefile.in
+index d49abac..d15b61e 100644
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -86,8 +86,9 @@ host_triplet = @host@
+ DIST_COMMON = $(srcdir)/libltdl/ltdl.mk INSTALL NEWS README AUTHORS \
+ 	ChangeLog $(srcdir)/Makefile.in $(srcdir)/Makefile.am \
+ 	$(top_srcdir)/configure $(am__configure_deps) \
+-	$(srcdir)/config-h.in $(top_srcdir)/libltdl/lt__dirent.c \
+-	$(top_srcdir)/libltdl/lt__strl.c $(top_srcdir)/libltdl/argz.c \
++	$(srcdir)/config-h.in $(top_srcdir)/libltdl/lt__strl.c \
++	$(top_srcdir)/libltdl/argz.c \
++	$(top_srcdir)/libltdl/lt__dirent.c \
+ 	$(top_srcdir)/build-aux/depcomp $(doc_libtool_TEXINFOS) \
+ 	$(top_srcdir)/build-aux/mdate-sh $(srcdir)/doc/version.texi \
+ 	$(srcdir)/doc/stamp-vti $(top_srcdir)/build-aux/texinfo.tex \
+@@ -2290,7 +2291,7 @@ $(libtool_1): $(ltmain_sh)
+ $(libtoolize_1): $(libtoolize_in)
+ 	$(AM_V_GEN)$(update_mans) libtoolize
+ 
+-install-data-local: $(lt_Makefile_in)
++install-data-local: $(lt_Makefile_in) install-scripts-local
+ 	@$(NORMAL_INSTALL)
+ 	-rm -rf '$(DESTDIR)$(pkgdatadir)'/*
+ 	@list='$(pkgmacro_files)'; for p in $$list; do \
+@@ -2318,8 +2319,13 @@ install-data-local: $(lt_Makefile_in)
+ 	  echo " $(INSTALL_DATA) '$(ltdldir)/$$p' '$(DESTDIR)$(pkgdatadir)/$$p'"; \
+ 	  $(INSTALL_DATA) "$(ltdldir)/$$p" "$(DESTDIR)$(pkgdatadir)/$$p"; \
+ 	done
+-	$(SCRIPT_ENV) '$(inline_source)' libtoolize > '$(DESTDIR)$(bindir)/libtoolize';
+-	-chmod a+x '$(DESTDIR)$(pkgdatadir)/configure' '$(DESTDIR)$(bindir)/libtoolize'
++	chmod a+x '$(DESTDIR)$(pkgdatadir)/configure'
++
++install-scripts-local: $(lt_Makefile_in)
++	@p=`echo libtoolize |sed -e '$(transform)'`; \
++	echo " $(SCRIPT_ENV) '$(inline_source)' libtoolize > '$(DESTDIR)$(bindir)/$$p'"; \
++	$(SCRIPT_ENV) '$(inline_source)' libtoolize > "$(DESTDIR)$(bindir)/$$p"; \
++	chmod a+x "$(DESTDIR)$(bindir)/$$p"
+ $(changelog): FORCE
+ 	$(AM_V_GEN)if test -d '$(srcdir)/.git'; then \
+ 	  $(gitlog_to_changelog) --amend=$(git_log_fix) \
+@@ -2366,8 +2372,9 @@ uninstall-hook:
+ 	  echo " rm -f '$(DESTDIR)$(aclocaldir)/$$f'"; \
+ 	  rm -f "$(DESTDIR)$(aclocaldir)/$$f"; \
+ 	done
+-	@echo " rm -f '$(DESTDIR)$(bindir)/libtoolize'"; \
+-	rm -f '$(DESTDIR)$(bindir)/libtoolize'
++	@p=`echo libtoolize |sed -e '$(transform)'`; \
++	echo " rm -f '$(DESTDIR)$(bindir)/$$p'"; \
++	rm -f "$(DESTDIR)$(bindir)/$$p"
+ 
+ $(testsuite): $(package_m4) $(TESTSUITE_AT) Makefile.am
+ 	$(AM_V_GEN)$(AUTOTEST) -I '$(srcdir)' -I '$(srcdir)/tests' $(TESTSUITE_AT) -o '$@'
