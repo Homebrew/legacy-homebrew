72abb6b8f8821e5046c9dfb6263c536c660821a1
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-10-29 18:20:27 +0900

    groonga 4.0.7
    - disable libstemmer because Homebrew does not have libstemmer
    - add lz4 optional dependency
    - add depends_on "openssl" bacause `brew audit groonga` complains using
      old system openssl

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Groonga < Formula
   homepage "http://groonga.org/"
-  url "http://packages.groonga.org/source/groonga/groonga-4.0.6.tar.gz"
-  sha1 "1ba7431cfca58dba7f5484d2c8013f24f6d8a322"
+  url "http://packages.groonga.org/source/groonga/groonga-4.0.7.tar.gz"
+  sha1 "b00c56c6d7318fe1a87f6f55366182ace4014a2a"
 
   bottle do
     sha1 "93dd670108b2ef9f219827eed3e158c4a8f6a4f6" => :mavericks
@@ -16,6 +16,8 @@ class Groonga < Formula
   depends_on "msgpack"
   depends_on "mecab" => :optional
   depends_on "mecab-ipadic" if build.with? "mecab"
+  depends_on "lz4" => :optional
+  depends_on "openssl"
 
   depends_on "glib" if build.include? "enable-benchmark"
 
@@ -27,10 +29,12 @@ class Groonga < Formula
       --with-zlib
       --disable-zeromq
       --with-mruby
+      --without-libstemmer
     ]
 
     args << "--enable-benchmark" if build.include? "enable-benchmark"
     args << "--with-mecab" if build.with? "mecab"
+    args << "--with-lz4" if build.with? "lz4"
 
     # ZeroMQ is an optional dependency that will be auto-detected unless we disable it
     system "./configure", *args
