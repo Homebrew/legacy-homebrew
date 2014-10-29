cc6815bbd086fa45df6542ecdfa5137388a45f4f
Sergejs Kovrovs <kovrov@gmail.com>
2014-10-29 11:24:47 -0700

    qbs 1.3.2
    Update qbs to v1.3.2

--- Library/Formula/qbs.rb
+++ Library/Formula/qbs.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Qbs < Formula
   homepage "http://qt-project.org/wiki/qbs"
-  url "http://download.qt-project.org/official_releases/qbs/1.3.0/qbs-1.3.0.src.tar.gz"
-  sha1 "27f2f0479fcb996e428bd4fa8379167e203d6d7a"
+  url "http://download.qt-project.org/official_releases/qbs/1.3.2/qbs-1.3.2.src.tar.gz"
+  sha1 "ce2d807c145e239d39e360521d62486eb1e3d108"
 
   bottle do
     cellar :any
