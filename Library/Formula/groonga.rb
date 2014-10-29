c8ae3980d29d0a2874a3d88db4ecb49b875fe010
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-10-29 18:20:27 +0900

    groonga 4.0.7

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Groonga < Formula
   homepage "http://groonga.org/"
-  url "http://packages.groonga.org/source/groonga/groonga-4.0.6.tar.gz"
-  sha1 "1ba7431cfca58dba7f5484d2c8013f24f6d8a322"
+  url "http://packages.groonga.org/source/groonga/groonga-4.0.7.tar.gz"
+  sha1 "b00c56c6d7318fe1a87f6f55366182ace4014a2a"
 
   bottle do
     sha1 "93dd670108b2ef9f219827eed3e158c4a8f6a4f6" => :mavericks
