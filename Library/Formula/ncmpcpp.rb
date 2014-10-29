ce9e28cfe4abb0edf796982f6ddb943712e86230
Jack Nagel <jacknagel@gmail.com>
2014-10-28 23:33:02 -0500

    ncmpcpp 0.6

--- Library/Formula/ncmpcpp.rb
+++ Library/Formula/ncmpcpp.rb
@@ -2,24 +2,8 @@ require 'formula'
 
 class Ncmpcpp < Formula
   homepage 'http://ncmpcpp.rybczak.net/'
-
-  stable do
-    url 'http://ncmpcpp.rybczak.net/stable/ncmpcpp-0.5.10.tar.bz2'
-    sha1 '5e34733e7fbaf2862f04fdf8af8195ce860a9014'
-
-    fails_with :clang do
-      cause "'itsTempString' is a private member of 'NCurses::basic_buffer<char>'"
-    end
-  end
-
-  devel do
-    url "http://ncmpcpp.rybczak.net/stable/ncmpcpp-0.6_beta4.tar.bz2"
-    sha1 "e995546831489e3629a961512365dc2d3f2f7310"
-    version "0.6-beta4"
-
-    depends_on 'boost'
-    depends_on 'readline'
-  end
+  url 'http://ncmpcpp.rybczak.net/stable/ncmpcpp-0.6.tar.bz2'
+  sha1 '7bbd63c6f17aa8cdf1190b19e2dc893df188da1c'
 
   head do
     url 'git://repo.or.cz/ncmpcpp.git'
@@ -27,20 +11,24 @@ class Ncmpcpp < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on 'boost'
-    depends_on 'readline'
   end
 
   depends_on 'pkg-config' => :build
   depends_on 'taglib'
   depends_on 'libmpdclient'
+  depends_on 'boost'
+  depends_on 'readline'
+
   depends_on 'fftw' if build.include? "visualizer"
 
   option 'outputs', 'Compile with mpd outputs control'
   option 'visualizer', 'Compile with built-in visualizer'
   option 'clock', 'Compile with optional clock tab'
 
+  needs :cxx11
+
   def install
+    ENV.cxx11
     ENV.append 'LDFLAGS', '-liconv'
     args = ["--disable-dependency-tracking",
             "--prefix=#{prefix}",
