4d3a76a2c86ed5d14ffe4ac3c76f43d4c50d340e
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-29 07:09:47 +0000

    pidgin <=> finch
    Merges Finch into Pidgin. Offering this as an alternative to #33699.

--- Library/Formula/pidgin.rb
+++ Library/Formula/pidgin.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Pidgin < Formula
-  homepage 'http://pidgin.im/'
-  url 'https://downloads.sourceforge.net/project/pidgin/Pidgin/2.10.10/pidgin-2.10.10.tar.bz2'
-  sha1 '81267c35c8d27f2c62320b564fc11af2cc1f3a4a'
+  homepage "https://pidgin.im/"
+  url "https://downloads.sourceforge.net/project/pidgin/Pidgin/2.10.10/pidgin-2.10.10.tar.bz2"
+  sha1 "81267c35c8d27f2c62320b564fc11af2cc1f3a4a"
 
   bottle do
     sha1 "8619dec05832868cf29df37382683b2c8d5944e4" => :yosemite
@@ -11,28 +11,41 @@ class Pidgin < Formula
     sha1 "ecd1805951f73871171f36587323f6ef1aeb82e0" => :mountain_lion
   end
 
-  depends_on :x11
-  depends_on 'pkg-config' => :build
-  depends_on 'intltool' => :build
-  depends_on 'gettext'
-  depends_on 'gnutls'
-  depends_on 'gtk+'
-
-  # for pidgin-otr
+  depends_on :x11 => :optional
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gettext"
+  depends_on "gtk+"
+  depends_on "gsasl" => :optional
+  depends_on "gnutls"
+  depends_on "libgcrypt"
   depends_on "libotr"
 
+  if build.without? "GUI"
+    depends_on "glib"
+    depends_on "libidn"
+  end
+
+  # Finch has an equal port called purple-otr but it is a NIGHTMARE to compile
+  # If you want to fix this and create a PR on Homebrew please do so.
   resource "pidgin-otr" do
     url "https://otr.cypherpunks.ca/pidgin-otr-4.0.1.tar.gz"
     sha1 "e231a2dc72c960f2aa70d8c9d4b05abc6d123085"
   end
 
-  option 'perl', 'Build pidgin with perl support'
+  deprecated_option "perl" => "with-perl"
+
+  option "with-perl", "Build Pidgin with Perl support"
+  option "without-GUI", "Build Finch instead of Pidgin"
 
   def install
     args = %W[
       --disable-debug
+      --disable-dependency-tracking
       --prefix=#{prefix}
       --disable-avahi
+      --disable-doxygen
+      --enable-gnutls=yes
       --disable-dbus
       --disable-gevolution
       --disable-gstreamer
@@ -41,22 +54,33 @@ class Pidgin < Formula
       --disable-idn
       --disable-meanwhile
       --disable-vv
-      --enable-gnutls=yes
     ]
 
-    args << '--disable-perl' unless build.include? 'perl'
+    args << "--with-x" if build.with? "GUI"
+    args << "--disable-perl" if build.without? "perl"
+    args << "--enable-cyrus-sasl" if build.with? "gsasl"
+
+    if build.without? "GUI"
+      args << "--with-tclconfig=#{MacOS.sdk_path}/usr/lib"
+      args << "--with-tkconfig=#{MacOS.sdk_path}/usr/lib"
+      args << "--without-x"
+      args << "--disable-gtkui"
+    end
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
 
-    resource("pidgin-otr").stage do
-      ENV.append_path "PKG_CONFIG_PATH", "#{lib}/pkgconfig"
-      system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
-      system "make", "install"
+    if build.with? "GUI"
+      resource("pidgin-otr").stage do
+        ENV.prepend "CFLAGS", "-I#{HOMEBREW_PREFIX}/opt/libotr/include/libotr"
+        ENV.append_path "PKG_CONFIG_PATH", "#{lib}/pkgconfig"
+        system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+        system "make", "install"
+      end
     end
   end
 
   test do
-    system "#{bin}/pidgin --version"
+    system "#{bin}/finch", "--version"
   end
 end
