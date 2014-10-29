4ac18eab1de933bd06dcffd332d28aec35e28fc3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-29 17:11:20 +0000

    autossh: add 1.4d bottle.
    Closes #33711.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/autossh.rb
+++ Library/Formula/autossh.rb
@@ -6,6 +6,13 @@ class Autossh < Formula
   mirror "http://ftp.de.debian.org/debian/pool/main/a/autossh/autossh_1.4d.orig.tar.gz"
   sha1 "27da23c357f8d263aba6ecf3e8792a3552d90e50"
 
+  bottle do
+    cellar :any
+    sha1 "4233a7ced33f61621bbfe993a38b1f341b50fa37" => :yosemite
+    sha1 "5438d61bc2751a0f7225882199f9e25cdc61863f" => :mavericks
+    sha1 "3dc121773b268bb858d65e2824be58dee8495289" => :mountain_lion
+  end
+
   patch :DATA
 
   def install
