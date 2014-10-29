a19bb2c4e68c4c4194be69893abc6fc55f42bf1e
Alessio Bogon <youtux@gmail.com>
2014-10-29 02:12:31 +0100

    Removed redundant version info

--- Library/Formula/ldid.rb
+++ Library/Formula/ldid.rb
@@ -3,7 +3,6 @@ require "formula"
 class Ldid < Formula
   homepage "http://www.saurik.com/id/8"
   url "git://git.saurik.com/ldid.git", :tag => "v1.1.2"
-  version "1.1.2"
 
   def install
     system "./make.sh"
