131e8f70116390bfe1a3cc27ab37495a1f0b2f50
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-30 16:54:18 +0000

    lensfun: add 0.3.0 bottle.

--- Library/Formula/lensfun.rb
+++ Library/Formula/lensfun.rb
@@ -6,6 +6,12 @@ class Lensfun < Formula
   url "https://downloads.sourceforge.net/project/lensfun/0.3.0/lensfun-0.3.0.tar.bz2"
   sha1 "60e2bf3a6a2f495076db1d88778a00d358cf0b69"
 
+  bottle do
+    sha1 "3d14bc917c95bda7eb6f9e98f83c18b22fb048f1" => :yosemite
+    sha1 "f75c7915e5701e605275e6d48a8c5c12ec981948" => :mavericks
+    sha1 "b6b0966f02558cb8304a1206536a1d1e16afcb05" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cmake" => :build
   depends_on "glib"
