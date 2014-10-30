cfd6413639119768462473f56b20e425242be9c3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-10-30 18:54:42 +0000

    with-readline: add 0.1.1 bottle.

--- Library/Formula/with-readline.rb
+++ Library/Formula/with-readline.rb
@@ -5,6 +5,13 @@ class WithReadline < Formula
   url "http://www.greenend.org.uk/rjk/sw/with-readline-0.1.1.tar.gz"
   sha1 "ac32f4b23853024f2a42441fa09b20cbe7617ff5"
 
+  bottle do
+    cellar :any
+    sha1 "465e7ae270b503a83bbdfc04703fea2a293d67f1" => :yosemite
+    sha1 "4d93c4c6724fb8f2fa97db8f292b87727b62b7aa" => :mavericks
+    sha1 "ad1b2fa7bf448e4fad18f93dc23e43521dce1857" => :mountain_lion
+  end
+
   depends_on "readline"
 
   option :universal
