7f0cd9d1458f9ce8a80be8818f23618025f855f8
Jack Nagel <jacknagel@gmail.com>
2014-10-30 11:42:09 -0500

    lensfun 0.3.0
    Closes #33734.

--- Library/Formula/lensfun.rb
+++ Library/Formula/lensfun.rb
@@ -1,19 +1,20 @@
-require 'formula'
+require "formula"
 
 class Lensfun < Formula
-  homepage 'http://lensfun.sourceforge.net/'
-  head 'git://git.code.sf.net/p/lensfun/code'
-  url 'https://downloads.sourceforge.net/project/lensfun/0.2.8/lensfun-0.2.8.tar.bz2'
-  sha1 '0e85eb7692620668d27e2303687492ad68c90eb4'
-  revision 1
+  homepage "http://lensfun.sourceforge.net/"
+  head "git://git.code.sf.net/p/lensfun/code"
+  url "https://downloads.sourceforge.net/project/lensfun/0.3.0/lensfun-0.3.0.tar.bz2"
+  sha1 "60e2bf3a6a2f495076db1d88778a00d358cf0b69"
 
-  depends_on 'doxygen' => :optional
-  depends_on 'glib'
-  depends_on 'libpng'
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "glib"
+  depends_on "gettext"
+  depends_on "libpng"
+  depends_on "doxygen" => :optional
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 end
