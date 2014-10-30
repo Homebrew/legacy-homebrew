a3c16261069c88b896d9450cfedc497c4031d870
Jack Nagel <jacknagel@gmail.com>
2014-10-29 22:11:51 -0500

    autoconf-archive 2014.10.15

--- Library/Formula/autoconf-archive.rb
+++ Library/Formula/autoconf-archive.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class AutoconfArchive < Formula
   homepage "http://savannah.gnu.org/projects/autoconf-archive/"
-  url "http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2014.02.28.tar.bz2"
-  mirror "http://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2014.02.28.tar.bz2"
-  sha1 "93cf72148adb86bc3221793c49bf2d73c6804af8"
+  url "http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2014.10.15.tar.xz"
+  mirror "http://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2014.10.15.tar.xz"
+  sha1 "7efcefd29a67da2a7243ea2b30e353027d70b460"
 
   def install
     system "./configure", "--prefix=#{prefix}"
