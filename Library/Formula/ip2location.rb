83c8f2a4bb7e73673fbdab37999135b93682b16c
velikan (home) <velikan55555@hotmail.com>
2014-10-31 00:19:24 +0300

    ip2location fixes

--- Library/Formula/ip2location.rb
+++ Library/Formula/ip2location.rb
@@ -2,12 +2,13 @@ require "formula"
 
 class Ip2location < Formula
   homepage "http://www.ip2location.com"
-  url "https://github.com/velikanov/ip2location/archive/v7.0.0.1.tar.gz"
-  sha1 "57d60932e04c89e6a16a775e224a3f74943df52c"
+  url "https://github.com/velikanov/ip2location/archive/v7.0.0.2.tar.gz"
+  sha1 "7a4d0b3e7b29ca2ec90a92b7901f22ecb3c6f50e"
 
   def install
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}/lib"
     system "make"
-    mv "libIP2Location.dylib", "#{HOMEBREW_PREFIX}/lib/libIP2Location.dylib"
+    system "make", "install"
   end
 
   test do
