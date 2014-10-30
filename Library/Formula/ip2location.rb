59d99688aafed4890aae171397548c704ae78f53
velikan (home) <velikan55555@hotmail.com>
2014-10-30 22:52:45 +0300

    ip2location v7.0.0 (new formula)
    ip2location v7.0.0 geolocation library

---
+++ Library/Formula/ip2location.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Ip2location < Formula
+  homepage "http://www.ip2location.com"
+  url "https://github.com/velikanov/ip2location/archive/v7.0.0.tar.gz"
+  sha1 "b8742cbe4b2cb2f2c4992da869498454fcb52847"
+
+  def install
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "test /usr/local/lib/libIP2Location.dylib"
+  end
+end
