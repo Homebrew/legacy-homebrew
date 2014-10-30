99435fc8233533b3281351e1a9a02ddfc298c580
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-30 19:44:02 +0000

    libtecla: 10.8> fixes.
    Closes #33733.

--- Library/Formula/libtecla.rb
+++ Library/Formula/libtecla.rb
@@ -1,13 +1,15 @@
-require 'formula'
+require "formula"
 
 class Libtecla < Formula
-  homepage 'http://www.astro.caltech.edu/~mcs/tecla/index.html'
-  url 'http://www.astro.caltech.edu/~mcs/tecla/libtecla-1.6.2.tar.gz'
-  sha1 '2eae391d29ee02d921e73c4acc78350c9b03d618'
+  homepage "http://www.astro.caltech.edu/~mcs/tecla/index.html"
+  url "http://www.astro.caltech.edu/~mcs/tecla/libtecla-1.6.2.tar.gz"
+  sha1 "2eae391d29ee02d921e73c4acc78350c9b03d618"
 
   def install
     ENV.j1
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make install"
+    # Emailed upstream with this fix on 31st October 2014.
+    inreplace "Makefile", "libgcc.a", ""
+    system "make", "install"
   end
 end
