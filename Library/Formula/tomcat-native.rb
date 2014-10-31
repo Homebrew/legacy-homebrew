d76f346255cb4582951bc696ce4eb3caa81f9064
asaph <ace@dontletgo.com>
2014-10-31 10:49:27 -0700

    tomcat-native 1.1.32

--- Library/Formula/tomcat-native.rb
+++ Library/Formula/tomcat-native.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class TomcatNative < Formula
   homepage 'http://tomcat.apache.org/native-doc/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-connectors/native/1.1.31/source/tomcat-native-1.1.31-src.tar.gz'
-  sha1 '177b1f43f3dbc16eeea39d85147355be29a6089f'
-  revision 1
+  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-connectors/native/1.1.32/source/tomcat-native-1.1.32-src.tar.gz'
+  sha1 'a4bfb7f79316c49cfed3a0c5c71ba11b51fe0922'
 
   depends_on "libtool" => :build
   depends_on "tomcat" => :recommended
