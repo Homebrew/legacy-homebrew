109f9aeb303842585be8d53029152d5b0c26284c
Michael Lopez <michael@weahead.se>
2014-10-31 12:06:34 +0100

    Update BFG to 1.11.8

--- Library/Formula/bfg.rb
+++ Library/Formula/bfg.rb
@@ -2,12 +2,12 @@ require "formula"
 
 class Bfg < Formula
   homepage "http://rtyley.github.io/bfg-repo-cleaner/"
-  url "http://repo1.maven.org/maven2/com/madgag/bfg/1.11.6/bfg-1.11.6.jar"
-  sha1 "bbe33eb231435c04f5713d05d3b17a9b88d4954e"
+  url "http://repo1.maven.org/maven2/com/madgag/bfg/1.11.8/bfg-1.11.8.jar"
+  sha1 "9834ed826f2b3f1da1f28f663e3c5b9cf807ab1e"
 
   def install
-    libexec.install "bfg-1.11.6.jar"
-    bin.write_jar_script libexec/"bfg-1.11.6.jar", "bfg"
+    libexec.install "bfg-1.11.8.jar"
+    bin.write_jar_script libexec/"bfg-1.11.8.jar", "bfg"
   end
 
   test do
