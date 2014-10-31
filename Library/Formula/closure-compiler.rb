00e7b9a7c817f54080ffcffa7eede77b482a98e7
Brett Koonce <koonce@gmail.com>
2014-10-31 16:59:52 -0700

    closure-compiler 20141023

--- Library/Formula/closure-compiler.rb
+++ Library/Formula/closure-compiler.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class ClosureCompiler < Formula
   homepage "https://github.com/google/closure-compiler"
-  url "https://github.com/google/closure-compiler/archive/maven-release-v20140814.tar.gz"
-  sha1 "d783183bd91e2661aa8cd1a8c35c210935e23e15"
+  url "https://github.com/google/closure-compiler/archive/maven-release-v20141023.tar.gz"
+  sha1 "4551a74f0ccfd7e1de325d495b5f71f71a3b24c8"
 
   head "https://github.com/google/closure-compiler.git"
 
