4a6f030d077abd9ee00a0bb83e999d6cfe2ce79a
Brett Koonce <koonce@gmail.com>
2014-10-31 17:37:49 -0700

    slurm 0.4.2

--- Library/Formula/slurm.rb
+++ Library/Formula/slurm.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Slurm < Formula
   homepage "https://github.com/mattthias/slurm"
-  url "https://github.com/mattthias/slurm/archive/upstream/0.4.0.tar.gz"
-  sha1 "b50c2245513f1241f78a487504eb5e460aec9a04"
+  url "https://github.com/mattthias/slurm/archive/upstream/0.4.2.tar.gz"
+  sha1 "927687980445066a0c12c938321a27bc717bcad9"
 
   bottle do
     cellar :any
@@ -14,10 +14,6 @@ class Slurm < Formula
 
   depends_on "scons" => :build
 
-  # patch to support colour ncurses on OS X
-  # see https://github.com/mattthias/slurm/pull/2
-  patch :DATA
-
   def install
     scons
     bin.install "slurm"
@@ -27,17 +23,3 @@ class Slurm < Formula
     system "#{bin}/slurm", "-h"
   end
 end
-
-__END__
-diff --git a/SConstruct b/SConstruct
-index 8c85b77..de3d82b 100644
---- a/SConstruct
-+++ b/SConstruct
-@@ -5,6 +5,8 @@ if os.uname()[0] == 'Linux':
-	env.Append(CPPDEFINES=['_HAVE_NCURSES','_HAVE_NCURSES_COLOR'])
- elif os.uname()[0] == 'GNU/kFreeBSD':
-	env.Append(CPPDEFINES=['_HAVE_NCURSES','_HAVE_NCURSES_COLOR'])
-+elif os.uname()[0] == 'Darwin':
-+	env.Append(CPPDEFINES=['_HAVE_NCURSES','_HAVE_NCURSES_COLOR'])
-
- env.Append( LIBS = ['ncurses'] )
