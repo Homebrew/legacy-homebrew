0e2b7a08ae298355a497beaa1613260c4033a285
Brett Koonce <koonce@gmail.com>
2014-10-31 17:35:35 -0700

    liblastfm 1.0.9

--- Library/Formula/liblastfm.rb
+++ Library/Formula/liblastfm.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Liblastfm < Formula
-  homepage 'https://github.com/lastfm/liblastfm/'
-  url 'https://github.com/lastfm/liblastfm/archive/1.0.8.tar.gz'
-  sha1 '5ef084d0ba27c5e2bc1ec1754618ded0cd2b430e'
+  homepage "https://github.com/lastfm/liblastfm/"
+  url "https://github.com/lastfm/liblastfm/archive/1.0.9.tar.gz"
+  sha1 "4a6323538a26c5ea2080a8ebe58e4407dbc42397"
 
   bottle do
     revision 1
@@ -12,16 +12,16 @@ class Liblastfm < Formula
     sha1 "8ca49c976b13020ea6aed69b4364c24a30783ba7" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'cmake' => :build
-  depends_on 'qt'
-  depends_on 'fftw'
-  depends_on 'libsamplerate'
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "qt"
+  depends_on "fftw"
+  depends_on "libsamplerate"
 
   def install
-    mkdir 'build' do
-      system 'cmake', '..', *std_cmake_args
-      system 'make', 'install'
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
     end
   end
 end
