d32d4c6edbfa11be5a3d04e048038011f5fd57ce
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-02 21:31:54 +0000

    acpica: update 20140926 bottle.

--- Library/Formula/acpica.rb
+++ Library/Formula/acpica.rb
@@ -8,9 +8,9 @@ class Acpica < Formula
 
   bottle do
     cellar :any
-    sha1 "355fadcc05a553d6ce9e07ff3093c6656e9b3270" => :mavericks
-    sha1 "fd2dfabd50acb1952273d7d8981737f65112359e" => :mountain_lion
-    sha1 "f9969f8a4c60c82de1c1f51b96d32146e4f94a22" => :lion
+    sha1 "67536b4f83b441486bc629290adf24ae1c068582" => :yosemite
+    sha1 "10f7afd73ba977b10db461492344efa40860165d" => :mavericks
+    sha1 "19b90793ec68a5c26a1add2cdabd6d6cd83b29de" => :mountain_lion
   end
 
   def install
