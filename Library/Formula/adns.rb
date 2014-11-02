8f70091ccfc4e6e5f1e442d373054c1a5b4de9ef
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-02 22:35:46 +0000

    adns: add 1.5.0 bottle.

--- Library/Formula/adns.rb
+++ Library/Formula/adns.rb
@@ -6,6 +6,13 @@ class Adns < Formula
   sha1 "38306b8030c61a78bee85e33f34de876392ca4f8"
   head "git://git.chiark.greenend.org.uk/~ianmdlvl/adns.git"
 
+  bottle do
+    cellar :any
+    sha1 "c06199054fc9cbffc10f0cc3fe9068e6f67bf0f7" => :yosemite
+    sha1 "e448d98d144e981dc37ff926637d834997197170" => :mavericks
+    sha1 "cb85701fcdfc8c2ef2de0486b0df57111bf99f38" => :mountain_lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dynamic"
     system "make"
