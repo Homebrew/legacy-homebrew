6bc195783941248999762eb6c17efd2ec572f26e
theirix <theirix@gmail.com>
2014-11-02 18:36:03 +0300

    Fixed indent, error checking

--- Library/Formula/snappystream.rb
+++ Library/Formula/snappystream.rb
@@ -11,37 +11,33 @@ class Snappystream < Formula
   depends_on "snappy"
 
   def install
-    args = std_cmake_args + %W[ -DBUILD_TESTS=ON ]
-
-    system "cmake", ".", *args
+    system "cmake", ".", *std_cmake_args, "-DBUILD_TESTS=ON"
     system "make", "all", "test", "install"
   end
 
   test do
-    (testpath/"testsnappystream.cxx").write <<-EOF
-#include <iostream>
-#include <fstream>
-#include <iterator>
-#include <algorithm>
-#include <snappystream.hpp>
+    (testpath/"testsnappystream.cxx").write <<-EOS.undent
+      #include <iostream>
+      #include <fstream>
+      #include <iterator>
+      #include <algorithm>
+      #include <snappystream.hpp>
 
-int main()
-{
-  { std::ofstream ofile("snappy-file.dat");
-    snappy::oSnappyStream osnstrm(ofile);
-    std::cin >> std::noskipws;
-    std::copy(std::istream_iterator<char>(std::cin), std::istream_iterator<char>(), std::ostream_iterator<char>(osnstrm));
-  }
-  { std::ifstream ifile("snappy-file.dat");
-    snappy::iSnappyStream isnstrm(ifile);
-    isnstrm >> std::noskipws;
-    std::copy(std::istream_iterator<char>(isnstrm), std::istream_iterator<char>(), std::ostream_iterator<char>(std::cout));
-  }
-}
-EOF
+      int main()
+      {
+        { std::ofstream ofile("snappy-file.dat");
+          snappy::oSnappyStream osnstrm(ofile);
+          std::cin >> std::noskipws;
+          std::copy(std::istream_iterator<char>(std::cin), std::istream_iterator<char>(), std::ostream_iterator<char>(osnstrm));
+        }
+        { std::ifstream ifile("snappy-file.dat");
+          snappy::iSnappyStream isnstrm(ifile);
+          isnstrm >> std::noskipws;
+          std::copy(std::istream_iterator<char>(isnstrm), std::istream_iterator<char>(), std::ostream_iterator<char>(std::cout));
+        }
+      }
+    EOS
     system ENV.cxx, "testsnappystream.cxx", "-lsnappy", "-lsnappystream", "-o", "testsnappystream"
-    assert $?.success?
     system "./testsnappystream < #{__FILE__} > out.dat && diff #{__FILE__} out.dat"
-    assert $?.success?
   end
 end
