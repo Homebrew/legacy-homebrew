b70bbd39ac5679b6f2792dcbbc6fa8e2f9eefe4a
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-02 21:51:45 +0000

    activemq-cpp 3.8.3
    Version bump, additional OpenSSL dependency, and simple test. Note,
    this will fail to install on at least 10.10 from source unless you have
    the CLT package installed due to our old friend apr-1. Bottling should
    solve that issue for most people though.
    
    Closes #33834.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/activemq-cpp.rb
+++ Library/Formula/activemq-cpp.rb
@@ -1,13 +1,20 @@
 require "formula"
 
 class ActivemqCpp < Formula
-  homepage "http://activemq.apache.org/cms/index.html"
-  url "http://www.apache.org/dyn/closer.cgi?path=activemq/activemq-cpp/3.8.2/activemq-cpp-library-3.8.2-src.tar.bz2"
-  sha1 "6fcaeb278f2d359343ccef4b5814f0092a4d7b54"
+  homepage "https://activemq.apache.org/cms/index.html"
+  url "http://www.apache.org/dyn/closer.cgi?path=activemq/activemq-cpp/3.8.3/activemq-cpp-library-3.8.3-src.tar.bz2"
+  sha1 "ea67d8b86a524ff57f2a2e0e2451deafacfd6d4b"
+
+  depends_on "pkg-config" => :build
+  depends_on "openssl"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/activemqcpp-config", "--version"
   end
 end
