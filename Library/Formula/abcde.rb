bf0dd0f8c33f3f7336229f41590006f649e6eb1c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-03 20:58:14 +0000

    abcde: add 2.6 bottle.

--- Library/Formula/abcde.rb
+++ Library/Formula/abcde.rb
@@ -5,6 +5,13 @@ class Abcde < Formula
   url 'http://abcde.einval.com/download/abcde-2.6.tar.gz'
   sha1 'a1545fb63673e247c8378e9925505e23ace806dc'
 
+  bottle do
+    cellar :any
+    sha1 "fb1aa6a4cb064c1b6d26ab87f7b7eb1a61388963" => :yosemite
+    sha1 "4fd607f7adef90f95d47dae5e1ae43bdbbd3c543" => :mavericks
+    sha1 "b98cc145bbbae45421b36721f39f0a5a7a8ee3f7" => :mountain_lion
+  end
+
   depends_on 'cd-discid'
   depends_on 'cdrtools'
   depends_on 'id3v2'
