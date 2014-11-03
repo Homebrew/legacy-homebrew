b6807a34028ad9ee55829bf91a545e807db2b11e
gio00 <gioskate00@gmail.com>
2014-11-03 11:12:36 +0100

    Update uget.rb

--- Library/Formula/uget.rb
+++ Library/Formula/uget.rb
@@ -3,7 +3,7 @@ require "formula"
 class Uget < Formula
   homepage "http://ugetdm.com/"
   url "https://downloads.sourceforge.net/project/urlget/uget%20%28stable%29/1.10.4/uget-1.10.4.tar.gz"
-  sha1 "eced8dd7d8b9d33b67ada5798e31f4a5eff06da2"
+  sha1 "4375bf38aa593e628351089e30d921db03e25908"
 
   depends_on :x11
   depends_on "intltool" => :build
