fb573d530cf7635a676268021aae22cd74543de6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-03 03:37:35 +0000

    spatialindex: update 1.8.5 bottle.

--- Library/Formula/spatialindex.rb
+++ Library/Formula/spatialindex.rb
@@ -5,6 +5,13 @@ class Spatialindex < Formula
   url "http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.5.tar.gz"
   sha1 "08af1fefd0a30c895d7d714056c2a8f021f46eb4"
 
+  bottle do
+    cellar :any
+    sha1 "ed7c92a7da78e4e7e5294ea52cd05c344094af98" => :yosemite
+    sha1 "07e4fe6747d4db327fadc2e3e4f12a975ff4aaba" => :mavericks
+    sha1 "2122a70a3dd9d966d8fdc7b9e0e285515356dd98" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make"
