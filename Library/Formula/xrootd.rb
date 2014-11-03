1746f9c67a30628fee696c0c312f663fd2751d49
Brett Koonce <koonce@gmail.com>
2014-11-02 20:43:48 -0800

    xrootd 4.0.4
    Closes #33856.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/xrootd.rb
+++ Library/Formula/xrootd.rb
@@ -2,10 +2,11 @@ require "formula"
 
 class Xrootd < Formula
   homepage "http://xrootd.org"
-  url "http://xrootd.org/download/v4.0.3/xrootd-4.0.3.tar.gz"
-  sha1 "e03d7dde9b7510c518b03b01b018ca4f1fba867f"
+  url "http://xrootd.org/download/v4.0.4/xrootd-4.0.4.tar.gz"
+  sha1 "e19432bfc016319f6e653674cfe71b308b296925"
 
   depends_on "cmake" => :build
+  depends_on "openssl"
 
   def install
     mkdir "build" do
