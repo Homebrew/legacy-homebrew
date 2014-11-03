f1184dc8468896b5762f8d0e001794c316f2844b
Howard Butler <howard@hobu.co>
2014-11-02 21:13:28 -0600

    spatialindex 1.8.5
    Closes #33851.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/spatialindex.rb
+++ Library/Formula/spatialindex.rb
@@ -2,15 +2,8 @@ require 'formula'
 
 class Spatialindex < Formula
   homepage 'http://libspatialindex.github.io'
-  url "http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.1.tar.gz"
-  sha1 "c05f8d081316a163aef117b7fd99811539ec421c"
-
-  bottle do
-    cellar :any
-    sha1 "54cf31266bd31f2da68f907357ea82660d3e69a7" => :mavericks
-    sha1 "39d6c3cae399cfe21ce654099d91e235db568e6e" => :mountain_lion
-    sha1 "4b3a2cc81e92d7129b55c90a0c35014b200acf1d" => :lion
-  end
+  url "http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.5.tar.gz"
+  sha1 "08af1fefd0a30c895d7d714056c2a8f021f46eb4"
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
