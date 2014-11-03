78220046ddaffb9fc10563386ab47889f904e29b
Brett Koonce <koonce@gmail.com>
2014-11-02 20:43:19 -0800

    solr 4.10.2

--- Library/Formula/solr.rb
+++ Library/Formula/solr.rb
@@ -2,15 +2,8 @@ require "formula"
 
 class Solr < Formula
   homepage "http://lucene.apache.org/solr/"
-  url "http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.10.1/solr-4.10.1.tgz"
-  sha1 "6033a9887bbfdd1d8ce1bdafce0d65975cf910d8"
-
-  bottle do
-    cellar :any
-    sha1 "813b592ac2f8608e4495c6b79e1ecafd63e96421" => :mavericks
-    sha1 "5059173bedf5a8a70102ac414c9a43e79ce3c265" => :mountain_lion
-    sha1 "6c17714dd9ee85c8bebe8c00adf21219d9f3a804" => :lion
-  end
+  url "http://www.apache.org/dyn/closer.cgi?path=lucene/solr/4.10.2/solr-4.10.2.tgz"
+  sha1 "b913204d07212d7bb814afe4641992f22404a27d"
 
   def install
     libexec.install Dir["*"]
