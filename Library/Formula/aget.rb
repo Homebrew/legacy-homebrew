9f04872e36a5b833633690fd7ceaabb7436230b6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-03 03:32:44 +0000

    aget: add 0.4.1 bottle.

--- Library/Formula/aget.rb
+++ Library/Formula/aget.rb
@@ -6,6 +6,13 @@ class Aget < Formula
   sha256 "d17393c7f44aab38028ae71f14b572ba1839b6e085fb2092b6ebe68bc931df4d"
   head "https://github.com/EnderUNIX/Aget.git"
 
+  bottle do
+    cellar :any
+    sha1 "62c6fbd2f84ce88cd4c10f7faf48f4436c2addde" => :yosemite
+    sha1 "7949a392886fe34408669c529af6697b96acafc9" => :mavericks
+    sha1 "2b6c73f112e7aedd05807c61e118a32a057cd5f7" => :mountain_lion
+  end
+
   def install
     # ENV replaces the MacPorts patch that ensured compile on OS X.
     # https://github.com/EnderUNIX/Aget/issues/3
