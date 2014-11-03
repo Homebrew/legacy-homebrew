eb35ee5f835b194b9b6da7b649bc1c76889297ef
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-03 06:47:23 +0000

    xrootd: add 4.0.4 bottle.

--- Library/Formula/xrootd.rb
+++ Library/Formula/xrootd.rb
@@ -5,6 +5,13 @@ class Xrootd < Formula
   url "http://xrootd.org/download/v4.0.4/xrootd-4.0.4.tar.gz"
   sha1 "e19432bfc016319f6e653674cfe71b308b296925"
 
+  bottle do
+    cellar :any
+    sha1 "755130a388fb467ddaf44a93494172876ba9af71" => :yosemite
+    sha1 "26bcf7eff4cf9dc93ba5940764ed5671fe1fe1d9" => :mavericks
+    sha1 "f5f476756b9f03fa0c3900861c08b28237c6c330" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "openssl"
 
