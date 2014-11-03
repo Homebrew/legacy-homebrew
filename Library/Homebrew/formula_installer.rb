44bff238434a48c4a8531bc2d1a29c81f18f8fab
Jack Nagel <jacknagel@gmail.com>
2014-11-02 19:11:18 -0600

    FormulaInstaller: make mode reader methods private

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -19,6 +19,7 @@ class FormulaInstaller
 
   def self.mode_attr_accessor(*names)
     attr_accessor(*names)
+    private(*names)
     names.each { |name| define_method("#{name}?") { !!send(name) }}
   end
 
