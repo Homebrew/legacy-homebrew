3d6c52b20ecdeef0ae8c5884b77f48bb3dc171f1
Brett Koonce <koonce@gmail.com>
2014-11-02 20:42:04 -0800

    MonetDB 11.19.3

--- Library/Formula/monetdb.rb
+++ Library/Formula/monetdb.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Monetdb < Formula
-  homepage 'https://www.monetdb.org/'
-  url 'https://dev.monetdb.org/downloads/sources/Jan2014-SP3/MonetDB-11.17.21.zip'
-  sha1 '53ee8de943ec2a247cf8b2bf645bc0a1b8fd5fc8'
+  homepage "https://www.monetdb.org/"
+  url "https://dev.monetdb.org/downloads/sources/Oct2014/MonetDB-11.19.3.zip"
+  sha1 "f8290358c1773afc2679d9cbfea456c691f50527"
 
   bottle do
     sha1 "fd10ee968f89f090ed519474aab1b564bc9f54f8" => :mavericks
@@ -11,14 +11,15 @@ class Monetdb < Formula
     sha1 "c04629aeed599972e59cc5e74c166c52be1dde19" => :lion
   end
 
-  head 'http://dev.monetdb.org/hg/MonetDB', :using => :hg
+  head "http://dev.monetdb.org/hg/MonetDB", :using => :hg
 
-  option 'with-java'
+  option "with-java"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :ant => :build
-  depends_on 'pcre'
-  depends_on 'readline' # Compilation fails with libedit.
+  depends_on "pcre"
+  depends_on "readline" # Compilation fails with libedit.
+  depends_on "openssl"
 
   def install
     system "./bootstrap" if build.head?
@@ -31,7 +32,7 @@ class Monetdb < Formula
             "--disable-jaql",
             "--without-rubygem"]
 
-    args << "--with-java=no" if build.without? 'java'
+    args << "--with-java=no" if build.without? "java"
 
     system "./configure", *args
     system "make install"
