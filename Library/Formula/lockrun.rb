0fd47a8cc2f0c7a6c958639dcdf0309d9a74e118
Richard <richie765@gmail.com>
2014-11-03 13:38:09 -0500

    lockrun 1.1.3
    Updated for new version
    
    Closes #33873.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/lockrun.rb
+++ Library/Formula/lockrun.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Lockrun < Formula
   homepage 'http://unixwiz.net/tools/lockrun.html'
   url 'http://unixwiz.net/tools/lockrun.c'
-  version '20130426'
-  sha1 'a2f0aaf9e42098c0f103042c5129f28c8798055d'
+  version '1.1.3'
+  sha1 '513e586884b9a7112b7da3acf53e75c84e28cda9'
 
   def install
     system "#{ENV.cc} #{ENV.cflags} lockrun.c -o lockrun"
