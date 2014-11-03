f6ae6a08fbcdf81f11b2ad0e0f446ce604b72a05
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-03 03:24:05 +0000

    libgeotiff: update 1.4.1 bottle.

--- Library/Formula/libgeotiff.rb
+++ Library/Formula/libgeotiff.rb
@@ -5,6 +5,12 @@ class Libgeotiff < Formula
   url 'http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.1.tar.gz'
   sha1 'bc9e2bb43f3877b795b4b191e7aec6267f4a1c7e'
 
+  bottle do
+    sha1 "36537de4979760793a529685086eba83f6bdf5d4" => :yosemite
+    sha1 "8d98c26cef589169e70db65455ee2df94cedc9ef" => :mavericks
+    sha1 "552b698613f5eab0842d408406185ca069fbb0d8" => :mountain_lion
+  end
+
   depends_on 'libtiff'
   depends_on 'lzlib'
   depends_on 'jpeg'
