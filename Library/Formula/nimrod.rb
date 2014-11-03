f266446de41203a10a55a1ff512350ebdc72603e
Dan Ambrisco <dambrisco@itriagehealth.com>
2014-11-03 14:09:22 -0700

    Updates nimrod recipe to work with 0.9.6

--- Library/Formula/nimrod.rb
+++ Library/Formula/nimrod.rb
@@ -2,32 +2,21 @@ require 'formula'
 
 class Nimrod < Formula
   homepage 'http://nimrod-code.org/'
-  url 'http://nimrod-code.org/download/nimrod_0.9.2.zip'
-  sha1 '326ecd61d6df45afdc04cb8685ef46f8fb8f9e47'
+  url 'http://nimrod-code.org/download/nimrod_0.9.6.zip'
+  sha1 'a0be99cd67ca2283c6cf076bb7edee74d2f32dc5'
 
   head 'https://github.com/Araq/Nimrod.git'
 
-  # Install to libexec until an upstream fix appears for
-  # https://github.com/Araq/Nimrod/issues/459
   def install
     system "/bin/sh", "./build.sh"
     inreplace 'install.sh', '$1/nimrod', '$1'
-    system "/bin/sh", "./install.sh", libexec
+    system "/bin/sh", "./install.sh", prefix
   end
 
   test do
     (testpath/'hello.nim').write <<-EOS.undent
       echo("Hi!")
     EOS
-    system "#{libexec}/bin/nimrod", "compile", "--run", "hello.nim"
-  end
-
-  def caveats; <<-EOS.undent
-    Nimrod has been installed to #{libexec}.
-    The compiler will currently fail to find system.nim if called through a
-    symlink. To compile nim files, specify the full path to the compiler:
-
-      #{libexec}/bin/nimrod compile --run hello.nim
-    EOS
+    system "nimrod", "compile", "--run", "hello.nim"
   end
 end
