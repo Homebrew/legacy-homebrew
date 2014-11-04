0d5e62ea99a4e59a3cac4e9e23bebb3ae7e93353
Nathan Toone <nathan@toonetown.com>
2014-11-04 09:19:11 -0700

    go-appengine-sdk (amd64) 1.9.15

--- Library/Formula/go-app-engine-64.rb
+++ Library/Formula/go-app-engine-64.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GoAppEngine64 < Formula
   homepage "http://code.google.com/appengine/docs/go/overview.html"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.14.zip"
-  sha1 "45493333185b74cba6be690f2ca72f7639a14969"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.15.zip"
+  sha1 "de638e484af2e6d94f5a8cbdc24a7b9c791c6531"
 
   def install
     cd ".."
