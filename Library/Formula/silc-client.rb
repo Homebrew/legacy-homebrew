f4e42317fe0a6c471cb940ded772712325d143de
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-04 23:06:40 +0000

    silc-client: add 1.1.11_1 bottle.

--- Library/Formula/silc-client.rb
+++ Library/Formula/silc-client.rb
@@ -4,6 +4,12 @@ class SilcClient < Formula
   homepage "http://silcnet.org/client.html"
   url "https://downloads.sourceforge.net/project/silc/silc/client/sources/silc-client-1.1.11.tar.gz"
   sha256 "8cedf2f3c15322296afe094de60504bc27e349f1942713a2f322c7ef6ad5089e"
+  bottle do
+    sha1 "3222c80d0b58f752bab07c29679dc0585d43d067" => :yosemite
+    sha1 "32093a6a21e593f2f5300b5cd933de720b76768f" => :mavericks
+    sha1 "b62f58cf75a826116df89a50015c20806c186d33" => :mountain_lion
+  end
+
   revision 1
 
   depends_on "pkg-config" => :build
