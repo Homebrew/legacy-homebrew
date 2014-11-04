d04087460560ee030df29d1d5cab1ef99a5c816f
David Christenson <david@the-david.com>
2014-11-03 23:26:03 -0700

    boost-bcp 1.57.0
    Update to latest release, change single quotes to double.
    
    Closes #33898.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/boost-bcp.rb
+++ Library/Formula/boost-bcp.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class BoostBcp < Formula
-  homepage 'http://www.boost.org/doc/tools/bcp/'
-  url 'https://downloads.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2'
-  sha1 'f94bb008900ed5ba1994a1072140590784b9b5df'
+  homepage "http://www.boost.org/doc/tools/bcp/"
+  url "https://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2"
+  sha1 "e151557ae47afd1b43dc3fac46f8b04a8fe51c12"
 
-  head 'http://svn.boost.org/svn/boost/trunk/'
+  head "http://svn.boost.org/svn/boost/trunk/"
 
-  depends_on 'boost-build' => :build
+  depends_on "boost-build" => :build
 
   def install
-    cd 'tools/bcp' do
+    cd "tools/bcp" do
       system "b2"
       prefix.install "../../dist/bin"
     end
