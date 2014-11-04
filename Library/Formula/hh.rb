58f3fb154316a3cd25c72dcc8427ea5506a083be
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-04 20:02:56 +0000

    hh: add 1.13 bottle.

--- Library/Formula/hh.rb
+++ Library/Formula/hh.rb
@@ -5,6 +5,13 @@ class Hh < Formula
   url "https://github.com/dvorka/hstr/releases/download/1.13/hh-1.13-src.tgz"
   sha1 "09fee6d687a8b8a7c6f508ced071fb88b0a9bb28"
 
+  bottle do
+    cellar :any
+    sha1 "3110fabac2e10d0bd49f0893a9bff1babf8ad4d3" => :yosemite
+    sha1 "9e7f8e5fab6fdfb0151eb426a97187e7abeb8519" => :mavericks
+    sha1 "e31eebb154535a7fc7e35a498178e1163068e4fb" => :mountain_lion
+  end
+
   head do
     url "https://github.com/dvorka/hstr.git"
     depends_on :autoconf
