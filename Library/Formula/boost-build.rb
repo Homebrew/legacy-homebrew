7982648a4ff68afb1898e0be7ffd241072139b20
David Christenson <david@the-david.com>
2014-11-03 23:20:20 -0700

    boost-build 2014.10
    Update to latest release, remove obsolete conditional directory change,
    change single quotes to double.

--- Library/Formula/boost-build.rb
+++ Library/Formula/boost-build.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class BoostBuild < Formula
-  homepage 'http://boost.org/boost-build2/'
-  url 'https://github.com/boostorg/build/archive/boost-1.55.0.tar.gz'
-  sha1 '9daf7587b017716ffd164bcf11d82c4ac00c8ca0'
+  homepage "http://boost.org/boost-build2/"
+  url "https://github.com/boostorg/build/archive/2014.10.tar.gz"
+  sha1 "64f59424b829e9b5741bda58c78d830c993a0042"
 
-  head 'https://github.com/boostorg/build.git'
+  head "https://github.com/boostorg/build.git"
 
   bottle do
     cellar :any
@@ -15,14 +15,7 @@ class BoostBuild < Formula
   end
 
   def install
-    if build.head?
-      system "./bootstrap.sh"
-      system "./b2", "--prefix=#{prefix}", "install"
-    else
-      cd 'v2' do
-        system "./bootstrap.sh"
-        system "./b2", "--prefix=#{prefix}", "install"
-      end
-    end
+    system "./bootstrap.sh"
+    system "./b2", "--prefix=#{prefix}", "install"
   end
 end
