b3d8004f653a5457553bc26278f4132115e8adc0
Nathan Toone <nathan@toonetown.com>
2014-11-04 09:18:25 -0700

    appengine-java-sdk 1.9.15

--- Library/Formula/app-engine-java-sdk.rb
+++ Library/Formula/app-engine-java-sdk.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class AppEngineJavaSdk < Formula
   homepage "https://developers.google.com/appengine/docs/java/gettingstarted/introduction"
-  url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.14.zip"
-  sha1 "326708bf2ce584a6603076e85797398991d195eb"
+  url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.15.zip"
+  sha1 "a1bb90ca46a6c6b12b6f10741f59f82bd817fa9e"
 
   def install
     rm Dir["bin/*.cmd"]
