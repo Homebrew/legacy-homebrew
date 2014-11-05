2a0c54b079cb365c72e9211b02869c2c29d7fdd5
Tresi Arvizo <github@tresi.org>
2014-11-05 01:35:21 -0500

    magit 1.2.1
    Needed to work with emacs 24.4 or greater.

--- Library/Formula/magit.rb
+++ Library/Formula/magit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Magit < Formula
   homepage 'https://github.com/magit/magit'
-  url 'https://github.com/magit/magit/archive/1.2.0.tar.gz'
-  sha1 '8ad7947a29d4d852f35f94a6821d038fe43a7c31'
+  url 'https://github.com/magit/magit/archive/1.2.1.tar.gz'
+  sha1 '3faeaab35934951a746e3be834d0457ca99bdc01'
 
   head 'https://github.com/magit/magit.git'
 
