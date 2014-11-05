893f20490f749750997791b990e2ff41897bd018
Jack Nagel <jacknagel@gmail.com>
2014-11-05 00:45:14 -0600

    Revert "libtool 2.4.3"
    Too many bugs.
    
    Squashed commit of the following:
    
    commit f86f5885384c7c19c3db9140e3afc0b327072b9e
    Author: Jack Nagel <jacknagel@gmail.com>
    Date:   Wed Nov 5 00:44:54 2014 -0600
    
        Revert "libtool 2.4.3"
    
        This reverts commit 6ba215f498c82ebca11f30f3d3fd948a62dfe862.
    
    commit 9c0ca0becd8c09852197df4fa5ea879a0c271f90
    Author: Jack Nagel <jacknagel@gmail.com>
    Date:   Wed Nov 5 00:44:49 2014 -0600
    
        Revert "libtool: add upstream patch to fix parallel build"
    
        This reverts commit 5f181c9d1ab094aa75c8e6e6a2caaec402483426.
    
    commit 2a2823175ee546176f36eea286c1cba49f1d2246
    Author: Jack Nagel <jacknagel@gmail.com>
    Date:   Wed Nov 5 00:44:41 2014 -0600
    
        Revert "libtool: update 2.4.3 bottle."
    
        This reverts commit 4f50210751864ecb7adc8e483026346fb9dbbf66.
    
    commit d13d9e965673881689171b2d4933f36f728108fc
    Author: Jack Nagel <jacknagel@gmail.com>
    Date:   Wed Nov 5 00:44:35 2014 -0600
    
        Revert "libtool: fix universal build"
    
        This reverts commit 0f80370be66ddcfcea7c9d2ea20dd331f32e3eb1.

--- Library/Formula/libtool.rb
+++ Library/Formula/libtool.rb
@@ -4,45 +4,40 @@ require "formula"
 # This is not the same so as a result we must install this as glibtool.
 
 class Libtool < Formula
-  homepage "https://www.gnu.org/software/libtool/"
-  url "http://ftpmirror.gnu.org/libtool/libtool-2.4.3.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/libtool/libtool-2.4.3.tar.xz"
-  sha1 "7e946bd07b846a8803bf4321e82cd6be8059c0ca"
+  homepage "http://www.gnu.org/software/libtool/"
+  url "http://ftpmirror.gnu.org/libtool/libtool-2.4.2.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libtool/libtool-2.4.2.tar.gz"
+  sha1 "22b71a8b5ce3ad86e1094e7285981cae10e6ff88"
 
   bottle do
     cellar :any
-    sha1 "ede326069fb609eb68c66718f1d03b9c98c16e09" => :yosemite
-    sha1 "7c1881c0245947681ea82334f72310743886c63b" => :mavericks
-    sha1 "c321dc3dab8da5faefb1ef35c2eec49996738e37" => :mountain_lion
+    revision 3
+    sha1 "e172450c5686c7f7e13237c927cb49cce4c0ac0c" => :yosemite
+    sha1 "bbf17c08138fb53a4512732a2dab4f5c8dbec364" => :mavericks
+    sha1 "c749e65dee61cd23b7e757a1308761d8396689e4" => :mountain_lion
+    sha1 "d709c921f42e1f299b5bf09314eb73ab0dfa716d" => :lion
   end
 
   keg_only :provided_until_xcode43
 
   option :universal
 
-  # apply upstream patch to respect '--program-prefix'
-  # http://git.savannah.gnu.org/cgit/libtool.git/commit/?id=c77eea5f6c0592423d925131489cc7772e34cf0b
-  # fix parallel build
-  # http://git.savannah.gnu.org/cgit/libtool.git/commit/?id=5627a7f498e07a40b970c3a5ab5e74a5053e956f
+  # Allow -stdlib= to pass through to linker
+  # http://git.savannah.gnu.org/gitweb/?p=libtool.git;a=commitdiff;h=8f975a1368594126e37d85511f1f96164e466d93
+  # https://trac.macports.org/ticket/32982
+
+  # Fix interpretation of MACOSX_DEPLOYMENT_TARGET on 10.10
+  # http://article.gmane.org/gmane.comp.gnu.libtool.patches/11730
+  # https://trac.macports.org/changeset/125325
   patch :DATA
 
   def install
     ENV.universal_binary if build.universal?
-
-    # Fix an issue with building universal; fixed upstream:
-    # http://debbugs.gnu.org/cgi/bugreport.cgi?bug=18942
-    # Need to touch the manpages after editing to prevent
-    # the buildsystem from thinking they need to be rebuilt.
-    inreplace Dir["build-aux/ltmain.{in,sh}"],
-      '$SED -e "$basename"', '$SED -e "$sed_basename"'
-    Dir["doc/*.1"].each {|f| touch f}
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--program-prefix=g",
                           "--enable-ltdl-install"
-    system "make"
-    system "make", "install"
+    system "make install"
   end
 
   def caveats; <<-EOS.undent
@@ -57,46 +52,58 @@ class Libtool < Formula
 end
 
 __END__
-diff --git a/Makefile.in b/Makefile.in
-index d49abac..6840d3f 100644
---- a/Makefile.in
-+++ b/Makefile.in
-@@ -2290,7 +2290,7 @@ $(libtool_1): $(ltmain_sh)
- $(libtoolize_1): $(libtoolize_in)
- 	$(AM_V_GEN)$(update_mans) libtoolize
- 
--install-data-local: $(lt_Makefile_in)
-+install-data-local: $(lt_Makefile_in) install-scripts-local
- 	@$(NORMAL_INSTALL)
- 	-rm -rf '$(DESTDIR)$(pkgdatadir)'/*
- 	@list='$(pkgmacro_files)'; for p in $$list; do \
-@@ -2318,8 +2318,15 @@ install-data-local: $(lt_Makefile_in)
- 	  echo " $(INSTALL_DATA) '$(ltdldir)/$$p' '$(DESTDIR)$(pkgdatadir)/$$p'"; \
- 	  $(INSTALL_DATA) "$(ltdldir)/$$p" "$(DESTDIR)$(pkgdatadir)/$$p"; \
- 	done
--	$(SCRIPT_ENV) '$(inline_source)' libtoolize > '$(DESTDIR)$(bindir)/libtoolize';
--	-chmod a+x '$(DESTDIR)$(pkgdatadir)/configure' '$(DESTDIR)$(bindir)/libtoolize'
-+	chmod a+x '$(DESTDIR)$(pkgdatadir)/configure'
-+
-+install-scripts-local: $(lt_Makefile_in)
-+	@p=`echo libtoolize |sed -e '$(transform)'`; \
-+	echo " $(SCRIPT_ENV) '$(inline_source)' libtoolize > '$(DESTDIR)$(bindir)/$$p'"; \
-+	d=`echo "$(DESTDIR)$(bindir)/$$p" |$(SED) 's|[^/]*$$||'`; \
-+	test -d "$$d" || $(mkinstalldirs) "$$d"; \
-+	$(SCRIPT_ENV) '$(inline_source)' libtoolize > "$(DESTDIR)$(bindir)/$$p"; \
-+	chmod a+x "$(DESTDIR)$(bindir)/$$p"
- $(changelog): FORCE
- 	$(AM_V_GEN)if test -d '$(srcdir)/.git'; then \
- 	  $(gitlog_to_changelog) --amend=$(git_log_fix) \
-@@ -2366,8 +2373,9 @@ uninstall-hook:
- 	  echo " rm -f '$(DESTDIR)$(aclocaldir)/$$f'"; \
- 	  rm -f "$(DESTDIR)$(aclocaldir)/$$f"; \
- 	done
--	@echo " rm -f '$(DESTDIR)$(bindir)/libtoolize'"; \
--	rm -f '$(DESTDIR)$(bindir)/libtoolize'
-+	@p=`echo libtoolize |sed -e '$(transform)'`; \
-+	echo " rm -f '$(DESTDIR)$(bindir)/$$p'"; \
-+	rm -f "$(DESTDIR)$(bindir)/$$p"
- 
- $(testsuite): $(package_m4) $(TESTSUITE_AT) Makefile.am
- 	$(AM_V_GEN)$(AUTOTEST) -I '$(srcdir)' -I '$(srcdir)/tests' $(TESTSUITE_AT) -o '$@'
+diff --git a/libltdl/config/ltmain.sh b/libltdl/config/ltmain.sh
+index 63ae69d..9ae038c 100644
+--- a/libltdl/config/ltmain.sh
++++ b/libltdl/config/ltmain.sh
+@@ -5851,9 +5851,10 @@ func_mode_link ()
+       # -tp=*                Portland pgcc target processor selection
+       # --sysroot=*          for sysroot support
+       # -O*, -flto*, -fwhopr*, -fuse-linker-plugin GCC link-time optimization
++      # -stdlib=*            select c++ std lib with clang
+       -64|-mips[0-9]|-r[0-9][0-9]*|-xarch=*|-xtarget=*|+DA*|+DD*|-q*|-m*| \
+       -t[45]*|-txscale*|-p|-pg|--coverage|-fprofile-*|-F*|@*|-tp=*|--sysroot=*| \
+-      -O*|-flto*|-fwhopr*|-fuse-linker-plugin)
++      -O*|-flto*|-fwhopr*|-fuse-linker-plugin|-stdlib=*)
+         func_quote_for_eval "$arg"
+ 	arg="$func_quote_for_eval_result"
+         func_append compile_command " $arg"
+diff --git a/configure b/configure
+index a1ef3e3..782d28a 100755
+--- a/configure
++++ b/configure
+@@ -7765,7 +7765,7 @@ $as_echo "$lt_cv_ld_force_load" >&6; }
+       case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
+ 	10.0,*86*-darwin8*|10.0,*-darwin[91]*)
+ 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
+-	10.[012]*)
++	10.[012][,.]*)
+ 	  _lt_dar_allow_undefined='${wl}-flat_namespace ${wl}-undefined ${wl}suppress' ;;
+ 	10.*)
+ 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
+diff --git a/libltdl/configure b/libltdl/configure
+index f18f272..fef1137 100755
+--- a/libltdl/configure
++++ b/libltdl/configure
+@@ -6978,7 +6978,7 @@ $as_echo "$lt_cv_ld_force_load" >&6; }
+       case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
+ 	10.0,*86*-darwin8*|10.0,*-darwin[91]*)
+ 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
+-	10.[012]*)
++	10.[012][,.]*)
+ 	  _lt_dar_allow_undefined='${wl}-flat_namespace ${wl}-undefined ${wl}suppress' ;;
+ 	10.*)
+ 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
+diff --git a/libltdl/m4/libtool.m4 b/libltdl/m4/libtool.m4
+index 44e0ecf..4adcf73 100644
+--- a/libltdl/m4/libtool.m4
++++ b/libltdl/m4/libtool.m4
+@@ -1052,7 +1052,7 @@ _LT_EOF
+       case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
+ 	10.0,*86*-darwin8*|10.0,*-darwin[[91]]*)
+ 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
+-	10.[[012]]*)
++	10.[[012]][[,.]]*)
+ 	  _lt_dar_allow_undefined='${wl}-flat_namespace ${wl}-undefined ${wl}suppress' ;;
+ 	10.*)
+ 	  _lt_dar_allow_undefined='${wl}-undefined ${wl}dynamic_lookup' ;;
