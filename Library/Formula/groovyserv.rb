30c60061a1f4597e3609394728b07bd6ecdfa4f9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-05 11:26:04 +0000

    groovyserv: add 1.0.0 bottle.

--- Library/Formula/groovyserv.rb
+++ Library/Formula/groovyserv.rb
@@ -4,8 +4,14 @@ class Groovyserv < Formula
   homepage 'http://kobo.github.io/groovyserv/'
   url 'https://bitbucket.org/kobo/groovyserv-mirror/downloads/groovyserv-1.0.0-src.zip'
   sha1 '46b946dee3e40457e667498b235bd8e1567ed9ed'
-
   head 'https://github.com/kobo/groovyserv.git'
+
+  bottle do
+    sha1 "2acd972802f63afcdebb622015ab059c23423789" => :yosemite
+    sha1 "b2cb34764f861b8ef374ead20bac89f1866d03d4" => :mavericks
+    sha1 "9936c0eadffe669dca07631c0609bc5a07968800" => :mountain_lion
+  end
+
   depends_on "go" => :build
 
   # This fix is upstream and can be removed in the next released version.
