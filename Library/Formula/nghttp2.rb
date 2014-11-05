7b807a1de9ed343391373254349a50df80ce9a13
Ryo Okamoto <ryo@aquahill.net>
2014-11-05 20:41:21 +0900

    tweaked format.

--- Library/Formula/nghttp2.rb
+++ Library/Formula/nghttp2.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Nghttp2 < Formula
-  homepage 'https://nghttp2.org'
-  url 'https://github.com/tatsuhiro-t/nghttp2/releases/download/v0.6.5/nghttp2-0.6.5.tar.bz2'
-  sha1 '8743e8b2208dc09fdab82feea5c3c027c2ba3468'
+  homepage "https://nghttp2.org"
+  url "https://github.com/tatsuhiro-t/nghttp2/releases/download/v0.6.5/nghttp2-0.6.5.tar.bz2"
+  sha1 "8743e8b2208dc09fdab82feea5c3c027c2ba3468"
 
   depends_on "pkg-config" => :build
   depends_on "cunit" => :build
