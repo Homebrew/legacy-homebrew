477b38ac13c1f6cea2082b8c93af38d73214aa9d
Michael Paquier <michael@otacoo.com>
2014-11-05 10:37:01 +0900

    Fix comment typo in mpd formula

--- Library/Formula/mpd.rb
+++ Library/Formula/mpd.rb
@@ -85,7 +85,7 @@ class Mpd < Formula
 
     system "./configure", *args
     system "make"
-    ENV.j1 # Directories are created in parallel, so let"s not do that
+    ENV.j1 # Directories are created in parallel, so let's not do that
     system "make install"
   end
 
