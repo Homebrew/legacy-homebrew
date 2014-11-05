0620bbc23ad0a64c1d6ea59aa29a09e1e519748e
ieee8023 <joseph@josephpcohen.com>
2014-11-05 16:26:44 -0500

    fixed up installer

--- Library/Formula/blucat.rb
+++ Library/Formula/blucat.rb
@@ -5,19 +5,23 @@ class Blucat < Formula
   url "http://blucat.sourceforge.net/blucat/wp-content/uploads/2013/10/blucat-aa3e02.zip"
   sha1 "c5c801700b5d4d59f6bf0a5f0e4a405237de1840"
 
-  depends_on "ant"
+  depends_on "ant" => :build
 
   def install
     system "ant"
     prefix.install "blucat"
-    prefix.install Dir["lib"]
-    prefix.install Dir["build"]
+    prefix.install "lib"
+    prefix.install "build"
     
-    ## now make a launcher script 
+    ## Now make a launcher script. This is required because launching
+    ## the default blucat script uses the script path to find the
+    ## library files. This allows a user to include the repo in their
+    ## PATH and changing it would be complicated I think. This allows
+    ## the code to change independent of this brew installer.
     system "echo \"#!/bin/bash\" > blucat"
     system "echo " << prefix << "/blucat >> blucat"
     system "chmod +x blucat"
-    bin.install "blucat"
+    libexec.install "blucat"
   end
 
   test do
