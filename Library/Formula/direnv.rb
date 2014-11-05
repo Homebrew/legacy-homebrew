951f6e902f4d3cf4922f658cca2145c170fbf92a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-05 15:40:52 +0000

    direnv: add 2.5.0 bottle.

--- Library/Formula/direnv.rb
+++ Library/Formula/direnv.rb
@@ -7,6 +7,12 @@ class Direnv < Formula
 
   head 'https://github.com/zimbatm/direnv.git'
 
+  bottle do
+    sha1 "fb5340876bc488bd38aba9e00d52fff0f029f797" => :yosemite
+    sha1 "fbc2ed0cadad4543df0d09e3afe756866e0e44b3" => :mavericks
+    sha1 "353d3f25f0b7e3bd57bc91dd855635163bc426e7" => :mountain_lion
+  end
+
   depends_on "go" => :build
 
   def install
