265b70b5961e0cf884fe5723a15012d3318e404b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-05 11:36:05 +0000

    arpon: add 2.7.2 bottle.

--- Library/Formula/arpon.rb
+++ Library/Formula/arpon.rb
@@ -7,6 +7,12 @@ class Arpon < Formula
 
   head "git://git.code.sf.net/p/arpon/code"
 
+  bottle do
+    sha1 "6e172972a05a57cf5023c9e74a25c8269cdf6fb4" => :yosemite
+    sha1 "acba3bde1ac94612b290aba641e70588a983a709" => :mavericks
+    sha1 "6a5938797d1b6e8b04827bf7a920555c55468fc8" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "libdnet"
   depends_on "libnet"
