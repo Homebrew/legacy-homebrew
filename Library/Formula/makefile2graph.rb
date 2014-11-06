7e2b579ac80dd6fca353194119a359b9d76b8dda
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-06 19:03:15 +0000

    makefile2graph: add 1.2 bottle.

--- Library/Formula/makefile2graph.rb
+++ Library/Formula/makefile2graph.rb
@@ -6,6 +6,13 @@ class Makefile2graph < Formula
   sha1 "fd6799c23e4b9599f4caf23b0ce0993ab33362a8"
   head "https://github.com/lindenb/makefile2graph.git"
 
+  bottle do
+    cellar :any
+    sha1 "17670db7e3a7e0b02ef812a6f7f803a9192dcf61" => :yosemite
+    sha1 "88171840b729d628a7ab58fc1febe312d87a970e" => :mavericks
+    sha1 "87b9cf5a1995046638de61c854b0c12f8457ed87" => :mountain_lion
+  end
+
   depends_on "graphviz" => :recommended
 
   def install
