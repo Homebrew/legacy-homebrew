be18ae7ec250e7033fa122903928baa1af8d61e8
Emmanuel STAPF [ES] <manus@eiffel.com>
2014-11-06 14:02:43 +0100

    Included fix from pull request comments.

--- Library/Formula/eiffelstudio.rb
+++ Library/Formula/eiffelstudio.rb
@@ -12,19 +12,10 @@ class Eiffelstudio < Formula
   def install
     system "./compile_exes macosx-x86-64"
     system "./make_images macosx-x86-64"
-	system "mv Eiffel_14.05/* #{prefix}/"
+    system "mv", "Eiffel_14.05/*", "#{prefix}/"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test eiffelstudio`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{prefix}/studio/spec/macosx-x86-64/bin/ec -version"
+    system "#{prefix}/studio/spec/macosx-x86-64/bin/ec", "-version"
   end
 end
