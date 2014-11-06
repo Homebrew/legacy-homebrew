6ef3212f31e3c7eeb9c9afc10370a97b1108158e
jenkins <jenkins-server@spotify.com>
2014-11-06 22:29:21 +0000

    helios 0.8.77
    Closes #33981.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/helios.rb
+++ Library/Formula/helios.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Helios < Formula
   homepage "https://github.com/spotify/helios"
-  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.8.76/helios-tools-0.8.76-shaded.jar"
-  sha1 "1da57909be1415644cb0613a8c3d6a481682ea75"
-  version "0.8.76"
+  url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.8.77/helios-tools-0.8.77-shaded.jar"
+  sha1 "15684178cf0a7d07db425a50f7371749a122846e"
+  version "0.8.77"
 
   depends_on :java => "1.7"
 
