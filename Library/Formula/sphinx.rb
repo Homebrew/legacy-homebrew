8952d43819d719fc4c0cbc4838be6c6b8be1bdd2
Patrick Robertson <robertson.patrick@gmail.com>
2014-11-07 07:45:08 +0000

    sphinx: note this is not sphinx python.
    Closes #33989.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/sphinx.rb
+++ Library/Formula/sphinx.rb
@@ -61,6 +61,9 @@ class Sphinx < Formula
   end
 
   def caveats; <<-EOS.undent
+    This is not sphinx - the Python Documentation Generator. To install
+    sphinx-python, use pip or easy_tools
+
     Sphinx has been compiled with libstemmer support.
 
     Sphinx depends on either MySQL or PostreSQL as a datasource.
