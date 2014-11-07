8a21ad6ae2b62dba5bdd4ff691cfa724fbb7f166
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-07 06:37:40 +0000

    md5deep: added head
    Adds the HEAD, improves the test, fixes up the formatting and adds a
    small note on usage on (way) older OS X versions.
    
    Closes #33987.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/md5deep.rb
+++ Library/Formula/md5deep.rb
@@ -1,21 +1,26 @@
-require 'formula'
+require "formula"
 
 class Md5deep < Formula
-  homepage 'https://github.com/jessek/hashdeep'
-  url 'https://github.com/jessek/hashdeep/archive/release-4.4.tar.gz'
-  sha1 'cb4e313352974299c32bc55fe56396adb74517ef'
+  homepage "https://github.com/jessek/hashdeep"
+  url "https://github.com/jessek/hashdeep/archive/release-4.4.tar.gz"
+  sha1 "cb4e313352974299c32bc55fe56396adb74517ef"
+  head "https://github.com/jessek/hashdeep.git"
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
+  # This won't work on < Leopard due to using the CommonCrypto Library
+  # Not completely impossible to fix, but doubt the demand is there.
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
-    system "sh bootstrap.sh"
+    system "sh", "bootstrap.sh"
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system bin/"md5deep", "-h"
-    system bin/"hashdeep", "-h"
+    (testpath/"testfile.txt").write("This is a test file")
+    # Do not reduce the spacing of the below text.
+    assert_equal "91b7b0b1e27bfbf7bc646946f35fa972c47c2d32  testfile.txt",
+    shell_output("#{bin}/sha1deep -b testfile.txt").strip
   end
 end
