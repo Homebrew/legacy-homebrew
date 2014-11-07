bd1cf2278f049b22567d54b5179ac941e32856a8
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-07 23:51:00 +0000

    amap: added openssl dependency
    Additional OpenSSL dependency, and bumps the links to SSL/TLS.
    
    Closes #34005.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/amap.rb
+++ Library/Formula/amap.rb
@@ -1,9 +1,12 @@
-require 'formula'
+require "formula"
 
 class Amap < Formula
-  homepage 'http://www.thc.org/thc-amap/'
-  url 'http://www.thc.org/releases/amap-5.4.tar.gz'
-  sha1 '79056f29a3b9e0a21062116aec3e966b1a46d7d3'
+  homepage "https://www.thc.org/thc-amap/"
+  url "https://www.thc.org/releases/amap-5.4.tar.gz"
+  sha1 "79056f29a3b9e0a21062116aec3e966b1a46d7d3"
+  revision 1
+
+  depends_on "openssl"
 
   def install
     system "./configure", "--prefix=#{prefix}"
