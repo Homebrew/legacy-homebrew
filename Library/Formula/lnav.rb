a3d34b44e94f1aa605f35ded408e307b421177ef
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-07 16:12:12 +0000

    lnav: add 0.7.1 bottle.

--- Library/Formula/lnav.rb
+++ Library/Formula/lnav.rb
@@ -7,6 +7,12 @@ class Lnav < Formula
 
   head 'https://github.com/tstack/lnav.git'
 
+  bottle do
+    sha1 "34a53eb6606660c29fa1cdadb6a45af811ff7ae1" => :yosemite
+    sha1 "57e96d8f6d40a01d1e6aa31b4b7ae0d738fb30fb" => :mavericks
+    sha1 "884bd4be97c7d778e2c619d6e71d95d1cc1d7663" => :mountain_lion
+  end
+
   depends_on 'readline'
   depends_on 'pcre'
 
