e5db051e4a3bcc44d3d0b6f219344ce3f1c934e9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-07 06:50:43 +0000

    md5deep: add 4.4 bottle.

--- Library/Formula/md5deep.rb
+++ Library/Formula/md5deep.rb
@@ -6,6 +6,13 @@ class Md5deep < Formula
   sha1 "cb4e313352974299c32bc55fe56396adb74517ef"
   head "https://github.com/jessek/hashdeep.git"
 
+  bottle do
+    cellar :any
+    sha1 "6c52a36cf6b416a099e3820fc484127ec810e798" => :yosemite
+    sha1 "0ce2ae757313c78b6c2f2fc572a8f068bfedd13a" => :mavericks
+    sha1 "6e40dd2d2902ef7b01c855089cf5b28098288bdf" => :mountain_lion
+  end
+
   # This won't work on < Leopard due to using the CommonCrypto Library
   # Not completely impossible to fix, but doubt the demand is there.
   depends_on "autoconf" => :build
