5dc6d771b482fba7ce2cc8c1c8398263a7ebb1a3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-08 04:56:03 +0000

    argus: add 3.0.8 bottle.

--- Library/Formula/argus.rb
+++ Library/Formula/argus.rb
@@ -5,6 +5,13 @@ class Argus < Formula
   url "http://qosient.com/argus/src/argus-3.0.8.tar.gz"
   sha1 "fe9833c7f8ea4cdf7054b37024e5d007613f9571"
 
+  bottle do
+    cellar :any
+    sha1 "5b8ca09efc8f4f84d78883f3f866628da061928b" => :yosemite
+    sha1 "c96587d47409a7cb961450ade0b76558c1bf3f9c" => :mavericks
+    sha1 "13a6e2c690d830adddb45bce4b8b0b24a01c9249" => :mountain_lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
