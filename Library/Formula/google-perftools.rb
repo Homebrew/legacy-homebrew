147e1ca9eb0dd4c54cb1a782aa670d09d050f378
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-08 15:46:51 +0000

    google-perftools: add 2.2.1 bottle.

--- Library/Formula/google-perftools.rb
+++ Library/Formula/google-perftools.rb
@@ -5,6 +5,13 @@ class GooglePerftools < Formula
   url "https://googledrive.com/host/0B6NtGsLhIcf7MWxMMF9JdTN3UVk/gperftools-2.2.1.tar.gz"
   sha1 "f505eb467bc5b55ea3a27e3386a70331bf6e38a0"
 
+  bottle do
+    cellar :any
+    sha1 "c5f3fcc3e72965cdf5a60b3ff26e5b8fcc1f6bd2" => :yosemite
+    sha1 "c930c122d93509860ed775647b6087ff47209646" => :mavericks
+    sha1 "012d32851ca538fd32cd85db4fe8b033cfe38755" => :mountain_lion
+  end
+
   fails_with :llvm do
     build 2326
     cause "Segfault during linking"
