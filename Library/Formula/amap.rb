8a2d0f3c549ed91cd84d4d34dc91d71b5d13acee
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-08 01:55:11 +0000

    amap: add 5.4_1 bottle.

--- Library/Formula/amap.rb
+++ Library/Formula/amap.rb
@@ -6,6 +6,13 @@ class Amap < Formula
   sha1 "79056f29a3b9e0a21062116aec3e966b1a46d7d3"
   revision 1
 
+  bottle do
+    cellar :any
+    sha1 "3e5f595cd8e8427dd9e644331538be8c39997500" => :yosemite
+    sha1 "8e3eaec3b621aa8cda4f9fae3a80f66bfb53cb47" => :mavericks
+    sha1 "6b625caf2d80f33a9175d1e6370ebff677237c29" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
