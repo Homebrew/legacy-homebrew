5c125376ca4be668e2aaf7367ea413770f126520
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-08 02:29:11 +0000

    apid: added openssl dependency
    Additional OpenSSL dependency, and getting to exercise the new :apr
    dependency as well to account for the hard links to apr-1 in the
    configure & make scripts.
    
    Closes #34010.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/apib.rb
+++ Library/Formula/apib.rb
@@ -1,17 +1,21 @@
-require 'formula'
+require "formula"
 
 class Apib < Formula
-  homepage 'https://github.com/apigee/apib'
-  url 'https://github.com/apigee/apib/archive/APIB_1_0.tar.gz'
-  sha1 'd7a5a2accd6bda7efeca433141b5df44ccd7f0b0'
+  homepage "https://github.com/apigee/apib"
+  url "https://github.com/apigee/apib/archive/APIB_1_0.tar.gz"
+  sha1 "d7a5a2accd6bda7efeca433141b5df44ccd7f0b0"
+  revision 1
+
+  depends_on :apr => :build
+  depends_on "openssl"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    bin.install 'apib', 'apibmon'
+    bin.install "apib", "apibmon"
   end
 
   test do
-    system "#{bin}/apib", "-c 1", "-d 1", "http://www.google.com"
+    system "#{bin}/apib", "-c 1", "-d 1", "https://www.google.com"
   end
 end
