581f92a80500bc55373d6c4f0da0bff71bd4b7c3
Shintaro Kojima <goodies@codeout.net>
2014-11-08 16:31:22 +0900

    rancid 3.1
    Closes #34018.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/rancid.rb
+++ Library/Formula/rancid.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rancid < Formula
   homepage 'http://www.shrubbery.net/rancid/'
-  url 'ftp://ftp.shrubbery.net/pub/rancid/rancid-2.3.8.tar.gz'
-  sha1 '7469d7f9e39e9f86f977f1f0963300e5d183088f'
+  url 'ftp://ftp.shrubbery.net/pub/rancid/rancid-3.1.tar.gz'
+  sha1 '5e5bdf84634c958ad4cd413c3e31c348340ebd05'
 
   def install
     system "./configure", "--prefix=#{prefix}", "--exec-prefix=#{prefix}", "--mandir=#{man}"
@@ -12,6 +12,6 @@ class Rancid < Formula
   end
 
   test do
-    system "#{bin}/rancid", "localhost"
+    system "#{bin}/rancid", "-t", "cisco", "localhost"
   end
 end
