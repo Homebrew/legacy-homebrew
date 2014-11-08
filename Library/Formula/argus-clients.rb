a0bf0ab0738447aeafb5c237b24ecbd18bc0af5a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-08 05:02:40 +0000

    argus-clients: add 3.0.8 bottle.

--- Library/Formula/argus-clients.rb
+++ Library/Formula/argus-clients.rb
@@ -5,6 +5,13 @@ class ArgusClients < Formula
   url "http://qosient.com/argus/src/argus-clients-3.0.8.tar.gz"
   sha1 "3b9d764dc067af64670bcd8328d6f48088b9b5ca"
 
+  bottle do
+    cellar :any
+    sha1 "fd64dab110eaa36c7722c547faa2baaed55684de" => :yosemite
+    sha1 "b3d61df656a92a74cfd57a0230ffe010a5ebc0f8" => :mavericks
+    sha1 "9e8d00a518618556b804803b46777fde95ec10d7" => :mountain_lion
+  end
+
   depends_on "readline" => :recommended
   depends_on "rrdtool" => :recommended
 
