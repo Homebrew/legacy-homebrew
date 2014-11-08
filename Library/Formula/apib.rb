edc41d9d1c0c2ab930b83689c46b17924127f25c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-08 02:35:52 +0000

    apib: add 1.0_1 bottle.

--- Library/Formula/apib.rb
+++ Library/Formula/apib.rb
@@ -6,6 +6,13 @@ class Apib < Formula
   sha1 "d7a5a2accd6bda7efeca433141b5df44ccd7f0b0"
   revision 1
 
+  bottle do
+    cellar :any
+    sha1 "ffdea23d6f3e9627d3d99ec931d29f1e93f7dd9f" => :yosemite
+    sha1 "d7443be34f3d7b9347c26bfdaae5c46eab41e3c8" => :mavericks
+    sha1 "0952c22383ab1f4da194d66b823dd513a3f30ac8" => :mountain_lion
+  end
+
   depends_on :apr => :build
   depends_on "openssl"
 
