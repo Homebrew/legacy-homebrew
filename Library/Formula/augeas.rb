fc553e0924a5d0f993c8e120c05a55a5aee4c0b9
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-09 06:08:20 +0000

    augeas 1.3.0
    Version bump.
    
    Closes #34037.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/augeas.rb
+++ Library/Formula/augeas.rb
@@ -1,41 +1,41 @@
-require 'formula'
+require "formula"
 
 class Augeas < Formula
-  homepage 'http://augeas.net'
-  url 'http://download.augeas.net/augeas-1.2.0.tar.gz'
-  sha1 'ab63548ae5462d7b3dc90e74311b8e566ba22485'
+  homepage "http://augeas.net"
+  url "http://download.augeas.net/augeas-1.3.0.tar.gz"
+  sha1 "052694bc84e3b8246dd32808b0e0e8c41c3de40b"
 
   head do
-    url 'https://github.com/hercules-team/augeas.git'
+    url "https://github.com/hercules-team/augeas.git"
 
     depends_on :autoconf
     depends_on :automake
     depends_on :libtool
-    depends_on 'bison' => :build
+    depends_on "bison" => :build
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libxml2'
-  depends_on 'readline'
+  depends_on "pkg-config" => :build
+  depends_on "libxml2"
+  depends_on "readline"
 
   def install
     if build.head?
       # The bootstrap script run by autogen needs to check the state of the
       # gnulib submodule.
-      ln_s cached_download + '.git', '.git'
-      ln_s cached_download + '.gnulib/.git', '.gnulib/.git'
+      ln_s cached_download + ".git", ".git"
+      ln_s cached_download + ".gnulib/.git", ".gnulib/.git"
 
-      system './autogen.sh'
+      system "./autogen.sh"
     end
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
 
     # libfa example program doesn't compile cleanly on OSX, so skip it
-    inreplace 'Makefile' do |s|
+    inreplace "Makefile" do |s|
       s.change_make_var! "SUBDIRS", "gnulib/lib src gnulib/tests tests man doc"
     end
 
-    system "make install"
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
