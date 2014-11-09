47bb77f688e57c5b4aa058e51bf3fbd506b87937
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-09 17:30:54 +0000

    dfu-util: add 0.8 bottle.

--- Library/Formula/dfu-util.rb
+++ Library/Formula/dfu-util.rb
@@ -6,6 +6,13 @@ class DfuUtil < Formula
   url "http://ftp.de.debian.org/debian/pool/main/d/dfu-util/dfu-util_0.8.orig.tar.gz"
   sha1 "164551ca40f0c569eb7ae3263a9945a1ef3fed4d"
 
+  bottle do
+    cellar :any
+    sha1 "285f3043fe09291b4485fe3c4dac36025489bca8" => :yosemite
+    sha1 "ed8be24bc65f7fc2791c42311be08f67649b6e23" => :mavericks
+    sha1 "cd533314e4700a3d746330b12aa08407f693d7a4" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libusb"
 
