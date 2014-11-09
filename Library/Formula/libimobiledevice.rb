7f143fbe11e274d74b907ef3cc9980f5d35a5b46
Chloe Stars <hachidorii@icloud.com>
2014-11-09 04:52:05 -0800

    libimobiledevice 1.1.7
    Closes #34045.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libimobiledevice.rb
+++ Library/Formula/libimobiledevice.rb
@@ -2,13 +2,11 @@ require "formula"
 
 class Libimobiledevice < Formula
   homepage "http://www.libimobiledevice.org/"
-  url "http://www.libimobiledevice.org/downloads/libimobiledevice-1.1.6.tar.bz2"
-  sha1 "3016bf1241bc5a77a621c49d82d71bb8f32905e4"
-  revision 1
+  url "http://www.libimobiledevice.org/downloads/libimobiledevice-1.1.7.tar.bz2"
+  sha1 "ac47ba39e7f8d8cb9379756773ece30458b93c80"
 
   bottle do
     cellar :any
-    revision 2
     sha1 "3798eed87ba1f8eac58fa4ceb5fc53b0c84c3612" => :yosemite
     sha1 "89ef56d797d9ed5ff2c4cd5477814473c19cf3ab" => :mavericks
     sha1 "4d7720e3cffd8a5d555f6c013475abee9e06ceb6" => :mountain_lion
