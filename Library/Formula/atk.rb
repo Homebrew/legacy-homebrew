d84180468be16a30fc3a2df32930d0694e010a52
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-09 05:06:34 +0000

    atk 2.15.1
    Version bump. Dependents all work fine without recompile.

--- Library/Formula/atk.rb
+++ Library/Formula/atk.rb
@@ -2,11 +2,10 @@ require "formula"
 
 class Atk < Formula
   homepage "http://library.gnome.org/devel/atk/"
-  url "http://ftp.gnome.org/pub/gnome/sources/atk/2.14/atk-2.14.0.tar.xz"
-  sha256 "2875cc0b32bfb173c066c22a337f79793e0c99d2cc5e81c4dac0d5a523b8fbad"
+  url "http://ftp.gnome.org/pub/gnome/sources/atk/2.15/atk-2.15.1.tar.xz"
+  sha256 "476e5147eb9ee5c8a6e91437d82189c3449dde127b3b6d90de6817565c164cc1"
 
   bottle do
-    revision 1
     sha1 "5a014bce43ff14675bec23b61909d3d85cff20f1" => :yosemite
     sha1 "f75b7b55547cb58c87fd35e38e8cdba0877516f8" => :mavericks
     sha1 "8aa05a84f58854ba26258d0d206c4e2fb663eb16" => :mountain_lion
@@ -24,6 +23,6 @@ class Atk < Formula
                           "--prefix=#{prefix}",
                           "--enable-introspection=yes"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
