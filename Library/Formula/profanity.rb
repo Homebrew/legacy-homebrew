11723bebd9d1a9bcaec6ebe986e4f9f67f750a33
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-10 01:26:05 +0000

    profanity: update 0.4.5 bottle.

--- Library/Formula/profanity.rb
+++ Library/Formula/profanity.rb
@@ -7,9 +7,9 @@ class Profanity < Formula
   head "https://github.com/boothj5/profanity.git"
 
   bottle do
-    sha1 "60efcafb41f98c6c19e3404b536fdc405b2f42fb" => :mavericks
-    sha1 "e7e4ecc29339d07e9e898b0c93a887a0007f42aa" => :mountain_lion
-    sha1 "040472aa3b48d4470fb652b3ce16c73e5c88983b" => :lion
+    sha1 "f53fc5631fa1b079414420275278e55d6225798d" => :yosemite
+    sha1 "240feb4c76b4ddb3170792710fa011cb90c302d4" => :mavericks
+    sha1 "2c5d854f76597d6b0aa55f4daad3aad3027f3bc4" => :mountain_lion
   end
 
   depends_on "autoconf" => :build
