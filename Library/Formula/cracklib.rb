522c93a86f456ca15afb4e25d699923d664f9bb8
Jack Nagel <jacknagel@gmail.com>
2014-11-10 11:42:06 -0600

    cracklib 2.9.2

--- Library/Formula/cracklib.rb
+++ Library/Formula/cracklib.rb
@@ -1,16 +1,11 @@
-require 'formula'
+require "formula"
 
 class Cracklib < Formula
-  homepage 'http://cracklib.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/cracklib/cracklib/2.9.0/cracklib-2.9.0.tar.gz'
-  sha1 '827dcd24b14bf23911c34f4226b4453b24f949a3'
+  homepage "http://cracklib.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/cracklib/cracklib/2.9.2/cracklib-2.9.2.tar.gz"
+  sha1 "a780211a87a284297aa473fe2b50584b842a0e98"
 
   bottle do
-    cellar :any
-    revision 1
-    sha1 "1d4005c5e5401a3104255efb1ae88f5396984f2c" => :yosemite
-    sha1 "232a22e23e25dd71405fe869050dfd82bb5141e0" => :mavericks
-    sha1 "50d5faf0c5c3a46985382340e311263de9c363a6" => :mountain_lion
   end
 
   depends_on "gettext"
@@ -19,8 +14,9 @@ class Cracklib < Formula
     ENV.deparallelize
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
+                          "--sbindir=#{bin}",
                           "--without-python",
                           "--with-default-dict=#{HOMEBREW_PREFIX}/share/cracklib-words"
-    system "make install"
+    system "make", "install"
   end
 end
