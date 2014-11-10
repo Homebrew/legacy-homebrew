423b451b763f9ea42b9f8fb72fce6ef75b2dbbeb
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-10 16:12:21 +0000

    media-info: add 0.7.71 bottle.

--- Library/Formula/media-info.rb
+++ Library/Formula/media-info.rb
@@ -6,6 +6,13 @@ class MediaInfo < Formula
   version "0.7.71"
   sha1 "d650d3202d1aa2b8af02a7ca53e04ee2c0f227d0"
 
+  bottle do
+    cellar :any
+    sha1 "93961776e55ad1c47a989f921426b8984ec5ae92" => :yosemite
+    sha1 "25bdcd8df95d77095eb304ed4633ddea6c6dee0c" => :mavericks
+    sha1 "e580796e61e1f30b05ac376089a3ec000b416a47" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   # fails to build against Leopard's older libcurl
   depends_on "curl" if MacOS.version < :snow_leopard
