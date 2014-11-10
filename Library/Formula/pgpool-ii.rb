1c22c53e78bc6c69ce0b42abdd0db437665ec3e0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-10 22:27:00 +0000

    pgpool-ii: add 3.4.0 bottle.

--- Library/Formula/pgpool-ii.rb
+++ Library/Formula/pgpool-ii.rb
@@ -5,6 +5,12 @@ class PgpoolIi < Formula
   url "http://www.pgpool.net/download.php?f=pgpool-II-3.4.0.tar.gz"
   sha1 "5502268055b6ba48013c1b7c9ac5a8ce3a0d30ed"
 
+  bottle do
+    sha1 "b05f96a3f5d0ff02afea65aa50fa33ca1f75defb" => :yosemite
+    sha1 "621632adc180b378e427e21587c0c005e8b125db" => :mavericks
+    sha1 "26d1434b15ce5f716012bc204b8b01e05b0574c7" => :mountain_lion
+  end
+
   depends_on :postgresql
 
   def install
