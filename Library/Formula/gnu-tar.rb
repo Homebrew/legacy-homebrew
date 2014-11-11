c4ecb2d15a26169bb992b327d5d0597bcee226f0
Shaun Jackman <sjackman@gmail.com>
2014-11-10 17:56:31 -0800

    gnu-tar 1.28: Add option --with-default-names
    Closes #34086.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gnu-tar.rb
+++ Library/Formula/gnu-tar.rb
@@ -6,6 +6,8 @@ class GnuTar < Formula
   mirror "http://ftp.gnu.org/gnu/tar/tar-1.28.tar.gz"
   sha1 "cd30a13bbfefb54b17e039be7c43d2592dd3d5d0"
 
+  option "with-default-names", "Do not prepend 'g' to the binary"
+
   bottle do
     revision 2
     sha1 "bc61f3210e6f8adaade8abe7e8bed4542ead62e2" => :yosemite
@@ -22,22 +24,24 @@ class GnuTar < Formula
 
   def install
     args = ["--prefix=#{prefix}", "--mandir=#{man}"]
-    args << "--program-prefix=g"
+    args << "--program-prefix=g" if build.without? "default-names"
 
     system "./configure", *args
     system "make", "install"
 
     # Symlink the executable into libexec/gnubin as "tar"
-    (libexec/"gnubin").install_symlink bin/"gtar" => "tar"
+    (libexec/"gnubin").install_symlink bin/"gtar" => "tar" if build.without? "default-names"
   end
 
-  def caveats; <<-EOS.undent
-    gnu-tar has been installed as "gtar".
+  def caveats
+    if build.without? "default-names" then <<-EOS.undent
+      gnu-tar has been installed as "gtar".
 
-    If you really need to use it as "tar", you can add a "gnubin" directory
-    to your PATH from your bashrc like:
+      If you really need to use it as "tar", you can add a "gnubin" directory
+      to your PATH from your bashrc like:
 
-        PATH="#{opt_libexec}/gnubin:$PATH"
-    EOS
+          PATH="#{opt_libexec}/gnubin:$PATH"
+      EOS
+    end
   end
 end
