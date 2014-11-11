176df29a07025ce8c903911a3fbd6fcdf58a02b1
mike@arpaia.co <mike@arpaia.co>
2014-11-11 09:41:45 -0500

    osquery 1.0.5
    Closes #34099.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/osquery.rb
+++ Library/Formula/osquery.rb
@@ -3,8 +3,7 @@ require "formula"
 class Osquery < Formula
   homepage "http://osquery.io"
   # pull from git tag to get submodules
-  url "https://github.com/facebook/osquery.git", :tag => "1.0.3"
-  sha1 "529d9a9abc0eb282fd0e61884e2c9f0ee24eddd0"
+  url "https://github.com/facebook/osquery.git", :tag => "1.0.5"
 
   bottle do
     cellar :any
@@ -13,7 +12,7 @@ class Osquery < Formula
   end
 
   # Build currently fails on Mountain Lion:
-  # https://github.com/facebook/osquery/issues/277
+  # https://github.com/facebook/osquery/issues/409
   # Will welcome PRs to fix this!
   depends_on :macos => :mavericks
 
@@ -34,12 +33,6 @@ class Osquery < Formula
   resource "jinja2" do
     url "https://pypi.python.org/packages/source/J/Jinja2/Jinja2-2.7.3.tar.gz"
     sha1 "25ab3881f0c1adfcf79053b58de829c5ae65d3ac"
-  end
-
-  # Fix build on mountain lion (https://github.com/facebook/osquery/issues/277)
-  patch do
-    url "https://github.com/facebook/osquery/commit/cd7454.diff"
-    sha1 "0555bef180598a8846a3aa5d27db4d3a37b5ba2e"
   end
 
   def install
