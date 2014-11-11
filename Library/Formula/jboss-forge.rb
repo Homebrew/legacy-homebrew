8fe5a13c4f7c4ec7f77bd7ff001cffa01d412518
George Gastaldi <gegastaldi@gmail.com>
2014-11-11 18:24:58 -0200

    Fixed SHA1

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -4,7 +4,7 @@ class JbossForge < Formula
   homepage 'http://forge.jboss.org/'
   url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.12.2.Final&e=zip&c=offline'
   version '2.12.2.Final'
-  sha1 '77616bdb2022d19c6dcc95d8008148119b278b33'
+  sha1 'ec682cbda6fbdea7155dc00d6af9338a38b12cc6'
 
   def install
     rm_f Dir["bin/*.bat"]
