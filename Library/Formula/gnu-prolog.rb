e6be5be5ad1a6a59ef726784ea269c9264bbb7f0
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-11 17:24:36 +0000

    gnu-prolog: add 1.4.4 bottle.

--- Library/Formula/gnu-prolog.rb
+++ Library/Formula/gnu-prolog.rb
@@ -5,6 +5,12 @@ class GnuProlog < Formula
   url 'http://gprolog.univ-paris1.fr/gprolog-1.4.4.tar.gz'
   sha1 '658b0efa5d916510dcddbbd980d90bc4d43a6e58'
 
+  bottle do
+    sha1 "8dc1b5782b6fb5e89c62fe3f5b07a908ddff6850" => :yosemite
+    sha1 "faf4b9bd8c63bb89bc2217cde8ce0ea00866ddf8" => :mavericks
+    sha1 "92366ec981d47a8f30a602b1b40cb4a1de99922c" => :mountain_lion
+  end
+
   # Upstream patch:
   # http://sourceforge.net/p/gprolog/code/ci/784b3443a0a2f087c1d1e7976739fa517efe6af6
   patch do
