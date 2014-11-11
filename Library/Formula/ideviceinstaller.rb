0f386ad96a5079a73705e6b5a3c035b56d8de70b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-11 00:03:37 +0000

    ideviceinstaller: add 1.1.0 bottle.

--- Library/Formula/ideviceinstaller.rb
+++ Library/Formula/ideviceinstaller.rb
@@ -5,6 +5,13 @@ class Ideviceinstaller < Formula
   url "http://www.libimobiledevice.org/downloads/ideviceinstaller-1.1.0.tar.bz2"
   sha1 "5e2c47b9e6ac6d610b7bfe5186c8e84536549ce4"
 
+  bottle do
+    cellar :any
+    sha1 "3c255cc07204fe0b7b9715cc8e9f5caf46f496e9" => :yosemite
+    sha1 "883165e904a0d4dde166597efc1f709598f2a8b8" => :mavericks
+    sha1 "23fbfb97b7ce046ac3348ad47914d28bd2165ba9" => :mountain_lion
+  end
+
   head do
     url "http://git.sukimashita.com/ideviceinstaller.git"
     depends_on "autoconf" => :build
