a81f020cfced139a14e6bd16f6c24632843d9f4e
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-11 20:13:13 +0000

    libmp3splt 0.9.2
    Closes #34107.
    Closes #34108.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libmp3splt.rb
+++ Library/Formula/libmp3splt.rb
@@ -1,29 +1,30 @@
-require 'formula'
+require "formula"
 
 class Libmp3splt < Formula
-  homepage 'http://mp3splt.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/mp3splt/libmp3splt/0.8.2/libmp3splt-0.8.2.tar.gz'
-  sha1 '5c8539391e26d047c30360b1dde2c08e6a02061f'
+  homepage "http://mp3splt.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/mp3splt/libmp3splt/0.9.2/libmp3splt-0.9.2.tar.gz"
+  sha1 "d4e84e1f466e7fdabe30d0a12acb751903bb9203"
 
   bottle do
-    revision 1
     sha1 "b78ccf811dd3878091ed2eb3285b45347e6babcc" => :yosemite
     sha1 "3e269de0fd2478cf5427d8992c30d7e2407da55d" => :mavericks
     sha1 "8d9aac62753ff445006c23bc323323592e605c63" => :mountain_lion
   end
 
-  depends_on 'libtool' => :build
-  depends_on 'pkg-config' => :build
-  depends_on 'gettext'
-  depends_on 'pcre'
-  depends_on 'libid3tag'
-  depends_on 'mad'
-  depends_on 'libvorbis'
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "pcre"
+  depends_on "libid3tag"
+  depends_on "mad"
+  depends_on "libvorbis"
+  depends_on "flac"
+  depends_on "libogg"
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
