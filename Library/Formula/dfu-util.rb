7f85c1c19516b3e5c9f9e7254c44d323b6f155a8
Liam Staskawicz <liam@stask.net>
2014-11-11 08:57:25 -0600

    dfu-util: add head

--- Library/Formula/dfu-util.rb
+++ Library/Formula/dfu-util.rb
@@ -5,10 +5,19 @@ class DfuUtil < Formula
   url 'http://dfu-util.gnumonks.org/releases/dfu-util-0.7.tar.gz'
   sha1 'a6f621a17a164bd86d2752e2fcd3903f94a8925b'
 
+  head do
+    url "git://gitorious.org/dfu-util/dfu-util.git"
+
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool' => :build
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libusb'
 
   def install
+    system "autoreconf", "-fi" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make install"
