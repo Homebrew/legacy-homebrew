cee42eb405e561491f86d648364ca615b88a64d0
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-12 19:10:05 +0000

    tup: yosemite fix
    Closes #33760. Not accepted upstream yet, but it does work, albeit
    generating a non-fatal error.

--- Library/Formula/tup.rb
+++ Library/Formula/tup.rb
@@ -16,6 +16,15 @@ class Tup < Formula
   depends_on "pkg-config" => :build
   depends_on :osxfuse
 
+  # Fixes the renaming of functions on OS X 10.10
+  # https://github.com/gittup/tup/issues/204
+  if MacOS.version >= :yosemite
+    patch do
+      url "https://github.com/gittup/tup/pull/205.diff"
+      sha1 "eebe8293b18096361130d0a97e0f10c524a716b3"
+    end
+  end
+
   def install
     ENV["TUP_LABEL"] = version
     system "./build.sh"
