5110c91d1c10320d564bd695f9e8b0c975c252e6
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-12 21:14:18 +0000

    gnu-units: option depreciation
    Depreciates the ‘default-names’ option, and lobs in a test.
    
    Closes #34138.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gnu-units.rb
+++ Library/Formula/gnu-units.rb
@@ -1,18 +1,24 @@
-require 'formula'
+require "formula"
 
 class GnuUnits < Formula
-  homepage 'http://www.gnu.org/software/units/'
-  url 'http://ftpmirror.gnu.org/units/units-2.02.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/units/units-2.02.tar.gz'
-  sha1 'e460371dc97034d17ce452e6b64991f7fd2d1409'
+  homepage "https://www.gnu.org/software/units/"
+  url "http://ftpmirror.gnu.org/units/units-2.02.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/units/units-2.02.tar.gz"
+  sha1 "e460371dc97034d17ce452e6b64991f7fd2d1409"
 
-  option 'default-names', "Do not prepend 'g' to the binary"
+  deprecated_option "default-names" => "with-default-names"
+
+  option "with-default-names", "Do not prepend 'g' to the binary"
 
   def install
     args = ["--prefix=#{prefix}"]
-    args << "--program-prefix=g" unless build.include? 'default-names'
+    args << "--program-prefix=g" if build.without? "default-names"
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "* 18288", shell_output("#{bin}/gunits '600 feet' 'cm' -1").strip
   end
 end
