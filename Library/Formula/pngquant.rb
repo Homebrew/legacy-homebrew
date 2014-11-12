f3b5665234cc1c7ee0fa3314542d5c67aca8b67d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-12 18:15:27 +0000

    pngquant: add 2.3.1 bottle.

--- Library/Formula/pngquant.rb
+++ Library/Formula/pngquant.rb
@@ -7,6 +7,13 @@ class Pngquant < Formula
 
   head "https://github.com/pornel/pngquant.git"
 
+  bottle do
+    cellar :any
+    sha1 "4c4b52ecd2d0c6e02e6086717277eca258e96239" => :yosemite
+    sha1 "73d1b4d1addd90e6c00497ff9403f97b0f6bc855" => :mavericks
+    sha1 "5a8b9d3200c479ed3cf7ee0af43dc127c848eacc" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libpng"
 
