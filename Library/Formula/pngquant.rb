64298ecc45e9a020dd3402ece17cd4cc54defae6
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-12 18:02:32 +0000

    pngquant 2.3.1
    Closes #34126.

--- Library/Formula/pngquant.rb
+++ Library/Formula/pngquant.rb
@@ -1,19 +1,20 @@
-require 'formula'
+require "formula"
 
 class Pngquant < Formula
-  homepage 'http://pngquant.org/'
-  url 'https://github.com/pornel/pngquant/archive/2.2.0.tar.gz'
-  sha1 'd7ed6f13fdbef52074cbaee5452a2e7e27573376'
+  homepage "http://pngquant.org/"
+  url "https://github.com/pornel/pngquant/archive/2.3.1.tar.gz"
+  sha1 "964987f24012b247be666903ab4e31f53d84d80c"
 
-  head 'https://github.com/pornel/pngquant.git'
+  head "https://github.com/pornel/pngquant.git"
 
-  depends_on 'libpng'
+  depends_on "pkg-config" => :build
+  depends_on "libpng"
 
   def install
     ENV.append_to_cflags "-DNDEBUG" # Turn off debug
     system "make", "PREFIX=#{prefix}", "CC=#{ENV.cc}"
-    bin.install 'pngquant'
-    man1.install 'pngquant.1'
+    bin.install "pngquant"
+    man1.install "pngquant.1"
   end
 
   test do
