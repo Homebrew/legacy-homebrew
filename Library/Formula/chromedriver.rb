c1983a73ee76aab9a239080eb5de149acfa2f268
Jack Nagel <jacknagel@gmail.com>
2014-11-11 20:35:19 -0600

    chromedriver: update homepage

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Chromedriver < Formula
-  homepage 'http://code.google.com/p/chromedriver/'
+  homepage 'https://sites.google.com/a/chromium.org/chromedriver/'
   url 'http://chromedriver.storage.googleapis.com/2.12/chromedriver_mac32.zip'
   sha256 'f93464dd4e57f7d59601e9ab92ad0770493f9ea00b69ddba8da3886c35852d4b'
   version '2.12'
