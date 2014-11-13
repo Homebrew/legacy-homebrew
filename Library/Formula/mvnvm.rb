d91bd2d393e6b3b0cd345ac740de4988cf64c8f5
Patrick Roumanoff <patrick@roumanoff.com>
2014-11-14 10:41:38 +1100

    remove empty line and use double quotes everywhere

--- Library/Formula/mvnvm.rb
+++ Library/Formula/mvnvm.rb
@@ -10,14 +10,14 @@ class Mvnvm < Formula
     bin.install "mvn"
   end
 
-  conflicts_with 'maven',
-    :because => 'instead of installing Maven via Homebrew, you should use mvnvm to manage your Maven versions.'
+  conflicts_with "maven",
+    :because => "instead of installing Maven via Homebrew, you should use mvnvm to manage your Maven versions."
 
   test do
-    (testpath/'mvnvm.properties').write <<-EOS.undent
+    (testpath/"mvnvm.properties").write <<-EOS.undent
       mvn_version=3.0.5
     EOS
-    (testpath/'pom.xml').write <<-EOS.undent
+    (testpath/"pom.xml").write <<-EOS.undent
       <?xml version="1.0" encoding="UTF-8"?>
       <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
@@ -25,10 +25,9 @@ class Mvnvm < Formula
         <groupId>org.homebrew</groupId>
         <artifactId>maven-test</artifactId>
         <version>1.0.0-SNAPSHOT</version>
-        
       </project>
     EOS
-    (testpath/'src/main/java/org/homebrew/MavenTest.java').write <<-EOS.undent
+    (testpath/"src/main/java/org/homebrew/MavenTest.java").write <<-EOS.undent
       package org.homebrew;
       public class MavenTest {
         public static void main(String[] args) {
