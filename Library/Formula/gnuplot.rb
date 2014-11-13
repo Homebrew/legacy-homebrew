aed1851b8e6604cb3617639fee4ce6cc3ddcb780
Jack Nagel <jacknagel@gmail.com>
2014-11-13 12:08:06 -0600

    gnuplot: update x11 option

--- Library/Formula/gnuplot.rb
+++ Library/Formula/gnuplot.rb
@@ -28,7 +28,6 @@ class Gnuplot < Formula
 
   option 'pdf',    'Build the PDF terminal using pdflib-lite'
   option 'wx',     'Build the wxWidgets terminal using pango'
-  option 'with-x', 'Build the X11 terminal'
   option 'qt',     'Build the Qt4 terminal'
   option 'cairo',  'Build the Cairo based terminals'
   option 'nolua',  'Build without the lua/TikZ terminal'
@@ -38,6 +37,8 @@ class Gnuplot < Formula
   option 'latex',  'Build with LaTeX support'
   option 'with-aquaterm', 'Build with AquaTerm support'
 
+  deprecated_option "with-x" => "with-x11"
+
   depends_on 'pkg-config' => :build
   depends_on LuaRequirement unless build.include? 'nolua'
   depends_on 'readline'
@@ -46,7 +47,7 @@ class Gnuplot < Formula
   depends_on "libtiff"
   depends_on "fontconfig"
   depends_on 'pango'       if build.include? 'cairo' or build.include? 'wx'
-  depends_on :x11          if build.with? "x"
+  depends_on :x11 => :optional
   depends_on 'pdflib-lite' if build.include? 'pdf'
   depends_on 'gd'          unless build.include? 'nogd'
   depends_on 'wxmac'       if build.include? 'wx'
@@ -95,7 +96,7 @@ class Gnuplot < Formula
     args << '--without-lisp-files'    if build.without? "emacs"
     args << (build.with?('aquaterm') ? '--with-aquaterm' : '--without-aquaterm')
 
-    if build.with? "x"
+    if build.with? "x11"
       args << "--with-x"
     else
       args << "--without-x"
