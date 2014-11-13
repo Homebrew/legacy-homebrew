c29ca1e4a69cc49e5397d31ef5905b7f5fcc20cc
Patrick Roumanoff <patrick@roumanoff.com>
2014-11-13 15:36:54 +1100

    version, hash,  test, comments
    * used today tar.gz, 
    * updated hash, 
    * changed test to copy the maven one + mvnvm.properties
    * removed comments

--- Library/Formula/mvnvm.rb
+++ Library/Formula/mvnvm.rb
@@ -1,28 +1,41 @@
 require "formula"
 
-# Homebrew formula to install mvnvm, the Maven Version Manager, see http://mvnvm.org
-
 class Mvnvm < Formula
   homepage "http://mvnvm.org"
-  url "https://bitbucket.org/mjensen/mvnvm/get/master.tar.gz"
-  version "0.2"  # Bump this and update the sha1 below when master contains a change to distribute
-  sha1 "8145a8965ea72dcb2b61737ba94a1a7cd6cf3360" # brew install tells us if this is wrong or missing
+  url "https://bitbucket.org/mjensen/mvnvm/get/0d4318ea71a2.zip"
+  version "0.0.1-20141113"
+  sha1 "9a6840ded35ce30bc58a573ea3757e5b2977c27d"
 
   def install
-    bin.install "mvn" # Sets the executable flag for us
-    ohai "If you previously installed mvnvm manually, please remove it using `rm -f ~/bin/mvn`"
-    ohai "And if you have any other Maven installations on your path, remove them too."
-    ohai ""
-    ohai "Running `mvn -version` should now output something like `[MVNVM] Using maven: x.y.z`, etc."
+    bin.install "mvn"
   end
 
   conflicts_with 'maven',
     :because => 'instead of installing Maven via Homebrew, you should use mvnvm to manage your Maven versions.'
 
   test do
-    # Run the test with `brew test mvnvm`
-    # A better test would be checking that the output of the command below
-    # starts with `[MVNVM] Using maven:`, but I'm not sure how to do that.
-    system "mvn", "-version"
+    (testpath/'mvnvm.properties').write <<-EOS.undent
+      mvn_version=3.0.5
+    EOS
+    (testpath/'pom.xml').write <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+        <modelVersion>4.0.0</modelVersion>
+        <groupId>org.homebrew</groupId>
+        <artifactId>maven-test</artifactId>
+        <version>1.0.0-SNAPSHOT</version>
+        
+      </project>
+    EOS
+    (testpath/'src/main/java/org/homebrew/MavenTest.java').write <<-EOS.undent
+      package org.homebrew;
+      public class MavenTest {
+        public static void main(String[] args) {
+          System.out.println("Testing Maven with Homebrew!");
+        }
+      }
+    EOS
+    system "#{bin}/mvn", 'compile'
   end
 end
