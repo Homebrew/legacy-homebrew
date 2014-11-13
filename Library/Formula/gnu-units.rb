bf9c87fde11791cd09dfc23996c3fc263a6fac18
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-13 00:51:10 +0000

    gnu-units: add 2.02 bottle.

--- Library/Formula/gnu-units.rb
+++ Library/Formula/gnu-units.rb
@@ -6,6 +6,12 @@ class GnuUnits < Formula
   mirror "http://ftp.gnu.org/gnu/units/units-2.02.tar.gz"
   sha1 "e460371dc97034d17ce452e6b64991f7fd2d1409"
 
+  bottle do
+    sha1 "255fd50fc880467483ae2654d1c34cd452247847" => :yosemite
+    sha1 "43beaf9b66127bd29a393e2386c2c9a53522762f" => :mavericks
+    sha1 "7d9b3438fbfeaa0d8a428a1ed6496df9d1c92cc6" => :mountain_lion
+  end
+
   deprecated_option "default-names" => "with-default-names"
 
   option "with-default-names", "Do not prepend 'g' to the binary"
