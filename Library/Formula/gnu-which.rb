dc2f097118c0a9a7dff1d7ab472957c4d48eb19b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-13 03:40:26 +0000

    gnu-which: add 2.20 bottle.

--- Library/Formula/gnu-which.rb
+++ Library/Formula/gnu-which.rb
@@ -7,6 +7,13 @@ class GnuWhich < Formula
   mirror "https://ftp.gnu.org/gnu/which/which-2.20.tar.gz"
   sha1 "3bcd6d87aa5231917ba7123319eedcae90cfa0fd"
 
+  bottle do
+    cellar :any
+    sha1 "567d232b6ad4221f446d2862a8f9e230d0a51eba" => :yosemite
+    sha1 "18afa8a2d0e56b74c0d22a02dd24b46635d2e14b" => :mavericks
+    sha1 "705c8aa57e1f67b7e9d61c36088902f0562aa2bf" => :mountain_lion
+  end
+
   deprecated_option "default-names" => "with-default-names"
 
   option "with-default-names", "Do not prepend 'g' to the binary"
