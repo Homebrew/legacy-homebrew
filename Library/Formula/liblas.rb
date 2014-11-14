5d11ed21f9d7f12c9dca1dfbafcd42e65d58cf60
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-14 09:05:32 +0000

    liblas: update 1.8.0 bottle.

--- Library/Formula/liblas.rb
+++ Library/Formula/liblas.rb
@@ -8,11 +8,9 @@ class Liblas < Formula
   head 'https://github.com/libLAS/libLAS.git'
 
   bottle do
-    cellar :any
-    revision 1
-    sha1 "9a0b8ec271d2d3d270cc0915084783197b9fc4d5" => :yosemite
-    sha1 "6be05807ebaa6ef9ca82498f4ca5ab2e13e3c450" => :mavericks
-    sha1 "864e456c0e74137730b55caa5e89c772f4bcfa60" => :mountain_lion
+    sha1 "7a8b736fe0e8637d641d26acf9328dca93d04c2a" => :yosemite
+    sha1 "15436841f3d688acfb17f537ebf1e808a462bdb2" => :mavericks
+    sha1 "520f9bd2e768601ab1b6ea6dde72c4714090bba2" => :mountain_lion
   end
 
   depends_on 'cmake' => :build
