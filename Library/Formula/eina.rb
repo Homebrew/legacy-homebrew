c4a2e191ca02f1d6a0afc921e8a2843562eae511
Jack Nagel <jacknagel@gmail.com>
2014-11-14 17:25:07 -0600

    eina 1.7.10

--- Library/Formula/eina.rb
+++ Library/Formula/eina.rb
@@ -1,30 +1,28 @@
-require 'formula'
+require "formula"
 
 class Eina < Formula
-  homepage 'http://trac.enlightenment.org/e/wiki/Eina'
-  url 'http://download.enlightenment.org/releases/eina-1.7.9.tar.gz'
-  sha1 '26f385e888b29876c8fb06f35f0433ab7e3251d5'
+  homepage "http://trac.enlightenment.org/e/wiki/Eina"
+  url "http://download.enlightenment.org/releases/eina-1.7.10.tar.gz"
+  sha1 "c2544357b27f59d1592a02228cb277dc6b4ef797"
 
   bottle do
-    sha1 "93a7d9b07e718f429b3d93f3e9143d8108f944ef" => :yosemite
-    sha1 "ae012b17e012a4c628bfa794b56a4c3631e83f4c" => :mavericks
-    sha1 "40d088362766ee5cbec76517e3fb3e128d2a4738" => :mountain_lion
   end
 
   head do
-    url 'http://svn.enlightenment.org/svn/e/trunk/eina/'
+    url "http://svn.enlightenment.org/svn/e/trunk/eina/"
 
-    depends_on :autoconf
-    depends_on :automake
-    depends_on :libtool
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
   end
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
 
   def install
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
