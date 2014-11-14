f7fd37b6524f47213224c4d2e3c95574774d4c26
Jon Koops <jonkoops@gmail.com>
2014-11-14 23:08:14 +0100

    Updated android-platform-tools to v21.0.0
    Apparently this version was released more recently and cannot be found in the XML feed from the comment. Might as well update to the latest version nonetheless.

--- Library/Formula/android-platform-tools.rb
+++ Library/Formula/android-platform-tools.rb
@@ -4,9 +4,9 @@ class AndroidPlatformTools < Formula
   homepage "http://developer.android.com/sdk"
   # the url is from:
   # http://dl-ssl.google.com/android/repository/repository-8.xml
-  url "http://dl-ssl.google.com/android/repository/platform-tools_r20-macosx.zip"
-  version '20.0.0'
-  sha1 "f2c65c58caf76169d9bebf25eef5c69ff99670b5"
+  url "http://dl-ssl.google.com/android/repository/platform-tools_r21-macosx.zip"
+  version '21.0.0'
+  sha1 "6675f9f583841972c5c5ef8d2c131e1209529fde"
 
   conflicts_with 'android-sdk',
     :because => "the Android Platform-tools are part of the Android SDK"
