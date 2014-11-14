61483e1421ffaab824c916421d4d0f91327077da
Daniel Norberg <dano@spotify.com>
2014-11-14 12:09:57 +0900

    helios: bump to version 0.8.94

--- Library/Formula/helios.rb
+++ Library/Formula/helios.rb
@@ -4,7 +4,7 @@ class Helios < Formula
   homepage "https://github.com/spotify/helios"
   url "https://oss.sonatype.org/service/local/repositories/releases/content/com/spotify/helios-tools/0.8.77/helios-tools-0.8.77-shaded.jar"
   sha1 "15684178cf0a7d07db425a50f7371749a122846e"
-  version "0.8.77"
+  version "0.8.94"
 
   depends_on :java => "1.7"
 
