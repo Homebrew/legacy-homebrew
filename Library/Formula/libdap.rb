e43e1fcc7469d534a162f4769df1d859e2584920
Jack Nagel <jacknagel@gmail.com>
2014-11-13 22:11:19 -0600

    libdap: add libxml2 dependency
    For consistency with other gdal dependencies.

--- Library/Formula/libdap.rb
+++ Library/Formula/libdap.rb
@@ -4,15 +4,13 @@ class Libdap < Formula
   homepage 'http://www.opendap.org'
   url 'http://www.opendap.org/pub/source/libdap-3.12.1.tar.gz'
   sha1 'bfb72dd3035e7720b1ada0bf762b9ab80bb6bbf2'
+  revision 1
 
   bottle do
-    revision 1
-    sha1 "34baca09631ed6f58e82579854e025bb032a4127" => :yosemite
-    sha1 "feda9fde7dc1898e19442d51e61959605ae182f8" => :mavericks
-    sha1 "a4d7b53a3f20d3bc19e0a24c1b50b8b7bffc051c" => :mountain_lion
   end
 
   depends_on 'pkg-config' => :build
+  depends_on "libxml2"
 
   def install
     # NOTE:
