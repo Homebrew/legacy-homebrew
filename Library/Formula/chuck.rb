7abfe8c79491a99d054e04a3a95c30e97e075aa1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-14 19:31:10 +0000

    chuck: add 1.3.4.0 bottle.

--- Library/Formula/chuck.rb
+++ Library/Formula/chuck.rb
@@ -5,6 +5,13 @@ class Chuck < Formula
   url "http://chuck.cs.princeton.edu/release/files/chuck-1.3.4.0.tgz"
   sha1 "d32faae2cb60fc81d2716b477cf2d54bc548d9c6"
 
+  bottle do
+    cellar :any
+    sha1 "54f55280bd5b153277bd7657a8338ae03f674a08" => :yosemite
+    sha1 "54a4b27ef78e49544134fbe688a356e4e9001dfc" => :mavericks
+    sha1 "515e650f18e765e023947a2f8c75334611f0f7ed" => :mountain_lion
+  end
+
   # Homebrew already takes care of setting the sysroot;
   # also lets the build work on CLT.
   # Also fixes the version regex for 10.10+; reported to chuck-dev.
