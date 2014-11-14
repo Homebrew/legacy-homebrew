3c4c5468217c870775ba72444268134b197084d7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-14 23:00:07 +0000

    nanomsg: add 0.5-beta bottle.

--- Library/Formula/nanomsg.rb
+++ Library/Formula/nanomsg.rb
@@ -5,6 +5,13 @@ class Nanomsg < Formula
   url 'http://download.nanomsg.org/nanomsg-0.5-beta.tar.gz'
   sha1 '2826bf58fe29550777dbe610e12ed20d386a6974'
 
+  bottle do
+    cellar :any
+    sha1 "3a645be193f896f1a3b4f8593c1554656abdc4c1" => :yosemite
+    sha1 "874eb890390defb22b89cbf4303d218d384bd9b6" => :mavericks
+    sha1 "66775cd465f4351a92cb05824e279ed597b90270" => :mountain_lion
+  end
+
   head do
     url 'https://github.com/nanomsg/nanomsg.git'
 
