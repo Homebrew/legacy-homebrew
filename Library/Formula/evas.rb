0cdcd737d1e72513d3e31d24a05a42b14080cae2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-14 23:34:47 +0000

    evas: add 1.7.10 bottle.

--- Library/Formula/evas.rb
+++ Library/Formula/evas.rb
@@ -5,6 +5,12 @@ class Evas < Formula
   url "http://download.enlightenment.org/releases/evas-1.7.10.tar.gz"
   sha1 "ad1002eded75ea0e90d80b3b6813b3278d9f4228"
 
+  bottle do
+    sha1 "2d46e4ae7cbd297b1937c6a0776cc0008f6c35f6" => :yosemite
+    sha1 "f13c2631054365b846063b1cf760aae5116afae8" => :mavericks
+    sha1 "2609ecbe2a0be5843674f59606d15ce1ee097699" => :mountain_lion
+  end
+
   option "with-docs", "Install development libraries/headers and HTML docs"
 
   depends_on "pkg-config" => :build
