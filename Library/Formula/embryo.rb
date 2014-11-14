a6732598770faee1883ef8872dab3b5869dfb15b
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-14 23:34:47 +0000

    embryo: add 1.7.10 bottle.

--- Library/Formula/embryo.rb
+++ Library/Formula/embryo.rb
@@ -5,6 +5,12 @@ class Embryo < Formula
   url "http://download.enlightenment.org/releases/embryo-1.7.10.tar.gz"
   sha1 "4e4b3eb809211876655564920fdb773fb034f22c"
 
+  bottle do
+    sha1 "8f7302633c71b4cbe0ca77b12a2c9a934c10f9f8" => :yosemite
+    sha1 "9edfb2b516c9934858264d3905beb7a0e2aee232" => :mavericks
+    sha1 "b2d9d7f7322053097094c4afe1474eec7e19987b" => :mountain_lion
+  end
+
   head do
     url "http://svn.enlightenment.org/svn/e/trunk/embryo/"
 
