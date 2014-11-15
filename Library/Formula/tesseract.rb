3fe691d83b10753ec3ca565dfdd81981bd490f9b
Jack Nagel <jacknagel@gmail.com>
2014-11-14 21:32:55 -0600

    tesseract: bump revision for leptonica change

--- Library/Formula/tesseract.rb
+++ Library/Formula/tesseract.rb
@@ -4,12 +4,9 @@ class Tesseract < Formula
   homepage 'http://code.google.com/p/tesseract-ocr/'
   url 'https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.02.tar.gz'
   sha1 'a950acf7b75cf851de2de787e9abb62c58ca1827'
-  revision 2
+  revision 3
 
   bottle do
-    sha1 "5949807031e5d1a5c4382ed93afa06e684be104f" => :mavericks
-    sha1 "44298a87cc05b956cb9b8feb85559530cac02ad9" => :mountain_lion
-    sha1 "5beafc2400bb170185c7934ed69749c9b8ef8c99" => :lion
   end
 
   devel do
