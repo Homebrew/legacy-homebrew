7807ec00024127fd0f1542cfa57fbd92745af196
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-11-15 18:10:48 +0900

    librsvg 2.40.5
    Closes #34214.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/librsvg.rb
+++ Library/Formula/librsvg.rb
@@ -1,8 +1,7 @@
 class Librsvg < Formula
-  homepage 'https://live.gnome.org/LibRsvg'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.36/librsvg-2.36.3.tar.xz'
-  sha256 '3d7d583271030e21acacc60cb6b81ee305713c9da5e98429cbd609312aea3632'
-  revision 1
+  homepage "https://live.gnome.org/LibRsvg"
+  url "http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.40/librsvg-2.40.5.tar.xz"
+  sha256 "d14d7b3e25023ce34302022fd7c9b3a468629c94dff6c177874629686bfc71a7"
 
   bottle do
     cellar :any
@@ -28,9 +27,9 @@ class Librsvg < Formula
             "--disable-Bsymbolic",
             "--enable-tools=yes",
             "--enable-pixbuf-loader=yes",
-            "--enable-introspection=no"]
+            "--enable-introspection=yes"]
 
-    args << "--enable-svgz" if build.with? 'libgsf'
+    args << "--enable-svgz" if build.with? "libgsf"
 
     system "./configure", *args
     system "make", "install",
