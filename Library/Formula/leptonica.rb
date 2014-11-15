d485f9861cbace83637451de500741428225b3ba
Jack Nagel <jacknagel@gmail.com>
2014-11-14 21:19:11 -0600

    leptonica: make libtiff recommended

--- Library/Formula/leptonica.rb
+++ Library/Formula/leptonica.rb
@@ -4,18 +4,14 @@ class Leptonica < Formula
   homepage 'http://www.leptonica.org/'
   url 'http://www.leptonica.org/source/leptonica-1.71.tar.gz'
   sha1 '1ee59b06fd6c6402876f46c26c21b17ffd3c9b6b'
+  revision 1
 
   bottle do
-    cellar :any
-    revision 2
-    sha1 "6d6139b159eb9cc8827932362b015530d867303a" => :yosemite
-    sha1 "4f6aef3ad34456c8d683ed316d9b5768862aeb87" => :mavericks
-    sha1 "b32efefe40bba7e3d96ccde1271c5856110611ce" => :mountain_lion
   end
 
   depends_on 'libpng' => :recommended
   depends_on 'jpeg' => :recommended
-  depends_on 'libtiff' => :optional
+  depends_on 'libtiff' => :recommended
   depends_on 'giflib' => :optional
   depends_on 'openjpeg' => :optional
   depends_on 'webp' => :optional
