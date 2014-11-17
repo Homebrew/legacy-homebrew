22c506f7c35de76089f8985b65caa430d18c3115
Jack Nagel <jacknagel@gmail.com>
2014-11-17 11:42:08 -0600

    libpng: fix source download
    Fixes #34257.

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Libpng < Formula
   homepage "http://www.libpng.org/pub/png/libpng.html"
-  url "https://downloads.sf.net/project/libpng/libpng16/1.6.13/libpng-1.6.13.tar.xz"
+  url "https://downloads.sourceforge.net/project/libpng/libpng16/older-releases/1.6.13/libpng-1.6.13.tar.xz"
   sha1 "5ae32b6b99cef6c5c85feab8edf9d619e1773b15"
 
   bottle do
