c7be7ebc51b496aae3c6e875cdd83d0cab967f4b
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-17 17:52:06 +0000

    sylpheed 3.4.2
    Version bump, fixes tapping the system OpenSSL.
    
    Closes #34263.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/sylpheed.rb
+++ Library/Formula/sylpheed.rb
@@ -1,20 +1,21 @@
-require 'formula'
+require "formula"
 
 class Sylpheed < Formula
-  homepage 'http://sylpheed.sraoss.jp/en/'
-  url 'http://sylpheed.sraoss.jp/sylpheed/v3.3/sylpheed-3.3.0.tar.bz2'
-  sha1 '07b7dd7f0b58065b6f8db7a3b443240aade7da6a'
+  homepage "http://sylpheed.sraoss.jp/en/"
+  url "http://sylpheed.sraoss.jp/sylpheed/v3.4/sylpheed-3.4.2.tar.gz"
+  sha1 "693514a792f64ac1a138c7a6542d5ca6b7ac4fdc"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on :x11
-  depends_on 'gpgme'
-  depends_on 'gtk+'
-  depends_on 'cairo'
+  depends_on "gpgme"
+  depends_on "gtk+"
+  depends_on "cairo"
+  depends_on "openssl"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-updatecheck"
-    system "make install"
+    system "make", "install"
   end
 end
