3059bbd18845e7c3b213748d8a35d966e4f9e193
Jack Nagel <jacknagel@gmail.com>
2014-11-17 13:21:31 -0600

    pngcrush 1.7.80

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -1,22 +1,14 @@
 require "formula"
 
-# Stay at least one version behind and use the old-versions directory, because
-# tarballs are routinely removed and upstream won't change this practice.
 class Pngcrush < Formula
   homepage "http://pmt.sourceforge.net/pngcrush/"
-  url "https://downloads.sourceforge.net/project/pmt/pngcrush/1.7.78/pngcrush-1.7.78.tar.gz"
-  sha1 "9506a4106ecf1f960e8fe2c2a7e34131c234e070"
+  url "https://downloads.sourceforge.net/project/pmt/pngcrush/1.7.80/pngcrush-1.7.80.tar.gz"
+  sha1 "299650645a675ec1652a8c97d2bc5094ff91ba5c"
 
   bottle do
-    cellar :any
-    sha1 "1b873fdea03cfb4a23d1c98ae6e670d4de176ba9" => :yosemite
-    sha1 "e2796d141f963d38341d295274e74987bada717e" => :mavericks
-    sha1 "76fd5fcfdc145c5a926d47cc552323e7150ba085" => :mountain_lion
   end
 
   def install
-    # Required to successfully build the bundled zlib 1.2.6
-    ENV.append_to_cflags "-DZ_SOLO"
     # Required to enable "-cc" (color counting) option (disabled by default since 1.5.1)
     ENV.append_to_cflags "-DPNGCRUSH_COUNT_COLORS"
 
