79cc9c218cf20b65783aa34d5e21b1eb8c9364f8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-17 17:48:06 +0000

    influxdb: update 0.8.6 bottle.

--- Library/Formula/influxdb.rb
+++ Library/Formula/influxdb.rb
@@ -6,9 +6,9 @@ class Influxdb < Formula
   sha1 "9668d1a457ffe6bac7dc78c5bc0771428e50f119"
 
   bottle do
-    sha1 "4b6fa7d8ba82b2bcc30ca10689786785f1b0070e" => :yosemite
-    sha1 "635af68566e91ff92b7b949407e05daf5d7c88a1" => :mavericks
-    sha1 "5bb355a8e176220d92aae1cdec7f14be5abd3471" => :mountain_lion
+    sha1 "087a8a843e5ecf423efe556278b716f36a8a067f" => :yosemite
+    sha1 "eeb291a545fc56938b31d366cf184a3120fad6b4" => :mavericks
+    sha1 "9c271c356c66279bd9951ebc9255ade48fa03c32" => :mountain_lion
   end
 
   depends_on "leveldb"
