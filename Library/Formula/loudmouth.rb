6d3aebb0cd3d06e2a5ca5f59dfbec3809073c30d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-17 10:20:37 +0000

    loudmouth: add 1.5.0.20121201_1 bottle.

--- Library/Formula/loudmouth.rb
+++ Library/Formula/loudmouth.rb
@@ -7,6 +7,13 @@ class Loudmouth < Formula
   sha1 "502963c3068f7033bb21d788918c1e5cd14f386e"
   revision 1
 
+  bottle do
+    cellar :any
+    sha1 "036bfd7eb2c9b064596bdbbf241bcd91247fbdb5" => :yosemite
+    sha1 "ea2c830197162a650c5b24b6e86b78f1ef9e878f" => :mavericks
+    sha1 "0b77821cecfa2ea9e3cd9789c21b2e8857f43be0" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "libidn"
