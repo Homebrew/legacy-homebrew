fff0609d385e4993f99f312c821874ba61d9d304
Sebastian Otaegui <feniix@gmail.com>
2014-11-17 13:25:24 -0600

    remove generated comments

--- Library/Formula/jooby-bootstrap.rb
+++ Library/Formula/jooby-bootstrap.rb
@@ -1,9 +1,5 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class JoobyBootstrap < Formula
   homepage "https://github.com/jooby-project/jooby-bootstrap"
   url "https://github.com/jooby-project/jooby-bootstrap/archive/0.2.2.tar.gz"
@@ -26,15 +22,6 @@ class JoobyBootstrap < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test jooby-bootstrap`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "#{bin}/jooby", 'version"'
+    system "#{bin}/jooby", "version"
   end
 end
