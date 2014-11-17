d3b1eee6f3a496d48d7f0581d856d667e2e436ee
kbinani <kbinani.bt@gmail.com>
2014-11-17 17:34:06 +0900

    astyle: Add HEAD option
    Closes #34255.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/astyle.rb
+++ Library/Formula/astyle.rb
@@ -1,12 +1,13 @@
-require 'formula'
+require "formula"
 
 class Astyle < Formula
-  homepage 'http://astyle.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_macosx.tar.gz'
-  sha1 '2aa956c4521a1163da6a8be741786fd89c1f39a7'
+  homepage "http://astyle.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_macosx.tar.gz"
+  sha1 "2aa956c4521a1163da6a8be741786fd89c1f39a7"
+  head "svn://svn.code.sf.net/p/astyle/code/trunk/AStyle"
 
   def install
-    cd 'src' do
+    cd "src" do
       system "make", "CXX=#{ENV.cxx}", "-f", "../build/mac/Makefile"
       bin.install "bin/astyle"
     end
