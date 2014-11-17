3693afbc4708de66ddc1d37762ad5700d838f061
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-17 17:54:32 +0000

    libpng: update 1.6.14 bottle.

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -6,6 +6,10 @@ class Libpng < Formula
   sha1 "9cc30ac84214fda2177a02da275359ffd5b068d9"
 
   bottle do
+    cellar :any
+    sha1 "08a5ef7f2c249c5da21de2d5405dab650d454f46" => :yosemite
+    sha1 "bb4d97b044dff54ff2dfcba06e28d77951ca0306" => :mavericks
+    sha1 "adb52fcc77e699a20e715c6eeaf5653fd27b9aca" => :mountain_lion
   end
 
   keg_only :provided_pre_mountain_lion
