f192f59503495c670341057d4ddf4bc7c223279f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-17 23:21:13 +0000

    duo_unix: add 1.9.13_1 bottle.

--- Library/Formula/duo_unix.rb
+++ Library/Formula/duo_unix.rb
@@ -4,6 +4,13 @@ class DuoUnix < Formula
   homepage "https://www.duosecurity.com/docs/duounix"
   url "https://dl.duosecurity.com/duo_unix-1.9.13.tar.gz"
   sha1 "96120910cbaa75c3a59e4c12006738b267c3e9f0"
+  bottle do
+    cellar :any
+    sha1 "23ef6a81af2f37166d7d7423b88f7716bf9b0629" => :yosemite
+    sha1 "fdc919d750012fbfeeec8b3f95d07000adc3c946" => :mavericks
+    sha1 "0d08b3ca611f47a25a922b2d942f157f1d6268c1" => :mountain_lion
+  end
+
   revision 1
 
   depends_on "openssl"
