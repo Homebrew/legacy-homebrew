fe40e790b6e04420b8cf145606f69e778ec66a1e
James Turner <james@calminferno.net>
2014-11-18 18:48:34 -0500

    mlite 20141116 (new formula)

--- Library/Formula/mlite.rb
+++ Library/Formula/mlite.rb
@@ -1,13 +1,18 @@
-require 'formula'
+require "formula"
 
 class Mlite < Formula
-  homepage 'http://t3x.org/mlite/index.html'
-  url 'http://t3x.org/mlite/mlite-20141116.tgz'
-  sha1 'bac7ff009848cafaa318585ad13446c89f08f84e'
+  homepage "http://t3x.org/mlite/index.html"
+  url "http://t3x.org/mlite/mlite-20141116.tgz"
+  sha1 "bac7ff009848cafaa318585ad13446c89f08f84e"
 
   def install
     system "make", "CC=#{ENV.cc}"
     system "make", "test"
     system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    (testpath/"test.m").write("len ` iota 1000")
+    system "#{bin}/mlite", "-f", "test.m"
   end
 end
