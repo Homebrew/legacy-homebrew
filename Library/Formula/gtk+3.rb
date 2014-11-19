79f2e1772b7c2e3d4005e623fb2c0b9a8591268f
Jack Nagel <jacknagel@gmail.com>
2014-11-18 23:13:54 -0600

    gtk+3 3.14.5

--- Library/Formula/gtk+3.rb
+++ Library/Formula/gtk+3.rb
@@ -2,13 +2,10 @@ require 'formula'
 
 class Gtkx3 < Formula
   homepage 'http://gtk.org/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.14/gtk+-3.14.4.tar.xz'
-  sha256 'a006c716d723dab0c623491566e3292af84c87d9198a30199051d23cfc7bef2f'
+  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.14/gtk+-3.14.5.tar.xz'
+  sha256 'ba70f5ccde6646c6d8aa5a6398794b7bcf23fc45af22580a215d258f392dbbe2'
 
   bottle do
-    sha1 "54ad82781a6f423b00acfdfc8425f21099b8ef1d" => :yosemite
-    sha1 "d182700a63a4de1d82db7036b7a728a23b5111f0" => :mavericks
-    sha1 "cee7dfbdecacadd2b8dea994ce698364fd2045c6" => :mountain_lion
   end
 
   option :universal
@@ -26,20 +23,6 @@ class Gtkx3 < Formula
   depends_on 'at-spi2-atk' if build.with? "x11"
   depends_on 'gobject-introspection'
   depends_on 'gsettings-desktop-schemas' => :recommended
-
-  # These two patches fix a compilation error on Snow Leopard when
-  # using quartz backend (no effect on later OS versions)
-  # Can be removed when upstream releases these commits (3.14.5?)
-  # See: https://bugzilla.gnome.org/show_bug.cgi?id=737561
-  patch do
-    url "https://git.gnome.org/browse/gtk+/patch/?id=7d3991f2757de8374e11891e21e82f61242b4034"
-    sha1 "66dc9d63ba6d2f9219d0dbe27f2dd8343b07ab0c"
-  end
-
-  patch do
-    url "https://git.gnome.org/browse/gtk+/patch/?id=830a72b307b33815179aa0b03dad498c9ac1bb14"
-    sha1 "5a39469d8923ac9ce1f6511c4e304f7e1cba781c"
-  end
 
   def install
     ENV.universal_binary if build.universal?
