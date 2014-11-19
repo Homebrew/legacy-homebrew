cd3d10a19585de3e4d958ca994c32b2362c9f584
Nguyen Anh Quynh <aquynh@gmail.com>
2014-11-19 22:33:56 +0800

    Update Capstone to 3.0

--- Library/Formula/capstone.rb
+++ Library/Formula/capstone.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Capstone < Formula
   homepage 'http://capstone-engine.org'
-  url 'http://capstone-engine.org/download/2.1.2/capstone-2.1.2.tgz'
-  sha1 '235ceab369025fbad9887fe826b741ca84b1ab41'
+  url 'http://capstone-engine.org/download/3.0/capstone-3.0.tgz'
+  sha1 '26e591b8323ed3f6e92637d7ee953cb505687efa'
 
   bottle do
     cellar :any
