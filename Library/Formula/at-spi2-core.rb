3ffc40987750a5595776fa195d4feabda4c9acad
Jack Nagel <jacknagel@gmail.com>
2014-11-18 23:13:01 -0600

    at-spi2-core 2.14.1

--- Library/Formula/at-spi2-core.rb
+++ Library/Formula/at-spi2-core.rb
@@ -2,13 +2,10 @@ require "formula"
 
 class AtSpi2Core < Formula
   homepage "http://a11y.org"
-  url "http://ftp.gnome.org/pub/GNOME/sources/at-spi2-core/2.14/at-spi2-core-2.14.0.tar.xz"
-  sha256 "5807b02e6ef695b52fde7ff26d675dd94a0707df3c42fe7fd224828e092514c8"
+  url "http://ftp.gnome.org/pub/GNOME/sources/at-spi2-core/2.14/at-spi2-core-2.14.1.tar.xz"
+  sha256 "eef9660b14fdf0fb1f30d1be7c72d591fa7cbb87b00ca3a444425712f46ce657"
 
   bottle do
-    sha1 "81d698ef6592a3dc2f1e34d2fc895d81cd79336b" => :mavericks
-    sha1 "d5513c28dc9326b161840deeb0c148a2d1f3bcd3" => :mountain_lion
-    sha1 "cd3260c7814275528333bfe4cf1e56a7d920cdf8" => :lion
   end
 
   depends_on "pkg-config" => :build
