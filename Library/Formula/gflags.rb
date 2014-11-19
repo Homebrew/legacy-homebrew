ec2740cea6d7beda313f02eeaaff8a2a4fc12695
Pete Gadomski <pete.gadomski@gmail.com>
2014-11-19 16:30:11 -0700

    gflags 2.1.1
    Update gflags to 2.1.1.
    
    Closes #34344.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gflags.rb
+++ Library/Formula/gflags.rb
@@ -1,12 +1,14 @@
 require 'formula'
 
 class Gflags < Formula
-  homepage 'http://code.google.com/p/google-gflags/'
-  url 'https://gflags.googlecode.com/files/gflags-2.0.tar.gz'
-  sha1 'dfb0add1b59433308749875ac42796c41e824908'
+  homepage "http://code.google.com/p/google-gflags/"
+  url "https://github.com/schuhschuh/gflags/archive/v2.1.1.tar.gz"
+  sha1 "59b37548b10daeaa87a3093a11d13c2442ac6849"
+
+  depends_on "cmake" => :build
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "cmake", ".", *std_cmake_args
     system "make install"
   end
 end
