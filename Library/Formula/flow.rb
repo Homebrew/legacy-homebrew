75abc9b9c44d3e649938ee4a94cfdda017da097c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-19 10:16:30 +0000

    flow: add 0.1.0 bottle.

--- Library/Formula/flow.rb
+++ Library/Formula/flow.rb
@@ -6,6 +6,13 @@ class Flow < Formula
   sha1 "780134b1fffd7f1dd8ba397290133d5a49c6e144"
   head "https://github.com/facebook/flow.git"
 
+  bottle do
+    cellar :any
+    sha1 "cc278985ed101cd8e267be5a80585acc79e15825" => :yosemite
+    sha1 "ee8fa0635437c54963c836ea105b69ac65b5f251" => :mavericks
+    sha1 "60c22342e4c298034939c29d44b41281bb75fa07" => :mountain_lion
+  end
+
   depends_on "objective-caml" => :build
 
   def install
