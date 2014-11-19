d6fd4f2f36e038356adcbb2067e9de434017c408
Ivan Brkanac <ibrkanac@gmail.com>
2014-11-19 15:51:54 +0100

    updated meld to 3.12.1

--- Library/Formula/meld.rb
+++ Library/Formula/meld.rb
@@ -1,20 +1,24 @@
 require 'formula'
 
 class Meld < Formula
-  homepage 'http://meldmerge.org'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/meld/1.8/meld-1.8.6.tar.xz'
-  sha256 'af96682b8f4bf3ad4221c853b1516218d62a17ff43c38f4a83e7e8ac6736e8a5'
+  homepage 'http://meldmerge.org/'
+  url 'http://download.gnome.org/sources/meld/3.12/meld-3.12.1.tar.xz'
+  sha256 '8e09284f8d50347386a428e1a2222f2b3a704bf84b3736908082968b5b0dc7f8'
 
   depends_on 'intltool' => :build
   depends_on 'rarian' => :build
   depends_on :x11
   depends_on :python
-  depends_on 'pygtk'
-  depends_on 'pygtksourceview'
-  depends_on 'pygobject'
+  depends_on 'gtk+3'
+  depends_on 'glib'
+  depends_on 'pygobject3'
+  depends_on 'gtksourceview3'
+  depends_on 'itstool'
+  depends_on 'libxml2' => 'with-python'
 
   def install
-    system "make", "prefix=#{prefix}", "install"
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
+    system "python", "setup.py", "--no-update-icon-cache", "--no-compile-schemas", "install", "--prefix=#{prefix}"
     bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV['PYTHONPATH'])
   end
 end
