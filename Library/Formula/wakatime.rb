ec6af86082c0b1eade6d73fe7d90330500b46201
Alan Hamlett <alan.hamlett@gmail.com>
2014-11-19 22:15:07 -0800

    remove unused lines, improve formula.

--- Library/Formula/wakatime.rb
+++ Library/Formula/wakatime.rb
@@ -4,19 +4,14 @@ class Wakatime < Formula
   homepage "https://wakatime.com/"
   url "https://pypi.python.org/packages/source/w/wakatime/wakatime-2.1.6.tar.gz"
   sha1 "e288befaeefaaeec303efa36f2b7488829a6cf0d"
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
     ENV["PYTHONPATH"] = lib+"python2.7/site-packages"
-    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
-    ENV.prepend_create_path "PYTHONPATH", prefix+"lib/python2.7/site-packages"
 
-    resources.each do |r|
-        r.stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
-    end
+    Language::Python.setup_install "python", libexec
 
-    system "python", "setup.py", "install", "--prefix=#{prefix}"
-
-    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.install Dir[libexec/"bin/*"]
   end
 
   test do
