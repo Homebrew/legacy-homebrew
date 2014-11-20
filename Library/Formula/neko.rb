79fe20067eac6244a80413304e76a840dfd0be1d
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-20 22:42:03 +0000

    neko: system openssl fix
    One of the MySql libraries in Neko is using the system OpenSSL. This
    pulls it onto using our OpenSSL instead.

--- Library/Formula/neko.rb
+++ Library/Formula/neko.rb
@@ -1,15 +1,19 @@
-require 'formula'
+require "formula"
 
 class Neko < Formula
-  homepage 'http://nekovm.org'
+  homepage "http://nekovm.org"
+  head "https://github.com/HaxeFoundation/neko.git"
+  revision 1
 
-  # revision includes recent parameterized build targets for mac.  Use a :tag
-  # on the next release
-  url 'https://github.com/HaxeFoundation/neko.git', :revision => '6ab8f48a8dc62e4d917b599b3d8c8e10f764f839'
+  stable do
+    # revision includes recent parameterized build targets for mac.  Use a :tag
+    # on the next release
+    url "https://github.com/HaxeFoundation/neko.git", :revision => "6ab8f48a8dc62e4d917b599b3d8c8e10f764f839"
+    version "2.0.0-6ab8f48"
 
-  version '2.0.0-6ab8f48'
-
-  head 'https://github.com/HaxeFoundation/neko.git'
+    # Revisit with each stable release. Could be a while though.
+    depends_on MaximumMacOSRequirement => :mavericks
+  end
 
   bottle do
     cellar :any
@@ -18,17 +22,18 @@ class Neko < Formula
     sha1 "77daff389d401d6764d1082ecc3448afbe27fccd" => :lion
   end
 
-  depends_on 'bdw-gc'
-  depends_on 'pcre'
+  depends_on "bdw-gc"
+  depends_on "pcre"
+  depends_on "openssl"
 
   def install
     # Build requires targets to be built in specific order
     ENV.deparallelize
     system "make", "os=osx", "LIB_PREFIX=#{HOMEBREW_PREFIX}", "INSTALL_FLAGS="
 
-    include.install Dir['vm/neko*.h']
-    neko = lib/'neko'
-    neko.install Dir['bin/*']
+    include.install Dir["vm/neko*.h"]
+    neko = lib/"neko"
+    neko.install Dir["bin/*"]
 
     # Symlink into bin so libneko.dylib resolves correctly for custom prefix
     %w(neko nekoc nekoml nekotools).each do |file|
@@ -43,8 +48,8 @@ class Neko < Formula
   end
 
   def caveats
-    s = ''
-    if HOMEBREW_PREFIX.to_s != '/usr/local'
+    s = ""
+    if HOMEBREW_PREFIX.to_s != "/usr/local"
       s << <<-EOS.undent
         You must add the following line to your .bashrc or equivalent:
           export NEKOPATH="#{HOMEBREW_PREFIX}/lib/neko"
