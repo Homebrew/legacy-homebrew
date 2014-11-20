48f6ae05bb2ae37013c5669c1245af0a6853e7ab
Peter Hubner <peter.hubner@ndpgroup.com>
2014-11-20 07:14:46 -0700

    gh-34340 fixed URL for nuttcp

--- Library/Formula/nuttcp.rb
+++ Library/Formula/nuttcp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Nuttcp < Formula
   homepage 'http://www.nuttcp.net/nuttcp'
-  url 'http://lcp.nrl.navy.mil/nuttcp/nuttcp-6.1.2.tar.bz2'
+  url 'http://www.nuttcp.net/nuttcp/nuttcp-6.1.2.tar.bz2'
   sha1 '329fcc3c0b75db18b7b4d73962992603f9ace9ca'
 
   def install
