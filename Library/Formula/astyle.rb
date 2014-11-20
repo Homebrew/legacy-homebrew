8f40bf958f63324e12008e4372ef0b3604c8ea2b
kbinani <kbinani.bt@gmail.com>
2014-11-20 11:15:10 +0900

    astyle 2.05

--- Library/Formula/astyle.rb
+++ Library/Formula/astyle.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Astyle < Formula
   homepage "http://astyle.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_macosx.tar.gz"
-  sha1 "2aa956c4521a1163da6a8be741786fd89c1f39a7"
+  url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.05/astyle_2.05_macosx.tar.gz"
+  sha1 "143be1d605ba05855451c3d96f9a4612d2feb554"
   head "svn://svn.code.sf.net/p/astyle/code/trunk/AStyle"
 
   bottle do
