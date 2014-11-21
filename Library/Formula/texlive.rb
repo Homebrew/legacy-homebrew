cae154bce5722bb941bbc648f2fe0b8bc3cba018
Shaun Jackman <sjackman@gmail.com>
2014-11-20 17:29:02 -0800

    texlive 20141120: New formula for Linuxbrew

---
+++ Library/Formula/texlive.rb
@@ -0,0 +1,34 @@
+require "formula"
+
+class Texlive < Formula
+  homepage "http://www.tug.org/texlive/"
+
+  url "http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz"
+  sha1 "997d2be24706327845925b0ef34544cb4a6fd2f1"
+  version "20141120"
+
+  option "with-full", "install everything"
+  option "with-medium", "install small + more packages and languages"
+  option "with-small", "install basic + xetex, metapost, a few languages [default]"
+  option "with-basic", "install plain and latex"
+  option "with-minimal", "install plain only"
+
+  def install
+    scheme = %w[full medium small basic minimal].find ->{"small"} {
+      |x| build.with? x
+    }
+
+    ohai "Downloading and installing TeX Live. This will take a few minutes."
+    ENV["TEXLIVE_INSTALL_PREFIX"] = prefix
+    system "./install-tl", "-scheme", scheme, "-portable", "-profile", "/dev/null"
+
+    binarch = bin/"x86_64-linux"
+    man1.install Dir[binarch/"man/man1/*"]
+    man5.install Dir[binarch/"man/man5/*"]
+    bin.install_symlink Dir[binarch/"*"]
+  end
+
+  test do
+    system "#{bin}/tex --version"
+  end
+end
