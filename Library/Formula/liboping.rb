9cf1e0024ed63991d7c6c8905318e3b2b6395f1c
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-21 16:06:04 +0000

    liboping: update 1.8.0 bottle.

--- Library/Formula/liboping.rb
+++ Library/Formula/liboping.rb
@@ -6,10 +6,9 @@ class Liboping < Formula
   sha256 '1dcb9182c981b31d67522ae24e925563bed57cf950dc681580c4b0abb6a65bdb'
 
   bottle do
-    revision 1
-    sha1 "fced606652325907943e882c5648682de6d1b507" => :yosemite
-    sha1 "fec2a87f6c3105dfd2febcc4f332ca936cb74f84" => :mavericks
-    sha1 "86996fe738d3912e7b4d20cde064415331e4a41f" => :mountain_lion
+    sha1 "227e7e74330510ac76e8442c685b8a6a5edea9cb" => :yosemite
+    sha1 "77cfac843cbcff2f2c0d8040e76b2abe321c9a64" => :mavericks
+    sha1 "1bf78b5ae02d6ec9c7436da117f3f33e7cb61bee" => :mountain_lion
   end
 
   def install
