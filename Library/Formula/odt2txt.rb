dcb198f31b70df6ff4ab0e87ce5e8dd9f088b5db
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-21 10:21:24 +0000

    odt2txt: add 0.5 bottle.

--- Library/Formula/odt2txt.rb
+++ Library/Formula/odt2txt.rb
@@ -5,6 +5,13 @@ class Odt2txt < Formula
   url "https://github.com/dstosberg/odt2txt/archive/v0.5.tar.gz"
   sha1 "deac1995f02d3b907843dd99a975b201a5f55177"
 
+  bottle do
+    cellar :any
+    sha1 "21e4d5f82d941a5ba13f191b6c4011aa456b5c7e" => :yosemite
+    sha1 "0bc3eb54110df5ba74f18f0b89dfe6819e7e3b75" => :mavericks
+    sha1 "064b2423089db500bf5d850f97b95e32bc947cab" => :mountain_lion
+  end
+
   def install
     system "make", "install"
   end
