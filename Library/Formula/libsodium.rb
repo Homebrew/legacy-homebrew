355aaef71de47dd3b444573c15afa89c6ba5bd74
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-21 05:41:34 +0000

    libsodium: update 1.0.1 bottle.

--- Library/Formula/libsodium.rb
+++ Library/Formula/libsodium.rb
@@ -7,9 +7,9 @@ class Libsodium < Formula
 
   bottle do
     cellar :any
-    sha1 "1432ec12353213ebfbb8e31e839eea9cedba7d2d" => :yosemite
-    sha1 "8b66ef0a978e3aac54036f447053585b809a6e40" => :mavericks
-    sha1 "9b6cc830b3b36e20ac8dcfc076e450ba1505c46f" => :mountain_lion
+    sha1 "37715a34a7ee3af1b584d4ad1f7ae1561f414a04" => :yosemite
+    sha1 "ae23cbaac10d5b77c89e8bc16ac3fbe5f0965633" => :mavericks
+    sha1 "d80e8038aed95ccaf681317615346cb0255a52b9" => :mountain_lion
   end
 
   head do
