c3da3ff52f6d443d72e61679d1d44872d8b27328
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-21 05:33:54 +0000

    libsodium 1.0.1
    Version bump.

--- Library/Formula/libsodium.rb
+++ Library/Formula/libsodium.rb
@@ -2,12 +2,11 @@ require "formula"
 
 class Libsodium < Formula
   homepage "https://github.com/jedisct1/libsodium/"
-  url "https://github.com/jedisct1/libsodium/releases/download/1.0.0/libsodium-1.0.0.tar.gz"
-  sha256 "ced1fe3d2066953fea94f307a92f8ae41bf0643739a44309cbe43aa881dbc9a5"
+  url "https://github.com/jedisct1/libsodium/releases/download/1.0.1/libsodium-1.0.1.tar.gz"
+  sha256 "c3090887a4ef9e2d63af1c1e77f5d5a0656fadb5105ebb9fb66a302210cb3af5"
 
   bottle do
     cellar :any
-    revision 1
     sha1 "1432ec12353213ebfbb8e31e839eea9cedba7d2d" => :yosemite
     sha1 "8b66ef0a978e3aac54036f447053585b809a6e40" => :mavericks
     sha1 "9b6cc830b3b36e20ac8dcfc076e450ba1505c46f" => :mountain_lion
@@ -29,7 +28,7 @@ class Libsodium < Formula
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
   end
 end
