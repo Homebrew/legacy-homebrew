34970553a86a94d611b9d18a3c24091400a8f697
Mike McQuaid <mike@mikemcquaid.com>
2014-11-22 14:39:00 +0000

    assimp: general cleanup.

--- Library/Formula/assimp.rb
+++ Library/Formula/assimp.rb
@@ -1,19 +1,20 @@
-require 'formula'
+require "formula"
 
 class Assimp < Formula
-  homepage 'http://assimp.sourceforge.net/'
+  homepage "http://assimp.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/assimp/assimp-3.1/assimp-3.1.1_no_test_models.zip"
   sha1 "d7bc1d12b01d5c7908d85ec9ff6b2d972e565e2d"
   version "3.1.1"
 
-  head 'https://github.com/assimp/assimp.git'
+  head "https://github.com/assimp/assimp.git"
 
   option "without-boost", "Compile without thread safe logging or multithreaded computation if boost isn't installed"
-  depends_on 'cmake' => :build
-  depends_on 'boost' => :recommended
+
+  depends_on "cmake" => :build
+  depends_on "boost" => :recommended
 
   def install
     system "cmake", ".", *std_cmake_args
-    system "make install"
+    system "make", "install"
   end
 end
