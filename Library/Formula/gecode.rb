cfb03ce129b9397f744dc31fbb7d8ce1222881b7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-23 21:01:53 +0000

    gecode: add 4.3.2 bottle.

--- Library/Formula/gecode.rb
+++ Library/Formula/gecode.rb
@@ -5,6 +5,13 @@ class Gecode < Formula
   url 'http://www.gecode.org/download/gecode-4.3.2.tar.gz'
   sha1 '05c79e185197d85bf38dd074fe7c299d4976f552'
 
+  bottle do
+    cellar :any
+    sha1 "ba6eb629c7c589313b01f7f1fa9e7efaa982e484" => :yosemite
+    sha1 "0e92784b621811737eabc4a002452fdf549e015c" => :mavericks
+    sha1 "e63ad8a3f8313fbf61a6d58b8db5bc82cb9b562e" => :mountain_lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-examples"
     system "make install"
