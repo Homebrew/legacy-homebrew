167ecb368f000e946ff19053c21d48c14d15ad05
Göran Gustafsson <gustafsson.g@gmail.com>
2014-11-23 10:18:41 +0100

    svtplay-dl 0.9.2014.11.16
    Updated svtplay-dl to version 0.9.2014.10.23
    
    Closes #34396.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/svtplay-dl.rb
+++ Library/Formula/svtplay-dl.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class SvtplayDl < Formula
   homepage "https://github.com/spaam/svtplay-dl"
-  url "https://github.com/spaam/svtplay-dl/archive/0.9.2014.10.23.tar.gz"
-  sha1 "ab3337934c875edddaf4c025b6df69be561df662"
+  url "https://github.com/spaam/svtplay-dl/archive/0.9.2014.11.16.tar.gz"
+  sha1 "46405160291d4734fe5ee33fbda1b3a205340e15"
 
   depends_on "rtmpdump"
 
@@ -12,8 +12,8 @@ class SvtplayDl < Formula
   end
 
   def caveats; <<-EOS.undent
-    You need PyCrypto (https://www.dlitz.net/software/pycrypto/) to
-    download encrypted HLS streams.
+    You need PyCrypto (https://www.dlitz.net/software/pycrypto/) to download
+    encrypted HLS streams.
     You need PyAMF (http://www.pyamf.org/) for kanal5play.se.
     EOS
   end
