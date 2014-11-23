72ef88061d272ca25a593a534915d1e76f4e2ba9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-23 05:08:23 +0000

    gtkwave: add 3.3.60 bottle.

--- Library/Formula/gtkwave.rb
+++ Library/Formula/gtkwave.rb
@@ -7,6 +7,13 @@ class Gtkwave < Formula
   url "http://gtkwave.sourceforge.net/gtkwave-3.3.60.tar.gz"
   sha1 "040894bd142623b1bb9d7000619e9787a6db3f76"
 
+  bottle do
+    cellar :any
+    sha1 "a606e5f48ba5aba2a413400009171c4e72e72520" => :yosemite
+    sha1 "17b37f240c67d2ead954232b674efa990dc381b5" => :mavericks
+    sha1 "d4354c0d03b86470803a93c02a3718f68b8bed43" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gtk+"
   depends_on "xz" # For LZMA support
