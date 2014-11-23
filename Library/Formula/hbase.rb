293b716299b65ceff199b897057c8c36657f2282
Takayuki Hasegawa <takayuki.hasegawa0311@gmail.com>
2014-11-23 14:50:32 +0900

    hbase 0.98.8
    Closes #34395.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hbase.rb
+++ Library/Formula/hbase.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Hbase < Formula
   homepage 'http://hbase.apache.org'
-  url 'http://www.apache.org/dyn/closer.cgi?path=hbase/hbase-0.98.6.1/hbase-0.98.6.1-hadoop2-bin.tar.gz'
-  sha1 '3ee9913f87f035e23cb20e5d69e652b19932a8d4'
+  url 'http://www.apache.org/dyn/closer.cgi?path=hbase/hbase-0.98.8/hbase-0.98.8-hadoop2-bin.tar.gz'
+  sha1 'a1b5d19a52976f2f42f21dd36bd040d081f3d491'
 
   depends_on 'hadoop'
 
