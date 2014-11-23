c4fbcf94d3b14c1549559c474a4b4f39413a6497
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-23 19:50:04 +0000

    json-c 0.12
    Version bump. Added HEAD.
    
    Closes #34405.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/json-c.rb
+++ Library/Formula/json-c.rb
@@ -2,16 +2,23 @@ require "formula"
 
 class JsonC < Formula
   homepage "https://github.com/json-c/json-c/wiki"
-  url "https://github.com/json-c/json-c/archive/json-c-0.11-20130402.tar.gz"
-  version "0.11"
-  sha1 "1910e10ea57a743ec576688700df4a0cabbe64ba"
+  url "https://github.com/json-c/json-c/archive/json-c-0.12-20140410.tar.gz"
+  version "0.12"
+  sha256 "99304a4a633f1ee281d6a521155a182824dd995139d5ed6ee5c93093c281092b"
 
   bottle do
     cellar :any
-    revision 2
     sha1 "9066fa256094d7836546e0a9a89f540010b48896" => :yosemite
     sha1 "3d4aff56dc15feecc79cda2f3cf30e0eb54e9489" => :mavericks
     sha1 "26b39365b735048ca6c42b340ececdc146666e79" => :mountain_lion
+  end
+
+  head do
+    url "https://github.com/json-c/json-c.git"
+
+    depends_on "libtool" => :build
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
   end
 
   option :universal
@@ -19,9 +26,11 @@ class JsonC < Formula
   def install
     ENV.universal_binary if build.universal?
 
+    system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
     ENV.deparallelize
-    system "make install"
+    system "make", "install"
   end
 end
--- Library/Formula/libspatialite.rb
+++ Library/Formula/libspatialite.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
 class Libspatialite < Formula
-  homepage 'https://www.gaia-gis.it/fossil/libspatialite/index'
+  homepage "https://www.gaia-gis.it/fossil/libspatialite/index"
   url "http://www.gaia-gis.it/gaia-sins/libspatialite-4.2.0.tar.gz"
   sha1 "698bf70bec669ddcbf1b0cc48f40c2e03776bffc"
+  revision 1
 
   bottle do
     cellar :any
@@ -20,27 +19,27 @@ class Libspatialite < Formula
     depends_on "libtool" => :build
   end
 
-  option 'without-freexl', 'Build without support for reading Excel files'
-  option 'without-libxml2', 'Disable support for xml parsing (parsing needed by spatialite-gui)'
-  option 'without-liblwgeom', 'Build without additional sanitization/segmentation routines provided by PostGIS 2.0+ library'
+  option "without-freexl", "Build without support for reading Excel files"
+  option "without-libxml2", "Disable support for xml parsing (parsing needed by spatialite-gui)"
+  option "without-liblwgeom", "Build without additional sanitization/segmentation routines provided by PostGIS 2.0+ library"
   option "without-geopackage", "Build without OGC GeoPackage support"
   option "without-check", "Do not run `make check` prior to installing"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'proj'
-  depends_on 'geos'
+  depends_on "pkg-config" => :build
+  depends_on "proj"
+  depends_on "geos"
   # Needs SQLite > 3.7.3 which rules out system SQLite on Snow Leopard and
   # below. Also needs dynamic extension support which rules out system SQLite
   # on Lion. Finally, RTree index support is required as well.
-  depends_on 'sqlite'
-  depends_on 'libxml2' => :recommended
-  depends_on 'freexl' => :recommended
-  depends_on 'liblwgeom' => :recommended
+  depends_on "sqlite"
+  depends_on "libxml2" => :recommended
+  depends_on "freexl" => :recommended
+  depends_on "liblwgeom" => :recommended
 
   def install
     system "autoreconf", "-fi" if build.head?
 
-    # New SQLite3 extension won't load via SELECT load_extension('mod_spatialite');
+    # New SQLite3 extension won't load via SELECT load_extension("mod_spatialite");
     # unless named mod_spatialite.dylib (should actually be mod_spatialite.bundle)
     # See: https://groups.google.com/forum/#!topic/spatialite-users/EqJAB8FYRdI
     #      needs upstream fixes in both SQLite and libtool
@@ -50,13 +49,13 @@ class Libspatialite < Formula
 
     # Ensure Homebrew's libsqlite is found before the system version.
     sqlite = Formula["sqlite"]
-    ENV.append 'LDFLAGS', "-L#{sqlite.opt_lib}"
-    ENV.append 'CFLAGS', "-I#{sqlite.opt_include}"
+    ENV.append "LDFLAGS", "-L#{sqlite.opt_lib}"
+    ENV.append "CFLAGS", "-I#{sqlite.opt_include}"
 
-    if build.with? 'liblwgeom'
+    if build.with? "liblwgeom"
       lwgeom = Formula["liblwgeom"]
-      ENV.append 'LDFLAGS', "-L#{lwgeom.opt_lib}"
-      ENV.append 'CFLAGS', "-I#{lwgeom.opt_include}"
+      ENV.append "LDFLAGS", "-L#{lwgeom.opt_lib}"
+      ENV.append "CFLAGS", "-I#{lwgeom.opt_include}"
     end
 
     args = %W[
@@ -65,12 +64,12 @@ class Libspatialite < Formula
       --with-sysroot=#{HOMEBREW_PREFIX}
     ]
     args << "--enable-geocallbacks"
-    args << '--enable-freexl=no' if build.without? 'freexl'
+    args << "--enable-freexl=no" if build.without? "freexl"
     args << "--enable-libxml2=no" if build.without? "libxml2"
-    args << '--enable-lwgeom=yes' if build.with? 'liblwgeom'
+    args << "--enable-lwgeom=yes" if build.with? "liblwgeom"
     args << "--enable-geopackage=no" if build.without? "geopackage"
 
-    system './configure', *args
+    system "./configure", *args
     system "make", "check" if build.with? "check"
     system "make", "install"
   end
@@ -79,5 +78,4 @@ class Libspatialite < Formula
     # Verify mod_spatialite extension can be loaded using Homebrew's SQLite
     system "echo \"SELECT load_extension('#{opt_lib}/mod_spatialite');\" | #{Formula["sqlite"].opt_bin}/sqlite3"
   end
-
 end
