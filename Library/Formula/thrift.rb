863f0dc81ba5e064ba922e8511b0088d4587ee2d
Cameron Ring <cameron@cs.stanford.edu>
2014-11-24 21:43:52 -0800

    thrift 0.9.2
    Closes #34433.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/thrift.rb
+++ Library/Formula/thrift.rb
@@ -4,23 +4,15 @@ class Thrift < Formula
   homepage "http://thrift.apache.org"
 
   stable do
-    url "http://archive.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz"
-    sha1 "dc54a54f8dc706ffddcd3e8c6cd5301c931af1cc"
+    url "http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz"
+    sha1 "02f78b158da795ea89a26ce41964fbe562cc4235"
 
-    # These patches are 0.9.1-specific and can go away once a newer version is released
+    # Apply any necessary patches (none currently required)
     [
+      # Example patch:
+      #
       # Apply THRIFT-2201 fix from master to 0.9.1 branch (required for clang to compile with C++11 support)
-      %w{836d95f9f00be73c6936d407977796181d1a506c 4bc8c19c51f3d9f30799251a810dd1ca63c4bf1e},
-      # Apply THRIFT-667
-      %w{12c09f44cb291b1ecc4074cb3a55775b375fa8b2 3c449146e13727a9dbeb66e9826968b85a6d8869},
-      # Apply THRIFT-1755
-      %w{8cd3efe50a42975375e8ff3bc03306d9e4174314 5b45d692b2e6a4480d088485d4f011b9cd7fff52},
-      # Apply THRIFT-2045
-      %w{282e440c6de219b7b8f32b01cc7eb599f534f33f a5ab25621be48a803de7f0893ada1cdadaa1bc11},
-      %w{9f9cd10e813ef574dd5578d78ca26a9088383d3a 65db24f59989eab8b3b0a0f1393a52faa3cba7ee},
-      %w{e957675647d3d7caafe842aa85cbd987e91b21f9 478a5ad6b08a268460793112361726976fee8157},
-      # Apply THRIFT-2229 fix from master to 0.9.1 branch
-      %w{5f2d34e5ab33651059a085525b3adbab6a877e6f ff01eeca821a1d97485c9075c4d65f0c5eef4931},
+      # %w{836d95f9f00be73c6936d407977796181d1a506c 4bc8c19c51f3d9f30799251a810dd1ca63c4bf1e},
     ].each do |name, sha|
       patch do
         url "https://git-wip-us.apache.org/repos/asf?p=thrift.git;a=patch;h=#{name}"
@@ -43,7 +35,6 @@ class Thrift < Formula
     depends_on :automake
     depends_on :libtool
     depends_on "pkg-config" => :build
-    depends_on "bison" => :build
   end
 
   option "with-haskell", "Install Haskell binding"
@@ -55,11 +46,12 @@ class Thrift < Formula
   depends_on "boost"
   depends_on "openssl"
   depends_on :python => :optional
+  depends_on "bison" => :build
 
   def install
     system "./bootstrap.sh" unless build.stable?
 
-    exclusions = ["--without-ruby", "--without-tests", "--without-php_extension"]
+    exclusions = ["--without-ruby", "--disable-tests", "--without-php_extension"]
 
     exclusions << "--without-python" if build.without? "python"
     exclusions << "--without-haskell" if build.without? "haskell"
