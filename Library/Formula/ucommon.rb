84c9b1c944b6423006741652a5ca4e87aefcf5a9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-25 20:41:40 +0000

    ucommon: add 6.2.2 bottle.

--- Library/Formula/ucommon.rb
+++ Library/Formula/ucommon.rb
@@ -5,6 +5,12 @@ class Ucommon < Formula
   url "http://ftpmirror.gnu.org/commonc++/ucommon-6.2.2.tar.gz"
   sha1 "867e06988de3dea1d04a71155bf77063df6fcd8f"
 
+  bottle do
+    sha1 "4fc88a216680dde8e5aa696a8839beaa0fb37231" => :yosemite
+    sha1 "599bda579daf84ad9975a0c7c96f406c716f17dd" => :mavericks
+    sha1 "1001ef2f9cf1fc1d4d982a6a03dd2a70f3754126" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gnutls"
 
