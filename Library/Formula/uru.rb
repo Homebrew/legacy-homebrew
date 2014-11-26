20fc8957cda174bd947427cdde3bf0016a343000
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-26 18:06:24 +0000

    uru: add 0.7.6 bottle.

--- Library/Formula/uru.rb
+++ Library/Formula/uru.rb
@@ -5,6 +5,12 @@ class Uru < Formula
   url "https://bitbucket.org/jonforums/uru/get/v0.7.6.tar.gz"
   sha1 "c1618f861c94318004cdede66946f31436b410e7"
 
+  bottle do
+    sha1 "9476d70ca74e2074129067a8b820065ec7e7a86b" => :yosemite
+    sha1 "61e905c6a60df009190c87afb830536e53419cdd" => :mavericks
+    sha1 "6353400d611ed746f6f232a86c8ba8a18e89a06a" => :mountain_lion
+  end
+
   depends_on "go" => :build
 
   def install
