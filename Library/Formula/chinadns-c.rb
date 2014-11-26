f51954a3ea2fde19478ec288b7e369829e9189e8
Heyward Fann <fannheyward@gmail.com>
2014-11-26 17:37:42 +0800

    rename to chinadns-c

--- Library/Formula/chinadns-c.rb
+++ Library/Formula/chinadns-c.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Chinadns < Formula
+class ChinadnsC < Formula
   homepage "https://github.com/clowwindy/ChinaDNS-C"
   url "https://github.com/clowwindy/ChinaDNS-C/releases/download/1.1.8/chinadns-c-1.1.8.tar.gz"
   sha1 "e712aab436e555a242f6c3c8acd7474b0b445bf1"
