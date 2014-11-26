d19aa689238a3cd3e53ebe30b420954fac6c7dd9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-26 10:23:51 +0000

    easy-tag: add 2.2.5 bottle.

--- Library/Formula/easy-tag.rb
+++ Library/Formula/easy-tag.rb
@@ -5,6 +5,12 @@ class EasyTag < Formula
   url "http://ftp.gnome.org/pub/GNOME/sources/easytag/2.2/easytag-2.2.5.tar.xz"
   sha256 "a6da3a094997fa225d1c01211b282f2d3c2dfd93a74f3bec24017bd5a13f3855"
 
+  bottle do
+    sha1 "a7f3903c91ea6e7884a238f5a93cc46dd9da6fcd" => :yosemite
+    sha1 "284cd60570c14b70dce6c670ba4d496399dc1c41" => :mavericks
+    sha1 "efb11e24318b92bd002b21ef78c62f796c0dbeba" => :mountain_lion
+  end
+
   depends_on :x11
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
