08c9e361828f1e37a9a713e308041066ddb587dd
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-26 22:16:49 +0000

    glog: add 0.3.3 bottle.

--- Library/Formula/glog.rb
+++ Library/Formula/glog.rb
@@ -5,6 +5,12 @@ class Glog < Formula
   url "https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz"
   sha1 "ed40c26ecffc5ad47c618684415799ebaaa30d65"
 
+  bottle do
+    sha1 "58f13dfb8f3c3804e3918930b1404efaaf1a19f8" => :yosemite
+    sha1 "aace7209305a11ae57a4e45ed283adaa0d82472f" => :mavericks
+    sha1 "086c05d1e18c99783c16d38dd7ad454137299e36" => :mountain_lion
+  end
+
   # Vendor an older version of gflags as the new version makes compile = nope.
   resource "gflags" do
     url "https://gflags.googlecode.com/files/gflags-2.0.tar.gz"
