789095d9a3c745fde9cef50e6320f37e11c885dc
Misty De Meo <mistydemeo@gmail.com>
2014-11-26 23:25:53 -0800

    cracklib: remove cellar :any
    Fixes #34490.

--- Library/Formula/cracklib.rb
+++ Library/Formula/cracklib.rb
@@ -6,7 +6,6 @@ class Cracklib < Formula
   sha1 "a780211a87a284297aa473fe2b50584b842a0e98"
 
   bottle do
-    cellar :any
     sha1 "51805becd70c9f2d62c40ab28b5e4a2d041b8b24" => :yosemite
     sha1 "418f78244ce9b7d8ab16bd5869d353981ce9afe7" => :mavericks
     sha1 "eb566f302ca1118f26a9e88aa537bbcf76c088d0" => :mountain_lion
