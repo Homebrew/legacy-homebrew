8999f061fe4a6d2698999df3539790fe154f2dc4
Arun Raghavan <git@arunraghavan.net>
2014-11-27 22:54:24 +0530

    srtp: Switch to the Cisco libsrtp repo and bump to 1.5.0
    The updated shared library is courtesy Nirbheek Chauhan. Original patch:
    
    https://github.com/nirbheek/cerbero/blob/openwebrtc/recipes/libsrtp/shared-library.patch

--- Library/Formula/srtp.rb
+++ Library/Formula/srtp.rb
@@ -1,20 +1,21 @@
 require 'formula'
 
 class Srtp < Formula
-  homepage 'http://srtp.sourceforge.net/srtp.html'
-  url 'https://downloads.sourceforge.net/project/srtp/srtp/1.4.4/srtp-1.4.4.tgz'
-  sha1 '299c6cfe0c9d6f1804bc5921cfbdb6a6bc76a521'
+  homepage 'https://github.com/cisco/libsrtp'
+  url 'https://codeload.github.com/cisco/libsrtp/tar.gz/v1.5.0'
+  sha1 'fbace084aa58dddc295e15aeab80208f154b4f16'
 
   # Add support for building shared libs
   patch do
-    url "http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/net-libs/libsrtp/files/libsrtp-1.4.4-shared.patch?revision=1.2"
-    sha1 "d7871a264dd9fd588fc6d79c15019a1dacd3edbb"
+    # Submitted upstream at https://github.com/cisco/libsrtp/pull/86
+    url "http://arunraghavan.net/downloads/brew/srtp-shared-library.patch"
+    sha1 "f44ff924a9e14826add202168f61843c1d88ddd4"
   end
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make libsrtp.dylib"
+    system "make shared_library"
     system "make install" # Can't go in parallel of building the dylib
   end
 end
