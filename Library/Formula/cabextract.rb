cfca53e05b7b8f6b7cdd08449a5af830b91bb386
Jack Nagel <jacknagel@gmail.com>
2014-11-28 16:37:18 -0500

    cabextract: remove unnecessary configure switch

--- Library/Formula/cabextract.rb
+++ Library/Formula/cabextract.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class Cabextract < Formula
-  homepage 'http://www.cabextract.org.uk/'
-  url 'http://www.cabextract.org.uk/cabextract-1.4.tar.gz'
-  sha1 'b1d5dd668d2dbe95b47aad6e92c0b7183ced70f1'
+  homepage "http://www.cabextract.org.uk/"
+  url "http://www.cabextract.org.uk/cabextract-1.4.tar.gz"
+  sha1 "b1d5dd668d2dbe95b47aad6e92c0b7183ced70f1"
 
   def install
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make install"
+                          "--prefix=#{prefix}"
+    system "make", "install"
   end
 end
