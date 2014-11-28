ebd5dc9cd3d63e3be2d5c657eee19ee465858a5d
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-11-28 23:53:36 +0900

    fixed wrong value of sha1

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -4,7 +4,7 @@ class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
   url "https://github.com/sstephenson/ruby-build/archive/v20141128.tar.gz"
-  sha1 "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+  sha1 "c276684cc379db237503142c99c57f19bde16a64"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
