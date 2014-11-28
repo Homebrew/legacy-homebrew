775f1bc0bb9fd7538a762afa4864bd984da1bf3a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-28 08:47:05 +0000

    qca: add 2.1.0 bottle.

--- Library/Formula/qca.rb
+++ Library/Formula/qca.rb
@@ -5,6 +5,12 @@ class Qca < Formula
   url "http://delta.affinix.com/download/qca/2.0/qca-2.1.0.tar.gz"
   sha1 "2b582b3ccc7e6098cd14d6f52a829ae1539e9cc8"
 
+  bottle do
+    sha1 "3ff5e09bb690122d807b48470cf1bbafcca89b41" => :yosemite
+    sha1 "f01d8cbd41c97068185af2f4c6efdec8ff0c6ce7" => :mavericks
+    sha1 "4638b43c0ce83a2cccc64d678c7eefffd1844ba3" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "qt"
