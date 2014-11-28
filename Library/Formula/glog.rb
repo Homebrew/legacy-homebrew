ca6cb29fe2d014a85d6a41cc95e7cd8ec60a1416
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-28 22:08:06 +0000

    Revert "glog: gflags compatibility fix"
    This reverts commit d12fcb69b9f8aaa29552195baec4c8099fcef32f.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/glog.rb
+++ Library/Formula/glog.rb
@@ -1,21 +1,11 @@
-require "formula"
+require 'formula'
 
 class Glog < Formula
-  homepage "https://code.google.com/p/google-glog/"
-  url "https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz"
-  sha1 "ed40c26ecffc5ad47c618684415799ebaaa30d65"
+  homepage 'http://code.google.com/p/google-glog/'
+  url 'https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz'
+  sha1 'ed40c26ecffc5ad47c618684415799ebaaa30d65'
 
-  bottle do
-    sha1 "58f13dfb8f3c3804e3918930b1404efaaf1a19f8" => :yosemite
-    sha1 "aace7209305a11ae57a4e45ed283adaa0d82472f" => :mavericks
-    sha1 "086c05d1e18c99783c16d38dd7ad454137299e36" => :mountain_lion
-  end
-
-  # Vendor an older version of gflags as the new version makes compile = nope.
-  resource "gflags" do
-    url "https://gflags.googlecode.com/files/gflags-2.0.tar.gz"
-    sha1 "dfb0add1b59433308749875ac42796c41e824908"
-  end
+  depends_on 'gflags'
 
   if MacOS.version >= :mavericks
     # Since 0.3.4 has not yet been released, manually apply
@@ -34,22 +24,8 @@ class Glog < Formula
   end
 
   def install
-    resource("gflags").stage do
-      system "./configure", "--disable-dependency-tracking", "--prefix=#{libexec}/gflags"
-      system "make", "install"
-    end
-
-    # Find our sandboxed gflags.
-    ENV.append_to_cflags "-I#{libexec}/gflags/include"
-    ENV.append "LDFLAGS", "-L#{libexec}/gflags/lib"
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make", "install"
-  end
-
-  test do
-    assert_equal "#{libexec}/gflags/lib/libgflags.2.dylib (compatibility version 4.0.0, current version 4.0.0)",
-    shell_output("otool -L #{lib}/libglog.0.dylib | grep libgflags").strip
+    system "make install"
   end
 end
