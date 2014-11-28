efee5748432e8d905ec19b2c8413d38a8efda01d
Rustam Zagirov <rustam@zagirov.name>
2014-11-28 19:05:27 +0300

    ruby-build 20141128

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,8 +3,8 @@ require "formula"
 class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  url "https://github.com/sstephenson/ruby-build/archive/v20141113.tar.gz"
-  sha1 "0afb243908e0177a7b19328ff3d4b6958bf4246a"
+  url "https://github.com/sstephenson/ruby-build/archive/v20141128.tar.gz"
+  sha1 "c276684cc379db237503142c99c57f19bde16a64"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
