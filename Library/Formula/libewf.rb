96d09cf9291eede07409dd69595c39e2ea0b6a8f
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-29 01:06:09 +0000

    libewf: update 20140608_1 bottle.

--- Library/Formula/libewf.rb
+++ Library/Formula/libewf.rb
@@ -7,6 +7,10 @@ class Libewf < Formula
   revision 1
 
   bottle do
+    cellar :any
+    sha1 "3a5b2ae9cedd50aca714dacc91716b845922e9fe" => :yosemite
+    sha1 "077829c293b2da99c076a6328e7e96837103fe6c" => :mavericks
+    sha1 "b3d3877c1683b551badb8a21491faceb27958cf1" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
