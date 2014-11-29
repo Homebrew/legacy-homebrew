6ca1b393586bec89f8191300385e0be83c5373d9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-29 06:19:10 +0000

    lsh: add 2.1 bottle.

--- Library/Formula/lsh.rb
+++ Library/Formula/lsh.rb
@@ -6,6 +6,12 @@ class Lsh < Formula
   mirror "https://ftp.gnu.org/gnu/lsh/lsh-2.1.tar.gz"
   sha1 "ddc9895a6f7d3725dd3080db8740832eb3147a85"
 
+  bottle do
+    sha1 "72d573729a1ab019f2b1f5f7f53a2dd6b9096044" => :yosemite
+    sha1 "059c5b4112b9f1d1645bcd98a42c0e8524ecaf8d" => :mavericks
+    sha1 "72229b6a88ad27b22f4904d3a4b3a3539bb9b09b" => :mountain_lion
+  end
+
   depends_on :x11 => :optional # For libXau library
   depends_on "nettle"
   depends_on "gmp"
