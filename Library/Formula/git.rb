9798c3b409b02bac7d5b6463e3a62d01e1808a34
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-29 03:03:04 +0000

    git: fix openssl linkage
    Prunes the system-openssl linkage in the event someone compiles
    git without brewed-openssl.

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -1,4 +1,4 @@
-require 'formula'
+require "formula"
 
 class Git < Formula
   homepage "http://git-scm.com"
@@ -23,32 +23,32 @@ class Git < Formula
     sha1 "fef0a1944d9d94d5a97e2dae3e53b9818af7973d"
   end
 
-  option 'with-blk-sha1', 'Compile with the block-optimized SHA1 implementation'
-  option 'without-completions', 'Disable bash/zsh completions from "contrib" directory'
-  option 'with-brewed-openssl', "Build with Homebrew OpenSSL instead of the system version"
-  option 'with-brewed-curl', "Use Homebrew's version of cURL library"
-  option 'with-brewed-svn', "Use Homebrew's version of SVN"
-  option 'with-persistent-https', 'Build git-remote-persistent-https from "contrib" directory'
+  option "with-blk-sha1", "Compile with the block-optimized SHA1 implementation"
+  option "without-completions", "Disable bash/zsh completions from 'contrib' directory"
+  option "with-brewed-openssl", "Build with Homebrew OpenSSL instead of the system version"
+  option "with-brewed-curl", "Use Homebrew's version of cURL library"
+  option "with-brewed-svn", "Use Homebrew's version of SVN"
+  option "with-persistent-https", "Build git-remote-persistent-https from 'contrib' directory"
 
-  depends_on 'pcre' => :optional
-  depends_on 'gettext' => :optional
-  depends_on 'openssl' if build.with? 'brewed-openssl'
-  depends_on 'curl' if build.with? 'brewed-curl'
-  depends_on 'go' => :build if build.with? 'persistent-https'
-  depends_on 'subversion' => 'perl' if build.with? 'brewed-svn'
+  depends_on "pcre" => :optional
+  depends_on "gettext" => :optional
+  depends_on "openssl" if build.with? "brewed-openssl"
+  depends_on "curl" if build.with? "brewed-curl"
+  depends_on "go" => :build if build.with? "persistent-https"
+  depends_on "subversion" => "perl" if build.with? "brewed-svn"
 
   def install
     # If these things are installed, tell Git build system to not use them
-    ENV['NO_FINK'] = '1'
-    ENV['NO_DARWIN_PORTS'] = '1'
-    ENV['V'] = '1' # build verbosely
-    ENV['NO_R_TO_GCC_LINKER'] = '1' # pass arguments to LD correctly
-    ENV['PYTHON_PATH'] = which 'python'
-    ENV['PERL_PATH'] = which 'perl'
+    ENV["NO_FINK"] = "1"
+    ENV["NO_DARWIN_PORTS"] = "1"
+    ENV["V"] = "1" # build verbosely
+    ENV["NO_R_TO_GCC_LINKER"] = "1" # pass arguments to LD correctly
+    ENV["PYTHON_PATH"] = which "python"
+    ENV["PERL_PATH"] = which "perl"
 
     perl_version = /\d\.\d+/.match(`perl --version`)
 
-    if build.with? 'brewed-svn'
+    if build.with? "brewed-svn"
       ENV["PERLLIB_EXTRA"] = "#{Formula["subversion"].prefix}/Library/Perl/#{perl_version}/darwin-thread-multi-2level"
     elsif MacOS.version >= :mavericks
       ENV["PERLLIB_EXTRA"] = %W{
@@ -60,20 +60,20 @@ class Git < Formula
       }.join(":")
     end
 
-    unless quiet_system ENV['PERL_PATH'], '-e', 'use ExtUtils::MakeMaker'
-      ENV['NO_PERL_MAKEMAKER'] = '1'
+    unless quiet_system ENV["PERL_PATH"], "-e", "use ExtUtils::MakeMaker"
+      ENV["NO_PERL_MAKEMAKER"] = "1"
     end
 
-    ENV['BLK_SHA1'] = '1' if build.with? 'blk-sha1'
+    ENV["BLK_SHA1"] = "1" if build.with? "blk-sha1"
 
-    if build.with? 'pcre'
-      ENV['USE_LIBPCRE'] = '1'
-      ENV['LIBPCREDIR'] = Formula['pcre'].opt_prefix
+    if build.with? "pcre"
+      ENV["USE_LIBPCRE"] = "1"
+      ENV["LIBPCREDIR"] = Formula["pcre"].opt_prefix
     end
 
-    ENV['NO_GETTEXT'] = '1' if build.without? 'gettext'
+    ENV["NO_GETTEXT"] = "1" if build.without? "gettext"
 
-    ENV['GIT_DIR'] = cached_download/".git" if build.head?
+    ENV["GIT_DIR"] = cached_download/".git" if build.head?
 
     system "make", "prefix=#{prefix}",
                    "sysconfdir=#{etc}",
@@ -83,64 +83,68 @@ class Git < Formula
                    "install"
 
     # Install the OS X keychain credential helper
-    cd 'contrib/credential/osxkeychain' do
+    cd "contrib/credential/osxkeychain" do
       system "make", "CC=#{ENV.cc}",
                      "CFLAGS=#{ENV.cflags}",
                      "LDFLAGS=#{ENV.ldflags}"
-      bin.install 'git-credential-osxkeychain'
+      bin.install "git-credential-osxkeychain"
       system "make", "clean"
     end
 
     # Install git-subtree
-    cd 'contrib/subtree' do
+    cd "contrib/subtree" do
       system "make", "CC=#{ENV.cc}",
                      "CFLAGS=#{ENV.cflags}",
                      "LDFLAGS=#{ENV.ldflags}"
-      bin.install 'git-subtree'
+      bin.install "git-subtree"
     end
 
-    if build.with? 'persistent-https'
-      cd 'contrib/persistent-https' do
+    if build.with? "persistent-https"
+      cd "contrib/persistent-https" do
         system "make"
-        bin.install 'git-remote-persistent-http',
-                    'git-remote-persistent-https',
-                    'git-remote-persistent-https--proxy'
+        bin.install "git-remote-persistent-http",
+                    "git-remote-persistent-https",
+                    "git-remote-persistent-https--proxy"
       end
     end
 
-    if build.with? 'completions'
-      # install the completion script first because it is inside 'contrib'
-      bash_completion.install 'contrib/completion/git-completion.bash'
-      bash_completion.install 'contrib/completion/git-prompt.sh'
+    if build.with? "completions"
+      # install the completion script first because it is inside "contrib"
+      bash_completion.install "contrib/completion/git-completion.bash"
+      bash_completion.install "contrib/completion/git-prompt.sh"
 
-      zsh_completion.install 'contrib/completion/git-completion.zsh' => '_git'
+      zsh_completion.install "contrib/completion/git-completion.zsh" => "_git"
       cp "#{bash_completion}/git-completion.bash", zsh_completion
     end
 
-    (share+'git-core').install 'contrib'
+    (share+"git-core").install "contrib"
 
     # We could build the manpages ourselves, but the build process depends
     # on many other packages, and is somewhat crazy, this way is easier.
-    man.install resource('man')
-    (share+'doc/git-doc').install resource('html')
+    man.install resource("man")
+    (share+"doc/git-doc").install resource("html")
 
     # Make html docs world-readable
     chmod 0644, Dir["#{share}/doc/git-doc/**/*.{html,txt}"]
     chmod 0755, Dir["#{share}/doc/git-doc/{RelNotes,howto,technical}"]
+
+    # To avoid this feature hooking into the system OpenSSL, remove it.
+    # If you need it, install git --with-brewed-openssl.
+    rm "#{libexec}/git-core/git-imap-send" if build.without? "brewed-openssl"
   end
 
   def caveats; <<-EOS.undent
     The OS X keychain credential helper has been installed to:
       #{HOMEBREW_PREFIX}/bin/git-credential-osxkeychain
 
-    The 'contrib' directory has been installed to:
+    The "contrib" directory has been installed to:
       #{HOMEBREW_PREFIX}/share/git-core/contrib
     EOS
   end
 
   test do
     HOMEBREW_REPOSITORY.cd do
-      assert_equal 'bin/brew', `#{bin}/git ls-files -- bin`.strip
+      assert_equal "bin/brew", `#{bin}/git ls-files -- bin`.strip
     end
   end
 end
