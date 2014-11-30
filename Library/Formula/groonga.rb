e3919a373c31e5a36c29b173e88355af1b486309
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-11-30 22:54:05 +0900

    build fix patches

--- Library/Formula/groonga.rb
+++ Library/Formula/groonga.rb
@@ -23,6 +23,10 @@ class Groonga < Formula
 
   option "enable-benchmark", "Enable benchmark program for developer use"
 
+  # Groonga issue #253, #254, #256
+  # already fixed at master
+  patch :DATA
+
   def install
     args = %W[
       --prefix=#{prefix}
@@ -41,3 +45,48 @@ class Groonga < Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/lib/ii.c b/lib/ii.c
+index 8f9f9a8..e82dc7f 100644
+--- a/lib/ii.c
++++ b/lib/ii.c
+@@ -37,6 +37,10 @@
+ # include <oniguruma.h>
+ #endif
+
++#ifndef O_DIRECT
++# define O_DIRECT 0
++#endif
++
+ #define MAX_PSEG                 0x20000
+ #define S_CHUNK                  (1 << GRN_II_W_CHUNK)
+ #define W_SEGMENT                18
+diff --git a/lib/grn.h b/lib/grn.h
+index ab720ef..868133c 100644
+--- a/lib/grn.h
++++ b/lib/grn.h
+@@ -174,6 +174,10 @@ typedef SOCKET grn_sock;
+ #  include <unistd.h>
+ # endif /* HAVE_UNISTD_H */
+
++# ifndef __off64_t_defined
++typedef off_t off64_t;
++# endif
++
+ # ifndef PATH_MAX
+ #  if defined(MAXPATHLEN)
+ #   define PATH_MAX MAXPATHLEN
+diff --git a/vendor/onigmo/Makefile.am b/vendor/onigmo/Makefile.am
+index 03083bd..9219783 100644
+--- a/vendor/onigmo/Makefile.am
++++ b/vendor/onigmo/Makefile.am
+@@ -7,7 +7,7 @@ CONFIGURE_DEPENDENCIES =			\
+ ALL_DEPEND_TARGETS = onigmo-all
+ CLEAN_DEPEND_TARGETS = onigmo-clean
+
+-INSTALL_DEPEND_TARGETS =
++INSTALL_DEPEND_TARGETS = onigmo-all
+ if WITH_SHARED_ONIGMO
+ INSTALL_DEPEND_TARGETS += onigmo-install
+ endif
