58980ce85e6ade4702c0b77b59c15e1f63043a01
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-01 15:26:31 +0000

    owamp: add 3.3 bottle.

--- Library/Formula/owamp.rb
+++ Library/Formula/owamp.rb
@@ -5,6 +5,13 @@ class Owamp < Formula
   url 'http://software.internet2.edu/sources/owamp/owamp-3.3.tar.gz'
   sha1 'ac3b77294ee30d41924b01fc009de0b2605a753c'
 
+  bottle do
+    cellar :any
+    sha1 "bae5fbfcde1fa634d1a97dcf085722e7d4f3e7cc" => :yosemite
+    sha1 "40052c815375a344fecc8985a72612fe91320269" => :mavericks
+    sha1 "34531c931ad6b22e4328b3823f2c2f38027db2c6" => :mountain_lion
+  end
+
   # Fix to prevent tests hanging under certain circumstances.
   # Provided by Aaron Brown via perfsonar-user mailing list:
   # https://lists.internet2.edu/sympa/arc/perfsonar-user/2014-11/msg00131.html
