3a0cb75a3b473f486d8d95c60f1b2c1c0d4c53aa
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2014-12-01 17:09:17 -0500

    JBoss Forge 2.12.3.Final
    Closes #34596.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class JbossForge < Formula
   homepage 'http://forge.jboss.org/'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.12.2.Final&e=zip&c=offline'
-  version '2.12.2.Final'
-  sha1 'ec682cbda6fbdea7155dc00d6af9338a38b12cc6'
+  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.12.3.Final&e=zip&c=offline'
+  version '2.12.3.Final'
+  sha1 '86cb164f7553f227ca9689b48c518e1963df3a79'
 
   def install
     rm_f Dir["bin/*.bat"]
