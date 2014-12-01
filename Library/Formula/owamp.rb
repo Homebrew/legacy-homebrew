dfbf2f2bcc2ff329dfb953a74a488fb4fabf4cc5
Brian Candler <b.candler@pobox.com>
2014-12-01 14:58:39 +0000

    owamp: apply patch to fix tests hanging.
    Closes #34588.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/owamp.rb
+++ Library/Formula/owamp.rb
@@ -5,6 +5,14 @@ class Owamp < Formula
   url 'http://software.internet2.edu/sources/owamp/owamp-3.3.tar.gz'
   sha1 'ac3b77294ee30d41924b01fc009de0b2605a753c'
 
+  # Fix to prevent tests hanging under certain circumstances.
+  # Provided by Aaron Brown via perfsonar-user mailing list:
+  # https://lists.internet2.edu/sympa/arc/perfsonar-user/2014-11/msg00131.html
+  patch do
+    url "http://ndb1.internet2.edu/~aaron/owamp_time_fix.patch"
+    sha1 "9e5588d57b357f438ae1a785a713f0deaea5a5ba"
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
