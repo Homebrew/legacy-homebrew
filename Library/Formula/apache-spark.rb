fae8489b2641086021cfdef5a5b84cb44af7e0a4
Brett Koonce <koonce@gmail.com>
2014-11-30 21:54:47 -0600

    apache-spark 1.1.1
    Closes #34502.
    
    Closes #34574.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/apache-spark.rb
+++ Library/Formula/apache-spark.rb
@@ -3,9 +3,9 @@ require "formula"
 class ApacheSpark < Formula
   homepage "http://spark.apache.org/"
   head "https://github.com/apache/spark.git"
-  url "http://d3kbcqa49mib13.cloudfront.net/spark-1.1.0-bin-hadoop2.4.tgz"
-  version "1.1.0"
-  sha1 "c7cf50454ac77014fa0f3a7f7a627f93a60fcac2"
+  url "http://d3kbcqa49mib13.cloudfront.net/spark-1.1.1-bin-hadoop2.4.tgz"
+  version "1.1.1"
+  sha1 "d1ef3edfe9174010c66ed39fbbc961b7db765a35"
 
   def install
     rm_f Dir["bin/*.cmd"]
