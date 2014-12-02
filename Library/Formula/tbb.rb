f26e52489389844fcde89016ba01a85bda1f2232
Jack Nagel <jacknagel@gmail.com>
2014-12-02 11:15:43 -0500

    tbb 4.3-20141023
    It seems that manually setting the compiler and architecture is no
    longer necessary.

--- Library/Formula/tbb.rb
+++ Library/Formula/tbb.rb
@@ -2,15 +2,11 @@ require "formula"
 
 class Tbb < Formula
   homepage "http://www.threadingbuildingblocks.org/"
-  url "https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb43_20140724oss_src.tgz"
-  sha1 "4cb73cd0ac61b790318358ae4782f80255715278"
-  version "4.3-20140724"
+  url "https://www.threadingbuildingblocks.org/sites/default/files/software_releases/source/tbb43_20141023oss_src.tgz"
+  sha1 "aaecdc97049fbe3c623be46c4e1261b74a1a41a3"
+  version "4.3-20141023"
 
   bottle do
-    cellar :any
-    sha1 "500a19e3b12c7ecd04d09c558403b03dabaef465" => :mavericks
-    sha1 "17194db68fe3dc0a932094f04776bc5c7eee756d" => :mountain_lion
-    sha1 "2bb200abaf9f8182bfb948e4dee513b9afca2198" => :lion
   end
 
   # requires malloc features first introduced in Lion
@@ -24,19 +20,6 @@ class Tbb < Formula
     ENV.no_optimization
 
     args = %W[tbb_build_prefix=BUILDPREFIX]
-
-    case ENV.compiler
-    when :clang
-      args << "compiler=clang"
-    else
-      args << "compiler=gcc"
-    end
-
-    if MacOS.prefer_64_bit?
-      args << "arch=intel64"
-    else
-      args << "arch=ia32"
-    end
 
     if build.cxx11?
       ENV.cxx11
