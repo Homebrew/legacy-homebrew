77ae4a5f63ab7762f0ff8317d6fa2ce566d5c0e2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-02 13:28:23 +0000

    git-review: add 1.24 bottle.

--- Library/Formula/git-review.rb
+++ Library/Formula/git-review.rb
@@ -5,6 +5,13 @@ class GitReview < Formula
   url "https://pypi.python.org/packages/source/g/git-review/git-review-1.24.tar.gz"
   sha1 "9183b505366b842cff32132ee88d8eff44bb7c89"
 
+  bottle do
+    cellar :any
+    sha1 "98167df95c295b0f194f7d775d8c231173557067" => :yosemite
+    sha1 "b66b3239d9079b451fbb665e718f201f6304ba8e" => :mavericks
+    sha1 "877276d594a72aced623689b31c1d4ef6dabdd0a" => :mountain_lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   resource "requests" do
