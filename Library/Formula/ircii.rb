71b8133e4d7e9de18079f3125b71204552952852
Jack Nagel <jacknagel@gmail.com>
2014-12-03 16:44:35 -0500

    ircii 20141122
    Closes #34650.

--- Library/Formula/ircii.rb
+++ Library/Formula/ircii.rb
@@ -2,13 +2,10 @@ require "formula"
 
 class Ircii < Formula
   homepage "http://www.eterna.com.au/ircii/"
-  url "http://ircii.warped.com/ircii-20140831.tar.bz2"
-  sha1 "a4d3b3a74a418f99217fe572f6e4c358f1ff3139"
+  url "http://ircii.warped.com/ircii-20141122.tar.bz2"
+  sha1 "e243dafb325334240c4306e568fb94fb21b201d6"
 
   bottle do
-    sha1 "c95445429da0b3d45122f84cb597ecc707a42939" => :mavericks
-    sha1 "461fa342adc66e1cd7e5d673bfaa9694d7cef766" => :mountain_lion
-    sha1 "ba3b54226f16b1dd7ab90ef7f4ee5f0c602eeca5" => :lion
   end
 
   depends_on "openssl"
