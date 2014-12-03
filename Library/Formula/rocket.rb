1952b4816bd30169e351053b9732fc781f0a66b9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-03 20:48:59 +0000

    rocket: add 0.1.0 bottle.

--- Library/Formula/rocket.rb
+++ Library/Formula/rocket.rb
@@ -8,6 +8,12 @@ class Rocket < Formula
   url "https://github.com/coreos/rocket/archive/v0.1.0.tar.gz"
   sha1 "04ae8cb9bac04eedacb03a7531e6b251556be653"
 
+  bottle do
+    sha1 "cb111d426363b5df807800ccf6b2f6eba7cdf10f" => :yosemite
+    sha1 "cd464addb3a1364a621aa64e0239a0d10ef11e65" => :mavericks
+    sha1 "70bf2fffc71a3e3c948bd20854880d2dca267611" => :mountain_lion
+  end
+
   depends_on "go" => :build
   depends_on "squashfs" => :build
 
