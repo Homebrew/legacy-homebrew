7360283d73e6fa17d13e1bc61439e33077f6e53e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-03 19:08:04 +0000

    odo: add 0.1.0 bottle.

--- Library/Formula/odo.rb
+++ Library/Formula/odo.rb
@@ -5,6 +5,13 @@ class Odo < Formula
   url "https://github.com/atomicobject/odo/archive/v0.1.0.tar.gz"
   sha1 "36cbe1b626612e0bbdf719640801d84b8234d7af"
 
+  bottle do
+    cellar :any
+    sha1 "0b19e2bc1308ef7bc9640cf8140ce60823933e07" => :yosemite
+    sha1 "d503b71664f5bb01be9d96d31ee6a7f6f6d4324b" => :mavericks
+    sha1 "42ec73e761493d9887c41abe5df3fe6de0f3741c" => :mountain_lion
+  end
+
   def install
     system "make"
     man1.mkpath
