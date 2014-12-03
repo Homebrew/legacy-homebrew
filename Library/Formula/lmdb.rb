d1a6df92978e9851deb0482447ac0122e0a0bce7
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-03 14:17:37 +0000

    lmdb 0.9.14
    Version bump.
    
    Closes #34636.

--- Library/Formula/lmdb.rb
+++ Library/Formula/lmdb.rb
@@ -1,10 +1,10 @@
-require 'formula'
+require "formula"
 
 class Lmdb < Formula
-  homepage 'http://symas.com/mdb/'
-  url "https://gitorious.org/mdb/mdb.git", :tag => "LMDB_0.9.13"
+  homepage "http://symas.com/mdb/"
+  url "https://gitorious.org/mdb/mdb.git", :tag => "LMDB_0.9.14"
 
-  head 'git://git.openldap.org/openldap.git', :branch => 'mdb.master'
+  head "git://git.openldap.org/openldap.git", :branch => "mdb.master"
 
   bottle do
     cellar :any
@@ -25,5 +25,9 @@ class Lmdb < Formula
     include.mkpath
 
     system "make", "-C", "libraries/liblmdb", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/mdb_dump", "-V"
   end
 end
