c9d3d234d482aac3792e0547e58534bcf15766a3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-03 21:53:13 +0000

    ircii: update 20141122 bottle.

--- Library/Formula/ircii.rb
+++ Library/Formula/ircii.rb
@@ -6,6 +6,9 @@ class Ircii < Formula
   sha1 "e243dafb325334240c4306e568fb94fb21b201d6"
 
   bottle do
+    sha1 "a91d564a3a3241e6684785d0a3a7a256ee319def" => :yosemite
+    sha1 "4691d4bbbe491b729af842870773b6d899a95433" => :mavericks
+    sha1 "826c088f54ddcf68afabc989c1915beb3ee0265d" => :mountain_lion
   end
 
   depends_on "openssl"
