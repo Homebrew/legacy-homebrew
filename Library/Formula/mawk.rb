e3e731fd7fbbad40b7c3e7aec70b120c392d8eb9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-04 22:25:12 +0000

    mawk: add 1.3.4-20141027 bottle.

--- Library/Formula/mawk.rb
+++ Library/Formula/mawk.rb
@@ -5,6 +5,13 @@ class Mawk < Formula
   url "ftp://invisible-island.net/mawk/mawk-1.3.4-20141027.tgz"
   sha1 "a0a54bde0c76a351fa073a6fa10ce912a1260144"
 
+  bottle do
+    cellar :any
+    sha1 "32fbaf50a142eba4ce968a0de1cc013cd2d39f66" => :yosemite
+    sha1 "9d733323a1e4e185ad6e9cf5a45139258f14ed37" => :mavericks
+    sha1 "169c70e2d8b83c9c19ebc97687278044fd8c855d" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
