911e17ae47b360884713c000a974df9614e0291a
Benjamin Weber <one@bwe.im>
2014-12-04 22:44:08 +0100

    mawk 1.3.4 20141027

--- Library/Formula/mawk.rb
+++ Library/Formula/mawk.rb
@@ -1,15 +1,23 @@
-require 'formula'
+require "formula"
 
 class Mawk < Formula
-  homepage 'http://invisible-island.net/mawk/mawk.html'
-  url 'ftp://invisible-island.net/mawk/mawk-1.3.4-20131226.tgz'
-  sha1 '5f7eb0d86e8984512facdec7c571110a6a1467b9'
+  homepage "http://invisible-island.net/mawk/"
+  url "ftp://invisible-island.net/mawk/mawk-1.3.4-20141027.tgz"
+  sha1 "a0a54bde0c76a351fa073a6fa10ce912a1260144"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
+                          "--disable-silent-rules",
                           "--with-readline=/usr/lib",
                           "--mandir=#{man}"
     system "make install"
   end
+
+  test do
+    version=`mawk '/version/ { print $2 }' #{prefix}/README`
+    assert_equal version, "#{version}"
+  end
+
 end
