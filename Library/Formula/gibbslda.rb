09f83f009b773ca572f588c0a978baf49b018007
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-04 22:49:12 +0000

    gibbslda: fix compile
    Closes #34678.
    
    Closes #34686.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gibbslda.rb
+++ Library/Formula/gibbslda.rb
@@ -1,13 +1,33 @@
-require 'formula'
+require "formula"
 
 class Gibbslda < Formula
-  homepage 'http://gibbslda.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/gibbslda/GibbsLDA%2B%2B/0.2/GibbsLDA%2B%2B-0.2.tar.gz'
-  sha1 '3264f01ae921b6dcbbe57dd877561271df214cdd'
+  homepage "http://gibbslda.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/gibbslda/GibbsLDA%2B%2B/0.2/GibbsLDA%2B%2B-0.2.tar.gz"
+  sha1 "3264f01ae921b6dcbbe57dd877561271df214cdd"
+
+  # Build fails without including stdlib - https://trac.macports.org/ticket/41915
+  # https://sourceforge.net/p/gibbslda/bugs/4/
+  patch :DATA
 
   def install
-    system "make clean"
-    system "make all"
+    system "make", "clean"
+    system "make", "all"
     bin.install "src/lda"
+    share.install "docs/GibbsLDA++Manual.pdf"
   end
 end
+
+__END__
+
+diff --git a/src/utils.cpp b/src/utils.cpp
+index e2f538b..1df5fb3 100644
+--- a/src/utils.cpp
++++ b/src/utils.cpp
+@@ -22,6 +22,7 @@
+  */
+
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <string>
+ #include <map>
+ #include "strtokenizer.h"
