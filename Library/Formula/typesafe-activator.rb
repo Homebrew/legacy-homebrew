683b9b63a2c09dcc49f2de0742c9c904b7ccbc8d
Will Sargent <wsargent@users.noreply.github.com>
2014-12-04 12:20:26 -0800

    typesafe-activator 1.2.12
    Update typesafe-activator.rb to 1.2.12.zip
    
    Closes #34679.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/typesafe-activator.rb
+++ Library/Formula/typesafe-activator.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class TypesafeActivator < Formula
-  homepage 'http://typesafe.com/activator'
-  url 'http://downloads.typesafe.com/typesafe-activator/1.2.10/typesafe-activator-1.2.10.zip'
-  sha1 'b6cb5f46d4a491cbfbff1944b5147c9daa93ffe4'
+  homepage 'https://typesafe.com/activator'
+  url 'http://downloads.typesafe.com/typesafe-activator/1.2.12/typesafe-activator-1.2.12.zip'
+  sha1 '31a7ad0ccbbb9308150e9bf5df98e632c3293cb7'
 
   def install
     rm Dir["*.bat"] # Remove Windows .bat files
