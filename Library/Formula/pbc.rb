b47d917168c697857a54233fe1c7aa8dd4a79249
Xu Cheng <xucheng@me.com>
2014-12-05 20:20:53 +0800

    pbc: add test

--- Library/Formula/pbc.rb
+++ Library/Formula/pbc.rb
@@ -13,4 +13,40 @@ class Pbc < Formula
                           "--prefix=#{prefix}"
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <pbc/pbc.h>
+      #include <assert.h>
+
+      int main()
+      {
+        pbc_param_t param;
+        pairing_t pairing;
+        element_t g1, g2, gt1, gt2, gt3, a, g1a;
+        pbc_param_init_a_gen(param, 160, 512);
+        pairing_init_pbc_param(pairing, param);
+        element_init_G1(g1, pairing);
+        element_init_G2(g2, pairing);
+        element_init_G1(g1a, pairing);
+        element_init_GT(gt1, pairing);
+        element_init_GT(gt2, pairing);
+        element_init_GT(gt3, pairing);
+        element_init_Zr(a, pairing);
+        element_random(g1); element_random(g2); element_random(a);
+        element_pairing(gt1, g1, g2); // gt1 = e(g1, g2)
+        element_pow_zn(g1a, g1, a); // g1a = g1^a
+        element_pow_zn(gt2, gt1, a); // gt2 = gt1^a = e(g1, g2)^a
+        element_pairing(gt3, g1a, g2); // gt3 = e(g1a, g2) = e(g1^a, g2)
+        assert(element_cmp(gt2, gt3) == 0); // assert gt2 == gt3
+        pairing_clear(pairing);
+        element_clear(g1); element_clear(g2); element_clear(gt1);
+        element_clear(gt2); element_clear(gt3); element_clear(a);
+        element_clear(g1a);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-lgmp", "-lpbc", "-o", "test"
+    system "./test"
+  end
 end
