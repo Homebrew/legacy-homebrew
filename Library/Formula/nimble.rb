225616c85843a15c7ea13b5d3e24323a8662d298
Philip Wernersbach <philip.wernersbach@gmail.com>
2014-12-04 21:53:45 -0500

    nimble: Add formula (nimrod's package manager).

---
+++ Library/Formula/nimble.rb
@@ -0,0 +1,38 @@
+require "formula"
+
+class Nimble < Formula
+  homepage 'https://github.com/nimrod-code/nimble'
+  head 'https://github.com/nimrod-code/nimble.git'
+
+  depends_on 'nimrod'
+
+  resource 'nimble-wrapper' do
+    url 'https://github.com/philip-wernersbach/nimble-wrapper.git'
+  end
+
+  def install
+    cd '..' do
+      mkdir prefix/'nimble_wrapper'
+
+      mv buildpath, prefix/'nimble_wrapper/nimble'
+      mkdir buildpath
+    end
+
+    resource('nimble-wrapper').stage do
+      system 'nimrod', 'c', '-r', 'install', prefix/'nimble_wrapper'
+    end
+
+    bin.install_symlink prefix/'nimble_wrapper/bin/nimble'
+  end
+
+  test do
+    system "#{bin}/nimble", '--version'
+  end
+
+  def caveats; <<-EOS.undent
+    nimble's bin directory can be included in $PATH for easier access to
+    executables installed by nimble packages:
+      ~/.babel/bin
+    EOS
+  end
+end
