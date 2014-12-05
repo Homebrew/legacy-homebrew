09b9c9dfba75c1beb49cd636e38a1c498aa72bda
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2014-12-05 09:41:35 +0000

    wxpython 3.0.2.0
    WXPython 3.0.2
    
    Closes #34699.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/wxpython.rb
+++ Library/Formula/wxpython.rb
@@ -15,9 +15,8 @@ end
 
 class Wxpython < Formula
   homepage "http://www.wxwidgets.org"
-  url "https://downloads.sourceforge.net/project/wxpython/wxPython/3.0.1.1/wxPython-src-3.0.1.1.tar.bz2"
-  sha1 "d2c4719015d7c499a9765b1e5107fdf37a32abfb"
-  revision 1
+  url "https://downloads.sourceforge.net/project/wxpython/wxPython/3.0.2.0/wxPython-src-3.0.2.0.tar.bz2"
+  sha1 "5053f3fa04f4eb3a9d4bfd762d963deb7fa46866"
 
   bottle do
     sha1 "d9fe82c3cc35a8b2a8a66ca66ed8024641884561" => :yosemite
@@ -30,18 +29,6 @@ class Wxpython < Formula
     depends_on FrameworkPython
   end
   depends_on "wxmac"
-
-  stable do
-    # See closed ticket #16590:
-    #     Update wxpython lib/plot.py (numpy has removed oldnumeric)
-    #     http://trac.wxwidgets.org/ticket/16590
-    # Applied upstream: http://trac.wxwidgets.org/changeset/77995
-    # This duplicate gist patch just strips "/trunk" from within target file's path
-    patch :p0 do
-      url "https://gist.githubusercontent.com/dakcarto/f0331c2e4e97a7c4271e/raw/9e65152464c6321bd2c5ff723c21b6cc78958e03/wxpython_77995.diff"
-      sha1 "73b90a983fbb5330abc1b3866817081c8efde479"
-    end
-  end
 
   def install
     ENV["WXWIN"] = buildpath
