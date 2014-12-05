edb55e0acd419ee4d6ce66b7255922b83a9afbd2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-05 17:44:17 +0000

    bwctl: add 1.5.2-10 bottle.

--- Library/Formula/bwctl.rb
+++ Library/Formula/bwctl.rb
@@ -5,6 +5,13 @@ class Bwctl < Formula
   url "http://software.internet2.edu/sources/bwctl/bwctl-1.5.2-10.tar.gz"
   sha1 "5dcc7a1d671ac8e061f859a430d56ae2551f507e"
 
+  bottle do
+    cellar :any
+    sha1 "b44fbe6a0a4cb82f9d5f9af6062006bbe22d163c" => :yosemite
+    sha1 "318f7c3022df966510467489102ea2f3fa6ea728" => :mavericks
+    sha1 "d18b048baeb365d22717b2b831c164eb1c8ba125" => :mountain_lion
+  end
+
   depends_on "i2util"
 
   def install
