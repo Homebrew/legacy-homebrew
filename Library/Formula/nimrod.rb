e9bf5adff76df0d0771ec5711c014e8982e752d3
Philip Wernersbach <philip.wernersbach@gmail.com>
2014-12-05 15:46:50 -0500

    nimrod: Change single quotes to double quotes.

--- Library/Formula/nimrod.rb
+++ Library/Formula/nimrod.rb
@@ -1,56 +1,56 @@
 require 'formula'
 
 class Nimrod < Formula
-  homepage 'http://nimrod-lang.org/'
+  homepage "http://nimrod-lang.org/"
 
-  url 'https://github.com/Araq/Nimrod/archive/v0.9.6.tar.gz'
-  sha1 '5052ef6faada272392cec415d9cd64cb530724b9'
+  url "https://github.com/Araq/Nimrod/archive/v0.9.6.tar.gz"
+  sha1 "5052ef6faada272392cec415d9cd64cb530724b9"
 
-  resource 'csources' do
-    url 'https://github.com/nimrod-code/csources.git'
+  resource "csources" do
+    url "https://github.com/nimrod-code/csources.git"
   end
 
   # Small patch to fix http://github.com/Araq/Nimrod/issues/1701
   patch :DATA
 
   def install
-    csources_path = buildpath/'csources'
+    csources_path = buildpath/"csources"
 
     # Some advanced tools like c2nim require files from the compiler
     # directory. Make a fresh one so we can move it after we build.
-    cp_r 'compiler', 'compiler.orig'
+    cp_r "compiler", "compiler.orig"
 
-    resource('csources').stage do
+    resource("csources").stage do
       csources_staging = Pathname.pwd
 
-      cd '..' do
+      cd ".." do
         mv csources_staging, csources_path
         mkdir csources_staging
       end
     end
 
     cd csources_path do
-      system '/bin/sh', './build.sh'
+      system "/bin/sh", "./build.sh"
     end
 
-    system './bin/nimrod', 'c', 'koch'
-    system './koch', 'boot', '-d:release'
+    system "./bin/nimrod", "c", "koch"
+    system "./koch", "boot", "-d:release"
 
-    # For some reason the mingw variable doesn't get passed through,
+    # For some reason the mingw variable doesn"t get passed through,
     # so hardcode it until upstream fixes it. In some ways the mingw
     # variable is unneeded because it is always mingw32.
-    inreplace 'compiler/nimrod.ini', '${mingw}', 'mingw32'
+    inreplace "compiler/nimrod.ini", "${mingw}", "mingw32"
 
-    system './koch', 'install', prefix
-    mv 'compiler.orig', prefix/'nimrod/compiler'
-    bin.install_symlink prefix/'nimrod/bin/nimrod'
+    system "./koch", "install", prefix
+    mv "compiler.orig", prefix/"nimrod/compiler"
+    bin.install_symlink prefix/"nimrod/bin/nimrod"
   end
 
   test do
-    (testpath/'hello.nim').write <<-EOS.undent
+    (testpath/"hello.nim").write <<-EOS.undent
       echo("Hi!")
     EOS
-    system "#{bin}/nimrod", 'compile', '--run', 'hello.nim'
+    system "#{bin}/nimrod", "compile", "--run", "hello.nim"
   end
 end
 
