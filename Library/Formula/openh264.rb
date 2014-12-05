c808f6629a5f2ccd522a1cdef2e1caad802d31c2
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-05 14:58:12 +0000

    openh264: add 1.1 bottle.

--- Library/Formula/openh264.rb
+++ Library/Formula/openh264.rb
@@ -6,6 +6,13 @@ class Openh264 < Formula
   sha256 "d1d8e0087adfd372c22fd84746092a04627355468e70420451b2c2dbd4c37699"
   head "https://github.com/cisco/openh264.git"
 
+  bottle do
+    cellar :any
+    sha1 "18d43434ccf60b8e77deabc4fe14fa8d5f3b8dd0" => :yosemite
+    sha1 "b846a0fca45ef3066904ce4ee12d3d2839bdb229" => :mavericks
+    sha1 "3efc07638d0379c12cc832d18c263cb314a77f6b" => :mountain_lion
+  end
+
   depends_on "nasm" => :build
 
   def install
