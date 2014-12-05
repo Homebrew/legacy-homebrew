9bd4200b80b6f808eaeb3e3528faa4636a2528b8
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-05 16:50:34 +0000

    gibbslda: add 0.2 bottle.

--- Library/Formula/gibbslda.rb
+++ Library/Formula/gibbslda.rb
@@ -5,6 +5,13 @@ class Gibbslda < Formula
   url "https://downloads.sourceforge.net/project/gibbslda/GibbsLDA%2B%2B/0.2/GibbsLDA%2B%2B-0.2.tar.gz"
   sha1 "3264f01ae921b6dcbbe57dd877561271df214cdd"
 
+  bottle do
+    cellar :any
+    sha1 "0a0861b3c2fd9037a8ec71f43a09115dceaed4a8" => :yosemite
+    sha1 "65dd821a8ce2c7f24001a56218b0c00cdf1d9051" => :mavericks
+    sha1 "7ecc74e2069e864ac26614209a7236215ab4faa0" => :mountain_lion
+  end
+
   # Build fails without including stdlib - https://trac.macports.org/ticket/41915
   # https://sourceforge.net/p/gibbslda/bugs/4/
   patch :DATA
