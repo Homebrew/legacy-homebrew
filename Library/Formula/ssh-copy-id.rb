1c723fa7d47e51fac4b8a4422ca92954379c1945
Birger J. Nordølum <birgerjn@gmail.com>
2014-12-05 21:31:52 +0100

    ssh-copy-id: 6.7p1
    Closes #34714.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ssh-copy-id.rb
+++ Library/Formula/ssh-copy-id.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class SshCopyId < Formula
   homepage 'http://www.openssh.com/'
-  url 'http://ftp.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.6p1.tar.gz'
-  mirror 'http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.6p1.tar.gz'
-  version '6.6p1'
-  sha1 'b850fd1af704942d9b3c2eff7ef6b3a59b6a6b6e'
+  url 'http://ftp.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz'
+  mirror 'http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz'
+  version '6.7p1'
+  sha1 '14e5fbed710ade334d65925e080d1aaeb9c85bf6'
 
   bottle do
     cellar :any
