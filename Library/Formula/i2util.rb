85eb3faa79113ef63b7c108b4b24a10d8dd6191e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-05 17:21:56 +0000

    i2util: add 1.2 bottle.

--- Library/Formula/i2util.rb
+++ Library/Formula/i2util.rb
@@ -5,6 +5,13 @@ class I2util < Formula
   url "http://software.internet2.edu/sources/I2util/I2util-1.2.tar.gz"
   sha1 "56218a6e0f1306a70b641246fa6f63dd686e6766"
 
+  bottle do
+    cellar :any
+    sha1 "14336de85e35136a369e889d1072adf866998f09" => :yosemite
+    sha1 "c88dd13937831bab2efd3ad928d54635a48ef129" => :mavericks
+    sha1 "eced6a87c580fe024730ca09fa61c6a5e3cc4d63" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
