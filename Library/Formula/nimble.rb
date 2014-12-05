b8933533b943146aba2e8f6e38b53c484ee349af
Philip Wernersbach <philip.wernersbach@gmail.com>
2014-12-05 15:48:28 -0500

    nimble: Change single quotes to double quotes.

--- Library/Formula/nimble.rb
+++ Library/Formula/nimble.rb
@@ -1,36 +1,36 @@
 require "formula"
 
 class Nimble < Formula
-  homepage 'https://github.com/nimrod-code/nimble'
-  head 'https://github.com/nimrod-code/nimble.git'
+  homepage "https://github.com/nimrod-code/nimble"
+  head "https://github.com/nimrod-code/nimble.git"
 
-  depends_on 'nimrod'
+  depends_on "nimrod"
 
-  resource 'nimble-wrapper' do
-    url 'https://github.com/philip-wernersbach/nimble-wrapper.git'
+  resource "nimble-wrapper" do
+    url "https://github.com/philip-wernersbach/nimble-wrapper.git"
   end
 
   def install
-    cd '..' do
-      mkdir prefix/'nimble_wrapper'
+    cd ".." do
+      mkdir prefix/"nimble_wrapper"
 
-      mv buildpath, prefix/'nimble_wrapper/nimble'
+      mv buildpath, prefix/"nimble_wrapper/nimble"
       mkdir buildpath
     end
 
-    resource('nimble-wrapper').stage do
-      system 'nimrod', 'c', '-r', 'install', prefix/'nimble_wrapper'
+    resource("nimble-wrapper").stage do
+      system "nimrod", "c", "-r", "install", prefix/"nimble_wrapper"
     end
 
-    bin.install_symlink prefix/'nimble_wrapper/bin/nimble'
+    bin.install_symlink prefix/"nimble_wrapper/bin/nimble"
   end
 
   test do
-    system "#{bin}/nimble", '--version'
+    system "#{bin}/nimble", "--version"
   end
 
   def caveats; <<-EOS.undent
-    nimble's bin directory can be included in $PATH for easier access to
+    nimble"s bin directory can be included in $PATH for easier access to
     executables installed by nimble packages:
       ~/.babel/bin
     EOS
