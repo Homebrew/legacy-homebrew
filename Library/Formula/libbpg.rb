320ea43108f216aec9cc881e3d853f525bcaa182
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-06 16:04:27 +0000

    libbpg: add 0.9 bottle.

--- Library/Formula/libbpg.rb
+++ Library/Formula/libbpg.rb
@@ -5,6 +5,13 @@ class Libbpg < Formula
   url "http://bellard.org/bpg/libbpg-0.9.tar.gz"
   sha1 "d40209384adf517c773a7a28cec0d4759051bf2c"
 
+  bottle do
+    cellar :any
+    sha1 "5ce24008a63d1362c54f63765fefbf0fd8fcd2d0" => :yosemite
+    sha1 "458ac4571970643dd81352d3a9591377c5004327" => :mavericks
+    sha1 "8b906bd6b3f1537f6805e3bbd2df84dad93bcf43" => :mountain_lion
+  end
+
   depends_on "libpng"
   depends_on "jpeg"
 
