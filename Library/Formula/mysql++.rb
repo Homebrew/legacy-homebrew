575d2210cee6af41769c86679eb40b4a2764c1fe
Ruben Fonseca <fonseka@gmail.com>
2014-12-07 23:36:45 +0000

    mysql++ 3.2.1
    Closes #34768.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/mysql++.rb
+++ Library/Formula/mysql++.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Mysqlxx < Formula
-  url 'http://tangentsoft.net/mysql++/releases/mysql++-3.2.0.tar.gz'
   homepage 'http://tangentsoft.net/mysql++/'
-  sha1 '4bd50f5b8259b5f12e42d6a810e5941eaddaf24a'
+  url 'http://tangentsoft.net/mysql++/releases/mysql++-3.2.1.tar.gz'
+  sha1 '1ce5a4484b66d9852718412315e1409cafd8c397'
 
   depends_on 'mysql-connector-c'
 
