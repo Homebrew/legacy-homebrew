a2134de037357ced58d28367c41f954537077407
Jack Nagel <jacknagel@gmail.com>
2014-12-07 02:34:22 -0500

    libfreenect 0.5.1
    Closes #32629.

--- Library/Formula/libfreenect.rb
+++ Library/Formula/libfreenect.rb
@@ -2,17 +2,12 @@ require "formula"
 
 class Libfreenect < Formula
   homepage "http://openkinect.org"
-  url "https://github.com/OpenKinect/libfreenect/archive/v0.5.0.tar.gz"
-  sha1 "3a0c6ca9b2515132a59232ed5719ee2731f6e580"
+  url "https://github.com/OpenKinect/libfreenect/archive/v0.5.1.tar.gz"
+  sha1 "1f7296e50c27c07e2f57ee906c195cabf97c1438"
 
   head "https://github.com/OpenKinect/libfreenect.git"
 
   bottle do
-    cellar :any
-    revision 1
-    sha1 "01f9e79b4be90a3a5487fe19e2ee54a62b732ed9" => :yosemite
-    sha1 "61ee80a144a0ca7a2b89ab6fec33524aa745bd67" => :mavericks
-    sha1 "39b2d3e43dc3e0de01b000c6ea9347b276af8383" => :mountain_lion
   end
 
   option :universal
