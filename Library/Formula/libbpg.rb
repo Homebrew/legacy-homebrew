d9c7001e7ba3464932c0116b61528c4deddd4674
PiRSquared17 <pirsquared@tools.wmflabs.org>
2014-12-07 13:17:38 -0500

    Update libbpg
    libbpg-0.9 tarball was removed, issues fixed in 0.9.1

--- Library/Formula/libbpg.rb
+++ Library/Formula/libbpg.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Libbpg < Formula
   homepage "http://bellard.org/bpg/"
-  url "http://bellard.org/bpg/libbpg-0.9.tar.gz"
-  sha1 "d40209384adf517c773a7a28cec0d4759051bf2c"
+  url "http://bellard.org/bpg/libbpg-0.9.1.tar.gz"
+  sha1 "0ece88372e178985d3327bbb7a0c94947586b3f1"
 
   bottle do
     cellar :any
@@ -16,17 +16,6 @@ class Libbpg < Formula
   depends_on "jpeg"
 
   def install
-    # Following changes are necessary for compilation on OS X. These have been
-    # reported to the author and can be removed once incorporated upstream.
-    inreplace "libavutil/mem.c" do |s|
-      s.gsub! "#include <malloc.h>", "#include <malloc/malloc.h>"
-    end
-
-    inreplace "Makefile" do |s|
-      s.gsub! "--gc-sections", "-dead_strip"
-      s.gsub! "LIBS:=-lrt -lm -lpthread", "LIBS:=-lm -lpthread"
-    end
-
     bin.mkpath
     system "make", "install", "prefix=#{prefix}"
   end
