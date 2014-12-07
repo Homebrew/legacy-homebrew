fb76b222c3012d25654c407640bfeaf1a2dab8da
Kyungdahm Yun <tomyun@gmail.com>
2014-12-07 12:17:54 -0800

    mess 0.156: use upstream patch URL

--- Library/Formula/mess.rb
+++ Library/Formula/mess.rb
@@ -20,7 +20,10 @@ class Mess < Formula
   # Fix for Cocoa framework linking and sdl-config path
   # It's been upstreamed, so remove from the next release
   # https://github.com/mamedev/mame/pull/60
-  patch :DATA
+  patch do
+    url "https://github.com/mbcoguno/mame/commit/5d8dc09b12ea7d5576ef92e3e14a1db3654ff810.diff"
+    sha1 "76c0f11f225968a10c0f024c55607e9f09634825"
+  end
 
   def install
     ENV['MACOSX_USE_LIBSDL'] = '1'
@@ -36,20 +39,3 @@ class Mess < Formula
     end
   end
 end
-
-__END__
-diff --git a/src/osd/sdl/sdl.mak b/src/osd/sdl/sdl.mak
-index 21dd814..eaec664 100644
---- a/src/osd/sdl/sdl.mak
-+++ b/src/osd/sdl/sdl.mak
-@@ -507,8 +507,8 @@ else
- # files (header files are #include "SDL/something.h", so the extra "/SDL"
- # causes a significant problem)
--INCPATH += `sdl-config --cflags | sed 's:/SDL::'`
-+INCPATH += `$(SDL_CONFIG) --cflags | sed 's:/SDL::'`
- CCOMFLAGS += -DNO_SDL_GLEXT
- # Remove libSDLmain, as its symbols conflict with SDLMain_tmpl.m
--LIBS += `sdl-config --libs | sed 's/-lSDLmain//'` -lpthread -framework OpenGL
-+LIBS += `$(SDL_CONFIG) --libs | sed 's/-lSDLmain//'` -lpthread -framework Cocoa -framework OpenGL
- DEFS += -DMACOSX_USE_LIBSDL
- endif   # MACOSX_USE_LIBSDL
