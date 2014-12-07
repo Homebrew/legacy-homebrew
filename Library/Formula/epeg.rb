aca987b7e803e918455975150f1fb83e1087b9ab
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-07 07:08:41 +0000

    epeg: add 0.9.1.042 bottle.

--- Library/Formula/epeg.rb
+++ Library/Formula/epeg.rb
@@ -7,6 +7,13 @@ class Epeg < Formula
 
   head "https://github.com/mattes/epeg.git"
 
+  bottle do
+    cellar :any
+    sha1 "a12a4553bd78bff070f01be5d3e7062372383e31" => :yosemite
+    sha1 "ce665a144c3c355ae745067e686465f1f6dfafc3" => :mavericks
+    sha1 "24ca70477748c42a71a31df9facc1b21a400c1e1" => :mountain_lion
+  end
+
   depends_on "automake" => :build
   depends_on "jpeg"
   depends_on "libexif"
