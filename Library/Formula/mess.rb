c1cf5fad54614b298898b879102e0c174d80fb5a
Kyungdahm Yun <tomyun@gmail.com>
2014-12-07 01:34:58 -0800

    mess 0.156
    * Switch to SDL2 with hotfix
    
    Closes #34751.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/mess.rb
+++ Library/Formula/mess.rb
@@ -2,20 +2,21 @@ require 'formula'
 
 class Mess < Formula
   homepage "http://www.mess.org/"
-  url "https://github.com/mamedev/mame/archive/mame0155.tar.gz"
-  sha1 "0e56d53dd6dd916b3c29387112a7042befc501bd"
-  version "0.155"
+  url "https://github.com/mamedev/mame/archive/mame0156.tar.gz"
+  sha1 "9cda662385c0b168ca564dab0fb1e839065f6a01"
+  version "0.156"
 
   head "https://github.com/mamedev/mame.git"
 
-  bottle do
-    cellar :any
-    sha1 "e5a44194f5e7498ef816530ec8196c1f96448f96" => :yosemite
-    sha1 "6003ef6faaced6b86a8f63ab7a2ec33885c2fa75" => :mavericks
-    sha1 "51c9c19ba6dfc316986406018da7a4f86c46dd1d" => :mountain_lion
-  end
+  depends_on "sdl2"
 
-  depends_on 'sdl'
+  # Fix for Cocoa framework linking and sdl-config path
+  # It's been upstreamed, so remove from the next release
+  # https://github.com/mamedev/mame/pull/60
+  patch do
+    url "https://github.com/mbcoguno/mame/commit/5d8dc09b12ea7d5576ef92e3e14a1db3654ff810.diff"
+    sha1 "76c0f11f225968a10c0f024c55607e9f09634825"
+  end
 
   def install
     ENV['MACOSX_USE_LIBSDL'] = '1'
