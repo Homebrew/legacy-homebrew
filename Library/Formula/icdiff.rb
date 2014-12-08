52a2ff0f81865499c03bd8b506027535fcb40980
Mike McQuaid <mike@mikemcquaid.com>
2014-12-08 15:22:07 +0000

    icdiff: remove unnecessary version, improve test.
    Let's not use temporary files that will stick around afterwards.

--- Library/Formula/icdiff.rb
+++ Library/Formula/icdiff.rb
@@ -5,14 +5,15 @@ class Icdiff < Formula
   homepage "https://github.com/jeffkaufman/icdiff"
   url "https://github.com/jeffkaufman/icdiff/archive/release-1.1.2.tar.gz"
   sha1 "89cfb79237a59ed33c55fb020965f7c94e7510bc"
-  version "1.1.2"
 
   def install
     bin.install "icdiff", "git-icdiff"
   end
 
   test do
-    system "#{bin}/icdiff", Tempfile.new('ic-diff').path, Tempfile.new('ic-diff').path
+    (testpath/"file1").write "test1"
+    (testpath/"file2").write "test2"
+    system "#{bin}/icdiff", "file1", "file2"
     system "git", "init"
     system "#{bin}/git-icdiff"
   end
