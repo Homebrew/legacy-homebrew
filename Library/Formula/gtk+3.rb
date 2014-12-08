a7bc93916443a329cb3aef59b185bca3176ae54d
Mike McQuaid <mike@mikemcquaid.com>
2014-12-08 19:33:45 +0000

    gtk+3: add revision, general cleanup.
    Requested in #34555.
    
    Closes #34797.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gtk+3.rb
+++ Library/Formula/gtk+3.rb
@@ -1,9 +1,10 @@
-require 'formula'
+require "formula"
 
 class Gtkx3 < Formula
-  homepage 'http://gtk.org/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/gtk+/3.14/gtk+-3.14.5.tar.xz'
-  sha256 'ba70f5ccde6646c6d8aa5a6398794b7bcf23fc45af22580a215d258f392dbbe2'
+  homepage "http://gtk.org/"
+  url "http://ftp.gnome.org/pub/gnome/sources/gtk+/3.14/gtk+-3.14.5.tar.xz"
+  sha256 "ba70f5ccde6646c6d8aa5a6398794b7bcf23fc45af22580a215d258f392dbbe2"
+  revision 1
 
   bottle do
     revision 1
@@ -14,25 +15,25 @@ class Gtkx3 < Formula
 
   option :universal
 
-  depends_on :x11 => ['2.5', :recommended] # needs XInput2, introduced in libXi 1.3
-  depends_on 'pkg-config' => :build
-  depends_on 'glib'
-  depends_on 'jpeg'
-  depends_on 'libtiff'
-  depends_on 'gdk-pixbuf'
-  depends_on 'pango'
-  depends_on 'cairo'
-  depends_on 'jasper' => :optional
-  depends_on 'atk'
-  depends_on 'at-spi2-atk' if build.with? "x11"
-  depends_on 'gobject-introspection'
-  depends_on 'gsettings-desktop-schemas' => :recommended
+  depends_on :x11 => ["2.5", :recommended] # needs XInput2, introduced in libXi 1.3
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "jpeg"
+  depends_on "libtiff"
+  depends_on "gdk-pixbuf"
+  depends_on "pango"
+  depends_on "cairo"
+  depends_on "jasper" => :optional
+  depends_on "atk"
+  depends_on "at-spi2-atk" if build.with? "x11"
+  depends_on "gobject-introspection"
+  depends_on "gsettings-desktop-schemas" => :recommended
 
   # Fixes runtime error in 3.14.5; can probably be removed in later versions
   # see http://comments.gmane.org/gmane.os.apple.macports.tickets/90114
   patch do
-    url 'https://git.gnome.org/browse/gtk+/patch/?id=0b8f666e022d983db2cefaffb24315dc34b26673'
-    sha1 'f7f475905245324caa5e7eb037b0de021bf2d9ff'
+    url "https://git.gnome.org/browse/gtk+/patch/?id=0b8f666e022d983db2cefaffb24315dc34b26673"
+    sha1 "f7f475905245324caa5e7eb037b0de021bf2d9ff"
   end
 
   def install
@@ -54,9 +55,9 @@ class Gtkx3 < Formula
     end
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
     # Prevent a conflict between this and Gtk+2
-    mv bin/'gtk-update-icon-cache', bin/'gtk3-update-icon-cache'
+    mv bin/"gtk-update-icon-cache", bin/"gtk3-update-icon-cache"
   end
 
   def post_install
