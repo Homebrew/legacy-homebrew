Homebrew/legacy-homebrew

commit b1f844cb2710163fe37489614c2728bf4d40e16e

fontforge: url fix
The upstream URL link moved retroactively, so this fixes that link for
from-source builds.

Closes #34508.

Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

Library/Formula/fontforge.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Fontforge < Formula
   homepage "https://fontforge.github.io"
-  url "https://github.com/fontforge/fontforge/releases/download/20141126/fontforge-20141126.tar.gz"
+  url "https://github.com/fontforge/fontforge/releases/download/20141126/fontforge-2014-11-26-Unix-Source.tar.gz"
   sha1 "ecd776480a47cdcbe1b30ce275172d7d52288e77"
   head "https://github.com/fontforge/fontforge.git"
 

commit 3aff90bb931a11d9636b688f12c0e7b98d84b705

Add an explicit version to fontforge.
The change of URL in b1f844c caused brew to fetch the wrong bottle,
which failed when validating the checksum.

Fixes #34778.

Library/Formula/fontforge.rb
@@ -5,6 +5,7 @@ class Fontforge < Formula
   url "https://github.com/fontforge/fontforge/releases/download/20141126/fontforge-2014-11-26-Unix-Source.tar.gz"
   sha1 "ecd776480a47cdcbe1b30ce275172d7d52288e77"
   head "https://github.com/fontforge/fontforge.git"
+  version "20141126"
 
   bottle do
     sha1 "9cb3881adf612eae21aa4c70eb17907a96f05d8d" => :yosemite