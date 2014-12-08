19082b01b39555907e6678c9b7baea6ceda5a543
0xbb <account@bierbaumer.net>
2014-12-08 01:50:03 +0000

    Remove boost, update to v0.4.1

--- Library/Formula/libjson-rpc-cpp.rb
+++ Library/Formula/libjson-rpc-cpp.rb
@@ -2,13 +2,13 @@ require "formula"
 
 class LibjsonRpcCpp < Formula
   homepage "https://github.com/cinemast/libjson-rpc-cpp"
-  url "https://github.com/cinemast/libjson-rpc-cpp/archive/v0.3.2.tar.gz"
-  sha1 "37697f2647a1062158a7f3bb83b503195bf27363"
+  url "https://github.com/cinemast/libjson-rpc-cpp/archive/v0.4.1.tar.gz"
+  sha1 "199c5f262c41e51ff1d4f3625fb25543d97e852b"
 
-  depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "argtable"
   depends_on "jsoncpp"
+  depends_on "libmicrohttpd"  
 
   def install
     system "cmake", ".", *std_cmake_args
