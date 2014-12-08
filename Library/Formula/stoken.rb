6bd2da69ce11516eb6906028c89a130347cf05c4
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-08 23:55:17 +0000

    stoken: add 0.81 bottle.

--- Library/Formula/stoken.rb
+++ Library/Formula/stoken.rb
@@ -5,6 +5,13 @@ class Stoken < Formula
   url "https://downloads.sourceforge.net/project/stoken/stoken-0.81.tar.gz"
   sha1 "db36aec5a8bd3f5f92deaebdea08cb639b78da73"
 
+  bottle do
+    cellar :any
+    sha1 "d2fc515e490de6bc426eaf2107904e2f367ee628" => :yosemite
+    sha1 "c77630e2aff83fb1b80b720fc301a0e50a7a2d4a" => :mavericks
+    sha1 "31ce6acf84301626c0763fd3815ccd1c83ae4d36" => :mountain_lion
+  end
+
   depends_on "gtk+3" => :optional
   if build.with? "gtk+3"
     depends_on "gnome-icon-theme"
