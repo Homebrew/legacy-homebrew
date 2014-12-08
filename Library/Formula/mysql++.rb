33515ef9419a1df8807df8f44252c842128615f5
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-08 04:10:48 +0000

    mysql++: add 3.2.1 bottle.

--- Library/Formula/mysql++.rb
+++ Library/Formula/mysql++.rb
@@ -5,6 +5,14 @@ class Mysqlxx < Formula
   url 'http://tangentsoft.net/mysql++/releases/mysql++-3.2.1.tar.gz'
   sha1 '1ce5a4484b66d9852718412315e1409cafd8c397'
 
+  bottle do
+    cellar :any
+    sha1 "51f59174988a858aabde99fbb4bee90da14b3f05" => :yosemite
+    sha1 "8efb125df0553a8c7e764c02846e258f8de73ebd" => :mavericks
+    sha1 "9fedd7c2ae4b463a0ff730e29a0e493274896d2b" => :mountain_lion
+  end
+
+
   depends_on 'mysql-connector-c'
 
   def install
