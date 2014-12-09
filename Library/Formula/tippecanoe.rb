ba9c733349f8f536f826f4a185a21cf7dbf60e0d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-09 15:35:14 +0000

    tippecanoe: add 1.0.2 bottle.

--- Library/Formula/tippecanoe.rb
+++ Library/Formula/tippecanoe.rb
@@ -5,6 +5,13 @@ class Tippecanoe < Formula
   url "https://github.com/mapbox/tippecanoe/archive/v1.0.2.tar.gz"
   sha1 "c7804c835a212a04573b94cc084bbae41e23d928"
 
+  bottle do
+    cellar :any
+    sha1 "deac5778d837d4fa0b89decacafbaf59fcfeabbb" => :yosemite
+    sha1 "911bac4f9ff553686b657004527d62f95b626c5d" => :mavericks
+    sha1 "5513d9d1ba3cc55061f094cf0dff8ee0430fddea" => :mountain_lion
+  end
+
   depends_on "protobuf-c"
 
   def install
