fe6862ed409af294491eb09c14a5c0dd9318feaf
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-09 04:09:39 +0000

    hydra 8.1
    Version bump.
    
    Closes #34816.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/hydra.rb
+++ Library/Formula/hydra.rb
@@ -2,16 +2,13 @@ require "formula"
 
 class Hydra < Formula
   homepage "https://www.thc.org/thc-hydra/"
-  url "https://www.thc.org/releases/hydra-8.0.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/h/hydra/hydra_8.0.orig.tar.gz"
-  sha1 "d1a705985846caf77c291461f391a43457cc76e5"
-  revision 1
+  url "https://www.thc.org/releases/hydra-8.1.tar.gz"
+  sha256 "e4bc2fd11f97a8d985a38a31785c86d38cc60383e47a8f4a5c436351e5135f19"
 
   head "https://github.com/vanhauser-thc/thc-hydra"
 
   bottle do
     cellar :any
-    revision 2
     sha1 "8eef0ef48e07a9b50f51d6b6f62d76be48cd2bb7" => :yosemite
     sha1 "a918878af2ceb50c9ae0cd7193d99c8911fc179b" => :mavericks
     sha1 "57e89de532dc1af0c6fd939357881d9920274597" => :mountain_lion
