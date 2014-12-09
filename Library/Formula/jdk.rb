1274ba7f92d932e0daa7971ea5aa666be36cca4d
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-12-09 02:02:14 +0000

    jdk 1.8.0-25: Use curl --cookie
    Closes Homebrew/linuxbrew#206

--- Library/Formula/jdk.rb
+++ Library/Formula/jdk.rb
@@ -1,11 +1,19 @@
 require "formula"
 
+class JdkDownloadStrategy < CurlDownloadStrategy
+  def _fetch
+    curl @url, "-C", downloaded_size, "-o", temporary_path,
+      "--cookie", "oraclelicense=accept-securebackup-cookie"
+  end
+end
+
 class Jdk < Formula
   homepage "http://www.java.com/"
 
   version "1.8.0-25"
   if OS.linux?
-    url "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz"
+    url "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz",
+      :using => JdkDownloadStrategy
     sha1 "0eb0448641c21c435cddc4705d23668d45f29fff"
   elsif OS.mac?
     raise "On Mac OS try instead `brew cask install java`"
@@ -15,6 +23,13 @@ class Jdk < Formula
 
   def install
     prefix.install Dir["*"]
+  end
+
+  def caveats; <<-EOS.undent
+    By installing and using JDK you agree to the
+    Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX
+    http://www.oracle.com/technetwork/java/javase/terms/license/index.html
+    EOS
   end
 
   test do
