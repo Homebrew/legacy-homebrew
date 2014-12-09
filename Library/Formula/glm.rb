47e371d1de27a426f00c221561e6f1c891b2ecfe
Benjamin Medicke <bmedicke@gmail.com>
2014-12-09 19:05:22 +0100

    glm: fix head
    Closes #34832.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/glm.rb
+++ Library/Formula/glm.rb
@@ -4,7 +4,7 @@ class Glm < Formula
   homepage "http://glm.g-truc.net/"
   url "https://downloads.sourceforge.net/project/ogl-math/glm-0.9.5.4/glm-0.9.5.4.zip"
   sha1 "d9666b5b013d374c7d1a498c9495f7142f6fe9d3"
-  head "https://github.com/Groovounet/glm.git"
+  head "https://github.com/g-truc/glm.git"
 
   def install
     include.install "glm"
