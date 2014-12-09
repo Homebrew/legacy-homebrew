b66a6ec4dbd47bbe2593945943986383cb68ef23
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-09 13:28:08 +0000

    queequeg: add 0.91 bottle.

--- Library/Formula/queequeg.rb
+++ Library/Formula/queequeg.rb
@@ -5,6 +5,13 @@ class Queequeg < Formula
   url "https://downloads.sourceforge.net/project/queequeg/queequeg/queequeg-0.91/queequeg-0.91.tar.gz"
   sha1 "e0ceae717a297ee866e38ae7fd10c216df1232e9"
 
+  bottle do
+    cellar :any
+    sha1 "865d487cdd7f447eee5cc102b2d5f79efdc1fb9e" => :yosemite
+    sha1 "c088cb0e9a0ddc16c113fc8ff41446376d7f755c" => :mavericks
+    sha1 "8cc2f12a1c395d8e1693f5ed6b37b94307968541" => :mountain_lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "wordnet"
 
