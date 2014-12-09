da046b8b1ab83112f212265f8ce2a8cd3cd140df
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-09 20:49:38 +0000

    fakeroot 1.20.2
    Version bump, and patches.
    
    Closes #33400.
    
    Closes #34836.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/fakeroot.rb
+++ Library/Formula/fakeroot.rb
@@ -1,29 +1,27 @@
-require "formula"
-
 class Fakeroot < Formula
   homepage "https://tracker.debian.org/pkg/fakeroot"
+  url "https://mirrors.kernel.org/debian/pool/main/f/fakeroot/fakeroot_1.20.2.orig.tar.bz2"
+  mirror "http://ftp.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.20.2.orig.tar.bz2"
+  sha1 "367040df07043edb630942b21939e493f3fad888"
 
-  stable do
-    url "https://mirrors.kernel.org/debian/pool/main/f/fakeroot/fakeroot_1.18.4.orig.tar.bz2"
-    mirror "http://ftp.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.18.4.orig.tar.bz2"
-    sha1 "60cdd12ea3a72f3676c0f3930ab908ff1f13b996"
-
-    # Monitor this with each release
-    # https://github.com/Homebrew/homebrew/issues/33400#issuecomment-59827330
-    depends_on MaximumMacOSRequirement => :mavericks
+  # Compile is broken. https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=766649
+  # Patches submitted upstream on 24/10/2014, but no reply from maintainer thus far.
+  patch do
+    url "https://bugs.debian.org/cgi-bin/bugreport.cgi?msg=5;filename=0001-Implement-openat-2-wrapper-which-handles-optional-ar.patch;att=1;bug=766649"
+    sha1 "9207619d6d8a55f25d50ba911bfa72f486911d81"
   end
 
-  head "https://anonscm.debian.org/git/users/clint/fakeroot.git"
-
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  patch do
+    url "https://bugs.debian.org/cgi-bin/bugreport.cgi?msg=5;filename=0002-OS-X-10.10-introduced-id_t-int-in-gs-etpriority.patch;att=2;bug=766649"
+    sha1 "a30907ffdcfe159c2ac6b3f829bd5b9a67188940"
+  end
 
   def install
-    system "./bootstrap"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-static",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
+    system "make"
     system "make", "install"
   end
 
