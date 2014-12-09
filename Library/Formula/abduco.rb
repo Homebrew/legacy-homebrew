b2fb6b5bc8963765bb1cd5f41a4b4d9d00736a35
Colin King <richard.colin.king@gmail.com>
2014-12-08 18:15:09 -0800

    Readded bottle block.

--- Library/Formula/abduco.rb
+++ Library/Formula/abduco.rb
@@ -6,6 +6,13 @@ class Abduco < Formula
   sha1 "e3a706b782fcb18fd70076ff3550bfdb4829b2ec"
   head "git://repo.or.cz/abduco.git"
 
+  bottle do
+    cellar :any
+    sha1 "dc3f05e32b87e67d3b9da26e199fb92de369794a" => :mavericks
+    sha1 "1fe57b58b3cdac26a667f19ad25bf0c06dcbf1f0" => :mountain_lion
+    sha1 "d7c559b7d1a696d8ca34934276f715461ee0ed33" => :lion
+  end
+
   def install
     inreplace "config.mk", 'CFLAGS += -std=c99 -Os ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700', 'CFLAGS += -std=c99 -Os ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG'
     system "make", "PREFIX=#{prefix}", "install"
