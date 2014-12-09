1bc666a2a3f8b17138b2154f50ac195f01eb8245
Ruben Fonseca <fonseka@gmail.com>
2014-12-09 19:44:33 +0000

    minuit2 5.34.14
    Closes #34834.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/minuit2.rb
+++ Library/Formula/minuit2.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Minuit2 < Formula
   homepage 'http://lcgapp.cern.ch/project/cls/work-packages/mathlibs/minuit/index.html'
-  url 'http://www.cern.ch/mathlibs/sw/5_28_00/Minuit2/Minuit2-5.28.00.tar.gz'
-  sha1 'edb79806c99501cd2cef336608135ec5def29041'
+  url 'http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz'
+  sha1 '6dc1d604f6b3adea1af72f9180d627a64ba3bc8a'
 
   def install
     system "./configure", "--disable-dependency-tracking",
