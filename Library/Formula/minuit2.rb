0eea66ab67d51fc0f1156e7d7c967b72feaf9278
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-09 20:13:18 +0000

    minuit2: add 5.34.14 bottle.

--- Library/Formula/minuit2.rb
+++ Library/Formula/minuit2.rb
@@ -5,6 +5,13 @@ class Minuit2 < Formula
   url 'http://www.cern.ch/mathlibs/sw/5_34_14/Minuit2/Minuit2-5.34.14.tar.gz'
   sha1 '6dc1d604f6b3adea1af72f9180d627a64ba3bc8a'
 
+  bottle do
+    cellar :any
+    sha1 "47323eddac5d8a29c33c1fc8be03e98559b21283" => :yosemite
+    sha1 "c47d5643105a8fb32c5a968a1777d6acf3fd3441" => :mavericks
+    sha1 "13995bc30ba826583b695c55cccc7b9d5c31a7ef" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--with-pic",
