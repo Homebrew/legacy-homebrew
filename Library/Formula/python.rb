330fca5ae6d48c82db6810ae13ba9450d56109ae
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-10 23:49:26 +0000

    python: update 2.7.9 bottle.

--- Library/Formula/python.rb
+++ Library/Formula/python.rb
@@ -7,10 +7,9 @@ class Python < Formula
   sha1 "7a191bcccb598ccbf2fa6a0edce24a97df3fc0ad"
 
   bottle do
-    revision 3
-    sha1 "9f8d1d3bf62e78c0918822517a041a845b6703d8" => :yosemite
-    sha1 "307042fc8dd7f736b450ee5fc631197290483cf7" => :mavericks
-    sha1 "f3708ea1d1f736527c428f0026aa42499c489fe2" => :mountain_lion
+    sha1 "9b476ce6b95d152635dfe96ca6e43266841ba745" => :yosemite
+    sha1 "e1436febf6af07689d6d7c5fa79071494604632b" => :mavericks
+    sha1 "184f8820f6eb31b470df564da09e14a0fce38970" => :mountain_lion
   end
 
   # Please don't add a wide/ucs4 option as it won't be accepted.
