f6c62f5418e81892e0cb90065336ac79851d5b9e
Alessio Bogon <youtux@gmail.com>
2014-12-10 14:47:12 +0100

    watch 3.3.10: new version and upstream switch
    Updated `watch` to the latest version from the procps-ng project, a Debian, Fedora and openSUSE fork of procps. It's more up-to-date than the procps project.
    In this version it also supports colored output.

--- Library/Formula/watch.rb
+++ Library/Formula/watch.rb
@@ -1,16 +1,20 @@
 require 'formula'
 
 class Watch < Formula
-  homepage 'http://procps.sourceforge.net/'
-  url 'http://procps.sourceforge.net/procps-3.2.8.tar.gz'
-  version '0.2.0' # watch command itself is version 0.2.0
-  sha1 'a0c86790569dec26b5d9037e8868ca907acc9829'
+  homepage 'http://sourceforge.net/projects/procps-ng/'
+  url 'http://download.sourceforge.net/project/procps-ng/Production/procps-ng-3.3.10.tar.xz'
+  sha1 '484db198d6a18a42b4011d5ecb2cb784a81b0e4f'
 
   conflicts_with 'visionmedia-watch'
 
   def install
-    system "make", "watch", "PKG_LDFLAGS="
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "watch", "AM_LDFLAGS="
     bin.install "watch"
     man1.install "watch.1"
+  end
+
+  test do
+    system "#{bin}/watch", "--version"
   end
 end
