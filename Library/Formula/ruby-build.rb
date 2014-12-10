e1b061afd9decfe88ce74023db7fb902dd174bec
Erik Michaels-Ober <sferik@gmail.com>
2014-12-10 15:54:53 +0100

    ruby-build 20141210
    Closes #34854.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,8 +3,8 @@ require "formula"
 class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  url "https://github.com/sstephenson/ruby-build/archive/v20141208.tar.gz"
-  sha1 "1f622ef50528ee2554b5b3183991eb55470fd701"
+  url "https://github.com/sstephenson/ruby-build/archive/v20141210.tar.gz"
+  sha1 "3a06376e12c9b6858512ae4c8656cb0d68ae92d4"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
