7929488dfde9460732ee6b481cd69820d63b628f
Alessio Bogon <youtux@gmail.com>
2014-12-10 14:47:12 +0100

    watch 3.3.10 (from procps-ng)
    Updated `watch` to the latest version from the procps-ng project, a
    Debian, Fedora and openSUSE fork of procps. It's more up-to-date than
    the procps project. In this version it also supports colored output.

--- Library/Formula/watch.rb
+++ Library/Formula/watch.rb
@@ -1,16 +1,25 @@
 require 'formula'
 
 class Watch < Formula
-  homepage 'http://procps.sourceforge.net/'
-  url 'http://procps.sourceforge.net/procps-3.2.8.tar.gz'
-  version '0.2.0' # watch command itself is version 0.2.0
-  sha1 'a0c86790569dec26b5d9037e8868ca907acc9829'
+  homepage 'http://sourceforge.net/projects/procps-ng/'
+  url 'http://download.sourceforge.net/project/procps-ng/Production/procps-ng-3.3.10.tar.xz'
+  sha1 '484db198d6a18a42b4011d5ecb2cb784a81b0e4f'
 
   conflicts_with 'visionmedia-watch'
 
   def install
-    system "make", "watch", "PKG_LDFLAGS="
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+
+    # AM_LDFLAGS contains a non-existing library './proc/libprocps.la' that
+    # breaks the linking process. Upstream developers have been informed (see 
+    # https://github.com/Homebrew/homebrew/pull/34852/files#r21796727).
+    system "make", "watch", "AM_LDFLAGS="
     bin.install "watch"
     man1.install "watch.1"
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system "#{bin}/watch", "--errexit", "--chgexit", "--interval", "1", "date"
   end
 end
