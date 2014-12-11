5b865cee6df623f594f6a8036bab4306fe1b5cac
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-11 13:15:37 +0100

    abcm2ps 7.8.11
    Closes #34882.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/abcm2ps.rb
+++ Library/Formula/abcm2ps.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Abcm2ps < Formula
   homepage "http://moinejf.free.fr"
-  url "http://moinejf.free.fr/abcm2ps-7.8.7.tar.gz"
-  sha1 "2e2380016069461bc45950f472e6b1ea22a94c5b"
+  url "http://moinejf.free.fr/abcm2ps-7.8.11.tar.gz"
+  sha1 "93e016cf933d88d817be76bd3eb4163851f3b132"
 
   bottle do
     sha1 "553e4b09045b5693a06faf5e202336ec5a87a9c2" => :mavericks
@@ -12,8 +12,8 @@ class Abcm2ps < Formula
   end
 
   devel do
-    url "http://moinejf.free.fr/abcm2ps-8.1.5.tar.gz"
-    sha1 "a268290d12fec84429c91e86f4b734e3e155c40c"
+    url "http://moinejf.free.fr/abcm2ps-8.3.4.tar.gz"
+    sha1 "e3a92e89eb55d36e582e3529846f17c60dfb788b"
   end
 
   depends_on "pkg-config" => :build
@@ -25,6 +25,27 @@ class Abcm2ps < Formula
   end
 
   test do
-    system "#{bin}/abcm2ps"
+    (testpath/"voices.abc").write <<-EOF.undent
+      X:7
+      T:Qui Tolis (Trio)
+      C:André Raison
+      M:3/4
+      L:1/4
+      Q:1/4=92
+      %%staves {(Pos1 Pos2) Trompette}
+      K:F
+      %
+      V:Pos1
+      %%MIDI program 78
+      "Positif"x3 |x3|c'>ba|Pga/g/f|:g2a |ba2 |g2c- |c2P=B  |c>de  |fga |
+      V:Pos2
+      %%MIDI program 78
+              Mf>ed|cd/c/B|PA2d |ef/e/d |:e2f |ef2 |c>BA |GA/G/F |E>FG |ABc- |
+      V:Trompette
+      %%MIDI program 56
+      "Trompette"z3|z3 |z3 |z3 |:Mc>BA|PGA/G/F|PE>EF|PEF/E/D|C>CPB,|A,G,F,-|
+    EOF
+
+    system "#{bin}/abcm2ps", (testpath/"voices")
   end
 end
