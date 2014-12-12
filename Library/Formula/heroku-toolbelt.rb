acb04f069168e4ecc0cef4f951900513904270c4
dickeyxxx <jeff@dickeyxxx.com>
2014-12-12 12:39:56 -0800

    updated heroku to 3.20.0

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class HerokuToolbelt < Formula
   homepage 'https://toolbelt.heroku.com/other'
-  url 'http://assets.heroku.com.s3.amazonaws.com/heroku-client/heroku-client-3.15.0.tgz'
-  sha1 '262f25eeb1d31ea8940acb49fd81b77bd0bdd6d6'
+  url 'http://assets.heroku.com.s3.amazonaws.com/heroku-client/heroku-client-3.20.0.tgz'
+  sha1 'e3edb525fc25d4fbafc46768cc06353a5519db88'
 
   def install
     libexec.install Dir["*"]
