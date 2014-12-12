62c5b559bd1000b1d70f1b7870926095271c9282
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-12 10:27:23 +0000

    opencc: add 1.0.1 bottle.

--- Library/Formula/opencc.rb
+++ Library/Formula/opencc.rb
@@ -5,6 +5,12 @@ class Opencc < Formula
   url 'http://dl.bintray.com/byvoid/opencc/opencc-1.0.1.tar.gz'
   sha1 'e5cdedef5d9d96f5046a0f44f3004611330ded4a'
 
+  bottle do
+    sha1 "8ccf8db44595debc423220a40f0fe6d715bd60ef" => :yosemite
+    sha1 "cb24320dd8415a6b06df2e82ae76e05f2e9396d7" => :mavericks
+    sha1 "98a43a46a22da47a206e0a04ab98e87990c72bbe" => :mountain_lion
+  end
+
   depends_on 'cmake' => :build
 
   needs :cxx11
