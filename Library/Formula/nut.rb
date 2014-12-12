0e551d714dce001f12255ef8a762c947f96eb969
Jack Nagel <jacknagel@gmail.com>
2014-12-11 22:46:00 -0500

    nut: bump revision for net-snmp changes

--- Library/Formula/nut.rb
+++ Library/Formula/nut.rb
@@ -4,11 +4,9 @@ class Nut < Formula
   homepage "http://www.networkupstools.org"
   url "http://www.networkupstools.org/source/2.7/nut-2.7.2.tar.gz"
   sha256 "4d5365359b059d96dfcb77458f361a114d26c84f1297ffcd0c6c166f7200376d"
+  revision 1
 
   bottle do
-    sha1 "4a37cf6350e56dfbbb80a7bfb03f3f6aa804e36a" => :mavericks
-    sha1 "467ed9df7b3aeae35060d5fecfe229caeba62180" => :mountain_lion
-    sha1 "4ab46368434117fbb380d90463295fbb8f254ef9" => :lion
   end
 
   head do
@@ -42,7 +40,7 @@ class Nut < Formula
   def install
     args = ["--disable-dependency-tracking",
             "--prefix=#{prefix}",
-            "--localstatedir=#{HOMEBREW_PREFIX}/var",
+            "--localstatedir=#{var}",
             "--without-doc",
             "--without-avahi",
             "--with-macosx_ups",
@@ -53,7 +51,7 @@ class Nut < Formula
     args << (build.with?("serial") ? "--with-serial" : "--without-serial")
     args << (build.with?("libusb") ? "--with-usb" : "--without-usb")
     args << (build.with?("dev") ? "--with-dev" : "--without-dev")
-    args << (build.with?("snmp") ? "--with-snmp" : "--without-snmp")
+    args << (build.with?("net-snmp") ? "--with-snmp" : "--without-snmp")
     args << (build.with?("neon") ? "--with-neon" : "--without-neon")
     args << (build.with?("powerman") ? "--with-powerman" : "--without-powerman")
     args << (build.with?("ipmi") ? "--with-ipmi" : "--without-ipmi")
