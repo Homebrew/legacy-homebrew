8029065202c6e4d3293bc4fc078d2e52e160ad0f
Alfred E. Heggestad <aeh@db.org>
2014-12-13 07:48:04 +0100

    libre 0.4.11
    Closes #34946.

--- Library/Formula/libre.rb
+++ Library/Formula/libre.rb
@@ -2,9 +2,8 @@ require "formula"
 
 class Libre < Formula
   homepage "http://www.creytiv.com"
-  url "http://www.creytiv.com/pub/re-0.4.8.tar.gz"
-  sha1 "92bdf13a5e5ba445e1457d3fd6a20bb779c4b4b0"
-  revision 1
+  url "http://www.creytiv.com/pub/re-0.4.11.tar.gz"
+  sha1 "6e04f8e30eaa273134c47433b41bcffadfca194c"
 
   bottle do
     cellar :any
