2950c635fd47a4bc50317af997fa77932309a6b3
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-13 14:18:55 +0000

    glbinding: add 1.0.2 bottle.

--- Library/Formula/glbinding.rb
+++ Library/Formula/glbinding.rb
@@ -5,6 +5,13 @@ class Glbinding < Formula
   url "https://github.com/hpicgs/glbinding/archive/v1.0.2.tar.gz"
   sha1 "19d6d143bd4106582af4c197e882222e86323820"
 
+  bottle do
+    cellar :any
+    sha1 "89417315c4b7b4da7c817700fecfd91692a39e9f" => :yosemite
+    sha1 "6bfe54d1a5f99e8af3059d6b02353c2d53d9ea95" => :mavericks
+    sha1 "6f9452c240bd6bd545af65c623e5068385acb206" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   needs :cxx11
 
