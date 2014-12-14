fa39c341b817096ac8e53037c362668b430fbf7b
Andreas Pfohl <andreas@pf0hl.de>
2014-12-14 10:19:44 +0100

    kore 1.2.1
    Release of Kore 1.2.1
    
    Closes #34954.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/kore.rb
+++ Library/Formula/kore.rb
@@ -2,16 +2,10 @@ require "formula"
 
 class Kore < Formula
   homepage "https://kore.io/"
-  url "https://kore.io/release/kore-1.2-stable.tgz"
-  sha1 "cb7f9eb7151d612d9acffc5eed1d79c296baeeba"
+  url "https://kore.io/release/kore-1.2.1-release.tgz"
+  sha1 "7af8d3d651657e54cee4d90c9be33e9bde8a0727"
 
   head "https://github.com/jorisvink/kore.git"
-
-  bottle do
-    sha1 "d0423698323272274a2642e19176696fec6003cd" => :yosemite
-    sha1 "cb230b9303f4c8201ad78a4a28da5fb5083dbfdb" => :mavericks
-    sha1 "6742b88959e925e0416f734d55913923a836d1da" => :mountain_lion
-  end
 
   depends_on "openssl"
   depends_on "postgresql" => :recommended
@@ -19,16 +13,10 @@ class Kore < Formula
   def install
     args = []
 
-    # The following inreplaces are a workaround. An upstream pull request has been
-    # created to solve this problem. (https://github.com/jorisvink/kore/pull/25)
-    inreplace "src/cli.c", / = "-I\/usr\/local\/include";/, " = \"-I#{include}\";"
-    inreplace "Makefile", /\/usr\/local/, prefix
-
     args << "PGSQL=1" if build.with? "postgresql"
 
-    system "make", "TASKS=1", *args
-    bin.mkdir
-    system "make", "install"
+    system "make", "PREFIX=#{prefix}", "TASKS=1", *args
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
