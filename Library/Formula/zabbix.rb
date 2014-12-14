9736f549be7ca9301acf92469a0e2edbf739600d
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-14 01:00:48 +0000

    zabbix: add 2.4.2 bottle.

--- Library/Formula/zabbix.rb
+++ Library/Formula/zabbix.rb
@@ -5,6 +5,12 @@ class Zabbix < Formula
   url "https://downloads.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/2.4.2/zabbix-2.4.2.tar.gz"
   sha1 "627ed5196b81a1f4e36690695dfbcaa1803890e3"
 
+  bottle do
+    sha1 "5e335b2e0fe5ccae9dbc375ca15a6c60b1560f1d" => :yosemite
+    sha1 "ff74e25cf747013e6f8f98685f51387758c85fd1" => :mavericks
+    sha1 "1bc4d521656fb5439330143352dcd7944dce88fb" => :mountain_lion
+  end
+
   option "with-mysql", "Use Zabbix Server with MySQL library instead PostgreSQL."
   option "agent-only", "Install only the Zabbix Agent without Server and Proxy."
 
