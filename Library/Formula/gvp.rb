8e6f570fb155995279deed5605dd1cc7c35c6343
Christopher Chow <chris@chowie.net>
2014-12-14 13:52:10 +1100

    gvp 0.2.0

--- Library/Formula/gvp.rb
+++ Library/Formula/gvp.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Gvp < Formula
   homepage "https://github.com/pote/gvp"
-  url "https://github.com/pote/gvp/archive/v0.1.0.tar.gz"
-  sha1 "c48bae47f8589e9bfd8addc94a9073be4c004305"
+  url "https://github.com/pote/gvp/archive/v0.2.0.tar.gz"
+  sha1 "d05a2f04ba06127c95fb1d1fb10a2643d6d27ac6"
 
   bottle do
     cellar :any
