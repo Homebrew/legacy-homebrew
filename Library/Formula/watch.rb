0c1630c52941610d19803642d85fd6b0f3049e6e
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-14 15:03:25 +0000

    watch: add 3.3.10 bottle.

--- Library/Formula/watch.rb
+++ Library/Formula/watch.rb
@@ -5,6 +5,13 @@ class Watch < Formula
   url 'http://download.sourceforge.net/project/procps-ng/Production/procps-ng-3.3.10.tar.xz'
   sha1 '484db198d6a18a42b4011d5ecb2cb784a81b0e4f'
 
+  bottle do
+    cellar :any
+    sha1 "02dd29b9894a881d150ae369a0bd7e6c38517158" => :yosemite
+    sha1 "4c879fbcd46a9867ec7a322ddbb466cb0a376825" => :mavericks
+    sha1 "a7c559378bc74cd30d00f962e63d6ee5c705aea1" => :mountain_lion
+  end
+
   conflicts_with 'visionmedia-watch'
 
   def install
