07301d1c9166c59082ecf9498b753b50c21767a9
Devon Stewart <blast@hardchee.se>
2014-12-13 19:38:15 -0800

    Adding vAVRdiasm v3.1

---
+++ Library/Formula/vavrdiasm.rb
@@ -0,0 +1,43 @@
+require 'formula'
+
+class Vavrdiasm < Formula
+  homepage 'https://github.com/vsergeev/vAVRdisasm'
+  url "https://github.com/vsergeev/vavrdisasm.git", :revision => "v3.1", :using => :git
+  version "3.1"
+
+  # Patch:
+  # - BSD `install(1)' does not have a GNU-compatible `-D' (create intermediate
+  #   directories) flag. Switch to using `mkdir -p'.
+  # - Make `PREFIX' overridable
+  #   https://github.com/vsergeev/vavrdisasm/pull/2
+  patch :DATA
+
+  def install
+    ENV['PREFIX'] = prefix
+    system "make"
+    system "make", "install"
+  end
+end
+
+__END__
+diff --git a/Makefile b/Makefile
+index 3b61942..f1c94fc 100644
+--- a/Makefile
++++ b/Makefile
+@@ -1,5 +1,5 @@
+ PROGNAME = vavrdisasm
+-PREFIX = /usr
++PREFIX ?= /usr
+ BINDIR = $(PREFIX)/bin
+ 
+ ################################################################################
+@@ -35,7 +35,8 @@ test: $(PROGNAME)
+ 	python2 crazy_test.py
+ 
+ install: $(PROGNAME)
+-	install -D -s -m 0755 $(PROGNAME) $(DESTDIR)$(BINDIR)/$(PROGNAME)
++	mkdir -p $(DESTDIR)$(BINDIR)
++	install -s -m 0755 $(PROGNAME) $(DESTDIR)$(BINDIR)/$(PROGNAME)
+ 
+ uninstall:
+ 	rm -f $(DESTDIR)$(BINDIR)/$(PROGNAME)
