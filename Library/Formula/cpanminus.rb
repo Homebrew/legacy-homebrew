151f9e9f32206adf349526f50e3bc7c78903a730
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-16 00:14:40 +0100

    cpanminus 1.7022
    Closes #35019.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/cpanminus.rb
+++ Library/Formula/cpanminus.rb
@@ -2,12 +2,16 @@ require "formula"
 
 class Cpanminus < Formula
   homepage "https://github.com/miyagawa/cpanminus"
-  url "https://github.com/miyagawa/cpanminus/archive/1.7014.tar.gz"
-  sha1 "b82e73a501b9b64d98e2dd4e7ed6b1db801619b8"
+  url "https://github.com/miyagawa/cpanminus/archive/1.7022.tar.gz"
+  sha1 "ce2e5321d676022b9e476bb8e5683ad60e13915c"
 
   head "https://github.com/miyagawa/cpanminus.git"
 
   def install
     bin.install "cpanm"
+  end
+
+  test do
+    system "#{bin}/cpanm", "-V"
   end
 end
