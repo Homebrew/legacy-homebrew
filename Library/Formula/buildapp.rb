384eb1d4fc92642eb62cc044ed39232985a6f51a
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-15 20:16:58 +0000

    buildapp: add 1.5.3 bottle.

--- Library/Formula/buildapp.rb
+++ Library/Formula/buildapp.rb
@@ -5,6 +5,12 @@ class Buildapp < Formula
   url 'https://github.com/xach/buildapp/archive/release-1.5.3.tar.gz'
   sha1 'a0601d144ee72719bd92298ca90e155234de26d0'
 
+  bottle do
+    sha1 "b5c93a13dea8b09844376e285fbc531ae24ab608" => :yosemite
+    sha1 "023440a32a3f053d4eba20534a73d4cc6ed9a8f7" => :mavericks
+    sha1 "eeb8f81c772672fbffdeca7e659cd8f57039dc1c" => :mountain_lion
+  end
+
   depends_on 'sbcl'
 
   def install
