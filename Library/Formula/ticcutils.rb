7209a5e4bd0bb4bc61e0f79162946673812dd562
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-12-15 13:28:51 +0100

    ticcutils (new formula)

--- Library/Formula/ticcutils.rb
+++ Library/Formula/ticcutils.rb
@@ -5,20 +5,15 @@ class Ticcutils < Formula
   url "http://software.ticc.uvt.nl/ticcutils-0.7.tar.gz"
   sha1 "f58ec1a6b64a0eebc1a8a74ef625cf6398a5fafc"
 
-  bottle do
-    cellar :any
-    sha1 "5e8c69c62810db58504dd2fd60c4d4d39929d87f" => :yosemite
-    sha1 "307d320bb730deeaf83d452619e043d0ac0bde88" => :mavericks
-    sha1 "336da8cc03521e25d220b56f722f6e7a4a8ab303" => :mountain_lion
-  end
 
   depends_on "pkg-config" => :build
 
   def install
+    ENV['CFLAGS']='-L/usr/local/opt/icu4c/lib'
+    ENV['LDFLAGS']='-I/usr/local/opt/icu4c/include'
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
 end
-
