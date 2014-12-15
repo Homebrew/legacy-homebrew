d3ad9af8d2e6ec3a54785b4233c330f578f4203b
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-15 21:01:43 +0100

    buildapp 1.5.3
    Closes #35010.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/buildapp.rb
+++ Library/Formula/buildapp.rb
@@ -2,13 +2,21 @@ require 'formula'
 
 class Buildapp < Formula
   homepage 'http://www.xach.com/lisp/buildapp/'
-  url 'https://github.com/xach/buildapp/archive/release-1.5.2.tar.gz'
-  sha1 'be9a8fbcbd52383041c96f3b761f0d8d8ed66de4'
+  url 'https://github.com/xach/buildapp/archive/release-1.5.3.tar.gz'
+  sha1 'a0601d144ee72719bd92298ca90e155234de26d0'
 
   depends_on 'sbcl'
 
   def install
     bin.mkpath
     system "make", "install", "DESTDIR=#{prefix}"
+  end
+
+  test do
+    code = "(defun f (a) (declare (ignore a)) (write-line \"Hello, homebrew\"))"
+    system "#{bin}/buildapp", "--eval", code,
+                              "--entry", "f",
+                              "--output", "t"
+    assert_equal `./t`, "Hello, homebrew\n"
   end
 end
