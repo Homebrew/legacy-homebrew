0f22afcd2548dc761ad9d68318b469ba81f78c51
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-15 23:36:45 +0000

    cpanminus: add 1.7022 bottle.

--- Library/Formula/cpanminus.rb
+++ Library/Formula/cpanminus.rb
@@ -7,6 +7,13 @@ class Cpanminus < Formula
 
   head "https://github.com/miyagawa/cpanminus.git"
 
+  bottle do
+    cellar :any
+    sha1 "52f75e63756ef4c858013fa3542ce138e899f904" => :yosemite
+    sha1 "2457a4bcbee3e5ba9dd6fd5717477b6fffe5dde9" => :mavericks
+    sha1 "8ee611a85ab2f397f810a4f92f9d5af987a3789f" => :mountain_lion
+  end
+
   def install
     bin.install "cpanm"
   end
