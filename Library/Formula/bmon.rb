51776894a06cf90cc2a36a78aa7b48bde3ef4f83
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-15 20:24:17 +0100

    bmon 3.6

--- Library/Formula/bmon.rb
+++ Library/Formula/bmon.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Bmon < Formula
   homepage 'https://github.com/tgraf/bmon'
-  url "https://github.com/tgraf/bmon/releases/download/v3.5/bmon-3.5.tar.gz"
-  sha1 "352f774e20f679e89de0f72159475d999cc7bf2f"
+  url "https://github.com/tgraf/bmon/releases/download/v3.6/bmon-3.6.tar.gz"
+  sha1 "abbe4362061493a1120eed074debd875916dd49a"
 
   bottle do
     sha1 "403e402182813cc1f63a6b75c2a40db32e328626" => :mavericks
@@ -27,5 +27,9 @@ class Bmon < Formula
                           "--mandir=#{man}"
     system "make" # two steps to prevent blowing up
     system "make install"
+  end
+
+  test do
+    system "#{bin}/bmon", "-o", "ascii:quitafter=1"
   end
 end
