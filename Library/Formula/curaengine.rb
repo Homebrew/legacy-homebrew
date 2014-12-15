19b625dffd8bbb7e63d938f8a0c7e16a5d31f766
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-16 00:36:42 +0100

    curaengine 14.12.1
    Closes #35020.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/curaengine.rb
+++ Library/Formula/curaengine.rb
@@ -2,17 +2,29 @@ require "formula"
 
 class Curaengine < Formula
   homepage "https://github.com/Ultimaker/CuraEngine"
+  url "https://github.com/Ultimaker/CuraEngine/archive/14.12.1.tar.gz"
+  sha1 "4d50fcd32e402f37b658691a4338e73fb4aaf3ec"
   head "https://github.com/Ultimaker/CuraEngine.git"
-  url "https://github.com/Ultimaker/CuraEngine/archive/14.03.tar.gz"
-  sha1 "d782c90d6e66580cc7e4b43a013da399e4623259"
 
   def install
     ENV.deparallelize
     system "make"
-    if build.head?
-      bin.install "build/CuraEngine"
-    else
-      bin.install "CuraEngine"
-    end
+    bin.install "build/CuraEngine"
+  end
+
+  test do
+    (testpath/"t.stl").write <<-EOF.undent
+      solid t
+        facet normal 0 -1 0
+         outer loop
+          vertex 0.83404 0 0.694596
+          vertex 0.36904 0 1.5
+          vertex 1.78814e-006 0 0.75
+         endloop
+        endfacet
+      endsolid Star
+    EOF
+
+    system "#{bin}/CuraEngine", "#{testpath}/t.stl"
   end
 end
