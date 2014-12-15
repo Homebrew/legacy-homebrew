70708cd00a3031cf558912e0015b2ea53b7357f0
Tom Scott <tscott@telvue.com>
2014-12-15 00:02:07 -0500

    Add Homer
    Homer is a home directory manager that takes the same approach as
    Homebrew does for package management. It converts the entire home
    directory into a Git repo, and provides a simple command-line interface
    for managing shell extensions such as plugins, `bin/` scripts and
    aliases using this repository. It also provides a very light set of
    scripts that act as a framework for the user to build their own shell
    environment.
    
    Homer relies on ZSH and Antigen, hence why that formula has been added
    in this PR as well.

--- Library/Formula/antigen.rb
+++ Library/Formula/antigen.rb
@@ -1,9 +1,5 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Antigen < Formula
   homepage "http://antigen.sharats.me/"
 
@@ -26,12 +22,12 @@ class Antigen < Formula
 
     Add the following to your ~/.zshenv to start using Antigen:
 
-       source /usr/local/lib/antigen.zsh
+       source #{share}/antigen.zsh
 
   TXT
   end
 
   test do
-    system "source #{prefix}/lib/antigen.zsh"
+    system "source #{share}/antigen.zsh"
   end
 end
