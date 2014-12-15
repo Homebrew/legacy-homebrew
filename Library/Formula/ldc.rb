d154c7ebfd45a13e63f54211d46a9fddea13a1ff
John Colvin <john.loughran.colvin@gmail.com>
2014-12-15 10:24:25 +0000

    ldc 0.15.1
    Closes #34993.

--- Library/Formula/ldc.rb
+++ Library/Formula/ldc.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Ldc < Formula
   homepage "http://wiki.dlang.org/LDC"
-  url "https://github.com/ldc-developers/ldc/releases/download/v0.14.0/ldc-0.14.0-src.tar.gz"
-  sha1 "843c9fb374bb900560f9548d5c4646788f967103"
+  url "https://github.com/ldc-developers/ldc/releases/download/v0.15.1/ldc-0.15.1-src.tar.gz"
+  sha1 "06db0c07d73bf714e6fc7a4aca4c44123e4a585a"
 
   bottle do
     sha1 "824272b3e29e1a5cdf03669099690f5c10021396" => :mavericks
@@ -11,11 +11,14 @@ class Ldc < Formula
     sha1 "fc18d767e69eca7d9a7a5456a5524c7ce1cb3d29" => :lion
   end
 
+  needs :cxx11
+
   depends_on "cmake" => :build
   depends_on "llvm" => :build
   depends_on "libconfig"
 
   def install
+    ENV.cxx11
     mkdir "build"
     cd "build" do
       system "cmake", "..", *std_cmake_args
