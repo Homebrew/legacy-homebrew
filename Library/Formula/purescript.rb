ad982324a2f41aab53639f69ea2b598af4e199f2
Jan Stępień <jstepien@users.sourceforge.net>
2014-12-15 14:46:09 +0100

    purescript 0.6.2

--- Library/Formula/purescript.rb
+++ Library/Formula/purescript.rb
@@ -5,8 +5,8 @@ class Purescript < Formula
   include Language::Haskell::Cabal
 
   homepage "http://www.purescript.org/"
-  url "https://hackage.haskell.org/package/purescript-0.6.1.2/purescript-0.6.1.2.tar.gz"
-  sha1 "645d9d11295e065d5a75cadf9a5eb08f2e3b90cb"
+  url "https://hackage.haskell.org/package/purescript-0.6.2/purescript-0.6.2.tar.gz"
+  sha1 "62c5793d9af0e0fdc9120b5b47fc87b2b34e137f"
 
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
