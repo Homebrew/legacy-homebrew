4dfc9159cad2ffd3b20de14c4df8ee962f0274f4
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-16 00:07:57 +0100

    couchpotatoserver 2.6.1
    Closes #35018.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/couchpotatoserver.rb
+++ Library/Formula/couchpotatoserver.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Couchpotatoserver < Formula
   homepage "https://couchpota.to"
-  url "https://github.com/RuudBurger/CouchPotatoServer/archive/build/2.6.0.tar.gz"
-  sha1 "7d7952daba686c840b9723495bf9f5bb61c8c6a8"
+  url "https://github.com/RuudBurger/CouchPotatoServer/archive/build/2.6.1.tar.gz"
+  sha1 "a0fa3b7b187e416bd78357a2738b09e0539972b9"
 
   head "https://github.com/RuudBurger/CouchPotatoServer.git"
 
