f1bd730b360a0baf67d10dba4bb51d7592d1de4f
Izzat Mukhanov <izzatbek.mukhanov@inria.fr>
2014-12-15 17:54:41 +0100

    libpointing: remove Qt dependency
    Closes #34938.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libpointing.rb
+++ Library/Formula/libpointing.rb
@@ -1,31 +1,26 @@
 require "formula"
 
 class Libpointing < Formula
-  homepage "http://libpointing.org/libpointing/index.html"
-  url "http://libpointing.org/libpointing-0.92.tar.gz"
-  sha1 "f2206fe87c46d089b8e1db01382aed03080e3a64"
-
-  bottle do
-    cellar :any
-    sha1 "4eff97b179e5f76dd07e5d2bb17f121580fbdab8" => :yosemite
-    sha1 "34a320fcd4ed6a23994e369f28f202b6db9d236a" => :mavericks
-    sha1 "5e432b13bba40d68e602da4900bceb7656b23cfa" => :mountain_lion
-  end
-
-  depends_on "qt5"
+  homepage "http://pointing.org"
+  url "http://libpointing.org/homebrew/libpointing-0.92.tar.gz"
+  sha1 "a7f20c405e87a4b6fae369f977c0615a621ab143"
 
   def install
-    system "qmake", "PREFIX=#{prefix}"
     system "make"
-    system "make", "install"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
     (testpath/"test.cpp").write <<-EOS.undent
-      #include <pointing/input/PointingDevice.h>
+      #include <pointing/pointing.h>
+      #include <iostream>
       int main() {
-        pointing::PointingDevice *device = pointing::PointingDevice::create("any:");
-        delete device;
+        std::cout << LIBPOINTING_VER_STRING << " |" ;
+        std::list<std::string> schemes = pointing::TransferFunction::schemes() ;
+        for (std::list<std::string>::iterator i=schemes.begin(); i!=schemes.end(); ++i) {
+          std::cout << " " << (*i) ;
+        }
+        std::cout << std::endl ;
         return 0;
       }
     EOS
