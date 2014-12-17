3cb57163d97945757098bc0ab513da8dc6da3dd4
johnlinvc <johnlinvc@gmail.com>
2014-12-16 17:44:21 -0800

    storm 0.9.3
    Closes #35044.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/storm.rb
+++ Library/Formula/storm.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Storm < Formula
   homepage 'https://github.com/nathanmarz/storm/wiki'
-  url 'http://mirror.csclub.uwaterloo.ca/apache/incubator/storm/apache-storm-0.9.2-incubating/apache-storm-0.9.2-incubating.zip'
-  sha1 '92536843d76463974dd7bce4f8694fa6e462f0b6'
+  url 'http://mirror.olnevhost.net/pub/apache/storm/apache-storm-0.9.3/apache-storm-0.9.3.zip'
+  sha1 'efccdca0babbcf36416f1229ee961639b234e53c'
 
   def install
     libexec.install Dir['*']
