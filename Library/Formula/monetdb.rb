8fadebba7a9748e8241ea68d4157da47620f7b31
Dimitar Nedev <dnedev@users.noreply.github.com>
2014-12-17 21:23:37 +0100

    monetdb 11.19.7
    Also support some optional packages.
    
    Closes #35067.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/monetdb.rb
+++ Library/Formula/monetdb.rb
@@ -1,9 +1,24 @@
 require "formula"
 
+class RRequirement < Requirement
+  fatal true
+
+  satisfy { which('r') }
+
+  def message; <<-EOS.undent
+    R not found. The R integration module requires R.
+    Do one of the following:
+    - install R
+    -- run brew install homebrew/science/r or brew install Caskroom/cask/r
+    - remove the --with-r option
+    EOS
+  end
+end
+
 class Monetdb < Formula
   homepage "https://www.monetdb.org/"
-  url "https://dev.monetdb.org/downloads/sources/Oct2014/MonetDB-11.19.3.zip"
-  sha1 "f8290358c1773afc2679d9cbfea456c691f50527"
+  url "https://dev.monetdb.org/downloads/sources/Oct2014-SP1/MonetDB-11.19.7.zip"
+  sha1 "af542dc85a8474eb4bcf32565eccae3ea5d22768"
 
   bottle do
     sha1 "e5802401b81d035fe81a9a708dd647128a0a4302" => :yosemite
@@ -11,17 +26,35 @@ class Monetdb < Formula
     sha1 "484c94edf77b0acb72aa0e0cb7a8017c149be95c" => :mountain_lion
   end
 
-  head "http://dev.monetdb.org/hg/MonetDB", :using => :hg
+  head do
+    url "http://dev.monetdb.org/hg/MonetDB", :using => :hg
 
-  option "with-java"
+    depends_on "libtool" => :build
+    depends_on "gettext" => :build
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
+
+  option "with-java", 'Build the JDBC dirver'
+  option "with-r", 'Build the R integration module'
+
+  depends_on RRequirement => :optional
 
   depends_on "pkg-config" => :build
   depends_on :ant => :build
+  depends_on "libatomic_ops" => [:build, :recommended]
   depends_on "pcre"
   depends_on "readline" # Compilation fails with libedit.
   depends_on "openssl"
 
+  depends_on "unixodbc" => :optional # Build the ODBC driver
+  depends_on "geos" => :optional # Build the GEOM module
+  depends_on "gsl" => :optional
+  depends_on "cfitsio" => :optional
+  depends_on "homebrew/php/libsphinxclient" => :optional
+
   def install
+    ENV["M4DIRS"] = "#{Formula["gettext"].opt_share}/aclocal" if build.head?
     system "./bootstrap" if build.head?
 
     args = ["--prefix=#{prefix}",
@@ -29,10 +62,11 @@ class Monetdb < Formula
             "--enable-assert=no",
             "--enable-optimize=yes",
             "--enable-testing=no",
-            "--disable-jaql",
-            "--without-rubygem"]
+            "--with-readline=#{Formula["readline"].opt_prefix}", # Use the correct readline
+            "--without-rubygem"] # Installing the RubyGems requires root permissions
 
     args << "--with-java=no" if build.without? "java"
+    args << "--disable-rintegration" if build.without? "r"
 
     system "./configure", *args
     system "make install"
