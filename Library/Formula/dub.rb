f74fea9bc8b300a8e6f3c58b47bd1a8335937eb7
Jack Nagel <jacknagel@gmail.com>
2014-12-16 20:47:39 -0500

    dub: remove custom download strategy

--- Library/Formula/dub.rb
+++ Library/Formula/dub.rb
@@ -1,20 +1,11 @@
 require "formula"
 
-# dub generates version information from git, when building.
-# To not break this, we provide a custom download strategy.
-class DubHeadDownloadStrategy < GitDownloadStrategy
-  def stage
-    cached_location.cd { reset }
-    quiet_safe_system "git", "clone", cached_location, "."
-  end
-end
-
 class Dub < Formula
   homepage "http://code.dlang.org/about"
   url  "https://github.com/D-Programming-Language/dub/archive/v0.9.22.tar.gz"
   sha1 "9a7b7c838f1241de209473c09a194d355279457b"
 
-  head "https://github.com/D-Programming-Language/dub.git", :using => DubHeadDownloadStrategy, :shallow => false
+  head "https://github.com/D-Programming-Language/dub.git", :shallow => false
 
   devel do
     url "https://github.com/D-Programming-Language/dub/archive/v0.9.22-rc.1.tar.gz"
@@ -26,6 +17,7 @@ class Dub < Formula
   depends_on "dmd"  => :build
 
   def install
+    ENV["GIT_DIR"] = cached_download/".git"
     system "./build.sh"
     bin.install "bin/dub"
   end
