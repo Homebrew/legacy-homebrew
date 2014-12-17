307fb002cd79508e00c768d26d4e244c63088fb6
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-12-17 14:58:04 -0800

    Update matcher.rb

--- Library/Formula/matcher.rb
+++ Library/Formula/matcher.rb
@@ -1,16 +1,14 @@
-require 'formula'
-
 class Matcher < Formula
-  homepage 'https://github.com/burke/matcher'
-  url 'https://github.com/burke/matcher/archive/1.0.0.tar.gz'
-  sha1 '47c4c47de5a94f36366c09d10508a338610b183d'
+  homepage "https://github.com/burke/matcher"
+  url "https://github.com/burke/matcher/archive/1.0.0.tar.gz"
+  sha1 "47c4c47de5a94f36366c09d10508a338610b183d"
 
   def install
-    system 'make'
-    system 'make', 'install'
+    system "make"
+    system "make", "install"
   end
 
   test do
-    system 'matcher -h'
+    system "matcher"
   end
 end
