ffc40130ed28103200d6f544a15252a2710eb912
Misty De Meo <mistydemeo@gmail.com>
2014-12-18 12:11:26 -0800

    libmagic: adjust patch comment, use upstream commit
    Closes #35102.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/libmagic.rb
+++ Library/Formula/libmagic.rb
@@ -16,8 +16,12 @@ class Libmagic < Formula
 
   depends_on :python => :optional
 
+  # Patch applied upstream, should be in 5.22
   # See http://bugs.gw.com/view.php?id=230
-  patch :DATA if MacOS.version < :lion
+  patch do
+    url "https://github.com/file/file/commit/f79e16aebe701fdb8e821c3c1f3504568d0c10f5.diff"
+    sha1 "7dcbf309bf013c11a6c5367bab8834050d762bd5"
+  end
 
   def install
     ENV.universal_binary if build.universal?
@@ -39,18 +43,3 @@ class Libmagic < Formula
     rm man1/"file.1"
   end
 end
-
-__END__
-diff --git a/src/getline.c b/src/getline.c
-index e3c41c4..74c314e 100644
---- a/src/getline.c
-+++ b/src/getline.c
-@@ -76,7 +76,7 @@ getdelim(char **buf, size_t *bufsiz, int delimiter, FILE *fp)
-  }
- }
-
--ssize_t
-+public ssize_t
- getline(char **buf, size_t *bufsiz, FILE *fp)
- {
-  return getdelim(buf, bufsiz, '\n', fp);
