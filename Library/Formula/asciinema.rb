30d6d76442ef8291a31fa48ea65378767eefaa2f
Marcin Kulik <m@ku1ik.com>
2014-12-18 14:10:25 +0100

    asciinema 0.9.9

---
+++ Library/Formula/asciinema.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Asciinema < Formula
+  homepage 'https://asciinema.org/'
+  url 'https://github.com/asciinema/asciinema-cli/archive/v0.9.9.tar.gz'
+  sha1 '155c19366ffb3347e97026e9ab8006c16d2a52c6'
+
+  depends_on "go" => :build
+  depends_on "hg" => :build
+
+  def install
+    ENV["GOBIN"] = bin
+    ENV["GOPATH"] = buildpath
+
+    system "go", "get", "-d", "-v", "./..."
+    system "go", "build", "-o", "asciinema"
+    bin.install "asciinema"
+  end
+end
