65b61a712ed31fbc79cc55bfcbed561fb380b372
Marcin Kulik <m@ku1ik.com>
2014-12-18 14:17:44 +0100

    Use double quotes for all strings

--- Library/Formula/asciinema.rb
+++ Library/Formula/asciinema.rb
@@ -1,7 +1,7 @@
 class Asciinema < Formula
-  homepage 'https://asciinema.org/'
-  url 'https://github.com/asciinema/asciinema-cli/archive/v0.9.9.tar.gz'
-  sha1 '155c19366ffb3347e97026e9ab8006c16d2a52c6'
+  homepage "https://asciinema.org/"
+  url "https://github.com/asciinema/asciinema-cli/archive/v0.9.9.tar.gz"
+  sha1 "155c19366ffb3347e97026e9ab8006c16d2a52c6"
 
   depends_on "go" => :build
   depends_on "hg" => :build
