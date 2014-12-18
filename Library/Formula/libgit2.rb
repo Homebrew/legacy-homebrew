06807d4451891f3603d9970c873c450bbce26955
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-18 22:24:56 +0000

    libgit2: update 0.21.3 bottle.

--- Library/Formula/libgit2.rb
+++ Library/Formula/libgit2.rb
@@ -8,6 +8,10 @@ class Libgit2 < Formula
   head "https://github.com/libgit2/libgit2.git", :branch => "development"
 
   bottle do
+    cellar :any
+    sha1 "1eaa718a043b055902f0cd5c01a7a93b14836d85" => :yosemite
+    sha1 "9b5d5f3026c699e05ad9a623ea37e004e800af30" => :mavericks
+    sha1 "059ee25e0a3d46ad685b3d55394ae1c8a007da70" => :mountain_lion
   end
 
   depends_on "cmake" => :build
