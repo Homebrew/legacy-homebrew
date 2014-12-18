a36174815bb70d906a232885e61c7583b3ebe729
Jack Nagel <jacknagel@gmail.com>
2014-12-18 13:06:05 -0500

    Prefer :using over made-up URL schemes

--- Library/Formula/log4c.rb
+++ Library/Formula/log4c.rb
@@ -5,7 +5,7 @@ class Log4c < Formula
   url 'https://downloads.sourceforge.net/project/log4c/log4c/1.2.4/log4c-1.2.4.tar.gz'
   sha1 'a2795d7fcbdf5e43e1cc644893adfa725046abe8'
 
-  head 'cvs://:pserver:anonymous:@log4c.cvs.sourceforge.net:/cvsroot/log4c:log4c'
+  head ':pserver:anonymous:@log4c.cvs.sourceforge.net:/cvsroot/log4c:log4c', :using => :cvs
 
   def install
     system "./configure", "--prefix=#{prefix}"
