707d37f284a272a4e024ef12ae96a734bf347d32
Denis Denisov <denji0k@gmail.com>
2014-12-19 00:51:51 +0200

    libmemcached: adjust patch based on upstream comments
    Closes #35113.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libmemcached.rb
+++ Library/Formula/libmemcached.rb
@@ -4,6 +4,7 @@ class Libmemcached < Formula
   homepage "http://libmemcached.org"
   url "https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz"
   sha1 "8be06b5b95adbc0a7cb0f232e237b648caf783e1"
+  revision 1
 
   bottle do
     cellar :any
@@ -40,24 +41,24 @@ end
 
 __END__
 diff --git a/clients/memflush.cc b/clients/memflush.cc
-index 8bd0dbf..cdba743 100644
+index 8bd0dbf..71545ea 100644
 --- a/clients/memflush.cc
 +++ b/clients/memflush.cc
 @@ -39,7 +39,7 @@ int main(int argc, char *argv[])
  {
    options_parse(argc, argv);
-
+ 
 -  if (opt_servers == false)
-+  if (*opt_servers != NULL)
++  if (opt_servers == NULL)
    {
      char *temp;
-
+ 
 @@ -48,7 +48,7 @@ int main(int argc, char *argv[])
        opt_servers= strdup(temp);
      }
-
+ 
 -    if (opt_servers == false)
-+    if (*opt_servers != NULL)
++    if (opt_servers == NULL)
      {
        std::cerr << "No Servers provided" << std::endl;
        exit(EXIT_FAILURE);
