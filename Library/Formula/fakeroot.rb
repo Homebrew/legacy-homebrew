b684728ec0852155cd843b7a9649cfc8fb000bd7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-19 22:02:31 +0000

    fakeroot: add 1.20.2 bottle.

--- Library/Formula/fakeroot.rb
+++ Library/Formula/fakeroot.rb
@@ -4,6 +4,13 @@ class Fakeroot < Formula
   mirror "http://ftp.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.20.2.orig.tar.bz2"
   sha1 "367040df07043edb630942b21939e493f3fad888"
 
+  bottle do
+    cellar :any
+    sha1 "1780485693e73b2c411e2c49d34aab5c139a095d" => :yosemite
+    sha1 "d7bd430316af3b01f641dc6a3cc1d66eaa5a4eee" => :mavericks
+    sha1 "82f4abf913c28ea7c32910537dcf8b87716168cc" => :mountain_lion
+  end
+
   # Compile is broken. https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=766649
   # Patches submitted upstream on 24/10/2014, but no reply from maintainer thus far.
   patch do
