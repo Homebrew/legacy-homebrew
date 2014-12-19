3285351d9b981b1949d2c2ed65ee9e860ebdb794
Wesley Hill <wesley@hakobaito.co.uk>
2014-12-19 13:46:07 +0000

    should have removed version from previous commit.

--- Library/Formula/memo.rb
+++ Library/Formula/memo.rb
@@ -2,7 +2,6 @@ class Memo < Formula
   url "http://www.getmemo.org/memo-1.4.tar.gz"
   homepage "http://www.getmemo.org"
   sha1 "0b4c1b22fac0644b54d7de1e89cb20a54c8fff25"
-  version "1.4"
 
 
   def install
