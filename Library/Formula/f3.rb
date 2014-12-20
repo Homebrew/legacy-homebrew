fa5839f4afc6dca940fc251ce58d38d364dc050a
Baptiste Fontaine <batifon@yahoo.fr>
2014-12-20 21:33:42 +0100

    f3: head, manpages & test added
    Closes #35155.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/f3.rb
+++ Library/Formula/f3.rb
@@ -5,8 +5,16 @@ class F3 < Formula
   url "https://github.com/AltraMayor/f3/archive/v3.0.tar.gz"
   sha1 "9e0d2ddec98c09be17b5d343bd6d5fac2606a963"
 
+  head "https://github.com/AltraMayor/f3.git"
+
   def install
-    system "make mac"
+    system "make", "mac"
     bin.install "f3read", "f3write"
+    man1.install "f3read.1"
+    man1.install_symlink "f3read.1" => "f3write.1"
+  end
+
+  test do
+    system "#{bin}/f3read", testpath
   end
 end
