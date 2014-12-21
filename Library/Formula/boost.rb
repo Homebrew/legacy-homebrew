c5a5a5008640b3982167a8871454a65fbea41d63
Bruno Kirschner <bruno.kirschner@online.de>
2014-12-21 13:39:50 +0100

    boost 1.57

--- Library/Formula/boost.rb
+++ Library/Formula/boost.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Boost < Formula
   homepage 'http://www.boost.org'
-  url 'https://downloads.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.bz2'
-  sha1 'f94bb008900ed5ba1994a1072140590784b9b5df'
+  url 'https://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2'
+  sha1 'e151557ae47afd1b43dc3fac46f8b04a8fe51c12'
 
   head 'https://github.com/boostorg/boost.git'
 
