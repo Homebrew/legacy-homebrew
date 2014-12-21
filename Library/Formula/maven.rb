7a4f9fc1569f8ce1c0eba3d6a4d15daafe9b2b8e
Andrew Holland <aholland@a1dutch.co.uk>
2014-12-21 23:22:39 +0000

    maven 3.2.5
    update to latest maven release
    
    Closes #35175.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/maven.rb
+++ Library/Formula/maven.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Maven < Formula
   homepage 'http://maven.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz'
-  sha1 '1ce6641f7dcb4fd5785bedcdca2979da83f6d23f'
+  url 'http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz'
+  sha1 '41009327d5494e0e8970b25b77ffed8934cd7ca1'
 
   def install
     # Remove windows files
