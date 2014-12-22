86634de9e9f685911a600c9772579b6906ef98f3
Kozo Nishida <knishida@riken.jp>
2014-12-22 13:23:46 +0900

    update redpen version, add test

--- Library/Formula/redpen.rb
+++ Library/Formula/redpen.rb
@@ -1,14 +1,16 @@
-require 'formula'
-
 class Redpen < Formula
   homepage "http://redpen.cc/"
-  url "https://github.com/recruit-tech/redpen/releases/download/v1.0-experimental-1/redpen-cli-1.0-assembled.tar.gz"
-  sha1 "7441d916dd7ebf73e2f4334496687270a3843d15"
+  url "https://github.com/recruit-tech/redpen/releases/download/v1.0/redpen-1.0.tar.gz"
+  sha1 "409c06450c35529d53ce5fe669c9fae662832cdf"
 
   depends_on :java => "1.8"
 
   def install
     libexec.install %w[bin lib]
-    bin.install_symlink libexec+'bin/redpen'
+    bin.install_symlink libexec+"bin/redpen"
+  end
+
+  test do
+    system "#{bin}/redpen", "version"
   end
 end
