f14576ec882c073172db5b17d7799dfbdcb03bc9
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-22 06:28:37 +0000

    libjson-rpc-cpp: add 0.4.1 bottle.

--- Library/Formula/libjson-rpc-cpp.rb
+++ Library/Formula/libjson-rpc-cpp.rb
@@ -3,6 +3,13 @@ class LibjsonRpcCpp < Formula
   url "https://github.com/cinemast/libjson-rpc-cpp/archive/v0.4.1.tar.gz"
   sha1 "199c5f262c41e51ff1d4f3625fb25543d97e852b"
 
+  bottle do
+    cellar :any
+    sha1 "d6d672f1bbfb224d822788937fb7f9c8c545c875" => :yosemite
+    sha1 "7cae25c4101d4b4b504b4cc8125734f64a7eccbc" => :mavericks
+    sha1 "320fbd360648ffcf84fce6a33694fcb0ce3dbf07" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "argtable"
   depends_on "jsoncpp"
