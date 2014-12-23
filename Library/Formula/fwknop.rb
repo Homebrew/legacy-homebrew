30c474d271d4114cb96e497defeb366a5bc6c04b
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-23 22:40:39 +0000

    fwknop 2.6.5
    Version bump, and better sandboxing of the test.
    
    Closes #35221.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/fwknop.rb
+++ Library/Formula/fwknop.rb
@@ -1,11 +1,8 @@
-require "formula"
-
 class Fwknop < Formula
   homepage "http://www.cipherdyne.org/fwknop/"
   head "https://github.com/mrash/fwknop.git"
-  url "https://github.com/mrash/fwknop/archive/2.6.4.tar.gz"
-  sha1 "b13ef022ade7da6dc5b08335d5a1d29dd898887b"
-  revision 1
+  url "https://github.com/mrash/fwknop/archive/2.6.5.tar.gz"
+  sha1 "52f5a191f37aeeb2716d2ad5a25dbf41d6593100"
 
   bottle do
     sha1 "f82b7596356044e1b6b0c41ede54bfcc11b3585a" => :yosemite
@@ -22,12 +19,15 @@ class Fwknop < Formula
   def install
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking", "--disable-silent-rules",
-                          "--prefix=#{prefix}", "--with-gpgme",
+                          "--prefix=#{prefix}", "--with-gpgme", "--sysconfdir=#{etc}",
                           "--with-gpg=#{Formula["gnupg2"].opt_prefix}/bin/gpg2"
     system "make", "install"
   end
 
   test do
+    ENV["HOME"] = testpath
+    touch testpath/".fwknoprc"
+    chmod 0600, testpath/".fwknoprc"
     system "#{bin}/fwknop", "--version"
   end
 end
