5ca66de9d17f02eaeb50c7bf768af1430c2b7e38
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-23 02:09:20 +0000

    schroedinger: added mirror
    The upstream website and URL are dead presently. This adds a mirror
    to mitigate the package being unavailable.
    
    The generation of bottles, which this PR will force, should also aid
    that situation.
    
    Problem reported in [this comment](https://github.com/Homebrew/homebrew/commit/4f4ada9cadc3996077c7fed23c16aa54dea79acd#commitcomment-9071321).
    
    Closes #35202.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/schroedinger.rb
+++ Library/Formula/schroedinger.rb
@@ -1,36 +1,33 @@
-require 'formula'
-
 class Schroedinger < Formula
-  homepage 'http://diracvideo.org/'
-  url 'http://diracvideo.org/download/schroedinger/schroedinger-1.0.11.tar.gz'
-  sha1 'c01ee0bed6c1bd7608763927f719f94ebc6aaa41'
+  homepage "http://diracvideo.org/"
+  url "http://diracvideo.org/download/schroedinger/schroedinger-1.0.11.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/s/schroedinger/schroedinger_1.0.11.orig.tar.gz"
+  sha1 "c01ee0bed6c1bd7608763927f719f94ebc6aaa41"
 
   head do
-    url 'git://diracvideo.org/git/schroedinger.git'
+    url "git://diracvideo.org/git/schroedinger.git"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'orc'
+  depends_on "pkg-config" => :build
+  depends_on "orc"
 
   def install
-    system "autoreconf -i -f" if build.head?
+    system "autoreconf", "-fvi" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
 
-    # Disable compiling the test suite.
-    # The test suite is known not to build against Orc 0.4.16 in Schroedinger 1.0.11.
+    # The test suite is known not to build against Orc >0.4.16 in Schroedinger 1.0.11.
     # A fix is in upstream, so test when pulling 1.0.12 if this is still needed. See:
     # http://www.mail-archive.com/schrodinger-devel@lists.sourceforge.net/msg00415.html
-
     inreplace "Makefile" do |s|
       s.change_make_var! "SUBDIRS", "schroedinger doc tools"
       s.change_make_var! "DIST_SUBDIRS", "schroedinger doc tools"
     end
 
-    system "make install"
+    system "make", "install"
   end
 end
