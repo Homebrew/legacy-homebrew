d2667d195b49cc20d52772b476f394c7cebc8b07
Xu Cheng <xucheng@me.com>
2014-12-25 02:29:15 +0800

    eigen: modernize
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/eigen.rb
+++ Library/Formula/eigen.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Eigen < Formula
-  homepage 'http://eigen.tuxfamily.org/'
-  url 'http://bitbucket.org/eigen/eigen/get/3.2.2.tar.bz2'
-  sha1 '1e1a85681777314805003db5469d1a00785c58df'
+  homepage "http://eigen.tuxfamily.org/"
+  url "http://bitbucket.org/eigen/eigen/get/3.2.2.tar.bz2"
+  sha1 "1e1a85681777314805003db5469d1a00785c58df"
 
   bottle do
     cellar :any
@@ -13,21 +11,21 @@ class Eigen < Formula
     sha1 "9654532e0b9f6762ee73c3938696cf95bc20a813" => :mountain_lion
   end
 
-  head 'https://bitbucket.org/eigen/eigen', :using => :hg
+  head "https://bitbucket.org/eigen/eigen", :using => :hg
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   option :universal
 
   def install
     ENV.universal_binary if build.universal?
-    mkdir 'eigen-build' do
+    mkdir "eigen-build" do
       args = std_cmake_args
-      args.delete '-DCMAKE_BUILD_TYPE=None'
-      args << '-DCMAKE_BUILD_TYPE=Release'
-      args << "-Dpkg_config_libdir=#{lib}" << '..'
-      system 'cmake', *args
-      system 'make install'
+      args.delete "-DCMAKE_BUILD_TYPE=None"
+      args << "-DCMAKE_BUILD_TYPE=Release"
+      args << "-Dpkg_config_libdir=#{lib}" << ".."
+      system "cmake", *args
+      system "make install"
     end
   end
 
