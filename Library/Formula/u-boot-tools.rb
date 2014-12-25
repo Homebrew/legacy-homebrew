17d04a5965346b0ae04907f1d963bbac163ea210
Victor Costan <costan@gmail.com>
2014-12-25 15:15:49 -0500

    u-boot-tools 2015.01
    Closes #35256.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/u-boot-tools.rb
+++ Library/Formula/u-boot-tools.rb
@@ -1,13 +1,18 @@
-require 'formula'
-
 class UBootTools < Formula
-  homepage 'http://www.denx.de/wiki/U-Boot/'
-  url 'ftp://ftp.denx.de/pub/u-boot/u-boot-2012.10.tar.bz2'
-  sha1 '80bc5f6d3e518803c9320d480e693fe1cb76a494'
+  homepage "http://www.denx.de/wiki/U-Boot/"
+  url "ftp://ftp.denx.de/pub/u-boot/u-boot-2015.01.tar.bz2"
+  sha1 "8d22ab0d9f3902122f160280facacc468bad0da9"
+
+  depends_on "openssl"
 
   def install
-    system 'make tools'
-    bin.install 'tools/mkimage'
-    man1.install 'doc/mkimage.1'
+    system "make", "sandbox_defconfig"
+    system "make", "tools"
+    bin.install "tools/mkimage"
+    man1.install "doc/mkimage.1"
+  end
+
+  test do
+    system bin/"mkimage", "-V"
   end
 end
