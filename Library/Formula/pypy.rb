1620a236c71bcb7a310d94982a78c8ac641b08d6
Tim D. Smith <git@tim-smith.us>
2014-12-25 00:08:41 -0500

    setuptools 9.1

--- Library/Formula/pypy.rb
+++ Library/Formula/pypy.rb
@@ -19,8 +19,8 @@ class Pypy < Formula
   depends_on "openssl"
 
   resource "setuptools" do
-    url "https://pypi.python.org/packages/source/s/setuptools/setuptools-8.2.1.tar.gz"
-    sha1 "ddb4454303142be3446437e4fafb13bbd4570133"
+    url "https://pypi.python.org/packages/source/s/setuptools/setuptools-9.1.tar.gz"
+    sha1 "b068a670c84df7b961730c6a0d00cd06c7b767f0"
   end
 
   resource "pip" do
