b6fc41c0af5ea9d0d229e58456c3138833e049dc
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-26 03:33:35 +0000

    libassuan: update 2.2.0 bottle.

--- Library/Formula/libassuan.rb
+++ Library/Formula/libassuan.rb
@@ -8,9 +8,9 @@ class Libassuan < Formula
 
   bottle do
     cellar :any
-    sha1 "8c89e123cca5d492535e9ba9ec3ca34141d54823" => :yosemite
-    sha1 "b1e6d766da6c4cd4b77ec6d41e3a3ec7d657bd88" => :mavericks
-    sha1 "9c395e49e64d3ce487ef36f66dda3c4d6f1f3214" => :mountain_lion
+    sha1 "8c80c50672dff700e0b95f2319b5b3393cc6a00c" => :yosemite
+    sha1 "560d930da2edfeaa9e0d6197e83627d15e0be1cb" => :mavericks
+    sha1 "adbf32aefee6ef81a73007647ad49818f61416b7" => :mountain_lion
   end
 
   depends_on "libgpg-error"
