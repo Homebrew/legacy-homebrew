cc19e8c75bc08ed8cfbc17e56db600441f57f805
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-26 03:14:03 +0000

    libcec: update 2.2.0 bottle.

--- Library/Formula/libcec.rb
+++ Library/Formula/libcec.rb
@@ -5,9 +5,9 @@ class Libcec < Formula
 
   bottle do
     cellar :any
-    sha1 "15c6c1b20a5d4847017d0d0a1f02c3519080a236" => :yosemite
-    sha1 "26a55716130e364641b6eec32e56ad41b7a7e7a3" => :mavericks
-    sha1 "34efc22ee55549ddee07ffc17f3647b8fe180399" => :mountain_lion
+    sha1 "d80df26b65e04fdc1a4a6ba2801e54231d9b7be7" => :yosemite
+    sha1 "9344093a4180a7f5d0e066ba4b50f301fd1e80b1" => :mavericks
+    sha1 "5b04eb4b157c34ab543082a9c9c0868450412c37" => :mountain_lion
   end
 
   depends_on "autoconf" => :build
