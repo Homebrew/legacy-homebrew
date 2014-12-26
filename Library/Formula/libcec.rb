4a795cbdf0cc03cc2179fe78d6be340b9ad10eda
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-26 03:03:46 +0000

    libcec 2.2.0
    Version bump.
    
    Closes #35267.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libcec.rb
+++ Library/Formula/libcec.rb
@@ -1,13 +1,10 @@
-require 'formula'
-
 class Libcec < Formula
-  homepage 'http://libcec.pulse-eight.com/'
-  url 'https://github.com/Pulse-Eight/libcec/archive/libcec-2.1.4.tar.gz'
-  sha1 '3ee241201b3650b97ec4fc41b0f5dd33476080f9'
+  homepage "http://libcec.pulse-eight.com/"
+  url "https://github.com/Pulse-Eight/libcec/archive/libcec-2.2.0-repack.tar.gz"
+  sha1 "42e19328c5b05b5fff27bcdf80e7acc329201913"
 
   bottle do
     cellar :any
-    revision 1
     sha1 "15c6c1b20a5d4847017d0d0a1f02c3519080a236" => :yosemite
     sha1 "26a55716130e364641b6eec32e56ad41b7a7e7a3" => :mavericks
     sha1 "34efc22ee55549ddee07ffc17f3647b8fe180399" => :mountain_lion
@@ -21,8 +18,9 @@ class Libcec < Formula
   def install
     system "./bootstrap"
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
