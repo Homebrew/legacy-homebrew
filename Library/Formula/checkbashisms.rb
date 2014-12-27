9e3ba1cc0e4c956fbc8b3f26c1e8d76612a30fb5
Mike McQuaid <mike@mikemcquaid.com>
2014-12-27 15:59:49 +0000

    checkbashisms: fix strict audit failures.

--- Library/Formula/checkbashisms.rb
+++ Library/Formula/checkbashisms.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Checkbashisms < Formula
-  homepage 'http://manpages.ubuntu.com/manpages/natty/man1/checkbashisms.1.html'
+  homepage "http://manpages.ubuntu.com/manpages/natty/man1/checkbashisms.1.html"
   # Get upgrades at https://launchpad.net/ubuntu/+source/devscripts/
-  url 'https://launchpad.net/ubuntu/+archive/primary/+files/devscripts_2.13.4.tar.xz'
-  sha1 '94e7225c2f9f9062cea35c8010e984ae98834c28'
+  url "https://launchpad.net/ubuntu/+archive/primary/+files/devscripts_2.13.4.tar.xz"
+  sha1 "94e7225c2f9f9062cea35c8010e984ae98834c28"
 
   bottle do
     cellar :any
@@ -15,9 +13,9 @@ class Checkbashisms < Formula
   end
 
   def install
-    inreplace 'scripts/checkbashisms.pl', '###VERSION###', "#{version}ubuntu1"
-    bin.install 'scripts/checkbashisms.pl' => 'checkbashisms'
-    man1.install 'scripts/checkbashisms.1'
+    inreplace "scripts/checkbashisms.pl", "###VERSION###", "#{version}ubuntu1"
+    bin.install "scripts/checkbashisms.pl" => "checkbashisms"
+    man1.install "scripts/checkbashisms.1"
   end
 
   test do
