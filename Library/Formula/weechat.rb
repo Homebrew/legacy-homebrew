068d8f6200f864a763a7d8111f9523dc7266ef31
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-27 02:32:08 +0000

    weechat 1.1-rc1 devel
    Devel version, returned the ` gettext ` dep that was removed in #18722
    but is now functional again, prunes the redundant ` -DENABLE_GTK=OFF `
    option as it’s not a valid option any more and reformats the args.

--- Library/Formula/weechat.rb
+++ Library/Formula/weechat.rb
@@ -1,11 +1,15 @@
-require 'formula'
-
 class Weechat < Formula
-  homepage 'http://www.weechat.org'
-  url 'http://weechat.org/files/src/weechat-1.0.1.tar.gz'
-  sha1 '1d33591b6c0adc2c30b36a7b349603cbdbcb40b2'
+  homepage "https://www.weechat.org"
+  url "https://weechat.org/files/src/weechat-1.0.1.tar.gz"
+  sha1 "1d33591b6c0adc2c30b36a7b349603cbdbcb40b2"
 
-  head 'https://github.com/weechat/weechat.git'
+  devel do
+    url "https://github.com/weechat/weechat/archive/v1.1-rc1.tar.gz"
+    sha1 "3cf06cfead34fe351a0e3eb53144cbf3f68bc7e5"
+    version "1.1-rc1"
+  end
+
+  head "https://github.com/weechat/weechat.git"
 
   bottle do
     sha1 "d7112142ed11d2a1a55b367e01e0200b5ba0cae6" => :mavericks
@@ -13,16 +17,19 @@ class Weechat < Formula
     sha1 "8cfbd8d53a85b88138bff6c4545efa955fa30c26" => :lion
   end
 
-  depends_on 'cmake' => :build
-  depends_on 'gnutls'
-  depends_on 'libgcrypt'
-  depends_on 'guile' => :optional
-  depends_on 'aspell' => :optional
-  depends_on 'lua' => :optional
-  depends_on :python => :optional
+  option "with-perl", "Build the perl module"
+  option "with-ruby", "Build the ruby module"
+  option "with-curl", "Build with brewed curl"
 
-  option 'with-perl', 'Build the perl module'
-  option 'with-ruby', 'Build the ruby module'
+  depends_on "cmake" => :build
+  depends_on "gnutls"
+  depends_on "libgcrypt"
+  depends_on "gettext"
+  depends_on "guile" => :optional
+  depends_on "aspell" => :optional
+  depends_on "lua" => :optional
+  depends_on :python => :optional
+  depends_on "curl" => :optional
 
   def install
     # builds against the python in PATH by asking cmake to use introspected
@@ -32,25 +39,18 @@ class Weechat < Formula
       inreplace "cmake/FindPython.cmake", "PATHS ${", "HINTS ${"
     end
 
-    args = std_cmake_args + %W[
-      -DPREFIX=#{prefix}
-      -DENABLE_GTK=OFF
-    ]
-    args << '-DENABLE_LUA=OFF'    if build.without? 'lua'
-    args << '-DENABLE_PERL=OFF'   if build.without? 'perl'
-    args << '-DENABLE_RUBY=OFF'   if build.without? 'ruby'
-    args << '-DENABLE_ASPELL=OFF' if build.without? 'aspell'
-    args << '-DENABLE_GUILE=OFF'  if build.without? 'guile'
-    args << '-DENABLE_PYTHON=OFF' if build.without? 'python'
+    args = std_cmake_args
 
-    # NLS/gettext support disabled for now since it doesn't work in stdenv
-    # see https://github.com/Homebrew/homebrew/issues/18722
-    args << "-DENABLE_NLS=OFF"
-    args << '..'
+    args << "-DENABLE_LUA=OFF"    if build.without? "lua"
+    args << "-DENABLE_PERL=OFF"   if build.without? "perl"
+    args << "-DENABLE_RUBY=OFF"   if build.without? "ruby"
+    args << "-DENABLE_ASPELL=OFF" if build.without? "aspell"
+    args << "-DENABLE_GUILE=OFF"  if build.without? "guile"
+    args << "-DENABLE_PYTHON=OFF" if build.without? "python"
 
-    mkdir 'build' do
-      system 'cmake', *args
-      system 'make install'
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
     end
   end
 
