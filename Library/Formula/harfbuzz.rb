8ea336c36ba3ada69f31bbd54de1ff7c3a80770b
Chongyu Zhu <i@lembacon.com>
2014-12-27 21:54:56 +0800

    harfbuzz 0.9.37
    Closes #35285.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -2,9 +2,8 @@ require "formula"
 
 class Harfbuzz < Formula
   homepage "http://www.freedesktop.org/wiki/Software/HarfBuzz"
-  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.35.tar.bz2"
-  sha256 "0aa1a8aba6f502321cf6fef3c9d2c73dde48389c5ed1d3615a7691944c2a06ed"
-  revision 1
+  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.37.tar.bz2"
+  sha256 "255f3b3842dead16863d1d0c216643d97b80bfa087aaa8fc5926da24ac120207"
 
   bottle do
     cellar :any
