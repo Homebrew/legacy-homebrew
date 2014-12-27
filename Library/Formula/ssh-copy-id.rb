39ff44a621c94fcffa4ba3608924e03caa24fafc
Mike McQuaid <mike@mikemcquaid.com>
2014-12-27 16:11:11 +0000

    ssh-copy-id: fix strict audit failures.

--- Library/Formula/ssh-copy-id.rb
+++ Library/Formula/ssh-copy-id.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class SshCopyId < Formula
-  homepage 'http://www.openssh.com/'
-  url 'http://ftp.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz'
-  mirror 'http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz'
-  version '6.7p1'
-  sha1 '14e5fbed710ade334d65925e080d1aaeb9c85bf6'
+  homepage "http://www.openssh.com/"
+  url "http://ftp.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz"
+  mirror "http://ftp3.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz"
+  version "6.7p1"
+  sha1 "14e5fbed710ade334d65925e080d1aaeb9c85bf6"
 
   bottle do
     cellar :any
@@ -15,7 +13,11 @@ class SshCopyId < Formula
   end
 
   def install
-    bin.install 'contrib/ssh-copy-id'
-    man1.install 'contrib/ssh-copy-id.1'
+    bin.install "contrib/ssh-copy-id"
+    man1.install "contrib/ssh-copy-id.1"
+  end
+
+  test do
+    shell_output bin/"ssh-copy-id -h", 1
   end
 end
