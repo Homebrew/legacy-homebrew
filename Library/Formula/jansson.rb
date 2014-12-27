b5c42b1a37f3cc9195290bb904ff27d1a7bc0992
Mike McQuaid <mike@mikemcquaid.com>
2014-12-27 16:08:38 +0000

    jansson: fix some strict audit failures.

--- Library/Formula/jansson.rb
+++ Library/Formula/jansson.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Jansson < Formula
-  homepage 'http://www.digip.org/jansson/'
-  url 'http://www.digip.org/jansson/releases/jansson-2.7.tar.gz'
-  sha1 '7d8686d84fd46c7c28d70bf2d5e8961bc002845e'
+  homepage "http://www.digip.org/jansson/"
+  url "http://www.digip.org/jansson/releases/jansson-2.7.tar.gz"
+  sha1 "7d8686d84fd46c7c28d70bf2d5e8961bc002845e"
 
   bottle do
     cellar :any
@@ -18,6 +16,6 @@ class Jansson < Formula
     ENV.universal_binary if build.universal?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
