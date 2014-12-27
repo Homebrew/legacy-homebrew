0e81bae396174d66a656e02f601fcc48ef433c48
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-27 18:55:51 +0000

    baresip: add 0.4.12 bottle.

--- Library/Formula/baresip.rb
+++ Library/Formula/baresip.rb
@@ -3,6 +3,12 @@ class Baresip < Formula
   url "http://www.creytiv.com/pub/baresip-0.4.12.tar.gz"
   sha1 "03f72e93ddf2f896568b092ee63203b1c6a19679"
 
+  bottle do
+    sha1 "bbbd13065696d79cb97d346ef5df38f0f8ad5c81" => :yosemite
+    sha1 "2805c3ab5c7bac73d7eaf54884832cfd5b0dd15b" => :mavericks
+    sha1 "aca67e94e7b6aa4fd8de6e6433332a968290b58b" => :mountain_lion
+  end
+
   depends_on "librem"
   depends_on "libre"
 
