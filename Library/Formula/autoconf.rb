a917c361ac46523aed6fd80caec4511306f52b78
Mike McQuaid <mike@mikemcquaid.com>
2014-12-27 15:59:32 +0000

    autoconf: fix strict audit failures.

--- Library/Formula/autoconf.rb
+++ Library/Formula/autoconf.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Autoconf < Formula
-  homepage 'http://www.gnu.org/software/autoconf'
-  url 'http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz'
-  sha1 '562471cbcb0dd0fa42a76665acf0dbb68479b78a'
+  homepage "http://www.gnu.org/software/autoconf"
+  url "http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
+  sha1 "562471cbcb0dd0fa42a76665acf0dbb68479b78a"
 
   bottle do
     revision 1
@@ -17,19 +15,19 @@ class Autoconf < Formula
   keg_only :provided_until_xcode43
 
   def install
-    ENV['PERL'] = '/usr/bin/perl'
+    ENV["PERL"] = "/usr/bin/perl"
 
     # force autoreconf to look for and use our glibtoolize
-    inreplace 'bin/autoreconf.in', 'libtoolize', 'glibtoolize'
-    # also touch the man page so that it isn't rebuilt
-    inreplace 'man/autoreconf.1', 'libtoolize', 'glibtoolize'
+    inreplace "bin/autoreconf.in", "libtoolize", "glibtoolize"
+    # also touch the man page so that it isn"t rebuilt
+    inreplace "man/autoreconf.1", "libtoolize", "glibtoolize"
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
-    rm_f info/'standards.info'
+    system "make", "install"
+    rm_f info/"standards.info"
   end
 
   test do
-    cp "#{share}/autoconf/autotest/autotest.m4", 'autotest.m4'
-    system "#{bin}/autoconf", 'autotest.m4'
+    cp "#{share}/autoconf/autotest/autotest.m4", "autotest.m4"
+    system "#{bin}/autoconf", "autotest.m4"
   end
 end
