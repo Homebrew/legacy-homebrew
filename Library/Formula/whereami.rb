fb18ee549638ed1dd2d93b95e971eca7b0ca5cc6
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-28 17:54:24 +0000

    whereami: add 1.0 bottle.

--- Library/Formula/whereami.rb
+++ Library/Formula/whereami.rb
@@ -3,6 +3,11 @@ class Whereami < Formula
   url "https://github.com/victor/whereami/archive/v1.0.tar.gz"
   sha1 "2f81e4b05af5f6806590d7212c0dbfbea00a75e0"
 
+  bottle do
+    cellar :any
+    sha1 "f1b35d4c5180a303169d0ba251daf39375c20065" => :yosemite
+  end
+
   depends_on :xcode => ["6.1.1", :build]
   depends_on :macos => :yosemite
 
