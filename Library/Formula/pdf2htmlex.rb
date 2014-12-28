0bd07cbe548953098ff4b6e8009a825b90a65593
Mike McQuaid <mike@mikemcquaid.com>
2014-12-28 14:17:54 +0000

    pdf2htmlex: depends on libtool at runtime.

--- Library/Formula/pdf2htmlex.rb
+++ Library/Formula/pdf2htmlex.rb
@@ -30,7 +30,7 @@ class Pdf2htmlex < Formula
   # Fontforge dependencies
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "libtool"
   depends_on "glib"
   depends_on "pango"
   depends_on "gettext"
