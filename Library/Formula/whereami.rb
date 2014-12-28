977e32e9bfb0fb3e582213f64d9a04e73376980c
Victor Jalencas <victor@victor.sh>
2014-12-28 18:15:05 +0100

    Change test method and add dependency on Yosemite

--- Library/Formula/whereami.rb
+++ Library/Formula/whereami.rb
@@ -4,12 +4,13 @@ class Whereami < Formula
   sha1 "2f81e4b05af5f6806590d7212c0dbfbea00a75e0"
 
   depends_on :xcode => ["6.1.1", :build]
+  depends_on :macos => :yosemite
 
   def install
     system "xcodebuild", "install", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin"
   end
 
   test do
-    system "whereami"
+    system "whereami", "--version"
   end
 end
