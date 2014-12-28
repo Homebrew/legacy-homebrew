4fcc24022097be045d7eef9eb03b490bb4d20794
Xu Cheng <xucheng@me.com>
2014-12-28 23:09:57 +0800

    nettle: modernize

--- Library/Formula/nettle.rb
+++ Library/Formula/nettle.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Nettle < Formula
-  homepage 'http://www.lysator.liu.se/~nisse/nettle/'
-  url 'http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz'
-  sha1 'e7477df5f66e650c4c4738ec8e01c2efdb5d1211'
+  homepage "http://www.lysator.liu.se/~nisse/nettle/"
+  url "http://www.lysator.liu.se/~nisse/archive/nettle-2.7.1.tar.gz"
+  sha1 "e7477df5f66e650c4c4738ec8e01c2efdb5d1211"
 
   bottle do
     cellar :any
@@ -14,14 +12,14 @@ class Nettle < Formula
     sha1 "6c56084887da5b7e99d7c730bf22a68c9af360e9" => :lion
   end
 
-  depends_on 'gmp'
+  depends_on "gmp"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--enable-shared"
     system "make"
-    system "make install"
-    system "make check"
+    system "make", "install"
+    system "make", "check"
   end
 end
