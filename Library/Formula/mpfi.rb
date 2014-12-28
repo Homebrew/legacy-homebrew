8d46a4e661ced361626acb0c1add394e23988ded
Xu Cheng <xucheng@me.com>
2014-12-28 22:42:01 +0800

    mpfi: modernize

--- Library/Formula/mpfi.rb
+++ Library/Formula/mpfi.rb
@@ -1,14 +1,12 @@
-require 'formula'
-
 class Mpfi < Formula
-  homepage 'http://perso.ens-lyon.fr/nathalie.revol/software.html'
-  url 'https://gforge.inria.fr/frs/download.php/30130/mpfi-1.5.1.tar.gz'
-  sha1 '288302c0cdefe823cc3aa71de31c1da82eeb6ad0'
+  homepage "http://perso.ens-lyon.fr/nathalie.revol/software.html"
+  url "https://gforge.inria.fr/frs/download.php/30130/mpfi-1.5.1.tar.gz"
+  sha1 "288302c0cdefe823cc3aa71de31c1da82eeb6ad0"
 
-  depends_on 'gmp'
-  depends_on 'mpfr'
+  depends_on "gmp"
+  depends_on "mpfr"
 
-  option '32-bit'
+  option "32-bit"
 
   def install
     ENV.m32 if build.build_32_bit?
