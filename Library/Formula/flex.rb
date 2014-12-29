b10a8d8556ad19c3199204da159a99810fc02e5a
Xu Cheng <xucheng@me.com>
2014-12-29 11:11:41 +0800

    flex: add test
    Closes #35328.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/flex.rb
+++ Library/Formula/flex.rb
@@ -19,4 +19,25 @@ class Flex < Formula
                           "--prefix=#{prefix}"
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.flex").write <<-EOS.undent
+      CHAR   [a-z][A-Z]
+      %%
+      {CHAR}+      printf("%s", yytext);
+      [ \\t\\n]+   printf("\\n");
+      %%
+      int main()
+      {
+        yyin = stdin;
+        yylex();
+      }
+    EOS
+    system "#{bin}/flex", "test.flex"
+    system ENV.cc, "lex.yy.c", "-L#{lib}", "-lfl", "-o", "test"
+    assert_equal shell_output("echo \"Hello World\" | ./test"), <<-EOS.undent
+      Hello
+      World
+    EOS
+  end
 end
