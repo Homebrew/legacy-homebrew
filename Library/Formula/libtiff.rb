199bf3651bf98a14ffe4d2689abbfe9f9d4f8f3b
Xu Cheng <xucheng@me.com>
2014-12-30 21:47:43 +0800

    libtiff: modernize

--- Library/Formula/libtiff.rb
+++ Library/Formula/libtiff.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Libtiff < Formula
-  homepage 'http://www.remotesensing.org/libtiff/'
-  url 'ftp://ftp.remotesensing.org/pub/libtiff/tiff-4.0.3.tar.gz'
-  mirror 'http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz'
-  sha256 'ea1aebe282319537fb2d4d7805f478dd4e0e05c33d0928baba76a7c963684872'
+  homepage "http://www.remotesensing.org/libtiff/"
+  url "ftp://ftp.remotesensing.org/pub/libtiff/tiff-4.0.3.tar.gz"
+  mirror "http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz"
+  sha256 "ea1aebe282319537fb2d4d7805f478dd4e0e05c33d0928baba76a7c963684872"
 
   bottle do
     cellar :any
@@ -17,7 +15,7 @@ class Libtiff < Formula
   option :universal
   option :cxx11
 
-  depends_on 'jpeg'
+  depends_on "jpeg"
 
   def install
     ENV.universal_binary if build.universal?
@@ -29,6 +27,6 @@ class Libtiff < Formula
                           "--disable-lzma",
                           "--with-jpeg-include-dir=#{jpeg}/include",
                           "--with-jpeg-lib-dir=#{jpeg}/lib"
-    system "make install"
+    system "make", "install"
   end
 end
