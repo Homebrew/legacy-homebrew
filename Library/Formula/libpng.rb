b8bf26f12c7d3ce1578080c3c4b85a5d26c8b0cd
Xu Cheng <xucheng@me.com>
2014-12-30 18:06:40 +0800

    libpng: add test
    Closes #35352.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -1,5 +1,3 @@
-require "formula"
-
 class Libpng < Formula
   homepage "http://www.libpng.org/pub/png/libpng.html"
   url "https://downloads.sf.net/project/libpng/libpng16/1.6.16/libpng-1.6.16.tar.xz"
@@ -24,5 +22,21 @@ class Libpng < Formula
     system "make"
     system "make", "test"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <png.h>
+
+      int main()
+      {
+        png_structp png_ptr;
+        png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lpng", "-o", "test"
+    system "./test"
   end
 end
