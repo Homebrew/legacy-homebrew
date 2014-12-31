192e12982c1487a90d700c8b5bbb39560ffbe7ed
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-31 04:59:19 +0000

    boxes: add 1.1.1 bottle.

--- Library/Formula/boxes.rb
+++ Library/Formula/boxes.rb
@@ -6,6 +6,12 @@ class Boxes < Formula
   head 'https://github.com/ascii-boxes/boxes.git'
   sha1 'fff0d7636c50a4ffa88389981a3d32e8d7a83b39'
 
+  bottle do
+    sha1 "6ace9ba08d5d1b528015598c5ed44b86c35c15f3" => :yosemite
+    sha1 "132294a712de399d24b7ebdc55ea611a1f9b0fe6" => :mavericks
+    sha1 "3078e1f6738312c3bc7b3c4a38043b037cc01fa0" => :mountain_lion
+  end
+
   def install
     ENV.m32
 
