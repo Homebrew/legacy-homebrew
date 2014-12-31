66377a46b86fd26562597dc81a60349493ce79a0
Jack Nagel <jacknagel@gmail.com>
2014-12-31 00:00:54 -0500

    oniguruma: style updates

--- Library/Formula/oniguruma.rb
+++ Library/Formula/oniguruma.rb
@@ -1,13 +1,10 @@
-require 'formula'
-
 class Oniguruma < Formula
-  homepage 'http://www.geocities.jp/kosako3/oniguruma/'
-  url 'http://www.geocities.jp/kosako3/oniguruma/archive/onig-5.9.6.tar.gz'
-  sha1 '08d2d7b64b15cbd024b089f0924037f329bc7246'
+  homepage "http://www.geocities.jp/kosako3/oniguruma/"
+  url "http://www.geocities.jp/kosako3/oniguruma/archive/onig-5.9.6.tar.gz"
+  sha1 "08d2d7b64b15cbd024b089f0924037f329bc7246"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make install"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
   end
 end
