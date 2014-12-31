66fcd5122edec6e064a72249d9877da577ef96c7
BrewTestBot <brew-test-bot@googlegroups.com>
2014-12-31 12:43:27 +0000

    bcrypt: add 1.1 bottle.

--- Library/Formula/bcrypt.rb
+++ Library/Formula/bcrypt.rb
@@ -3,6 +3,13 @@ class Bcrypt < Formula
   url "http://bcrypt.sourceforge.net/bcrypt-1.1.tar.gz"
   sha1 "fd4c7c83fdc560f143bb0e0a8c9fb7aa57e69698"
 
+  bottle do
+    cellar :any
+    sha1 "c762b0073e30fd3a8cb615b30d7b39668c8d672b" => :yosemite
+    sha1 "03c15bc2a81e3ff43fddb2594428897b1c44bee1" => :mavericks
+    sha1 "d48a408b10ce81e33f21301b9885e0e9878ddd63" => :mountain_lion
+  end
+
   def install
     system "make", "CC=#{ENV.cc}",
                    "CFLAGS=#{ENV.cflags}",
