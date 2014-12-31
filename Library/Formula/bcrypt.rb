7704cacd33b9333a0aaa0e0b51e7e269e123a517
Xu Cheng <xucheng@me.com>
2014-12-31 20:03:06 +0800

    bcrypt: modernize

--- Library/Formula/bcrypt.rb
+++ Library/Formula/bcrypt.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Bcrypt < Formula
-  homepage 'http://bcrypt.sourceforge.net'
-  url 'http://bcrypt.sourceforge.net/bcrypt-1.1.tar.gz'
-  sha1 'fd4c7c83fdc560f143bb0e0a8c9fb7aa57e69698'
+  homepage "http://bcrypt.sourceforge.net"
+  url "http://bcrypt.sourceforge.net/bcrypt-1.1.tar.gz"
+  sha1 "fd4c7c83fdc560f143bb0e0a8c9fb7aa57e69698"
 
   def install
     system "make", "CC=#{ENV.cc}",
