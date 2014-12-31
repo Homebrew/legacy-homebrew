22a82933b864b55c29d9a1d85bcbff00195d8769
Jack Nagel <jacknagel@gmail.com>
2014-12-31 00:01:52 -0500

    freetype: style updates

--- Library/Formula/freetype.rb
+++ Library/Formula/freetype.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Freetype < Formula
-  homepage 'http://www.freetype.org'
-  url 'https://downloads.sf.net/project/freetype/freetype2/2.5.5/freetype-2.5.5.tar.bz2'
-  sha1 '7b7460ef51a8fdb17baae53c6658fc1ad000a1c2'
+  homepage "http://www.freetype.org"
+  url "https://downloads.sf.net/project/freetype/freetype2/2.5.5/freetype-2.5.5.tar.bz2"
+  sha1 "7b7460ef51a8fdb17baae53c6658fc1ad000a1c2"
 
   bottle do
     cellar :any
@@ -15,7 +13,7 @@ class Freetype < Formula
   keg_only :provided_pre_mountain_lion
 
   option :universal
-  option 'without-subpixel', "Disable sub-pixel rendering (a.k.a. LCD rendering, or ClearType)"
+  option "without-subpixel", "Disable sub-pixel rendering (a.k.a. LCD rendering, or ClearType)"
 
   depends_on "libpng"
 
@@ -33,7 +31,7 @@ class Freetype < Formula
   end
 
   test do
-    system "#{bin}/freetype-config", '--cflags', '--libs', '--ftversion',
-      '--exec-prefix', '--prefix'
+    system "#{bin}/freetype-config", "--cflags", "--libs", "--ftversion",
+      "--exec-prefix", "--prefix"
   end
 end
