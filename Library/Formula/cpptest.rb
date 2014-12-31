204e75091675579a872d242e3cf4556da5a95a52
Xu Cheng <xucheng@me.com>
2014-12-31 21:19:09 +0800

    cpptest: modernize

--- Library/Formula/cpptest.rb
+++ Library/Formula/cpptest.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Cpptest < Formula
-  homepage 'http://cpptest.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/cpptest/cpptest/cpptest-1.1.2/cpptest-1.1.2.tar.gz'
-  sha1 'c8e69ca98f9b39016c94f1f78659f412ee825049'
+  homepage "http://cpptest.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/cpptest/cpptest/cpptest-1.1.2/cpptest-1.1.2.tar.gz"
+  sha1 "c8e69ca98f9b39016c94f1f78659f412ee825049"
 
   bottle do
     cellar :any
@@ -15,6 +13,6 @@ class Cpptest < Formula
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
