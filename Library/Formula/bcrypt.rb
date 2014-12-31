037b275af2013ac8c04edf20275abc390b6d747a
Xu Cheng <xucheng@me.com>
2014-12-31 20:33:24 +0800

    bcrypt: add test

--- Library/Formula/bcrypt.rb
+++ Library/Formula/bcrypt.rb
@@ -10,4 +10,12 @@ class Bcrypt < Formula
     bin.install "bcrypt"
     man1.install gzip("bcrypt.1")
   end
+
+  test do
+    (testpath/"test.txt").write("Hello World!")
+    pipe_output("#{bin}/bcrypt -r test.txt", "12345678\n12345678\n")
+    mv "test.txt.bfe", "test.out.txt.bfe"
+    pipe_output("#{bin}/bcrypt -r test.out.txt.bfe", "12345678\n")
+    assert_equal File.read("test.txt"), File.read("test.out.txt")
+  end
 end
