87e1baceba5b111d349eed4a93fd42dc0daf6119
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-01 20:55:41 +0000

    id3ed: add 1.10.4 bottle.

--- Library/Formula/id3ed.rb
+++ Library/Formula/id3ed.rb
@@ -3,6 +3,13 @@ class Id3ed < Formula
   url "http://code.fluffytapeworm.com/projects/id3ed/id3ed-1.10.4.tar.gz"
   sha1 "b699e645fcea2fa42658886621eb10531d24008a"
 
+  bottle do
+    cellar :any
+    sha1 "ef017f7b0b088be78071487e6c39f3a3e0d43cca" => :yosemite
+    sha1 "c40ca26c9c0e89f78c21f852340342063f5b82f9" => :mavericks
+    sha1 "b49bc15f4ed27f61c12bc903cf2a7eb60293c65e" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
