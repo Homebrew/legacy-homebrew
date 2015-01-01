fc8ee8e5f655b6ea20fa3dc3b74a3fbe30a91dc1
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-01 20:05:31 +0100

    id3ed added
    Closes #35426.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/id3ed.rb
@@ -0,0 +1,21 @@
+class Id3ed < Formula
+  homepage "http://code.fluffytapeworm.com/projects/id3ed"
+  url "http://code.fluffytapeworm.com/projects/id3ed/id3ed-1.10.4.tar.gz"
+  sha1 "b699e645fcea2fa42658886621eb10531d24008a"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--bindir=#{bin}/",
+                          "--mandir=#{man1}"
+    bin.mkpath
+    man1.mkpath
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/id3ed", "-r", "-q", test_fixtures("test.mp3")
+  end
+end
