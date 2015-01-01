30f4c0e36298ee12f45e34f4daf46cc4ab2a0c06
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-01 16:31:30 +0000

    dateutils: add 0.2.5 bottle.

--- Library/Formula/dateutils.rb
+++ Library/Formula/dateutils.rb
@@ -3,6 +3,13 @@ class Dateutils < Formula
   url "https://bitbucket.org/hroptatyr/dateutils/downloads/dateutils-0.2.5.tar.xz"
   sha1 "47f2ba469daff7586d47473f54a77848b724ba45"
 
+  bottle do
+    cellar :any
+    sha1 "bcacbd417ff8109bd9bd73aaa0397e7738cda974" => :yosemite
+    sha1 "aea972b1c122db46172d6e37fbfd3fd015fa7bdc" => :mavericks
+    sha1 "628ce464741daf92af6dd31d8c93bb012324ae87" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
