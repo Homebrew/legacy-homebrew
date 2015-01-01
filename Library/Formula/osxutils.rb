000a9b87ae110f682bc3e02a6bd382f896c94a76
Xu Cheng <xucheng@me.com>
2015-01-02 00:05:36 +0800

    osxutils: add test

--- Library/Formula/osxutils.rb
+++ Library/Formula/osxutils.rb
@@ -1,18 +1,20 @@
-require 'formula'
-
 class Osxutils < Formula
-  homepage 'https://github.com/vasi/osxutils'
-  head 'https://github.com/vasi/osxutils.git'
-  url 'https://github.com/vasi/osxutils/archive/v1.8.1.tar.gz'
-  sha1 'b6c0e2b0c699a4bf9d51c582c8107ce40cfdec8b'
+  homepage "https://github.com/vasi/osxutils"
+  head "https://github.com/vasi/osxutils.git"
+  url "https://github.com/vasi/osxutils/archive/v1.8.1.tar.gz"
+  sha1 "b6c0e2b0c699a4bf9d51c582c8107ce40cfdec8b"
 
-  conflicts_with 'trash', :because => 'both install a trash binary'
-  conflicts_with 'leptonica',
+  conflicts_with "trash", :because => 'both install a trash binary'
+  conflicts_with "leptonica",
     :because => "both leptonica and osxutils ship a `fileinfo` executable."
-  conflicts_with 'googlecl', :because => 'both install a google binary'
+  conflicts_with "googlecl", :because => 'both install a google binary'
 
   def install
-    system 'make'
-    system 'make', "PREFIX=#{prefix}", 'install'
+    system "make"
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    system "#{bin}/trash"
   end
 end
