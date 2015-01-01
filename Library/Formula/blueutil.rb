e2297bf9a754491887692bf9f241427fcebef3d8
Xu Cheng <xucheng@me.com>
2015-01-01 23:54:26 +0800

    blueutil: add test
    Closes #35420.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/blueutil.rb
+++ Library/Formula/blueutil.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class Blueutil < Formula
-  homepage 'https://github.com/toy/blueutil'
-  url 'https://github.com/toy/blueutil/archive/v1.0.0.tar.gz'
-  sha1 'b1cce64f7fa87eb0cfa32ef8e1dfc1aa06dbbd98'
+  homepage "https://github.com/toy/blueutil"
+  url "https://github.com/toy/blueutil/archive/v1.0.0.tar.gz"
+  sha1 "b1cce64f7fa87eb0cfa32ef8e1dfc1aa06dbbd98"
 
-  head 'https://github.com/toy/blueutil.git'
+  head "https://github.com/toy/blueutil.git"
 
   bottle do
     cellar :any
@@ -18,7 +16,11 @@ class Blueutil < Formula
 
   def install
     # Set to build with SDK=macosx10.6, but it doesn't actually need 10.6
-    xcodebuild 'SDKROOT=', 'SYMROOT=build'
-    bin.install 'build/Release/blueutil'
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/blueutil"
+  end
+
+  test do
+    system "#{bin}/blueutil"
   end
 end
