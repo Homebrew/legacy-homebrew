e1388b0571b71472c5053b4ef1be2d427750b4cd
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-01 20:41:35 +0100

    mp3val: modernize + test added

--- Library/Formula/mp3val.rb
+++ Library/Formula/mp3val.rb
@@ -1,12 +1,15 @@
-require 'formula'
-
 class Mp3val < Formula
-  homepage 'http://mp3val.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/mp3val/mp3val-0.1.8-src.tar.gz'
-  sha1 '19f7506d387f72def2861ec271c5cb4135bd8f5e'
+  homepage "http://mp3val.sourceforge.net/"
+  url "https://downloads.sourceforge.net/mp3val/mp3val-0.1.8-src.tar.gz"
+  sha1 "19f7506d387f72def2861ec271c5cb4135bd8f5e"
 
   def install
-    system "gnumake -f Makefile.gcc"
+    system "gnumake", "-f", "Makefile.gcc"
     bin.install "mp3val.exe" => "mp3val"
+  end
+
+  test do
+    mp3 = test_fixtures("test.mp3")
+    assert_match(/Done!$/, shell_output("#{bin}/mp3val -f #{mp3}"))
   end
 end
