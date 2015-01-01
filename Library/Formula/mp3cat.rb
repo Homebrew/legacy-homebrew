cc6eb5be828b450e0f0f64fcc227248bfa1b704a
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-01 18:21:24 +0100

    mp3cat: modernize + test added

--- Library/Formula/mp3cat.rb
+++ Library/Formula/mp3cat.rb
@@ -1,12 +1,14 @@
-require 'formula'
-
 class Mp3cat < Formula
-  homepage 'http://tomclegg.net/mp3cat'
-  url 'http://tomclegg.net/software/mp3cat-0.4.tar.gz'
-  sha1 '442d2b2b546fec4535c2aa892a8fc61db21eb917'
+  homepage "http://tomclegg.net/mp3cat"
+  url "http://tomclegg.net/software/mp3cat-0.4.tar.gz"
+  sha1 "442d2b2b546fec4535c2aa892a8fc61db21eb917"
 
   def install
     system "make"
-    bin.install %W( mp3cat mp3log mp3log-conf mp3dirclean mp3http mp3stream-conf )
+    bin.install %w(mp3cat mp3log mp3log-conf mp3dirclean mp3http mp3stream-conf)
+  end
+
+  test do
+    pipe_output("#{bin}/mp3cat -v --noclean - -", test_fixtures("test.mp3"))
   end
 end
