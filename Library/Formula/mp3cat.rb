51d08f7f229f1c83cc5f013508f435c1d3bedec5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-01 21:23:33 +0000

    mp3cat: add 0.4 bottle.

--- Library/Formula/mp3cat.rb
+++ Library/Formula/mp3cat.rb
@@ -3,6 +3,13 @@ class Mp3cat < Formula
   url "http://tomclegg.net/software/mp3cat-0.4.tar.gz"
   sha1 "442d2b2b546fec4535c2aa892a8fc61db21eb917"
 
+  bottle do
+    cellar :any
+    sha1 "ad6a863bffdc425ef897b1dc77062eeb8ee41f4a" => :yosemite
+    sha1 "8cf1ca338feb3c3520c84013326bc86c0d48f4d7" => :mavericks
+    sha1 "cc89db74d0e7ddfbc38beeb9bc53d3ade5a7e8dc" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install %w(mp3cat mp3log mp3log-conf mp3dirclean mp3http mp3stream-conf)
