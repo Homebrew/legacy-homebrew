8d231f42e6546bf1c1bc9d78305e2c920aa56234
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-01 01:23:35 +0100

    zssh: modernize + basic test added

--- Library/Formula/zssh.rb
+++ Library/Formula/zssh.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class Zssh < Formula
-  homepage 'http://zssh.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/zssh/zssh/1.5/zssh-1.5c.tgz'
-  sha1 '68dc9b8572646ef63909b3855e7990d75f49926c'
+  homepage "http://zssh.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/zssh/zssh/1.5/zssh-1.5c.tgz"
+  sha1 "68dc9b8572646ef63909b3855e7990d75f49926c"
 
-  depends_on 'lrzsz'
+  depends_on "lrzsz"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
@@ -14,5 +12,9 @@ class Zssh < Formula
 
     bin.install "zssh", "ztelnet"
     man1.install "zssh.1", "ztelnet.1"
+  end
+
+  test do
+    system "#{bin}/zssh", "-s", "exit", "localhost"
   end
 end
