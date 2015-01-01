e9f17e91ca6e527c0d04596d75b3734d3bec772a
Xu Cheng <xucheng@me.com>
2015-01-01 14:23:52 +0800

    libvo-aacenc: modernize

--- Library/Formula/libvo-aacenc.rb
+++ Library/Formula/libvo-aacenc.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class LibvoAacenc < Formula
-  homepage 'http://opencore-amr.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/opencore-amr/vo-aacenc/vo-aacenc-0.1.2.tar.gz'
-  sha1 'ac56325c05eba4c4f8fe2c5443121753f4d70255'
+  homepage "http://opencore-amr.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/opencore-amr/vo-aacenc/vo-aacenc-0.1.2.tar.gz"
+  sha1 "ac56325c05eba4c4f8fe2c5443121753f4d70255"
 
   bottle do
     cellar :any
@@ -14,6 +12,6 @@ class LibvoAacenc < Formula
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
