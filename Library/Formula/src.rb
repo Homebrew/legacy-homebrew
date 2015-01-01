24d6ae2ecfb07441dd3eb67e422f892f993ed9bc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-01 15:30:39 +0000

    src: add 0.18 bottle.

--- Library/Formula/src.rb
+++ Library/Formula/src.rb
@@ -3,6 +3,12 @@ class Src < Formula
   url "http://www.catb.org/~esr/src/src-0.18.tar.gz"
   sha1 "d4234bb6c56073204836ab23d18f36c1a732dd2c"
 
+  bottle do
+    cellar :any
+    sha1 "30728356bf11780f2c9c4894bd381b4b107f8661" => :yosemite
+    sha1 "bbabdfe74081f4624a487d845f20750b142c5b05" => :mavericks
+  end
+
   # test is failing on Mountain Lion
   depends_on :macos => :mavericks
 
