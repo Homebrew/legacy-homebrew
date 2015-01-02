bfa94d11227800678dd57ce1f9a7b988ae2c313a
Alexis Hildebrandt <afh@surryhill.net>
2015-01-02 18:13:31 +0100

    gwenhywfar 4.13.0
    Add openssl dependency as suggested by brew audit
    
    Closes #35471.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gwenhywfar.rb
+++ Library/Formula/gwenhywfar.rb
@@ -2,20 +2,15 @@ require 'formula'
 
 class Gwenhywfar < Formula
   homepage 'http://www.aqbanking.de/'
-  url 'http://www.aquamaniac.de/sites/download/download.php?package=01&release=67&file=01&dummy=gwenhywfar-4.3.3.tar.gz'
-  sha1 'c2ba4c45f1eeb379db6c2ae09122c592893f3bd0'
+  url 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=77&file=01&dummy=gwenhywfar-4.13.0.tar.gz'
+  sha1 'c4f37eb7fed069f3478e06a9311193a98cc9ddbf'
   head 'http://devel.aqbanking.de/svn/gwenhywfar/trunk'
-  revision 2
-
-  devel do
-    url 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=76&file=01&dummy=gwenhywfar-4.12.0beta.tar.gz'
-    sha1 '02fe19f12970cf94c495ba78a99f492f0a9067b0'
-  end
 
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
   depends_on 'gnutls'
   depends_on 'libgcrypt'
+  depends_on 'openssl'
   depends_on 'gtk+' => :optional
   depends_on 'qt' => :optional
 
