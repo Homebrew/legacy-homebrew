b6498aacf4acda3250783400095c52a807736216
Alexis Hildebrandt <afh@surryhill.net>
2015-01-02 18:33:48 +0100

    aqbanking 5.5.1

--- Library/Formula/aqbanking.rb
+++ Library/Formula/aqbanking.rb
@@ -3,25 +3,16 @@ require 'formula'
 class Aqbanking < Formula
   homepage 'http://www.aqbanking.de/'
   head 'http://devel.aqbanking.de/svn/aqbanking/trunk'
+  url 'http://www2.aquamaniac.de/sites/download/download.php?package=03&release=118&file=01&dummy=aqbanking-5.5.1.tar.gz'
+  sha1 '4783890253acf04dddede6d34bf81b8f1c24480d'
 
-  stable do
-    url 'http://www2.aquamaniac.de/sites/download/download.php?package=03&release=95&file=01&dummy=aqbanking-5.0.25.tar.gz'
-    sha1 '80314a6f6114a0a3f0062161bb38effc0f1f4b62'
-    depends_on 'gwenhywfar' # see caveats
-  end
-
-  devel do
-    url 'http://www2.aquamaniac.de/sites/download/download.php?package=03&release=115&file=01&dummy=aqbanking-5.4.3beta.tar.gz'
-    sha1 'd3d4dac73794227041c8ec4a777f00ac17efd8ca'
-
-    depends_on 'pkg-config' => :build
-    depends_on 'libxmlsec1'
-    depends_on 'libxslt'
-    depends_on 'libxml2'
-  end
-
+  depends_on 'gwenhywfar'
+  depends_on 'libxmlsec1'
+  depends_on 'libxslt'
+  depends_on 'libxml2'
   depends_on 'gettext'
   depends_on 'gmp'
+  depends_on 'pkg-config' => :build
   depends_on 'ktoblzcheck' => :recommended
 
   def install
@@ -32,16 +23,5 @@ class Aqbanking < Formula
                           "--enable-cli",
                           "--with-gwen-dir=#{HOMEBREW_PREFIX}"
     system "make install"
-  end
-
-  def caveats; <<-EOS.undent
-    To build the devel version install all the dependencies first,
-    then install the devel version of gwenhywfar separately,
-    and install the devel version of aqbanking afterwards.
-
-    brew install aqbanking --devel --only-dependencies
-    brew install gwenhywfar --devel
-    brew install aqbanking --devel
-    EOS
   end
 end
