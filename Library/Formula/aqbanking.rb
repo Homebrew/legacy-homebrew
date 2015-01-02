9efa4ef337074cd7d2ed4687ead7f857ed1bd138
Alexis Hildebrandt <afh@surryhill.net>
2015-01-02 19:21:51 +0100

    aqbanking: modernize
    * Replace single quotes with double quotes
    * Run make check during install
    * Add test
    
    Closes #35472.

--- Library/Formula/aqbanking.rb
+++ Library/Formula/aqbanking.rb
@@ -1,19 +1,17 @@
-require 'formula'
-
 class Aqbanking < Formula
-  homepage 'http://www.aqbanking.de/'
-  head 'http://devel.aqbanking.de/svn/aqbanking/trunk'
-  url 'http://www2.aquamaniac.de/sites/download/download.php?package=03&release=118&file=01&dummy=aqbanking-5.5.1.tar.gz'
-  sha1 '4783890253acf04dddede6d34bf81b8f1c24480d'
+  homepage "http://www.aqbanking.de/"
+  head "http://devel.aqbanking.de/svn/aqbanking/trunk"
+  url "http://www2.aquamaniac.de/sites/download/download.php?package=03&release=118&file=01&dummy=aqbanking-5.5.1.tar.gz"
+  sha1 "4783890253acf04dddede6d34bf81b8f1c24480d"
 
-  depends_on 'gwenhywfar'
-  depends_on 'libxmlsec1'
-  depends_on 'libxslt'
-  depends_on 'libxml2'
-  depends_on 'gettext'
-  depends_on 'gmp'
-  depends_on 'pkg-config' => :build
-  depends_on 'ktoblzcheck' => :recommended
+  depends_on "gwenhywfar"
+  depends_on "libxmlsec1"
+  depends_on "libxslt"
+  depends_on "libxml2"
+  depends_on "gettext"
+  depends_on "gmp"
+  depends_on "pkg-config" => :build
+  depends_on "ktoblzcheck" => :recommended
 
   def install
     ENV.j1
@@ -22,6 +20,58 @@ class Aqbanking < Formula
                           "--prefix=#{prefix}",
                           "--enable-cli",
                           "--with-gwen-dir=#{HOMEBREW_PREFIX}"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    ENV["TZ"] = "UTC"
+    context = "balance.ctx"
+    (testpath/context).write <<-EOS.undent
+    accountInfoList {
+      accountInfo {
+        char bankCode="110000000"
+        char bankName="STRIPE TEST BANK"
+        char accountNumber="000123456789"
+        char accountName="demand deposit"
+        char iban="US44110000000000123456789"
+        char bic="BYLADEM1001"
+        char owner="JOHN DOE"
+        char currency="USD"
+        int  accountType="0"
+        int  accountId="2"
+
+        statusList {
+          status {
+            int  time="1388664000"
+
+            notedBalance {
+              value {
+                char value="123456%2F100"
+                char currency="USD"
+              } #value
+
+              int  time="1388664000"
+            } #notedBalance
+          } #status
+
+          status {
+            int  time="1388750400"
+
+            notedBalance {
+              value {
+                char value="132436%2F100"
+                char currency="USD"
+              } #value
+
+              int  time="1388750400"
+            } #notedBalance
+          } #status
+        } #statusList
+
+      } # accountInfo
+    } # accountInfoList
+    EOS
+    assert_match /^Account\s+110000000\s+000123456789\s+STRIPE TEST BANK\s+03.01.2014\s+12:00\s+1324.36\s+USD\s+$/, shell_output("#{bin}/aqbanking-cli listbal -c #{context}")
   end
 end
