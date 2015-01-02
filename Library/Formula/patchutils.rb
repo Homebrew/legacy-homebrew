f67314a82442fbdbde4f50400b7e6678f41fe704
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-02 05:48:57 +0000

    patchutils: add 0.3.3 bottle.

--- Library/Formula/patchutils.rb
+++ Library/Formula/patchutils.rb
@@ -3,6 +3,13 @@ class Patchutils < Formula
   url "http://cyberelk.net/tim/data/patchutils/stable/patchutils-0.3.3.tar.xz"
   sha1 "89d3f8a454bacede1b9a112b3a13701ed876fcc1"
 
+  bottle do
+    cellar :any
+    sha1 "e339d44549d5adaf0907752d87a15c74fdc14892" => :yosemite
+    sha1 "1d2ef386ca2d3f17574ecdb8ca2cf3dbde142296" => :mavericks
+    sha1 "56c838384a5712786d2a424a57ce69b168e1f66e" => :mountain_lion
+  end
+
   # Fix 'filterdiff --exclude-from-file...' crashes
   # https://fedorahosted.org/patchutils/ticket/30
   patch do
