11841e3633a9ab9ec6ea6c82456ad178608acdae
Xu Cheng <xucheng@me.com>
2015-01-02 15:41:08 +0800

    recutils: add test
    Closes #35452.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/recutils.rb
+++ Library/Formula/recutils.rb
@@ -1,13 +1,19 @@
-require 'formula'
-
 class Recutils < Formula
-  homepage 'http://www.gnu.org/software/recutils/'
-  url 'http://ftpmirror.gnu.org/recutils/recutils-1.7.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/recutils/recutils-1.7.tar.gz'
-  sha1 '20d265aecb05ca4e4072df9cfac08b1392da6919'
+  homepage "http://www.gnu.org/software/recutils/"
+  url "http://ftpmirror.gnu.org/recutils/recutils-1.7.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/recutils/recutils-1.7.tar.gz"
+  sha1 "20d265aecb05ca4e4072df9cfac08b1392da6919"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.csv").write <<-EOS.undent
+      a,b,c
+      1,2,3
+    EOS
+    system "#{bin}/csv2rec", "test.csv"
   end
 end
