c4e656a36ce79de0d01612bb8b63f765502fb603
Xu Cheng <xucheng@me.com>
2015-01-02 15:51:23 +0800

    icoutils: add test
    Closes #35453.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/icoutils.rb
+++ Library/Formula/icoutils.rb
@@ -1,17 +1,19 @@
-require 'formula'
-
 class Icoutils < Formula
-  homepage 'http://www.nongnu.org/icoutils/'
-  url 'http://savannah.nongnu.org/download/icoutils/icoutils-0.31.0.tar.bz2'
-  sha1 '2712acd33c611588793562310077efd2ff35dca5'
+  homepage "http://www.nongnu.org/icoutils/"
+  url "http://savannah.nongnu.org/download/icoutils/icoutils-0.31.0.tar.bz2"
+  sha1 "2712acd33c611588793562310077efd2ff35dca5"
   revision 1
 
-  depends_on 'libpng'
+  depends_on "libpng"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-rpath",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/icotool", "-l", test_fixtures("test.ico")
   end
 end
