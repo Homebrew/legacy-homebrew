3ff5b06e695198b5eb4459990fb6916a870f9a03
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-02 10:37:23 +0000

    xmp: update 4.0.10 bottle.

--- Library/Formula/xmp.rb
+++ Library/Formula/xmp.rb
@@ -4,9 +4,9 @@ class Xmp < Formula
   sha1 "e6a39a5158a9e36d437b233d68a8dc7756a971e1"
 
   bottle do
-    sha1 "f9b3c62d4952cd0ede1c131dcdf973903316d87d" => :mavericks
-    sha1 "6adf877d9f4cd3c1b96da42f806724a9801ca311" => :mountain_lion
-    sha1 "4ad77815e84f8251d2c06fc72fa5ecfbf0447b19" => :lion
+    sha1 "fb720ca09235acac04666f9661eb106717450f11" => :yosemite
+    sha1 "141425e9760daeb055ee61829ed2ffcf7a20b65a" => :mavericks
+    sha1 "4bcc80d2356c0a89f291d592c711c449882d8254" => :mountain_lion
   end
 
   head do
