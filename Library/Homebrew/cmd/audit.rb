2e37af87683befdf9bf4b2a45efd09c3607b58ec
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-02 11:11:26 +0100

    audit: system check fixed for multiple arguments
    Closes #35459.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Homebrew/cmd/audit.rb
+++ Library/Homebrew/cmd/audit.rb
@@ -552,7 +552,7 @@ class FormulaAuditor
     end
 
     if @strict
-      if line =~ /system (["'][^"' ]*\s[^"' ]*["'])/
+      if line =~ /system (["'][^"' ]*(?:\s[^"' ]*)+["'])/
         bad_system = $1
         good_system = bad_system.gsub(" ", "\", \"")
         problem "Use `system #{good_system}` instead of `system #{bad_system}` "
