d66ea2abdfceaf60be04dbfcc6fc0ccc528d8ad8
Xu Cheng <xucheng@me.com>
2015-01-02 16:12:57 +0800

    xml-coreutils: add test
    Closes #35454.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/xml-coreutils.rb
+++ Library/Formula/xml-coreutils.rb
@@ -1,15 +1,20 @@
-require 'formula'
-
 class XmlCoreutils < Formula
-  homepage 'http://www.lbreyer.com/xml-coreutils.html'
-  url 'http://www.lbreyer.com/gpl/xml-coreutils-0.8.1.tar.gz'
-  sha1 'fd73b2a087ea1c630571070b1ab4ea3b40138a46'
+  homepage "http://www.lbreyer.com/xml-coreutils.html"
+  url "http://www.lbreyer.com/gpl/xml-coreutils-0.8.1.tar.gz"
+  sha1 "fd73b2a087ea1c630571070b1ab4ea3b40138a46"
 
-  depends_on 's-lang'
+  depends_on "s-lang"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.xml").write <<-EOS.undent
+      <hello>world!</hello>
+    EOS
+    assert_match /0\s+1\s+1/, shell_output("#{bin}/xml-wc test.xml")
   end
 end
