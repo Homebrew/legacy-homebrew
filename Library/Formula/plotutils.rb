c4a9aacf61f13629b8b3ee4db70cda09ba456c33
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-02 09:53:27 +0000

    plotutils: add 2.6_1 bottle.

--- Library/Formula/plotutils.rb
+++ Library/Formula/plotutils.rb
@@ -5,6 +5,13 @@ class Plotutils < Formula
   sha1 "7921301d9dfe8991e3df2829bd733df6b2a70838"
   revision 1
 
+  bottle do
+    cellar :any
+    sha1 "e77297eb70e65c76f79064fe0b1649200812d90d" => :yosemite
+    sha1 "26e33cc79cfe51e36c3952ea4f541183fbb0605b" => :mavericks
+    sha1 "224bcf0989a50cc12c2eb0890b81c733926e695c" => :mountain_lion
+  end
+
   depends_on "libpng"
   depends_on :x11 => :optional
 
