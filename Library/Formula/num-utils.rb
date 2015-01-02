b791b8193a7c138853f91577798ab0968cffac1e
Xu Cheng <xucheng@me.com>
2015-01-02 13:56:41 +0800

    num-utils: add test
    Closes #35447.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/num-utils.rb
+++ Library/Formula/num-utils.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class NumUtils < Formula
-  homepage 'http://suso.suso.org/programs/num-utils/'
+  homepage "http://suso.suso.org/programs/num-utils/"
   url "http://suso.suso.org/programs/num-utils/downloads/num-utils-0.5.tar.gz"
-  sha1 '3fc6130874129fe1e98db6db8b3dc43f0e1a89ac'
+  sha1 "3fc6130874129fe1e98db6db8b3dc43f0e1a89ac"
 
-  conflicts_with 'normalize', :because => 'both install `normalize` binaries'
+  conflicts_with "normalize", :because => "both install `normalize` binaries"
 
   def install
     %w(average bound interval normalize numgrep numprocess numsum random range round).each do |p|
@@ -13,5 +11,9 @@ class NumUtils < Formula
       bin.install p
       man1.install "#{p}.1"
     end
+  end
+
+  test do
+    assert_equal "2", pipe_output("#{bin}/average", "1\n2\n3\n").strip()
   end
 end
