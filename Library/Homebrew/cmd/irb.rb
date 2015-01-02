f83813f3cb9d0518c7aa2e96d7ad1bba2455ac8a
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-02 15:11:24 +0100

    brew irb --help replaced with --examples
    The --help flag is intercepted by brew.rb and prints the global usage
    string. `irb` is never called when `brew irb --help` is executed.
    
    Closes #35466.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Homebrew/cmd/irb.rb
+++ Library/Homebrew/cmd/irb.rb
@@ -15,14 +15,14 @@ end
 
 module Homebrew
   def irb
-    if ARGV.include? "--help"
+    if ARGV.include? "--examples"
       puts "'v8'.f # => instance of the v8 formula"
       puts ":hub.f.installed?"
       puts ":lua.f.methods - 1.methods"
       puts ":mpd.f.recursive_dependencies.reject(&:installed?)"
     else
       ohai "Interactive Homebrew Shell"
-      puts "Example commands available with: brew irb --help"
+      puts "Example commands available with: brew irb --examples"
       IRB.start
     end
   end
