136e733a20da5a7424e60955bfee71a916d913c7
Alexis Hildebrandt <afh@surryhill.net>
2015-01-02 18:24:02 +0100

    gwenhywfar: modernize
    Closes #35471.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gwenhywfar.rb
+++ Library/Formula/gwenhywfar.rb
@@ -1,20 +1,19 @@
-require 'formula'
-
 class Gwenhywfar < Formula
-  homepage 'http://www.aqbanking.de/'
-  url 'http://www2.aquamaniac.de/sites/download/download.php?package=01&release=77&file=01&dummy=gwenhywfar-4.13.0.tar.gz'
-  sha1 'c4f37eb7fed069f3478e06a9311193a98cc9ddbf'
-  head 'http://devel.aqbanking.de/svn/gwenhywfar/trunk'
+  homepage "http://www.aqbanking.de/"
+  url "http://www2.aquamaniac.de/sites/download/download.php?package=01&release=77&file=01&dummy=gwenhywfar-4.13.0.tar.gz"
+  sha1 "c4f37eb7fed069f3478e06a9311193a98cc9ddbf"
+  head "http://devel.aqbanking.de/svn/gwenhywfar/trunk"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gettext'
-  depends_on 'gnutls'
-  depends_on 'libgcrypt'
-  depends_on 'openssl'
-  depends_on 'gtk+' => :optional
-  depends_on 'qt' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gnutls"
+  depends_on "openssl"
+  depends_on "libgcrypt"
+  depends_on "gtk+" => :optional
+  depends_on "qt" => :optional
 
-  option 'without-cocoa', 'Build without cocoa support'
+  option "without-cocoa", "Build without cocoa support"
+  option "with-check", "Run build-time check"
 
   def install
     guis = []
@@ -25,7 +24,8 @@ class Gwenhywfar < Formula
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--with-guis=#{guis.join(' ')}"
-    system "make install"
+                          "--with-guis=#{guis.join(" ")}"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
   end
 end
