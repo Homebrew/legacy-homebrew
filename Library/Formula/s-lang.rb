6d13da0a9726ba38cf6bcd29f6ffb02159f1b20d
Xu Cheng <xucheng@me.com>
2015-01-02 16:19:43 +0800

    s-lang: modernize

--- Library/Formula/s-lang.rb
+++ Library/Formula/s-lang.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class SLang < Formula
-  homepage 'http://www.jedsoft.org/slang/'
-  url 'ftp://space.mit.edu/pub/davis/slang/v2.2/slang-2.2.4.tar.bz2'
-  mirror 'http://pkgs.fedoraproject.org/repo/pkgs/slang/slang-2.2.4.tar.bz2/7fcfd447e378f07dd0c0bae671fe6487/slang-2.2.4.tar.bz2'
-  sha1 '34e68a993888d0ae2ebc7bc31b40bc894813a7e2'
+  homepage "http://www.jedsoft.org/slang/"
+  url "ftp://space.mit.edu/pub/davis/slang/v2.2/slang-2.2.4.tar.bz2"
+  mirror "http://pkgs.fedoraproject.org/repo/pkgs/slang/slang-2.2.4.tar.bz2/7fcfd447e378f07dd0c0bae671fe6487/slang-2.2.4.tar.bz2"
+  sha1 "34e68a993888d0ae2ebc7bc31b40bc894813a7e2"
   revision 1
 
   bottle do
@@ -14,9 +12,9 @@ class SLang < Formula
     sha1 "274fdf93326cb429e850181a58ae13a85bfa5c4c" => :mountain_lion
   end
 
-  depends_on 'libpng'
-  depends_on 'pcre' => :optional
-  depends_on 'oniguruma' => :optional
+  depends_on "libpng"
+  depends_on "pcre" => :optional
+  depends_on "oniguruma" => :optional
 
   def install
     png = Formula["libpng"]
@@ -25,6 +23,6 @@ class SLang < Formula
                           "--with-pnginc=#{png.include}"
     ENV.j1
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
