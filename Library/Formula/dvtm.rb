938cfdf45566dae7d5d0c5893a18f7f2c3c99cba
Alexis Hildebrandt <afh@surryhill.net>
2015-01-03 07:39:47 +0100

    dvtm: modernize

--- Library/Formula/dvtm.rb
+++ Library/Formula/dvtm.rb
@@ -1,5 +1,3 @@
-require "formula"
-
 class Dvtm < Formula
   homepage "http://www.brain-dump.org/projects/dvtm/"
   url "http://www.brain-dump.org/projects/dvtm/dvtm-0.13.tar.gz"
@@ -9,5 +7,11 @@ class Dvtm < Formula
   def install
     ENV.append_to_cflags "-D_DARWIN_C_SOURCE"
     system "make", "PREFIX=#{prefix}", "LIBS=-lc -lutil -lncurses", "install"
+  end
+
+  test do
+    result = shell_output("#{bin}/dvtm -v")
+    result.force_encoding("UTF-8") if result.respond_to?(:force_encoding)
+    assert_match /^dvtm-[0-9.]+ © 2007-\d{4} Marc André Tanner$/, result
   end
 end
