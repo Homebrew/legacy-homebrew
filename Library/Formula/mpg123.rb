336e5be909afecaeadbca4e88118080709cb4ebb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-03 23:10:06 +0000

    mpg123: update 1.21.0 bottle.

--- Library/Formula/mpg123.rb
+++ Library/Formula/mpg123.rb
@@ -6,9 +6,9 @@ class Mpg123 < Formula
 
   bottle do
     cellar :any
-    sha1 "30a095c10283c8344e89fe7014b2ab7858a49844" => :yosemite
-    sha1 "38109b9978996107392423313751c9c5c31a0077" => :mavericks
-    sha1 "d5dd10fcc5342bdeaf737b2e24cd5c1e70682585" => :mountain_lion
+    sha1 "79906a0fc98440a39b3e858d3d4e52a308f05739" => :yosemite
+    sha1 "5ce299d371755450153e411fd1cc812a1ec82dd4" => :mavericks
+    sha1 "424a07fe1cb44ed42cbf6971e6722da38cc18554" => :mountain_lion
   end
 
   def install
