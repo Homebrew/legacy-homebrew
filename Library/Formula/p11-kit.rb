02cc54febc86c267b275990818c36199325a8b13
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-03 15:19:43 +0000

    p11-kit: add 0.18.4 bottle.

--- Library/Formula/p11-kit.rb
+++ Library/Formula/p11-kit.rb
@@ -3,6 +3,12 @@ class P11Kit < Formula
   url "http://p11-glue.freedesktop.org/releases/p11-kit-0.18.4.tar.gz"
   sha256 "df5424ec39e17c2b3b98819bf772626e9b8c73871a8b82e54151f6297d8575fd"
 
+  bottle do
+    sha1 "1f75969e6cbf89921ba4d205e093c6d7bddd755e" => :yosemite
+    sha1 "4baec782e0d55aacb458bcd752d32f4d63de4424" => :mavericks
+    sha1 "1f16ca29e9c9b38ec832ce04c3802d439c2ba896" => :mountain_lion
+  end
+
   option :universal
 
   depends_on "pkg-config" => :build
