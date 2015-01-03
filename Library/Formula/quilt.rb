d9f5a3317ef08738ca517429d18bf311871fe517
Xu Cheng <xucheng@me.com>
2015-01-03 16:28:02 +0800

    quilt: modernize

--- Library/Formula/quilt.rb
+++ Library/Formula/quilt.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Quilt < Formula
-  homepage 'http://savannah.nongnu.org/projects/quilt'
-  url 'http://download.savannah.gnu.org/releases/quilt/quilt-0.63.tar.gz'
-  sha1 '19f2ba0384521eb3d8269b8a1097b16b07339be5'
+  homepage "http://savannah.nongnu.org/projects/quilt"
+  url "http://download.savannah.gnu.org/releases/quilt/quilt-0.63.tar.gz"
+  sha1 "19f2ba0384521eb3d8269b8a1097b16b07339be5"
 
   bottle do
     sha1 "04771a84ee741ad82390373e126db9969cb1abae" => :mavericks
@@ -11,14 +9,14 @@ class Quilt < Formula
     sha1 "1e30a1d5d3df8af87ae2b34e1423aec0761992a9" => :lion
   end
 
-  depends_on 'gnu-sed'
-  depends_on 'coreutils'
+  depends_on "gnu-sed"
+  depends_on "coreutils"
 
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--with-sed=#{HOMEBREW_PREFIX}/bin/gsed",
                           "--without-getopt"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
