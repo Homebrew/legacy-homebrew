ddf09d19395b3da2dca31850f20be1fe8fbf11ce
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-03 20:33:29 +0000

    png2ico: add 2002-1_1 bottle.

--- Library/Formula/png2ico.rb
+++ Library/Formula/png2ico.rb
@@ -2,6 +2,13 @@ class Png2ico < Formula
   homepage "http://www.winterdrache.de/freeware/png2ico/"
   url "http://www.winterdrache.de/freeware/png2ico/data/png2ico-src-2002-12-08.tar.gz"
   sha1 "955004bee9a20f12b225aa01895762cbbafaeb28"
+  bottle do
+    cellar :any
+    sha1 "b3c0353afa9ea55e8dc9e8f41eeaac8968f569d3" => :yosemite
+    sha1 "75edd65a4d22ec52b41010b2c8140e3fe20c0895" => :mavericks
+    sha1 "1d603b519c4067f20de399892d85fbcd834a9f39" => :mountain_lion
+  end
+
   revision 1
 
   depends_on "libpng"
