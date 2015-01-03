42211cd00f96d6124716c6b5db99cb07286d799d
Brett Koonce <koonce@gmail.com>
2015-01-02 23:35:47 -0600

    mpg123 1.21.0

--- Library/Formula/mpg123.rb
+++ Library/Formula/mpg123.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Mpg123 < Formula
   homepage "http://www.mpg123.de/"
-  url "https://downloads.sourceforge.net/project/mpg123/mpg123/1.20.1/mpg123-1.20.1.tar.bz2"
-  mirror "http://mpg123.orgis.org/download/mpg123-1.20.1.tar.bz2"
-  sha1 "5d7f9c27cbf258f258cb3ad7c17ebe5cda292bce"
+  url "https://downloads.sourceforge.net/project/mpg123/mpg123/1.21.0/mpg123-1.21.0.tar.bz2"
+  mirror "http://mpg123.orgis.org/download/mpg123-1.21.0.tar.bz2"
+  sha1 "a2fd84078632b7ab73ae4cd64c3f941d140167a8"
 
   bottle do
     cellar :any
