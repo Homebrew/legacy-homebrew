c8fb095fd19cd4f47f939fb5e97a61e78128d8f6
Josh Hagins <hagins.josh@gmail.com>
2015-01-03 12:32:32 -0500

    scalastyle: Update binary and config to 0.6.0

--- Library/Formula/scalastyle.rb
+++ Library/Formula/scalastyle.rb
@@ -1,25 +1,22 @@
-require "formula"
-
 class Scalastyle < Formula
   homepage "http://www.scalastyle.org/command-line.html"
-  url "https://oss.sonatype.org/content/repositories/releases/org/scalastyle/scalastyle-batch_2.10/0.5.0/scalastyle-batch_2.10-0.5.0-distribution.zip"
-  version "0.5.0"
-  sha1 "bf009e0c4d48e5effce5113f5feb3da626ca1248"
+  url "https://oss.sonatype.org/content/repositories/releases/org/scalastyle/scalastyle_2.10/0.6.0/scalastyle_2.10-0.6.0-batch.jar"
+  sha1 "01c744fa7eef25f7183beea5d603ce9658861fcf"
 
   resource "default_config" do
-    url "https://raw.githubusercontent.com/scalastyle/scalastyle/e41e5df2cf4b616d06d1f5b8c383ec084fc32012/lib/scalastyle_config.xml"
-    sha1 "993c4d665ae75930c262fcd8a13eef0e9604920f"
+    url "https://raw.githubusercontent.com/scalastyle/scalastyle/v0.6.0/lib/scalastyle_config.xml"
+    version "0.6.0"
+    sha1 "c22165ccda7ef285665041ad07a4b31a639a85c3"
   end
 
   def install
-    (libexec/"lib").install Dir["lib/*.jar"]
-    libexec.install "scalastyle-batch_2.10.jar"
+    libexec.install "scalastyle_2.10-0.6.0-batch.jar"
 
     etc.install resource("default_config")
 
     (bin/"scalastyle").write <<-EOS.undent
       #!/bin/sh
-      java -jar "#{libexec}/scalastyle-batch_2.10.jar" --config "#{etc}/scalastyle_config.xml" "$@"
+      java -jar "#{libexec}/scalastyle_2.10-0.6.0-batch.jar" --config "#{etc}/scalastyle_config.xml" "$@"
     EOS
   end
 
