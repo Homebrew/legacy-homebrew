79a9572ae1883abc6d248a9f3774ed4bdc3cba37
Brett Koonce <koonce@gmail.com>
2015-01-02 23:35:23 -0600

    libgetdata 0.8.6

--- Library/Formula/libgetdata.rb
+++ Library/Formula/libgetdata.rb
@@ -1,10 +1,7 @@
-require 'formula'
-
 class Libgetdata < Formula
-  homepage 'http://getdata.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/getdata/getdata/0.8.5/getdata-0.8.5.tar.bz2'
-  sha1 '4f509e10f9eb6e8cfc378efd8b483b2a2508917f'
-  revision 1
+  homepage "http://getdata.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/getdata/getdata/0.8.6/getdata-0.8.6.tar.bz2"
+  sha1 "57f12393916a658123c759d04ed88fe0348cbac6"
 
   bottle do
     sha1 "845fc0330d8e4ae33987e898c1cbf20d7217b847" => :yosemite
@@ -12,14 +9,14 @@ class Libgetdata < Formula
     sha1 "1787e29cddbfd694b15860d0af697657d0602210" => :mountain_lion
   end
 
-  option 'with-fortran', 'Build Fortran 77 bindings'
-  option 'with-perl', 'Build Perl binding'
-  option 'lzma', 'Build with LZMA compression support'
-  option 'zzip', 'Build with zzip compression support'
+  option "with-fortran", "Build Fortran 77 bindings"
+  option "with-perl", "Build Perl binding"
+  option "lzma", "Build with LZMA compression support"
+  option "zzip", "Build with zzip compression support"
 
   depends_on :fortran => :optional
-  depends_on 'xz' if build.include? 'lzma'
-  depends_on 'libzzip' if build.include? 'zzip'
+  depends_on "xz" if build.include? "lzma"
+  depends_on "libzzip" if build.include? "zzip"
 
 
   def install
