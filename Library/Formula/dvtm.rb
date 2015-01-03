61a0d69e010fb11012f1d600817613a1ddddaea9
Alexis Hildebrandt <afh@surryhill.net>
2015-01-03 07:39:47 +0100

    dvtm: modernize
    Closes #35470.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/dvtm.rb
+++ Library/Formula/dvtm.rb
@@ -1,4 +1,4 @@
-require "formula"
+# encoding: UTF-8
 
 class Dvtm < Formula
   homepage "http://www.brain-dump.org/projects/dvtm/"
@@ -9,5 +9,11 @@ class Dvtm < Formula
   def install
     ENV.append_to_cflags "-D_DARWIN_C_SOURCE"
     system "make", "PREFIX=#{prefix}", "LIBS=-lc -lutil -lncurses", "install"
+  end
+
+  test do
+    result = shell_output("#{bin}/dvtm -v")
+    result.force_encoding("UTF-8") if result.respond_to?(:force_encoding)
+    assert_match /^dvtm-[0-9.]+ © 2007-\d{4} Marc André Tanner$/, result
   end
 end
