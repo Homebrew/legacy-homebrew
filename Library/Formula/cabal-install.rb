23b4838a196e95aca264250047c72d4d8b267410
Alexander Altman <alexanderaltman@me.com>
2015-01-03 18:14:20 -0800

    Cabal-install no longer requires the version-swap workaround

--- Library/Formula/cabal-install.rb
+++ Library/Formula/cabal-install.rb
@@ -27,10 +27,6 @@ class CabalInstall < Formula
     inreplace "bootstrap.sh", "list --global",
       "list --global --no-user-package-db"
 
-    # Avoid a nasty bug in Cabal by forcing the bootstrap script to pull a later version.
-    # (q.v. https://github.com/haskell/cabal/issues/1740)
-    inreplace "bootstrap.sh", 'CABAL_VER="1.20.0.0";', 'CABAL_VER="1.20.0.3";'
-
     system "sh", "bootstrap.sh"
     bin.install "bin/cabal"
     bash_completion.install "bash-completion/cabal"
