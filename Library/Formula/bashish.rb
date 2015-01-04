4963c35e63597ea4a357e2c882bae982d6cf515b
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-04 15:31:08 +0100

    bashish: test added
    Closes #35544.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/bashish.rb
+++ Library/Formula/bashish.rb
@@ -1,15 +1,17 @@
-require 'formula'
-
 class Bashish < Formula
-  homepage 'http://bashish.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/bashish/bashish/2.2.4/bashish-2.2.4.tar.gz'
-  sha1 '532ed2a1c1bacafb459c2c7dedf720e9047bd716'
+  homepage "http://bashish.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/bashish/bashish/2.2.4/bashish-2.2.4.tar.gz"
+  sha1 "532ed2a1c1bacafb459c2c7dedf720e9047bd716"
 
-  depends_on 'dialog'
+  depends_on "dialog"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/bashish", "list"
   end
 end
