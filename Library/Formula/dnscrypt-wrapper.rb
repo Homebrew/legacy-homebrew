f63246004691112c9caef4dfeb1d9be9e1e807d1
Yecheng Fu <cofyc.jackson@gmail.com>
2015-01-06 00:03:53 +0800

    1. fix style 2. add test command to test it's runnable or not

--- Library/Formula/dnscrypt-wrapper.rb
+++ Library/Formula/dnscrypt-wrapper.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class DnscryptWrapper < Formula
   homepage "http://cofyc.github.io/dnscrypt-wrapper/"
-  head "https://github.com/Cofyc/dnscrypt-wrapper.git"
   url "https://github.com/Cofyc/dnscrypt-wrapper/releases/download/v0.1.15/dnscrypt-wrapper-v0.1.15.tar.bz2"
   sha256 "d486f3f923c6809c830e9db39290b0f44b1683f63f8bd3aeaa6225c64af232c1"
+  head "https://github.com/Cofyc/dnscrypt-wrapper.git"
 
   depends_on "autoconf" => :build
 
@@ -12,11 +10,14 @@ class DnscryptWrapper < Formula
   depends_on "libevent"
 
   def install
-    args = ["--prefix=#{prefix}"]
-
-    system "make configure"
-    system "./configure", *args
+    system "make", "configure"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/dnscrypt-wrapper", "--gen-provider-keypair"
+    system "#{bin}/dnscrypt-wrapper", "--gen-crypt-keypair"
   end
 end
