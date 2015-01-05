fe3abd713aa1e75a2ae25c2daf56a25687d24cfd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-05 16:18:54 +0000

    dnscrypt-wrapper: add 0.1.15 bottle.

--- Library/Formula/dnscrypt-wrapper.rb
+++ Library/Formula/dnscrypt-wrapper.rb
@@ -4,6 +4,13 @@ class DnscryptWrapper < Formula
   sha256 "d486f3f923c6809c830e9db39290b0f44b1683f63f8bd3aeaa6225c64af232c1"
   head "https://github.com/Cofyc/dnscrypt-wrapper.git"
 
+  bottle do
+    cellar :any
+    sha1 "6275cdc455d328a0b1201c0b4dd6543487b42715" => :yosemite
+    sha1 "384d364ee11cfb150b8072d303de6a7ed72ba450" => :mavericks
+    sha1 "db2434e57e8deb88e066cc575ca1052076156eec" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
 
   depends_on "libsodium"
