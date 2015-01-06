8deb47e2bf6c6ad007cd5c2d82edaf42b278698c
Alexis Hildebrandt <afh@surryhill.net>
2015-01-06 20:49:13 +0100

    wyrd: modernize
    * Remove obsolete require 'formula'
    * Replace single quotes with double quotes
    * Split system argument string
    * Add test
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/wyrd.rb
+++ Library/Formula/wyrd.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Wyrd < Formula
-  homepage 'http://pessimization.com/software/wyrd/'
-  url 'http://pessimization.com/software/wyrd/wyrd-1.4.6.tar.gz'
-  sha1 'aaeca29b844825f45aadcf5207f4d1c875e4dc82'
+  homepage "http://pessimization.com/software/wyrd/"
+  url "http://pessimization.com/software/wyrd/wyrd-1.4.6.tar.gz"
+  sha1 "aaeca29b844825f45aadcf5207f4d1c875e4dc82"
 
   bottle do
     sha1 "dde6d73145bc484a9f0f62c909c20b26f5029e8d" => :yosemite
@@ -11,9 +9,9 @@ class Wyrd < Formula
     sha1 "dc2c60f856f3a7b7751a169c4c1ad2e860612af7" => :mountain_lion
   end
 
-  depends_on 'remind'
-  depends_on 'objective-caml' => :build
-  depends_on 'camlp4' => :build
+  depends_on "remind"
+  depends_on "objective-caml" => :build
+  depends_on "camlp4" => :build
 
   def install
     ENV.deparallelize
@@ -21,6 +19,10 @@ class Wyrd < Formula
                           "--prefix=#{prefix}",
                           "--enable-utf8"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "REM Jan 1 2015 AT 00:01 MSG Happy New Year!\n", shell_output("#{bin}/wyrd --add 'Happy New Year! on 2015-01-1 at 00:01' /dev/stdout")
   end
 end
