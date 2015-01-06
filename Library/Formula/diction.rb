00f27cb5124de5296577c509f6e1226fc943c1ba
Alexis Hildebrandt <afh@surryhill.net>
2015-01-06 20:45:03 +0100

    diction: modernize
    * Remove obsolete require 'formula'
    * Replace single quotes with double quotes
    * Split system argument string
    * Add test
    
    Closes #35615.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/diction.rb
+++ Library/Formula/diction.rb
@@ -1,14 +1,19 @@
-require 'formula'
-
 class Diction < Formula
-  homepage 'http://www.gnu.org/software/diction/'
-  url 'http://ftpmirror.gnu.org/diction/diction-1.11.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/diction/diction-1.11.tar.gz'
-  sha1 '30c7c778959120d30fa67be9261d41de894f498b'
+  homepage "https://www.gnu.org/software/diction/"
+  url "http://ftpmirror.gnu.org/diction/diction-1.11.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/diction/diction-1.11.tar.gz"
+  sha1 "30c7c778959120d30fa67be9261d41de894f498b"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    file = "test.txt"
+    (testpath/file).write "The quick brown fox jumps over the lazy dog."
+    assert_match /^.*35 characters.*9 words.*$/m, shell_output("#{bin}/style #{file}")
+    assert_match /No phrases in 1 sentence found./, shell_output("#{bin}/diction #{file}")
   end
 end
