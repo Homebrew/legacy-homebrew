d2e9cdebf5a1f5feecf95512db8ca7a2ffecfd93
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-06 17:27:31 +0000

    apr: add 1.5.1 bottle.

--- Library/Formula/apr.rb
+++ Library/Formula/apr.rb
@@ -3,6 +3,12 @@ class Apr < Formula
   url "http://www.apache.org/dyn/closer.cgi?path=apr/apr-1.5.1.tar.bz2"
   sha1 "f94e4e0b678282e0704e573b5b2fe6d48bd1c309"
 
+  bottle do
+    sha1 "dd876e8523f31e935a605c3cb710d681aa762ae7" => :yosemite
+    sha1 "881def6c18b664e2d3cc88f1e8c975fb314bb105" => :mavericks
+    sha1 "48905e0fc63b2ae63cd3fd0912cec8ea09441420" => :mountain_lion
+  end
+
   keg_only :provided_by_osx, "Apple's CLT package contains apr."
 
   def install
