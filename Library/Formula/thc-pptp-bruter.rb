6d9435f9857d3323e7185bd9caf4f3ad640297c6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-06 14:31:45 +0000

    thc-pptp-bruter: add 0.1.4 bottle.

--- Library/Formula/thc-pptp-bruter.rb
+++ Library/Formula/thc-pptp-bruter.rb
@@ -3,6 +3,13 @@ class ThcPptpBruter < Formula
   url "https://freeworld.thc.org/releases/thc-pptp-bruter-0.1.4.tar.gz"
   sha1 "88a797ed0dcfb79aba92b319e29d1c5d8c1b14a4"
 
+  bottle do
+    cellar :any
+    sha1 "1693dcceb59ea85f265be800af5615d75dd217b0" => :yosemite
+    sha1 "e75d5e3c5eb439522a3896956918ad0f1f676907" => :mavericks
+    sha1 "febbca57781732342980f6e52c3dad393cc43b30" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
