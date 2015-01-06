8685f1d3bb9a34fd9d0e682ff864e65fd9b54a11
David Kocher <dkocher@sudo.ch>
2015-01-06 22:29:03 +0100

    Rewrite formula to build from source tarball

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -1,25 +1,20 @@
-require "formula"
-
 class Duck < Formula
   homepage "https://duck.sh/"
-  # Reference to the CLI version of Cyberduck
-  url "https://update.cyberduck.io/cli//duck-4.6.2.16269.tar.gz"
-  version "4.6.2.16269"
-  sha1 "c417af3c8069ac5a3dc58329eaf2323234925cb5"
+  url "https://update.cyberduck.io/src/cyberduck-src-16276.tar.gz"
+  head "https://svn.cyberduck.io/trunk/"
+
+  depends_on :java => :build
+  depends_on :xcode => :build
+  depends_on "ant" => :build
+  depends_on "openssl" => :recommended
 
   def install
-    # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
-    libexec.install Dir['*']
+    system "ant", "-Drevision=16276", "cli"
+    libexec.install Dir["build/duck.bundle/*"]
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
 
   test do
     system "#{bin}/duck", "-version"
-  end
-
-  def caveats;
-    <<-EOS.undent
-    For more information refer to https://duck.sh/
-    EOS
   end
 end
