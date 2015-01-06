f5e39c1a12d7bd329026cb226e58ffb3c41ce55a
kn1kn1 <kn1kn1@users.noreply.github.com>
2015-01-06 23:55:51 +0900

    Change to double-quotes

--- Library/Formula/cfdg.rb
+++ Library/Formula/cfdg.rb
@@ -1,18 +1,18 @@
 class Cfdg < Formula
-  homepage 'http://www.contextfreeart.org/'
-  url 'http://www.contextfreeart.org/download/ContextFreeSource3.0.8.tgz'
-  sha1 'bbd90c74fd0e66cb071c342da52ad016e023da2b'
+  homepage "http://www.contextfreeart.org/"
+  url "http://www.contextfreeart.org/download/ContextFreeSource3.0.8.tgz"
+  sha1 "bbd90c74fd0e66cb071c342da52ad016e023da2b"
 
-  depends_on 'bison' => :build
-  depends_on 'libpng'
+  depends_on "bison" => :build
+  depends_on "libpng"
 
   def install
-    system 'make'
-    bin.install 'cfdg'
+    system "make"
+    bin.install "cfdg"
   end
 
   test do
-    (testpath/'test.cfdg').write <<-EOS.undent
+    (testpath/"test.cfdg").write <<-EOS.undent
     startshape SPIKE
 
     CF::Symmetry = CF::Dihedral, 6
@@ -29,6 +29,6 @@ class Cfdg < Formula
       SPIKE [y 0.95 s 0.97]
     }
     EOS
-    system 'cfdg', '-s', '700', 'test.cfdg', 'test.cfdg.png'
+    system "cfdg", "-s", "700", "test.cfdg", "test.cfdg.png"
   end
 end
