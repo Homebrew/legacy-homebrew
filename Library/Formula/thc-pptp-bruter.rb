18830d2c9fbbd7a11b4b4c0a1562b85deb53c612
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-01-06 13:54:54 +0100

    thc-pptp-bruter: general cleanup.
    Use https, test added, openssl dependency, modernizing.
    
    Closes #35603.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/thc-pptp-bruter.rb
+++ Library/Formula/thc-pptp-bruter.rb
@@ -1,17 +1,21 @@
-require 'formula'
-
 class ThcPptpBruter < Formula
-  homepage 'http://thc.org'
-  url 'http://freeworld.thc.org/releases/thc-pptp-bruter-0.1.4.tar.gz'
-  sha1 '88a797ed0dcfb79aba92b319e29d1c5d8c1b14a4'
+  homepage "https://thc.org"
+  url "https://freeworld.thc.org/releases/thc-pptp-bruter-0.1.4.tar.gz"
+  sha1 "88a797ed0dcfb79aba92b319e29d1c5d8c1b14a4"
+
+  depends_on "openssl"
 
   def install
     # The function openpty() is defined in pty.h on Linux, but in util.h on OS X.
-    # See http://groups.google.com/group/sage-devel/msg/97916255b631e3e5
-    inreplace 'src/pptp_bruter.c', 'pty.h', 'util.h'
+    # See https://groups.google.com/group/sage-devel/msg/97916255b631e3e5
+    inreplace "src/pptp_bruter.c", "pty.h", "util.h"
 
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/thc-pptp-bruter"
   end
 end
