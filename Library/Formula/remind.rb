a08156bf4209d59cc9a010e74bf341335ea98542
Alexis Hildebrandt <afh@surryhill.net>
2015-01-06 20:48:56 +0100

    remind: modernize
    * Remove obsolete require 'formula'
    * Replace single quotes with double quotes
    * Split system argument string
    * Add test
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/remind.rb
+++ Library/Formula/remind.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Remind < Formula
-  homepage 'http://www.roaringpenguin.com/products/remind'
-  url 'http://www.roaringpenguin.com/files/download/remind-03.01.13.tar.gz'
-  sha1 'dce46b2334b3849255feffe6cba4973f3c883647'
+  homepage "http://www.roaringpenguin.com/products/remind"
+  url "http://www.roaringpenguin.com/files/download/remind-03.01.13.tar.gz"
+  sha1 "dce46b2334b3849255feffe6cba4973f3c883647"
 
   def install
     # Remove unnecessary sleeps when running on Apple
@@ -14,6 +12,12 @@ class Remind < Formula
     end
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    reminders = "reminders"
+    (testpath/reminders).write "ONCE 2015-01-01 Homebrew Test"
+    assert_equal "Reminders for Thursday, 1st January, 2015:\n\nHomebrew Test\n\n", shell_output("#{bin}/remind #{reminders} 2015-01-01")
   end
 end
