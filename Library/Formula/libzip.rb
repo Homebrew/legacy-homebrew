ecc1f1bd748a9d894c96b84c755163da244637a4
Xu Cheng <xucheng@me.com>
2015-01-06 14:07:14 +0800

    libzip: modernize

--- Library/Formula/libzip.rb
+++ Library/Formula/libzip.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Libzip < Formula
-  homepage 'http://www.nih.at/libzip/'
-  url 'http://www.nih.at/libzip/libzip-0.11.2.tar.gz'
-  sha1 'eeb3b5567fcf3532fa4bcb6440a87c7ad8507d2d'
+  homepage "http://www.nih.at/libzip/"
+  url "http://www.nih.at/libzip/libzip-0.11.2.tar.gz"
+  sha1 "eeb3b5567fcf3532fa4bcb6440a87c7ad8507d2d"
 
   bottle do
     cellar :any
@@ -20,6 +18,6 @@ class Libzip < Formula
                           "--mandir=#{man}",
                           "CXX=#{ENV.cxx}",
                           "CXXFLAGS=#{ENV.cflags}"
-    system "make install"
+    system "make", "install"
   end
 end
