b21884bdd1cf9fea4b60793f669fd7a9ce576c45
David Kocher <dkocher@sudo.ch>
2015-01-07 13:28:11 +0100

    Add checksum and explicit version with update to 4.6.2.16295

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -1,7 +1,9 @@
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://update.cyberduck.io/src/cyberduck-src-16276.tar.gz"
+  url "https://update.cyberduck.io/src/cyberduck-src-16295.tar.gz"
+  version "4.6.2.16295"
   head "https://svn.cyberduck.io/trunk/"
+  sha1 "45a6549c1af367b65cbdf38dd6a45499f64f117f"
 
   depends_on :java => :build
   depends_on :xcode => :build
