316fe7e574312b604e01fe5ca880e6c80a2795ee
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-07 16:32:15 +0000

    lout: add 3.40 bottle.

--- Library/Formula/lout.rb
+++ Library/Formula/lout.rb
@@ -3,6 +3,12 @@ class Lout < Formula
   url "http://download.savannah.gnu.org/releases/lout/lout-3.40.tar.gz"
   sha1 "adb7f632202319a370eaada162fa52cf334f40b3"
 
+  bottle do
+    sha1 "104638e651e1978c63efe05e4fc7a862c508bc04" => :yosemite
+    sha1 "8b523c9ebd004613a18bc212d840532db4089f40" => :mavericks
+    sha1 "7743b80e1fae54efe71f90c7a9d632a3dfbdcfc0" => :mountain_lion
+  end
+
   def install
     bin.mkpath
     man1.mkpath
