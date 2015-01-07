13ac5b2358e5b80294443af5af37c59ba5009021
Kirill A. Korinskiy <catap@catap.ru>
2015-01-07 14:15:46 +0700

    Add HEAD to the namebench, fix build, fix geoip and add compatible with Safari 8

--- Library/Formula/namebench.rb
+++ Library/Formula/namebench.rb
@@ -5,6 +5,15 @@ class Namebench < Formula
   url 'https://namebench.googlecode.com/files/namebench-1.3.1-source.tgz'
   sha1 '2e6ca5a4f20512cb967c5ac43b023cc38c271131'
 
+  head do
+    url 'http://namebench.googlecode.com/svn/trunk/'
+
+    patch :p0 do
+      url 'https://gist.githubusercontent.com/catap/888d3da014f0d8073401/raw/746f0aea15bc3791cf96234d6b79ad1bd2be4887/patch.diff'
+      sha1 '43bb7f717c2cc188500615f833c131dae08962f1'
+    end
+  end
+
   option 'no-third-party', 'Do not install bundled third-party modules'
 
   def install
