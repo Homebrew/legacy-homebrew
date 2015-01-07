de0c91ea4aed0add99fe7b756def85d933b48dd7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-07 17:35:46 +0000

    libhdhomerun: add 20141210 bottle.

--- Library/Formula/libhdhomerun.rb
+++ Library/Formula/libhdhomerun.rb
@@ -5,6 +5,13 @@ class Libhdhomerun < Formula
   url "http://download.silicondust.com/hdhomerun/libhdhomerun_20141210.tgz"
   sha1 "4f6827e17f8f79401f272f62089352fe01eae740"
 
+  bottle do
+    cellar :any
+    sha1 "2c58f3fd30b8ea2f03c5f0edc051c6b0cdc9ca14" => :yosemite
+    sha1 "cc8190e5256ae8ba11041449e98b7c85ae2565a2" => :mavericks
+    sha1 "7e96bccea269523447a15356e8cb65216365ca99" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "hdhomerun_config"
