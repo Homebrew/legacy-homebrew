c2f4313787a09188e94e253710f9d7a31aa738de
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-07 16:32:14 +0000

    diction: add 1.11 bottle.

--- Library/Formula/diction.rb
+++ Library/Formula/diction.rb
@@ -4,6 +4,12 @@ class Diction < Formula
   mirror "https://ftp.gnu.org/gnu/diction/diction-1.11.tar.gz"
   sha1 "30c7c778959120d30fa67be9261d41de894f498b"
 
+  bottle do
+    sha1 "5a3520b96c45b01b39c14eb55537780fdbaeb830" => :yosemite
+    sha1 "cc7d45e7d240f0eef22dfcda475ee9daac382c36" => :mavericks
+    sha1 "23b2b3ebafb4434c0097cef9deef057ce1eec850" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
