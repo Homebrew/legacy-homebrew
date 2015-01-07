392afa14ad890b039a01f65a8892f81439f8d4cc
David Kocher <dkocher@sudo.ch>
2015-01-07 16:54:55 +0100

    Update to 4.6.2.16304

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -1,9 +1,9 @@
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://update.cyberduck.io/src/cyberduck-src-16295.tar.gz"
-  version "4.6.2.16295"
+  url "https://update.cyberduck.io/src/cyberduck-src-16304.tar.gz"
+  version "4.6.2.16304"
   head "https://svn.cyberduck.io/trunk/"
-  sha1 "45a6549c1af367b65cbdf38dd6a45499f64f117f"
+  sha1 "50a9bafe9774137d63bd8aa91804789b225ce9fc"
 
   depends_on :java => ["1.7", :build]
   depends_on :xcode => :build
@@ -11,7 +11,7 @@ class Duck < Formula
   depends_on "openssl"
 
   def install
-    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=16295", "cli"
+    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=16304", "cli"
     system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/libPrime.dylib"
     system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/librococoa.dylib"
     libexec.install Dir["build/duck.bundle/*"]
