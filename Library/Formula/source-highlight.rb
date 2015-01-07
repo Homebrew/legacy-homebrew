0214a77755cdc2cb31093f75dcb731517884bb69
Zach Pomerantz <zmp@umich.edu>
2015-01-07 00:05:42 -0800

    source-highlight: use mirror from different domain
    Closes #35625.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/source-highlight.rb
+++ Library/Formula/source-highlight.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class SourceHighlight < Formula
-  homepage 'http://www.gnu.org/software/src-highlite/'
-  url 'http://ftpmirror.gnu.org/src-highlite/source-highlight-3.1.7.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/src-highlite/source-highlight-3.1.7.tar.gz'
+  homepage 'https://www.gnu.org/software/src-highlite/'
+  url 'https://ftpmirror.gnu.org/src-highlite/source-highlight-3.1.7.tar.gz'
+  mirror 'https://ftp.gnu.org/gnu/src-highlite/source-highlight-3.1.7.tar.gz'
+  mirror 'http://mirror.anl.gov/pub/gnu/src-highlite/source-highlight-3.1.7.tar.gz'
   sha1 '71c637548be71afc3f895b0d8ada1a72a8dab4a0'
 
   depends_on 'boost'
