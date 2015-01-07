7fe8a7dbd8cc45dc5970c27cb3303d1d9c198732
Kazuhiro Sera <seratch@gmail.com>
2015-01-08 07:54:02 +0900

    skinny 1.3.10
    Closes #35641.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/skinny.rb
+++ Library/Formula/skinny.rb
@@ -10,8 +10,8 @@ end
 
 class Skinny < Formula
   homepage "http://skinny-framework.org/"
-  url "https://github.com/skinny-framework/skinny-framework/releases/download/1.3.9/skinny-1.3.9.tar.gz"
-  sha1 "9f32e1d010d2756bb66afd03f3877a2324171b64"
+  url "https://github.com/skinny-framework/skinny-framework/releases/download/1.3.10/skinny-1.3.10.tar.gz"
+  sha1 "08b42b3bacf6606f1563b50f8c4c589e339f644c"
 
   depends_on "node"
   depends_on UniversalNpm
