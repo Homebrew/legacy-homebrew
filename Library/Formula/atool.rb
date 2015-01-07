3c4fd0f72ab1610ee6b24bf191b5bb37d9756264
Dominyk Tiller <dominyktiller@gmail.com>
2015-01-07 21:22:36 +0000

    atool: added test
    Modernises the formula, and adds a two-part test.
    
    The first part of the test creates a tarball from two files, and the
    second part of the test looks inside that tarball without actually
    untarring it to inspect its contents, which we then verify are correct
    and expected.
    
    Closes #35637.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/atool.rb
+++ Library/Formula/atool.rb
@@ -1,12 +1,22 @@
-require 'formula'
-
 class Atool < Formula
-  homepage 'http://www.nongnu.org/atool/'
-  url 'http://savannah.nongnu.org/download/atool/atool-0.39.0.tar.gz'
-  sha1 '40865bdc533f95fcaffdf8002889eb2ac67224a9'
+  homepage "https://savannah.nongnu.org/projects/atool/"
+  url "http://savannah.nongnu.org/download/atool/atool-0.39.0.tar.gz"
+  sha1 "40865bdc533f95fcaffdf8002889eb2ac67224a9"
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    mkdir "apple_juice"
+    cd testpath/"apple_juice" do
+      touch "example.txt"
+      touch "example2.txt"
+      system bin/"apack", "test.tar.gz", "example.txt", "example2.txt"
+    end
+    output = shell_output("#{bin}/als #{testpath}/apple_juice/test.tar.gz")
+    assert output.include? "example.txt"
+    assert output.include? "example2.txt"
   end
 end
