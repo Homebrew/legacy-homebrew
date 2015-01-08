b18eb63149f1163f689af6c09992a7fd6cd20b32
Xu Cheng <xu-cheng@users.noreply.github.com>
2015-01-09 00:53:18 +0800

    gcc bump version

--- Library/Formula/gcc.rb
+++ Library/Formula/gcc.rb
@@ -25,7 +25,7 @@ class Gcc < Formula
   url "http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-4.9.2.tar.bz2"
   mirror "ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.2/gcc-4.9.2.tar.bz2"
   sha1 "79dbcb09f44232822460d80b033c962c0237c6d8"
-  revision 1
+  revision 2
 
   bottle do
     sha1 "8cc5eefa405e7d818a13fa1ad5a144b4442bed40" => :yosemite
