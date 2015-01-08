8494070725e1a2f85c24aa15c32304818ca8233e
Alex Gaynor <alex.gaynor@gmail.com>
2015-01-08 09:41:48 -0800

    Add back the mirror now that OpenSSL is there

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,6 +1,7 @@
 class Openssl < Formula
   homepage "https://openssl.org"
   url "https://www.openssl.org/source/openssl-1.0.1k.tar.gz"
+  mirror "https://raw.githubusercontent.com/DomT4/LibreMirror/master/OpenSSL/openssl-1.0.1k.tar.gz"
   sha256 "8f9faeaebad088e772f4ef5e38252d472be4d878c6b3a2718c10a4fcebe7a41c"
 
   bottle do
