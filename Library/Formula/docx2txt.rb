c7bce64fc6c5d8a7a1d81f67d72cb99f12eb1d81
Kwaku Yeboah-Antwi <kyeboaha@mod-0.irisa.fr>
2015-01-08 15:52:20 +0100

    Removed extra whitespace Updated head to point to git master Compressed mv and bin.install to one line Replace single quotes with double quotes Added a really basic text.Couldnt add a more detailed test because it would require addingan extra docx file just for testing

--- Library/Formula/docx2txt.rb
+++ Library/Formula/docx2txt.rb
@@ -1,26 +1,24 @@
 class Docx2txt < Formula
   homepage "http://docx2txt.sourceforge.net/"
   url "https://github.com/Killeroid/docx2txt/archive/v1.4.tar.gz"
-  head "https://github.com/Killeroid/docx2txt.git", :tag => "v1.4"
+  head "https://github.com/Killeroid/docx2txt.git"
   sha1 "c4be6ce70e0bb3216542b95bf6574c2ee40f660a"
 
-
   def install
-    inreplace "README" do |s|
-      s.gsub! "docx2txt.pl", "docx2txt"
-    end
+    inreplace "README", "docx2txt.pl", "docx2txt"
 
     inreplace "docx2txt.pl" do |s|
       s.gsub! "our \$systemConfigDir = \"/etc\"", "our \$systemConfigDir = \"#{etc}\""
     end
 
-    mv 'docx2txt.pl', 'docx2txt'
+    bin.install "docx2txt.pl" => "docx2txt"
+    etc.install "docx2txt.config"
+    doc.install "AUTHORS", "COPYING", "README"
+    man1.install "docx2txt.1"
+  end
 
-    bin.install 'docx2txt'
-    etc.install 'docx2txt.config'
-    doc.install 'AUTHORS', 'COPYING', 'README'
-    man1.install 'docx2txt.1'
-
+  test do
+    system bin/"docx2txt", "-h"
   end
 
 end
