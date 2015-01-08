fa4015609af6baa890828b8fbc5b40dc07cf6e25
Sean Webster <sean@cognoseed.org>
2015-01-08 14:40:45 -0800

    chromedriver 2.13
    Closes #35678.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/chromedriver.rb
+++ Library/Formula/chromedriver.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Chromedriver < Formula
   homepage 'https://sites.google.com/a/chromium.org/chromedriver/'
-  url 'http://chromedriver.storage.googleapis.com/2.12/chromedriver_mac32.zip'
-  sha256 'f93464dd4e57f7d59601e9ab92ad0770493f9ea00b69ddba8da3886c35852d4b'
-  version '2.12'
+  url 'http://chromedriver.storage.googleapis.com/2.13/chromedriver_mac32.zip'
+  sha256 '0e9e9fd10b2f9b563d6b76bffe0e5222134f14423db21e6cf7fb9bd4c6a91b30'
+  version '2.13'
 
   def install
     bin.install 'chromedriver'
