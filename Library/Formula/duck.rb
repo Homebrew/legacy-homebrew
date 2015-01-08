e9eceb371eba5c8eb984d711250594c5dfd7dc71
David Kocher <dkocher@sudo.ch>
2015-01-08 14:03:25 +0100

    Update to 4.6.2.16323 with fixed package URLs and checksum. Add version match test

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -1,9 +1,11 @@
+require "formula"
+
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://update.cyberduck.io/src/cyberduck-src-16304.tar.gz"
-  version "4.6.2.16304"
+  url "https://dist.duck.sh/duck-src-16323.tar.gz"
+  version "4.6.2.16323"
   head "https://svn.cyberduck.io/trunk/"
-  sha1 "50a9bafe9774137d63bd8aa91804789b225ce9fc"
+  sha1 "4835fde547c4467a2863f31dd775034da6b83be3"
 
   depends_on :java => ["1.7", :build]
   depends_on :xcode => :build
@@ -11,7 +13,7 @@ class Duck < Formula
   depends_on "openssl"
 
   def install
-    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=16304", "cli"
+    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=16323", "cli"
     system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/libPrime.dylib"
     system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/librococoa.dylib"
     libexec.install Dir["build/duck.bundle/*"]
@@ -19,7 +21,9 @@ class Duck < Formula
   end
 
   test do
-    system "#{bin}/duck", "-version"
+    unless "Cyberduck 4.6.2 (16323)\n".eql? %x[#{bin}/duck -version]
+      raise "Version mismatch"
+    end
     filename = (testpath/"test")
     system "#{bin}/duck", "--download", stable.url, filename
     filename.verify_checksum stable.checksum
