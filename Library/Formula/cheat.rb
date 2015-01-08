6caf1988f0d0326d672066112396e257b63585ca
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-08 22:53:46 +0000

    cheat: update 2.1.3 bottle.

--- Library/Formula/cheat.rb
+++ Library/Formula/cheat.rb
@@ -6,9 +6,9 @@ class Cheat < Formula
 
   bottle do
     cellar :any
-    sha1 "6f98925637992ad9d976aee595a3354c61712a10" => :yosemite
-    sha1 "ae00b12b7f7c7556bcfc8f8aebf4a56b00495aa3" => :mavericks
-    sha1 "33793a17ec95d635781937658e7a6ec40fb58438" => :mountain_lion
+    sha1 "21368d2e4f0394e461c473a637be57c0eb1a17c4" => :yosemite
+    sha1 "c117c796c2be0b330ee3b91acc03a73ad14f9a79" => :mavericks
+    sha1 "7e889187dcef76f9ee8df1c581bb003a14abf8de" => :mountain_lion
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
