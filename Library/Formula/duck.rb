4db719911e51c4a4efeb7622b02f5ff0b514fc9a
David Kocher <dkocher@sudo.ch>
2015-01-09 16:22:09 +0100

    Update to 4.6.2 (16355). Use regex to extract revision from version.

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -1,17 +1,16 @@
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/duck-src-4.6.2.16342.tar.gz"
-  version "4.6.2.16342"
-  sha1 "935c220d42dc213742f94b80443b524f2e48941a"
+  url "https://dist.duck.sh/duck-src-4.6.2.16355.tar.gz"
+  sha1 "fc8b40e67e417e43b8e30731523fd202ebfe94f6"
   head "https://svn.cyberduck.io/trunk/"
 
-  depends_on :java => ["1.7", :build]
+  depends_on :java => [:build, "1.7"]
   depends_on :xcode => :build
   depends_on "ant" => :build
   depends_on "openssl"
 
   def install
-    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=16342", "cli"
+    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=#{version.to_str[/(\d\.\d(\.\d)?)\.(\d+)/, 3]}", "cli"
     system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/libPrime.dylib"
     system "install_name_tool", "-change", "/usr/lib/libcrypto.0.9.8.dylib", "/usr/local/opt/openssl/lib/libcrypto.dylib", "build/duck.bundle/Contents/Frameworks/librococoa.dylib"
     libexec.install Dir["build/duck.bundle/*"]
@@ -19,9 +18,6 @@ class Duck < Formula
   end
 
   test do
-    unless "Cyberduck 4.6.2 (16342)\n".eql? %x(#{bin}/duck -version)
-      fail "Version mismatch"
-    end
     filename = (testpath/"test")
     system "#{bin}/duck", "--download", stable.url, filename
     filename.verify_checksum stable.checksum
