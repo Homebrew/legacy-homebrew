9abf75f333422793a45c6a8b9a49c92b1b0ed6f8
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-01-09 04:05:53 +0100

    curl: pass audit --strict, swap ftp for http
    Closes #35684.

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -1,7 +1,7 @@
 class Curl < Formula
   homepage "http://curl.haxx.se/"
   url "http://curl.haxx.se/download/curl-7.40.0.tar.bz2"
-  mirror "ftp://ftp.sunet.se/pub/www/utilities/curl/curl-7.40.0.tar.bz2"
+  mirror "http://ftp.sunet.se/pub/www/utilities/curl/curl-7.40.0.tar.bz2"
   sha256 "899109eb3900fa6b8a2f995df7f449964292776a04763e94fae640700f883fba"
 
   bottle do
@@ -44,7 +44,7 @@ class Curl < Formula
   def install
     # Throw an error if someone actually tries to rock both SSL choices.
     # Long-term, make this singular-ssl-option-only a requirement.
-    if build.with? "libressl" and build.with? "openssl"
+    if build.with?("libressl") && build.with?("openssl")
       ohai <<-EOS.undent
       --with-openssl and --with-libressl are both specified and
       curl can only use one at a time; proceeding with openssl.
@@ -61,7 +61,7 @@ class Curl < Formula
     # cURL has a new firm desire to find ssl with PKG_CONFIG_PATH instead of using
     # "--with-ssl" any more. "when possible, set the PKG_CONFIG_PATH environment
     # variable instead of using this option". Multi-SSL choice breaks w/o using it.
-    if MacOS.version < :mountain_lion or build.with? "openssl"
+    if MacOS.version < :mountain_lion || build.with?("openssl")
       ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["openssl"].opt_prefix}/lib/pkgconfig"
       args << "--with-ssl=#{Formula["openssl"].opt_prefix}"
       args << "--with-ca-bundle=#{etc}/openssl/cert.pem"
