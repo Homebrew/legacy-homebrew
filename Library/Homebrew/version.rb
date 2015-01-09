788de017c7c9f11721059d8090a431379a9419af
Alessio Bogon <youtux@gmail.com>
2015-01-09 11:21:01 +0100

    version: fix fake versions not being correctly compared
    This patch fixes _wrong_ comparisons like 1.9 > 1.9a, which occurs for example with tmux. The problem was that literal tokens like 'a' or 'b' were treated like alpha and beta tokens, while it's not true generally.

--- Library/Homebrew/version.rb
+++ Library/Homebrew/version.rb
@@ -51,6 +51,8 @@ class Version
 
     def <=>(other)
       case other
+      when AlphaToken, BetaToken, RCToken
+        1
       when StringToken
         value <=> other.value
       when NumericToken, NullToken
@@ -85,12 +87,17 @@ class Version
   end
 
   class AlphaToken < CompositeToken
-    PATTERN = /a(?:lpha)?[0-9]*/i
+    PATTERN = Regexp.union(
+      /alpha[0-9]*/i,
+      /a[0-9]+/,
+    )
 
     def <=>(other)
       case other
       when AlphaToken
         rev <=> other.rev
+      when BetaToken, RCToken, PatchToken, StringToken
+        -1
       else
         super
       end
@@ -98,7 +105,10 @@ class Version
   end
 
   class BetaToken < CompositeToken
-    PATTERN = /b(?:eta)?[0-9]*/i
+    PATTERN = Regexp.union(
+      /beta[0-9]*/i,
+      /b[0-9]+/,
+    )
 
     def <=>(other)
       case other
@@ -106,7 +116,7 @@ class Version
         rev <=> other.rev
       when AlphaToken
         1
-      when RCToken, PatchToken
+      when RCToken, PatchToken, StringToken
         -1
       else
         super
@@ -123,7 +133,7 @@ class Version
         rev <=> other.rev
       when AlphaToken, BetaToken
         1
-      when PatchToken
+      when PatchToken, StringToken
         -1
       else
         super
