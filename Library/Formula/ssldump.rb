99cf03ec37004b7d29f318694ff9e2ac5ed8b144
Xu Cheng <xucheng@me.com>
2015-01-09 13:50:44 +0800

    ssldump: link to homebrew openssl

--- Library/Formula/ssldump.rb
+++ Library/Formula/ssldump.rb
@@ -3,6 +3,8 @@ class Ssldump < Formula
   url "http://www.rtfm.com/ssldump/ssldump-0.9b3.tar.gz"
   sha1 "a633a9a811a138eac5ed440d583473b644135ef5"
 
+  depends_on "openssl"
+
   # reorder include files
   # http://sourceforge.net/tracker/index.php?func=detail&aid=1622854&group_id=68993&atid=523055
   # increase pcap sample size from an arbitrary 5000 the max TLS packet size 18432
