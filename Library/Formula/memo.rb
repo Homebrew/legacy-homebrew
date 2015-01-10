d560c499feb8cc7b3efe1c990d9c289ba626e07b
Wesley Hill <wesley@hakobaito.co.uk>
2015-01-10 04:59:36 +0000

    Changed install command.

--- Library/Formula/memo.rb
+++ Library/Formula/memo.rb
@@ -4,9 +4,8 @@ class Memo < Formula
   sha1 "3e047b09e91d695f1767d1dd6d179732c07a5759"
 
   def install
-    system "make"
-    bin.install "memo"
-    man1.install "memo.1"
+    bin.mkpath
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
