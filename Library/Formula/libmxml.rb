03fe23930d21182f31631bd7d3ed26122dc5f36c
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-01-10 05:01:33 +0100

    libmxml: 2.9, pass strict audit, added https, head

--- Library/Formula/libmxml.rb
+++ Library/Formula/libmxml.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Libmxml < Formula
   homepage "http://www.minixml.org/"
-  url "http://www.msweet.org/files/project3/mxml-2.8.tar.gz"
-  sha1 "09d88f1720f69b64b76910dfe2a5c5fa24a8b361"
+  url "https://www.msweet.org/files/project3/mxml-2.9.tar.gz"
+  sha1 "a3d9c1f8cf8c7f85d76bb6954af1888d55f926f0"
+
+  head "http://svn.msweet.org/mxml/"
 
   bottle do
     cellar :any
@@ -20,6 +20,16 @@ class Libmxml < Formula
                           "--enable-shared",
                           "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      int testfunc(char *string)
+      {
+        return string ? string[0] : 0;
+      }
+    EOS
+    assert_match /testfunc/, shell_output("#{bin}/mxmldoc test.c")
   end
 end
