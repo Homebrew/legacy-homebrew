a1ec01be615e34987059ae9ff15d9766e33a9256
Brett Koonce <koonce@gmail.com>
2015-01-10 09:44:35 -0800

    chinadns 1.2.2

--- Library/Formula/chinadns-c.rb
+++ Library/Formula/chinadns-c.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class ChinadnsC < Formula
   homepage "https://github.com/clowwindy/ChinaDNS-C"
-  url "https://github.com/clowwindy/ChinaDNS-C/releases/download/1.1.8/chinadns-c-1.1.8.tar.gz"
-  sha1 "e712aab436e555a242f6c3c8acd7474b0b445bf1"
+  url "https://github.com/clowwindy/ChinaDNS/releases/download/1.2.2/chinadns-1.2.2.tar.gz"
+  sha1 "6498dacfce023d56699f80bcf1fed9fd9eda99d9"
 
   bottle do
     cellar :any
