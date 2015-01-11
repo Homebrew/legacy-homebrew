873c14e2b1c2f3674a5abbc530346733615d57cd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-11 23:40:12 +0000

    duck: add 4.6.2.16366 bottle.

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -4,6 +4,13 @@ class Duck < Formula
   sha1 "135f7ca7315fd478607e590eaa2df43418d85e0d"
   head "https://svn.cyberduck.io/trunk/"
 
+  bottle do
+    cellar :any
+    sha1 "05353941783e34f1931b0cdd78a89f245d8daf33" => :yosemite
+    sha1 "a2120b7b4b7ae15f9b941c24bb9d4f7c36392e74" => :mavericks
+    sha1 "fb6e8e20d702cf3d9808e758328cd7ebc8830283" => :mountain_lion
+  end
+
   depends_on :java => [:build, "1.7"]
   depends_on :xcode => :build
   depends_on "ant" => :build
