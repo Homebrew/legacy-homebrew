63dc13e09d03d53d67716f95de86f93b7092102d
Mikaela Suomalainen <mikaela+git@mikaela.info>
2015-01-11 11:23:07 +0200

    weechat 1.1
    Update WeeChat formula to 1.1
    
    Closes #35740.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/weechat.rb
+++ Library/Formula/weechat.rb
@@ -1,13 +1,7 @@
 class Weechat < Formula
   homepage "https://www.weechat.org"
-  url "https://weechat.org/files/src/weechat-1.0.1.tar.gz"
-  sha1 "1d33591b6c0adc2c30b36a7b349603cbdbcb40b2"
-
-  devel do
-    url "https://github.com/weechat/weechat/archive/v1.1-rc1.tar.gz"
-    sha1 "3cf06cfead34fe351a0e3eb53144cbf3f68bc7e5"
-    version "1.1-rc1"
-  end
+  url "https://weechat.org/files/src/weechat-1.1.tar.gz"
+  sha1 "8cc055051b0df6755e8310e4fd624623707e81dc"
 
   head "https://github.com/weechat/weechat.git"
 
@@ -33,12 +27,6 @@ class Weechat < Formula
   depends_on "curl" => :optional
 
   def install
-    # builds against the python in PATH by asking cmake to use introspected
-    # values instead of ignoring them
-    # https://github.com/weechat/weechat/pull/217
-    if build.stable?
-      inreplace "cmake/FindPython.cmake", "PATHS ${", "HINTS ${"
-    end
 
     args = std_cmake_args
 
