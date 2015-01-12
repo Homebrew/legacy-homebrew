1f05886306b8a8343278185b34ac50c856fe1bae
Karan Goel <karan@goel.im>
2015-01-11 23:04:35 -0800

    Fix audit issues
    There were 4 audit issues with the gitignore.rb file:
    * Stable: version 0.0.2 is redundant with version scanned from URL
    * 8: Trailing whitespace was found
    * 17: Trailing whitespace was found
    * 23: Trailing whitespace was found
    
    All these have now been fixed.

--- Library/Formula/gitignore.rb
+++ Library/Formula/gitignore.rb
@@ -1,11 +1,10 @@
 class Gitignore < Formula
   homepage "https://github.com/karan/joe"
   url "https://github.com/karan/joe/archive/0.0.2.tar.gz"
-  version "0.0.2"
   sha1 "da3abe49d76a4844d0744951b61c3dd71fd9959c"
 
   depends_on :python if MacOS.version <= :snow_leopard
-  
+
   resource "docopt" do
     url "https://github.com/docopt/docopt/archive/0.6.1.tar.gz"
     sha1 "2228a1bc58665664550a4552fb53906704918866"
@@ -14,13 +13,13 @@ class Gitignore < Formula
   def install
     ENV["PYTHONPATH"] = libexec/"vendor/lib/python2.7/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    
+
     %w[docopt].each do |r|
       resource(r).stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
-    
+
     system "python", *Language::Python.setup_install_args(libexec)
 
     bin.install Dir["#{libexec}/bin/*"]
