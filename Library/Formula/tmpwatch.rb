0994c0b6914e6f033f0119d5263e76dce395f9c2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-12 14:46:19 +0000

    tmpwatch: add 2.11 bottle.

--- Library/Formula/tmpwatch.rb
+++ Library/Formula/tmpwatch.rb
@@ -3,6 +3,13 @@ class Tmpwatch < Formula
   url "https://fedorahosted.org/releases/t/m/tmpwatch/tmpwatch-2.11.tar.bz2"
   sha1 "c578dd98e5ea64ad987a95ae55926685a0df0659"
 
+  bottle do
+    cellar :any
+    sha1 "4afc50b010310169a0bd37dac99df9e2b44370c6" => :yosemite
+    sha1 "8eef5be45661363ee34a791f1f331ba519f4d9b4" => :mavericks
+    sha1 "079eb69792c8a9de70cb02eaebe0c77642b7d18b" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
