79352f63916568fd86b80628d34ba74bf4f24b65
Xu Cheng <xucheng@me.com>
2015-01-12 21:34:02 +0800

    screenbrightness: add test and modernize
    Closes #35781.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/screenbrightness.rb
+++ Library/Formula/screenbrightness.rb
@@ -1,12 +1,14 @@
-require 'formula'
-
 class Screenbrightness < Formula
-  homepage 'https://github.com/jmstacey/screenbrightness'
-  url 'https://github.com/jmstacey/screenbrightness/archive/1.1.tar.gz'
-  sha1 'f9750733ac298837f519fcfedcbfec74f781bc68'
+  homepage "https://github.com/jmstacey/screenbrightness"
+  url "https://github.com/jmstacey/screenbrightness/archive/1.1.tar.gz"
+  sha1 "f9750733ac298837f519fcfedcbfec74f781bc68"
 
   def install
     system "make"
     system "make", "prefix=#{prefix}", "install"
+  end
+
+  test do
+    system "#{bin}/screenbrightness", "-l"
   end
 end
