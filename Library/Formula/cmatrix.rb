0d750f0d5c2a54db676025d495897ce3da2116de
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-12 09:50:49 +0100

    cmatrix: modernize + basic test added
    Closes #35770.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cmatrix.rb
+++ Library/Formula/cmatrix.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class Cmatrix < Formula
-  homepage 'http://www.asty.org/cmatrix/'
-  url 'http://www.asty.org/cmatrix/dist/cmatrix-1.2a.tar.gz'
-  sha1 'ca078c10322a47e327f07a44c9a42b52eab5ad93'
+  homepage "http://www.asty.org/cmatrix/"
+  url "http://www.asty.org/cmatrix/dist/cmatrix-1.2a.tar.gz"
+  sha1 "ca078c10322a47e327f07a44c9a42b52eab5ad93"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/cmatrix", "-V"
   end
 end
