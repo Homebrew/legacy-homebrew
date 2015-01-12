ef0c3df4d800c6ce9ead2b722232a921bdfa5048
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-12 23:46:57 +0100

    jp2a: TERM env variable defined in test
    Closes #35792.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/jp2a.rb
+++ Library/Formula/jp2a.rb
@@ -23,6 +23,8 @@ class Jp2a < Formula
   end
 
   test do
+    # the test fails if this is not set
+    ENV["TERM"] = "xterm-256color"
     system "#{bin}/jp2a", test_fixtures("test.jpg")
   end
 end
