526cc7e349779809d593f898a77dc584eaa2611f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-12 13:48:26 +0000

    screenbrightness: add 1.1 bottle.

--- Library/Formula/screenbrightness.rb
+++ Library/Formula/screenbrightness.rb
@@ -3,6 +3,13 @@ class Screenbrightness < Formula
   url "https://github.com/jmstacey/screenbrightness/archive/1.1.tar.gz"
   sha1 "f9750733ac298837f519fcfedcbfec74f781bc68"
 
+  bottle do
+    cellar :any
+    sha1 "af2ab5c5ba668e9be32b95ed3ac581423e14b1b8" => :yosemite
+    sha1 "c780916b31a50d6a599b1c6238548a8db3a008a1" => :mavericks
+    sha1 "9cf03ce35a3cb24a25bd28b9a213963f64a3dfdd" => :mountain_lion
+  end
+
   def install
     system "make"
     system "make", "prefix=#{prefix}", "install"
