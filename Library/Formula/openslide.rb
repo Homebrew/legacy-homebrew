3d0c33f43b5090df9e77d3e3f167f121b45701b4
Victor Mateevitsi <mvictoras@gmail.com>
2015-01-12 12:11:55 -0500

    openslide 3.4.0
    Closes #35788.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/openslide.rb
+++ Library/Formula/openslide.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Openslide < Formula
   homepage 'http://openslide.org/'
-  url 'http://download.openslide.org/releases/openslide/openslide-3.3.3.tar.gz'
-  sha1 '2315f0daa5d963e6ba9f1e67517cee44f9deabe5'
-  revision 1
+  url 'https://github.com/openslide/openslide/releases/download/v3.4.0/openslide-3.4.0.tar.gz'
+  sha1 'edd28a142f7801a354949c05e7f1b7dcb246db6b'
 
   depends_on 'pkg-config' => :build
   depends_on 'libpng'
@@ -14,6 +13,7 @@ class Openslide < Formula
   depends_on 'glib'
   depends_on 'openjpeg'
   depends_on 'cairo'
+  depends_on 'gdk-pixbuf'
 
   def install
     system "./configure", "--disable-dependency-tracking",
