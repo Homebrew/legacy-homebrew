62de346db492e25b8a1366eb68d6b9660736fbbc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-13 14:36:10 +0000

    surfraw: add 2.2.9 bottle.

--- Library/Formula/surfraw.rb
+++ Library/Formula/surfraw.rb
@@ -4,6 +4,12 @@ class Surfraw < Formula
   url "http://surfraw.alioth.debian.org/dist/surfraw-2.2.9.tar.gz"
   sha1 "70bbba44ffc3b1bf7c7c4e0e9f0bdd656698a1c0"
 
+  bottle do
+    sha1 "6c592c99adf6c1a0bb4993a36e4392bce6e24eaa" => :yosemite
+    sha1 "8ca477ffd5f157aa40b258d66e2536691d769921" => :mavericks
+    sha1 "b17ead2b3f8030e659d0af4b44332f76397607f5" => :mountain_lion
+  end
+
   def install
     system "./prebuild" if build.head?
     system "./configure", "--prefix=#{prefix}",
