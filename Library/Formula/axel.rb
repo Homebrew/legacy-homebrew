ef0477c10a8c1e39b1978dd66e85239cadc7c6b6
Dominyk Tiller <dominyktiller@gmail.com>
2015-01-13 19:54:48 +0000

    axel: modernise and add test
    Confession: I completely stole the cURL test here.

--- Library/Formula/axel.rb
+++ Library/Formula/axel.rb
@@ -1,13 +1,19 @@
-require 'formula'
-
 class Axel < Formula
-  homepage 'http://packages.debian.org/wheezy/axel'
-  url 'http://ftp.de.debian.org/debian/pool/main/a/axel/axel_2.4.orig.tar.gz'
-  sha1 '6d89a7ce797ddf4c23a210036d640d013fe843ca'
+  homepage "https://packages.debian.org/sid/axel"
+  url "https://mirrors.kernel.org/debian/pool/main/a/axel/axel_2.4.orig.tar.gz"
+  mirror "http://ftp.de.debian.org/debian/pool/main/a/axel/axel_2.4.orig.tar.gz"
+  sha1 "6d89a7ce797ddf4c23a210036d640d013fe843ca"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--debug=0", "--i18n=0"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    filename = (testpath/"axel.tar.gz")
+    system bin/"axel", "-o", "axel.tar.gz", stable.url
+    filename.verify_checksum stable.checksum
+    assert File.exist?("axel.tar.gz")
   end
 end
