3017188f36d29796ae11ce612adae4b10eed063f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-13 07:40:13 +0000

    cpp-netlib: add netlib bottle.

--- Library/Formula/cpp-netlib.rb
+++ Library/Formula/cpp-netlib.rb
@@ -3,6 +3,12 @@ class CppNetlib < Formula
   url "https://storage.googleapis.com/cpp-netlib-downloads/0.11.1/cpp-netlib-0.11.1-final.tar.bz2"
   sha1 "213ba700e534596b44409cd6d5738b959fe41746"
 
+  bottle do
+    sha1 "799f0fc4ecf5b9c266f429d700095c4439e1ff87" => :yosemite
+    sha1 "cdadc6e7d4c4f721c694efaed258affc9c93b86f" => :mavericks
+    sha1 "4d839cdf6ed3b92a5e4c6c1738ff058b979c4e21" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "boost" => "c++11"
   needs :cxx11
