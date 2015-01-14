48fd7d46345d45b5efab79c7d7e432abe92ea541
Denis Davydov <davydden@gmail.com>
2015-01-14 22:58:23 +0100

    Binutils (add libiberty)
    addedd an option to install libiberty, which is required by some Formulas.

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -1,7 +1,7 @@
 class Binutils < Formula
-  homepage "http://www.gnu.org/software/binutils/binutils.html"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.25.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz"
+  homepage "https://www.gnu.org/software/binutils/binutils.html"
+  url "https://ftpmirror.gnu.org/binutils/binutils-2.25.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz"
   sha1 "f10c64e92d9c72ee428df3feaf349c4ecb2493bd"
 
   # No --default-names option as it interferes with Homebrew builds.
@@ -12,20 +12,29 @@ class Binutils < Formula
     sha1 "8e71e8d290d3c71926ad1bb48b74b8cd462fce3d" => :mountain_lion
   end
 
+  option "with-libiberty",  "Instal with libiberty."
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--program-prefix=g",
-                          "--prefix=#{prefix}",
-                          "--infodir=#{info}",
-                          "--mandir=#{man}",
-                          "--disable-werror",
-                          "--enable-interwork",
-                          "--enable-multilib",
-                          "--enable-64-bit-bfd",
-                          "--enable-targets=all"
+    args =  %W[--disable-debug
+               --disable-dependency-tracking
+               --program-prefix=g
+               --prefix=#{prefix}
+               --infodir=#{info}
+               --mandir=#{man}
+               --disable-werror
+               --enable-interwork
+               --enable-multilib
+               --enable-64-bit-bfd
+               --enable-targets=all]
+
+    args << "--enable-install-libiberty" if build.with? "libiberty"
+
+    system "./configure",*args
     system "make"
     system "make", "install"
+
+    # symilnk libiberty to lib folder:
+    lib.install_symlink Dir["#{lib}/x86_64/*"]
   end
 
   test do
