8712010beeacfa5a4b1cb28a2bc52c2457c7b574
Dominyk Tiller <dominyktiller@gmail.com>
2015-01-14 03:30:53 +0000

    bcpp 20131209
    Version bump, style nits, added test.

--- Library/Formula/bcpp.rb
+++ Library/Formula/bcpp.rb
@@ -1,12 +1,23 @@
-require 'formula'
-
 class Bcpp < Formula
-  homepage 'http://invisible-island.net/bcpp/'
-  url 'ftp://invisible-island.net/bcpp/bcpp-20090630.tgz'
-  sha1 'f8ce9736fa2775e8c15b7fcbfee156103d90ece8'
+  homepage "http://invisible-island.net/bcpp/"
+  url "ftp://invisible-island.net/bcpp/bcpp-20131209.tgz"
+  sha1 "5b38e0ae532ed5fc9ee8d5fc8bf84511d55080a8"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+    etc.install "bcpp.cfg"
+  end
+
+  test do
+    (testpath/"test.txt").write <<-EOS.undent
+              test
+                 test
+          test
+                test
+    EOS
+    system bin/"bcpp", "test.txt", "-fnc", "#{etc}/bcpp.cfg"
+    assert File.exist?("test.txt.orig")
+    assert File.exist?("test.txt")
   end
 end
