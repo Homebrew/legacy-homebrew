c444f3a374aa5721173f27c75e2d451443aa6f0a
Fredrik Pettai <pettai@nordu.net>
2015-01-14 10:17:46 +0100

    Update to version 0.1.3
    Closes #35871.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/yubico-piv-tool.rb
+++ Library/Formula/yubico-piv-tool.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class YubicoPivTool < Formula
   homepage "http://opensource.yubico.com/yubico-piv-tool/"
-  url "http://opensource.yubico.com/yubico-piv-tool/releases/yubico-piv-tool-0.1.0.tar.gz"
-  sha1 "bebf01280e0ace0c43ab398a06cc0bc2e7fe2af9"
+  url "http://opensource.yubico.com/yubico-piv-tool/releases/yubico-piv-tool-0.1.3.tar.gz"
+  sha1 "425269827da9f47a6277bd42b6415c8527d9ff8e"
 
   bottle do
     cellar :any
