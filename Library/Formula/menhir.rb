748c86c89c271021cc3dd769523022e4955f9c81
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-14 08:46:29 +0000

    menhir: add 20141215 bottle.

--- Library/Formula/menhir.rb
+++ Library/Formula/menhir.rb
@@ -3,6 +3,12 @@ class Menhir < Formula
   url "http://cristal.inria.fr/~fpottier/menhir/menhir-20141215.tar.gz"
   sha1 "0aa5d58a5cdf0daa69bb577daf379997dc3af1c1"
 
+  bottle do
+    sha1 "a2d506915eb26d66fbf529fd48b906a3289601e3" => :yosemite
+    sha1 "8a6e1f662272c5536d33645a3e67616d19ec7706" => :mavericks
+    sha1 "7ab625d14199153fac46742c684e623e68590469" => :mountain_lion
+  end
+
   depends_on "objective-caml"
 
   def install
