9e755a4aabca096fac4f722387b7644d187958d3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-14 13:29:31 +0000

    pv: update 1.5.7 bottle.

--- Library/Formula/pv.rb
+++ Library/Formula/pv.rb
@@ -4,9 +4,10 @@ class Pv < Formula
   sha1 "173d87d11d02a524037228f6495c46cad3214b7d"
 
   bottle do
-    sha1 "74c116553193e57d114106e970b9c49b9f4104b5" => :mavericks
-    sha1 "d5a03a105681454bdb9bb27259a7b590dd12afa8" => :mountain_lion
-    sha1 "47a5ca248413625e8b70de1b13a96c313c5952ed" => :lion
+    revision 1
+    sha1 "0c77b97448d2705d9e9f5a63ceafa42e483a21d9" => :yosemite
+    sha1 "a49bcc1abe924b622d06fb15eb01141d0c252f40" => :mavericks
+    sha1 "1441d97ae226d2cb3e3519ae714cf47940d6595a" => :mountain_lion
   end
 
   depends_on "gettext"
