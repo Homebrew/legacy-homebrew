27654987b10624c624b5fdf72553145553aeda67
Chongyu Zhu <i@lembacon.com>
2015-01-15 03:13:45 +0800

    google-perftools 2.4

--- Library/Formula/google-perftools.rb
+++ Library/Formula/google-perftools.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class GooglePerftools < Formula
-  homepage 'http://code.google.com/p/gperftools/'
-  url "https://googledrive.com/host/0B6NtGsLhIcf7MWxMMF9JdTN3UVk/gperftools-2.3.tar.gz"
-  sha1 "3f7d48a8dfd519f744d94cd2dc6a7875e456e632"
+  homepage "http://code.google.com/p/gperftools/"
+  url "https://googledrive.com/host/0B6NtGsLhIcf7MWxMMF9JdTN3UVk/gperftools-2.4.tar.gz"
+  sha1 "13b904d0d1f220e43e4495f3403ee280c6da26ea"
 
   bottle do
     cellar :any
@@ -18,7 +16,7 @@ class GooglePerftools < Formula
   end
 
   def install
-    ENV.append_to_cflags '-D_XOPEN_SOURCE'
+    ENV.append_to_cflags "-D_XOPEN_SOURCE"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
