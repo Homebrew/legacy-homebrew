7738cedb8f06db6e67bab661c5374ebda583b658
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-14 09:41:54 +0100

    hub: devel added
    Closes #35863.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hub.rb
+++ Library/Formula/hub.rb
@@ -1,19 +1,25 @@
-require 'formula'
-
 class Hub < Formula
-  homepage 'http://hub.github.com/'
-  url 'https://github.com/github/hub/archive/v1.12.4.tar.gz'
-  sha1 '25135167108cd777ba6ec2dd5a9a25e248d98d4b'
+  homepage "http://hub.github.com/"
+  url "https://github.com/github/hub/archive/v1.12.4.tar.gz"
+  sha1 "25135167108cd777ba6ec2dd5a9a25e248d98d4b"
 
   head do
     url "https://github.com/github/hub.git"
     depends_on "go" => :build
   end
 
-  option 'without-completions', 'Disable bash/zsh completions'
+  devel do
+    url "https://github.com/github/hub/archive/v2.2.0-rc1.tar.gz"
+    sha1 "029d154ce0f9c4999e4dd6ef23eab5e411370c4f"
+    version "2.2.0-rc1"
+
+    depends_on "go" => :build
+  end
+
+  option "without-completions", "Disable bash/zsh completions"
 
   def install
-    if build.head?
+    if build.head? || build.devel?
       ENV["GIT_DIR"] = cached_download/".git"
       system "script/build"
       bin.install "hub"
@@ -21,9 +27,9 @@ class Hub < Formula
       rake "install", "prefix=#{prefix}"
     end
 
-    if build.with? 'completions'
-      bash_completion.install 'etc/hub.bash_completion.sh'
-      zsh_completion.install 'etc/hub.zsh_completion' => '_hub'
+    if build.with? "completions"
+      bash_completion.install "etc/hub.bash_completion.sh"
+      zsh_completion.install "etc/hub.zsh_completion" => "_hub"
     end
   end
 
