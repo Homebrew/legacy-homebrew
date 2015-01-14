aaef40db0c56dda7a8e9e1d6f9e6279be13153e3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-14 04:43:04 +0000

    libssh: update 0.6.4 bottle.

--- Library/Formula/libssh.rb
+++ Library/Formula/libssh.rb
@@ -6,6 +6,9 @@ class Libssh < Formula
   head "git://git.libssh.org/projects/libssh.git"
 
   bottle do
+    sha1 "de5ef207ed3ae4d79f180a5835fe491409a0804d" => :yosemite
+    sha1 "3de5d0a02be7bda59dabe91cd7db4ece978d25bb" => :mavericks
+    sha1 "983355b795316434c54dd61e85495a808a147845" => :mountain_lion
   end
 
   depends_on "cmake" => :build
