0f500c521975f8ec1a32104ce6206a23d7883642
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-14 23:34:25 +0000

    gistit: add 0.1.3 bottle.

--- Library/Formula/gistit.rb
+++ Library/Formula/gistit.rb
@@ -3,6 +3,13 @@ class Gistit < Formula
   url "https://github.com/jrbasso/gistit/archive/v0.1.3.tar.gz"
   sha1 "62b9797a656b15da9196b6c9ee355c0c81bdc3ac"
 
+  bottle do
+    cellar :any
+    sha1 "ef2f453dfcd08d831396992392f377fe8fba96bf" => :yosemite
+    sha1 "20bb22672bfc53b1f51e469846eeece2babdbd65" => :mavericks
+    sha1 "b0be23b824c1ea269c0812a6a8ddaecd6917c549" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "jansson"
