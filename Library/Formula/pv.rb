92711e5cd439279aa2f41a3477180612b2fc6542
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-14 14:17:34 +0100

    pv: test added
    Closes #35873.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pv.rb
+++ Library/Formula/pv.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Pv < Formula
-  homepage 'http://www.ivarch.com/programs/pv.shtml'
-  url 'http://www.ivarch.com/programs/sources/pv-1.5.7.tar.bz2'
-  sha1 '173d87d11d02a524037228f6495c46cad3214b7d'
+  homepage "http://www.ivarch.com/programs/pv.shtml"
+  url "http://www.ivarch.com/programs/sources/pv-1.5.7.tar.bz2"
+  sha1 "173d87d11d02a524037228f6495c46cad3214b7d"
 
   bottle do
     sha1 "74c116553193e57d114106e970b9c49b9f4104b5" => :mavericks
@@ -11,7 +9,7 @@ class Pv < Formula
     sha1 "47a5ca248413625e8b70de1b13a96c313c5952ed" => :lion
   end
 
-  depends_on 'gettext'
+  depends_on "gettext"
 
   fails_with :llvm do
     build 2334
@@ -21,6 +19,11 @@ class Pv < Formula
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    progress = pipe_output("#{bin}/pv -ns 4 2>&1 >/dev/null", "beer")
+    assert_equal "100", progress.strip
   end
 end
