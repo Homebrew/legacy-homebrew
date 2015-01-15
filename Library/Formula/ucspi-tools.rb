b04866fa39f21783e5d6412b2b037d45847d13d7
Stefan Thiemann <stefan.thiemann@gmail.com>
2015-01-15 18:26:57 +0100

    ucspi-tools 1.2 (new formula)
    Closes #35909.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/ucspi-tools.rb
@@ -0,0 +1,18 @@
+class UcspiTools < Formula
+  homepage "https://github.com/younix/ucspi/blob/master/README.md"
+  url "https://github.com/younix/ucspi/archive/v1.2.tar.gz"
+  sha1 "38a708efd6d72e0d9d077efb15477763bdea39b0"
+
+  depends_on "pkg-config" => :build
+  depends_on "ucspi-tcp"
+  depends_on "libressl"
+
+  def install
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    out = shell_output("#{bin}/tlsc 2>&1", 1)
+    assert_equal "tlsc [-hCH] [-c cert_file] [-f ca_file] [-p ca_path] program [args...]\n", out
+  end
+end
