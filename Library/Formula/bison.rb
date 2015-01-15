3d8f9a0990f12ef4a323f7b22c6e996fc54f1527
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-15 13:29:58 +0000

    bison: update 3.0.3 bottle.

--- Library/Formula/bison.rb
+++ Library/Formula/bison.rb
@@ -5,10 +5,9 @@ class Bison < Formula
   sha1 "1fb1b831d53e2da3834e4e1eb2e14f93e9a9111d"
 
   bottle do
-    revision 1
-    sha1 "12fdce885665b82a723b815f40afa81f8765e1ec" => :yosemite
-    sha1 "59bd723116afc1a23813413e113d36c0a4361a3a" => :mavericks
-    sha1 "995e0372cc6b74c5475f740b83c17963fddbf320" => :mountain_lion
+    sha1 "b0406db2d7126e9e978356751db6eba530bfbea9" => :yosemite
+    sha1 "eac748515a1c256913d6d2f07b7fcc8fa93d49ec" => :mavericks
+    sha1 "55091763e3688a564173074a145b3e5597bf0f21" => :mountain_lion
   end
 
   keg_only :provided_by_osx, "Some formulae require a newer version of bison."
