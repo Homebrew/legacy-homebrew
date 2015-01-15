e5955e2eefc961229a83edf3aa78e7635652308d
Sergey M․ <dstftw@gmail.com>
2015-01-16 03:48:32 +0600

    youtube-dl 2015.01.15.1
    Closes #35916.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -5,8 +5,8 @@ require "formula"
 # https://pypi.python.org/pypi/youtube_dl
 class YoutubeDl < Formula
   homepage "http://rg3.github.io/youtube-dl/"
-  url "https://yt-dl.org/downloads/2015.01.15/youtube-dl-2015.01.15.tar.gz"
-  sha256 "085ca3f752b80467f8579c08736286f956269bebdebfa8ae946225a8ad094594"
+  url "https://yt-dl.org/downloads/2015.01.15.1/youtube-dl-2015.01.15.1.tar.gz"
+  sha256 "33ca4587ada5fb57646d3ff32f8502ec5f2573043cbd9ce634305e299fb22a17"
 
   bottle do
     cellar :any
