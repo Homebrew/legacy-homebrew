b03d3282a8e7b7fac1a1c2ae67aa0093561c608e
Adam C. Foltzer <acfoltzer@galois.com>
2015-01-15 15:40:14 -0800

    revert change to organization of stable and head

--- Library/Formula/idris.rb
+++ Library/Formula/idris.rb
@@ -4,6 +4,15 @@ class Idris < Formula
   include Language::Haskell::Cabal
 
   homepage "http://www.idris-lang.org"
+  url "https://github.com/idris-lang/Idris-dev/archive/v0.9.16.tar.gz"
+  sha1 "01f794c4e516454b8352266c26c92549e90c708f"
+  head "https://github.com/idris-lang/Idris-dev.git"
+
+  bottle do
+    sha1 "1fd1c7c3f223512d2869e8790c4e5afb0f196409" => :yosemite
+    sha1 "b8a7ffc5ed4429665658e54cefe400bacbc26c53" => :mavericks
+    sha1 "6dc50e41562eef4d18987f6f673b8a77f22a5512" => :mountain_lion
+  end
 
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
@@ -11,21 +20,6 @@ class Idris < Formula
 
   depends_on "libffi" => :recommended
   depends_on "pkg-config" => :build if build.with? "libffi"
-
-  stable do
-    url "https://github.com/idris-lang/Idris-dev/archive/v0.9.16.tar.gz"
-    sha1 "01f794c4e516454b8352266c26c92549e90c708f"
-
-    bottle do
-      sha1 "1fd1c7c3f223512d2869e8790c4e5afb0f196409" => :yosemite
-      sha1 "b8a7ffc5ed4429665658e54cefe400bacbc26c53" => :mavericks
-      sha1 "6dc50e41562eef4d18987f6f673b8a77f22a5512" => :mountain_lion
-    end
-  end
-
-  head do
-    url "https://github.com/idris-lang/Idris-dev.git"
-  end
 
   def install
     if build.with? "libffi"
