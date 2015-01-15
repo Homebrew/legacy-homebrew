c436fd5550678fc1d0862df3203f40d0c19eb78d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-15 09:48:40 +0000

    babeld: add 1.5.1 bottle.

--- Library/Formula/babeld.rb
+++ Library/Formula/babeld.rb
@@ -4,6 +4,13 @@ class Babeld < Formula
   sha1 "6ff3a7685e62034df83b143a36a4960b2e4d89b9"
   head "https://github.com/jech/babeld.git"
 
+  bottle do
+    cellar :any
+    sha1 "a965f70b84f8fea7ce7a7f92252e61d01a6b9e7b" => :yosemite
+    sha1 "22262a12d06cf83d35f351a49d6af7e40e7f554d" => :mavericks
+    sha1 "69e5c32092ccfdaeaff3615fbbaf15e41e327e41" => :mountain_lion
+  end
+
   # https://lists.alioth.debian.org/pipermail/babel-users/2015-January/001826.html
   patch :DATA
 
