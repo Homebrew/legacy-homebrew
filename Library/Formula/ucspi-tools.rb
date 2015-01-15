f3829e525c04eb00d1ff5bcc13de2edec42e5a63
Stefan Thiemann <stefan.thiemann@gmail.com>
2015-01-15 18:26:57 +0100

    ucspi-tools 1.1 (new formula)

---
+++ Library/Formula/ucspi-tools.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class UcspiTools < Formula
+  homepage "https://github.com/younix/ucspi/blob/master/README.md"
+  url "https://github.com/younix/ucspi/archive/v1.1.tar.gz"
+  sha1 "9196a1033f3131a888acf091d0acbc1a83a02702"
+
+  depends_on "pkg-config" => :build
+  depends_on "ucspi-tcp"
+  depends_on "libressl"
+
+  def install
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+end
