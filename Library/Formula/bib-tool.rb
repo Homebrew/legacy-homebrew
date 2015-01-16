eccfbae0114b3944f243dcf9433b831221bea748
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-16 07:44:04 +0000

    bib-tool: add 2.57 bottle.

--- Library/Formula/bib-tool.rb
+++ Library/Formula/bib-tool.rb
@@ -3,6 +3,12 @@ class BibTool < Formula
   url "http://www.gerd-neugebauer.de/software/TeX/BibTool/BibTool-2.57.tar.gz"
   sha1 "a6e80c86d347a39f3883e552db2dd4deb72b0e86"
 
+  bottle do
+    sha1 "d5822fd899e1238c75964b7c0763dad2f24386e7" => :yosemite
+    sha1 "dcaaf50c9992f1e1812845914ab3bd4b8be521de" => :mavericks
+    sha1 "e0b9efe0a08a1c02a94ddd95fdfcef8863f1ad01" => :mountain_lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--without-kpathsea"
     system "make"
