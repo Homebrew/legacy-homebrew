f670f189e07b9475b9aa98b99676a6071e051c89
JP Simard <jp@jpsim.com>
2015-01-15 17:51:50 -0800

    Create sourcekitten.rb

---
+++ Library/Formula/sourcekitten.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class SourceKitten < Formula
+  homepage "https://github.com/jpsim/SourceKitten"
+  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.3.0"
+  head "https://github.com/jpsim/SourceKitten.git"
+
+  depends_on :xcode => ["6.1.1", :build]
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}"
+    system "make", "clean"
+  end
+
+  test do
+    system "#{bin}/sourcekitten", "version"
+  end
+end
