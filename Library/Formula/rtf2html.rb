3ef0ab4c66375e3548239f7af96f7a8fc61a293e
Ryan Duryea <aguynamedryan@gmail.com>
2015-01-16 12:06:39 -0800

    Update rtf2html.rb
    Drop version

--- Library/Formula/rtf2html.rb
+++ Library/Formula/rtf2html.rb
@@ -2,7 +2,6 @@ class Rtf2html < Formula
   homepage "http://sourceforge.net/projects/rtf2html-lite/"
   url "https://downloads.sourceforge.net/project/rtf2html-lite/RTF%202%20HTML%20Lite%20-%20Sources/2008-10-03%2C%20first%20LGPL%20release/rtf2html-lite-20081003.tar.bz2"
   sha1 "eec3b29633f44290fcab3acd5336462cfc737140"
-  version "20081003"
 
   def install
     system "make"
