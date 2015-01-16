ac01b378ff9ba3c77f0e5791c668468f97d68ba9
Michiaki Ariga <chezou+github@gmail.com>
2015-01-16 09:53:17 +0900

    kytea 0.4.7
    Closes #35924.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/kytea.rb
+++ Library/Formula/kytea.rb
@@ -2,17 +2,8 @@ require 'formula'
 
 class Kytea < Formula
   homepage 'http://www.phontron.com/kytea/'
-
-  stable do
-    url 'http://www.phontron.com/kytea/download/kytea-0.4.6.tar.gz'
-    sha1 '2fb22c64a7babff26d95874877d83a9ef1f09617'
-
-    # Upstream patch to fix compilation on OS X
-    patch do
-      url "https://github.com/neubig/kytea/commit/782553f61bde4f3366f946c9a390500c028fa17f.diff"
-      sha1 "260d62738e53622873b1d467b44576860e5141fe"
-    end
-  end
+  url 'http://www.phontron.com/kytea/download/kytea-0.4.7.tar.gz'
+  sha1 '684f6b90bd4ae048ea3a6fc9762b1baecb006eb3'
 
   head do
     url 'https://github.com/neubig/kytea.git'
