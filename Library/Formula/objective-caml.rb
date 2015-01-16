f3406f5a8f22b06a01380ba96ab3eca69099ab7d
Jack Nagel <jacknagel@gmail.com>
2015-01-15 23:21:11 -0500

    ocaml: get rid of site-lib symlink shenanigans
    This hack probably should have been removed at
    e995ddde8e8186f8fd9651f3d6fe69546544692e.

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -1,20 +1,15 @@
-require "formula"
-
 class ObjectiveCaml < Formula
   homepage "http://ocaml.org"
   url "http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.1.tar.gz"
   sha1 "6af8c67f2badece81d8e1d1ce70568a16e42313e"
+  revision 2
 
   head "http://caml.inria.fr/svn/ocaml/trunk", :using => :svn
-  revision 1
 
   option "with-x11", "Install with the Graphics module"
   depends_on :x11 => :optional
 
   bottle do
-    sha1 "9a734836f27712fbd3a454152100fcd696932bf8" => :yosemite
-    sha1 "4d80628d7bea7d38888dd3ceb4fb1393613ccea1" => :mavericks
-    sha1 "625826bbc9c7fc5b082abbe59b099f17032a3a00" => :mountain_lion
   end
 
   def install
@@ -33,12 +28,5 @@ class ObjectiveCaml < Formula
     system "make", "opt"
     system "make", "opt.opt"
     system "make", "PREFIX=#{prefix}", "install"
-    (lib/"ocaml/site-lib").mkpath
-  end
-
-  def post_install
-    # site-lib in the Cellar will be a symlink to the HOMEBREW_PREFIX location,
-    # which is mkpath'd by Keg#link when something installs into it
-    (lib/"ocaml").install_symlink HOMEBREW_PREFIX/"lib/ocaml/site-lib"
   end
 end
