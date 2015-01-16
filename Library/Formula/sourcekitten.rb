89286b69427729633d5cea6db57b9245772b0f37
JP Simard <jp@jpsim.com>
2015-01-16 10:10:57 -0800

    changes from PR review

--- Library/Formula/sourcekitten.rb
+++ Library/Formula/sourcekitten.rb
@@ -1,4 +1,4 @@
-class SourceKitten < Formula
+class Sourcekitten < Formula
   homepage "https://github.com/jpsim/SourceKitten"
   url "https://github.com/jpsim/SourceKitten.git", :tag => "0.3.0"
   head "https://github.com/jpsim/SourceKitten.git"
@@ -7,10 +7,10 @@ class SourceKitten < Formula
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}"
-    system "make", "clean"
+    system "make", "clean" # remove temporary installation files in /tmp/SourceKitten.dst
   end
 
   test do
-    system "#{bin}/sourcekitten", "version"
+    system "#{bin}/sourcekitten", "syntax", "--text", "import Foundation // Hello World"
   end
 end
