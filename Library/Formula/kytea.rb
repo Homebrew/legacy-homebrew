fc8eed4a108f1421ce5083df5a1bae9c8c06e4eb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-16 01:05:35 +0000

    kytea: add 0.4.7 bottle.

--- Library/Formula/kytea.rb
+++ Library/Formula/kytea.rb
@@ -5,6 +5,12 @@ class Kytea < Formula
   url 'http://www.phontron.com/kytea/download/kytea-0.4.7.tar.gz'
   sha1 '684f6b90bd4ae048ea3a6fc9762b1baecb006eb3'
 
+  bottle do
+    sha1 "7a04bfa95710808c1f0f26e9c384d10705ca367d" => :yosemite
+    sha1 "b1631db9325f7f52c54edbff0712a7b513047c9e" => :mavericks
+    sha1 "5d0b41f2112c6c06ec088c2fc2e32dd8c60a6790" => :mountain_lion
+  end
+
   head do
     url 'https://github.com/neubig/kytea.git'
     depends_on 'autoconf' => :build
