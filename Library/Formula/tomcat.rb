82d7dc446f7718089d6a5a9cdfcb7a160632e093
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-16 05:03:36 +0000

    tomcat: update 8.0.17 bottle.

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -7,9 +7,9 @@ class Tomcat < Formula
 
   bottle do
     cellar :any
-    sha1 "afcce9cce701aa992521e912a64820c5cf281d76" => :yosemite
-    sha1 "31ac8950914cd7cb17d51979d2e51c10e5071443" => :mavericks
-    sha1 "f4f9f3a45f15ff71e8f7a8c5468227cfc374f158" => :mountain_lion
+    sha1 "7bfb1b89afdeed253a724a4c3d2e112ba8b01fb0" => :yosemite
+    sha1 "5b86437af62f3df9870634e20a22bf1ad0ce7d3e" => :mavericks
+    sha1 "3006844e9d23c6253fbad346476c97694170d2d2" => :mountain_lion
   end
 
   option "with-fulldocs", "Install full documentation locally"
