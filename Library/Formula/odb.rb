8073ee88a781c5c5d80e0739bcd57e23c1892c42
leezii <admin@leezii.com>
2015-01-17 21:42:04 +0800

    fix for brew audit failure

--- Library/Formula/odb.rb
+++ Library/Formula/odb.rb
@@ -1,5 +1,3 @@
-require "formula"
-
 class Odb < Formula
   homepage "http://www.codesynthesis.com/products/odb"
   url "http://www.codesynthesis.com/download/odb/2.3/odb-2.3.0.tar.gz"
@@ -9,27 +7,22 @@ class Odb < Formula
   fails_with :gcc_4_0
   fails_with :gcc
   fails_with :llvm
-  
   depends_on "gcc"
 
   resource "libcutl" do
     url "http://www.codesynthesis.com/download/libcutl/1.8/libcutl-1.8.1.tar.gz"
     sha1 "5411892a2959b6164321ebfb6e8e52255786b143"
   end
-  
   patch do
     url "http://codesynthesis.com/~boris/tmp/odb/odb-2.3.0-gcc-4.9.0.patch"
     sha1 "9d0fe9db9d8667c76cd6a1bb15e911560796b656"
   end
-  
   def install
     resource("libcutl").stage{
       system "./configure", "--prefix=#{prefix}/libcutl"
       system "make", "install"
     }
-    
     ENV.append "CXXFLAGS", "-fno-devirtualize -I#{prefix}/libcutl/include -L#{prefix}/libcutl/lib -I/usr/local/include"
-    #print ENV["CXXFLAGS"]
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
     bin.env_script_all_files(libexec/"bin", {})
@@ -40,5 +33,4 @@ class Odb < Formula
   test do
     system "odb", "--help"
   end
-
 end
