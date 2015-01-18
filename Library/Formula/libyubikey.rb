dfd72fa58432c4e9d5875d9442cd30be7de69518
Jack Nagel <jacknagel@gmail.com>
2015-01-17 23:06:22 -0600

    libyubikey 1.12

--- Library/Formula/libyubikey.rb
+++ Library/Formula/libyubikey.rb
@@ -1,15 +1,9 @@
-require 'formula'
-
 class Libyubikey < Formula
-  homepage 'http://yubico.github.io/yubico-c/'
-  url 'http://yubico.github.io/yubico-c/releases/libyubikey-1.11.tar.gz'
-  sha1 'a939abc129ed66af193d979765a8d8ac59ad7c40'
+  homepage "http://yubico.github.io/yubico-c/"
+  url "https://developers.yubico.com/yubico-c/Releases/libyubikey-1.12.tar.gz"
+  sha1 "6a73d548e61f0b622a9447917f03c78686ab386d"
 
   bottle do
-    cellar :any
-    revision 1
-    sha1 "daad568fcab3dd93b6e04c5f387556d6967c0861" => :yosemite
-    sha1 "6180685ff58f83470f2b92ee3537b03c4de6fe96" => :mavericks
   end
 
   option :universal
