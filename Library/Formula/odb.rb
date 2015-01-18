4a6f37f2739f9f76a55d89d041d4be82f5a5450b
leezii <admin@leezii.com>
2015-01-18 11:10:23 +0800

    Add testcase.

--- Library/Formula/odb.rb
+++ Library/Formula/odb.rb
@@ -8,21 +8,24 @@ class Odb < Formula
   fails_with :gcc
   fails_with :llvm
   depends_on "gcc"
-
+  depends_on "libodb"
+  
   resource "libcutl" do
     url "http://www.codesynthesis.com/download/libcutl/1.8/libcutl-1.8.1.tar.gz"
     sha1 "5411892a2959b6164321ebfb6e8e52255786b143"
   end
+
   patch do
     url "http://codesynthesis.com/~boris/tmp/odb/odb-2.3.0-gcc-4.9.0.patch"
     sha1 "9d0fe9db9d8667c76cd6a1bb15e911560796b656"
   end
+
   def install
     resource("libcutl").stage{
       system "./configure", "--prefix=#{prefix}/libcutl"
       system "make", "install"
     }
-    ENV.append "CXXFLAGS", "-fno-devirtualize -I#{prefix}/libcutl/include -L#{prefix}/libcutl/lib -I/usr/local/include"
+    ENV.append "CXXFLAGS", "-fno-devirtualize -I#{prefix}/libcutl/include -L#{prefix}/libcutl/lib"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
     bin.env_script_all_files(libexec/"bin", {})
@@ -31,6 +34,26 @@ class Odb < Formula
   end
 
   test do
-    system "odb", "--help"
+    (testpath/'person.hxx').write <<-EOS.undent
+#include <string>
+
+#include <odb/core.hxx>     
+
+#pragma db object           
+class person
+{
+private:
+  friend class odb::access; 
+
+  #pragma db id auto        
+  unsigned long id_;        
+
+  std::string first_;
+  std::string last_;
+  unsigned short age_;
+};
+EOS
+    system "odb", "-d common", "--generate-query", "person.hxx"
   end
+
 end
