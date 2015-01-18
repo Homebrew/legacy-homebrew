cbf1db8bd850bdb79f1662c1ec0ddc28a9d40894
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-19 00:16:57 +0100

    cowsay: modernize, homepage fixed

--- Library/Formula/cowsay.rb
+++ Library/Formula/cowsay.rb
@@ -1,16 +1,14 @@
-require 'formula'
-
 class Cowsay < Formula
-  homepage 'http://www.nog.net/~tony/warez/cowsay.shtml'
-  url 'http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/c/cowsay/cowsay_3.03.orig.tar.gz'
-  sha1 'cc65a9b13295c87df94a58caa8a9176ce5ec4a27'
+  homepage "https://web.archive.org/web/20120225123719/http://www.nog.net/~tony/warez/cowsay.shtml"
+  url "http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/c/cowsay/cowsay_3.03.orig.tar.gz"
+  sha1 "cc65a9b13295c87df94a58caa8a9176ce5ec4a27"
 
   # Official download is 404:
-  # url 'http://www.nog.net/~tony/warez/cowsay-3.03.tar.gz'
+  # url "http://www.nog.net/~tony/warez/cowsay-3.03.tar.gz"
 
   def install
     system "/bin/sh", "install.sh", prefix
-    mv prefix/'man', share
+    mv prefix/"man", share
   end
 
   test do
