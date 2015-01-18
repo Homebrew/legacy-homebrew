d666f6efcf4d99340143bc39bbcc7d8c51be8a97
Jack Nagel <jacknagel@gmail.com>
2015-01-18 00:12:14 -0600

    csync 0.50.0

--- Library/Formula/csync.rb
+++ Library/Formula/csync.rb
@@ -1,22 +1,9 @@
-require 'formula'
-
 class Csync < Formula
-  homepage 'http://www.csync.org/'
+  homepage "http://www.csync.org/"
+  url "https://open.cryptomilk.org/attachments/download/27/csync-0.50.0.tar.xz"
+  sha1 "8df896be17f7f038260159469a6968a9d563cb3c"
 
-  stable do
-    url 'http://www.csync.org/files/csync-0.49.9.tar.gz'
-    sha1 'fd7df6c13aa6fc6de74cb48c2ac35ad11f6d895d'
-
-    depends_on 'log4c'
-    depends_on 'samba'
-  end
-
-  head do
-    url 'git://git.csync.org/projects/csync.git'
-    # Log4c and libsmbclient are optional in HEAD.
-    depends_on 'log4c' => :optional
-    depends_on 'samba' => :optional
-  end
+  head "git://git.csync.org/projects/csync.git"
 
   depends_on 'check' => :build
   depends_on 'cmake' => :build
@@ -25,13 +12,13 @@ class Csync < Formula
   depends_on 'iniparser'
   depends_on 'sqlite'
   depends_on 'libssh' => :optional
+  depends_on 'log4c' => :optional
+  depends_on 'samba' => :optional
 
   depends_on :macos => :lion
 
-  patch :DATA
-
   def install
-    mkdir 'build' unless build.head?
+    mkdir "build" unless build.head?
     cd 'build' do
       system "cmake", "..", *std_cmake_args
       # We need to run make csync first to make the "core",
@@ -53,18 +40,3 @@ class Csync < Formula
     system bin/"csync", "-V"
   end
 end
-
-__END__
---- a/src/csync_propagate.c 2012-07-01 13:12:12.000000000 +0200
-+++ b/src/csync_propagate.c 2012-07-01 13:12:59.000000000 +0200
-@@ -101,10 +101,6 @@
-   /* Open the source file */
-   ctx->replica = srep;
-   flags = O_RDONLY|O_NOFOLLOW;
--  /* O_NOATIME can only be set by the owner of the file or the superuser */
--  if (st->uid == ctx->pwd.uid || ctx->pwd.euid == 0) {
--    flags |= O_NOATIME;
--  }
-   sfp = csync_vio_open(ctx, suri, flags, 0);
-   if (sfp == NULL) {
-     if (errno == ENOMEM) {
