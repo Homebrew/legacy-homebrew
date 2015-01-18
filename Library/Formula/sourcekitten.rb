f5c81386e3b51eb4e3e3b44319456d6eeae05c00
JP Simard <jp@jpsim.com>
2015-01-18 15:19:54 -0800

    move temporary folder to buildpath

--- Library/Formula/sourcekitten.rb
+++ Library/Formula/sourcekitten.rb
@@ -1,16 +1,12 @@
 class Sourcekitten < Formula
   homepage "https://github.com/jpsim/SourceKitten"
-  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.3.0"
+  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.3.1"
   head "https://github.com/jpsim/SourceKitten.git"
 
   depends_on :xcode => ["6.1.1", :build]
 
   def install
-    # FIXME: Support installing with a custom TEMPORARY_FOLDER so that
-    #        homebrew can automatically clean up the directory.
-    #        https://github.com/jpsim/SourceKitten/issues/28
-    system "make", "prefix_install", "PREFIX=#{prefix}"
-    system "make", "clean" # remove temporary installation files in /tmp/SourceKitten.dst
+    system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SourceKitten.dst"
   end
 
   test do
