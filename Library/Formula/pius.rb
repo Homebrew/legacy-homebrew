dd5ba29d3e98accc15f20b4fea3ea676f9aabafe
Xu Cheng <xucheng@me.com>
2015-01-18 21:57:54 +0800

    pius: modernize and add caveats and test
    Closes #35996.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pius.rb
+++ Library/Formula/pius.rb
@@ -1,34 +1,26 @@
-require 'formula'
-class GnupgInstalled < Requirement
-  fatal true
-
-  satisfy { which('gpg') || which('gpg2') }
-
-  def message; <<-EOS.undent
-    Gnupg is required to use these tools.
-
-    You can install Gnupg or Gnupg2 with Homebrew:
-      brew install gnupg
-      brew install gnupg2
-
-    Or you can use one of several different
-    prepackaged installers that are available.
-    EOS
-  end
-end
-
 class Pius < Formula
-  homepage 'http://www.phildev.net/pius/'
-  url 'https://downloads.sourceforge.net/project/pgpius/pius/2.0.11/pius-2.0.11.tar.bz2'
-  sha1 '0c9b74f271bf195d8636d8406fbb56cc024195ad'
+  homepage "http://www.phildev.net/pius/"
+  url "https://downloads.sourceforge.net/project/pgpius/pius/2.0.11/pius-2.0.11.tar.bz2"
+  sha1 "0c9b74f271bf195d8636d8406fbb56cc024195ad"
 
-  depends_on GnupgInstalled
+  depends_on :gpg
 
   def install
     # Replace hardcoded gpg path: https://sourceforge.net/p/pgpius/bugs/12/
-    inreplace 'pius', '/usr/bin/gpg', HOMEBREW_PREFIX/'bin/gpg'
-    bin.install 'pius'
-    bin.install 'pius-keyring-mgr'
-    bin.install 'pius-party-worksheet'
+    inreplace "pius", "/usr/bin/gpg", HOMEBREW_PREFIX/"bin/gpg"
+    bin.install "pius"
+    bin.install "pius-keyring-mgr"
+    bin.install "pius-party-worksheet"
+  end
+
+  def caveats; <<-EOS.undent
+      The gpg path is hardcoded in pius. You can specific your own gpg path
+      by adding follow line into ~/.pius file:
+        gpg-path=/path/to/gpg
+    EOS
+  end
+
+  test do
+    system "#{bin}/pius", "-T"
   end
 end
