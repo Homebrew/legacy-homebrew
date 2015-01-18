038dc13753bc4dc130e28aafa4b3182e5cb69bd4
Mike McQuaid <mike@mikemcquaid.com>
2015-01-18 17:30:33 +0100

    torrentcheck: fix CFLAGS splitting.

--- Library/Formula/torrentcheck.rb
+++ Library/Formula/torrentcheck.rb
@@ -7,7 +7,7 @@ class Torrentcheck < Formula
 
   def install
     inreplace "torrentcheck.c", "#include <malloc.h>", ""
-    system ENV.cc, "torrentcheck.c", "sha1.c", "-o", "torrentcheck", *ENV.cflags.split
+    system ENV.cc, "torrentcheck.c", "sha1.c", "-o", "torrentcheck", *ENV.cflags.to_s.split
     bin.install 'torrentcheck'
   end
 
