4a394e94847404fdf5d873713bc2b4605749dd6e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-18 23:41:52 +0000

    cowsay: add 3.03 bottle.

--- Library/Formula/cowsay.rb
+++ Library/Formula/cowsay.rb
@@ -3,6 +3,12 @@ class Cowsay < Formula
   url "http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/c/cowsay/cowsay_3.03.orig.tar.gz"
   sha1 "cc65a9b13295c87df94a58caa8a9176ce5ec4a27"
 
+  bottle do
+    sha1 "50dcf6ac955d2644d2543728bda2ee02abafcb67" => :yosemite
+    sha1 "79131dfcab303271d332bf69373f11433401f1b3" => :mavericks
+    sha1 "65f118084f895e553faf2eac15335fc23e331b77" => :mountain_lion
+  end
+
   # Official download is 404:
   # url "http://www.nog.net/~tony/warez/cowsay-3.03.tar.gz"
 
