871b003fd41a6a4f0119dcb7db33987e34f5abb0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-18 23:49:54 +0000

    sourcekitten: add 0.3.1 bottle.

--- Library/Formula/sourcekitten.rb
+++ Library/Formula/sourcekitten.rb
@@ -3,6 +3,12 @@ class Sourcekitten < Formula
   url "https://github.com/jpsim/SourceKitten.git", :tag => "0.3.1"
   head "https://github.com/jpsim/SourceKitten.git"
 
+  bottle do
+    cellar :any
+    sha1 "0c2eeaebca3d1e193ff097153d9f8d2afb307964" => :yosemite
+    sha1 "f55c779524fb1dc74554e8e2592d402198bbc55d" => :mavericks
+  end
+
   depends_on :xcode => ["6.1.1", :build]
 
   def install
