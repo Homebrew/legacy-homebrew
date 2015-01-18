0f63081714a7d463e9c30a7e42db7239da3eb8ad
leezii <admin@leezii.com>
2015-01-18 22:07:52 +0800

    ODB: C++ Object-Relational Mapping (ORM)
    Add ODB compiler, ODB runtime library and ODB sqlite library.

---
+++ Library/Formula/libodb-sqlite.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class LibodbSqlite < Formula
+  homepage "http://www.codesynthesis.com/products.odb"
+  url "http://www.codesynthesis.com/download/odb/2.3/libodb-sqlite-2.3.0.tar.bz2"
+  sha1 "512a124e0b78ae36deee25d595e3e169bd24d216"
+
+  depends_on "libodb" => :build
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  def caveats; <<-EOS.undent
+        ODB is undel GPL v2 or NCUEL.
+        Please read http://codesynthesis.com/products/odb/license.xhtml before use.
+        EOS
+  end
+end
