c7b5402a3be7f0b9a7c6275f57b3b4b62b6d0dfb
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2015-01-18 20:00:47 +0300

    scala 2.11.5
    Closes #36001.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/scala.rb
+++ Library/Formula/scala.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Scala < Formula
   homepage 'http://www.scala-lang.org/'
-  url 'http://www.scala-lang.org/files/archive/scala-2.11.4.tgz'
-  sha1 'a6d319b26ccabe9c609fadebc32e797bf9cb1084'
+  url 'http://www.scala-lang.org/files/archive/scala-2.11.5.tgz'
+  sha1 '10820b7d65727fe8d3d67855ef603c425cf65a4e'
 
   bottle do
     cellar :any
@@ -16,13 +16,13 @@ class Scala < Formula
   option 'with-src', 'Also install sources for IDE support'
 
   resource 'docs' do
-    url 'http://www.scala-lang.org/files/archive/scala-docs-2.11.4.zip'
-    sha1 'de6a5545f13542667d8ff795883fdf192effce2f'
+    url 'http://www.scala-lang.org/files/archive/scala-docs-2.11.5.zip'
+    sha1 '4a6856a822fbdda7b76674e999a70666d0cdfc3a'
   end
 
   resource 'src' do
-    url 'https://github.com/scala/scala/archive/v2.11.4.tar.gz'
-    sha1 '15f9a8f1d3947b5e1ddd3c653968481626caf418'
+    url 'https://github.com/scala/scala/archive/v2.11.5.tar.gz'
+    sha1 '065fbaa0b982256c84df20d444dff03368332b38'
   end
 
   resource 'completion' do
