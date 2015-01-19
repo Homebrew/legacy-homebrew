079a8f1ef663bf489f45eace2eea32f1198580ca
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2015-01-19 16:22:25 +0900

    slib: add supported scheme implementation check

--- Library/Formula/slib.rb
+++ Library/Formula/slib.rb
@@ -1,7 +1,36 @@
+class SchemeInstalled < Requirement
+  fatal true
+
+  satisfy { which('bigloo') || which('gsi') || which('gosh') ||
+            which('guile') || which('kawa') || which('scheme48') ||
+            which('scsh') || which('sisc') }
+
+  def message; <<-EOS.undent
+    A Scheme implementation is required to use SLIB.
+
+    You can install one of the SLIB supported Scheme
+    implementation with Homebrew:
+      brew install bigloo
+      brew install gambit-scheme
+      brew install gauche
+      brew install guile
+      brew install kawa
+      brew install scheme48
+      brew install scsh
+      brew install sisc-scheme
+
+    Or you can use one of several different prepackaged installers
+    that are available.
+    EOS
+  end
+end
+
 class Slib < Formula
   homepage "http://people.csail.mit.edu/jaffer/SLIB"
   url "http://groups.csail.mit.edu/mac/ftpdir/scm/slib-3b4.zip"
   sha1 "dda1ed78ff7164738a1a8c51f1f7c08ec1db79eb"
+
+  depends_on SchemeInstalled
 
   def install
     system "./configure", "--prefix=#{prefix}"
