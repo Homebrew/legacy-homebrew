da1bf77cec26be587073a3b8ca7c224b3fa64ee5
Stefan Thiemann <stefan.thiemann@gmail.com>
2015-01-19 19:36:00 +0100

    edit test method

--- Library/Formula/ucspi-tools.rb
+++ Library/Formula/ucspi-tools.rb
@@ -12,17 +12,16 @@ class UcspiTools < Formula
   end
 
   test do
-    (testpath/"http.sh").write <<-EOL
-#!/bin/sh
-printf "GET / HTTP/1.1\\r\\n" >&6
-printf "Host: github.com\\r\\n" >&6
-printf "\\r\\n" >&6
-cat - <&7
-EOL
+    (testpath/"http.sh").write <<-EOS.undent
+      #!/bin/sh
+      printf "GET / HTTP/1.1\\r\\n" >&6
+      printf "Host: github.com\\r\\n" >&6
+      printf "Connection: Close\\r\\n" >&6
+      printf "\\r\\n" >&6
+      grep -m 1 "HTTP/1.1 200 OK" <&7
+    EOS
     chmod 0755, testpath/"http.sh"
-    out = shell_output("tcpclient github.com 443 #{bin}/tlsc -CH #{testpath}/http.sh", 1)
+    out = shell_output("tcpclient -4 GitHub.com 443 #{bin}/tlsc -C #{testpath}/http.sh", 1)
     assert_match %r{HTTP/1.1 200 OK}, out
-    assert_match /Status: 200 OK/, out
-    assert_match /<!DOCTYPE html>/, out
   end
 end
