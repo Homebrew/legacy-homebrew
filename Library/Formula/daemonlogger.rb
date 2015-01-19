d0bd0638522494e7ebd74fef7fb32663114a8567
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-19 10:00:53 +0100

    daemonlogger: modernize, homepage fixed
    Closes #36022.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/daemonlogger.rb
+++ Library/Formula/daemonlogger.rb
@@ -1,15 +1,16 @@
-require 'formula'
-
 class Daemonlogger < Formula
-  homepage 'http://www.snort.org/snort-downloads/additional-downloads#daemonlogger'
-  url 'http://www.snort.org/downloads/463'
-  version '1.2.1'
-  sha1 'ce0aa20b38f18eca94a6d00fe9126d441fe2818a'
+  homepage "http://sourceforge.net/projects/daemonlogger/"
+  url "https://downloads.sourceforge.net/project/daemonlogger/daemonlogger-1.2.1.tar.gz"
+  sha1 "ce0aa20b38f18eca94a6d00fe9126d441fe2818a"
 
-  depends_on 'libdnet'
+  depends_on "libdnet"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/daemonlogger", "-h"
   end
 end
