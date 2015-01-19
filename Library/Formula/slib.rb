225aaa8741b9a5e84dc83008ad0693ac5ed368fc
Kiyoshi Mizumaru <kiyoshi.mizumaru@gmail.com>
2015-01-19 12:06:51 +0900

    slib: remove version statement from formula

--- Library/Formula/slib.rb
+++ Library/Formula/slib.rb
@@ -1,7 +1,6 @@
 class Slib < Formula
   homepage "http://people.csail.mit.edu/jaffer/SLIB"
   url "http://groups.csail.mit.edu/mac/ftpdir/scm/slib-3b4.zip"
-  version "3b4"
   sha1 "dda1ed78ff7164738a1a8c51f1f7c08ec1db79eb"
 
   def install
