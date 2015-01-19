051acd7729ec66c49ec2759bbe4f44cbcdf8c84e
Nate Rauh <NRauh@users.noreply.github.com>
2015-01-19 02:48:43 -0600

    WordGrinder 0.5.2 (new formula)
    Formula for WordGrinder, a terminal based word processor.

---
+++ Library/Formula/wordgrinder.rb
@@ -0,0 +1,15 @@
+class Wordgrinder < Formula
+  homepage "http://wordgrinder.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/wordgrinder/wordgrinder/wordgrinder-0.5.2.tar.bz2"
+  sha1 "4bcc600fbd110d9d87961feab15045b9b518b2db"
+
+  depends_on "lua"
+  depends_on "ncurses" => :build
+
+  def install
+    system "make"
+    (bin).install "bin/wordgrinder"
+    (man1).install "wordgrinder.man" => "wordgrinder.1"
+    (doc).install "README.wg"
+  end
+end
