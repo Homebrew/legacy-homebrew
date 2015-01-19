de0146bc524f7da95f74ddc879035f7e2356bd1e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-19 15:45:47 +0000

    pigz: add 2.3.1 bottle.

--- Library/Formula/pigz.rb
+++ Library/Formula/pigz.rb
@@ -3,6 +3,13 @@ class Pigz < Formula
   url 'http://www.zlib.net/pigz/pigz-2.3.1.tar.gz'
   sha1 '2d5c9a70a6afcf1d8986890026d70c96b9346c07'
 
+  bottle do
+    cellar :any
+    sha1 "3aa3d204457ffda3815b62f3f1530caedfede0f4" => :yosemite
+    sha1 "836f8a39e7e524da44cb03a34a5962c690668378" => :mavericks
+    sha1 "65c9d79d6610f35008549afab203e9058f937a20" => :mountain_lion
+  end
+
   # avoid directory traversal vulnerability CVE-2015-1191
   # http://www.openwall.com/lists/oss-security/2015/01/12/4
   # https://github.com/madler/pigz/commit/fdad1406b3ec809f4954ff7cdf9e99eb18c2458f
