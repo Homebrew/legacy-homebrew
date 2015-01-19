0d5d45b848d9557dae11f885fe8b2bccb584fa2d
Misty De Meo <mistydemeo@gmail.com>
2015-01-19 14:12:27 -0800

    raptor 2.0.15

--- Library/Formula/raptor.rb
+++ Library/Formula/raptor.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Raptor < Formula
   homepage 'http://librdf.org/raptor/'
-  url 'http://download.librdf.org/source/raptor2-2.0.14.tar.gz'
-  sha1 'f0dc155ee616aac0e5397dd659519c9d0a262f21'
+  url 'http://download.librdf.org/source/raptor2-2.0.15.tar.gz'
+  sha1 '504231f87024df9aceb90eb957196b557b4b8e38'
 
   bottle do
     cellar :any
