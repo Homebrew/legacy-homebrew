f3e9d162d68a3e6492d41649d6c278d5fca3a598
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-20 10:27:42 +0100

    ec2-api-tools: test added

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -1,11 +1,15 @@
-require 'formula'
-
 class Ec2ApiTools < AmazonWebServicesFormula
-  homepage 'http://aws.amazon.com/developertools/351'
-  url 'http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.7.1.0.zip'
-  sha1 '154acfb5d117d0af1311c65030671cd56eb987aa'
+  homepage "http://aws.amazon.com/developertools/351"
+  url "http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.7.1.0.zip"
+  sha1 "154acfb5d117d0af1311c65030671cd56eb987aa"
 
   def caveats
     standard_instructions "EC2_HOME"
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    ENV["EC2_HOME"] = libexec
+    assert_match version.to_s, shell_output("#{bin}/ec2-version")
   end
 end
