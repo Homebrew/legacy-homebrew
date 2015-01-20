247d96dcc438ea57b8baf88a7c90a5081606b3ae
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-20 11:19:37 +0100

    gdk-pixbuf 2.31.2

--- Library/Formula/gdk-pixbuf.rb
+++ Library/Formula/gdk-pixbuf.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class GdkPixbuf < Formula
-  homepage 'http://gtk.org'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.30/gdk-pixbuf-2.30.8.tar.xz'
-  sha256 '4853830616113db4435837992c0aebd94cbb993c44dc55063cee7f72a7bef8be'
+  homepage "http://gtk.org"
+  url "http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.31/gdk-pixbuf-2.31.2.tar.xz"
+  sha256 "9e467ed09894c802499fb2399cd9a89ed21c81700ce8f27f970a833efb1e47aa"
 
   bottle do
     revision 1
@@ -21,8 +19,8 @@ class GdkPixbuf < Formula
   depends_on "libpng"
   depends_on "gobject-introspection"
 
-  # 'loaders.cache' must be writable by other packages
-  skip_clean 'lib/gdk-pixbuf-2.0'
+  # "loaders.cache" must be writable by other packages
+  skip_clean "lib/gdk-pixbuf-2.0"
 
   def install
     ENV.universal_binary if build.universal?
@@ -38,10 +36,10 @@ class GdkPixbuf < Formula
 
     # Other packages should use the top-level modules directory
     # rather than dumping their files into the gdk-pixbuf keg.
-    inreplace lib/'pkgconfig/gdk-pixbuf-2.0.pc' do |s|
-      libv = s.get_make_var 'gdk_pixbuf_binary_version'
-      s.change_make_var! 'gdk_pixbuf_binarydir',
-        HOMEBREW_PREFIX/'lib/gdk-pixbuf-2.0'/libv
+    inreplace lib/"pkgconfig/gdk-pixbuf-2.0.pc" do |s|
+      libv = s.get_make_var "gdk_pixbuf_binary_version"
+      s.change_make_var! "gdk_pixbuf_binarydir",
+        HOMEBREW_PREFIX/"lib/gdk-pixbuf-2.0"/libv
     end
   end
 
@@ -57,5 +55,9 @@ class GdkPixbuf < Formula
     If you need to manually update the query loader cache, set GDK_PIXBUF_MODULEDIR then run
       #{bin}/gdk-pixbuf-query-loaders --update-cache
     EOS
+  end
+
+  test do
+    system "#{bin}/gdk-pixbuf-pixdata", "-h"
   end
 end
