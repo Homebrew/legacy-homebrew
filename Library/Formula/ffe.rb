8bbf5ca99cb24cf9067c28f1e7361b89b7de5978
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-20 17:14:31 +0000

    ffe: add 0.3.4 bottle.

--- Library/Formula/ffe.rb
+++ Library/Formula/ffe.rb
@@ -3,6 +3,13 @@ class Ffe < Formula
   url "https://downloads.sourceforge.net/project/ff-extractor/ff-extractor/0.3.4/ffe-0.3.4.tar.gz"
   sha1 "9e0f435568a65ef552d71e1a10a12f0627e1ccf9"
 
+  bottle do
+    cellar :any
+    sha1 "7410c7c429880b06631ad40ae2de9c327a753a2b" => :yosemite
+    sha1 "ae4d3ab7b308b788b26051ae5df452a473cbc573" => :mavericks
+    sha1 "d58fb6890e7732247a1a99fb12a4dd131c7cabd3" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
