d658e3d30de50e9352b0a3bc63efab102fab3997
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-01-20 20:12:43 +0100

    cloud-watch: pass strict audit, use https
    Closes #36074.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cloud-watch.rb
+++ Library/Formula/cloud-watch.rb
@@ -1,16 +1,21 @@
-require 'formula'
-
 class CloudWatch < AmazonWebServicesFormula
-  homepage 'http://aws.amazon.com/developertools/2534'
-  url 'http://ec2-downloads.s3.amazonaws.com/CloudWatch-2010-08-01.zip'
-  version '1.0.20.0'
-  sha1 'c3f5b856b3ff8e1ae06d0ad5db5dd27c214c4881'
+  homepage "https://aws.amazon.com/developertools/2534"
+  url "https://ec2-downloads.s3.amazonaws.com/CloudWatch-2010-08-01.zip"
+  version "1.0.20.0"
+  sha1 "c3f5b856b3ff8e1ae06d0ad5db5dd27c214c4881"
 
   def caveats
     s = standard_instructions "AWS_CLOUDWATCH_HOME"
     s += <<-EOS.undent
       export SERVICE_HOME="$AWS_CLOUDWATCH_HOME"
     EOS
-    return s
+    s
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    ENV["AWS_CLOUDWATCH_HOME"] = libexec
+    ENV["SERVICE_HOME"] = libexec
+    assert_match /w.x.y.z/, shell_output("#{bin}/mon-version")
   end
 end
