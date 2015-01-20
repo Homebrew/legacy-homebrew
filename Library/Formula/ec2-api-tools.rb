bc071fb5448628aea8f066bbc0f37b0ecb4f11ee
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-01-20 20:17:13 +0100

    ec2-api-tools: use https
    Closes #36077.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -1,6 +1,6 @@
 class Ec2ApiTools < AmazonWebServicesFormula
-  homepage "http://aws.amazon.com/developertools/351"
-  url "http://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.7.1.0.zip"
+  homepage "https://aws.amazon.com/developertools/351"
+  url "https://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.7.1.0.zip"
   sha1 "154acfb5d117d0af1311c65030671cd56eb987aa"
 
   def caveats
