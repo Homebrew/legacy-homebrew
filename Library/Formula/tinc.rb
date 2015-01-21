8a83285fbf06436b614bbf05774d0969df1de815
Alexis Hildebrandt <afh@surryhill.net>
2015-01-21 22:42:27 +0100

    tinc 1.0.25

--- Library/Formula/tinc.rb
+++ Library/Formula/tinc.rb
@@ -2,9 +2,8 @@ require "formula"
 
 class Tinc < Formula
   homepage "http://www.tinc-vpn.org"
-  url "http://tinc-vpn.org/packages/tinc-1.0.24.tar.gz"
-  sha1 "e32f56b234922570a9a8a267b1143e2752133696"
-  revision 1
+  url "http://tinc-vpn.org/packages/tinc-1.0.25.tar.gz"
+  sha1 "b728e1ed02862aab04b2a53a62c2f67edd0c3949"
 
   bottle do
     sha1 "b41c2f938dc74d722fc671dba917162c593903ce" => :mavericks
@@ -21,13 +20,6 @@ class Tinc < Formula
   depends_on "openssl"
 
   def install
-    # Tinc does not automatically link against libresolv on Mac OS X.
-    # A fix has been already merged upstream. When updating this formula
-    # make sure the following changes have been applied:
-    # https://github.com/gsliepen/tinc/commit/241670ec23d05800e0a04957d6293de9a39075fb
-    # and remove this comment in addition to the next line.
-    ENV.append "LDFLAGS", "-lresolv"
-
     system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}",
                           "--with-openssl=#{Formula["openssl"].opt_prefix}"
     system "make"
