db10f837d384b056a9da3be99f2802dfc209044b
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-01-23 20:14:11 +0100

    one simple -v for testing

--- Library/Formula/galen-bin.rb
+++ Library/Formula/galen-bin.rb
@@ -18,31 +18,7 @@ class GalenBin < Formula
   end
 
   test do
-    # selenium looks only into /Applications (https://code.google.com/p/selenium/source/browse/java/client/src/org/openqa/selenium/firefox/internal/Executable.java)
-    if File.executable?("/Applications/Firefox.app/Contents/MacOS/firefox-bin")
-      (testpath/"homepage.spec").write <<-EOS.undent
-           ==================================================
-           header              id      header
-           ==================================================
-
-           header
-               height: 100px
-      EOS
-
-      (testpath/"my.test").write <<-EOS.undent
-           Homepage in local Firefox browser
-               selenium firefox http://samples.galenframework.com/tutorial1/tutorial1.html 640x480
-                   check #{testpath}/homepage.spec
-      EOS
-
-      output = shell_output("#{bin}/galen test #{testpath}/my.test")
-      # print output
-      assert output.include?("Status: PASS")
-    else
-      # print "Firefox not found, just checked galen version\n"
-
-      output = shell_output("#{bin}/galen -v")
-      assert output.include?("Galen Framework\nVersion: 1.4.10")
-    end
+    output = shell_output("#{bin}/galen -v")
+    assert output.include?("Galen Framework\nVersion: 1.4.10")
   end
 end
