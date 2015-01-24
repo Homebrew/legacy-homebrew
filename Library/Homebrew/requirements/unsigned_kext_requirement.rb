8ff7d86cf9c19434967c592d73fc664e4c6ab675
Doug DeJulio <ddj@aisb.org>
2015-01-23 19:13:22 -0500

    Revert "Added support for Yosemite systems booted with boot-args including"
    This reverts commit 41345b2ce5f6010a1c22d6880822b3cc5084df14.  They
    suggested a different approach.

--- Library/Homebrew/requirements/unsigned_kext_requirement.rb
+++ Library/Homebrew/requirements/unsigned_kext_requirement.rb
@@ -1,19 +1,14 @@
 require 'requirement'
-require 'extend/ARGV'
 
 class UnsignedKextRequirement < Requirement
   fatal true
 
-  
-  satisfy { MacOS.version < :yosemite || ARGV.kext_dev_mode? }
+  satisfy { MacOS.version < :yosemite }
 
   def message
     s = <<-EOS.undent
       Building this formula from source isn't possible due to OS X
-      Yosemite and above's strict unsigned kext ban, unless you run
-      in kext development mode.  To indicate that, either use the
-      --kext-dev-mode argument or set the HOMEBREW_KEXT_DEV_MODE
-      environment variable.
+      Yosemite and above's strict unsigned kext ban.
     EOS
     s += super
     s
