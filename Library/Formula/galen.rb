944a2f2a8e8e56e7b72426856551faabedfe6bb9
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-01-24 11:28:31 +0100

    renamed to galen (suggested by Mike)

--- Library/Formula/galen.rb
+++ Library/Formula/galen.rb
@@ -1,4 +1,4 @@
-class GalenBin < Formula
+class Galen < Formula
   homepage "http://galenframework.com/"
   url "https://github.com/galenframework/galen/releases/download/galen-1.4.10/galen-bin-1.4.10.zip"
   sha1 "237896138a244d1a168a3220d07edd88f6f39da9"
