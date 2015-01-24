4a45caeffb7abaf7b439fea41c172b757e916cc0
Arthur Liu <arthurhsliu@gmail.com>
2015-01-25 09:57:04 +1100

    use sha1 instead of sha256

--- Library/Formula/libforensic1394.rb
+++ Library/Formula/libforensic1394.rb
@@ -1,7 +1,7 @@
 class Libforensic1394 < Formula
   homepage "https://freddie.witherden.org/tools/libforensic1394/"
   url "https://freddie.witherden.org/tools/libforensic1394/releases/libforensic1394-0.2.tar.gz"
-  sha256 "50a82fe2899aa901104055da2ac00b4c438cf1d0d991f5ec1215d4658414652e"
+  sha1 "179526652977c291303e4f7edc3c7ed487eb0eca"
   head "git://git.freddie.witherden.org/forensic1394.git"
 
   depends_on "cmake" => :build
