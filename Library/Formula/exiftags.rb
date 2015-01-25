fccd322412c074d52608cb45dda7084c056a18f2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-25 21:47:52 +0000

    exiftags: add 1.01 bottle.

--- Library/Formula/exiftags.rb
+++ Library/Formula/exiftags.rb
@@ -3,6 +3,13 @@ class Exiftags < Formula
   url "http://johnst.org/sw/exiftags/exiftags-1.01.tar.gz"
   sha1 "06636feb7d5c5835da01d5da8cd0f4a291d23fd8"
 
+  bottle do
+    cellar :any
+    sha1 "f798eb88c317e58eb1d37c1da9f4755d9a6f478c" => :yosemite
+    sha1 "5dbb67622e47f1b1cdc3e9810ad84dc2d4746ae3" => :mavericks
+    sha1 "a427dbfca42716ba54f26d8f2c9d5cbe543dc8bc" => :mountain_lion
+  end
+
   def install
     bin.mkpath
     man1.mkpath
