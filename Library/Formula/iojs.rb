86d926cdf255f51ac62d348de3657c877d851854
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-25 01:31:08 +0000

    iojs: add 1.0.4 bottle.

--- Library/Formula/iojs.rb
+++ Library/Formula/iojs.rb
@@ -3,6 +3,12 @@ class Iojs < Formula
   url "https://iojs.org/dist/v1.0.4/iojs-v1.0.4.tar.xz"
   sha256 "c902f5abbd59c56346680f0b4a71056c51610847b9576acf83a9c210bf664e98"
 
+  bottle do
+    sha1 "a88bf32209a487ed8329476325aeeed2cc5ddb33" => :yosemite
+    sha1 "1bb64de12cdbfc2c5fc4aa662c1025ee2bed84b2" => :mavericks
+    sha1 "f62a32113ae476095c7bcdee8dffe6c87f3675a8" => :mountain_lion
+  end
+
   keg_only "`iojs` symlinks conflict with `node` but can be used by prepending your PATH"
 
   option "with-debug", "Build with debugger hooks"
