1bc50992250e239fd7692170f8a27e8a9215bf44
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-26 15:39:43 +0000

    juman: add 7.01 bottle.

--- Library/Formula/juman.rb
+++ Library/Formula/juman.rb
@@ -3,6 +3,12 @@ class Juman < Formula
   url "http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/juman/juman-7.01.tar.bz2"
   sha1 "e22eb113f40216b8a04d811484a94f93f834ae9a"
 
+  bottle do
+    sha1 "124c6163b63a4ae87269419329ebb65c810f30b2" => :yosemite
+    sha1 "d3c9051c201101118fdde6ae5a49dea75911a59e" => :mavericks
+    sha1 "d886637082bfb1a55c528077abcaaf43d6b54372" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
