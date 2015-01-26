76cf7878d30acacbb2b8d67171f880bdbc08d4e5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-26 12:09:11 +0000

    libsoup: update 2.48.1 bottle.

--- Library/Formula/libsoup.rb
+++ Library/Formula/libsoup.rb
@@ -6,10 +6,9 @@ class Libsoup < Formula
   sha256 '9b0d14b36e36a3131a06c6e3aa7245716e6904e3e636df81c0b6c8bd3f646f9a'
 
   bottle do
-    revision 1
-    sha1 "dc4dcb53c9da238e99feb055e70789eb54a2e98f" => :yosemite
-    sha1 "4c7ab2c3b250ace75f099f80dd9ba36f4b277550" => :mavericks
-    sha1 "693146c16d4105de3716252f2da9cf35156c0328" => :mountain_lion
+    sha1 "58bfb3f803cdb164301d33754d80cf0b12b44f4c" => :yosemite
+    sha1 "6701311c286723f87dc8699a150b9085f5b14431" => :mavericks
+    sha1 "94ad9a680bdd06cb14721217ffbe091c2f46c143" => :mountain_lion
   end
 
   depends_on 'pkg-config' => :build
