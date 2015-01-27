45efbbf1b7b1f314109e95bfd3849711b7317ca9
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-27 10:31:48 +0100

    f3 5.0
    Closes #36263.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/f3.rb
+++ Library/Formula/f3.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class F3 < Formula
   homepage "http://oss.digirati.com.br/f3/"
-  url "https://github.com/AltraMayor/f3/archive/v3.0.tar.gz"
-  sha1 "9e0d2ddec98c09be17b5d343bd6d5fac2606a963"
+  url "https://github.com/AltraMayor/f3/archive/v5.0.tar.gz"
+  sha1 "be28f55ea8450a388a0ea63dc1f810080d822d22"
 
   head "https://github.com/AltraMayor/f3.git"
 
@@ -15,8 +13,8 @@ class F3 < Formula
   end
 
   def install
-    system "make", "mac"
-    bin.install "f3read", "f3write"
+    system "make", "all"
+    bin.install %w[f3read f3write]
     man1.install "f3read.1"
     man1.install_symlink "f3read.1" => "f3write.1"
   end
