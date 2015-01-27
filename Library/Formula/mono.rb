8e8fcfdfc24011329349cd0c4d6755c0a814419c
Henrik Feldt <henrik@haf.se>
2015-01-27 17:53:27 +0100

    [mono] support building HEAD

--- Library/Formula/mono.rb
+++ Library/Formula/mono.rb
@@ -4,6 +4,7 @@ class Mono < Formula
   homepage "http://www.mono-project.com/"
   url "http://download.mono-project.com/sources/mono/mono-3.12.0.tar.bz2"
   sha1 "cec83efd13ffc1e212c632395a5aac75772a09e7"
+  head 'https://github.com/mono/mono.git'
 
   # xbuild requires the .exe files inside the runtime directories to
   # be executable
