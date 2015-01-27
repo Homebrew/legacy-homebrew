a53e278ab1e3f692a070c149af9ba22d74f90667
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-27 10:15:05 +0000

    f3: update 5.0 bottle.

--- Library/Formula/f3.rb
+++ Library/Formula/f3.rb
@@ -7,9 +7,9 @@ class F3 < Formula
 
   bottle do
     cellar :any
-    sha1 "939ab204d6e1ff0be2c487fd6cce4ba5bcad66f7" => :yosemite
-    sha1 "6ae3660786be6daa0641bbaaae7b7e66fc5f5886" => :mavericks
-    sha1 "f83fb9c34469ea937f51874a70f3ec94d48980db" => :mountain_lion
+    sha1 "60c3eabb3069d9aca853a8e9a482b7922499dc56" => :yosemite
+    sha1 "e79ac68956f2b72bd76dbb8a359a75da12ed85fe" => :mavericks
+    sha1 "2407d28f6497df41aa5784e1cd0153d3fc8214c7" => :mountain_lion
   end
 
   def install
