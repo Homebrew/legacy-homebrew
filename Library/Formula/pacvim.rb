2a9e8491961094451146182be9ee2bb8546f49bb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-27 10:08:20 +0000

    pacvim: add 1.1.1 bottle.

--- Library/Formula/pacvim.rb
+++ Library/Formula/pacvim.rb
@@ -4,6 +4,12 @@ class Pacvim < Formula
   sha1 "496ed02edba8dad15ade95352a7c6441f97fdf7a"
   head "https://github.com/jmoon018/PacVim.git"
 
+  bottle do
+    sha1 "72ab787738913d09abc2221f37a8ec9c6da31d5a" => :yosemite
+    sha1 "8fab0bdb05fbd24bb0282e68d805fe2e0eaf0908" => :mavericks
+    sha1 "c38b89d4de35684122e4ab14ca605fdf4977a7b5" => :mountain_lion
+  end
+
   needs :cxx11
 
   def install
