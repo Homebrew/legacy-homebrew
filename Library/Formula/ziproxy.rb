8cb80f0741791eef9d3ad14b12a842d2cb529b8b
Maxime Mouchet <max@maxmouchet.com>
2015-01-28 08:31:46 -0800

    Add dependencies for Ziproxy

--- Library/Formula/ziproxy.rb
+++ Library/Formula/ziproxy.rb
@@ -3,6 +3,11 @@ class Ziproxy < Formula
   url "https://downloads.sourceforge.net/project/ziproxy/ziproxy/ziproxy-3.3.1/ziproxy-3.3.1.tar.gz"
   sha1 "7c8db842754d1e3e90b42c3c37f9b15e932a6545"
 
+  depends_on "jasper"
+  depends_on "giflib"
+  depends_on "libpng"
+  depends_on "jpeg"
+
   def install
     ENV["LDFLAGS"] = "-lresolv"
 
