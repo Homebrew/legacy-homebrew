bebdc9b9ce2c957bd0c03208839c5a771cc9fd90
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-29 00:12:30 +0100

    rcs 5.9.4

--- Library/Formula/rcs.rb
+++ Library/Formula/rcs.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Rcs < Formula
   homepage "https://www.gnu.org/software/rcs/"
-  url "http://ftpmirror.gnu.org/rcs/rcs-5.9.3.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/rcs/rcs-5.9.3.tar.xz"
-  sha1 "e4a9549678618f5d69968b10c6c9b92b29519813"
+  url "http://ftpmirror.gnu.org/rcs/rcs-5.9.4.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/rcs/rcs-5.9.4.tar.xz"
+  sha1 "e87fb2e587fa165204ef69f69b7d5cd354c4e44d"
 
   bottle do
     cellar :any
