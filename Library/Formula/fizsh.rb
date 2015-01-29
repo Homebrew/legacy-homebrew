bcceef70a821969ca2141fed45846a1e32abf3da
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-29 16:47:00 +0000

    fizsh: add 1.0.8 bottle.

--- Library/Formula/fizsh.rb
+++ Library/Formula/fizsh.rb
@@ -8,6 +8,13 @@ class Fizsh < Formula
 
   head "https://github.com/zsh-users/fizsh", :using => :git
 
+  bottle do
+    cellar :any
+    sha1 "31962775bf8fde4d57fa03bb2a9c1c7ae2449afb" => :yosemite
+    sha1 "e95ca1c85aae0e07d8fe99f631a3eb00ae6bc708" => :mavericks
+    sha1 "b1ce95c0da21159a36c8153064fcb82ed708c8f7" => :mountain_lion
+  end
+
   depends_on "zsh"
 
   def install
