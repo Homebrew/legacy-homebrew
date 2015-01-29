6f68856f481c2db9d636ccb02b48d57414f143f7
yanghua <yanghua1127@gmail.com>
2015-01-29 11:28:20 +0800

    update the sha1 code

--- Library/Formula/mysqlreport.rb
+++ Library/Formula/mysqlreport.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Mysqlreport < Formula
   homepage 'http://hackmysql.com/mysqlreport'
   url 'http://hackmysql.com/scripts/mysqlreport-3.5.tgz'
-  sha1 '00d2790a9b76422a936f96a622567aa03437b9a3'
+  sha1 '1d54a4a95f6537eea8a4631a6954754e53f19a0e'
 
   def install
     bin.install "mysqlreport"
