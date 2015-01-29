456d1630027f8120439247792a458b29bd0acc20
Bret Comnes <bcomnes@gmail.com>
2015-01-29 15:01:18 -0800

    Graduate iojs out of keg-only
    This PR
    - takes iojs out of keg-only
    - conflicts node and iojs
    - does not patch npm
    - Added NPM install code to language/javascript.rb
    - Added npm caveats to iojs and node formula based
      off of python caveats.
    - incorporated 1580f8b change
    
    Signed-off-by: Bret Comnes <bcomnes@gmail.com>
    
    Update npm caveats

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,5 +1,9 @@
+require "language/javascript"
+
 # Note that x.even are stable releases, x.odd are devel releases
 class Node < Formula
+  include Language::JS
+
   homepage "https://nodejs.org/"
   url "https://nodejs.org/dist/v0.12.2/node-v0.12.2.tar.gz"
   sha256 "ac7e78ade93e633e7ed628532bb8e650caba0c9c33af33581957f3382e2a772d"
@@ -11,6 +15,8 @@ class Node < Formula
     sha256 "a995d265c9db0b021de140cdcc78432b96bcbf3fdb6989cc401d9cd7c2164cdd" => :mavericks
     sha256 "cde73f7ca5b2c080bb8e7d5b99fa02ac43908ec8ad24ef52d9a3a44141ffbe75" => :mountain_lion
   end
+
+  conflicts_with "iojs", :because => "node and iojs both install a binary/link named node"
 
   option "with-debug", "Build with debugger hooks"
   option "without-npm", "npm will not be installed"
@@ -50,23 +56,11 @@ class Node < Formula
     system "make", "install"
 
     if build.with? "npm"
-      resource("npm").stage buildpath/"npm_install"
-
-      # make sure npm can find node
-      ENV.prepend_path "PATH", bin
-      # make sure user prefix settings in $HOME are ignored
-      ENV["HOME"] = buildpath/"home"
-      # set log level temporarily for npm's `make install`
-      ENV["NPM_CONFIG_LOGLEVEL"] = "verbose"
-
-      cd buildpath/"npm_install" do
-        system "./configure", "--prefix=#{libexec}/npm"
-        system "make", "install"
-      end
+      resource("npm").stage npm_buildpath = buildpath/"npm_install"
+      install_npm npm_buildpath
 
       if build.with? "completion"
-        bash_completion.install \
-          buildpath/"npm_install/lib/utils/completion.sh" => "npm"
+        install_npm_bash_completion npm_buildpath
       end
     end
   end
@@ -74,44 +68,13 @@ class Node < Formula
   def post_install
     return if build.without? "npm"
 
-    node_modules = HOMEBREW_PREFIX/"lib/node_modules"
-    node_modules.mkpath
-    npm_exec = node_modules/"npm/bin/npm-cli.js"
-    # Kill npm but preserve all other modules across node updates/upgrades.
-    rm_rf node_modules/"npm"
-
-    cp_r libexec/"npm/lib/node_modules/npm", node_modules
-    # This symlink doesn't hop into homebrew_prefix/bin automatically so
-    # remove it and make our own. This is a small consequence of our bottle
-    # npm make install workaround. All other installs **do** symlink to
-    # homebrew_prefix/bin correctly. We ln rather than cp this because doing
-    # so mimics npm's normal install.
-    ln_sf npm_exec, "#{HOMEBREW_PREFIX}/bin/npm"
-
-    # Let's do the manpage dance. It's just a jump to the left.
-    # And then a step to the right, with your hand on rm_f.
-    ["man1", "man3", "man5", "man7"].each do |man|
-      # Dirs must exist first: https://github.com/Homebrew/homebrew/issues/35969
-      mkdir_p HOMEBREW_PREFIX/"share/man/#{man}"
-      rm_f Dir[HOMEBREW_PREFIX/"share/man/#{man}/{npm.,npm-,npmrc.}*"]
-      ln_sf Dir[libexec/"npm/share/man/#{man}/npm*"], HOMEBREW_PREFIX/"share/man/#{man}"
-    end
-
-    npm_root = node_modules/"npm"
-    npmrc = npm_root/"npmrc"
-    npmrc.atomic_write("prefix = #{HOMEBREW_PREFIX}\n")
+    npm_post_install libexec
   end
 
   def caveats
     s = ""
 
-    if build.without? "npm"
-      s += <<-EOS.undent
-        Homebrew has NOT installed npm. If you later install it, you should supplement
-        your NODE_PATH with the npm module folder:
-          #{HOMEBREW_PREFIX}/lib/node_modules
-      EOS
-    end
+    s += npm_caveats
 
     if build.with? "icu4c"
       s += <<-EOS.undent
@@ -140,9 +103,7 @@ class Node < Formula
       # make sure npm can find node
       ENV.prepend_path "PATH", opt_bin
       assert_equal which("node"), opt_bin/"node"
-      assert (HOMEBREW_PREFIX/"bin/npm").exist?, "npm must exist"
-      assert (HOMEBREW_PREFIX/"bin/npm").executable?, "npm must be executable"
-      system "#{HOMEBREW_PREFIX}/bin/npm", "--verbose", "install", "npm@latest"
+      npm_test_install
     end
   end
 end
