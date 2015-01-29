94ff8f7b15327e15089fd6b985ba77762578228a
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-29 13:55:16 +0100

    cbmbasic: head added

--- Library/Formula/cbmbasic.rb
+++ Library/Formula/cbmbasic.rb
@@ -1,16 +1,15 @@
-require 'formula'
-
 class Cbmbasic < Formula
-  homepage 'http://cbmbasic.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/cbmbasic/cbmbasic/1.0/cbmbasic-1.0.tgz'
-  sha1 '54564daa7f28be98b03ae7dd1eece9e5439c95c3'
+  homepage "https://github.com/mist64/cbmbasic"
+  url "https://downloads.sourceforge.net/project/cbmbasic/cbmbasic/1.0/cbmbasic-1.0.tgz"
+  sha1 "54564daa7f28be98b03ae7dd1eece9e5439c95c3"
+  head "https://github.com/mist64/cbmbasic.git"
 
   def install
     system "make", "CFLAGS=#{ENV.cflags}", "LDFLAGS=#{ENV.ldflags}"
-    bin.install 'cbmbasic'
+    bin.install "cbmbasic"
   end
 
   test do
-    assert_match /READY.\r\n 1/, pipe_output("#{bin}/cbmbasic", "PRINT 1\n")
+    assert_match(/READY.\r\n 1/, pipe_output("#{bin}/cbmbasic", "PRINT 1\n", 0))
   end
 end
