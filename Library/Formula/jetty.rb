8aa592a8650d75ea80e96494c1f6352dedf5c140
Pedro Pinheiro <pdpinheiro@gmail.com>
2015-01-29 10:54:53 +0000

    Update Jetty URL

--- Library/Formula/jetty.rb
+++ Library/Formula/jetty.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Jetty < Formula
   homepage "http://www.eclipse.org/jetty/"
-  url "http://download.eclipse.org/jetty/9.2.5.v20141112/dist/jetty-distribution-9.2.5.v20141112.tar.gz"
+  url "http://eclipse.org/downloads/download.php?file=/jetty/9.2.5.v20141112/dist/jetty-distribution-9.2.5.v20141112.tar.gz"
   version "9.2.5.v20141112"
   sha1 "30a7a34a7ac423fb15885a63f03bffc3669e4d9a"
 
