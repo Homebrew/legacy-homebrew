0b9e5f00caf592d6e501cc92826cb0c6b89f7e1e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-29 13:04:06 +0000

    cbmbasic: add 1.0 bottle.

--- Library/Formula/cbmbasic.rb
+++ Library/Formula/cbmbasic.rb
@@ -4,6 +4,13 @@ class Cbmbasic < Formula
   sha1 "54564daa7f28be98b03ae7dd1eece9e5439c95c3"
   head "https://github.com/mist64/cbmbasic.git"
 
+  bottle do
+    cellar :any
+    sha1 "83830760860b56bb7c10dd301c72aae60e8172c1" => :yosemite
+    sha1 "3e6da115337e59d612236113185fd29d49cd5763" => :mavericks
+    sha1 "7da6d8d160c73fd00e28999ae6c6f4c89b1cacbb" => :mountain_lion
+  end
+
   def install
     system "make", "CFLAGS=#{ENV.cflags}", "LDFLAGS=#{ENV.ldflags}"
     bin.install "cbmbasic"
