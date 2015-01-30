246a6476b80f957252385bc3d9c5721a8605fd04
Robson Peixoto <robsonpeixoto@gmail.com>
2015-01-30 15:30:16 -0300

    checkstyle: fix test and install

--- Library/Formula/checkstyle.rb
+++ Library/Formula/checkstyle.rb
@@ -6,7 +6,7 @@ class Checkstyle < Formula
   sha1 "5b92798c55cbc8cf87f1435a2fc64e04d50a6648"
 
   def install
-    libexec.install "checkstyle-#{version}-all.jar", "sun_checks.xml"
+    libexec.install "checkstyle-#{version}-all.jar"
     bin.write_jar_script libexec/"checkstyle-#{version}-all.jar", "checkstyle"
   end
 
@@ -14,7 +14,7 @@ class Checkstyle < Formula
     path = testpath/"foo.java"
     path.write "public class Foo{ }\n"
 
-    output = `#{bin}/checkstyle -c #{libexec}/sun_checks.xml -r #{path}`
+    output = `#{bin}/checkstyle -c /sun_checks.xml -r #{path}`
     errors = output.split("\n").select { |line| line.start_with?(path) }
     assert errors.include?("#{path}:1:17: '{' is not preceded with whitespace.")
     assert_equal errors.size, $?.exitstatus
