d7b1bd75ff0a9f2a7d6644c8d9333214703398d3
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-30 10:27:42 +0100

    galen: use new Java requirement style

--- Library/Formula/galen.rb
+++ Library/Formula/galen.rb
@@ -3,7 +3,7 @@ class Galen < Formula
   url "https://github.com/galenframework/galen/releases/download/galen-1.5.1/galen-bin-1.5.1.zip"
   sha1 "ea3e261e0409c8797a3c627536444bfd4c5ff311"
 
-  depends_on :java => "1.6"
+  depends_on :java => "1.6+"
 
   def install
     libexec.install "galen.jar"
