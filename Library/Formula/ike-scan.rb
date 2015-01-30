2792f6fe4f4ce233f304ba98e101efbeb1aeda85
Dominyk Tiller <dominyktiller@gmail.com>
2015-01-30 16:41:51 +0000

    ike-scan: system openssl fix
    Another system OpenSSL Fix, and added the HEAD, and strict audit fixes.
    
    Closes #36384.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/ike-scan.rb
+++ Library/Formula/ike-scan.rb
@@ -1,15 +1,30 @@
-require 'formula'
-
 class IkeScan < Formula
-  homepage 'http://www.nta-monitor.com/tools/ike-scan/'
-  url 'http://www.nta-monitor.com/tools/ike-scan/download/ike-scan-1.9.tar.gz'
-  sha1 'e973742c32c7b65fe134233482c94e3e94db3b32'
+  homepage "http://www.nta-monitor.com/tools/ike-scan/"
+  url "http://www.nta-monitor.com/tools/ike-scan/download/ike-scan-1.9.tar.gz"
+  sha1 "e973742c32c7b65fe134233482c94e3e94db3b32"
+  revision 1
+
+  head do
+    url "https://github.com/royhills/ike-scan.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "openssl"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "autoreconf", "-fvi" if build.head?
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
-                          "--with-openssl"
-    system "make install"
+                          "--with-openssl=#{Formula["openssl"].opt_prefix}"
+    system "make", "install"
+  end
+
+  test do
+    # We probably shouldn't probe any host for VPN servers, so let's keep this simple.
+    system bin/"ike-scan", "--version"
   end
 end
