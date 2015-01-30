0b16961bd31be7271abb540f580263ee85b796b1
Mike McQuaid <mike@mikemcquaid.com>
2015-01-30 10:11:42 +0000

    cmake: remove unused embedded patch .

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -117,19 +117,3 @@ class Cmake < Formula
     system "#{bin}/cmake", "."
   end
 end
-
-__END__
-
-diff --git a/Modules/FindOpenSSL.cmake b/Modules/FindOpenSSL.cmake
-index 340b417..6b4f985 100644
---- a/Modules/FindOpenSSL.cmake
-+++ b/Modules/FindOpenSSL.cmake
-@@ -279,7 +279,7 @@ if (OPENSSL_INCLUDE_DIR)
-     set(OPENSSL_VERSION "${_OPENSSL_VERSION}")
-   elseif(OPENSSL_INCLUDE_DIR AND EXISTS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h")
-     file(STRINGS "${OPENSSL_INCLUDE_DIR}/openssl/opensslv.h" openssl_version_str
--         REGEX "^#define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x([0-9a-fA-F])+.*")
-+         REGEX "^# *define[\t ]+OPENSSL_VERSION_NUMBER[\t ]+0x([0-9a-fA-F])+.*")
-
-     # The version number is encoded as 0xMNNFFPPS: major minor fix patch status
-     # The status gives if this is a developer or prerelease and is ignored here.
