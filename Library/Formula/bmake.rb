bac944b0c9758c39396c7b1e29a112bba325ea6c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-31 08:25:52 +0000

    bmake: add 20141111 bottle.

--- Library/Formula/bmake.rb
+++ Library/Formula/bmake.rb
@@ -5,6 +5,12 @@ class Bmake < Formula
   url "http://www.crufty.net/ftp/pub/sjg/bmake-20141111.tar.gz"
   sha1 "a6f670210f9ccde09a0e93a29a766f4726ddfeed"
 
+  bottle do
+    sha1 "bfb80b95386a6e9c439167573d58a9623650708e" => :yosemite
+    sha1 "9ae644344202f0e42eaac253e38b95d16c23433d" => :mavericks
+    sha1 "e91b8e0f374eeaf85ef53bf4d92df3cf1d646a97" => :mountain_lion
+  end
+
   def install
     # The first, an oversight upstream; the second, don't pre-roff cat pages.
     inreplace "bmake.1", ".Dt MAKE", ".Dt BMAKE"
