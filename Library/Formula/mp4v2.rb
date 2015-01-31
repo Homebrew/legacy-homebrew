a2238e5ef894682202ab41dcaef418d18e4ebe1c
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-01-31 11:40:34 +0100

    mp4v2 - use HTTPS for Google Code homepage

--- Library/Formula/mp4v2.rb
+++ Library/Formula/mp4v2.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Mp4v2 < Formula
-  homepage "http://code.google.com/p/mp4v2/"
+  homepage "https://code.google.com/p/mp4v2/"
   url "https://mp4v2.googlecode.com/files/mp4v2-2.0.0.tar.bz2"
   sha1 "193260cfb7201e6ec250137bcca1468d4d20e2f0"
 
