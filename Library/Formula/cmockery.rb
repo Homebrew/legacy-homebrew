314a4a14027e614460f7f525ea8d68e03218481c
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-01-31 11:20:25 +0100

    cmockery - use HTTPS for Google Code homepage

--- Library/Formula/cmockery.rb
+++ Library/Formula/cmockery.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Cmockery < Formula
-  homepage 'http://code.google.com/p/cmockery/'
+  homepage 'https://code.google.com/p/cmockery/'
   url 'https://cmockery.googlecode.com/files/cmockery-0.1.2.tar.gz'
   sha1 '964ed1104a0cbbea8a9a34e88c6e79b546eff1bc'
 
