c6f2abf3f78e84ab0ee5fee02c73c8e89d9e8fa7
Jack Nagel <jacknagel@gmail.com>
2015-02-02 00:39:04 -0500

    Remove message that is never displayed
    Requirements with default formulae cannot fail the build.

--- Library/Homebrew/requirements/apr_dependency.rb
+++ Library/Homebrew/requirements/apr_dependency.rb
@@ -14,27 +14,4 @@ class AprDependency < Requirement
       ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr-util"].opt_libexec}/lib/pkgconfig"
     end
   end
-
-  def message
-    message = <<-EOS.undent
-      Due to packaging problems on Apple's part, software that compiles
-      against APR requires the standalone Command Line Tools.
-    EOS
-    if MacOS.version >= :mavericks
-      message += <<-EOS.undent
-        Either
-        `brew install apr-util`
-        or
-        `xcode-select --install`
-        to install APR.
-      EOS
-    else
-      message += <<-EOS.undent
-        The standalone package can be obtained from
-        https://developer.apple.com/downloads/,
-        or it can be installed via Xcode's preferences.
-        Or you can `brew install apr-util`.
-      EOS
-    end
-  end
 end
