6e007a092350accbcb61a922d7dd9f704e7109c8
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-02 22:25:57 +0100

    globus-toolkit 6.0
    Closes #36415.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/globus-toolkit.rb
+++ Library/Formula/globus-toolkit.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class GlobusToolkit < Formula
-  homepage 'http://www.globus.org/toolkit/'
-  url 'http://toolkit.globus.org/ftppub/gt5/5.2/5.2.5/installers/src/gt5.2.5-all-source-installer.tar.gz'
-  version '5.2.5'
-  sha1 '2e39065e0c3970b660e081705915d45640d3c350'
+  homepage "http://www.globus.org/toolkit/"
+  # Note: Stable distributions have an even minor version number (e.g. 5.0.3)
+  url "http://toolkit.globus.org/ftppub/gt6/installers/src/globus_toolkit-6.0.tar.gz"
+  sha1 "cb688858f96b26a92d72efedc1153a8cbf2516b8"
 
   bottle do
     sha1 "b0a24a8d66dd36aa0ac48f0ae5dddfd02af22d6c" => :yosemite
@@ -12,14 +10,27 @@ class GlobusToolkit < Formula
     sha1 "1cbbe7a3d08711cbc5665e91558417d7b2028d3a" => :mountain_lion
   end
 
+  depends_on "openssl"
   depends_on "libtool" => :run
+  depends_on "pkg-config" => :build
+
+  option "with-check", "Test the toolkit when installing"
 
   def install
     ENV.deparallelize
     ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
-    system "./configure", "--prefix=#{prefix}"
+    man.mkpath
+    system "./configure", "--prefix=#{libexec}",
+                          "--mandir=#{man}",
+                          "--disable-dependency-tracking"
     system "make"
-    system "make install"
-    share.install prefix/'man'
+    system "make", "check" if build.with? "check"
+    system "make", "install"
+    bins = Dir["#{libexec}/bin/*"].select { |f| File.executable? f }
+    bin.write_exec_script bins
+  end
+
+  test do
+    system "#{bin}/globusrun", "-help"
   end
 end
