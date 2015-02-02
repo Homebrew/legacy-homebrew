cb552f0f601f2ff3d24937d48e6053c08738231f
Jack Nagel <jacknagel@gmail.com>
2015-02-02 00:39:04 -0500

    AprRequirement's default formula should be apr-util
    This matches the recommendation in the message.

--- Library/Homebrew/requirements/apr_dependency.rb
+++ Library/Homebrew/requirements/apr_dependency.rb
@@ -2,15 +2,16 @@ require "requirement"
 
 class AprDependency < Requirement
   fatal true
-  default_formula "apr"
+  default_formula "apr-util"
 
   satisfy { MacOS::CLT.installed? || Formula["apr"].installed? }
 
   env do
     unless MacOS::CLT.installed?
-      ENV.prepend_path "PATH", "#{Formula["apr-util"].opt_prefix}/bin"
-      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr"].opt_prefix}/libexec/lib/pkgconfig"
-      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr-util"].opt_prefix}/libexec/lib/pkgconfig"
+      ENV.prepend_path "PATH", Formula["apr-util"].opt_bin
+      ENV.prepend_path "PATH", Formula["apr"].opt_bin
+      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr"].opt_libexec}/lib/pkgconfig"
+      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr-util"].opt_libexec}/lib/pkgconfig"
     end
   end
 
