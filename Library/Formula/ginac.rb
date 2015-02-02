fec5cf917b5b7558dfe5daec061c3f60cfc4a3ac
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-02 21:22:25 +0000

    ginac: add 1.6.3 bottle.

--- Library/Formula/ginac.rb
+++ Library/Formula/ginac.rb
@@ -5,6 +5,13 @@ class Ginac < Formula
   url 'http://www.ginac.de/ginac-1.6.3.tar.bz2'
   sha1 '39ebd0035491da84ca3406688c15930faebe5ef1'
 
+  bottle do
+    cellar :any
+    sha1 "04f032b8d9f13b33457cf78569b71a55572fb2b8" => :yosemite
+    sha1 "ecc3f94892bccb66c9f240aecc9d6d70e8fbf500" => :mavericks
+    sha1 "062569680b5a725eb0316574ba2d6c22760a02e2" => :mountain_lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'cln'
   depends_on 'readline'
