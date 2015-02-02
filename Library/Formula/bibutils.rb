6833ac152a2a7045f21acd5011b3cb81935ae95f
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-02 22:50:11 +0100

    bibutils 5.6

--- Library/Formula/bibutils.rb
+++ Library/Formula/bibutils.rb
@@ -1,7 +1,7 @@
 class Bibutils < Formula
   homepage "http://sourceforge.net/p/bibutils/home/Bibutils/"
-  url "https://downloads.sourceforge.net/project/bibutils/bibutils_5.5_src.tgz"
-  sha1 "f7cb7a8bd62ac3b5f0caf63a4a6a793355a417f2"
+  url "https://downloads.sourceforge.net/project/bibutils/bibutils_5.6_src.tgz"
+  sha1 "effec35d97ed2d8454721eaf37ba8b484f02e8da"
 
   bottle do
     cellar :any
@@ -11,14 +11,9 @@ class Bibutils < Formula
   end
 
   def install
-    system "./configure", "--install-dir", prefix
-    system "make", "CC=#{ENV.cc}"
-
-    cd "bin" do
-      bin.install %w[bib2xml ris2xml end2xml endx2xml med2xml isi2xml copac2xml
-                     biblatex2xml ebi2xml wordbib2xml xml2ads xml2bib xml2end
-                     xml2isi xml2ris xml2wordbib modsclean]
-    end
+    system "./configure", "--install-dir", bin,
+                          "--install-lib", lib
+    system "make", "install", "CC=#{ENV.cc}"
   end
 
   test do
