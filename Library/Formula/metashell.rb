cfc4ff588aba9fb34d79d68415d82f9c435ddd4f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-02 13:16:58 +0000

    metashell: add 2.0.0 bottle.

--- Library/Formula/metashell.rb
+++ Library/Formula/metashell.rb
@@ -3,6 +3,12 @@ class Metashell < Formula
   url "https://github.com/sabel83/metashell/archive/v2.0.0.tar.gz"
   sha1 "4dec47b6ee32cdf179b2eb297c289b296d3fba8f"
 
+  bottle do
+    sha1 "35cb28330f73ec44ecf27244347805dc2561439f" => :yosemite
+    sha1 "0d8a62d552ae6ff960ac26fe8344181ed62895d5" => :mavericks
+    sha1 "76874d93ced1815ccfcbddb6da75e8c56aff44e4" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   needs :cxx11
