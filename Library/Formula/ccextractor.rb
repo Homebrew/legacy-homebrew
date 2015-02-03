4aad4f7cdb795d7987649fb5aea71647df393ce3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-03 07:55:01 +0000

    ccextractor: add 0.75 bottle.

--- Library/Formula/ccextractor.rb
+++ Library/Formula/ccextractor.rb
@@ -4,6 +4,13 @@ class Ccextractor < Formula
   sha1 "c36f8eadb2074d88782d6628e07c762e80e4c31c"
   head "https://github.com/ccextractor/ccextractor.git"
 
+  bottle do
+    cellar :any
+    sha1 "12fc81b745160eaf4d664907defe87230ffa022e" => :yosemite
+    sha1 "4e4775826383cc891cb63f9f33f5d5a0f74ff5ac" => :mavericks
+    sha1 "9eefc9ce1714e675a9ef01841e79b6b72170173d" => :mountain_lion
+  end
+
   def install
     cd "mac" do
       system "bash", "./build.command"
