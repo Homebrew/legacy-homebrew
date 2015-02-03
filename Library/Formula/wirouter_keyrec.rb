5c7033e1a81c64a257285c717b558da5e63f817d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-03 16:00:45 +0000

    wirouter_keyrec: add 1.1.2 bottle.

--- Library/Formula/wirouter_keyrec.rb
+++ Library/Formula/wirouter_keyrec.rb
@@ -3,6 +3,12 @@ class WirouterKeyrec < Formula
   url "http://tools.salvatorefresta.net/WiRouter_KeyRec_1.1.2.zip"
   sha1 "3c17f2d0bf3d6201409862fd903ebfd60c1e8a2e"
 
+  bottle do
+    sha1 "70d0cc222e0a6215d9ba9868c281603eac63c8a7" => :yosemite
+    sha1 "ef0a2fcab2ebcefe87b91946036cd8d22331f54d" => :mavericks
+    sha1 "cdbdb678f080ada0d5432f8f2b1c6b0b2f1c8254" => :mountain_lion
+  end
+
   def install
     inreplace "src/agpf.h", %r{/etc}, "#{prefix}/etc"
     inreplace "src/teletu.h", %r{/etc}, "#{prefix}/etc"
