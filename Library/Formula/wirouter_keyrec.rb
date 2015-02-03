dcf97c3093a50754e5a314f34cde3c5dce6d27af
Giordano Vicari <giordanovi86@gmail.com>
2015-02-03 16:45:00 +0100

    Using %r for regular expressions matching

--- Library/Formula/wirouter_keyrec.rb
+++ Library/Formula/wirouter_keyrec.rb
@@ -4,8 +4,8 @@ class WirouterKeyrec < Formula
   sha1 "3c17f2d0bf3d6201409862fd903ebfd60c1e8a2e"
 
   def install
-    inreplace "src/agpf.h", /\/etc/, "#{prefix}/etc"
-    inreplace "src/teletu.h", /\/etc/, "#{prefix}/etc"
+    inreplace "src/agpf.h", %r{/etc}, "#{prefix}/etc"
+    inreplace "src/teletu.h", %r{/etc}, "#{prefix}/etc"
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
