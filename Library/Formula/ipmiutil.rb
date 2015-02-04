850a00d099a345e4a684302a2b5147737d66bf9e
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-02-04 17:04:39 +0100

    Update ipmiutil, drop patch

--- Library/Formula/ipmiutil.rb
+++ Library/Formula/ipmiutil.rb
@@ -2,11 +2,8 @@ require 'formula'
 
 class Ipmiutil < Formula
   homepage 'http://ipmiutil.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/ipmiutil/ipmiutil-2.9.1.tar.gz'
-  sha1 'f23fabe8339842fea9b8c2a601717dc002e44a9d'
-
-  # Make ipmiutil treat Darwin as BSD
-  patch :DATA
+  url 'https://downloads.sourceforge.net/project/ipmiutil/ipmiutil-2.9.5.tar.gz'
+  sha1 '265f022c876da373b2ecb4be2bc0f98e65f70977'
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
@@ -28,17 +25,3 @@ class Ipmiutil < Formula
     system "#{bin}/ipmiutil", "delloem", "help"
   end
 end
-
-__END__
-diff -u ./configure.bak ./configure
---- ./configure.bak	2012-09-18 23:19:11.000000000 +0800
-+++ ./configure	2012-09-18 23:21:04.000000000 +0800
-@@ -20983,7 +20983,7 @@
-	OS_CFLAGS="-DLINUX $MD2_CFLAGS $cfwarn"
-   else
-      # usually "x$sysname" = "xFreeBSD", but allow NetBSD
--     echo $sysname | grep BSD >/dev/null 2>&1
-+     echo $sysname | grep 'BSD\|Darwin' >/dev/null 2>&1
-      if test $? -eq 0; then
-	os=bsd
-	OS_CFLAGS="-DBSD"
