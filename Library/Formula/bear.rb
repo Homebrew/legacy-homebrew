84a39a9d1fbc1bca35c4c65ff37a8de8c75957cb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-04 16:54:21 +0000

    bear: add 2.0.1 bottle.

--- Library/Formula/bear.rb
+++ Library/Formula/bear.rb
@@ -4,6 +4,12 @@ class Bear < Formula
   sha1 "31cf2b82a44f6eb5a3740c9f8aa9f2cd662e9a68"
   head "https://github.com/rizsotto/Bear.git"
 
+  bottle do
+    sha1 "6d868313f7e4afa2ac5e5a8979f47299e776a439" => :yosemite
+    sha1 "1aa1d89fe02160069fb32c666e982c4b0fdd5841" => :mavericks
+    sha1 "a576d3a8ab01c8bbdb1c387d314736d6d3c7cae7" => :mountain_lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
