e7d5829eea1c1ef626786868c2974abf60627857
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-02-04 22:19:02 +0100

    Fix configure bug and link failure
    configure on OSX is broken: it assumes that MD2 is not available without
    testing (too conservative, except when using Homebrew's OpenSSL), but
    then does not put the flag in any variable that is actually used.
    
    In fact, if you --enable-gpl, they even have a reimplementation of MD2.
    We should expose that flag.

--- Library/Formula/ipmiutil.rb
+++ Library/Formula/ipmiutil.rb
@@ -5,6 +5,7 @@ class Ipmiutil < Formula
   url 'https://downloads.sourceforge.net/project/ipmiutil/ipmiutil-2.9.5.tar.gz'
   sha1 '265f022c876da373b2ecb4be2bc0f98e65f70977'
 
+  patch :DATA
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -25,3 +26,26 @@ class Ipmiutil < Formula
     system "#{bin}/ipmiutil", "delloem", "help"
   end
 end
+
+__END__
+diff -u ./configure.bak ./configure
+--- ./configure.bak       2015-02-04 22:15:07.000000000 +0100
++++ ./configure   2015-02-04 22:16:18.000000000 +0100
+@@ -20739,7 +20739,7 @@
+            echo "Detected HP-UX"
+            os=hpux
+            MD2_CFLAGS="-DSKIP_MD2"
+-           OS_CFLAGS="-DHPUX"
++           OS_CFLAGS="-DHPUX $MD2_CFLAGS"
+            OS_LFLAGS=""
+            OS_DRIVERS="ipmimv.c"
+            drivers="open"
+@@ -20748,7 +20748,7 @@
+            echo "Detected MacOSX"
+            os=macos
+            MD2_CFLAGS="-DSKIP_MD2"
+-           OS_CFLAGS="-DMACOS"
++           OS_CFLAGS="-DMACOS $MD2_CFLAGS"
+            OS_LFLAGS=""
+           OS_DRIVERS="ipmimv.c ipmidir.c"
+           drivers="open direct"
