bbf653f21ac3760e02169e9db9722df8be71419d
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-04 18:09:55 +0000

    blazeblogger: add test
    Adds a test to the blazeblogger formula, fixes strict audit failures.
    
    Closes #36532.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/blazeblogger.rb
+++ Library/Formula/blazeblogger.rb
@@ -1,13 +1,19 @@
-require 'formula'
-
 class Blazeblogger < Formula
-  homepage 'http://blaze.blackened.cz/'
-  url 'https://blazeblogger.googlecode.com/files/blazeblogger-1.2.0.tar.gz'
-  sha1 '280894fca6594d0c0df925aa0a16b9116ee19f17'
+  homepage "http://blaze.blackened.cz/"
+  url "https://blazeblogger.googlecode.com/files/blazeblogger-1.2.0.tar.gz"
+  sha1 "280894fca6594d0c0df925aa0a16b9116ee19f17"
 
   def install
-    system "make", "prefix=#{prefix}",
-                   "compdir=#{prefix}",
-                   "install"
+    # https://code.google.com/p/blazeblogger/issues/detail?id=51
+    ENV.deparallelize
+    system "make", "prefix=#{prefix}", "compdir=#{prefix}", "install"
+  end
+
+  test do
+    system bin/"blaze", "init"
+    system bin/"blaze", "config", "blog.title", "Homebrew!"
+    system bin/"blaze", "make"
+    assert File.exist? "default.css"
+    assert File.read(".blaze/config").include?("Homebrew!")
   end
 end
