a3de2c6e99fb4e8ef1fb29baf36220cbba8a11d3
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-04 03:18:16 +0000

    bear 2.0.1
    The existing test seems to be erroring out now, so I’ve replaced it
    with what existed previously. Otherwise, just a bump.
    
    Closes #36520.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/bear.rb
+++ Library/Formula/bear.rb
@@ -1,10 +1,10 @@
-require "formula"
-
 class Bear < Formula
   homepage "https://github.com/rizsotto/Bear"
-  url "https://github.com/rizsotto/Bear/archive/1.4.3.tar.gz"
-  sha1 "10a212ba608b9fae4101fb5cf8c05c4279524209"
+  url "https://github.com/rizsotto/Bear/archive/2.0.1.tar.gz"
+  sha1 "31cf2b82a44f6eb5a3740c9f8aa9f2cd662e9a68"
+  head "https://github.com/rizsotto/Bear.git"
 
+  depends_on :python if MacOS.version <= :snow_leopard
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "libconfig"
@@ -17,6 +17,7 @@ class Bear < Formula
   end
 
   test do
-    system "#{bin}/bear", "--", "true"
+    system "#{bin}/bear", "true"
+    assert File.exist? "compile_commands.json"
   end
 end
