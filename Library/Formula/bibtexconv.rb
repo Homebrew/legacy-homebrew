5b1b1f368c07ac107676cbe6db9325a30b3eb14b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-04 04:59:13 +0000

    bibtexconv: add 1.1.0 bottle.

--- Library/Formula/bibtexconv.rb
+++ Library/Formula/bibtexconv.rb
@@ -3,6 +3,13 @@ class Bibtexconv < Formula
   url "https://www.uni-due.de/~be0001/bibtexconv/download/bibtexconv-1.1.0.tar.gz"
   sha1 "c9ea5d067de069ddbe7b6500043ff87cae2fb8ba"
 
+  bottle do
+    cellar :any
+    sha1 "181370ff5fd768678de4e015e071844281343094" => :yosemite
+    sha1 "ce905adc7117c2494069c58f617c3d06a787e919" => :mavericks
+    sha1 "570f919ab0a297ef720c5a6ffa326350e55b8195" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
