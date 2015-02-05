d69c915319b3f5de4f0553f3db69f09796b2fa64
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-05 21:58:48 +0000

    byacc 20141128
    Version bump.
    
    Simple test added, Apologies to Mike in advance - I don’t know how to
    handle a more difficult one here ;)
    
    Closes #36577.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/byacc.rb
+++ Library/Formula/byacc.rb
@@ -1,15 +1,15 @@
-require "formula"
-
 class Byacc < Formula
   homepage "http://invisible-island.net/byacc/byacc.html"
-  url "ftp://invisible-island.net/byacc/byacc-20140422.tgz"
-  sha1 "aa30cac1e7b3cf9d6d19d6b9653575d56564b213"
+  url "ftp://invisible-island.net/byacc/byacc-20141128.tgz"
+  sha1 "59ea0a166b10eaec99edacc4c38fcb006c6e84d3"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--program-prefix=b",
-                          "--prefix=#{prefix}",
-                          "--man=#{man}"
-    system "make install"
+                          "--program-prefix=b", "--prefix=#{prefix}", "--man=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"byacc", "-V"
   end
 end
