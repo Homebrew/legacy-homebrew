ac9eb952541e99ece45e7a2ca215fce2d6d8b27b
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-05 21:34:57 +0000

    btpd: fix for system openssl linkage
    ^
    
    Closes #36574.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/btpd.rb
+++ Library/Formula/btpd.rb
@@ -1,13 +1,18 @@
-require 'formula'
-
 class Btpd < Formula
-  homepage 'https://github.com/btpd/btpd'
-  url 'https://github.com/downloads/btpd/btpd/btpd-0.16.tar.gz'
-  sha1 'fb7d396ed5c224dc6e743ac481e4feb4a3cf7b75'
+  homepage "https://github.com/btpd/btpd"
+  url "https://github.com/downloads/btpd/btpd/btpd-0.16.tar.gz"
+  sha1 "fb7d396ed5c224dc6e743ac481e4feb4a3cf7b75"
+  revision 1
+
+  depends_on "openssl"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match /Torrents can be specified/, pipe_output("#{bin}/btcli --help 2>&1")
   end
 end
