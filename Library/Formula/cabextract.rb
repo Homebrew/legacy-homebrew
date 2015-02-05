f24da93d4632266b95da90c8bd4b277701ba5b74
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-05 22:57:31 +0000

    cabextract: update 1.5 bottle.

--- Library/Formula/cabextract.rb
+++ Library/Formula/cabextract.rb
@@ -5,9 +5,9 @@ class Cabextract < Formula
 
   bottle do
     cellar :any
-    sha1 "7bb2b3a1ed06161c81d23fd8735477317bad86d7" => :yosemite
-    sha1 "b81507d68f43f9c32f5614293247eab43a61177c" => :mavericks
-    sha1 "8c9696a300165c215e741d644502a5ea22a65173" => :mountain_lion
+    sha1 "c7cd0a4500cfc35680ffa4687f9683bc741bbec0" => :yosemite
+    sha1 "2367b8a88d18e528e1dded8425a8954e5402f472" => :mavericks
+    sha1 "838d7c66e6447f79ea4b6bff4d7e29af1f25c5a9" => :mountain_lion
   end
 
   def install
