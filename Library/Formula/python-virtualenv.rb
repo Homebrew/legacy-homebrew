6f3f90a94a8be884676679fc7933e71f91aa2702
Tomohiro Nakamura <quickness.net@gmail.com>
2015-02-05 12:54:41 +0900

    mod python-virtualenv Formula: add test

--- Library/Formula/python-virtualenv.rb
+++ Library/Formula/python-virtualenv.rb
@@ -1,5 +1,5 @@
 class PythonVirtualenv < Formula
-  homepage "https://pypi.python.org/pypi/virtualenv/"
+  homepage "https://virtualenv.pypa.io/"
   url "https://pypi.python.org/packages/source/v/virtualenv/virtualenv-12.0.6.tar.gz"
   version "12.0.6"
   sha1 "0bb156834a60cc75763eb7c859383d6b86d4ac0c"
@@ -12,5 +12,9 @@ class PythonVirtualenv < Formula
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    system bin/"virtualenv", testpath/"venv"
   end
 end
