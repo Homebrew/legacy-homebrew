4b52565bbbee83540df643fcbc33d49cb7aba8ec
Anand Buddhdev <arhbkb@gmail.com>
2015-02-05 13:32:37 +0100

    nsd 4.1.1
    Update NSD to 4.1.1
    
    Closes #36554.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/nsd.rb
+++ Library/Formula/nsd.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Nsd < Formula
   homepage 'http://www.nlnetlabs.nl/projects/nsd/'
-  url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-4.1.0.tar.gz'
-  sha1 'cb4ec496eaec5bfbd5b9f4b361da2a0a79d17030'
+  url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-4.1.1.tar.gz'
+  sha1 '11ca8a7bef74500f486db5deac994fdf3dec7358'
 
   option 'with-root-server', 'Allow NSD to run as a root name server'
   option 'with-bind8-stats', 'Enable BIND8-like NSTATS & XSTATS'
   option 'with-ratelimit', 'Enable rate limiting'
+  option 'with-zone-stats', 'Enable per-zone statistics'
 
   depends_on 'libevent'
 
@@ -20,6 +21,7 @@ class Nsd < Formula
     args << '--enable-root-server' if build.with? 'root-server'
     args << '--enable-bind8-stats' if build.with? 'bind8-stats'
     args << '--enable-ratelimit' if build.with? 'ratelimit'
+    args << '--enable-zone-stats' if build.with? 'zone-stats'
 
     system "./configure", *args
     system "make install"
