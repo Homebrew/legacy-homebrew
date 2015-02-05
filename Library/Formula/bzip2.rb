31c421a904e90ccdec69c863e3c11e00c1f6bbad
Shaun Jackman <sjackman@gmail.com>
2015-02-05 11:40:14 -0800

    bzip2 1.0.6: Merge homebrew/dupes/bzip2

--- Library/Formula/bzip2.rb
+++ Library/Formula/bzip2.rb
@@ -1,12 +1,21 @@
-require "formula"
-
 class Bzip2 < Formula
-  homepage "http://www.bzip.org/downloads.html"
+  homepage "http://www.bzip.org/"
   url "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
   sha1 "3f89f861209ce81a6bab1fd1998c0ef311712002"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/dupes"
+    sha1 "5d515fa12bc177239863569721caad7987e267dc" => :yosemite
+    sha1 "b250429608acd57ea5411dd6ae9b3d4675fbbffb" => :mavericks
+    sha1 "07a558b9a4271377490b876c1d1cb345ee4d3f18" => :mountain_lion
+  end
+
+  keg_only :provided_by_osx
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
+    mkdir_p share
+    mv prefix/"man", share
 
     if OS.linux?
       # Install the shared library.
@@ -19,6 +28,14 @@ class Bzip2 < Formula
   end
 
   test do
-    system "#{bin}/bzip2 --help"
+    testfilepath = testpath + "sample_in.txt"
+    zipfilepath = testpath + "sample_in.txt.bz2"
+
+    testfilepath.write "TEST CONTENT"
+
+    system "#{bin}/bzip2", testfilepath
+    system "#{bin}/bunzip2", zipfilepath
+
+    assert_equal "TEST CONTENT", testfilepath.read
   end
 end
