43d90c99b969d1de7d0334c57166dd2154997cf8
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-06 09:16:42 +0100

    ht: basic test added

--- Library/Formula/ht.rb
+++ Library/Formula/ht.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Ht < Formula
-  homepage 'http://hte.sf.net/'
-  url 'https://downloads.sourceforge.net/project/hte/ht-source/ht-2.1.0.tar.bz2'
-  sha256 '31f5e8e2ca7f85d40bb18ef518bf1a105a6f602918a0755bc649f3f407b75d70'
+  homepage "http://hte.sf.net/"
+  url "https://downloads.sourceforge.net/project/hte/ht-source/ht-2.1.0.tar.bz2"
+  sha256 "31f5e8e2ca7f85d40bb18ef518bf1a105a6f602918a0755bc649f3f407b75d70"
 
   bottle do
     cellar :any
@@ -12,13 +10,17 @@ class Ht < Formula
     sha1 "1e862ab300b0e5a417d96d628a7d2163865bcce9" => :mountain_lion
   end
 
-  depends_on 'lzo'
+  depends_on "lzo"
 
   def install
     chmod 0755, "./install-sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-x11-textmode"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "ht #{version}", shell_output("#{bin}/ht -v")
   end
 end
