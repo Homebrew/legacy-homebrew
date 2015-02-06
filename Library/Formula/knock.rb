5e3c955f9b6eb1bfd8a81aa9c1dc718c346f3037
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-06 09:35:53 +0000

    knock: add 0.7 bottle.

--- Library/Formula/knock.rb
+++ Library/Formula/knock.rb
@@ -3,6 +3,13 @@ class Knock < Formula
   url "http://www.zeroflux.org/proj/knock/files/knock-0.7.tar.gz"
   sha1 "186a687c9db11733cf23a7423884a863873ebb27"
 
+  bottle do
+    cellar :any
+    sha1 "e81973835ea940928a166c0edc2c405fe8a6cac5" => :yosemite
+    sha1 "df0459ca7e7f8b18cd719e333c7a016f130c2e88" => :mavericks
+    sha1 "ab688df7694977ee058159505c2b3f674df31fc4" => :mountain_lion
+  end
+
   head do
     url "https://github.com/jvinet/knock.git"
 
