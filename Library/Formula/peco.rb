6cc6e00c08eddf141c48f3a083b23ffce3fb7029
Kozo Nishida <knishida@riken.jp>
2015-02-08 03:02:28 +0900

    use go_resource

--- Library/Formula/peco.rb
+++ Library/Formula/peco.rb
@@ -1,25 +1,37 @@
+require "language/go"
+
 class Peco < Formula
   homepage "https://github.com/peco/peco"
-  url "https://github.com/peco/peco/releases/download/v0.2.11/peco_darwin_amd64.zip"
-  sha1 "1c92cfa7f2dc6d85f11c4225c1b2afff506b141f"
-  version "0.2.11"
+  url "https://github.com/peco/peco/archive/v0.2.11.tar.gz"
+  sha1 "438e76dc7f31215eb1195d5eb14a66cf7fef318e"
 
-  head do
-    url "https://github.com/peco/peco.git"
-    depends_on "go" => :build
+  go_resource "github.com/jessevdk/go-flags" do
+    url "https://github.com/jessevdk/go-flags.git",
+      :revision => "15347ef417a300349807983f15af9e65cd2e1b3a"
   end
 
+  go_resource "github.com/mattn/go-runewidth" do
+    url "https://github.com/mattn/go-runewidth.git",
+      :revision => "8adae32de8a26f36cc7acaa53051407d514bb5f0"
+  end
+
+  go_resource "github.com/nsf/termbox-go" do
+    url "https://github.com/nsf/termbox-go.git",
+      :revision => "9e7f2135126fcf13f331e7b24f5d66fd8e8e1690"
+  end
+
+  go_resource "github.com/peco/peco" do
+    url "https://github.com/peco/peco.git",
+      :revision => "0ad82671a0546fe4cace0eb9787b900bcc77aad0"
+  end
+
+  depends_on "go" => :build
+
   def install
-    if build.head?
-      ENV["GOPATH"] = buildpath
-      mkdir_p buildpath/"src/github.com/peco"
-      ln_s buildpath, buildpath/"src/github.com/peco/peco"
-      system "go", "get", "github.com/jessevdk/go-flags"
-      system "go", "get", "github.com/mattn/go-runewidth"
-      system "go", "get", "github.com/nsf/termbox-go"
-      system "go", "get", "github.com/peco/peco"
-      system "go", "build", "cmd/peco/peco.go"
-    end
+    ENV["GOPATH"] = buildpath
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    system "go", "build", "cmd/peco/peco.go"
     bin.install "peco"
   end
 
