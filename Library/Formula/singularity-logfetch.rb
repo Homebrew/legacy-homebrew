a6dd993ff234b6ad77140c1c474f835248cd8a84
tpetr <tpetr@hubspot.com>
2015-02-07 15:36:24 -0500

    missed some deps, apparently

--- Library/Formula/singularity-logfetch.rb
+++ Library/Formula/singularity-logfetch.rb
@@ -17,6 +17,16 @@ class SingularityLogfetch < Formula
     md5 "ad2a71db8bd9a017ed4735eac7acfa07"
   end
 
+  resource "gevent" do
+    url "https://pypi.python.org/packages/source/g/gevent/gevent-1.0.1.tar.gz"
+    md5 "7b952591d1a0174d6eb6ac47bd975ab6"
+  end
+
+  resource "greenlet" do
+    url "https://pypi.python.org/packages/source/g/greenlet/greenlet-0.4.5.zip"
+    md5 "ce383f6475e6311cf8932ea779938703"
+  end
+
   resource "grequests" do
     url "https://pypi.python.org/packages/source/g/grequests/grequests-0.2.0.tar.gz"
     md5 "23186795cf69d127f5e90df665d25387"
@@ -32,14 +42,14 @@ class SingularityLogfetch < Formula
     md5 "043e89644f8909d462fbbfa511c768df"
   end
 
-  resource "gevent" do
-    url "https://pypi.python.org/packages/source/g/gevent/gevent-1.0.1.tar.gz"
-    md5 "7b952591d1a0174d6eb6ac47bd975ab6"
+  resource "wsgiref" do
+    url "https://pypi.python.org/packages/source/w/wsgiref/wsgiref-0.1.2.zip"
+    md5 "29b146e6ebd0f9fb119fe321f7bcf6cb"
   end
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    %w[argparse ConfigParser grequests requests termcolor gevent].each do |r|
+    %w[argparse ConfigParser gevent greenlet grequests requests termcolor wsgiref].each do |r|
       resource(r).stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
