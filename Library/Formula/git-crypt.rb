dc3f8feb530b5c8ed78bbae6ec8dbe57fd937581
Elliot Saba <staticfloat@gmail.com>
2015-02-07 02:55:39 -0800

    Bump git-crypt to 0.4.2

--- Library/Formula/git-crypt.rb
+++ Library/Formula/git-crypt.rb
@@ -2,15 +2,8 @@ require 'formula'
 
 class GitCrypt < Formula
   homepage 'https://www.agwa.name/projects/git-crypt/'
-  url "https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.4.1.tar.gz"
-  sha1 "b82aca26385ffd96432fa25fda7c4753874d917f"
-
-  bottle do
-    cellar :any
-    sha1 "cf82899f5909b6f4fba7375f0ca14328f2b3d6af" => :yosemite
-    sha1 "f15f0c82ad1114c2bdc676db25a0801c915eabdf" => :mavericks
-    sha1 "0cbd44ee06d875c9784307cc8a45e593dbb5bdd7" => :mountain_lion
-  end
+  url "https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.4.2.tar.gz"
+  sha1 "c084d73d285ed2c7b9840d4d527cb51ecc7a687b"
 
   depends_on "openssl"
 
@@ -20,6 +13,7 @@ class GitCrypt < Formula
   end
 
   test do
+    # Note; `git-crypt keygen` planned for deprecation
     system "#{bin}/git-crypt", "keygen", "keyfile"
   end
 end
