0ec3542e573d4d2c9cef516e05c8f0ab5ba969dc
Alex Leighton <axlelonghorn@gmail.com>
2015-02-07 19:17:52 -0800

    gdbm: add with-libgdbm-compat to build ndbm header
    Closes #36640.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gdbm.rb
+++ Library/Formula/gdbm.rb
@@ -1,7 +1,7 @@
 class Gdbm < Formula
-  homepage "http://www.gnu.org/software/gdbm/"
-  url "http://ftpmirror.gnu.org/gdbm/gdbm-1.11.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/gdbm/gdbm-1.11.tar.gz"
+  homepage "https://www.gnu.org/software/gdbm/"
+  url "https://ftpmirror.gnu.org/gdbm/gdbm-1.11.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/gdbm/gdbm-1.11.tar.gz"
   sha1 "ce433d0f192c21d41089458ca5c8294efe9806b4"
 
   bottle do
@@ -13,12 +13,20 @@ class Gdbm < Formula
   end
 
   option :universal
+  option "with-libgdbm-compat", "Build libgdbm_compat, a compatibility layer which provides UNIX-like dbm and ndbm interfaces."
 
   def install
     ENV.universal_binary if build.universal?
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+    ]
+
+    args << "--enable-libgdbm-compat" if build.with? "libgdbm-compat"
+
+    system "./configure", *args
     system "make", "install"
   end
 
