f79c080091d094b82fa4a44220e2885afbd4b4d7
tpetr <tpetr@hubspot.com>
2015-02-08 15:14:21 -0500

    use sha1 instead of md5

--- Library/Formula/singularity-logfetch.rb
+++ Library/Formula/singularity-logfetch.rb
@@ -9,42 +9,42 @@ class SingularityLogfetch < Formula
 
   resource "argparse" do
     url "https://pypi.python.org/packages/source/a/argparse/argparse-1.2.2.tar.gz"
-    md5 "38589b29d9120b19dfca32f86406a1f5"
+    sha1 "d5eb880eafd4aae5f66f9fe94262571725d1b036"
   end
 
   resource "ConfigParser" do
     url "https://pypi.python.org/packages/source/c/configparser/configparser-3.5.0b2.tar.gz"
-    md5 "ad2a71db8bd9a017ed4735eac7acfa07"
+    sha1 "253a8eeb97e1a6b983a4d5dcaa74d8f15078e0a4"
   end
 
   resource "gevent" do
     url "https://pypi.python.org/packages/source/g/gevent/gevent-1.0.1.tar.gz"
-    md5 "7b952591d1a0174d6eb6ac47bd975ab6"
+    sha1 "2cc1b6e1fa29b30ea881fa6a195e57faaf089ae8"
   end
 
   resource "greenlet" do
     url "https://pypi.python.org/packages/source/g/greenlet/greenlet-0.4.5.zip"
-    md5 "ce383f6475e6311cf8932ea779938703"
+    sha1 "97f18d651595bd30243ad2f4702764791e57fa6e"
   end
 
   resource "grequests" do
     url "https://pypi.python.org/packages/source/g/grequests/grequests-0.2.0.tar.gz"
-    md5 "23186795cf69d127f5e90df665d25387"
+    sha1 "9a55beb7a5c7c2ea0e59497442164ec6a0958204"
   end
 
   resource "requests" do
     url "https://pypi.python.org/packages/source/r/requests/requests-2.5.0.tar.gz"
-    md5 "b8bf3ddca75e7ecf1b6776da1e6e3385"
+    sha1 "d60dfaaa0b4b62a6646fcb6c3954ea369317ca9f"
   end
 
   resource "termcolor" do
     url "https://pypi.python.org/packages/source/t/termcolor/termcolor-1.1.0.tar.gz"
-    md5 "043e89644f8909d462fbbfa511c768df"
+    sha1 "52045880a05c0fbd192343d9c9aad46a73d20e8c"
   end
 
   resource "wsgiref" do
     url "https://pypi.python.org/packages/source/w/wsgiref/wsgiref-0.1.2.zip"
-    md5 "29b146e6ebd0f9fb119fe321f7bcf6cb"
+    sha1 "80b7e9b039e40a2f8419e00b393a6516d80cf8de"
   end
 
   def install
