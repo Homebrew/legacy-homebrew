7ee315fceb37114600c18cee3a1e73bd9e5e759b
Mike McQuaid <mike@mikemcquaid.com>
2015-02-09 12:38:35 +0000

    postgresql: fix perl option.
    Addresses comment on d3074f5bb9bb7d522bff7ad7873f03610ed15054.

--- Library/Formula/postgresql.rb
+++ Library/Formula/postgresql.rb
@@ -53,7 +53,7 @@ class Postgresql < Formula
     ]
 
     args << "--with-python" if build.with? "python"
-    args << "--with-perl" if build.with? "no-perl"
+    args << "--with-perl" if build.with? "perl"
 
     # The CLT is required to build tcl support on 10.7 and 10.8 because
     # tclConfig.sh is not part of the SDK
