bb5c1d7a79488bc46a0d8d23ccfc7416f428c97f
Dave Cottlehuber <dch@skunkwerks.at>
2015-02-09 14:22:22 +0100

    riemann-client (new formula)
    A C-based Riemann client.
    
    Closes #36697.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/riemann-client.rb
@@ -0,0 +1,25 @@
+class RiemannClient < Formula
+  homepage "https://github.com/algernon/riemann-c-client"
+  url "https://github.com/algernon/riemann-c-client/archive/riemann-c-client-1.3.0.tar.gz"
+  sha1 "0b360e12839683a3a89caf2cd58a8fb1e337e19e"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+
+  depends_on "json-c"
+  depends_on "protobuf-c"
+
+  def install
+    system "autoreconf", "-i"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/riemann-client", "send", "-h"
+  end
+end
