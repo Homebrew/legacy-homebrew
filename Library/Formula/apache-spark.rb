d3a1e3cc9365a37b1a1fac1dbc80c9e061578d8f
Glavin Wiechert <glavin.wiechert@gmail.com>
2015-02-10 10:03:42 -0400

    apache-spark 1.2.1

--- Library/Formula/apache-spark.rb
+++ Library/Formula/apache-spark.rb
@@ -3,9 +3,9 @@ require "formula"
 class ApacheSpark < Formula
   homepage "http://spark.apache.org/"
   head "https://github.com/apache/spark.git"
-  url "http://d3kbcqa49mib13.cloudfront.net/spark-1.2.0-bin-hadoop2.4.tgz"
-  version "1.2.0"
-  sha1 "57d19d43aa058acefbde32b2304678a8b07eaa80"
+  url "http://d3kbcqa49mib13.cloudfront.net/spark-1.2.1-bin-hadoop2.4.tgz"
+  version "1.2.1"
+  sha1 "2c8023f339ee9a0a05ecf6cb854f1de70b324f41"
 
   conflicts_with 'hive', :because => 'both install `beeline` binaries'
 
