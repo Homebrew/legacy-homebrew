02ee7d80a2f3bd2df44d2233c1364819bd1d0c3f
Mickele Moriconi <mickele.moriconi@gmail.com>
2015-02-09 23:55:00 -0200

    Remove unnecessary blank lines

--- Library/Formula/kotlin-compiler.rb
+++ Library/Formula/kotlin-compiler.rb
@@ -1,5 +1,4 @@
 class KotlinCompiler < Formula
-
   homepage "http://kotlinlang.org/"
   url "https://github.com/JetBrains/kotlin/releases/download/build-0.10.195/kotlin-compiler-0.10.195.zip"
   sha1 "f37b8ed7dd6e71ad367b1d2356d12edbeb598479"
@@ -16,5 +15,4 @@ class KotlinCompiler < Formula
     script_path.write "println(\"Hello Kotlin\")"
     assert_equal "Hello Kotlin", shell_output("#{bin}/kotlinc -script #{script_path}").strip
   end
-
 end
