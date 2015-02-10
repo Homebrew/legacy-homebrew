87178154c4fa3e030d97aa6167db32d7731a4d44
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-10 12:34:59 +0000

    riemann-client: add 1.3.0 bottle.

--- Library/Formula/riemann-client.rb
+++ Library/Formula/riemann-client.rb
@@ -3,6 +3,13 @@ class RiemannClient < Formula
   url "https://github.com/algernon/riemann-c-client/archive/riemann-c-client-1.3.0.tar.gz"
   sha1 "0b360e12839683a3a89caf2cd58a8fb1e337e19e"
 
+  bottle do
+    cellar :any
+    sha1 "348a16b5fb7d400481de3109f147cfe198cf2717" => :yosemite
+    sha1 "12cd20d92512f4fb1f8b559da06fa88f78e267e5" => :mavericks
+    sha1 "9c3e0cf990883eec78f9cc51f42407cc80f832cc" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
