b6bdce670dd22247bfb4085c2d533c7ed7a3d0c4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-11 05:04:15 +0000

    peco: add 0.2.11 bottle.

--- Library/Formula/peco.rb
+++ Library/Formula/peco.rb
@@ -5,6 +5,12 @@ class Peco < Formula
   url "https://github.com/peco/peco/archive/v0.2.11.tar.gz"
   sha1 "438e76dc7f31215eb1195d5eb14a66cf7fef318e"
 
+  bottle do
+    sha1 "09dcbdd0a4cc55c36cc4578f237ac19c360651a1" => :yosemite
+    sha1 "eaf5161f6ce66b67a026825a4f3047974d147500" => :mavericks
+    sha1 "61e853d89cd34129cf0e010c36939491c3b11329" => :mountain_lion
+  end
+
   go_resource "github.com/jessevdk/go-flags" do
     url "https://github.com/jessevdk/go-flags.git",
       :revision => "15347ef417a300349807983f15af9e65cd2e1b3a"
