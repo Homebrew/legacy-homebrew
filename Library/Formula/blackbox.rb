05d541c211504a3c04fb97ce81f7365abf22353d
Erik Kristensen <erik@erikkristensen.com>
2015-02-11 15:14:55 -0700

    fixing blackbox formula

--- Library/Formula/blackbox.rb
+++ Library/Formula/blackbox.rb
@@ -4,7 +4,26 @@ class Blackbox < Formula
   version "1.20150203"
   sha1 "dbe69ab20924ecc5c02ad917a52a57ec1f01addd"
 
+  patch do
+    url "https://gist.githubusercontent.com/ekristen/84875c88ffd502f190cc/raw/770221fb0cefea9212fc987a08fad2bfe181c110/homebrew.patch"
+    sha1 "c507a4ba7daccd755e6505c34cdb241087bc0143"
+  end
+
   def install
-    bin.install 'bin/_blackbox_common.sh', 'bin/_stack_lib.sh', 'bin/blackbox_addadmin', 'bin/blackbox_cat', 'bin/blackbox_edit', 'bin/blackbox_edit_end', 'bin/blackbox_edit_start', 'bin/blackbox_initialize', 'bin/blackbox_postdeploy', 'bin/blackbox_register_new_file', 'bin/blackbox_removeadmin', 'bin/blackbox_shred_all_files', 'bin/blackbox_update_all_files'
+    chmod 0755, Dir["bin/*"]
+    libexec.install Dir['*']
+    
+    bin.install_symlink "#{libexec}/bin/blackbox_addadmin"
+    bin.install_symlink "#{libexec}/bin/blackbox_cat"
+    bin.install_symlink "#{libexec}/bin/blackbox_edit"
+    bin.install_symlink "#{libexec}/bin/blackbox_edit_end"
+    bin.install_symlink "#{libexec}/bin/blackbox_edit_start"
+    bin.install_symlink "#{libexec}/bin/blackbox_initialize"
+    bin.install_symlink "#{libexec}/bin/blackbox_postdeploy"
+    bin.install_symlink "#{libexec}/bin/blackbox_register_new_file"
+    bin.install_symlink "#{libexec}/bin/blackbox_removeadmin"
+    bin.install_symlink "#{libexec}/bin/blackbox_shred_all_files"
+    bin.install_symlink "#{libexec}/bin/blackbox_update_all_files"
+    bin.install_symlink "#{libexec}/bin/blackbox_addadmin"
   end
 end
