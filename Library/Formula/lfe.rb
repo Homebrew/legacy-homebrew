c8576e0efed79b62922980a1bdef0dc4e81d8616
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-11 10:57:56 +0000

    lfe: add 0.9.1 bottle.

--- Library/Formula/lfe.rb
+++ Library/Formula/lfe.rb
@@ -5,6 +5,12 @@ class Lfe < Formula
 
   head "https://github.com/rvirding/lfe.git", :branch => "develop"
 
+  bottle do
+    sha1 "7b297fcd87472e51a9bbaa996bf87de9ebd3c3d6" => :yosemite
+    sha1 "3b04cf153041bf79eb49218f8006fe2c7a998fa6" => :mavericks
+    sha1 "4fd64b920713e4f5af36fedd04fc9b840e0280cb" => :mountain_lion
+  end
+
   depends_on "erlang"
   depends_on "rebar"
 
