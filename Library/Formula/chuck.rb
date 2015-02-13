1dfa98af4e7a4276d593b7d7dbaf36062dd672ab
Alexis Hildebrandt <afh@surryhill.net>
2015-02-13 22:41:38 +0100

    chuck 1.3.5.0
    Closes #36804.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/chuck.rb
+++ Library/Formula/chuck.rb
@@ -1,7 +1,7 @@
 class Chuck < Formula
   homepage "http://chuck.cs.princeton.edu/"
-  url "http://chuck.cs.princeton.edu/release/files/chuck-1.3.4.0.tgz"
-  sha1 "d32faae2cb60fc81d2716b477cf2d54bc548d9c6"
+  url "http://chuck.cs.princeton.edu/release/files/chuck-1.3.5.0.tgz"
+  sha1 "a97ad56f9fd1b0793ead099cc9723e888b3782c3"
 
   bottle do
     cellar :any
@@ -9,11 +9,6 @@ class Chuck < Formula
     sha1 "513d888da91c755f743693a607075ee4d87b7661" => :mavericks
     sha1 "f9831daea9100f13865049b40057fcec6b6e6203" => :mountain_lion
   end
-
-  # Homebrew already takes care of setting the sysroot;
-  # also lets the build work on CLT.
-  # Also fixes the version regex for 10.10+; reported to chuck-dev.
-  patch :DATA
 
   def install
     system "make", "-C", "src", "osx"
@@ -25,20 +20,3 @@ class Chuck < Formula
     assert_match /probe \[success\]/m, shell_output("#{bin}/chuck --probe 2>&1")
   end
 end
-
-__END__
-diff --git a/src/makefile.osx b/src/makefile.osx
-index 4531ee1..65cee97 100644
---- a/src/makefile.osx
-+++ b/src/makefile.osx
-@@ -1,9 +1,7 @@
- # uncomment the following to force 32-bit compilation
- # FORCE_M32=-m32
- 
--ifneq ($(shell sw_vers -productVersion | egrep '10\.[6789](\.[0-9]+)?'),)
--SDK=$(shell xcodebuild -sdk macosx -version | grep '^Path:' | sed 's/Path: \(.*\)/\1/')
--ISYSROOT=-isysroot $(SDK)
-+ifneq ($(shell sw_vers -productVersion | egrep '10\.([6789]|1[0-9]+)(\.[0-9]+)?'),)
- LINK_EXTRAS=-F/System/Library/PrivateFrameworks \
-     -weak_framework MultitouchSupport
- else
