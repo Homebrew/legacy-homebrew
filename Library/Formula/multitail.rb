277769c654e3ea24265a89ee6b7171a9578485c8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-13 10:53:26 +0000

    multitail: add 6.4.1 bottle.

--- Library/Formula/multitail.rb
+++ Library/Formula/multitail.rb
@@ -3,6 +3,13 @@ class Multitail < Formula
   url "http://www.vanheusden.com/multitail/multitail-6.4.1.tgz"
   sha1 "9191b807f8d727810b2824a9bc8aafa17cb0d10e"
 
+  bottle do
+    cellar :any
+    sha1 "a6911971f13cac78f86e31643c7eb5746092c553" => :yosemite
+    sha1 "69576c39aab13d2ce6451db33dbcf46b68e20596" => :mavericks
+    sha1 "81473772c2a8b55e79299c17d1a595496da7c1ff" => :mountain_lion
+  end
+
   # Upstream pull request to fix compilation issues on OS X
   # https://github.com/flok99/multitail/pull/13
   patch do
