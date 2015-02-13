2e0aea6e195739c52a09ddeb7f6c98caba3615b2
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-13 09:30:31 +0100

    multitail 6.4.1
    Closes #36782.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/multitail.rb
+++ Library/Formula/multitail.rb
@@ -1,16 +1,26 @@
-require 'formula'
-
 class Multitail < Formula
-  homepage 'http://www.vanheusden.com/multitail/download.html'
-  url 'http://www.vanheusden.com/multitail/multitail-6.2.1.tgz'
-  sha1 '30bd77c4f542cd6ad6f1a0351f7d9e0a6cef6585'
+  homepage "http://vanheusden.com/multitail/"
+  url "http://www.vanheusden.com/multitail/multitail-6.4.1.tgz"
+  sha1 "9191b807f8d727810b2824a9bc8aafa17cb0d10e"
+
+  # Upstream pull request to fix compilation issues on OS X
+  # https://github.com/flok99/multitail/pull/13
+  patch do
+    url "https://github.com/flok99/multitail/pull/13.diff"
+    sha1 "cf1c457ecfc2fbe76f60050829af30c8a6698d5c"
+  end
 
   def install
-    ENV['DESTDIR'] = prefix
     system "make", "-f", "makefile.macosx", "multitail"
 
     bin.install "multitail"
     man1.install gzip("multitail.1")
     etc.install "multitail.conf"
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    assert_match "multitail #{version}",
+      shell_output("#{bin}/multitail -h 2>&1", 1)
   end
 end
