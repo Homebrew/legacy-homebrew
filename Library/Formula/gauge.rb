1f06e0db9903d825a684241c00dc6e2a6579f01f
prateekbaheti <prateektheone@gmail.com>
2015-02-14 00:05:47 +0530

    gauge 0.0.4
    Closes #36799.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gauge.rb
+++ Library/Formula/gauge.rb
@@ -2,8 +2,8 @@ require "language/go"
 
 class Gauge < Formula
   homepage "http://getgauge.io"
-  url "https://github.com/getgauge/gauge/archive/v0.0.3.tar.gz"
-  sha1 "5cb64ce8d803c739c3e21513b334e0d2216cc68a"
+  url "https://github.com/getgauge/gauge/archive/v0.0.4.tar.gz"
+  sha1 "d0bbfd4c782bccca5fbf3f8fa9ada71e1723a5eb"
 
   bottle do
     sha1 "6b503ea7ac28bd315a8a7fa55917aaccb20e9e87" => :yosemite
@@ -13,20 +13,36 @@ class Gauge < Formula
 
   depends_on "go" => :build
 
-  go_resource "code.google.com/p/goprotobuf" do
-    url "https://code.google.com/p/goprotobuf",
-        :revision => "725eb0907b649c438e0e8f1601648597141fb66c",
-        :using => :hg
+  go_resource "github.com/golang/protobuf" do
+    url "https://github.com/golang/protobuf.git",
+        :revision => "7f07925444bb51fa4cf9dfe6f7661876f8852275"
   end
 
   go_resource "github.com/getgauge/common" do
     url "https://github.com/getgauge/common.git",
-        :revision => "4c20d2d8fc5c4c447a6110f4f888f72c3d66f84c"
+        :revision => "a947acc34020882e7a00d6366a66b24ec7f3f578"
   end
 
   go_resource "github.com/getgauge/mflag" do
     url "https://github.com/getgauge/mflag.git",
         :revision => "d64a28a7abc05602c9e6d9c5a1488ee69f9fcb83"
+  end
+
+  go_resource "github.com/op/go-logging" do
+    url "https://github.com/op/go-logging.git",
+        :revision => "fb0230561a6ba1cab17beb95f1faedc16584fdb8"
+  end
+
+  go_resource "golang.org/x/tools" do
+    url "https://go.googlesource.com/tools",
+        :revision => "2ef5a0d23bc4e07573bb094b97e96c9cd9844fca",
+        :using => :git
+  end
+
+  go_resource "gopkg.in/natefinch/lumberjack.v2" do
+    url "https://gopkg.in/natefinch/lumberjack.v2",
+        :revision => "d28785c2f27cd682d872df46ccd8232843629f54",
+        :using =>:git
   end
 
   go_resource "github.com/dmotylev/goproperties" do
