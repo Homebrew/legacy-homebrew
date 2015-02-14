214619440c9d376ee82084b51fc84396fd372eaa
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-14 23:37:13 +0100

    curlish 1.22 (new formula)
    Closes #36823.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/curlish.rb
@@ -0,0 +1,16 @@
+class Curlish < Formula
+  homepage "https://pythonhosted.org/curlish/"
+  url "https://github.com/fireteam/curlish/archive/1.22.tar.gz"
+  sha1 "45e9a7d92b5f70adf257cda3f9f0f205eef0245b"
+
+  # curlish needs argparse (2.7+)
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    bin.install "curlish.py" => "curlish"
+  end
+
+  test do
+    system "#{bin}/curlish", "http://brew.sh"
+  end
+end
