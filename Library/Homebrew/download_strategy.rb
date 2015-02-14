16d1a91a5d3e7a18dcad315d2a3ce0da408b5b83
Jack Nagel <jacknagel@gmail.com>
2015-02-14 16:32:24 -0500

    Stage entire git repo into build directory
    Closes #27193.

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -537,13 +537,7 @@ class GitDownloadStrategy < VCSDownloadStrategy
 
   def stage
     super
-
-    dst = Dir.getwd
-    cached_location.cd do
-      # https://stackoverflow.com/questions/160608/how-to-do-a-git-export-like-svn-export
-      safe_system 'git', 'checkout-index', '-a', '-f', "--prefix=#{dst}/"
-      checkout_submodules(dst) if submodules?
-    end
+    cp_r File.join(cached_location, "."), Dir.pwd
   end
 
   private
@@ -648,12 +642,6 @@ class GitDownloadStrategy < VCSDownloadStrategy
   def update_submodules
     quiet_safe_system "git", "submodule", "foreach", "--recursive", "git submodule sync"
     quiet_safe_system "git", "submodule", "update", "--init", "--recursive"
-  end
-
-  def checkout_submodules(dst)
-    escaped_clone_path = cached_location.to_s.gsub(/\//, '\/')
-    sub_cmd = %[git checkout-index -a -f --prefix="#{dst}/${toplevel/#{escaped_clone_path}/}/$path/"]
-    quiet_safe_system "git", "submodule", "foreach", "--recursive", sub_cmd
   end
 end
 
