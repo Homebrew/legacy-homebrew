af001dcbe5c5f893a1a846ce344f06275f0743cf
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-14 22:11:06 +0000

    cdk 5.0.20141106
    Bump.
    
    Closes #36822.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/cdk.rb
+++ Library/Formula/cdk.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class Cdk < Formula
-  homepage 'http://invisible-island.net/cdk/'
-  url 'ftp://invisible-island.net/cdk/cdk-5.0-20140118.tgz'
-  version '5.0.20140118'
-  sha1 'd900e9e0d54a90701541d40ff7137507baf3b382'
+  homepage "http://invisible-island.net/cdk/"
+  url "ftp://invisible-island.net/cdk/cdk-5.0-20141106.tgz"
+  version "5.0.20141106"
+  sha1 "81d1804412dbcdc399a91e08e024e30890c1a291"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--with-ncurses"
     system "make", "install"
+  end
+
+  test do
+    assert_match "#{lib}", shell_output("#{bin}/cdk5-config --libdir")
   end
 end
