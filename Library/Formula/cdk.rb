ed8a14b072e963eccb3f459dfccb64d3ae61d9c5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-14 22:21:23 +0000

    cdk: add 5.0.20141106 bottle.

--- Library/Formula/cdk.rb
+++ Library/Formula/cdk.rb
@@ -4,6 +4,13 @@ class Cdk < Formula
   version "5.0.20141106"
   sha1 "81d1804412dbcdc399a91e08e024e30890c1a291"
 
+  bottle do
+    cellar :any
+    sha1 "12c872acc62f90d545301bffab39162b84e630e9" => :yosemite
+    sha1 "a9277afebbdf2837c2b91eb6a31eaa49f83d0885" => :mavericks
+    sha1 "5a9922e908c6ab427688ec141a03dfb29562cfa5" => :mountain_lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--with-ncurses"
     system "make", "install"
