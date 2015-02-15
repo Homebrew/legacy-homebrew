22d4797781350404a3b442871558140bf5beb33a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-15 12:00:42 +0000

    tmux-mem-cpu-load: add 3.1.0 bottle.

--- Library/Formula/tmux-mem-cpu-load.rb
+++ Library/Formula/tmux-mem-cpu-load.rb
@@ -5,6 +5,13 @@ class TmuxMemCpuLoad < Formula
   url "https://github.com/thewtex/tmux-mem-cpu-load/archive/v3.1.0.tar.gz"
   sha1 "b243a064a24bf86853ae9ab1daf0fb650a23bd97"
 
+  bottle do
+    cellar :any
+    sha1 "f1128ff9697f59c56caa8f83d09752749fa2937d" => :yosemite
+    sha1 "9b8093ea6d70b3665594fa166e8067c4bd0576fa" => :mavericks
+    sha1 "93df136a1c19a26bb236b4b99e6a96b39014bf17" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
