3e5277e684abe4b2ea9d856ecbc271cca279c3bb
Colin King <richard.colin.king@gmail.com>
2015-02-15 12:17:48 -0600

    abduco 0.3
    Closes #36830.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/abduco.rb
+++ Library/Formula/abduco.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Abduco < Formula
   homepage "http://www.brain-dump.org/projects/abduco"
-  url "http://www.brain-dump.org/projects/abduco/abduco-0.1.tar.gz"
-  sha1 "063b66d8a9a83ecd5b9501afc86812a06ad79076"
+  url "http://www.brain-dump.org/projects/abduco/abduco-0.3.tar.gz"
+  sha1 "175b2c0eaf2a8b7fb044f1454d018dac4ec31293"
   head "git://repo.or.cz/abduco.git"
 
   bottle do
@@ -13,21 +11,21 @@ class Abduco < Formula
     sha1 "d7c559b7d1a696d8ca34934276f715461ee0ed33" => :lion
   end
 
-  # upstream fix for BSD strip -s behavior compared to GNU
-  # safe to remove in abduco versions after 0.1
+  # upstream fix for create-session: Invalid argument
+  # safe to remove in versions > 0.3
   patch do
-    url "http://repo.or.cz/w/abduco.git/patch/940519"
-    sha1 "45bd29721d0e52d6875a8e3e21c16792a5758be9"
+    url "http://repo.or.cz/w/abduco.git/patch/91b733"
+    sha1 "4370ceb4c304ecd916e69c6c7c876666807f6fb8"
   end
 
   def install
+    ENV.append_to_cflags "-D_DARWIN_C_SOURCE"
     system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
-    session = "0.1-homebrew-test-session"
-    expected = "[?25h[999Habduco: #{session}: session terminated with exit status 0\n"
-    output = `printf "" | #{bin}/abduco -e "" -c #{session} true &> /dev/null && printf "\n" | #{bin}/abduco -a #{session} 2>&1 | sed '$d' | tail -1 | sed 's/.$//'`
-    assert_equal expected, output
+    result = shell_output("#{bin}/abduco -v")
+    result.force_encoding("UTF-8") if result.respond_to?(:force_encoding)
+    assert_match /^abduco-#{version}/, result
   end
 end
