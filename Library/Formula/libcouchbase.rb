0c2bd56a536987132457108583936ff3d6b13e7c
Mark Nunberg <mnunberg@haskalah.org>
2015-02-16 07:02:56 -0800

    libcouchbase 2.4.7
    Closes #36860.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -1,7 +1,7 @@
 class Libcouchbase < Formula
   homepage 'http://docs.couchbase.com/developer/c-2.4/c-intro.html'
-  url 'http://packages.couchbase.com/clients/c/libcouchbase-2.4.6.tar.gz'
-  sha1 '2a4ce6b0f6a934a3fa3ef697d26ed6c4737aacae'
+  url 'http://packages.couchbase.com/clients/c/libcouchbase-2.4.7.tar.gz'
+  sha1 '62c68ccc269099cdc1fd624324107131347aad45'
   head "https://github.com/couchbase/libcouchbase", :using => :git
 
   bottle do
