f7df4eea6c0a236d21fa7dd139e13310d0d70361
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-16 12:01:54 +0100

    fex: test added
    Closes #36854.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/fex.rb
+++ Library/Formula/fex.rb
@@ -1,12 +1,13 @@
-require 'formula'
-
 class Fex < Formula
-  homepage 'http://www.semicomplete.com/projects/fex/'
-  url 'https://semicomplete.googlecode.com/files/fex-2.0.0.tar.gz'
-  sha1 '014938009ffe0b2ec3d1293154a22e4a40fee4a9'
+  homepage "http://www.semicomplete.com/projects/fex/"
+  url "https://semicomplete.googlecode.com/files/fex-2.0.0.tar.gz"
+  sha1 "014938009ffe0b2ec3d1293154a22e4a40fee4a9"
 
   def install
-    ENV['PREFIX'] = prefix
-    system "make install"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_equal "foo", pipe_output("#{bin}/fex 1", "foo bar", 0).chomp
   end
 end
