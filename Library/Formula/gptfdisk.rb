b9c279c6679c71329aa35dfcf5afe9783f3f2dd3
hallucynogenyc <hallucynogenyc@gmail.com>
2015-02-16 19:06:14 +0000

    Sourceforge main mirror is not working
    This forces a different mirror.

--- Library/Formula/gptfdisk.rb
+++ Library/Formula/gptfdisk.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Gptfdisk < Formula
   homepage "http://www.rodsbooks.com/gdisk/"
-  url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/0.8.10/gptfdisk-0.8.10.tar.gz"
+  url "http://cznic.dl.sourceforge.net/project/gptfdisk/gptfdisk/0.8.10/gptfdisk-0.8.10.tar.gz"
   sha1 "1708e232220236b6bdf299b315e9bc2205c01ba5"
   revision 2
 
