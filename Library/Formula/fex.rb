a50b7c7e05eb99a3b5d504d7a8617c7e594a09b6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-16 11:15:03 +0000

    fex: add 2.0.0 bottle.

--- Library/Formula/fex.rb
+++ Library/Formula/fex.rb
@@ -3,6 +3,13 @@ class Fex < Formula
   url "https://semicomplete.googlecode.com/files/fex-2.0.0.tar.gz"
   sha1 "014938009ffe0b2ec3d1293154a22e4a40fee4a9"
 
+  bottle do
+    cellar :any
+    sha1 "1ae0b6d8788ede53263fc5e0dea922444bab606b" => :yosemite
+    sha1 "2759dee42d85788ba2dc04bdf6f85ad3f9eecd87" => :mavericks
+    sha1 "5abfa033f2dbe1725633a0d4390ac6e558982187" => :mountain_lion
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
