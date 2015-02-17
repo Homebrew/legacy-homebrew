206df1542aff9e86a7bb581c6f9fb8d14e92a10c
Tyler Battle <tbattle@boundlessgeo.com>
2015-02-17 12:18:07 -0800

    typesafe-activator: change chmod to octal notation

--- Library/Formula/typesafe-activator.rb
+++ Library/Formula/typesafe-activator.rb
@@ -9,7 +9,7 @@ class TypesafeActivator < Formula
     rm Dir["*.bat"] # Remove Windows .bat files
     prefix.install_metafiles
     libexec.install Dir['*']
-    chmod 'a+x', libexec/'activator'
+    chmod 0755, libexec/'activator'
     bin.write_exec_script libexec/'activator'
   end
 end
