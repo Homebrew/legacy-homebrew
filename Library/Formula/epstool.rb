6a63297b43a5cf189c31bbb15fffdd48d35e5402
Xu Cheng <xucheng@me.com>
2015-02-18 00:43:11 +0800

    epstool: add test and missing runtime dependency
    Closes #36905.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/epstool.rb
+++ Library/Formula/epstool.rb
@@ -1,15 +1,20 @@
-require "formula"
-
 class Epstool < Formula
   homepage "http://pages.cs.wisc.edu/~ghost/gsview/epstool.htm"
   url "http://pkgs.fedoraproject.org/repo/pkgs/epstool/epstool-3.08.tar.gz/465a57a598dbef411f4ecbfbd7d4c8d7/epstool-3.08.tar.gz"
   mirror "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Epstool/epstool-3.08.tar.gz"
   sha1 "dc495934f06d3ea8b3209e8b02ea96c66c34f614"
 
+  depends_on "ghostscript"
+
   def install
     system "make", "install",
                    "EPSTOOL_ROOT=#{prefix}",
                    "EPSTOOL_MANDIR=#{man}",
                    "CC=#{ENV.cc}"
+  end
+
+  test do
+    system bin/"epstool", "--add-tiff-preview", "--device", "tiffg3", test_fixtures("test.eps"), "test2.eps"
+    assert File.exist?("test2.eps")
   end
 end
