e8887bcf26b1f3082e88f75f8cc3ef67122de5b3
stites <sam@stites.io>
2015-02-17 14:27:30 -0800

    task 2.4.1
    bump - task to 2.4.1
    
    Closes #36912.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -1,8 +1,8 @@
 class Task < Formula
   homepage "http://www.taskwarrior.org/"
-  url "http://www.taskwarrior.org/download/task-2.4.0.tar.gz"
-  sha1 "2c01e3eb874e7e499d31c99f93a37925b2f60ce8"
-  head "https://git.tasktools.org/scm/tm/task.git", :branch => "2.4.1", :shallow => false
+  url "http://taskwarrior.org/download/task-2.4.1.tar.gz"
+  sha1 "4882628ac339e31fcb0fef2f4a1c585e86eb95c7"
+  head "https://git.tasktools.org/scm/tm/task.git", :branch => "2.4.2", :shallow => false
 
   bottle do
     sha1 "38699c8bead1b03912c2ca5bc7205d655629ae75" => :yosemite
