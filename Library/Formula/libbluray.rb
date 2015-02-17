a4cdd99ce0ceb103208992420e2ec5c16e1a398a
David Christenson <david@the-david.com>
2015-02-16 22:56:48 -0700

    libbluray 0.7.0
    Update to latest stable release, require fontconfig for head and
    stable, modernize formula.
    
    Closes #36886.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libbluray.rb
+++ Library/Formula/libbluray.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Libbluray < Formula
   homepage "https://www.videolan.org/developers/libbluray.html"
-  url "ftp://ftp.videolan.org/pub/videolan/libbluray/0.6.2/libbluray-0.6.2.tar.bz2"
-  sha1 "a1ab8c8c9310680fb1fe6a58f9fc5430613600fe"
+  url "ftp://ftp.videolan.org/pub/videolan/libbluray/0.7.0/libbluray-0.7.0.tar.bz2"
+  sha1 "12baf71accac2ae9efe7fb077999821ea1430d7b"
 
   bottle do
     cellar :any
@@ -20,11 +18,11 @@ class Libbluray < Formula
     depends_on "automake" => :build
     depends_on "libtool" => :build
     depends_on "ant" => :build
-    depends_on "fontconfig"
   end
 
   depends_on "pkg-config" => :build
   depends_on "freetype" => :recommended
+  depends_on "fontconfig"
 
   def install
     # https://mailman.videolan.org/pipermail/libbluray-devel/2014-April/001401.html
@@ -37,6 +35,6 @@ class Libbluray < Formula
     system "./bootstrap" if build.head?
     system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
