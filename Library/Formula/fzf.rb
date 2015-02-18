31153619b1b2e4bcd44f3d732fc3d849567f4d0f
Junegunn Choi <junegunn.c@gmail.com>
2015-02-18 13:35:46 +0900

    fzf 0.9.3
    Closes #36918.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/fzf.rb
+++ Library/Formula/fzf.rb
@@ -3,8 +3,8 @@ require "language/go"
 
 class Fzf < Formula
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.9.2.tar.gz"
-  sha1 "b065794baa83b418ee2ca1e87c46a19c0478bd7c"
+  url "https://github.com/junegunn/fzf/archive/0.9.3.tar.gz"
+  sha1 "0d18bf1c084e31187910202ee1e2e6bdcf26d97a"
 
   bottle do
     sha1 "36a1edab80c2afba434775977489b1a3bd978805" => :yosemite
