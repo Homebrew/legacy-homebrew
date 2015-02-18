9b5acdff63d63aafaf35dfb66fbd41be6a37c739
Xu Cheng <xucheng@me.com>
2015-02-18 23:26:04 +0800

    pngpaste: add test
    Closes #36934.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/pngpaste.rb
+++ Library/Formula/pngpaste.rb
@@ -1,5 +1,3 @@
-require "formula"
-
 class Pngpaste < Formula
   homepage "https://github.com/jcsalterego/pngpaste"
   url "https://github.com/jcsalterego/pngpaste/archive/0.2.1.tar.gz"
@@ -8,5 +6,12 @@ class Pngpaste < Formula
   def install
     system "make", "all"
     bin.install "pngpaste"
+  end
+
+  test do
+    png = test_fixtures("test.png")
+    system "osascript", "-e", "set the clipboard to POSIX file (\"#{png}\")"
+    system bin/"pngpaste", "test.png"
+    assert File.exist? "test.png"
   end
 end
