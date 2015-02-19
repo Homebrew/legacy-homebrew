39871ed81a18bfb26b8d7f9b2aaad6824f1de3e0
Mike McQuaid <mike@mikemcquaid.com>
2015-02-19 12:47:41 +0000

    software_spec: use homebrew Bintray subdomain.
    Also: split into two variables so they can be used for taps.

--- Library/Homebrew/software_spec.rb
+++ Library/Homebrew/software_spec.rb
@@ -252,7 +252,8 @@ class BottleSpecification
   DEFAULT_PREFIX = "/usr/local".freeze
   DEFAULT_CELLAR = "/usr/local/Cellar".freeze
   if ENV["HOMEBREW_BINTRAY_TESTING"]
-    DEFAULT_ROOT_URL = "https://bintray.com/artifact/download/homebrew/bottles".freeze
+    DEFAULT_DOMAIN = "https://homebrew.bintray.com".freeze
+    DEFAULT_ROOT_URL = "#{DEFAULT_DOMAIN}/bottles".freeze
   else
     DEFAULT_ROOT_URL = "https://downloads.sf.net/project/machomebrew/Bottles".freeze
   end
