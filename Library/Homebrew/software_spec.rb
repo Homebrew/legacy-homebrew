0d8c63b6a580d8776eeb9bf47fbaca8a7eb7f542
Mike McQuaid <mike@mikemcquaid.com>
2015-02-19 16:57:30 +0000

    Switch bottle provider over to Bintray.

--- Library/Homebrew/software_spec.rb
+++ Library/Homebrew/software_spec.rb
@@ -251,11 +251,11 @@ end
 class BottleSpecification
   DEFAULT_PREFIX = "/usr/local".freeze
   DEFAULT_CELLAR = "/usr/local/Cellar".freeze
-  if ENV["HOMEBREW_BINTRAY_TESTING"]
+  if ENV["HOMEBREW_SOURCEFORGE_TESTING"]
+    DEFAULT_ROOT_URL = "https://downloads.sf.net/project/machomebrew/Bottles".freeze
+  else
     DEFAULT_DOMAIN = "https://homebrew.bintray.com".freeze
     DEFAULT_ROOT_URL = "#{DEFAULT_DOMAIN}/bottles".freeze
-  else
-    DEFAULT_ROOT_URL = "https://downloads.sf.net/project/machomebrew/Bottles".freeze
   end
 
   attr_rw :root_url, :prefix, :cellar, :revision
