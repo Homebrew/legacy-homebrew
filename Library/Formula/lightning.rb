1351434f7c63a159f7db1bed905c8b1c187b3b04
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-19 10:21:52 +0100

    lightning 2.1.0

--- Library/Formula/lightning.rb
+++ Library/Formula/lightning.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Lightning < Formula
-  homepage "http://www.gnu.org/software/lightning/"
-  url "http://ftpmirror.gnu.org/lightning/lightning-2.0.5.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/lightning/lightning-2.0.5.tar.gz"
-  sha1 "a3b7ba0697690f578bac1d4c991c8e3cbaa33aaf"
+  homepage "https://www.gnu.org/software/lightning/"
+  url "http://ftpmirror.gnu.org/lightning/lightning-2.1.0.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/lightning/lightning-2.1.0.tar.gz"
+  sha1 "d08aa434fba8fb29d0c1b240b034042c26cbf2b3"
 
   bottle do
     cellar :any
@@ -13,8 +11,47 @@ class Lightning < Formula
     sha1 "eb8db8f32ceddaddaf445bb45268d13d753f89ba" => :lion
   end
 
+  depends_on "binutils" => [:build, :optional]
+
   def install
-    system "./configure","--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    args = [
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}",
+    ]
+    args << "--disable-disassembler" if build.without? "binutils"
+
+    system "./configure", *args
+    system "make", "check", "-j1"
+    system "make", "install"
+  end
+
+  test do
+    # from http://www.gnu.org/software/lightning/manual/lightning.html#incr
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <lightning.h>
+      static jit_state_t *_jit;
+      typedef int (*pifi)(int);
+      int main(int argc, char *argv[]) {
+        jit_node_t  *in;
+        pifi incr;
+        init_jit(argv[0]);
+        _jit = jit_new_state();
+        jit_prolog();
+        in = jit_arg();
+        jit_getarg(JIT_R0, in);
+        jit_addi(JIT_R0, JIT_R0, 1);
+        jit_retr(JIT_R0);
+        incr = jit_emit();
+        jit_clear_state();
+        printf("%d + 1 = %d\\n", 5, incr(5));
+        jit_destroy_state();
+        finish_jit();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-llightning", "-o", "test"
+    system "./test"
   end
 end
