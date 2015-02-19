35f1d0b86e48a0725a0158720fca6824c2ce6c52
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-19 14:59:20 +0000

    kvazaar: add 0.4.0 bottle.

--- Library/Formula/kvazaar.rb
+++ Library/Formula/kvazaar.rb
@@ -3,6 +3,13 @@ class Kvazaar < Formula
   url "https://github.com/ultravideo/kvazaar/archive/v0.4.0.tar.gz"
   sha1 "28fce493e8fcd7274993ab46eb1a1c5d07569853"
 
+  bottle do
+    cellar :any
+    sha1 "14925edf1ac9c07c54c872d7b5d3d7708a9f814a" => :yosemite
+    sha1 "f7c026308db68e957324585b74a3ab0746289a6d" => :mavericks
+    sha1 "d0c6b625b2beefbb9dafe2f9eaad678fe97d0fa7" => :mountain_lion
+  end
+
   depends_on "yasm" => :build
 
   def install
