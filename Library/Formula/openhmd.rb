ce7987d865de032d50ebbf8d70e13d6d17b1067e
David Poirier-Quinot <davipoir@gmail.com>
2015-02-21 00:17:52 +0100

    openhmd - update (homebrew format compliance and test)

--- Library/Formula/openhmd.rb
+++ Library/Formula/openhmd.rb
@@ -4,31 +4,24 @@ class Openhmd < Formula
   url "http://openhmd.net/releases/openhmd-0.1.0.tar.gz"
   sha1 "186c747399bd9a509ac8300acbae8823fc4fcc79"
 
-  head "https://github.com/OpenHMD/OpenHMD.git", :branch => "master"
-
   head do
-    sha1 "bd475d6a2992d0729e3c376a013ff97f3bbce387"
+    url "https://github.com/OpenHMD/OpenHMD.git", :branch => "master"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
   end
 
   depends_on "cmake" => :build
-  depends_on "autoconf" => :build # if built from git
-  depends_on "automake" => :build # if built from git
-  depends_on "pkg-config" => :build # not sure yet
-  depends_on "hidapi"
-  depends_on "libtool" => :build if build.head?
+  depends_on "pkg-config" => :build
+  depends_on "hidapi" => :build
 
   def install
-
-    if not build.head?
-      opoo "run `brew install openhmd --HEAD' (from git HEAD) for DK2 support (02/2015)"
-    end
-
     args = ["--prefix", prefix,
             "--disable-debug",
             "--disable-silent-rules",
             "--disable-dependency-tracking"]
 
-    system "./autogen.sh" if build.head? # (if building openhmd from the git repository)
+    system "./autogen.sh" if build.head?
 
     system "./configure", *args
 
@@ -36,6 +29,6 @@ class Openhmd < Formula
   end
 
   test do
-    system "false"
+    system "unittests"
   end
 end
