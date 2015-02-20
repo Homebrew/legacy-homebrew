54eb346466891def2eb251ad3bf764ba92b24de6
Mike McQuaid <mike@mikemcquaid.com>
2015-02-20 15:37:29 +0000

    juju: mark bottle as relocatable.

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -6,6 +6,7 @@ class Juju < Formula
   sha1 '760281b70c33b6f7fd2c24525d9a892a3deec5df'
 
   bottle do
+    cellar :any
     sha1 "fd2f07e8df8cb63c19121e6f73cce7dda8e3a72d" => :yosemite
     sha1 "ff71ae1fe7c28eb0e13e65f917123b7214e23d16" => :mavericks
     sha1 "81eabb39070e65f598382e3aa9dda43eacd1c8be" => :mountain_lion
