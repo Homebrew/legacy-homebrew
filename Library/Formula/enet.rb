d6a4ce4321e25b89bb94e2589a19bcad0924b35c
Peter Powell <petpow@saberuk.com>
2015-02-20 16:46:41 +0000

    ENet 1.3.12
    Closes #37000.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/enet.rb
+++ Library/Formula/enet.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Enet < Formula
   homepage 'http://enet.bespin.org'
-  url 'http://enet.bespin.org/download/enet-1.3.11.tar.gz'
-  sha1 'f1b2e49aa2bbdd7d75e889f3f6d3b0c1a56b6080'
+  url 'http://enet.bespin.org/download/enet-1.3.12.tar.gz'
+  sha1 '5fd9ef590dc7bcff652e99fef3e7241b3a743f25'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
