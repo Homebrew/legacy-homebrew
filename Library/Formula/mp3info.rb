9df35bf65bb49437281a05a6ea51a17cd9498aef
Xu Cheng <xucheng@me.com>
2015-02-20 22:01:17 +0800

    mp3info: add test
    Closes #36996.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/mp3info.rb
+++ Library/Formula/mp3info.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Mp3info < Formula
-  homepage 'http://www.ibiblio.org/mp3info/'
-  url 'http://www.ibiblio.org/pub/linux/apps/sound/mp3-utils/mp3info/mp3info-0.8.5a.tgz'
-  sha1 '54df29eb6c2e581899affc12be698b0a71d91ca6'
+  homepage "http://www.ibiblio.org/mp3info/"
+  url "http://www.ibiblio.org/pub/linux/apps/sound/mp3-utils/mp3info/mp3info-0.8.5a.tgz"
+  sha1 "54df29eb6c2e581899affc12be698b0a71d91ca6"
 
   patch :p0 do
     url "https://trac.macports.org/export/34602/trunk/dports/audio/mp3info/files/patch-mp3tech.c.diff"
@@ -11,8 +9,12 @@ class Mp3info < Formula
   end
 
   def install
-    system "make mp3info doc"
+    system "make", "mp3info", "doc"
     bin.install "mp3info"
     man1.install "mp3info.1"
+  end
+
+  test do
+    system bin/"mp3info", "-x", test_fixtures("test.mp3")
   end
 end
