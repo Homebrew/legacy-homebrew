d5f357762ceaab92a33032fa4cccebf363fe6e95
Kegan Pankratz <kepankra@cisco.com>
2015-02-20 15:05:11 -0800

    activemq 5.10.2
    Closes #37008.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Activemq < Formula
   homepage "http://activemq.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=/activemq/5.10.1/apache-activemq-5.10.1-bin.tar.gz"
-  sha1 "5e62deb1ccd103ec1765f836756a7889f6131ab0"
+  url "http://www.apache.org/dyn/closer.cgi?path=/activemq/5.10.2/apache-activemq-5.10.2-bin.tar.gz"
+  sha1 "7b5d797abf6d6767d2f5a17935d8cac829d230cd"
 
   def install
     rm_rf Dir["bin/linux-x86-*"]
