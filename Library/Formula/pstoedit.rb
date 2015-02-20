c5290a8af1d913e26b568af671a2a2f6471be6f0
Xu Cheng <xucheng@me.com>
2015-02-21 01:54:54 +0800

    pstoedit: add test
    Closes #36902.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/pstoedit.rb
+++ Library/Formula/pstoedit.rb
@@ -1,21 +1,24 @@
-require 'formula'
-
 class Pstoedit < Formula
-  homepage 'http://www.pstoedit.net'
-  url 'https://downloads.sourceforge.net/project/pstoedit/pstoedit/3.62/pstoedit-3.62.tar.gz'
-  sha1 '50d5a4e2fe0e0ff2f73cb094cb945b221083e742'
+  homepage "http://www.pstoedit.net"
+  url "https://downloads.sourceforge.net/project/pstoedit/pstoedit/3.62/pstoedit-3.62.tar.gz"
+  sha1 "50d5a4e2fe0e0ff2f73cb094cb945b221083e742"
   revision 1
 
-  depends_on 'pkg-config' => :build
-  depends_on 'plotutils'
-  depends_on 'ghostscript'
-  depends_on 'imagemagick'
-  depends_on 'xz' if MacOS.version < :mavericks
+  depends_on "pkg-config" => :build
+  depends_on "plotutils"
+  depends_on "ghostscript"
+  depends_on "imagemagick"
+  depends_on "xz" if MacOS.version < :mavericks
 
   def install
     ENV.deparallelize
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"pstoedit", "-f", "pdf", test_fixtures("test.ps"), "test.pdf"
+    assert File.exist?("test.pdf")
   end
 end
