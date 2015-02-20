6fa1046470c86e12de4369cefd21f00357b44737
Alexis Hildebrandt <afh@surryhill.net>
2015-02-20 23:26:27 +0100

    newsbeuter: modernize
    * Replace single quotes with double quotes
    * Remove obsolete require 'formula'
    * Add test

--- Library/Formula/newsbeuter.rb
+++ Library/Formula/newsbeuter.rb
@@ -1,17 +1,15 @@
-require 'formula'
-
 class Newsbeuter < Formula
-  homepage 'http://newsbeuter.org/'
-  url 'http://www.newsbeuter.org/downloads/newsbeuter-2.9.tar.gz'
-  sha1 'e0d61cda874ea9b77ed27f2edfea50a6ea471894'
+  homepage "http://newsbeuter.org/"
+  url "http://www.newsbeuter.org/downloads/newsbeuter-2.9.tar.gz"
+  sha1 "e0d61cda874ea9b77ed27f2edfea50a6ea471894"
 
-  head 'https://github.com/akrennmair/newsbeuter.git'
+  head "https://github.com/akrennmair/newsbeuter.git"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gettext'
-  depends_on 'json-c'
-  depends_on 'libstfl'
-  depends_on 'sqlite'
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "json-c"
+  depends_on "libstfl"
+  depends_on "sqlite"
 
   needs :cxx11
 
@@ -19,5 +17,11 @@ class Newsbeuter < Formula
     ENV.cxx11
     system "make"
     system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    urlfile = "urls.txt"
+    (testpath/urlfile).write "https://github.com/blog/subscribe\n"
+    assert_match /newsbeuter - Exported Feeds/m, shell_output("#{bin}/newsbeuter -e -u #{urlfile}")
   end
 end
