c2d0ef1d73bfbcfffb7785da910489781885b480
Tim D. Smith <git@tim-smith.us>
2015-02-21 12:11:34 -0800

    googlecl: include gdata resource
    Fixes #36992.
    
    Closes #37035.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/googlecl.rb
+++ Library/Formula/googlecl.rb
@@ -1,5 +1,3 @@
-require 'formula'
-
 class Googlecl < Formula
   homepage 'https://code.google.com/p/googlecl/'
   url 'https://googlecl.googlecode.com/files/googlecl-0.9.14.tar.gz'
@@ -9,16 +7,25 @@ class Googlecl < Formula
 
   conflicts_with 'osxutils', :because => 'both install a google binary'
 
-  def install
-    ENV["PYTHONPATH"] = lib+"python2.7/site-packages"
-    system "python", "setup.py", "install",
-      "--prefix=#{prefix}", "--single-version-externally-managed",
-      "--record=installed.txt"
+  resource "gdata" do
+    url "https://pypi.python.org/packages/source/g/gdata/gdata-2.0.18.tar.gz"
+    sha1 "4cd6804f2af81697219307421996c6055c7c16e4"
+  end
 
+  def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    resource("gdata").stage do
+      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+    end
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV['PYTHONPATH'])
   end
 
   test do
-    system "#{bin}/google", '--version'
+    system "#{bin}/google", "help", "docs"
   end
 end
