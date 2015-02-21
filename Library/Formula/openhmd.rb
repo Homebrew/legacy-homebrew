670b44fb0583969e48c161156ffa0a72d01de4db
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-21 15:34:48 +0000

    openhmd: add 0.1.0 bottle.

--- Library/Formula/openhmd.rb
+++ Library/Formula/openhmd.rb
@@ -3,6 +3,13 @@ class Openhmd < Formula
   url "http://openhmd.net/releases/openhmd-0.1.0.tar.gz"
   sha1 "186c747399bd9a509ac8300acbae8823fc4fcc79"
 
+  bottle do
+    cellar :any
+    sha1 "dac2183e6b2b3e63d8046204fadc727db1eaab3a" => :yosemite
+    sha1 "4e7dcfa3ed6f521d908859251d3e6552e8aa70a2" => :mavericks
+    sha1 "d72136368a6c671711c9a135d4b256813e06c755" => :mountain_lion
+  end
+
   head do
     url "https://github.com/OpenHMD/OpenHMD.git"
     depends_on "autoconf" => :build
