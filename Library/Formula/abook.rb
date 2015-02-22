b42eefb53e951ead7a0e64cf3ab134ba38cacb25
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-22 20:54:45 +0000

    abook: add 0.5.6 bottle.

--- Library/Formula/abook.rb
+++ Library/Formula/abook.rb
@@ -4,6 +4,12 @@ class Abook < Formula
   sha1 "79f04f2264c8bd81bbc952b6560c86d69b21615d"
   head "git://git.code.sf.net/p/abook/git"
 
+  bottle do
+    sha1 "79a5f0c68339b53df2558854ffef927aa1ef9b29" => :yosemite
+    sha1 "2b71a4c791d982933129d096468e9faf365cf35a" => :mavericks
+    sha1 "9d84e27e92763c9c9d45f4af89e433e6f5b77a49" => :mountain_lion
+  end
+
   devel do
     url "http://abook.sourceforge.net/devel/abook-0.6.0pre2.tar.gz"
     sha1 "42a939fba43e51aa011fa185113c12ec4bc1e1ec"
