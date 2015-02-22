1448028e8eea523143da7b81c830ec917dc0c029
Geoff Nixon <geoff@geoff.codes>
2015-02-22 01:40:20 -0800

    sipsak: update url, homepage
    Closes #37067.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/sipsak.rb
+++ Library/Formula/sipsak.rb
@@ -1,17 +1,21 @@
-require 'formula'
-
 class Sipsak < Formula
-  homepage 'http://sipsak.org/'
-  url 'http://download.berlios.de/sipsak/sipsak-0.9.6-1.tar.gz'
-  version '0.9.6'
-  sha1 '6d2fd2c90ea04be749e48927b3e7fc08c52166b6'
+  homepage "http://sourceforge.net/projects/sipsak.berlios/"
+  url "https://downloads.sourceforge.net/project/sipsak.berlios/sipsak-0.9.6-1.tar.gz"
+  version "0.9.6"
+  sha1 "6d2fd2c90ea04be749e48927b3e7fc08c52166b6"
+
+  depends_on "openssl"
 
   def install
-    ENV.append 'CFLAGS', '-std=gnu89'
+    ENV.append "CFLAGS", "-std=gnu89"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/sipsak", "-V"
   end
 end
