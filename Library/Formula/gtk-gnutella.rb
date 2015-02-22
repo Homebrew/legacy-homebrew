7573695cb9a150cab4a14a36699db2d824111a6e
Geoff Nixon <geoff@geoff.codes>
2015-02-22 02:01:31 -0800

    gtk-gnutella 1.1.1
    Closes #37060.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/gtk-gnutella.rb
+++ Library/Formula/gtk-gnutella.rb
@@ -1,15 +1,16 @@
-require 'formula'
-
 class GtkGnutella < Formula
-  homepage 'http://gtk-gnutella.sourceforge.net/en/?page=news'
-  url 'https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.0.0/gtk-gnutella-1.0.0.tar.bz2'
-  sha1 'bd38b103dbccbca5d1431330df5c5d5b3eca4a24'
+  homepage "http://gtk-gnutella.sourceforge.net/en/?page=news"
+  url "https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.1.1/gtk-gnutella-1.1.1.tar.bz2"
+  sha1 "aa4f21fe758f7ac52c15ee0156247443ce8c7d6f"
 
-  depends_on 'gtk+'
+  depends_on "pkg-config" => :build
+  depends_on "gtk+"
 
   def install
+    ENV.deparallelize
+
     system "./build.sh",  "--prefix=#{prefix}", "--disable-nls"
-    system "make install"
+    system "make", "install"
     rm_rf share+"pixmaps"
     rm_rf share+"applications"
   end
