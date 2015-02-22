850fd2276a288e027dfed638969ee164b2d8d914
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-22 20:36:24 +0100

    aacgain: test added
    Closes #37079.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/aacgain.rb
+++ Library/Formula/aacgain.rb
@@ -1,17 +1,23 @@
-require 'formula'
-
 class Aacgain < Formula
-  homepage 'http://aacgain.altosdesign.com/'
+  homepage "http://aacgain.altosdesign.com/"
   # This server will autocorrect a 1.9 url back to this 1.8 tarball.
   # The 1.9 version mentioned on the website is pre-release, so make
   # sure 1.9 is actually out before updating.
   # See: https://github.com/Homebrew/homebrew/issues/16838
-  url 'http://aacgain.altosdesign.com/alvarez/aacgain-1.8.tar.bz2'
-  sha1 '331039c4231e4d85ae878795ce3095dd96dcbfdb'
+  url "http://aacgain.altosdesign.com/alvarez/aacgain-1.8.tar.bz2"
+  sha1 "331039c4231e4d85ae878795ce3095dd96dcbfdb"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    # aacgain modifies files in-place
+    # See: https://github.com/Homebrew/homebrew/pull/37080
+    cp test_fixtures("test.mp3"), "test.mp3"
+    system "#{bin}/aacgain", "test.mp3"
   end
 end
