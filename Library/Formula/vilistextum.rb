e8ef93f5d33156b4d76180573100aba0fbf6a9e1
Geoff Nixon <geoff@geoff.codes>
2015-02-22 01:26:58 -0800

    vilistextum: update url, homepage
    Closes #37071.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/vilistextum.rb
+++ Library/Formula/vilistextum.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class Vilistextum < Formula
-  homepage 'http://bhaak.dyndns.org/vilistextum/'
-  url 'http://bhaak.dyndns.org/vilistextum/vilistextum-2.6.9.tar.gz'
-  sha1 'd62fe5213b61c0d0356bb2e60757dd535ac0a82b'
+  homepage "http://bhaak.net/vilistextum/"
+  url "http://bhaak.net/vilistextum/vilistextum-2.6.9.tar.gz"
+  sha1 "d62fe5213b61c0d0356bb2e60757dd535ac0a82b"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make install"
+                          "--prefix=#{prefix}", "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/vilistextum", "-v"
   end
 end
