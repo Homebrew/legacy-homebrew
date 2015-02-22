44bf44144b14cf216463d3ed04b844a6b51b35df
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-22 11:39:38 +0000

    logcheck: add 1.3.17 bottle.

--- Library/Formula/logcheck.rb
+++ Library/Formula/logcheck.rb
@@ -3,6 +3,13 @@ class Logcheck < Formula
   url "http://ftp.de.debian.org/debian/pool/main/l/logcheck/logcheck_1.3.17.tar.xz"
   sha1 "adb54e75f8a17e3aff4abb3066122c0dfdde21e3"
 
+  bottle do
+    cellar :any
+    sha1 "94773d87f8879d53bdcaa26853342d7a45af1da8" => :yosemite
+    sha1 "88197b7fa15f6842a2fe3e07f38e825e7b02947e" => :mavericks
+    sha1 "c2b5c09aae9987d2441ca205d74ce44439e912c0" => :mountain_lion
+  end
+
   def install
     system "make", "install",
                    "--always-make",
