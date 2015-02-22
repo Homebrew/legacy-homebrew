3b293fbd956b2814e5479b34261861c725b8ab2c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-22 14:05:56 +0000

    sipsak: add 0.9.6 bottle.

--- Library/Formula/sipsak.rb
+++ Library/Formula/sipsak.rb
@@ -4,6 +4,13 @@ class Sipsak < Formula
   version "0.9.6"
   sha1 "6d2fd2c90ea04be749e48927b3e7fc08c52166b6"
 
+  bottle do
+    cellar :any
+    sha1 "0887f7a2fc31fa4dac20abc09b3978102ceb585e" => :yosemite
+    sha1 "0792fa0643ed06e407ee67fdf34a0bca6ef93fdd" => :mavericks
+    sha1 "bd7856973c90e54510f7d5b6305f032bd0f006fa" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
