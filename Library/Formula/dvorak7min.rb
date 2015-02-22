398c405eb6f41df236e69ca207cf3124648a3e8f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-22 12:40:49 +0000

    dvorak7min: add 1.6.1 bottle.

--- Library/Formula/dvorak7min.rb
+++ Library/Formula/dvorak7min.rb
@@ -4,6 +4,13 @@ class Dvorak7min < Formula
   sha1 "d4d2d300d6c6dc037f4411cccfe9bb237d26d178"
   version "1.6.1"
 
+  bottle do
+    cellar :any
+    sha1 "3fa9330cc362cdf2e1141fdc334087c98fe4bce1" => :yosemite
+    sha1 "245f20ef29453e0a269beee466b1d18b23ddcfc4" => :mavericks
+    sha1 "f4654d6ad3c11702c98b9343fbed51d6a9eba401" => :mountain_lion
+  end
+
   def install
     # Remove pre-built ELF binary first
     system "make", "clean"
