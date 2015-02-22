c19d2b955016e153c52514cb409b6e94ac122c61
Geoff Nixon <geoff@geoff.codes>
2015-02-22 00:18:46 -0800

    Split out from #37014 per "one commit per formula, one formula per commit" guideline.

--- Library/Homebrew/tap_migrations.rb
+++ Library/Homebrew/tap_migrations.rb
@@ -22,6 +22,7 @@ TAP_MIGRATIONS = {
   "cmucl" => "homebrew/binary",
   "comparepdf" => "homebrew/boneyard",
   "connect" => "homebrew/boneyard",
+  "coremod" => "homebrew/boneyard",
   "curlftpfs" => "homebrew/x11",
   "cwm" => "homebrew/x11",
   "dart" => "dart-lang/dart",
