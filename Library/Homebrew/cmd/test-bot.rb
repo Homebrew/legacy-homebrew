0e20977c82bc9ca46351c71091638ea2cf72f616
Mike McQuaid <mike@mikemcquaid.com>
2015-02-23 15:12:09 +0000

    test-bot: don't blow up if formula can't be found.

--- Library/Homebrew/cmd/test-bot.rb
+++ Library/Homebrew/cmd/test-bot.rb
@@ -725,7 +725,8 @@ module Homebrew
       existing_bottles = {}
       Dir.glob("*.bottle*.tar.gz") do |filename|
         formula_name = bottle_filename_formula_name filename
-        formula = Formulary.factory formula_name
+        formula = Formulary.factory formula_name rescue nil
+        next unless formula
         existing_bottles[formula_name] = !!formula.bottle
       end
 
