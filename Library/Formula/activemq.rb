549045f8ba1ada55389002a325af2cdabf5164dc
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-23 11:47:08 +0100

    activemq 5.11.1

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Activemq < Formula
   homepage "http://activemq.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=/activemq/5.10.2/apache-activemq-5.10.2-bin.tar.gz"
-  sha1 "7b5d797abf6d6767d2f5a17935d8cac829d230cd"
+  url "http://www.apache.org/dyn/closer.cgi?path=/activemq/5.11.1/apache-activemq-5.11.1-bin.tar.gz"
+  sha1 "db4310f037af4a430abfdddf18c2df4082a34340"
+
+  depends_on :java => "1.6+"
 
   def install
     rm_rf Dir["bin/linux-x86-*"]
@@ -13,5 +13,10 @@ class Activemq < Formula
 
     bin.write_exec_script libexec/"bin/activemq"
     bin.write_exec_script libexec/"bin/activemq-admin"
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    system "#{bin}/activemq-admin", "browse", "-h"
   end
 end
