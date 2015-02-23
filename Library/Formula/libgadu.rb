1980971168ca576f12dae31c92cb30d00522eca2
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-02-23 10:19:28 +0100

    libgadu: use https url
    Closes #37097.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/libgadu.rb
+++ Library/Formula/libgadu.rb
@@ -1,6 +1,6 @@
 class Libgadu < Formula
   homepage "http://libgadu.net/"
-  url "http://github.com/wojtekka/libgadu/releases/download/1.12.1/libgadu-1.12.1.tar.gz"
+  url "https://github.com/wojtekka/libgadu/releases/download/1.12.1/libgadu-1.12.1.tar.gz"
   sha1 "a41435c0ae5dd5e7e3b998915639a8288398f86e"
 
   bottle do
