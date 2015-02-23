f8d2198b3ca5b89aa919e9176fbe4589c476ea24
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-23 01:52:38 +0000

    vilistextum: add 2.6.9 bottle.

--- Library/Formula/vilistextum.rb
+++ Library/Formula/vilistextum.rb
@@ -3,6 +3,13 @@ class Vilistextum < Formula
   url "http://bhaak.net/vilistextum/vilistextum-2.6.9.tar.gz"
   sha1 "d62fe5213b61c0d0356bb2e60757dd535ac0a82b"
 
+  bottle do
+    cellar :any
+    sha1 "8d29715464dd1faf569cb74fb673401b0e507ecc" => :yosemite
+    sha1 "28f4fe3151aca197383f409486840106d307355b" => :mavericks
+    sha1 "c11e4458f504653f89b98e5058534282929cfa53" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--mandir=#{man}"
