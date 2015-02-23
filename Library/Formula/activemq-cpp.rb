338db8c3f028d830b23d21fd4281e67f5e2de12d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-23 15:15:41 +0000

    activemq-cpp: add 3.8.4 bottle.

--- Library/Formula/activemq-cpp.rb
+++ Library/Formula/activemq-cpp.rb
@@ -3,6 +3,14 @@ class ActivemqCpp < Formula
   url "http://www.apache.org/dyn/closer.cgi?path=activemq/activemq-cpp/3.8.4/activemq-cpp-library-3.8.4-src.tar.bz2"
   sha1 "7c0c79833f1647df3905948f3b8f2592bc7a8994"
 
+  bottle do
+    cellar :any
+    revision 1
+    sha1 "0a9eb28a54a39e34c7acdbf2223bc3e4321b6e0e" => :yosemite
+    sha1 "4274dcd894a783d0b59f197feece8e5b1c844575" => :mavericks
+    sha1 "4a4fa0eaae9b13720bf78ee95a056ab7165fc07f" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "openssl"
 
