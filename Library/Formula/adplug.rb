6b1a7d0a3eb40cd430ff047ac2c8636d36ea3c89
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-23 12:27:17 +0100

    adplug: test added
    Closes #37105.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/adplug.rb
+++ Library/Formula/adplug.rb
@@ -1,16 +1,27 @@
-require 'formula'
-
 class Adplug < Formula
-  homepage 'http://adplug.sf.net'
-  url 'https://downloads.sourceforge.net/project/adplug/AdPlug%20core%20library/2.2.1/adplug-2.2.1.tar.bz2'
-  sha1 '5023282ff44f183c9b6d7da9f57f51595742a5f7'
+  homepage "http://adplug.sf.net"
+  url "https://downloads.sourceforge.net/project/adplug/AdPlug%20core%20library/2.2.1/adplug-2.2.1.tar.bz2"
+  sha1 "5023282ff44f183c9b6d7da9f57f51595742a5f7"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libbinio'
+  depends_on "pkg-config" => :build
+  depends_on "libbinio"
+
+  resource "ksms" do
+    url "http://advsys.net/ken/ksmsongs.zip"
+    sha1 "7643cf95ae17233898b2b092ea9c8a69ec52532b"
+  end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    resource("ksms").stage do
+      mkdir "#{testpath}/.adplug"
+      system "#{bin}/adplugdb", "-v", "add", "JAZZSONG.KSM"
+    end
   end
 end
