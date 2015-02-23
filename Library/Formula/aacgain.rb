673a700d139dc3adf56cd2185dc8dc01042a448f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-23 08:39:35 +0000

    aacgain: add 1.8 bottle.

--- Library/Formula/aacgain.rb
+++ Library/Formula/aacgain.rb
@@ -7,6 +7,13 @@ class Aacgain < Formula
   url "http://aacgain.altosdesign.com/alvarez/aacgain-1.8.tar.bz2"
   sha1 "331039c4231e4d85ae878795ce3095dd96dcbfdb"
 
+  bottle do
+    cellar :any
+    sha1 "a90e244d0c89d787d1687ef2adb482624f6d2bb8" => :yosemite
+    sha1 "0a5c772e3ca281678d468dec7757df7517f9ae1e" => :mavericks
+    sha1 "98f1d3415005700417fc8116d0428a0df975b2e4" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
