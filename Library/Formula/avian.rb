983b94a679bf4433a1f4b329e33dc65ed7ab787b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-24 07:31:54 +0000

    avian: add 1.1 bottle.

--- Library/Formula/avian.rb
+++ Library/Formula/avian.rb
@@ -4,6 +4,13 @@ class Avian < Formula
   url "https://github.com/ReadyTalk/avian/archive/v1.1.tar.gz"
   sha1 "de51fb048b0b81a131ddbb3387adb229d3eddf2f"
 
+  bottle do
+    cellar :any
+    sha1 "ec4bf9872744d3afd83131dd585b525fce19f898" => :yosemite
+    sha1 "3c8e3d20400aad2f5c5dc60f6b6b4ae3094eef6b" => :mavericks
+    sha1 "dfb49aaec4a22570127f72f503712aa4475e1742" => :mountain_lion
+  end
+
   depends_on :macos => :lion
   depends_on :java
 
