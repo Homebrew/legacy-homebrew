86ed188293b4e3ba35ee60e79bd81aadb12a07e3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-24 21:54:20 +0000

    openslide: add 3.4.0 bottle.

--- Library/Formula/openslide.rb
+++ Library/Formula/openslide.rb
@@ -5,6 +5,13 @@ class Openslide < Formula
   url 'https://github.com/openslide/openslide/releases/download/v3.4.0/openslide-3.4.0.tar.gz'
   sha1 'edd28a142f7801a354949c05e7f1b7dcb246db6b'
 
+  bottle do
+    cellar :any
+    sha1 "f03b2209cd35a2b548b012210ea2e439c80b44d7" => :yosemite
+    sha1 "5327cde1c873328b6b90f48a8400b569436323de" => :mavericks
+    sha1 "ef49c02614e0ec8c2a0e5f71a6de309023a77a0c" => :mountain_lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libpng'
   depends_on 'jpeg'
