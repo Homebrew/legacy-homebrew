dd1656ec80ecc25d8536bbba4b616a6f40399c61
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-02-25 14:24:17 +0100

    ec2-api-tools: depend on java

--- Library/Formula/ec2-api-tools.rb
+++ Library/Formula/ec2-api-tools.rb
@@ -3,12 +3,13 @@ class Ec2ApiTools < AmazonWebServicesFormula
   url "https://ec2-downloads.s3.amazonaws.com/ec2-api-tools-1.7.3.0.zip"
   sha1 "c2a77b6c3b9fe05fdf4a126d03f8cd07791aaa50"
 
+  depends_on :java
+
   def caveats
     standard_instructions "EC2_HOME"
   end
 
   test do
-    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
     ENV["EC2_HOME"] = libexec
     assert_match version.to_s, shell_output("#{bin}/ec2-version")
   end
