05d7dc12435c6f37cd083b9a5bd0f86e73c1beac
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-25 15:08:12 +0000

    ecl: add 15.2.21 bottle.

--- Library/Formula/ecl.rb
+++ Library/Formula/ecl.rb
@@ -5,6 +5,12 @@ class Ecl < Formula
   url 'https://downloads.sourceforge.net/project/ecls/ecls/15.2/ecl-15.2.21.tgz'
   sha1 'fcf70c11ddb1602e88972f0c1596037df229f473'
 
+  bottle do
+    sha1 "1f5441b4a7cd4f62be787b771060655ec92b0642" => :yosemite
+    sha1 "c042f476840561458d016a980b45fbd2547f98b4" => :mavericks
+    sha1 "ab1ac89fe3e324fc0cf4f74acd058af6a22b235a" => :mountain_lion
+  end
+
   depends_on 'gmp'
 
   def install
