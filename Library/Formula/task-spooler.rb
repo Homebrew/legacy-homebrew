0e1f50fe2a6140921963a0fd8eb8961dd2b8f3a5
David Boone <dave@awesense.com>
2015-02-25 02:07:01 -0800

    Fix error where server fails on strdup call.

--- Library/Formula/task-spooler.rb
+++ Library/Formula/task-spooler.rb
@@ -5,6 +5,8 @@ class TaskSpooler < Formula
   url 'http://vicerveza.homeunix.net/~viric/soft/ts/ts-0.7.4.tar.gz'
   sha1 '92813a3b0eedfe1d4a177727122e6d08695f6bc8'
 
+  patch :DATA
+
   conflicts_with 'moreutils',
     :because => "both install a 'ts' executable."
 
@@ -12,3 +14,17 @@ class TaskSpooler < Formula
     system "make", "install", "PREFIX=#{prefix}"
   end
 end
+__END__
+diff --git a/server.c b/server.c
+index a58ad87..aec0c70 100644
+--- a/server.c
++++ b/server.c
+@@ -179,7 +179,8 @@ void server_main(int notify_fd, char *_path)
+     path = _path;
+
+     /* Move the server to the socket directory */
+-    dirpath = strdup(path);
++    dirpath = malloc(strlen(path)+1);
++    strcpy(dirpath, path);
+     chdir(dirname(dirpath));
+     free(dirpath);
