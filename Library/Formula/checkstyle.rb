49fe9459be5d2b106579efb8b1f69db5ea27fa5d
Josh Hagins <hagins.josh@gmail.com>
2015-02-25 15:57:07 -0500

    Upgrade checkstyle to 6.3

--- Library/Formula/checkstyle.rb
+++ Library/Formula/checkstyle.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Checkstyle < Formula
   homepage "http://checkstyle.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/checkstyle/checkstyle/6.2/checkstyle-6.2-bin.tar.gz"
-  sha1 "5b92798c55cbc8cf87f1435a2fc64e04d50a6648"
+  url "https://downloads.sourceforge.net/project/checkstyle/checkstyle/6.3/checkstyle-6.3-bin.tar.gz"
+  sha1 "13cc70785a4208f854677726ede5518997122224"
 
   def install
     libexec.install "checkstyle-#{version}-all.jar"
