dfff1d90456e8c3df4b9791dbfedda2b5b1c428a
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-02-25 14:25:02 +0100

    ec2-ami-tools: depend on java

--- Library/Formula/ec2-ami-tools.rb
+++ Library/Formula/ec2-ami-tools.rb
@@ -3,12 +3,13 @@ class Ec2AmiTools < AmazonWebServicesFormula
   url "https://ec2-downloads.s3.amazonaws.com/ec2-ami-tools-1.5.6.zip"
   sha1 "cb1a0e807e6e4e473a5a462633cb9990ec851093"
 
+  depends_on :java
+
   def caveats
     standard_instructions "EC2_AMITOOL_HOME"
   end
 
   test do
-    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
     ENV["EC2_AMITOOL_HOME"] = libexec
     assert_match version.to_s, shell_output("#{bin}/ec2-ami-tools-version")
   end
