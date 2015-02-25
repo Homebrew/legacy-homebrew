41583e6777a8af7deb2ba8884c840790dcf7a5b6
Josh Tilles <jtilles@sailthru.com>
2015-02-25 17:30:41 -0500

    git-fixup 1.0.1
    Closes #37202.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/git-fixup.rb
+++ Library/Formula/git-fixup.rb
@@ -1,7 +1,7 @@
 class GitFixup < Formula
   homepage "https://github.com/keis/git-fixup"
-  url "https://github.com/keis/git-fixup/archive/v1.0.0.tar.gz"
-  sha1 "836613c7b9d1ccafaa5f5250b6ff2e125fabf974"
+  url "https://github.com/keis/git-fixup/archive/v1.0.1.tar.gz"
+  sha1 "0c2f2dd1f6543e537291c0f9e0e06905725b0ccc"
 
   head "https://github.com/keis/git-fixup.git", :branch => "master"
 
@@ -13,7 +13,7 @@ class GitFixup < Formula
   end
 
   def install
-    system "make", "prefix=#{prefix}", "install"
+    system "make", "PREFIX=#{prefix}", "install"
     zsh_completion.install "completion.zsh" => "_git-fixup"
   end
 
