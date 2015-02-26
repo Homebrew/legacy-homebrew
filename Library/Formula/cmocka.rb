91e5b1397ee343054c57ce8398b72801ea931186
Akito Tabira <mythosil@yahoo.co.jp>
2015-02-27 01:47:29 +0900

    cmocka 1.0.0
    Closes #37219.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/cmocka.rb
+++ Library/Formula/cmocka.rb
@@ -1,17 +1,37 @@
-require 'formula'
-
 class Cmocka < Formula
-  homepage 'http://cmocka.cryptomilk.org/'
-  url 'https://open.cryptomilk.org/attachments/download/42/cmocka-0.4.1.tar.xz'
-  sha1 'ef8b64878edc5f0e442dc6aaf3dc83ef912c51c2'
+  homepage "https://cmocka.org/"
+  url "https://cmocka.org/files/1.0/cmocka-1.0.0.tar.xz"
+  sha1 "c1b6bdab164b2d54c26edacef9b9308e4de154f9"
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   def install
-    mkdir 'build' do
+    mkdir "build" do
       system "cmake", "..", "-DUNIT_TESTING=On", *std_cmake_args
       system "make"
-      system "make install"
+      system "make", "install"
     end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdarg.h>
+      #include <stddef.h>
+      #include <setjmp.h>
+      #include <cmocka.h>
+
+      static void null_test_success(void **state) {
+        (void) state; /* unused */
+      }
+
+      int main(void) {
+        const struct CMUnitTest tests[] = {
+            cmocka_unit_test(null_test_success),
+        };
+        return cmocka_run_group_tests(tests, NULL, NULL);
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lcmocka", "-o", "test"
+    system "./test"
   end
 end
