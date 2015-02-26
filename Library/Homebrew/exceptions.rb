b8a84c7e0d54452ba4b91855a178a0e3c3b0a039
Mike McQuaid <mike@mikemcquaid.com>
2015-02-26 19:17:12 +0000

    exceptions: instruct not to file boneyard issues.

--- Library/Homebrew/exceptions.rb
+++ Library/Homebrew/exceptions.rb
@@ -153,8 +153,14 @@ class BuildError < RuntimeError
       puts
       puts "#{Tty.red}READ THIS#{Tty.reset}: #{Tty.em}#{OS::ISSUES_URL}#{Tty.reset}"
       if formula.tap?
-        puts "If reporting this issue please do so at (not Homebrew/homebrew):"
-        puts "  https://github.com/#{formula.tap}/issues"
+        case formula.tap
+        when "homebrew/homebrew-boneyard"
+          puts "#{formula} was moved to homebrew-boneyard because it has unfixable issues."
+          puts "Please do not file any issues about this. Sorry!"
+        else
+          puts "If reporting this issue please do so at (not Homebrew/homebrew):"
+          puts "  https://github.com/#{formula.tap}/issues"
+        end
       end
     else
       require 'cmd/config'
