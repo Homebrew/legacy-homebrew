3a53be6ccae2a8f0519029413cb99276bacc85cc
Ralph Samuel <theralphsamuel@gmail.com>
2015-02-27 01:33:50 -0800

    Fixes --with-svn flag for configure.sh call
    makes it non-prefix-subjective

--- Library/Formula/mesos.rb
+++ Library/Formula/mesos.rb
@@ -23,8 +23,7 @@ class Mesos < Formula
             "--disable-debug",
             "--disable-dependency-tracking",
             "--disable-silent-rules",
-            "--with-svn=/usr/local/Cellar/subversion/1.8.11",
-            "--with-apr=/usr/local/Cellar/apr/1.5.1"
+            "--with-svn=#{Formula["subversion"].opt_prefix}"
            ]
 
     ENV.cxx11
