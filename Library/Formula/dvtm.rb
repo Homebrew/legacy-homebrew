d06676d72de0e02ed9dfe4308970cc5c8cec2efc
Colin King <richard.colin.king@gmail.com>
2015-02-26 22:38:41 -0600

    dvtm 0.14
    Closes #37235.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/dvtm.rb
+++ Library/Formula/dvtm.rb
@@ -1,9 +1,7 @@
-# encoding: UTF-8
-
 class Dvtm < Formula
   homepage "http://www.brain-dump.org/projects/dvtm/"
-  url "http://www.brain-dump.org/projects/dvtm/dvtm-0.13.tar.gz"
-  sha1 "8a4fc2440faa3050244e5a492fb6766899e0c0d7"
+  url "http://www.brain-dump.org/projects/dvtm/dvtm-0.14.tar.gz"
+  sha1 "205a2165e70455309f7ed6a6f11b3072fb9b13c3"
   head "git://repo.or.cz/dvtm.git"
 
   bottle do
@@ -21,6 +19,6 @@ class Dvtm < Formula
   test do
     result = shell_output("#{bin}/dvtm -v")
     result.force_encoding("UTF-8") if result.respond_to?(:force_encoding)
-    assert_match /^dvtm-[0-9.]+ © 2007-\d{4} Marc André Tanner$/, result
+    assert_match /^dvtm-#{version}/, result
   end
 end
