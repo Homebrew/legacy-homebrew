6b46a0da8e42b79f7b60d149a85c77520e637a79
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-27 22:45:44 +0000

    sha2: add 1.0.1 bottle.

--- Library/Formula/sha2.rb
+++ Library/Formula/sha2.rb
@@ -3,6 +3,13 @@ class Sha2 < Formula
   url "http://www.aarongifford.com/computers/sha2-1.0.1.tgz"
   sha1 "ffbc69e0b0ab47f70e61aeb14fa16ba2b74dc728"
 
+  bottle do
+    cellar :any
+    sha1 "df93015804de4b208f7eb7c3e6ea390aa4839da0" => :yosemite
+    sha1 "6a70d9dab98a22b03aa83b735746c356fcfd4ced" => :mavericks
+    sha1 "23658e75f2a245c7fbcb63d792db85a5b8c9d5c0" => :mountain_lion
+  end
+
   option "without-check", "Skip compile-time tests"
 
   def install
