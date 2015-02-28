6bbbaf13acb7649ed4b15bd343283b2a408c10b0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-28 21:24:46 +0000

    pdfgrep: add 1.3.1 bottle.

--- Library/Formula/pdfgrep.rb
+++ Library/Formula/pdfgrep.rb
@@ -5,6 +5,13 @@ class Pdfgrep < Formula
 
   head "https://git.gitorious.org/pdfgrep/pdfgrep.git"
 
+  bottle do
+    cellar :any
+    sha1 "c97fb86fb4f8bb91ce5ced1a0b5a26ae25157ea9" => :yosemite
+    sha1 "c562a14dd41e33ec105f4f0735c0b7f0fc6e96b6" => :mavericks
+    sha1 "cfc5c5ddc203b615ce93ecbb70e1e103b29d8591" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "poppler"
 
