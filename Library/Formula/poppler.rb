ae9d0a95db5914e1d62b8c3556692c26f11ec7d8
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-28 23:19:59 +0100

    poppler 0.34.0

--- Library/Formula/poppler.rb
+++ Library/Formula/poppler.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Poppler < Formula
   desc "PDF rendering library (based on the xpdf-3.0 code base)"
-  homepage 'http://poppler.freedesktop.org'
-  url 'http://poppler.freedesktop.org/poppler-0.29.0.tar.xz'
-  sha1 'ba3330ab884e6a139ca63dd84d0c1c676f545b5e'
+  homepage "http://poppler.freedesktop.org"
+  url "http://poppler.freedesktop.org/poppler-0.34.0.tar.xz"
+  sha256 "1ba4ba9a2f9eb1e62ee6d736f4d82be4fc5f6dd177dc2b03febbe2ef2e515cb0"
 
   bottle do
     sha1 "b83e3b7fe032d69343367ceb481a0387e447e565" => :yosemite
@@ -19,27 +17,27 @@ class Poppler < Formula
   deprecated_option "with-qt4" => "with-qt"
   deprecated_option "with-lcms2" => "with-little-cms2"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'cairo'
-  depends_on 'fontconfig'
-  depends_on 'freetype'
-  depends_on 'gettext'
-  depends_on 'glib'
-  depends_on 'gobject-introspection'
-  depends_on 'jpeg'
-  depends_on 'libpng'
-  depends_on 'libtiff'
-  depends_on 'openjpeg'
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "openjpeg"
 
   depends_on "qt" => :optional
   depends_on "qt5" => :optional
   depends_on "little-cms2" => :optional
 
-  conflicts_with 'pdftohtml', :because => 'both install `pdftohtml` binaries'
+  conflicts_with "pdftohtml", :because => "both install `pdftohtml` binaries"
 
-  resource 'font-data' do
-    url 'http://poppler.freedesktop.org/poppler-data-0.4.7.tar.gz'
-    sha1 '556a5bebd0eb743e0d91819ba11fd79947d8c674'
+  resource "font-data" do
+    url "http://poppler.freedesktop.org/poppler-data-0.4.7.tar.gz"
+    sha256 "e752b0d88a7aba54574152143e7bf76436a7ef51977c55d6bd9a48dccde3a7de"
   end
 
   def install
@@ -63,7 +61,13 @@ class Poppler < Formula
     args << "--enable-cms=lcms2" if build.with? "little-cms2"
 
     system "./configure", *args
-    system "make install"
-    resource('font-data').stage { system "make", "install", "prefix=#{prefix}" }
+    system "make", "install"
+    resource("font-data").stage do
+      system "make", "install", "prefix=#{prefix}"
+    end
+  end
+
+  test do
+    system "#{bin}/pdfinfo", test_fixtures("test.pdf")
   end
 end
