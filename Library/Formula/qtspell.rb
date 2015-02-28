d5ea37423c06099ae3604d67f880bcacb4b6145e
Gerrit Beine <gerrit.beine@gmx.de>
2015-02-28 11:40:23 +0100

    Introducing qtspell
    Spell checking for Qt text widgets

---
+++ Library/Formula/qtspell.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Qtspell < Formula
+  homepage 'https://github.com/manisandro/qtspell'
+  url 'https://github.com/manisandro/qtspell/releases/download/0.7.1/qtspell-0.7.1.tar.xz'
+  sha1 '7e959b8147192818fa56824bf39d4384b0242032'
+
+  depends_on 'cmake' => :build
+  depends_on 'qt'
+  depends_on 'enchant'
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+end
