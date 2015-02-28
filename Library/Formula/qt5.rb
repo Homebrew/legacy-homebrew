a04cf42b78b3f36459778c25ba4af05165aaaf3a
luzpaz <luzpaz@users.noreply.github.com>
2015-02-28 06:48:20 -0600

    Qt5.4.1

--- Library/Formula/qt5.rb
+++ Library/Formula/qt5.rb
@@ -14,7 +14,7 @@ class Qt5 < Formula
   url "https://download.qt.io/official_releases/qt/5.4/5.4.1/single/qt-everywhere-opensource-src-5.4.1.tar.xz"
   mirror "http://qtmirror.ics.com/pub/qtproject/official_releases/qt/5.4/5.4.1/single/qt-everywhere-opensource-src-5.4.1.tar.xz"
   sha1 "daa3373af8d6656a1066ff23bc9100b5ca004ecf"
-  
+
 bottle do
   sha1 "072ed2c806664fd1da3ba7c90c8e4887509fb91b" => :yosemite
   sha1 "1ca730d96a962a5c4fcbd605542b7bfb528d6c58" => :mavericks
