1b16fd6428772d8840a49203f5797bcb14a47a1c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-28 17:24:49 +0000

    purescript: add 0.6.8 bottle.

--- Library/Formula/purescript.rb
+++ Library/Formula/purescript.rb
@@ -7,6 +7,12 @@ class Purescript < Formula
   url "http://hackage.haskell.org/package/purescript-0.6.8/purescript-0.6.8.tar.gz"
   sha1 "70fd4d3109d61c34c8898a30d222c4b1ad8fd7a5"
 
+  bottle do
+    sha1 "b5628dbaabd07215c54979156b2d2f66fb6034c0" => :yosemite
+    sha1 "0d082d33a31bae337188e0866180120a8b38c66d" => :mavericks
+    sha1 "e9bbad2add5f0961926a0df8adbd8a3848781747" => :mountain_lion
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc"
 
