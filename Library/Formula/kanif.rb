5b54e6c72f7f9dc509a27f447ae1035f2eb171b2
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-28 23:09:11 +0100

    kanif: test added

--- Library/Formula/kanif.rb
+++ Library/Formula/kanif.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Kanif < Formula
   homepage "http://taktuk.gforge.inria.fr/kanif/"
   url "http://gforge.inria.fr/frs/download.php/26773/kanif-1.2.2.tar.gz"
   sha1 "7a10fe0e74159875f004b6c4a12a0202ff092ce9"
 
-  depends_on "taktuk"
+  depends_on "taktuk" => :run
 
   def install
     system "./configure", "--disable-debug",
@@ -13,5 +11,10 @@ class Kanif < Formula
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    assert_match "taktuk -s -c 'ssh' -l brew",
+      shell_output("#{bin}/kash -q -l brew -r ssh")
   end
 end
