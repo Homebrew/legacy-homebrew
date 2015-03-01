7b6cbe81d55eaf421bbee67bca9358514f8efed7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-01 05:09:40 +0000

    cmocka: add 1.0.0 bottle.

--- Library/Formula/cmocka.rb
+++ Library/Formula/cmocka.rb
@@ -3,6 +3,12 @@ class Cmocka < Formula
   url "https://cmocka.org/files/1.0/cmocka-1.0.0.tar.xz"
   sha1 "c1b6bdab164b2d54c26edacef9b9308e4de154f9"
 
+  bottle do
+    sha1 "7ce09408d9d5d7b08c19dd7d483058c9fd6bd9fa" => :yosemite
+    sha1 "d9f4f45dd1338eaf3b19d42fc3b5b88f538c997d" => :mavericks
+    sha1 "4e365b5ba01862722e3d91e1c1cdc8d34c6e3e34" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
