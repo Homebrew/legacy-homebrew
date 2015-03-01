31ac409b4652161ebc916904d0ac49cf70b40794
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-01 01:43:12 +0100

    diff-pdf: bump the revision to use latest poppler

--- Library/Formula/diff-pdf.rb
+++ Library/Formula/diff-pdf.rb
@@ -1,10 +1,9 @@
-require "formula"
-
 class DiffPdf < Formula
   desc "Visually compare two PDF files"
-  homepage "http://vslavik.github.io/diff-pdf/"
+  homepage "https://vslavik.github.io/diff-pdf/"
   url "https://github.com/vslavik/diff-pdf/archive/v0.2.tar.gz"
-  sha1 "308ea8e92ac609ca88303dce6a6e8403c6b9f11f"
+  sha256 "cb90f2e0fd4bc3fe235111f982bc20455a1d6bc13f4219babcba6bd60c1fe466"
+  revision 1
 
   depends_on "pkg-config" => :build
   depends_on "automake" => :build
