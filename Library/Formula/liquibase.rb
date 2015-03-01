3c2eec079f63df17f9dda516518d4835647a2428
Matthias Nuessler <m.nuessler@web.de>
2015-03-01 15:50:28 +0100

    liquibase 3.3.2
    Closes #37292.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/liquibase.rb
+++ Library/Formula/liquibase.rb
@@ -1,7 +1,7 @@
 class Liquibase < Formula
   homepage "http://liquibase.org"
-  url "https://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.3.1-bin.tar.gz"
-  sha1 "4176e2f856ad23f6848a7874a0e855c9f9a00f0e"
+  url "https://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.3.2-bin.tar.gz"
+  sha1 "89ddda7d5ca8d38947bfee0d4aa58534d943b990"
 
   def install
     rm_f Dir["*.bat"]
