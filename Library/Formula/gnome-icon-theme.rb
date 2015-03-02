4723ad606615568ac9e72221a9f25cc8247dcc2a
Alexander Lent <git@xanderlent.com>
2015-03-01 17:18:34 -0800

    Update gnome-icon-theme to 3.14.1 and alias adwaita-icon-theme
    In GNOME 3.14, gnome-icon-theme and gnome-icon-theme-symbolic were merged into
    adwaita-icon-theme, because the symbolic icons are no longer considered
    optional.[1]
    
    Moving to the latest stable version of this package fixes the lack of symbolic
    icons for GTK+3 in Homebrew.
    
    The tests I've written are simple sanity checks that ensure certain files were
    created in the install process.
    Someone with GTK+3 knowledge could probably write better ones that check if GTK+
    properly detects the icon files.
    
    [1]: https://mail.gnome.org/archives/desktop-devel-list/2014-April/msg00086.html

--- Library/Formula/gnome-icon-theme.rb
+++ Library/Formula/gnome-icon-theme.rb
@@ -1,10 +1,7 @@
-require "formula"
-
 class GnomeIconTheme < Formula
   homepage "https://developer.gnome.org"
-  url "http://ftp.gnome.org/pub/GNOME/sources/gnome-icon-theme/3.12/gnome-icon-theme-3.12.0.tar.xz"
-  sha1 "cc0f0dc55db3c4ca7f2f34564402f712807f1342"
-  revision 1
+  url "http://ftp.gnome.org/pub/GNOME/sources/adwaita-icon-theme/3.14/adwaita-icon-theme-3.14.1.tar.xz"
+  sha1 "e1d603d9cc4e4b7f83f749ba20934832d4321dd2"
 
   bottle do
     cellar :any
@@ -22,8 +19,23 @@ class GnomeIconTheme < Formula
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--enable-icon-mapping",
                           "GTK_UPDATE_ICON_CACHE=#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache"
     system "make", "install"
+  end
+
+  test do
+    # This checks that a -symbolic png file generated from svg exists
+    # and that a file created late in the install process exists.
+    # Someone who understands GTK+3 could probably write better tests that
+    # check if GTK+3 can find the icons.
+    (testpath/"test.sh").write <<-EOS.undent
+    #!/usr/bin/env bash
+    if [ -r "#{share}/icons/Adwaita/96x96/status/weather-storm-symbolic.symbolic.png" ] && [ -r "#{share}/icons/Adwaita/index.theme" ]; then
+        exit 0;
+    else
+        exit 1;
+    fi
+    EOS
+    system "bash", "./test.sh"
   end
 end
