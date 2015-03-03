dc50d2b209075e9a21bc5f516b469077eb7018ae
Tim D. Smith <git@tim-smith.us>
2015-03-02 21:07:51 -0800

    nu: disable gc to fix build
    Applies upstream commit to disable obsolete garbage collection option.
    
    Fixes #37341.

--- Library/Formula/nu.rb
+++ Library/Formula/nu.rb
@@ -18,6 +18,14 @@ class Nu < Formula
     cause 'nu only builds with clang'
   end
 
+  # remove deprecated -fobjc-gc
+  # https://github.com/timburks/nu/pull/74
+  # https://github.com/Homebrew/homebrew/issues/37341
+  patch do
+    url "https://github.com/timburks/nu/commit/c0b05f1.diff"
+    sha256 "f6c1a66e470e7132ba11937c971f9b90824bb03eaa030b3e70004f9d2725c636"
+  end
+
   def install
     ENV['PREFIX'] = prefix
 
