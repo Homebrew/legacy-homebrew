fc6e20d6bbc0fa1d3f03163f543c4f0b59a3e372
Bastian Müller <bastian@turbolent.com>
2015-03-03 16:49:20 +0100

    HEAD option for sphinxbase and pocketsphinx

--- Library/Formula/cmu-pocketsphinx.rb
+++ Library/Formula/cmu-pocketsphinx.rb
@@ -4,13 +4,21 @@ class CmuPocketsphinx < Formula
   homepage 'http://cmusphinx.sourceforge.net/'
   url 'https://downloads.sourceforge.net/project/cmusphinx/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz'
   sha1 'd9efdd0baddd2e47c2ba559caaca62ffa0c0eede'
+  head "https://github.com/cmusphinx/pocketsphinx.git"
 
   depends_on 'pkg-config' => :build
+  if build.head?
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "swig" => :build
+  end
   depends_on 'cmu-sphinxbase'
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system (if build.head? then "./autogen.sh" else "./configure" end),
+      "--disable-dependency-tracking",
+      "--prefix=#{prefix}"
     system "make install"
   end
 end
--- Library/Formula/cmu-sphinxbase.rb
+++ Library/Formula/cmu-sphinxbase.rb
@@ -4,17 +4,25 @@ class CmuSphinxbase < Formula
   homepage "http://cmusphinx.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/cmusphinx/sphinxbase/0.8/sphinxbase-0.8.tar.gz"
   sha1 "c0c4d52e143d07cd593bd6bcaeb92b9a8a5a8c8e"
+  head "https://github.com/cmusphinx/sphinxbase.git"
 
   depends_on "pkg-config" => :build
+  if build.head?
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "swig" => :build
+  end
   # If these are found, they will be linked against and there is no configure
   # switch to turn them off.
   depends_on "libsndfile"
   depends_on "libsamplerate" => "with-libsndfile"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system (if build.head? then "./autogen.sh" else "./configure" end),
+       "--disable-debug",
+       "--disable-dependency-tracking",
+       "--prefix=#{prefix}"
     system "make install"
   end
 end
