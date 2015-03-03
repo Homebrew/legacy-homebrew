a5f5e5fd6541df8a3b5a5e0c2393a65f700d6e39
Xu Cheng <xucheng@me.com>
2015-03-03 19:47:56 +0800

    denominator: drop ScriptFileFormula

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
-class Denominator < ScriptFileFormula
+class Denominator < Formula
   # This is a shell script wrapping a java jar; download as single file using nounzip
-  homepage 'https://github.com/Netflix/denominator/tree/master/cli'
-  url 'http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/4.3.3/denominator?direct',
+  homepage "https://github.com/Netflix/denominator/tree/master/cli"
+  url "http://dl.bintray.com/content/netflixoss/denominator/denominator-cli/release/4.3.3/denominator?direct",
     :using  => :nounzip
-  sha1 'df078fe7e1c3739ea17dfeced936515026890c1e'
+  sha1 "df078fe7e1c3739ea17dfeced936515026890c1e"
+
+  def install
+    bin.install "denominator"
+  end
 
   test do
-    system "#{bin}/denominator", "help"
+    system "#{bin}/denominator", "providers"
   end
 end
