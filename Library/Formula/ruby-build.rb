64514c215d0001d3819bdbcd3f29c7bf191dffae
Rustam Zagirov <rustam@zagirov.name>
2015-03-03 11:23:40 +0300

    ruby-build 20150303
    Closes #37353.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,8 +3,8 @@ require "formula"
 class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  url "https://github.com/sstephenson/ruby-build/archive/v20150226.tar.gz"
-  sha1 "a366f03b8233d4b6582bd5efadf90bafeb4a35a8"
+  url "https://github.com/sstephenson/ruby-build/archive/v20150303.tar.gz"
+  sha1 "c15c1f6613d6c518544ed0d90a0cf33eefc7d73f"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
