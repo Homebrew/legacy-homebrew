496a0f281d37a1df763de957949aaa652eaaac0e
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-03 19:25:24 +0100

    plzip 1.3
    Closes #37372.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/plzip.rb
+++ Library/Formula/plzip.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Plzip < Formula
   homepage "http://www.nongnu.org/lzip/plzip.html"
-  url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.2.tar.gz"
-  sha1 "072f3253322ed36f96d452931780eea3dc7ec494"
+  url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.3.tar.gz"
+  sha1 "e339c06093d8e7905390cc7c39f28f6198a66471"
 
   bottle do
     cellar :any
@@ -12,13 +10,26 @@ class Plzip < Formula
     sha1 "c9826ce05d1b5055a3ba5d20fae5c7d1cd4a091b" => :lion
   end
 
+  devel do
+    url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.4-pre1.tar.gz"
+    sha1 "817b9d1635be6db35907733f7eedcd2b7642ccdd"
+    version "1.4-pre1"
+  end
+
   depends_on "lzlib"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "CXX=#{ENV.cxx}",
+    system "./configure", "--prefix=#{prefix}",
+                          "CXX=#{ENV.cxx}",
                           "CXXFLAGS=#{ENV.cflags}"
     system "make"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "-j1", "install"
+  end
+
+  test do
+    text = "Hello Homebrew!"
+    compressed = pipe_output("#{bin}/plzip -c", text)
+    assert_equal text, pipe_output("#{bin}/plzip -d", compressed)
   end
 end
