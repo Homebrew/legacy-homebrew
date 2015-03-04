0788a3cf4f789996b690b2506dfa1edfc11a9507
chdiza <chdiza@gmail.com>
2015-03-04 00:12:06 -0500

    w-calc 2.5
    Modernize too.
    
    Closes #37387.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/w-calc.rb
+++ Library/Formula/w-calc.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class WCalc < Formula
-  url 'https://downloads.sourceforge.net/w-calc/wcalc-2.4.1.tar.bz2'
-  homepage 'http://w-calc.sourceforge.net'
-  sha1 'e3ba04dcfc60a47b60c79fc6f9f4a8fa750ee5f9'
+  url "https://downloads.sourceforge.net/w-calc/wcalc-2.5.tar.bz2"
+  homepage "http://w-calc.sourceforge.net"
+  sha256 "0e2c17c20f935328dcdc6cb4c06250a6732f9ee78adf7a55c01133960d6d28ee"
 
-  depends_on 'gmp'
-  depends_on 'mpfr'
+  depends_on "gmp"
+  depends_on "mpfr"
 
   def install
     system "./configure", "--prefix=#{prefix}"
@@ -15,7 +13,6 @@ class WCalc < Formula
   end
 
   test do
-    system "#{bin}/wcalc 2+2"
+    system "#{bin}/wcalc", "2+2"
   end
-
 end
