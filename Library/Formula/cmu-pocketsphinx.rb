1214d4adb596b5446ba7fa0dc4d614d903ca94f0
Bastian Müller <bastian@turbolent.com>
2015-03-04 08:34:45 +0100

    pocketsphinx: use head block and NOCONFIGURE

--- Library/Formula/cmu-pocketsphinx.rb
+++ Library/Formula/cmu-pocketsphinx.rb
@@ -4,21 +4,26 @@ class CmuPocketsphinx < Formula
   homepage 'http://cmusphinx.sourceforge.net/'
   url 'https://downloads.sourceforge.net/project/cmusphinx/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz'
   sha1 'd9efdd0baddd2e47c2ba559caaca62ffa0c0eede'
-  head "https://github.com/cmusphinx/pocketsphinx.git"
 
-  depends_on 'pkg-config' => :build
-  if build.head?
+  head do
+    url "https://github.com/cmusphinx/pocketsphinx.git"
+
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
     depends_on "swig" => :build
   end
+
+  depends_on 'pkg-config' => :build
   depends_on 'cmu-sphinxbase'
 
   def install
-    system (if build.head? then "./autogen.sh" else "./configure" end),
-      "--disable-dependency-tracking",
-      "--prefix=#{prefix}"
+    if build.head? then
+      ENV["NOCONFIGURE"] = "yes"
+      system "./autogen.sh"
+    end
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make install"
   end
 end
