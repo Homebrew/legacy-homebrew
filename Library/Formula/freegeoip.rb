26d17a8ef79513dbbf55c9b155ed2d5919aef091
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-03-05 16:00:37 +0100

    source version

--- Library/Formula/freegeoip.rb
+++ Library/Formula/freegeoip.rb
@@ -1,16 +1,37 @@
 class Freegeoip < Formula
   homepage "https://github.com/fiorix/freegeoip/"
-  url "https://github.com/fiorix/freegeoip/releases/download/v3.0.4/freegeoip-3.0.4-darwin-amd64.tar.gz"
-  sha1 "b13b57ceb6f49be28459fc8c8fd04de78985a8df"
+  url "https://github.com/fiorix/freegeoip/archive/v3.0.4.tar.gz"
+  sha1 "6be48a6cd41b699f887e3283c2f209349b7fa45f"
 
   depends_on "go" => :build
 
   def install
+    (buildpath + "src/github.com/fiorix/freegeoip").install Dir[buildpath/"*"]
+
+    cd "src/github.com/fiorix/freegeoip" do
+      ENV["GOPATH"] = buildpath
+      system "go", "get", "github.com/fiorix/go-redis/redis"
+      system "go", "get", "github.com/fiorix/go-web/httpxtra"
+      system "go", "get", "github.com/gorilla/context"
+
+      system "go", "get", "github.com/howeyc/fsnotify"
+      system "go", "get", "github.com/oschwald/maxminddb-golang"
+      system "go", "get", "github.com/robertkrimen/otto"
+
+      system "go", "build", "-o", "#{buildpath}/freegeoip", "github.com/fiorix/freegeoip/cmd/freegeoip"
+    end
+
     bin.install "freegeoip"
-    libexec.install "public"
+    libexec.install "#{buildpath}/src/github.com/fiorix/freegeoip/cmd/freegeoip/public"
   end
 
   test do
     assert_equal "freegeoip v3.0.4\n", shell_output("#{bin}/freegeoip --version")
+  end
+
+  def caveats; <<-EOS.undent
+    You can use public directory from #{libexec}/public starting with argument:
+      freegeoip -public=#{libexec}/public
+    EOS
   end
 end
