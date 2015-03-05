7e8b0d67fde8d7b21c5ecb36296afbe329b38dde
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-03-05 07:02:50 -0800

    add sed test

--- Library/Formula/c.rb
+++ Library/Formula/c.rb
@@ -3,15 +3,21 @@ class C < Formula
   url "https://github.com/ryanmjacobs/c/archive/v0.07.tar.gz"
   sha1 "c40044a2747b97a2f8b550c1d417bb03848fcd40"
 
-  depends_on "gnu-sed"
-
   def install
       bin.install "c"
   end
 
   test do
-    Dir.chdir("tests") do
-      system "test.sh"
-    end
+      system "c", "--help"
+
+      File.open("test.c", "rw") do |f|
+          f.write("#!/usr/bin/c blah blah blah")
+      end
+      system "sed". "-i.bak", "'1!b;s/^#!/\/\/#!/'", "test.c"
+      File.open("test.c", "r") do |f|
+          if f.readline != "//#!/usr/bin/c blah blah blah" then
+              abort "error: sed is not compatible!"
+          end
+      end
   end
 end
