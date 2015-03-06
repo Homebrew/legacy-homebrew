e296d51d6f7863ec5d6ee3a2a3c0a76dc4951f26
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-06 18:26:05 +0000

    nvm: add 0.24.0 bottle.

--- Library/Formula/nvm.rb
+++ Library/Formula/nvm.rb
@@ -4,6 +4,13 @@ class Nvm < Formula
   sha256 "f76800f248ebe611c8c2588553cc906d335baafbfd083ee6fdedee349a979c54"
   head "https://github.com/creationix/nvm.git"
 
+  bottle do
+    cellar :any
+    sha256 "efaf140cac680e83807172d177d09c53720e9253dc21aa4264b13921bb7d2267" => :yosemite
+    sha256 "d7de5b13f7be5f0e2e1900136ce85756cf29a01ed01d86ec879baa30b1fb623c" => :mavericks
+    sha256 "ccdca35f48abebdb064c3940df4c3d4f508fb4fe79c7184ab7c566ee93886873" => :mountain_lion
+  end
+
   def install
     prefix.install "nvm.sh"
     bash_completion.install "bash_completion" => "nvm"
