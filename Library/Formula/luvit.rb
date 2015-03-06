60a6fecb9b850c458106ef3b2bfc77141001ac01
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-06 22:53:43 +0000

    luvit: fix for system openssl linkage
    Various style fixes as well.

--- Library/Formula/luvit.rb
+++ Library/Formula/luvit.rb
@@ -1,20 +1,23 @@
-require 'formula'
-
 class Luvit < Formula
-  homepage 'http://luvit.io'
-  url 'http://luvit.io/dist/latest/luvit-0.8.2.tar.gz'
-  sha1 '0e53fa128eb48d429134435f15653c80b8c9485c'
+  homepage "https://luvit.io"
+  url "https://luvit.io/dist/latest/luvit-0.8.2.tar.gz"
+  sha256 "c2639348d1716c38ac3cd66ea4c4ff1c8a72f4610dbd6e50cf31426d3956c5ff"
+  head "https://github.com/luvit/luvit.git"
+  revision 1
 
-  head 'https://github.com/luvit/luvit.git'
-
-  depends_on 'pkg-config' => :build
-  depends_on 'luajit'
+  depends_on "pkg-config" => :build
+  depends_on "luajit"
+  depends_on "openssl"
 
   def install
-    ENV['USE_SYSTEM_SSL'] = '1'
-    ENV['USE_SYSTEM_LUAJIT'] = '1'
-    ENV['PREFIX'] = prefix
-    system 'make'
-    system 'make', 'install'
+    ENV["USE_SYSTEM_SSL"] = "1"
+    ENV["USE_SYSTEM_LUAJIT"] = "1"
+    ENV["PREFIX"] = prefix
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"luvit", "--cflags", "--libs"
   end
 end
