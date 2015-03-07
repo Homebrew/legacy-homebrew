2abd7e88118f0fbe21cafb8a29cfac86306b2c7d
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-07 14:35:13 +0100

    pipemeter: test added
    Closes #37481.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pipemeter.rb
+++ Library/Formula/pipemeter.rb
@@ -1,14 +1,22 @@
-require 'formula'
-
 class Pipemeter < Formula
-  homepage 'https://launchpad.net/pipemeter'
-  url 'https://launchpad.net/pipemeter/trunk/1.1.3/+download/pipemeter-1.1.3.tar.gz'
-  sha1 '8fe224b0cf77e9f3932f46fed0af5acc6547cd0c'
+  homepage "https://launchpad.net/pipemeter"
+  url "https://launchpad.net/pipemeter/trunk/1.1.3/+download/pipemeter-1.1.3.tar.gz"
+  sha256 "1ff952cb2127476ca9879f4b28fb92d6dabb0cc02db41f657025f7782fd50aaf"
 
   def install
-    system "./configure", "--disable-debug", "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make"
-    bin.install "pipemeter"
-    man1.install "pipemeter.1"
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+
+    # Fix the man1 directory location
+    inreplace "Makefile", "$(PREFIX)/man/man1", man1
+
+    bin.mkpath
+    man1.mkpath
+    system "make", "install"
+  end
+
+  test do
+    assert_match "3.00B", pipe_output("pipemeter -r 2>&1 >/dev/null", "foo", 0)
   end
 end
