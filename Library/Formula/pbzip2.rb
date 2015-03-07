322ec36d26a2d1ccc9b603790d59eda2d0e92453
David Christenson <david@the-david.com>
2015-03-07 00:33:22 -0700

    pbzip2 1.1.12
    Update to latest stable release, remove obsolete `inreplace`, modernize
    formula.

--- Library/Formula/pbzip2.rb
+++ Library/Formula/pbzip2.rb
@@ -1,17 +1,13 @@
-require 'formula'
-
 class Pbzip2 < Formula
-  homepage 'http://compression.ca/pbzip2/'
-  url 'http://compression.ca/pbzip2/pbzip2-1.1.8.tar.gz'
-  sha1 '6957483690f00c33ffeabbe0e9e6475098820cd5'
+  homepage "http://compression.ca/pbzip2/"
+  url "https://launchpad.net/pbzip2/1.1/1.1.12/+download/pbzip2-1.1.12.tar.gz"
+  sha256 "573bb358a5a7d3bf5f42f881af324cedf960c786e8d66dd03d448ddd8a0166ee"
 
   fails_with :llvm do
     build 2334
   end
 
   def install
-    inreplace "Makefile", "$(PREFIX)/man", "$(PREFIX)/share/man"
-
     system "make", "PREFIX=#{prefix}",
                    "CC=#{ENV.cxx}",
                    "CFLAGS=#{ENV.cflags}",
