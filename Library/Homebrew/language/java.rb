325f1adf2ad6c24b624c36b50455fbe152e6a46e
Xu Cheng <xucheng@me.com>
2015-03-07 12:33:49 +0800

    Fix java_home_env for Linuxbrew
    Closes #305.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Library/Homebrew/language/java.rb
+++ Library/Homebrew/language/java.rb
@@ -1,6 +1,7 @@
 module Language
   module Java
     def self.java_home_env(version=nil)
+      return {} unless OS.mac?
       version_flag = " --version #{version}" if version
       { :JAVA_HOME => "$(/usr/libexec/java_home#{version_flag})" }
     end
