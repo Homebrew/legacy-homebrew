1bd8a9bfe0b53ef139532c45754e2ec1b98059a5
Sebastian Stenzel <sebastianstenzel@gmail.com>
2015-03-07 13:18:35 +0100

    audit --strict

--- Library/Formula/clang-omp.rb
+++ Library/Formula/clang-omp.rb
@@ -1,26 +1,24 @@
-require "formula"
-
 class ClangOmp < Formula
   homepage "https://clang-omp.github.io/"
   url "https://github.com/clang-omp/llvm/archive/1013141148.tar.gz"
-  sha1 "84b67237aa3a5603143eb22e967b8b35f127bc4e"
+  sha256 "744e83339eca7494fe731c945fc12ab15a5e496de4b859f0d77301310c847a14"
 
   depends_on "libiomp"
   depends_on "cmake" => :build
 
   resource "compiler-rt" do
     url "https://github.com/clang-omp/compiler-rt/archive/1013141148.tar.gz"
-    sha1 "e85548d5b7d0f99d9699af7d17f2dbac1674b19d"
+    sha256 "ef289ca49c0282cb32053d56d319732ea1cce6b8fdd10814bcfbe4f8abd9bfea"
   end
 
   resource "clang" do
     url "https://github.com/clang-omp/clang/archive/1013141148.tar.gz"
-    sha1 "e066330a9fc699b9c539eeab4090c998b3f78214"
+    sha256 "a18fe403933f31471f11e6365c742a4f932dec42a55589c36b570c53cc21d358"
   end
 
   def install
-    resource("compiler-rt").stage { (buildpath/'projects/compiler-rt').install Dir['*'] }
-    resource("clang").stage { (buildpath/'tools/clang').install Dir['*'] }
+    resource("compiler-rt").stage { (buildpath/"projects/compiler-rt").install Dir["*"] }
+    resource("clang").stage { (buildpath/"tools/clang").install Dir["*"] }
 
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
@@ -44,13 +42,12 @@ class ClangOmp < Formula
     EOS
     (testpath/"test.c").write(testfile)
     system "#{bin}/clang-omp", "-liomp5", "-fopenmp", "-Werror", "-Wall", "test.c", "-o", "test"
-    system "./test > #{testpath}/testresult"
+    system "./test>#{testpath}/testresult"
 
-    testStdOut = (testpath/"testresult").read
-    assert_includes testStdOut, "Hello from thread 0, nthreads 4"
-    assert_includes testStdOut, "Hello from thread 1, nthreads 4"
-    assert_includes testStdOut, "Hello from thread 2, nthreads 4"
-    assert_includes testStdOut, "Hello from thread 3, nthreads 4"
+    test_std_out = (testpath/"testresult").read
+    assert_includes test_std_out, "Hello from thread 0, nthreads 4"
+    assert_includes test_std_out, "Hello from thread 1, nthreads 4"
+    assert_includes test_std_out, "Hello from thread 2, nthreads 4"
+    assert_includes test_std_out, "Hello from thread 3, nthreads 4"
   end
-
 end
