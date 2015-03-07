7334f622b3af284f02fe80a8b7e3f81a8790f6ed
David Christenson <david@the-david.com>
2015-03-07 01:28:52 -0700

    chromaprint 1.1
    Update to latest stable release, remove patch, use HTTPS on homepage
    link, modernize formula.
    
    Closes #37464.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/chromaprint.rb
+++ Library/Formula/chromaprint.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
 class Chromaprint < Formula
-  homepage 'http://acoustid.org/chromaprint'
-  url 'https://bitbucket.org/acoustid/chromaprint/downloads/chromaprint-1.0.tar.gz'
-  sha1 '919e012af588a7e6fea862b29a30e3a5da67526a'
+  homepage "https://acoustid.org/chromaprint"
+  url "https://bitbucket.org/acoustid/chromaprint/downloads/chromaprint-1.1.tar.gz"
+  sha256 "6b14d7ea4964581b73bd3f8038c8857c01e446421c1ae99cbbf64de26b47cd12"
+
   bottle do
     cellar :any
     sha1 "4cc5cb817710059239610681dfffa91e687e14b0" => :mavericks
@@ -11,34 +10,15 @@ class Chromaprint < Formula
     sha1 "93e7e29179d55c4d35c7993fd091c46d7aec622a" => :lion
   end
 
-  revision 1
+  option "without-examples", "Don't build examples (including fpcalc)"
 
-  option 'without-examples', "Don't build examples (including fpcalc)"
-
-  depends_on 'cmake' => :build
-  depends_on 'ffmpeg' if build.with? "examples"
-
-  # Upstream patch:
-  # https://bitbucket.org/acoustid/chromaprint/commits/d0a8d8bc7c1ad5bda3294836f49184fe34a92454
-  patch :DATA
+  depends_on "cmake" => :build
+  depends_on "ffmpeg" if build.with? "examples"
 
   def install
     args = std_cmake_args
-    args << '-DBUILD_EXAMPLES=ON' if build.with? "examples"
+    args << "-DBUILD_EXAMPLES=ON" if build.with? "examples"
     system "cmake", ".", *args
-    system "make install"
+    system "make", "install"
   end
 end
-
-__END__
-diff --git a/src/utils.h b/src/utils.h
-index 47c6b98..76fb240 100644
---- a/src/utils.h
-+++ b/src/utils.h
-@@ -28,6 +28,7 @@
- #include <math.h>
- #include <stddef.h>
- #include <stdint.h>
-+#include <algorithm>
- #include <limits>
- #include <iterator>
