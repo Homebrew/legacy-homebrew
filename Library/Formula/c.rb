be7c91f5ed53ce477a867a7a4cc13b0f9adb2d61
Jan-Gerd Tenberge <janten@gmail.com>
2015-03-08 17:09:08 +0100

    Fix test

--- Library/Formula/c.rb
+++ Library/Formula/c.rb
@@ -1,15 +1,13 @@
 class C < Formula
   homepage "https://github.com/ryanmjacobs/c"
-  url "https://github.com/ryanmjacobs/c/archive/v0.07.tar.gz"
-  sha1 "c40044a2747b97a2f8b550c1d417bb03848fcd40"
-
-  depends_on "gnu-sed" # https://github.com/ryanmjacobs/c/issues/7
+  url "https://github.com/ryanmjacobs/c/archive/v0.09.tar.gz"
+  sha256 "e22bc962ba4ed9a1d84929670626abf7372271efc9d478899fef9981449382ab"
 
   def install
     bin.install "c"
   end
 
   test do
-    system "echo", "int main(void){return 0;}", "|c"
+    system "echo \"int main(void){return 0;}\" | #{bin}/c"
   end
 end
