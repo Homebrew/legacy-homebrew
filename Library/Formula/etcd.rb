7114a83105c24305daf93003a0984b939697b943
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-09 13:22:46 +0000

    etcd: update 2.0.4 bottle.

--- Library/Formula/etcd.rb
+++ Library/Formula/etcd.rb
@@ -5,9 +5,9 @@ class Etcd < Formula
   head "https://github.com/coreos/etcd.git"
 
   bottle do
-    sha1 "ce4c0100d8b80cd1ccf654d21561c1f0124fdddb" => :yosemite
-    sha1 "1296fb789c837c58149ff01794f4bab6509aa584" => :mavericks
-    sha1 "ab9d979ce7867cf64c027bf915689ec6dd8bfdb3" => :mountain_lion
+    sha256 "821841848d23eb547ad98612f4e4048a1e47189bc9a18cbf3826ed3c86180510" => :yosemite
+    sha256 "d319db0e1163f7c66606c42f672f5405ddc206b1f5665928d5a1f03469809b9e" => :mavericks
+    sha256 "76ef13aa736e2f9480c99175db429a389c67e825b786291dff3b374fa36121d5" => :mountain_lion
   end
 
   depends_on "go" => :build
