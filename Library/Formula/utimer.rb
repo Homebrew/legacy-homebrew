a6af6c570450e8ee10feb9095aaa8e7ac73fbf4e
Greg Werbin <gw2286@columbia.edu>
2015-03-09 10:15:44 -0400

    Update homepage and URL
    Not sure why the SHA1 (using `sha1sum utimer-0.4.tar.gz`) is different from what was already there.

--- Library/Formula/utimer.rb
+++ Library/Formula/utimer.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class Utimer < Formula
-  homepage "http://utimer.codealpha.net/utimer/"
-  url "http://utimer.codealpha.net/dl.php?file=utimer-0.4.tar.gz"
-  sha1 "b9590ef4ff6bb8ecf64bb703f50f1bfeddf3fbdd"
+  homepage "https://launchpad.net/utimer"
+  url "https://launchpad.net/utimer/0.4/0.4/+download/utimer-0.4.tar.gz"
+  sha1 "d2f1a7c01da6eeaec28622948c17756456651258"
 
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
