9d6e0a328aa61324a571216758b0f4fc35e8199c
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-10 00:45:33 +0100

    qdbm: test added
    Closes #37547.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/qdbm.rb
+++ Library/Formula/qdbm.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Qdbm < Formula
-  homepage 'http://fallabs.com/qdbm'
-  url 'http://fallabs.com/qdbm/qdbm-1.8.78.tar.gz'
-  sha1 '8c2ab938c2dad8067c29b0aa93efc6389f0e7076'
+  homepage "http://fallabs.com/qdbm"
+  url "http://fallabs.com/qdbm/qdbm-1.8.78.tar.gz"
+  sha256 "b466fe730d751e4bfc5900d1f37b0fb955f2826ac456e70012785e012cdcb73e"
 
   def install
     system "./configure", "--disable-debug",
@@ -11,7 +9,39 @@ class Qdbm < Formula
                           "--enable-bzip",
                           "--enable-zlib",
                           "--enable-iconv"
-    system "make mac"
-    system "make install-mac"
+    system "make", "mac"
+    system "make", "check-mac"
+    system "make", "install-mac"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <depot.h>
+      #include <stdlib.h>
+      #include <stdio.h>
+
+      #define NAME     "mike"
+      #define NUMBER   "00-12-34-56"
+      #define DBNAME   "book"
+
+      int main(void) {
+        DEPOT *depot;
+        char *val;
+
+        if(!(depot = dpopen(DBNAME, DP_OWRITER | DP_OCREAT, -1))) { return 1; }
+        if(!dpput(depot, NAME, -1, NUMBER, -1, DP_DOVER)) { return 1; }
+        if(!(val = dpget(depot, NAME, -1, 0, -1, NULL))) { return 1; }
+
+        printf("%s, %s\\n", NAME, val);
+        free(val);
+
+        if(!dpclose(depot)) { return 1; }
+
+        return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lqdbm", "-o", "test"
+    assert_equal "mike, 00-12-34-56", shell_output("./test").chomp
   end
 end
