aabfa1d7f8e13a7cee8acb4778993ead32c970a2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-09 15:04:33 +0000

    libechonest: update 2.3.1 bottle.

--- Library/Formula/libechonest.rb
+++ Library/Formula/libechonest.rb
@@ -5,10 +5,9 @@ class Libechonest < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha1 "c28f255845403371492620b020672e8e64d19b9b" => :yosemite
-    sha1 "12ab88fa73e2c0ab2650972f31a446c0ea90bb62" => :mavericks
-    sha1 "1ae85cce12862896bcc2d28509854dd8e6b3d9be" => :mountain_lion
+    sha256 "155a7921bd0e807a5702d9cf2f85b0fa2636713c57ee2c355cf27b70bd567c79" => :yosemite
+    sha256 "0e774fa6901a1d8c7244e8fcbbaefdd2a1b4f1d69d9ab3061c9dd67d468ac57b" => :mavericks
+    sha256 "093d7b8f68f0ef72da44b8118d96a0e103f37788e91294006fb1fd09fbe04d82" => :mountain_lion
   end
 
   depends_on "cmake" => :build
