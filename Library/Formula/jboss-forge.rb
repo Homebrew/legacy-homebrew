dba796a5b0b202fbc91a1c22a0ef635ae1ec9c48
George Gastaldi <gegastaldi@gmail.com>
2015-03-10 13:42:25 -0300

    jboss-forge 2.15.1.Final

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class JbossForge < Formula
   homepage 'http://forge.jboss.org/'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.15.0.Final&e=zip&c=offline'
-  version '2.15.0.Final'
-  sha1 'a63fc49a0c6923cd3074300ca3f4e730b3d7f5d5'
+  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.15.1.Final&e=zip&c=offline'
+  version '2.15.1.Final'
+  sha1 '91fd0c0aa231444f1bf58e424346f70d42ac9dee'
 
   def install
     rm_f Dir["bin/*.bat"]
