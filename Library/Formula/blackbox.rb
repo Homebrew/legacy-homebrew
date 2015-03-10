083e7f9a36a40c8afc71afbc058370055461d75c
Erik Kristensen <erik@erikkristensen.com>
2015-03-10 07:42:01 -0600

    removing version

--- Library/Formula/blackbox.rb
+++ Library/Formula/blackbox.rb
@@ -1,7 +1,6 @@
 class Blackbox < Formula
   homepage "https://github.com/StackExchange/blackbox"
   url "https://github.com/StackExchange/blackbox/archive/v1.20150304.tar.gz"
-  version "1.20150304"
   sha256 "62093e2dad03344f677f0bb1243d95da11e5518280a176787b76a86196fbad8b"
 
   def install
