fcbac9ccc88880dcba93fa57b2184ef577ddfdbd
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-10 11:21:36 +0100

    mpck: test added
    Closes #37555.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mpck.rb
+++ Library/Formula/mpck.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class Mpck < Formula
-  homepage 'http://checkmate.gissen.nl/'
-  url 'http://checkmate.gissen.nl/checkmate-0.19.tar.gz'
-  sha1 '4d96d65c9ad8c738cb87d197a45938ca955337d6'
+  homepage "http://checkmate.gissen.nl/"
+  url "http://checkmate.gissen.nl/checkmate-0.19.tar.gz"
+  sha256 "940f95d445bab629051930ef61c5614bdfbe9df6f450f1ffab86eaf885e79195"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/mpck", test_fixtures("test.mp3")
   end
 end
