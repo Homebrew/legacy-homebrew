6c2edc7fa0688bc625b572f62f9f361ca69e00e5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-10 07:45:57 +0000

    jenkins: add 1.602 bottle.

--- Library/Formula/jenkins.rb
+++ Library/Formula/jenkins.rb
@@ -3,6 +3,12 @@ class Jenkins < Formula
   url "http://mirrors.jenkins-ci.org/war/1.602/jenkins.war"
   sha1 "3472f3a2c64192c65796069284cdba9a9a57dabe"
 
+  bottle do
+    sha256 "b35ff0713afb0e08456d49df5a307ffb2c05b5a0b8048f528fbdaedace8f64a5" => :yosemite
+    sha256 "88b80560255449a3aa8e68db6ed06ea53bdd0cc75f01b4ef39cfe8f6900b7ed6" => :mavericks
+    sha256 "f92565e1f4566fdd633d2d444856e61d7b6b9deda8b27c85468630c75701190d" => :mountain_lion
+  end
+
   head do
     url "https://github.com/jenkinsci/jenkins.git"
     depends_on "maven" => :build
