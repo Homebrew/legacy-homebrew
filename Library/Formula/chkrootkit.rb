5151bce6a297b311e4ca5d5b7a39dcfb1608fe6d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-10 23:18:44 +0000

    chkrootkit: add 0.50 bottle.

--- Library/Formula/chkrootkit.rb
+++ Library/Formula/chkrootkit.rb
@@ -3,6 +3,13 @@ class Chkrootkit < Formula
   url "https://mirrors.kernel.org/debian/pool/main/c/chkrootkit/chkrootkit_0.50.orig.tar.gz"
   sha256 "9548fc922b0cb8ddf055faff4a4887f140a31c45f2f5e3aa64aad91ecfa56cc7"
 
+  bottle do
+    cellar :any
+    sha256 "e55c78bfac4b081cef6b51e46cf3ff7fed13b4150f8772535fa401e83bcfe213" => :yosemite
+    sha256 "db4476a3cfc7d10f9991628c95068784c702662bc0be79b124fd775c56c65ecf" => :mavericks
+    sha256 "8aa44d8d76a8d6ddbc27adac04e64e3120c4599b2d617ff0fa82d15f8229957b" => :mountain_lion
+  end
+
   def install
     system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}",
                    "STATIC=", "sense", "all"
