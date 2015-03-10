16d30d75cd2d2854d9220e70cdab1fb97d7586a0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-10 08:44:50 +0000

    sqlite-analyzer: add 3.8.8.3 bottle.

--- Library/Formula/sqlite-analyzer.rb
+++ Library/Formula/sqlite-analyzer.rb
@@ -4,6 +4,13 @@ class SqliteAnalyzer < Formula
   version "3.8.8.3"
   sha256 "790ff6be164488d176b3bed7e0e0850bac1567a4011381307685d48eb69fab48"
 
+  bottle do
+    cellar :any
+    sha256 "9eb7d3df9680d81826da6e39041d28abeba247cbb084a8690524485d34a934c7" => :yosemite
+    sha256 "5092ae2ccce00d9dbf4b8d33a318883054eafd904d55e0ba8ef6a415f7b31497" => :mavericks
+    sha256 "abaf9bc21f00454ee737716345f633383beb92bda83f45c27420eebab3d4d5b7" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make", "sqlite3_analyzer"
