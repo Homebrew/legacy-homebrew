2ca2529452fea41a3607918e93e1485858cc8993
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-10 15:59:03 +0100

    stklos: test added
    Closes #37574.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/stklos.rb
+++ Library/Formula/stklos.rb
@@ -1,17 +1,19 @@
-require 'formula'
-
 class Stklos < Formula
-  homepage 'http://www.stklos.net/'
-  url 'http://www.stklos.net/download/stklos-1.10.tar.gz'
-  sha1 '113551b6bee26fbe5c835ef0db292b9276cccaa0'
+  homepage "http://www.stklos.net/"
+  url "http://www.stklos.net/download/stklos-1.10.tar.gz"
+  sha256 "215e6e6ffcf7751be5f0c114f83286f99e556e089ca1b0ee66f54dd1be080de4"
 
-  depends_on 'gmp'
-  depends_on 'pcre'
-  depends_on 'bdw-gc'
+  depends_on "gmp"
+  depends_on "pcre"
+  depends_on "bdw-gc"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "42", shell_output("stklos -e '(print (+ 41 1))'").chomp
   end
 end
