7bc296f22f31ae108b1bcfb7bdfe255b11de62fc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-10 23:26:05 +0000

    cfv: add 1.18.3 bottle.

--- Library/Formula/cfv.rb
+++ Library/Formula/cfv.rb
@@ -3,6 +3,13 @@ class Cfv < Formula
   url "https://downloads.sourceforge.net/project/cfv/cfv/1.18.3/cfv-1.18.3.tar.gz"
   sha256 "ff28a8aa679932b83eb3b248ed2557c6da5860d5f8456ffe24686253a354cff6"
 
+  bottle do
+    cellar :any
+    sha256 "b378887eb353865af5aea33d214c4efbdb5fa5ef9d02d823be9be96075d460c6" => :yosemite
+    sha256 "8a52341837954b6f13b92dbe0a6ae2b50a1eaebfe29e059b9db23caff7081adf" => :mavericks
+    sha256 "eb8b778c4033e22aa8d5a5b765086b94570f298f30c1f2480aa3947cce5a6db2" => :mountain_lion
+  end
+
   def install
     system "make", "prefix=#{prefix}", "mandir=#{man}", "install"
   end
