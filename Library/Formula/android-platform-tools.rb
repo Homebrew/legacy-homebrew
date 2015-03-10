433db32d32ada5b55403c148b7c6607deb581500
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-03-10 15:55:05 +0100

    android-platform-tools: use https, modernize
    Closes #37573.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/android-platform-tools.rb
+++ Library/Formula/android-platform-tools.rb
@@ -1,14 +1,12 @@
-require "formula"
-
 class AndroidPlatformTools < Formula
-  homepage "http://developer.android.com/sdk"
+  homepage "https://developer.android.com/sdk"
   # the url is from:
-  # http://dl-ssl.google.com/android/repository/repository-8.xml
-  url "http://dl-ssl.google.com/android/repository/platform-tools_r21-macosx.zip"
-  version '21.0.0'
-  sha1 "6675f9f583841972c5c5ef8d2c131e1209529fde"
+  # https://dl.google.com/android/repository/repository-8.xml
+  url "https://dl.google.com/android/repository/platform-tools_r21-macosx.zip"
+  version "21.0.0"
+  sha256 "30ae8724da3db772a776d616b4746516f24ae81330e84315a7ce0c49e0b0b3cb"
 
-  conflicts_with 'android-sdk',
+  conflicts_with "android-sdk",
     :because => "the Android Platform-tools are part of the Android SDK"
 
   def install
