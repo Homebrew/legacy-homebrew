a19329891f154bac36c180bf632e96352d72e9ac
Vlad Shablinsky <vladshablinsky@gmail.com>
2015-03-10 20:48:09 +0300

    yajl: fix https, remove require, fix system make install

--- Library/Formula/yajl.rb
+++ Library/Formula/yajl.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Yajl < Formula
-  homepage 'http://lloyd.github.io/yajl/'
-  url 'https://github.com/lloyd/yajl/archive/2.1.0.tar.gz'
-  sha256 '3fb73364a5a30efe615046d07e6db9d09fd2b41c763c5f7d3bfb121cd5c5ac5a'
+  homepage "https://lloyd.github.io/yajl/"
+  url "https://github.com/lloyd/yajl/archive/2.1.0.tar.gz"
+  sha256 "3fb73364a5a30efe615046d07e6db9d09fd2b41c763c5f7d3bfb121cd5c5ac5a"
 
   bottle do
     cellar :any
@@ -14,14 +12,14 @@ class Yajl < Formula
   end
 
   # Configure uses cmake internally
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   def install
     ENV.deparallelize
 
     system "cmake", ".", *std_cmake_args
-    system "make install"
-    (include/'yajl').install Dir['src/api/*.h']
+    system "make", "install"
+    (include/"yajl").install Dir["src/api/*.h"]
   end
 
   test do
