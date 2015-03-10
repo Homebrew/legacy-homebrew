059604bbeef3d2bfec4e85bdd27ec2844ca7324d
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-10 11:26:02 +0100

    stow: test added
    Closes #37556.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/stow.rb
+++ Library/Formula/stow.rb
@@ -1,13 +1,16 @@
-require 'formula'
-
 class Stow < Formula
-  homepage 'http://www.gnu.org/software/stow/'
-  url 'http://ftpmirror.gnu.org/stow/stow-2.2.0.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/stow/stow-2.2.0.tar.gz'
-  sha1 'b95091be6ebbbac8c5e5112d6d063299c5eefff2'
+  homepage "https://www.gnu.org/software/stow/"
+  url "http://ftpmirror.gnu.org/stow/stow-2.2.0.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/stow/stow-2.2.0.tar.gz"
+  sha256 "8b89d79939cf9ae87d2f223bb36a3b2d0c66775b62aeb9953c6d33dab40d3c2b"
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test").mkpath
+    system "#{bin}/stow", "-nvS", "test"
   end
 end
