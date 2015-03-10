91dd3b5bab1898effa7a1b384af12202116a2531
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-10 15:43:22 +0100

    rush: basic test added
    Closes #37571.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/rush.rb
+++ Library/Formula/rush.rb
@@ -1,14 +1,17 @@
-require 'formula'
-
 class Rush < Formula
-  homepage 'http://www.gnu.org/software/rush/'
-  url 'http://ftpmirror.gnu.org/rush/rush-1.7.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/rush/rush-1.7.tar.gz'
-  sha1 'f886eaf093332a8b8503afcf8ca2acff7eea7191'
+  homepage "https://www.gnu.org/software/rush/"
+  url "http://ftpmirror.gnu.org/rush/rush-1.7.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/rush/rush-1.7.tar.gz"
+  sha256 "35077fa36902fd451db52b49bf059992a20cc8ea031437171f384670d77a003a"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{sbin}/rush", "-h"
   end
 end
