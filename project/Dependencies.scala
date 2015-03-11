6d47c1b82aaf1cb74e2e2f684166578bb3a31844
Peter Rudenko <petro.rudenko@gmail.com>
2015-03-11 16:18:10 +0200

    Unable to use yarn-client with spark 1.2.0 #61
    #61 issue

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,11 +26,12 @@ object Dependencies {
     "io.spray" %% "spray-client" % "1.3.2",
     yammerDeps
   ) ++ yodaDeps
-
+  
+  val sparkVersion = sys.env.getOrElse("SPARK_VERSION", "1.2.1")
   lazy val sparkDeps = Seq(
-    "org.apache.spark" %% "spark-core" % "1.2.0" % "provided" exclude(
+    "org.apache.spark" %% "spark-core" % sparkVersion % "provided" exclude(
                                             "io.netty", "netty-all") excludeAll(excludeQQ),
-    "org.apache.spark" %% "spark-sql" % "1.2.0" % "provided" exclude(
+    "org.apache.spark" %% "spark-sql" % sparkVersion % "provided" exclude(
                                             "io.netty", "netty-all") excludeAll(excludeQQ),
     // Force netty version.  This avoids some Spark netty dependency problem.
     "io.netty" % "netty-all" % "4.0.23.Final"
