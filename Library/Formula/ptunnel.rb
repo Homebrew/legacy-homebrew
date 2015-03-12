a405987874081c466860ad8993e684912eb5456f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-12 19:39:25 +0000

    ptunnel: add 0.72 bottle.

--- Library/Formula/ptunnel.rb
+++ Library/Formula/ptunnel.rb
@@ -3,6 +3,13 @@ class Ptunnel < Formula
   url "http://www.cs.uit.no/~daniels/PingTunnel/PingTunnel-0.72.tar.gz"
   sha256 "b318f7aa7d88918b6269d054a7e26f04f97d8870f47bd49a76cb2c99c73407a4"
 
+  bottle do
+    cellar :any
+    sha256 "b2b0466103ab28caf40e4f3c7973ecbd52b4f50c18a91d741fbda96941e44437" => :yosemite
+    sha256 "a62f7bef1e4d37a0b3619864e1527c32ff64b26cbd9367e692f585e80e861e9c" => :mavericks
+    sha256 "6a3a558951e2ed11a4cb888d02dfde652145eb8175677e6a3b65f9c928117751" => :mountain_lion
+  end
+
   def install
     system "make", "install", "prefix=#{prefix}"
   end
