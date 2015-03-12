94c4a685ce48006bf56ec367d89ecf18d75e1875
Martin Topholm <mph@hoth.dk>
2015-03-12 16:28:03 +0100

    xping: address comments from mikemcquaid
    * Whitespace changes
    * Upgrade hash algorithm
    * Add comments to wierd homebrew constructs
    
    Also satisfy checks from brew audit:
    
    * https for github
    * better path variables for install
    * "better" description of setuid requirements

--- Library/Formula/xping.rb
+++ Library/Formula/xping.rb
@@ -1,49 +1,47 @@
-require "formula"
-
 class Xping < Formula
+  homepage "https://github.com/martintopholm/xping"
 
-  homepage "http://github.com/martintopholm/xping"
-  version "1.3"
   depends_on "libevent"
 
   stable do
-    url "https://github.com/martintopholm/xping/archive/v#{version}.tar.gz"
-    sha1 "822cd76851af577d2b987dd65933ca9187a4ea8f"
+    url "https://github.com/martintopholm/xping/archive/v1.3.tar.gz"
+    sha256 "6bab7abf5433fd1daa7de939727db95f88a2ac3630f92ec2f55cfae9d69846e5"
+
+    # patches waiting for upstream release tag
     patch do
       url "http://martin.topholm.eu/pub/xping/brew/0001-main-keep-env-cflags-and-clear-install-deps.patch"
-      sha1 "c52dd1d313a0b0f8055f0669effbddf14cae067e"
+      sha256 "50455e4d6ca14f8e7264bcc5b5b71226f149e082e2944901cec3958cac7249d6"
     end
+
     patch do
       url "http://martin.topholm.eu/pub/xping/brew/0003-termio-correct-labelwidth-variable-scope.patch"
-      sha1 "dfac12bbf127aad28ed9702c5d08ecd3996256a6"
+      sha256 "dc3a0b7ad9f0031fead8f41cd59ed338bb701ef1615985f84388b0587229189c"
     end
   end
+
   head do
-    url "https://github.com/martintopholm/xping.git", :using => :git, :branch => "master"
+    url "https://github.com/martintopholm/xping.git", :branch => "master"
   end
 
   def install
+    # Terminal.app doesn't play well with ANSI sequences.
+    # Link against NCURSES instead.
     ENV.append "DEPS", "check-curses.c"
     ENV.append "CFLAGS", "-DNCURSES"
     ENV.append "LIBS", "-lncurses"
     system "make"
-    system "make install PREFIX=#{prefix} MANPATH=#{prefix}/share/man"
+    system "make", "install", "BINPATH=#{bin}", "MANPATH=#{man}"
   end
 
   test do
-    system "#{bin}/xping -V"
+    # Non-setuid xping can't open raw socket and will return error.
+    # Request version string instead.
+    system "#{bin}/xping", "-V"
   end
 
   def caveats; <<-EOS.undent
-    xping requires superuser privileges. You can either run the program
-    via `sudo`, or change its ownership to root and set the setuid bit:
-
-      sudo chown root:wheel #{bin}/xping
-      sudo chmod u+s #{bin}/xping
-
-    In any case, you should be certain that you trust the software you
-    are executing with elevated privileges.
+    xping requires root privileges so you will need to run `sudo xping`.
+    You should be certain that you trust any software you grant root privileges.
     EOS
   end
-
 end
