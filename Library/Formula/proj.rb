c4a046f0be6a8b616a9d62145302a739872f6b50
EWeber <eweber@swc-eweber-m.nmfs.local>
2015-03-12 13:16:58 -0700

    update proj to version 4.9.1 and add tests

--- Library/Formula/proj.rb
+++ Library/Formula/proj.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Proj < Formula
-  homepage 'http://trac.osgeo.org/proj/'
-  url 'http://download.osgeo.org/proj/proj-4.8.0.tar.gz'
-  sha1 '5c8d6769a791c390c873fef92134bf20bb20e82a'
-  head "http://svn.osgeo.org/metacrs/proj/trunk/proj/"
+  homepage 'https://trac.osgeo.org/proj/'
+  url 'http://download.osgeo.org/proj/proj-4.9.1.tar.gz'
+  sha1 '0bc63a41f1bdcff600d076c056f796007abf3f2f'
+  head "https://svn.osgeo.org/metacrs/proj/trunk/proj/"
 
   option "with-vdatum", "Install vertical datum files (~380 MB)"
 
@@ -85,5 +85,10 @@ class Proj < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+  test do
+    system "proj -l"
+    system "echo -120 36 | proj +proj=tmerc +ellps=WGS84 +units=km +lon_0=-121.15 +lat_0=34.15"
+    system "echo 100 206 | proj -I +proj=tmerc +ellps=WGS84 +units=km +lon_0=-121.15 +lat_0=34.15"
   end
 end
