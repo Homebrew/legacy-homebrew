063204c8bf26cad00be36ab5f7ac880e499a66bc
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-12 17:43:37 +0100

    ptunnel: test added

--- Library/Formula/ptunnel.rb
+++ Library/Formula/ptunnel.rb
@@ -1,14 +1,10 @@
-require 'formula'
-
 class Ptunnel < Formula
-  homepage 'http://www.cs.uit.no/~daniels/PingTunnel/'
-  url 'http://www.cs.uit.no/~daniels/PingTunnel/PingTunnel-0.72.tar.gz'
-  sha1 'd5d874ec7b4f68d2307cacc83a1c408aeb4206a5'
+  homepage "http://www.cs.uit.no/~daniels/PingTunnel/"
+  url "http://www.cs.uit.no/~daniels/PingTunnel/PingTunnel-0.72.tar.gz"
+  sha256 "b318f7aa7d88918b6269d054a7e26f04f97d8870f47bd49a76cb2c99c73407a4"
 
   def install
-    system "make"
-    bin.install "ptunnel"
-    man8.install "ptunnel.8"
+    system "make", "install", "prefix=#{prefix}"
   end
 
   def caveats; <<-EOS.undent
@@ -17,5 +13,9 @@ class Ptunnel < Formula
     Alternatively, you can try using the -u flag to start ptunnel in 'unprivileged' mode,
     but this is not recommended. See http://www.cs.uit.no/~daniels/PingTunnel/ for details.
     EOS
+  end
+
+  test do
+    assert_match "v #{version}", shell_output("#{bin}/ptunnel -h", 1)
   end
 end
