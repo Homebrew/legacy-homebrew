b22e844deecd14762f00d58c6079cfc214dd16c4
Xu Cheng <xucheng@me.com>
2015-03-12 23:35:24 +0800

    java overridable_java_home_env
    Closes #37622

--- Library/Homebrew/language/java.rb
+++ Library/Homebrew/language/java.rb
@@ -1,8 +1,16 @@
 module Language
   module Java
-    def self.java_home_env(version=nil)
+    def self.java_home_cmd(version=nil)
       version_flag = " --version #{version}" if version
-      { :JAVA_HOME => "$(/usr/libexec/java_home#{version_flag})" }
+      "/usr/libexec/java_home#{version_flag}"
+    end
+
+    def self.java_home_env(version=nil)
+      { :JAVA_HOME => "$(#{java_home_cmd(version)})" }
+    end
+
+    def self.overridable_java_home_env(version=nil)
+      { :JAVA_HOME => "${JAVA_HOME:-$(#{java_home_cmd(version)})}" }
     end
   end
 end
