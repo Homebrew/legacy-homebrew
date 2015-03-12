eae9f11c40aa945147d661012fd465917f246c08
CNA-Bld <cna.bld@gmail.com>
2015-03-12 15:28:22 +0800

    ykclient 2.14
    Closes #37626.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ykclient.rb
+++ Library/Formula/ykclient.rb
@@ -1,19 +1,21 @@
-require 'formula'
-
 class Ykclient < Formula
-  homepage 'http://yubico.github.io/yubico-c-client/'
-  url 'http://yubico.github.io/yubico-c-client/releases/ykclient-2.12.tar.gz'
-  sha1 '518ce53ba9ef61a619f9150778f19fad23014a9c'
+  homepage "https://yubico.github.io/yubico-c-client/"
+  url "https://yubico.github.io/yubico-c-client/releases/ykclient-2.14.tar.gz"
+  sha256 "bd7ddaa1d88ec7be323738cabdf58ff99cb6d6cd3fe937c69b52fa9b3764d362"
 
   option :universal
 
-  depends_on 'pkg-config' => :build
-  depends_on 'help2man' => :build
+  depends_on "pkg-config" => :build
+  depends_on "help2man" => :build
 
   def install
     ENV.universal_binary if build.universal?
 
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/ykclient", "--version"
   end
 end
