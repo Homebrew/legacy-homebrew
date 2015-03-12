ce3b8d74080dd52fd4be3552b24a159067eda187
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-13 00:13:35 +0100

    mftrace: test added

--- Library/Formula/mftrace.rb
+++ Library/Formula/mftrace.rb
@@ -1,15 +1,18 @@
-require 'formula'
-
 class Mftrace < Formula
-  homepage 'http://lilypond.org/mftrace/'
-  url 'http://lilypond.org/download/sources/mftrace/mftrace-1.2.18.tar.gz'
-  sha1 '8ae5c69a49f3d34e021913b6378056a6d43c2fd2'
+  homepage "http://lilypond.org/mftrace/"
+  url "http://lilypond.org/download/sources/mftrace/mftrace-1.2.18.tar.gz"
+  sha256 "0d31065f1d35919e311d9170bbfcdacc58736e3f783311411ed1277aa09d3261"
 
-  depends_on 'potrace'
-  depends_on 't1utils'
+  depends_on "potrace"
+  depends_on "t1utils"
+  depends_on "fontforge" => [:recommended, :run]
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/mftrace", "--version"
   end
 end
