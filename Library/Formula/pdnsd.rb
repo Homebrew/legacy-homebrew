6ffa5957f7905d3f409e13ab8ef9e64e18e57cd7
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-12 17:31:50 +0100

    pdnsd: test added
    Closes #37648.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pdnsd.rb
+++ Library/Formula/pdnsd.rb
@@ -1,18 +1,22 @@
-require 'formula'
-
 class Pdnsd < Formula
-  homepage 'http://members.home.nl/p.a.rombouts/pdnsd/'
-  url 'http://members.home.nl/p.a.rombouts/pdnsd/releases/pdnsd-1.2.9a-par.tar.gz'
-  version '1.2.9a-par'
-  sha1 '55d8fd71e2f291a3ead53c8097729fa95264bea9'
+  homepage "http://members.home.nl/p.a.rombouts/pdnsd/"
+  url "http://members.home.nl/p.a.rombouts/pdnsd/releases/pdnsd-1.2.9a-par.tar.gz"
+  version "1.2.9a-par"
+  sha256 "bb5835d0caa8c4b31679d6fd6a1a090b71bdf70950db3b1d0cea9cf9cb7e2a7b"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--localstatedir=#{var}",
                           "--sysconfdir=#{etc}",
                           "--mandir=#{man}",
                           "--with-cachedir=#{var}/cache/pdnsd"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "version #{version}",
+      shell_output("#{sbin}/pdnsd --version", 1)
   end
 end
