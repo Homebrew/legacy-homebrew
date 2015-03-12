9a6bcfc0a08f9dc900118958e7514b66fb50aeaf
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-12 16:05:37 +0000

    bgpq3: add 0.1.27 bottle.

--- Library/Formula/bgpq3.rb
+++ Library/Formula/bgpq3.rb
@@ -6,6 +6,13 @@ class Bgpq3 < Formula
   sha1 'fae5f735033202ab288aef3ad7a83a0b323ae8d7'
   head "https://github.com/snar/bgpq3.git"
 
+  bottle do
+    cellar :any
+    sha256 "d9af2aa66da7aecb0f86f870f8bedede3a99f68d90822a425d7de96b9bedeed9" => :yosemite
+    sha256 "e82b1a638d5b0c9a1eb500311887f0221a599c0fa1be760c1cb1a7feea8d3ddd" => :mavericks
+    sha256 "88e24d49eb3cc2328fcfb2fe3e455b96bd9954d8547bea3d0e1bc442e084e456" => :mountain_lion
+  end
+
   # Makefile: upstream has been informed of the patch through email (multiple
   # times) but no plans yet to incorporate it https://github.com/snar/bgpq3/pull/2
   # there was discussion about this patch for 0.1.18 and 0.1.19 as well
