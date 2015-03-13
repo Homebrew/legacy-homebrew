35e7251b1522d69ffbf06e4114403c64db520174
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-13 14:08:47 +0100

    udunits 2.2.18
    Closes #37680.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/udunits.rb
+++ Library/Formula/udunits.rb
@@ -1,19 +1,25 @@
-require 'formula'
-
 class Udunits < Formula
-  homepage 'http://www.unidata.ucar.edu/software/udunits/'
-  url 'ftp://ftp.unidata.ucar.edu/pub/udunits/udunits-2.1.24.tar.gz'
-  sha1 '64bbb4b852146fb5d476baf4d37c9d673cfa42f9'
+  homepage "http://www.unidata.ucar.edu/software/udunits/"
+  url "ftp://ftp.unidata.ucar.edu/pub/udunits/udunits-2.2.18.tar.gz"
+  sha256 "f542ed81140db2dae862a0018c8cddaf3b9ded1886e3755489b9329c7ecf8de0"
 
-  option "html-docs", "Installs html documentation"
-  option "pdf-docs", "Installs pdf documentation"
+  option "with-html-docs", "Installs html documentation"
+  option "with-pdf-docs", "Installs pdf documentation"
+
+  deprecated_option "html-docs" => "with-html-docs"
+  deprecated_option "pdf-docs" => "with-pdf-docs"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     targets = ["install"]
     targets << "install-html" if build.include? "html-docs"
     targets << "install-pdf" if build.include? "pdf-docs"
     system "make", *targets
+  end
+
+  test do
+    assert_match(/1 kg = 1000 g/, shell_output("#{bin}/udunits2 -H kg -W g"))
   end
 end
