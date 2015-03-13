8303d7dfca89c7ade8cd2685b8a3b8d9bde05a61
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-13 22:04:26 +0000

    emscripten: update 1.29.11 bottle.

--- Library/Formula/emscripten.rb
+++ Library/Formula/emscripten.rb
@@ -4,9 +4,9 @@ class Emscripten < Formula
   sha1 "d926f406486e3c3a94a1fe58783a91fd6975bc9c"
 
   bottle do
-    sha1 "e0cfe1c9d8bc969814281c2d8dffe2323d750259" => :yosemite
-    sha1 "39cf18caf1c8483fe0fa9d86dc42652544972b41" => :mavericks
-    sha1 "ecf68dab9122422f61d8dbec3023e0df5d562b17" => :mountain_lion
+    sha256 "75c43ccaa49dc749f51bae40ac86d4bb767df74866ab9650dd8153f79139d529" => :yosemite
+    sha256 "61d9ec9b4f6f914fcc17cc8278081dfd5b4858c287fc5b1ef1a333bd175a190e" => :mavericks
+    sha256 "699b9c70104360d5210a3eeb6a09e93c6b78140566ac39eda89c9eb317050ccb" => :mountain_lion
   end
 
   head do
