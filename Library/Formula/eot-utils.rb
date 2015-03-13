667601add385e36362de1d3526414e3d83764444
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-13 11:27:24 +0100

    eot-utils: test added
    Closes #37671.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/eot-utils.rb
+++ Library/Formula/eot-utils.rb
@@ -1,12 +1,21 @@
-require 'formula'
-
 class EotUtils < Formula
-  homepage 'http://www.w3.org/Tools/eot-utils/'
-  url 'http://www.w3.org/Tools/eot-utils/eot-utilities-1.1.tar.gz'
-  sha1 '7e8a68ba1ae4b533113e7965aa2cca133367f31f'
+  homepage "https://www.w3.org/Tools/eot-utils/"
+  url "https://www.w3.org/Tools/eot-utils/eot-utilities-1.1.tar.gz"
+  sha256 "4eed49dac7052e4147deaddbe025c7dfb404fc847d9fe71e1c42eba5620e6431"
+
+  resource "eot" do
+    url "https://github.com/RoelN/font-face-render-check/raw/98f0adda9cfe44fe97f6d538aa893a37905a7add/dev/pixelambacht-dash.eot"
+    sha256 "23d6fbe778abe8fe51cfc5ea22f8e061b4c8d32b096ef4a252ba6f2f00406c91"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    resource("eot").stage do
+      system "#{bin}/eotinfo", "pixelambacht-dash.eot"
+    end
   end
 end
