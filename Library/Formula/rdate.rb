e1ebb1d74c9ce4a3e2c5c932fca3b33815d11a94
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-13 14:17:52 +0100

    rdate: test added
    Closes #37682.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/rdate.rb
+++ Library/Formula/rdate.rb
@@ -1,14 +1,17 @@
-require 'formula'
-
 class Rdate < Formula
-  homepage 'http://www.aelius.com/njh/rdate/'
-  url 'http://www.aelius.com/njh/rdate/rdate-1.5.tar.gz'
-  sha1 'd7212503907db741ab53c4dd13ed141702d1809b'
+  homepage "http://www.aelius.com/njh/rdate/"
+  url "http://www.aelius.com/njh/rdate/rdate-1.5.tar.gz"
+  sha256 "6e800053eaac2b21ff4486ec42f0aca7214941c7e5fceedd593fa0be99b9227d"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/rdate", "-p", "-t", "2", "0.pool.ntp.org"
   end
 end
