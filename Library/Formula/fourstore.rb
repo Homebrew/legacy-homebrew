4e55b3901fb247fab2c47e3ff32a75c2a1978a2f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-13 03:34:24 +0000

    fourstore: add 1.1.5 bottle.

--- Library/Formula/fourstore.rb
+++ Library/Formula/fourstore.rb
@@ -3,6 +3,12 @@ class Fourstore < Formula
   url "http://4store.org/download/4store-v1.1.5.tar.gz"
   sha256 "2bdd6fb804288802187c5779e365eea2b3ddebce419b3da0609be38edc9e8c5b"
 
+  bottle do
+    sha256 "af6db4040f02b4d05cad3e860030522089e00882b924cf4ac420a08ec1be4cc7" => :yosemite
+    sha256 "1999ddd3484593da6fa2579eb9978457bd844c157bff8796182ac2b2ed2e86c5" => :mavericks
+    sha256 "d2c2d74e44e4aa14215f2f22778744993140479b3269ab7483f80d980bd14d7b" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "raptor"
