490e34237088d764732f8e14933981cc4a59c583
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-13 14:00:49 +0100

    jnettop: basic test added

--- Library/Formula/jnettop.rb
+++ Library/Formula/jnettop.rb
@@ -1,17 +1,20 @@
-require 'formula'
-
 class Jnettop < Formula
-  homepage 'http://jnettop.kubs.info/'
-  url 'http://jnettop.kubs.info/dist/jnettop-0.13.0.tar.gz'
-  sha1 '59f4c28db6f8b1c58050d72aaa4b3b6d5a4a75e0'
+  homepage "http://jnettop.kubs.info/"
+  url "http://jnettop.kubs.info/dist/jnettop-0.13.0.tar.gz"
+  sha256 "e987a1a9325595c8a0543ab61cf3b6d781b4faf72dd0e0e0c70b2cc2ceb5a5a0"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'glib'
+  depends_on "pkg-config" => :build
+  depends_on "glib"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--man=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/jnettop", "-h"
   end
 end
