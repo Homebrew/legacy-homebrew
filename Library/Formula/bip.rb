1fda943c2add0c42d0db42f6bf1b4d77549c8a1e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-13 00:54:52 +0000

    bip: add 0.8.9_1 bottle.

--- Library/Formula/bip.rb
+++ Library/Formula/bip.rb
@@ -4,6 +4,13 @@ class Bip < Formula
   sha256 "3c950f71ef91c8b686e6835f9b722aa7ccb88d3da4ec1af19617354fd3132461"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "73a885e1f2655a3c6d8ff108559e00171a78767dbb57ce79cfbfd77e68362d8c" => :yosemite
+    sha256 "a979c86cee1ea3c7cbe99039eda5ae1b8888547a74339b0f8d20a170b331c169" => :mavericks
+    sha256 "fb8255c8de05e2cd287352e7a0088cc10b83c2f8a30e2e92af30237c2390d35d" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
