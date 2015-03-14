d24833f4a773aff380bb922838679c0b7cbe739b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-14 11:16:07 +0000

    idutils: add 4.6 bottle.

--- Library/Formula/idutils.rb
+++ Library/Formula/idutils.rb
@@ -4,6 +4,12 @@ class Idutils < Formula
   mirror "https://ftp.gnu.org/gnu/idutils/idutils-4.6.tar.xz"
   sha256 "8181f43a4fb62f6f0ccf3b84dbe9bec71ecabd6dfdcf49c6b5584521c888aac2"
 
+  bottle do
+    sha256 "a0533ea0385b7aaae0222ed55e711fa5ce96194d7ab021f6a3209dc265a7fc95" => :yosemite
+    sha256 "ff09770db3a3fd0c5bdb02387b1c41375a74c938b2d0de0e8f340aed2fcb24a8" => :mavericks
+    sha256 "d078b7c9c5e7c011141d9f2665a9509c93996d49f301bc3ef49fc54fe0c4f663" => :mountain_lion
+  end
+
   conflicts_with "coreutils", :because => "both install `gid` and `gid.1`"
 
   def install
