b27747f8a300f6c7a2f95c5d89bb4f855acaffc9
Xu Cheng <xucheng@me.com>
2015-03-14 19:05:13 +0800

    idutils: add test
    Closes #37712.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/idutils.rb
+++ Library/Formula/idutils.rb
@@ -1,15 +1,18 @@
-require 'formula'
-
 class Idutils < Formula
-  homepage 'http://www.gnu.org/s/idutils/'
-  url 'http://ftpmirror.gnu.org/idutils/idutils-4.6.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/idutils/idutils-4.6.tar.xz'
-  sha256 '8181f43a4fb62f6f0ccf3b84dbe9bec71ecabd6dfdcf49c6b5584521c888aac2'
+  homepage "https://www.gnu.org/s/idutils/"
+  url "http://ftpmirror.gnu.org/idutils/idutils-4.6.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/idutils/idutils-4.6.tar.xz"
+  sha256 "8181f43a4fb62f6f0ccf3b84dbe9bec71ecabd6dfdcf49c6b5584521c888aac2"
 
-  conflicts_with 'coreutils', :because => 'both install `gid` and `gid.1`'
+  conflicts_with "coreutils", :because => "both install `gid` and `gid.1`"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"mkid", "/usr/include"
+    system bin/"lid", "FILE"
   end
 end
