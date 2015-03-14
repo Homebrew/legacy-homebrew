bf0484d90cc45f64084722b9f69b3302e5f8b20c
Mário Freitas <imkira@gmail.com>
2015-03-14 17:36:19 +0900

    mobiledevice 2.0.0 (new formula)
    mobiledevice is a command line utility for interacting with Apple's
    Private Mobile Device Framework. It can be used for automating several
    tasks like installing and uninstalling apps on your iPhone/iPad without
    having to manually do it via Xcode or iTunes. And you don't need a
    jailbroken device!

---
+++ Library/Formula/mobiledevice.rb
@@ -0,0 +1,14 @@
+class Mobiledevice < Formula
+  homepage "https://github.com/imkira/mobiledevice"
+  url "https://github.com/imkira/mobiledevice/archive/v2.0.0.tar.gz"
+  sha256 "07b167f6103175c5eba726fd590266bf6461b18244d34ef6d05a51fc4871e424"
+
+  def install
+    system "make", "install", "CC=#{ENV.cc}", "PREFIX=#{prefix}"
+  end
+
+  test do
+    has_version = shell_output("#{bin}/mobiledevice version").strip
+    assert_match "#{version}", has_version
+  end
+end
