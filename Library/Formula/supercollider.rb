8d0cdd025db0b56cc10bf83bd10667dedd95c48c
Chris Sattinger <crucialfelix@gmail.com>
2015-03-14 18:00:19 +0100

    cleanup

--- Library/Formula/supercollider.rb
+++ Library/Formula/supercollider.rb
@@ -23,8 +23,6 @@ class Supercollider < Formula
       "-G", "Xcode",
       "-DCMAKE_PREFIX_PATH=#{Formula["qt5"].opt_prefix}",
       "-DSC_QT=1",
-      # defaults to the user's OS
-      # "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7",
       "-DREADLINE_INCLUDE_DIR=#{Formula["readline"].opt_include}",
       "-DREADLINE_LIBRARY=#{Formula["readline"].opt_lib}/libreadline.dylib",
       ".."
@@ -35,23 +33,18 @@ class Supercollider < Formula
       xcodebuild "-target", "install",
                 "-configuration", "Release"
     end
-    # "SYMROOT=#{bp}" -- fails
 
-    # Copy the app into the cellar
-    # User should then run `brew linkapps supercollider`
-    # to install it into /Applications
-    cp_r "./build/Install/SuperCollider/SuperCollider.app", prefix/"SuperCollider.app"
+    prefix.install "build/Install/SuperCollider"
 
     # Install sclang and scsynth binaries.
     # sclang and scsynth need to be executed in the bundle
     # or the dynamic linking to the Qt libraries gets confused.
     # write_exec_script creates a wrapper to exec it
-    bin.write_exec_script prefix/"SuperCollider.app/Contents/MacOS/sclang"
-    bin.write_exec_script prefix/"SuperCollider.app/Contents/MacOS/scsynth"
+    bin.write_exec_script prefix/"SuperCollider/SuperCollider.app/Contents/MacOS/sclang"
+    bin.write_exec_script prefix/"SuperCollider/SuperCollider.app/Contents/MacOS/scsynth"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
     assert File.exist?("#{prefix}/SuperCollider.app")
     assert File.exist?("#{bin}/sclang")
     assert File.exist?("#{bin}/scsynth")
