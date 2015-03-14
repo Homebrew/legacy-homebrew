3c83f088ca5b571a965deb5780d4ffd41dbfb14a
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-14 05:55:25 +0000

    erlang-versions: bottle regex fix
    Note: This isn’t a fix yet. It’s still detecting just the R14 part and
    ignoring the latter.
    
    Mike just asked me to push to a remote branch so he can giggle at how
    bad my regexes are/help me get it solved :).

--- Library/Homebrew/bottle_version.rb
+++ Library/Homebrew/bottle_version.rb
@@ -16,6 +16,10 @@ class BottleVersion < Version
     m = /(00-\d+\.\d+(\.\d+)+)/.match(stem)
     return m.captures.first unless m.nil?
 
+    # e.g. R14BO4 from erlang-r14-R14BO4.yosemite.bottle.tar.gz
+    m = /-(D+\d+\d+\D+\d+\d+)/.match(stem)
+    return m.captures.first unless m.nil?
+
     # e.g. 13-2.9.19 from libpano-13-2.9.19_1.yosemite.bottle.tar.gz
     m = /\D+-(\d+-[\d\.]+)/.match(stem)
     return m.captures.first unless m.nil?
