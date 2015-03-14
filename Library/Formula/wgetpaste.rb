f5108f3cc21514981a52d29dceaeaca36d282215
Xu Cheng <xucheng@me.com>
2015-03-14 19:32:41 +0800

    wgetpaste: add test
    Closes #37714.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/wgetpaste.rb
+++ Library/Formula/wgetpaste.rb
@@ -1,11 +1,13 @@
-require "formula"
-
 class Wgetpaste < Formula
   homepage "http://wgetpaste.zlin.dk/"
   url "http://wgetpaste.zlin.dk/wgetpaste-2.25.tar.bz2"
-  sha1 "4f2220714522c724928dee67ed1c5d7feed1c207"
+  sha256 "6a7e078e6607f4fe819fc52857a601630ec0f7d2ee855d3b1cd7e75a904c40f4"
 
   def install
     bin.install "wgetpaste"
+  end
+
+  test do
+    system bin/"wgetpaste", "-S"
   end
 end
