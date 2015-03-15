d1aaaead80e4e23956ef201889ff6a18634a58cd
Koen Rouwhorst <info@koenrouwhorst.nl>
2015-03-15 16:50:55 +0100

    s3cmd 1.5.2
    Closes #37741.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/s3cmd.rb
+++ Library/Formula/s3cmd.rb
@@ -1,8 +1,8 @@
 class S3cmd < Formula
-  homepage 'http://s3tools.org/s3cmd'
-  url 'https://downloads.sourceforge.net/project/s3tools/s3cmd/1.5.0/s3cmd-1.5.0.tar.gz'
-  sha1 '53ebc485329cb15cad8f61ca0c8c2d06563ee2f3'
-  head 'https://github.com/s3tools/s3cmd.git'
+  homepage "http://s3tools.org/s3cmd"
+  url "https://downloads.sourceforge.net/project/s3tools/s3cmd/1.5.2/s3cmd-1.5.2.tar.gz"
+  sha256 "ff8a6764e8bdd7ed48a93e51b08222bea33469d248a90b8d25315b023717b42d"
+  head "https://github.com/s3tools/s3cmd.git"
 
   depends_on :python if MacOS.version <= :snow_leopard
 
@@ -16,12 +16,12 @@ class S3cmd < Formula
 
   resource "six" do
     url "https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz"
-    sha1 "d168e6d01f0900875c6ecebc97da72d0fda31129"
+    sha256 "e24052411fc4fbd1f672635537c3fc2330d9481b18c0317695b46259512c91d5"
   end
 
   resource "python-dateutil" do
     url "https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.4.0.tar.gz"
-    sha1 "159081a4c5b3602ab440a7db305f987c00ee8c6d"
+    sha256 "439df33ce47ef1478a4f4765f3390eab0ed3ec4ae10be32f2930000c8d19f417"
   end
 
   def install
