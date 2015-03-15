5d7fade3f730bad50951ec8acee104d8cf60e2e2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-15 13:44:41 +0000

    dark-mode: add 1.0.1 bottle.

--- Library/Formula/dark-mode.rb
+++ Library/Formula/dark-mode.rb
@@ -4,6 +4,11 @@ class DarkMode < Formula
   sha256 "7c71d865ad1a058c98909b442cdeef6b95be62313909c176a9e58db0a7512902"
   head "https://github.com/sindresorhus/dark-mode.git"
 
+  bottle do
+    cellar :any
+    sha256 "f58b190cb89027daaea97e8cdc31bc4f097c51fef33e805751a1aef797871e5f" => :yosemite
+  end
+
   depends_on :macos => :yosemite
   depends_on :xcode => :build
 
