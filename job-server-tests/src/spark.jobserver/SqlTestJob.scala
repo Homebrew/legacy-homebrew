8f7ad9979e97a71dec457f298688df98f3426d10
Antony Arokiasamy <aarokiasamy@netflix.com>
2015-03-15 20:07:38 -0700

    Updated for Spark 1.3.0

--- job-server-tests/src/spark.jobserver/SqlTestJob.scala
+++ job-server-tests/src/spark.jobserver/SqlTestJob.scala
@@ -20,9 +20,9 @@ object SqlLoaderJob extends SparkSqlJob {
   def validate(sql: SQLContext, config: Config): SparkJobValidation = SparkJobValid
 
   def runJob(sql: SQLContext, config: Config): Any = {
-    import sql.createSchemaRDD
+    import sql.implicits._
     val addrRdd = sql.sparkContext.parallelize(addresses)
-    addrRdd.registerTempTable("addresses")
+    addrRdd.toDF().registerTempTable("addresses")
     addrRdd.count()
   }
 }
