b412335f9408d9f19979963164ba94e05bce3986
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-17 00:51:30 +0100

    fondu: test added

--- Library/Formula/fondu.rb
+++ Library/Formula/fondu.rb
@@ -1,15 +1,25 @@
-require 'formula'
-
 class Fondu < Formula
-  homepage 'http://fondu.sourceforge.net/'
-  url 'http://fondu.sourceforge.net/fondu_src-060102.tgz'
-  sha1 '4fa5438df935f15f0370802f94a04e8d1263061b'
+  homepage "http://fondu.sourceforge.net/"
+  url "http://fondu.sourceforge.net/fondu_src-060102.tgz"
+  sha256 "22bb535d670ebc1766b602d804bebe7e84f907c219734e6a955fcbd414ce5794"
+
+  resource "cminch.ttf" do
+    url "http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/cminch.ttf"
+    sha256 "03aacbe19eac7d117019b6a6bf05197086f9de1a63cb4140ff830c40efebac63"
+  end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make"
-    bin.install 'fondu', 'ufond'
-    man1.install 'fondu.1', 'ufond.1'
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--exec-prefix=#{prefix}"
+    system "make", "install"
+    man1.install Dir["*.1"]
+  end
+
+  test do
+    resource("cminch.ttf").stage do
+      system "#{bin}/ufond", "cminch.ttf"
+    end
   end
 end
