7409abe44caa2ddbe7ab68a879a8c00a5ec8bf29
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-16 01:13:15 +0100

    edelta: test added
    Closes #37753.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/edelta.rb
+++ Library/Formula/edelta.rb
@@ -1,12 +1,17 @@
-require 'formula'
-
 class Edelta < Formula
-  homepage 'http://www.diku.dk/hjemmesider/ansatte/jacobg/edelta/'
-  url 'http://www.diku.dk/hjemmesider/ansatte/jacobg/edelta/edelta-0.10b.tar.gz'
-  sha1 'bcf24ff68c67da47484beb9f4869c726b9f06dc2'
+  homepage "http://www.diku.dk/hjemmesider/ansatte/jacobg/edelta/"
+  url "http://www.diku.dk/hjemmesider/ansatte/jacobg/edelta/edelta-0.10b.tar.gz"
+  sha256 "86d60b8726d37d5486b0d8030492a99b2f4ce1266ad50a99edb07ee6d529815e"
 
   def install
     system "make", "CFLAGS=#{ENV.cflags}"
-    bin.install 'edelta'
+    bin.install "edelta"
+  end
+
+  test do
+    (testpath/"test1").write "foo"
+    (testpath/"test2").write "bar"
+
+    system "#{bin}/edelta", "delta", "test1", "test2"
   end
 end
