3ac7dba4c1fbdee7121e833c234a5da336141a0f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-16 00:45:15 +0000

    one-ml: add 0.1 bottle.

--- Library/Formula/one-ml.rb
+++ Library/Formula/one-ml.rb
@@ -3,6 +3,13 @@ class OneMl < Formula
   url "https://www.mpi-sws.org/~rossberg/1ml/1ml-0.1.zip"
   sha256 "64c40c497f48355811fc198a2f515d46c1bb5031957b87f6a297822b07bb9c9a"
 
+  bottle do
+    cellar :any
+    sha256 "85f22e9405c247ae477de31759fe4c5a7dacd9cdd98f0c6c4a13aac4cf6bc3c2" => :yosemite
+    sha256 "06e36adef37c7f05e7a615609a3fbe5c281187808006dba695ffaf77b132a5ff" => :mavericks
+    sha256 "65566030fd29864434eb8a54ae582c85aa850fecbc570dc12d84ff385dd004a6" => :mountain_lion
+  end
+
   depends_on "objective-caml" => :build
 
   def install
