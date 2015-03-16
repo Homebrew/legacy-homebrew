9ad465698432a6a69ee244e424464ccd7b615b16
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-16 21:31:47 +0000

    spidermonkey: update 1.8.5_1 bottle.

--- Library/Formula/spidermonkey.rb
+++ Library/Formula/spidermonkey.rb
@@ -10,10 +10,9 @@ class Spidermonkey < Formula
   head 'https://hg.mozilla.org/tracemonkey/archive/tip.tar.gz'
 
   bottle do
-    revision 1
-    sha1 "6c6fd6d40d41764a086a6fb134176253deb1a51b" => :yosemite
-    sha1 "5d19010b10a5f1827511ca791debf9f2d9076e47" => :mavericks
-    sha1 "37d04b64aba47dbf65f197aec94da9acf5f1fd4c" => :mountain_lion
+    sha256 "7ab660cad3aac11fbf4befa3fbbf65a7ee64d858539ad81298271389b2957375" => :yosemite
+    sha256 "cda0b81bd974640690fe067691efca6bc7d1583117cd5db28cca43ab8e2f884c" => :mavericks
+    sha256 "769035a4fa0ed09b71aa9747c2834a51285903e51d9bc478f865c037a8666370" => :mountain_lion
   end
 
   conflicts_with 'narwhal', :because => 'both install a js binary'
