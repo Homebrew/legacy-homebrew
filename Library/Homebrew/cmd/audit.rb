3caaacba4976a1b96fc45f7e27e153c45418ff32
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-03-16 23:55:11 +0100

    audit: enforce https for apache.org
    - including all apache.org subdomains
    - for `homepage` as well

--- Library/Homebrew/cmd/audit.rb
+++ Library/Homebrew/cmd/audit.rb
@@ -300,6 +300,9 @@ class FormulaAuditor
     if homepage =~ %r[^http://[^/]*github\.io/]
       problem "Github Pages links should be https:// (URL is #{homepage})."
     end
+    if homepage =~ %r[^http://[^/]*\.apache\.org/]
+      problem "Apache homepages should be https:// links (URL is #{homepage})."
+    end
 
     # There's an auto-redirect here, but this mistake is incredibly common too.
     # Only applies to the homepage and subdomains for now, not the FTP links.
@@ -326,8 +329,8 @@ class FormulaAuditor
       case p
       when %r[^http://ftp\.gnu\.org/]
         problem "ftp.gnu.org urls should be https://, not http:// (url is #{p})."
-      when %r[^http://archive\.apache\.org/]
-        problem "archive.apache.org urls should be https://, not http (url is #{p})."
+      when %r[^http://[^/]*\.apache\.org/]
+        problem "Apache urls should be https://, not http (url is #{p})."
       when %r[^http://code\.google\.com/]
         problem "code.google.com urls should be https://, not http (url is #{p})."
       when %r[^http://fossies\.org/]
