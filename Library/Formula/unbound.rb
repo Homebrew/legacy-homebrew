aa6c5708b213b4ec6ef063eb5e86472e8c251852
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-03-16 23:41:37 +0100

    unbound: use https url and homepage

--- Library/Formula/unbound.rb
+++ Library/Formula/unbound.rb
@@ -1,8 +1,8 @@
 require "formula"
 
 class Unbound < Formula
-  homepage "http://www.unbound.net"
-  url "http://unbound.net/downloads/unbound-1.5.3.tar.gz"
+  homepage "https://www.unbound.net"
+  url "https://unbound.net/downloads/unbound-1.5.3.tar.gz"
   sha256 "76bdc875ed4d1d3f8e4cfe960e6df78ee5c6c7c18abac11331cf93a7ae129eca"
 
   depends_on "openssl"
