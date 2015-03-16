f06296815dc93d8ac3de785cddf2ad874960c43a
Nicolas Brousse <nicolas@brousse.info>
2015-03-15 20:02:14 -0700

    update pigz version to 2.3.3
    Change-Id: Ibadf202f334b15a98ca787ca06a3e32665bd9a87

--- Library/Formula/pigz.rb
+++ Library/Formula/pigz.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pigz < Formula
   homepage 'http://www.zlib.net/pigz/'
-  url 'http://www.zlib.net/pigz/pigz-2.2.4.tar.gz'
-  md5 '9df2a3c742524446fa4e797c17e8fd85'
+  url 'http://www.zlib.net/pigz/pigz-2.3.3.tar.gz'
+  sha1 '11252d38fe2a7b8d7a712dff22bbb7630287d00b'
 
   def install
     system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
