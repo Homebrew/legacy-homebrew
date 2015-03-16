8a9edb9a4f0e911a8993fda8887b3cc9780a0d45
Patrick <pdgwien@gmail.com>
2015-03-16 09:30:03 +0100

    task 2.4.2
    Closes #37760.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -1,8 +1,8 @@
 class Task < Formula
   homepage "http://www.taskwarrior.org/"
-  url "http://taskwarrior.org/download/task-2.4.1.tar.gz"
-  sha1 "4882628ac339e31fcb0fef2f4a1c585e86eb95c7"
-  head "https://git.tasktools.org/scm/tm/task.git", :branch => "2.4.2", :shallow => false
+  url "http://taskwarrior.org/download/task-2.4.2.tar.gz"
+  sha1 "94699f518150e736e8d5ad2b5b7ccf8c988ed4d0"
+  head "https://git.tasktools.org/scm/tm/task.git", :branch => "2.4.3", :shallow => false
 
   bottle do
     revision 1
