560a6b175e72ded05e0d0445c8407b3fc88518b5
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-17 00:17:26 +0100

    flasm: test added

--- Library/Formula/flasm.rb
+++ Library/Formula/flasm.rb
@@ -1,13 +1,25 @@
-require 'formula'
-
 class Flasm < Formula
-  homepage 'http://www.nowrap.de/flasm.html'
-  url 'http://www.nowrap.de/download/flasm16src.zip'
-  sha1 '3b383fa042eae414c5e5608bfa91a42f44bd1a86'
-  version '1.62'
+  homepage "http://www.nowrap.de/flasm.html"
+  url "http://www.nowrap.de/download/flasm16src.zip"
+  sha256 "df1273a506e2479cf95775197f5b7fa94e29fe1e0aae5aa190ed5bbebc4be5c6"
+  version "1.62"
 
   def install
     system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
     bin.install "flasm"
+  end
+
+  test do
+    (testpath/"test").write <<-EOS.undent
+      constants 'a', 'b'
+      push 'a', 'b'
+      getVariable
+      push 'b'
+      getVariable
+      multiply
+      setVariable
+    EOS
+
+    system "#{bin}/flasm", "-b", "test"
   end
 end
