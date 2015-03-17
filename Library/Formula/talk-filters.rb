f7edf420b772b3fc688e2126656c5a88701390d7
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-17 22:51:22 +0100

    talk-filters: test added
    Closes #37815.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/talk-filters.rb
+++ Library/Formula/talk-filters.rb
@@ -1,13 +1,17 @@
-require 'formula'
-
 class TalkFilters < Formula
-  homepage 'http://www.hyperrealm.com/main.php?s=talkfilters'
-  url 'http://www.hyperrealm.com/talkfilters/talkfilters-2.3.8.tar.gz'
-  sha1 '4aafacfe8842889531ad5a37402b06813c32fc64'
+  homepage "http://www.hyperrealm.com/main.php?s=talkfilters"
+  url "http://www.hyperrealm.com/talkfilters/talkfilters-2.3.8.tar.gz"
+  sha256 "4681e71170af06c6bffcd4e454eff67224cde06f0d678d26dc72da45f02ecca6"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "MKDIR_P=mkdir -p", "install"
+  end
+
+  test do
+    assert_equal "hellu Humebroo",
+      pipe_output("#{bin}/chef", "hello Homebrew", 0)
   end
 end
