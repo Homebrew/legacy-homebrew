5f25b9317b8dac1e4896b32efe2b8ffe2479b77a
Alexander Graefe <alex@codemo.de>
2015-03-17 07:40:07 +0100

    multimarkdown 4.7.1
    Closes #37786.
    Closes #37583.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/multimarkdown.rb
+++ Library/Formula/multimarkdown.rb
@@ -3,7 +3,8 @@ require 'formula'
 class Multimarkdown < Formula
   homepage 'http://fletcherpenney.net/multimarkdown/'
   # Use git tag instead of the tarball to get submodules
-  url 'https://github.com/fletcher/MultiMarkdown-4.git', :tag => '4.6'
+  url "https://github.com/fletcher/MultiMarkdown-4.git", :tag => "4.7.1",
+     :revision => "3083076038cdaceb666581636ef9e1fc68472ff0"
   head 'https://github.com/fletcher/MultiMarkdown-4.git'
 
   bottle do
@@ -18,6 +19,7 @@ class Multimarkdown < Formula
   def install
     ENV.append 'CFLAGS', '-g -O3 -include GLibFacade.h'
     system "make"
+    rm_f Dir["scripts/*.bat"]
     bin.install 'multimarkdown', Dir['scripts/*']
     prefix.install 'Support'
   end
