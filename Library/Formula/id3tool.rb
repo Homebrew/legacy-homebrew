937fd4811a97de96361640e13b98783f900c09c6
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-17 16:21:39 +0100

    id3tool: test added
    Closes #37798.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/id3tool.rb
+++ Library/Formula/id3tool.rb
@@ -1,14 +1,22 @@
-require 'formula'
-
 class Id3tool < Formula
-  homepage 'http://nekohako.xware.cx/id3tool/'
-  url 'http://nekohako.xware.cx/id3tool/id3tool-1.2a.tar.gz'
-  sha1 '23ce1b33c44290e72a0520c07bc43d1dd4cc7886'
+  homepage "http://nekohako.xware.cx/id3tool/"
+  url "http://nekohako.xware.cx/id3tool/id3tool-1.2a.tar.gz"
+  sha256 "7908d66c5aabe2a53ae8019e8234f4231485d80be4b2fe72c9d04013cff1caec"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    mp3 = "#{testpath}/test.mp3"
+    cp test_fixtures("test.mp3"), mp3
+
+    system "#{bin}/id3tool", "-t", "Homebrew", mp3
+    assert_match(/Song Title:\s+Homebrew/,
+                 shell_output("#{bin}/id3tool #{mp3}").chomp)
   end
 end
