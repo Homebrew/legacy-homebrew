f4d90370c533d8549ea5bf72e39a674dde5af36e
Frederik De Bleser <frederik@debleser.be>
2015-03-17 12:43:57 +0100

    Remove redundant version number.

--- Library/Formula/hackrf.rb
+++ Library/Formula/hackrf.rb
@@ -1,7 +1,6 @@
 class Hackrf < Formula
   homepage "https://github.com/mossmann/hackrf"
   url "https://github.com/mossmann/hackrf/archive/v2014.08.1.tar.gz"
-  version "2014.08.1"
   sha256 "5ab1641a9af766c476e04bfe2f7cbe3d7edd22c324453c22e58e3f0ef51082eb"
 
   depends_on "cmake" => :build
