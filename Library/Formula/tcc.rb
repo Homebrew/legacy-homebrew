48a8c7de93352d7cd467abb65584eb31821b9647
Joel Gibson <joel@jgibson.id.au>
2015-03-17 16:00:57 +1100

    tcc: add cross option
    Adds a --with-cross option to enable the cross compilers.

--- Library/Formula/tcc.rb
+++ Library/Formula/tcc.rb
@@ -5,11 +5,19 @@ class Tcc < Formula
   url 'http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.26.tar.bz2'
   sha1 '7110354d3637d0e05f43a006364c897248aed5d0'
 
+  option "with-cross", "Build all cross compilers"
+
   def install
+    args = %W[
+      --prefix=#{prefix}
+      --source-path=#{buildpath}
+      --sysincludepaths=/usr/local/include:#{MacOS.sdk_path}/usr/include:{B}/include
+    ]
+
+    args << "--enable-cross" if build.with? "cross"
+
     ENV.j1
-    system "./configure", "--prefix=#{prefix}",
-                          "--source-path=#{buildpath}",
-                          "--sysincludepaths=/usr/local/include:#{MacOS.sdk_path}/usr/include:{B}/include"
+    system "./configure", *args
     system "make"
     system "make", "install"
     system "make", "test"
