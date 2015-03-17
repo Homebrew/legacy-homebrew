5a74f4a7750018f51d1ee38daaffe31db129a4e5
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-17 22:28:29 +0100

    pound 2.7
    Closes #37812.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/pound.rb
+++ Library/Formula/pound.rb
@@ -1,20 +1,38 @@
-require 'formula'
-
 class Pound < Formula
-  homepage 'http://www.apsis.ch/pound'
-  url 'http://www.apsis.ch/pound/Pound-2.6.tgz'
-  sha1 '91ba84c6db579b06dc82fceb790e55e344b1dc40'
+  homepage "http://www.apsis.ch/pound"
+  url "http://www.apsis.ch/pound/Pound-2.7.tgz"
+  sha256 "cdfbf5a7e8dc8fbbe0d6c1e83cd3bd3f2472160aac65684bb01ef661c626a8e4"
 
-  depends_on 'pcre'
+  depends_on "openssl"
+  depends_on "pcre"
+  depends_on "google-perftools" => :recommended
 
   def install
     # find Homebrew's libpcre
-    ENV.append 'LDFLAGS', "-L#{HOMEBREW_PREFIX}/lib"
+    ENV.append "LDFLAGS", "-L#{Formula["pcre"].lib}"
 
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-tcmalloc"
     system "make"
     # Manual install to get around group issues
     sbin.install "pound", "poundctl"
     man8.install "pound.8", "poundctl.8"
+  end
+
+  test do
+    (testpath/"pound.cfg").write <<-EOS.undent
+      ListenHTTP
+        Address 1.2.3.4
+        Port    80
+        Service
+          HeadRequire "Host: .*www.server0.com.*"
+          BackEnd
+            Address 192.168.0.10
+            Port    80
+          End
+        End
+      End
+    EOS
+
+    system "#{sbin}/pound", "-f", "#{testpath}/pound.cfg", "-c"
   end
 end
