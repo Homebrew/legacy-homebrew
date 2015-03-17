35e22f0add1da811e7531779c867e64579044fb1
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-17 18:01:12 +0000

    musepack: add r475 bottle.

--- Library/Formula/musepack.rb
+++ Library/Formula/musepack.rb
@@ -4,6 +4,13 @@ class Musepack < Formula
   version "r475"
   sha256 "a4b1742f997f83e1056142d556a8c20845ba764b70365ff9ccf2e3f81c427b2b"
 
+  bottle do
+    cellar :any
+    sha256 "2e6bad894063f1178ff43b5cd10a899b82baa757228c3a51488793a4a22acdf2" => :yosemite
+    sha256 "4ff9d23d8631300ff2619d9f1a7d99e07f8636c6e8d8db8dbf1335a21c1f7e27" => :mavericks
+    sha256 "ac50f1100cdb2c91846a5d5a8d364cdb31abaeb7823ccbfc6614829f96540c6d" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "libcuefile"
   depends_on "libreplaygain"
