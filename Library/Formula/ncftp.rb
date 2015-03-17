95007829e55a98eec48ae61eca8bd4b0b9b619b8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-17 16:45:07 +0000

    ncftp: add 3.2.5 bottle.

--- Library/Formula/ncftp.rb
+++ Library/Formula/ncftp.rb
@@ -3,6 +3,12 @@ class Ncftp < Formula
   url "ftp://ftp.ncftp.com/ncftp/ncftp-3.2.5-src.tar.gz"
   sha256 "ac111b71112382853b2835c42ebe7bd59acb7f85dd00d44b2c19fbd074a436c4"
 
+  bottle do
+    sha256 "50fb345b28bc8a20d2877d67108f87c9544568de9e27ae7a3545da3af3cb0b35" => :yosemite
+    sha256 "07a73c0ac7005566895f7e42f4a1d1b8295a9cdc03ec7986028ae783313e428f" => :mavericks
+    sha256 "e59965dc867f70420046475b30829cad0b334bc687e18362918fdfbab7521b59" => :mountain_lion
+  end
+
   def install
     # "disable universal" doesn't seem to work.
     # If ncftp detects the 10.4 SDK, it will try to use GCC 4.0 which doesn't
