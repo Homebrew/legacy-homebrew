13b7bf4f206e7b506fe6f3269b9b1937409b2087
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-17 03:23:52 +0000

    fdk-aac: add 0.1.4 bottle.

--- Library/Formula/fdk-aac.rb
+++ Library/Formula/fdk-aac.rb
@@ -3,6 +3,13 @@ class FdkAac < Formula
   url "https://downloads.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-0.1.4.tar.gz"
   sha256 "5910fe788677ca13532e3f47b7afaa01d72334d46a2d5e1d1f080f1173ff15ab"
 
+  bottle do
+    cellar :any
+    sha256 "69c1659f65a9ca7644bf81ac4a8833ad23f63239c9f4cc14ab39bc203dbe2c68" => :yosemite
+    sha256 "6b4ea8d82e310acb425325ad19d79ad039c3a3c88db0534e59d0fd973c16d058" => :mavericks
+    sha256 "6288e9b2d40ea937f7964aa0ea86e99884d8f7729a8f57429ec51cdb53b3ca4f" => :mountain_lion
+  end
+
   head do
     url "git://opencore-amr.git.sourceforge.net/gitroot/opencore-amr/fdk-aac"
 
