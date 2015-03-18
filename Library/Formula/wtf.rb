17112ae34f2c4e748ba9737cd1539fa01bae07f9
Vlad Shablinsky <vladshablinsky@gmail.com>
2015-03-18 15:59:05 +0300

    wtf: sha256, test do

--- Library/Formula/wtf.rb
+++ Library/Formula/wtf.rb
@@ -1,12 +1,16 @@
 class Wtf < Formula
   homepage "http://cvsweb.netbsd.org/bsdweb.cgi/src/games/wtf/"
   url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20141212.tar.gz"
-  sha1 "406af25912e948e02e68034415292ca3500e8f02"
+  sha256 "31ee95558aee74f76d9e531fb2489765eb51c963b7b514cc07f8b407baf7f2a2"
 
   def install
     inreplace %w[wtf wtf.6], "/usr/share", share
     bin.install "wtf"
     man6.install "wtf.6"
     (share+"misc").install %w[acronyms acronyms.comp]
+  end
+
+  test do
+    system bin/"wtf", "needle"
   end
 end
