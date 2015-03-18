b1492e23de4382b56fd6248958ed2e4d9c8753eb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-18 05:11:10 +0000

    cless: add 0.2.0.0 bottle.

--- Library/Formula/cless.rb
+++ Library/Formula/cless.rb
@@ -7,6 +7,13 @@ class Cless < Formula
   url "https://hackage.haskell.org/package/cless-0.2.0.0/cless-0.2.0.0.tar.gz"
   sha256 "d6cbeef102f908109c1434dadf7adfda8adbec0d6de6d2f04db3ff8274460683"
 
+  bottle do
+    cellar :any
+    sha256 "bddfbbaaafa58c953f4724829c64f84d8f0b3afb1c0e31dae14f6fa45d9a07c3" => :yosemite
+    sha256 "1611af00bd755a49e9109a17d847491b455af9333e9d140ddfbf1b4a1bd25ea3" => :mavericks
+    sha256 "dee0b7d120b98b0107c8f480bb775e036a3411bb2cc9394a706c0f4ce193c112" => :mountain_lion
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
