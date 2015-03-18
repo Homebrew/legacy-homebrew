9ffd2cd6181d734dc0974e931c8085f482dbc1b5
Vlad Shablinsky <vladshablinsky@gmail.com>
2015-03-18 16:03:42 +0300

    apg: sha256, test do, remove require

--- Library/Formula/apg.rb
+++ Library/Formula/apg.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Apg < Formula
-  homepage 'http://www.adel.nursat.kz/apg/'
-  url 'http://www.adel.nursat.kz/apg/download/apg-2.2.3.tar.gz'
-  sha1 '7bdbc931ef8477717186dc3ab3a2d3c25012b4ca'
+  homepage "http://www.adel.nursat.kz/apg/"
+  url "http://www.adel.nursat.kz/apg/download/apg-2.2.3.tar.gz"
+  sha256 "69c9facde63958ad0a7630055f34d753901733d55ee759d08845a4eda2ba7dba"
 
   def install
     system "make", "standalone",
@@ -11,7 +9,11 @@ class Apg < Formula
                    "FLAGS=#{ENV.cflags}",
                    "LIBS=", "LIBM="
 
-    bin.install 'apg', 'apgbfm'
-    man1.install 'doc/man/apg.1', 'doc/man/apgbfm.1'
+    bin.install "apg", "apgbfm"
+    man1.install "doc/man/apg.1", "doc/man/apgbfm.1"
+  end
+
+  test do
+    system bin/"apg", "-a", "1", "-M", "n", "-n", "3", "-m", "8", "-E", "23456789"
   end
 end
