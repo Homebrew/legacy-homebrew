e4665124daa148f2ff55d598f7e7e5e22d2bf05d
Alexander Altman <alexanderaltman@me.com>
2015-03-17 18:37:03 -0700

    mighttpd2 3.2.4
    Also updates and simplified the formula.
    
    Closes #37822.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mighttpd2.rb
+++ Library/Formula/mighttpd2.rb
@@ -1,12 +1,11 @@
-require "formula"
 require "language/haskell"
 
 class Mighttpd2 < Formula
   include Language::Haskell::Cabal
 
-  homepage "http://mew.org/~kazu/proj/mighttpd/en/"
-  url "http://hackage.haskell.org/package/mighttpd2-3.2.0/mighttpd2-3.2.0.tar.gz"
-  sha1 "878a9e03ad3a62221dab22cb6412ae446368e1bd"
+  homepage "http://www.mew.org/~kazu/proj/mighttpd/en/"
+  url "http://hackage.haskell.org/package/mighttpd2-3.2.4/mighttpd2-3.2.4.tar.gz"
+  sha256 "3b99ac25a07b8329f6111611a8fa0278d021323d28a4489d0f3ef4fd79042568"
 
   bottle do
     sha1 "3bcfd785e9a7d222a12902e962862813593364c5" => :mavericks
@@ -19,11 +18,7 @@ class Mighttpd2 < Formula
   depends_on "gmp"
 
   def install
-    cabal_sandbox do
-      cabal_install "--only-dependencies"
-      cabal_install "--prefix=#{prefix}"
-    end
-    cabal_clean_lib
+    install_cabal_package
   end
 
   test do
