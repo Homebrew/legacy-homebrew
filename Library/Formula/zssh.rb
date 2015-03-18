474f06043948411b1fb17aa53c9b32d4cce4955c
Vlad Shablinsky <vladshablinsky@gmail.com>
2015-03-18 15:58:29 +0300

    zssh: sha256

--- Library/Formula/zssh.rb
+++ Library/Formula/zssh.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class Zssh < Formula
-  homepage 'http://zssh.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/zssh/zssh/1.5/zssh-1.5c.tgz'
-  sha1 '68dc9b8572646ef63909b3855e7990d75f49926c'
+  homepage "http://zssh.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/zssh/zssh/1.5/zssh-1.5c.tgz"
+  sha256 "a2e840f82590690d27ea1ea1141af509ee34681fede897e58ae8d354701ce71b"
 
-  depends_on 'lrzsz'
+  depends_on "lrzsz"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
