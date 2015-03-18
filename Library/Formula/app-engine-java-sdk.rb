ded18e5820785de12e837eb0bdb5971c230a5ceb
ntalbs <ntalbsen@gmail.com>
2015-03-18 16:32:41 +0900

    app-engine-java-sdk 1.9.18
    Update app-engine-java-sdk.rb
    
    Update app-engine-ava-sdk version to 1.9.18.
    
    Closes #37827.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/app-engine-java-sdk.rb
+++ Library/Formula/app-engine-java-sdk.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class AppEngineJavaSdk < Formula
   homepage "https://developers.google.com/appengine/docs/java/gettingstarted/introduction"
-  url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.17.zip"
-  sha1 "ca0bbae7e6c24dfe90cc06be384d594840dd2251"
+  url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.18.zip"
+  sha1 "7f554498a7320192186fc8c3e4ad3f1aaab4d6dd"
 
   def install
     rm Dir["bin/*.cmd"]
