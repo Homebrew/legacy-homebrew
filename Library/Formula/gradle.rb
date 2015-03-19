9aa1c22154df073f1c1db6c4cc80a0088a5ad0d9
Artur Dryomov <artur.dryomov@gmail.com>
2015-03-19 22:31:49 +0300

    Modernize Gradle formula.

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Gradle < Formula
-  homepage 'http://www.gradle.org/'
-  url 'https://downloads.gradle.org/distributions/gradle-2.3-bin.zip'
-  sha1 'db857319150aa8f90cfcc1df7991c0ef4bcd180a'
+  homepage "http://www.gradle.org/"
+  url "https://downloads.gradle.org/distributions/gradle-2.3-bin.zip"
+  sha256 "010dd9f31849abc3d5644e282943b1c1c355f8e2635c5789833979ce590a3774"
 
   def install
     libexec.install %w[bin lib]
-    bin.install_symlink libexec+'bin/gradle'
+    bin.install_symlink libexec+"bin/gradle"
   end
 end
