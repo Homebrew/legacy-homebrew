a78f6ed513b5013c6f45af98dd0fa08f3d70f655
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-19 22:16:46 +0100

    dhcping: homepage fixed

--- Library/Formula/dhcping.rb
+++ Library/Formula/dhcping.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Dhcping < Formula
-  homepage 'http://www.mavetju.org'
-  url 'http://www.mavetju.org/download/dhcping-1.2.tar.gz'
-  sha1 '97927886adc1e5b3a67c55f9010a918e2e880f1e'
+  homepage "http://www.mavetju.org/unix/general.php"
+  url "http://www.mavetju.org/download/dhcping-1.2.tar.gz"
+  sha256 "32ef86959b0bdce4b33d4b2b216eee7148f7de7037ced81b2116210bc7d3646a"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
   end
 end
