19f591d5bbb0db5cec940da5cc04ee5c8cc185f3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-19 11:07:54 +0000

    fmdiff: add 20120813 bottle.

--- Library/Formula/fmdiff.rb
+++ Library/Formula/fmdiff.rb
@@ -3,6 +3,12 @@ class Fmdiff < Formula
   url "http://bruno.defraine.net/fmdiff/fmscripts-20120813.tar.gz"
   sha256 "7312654040acc29787c15fb2b6d53abe0a397fb9faec6bf43398d25bb31f38ee"
 
+  bottle do
+    sha256 "a713fc8fb048aaaac4dcac2a79933a54ff229d5ede4024a10399255f2114c502" => :yosemite
+    sha256 "bbce86236cc094efb560d2f4492bb35e49ff45aefc99d063192232ea4f6a6fcb" => :mavericks
+    sha256 "6ed6345bf6d69a8d33f6f6f72c836088ed10deaf397f8d2742059f810c14aaa2" => :mountain_lion
+  end
+
   head "http://soft.vub.ac.be/svn-gen/bdefrain/fmscripts/", :using => :svn
 
   def install
