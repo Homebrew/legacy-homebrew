797c7625222eeb6fe08032e1af82ef23ca7ef7e7
George Gastaldi <gegastaldi@gmail.com>
2015-03-19 17:06:42 -0300

    jboss-forge 2.15.2.Final
    Closes #37891.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class JbossForge < Formula
   homepage 'http://forge.jboss.org/'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.15.1.Final&e=zip&c=offline'
-  version '2.15.1.Final'
-  sha1 '91fd0c0aa231444f1bf58e424346f70d42ac9dee'
+  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.15.2.Final&e=zip&c=offline'
+  version '2.15.2.Final'
+  sha1 'f0931c94a47274985eb801523c69556aa999a135'
 
   def install
     rm_f Dir["bin/*.bat"]
