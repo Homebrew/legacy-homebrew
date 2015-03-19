bcbd94a509ac6e073b346dbe2e020277d4279cc8
Artur Dryomov <artur.dryomov@gmail.com>
2015-03-19 22:44:59 +0300

    Modernize pidcat formula.

--- Library/Formula/pidcat.rb
+++ Library/Formula/pidcat.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Pidcat < Formula
   homepage "https://github.com/JakeWharton/pidcat"
   head "https://github.com/JakeWharton/pidcat.git"
   url "https://github.com/JakeWharton/pidcat/archive/1.4.1.tar.gz"
-  sha1 "89f806ae1fa3375ce188851c8c95fc1097467b82"
+  sha256 "5e4d340b9b89cc0c23562714acacd6ccb56c509859907782d6a0ced44b170b3d"
 
   def install
     bin.install "pidcat.py" => "pidcat"
