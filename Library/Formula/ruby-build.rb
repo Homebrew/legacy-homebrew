81d396f94e5256fd8200e6808f97d2c5321a2a01
Erik Michaels-Ober <sferik@gmail.com>
2015-03-19 18:37:56 -0700

    ruby-build 20150319zf

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -3,8 +3,9 @@ require "formula"
 class RubyBuild < Formula
   head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  url "https://github.com/sstephenson/ruby-build/archive/v20150303.tar.gz"
-  sha1 "c15c1f6613d6c518544ed0d90a0cf33eefc7d73f"
+  sha1 "31c2e082b3fc82e90f86ff787184ecf0f9ace659"
+  url "https://github.com/sstephenson/ruby-build/archive/v20150319zf.tar.gz"
+  version "20150319zf"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
