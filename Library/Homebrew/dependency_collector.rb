25ce151e55209b4a8ca5959e7c691b8f4a6918fc
Tanachat Nilanon <gor-tanachat@users.noreply.github.com>
2015-03-19 18:22:57 -0700

    add :python3 to LANGUAGE_MODULES
    Add special :python3 dependency tag to LANGUAGE_MODULES in
    dependency_collector.
    
    Closes #37898.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Homebrew/dependency_collector.rb
+++ Library/Homebrew/dependency_collector.rb
@@ -18,7 +18,7 @@ require 'set'
 class DependencyCollector
   # Define the languages that we can handle as external dependencies.
   LANGUAGE_MODULES = Set[
-    :chicken, :jruby, :lua, :node, :ocaml, :perl, :python, :rbx, :ruby
+    :chicken, :jruby, :lua, :node, :ocaml, :perl, :python, :python3, :rbx, :ruby
   ].freeze
 
   CACHE = {}
