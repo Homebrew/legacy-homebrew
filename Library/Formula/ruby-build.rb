0367e7e112e5babb38bf2eed94b6a8fad7ac1329
Mike McQuaid <mike@mikemcquaid.com>
2015-03-20 11:07:09 +0000

    ruby-build: tweak layout, use sha256.

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -1,11 +1,12 @@
 require "formula"
 
 class RubyBuild < Formula
-  head "https://github.com/sstephenson/ruby-build.git"
   homepage "https://github.com/sstephenson/ruby-build"
-  sha1 "31c2e082b3fc82e90f86ff787184ecf0f9ace659"
   url "https://github.com/sstephenson/ruby-build/archive/v20150319zf.tar.gz"
+  sha256 "5c6c34aad60df2b93e30fc2263fa400bfe20cbd4e2ceaf497431fb3568bdb738"
   version "20150319zf"
+
+  head "https://github.com/sstephenson/ruby-build.git"
 
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
