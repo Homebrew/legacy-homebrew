13a81c0a532a536d804fcd42fe8fa3d59dd2fa0b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-21 08:32:58 +0000

    ps2eps: add 1.68 bottle.

--- Library/Formula/ps2eps.rb
+++ Library/Formula/ps2eps.rb
@@ -3,6 +3,13 @@ class Ps2eps < Formula
   url "https://www.tm.uka.de/~bless/ps2eps-1.68.tar.gz"
   sha256 "b08f12eed88965d1891261fb70e87c7e3a3f3172ebc31bdb7994a7ce854dd925"
 
+  bottle do
+    cellar :any
+    sha256 "99b3838d2a7135d8794e4f48e428bd8afc0f18db8998f071c74faa449591ad7f" => :yosemite
+    sha256 "01fbee92f6a8534a4618bb94b9d21913f203b42f7abe41023c7c2b2f68775880" => :mavericks
+    sha256 "4671a8ae732598cbf5c006b7cf6f9924455a8f61dcc660733e14104707974c27" => :mountain_lion
+  end
+
   depends_on "ghostscript"
 
   def install
