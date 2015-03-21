57039544437fffe1dc65585cab78f92dbcb8eb31
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-21 20:34:55 +0000

    gexiv2: add 0.10.2 bottle.

--- Library/Formula/gexiv2.rb
+++ Library/Formula/gexiv2.rb
@@ -3,6 +3,13 @@ class Gexiv2 < Formula
   url "https://download.gnome.org/sources/gexiv2/0.10/gexiv2-0.10.2.tar.xz"
   sha256 "2fd21f0ed5125e51d02226e7f41be751cfa8ae411a8ed1a651e16b06d79047b2"
 
+  bottle do
+    cellar :any
+    sha256 "2919516539dca1635b6e680c9065ee6617b497a0c38d0268433d47e0a6e17365" => :yosemite
+    sha256 "10edd0c5a927dfeb9e2a6c3dcae5c64fc36f817cd701bcaf36b41b78c42d4370" => :mavericks
+    sha256 "76c9f4629361d6409775d40276cf868216cbf0fbc8f061fc1bc208e73eab3509" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "exiv2"
