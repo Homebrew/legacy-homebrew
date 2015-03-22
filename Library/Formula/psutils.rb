28331040cf9171e15325670d4e5b7a71c7f297b0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-22 20:54:04 +0000

    psutils: add p17 bottle.

--- Library/Formula/psutils.rb
+++ Library/Formula/psutils.rb
@@ -4,6 +4,12 @@ class Psutils < Formula
   version "p17"
   sha256 "3853eb79584ba8fbe27a815425b65a9f7f15b258e0d43a05a856bdb75d588ae4"
 
+  bottle do
+    sha256 "44a9141909d2590f8e6b7ffcd48451ad3bce52bcfb7d2bf0f0c5a567c9f1705b" => :yosemite
+    sha256 "4be42891f539aa49bdbc1fc75d07defecbd9ca990bf39d5ec7a4a8187fc3efc5" => :mavericks
+    sha256 "73d8ddfe85026f715b8f3ade892b4eacca594f48921625b937d6091c32ed6628" => :mountain_lion
+  end
+
   def install
     # This is required, because the makefile expects that its man folder exists
     man1.mkpath
