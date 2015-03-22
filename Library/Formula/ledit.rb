aae242a3d43a8b4cb260096032a48a82596c37d2
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-22 21:56:24 +0100

    ledit: test added
    Closes #37972.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ledit.rb
+++ Library/Formula/ledit.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Ledit < Formula
-  homepage 'http://pauillac.inria.fr/~ddr/ledit/'
-  url 'http://pauillac.inria.fr/~ddr/ledit/distrib/src/ledit-2.03.tgz'
-  sha1 '8fef728f38e8d6fc30dd5f71dd5b6b647212a43a'
+  homepage "http://pauillac.inria.fr/~ddr/ledit/"
+  url "http://pauillac.inria.fr/~ddr/ledit/distrib/src/ledit-2.03.tgz"
+  sha256 "ce08a8568c964009ccb0cbba45ae78b9a96c823f42a4fd61431a5b0c2c7a19ce"
 
-  depends_on 'objective-caml'
-  depends_on 'camlp5'
+  depends_on "objective-caml"
+  depends_on "camlp5"
 
   def install
     # like camlp5, this build fails if the jobs are parallelized
@@ -14,5 +12,12 @@ class Ledit < Formula
     args = %W[BINDIR=#{bin} LIBDIR=#{lib} MANDIR=#{man}]
     system "make", *args
     system "make", "install", *args
+  end
+
+  test do
+    history = testpath/"history"
+    pipe_output("#{bin}/ledit -x -h #{history} bash", "exit\n", 0)
+    assert history.exist?
+    assert_equal "exit\n", history.read
   end
 end
