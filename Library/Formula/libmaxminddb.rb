13cd86f2cbe4e0de5163af24c4d414de95ebb9cf
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-22 17:10:25 +0000

    libmaxminddb: update 1.0.4 bottle.

--- Library/Formula/libmaxminddb.rb
+++ Library/Formula/libmaxminddb.rb
@@ -5,10 +5,9 @@ class Libmaxminddb < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha1 "9ef726ff11d9933e9aa1df2ce8adc21493714259" => :yosemite
-    sha1 "0e3a37a3ad55d27220ec49ee653016ecc649d952" => :mavericks
-    sha1 "3888352b39e903d2f30d6947db0c041ecaa3540a" => :mountain_lion
+    sha256 "f923147f3aacda41439e72737c22899356b11d61f2c9628839bac361d365a57e" => :yosemite
+    sha256 "ffc9884a7818e4b31b0a4d7e7a2d9edb10de783039b3ee4a20084a91373447ea" => :mavericks
+    sha256 "1c3da6449a58c7f2840b51dd0de405b1d27a6d3c8ae211d0a6f38a21a17f780a" => :mountain_lion
   end
 
   head do
