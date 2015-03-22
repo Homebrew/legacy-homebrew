0c21e399339fee5ecdd4703106c595bc504a7634
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-22 22:55:09 +0100

    avce00: lib installed, test added

--- Library/Formula/avce00.rb
+++ Library/Formula/avce00.rb
@@ -1,12 +1,19 @@
-require 'formula'
-
 class Avce00 < Formula
-  homepage 'http://avce00.maptools.org/avce00/index.html'
-  url 'http://avce00.maptools.org/dl/avce00-2.0.0.tar.gz'
-  sha1 '2948d9b1cfb6e80faf2e9b90c86fd224617efd75'
+  homepage "http://avce00.maptools.org/avce00/index.html"
+  url "http://avce00.maptools.org/dl/avce00-2.0.0.tar.gz"
+  sha256 "c0851f86b4cd414d6150a04820491024fb6248b52ca5c7bd1ca3d2a0f9946a40"
+
+  conflicts_with "gdal", :because => "both install a cpl_conv.h header"
 
   def install
     system "make", "CC=#{ENV.cc}"
     bin.install "avcimport",  "avcexport", "avcdelete", "avctest"
+    lib.install "avc.a"
+    include.install Dir["*.h"]
+  end
+
+  test do
+    touch testpath/"test"
+    system "#{bin}/avctest", "-b", "test"
   end
 end
