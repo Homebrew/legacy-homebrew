f78cf86d97f91be479bef9dca3933d07834542e2
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-22 14:21:14 +0100

    psutils: modernize, use sha256

--- Library/Formula/psutils.rb
+++ Library/Formula/psutils.rb
@@ -1,13 +1,12 @@
-require 'formula'
-
 class Psutils < Formula
-  homepage 'http://knackered.org/angus/psutils/'
-  url 'ftp://ftp.knackered.org/pub/psutils/psutils-p17.tar.gz'
-  version 'p17'
-  sha1 '6f1ecb5846cffb644826a02bd9153fe5d6387a9b'
+  homepage "http://knackered.org/angus/psutils/"
+  url "ftp://ftp.knackered.org/pub/psutils/psutils-p17.tar.gz"
+  version "p17"
+  sha256 "3853eb79584ba8fbe27a815425b65a9f7f15b258e0d43a05a856bdb75d588ae4"
 
   def install
-    man1.mkpath # This is required, because the makefile expects that its man folder exists
+    # This is required, because the makefile expects that its man folder exists
+    man1.mkpath
     system "make", "-f", "Makefile.unix",
                          "PERL=/usr/bin/perl",
                          "BINDIR=#{bin}",
@@ -17,8 +16,9 @@ class Psutils < Formula
   end
 
   test do
-    system "sh -c '#{bin}/showchar Palatino B > test.ps'"
-    system "#{bin}/psmerge", "-omulti.ps", "test.ps", "test.ps", "test.ps", "test.ps"
+    system "sh", "-c", "#{bin}/showchar Palatino B > test.ps"
+    system "#{bin}/psmerge", "-omulti.ps", "test.ps", "test.ps",
+      "test.ps", "test.ps"
     system "#{bin}/psnup", "-n", "2", "multi.ps", "nup.ps"
     system "#{bin}/psselect", "-p1", "multi.ps", "test2.ps"
   end
