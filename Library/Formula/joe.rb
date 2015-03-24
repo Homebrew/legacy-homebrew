2846b893567b29f043b82db202da41d5f2e23c5a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-24 16:05:27 +0000

    youtube-dl: update 2015.03.24 bottle.

--- Library/Formula/joe.rb
+++ Library/Formula/joe.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Joe < Formula
   homepage 'http://joe-editor.sourceforge.net/index.html'
-  url 'https://sourceforge.net/projects/joe-editor/files/JOE%20sources/joe-4.0/joe-4.0.tar.gz'
+  url 'https://downloads.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.0/joe-4.0.tar.gz'
   sha1 'a51827c8c61c3cb09a038d8f6670efe84e144927'
 
   def install
