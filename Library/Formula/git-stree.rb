a356ddb12eb3ff86cb45e82c25893e60822c20a4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-24 12:35:25 +0000

    git-stree: add 0.4.0 bottle.

--- Library/Formula/git-stree.rb
+++ Library/Formula/git-stree.rb
@@ -4,6 +4,13 @@ class GitStree < Formula
   url "https://github.com/tdd/git-stree/archive/v0.4.0.tar.gz"
   sha256 "c04d19c8eb1a566a8e7f373d447ab7e3480ee95f94802fa957afeb22c4bed0ef"
 
+  bottle do
+    cellar :any
+    sha256 "268ae53b67f0f9063312492267a6eb54521f044127fdc69e148205aa43b8bd77" => :yosemite
+    sha256 "d81c6f2a7631446fc1cd85b25bc72d3900661d8e9cc3c58bee553b240bc93223" => :mavericks
+    sha256 "cfd20af0102baa792d35fded2bb5fd3a7ba505741d44bcd5c6b24d87be4d9925" => :mountain_lion
+  end
+
   def install
     bin.install "git-stree"
     bash_completion.install "git-stree-completion.bash" => "git-stree"
