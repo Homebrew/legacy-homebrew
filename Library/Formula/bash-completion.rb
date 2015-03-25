e01e0b94d6a3059382c34b5fa61d7bb8cafe7768
Mohak Shah <mohak@mohakshah.in>
2015-03-26 01:50:08 +0530

    bash-completion: Now using SHA256, removed `require 'formula'` and replaced `system "make install"` with `system "make", "install"`

--- Library/Formula/bash-completion.rb
+++ Library/Formula/bash-completion.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 # NOTE: version 2.0 is out, but it requires Bash 4, and OS X ships
 # with 3.2.48. See homebrew-versions for a 2.0 formula.
 class BashCompletion < Formula
   homepage 'http://bash-completion.alioth.debian.org/'
   url 'http://bash-completion.alioth.debian.org/files/bash-completion-1.3.tar.bz2'
   mirror 'http://pkgs.fedoraproject.org/repo/pkgs/bash-completion/bash-completion-1.3.tar.bz2/a1262659b4bbf44dc9e59d034de505ec/bash-completion-1.3.tar.bz2'
-  sha1 '6a46b93f44c56cc336632ab28d90c0595fbcc98f'
+  sha256 '8ebe30579f0f3e1a521013bcdd183193605dab353d7a244ff2582fb3a36f7bec'
 
   # Backports the following upstream patch from 2.x:
   # http://anonscm.debian.org/gitweb/?p=bash-completion/bash-completion.git;a=patch;h=50ae57927365a16c830899cc1714be73237bdcb2
@@ -23,7 +21,7 @@ class BashCompletion < Formula
     end
 
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
 
     unless (compdir/'brew_bash_completion.sh').exist?
       compdir.install_symlink HOMEBREW_CONTRIB/'brew_bash_completion.sh'
