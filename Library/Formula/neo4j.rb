88190bb618571be7d9bdbfd35225b3b94337d45f
mindrones <mindrones@users.noreply.github.com>
2015-03-25 08:40:35 +0100

    neo4j 2.2.0
    Closes #38048.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/neo4j.rb
+++ Library/Formula/neo4j.rb
@@ -2,21 +2,15 @@ require "formula"
 
 class Neo4j < Formula
   homepage "http://neo4j.org"
-  url "http://dist.neo4j.org/neo4j-community-2.1.7-unix.tar.gz"
-  sha1 "1827c318c019042c3ca2da89481f717f711d9aed"
-  version "2.1.7"
+  url "http://dist.neo4j.org/neo4j-community-2.2.0-unix.tar.gz"
+  sha1 "af6d05bfd1bf2ca9644bde2571d1b37a6fd972ab"
+  version "2.2.0"
 
   option "with-neo4j-shell-tools", "Add neo4j-shell-tools to the standard neo4j-shell"
 
   resource "neo4j-shell-tools" do
     url "http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_2.1.zip"
     sha1 "83011a6dcf1cb49ee609e973fdb61f32f765b224"
-  end
-
-  devel do
-    url "http://dist.neo4j.org/neo4j-community-2.2.0-RC01-unix.tar.gz"
-    sha1 "65165b83ee2ba91e9ba99cb2acce9ebcf7ad5434"
-    version "2.2.0-RC01"
   end
 
   def install
@@ -27,9 +21,7 @@ class Neo4j < Formula
     libexec.install Dir["*"]
 
     # Symlink binaries
-    bin.install_symlink Dir["#{libexec}/bin/neo4j{,-shell}"]
-
-    bin.install_symlink libexec/"bin/neo4j-import" if build.devel?
+    bin.install_symlink Dir["#{libexec}/bin/neo4j{,-shell,-import}"]
 
     # Eventually, install neo4j-shell-tools
     # omiting "opencsv-2.3.jar" because it already comes with neo4j (see libexec/lib)
