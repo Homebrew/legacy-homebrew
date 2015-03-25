ed50881157e3864c50622ccbc858f3712cf646b8
Felix Klein <me@felixklein.at>
2015-03-25 17:03:28 +0100

    Integrate suggestions into Formula - should be much better now.

--- Library/Formula/bazel.rb
+++ Library/Formula/bazel.rb
@@ -1,59 +1,21 @@
-#
-# Brew Formula for Google's Bazel
-# written by Felix Klein <me@felixklein.at>
-#
-
 class Bazel < Formula
   homepage "http://bazel.io/"
 
-  #
-  # Version: Bazel 0.0.2 (latest tag from GitHub)
-  # This version is quite old, however there are no newer tags available on GitHub.
-  #
-  url "https://github.com/google/bazel/archive/0.0.2.tar.gz"
+  head "https://github.com/google/bazel.git"
+
+  url "https://github.com/google/bazel.git", :tag => "0.0.2"
   sha256 "5865356e74622c248248174acd09f38c07f3983867d7e687fa5932f2216e84c0"
-  version "0.0.2"
 
-  #
-  # Use userpaths, otherwise the build script doesn't find the libarchive library
-  #
-  env :userpaths
-
-  #
-  # Dependencies
-  #
   depends_on "protobuf" => :build
   depends_on "libarchive"
 
-  #
-  # Install the latest version from GitHub, if the user passes the --HEAD argument
-  #
-  head do
-    url "https://github.com/google/bazel.git", :using => :git
-  end
-
-  #
-  # Set flags for optimization, and build with supplied build scripts
-  # Install the bazel binary
-  #
   def install
-    ENV.append_to_cflags "-Ofast -march=native -flto"
+    inreplace "compile.sh" do |s|
+      s.gsub! "$(brew --prefix 2>/dev/null)/Cellar/libarchive/*/include/archive.h 2>/dev/null | head -n1",
+              "#{Formula["libarchive"].opt_include}/archive.h | head -n1"
+    end
+
     system "./compile.sh"
-    system "./output/bazel help"
-    bin.install "./output/bazel"
-  end
 
-  #
-  # Warn the user that this is potentially unstable software
-  #
-  def caveats
-    "This software is still in Alpha, visit http://bazel.io/ for more information."
-  end
-
-  #
-  # Runn the supplied test script
-  #
-  test do
-    system "./bootstrap_test.sh test"
   end
 end
