bd322690d836bf3bc300661ed35b1658b62c971d
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-25 18:45:43 +0100

    bbcolors: test added
    Closes #38068.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/bbcolors.rb
+++ Library/Formula/bbcolors.rb
@@ -1,11 +1,14 @@
-require 'formula'
-
 class Bbcolors < Formula
-  homepage 'http://daringfireball.net/projects/bbcolors/'
-  url 'http://daringfireball.net/projects/downloads/bbcolors_1.0.1.zip'
-  sha1 'ce47e5ffbcafb01c21acdf242372f351215a80bf'
+  homepage "http://daringfireball.net/projects/bbcolors/"
+  url "http://daringfireball.net/projects/downloads/bbcolors_1.0.1.zip"
+  sha256 "6ea07b365af1eb5f7fb9e68e4648eec24a1ee32157eb8c4a51370597308ba085"
 
   def install
     bin.install "bbcolors"
+  end
+
+  test do
+    (testpath/"Library/Application Support/BBColors").mkpath
+    system "#{bin}/bbcolors", "-list"
   end
 end
