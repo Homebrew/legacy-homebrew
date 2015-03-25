441fdacbaaeb6435914e774deadef410cdc44d9f
Mike McQuaid <mike@mikemcquaid.com>
2015-03-25 08:57:07 +0000

    joe: fix strict audit, add test.

--- Library/Formula/joe.rb
+++ Library/Formula/joe.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Joe < Formula
-  homepage 'http://joe-editor.sourceforge.net/index.html'
-  url 'https://downloads.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.0/joe-4.0.tar.gz'
-  sha1 'a51827c8c61c3cb09a038d8f6670efe84e144927'
+  homepage "http://joe-editor.sourceforge.net/index.html"
+  url "https://downloads.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.0/joe-4.0.tar.gz"
+  sha256 "c556adff77fd97bf1b86198de6cb82e0b92cda18579c4fef6c83b608d2ed2915"
 
   bottle do
     sha256 "d6739911e38e9017999136d04c9b852110f2d625cd6048188d2618f072aaec0b" => :yosemite
@@ -13,7 +11,10 @@ class Joe < Formula
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
+  test do
+    system "#{bin}/stringify"
+  end
 end
