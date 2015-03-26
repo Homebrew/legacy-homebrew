59f67ddef2d05aa0bfc5c5af70905356c2f9c50e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-26 17:38:57 +0000

    c-kermit: add 9.0.302 bottle.

--- Library/Formula/c-kermit.rb
+++ Library/Formula/c-kermit.rb
@@ -4,6 +4,12 @@ class CKermit < Formula
   version "9.0.302"
   sha256 "0d5f2cd12bdab9401b4c836854ebbf241675051875557783c332a6a40dac0711"
 
+  bottle do
+    sha256 "01efa74febe89ad045ce0f6be8d80e10d9ef6a4efbc382b4573143d9d72ce48f" => :yosemite
+    sha256 "e170c957947432e62c7131494dc694c39df7d09e5b2c6de0323d40e86e269188" => :mavericks
+    sha256 "1ba733ef3ae9ffbdb1563741bf1c4852dd0f84c55c9e7f0da9e60942814fb61a" => :mountain_lion
+  end
+
   def install
     system "make", "macosx"
     man1.mkpath
