b1b081e49b6a74864128e7542590e594a3f32bb7
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-26 10:09:25 +0100

    dnstracer: test added
    Closes #38095.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/dnstracer.rb
+++ Library/Formula/dnstracer.rb
@@ -1,16 +1,19 @@
-require 'formula'
-
 class Dnstracer < Formula
-  homepage 'http://www.mavetju.org/unix/dnstracer.php'
-  url 'http://www.mavetju.org/download/dnstracer-1.9.tar.gz'
-  sha1 'b8c60b281c0eb309acd1b1551c51cccb951685c7'
+  homepage "http://www.mavetju.org/unix/dnstracer.php"
+  url "http://www.mavetju.org/download/dnstracer-1.9.tar.gz"
+  sha256 "2ebc08af9693ba2d9fa0628416f2d8319ca1627e41d64553875d605b352afe9c"
 
   def install
-    ENV.append 'LDFLAGS', '-lresolv'
+    ENV.append "LDFLAGS", "-lresolv"
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/dnstracer", "brew.sh"
   end
 end
