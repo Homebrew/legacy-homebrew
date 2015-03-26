5318b9b749c3d83cc517d05e4e660f8b4b46c524
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-26 09:11:05 +0000

    apparix: add 11-0 bottle.

--- Library/Formula/apparix.rb
+++ Library/Formula/apparix.rb
@@ -3,6 +3,13 @@ class Apparix < Formula
   url "http://micans.org/apparix/src/apparix-11-062.tar.gz"
   sha256 "211bb5f67b32ba7c3e044a13e4e79eb998ca017538e9f4b06bc92d5953615235"
 
+  bottle do
+    cellar :any
+    sha256 "95d294acae9177133291fd95375d16f37a2ff606a628116a51c0b1aea3d7494c" => :yosemite
+    sha256 "9c460d709002ff092701d2cbd3cf301231eeaa4e5de6fceb59d088c5b0fde7f7" => :mavericks
+    sha256 "6e839a989c4495bb498d2d682cd98434d50eb659582ba4d5d07b4438bd4922e4" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
