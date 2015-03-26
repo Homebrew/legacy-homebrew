a396eda4a98ef92253d6be1fce4602fce249572c
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-26 22:47:12 +0100

    fcrackzip: test added
    Closes #38109.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/fcrackzip.rb
+++ Library/Formula/fcrackzip.rb
@@ -1,14 +1,19 @@
-require 'formula'
-
 class Fcrackzip < Formula
-  homepage 'http://oldhome.schmorp.de/marc/fcrackzip.html'
-  url 'http://oldhome.schmorp.de/marc/data/fcrackzip-1.0.tar.gz'
-  sha1 '92e4f8caa880c55b20e13feb7a25c8b8fd3accf8'
+  homepage "http://oldhome.schmorp.de/marc/fcrackzip.html"
+  url "http://oldhome.schmorp.de/marc/data/fcrackzip-1.0.tar.gz"
+  sha256 "4a58c8cb98177514ba17ee30d28d4927918bf0bdc3c94d260adfee44d2d43850"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"secret").write "homebrew"
+    system "zip", "-qe", "-P", "a", "secret.zip", "secret"
+    assert_equal "PASSWORD FOUND!!!!: pw == a",
+                 shell_output("#{bin}/fcrackzip -u -l 1 secret.zip").strip
   end
 end
