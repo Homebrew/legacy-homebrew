b149487885cf9dddeb0f0024e59a677c9d3a8899
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-26 22:51:37 +0100

    xaric: test added

--- Library/Formula/xaric.rb
+++ Library/Formula/xaric.rb
@@ -1,13 +1,19 @@
-require 'formula'
-
 class Xaric < Formula
-  homepage 'http://xaric.org/'
-  url 'http://xaric.org/software/xaric/releases/xaric-0.13.6.tar.gz'
-  sha1 'ed10e395dea29fdf7bbc0d65389d789d7d4ca09b'
+  homepage "http://xaric.org/"
+  url "http://xaric.org/software/xaric/releases/xaric-0.13.6.tar.gz"
+  sha256 "dbed41ed43efcea05baac0af0fe87cca36eebd96e5b7d4838b38cca3da4518bb"
+  revision 1
+
+  depends_on "openssl"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match(/Xaric #{version}/,
+                 shell_output("script -q /dev/null xaric -v"))
   end
 end
