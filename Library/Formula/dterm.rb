a41e2609a82081e447869a891baeb4eef955df4c
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-26 10:21:04 +0100

    dterm: test added
    Closes #38096.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/dterm.rb
+++ Library/Formula/dterm.rb
@@ -1,12 +1,15 @@
-require 'formula'
-
 class Dterm < Formula
-  homepage 'http://www.knossos.net.nz/resources/free-software/dterm/'
-  url 'http://www.knossos.net.nz/downloads/dterm-0.3.tgz'
-  sha1 'b7dd8cf8c0eb3ef43a32db6011361e886850390d'
+  homepage "http://www.knossos.net.nz/resources/free-software/dterm/"
+  url "http://www.knossos.net.nz/downloads/dterm-0.3.tgz"
+  sha256 "c12ece81a57296e39f564fe0269dbe34f048f4a47d2bf79b6a6215718091e67f"
 
   def install
+    bin.mkpath
     system "make"
-    bin.install "dterm"
+    system "make", "install", "BIN=#{bin}/"
+  end
+
+  test do
+    system "#{bin}/dterm", "help"
   end
 end
