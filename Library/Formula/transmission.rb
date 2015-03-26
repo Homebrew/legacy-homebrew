15f01a8e742dadd888d51c61ba7e5c130355adde
Shaun Jackman <sjackman@gmail.com>
2015-03-26 15:22:50 -0700

    transmission 2.84: Disable libiconv for Linuxbrew

--- Library/Formula/transmission.rb
+++ Library/Formula/transmission.rb
@@ -15,11 +15,10 @@ class Transmission < Formula
     depends_on "intltool" => :build
     depends_on "gettext"
   end
-  depends_on "homebrew/homebrew-dupes/libiconv" unless OS.mac?
 
   def install
     ENV.append "LDFLAGS", "-framework Foundation -prebind" if OS.mac?
-    ENV.append "LDFLAGS", "-liconv"
+    ENV.append "LDFLAGS", "-liconv" if OS.mac?
 
     args = %W[--disable-dependency-tracking
               --prefix=#{prefix}
