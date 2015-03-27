4e75f57930da0a2308b1fa351d9bb18c7b1634ba
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-27 13:32:55 +0100

    vbindiff: test added
    Closes #38119.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/vbindiff.rb
+++ Library/Formula/vbindiff.rb
@@ -1,14 +1,17 @@
-require 'formula'
-
 class Vbindiff < Formula
-  homepage 'http://www.cjmweb.net/vbindiff/'
-  url 'http://www.cjmweb.net/vbindiff/vbindiff-3.0_beta4.tar.gz'
-  version '3.0_beta4'
-  sha1 '0e8c63debafe3d5bb8741e0254c95457d78c7f2d'
+  homepage "http://www.cjmweb.net/vbindiff/"
+  url "http://www.cjmweb.net/vbindiff/vbindiff-3.0_beta4.tar.gz"
+  version "3.0_beta4"
+  sha256 "7d5d5a87fde953dc2089746f6f6ab811d60e127b01074c97611898fb1ef1983d"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/vbindiff", "-L"
   end
 end
