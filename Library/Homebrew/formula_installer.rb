3cebc21bb6a493cd96a679173f8c4b1ce8827125
Xu Cheng <xucheng@me.com>
2015-03-27 23:51:52 +0800

    Silently ignore FormulaUnavailableError in check_conflicts
    If the formula name is in full-qualified name. Let's silently
    ignore it as we don't care about things used in taps that aren't
    currently tapped.
    
    Closes #38089.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -192,8 +192,15 @@ class FormulaInstaller
     return if ARGV.force?
 
     conflicts = formula.conflicts.select do |c|
-      f = Formulary.factory(c.name)
-      f.linked_keg.exist? && f.opt_prefix.exist?
+      begin
+        f = Formulary.factory(c.name)
+        f.linked_keg.exist? && f.opt_prefix.exist?
+      rescue FormulaUnavailableError
+        raise unless c.name =~ HOMEBREW_TAP_FORMULA_REGEX
+        # If the formula name is in full-qualified name. Let's silently
+        # ignore it as we don't care about things used in taps that aren't
+        # currently tapped.
+      end
     end
 
     raise FormulaConflictError.new(formula, conflicts) unless conflicts.empty?
