3319b25f62daec5dcc4741d8e38f5ec3c9d00947
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-03-28 00:20:35 +0900

    fsharp 3.1.1.31
    Closes #38124.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/fsharp.rb
+++ Library/Formula/fsharp.rb
@@ -2,7 +2,8 @@ require "formula"
 
 class Fsharp < Formula
   homepage "http://fsharp.org/"
-  url "https://github.com/fsharp/fsharp.git", :tag => "3.1.1.25"
+  url "https://github.com/fsharp/fsharp.git", :tag => "3.1.1.31",
+    :revision => "1f79c0455fb8b5ec816985f922413894ce19359a"
 
   bottle do
     sha1 "099b7e8179570aca78ab427a8d8ce25e0b3f0cd6" => :mavericks
