3f1aad32a789209d9f33186e401917d8672b7a9a
John Colvin <john.loughran.colvin@gmail.com>
2015-03-27 15:04:55 +0000

    add test

--- Library/Formula/dcd.rb
+++ Library/Formula/dcd.rb
@@ -1,12 +1,31 @@
 class Dcd < Formula
   homepage "https://github.com/Hackerpilot/DCD"
   url "https://github.com/Hackerpilot/DCD.git", :tag => "v0.5.1",
-    :revision => "351bf2ee2d5f1c4986c2c5957f542dda17b1d085"
-
+      :revision => "351bf2ee2d5f1c4986c2c5957f542dda17b1d085"
+  
   depends_on "dmd" => :build
-
+  
   def install
     system "make"
     bin.install "bin/dcd-client", "bin/dcd-server"
+  end
+  
+  test do
+    begin
+      #spawn a server
+      puts "==> dcd-server"
+      server = spawn "dcd-server" 
+      Process.detach server
+      #Give it generous time to load
+      sleep 0.5
+      #query the server from a client
+      client = system "dcd-client", "-q"
+    rescue
+      #clean up the server process
+      Process.kill "TERM", server
+      raise
+    end
+    #Ditto
+    Process.kill "TERM", server
   end
 end
