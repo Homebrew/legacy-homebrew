66faee4dc87b9e41b1a83045307fd0ed522af462
John Colvin <john.loughran.colvin@gmail.com>
2015-03-27 11:55:47 +0000

    add commit hash

--- Library/Formula/dcd.rb
+++ Library/Formula/dcd.rb
@@ -1,6 +1,7 @@
 class Dcd < Formula
   homepage "https://github.com/Hackerpilot/DCD"
-  url "https://github.com/Hackerpilot/DCD.git", :tag => "v0.5.1"
+  url "https://github.com/Hackerpilot/DCD.git", :tag => "v0.5.1",
+    :revision => "351bf2ee2d5f1c4986c2c5957f542dda17b1d085"
 
   depends_on "dmd" => :build
 
