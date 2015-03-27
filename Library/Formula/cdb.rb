789467b5f3977565901da1561444e15c81ad64d3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-27 13:41:41 +0000

    cdb: add 0.75 bottle.

--- Library/Formula/cdb.rb
+++ Library/Formula/cdb.rb
@@ -3,6 +3,13 @@ class Cdb < Formula
   url "http://cr.yp.to/cdb/cdb-0.75.tar.gz"
   sha256 "1919577799a50c080a8a05a1cbfa5fa7e7abc823d8d7df2eeb181e624b7952c5"
 
+  bottle do
+    cellar :any
+    sha256 "1e10737fdb2b905ca823a5304a1570906f262c3e4d9b4f6fa35f3d24ad084001" => :yosemite
+    sha256 "b99e71ef152cfc4744face7938bea54a1da6ca0b51b9e53c708b0472b1485e9c" => :mavericks
+    sha256 "700350e94a89862ea16075d66f8529ee58ba97b3ead1f2419992f6a3024b40f8" => :mountain_lion
+  end
+
   def install
     inreplace "conf-home", "/usr/local", prefix
     system "make", "setup"
