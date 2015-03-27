194dfe0299b5f633890900663c427022581b5a72
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-27 03:06:59 +0000

    dterm: add 0.3 bottle.

--- Library/Formula/dterm.rb
+++ Library/Formula/dterm.rb
@@ -3,6 +3,13 @@ class Dterm < Formula
   url "http://www.knossos.net.nz/downloads/dterm-0.3.tgz"
   sha256 "c12ece81a57296e39f564fe0269dbe34f048f4a47d2bf79b6a6215718091e67f"
 
+  bottle do
+    cellar :any
+    sha256 "021c79b8bd9eaf005e30fb2291c6ca7210ff37bdcf9ba6762fb53b81a21f1cf1" => :yosemite
+    sha256 "485ec99b145bdc2de1e066efc947e14007d5f4d9e421aefad31db399bab29e76" => :mavericks
+    sha256 "7eef65a8c4e22d8c1918b9ad38bc2feae87c1a4cec3671a5c5227648288dd319" => :mountain_lion
+  end
+
   def install
     bin.mkpath
     system "make"
