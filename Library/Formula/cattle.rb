cd095c60124caeed29600e469ba2b3bc24e52dfb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-29 02:46:55 +0100

    cattle: add 1.2.0 bottle.

--- Library/Formula/cattle.rb
+++ Library/Formula/cattle.rb
@@ -4,6 +4,12 @@ class Cattle < Formula
   sha256 "ff0b424011c56f61cb463f1d3fb32e58e40da41260298c407bd0748eb506468d"
   head "https://github.com/andreabolognani/cattle.git"
 
+  bottle do
+    sha256 "b646d326fa499931ae7ef731fa6fce4232239fce1d7f5b217b109ceafd784153" => :yosemite
+    sha256 "4778f55a4ac08e5bf3f9402a37a5c6d97747ee16425325fa3a7fcda69034429d" => :mavericks
+    sha256 "798aa25810d5dbfa2526cf0edf9117798b311c6ae6ce42b3a05c0bc90d087125" => :mountain_lion
+  end
+
   # https://github.com/andreabolognani/cattle/issues/2
   patch do
     url "https://github.com/andreabolognani/cattle/pull/3.diff"
