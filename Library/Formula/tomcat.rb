678f1a2b4d2f561c7874adb833aa2380a3c37758
asaph <ace@dontletgo.com>
2015-03-30 09:50:25 -0700

    tomcat 8.0.21 and change apache urls to https

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -1,8 +1,8 @@
 class Tomcat < Formula
   homepage "https://tomcat.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20.tar.gz"
-  sha1 "f27e57423a5728216dfe0d38d6f7f4dc03ffee97"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21.tar.gz"
+  sha1 "957e88df8a9c3fc6b786321c4014b44c5c775773"
 
   bottle do
     cellar :any
@@ -14,10 +14,10 @@ class Tomcat < Formula
   option "with-fulldocs", "Install full documentation locally"
 
   resource "fulldocs" do
-    url "http://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20-fulldocs.tar.gz"
-    mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20-fulldocs.tar.gz"
-    version "8.0.20"
-    sha1 "7ad70556833a952a084490788d9ea41262a8911c"
+    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21-fulldocs.tar.gz"
+    mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21-fulldocs.tar.gz"
+    version "8.0.21"
+    sha1 "3fc4db49c36846b4197810b7f26d07f5bdd17931"
   end
 
   def install
