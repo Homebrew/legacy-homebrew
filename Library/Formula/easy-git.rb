3835fad4e92775f63ebd9aead68931798f1d9b30
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-04-01 01:47:58 +0200

    easy-git: use https url
    Closes #38249.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/easy-git.rb
+++ Library/Formula/easy-git.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class EasyGit < Formula
-  homepage 'http://people.gnome.org/~newren/eg/'
+  homepage 'https://people.gnome.org/~newren/eg/'
   url 'https://people.gnome.org/~newren/eg/download/1.7.5.2/eg'
   version '1.7.5.2'
   sha1 'c59a10affaae79bddbbe1de743d85d7771575905'
