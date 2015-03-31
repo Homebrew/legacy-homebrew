ef0159b06ed2192b3a019e2c07eff2c7bdc3cb77
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-01 00:53:28 +0100

    gflags: add 2.1.2 bottle.

--- Library/Formula/gflags.rb
+++ Library/Formula/gflags.rb
@@ -3,6 +3,13 @@ class Gflags < Formula
   url "https://github.com/gflags/gflags/archive/v2.1.2.tar.gz"
   sha256 "d8331bd0f7367c8afd5fcb5f5e85e96868a00fd24b7276fa5fcee1e5575c2662"
 
+  bottle do
+    cellar :any
+    sha256 "869687ce2351144f53adb6a4606e6dc7ffc791cf6be9ec1cc229e29cb7754092" => :yosemite
+    sha256 "621970cda40498627ae050ef66a8c43e94f6266014f9228424a4363b6d2d0fb8" => :mavericks
+    sha256 "c252863cdb5da9ad87241f906a9d218a3e8c7136f8fff567ddf9922715c22389" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
