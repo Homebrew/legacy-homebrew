79c42b8cca984f65628a4347f5ff8cfcc2f786ff
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-04-01 11:54:02 +0200

    grails: https homepage

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Grails < Formula
-  homepage 'http://grails.org'
+  homepage 'https://grails.org'
   url 'https://github.com/grails/grails-core/releases/download/v3.0.0/grails-3.0.0.zip'
   sha256 '28b8288c1062e6f42dd43f19f4bbde62604a968f65068b4b77f9e5178db3bb3c'
 
