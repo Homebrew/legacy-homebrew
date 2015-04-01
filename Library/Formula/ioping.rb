b4f9e995c1dc7bcdef7394702526326f11f5cefc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-01 10:24:56 +0100

    ioping: add 0.9 bottle.

--- Library/Formula/ioping.rb
+++ Library/Formula/ioping.rb
@@ -5,6 +5,13 @@ class Ioping < Formula
 
   head "https://github.com/koct9i/ioping.git"
 
+  bottle do
+    cellar :any
+    sha256 "3e41312ac455f0901b365b8c60de6c55869cea417ce57960ed9affb9721690d0" => :yosemite
+    sha256 "e0c9fd5902a4147e866f38c4d4123f9133ad7cfbe10ab1f9d2e78a887ee0603a" => :mavericks
+    sha256 "a01eb9b7c2a39d65fcfc55a70be1333ec0f1df19ad2582dec81a6e40b7ab95c8" => :mountain_lion
+  end
+
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
