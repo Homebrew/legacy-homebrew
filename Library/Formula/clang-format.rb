731b72b066443562f68a97e89c929222fa9ce5db
Martin Probst <martin@probst.io>
2015-04-01 15:32:58 -0700

    clang-format 2015-04-01
    Uses libcxx to build on mountain_lion.

--- Library/Formula/clang-format.rb
+++ Library/Formula/clang-format.rb
@@ -1,6 +1,6 @@
 class ClangFormat < Formula
   homepage "http://clang.llvm.org/docs/ClangFormat.html"
-  version "2015-01-13"
+  version "2015-04-02"
 
   bottle do
     cellar :any
@@ -10,15 +10,15 @@ class ClangFormat < Formula
   end
 
   stable do
-    url "http://llvm.org/svn/llvm-project/llvm/tags/google/testing/2015-01-13/", :using => :svn
+    url "http://llvm.org/svn/llvm-project/llvm/tags/google/testing/2015-04-02/", :using => :svn
 
     resource "clang" do
-      url "http://llvm.org/svn/llvm-project/cfe/tags/google/testing/2015-01-13/", :using => :svn
+      url "http://llvm.org/svn/llvm-project/cfe/tags/google/testing/2015-04-02/", :using => :svn
     end
 
     resource "libcxx" do
-      url "http://llvm.org/releases/3.5.0/libcxx-3.5.0.src.tar.xz"
-      sha1 "c98beed86ae1adf9ab7132aeae8fd3b0893ea995"
+      url "http://llvm.org/releases/3.6.0/libcxx-3.6.0.src.tar.xz"
+      sha1 "5445194366ae2291092fd2204030cb3d01ad6272"
     end
   end
 
@@ -30,8 +30,8 @@ class ClangFormat < Formula
     end
 
     resource "libcxx" do
-      url "http://llvm.org/releases/3.5.0/libcxx-3.5.0.src.tar.xz"
-      sha1 "c98beed86ae1adf9ab7132aeae8fd3b0893ea995"
+      url "http://llvm.org/releases/3.6.0/libcxx-3.6.0.src.tar.xz"
+      sha1 "5445194366ae2291092fd2204030cb3d01ad6272"
     end
   end
 
@@ -39,21 +39,15 @@ class ClangFormat < Formula
   depends_on "ninja" => :build
   depends_on "subversion" => :build
 
-  # It'll link to GCC's libatomic in this case so need to manually specify
-  # the dependency for runtime linking.
-  depends_on "gcc" if MacOS.version <= :mountain_lion
-
-  fails_with :clang do
-    build 503
-    cause "Host Clang must be able to find libstdc++4.7 or newer!"
-  end
-
   def install
     (buildpath/"projects/libcxx").install resource("libcxx")
     (buildpath/"tools/clang").install resource("clang")
 
     mkdir "build" do
-      system "cmake", "..", "-G", "Ninja", *std_cmake_args
+      args = std_cmake_args
+      args << "-DLLVM_ENABLE_LIBCXX=ON"
+      args << ".."
+      system "cmake", "-G", "Ninja", *args
       system "ninja", "clang-format"
       bin.install "bin/clang-format"
     end
