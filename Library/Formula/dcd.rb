f9fb2aa9dd564d84f376945bdfec843a3a1869bf
John Colvin <john.loughran.colvin@gmail.com>
2015-04-01 09:54:59 +0100

    to pass audit

--- Library/Formula/dcd.rb
+++ Library/Formula/dcd.rb
@@ -1,21 +1,22 @@
 class Dcd < Formula
   homepage "https://github.com/Hackerpilot/DCD"
-  url "https://github.com/Hackerpilot/DCD.git", :tag => "v0.5.1",
+  url "https://github.com/Hackerpilot/DCD.git",
+      :tag => "v0.5.1",
       :revision => "351bf2ee2d5f1c4986c2c5957f542dda17b1d085"
-  
+
   depends_on "dmd" => :build
-  
+
   def install
     system "make"
     bin.install "bin/dcd-client", "bin/dcd-server"
   end
-  
+
   test do
     begin
       # spawn a server, using a non-default port to avoid
       # clashes with pre-existing dcd-server instances
       puts "==> dcd-server -p9167"
-      server = spawn "dcd-server -p9167" 
+      server = spawn "dcd-server -p9167"
       Process.detach server
       # Give it generous time to load
       sleep 0.5
