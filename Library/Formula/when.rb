e178cd5c8dd2d2c32547b48f8b92c384c9b16b52
Alex Dunn <dunn.alex@gmail.com>
2015-04-01 00:08:42 -0700

    when 1.1.34
    Closes #38258.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/when.rb
+++ Library/Formula/when.rb
@@ -1,12 +1,21 @@
-require 'formula'
-
 class When < Formula
-  homepage 'http://www.lightandmatter.com/when/when.html'
-  url 'http://ftp.de.debian.org/debian/pool/main/w/when/when_1.1.33.orig.tar.gz'
-  sha1 'e314a64d74f79be07da1ade6d39a1fc51205f81d'
+  homepage "http://www.lightandmatter.com/when/when.html"
+  url "https://mirrors.kernel.org/debian/pool/main/w/when/when_1.1.34.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/w/when/when_1.1.34.orig.tar.gz"
+  sha256 "ce0540bde96b361d6d0770803901364a687d971ffedd33e36f8f8bef32b19600"
+  head "https://github.com/bcrowell/when.git"
 
   def install
-    bin.install 'when'
-    man1.install 'when.1'
+    system "make", "prefix=#{prefix}", "install"
+  end
+
+  test do
+    temp_home = Pathname(ENV["HOME"])
+    (temp_home/".when/preferences").write <<-EOS.undent
+      calendar = #{testpath}/calendar
+    EOS
+
+    (testpath/"calendar").write "2015 April 1, stay off the internet"
+    system bin/"when", "i"
   end
 end
