f157d1b607f6a6d78a74e937f3a6ac1518a13d9b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-02 07:50:41 +0100

    when: add 1.1.34 bottle.

--- Library/Formula/when.rb
+++ Library/Formula/when.rb
@@ -5,6 +5,13 @@ class When < Formula
   sha256 "ce0540bde96b361d6d0770803901364a687d971ffedd33e36f8f8bef32b19600"
   head "https://github.com/bcrowell/when.git"
 
+  bottle do
+    cellar :any
+    sha256 "2cabf7cbe84effcefcba892ba5008e5fbd6f1558cdf967fbc9ce09d2f3dea6d3" => :yosemite
+    sha256 "b4724a057c7078514306e334e16ff242a2085adf09a2c41c5228e211d43a7729" => :mavericks
+    sha256 "2bb76c69919b847c94ecd3600908848e989d93e5c5a3b949410cad73c376a2f5" => :mountain_lion
+  end
+
   def install
     system "make", "prefix=#{prefix}", "install"
   end
