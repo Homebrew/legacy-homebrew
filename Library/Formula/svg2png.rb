6cf0ad102c81b2f6738c8120ac4ee00c0630081c
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-02 23:47:56 +0200

    svg2png: test added
    Closes #38319.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/svg2png.rb
+++ Library/Formula/svg2png.rb
@@ -1,17 +1,21 @@
-require 'formula'
-
 class Svg2png < Formula
-  homepage 'http://cairographics.org/'
-  url 'http://cairographics.org/snapshots/svg2png-0.1.3.tar.gz'
-  sha1 'afd207ef745fe2e20e01585bbc9a576e7cc9caf5'
+  homepage "http://cairographics.org/"
+  url "http://cairographics.org/snapshots/svg2png-0.1.3.tar.gz"
+  sha256 "e658fde141eb7ce981ad63d319339be5fa6d15e495d1315ee310079cbacae52b"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libsvg-cairo'
+  depends_on "pkg-config" => :build
+  depends_on "libsvg-cairo"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/svg2png", test_fixtures("test.svg"), "test.png"
+    assert File.exist? "test.png"
   end
 end
