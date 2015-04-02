a89de793cc64a3898b311162cd9f483483b3ba3e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-02 09:14:47 +0100

    svg2pdf: add 0.1.3 bottle.

--- Library/Formula/svg2pdf.rb
+++ Library/Formula/svg2pdf.rb
@@ -3,6 +3,13 @@ class Svg2pdf < Formula
   url "http://cairographics.org/snapshots/svg2pdf-0.1.3.tar.gz"
   sha256 "854a870722a9d7f6262881e304a0b5e08a1c61cecb16c23a8a2f42f2b6a9406b"
 
+  bottle do
+    cellar :any
+    sha256 "0ff81cf4177cd94c385c98b89c9620235c78fc154a6781996b4146d3777d4c5f" => :yosemite
+    sha256 "ec23a2efe2fe015c475e42ceb44674e1ec4ba944081e9ce212e87ce25403fef5" => :mavericks
+    sha256 "9d1880e5e4bfbc3ef676415a3c8f0480c312c3619b07efdb3249191e8f9d47a0" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libsvg-cairo"
 
