9da7a709e1c0082ff1525063cc4e6ac4068898e8
Alessio Bogon <youtux@gmail.com>
2015-04-02 15:45:13 +0200

    heroku-toolbelt: add head

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -2,6 +2,7 @@ class HerokuToolbelt < Formula
   homepage "https://toolbelt.heroku.com/other"
   url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.30.4.tgz"
   sha256 "51a92e85994c65d016823ad7b8fa74bf30ddeb59afb33751c1f93e8105c80e9e"
+  head "https://github.com/heroku/heroku.git"
 
   def install
     libexec.install Dir["*"]
