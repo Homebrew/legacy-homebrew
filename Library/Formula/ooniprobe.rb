e8f9b764300fc4b9ea6ffcc2158ed312537084ba
Tim D. Smith <git@tim-smith.us>
2015-04-02 10:29:27 -0700

    hint libdnet module path

--- Library/Formula/ooniprobe.rb
+++ Library/Formula/ooniprobe.rb
@@ -114,6 +114,7 @@ class Ooniprobe < Formula
   end
 
   def install
+    ENV["PYTHONPATH"] = Formula["libdnet"].opt_lib/"python2.7/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
     resources.each do |r|
       r.stage do
