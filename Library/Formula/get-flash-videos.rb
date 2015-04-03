3ed9d522f5bac9c4efaffd4e62990363ce0358be
Steven Willis <onlynone@gmail.com>
2015-04-03 15:17:34 -0400

    get-flash-videos: addressing brew audit comments

--- Library/Formula/get-flash-videos.rb
+++ Library/Formula/get-flash-videos.rb
@@ -1,12 +1,11 @@
 class GetFlashVideos < Formula
-  homepage "https://code.google.com/p/get-flash-videos"
-  url "http://get-flash-videos.googlecode.com/files/combined-get_flash_videos-1.24", :using => :nounzip
+  homepage "https://github.com/monsieurvideo/get-flash-videos"
+  url "https://get-flash-videos.googlecode.com/files/combined-get_flash_videos-1.24", :using => :nounzip
   version "1.24"
   sha256 "e41da715f817bfa1a65ae82cc4f5a82268b6e72a7f4d90ffabf89b3a522cbb91"
 
   def install
-    mv "combined-get_flash_videos-1.24", "get_flash_videos"
-    bin.install "get_flash_videos"
+    bin.install "combined-get_flash_videos-1.24" => "get_flash_videos"
   end
 
   test do
