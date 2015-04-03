80e79ab401e4d21475186c6df83badbad26619b4
Daniel <daniel65536@gmail.com>
2015-04-03 20:27:57 +0800

    Use pipe_output in test

--- Library/Formula/cidrmerge.rb
+++ Library/Formula/cidrmerge.rb
@@ -9,15 +9,12 @@ class Cidrmerge < Formula
   end
 
   test do
-    system "cidrmerge", "-v"
-    require "open3"
-    Open3.popen3("cidrmerge") do |stdin, stdout, _|
-      stdin.write("10.1.1.0/24\n")
-      stdin.write("10.1.1.1/32\n")
-      stdin.write("192.1.4.5/32\n")
-      stdin.write("192.1.4.4/32\n")
-      stdin.close
-      assert_equal "10.1.1.0/24\n192.1.4.4/31\n", stdout.read
-    end
+    input = <<-EOS.undent
+    10.1.1.0/24
+    10.1.1.1/32
+    192.1.4.5/32
+    192.1.4.4/32
+    EOS
+    assert_equal "10.1.1.0/24\n192.1.4.4/31\n", pipe_output("#{bin}/cidrmerge", input)
   end
 end
