3ffd1a1b616697d0e78ad2febc5681b78bfb07c3
Xu Cheng <xucheng@me.com>
2015-04-03 21:07:25 +0800

    shtool: modernize and add test

--- Library/Formula/shtool.rb
+++ Library/Formula/shtool.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class Shtool < Formula
-  homepage 'http://www.gnu.org/software/shtool/'
-  url 'http://ftpmirror.gnu.org/shtool/shtool-2.0.8.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/shtool/shtool-2.0.8.tar.gz'
-  sha1 '4b974f92d3932ea121e311e3b22c328d3b3572d4'
+  homepage "https://www.gnu.org/software/shtool/"
+  url "http://ftpmirror.gnu.org/shtool/shtool-2.0.8.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/shtool/shtool-2.0.8.tar.gz"
+  sha256 "1298a549416d12af239e9f4e787e6e6509210afb49d5cf28eb6ec4015046ae19"
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "Hello World!", pipe_output("#{bin}/shtool echo 'Hello World!'").chomp
   end
 end
