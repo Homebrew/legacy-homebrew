ef5df8731cbc075d478690d26b2ede86ea1f8a15
Alex Dunn <dunn.alex@gmail.com>
2015-04-04 23:09:28 -0700

    harfbuzz 0.9.40
    Closes #38379.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -1,7 +1,8 @@
+# encoding: utf-8
 class Harfbuzz < Formula
-  homepage "http://www.freedesktop.org/wiki/Software/HarfBuzz"
-  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.38.tar.bz2"
-  sha256 "6736f383b4edfcaaeb6f3292302ca382d617d8c79948bb2dd2e8f86cdccfd514"
+  homepage "https://www.freedesktop.org/wiki/Software/HarfBuzz"
+  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-0.9.40.tar.bz2"
+  sha256 "1771d53583be6d91ca961854b2a24fb239ef0545eed221ae3349abae0ab8321f"
 
   bottle do
     cellar :any
@@ -16,10 +17,22 @@ class Harfbuzz < Formula
   depends_on "icu4c" => :recommended
   depends_on "freetype"
 
+  resource "ttf" do
+    url "https://github.com/behdad/harfbuzz/raw/fc0daafab0336b847ac14682e581a8838f36a0bf/test/shaping/fonts/sha1sum/270b89df543a7e48e206a2d830c0e10e5265c630.ttf"
+    sha256 "9535d35dab9e002963eef56757c46881f6b3d3b27db24eefcc80929781856c77"
+  end
+
   def install
     args = %W[--disable-dependency-tracking --prefix=#{prefix}]
     args << "--with-icu" if build.with? "icu4c"
     system "./configure", *args
     system "make", "install"
+  end
+
+  test do
+    resource("ttf").stage do
+      shape = `echo 'സ്റ്റ്' | #{bin}/hb-shape 270b89df543a7e48e206a2d830c0e10e5265c630.ttf`.chomp
+      assert_equal "[glyph201=0+1183|U0D4D=0+0]", shape
+    end
   end
 end
