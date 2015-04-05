b5bc9ac50a3896c87ace28956096e2704d6309b9
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-05 17:15:02 +0200

    tre: test added
    Closes #38385.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/tre.rb
+++ Library/Formula/tre.rb
@@ -1,13 +1,16 @@
-require 'formula'
-
 class Tre < Formula
-  homepage 'http://laurikari.net/tre/'
-  url 'http://laurikari.net/tre/tre-0.8.0.tar.bz2'
-  sha1 'a41692e64b40ebae3cffe83931ddbf8420a10ae3'
+  homepage "http://laurikari.net/tre/"
+  url "http://laurikari.net/tre/tre-0.8.0.tar.bz2"
+  sha256 "8dc642c2cde02b2dac6802cdbe2cda201daf79c4ebcbb3ea133915edf1636658"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "brow", pipe_output("#{bin}/agrep -1 brew", "brow", 0)
   end
 end
