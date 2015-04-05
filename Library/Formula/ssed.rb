2ed51fc82bd0554aa41e011d3395adc98ee7aefc
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-05 20:18:37 +0200

    ssed: test added

--- Library/Formula/ssed.rb
+++ Library/Formula/ssed.rb
@@ -1,9 +1,9 @@
-require 'formula'
-
 class Ssed < Formula
-  homepage 'http://sed.sourceforge.net/grabbag/ssed/'
-  url 'http://sed.sourceforge.net/grabbag/ssed/sed-3.62.tar.gz'
-  sha1 '6bdc4cd07780e397787c0d494c962827bb407fa2'
+  homepage "http://sed.sourceforge.net/grabbag/ssed/"
+  url "http://sed.sourceforge.net/grabbag/ssed/sed-3.62.tar.gz"
+  sha256 "af7ff67e052efabf3fd07d967161c39db0480adc7c01f5100a1996fec60b8ec4"
+
+  conflicts_with "gnu-sed", :because => "both install share/info/sed.info"
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -11,6 +11,11 @@ class Ssed < Formula
                           "--mandir=#{man}",
                           "--infodir=#{info}",
                           "--program-prefix=s"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "homebrew",
+      pipe_output("#{bin}/ssed s/neyd/mebr/", "honeydew", 0).chomp
   end
 end
