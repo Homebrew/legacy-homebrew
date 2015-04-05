502f5d2d0b2738e0a7c21200974d58a34e2b1c41
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-05 18:22:24 +0200

    idcomments: test added
    Closes #38387.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/idcomments.rb
+++ Library/Formula/idcomments.rb
@@ -1,13 +1,22 @@
-require 'formula'
-
 class Idcomments < Formula
-  homepage 'http://tools.ietf.org/tools/idcomments/'
-  url 'http://tools.ietf.org/tools/idcomments/idcomments-0.18.tgz'
-  sha1 'e7e6430926b0c502afcb55a91e90cd84d55c07d4'
+  homepage "https://tools.ietf.org/tools/idcomments/"
+  url "https://tools.ietf.org/tools/idcomments/idcomments-0.18.tgz"
+  sha256 "a75774495e1b9b799326f3c6875db927835546b82762d565efe17e3cbfa6c7b8"
+
+  depends_on "rfcdiff"
 
   def install
-    inreplace 'idcomments', '$(tempfile)', '$(mktemp /tmp/idcomments.XXXXXXXX)'
-    bin.install 'idcomments'
-    doc.install %w(about changelog copyright todo)
+    inreplace "idcomments", "$(tempfile)", "$(mktemp /tmp/idcomments.XXXXXXXX)"
+    bin.install "idcomments"
+    doc.install %w[about changelog copyright todo]
+  end
+
+  test do
+    (testpath/"draft").write <<-EOS.undent
+      COMMENT: brew
+    EOS
+
+    assert_equal "INTRODUCTION, paragraph 0:\n\n\n  brew",
+      shell_output("#{bin}/idcomments draft").strip
   end
 end
