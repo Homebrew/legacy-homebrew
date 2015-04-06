3254ae838f0d396cec9227e48550db414657c946
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-06 22:47:48 +0200

    rpl: test added

--- Library/Formula/rpl.rb
+++ Library/Formula/rpl.rb
@@ -1,14 +1,20 @@
-require 'formula'
-
 class Rpl < Formula
-  homepage 'http://www.laffeycomputer.com/rpl.html'
-  url 'ftp://ftp2.laffeycomputer.com/pub/current_builds/rpl-1.4.1.tar.gz'
-  sha1 '6c67ecd2307f378b44b697411b0ab65bc5d2cdaa'
+  homepage "http://www.laffeycomputer.com/rpl.html"
+  url "ftp://ftp2.laffeycomputer.com/pub/current_builds/rpl-1.4.1.tar.gz"
+  sha256 "291055dc8763c855bab76142b5f7e9625392bcefa524b796bc4ddbcf941a1310"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test").write "I like water."
+
+    system "#{bin}/rpl", "-v", "water", "beer", "test"
+    assert_equal "I like beer.", (testpath/"test").read
   end
 end
