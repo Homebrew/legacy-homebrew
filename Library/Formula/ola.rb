7b248c1d72fb361002c27dcb7a7cd5f4bf6e2123
James Elliott <james.elliott@singlewire.com>
2015-04-05 19:31:46 -0500

    ola 0.9.5 with sha256

--- Library/Formula/ola.rb
+++ Library/Formula/ola.rb
@@ -3,7 +3,7 @@ require "formula"
 class Ola < Formula
   homepage "http://www.openlighting.org/ola/"
   url "https://github.com/OpenLightingProject/ola/releases/download/0.9.5/ola-0.9.5.tar.gz"
-  sha1 "e9b0d8e4e3e4acd696cd7a0877d72d593727a7bc"
+  sha256 "7c24ac98e865b4c354a04563b88012e782205ffd932a05cf944273f6f5ea82ca"
 
   bottle do
     sha1 "084b25099b2eaf5d90a69ebc20b43c0cf338b614" => :yosemite
