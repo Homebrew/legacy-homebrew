b6868816f07948c3f2d1ae1870a26100063370ec
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-06 14:38:08 +0100

    dub: add 0.9.23 bottle.

--- Library/Formula/dub.rb
+++ Library/Formula/dub.rb
@@ -3,6 +3,12 @@ class Dub < Formula
   url  "https://github.com/D-Programming-Language/dub/archive/v0.9.23.tar.gz"
   sha1 "88c908cab5d396435091cb9cc31dcd886fdb26c9"
 
+  bottle do
+    sha256 "e2613efeb8173e8f1beb2a4b1bf14baa70a4fa7781919c87be435bb1d55d8ae2" => :yosemite
+    sha256 "b108a54497cf52eb62d4aad877804630a26d47b59e606d300e7becb3a98be877" => :mavericks
+    sha256 "4afe785f25db496f8634c19211d68c7ea05778f9ed1e7e772f8aa9ac701beca1" => :mountain_lion
+  end
+
   head "https://github.com/D-Programming-Language/dub.git", :shallow => false
 
   depends_on "pkg-config" => :build
