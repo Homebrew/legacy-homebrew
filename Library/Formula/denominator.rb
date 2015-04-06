af874aae4b03cb31709cfdaddf3910483450f99a
Adrian Cole <adrian.f.cole@gmail.com>
2015-04-05 20:12:42 -0700

    denominator 4.5.0
    Closes #38401.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/denominator.rb
+++ Library/Formula/denominator.rb
@@ -1,10 +1,10 @@
 class Denominator < Formula
-  homepage "https://github.com/Netflix/denominator/tree/v4.4.2/cli"
-  url "https://bintray.com/artifact/download/netflixoss/maven/com/netflix/denominator/denominator-cli/4.4.2/denominator-cli-4.4.2-fat.jar"
-  sha1 "b60b4dbcff98cc9d08f3eec1273c1ce3ff146860"
+  homepage "https://github.com/Netflix/denominator/tree/v4.5.0/cli"
+  url "https://bintray.com/artifact/download/netflixoss/maven/com/netflix/denominator/denominator-cli/4.5.0/denominator-cli-4.5.0-fat.jar"
+  sha1 "3bdcef1f4942605c2d49ff6823f10c0ace82338a"
 
   def install
-    bin.install "denominator-cli-4.4.2-fat.jar" => "denominator"
+    bin.install "denominator-cli-4.5.0-fat.jar" => "denominator"
   end
 
   test do
