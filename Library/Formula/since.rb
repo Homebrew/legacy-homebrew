4be0848a34f57bb58670f4e4051a4d846e8236f8
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-06 23:13:49 +0200

    since: test added

--- Library/Formula/since.rb
+++ Library/Formula/since.rb
@@ -1,13 +1,20 @@
-require 'formula'
-
 class Since < Formula
-  homepage 'http://welz.org.za/projects/since'
-  url 'http://welz.org.za/projects/since/since-1.1.tar.gz'
-  sha1 '67f07e8237d63f846cd8ca60b5a16fc32d4f81a5'
+  homepage "http://welz.org.za/projects/since"
+  url "http://welz.org.za/projects/since/since-1.1.tar.gz"
+  sha256 "739b7f161f8a045c1dff184e0fc319417c5e2deb3c7339d323d4065f7a3d0f45"
 
   def install
-    system "make"
-    bin.install "since"
-    man1.install "since.1"
+    bin.mkpath
+    man1.mkpath
+    system "make", "install", "prefix=#{prefix}", "INSTALL=install"
+  end
+
+  test do
+    (testpath/"test").write <<-EOS.undent
+      foo
+      bar
+    EOS
+    system "#{bin}/since", "-z", "test"
+    assert File.exist? ".since"
   end
 end
