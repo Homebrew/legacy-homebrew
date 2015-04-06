e5200e35906cd259bd71be603e08366884fdf69e
John Colvin <john.loughran.colvin@gmail.com>
2015-04-06 14:30:40 +0100

    dub 0.9.23
    Closes #38407.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/dub.rb
+++ Library/Formula/dub.rb
@@ -1,15 +1,9 @@
 class Dub < Formula
   homepage "http://code.dlang.org/about"
-  url  "https://github.com/D-Programming-Language/dub/archive/v0.9.22.tar.gz"
-  sha1 "9a7b7c838f1241de209473c09a194d355279457b"
+  url  "https://github.com/D-Programming-Language/dub/archive/v0.9.23.tar.gz"
+  sha1 "88c908cab5d396435091cb9cc31dcd886fdb26c9"
 
   head "https://github.com/D-Programming-Language/dub.git", :shallow => false
-
-  devel do
-    url "https://github.com/D-Programming-Language/dub/archive/v0.9.23-rc.1.tar.gz"
-    sha1 "3fe35139a58d9ceaee1e920940dc5e67f2b1f178"
-    version "0.9.23-rc.1"
-  end
 
   depends_on "pkg-config" => :build
   depends_on "dmd" => :build
