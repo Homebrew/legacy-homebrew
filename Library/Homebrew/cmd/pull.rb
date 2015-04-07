5cee11f8abcc3bfb92c6823e9d4ab2848745b59f
Mike McQuaid <mike@mikemcquaid.com>
2015-04-07 08:31:36 +0100

    pull: try downloading bottles after upload.

--- Library/Homebrew/cmd/pull.rb
+++ Library/Homebrew/cmd/pull.rb
@@ -176,6 +176,7 @@ module Homebrew
               "-u#{bintray_user}:#{bintray_key}", "-X", "POST",
               "https://api.bintray.com/content/homebrew/#{repo}/#{package}/#{version}/publish"
             puts
+            safe_system "brew", "fetch", "--force-bottle", f.name
           end
         else
           opoo "You must set BINTRAY_USER and BINTRAY_KEY to add or update bottles on Bintray!"
