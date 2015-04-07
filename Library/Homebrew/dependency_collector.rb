c7950e08ad50013d567676f64dde7b9fdd8fa795
Tim D. Smith <git@tim-smith.us>
2015-04-06 23:20:07 -0700

    Prioritize TapDependency in parse_string_spec
    test-bot expects the dependencies returned from SoftwareSpec#deps to
    satisfy is_a?(TapDependency) if they come from a tap which might need to
    be tapped.

--- Library/Homebrew/dependency_collector.rb
+++ Library/Homebrew/dependency_collector.rb
@@ -89,12 +89,12 @@ class DependencyCollector
   end
 
   def parse_string_spec(spec, tags)
-    if tags.empty?
+    if HOMEBREW_TAP_FORMULA_REGEX === spec
+      TapDependency.new(spec, tags)
+    elsif tags.empty?
       Dependency.new(spec, tags)
     elsif (tag = tags.first) && LANGUAGE_MODULES.include?(tag)
       LanguageModuleDependency.new(tag, spec, tags[1])
-    elsif HOMEBREW_TAP_FORMULA_REGEX === spec
-      TapDependency.new(spec, tags)
     else
       Dependency.new(spec, tags)
     end
