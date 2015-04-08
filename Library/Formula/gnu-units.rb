c2ee251f0e0217ecb7762ce29b1c476b31439def
Sebastian Boehm <sebastian@sometimesfood.org>
2015-04-08 10:23:59 +0200

    gnu-utils: fix "invalid/nonprinting UTF-8" warning
    Closes #38454.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gnu-units.rb
+++ Library/Formula/gnu-units.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 require "formula"
 
 class GnuUnits < Formula
   homepage "https://www.gnu.org/software/units/"
   url "http://ftpmirror.gnu.org/units/units-2.02.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/units/units-2.02.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/units/units-2.02.tar.gz"
   sha1 "e460371dc97034d17ce452e6b64991f7fd2d1409"
 
   bottle do
@@ -15,6 +16,17 @@ class GnuUnits < Formula
   deprecated_option "default-names" => "with-default-names"
 
   option "with-default-names", "Do not prepend 'g' to the binary"
+
+  # Temporary fix for "invalid/nonprinting UTF-8" warnings on startup,
+  # see https://github.com/Homebrew/homebrew/issues/20297.
+  #
+  # The current maintainer of GNU units, Adrian Mariano, is aware of
+  # the issue (reported by mail on 2015-04-08) and has fixed it in the
+  # currently unreleased development version which will be released
+  # later this year.
+  #
+  # See https://github.com/Homebrew/homebrew/issues/38454 for details.
+  patch :DATA
 
   def install
     args = ["--prefix=#{prefix}"]
@@ -28,3 +40,28 @@ class GnuUnits < Formula
     assert_equal "* 18288", shell_output("#{bin}/gunits '600 feet' 'cm' -1").strip
   end
 end
+
+__END__
+diff --git a/definitions.units b/definitions.units
+index a0c61f2..06269ce 100644
+--- a/definitions.units
++++ b/definitions.units
+@@ -5248,9 +5248,6 @@ röntgen                 roentgen
+ ㍴                      bar
+ # ㍵                          oV???
+ ㍶                      pc
+-#㍷                      dm      invalid on Mac
+-#㍸                      dm^2    invalid on Mac
+-#㍹                      dm^3    invalid on Mac
+ ㎀                      pA
+ ㎁                      nA
+ ㎂                      µA
+@@ -5342,9 +5339,6 @@ röntgen                 roentgen
+ ㏛                      sr
+ ㏜                      Sv
+ ㏝                      Wb
+-#㏞                      V/m     Invalid on Mac
+-#㏟                      A/m     Invalid on Mac
+-#㏿                      gal     Invalid on Mac
+
+ !endutf8
