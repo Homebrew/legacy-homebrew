7e76df70b6537e8650e2964e47b2380816d5797a
Alistair McMillan <alistairmcmillan75@gmail.com>
2015-04-08 01:13:50 +0100

    gexiv2 0.10.3
    Also: add introspection.
    
    Closes #38449.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gexiv2.rb
+++ Library/Formula/gexiv2.rb
@@ -1,7 +1,7 @@
 class Gexiv2 < Formula
   homepage "https://wiki.gnome.org/Projects/gexiv2"
-  url "https://download.gnome.org/sources/gexiv2/0.10/gexiv2-0.10.2.tar.xz"
-  sha256 "2fd21f0ed5125e51d02226e7f41be751cfa8ae411a8ed1a651e16b06d79047b2"
+  url "https://download.gnome.org/sources/gexiv2/0.10/gexiv2-0.10.3.tar.xz"
+  sha256 "390cfb966197fa9f3f32200bc578d7c7f3560358c235e6419657206a362d3988"
 
   bottle do
     cellar :any
@@ -12,6 +12,8 @@ class Gexiv2 < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+  depends_on "gobject-introspection" => :build
   depends_on "glib"
   depends_on "exiv2"
 
@@ -19,6 +21,7 @@ class Gexiv2 < Formula
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
+                          "--enable-introspection",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
