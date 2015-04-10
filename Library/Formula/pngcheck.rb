fcae6ed121ae156631b0483114d0d8176325ef48
Xu Cheng <xucheng@me.com>
2015-04-10 17:52:06 +0800

    pngcheck: modernize and add test

--- Library/Formula/pngcheck.rb
+++ Library/Formula/pngcheck.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Pngcheck < Formula
-  homepage 'http://www.libpng.org/pub/png/apps/pngcheck.html'
-  url 'https://downloads.sourceforge.net/project/png-mng/pngcheck/2.3.0/pngcheck-2.3.0.tar.gz'
-  sha1 'e7f1535abbf2f809e036a9a43c759eeac5e39350'
+  homepage "http://www.libpng.org/pub/png/apps/pngcheck.html"
+  url "https://downloads.sourceforge.net/project/png-mng/pngcheck/2.3.0/pngcheck-2.3.0.tar.gz"
+  sha1 "e7f1535abbf2f809e036a9a43c759eeac5e39350"
   revision 1
 
   bottle do
@@ -14,7 +12,11 @@ class Pngcheck < Formula
   end
 
   def install
-    system 'make -f Makefile.unx ZINC= ZLIB=-lz'
+    system "make", "-f", "Makefile.unx", "ZINC=", "ZLIB=-lz"
     bin.install %w[pngcheck pngsplit png-fix-IDAT-windowsize]
+  end
+
+  test do
+    system bin/"pngcheck", test_fixtures("test.png")
   end
 end
