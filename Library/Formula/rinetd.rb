7b99108833aadd3026718d8a18090af5f230d4e9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-11 15:40:52 +0100

    rinetd: add 0.62 bottle.

--- Library/Formula/rinetd.rb
+++ Library/Formula/rinetd.rb
@@ -4,6 +4,13 @@ class Rinetd < Formula
   version "0.62"
   sha256 "0c68d27c5bd4b16ce4f58a6db514dd6ff37b2604a88b02c1dfcdc00fc1059898"
 
+  bottle do
+    cellar :any
+    sha256 "ee8653ffad51210e68b26e6f43a34f580180aed315b050ca48cd0adcadd3e6b0" => :yosemite
+    sha256 "07cbd9a185d1b3698078fd26737af105cfe31a2eb2d1a4b229e9878561f162ef" => :mavericks
+    sha256 "2ed57913af533cd7778526ff633b24dbaf753558293d472d81d03ffacd9e869e" => :mountain_lion
+  end
+
   def install
     inreplace "rinetd.c" do |s|
       s.gsub! "/etc/rinetd.conf", "#{etc}/rinetd.conf"
