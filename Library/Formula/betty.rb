88912e5c971f8c058d3cc7c7b8cc368d5c2a8d07
Mike McQuaid <mike@mikemcquaid.com>
2015-04-11 19:07:26 +0100

    betty: use RubyRequirement.

--- Library/Formula/betty.rb
+++ Library/Formula/betty.rb
@@ -1,25 +1,9 @@
-require "formula"
-
-class Ruby19Dependency < Requirement
-  fatal true
-  default_formula "ruby"
-
-  satisfy do
-    `ruby --version` =~ /ruby (\d\.\d).\d/
-    $1.to_f >= 1.9
-  end
-
-  def message
-    "Betty requires Ruby 1.9 or better."
-  end
-end
-
 class Betty < Formula
   homepage "https://github.com/pickhardt/betty"
   url "https://github.com/pickhardt/betty/archive/v0.1.7.tar.gz"
   sha1 "ec21ec5541289a9902874c7897f8d521044daf27"
 
-  depends_on Ruby19Dependency
+  depends_on :ruby => "1.9"
 
   def install
     libexec.install "lib", "main.rb" => "betty"
