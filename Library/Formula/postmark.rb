ea43bf98b11c18f2f6e696921fe95aac90926323
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-11 10:33:41 +0200

    postmark: test added
    Closes #38542.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/postmark.rb
+++ Library/Formula/postmark.rb
@@ -1,13 +1,16 @@
-require 'formula'
-
 class Postmark < Formula
-  homepage 'http://packages.debian.org/stable/utils/postmark'
-  url 'http://mirrors.kernel.org/debian/pool/main/p/postmark/postmark_1.51.orig.tar.gz'
-  mirror 'http://ftp.us.debian.org/debian/pool/main/p/postmark/postmark_1.51.orig.tar.gz'
-  sha1 '2cf0be75e3cb255f36fb1f3e412bcf8f81b39969'
+  homepage "https://packages.debian.org/stable/utils/postmark"
+  url "https://mirrors.kernel.org/debian/pool/main/p/postmark/postmark_1.51.orig.tar.gz"
+  mirror "http://ftp.us.debian.org/debian/pool/main/p/postmark/postmark_1.51.orig.tar.gz"
+  sha256 "7cb7c31d4e7725ce8d8e11fb7df62ed700dee4dbd5ca1e31bf3a9161fc890b41"
 
   def install
-    system "cc -o postmark postmark-*.c"
+    system ENV.cc, "-o", "postmark", "postmark-#{version}.c"
     bin.install "postmark"
+  end
+
+  test do
+    assert_match(/PostMark v#{version}/,
+                 pipe_output("#{bin}/postmark", "run\n", 0))
   end
 end
