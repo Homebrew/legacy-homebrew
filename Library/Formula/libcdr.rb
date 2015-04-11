477e342ab72db1f4217a1187ce2447dec6047dc0
luzpaz <luzpaz@users.noreply.github.com>
2015-04-11 09:16:58 -0500

    Update libcdr.rb
    committed @DomT4 corrections

--- Library/Formula/libcdr.rb
+++ Library/Formula/libcdr.rb
@@ -1,16 +1,13 @@
-require "formula"
-
 class Libcdr < Formula
   homepage "https://wiki.documentfoundation.org/DLP/Libraries/libcdr"
   url "http://dev-www.libreoffice.org/src/libcdr/libcdr-0.1.1.tar.bz2"
-  sha1 "0795b0409c86e3d3edafbedf045146469db8080b"
+  sha256 "72fe7bbbf2275242acdf67ad2f9b6c71ac9146a56c409def360dabcac5695b49"
 
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
   depends_on "icu4c"
   depends_on "librevenge"
   depends_on "little-cms2"
-
 
   def install
     ENV.cxx11
@@ -20,8 +17,8 @@ class Libcdr < Formula
     system "make", "install"
   end
 
-    test do
-    (testpath/'test.cpp').write <<-EOS.undent
+  test do
+    ('test.cpp').write <<-EOS.undent
       #include <libcdr/libcdr.h>
       int main() {
         libcdr::CDRDocument::isSupported(0);
