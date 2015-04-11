a103fdeb424cd52040a21b91fb8ad727aeb468fc
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-11 11:52:38 +0200

    shntool: test added
    Closes #38544.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/shntool.rb
+++ Library/Formula/shntool.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class Shntool < Formula
-  homepage 'http://etree.org/shnutils/shntool/'
-  url 'http://etree.org/shnutils/shntool/dist/src/shntool-3.0.10.tar.gz'
-  sha1 '7a2bc8801e180cf582f0e39775603582e35d50d2'
+  homepage "http://etree.org/shnutils/shntool/"
+  url "http://etree.org/shnutils/shntool/dist/src/shntool-3.0.10.tar.gz"
+  sha256 "74302eac477ca08fb2b42b9f154cc870593aec8beab308676e4373a5e4ca2102"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/shninfo", test_fixtures("test.wav")
   end
 end
