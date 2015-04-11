9f199ef9de658c3150f5212c59fc2a8e8fadf52c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-11 09:24:55 +0100

    shorten: add 3.6.1 bottle.

--- Library/Formula/shorten.rb
+++ Library/Formula/shorten.rb
@@ -3,6 +3,13 @@ class Shorten < Formula
   url "http://www.etree.org/shnutils/shorten/dist/src/shorten-3.6.1.tar.gz"
   sha256 "ce22e0676c93494ee7d094aed9b27ad018eae5f2478e8862ae1e962346405b66"
 
+  bottle do
+    cellar :any
+    sha256 "5f48b61ce709915830f433dd381fe531c1354b56619bbdb441dc19f985df7467" => :yosemite
+    sha256 "a802da618fffa3eb292705140c882fcedbffae09017f0efdf69085004952a148" => :mavericks
+    sha256 "ca55e37b24202c500a03dfe36a41dd06ebaa02b19ddf65c26cc440376149c5da" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
