14dcb12623947d988dea1e4f25a9a2631240ecfd
Mike McQuaid <mike@mikemcquaid.com>
2015-04-11 19:07:39 +0100

    heroku-toolbelt: use RubyRequirement.

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -1,31 +1,10 @@
-class Ruby19 < Requirement
-  fatal true
-  default_formula "ruby"
-
-  satisfy :build_env => false do
-    next unless which "ruby"
-    version = /\d\.\d/.match `ruby --version 2>&1`
-    next unless version
-    Version.new(version.to_s) >= Version.new("1.9")
-  end
-
-  env do
-    ENV.prepend_path "PATH", which("ruby").dirname
-  end
-
-  def message; <<-EOS.undent
-    The Heroku Toolbelt needs Ruby >=1.9
-    EOS
-  end
-end
-
 class HerokuToolbelt < Formula
   homepage "https://toolbelt.heroku.com/other"
   url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.31.2.tgz"
   sha256 "8e859fc1f95508ddfb4684f752ced4c2876aa94839830619ec659f83d72050fd"
   head "https://github.com/heroku/heroku.git"
 
-  depends_on Ruby19
+  depends_on :ruby => "1.9"
 
   def install
     libexec.install Dir["*"]
