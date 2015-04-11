85161631b276d297203c83a31237daf1443065e4
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-11 10:18:23 +0200

    shorten: test added

--- Library/Formula/shorten.rb
+++ Library/Formula/shorten.rb
@@ -1,13 +1,17 @@
-require 'formula'
-
 class Shorten < Formula
-  homepage 'http://www.etree.org/shnutils/shorten/'
-  url 'http://www.etree.org/shnutils/shorten/dist/src/shorten-3.6.1.tar.gz'
-  sha1 'bd525ced0b9ba99a7a178c11647a853147977fa5'
+  homepage "http://www.etree.org/shnutils/shorten/"
+  url "http://www.etree.org/shnutils/shorten/dist/src/shorten-3.6.1.tar.gz"
+  sha256 "ce22e0676c93494ee7d094aed9b27ad018eae5f2478e8862ae1e962346405b66"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/shorten", test_fixtures("test.wav"), "test"
+    assert File.exist? "test"
   end
 end
