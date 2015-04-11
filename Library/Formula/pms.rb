c7d492edfc062fd44aa3a0df48aecb4cabf0432c
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-11 10:39:30 +0200

    pms: test added

--- Library/Formula/pms.rb
+++ Library/Formula/pms.rb
@@ -1,17 +1,20 @@
-require 'formula'
-
 class Pms < Formula
-  homepage 'http://pms.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/pms/pms/0.42/pms-0.42.tar.bz2'
-  sha1 '344a1d211d752e32d32f42a3ad0ea513fcc42f81'
+  homepage "http://pms.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/pms/pms/0.42/pms-0.42.tar.bz2"
+  sha256 "96bf942b08cba10ee891a63eeccad307fd082ef3bd20be879f189e1959e775a6"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gettext'
-  depends_on 'glib'
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "glib"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match(/Practical Music Search v#{version}/,
+                 shell_output("#{bin}/pms -?", 4))
   end
 end
