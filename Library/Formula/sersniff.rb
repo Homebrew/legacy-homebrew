20523c212df341e3a35ce02cbbe9f600c62ffb6f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-11 11:22:08 +0100

    sersniff: add 0.0.5 bottle.

--- Library/Formula/sersniff.rb
+++ Library/Formula/sersniff.rb
@@ -5,6 +5,13 @@ class Sersniff < Formula
 
   head "git://the.earth.li/sersniff"
 
+  bottle do
+    cellar :any
+    sha256 "ef92e5ed1bf8ebeba3d0584a7ed0b2bb9fef53947e16e4cbbf9834f43f7a23be" => :yosemite
+    sha256 "40b46ca2b8bf6097f3a6f2681387627546ec4f47dc1f97acc5c84ad46a2d191a" => :mavericks
+    sha256 "f7e6c501ae82d3c2291bf0bbbdde5e6e8ad6731798efb21e12b44db6ee5d39fb" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "sersniff"
