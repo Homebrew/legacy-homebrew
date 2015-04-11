583ddadfa39d6836442ce7e357c9ff7c73d12ce5
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-11 12:14:18 +0200

    sersniff: basic test added
    Closes #38546.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/sersniff.rb
+++ Library/Formula/sersniff.rb
@@ -1,15 +1,18 @@
-require 'formula'
-
 class Sersniff < Formula
-  homepage 'http://www.earth.li/projectpurple/progs/sersniff.html'
-  url 'http://www.earth.li/projectpurple/files/sersniff-0.0.5.tar.gz'
-  sha1 'f320171e38ce8877646be65664328071254ed3ce'
+  homepage "http://www.earth.li/projectpurple/progs/sersniff.html"
+  url "http://www.earth.li/projectpurple/files/sersniff-0.0.5.tar.gz"
+  sha256 "8aa93f3b81030bcc6ff3935a48c1fd58baab8f964b1d5e24f0aaecbd78347209"
 
-  head 'git://the.earth.li/sersniff'
+  head "git://the.earth.li/sersniff"
 
   def install
     system "make"
     bin.install "sersniff"
     man8.install "sersniff.8"
+  end
+
+  test do
+    assert_match(/sersniff v#{version}/,
+                 shell_output("#{bin}/sersniff -h 2>&1", 1))
   end
 end
