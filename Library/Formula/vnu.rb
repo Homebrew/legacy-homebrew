73a2a72e1e8593a1c0b38e5b00dee22088066336
Alex Dunn <dunn.alex@gmail.com>
2015-04-12 10:29:04 -0700

    vnu 20150412
    Closes #38588.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/vnu.rb
+++ Library/Formula/vnu.rb
@@ -1,8 +1,8 @@
 class Vnu < Formula
   homepage "https://validator.github.io/validator/"
-  url "https://github.com/validator/validator/releases/download/15.3.28/vnu.jar_15.3.28.zip"
-  sha256 "5b7d17fca4900253d30d0b367afa1b7afc3b8f09f3cfa4e4e656e679b21d7841"
-  version "20150328"
+  url "https://github.com/validator/validator/releases/download/15.4.12/vnu.jar_15.4.12.zip"
+  sha256 "2ae32a816aea9a7f60f359bdace9324d1a5532da61b8fb494eefdddc85148ba9"
+  version "20150412"
 
   def install
     libexec.install "vnu.jar"
