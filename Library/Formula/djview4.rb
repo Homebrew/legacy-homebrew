15152f04adaab69e2a5d6a8e6149f4a52f177637
theirix <theirix@gmail.com>
2015-04-13 01:02:49 +0300

    Updates djview4 to 4.10
    Upgraded djview4 to 4.10
    Dropped unneeded "myswap" patch.
    Added patches to fix configure.ac and Makefile.am

--- Library/Formula/djview4.rb
+++ Library/Formula/djview4.rb
@@ -2,22 +2,27 @@ require 'formula'
 
 class Djview4 < Formula
   homepage 'http://djvu.sourceforge.net/djview4.html'
-  url 'https://downloads.sourceforge.net/project/djvu/DjView/4.8/djview-4.8.tar.gz'
-  sha1 '266d207afb63a1ee63eed054190bf88888fda572'
+  url 'https://downloads.sourceforge.net/project/djvu/DjView/4.10/djview-4.10.tar.gz'
+  sha1 '7526d23aa501ce34468e42f094f49b6b96cce186'
 
   depends_on 'pkg-config' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
   depends_on 'djvulibre'
   depends_on 'qt'
 
-  # Patch for Qt 4.8 compatibility. See:
-  # https://build.opensuse.org/package/view_file?file=djview4-qt-4.8.patch&package=djvulibre-djview4&project=graphics&rev=8c40ae0f91469bb1af80f36c24516251
-  # When updating this formula, check if this patch is still needed.
+  # Patch build files for correct flag and dependency handling (known problems at upstream)
   patch :DATA
 
+  ENV['NOCONFIGURE'] = '1'
+
   def install
+    system "./autogen.sh"
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--with-x=no",
+                          "--disable-nsdejavu",
                           "--disable-desktopfiles"
     system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}"
 
@@ -30,40 +35,26 @@ end
 
 
 __END__
-Index: djview-4.8/src/qdjvuwidget.cpp
-===================================================================
---- djview-4.8.orig/src/qdjvuwidget.cpp
-+++ djview-4.8/src/qdjvuwidget.cpp
-@@ -153,7 +153,7 @@ all_numbers(const char *s)
- }
+--- djview-4.10-orig/src/Makefile.am	2015-02-10 04:50:29.000000000 +0300
++++ djview-4.10/src/Makefile.am	2015-04-12 23:01:55.000000000 +0300
+@@ -12,7 +12,7 @@
+ bin_SCRIPTS = djview
  
- template<class T> static inline void 
--swap(T& x, T& y)
-+myswap(T& x, T& y)
- {
-   T tmp;
-   tmp = x;
-@@ -173,11 +173,11 @@ ksmallest(T *v, int n, int k)
-       /* Sort v[lo], v[m], v[hi] by insertion */
-       m = (lo+hi)/2;
-       if (v[lo]>v[m])
--        swap(v[lo],v[m]);
-+        myswap(v[lo],v[m]);
-       if (v[m]>v[hi]) {
--        swap(v[m],v[hi]);
-+        myswap(v[m],v[hi]);
-         if (v[lo]>v[m])
--          swap(v[lo],v[m]);
-+          myswap(v[lo],v[m]);
-       }
-       /* Extract pivot, place sentinel */
-       pivot = v[m];
-@@ -191,7 +191,7 @@ ksmallest(T *v, int n, int k)
-       do ++l; while (v[l]<pivot);
-       do --h; while (v[h]>pivot);
-       if (l < h) { 
--        swap(v[l],v[h]); 
-+        myswap(v[l],v[h]); 
-         goto loop; 
-       }
-       /* Finish up */
+ djview: Makefile.qmake FORCE
+-	${MAKE} -f Makefile.qmake djview
++	${MAKE} -f Makefile.qmake
+ 
+ FORCE: 
+ 
+--- djview-4.10-orig/configure.ac	2015-02-10 04:51:25.000000000 +0300
++++ djview-4.10/configure.ac	2015-04-13 00:42:16.000000000 +0300
+@@ -256,7 +256,7 @@
+     AC_SUBST(NSDEJAVU_LIBS)
+ fi
+ 
+-AM_CONDITIONAL([WANT_NSDEJAVU], [test "x${ac_nsdejavu}" != "no"])
++AM_CONDITIONAL([WANT_NSDEJAVU], [test "x${ac_nsdejavu}" != "xno"])
+ 
+ 
+ # ----------------------------------------
+
