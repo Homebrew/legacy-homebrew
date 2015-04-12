2cb6ee67925db1d1725e44bdd396dd27fb3b849d
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-12 09:51:57 +0200

    nylon: basic test added
    Closes #38578.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/nylon.rb
+++ Library/Formula/nylon.rb
@@ -1,17 +1,21 @@
-require 'formula'
-
 class Nylon < Formula
-  homepage 'http://monkey.org/~marius/pages/?page=nylon'
-  url 'http://monkey.org/~marius/nylon/nylon-1.21.tar.gz'
-  sha1 '96f82a785ffe92fd6c1eebb69787327eecc90569'
+  homepage "http://monkey.org/~marius/pages/?page=nylon"
+  url "http://monkey.org/~marius/nylon/nylon-1.21.tar.gz"
+  sha256 "34c132b005c025c1a5079aae9210855c80f50dc51dde719298e1113ad73408a4"
 
-  depends_on 'libevent'
+  depends_on "libevent"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--with-libevent=#{HOMEBREW_PREFIX}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "nylon: nylon version #{version}",
+      shell_output("#{bin}/nylon -V 2>&1").chomp
   end
 end
