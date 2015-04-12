ea6b078b65685ed9a1826fc4383d0e12b05e3277
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-12 09:54:26 +0200

    most: test added
    Closes #38579.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/most.rb
+++ Library/Formula/most.rb
@@ -1,18 +1,22 @@
-require 'formula'
-
 class Most < Formula
-  homepage 'http://www.jedsoft.org/most/'
-  url 'ftp://space.mit.edu/pub/davis/most/most-5.0.0a.tar.bz2'
-  sha1 'de9fe30ae405c32f8424f10571839519a25f3043'
+  homepage "http://www.jedsoft.org/most/"
+  url "ftp://space.mit.edu/pub/davis/most/most-5.0.0a.tar.bz2"
+  sha256 "94cb5a2e71b6b9063116f4398a002a757e59cd1499f1019dde8874f408485aa9"
 
-  head 'git://git.jedsoft.org/git/most.git'
+  head "git://git.jedsoft.org/git/most.git"
 
-  depends_on 's-lang'
+  depends_on "s-lang"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-slang=#{HOMEBREW_PREFIX}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    text = "This is Homebrew"
+    assert_equal text, pipe_output("#{bin}/most -C", text)
   end
 end
