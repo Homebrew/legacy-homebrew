5ef75315d41bba4714fe6f58884c3263e9a8808f
Daniel Slapman <danslapman@gmail.com>
2015-04-13 13:38:22 +0300

    jython 2.7-rc2

--- Library/Formula/jython.rb
+++ Library/Formula/jython.rb
@@ -6,8 +6,8 @@ class Jython < Formula
   sha1 '6b6ac4354733b6d68d51acf2f3d5c823a10a4ce4'
 
   devel do
-    url 'http://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7-rc1/jython-installer-2.7-rc1.jar'
-    sha1 '772b88e129612cc30636b36d3d9ea47bd18666ec'
+    url 'http://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7-rc2/jython-installer-2.7-rc2.jar'
+    sha1 'd9e1f57d387b0e9d19d29eeb3c860175456e6077'
   end
 
   def install
