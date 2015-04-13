3ade86c4ca4c1ebfdc635d9d6f9103a02e3f3c26
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-13 07:13:52 +0100

    gst-validate: add 1.4.0 bottle.

--- Library/Formula/gst-validate.rb
+++ Library/Formula/gst-validate.rb
@@ -3,6 +3,12 @@ class GstValidate < Formula
   url "http://gstreamer.freedesktop.org/src/gst-validate/gst-validate-1.4.0.tar.xz"
   sha256 "ac30d1041a8cfe5d268439f5cedee7e9753ba49080fc67bff1266581198438d0"
 
+  bottle do
+    sha256 "f5dca749834de5b432007b4c2f3d88dd1aaf1b144b5209318c62fa62a75d8c51" => :yosemite
+    sha256 "3f916cd3acd23dac5159684f709c0944c0977a057b25aec0072bd2312a62fb25" => :mavericks
+    sha256 "a19857658ef39937d9702c20d271068edad58f31e2e9d7fa64278c672d920694" => :mountain_lion
+  end
+
   head do
     url "git://anongit.freedesktop.org/gstreamer/gst-devtools"
 
