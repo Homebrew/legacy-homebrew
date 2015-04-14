8879c40d50b4a9fd77fc2495e984fcf32b5d39a2
Mike McQuaid <mike@mikemcquaid.com>
2015-04-14 13:20:41 -0700

    sfcgal: bump revision, add test, cleanup.
    Closes #38597

--- Library/Formula/sfcgal.rb
+++ Library/Formula/sfcgal.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Sfcgal < Formula
   homepage "http://sfcgal.org/"
   url "https://github.com/Oslandia/SFCGAL/archive/v1.0.5.tar.gz"
   sha256 "a9cdaf7334bf28dc71c6338d090c1d1402041c5e320b6c2e3669f7758946a01c"
-  revision 1
+  revision 2
 
   bottle do
     revision 2
@@ -21,5 +19,9 @@ class Sfcgal < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    assert_equal prefix.to_s, shell_output("#{bin}/sfcgal-config --prefix").strip
   end
 end
