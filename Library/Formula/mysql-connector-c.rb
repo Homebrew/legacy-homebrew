7e4101c3fcc3f00cadc86bd218077f4fc71e1e64
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-14 21:05:25 +0100

    mysql-connector-c: add 6.1.6 bottle.

--- Library/Formula/mysql-connector-c.rb
+++ Library/Formula/mysql-connector-c.rb
@@ -3,6 +3,12 @@ class MysqlConnectorC < Formula
   url "http://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.6-src.tar.gz"
   sha256 "2222433012c415871958b61bc4f3683e1ebe77e3389f698b267058c12533ea78"
 
+  bottle do
+    sha256 "b7cc223beca61228800d26878f7882b23afeb3a8a4297c1b2b84a9faa604a7e4" => :yosemite
+    sha256 "c9a3c310c3eb3f9a0284b6b337bc531e0bb1e5351134fa7d8b26d331aaaebd7b" => :mavericks
+    sha256 "5928f92ae51c122d69863ecfdda5f055bc0f6e8aea02c8ae484f3148747f5984" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   conflicts_with "mysql", "mariadb", "percona-server",
