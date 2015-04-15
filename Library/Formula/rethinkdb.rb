18fa7b2712755bd79ce8a4a1e982d11a626c57a9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-15 08:39:23 +0100

    rethinkdb: update 2.0.0-1 bottle.

--- Library/Formula/rethinkdb.rb
+++ Library/Formula/rethinkdb.rb
@@ -4,9 +4,10 @@ class Rethinkdb < Formula
   sha1 "8eb3d5d1d5b4de62cb7dff8d53ed403ae7b9984e"
 
   bottle do
-    sha256 "1acd044f93fb9e8cca6a297534471a812b6bb7e65068514c2eaca967e60e4753" => :yosemite
-    sha256 "8f6c32d31ebfb2f15b3632dcdd72983a79f157474ffa3dfbb0aa572022d7290c" => :mavericks
-    sha256 "19097d01125c4e32a39a61277eb8a2c33cb7b5667902fed2311f5b10c82ae96e" => :mountain_lion
+    revision 1
+    sha256 "9f79ccf7a5996ab29bc6e4255113134c180cd053220d705a307e2ba8f623071d" => :yosemite
+    sha256 "40ec50be78182f269491d8f54b15f16e87601e5e19f5abf6528f392bd0962059" => :mavericks
+    sha256 "0f02561c97c5c6e4df680f9d693dad4ad6e5c768874ecbc856b5d55444277843" => :mountain_lion
   end
 
   depends_on :macos => :lion
