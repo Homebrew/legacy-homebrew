28bed416388c368253d042186c433cfafea98418
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-15 13:21:51 +0100

    chinadns-c: update 1.3.1 bottle.

--- Library/Formula/chinadns-c.rb
+++ Library/Formula/chinadns-c.rb
@@ -5,9 +5,9 @@ class ChinadnsC < Formula
 
   bottle do
     cellar :any
-    sha1 "68b1bee30ce1870bce29680ef25d3a180674ecd3" => :yosemite
-    sha1 "53c4a55f0bf19f11d06a4d80200fd9e4fc5040ef" => :mavericks
-    sha1 "1ce8fcc9e873ca5aa24133fb0b0c0bd98fde27d3" => :mountain_lion
+    sha256 "5f4c5283c7230f2a7368da43db1a5ac6c5b4adadb4dea9a2e601dfa97d3b5337" => :yosemite
+    sha256 "3b0298252c265079645530d03ab1ebdd784aabdd2445aa0b3dbdf75f93d47b2e" => :mavericks
+    sha256 "1c6594b10d571de1d838869f684fef343c6da4431cfc1294e0950ee19f823f00" => :mountain_lion
   end
 
   def install
