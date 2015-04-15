2c75d7f3d8eb99dbbd5e575d3121d966c2d7ec48
Diego Zamboni <diego@zzamboni.org>
2015-04-14 20:35:01 -0700

    Fixed hugo HEAD compilation
    Updated some libraries and dependencies to make the HEAD compilation complete correctly.

--- Library/Formula/hugo.rb
+++ Library/Formula/hugo.rb
@@ -79,7 +79,7 @@ class Hugo < Formula
   end
   go_resource "github.com/spf13/cobra" do
     url "https://github.com/spf13/cobra.git",
-        :revision => "f8e1ec56bdd7494d309c69681267859a6bfb7549"
+	:revision => "9cb5e8502924a8ff1cce18a9348b61995d7b4fde"
   end
   go_resource "github.com/spf13/fsync" do
     url "https://github.com/spf13/fsync.git",
@@ -105,10 +105,9 @@ class Hugo < Formula
     url "https://github.com/yosssi/ace.git",
         :revision => "1f82044938a7180f6fb6bbb3a29688d1e6dbe74b"
   end
-  go_resource "gopkg.in/fsnotify.v0" do
+  go_resource "gopkg.in/fsnotify.v1" do
     url "https://github.com/go-fsnotify/fsnotify.git",
-        :branch => "v0",
-        :revision => "ea925a0a47d225b2ca7f9932b01d2ed4f3ec74f6"
+	:revision => "6549b98005f3e4026ad9f50ef7d5011f40ba1397"
   end
   go_resource "gopkg.in/yaml.v2" do
     url "https://github.com/go-yaml/yaml.git",
@@ -122,11 +121,16 @@ class Hugo < Formula
         :revision => "8e87604bec3c645a4eeaee97dfec9f25811ff20d"
   end
 
-  # dependency for cobra
+  # dependencies for cobra
   go_resource "github.com/spf13/pflag" do
     url "https://github.com/spf13/pflag.git",
-        :revision => "370c3171201099fa6b466db45c8a032cbce33d8d"
+        :revision => "18d831e92d67eafd1b0db8af9ffddbd04f7ae1f4"
   end
+  go_resource "github.com/inconshreveable/mousetrap" do
+    url "https://github.com/inconshreveable/mousetrap.git",
+	:revision => "76626ae9c91c4f2a10f34cad8ce83ea42c93bb75"
+  end
+
 
   # dependency for purell
   go_resource "github.com/opennota/urlesc" do
