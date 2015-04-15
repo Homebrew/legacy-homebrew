ea33997ca5a6dcdc3cc34c7d94c70dddb3e0d362
Alex Dunn <dunn.alex@gmail.com>
2015-04-15 15:14:32 -0700

    hicolor-icon-theme 0.15
    Closes #38692.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hicolor-icon-theme.rb
+++ Library/Formula/hicolor-icon-theme.rb
@@ -1,9 +1,13 @@
-require 'formula'
-
 class HicolorIconTheme < Formula
-  homepage 'http://icon-theme.freedesktop.org/wiki/HicolorTheme'
-  url 'http://icon-theme.freedesktop.org/releases/hicolor-icon-theme-0.13.tar.gz'
-  sha1 '15e30dfcf5e7b53c1a6f9028c30665006abba55c'
+  homepage "https://wiki.freedesktop.org/www/Software/icon-theme/"
+  url "http://icon-theme.freedesktop.org/releases/hicolor-icon-theme-0.15.tar.xz"
+  sha256 "9cc45ac3318c31212ea2d8cb99e64020732393ee7630fa6c1810af5f987033cc"
+
+  head do
+    url "http://anongit.freedesktop.org/git/xdg/default-icon-theme.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
 
   bottle do
     cellar :any
@@ -14,7 +18,16 @@ class HicolorIconTheme < Formula
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    args = %W[--prefix=#{prefix} --disable-silent-rules]
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+    system "make", "install"
+  end
+
+  test do
+    File.exist? share/"icons/hicolor/index.theme"
   end
 end
