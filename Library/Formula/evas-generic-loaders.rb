23954e3525008dd1d0573321864466557dc79c57
Leif Middelschulte <leif.middelschulte@gmail.com>
2015-04-15 11:58:49 +0200

    evas-generic-loaders: formula formating: ' -> "

--- Library/Formula/evas-generic-loaders.rb
+++ Library/Formula/evas-generic-loaders.rb
@@ -3,8 +3,8 @@ class EvasGenericLoaders < Formula
   url "http://download.enlightenment.org/rel/libs/evas_generic_loaders/evas_generic_loaders-1.14.0-beta1.tar.gz"
   sha256 "787570a63ef59004942b24e9dd28d4e66396c45dcebda105d9f6dfa4b1b6ee16"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'efl'
+  depends_on "pkg-config" => :build
+  depends_on "efl"
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -16,8 +16,8 @@ class EvasGenericLoaders < Formula
   test do
     # `test do` will create, run in and delete a temporary directory.
     #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
+    # This test will fail and we won"t accept that! It"s enough to just replace
+    # "false" with the main program this formula installs, but it"d be nice if you
     # were more thorough. Run the test with `brew test evas_generic_loaders`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
