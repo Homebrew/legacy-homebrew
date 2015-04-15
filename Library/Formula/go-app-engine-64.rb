710e71b42dd4ccd1d41a40a9e102a51839edb1a6
Sam Thorogood <samthor@users.noreply.github.com>
2015-04-15 18:05:04 +1000

    go-app-engine-64: 1.9.19
    Closes #38668.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/go-app-engine-64.rb
+++ Library/Formula/go-app-engine-64.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GoAppEngine64 < Formula
   homepage "https://cloud.google.com/appengine/docs/go/"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.18.zip"
-  sha1 "21721945d0fee2f523d3997205164e7e9888297c"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.19.zip"
+  sha1 "538cb6a92e6fd59c6340aea433d78df670086026"
 
   def install
     cd ".."
