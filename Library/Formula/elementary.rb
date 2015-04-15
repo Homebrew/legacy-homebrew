3c6586825f3e1274f0c365a92847ed9897f5bd86
Leif Middelschulte <leif.middelschulte@gmail.com>
2015-04-15 11:57:43 +0200

    elementary: formula formating: ' -> "

--- Library/Formula/elementary.rb
+++ Library/Formula/elementary.rb
@@ -3,8 +3,8 @@ class Elementary < Formula
   url "http://download.enlightenment.org/rel/libs/elementary/elementary-1.14.0-beta1.tar.gz"
   sha256 "fe45a8a9e2093c9b7d40aff7692a604099dd21d6eace28e17aa3e8c2c28d0cf5"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'efl'
+  depends_on "pkg-config" => :build
+  depends_on "efl"
 
   def install
     system "./configure", "--disable-debug",
