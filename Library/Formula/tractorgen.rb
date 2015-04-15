4bec9e7de6007e2882d7cf047069c3369bc162a3
mroach <mroach@mroach.com>
2015-04-15 09:24:31 +0800

    tractorgen: update homepage and download URL.
    Removed unnecessary configure flags
    
    Closes #38660.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/tractorgen.rb
+++ Library/Formula/tractorgen.rb
@@ -1,13 +1,12 @@
 require 'formula'
 
 class Tractorgen < Formula
-  homepage 'http://www.vergenet.net/~conrad/software/tractorgen/'
-  url 'http://www.vergenet.net/~conrad/software/tractorgen/dl/tractorgen-0.31.7.tar.gz'
+  homepage 'http://www.kfish.org/software/tractorgen/'
+  url 'http://www.kfish.org/software/tractorgen/dl/tractorgen-0.31.7.tar.gz'
   sha1 '7d5d0c84a030a71840ee909b2124797b5281ddcc'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 end
