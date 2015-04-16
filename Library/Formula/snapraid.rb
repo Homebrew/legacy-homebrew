8b53a8d5b852a5e79feb6f35fd37c2a84d2d4aef
Steven Kinley <tseven@gmail.com>
2015-04-16 12:55:40 -0700

    Bumping version to 7.1
    Updated version to 7.1
    new sha1: 3aa3ee982c21c6e19f31988cc1805dc535404334

--- Library/Formula/snapraid.rb
+++ Library/Formula/snapraid.rb
@@ -1,8 +1,8 @@
 class Snapraid < Formula
   homepage "http://snapraid.sourceforge.net/"
   head "git://snapraid.git.sourceforge.net/gitroot/snapraid/snapraid"
-  url "https://downloads.sourceforge.net/project/snapraid/snapraid-7.0.tar.gz"
-  sha1 "aec231ec0ad006cfc2f73cff01ed0fd3254dc111"
+  url "https://downloads.sourceforge.net/project/snapraid/snapraid-7.1.tar.gz"
+  sha1 "3aa3ee982c21c6e19f31988cc1805dc535404334"
 
   def install
     system "./configure", "--prefix=#{prefix}"
