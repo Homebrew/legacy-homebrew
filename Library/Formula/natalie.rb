6c5776f5fd2ce822e5655f8112c1426b1aede5d7
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-04-16 17:05:27 +0200

    indentation update

--- Library/Formula/natalie.rb
+++ Library/Formula/natalie.rb
@@ -5,6 +5,6 @@ class Natalie < Formula
   sha256 "943dab1b7ac16555ec3047983a3c397f7ebe7559d3c098c02a6ee6d8e966655e"
 
   def install
-  	bin.install 'natalie.swift'
+    bin.install 'natalie.swift'
   end
 end
