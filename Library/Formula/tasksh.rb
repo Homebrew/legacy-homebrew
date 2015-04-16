391e55ee229029b79eca0b8e0ffcc5e671df5c63
Christian Drexler <christian@drexler-online.net>
2015-04-16 10:55:08 +0200

    deleted test

--- Library/Formula/tasksh.rb
+++ Library/Formula/tasksh.rb
@@ -5,14 +5,10 @@ class Tasksh < Formula
   sha256 "9accc81f5ae3a985e33be728d56aba0401889d21f856cd94734d73c221bf8652"
   depends_on "task"
   depends_on "cmake" => :build
-  #depends_on :x11 # if your formula requires any X11/XQuartz components
+
 
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
-  end
-
-  test do
-    system "false"
   end
 end
