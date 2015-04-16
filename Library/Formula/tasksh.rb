94bf90f9961c19a3cb935aed16648e7f60cd430a
Christian Drexler <christian@drexler-online.net>
2015-04-16 10:37:58 +0200

    Fixed missing dependency on task itself

--- Library/Formula/tasksh.rb
+++ Library/Formula/tasksh.rb
@@ -3,7 +3,7 @@ class Tasksh < Formula
   url "http://taskwarrior.org/download/tasksh-1.0.0.tar.gz"
   version "1.0.0"
   sha256 "9accc81f5ae3a985e33be728d56aba0401889d21f856cd94734d73c221bf8652"
-
+  depends_on "task"
   depends_on "cmake" => :build
   #depends_on :x11 # if your formula requires any X11/XQuartz components
 
