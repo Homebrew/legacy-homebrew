5f7d66a768f9736a7c8436a855400e8dfe288d19
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-17 00:10:20 +0100

    mypy: add 0.2.0 bottle.

--- Library/Formula/mypy.rb
+++ Library/Formula/mypy.rb
@@ -4,6 +4,13 @@ class Mypy < Formula
   sha256 "0c24f50509bdf3e0d9bd386a08ef4f11ee0114e1f5a9b2afeacbf9561cf022c1"
   head "https://github.com/JukkaL/mypy.git"
 
+  bottle do
+    cellar :any
+    sha256 "6497207e2801737ab861c432fbba890dbfb0b8d85a6b4f809dcc4e3df0699b5f" => :yosemite
+    sha256 "50267f741ceadcf49dfa4115a38e820995d0174cbeb88264b431f5ea93179dea" => :mavericks
+    sha256 "dca4b115dda17d0ea86a1904c59447664fce86b407c3d054483d4c708a5ca54e" => :mountain_lion
+  end
+
   option "without-docs", "Don't build documentation"
 
   depends_on :python3
