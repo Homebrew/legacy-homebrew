233cecd00ce47e73d61a5fd63892b05089a32fcb
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-17 01:36:03 +0200

    ficy 1.0.19
    Closes #38725.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/ficy.rb
+++ Library/Formula/ficy.rb
@@ -1,12 +1,17 @@
-require 'formula'
-
 class Ficy < Formula
-  homepage 'http://www.thregr.org/~wavexx/software/fIcy/'
-  url 'http://www.thregr.org/~wavexx/software/fIcy/releases/fIcy-1.0.18.tar.gz'
-  sha1 '326d1b5417e9507974df94d227c7e7e476b7598f'
+  homepage "http://www.thregr.org/~wavexx/software/fIcy/"
+  url "http://www.thregr.org/~wavexx/software/fIcy/releases/fIcy-1.0.19.tar.gz"
+  sha256 "397df996fd63ce8608e62b195af61e88b0571aac01a51f1935dbf639c5424dcb"
+
+  head "https://github.com/wavexx/fIcy.git"
 
   def install
     system "make"
-    bin.install 'fIcy', 'fPls', 'fResync'
+    bin.install "fIcy", "fPls", "fResync"
+  end
+
+  test do
+    cp test_fixtures("test.mp3"), testpath
+    system "#{bin}/fResync", "-n", "1", "test.mp3"
   end
 end
