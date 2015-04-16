dbde41e794f8e2b6790e5ac205b70e9c13c55145
Christian Drexler <christian@drexler-online.net>
2015-04-16 10:30:02 +0200

    Create tasksh.rb

---
+++ Library/Formula/tasksh.rb
@@ -0,0 +1,18 @@
+class Tasksh < Formula
+  homepage "http://tasktools.org/"
+  url "http://taskwarrior.org/download/tasksh-1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "9accc81f5ae3a985e33be728d56aba0401889d21f856cd94734d73c221bf8652"
+
+  depends_on "cmake" => :build
+  #depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
