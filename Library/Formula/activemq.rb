31a27600a6a03aa4ed8ca7ba618be11288c21973
SouthWolf <wp.southwolf@gmail.com>
2015-04-17 17:07:48 +0800

    Remove trailing spaces

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -16,7 +16,7 @@ class Activemq < Formula
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" 
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
     "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
       <dict>
