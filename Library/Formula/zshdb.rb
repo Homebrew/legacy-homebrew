3519c940781c3982a15ad9562301db95276d98a8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-18 06:01:54 +0100

    zshdb: add 0.09 bottle.

--- Library/Formula/zshdb.rb
+++ Library/Formula/zshdb.rb
@@ -3,6 +3,12 @@ class Zshdb < Formula
   url "https://downloads.sourceforge.net/project/bashdb/zshdb/0.9/zshdb-0.09.tar.bz2"
   sha256 "3ad756485a5bfd014649f4ede15187d0cf070919d2ddb15b4ded61e8990e8d4f"
 
+  bottle do
+    sha256 "42267d6b8d74b9bf81f0b3f57b3fe1f2a724f35a3c9bda366d8167941c78ff59" => :yosemite
+    sha256 "660523d688ea4ee07a8ab93d587a30eee65af059b2dffcd6b44ece9a8ca4e4cf" => :mavericks
+    sha256 "c3c0946b33bad4787934c7837f5e987ff9b29743758501ddc617bd0278d9c8f0" => :mountain_lion
+  end
+
   head do
     url "https://github.com/rocky/zshdb.git"
     depends_on "autoconf" => :build
