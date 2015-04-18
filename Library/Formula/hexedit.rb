659c6da90d1aabca33b8aed5dd92d644c51b73b9
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-18 11:53:34 +0200

    hexedit: test added

--- Library/Formula/hexedit.rb
+++ Library/Formula/hexedit.rb
@@ -1,14 +1,17 @@
-require 'formula'
-
 class Hexedit < Formula
-  homepage 'http://rigaux.org/hexedit.html'
-  url 'http://rigaux.org/hexedit-1.2.13.src.tgz'
-  sha1 '1acb7ca37063d9f4b4d118ef223548fde3b753f1'
+  homepage "http://rigaux.org/hexedit.html"
+  url "http://rigaux.org/hexedit-1.2.13.src.tgz"
+  sha256 "6a126da30a77f5c0b08038aa7a881d910e3b65d13767fb54c58c983963b88dd7"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    shell_output("#{bin}/hexedit -h 2>&1", 1)
   end
 end
