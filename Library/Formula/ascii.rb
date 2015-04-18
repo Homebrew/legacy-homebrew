d61adf74406acfe8b6c0b2ae15b16a8437001df3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-18 04:30:21 +0100

    ascii: add 3.15 bottle.

--- Library/Formula/ascii.rb
+++ Library/Formula/ascii.rb
@@ -3,6 +3,13 @@ class Ascii < Formula
   url "http://www.catb.org/~esr/ascii/ascii-3.15.tar.gz"
   sha256 "ace1db8b64371d53d9ad420d341f2b542324ae70437e37b4b75646f12475ff5f"
 
+  bottle do
+    cellar :any
+    sha256 "b7b74752e577efa60d98732e688910980436e42fbbf1f77a041cb2af458789f5" => :yosemite
+    sha256 "56cec53206fc55f1fcd63b09b69c1afe858f4097ac6a460b7c9c07fbdfeaa0ed" => :mavericks
+    sha256 "1a25c357bde021b59904fc8184c45a5eb85ae6be507a1e100aa79d441ad07943" => :mountain_lion
+  end
+
   head do
     url "git://thyrsus.com/repositories/ascii.git"
     depends_on "xmlto" => :build
