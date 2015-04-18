8cd3d6fd382da7dce3ba517605b12bd30c852b14
Alex Dunn <dunn.alex@gmail.com>
2015-04-17 20:48:26 -0700

    cvsutils 0.2.6

--- Library/Formula/cvsutils.rb
+++ Library/Formula/cvsutils.rb
@@ -1,14 +1,13 @@
-require 'formula'
-
 class Cvsutils < Formula
-  homepage 'http://www.red-bean.com/cvsutils/'
-  url 'http://www.red-bean.com/cvsutils/releases/cvsutils-0.2.5.tar.gz'
-  sha1 '294599ce431be50ad1da7295e8b6a65a17fbf531'
+  homepage "http://www.red-bean.com/cvsutils/"
+  url "http://www.red-bean.com/cvsutils/releases/cvsutils-0.2.6.tar.gz"
+  sha256 "174bb632c4ed812a57225a73ecab5293fcbab0368c454d113bf3c039722695bb"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
