71567fb15a31d6aebdcb17c9e97010ca5f2bc070
Alex Dunn <dunn.alex@gmail.com>
2015-04-17 22:10:24 -0700

    zpaq 7.05

--- Library/Formula/zpaq.rb
+++ Library/Formula/zpaq.rb
@@ -1,26 +1,25 @@
-require "formula"
-
 class Zpaq < Formula
   homepage "http://mattmahoney.net/dc/zpaq.html"
-  url "http://mattmahoney.net/dc/zpaq655.zip"
-  sha1 "0a64fada7112e59a85b8a53c5bf13f22b6f63620"
-  version "6.55"
+  url "http://mattmahoney.net/dc/zpaq705.zip"
+  sha256 "d8abe3e3620d4c6f3ddc1da149acffa4c24296fd9c74c9d7b62319e308b63334"
+  version "7.05"
+
+  head "https://github.com/zpaq/zpaq.git"
 
   def install
-    ENV.append_to_cflags "-Dunix -O3"
-    system "make", "libzpaq.o", "divsufsort.o", "zpaq.o"
-    system "ar", "-r", "libzpaq.a", "libzpaq.o", "divsufsort.o"
-    system ENV.cxx, "-o", "zpaq", "libzpaq.a", "zpaq.o"
-    lib.install "libzpaq.a"
+    system "make"
     include.install "libzpaq.h"
     bin.install "zpaq"
+
+    system "pod2man", "zpaq.pod", "zpaq.1"
+    man1.install "zpaq.1"
   end
 
   test do
-    archive = testpath/'test.zpaq'
+    archive = testpath/"test.zpaq"
     zpaq = bin/"zpaq"
     system zpaq, "a", archive, "#{include}/libzpaq.h"
-    system zpaq, "t", archive
+    system zpaq, "l", archive
     assert_equal "7kSt", archive.read(4)
   end
 end
