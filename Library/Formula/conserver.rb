260662d96ed70560246caa8bd6a3bc7871beae84
Alex Dunn <dunn.alex@gmail.com>
2015-04-18 12:14:24 -0700

    conserver 8.2.0
    Closes #38798.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/conserver.rb
+++ Library/Formula/conserver.rb
@@ -1,13 +1,19 @@
-require 'formula'
-
 class Conserver < Formula
-  homepage 'http://conserver.com'
-  url 'http://conserver.com/conserver-8.1.19.tar.gz'
-  sha1 'e13974427a91f3740057fb1170378021f311e7ac'
+  homepage "http://conserver.com"
+  url "http://conserver.com/conserver-8.2.0.tar.gz"
+  sha256 "a05beb7b66c0228ece62786c3cfe4b0048c5d508e072fb3631c2bf460cab904f"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    console = fork do
+      system bin/"console", "-n", "-p", "8000", "test"
+    end
+    sleep 1
+    Process.kill("TERM",console)
   end
 end
