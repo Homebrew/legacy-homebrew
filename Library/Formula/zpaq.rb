a127998aa6863476a89dc25d9f79ff3172f62a1e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-18 06:20:19 +0100

    zpaq: add 7.05 bottle.

--- Library/Formula/zpaq.rb
+++ Library/Formula/zpaq.rb
@@ -6,6 +6,13 @@ class Zpaq < Formula
 
   head "https://github.com/zpaq/zpaq.git"
 
+  bottle do
+    cellar :any
+    sha256 "bdd4716595b8eed1c52d2a2ef43abb095a996d05a0489f81f0b22ae43a26d51d" => :yosemite
+    sha256 "a71ad4808da8c69844f72322ff7633b895d034028129b30c3d25409d91bd4213" => :mavericks
+    sha256 "a2d2215b51cc18370f5ebe700160d1d7143ce5d680b2365a5adb4bb1622d06fc" => :mountain_lion
+  end
+
   def install
     system "make"
     include.install "libzpaq.h"
