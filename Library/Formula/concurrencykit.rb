c756555064a72a4e00a19fef0d96c627dbcc31d1
Alex Dunn <dunn.alex@gmail.com>
2015-04-18 14:38:02 -0700

    concurrencykit 0.4.5
    Closes #38801.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/concurrencykit.rb
+++ Library/Formula/concurrencykit.rb
@@ -1,11 +1,10 @@
-require 'formula'
-
 class Concurrencykit < Formula
-  homepage 'http://concurrencykit.org'
-  url 'http://concurrencykit.org/releases/ck-0.4.4.tar.gz'
-  sha1 'bfddebf5af1056ddab1345711e5836563131a252'
+  homepage "http://concurrencykit.org"
+  url "http://concurrencykit.org/releases/ck-0.4.5.tar.gz"
+  mirror "https://github.com/concurrencykit/ck/archive/0.4.5.tar.gz"
+  sha256 "89feea338cd6a8efbe7bd64d033cefccb34775ea0bedbcb1612df2b822fa0356"
 
-  head 'https://github.com/concurrencykit/ck.git'
+  head "https://github.com/concurrencykit/ck.git"
 
   bottle do
     cellar :any
@@ -16,7 +15,20 @@ class Concurrencykit < Formula
 
   def install
     system "./configure", "--prefix=#{prefix}"
-    system "make", "CC=#{ENV.cc}"
-    system "make install"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <ck_spinlock.h>
+      int main()
+      {
+          return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lck",
+           testpath/"test.c", "-o", testpath/"test"
+    system "./test"
   end
 end
