418fd2889bbce0d0bfde99186d83906242cfc511
Alex Dunn <dunn.alex@gmail.com>
2015-04-17 19:27:45 -0700

    ascii 3.15
    Closes #38774.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/ascii.rb
+++ Library/Formula/ascii.rb
@@ -1,14 +1,21 @@
-require 'formula'
-
 class Ascii < Formula
-  homepage 'http://www.catb.org/~esr/ascii/'
-  url 'http://www.catb.org/~esr/ascii/ascii-3.14.tar.gz'
-  sha1 'fd8281078c1b3d52d4080ced1855f4f540d5a501'
+  homepage "http://www.catb.org/~esr/ascii/"
+  url "http://www.catb.org/~esr/ascii/ascii-3.15.tar.gz"
+  sha256 "ace1db8b64371d53d9ad420d341f2b542324ae70437e37b4b75646f12475ff5f"
+
+  head do
+    url "git://thyrsus.com/repositories/ascii.git"
+    depends_on "xmlto" => :build
+  end
 
   def install
+    if build.head?
+      ENV["XML_CATALOG_FILES"] = HOMEBREW_PREFIX/"etc/xml/catalog"
+    end
+    bin.mkpath
+    man1.mkpath
     system "make"
-    bin.install "ascii"
-    man1.install 'ascii.1'
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
