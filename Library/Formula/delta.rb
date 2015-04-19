c45ebb6a378dc8bf65c50b04ea2de4c3188f898b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-19 21:01:28 +0100

    delta: add 2006.08.03 bottle.

--- Library/Formula/delta.rb
+++ Library/Formula/delta.rb
@@ -3,6 +3,13 @@ class Delta < Formula
   url "https://mirrors.kernel.org/debian/pool/main/d/delta/delta_2006.08.03.orig.tar.gz"
   sha256 "38184847a92b01b099bf927dbe66ef88fcfbe7d346a7304eeaad0977cb809ca0"
 
+  bottle do
+    cellar :any
+    sha256 "1a2d27b054e1f63ec5770a12e0ed86236a16e9920f4c1765e44cf73967a64fa9" => :yosemite
+    sha256 "5c0280a5fc93da0867237ca066e8fa14f8162af9aaf75a8dbd5a1cfc5df48f9c" => :mavericks
+    sha256 "f3a5161d75202d42fe446ea7f1bdde25d5a55a6ca48e0ff1500c07f55b67e978" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "delta", "multidelta", "topformflat"
