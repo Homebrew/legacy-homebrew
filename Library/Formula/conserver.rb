c136947e16cfa908ae6fa711f2660caac87a883e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-19 20:16:00 +0100

    conserver: add 8.2.0 bottle.

--- Library/Formula/conserver.rb
+++ Library/Formula/conserver.rb
@@ -3,6 +3,12 @@ class Conserver < Formula
   url "http://conserver.com/conserver-8.2.0.tar.gz"
   sha256 "a05beb7b66c0228ece62786c3cfe4b0048c5d508e072fb3631c2bf460cab904f"
 
+  bottle do
+    sha256 "124e2eab8cea657ee17fea2ed6c9bd5592f172fa401b72f63f37299989a36b22" => :yosemite
+    sha256 "faa174dbcf430a5410654ce87e2cc69bd0ee4382f253dff38ff5ee4f992c259d" => :mavericks
+    sha256 "81aee0e591a8c878a55cf173c0d9b70e3539bb7cebbd81466ffabd270ee07ce2" => :mountain_lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
