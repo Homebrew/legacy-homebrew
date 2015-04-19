b4a1f657972eb6fd33721477d0eae90391b82ede
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-19 10:37:46 +0100

    dirmngr: update 1.1.1_1 bottle.

--- Library/Formula/dirmngr.rb
+++ Library/Formula/dirmngr.rb
@@ -6,10 +6,10 @@ class Dirmngr < Formula
   revision 1
 
   bottle do
-    revision 1
-    sha1 "55a34a49127b13bf4b118b404b5fe9c204dffb96" => :yosemite
-    sha1 "d59388da85e392b7ab6b4057b2b7db270d29807f" => :mavericks
-    sha1 "ae23c406d2c1b181ad987d08121cea3f20e04a6e" => :mountain_lion
+    revision 2
+    sha256 "ca8d166e23d655deb6fba0f22b675482791bcfe1008ec07bdef8bfdc5f7240ec" => :yosemite
+    sha256 "ce5b93c43cf4d15a6510782663583683a1fa0582f4f2a270230c91b081ae2ba8" => :mavericks
+    sha256 "5b4f0faf48899220e39b649aed3d3a772393bfa02f066dcfb8b6d47cc832165d" => :mountain_lion
   end
 
   depends_on "libassuan"
