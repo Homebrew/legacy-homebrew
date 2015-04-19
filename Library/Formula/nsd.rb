b517e4af6427e3ea4cbaf77c88e1ce62d4571375
Anand Buddhdev <arhbkb@gmail.com>
2015-04-19 12:06:42 +0200

    nsd 4.1.2
    Made formula comply with brew style guide; "brew audit nsd --strict" is now happy
    
    Closes #38813.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/nsd.rb
+++ Library/Formula/nsd.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Nsd < Formula
-  homepage 'http://www.nlnetlabs.nl/projects/nsd/'
-  url 'http://www.nlnetlabs.nl/downloads/nsd/nsd-4.1.1.tar.gz'
-  sha1 '11ca8a7bef74500f486db5deac994fdf3dec7358'
+  homepage "http://www.nlnetlabs.nl/projects/nsd/"
+  url "http://www.nlnetlabs.nl/downloads/nsd/nsd-4.1.2.tar.gz"
+  sha256 "8514b75bb8884526a637e1666911f429e0f52c5a3b0186104bb111371993644d"
 
   bottle do
     sha1 "e0b29e8c010ea0e20949e3a7bdf2206136d68e2a" => :yosemite
@@ -11,12 +9,12 @@ class Nsd < Formula
     sha1 "a0ec3d813f678d8f83f11011693fcdbed0ebe4f5" => :mountain_lion
   end
 
-  option 'with-root-server', 'Allow NSD to run as a root name server'
-  option 'with-bind8-stats', 'Enable BIND8-like NSTATS & XSTATS'
-  option 'with-ratelimit', 'Enable rate limiting'
-  option 'with-zone-stats', 'Enable per-zone statistics'
+  option "with-root-server", "Allow NSD to run as a root name server"
+  option "with-bind8-stats", "Enable BIND8-like NSTATS & XSTATS"
+  option "with-ratelimit", "Enable rate limiting"
+  option "with-zone-stats", "Enable per-zone statistics"
 
-  depends_on 'libevent'
+  depends_on "libevent"
 
   def install
     args = %W[
@@ -24,12 +22,16 @@ class Nsd < Formula
       --with-libevent=#{Formula["libevent"].opt_prefix}
     ]
 
-    args << '--enable-root-server' if build.with? 'root-server'
-    args << '--enable-bind8-stats' if build.with? 'bind8-stats'
-    args << '--enable-ratelimit' if build.with? 'ratelimit'
-    args << '--enable-zone-stats' if build.with? 'zone-stats'
+    args << "--enable-root-server" if build.with? "root-server"
+    args << "--enable-bind8-stats" if build.with? "bind8-stats"
+    args << "--enable-ratelimit" if build.with? "ratelimit"
+    args << "--enable-zone-stats" if build.with? "zone-stats"
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{sbin}/nsd", "-v"
   end
 end
