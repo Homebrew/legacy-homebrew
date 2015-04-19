8dfbd7de7a3eefd7518aa5d38f77c2ed104a3eac
Mike McQuaid <mike@mikemcquaid.com>
2015-04-19 11:26:46 +0100

    mplayershell: fix strict audit.

--- Library/Formula/mplayershell.rb
+++ Library/Formula/mplayershell.rb
@@ -1,19 +1,17 @@
-require 'formula'
-
 class MPlayerRequirement < Requirement
   fatal true
-  default_formula 'mplayer'
+  default_formula "mplayer"
 
-  satisfy { which('mplayer') || which('mplayer2') }
+  satisfy { which("mplayer") || which("mplayer2") }
 end
 
 class Mplayershell < Formula
   desc "Improved visual experience for MPlayer on OS X"
-  homepage 'https://github.com/donmelton/MPlayerShell'
-  url 'https://github.com/donmelton/MPlayerShell/archive/0.9.3.tar.gz'
-  sha1 '22af911cfd379d34756c0a59b07fcdeaad77c493'
+  homepage "https://github.com/donmelton/MPlayerShell"
+  url "https://github.com/donmelton/MPlayerShell/archive/0.9.3.tar.gz"
+  sha256 "a1751207de9d79d7f6caa563a3ccbf9ea9b3c15a42478ff24f5d1e9ff7d7226a"
 
-  head 'https://github.com/donmelton/MPlayerShell.git'
+  head "https://github.com/donmelton/MPlayerShell.git"
 
   bottle do
     cellar :any
