0a1765b99b9798c6673fdceb744aa4ae98b6d348
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-19 23:01:14 +0100

    jlog: add 2.0.1 bottle.

--- Library/Formula/jlog.rb
+++ Library/Formula/jlog.rb
@@ -3,6 +3,13 @@ class Jlog < Formula
   url "https://github.com/omniti-labs/jlog/archive/2.0.1.tar.gz"
   sha256 "ce4ec8495798e4f50af878f9fc3b922a46dd41df498a5602c802757df16499b7"
 
+  bottle do
+    cellar :any
+    sha256 "72dcde70465b3483a9cca19b8ca6934a99176bfdb6582f89dd3d6b8009bf2f3b" => :yosemite
+    sha256 "d0cd10cec8add751435cc2fda57d95b105df3ad2c5e58610051c125fdd52de47" => :mavericks
+    sha256 "5bf4c685c14c48034909ac2f6a0346fc7992ae11e659e7f8b99964d7d41813c9" => :mountain_lion
+  end
+
   depends_on "automake" => :build
   depends_on "autoconf" => :build
 
