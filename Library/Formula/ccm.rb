5d8c2e0bba04234695efd1cd5f91f9e0eb78fbfa
Alex Dunn <dunn.alex@gmail.com>
2015-04-18 21:06:43 -0700

    ccm 2.0.3
    Closes #38805.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ccm.rb
+++ Library/Formula/ccm.rb
@@ -1,7 +1,7 @@
 class Ccm < Formula
   homepage "https://github.com/pcmanus/ccm"
-  url "https://github.com/pcmanus/ccm/archive/ccm-2.0.2.tar.gz"
-  sha1 "d6fc0d5b5640f16c08919308c0ab4298bfb90823"
+  url "https://github.com/pcmanus/ccm/archive/ccm-2.0.3.tar.gz"
+  sha256 "591c441c6d1b06595d1112951c6edccec29e5e793ee8562c318eeee84fc62301"
   head "https://github.com/pcmanus/ccm.git"
 
   bottle do
@@ -15,17 +15,17 @@ class Ccm < Formula
 
   resource "six" do
     url "https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz"
-    sha1 "d168e6d01f0900875c6ecebc97da72d0fda31129"
+    sha256 "e24052411fc4fbd1f672635537c3fc2330d9481b18c0317695b46259512c91d5"
   end
 
   resource "psutil" do
     url "https://pypi.python.org/packages/source/p/psutil/psutil-2.2.1.tar.gz"
-    sha1 "ddf58b3a0e699e142586b67097e3ae062766f11d"
+    sha256 "a0e9b96f1946975064724e242ac159f3260db24ffa591c3da0a355361a3a337f"
   end
 
   resource "pyyaml" do
     url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
-    sha1 "1a2d5df8b31124573efb9598ec6d54767f3c4cd4"
+    sha256 "c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8"
   end
 
   def install
@@ -44,6 +44,6 @@ class Ccm < Formula
   end
 
   test do
-    assert_match /Usage/, shell_output("#{bin}/ccm; 2>&1")
+    assert_match(/Usage/, shell_output("#{bin}/ccm; 2>&1"))
   end
 end
