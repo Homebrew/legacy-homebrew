b4b656a1e9bda8d6f5030ebb5af94b3db155f8d9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-19 21:09:45 +0100

    jmxtrans: add 20150330-249 bottle.

--- Library/Formula/jmxtrans.rb
+++ Library/Formula/jmxtrans.rb
@@ -4,6 +4,13 @@ class Jmxtrans < Formula
   version "20150330-249"
   sha256 "770699c04d3cbc36f877551821e15b6daa6dd9decf76b971f70534533854f7f2"
 
+  bottle do
+    cellar :any
+    sha256 "a8acc895c3c7d2b214c0fc3ba19191deb5b3293566377dc6a7d30ebbcd77c254" => :yosemite
+    sha256 "707e8bdf07687133290f9cfc3d3ab4861ee23785c154ef825a134ab5dc6363de" => :mavericks
+    sha256 "22ec2c5971a36b82121cdf25135613fc7bcbbe13a4f77c56141ff5dda01ad0d5" => :mountain_lion
+  end
+
   depends_on :java => "1.6+"
   depends_on "maven" => :build
 
