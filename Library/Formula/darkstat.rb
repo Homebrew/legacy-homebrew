07692b639dd523d06df21c6902c830579cf38270
Alex Dunn <dunn.alex@gmail.com>
2015-04-20 11:57:43 -0700

    darkstat 3.0.718
    Closes #38865.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/darkstat.rb
+++ Library/Formula/darkstat.rb
@@ -1,16 +1,19 @@
-require 'formula'
-
 class Darkstat < Formula
-  homepage 'http://unix4lyfe.org/darkstat/'
-  url 'http://unix4lyfe.org/darkstat/darkstat-3.0.717.tar.bz2'
-  sha1 '3a774ab48f9523a3a47a5f2b89174843a1b6fa76'
+  homepage "https://unix4lyfe.org/darkstat/"
+  url "https://unix4lyfe.org/darkstat/darkstat-3.0.718.tar.bz2"
+  sha256 "682f3e53f4e89ea6ad08236b4225a5e0859428299765d8d995374cd7fa22adff"
+
+  devel do
+    url "https://unix4lyfe.org/darkstat/darkstat-3.0.719rc1.tar.bz2"
+    sha256 "827e91aa9261d3f6783cf3f8affa80590800cc5740dcac5f42c88e2bc781390b"
+  end
 
   def install
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system "#{sbin}/darkstat", "--version"
+    system sbin/"darkstat", "--verbose", "-r", test_fixtures("test.pcap")
   end
 end
