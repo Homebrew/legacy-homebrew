281be6cb14d9e79fd3a4f153e9c3473ba5d86852
Alex Dunn <dunn.alex@gmail.com>
2015-04-19 17:17:40 -0700

    djvulibre 3.5.27
    Closes #38839.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/djvulibre.rb
+++ Library/Formula/djvulibre.rb
@@ -1,15 +1,14 @@
-require 'formula'
-
 class Djvulibre < Formula
-  homepage 'http://djvu.sourceforge.net/'
+  homepage "http://djvu.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/djvu/DjVuLibre/3.5.27/djvulibre-3.5.27.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/d/djvulibre/djvulibre_3.5.27.orig.tar.gz"
+  sha256 "e69668252565603875fb88500cde02bf93d12d48a3884e472696c896e81f505f"
 
-  stable do
-    url 'http://ftp.de.debian.org/debian/pool/main/d/djvulibre/djvulibre_3.5.25.4.orig.tar.gz'
-    sha1 'c7044201703f30df0f1732c54c6544467412811d'
-    # Fixes 10.9 clang/libcxx:
-    # http://sourceforge.net/p/djvu/bugs/236/#ce5c/a7ca
-    # http://sourceforge.net/p/djvu/djvulibre-git/ci/2c904e/
-    patch :DATA
+  head do
+    url "git://git.code.sf.net/p/djvu/djvulibre-git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
   end
 
   bottle do
@@ -18,17 +17,16 @@ class Djvulibre < Formula
     sha1 "61a53093e18457fb4235cc0afe3717eef851b85e" => :mavericks
   end
 
-  head 'git://git.code.sf.net/p/djvu/djvulibre-git'
-
-  depends_on 'jpeg'
-  depends_on 'libtiff'
+  depends_on "jpeg"
+  depends_on "libtiff"
 
   def install
+    system "./autogen.sh" if build.head?
     # Don't build X11 GUI apps, Spotlight Importer or QuickLook plugin
     system "./configure", "--prefix=#{prefix}", "--disable-desktopfiles"
     system "make"
-    system "make install"
-    (share/'doc/djvu').install Dir['doc/*']
+    system "make", "install"
+    (share/"doc/djvu").install Dir["doc/*"]
   end
 
   test do
@@ -36,16 +34,3 @@ class Djvulibre < Formula
     assert_equal "2", output.strip
   end
 end
-
-__END__
---- a/libdjvu/atomic.h
-+++ b/libdjvu/atomic.h
-@@ -122,7 +122,7 @@
-   static inline int atomicDecrement(int volatile *var) {
-     int ov; __asm__ __volatile__ ("lock; xaddl %0, %1"
-          : "=r" (ov), "=m" (*var) : "0" (-1), "m" (*var) : "cc" );
--    return ov + 1;
-+    return ov - 1;
-   }
-   static inline int atomicExchange(int volatile *var, int nv) {
-     int ov; __asm__ __volatile__ ("xchgl %0, %1"
