2e744cd338cfa7d060edb857a63dab47a90938af
Alex Dunn <dunn.alex@gmail.com>
2015-04-20 08:51:16 -0700

    dfu-programmer 0.7.2

--- Library/Formula/dfu-programmer.rb
+++ Library/Formula/dfu-programmer.rb
@@ -1,16 +1,24 @@
-require "formula"
-
 class DfuProgrammer < Formula
   homepage "http://dfu-programmer.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/dfu-programmer/dfu-programmer/0.7.0/dfu-programmer-0.7.0.tar.gz"
-  sha1 "a8d91053b7ec20185eb87c31f63340474e64b1dd"
+  url "https://downloads.sourceforge.net/project/dfu-programmer/dfu-programmer/0.7.2/dfu-programmer-0.7.2.tar.gz"
+  sha256 "1db4d36b1aedab2adc976e8faa5495df3cf82dc4bf883633dc6ba71f7c4af995"
+
+  head do
+    url "https://github.com/dfu-programmer/dfu-programmer.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
 
   depends_on "libusb-compat"
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                           "--disable-libusb_1_0"
-    system "make install"
+    system "./bootstrap.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-libusb_1_0"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"dfu-programmer", "--targets"
   end
 end
