94cb7b3bae9654b79c39486a7d484eb3983cb806
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-20 17:48:02 +0100

    testdisk: add 7.0 bottle.

--- Library/Formula/testdisk.rb
+++ Library/Formula/testdisk.rb
@@ -3,6 +3,13 @@ class Testdisk < Formula
   url "http://www.cgsecurity.org/testdisk-7.0.tar.bz2"
   sha256 "00bb3b6b22e6aba88580eeb887037aef026968c21a87b5f906c6652cbee3442d"
 
+  bottle do
+    cellar :any
+    sha256 "0ca542c57ca8ed24c3ecb5d5b449dc18fda306744309fb59598e2a7e9c55c08b" => :yosemite
+    sha256 "e537da6b508c8aac5842c3d2094b5b914effea9ea04484cf09ac9bb80d46e3f0" => :mavericks
+    sha256 "afbf7d1f54c14b40d86baadc34efeaed0399257598d60755d910aec978aee7ef" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
