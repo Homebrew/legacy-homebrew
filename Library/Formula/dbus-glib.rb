7d9408553dd322f35f0f228cc966852be4c2bd92
Alex Dunn <dunn.alex@gmail.com>
2015-04-20 13:01:28 -0700

    dbus-glib 0.104
    Closes #38870.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/dbus-glib.rb
+++ Library/Formula/dbus-glib.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class DbusGlib < Formula
-  homepage 'http://www.freedesktop.org/Software/dbus'
-  url 'http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.102.tar.gz'
-  sha1 '58a8955972f6c221461a49f9c541c22e838a5776'
+  homepage "https://wiki.freedesktop.org/www/Software/DBusBindings/"
+  url "http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.104.tar.gz"
+  sha256 "bfc1f1a82bfc3ec3ecafe04d0e87bab7e999f50dce4f4a34d0b89caf6bd821f6"
 
   bottle do
     revision 1
@@ -11,13 +9,18 @@ class DbusGlib < Formula
     sha1 "a3ed176614007538b3fd7e788e0a72e4710b3762" => :mavericks
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gettext'
-  depends_on 'glib'
-  depends_on 'd-bus'
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "d-bus"
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"dbus-binding-tool", "--help"
   end
 end
