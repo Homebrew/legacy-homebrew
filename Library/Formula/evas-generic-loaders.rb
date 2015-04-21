9ef950c9577e10eb2f11d82d7fca93e2e0c23af9
Leif Middelschulte <leif.middelschulte@gmail.com>
2015-04-21 11:06:48 +0200

    evas-generic-loaders: version update

--- Library/Formula/evas-generic-loaders.rb
+++ Library/Formula/evas-generic-loaders.rb
@@ -1,7 +1,7 @@
 class EvasGenericLoaders < Formula
   homepage "https://enlightenment.org"
-  url "http://download.enlightenment.org/rel/libs/evas_generic_loaders/evas_generic_loaders-1.14.0-beta1.tar.gz"
-  sha256 "787570a63ef59004942b24e9dd28d4e66396c45dcebda105d9f6dfa4b1b6ee16"
+  url "http://download.enlightenment.org/rel/libs/evas_generic_loaders/evas_generic_loaders-1.14.0-beta2.tar.gz"
+  sha256 "7434f7230b8a46369b84489a8bb0e97c64f35fffabdda8ab0a788870408b9b35"
 
   depends_on "pkg-config" => :build
   depends_on "efl"
