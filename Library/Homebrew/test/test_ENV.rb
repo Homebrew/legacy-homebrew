9d6e036aaae76a015741b4d70e33f5cc7ffd8221
Jack Nagel <jacknagel@gmail.com>
2015-04-20 22:33:25 -0400

    Revert "Use real repo to run tests"
    This reverts commit 5aaf13c6cf784cb4be62932b3e528e8f93580a65.

--- Library/Homebrew/test/test_ENV.rb
+++ Library/Homebrew/test/test_ENV.rb
@@ -127,12 +127,24 @@ end
 class SuperenvTests < Homebrew::TestCase
   include SharedEnvTests
 
+  attr_reader :env, :bin
+
   def setup
     @env = {}.extend(Superenv)
+    @bin = HOMEBREW_REPOSITORY/"Library/ENV/#{MacOS::Xcode.version}"
+    bin.mkpath
+  end
+
+  def test_bin
+    assert_equal bin, Superenv.bin
   end
 
   def test_initializes_deps
-    assert_equal [], @env.deps
-    assert_equal [], @env.keg_only_deps
+    assert_equal [], env.deps
+    assert_equal [], env.keg_only_deps
+  end
+
+  def teardown
+    bin.rmtree
   end
 end
