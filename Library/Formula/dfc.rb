635d731af6438dbae8d836a154cda19f043216c9
Alex Dunn <dunn.alex@gmail.com>
2015-04-20 22:24:58 -0700

    dfc 3.0.5
    Closes #38885.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/dfc.rb
+++ Library/Formula/dfc.rb
@@ -1,15 +1,22 @@
-require 'formula'
-
 class Dfc < Formula
-  homepage 'http://projects.gw-computing.net/projects/dfc'
-  url 'http://projects.gw-computing.net/attachments/download/79/dfc-3.0.4.tar.gz'
-  sha1 'e3b7fc7474f2ca36c1370a3dcbcd3d1020766f1a'
+  homepage "http://projects.gw-computing.net/projects/dfc"
+  url "http://projects.gw-computing.net/attachments/download/467/dfc-3.0.5.tar.gz"
+  sha256 "3c947a1d6bc53347b1643921dcbf4c6f8fe7eb6167fc1f4e9436366f036d857a"
 
-  depends_on 'cmake' => :build
-  depends_on 'gettext'
+  head "https://github.com/Rolinh/dfc.git"
+
+  depends_on "cmake" => :build
+  depends_on "gettext"
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system bin/"dfc", "-T"
+    assert_match ",%USED,", shell_output("#{bin}/dfc -e csv")
   end
 end
