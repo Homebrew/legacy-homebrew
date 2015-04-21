dc127104a118fcfe3378f35b37655a4a4c21e4de
Josh Hagins <hagins.josh@gmail.com>
2015-04-20 21:41:16 -0400

    scalariform: add HEAD
    Closes #38881.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/scalariform.rb
+++ Library/Formula/scalariform.rb
@@ -3,9 +3,20 @@ class Scalariform < Formula
   url "https://github.com/daniel-trinh/scalariform/releases/download/0.1.6/scalariform.jar"
   sha256 "346276c5f3a25a44d64ed38f43739813933487299a651f7c64db748427641c54"
 
+  head do
+    url "https://github.com/daniel-trinh/scalariform.git"
+    depends_on "sbt" => :build
+  end
+
   def install
-    libexec.install "scalariform.jar"
-    bin.write_jar_script libexec/"scalariform.jar", "scalariform"
+    if build.head?
+      system "sbt", "project cli", "assembly"
+      libexec.install Dir["cli/target/scala-*/cli-assembly-*.jar"]
+      bin.write_jar_script Dir[libexec/"cli-assembly-*.jar"][0], "scalariform"
+    else
+      libexec.install "scalariform.jar"
+      bin.write_jar_script libexec/"scalariform.jar", "scalariform"
+    end
   end
 
   test do
