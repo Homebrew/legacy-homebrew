8431ac489ee70c7876030167ad8f5e7f69818f72
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-21 20:12:15 +0100

    epic5: add 1.1.10 bottle.

--- Library/Formula/epic5.rb
+++ Library/Formula/epic5.rb
@@ -4,6 +4,12 @@ class Epic5 < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/e/epic5/epic5_1.1.10.orig.tar.bz2"
   sha256 "a4f19214e8eb9a7aceaed62d924d96d8c9359b186ff230c01daff398dd62cdb5"
 
+  bottle do
+    sha256 "91315372bd0f8ac3604263eca9a05d1c894ee9fa1bec255249d8a45c23127c66" => :yosemite
+    sha256 "772359b5b050ab8fbca51c802e0d8856b61925e458a7d9ebee8941bfadc33b48" => :mavericks
+    sha256 "6c320f0437d0e2dd85b758b35259a258cd9053cb2e8450e0346dd228212d0aea" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
