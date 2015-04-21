2df41076c87677ac5faadeccfb689969284401cc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-21 02:06:33 +0100

    mongrel2: add 1.9.2 bottle.

--- Library/Formula/mongrel2.rb
+++ Library/Formula/mongrel2.rb
@@ -7,6 +7,13 @@ class Mongrel2 < Formula
 
   head 'https://github.com/zedshaw/mongrel2.git'
 
+  bottle do
+    cellar :any
+    sha256 "b25a776905c9e439d63a190bdcb5a6fedf689dc70e22ae2fd371d7ea61dbb6d4" => :yosemite
+    sha256 "a2d42a99ccf6cbf342f558e2ccebf54c010a17014ce8ea2bfde1b4678f9bd98d" => :mavericks
+    sha256 "23a8bf5473815f626590e58124a5a57cf73ca1e05e846dfd28ab160ac4326e18" => :mountain_lion
+  end
+
   depends_on 'zeromq'
 
   def install
