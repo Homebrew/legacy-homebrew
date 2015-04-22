919b8aaa3f0642f87ea26bc819362dca61db1177
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-22 07:03:40 +0100

    thefuck: add 1.26 bottle.

--- Library/Formula/thefuck.rb
+++ Library/Formula/thefuck.rb
@@ -3,6 +3,13 @@ class Thefuck < Formula
   url "https://pypi.python.org/packages/source/t/thefuck/thefuck-1.26.tar.gz"
   sha256 "b81e0d4ac5eb3e3aec1014d948d26419a60a39cc1e20f20d82f7be652ed3b8c8"
 
+  bottle do
+    cellar :any
+    sha256 "826523b7e2239127c65ee38ec6cd05ff630ddb76ae2b34cd2632c23e8651bccb" => :yosemite
+    sha256 "0c45edcf78a080a5fed9851205ea1f295d892f97ca83ecc9039e4609496e5478" => :mavericks
+    sha256 "f51e804a09b7d6699bb1ccca568245f1f71b06c58a656c01b5247df1809e2634" => :mountain_lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   resource "psutil" do
