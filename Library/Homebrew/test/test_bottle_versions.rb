13fabf48282700ff0504f59bc3031583f538ad73
Xu Cheng <xucheng@me.com>
2015-04-22 21:50:26 +0800

    test_bottle_version: update with new BottleVersion class

--- Library/Homebrew/test/test_bottle_versions.rb
+++ Library/Homebrew/test/test_bottle_versions.rb
@@ -3,7 +3,7 @@ require 'bottle_version'
 
 class BottleVersionParsingTests < Homebrew::TestCase
   def assert_version_detected expected, path
-    assert_equal expected, BottleVersion.parse(path).to_s
+    assert_equal expected, BottleVersion.parse(path).version.to_s
   end
 
   def test_perforce_style
@@ -58,7 +58,7 @@ class BottleVersionParsingTests < Homebrew::TestCase
 
   def test_libpano_style
     assert_version_detected '13-2.9.19',
-      'libpano-13-2.9.19_1.yosemite.bottle.tar.gz'
+      'libpano-13-2.9.19.yosemite.bottle.tar.gz'
   end
 
   def test_imapuw_style
