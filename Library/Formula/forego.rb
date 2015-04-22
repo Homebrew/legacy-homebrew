76293d9291c8241e88f35cd39ba10916830383c7
Alex Dunn <dunn.alex@gmail.com>
2015-04-22 10:42:09 -0700

    forego 0.16.1

--- Library/Formula/forego.rb
+++ Library/Formula/forego.rb
@@ -1,10 +1,9 @@
-require "formula"
 require "language/go"
 
 class Forego < Formula
   homepage "https://github.com/ddollar/forego"
-  url "https://github.com/ddollar/forego/archive/v0.13.1.tar.gz"
-  sha1 "63ed315ef06159438e3501512a5b307486d49d5c"
+  url "https://github.com/ddollar/forego/archive/v0.16.1.tar.gz"
+  sha256 "d4c8305262ac18c7e51d9d8028827f83b37fb3f9373d304686d084d68033ac6d"
 
   head "https://github.com/ddollar/forego.git"
 
@@ -16,23 +15,29 @@ class Forego < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/kr/godep" do
-    url "https://github.com/kr/godep.git", :revision => "edcaa96f040b31f4186738decac57f88d6061b8d"
+  go_resource "github.com/tools/godep" do
+    url "https://github.com/tools/godep.git",
+        :revision => "58d90f262c13357d3203e67a33c6f7a9382f9223"
   end
 
   go_resource "github.com/kr/fs" do
-    url "https://github.com/kr/fs.git", :revision => "2788f0dbd16903de03cb8186e5c7d97b69ad387b"
+    url "https://github.com/kr/fs.git",
+        :revision => "2788f0dbd16903de03cb8186e5c7d97b69ad387b"
   end
 
-  go_resource "code.google.com/p/go.tools" do
-    url "https://code.google.com/p/go.tools/", :revision => "140fcaadc586", :using => :hg
+  go_resource "golang.org/x/tools" do
+    url "https://go.googlesource.com/tools",
+        :using => :git,
+        :revision => "b1aed1a596ad02d2aa2eb5c5af431a7ba2f6afc4"
   end
 
   def install
     ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/ddollar/"
+    ln_sf buildpath, buildpath/"src/github.com/ddollar/forego"
     Language::Go.stage_deps resources, buildpath/"src"
 
-    cd "src/github.com/kr/godep" do
+    cd "src/github.com/tools/godep" do
       system "go", "install"
     end
 
