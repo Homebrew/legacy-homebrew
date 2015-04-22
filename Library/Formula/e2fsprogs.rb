5f4267cc91fa818f9bcf9530bab9d233b6d8b5d9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-22 21:05:35 +0100

    e2fsprogs: add 1.42.12 bottle.

--- Library/Formula/e2fsprogs.rb
+++ Library/Formula/e2fsprogs.rb
@@ -6,6 +6,12 @@ class E2fsprogs < Formula
 
   head "https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git"
 
+  bottle do
+    sha256 "d2e1bcb92be39ebf971032b385fe5ffc6b0d69ec5df23fd0f4c5fb739f7f031e" => :yosemite
+    sha256 "e77469345bab82888029836b5b221cfd07ac9a3e3dea79bb0893762312d31094" => :mavericks
+    sha256 "fe5542c41eb4a541ef533d07a086fb3e6c9cdc51157ae9c32f62ac049842a74f" => :mountain_lion
+  end
+
   keg_only "This brew installs several commands which override OS X-provided file system commands."
 
   depends_on "pkg-config" => :build
