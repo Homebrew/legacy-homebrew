6b9aea9a6d79cb59eadc49e9a6e1b5ee51fc6c5d
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-04-22 10:50:11 +0200

    glibmm 2.44.0
    version bump
    passed brew audit --strict after adding test block

--- Library/Formula/glibmm.rb
+++ Library/Formula/glibmm.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Glibmm < Formula
   homepage "http://www.gtkmm.org/"
-  url "http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.42/glibmm-2.42.0.tar.xz"
-  sha256 "985083d97378d234da27a7243587cc0d186897a4b2d3c1286f794089be1a3397"
+  url "http://ftp.gnome.org/pub/GNOME/sources/glibmm/2.44/glibmm-2.44.0.tar.xz"
+  sha256 "1b0ac0425d24895507c0e0e8088a464c7ae2d289c47afa1c11f63278fc672ea8"
 
   bottle do
     revision 1
@@ -18,6 +16,19 @@ class Glibmm < Formula
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <glibmm.h>
+
+      int main(int argc, char *argv[])
+      {
+         Glib::ustring my_string("testing");
+         return 0;
+      }
+    EOS
+    system ENV.cxx, "-I#{HOMEBREW_PREFIX}/include/glibmm-2.4", "-I#{HOMEBREW_PREFIX}/lib/glibmm-2.4/include", "-I#{HOMEBREW_PREFIX}/include/glib-2.0", "-I#{HOMEBREW_PREFIX}/lib/glib-2.0/include", "-I#{HOMEBREW_PREFIX}/opt/gettext/include", "-I#{HOMEBREW_PREFIX}/include/sigc++-2.0", "-I#{HOMEBREW_PREFIX}/lib/sigc++-2.0/include", "test.cpp", "-L#{HOMEBREW_PREFIX}/lib", "-L#{HOMEBREW_PREFIX}/lib", "-L#{HOMEBREW_PREFIX}/opt/gettext/lib", "-L#{HOMEBREW_PREFIX}/lib", "-lglibmm-2.4", "-lgobject-2.0", "-lglib-2.0", "-lintl", "-lsigc-2.0", "-o", "test"
+    system "./test"
   end
 end
