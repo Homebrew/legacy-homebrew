a02fe9b7437bafb75129adbce0ea039e37e8e451
Alex Dunn <dunn.alex@gmail.com>
2015-04-21 18:44:23 -0700

    ecl 15.3.7
    Closes #38920.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/ecl.rb
+++ Library/Formula/ecl.rb
@@ -1,9 +1,9 @@
-require 'formula'
-
 class Ecl < Formula
-  homepage 'http://ecls.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/ecls/ecls/15.2/ecl-15.2.21.tgz'
-  sha1 'fcf70c11ddb1602e88972f0c1596037df229f473'
+  homepage "http://ecls.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/ecls/ecls/15.3/ecl-15.3.7.tgz"
+  sha256 "2dc6ffbbf1e0a7b1323d49a991ba1f005127ca3e153651d91ba9e65bdaec948f"
+
+  head "https://gitlab.com/embeddable-common-lisp/ecl.git"
 
   bottle do
     sha1 "1f5441b4a7cd4f62be787b771060655ec92b0642" => :yosemite
@@ -11,19 +11,22 @@ class Ecl < Formula
     sha1 "ab1ac89fe3e324fc0cf4f74acd058af6a22b235a" => :mountain_lion
   end
 
-  depends_on 'gmp'
+  depends_on "gmp"
 
   def install
-    ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}", "--enable-unicode=yes", "--enable-threads=yes", "--with-system-gmp=yes"
+    system "./configure", "--prefix=#{prefix}",
+                          "--enable-unicode=yes",
+                          "--enable-threads=yes",
+                          "--with-system-gmp=yes"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    (testpath/'simple.cl').write <<-EOS.undent
+    (testpath/"simple.cl").write <<-EOS.undent
       (write-line (write-to-string (+ 2 2)))
     EOS
-    assert_equal "4\n", shell_output("#{bin}/ecl -shell #{testpath}/simple.cl")
+    assert_equal "4",
+                 shell_output("#{bin}/ecl -shell #{testpath}/simple.cl").chomp
   end
 end
