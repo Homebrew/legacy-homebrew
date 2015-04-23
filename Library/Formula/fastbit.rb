e5d785060e116e65046efd5eeaa2849eb7892993
Alex Dunn <dunn.alex@gmail.com>
2015-04-22 19:33:44 -0700

    fastbit 2.0.2

--- Library/Formula/fastbit.rb
+++ Library/Formula/fastbit.rb
@@ -1,19 +1,36 @@
-require 'formula'
-
 class Fastbit < Formula
-  homepage 'https://sdm.lbl.gov/fastbit/'
-  url 'https://codeforge.lbl.gov/frs/download.php/409/fastbit-ibis1.3.8.tar.gz'
-  sha1 '3e0feed7932d34be49ca41fdb689f55b0466a28a'
+  homepage "https://sdm.lbl.gov/fastbit/"
+  url "https://codeforge.lbl.gov/frs/download.php/416/fastbit-2.0.2.tar.gz"
+  sha256 "a9d6254fcc32da6b91bf00285c7820869950bed25d74c993da49e1336fd381b4"
 
-  conflicts_with 'iniparser', :because => 'Both install `include/dictionary.h`'
+  head "https://codeforge.lbl.gov/anonscm/fastbit/trunk",
+       :using => :svn
+
+  conflicts_with "iniparser",
+                 :because => "Both install `include/dictionary.h`"
+
+  depends_on :java
+  needs :cxx11
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    ENV.cxx11
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+    libexec.install lib/"fastbitjni.jar"
+    bin.write_jar_script libexec/"fastbitjni.jar", "fastbitjni"
   end
 
   test do
-    system "#{bin}/fastbit-config", "--version"
+    assert_equal prefix.to_s,
+                 shell_output("#{bin}/fastbit-config --prefix").chomp
+    (testpath/"test.csv").write <<-EOS
+      Potter,Harry
+      Granger,Hermione
+      Weasley,Ron
+     EOS
+    system bin/"ardea", "-d", testpath,
+           "-m", "a:t,b:t", "-t", testpath/"test.csv"
   end
 end
