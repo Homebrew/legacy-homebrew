987f3379441434548afce3f31eabda8ff16eb337
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-23 21:00:34 +0200

    kes: test added

--- Library/Formula/kes.rb
+++ Library/Formula/kes.rb
@@ -1,18 +1,24 @@
-require 'formula'
-
 class Kes < Formula
-  homepage 'https://github.com/epilnivek/kes'
-  url 'https://github.com/epilnivek/kes/archive/v0.9.tar.gz'
-  sha1 '483ff8c76372bc12a852ae10d8d5edc7591cfe09'
+  homepage "https://github.com/epilnivek/kes"
+  url "https://github.com/epilnivek/kes/archive/v0.9.tar.gz"
+  sha256 "d0db16ba7892d9692cacd552d684f03a9d0333ba0e7b629a998fa9c127ef050e"
 
-  head 'https://github.com/epilnivek/kes.git'
+  head "https://github.com/epilnivek/kes.git"
 
-  depends_on 'readline'
+  depends_on "readline"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--with-readline"
-    system "make"
-    bin.install 'es'
-    man1.install 'doc/es.1'
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--with-readline"
+
+    bin.mkpath
+    man1.mkpath
+
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "Homebrew\n", shell_output("#{bin}/es -c 'echo Homebrew'")
   end
 end
