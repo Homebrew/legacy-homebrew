ccde180258f6b8409686ac868d0edd06998ba1ac
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-24 00:33:52 +0100

    kes: add 0.9 bottle.

--- Library/Formula/kes.rb
+++ Library/Formula/kes.rb
@@ -5,6 +5,13 @@ class Kes < Formula
 
   head "https://github.com/epilnivek/kes.git"
 
+  bottle do
+    cellar :any
+    sha256 "84db02dd57749a28718482893d9c032c7056ddc32ea9d305d38172ca14199c80" => :yosemite
+    sha256 "b07636ad0d38972397fcc64059b2e5505110400415e41ff0417ecccb71fcff38" => :mavericks
+    sha256 "a736dd5aa01f8a8011f6a05fe82ba43954fdf0f3037e483c96bfda9ee7591c48" => :mountain_lion
+  end
+
   depends_on "readline"
 
   def install
