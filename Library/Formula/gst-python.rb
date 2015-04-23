09a10644319e8a03f17143e5ede1116c51bdfbba
hyunkim <hyunkim9123@gmail.com>
2015-04-23 23:17:52 +0900

    gst-python 1.4.0 (new formula)

---
+++ Library/Formula/gst-python.rb
@@ -0,0 +1,23 @@
+require "formula"
+
+class GstPython < Formula
+  homepage "http://gstreamer.freedesktop.org/modules/gst-python.html"
+  url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.4.0.tar.xz"
+  version "1.4.0"
+  sha256 "b1e40c29ceb41b03f08d38aca6056054f0341d0706276326dceeec6ac8d53d3e"
+
+  head do
+    url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.4.0.tar.xz"
+  end
+  depends_on "gst-plugins-base"
+  depends_on "pygtk"
+  depends_on "pygobject3"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
