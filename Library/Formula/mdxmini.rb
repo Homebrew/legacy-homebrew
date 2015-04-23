523217d973a74d6dcf0906325ff5160c0b42078f
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-23 22:41:27 +0200

    mdxmini 20140608
    Closes #38987.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mdxmini.rb
+++ Library/Formula/mdxmini.rb
@@ -1,31 +1,35 @@
-require 'formula'
-
 class Mdxmini < Formula
-  homepage 'http://clogging.web.fc2.com/psp/'
-  url 'https://github.com/BouKiCHi/mdxplayer/archive/9afbc01f60a12052817cb14a81a8c3c976953506.tar.gz'
-  version '20130115'
-  sha1 '8ca3b597f009ee7de697329e26b9f3c402dda173'
+  homepage "http://clogging.web.fc2.com/psp/"
+  url "https://github.com/BouKiCHi/mdxplayer/archive/ae219b67a9d2a82f43ba35323c1d85d33959d319.tar.gz"
+  version "20140608"
+  sha256 "a3c4f1b60a3771826de9d3615a7485126818811a3b119ee1354e7b1cb84b66b3"
 
-  option "lib-only", "Do not build commandline player"
+  option "with-lib-only", "Do not build commandline player"
+  deprecated_option "lib-only" => "with-lib-only"
 
-  depends_on 'sdl' unless build.include? "lib-only"
+  depends_on "sdl" if build.without? "lib-only"
 
   def install
     cd "jni/mdxmini" do
       # Specify Homebrew's cc
       inreplace "mak/general.mak", "gcc", ENV.cc
-      if build.include? "lib-only"
-        system "make -f Makefile.lib"
+      if build.with? "lib-only"
+        system "make", "-f", "Makefile.lib"
       else
         system "make"
       end
 
       # Makefile doesn't build a dylib
-      system "#{ENV.cc} -dynamiclib -install_name #{lib}/libmdxmini.dylib -o libmdxmini.dylib -undefined dynamic_lookup obj/*.o"
+      system ENV.cc, "-dynamiclib", "-install_name", "#{lib}/libmdxmini.dylib",
+        "-o", "libmdxmini.dylib", "-undefined", "dynamic_lookup", *Dir["obj/*.o"]
 
-      bin.install "mdxplay" unless build.include? "lib-only"
-      lib.install "libmdxmini.a", "libmdxmini.dylib"
-      (include+'libmdxmini').install Dir['src/*.h']
+      bin.install "mdxplay" if build.without? "lib-only"
+      lib.install "libmdxmini.dylib"
+      (include/"libmdxmini").install Dir["src/*.h"]
     end
+  end
+
+  test do
+    assert (include/"libmdxmini/class.h").exist?
   end
 end
