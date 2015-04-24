36189b9ace780cc0df3347ed7d3923e8baa606a1
Paul <hyunkim9123@gmail.com>
2015-04-24 16:01:27 +0900

    Update gst-python.rb

--- Library/Formula/gst-python.rb
+++ Library/Formula/gst-python.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class GstPython < Formula
   homepage "http://gstreamer.freedesktop.org/modules/gst-python.html"
   url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.4.0.tar.xz"
   version "1.4.0"
   sha256 "b1e40c29ceb41b03f08d38aca6056054f0341d0706276326dceeec6ac8d53d3e"
-  
+
   head do
     url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.4.0.tar.xz"
   end
@@ -18,6 +16,6 @@ class GstPython < Formula
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "make", "install" 
+    system "make", "install"
   end
 end
