97ebffce88c22339b203d0696ac5d8482cad11ce
Alex Dunn <dunn.alex@gmail.com>
2015-04-24 10:10:21 -0700

    gambit-scheme 4.7.5

--- Library/Formula/gambit-scheme.rb
+++ Library/Formula/gambit-scheme.rb
@@ -1,9 +1,9 @@
-require 'formula'
-
 class GambitScheme < Formula
-  homepage 'http://dynamo.iro.umontreal.ca/~gambit/wiki/index.php/Main_Page'
-  url 'http://www.iro.umontreal.ca/~gambit/download/gambit/v4.7/source/gambc-v4_7_3.tgz'
-  sha256 '59c4c62f2cfaf698b54a862e7af9c1b3e4cc27e46d386f31c66e00fed4701777'
+  homepage "http://dynamo.iro.umontreal.ca/~gambit/wiki/index.php/Main_Page"
+  url "http://www-labs.iro.umontreal.ca/~gambit/download/gambit/v4.7/source/gambc-v4_7_5.tgz"
+  sha256 "5dee880b5ab8679c9aa72baaee8f84320692143c6a003379a458104f39c671b0"
+
+  head "https://github.com/feeley/gambit.git"
 
   bottle do
     sha1 "4f04f85300495e2c3fad49206b57605d010ad1f7" => :mavericks
@@ -11,11 +11,12 @@ class GambitScheme < Formula
     sha1 "f4002601e8f904d064909b5df30479a26c916f8d" => :lion
   end
 
-  conflicts_with 'ghostscript', :because => 'both install `gsc` binaries'
-  conflicts_with 'scheme48', :because => 'both install `scheme-r5rs` binaries'
+  conflicts_with "ghostscript", :because => "both install `gsc` binaries"
+  conflicts_with "scheme48", :because => "both install `scheme-r5rs` binaries"
 
-  option 'with-check', 'Execute "make check" before installing'
-  option 'enable-shared', 'Build Gambit Scheme runtime as shared library'
+  option "with-check", "Execute 'make check' before installing"
+  option "with-shared-library", "Build Gambit Scheme runtime as shared library"
+  deprecated_option "enable-shared" => "with-shared-library"
 
   fails_with :llvm
 
@@ -30,10 +31,10 @@ class GambitScheme < Formula
 
     # Recommended to improve the execution speed and compactness
     # of the generated executables. Increases compilation times.
-    # Don't enable this when using clang, per configure warning.
+    # Don"t enable this when using clang, per configure warning.
     args << "--enable-single-host" unless ENV.compiler == :clang
 
-    args << "--enable-shared" if build.include? 'enable-shared'
+    args << "--enable-shared" if build.with? "shared-library"
 
     if ENV.compiler == :clang
       opoo <<-EOS.undent
@@ -45,14 +46,14 @@ class GambitScheme < Formula
     end
 
     system "./configure", *args
-    system "make check" if build.with? "check"
+    system "make", "check" if build.with? "check"
 
-    ENV.j1
     system "make"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/gsi", "-e", '(print "hello world")'
+    assert_equal "hello world",
+                 shell_output("#{bin}/gsi -e '(print \"hello world\")'")
   end
 end
