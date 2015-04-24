ed0b56a73289f1d4ebdd288b32aa938591cb95c0
hyunkim <hyunkim9123@gmail.com>
2015-04-24 22:16:53 +0900

    Update gst-python.rb

--- Library/Formula/gst-python.rb
+++ Library/Formula/gst-python.rb
@@ -1,12 +1,10 @@
 class GstPython < Formula
   homepage "http://gstreamer.freedesktop.org/modules/gst-python.html"
   url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.4.0.tar.xz"
-  version "1.4.0"
   sha256 "b1e40c29ceb41b03f08d38aca6056054f0341d0706276326dceeec6ac8d53d3e"
 
-  head do
-    url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.4.0.tar.xz"
-  end
+  head "http:/"
+
   depends_on "gst-plugins-base"
   depends_on "pygtk"
   depends_on "pygobject3"
@@ -19,6 +17,6 @@ class GstPython < Formula
     system "make", "install"
   end
   test do
-    system "gst-inspect-1.0", "--vd", "python"
+    system "gst-inspect-1.0 python"
   end
 end
