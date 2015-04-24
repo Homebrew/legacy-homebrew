1be76118f7018c899b37b80a5aa6299439cf74d0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-24 15:30:52 +0100

    gsettings-desktop-schemas: update 3.14.0 bottle.

--- Library/Formula/gsettings-desktop-schemas.rb
+++ Library/Formula/gsettings-desktop-schemas.rb
@@ -4,10 +4,10 @@ class GsettingsDesktopSchemas < Formula
   sha256 "cf3ba58f6257155080b1872b4a6ce4a2424bb7af3f08e607b428cb47b065f2d7"
 
   bottle do
-    revision 1
-    sha1 "23d7c8b2d97fc5a9957573c140da28afe8acfacb" => :yosemite
-    sha1 "2bcb5e43cdefd81eef7e3bc45b9df4b7d8c2c847" => :mavericks
-    sha1 "b812cc34c9f54f053707cd4a5f5995cf903dd278" => :mountain_lion
+    revision 2
+    sha256 "ed713e36bb658c3ac715a43ac505389a427f06540e47a2521f7291875ac1d4fa" => :yosemite
+    sha256 "45e1e8d292bd2356cfcbc7090fb189536c974f73881cef20f2fdb40c6c6c013c" => :mavericks
+    sha256 "b1820bdd931682580c900920d0cf1edb4c1a0d86755dbb6dd310f5552f72879a" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
