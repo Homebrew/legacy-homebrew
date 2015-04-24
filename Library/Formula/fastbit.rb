9da03f2cbb49885660f5a1e698559f8fbb13ecd5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-24 22:30:16 +0100

    fastbit: add 2.0.2 bottle.

--- Library/Formula/fastbit.rb
+++ Library/Formula/fastbit.rb
@@ -3,6 +3,13 @@ class Fastbit < Formula
   url "https://codeforge.lbl.gov/frs/download.php/416/fastbit-2.0.2.tar.gz"
   sha256 "a9d6254fcc32da6b91bf00285c7820869950bed25d74c993da49e1336fd381b4"
 
+  bottle do
+    cellar :any
+    sha256 "33aa7e0a593b07b6d1615200b06ccd6eecd742beac345bd94b262f1b386d3591" => :yosemite
+    sha256 "f2da8bed14e66f334f870cec65c678961f925d519bddefc74ef806b493346833" => :mavericks
+    sha256 "d62226b902928b479e2835848a8eafdcd52557cb4249c59bd15cc1bd23d1e67e" => :mountain_lion
+  end
+
   head "https://codeforge.lbl.gov/anonscm/fastbit/trunk",
        :using => :svn
 
