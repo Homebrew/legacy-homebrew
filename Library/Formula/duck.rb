9b0048626175ca796f5acd5bca6996ed81238a9f
Jenkins CI <noreply@cyberduck.io>
2015-04-24 13:05:46 +0200

    duck 17432

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -1,7 +1,7 @@
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/duck-src-4.7.17420.tar.gz"
-  sha1 "e65f1544bf697ed6aeaa55ee41d0a2600b9f63f7"
+  url "https://dist.duck.sh/duck-src-4.7.17432.tar.gz"
+  sha1 "100996ffbabf2586eb149efd3097b4af20efa728"
   head "https://svn.cyberduck.io/trunk/"
 
   bottle do
