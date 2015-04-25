5eb0c8246934072777c7f0a4ec094237ec350373
Viktor Szakats <vszakats@users.noreply.github.com>
2015-04-25 13:51:29 +0200

    fon-flash-cli: https url and homepage
    Closes #39049.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/fon-flash-cli.rb
+++ Library/Formula/fon-flash-cli.rb
@@ -1,6 +1,6 @@
 class FonFlashCli < Formula
-  homepage "http://www.gargoyle-router.com/wiki/doku.php?id=fon_flash"
-  url "http://www.gargoyle-router.com/downloads/src/gargoyle_1.7.1-src.tar.gz"
+  homepage "https://www.gargoyle-router.com/wiki/doku.php?id=fon_flash"
+  url "https://www.gargoyle-router.com/downloads/src/gargoyle_1.7.1-src.tar.gz"
   sha256 "96089b8ce40d7fe821375173f9f2060530e78fa50e9029e932bc35bc2dd2c300"
   version "1.7.1"
 
