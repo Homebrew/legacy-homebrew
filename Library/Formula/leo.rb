3328e7dbfbfbd94bf4b630a80d0cc6ce450d1f5b
Edward K. Ream <edreamleo@gmail.com>
2015-04-25 12:45:50 -0500

    Added leo.rb for Leo 5.1 final, following suggestions of Mike McQuaid and Brett Koonce. - Use sha256 hash. - Added double quotes. - Corrected formatting of arguments to system. - Run system command in bin/.
    These tests pass when run from local homebrew folder:
    
    brew audit --strict Leo
    brew uninstall Leo
    brew install --verbose --debug Leo

--- Library/Formula/leo.rb
+++ Library/Formula/leo.rb
@@ -1,19 +1,13 @@
-require 'formula'
-
-# homebrew formula for Leo 5.1, version 2.
-
 class Leo < Formula
 
-  homepage 'http://leoeditor.com/'
-  url 'http://sourceforge.net/projects/leo/files/Leo/5.0-final/Leo-5.0-final.zip'
-  sha1 '9a8ca37688e60851b4faea3adfa30aae2156eb9f'
-  
-  head https://github.com/leo-editor/leo-editor, :using => :git
+  homepage "http://leoeditor.com/"
+  url "http://sourceforge.net/projects/leo/files/Leo/5.0-final/Leo-5.0-final.zip"
+  sha256 "2d742f9825959ba5c7624d1179b9f3065e14e055c90272fbce199f91770de826"
+  head "https://github.com/leo-editor/leo-editor", :using => :git
 
-  depends_on 'pyqt'
-  depends_on 'enchant' => :recommended
+  depends_on "pyqt"
+  depends_on "enchant" => :recommended
   depends_on :python if MacOS.version <= :snow_leopard
-    # Ensure Python 2.6+.
 
   def install
     (lib+"python2.7/site-packages").install "leo"
@@ -21,6 +15,6 @@ class Leo < Formula
   end
 
   test do
-    system "python -c 'import leo'"
+    system bin/"python", "-c", "import leo"
   end
 end
