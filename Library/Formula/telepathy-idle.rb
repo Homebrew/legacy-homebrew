d6a9ee59ea96d7ce82b7a6ff5f2b4f1d891369fb
John Benediktsson <mrjbq7@gmail.com>
2015-04-25 14:27:40 -0700

    Fix freedesktop.org URLs.

--- Library/Formula/telepathy-idle.rb
+++ Library/Formula/telepathy-idle.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class TelepathyIdle < Formula
-  homepage "http://telepathy.freedesktop.org/wiki/Components/"
+  homepage "https://wiki.freedesktop.org/telepathy/Components/"
   url "http://telepathy.freedesktop.org/releases/telepathy-idle/telepathy-idle-0.2.0.tar.gz"
   sha1 "932c11e7c131cc106ef0a6670158535925d9ca9e"
 
