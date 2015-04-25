b36953551b52cd3e0961951ac2173c1d3175d58c
Alex Dunn <dunn.alex@gmail.com>
2015-04-25 16:21:54 -0700

    highlight 3.22

--- Library/Formula/highlight.rb
+++ Library/Formula/highlight.rb
@@ -1,18 +1,21 @@
-require 'formula'
-
 class Highlight < Formula
-  homepage 'http://www.andre-simon.de/doku/highlight/en/highlight.html'
-  url 'http://www.andre-simon.de/zip/highlight-3.18.tar.bz2'
-  sha1 '985d0a3c707e3251fe50ffff66e11a8563777202'
-  revision 1
+  homepage "http://www.andre-simon.de/doku/highlight/en/highlight.html"
+  url "http://www.andre-simon.de/zip/highlight-3.22.tar.bz2"
+  sha256 "4776ce4305e6f92d8a9faf5d0aeffd56f413ad57e55303e7a2a6357387ec056f"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'boost'
-  depends_on 'lua'
+  head "svn://svn.code.sf.net/p/syntaxhighlight/code/highlight/"
+
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "lua"
 
   def install
-    conf_dir = etc+'highlight/' # highlight needs a final / for conf_dir
+    conf_dir = etc/"highlight/" # highlight needs a final / for conf_dir
     system "make", "PREFIX=#{prefix}", "conf_dir=#{conf_dir}"
     system "make", "PREFIX=#{prefix}", "conf_dir=#{conf_dir}", "install"
+  end
+
+  test do
+    system bin/"highlight", doc/"examples/highlight_pipe.php"
   end
 end
