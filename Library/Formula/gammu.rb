5487f07d9ea4cf68ea01fb0ef64b00d44d4db408
Viktor Szakats <vszakats@users.noreply.github.com>
2015-04-25 13:20:40 +0200

    gammu: https url
    Closes #39037.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gammu.rb
+++ Library/Formula/gammu.rb
@@ -1,6 +1,6 @@
 class Gammu < Formula
   homepage "http://wammu.eu/gammu/"
-  url "http://dl.cihar.com/gammu/releases/gammu-1.36.0.tar.xz"
+  url "https://dl.cihar.com/gammu/releases/gammu-1.36.0.tar.xz"
   mirror "https://mirrors.kernel.org/debian/pool/main/g/gammu/gammu_1.36.0.orig.tar.xz"
   sha256 "9c89fd204e190db5b301d28b793e8d0f2b05069a5b2b91fde451a6dae7f7d633"
 
