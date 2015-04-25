4b5d78989020a66b64768113cf4284f53ff1341e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-25 09:36:47 +0100

    peg-markdown: add 0.4.14 bottle.

--- Library/Formula/peg-markdown.rb
+++ Library/Formula/peg-markdown.rb
@@ -5,6 +5,13 @@ class PegMarkdown < Formula
 
   head "https://github.com/jgm/peg-markdown.git"
 
+  bottle do
+    cellar :any
+    sha256 "8b73ccf611b6639935b1fe7b42d5bab2518703f81b182769c537f7224ac1e803" => :yosemite
+    sha256 "67d5b05f4cb166d398f352cfa2a1d9ce97ae6ed1a7da5f8012c1d3544ec496d6" => :mavericks
+    sha256 "81b615979242abe96da6a8657c9357ad6f59d585a2533db1469b2e9ead567d9b" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
 
