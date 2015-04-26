fc090efb1dc2689fab997d7bd90ff883bbee8171
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-26 07:23:00 +0100

    highlight: add 3.22 bottle.

--- Library/Formula/highlight.rb
+++ Library/Formula/highlight.rb
@@ -5,6 +5,12 @@ class Highlight < Formula
 
   head "svn://svn.code.sf.net/p/syntaxhighlight/code/highlight/"
 
+  bottle do
+    sha256 "f530e34bbd1769fc6f434e65cbb7b81eef505d1508bfe24ca74277685407b6a3" => :yosemite
+    sha256 "8a9d9ecea3d0045dd4736859535cda66cb47cb1fc222b6244458350be62c2d39" => :mavericks
+    sha256 "81d3718fd0a1258a75d7ebce4ca36adb4ff0577e9c25dae5beae981dbdf61ade" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "boost"
   depends_on "lua"
