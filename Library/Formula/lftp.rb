8a35703b0663ec57250edf55eb94ea240dd04386
pyed <iabdulelah@gmail.com>
2015-04-26 17:15:35 +0300

    lftp 4.6.2
    update to 4.6.2
    
    Closes #39080.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Lftp < Formula
   homepage "http://lftp.yar.ru/"
-  url "http://lftp.yar.ru/ftp/lftp-4.6.1.tar.gz"
-  sha1 "57b62d7365de1698433a3b1b5daf7192adc2517a"
+  url "http://lftp.yar.ru/ftp/lftp-4.6.2.tar.gz"
+  sha1 "8c50f1b4c721143ac3e3428a72c9864edfde61f6"
 
   bottle do
     sha1 "4eb97a364cce3e0af57a0fb08e7b8094bf3fa5f5" => :yosemite
