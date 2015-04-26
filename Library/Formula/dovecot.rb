cd6a407fdbc3c8aff463d480e43928d3afc3f598
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-27 00:59:34 +0100

    dovecot 2.2.16
    Bump. Turned the caveats into a plist, since we can control that
    whole process and actually write the plist for the user and tell
    them where to put it automatically.

--- Library/Formula/dovecot.rb
+++ Library/Formula/dovecot.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Dovecot < Formula
   homepage "http://dovecot.org/"
-  url "http://dovecot.org/releases/2.2/dovecot-2.2.15.tar.gz"
-  mirror "http://fossies.org/linux/misc/dovecot-2.2.15.tar.gz"
-  sha1 "10c90f1f08797b5931703d52a871437e6561d76f"
+  url "http://dovecot.org/releases/2.2/dovecot-2.2.16.tar.gz"
+  mirror "https://fossies.org/linux/misc/dovecot-2.2.16.tar.gz"
+  sha256 "56ce1287a17fa88a2083116db00200deff1a5390af5eac1c8ae3f59a2079cff0"
 
   bottle do
     sha1 "3fce7a1596555dbd75ba1877b36e99bebd8ed364" => :yosemite
@@ -12,19 +10,21 @@ class Dovecot < Formula
     sha1 "c347ece69c0d20b7849e656568364af02b042c3c" => :mountain_lion
   end
 
-  depends_on "clucene" => :optional
   depends_on "openssl"
+  depends_on "clucene" => :optional
 
   def install
-    args = %W[--prefix=#{prefix}
-              --disable-dependency-tracking
-              --libexecdir=#{libexec}
-              --sysconfdir=#{etc}
-              --localstatedir=#{var}
-              --with-ssl=openssl
-              --with-sqlite
-              --with-zlib
-              --with-bzlib]
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --libexecdir=#{libexec}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --with-ssl=openssl
+      --with-sqlite
+      --with-zlib
+      --with-bzlib
+    ]
 
     args << "--with-lucene" if build.with? "clucene"
 
@@ -32,36 +32,38 @@ class Dovecot < Formula
     system "make", "install"
   end
 
-  def caveats; <<-EOS.undent
-      For Dovecot to work, you will need to do the following:
+  plist_options :startup => true
 
-      1) Create configuration in #{etc}
-
-      2) If required by the configuration above, create a dovecot user and group
-
-      3) possibly create a launchd item in /Library/LaunchDaemons/#{plist_path.basename}, like so:
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
       <dict>
-              <key>Label</key>
-              <string>#{plist_name}</string>
-              <key>OnDemand</key>
-              <false/>
-              <key>ProgramArguments</key>
-              <array>
-                      <string>#{HOMEBREW_PREFIX}/sbin/dovecot</string>
-                      <string>-F</string>
-              </array>
-              <key>RunAtLoad</key>
-              <true/>
-              <key>ServiceDescription</key>
-              <string>Dovecot mail server</string>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>OnDemand</key>
+        <false/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_sbin}/dovecot</string>
+          <string>-F</string>
+        </array>
+        <key>ServiceDescription</key>
+        <string>Dovecot mail server</string>
       </dict>
-      </plist>
-
-      Source: http://wiki.dovecot.org/LaunchdInstall
-      4) start the server using: sudo launchctl load /Library/LaunchDaemons/#{plist_path.basename}
+    </plist>
     EOS
+  end
+
+  def caveats; <<-EOS.undent
+    For Dovecot to work, you may need to create a dovecot user
+    and group depending on your configuration file options.
+    EOS
+  end
+
+  test do
+    assert_match /#{version}/, shell_output("#{sbin}/dovecot --version")
   end
 end
