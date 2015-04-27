91cdd3f3c5b431df53db314ddf22203da8034930
Xu Cheng <xucheng@me.com>
2015-04-27 17:05:57 +0800

    cryptopp: modernize and add test
    Closes #39099.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/cryptopp.rb
+++ Library/Formula/cryptopp.rb
@@ -1,17 +1,31 @@
-require 'formula'
-
 class Cryptopp < Formula
-  homepage 'http://www.cryptopp.com/'
-  url 'https://downloads.sourceforge.net/project/cryptopp/cryptopp/5.6.2/cryptopp562.zip'
-  sha1 'ddc18ae41c2c940317cd6efe81871686846fa293'
-  version '5.6.2'
+  homepage "http://www.cryptopp.com/"
+  url "https://downloads.sourceforge.net/project/cryptopp/cryptopp/5.6.2/cryptopp562.zip"
+  mirror "http://www.cryptopp.com/cryptopp562.zip"
+  sha256 "5cbfd2fcb4a6b3aab35902e2e0f3b59d9171fee12b3fc2b363e1801dfec53574"
+  version "5.6.2"
 
   def install
-    # patches welcome to re-enable this on configurations that support it
-    ENV.append 'CXXFLAGS', '-DCRYPTOPP_DISABLE_ASM'
+    system "make", "CXX=#{ENV.cxx}"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
 
-    system "make", "CXX=#{ENV.cxx}", "CXXFLAGS=#{ENV.cxxflags}"
-    lib.install "libcryptopp.a"
-    (include+'cryptopp').install Dir["*.h"]
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <cryptopp/sha.h>
+      #include <string>
+      using namespace CryptoPP;
+      using namespace std;
+
+      int main()
+      {
+        byte digest[SHA::DIGESTSIZE];
+        string data = "Hello World!";
+        SHA().CalculateDigest(digest, (byte*) data.c_str(), data.length());
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-L#{lib}", "-lcryptopp", "-o", "test"
+    system "./test"
   end
 end
