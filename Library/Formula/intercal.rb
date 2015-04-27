fdcd5c6b0ee0454282057a2c5058ec3f17d79dbb
Alex Dunn <dunn.alex@gmail.com>
2015-04-27 13:07:45 -0700

    intercal 0.30
    Closes #39114.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/intercal.rb
+++ Library/Formula/intercal.rb
@@ -1,9 +1,14 @@
-require 'formula'
-
 class Intercal < Formula
-  homepage 'http://catb.org/~esr/intercal/'
-  url 'http://catb.org/~esr/intercal/intercal-0.29.tar.gz'
-  sha1 '6f496b158e5f9dbf05a81c5e75f2d61698e65b15'
+  homepage "http://catb.org/~esr/intercal/"
+  url "http://catb.org/~esr/intercal/intercal-0.30.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/i/intercal/intercal_0.30.orig.tar.gz"
+  sha256 "b38b62a61a3cb5b0d3ce9f2d09c97bd74796979d532615073025a7fff6be1715"
+
+  head do
+    url "git://thyrsus.com/repositories/intercal.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
 
   bottle do
     sha1 "6a3fe7a08a8e51baf99acefe92db529a8d709504" => :mavericks
@@ -12,13 +17,19 @@ class Intercal < Formula
   end
 
   def install
+    if build.head?
+      cd "buildaux" do
+        system "./regenerate-build-system.sh"
+      end
+    end
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+    etc.install Dir["etc/*"]
+    share.install "pit"
   end
 
   test do
-    system "#{bin}/convickt"
-    system "#{bin}/ick"
+    system bin/"ick", share/"pit/beer.i"
   end
 end
