9da6164883a377d6c4dcce51f72156366cd213c4
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-27 21:23:58 +0100

    t1utils 1.39
    Version bump, added head.
    
    Closes #39115.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/t1utils.rb
+++ Library/Formula/t1utils.rb
@@ -1,14 +1,21 @@
-require 'formula'
-
 class T1utils < Formula
-  homepage 'http://www.lcdf.org/type/'
-  url 'http://www.lcdf.org/type/t1utils-1.38.tar.gz'
-  sha1 'a97ba119a2e376db49d9d4911472c0033e2fece8'
+  homepage "http://www.lcdf.org/type/"
+  url "http://www.lcdf.org/type/t1utils-1.39.tar.gz"
+  sha256 "0faef3e5c4927b38b05ac99ee177b7d7cddbbf5b4452f98b244f684b52b0d4c4"
+
+  head do
+    url "https://github.com/kohler/t1utils.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
 
   def install
+    system "./bootstrap.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
