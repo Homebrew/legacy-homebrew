52b2d3480f4087f7f90bdca6b611a3a182d94373
Paul Austin <paul.austin@revolsys.com>
2015-04-27 09:26:27 -0700

    gdal 1.11.2_2 java swig bindings #39105
    Added the --with-swig-java option to enable compilation of the swig
    java bindings for gdal. Added the code to create the required
    java config file for java 1.7 and run make on swig java binding.

--- Library/Formula/gdal.rb
+++ Library/Formula/gdal.rb
@@ -2,7 +2,7 @@ class Gdal < Formula
   homepage 'http://www.gdal.org/'
   url "http://download.osgeo.org/gdal/1.11.2/gdal-1.11.2.tar.gz"
   sha1 "6f3ccbe5643805784812072a33c25be0bbff00db"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "a0fd2413588bac1b4705349796cf1a93a20770ee16ae0044e56bc93ceaa18a72" => :yosemite
@@ -21,6 +21,7 @@ class Gdal < Formula
   option 'enable-unsupported', "Allow configure to drag in any library it can find. Invoke this at your own risk."
   option 'enable-mdb', 'Build with Access MDB driver (requires Java 1.6+ JDK/JRE, from Apple or Oracle).'
   option "with-libkml", "Build with Google's libkml driver (requires libkml --HEAD or >= 1.3)"
+  option 'with-swig-java', 'Build the swig java bindings'
 
   depends_on :python => :optional
   if build.with? "python"
@@ -69,6 +70,8 @@ class Gdal < Formula
     depends_on "poppler"
     depends_on "json-c"
   end
+
+  depends_on 'swig'
 
   # Extra linking libraries in configure test of armadillo may throw warning
   # see: https://trac.osgeo.org/gdal/ticket/5455
@@ -282,6 +285,19 @@ class Gdal < Formula
     cd 'swig/python' do
       system "python", "setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt", "--single-version-externally-managed"
       bin.install Dir['scripts/*']
+    end
+
+    if 'with-swig-java'
+      cd 'swig/java' do
+        system "echo 'JAVA_HOME='`/usr/libexec/java_home -v 1.7` > java.opt"
+        system "echo 'JAVADOC=$(JAVA_HOME)/bin/javadoc' >> java.opt"
+        system "echo 'JAVAC=$(JAVA_HOME)/bin/javac' >> java.opt"
+        system "echo 'JAVA=$(JAVA_HOME)/bin/java' >> java.opt"
+        system "echo 'JAR=$(JAVA_HOME)/bin/jar' >> java.opt"
+        system "echo 'JAVA_INCLUDE=-I$(JAVA_HOME)/include -I$(JAVA_HOME)/include/darwin' >> java.opt"
+        system "make"
+        system "make install"
+      end
     end
 
     system 'make', 'man' if build.head?
