46b28f8ce0209dc28664fb9891eea723cf4cbd35
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-27 16:59:47 +0100

    ispell: add 3.4.00 bottle.

--- Library/Formula/ispell.rb
+++ Library/Formula/ispell.rb
@@ -4,6 +4,12 @@ class Ispell < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/i/ispell/ispell_3.4.00.orig.tar.gz"
   sha256 "5dc42e458635f218032d3ae929528e5587b1e7247564f0e9f9d77d5ccab7aec2"
 
+  bottle do
+    sha256 "ff46baf7aa6daf42fddde68897bd80dbb073922b4556c502e7b0072656b48498" => :yosemite
+    sha256 "f1ee90dcc76682d17c2b758d2a896493448753acc0e556e9b0c8bf7ec0f552df" => :mavericks
+    sha256 "dbbaabbc715f6f16dfb9f2cd05755a88e471b92c63d7f87f79a940a5df8dadfb" => :mountain_lion
+  end
+
   def install
     ENV.deparallelize
     ENV.no_optimization
