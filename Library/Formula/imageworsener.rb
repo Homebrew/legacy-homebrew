aa3a845596531ff28da57d5f0ddb72d8498084fd
Alex Dunn <dunn.alex@gmail.com>
2015-04-27 14:02:46 -0700

    imageworsener 1.2.0
    Closes #39118.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/imageworsener.rb
+++ Library/Formula/imageworsener.rb
@@ -1,19 +1,39 @@
-require 'formula'
-
 class Imageworsener < Formula
-  homepage 'http://entropymine.com/imageworsener/'
-  url 'http://entropymine.com/imageworsener/imageworsener-1.1.0.tar.gz'
-  sha1 'ce552890396efc03f0e7860b74cd635e5185858d'
-  revision 1
+  homepage "http://entropymine.com/imageworsener/"
+  url "http://entropymine.com/imageworsener/imageworsener-1.2.0.tar.gz"
+  sha256 "97fdb1aafac7bc2339b2ce813071f0900de0b093b96ab1a99a43f9647afdfe35"
 
-  depends_on 'libpng' => :recommended
+  head do
+    url "https://github.com/jsummers/imageworsener.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "libpng" => :recommended
+  depends_on "jpeg" => :optional
+  depends_on "webp" => :optional
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    if build.head?
+      inreplace "./scripts/autogen.sh", "libtoolize", "glibtoolize"
+      system "./scripts/autogen.sh"
+    end
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+    args << "--without-jpeg" if build.without? "jpeg"
+    args << "--without-webp" if build.without? "webp"
+
+    system "./configure", *args
+    system "make", "install"
+    share.install "tests"
   end
 
   test do
-    system "#{bin}/imagew", "--version"
+    cd share/"tests" do
+      system "./runtest", bin/"imagew"
+    end
   end
 end
