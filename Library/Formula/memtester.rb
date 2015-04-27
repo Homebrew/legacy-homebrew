9e12ae88958cb138fe6cbfb0522a1280adeeb1e5
Larry Gilbert <larry@L2G.to>
2015-04-27 11:19:07 -0700

    memtester: fix audit --strict.
    * Remove "require 'formula'"
    * Convert all single quotes to double quotes
    * Switch from SHA-1 hash to SHA-2(256) hash
    * Break up arguments to system call
    * Add test block
    
    Closes #39173.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/memtester.rb
+++ Library/Formula/memtester.rb
@@ -1,16 +1,18 @@
-require 'formula'
-
 class Memtester < Formula
-  homepage 'http://pyropus.ca/software/memtester/'
-  url 'http://pyropus.ca/software/memtester/old-versions/memtester-4.3.0.tar.gz'
-  sha1 'eff0e3020a7750bd5452b051526ad6439e92b9cd'
+  homepage "http://pyropus.ca/software/memtester/"
+  url "http://pyropus.ca/software/memtester/old-versions/memtester-4.3.0.tar.gz"
+  sha256 "f9dfe2fd737c38fad6535bbab327da9a21f7ce4ea6f18c7b3339adef6bf5fd88"
 
   def install
     inreplace "Makefile" do |s|
-      s.change_make_var! 'INSTALLPATH', prefix
-      s.gsub! 'man/man8', 'share/man/man8'
+      s.change_make_var! "INSTALLPATH", prefix
+      s.gsub! "man/man8", "share/man/man8"
     end
     inreplace "conf-ld", " -s", ""
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"memtester", "1", "1"
   end
 end
