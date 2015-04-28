c8de2501bff1f685af3256f83ef2675fe514e33c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-28 13:59:04 +0100

    libcdr: add 0.1.1 bottle.

--- Library/Formula/libcdr.rb
+++ Library/Formula/libcdr.rb
@@ -3,6 +3,13 @@ class Libcdr < Formula
   url "http://dev-www.libreoffice.org/src/libcdr/libcdr-0.1.1.tar.bz2"
   sha256 "72fe7bbbf2275242acdf67ad2f9b6c71ac9146a56c409def360dabcac5695b49"
 
+  bottle do
+    cellar :any
+    sha256 "2ca4c0020f72966b3070d910dec34601cf6b22be312a539f2d9ee73d0bfa4139" => :yosemite
+    sha256 "f3ba5ff4b6bd4f4afe141d5b38bfb40355abb37d1865284b117c5bd766b7f613" => :mavericks
+    sha256 "cf6b2ed3191cd32b9303f759226da36ba336a9d6448587fcc8bf8f7fd1a20037" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
   depends_on "icu4c"
