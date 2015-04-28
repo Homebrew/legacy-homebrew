a62cd7fd7d1e2aa2322e8ea42ca00b8d15a4ecc1
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-04-28 21:34:29 +0200

    maven 3.3.3
    Closes #39153.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/maven.rb
+++ Library/Formula/maven.rb
@@ -1,8 +1,8 @@
 class Maven < Formula
   homepage "https://maven.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz"
-  sha256 "153564900617218a126f78d2603d060d0a15f19f3ec1689fc2b7692a3c15b9aa"
+  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz"
+  sha256 "3a8dc4a12ab9f3607a1a2097bbab0150c947ad6719d8f1bb6d5b47d0fb0c4779"
 
   depends_on :java
 
