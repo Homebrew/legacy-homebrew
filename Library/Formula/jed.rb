5d09bea1d14ab2951a1012644f4fdcc42c02ad5e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-28 18:45:15 +0100

    jed: add 0.99-19 bottle.

--- Library/Formula/jed.rb
+++ Library/Formula/jed.rb
@@ -4,6 +4,12 @@ class Jed < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/j/jed/jed_0.99.19.orig.tar.gz"
   sha256 "5eed5fede7a95f18b33b7b32cb71be9d509c6babc1483dd5c58b1a169f2bdf52"
 
+  bottle do
+    sha256 "3b316c792feabf9622a70a8ccdf2d2e985e7f991dbcd49a104b2ee6b8ea078cb" => :yosemite
+    sha256 "f0e02951e534d96baad147970e51b2f5c09155ad0e51114cfc72b3f49301dff3" => :mavericks
+    sha256 "595aff2e43b8ec8c31626831dcab83ffe4ffc129ea85efa61bc2e35877137a29" => :mountain_lion
+  end
+
   head do
     url "git://git.jedsoft.org/git/jed.git"
     depends_on "automake" => :build
