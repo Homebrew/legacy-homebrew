2fa1498f096e2e789139d6d9b9a39c283605ca52
Jack Nagel <jacknagel@gmail.com>
2015-04-29 19:23:06 -0400

    Default HOMEBREW_TEMP to Dir.tmpdir

--- Library/Homebrew/config.rb
+++ Library/Homebrew/config.rb
@@ -49,7 +49,7 @@ end
 
 HOMEBREW_LOGS = Pathname.new(ENV['HOMEBREW_LOGS'] || '~/Library/Logs/Homebrew/').expand_path
 
-HOMEBREW_TEMP = Pathname.new(ENV.fetch('HOMEBREW_TEMP', '/tmp'))
+HOMEBREW_TEMP = Pathname.new(ENV["HOMEBREW_TEMP"] || Dir.tmpdir)
 
 if not defined? HOMEBREW_LIBRARY_PATH
   HOMEBREW_LIBRARY_PATH = Pathname.new(__FILE__).realpath.parent.join("Homebrew")
