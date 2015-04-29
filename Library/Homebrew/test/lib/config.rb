f0fa16be2385c20529c9e3253585a913332c38c2
Jack Nagel <jacknagel@gmail.com>
2015-04-28 22:37:26 -0400

    Extract runtime configuration from global.rb
    This allows global.rb to be safely loaded in the test environment.

---
+++ Library/Homebrew/test/lib/config.rb
@@ -0,0 +1,8 @@
+HOMEBREW_PREFIX        = Pathname.new(TEST_TMPDIR).join("prefix")
+HOMEBREW_REPOSITORY    = HOMEBREW_PREFIX
+HOMEBREW_LIBRARY       = HOMEBREW_REPOSITORY+"Library"
+HOMEBREW_CACHE         = HOMEBREW_PREFIX.parent+"cache"
+HOMEBREW_CACHE_FORMULA = HOMEBREW_PREFIX.parent+"formula_cache"
+HOMEBREW_CELLAR        = HOMEBREW_PREFIX.parent+"cellar"
+HOMEBREW_LOGS          = HOMEBREW_PREFIX.parent+"logs"
+HOMEBREW_TEMP          = Pathname.new(ENV["HOMEBREW_TEMP"] || "/tmp")
