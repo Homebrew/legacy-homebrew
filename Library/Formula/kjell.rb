a8956bee390d9d11511b6b67995628a6668c8339
Alex Dunn <dunn.alex@gmail.com>
2015-04-28 18:54:22 -0700

    kjell 0.2.5

--- Library/Formula/kjell.rb
+++ Library/Formula/kjell.rb
@@ -1,9 +1,11 @@
-require 'formula'
-
 class Kjell < Formula
-  homepage 'http://karlll.github.io/kjell/'
-  url 'https://github.com/karlll/kjell.git', :tag => '0.2.2'
-  sha1 '514fc79b62093edd4149b9d1ab54874d4e9e3fae'
+  homepage "https://karlll.github.io/kjell/"
+  # clone repository in order to get extensions submodule
+  url "https://github.com/karlll/kjell.git",
+      :tag => "0.2.5",
+      :revision => "42e2b68742533181fc1da868cceca2efb88bf3f4"
+
+  head "https://github.com/karlll/kjell.git"
 
   depends_on "erlang"
 
@@ -14,16 +16,7 @@ class Kjell < Formula
     system "make", "install-extensions"
   end
 
-  test do
-    require 'open3'
-    Open3.popen3("#{bin}/kjell") do |stdin, stdout, _|
-      stdin.write("q().\n")
-      stdin.close
-    end
-  end
-
   def caveats
-    "Extension kjell-prompt requires a powerline patched font. See https://github.com/Lokaltog/powerline-fonts"
+    "Extension 'kjell-prompt' requires a powerline patched font. See https://github.com/Lokaltog/powerline-fonts"
   end
-
 end
