22d1148aa8b06b5f17c0cffbd2fb3bedcb257c55
Simon Dick <simon.dick@sainsburysentertainment.co.uk>
2015-04-29 10:14:36 +0100

    vault 0.1.0 (new formula)
    Add formula for vault 0.1.0

---
+++ Library/Formula/vault.rb
@@ -0,0 +1,15 @@
+class Vault < Formula
+  homepage "https://vaultproject.io/"
+  url "https://dl.bintray.com/mitchellh/vault/vault_0.1.0_darwin_amd64.zip"
+  version "0.1.0"
+  sha256 "e0a1a1ace4c9577408800825b374c0239c04ebc9ab6ed18c924dc8f0393b9fd5"
+
+  def install
+    bin.mkpath
+    bin.install "vault"
+  end
+
+  test do
+    system bin/"vault", "--version"
+  end
+end
