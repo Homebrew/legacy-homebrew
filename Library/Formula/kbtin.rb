5c3f934b50b665d893c673ea96e3ed85b52605f3
Alex Dunn <dunn.alex@gmail.com>
2015-04-28 20:17:28 -0700

    kbtin 1.0.15

--- Library/Formula/kbtin.rb
+++ Library/Formula/kbtin.rb
@@ -1,17 +1,26 @@
-require 'formula'
-
 class Kbtin < Formula
-  homepage 'http://kbtin.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/kbtin/kbtin/1.0.14/kbtin-1.0.14.tar.xz'
-  sha1 'b6e09ee7702fe52d48db53098c79fe243505cc0f'
-  revision 1
+  homepage "http://kbtin.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/kbtin/kbtin/1.0.15/kbtin-1.0.15.tar.xz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/k/kbtin/kbtin_1.0.15.orig.tar.xz"
+  sha256 "57231dfd5dc0edcf71dcdafc16dc2e836c5d96e3b28927997bd2cc5061a313fa"
 
-  depends_on 'gnutls'
+  depends_on "gnutls"
+
+  if MacOS.version >= :mavericks
+    # https://sourceforge.net/p/kbtin/bugs/11/
+    fails_with :clang do
+      cause "error: conflicting types for '__builtin___strlcpy_chk'"
+    end
+  end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "background-color: black;",
+                 `echo homebrew | #{bin}/ansi2html`
   end
 end
