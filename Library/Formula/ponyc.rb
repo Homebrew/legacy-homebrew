f07c236c1c2fa2805e910a52b9af17cb55e03e18
Sebastian Blessing <sebastian@causality.io>
2015-04-29 21:09:59 +0200

    added formula for ponyc compiler

---
+++ Library/Formula/ponyc.rb
@@ -0,0 +1,16 @@
+class Ponyc < Formula
+  homepage "http://www.ponylang.org"
+  url "http://www.ponylang.org/releases/source/ponyc-0.1.1.tar.bz2"
+  version "0.1.1"
+  sha256 "8ed8e5139635cf7f60cf87d51fd36210ab11a8a7fcefbdfa9b5c25a2666bee1e"
+
+  depends_on "homebrew/versions/llvm36"
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/ponyc --version"
+  end
+end
