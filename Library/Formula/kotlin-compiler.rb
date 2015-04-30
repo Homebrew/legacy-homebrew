94ca97c04d4cf2fbba3456a740d4523860066a59
Michael Pardo <kensuke155@gmail.com>
2015-04-30 13:42:18 -0400

    Copy all but .bat from bin. Test compile app.

--- Library/Formula/kotlin-compiler.rb
+++ Library/Formula/kotlin-compiler.rb
@@ -3,15 +3,20 @@ class KotlinCompiler < Formula
   url "https://github.com/JetBrains/kotlin/releases/download/M11.1-bootstrap/kotlin-compiler-0.11.91.1.zip"
   sha256 "5a68aa0f42d1e14f28fdcbf317d873fdbac1ef736934f7877fa26dac64377f64"
 
-
   def install
+    rm Dir['*.bat']
     libexec.install %w[bin lib]
-    bin.install_symlink "#{libexec}/bin/kotlinc"
-    bin.install_symlink "#{libexec}/bin/kotlinc-js"
-    bin.install_symlink "#{libexec}/bin/kotlinc-jvm"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    system "#{bin}/kotlinc", "-version"
+    (testpath/"test.kt").write <<-EOS.undent
+      fun main(args: Array<String>) {
+        println("Hello World!")
+      }
+    EOS
+    system "#{bin}/kotlinc", "test.kt", "-include-runtime", "-d", "test.jar"
+    system "#{bin}/kotlinc-js", "test.kt", "-output", "test.js"
+    system "#{bin}/kotlinc-jvm", "test.kt", "-include-runtime", "-d", "test.jar"
   end
 end
