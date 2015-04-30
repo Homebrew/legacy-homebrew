22c1aa25a830e83af8aa29a3b8254daeadab4322
Alex Dunn <dunn.alex@gmail.com>
2015-04-30 10:31:47 -0700

    minised 1.15
    Closes #39219.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/minised.rb
+++ Library/Formula/minised.rb
@@ -1,12 +1,15 @@
-require 'formula'
-
 class Minised < Formula
-  homepage 'http://www.exactcode.de/site/open_source/minised/'
-  url 'http://dl.exactcode.de/oss/minised/minised-1.14.tar.gz'
-  sha1 'ddfd770b911ae482fd79a6d8f5f1e731ad2784e8'
+  homepage "http://www.exactcode.de/site/open_source/minised/"
+  url "http://dl.exactcode.de/oss/minised/minised-1.15.tar.gz"
+  sha256 "ada36a55b71d1f2eb61f2f3b95f112708ce51e69f601bf5ea5d7acb7c21b3481"
 
   def install
     system "make"
     system "make", "DESTDIR=#{prefix}", "PREFIX=", "install"
+  end
+
+  test do
+    output = pipe_output("#{bin}/minised 's:o::'", "hello world", 0)
+    assert_equal "hell world", output.chomp
   end
 end
