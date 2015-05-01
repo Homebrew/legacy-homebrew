f83d84c44dae58266b161b5b81c06945dad03d30
Alex Dunn <dunn.alex@gmail.com>
2015-05-01 08:58:30 -0700

    mitie 0.4
    Closes #39244.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/mitie.rb
+++ Library/Formula/mitie.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Mitie < Formula
   homepage "https://github.com/mit-nlp/MITIE/"
-  url "https://github.com/mit-nlp/MITIE/archive/v0.3.tar.gz"
-  sha1 "05ea37283bd75487db469b68b0e2ecdea991c05e"
+  url "https://github.com/mit-nlp/MITIE/archive/v0.4.tar.gz"
+  sha256 "3902c9a6332354fb23f1cbdc4ef9631fa1477b69e37e4b7822d3c18a8bbefdc9"
+
+  head "https://github.com/mit-nlp/MITIE.git"
 
   bottle do
     cellar :any
@@ -14,27 +14,37 @@ class Mitie < Formula
 
   depends_on :python if MacOS.version <= :snow_leopard
 
+  option "without-models", "Don't download the v0.2 models (~415MB)"
+
   resource "models-english" do
-    url "http://sourceforge.net/projects/mitie/files/binaries/MITIE-models-v0.2.tar.bz2"
-    sha1 "a193e0a133926b5355b4e840ddd7f5112d785999"
+    url "https://downloads.sourceforge.net/project/mitie/binaries/MITIE-models-v0.2.tar.bz2"
+    sha256 "dc073eaef980e65d68d18c7193d94b9b727beb254a0c2978f39918f158d91b31"
   end
 
   def install
-    (share+"MITIE-models").install resource("models-english")
+    if build.with? "models"
+      (share/"MITIE-models").install resource("models-english")
+    end
+
     inreplace "mitielib/makefile" do |s|
-      s.gsub! /libmitie.so/, "libmitie.dylib"
+      s.gsub!(/libmitie.so/, "libmitie.dylib")
     end
     system "make", "mitielib"
     system "make"
+
+    include.install Dir["mitielib/include/*"]
     lib.install "mitielib/libmitie.dylib", "mitielib/libmitie.a"
-    (lib+'python2.7/site-packages').install "mitielib/mitie.py"
-    (share+"mitie").install "examples", "sample_text.txt",
+    (lib/"python2.7/site-packages").install "mitielib/mitie.py"
+    (share/"mitie").install "examples", "sample_text.txt",
       "sample_text.reference-output", "sample_text.reference-output-relations"
     bin.install "ner_example", "ner_stream", "relation_extraction_example"
   end
 
   test do
-    system "#{bin}/ner_example", "#{share}/MITIE-models/english/ner_model.dat", "#{share}/mitie/sample_text.txt"
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lmitie",
+           share/"mitie/examples/C/ner/ner_example.c",
+           "-o", testpath/"ner_example"
+    system "./ner_example", share/"MITIE-models/english/ner_model.dat",
+           share/"mitie/sample_text.txt"
   end
-
 end
