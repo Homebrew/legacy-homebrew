3dc60ff9242b37c8235035fcc98d26ce0e926748
Hill Ma <maahiuzeon@gmail.com>
2015-04-30 22:41:07 -0700

    tcpflow: fix HEAD build
    Closes #39240.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/tcpflow.rb
+++ Library/Formula/tcpflow.rb
@@ -16,10 +16,12 @@ class Tcpflow < Formula
   depends_on 'sqlite' if MacOS.version < :lion
   depends_on "openssl"
 
-  # Upstream fix for 10.6; can be removed in next release
-  patch do
-    url "https://github.com/simsong/tcpflow/commit/1cd5a9168c2ebf72c1fadcd64634398bd8470bce.diff"
-    sha1 "5264d287a5e62b647da0aa6f2bfa237bc8171c3a"
+  stable do
+    # Upstream fix for 10.6; can be removed in next release
+    patch do
+      url "https://github.com/simsong/tcpflow/commit/1cd5a9168c2ebf72c1fadcd64634398bd8470bce.diff"
+      sha1 "5264d287a5e62b647da0aa6f2bfa237bc8171c3a"
+    end
   end
 
   def install
