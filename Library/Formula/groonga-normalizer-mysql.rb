15187f0be911f0070b7cb89e4d6e351d24d6fd92
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-05-02 14:48:25 +0900

    groonga-normalizer-mysql 1.0.9

--- Library/Formula/groonga-normalizer-mysql.rb
+++ Library/Formula/groonga-normalizer-mysql.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GroongaNormalizerMysql < Formula
   homepage 'https://github.com/groonga/groonga-normalizer-mysql'
-  url 'http://packages.groonga.org/source/groonga-normalizer-mysql/groonga-normalizer-mysql-1.0.6.tar.gz'
-  sha1 '61d27a4782d4821be7d26e07cf207c6d7f719ebf'
+  url 'http://packages.groonga.org/source/groonga-normalizer-mysql/groonga-normalizer-mysql-1.0.9.tar.gz'
+  sha1 '9057570c1f0e3b5974bf7ac58877af8971ef2a25'
 
   depends_on 'pkg-config' => :build
   depends_on 'groonga'
