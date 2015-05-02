56289d5ef66b1a4541f43a9c3d45a234cf67af2b
Ruben <ruben2020-git@users.sourceforge.net>
2015-05-03 03:44:48 +0800

    intermediate fix

--- Library/Formula/codequery.rb
+++ Library/Formula/codequery.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Codequery < Formula
   homepage "https://github.com/ruben2020/codequery"
-  url "https://github.com/ruben2020/codequery/archive/v0.16.0.tar.gz"
+  url "https://github.com/ruben2020/codequery/archive/v0.16.0.tar.gz"x
   sha256 "4896435a8aa35dbdca43cba769aece9731f647ac9422a92c3209c2955d2e7101"
 
   bottle do
