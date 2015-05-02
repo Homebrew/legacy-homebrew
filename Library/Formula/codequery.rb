42f827b05d6144f850c2f2da73b552e16130b494
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-02 20:55:22 +0100

    codequery: update 0.16.0 bottle.

--- Library/Formula/codequery.rb
+++ Library/Formula/codequery.rb
@@ -7,9 +7,9 @@ class Codequery < Formula
 
   bottle do
     cellar :any
-    sha1 "a9bf2cb776cfeb79b4b13f18fcd34f9e2033545e" => :mavericks
-    sha1 "736902fbf7aa648b4d06136cb02c56f968ed7aa9" => :mountain_lion
-    sha1 "4d25e90e1258b94a32273424d95501ba921dc685" => :lion
+    sha256 "622d9750948ae1eeef67c4f2824b3af800a671bec6a358ffef3019711991796b" => :yosemite
+    sha256 "ff3d941bf41fd5d9cb0947bddf3236846722a2ea6fdbb24e7f291c039a56140a" => :mavericks
+    sha256 "e5e30987a1a4ca4358f2dcb6848df9cfef4d8bfce49aa72831a22cbe6125d8a6" => :mountain_lion
   end
 
   depends_on "cmake" => :build
