af32c770fed44b8e15503f557b25c013f5a9190f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-02 13:11:11 +0100

    mitie: update 0.4 bottle.

--- Library/Formula/mitie.rb
+++ Library/Formula/mitie.rb
@@ -7,9 +7,9 @@ class Mitie < Formula
 
   bottle do
     cellar :any
-    sha1 "5340adc68370e4aec0114080e143e779595fed37" => :yosemite
-    sha1 "d0fb785080028ad13aec010e990a82efcfce8c57" => :mavericks
-    sha1 "54b250080d42b7da77b3c92d67d32431be180dfd" => :mountain_lion
+    sha256 "4570f1e2ac9721e5f53beda0d3e6be2ab88604e490641273b2959424fdf095a2" => :yosemite
+    sha256 "55220ba374b6b03316fd757d2731e8745aa8be50ddc983e0fd28e04bdf5f26da" => :mavericks
+    sha256 "25a3ca7c81987f46cb52f4cc8b8c8de674db5c232d9b6e8383d376fad00ae3ea" => :mountain_lion
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
