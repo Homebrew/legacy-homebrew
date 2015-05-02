bf9265ec6537cf9699fc4fc4b22a0809b318ec23
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-02 12:36:34 +0100

    curlpp: add 0.7.3 bottle.

--- Library/Formula/curlpp.rb
+++ Library/Formula/curlpp.rb
@@ -3,6 +3,13 @@ class Curlpp < Formula
   url "https://github.com/jpbarrette/curlpp/archive/v0.7.3.tar.gz"
   sha256 "b72093f221a9e2d0f7ce0bd0f846587835e01607a7bb0f106ff4317a8c30a81c"
 
+  bottle do
+    cellar :any
+    sha256 "02c9fcff3e1f44aa6a6a2b7d11f3430be4408c2e8ceaec592819a7e80761c492" => :yosemite
+    sha256 "212198492acef15ac67840e80d052f451a02531a04632aa49a4b12dde9baec3a" => :mavericks
+    sha256 "89d6e74a2ec2482014e6210876eeface47b23b1388f54553c9073c982cd0ddcb" => :mountain_lion
+  end
+
   depends_on "boost" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
