099c04269ebed63777970722eb57985e979793b3
Guo Xiao <guoxiao08@gmail.com>
2015-05-02 15:00:27 +0800

    spawn-fcgi: add disable-ipv6 option and test
    Closes #39273.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/spawn-fcgi.rb
+++ Library/Formula/spawn-fcgi.rb
@@ -1,13 +1,19 @@
-require 'formula'
-
 class SpawnFcgi < Formula
-  homepage 'http://redmine.lighttpd.net/projects/spawn-fcgi'
-  url 'http://www.lighttpd.net/download/spawn-fcgi-1.6.4.tar.gz'
-  sha1 '0b10126be40431b00591a28f155efdb77a460161'
+  homepage "http://redmine.lighttpd.net/projects/spawn-fcgi"
+  url "http://www.lighttpd.net/download/spawn-fcgi-1.6.4.tar.gz"
+  sha256 "ab327462cb99894a3699f874425a421d934f957cb24221f00bb888108d9dd09e"
+
+  option "without-ipv6", "Build without ipv6 support"
 
   def install
+    args = []
+    args << "--disable-ipv6" if build.without? "ipv6"
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make install"
+                          "--prefix=#{prefix}", *args
+    system "make", "install"
+  end
+
+  test do
+    system "spawn-fcgi", "--version"
   end
 end
