c48f26d1dc9a04b96ffd7339a22c51ceeade046a
Peter Cottle <pcottle@fb.com>
2015-05-03 13:46:54 -0700

    final recipe!!!

--- Library/Formula/fpp.rb
+++ Library/Formula/fpp.rb
@@ -1,8 +1,8 @@
 class Fpp < Formula
   homepage "https://facebook.github.io/PathPicker/"
-  # To be changed -- just a sample tar for now
-  url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.0.0.4.tar.gz"
-  sha256 "25f3edddc28b7a353c407d48a74a92f9a364160de790122357a3785d9d9e8aeb"
+  # TODO -- change away from gh-pages link
+  url "https://facebook.github.io/PathPicker/dist/fpp.0.5.0.tar.gz"
+  sha256 "634cbd2f501639186561418bf2036529d3995676836378b5865e7130d34733e8"
   head "https://github.com/facebook/pathpicker.git"
 
   depends_on :python if MacOS.version <= :snow_leopard
@@ -12,11 +12,11 @@ class Fpp < Formula
     # we need to copy the bash file and source python files
     libexec.install Dir["*"]
     # and then symlink the bash file
-    bin.install_symlink libexec/"example"
+    bin.install_symlink libexec/"fpp"
   end
 
   test do
-    system "example --help"
+    system "fpp --help"
   end
 
 end
