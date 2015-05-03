a96aaca60abbd8215b265bc87b11d8c37f47ac5f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-03 01:00:19 +0100

    groonga-normalizer-mysql: add 1.0.9 bottle.

--- Library/Formula/groonga-normalizer-mysql.rb
+++ Library/Formula/groonga-normalizer-mysql.rb
@@ -3,6 +3,13 @@ class GroongaNormalizerMysql < Formula
   url "http://packages.groonga.org/source/groonga-normalizer-mysql/groonga-normalizer-mysql-1.0.9.tar.gz"
   sha1 "9057570c1f0e3b5974bf7ac58877af8971ef2a25"
 
+  bottle do
+    cellar :any
+    sha256 "f418a3ff019b68959cd9da98b406a39c0e0d2a90e75d6f86ecd717b7c9ae4a67" => :yosemite
+    sha256 "80cc94cbb3158e9459a5c95b274154f0e4d10ee9e6c73a17029b18ddf0b22df5" => :mavericks
+    sha256 "7de3a15fd4120aa918222be1613757642f4f427f4e98b782be59af427f40b994" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "groonga"
 
