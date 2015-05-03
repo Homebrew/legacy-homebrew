9980bc64e24aefb9c2e419a94b1b0ac13ce97f18
Scott <scott@internaught.io>
2015-05-02 22:19:05 -0700

    update xonsh.rb
    removed the second ply staging and also removed lines 20 and 21

--- Library/Formula/xonsh.rb
+++ Library/Formula/xonsh.rb
@@ -17,12 +17,6 @@ class Xonsh < Formula
 
     resource("ply").stage do
       system "python3", *Language::Python.setup_install_args(libexec/"vendor")
-      ENV["PYTHONPATH"] = lib/"python#{version}/site-packages"
-      ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{version}/site-packages"
-    end
-
-    resource("ply").stage do
-      system "python3", *Language::Python.setup_install_args(libexec)
     end
 
     system "python3", *Language::Python.setup_install_args(libexec)
