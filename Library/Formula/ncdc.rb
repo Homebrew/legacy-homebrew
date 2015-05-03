f2b6e97824b1044bf34b85e1a224e4d685e7460e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-03 12:24:27 +0100

    ncdc: add 1.19.1 bottle.

--- Library/Formula/ncdc.rb
+++ Library/Formula/ncdc.rb
@@ -3,6 +3,13 @@ class Ncdc < Formula
   url "http://dev.yorhel.nl/download/ncdc-1.19.1.tar.gz"
   sha256 "a6b23381434a47f7134d9ebdf5658fd06768f9b5de498c43e0fa00d1c7229d47"
 
+  bottle do
+    cellar :any
+    sha256 "ca94bbb642bde3040c7ff044416f1f4c317e987ec391a08d19e0eb3255cfe458" => :yosemite
+    sha256 "2534b7f72747615d2eb9989a2ea4147312cb604c7723b3421155f7b0837e8e71" => :mavericks
+    sha256 "1e362cd2a91f1a9a7bfe401e4665d33a8a35076097e8acb0153cadc67aea8b6c" => :mountain_lion
+  end
+
   head do
     url "git://g.blicky.net/ncdc.git", :shallow => false
 
