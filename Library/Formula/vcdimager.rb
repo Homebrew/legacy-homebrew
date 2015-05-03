a7b53cd5cec9fb51ac1d1c101dfa8550a4305055
Larry Gilbert <larry@L2G.to>
2015-05-03 01:01:43 -0700

    vcdimager: add test
    Closes #39315.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/vcdimager.rb
+++ Library/Formula/vcdimager.rb
@@ -1,20 +1,22 @@
-require 'formula'
-
 class Vcdimager < Formula
-  homepage 'http://www.gnu.org/software/vcdimager/'
-  url 'http://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz'
-  sha1 '8c245555c3e21dcbc3d4dbb2ecca74f609545424'
+  homepage "https://www.gnu.org/software/vcdimager/"
+  url "http://ftpmirror.gnu.org/vcdimager/vcdimager-0.7.24.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/vcdimager/vcdimager-0.7.24.tar.gz"
+  sha256 "075d7a67353ff3004745da781435698b6bc4a053838d0d4a3ce0516d7d974694"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libcdio'
-  depends_on 'popt'
+  depends_on "pkg-config" => :build
+  depends_on "libcdio"
+  depends_on "popt"
 
   def install
     ENV.libxml2
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"vcdimager", "--help"
   end
 end
