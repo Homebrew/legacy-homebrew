a7a67ae5a7cf21ad69994bda4c73dbafd3e75001
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-04 18:20:19 +0200

    beecrypt, gptfdisk, mapnik: bump for icu4c, beecrypt test, strict audit

--- Library/Formula/beecrypt.rb
+++ Library/Formula/beecrypt.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Beecrypt < Formula
   homepage "http://beecrypt.sourceforge.net"
   url "https://downloads.sourceforge.net/project/beecrypt/beecrypt/4.2.1/beecrypt-4.2.1.tar.gz"
   sha256 "286f1f56080d1a6b1d024003a5fa2158f4ff82cae0c6829d3c476a4b5898c55d"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
@@ -26,8 +24,34 @@ class Beecrypt < Formula
                           "--without-java",
                           "--without-python"
     system "make"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include "beecrypt/base64.h"
+      #include "beecrypt/sha256.h"
+      #include <stdio.h>
+
+      int main(void)
+      {
+        sha256Param hash;
+        const byte *string = (byte *) "abc";
+        byte digest[32];
+
+        sha256Reset(&hash);
+        sha256Update(&hash, string, sizeof(string) / sizeof(*string));
+        sha256Process(&hash);
+        sha256Digest(&hash, digest);
+
+        printf("%s\\n", b64crc(digest, 32));
+
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-lbeecrypt", "-o", "test"
+    assert_match /ZF8D/, shell_output("./test")
   end
 end
 
