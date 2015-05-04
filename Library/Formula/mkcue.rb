343529d3e259b835969cface8acf52d1f79b74e8
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-04 05:17:26 +0100

    mkcue: use ssl/tls urls

--- Library/Formula/mkcue.rb
+++ Library/Formula/mkcue.rb
@@ -1,6 +1,7 @@
 class Mkcue < Formula
   homepage "https://packages.debian.org/source/stable/mkcue"
-  url "http://ftp.de.debian.org/debian/pool/main/m/mkcue/mkcue_1.orig.tar.gz"
+  url "https://mirrors.kernel.org/debian/pool/main/m/mkcue/mkcue_1.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mkcue/mkcue_1.orig.tar.gz"
   version "1"
   sha256 "2aaf57da4d0f2e24329d5e952e90ec182d4aa82e4b2e025283e42370f9494867"
 
