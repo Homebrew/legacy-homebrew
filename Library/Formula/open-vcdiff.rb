525b3e67ac0b1bd70aa974c9e5a89be76b81d302
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-04 08:20:22 +0100

    open-vcdiff: add 0.8.4 bottle.

--- Library/Formula/open-vcdiff.rb
+++ Library/Formula/open-vcdiff.rb
@@ -4,6 +4,13 @@ class OpenVcdiff < Formula
   version "0.8.4"
   sha256 "2b142b1027fb0a62c41347600e01a53fa274dad15445a7da48083c830c3138b3"
 
+  bottle do
+    cellar :any
+    sha256 "837a43737930f1f09cb5d21fe3bdc18b0cd0365fef954cfd30c81478348abc0f" => :yosemite
+    sha256 "84ef8313b38bac7c846d70a9315fffc81daeb78d82ef5c0338cee438487cecbb" => :mavericks
+    sha256 "aeac4cdebb0bc4796ca39b1ad04cc5a267bf9a34250d59b9268ed69999ee9f05" => :mountain_lion
+  end
+
   def install
     system "./configure", "CPPFLAGS=-DGTEST_USE_OWN_TR1_TUPLE=1",
                           "--disable-debug",
