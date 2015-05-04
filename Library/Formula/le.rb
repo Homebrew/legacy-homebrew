5d4bb04985e3ef5259429ab141deb30a190a2d0c
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-04 04:44:16 +0100

    le: use official upstream url/homepage
    Homepage has moved to Github, and upstream is publishing new releases
    under a slightly different server name to before. No change in maintainer.

--- Library/Formula/le.rb
+++ Library/Formula/le.rb
@@ -1,18 +1,14 @@
-require 'formula'
-
 class Le < Formula
-  homepage 'http://freecode.com/projects/leeditor'
-  # url 'http://ftp.yar.ru/pub/source/le/le-1.14.9.tar.xz'
-  # upstream not responding, source from debian
-  url 'http://ftp.de.debian.org/debian/pool/main/l/le/le_1.14.9.orig.tar.gz'
-  sha1 'ce85cbefb30cf1f5a7e8349dbb24ffa0f65b1fd7'
+  homepage "https://github.com/lavv17/le"
+  url "http://lav.yar.ru/download/le/le-1.15.1.tar.xz"
+  sha256 "d9895ef82c89ae9cf30946bd43fa18b294f645e6a2bacd3ed9d39a3ccf324a4f"
 
-  conflicts_with 'logentries', :because => 'both install a le binary'
+  conflicts_with "logentries", :because => "both install a le binary"
 
   def install
     ENV.j1
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
