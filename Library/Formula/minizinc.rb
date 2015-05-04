a692e94c1febeb0bb94f7da0eb5eb999d3eee1b5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-04 08:12:59 +0100

    minizinc: add 2.0.1 bottle.

--- Library/Formula/minizinc.rb
+++ Library/Formula/minizinc.rb
@@ -11,6 +11,13 @@ class Minizinc < Formula
       sha256 "30562c42e08bda8f79ff87e53054c7def125ce5e4a7ee1d74599068bdd572505"
     end
   end
+  bottle do
+    cellar :any
+    sha256 "ae9f777c740457c2d5698e339ac0434682f43bbc1e154cf797d586647624e5bf" => :yosemite
+    sha256 "58bf476cbe2181e3a6420aa709ade2aecde6b400a70f291aeb7c3a7e747d1e05" => :mavericks
+    sha256 "d5d00aeaf5f6bb6b3f2cb0d78a29271d98fa73deeb7d939055992f631da78f34" => :mountain_lion
+  end
+
 
   depends_on :arch => :x86_64
   depends_on "cmake" => :build
