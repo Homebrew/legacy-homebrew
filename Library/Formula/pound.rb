88bf6897e857450e656606d0c2fc77bca9d5bf10
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-04 02:02:02 +0100

    pound: add 2.7 bottle.

--- Library/Formula/pound.rb
+++ Library/Formula/pound.rb
@@ -3,6 +3,12 @@ class Pound < Formula
   url "http://www.apsis.ch/pound/Pound-2.7.tgz"
   sha256 "cdfbf5a7e8dc8fbbe0d6c1e83cd3bd3f2472160aac65684bb01ef661c626a8e4"
 
+  bottle do
+    sha256 "aebc9ef8e97b4995923752811da180e83adcf2ef55d1809d7dc51b44a73d1b02" => :yosemite
+    sha256 "74c64dba8bf19737259ad996f15bbf66bb2bcd24e71ef206ec4b0e6bf1042a70" => :mavericks
+    sha256 "fa872353daeab3c6386f947b74c43932f66a20acf230575dec3afaf835edc22b" => :mountain_lion
+  end
+
   depends_on "openssl"
   depends_on "pcre"
   depends_on "google-perftools" => :recommended
