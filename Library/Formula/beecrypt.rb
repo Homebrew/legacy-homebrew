9229582d4b4dc84244d68627d403fdf266ca24de
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-05 00:46:12 +0100

    beecrypt: update 4.2.1_3 bottle.

--- Library/Formula/beecrypt.rb
+++ Library/Formula/beecrypt.rb
@@ -6,7 +6,9 @@ class Beecrypt < Formula
 
   bottle do
     cellar :any
-    sha1 "5ebfd5789fc518c873994cb5d9b404837a742b5a" => :mavericks
+    sha256 "1e0e605e9f684ec4ae25018b0f50427700a8cf12db37758d45181e30bbf1b151" => :yosemite
+    sha256 "4f66c5f28a672bd27652b82fef016763b2140de962f8b34defa8fda82d98b070" => :mavericks
+    sha256 "9baaefc4c2af1220ec4d8f8d3328f8d2107bd43a4c0499fc2402b647684d9b04" => :mountain_lion
   end
 
   depends_on "icu4c"
