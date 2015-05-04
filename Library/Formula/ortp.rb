03b9d03b0284533bb439f1f87a2bda79c2d33455
Alex Dunn <dunn.alex@gmail.com>
2015-05-04 14:53:44 -0700

    ortp 0.24.1
    Closes #39363.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/ortp.rb
+++ Library/Formula/ortp.rb
@@ -1,13 +1,27 @@
-require "formula"
-
 class Ortp < Formula
   homepage "http://www.linphone.org/eng/documentation/dev/ortp.html"
-  url "http://download.savannah.gnu.org/releases/linphone/ortp/sources/ortp-0.23.0.tar.gz"
-  sha1 "87a154295636652cc9ac7310dc02e0258db08790"
+  url "http://nongnu.askapache.com/linphone/ortp/sources/ortp-0.24.1.tar.gz"
+  sha256 "d1ecce20bed145186f727914f10f500d0594e30c2a234a276e7e12dcd8814cd5"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include "ortp/logging.h"
+      #include "ortp/rtpsession.h"
+      #include "ortp/sessionset.h"
+      int main()
+      {
+        ORTP_PUBLIC void ortp_init(void);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lortp",
+           testpath/"test.c", "-o", "test"
+    system "./test"
   end
 end
