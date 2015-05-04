eeb41c01e333f71d35aefe0a38746fe8fa582f9f
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-04 05:20:16 +0100

    iprint: use ssl/tls links

--- Library/Formula/iprint.rb
+++ Library/Formula/iprint.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Iprint < Formula
-
-  homepage "http://www.samba.org/ftp/unpacked/junkcode/i.c"
-  url "http://ftp.debian.org/debian/pool/main/i/iprint/iprint_1.3.orig.tar.gz"
-  sha1 "1b423107cf6b1e5c2257648047ff91f5c6ac2249"
+  homepage "https://www.samba.org/ftp/unpacked/junkcode/i.c"
+  url "https://mirrors.kernel.org/debian/pool/main/i/iprint/iprint_1.3.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/iprint/iprint_1.3.orig.tar.gz"
+  sha256 "1079b2b68f4199bc286ed08abba3ee326ce3b4d346bdf77a7b9a5d5759c243a3"
   version "1.3-9"
 
   bottle do
@@ -15,8 +13,8 @@ class Iprint < Formula
   end
 
   patch do
-    url "http://ftp.debian.org/debian/pool/main/i/iprint/iprint_1.3-9.diff.gz"
-    sha1 "6164e8e9bd0e08542de34bbf386cd1f0193f17c5"
+    url "https://mirrors.kernel.org/debian/pool/main/i/iprint/iprint_1.3-9.diff.gz"
+    sha256 "3a1ff260e6d639886c005ece754c2c661c0d3ad7f1f127ddb2943c092e18ab74"
   end
 
   def install
@@ -29,5 +27,4 @@ class Iprint < Formula
   test do
     assert_equal shell_output("#{bin}/i 1234"), "1234 0x4D2 02322 0b10011010010\n"
   end
-
 end
