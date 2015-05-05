101a0ad92e1870f0d129b12e30c446c5e4214d12
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-06 01:15:09 +0200

    base64: use SSL/TLS URLs
    The certificate appears to be somewhat problematic, though latest Firefox and curl accepts it on OS X.
    
    Closes #39418.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/base64.rb
+++ Library/Formula/base64.rb
@@ -1,6 +1,6 @@
 class Base64 < Formula
-  homepage "http://www.fourmilab.ch/webtools/base64/"
-  url "http://www.fourmilab.ch/webtools/base64/base64-1.5.tar.gz"
+  homepage "https://www.fourmilab.ch/webtools/base64/"
+  url "https://www.fourmilab.ch/webtools/base64/base64-1.5.tar.gz"
   sha256 "2416578ba7a7197bddd1ee578a6d8872707c831d2419bdc2c1b4317a7e3c8a2a"
 
   def install
