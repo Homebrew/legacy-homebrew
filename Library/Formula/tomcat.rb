16644caa0171a08f06c790e905114d54ec21ab61
makigumo <dan@wadoku.de>
2015-05-05 10:08:33 +0200

    tomcat 8.0.22
    Closes #39478.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -1,8 +1,8 @@
 class Tomcat < Formula
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21.tar.gz"
-  sha1 "957e88df8a9c3fc6b786321c4014b44c5c775773"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.0.22/bin/apache-tomcat-8.0.22.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.22/bin/apache-tomcat-8.0.22.tar.gz"
+  sha256 "cb163576d03f45981bf025b62b46e71c3c634584daa4662fc721d37d46989eff"
 
   bottle do
     cellar :any
@@ -14,21 +14,21 @@ class Tomcat < Formula
   option "with-fulldocs", "Install full documentation locally"
 
   resource "fulldocs" do
-    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21-fulldocs.tar.gz"
-    mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.21/bin/apache-tomcat-8.0.21-fulldocs.tar.gz"
-    version "8.0.21"
-    sha1 "3fc4db49c36846b4197810b7f26d07f5bdd17931"
+    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-8/v8.0.22/bin/apache-tomcat-8.0.22-fulldocs.tar.gz"
+    mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.22/bin/apache-tomcat-8.0.22-fulldocs.tar.gz"
+    version "8.0.22"
+    sha256 "f1287f7a005180d8ff8b60461327554c9d5473d1d6784d97cbb4b580ef292c8c"
   end
 
   def install
     # Remove Windows scripts
-    rm_rf Dir['bin/*.bat']
+    rm_rf Dir["bin/*.bat"]
 
     # Install files
-    prefix.install %w{ NOTICE LICENSE RELEASE-NOTES RUNNING.txt }
-    libexec.install Dir['*']
+    prefix.install %w[ NOTICE LICENSE RELEASE-NOTES RUNNING.txt ]
+    libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/catalina.sh" => "catalina"
 
-    (share/'fulldocs').install resource('fulldocs') if build.with? 'fulldocs'
+    (share/"fulldocs").install resource("fulldocs") if build.with? "fulldocs"
   end
 end
