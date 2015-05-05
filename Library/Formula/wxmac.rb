1d0e653019e4f3d7a9cd2db9782c2daeae2945f4
Ryan Mulder <ryanjmulder@gmail.com>
2015-05-05 13:39:35 -0400

    specify enable/disable shared directly on the configure line

--- Library/Formula/wxmac.rb
+++ Library/Formula/wxmac.rb
@@ -36,7 +36,6 @@ class Wxmac < Formula
     args = [
       "--disable-debug",
       "--prefix=#{prefix}",
-      "--enable-shared",
       "--enable-unicode",
       "--enable-std_string",
       "--enable-display",
@@ -69,14 +68,11 @@ class Wxmac < Formula
       "--disable-monolithic"
     ]
 
-    system "./configure", *args
+    system "./configure", "--enable-shared", *args
     system "make", "install"
 
     if build.with? "static"
-      args.delete("--enable-shared")
-      args << "--disable-shared"
-
-      system "./configure", *args
+      system "./configure", "--disable-shared", *args
       system "make", "install"
     end
   end
