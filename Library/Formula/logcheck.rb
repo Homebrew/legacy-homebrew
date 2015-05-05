03ceac66eaa1e37622dd65bf5a65f4d107d0015f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-05 02:59:06 +0100

    logcheck: update 1.3.17 bottle.

--- Library/Formula/logcheck.rb
+++ Library/Formula/logcheck.rb
@@ -6,9 +6,10 @@ class Logcheck < Formula
 
   bottle do
     cellar :any
-    sha1 "94773d87f8879d53bdcaa26853342d7a45af1da8" => :yosemite
-    sha1 "88197b7fa15f6842a2fe3e07f38e825e7b02947e" => :mavericks
-    sha1 "c2b5c09aae9987d2441ca205d74ce44439e912c0" => :mountain_lion
+    revision 1
+    sha256 "cd8a2d7c4a28b13047fb89624bfcbc5dad359545c1cc80cd1b9e79018f368372" => :yosemite
+    sha256 "4757698c916095d0f9a29c0e42e55b6c5c24a3a972a569ea2eae2e795f0a59ba" => :mavericks
+    sha256 "1a005750f43b2bc64f8031233470f827c10fb567845140d5b1e91e668cc249af" => :mountain_lion
   end
 
   def install
