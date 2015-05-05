d602498d279c3df5a442f3c97142883820d0a81a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-05 02:59:06 +0100

    pmccabe: add 2.6 bottle.

--- Library/Formula/pmccabe.rb
+++ Library/Formula/pmccabe.rb
@@ -4,6 +4,13 @@ class Pmccabe < Formula
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/p/pmccabe/pmccabe_2.6.tar.gz"
   sha256 "e490fe7c9368fec3613326265dd44563dc47182d142f579a40eca0e5d20a7028"
 
+  bottle do
+    cellar :any
+    sha256 "babed1092c9b75baf350599cde2359c9a339db8f40a0a88875757a5834ac51a7" => :yosemite
+    sha256 "3749503b9a930447c9e2e778205965a6cbcd7987950bd93bc6530646539e7735" => :mavericks
+    sha256 "62b9b9309f189d79821368ca2e69912ee33a3fef7a208f57dfdb446e4249f36a" => :mountain_lion
+  end
+
   def install
     ENV.append_to_cflags "-D__unix"
 
