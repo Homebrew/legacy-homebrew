afe0555f88b08551e2515ac5566adc415ffff59a
Tim D. Smith <git@tim-smith.us>
2015-05-05 11:37:17 -0700

    mesos: simplify test
    Setting up the .pth file is done automatically for tests now.
    
    Closes #39402.

--- Library/Formula/mesos.rb
+++ Library/Formula/mesos.rb
@@ -68,7 +68,7 @@ class Mesos < Formula
     # https://github.com/Homebrew/homebrew/pull/37087
     native_patch = <<-EOS.undent
       import os
-      os.environ["CC"] = "#{ENV.cxx}"
+      os.environ["CC"] = os.environ["CXX"]
       os.environ["LDFLAGS"] = "@LIBS@"
       \\0
     EOS
@@ -140,11 +140,6 @@ class Mesos < Formula
     Process.kill("TERM", master)
     Process.kill("TERM", slave)
     assert File.exist?("#{testpath}/executed")
-
-    user_site = Language::Python.user_site_packages("python")
-    mkdir_p user_site
-    pth_contents = "import site; site.addsitedir('#{Language::Python.homebrew_site_packages}')\n"
-    (user_site/"homebrew.pth").write pth_contents
     system "python", "-c", "import mesos.native"
   end
 end
