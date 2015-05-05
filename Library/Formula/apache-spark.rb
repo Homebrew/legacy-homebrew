6714350d55e51ac12c03a78287b35d04857dc90b
Tim D. Smith <git@tim-smith.us>
2015-05-05 15:24:12 -0700

    apache-spark: rename beeline to avoid hive conflict
    Reference #39319.

--- Library/Formula/apache-spark.rb
+++ Library/Formula/apache-spark.rb
@@ -5,11 +5,13 @@ class ApacheSpark < Formula
   head "https://github.com/apache/spark.git"
   url "https://d3kbcqa49mib13.cloudfront.net/spark-1.3.1-bin-hadoop2.6.tgz"
   version "1.3.1"
+  revision 1
   sha1 "86911b6c8964230a93691bd45589f491c10d36c0"
 
-  conflicts_with 'hive', :because => 'both install `beeline` binaries'
-
   def install
+    # Rename beeline to distinguish it from hive's beeline
+    mv "bin/beeline", "bin/spark-beeline"
+
     rm_f Dir["bin/*.cmd"]
     libexec.install Dir["*"]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
