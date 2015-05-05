e32e5a3868ae300b646d7213c79e0aed26669a21
Baptiste Fontaine <batifon@yahoo.fr>
2015-05-05 20:58:02 +0200

    xmlrpc-c 1.33.17
    Closes #39403.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/xmlrpc-c.rb
+++ Library/Formula/xmlrpc-c.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class XmlrpcC < Formula
-  homepage 'http://xmlrpc-c.sourceforge.net/'
-  url 'http://svn.code.sf.net/p/xmlrpc-c/code/stable', :revision => 2489
-  version '1.33.03'
+  homepage "http://xmlrpc-c.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/xmlrpc-c/Xmlrpc-c%20Super%20Stable/1.33.17/xmlrpc-c-1.33.17.tgz"
+  sha256 "50118a3ca1114828f7cae27b7253cc25045849e487f5d0bb91d962777fc05355"
 
   def install
     ENV.deparallelize
@@ -13,6 +11,10 @@ class XmlrpcC < Formula
 
     # xmlrpc-config.h cannot be found if only calling make install
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/xmlrpc-c-config", "--features"
   end
 end
