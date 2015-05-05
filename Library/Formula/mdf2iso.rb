e32ca9eb34cd6d193d6e1124f839c113f66e3389
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-05 02:59:06 +0100

    mdf2iso: add 0.3.1 bottle.

--- Library/Formula/mdf2iso.rb
+++ Library/Formula/mdf2iso.rb
@@ -4,6 +4,13 @@ class Mdf2iso < Formula
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mdf2iso/mdf2iso_0.3.1.orig.tar.gz"
   sha256 "906f0583cb3d36c4d862da23837eebaaaa74033c6b0b6961f2475b946a71feb7"
 
+  bottle do
+    cellar :any
+    sha256 "ae592a8d5662b2ae962603addaf55b521f3968f5d7745f761a9b90f7b5cdabf6" => :yosemite
+    sha256 "e9af5cc191566bcf09cf9f1d22532c2971b0d66518a4b7b8c1a613f151536791" => :mavericks
+    sha256 "8a82cbf7a260bb216412f27dc67f75ea047472b5ce01526ee5246466574a27c3" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
