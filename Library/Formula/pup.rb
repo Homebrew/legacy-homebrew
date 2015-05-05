d83736dbbb48066260cb3d75fa7897c0f551d4fd
Alex Dunn <dunn.alex@gmail.com>
2015-05-05 15:55:39 -0700

    pup 0.3.9
    Closes #39417.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/pup.rb
+++ Library/Formula/pup.rb
@@ -2,8 +2,8 @@ require "language/go"
 
 class Pup < Formula
   homepage "https://github.com/EricChiang/pup"
-  url "https://github.com/ericchiang/pup/archive/v0.3.8.tar.gz"
-  sha256 "8cf911f9ff5ddc5138ab86ce58f93f468e233e78782ca0dc985d5e3485c4c8d6"
+  url "https://github.com/ericchiang/pup/archive/v0.3.9.tar.gz"
+  sha256 "5e59805edf84d73b2b4c58fe5aeb9a12fc70c028b4aaf58ded6b91ff418b0dda"
 
   head "https://github.com/EricChiang/pup.git"
 
@@ -31,28 +31,23 @@ class Pup < Formula
   # find . -name "*.go" -exec head -20 "{}" ";" | grep ".*\..*/" | sort | uniq
   go_resource "github.com/fatih/color" do
     url "https://github.com/fatih/color.git",
-        # this was just the `master` at the time of this Formula
-        :revision => "1b4171c5cdb770e90ab04e960b52f43b583d398c"
+        :revision => "b8f08a5598ffe40b0e3f45d483d3cfe3c1dc4964"
   end
   go_resource "github.com/shiena/ansicolor" do
     url "https://github.com/shiena/ansicolor.git",
-        # this was just the `master` at the time of this Formula
         :revision => "8368d3b31cf6f2c2464c7a91675342c9a0ac6658"
   end
   go_resource "github.com/mattn/go-colorable" do
     url "https://github.com/mattn/go-colorable.git",
-        # this was just the `master` at the time of this Formula
         :revision => "d67e0b7d1797975196499f79bcc322c08b9f218b"
   end
   go_resource "golang.org/x/net" do
     url "https://go.googlesource.com/net.git",
-        # this was just the `master` at the time of this Formula
-        :revision => "7dbad50ab5b31073856416cdcfeb2796d682f844"
+        :revision => "a8c61998a557a37435f719980da368469c10bfed"
   end
   go_resource "golang.org/x/text" do
     url "https://go.googlesource.com/text.git",
-      # this was just the `tip` at the time of this Formula
-      :revision => "a9f4d1a427d033e4320e1c1639b87e41bd310cce"
+      :revision => "cee5b80e82c1d078cfdbe853beb9c8318c593677"
   end
 
   def install
@@ -65,7 +60,7 @@ class Pup < Formula
     prefix.install "LICENSE"
     my_pkg.install Dir["*"]
 
-    Language::Go.stage_deps resources, buildpath / "src"
+    Language::Go.stage_deps resources, buildpath/"src"
 
     ENV["GOPATH"] = buildpath
     cd "src/github.com/mitchellh/gox" do
@@ -86,8 +81,8 @@ class Pup < Formula
   end
 
   test do
-    expected = %(Hello)
-    actual = %x(echo "<body><p>Hello</p></body>" | pup p "text{}").strip
+    expected = "Hello"
+    actual = pipe_output("pup p \"text{}\"", "<body><p>Hello</p></body>").strip
     assert_equal expected, actual
   end
 end
