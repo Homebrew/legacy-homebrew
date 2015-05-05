84021a2212ef61a17204cf4eb0d9e63a087861c3
Alex Dunn <dunn.alex@gmail.com>
2015-05-04 18:26:19 -0700

    openslide 3.4.1
    Closes #39368.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/openslide.rb
+++ Library/Formula/openslide.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Openslide < Formula
-  homepage 'http://openslide.org/'
-  url 'https://github.com/openslide/openslide/releases/download/v3.4.0/openslide-3.4.0.tar.gz'
-  sha1 'edd28a142f7801a354949c05e7f1b7dcb246db6b'
+  homepage "http://openslide.org/"
+  url "https://github.com/openslide/openslide/releases/download/v3.4.1/openslide-3.4.1.tar.xz"
+  sha256 "9938034dba7f48fadc90a2cdf8cfe94c5613b04098d1348a5ff19da95b990564"
 
   bottle do
     cellar :any
@@ -12,19 +10,30 @@ class Openslide < Formula
     sha1 "ef49c02614e0ec8c2a0e5f71a6de309023a77a0c" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libpng'
-  depends_on 'jpeg'
-  depends_on 'libxml2'
-  depends_on 'libtiff'
-  depends_on 'glib'
-  depends_on 'openjpeg'
-  depends_on 'cairo'
-  depends_on 'gdk-pixbuf'
+  depends_on "pkg-config" => :build
+  depends_on "libpng"
+  depends_on "jpeg"
+  depends_on "libxml2"
+  depends_on "libtiff"
+  depends_on "glib"
+  depends_on "openjpeg"
+  depends_on "cairo"
+  depends_on "gdk-pixbuf"
+
+  resource "svs" do
+    url "http://openslide.cs.cmu.edu/download/openslide-testdata/Aperio/CMU-1-Small-Region.svs"
+    sha256 "ed92d5a9f2e86df67640d6f92ce3e231419ce127131697fbbce42ad5e002c8a7"
+  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    resource("svs").stage do
+      system bin/"openslide-show-properties", "CMU-1-Small-Region.svs"
+    end
   end
 end
