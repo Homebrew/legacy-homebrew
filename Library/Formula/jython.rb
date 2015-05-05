fad48e69c25a6846d95008b8fd69e6f4c73747a9
Daniel Slapman <danslapman@gmail.com>
2015-05-05 12:03:15 +0300

    jython 2.7.0
    Remove devel jython version
    
    Replace sha1 signature with sha256
    
    Remove inreplace command
    
    Closes #39380.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/jython.rb
+++ Library/Formula/jython.rb
@@ -1,16 +1,10 @@
 class Jython < Formula
   homepage "http://www.jython.org"
-  url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.5.3/jython-installer-2.5.3.jar"
-  sha256 "05405966cdfa57abc8e705dd6aab92b8240097ce709fb916c8a0dbcaa491f99e"
-
-  devel do
-    url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7-rc3/jython-installer-2.7-rc3.jar"
-    sha256 "882b0139b302a2bfd1158e3b6640530dc224be7ce10d850df0b6d28e63f869f1"
-  end
+  url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7.0/jython-installer-2.7.0.jar"
+  sha256 "b44352ece72382268a60e2848741c96609a91d796bb9a9c6ebeff62f0c12c9cf"
 
   def install
     system "java", "-jar", cached_download, "-s", "-d", libexec
-    inreplace libexec/"bin/jython", "PRG=$0", "PRG=#{libexec}/bin/jython" if build.stable?
     bin.install_symlink libexec/"bin/jython"
   end
 end
