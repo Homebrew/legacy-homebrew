452e0cf54ca6b16735f1209b67097ef88144ab9a
Viktor Szakáts <vszakats@users.noreply.github.com>
2015-05-04 23:17:19 -0700

    gptfdisk: revision for icu4c

--- Library/Formula/gptfdisk.rb
+++ Library/Formula/gptfdisk.rb
@@ -2,6 +2,7 @@ class Gptfdisk < Formula
   homepage "http://www.rodsbooks.com/gdisk/"
   url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.0/gptfdisk-1.0.0.tar.gz"
   sha256 "5b66956743a799fc0471cdb032665c1391e82f9c5b3f1d7d726d29fe2ba01d6c"
+  revision 1
 
   bottle do
     cellar :any
