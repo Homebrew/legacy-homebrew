85c43af192d36676ae4cf433b50f5e9e5319eba2
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-06 01:20:44 +0200

    ant: use SSL/TLS, sha256, .bz2
    Closes #39419.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/ant.rb
+++ Library/Formula/ant.rb
@@ -1,7 +1,7 @@
 class Ant < Formula
-  homepage "http://ant.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.4-bin.tar.gz"
-  sha1 "6c41481e8201f6b3f7e216146b95bb6de70208bb"
+  homepage "https://ant.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.4-bin.tar.bz2"
+  sha256 "20c16575684b8869dd9d19abe0fb504566adefb5d397881e70a417886e0088cf"
   head "https://git-wip-us.apache.org/repos/asf/ant.git"
 
   bottle do
@@ -17,13 +17,13 @@ class Ant < Formula
   option "with-bcel", "Install Byte Code Engineering Library"
 
   resource "ivy" do
-    url "http://www.apache.org/dyn/closer.cgi?path=ant/ivy/2.4.0/apache-ivy-2.4.0-bin.tar.gz"
-    sha1 "97a206e3b6ec2ce9793d2ee151fa997a12647c7f"
+    url "https://www.apache.org/dyn/closer.cgi?path=ant/ivy/2.4.0/apache-ivy-2.4.0-bin.tar.gz"
+    sha256 "7a3d13a80b69d71608191463dfc2a74fff8ef638ce0208e70d54d28ba9785ee9"
   end
 
   resource "bcel" do
     url "http://central.maven.org/maven2/org/apache/bcel/bcel/5.2/bcel-5.2.jar"
-    sha1 "96b2cefeb067c08c31225d48e2a689f814baae25"
+    sha256 "7b87e2fd9ac3205a6e5ba9ef5e58a8f0ab8d1a0e0d00cb2a761951fa298cc733"
   end
 
   def install
