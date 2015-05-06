a000119f9023c2bcaf2f4851248e0e48071ea296
ZeoS (Fernando Otero) <fotero@gmail.com>
2015-05-06 17:51:37 -0300

    do not depend on system file

--- job-server-tests/src/spark.jobserver/StreamingTestJob.scala
+++ job-server-tests/src/spark.jobserver/StreamingTestJob.scala
@@ -1,7 +1,10 @@
 package spark.jobserver
 
 import com.typesafe.config.Config
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{Time, Seconds, StreamingContext}
+
+import scala.collection.mutable
 
 /**
  * listen on a configured port for lines
@@ -9,8 +12,12 @@ import org.apache.spark.streaming.{Time, Seconds, StreamingContext}
 object StreamingTestJob extends SparkStramingJob {
   def validate(ssc: StreamingContext, config: Config): SparkJobValidation = SparkJobValid
 
+
   def runJob(ssc: StreamingContext, config: Config): Any = {
-    val lines = ssc.textFileStream(config.getString("test.file"))
+    val queue = mutable.Queue[RDD[String]]()
+    queue += ssc.sparkContext.makeRDD(Seq("123", "test", "test2"))
+    val lines = ssc.queueStream(queue)
+    lines.foreachRDD(rdd => println("count: " + rdd.count()))
     val words = lines.flatMap(_.split(" "))
     words.foreachRDD(rdd => rdd.collect().foreach("test " + print(_)))
     val pairs = words.map(word => (word, 1))
