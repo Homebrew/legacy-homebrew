5becfebb9285e12b53983997c919c0d4932f9929
Alex Dunn <dunn.alex@gmail.com>
2015-05-05 18:51:00 -0700

    the_platinum_searcher 1.7.6
    Closes #39424.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/the_platinum_searcher.rb
+++ Library/Formula/the_platinum_searcher.rb
@@ -1,33 +1,32 @@
-require "formula"
 require "language/go"
 
 class ThePlatinumSearcher < Formula
   homepage "https://github.com/monochromegane/the_platinum_searcher"
-  url "https://github.com/monochromegane/the_platinum_searcher/archive/v1.7.5.tar.gz"
-  sha1 "775f73aaf894dabcfbd80f07eeb0d8450010ac15"
+  url "https://github.com/monochromegane/the_platinum_searcher/archive/v1.7.6.tar.gz"
+  sha256 "7c7249f88eab09b8f1b89aad2987dc5e23b8ae0df73390150ae9a8a77df346e2"
   head "https://github.com/monochromegane/the_platinum_searcher.git"
+
+  depends_on "go" => :build
 
   go_resource "github.com/jessevdk/go-flags" do
     url "https://github.com/jessevdk/go-flags.git",
-      :revision => "7047cf7a8dc6f41e53365420ab62d415055232c6"
+        :revision => "5e118789801496c93ba210d34ef1f2ce5a9173bd"
   end
 
   go_resource "github.com/monochromegane/terminal" do
     url "https://github.com/monochromegane/terminal.git",
-      :revision => "6d255869fb99937f1f287bd1fe3a034c6c4f68f6"
+        :revision => "6d255869fb99937f1f287bd1fe3a034c6c4f68f6"
   end
 
   go_resource "github.com/shiena/ansicolor" do
     url "https://github.com/shiena/ansicolor.git",
-      :revision => "6046e7d18a7698e98846e5d25842e9cf15aecf2c"
+        :revision => "8368d3b31cf6f2c2464c7a91675342c9a0ac6658"
   end
 
-  go_resource "code.google.com/p/go.text" do
-    url "https://code.google.com/p/go.text", :using => :hg,
-      :revision => "46250cb715a27b42c736a5ff2a4e6fa0b2118952"
+  go_resource "golang.org/x/text" do
+    url "https://github.com/golang/text.git",
+        :revision => "6c3b324efd553c7d76e4da4ed671de76c6bbc791"
   end
-
-  depends_on "go" => :build
 
   bottle do
     cellar :any
@@ -44,8 +43,7 @@ class ThePlatinumSearcher < Formula
     ENV["GOPATH"] = buildpath
     Language::Go.stage_deps resources, buildpath/"src"
 
-    system "go", "build", "-o", "pt", "cmd/pt/main.go"
-    bin.install "pt"
+    system "go", "build", "-o", bin/"pt", "cmd/pt/main.go"
   end
 
   test do
