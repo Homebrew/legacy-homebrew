361b7784244d973336b8692253b73ea564ea5ae4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-07 19:54:48 +0100

    carthage: update 0.7.1 bottle.

--- Library/Formula/carthage.rb
+++ Library/Formula/carthage.rb
@@ -9,7 +9,7 @@ class Carthage < Formula
 
   bottle do
     cellar :any
-    sha256 "4c72a1e4fb5407c8b50de7d84828f55bec23a05e3999eb4d1049e9a37b23ef95" => :yosemite
+    sha256 "562b2ee4dd925cf67ad21a9a6103550aeba60540f7a669050ae2e5e960b35496" => :yosemite
   end
 
   def install
