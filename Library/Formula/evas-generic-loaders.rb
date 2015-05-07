6a43585f5de57ceb8310f91f54d56515fdb95a9d
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-07 23:12:22 +0200

    evas-generic-loader: use SSL/TLS in url

--- Library/Formula/evas-generic-loaders.rb
+++ Library/Formula/evas-generic-loaders.rb
@@ -1,6 +1,6 @@
 class EvasGenericLoaders < Formula
   homepage "https://enlightenment.org"
-  url "http://download.enlightenment.org/rel/libs/evas_generic_loaders/evas_generic_loaders-1.14.0.tar.gz"
+  url "https://download.enlightenment.org/rel/libs/evas_generic_loaders/evas_generic_loaders-1.14.0.tar.gz"
   sha256 "943b25427c4e77a3aeae72811557a0b1b7ec4c61aa53922a4c4faf17b3dea812"
 
   bottle do
