0d749e8b801f763fe1bd80134670cbfe0183378a
Sebastian Blessing <sebastian@causality.io>
2015-05-07 11:37:50 +0200

    upgraded ponyc to v 0.1.3

--- Library/Formula/ponyc.rb
+++ Library/Formula/ponyc.rb
@@ -1,8 +1,7 @@
 class Ponyc < Formula
   homepage "http://www.ponylang.org"
-  url "http://www.ponylang.org/releases/source/ponyc-0.1.2.tar.bz2"
-  sha256 "224aa6d6c80574dd6074041eb7c6a83171862aea81d69ad1bf91f04d4f567d0a"
-
+  url "http://www.ponylang.org/releases/source/ponyc-0.1.3.tar.bz2"
+  sha256 "bdf6af248362bcb4b9d3869eca5b17b35ef1212786cc93a10efaace516bb6b3e" 
   depends_on "llvm"
 
   def install
