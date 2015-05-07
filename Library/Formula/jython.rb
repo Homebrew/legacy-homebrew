194133cc6601e1993e080b49de9b972f20ebe748
Daniel Slapman <danslapman@gmail.com>
2015-05-07 10:01:48 +0300

    Replace sha1 signature with sha256

--- Library/Formula/jython.rb
+++ Library/Formula/jython.rb
@@ -1,7 +1,7 @@
 class Jython < Formula
   homepage "http://www.jython.org"
   url "https://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7.0/jython-installer-2.7.0.jar"
-  sha1 "b08d73b5df7d95e35e7b8dcaf1558025cf24f0f1"
+  sha256 "b44352ece72382268a60e2848741c96609a91d796bb9a9c6ebeff62f0c12c9cf"
 
   def install
     system "java", "-jar", cached_download, "-s", "-d", libexec
