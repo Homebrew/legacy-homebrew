25c5688bc42605e000321f4e22d1cf7f3bb0fb43
Canmor Lam <canmor.lam@gmail.com>
2015-05-07 12:07:02 +0800

    android-platform-tools: updated comments
    Closes #39398.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/android-platform-tools.rb
+++ Library/Formula/android-platform-tools.rb
@@ -1,7 +1,7 @@
 class AndroidPlatformTools < Formula
   homepage "https://developer.android.com/sdk"
   # the url is from:
-  # https://dl.google.com/android/repository/repository-8.xml
+  # https://dl.google.com/android/repository/repository-10.xml
   url "https://dl.google.com/android/repository/platform-tools_r22-macosx.zip"
   version "22.0.0"
   sha256 "c06721ff66a1a3f70e489325f06474e28cd0efd5352f097ee2ff58167b4f6564"
