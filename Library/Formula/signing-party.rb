4f20731bd4d9207149f262f09bbcbc26718a023d
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-07 19:57:35 +0200

    signing-party: use SSL/TLS mirrors, fix mirrors identical to url
    Closes #39483.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/signing-party.rb
+++ Library/Formula/signing-party.rb
@@ -24,61 +24,61 @@ class SigningParty < Formula
 
   resource "GnuPG::Interface" do
     url "http://search.cpan.org/CPAN/authors/id/A/AL/ALEXMV/GnuPG-Interface-0.50.tar.gz"
-    mirror "http://search.mcpan.org/CPAN/authors/id/A/AL/ALEXMV/GnuPG-Interface-0.50.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/A/AL/ALEXMV/GnuPG-Interface-0.50.tar.gz"
     sha256 "66e58e41c20d4ecd4e387e1ab1eee887f1c3c9fb0c65d0af2f95620e7b97141e"
   end
 
   resource "Text::Iconv" do
     url "http://search.cpan.org/CPAN/authors/id/M/MP/MPIOTR/Text-Iconv-1.7.tar.gz"
-    mirror "http://search.mcpan.org/CPAN/authors/id/M/MP/MPIOTR/Text-Iconv-1.7.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/M/MP/MPIOTR/Text-Iconv-1.7.tar.gz"
     sha256 "5b80b7d5e709d34393bcba88971864a17b44a5bf0f9e4bcee383d029e7d2d5c3"
   end
 
   resource "Text::Template" do
     url "http://search.cpan.org/CPAN/authors/id/M/MJ/MJD/Text-Template-1.46.tar.gz"
-    mirror "http://search.mcpan.org/CPAN/authors/id/M/MJ/MJD/Text-Template-1.46.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/M/MJ/MJD/Text-Template-1.46.tar.gz"
     sha256 "77d812cb86e48091bcd59aa8522ef887b33a0ff758f8a269da8c2b733889d580"
   end
 
   resource "Net::IDN::Encode" do
     url "http://search.cpan.org/CPAN/authors/id/C/CF/CFAERBER/Net-IDN-Encode-2.200.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/C/CF/CFAERBER/Net-IDN-Encode-2.200.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/C/CF/CFAERBER/Net-IDN-Encode-2.200.tar.gz"
     sha256 "c7ce0bb438bb22c755c958d02ff8d9385ec71b213efaf77b3723d91d562b9360"
   end
 
   resource "MooX::late" do
     url "http://search.cpan.org/CPAN/authors/id/T/TO/TOBYINK/MooX-late-0.014.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/T/TO/TOBYINK/MooX-late-0.014.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/T/TO/TOBYINK/MooX-late-0.014.tar.gz"
     sha256 "7bc00d77a720319b1baf7fbde99d42beac87de256cee44d2f32be25014a5e707"
   end
 
   resource "MooX::HandlesVia" do
     url "http://search.cpan.org/CPAN/authors/id/M/MA/MATTP/MooX-HandlesVia-0.001005.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/M/MA/MATTP/MooX-HandlesVia-0.001005.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/M/MA/MATTP/MooX-HandlesVia-0.001005.tar.gz"
     sha256 "a69de0c201269a4ad2bfce51b200a91bf7f20c0a1d392f130185c8ae533f9360"
   end
 
   resource "Type::Tiny" do
     url "http://search.cpan.org/CPAN/authors/id/T/TO/TOBYINK/Type-Tiny-1.000005.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/T/TO/TOBYINK/Type-Tiny-1.000005.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/T/TO/TOBYINK/Type-Tiny-1.000005.tar.gz"
     sha256 "42ed36c011825aa1e6995a4e8638621a1b2103a0970b15764ca3919368042365"
   end
 
   resource "Exporter::Tiny" do
     url "http://search.cpan.org/CPAN/authors/id/T/TO/TOBYINK/Exporter-Tiny-0.042.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/T/TO/TOBYINK/Exporter-Tiny-0.042.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/T/TO/TOBYINK/Exporter-Tiny-0.042.tar.gz"
     sha256 "8f1622c5ebbfbcd519ead81df7917e48cb16cc527b1c46737b0459c3908a023f"
   end
 
   resource "Data::Perl" do
     url "http://search.cpan.org/CPAN/authors/id/M/MA/MATTP/Data-Perl-0.002009.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/M/MA/MATTP/Data-Perl-0.002009.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/M/MA/MATTP/Data-Perl-0.002009.tar.gz"
     sha256 "b62b2225870c2c3b16fb78c429f8729ddb8ed0e342f4209ec3c261b764c36f8b"
   end
 
   resource "CPAN::Meta::Requirements" do
     url "http://search.cpan.org/CPAN/authors/id/D/DA/DAGOLDEN/CPAN-Meta-Requirements-2.126.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/D/DA/DAGOLDEN/CPAN-Meta-Requirements-2.126.tar.gz"
+    mirror "https://cpan.metacpan.org/authors/id/D/DA/DAGOLDEN/CPAN-Meta-Requirements-2.126.tar.gz"
     sha256 "4b4ec24844692482b9c69a231a2145adb44356f0d0e9b56b171263f0f56db622"
   end
 
