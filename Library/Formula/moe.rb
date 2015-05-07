ed8800051f162d3f202ebbb648a0a747777a2404
Dj Padzensky <dj@contextengines.com>
2015-05-06 22:56:37 -0700

    moe 1.7 (new formula)

--- Library/Formula/moe.rb
+++ Library/Formula/moe.rb
@@ -1,6 +1,7 @@
 class Moe < Formula
   homepage "https://www.gnu.org/software/moe/moe.html"
   url "http://ftpmirror.gnu.org/moe/moe-1.7.tar.lz"
+  mirror "https://ftp.gnu.org/pub/gnu/moe/moe-1.7.tar.lz"
   sha256 "33ba66f948353c105232e9c8b2da8dc7b6ddc8cdff4beb3e3f55d78bc3acf1bb"
 
   def install
@@ -9,6 +10,6 @@ class Moe < Formula
   end
 
   test do
-    system "${bin}/moe", "--version"
+    system "#{bin}/moe", "--version"
   end
 end
