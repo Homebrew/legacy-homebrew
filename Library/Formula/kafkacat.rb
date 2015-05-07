7edc54a9a696cfe87ef53eaea37760102bd24ee6
Redmar Kerkhoff <redmar@creativecode.nl>
2015-05-07 21:36:58 +0200

    kafkacat

---
+++ Library/Formula/kafkacat.rb
@@ -0,0 +1,19 @@
+class Kafkacat < Formula
+  homepage "https://github.com/edenhill/kafkacat"
+  url "https://github.com/edenhill/kafkacat/archive/1.1.0.tar.gz"
+  version "1.1.0"
+  sha256 "290a16b6bbacedf2ae7b0eb89e1295ec803a378ecd7307485ebf4ef0a89702d9"
+  
+  depends_on "librdkafka"
+  
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/kafkacat | grep -ci \"Apache Kafka producer and consumer tool\""
+  end
+end
