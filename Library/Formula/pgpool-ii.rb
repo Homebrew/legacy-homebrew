dff99529a6c9afb2796a3e7ae982577daef5f4da
Alex Dunn <dunn.alex@gmail.com>
2015-05-07 10:42:54 -0700

    pgpool-ii 3.4.2
    Closes #39482.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/pgpool-ii.rb
+++ Library/Formula/pgpool-ii.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class PgpoolIi < Formula
   homepage "http://www.pgpool.net/mediawiki/index.php/Main_Page"
-  url "http://www.pgpool.net/download.php?f=pgpool-II-3.4.0.tar.gz"
-  sha1 "5502268055b6ba48013c1b7c9ac5a8ce3a0d30ed"
+  url "http://www.pgpool.net/download.php?f=pgpool-II-3.4.2.tar.gz"
+  sha256 "d031fea1313eaf84116f16bc6d0053c9432b04da160e5544ab6445c1f876c351"
 
   bottle do
     sha1 "b05f96a3f5d0ff02afea65aa50fa33ca1f75defb" => :yosemite
@@ -16,5 +14,9 @@ class PgpoolIi < Formula
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    system bin/"pg_md5", "--md5auth", "pool_passwd", "--config-file", etc/"pgpool.conf.sample"
   end
 end
