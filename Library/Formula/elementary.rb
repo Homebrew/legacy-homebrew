0779b9304fc382a634a85270f6d922c344d1cc18
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-07 23:12:51 +0200

    elementary: use SSL/TLS in url

--- Library/Formula/elementary.rb
+++ Library/Formula/elementary.rb
@@ -1,6 +1,6 @@
 class Elementary < Formula
   homepage "https://www.enlightenment.org"
-  url "http://download.enlightenment.org/rel/libs/elementary/elementary-1.14.0.tar.gz"
+  url "https://download.enlightenment.org/rel/libs/elementary/elementary-1.14.0.tar.gz"
   sha256 "aa06ca1b332b3cd29ffd136c1f10edf08a06906b3532785ce7e390c1cad6090e"
 
   bottle do
