d316cc41cd30c8294a3ddfeca4f75427a4e6371f
Xu Cheng <xucheng@me.com>
2015-05-08 13:46:42 +0800

    bear 2.0.3
    Closes #39515.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/bear.rb
+++ Library/Formula/bear.rb
@@ -1,7 +1,7 @@
 class Bear < Formula
   homepage "https://github.com/rizsotto/Bear"
-  url "https://github.com/rizsotto/Bear/archive/2.0.1.tar.gz"
-  sha1 "31cf2b82a44f6eb5a3740c9f8aa9f2cd662e9a68"
+  url "https://github.com/rizsotto/Bear/archive/2.0.3.tar.gz"
+  sha256 "e9d217465198453ce87237e650dedb77f92cb530a10eac53b4a062ba779bd6c1"
   head "https://github.com/rizsotto/Bear.git"
 
   bottle do
@@ -12,8 +12,6 @@ class Bear < Formula
 
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
-  depends_on "libconfig"
 
   def install
     mkdir "build" do
