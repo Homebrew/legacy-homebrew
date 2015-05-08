8892f349a82aedaae494fb254bf965ea35455b2a
Alex Dunn <dunn.alex@gmail.com>
2015-05-07 17:13:31 -0700

    quex 0.65.4
    Closes #39508.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/quex.rb
+++ Library/Formula/quex.rb
@@ -1,21 +1,28 @@
-require 'formula'
-
 class Quex < Formula
-  homepage 'http://quex.org/'
-  url 'https://downloads.sourceforge.net/project/quex/DOWNLOAD/quex-0.64.8.tar.gz'
-  sha1 'efe3d3f5fa0cf9a1130c16d1e0dd4e48cf72011b'
+  homepage "http://quex.org/"
+  url "https://downloads.sourceforge.net/project/quex/DOWNLOAD/quex-0.65.4.tar.gz"
+  sha256 "42245d5795d03ca053741947733660ac4a0d6d54abdd7d2fa9997a9f2bd4ef3f"
+
+  head "http://svn.code.sf.net/p/quex/code/trunk"
 
   def install
-    libexec.install 'demo', 'quex', 'quex-exe.py'
-
+    libexec.install "quex", "quex-exe.py"
+    doc.install "README", "demo"
     # Use a shim script to set QUEX_PATH on the user's behalf
-    (bin+'quex').write <<-EOS.undent
+    (bin+"quex").write <<-EOS.undent
       #!/bin/bash
       QUEX_PATH="#{libexec}" "#{libexec}/quex-exe.py" "$@"
     EOS
+
+    if build.head?
+      man1.install "doc/manpage/quex.1"
+    else
+      man1.install "manpage/quex.1"
+    end
   end
 
   test do
-    system "#{bin}/quex", "--help"
+    system bin/"quex", "-i", doc/"demo/C/000/simple.qx", "-o", "tiny_lexer"
+    File.exist? "tiny_lexer"
   end
 end
