6946e47444b44d2f43a12ff25566ca3a8f6351e2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-08 02:15:33 +0100

    quex: add 0.65.4 bottle.

--- Library/Formula/quex.rb
+++ Library/Formula/quex.rb
@@ -5,6 +5,13 @@ class Quex < Formula
 
   head "http://svn.code.sf.net/p/quex/code/trunk"
 
+  bottle do
+    cellar :any
+    sha256 "78981cf57de1b86adf12c2a7605aad9a8080f8824fe26281f3981f1bec48a978" => :yosemite
+    sha256 "1a90be64b27e77441ce719ab44b3dc2aca5adf392f245056cd9ec32dabec50e2" => :mavericks
+    sha256 "097c4f5030b4163f1c6544eba69eed8b00982a3c26e27d67702b5ff2e016da6e" => :mountain_lion
+  end
+
   def install
     libexec.install "quex", "quex-exe.py"
     doc.install "README", "demo"
