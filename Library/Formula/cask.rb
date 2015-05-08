53ca65fe767275f73907bc311abeca99e6fd5c2d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-08 09:35:10 +0100

    cask: add 0.7.2 bottle.

--- Library/Formula/cask.rb
+++ Library/Formula/cask.rb
@@ -4,6 +4,13 @@ class Cask < Formula
   sha256 "5c8804933dd395ec79e957c96179bf6ac20af24066928685a713e54f44107a2c"
   head "https://github.com/cask/cask.git"
 
+  bottle do
+    cellar :any
+    sha256 "68b9e9f496dabaf85bdbef1414679bb5cbd5531383db02ab625d7bab454b6a78" => :yosemite
+    sha256 "b8bb1e95119383cb7fa3e22eea1d73cafd77cadcc8fff32b22414115b24faabc" => :mavericks
+    sha256 "2a9c3376bc81daa443d7b9a10043e871f7439eb8d11ae2523b18ca0cf11e3832" => :mountain_lion
+  end
+
   depends_on :emacs => "23"
 
   def install
