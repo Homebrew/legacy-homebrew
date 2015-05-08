3cbb1ddd67eda38935b747e2dff3642045fbacd4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-08 09:25:27 +0100

    cppformat: add 1.1.0 bottle.

--- Library/Formula/cppformat.rb
+++ Library/Formula/cppformat.rb
@@ -3,6 +3,13 @@ class Cppformat < Formula
   url "https://github.com/cppformat/cppformat/releases/download/1.1.0/cppformat-1.1.0.zip"
   sha256 "bfa5db9d5bafe079b711981c336ec33b3980715aadf89efc7855aca507845a0e"
 
+  bottle do
+    cellar :any
+    sha256 "69209bdc16296c7d84ca4e1484f64384c7e880715d338be69e792fe3e55976d6" => :yosemite
+    sha256 "caef34333b39765174c968ff3a7bbdf303894a9a402b172b4035338b7ca88c0c" => :mavericks
+    sha256 "b2880f33158da37e8a1b064e2bd30edb6c091cff42b6c126fb1b0627f682850f" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
