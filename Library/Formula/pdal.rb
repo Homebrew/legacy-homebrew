1a873336e4bbe6e2e37f11ac5dbc2a489368aed0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-08 04:27:19 +0100

    pdal: add 0.9.9 bottle.

--- Library/Formula/pdal.rb
+++ Library/Formula/pdal.rb
@@ -5,6 +5,12 @@ class Pdal < Formula
 
   head "https://github.com/PDAL/PDAL.git"
 
+  bottle do
+    sha256 "415bd8c342d4a50803245555cafa1d3fd854135fcf7c585ce0b511e292416179" => :yosemite
+    sha256 "a3ad47e4bcfbfd71f18f2677e02ac415c19a5b5f64e906200d41caba3f43e9dd" => :mavericks
+    sha256 "2608de95a5dd377421873b08a72ad011bcd0309b43dda673409f2e9389817453" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "gdal"
 
