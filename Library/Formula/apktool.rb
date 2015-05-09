f28d5a47c1ff22434275b3b4b2868deb9d0ec1dd
Jin, Heonkyu <heonkyu.jin@gmail.com>
2015-05-09 16:40:28 +0900

    Fixes typo; sha1 to sha256

--- Library/Formula/apktool.rb
+++ Library/Formula/apktool.rb
@@ -1,7 +1,7 @@
 class Apktool < Formula
   homepage "https://github.com/iBotPeaches/Apktool"
   url "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.0.jar", :using => :nounzip
-  sha1 "0dd25996d8e23d8efcca0872dab6498af6f97c5e6cdef10005f5d82a7636f57d"
+  sha256 "0dd25996d8e23d8efcca0872dab6498af6f97c5e6cdef10005f5d82a7636f57d"
 
   def install
     libexec.install "apktool_#{version}.jar"
