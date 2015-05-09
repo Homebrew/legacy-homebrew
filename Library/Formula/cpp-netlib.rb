0abfcfd6bf75870efeacea8d1e1bfbfab22321ec
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-10 00:58:56 +0100

    cpp-netlib: fix version, unbottle

--- Library/Formula/cpp-netlib.rb
+++ Library/Formula/cpp-netlib.rb
@@ -2,12 +2,7 @@ class CppNetlib < Formula
   homepage "http://cpp-netlib.org"
   url "https://storage.googleapis.com/cpp-netlib-downloads/0.11.1/cpp-netlib-0.11.1-final.tar.bz2"
   sha256 "8f5a0bb7e5940490b4e409f9c805b752ee4b598e06d740d04adfc76eb5c8e23e"
-
-  bottle do
-    sha1 "799f0fc4ecf5b9c266f429d700095c4439e1ff87" => :yosemite
-    sha1 "cdadc6e7d4c4f721c694efaed258affc9c93b86f" => :mavericks
-    sha1 "4d839cdf6ed3b92a5e4c6c1738ff058b979c4e21" => :mountain_lion
-  end
+  version "0.11.1"
 
   depends_on "cmake" => :build
 
