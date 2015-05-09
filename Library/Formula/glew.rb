3e187707c781ea629909aa669ca6941a5a3f710b
Anatoliy Bulukin <bulukin@gmail.com>
2015-05-10 00:18:02 +0300

    glew 1.12.0
    update to glew 1.12.0

--- Library/Formula/glew.rb
+++ Library/Formula/glew.rb
@@ -1,17 +1,7 @@
-require "formula"
-
 class Glew < Formula
   homepage "http://glew.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/glew/glew/1.11.0/glew-1.11.0.tgz"
-  sha1 "9bb5c87c055acd122a4956112bbb18ee72c38e5c"
-
-  bottle do
-    cellar :any
-    revision 2
-    sha1 "0f140259d5cb5525153b32102220432fefba1bee" => :yosemite
-    sha1 "ab31a942adaf43f20cea1fd39d1a04949615c2de" => :mavericks
-    sha1 "0ea8a4b4ec385c39eb52732ce8527f68410e35da" => :mountain_lion
-  end
+  url "https://downloads.sourceforge.net/project/glew/glew/1.12.0/glew-1.12.0.tgz"
+  sha256 "af58103f4824b443e7fa4ed3af593b8edac6f3a7be3b30911edbc7344f48e4bf"
 
   option :universal
 
@@ -29,5 +19,9 @@ class Glew < Formula
     inreplace "glew.pc.in", "Requires: @requireslib@", ""
     system "make", "GLEW_PREFIX=#{prefix}", "GLEW_DEST=#{prefix}", "all"
     system "make", "GLEW_PREFIX=#{prefix}", "GLEW_DEST=#{prefix}", "install.all"
+  end
+
+  test do
+    assert_equal "GLEW version 1.12.0", shell_output("#{bin}/glewinfo | grep \"GLEW version\"").strip
   end
 end
