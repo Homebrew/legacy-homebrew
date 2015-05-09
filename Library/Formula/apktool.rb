6ed18bd94ae864f37194cdf01c51d8c051a54750
Jin, Heonkyu <heonkyu.jin@gmail.com>
2015-05-09 16:37:09 +0900

    Updates to SHA-256 and changes single quotes to double quotes

--- Library/Formula/apktool.rb
+++ Library/Formula/apktool.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Apktool < Formula
-  homepage 'https://github.com/iBotPeaches/Apktool'
-  url 'https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.0.jar', :using => :nounzip
-  sha1 '577e351e5b06986c7e099ec33944934f3b9e31e1'
+  homepage "https://github.com/iBotPeaches/Apktool"
+  url "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.0.jar", :using => :nounzip
+  sha1 "0dd25996d8e23d8efcca0872dab6498af6f97c5e6cdef10005f5d82a7636f57d"
 
   def install
     libexec.install "apktool_#{version}.jar"
-    bin.write_jar_script libexec/"apktool_#{version}.jar", 'apktool'
+    bin.write_jar_script libexec/"apktool_#{version}.jar", "apktool"
   end
 end
