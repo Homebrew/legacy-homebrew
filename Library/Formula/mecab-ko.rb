58b107fe583d724c41f2d037d72881ac914576c2
Jack Nagel <jacknagel@gmail.com>
2015-05-09 13:55:15 -0400

    mecab-ko: use opt_prefix

--- Library/Formula/mecab-ko.rb
+++ Library/Formula/mecab-ko.rb
@@ -6,15 +6,16 @@ class MecabKo < Formula
   version "0.996-ko-0.9.2"
   sha1 "4947620429dea49a69be84efc17736c4875c7180"
 
-  # https://bitbucket.org/eunjeon/mecab-ko/pull-request/1/mecab-ko-ipadic-ipadic/diff
-  # Upstream decided not to comment out the dicdir path but replaced
-  # 'ipadic' with 'mecab-ko-dic' instead in mecabrc file.
-  # Though the change resolves the error of mecab-ko-dic path by source
-  # installation, it still doesn't fit with Homebrew as it's expected
-  # installed under /usr/local/ with mecab-ko-dic regardless of version.
-
   def install
-    inreplace "mecabrc.in", "@prefix@/lib/mecab/dic/mecab-ko-dic", "#{HOMEBREW_PREFIX}/opt/mecab-ko-dic"
+    # https://bitbucket.org/eunjeon/mecab-ko/pull-request/1/mecab-ko-ipadic-ipadic/diff
+    # Upstream decided not to comment out the dicdir path but replaced 'ipadic'
+    # with 'mecab-ko-dic' instead in mecabrc file. Though the change resolves
+    # the error of mecab-ko-dic path by source installation, it still doesn't
+    # fit with Homebrew as it's expected installed under /usr/local/ with
+    # mecab-ko-dic regardless of version.
+    inreplace "mecabrc.in",
+      "@prefix@/lib/mecab/dic/mecab-ko-dic",
+      Formula["mecab-ko-dic"].opt_prefix
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
