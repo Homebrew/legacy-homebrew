78dc9c82e3a45bbe880ac368ac1b45a424fabd2f
Alex Dunn <dunn.alex@gmail.com>
2015-05-08 23:37:30 -0700

    rt-audio 4.1.1
    Closes #39546.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/rt-audio.rb
+++ Library/Formula/rt-audio.rb
@@ -1,15 +1,26 @@
-require 'formula'
-
 class RtAudio < Formula
-  homepage 'http://www.music.mcgill.ca/~gary/rtaudio/'
-  url 'http://www.music.mcgill.ca/~gary/rtaudio/release/rtaudio-4.0.12.tar.gz'
-  sha1 'a9a4783183e47f07cff0c62d3882ee95b4eefc4d'
+  homepage "https://www.music.mcgill.ca/~gary/rtaudio/"
+  url "https://www.music.mcgill.ca/~gary/rtaudio/release/rtaudio-4.1.1.tar.gz"
+  sha256 "e279e01243ffddc0ab1640ed2f580b48c2a79ffab3e21c64845f586a9a772598"
+
+  head "https://github.com/thestk/rtaudio.git"
+
+  depends_on "cmake" => :build
 
   def install
-    ENV.j1 # makefile isn't parallel-safe
-    system "./configure"
-    system "make"
-    lib.install "librtaudio.a"
-    include.install "RtAudio.h", "RtError.h"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      lib.install "librtaudio_static.a", "librtaudio.dylib"
+    end
+    include.install "RtAudio.h", Dir["include/*"]
+    prefix.install "contrib", "tests"
+    doc.install Dir["doc/*"]
+  end
+
+  test do
+    system ENV.cxx, "-I#{include}", "-L#{lib}", "-lrtaudio",
+           prefix/"tests/testall.cpp", "-o", "test"
+    system "./test"
   end
 end
