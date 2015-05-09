0f42e3703d41bf2be5440a5d0c8ad26dadc30f71
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-09 22:31:40 +0100

    rt-audio: add 4.1.1 bottle.

--- Library/Formula/rt-audio.rb
+++ Library/Formula/rt-audio.rb
@@ -5,6 +5,13 @@ class RtAudio < Formula
 
   head "https://github.com/thestk/rtaudio.git"
 
+  bottle do
+    cellar :any
+    sha256 "1ad4fce27b7f11e88bc3d4d958d9f9a9add117dc5ad8fb0a225edcde89b6bce6" => :yosemite
+    sha256 "2ac64592ba5e1c9f5dacd5774d25fea7f968cb632ae3ea48e8d3f8c95fad1420" => :mavericks
+    sha256 "32fb53538dceba914cb8369e25a8d74c6ebf115f237a11dadb618e4339653a32" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
