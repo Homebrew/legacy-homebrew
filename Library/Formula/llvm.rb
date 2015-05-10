e09a5cef3cb45e370bcb6d302b218b4f3847d1e2
Sebastian Blessing <sebastian@causality.io>
2015-05-10 21:27:20 +0200

    use llvm.rb from upstream master

--- Library/Formula/llvm.rb
+++ Library/Formula/llvm.rb
@@ -61,6 +61,9 @@ class Llvm < Formula
     end
   end
 
+  # Use absolute paths for shared library IDs
+  patch :DATA
+
   option :universal
   option "with-clang", "Build Clang support library"
   option "with-lld", "Build LLD linker"
@@ -153,3 +156,23 @@ class Llvm < Formula
     EOS
   end
 end
+
+__END__
+diff --git a/Makefile.rules b/Makefile.rules
+index ebebc0a..b0bb378 100644
+--- a/Makefile.rules
++++ b/Makefile.rules
+@@ -599,7 +599,12 @@ ifneq ($(HOST_OS), $(filter $(HOST_OS), Cygwin MingW))
+ ifneq ($(HOST_OS),Darwin)
+   LD.Flags += $(RPATH) -Wl,'$$ORIGIN'
+ else
+-  LD.Flags += -Wl,-install_name  -Wl,"@rpath/lib$(LIBRARYNAME)$(SHLIBEXT)"
++  LD.Flags += -Wl,-install_name
++  ifdef LOADABLE_MODULE
++    LD.Flags += -Wl,"$(PROJ_libdir)/$(LIBRARYNAME)$(SHLIBEXT)"
++  else
++    LD.Flags += -Wl,"$(PROJ_libdir)/$(SharedPrefix)$(LIBRARYNAME)$(SHLIBEXT)"
++  endif
+ endif
+ endif
+ endif
