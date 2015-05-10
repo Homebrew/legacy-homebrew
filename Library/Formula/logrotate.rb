b8be35ac5708e1f46565c5d555327d66870a63f4
Guo Xiao <guoxiao08@gmail.com>
2015-05-10 16:02:33 +0800

    logrotate 3.9.1
    Closes #39579.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/logrotate.rb
+++ Library/Formula/logrotate.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class Logrotate < Formula
-  homepage 'http://packages.debian.org/testing/admin/logrotate'
-  url 'https://fedorahosted.org/releases/l/o/logrotate/logrotate-3.8.3.tar.gz'
-  sha1 '19d70e2cfb97c1cee32e0d709da990856311022a'
+  homepage "https://fedorahosted.org/logrotate/"
+  url "https://fedorahosted.org/releases/l/o/logrotate/logrotate-3.9.1.tar.gz"
+  sha256 "022769e3288c80981559a8421703c88e8438b447235e36dd3c8e97cd94c52545"
 
-  depends_on 'popt'
+  depends_on "popt"
 
   # Per MacPorts, let these variables be overridden by ENV vars.
   # Also, use HOMEBREW suggested locations for run and log files.
@@ -19,17 +17,17 @@ class Logrotate < Formula
     ENV["STATEFILE"] = "#{var}/lib/logrotate.status"
 
     system "make"
-    sbin.install 'logrotate'
-    man8.install 'logrotate.8'
-    man5.install 'logrotate.conf.5'
+    sbin.install "logrotate"
+    man8.install "logrotate.8"
+    man5.install "logrotate.conf.5"
 
     mv "examples/logrotate-default", "logrotate.conf"
     inreplace "logrotate.conf" do |s|
       s.gsub! "/etc/logrotate.d", "#{etc}/logrotate.d"
     end
 
-    etc.install 'logrotate.conf'
-    (etc/'logrotate.d').mkpath
+    etc.install "logrotate.conf"
+    (etc/"logrotate.d").mkpath
   end
 
   def plist; <<-EOS.undent
@@ -60,11 +58,11 @@ class Logrotate < Formula
 end
 
 __END__
-diff --git a/Makefile b/Makefile
-index d65a3f3..14d1f3f 100644
---- a/Makefile
-+++ b/Makefile
-@@ -76,6 +76,22 @@ ifneq ($(POPT_DIR),)
+diff --git i/Makefile w/Makefile
+index 5a42fb6..e53f4f7 100644
+--- i/Makefile
++++ w/Makefile
+@@ -86,6 +86,22 @@ ifneq ($(POPT_DIR),)
      LOADLIBES += -L$(POPT_DIR)
  endif
  
@@ -87,24 +85,23 @@ index d65a3f3..14d1f3f 100644
  ifneq ($(STATEFILE),)
      CFLAGS += -DSTATEFILE=\"$(STATEFILE)\"
  endif
-
-diff --git a/examples/logrotate-default b/examples/logrotate-default
-index 7da6bb7..0b27baf 100644
---- a/examples/logrotate-default
-+++ b/examples/logrotate-default
-@@ -14,22 +14,5 @@ dateext
+diff --git i/examples/logrotate-default w/examples/logrotate-default
+index 56e9103..c61a33a 100644
+--- i/examples/logrotate-default
++++ w/examples/logrotate-default
+@@ -14,22 +14,7 @@ dateext
  # uncomment this if you want your log files compressed
  #compress
-
+ 
 -# RPM packages drop log rotation information into this directory
-+# homebrew packages drop log rotation information into this directory
++# Homebrew packages drop log rotation information into this directory
  include /etc/logrotate.d
--
+ 
 -# no packages own wtmp and btmp -- we'll rotate them here
 -/var/log/wtmp {
 -    monthly
 -    create 0664 root utmp
--	minsize 1M
+-    minsize 1M
 -    rotate 1
 -}
 -
@@ -115,4 +112,4 @@ index 7da6bb7..0b27baf 100644
 -    rotate 1
 -}
 -
--# system-specific logs may be also be configured here.
+ # system-specific logs may be also be configured here.
