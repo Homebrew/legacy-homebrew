8f758d7ca2cf284cd22ce64e02a13e4798359da7
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-10 15:53:24 +0200

    pam_yubico 2.19, add test

--- Library/Formula/pam_yubico.rb
+++ Library/Formula/pam_yubico.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class PamYubico < Formula
-  homepage "http://opensource.yubico.com/yubico-pam/"
-  url "https://github.com/Yubico/yubico-pam/archive/2.16.tar.gz"
-  sha1 "e0724a433a2c84b303fdb28efdda023349a27193"
+  homepage "https://developers.yubico.com/yubico-pam/"
+  url "https://github.com/Yubico/yubico-pam/archive/2.19.tar.gz"
+  sha256 "64900586555adadd515189a6f58aaf3b12c5fb0012d030a94ce4d7c1747c702c"
 
   option :universal
 
@@ -11,6 +9,7 @@ class PamYubico < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "asciidoc" => :build
   depends_on "libyubikey"
   depends_on "ykclient"
   depends_on "ykpers"
@@ -18,10 +17,17 @@ class PamYubico < Formula
   def install
     ENV.universal_binary if build.universal?
 
+    ENV["XML_CATALOG_FILES"] = HOMEBREW_PREFIX/"etc/xml/catalog"
+
     system "autoreconf", "-fvi"
     system "./configure", "--prefix=#{prefix}",
                           "--with-libyubikey-prefix=#{Formula["libyubikey"].opt_prefix}",
                           "--with-libykclient-prefix=#{Formula["ykclient"].opt_prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    # Not much more to test without an actual yubikey device.
+    system "#{bin}/ykpamcfg", "-V"
   end
 end
