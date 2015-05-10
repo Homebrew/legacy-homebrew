46b9bb33e855143fd42a2a457039b0a55a0e2474
Fil <fil@rezo.net>
2015-05-10 11:24:53 +0200

    tika 1.8
    cc: @robsonpeixoto @anjackson

--- Library/Formula/tika.rb
+++ Library/Formula/tika.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Tika < Formula
   homepage 'http://tika.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.7.jar'
-  sha1 'd9516b1964be8775edbe0d6d167234c2967fea7a'
+  url 'http://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.8.jar'
+  sha1 '0acf5fd2fd7ff410120d6d698b3c762d8bea83f1'
 
   resource 'server' do
-    url 'http://repo1.maven.org/maven2/org/apache/tika/tika-server/1.7/tika-server-1.7.jar'
-    sha1 'b99f9ee4c5b9bfcfc3411171f67e2dcaec13dcef'
+    url 'http://repo1.maven.org/maven2/org/apache/tika/tika-server/1.8/tika-server-1.8.jar'
+    sha1 '9b5c5550ff57433796dd9d8807ffb8f2f102bc91'
   end
 
   def install
