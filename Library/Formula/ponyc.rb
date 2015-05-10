638aa4d45960c62f707f8665c2eb7c0a22e7f86a
Sebastian Blessing <sebastian@causality.io>
2015-05-11 00:08:49 +0200

    ensure ponyc.rb passes brew audit

--- Library/Formula/ponyc.rb
+++ Library/Formula/ponyc.rb
@@ -1,7 +1,7 @@
 class Ponyc < Formula
   homepage "http://www.ponylang.org"
   url "http://www.ponylang.org/releases/source/ponyc-0.1.3.tar.bz2"
-  sha256 "bdf6af248362bcb4b9d3869eca5b17b35ef1212786cc93a10efaace516bb6b3e" 
+  sha256 "bdf6af248362bcb4b9d3869eca5b17b35ef1212786cc93a10efaace516bb6b3e"
 
   depends_on "llvm" => ["with-rtti"]
 
