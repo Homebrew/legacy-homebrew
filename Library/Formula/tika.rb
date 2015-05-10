63be8e78eec2551a18097a6379b490706c65ee16
Fil <fil@rezo.net>
2015-05-10 21:47:41 +0200

    jenkins requires https for apache.org

--- Library/Formula/tika.rb
+++ Library/Formula/tika.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Tika < Formula
   homepage 'http://tika.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.8.jar'
+  url 'https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.8.jar'
   sha256 '9346cf68c00a46b2e6189794d5fb2e127bf9b60ef6d216edf06e01917f7deaef'
 
   resource 'server' do
