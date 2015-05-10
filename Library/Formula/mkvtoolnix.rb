bb4e6d90141d7fee3b1a40d72e33d32a2a61c428
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-10 17:37:28 +0100

    mkvtoolnix: update 7.9.0 bottle.

--- Library/Formula/mkvtoolnix.rb
+++ Library/Formula/mkvtoolnix.rb
@@ -4,9 +4,9 @@ class Mkvtoolnix < Formula
   sha256 "39788fa57d9cebd6ea3be9db58dbf8a10fd7c96ad8fa4f79bdf4dadca77bba4a"
 
   bottle do
-    sha1 "c0aa6d1f587159dfcf89f0f193c1fc97ae8554da" => :yosemite
-    sha1 "022ff93c8ee34b6958ccbcba2aa96e82e8b5410e" => :mavericks
-    sha1 "1ded8d87b52b495e9a94e0cfab5e4623532ff98a" => :mountain_lion
+    sha256 "0d2d3e8b470314dac1b2e109147e50e3b26080e5960fbd6a528723f096424b34" => :yosemite
+    sha256 "9388d3316b8a2c09cb79d1e5cd8c52053ee3748c7db97b7e33ecd94da260c78c" => :mavericks
+    sha256 "8ea97bdf68399e4aa92c923e3c9149a80438f133c6be948ba15d7a97fea4b80c" => :mountain_lion
   end
 
   head do
