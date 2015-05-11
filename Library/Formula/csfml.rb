f92e9d25bebfe8644d9672fd3982ff1a00fca35e
Anatoliy Bulukin <bulukin@gmail.com>
2015-05-11 14:41:16 +0300

    csfml bumping revisions

--- Library/Formula/csfml.rb
+++ Library/Formula/csfml.rb
@@ -5,13 +5,6 @@ class Csfml < Formula
   url "http://www.sfml-dev.org/download/csfml/CSFML-2.2-sources.zip"
   sha1 "1dc3d2dadef6e6daa05b0dd868367ad5f02419e4"
 
-  bottle do
-    cellar :any
-    sha256 "4cc1bc694356a8206e1164cc266ef68b5c45e7a1ee60d0e210c4a8e49c6abb97" => :yosemite
-    sha256 "d1d8e0343fe42fd91029a770cdf912587fbf7388ea997d68ec4f96f33548cb77" => :mavericks
-    sha256 "3ecb8338214a6c0485fe294af23c4ec8440e35878ef0521a4fe6d6da792e6c77" => :mountain_lion
-  end
-
   depends_on "cmake" => :build
   depends_on "sfml"
 
