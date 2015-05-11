68cba206971d089a749c32f281c488e981ee2933
Baptiste Fontaine <batifon@yahoo.fr>
2015-05-12 00:26:31 +0200

    ezlupdate 2015.01.2
    Closes #39659.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/ezlupdate.rb
+++ Library/Formula/ezlupdate.rb
@@ -1,11 +1,11 @@
-require 'formula'
-
 class Ezlupdate < Formula
-  homepage 'http://ezpedia.org/ez/ezlupdate'
-  url 'https://github.com/downloads/ezsystems/ezpublish-legacy/ezpublish_community_project-2011.10-with_ezc.tar.bz2'
-  sha1 'd20912fda2438143d3981ca8e07bb52329e2cf25'
+  homepage "http://ezpedia.org/ez/ezlupdate"
+  url "https://github.com/ezsystems/ezpublish-legacy/archive/v2015.01.2.tar.gz"
+  sha256 "17ce825bc4b85a1c84f7d23b1f8ff0099c0272038be360aca243dda112514f6c"
 
-  depends_on 'qt'
+  head "https://github.com/ezsystems/ezpublish-legacy.git"
+
+  depends_on "qt"
 
   def install
     cd "support/ezlupdate-qt4.5/ezlupdate" do
@@ -14,6 +14,11 @@ class Ezlupdate < Formula
       system "#{HOMEBREW_PREFIX}/bin/qmake", "ezlupdate.pro"
       system "make"
     end
-    bin.install 'bin/macosx/ezlupdate'
+    bin.install "bin/macosx/ezlupdate"
+  end
+
+  test do
+    (testpath/"share/translation").mkpath
+    system "#{bin}/ezlupdate", "-v", "eng-GB"
   end
 end
