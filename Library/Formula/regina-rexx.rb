137d13f3fef0e2d6f5b389f9841ad8d34444d996
Alex Dunn <dunn.alex@gmail.com>
2015-05-11 11:38:32 -0700

    regina-rexx 3.9.1
    Closes #39647.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/regina-rexx.rb
+++ Library/Formula/regina-rexx.rb
@@ -1,14 +1,22 @@
-require "formula"
-
 class ReginaRexx < Formula
   homepage "http://regina-rexx.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/regina-rexx/regina-rexx/3.8.2/Regina-REXX-3.8.2.tar.gz"
-  sha1 "aa713faae560c37669b304794af946b59d3ebd7e"
+  url "https://downloads.sourceforge.net/project/regina-rexx/regina-rexx/3.9.1/Regina-REXX-3.9.1.tar.gz"
+  sha256 "5d13df26987e27f25e7779a2efa87a5775213beeda449a9efac59b57a5d5f3ee"
 
   def install
     ENV.j1 # No core usage for you, otherwise race condition = missing files.
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test").write <<-EOS.undent
+      #!#{bin}/regina
+      Parse Version ver
+      Say ver
+    EOS
+    chmod 0755, testpath/"test"
+    assert_match version.to_s, shell_output(testpath/"test")
   end
 end
