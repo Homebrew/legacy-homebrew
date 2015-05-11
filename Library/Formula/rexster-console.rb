d83789641c5a9b6bb5a83feed1a3e334ce1652f5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-11 04:39:52 +0100

    rexster-console: add 2.6.0 bottle.

--- Library/Formula/rexster-console.rb
+++ Library/Formula/rexster-console.rb
@@ -3,6 +3,13 @@ class RexsterConsole < Formula
   url "http://tinkerpop.com/downloads/rexster/rexster-console-2.6.0.zip"
   sha256 "5f3af7bfc95847e8efa28610b23e2c175c6d92c14e5f3a468b9476cb1f2dfe1e"
 
+  bottle do
+    cellar :any
+    sha256 "affe578e75691159a7a850c8b144eaaabc58d8375d7172852069b951ddc88239" => :yosemite
+    sha256 "17254b31620dc42f4ee9c49a7bba38a1506312939dcf8d2a54a16f1a6cafd2e6" => :mavericks
+    sha256 "a9dd91d430d35af266e9298d3bae82445f6cbf0521cb615f5cbc854974b89308" => :mountain_lion
+  end
+
   def install
     libexec.install %w[lib doc]
     (libexec/"ext").mkpath
