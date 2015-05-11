f0c4fcf9216ee9fd8a64b0787078bb55703f40fb
Rodger Combs <rodger.combs@gmail.com>
2015-05-11 18:03:23 -0500

    dcadec 0.1.0 (new formula)
    Closes #46411.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Library/Formula/dcadec.rb
@@ -0,0 +1,25 @@
+class Dcadec < Formula
+  desc "DTS Coherent Acoustics decoder with support for HD extensions"
+  homepage "https://github.com/foo86/dcadec"
+  url "https://github.com/foo86/dcadec.git",
+    :tag => "v0.1.0",
+    :revision => "2449e5d00ad32da3aed7cedfcec5bd475af9f678"
+  head "https://github.com/foo86/dcadec.git"
+
+  resource "sample" do
+    url "https://github.com/foo86/dcadec-samples/raw/fa7dcf8c98c6d/xll_71_24_96_768.dtshd"
+    sha256 "d2911b34183f7379359cf914ee93228796894e0b0f0055e6ee5baefa4fd6a923"
+  end
+
+  def install
+    system "make", "all"
+    system "make", "check"
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    resource("sample").stage do |r|
+      system "#{bin}/dcadec", r.cached_download
+    end
+  end
+end
