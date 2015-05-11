094218af1823b35ea258526d6570e06ce5faf823
Alex Dunn <dunn.alex@gmail.com>
2015-05-10 19:40:54 -0700

    rfcmarkup 1.108
    Closes #39617.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/rfcmarkup.rb
+++ Library/Formula/rfcmarkup.rb
@@ -1,11 +1,13 @@
-require 'formula'
-
 class Rfcmarkup < Formula
-  homepage 'http://tools.ietf.org/tools/rfcmarkup/'
-  url 'http://tools.ietf.org/tools/rfcmarkup/rfcmarkup-1.106.tgz'
-  sha1 '1df2fbaea768ab5390c83c2f6185670a429d0051'
+  homepage "https://tools.ietf.org/tools/rfcmarkup/"
+  url "https://tools.ietf.org/tools/rfcmarkup/rfcmarkup-1.108.tgz"
+  sha256 "77a78a1df5e155acb1df24ced950b9c7af022a71f07ad7eec2d64b9cc8ef5466"
 
   def install
     bin.install "rfcmarkup"
+  end
+
+  test do
+    system bin/"rfcmarkup", "--help"
   end
 end
