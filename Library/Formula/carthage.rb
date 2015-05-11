1420b7bc0bb45316bd6dcd69a41922fbc9821d63
Rob Rix <rob.rix@github.com>
2015-05-11 16:46:17 -0400

    carthage 0.7.2
    Closes #39652.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- Library/Formula/carthage.rb
+++ Library/Formula/carthage.rb
@@ -1,7 +1,7 @@
 class Carthage < Formula
   homepage "https://github.com/Carthage/Carthage"
-  url "https://github.com/Carthage/Carthage.git", :tag => "0.7.1",
-                                                  :revision => "3ac570dd6e0cfc29624c99b4881ac6ddb32290d7",
+  url "https://github.com/Carthage/Carthage.git", :tag => "0.7.2",
+                                                  :revision => "c922316638bae863be697fb930bfef1c6b034ace",
                                                   :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
