0b5c7deb931f12b5d0f9efaae37189765b9462ae
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-05-11 11:33:54 +0200

    json-glib 1.0.4
    version bump
    brew audit --strict compliance
    
    Closes #39581.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/json-glib.rb
+++ Library/Formula/json-glib.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class JsonGlib < Formula
-  homepage "http://live.gnome.org/JsonGlib"
-  url "http://ftp.gnome.org/pub/gnome/sources/json-glib/1.0/json-glib-1.0.2.tar.xz"
-  sha256 "887bd192da8f5edc53b490ec51bf3ffebd958a671f5963e4f3af32c22e35660a"
+  homepage "https://live.gnome.org/JsonGlib"
+  url "http://ftp.gnome.org/pub/gnome/sources/json-glib/1.0/json-glib-1.0.4.tar.xz"
+  sha256 "80f3593cb6bd13f1465828e46a9f740e2e9bd3cd2257889442b3e62bd6de05cd"
 
   bottle do
     revision 2
@@ -21,6 +19,37 @@ class JsonGlib < Formula
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--enable-introspection=yes"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <json-glib/json-glib.h>
+
+      int main(int argc, char *argv[]) {
+        JsonParser *parser = json_parser_new();
+        return 0;
+      }
+    EOS
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/json-glib-1.0
+      -D_REENTRANT
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lintl
+      -ljson-glib-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
   end
 end
