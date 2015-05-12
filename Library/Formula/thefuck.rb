c060cffe5ed762fb77ae9c7f954eef5fa3a49cea
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-12 07:25:07 +0100

    thefuck: update 1.40 bottle.

--- Library/Formula/thefuck.rb
+++ Library/Formula/thefuck.rb
@@ -5,9 +5,9 @@ class Thefuck < Formula
 
   bottle do
     cellar :any
-    sha256 "257c5674dd683812bda279eb9709809440d7cba032862d234523a0e2dfd38d1d" => :yosemite
-    sha256 "b2cfa406696a624e311bed92ef08c314f0fe6c893308d8e67fecc08102da9b32" => :mavericks
-    sha256 "68a2b3f99d193ab817f31e8c7941d0f4574eccb36f9f29884e9d03e608999afe" => :mountain_lion
+    sha256 "890762ad1d9324ab346cab435dd0d89f8820ee43c68046847fb1218aed9fe246" => :yosemite
+    sha256 "8eb332807213eedc8f34e3c60f5305e24d1deb0d1c09907994ca0a05bbb47f66" => :mavericks
+    sha256 "ed2b83dd297c9a8ff578f48157cf7740a17e261d167c0f1243580bd6f8767829" => :mountain_lion
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
