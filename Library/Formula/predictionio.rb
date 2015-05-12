568b6bf306443ec5313397846a607e59db50d18a
Peter Garbers <peter@garbers.me>
2015-05-12 16:00:00 -0400

    PredictionIO 0.9.2
    Closes #39688.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/predictionio.rb
+++ Library/Formula/predictionio.rb
@@ -1,20 +1,18 @@
-require 'formula'
-
 class Predictionio < Formula
-  homepage 'http://prediction.io/'
-  url 'http://download.prediction.io/PredictionIO-0.9.0.tar.gz'
-  sha1 'e5724b637382a06c098bbb3617b35f5bcff1b469'
+  homepage "http://prediction.io/"
+  url "http://download.prediction.io/PredictionIO-0.9.2.tar.gz"
+  sha1 "199d1c9a4b69c3bb009ed7819294183e99ced4e9"
 
-  depends_on 'elasticsearch'
-  depends_on 'hadoop'
-  depends_on 'hbase'
-  depends_on 'apache-spark'
-  depends_on :java => "1.7"
+  depends_on "elasticsearch"
+  depends_on "hadoop"
+  depends_on "hbase"
+  depends_on "apache-spark"
+  depends_on :java => "1.7+"
 
   def install
     rm_f Dir["bin/*.bat"]
 
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.write_exec_script libexec/"bin/pio"
 
     inreplace libexec/"conf/pio-env.sh" do |s|
@@ -22,5 +20,4 @@ class Predictionio < Formula
        s.gsub! /SPARK_HOME=.+$/, "SPARK_HOME=#{Formula['apache-spark'].opt_prefix}"
     end
   end
-
 end
