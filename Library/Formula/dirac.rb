c351c1aafa44ec8fb011756bcae0e7bf5f23392a
Larry Gilbert <larry@l2g.to>
2015-05-12 12:01:12 -0700

    dirac: edits for brew audit --strict

--- Library/Formula/dirac.rb
+++ Library/Formula/dirac.rb
@@ -1,20 +1,18 @@
-require 'formula'
-
 class Dirac < Formula
-  homepage 'http://diracvideo.org/'
-  url 'http://diracvideo.org/download/dirac-research/dirac-1.0.2.tar.gz'
-  sha1 '895aaad832a54b754e58f77c87d38c0c37752b0b'
+  homepage "http://diracvideo.org/"
+  url "http://diracvideo.org/download/dirac-research/dirac-1.0.2.tar.gz"
+  sha256 "816b16f18d235ff8ccd40d95fc5b4fad61ae47583e86607932929d70bf1f00fd"
 
   fails_with :llvm do
     build 2334
   end
 
   def install
-    # BSD cp doesn't have '-d'
-    inreplace 'doc/Makefile.in', 'cp -dR', 'cp -R'
+    # BSD cp doesn't have "-d"
+    inreplace "doc/Makefile.in", "cp -dR", "cp -R"
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
