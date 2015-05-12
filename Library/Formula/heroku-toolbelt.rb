83838fc97f5e4cfd7869a1ef8e6adf23d751b10d
Xu Cheng <xucheng@me.com>
2015-05-12 18:34:14 +0800

    heroku: unbottle
    There's no compiling so the bottle is unnecessary. At the same time,
    bottle will make ruby becoming the hard dependency.
    
    Closes #39649.

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -4,13 +4,6 @@ class HerokuToolbelt < Formula
   sha256 "b57a991828663e3587e07a66b796bf56bf8f4610a618241195bd760a4cf29319"
   head "https://github.com/heroku/heroku.git"
 
-  bottle do
-    cellar :any
-    sha256 "f5ca85f9fa06000417981d6b7e0442eeea5b9d43ac0efe0be1a66803943b960d" => :yosemite
-    sha256 "b4c3d2200b49e798b507e2d39b062da20b951aee637484ddf65a7d98261118e5" => :mavericks
-    sha256 "3dc678678593f75e0ac0ba8862004e1fa9e857b2a39dfc46d4cca230c53f95fe" => :mountain_lion
-  end
-
   depends_on :ruby => "1.9"
 
   def install
