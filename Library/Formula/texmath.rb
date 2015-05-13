61f3818cb8fd43f323518464e8a68dd5c3e65e14
Geoff Nixon <geoff@geoff.codes>
2015-05-13 04:09:16 -0700

    texmath: remove gmp dependency

--- Library/Formula/texmath.rb
+++ Library/Formula/texmath.rb
@@ -7,6 +7,8 @@ class Texmath < Formula
   url "https://hackage.haskell.org/package/texmath-0.8.0.2/texmath-0.8.0.2.tar.gz"
   sha256 "47b9c3fdceed63c5d63987db7e511a38ea8ddf8591786ef56efea734a3c31f86"
 
+  revision 1
+
   bottle do
     sha256 "2b95621346d6c76d000f6830efc0104eb6c6cacc4b256b30418e4d7a67290fb0" => :yosemite
     sha256 "a24734d011de31db7494f97bcb019116053867ca630e1c13a9063bea30af38e0" => :mavericks
@@ -15,9 +17,6 @@ class Texmath < Formula
 
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
-  depends_on "gmp"
-
-  fails_with(:clang) { build 425 } # clang segfaults on Lion
 
   def install
     cabal_sandbox do
