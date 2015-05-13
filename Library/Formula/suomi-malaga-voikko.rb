87345206d58d2c23b5e625901d98ab48746d27a6
Alex Dunn <dunn.alex@gmail.com>
2015-05-12 23:15:07 -0700

    suomi-malaga-voikko 1.18
    Closes #39705.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/suomi-malaga-voikko.rb
+++ Library/Formula/suomi-malaga-voikko.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class SuomiMalagaVoikko < Formula
   homepage "http://voikko.puimula.org/"
-  url "http://www.puimula.org/voikko-sources/suomi-malaga/suomi-malaga-1.17.tar.gz"
-  sha1 "e3d3947b06f3ac2b13eaa2236c328b788c7d6bb8"
+  url "http://www.puimula.org/voikko-sources/suomi-malaga/suomi-malaga-1.18.tar.gz"
+  sha256 "83655d56aa8255d8926ad3bafa190b8d7da32a0e3ff12150dc2dac31c92c5b0d"
+
+  head "https://github.com/voikko/corevoikko.git"
 
   bottle do
     cellar :any
@@ -15,6 +15,7 @@ class SuomiMalagaVoikko < Formula
   depends_on "malaga"
 
   def install
+    Dir.chdir "suomimalaga" if build.head?
     system "make", "voikko"
     system "make", "voikko-install", "DESTDIR=#{lib}/voikko"
   end
