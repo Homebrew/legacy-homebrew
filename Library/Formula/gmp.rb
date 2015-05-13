6a51663cf61c35c2df74499195ac8056293a1bb3
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-13 21:33:21 +0200

    gmp: use SSL/TLS mirror and homepage
    Closes #39728.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gmp.rb
+++ Library/Formula/gmp.rb
@@ -1,7 +1,7 @@
 class Gmp < Formula
-  homepage "http://gmplib.org/"
+  homepage "https://gmplib.org/"
   url "http://ftpmirror.gnu.org/gmp/gmp-6.0.0a.tar.bz2"
-  mirror "ftp://ftp.gmplib.org/pub/gmp/gmp-6.0.0a.tar.bz2"
+  mirror "https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2"
   mirror "https://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.bz2"
   sha256 "7f8e9a804b9c6d07164cf754207be838ece1219425d64e28cfa3e70d5c759aaf"
 
