849d0a53600a29fbb6d145031382d9e37ac3d992
Geoff Nixon <geoff@geoff.codes>
2015-05-13 04:08:25 -0700

    shellcheck: remove gmp dependency

--- Library/Formula/shellcheck.rb
+++ Library/Formula/shellcheck.rb
@@ -7,6 +7,8 @@ class Shellcheck < Formula
   url "https://github.com/koalaman/shellcheck/archive/v0.3.7.tar.gz"
   sha256 "9f421052bc07047b65854544bfe32c5503cdad09f00d7a63a2f28b09b03a08f6"
 
+  revision 1
+
   bottle do
     sha256 "10b7f52f1bd4224ef1bb45faa45e59b3f0b022746a20d90be1e231bfcc5ee4c0" => :yosemite
     sha256 "04b58769d3ec3048f97d3fff1d211e0bb7fe6c461c262c328c8dfeb20a38cb60" => :mavericks
@@ -16,7 +18,6 @@ class Shellcheck < Formula
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
   depends_on "pandoc" => :build
-  depends_on "gmp"
 
   def install
     install_cabal_package
