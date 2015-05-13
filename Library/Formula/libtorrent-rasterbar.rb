d9635c6d7bdfb6281e0b35924ba52931d3a0c650
David Christenson <david@the-david.com>
2015-05-13 10:38:39 -0600

    libtorrent-rasterbar 1.0.5
    Update to latest stable release, modernize formula.
    
    Closes #39720.
    
    Signed-off-by: Brett Koonce <koonce@gmail.com>

--- Library/Formula/libtorrent-rasterbar.rb
+++ Library/Formula/libtorrent-rasterbar.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class LibtorrentRasterbar < Formula
   homepage "http://sourceforge.net/projects/libtorrent/"
-  url "https://downloads.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-1.0.4.tar.gz"
-  sha256 "1f567823133b1493b9717afc8334eed691bf0ab452d4a2e0f644989f13ce9db0"
+  url "https://downloads.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-1.0.5.tar.gz"
+  sha256 "474a43da2147bec7e333c10f70b07221f4732dd67e071d5e90acc7edb8f657b0"
 
   head do
     url "https://libtorrent.googlecode.com/svn/trunk"
@@ -27,12 +25,12 @@ class LibtorrentRasterbar < Formula
   depends_on "boost-python" if build.with? "python"
 
   def install
-    args = [ "--disable-debug",
-             "--disable-dependency-tracking",
-             "--disable-silent-rules",
-             "--enable-encryption",
-             "--prefix=#{prefix}",
-             "--with-boost=#{Formula["boost"].opt_prefix}" ]
+    args = ["--disable-debug",
+            "--disable-dependency-tracking",
+            "--disable-silent-rules",
+            "--enable-encryption",
+            "--prefix=#{prefix}",
+            "--with-boost=#{Formula["boost"].opt_prefix}"]
 
     # Build python bindings requires forcing usage of the mt version of boost_python.
     if build.with? "python"
