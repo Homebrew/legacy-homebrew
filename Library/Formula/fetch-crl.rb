b51fc8b9b72e7ea3eae146e1a3ec62d15fb71c51
Manuel Giffels <giffels@gmail.com>
2015-05-13 14:46:40 +0200

    fetch-crl 3.0.16
    New version fetch-crl 3.0.16
    
    Closes #39711.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/fetch-crl.rb
+++ Library/Formula/fetch-crl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class FetchCrl < Formula
   homepage 'http://wiki.nikhef.nl/grid/FetchCRL3'
-  url 'https://dist.eugridpma.info/distribution/util/fetch-crl3/fetch-crl-3.0.13.tar.gz'
-  sha1 'bb1de570e8c4b977a8ec623c808df80cd10a3d8f'
+  url 'https://dist.eugridpma.info/distribution/util/fetch-crl3/fetch-crl-3.0.16.tar.gz'
+  sha1 '3c89e751bc055492a5975fcf11d7ce5ca3309489'
 
   def install
     system "make", "install", "PREFIX=#{prefix}", "ETC=#{etc}", "CACHE=#{var}/cache"
