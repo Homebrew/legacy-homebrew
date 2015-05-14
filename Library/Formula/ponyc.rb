3a4fcddfeeb9aaebb67508b2de42c7e073b239d1
Sebastian Blessing <sebastian@causality.io>
2015-05-14 14:41:09 +0200

    improved test section for ponyc formula and adopted it to the llvm formula change

--- Library/Formula/ponyc.rb
+++ Library/Formula/ponyc.rb
@@ -3,13 +3,25 @@ class Ponyc < Formula
   url "http://www.ponylang.org/releases/source/ponyc-0.1.3.tar.bz2"
   sha256 "bdf6af248362bcb4b9d3869eca5b17b35ef1212786cc93a10efaace516bb6b3e"
 
-  depends_on "llvm" => ["with-rtti"]
+  depends_on "llvm" => ["with-rtti", "without-shared"]
 
   def install
-    system "make", "install", "prefix=#{prefix}"
+    system "make", "install", "destdir=#{prefix}"
   end
 
   test do
-    system "#{bin}/ponyc", "--version"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/builtin"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/collections"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/encode/base64"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/files"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/math"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/net/http"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/net/ssl"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/options"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/ponytest"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/random"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/regex"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/readline"
+    system "#{bin}/ponyc", "-rexpr", "#{prefix}/packages/time"
   end
 end
