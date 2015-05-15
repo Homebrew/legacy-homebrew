7dde2c07c35a7b779a826572b8aaa2131162e8be
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 01:04:11 +0100

    libghthash: add 0.6.2 bottle.

--- Library/Formula/libghthash.rb
+++ Library/Formula/libghthash.rb
@@ -3,6 +3,13 @@ class Libghthash < Formula
   url "http://www.bth.se/people/ska/sim_home/filer/libghthash-0.6.2.tar.gz"
   sha256 "d1ccbb81f4c8afd7008f56ecb874f5cf497de480f49ee06929b4303d5852a7dd"
 
+  bottle do
+    cellar :any
+    sha256 "0487e2e14b14ae288428c474fe9ce3e9baf814d4d73de8b0113ca9cc502ffd63" => :yosemite
+    sha256 "207d07d59447e098c1987286324866ef8b26e0c4c191e4c1c0268ba8d95c5fac" => :mavericks
+    sha256 "67fa9f1cda39b827ecd318a9f08980e322be034f00f198c0b7c83c2cf9a3d6a8" => :mountain_lion
+  end
+
   option :universal
 
   depends_on "libtool" => :build
