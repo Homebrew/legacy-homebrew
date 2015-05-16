8060e1c7762bc7669ad63f2c4f1c5adaad4496ca
Alex Dunn <dunn.alex@gmail.com>
2015-05-16 12:56:16 -0700

    srtp 1.5.2
    Closes #39818.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/srtp.rb
+++ Library/Formula/srtp.rb
@@ -1,9 +1,9 @@
-require 'formula'
-
 class Srtp < Formula
-  homepage 'https://github.com/cisco/libsrtp'
-  url 'https://codeload.github.com/cisco/libsrtp/tar.gz/v1.5.0'
-  sha1 'fbace084aa58dddc295e15aeab80208f154b4f16'
+  homepage "https://github.com/cisco/libsrtp"
+  url "https://github.com/cisco/libsrtp/archive/v1.5.2.tar.gz"
+  sha256 "86e1efe353397c0751f6bdd709794143bd1b76494412860f16ff2b6d9c304eda"
+
+  head "https://github.com/cisco/libsrtp.git"
 
   bottle do
     cellar :any
@@ -12,17 +12,10 @@ class Srtp < Formula
     sha1 "079bb268a4ccd761d073d3b4f0eed0d2411bdcaa" => :mountain_lion
   end
 
-  # Add support for building shared libs
-  patch do
-    # Submitted upstream at https://github.com/cisco/libsrtp/pull/86
-    url "http://arunraghavan.net/downloads/brew/srtp-shared-library.patch"
-    sha1 "f44ff924a9e14826add202168f61843c1d88ddd4"
-  end
-
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
                           "--prefix=#{prefix}"
-    system "make shared_library"
-    system "make install" # Can't go in parallel of building the dylib
+    system "make", "shared_library"
+    system "make", "install" # Can't go in parallel of building the dylib
   end
 end
