728db91cd710f8724942266ca7ea13c8521ce635
YoungTaek Oh <youngtaek.oh@gmail.com>
2015-05-16 23:26:59 +0900

    Open-mesh version update (3.1 -> 3.3)  - 'http://' is added to url

--- Library/Formula/open-mesh.rb
+++ Library/Formula/open-mesh.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class OpenMesh < Formula
   homepage "http://openmesh.org"
-  url "www.openmesh.org/media/Releases/3.3/OpenMesh-3.3.tar.gz"
+  url "http://www.openmesh.org/media/Releases/3.3/OpenMesh-3.3.tar.gz"
   sha1 "673677702a27a929c9124f8d4ea2b188a0500c50"
 
   head "http://openmesh.org/svnrepo/OpenMesh/trunk/", :using => :svn
