a6088beefaea7100464cbe374826b0f8846e851d
Larry Gilbert <larry@l2g.to>
2015-05-17 13:59:42 -0700

    swish-e: cleanup and add test.
    Closes #39856.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/swish-e.rb
+++ Library/Formula/swish-e.rb
@@ -1,14 +1,18 @@
-require 'formula'
-
 class SwishE < Formula
-  homepage 'http://swish-e.org/'
-  url 'http://swish-e.org/distribution/swish-e-2.4.7.tar.gz'
-  sha1 '0970c5f8dcb2f12130b38a9fc7dd99c2f2d7ebcb'
+  homepage "http://swish-e.org/"
+  url "http://swish-e.org/distribution/swish-e-2.4.7.tar.gz"
+  sha256 "5ddd541ff8ecb3c78ad6ca76c79e620f457fac9f7d0721ad87e9fa22fe997962"
 
-  depends_on 'libxml2'
+  depends_on "libxml2"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"swish-e", "-S", "fs", "-i", *Dir[HOMEBREW_PREFIX/"*.md"]
+    output = shell_output("#{bin}/swish-e -w respect")
+    assert_match /^# Number of hits: [1-9]\d*$/, output
   end
 end
