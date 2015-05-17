fed6e410d1e86e940de9be8775107d5b3e1ff06b
Larry Gilbert <larry@l2g.to>
2015-05-17 14:52:22 -0700

    hfsutils: fix audit, add test.
    Closes #39857.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/hfsutils.rb
+++ Library/Formula/hfsutils.rb
@@ -1,14 +1,20 @@
-require 'formula'
-
 class Hfsutils < Formula
-  homepage 'http://www.mars.org/home/rob/proj/hfs/'
-  url 'ftp://ftp.mars.org/pub/hfs/hfsutils-3.2.6.tar.gz'
-  sha1 '6d71dfb2c93c0d8082972d39f3f75ae53a438d5d'
+  homepage "http://www.mars.org/home/rob/proj/hfs/"
+  url "ftp://ftp.mars.org/pub/hfs/hfsutils-3.2.6.tar.gz"
+  sha256 "bc9d22d6d252b920ec9cdf18e00b7655a6189b3f34f42e58d5bb152957289840"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     bin.mkpath
     man1.mkpath
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "dd", "if=/dev/zero", "of=disk.hfs", "bs=1k", "count=800"
+    system bin/"hformat", "-l", "Test Disk", "disk.hfs"
+    output = shell_output("#{bin}/hmount disk.hfs")
+    assert_match /^Volume name is "Test Disk"$/, output
+    assert_match /^Volume has 803840 bytes free$/, output
   end
 end
