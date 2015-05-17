6daa3bec521a8276ebe693972db5a2e1660c7285
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-17 04:18:31 +0100

    libexosip: add 4.1.0 bottle.

--- Library/Formula/libexosip.rb
+++ Library/Formula/libexosip.rb
@@ -3,6 +3,13 @@ class Libexosip < Formula
   url "http://download.savannah.gnu.org/releases/exosip/libeXosip2-4.1.0.tar.gz"
   sha256 "3c77713b783f239e3bdda0cc96816a544c41b2c96fa740a20ed322762752969d"
 
+  bottle do
+    cellar :any
+    sha256 "f00b16bbe31ef9ce893ed3a5e2f832fc64e6abff23ee64bc1200a5c0bcb5a8d4" => :yosemite
+    sha256 "72e5336eb6c2d7262783aacc1d5199e0bb8e80bd941660fb8f42346e4d48e075" => :mavericks
+    sha256 "3b0e88403755009f44cba62c2afd81dfa54255c35c4f9bd6a72f230fc21deebb" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libosip"
   depends_on "openssl"
