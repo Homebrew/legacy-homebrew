22cae9ea158202903c32d399f29c237c6b79c6be
Alex Dunn <dunn.alex@gmail.com>
2015-05-16 17:47:09 -0700

    heroku-toolbelt 3.37.0
    Closes #39830.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -1,7 +1,7 @@
 class HerokuToolbelt < Formula
   homepage "https://toolbelt.heroku.com/other"
-  url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.36.7.tgz"
-  sha256 "008393132b6422982221245f50cf28792f9f7a298d0ae13e34273d80976e0e77"
+  url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.37.0.tgz"
+  sha256 "26f28e0921eba6524e65b50ee21771759c27187b3673146cc6f22fa607513265"
   head "https://github.com/heroku/heroku.git"
 
   depends_on :ruby => "1.9"
