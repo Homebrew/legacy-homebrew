415f5e22abb9316f709e69e16f76d119ef30d9be
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-05-18 18:00:06 +0100

    gtk-engines went quartz only
    revision bump
    audit --strict compliance
    strange test since it spawns a GUI. Hope the buildbot wont mind

--- Library/Formula/gtk-engines.rb
+++ Library/Formula/gtk-engines.rb
@@ -1,26 +1,25 @@
-require 'formula'
-
 class GtkEngines < Formula
   desc "Themes for GTK+"
-  homepage 'http://git.gnome.org/browse/gtk-engines/'
-  url 'http://ftp.gnome.org/pub/gnome/sources/gtk-engines/2.20/gtk-engines-2.20.2.tar.bz2'
-  sha1 '574c7577d70eaacecd2ffa14e288ef88fdcb6c2a'
+  homepage "https://git.gnome.org/browse/gtk-engines/"
+  url "https://download.gnome.org/sources/gtk-engines/2.20/gtk-engines-2.20.2.tar.bz2"
+  sha256 "15b680abca6c773ecb85253521fa100dd3b8549befeecc7595b10209d62d66b5"
+  revision 1
 
-  depends_on 'pkg-config' => :build
-  depends_on 'intltool' => :build
-  depends_on 'gettext'
-  depends_on 'cairo'
-  depends_on 'gtk+'
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gettext"
+  depends_on "cairo"
+  depends_on "gtk+"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    ENV['GTK2_RC_FILES'] = "#{share}/themes/Clearlooks/gtk-2.0/gtkrc"
-    ENV['GTK_PATH'] = "#{HOMEBREW_PREFIX}/lib/gtk-2.0"
+    ENV["GTK2_RC_FILES"] = "#{share}/themes/Clearlooks/gtk-2.0/gtkrc"
+    ENV["GTK_PATH"] = "#{HOMEBREW_PREFIX}/lib/gtk-2.0"
     system "#{HOMEBREW_PREFIX}/bin/gtk-demo"
   end
 
