863f03ac5e86f7810f72f1ec764ff36e4d75957c
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-05-18 19:45:21 +0100

    gtk-gnutella went quartz-only
    version bump
    audit --strict compliance

--- Library/Formula/gtk-gnutella.rb
+++ Library/Formula/gtk-gnutella.rb
@@ -1,8 +1,8 @@
 class GtkGnutella < Formula
   desc "Share files in a peer-to-peer (P2P) network"
   homepage "http://gtk-gnutella.sourceforge.net/en/?page=news"
-  url "https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.1.1/gtk-gnutella-1.1.1.tar.bz2"
-  sha1 "aa4f21fe758f7ac52c15ee0156247443ce8c7d6f"
+  url "https://downloads.sourceforge.net/project/gtk-gnutella/gtk-gnutella/1.1.3/gtk-gnutella-1.1.3.tar.bz2"
+  sha256 "2659ddb846f60d13789674e926a71bbb4a8b9d3ca98c6b034a95eaa073531405"
 
   bottle do
     revision 1
@@ -21,5 +21,9 @@ class GtkGnutella < Formula
     system "make", "install"
     rm_rf share+"pixmaps"
     rm_rf share+"applications"
+  end
+
+  test do
+    system "#{bin}/gtk-gnutella", "--version"
   end
 end
