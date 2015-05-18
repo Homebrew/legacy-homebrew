f45116a28a9a1e91e393c576bcb574f5fb737e5e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-18 06:45:41 +0100

    swiftlint: add 0.1.0 bottle.

--- Library/Formula/swiftlint.rb
+++ Library/Formula/swiftlint.rb
@@ -3,6 +3,11 @@ class Swiftlint < Formula
   url "https://github.com/realm/SwiftLint.git", :tag => "0.1.0", :revision => "ed7eadd5c6011c35d017290fb7fb55e735dc18ba"
   head "https://github.com/realm/SwiftLint.git"
 
+  bottle do
+    cellar :any
+    sha256 "1d6c4631e894635fbbbb6d006e5e186452cf40ea0e2e5d98442dda2bda6efd20" => :yosemite
+  end
+
   depends_on :xcode => ["6.3", :build]
 
   def install
