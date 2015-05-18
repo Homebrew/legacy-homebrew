52098aca82f95cf2efadf1ab6e4cef435b18d755
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-05-18 14:55:28 +0100

    pygtk goes quartz
    revision bump
    X11 dependency removed
    libglade optional dependency removed -> will remove this formula later

--- Library/Formula/pygtk.rb
+++ Library/Formula/pygtk.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class Pygtk < Formula
   desc "GTK+ bindings for Python"
   homepage 'http://www.pygtk.org/'
   url 'http://ftp.gnome.org/pub/GNOME/sources/pygtk/2.24/pygtk-2.24.0.tar.bz2'
-  sha1 '344e6a32a5e8c7e0aaeb807e0636a163095231c2'
+  sha256 "cd1c1ea265bd63ff669e92a2d3c2a88eb26bcd9e5363e0f82c896e649f206912"
+  revision 1
 
   bottle do
     sha1 "be4727a98966bb3c60d5f28b727dcab578361c1c" => :yosemite
@@ -12,28 +11,26 @@ class Pygtk < Formula
     sha1 "c715bf7639a0c547441da94e5e9052fc5e756ef9" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on :x11
-  depends_on 'glib'
-  depends_on 'gtk+'
-  depends_on 'atk'
-  depends_on 'pygobject'
-  depends_on 'py2cairo'
-  depends_on 'libglade' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "gtk+"
+  depends_on "atk"
+  depends_on "pygobject"
+  depends_on "py2cairo"
 
   option :universal
 
   def install
-    ENV.append 'CFLAGS', '-ObjC'
+    ENV.append "CFLAGS", "-ObjC"
     ENV.universal_binary if build.universal?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
 
     # Fixing the pkgconfig file to find codegen, because it was moved from
     # pygtk to pygobject. But our pkgfiles point into the cellar and in the
     # pygtk-cellar there is no pygobject.
-    inreplace lib/'pkgconfig/pygtk-2.0.pc', 'codegendir=${datadir}/pygobject/2.0/codegen', "codegendir=#{HOMEBREW_PREFIX}/share/pygobject/2.0/codegen"
+    inreplace lib/"pkgconfig/pygtk-2.0.pc", "codegendir=${datadir}/pygobject/2.0/codegen", "codegendir=#{HOMEBREW_PREFIX}/share/pygobject/2.0/codegen"
     inreplace bin/"pygtk-codegen-2.0", "exec_prefix=${prefix}", "exec_prefix=#{Formula["pygobject"].opt_prefix}"
   end
 
