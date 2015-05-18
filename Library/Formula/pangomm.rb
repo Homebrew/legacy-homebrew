35def554807f175d02f122d25e7569eebe55fe10
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-05-18 14:26:40 +0100

    pangomm goes quartz only
    version bump
    audit --strict compliance
    no revision bump required

--- Library/Formula/pangomm.rb
+++ Library/Formula/pangomm.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Pangomm < Formula
   desc "C++ interface to Pango"
   homepage 'http://www.pango.org/'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/pangomm/2.34/pangomm-2.34.0.tar.xz'
-  sha256 '0e82bbff62f626692a00f3772d8b17169a1842b8cc54d5f2ddb1fec2cede9e41'
+  url "http://ftp.gnome.org/pub/GNOME/sources/pangomm/2.36/pangomm-2.36.0.tar.xz"
+  sha256 "a8d96952c708d7726bed260d693cece554f8f00e48b97cccfbf4f5690b6821f0"
 
   bottle do
     revision 1
@@ -13,13 +11,23 @@ class Pangomm < Formula
     sha1 "3df791891b37c582cad8712fe5a93d6c067ca90e" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'pango'
-  depends_on 'glibmm'
-  depends_on 'cairomm'
+  depends_on "pkg-config" => :build
+  depends_on "glibmm"
+  depends_on "cairomm"
+  depends_on "pango"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <pangomm.h>
+      int main(int argc, char *argv[])
+      {
+        Pango::FontDescription fd;
+        return 0;
+      }
+    EOS
   end
 end
