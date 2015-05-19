273c648e475bca6e2c0041aad1c9b2fa21f3ae78
Alex Dunn <dunn.alex@gmail.com>
2015-05-19 12:06:14 -0700

    sflowtool 3.35
    Closes #39912.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/sflowtool.rb
+++ Library/Formula/sflowtool.rb
@@ -1,22 +1,23 @@
-require 'formula'
-
 class Sflowtool < Formula
-  homepage 'http://www.inmon.com/technology/sflowTools.php'
-  url 'http://www.inmon.com/bin/sflowtool-3.27.tar.gz'
-  sha1 '5205ef2df9cc0b1253765a27ea200446c4525642'
+  homepage "http://www.inmon.com/technology/sflowTools.php"
+  url "http://www.inmon.com/bin/sflowtool-3.35.tar.gz"
+  sha256 "65ee9880bcc46014e5d8deb46dd52ef760e6ec4e2034796b9681b2da3d74393a"
+
+  resource "scripts" do
+    url "http://www.inmon.com/bin/sflowutils.tar.gz"
+    sha256 "45f6a0f96bdb6a1780694b9a4ef9bbd2fd719b9f7f3355c6af1427631b311d56"
+  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
+    (prefix/"contrib").install resource("scripts")
   end
 
   test do
-    require 'open3'
-    Open3.popen3("#{bin}/sflowtool", "-h") do |_, _, stderr|
-      assert_match /sflowtool version: #{Regexp.escape(version)}/, stderr.read
-    end
+    assert_match version.to_s, shell_output("#{bin}/sflowtool -h 2>&1")
   end
 end
