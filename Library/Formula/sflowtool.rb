49a920672e78fbc0da7e72900caec0fee4343a49
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-19 22:24:27 +0100

    sflowtool: add 3.35 bottle.

--- Library/Formula/sflowtool.rb
+++ Library/Formula/sflowtool.rb
@@ -3,6 +3,13 @@ class Sflowtool < Formula
   url "http://www.inmon.com/bin/sflowtool-3.35.tar.gz"
   sha256 "65ee9880bcc46014e5d8deb46dd52ef760e6ec4e2034796b9681b2da3d74393a"
 
+  bottle do
+    cellar :any
+    sha256 "e650482fea249a2e4ab05286b38f8e569709cf31fb20af020a4cfd5f5695e9d8" => :yosemite
+    sha256 "a7a7b98db64cea2cdbed7cecc58a2051e6be0b1c1fa6cb3b8bef38dae595ab9c" => :mavericks
+    sha256 "0cb23bdf819892e4d6aaa34f14b3859df7d41694748ef7fd41827e8591b411eb" => :mountain_lion
+  end
+
   resource "scripts" do
     url "http://www.inmon.com/bin/sflowutils.tar.gz"
     sha256 "45f6a0f96bdb6a1780694b9a4ef9bbd2fd719b9f7f3355c6af1427631b311d56"
