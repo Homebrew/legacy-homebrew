ae583540cb762228dbb66117dd939bad619c3629
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-05-19 19:54:10 +0100

    grsync went quartz
    revision bump
    audit --strict compliance

--- Library/Formula/grsync.rb
+++ Library/Formula/grsync.rb
@@ -1,10 +1,9 @@
-require "formula"
-
 class Grsync < Formula
   desc "GUI for rsync"
-  homepage "http://sourceforge.net/projects/grsync/"
+  homepage "http://www.opbyte.it/grsync/"
   url "https://downloads.sourceforge.net/project/grsync/grsync-1.2.5.tar.gz"
   sha256 "4f1443154f7c85ca7b0e93d5fea438e2709776005e7cfc97da89f4899b1c12e5"
+  revision 1
 
   bottle do
     sha1 "2f246934c2877a3afc7259676f24657f100e825c" => :mavericks
@@ -17,12 +16,17 @@ class Grsync < Formula
   depends_on "gettext"
   depends_on "gtk+"
 
-
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-unity",
                           "--prefix=#{prefix}"
 
     system "make", "install"
+  end
+
+  test do
+    # running the executable always produces the GUI, which is undesirable for the test
+    # so we'll just check if the executable exists
+    assert (bin/"grsync").exist?
   end
 end
