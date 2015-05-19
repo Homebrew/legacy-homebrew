69f91c86b7c72b7e12791b7b4aee4bfc88421890
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-05-19 10:26:33 +0100

    lablgtk went quart-only
    version bump
    audit --strict compliance

--- Library/Formula/lablgtk.rb
+++ Library/Formula/lablgtk.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Lablgtk < Formula
   desc "Objective Caml interface to gtk+"
-  homepage 'http://wwwfun.kurims.kyoto-u.ac.jp/soft/lsl/lablgtk.html'
-  url 'http://wwwfun.kurims.kyoto-u.ac.jp/soft/lsl/dist/lablgtk-2.14.2.tar.gz'
-  sha1 'fd184418ccbc542825748ca63fba75138d2ea561'
+  homepage "http://lablgtk.forge.ocamlcore.org"
+  url "https://forge.ocamlcore.org/frs/download.php/1479/lablgtk-2.18.3.tar.gz"
+  sha256 "975bebf2f9ca74dc3bf7431ebb640ff6a924bb80c8ee5f4467c475a7e4b0cbaf"
 
   bottle do
     sha1 "f17c4f647c272598eebde8a169be6cdac11e30e5" => :yosemite
@@ -12,12 +10,11 @@ class Lablgtk < Formula
     sha1 "d78c6d4a152d142372265af77e9ac95cfb9e92a8" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'camlp4' => :build
-  depends_on :x11
-  depends_on 'objective-caml'
-  depends_on 'gtk+'
-  depends_on 'librsvg'
+  depends_on "pkg-config" => :build
+  depends_on "camlp4" => :build
+  depends_on "objective-caml"
+  depends_on "gtk+"
+  depends_on "librsvg"
 
   def install
     system "./configure", "--bindir=#{bin}",
@@ -25,7 +22,17 @@ class Lablgtk < Formula
                           "--mandir=#{man}",
                           "--with-libdir=#{lib}/ocaml"
     ENV.j1
-    system "make world"
-    system "make install"
+    system "make", "world"
+    system "make", "old-install"
+  end
+
+  test do
+    (testpath/"test.ml").write <<-EOS.undent
+      let main () =
+        GtkMain.Main.init ()
+      let _ = main ()
+    EOS
+    system "ocamlc", "-I", "+lablgtk2", "-o", "test", "lablgtk.cma", "gtkInit.cmo", "test.ml"
+    system "./test"
   end
 end
