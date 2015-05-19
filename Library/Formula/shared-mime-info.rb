6b1360a9ac71b2a1ed7b9811ab4bbe4d2c9a764b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-19 04:38:51 +0100

    shared-mime-info: add 1.4 bottle.

--- Library/Formula/shared-mime-info.rb
+++ Library/Formula/shared-mime-info.rb
@@ -3,6 +3,13 @@ class SharedMimeInfo < Formula
   url "http://freedesktop.org/~hadess/shared-mime-info-1.4.tar.xz"
   sha256 "bbc0bd023f497dfd75e1ca73441cbbb5a63617d9e14f2790b868361cc055b5b1"
 
+  bottle do
+    cellar :any
+    sha256 "d9319f2574b82d7d17b413233307bfbd32691604194244d8ff529aae79db52c6" => :yosemite
+    sha256 "3931c3aa4e637cfb64617f8b0a9b7ceda2520a96c08fdb45235f138268d81064" => :mavericks
+    sha256 "0d66e477d10e982d33ad14c0824419738c5e465ae767f0fcda4e8f11dd82097b" => :mountain_lion
+  end
+
   head do
     url "http://anongit.freedesktop.org/git/xdg/shared-mime-info.git"
     depends_on "automake" => :build
