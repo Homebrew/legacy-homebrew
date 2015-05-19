a9e93fd359413d5a3a27daccda347d8f760b60a4
Alex Dunn <dunn.alex@gmail.com>
2015-05-19 15:26:40 -0700

    sf-pwgen 1.3
    Closes #39915.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/sf-pwgen.rb
+++ Library/Formula/sf-pwgen.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class SfPwgen < Formula
   homepage "https://bitbucket.org/anders/sf-pwgen/"
-  url "https://bitbucket.org/anders/sf-pwgen/downloads/sf-pwgen-1.2.tar.gz"
-  sha1 "7e8bc5e6c4f75ea9fbd904198a188264ff05837c"
+  url "https://bitbucket.org/anders/sf-pwgen/downloads/sf-pwgen-1.3.tar.gz"
+  sha256 "0489dace9de7ad65bf545e774dbf67b6d24cecdcbd32fe5d41397140ccf3aa84"
+
+  head "https://bitbucket.org/anders/sf-pwgen", :using => :hg
 
   bottle do
     cellar :any
@@ -19,6 +19,6 @@ class SfPwgen < Formula
   end
 
   test do
-    system "#{bin}/sf-pwgen", "-a", "memorable", "-c", "10", "-l", "20"
+    assert_equal 20, shell_output("#{bin}/sf-pwgen -a memorable -c 1 -l 20").chomp.length
   end
 end
