687fa6819060cf874545605985b36bdf282e6a11
Alex Dunn <dunn.alex@gmail.com>
2015-05-19 00:21:19 -0700

    sfml/csfml 2.3
    Closes #39900.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/sfml.rb
+++ Library/Formula/sfml.rb
@@ -1,18 +1,10 @@
 class Sfml < Formula
+  # Don't update SFML until there's a corresponding CSFML release
   homepage "http://www.sfml-dev.org/"
-  revision 1
+  url "http://www.sfml-dev.org/files/SFML-2.3-sources.zip"
+  sha256 "a1dc8b00958000628c5394bc8438ba1aa5971fbeeef91a2cf3fa3fff443de7c1"
 
-  stable do
-    url "http://www.sfml-dev.org/download/sfml/2.2/SFML-2.2-sources.zip"
-    sha1 "b21721a3dc221a790e4b81d6ba358c16cb1c1cd3"
-
-    # Patch to support removing the extlibs dir which caused install_name_tool complications.
-    # Already merged in HEAD and will be in the next release.
-    patch do
-      url "https://github.com/LaurentGomila/SFML/commit/9f2aecf9cff75307.diff"
-      sha1 "87646d34cf0315c814ca6313cf75bb0439113eef"
-    end
-  end
+  head "https://github.com/SFML/SFML.git"
 
   bottle do
     cellar :any
@@ -21,26 +13,39 @@ class Sfml < Formula
     sha256 "7471263a9b8cbc9694aa5ad4d5f563cd54119770b6a0be5a8dd2a7d9c9d3dffd" => :mountain_lion
   end
 
-  head "https://github.com/LaurentGomila/SFML.git"
-
   depends_on "cmake" => :build
+  depends_on "doxygen" => :optional
+  depends_on "flac"
   depends_on "freetype"
   depends_on "glew"
   depends_on "jpeg"
-  depends_on "libsndfile"
-  depends_on "doxygen" => :optional
+  depends_on "libogg"
+  depends_on "libvorbis"
+  depends_on "openal-soft"
 
   def install
     args = std_cmake_args
-    args << "."
-
     args << "-DSFML_BUILD_DOC=TRUE" if build.with? "doxygen"
 
-    # Always remove the "extlibs" to avoid install_name_tool failure.
-    # https://github.com/Homebrew/homebrew/pull/35279
-    rm_rf buildpath/"extlibs"
+    # Always remove the "extlibs" to avoid install_name_tool failure
+    # (https://github.com/Homebrew/homebrew/pull/35279) but leave the
+    # headers that were moved there in https://github.com/SFML/SFML/pull/795
+    rm_rf Dir["extlibs/*"] - ["extlibs/headers"]
 
-    system "cmake", *args
+    system "cmake", ".", *args
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include "Time.hpp"
+      int main() {
+        sf::Time t1 = sf::milliseconds(10);
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-I#{include}/SFML/System", "-L#{lib}", "-lsfml-system",
+           testpath/"test.cpp", "-o", "test"
+    system "./test"
   end
 end
