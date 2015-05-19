292279c9e462c098236687fba7165d7e5761a62f
Alex Dunn <dunn.alex@gmail.com>
2015-05-18 20:29:59 -0700

    shared-mime-info 1.4
    Closes #39895.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/shared-mime-info.rb
+++ Library/Formula/shared-mime-info.rb
@@ -1,20 +1,36 @@
-require 'formula'
-
 class SharedMimeInfo < Formula
-  homepage 'http://www.freedesktop.org/wiki/Software/shared-mime-info'
-  url 'http://freedesktop.org/~hadess/shared-mime-info-1.3.tar.xz'
-  sha1 'dfc8f2724df2172be2f2782be0c40c23e1d8f54f'
+  homepage "https://wiki.freedesktop.org/www/Software/shared-mime-info"
+  url "http://freedesktop.org/~hadess/shared-mime-info-1.4.tar.xz"
+  sha256 "bbc0bd023f497dfd75e1ca73441cbbb5a63617d9e14f2790b868361cc055b5b1"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'intltool' => :build
-  depends_on 'gettext'
-  depends_on 'glib'
+  head do
+    url "http://anongit.freedesktop.org/git/xdg/shared-mime-info.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "intltool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gettext"
+  depends_on "glib"
 
   def install
     # Disable the post-install update-mimedb due to crash
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--disable-update-mimedb"
-    system "make install"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --disable-update-mimedb
+    ]
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+    system "make", "install"
+  end
+
+  test do
+    system bin/"update-mime-database", share/"mime"
   end
 end
