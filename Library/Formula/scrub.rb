cf89baabbd887f5d69954d425b1f4b4e0b58f852
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-20 07:59:12 +0100

    scrub: add 2.6.1 bottle.

--- Library/Formula/scrub.rb
+++ Library/Formula/scrub.rb
@@ -4,6 +4,13 @@ class Scrub < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/s/scrub/scrub_2.6.1.orig.tar.gz"
   sha256 "43d98d3795bc2de7920efe81ef2c5de4e9ed1f903c35c939a7d65adc416d6cb8"
 
+  bottle do
+    cellar :any
+    sha256 "ceff54e0445d90ba0ae84700f5a1d5464f47a00f5d6447d2a408894bba1b3c80" => :yosemite
+    sha256 "2b80cd482dd6ff2b9cdb3d730b06b5f3a7a1baa8e214026c63eeb8be410c20a7" => :mavericks
+    sha256 "c713791e6a38ef18474678bbf12592cd69192cfd983f9344fbd76251579c25be" => :mountain_lion
+  end
+
   head do
     url "https://github.com/chaos/scrub.git"
     depends_on "automake" => :build
