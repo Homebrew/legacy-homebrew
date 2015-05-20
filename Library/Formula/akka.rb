9f043b6f252c578d0dda9ca424c0b35545a3f679
Robson Peixoto <robsonpeixoto@gmail.com>
2015-05-20 18:02:47 -0300

    akka 2.3.11

--- Library/Formula/akka.rb
+++ Library/Formula/akka.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Akka < Formula
   homepage "http://akka.io/"
-  url "https://downloads.typesafe.com/akka/akka_2.10-2.3.9.zip"
-  sha256 "b790207f2bd6b8b615c08c2615f26a972580cb0391339f3c4211242adcc93d2c"
+  url "https://downloads.typesafe.com/akka/akka_2.11-2.3.11.zip"
+  sha256 "58157e4f85024d66e20d7e14d2681e041e385af82985eceb5210a454c251abec"
 
   def install
     # Remove Windows files
@@ -12,12 +12,16 @@ class Akka < Formula
     chmod 0755, "bin/akka"
     chmod 0755, "bin/akka-cluster"
 
+    # dos to unix
+    inreplace ["bin/akka", "bin/akka-cluster"], "\r", ""
+
     # Translate akka script
-    inreplace "bin/akka" do |s|
+    inreplace ["bin/akka", "bin/akka-cluster"] do |s|
       s.gsub! /^declare AKKA_HOME=.*$/, "declare AKKA_HOME=#{libexec}"
     end
 
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/akka"
+    bin.install_symlink libexec/"bin/akka-cluster"
   end
 end
