713d2d8aa75806cbe9ef855ed86bb7a133cd0fbc
pandax381 <pandax381@gmail.com>
2015-05-21 04:04:00 +0900

    define the explicit version

--- Library/Formula/miruo.rb
+++ Library/Formula/miruo.rb
@@ -2,6 +2,7 @@ class Miruo < Formula
   homepage "https://github.com/KLab/miruo/"
   url "https://github.com/KLab/miruo/archive/0.9.6a.tar.gz"
   sha256 "346e8bd892fe15e6859486d50231238879d3b620b93df444b13a910e84864e7f"
+  version "0.9.6a"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-libpcap=#{MacOS.sdk_path}/usr"
