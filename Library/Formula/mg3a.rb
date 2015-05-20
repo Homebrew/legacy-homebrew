4e318a120d6170b97f7e21cc8790b7215ae6894f
paaguti <paag@paag.local>
2015-05-20 11:19:25 +0200

    Applied all changes suggested as a response to the pull request and updates to the latest release

--- Library/Formula/mg3a.rb
+++ Library/Formula/mg3a.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Mg3a < Formula
   homepage "http://www.bengtl.net/files/mg3a/"
-  # url "http://www.bengtl.net/files/mg3a/mg3a.150427.tar.gz"
-  # sha256 "d18abb1d8c5d32a72de2c7eea1860974b97e02a0a66dc9a81c3920805c7f3d64"
-  url "http://www.bengtl.net/files/mg3a/snapshots/mg3a.150506.tar.gz"
-  sha256 "988873190544b17e9d90bcc4101156c044a10ea1e256424a59702986c92ca4e4"
+  url "http://www.bengtl.net/files/mg3a/mg3a.150519.tar.gz"
+  sha256 "b1735f3b933084b6760693022773e8b7395522b8db878a45a34f0c02e60374bc"
+
+  conflicts_with "mg", :because => "both install `mg`"
 
   option "without-emacs-quit", "Use ^U ^X ^C to exit saving"
   option "with-c-mode", "Include the original C mode"
@@ -17,30 +15,22 @@ class Mg3a < Formula
   option "with-user-macros", "Include the support for user defined macros"
   option "with-most","Include c-like and python modes, user modes and user macros"
   option "with-all","Include all fancy stuff"
-  
-  # currently, Bengt's makefile doesn't allow to customize
-  # the supported modes while making mg. We need a small patch for that
-  # should be removed when that is fixed
-  
+
   def install
     mg3aopts=""
-    mg3aopts << " -DDIRED" if build.with? 'dired' or build.with? 'most'
-    mg3aopts << " -DPREFIXREGION" if build.with? 'prefix-region' or build.with? 'most'
-    mg3aopts << " -DLANGMODE_C" if build.with? 'c-mode'
-    mg3aopts << " -DLANGMODE_PYTHON" if build.with? 'python-mode' or build.with? 'most'
-    mg3aopts << " -DLANGMODE_CLIKE" if build.with? 'clike-mode' or build.with? 'most'
-    mg3aopts << " -DUSER_MODES" if build.with? 'user-modes' or build.with? 'most'
-    mg3aopts << " -DUSER_MACROS" if build.with? 'user-macros' or build.with? 'most'
-    mg3aopts = "-DALL" if build.with? 'all'
-    mg3aopts << " -DEMACS_QUIT" if build.with? 'emacs-quit'
-    
-    system "make clean; make CDEFS=\"#{mg3aopts}\" LIBS=-lncurses COPT=-O3"
+    mg3aopts << " -DDIRED" if build.with?("dired") || build.with?("most")
+    mg3aopts << " -DPREFIXREGION" if build.with?("prefix-region") || build.with?("most")
+    mg3aopts << " -DLANGMODE_C" if build.with?("c-mode")
+    mg3aopts << " -DLANGMODE_PYTHON" if build.with?("python-mode") || build.with?("most")
+    mg3aopts << " -DLANGMODE_CLIKE" if build.with?("clike-mode") || build.with?("most")
+    mg3aopts << " -DUSER_MODES" if build.with?("user-modes") || build.with?("most")
+    mg3aopts << " -DUSER_MACROS" if build.with?("user-macros") || build.with?("most")
+    mg3aopts = "-DALL" if build.with?("all")
+    mg3aopts << " -DEMACS_QUIT" if build.with?("emacs-quit")
+
+    system "make", "CDEFS=#{mg3aopts}", "LIBS=-lncurses", "COPT=-O3"
     bin.install "mg"
-    doc.install Dir['bl/dot.*']
-    doc.install Dir['README*']
-  end
-  
-  test do
-    system "false"
+    doc.install Dir["bl/dot.*"]
+    doc.install Dir["README*"]
   end
 end
