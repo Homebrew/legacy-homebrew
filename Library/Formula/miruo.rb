9ca4ae275f5ada975634eb8f6c2e1f619e1271e7
pandax381 <pandax381@gmail.com>
2015-05-21 02:34:26 +0900

    miruo 0.9.6a (new formula)

---
+++ Library/Formula/miruo.rb
@@ -0,0 +1,14 @@
+class Miruo < Formula
+  homepage "https://github.com/KLab/miruo/"
+  url "https://github.com/KLab/miruo/archive/0.9.6a.tar.gz"
+  sha256 "346e8bd892fe15e6859486d50231238879d3b620b93df444b13a910e84864e7f"
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking", "--with-libpcap=#{MacOS.sdk_path}/usr"
+    system "make", "install"
+  end
+
+  test do
+    system "#{sbin}/miruo", "--version"
+  end
+end
