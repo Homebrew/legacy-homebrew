b814697d580a73ab2a5ed9eefd0dcedd215f90cb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-20 15:32:31 +0100

    http-parser: add 2.5.0 bottle.

--- Library/Formula/http-parser.rb
+++ Library/Formula/http-parser.rb
@@ -3,6 +3,12 @@ class HttpParser < Formula
   url "https://github.com/joyent/http-parser/archive/v2.5.0.tar.gz"
   sha256 "e3b4ba58f4e6ee5fbec781df020e5cb74c3a799a07f059e1e125127a0b801481"
 
+  bottle do
+    sha256 "41cf106f3766a8fd48604e1c53f4cdd20a34e5605eec8bd41674858a82755fde" => :yosemite
+    sha256 "7b047f85124a7820932a39147df2abda08a909f53ec8c00c763ee04c9f441ebc" => :mavericks
+    sha256 "27aa668522772cee01605a5ab047930dc17dba77b299e91304ab973ae66af920" => :mountain_lion
+  end
+
   depends_on "coreutils" => :build
 
   patch do
