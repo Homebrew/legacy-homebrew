308b782bb679b0d650c324bb09e2cd4f84a5553d
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-21 00:24:47 +0100

    mozjpeg 3.1
    Bump, and vendored nasm.
    
    Closes #39939
    
    Closes #39941.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mozjpeg.rb
+++ Library/Formula/mozjpeg.rb
@@ -1,7 +1,7 @@
 class Mozjpeg < Formula
   homepage "https://github.com/mozilla/mozjpeg"
-  url "https://github.com/mozilla/mozjpeg/releases/download/v3.0/mozjpeg-3.0-release-source.tar.gz"
-  sha1 "9b56af77ce376300e1f6de78b52b2ce9b6f19596"
+  url "https://github.com/mozilla/mozjpeg/releases/download/v3.1/mozjpeg-3.1-release-source.tar.gz"
+  sha256 "deedd88342c5da219f0047d9a290cd58eebe1b7a513564fcd8ebc49670077a1f"
 
   head do
     url "https://github.com/mozilla/mozjpeg.git"
@@ -19,12 +19,25 @@ class Mozjpeg < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "nasm" => :build
   depends_on "libpng" => :optional
 
   keg_only "mozjpeg is not linked to prevent conflicts with the standard libjpeg."
 
+  # https://github.com/mozilla/mozjpeg/issues/175
+  # https://github.com/Homebrew/homebrew/issues/39939
+  resource "nasm" do
+    url "http://www.nasm.us/pub/nasm/releasebuilds/2.11.06/nasm-2.11.06.tar.xz"
+    sha256 "90f60d95a15b8a54bf34d87b9be53da89ee3d6213ea739fb2305846f4585868a"
+  end
+
   def install
+    resource("nasm").stage do
+      system "./configure", "--prefix=#{buildpath}/nasm"
+      system "make", "install"
+    end
+
+    ENV.prepend_path "PATH", buildpath/"nasm/bin"
+
     system "autoreconf", "-i" if build.head?
     system "./configure", "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
