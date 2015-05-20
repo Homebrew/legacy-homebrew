88d5c212b87a9afe424c5932f076b0156a26f988
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-20 01:31:34 +0100

    nvm 0.25.2

--- Library/Formula/nvm.rb
+++ Library/Formula/nvm.rb
@@ -1,11 +1,11 @@
 class Nvm < Formula
   homepage "https://github.com/creationix/nvm"
-  url "https://github.com/creationix/nvm/archive/v0.25.1.tar.gz"
-  sha256 "3c67d2ced9cf29fe067e434dba2015e05c4305258541204e5c5ede52c93033fe"
+  url "https://github.com/creationix/nvm/archive/v0.25.2.tar.gz"
+  sha256 "ae3f0320071eaf347437adaf55d776342abefe3e1d991e9a6aa9ce718b4f0593"
   head "https://github.com/creationix/nvm.git"
 
   resource "nvm-exec" do
-    url "https://raw.githubusercontent.com/creationix/nvm/v0.25.1/nvm-exec"
+    url "https://raw.githubusercontent.com/creationix/nvm/v0.25.2/nvm-exec"
     sha256 "a0581795f10114b9759992a82a43496daf4b4a52ad381a3072d8eee9866a28c5"
   end
 
