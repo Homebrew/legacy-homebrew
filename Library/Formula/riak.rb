679b7c6daedabf9d0ab3ef9fe070b546fde082e6
Chris Dillon <squarism@gmail.com>
2015-05-21 16:25:04 -0700

    riak: add link to riak docs about ulimit settings when too low

--- Library/Formula/riak.rb
+++ Library/Formula/riak.rb
@@ -1,8 +1,6 @@
-require "formula"
-
 class Riak < Formula
-  desc "Distributed database"
   homepage "http://basho.com/riak/"
+  desc "Riak is a distributed NoSQL key-value data store."
   url "https://s3.amazonaws.com/downloads.basho.com/riak/2.1/2.1.1/osx/10.8/riak-2.1.1-OSX-x86_64.tar.gz"
   version "2.1.1"
   sha256 "ee06193b5fc4bb56746f8f648794b732b96879369835a94f22235e0561d652d7"
@@ -29,5 +27,41 @@ class Riak < Formula
     bin.write_exec_script libexec/"bin/riak-admin"
     bin.write_exec_script libexec/"bin/riak-debug"
     bin.write_exec_script libexec/"bin/search-cmd"
+  end
+
+  def recommended_maxfiles
+    65536
+  end
+
+  def recommended_maxproc
+    2048
+  end
+
+  def caveats
+    msg = nil
+    if maxfiles_too_low? || maxprocs_too_low?
+      msg = "Your maxfiles limit or maxprocs limit is too low.  See the riak docs:\n"
+      msg << "http://docs.basho.com/riak/latest/ops/tuning/open-files-limit/"
+    end
+
+    msg
+  end
+
+  def maxfiles_too_low?
+    maxfiles = `launchctl limit maxfiles`
+    _, maxfiles_soft, maxfiles_hard = maxfiles.split
+
+    maxfiles_soft.to_i < recommended_maxfiles || maxfiles_hard.to_i < recommended_maxfiles
+  end
+
+  def maxprocs_too_low?
+    maxproc= `launchctl limit maxproc`
+    _, maxproc_soft, maxproc_hard = maxproc.split
+
+    maxproc_soft.to_i < recommended_maxproc || maxproc_hard.to_i < recommended_maxproc
+  end
+
+  test do
+    system "#{bin}/riak", "help", "0"
   end
 end
