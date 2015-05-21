c82d396ada49abe8f6a923a0ad712e2014425e64
Lachlan Cooper <lachlan.cooper@anchor.net.au>
2015-05-21 12:29:09 +1000

    Require autotools

--- Library/Formula/tnef.rb
+++ Library/Formula/tnef.rb
@@ -5,6 +5,10 @@ class Tnef < Formula
   url "https://github.com/verdammelt/tnef/archive/1.4.12.tar.gz"
   sha256 "fefea5d9481555cc150ab799b9b1e957564e7fd2ead99fa19e87258f263f7c37"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
   def install
     system "autoreconf", "-i"
     system "./configure", "--prefix=#{prefix}"
