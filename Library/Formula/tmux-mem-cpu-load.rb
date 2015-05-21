9b3ef476101c5e1c98afe3035c30f5e62786d02d
Alex Dunn <dunn.alex@gmail.com>
2015-05-21 11:19:55 -0700

    tmux-mem-cpu-load 3.2.2
    Closes #39961.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/tmux-mem-cpu-load.rb
+++ Library/Formula/tmux-mem-cpu-load.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class TmuxMemCpuLoad < Formula
   homepage "https://github.com/thewtex/tmux-mem-cpu-load"
-  url "https://github.com/thewtex/tmux-mem-cpu-load/archive/v3.1.0.tar.gz"
-  sha1 "b243a064a24bf86853ae9ab1daf0fb650a23bd97"
+  url "https://github.com/thewtex/tmux-mem-cpu-load/archive/v3.2.2.tar.gz"
+  sha256 "cf595916ccf92ece9d3bc50c71d6963f83515605ad7639407e7f893203ae5022"
+
+  head "https://github.com/thewtex/tmux-mem-cpu-load.git"
 
   bottle do
     cellar :any
@@ -17,5 +17,9 @@ class TmuxMemCpuLoad < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    system bin/"tmux-mem-cpu-load"
   end
 end
