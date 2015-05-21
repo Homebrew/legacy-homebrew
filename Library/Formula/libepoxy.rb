f321801ab37491b54afdc4ffd1bf206f48434002
Ryan Hendrickson <ryan.hendrickson@alum.mit.edu>
2015-05-21 11:41:15 -0400

    libepoxy: build requires Python 2.7
    Which ships with Lion, but Snow Leopard is still on 2.6.
    
    Closes #39956.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libepoxy.rb
+++ Library/Formula/libepoxy.rb
@@ -14,6 +14,7 @@ class Libepoxy < Formula
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
+  depends_on :python => :build if MacOS.version <= :snow_leopard
 
   resource "xorg-macros" do
     url "http://xorg.freedesktop.org/releases/individual/util/util-macros-1.19.0.tar.bz2"
