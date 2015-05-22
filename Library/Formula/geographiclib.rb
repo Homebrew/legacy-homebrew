f78c2f4ad8977a594bc6dc55ff1feb65db59b300
Charles Karney <charles.karney@sri.com>
2015-05-22 16:46:17 -0400

    geographiclib 1.43
    Closes #39998.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/geographiclib.rb
+++ Library/Formula/geographiclib.rb
@@ -1,8 +1,8 @@
 require "formula"
 class Geographiclib < Formula
   homepage "http://geographiclib.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.42.tar.gz"
-  sha1 "cb1cd87d0e00bffb1d9192d13e83b38deb1a4266"
+  url "https://downloads.sourceforge.net/project/geographiclib/distrib/GeographicLib-1.43.tar.gz"
+  sha1 "444650196e1e7fd18c7d3e184599d05be5221e57"
 
   bottle do
     cellar :any
