5ac6367565a116e1823e27cf1b6c7a7e314c5f39
Alex Dunn <dunn.alex@gmail.com>
2015-05-22 22:48:46 -0700

    irssi: add head
    Closes #40011.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/irssi.rb
+++ Library/Formula/irssi.rb
@@ -1,11 +1,17 @@
-require "formula"
-
 class Irssi < Formula
   homepage "http://irssi.org/"
-  url "http://irssi.org/files/irssi-0.8.17.tar.bz2"
-  mirror "http://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/irssi-0.8.17.tar.bz2"
-  sha1 "3bdee9a1c1f3e99673143c275d2c40275136664a"
+  url "http://irssi.org/files/irssi-0.8.17.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/i/irssi/irssi_0.8.17.orig.tar.gz"
+  sha256 "0ae01f76797fb6d6b8e0f2268b39c7afb90ac62658ec754c82acfc344b8203e9"
   revision 2
+
+  head do
+    url "https://github.com/irssi/irssi.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+    depends_on "lynx" => :build
+  end
 
   bottle do
     revision 1
@@ -21,10 +27,14 @@ class Irssi < Formula
   depends_on "openssl" => :recommended
   depends_on "dante" => :optional
 
-  # Fix Perl build flags and paths in man page
-  patch :DATA
-
   def install
+    # Make paths in man page Homebrew-specific
+    # https://github.com/irssi/irssi/issues/251
+    inreplace "docs/irssi.1" do |s|
+      s.gsub! "/usr/share", "#{HOMEBREW_PREFIX}/share"
+      s.gsub! "/etc/irssi.conf", "#{HOMEBREW_PREFIX}/etc/irssi.conf"
+    end
+
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
@@ -50,66 +60,20 @@ class Irssi < Formula
 
     args << "--disable-ssl" if build.without? "openssl"
 
-    system "./configure", *args
+    if build.head?
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
     # "make" and "make install" must be done separately on some systems
     system "make"
     system "make", "install"
   end
+
+  test do
+    IO.popen("#{bin}/irssi --connect=irc.freenode.net", "w") do |pipe|
+      pipe.puts "/quit\n"
+      pipe.close_write
+    end
+  end
 end
-
-__END__
---- a/configure	2009-12-03 19:35:07.000000000 -0800
-+++ b/configure	2009-12-03 19:35:33.000000000 -0800
-@@ -27419,7 +27419,7 @@
- 	if test -z "$perlpath"; then
- 		perl_check_error="perl binary not found"
- 	else
--		PERL_CFLAGS=`$perlpath -MExtUtils::Embed -e ccopts 2>/dev/null`
-+		PERL_CFLAGS=`$perlpath -MExtUtils::Embed -e ccopts 2>/dev/null | $SED -e 's/-arch [^ ]\{1,\}//g'`
- 	fi
-
- 	if test "x$ac_cv_c_compiler_gnu" = "xyes" -a -z "`echo $host_os|grep 'bsd\|linux'`"; then
-@@ -27437,7 +27437,7 @@
- $as_echo "not found, building without Perl" >&6; }
- 		want_perl=no
- 	else
--		PERL_LDFLAGS=`$perlpath -MExtUtils::Embed -e ldopts 2>/dev/null`
-+		PERL_LDFLAGS=`$perlpath -MExtUtils::Embed -e ldopts 2>/dev/null | $SED -e 's/-arch [^ ]\{1,\}//g'`
-
- 		if test "x$DYNLIB_MODULES" = "xno" -a "$want_perl" != "static"; then
- 						want_perl=static
-
-diff --git a/docs/irssi.1 b/docs/irssi.1
-index 62c2844..482cd96 100644
---- a/docs/irssi.1
-+++ b/docs/irssi.1
-@@ -65,10 +65,10 @@ display brief usage message.
- .SH SEE ALSO
- .B Irssi
- has been supplied with a huge amount of documentation. Check /help or look
--at the files contained by /usr/share/doc/irssi*
-+at the files contained by HOMEBREW_PREFIX/share/doc/irssi*
- .SH FILES
- .TP
--.I /etc/irssi.conf
-+.I HOMEBREW_PREFIX/etc/irssi.conf
- Global configuration file
- .TP
- .I ~/.irssi/config
-@@ -83,13 +83,13 @@ Default irssi theme
- .I ~/.irssi/away.log
- Logged messages in away status
- .TP
--.I /usr/share/irssi/help/
-+.I HOMEBREW_PREFIX/share/irssi/help/
- Directory including many help files
- .TP
--.I /usr/share/irssi/scripts/
-+.I HOMEBREW_PREFIX/share/irssi/scripts/
- Global scripts directory
- .TP
--.I /usr/share/irssi/themes/
-+.I HOMEBREW_PREFIX/share/irssi/themes/
- Global themes directory
- .TP
- .I ~/.irssi/scripts/
