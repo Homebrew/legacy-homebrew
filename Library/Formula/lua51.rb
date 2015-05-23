7d7eecb3d884a9838e702d4c8176b51a00758ed9
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-23 05:12:12 +0100

    lua51: bump to luarocks 2.2.2
    Closes #40007.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/lua51.rb
+++ Library/Formula/lua51.rb
@@ -46,8 +46,8 @@ class Lua51 < Formula
   end
 
   resource "luarocks" do
-    url "https://github.com/keplerproject/luarocks/archive/v2.2.1.tar.gz"
-    sha256 "30e5bd99f82f5e3ea174572c1831f9ff83dfe37727f9fcfc89168b4572193571"
+    url "https://keplerproject.github.io/luarocks/releases/luarocks-2.2.2.tar.gz"
+    sha256 "4f0427706873f30d898aeb1dfb6001b8a3478e46a5249d015c061fe675a1f022"
   end
 
   def install
