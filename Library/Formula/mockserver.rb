405d0b11a8a149d0c3aee1ca2d8d76ea919b2d3a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-23 19:32:49 +0100

    mockserver: add 3.9.15 bottle.

--- Library/Formula/mockserver.rb
+++ Library/Formula/mockserver.rb
@@ -4,6 +4,13 @@ class Mockserver < Formula
   version "3.9.15"
   sha256 "0dbe6e78c3753c1da0381322ec0d348c2a3a02783fd65bf280a7bfaff02046d7"
 
+  bottle do
+    cellar :any
+    sha256 "7863dee35cd11f9825d6b14c4e7b09c220ac3c2bbb8387c430df046b80b21482" => :yosemite
+    sha256 "c498debd91b33632811c6fafe761563172f97ddf904a69360ce6335e4a714713" => :mavericks
+    sha256 "df557bf66af8ceead30cf1fc1948e993d04baf7be85a832db28f6b86b4150ddb" => :mountain_lion
+  end
+
   depends_on :java => "1.6+"
 
   def install
