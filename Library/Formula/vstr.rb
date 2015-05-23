ab4380bdef397fc6d4d2d58903e8008dd7711a4f
Baptiste Fontaine <batifon@yahoo.fr>
2015-05-23 15:44:35 +0200

    vstr: test added
    Closes #40017.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/vstr.rb
+++ Library/Formula/vstr.rb
@@ -1,16 +1,48 @@
-require 'formula'
-
 class Vstr < Formula
-  homepage 'http://www.and.org/vstr/'
-  url 'http://www.and.org/vstr/1.0.15/vstr-1.0.15.tar.bz2'
-  sha1 '4d2c19cb83f30239b3800abc5c9eda4f24dd0b78'
+  homepage "http://www.and.org/vstr/"
+  url "http://www.and.org/vstr/1.0.15/vstr-1.0.15.tar.bz2"
+  sha256 "d33bcdd48504ddd21c0d53e4c2ac187ff6f0190d04305e5fe32f685cee6db640"
 
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
 
   def install
-    ENV.append 'CFLAGS', '--std=gnu89' if ENV.compiler == :clang
+    ENV.append "CFLAGS", "--std=gnu89" if ENV.compiler == :clang
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--mandir=#{man}"
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      // based on http://www.and.org/vstr/examples/ex_hello_world.c
+      #define VSTR_COMPILE_INCLUDE 1
+      #include <vstr.h>
+      #include <errno.h>
+      #include <err.h>
+      #include <unistd.h>
+
+      int main(void) {
+        Vstr_base *s1 = NULL;
+
+        if (!vstr_init())
+          err(EXIT_FAILURE, "init");
+
+        if (!(s1 = vstr_dup_cstr_buf(NULL, "Hello Homebrew\\n")))
+          err(EXIT_FAILURE, "Create string");
+
+        while (s1->len)
+          if (!vstr_sc_write_fd(s1, 1, s1->len, STDOUT_FILENO, NULL)) {
+            if ((errno != EAGAIN) && (errno != EINTR))
+              err(EXIT_FAILURE, "write");
+          }
+
+        vstr_free_base(s1);
+        vstr_exit();
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-L#{lib}", "-lvstr", "-o", "test"
+    system "./test"
   end
 end
