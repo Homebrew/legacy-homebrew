0fe8402d2d84ff3cd5215ee9ebe8551982610e7b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-23 05:24:45 +0100

    rsstail: add 1.8 bottle.

--- Library/Formula/rsstail.rb
+++ Library/Formula/rsstail.rb
@@ -3,6 +3,13 @@ class Rsstail < Formula
   url "http://www.vanheusden.com/rsstail/rsstail-1.8.tgz"
   sha256 "19284f3eca4bfa649f53848e19e6ee134bce17ccf2a22919cc8c600684877801"
 
+  bottle do
+    cellar :any
+    sha256 "0d18005ca03757ba45acb78737587eeb9abbe31e5346b0a0738dbc63951a3311" => :yosemite
+    sha256 "1ec19466e92bbeae9ed3b9e4f9ea9af18eabfd7c8c86e74eda409271028d1b3e" => :mavericks
+    sha256 "b17a2996711143d24d0a85ecfe26592b4ad9927e2f42e22af7289abe4e5bc4f5" => :mountain_lion
+  end
+
   depends_on "libmrss"
 
   head "https://github.com/flok99/rsstail.git"
