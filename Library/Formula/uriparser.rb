5e18ae01309dfa61d8c399c6211c01311d42f3fe
Alex Dunn <dunn.alex@gmail.com>
2015-05-23 16:36:20 -0700

    uriparser 0.8.2
    Closes #40032.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/uriparser.rb
+++ Library/Formula/uriparser.rb
@@ -1,9 +1,15 @@
-require "formula"
-
 class Uriparser < Formula
   homepage "http://uriparser.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/uriparser/Sources/0.8.1/uriparser-0.8.1.tar.bz2"
-  sha1 "4405d8baa0d9f5bc0319e6d5e68770acab67b602"
+  url "https://downloads.sourceforge.net/project/uriparser/Sources/0.8.2/uriparser-0.8.2.tar.bz2"
+  mirror "https://mirrors.kernel.org/debian/pool/main/u/uriparser/uriparser_0.8.2.orig.tar.bz2"
+  sha256 "6d6e66b0615f65e9e2391933dab7e45eca0947160f10c6b47bc50feda93e508f"
+
+  head do
+    url "git://git.code.sf.net/p/uriparser/git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
 
   bottle do
     cellar :any
@@ -18,10 +24,21 @@ class Uriparser < Formula
   conflicts_with "libkml", :because => "both install `liburiparser.dylib`"
 
   def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-doc"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    expected = <<-EOS.undent
+      uri:          http://brew.sh
+      scheme:       http
+      hostText:     brew.sh
+      absolutePath: false
+    EOS
+    assert_equal expected, shell_output("#{bin}/uriparse http://brew.sh").chomp
   end
 end
