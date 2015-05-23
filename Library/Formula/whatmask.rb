eb9e8bb17f0166024cf5582a61a52494ec5f4dcb
Baptiste Fontaine <batifon@yahoo.fr>
2015-05-23 14:05:20 +0200

    whatmask: modernize, use sha256
    Closes #40015.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/whatmask.rb
+++ Library/Formula/whatmask.rb
@@ -1,29 +1,28 @@
-require 'formula'
-
 class Whatmask < Formula
-  homepage 'http://www.laffeycomputer.com/whatmask.html'
-  url 'http://downloads.laffeycomputer.com/current_builds/whatmask/whatmask-1.2.tar.gz'
-  sha1 '313762672acacd40de8021132b1024a5c96e2ad5'
+  homepage "http://www.laffeycomputer.com/whatmask.html"
+  url "http://downloads.laffeycomputer.com/current_builds/whatmask/whatmask-1.2.tar.gz"
+  sha256 "7dca0389e22e90ec1b1c199a29838803a1ae9ab34c086a926379b79edb069d89"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--mandir=#{man}",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    assert_equal <<-EOS, `#{bin}/whatmask /24`
+    assert_equal <<-EOS.undent, shell_output("#{bin}/whatmask /24")
 
----------------------------------------------
-       TCP/IP SUBNET MASK EQUIVALENTS
----------------------------------------------
-CIDR = .....................: /24
-Netmask = ..................: 255.255.255.0
-Netmask (hex) = ............: 0xffffff00
-Wildcard Bits = ............: 0.0.0.255
-Usable IP Addresses = ......: 254
+      ---------------------------------------------
+             TCP/IP SUBNET MASK EQUIVALENTS
+      ---------------------------------------------
+      CIDR = .....................: /24
+      Netmask = ..................: 255.255.255.0
+      Netmask (hex) = ............: 0xffffff00
+      Wildcard Bits = ............: 0.0.0.255
+      Usable IP Addresses = ......: 254
 
-EOS
+    EOS
   end
 end
