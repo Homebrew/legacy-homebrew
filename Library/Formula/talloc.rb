ba634e1480fd475c287228fe1ab59b59645a8239
Alex Dunn <dunn.alex@gmail.com>
2015-05-22 18:59:34 -0700

    talloc 2.1.2
    Closes #40006.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/talloc.rb
+++ Library/Formula/talloc.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class Talloc < Formula
-  homepage "http://talloc.samba.org/"
-  url "http://www.samba.org/ftp/talloc/talloc-2.1.1.tar.gz"
-  sha1 "380bb786274dfd1a4a8179d31cd88cbee15c97bf"
+  homepage "https://talloc.samba.org/"
+  url "https://www.samba.org/ftp/talloc/talloc-2.1.2.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/t/talloc/talloc_2.1.2.orig.tar.gz"
+  sha256 "230d78a3fca75a15ab0f5d76d7bbaeadd3c1e695adcbb085932d227f5c31838d"
 
   bottle do
     cellar :any
@@ -19,6 +18,27 @@ class Talloc < Formula
                           "--disable-rpath",
                           "--without-gettext",
                           "--disable-python"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <talloc.h>
+      int main()
+      {
+        int ret;
+        TALLOC_CTX *tmp_ctx = talloc_new(NULL);
+        if (tmp_ctx == NULL) {
+          ret = 1;
+          goto done;
+        }
+        ret = 0;
+      done:
+        talloc_free(tmp_ctx);
+        return ret;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-ltalloc", "test.c", "-o", "test"
+    system testpath/"test"
   end
 end
