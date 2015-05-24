4b66c3972e40edeb1cacc1c28cdad9a0b55844a7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-24 18:14:16 +0100

    texapp: add 0.6.10 bottle.

--- Library/Formula/texapp.rb
+++ Library/Formula/texapp.rb
@@ -3,6 +3,13 @@ class Texapp < Formula
   url "http://www.floodgap.com/software/texapp/dist0/0.6.10.txt"
   sha256 "e05fa2b3cdc90c79c8725141b3176a312d46251606431226f0754d96fb0b10fd"
 
+  bottle do
+    cellar :any
+    sha256 "836d36b2815deefebd8052ec82790f53075c646bfa69d66ef5c9ac68a7a5fb7f" => :yosemite
+    sha256 "d6d7c7f46df6ca2c19b25795e9c739a50737b3881ee34d4a10a87bf074ee6043" => :mavericks
+    sha256 "055321fde66b9fb34f4fd97069ef5310228c41384b3ee3a0eef98b9d33c1707b" => :mountain_lion
+  end
+
   depends_on "readline" => :optional
 
   resource "Term::ReadLine::TTYtter" do
