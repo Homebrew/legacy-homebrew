cc5b606befcf729376c7feb1aab2551f84b8729b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-24 18:21:53 +0100

    unrtf: add 0.21.9 bottle.

--- Library/Formula/unrtf.rb
+++ Library/Formula/unrtf.rb
@@ -4,6 +4,12 @@ class Unrtf < Formula
   mirror "https://ftp.gnu.org/gnu/unrtf/unrtf-0.21.9.tar.gz"
   sha256 "22a37826f96d754e335fb69f8036c068c00dd01ee9edd9461a36df0085fb8ddd"
 
+  bottle do
+    sha256 "42737f31a7ea06592c2ad22a48f0e2537c0cd025129870399bd4f0fbe7362a98" => :yosemite
+    sha256 "852bd896c8537489400e646ed41f2876079e124203e493cfc1e2d7f51d024726" => :mavericks
+    sha256 "7e680da7a4c4da9ed3b21f04e5125721cf506cc9579a2e95b4635078b0606cb2" => :mountain_lion
+  end
+
   head "http://hg.savannah.gnu.org/hgweb/unrtf/", :using => :hg
 
   depends_on "automake" => :build
