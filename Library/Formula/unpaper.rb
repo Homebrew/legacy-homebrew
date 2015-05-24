4f3dba0fa88bf055cce3c092503543f8a568b796
Alex Dunn <dunn.alex@gmail.com>
2015-05-23 22:14:48 -0700

    unpaper 6.1
    Closes #40040.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/unpaper.rb
+++ Library/Formula/unpaper.rb
@@ -1,9 +1,16 @@
-require "formula"
-
 class Unpaper < Formula
   homepage "https://www.flameeyes.eu/projects/unpaper"
-  url "https://www.flameeyes.eu/files/unpaper-5.1.tar.xz"
-  sha1 "97068a99d47d1d65030c88d52058c1d5ff7b41d1"
+  url "https://www.flameeyes.eu/files/unpaper-6.1.tar.xz"
+  sha256 "237c84f5da544b3f7709827f9f12c37c346cdf029b1128fb4633f9bafa5cb930"
+
+  head do
+    url "https://github.com/Flameeyes/unpaper.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "ffmpeg"
 
   bottle do
     cellar :any
@@ -13,7 +20,27 @@ class Unpaper < Formula
   end
 
   def install
+    system "autoreconf", "-i" if build.head?
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.pbm").write <<-EOS.undent
+      P1
+      6 10
+      0 0 0 0 1 0
+      0 0 0 0 1 0
+      0 0 0 0 1 0
+      0 0 0 0 1 0
+      0 0 0 0 1 0
+      0 0 0 0 1 0
+      1 0 0 0 1 0
+      0 1 1 1 0 0
+      0 0 0 0 0 0
+      0 0 0 0 0 0
+    EOS
+    system bin/"unpaper", testpath/"test.pbm", testpath/"out.pbm"
+    File.exist? testpath/"out.pbm"
   end
 end
