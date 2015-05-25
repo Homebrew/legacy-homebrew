e966c05560388c59392d8d01a701e3c7070a9d62
Simon Hewitt <si@sjhewitt.co.uk>
2015-05-25 13:50:25 -0700

    update logstash sha

--- Library/Formula/logstash.rb
+++ Library/Formula/logstash.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Logstash < Formula
   homepage 'https://www.elastic.co/products/logstash'
   url 'https://download.elasticsearch.org/logstash/logstash/logstash-1.5.0.tar.gz'
-  sha256 '9729c2d31fddaabdd3d8e94c34a6d1f61d57f94a'
+  sha256 '8e073ee4c0dd346c62d538d3f28c95e536b9d3f269d0f993cff8662d799163d3'
 
   def install
     inreplace %w{bin/logstash}, /^\. "\$\(cd `dirname \$0`\/\.\.; pwd\)\/bin\/logstash\.lib\.sh\"/, ". #{libexec}/bin/logstash.lib.sh"
