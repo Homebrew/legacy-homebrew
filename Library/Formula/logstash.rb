afeb080e5f11be85d9b3348c2162311f0de45444
Simon Hewitt <si@sjhewitt.co.uk>
2015-05-25 09:50:07 -0700

    Update logstash to version 1.5.0

--- Library/Formula/logstash.rb
+++ Library/Formula/logstash.rb
@@ -2,19 +2,20 @@ require 'formula'
 
 class Logstash < Formula
   homepage 'http://logstash.net/'
-  url 'https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz'
-  sha1 'd59ef579c7614c5df9bd69cfdce20ed371f728ff'
+  url 'https://download.elasticsearch.org/logstash/logstash/logstash-1.5.0.tar.gz'
+  sha1 '9729c2d31fddaabdd3d8e94c34a6d1f61d57f94a'
 
   def install
-    inreplace %w{bin/logstash bin/logstash.lib.sh}, /^basedir=.*$/, "basedir=#{libexec}"
+    inreplace %w{bin/logstash}, /^\. "\$\(cd `dirname \$0`\/\.\.; pwd\)\/bin\/logstash\.lib\.sh\"/, ". #{libexec}/bin/logstash.lib.sh"
+    inreplace %w{bin/logstash.lib.sh}, /^LOGSTASH_HOME=.*$/, "LOGSTASH_HOME=#{libexec}"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/logstash"
   end
 
   def caveats; <<-EOS.undent
-    Logstash 1.4 emits an unhelpful error if you try to start it without config.
+    Logstash 1.5 emits an unhelpful error if you try to start it without config.
     Please read the getting started guide located at:
-    http://logstash.net/docs/latest/tutorials/getting-started-with-logstash
+    https://www.elastic.co/guide/en/logstash/current/getting-started-with-logstash.html
     EOS
   end
 
