4e7a1aff72a0ba14ee280c8a039818036ccee516
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-25 01:07:30 +0100

    fswatch: update 1.4.7 bottle.

--- Library/Formula/fswatch.rb
+++ Library/Formula/fswatch.rb
@@ -6,9 +6,9 @@ class Fswatch < Formula
   sha1 "bfc41da5a56ab810b28945506a0ad4198b13ffef"
 
   bottle do
-    sha1 "11c520a2f84b858ed4b7f1af0390cc35b2a0572c" => :yosemite
-    sha1 "6fa6c0691c4156ea0afd4eb7bd3e8ba9635140e1" => :mavericks
-    sha1 "355272841f59a7ab5cb248acc48940bec005e5f8" => :mountain_lion
+    sha256 "1263ca1127a202ffd678803608d2620af5c43a4a8b3db1d987147df2b8a0cf7a" => :yosemite
+    sha256 "ce054437060df774bd2a0d8587e5fd8230ecd738b3a06471c5c0cb4ec8713557" => :mavericks
+    sha256 "76e5f7ee8a312b6f9b21e5e4f547586d2788b74c7467bcf5e4596300af3a4346" => :mountain_lion
   end
 
   needs :cxx11
