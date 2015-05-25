5ce594e0bbb6e38caedde04747da804cd5802a6a
Tomasz Pajor <tomek@polishgeeks.com>
2015-05-25 14:34:06 +0200

    awscli 1.7.29

--- Library/Formula/awscli.rb
+++ Library/Formula/awscli.rb
@@ -1,7 +1,7 @@
 class Awscli < Formula
   homepage "https://aws.amazon.com/cli/"
-  url "https://pypi.python.org/packages/source/a/awscli/awscli-1.7.25.tar.gz"
-  sha256 "48b46892c1b95b6467335145fd8db2fdab6a6cba14c481dfde4fe5af7114b3b4"
+  url "https://pypi.python.org/packages/source/a/awscli/awscli-1.7.29.tar.gz"
+  sha256 "1a5020149987afac863f795c89ad67a2c7162d7e4be8b2f15aa810aab4c218c8"
 
   bottle do
     cellar :any
@@ -36,8 +36,8 @@ class Awscli < Formula
   end
 
   resource "python-dateutil" do
-    url "https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.4.1.post1.tar.gz"
-    sha256 "aa9bdbd60c395db90204609f1fb5aeb3797870f65c09f04f243476d22f8f4615"
+    url "https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.4.2.tar.gz"
+    sha256 "3e95445c1db500a344079a47b171c45ef18f57d188dffdb0e4165c71bea8eb3d"
   end
 
   resource "colorama" do
@@ -51,8 +51,8 @@ class Awscli < Formula
   end
 
   resource "botocore" do
-    url "https://pypi.python.org/packages/source/b/botocore/botocore-0.106.0.tar.gz"
-    sha256 "e0e56faedbd2bf149ebb77a96d74c2fd5a74f91bc265d2750f24d6e1db58a6b0"
+    url "https://pypi.python.org/packages/source/b/botocore/botocore-1.0.0a1.tar.gz"
+    sha256 "b55beefa3020e1f00c5ffeaf32ec5f1148b2aef57086634f61773b4011c9cb3b"
   end
 
   resource "docutils" do
