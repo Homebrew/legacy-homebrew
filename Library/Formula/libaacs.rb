50b1bd46942d844820452cdc74211a911148839c
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-27 00:13:12 +0200

    libaacs: use ssl/tls url (was: ftp)
    Closes #40114.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libaacs.rb
+++ Library/Formula/libaacs.rb
@@ -1,6 +1,6 @@
 class Libaacs < Formula
   homepage "https://www.videolan.org/developers/libaacs.html"
-  url "ftp://ftp.videolan.org/pub/videolan/libaacs/0.8.1/libaacs-0.8.1.tar.bz2"
+  url "https://download.videolan.org/pub/videolan/libaacs/0.8.1/libaacs-0.8.1.tar.bz2"
   mirror "http://videolan-nyc.defaultroute.com/libaacs/0.8.1/libaacs-0.8.1.tar.bz2"
   sha256 "95c344a02c47c9753c50a5386fdfb8313f9e4e95949a5c523a452f0bcb01bbe8"
 
