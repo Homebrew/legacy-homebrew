820c256fb2704c92b4fcba68d914cdfb8ff11f5a
Marius Karnauskas <cofkie@gmail.com>
2015-05-27 02:19:35 +0100

    typo fix

--- Library/Formula/sshuttle.rb
+++ Library/Formula/sshuttle.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Sshuttle < Formula
   homepage 'https://github.com/sshuttle/sshuttle'
-  url "https://github.com/sshuttle/sshuttle/archive/sshuttle-0.71.tar.gz"
+  url 'https://github.com/sshuttle/sshuttle/archive/sshuttle-0.71.tar.gz'
   sha256 '62f0f8be5497c2d0098238c54e881ac001cd84fce442c2506ab6d37aa2f698f0'
 
   head 'https://github.com/sshuttle/sshuttle.git'
