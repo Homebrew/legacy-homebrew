58db4b5004f966a70bd84739d509516532a7e4d0
Baptiste Fontaine <batifon@yahoo.fr>
2015-05-28 00:27:10 +0200

    gti: test added
    Closes #40147.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/gti.rb
+++ Library/Formula/gti.rb
@@ -1,14 +1,17 @@
-require 'formula'
-
 class Gti < Formula
-  homepage 'http://r-wos.org/hacks/gti'
-  url 'https://github.com/rwos/gti/archive/v1.2.0.tar.gz'
-  sha1 'c24e662486ccb4065ad36cab455d864cb17ecfc4'
+  homepage "http://r-wos.org/hacks/gti"
+  url "https://github.com/rwos/gti/archive/v1.2.0.tar.gz"
+  sha256 "e76a3d44610c1445263860391f8243cde622f58945101075f750e96d96063762"
 
-  head 'https://github.com/rwos/gti.git'
+  head "https://github.com/rwos/gti.git"
 
   def install
-    system 'make', "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
-    bin.install 'gti'
+    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
+    bin.install "gti"
+    man6.install "gti.6"
+  end
+
+  test do
+    system "#{bin}/gti", "init"
   end
 end
