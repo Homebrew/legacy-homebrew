d20d7608161ee200485a667b569dfabdc1c75148
Robson Peixoto <robsonpeixoto@gmail.com>
2015-05-27 15:35:02 -0300

    plantuml 8024
    Closes #40140.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/plantuml.rb
+++ Library/Formula/plantuml.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Plantuml < Formula
   homepage "http://plantuml.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/plantuml/plantuml.8018.jar"
-  sha1 "0a2e88c09812b6636bcb5acb6507a0462dbe08af"
+  url "https://downloads.sourceforge.net/project/plantuml/plantuml.8024.jar"
+  sha256 "03aa4a1ead12aa349575e32889cac5a38cb65c658138e75d31b26191bdfd8ada"
 
   depends_on "graphviz"
 
