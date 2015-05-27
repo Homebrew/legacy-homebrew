265ba1f4aa81f217a9242316b5e7a9c409661f55
Xu Cheng <xucheng@me.com>
2015-05-27 20:40:32 +0800

    caveats: use Formula#full_name

--- Library/Homebrew/caveats.rb
+++ Library/Homebrew/caveats.rb
@@ -124,16 +124,16 @@ class Caveats
       # https://github.com/Homebrew/homebrew/issues/33815
       if !plist_path.file? || !plist_path.symlink?
         if f.plist_startup
-          s << "To have launchd start #{f.name} at startup:"
+          s << "To have launchd start #{f.full_name} at startup:"
           s << "    sudo mkdir -p #{destination}" unless destination_path.directory?
           s << "    sudo cp -fv #{f.opt_prefix}/*.plist #{destination}"
           s << "    sudo chown root #{plist_link}"
         else
-          s << "To have launchd start #{f.name} at login:"
+          s << "To have launchd start #{f.full_name} at login:"
           s << "    mkdir -p #{destination}" unless destination_path.directory?
           s << "    ln -sfv #{f.opt_prefix}/*.plist #{destination}"
         end
-        s << "Then to load #{f.name} now:"
+        s << "Then to load #{f.full_name} now:"
         if f.plist_startup
           s << "    sudo launchctl load #{plist_link}"
         else
@@ -142,17 +142,17 @@ class Caveats
       # For startup plists, we cannot tell whether it's running on launchd,
       # as it requires for `sudo launchctl list` to get real result.
       elsif f.plist_startup
-          s << "To reload #{f.name} after an upgrade:"
+          s << "To reload #{f.full_name} after an upgrade:"
           s << "    sudo launchctl unload #{plist_link}"
           s << "    sudo cp -fv #{f.opt_prefix}/*.plist #{destination}"
           s << "    sudo chown root #{plist_link}"
           s << "    sudo launchctl load #{plist_link}"
       elsif Kernel.system "/bin/launchctl list #{plist_domain} &>/dev/null"
-          s << "To reload #{f.name} after an upgrade:"
+          s << "To reload #{f.full_name} after an upgrade:"
           s << "    launchctl unload #{plist_link}"
           s << "    launchctl load #{plist_link}"
       else
-          s << "To load #{f.name}:"
+          s << "To load #{f.full_name}:"
           s << "    launchctl load #{plist_link}"
       end
 
