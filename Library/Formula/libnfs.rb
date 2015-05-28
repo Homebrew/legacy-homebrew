4b8a94a5e40fa71d90878ab07ae5c79348e7a7a7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-28 01:08:39 +0100

    libnfs: add 1.9.7 bottle.

--- Library/Formula/libnfs.rb
+++ Library/Formula/libnfs.rb
@@ -3,6 +3,12 @@ class Libnfs < Formula
   url "https://github.com/sahlberg/libnfs/archive/libnfs-1.9.7.tar.gz"
   sha256 "7c2e088f5fd85b791ab644a5221b717894208bc5fb8b8a5a49633802ecaa0990"
 
+  bottle do
+    cellar :any
+    sha256 "5e5b0bef16c703596a928b0ff474264745f2b33a86041af948a2736f41713a5e" => :yosemite
+    sha256 "7e89a3a33331387578ae4ef53037960c689e19c2f4a788f7763841d3036e8e0d" => :mavericks
+  end
+
   depends_on "libtool" => :build
   depends_on "automake" => :build
   depends_on "autoconf" => :build
