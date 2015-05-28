e6606b252ca43cf5e07b981b55440ad1a5a89f38
Xu Cheng <xucheng@me.com>
2015-05-28 19:13:56 +0800

    uses: use Formula#full_name

--- Library/Homebrew/cmd/uses.rb
+++ Library/Homebrew/cmd/uses.rb
@@ -25,8 +25,8 @@ module Homebrew
             reqs = f.recursive_requirements do |dependent, req|
               Requirement.prune if ignores.any? { |ignore| req.send(ignore) } && !dependent.build.with?(req)
             end
-            deps.any? { |dep| dep.to_formula.name == ff.name } ||
-              reqs.any? { |req| req.name == ff.name || req.class.default_formula == ff.name }
+            deps.any? { |dep| dep.to_formula.full_name == ff.full_name } ||
+              reqs.any? { |req| req.name == ff.name || [ff.name, ff.full_name].include?(req.class.default_formula) }
           else
             deps = f.deps.reject do |dep|
               ignores.any? { |ignore| dep.send(ignore) }
@@ -34,8 +34,8 @@ module Homebrew
             reqs = f.requirements.reject do |req|
               ignores.any? { |ignore| req.send(ignore) }
             end
-            deps.any? { |dep| dep.to_formula.name == ff.name } ||
-              reqs.any? { |req| req.name == ff.name || req.class.default_formula == ff.name }
+            deps.any? { |dep| dep.to_formula.full_name == ff.full_name } ||
+              reqs.any? { |req| req.name == ff.name || [ff.name, ff.full_name].include?(req.class.default_formula) }
           end
         rescue FormulaUnavailableError
           # Silently ignore this case as we don't care about things used in
@@ -44,6 +44,6 @@ module Homebrew
       end
     end
 
-    puts_columns uses.map(&:name)
+    puts_columns uses.map(&:full_name)
   end
 end
