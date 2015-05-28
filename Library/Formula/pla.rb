941e01c12f7e1ce82e762d77ad17d1c4eb0339f5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-28 16:34:59 +0100

    pla: add 1.1 bottle.

--- Library/Formula/pla.rb
+++ Library/Formula/pla.rb
@@ -4,6 +4,13 @@ class Pla < Formula
   url "http://www.arpalert.org/src/pla-1.1.tar.gz"
   sha256 "a213c5e1060c97618d0eb0462cfdf738532591b30f18b36a37c1a4398346ac37"
 
+  bottle do
+    cellar :any
+    sha256 "03a5a838c567433c41306ca1b8f1464612647c5348aa82475f2cb4e153701404" => :yosemite
+    sha256 "57cd25f3878fd390b1387883632f6d2368337a189c8cf1481d74141716b646f1" => :mavericks
+    sha256 "956bf4efa39b1ef9e29c17be02e444dfdf2bac81ba73235a8be72904427989c9" => :mountain_lion
+  end
+
   depends_on "cairo"
 
   def install
