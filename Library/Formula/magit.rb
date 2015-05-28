5e895335ff54c3d209cf3b9eacdb1b1689a7a780
Alex Dunn <dunn.alex@gmail.com>
2015-05-28 13:43:38 -0700

    magit 1.4.2
    Closes #40590.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/magit.rb
+++ Library/Formula/magit.rb
@@ -1,8 +1,34 @@
 class Magit < Formula
   desc "Emacs interface for Git"
   homepage "https://github.com/magit/magit"
-  url "https://github.com/magit/magit/releases/download/1.4.1/magit-1.4.1.tar.gz"
-  sha256 "e7b18d2acb7b79a622017726d7b2e48fcb893f3055ecd43b3672175599f97b2d"
+
+  stable do
+    url "https://github.com/magit/magit/archive/1.4.2.tar.gz"
+    sha256 "460040e32a47a04222ea3bc3f78434308d8b0c6e82b03bfe3e6579068c54a2ab"
+
+    depends_on :emacs => "23.2"
+
+    resource "git-commit-mode" do
+      url "https://github.com/magit/git-modes/raw/3423997a89f63eb4c8a4ce495928bc5951767932/git-commit-mode.el"
+      sha256 "ed33f324e46ab81232bed5c38c4f8f794bd689f58aa49e98e386b628182b32e0"
+    end
+
+    resource "git-rebase-mode" do
+      url "https://github.com/magit/git-modes/raw/3423997a89f63eb4c8a4ce495928bc5951767932/git-rebase-mode.el"
+      sha256 "21670e2dcabadc18f2c2caff9b97d7affe1697a64d522a40fce6d8f1f5cd5ea5"
+    end
+  end
+
+  head do
+    url "https://github.com/magit/magit.git", :branch => "next"
+
+    depends_on :emacs => "24.4"
+
+    resource "dash" do
+      url "http://melpa.org/packages/dash-20150611.922.el"
+      sha256 "9a4de6d5adf5a976c4bca3d6c8ac653ba8c859b23e01b4e38e27820104c43043"
+    end
+  end
 
   bottle do
     cellar :any
@@ -11,28 +37,22 @@ class Magit < Formula
     sha256 "e4db292d7cc955b343cbacec8e0d1ed02195b4e0992945cff0bac38ce6c7ea32" => :mountain_lion
   end
 
-  # see https://github.com/magit/magit/tree/master#its-magit--a-git-porcelain-inside-emacs
-  # will require 24.4 upon release of magit 2.1.0
-  depends_on :emacs => "23.2"
-
-  # remove at 2.1.0
-  resource "git-commit-mode" do
-    url "https://github.com/magit/git-modes/raw/3423997a89f63eb4c8a4ce495928bc5951767932/git-commit-mode.el"
-    sha256 "ed33f324e46ab81232bed5c38c4f8f794bd689f58aa49e98e386b628182b32e0"
-  end
-
-  # remove at 2.1.0
-  resource "git-rebase-mode" do
-    url "https://github.com/magit/git-modes/raw/3423997a89f63eb4c8a4ce495928bc5951767932/git-rebase-mode.el"
-    sha256 "21670e2dcabadc18f2c2caff9b97d7affe1697a64d522a40fce6d8f1f5cd5ea5"
-  end
-
   def install
-    buildpath.install resource("git-commit-mode"),
-                      resource("git-rebase-mode")
-    system "make", "install", "PREFIX=#{prefix}"
-    (share/"emacs/site-lisp").install "git-commit-mode.el",
-                                      "git-rebase-mode.el"
+    if build.stable?
+      buildpath.install resource("git-commit-mode"),
+                        resource("git-rebase-mode")
+      system "make", "install", "PREFIX=#{prefix}"
+      (share/"emacs/site-lisp").install "git-commit-mode.el",
+                                        "git-rebase-mode.el"
+    else
+      # Can't run `make install` alone without ENV.j1:
+      # https://github.com/magit/magit/issues/1670
+      system "make"
+      system "make", "install", "PREFIX=#{prefix}"
+      resource("dash").stage do
+        (share/"emacs/site-lisp").install "dash-20150611.922.el" => "dash.el"
+      end
+    end
   end
 
   def caveats; <<-EOS.undent
