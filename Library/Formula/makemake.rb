0d3d2f15c7ef7f9fee3033fabcdbc2034c2634c9
juloo <juloo.dsi@gmail.com>
2015-05-30 01:33:37 +0200

    Add Revision

--- Library/Formula/makemake.rb
+++ Library/Formula/makemake.rb
@@ -1,6 +1,6 @@
 class Makemake < Formula
   homepage "https://github.com/Julow/makemake"
-  url "https://github.com/Julow/makemake.git", :tag => "1.0-release"
+  url "https://github.com/Julow/makemake.git", :tag => "1.0-release", :revision => "f3ebda821b4e859cfdd20b579d1074c86330d14f"
   head "https://github.com/Julow/makemake.git"
   sha256 "47b79f953e39db26d4cdd612572c52e9a06b7899025ef53ed874baa4a7a03b23"
   version "1.0"
