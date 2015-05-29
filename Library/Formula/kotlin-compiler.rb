3e63eccf0aab2b1e32cccc2e35b15ad8b1832c22
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-29 20:02:13 +0100

    kotlin-compiler: add 0.12.200 bottle.

--- Library/Formula/kotlin-compiler.rb
+++ Library/Formula/kotlin-compiler.rb
@@ -3,6 +3,13 @@ class KotlinCompiler < Formula
   url "https://github.com/JetBrains/kotlin/releases/download/build-0.12.200/kotlin-compiler-0.12.200.zip"
   sha256 "d14a74859698c2a88f004dfb7dd35d35234bc5b99f452694159f2db1ed5a1060"
 
+  bottle do
+    cellar :any
+    sha256 "1dd707c9f621a243bb2668bed5051ecbf93da2c3090a5f287f609a2aebe343bc" => :yosemite
+    sha256 "5441d3359bda051c1dcd9f64a7534e54932a09e99722efb609bbaddf1e274712" => :mavericks
+    sha256 "d624292dae9a2ddb7b9f14eaad749d8f1c38465976a64937993ba281baf6eb95" => :mountain_lion
+  end
+
   def install
     libexec.install %w[bin lib]
     rm Dir["#{libexec}/bin/*.bat"]
