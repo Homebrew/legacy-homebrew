0efaabb9e035cd0668ae67b33dec7420d9fbdcec
juloo <juloo.dsi@gmail.com>
2015-05-30 01:20:39 +0200

    Fix things

--- Library/Formula/makemake.rb
+++ Library/Formula/makemake.rb
@@ -1,27 +1,15 @@
-# makemake formula
-
 class Makemake < Formula
-
   homepage "https://github.com/Julow/makemake"
-
-  url "https://github.com/Julow/makemake.git", :revision => "a11bba0903aad98765b7fdac0a67ed09903ba669"
+  url "https://github.com/Julow/makemake.git", :tag => "1.0-release"
   head "https://github.com/Julow/makemake.git"
-  # :using => :git is "Redundant"
-
   sha256 "47b79f953e39db26d4cdd612572c52e9a06b7899025ef53ed874baa4a7a03b23"
   version "1.0"
-
   def install
-
     mv "makemake.py", "makemake"
     bin.install "makemake"
-
   end
-
   test do
-
-    system "echo", "Makemake is an interative program"
-    system "makemake", "-h"
-
+    system "makemake", "--test"
+    system "cat", "Makefile"
   end
 end
