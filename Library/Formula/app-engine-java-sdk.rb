ff92181348b201e3d66317e15a4f137fab345237
Matt Newman <mdjnewman@gmail.com>
2015-05-30 21:45:50 +1000

    app-engine-java-sdk 1.9.21, using sha256 instead of sha1

--- Library/Formula/app-engine-java-sdk.rb
+++ Library/Formula/app-engine-java-sdk.rb
@@ -3,7 +3,7 @@ require "formula"
 class AppEngineJavaSdk < Formula
   homepage "https://developers.google.com/appengine/docs/java/gettingstarted/introduction"
   url "https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.21.zip"
-  sha1 "a405c32aa72d90c404fff0cab59624bf44d9c43d"
+  sha256 "1c1a107330ab45945b4ca3e787dd83be23d0aaf1d177ca30857208d5aec8ac96"
 
   def install
     rm Dir["bin/*.cmd"]
