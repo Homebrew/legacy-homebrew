72e705cdb77d0ab2f964bc3b741c3b8b60d84bfc
Baptiste Fontaine <batifon@yahoo.fr>
2015-05-30 13:28:18 +0200

    rc: use sha256
    Closes #40213.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/rc.rb
+++ Library/Formula/rc.rb
@@ -1,16 +1,14 @@
-require 'formula'
-
 class Rc < Formula
-  homepage 'http://doc.cat-v.org/plan_9/4th_edition/papers/rc'
-  url 'ftp://rc.quanstro.net/pub/rc-1.7.2.tgz'
-  sha1 '9e51d99677244af1768ff8dd2cbac4ac162350c1'
+  homepage "http://doc.cat-v.org/plan_9/4th_edition/papers/rc"
+  url "ftp://rc.quanstro.net/pub/rc-1.7.2.tgz"
+  sha256 "89487c6c9f5ad0fdae658bf9564f6e920935bbdeabf63abdbf69b7c46f7ee40c"
 
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--with-editline"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 
   test do
