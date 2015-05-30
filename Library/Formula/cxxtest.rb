0fa7e71f28b196967f97ae2ca2324c13fc8469ba
Dmitry S. Dolzhenko <dmitrys.dolzhenko@gmail.com>
2015-05-31 01:59:07 +0300

    cxxtest 4.3: update test

--- Library/Formula/cxxtest.rb
+++ Library/Formula/cxxtest.rb
@@ -11,6 +11,21 @@ class Cxxtest < Formula
   end
 
   test do
-    system "#{bin}/cxxtestgen", "--version"
+    testfile = testpath/"MyTestSuite1.h"
+    testfile.write <<-EOS.undent
+      #include <cxxtest/TestSuite.h>
+      
+      class MyTestSuite1 : public CxxTest::TestSuite {
+      public:
+          void testAddition(void) {
+              TS_ASSERT(1 + 1 > 1);
+              TS_ASSERT_EQUALS(1 + 1, 2);
+          }
+      };
+    EOS
+    
+    system bin/"cxxtestgen", "--error-printer", "-o", testpath/"runner.cpp", testfile
+    system "g++", "-o", testpath/"runner", testpath/"runner.cpp"
+    system testpath/"runner"
   end
 end
