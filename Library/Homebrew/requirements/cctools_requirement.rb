cad81c5cf02d954f29bc9cc3556d33f76a830b87
William Woodruff <william@tuffbizz.com>
2015-05-29 20:14:08 -0400

    homebrew handles the message and constructor for us in this case

--- Library/Homebrew/requirements/cctools_requirement.rb
+++ Library/Homebrew/requirements/cctools_requirement.rb
@@ -2,25 +2,7 @@ class CctoolsRequirement < Requirement
   fatal true
   default_formula 'cctools'
 
-  def initialize(tags)
-    super
-  end
-
   satisfy do
-    satisfied = false
-
-    if MacOS::XCode.installed? || MacOS::CLT.installed?
-      satisfied = true
-    elsif which('install_name_tool') && which('otool')
-      satisfied = true
-    else
-      satisfied = false
-    end
-
-    satisfied
-  end
-
-  def message
-    'cctools are required (via XCode, CLT, or Homebrew).'
+    MacOS::XCode.installed? || MacOS::CLT.installed?
   end
 end
