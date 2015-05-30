1cb88e1d85d23c92222275069b6b59cc5edb797a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-30 14:51:17 +0100

    rc: add 1.7.2 bottle.

--- Library/Formula/rc.rb
+++ Library/Formula/rc.rb
@@ -3,6 +3,13 @@ class Rc < Formula
   url "ftp://rc.quanstro.net/pub/rc-1.7.2.tgz"
   sha256 "89487c6c9f5ad0fdae658bf9564f6e920935bbdeabf63abdbf69b7c46f7ee40c"
 
+  bottle do
+    cellar :any
+    sha256 "1628bbad2fa8417318ee488a748a1ae769606baf950300c895e4592cbe013edf" => :yosemite
+    sha256 "6cd2807091b6e1e8359ebedab8d211f00d0ac84825f16d116bd4500c6dd5b3f4" => :mavericks
+    sha256 "c09ab4ed5d45ef028bb235e246e32b7e5d0603952a7c9e068ba64faf78e16cf1" => :mountain_lion
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
