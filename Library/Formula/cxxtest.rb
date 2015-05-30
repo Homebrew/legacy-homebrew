f86f698e65eb35ad5384874b08d294a280e76061
Dmitry S. Dolzhenko <dmitrys.dolzhenko@gmail.com>
2015-05-31 00:46:20 +0300

    cxxtest 4.3: remove redundant property "version"

--- Library/Formula/cxxtest.rb
+++ Library/Formula/cxxtest.rb
@@ -2,11 +2,10 @@ class Cxxtest < Formula
   desc "CxxTest Unit Testing Framework"
   homepage "http://cxxtest.com"
   url "https://github.com/CxxTest/cxxtest/archive/4.3.tar.gz"
-  version "4.3"
   sha256 "356d0f4810e8eb5c344147a0cca50fc0d84122c286e7644b61cb365c2ee22083"
 
   def install
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/cxxtestgen"
     include.install_symlink "#{libexec}/cxxtest"
   end
