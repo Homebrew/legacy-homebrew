add8061148dce139393f558e48b2895d73480873
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-31 20:25:04 +0100

    yafc: add 1.3.6 bottle.

--- Library/Formula/yafc.rb
+++ Library/Formula/yafc.rb
@@ -3,6 +3,12 @@ class Yafc < Formula
   url "http://www.yafc-ftp.com/upload/yafc-1.3.6.tar.xz"
   sha256 "63a32556ed16a589634a5aca3f03134b8d488e06ecb36f9df6ef450aa3a47973"
 
+  bottle do
+    sha256 "98f7b7f7771dfe2d9c8e992ce9b31ee5d5734d056642a9dc8e1373f2a62ce0e0" => :yosemite
+    sha256 "f50af31afd208189012ab71a498c2057276a12586e4b460aabd976e0ddcef8de" => :mavericks
+    sha256 "0342f6efbb4decd0031b0c9e06dbb82951465cbb64a0b96fbec3ea24652b94f1" => :mountain_lion
+  end
+
   depends_on "readline"
   depends_on "libssh" => :recommended
   depends_on "pkg-config" => :build
