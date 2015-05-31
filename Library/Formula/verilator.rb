806b39e6c87932d7ad3fd3f3730c284ab14fadf1
Alex Dunn <dunn.alex@gmail.com>
2015-05-31 11:13:07 -0700

    verilator 3.872
    Closes #40245.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/verilator.rb
+++ Library/Formula/verilator.rb
@@ -1,9 +1,14 @@
-require "formula"
-
 class Verilator < Formula
   homepage "http://www.veripool.org/wiki/verilator"
-  url "http://www.veripool.org/ftp/verilator-3.862.tgz"
-  sha1 "dd5d3f13478b51b0bd7d37ed353fcf58a6e774e1"
+  url "http://www.veripool.org/ftp/verilator-3.872.tgz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/v/verilator/verilator_3.872.orig.tar.gz"
+  sha256 "d00b7a4838bea9cf2d80d9693729f225c2369ab679f8f8fc4f5153e3f7517084"
+
+  head do
+    url "http://git.veripool.org/git/verilator", :using => :git
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
 
   skip_clean "bin" # Allows perl scripts to keep their executable flag
 
@@ -12,8 +17,38 @@ class Verilator < Formula
   depends_on "flex" if MacOS.version <= :lion
 
   def install
+    system "autoconf" if build.head?
     system "./configure", "--prefix=#{prefix}"
+    # `make` and `make install` need to be separate for parallel builds
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.v").write <<-EOS.undent
+      module test;
+         initial begin $display("Hello World"); $finish; end
+      endmodule
+    EOS
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include "Vtest.h"
+      #include "verilated.h"
+      int main(int argc, char **argv, char **env) {
+          Verilated::commandArgs(argc, argv);
+          Vtest* top = new Vtest;
+          while (!Verilated::gotFinish()) { top->eval(); }
+          delete top;
+          exit(0);
+      }
+    EOS
+    system "/usr/bin/perl", bin/"verilator", "-Wall", "--cc", "test.v", "--exe", "test.cpp"
+    cd "obj_dir" do
+      system "make", "-j", "-f", "Vtest.mk", "Vtest"
+      expected = <<-EOS.undent
+        Hello World
+        - test.v:2: Verilog $finish
+      EOS
+      assert_equal expected, shell_output("./Vtest")
+    end
   end
 end
