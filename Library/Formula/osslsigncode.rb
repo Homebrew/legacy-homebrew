97f722aa66dc242a16eda489c8d90109bec3f501
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-31 20:14:28 +0200

    osslsigncode 1.7.1
    Closes #40244.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/osslsigncode.rb
+++ Library/Formula/osslsigncode.rb
@@ -1,10 +1,7 @@
-require "formula"
-
 class Osslsigncode < Formula
-  homepage "http://sourceforge.net/projects/osslsigncode/"
-  url "https://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.6.tar.gz"
-  sha1 "83c169638c8c1e0122127674cbb73d2e0e6b5bc2"
-  revision 1
+  homepage "https://sourceforge.net/projects/osslsigncode/"
+  url "https://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.7.1.tar.gz"
+  sha256 "f9a8cdb38b9c309326764ebc937cba1523a3a751a7ab05df3ecc99d18ae466c9"
 
   bottle do
     cellar :any
@@ -15,6 +12,7 @@ class Osslsigncode < Formula
   end
 
   depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
   depends_on "openssl"
   depends_on "libgsf" => :optional
 
