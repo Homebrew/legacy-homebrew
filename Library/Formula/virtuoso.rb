29d9fd738eb874c7f54200024554fd32f168c3f0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-31 19:26:37 +0100

    virtuoso: add 7.2.0-1 bottle.

--- Library/Formula/virtuoso.rb
+++ Library/Formula/virtuoso.rb
@@ -5,6 +5,12 @@ class Virtuoso < Formula
   sha256 "fe0fc64c169fc59322142d4e4a90d1328f7063a07f9d9035aaf5adc9dde28302"
   version "7.2.0-1"
 
+  bottle do
+    sha256 "21d31984d4134baf0d2061ca3f3d41a4c150077e5de87b48fa1e5d4abd81bee6" => :yosemite
+    sha256 "866524f118b51e320accf5fa9c67ef33ccd5cc8631093ab6bce9c3a927cfef72" => :mavericks
+    sha256 "be2bebd5c58be3c6f947c801d4f68eefc51ceff1cd4959c279da98e6c12cccf6" => :mountain_lion
+  end
+
   head do
     url "https://github.com/openlink/virtuoso-opensource.git", :branch => "develop/7"
 
