93c58eaa4dd0e02ca1e244e0d9e10657ab930393
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-01 00:57:49 +0100

    hashpump: update 1.2.0 bottle.

--- Library/Formula/hashpump.rb
+++ Library/Formula/hashpump.rb
@@ -5,9 +5,9 @@ class Hashpump < Formula
 
   bottle do
     cellar :any
-    sha1 "b205ab988f9d104af20fc395167a2fd101acc2fb" => :mavericks
-    sha1 "38feeea2fae40cdeadee93423cc32d564775f8b7" => :mountain_lion
-    sha1 "0b6068bb47267c3df84c9fd51656e2760fba1cd8" => :lion
+    sha256 "4f6c7963fec55f5c8cd8ef37771d839551f0def45183f78b4644798c615ab311" => :yosemite
+    sha256 "5c5a3868482428e8ccfa12cbc4efad0e7a8027c82fe958106c8eed10310efaa3" => :mavericks
+    sha256 "cf4a9cee9d04bef03016073dc0e516e5349861691ad80bae1706cbd846b03920" => :mountain_lion
   end
 
   depends_on "openssl"
