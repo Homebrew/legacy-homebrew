59c9ac0cf70ef25d64f1d5e68a25a5fb5c1d76a2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-31 05:27:18 +0100

    git-crypt: update 0.5.0 bottle.

--- Library/Formula/git-crypt.rb
+++ Library/Formula/git-crypt.rb
@@ -5,9 +5,9 @@ class GitCrypt < Formula
 
   bottle do
     cellar :any
-    sha1 "f06679b30da507716f4db5fe06c8251e3702d5a7" => :yosemite
-    sha1 "bbe990ecebdefe37de0a46e4ad9f869e39a8076e" => :mavericks
-    sha1 "325e8630865b13f9d319d33777053921838d16e7" => :mountain_lion
+    sha256 "ce33f2d01af41259b6ea9be1e849000bdd08413b1f109268ea65709644d455eb" => :yosemite
+    sha256 "2cedd573983fe7ec7387e76f9ffd0ba351e71e19e3382f7365209d1aad0f7e3f" => :mavericks
+    sha256 "1bba33a973b90d39140a64193bcdab63b34c3b4f379850ee41ee155325173f4f" => :mountain_lion
   end
 
   depends_on "openssl"
