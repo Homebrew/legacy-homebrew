c7a17fb651aa790a27db69ea8493839e03670325
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-31 09:42:28 +0100

    libxdg-basedir: add 1.2.0 bottle.

--- Library/Formula/libxdg-basedir.rb
+++ Library/Formula/libxdg-basedir.rb
@@ -4,6 +4,13 @@ class LibxdgBasedir < Formula
   url "http://github.com/devnev/libxdg-basedir/archive/libxdg-basedir-1.2.0.tar.gz"
   sha256 "1c2b0032a539033313b5be2e48ddd0ae94c84faf21d93956d53562eef4614868"
 
+  bottle do
+    cellar :any
+    sha256 "7e165b0e949f559789981a5c0e0fd68bbf478943a0c9b03ad3778cecb0219691" => :yosemite
+    sha256 "5c7bfadf4ca8b26c077eea7480df5a4ca3634b5823860a06ce2756050acbe84a" => :mavericks
+    sha256 "63250688af20be1cb163530ea3849e4ca8b4ce93e15040904936414995af93f0" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
