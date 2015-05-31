3875a8ec119a6f725563be2439ea04aa20738d2c
Viktor Szakats <vszakats@users.noreply.github.com>
2015-05-31 13:47:10 +0200

    jetty: use ssl/tls homepage
    (homepage has mixed content)

--- Library/Formula/jetty.rb
+++ Library/Formula/jetty.rb
@@ -1,5 +1,5 @@
 class Jetty < Formula
-  homepage "http://www.eclipse.org/jetty/"
+  homepage "https://www.eclipse.org/jetty/"
   url "http://download.eclipse.org/jetty/9.2.11.v20150529/dist/jetty-distribution-9.2.11.v20150529.tar.gz"
   version "9.2.11.v20150529"
   sha256 "2dd7d03d49c44d25a0a852f6f40a8ea5548edd7f1ca33eb7e98386450c4d3ab5"
