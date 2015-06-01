a8d048861065f55c4a396efc9eafea5f685f35b8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-01 13:59:44 +0100

    cxxtest: add 4.3 bottle.

--- Library/Formula/cxxtest.rb
+++ Library/Formula/cxxtest.rb
@@ -4,6 +4,13 @@ class Cxxtest < Formula
   url "https://github.com/CxxTest/cxxtest/archive/4.3.tar.gz"
   sha256 "356d0f4810e8eb5c344147a0cca50fc0d84122c286e7644b61cb365c2ee22083"
 
+  bottle do
+    cellar :any
+    sha256 "adf5f38f015e2446c0b73afe5e0d4782a172f42d9cad5a6415f0714775fb7501" => :yosemite
+    sha256 "64309902d052b985325fd1372f76802f52434a005966bd0388241c2af6da83ff" => :mavericks
+    sha256 "e894f5ede73f1b13545c4db334c8e9ac1408fd43ca1316a48b38fc1d727d6cfb" => :mountain_lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
