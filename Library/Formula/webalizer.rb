2cc041230894a2d92571344034efbcd5b48e71b5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-02 06:14:26 +0100

    webalizer: update 2.23-08 bottle.

--- Library/Formula/webalizer.rb
+++ Library/Formula/webalizer.rb
@@ -5,9 +5,9 @@ class Webalizer < Formula
   sha256 "edaddb5aa41cc4a081a1500e3fa96615d4b41bc12086bcedf9938018ce79ed8d"
 
   bottle do
-    sha1 "4eebb723a0516001a63c0546a2e1c1561df07ad6" => :mavericks
-    sha1 "1a6a9fa2987ad8802c90b721998f290b7e11cfc0" => :mountain_lion
-    sha1 "308877c08ac3f82def55bd00f65772cc3c0c8805" => :lion
+    sha256 "65ef54216360d45dc10285e8e12f1381ad273093ad4e88c3d659d91f8f4d63eb" => :yosemite
+    sha256 "0910ecb43a791249be8eb075ea012868fea97d704f49a0d1ae3ec246ff169f4f" => :mavericks
+    sha256 "3677442a19563ad21e7f5e2b3d7e6335cd388a8e9ca0223d4012ee5787c3f503" => :mountain_lion
   end
 
   depends_on "gd"
