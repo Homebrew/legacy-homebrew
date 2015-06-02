456278a4c7e5bb0cd3313b1b6c0f843c77ba203c
Alex Dunn <dunn.alex@gmail.com>
2015-06-01 20:58:25 -0700

    web100clt 3.7.0.2
    Closes #40292.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/web100clt.rb
+++ Library/Formula/web100clt.rb
@@ -1,16 +1,20 @@
-require 'formula'
-
 class Web100clt < Formula
-  homepage 'http://www.internet2.edu/performance/ndt/'
-  url 'http://software.internet2.edu/sources/ndt/ndt-3.6.5.2.tar.gz'
-  sha1 '533a7dbb1b660a0148a0e295b481f63ab9ecb8f7'
+  homepage "http://software.internet2.edu/ndt/"
+  url "http://software.internet2.edu/sources/ndt/ndt-3.7.0.2.tar.gz"
+  sha256 "bd298eb333d4c13f191ce3e9386162dd0de07cddde8fe39e9a74fde4e072cdd9"
+
+  depends_on "i2util"
+  depends_on "jansson"
+  depends_on "openssl"
 
   # fixes issue with new default secure strlcpy/strlcat functions in 10.9
   # https://code.google.com/p/ndt/issues/detail?id=106
-  patch do
-    url "https://gist.githubusercontent.com/igable/8077668/raw/4475e6e653f080be111fa0a3fd649af42fa14c3d/ndt-3.6.5.2-osx-10.9.patch"
-    sha1 "1423d4b863049b21fcee4ecf9dd0660462406a39"
-  end if MacOS.version >= :mavericks
+  if MacOS.version >= :mavericks
+    patch do
+      url "https://gist.githubusercontent.com/igable/8077668/raw/4475e6e653f080be111fa0a3fd649af42fa14c3d/ndt-3.6.5.2-osx-10.9.patch"
+      sha256 "86d2399e3d139c02108ce2afb45193d8c1f5782996714743ec673c7921095e8e"
+    end
+  end
 
   def install
     system "./configure", "--disable-debug",
@@ -21,7 +25,7 @@ class Web100clt < Formula
     # we only want to build the web100clt client so we need
     # to change to the src directory before installing.
     system "make", "-C", "src", "install"
-    man1.install 'doc/web100clt.man' => 'web100clt.1'
+    man1.install "doc/web100clt.man" => "web100clt.1"
   end
 
   test do
