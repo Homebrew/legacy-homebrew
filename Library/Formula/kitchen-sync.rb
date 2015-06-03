93bb9a3348c77622a05c5c0647f175db2e47ac7d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-03 20:17:25 +0100

    kitchen-sync: add 0.38 bottle.

--- Library/Formula/kitchen-sync.rb
+++ Library/Formula/kitchen-sync.rb
@@ -5,6 +5,13 @@ class KitchenSync < Formula
   sha256 "786bf30caca4e58347c0880dc1ca54685cd01d0834024c425029fb5466c96041"
   head "https://github.com/willbryant/kitchen_sync.git"
 
+  bottle do
+    cellar :any
+    sha256 "3e51f0cdeefc74a5d3c87f9dd701c07adff75323ffd90a15424f21a3666d2ea0" => :yosemite
+    sha256 "da57b940d500ded730b4d9defaa197dab1c2e5e59b14cab5209243b21c7ff9ac" => :mavericks
+    sha256 "c7fd76c7ef6eef65ef0dbc69bc3063accf1f0262ebce4a5b8eb8fdc3f3515cb8" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on :mysql => :recommended
