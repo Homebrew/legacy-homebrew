c6753da864f5bbeacf0e561e83fba3fb7280afe8
Lars Fronius <l.fronius@googlemail.com>
2015-06-03 22:13:56 +0200

    update to drill 1.0.0

--- Library/Formula/apache-drill.rb
+++ Library/Formula/apache-drill.rb
@@ -1,16 +1,14 @@
 class ApacheDrill < Formula
   homepage "https://drill.apache.org/download/"
-  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-0.9.0/apache-drill-0.9.0.tar.gz"
-  sha256 "1680b4b937d26623f0f01d7719b0809decf3341f445f5caea3feed1ecbc16c4d"
+  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.0.0/apache-drill-1.0.0.tar.gz"
+  sha1 "0b43d52f6754bad7c7bb3c36e0dfc4c9de6d71e8"
 
   def install
     libexec.install Dir["*"]
-    ["drill_dumpcat", "runbit", "sqlline"].each do |bin_file|
-      bin.write_exec_script Dir["#{libexec}/bin/#{bin_file}"]
-    end
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
   end
 
   test do
-    system "#{bin}/sqlline <<< 'SELECT * FROM cp.`employee.json`;'"
+    system "#{bin}/sqlline -u jdbc:drill:zk=local <<< '!tables'"
   end
 end
