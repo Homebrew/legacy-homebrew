1ad63d4f701dc607526b6089838a63487bf76a8e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-03 22:14:07 +0100

    teapot: add 2.3.0 bottle.

--- Library/Formula/teapot.rb
+++ Library/Formula/teapot.rb
@@ -3,6 +3,13 @@ class Teapot < Formula
   url "http://www.syntax-k.de/projekte/teapot/teapot-2.3.0.tar.gz"
   sha256 "580e0cb416ae3fb3df87bc6e92e43bf72929d47b65ea2b50bc09acea3bff0b65"
 
+  bottle do
+    cellar :any
+    sha256 "02cfa676915adfa9702eab91be72c757bb250d492f446a7f3f084dcff2d63727" => :yosemite
+    sha256 "13998ff994a63d0e59fb99582c18aee845d760706591ae8e0f57036aa9f685d5" => :mavericks
+    sha256 "c130b452b7d339181fff49b4f5438de67c56402f81b9fffe10a154fa07351dcb" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   # The upstream tarball still defines the version number as "2.2.1", even
