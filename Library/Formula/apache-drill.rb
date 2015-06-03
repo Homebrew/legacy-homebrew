827168eb89ed37427fea951d1bd92e20c4adda6d
Lars Fronius <l.fronius@googlemail.com>
2015-06-03 22:13:56 +0200

    apache-drill 1.0.0
    Closes #40356.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/apache-drill.rb
+++ Library/Formula/apache-drill.rb
@@ -1,16 +1,14 @@
 class ApacheDrill < Formula
   homepage "https://drill.apache.org/download/"
-  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-0.9.0/apache-drill-0.9.0.tar.gz"
-  sha256 "1680b4b937d26623f0f01d7719b0809decf3341f445f5caea3feed1ecbc16c4d"
+  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.0.0/apache-drill-1.0.0.tar.gz"
+  sha256 "59f293aeb61ff55e3ab280dc3fbc9648dd6c1aefa2282a5a772be11d4f5a2682"
 
   def install
     libexec.install Dir["*"]
-    ["drill_dumpcat", "runbit", "sqlline"].each do |bin_file|
-      bin.write_exec_script Dir["#{libexec}/bin/#{bin_file}"]
-    end
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
   end
 
   test do
-    system "#{bin}/sqlline <<< 'SELECT * FROM cp.`employee.json`;'"
+    pipe_output("#{bin}/sqlline -u jdbc:drill:zk=local", "!tables", 0)
   end
 end
