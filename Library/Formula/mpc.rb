43d6a923de8a2cde9480547eaecf3b00ba02dbd1
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-04 03:49:53 +0100

    mpc: add 0.27 bottle.

--- Library/Formula/mpc.rb
+++ Library/Formula/mpc.rb
@@ -5,6 +5,13 @@ class Mpc < Formula
   url 'http://www.musicpd.org/download/mpc/0/mpc-0.27.tar.gz'
   sha1 '256926aa3ff8e9665a757d575bb962c094e4c352'
 
+  bottle do
+    cellar :any
+    sha256 "8cc20e2845ff22ed345dcdb1fe018d50e3ceb208193e1a4e7fc414bc5e825a42" => :yosemite
+    sha256 "0b2c7bf14d9ca916295e29784d8bc4d75637c86d9bf8d4c59713d54fcc6d9b31" => :mavericks
+    sha256 "c80bf07b0a0e3c30cb4d4dd4d4a3da5479c43b9a1be389ed8e164e5e618a8ac3" => :mountain_lion
+  end
+
   depends_on 'pkg-config' => :build
   depends_on 'libmpdclient'
 
