b2e05535f9263832bb9b848cd68bb309c21695a5
Michael Paquier <michael@otacoo.com>
2015-06-04 11:40:36 +0900

    mpc 0.27
    Closes #40362.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mpc.rb
+++ Library/Formula/mpc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mpc < Formula
   homepage 'http://www.musicpd.org/clients/mpc/'
-  url 'http://www.musicpd.org/download/mpc/0/mpc-0.26.tar.gz'
-  sha1 '56f434a0dae82d88a4bc24e50be7870ca20676a1'
+  url 'http://www.musicpd.org/download/mpc/0/mpc-0.27.tar.gz'
+  sha1 '256926aa3ff8e9665a757d575bb962c094e4c352'
 
   depends_on 'pkg-config' => :build
   depends_on 'libmpdclient'
