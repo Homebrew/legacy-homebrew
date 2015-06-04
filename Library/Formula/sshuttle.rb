0a1b175ee7dd479f9062efebf23e3e27cd61f2a5
Adam Huffman <bloch@verdurin.com>
2015-06-04 06:48:03 +0100

    Add test with forked process which checks network connectivity

--- Library/Formula/sshuttle.rb
+++ Library/Formula/sshuttle.rb
@@ -1,4 +1,6 @@
 require 'formula'
+# for test
+require 'open3'
 
 class Sshuttle < Formula
   homepage 'https://github.com/sshuttle/sshuttle'
@@ -14,4 +16,15 @@ class Sshuttle < Formula
     libexec.install Dir['src/*']
     bin.write_exec_script libexec/'sshuttle'
   end
+  test do
+    cmd = '#{bin}/sshuttle --server'
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      sleep 1
+      exit_status = wait_thr.value
+      unless exit_status.success?
+        abort "Test failed #{cmd}"
+      end
+    end
+  end
+
 end
