7f6ab8e534e4ee18506bc7dd53c5dd5c0aecc0ae
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-05 18:57:13 +0100

    qpdf: add 5.1.3 bottle.

--- Library/Formula/qpdf.rb
+++ Library/Formula/qpdf.rb
@@ -6,6 +6,12 @@ class Qpdf < Formula
   url "https://downloads.sourceforge.net/project/qpdf/qpdf/5.1.3/qpdf-5.1.3.tar.gz"
   sha256 "d5164bdad3afc381568dbe8e1509a4a6a911d4d077f1fc20b9866ef8fad901d3"
 
+  bottle do
+    sha256 "28e4e0e21d2de2e45dfb38afb53968f840b01480459590b2026c1c63803d5226" => :yosemite
+    sha256 "6439a662091505b0ba31e0f5c95a676ce934c758ea00284006b092b954a3955b" => :mavericks
+    sha256 "424d315f98dd4276884b66b3135fc04ee394a3235ae17cf65d4c596aa61f2506" => :mountain_lion
+  end
+
   depends_on "pcre"
 
   def install
