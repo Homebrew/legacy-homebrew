22bfa170a175257b6416b10688613ada6f94c123
Baptiste Fontaine <batifon@yahoo.fr>
2015-06-05 19:48:49 +0200

    colortail 0.3.4
    Closes #40428.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/colortail.rb
+++ Library/Formula/colortail.rb
@@ -1,16 +1,27 @@
 class Colortail < Formula
   desc "Like tail(1), but with various colors for specified output"
-  homepage "http://joakimandersson.se/projects/colortail/"
-  url "http://joakimandersson.se/files/colortail-0.3.3.tar.gz"
-  sha1 "2c6cf501758cbc0f8a46d0b2122839e5cb30fdfc"
+  homepage "https://github.com/joakim666/colortail"
+  url "https://github.com/joakim666/colortail.git",
+    :revision => "f44fce0dbfd6bd38cba03400db26a99b489505b5"
+  version "0.3.4"
+
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+
+  # Upstream PR to fix the build on ML
+  patch do
+    url "https://github.com/joakim666/colortail/pull/12.diff"
+    sha256 "2bb9963f6fc586c8faff3b51a48896cf09c68c4229c39c6ae978a59cb58d0fd7"
+  end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "./autogen.sh", "--disable-dependency-tracking",
+                           "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    (testpath/"test.txt").write("Hello\nWorld!\n")
-    assert_match /World!/, shell_output("#{bin}/colortail -n 1 test.txt")
+    (testpath/"test.txt").write "Hello\nWorld!\n"
+    assert_match(/World!/, shell_output("#{bin}/colortail -n 1 test.txt"))
   end
 end
