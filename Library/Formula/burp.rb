93f16fa06f609ced78a9ca91dff038199ffa5236
Baptiste Fontaine <batifon@yahoo.fr>
2015-06-05 02:16:02 +0200

    burp 2.0.20

--- Library/Formula/burp.rb
+++ Library/Formula/burp.rb
@@ -1,8 +1,8 @@
 class Burp < Formula
   desc "Network backup and restore"
   homepage "http://burp.grke.org/"
-  url "http://burp.grke.org/downloads/burp-1.4.28/burp-1.4.28.tar.bz2"
-  sha256 "01c41c07805ebe8c882d7cb9f294e779b6decbe2eb2c81fd65d8fb2b174e18f0"
+  url "https://github.com/grke/burp/archive/2.0.20.tar.gz"
+  sha256 "165310395c30032623032bb593596c88e2bc8cbfa98489c0e6995ad3beda1303"
   head "https://github.com/grke/burp.git"
 
   bottle do
@@ -16,10 +16,19 @@ class Burp < Formula
   depends_on "librsync"
   depends_on "openssl"
 
+  resource "uthash" do
+    url "https://raw.githubusercontent.com/troydhanson/uthash/ac47d4928e61c5abc6e977d91310d31ed74690e4/src/uthash.h"
+    sha256 "04dff825f3c79685b4908dd5461bc2a8ff03d9572f0f41e948b828e0c073e929"
+  end
+
   # patches to change directories to brew conventions in Makefile and config files
   patch :DATA
 
   def install
+    # we don't package uthash because it's only one header
+    resource("uthash").stage buildpath/"uthash-include"
+    ENV.append_to_cflags "-I#{buildpath}/uthash-include"
+
     system "./configure", "--prefix=#{prefix}",
                           "--sysconfdir=#{etc}/burp",
                           "--sbindir=#{bin}",
@@ -64,14 +73,20 @@ class Burp < Formula
     </plist>
     EOS
   end
+
+  def caveats; <<-EOS.undent
+    Before installing the launchd entry you should configure your burp client in
+    #{etc}/burp/burp.conf
+    EOS
+  end
 end
 
 __END__
 diff --git a/Makefile.in b/Makefile.in
-index ac22a24..0cf112b 100755
+index 8f93cf5..fcf95fc 100755
 --- a/Makefile.in
 +++ b/Makefile.in
-@@ -90,8 +90,8 @@ installdirs:
+@@ -102,8 +102,8 @@ installdirs:
  	$(MKDIR) $(DESTDIR)$(sbindir)
  	$(MKDIR) $(DESTDIR)$(sysconfdir)
  	$(MKDIR) $(DESTDIR)$(sysconfdir)/CA-client
@@ -79,63 +94,41 @@ index ac22a24..0cf112b 100755
 -	$(MKDIR) $(DESTDIR)/var/spool/burp
 +	$(MKDIR) HOMEBREW_PREFIX/var/run
 +	$(MKDIR) HOMEBREW_PREFIX/var/spool/burp
- 	@if [ ! -d $(DESTDIR)$(sysconfdir)/clientconfdir ] ; then $(MKDIR) $(DESTDIR)$(sysconfdir)/clientconfdir ; cp configs/server/clientconfdir/testclient $(DESTDIR)$(sysconfdir)/clientconfdir/testclient ; fi
- 	@if [ ! -d $(DESTDIR)$(sysconfdir)/clientconfdir/incexc ] ; then $(MKDIR) $(DESTDIR)$(sysconfdir)/clientconfdir/incexc ; cp configs/server/clientconfdir/incexc $(DESTDIR)$(sysconfdir)/clientconfdir/incexc/example ; fi
- 	@if [ ! -d $(DESTDIR)$(sysconfdir)/autoupgrade/client ] ; then $(MKDIR) $(DESTDIR)$(sysconfdir)/autoupgrade/client ; fi
+ 	$(MKDIR) $(DESTDIR)$(sysconfdir)/clientconfdir
+ 	$(MKDIR) $(DESTDIR)$(sysconfdir)/clientconfdir/incexc
+ 	$(MKDIR) $(DESTDIR)$(sysconfdir)/autoupgrade/client
 diff --git a/configs/client/burp.conf b/configs/client/burp.conf
-index 1b47476..76fcd8d 100644
+index f17eda6..d84ec32 100644
 --- a/configs/client/burp.conf
 +++ b/configs/client/burp.conf
-@@ -5,7 +5,7 @@ port = 4971
- server = 127.0.0.1
- password = abcdefgh
- cname = testclient
--pidfile = /var/run/burp.client.pid
+@@ -11,7 +11,7 @@ cname = testclient
+ # with a pseudo mirrored storage on the server and optional rsync). 2 forces
+ # protocol2 mode (inline deduplication with variable length blocks).
+ # protocol = 0
+-pidfile = @runstatedir@/burp.client.pid
 +pidfile = HOMEBREW_PREFIX/var/run/burp.client.pid
  syslog = 0
  stdout = 1
  progress_counter = 1
 diff --git a/configs/server/burp.conf b/configs/server/burp.conf
-index 45a4b70..c9081d0 100644
+index 1cc534f..fbda82a 100644
 --- a/configs/server/burp.conf
 +++ b/configs/server/burp.conf
-@@ -3,9 +3,9 @@
- mode = server
- port = 4971
+@@ -12,7 +12,7 @@ port = 4971
+ # If you do not wish to run a status server at all, comment status_port out.
  status_port = 4972
+ 
 -directory = /var/spool/burp
--clientconfdir = /etc/burp/clientconfdir
--pidfile = /var/run/burp.server.pid
 +directory = HOMEBREW_PREFIX/var/spool/burp
-+clientconfdir = HOMEBREW_PREFIX/etc/burp/clientconfdir
+ dedup_group = global
+ clientconfdir = @sysconfdir@/clientconfdir
+ # Choose the protocol to use.
+@@ -22,7 +22,7 @@ clientconfdir = @sysconfdir@/clientconfdir
+ # Like many other settings, this can be set per client in the clientconfdir
+ # files.
+ # protocol = 0
+-pidfile = @runstatedir@/burp.server.pid
 +pidfile = HOMEBREW_PREFIX/var/run/burp.server.pid
  hardlinked_archive = 0
  working_dir_recovery_method = delete
  max_children = 5
-diff --git a/configs/client/burp.conf b/configs/client/burp.conf
-index 76fcd8d..9cd7edb 100644
---- a/configs/client/burp.conf
-+++ b/configs/client/burp.conf
-@@ -41,17 +41,17 @@ cross_all_filesystems=0
- 
- # Uncomment the following lines to automatically generate a certificate signing
- # request and send it to the server.
--ca_burp_ca = /usr/sbin/burp_ca
--ca_csr_dir = /etc/burp/CA-client
-+ca_burp_ca = HOMEBREW_PREFIX/bin/burp_ca
-+ca_csr_dir = HOMEBREW_PREFIX/etc/burp/CA-client
- 
- # SSL certificate authority - same file on both server and client
--ssl_cert_ca = /etc/burp/ssl_cert_ca.pem
-+ssl_cert_ca = HOMEBREW_PREFIX/etc/burp/ssl_cert_ca.pem
- 
- # Client SSL certificate
--ssl_cert = /etc/burp/ssl_cert-client.pem
-+ssl_cert = HOMEBREW_PREFIX/etc/burp/ssl_cert-client.pem
- 
- # Client SSL key
--ssl_key = /etc/burp/ssl_cert-client.key
-+ssl_key = HOMEBREW_PREFIX/etc/burp/ssl_cert-client.key
- 
- # Client SSL ciphers
- #ssl_ciphers = 
