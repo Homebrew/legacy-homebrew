4fa92db2e5b6b50e504639f8805fd7a8710c08a2
Mike McQuaid <mike@mikemcquaid.com>
2015-06-05 10:29:06 +0100

    mysql: add sourceforge mirror.
    Oracle drops their sources quicker than SourceForge does. Also, we're
    using SHA256 if you're paranoid about SourceForge lately.

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -1,6 +1,7 @@
 class Mysql < Formula
   homepage "https://dev.mysql.com/doc/refman/5.6/en/"
   url "https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.25.tar.gz"
+  mirror "http://downloads.sourceforge.net/project/mysql.mirror/MySQL%205.6.25/mysql-5.6.25.tar.gz"
   sha256 "15079c0b83d33a092649cbdf402c9225bcd3f33e87388407be5cdbf1432c7fbd"
 
   bottle do
