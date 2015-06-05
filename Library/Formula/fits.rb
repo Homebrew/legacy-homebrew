f6fc31de209f7fb5897cbee05ca1f162802e677f
Baptiste Fontaine <batifon@yahoo.fr>
2015-06-05 21:00:37 +0200

    fits 0.8.6-1
    Closes #40434.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/fits.rb
+++ Library/Formula/fits.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class Fits < Formula
   desc "File Information Tool Set"
-  homepage 'http://fitstool.org/'
-  url 'http://projects.iq.harvard.edu/files/fits/files/fits-0.8.4_0.zip'
-  version '0.8.4'
-  sha1 'f9b6b13cf1e818c6cdcfec71eb3dcd6804dd8819'
+  homepage "http://projects.iq.harvard.edu/fits"
+  url "https://projects.iq.harvard.edu/files/fits/files/fits-0.8.6_1.zip"
+  version "0.8.6-1"
+  sha256 "d45f67a2606aaa0fdcbbade576f70f1590916b043fec28dcfdef1a8242fd4040"
 
   bottle do
     cellar :any
@@ -17,27 +15,32 @@ class Fits < Formula
   # provided jars may not be compatible with installed java,
   # but works when built from source
   depends_on "ant" => :build
-  depends_on :java => "1.8"
+  depends_on :java => "1.7+"
 
   def install
     system "ant"
 
-    inreplace 'fits-env.sh' do |s|
-      s.gsub!  "FITS_HOME=`echo \"$0\" | sed 's,/[^/]*$,,'`", "FITS_HOME=#{prefix}"
+    inreplace "fits-env.sh" do |s|
+      s.gsub! "FITS_HOME=`echo \"$0\" | sed 's,/[^/]*$,,'`", "FITS_HOME=#{prefix}"
       s.gsub! "${FITS_HOME}/lib", libexec
     end
 
-    prefix.install %w{ COPYING COPYING.LESSER tools xml }
-    prefix.install Dir['*.txt']
-    libexec.install Dir['lib/*']
+    prefix.install %w[COPYING COPYING.LESSER tools xml]
+    prefix.install Dir["*.txt"]
+    libexec.install Dir["lib/*"]
 
     # fits-env.sh is a helper script that sets up environment
     # variables, so we want to tuck this away in libexec
-    libexec.install 'fits-env.sh'
+    libexec.install "fits-env.sh"
     inreplace %w[fits.sh fits-ngserver.sh],
       '"$(dirname $BASH_SOURCE)/fits-env.sh"', "'#{libexec}/fits-env.sh'"
 
-    bin.install 'fits.sh' => 'fits'
-    bin.install 'fits-ngserver.sh' => 'fits-ngserver'
+    bin.install "fits.sh" => "fits"
+    bin.install "fits-ngserver.sh" => "fits-ngserver"
+  end
+
+  test do
+    assert_match 'mimetype="audio/mpeg"',
+      shell_output("#{bin}/fits -i #{test_fixtures "test.mp3"} 2>&1")
   end
 end
