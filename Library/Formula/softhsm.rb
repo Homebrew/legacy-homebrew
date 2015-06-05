7b41b419ec04ce092f087b6f781aedbcf34353ee
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-05 10:34:50 +0100

    softhsm: add 1.3.7 bottle.

--- Library/Formula/softhsm.rb
+++ Library/Formula/softhsm.rb
@@ -4,6 +4,12 @@ class Softhsm < Formula
   url "https://dist.opendnssec.org/source/softhsm-1.3.7.tar.gz"
   sha256 "d12d6456a85561266d9da427565f3ee3746a35df6670d5e6be75de253c2810a4"
 
+  bottle do
+    sha256 "2e93310b6db277071b4643e327954fe002270dbb7f9ca43080455eedb6c699c1" => :yosemite
+    sha256 "94147927ce8f7f263a89f5423e0c78263af2a2c6ee0e100e735d578b80c8a89e" => :mavericks
+    sha256 "5c85b169eb53ba6f4e86344b173d10b6ab6e983bd0638a4fd6273ae5ff8c703a" => :mountain_lion
+  end
+
   depends_on "botan"
 
   def install
