0c7eddd5ca3b85e5723a3f259e89033e0bcf6f04
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-06 22:11:58 +0100

    tcsh 6.19.00
    Closes #40462.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/tcsh.rb
+++ Library/Formula/tcsh.rb
@@ -1,25 +1,23 @@
-require "formula"
-
 class Tcsh < Formula
   desc "Enhanced, fully compatible version of the Berkeley C shell"
-  homepage 'http://www.tcsh.org/'
-  url 'ftp://ftp.astron.com/pub/tcsh/tcsh-6.18.01.tar.gz'
-  sha1 'eee2035645737197ff8059c84933a75d23cd76f9'
+  homepage "http://www.tcsh.org/"
+  url "ftp://ftp.astron.com/pub/tcsh/tcsh-6.19.00.tar.gz"
+  mirror "http://ftp.funet.fi/pub/mirrors/ftp.astron.com/pub/tcsh/tcsh-6.19.00.tar.gz"
+  sha256 "12e271e0b89e4259d9d6e8d525322e77340e7244cfbd199a591e5f8146285c49"
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
     system "make", "install"
   end
 
   test do
-    (testpath/'test.csh').write <<-EOS.undent
+    (testpath/"test.csh").write <<-EOS.undent
       #!#{bin}/tcsh
       set ARRAY=( "t" "e" "s" "t" )
       foreach i ( `seq $#ARRAY` )
         echo -n $ARRAY[$i]
       end
     EOS
-    assert_equal "test", `#{bin}/tcsh ./test.csh`
+    assert_equal "test", shell_output("#{bin}/tcsh ./test.csh")
   end
 end
