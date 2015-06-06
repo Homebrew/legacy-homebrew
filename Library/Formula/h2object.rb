2004dff00f1bf2b904695d7944c1b2f390547998
刘建平 <liujianping@h2object.io>
2015-06-06 21:01:32 +0800

    change the package

--- Library/Formula/h2object.rb
+++ Library/Formula/h2object.rb
@@ -3,7 +3,7 @@ require 'formula'
 class H2object < Formula
   homepage 'http://h2object.io'
   url 'http://dl.h2object.io/h2object/macosx/h2object-0.0.1-beta.tar.gz'
-  sha1 'b4a5ff6edab956df4128c3bab75b45dbbec125f2'
+  sha1 '588bab3d9372b433d15616a8b8bbe6d2720f27d6'
 
   def install
     bin.install 'h2object'
