ee5b3fd9384c1c6dd8e349b59ae0e0f123ed1d94
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-06 16:01:58 +0100

    aap: add 1.094 bottle.

--- Library/Formula/aap.rb
+++ Library/Formula/aap.rb
@@ -4,6 +4,12 @@ class Aap < Formula
   url "https://downloads.sourceforge.net/project/a-a-p/aap-1.094.zip"
   sha256 "3f53b2fc277756042449416150acc477f29de93692944f8a77e8cef285a1efd8"
 
+  bottle do
+    sha256 "b95b0d83504a5ec5c0d10143f2f85aa9fa21f394fda2cf14bebee8b1b643dd37" => :yosemite
+    sha256 "019cb29542a8d8250e22cc10e389d1a2159a9db99db90dc9392650b288025507" => :mavericks
+    sha256 "e496122256798dc62cd02ffdd0d72c7a1d682243c6743b1b7b0b88fc6b3bd34a" => :mountain_lion
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
