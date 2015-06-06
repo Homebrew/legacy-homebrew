6377abebab2f78ff59f46d80dac92589ff454a58
thomas <tgu@mac.com>
2015-06-06 12:53:25 +0200

    added disable-features to compile on a clean system

--- Library/Formula/owfs.rb
+++ Library/Formula/owfs.rb
@@ -2,16 +2,22 @@ require 'formula'
 
 class Owfs < Formula
   homepage 'http://owfs.org/'
-  url 'http://sourceforge.net/projects/owfs/files/owfs/3.1p0/owfs-3.1p0.tar.gz'
+  url 'http://downloads.sourceforge.net/project/projects/owfs/files/owfs/3.1p0/owfs-3.1p0.tar.gz'
   version '3.1p0'
   sha256 '62fca1b3e908cd4515c9eb499bf2b05020bbbea4a5b73611ddc6f205adec7a54'
 
   depends_on 'libusb-compat'
 
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
+                          "--disable-swig",
+                          "--disable-owfs",
                           "--disable-owtcl",
+                          "--disable-zero",
+                          "--disable-owpython",
+                          "--disable-owperl",
                           "--prefix=#{prefix}"
     system "make install"
   end
