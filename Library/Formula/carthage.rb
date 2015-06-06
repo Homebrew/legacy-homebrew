3c7b2ce7b35ad404720536228f9fae1baa2bb252
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-06-05 22:33:22 -0700

    carthage 0.7.4
    Closes #40450.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/carthage.rb
+++ Library/Formula/carthage.rb
@@ -1,8 +1,8 @@
 class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/Carthage/Carthage"
-  url "https://github.com/Carthage/Carthage.git", :tag => "0.7.3",
-                                                  :revision => "ad8fba1a091ae493281ab44953f1e8ca4e6c0405",
+  url "https://github.com/Carthage/Carthage.git", :tag => "0.7.4-homebrew",
+                                                  :revision => "199d3b3dfd5a5ce7aeb919eb9206b7faf1374e20",
                                                   :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
