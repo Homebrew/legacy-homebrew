6de191b7379912a7327b93c0f418b84f0d52f930
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-07 21:59:55 +0100

    dex: add 1.0 bottle.

--- Library/Formula/dex.rb
+++ Library/Formula/dex.rb
@@ -6,6 +6,12 @@ class Dex < Formula
 
   head "https://github.com/tihirvon/dex.git"
 
+  bottle do
+    sha256 "096db7c0595eede488f08321d97763360adb34aefb091cafba8e617e1e1c551e" => :yosemite
+    sha256 "c45e52da1f3bda6fa0c50f25eba8b746279abd26a0a032980aa4c2887501303f" => :mavericks
+    sha256 "6f2d7d8c6a1e54dbe6d815915ac0eb0100fd68426b3d91ec9d04a5356ca0a48b" => :mountain_lion
+  end
+
   depends_on "homebrew/dupes/ncurses" => :optional
   depends_on "homebrew/dupes/libiconv" => :optional
 
