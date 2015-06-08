49b511a6dbe88ba4a80f42c955b7e45902752f57
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-08 17:27:18 +0100

    gupnp-tools: add 0.8.10 bottle.

--- Library/Formula/gupnp-tools.rb
+++ Library/Formula/gupnp-tools.rb
@@ -4,6 +4,12 @@ class GupnpTools < Formula
   url "https://download.gnome.org/sources/gupnp-tools/0.8/gupnp-tools-0.8.10.tar.xz"
   sha256 "592c53289ff1cd70e676405c56ca87b28d2da37a385d34a3bb9b944ba9108d17"
 
+  bottle do
+    sha256 "f136c24128d85cf73a05cde6e7debbafe69cd11cb9a58c2b8a70d027b171af20" => :yosemite
+    sha256 "b1b64ff86b74b5bd9ccec60ab77eaa5f61ea593ee029f5e31bedb178422c741d" => :mavericks
+    sha256 "790821aad6ce8ea5b1b677372b1790133134ef3e7e2e4119ec3fcea2c7172aae" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "gettext"
