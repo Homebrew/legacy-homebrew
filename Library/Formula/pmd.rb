c9ac8c454aa861cd89b04b97efa948bd68de4ee1
Juan Pablo Civile <elementohb@gmail.com>
2015-06-08 14:38:50 -0300

    Unzip instead of patch

--- Library/Formula/pmd.rb
+++ Library/Formula/pmd.rb
@@ -7,16 +7,14 @@ class Pmd < Formula
 
   depends_on "maven" => :build
 
-  # The mvn package target produces a .zip with all the jars needed for PMD
-  # This patch makes it produce a folder instead so installing the jars is straightforward
-  patch :DATA
-
   def install
     system "mvn", "clean", "package"
 
     doc.install "LICENSE", "NOTICE", "README.md"
 
-    libexec.install "pmd-dist/target/pmd-bin-#{version}/pmd-bin-#{version}/bin", "pmd-dist/target/pmd-bin-#{version}/pmd-bin-#{version}/lib"
+    # The mvn package target produces a .zip with all the jars needed for PMD
+    with_system_path { safe_system 'unzip', '-qq', buildpath/"pmd-dist/target/pmd-bin-#{version}.zip" }
+    libexec.install "pmd-bin-#{version}/bin", "pmd-bin-#{version}/lib"
 
     bin.install_symlink "#{libexec}/bin/run.sh" => "pmd"
     inreplace "#{libexec}/bin/run.sh", "${script_dir}/../lib", "#{libexec}/lib"
@@ -45,17 +43,3 @@ class Pmd < Formula
   end
 end
 
-__END__
-diff --git a/pmd-dist/src/main/assembly/bin.xml b/pmd-dist/src/main/assembly/bin.xml
-index 2b44241..685badb 100755
---- a/pmd-dist/src/main/assembly/bin.xml
-+++ b/pmd-dist/src/main/assembly/bin.xml
-@@ -3,7 +3,7 @@
- 
-     <id>bin</id>
-     <formats>
--        <format>zip</format>
-+        <format>dir</format>
-     </formats>
- 
-     <includeBaseDirectory>true</includeBaseDirectory>
