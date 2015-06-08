5b6f21b3b8f3ef87763c6f251c50fd3bfd684703
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-08 12:52:22 +0100

    moco: add 0.10.1 bottle.

--- Library/Formula/moco.rb
+++ Library/Formula/moco.rb
@@ -5,6 +5,13 @@ class Moco < Formula
   version "0.10.1"
   sha256 "77996c0c5518beec0179fcdef83c2c606c5704073efc4ab6f9af80fc7756efbe"
 
+  bottle do
+    cellar :any
+    sha256 "c3ade71eea4010c60bc478e9a89fe888762b46f22855219148ede03bf952d62a" => :yosemite
+    sha256 "bd91b8f2dcc6d373a0d4e3a0639582c84c2b783927a1765543e5d333475cf02c" => :mavericks
+    sha256 "baf157a25988a5b80e3efeaa265d1f6032c183fa1e6a3fe8c3338e8246573b65" => :mountain_lion
+  end
+
   def install
     libexec.install "moco-runner-0.10.1-standalone.jar"
     bin.write_jar_script libexec/"moco-runner-0.10.1-standalone.jar", "moco"
