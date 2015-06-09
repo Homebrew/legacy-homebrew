9b0d706c48455509a26e51a68f4521cc2a27b345
Alex Dunn <dunn.alex@gmail.com>
2015-06-09 12:09:29 -0700

    yle-dl 2.7.2

--- Library/Formula/yle-dl.rb
+++ Library/Formula/yle-dl.rb
@@ -1,8 +1,8 @@
 class YleDl < Formula
   desc "Download Yle videos from the command-line"
   homepage "https://aajanki.github.io/yle-dl/index-en.html"
-  url "https://github.com/aajanki/yle-dl/archive/2.7.1.tar.gz"
-  sha1 "041f212d56a981e1c53f6bdf042a281db965d6ed"
+  url "https://github.com/aajanki/yle-dl/archive/2.7.2.tar.gz"
+  sha256 "7c4bea154ce91f171876c98d80d8a7cc56d0ffe925ed41302a09866226bdda92"
 
   head "https://github.com/aajanki/yle-dl.git"
 
@@ -19,13 +19,13 @@ class YleDl < Formula
   resource "AdobeHDS.php" do
     # NOTE: yle-dl always installs the HEAD version of AdobeHDS.php. We use a specific commit.
     # Check if there are bugfixes at https://github.com/K-S-V/Scripts/commits/master/AdobeHDS.php
-    url "https://raw.githubusercontent.com/K-S-V/Scripts/9c1afcc4b452cb9bf75f8653495c80180e2bf086/AdobeHDS.php"
-    sha1 "bd562cb02087c83eea70a4e9a306be27980ee12c"
+    url "https://raw.githubusercontent.com/K-S-V/Scripts/460814d210e8e19a98bddd12c178fc3b5f43b0bb/AdobeHDS.php"
+    sha256 "7087e3857ef7335717e142996e27c5546806b0babd92cd540d783ae4ce2bcf3f"
   end
 
   resource "pycrypto" do
     url "https://pypi.python.org/packages/source/p/pycrypto/pycrypto-2.6.1.tar.gz"
-    sha1 "aeda3ed41caf1766409d4efc689b9ca30ad6aeb2"
+    sha256 "f2ce1e989b272cfcb677616763e0a2e7ec659effa67a88aa92b3a65528f60a3c"
   end
 
   def install
