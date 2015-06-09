045468fd0f75f2b7211a7e26e7afda2a9e418ed5
Alex Dunn <dunn.alex@gmail.com>
2015-06-09 09:53:21 -0700

    x265 1.7
    Closes #40529.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/x265.rb
+++ Library/Formula/x265.rb
@@ -1,10 +1,11 @@
-require 'formula'
-
 class X265 < Formula
   desc "H.265/HEVC encoder"
   homepage "http://x265.org"
-  url "https://bitbucket.org/multicoreware/x265/downloads/x265_1.6.tar.gz"
-  sha1 "a3d568e39e3f9cd23081a1a2c5277f87de25a22a"
+  url "https://bitbucket.org/multicoreware/x265/downloads/x265_1.7.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/x/x265/x265_1.7.orig.tar.gz"
+  sha256 "a52ca95e8e64219c1e8e73a9abf0bb7151ced2c93756a623cf2b7e5cf8226585"
+
+  head "https://bitbucket.org/multicoreware/x265", :using => :hg
 
   bottle do
     cellar :any
@@ -13,20 +14,17 @@ class X265 < Formula
     sha256 "440805e02452b80400ff428967bd4c5d24f042bea2cc6b5d76a3a37648965a60" => :mountain_lion
   end
 
-  head 'https://bitbucket.org/multicoreware/x265', :using => :hg
+  option "with-16-bit", "Build a 16-bit x265 (default: 8-bit)"
 
-  depends_on 'yasm' => :build
-  depends_on 'cmake' => :build
+  deprecated_option "16-bit" => "with-16-bit"
+
+  depends_on "yasm" => :build
+  depends_on "cmake" => :build
   depends_on :macos => :lion
 
-  option '16-bit', 'Build a 16-bit x265 (default: 8-bit)'
-
   def install
-
     args = std_cmake_args
-    args.delete '-DCMAKE_BUILD_TYPE=None'
-    args << '-DCMAKE_BUILD_TYPE=Release'
-    args << '-DHIGH_BIT_DEPTH=ON' if build.include? '16-bit'
+    args << "-DHIGH_BIT_DEPTH=ON" if build.with? "16-bit"
 
     system "cmake", "source",  *args
     system "make", "install"
@@ -36,8 +34,8 @@ class X265 < Formula
     yuv_path = testpath/"raw.yuv"
     x265_path = testpath/"x265.265"
     yuv_path.binwrite "\xCO\xFF\xEE" * 3200
-    system "#{bin}/x265 --input-res 80x80 --fps 1 #{yuv_path} #{x265_path}"
-    header = 'AAAAAUABDAH//w=='
+    system bin/"x265", "--input-res", "80x80", "--fps", "1", yuv_path, x265_path
+    header = "AAAAAUABDAH//w=="
     assert_equal header.unpack("m"), [x265_path.read(10)]
   end
 end
