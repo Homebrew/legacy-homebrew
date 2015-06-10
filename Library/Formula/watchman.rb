117629bf3a8ede466ee4903cbf20b3106844102e
Wez Furlong <wez@wezfurlong.org>
2015-06-10 16:06:00 -0700

    watchman 3.2.0
    We tagged this today.
    
    Tested via:
    
    ```
    brew install watchman
    brew audit watchman
    brew test watchman
    ```
    
    Closes #40569.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/watchman.rb
+++ Library/Formula/watchman.rb
@@ -4,13 +4,10 @@ class Watchman < Formula
   desc "Watch files and take action when they change"
   homepage "https://github.com/facebook/watchman"
   head "https://github.com/facebook/watchman.git"
-  url "https://github.com/facebook/watchman/archive/v3.1.tar.gz"
-  sha1 "eb5572cd9cf4ce2b8e31d51ed22d5ec8cc6301ae"
+  url "https://github.com/facebook/watchman/archive/v3.2.0.tar.gz"
+  sha1 "b7313c240e4977ee6ea8906bdca9680db34df2e9"
 
   bottle do
-    sha256 "dd66354cdbe2ab6a9672b6f4619e2e8123cf26f60fa10b7ffaa32fb9e93bab90" => :yosemite
-    sha256 "8c8f84947bc5430ee8f5909bc2aa5dd6f49869c18cc600b24e1ae566eb5ecef5" => :mavericks
-    sha256 "923fd3ac8d2ac1b3752c82bb7559e311a2bfdacf30e61e4c3e371e7fb74298e8" => :mountain_lion
   end
 
   depends_on "autoconf" => :build
