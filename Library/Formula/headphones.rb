b91e0a6561da1a752b3b233247fc29e13dc6576b
Jon Hermansen <jon@jh86.org>
2015-06-09 19:53:51 -0700

    headphones: fix plist DOCTYPE
    Closes #40541.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/headphones.rb
+++ Library/Formula/headphones.rb
@@ -41,7 +41,7 @@ class Headphones < Formula
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/$
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
     <dict>
       <key>Label</key>
