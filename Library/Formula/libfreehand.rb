09e067ea6b7905b04b9a02ffdb6e95b5f8736df5
luzpaz <luzpaz@users.noreply.github.com>
2015-06-11 16:36:11 -0500

    libfreehand 0.1.1

---
+++ Library/Formula/libfreehand.rb
@@ -0,0 +1,38 @@
+class Libfreehand < Formula
+  desc "Interpret and import Aldus/Macromedia/Adobe FreeHand documents"
+  homepage "https://wiki.documentfoundation.org/DLP/Libraries/libfreehand"
+  url "http://dev-www.libreoffice.org/src/libfreehand/libfreehand-0.1.1.tar.xz"
+  sha256 "ec6676d0c63f7feac7801a1fe18dd7abe9044b39c3882fc99b9afef39bdf1d30"
+
+  depends_on "pkg-config" => :build
+  depends_on "boost" => :build
+  depends_on "librevenge"
+  depends_on "icu4c"
+  depends_on "lcms2"
+
+  def install
+    system "./configure", "--without-docs",
+                          "--disable-dependency-tracking",
+                          "--enable-static=no",
+                          "--disable-werror",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <libfreehand/FreeHandDocument.h>
+      int main() {
+        return libfreehand::FreeHandDocument::RESULT_OK;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-o", "test",
+                    "-I#{Formula["librevenge"].include}/librevenge-0.0",
+                    "-I#{include}/libfreehand-0.1.1",
+                    "-L#{Formula["librevenge"].lib}",
+                    "-L#{lib}",
+                    "-lrevenge-0.0",
+                    "-libfreehand-0.1.1"
+    system "./test"
+  end
+end
