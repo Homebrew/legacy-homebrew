4cebf3387a4e4cd2eafca9350d2dc65f95dd5f16
Adam Liter <adam.liter@gmail.com>
2015-06-10 21:51:19 -0400

    bib-tool 2.60
    Closes #40574.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/bib-tool.rb
+++ Library/Formula/bib-tool.rb
@@ -1,8 +1,20 @@
+# This is needed because of a problem with the tarball for 2.60
+# Hopefully, it will not be needed for future releases
+# See https://github.com/Homebrew/homebrew/issues/40559
+class BibToolDownloadStrategy < CurlDownloadStrategy
+  def stage
+     with_system_path { safe_system 'tar', 'xqf', cached_location, 'BibTool/doc/bibtool.tex' }
+     with_system_path { safe_system 'tar', 'xf', cached_location, '--exclude', 'BibTool/doc/bibtool.tex' }
+     chdir
+  end
+end
+
 class BibTool < Formula
   desc "Manipulates BibTeX databases"
   homepage "http://www.gerd-neugebauer.de/software/TeX/BibTool/index.en.html"
-  url "http://www.gerd-neugebauer.de/software/TeX/BibTool/BibTool-2.57.tar.gz"
-  sha1 "a6e80c86d347a39f3883e552db2dd4deb72b0e86"
+  url "http://www.gerd-neugebauer.de/software/TeX/BibTool/BibTool-2.60.tar.gz",
+    :using => BibToolDownloadStrategy
+  sha256 "db84b264df7c069b5b1c8e0778dc70f4e335cd1c39d711dcd65bae02df809ad1"
 
   bottle do
     sha1 "d5822fd899e1238c75964b7c0763dad2f24386e7" => :yosemite
