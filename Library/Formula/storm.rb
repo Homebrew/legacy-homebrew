4ee58aa900a25f8170c78984dbf894e8c6e55599
Nicholas Robison <nick@nickrobison.com>
2015-06-10 23:56:12 -0700

    storm 0.9.5: Change URL to apache mirror
    Changed the download URL from github back to the apache mirror.

--- Library/Formula/storm.rb
+++ Library/Formula/storm.rb
@@ -1,8 +1,8 @@
 class Storm < Formula
   desc "Distributed realtime computation system to process data streams"
   homepage "https://storm.apache.org"
-  url "https://github.com/apache/storm/archive/v0.9.5.tar.gz"
-  sha256 "bff3fdd4ba04380b87777b5fa97fbc207fefe3653606a8e341d412415e9c9c8d"
+  url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-0.9.5/apache-storm-0.9.5.tar.gz"
+  sha256 "2e8337126de8d1e180abe77fb81af7c971f8c4b2dad94e446ac86c0f02ba3fb2"
 
   def install
     libexec.install Dir["*"]
