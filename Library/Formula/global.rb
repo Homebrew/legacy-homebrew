4b8ca8c9d77248685e9c8266e9472227bd448d5d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-12 00:28:35 +0100

    global: update 6.5 bottle.

--- Library/Formula/global.rb
+++ Library/Formula/global.rb
@@ -6,9 +6,9 @@ class Global < Formula
   sha256 "4afd12db1aa600277b39113cc2d61dc59bd6c6b4ee8033da8bb6dd0c39a4c6a9"
 
   bottle do
-    sha256 "543a6fa77e22e58b4df082a53353b9e264942e0b5e011df648301f916c06a79d" => :yosemite
-    sha256 "4a3d7ee45ba3125725f584d9e711d5bc4061b41db4b58e0a63ca466db6b6bc4c" => :mavericks
-    sha256 "09659b458ac359daadad56aee968796ee7df2e8c3ca10b647d939aef26ba8f8b" => :mountain_lion
+    sha256 "6450fd297faa82a13e2505e20a5f131d742c313ef63dcb65fb56324117815368" => :yosemite
+    sha256 "e745d15ebfad496459fd55736b7e479ea7c8b276221a683609f3e0543d234e91" => :mavericks
+    sha256 "e25e53f6f2be9c36334622c45c5f00725c7cbcc513e1016c5bd7ba40f6ed192d" => :mountain_lion
   end
 
   head do
