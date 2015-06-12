4c3a82cf0e0ca7f66f865348ad6c9c90b40fafd2
Misty De Meo <mistydemeo@gmail.com>
2015-06-12 10:24:33 -0700

    libfreehand: style nits

--- Library/Formula/libfreehand.rb
+++ Library/Formula/libfreehand.rb
@@ -13,8 +13,8 @@ class Libfreehand < Formula
 
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
-  depends_on "librevenge"
   depends_on "icu4c"
+  depends_on "librevenge"
   depends_on "little-cms2"
 
   def install
