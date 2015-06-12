fd784e9e9523d5ac47914e96c6a80d69288a84a3
Larry Gilbert <larry@l2g.to>
2015-06-11 18:40:04 -0700

    uim: add X11 dependency

--- Library/Formula/uim.rb
+++ Library/Formula/uim.rb
@@ -4,6 +4,7 @@ class Uim < Formula
   url "https://github.com/uim/uim/releases/download/uim-1.8.6/uim-1.8.6.tar.gz"
   sha256 "0eebcf5b2192d0cd11018adbd69e358bfa062d3b5d63c1dd0b73d5f63eb7afe7"
 
+  depends_on :x11
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "intltool"
