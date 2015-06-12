0920efb1a0fda9d62e66fb951982fa4914947917
user <user@crapple.local>
2015-06-13 00:43:56 +0200

    add cjdns formula #40628 - shorter description and use double quotes

--- Library/Formula/cjdns.rb
+++ Library/Formula/cjdns.rb
@@ -1,5 +1,5 @@
 class Cjdns < Formula
-  desc "Advanced mesh routing with cryptographic addressing and integrity/confidentiality protection"
+  desc "Advanced mesh routing system with cryptographic addressing"
   homepage "https://github.com/cjdelisle/cjdns/"
   url "https://github.com/cjdelisle/cjdns/archive/cjdns-v16.1.tar.gz"
   sha256 "3eebb276ee63f103c54aa9201e67d8d810ae3a88af90300dd3a3a894e73cb400"
@@ -7,7 +7,7 @@ class Cjdns < Formula
   depends_on "node" => :build
 
   def install
-    system './do'
-    bin.install 'cjdroute'
+    system "./do"
+    bin.install "cjdroute"
   end
 end
