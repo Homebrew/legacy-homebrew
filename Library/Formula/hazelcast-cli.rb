f45c782d2bf4e6b5968357340e3c7a4823021edc
Bilal Yasar <bilalyasar45@gmail.com>
2015-06-12 14:23:20 +0300

    Create hazelcast-cli.rb

---
+++ Library/Formula/hazelcast-cli.rb
@@ -0,0 +1,21 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class HazelcastCli < Formula
+  homepage "github.com/hazelcast"
+  url "http://download.hazelcast.com/enterprise/brew/hazelcast-cli-1.0.tar.gz"
+  version "1.0"
+  sha256 "086896eb5d88daefd262fab31a7b81252cb2b04923b8053a6b455b9a3872ea0b"
+
+  # depends_on "cmake" => :build
+
+  def install
+   libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/hazelcast.sh" => "hazelcast"
+  end
+
+  test do
+    system "false"
+  end
+end
