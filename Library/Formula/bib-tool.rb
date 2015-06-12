4842d52b9006e5139e5ddf88d7445475743f21be
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-12 06:30:41 +0100

    bib-tool: update 2.60 bottle.

--- Library/Formula/bib-tool.rb
+++ Library/Formula/bib-tool.rb
@@ -17,9 +17,9 @@ class BibTool < Formula
   sha256 "db84b264df7c069b5b1c8e0778dc70f4e335cd1c39d711dcd65bae02df809ad1"
 
   bottle do
-    sha1 "d5822fd899e1238c75964b7c0763dad2f24386e7" => :yosemite
-    sha1 "dcaaf50c9992f1e1812845914ab3bd4b8be521de" => :mavericks
-    sha1 "e0b9efe0a08a1c02a94ddd95fdfcef8863f1ad01" => :mountain_lion
+    sha256 "62861fe6407c2953ada2a8066ac23790b52eeab4c748a893a64358cffb7149e4" => :yosemite
+    sha256 "589bd5d8c386cd9c87cf4a3b7b637f3a65ac449c1e0fc632dd88902ecf9e57f4" => :mavericks
+    sha256 "2023b312b178d1bcac02af7cd1dfbb2fd0a0edd4c8c48c30b38bea2ee0336ab7" => :mountain_lion
   end
 
   def install
