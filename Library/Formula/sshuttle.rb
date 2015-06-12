0a51eca019d29a9c431b8d0909326248b977e6db
Paul Hooijenga <paulhooijenga@gmail.com>
2015-06-12 09:19:43 +0200

    sshuttle 0.71
    Closes #40609.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/sshuttle.rb
+++ Library/Formula/sshuttle.rb
@@ -2,17 +2,17 @@ require 'formula'
 
 class Sshuttle < Formula
   desc "Proxy server that works as a poor man's VPN"
-  homepage 'https://github.com/apenwarr/sshuttle'
-  url 'https://github.com/apenwarr/sshuttle/archive/sshuttle-0.61.tar.gz'
-  sha1 '05551cdc78e866d983470ba4084beb206bacebd8'
+  homepage 'https://github.com/sshuttle/sshuttle'
+  url 'https://github.com/sshuttle/sshuttle/archive/sshuttle-0.71.tar.gz'
+  sha256 '62f0f8be5497c2d0098238c54e881ac001cd84fce442c2506ab6d37aa2f698f0'
 
-  head 'https://github.com/apenwarr/sshuttle.git'
+  head 'https://github.com/sshuttle/sshuttle.git'
 
   def install
     # Building the docs requires installing
     # markdown & BeautifulSoup Python modules
     # so we don't.
     libexec.install Dir['*']
-    bin.write_exec_script libexec/'sshuttle'
+    bin.write_exec_script libexec/'src/sshuttle'
   end
 end
