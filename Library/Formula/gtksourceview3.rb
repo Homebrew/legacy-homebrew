ec8616abc8a8e3b5621a542215d0017b85aa63c9
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-12 17:40:47 +0100

    gtksourceview3 3.16.1
    version bump
    audit --strict compliance

--- Library/Formula/gtksourceview3.rb
+++ Library/Formula/gtksourceview3.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Gtksourceview3 < Formula
   desc "Text view with syntax, undo/redo, and text marks"
-  homepage "http://projects.gnome.org/gtksourceview/"
-  url "http://ftp.gnome.org/pub/gnome/sources/gtksourceview/3.14/gtksourceview-3.14.2.tar.xz"
-  sha256 "b3c4a4f464fdb23ecc708a61c398aa3003e05adcd7d7223d48d9c04fe87524ad"
+  homepage "https://projects.gnome.org/gtksourceview/"
+  url "https://download.gnome.org/sources/gtksourceview/3.16/gtksourceview-3.16.1.tar.xz"
+  sha256 "669e9846ca744ec4d04ce61377e85df7ca909ad476329922a6abf4da423f1b0c"
 
   bottle do
     sha1 "7ff4f773b49943b3875b2c05b1d44f830f74ed43" => :yosemite
@@ -20,6 +18,72 @@ class Gtksourceview3 < Formula
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <gtksourceview/gtksourceview.h>
+
+      int main(int argc, char *argv[]) {
+        gchar *text = gtk_source_utils_unescape_search_text("hello world");
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    atk = Formula["atk"]
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gdk_pixbuf = Formula["gdk-pixbuf"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    gtkx3 = Formula["gtk+3"]
+    libepoxy = Formula["libepoxy"]
+    libpng = Formula["libpng"]
+    pango = Formula["pango"]
+    pixman = Formula["pixman"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/gio-unix-2.0/
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{gtkx3.opt_include}/gtk-3.0
+      -I#{include}/gtksourceview-3.0
+      -I#{libepoxy.opt_include}
+      -I#{libpng.opt_include}/libpng16
+      -I#{pango.opt_include}/pango-1.0
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{atk.opt_lib}
+      -L#{cairo.opt_lib}
+      -L#{gdk_pixbuf.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{gtkx3.opt_lib}
+      -L#{lib}
+      -L#{pango.opt_lib}
+      -latk-1.0
+      -lcairo
+      -lcairo-gobject
+      -lgdk-3
+      -lgdk_pixbuf-2.0
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lgtk-3
+      -lgtksourceview-3.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
   end
 end
