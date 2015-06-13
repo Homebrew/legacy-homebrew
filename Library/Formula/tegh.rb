f5df7358ae907e23992598a3368914eb2a563997
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-13 22:11:46 +0100

    tegh: add 0.3.1 bottle.

--- Library/Formula/tegh.rb
+++ Library/Formula/tegh.rb
@@ -6,6 +6,12 @@ class Tegh < Formula
 
   head "https://github.com/D1plo1d/tegh.git", :branch => "develop"
 
+  bottle do
+    sha256 "e331da57cd2bb89b5ac54ac12535c388495985c22046a565ec7799ba0b238eca" => :yosemite
+    sha256 "c2f4b64b7945944f0e99bd208dc9320b169116152208623a0189c7b09ed7bb7b" => :mavericks
+    sha256 "15207ec101af6d37b427abb9f6e5e4032373cd9b8f4fda260f0dbc4ae7987459" => :mountain_lion
+  end
+
   depends_on "node"
 
   def install
