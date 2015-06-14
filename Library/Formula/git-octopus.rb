a5de70dbd00a476a47f37813b4c2fc23cce316f9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-14 10:20:36 +0100

    git-octopus: add 1.0 bottle.

--- Library/Formula/git-octopus.rb
+++ Library/Formula/git-octopus.rb
@@ -6,6 +6,13 @@ class GitOctopus < Formula
 
   head "https://github.com/lesfurets/git-octopus.git"
 
+  bottle do
+    cellar :any
+    sha256 "be33fe6c653675fb777b830ba7b5ba5d41986e87bcd2d79d5887240862c287a9" => :yosemite
+    sha256 "8fa36a0ea95851074f36b0375f0138293f7c3aa0fc6f819a14987e4c1dc520ee" => :mavericks
+    sha256 "7c953c0d60e50045229f63d9efecb9a9b25791f336efde09b91026067102682e" => :mountain_lion
+  end
+
   def install
     bin.install "bin/git-octopus"
     man1.install "man/man1/git-octopus.1"
