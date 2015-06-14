64d49e312d5ec32288f8ae67602bee3fc96a0781
Viktor Szakats <vszakats@users.noreply.github.com>
2015-06-14 10:10:22 +0200

    iso-codes: use https urls, add head
    Closes #40694.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/iso-codes.rb
+++ Library/Formula/iso-codes.rb
@@ -1,8 +1,10 @@
 class IsoCodes < Formula
   desc "ISO language, territory, currency, script codes, and their translations"
-  homepage "http://pkg-isocodes.alioth.debian.org/"
-  url "http://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.58.tar.xz"
+  homepage "https://pkg-isocodes.alioth.debian.org/"
+  url "https://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.58.tar.xz"
   sha256 "86af5735dce6e4eff2b983e5d8aa9a3dea1b8db702333ff20be89e45f7f35a72"
+
+  head "git://git.debian.org/git/iso-codes/iso-codes.git"
 
   bottle do
     cellar :any
