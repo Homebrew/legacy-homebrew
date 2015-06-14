2e80e83fa37537f0a73a92f893dd1002f57aaae2
Baptiste Fontaine <batifon@yahoo.fr>
2015-06-14 09:41:55 +0200

    git-cal: head added
    Closes #40688.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/git-cal.rb
+++ Library/Formula/git-cal.rb
@@ -1,13 +1,15 @@
-require 'formula'
-
 class GitCal < Formula
   desc "GitHub-like contributions calendar but on the command-line"
-  homepage 'https://github.com/k4rthik/git-cal'
-  url 'https://github.com/k4rthik/git-cal/archive/v0.9.1.tar.gz'
-  sha1 '74c70107c9580c0455e01414ccae7333a746bca1'
+  homepage "https://github.com/k4rthik/git-cal"
+  url "https://github.com/k4rthik/git-cal/archive/v0.9.1.tar.gz"
+  sha256 "783fa73197b349a51d90670480a750b063c97e5779a5231fe046315af0a946cd"
+
+  head "https://github.com/k4rthik/git-cal.git"
 
   def install
-    bin.install 'git-cal'
+    system "perl", "Makefile.PL", "PREFIX=#{prefix}"
+    system "make"
+    system "make", "install"
   end
 
   test do
