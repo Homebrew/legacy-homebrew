c5dcbb8d621e12bc12f67f41eea5c6b3c0a0d42c
Ajay Solleti <solleti.ajay@gmail.com>
2015-06-14 20:37:44 +0530

    repo: use correct url for version
    updated URL and SHA1 to correct version.
    
    Closes #40705.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/repo.rb
+++ Library/Formula/repo.rb
@@ -3,9 +3,10 @@ require 'formula'
 class Repo < Formula
   desc "Repository tool for Android development"
   homepage 'http://source.android.com/source/git-repo.html'
-  url 'https://raw.githubusercontent.com/android/tools_repo/v1.12.13/repo'
+  url 'https://raw.githubusercontent.com/android/tools_repo/v1.12.21/repo'
   version '1.21'
-  sha1 'b8bd1804f432ecf1bab730949c82b93b0fc5fede'
+  revision 1
+  sha1 '9421408dba2d80e3c73910567f70f2e0c91dab2f'
 
   def install
     bin.install 'repo'
