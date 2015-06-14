a5c9328d0b77741bce4a04cd1e1c1f7aff5a7a73
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-14 21:46:28 +0100

    libp11: fix system openssl linkage
    Closes #40716.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libp11.rb
+++ Library/Formula/libp11.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class Libp11 < Formula
   desc "PKCS#11 wrapper library in C"
-  homepage 'https://github.com/OpenSC/libp11/wiki'
-  url 'https://downloads.sourceforge.net/project/opensc/libp11/libp11-0.2.8.tar.gz'
-  sha1 '2d1f6dc4200038f55a0cb7e22858f93e484b0724'
+  homepage "https://github.com/OpenSC/libp11/wiki"
+  url "https://downloads.sourceforge.net/project/opensc/libp11/libp11-0.2.8.tar.gz"
+  sha256 "a4121015503ade98074b5e2a2517fc8a139f8b28aed10021db2bb77283f40691"
+  revision 1
 
   bottle do
     cellar :any
@@ -15,18 +14,19 @@ class Libp11 < Formula
   end
 
   head do
-    url 'https://github.com/OpenSC/libp11.git'
+    url "https://github.com/OpenSC/libp11.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
 
   depends_on "pkg-config" => :build
   depends_on "libtool" => :run
+  depends_on "openssl"
 
   def install
     system "./bootstrap" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
