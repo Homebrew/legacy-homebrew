50a27a3d8c0c6bef027906bdf2ff884cabdf5ad9
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-14 19:54:12 +0100

    repo: style nits

--- Library/Formula/repo.rb
+++ Library/Formula/repo.rb
@@ -1,14 +1,12 @@
-require 'formula'
-
 class Repo < Formula
   desc "Repository tool for Android development"
-  homepage 'http://source.android.com/source/git-repo.html'
-  url 'https://raw.githubusercontent.com/android/tools_repo/v1.12.21/repo'
-  version '1.21'
+  homepage "http://source.android.com/source/git-repo.html"
+  url "https://raw.githubusercontent.com/android/tools_repo/v1.12.21/repo"
+  version "1.21"
+  sha1 "9421408dba2d80e3c73910567f70f2e0c91dab2f"
   revision 1
-  sha1 '9421408dba2d80e3c73910567f70f2e0c91dab2f'
 
   def install
-    bin.install 'repo'
+    bin.install "repo"
   end
 end
