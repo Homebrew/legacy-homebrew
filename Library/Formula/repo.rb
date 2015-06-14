6c1d213680b0ab9033aafba56f104621a8170b30
Viktor Szakats <vszakats@users.noreply.github.com>
2015-06-14 21:36:37 +0200

    repo: update homepage
    Closes #40714.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/repo.rb
+++ Library/Formula/repo.rb
@@ -1,6 +1,6 @@
 class Repo < Formula
   desc "Repository tool for Android development"
-  homepage "http://source.android.com/source/git-repo.html"
+  homepage "https://source.android.com/source/developing.html"
   url "https://raw.githubusercontent.com/android/tools_repo/v1.12.21/repo"
   version "1.21"
   sha1 "9421408dba2d80e3c73910567f70f2e0c91dab2f"
