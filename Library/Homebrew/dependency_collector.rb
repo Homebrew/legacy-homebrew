2980fc82643c181a73abaae61807f6ad965748dc
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2015-06-14 14:05:13 +0000

    Add unzip dependency for zip files for Linuxbrew
    Closes #436.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Library/Homebrew/dependency_collector.rb
+++ Library/Homebrew/dependency_collector.rb
@@ -189,6 +189,7 @@ class DependencyCollector
     when '.lz'  then Dependency.new('lzip', tags)
     when '.rar' then Dependency.new('unrar', tags)
     when '.7z'  then Dependency.new('p7zip', tags)
+    when '.zip' then Dependency.new('homebrew/dupes/unzip', tags) unless OS.mac?
     end
   end
 end
