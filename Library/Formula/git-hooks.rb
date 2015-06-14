dafa1f9d37e0b88f9f741eab8e0cf981a3c241c0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-14 09:31:37 +0100

    git-hooks: add 1.00.0 bottle.

--- Library/Formula/git-hooks.rb
+++ Library/Formula/git-hooks.rb
@@ -6,6 +6,13 @@ class GitHooks < Formula
 
   head "https://github.com/icefox/git-hooks.git"
 
+  bottle do
+    cellar :any
+    sha256 "d4c5fba7f1b80e8e68762356a2f64ac216bf4e9f3151cf2f236c92a9524b97ed" => :yosemite
+    sha256 "ace6acaff04ef09795d26e6034bf411a89c9f348287dd95f756c82068cea123d" => :mavericks
+    sha256 "d6fb1c69f50a6798e288b46f36d331fe86f8c2cc7b6ea2c8e43dfe675ed80132" => :mountain_lion
+  end
+
   def install
     bin.install "git-hooks"
     (etc/"git-hooks").install "contrib"
