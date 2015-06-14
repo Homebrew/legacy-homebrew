90a6c7827da65104e0161921cde4bd8fe0cb1f9b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-14 23:01:20 +0100

    git-ssh: add 0.2.0 bottle.

--- Library/Formula/git-ssh.rb
+++ Library/Formula/git-ssh.rb
@@ -6,6 +6,13 @@ class GitSsh < Formula
 
   head "https://github.com/lemarsu/git-ssh.git"
 
+  bottle do
+    cellar :any
+    sha256 "24e1f6a4d2dfe83141cd869cf75a512e98079a48267afce34032babd64a9fac5" => :yosemite
+    sha256 "7521baa200f0917ae75167e2f16fbaf41990dc0a13031f9aa2d94a68fcff8140" => :mavericks
+    sha256 "5edbeb19d4c76a82ec29e62e2cbe3a0860b62697f70b96bcc0e472dc8c065a6a" => :mountain_lion
+  end
+
   def install
     # Change loading of required code from libexec location (Cellar only)
     inreplace "bin/git-ssh" do |s|
