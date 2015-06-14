d3397aca00440b581aac0a8b8b3559593d3f7b92
Jingwen Owen Ou <jingweno@gmail.com>
2015-06-14 17:06:17 -0400

    `ccat` v1.0.0
    Release note: https://github.com/jingweno/ccat/releases/tag/v1.0.0

--- Library/Formula/ccat.rb
+++ Library/Formula/ccat.rb
@@ -1,8 +1,8 @@
 class Ccat < Formula
   desc "Like cat but displays content with syntax highlighting"
   homepage "https://github.com/jingweno/ccat"
-  url "https://github.com/jingweno/ccat/archive/v0.1.0.tar.gz"
-  sha256 "437c7582ab39c8a1fb5e97ae9569e0096c98209a2ebb11ca67a7f9246b129c7c"
+  url "https://github.com/jingweno/ccat/archive/v1.0.0.tar.gz"
+  sha256 "5bd558009a9054ff25f3d023d67080c211354d1552ffe377ce11d49376fb4aee"
 
   bottle do
     cellar :any
