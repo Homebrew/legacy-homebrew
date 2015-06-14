ab8eb0ea2e5720dbb918b87e875bbc5a108bd501
Baptiste Fontaine <batifon@yahoo.fr>
2015-06-14 23:54:38 +0200

    git-ssh: head added
    Closes #40719.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/git-ssh.rb
+++ Library/Formula/git-ssh.rb
@@ -4,19 +4,19 @@ class GitSsh < Formula
   url "https://github.com/lemarsu/git-ssh/archive/v0.2.0.tar.gz"
   sha256 "f7cf45f71e1f3aa23ef47cbbc411855f60d15ee69992c9f57843024e241a842f"
 
+  head "https://github.com/lemarsu/git-ssh.git"
+
   def install
     # Change loading of required code from libexec location (Cellar only)
     inreplace "bin/git-ssh" do |s|
       s.sub!(/path = .*$/, "path = '#{libexec}'")
     end
-    # Install main script
     bin.install "bin/git-ssh"
-    # Install required code into libexec location (Cellar only)
     libexec.install Dir["lib/*"]
   end
 
   test do
-    assert_equal "#{bin}/git-ssh v#{version}",
+    assert_equal "#{bin}/git-ssh v0.2.0",
       shell_output("#{bin}/git-ssh -V").chomp
   end
 end
