f1b45edde6dde87445ae08802ff5767bc711ed6c
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-15 20:59:55 +0100

    libtommath: add macports mirror
    Closes #40757.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libtommath.rb
+++ Library/Formula/libtommath.rb
@@ -1,10 +1,10 @@
-require 'formula'
-
 class Libtommath < Formula
   desc "C library for number theoretic multiple-precision integers"
-  homepage 'http://libtom.org/?page=features&newsitems=5&whatfile=ltm'
-  url 'http://libtom.org/files/ltm-0.42.0.tar.bz2'
-  sha1 '9b192701cf62b85e9bd65fbb4d622d04cfa5ee0d'
+  # homepage/url down since ~May 2015
+  homepage "http://libtom.org/?page=features&newsitems=5&whatfile=ltm"
+  url "http://libtom.org/files/ltm-0.42.0.tar.bz2"
+  mirror "https://distfiles.macports.org/libtommath/ltm-0.42.0.tar.bz2"
+  sha256 "7b5c258304c34ac5901cfddb9f809b9b3b8ac7d04f700cf006ac766a923eb217"
 
   bottle do
     cellar :any
@@ -14,9 +14,10 @@ class Libtommath < Formula
   end
 
   def install
-    ENV['DESTDIR'] = prefix
+    ENV["DESTDIR"] = prefix
+
     system "make"
-    include.install Dir['tommath*.h']
-    lib.install 'libtommath.a'
+    include.install Dir["tommath*.h"]
+    lib.install "libtommath.a"
   end
 end
