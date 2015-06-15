61cc74c77a077344c95d9761d0974c2860864dbf
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-15 22:14:23 +0100

    httpry: add 0.1.8 bottle.

--- Library/Formula/httpry.rb
+++ Library/Formula/httpry.rb
@@ -5,6 +5,13 @@ class Httpry < Formula
   sha256 "b3bcbec3fc6b72342022e940de184729d9cdecb30aa754a2c994073447468cf0"
   head "https://github.com/jbittel/httpry.git"
 
+  bottle do
+    cellar :any
+    sha256 "da227ba2d41c8723f5a8e117883cab86e30a730ac9638b23c9a537c825838237" => :yosemite
+    sha256 "64236c24fbcf79650ed1cd3c8969c8fa387c279d93da4a2de58227f840955632" => :mavericks
+    sha256 "01bcaee52ad4b3bc23d6b725996b8236d4d5a54cff7984c9bc7f733b44ea557c" => :mountain_lion
+  end
+
   depends_on "bsdmake" => :build
 
   def install
