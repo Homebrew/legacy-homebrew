9a9080d5cc046b55ccb99e0fe5358c5bc32ec2ed
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-15 20:50:33 +0100

    libtar: use git tag instead of snapshot

--- Library/Formula/libtar.rb
+++ Library/Formula/libtar.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class Libtar < Formula
   desc "C library for manipulating POSIX tar files"
-  homepage 'http://repo.or.cz/w/libtar.git'
-  url 'http://repo.or.cz/w/libtar.git/snapshot/v1.2.20.tar.gz'
-  sha1 '3432cc24936d23f14f1e74ac1f77a1b2ad36dffa'
+  homepage "http://repo.or.cz/w/libtar.git"
+  url "http://repo.or.cz/libtar.git",
+      :tag => "v1.2.20",
+      :revision => "0907a9034eaf2a57e8e4a9439f793f3f05d446cd"
 
   bottle do
     cellar :any
@@ -14,9 +13,9 @@ class Libtar < Formula
     sha1 "96e778eed9bba1d3cfd8fbc81e616ad580770a84" => :mountain_lion
   end
 
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def install
     system "autoreconf", "--force", "--install"
@@ -25,5 +24,16 @@ class Libtar < Formula
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"homebrew.txt").write "This is a simple example"
+    system "tar", "-cvf", "test.tar", "homebrew.txt"
+    rm "homebrew.txt"
+    assert !File.exist?("homebrew.txt")
+    assert File.exist?("test.tar")
+
+    system bin/"libtar", "-x", "test.tar"
+    assert File.exist?("homebrew.txt")
   end
 end
