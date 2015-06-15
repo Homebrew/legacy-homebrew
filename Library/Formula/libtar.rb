305920ab506dfc4025ecaf2429d37f5f743b292d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-15 22:14:24 +0100

    libtar: update 1.2.20 bottle.

--- Library/Formula/libtar.rb
+++ Library/Formula/libtar.rb
@@ -7,10 +7,10 @@ class Libtar < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha1 "18d378564e3507204dd29e84bf09840e335206a2" => :yosemite
-    sha1 "bcbd1747ca7827d795a13d11648d72ecb5b5e1a2" => :mavericks
-    sha1 "96e778eed9bba1d3cfd8fbc81e616ad580770a84" => :mountain_lion
+    revision 2
+    sha256 "d8d138fb4c1cf8c33aaaf8633cd748e9a423e84f1df886ae1842d4816b1f34a0" => :yosemite
+    sha256 "b8eb40cc1715243eb0ff85eddd2a5f5546e19ca7278ce30b08bd8e1bd3f0682f" => :mavericks
+    sha256 "7e79320f86f0fc61f80a041e48aafacc9148403b124dd0543b78c84bdf1b3384" => :mountain_lion
   end
 
   depends_on "autoconf" => :build
