89c369362308f3ac0b67368e9e679b76cdd87ace
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-15 20:55:47 +0100

    libtomcrypt: add macports mirror

--- Library/Formula/libtomcrypt.rb
+++ Library/Formula/libtomcrypt.rb
@@ -1,10 +1,10 @@
-require 'formula'
-
 class Libtomcrypt < Formula
   desc "Modular and portable cryptographic toolkit"
-  homepage 'http://libtom.org/?page=features&whatfile=crypt'
-  url 'http://libtom.org/files/crypt-1.17.tar.bz2'
-  sha256 'e33b47d77a495091c8703175a25c8228aff043140b2554c08a3c3cd71f79d116'
+  # homepage/url down since ~May 2015
+  homepage "http://libtom.org/?page=features&whatfile=crypt"
+  url "http://libtom.org/files/crypt-1.17.tar.bz2"
+  mirror "https://distfiles.macports.org/libtomcrypt/crypt-1.17.tar.bz2"
+  sha256 "e33b47d77a495091c8703175a25c8228aff043140b2554c08a3c3cd71f79d116"
 
   bottle do
     cellar :any
@@ -14,15 +14,15 @@ class Libtomcrypt < Formula
     sha1 "eb5270eff08703ed0d72ece429f9df5f0273fb65" => :mountain_lion
   end
 
-  depends_on 'libtommath'
+  depends_on "libtommath"
 
   def install
-    ENV['DESTDIR'] = prefix
+    ENV["DESTDIR"] = prefix
+    ENV["EXTRALIBS"] = "-ltommath"
     ENV.append "CFLAGS", "-DLTM_DESC -DUSE_LTM"
-    ENV['EXTRALIBS'] = "-ltommath"
 
     system "make", "library"
-    include.install Dir['src/headers/*']
-    lib.install 'libtomcrypt.a'
+    include.install Dir["src/headers/*"]
+    lib.install "libtomcrypt.a"
   end
 end
