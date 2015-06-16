0f3557268c784616a4f72f5b3c3ea660deb3eecf
Viktor Szakats <vszakats@users.noreply.github.com>
2015-06-16 09:53:03 +0200

    mysql-connector-odbc: use https urls
    Closes #40774.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mysql-connector-odbc.rb
+++ Library/Formula/mysql-connector-odbc.rb
@@ -1,7 +1,7 @@
 class MysqlConnectorOdbc < Formula
   desc "Standardized database driver"
-  homepage "http://dev.mysql.com/doc/refman/5.1/en/connector-odbc.html"
-  url "http://cdn.mysql.com/Downloads/Connector-ODBC/5.3/mysql-connector-odbc-5.3.4-src.tar.gz"
+  homepage "https://dev.mysql.com/doc/refman/5.1/en/connector-odbc.html"
+  url "https://cdn.mysql.com/Downloads/Connector-ODBC/5.3/mysql-connector-odbc-5.3.4-src.tar.gz"
   sha256 "a5f7a490f2958f2768d18b8a57f71909f9699a8619c82776b3ad1c02b8abce0d"
 
   bottle do
