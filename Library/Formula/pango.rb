ece598c8594378596fd0cb19c9feb52aa7c31b71
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-16 11:04:54 +0100

    several small fixes
    Closes #39868.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -1,7 +1,7 @@
 class Pango < Formula
   desc "Framework for layout and rendering of i18n text"
   homepage "http://www.pango.org/"
-  url "http://ftp.gnome.org/pub/GNOME/sources/pango/1.36/pango-1.36.8.tar.xz"
+  url "https://download.gnome.org/sources/pango/1.36/pango-1.36.8.tar.xz"
   sha256 "18dbb51b8ae12bae0ab7a958e7cf3317c9acfc8a1e1103ec2f147164a0fc2d07"
   revision 1
 
@@ -12,13 +12,6 @@ class Pango < Formula
     depends_on "autoconf" => :build
     depends_on "libtool" => :build
     depends_on "gtk-doc" => :build
-  end
-
-  bottle do
-    revision 1
-    sha1 "b30d81e5b4b90792e14aa02b273fcf93e9675fc7" => :yosemite
-    sha1 "eb30e96c1d896cd8fc7e1053513b3e298645c9af" => :mavericks
-    sha1 "ea288645c2ca58b4addf29c0140fb3ecec6ea3ab" => :mountain_lion
   end
 
   option :universal
