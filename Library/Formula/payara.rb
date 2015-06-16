c56c40638e283da85a988ebc35b4c5db2612f1ce
taiichiro-nagase <taiichiro.nagase@gree.net>
2015-06-16 17:11:58 +0900

    fix warnings

--- Library/Formula/payara.rb
+++ Library/Formula/payara.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Payara < Formula
   desc "Java EE application server"
-  homepage 'http://www.payara.co.uk/home'
-  url 'https://s3-eu-west-1.amazonaws.com/payara.co/Payara+Downloads/payara-4.1.152.1.zip'
-  sha1 'c7bdbe9c79b5206f1d4aeb51d2671512c4fd5df1'
+  homepage "http://www.payara.co.uk/home"
+  url "https://s3-eu-west-1.amazonaws.com/payara.co/Payara+Downloads/payara-4.1.152.1.zip"
+  sha256 "b57e5c816685b92c4ceecdaae6d1822a83fc6d2ee87765c9dcf68ae6c6d7e356"
 
   def install
     rm_rf Dir["bin/*.bat"]
@@ -22,5 +20,9 @@ class Payara < Formula
     Note: The support scripts used by Payara Application Server 4
     are *NOT* linked to bin.
   EOS
+  end
+
+  test do
+    system "#{bin}/asadmin", "--help"
   end
 end
