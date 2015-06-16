d607259c8c4d5e4005082f1ebd57f54b23962181
Tomasz Pajor <tomek@polishgeeks.com>
2015-06-16 08:45:53 +0200

    libbluray 0.8.1
    Closes #40767.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/libbluray.rb
+++ Library/Formula/libbluray.rb
@@ -1,8 +1,8 @@
 class Libbluray < Formula
   desc "Blu-Ray disc playback library for media players like VLC"
   homepage "https://www.videolan.org/developers/libbluray.html"
-  url "https://download.videolan.org/pub/videolan/libbluray/0.7.0/libbluray-0.7.0.tar.bz2"
-  sha256 "f79beb9fbb24117cbb1264c919e686ae9e6349c0ad08b48c4b6233b2887eb68d"
+  url "https://download.videolan.org/pub/videolan/libbluray/0.8.1/libbluray-0.8.1.tar.bz2"
+  sha256 "cdbec680c5bbc2251de6ccd109cf5f798ea51db6fcb938df39283be1799efb8f"
 
   bottle do
     cellar :any
@@ -17,12 +17,12 @@ class Libbluray < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on "ant" => :build
   end
 
   depends_on "pkg-config" => :build
   depends_on "freetype" => :recommended
   depends_on "fontconfig"
+  depends_on "ant" => :build
 
   def install
     # https://mailman.videolan.org/pipermail/libbluray-devel/2014-April/001401.html
