67686fdb336613b6a97263cb5829835a18d1ec78
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-17 02:48:57 +0100

    bash-completion: add 1.3 bottle.

--- Library/Formula/bash-completion.rb
+++ Library/Formula/bash-completion.rb
@@ -9,6 +9,12 @@ class BashCompletion < Formula
   mirror 'http://pkgs.fedoraproject.org/repo/pkgs/bash-completion/bash-completion-1.3.tar.bz2/a1262659b4bbf44dc9e59d034de505ec/bash-completion-1.3.tar.bz2'
   sha256 '8ebe30579f0f3e1a521013bcdd183193605dab353d7a244ff2582fb3a36f7bec'
 
+  bottle do
+    sha256 "f522c40d199aff2cdfb71f5b3b17f8e4719e78bed48b13dab1714d3457e93b23" => :yosemite
+    sha256 "2f321fe1b85c9df65b1a3a2b4f1aa66189b59fca4d4a2193f35abd697f9fe40b" => :mavericks
+    sha256 "1b5bae29dc78c12ac39563380c5bfb84d76094b2661a6c12e19704510981f4e4" => :mountain_lion
+  end
+
   # Backports the following upstream patch from 2.x:
   # https://anonscm.debian.org/gitweb/?p=bash-completion/bash-completion.git;a=commitdiff_plain;h=50ae57927365a16c830899cc1714be73237bdcb2
   patch :DATA
