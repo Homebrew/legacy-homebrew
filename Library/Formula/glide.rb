207997f1951bcf46d9106efa41bb214566e8b4e3
Matt Farina <matt@mattfarina.com>
2015-06-17 11:33:12 -0400

    glide 0.3.0
    glide updated to 0.3.0
    
    Closes #40818.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/glide.rb
+++ Library/Formula/glide.rb
@@ -4,8 +4,8 @@ require "language/go"
 class Glide < Formula
   desc "Simplified Go project management, dependency management, and vendoring"
   homepage "https://github.com/Masterminds/glide"
-  url "https://github.com/Masterminds/glide/archive/0.2.0.tar.gz"
-  sha1 "a01fc0a56cfcb9070af2e8bbca35da81310b0b21"
+  url "https://github.com/Masterminds/glide/archive/0.3.0.tar.gz"
+  sha1 "60fb5978446bae925158b594dbf2e15546fd3168"
 
   bottle do
     sha1 "dea3d205ecbfa38f7cfe5e2b79c5ab12af462df9" => :mavericks
@@ -17,17 +17,17 @@ class Glide < Formula
 
   go_resource "github.com/kylelemons/go-gypsy" do
     url "https://github.com/kylelemons/go-gypsy.git",
-      :revision => "0ddb27ce74ade2ca988ec85587d636e10312dfc6"
+      :revision => "42fc2c7ee9b8bd0ff636cd2d7a8c0a49491044c5"
   end
 
   go_resource "github.com/Masterminds/cookoo" do
     url "https://github.com/Masterminds/cookoo.git",
-      :revision => "b85ae3d2668c9a4e99419bd98ea9803926a1e306"
+      :revision => "623f8762b2474f1ad6c2cac6bf331b8871591379"
   end
 
   go_resource "github.com/codegangsta/cli" do
     url "https://github.com/codegangsta/cli.git",
-      :revision => "f7ebb761e83e21225d1d8954fde853bf8edd46c4"
+      :revision => "7ad88c27405eca0bb4a04bb45897fb7985bd1217"
   end
 
   def install
@@ -36,12 +36,12 @@ class Glide < Formula
     ENV["GOPATH"] = buildpath
     Language::Go.stage_deps resources, buildpath/"src"
 
-    system "go", "build", "-o", "glide", "-ldflags", "-X main.version 0.2.0", "#{buildpath}/src/github.com/Masterminds/glide/glide.go"
+    system "go", "build", "-o", "glide", "-ldflags", "-X main.version 0.3.0", "#{buildpath}/src/github.com/Masterminds/glide/glide.go"
     bin.install "glide"
   end
 
   test do
     version = pipe_output("#{bin}/glide --version")
-    assert_match /0.2.0/, version
+    assert_match /0.3.0/, version
   end
 end
