3bd0b30d1cb21970275fc3facf1a755cc236959c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 00:11:09 +0100

    lftp: update 4.6.3a bottle.

--- Library/Formula/lftp.rb
+++ Library/Formula/lftp.rb
@@ -7,9 +7,9 @@ class Lftp < Formula
   sha256 "8c3a12a1f9ec288132b245bdd7d14d88ade1aa5cb1c14bb68c8fab3b68793840"
 
   bottle do
-    sha256 "725145ef258d9a536bd78e7bab9c31ff1a5ae1f8d6c3ec86aa52dc6e0948e3b7" => :yosemite
-    sha256 "6980ddc42aa5e021a3f5d0f2e06b2613151b9972390b077f34ed765b30d28c81" => :mavericks
-    sha256 "40aa62dc70760f0cca86c3cbd52da7392c0aa118c48877aa3df4ca7258b1ba03" => :mountain_lion
+    sha256 "5f939d210823658f99f455b76250aac59e4db8a02673f3ec69087cf4a61ea20b" => :yosemite
+    sha256 "d5f13cc616fb5d3346fc83a15ec3c6185e00aba6964c6c755b520b1f8367a973" => :mavericks
+    sha256 "7ad557cfbebbe54ac6e7c017aa183752b6ae9bbf9939231b2a403feaf297be6b" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
