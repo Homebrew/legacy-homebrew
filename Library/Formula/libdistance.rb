ebad64a676fef0cacebdb7ee49a8e020b3c623b2
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-06-16 22:28:06 -0230

    Add a description from the homepage.
    `brew audit libdistance --strict` was complaining about a lack of
    description, causing a Jenkins failure. Here is the description from the
    upstream project's homepage.

--- Library/Formula/libdistance.rb
+++ Library/Formula/libdistance.rb
@@ -1,5 +1,6 @@
 class Libdistance < Formula
   homepage "http://monkey.org/~jose/software/libdistance/"
+  desc "The distance library is used to compare pieces of data for similarity. Specifically, it contains a number of methods to find the \"edit distance\" between inputs, or the number of differences between them. These differences are calculated using various mechanisms. The inputs to these functions can be character strings or arbitrary data."
   url "http://monkey.org/~jose/software/libdistance/libdistance-0.2.2.tar.gz"
   sha256 "65364e07c9fe78ef34fc0e563aff2317ab0aba7549da07adea929426a708e6ad"
 
