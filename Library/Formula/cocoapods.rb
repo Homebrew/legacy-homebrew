4c06500568b1c07bde93cc74e857b69f63b2e479
Keith Smiley <keithbsmiley@gmail.com>
2015-06-16 21:26:31 -0700

    Remove xcproj dependency

--- Library/Formula/cocoapods.rb
+++ Library/Formula/cocoapods.rb
@@ -3,8 +3,6 @@ class Cocoapods < Formula
   url "http://keith.github.io/CocoaPods/cocoapods-0.37.2.tar.gz"
   sha1 "f727a8027a747f01323ee1188c4ce654731e3e51"
 
-  depends_on "xcproj" => :recommended
-
   def install
     prefix.install "vendor"
     prefix.install "lib" => "rubylib"
