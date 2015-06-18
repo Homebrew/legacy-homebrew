051b7ba47062fb5c5b96a19b11214129cbb74b97
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-18 01:52:36 +0100

    py2cairo: revision for gtk changes

--- Library/Formula/py2cairo.rb
+++ Library/Formula/py2cairo.rb
@@ -2,7 +2,9 @@ class Py2cairo < Formula
   desc "Python 2 bindings for the Cairo graphics library"
   homepage "http://cairographics.org/pycairo/"
   url "http://cairographics.org/releases/py2cairo-1.10.0.tar.bz2"
-  sha1 "2efa8dfafbd6b8e492adaab07231556fec52d6eb"
+  mirror "https://distfiles.macports.org/py-cairo/py2cairo-1.10.0.tar.bz2"
+  sha256 "d30439f06c2ec1a39e27464c6c828b6eface3b22ee17b2de05dc409e429a7431"
+  revision 1
 
   bottle do
     cellar :any
@@ -12,11 +14,11 @@ class Py2cairo < Formula
     sha1 "644f231336215c9f127b8191a32f4b28131ba4eb" => :mountain_lion
   end
 
+  option :universal
+
   depends_on "pkg-config" => :build
   depends_on "cairo"
   depends_on :python if MacOS.version <= :snow_leopard
-
-  option :universal
 
   fails_with :llvm do
     build 2336
@@ -33,12 +35,16 @@ class Py2cairo < Formula
     ENV.append_to_cflags `python-config --includes`
 
     # Python extensions default to universal but cairo may not be universal
-    ENV['ARCHFLAGS'] = "-arch #{MacOS.preferred_arch}" unless build.universal?
+    ENV["ARCHFLAGS"] = "-arch #{MacOS.preferred_arch}" unless build.universal?
 
     system "./waf", "configure", "--prefix=#{prefix}", "--nopyc", "--nopyo"
     system "./waf", "install"
 
     module_dir = lib/"python2.7/site-packages/cairo"
     mv module_dir/"lib_cairo.dylib", module_dir/"_cairo.so"
+  end
+
+  test do
+    system "python", "-c", "import cairo; print(cairo.version)"
   end
 end
