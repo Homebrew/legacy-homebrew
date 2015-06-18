33a51976554e4561abf4d7487956f9a0d009a202
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-18 02:29:54 +0100

    libcouchbase: use .git for head

--- Library/Formula/libcouchbase.rb
+++ Library/Formula/libcouchbase.rb
@@ -1,9 +1,9 @@
 class Libcouchbase < Formula
   desc "C library for Couchbase"
-  homepage 'http://docs.couchbase.com/developer/c-2.4/c-intro.html'
-  url 'http://packages.couchbase.com/clients/c/libcouchbase-2.5.1.tar.gz'
-  sha1 '11a19b7dc7b8cad5ef6ad8a089d7a177c96451af'
-  head "https://github.com/couchbase/libcouchbase", :using => :git
+  homepage "http://docs.couchbase.com/developer/c-2.4/c-intro.html"
+  url "http://packages.couchbase.com/clients/c/libcouchbase-2.5.1.tar.gz"
+  sha1 "11a19b7dc7b8cad5ef6ad8a089d7a177c96451af"
+  head "https://github.com/couchbase/libcouchbase.git"
 
   bottle do
     sha256 "08be2172b00dbd629e81ea977d3098b487b1aa4cbccb205d75edbed558ba5761" => :yosemite
@@ -22,26 +22,26 @@ class Libcouchbase < Formula
   depends_on "libuv" => :optional
   depends_on "libevent" => :recommended
   depends_on "openssl"
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   def install
     args = std_cmake_args
-    args << '-DLCB_NO_TESTS=1'
+    args << "-DLCB_NO_TESTS=1"
 
-    ['libev', 'libevent', 'libuv'].each do |pname|
-        args << "-DLCB_BUILD_#{pname.upcase}=" + (build.with?("#{pname}") ? 'ON' : 'OFF')
+    ["libev", "libevent", "libuv"].each do |pname|
+      args << "-DLCB_BUILD_#{pname.upcase}=" + (build.with?("#{pname}") ? "ON" : "OFF")
     end
     if build.universal?
-      args << '-DLCB_UNIVERSAL_BINARY=1'
+      args << "-DLCB_UNIVERSAL_BINARY=1"
       ENV.universal_binary
     end
-    if build.without?('libev') && build.without?('libuv') && build.without?('libevent')
-      args << '-DLCB_NO_PLUGINS=1'
+    if build.without?("libev") && build.without?("libuv") && build.without?("libevent")
+      args << "-DLCB_NO_PLUGINS=1"
     end
 
-    mkdir 'LCB-BUILD' do
+    mkdir "LCB-BUILD" do
       system "cmake", "..", *args
-      system 'make install'
+      system "make", "install"
     end
   end
 
