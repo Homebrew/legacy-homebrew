a1c8e71e0e59d7451b9b0c25d51e0627f37237dc
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-06-17 22:28:21 -0700

    carthage 0.7.5
    https://github.com/Carthage/Carthage/releases/tag/0.7.5
    
    Closes #40844.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/carthage.rb
+++ Library/Formula/carthage.rb
@@ -1,8 +1,8 @@
 class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/Carthage/Carthage"
-  url "https://github.com/Carthage/Carthage.git", :tag => "0.7.4-homebrew",
-                                                  :revision => "199d3b3dfd5a5ce7aeb919eb9206b7faf1374e20",
+  url "https://github.com/Carthage/Carthage.git", :tag => "0.7.5",
+                                                  :revision => "41e93849adcd05d1e2ffc7a7f409c056a23a53ab",
                                                   :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
