4eda471923229cb8bae1ffd6cfede153dc212c59
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 02:14:41 +0100

    giflossy: add 1.82.1 bottle.

--- Library/Formula/giflossy.rb
+++ Library/Formula/giflossy.rb
@@ -5,6 +5,13 @@ class Giflossy < Formula
   sha256 "a0d048f0c2274c81532a988d2f3ea16c3f1cbb6878f13deeb425d34826e4ed23"
   head "https://github.com/pornel/giflossy.git"
 
+  bottle do
+    cellar :any
+    sha256 "2c4d49abc644aa4e98eaaae86002c7153bf9e3990186e730d9ebb3ff4ceb0b22" => :yosemite
+    sha256 "96c42819acabd42287e2e032f10969e9ac08bfb28848626ac0df4f926f116a99" => :mavericks
+    sha256 "1e71bcca774a3f01d8ede50d6040d06c2f25df7e6e3635efab4c5daf06f2bcb1" => :mountain_lion
+  end
+
   conflicts_with "gifsicle",
     :because => "both install an `gifsicle` binary"
 
