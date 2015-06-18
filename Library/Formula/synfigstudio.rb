cfc04e6b0a2f562f53f7c51636e7a283725fef1d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 07:45:38 +0100

    synfigstudio: add 1.0 bottle.

--- Library/Formula/synfigstudio.rb
+++ Library/Formula/synfigstudio.rb
@@ -4,6 +4,13 @@ class Synfigstudio < Formula
   url "https://downloads.sourceforge.net/project/synfig/releases/1.0/source/synfigstudio-1.0.tar.gz"
   sha256 "2b23916ca0be4073edad9b0cb92fd30311dd3b8f73372c836ba735100251ee28"
 
+  bottle do
+    revision 1
+    sha256 "33b91aef1e6a80c880f0638984f4135340e6207472681775e06984556016aa0d" => :yosemite
+    sha256 "47cdfcbf5c6ce66ddde2810dd8df18e2dc7590633ba36ba89f697876ab19a890" => :mavericks
+    sha256 "281f88e6f2b4de77a86406c2360a458222f830345f410f1856c3a295e0166c89" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "gettext"
