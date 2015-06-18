0497656a0c08a3f96b0c570bef3ef8a6742e00f4
Guo Xiao <guoxiao08@gmail.com>
2015-06-18 14:55:12 +0800

    rabbitmq-c 0.7.0
    Closes #40846.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/rabbitmq-c.rb
+++ Library/Formula/rabbitmq-c.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class RabbitmqC < Formula
   desc "RabbitMQ C client"
   homepage "https://github.com/alanxz/rabbitmq-c"
-  url "https://github.com/alanxz/rabbitmq-c/archive/v0.5.2.tar.gz"
-  sha1 "6c442aefbc4477ac0598c05361c767a75d6e1541"
+  url "https://github.com/alanxz/rabbitmq-c/archive/v0.7.0.tar.gz"
+  sha256 "b8b9a5cca871968de95538a87189f7321a3f86aca07ae8a81874e93d73cf9a4d"
 
   head "https://github.com/alanxz/rabbitmq-c.git"
 
@@ -20,8 +18,8 @@ class RabbitmqC < Formula
 
   depends_on "pkg-config" => :build
   depends_on "cmake" => :build
-  depends_on "rabbitmq" => :recommended
   depends_on "popt" if build.with? "tools"
+  depends_on "openssl"
 
   def install
     ENV.universal_binary if build.universal?
@@ -31,12 +29,16 @@ class RabbitmqC < Formula
     args << "-DBUILD_API_DOCS=OFF"
 
     args << if build.with? "tools"
-      "-DBUILD_TOOLS=ON"
-    else
-      "-DBUILD_TOOLS=OFF"
-    end
+              "-DBUILD_TOOLS=ON"
+            else
+              "-DBUILD_TOOLS=OFF"
+            end
 
     system "cmake", ".", *args
     system "make", "install"
+  end
+
+  test do
+    system "amqp-get", "--help"
   end
 end
