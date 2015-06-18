70ee0336f84ac8cf64837e87063bd6d8fea02cce
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-18 01:54:45 +0100

    py3cairo: revision for gtk changes
    Fixes #40824
    
    Closes #40839.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/py3cairo.rb
+++ Library/Formula/py3cairo.rb
@@ -2,7 +2,9 @@ class Py3cairo < Formula
   desc "Python 3 bindings for the Cairo graphics library"
   homepage "http://cairographics.org/pycairo/"
   url "http://cairographics.org/releases/pycairo-1.10.0.tar.bz2"
+  mirror "https://distfiles.macports.org/py-cairo/pycairo-1.10.0.tar.bz2"
   sha256 "9aa4078e7eb5be583aeabbe8d87172797717f95e8c4338f0d4a17b683a7253be"
+  revision 1
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
