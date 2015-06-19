db60948ee0cf544a1b6b7d723f300845e1c88a95
Marc-Antoine Parent <maparent@acm.org>
2015-06-19 10:39:00 -0400

    sha256

--- Library/Formula/libiodbc.rb
+++ Library/Formula/libiodbc.rb
@@ -1,14 +1,13 @@
 class Libiodbc < Formula
   homepage "http://www.iodbc.org/dataspace/iodbc/wiki/iODBC/"
   url "http://sourceforge.net/projects/iodbc/files/iodbc/3.52.10/libiodbc-3.52.10.tar.gz/download"
-  sha1 "2a8328cea84a3ddb78b10a009652cdd3f1d39fb9"
+  sha256 "1568b42b4e97f36110af661d39bfea7d94ac4ff020014574b16a7199f068e11f"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
 
   def install
-    # run bootstrap.sh
     system "sh",  "./bootstrap.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
