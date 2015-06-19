a445fafc302c03b0bcece66bd48cef3d0ad0bfa4
Marc-Antoine Parent <maparent@acm.org>
2015-06-19 10:37:16 -0400

    double quotes

--- Library/Formula/libiodbc.rb
+++ Library/Formula/libiodbc.rb
@@ -1,11 +1,11 @@
 class Libiodbc < Formula
-  homepage 'http://www.iodbc.org/dataspace/iodbc/wiki/iODBC/'
-  url 'http://sourceforge.net/projects/iodbc/files/iodbc/3.52.10/libiodbc-3.52.10.tar.gz/download'
-  sha1 '2a8328cea84a3ddb78b10a009652cdd3f1d39fb9'
+  homepage "http://www.iodbc.org/dataspace/iodbc/wiki/iODBC/"
+  url "http://sourceforge.net/projects/iodbc/files/iodbc/3.52.10/libiodbc-3.52.10.tar.gz/download"
+  sha1 "2a8328cea84a3ddb78b10a009652cdd3f1d39fb9"
 
-  depends_on 'automake' => :build
-  depends_on 'autoconf' => :build
-  depends_on 'libtool' => :build
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
 
   def install
     # run bootstrap.sh
