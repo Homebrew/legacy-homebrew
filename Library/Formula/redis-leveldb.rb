939c33215b54e23c1209ae8de65d3986d7a43352
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-20 22:27:41 +0100

    redis-leveldb: add 1.4 bottle.

--- Library/Formula/redis-leveldb.rb
+++ Library/Formula/redis-leveldb.rb
@@ -5,6 +5,13 @@ class RedisLeveldb < Formula
   sha256 "b34365ca5b788c47b116ea8f86a7a409b765440361b6c21a46161a66f631797c"
   head "https://github.com/KDr2/redis-leveldb.git"
 
+  bottle do
+    cellar :any
+    sha256 "bc87b077a7782bdc7d117f1aaafe788bc7fe954434ddc5263b83bf04f9d9e151" => :yosemite
+    sha256 "ab18b0f70c39fb6a786c7a4efb264176ce8debcb6e6caf19ec01bf062c48e6bc" => :mavericks
+    sha256 "cebb22e111395ed877ba8172f12481b25bbeede765f8d00f74bfab97746b6576" => :mountain_lion
+  end
+
   depends_on "libev"
   depends_on "gmp"
   depends_on "leveldb"
