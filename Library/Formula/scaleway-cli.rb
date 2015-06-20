f50859e5744e0c16f8a4654cc8de15f1e91fa954
Nicolas Brousse <pro@nicolas-brousse.fr>
2015-06-20 13:50:24 +0200

    Fixes after bot tests

--- Library/Formula/scaleway-cli.rb
+++ Library/Formula/scaleway-cli.rb
@@ -1,14 +1,13 @@
-require "formula"
-
 class ScalewayCli < Formula
-  desc "Interact with Scaleway API from the command line"
+  desc "Interact with Scaleway API from the command-line"
   homepage "https://github.com/scaleway/scaleway-cli/"
   url "https://github.com/scaleway/scaleway-cli/releases/download/v1.1.0/scw-Darwin-x86_64"
-  sha1 "8860b5daf4af43684c523aab62c48ee7e99517d4"
+  version "1.1.0"
+  sha256 "78ac104bf11f856ea50e7c75442264e5601214b5d33118648866250fcc0bff80"
 
   def install
-    mv 'scw-Darwin-x86_64', 'scw'
-    bin.install 'scw'
+    mv "scw-Darwin-x86_64", "scw"
+    bin.install "scw"
   end
 
   test do
