ce26c10beb65a7cde915b5af47af76f7c5c530b0
Hs_Yeah <bYeahq@gmail.com>
2015-06-20 10:00:53 +0800

    Replace the correct sha1

--- Library/Formula/acpica.rb
+++ Library/Formula/acpica.rb
@@ -3,11 +3,11 @@ class Acpica < Formula
   homepage "https://www.acpica.org/"
   head "https://github.com/acpica/acpica.git"
   url "https://acpica.org/sites/acpica/files/acpica-unix2-20150619.tar_1.gz"
-  sha1 "8c5514b1171afb40dca40289581c8ba3f17583e1"
+  sha1 "283c594c4b89db8e3bef8fca25021c52938987ad"
 
   bottle do
     cellar :any
-    sha1 "283c594c4b89db8e3bef8fca25021c52938987ad" => :yosemite
+    sha1 "dbb1b43a1702b3eb2855dd4df1ffbb81a4e5e3c1" => :yosemite
     sha1 "5c2ded46bf71f187b637fd748d541985493ddf57" => :mavericks
     sha1 "c546de5f0bd9ead75dc58baba0489e0a37eb0f60" => :mountain_lion
   end
