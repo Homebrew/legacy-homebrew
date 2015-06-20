98fdf81b2d5f6c3b65b67759f9f209f292a2e120
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-20 22:51:26 +0100

    dnscrypt-wrapper: update 0.1.16 bottle.

--- Library/Formula/dnscrypt-wrapper.rb
+++ Library/Formula/dnscrypt-wrapper.rb
@@ -7,9 +7,9 @@ class DnscryptWrapper < Formula
 
   bottle do
     cellar :any
-    sha1 "6275cdc455d328a0b1201c0b4dd6543487b42715" => :yosemite
-    sha1 "384d364ee11cfb150b8072d303de6a7ed72ba450" => :mavericks
-    sha1 "db2434e57e8deb88e066cc575ca1052076156eec" => :mountain_lion
+    sha256 "0a72369104ba7b69642f7cf48de1bf659185fece25d9c157b0b8e19a5db56b10" => :yosemite
+    sha256 "5977dba418f3a05ca689e04025333d9ba0755d4c4a6bd5875afbe96242dcfe8c" => :mavericks
+    sha256 "b15c78585bf3b49dcfb244c812804388f93b423198e465ce10129033edf27fd8" => :mountain_lion
   end
 
   depends_on "autoconf" => :build
