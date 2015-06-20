d566e003383aeea61140522adb5f68378fa620c0
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-20 03:10:28 +0100

    fluid-synth: add portaudio option
    Closes #40906.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/fluid-synth.rb
+++ Library/Formula/fluid-synth.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class FluidSynth < Formula
   desc "Real-time software synthesizer based on the SoundFont 2 specs"
-  homepage 'http://www.fluidsynth.org'
-  url 'https://downloads.sourceforge.net/project/fluidsynth/fluidsynth-1.1.6/fluidsynth-1.1.6.tar.gz'
-  sha1 '155de731e72e91e1d4b7f52c33d8171596fbf244'
+  homepage "http://www.fluidsynth.org"
+  url "https://downloads.sourceforge.net/project/fluidsynth/fluidsynth-1.1.6/fluidsynth-1.1.6.tar.gz"
+  sha256 "50853391d9ebeda9b4db787efb23f98b1e26b7296dd2bb5d0d96b5bccee2171c"
 
   bottle do
     cellar :any
@@ -13,15 +11,25 @@ class FluidSynth < Formula
     sha1 "5cde076c223fa805c4e9fba1d4b7cdf5f5a4b5d5" => :lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'cmake' => :build
-  depends_on 'glib'
-  depends_on 'libsndfile' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "glib"
+  depends_on "libsndfile" => :optional
+  depends_on "portaudio" => :optional
 
   def install
-    mkdir 'build' do
-      system "cmake", "..", "-Denable-framework=OFF", "-DLIB_SUFFIX=", *std_cmake_args
-      system "make install"
+    args = std_cmake_args
+    args << "-Denable-framework=OFF" << "-DLIB_SUFFIX="
+    args << "-Denable-portaudio=ON" if build.with? "portaudio"
+    args << "-Denable-libsndfile=OFF" if build.without? "libsndfile"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
     end
+  end
+
+  test do
+    assert_match /#{version}/, shell_output("#{bin}/fluidsynth --version")
   end
 end
