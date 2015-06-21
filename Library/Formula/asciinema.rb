4036e7fe0e52a4b7fff5d51b30bda2ca2fe6f893
Marcin Kulik <m@ku1ik.com>
2015-06-21 18:19:04 +0200

    asciinema 1.1.1
    Bump version and remove go resource definitions as the project vendors everything since 1.0.0.
    
    Closes #40946.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/asciinema.rb
+++ Library/Formula/asciinema.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Asciinema < Formula
   desc "Record and share terminal sessions"
   homepage "https://asciinema.org/"
-  url "https://github.com/asciinema/asciinema/archive/v1.1.0.tar.gz"
-  sha256 "2f03549620534341e883b630c6947c3b4ecd234105ec1d7aa98393a00f0845e8"
+  url "https://github.com/asciinema/asciinema/archive/v1.1.1.tar.gz"
+  sha256 "841b3393a65a4f49a01354aed4e2da6c30822dc83bcd988ff100fabda7038055"
 
   head "https://github.com/asciinema/asciinema.git"
 
@@ -17,26 +17,10 @@ class Asciinema < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/kr/pty" do
-    url "https://github.com/kr/pty.git",
-      :revision => "5cf931ef8f76dccd0910001d74a58a7fca84a83d"
-  end
-
-  go_resource "code.google.com/p/go.crypto" do
-    url "https://code.google.com/p/go.crypto/",
-      :revision => "aa2644fe4aa5", :using => :hg
-  end
-
-  go_resource "code.google.com/p/gcfg" do
-    url "https://code.google.com/p/gcfg/",
-      :revision => "c2d3050044d05357eaf6c3547249ba57c5e235cb", :using => :git
-  end
-
   def install
     ENV["GOPATH"] = buildpath
     mkdir_p buildpath/"src/github.com/asciinema"
     ln_s buildpath, buildpath/"src/github.com/asciinema/asciinema"
-    Language::Go.stage_deps resources, buildpath/"src"
 
     system "go", "build", "-o", bin/"asciinema"
   end
