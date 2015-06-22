56075d493909b55f29c9129de1c5dcb400d454c2
Denis Gladkikh <denis@gladkikh.email>
2015-06-22 14:23:52 -0700

    gotags 1.3.0 changes space tabs to 2

--- Library/Formula/gotags.rb
+++ Library/Formula/gotags.rb
@@ -1,21 +1,21 @@
 class Gotags < Formula
-    desc "ctags-compatible tag generator for Go"
-    homepage "https://github.com/jstemmer/gotags"
-    url "https://github.com/jstemmer/gotags/archive/v1.3.0.tar.gz"
-    sha256 "414e1f96b560b089f11f814cd9000974a8ee376bb2cd9119cce60368e89ba226"
+  desc "ctags-compatible tag generator for Go"
+  homepage "https://github.com/jstemmer/gotags"
+  url "https://github.com/jstemmer/gotags/archive/v1.3.0.tar.gz"
+  sha256 "414e1f96b560b089f11f814cd9000974a8ee376bb2cd9119cce60368e89ba226"
 
-    head "https://github.com/jstemmer/gotags.git"
+  head "https://github.com/jstemmer/gotags.git"
 
-    depends_on "go" => :build
+  depends_on "go" => :build
 
-    def install
-        ENV["GOPATH"] = buildpath
+  def install
+    ENV["GOPATH"] = buildpath
 
-        system "go", "build", "-o", "gotags"
-        bin.install "gotags"
-    end
+    system "go", "build", "-o", "gotags"
+    bin.install "gotags"
+  end
 
-    test do
-        system "#{bin}/gotags", "-v"
-    end
+  test do
+    system "#{bin}/gotags", "-v"
+  end
 end
