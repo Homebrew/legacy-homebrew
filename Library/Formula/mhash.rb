2f496480b2e38f01161804a8b603446f29ad6263
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-22 19:38:09 +0100

    mhash: add 0.9.9.9 bottle.

--- Library/Formula/mhash.rb
+++ Library/Formula/mhash.rb
@@ -4,6 +4,13 @@ class Mhash < Formula
   url "https://downloads.sourceforge.net/project/mhash/mhash/0.9.9.9/mhash-0.9.9.9.tar.gz"
   sha256 "3dcad09a63b6f1f634e64168dd398e9feb9925560f9b671ce52283a79604d13e"
 
+  bottle do
+    cellar :any
+    sha256 "fb03873f042a16fd2db5ae2a7eb62e970927b75a9dff92decbb3fd035a2bd41f" => :yosemite
+    sha256 "eb2799dca9f7c9d020e76034361c5859a4dc7e8aecaf4f1e989901b12ef4420c" => :mavericks
+    sha256 "611123db41bf9d43b1937fe15451da7a0f4d2049660a98a4237618345b8d4a50" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
