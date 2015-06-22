567007d19d87d81dbfcf736d43e1f6469708bee9
Alan Ivey <alanivey@gmail.com>
2015-06-22 10:02:32 -0400

    mhash: modernize formula, add test

--- Library/Formula/mhash.rb
+++ Library/Formula/mhash.rb
@@ -1,14 +1,24 @@
-require 'formula'
-
 class Mhash < Formula
   desc "Uniform interface to a large number of hash algorithms"
-  homepage 'http://mhash.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/mhash/mhash/0.9.9.9/mhash-0.9.9.9.tar.gz'
-  sha1 'c898de5ea60d9e0873a1b73caa031bb1b5797c03'
+  homepage "http://mhash.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/mhash/mhash/0.9.9.9/mhash-0.9.9.9.tar.gz"
+  sha256 "3dcad09a63b6f1f634e64168dd398e9feb9925560f9b671ce52283a79604d13e"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include "mhash.h"
+      int main() {
+        MHASH td;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-L#{lib}", "-lmhash", "-o", "test"
+    system "./test"
   end
 end
