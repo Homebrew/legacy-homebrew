b9e20b93a0955e638f1f3c694596c6ed9300f16a
Denis Gladkikh <denis@gladkikh.email>
2015-06-22 09:35:58 -0700

    gotags 1.3.0

---
+++ Library/Formula/gotags.rb
@@ -0,0 +1,30 @@
+class Gotags < Formula
+    desc "ctags-compatible tag generator for Go"
+    homepage "https://github.com/jstemmer/gotags"
+    url "https://github.com/jstemmer/gotags/archive/v1.3.0.tar.gz"
+    sha256 "414e1f96b560b089f11f814cd9000974a8ee376bb2cd9119cce60368e89ba226"
+
+    head do
+        url "https://github.com/jstemmer/gotags.git"
+    end
+
+    depends_on "go" => :build
+
+    def install
+        ENV["GOPATH"] = buildpath
+
+        system "go", "build", "-o", "gotags"
+        bin.install "gotags"
+    end
+
+    def caveats
+        <<-EOS.undent
+        Check https://github.com/jstemmer/gotags/blob/master/README.md
+        for usage and Vim/Emacs configuration
+        EOS
+    end
+
+    test do
+        system "#{bin}/gotags", "-v"
+    end
+end
