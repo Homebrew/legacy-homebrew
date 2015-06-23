25d1c9bb71dc1dca7df026b34cf117fd8586a5c9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-23 06:06:26 +0100

    pipebench: add 0.40 bottle.

--- Library/Formula/pipebench.rb
+++ Library/Formula/pipebench.rb
@@ -1,6 +1,13 @@
 class Pipebench < Formula
   desc "Measure the speed of STDIN/STDOUT communication"
   homepage "http://www.habets.pp.se/synscan/programs.php?prog=pipebench"
+  bottle do
+    cellar :any
+    sha256 "a999c7ba2978e60d38bdeec63d1f1a8b2667cb0d77d35c4da2e57212a37b85ac" => :yosemite
+    sha256 "c5b0805588d8a8d9047d30b9330dd0be0215bfe694ed662cf11fa84595bba85c" => :mavericks
+    sha256 "217b9b59e5592718c530e4843cb0dfdc830f9f8fc175766abfd3cade97c66c80" => :mountain_lion
+  end
+
   # Upstream server behaves oddly: https://github.com/Homebrew/homebrew/issues/40897
   # url "http://www.habets.pp.se/synscan/files/pipebench-0.40.tar.gz"
   url "https://mirrors.ocf.berkeley.edu/debian/pool/main/p/pipebench/pipebench_0.40.orig.tar.gz"
