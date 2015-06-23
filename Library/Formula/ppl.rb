e87588a3f3cdbedccb76b81beb644a56cace0e7c
Francois BERENGER <francois.berenger@inria.fr>
2015-06-23 09:47:03 +0200

    added license info

--- Library/Formula/ppl.rb
+++ Library/Formula/ppl.rb
@@ -1,5 +1,5 @@
 class Ppl < Formula
-  desc "Parma Polyhedra Library"
+  desc "Parma Polyhedra Library; GPL licensed"
   homepage "http://bugseng.com/products/ppl"
   url "http://bugseng.com/products/ppl/download/ftp/releases/1.1/ppl-1.1.tar.xz"
   sha256 "c48ccd74664ec2cd3cdb5e37f287974ccb062f0384dc658d4053c424b19ad178"
