19dadaa850cc5aa14f17bd15d93097d7f738fe2f
Brent Vatne <brentvatne@gmail.com>
2015-06-22 17:48:24 -0700

    Fix based on feedback

--- Library/Formula/trace2html.rb
+++ Library/Formula/trace2html.rb
@@ -1,19 +1,19 @@
 class Trace2html < Formula
   desc "Utility from Google Trace Viewer to convert JSON traces to HTML"
   homepage "https://github.com/google/trace-viewer"
-  url "https://github.com/google/trace-viewer/archive/a4c4894801935627ff412c3cc6c3ddae96019cc5.zip"
-  sha256 "b9fc71e6f1a36f91ca09c1ea26ae2c38e42d3183b6470e603d1db8b1ee6e8316"
+  url "https://github.com/google/trace-viewer/archive/a4c4894801935627ff412c3cc6c3ddae96019cc5.tar.gz"
+  sha256 "40bf1613e23c0f483536667d3ab584889c50219768414f0086150b3c364269c3"
 
-  depends_on "Python"
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    cp_r(buildpath, libexec)
+    libexec.install Dir["*"]
     bin.install_symlink libexec/"trace2html"
   end
 
   test do
     touch "test.json"
-    system "#{bin}/trace2html test.json"
+    system "#{bin}/trace2html", "test.json"
     assert File.exist?("test.html")
   end
 end
