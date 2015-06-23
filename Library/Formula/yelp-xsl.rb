ecb924930ace7438442e1173103c7f18450f2684
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-23 15:28:02 +0100

    yelp-xsl: add 3.16.1 bottle.

--- Library/Formula/yelp-xsl.rb
+++ Library/Formula/yelp-xsl.rb
@@ -4,6 +4,13 @@ class YelpXsl < Formula
   url "https://download.gnome.org/sources/yelp-xsl/3.16/yelp-xsl-3.16.1.tar.xz"
   sha256 "3295eecc4b03d2a239f7a1bdf4a1311d34c46c3055e6535c1f72bb5a49b4174a"
 
+  bottle do
+    cellar :any
+    sha256 "7517192fb0c5b6bc6d5133486cb460fc25cb9e12327752401c34f38045030518" => :yosemite
+    sha256 "e8cba6dd4211955a183b926eeb6aa2ba1aafe7571d129029eb8d8b5492adea0d" => :mavericks
+    sha256 "ce9aa0d02239264b3a651cfe5d13f1675bb276325a079de5efca83a1940f29cb" => :mountain_lion
+  end
+
   depends_on "libxslt"
   depends_on "libxml2"
   depends_on "itstool" => :build
