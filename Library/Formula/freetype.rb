6d07ab3c159f0198ad3ba62c49892c72e053d51b
Tim D. Smith <git@tim-smith.us>
2015-06-23 15:58:26 -0700

    freetype: fix R build
    Don't #define TYPEOF; see Homebrew/homebrew-science#2434.
    
    Closes #41020.

--- Library/Formula/freetype.rb
+++ Library/Formula/freetype.rb
@@ -4,6 +4,7 @@ class Freetype < Formula
   url "https://downloads.sf.net/project/freetype/freetype2/2.6/freetype-2.6.tar.bz2"
   mirror "http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.bz2"
   sha256 "8469fb8124764f85029cc8247c31e132a2c5e51084ddce2a44ea32ee4ae8347e"
+  revision 1
 
   bottle do
     cellar :any
@@ -18,6 +19,14 @@ class Freetype < Formula
   option "without-subpixel", "Disable sub-pixel rendering (a.k.a. LCD rendering, or ClearType)"
 
   depends_on "libpng"
+
+  # Don't define a TYPEOF macro in ftconfig.h
+  # https://savannah.nongnu.org/bugs/index.php?45376
+  # http://git.savannah.gnu.org/cgit/freetype/freetype2.git/commit/?id=5931268eecaeda3e05580bdc8885348fecc43fa8
+  patch do
+    url "https://gist.githubusercontent.com/anonymous/b47d77c41a6801879fd2/raw/fc21c3516b465095da7ed13f98bea491a7d18bbd/patch"
+    sha256 "5b21575d0384c9e502b51b0ba4be0ff453a34bcf9deba52b6baa38c3ffcde063"
+  end
 
   def install
     if build.with? "subpixel"
