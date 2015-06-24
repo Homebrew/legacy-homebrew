546f09f360a7d6374942ae070fbe8eed4cf82b73
Franklin Chen <franklinchen@franklinchen.com>
2015-06-24 13:59:32 -0400

    More formula cleanup.

--- Library/Formula/mosml.rb
+++ Library/Formula/mosml.rb
@@ -1,18 +1,14 @@
 class Mosml < Formula
   homepage "http://mosml.org"
   url "https://github.com/kfl/mosml/archive/ver-2.10.1.tar.gz"
-  sha1 "e53fa82074d1c60499dc4ca83521c229e0655ccc"
+  sha256 "fed5393668b88d69475b070999b1fd34e902591345de7f09b236824b92e4a78f"
 
   depends_on "gmp"
 
   def install
-    cd 'src' do
-      system "make", "PREFIX=#{prefix}",
-                     "CC=#{ENV.cc}",
-                     "world"
-      system "make", "PREFIX=#{prefix}",
-                     "CC=#{ENV.cc}",
-                     "install"
+    cd "src" do
+      system "make", "PREFIX=#{prefix}", "CC=#{ENV.cc}", "world"
+      system "make", "PREFIX=#{prefix}", "CC=#{ENV.cc}", "install"
     end
   end
 
