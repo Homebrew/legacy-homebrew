a44c4a23b1eef972d62e9f3f946a0b43d86175b3
Franklin Chen <franklinchen@franklinchen.com>
2015-06-24 10:15:08 -0400

    Add back mosml, most recent version with clean build. Address https://github.com/Homebrew/homebrew/issues/17210 properly.

---
+++ Library/Formula/mosml.rb
@@ -0,0 +1,24 @@
+require "formula"
+
+class Mosml < Formula
+  homepage "http://mosml.org"
+  url "https://github.com/kfl/mosml/archive/ver-2.10.1.tar.gz"
+  sha1 "e53fa82074d1c60499dc4ca83521c229e0655ccc"
+
+  depends_on "gmp"
+
+  def install
+    cd 'src' do
+      system "make", "PREFIX=#{prefix}",
+                     "CC=#{ENV.cc}",
+                     "world"
+      system "make", "PREFIX=#{prefix}",
+                     "CC=#{ENV.cc}",
+                     "install"
+    end
+  end
+
+  test do
+    system "mosml", "-P full"
+  end
+end
