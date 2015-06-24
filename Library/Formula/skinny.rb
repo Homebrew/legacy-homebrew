78ae0ae9b7c273cfa0b9714a492068537b5b9a23
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 00:47:37 +0100

    skinny: update 1.3.19 bottle.

--- Library/Formula/skinny.rb
+++ Library/Formula/skinny.rb
@@ -6,9 +6,9 @@ class Skinny < Formula
 
   bottle do
     cellar :any
-    sha256 "ee2dcfdd576d69810a50176c9b879b1f567e600edc3e892b0bb9dcbaca72763d" => :yosemite
-    sha256 "0b93e3d12480db7a506129d71e0ed74198eec08ee2ad363bef6c2cacc30ca396" => :mavericks
-    sha256 "54dda9c068592af2c27c77ba38049d64119c78a5c2a7c7ab722d248473f0394a" => :mountain_lion
+    sha256 "b4c935695d70c76e939cd5d37d3fb3fe631a6e5bc0ef93be7ef12bb2c66c5854" => :yosemite
+    sha256 "e24d6147aec3faeff41a1147f4fa0c3b646e4f321b7459943eef7eacfdcf4117" => :mavericks
+    sha256 "925d1f6bf905805e98d5d316de2baff5f0258928d3db0cffc6bb7592ba6e0667" => :mountain_lion
   end
 
   option "without-npm-generator", "Yeoman generator will not be installed"
