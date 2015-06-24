feea358ab414521ceeaa83dda0f4d39b2921de99
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-24 06:23:33 +0100

    smpeg2: add 2.0.0 bottle.

--- Library/Formula/smpeg2.rb
+++ Library/Formula/smpeg2.rb
@@ -4,6 +4,13 @@ class Smpeg2 < Formula
   url "svn://svn.icculus.org/smpeg/tags/release_2_0_0/", :revision => "408"
   head "svn://svn.icculus.org/smpeg/trunk"
 
+  bottle do
+    cellar :any
+    sha256 "6031bf704fd0508bb90322dbe77f62580708e3fe77362e3dea6b0691360b686b" => :yosemite
+    sha256 "fa5760a0f8ff18f596b0044a0da7562a361904f2520a7406c3681ace8a705950" => :mavericks
+    sha256 "fe631e594d91ffa786d7023d7eb2b8b8a25d747a2e227458da26aaf9b935dfc8" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
