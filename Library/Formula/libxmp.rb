9a591afbe8050bf451533c8c766524e053d7eef5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 04:08:41 +0100

    libxmp: update 4.3.9 bottle.

--- Library/Formula/libxmp.rb
+++ Library/Formula/libxmp.rb
@@ -6,9 +6,9 @@ class Libxmp < Formula
 
   bottle do
     cellar :any
-    sha256 "6667ec2fe01c9907bb496aedca3fb28a3b9e4640ab6d35a08a39e41820d84a5c" => :yosemite
-    sha256 "94d3c9fd92050aa08d3b56631d68e3ca0f9ad537fbc2dc5ac096f339f57bb9b6" => :mavericks
-    sha256 "570deab3ca3accf745115d103e88dcb185d1b1ca3523d8ca643cdf623a92d361" => :mountain_lion
+    sha256 "157fc6129d524c09e4a579518ce188232c57289796bd12dbc2336903db96585e" => :yosemite
+    sha256 "1deb9f6fbcc61ed06c214689e28dd4eb928e0fc48d7b4f195957127c9a63f30f" => :mavericks
+    sha256 "404246eba6c82c4768baef86453a8317fb4ec911619cd74e391332675c62d45e" => :mountain_lion
   end
 
   # CC BY-NC-ND licensed set of five mods by Keith Baylis/Vim! for testing purposes
