55b001d62c48c3e7174bc856935df2a564f0a367
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 07:04:27 +0100

    cv: add 0.7.1 bottle.

--- Library/Formula/cv.rb
+++ Library/Formula/cv.rb
@@ -5,6 +5,13 @@ class Cv < Formula
   sha256 "c8ab81b09f6026cbfdc94c9453d4a0fad7ea5e1e34efd1c1559f88f7398cf4ee"
   head "https://github.com/Xfennec/cv.git"
 
+  bottle do
+    cellar :any
+    sha256 "ca3b822899bd8c370fb4049f9b68574ad3bc897495bd6bb985279b29c70c554c" => :yosemite
+    sha256 "0ade55ecc626b9559aa842b190cebcef0791287b019502eff106abe53960a4c7" => :mavericks
+    sha256 "849b187b69a23fbfad43121337a89c618426080571bc6d87e99b067a3dcfe4c7" => :mountain_lion
+  end
+
   def install
     system "make", "PREFIX=#{prefix}", "install"
   end
