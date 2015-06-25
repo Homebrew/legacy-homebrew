462827a90be6e403e5ca31f7d9d393123a4c5a30
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 18:53:56 +0100

    norm: add 1.5b3 bottle.

--- Library/Formula/norm.rb
+++ Library/Formula/norm.rb
@@ -4,6 +4,13 @@ class Norm < Formula
   url "https://downloads.pf.itd.nrl.navy.mil/norm/archive/src-norm-1.5b3.tgz"
   sha256 "ec2014f57be12ea3ade0685faa1e6e161f168311c8975093bce33a6e1dc5e77c"
 
+  bottle do
+    cellar :any
+    sha256 "eeb502e10f6407499c78292b29177c49f0723bf5ea5d199b11ce4aad4ed2be6f" => :yosemite
+    sha256 "973ac3cfc7ee43dfddfbfb4cd967ceb833ba35d4b29c09db716883c02be176a7" => :mavericks
+    sha256 "d8449b294d71b3fbba503c2ef1f3399f85dc5e1aad7fbf6f5b0209ca2e5a80e6" => :mountain_lion
+  end
+
   def install
     system "./waf", "configure", "--prefix=#{prefix}"
     system "./waf", "install"
