a101b066c41142837e502794250d56d50c249f9b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-25 19:01:06 +0100

    gist: add 4.4.0 bottle.

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -7,6 +7,13 @@ class Gist < Formula
   sha1 "142859ba1eb9385fa78d936ad4efd62584bd618b"
   head "https://github.com/defunkt/gist.git"
 
+  bottle do
+    cellar :any
+    sha256 "c0c9c72e201561ad39f5a3a75b2df8ec4de02c10c7ba0d4a322c3bce2b41d7e5" => :yosemite
+    sha256 "09c7ca22267c70e5c9e7382aec49a74ade8723df7c94a5d7b13ef5f8864fed64" => :mavericks
+    sha256 "c8591b8d83a21737899487c461ee856a768b024bf58dfc76a0f0331757884c7a" => :mountain_lion
+  end
+
   def install
     rake "install", "prefix=#{prefix}"
   end
