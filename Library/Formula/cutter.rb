5f8f756f9759c6c4d4d9dab1e860211521cf12a2
Xu Cheng <xucheng@me.com>
2015-06-25 22:44:46 +0800

    cutter: fix desc and modernize
    Fixes #41073.

--- Library/Formula/cutter.rb
+++ Library/Formula/cutter.rb
@@ -1,10 +1,9 @@
-require "formula"
-
 class Cutter < Formula
-  desc "Disconnect routed IP connections"
+  desc "a Unit Testing Framework for C and C++"
   homepage "http://cutter.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/cutter/cutter/1.2.4/cutter-1.2.4.tar.gz"
-  sha1 "cf4bc0dc92fc8ac51441536544acc8a81a195a79"
+  sha256 "b917a5b67b1d5f24458db5ab177dc11d23b47f04f9cb7eef757f456483c629c6"
+  head "https://github.com/clear-code/cutter.git"
 
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
@@ -18,6 +17,12 @@ class Cutter < Formula
                           "--disable-gstreamer",
                           "--disable-libsoup"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    touch "1.txt"
+    touch "2.txt"
+    system bin/"cut-diff", "1.txt", "2.txt"
   end
 end
