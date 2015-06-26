24adbf9ffec37f7d1281813fb7ecf22202c7cc04
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-26 06:10:00 +0100

    xcode: update clang version
    Closes #41095.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Homebrew/os/mac/xcode.rb
+++ Library/Homebrew/os/mac/xcode.rb
@@ -119,7 +119,8 @@ module OS
           when 51      then "5.1"
           when 60      then "6.0"
           when 61      then "6.1"
-          else "6.1"
+          when 70      then "7.0"
+          else "7.0"
           end
         end
       end
@@ -162,7 +163,7 @@ module OS
 
       def latest_version
         case MacOS.version
-        when "10.11" then "700.0.53"
+        when "10.11" then "700.0.53.3"
         when "10.10" then "602.0.53"
         when "10.9"  then "600.0.57"
         when "10.8"  then "503.0.40"
