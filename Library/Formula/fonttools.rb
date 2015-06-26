8558330ab9c4dbdf4c22d8141f29344650c822a8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-26 19:42:14 +0100

    fonttools: add 2.5 bottle.

--- Library/Formula/fonttools.rb
+++ Library/Formula/fonttools.rb
@@ -5,6 +5,13 @@ class Fonttools < Formula
   sha256 "c89603f9f3346f48f4a24f786422e935423554e0a9172dcd3ec8ffbd556d2159"
   head "https://github.com/behdad/fonttools.git"
 
+  bottle do
+    cellar :any
+    sha256 "ec7dafb8a42f26a326f2569fd9c9ac20586202ade9869d0558e21362eda42058" => :yosemite
+    sha256 "eacf6abd99b4a9038d5635a2d5427e1bb65d5040dc71caf46b9c68950395d702" => :mavericks
+    sha256 "ddfe6802322d603209ecd08f64cf65ee3ebaff5a7ff27782a4a79671e8af821c" => :mountain_lion
+  end
+
   option "with-pygtk", "Build with pygtk support for pyftinspect"
 
   depends_on :python if MacOS.version <= :snow_leopard
