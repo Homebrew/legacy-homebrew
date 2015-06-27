6a8735196a5f7d1167b1859392e8821f9dc18b2f
Rob Hardwick <robhardwick@gmail.com>
2015-06-27 11:18:45 +0100

    rtmidi 2.1.0 (new formula)
    RtMidi - a set of C++ classes that provide a common API for realtime
    MIDI input/output across Linux (ALSA & JACK), Macintosh OS X (CoreMidi &
    JACK) and Windows (Multimedia).

---
+++ Library/Formula/rtmidi.rb
@@ -0,0 +1,16 @@
+class Rtmidi < Formula
+  desc "C++ classes that provide a common API for realtime MIDI input/output"
+  homepage "http://www.music.mcgill.ca/~gary/rtmidi/"
+  url "https://github.com/thestk/rtmidi/archive/2.1.0.tar.gz"
+  sha256 "52e6822fc413d5d3963c5b7bfe412ed69233bb81a7f04d6097f5b56aafa28934"
+
+  depends_on "autoconf"
+
+  def install
+    system "autoconf"
+    system "./configure", "--prefix=#{prefix}"
+    system "make librtmidi.a"
+    lib.install Dir["*.a", "*.dylib"]
+    include.install Dir["*.h"]
+  end
+end
