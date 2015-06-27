da1bd6e3c1bff0373510645276a5ae6a08daa193
Alex Dunn <dunn.alex@gmail.com>
2015-06-27 10:52:00 -0700

    cheat 2.1.9
    Closes #41135.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/cheat.rb
+++ Library/Formula/cheat.rb
@@ -1,8 +1,8 @@
 class Cheat < Formula
   desc "Create and view interactive cheat sheets for *nix commands"
   homepage "https://github.com/chrisallenlane/cheat"
-  url "https://github.com/chrisallenlane/cheat/archive/2.1.8.tar.gz"
-  sha1 "75176e3f8c75f4a476a7ecb53497e72eceace2e2"
+  url "https://github.com/chrisallenlane/cheat/archive/2.1.9.tar.gz"
+  sha256 "58e4bc81a29d1ca659430c5bced6aaba836a023c55a265561c1a51af44fdce3c"
   head "https://github.com/chrisallenlane/cheat.git"
 
   bottle do
@@ -16,12 +16,12 @@ class Cheat < Formula
 
   resource "docopt" do
     url "https://pypi.python.org/packages/source/d/docopt/docopt-0.6.2.tar.gz"
-    sha1 "224a3ec08b56445a1bd1583aad06b00692671e04"
+    sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
   end
 
   resource "Pygments" do
     url "https://pypi.python.org/packages/source/P/Pygments/Pygments-2.0.2.tar.gz"
-    sha1 "fe2c8178a039b6820a7a86b2132a2626df99c7f8"
+    sha256 "7320919084e6dac8f4540638a46447a3bd730fca172afc17d2c03eed22cf4f51"
   end
 
   def install
