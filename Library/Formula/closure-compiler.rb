7155d75519c1920caf72c6655d60bb1efe207e57
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-27 18:57:00 +0100

    closure-compiler: update 20150609 bottle.

--- Library/Formula/closure-compiler.rb
+++ Library/Formula/closure-compiler.rb
@@ -7,9 +7,9 @@ class ClosureCompiler < Formula
 
   bottle do
     cellar :any
-    sha256 "03bf823ee5f71f31c88ed4a412506bb7e0af4eb90f608d89aae86ac0d0828fec" => :yosemite
-    sha256 "94a4040f31ff31c318906e6fbf757cf3669b700a9ed07f9d665abbed30444575" => :mavericks
-    sha256 "e2609e83ace94265ebbbeeff90e3b2599917375eb5aecaaa694ba2998dea86c6" => :mountain_lion
+    sha256 "72e0992ba4be24216a35b4c19a7165f326d1b65ee92e528fab18390fe8e009f1" => :yosemite
+    sha256 "eb17bd8c3de9ece500c9fdfe9c9cb2cf4920c8311f0ca7d649dafecd488e6ebe" => :mavericks
+    sha256 "5c69940a23bc17167ff751a5e65945a08fb01ceaddabb8ea35579289730d819f" => :mountain_lion
   end
 
   depends_on :ant => :build
