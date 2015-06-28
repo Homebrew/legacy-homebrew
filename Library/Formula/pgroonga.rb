dab3265e2442461827d7d9b23d2e1107ba096aa3
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-06-28 22:11:54 +0900

    pgroonga 0.6.0 (new formula)

---
+++ Library/Formula/pgroonga.rb
@@ -0,0 +1,16 @@
+class Pgroonga < Formula
+  desc "An extension for PostgreSQL to use Groonga as an index."
+  homepage "https://pgroonga.github.io/"
+  url "http://packages.groonga.org/source/pgroonga/pgroonga-0.6.0.tar.gz"
+  sha256 "29a631f68013bba19321beac0d7b933eafce60aec1597aa8fff0b0898ff2a73d"
+
+  depends_on "cmake" => :build
+  depends_on "pkgconfig" => :build
+  depends_on "postgresql"
+  depends_on "groonga"
+
+  def install
+    system "make"
+    system "make", "install"
+  end
+end
