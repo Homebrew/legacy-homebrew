70d65715dd7a86599f8915e42523e33085c6016a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-28 02:59:15 +0100

    getmail: add 4.48.0 bottle.

--- Library/Formula/getmail.rb
+++ Library/Formula/getmail.rb
@@ -5,6 +5,13 @@ class Getmail < Formula
   mirror "https://mirrors.kernel.org/debian/pool/main/g/getmail4/getmail4_4.48.0.orig.tar.gz"
   sha256 "49441e92eed577127331caf9b97f2ddaea14e97e8a49259efd9184a766a9b94c"
 
+  bottle do
+    cellar :any
+    sha256 "d419e3a1ef926b9f9883497b58b0adf5e4d2abab5568028f180689405b6f8044" => :yosemite
+    sha256 "525b3e87f0e39be465de58a425755800dc8c0cd00383c879008caa9ec4edc347" => :mavericks
+    sha256 "3549cc78fb6723551dd3c69bff5ccc8f8830bb2fa5d6261a8218fb24435dbcaf" => :mountain_lion
+  end
+
   # See: https://github.com/Homebrew/homebrew/pull/28739
   patch do
     url "https://gist.githubusercontent.com/sigma/11295734/raw/5a7f39d600fc20d7605d3c9e438257285700b32b/ssl_timeout.patch"
