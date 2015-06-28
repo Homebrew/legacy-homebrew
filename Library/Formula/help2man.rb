c03dd129bdf8a934e793da851a9c7a1c94ace0ec
Alex Dunn <dunn.alex@gmail.com>
2015-06-27 20:00:37 -0700

    help2man 1.47.1
    Closes #41146.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/help2man.rb
+++ Library/Formula/help2man.rb
@@ -1,9 +1,9 @@
 class Help2man < Formula
   desc "Automatically generate simple man pages"
   homepage "https://www.gnu.org/software/help2man/"
-  url "http://ftpmirror.gnu.org/help2man/help2man-1.46.5.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/help2man/help2man-1.46.5.tar.xz"
-  sha256 "0ada23867183c5e779e06e6441801c5c74ff77af258e2f1bb5fce181fbc30ebf"
+  url "http://ftpmirror.gnu.org/help2man/help2man-1.47.1.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/help2man/help2man-1.47.1.tar.xz"
+  sha256 "c59b26f60cb06e45b00e729dea721e7a17220e2c17d800eb428271a750382b06"
 
   bottle do
     cellar :any
