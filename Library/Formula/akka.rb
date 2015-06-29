5fc26e9374784e6e77d9cebf69f37fd7334f72bf
Atkins <atkinschang@icloud.com>
2015-06-30 01:06:03 +0800

    akka 2.4-M2 (devel)

--- Library/Formula/akka.rb
+++ Library/Formula/akka.rb
@@ -3,8 +3,17 @@ require "formula"
 class Akka < Formula
   desc "Toolkit for building concurrent, distributed, and fault tolerant apps"
   homepage "http://akka.io/"
-  url "https://downloads.typesafe.com/akka/akka_2.11-2.3.11.zip"
-  sha256 "58157e4f85024d66e20d7e14d2681e041e385af82985eceb5210a454c251abec"
+
+  stable do
+    url "https://downloads.typesafe.com/akka/akka_2.11-2.3.11.zip"
+    sha256 "58157e4f85024d66e20d7e14d2681e041e385af82985eceb5210a454c251abec"
+  end
+
+  devel do
+    url "https://downloads.typesafe.com/akka/akka_2.11-2.4-M2.zip"
+    sha256 "dc62020928a182cfd1125bd9ec5ff4ce4fcd3629004af450653cc45ad583cfd2"
+    version "2.4-M2"
+  end
 
   def install
     # Remove Windows files
@@ -13,12 +22,11 @@ class Akka < Formula
     chmod 0755, "bin/akka"
     chmod 0755, "bin/akka-cluster"
 
-    # dos to unix
-    inreplace ["bin/akka", "bin/akka-cluster"], "\r", ""
-
-    # Translate akka script
     inreplace ["bin/akka", "bin/akka-cluster"] do |s|
+      # Translate akka script
       s.gsub! /^declare AKKA_HOME=.*$/, "declare AKKA_HOME=#{libexec}"
+      # dos to unix (bug fix for version 2.3.11)
+      s.gsub! /\r?/, ""
     end
 
     libexec.install Dir["*"]
