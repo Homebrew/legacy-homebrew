6e581bc16f0895d45c7f8f50b7f63a0624b73bff
maverick <maverick@factual.com>
2015-06-29 17:20:16 +0800

    drake 1.0.0
    Closes #41292.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/drake.rb
+++ Library/Formula/drake.rb
@@ -1,20 +1,28 @@
-require 'formula'
-
 class Drake < Formula
   desc "Data workflow tool meant to be 'make for data'"
-  homepage 'https://github.com/Factual/drake'
-  url 'https://github.com/Factual/drake/releases/download/v0.1.7/drake.jar'
-  version '0.1.7'
-  sha1 'c3092bd4e62effa38d4ade09bc191463b1c93c6a'
+  homepage "https://github.com/Factual/drake"
+  url "https://raw.githubusercontent.com/Factual/drake/1.0.0/bin/drake-pkg"
+  version "1.0.0"
+  sha256 "6b75504f85f01a7d213c60f87e7da0d94a4961bbc7e9b7e90f79efcc38ae5165"
+  head "https://github.com/Factual/drake.git"
+
+  resource "jar" do
+    url "https://github.com/Factual/drake/releases/download/1.0.0/drake.jar"
+    sha256 "f601f059dd23f87ccb1fa9ce1c39067e8bbeed36f08820769c6132c311e99aa8"
+  end
 
   def install
-    libexec.install Dir['*']
-    bin.write_jar_script libexec/'drake.jar', 'drake'
+    jar = "drake-#{version}-standalone.jar"
+    inreplace "drake-pkg", /DRAKE_JAR/, libexec/jar
+    bin.install "drake-pkg" => "drake"
+    resource("jar").stage do
+      libexec.install "drake.jar" => jar
+    end
   end
 
   test do
     # count lines test
-    (testpath/'Drakefile').write <<-EOS.undent
+    (testpath/"Drakefile").write <<-EOS.undent
       find_lines <- [shell]
         echo 'drake' > $OUTPUT
 
