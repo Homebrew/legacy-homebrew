9edfeb554b1abc31e517d1643e2017ecc2cbb6f2
Andrew Barchuk <raindev@icloud.com>
2015-06-29 13:40:13 +0300

    Make go-app-engine formulae use SHA-256
    Closes #41180.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/go-app-engine-32.rb
+++ Library/Formula/go-app-engine-32.rb
@@ -4,7 +4,7 @@ class GoAppEngine32 < Formula
   desc "Google App Engine SDK for Go!"
   homepage "https://cloud.google.com/appengine/docs/go/"
   url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_386-1.9.23.zip"
-  sha1 "43532c19ea8a0c282ddb93cdf8af079a280c2240"
+  sha256 "01271376dd0f04e1ff3bc52d71f2a933b4add478a6c9e8ca3b2de3468327b561"
 
   def install
     cd ".."
