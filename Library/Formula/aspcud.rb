604494136f1cb0c44d8c5534422a4bb432b24308
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-29 20:17:28 +0100

    aspcud: fix style nits

--- Library/Formula/aspcud.rb
+++ Library/Formula/aspcud.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Aspcud < Formula
   desc "Package dependency solver"
-  homepage 'http://potassco.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/potassco/aspcud/1.9.1/aspcud-1.9.1-source.tar.gz'
-  sha256 'e0e917a9a6c5ff080a411ff25d1174e0d4118bb6759c3fe976e2e3cca15e5827'
+  homepage "http://potassco.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/potassco/aspcud/1.9.1/aspcud-1.9.1-source.tar.gz"
+  sha256 "e0e917a9a6c5ff080a411ff25d1174e0d4118bb6759c3fe976e2e3cca15e5827"
 
   bottle do
     sha256 "060fd7d438962d4cfb70b51121fef20f00c4bb5d510c0a7881b99e747c4e9cc5" => :yosemite
@@ -12,28 +10,33 @@ class Aspcud < Formula
     sha256 "f37a5588eece4ffb99b5eb5acd224f71f2286b482d85428bd2b76853acd23c7c" => :mountain_lion
   end
 
-  depends_on 'boost' => :build
-  depends_on 'cmake' => :build
-  depends_on 're2c'  => :build
-  depends_on 'gringo'
-  depends_on 'clasp'
+  depends_on "boost" => :build
+  depends_on "cmake" => :build
+  depends_on "re2c"  => :build
+  depends_on "gringo"
+  depends_on "clasp"
 
   def install
+    args = std_cmake_args
+    args << "-DGRINGO_LOC=#{Formula["gringo"].opt_bin}/gringo"
+    args << "-DCLASP_LOC=#{Formula["clasp"].opt_bin}/clasp"
+
     mkdir "build" do
-      system "cmake", "..", "-DGRINGO_LOC=#{Formula["gringo"].opt_bin}/gringo", "-DCLASP_LOC=#{Formula["clasp"].opt_bin}/clasp", *std_cmake_args
+      system "cmake", "..", *args
       system "make"
       system "make", "install"
     end
   end
 
   test do
-   fixture = <<-EOS.undent
+    fixture = <<-EOS.undent
       package: foo
       version: 1
 
       request: foo >= 1
     EOS
-    (testpath/'in.cudf').write(fixture)
+
+    (testpath/"in.cudf").write(fixture)
     system "#{bin}/aspcud", "in.cudf", "out.cudf"
   end
 end
