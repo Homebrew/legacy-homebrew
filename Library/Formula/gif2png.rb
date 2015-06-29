a7aea14de70dd5f85ef9c14b9520a9921c5bd8e3
Keith Shaw <github@keithws.net>
2015-06-29 12:05:56 -0700

    gif2png 2.5.10 (new formula)

---
+++ Library/Formula/gif2png.rb
@@ -0,0 +1,22 @@
+class Gif2png < Formula
+  homepage "http://www.catb.org/~esr/gif2png/"
+  url "http://www.catb.org/~esr/gif2png/gif2png-2.5.10.tar.gz"
+  sha256 "3a593156f335c4ea6be68e37e09994461193f31872362de4b27ef6301492d5fd"
+
+  depends_on "libpng"
+
+  def install
+    ENV.deparallelize # parallel install fails
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    require "base64"
+    (testpath/"test.gif").write(::Base64.decode64("R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="))
+    system "#{bin}/gif2png", "test.gif"
+  end
+end
