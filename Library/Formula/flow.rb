55413c344aa4a44aa052be4e2843088adef6b0a5
Jeff Morrison <lbljeffmo@gmail.com>
2015-06-29 21:48:48 -0400

    Flow v0.13.1

--- Library/Formula/flow.rb
+++ Library/Formula/flow.rb
@@ -3,8 +3,8 @@ require "formula"
 class Flow < Formula
   desc "Static type checker for JavaScript"
   homepage "http://flowtype.org/"
-  url "https://github.com/facebook/flow/archive/v0.12.0.tar.gz"
-  sha1 "fd8edf6cf291d66bc20053dd02eb66507f36023a"
+  url "https://github.com/facebook/flow/archive/v0.13.1.tar.gz"
+  sha1 "8185edc3598f4261b25cc4300f3f7cee12312240"
   head "https://github.com/facebook/flow.git"
 
   bottle do
