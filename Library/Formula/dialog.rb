517c5221134c031f4ff91e92a53733d01c83743d
Viktor Szakats <vszakats@users.noreply.github.com>
2015-06-30 10:43:06 +0200

    dialog: add https mirror
    Closes #41203.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/dialog.rb
+++ Library/Formula/dialog.rb
@@ -2,6 +2,7 @@ class Dialog < Formula
   desc "Display user-friendly dialog boxes from shell scripts"
   homepage "http://invisible-island.net/dialog/"
   url "ftp://invisible-island.net/dialog/dialog-1.2-20150528.tgz"
+  mirror "https://fossies.org/linux/misc/dialog-1.2-20150528.tgz"
   sha256 "a8cd7a66bdb41e53a3145cbb0eb370c5ce7300fe0e9ad6d3e8d3b9e16ff16418"
 
   bottle do
