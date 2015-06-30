12f8c3ad0fdefb4a4de91ceb069f7f2d14542e50
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-30 02:57:14 +0100

    ffmpeg: restore https url

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -1,7 +1,7 @@
 class Ffmpeg < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
-  url "http://ffmpeg.org/releases/ffmpeg-2.7.1.tar.bz2"
+  url "https://ffmpeg.org/releases/ffmpeg-2.7.1.tar.bz2"
   sha256 "7e07b97d2415feeae9c9b5595e35e7b7aab33207e81bf9f8c0d1eece43f7f720"
   head "https://github.com/FFmpeg/FFmpeg.git"
 
