ce0b5e3ba54285efa21548393f880883c13c6872
Viktor Szakats <vszakats@users.noreply.github.com>
2015-06-30 21:44:34 +0200

    rtmidi: use https homepage
    Closes #41218.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/rtmidi.rb
+++ Library/Formula/rtmidi.rb
@@ -1,6 +1,6 @@
 class Rtmidi < Formula
   desc "C++ classes that provide a common API for realtime MIDI input/output"
-  homepage "http://www.music.mcgill.ca/~gary/rtmidi/"
+  homepage "https://www.music.mcgill.ca/~gary/rtmidi/"
   url "https://github.com/thestk/rtmidi/archive/2.1.0.tar.gz"
   sha256 "52e6822fc413d5d3963c5b7bfe412ed69233bb81a7f04d6097f5b56aafa28934"
 
