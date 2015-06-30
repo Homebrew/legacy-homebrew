66a43fb5478a75ab0b55b435d59500e8c08a989d
Rafael Campos <rafaelcdn@gmail.com>
2015-06-29 22:39:59 -0700

    Consolidation of arguments into one array

--- Library/Formula/openvdb.rb
+++ Library/Formula/openvdb.rb
@@ -30,67 +30,60 @@ class Openvdb < Formula
   def install
     ENV.cxx11
     # Adjust hard coded paths in Makefile
+    args = [
+      "DESTDIR=#{prefix}",
+      "BOOST_INCL_DIR=#{Formula["boost"].opt_lib}/include",
+      "BOOST_LIB_DIR=#{Formula["boost"].opt_lib}",
+      "BOOST_THREAD_LIB=-lboost_thread-mt",
+      "TBB_INCL_DIR=#{Formula["tbb"].opt_lib}/include",
+      "TBB_LIB_DIR=#{Formula["tbb"].opt_lib}/lib",
+      "EXR_INCL_DIR=#{Formula["openexr"].opt_lib}/include",
+      "EXR_LIB_DIR=#{Formula["openexr"].opt_lib}/lib",
+      "BLOSC_INCL_DIR=", # Blosc is not yet supported.
+      "PYTHON_VERSION=",
+      "NUMPY_INCL_DIR="
+    ]
+
     if build.with? "jemalloc"
-      jemalloc = "CONCURRENT_MALLOC_LIB_DIR=#{Formula["jemalloc"].opt_lib}/lib"
+      args << "CONCURRENT_MALLOC_LIB_DIR=#{Formula["jemalloc"].opt_lib}/lib"
     else
-      jemalloc = "CONCURRENT_MALLOC_LIB="
+      args << "CONCURRENT_MALLOC_LIB="
     end
 
     if build.with? "viewer"
-      glfw3 = "GLFW_INCL_DIR=#{Formula["homebrew/versions/glfw3"].opt_lib}/include"
-      glfw3_lib = "GLFW_LIB_DIR=#{Formula["homebrew/versions/glfw3"].opt_lib}/lib"
-      glfw3_libname =  "GLFW_LIB=-lglfw3"
+      args << "GLFW_INCL_DIR=#{Formula["homebrew/versions/glfw3"].opt_lib}/include"
+      args << "GLFW_LIB_DIR=#{Formula["homebrew/versions/glfw3"].opt_lib}/lib"
+      args << "GLFW_LIB=-lglfw3"
     else
-      glfw3 =  "GLFW_INCL_DIR="
-      glfw3_lib = "GLFW_LIB_DIR="
-      glfw3_libname = "GLFW_LIB="
+      args << "GLFW_INCL_DIR="
+      args << "GLFW_LIB_DIR="
+      args << "GLFW_LIB="
     end
 
     if build.with? "docs"
-      doxygen = "DOXYGEN=doxygen"
+      args << "DOXYGEN=doxygen"
     else
-      doxygen = "DOXYGEN="
+      args << "DOXYGEN="
     end
 
     if build.with? "tests"
-      cppunit = "CPPUNIT_INCL_DIR=#{Formula["cppunit"].opt_lib}/include"
-      cppunit_lib =  "CPPUNIT_LIB_DIR=#{Formula["cppunit"].opt_lib}/lib"
+      args << "CPPUNIT_INCL_DIR=#{Formula["cppunit"].opt_lib}/include"
+      args << "CPPUNIT_LIB_DIR=#{Formula["cppunit"].opt_lib}/lib"
     else
-      cppunit = "CPPUNIT_INCL_DIR="
-      cppunit_lib =  "CPPUNIT_LIB_DIR="
+      args << "CPPUNIT_INCL_DIR=" << "CPPUNIT_LIB_DIR="
     end
 
     if build.with? "logging"
-      log4cplus = "LOG4CPLUS_INCL_DIR=#{Formula["log4cplus"].opt_lib}/include"
-      log4cplus_lib =  "LOG4CPLUS_LIB_DIR=#{Formula["log4cplus"].opt_lib}/lib"
+      args << "LOG4CPLUS_INCL_DIR=#{Formula["log4cplus"].opt_lib}/include"
+      args << "LOG4CPLUS_LIB_DIR=#{Formula["log4cplus"].opt_lib}/lib"
     else
-      log4cplus = "LOG4CPLUS_INCL_DIR="
-      log4cplus_lib =  "LOG4CPLUS_LIB_DIR="
+      args << "LOG4CPLUS_INCL_DIR=" << "LOG4CPLUS_LIB_DIR="
     end
 
     ENV.append_to_cflags "-I #{buildpath}"
 
     cd "openvdb" do
-      system "make", "install", "DESTDIR=#{prefix}",
-                              "BOOST_INCL_DIR=#{Formula["boost"].opt_lib}/include",
-                              "BOOST_LIB_DIR=#{Formula["boost"].opt_lib}",
-                              "BOOST_THREAD_LIB=-lboost_thread-mt",
-                              "TBB_INCL_DIR=#{Formula["tbb"].opt_lib}/include",
-                              "TBB_LIB_DIR=#{Formula["tbb"].opt_lib}/lib",
-                              "EXR_INCL_DIR=#{Formula["openexr"].opt_lib}/include",
-                              "EXR_LIB_DIR=#{Formula["openexr"].opt_lib}/lib",
-                              "BLOSC_INCL_DIR=", # Blosc is not yet supported.
-                              "PYTHON_VERSION=",
-                              "NUMPY_INCL_DIR=",
-                              *jemalloc,
-                              *glfw3,
-                              *glfw3_lib,
-                              *glfw3_libname,
-                              *doxygen,
-                              *cppunit,
-                              *cppunit_lib,
-                              *log4cplus,
-                              *log4cplus_lib
+      system "make", "install", *args
     end
   end
 
