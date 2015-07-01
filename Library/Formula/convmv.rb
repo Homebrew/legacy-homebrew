d490c4416d6bff218e95517bbf08db2f4b79f674
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-01 22:38:57 +0100

    convmv: add 2.0 bottle.

--- Library/Formula/convmv.rb
+++ Library/Formula/convmv.rb
@@ -4,6 +4,13 @@ class Convmv < Formula
   url "https://www.j3e.de/linux/convmv/convmv-2.0.tar.gz"
   sha256 "170cf675be1fca77868ff472e9340ca828b1463865a63d4f4b7b3bf4053db93f"
 
+  bottle do
+    cellar :any
+    sha256 "0293e2f79297e7be509804444fe9ec437ae12f38a2aafab226b27fc2c41da1fe" => :yosemite
+    sha256 "14eb563118272c69d38b71a972976a0736757c529741c154ba311c1d9082313a" => :mavericks
+    sha256 "355114a774c9a036a10e77e8e8570f783fe57e50847c3c7f7bd449f016862900" => :mountain_lion
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
