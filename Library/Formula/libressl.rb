55908dbf569e81c8c50e5b0216070092425ee34e
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-01 23:23:36 +0100

    libressl: backport #41071 for expired certs

--- Library/Formula/libressl.rb
+++ Library/Formula/libressl.rb
@@ -47,8 +47,21 @@ class Libressl < Formula
       /System/Library/Keychains/SystemRootCertificates.keychain
     ]
 
-    # Bootstrap CAs from the system keychain.
-    (etc/"libressl/cert.pem").atomic_write `security find-certificate -a -p #{keychains.join(" ")}`
+    certs_list = `security find-certificate -a -p #{keychains.join(" ")}`
+    certs = certs_list.scan(
+      /-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----/m
+    )
+
+    valid_certs = certs.select do |cert|
+      IO.popen("openssl x509 -inform pem -checkend 0 -noout", "w") do |openssl_io|
+        openssl_io.write(cert)
+        openssl_io.close_write
+      end
+
+      $?.success?
+    end
+
+    (etc/"libressl/cert.pem").atomic_write(valid_certs.join("\n"))
   end
 
   def caveats; <<-EOS.undent
