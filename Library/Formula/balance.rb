21e2ba63576e2f07fa0f46c1817e4f76de1dc179
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-01 08:47:26 +0200

    balance 3.57
    Closes #41233.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/balance.rb
+++ Library/Formula/balance.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Balance < Formula
   desc "Software load balancer"
-  homepage 'http://www.inlab.de/balance.html'
-  url 'http://www.inlab.de/balance-3.56.tar.gz'
-  sha1 '04067301a9eda42659e88c3a9438bc1bf8a96902'
+  homepage "https://www.inlab.de/balance.html"
+  url "https://www.inlab.de/balance-3.57.tar.gz"
+  sha256 "b355f98932a9f4c9786cb61012e8bdf913c79044434b7d9621e2fa08370afbe1"
 
   def install
     system "make"
