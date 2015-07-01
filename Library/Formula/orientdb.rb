49982fc6d3cd2bc0d75accf2f1724534d4cf52fd
Shawn Poindexter <shawnpoindexter@gmail.com>
2015-07-01 07:14:41 -0700

    orientdb-2.0.12

--- Library/Formula/orientdb.rb
+++ Library/Formula/orientdb.rb
@@ -3,9 +3,8 @@ require 'formula'
 class Orientdb < Formula
   desc "Graph database"
   homepage 'http://www.orientdb.org/index.htm'
-  url 'http://www.orientechnologies.com/download.php?email=unknown@unknown.com&file=orientdb-community-2.0.11.tar.gz&os=mac'
-  version '2.0.11'
-  sha1 '812df4b971c3afc7bc9760b7de9bcc6680c060f0'
+  url 'https://orientdb.com/download.php?file=orientdb-community-2.0.12.tar.gz'
+  sha256 '68315fbcd9997451b84ea695fe862665fc5f8c29ec0a5e65d8a9cbe389687b56'
 
   bottle do
     cellar :any
@@ -17,7 +16,7 @@ class Orientdb < Formula
   # Fixing OrientDB init scripts
   patch do
     url "https://gist.githubusercontent.com/maggiolo00/84835e0b82a94fe9970a/raw/1ed577806db4411fd8b24cd90e516580218b2d53/orientdbsh"
-    sha1 "280284f3a8b6e280e46078b746f8250aa5648979"
+    sha256 "d8b89ecda7cb78c940b3c3a702eee7b5e0f099338bb569b527c63efa55e6487e"
   end
 
   def install
