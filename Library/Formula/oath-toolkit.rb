11f5c60d422fb0b3442b8bd20efeaec3312c06e2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-01 22:11:30 +0100

    oath-toolkit: add 2.6.0 bottle.

--- Library/Formula/oath-toolkit.rb
+++ Library/Formula/oath-toolkit.rb
@@ -5,6 +5,12 @@ class OathToolkit < Formula
   mirror "http://download-mirror.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.6.0.tar.gz"
   sha256 "2346cb8a2fc75ea56934cb9867463001665772308f2d9e7fe487159d38960926"
 
+  bottle do
+    sha256 "e9631ace23f064754302813192003e0032405265d0feb30ed725cf72bd412976" => :yosemite
+    sha256 "c288201dd37a7290ee995b57a5857099fe876fa2808c054a6cc4bfbda70fb07d" => :mavericks
+    sha256 "af35cf7a4e69382dfd0d9c00b62e83d194e36a0b3a8e9a22f1aca21f56bd4153" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libxmlsec1"
 
