0c8dd8c5f0ca72b6f447b03ebfcfb3f4ece47f50
Viktor Szakats <vszakats@users.noreply.github.com>
2015-07-02 01:04:36 +0200

    oath-toolkit: replace mirror with an https one
    Closes #41260.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/oath-toolkit.rb
+++ Library/Formula/oath-toolkit.rb
@@ -2,7 +2,7 @@ class OathToolkit < Formula
   desc "Tools for one-time password authentication systems"
   homepage "http://www.nongnu.org/oath-toolkit/"
   url "http://download.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.6.0.tar.gz"
-  mirror "http://download-mirror.savannah.gnu.org/releases/oath-toolkit/oath-toolkit-2.6.0.tar.gz"
+  mirror "https://fossies.org/linux/privat/oath-toolkit-2.6.0.tar.gz"
   sha256 "2346cb8a2fc75ea56934cb9867463001665772308f2d9e7fe487159d38960926"
 
   bottle do
@@ -17,6 +17,6 @@ class OathToolkit < Formula
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
