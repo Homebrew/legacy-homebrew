fbf3f49b8de3c4d658846c27ddc3fc1ed973c77a
Andrew Dunning <adunning@users.noreply.github.com>
2015-07-02 19:36:47 -0400

    pandoc 1.15.0.4
    Closes #41309.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/pandoc.rb
+++ Library/Formula/pandoc.rb
@@ -5,13 +5,12 @@ class Pandoc < Formula
 
   desc "Swiss-army knife of markup format conversion"
   homepage "http://pandoc.org"
-  url "https://hackage.haskell.org/package/pandoc-1.14.0.4/pandoc-1.14.0.4.tar.gz"
-  sha256 "01955bfb1f397ec22bbce10e2df7b4f2214b7289bf79bf51eb7ae0e3b427fadf"
+  url "https://hackage.haskell.org/package/pandoc-1.15.0.4/pandoc-1.15.0.4.tar.gz"
+  sha256 "5660c534f78f6baeb54a041108efe664452d6fc7da0b4181062a1ed559db07d7"
 
   head "https://github.com/jgm/pandoc.git"
 
   bottle do
-    revision 1
     sha256 "a7470327540c74e9c5e05a7b6b6240faa82e521147439f8bfb3fc100e12b3cfa" => :yosemite
     sha256 "7e803fffac74ac594caf39cdd194983083c2a7eaf3d86b358c9d41b59addcb1a" => :mavericks
     sha256 "09efd70cef2bfdfa2b43dd80abd51faa276ff600703cd16928f06f45fc2eb8d6" => :mountain_lion
@@ -27,8 +26,6 @@ class Pandoc < Formula
     cabal_sandbox do
       cabal_install "--only-dependencies"
       cabal_install "--prefix=#{prefix}"
-      man1.install "man/man1/pandoc.1"
-      man5.install "man/man5/pandoc_markdown.5"
     end
     cabal_clean_lib
   end
