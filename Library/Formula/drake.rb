5a4fe41d3078517fa6d28c8affe74a7fb12418df
maverick <maverick@factual.com>
2015-07-03 00:17:26 +0800

    create a new script which stripped out self downloading and self upgrade feature from drake script

--- Library/Formula/drake.rb
+++ Library/Formula/drake.rb
@@ -2,13 +2,24 @@ require 'formula'
 
 class Drake < Formula
   desc "Data workflow tool meant to be 'make for data'"
-  homepage 'https://github.com/Factual/drake'
-  url 'https://raw.githubusercontent.com/Factual/drake/v1.0.0/bin/drake'
-  version '1.0.0'
-  sha1 '9a70c19e0cd80ba3f9c5a5df2c752c6a194af913'
+  homepage "https://github.com/Factual/drake"
+  head "https://github.com/Factual/drake.git"
+  version "v1.0.0"
+  url "https://raw.githubusercontent.com/Factual/drake/master/bin/drake-pkg"
+  sha1 "3791f8e681620dae927bc70e22452b2703402724"
+
+  resource "jar" do
+    url "https://github.com/Factual/drake/releases/download/v1.0.0/drake.jar"
+    sha1 "0ee261ba884c6bf2572d99b0c2bbf943b4da8da0"
+  end
 
   def install
-    bin.install "drake"
+    jar = "drake-#{version}-standalone.jar"
+    inreplace "drake-pkg", /DRAKE_JAR/, lib/jar
+    bin.install "drake-pkg" => "drake"
+    resource("jar").stage do
+      lib.install "drake.jar" => jar
+    end
   end
 
   test do
