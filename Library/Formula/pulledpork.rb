45acc7586a6bfb4e53ad2ef1cf042437e81549d4
Drew Sh <drewshg@gmail.com>
2015-07-03 11:07:22 -0700

    pulledpork: add head

--- Library/Formula/pulledpork.rb
+++ Library/Formula/pulledpork.rb
@@ -5,6 +5,7 @@ class Pulledpork < Formula
   homepage 'http://code.google.com/p/pulledpork/'
   url 'https://pulledpork.googlecode.com/files/pulledpork-0.7.0.tar.gz'
   sha1 'fd7f2b195b473ba80826c4f06dd6ef2dd445814e'
+  head "http://pulledpork.googlecode.com/svn/trunk/"
 
   depends_on 'Switch' => :perl
   depends_on 'Crypt::SSLeay' => :perl
