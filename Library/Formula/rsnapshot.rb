4c99b4aa6ccf723e963e67a50569ff1742f22712
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-03 12:06:20 +0200

    rsnapshot 1.4.0
    Closes #41317.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/rsnapshot.rb
+++ Library/Formula/rsnapshot.rb
@@ -1,16 +1,8 @@
-require "formula"
-
 class Rsnapshot < Formula
   desc "File system snapshot utility (based on rsync)"
   homepage "http://rsnapshot.org"
-
-  stable do
-    url "http://rsnapshot.org/downloads/rsnapshot-1.3.1.tar.gz"
-    sha1 "a3aa3560dc389e1b00155a5869558522c4a29e05"
-
-    # Fix pod2man error; docs in HEAD do not have this problem
-    patch :DATA
-  end
+  url "http://rsnapshot.org/downloads/rsnapshot-1.4.0.tar.gz"
+  sha256 "222574fee2f59d0e3ef5da6e6dd0f445205fecfa7ca12ef821eb8a89cf4f2ca8"
 
   head "https://github.com/DrHyde/rsnapshot.git"
 
@@ -19,18 +11,3 @@ class Rsnapshot < Formula
     system "make", "install"
   end
 end
-
-__END__
-diff --git a/rsnapshot-program.pl b/rsnapshot-program.pl
-index dfd7ef6..72de4af 100755
---- a/rsnapshot-program.pl
-+++ b/rsnapshot-program.pl
-@@ -6666,6 +6666,8 @@ additional disk space will be taken up.
- 
- =back
- 
-+=back
-+
- Remember that tabs must separate all elements, and that
- there must be a trailing slash on the end of every directory.
- 
