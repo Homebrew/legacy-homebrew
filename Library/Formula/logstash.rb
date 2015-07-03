fd5e42e9ce6e58c6e3bd3329cf264e57204a8c52
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-03 03:07:40 +0200

    logstash 1.5.2
    Closes #41311.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/logstash.rb
+++ Library/Formula/logstash.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Logstash < Formula
   desc "Tool for managing events and logs"
-  homepage 'https://www.elastic.co/products/logstash'
-  url 'https://download.elasticsearch.org/logstash/logstash/logstash-1.5.0.tar.gz'
-  sha256 '8e073ee4c0dd346c62d538d3f28c95e536b9d3f269d0f993cff8662d799163d3'
+  homepage "https://www.elastic.co/products/logstash"
+  url "https://download.elasticsearch.org/logstash/logstash/logstash-1.5.2.tar.gz"
+  sha256 "1d1805d388392a69f5049b35c176186389a7f8bf7347c4528c255edc1f9b0d6a"
 
   def install
     inreplace %w{bin/logstash}, /^\. "\$\(cd `dirname \$0`\/\.\.; pwd\)\/bin\/logstash\.lib\.sh\"/, ". #{libexec}/bin/logstash.lib.sh"
