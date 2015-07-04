951eabc9629d8e1d3eff353923c55a227f328d27
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-04 10:27:35 +0100

    youtube-dl: update 2015.07.04 bottle.

--- Library/Formula/youtube-dl.rb
+++ Library/Formula/youtube-dl.rb
@@ -11,9 +11,9 @@ class YoutubeDl < Formula
 
   bottle do
     cellar :any
-    sha256 "bd25eb89c3d31f7123d6b89cb87004aa3836340bd16690aa06ab484468cede6a" => :yosemite
-    sha256 "c139850ba1632eb79d0f4cc575f00268c2a4753edab0b76c1b8c6321bc4c74d9" => :mavericks
-    sha256 "b7e91faae228468f7a91633b8bb2b9e1637389baf62f9afff4483c46e6ea19f7" => :mountain_lion
+    sha256 "968f2c59df9c0ceca3138c2c5bba1335d94d004eb6b79b81d68d1be8c5d426d8" => :yosemite
+    sha256 "74ad6435131b79c78c8cecb75aaf0003454ea716bd090820b9818cb6e40213c7" => :mavericks
+    sha256 "e99732006abacb51d3fcb19845b314aca002ef4d6d9ac22223c5c208880092bb" => :mountain_lion
   end
 
   head do
