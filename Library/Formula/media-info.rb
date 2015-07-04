a7daec1ea699740262b4b8668f8fce3538718cd3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-04 08:24:46 +0100

    media-info: update 0.7.75 bottle.

--- Library/Formula/media-info.rb
+++ Library/Formula/media-info.rb
@@ -9,9 +9,9 @@ class MediaInfo < Formula
 
   bottle do
     cellar :any
-    sha256 "a3fb613a28c28aab0a792bd3a13da9ad8715ef2d17421ebc9154f73ec82ae57d" => :yosemite
-    sha256 "7f4f0ef118462e60579754016fa3a64ee81bd8d8f81fd776ff7225fa0a62c43c" => :mavericks
-    sha256 "e69fce673bf6245f7eea82e8a47d39d08a708cdab9f1f1898e235c5e08c0b755" => :mountain_lion
+    sha256 "775f21604db1c9f4d4158d6310970799e040443e3aed15c2caa99f4f6072d618" => :yosemite
+    sha256 "7d812960fc07dc8b5e11438822336c24d5191eaa0d6661418e536ac2584c13f7" => :mavericks
+    sha256 "d50e07286760c0e75e49738e08d8563000b6b5a6d2475604309ea2093dada7af" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
