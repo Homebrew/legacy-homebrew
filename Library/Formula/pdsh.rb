cb043077492954bbdbb5fe76657157af46222272
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-04 09:41:17 +0100

    pdsh: add 2.31 bottle.

--- Library/Formula/pdsh.rb
+++ Library/Formula/pdsh.rb
@@ -7,6 +7,12 @@ class Pdsh < Formula
 
   head "https://code.google.com/p/pdsh.git"
 
+  bottle do
+    sha256 "a48664745214479e43afbcb75a35cb2c1056a8036b13e2ff5e757c603db03eb0" => :yosemite
+    sha256 "7a994ac4f24950407b46d3d97758d60d5b9ae94b23d8b308a3a9139a7db4efc1" => :mavericks
+    sha256 "a40070aef9be5f23d6a739bcff1f143cff3123ac50c7c8801874797f6011578f" => :mountain_lion
+  end
+
   option "without-dshgroups", "This option should be specified to load genders module first"
 
   depends_on "readline"
