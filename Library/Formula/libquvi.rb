29c05bcd593cf0c5c8d1331d5aa5ca246406f353
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-04 04:49:48 +0100

    libquvi: remove unnecessary lua pointers
    Closes #41344.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libquvi.rb
+++ Library/Formula/libquvi.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Libquvi < Formula
   desc "C library to parse flash media stream properties"
-  homepage 'http://quvi.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/quvi/0.4/libquvi/libquvi-0.4.1.tar.bz2'
-  sha1 'b7ac371185c35a1a9a2135ef4ee61c86c48f78f4'
+  homepage "http://quvi.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/quvi/0.4/libquvi/libquvi-0.4.1.tar.bz2"
+  sha256 "f5a2fb0571634483e8a957910f44e739f5a72eb9a1900bd10b453c49b8d5f49d"
   revision 1
 
   bottle do
@@ -14,27 +12,23 @@ class Libquvi < Formula
     sha1 "6a963aa4d1ce0eb5768a529f07227621b320bab0" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'lua'
+  depends_on "pkg-config" => :build
+  depends_on "lua"
 
-  resource 'scripts' do
-    url 'https://downloads.sourceforge.net/project/quvi/0.4/libquvi-scripts/libquvi-scripts-0.4.14.tar.xz'
-    sha1 'fe721c8d882c5c4a826f1339c79179c56bb0fe41'
+  resource "scripts" do
+    url "https://downloads.sourceforge.net/project/quvi/0.4/libquvi-scripts/libquvi-scripts-0.4.14.tar.xz"
+    sha256 "b8d17d53895685031cd271cf23e33b545ad38cad1c3bddcf7784571382674c65"
   end
 
   def install
-    scripts = prefix/'libquvi-scripts'
-    resource('scripts').stage do
+    scripts = prefix/"libquvi-scripts"
+    resource("scripts").stage do
       system "./configure", "--prefix=#{scripts}", "--with-nsfw"
-      system "make install"
+      system "make", "install"
     end
-    ENV.append_path 'PKG_CONFIG_PATH', "#{scripts}/lib/pkgconfig"
-
-    # Lua 5.2 does not have a proper lua.pc
-    ENV['liblua_CFLAGS'] = ' '
-    ENV['liblua_LIBS'] = '-llua'
+    ENV.append_path "PKG_CONFIG_PATH", "#{scripts}/lib/pkgconfig"
 
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
