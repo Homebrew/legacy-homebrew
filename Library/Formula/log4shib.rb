2f1594f7157c220d22eaab544e474a77f95f6d19
Alex Dunn <dunn.alex@gmail.com>
2015-07-04 16:30:14 -0700

    log4shib 1.0.9
    Closes #41362.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/log4shib.rb
+++ Library/Formula/log4shib.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Log4shib < Formula
   desc "Forked version of log4cpp for the Shibboleth project"
-  homepage 'https://wiki.shibboleth.net/confluence/display/OpenSAML/log4shib'
-  url 'http://shibboleth.net/downloads/log4shib/1.0.8/log4shib-1.0.8.tar.gz'
-  sha1 '407c70935917a59034acba4e63803d32465af641'
+  homepage "https://wiki.shibboleth.net/confluence/display/OpenSAML/log4shib"
+  url "http://shibboleth.net/downloads/log4shib/1.0.9/log4shib-1.0.9.tar.gz"
+  sha256 "b34cc90f50962cc245a238b472f72852732d32a9caf9a10e0244d0e70a311d6d"
 
   bottle do
     cellar :any
@@ -15,6 +13,11 @@ class Log4shib < Formula
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "-L#{HOMEBREW_PREFIX}/Cellar/log4shib/1.0.9/lib -llog4shib",
+                 shell_output("#{bin}/log4shib-config --libs").chomp
   end
 end
