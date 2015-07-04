09b01cd66e23b802e3d9d79d5a34a35f57ad2739
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-04 06:53:01 +0100

    owfs: add 3.1p0 bottle.

--- Library/Formula/owfs.rb
+++ Library/Formula/owfs.rb
@@ -5,6 +5,13 @@ class Owfs < Formula
   version "3.1p0"
   sha256 "62fca1b3e908cd4515c9eb499bf2b05020bbbea4a5b73611ddc6f205adec7a54"
 
+  bottle do
+    cellar :any
+    sha256 "97c0ad182ab7c5f000ed8734272be3a0aac38cf6df9cb7bb2e0f67825cf0a717" => :yosemite
+    sha256 "5682fed7463f0c20636a6fb961ff4edde845a497b55618eaff2d986613e09e20" => :mavericks
+    sha256 "f71d3106d44a4afd36a8d19057da893528e85c528a5727cc1ed278db0e44da2e" => :mountain_lion
+  end
+
   depends_on "libusb-compat"
 
   def install
