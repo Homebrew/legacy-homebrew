e3acf136ec27fc2caf1dd52f772239457453791b
Alex Dunn <dunn.alex@gmail.com>
2015-07-04 10:58:20 -0700

    xml-security-c 1.7.3
    Closes #41358.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/xml-security-c.rb
+++ Library/Formula/xml-security-c.rb
@@ -1,9 +1,8 @@
 class XmlSecurityC < Formula
   desc "Implementation of primary security standards for XML"
   homepage "https://santuario.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=/santuario/c-library/xml-security-c-1.7.2.tar.gz"
-  sha256 "d576b07bb843eaebfde3be01301db40504ea8e8e477c0ad5f739b07022445452"
-  revision 1
+  url "https://www.apache.org/dyn/closer.cgi?path=/santuario/c-library/xml-security-c-1.7.3.tar.gz"
+  sha256 "e5226e7319d44f6fd9147a13fb853f5c711b9e75bf60ec273a0ef8a190592583"
 
   bottle do
     cellar :any
