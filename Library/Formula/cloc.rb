3ae58ae0f826b979ff158468e1db5bfb6f10c2c6
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-05 21:13:50 +0200

    cloc 1.64
    Closes #41382.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/cloc.rb
+++ Library/Formula/cloc.rb
@@ -1,8 +1,8 @@
 class Cloc < Formula
   desc "Statistics utility to count lines of code"
   homepage "http://cloc.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/cloc/cloc/v1.62/cloc-1.62.pl"
-  sha1 "78f6123c967f9b142f77cba48decd11d56ab6c38"
+  url "https://downloads.sourceforge.net/project/cloc/cloc/v1.64/cloc-1.64.pl"
+  sha256 "79edea7ea1f442b1632001e23418193ae4571810e60de8bd25e491036d60eb3d"
 
   def install
     bin.install "cloc-#{version}.pl" => "cloc"
