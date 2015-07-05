386146b78a0ee7714769611d9a9ec6411225272d
Alex Dunn <dunn.alex@gmail.com>
2015-07-05 09:54:14 -0700

    libewf: fix homepage, add HEAD
    Closes #41377.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libewf.rb
+++ Library/Formula/libewf.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Libewf < Formula
   desc "Library for support of the Expert Witness Compression Format"
-  homepage "http://code.google.com/p/libewf/"
+  homepage "https://github.com/libyal/libewf"
   url "https://googledrive.com/host/0B3fBvzttpiiSMTdoaVExWWNsRjg/libewf-20140608.tar.gz"
-  sha1 "c17384a3d2c1d63bd5b1aaa2aead6ee3c82a2368"
+  sha256 "d14030ce6122727935fbd676d0876808da1e112721f3cb108564a4d9bf73da71"
   revision 1
 
   bottle do
@@ -14,11 +12,27 @@ class Libewf < Formula
     sha1 "b3d3877c1683b551badb8a21491faceb27958cf1" => :mountain_lion
   end
 
+  head do
+    url "https://github.com/libyal/libewf.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "pkg-config" => :build
   depends_on "openssl"
 
   def install
+    if build.head?
+      system "./synclibs.sh"
+      system "./autogen.sh"
+    end
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ewfinfo -V")
   end
 end
