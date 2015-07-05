ff45dc6c4b6573072583ecd7630cd9bc9a5e66e9
Daniel Anolik <dan@anolik.net>
2015-07-05 10:37:41 -0700

    siege 3.1.0
    Closes #41380.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/siege.rb
+++ Library/Formula/siege.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Siege < Formula
   desc "HTTP regression testing and benchmarking utility"
   homepage 'http://www.joedog.org/index/siege-home'
-  url 'http://download.joedog.org/siege/siege-3.0.9.tar.gz'
-  sha256 '82376eb466414ef4872a979a372972658df9813778ee8572341d4736ed30cb8f'
+  url 'http://download.joedog.org/siege/siege-3.1.0.tar.gz'
+  sha256 'f6a104cb2a3ac6c0efb2699649e8c4f8da2b548147bbbb7af2483089e4940e7f'
 
   depends_on 'openssl'
 
