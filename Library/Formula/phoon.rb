99823bf2edbd667535efd669193f92f1bcb85253
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-05 21:22:07 +0200

    phoon 04A
    phoon: 04A (14Aug2014)
    
    Closes #41383.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/phoon.rb
+++ Library/Formula/phoon.rb
@@ -1,15 +1,13 @@
-require 'formula'
-
 class Phoon < Formula
   desc "Displays current or specified phase of the moon via ASCII art"
-  homepage 'http://www.acme.com/software/phoon/'
-  url 'http://www.acme.com/software/phoon/phoon_29jun2005.tar.gz'
-  version '03A'
-  sha1 'd646af184b83e1a4104fe82588daadae2938e08c'
+  homepage "http://www.acme.com/software/phoon/"
+  url "http://www.acme.com/software/phoon/phoon_14Aug2014.tar.gz"
+  version "04A"
+  sha256 "bad9b5e37ccaf76a10391cc1fa4aff9654e54814be652b443853706db18ad7c1"
 
   def install
     system "make"
-    bin.install 'phoon'
-    man1.install 'phoon.1'
+    bin.install "phoon"
+    man1.install "phoon.1"
   end
 end
