79b5fcf37e7d5057a7b83614e8bf04333a5d0475
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-06 11:23:35 +0100

    drake: add 1.0.0 bottle.

--- Library/Formula/drake.rb
+++ Library/Formula/drake.rb
@@ -6,6 +6,13 @@ class Drake < Formula
   sha256 "6b75504f85f01a7d213c60f87e7da0d94a4961bbc7e9b7e90f79efcc38ae5165"
   head "https://github.com/Factual/drake.git"
 
+  bottle do
+    cellar :any
+    sha256 "0e1d17ad42bb1bd51792aa625664f1d1fb12748ac2f1783a53da4c6e6ba74124" => :yosemite
+    sha256 "7f1113ff253e4dccb34f54ff710da37738d39ce60fe014a97103af38a848fdb8" => :mavericks
+    sha256 "9b83197b887b6ce5f7912afbebcad3dff4c550b4057b45673d826982f94e481f" => :mountain_lion
+  end
+
   resource "jar" do
     url "https://github.com/Factual/drake/releases/download/1.0.0/drake.jar"
     sha256 "f601f059dd23f87ccb1fa9ce1c39067e8bbeed36f08820769c6132c311e99aa8"
