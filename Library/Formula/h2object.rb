789820e5716160ae77fa89a2cc7ab24f87510400
刘建平 <liujianping@h2object.io>
2015-07-06 23:59:18 +0800

    fix homebrew audit fails

--- Library/Formula/h2object.rb
+++ Library/Formula/h2object.rb
@@ -1,8 +1,8 @@
 class H2object < Formula
+  desc "another fast & flexible static website generator & deployment tool"
   homepage "https://github.com/h2object/h2object"
   url "http://dl.h2object.io/h2object/macosx/1.0.0.tar.gz"
-  sha256 "2b9df7547876dcf24c4b0b39568b2da0198b6102960d41e522994a0e4ab060c6"        
-  desc "the most fasting http server with themes to build web application"
+  sha256 "2b9df7547876dcf24c4b0b39568b2da0198b6102960d41e522994a0e4ab060c6"
 
   def install
     bin.install "h2object"
