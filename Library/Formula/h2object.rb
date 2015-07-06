162593f84b4c6a134eb216f04285728dbbe49ccb
刘建平 <liujianping@h2object.io>
2015-07-06 23:35:38 +0800

    modify h2object homepage to the https://github.com/h2object/h2object

--- Library/Formula/h2object.rb
+++ Library/Formula/h2object.rb
@@ -1,5 +1,5 @@
 class H2object < Formula
-  homepage "http://h2object.io"
+  homepage "https://github.com/h2object/h2object"
   url "http://dl.h2object.io/h2object/macosx/1.0.0.tar.gz"
   sha256 "2b9df7547876dcf24c4b0b39568b2da0198b6102960d41e522994a0e4ab060c6"        
   desc "the most fasting http server with themes to build web application"
