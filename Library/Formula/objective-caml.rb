65e2af27fcc4055ac5dcfd804fe69570b1eb7cf0
Alex Dunn <dunn.alex@gmail.com>
2015-07-06 15:13:05 -0700

    objective-caml 4.02.2
    Closes #41432.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/objective-caml.rb
+++ Library/Formula/objective-caml.rb
@@ -1,21 +1,19 @@
 class ObjectiveCaml < Formula
   desc "General purpose programming language in the ML family"
   homepage "https://ocaml.org/"
-  url "http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.1.tar.gz"
-  sha256 "3cbc7af5a3886c8c5af8dab5568d6256a191d89ecbd4aea18eaf5b47034c6138"
-  revision 3
-
+  url "http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.2.tar.bz2"
+  sha256 "b18265582b1c2fd5c1e67da3f744bf1ff474d194bb277c3a9ceb5eb16a1ea703"
   head "http://caml.inria.fr/svn/ocaml/trunk", :using => :svn
-
-  option "with-x11", "Install with the Graphics module"
-
-  depends_on :x11 => :optional
 
   bottle do
     sha256 "b9d8aca6567340adbed781ed3e5a7137e21999bd828ff07c766402b4f6070400" => :yosemite
     sha256 "d2e27ecccfb743c52395b24b0b1792e023214a3c7d66031953f6fd4a8a0bbd7e" => :mavericks
     sha256 "620b3d86106535c471cc81f6e2993a72e9c856f98fab012169d2d5428bb878e9" => :mountain_lion
   end
+
+  option "with-x11", "Install with the Graphics module"
+
+  depends_on :x11 => :optional
 
   def install
     ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
