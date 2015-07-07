38ea363fc5000590cee8d805e81b9352bffca5cf
Franklin Chen <franklinchen@franklinchen.com>
2015-07-06 20:48:01 -0400

    logtalk 3.00.4
    logtalk: update to 3.00.4; remove devel because 3.00 is stable now.
    
    Closes #41434.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/logtalk.rb
+++ Library/Formula/logtalk.rb
@@ -3,17 +3,9 @@ require "formula"
 class Logtalk < Formula
   desc "Object-oriented logic programming language"
   homepage "http://logtalk.org"
-  url "http://logtalk.org/files/lgt2441.tar.bz2"
-  sha256 "ca49d8e26a08dcf2a39358efa827789ecc5c47791035e5bb5830a6aec59b94e9"
-  version "2.44.1"
-
-  devel do
-    url "http://logtalk.org/files/logtalk-3.00.0-rc7.tar.bz2"
-    sha256 "50664e28eaa75eb0b293345919a9178bc9b2e2135a43f066265cfa99cb2c042a"
-    version "3.00.0-rc7"
-  end
-
-  head "https://github.com/LogtalkDotOrg/logtalk3.git"
+  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3004stable.tar.gz"
+  sha256 "2f1275d43ec5c4c65161b4673ed214311272a8af131a748c37e2ffec33532dfc"
+  version "3.00.4"
 
   option "swi-prolog", "Build using SWI Prolog as backend"
   option "gnu-prolog", "Build using GNU Prolog as backend (Default)"
@@ -25,10 +17,6 @@ class Logtalk < Formula
   end
 
   def install
-    if build.stable?
-      cd("scripts") { system "./install.sh", prefix }
-    else
-      cd("scripts") { system "./install.sh", "-p", prefix }
-    end
+    cd("scripts") { system "./install.sh", "-p", prefix }
   end
 end
