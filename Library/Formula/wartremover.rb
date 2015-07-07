697c8cb2144087d03a3558a8c3ebd30b358decac
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-07 07:50:14 +0100

    wartremover: add 0.13 bottle.

--- Library/Formula/wartremover.rb
+++ Library/Formula/wartremover.rb
@@ -6,6 +6,13 @@ class Wartremover < Formula
 
   head "https://github.com/puffnfresh/wartremover.git"
 
+  bottle do
+    cellar :any
+    sha256 "7cf8e78114d8212a56e71cf62b415151e0458141bef644a4a5f8e75417cc7f8b" => :yosemite
+    sha256 "a5c0eb64c5d8165c918772f3462eada415ad90545f77eb6d8aa8d636ba73ed32" => :mavericks
+    sha256 "4727d2f98bbe8c3b4e60a99cb97af633a1b33a8a1e42b8caade98ebd1d7131d0" => :mountain_lion
+  end
+
   depends_on "sbt" => :build
 
   def install
