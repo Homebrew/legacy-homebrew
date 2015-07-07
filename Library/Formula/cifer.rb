e30b3e3bda5edddb26405190ab40458e84149824
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-07 06:55:57 +0100

    cifer: add 1.2.0 bottle.

--- Library/Formula/cifer.rb
+++ Library/Formula/cifer.rb
@@ -4,6 +4,13 @@ class Cifer < Formula
   url "https://cifer.googlecode.com/files/cifer-1.2.0.tar.gz"
   sha256 "436816c1f9112b8b80cf974596095648d60ffd47eca8eb91fdeb19d3538ea793"
 
+  bottle do
+    cellar :any
+    sha256 "bde7d97d9ef2a07c481ff8c5ec717fb2ec455fdef864db2a1a7b3056aa1934d2" => :yosemite
+    sha256 "f4e2f4024a8daf1e2a1fc071113947561c803503fa242b7df7970e8979cf10be" => :mavericks
+    sha256 "8e7b415f5012c001c47484a29e55c608b51e82f5fc5be41787b061765e405d64" => :mountain_lion
+  end
+
   def install
     system "make", "prefix=#{prefix}",
                    "CC=#{ENV.cc}",
