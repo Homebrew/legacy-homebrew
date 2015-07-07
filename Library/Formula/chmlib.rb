630560ce624f6d8e4e5ad2b36ed6137d1dceda12
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-07 05:35:40 +0100

    chmlib: add debian mirror

--- Library/Formula/chmlib.rb
+++ Library/Formula/chmlib.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class Chmlib < Formula
   desc "Library for dealing with Microsoft ITSS/CHM files"
-  homepage 'http://www.jedrea.com/chmlib'
-  url 'http://www.jedrea.com/chmlib/chmlib-0.40.tar.gz'
-  sha1 '8d9e4b9b79a23974aa06fb792ae652560bac5c4e'
+  homepage "http://www.jedrea.com/chmlib"
+  url "http://www.jedrea.com/chmlib/chmlib-0.40.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/c/chmlib/chmlib_0.40.orig.tar.gz"
+  sha256 "512148ed1ca86dea051ebcf62e6debbb00edfdd9720cde28f6ed98071d3a9617"
 
   bottle do
     cellar :any
@@ -16,6 +15,6 @@ class Chmlib < Formula
 
   def install
     system "./configure", "--disable-io64", "--enable-examples", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 end
