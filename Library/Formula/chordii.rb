5b7a1ed867d4a8c31e9ba778f09183c417fd0079
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-07 06:55:57 +0100

    chordii: add 4.5.1 bottle.

--- Library/Formula/chordii.rb
+++ Library/Formula/chordii.rb
@@ -4,6 +4,13 @@ class Chordii < Formula
   url "https://downloads.sourceforge.net/project/chordii/chordii/4.5/chordii-4.5.1.tar.gz"
   sha256 "16a3fe92a82ca734cb6d08a573e513510acd41d4020a6306ac3769f6af5aa08d"
 
+  bottle do
+    cellar :any
+    sha256 "5592e19ddb7affade8a918992648c87bb92a83e201e28f8afdae87e3e3ba4c2b" => :yosemite
+    sha256 "f828c0158bfa52c9e136c0332ea595e788af14073082847d51bc4c96e6c909ac" => :mavericks
+    sha256 "532cb785f263790e7a314df2a276c2ee73de50fe630df8514586d888c0bb6281" => :mountain_lion
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
