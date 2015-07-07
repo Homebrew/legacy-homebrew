7d090cbab44e759b66311f5aafed6e38c2509455
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-07 01:59:00 +0100

    logtalk: add 3.00.4 bottle.

--- Library/Formula/logtalk.rb
+++ Library/Formula/logtalk.rb
@@ -7,6 +7,13 @@ class Logtalk < Formula
   sha256 "2f1275d43ec5c4c65161b4673ed214311272a8af131a748c37e2ffec33532dfc"
   version "3.00.4"
 
+  bottle do
+    cellar :any
+    sha256 "ecba33b85ad7147dc41883f467b563814e72fc1e70b062e3805c58ec4712f3a3" => :yosemite
+    sha256 "3e78e9174d6dac8e281b922a02264127165cfd0bb90c8b1e092a2c1d5eb68670" => :mavericks
+    sha256 "197132d44948afbc3c0ae1c811ff8c2b81a93934f099be8a4271eb49d82ceabf" => :mountain_lion
+  end
+
   option "swi-prolog", "Build using SWI Prolog as backend"
   option "gnu-prolog", "Build using GNU Prolog as backend (Default)"
 
