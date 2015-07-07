c4162af528ab33cd2d9f1451c77e789ed9fe5448
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-07 05:04:47 +0100

    keychain 2.8.1
    Closes #41438.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/keychain.rb
+++ Library/Formula/keychain.rb
@@ -1,18 +1,9 @@
-require 'formula'
-
 class Keychain < Formula
   desc "User-friendly front-end to ssh-agent(1)"
-  homepage 'http://www.funtoo.org/Keychain'
-  url 'http://build.funtoo.org/distfiles/keychain/keychain-2.8.0.tar.bz2'
-  mirror 'http://ftp.osuosl.org/pub/funtoo/distfiles/keychain/keychain-2.8.0.tar.bz2'
-  sha256 '411bfe6a3ac9daca1c35f9a56828f03cc8474e8a668e122773397deb8f7a0799'
-
-  bottle do
-    cellar :any
-    sha256 "adc5b68fb6f09789eb8680ddafff69cc1639de555162f122977e7d3fde2ed7d7" => :yosemite
-    sha256 "fdca087e025f08bfc9241b0a83b079f0f3c6958fcde82adaba05a14a3f96ca4e" => :mavericks
-    sha256 "dc9114d1a46f7a7e7826d2575abdaac0fcf88075e9fb0e2cf65e7076511512ef" => :mountain_lion
-  end
+  homepage "http://www.funtoo.org/Keychain"
+  url "http://build.funtoo.org/distfiles/keychain/keychain-2.8.1.tar.bz2"
+  mirror "https://distfiles.macports.org/keychain/keychain-2.8.1.tar.bz2"
+  sha256 "1568c0946db3638fe081d5a7ba3df022b533dbeb8aa67cd07dc8276e87598809"
 
   def install
     bin.install "keychain"
