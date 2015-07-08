23c903017215fbf6a7fc9b339883b9f9eda69935
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-09 00:07:10 +0100

    liquibase: fix style nits

--- Library/Formula/liquibase.rb
+++ Library/Formula/liquibase.rb
@@ -2,20 +2,19 @@ class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "http://liquibase.org"
   url "https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.4.0/liquibase-3.4.0-bin.tar.gz"
-  sha1 "f2747762de5dce1876bbd4f6b9c9e00e97de7f97"
+  sha256 "c3c6869b3f36433f3a1f9c49890047100594b57ed54183f686e7421a43567f03"
 
   def install
     rm_f Dir["*.bat"]
     chmod 0755, "liquibase"
     prefix.install_metafiles
     libexec.install Dir["*"]
-    bin.install_symlink libexec+"liquibase"
+    bin.install_symlink libexec/"liquibase"
   end
 
-  def caveats
-    <<-EOS.undent
-      You should set the environment variable LIQUIBASE_HOME to
-        #{libexec}
+  def caveats; <<-EOS.undent
+    You should set the environment variable LIQUIBASE_HOME to
+      #{libexec}
     EOS
   end
 
