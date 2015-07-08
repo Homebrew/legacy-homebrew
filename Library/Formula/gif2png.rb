8bdff109b0a7aeb5e8ad6dc33c55db28c15a1d97
Keith Shaw <kshaw@qualys.com>
2015-07-08 11:54:24 -0700

    Added description

--- Library/Formula/gif2png.rb
+++ Library/Formula/gif2png.rb
@@ -1,4 +1,5 @@
 class Gif2png < Formula
+  desc "Convert GIFs to PNGs."
   homepage "http://www.catb.org/~esr/gif2png/"
   url "http://www.catb.org/~esr/gif2png/gif2png-2.5.10.tar.gz"
   sha256 "3a593156f335c4ea6be68e37e09994461193f31872362de4b27ef6301492d5fd"
