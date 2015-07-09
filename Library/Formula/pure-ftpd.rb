9eb5ddaade412af74ffbcc4dc747f016e94ef4eb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-09 05:37:07 +0100

    pure-ftpd: add 1.0.41 bottle.

--- Library/Formula/pure-ftpd.rb
+++ Library/Formula/pure-ftpd.rb
@@ -5,6 +5,13 @@ class PureFtpd < Formula
   mirror "ftp://ftp.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.41.tar.gz"
   sha256 "a877c689ae1b982c968a767631740a84f164ac2ae6312a4a2f9f93ba79a348e8"
 
+  bottle do
+    cellar :any
+    sha256 "197b26f65f7c63ef7844f6d40dab6076b132cb6844d65a1c791546be42dd2a26" => :yosemite
+    sha256 "d6e8c0cb8772b447202aa9774d3fc1c4c8857c2ff81875e40c0b935940a8e318" => :mavericks
+    sha256 "c820d10085fbb7ad69a09f928da5d5994bbdee881520f07c5db3c5d1bf47b9dd" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
