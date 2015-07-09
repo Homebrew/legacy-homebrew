18171aef048e8d0ca576e895d7d8f9815cd52a83
Alex Dunn <dunn.alex@gmail.com>
2015-07-08 21:15:15 -0700

    pure-ftpd 1.0.41
    Closes #41502.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/pure-ftpd.rb
+++ Library/Formula/pure-ftpd.rb
@@ -1,9 +1,9 @@
 class PureFtpd < Formula
   desc "Secure and efficient FTP server"
   homepage "http://www.pureftpd.org/"
-  url "http://download.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.37.tar.gz"
-  mirror "ftp://ftp.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.37.tar.gz"
-  sha256 "a9c10b0b8b5772fbf2212bc46fece86f9d4bcc07e58dfd83b58e42a1b2acb76c"
+  url "http://download.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.41.tar.gz"
+  mirror "ftp://ftp.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.41.tar.gz"
+  sha256 "a877c689ae1b982c968a767631740a84f164ac2ae6312a4a2f9f93ba79a348e8"
 
   depends_on "openssl"
 
