5f00a4d07acd42fe5f1566a861d0d4ef20e0ec51
Dennis Oelkers <dennis@graylog.com>
2015-07-09 14:18:30 +0200

    Replacing resource with inline shell wrapper.

--- Library/Formula/sjk.rb
+++ Library/Formula/sjk.rb
@@ -6,14 +6,16 @@ class Sjk < Formula
 
   depends_on :java
 
-  resource "shell_wrapper" do
-    url "https://gist.githubusercontent.com/dennisoelkers/2bda4937ab823ea54d44/raw/87af0aa7510afea4022a0296f2c93cc1c0a2bee0/sjk.sh"
-    sha256 "42ecaa01e781553c36526485efc9702c9c2138c948f328513e22e06a74962007"
-  end
-
   def install
     lib.install "sjk-plus-0.3.6.jar"
-    resource("shell_wrapper").stage { bin.install "sjk.sh" => "sjk" }
+    (bin/"sjk").write <<-EOS.undent
+      #!/bin/sh
+
+      PREFIX=`dirname $0`/../lib
+      JARFILE=$PREFIX/sjk-plus-0.3.6.jar
+
+      java -jar $JARFILE $@
+    EOS
   end
 
   test do
