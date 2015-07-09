75c3158bca9f9b1ed116f2fd778b1fe26b7f3f11
Adrian Colley <aecolley@gmail.com>
2015-07-09 01:59:01 +0100

    szl 1.0.1 (new maintainer)
    I fixed many bugs in szl, but Google has abandoned it and no longer
    responds to bug reports or messages on szl-users. So I took it over.
    
    I sent an earlier version of this formula to Adam Vandenberg, whom I found
    in the git commit history. But he said "Please send this to the Homebrew
    mailing list, not me directly; or open a ticket with this information. I
    don't maintain this formula." I'll maintain it, then.
    
    This formula is cleaner (no patches), more stable (the code is more
    portable) and more useful (bugs are fixed) than the previous one.

--- Library/Formula/szl.rb
+++ Library/Formula/szl.rb
@@ -1,79 +1,49 @@
 class Szl < Formula
-  desc "Compiler and runtime for the Sawzall language"
-  homepage "https://code.google.com/p/szl/"
-  url "https://szl.googlecode.com/files/szl-1.0.tar.gz"
-  sha1 "e4c6d4aec1afc025257d41dd77b8f5c25ea120d4"
-  revision 5
+  desc "Google's Sawzall language for statistical aggregation of log data."
+  homepage "https://bitbucket.org/aecolley/szl"
+  url "https://bitbucket.org/aecolley/szl/downloads/szl-1.0.1.tar.gz"
+  sha1 "9a5f0f1914781a7ca8f80896124c1a39bae329f2"
+  sha256 "791ff01fbf72fb3ad0c4e49a8de40dc3d7822fe549ee94d11ea7226557c0e013"
 
-  bottle do
-    cellar :any
-    revision 1
-    sha256 "92a10b08dae8394ac7ac5db90d351fad99bc74ff94f966bb888d3797e78c2891" => :yosemite
-    sha256 "235f69f9ca8061937fe3f72c6f2467027fe2083837cebbeb2d98e33a25c09b83" => :mavericks
+  head do
+    url "https://bitbucket.org/aecolley/szl", :using => :hg
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
   end
 
-  depends_on :macos => :mavericks
-
-  depends_on "binutils" # For objdump
   depends_on "icu4c"
   depends_on "protobuf"
+  depends_on "protobuf-c"
   depends_on "pcre"
-  depends_on "openssl"
-
-  # 10.9 and clang fixes
-  # Include reported upstream in:
-  # https://code.google.com/p/szl/issues/detail?id=28
-  # Clang issue reported upstream in:
-  # https://code.google.com/p/szl/issues/detail?id=34
-  patch :DATA
+  # We don't need gobjdump for core functionality, but only for
+  # "szl --print_code --native". We can use it if it's installed later.
+  depends_on "binutils" => :optional
 
   def install
-    ENV["OBJDUMP"] = "#{HOMEBREW_PREFIX}/bin/gobjdump"
+    if build.head?
+      system "./autogen.sh", "-f"
+    end
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+                          "--disable-debug",
+                          "--disable-rpath",
+                          "--prefix=#{prefix}",
+                          "OBJDUMP=#{Formula["binutils"].opt_bin}/gobjdump"
     system "make", "install"
   end
 
   test do
-    system bin/"szl", "-V"
+    require "open3"
+    args = [
+      '--e=
+        t: table sum of int;
+        if (match("<[Pp]>", string(input)))
+          emit t <- 1;',
+      "--table_output=t",
+      "#{doc}/sawzall-spec.html"
+    ]
+    output, status = Open3.capture2("#{bin}/szl", *args)
+    assert_equal 0, status
+    assert_equal "t[] = 30\n", output
   end
 end
-
-__END__
-diff --git a/src/utilities/random_base.cc b/src/utilities/random_base.cc
-index 1d64521..e488321 100644
---- a/src/utilities/random_base.cc
-+++ b/src/utilities/random_base.cc
-@@ -18,6 +18,7 @@
- #include <string>
- #include <memory.h>
- #include <assert.h>
-+#include <unistd.h>
-
- #include "public/porting.h"
- #include "public/logging.h"
-diff --git a/src/engine/code.cc b/src/engine/code.cc
-index d149f9a..75ab84b 100644
---- a/src/engine/code.cc
-+++ b/src/engine/code.cc
-@@ -18,6 +18,7 @@
- #include <string>
- #include <errno.h>
- #include <sys/mman.h>
-+#include <unistd.h>
-
- #include "engine/globals.h"
- #include "public/logging.h"
-diff --git a/src/engine/symboltable.cc b/src/engine/symboltable.cc
-index 6d84592..71965f3 100644
---- a/src/engine/symboltable.cc
-+++ b/src/engine/symboltable.cc
-@@ -44,7 +44,7 @@ namespace sawzall {
- // ------------------------------------------------------------------------------
- // Implementation of SymbolTable
-
--Proc::Proc* SymbolTable::init_proc_ = NULL;
-+Proc* SymbolTable::init_proc_ = NULL;
-
- List<TableType*>* SymbolTable::table_types_ = NULL;
- TableType* SymbolTable::collection_type_ = NULL;
