0db9fcdd5fdc0c08f8100a4523a473f622268544
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-09 19:36:06 +0100

    shmcat: use sha256

--- Library/Formula/shmcat.rb
+++ Library/Formula/shmcat.rb
@@ -1,30 +1,35 @@
-require "formula"
-
 class Shmcat < Formula
   desc "Tool that dumps shared memory segments (System V and POSIX)"
   homepage "http://shmcat.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/shmcat/shmcat-1.7.tar.bz2"
-  sha1 "30f6bc931859e1544e783edc84f137e9c4a0b5f6"
+  sha256 "dfe113592425373ea3d67cad5e9e44cbc27e45c75af3b308240aee9530d169cc"
 
   option "with-ftok", "Build the ftok utility"
-  option "with-nls", "Use Native Language Support"
+  option "with-gettext", "Build with Native Language Support"
 
-  depends_on "gettext" if build.with? "nls"
+  deprecated_option "with-nls" => "with-gettext"
+
+  depends_on "gettext" => :optional
 
   def install
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
+
     args << "--disable-ftok" if build.without? "ftok"
-    if build.with? "nls"
-      gettext = Formula["gettext"]
-      args << "--with-libintl-prefix=#{gettext.include}"
+
+    if build.with? "gettext"
+      args << "--with-libintl-prefix=#{Formula["gettext"].opt_include}"
     else
       args << "--disable-nls"
     end
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    assert_match /#{version}/, shell_output("#{bin}/shmcat --version")
   end
 end
