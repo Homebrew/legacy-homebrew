ed3cd63003a0ce9ddef769ab4226c3e56a478fda
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-09 20:13:23 +0100

    exif: make gettext optional
    Closes #41541.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/exif.rb
+++ Library/Formula/exif.rb
@@ -1,18 +1,26 @@
-require 'formula'
-
 class Exif < Formula
   desc "Read, write, modify, and display EXIF data on the command-line"
-  homepage 'http://libexif.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/libexif/exif/0.6.21/exif-0.6.21.tar.gz'
-  sha1 'd23139d26226b70c66d035bbc64482792c9f1101'
+  homepage "http://libexif.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/libexif/exif/0.6.21/exif-0.6.21.tar.gz"
+  sha256 "1e2e40e5d919edfb23717308eb5aeb5a11337741e6455c049852128a42288e6d"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'popt'
-  depends_on 'libexif'
-  depends_on 'gettext'
+  option "with-gettext", "Build with Native Language Support"
+
+  depends_on "pkg-config" => :build
+  depends_on "popt"
+  depends_on "libexif"
+  depends_on "gettext" => :optional
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-    system "make install"
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    args << "--disable-nls" if build.without? "gettext"
+
+    system "./configure", *args
+    system "make", "install"
   end
 end
