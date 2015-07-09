22619cb1fe196c9c87a602bd50d851fd1f4f1795
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-09 21:18:18 +0100

    rubber: add 20150624-1.2 bottle.

--- Library/Formula/rubber.rb
+++ Library/Formula/rubber.rb
@@ -5,6 +5,12 @@ class Rubber < Formula
   sha256 "776d859fb75f952cac37bd1b60833840e3a6cdd8d6d01f214b379d3c76618e95"
   version "20150624-1.2"
 
+  bottle do
+    sha256 "928de907e2ff3961c72ff71380fd7db0fc072ae526dc77068718b618ce80f5e6" => :yosemite
+    sha256 "33e93b904403bccb9a139ac963cbec128d3a23422e3bb69bd31bbe03191a464a" => :mavericks
+    sha256 "e7480ca80262718ac62b26c3573c1b7e3091a2ed1766d7f33967f2620911fbc4" => :mountain_lion
+  end
+
   head "lp:rubber", :using => :bzr
 
   def install
