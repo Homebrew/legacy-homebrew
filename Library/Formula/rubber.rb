dc5bf08bd9960e8abff099767c6c8c5fbd723912
Alex Dunn <dunn.alex@gmail.com>
2015-07-09 10:23:50 -0700

    rubber 20150624-1.2
    Closes #41535.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/rubber.rb
+++ Library/Formula/rubber.rb
@@ -1,20 +1,25 @@
-require 'formula'
-
 class Rubber < Formula
   desc "Automated building of LaTeX documents"
-  homepage 'https://launchpad.net/rubber/'
-  url 'http://launchpad.net/rubber/trunk/1.1/+download/rubber-20100306.tar.gz'
-  sha1 'cd382a19cc9fc65d114456ec9d6b042dc0e65b53'
+  homepage "https://launchpad.net/rubber/"
+  url "https://launchpad.net/rubber/trunk/1.2/+download/rubber-1.2.tar.gz"
+  sha256 "776d859fb75f952cac37bd1b60833840e3a6cdd8d6d01f214b379d3c76618e95"
+  version "20150624-1.2"
+
+  head "lp:rubber", :using => :bzr
 
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
-                          "--infodir=#{info}",
-                          "--datadir=#{share}"
+                          "--infodir=#{info}"
+    # `install` requires but does not make the docs
     system "make"
-    system "make install"
+    system "make", "install"
 
     # Don't need to peg to a specific Python version
-    inreplace Dir["#{bin}/*"], /^#!.*\/python.*$/, "#!/usr/bin/env python"
+    inreplace Dir["#{bin}/*"], %r{^#!.*\/python.*$}, "#!/usr/bin/env python"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/rubber --version")
   end
 end
