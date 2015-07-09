d172a498ad99310845ae86f4d9cc2fc2cff755b4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-10 00:34:36 +0100

    norm: update 1.5r6 bottle.

--- Library/Formula/norm.rb
+++ Library/Formula/norm.rb
@@ -7,9 +7,9 @@ class Norm < Formula
 
   bottle do
     cellar :any
-    sha256 "ae2a33d4034fa0f3d4845c7269ab0f178725185be028198a4cb541865a9e595d" => :yosemite
-    sha256 "52cf6e92bffd6f67e050172c7956f071c5e5d457db87dca957c30dd23bbf9704" => :mavericks
-    sha256 "1dc0340c8f0fe4fb6360cf4763080987e62180cc454e09c1a6da587f950c4b7f" => :mountain_lion
+    sha256 "2c165178bfce5879bb6e031b4d54f741cad2868d67b03783f89a13d15503f28d" => :yosemite
+    sha256 "b5f802ff09e68b712f472f45aea9b634f6c45868bccaf708d565ff98a95b145e" => :mavericks
+    sha256 "e04bed5670762178e4415296b95829b9e3c75da265f9375c6c6efac310b7b3d4" => :mountain_lion
   end
 
   def install
