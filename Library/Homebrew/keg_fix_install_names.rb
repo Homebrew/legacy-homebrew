902cc35a3ba5311380c6f684c4d3e5b828435219
Xu Cheng <xucheng@me.com>
2015-07-10 19:51:43 +0800

    bottle: improve relocation
    * skip document for relocatable check
    * relocate INSTALL_RECEIPT.json file
    
    Fixes #41545.
    
    Closes #41568.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Homebrew/keg_fix_install_names.rb
+++ Library/Homebrew/keg_fix_install_names.rb
@@ -39,6 +39,7 @@ class Keg
     end
 
     files = pkgconfig_files | libtool_files | script_files | plist_files
+    files << tab_file if tab_file.file?
 
     files.group_by { |f| f.stat.ino }.each_value do |first, *rest|
       s = first.open("rb", &:read)
@@ -206,5 +207,9 @@ class Keg
       plist_files << pn
     end
     plist_files
+  end
+
+  def tab_file
+    join(Tab::FILENAME)
   end
 end
