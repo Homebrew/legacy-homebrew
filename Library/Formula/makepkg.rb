f94ea38634d4be97c319248b122dbdcf5f7b8526
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-10 03:26:28 +0100

    makepkg: update 4.2.1 bottle.

--- Library/Formula/makepkg.rb
+++ Library/Formula/makepkg.rb
@@ -8,10 +8,9 @@ class Makepkg < Formula
   head "https://projects.archlinux.org/git/pacman.git"
 
   bottle do
-    revision 1
-    sha1 "302edf1a558cd607e1747a7b25e5011678cde67e" => :mavericks
-    sha1 "1044a51bec7287423cad12ad2816c0534a7788f0" => :mountain_lion
-    sha1 "4908957c636158ed40fbdde85f6630df36469699" => :lion
+    sha256 "5f7bfd1819f0d614801b498f36220f05c3830237a5bbcb6043ae3beab90ebd86" => :yosemite
+    sha256 "a61a79f44b9738b3e10a55edf1b72d18ef6f3d082ab83d080bf162d654b3f754" => :mavericks
+    sha256 "a9efd4152ec9e10dcce8573cd6875d767dcdf28ac834881daf1d7a730c951299" => :mountain_lion
   end
 
   depends_on "automake" => :build
