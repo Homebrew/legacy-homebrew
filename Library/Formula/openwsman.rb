c15a505fb6bcc0bc24725f18d7ced60947e6173f
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-10 02:54:50 +0100

    openwsman 2.6.0

--- Library/Formula/openwsman.rb
+++ Library/Formula/openwsman.rb
@@ -1,8 +1,8 @@
 class Openwsman < Formula
   desc "Implementation of WS-Management, enabling in-band resource management"
   homepage "https://openwsman.github.io"
-  url "https://github.com/Openwsman/openwsman/archive/v2.5.0.tar.gz"
-  sha256 "538ad011c3be59ed9843f8499ce749b98cc245208f7b3ca9ca9d53611fee7fe5"
+  url "https://github.com/Openwsman/openwsman/archive/v2.6.0.tar.gz"
+  sha256 "449b4972dc7700918f8f674047f0a1a9c19eeee3f4ce82f47890dddfa56e823f"
 
   bottle do
     sha256 "8cd55e679f4ffca631ae1a7d86b4e9c70ee4c85d82dc5b4c7429030d0ded85ea" => :yosemite
@@ -10,19 +10,20 @@ class Openwsman < Formula
     sha256 "aeadefecb859c12ad6d95ee42724be21a81e5ce28611b23e3dfa96efb2bfead9" => :mountain_lion
   end
 
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   depends_on "libxml2"
   depends_on "sblim-sfcc"
-  depends_on "automake"   => :build
-  depends_on "autoconf"   => :build
-  depends_on "libtool"    => :build
-  depends_on "pkg-config" => :build
   depends_on "openssl"
 
   def install
     system "./autoconfiscate.sh"
     system "./configure", "--disable-more-warnings",
                           "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
     system "make", "install"
   end
 
