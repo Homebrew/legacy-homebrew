d5af51bbb71d29228386157298200f5865ca8a19
Alex Dunn <dunn.alex@gmail.com>
2015-07-11 13:26:51 -0700

    trafficserver 5.3.1
    Closes #41604.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/trafficserver.rb
+++ Library/Formula/trafficserver.rb
@@ -1,25 +1,21 @@
 class Trafficserver < Formula
   desc "HTTP/1.1 compliant caching proxy server"
   homepage "https://trafficserver.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=trafficserver/trafficserver-5.3.0.tar.bz2"
-  mirror "https://archive.apache.org/dist/trafficserver/trafficserver-5.3.0.tar.bz2"
-  sha256 "d14184546769bac7134804c15c2fea32232c28c920717167e633d29e6eb0822b"
+  url "https://www.apache.org/dyn/closer.cgi?path=trafficserver/trafficserver-5.3.1.tar.bz2"
+  mirror "https://archive.apache.org/dist/trafficserver/trafficserver-5.3.1.tar.bz2"
+  sha256 "e6c33c7cfb629406a320a61217e08db3123cfe4b77c2eaef0eaa520065dbeb43"
+
+  bottle do
+    sha256 "63e65ccf81d3bd0624026df914cbc32a92339831c66f3dce1e5ce23ea3ea0c8d" => :yosemite
+    sha256 "c24efc1d0699ca8e2dae90ce7d8fc868cc5bd777fa6ac50374c154af82694f8e" => :mavericks
+    sha256 "8616a64385d421ed0db0c3d692a00b6b21043a54a7125c0f331d3b56659b073f" => :mountain_lion
+  end
 
   head do
     url "https://github.com/apache/trafficserver.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool"  => :build
-
-    fails_with :gcc do
-      cause "symbol(s) not found for architecture x86_64: https://issues.apache.org/jira/browse/TS-3630"
-    end
-  end
-
-  bottle do
-    sha256 "63e65ccf81d3bd0624026df914cbc32a92339831c66f3dce1e5ce23ea3ea0c8d" => :yosemite
-    sha256 "c24efc1d0699ca8e2dae90ce7d8fc868cc5bd777fa6ac50374c154af82694f8e" => :mavericks
-    sha256 "8616a64385d421ed0db0c3d692a00b6b21043a54a7125c0f331d3b56659b073f" => :mountain_lion
   end
 
   option "with-spdy", "Build with SPDY protocol support"
