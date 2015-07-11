a5bbfc1100fb117293ffab0a871588a10ab7d10c
Viktor Szakats <vszakats@users.noreply.github.com>
2015-07-11 12:47:10 +0200

    rock: https homepage, pass strict audit
    Closes #41596.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/rock.rb
+++ Library/Formula/rock.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Rock < Formula
   desc "ooc compiler written in ooc"
-  homepage 'http://ooc-lang.org'
-  url 'https://github.com/fasterthanlime/rock/archive/v0.9.10.tar.gz'
-  sha256 '39ac190ee457b2ea3c650973899bcf8930daab5b9e7e069eb1bc437a08e8b6e8'
+  homepage "https://ooc-lang.org/"
+  url "https://github.com/fasterthanlime/rock/archive/v0.9.10.tar.gz"
+  sha256 "39ac190ee457b2ea3c650973899bcf8930daab5b9e7e069eb1bc437a08e8b6e8"
 
-  head 'https://github.com/fasterthanlime/rock.git'
+  head "https://github.com/fasterthanlime/rock.git"
 
   bottle do
     cellar :any
@@ -15,19 +13,19 @@ class Rock < Formula
     sha256 "34308d7af495565e3b9bc274615e0e5412a3721fc538ce73f68d973b1cb23a6a" => :mountain_lion
   end
 
-  depends_on 'bdw-gc'
+  depends_on "bdw-gc"
 
   def install
-      # make rock using provided bootstrap
-      ENV['OOC_LIBS'] = prefix
-      system "make rescue"
-      bin.install 'bin/rock'
-      man1.install "docs/rock.1"
+    # make rock using provided bootstrap
+    ENV["OOC_LIBS"] = prefix
+    system "make", "rescue"
+    bin.install "bin/rock"
+    man1.install "docs/rock.1"
 
-      # install misc authorship files & rock binary in place
-      # copy the sdk, libs and docs
-      prefix.install "rock.use", "sdk.use", "sdk-net.use", "sdk-dynlib.use", "pcre.use", "sdk", "README.md"
-      doc.install Dir["docs/*"]
+    # install misc authorship files & rock binary in place
+    # copy the sdk, libs and docs
+    prefix.install "rock.use", "sdk.use", "sdk-net.use", "sdk-dynlib.use", "pcre.use", "sdk", "README.md"
+    doc.install Dir["docs/*"]
   end
 
   test do
