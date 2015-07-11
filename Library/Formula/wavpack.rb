9b879f7b87cb95e5a24307bd0cd975bb80cd2d06
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-11 03:54:19 +0100

    wavpack: add 4.75.0 bottle.

--- Library/Formula/wavpack.rb
+++ Library/Formula/wavpack.rb
@@ -11,6 +11,13 @@ class Wavpack < Formula
       sha256 "a6e010e3a50697811db42860ee4af1190ef4c098f65738d21890c2fb2d57f282"
     end
   end
+  bottle do
+    cellar :any
+    sha256 "b4638907a9b334bd9a5031afafa3f63d17d2d3e5f85eb15c615ce8aaff08b3dd" => :yosemite
+    sha256 "b96271f6122b58ffbc59bd8a111afd127c33a35f94e114db9efa2a9d44ccc9a5" => :mavericks
+    sha256 "1fe74ab9dbae953e2e06dd623800a8de1523d3c1651df378c339d8b6f4b3476d" => :mountain_lion
+  end
+
 
   head do
     url "https://github.com/dbry/WavPack.git"
