546ff0cc8b3ab429dca0344108f2cd0d8d75f140
Baptiste Fontaine <batifon@yahoo.fr>
2015-07-12 00:33:36 +0200

    bibtexconv 1.1.3, head added
    Closes #41611.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/bibtexconv.rb
+++ Library/Formula/bibtexconv.rb
@@ -1,8 +1,8 @@
 class Bibtexconv < Formula
   desc "BibTeX file converter"
-  homepage "https://www.iem.uni-due.de/~dreibh/bibtexconv/"
-  url "https://www.uni-due.de/~be0001/bibtexconv/download/bibtexconv-1.1.0.tar.gz"
-  sha1 "c9ea5d067de069ddbe7b6500043ff87cae2fb8ba"
+  homepage "https://www.uni-due.de/~be0001/bibtexconv/"
+  url "https://www.uni-due.de/~be0001/bibtexconv/download/bibtexconv-1.1.3.tar.gz"
+  sha256 "f7fd69ff99cd48a77e53aed4219defaf1f45485a07978faec01c2b9074886e03"
 
   bottle do
     cellar :any
@@ -11,9 +11,21 @@ class Bibtexconv < Formula
     sha1 "570f919ab0a297ef720c5a6ffa326350e55b8195" => :mountain_lion
   end
 
+  head do
+    url "https://github.com/dreibh/bibtexconv.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "openssl"
 
   def install
+    if build.head?
+      inreplace "bootstrap", "/usr/bin/glibtoolize", Formula["libtool"].bin/"glibtoolize"
+      system "./bootstrap"
+    end
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
@@ -25,7 +37,9 @@ class Bibtexconv < Formula
   test do
     cp "#{Formula["bibtexconv"].opt_share}/doc/bibtexconv/examples/ExampleReferences.bib", testpath
 
-    system bin/"bibtexconv", "#{testpath}/ExampleReferences.bib", "-export-to-bibtex=UpdatedReferences.bib",
-                             "-check-urls", "-only-check-new-urls", "-non-interactive"
+    system bin/"bibtexconv", "#{testpath}/ExampleReferences.bib",
+                             "-export-to-bibtex=UpdatedReferences.bib",
+                             "-check-urls", "-only-check-new-urls",
+                             "-non-interactive"
   end
 end
