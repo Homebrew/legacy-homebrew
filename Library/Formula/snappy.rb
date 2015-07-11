dd33260bde37577befcdd0bd6fb51891104824e0
Alex Dunn <dunn.alex@gmail.com>
2015-07-10 20:57:02 -0700

    snappy 1.1.3
    Closes #41590.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/snappy.rb
+++ Library/Formula/snappy.rb
@@ -1,23 +1,21 @@
 class Snappy < Formula
   desc "Compression/decompression library aiming for high speed"
   homepage "https://code.google.com/p/snappy/"
-  url "https://drive.google.com/uc?id=0B0xs9kK-b5nMOWIxWGJhMXd6aGs&export=download"
-  mirror "https://mirrors.kernel.org/debian/pool/main/s/snappy/snappy_1.1.2.orig.tar.gz"
-  sha256 "f9d8fe1c85494f62dbfa3efe8e73bc23d8dec7a254ff7fe09ec4b0ebfc586af4"
-  version "1.1.2"
-
-  head do
-    url "https://github.com/google/snappy.git"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-    depends_on "libtool" => :build
-  end
+  url "https://github.com/google/snappy/releases/download/1.1.3/snappy-1.1.3.tar.gz"
+  sha256 "2f1e82adf0868c9e26a5a7a3115111b6da7e432ddbac268a7ca2fae2a247eef3"
 
   bottle do
     cellar :any
     sha256 "c95c90ea412c185d42346f21637157c82ccfd6cfee7830d3fdf7c7427cc1630f" => :yosemite
     sha256 "3237d3ca278cabb40968699b45914fd3468363cb291bfc19e45a265a68a54c28" => :mavericks
     sha256 "36ac0b1d4cb6edbe915ba54a79a600674757ce5682fa1389e4a1ef98d7c10884" => :mountain_lion
+  end
+
+  head do
+    url "https://github.com/google/snappy.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
   end
 
   option :universal
