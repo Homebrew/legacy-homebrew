424a886f4bdde5fe851cedf3c5ef2b66513b9491
Viktor Szakats <vszakats@users.noreply.github.com>
2015-07-12 22:37:53 +0200

    gpsbabel: use https homepage, fix style nits

--- Library/Formula/gpsbabel.rb
+++ Library/Formula/gpsbabel.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Gpsbabel < Formula
   desc "GPSBabel converts/uploads GPS waypoints, tracks, and routes"
-  homepage 'http://www.gpsbabel.org'
+  homepage "https://www.gpsbabel.org/"
   url "http://gpsbabel.googlecode.com/svn/trunk/gpsbabel", :revision => "4962"
   version "1.5.2"
 
-  head 'http://gpsbabel.googlecode.com/svn/trunk/gpsbabel'
+  head "http://gpsbabel.googlecode.com/svn/trunk/gpsbabel"
 
   bottle do
     sha256 "029bee003e90047bfe0d68958e72c1aa638d8eb57aa70a176ef0585d71a9a548" => :yosemite
@@ -14,14 +12,14 @@ class Gpsbabel < Formula
     sha256 "6a594e455bb92b69c35131f46fdd3eb3a651c012fa549fe927e8010dd6e4d4fd" => :mountain_lion
   end
 
-  depends_on 'libusb' => :optional
+  depends_on "libusb" => :optional
   depends_on "qt"
 
   def install
-    args = ['--disable-debug', '--disable-dependency-tracking',
-            "--prefix=#{prefix}", '--with-zlib=system']
-    args << '--without-libusb' if build.without? 'libusb'
+    args = ["--disable-debug", "--disable-dependency-tracking",
+            "--prefix=#{prefix}", "--with-zlib=system"]
+    args << "--without-libusb" if build.without? "libusb"
     system "./configure", *args
-    system 'make', 'install'
+    system "make", "install"
   end
 end
