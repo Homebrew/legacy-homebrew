fe900321f8fac5c6901e7e8b0eaf069754f097c7
Geoff Nixon <geoff@geoff.codes>
2015-07-12 08:09:55 -0700

    fish 2.2.0
    fish 2.2.0 + formula cleanup
    
    Closes #41631.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/fish.rb
+++ Library/Formula/fish.rb
@@ -1,8 +1,8 @@
 class Fish < Formula
   desc "User-friendly command-line shell for UNIX-like operating systems"
   homepage "http://fishshell.com"
-  url "http://fishshell.com/files/2.1.2/fish-2.1.2.tar.gz"
-  sha1 "f7f8d8d26721833be3458b8113c74b747296ec0b"
+  url "http://fishshell.com/files/2.2.0/fish-2.2.0.tar.gz"
+  sha256 "a76339fd14ce2ec229283c53e805faac48c3e99d9e3ede9d82c0554acfc7b77a"
 
   bottle do
     revision 1
@@ -15,11 +15,8 @@ class Fish < Formula
     url "https://github.com/fish-shell/fish-shell.git", :shallow => false
 
     depends_on "autoconf" => :build
-    # Indeed, the head build always builds documentation
     depends_on "doxygen" => :build
   end
-
-  skip_clean "share/doc"
 
   def install
     system "autoconf" if build.head?
@@ -29,20 +26,20 @@ class Fish < Formula
     system "make", "install"
   end
 
-  def post_install
-    system "pkill fishd || true"
-  end
-
-  test do
-    system "#{bin}/fish", "-c", "echo"
-  end
-
   def caveats; <<-EOS.undent
     You will need to add:
       #{HOMEBREW_PREFIX}/bin/fish
     to /etc/shells. Run:
       chsh -s #{HOMEBREW_PREFIX}/bin/fish
     to make fish your default shell.
+
+    If you are upgrading from an older version of fish, you should now run:
+      killall fishd
+    to terminate the outdated fish daemon.
     EOS
+  end
+
+  test do
+    system "#{bin}/fish", "-c", "echo"
   end
 end
