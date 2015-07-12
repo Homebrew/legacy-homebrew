32e46b0a2e6efc23a32277e24a72dce48b8ffceb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-12 10:36:00 +0100

    freeimage: add 3.17.0 bottle.

--- Library/Formula/freeimage.rb
+++ Library/Formula/freeimage.rb
@@ -16,6 +16,13 @@ class Freeimage < Formula
   version '3.17.0'
   sha256 'fbfc65e39b3d4e2cb108c4ffa8c41fd02c07d4d436c594fff8dab1a6d5297f89'
 
+  bottle do
+    cellar :any
+    sha256 "3b9bf4ed62d4d0d88daa301cf5c183d16474d1a0957a14fffc2ead3d0d88ecb8" => :yosemite
+    sha256 "d7d197266815f5f3d732c9afd66055e7abf49362d7449bfea21e3aa358e5a4a1" => :mavericks
+    sha256 "9d1e914ae20deb7066caf5f1cf52c3d48c0c04ccd36b791170c7e1fcb3528a36" => :mountain_lion
+  end
+
   option :universal
 
   patch :DATA
