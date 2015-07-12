d90e47ad5c14b1a7fd82c42b102e3c3846a6eeb6
Ben Bailey <bennettbailey@gmail.com>
2015-07-12 01:23:34 -0500

    remove extraneous test text

--- Library/Formula/nmh.rb
+++ Library/Formula/nmh.rb
@@ -14,15 +14,6 @@ class Nmh < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test nmh`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "inc -help"
     system "scan -help"
     system "rmm -help"
