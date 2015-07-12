f962930b8c3c57edf88c19cd47e102077b459b67
Viktor Szakats <vszakats@users.noreply.github.com>
2015-07-12 15:27:20 +0200

    pyside-tools: fix head url and style nits
    also:
    * follow homepage redirect and update to https

--- Library/Formula/pyside-tools.rb
+++ Library/Formula/pyside-tools.rb
@@ -1,18 +1,16 @@
-require 'formula'
-
 class PysideTools < Formula
   desc "PySide development tools (pyuic and pyrcc)"
-  homepage 'http://www.pyside.org'
-  url 'https://github.com/PySide/Tools/archive/0.2.15.tar.gz'
-  sha1 'b668d15e8d67e25a653db5abf8f542802e2ee2dd'
+  homepage "https://wiki.qt.io/PySide"
+  url "https://github.com/PySide/Tools/archive/0.2.15.tar.gz"
+  sha256 "8a7fe786b19c5b2b4380aff0a9590b3129fad4a0f6f3df1f39593d79b01a9f74"
 
-  head 'git://gitorious.org/pyside/pyside-tools.git'
+  head "https://github.com/PySide/Tools.git"
 
-  depends_on 'cmake' => :build
-  depends_on 'pyside'
+  depends_on "cmake" => :build
+  depends_on "pyside"
 
   def install
     system "cmake", ".", "-DSITE_PACKAGE=lib/python2.7/site-packages", *std_cmake_args
-    system "make install"
+    system "make", "install"
   end
 end
