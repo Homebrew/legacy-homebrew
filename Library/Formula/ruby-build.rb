845b642fcb1228ef74d3cdf75ecb2e4a06687b91
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-12 04:29:28 +0100

    ruby-build: bump revision

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -1,10 +1,9 @@
-require "formula"
-
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/sstephenson/ruby-build"
   url "https://github.com/sstephenson/ruby-build/archive/v20150709.tar.gz"
   sha256 "dff8350bd6ad6da4853c8a074aa77ebe7525410306bc0bb58d91d12888c9421f"
+  revision 1
 
   head "https://github.com/sstephenson/ruby-build.git"
 
