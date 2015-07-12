99346926c8648eaefdc5296b759705d637a0f793
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-12 20:02:24 +0100

    pyside-tools: add 0.2.15 bottle.

--- Library/Formula/pyside-tools.rb
+++ Library/Formula/pyside-tools.rb
@@ -6,6 +6,12 @@ class PysideTools < Formula
 
   head "https://github.com/PySide/Tools.git"
 
+  bottle do
+    sha256 "1f61192245b05946057f47d6a73788a998c86eefc846e1d09821a316c5bc2975" => :yosemite
+    sha256 "2cb88ceddebde2c5ff388a090b53f9a8c6dc986d881a1eab1f57ff2cdb43edf0" => :mavericks
+    sha256 "0c829a3c663e1cdecc343377d7e0ddfe18fc8518360ed495695fefe84da7b221" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
   depends_on "pyside"
 
