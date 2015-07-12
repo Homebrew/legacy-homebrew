69cd0f9b360641f6da404c6e23980cb55703e9d4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-12 11:09:45 +0100

    gpsbabel: add 1.5.2 bottle.

--- Library/Formula/gpsbabel.rb
+++ Library/Formula/gpsbabel.rb
@@ -8,6 +8,12 @@ class Gpsbabel < Formula
 
   head 'http://gpsbabel.googlecode.com/svn/trunk/gpsbabel'
 
+  bottle do
+    sha256 "029bee003e90047bfe0d68958e72c1aa638d8eb57aa70a176ef0585d71a9a548" => :yosemite
+    sha256 "056e9b4f345b1b3fa8138e2381de434da7a5eb7a876859009f89905f9b5a4a61" => :mavericks
+    sha256 "6a594e455bb92b69c35131f46fdd3eb3a651c012fa549fe927e8010dd6e4d4fd" => :mountain_lion
+  end
+
   depends_on 'libusb' => :optional
   depends_on "qt"
 
