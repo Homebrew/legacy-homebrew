ad6927148968aebb581f618810726c52d68dcccb
ridiculousfish <corydoras@ridiculousfish.com>
2015-07-12 03:01:20 -0700

    Update fish recipe for fish 2.2.0 release

--- Library/Formula/fish.rb
+++ Library/Formula/fish.rb
@@ -1,15 +1,8 @@
 class Fish < Formula
   desc "User-friendly command-line shell for UNIX-like operating systems"
   homepage "http://fishshell.com"
-  url "http://fishshell.com/files/2.1.2/fish-2.1.2.tar.gz"
-  sha1 "f7f8d8d26721833be3458b8113c74b747296ec0b"
-
-  bottle do
-    revision 1
-    sha1 "7560818f385831e3d18be1458b1c5e52216f121c" => :yosemite
-    sha1 "a158ae57a437e5f8b2fdff88177be13fa6f35502" => :mavericks
-    sha1 "8d0aa59ebb4cf446e0f0fdf0f91738ffee7edbae" => :mountain_lion
-  end
+  url "http://fishshell.com/files/2.2.0/fish-2.2.0.tar.gz"
+  sha1 "cd7935b3301444afa88afb272d8db5542085c9f2"
 
   head do
     url "https://github.com/fish-shell/fish-shell.git", :shallow => false
