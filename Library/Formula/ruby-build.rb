1bc4e58127026490dd0aca8523f83095a01b2452
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-13 20:44:01 +0100

    ruby-build: add 20150709_1 bottle.

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -7,6 +7,13 @@ class RubyBuild < Formula
 
   head "https://github.com/sstephenson/ruby-build.git"
 
+  bottle do
+    cellar :any
+    sha256 "ce607b76a4af929273a733171c9fb79cb37acecbe24c240facac2bcbc73951d8" => :yosemite
+    sha256 "b4f25b3c8bf8adc4367955fb94ca59f2ecb6d777aae661dbe59776cd127cd577" => :mavericks
+    sha256 "b114523afb6b27a37ad20b7279e9c970fa6eb418f066f64fa69efd829663e6ac" => :mountain_lion
+  end
+
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
   depends_on "openssl" => :recommended
