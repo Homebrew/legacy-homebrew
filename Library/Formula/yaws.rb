a4861155e52af08e5b944b1c03303c07d4ffcffc
optikfluffel <optik@fluffel.io>
2015-07-13 19:14:13 +0200

    yaws 2.0
    use updated yaws "release" for otp 18 support
    
    Closes #41668.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/yaws.rb
+++ Library/Formula/yaws.rb
@@ -1,8 +1,8 @@
 class Yaws < Formula
   desc "Webserver for dynamic content (written in Erlang)"
   homepage "http://yaws.hyber.org"
-  url "https://github.com/klacke/yaws/archive/yaws-1.99.tar.gz"
-  sha1 "ea407afe7b080ed065182d73503899a75360dfaf"
+  url "https://github.com/klacke/yaws/archive/yaws-2.0.tar.gz"
+  sha256 "1b51a5c442ca70e9de562efd2e1d36a7a4ccde05fbeb961a4b538641b7c8173c"
   head "https://github.com/klacke/yaws.git"
 
   bottle do
