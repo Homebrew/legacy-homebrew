799bcbb8aa101689f961717708225f34aa219f4d
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-13 07:18:43 +0100

    iodine: add head

--- Library/Formula/iodine.rb
+++ Library/Formula/iodine.rb
@@ -1,10 +1,11 @@
-require "formula"
-
 class Iodine < Formula
   desc "Tool for tunneling IPv4 data through a DNS server"
   homepage "http://code.kryo.se/iodine/"
   url "http://code.kryo.se/iodine/iodine-0.7.0.tar.gz"
-  sha1 "f4c49305b6f46a547b160b3bd8c962942d701a63"
+  mirror "https://mirrors.kernel.org/debian/pool/main/i/iodine/iodine_0.7.0.orig.tar.gz"
+  sha256 "ad2b40acf1421316ec15800dcde0f587ab31d7d6f891fa8b9967c4ded93c013e"
+
+  head "https://github.com/yarrick/iodine.git"
 
   bottle do
     cellar :any
