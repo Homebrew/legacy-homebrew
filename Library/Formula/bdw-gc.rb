ed3db4f741effd5e7cfc412470840756fb326f01
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-14 12:10:55 +0200

    bdw-gc: use sha256

--- Library/Formula/bdw-gc.rb
+++ Library/Formula/bdw-gc.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class BdwGc < Formula
   desc "Garbage collector for C and C++"
   homepage "http://www.hboehm.info/gc/"
   url "http://www.hboehm.info/gc/gc_source/gc-7.4.2.tar.gz"
-  sha1 "cd4a54620c38a2c361b3ee99dd134dbffb57c313"
+  sha256 "63320ad7c45460e4a40e03f5aa4c6893783f21a16416c3282b994f933312afa2"
 
   depends_on "pkg-config" => :build
   depends_on "libatomic_ops" => :build
