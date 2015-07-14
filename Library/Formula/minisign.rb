f0bc57e4ee0cdb2d536f5729efa71a2b9dfdfd70
Bryan Black <reelsense@users.noreply.github.com>
2015-07-13 18:48:06 -0700

    minisign 0.4 (new formula)
    description is shorter
    
    Closes #41682.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/minisign.rb
+++ Library/Formula/minisign.rb
@@ -1,5 +1,5 @@
 class Minisign < Formula
-  desc "Simply sign files and verify signatures. Compatible with signify in OpenBSD."
+  desc "Sign files & verify signatures. Works with signify in OpenBSD."
   homepage "https://jedisct1.github.io/minisign/"
   url "https://github.com/jedisct1/minisign/archive/0.4.tar.gz"
   sha256 "dc7695513e715654a51d07ad3e6b0083f9cb38b1a5bc9f16e1177d15af992dcc"
