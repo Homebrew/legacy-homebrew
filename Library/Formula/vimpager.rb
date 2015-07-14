2d422341211177054011f77485a02e6c4f500f80
Decors <decors1215@gmail.com>
2015-07-14 18:38:34 +0900

    vimpager 2.06
    Add test code.

--- Library/Formula/vimpager.rb
+++ Library/Formula/vimpager.rb
@@ -1,8 +1,8 @@
 class Vimpager < Formula
   desc "Use ViM as PAGER"
   homepage "https://github.com/rkitover/vimpager"
-  url "https://github.com/rkitover/vimpager/archive/2.04.tar.gz"
-  sha256 "eefbfe178ea03be3df8bbad82ba162797ab8fb49c994b4b240d513cd0c3ef3f0"
+  url "https://github.com/rkitover/vimpager/archive/2.06.tar.gz"
+  sha256 "cc616d0840a6f2501704eea70de222ab662421f34b2da307e11fb62aa70bda5d"
   head "https://github.com/rkitover/vimpager.git"
 
   option "with-pandoc", "Use pandoc to build and install man pages"
@@ -20,5 +20,13 @@ class Vimpager < Formula
     To use vimpager as your default pager, add `export PAGER=vimpager` to your
     shell configuration.
     EOS
+  end
+
+  test do
+    (testpath/"test.txt").write <<-EOS.undent
+      This is test
+    EOS
+
+    assert_match(/This is test/, shell_output("#{bin}/vimcat test.txt"))
   end
 end
