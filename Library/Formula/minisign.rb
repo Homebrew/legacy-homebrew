6115d6e56dba507ae4cca0e1ef8ffa0dc1d6ff4a
Bryan Black <reelsense@users.noreply.github.com>
2015-07-13 18:25:32 -0700

    Update minisign.rb
    removed duplicate
    removed version
    added test
    shortened desc

--- Library/Formula/minisign.rb
+++ Library/Formula/minisign.rb
@@ -1,11 +1,9 @@
 class Minisign < Formula
-  desc "A simple tool to sign files and verify signatures. Signatures are compatible with OpenBSD’s signify tool."
+  desc "Simply sign files and verify signatures. Compatible with signify in OpenBSD."
   homepage "https://jedisct1.github.io/minisign/"
   url "https://github.com/jedisct1/minisign/archive/0.4.tar.gz"
-  version "0.4"
   sha256 "dc7695513e715654a51d07ad3e6b0083f9cb38b1a5bc9f16e1177d15af992dcc"
 
-  # depends_on "cmake" => :build
   depends_on "libsodium"
   depends_on "cmake" => :build
 
@@ -16,6 +14,6 @@ class Minisign < Formula
   end
 
   test do
-    #system "false"
+    system "minisign", "-v"
   end
 end
