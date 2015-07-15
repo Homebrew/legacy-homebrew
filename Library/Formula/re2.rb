2bf34368b79444ee9cf3c4cf4e377786fdff50e6
Alex Dunn <dunn.alex@gmail.com>
2015-07-14 22:00:26 -0700

    re2: fix HEAD build
    Closes #41727.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/re2.rb
+++ Library/Formula/re2.rb
@@ -1,24 +1,26 @@
 class Re2 < Formula
   desc "Alternative to backtracking PCRE-style regular expression engines"
   homepage "https://github.com/google/re2"
-  url "https://github.com/google/re2/archive/2015-07-01.tar.gz"
-  version "20150701"
-  sha256 "e32d6dfa48d882a81086ae26537dc2e78877bb427f61c8cd4062dff7d0b0ef08"
+
+  stable do
+    url "https://github.com/google/re2/archive/2015-07-01.tar.gz"
+    version "20150701"
+    sha256 "e32d6dfa48d882a81086ae26537dc2e78877bb427f61c8cd4062dff7d0b0ef08"
+    # Fix the symbol table to work with both libc++ and libstdc++
+    # Will be included in the next release
+    # https://github.com/google/re2/issues/1
+    patch do
+      url "https://github.com/google/re2/commit/44cdc782e5debc2c841b3fafa9ee5a61e8c42b95.patch"
+      sha256 "b1720a4f3dd0b28969600698b1aadf15bf30b9e6aef42c589d512c6a2c088579"
+    end
+
+    patch do
+      url "https://github.com/google/re2/commit/a99f38705611f678f70b7e1944d188a3c28a12ab.patch"
+      sha256 "22dac0b90f34b8fa693994831d1ea24b0d2f8fe49e404975a507ed744fb1de83"
+    end
+  end
 
   head "https://github.com/google/re2.git"
-
-  # Fix the symbol table to work with both libc++ and libstdc++
-  # Will be included in the next release
-  # https://github.com/google/re2/issues/1
-  patch do
-    url "https://github.com/google/re2/commit/44cdc782e5debc2c841b3fafa9ee5a61e8c42b95.patch"
-    sha256 "b1720a4f3dd0b28969600698b1aadf15bf30b9e6aef42c589d512c6a2c088579"
-  end
-
-  patch do
-    url "https://github.com/google/re2/commit/a99f38705611f678f70b7e1944d188a3c28a12ab.patch"
-    sha256 "22dac0b90f34b8fa693994831d1ea24b0d2f8fe49e404975a507ed744fb1de83"
-  end
 
   bottle do
     cellar :any
@@ -29,7 +31,7 @@ class Re2 < Formula
 
   def install
     system "make", "install", "prefix=#{prefix}"
-    mv lib/"libre2.so.0.0.0", lib/"libre2.0.0.0.dylib"
+    mv lib/"libre2.so.0.0.0", lib/"libre2.0.0.0.dylib" if build.stable?
     system "install_name_tool", "-id", "#{lib}/libre2.0.dylib", "#{lib}/libre2.0.0.0.dylib"
     lib.install_symlink "libre2.0.0.0.dylib" => "libre2.0.dylib"
     lib.install_symlink "libre2.0.0.0.dylib" => "libre2.dylib"
