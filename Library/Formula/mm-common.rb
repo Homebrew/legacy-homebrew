94f2663aed45699d39a811a1827fd94ef2a0b986
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-16 01:01:11 +0200

    mm-common 0.9.8
    Closes #41758.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/mm-common.rb
+++ Library/Formula/mm-common.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class MmCommon < Formula
   desc "C++ interfaces for GTK+ and GNOME"
   homepage "http://www.gtkmm.org"
-  url "http://ftp.gnome.org/pub/GNOME/sources/mm-common/0.9/mm-common-0.9.7.tar.xz"
-  sha256 "78f47336f3bdf034a384c59a39cc9f0d566e69e36aa7c9ee3ec0bb6a94bf8b3e"
+  url "https://download.gnome.org/sources/mm-common/0.9/mm-common-0.9.8.tar.xz"
+  sha256 "c9ab5fd3872fbe245fbc35347acf4a95063111f81d54c43df3af662dad0a03d5"
 
   def install
     system "./configure", "--disable-silent-rules", "--prefix=#{prefix}"
