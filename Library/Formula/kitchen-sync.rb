966f3d866c54def8bada0a05d608b907a20d832b
Denis Denisov <denji0k@gmail.com>
2015-07-15 19:29:23 +0300

    kitchen-sync 0.44
    Closes #41746.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/kitchen-sync.rb
+++ Library/Formula/kitchen-sync.rb
@@ -1,8 +1,8 @@
 class KitchenSync < Formula
   desc "Fast efficiently sync database without dumping & reloading"
   homepage "https://github.com/willbryant/kitchen_sync"
-  url "https://github.com/willbryant/kitchen_sync/archive/0.40.tar.gz"
-  sha256 "2a595cf3b562a2d9d02fcb8fbc5bd8908b2cbadcbf51d817fe9562ae85b77061"
+  url "https://github.com/willbryant/kitchen_sync/archive/0.44.tar.gz"
+  sha256 "71a0c67b22e2d24da5b2c6faa9a90e5be916c13ad912f157bd6bc8a04764ee2e"
   head "https://github.com/willbryant/kitchen_sync.git"
 
   bottle do
