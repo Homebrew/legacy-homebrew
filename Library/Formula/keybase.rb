ea307e235a3956b2bfa206a11f004a03f6350835
Deirdre Connolly <durumcrustulum@gmail.com>
2015-07-15 13:23:29 -0400

    keybase 0.8.9
    Closes #41749.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/keybase.rb
+++ Library/Formula/keybase.rb
@@ -1,8 +1,8 @@
 class Keybase < Formula
   desc "Command-line interface to Keybase.io"
   homepage "https://keybase.io/"
-  url "https://github.com/keybase/node-client/archive/v0.8.6.tar.gz"
-  sha256 "b469b481def07e7466177d30d93bc4e98ad164bbd855bd722dbb7fff07569eda"
+  url "https://github.com/keybase/node-client/archive/v0.8.9.tar.gz"
+  sha256 "2feb54d26afc639f4d4998fbc35aafa2100a9926a6ec4f84343ca56d0284a3dd"
   head "https://github.com/keybase/node-client.git"
 
   depends_on "node"
