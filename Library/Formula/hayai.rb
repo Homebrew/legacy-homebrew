fd73dac93dac3867d94ef2de96819db6ea630b72
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-15 21:50:45 +0100

    hayai: add 0.1.0 bottle.

--- Library/Formula/hayai.rb
+++ Library/Formula/hayai.rb
@@ -4,6 +4,12 @@ class Hayai < Formula
   url "https://github.com/nickbruun/hayai/archive/v0.1.0.tar.gz"
   sha256 "739d6dc5126a0b3dfe6c944999f48e7ede823bfdbf1f8801f23af25bf355db65"
 
+  bottle do
+    sha256 "3bc74df44803916f35cba264b4785d3b370ead8fc8dcce37493140504f562a3f" => :yosemite
+    sha256 "31adaeee9eb993ad1eee8285ae3841cb26c5fded54368899ed6578d3e70664ed" => :mavericks
+    sha256 "717b2a63a7c070498351a24f8e41c40ea982d21ef32b6c074a442e3714974597" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
