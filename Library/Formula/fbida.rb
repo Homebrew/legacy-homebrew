f2936bcf48b52071fb5ba314fd68ce653d564c4b
Larry Gilbert <larry@l2g.to>
2015-07-15 09:29:07 -0700

    fbida 2.10
    * New upstream version
    * Update patch for new version
    * Add test
    * Other changes for `brew audit --strict`
    
    Closes #41748.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/fbida.rb
+++ Library/Formula/fbida.rb
@@ -1,38 +1,38 @@
-require 'formula'
-
 class Fbida < Formula
   desc "View and edit photo images"
-  homepage 'http://linux.bytesex.org/fbida/'
-  url 'http://dl.bytesex.org/releases/fbida/fbida-2.09.tar.gz'
-  sha1 '5838b2cb7188481fece6159fccb3b121acd906e7'
+  homepage "http://linux.bytesex.org/fbida/"
+  url "http://dl.bytesex.org/releases/fbida/fbida-2.10.tar.gz"
+  sha256 "7a5a3aac61b40a6a2bbf716d270a46e2f8e8d5c97e314e927d41398a4d0b6cb6"
 
-  depends_on 'libexif'
-  depends_on 'jpeg'
+  depends_on "libexif"
+  depends_on "jpeg"
 
-  # Fix for build failure in fbida 2.09 (and earlier)
-  # Check again in fbida 2.10
+  # Fix issue in detection of jpeg library
   patch :DATA
 
   def install
-    ENV.append 'LDFLAGS', '-liconv'
+    ENV.append "LDFLAGS", "-liconv"
     system "make"
-    bin.install 'exiftran'
-    man1.install 'exiftran.man' => 'exiftran.1'
+    bin.install "exiftran"
+    man1.install "exiftran.man" => "exiftran.1"
+  end
+
+  test do
+    system "#{bin}/exiftran", "-9", "-o", "out.jpg", test_fixtures("test.jpg")
   end
 end
 
 __END__
 diff --git a/GNUmakefile b/GNUmakefile
-index 2efae6c..9c45561 100644
+index 2d18ab4..5b409fb 100644
 --- a/GNUmakefile
 +++ b/GNUmakefile
-@@ -30,8 +30,7 @@ include $(srcdir)/mk/Autoconf.mk
+@@ -30,7 +30,7 @@ include $(srcdir)/mk/Autoconf.mk
  
  ac_jpeg_ver = $(shell \
  	$(call ac_init,for libjpeg version);\
--	$(call ac_s_cmd,echo -e '\#include <jpeglib.h>\nJPEG_LIB_VERSION' \
--		| cpp | tail -n 1);\
-+	$(call ac_s_cmd,printf JPEG_LIB_VERSION | cpp -include jpeglib.h | tail -n 1);\
+-	$(call ac_s_cmd,echo JPEG_LIB_VERSION \
++	$(call ac_s_cmd,printf JPEG_LIB_VERSION \
+		| cpp -include jpeglib.h | tail -n 1);\
  	$(call ac_fini))
  
- define make-config
