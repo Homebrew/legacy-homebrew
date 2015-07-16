7e93ad15a80acfd5c39c8aa4a2e53ee9d5a6b7e7
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-16 07:35:16 +0100

    gsl: add test

--- Library/Formula/gsl.rb
+++ Library/Formula/gsl.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class Gsl < Formula
   desc "Numerical library for C and C++"
-  homepage 'https://www.gnu.org/software/gsl/'
-  url 'http://ftpmirror.gnu.org/gsl/gsl-1.16.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/gsl/gsl-1.16.tar.gz'
-  sha1 '210af9366485f149140973700d90dc93a4b6213e'
+  homepage "https://www.gnu.org/software/gsl/"
+  url "http://ftpmirror.gnu.org/gsl/gsl-1.16.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/gsl/gsl-1.16.tar.gz"
+  sha256 "73bc2f51b90d2a780e6d266d43e487b3dbd78945dd0b04b14ca5980fe28d2f53"
 
   bottle do
     cellar :any
@@ -20,9 +18,12 @@ class Gsl < Formula
   def install
     ENV.universal_binary if build.universal?
 
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make" # A GNU tool which doesn't support just make install! Shameful!
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gsl-randist", "0", "20", "cauchy", "30"
   end
 end
