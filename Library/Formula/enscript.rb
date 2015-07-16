7133c94ef3e988160225145fae3b687562d6af74
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-16 04:58:11 +0100

    enscript: use secure mirror

--- Library/Formula/enscript.rb
+++ Library/Formula/enscript.rb
@@ -1,22 +1,20 @@
-require 'formula'
-
 class Enscript < Formula
   desc "Convert text to Postscript, HTML, or RTF, with syntax highlighting"
-  homepage 'http://www.gnu.org/software/enscript/'
-  url 'http://ftpmirror.gnu.org/enscript/enscript-1.6.6.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/enscript/enscript-1.6.6.tar.gz'
-  sha1 '1f1e97a2ebb3d77f48c57487fe39e64139fb2beb'
+  homepage "https://www.gnu.org/software/enscript/"
+  url "http://ftpmirror.gnu.org/enscript/enscript-1.6.6.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/enscript/enscript-1.6.6.tar.gz"
+  sha256 "6d56bada6934d055b34b6c90399aa85975e66457ac5bf513427ae7fc77f5c0bb"
 
-  head 'git://git.savannah.gnu.org/enscript.git'
+  head "git://git.savannah.gnu.org/enscript.git"
 
   keg_only :provided_pre_mountain_lion
 
-  depends_on 'gettext'
+  depends_on "gettext"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
