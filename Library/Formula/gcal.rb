87293cafb988c67eea71f787063df12875cf90ce
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-16 05:10:08 +0100

    gcal: add test
    Closes #41768.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/gcal.rb
+++ Library/Formula/gcal.rb
@@ -1,14 +1,17 @@
-require 'formula'
-
 class Gcal < Formula
   desc "Gcal is a program for calculating and printing calendars"
-  homepage 'http://www.gnu.org/software/gcal/'
-  url 'http://ftpmirror.gnu.org/gcal/gcal-3.6.3.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/gcal/gcal-3.6.3.tar.xz'
-  sha1 'a5d68216d8b0735c9b095fb81a08d6dbf5cdeedd'
+  homepage "https://www.gnu.org/software/gcal/"
+  url "http://ftpmirror.gnu.org/gcal/gcal-3.6.3.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/gcal/gcal-3.6.3.tar.xz"
+  sha256 "6742913a1d011ac109ad713ef4a8263eaf4c5cfd315471626a92f094e3e4b31b"
 
   def install
-    system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking'
-    system "make install"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make", "install"
+  end
+
+  test do
+    date = shell_output("date +%Y")
+    assert_match date, shell_output("#{bin}/gcal")
   end
 end
