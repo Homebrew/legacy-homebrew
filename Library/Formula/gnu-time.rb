fc75893304f86473f9d2db6efae987b8b56055a1
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-16 07:11:34 +0100

    gnu-time: add test

--- Library/Formula/gnu-time.rb
+++ Library/Formula/gnu-time.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class GnuTime < Formula
   desc "GNU implementation of time utility"
-  homepage 'https://www.gnu.org/software/time/'
-  url 'http://ftpmirror.gnu.org/time/time-1.7.tar.gz'
-  mirror 'http://ftp.gnu.org/gnu/time/time-1.7.tar.gz'
-  sha1 'dde0c28c7426960736933f3e763320680356cc6a'
+  homepage "https://www.gnu.org/software/time/"
+  url "http://ftpmirror.gnu.org/time/time-1.7.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/time/time-1.7.tar.gz"
+  sha256 "e37ea79a253bf85a85ada2f7c632c14e481a5fd262a362f6f4fd58e68601496d"
 
   bottle do
     cellar :any
@@ -17,8 +15,8 @@ class GnuTime < Formula
   option "with-default-names", "Do not prepend 'g' to the binary"
 
   # Fixes issue with main returning void rather than int
-  # http://trac.macports.org/ticket/32860
-  # http://trac.macports.org/browser/trunk/dports/sysutils/gtime/files/patch-time.c.diff?rev=88924
+  # https://trac.macports.org/ticket/32860
+  # https://trac.macports.org/browser/trunk/dports/sysutils/gtime/files/patch-time.c.diff?rev=88924
   patch :DATA
 
   def install
@@ -27,10 +25,15 @@ class GnuTime < Formula
       "--mandir=#{man}",
       "--info=#{info}"
     ]
+
     args << "--program-prefix=g" if build.without? "default-names"
 
     system "./configure", *args
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gtime", "ruby", "--version"
   end
 end
 
