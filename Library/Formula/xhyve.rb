3da4fca596cf0aee62e537526f30ea51c8c16135
Bartek Rutkowski <bartekrutkowski@users.noreply.github.com>
2015-07-16 10:53:27 +0100

    Update project's URL and clarify description

--- Library/Formula/xhyve.rb
+++ Library/Formula/xhyve.rb
@@ -1,6 +1,6 @@
 class Xhyve < Formula
-  desc "xhyve, a lightweight OS X virtualization solution"
-  homepage "http://www.pagetable.com/?p=831"
+  desc "xhyve, lightweight OS X virtualization solution based on FreeBSD's bhyve"
+  homepage "https://github.com/mist64/xhyve"
   url "https://github.com/mist64/xhyve/archive/v0.1.0.tar.gz"
   sha256 "75be0cf1ab345874aef00833e34cf337e71aad0f946d7f9bf8ade248fc4f6b59"
   head "https://github.com/mist64/xhyve.git"
