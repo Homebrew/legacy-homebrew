fdcd9e8b8bbc4bee1cad935ea07068bbf2da8174
Alex Dunn <dunn.alex@gmail.com>
2015-07-17 15:20:10 -0700

    jboss-forge: add test
    Closes #41840.

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class JbossForge < Formula
   desc "Tools to help set up and configure a project"
-  homepage 'http://forge.jboss.org/'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.17.0.Final&e=zip&c=offline'
-  version '2.17.0.Final'
-  sha256 'c0d8cadb315b23d99fb582db670ef32beea51b9b06ea9313ec81daa3998bedd8'
+  homepage "http://forge.jboss.org/"
+  url "https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.17.0.Final&e=zip&c=offline"
+  version "2.17.0.Final"
+  sha256 "c0d8cadb315b23d99fb582db670ef32beea51b9b06ea9313ec81daa3998bedd8"
 
   bottle do
     cellar :any
@@ -16,7 +14,11 @@ class JbossForge < Formula
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install %w{ addons bin img lib logging.properties }
-    bin.install_symlink libexec/'bin/forge'
+    libexec.install %w[addons bin img lib logging.properties]
+    bin.install_symlink libexec/"bin/forge"
+  end
+
+  test do
+    assert_match "org.jboss.forge.addon:core", shell_output("#{bin}/forge --list")
   end
 end
