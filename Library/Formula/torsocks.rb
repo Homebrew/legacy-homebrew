c00b8b8ca4c8beef0aa69addbf6c270c35e9a7d7
Jeffrey C <spaz926@users.noreply.github.com>
2015-07-17 02:44:18 -0500

    Update torsocks.rb
    Updated to v2.1.0 revision a43a3656a5bb4391fb1654d5ff44a5257e1f165f

--- Library/Formula/torsocks.rb
+++ Library/Formula/torsocks.rb
@@ -2,16 +2,10 @@ class Torsocks < Formula
   desc "Use SOCKS-friendly applications with Tor"
   homepage "https://gitweb.torproject.org/torsocks.git/"
   url "https://git.torproject.org/torsocks.git",
-    :tag => "v2.0.0",
-    :revision => "ea105bb76ea1e9f9660dd2307639b75ca6d76569"
+    :tag => "v2.1.0",
+    :revision => "a43a3656a5bb4391fb1654d5ff44a5257e1f165f"
 
   head "https://git.torproject.org/torsocks.git"
-
-  bottle do
-    sha1 "75a53b9a12c5f3b1dbcdfd659f9bdecf6703a2f8" => :yosemite
-    sha1 "02573816190ad4fa6ee829e59b293224a90b6dad" => :mavericks
-    sha1 "d10034aa108b8a4baf2a6ecd73457cf279681eb3" => :mountain_lion
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -19,7 +13,8 @@ class Torsocks < Formula
 
   def install
     system "./autogen.sh"
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
