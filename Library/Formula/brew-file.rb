fdbd8a4288f0484570c4cd65013cca6986e3016e
rcmdnk <rcmdnk@gmail.com>
2015-07-17 23:09:59 +0900

    zip to tar.gz

--- Library/Formula/brew-file.rb
+++ Library/Formula/brew-file.rb
@@ -1,8 +1,8 @@
 class BrewFile < Formula
   desc "Brewfile manager for Homebrew."
   homepage "https://github.com/rcmdnk/homebrew-file/"
-  url "https://github.com/rcmdnk/homebrew-file/archive/v3.5.9.zip"
-  sha256 "65e05b90359abb997396e7e2ab060cc577aa12da4e5dfe5e79e2b6687caf1d1d"
+  url "https://github.com/rcmdnk/homebrew-file/archive/v3.5.9.tar.gz"
+  sha256 "2c7028a3798fcbf5569c5e318edb34bb48f79ff3765f10beb54dcaf385863bf2"
 
   head "https://github.com/rcmdnk/homebrew-file.git"
 
