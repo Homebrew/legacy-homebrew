1d9382f31cacd5a53a5a394b9447f21b192589f5
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-17 04:31:25 +0100

    vcsh: fix nits

--- Library/Formula/vcsh.rb
+++ Library/Formula/vcsh.rb
@@ -1,15 +1,14 @@
-require 'formula'
-
 class Vcsh < Formula
   desc "Config manager based on git"
-  homepage 'https://github.com/RichiH/vcsh'
-  url 'https://github.com/RichiH/vcsh/archive/v1.20150502-1.tar.gz'
-  version '1.20150502-1'
-  sha256 'ffb2f619926eee942da5687262e63f0816b10abdfd7f10bf5f44d50739d5f4d1'
+  homepage "https://github.com/RichiH/vcsh"
+  url "https://github.com/RichiH/vcsh/archive/v1.20150502-1.tar.gz"
+  version "1.20150502-1"
+  sha256 "ffb2f619926eee942da5687262e63f0816b10abdfd7f10bf5f44d50739d5f4d1"
+
   def install
-    bin.install 'vcsh'
-    man1.install 'vcsh.1'
-    zsh_completion.install '_vcsh'
+    bin.install "vcsh"
+    man1.install "vcsh.1"
+    zsh_completion.install "_vcsh"
   end
 
   test do
