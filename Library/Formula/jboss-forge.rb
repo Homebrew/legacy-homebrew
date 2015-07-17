43205fb6648e32f46e27ab37596128c1c932bd3f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-17 18:20:44 +0100

    jboss-forge: add 2.17.0.Final bottle.

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -7,6 +7,13 @@ class JbossForge < Formula
   version '2.17.0.Final'
   sha256 'c0d8cadb315b23d99fb582db670ef32beea51b9b06ea9313ec81daa3998bedd8'
 
+  bottle do
+    cellar :any
+    sha256 "4accd77051ff6cd0e425bbb17e6557b8110999430d1ab504336325605b198efc" => :yosemite
+    sha256 "aff4bb3754d83399de23494e3f223cd071678cd5f5a204b4883f0ecb784c4290" => :mavericks
+    sha256 "d69a922b8ac9cf315f8e2af074c068e163b13aa551bfc7641c557f7bc137305e" => :mountain_lion
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w{ addons bin img lib logging.properties }
