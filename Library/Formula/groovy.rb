97a60211dbf3d3634aeb9689da7f3833eb8391ba
Jay Barker <jbarker@jbarker.com>
2015-07-18 19:39:49 +0200

    groovy 2.4.4
    Closes #41864.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/groovy.rb
+++ Library/Formula/groovy.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Groovy < Formula
   desc "Groovy: a Java-based scripting language"
   homepage 'http://www.groovy-lang.org'
-  url 'https://dl.bintray.com/groovy/maven/groovy-binary-2.4.3.zip'
-  sha1 '47837096d0307e5e8c4f74c87a3096ddd0706eff'
+  url 'https://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.4.zip'
+  sha256 'a7cc1e5315a14ea38db1b2b9ce0792e35174161141a6a3e2ef49b7b2788c258c'
 
   option 'invokedynamic', "Install the InvokeDynamic version of Groovy (only works with Java 1.7+)"
 
