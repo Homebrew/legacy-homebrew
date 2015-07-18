6d833b2407410b806cb632167f73f30fb1e239f2
Baptiste Fontaine <batifon@yahoo.fr>
2015-07-18 09:47:01 +0200

    opusfile: test added
    Closes #41850.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/opusfile.rb
+++ Library/Formula/opusfile.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Opusfile < Formula
   desc "API for decoding and seeking in .opus files"
-  homepage 'http://www.opus-codec.org/'
-  url 'http://downloads.xiph.org/releases/opus/opusfile-0.6.tar.gz'
-  sha1 '2422e3c7bf6105a832226850b19053ec5ac41293'
+  homepage "https://www.opus-codec.org/"
+  url "http://downloads.xiph.org/releases/opus/opusfile-0.6.tar.gz"
+  sha256 "2428717b356e139f18ed2fdb5ad990b5654a238907a0058200b39c46a7d03ea6"
 
   head do
     url "https://git.xiph.org/opusfile.git"
@@ -13,15 +11,45 @@ class Opusfile < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'opus'
-  depends_on 'libogg'
+  depends_on "openssl"
+  depends_on "pkg-config" => :build
+  depends_on "opus"
+  depends_on "libogg"
+
+  resource "music_48kbps.opus" do
+    url "https://www.opus-codec.org/examples/samples/music_48kbps.opus"
+    sha256 "64571f56bb973c078ec784472944aff0b88ba0c88456c95ff3eb86f5e0c1357d"
+  end
 
   def install
     system "./autogen.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <opus/opusfile.h>
+      #include <stdlib.h>
+      int main(int argc, const char **argv) {
+        int ret;
+        OggOpusFile *of;
+
+        of = op_open_file(argv[1], &ret);
+        if (of == NULL) {
+          fprintf(stderr, "Failed to open file '%s': %i\\n", argv[1], ret);
+          return EXIT_FAILURE;
+        }
+        op_free(of);
+        return EXIT_SUCCESS;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{Formula["opus"].include}/opus",
+                             "-L#{lib}",
+                             "-lopusfile",
+                             "-o", "test"
+    resource("music_48kbps.opus").stage testpath
+    system "./test", "music_48kbps.opus"
+  end
 end
