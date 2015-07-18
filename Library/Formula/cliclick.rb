2d16af263bfe82dbc929aeb4acae0ef00bae2732
Carsten Blüm <carsten@bluem.net>
2015-07-18 21:54:29 +0200

    cliclick 3.1
    Closes #41887.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/cliclick.rb
+++ Library/Formula/cliclick.rb
@@ -3,8 +3,8 @@ require "formula"
 class Cliclick < Formula
   desc "Tool for automating emulated mouse clicks"
   homepage "http://www.bluem.net/jump/cliclick/"
-  url "https://github.com/BlueM/cliclick/archive/3.0.3.tar.gz"
-  sha1 "7d2f21065c1d77f46292f72f7aa50c91abb48a53"
+  url "https://github.com/BlueM/cliclick/archive/3.1.tar.gz"
+  sha256 "d54273403ea786facb56fa85e8025f8fbf6bd1819ecd4b24625fa110a4ca3bec"
 
   bottle do
     cellar :any
