67557f8fb61fbfa0bfa17ac1b82cfd4f6e50004f
Viktor Szakats <vszakats@users.noreply.github.com>
2015-07-19 11:45:55 +0200

    cliclick: use https homepage
    Closes #41891.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/cliclick.rb
+++ Library/Formula/cliclick.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Cliclick < Formula
   desc "Tool for automating emulated mouse clicks"
-  homepage "http://www.bluem.net/jump/cliclick/"
+  homepage "https://www.bluem.net/jump/cliclick/"
   url "https://github.com/BlueM/cliclick/archive/3.1.tar.gz"
   sha256 "d54273403ea786facb56fa85e8025f8fbf6bd1819ecd4b24625fa110a4ca3bec"
 
