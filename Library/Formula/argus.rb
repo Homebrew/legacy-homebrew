56b26f5e74af4af5db9c1ef54659d1db4e0d3596
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-19 16:29:22 +0200

    argus 3.0.8.1
    Closes #41899.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/argus.rb
+++ Library/Formula/argus.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Argus < Formula
   desc "Audit Record Generation and Utilization System server"
   homepage "http://qosient.com/argus/"
-  url "http://qosient.com/argus/src/argus-3.0.8.tar.gz"
-  sha1 "fe9833c7f8ea4cdf7054b37024e5d007613f9571"
+  url "http://qosient.com/argus/src/argus-3.0.8.1.tar.gz"
+  sha256 "1fb921104c8bd843fb9f5a1c32b57b20bfe8cd8a103b3f1d9bb686b9e6c490a4"
 
   bottle do
     cellar :any
