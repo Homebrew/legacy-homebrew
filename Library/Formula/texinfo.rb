28fc702f82a3add473114faf973fc9420a327445
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-19 16:32:37 +0100

    texinfo: use secure mirror
    Closes #41905.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/texinfo.rb
+++ Library/Formula/texinfo.rb
@@ -2,8 +2,8 @@ class Texinfo < Formula
   desc "Official documentation format of the GNU project"
   homepage "https://www.gnu.org/software/texinfo/"
   url "http://ftpmirror.gnu.org/texinfo/texinfo-5.2.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/texinfo/texinfo-5.2.tar.gz"
-  sha1 "dc54edfbb623d46fb400576b3da181f987e63516"
+  mirror "https://ftp.gnu.org/gnu/texinfo/texinfo-5.2.tar.gz"
+  sha256 "6b8ca30e9b6f093b54fe04439e5545e564c63698a806a48065c0bba16994cf74"
 
   bottle do
     sha1 "988fc8c195a43ad8b9dea1da2827fb24c794c200" => :yosemite
@@ -21,16 +21,6 @@ class Texinfo < Formula
                           "--disable-install-warnings",
                           "--prefix=#{prefix}"
     system "make", "install"
-
-    # The install warns about needing to install texinfo.tex and some other support files.
-    # The texinfo.tex in tex-live 2008 is identical to texinfo's version, so we can ignore this.
-
-    # However, it complains about installing epsf.tex in TEXMF/tex/generic/dvips, so let's do that...
-    # This somewhat breaks the homebrew philosophy, I am sorry.
-    # Also, we don't depend on tex-live, but this directory only exists if it is installed.
-    if File.exist? "#{HOMEBREW_PREFIX}/share/texmf-dist/" then
-      cp "doc/epsf.tex", "#{HOMEBREW_PREFIX}/share/texmf-dist/tex/generic/dvips/"
-    end
   end
 
   test do
