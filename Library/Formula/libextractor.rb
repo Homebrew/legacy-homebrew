62c227caad66084568fd709fc2a8e3dd2533813e
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-19 02:03:24 +0100

    libextractor: improve test

--- Library/Formula/libextractor.rb
+++ Library/Formula/libextractor.rb
@@ -1,11 +1,9 @@
-require "formula"
-
 class Libextractor < Formula
   desc "Library to extract meta data from files"
   homepage "https://www.gnu.org/software/libextractor/"
   url "http://ftpmirror.gnu.org/libextractor/libextractor-1.3.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/libextractor/libextractor-1.3.tar.gz"
-  sha1 "613d0b80e83c79c3e05e073bcda0d0d0bd1f3336"
+  mirror "https://ftp.gnu.org/gnu/libextractor/libextractor-1.3.tar.gz"
+  sha256 "868ad64c9a056d6b923d451d746935bffb1ddf5d89c3eb4f67d786001a3f7b7f"
 
   bottle do
     cellar :any
@@ -21,13 +19,15 @@ class Libextractor < Formula
 
   def install
     ENV.deparallelize
+
     system "./configure", "--disable-silent-rules",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/extract", "-v"
+    fixture = test_fixtures("test.png")
+    assert_match /Keywords for file/, shell_output("#{bin}/extract #{fixture}")
   end
 end
