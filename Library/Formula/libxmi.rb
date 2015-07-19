0f47b0d2e9de3fb897eb5548f85ded276d6d475e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-19 03:47:57 +0100

    libxmi: update 1.2 bottle.

--- Library/Formula/libxmi.rb
+++ Library/Formula/libxmi.rb
@@ -7,10 +7,10 @@ class Libxmi < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha1 "883a10b3f456d9ac39ca1442253a90167157b50c" => :yosemite
-    sha1 "b7d2b8c174df9ac8b9e77aada7efe2e94384a913" => :mavericks
-    sha1 "d21c5eb24647aa5d18f589ab4327c215df6383f6" => :mountain_lion
+    revision 2
+    sha256 "b8a406a6559eb59890d519e41c824f75f1b37027e6dda108f3648d85480ba5f8" => :yosemite
+    sha256 "fe1bd51baf04d248d233d92ed8c2343d49b03e09427dd6774a86cabfc21372e9" => :mavericks
+    sha256 "1bfaff32eb89a52d7a3b3ef98e2e7070df837d904590c0c54e31df3e61e01172" => :mountain_lion
   end
 
   def install
