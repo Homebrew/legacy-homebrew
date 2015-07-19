5e5c1016e92302f486408dc2b597f05350950018
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-19 16:28:52 +0100

    pth: use secure mirror

--- Library/Formula/pth.rb
+++ Library/Formula/pth.rb
@@ -2,8 +2,8 @@ class Pth < Formula
   desc "GNU Portable THreads"
   homepage "https://www.gnu.org/software/pth/"
   url "http://ftpmirror.gnu.org/pth/pth-2.0.7.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/pth/pth-2.0.7.tar.gz"
-  sha1 "9a71915c89ff2414de69fe104ae1016d513afeee"
+  mirror "https://ftp.gnu.org/gnu/pth/pth-2.0.7.tar.gz"
+  sha256 "72353660c5a2caafd601b20e12e75d865fd88f6cf1a088b306a3963f0bc77232"
 
   bottle do
     cellar :any
@@ -15,6 +15,7 @@ class Pth < Formula
 
   def install
     ENV.deparallelize
+
     # Note: shared library will not be build with --disable-debug, so don't add that flag
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make"
