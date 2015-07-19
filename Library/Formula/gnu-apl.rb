698fde5176d476a62827cd51c0566241561e867e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-19 06:30:05 +0100

    gnu-apl: update 1.4 bottle.

--- Library/Formula/gnu-apl.rb
+++ Library/Formula/gnu-apl.rb
@@ -6,7 +6,9 @@ class GnuApl < Formula
   sha256 "69da31db180e6ae17116122758c266e19f62e256f04e3c4959f6f2b224a1893a"
 
   bottle do
-    sha1 "e067c7741c9f7e174f0a5f06d99e59fe5b6e930b" => :mavericks
+    revision 1
+    sha256 "0a061c8ca10b237d15eddd13acf0bc1007b1b18888eda30cd43beeaa77602979" => :yosemite
+    sha256 "350db093b3749d355147717f3e3209378c32aae2e508e2b54534f002be5fdedd" => :mavericks
   end
 
   # GNU Readline is required; libedit won't work.
