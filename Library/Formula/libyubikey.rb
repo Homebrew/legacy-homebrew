fad732a9de6ce3331f1a16e83739303493ea16de
Evan Kinney <evan@evan.cat>
2015-07-20 12:26:21 -0700

    libyubikey 1.13
    Closes #41941.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libyubikey.rb
+++ Library/Formula/libyubikey.rb
@@ -1,8 +1,8 @@
 class Libyubikey < Formula
   desc "C library for manipulating Yubico one-time passwords"
   homepage "https://yubico.github.io/yubico-c/"
-  url "https://developers.yubico.com/yubico-c/Releases/libyubikey-1.12.tar.gz"
-  sha256 "b95387f791dad4d2544282299800f016736c363838e1e732f199bf90eba717b8"
+  url "https://developers.yubico.com/yubico-c/Releases/libyubikey-1.13.tar.gz"
+  sha256 "04edd0eb09cb665a05d808c58e1985f25bb7c5254d2849f36a0658ffc51c3401"
 
   bottle do
     cellar :any
