84da15c6b5ef623891a56d3021c931efb8476f12
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-07-20 19:14:04 +0900

    embulk 0.6.17
    Closes #41931.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/embulk.rb
+++ Library/Formula/embulk.rb
@@ -1,15 +1,15 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "http://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.12.jar"
-  sha256 "440be2dc34e6f3346fc808ece4c7ab713dbcea90c992149d56f407cf264c2598"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.17.jar"
+  sha256 "68c884d84663ecc821ed327b13b90ad826ba8d5e998f422cfce6cc9745a086c6"
 
   depends_on :java
 
   skip_clean "libexec"
 
   def install
-    (libexec/"bin").install "embulk-0.6.12.jar" => "embulk"
+    (libexec/"bin").install "embulk-0.6.17.jar" => "embulk"
     chmod 0755, libexec/"bin/embulk"
     bin.write_exec_script libexec/"bin/embulk"
   end
