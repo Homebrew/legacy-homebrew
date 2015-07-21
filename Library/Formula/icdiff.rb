3da5fcac0960d6edb4e377cc0d0ad9b2577a1f82
Decors <decors1215@gmail.com>
2015-07-21 19:16:07 +0900

    icdiff 1.7.3
    Closes #41964.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/icdiff.rb
+++ Library/Formula/icdiff.rb
@@ -1,8 +1,8 @@
 class Icdiff < Formula
   desc "Improved colored diff"
   homepage "https://github.com/jeffkaufman/icdiff"
-  url "https://github.com/jeffkaufman/icdiff/archive/release-1.7.1.tar.gz"
-  sha1 "8af0bb43cb15096fcea1a1ab9a7130d4ca35a058"
+  url "https://github.com/jeffkaufman/icdiff/archive/release-1.7.3.tar.gz"
+  sha256 "5161265f72a7c9c1d2d7b0780a381743ef3d3127944a96786422802a6bc14ca5"
 
   bottle do
     cellar :any
