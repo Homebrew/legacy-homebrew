73d5900ba3269a0c9b3d4a694232ff0b0193c1b0
Yeah <bYeahq@gmail.com>
2015-07-21 18:55:20 +0800

    acpica 20150717
    Closes #41967.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/acpica.rb
+++ Library/Formula/acpica.rb
@@ -2,8 +2,8 @@ class Acpica < Formula
   desc "OS-independent implementation of the ACPI specification"
   homepage "https://www.acpica.org/"
   head "https://github.com/acpica/acpica.git"
-  url "https://acpica.org/sites/acpica/files/acpica-unix2-20150619.tar.gz"
-  sha256 "7884f414a8f3bc58c21f3e9bc4f0094771fa665be0b24140b54bd7477764f215"
+  url "https://acpica.org/sites/acpica/files/acpica-unix2-20150717.tar.gz"
+  sha256 "dd60f846ad8393d89d2cbadf362c6547c5e53405f5ee51097c90db3636f79e0a"
 
   bottle do
     cellar :any
