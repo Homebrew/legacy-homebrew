8e873ed31bfd87600b6df8f4bc60d30f3c6e9470
Xu Cheng <xucheng@me.com>
2015-07-21 21:07:51 +0800

    coverage: stop recording compat codes
    Closes #41973.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Homebrew/test/testing_env.rb
+++ Library/Homebrew/test/testing_env.rb
@@ -11,9 +11,10 @@ if ENV["HOMEBREW_TESTS_COVERAGE"]
     coverage_dir File.expand_path("#{tests_path}/coverage")
     root File.expand_path("#{tests_path}/..")
 
-    add_filter "Homebrew/test"
-    add_filter "vendor/bundle"
-    add_filter "Homebrew/vendor"
+    add_filter "Homebrew/test/"
+    add_filter "vendor/bundle/"
+    add_filter "Homebrew/vendor/"
+    add_filter "Homebrew/compat/"
   end
 end
 
