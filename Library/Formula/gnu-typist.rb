91802dd2c396e56f78a108a2252935bdecdca431
Guo Xiao <guoxiao08@gmail.com>
2015-07-21 14:14:42 +0800

    Use exec instead of system

--- Library/Formula/gnu-typist.rb
+++ Library/Formula/gnu-typist.rb
@@ -29,7 +29,7 @@ class GnuTypist < Formula
 
   test do
     session = fork do
-      system bin/"gtypist", "-t", "-q", "-l", "DEMO_0", share/"gtypist/demo.typ"
+      exec bin/"gtypist", "-t", "-q", "-l", "DEMO_0", share/"gtypist/demo.typ"
     end
     sleep 2
     Process.kill("TERM", session)
