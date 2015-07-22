fdcda6d695adf4ee5055a7317070f3f6b01fa4af
Larry Gilbert <larry@L2G.to>
2015-07-22 12:41:52 -0700

    mr 1.20150503
    * New upstream version
    * Add test based on small Git clone
    * Other changes for `brew audit --strict`

--- Library/Formula/mr.rb
+++ Library/Formula/mr.rb
@@ -1,14 +1,23 @@
-require 'formula'
-
 class Mr < Formula
   desc "Multiple Repository management tool"
-  homepage 'http://myrepos.branchable.com/'
-  url 'git://myrepos.branchable.com/', :tag => '1.20141024'
+  homepage "http://myrepos.branchable.com/"
+  url "git://myrepos.branchable.com/", :tag => "1.20150503", :revision => "073a3b517580bcd2236bf9d36beff18c1222e8aa"
+
+  resource("test-repo") do
+    url "https://github.com/Homebrew/homebrew-command-not-found.git"
+  end
 
   def install
     system "make"
-    bin.install 'mr', 'webcheckout'
-    man1.install gzip('mr.1', 'webcheckout.1')
-    (share/'mr').install Dir['lib/*']
+    bin.install "mr", "webcheckout"
+    man1.install gzip("mr.1", "webcheckout.1")
+    (share/"mr").install Dir["lib/*"]
+  end
+
+  test do
+    resource("test-repo").stage do
+      system bin/"mr", "register"
+      assert_match(/^mr status: #{Dir.pwd}$/, shell_output("#{bin}/mr status"))
+    end
   end
 end
