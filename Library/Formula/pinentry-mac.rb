f40e3dd0e5454136635f2af9737b13292eea90a0
chdiza <chdiza@gmail.com>
2015-07-22 12:06:48 -0400

    pinentry-mac 0.9.4
    Closes #42021.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/pinentry-mac.rb
+++ Library/Formula/pinentry-mac.rb
@@ -4,14 +4,8 @@ class PinentryMac < Formula
   head "https://github.com/GPGTools/pinentry-mac.git"
 
   stable do
-    url "https://github.com/GPGTools/pinentry-mac/archive/v0.8.1.tar.gz"
-    sha256 "79aaa11fa8076ff335b3a1f41c230ef7c8435a757705e6484199f562f26b490f"
-
-    # Removes the remote code execution previously run automatically.
-    patch do
-      url "https://github.com/GPGTools/pinentry-mac/commit/89dd4789818894.diff"
-      sha1 "ab4db3264e1eb5ec9f9e1a31ad28b43d869c0f82"
-    end
+    url "https://github.com/GPGTools/pinentry-mac/archive/v0.9.4.tar.gz"
+    sha256 "037ebb010377d3a3879ae2a832cefc4513f5c397d7d887d7b86b4e5d9a628271"
   end
 
   bottle do
@@ -22,18 +16,9 @@ class PinentryMac < Formula
     sha1 "49db48b86524242985fba13304a2c8116c087e6b" => :mountain_lion
   end
 
-  # Manual cloning of this repo is now the upstream default:
-  # https://github.com/GPGTools/pinentry-mac/commit/89dd47898188
-  resource "core_clone" do
-    url "https://github.com/GPGTools/GPGTools_Core.git",
-        :revision => "f1e458ab2daeb104328ea4ec2aa7c71e8fca758d"
-  end
-
   depends_on :xcode => :build
 
   def install
-    (buildpath/"Dependencies/GPGTools_Core").install resource("core_clone")
-
     system "make"
     prefix.install "build/Release/pinentry-mac.app"
     bin.write_exec_script "#{prefix}/pinentry-mac.app/Contents/MacOS/pinentry-mac"
