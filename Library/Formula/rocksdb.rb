daedc58b73f0194c5e1cd78a2345efa637edd4d4
Dave Cottlehuber <dch@skunkwerks.at>
2015-07-22 14:45:50 +0200

    rocksdb 3.12

--- Library/Formula/rocksdb.rb
+++ Library/Formula/rocksdb.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Rocksdb < Formula
   desc "Persistent key-value store for fast storage environments"
   homepage "http://rocksdb.org"
-  url "https://github.com/facebook/rocksdb/archive/rocksdb-3.10.2.tar.gz"
-  sha256 "5ace408b12e5e5c836c9ba0b1bd57662784d15820cd02b203459b3ac2e01fde7"
+  url "https://github.com/facebook/rocksdb/archive/rocksdb-3.12.tar.gz"
+  sha256 "2f50f134572d97f0f00f7bb3582ce22ad82e3f0281f3736bc87237fc5a1c02e6"
 
   bottle do
     cellar :any
@@ -19,6 +17,9 @@ class Rocksdb < Formula
   needs :cxx11
   depends_on "snappy"
   depends_on "lz4"
+
+  # fix the weird library naming scheme in rocksdb Makefile
+  patch :DATA
 
   def install
     ENV.cxx11
@@ -53,3 +54,36 @@ class Rocksdb < Formula
     system "./db_test"
   end
 end
+__END__
+From eb8e3b4c7a5f4d3054f4a79ad7559928e841e57f Mon Sep 17 00:00:00 2001
+From: Dave Cottlehuber <dch@skunkwerks.at>
+Date: Thu, 23 Jul 2015 15:08:24 +0200
+Subject: [PATCH] Fix shared library names on OSX
+
+- OSX format is libname.major.minor.patch.dylib
+- closes #666
+---
+ Makefile | 7 +++++++
+ 1 file changed, 7 insertions(+)
+
+diff --git a/Makefile b/Makefile
+index b09bdd8..4c1003b 100644
+--- a/Makefile
++++ b/Makefile
+@@ -345,9 +345,16 @@ SHARED_MAJOR = $(ROCKSDB_MAJOR)
+ SHARED_MINOR = $(ROCKSDB_MINOR)
+ SHARED_PATCH = $(ROCKSDB_PATCH)
+ SHARED1 = ${LIBNAME}.$(PLATFORM_SHARED_EXT)
++ifeq ($(PLATFORM), OS_MACOSX)
++SHARED_OSX = $(LIBNAME).$(SHARED_MAJOR)
++SHARED2 = $(SHARED_OSX).$(PLATFORM_SHARED_EXT)
++SHARED3 = $(SHARED_OSX).$(SHARED_MINOR).$(PLATFORM_SHARED_EXT)
++SHARED4 = $(SHARED_OSX).$(SHARED_MINOR).$(SHARED_PATCH).$(PLATFORM_SHARED_EXT)
++else
+ SHARED2 = $(SHARED1).$(SHARED_MAJOR)
+ SHARED3 = $(SHARED1).$(SHARED_MAJOR).$(SHARED_MINOR)
+ SHARED4 = $(SHARED1).$(SHARED_MAJOR).$(SHARED_MINOR).$(SHARED_PATCH)
++endif
+ SHARED = $(SHARED1) $(SHARED2) $(SHARED3) $(SHARED4)
+ $(SHARED1): $(SHARED4)
+ 	ln -fs $(SHARED4) $(SHARED1)
