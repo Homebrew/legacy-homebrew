2e7e982635c29ef1bf1b294a9fe7284dd1d8c95b
Dave Cottlehuber <dch@skunkwerks.at>
2015-07-22 15:34:01 +0200

    lz4 add HEAD url

--- Library/Formula/lz4.rb
+++ Library/Formula/lz4.rb
@@ -4,6 +4,7 @@ class Lz4 < Formula
   url "https://github.com/Cyan4973/lz4/archive/r131.tar.gz"
   sha256 "9d4d00614d6b9dec3114b33d1224b6262b99ace24434c53487a0c8fd0b18cfed"
   version "r131"
+  head "https://github.com/Cyan4973/lz4.git"
 
   bottle do
     cellar :any
