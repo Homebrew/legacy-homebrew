ecbdde1c18b0e2013f07cd21fe063292e83bae90
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-22 04:46:26 +0100

    tsung: style fixes

--- Library/Formula/tsung.rb
+++ Library/Formula/tsung.rb
@@ -1,12 +1,10 @@
-require 'formula'
-
 class Tsung < Formula
   desc "Load testing for HTTP, PostgreSQL, Jabber, and others"
-  homepage 'http://tsung.erlang-projects.org/'
-  url 'http://tsung.erlang-projects.org/dist/tsung-1.6.0.tar.gz'
-  sha256 '56846c3a90fd7037d9a76330cb8f3052238344491e2fe6ef1ebdb0b620eb3d84'
+  homepage "http://tsung.erlang-projects.org/"
+  url "http://tsung.erlang-projects.org/dist/tsung-1.6.0.tar.gz"
+  sha256 "56846c3a90fd7037d9a76330cb8f3052238344491e2fe6ef1ebdb0b620eb3d84"
 
-  head 'https://github.com/processone/tsung.git'
+  head "https://github.com/processone/tsung.git"
 
   bottle do
     sha256 "a4d0f870f04ae0683f4647123c5c3e94e39fd0bcf97edaa98dd2a4a4aa4dca33" => :yosemite
@@ -14,13 +12,17 @@ class Tsung < Formula
     sha256 "1fe31187fd7662216c1a879bb62302a638834220e84becf8a1ab182df45c3d14" => :mountain_lion
   end
 
-  depends_on 'erlang'
-  depends_on 'gnuplot'
+  depends_on "erlang"
+  depends_on "gnuplot"
 
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
-    ENV.j1
-    system "make install"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  test do
+    system bin/"tsung", "status"
   end
 end
