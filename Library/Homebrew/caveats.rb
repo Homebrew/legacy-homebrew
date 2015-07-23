3ffa444831c8253d0cf80a1432a229705f2d3d68
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-23 15:17:31 +0100

    caveats: use two spaces instead of 4

--- Library/Homebrew/caveats.rb
+++ Library/Homebrew/caveats.rb
@@ -155,43 +155,43 @@ class Caveats
       if !plist_path.file? || !plist_path.symlink?
         if f.plist_startup
           s << "To have launchd start #{f.full_name} at startup:"
-          s << "    sudo mkdir -p #{destination}" unless destination_path.directory?
-          s << "    sudo cp -fv #{f.opt_prefix}/*.plist #{destination}"
-          s << "    sudo chown root #{plist_link}"
+          s << "  sudo mkdir -p #{destination}" unless destination_path.directory?
+          s << "  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}"
+          s << "  sudo chown root #{plist_link}"
         else
           s << "To have launchd start #{f.full_name} at login:"
-          s << "    mkdir -p #{destination}" unless destination_path.directory?
-          s << "    ln -sfv #{f.opt_prefix}/*.plist #{destination}"
+          s << "  mkdir -p #{destination}" unless destination_path.directory?
+          s << "  ln -sfv #{f.opt_prefix}/*.plist #{destination}"
         end
         s << "Then to load #{f.full_name} now:"
         if f.plist_startup
-          s << "    sudo launchctl load #{plist_link}"
+          s << "  sudo launchctl load #{plist_link}"
         else
-          s << "    launchctl load #{plist_link}"
+          s << "  launchctl load #{plist_link}"
         end
       # For startup plists, we cannot tell whether it's running on launchd,
       # as it requires for `sudo launchctl list` to get real result.
       elsif f.plist_startup
-          s << "To reload #{f.full_name} after an upgrade:"
-          s << "    sudo launchctl unload #{plist_link}"
-          s << "    sudo cp -fv #{f.opt_prefix}/*.plist #{destination}"
-          s << "    sudo chown root #{plist_link}"
-          s << "    sudo launchctl load #{plist_link}"
+        s << "To reload #{f.full_name} after an upgrade:"
+        s << "  sudo launchctl unload #{plist_link}"
+        s << "  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}"
+        s << "  sudo chown root #{plist_link}"
+        s << "  sudo launchctl load #{plist_link}"
       elsif Kernel.system "/bin/launchctl list #{plist_domain} &>/dev/null"
-          s << "To reload #{f.full_name} after an upgrade:"
-          s << "    launchctl unload #{plist_link}"
-          s << "    launchctl load #{plist_link}"
+        s << "To reload #{f.full_name} after an upgrade:"
+        s << "  launchctl unload #{plist_link}"
+        s << "  launchctl load #{plist_link}"
       else
-          s << "To load #{f.full_name}:"
-          s << "    launchctl load #{plist_link}"
+        s << "To load #{f.full_name}:"
+        s << "  launchctl load #{plist_link}"
       end
 
       if f.plist_manual
         s << "Or, if you don't want/need launchctl, you can just run:"
-        s << "    #{f.plist_manual}"
+        s << "  #{f.plist_manual}"
       end
 
-      s << "" << "WARNING: launchctl will fail when run under tmux." if ENV['TMUX']
+      s << "" << "WARNING: launchctl will fail when run under tmux." if ENV["TMUX"]
     end
     s.join("\n") unless s.empty?
   end
