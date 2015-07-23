2d40291ea89760b19611617168068d948bb7fb5f
Lawrence D'Anna <larry@elder-gods.org>
2015-07-23 02:30:57 -0700

    rtags: switch to using a git tag for stable
    Signed-off-by: Lawrence D'Anna <larry@elder-gods.org>

--- Library/Formula/rtags.rb
+++ Library/Formula/rtags.rb
@@ -2,12 +2,9 @@ class Rtags < Formula
   homepage "https://github.com/Andersbakken/rtags"
 
   stable do
-    url "https://github.com/Andersbakken/rtags/archive/v2.0.tar.gz"
-    sha256 "36733945ea34517903a0e5b800b06a41687ee25d3ab360072568523e5d610d6f"
-
-    resource "rct" do
-      url "https://github.com/Andersbakken/rct.git", :revision => "10700c615179f07d4832d459e6453eed736cfaef"
-    end
+    url "https://github.com/Andersbakken/rtags.git",
+        :tag => "v2.0",
+        :revision => "ba85598841648490e64246be802fc2dcdd45bc3c"
   end
 
   head "https://github.com/Andersbakken/rtags.git"
@@ -17,10 +14,6 @@ class Rtags < Formula
   depends_on "openssl"
 
   def install
-    unless build.head?
-      (buildpath/"src/rct").install resource("rct")
-    end
-
     # we use brew's LLVM instead of the macosx llvm because the macosx one
     # doesn't include libclang.
     ENV.prepend_path "PATH", "#{opt_libexec}/llvm/bin"
