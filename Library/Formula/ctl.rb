7712258291235dfa27f8a3d842829ac3261143c0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-23 04:25:25 +0100

    ctl: update 1.5.2_1 bottle.

--- Library/Formula/ctl.rb
+++ Library/Formula/ctl.rb
@@ -6,9 +6,9 @@ class Ctl < Formula
   revision 1
 
   bottle do
-    sha256 "860d6ce8218cf26830ac61327c45d862df1dd33b58996278d37341b34c8062e6" => :yosemite
-    sha256 "ed30b61d129d781b395fde6b0d925ad01d6bd5f64220c54c04d7971b20ae9663" => :mavericks
-    sha256 "ff805ef59d8958823c8eab7b0dab86eddc923d37493874ac4b6ec2908f1b348b" => :mountain_lion
+    sha256 "d04f2f46eaa9b3fa1d6e67e0493e8b206f873c6211874962b8ef06865bbf5c71" => :yosemite
+    sha256 "59a4e155666ed1854a2b38204d750ddc0af75ebd988367b1629be4a9f00a6c1f" => :mavericks
+    sha256 "84aaaa696f7ea8837d13a3a1e58d826978be781c01dd5a77fa20197f45b7d25b" => :mountain_lion
   end
 
   depends_on "cmake" => :build
