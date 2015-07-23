9114cabe7d474884787d13407c197554731d71b5
Dmitry Moskalchuk <dm@crystax.net>
2015-07-23 20:54:28 +0300

    crystax-ndk - use sha256 instead of sha1
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- Library/Formula/crystax-ndk.rb
+++ Library/Formula/crystax-ndk.rb
@@ -6,10 +6,10 @@ class CrystaxNdk < Formula
 
   if MacOS.prefer_64_bit?
     url "https://www.crystax.net/download/crystax-ndk-#{version}-darwin-x86_64.7z"
-    sha1 "16b2255ad0fcc9c677dbb377899b972dc37a8079"
+    sha256 "1503eec2b883ffbe8f24bcfd2f3d47579ff1c9ce84be3612d8cfe5339aa0df40"
   else
     url "https://www.crystax.net/download/crystax-ndk-#{version}-darwin-x86.7z"
-    sha1 "9afe0c2e250dbff30caf709dcd75d40a9bdd7852"
+    sha256 "a46e5741d42406c39e85c79bfac895374b1831c20e16cfa5ea57d705c52dc1f1"
   end
 
   depends_on "android-sdk" => :recommended
