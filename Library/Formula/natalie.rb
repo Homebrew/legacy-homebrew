f115ea822af846b51e2d2626745dfb667eb1fb0c
Loïs Di Qual <lois.diqual@gmail.com>
2015-07-23 02:08:44 -0700

    Better tokenization

--- Library/Formula/natalie.rb
+++ Library/Formula/natalie.rb
@@ -8,8 +8,8 @@ class Natalie < Formula
   depends_on :xcode => "6.3"
 
   def install
-    system *%W[mv natalie.swift natalie-script.swift]
-    system *%W[xcrun -sdk macosx swiftc -O natalie-script.swift -o natalie.swift]
+    system "mv", "natalie.swift", "natalie-script.swift"
+    system "xcrun", "-sdk", "macosx", "swiftc", "-O", "natalie-script.swift", "-o", "natalie.swift"
     bin.install "natalie.swift"
     share.install "NatalieExample"
   end
