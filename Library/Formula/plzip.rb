ee801cd4c72c0a2cd2c7e4e16e0d33b81ed0419f
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-24 08:46:23 +0200

    plzip 1.4
    Closes #42083.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/plzip.rb
+++ Library/Formula/plzip.rb
@@ -1,20 +1,14 @@
 class Plzip < Formula
   desc "Data compressor"
   homepage "http://www.nongnu.org/lzip/plzip.html"
-  url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.3.tar.gz"
-  sha1 "e339c06093d8e7905390cc7c39f28f6198a66471"
+  url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.4.tar.gz"
+  sha256 "2a152ee429495cb96c22a51b618d1d19882db3e24aff79329d9c755a2a2f67bb"
 
   bottle do
     cellar :any
     sha1 "c555c0fff979bfdbd28e91b992395bc1f78c8ad1" => :yosemite
     sha1 "991dad931e4083afa7a5c50ebbf9bb8459bb266e" => :mavericks
     sha1 "3371a5b356a73f401951dd621c2d5782be2b275b" => :mountain_lion
-  end
-
-  devel do
-    url "http://download.savannah.gnu.org/releases/lzip/plzip/plzip-1.4-pre1.tar.gz"
-    sha1 "817b9d1635be6db35907733f7eedcd2b7642ccdd"
-    version "1.4-pre1"
   end
 
   depends_on "lzlib"
