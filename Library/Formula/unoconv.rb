802d22928051e0d7bee24f4332d39b271c263372
Ryan Desmond <rddesmond@missionfocus.com>
2015-07-24 10:03:48 -0400

    unoconv 0.7
    https://github.com/dagwieers/unoconv/blob/0.7/CHANGELOG.md
    
    Closes #42094.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/unoconv.rb
+++ Library/Formula/unoconv.rb
@@ -3,8 +3,8 @@ require "formula"
 class Unoconv < Formula
   desc "Convert between any document format supported by OpenOffice"
   homepage "http://dag.wiee.rs/home-made/unoconv/"
-  url "http://dag.wieers.com/home-made/unoconv/unoconv-0.6.tar.gz"
-  sha1 "d6c3574639b9ceedcb866b84f18ba7158f25db48"
+  url "http://dag.wieers.com/home-made/unoconv/unoconv-0.7.tar.gz"
+  sha256 "56abbec55632b19dcaff7d506ad6e2fd86f53afff412e622cc1e162afb1263fa"
   head "https://github.com/dagwieers/unoconv.git"
 
   bottle do
@@ -21,7 +21,7 @@ class Unoconv < Formula
   end
 
   def caveats; <<-EOS.undent
-    In order to use unoconv LibreOffice 3.6.0.1 or later must be installed.
+    In order to use unoconv, a copy of LibreOffice between versions 3.6.0.1 - 4.3.x must be installed.
     EOS
   end
 end
