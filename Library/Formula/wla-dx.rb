7bf0de07b88d2dddc2873235b52947d713c30986
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-24 16:08:03 +0100

    wla-dx: fix style nits

--- Library/Formula/wla-dx.rb
+++ Library/Formula/wla-dx.rb
@@ -1,11 +1,9 @@
-require 'formula'
-
 class WlaDx < Formula
   desc "Yet another crossassembler package"
-  homepage 'https://github.com/vhelin/wla-dx'
-  url 'https://github.com/vhelin/wla-dx/archive/v9.6.tar.gz'
-  sha256 'd368f4fb7d8a394f65730682dba6fddfe75b3c6119756799cdb3cd5e1ae78e0d'
-  head 'https://github.com/vhelin/wla-dx.git'
+  homepage "https://github.com/vhelin/wla-dx"
+  url "https://github.com/vhelin/wla-dx/archive/v9.6.tar.gz"
+  sha256 "d368f4fb7d8a394f65730682dba6fddfe75b3c6119756799cdb3cd5e1ae78e0d"
+  head "https://github.com/vhelin/wla-dx.git"
 
   bottle do
     cellar :any
@@ -15,11 +13,12 @@ class WlaDx < Formula
   end
 
   def install
-    %w{CFLAGS CXXFLAGS CPPFLAGS}.each { |e| ENV.delete(e) }
-    ENV.append_to_cflags '-c -O3 -ansi -pedantic -Wall'
+    %w[CFLAGS CXXFLAGS CPPFLAGS].each { |e| ENV.delete(e) }
+    ENV.append_to_cflags "-c -O3 -ansi -pedantic -Wall"
+
     chmod 0755, "unix.sh"
     system "./unix.sh", ENV.make_jobs
-    bin.install Dir['./binaries/*']
+    bin.install Dir["./binaries/*"]
   end
 
   test do
