e385049f85c204ef65b25a4ca414f4529f49917f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-24 13:06:01 +0100

    wla-dx: add 9.6 bottle.

--- Library/Formula/wla-dx.rb
+++ Library/Formula/wla-dx.rb
@@ -7,6 +7,13 @@ class WlaDx < Formula
   sha256 'd368f4fb7d8a394f65730682dba6fddfe75b3c6119756799cdb3cd5e1ae78e0d'
   head 'https://github.com/vhelin/wla-dx.git'
 
+  bottle do
+    cellar :any
+    sha256 "4c192ebb6538da0313496f6927a52f7a64782dbeedbf454b0004e203ddb2109e" => :yosemite
+    sha256 "2b8cf1da40e1ce9fedbbf41277ee44955fa99712d46c2fb8d6140c8452b74445" => :mavericks
+    sha256 "a78744385e31896de6b34ae3426c6081ada2f28b997f0c5c15555e10bd03d53e" => :mountain_lion
+  end
+
   def install
     %w{CFLAGS CXXFLAGS CPPFLAGS}.each { |e| ENV.delete(e) }
     ENV.append_to_cflags '-c -O3 -ansi -pedantic -Wall'
