e575a5bf8cf69b182bb45e6399dfa2f398f5c48c
Alex Dunn <dunn.alex@gmail.com>
2015-07-24 21:39:02 -0700

    libicns: add mirror, test
    Re #42109.
    
    Closes #42111.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/libicns.rb
+++ Library/Formula/libicns.rb
@@ -1,10 +1,9 @@
-require 'formula'
-
 class Libicns < Formula
   desc "Library for manipulation of the OS X .icns resource format"
-  homepage 'http://icns.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/icns/libicns-0.8.1.tar.gz'
-  sha256 '335f10782fc79855cf02beac4926c4bf9f800a742445afbbf7729dab384555c2'
+  homepage "http://icns.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/icns/libicns-0.8.1.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/libi/libicns/libicns_0.8.1.orig.tar.gz"
+  sha256 "335f10782fc79855cf02beac4926c4bf9f800a742445afbbf7729dab384555c2"
   revision 1
 
   bottle do
@@ -17,19 +16,36 @@ class Libicns < Formula
 
   option :universal
 
-  depends_on 'jasper'
-  depends_on 'libpng'
+  depends_on "jasper"
+  depends_on "libpng"
 
   def install
     # Fix for libpng 1.5
-    inreplace 'icnsutils/png2icns.c',
-      'png_set_gray_1_2_4_to_8',
-      'png_set_expand_gray_1_2_4_to_8'
+    inreplace "icnsutils/png2icns.c",
+      "png_set_gray_1_2_4_to_8",
+      "png_set_expand_gray_1_2_4_to_8"
 
     ENV.universal_binary if build.universal?
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include "icns.h"
+      int main(void)
+      {
+        int    error = 0;
+        FILE            *inFile = NULL;
+        icns_family_t  *iconFamily = NULL;
+        icns_image_t  iconImage;
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-L#{lib}", "-licns", testpath/"test.c", "-o", "test"
+    system "./test"
   end
 end
