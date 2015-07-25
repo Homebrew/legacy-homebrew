abf7bad9856ea22daa22ff74325176d907c3cef3
Lawrence D'Anna <larry@elder-gods.org>
2015-07-24 19:22:50 -0700

    rtags: style fixes, don't modify PATH
    It looks like modifying PATH is no longer needed.
    
    Signed-off-by: Lawrence D'Anna <larry@elder-gods.org>

--- Library/Formula/rtags.rb
+++ Library/Formula/rtags.rb
@@ -2,31 +2,22 @@ class Rtags < Formula
   desc "ctags-like source code cross-referencer with a clang frontend"
   homepage "https://github.com/Andersbakken/rtags"
 
-  head "https://github.com/Andersbakken/rtags.git"
+  url "https://github.com/Andersbakken/rtags.git",
+      :tag => "v2.0",
+      :revision => "ba85598841648490e64246be802fc2dcdd45bc3c"
 
-  stable do
-    url "https://github.com/Andersbakken/rtags.git",
-        :tag => "v2.0",
-        :revision => "ba85598841648490e64246be802fc2dcdd45bc3c"
-  end
+  head "https://github.com/Andersbakken/rtags.git"
 
   depends_on "cmake" => :build
   depends_on "llvm" => "with-clang"
   depends_on "openssl"
 
   def install
-    # we use brew's LLVM instead of the macosx llvm because the macosx one
-    # doesn't include libclang.
-    ENV.prepend_path "PATH", "#{opt_libexec}/llvm/bin"
-
     # Homebrew llvm libc++.dylib doesn't correctly reexport libc++abi
     ENV.append("LDFLAGS", "-lc++abi")
 
     mkdir "build" do
-      args = std_cmake_args
-      args << ".."
-
-      system "cmake", *args
+      system "cmake", "..", *std_cmake_args
       system "make"
       system "make", "install"
     end
