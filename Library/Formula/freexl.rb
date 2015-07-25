7d80778774e7269113c985cb2bee1f5206921b84
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-25 12:00:42 +0200

    freexl 1.0.2
    Closes #42117.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/freexl.rb
+++ Library/Formula/freexl.rb
@@ -1,12 +1,8 @@
 class Freexl < Formula
   desc "Library to extract data from Excel .xls files"
   homepage "https://www.gaia-gis.it/fossil/freexl/index"
-  url "https://www.gaia-gis.it/gaia-sins/freexl-sources/freexl-1.0.1.tar.gz"
-  sha256 "df0127e1e23e9ac9a189c27880fb71207837e8cba93d21084356491c9934b89b"
-
-  option "without-check", "Skip compile-time make checks."
-
-  depends_on "doxygen" => [:optional, :build]
+  url "https://www.gaia-gis.it/gaia-sins/freexl-sources/freexl-1.0.2.tar.gz"
+  sha256 "b39a4814a0f53f5e09a9192c41e3e51bd658843f770399023a963eb064f6409d"
 
   bottle do
     cellar :any
@@ -14,6 +10,10 @@ class Freexl < Formula
     sha256 "5d007d42d495824c89f50b8eeabd35deaf32abf343573f30d68502bf8979d157" => :mavericks
     sha256 "53345a758e705765c6473327cb91c591ab50858144490973549f89750b31656d" => :mountain_lion
   end
+
+  option "without-check", "Skip compile-time make checks."
+
+  depends_on "doxygen" => [:optional, :build]
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}",
@@ -26,5 +26,9 @@ class Freexl < Formula
       system "doxygen"
       doc.install "html"
     end
+  end
+
+  test do
+    assert File.exist? "#{lib}/libfreexl.a"
   end
 end
