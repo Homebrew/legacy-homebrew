340f9d594db2d83be6ed166284a8baf16e6d68e6
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-07-26 15:15:19 +0900

    embulk 0.6.19

--- Library/Formula/embulk.rb
+++ Library/Formula/embulk.rb
@@ -1,15 +1,15 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "http://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.17.jar"
-  sha256 "68c884d84663ecc821ed327b13b90ad826ba8d5e998f422cfce6cc9745a086c6"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.19.jar"
+  sha256 "6de93139322609ff26b77ddf51916d1fcd1b472a84073ac8d242552a64501781"
 
   depends_on :java
 
   skip_clean "libexec"
 
   def install
-    (libexec/"bin").install "embulk-0.6.17.jar" => "embulk"
+    (libexec/"bin").install "embulk-0.6.19.jar" => "embulk"
     chmod 0755, libexec/"bin/embulk"
     bin.write_exec_script libexec/"bin/embulk"
   end
