58cd2c55457ceab9990e5d998e44de830f6857df
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-07-26 15:15:19 +0900

    embulk 0.6.19
    Closes #42126.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/embulk.rb
+++ Library/Formula/embulk.rb
@@ -1,23 +1,22 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "http://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.17.jar"
-  sha256 "68c884d84663ecc821ed327b13b90ad826ba8d5e998f422cfce6cc9745a086c6"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.19.jar"
+  sha256 "6de93139322609ff26b77ddf51916d1fcd1b472a84073ac8d242552a64501781"
 
   depends_on :java
 
   skip_clean "libexec"
 
   def install
-    (libexec/"bin").install "embulk-0.6.17.jar" => "embulk"
-    chmod 0755, libexec/"bin/embulk"
-    bin.write_exec_script libexec/"bin/embulk"
+    (libexec/"bin").install "embulk-#{version}.jar" => "embulk"
+    bin.write_jar_script libexec/"bin/embulk", "embulk"
   end
 
   test do
-    system "embulk", "example", "./try1"
-    system "embulk", "guess", "./try1/example.yml", "-o", "config.yml"
-    system "embulk", "preview", "config.yml"
-    system "embulk", "run", "config.yml"
+    system bin/"embulk", "example", "./try1"
+    system bin/"embulk", "guess", "./try1/example.yml", "-o", "config.yml"
+    system bin/"embulk", "preview", "config.yml"
+    system bin/"embulk", "run", "config.yml"
   end
 end
