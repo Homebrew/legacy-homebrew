97ee25b808848112723638df759a0baf42c81ea1
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-26 01:04:50 +0100

    wildfly-as: style nits

--- Library/Formula/wildfly-as.rb
+++ Library/Formula/wildfly-as.rb
@@ -1,15 +1,13 @@
-require 'formula'
-
 class WildflyAs < Formula
   desc "Managed application runtime for building applications"
-  homepage 'http://wildfly.org/'
-  url 'http://download.jboss.org/wildfly/9.0.1.Final/wildfly-9.0.1.Final.tar.gz'
-  sha256 '8f570f978dff65a006c1c6cb58c26a3856b19f01dad5b2a3ef9e463c8dee2a54'
+  homepage "http://wildfly.org/"
+  url "http://download.jboss.org/wildfly/9.0.1.Final/wildfly-9.0.1.Final.tar.gz"
+  sha256 "8f570f978dff65a006c1c6cb58c26a3856b19f01dad5b2a3ef9e463c8dee2a54"
 
   def install
     rm_f Dir["bin/*.bat"]
     rm_f Dir["bin/*.ps1"]
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
   end
 
   def caveats; <<-EOS.undent
