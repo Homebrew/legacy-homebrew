faa4f18734bbad367cbdf9f768e2227031086a3d
Evan Chan <velvia@gmail.com>
2015-07-26 16:19:12 -0700

    server_start.sh: add JOBSERVER_FG, JOBSERVER_CONFIG options

--- job-server/config/docker.sh
+++ job-server/config/docker.sh
@@ -2,7 +2,8 @@
 LOG_DIR=/var/log/job-server
 PIDFILE=spark-jobserver.pid
 # DRIVER_MEMORY=2G
-DRIVER_MEMORY=512m
-SPARK_VERSION=1.3.1
+JOBSERVER_MEMORY=512m
 SPARK_HOME=/usr/local/spark
 SPARK_CONF_DIR=$SPARK_HOME/conf
+# For Docker, always run start script as foreground
+JOBSERVER_FG=1
