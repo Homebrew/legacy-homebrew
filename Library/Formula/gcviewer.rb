27293e9aebba77a865b455b3ee5aa1d4d4f03384
Federico Ragona <f.ragona@thenewmotion.com>
2015-07-27 13:02:29 +0200

    Add description

--- Library/Formula/gcviewer.rb
+++ Library/Formula/gcviewer.rb
@@ -1,4 +1,5 @@
 class Gcviewer < Formula
+  desc "Java garbage collection visualization tool"
   homepage "https://github.com/chewiebug/GCViewer"
   url "https://github.com/chewiebug/GCViewer/archive/1.34.1.tar.gz"
   sha256 "e0e97a94c80be8323772dc8953d463fe8eaf60c3a1c5f212d0b575a3b5c640ba"
