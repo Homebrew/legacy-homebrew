8d3554686c39832c072eb0a70e6209f176d2bf12
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-27 12:03:37 +0200

    lynis 2.1.1
    Closes #42161.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/lynis.rb
+++ Library/Formula/lynis.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Lynis < Formula
   desc "Security and system auditing tool to harden systems"
   homepage "https://cisofy.com/lynis/"
-  url "https://cisofy.com/files/lynis-2.1.0.tar.gz"
-  sha256 "16ed596c8c283b8e4c635ada25ceb042371384ae09b3238a658ca60801a73c24"
+  url "https://cisofy.com/files/lynis-2.1.1.tar.gz"
+  sha256 "d17b3cbbd305c52b9cd0d5141f41954882f398db44f26c10cb45fdaaa46a99d2"
 
   bottle do
     cellar :any
@@ -16,13 +14,13 @@ class Lynis < Formula
   def install
     inreplace "lynis" do |s|
       s.gsub! 'tINCLUDE_TARGETS="/usr/local/include/lynis /usr/local/lynis/include /usr/share/lynis/include ./include"',
-        %{tINCLUDE_TARGETS="#{include}"}
+        %(tINCLUDE_TARGETS="#{include}")
       s.gsub! 'tPLUGIN_TARGETS="/usr/local/lynis/plugins /usr/local/share/lynis/plugins /usr/share/lynis/plugins /etc/lynis/plugins ./plugins"',
-        %{tPLUGIN_TARGETS="#{prefix}/plugins"}
+        %(tPLUGIN_TARGETS="#{prefix}/plugins")
       s.gsub! 'tDB_TARGETS="/usr/local/share/lynis/db /usr/local/lynis/db /usr/share/lynis/db ./db"',
-        %{tDB_TARGETS="#{prefix}/db"}
+        %(tDB_TARGETS="#{prefix}/db")
       s.gsub! 'tPROFILE_TARGETS="/usr/local/etc/lynis/default.prf /etc/lynis/default.prf /usr/local/lynis/default.prf ./default.prf"',
-        %{tPROFILE_TARGETS="#{prefix}/default.prf"}
+        %(tPROFILE_TARGETS="#{prefix}/default.prf")
     end
 
     prefix.install "db", "include", "plugins", "default.prf"
