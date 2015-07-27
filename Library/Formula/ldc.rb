ba61f75fab60da9367ee708faeb2cffcdf6a51b9
John Colvin <john.loughran.colvin@gmail.com>
2015-07-27 11:18:11 +0100

    pass strict online audit

--- Library/Formula/ldc.rb
+++ Library/Formula/ldc.rb
@@ -2,16 +2,16 @@ class Ldc < Formula
   desc "Portable D programming language compiler"
   homepage "http://wiki.dlang.org/LDC"
   url "https://github.com/ldc-developers/ldc/releases/download/v0.15.2-beta2/ldc-0.15.2-beta2-src.tar.gz"
-  sha256 "b421acbca0cdeef42c5af2bd53060253822dea6d78d216f973ee5e2b362723e2"
   version "0.15.2-beta2"
+  sha256 "b421acbca0cdeef42c5af2bd53060253822dea6d78d216f973ee5e2b362723e2"
+
+  head "https://github.com/ldc-developers/ldc.git", :shallow => false
 
   bottle do
     sha256 "42429da7c6b69babb33d17797492179faf584cee6a55980f5beba8c79d951c5a" => :yosemite
     sha256 "0b9aaf9f9f5b8dc05a4a0989ce1d54bd81fe857e36d84d54dee0ba3cad008c09" => :mavericks
     sha256 "8ea1ad6cbd93ca1b909fdce9ff136fe56ada447cd535840f2684088dcab0fea2" => :mountain_lion
   end
-
-  head "https://github.com/ldc-developers/ldc.git", :shallow => false
 
   needs :cxx11
 
@@ -38,9 +38,9 @@ class Ldc < Formula
       }
     EOS
 
-    system "#{bin}/ldc2", 'test.d'
+    system "#{bin}/ldc2", "test.d"
     system "./test"
-    system "#{bin}/ldmd2", 'test.d'
+    system "#{bin}/ldmd2", "test.d"
     system "./test"
   end
 end
