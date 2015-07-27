884caa1b859a5e8f82593d2d73738d4ec0fbeb2b
Nick Bruun <nick@bruun.co>
2015-07-27 11:11:06 +0200

    hayai 1.0.1.
    Closes #42157.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/hayai.rb
+++ Library/Formula/hayai.rb
@@ -1,8 +1,8 @@
 class Hayai < Formula
   desc "A C++ benchmarking framework inspired by the googletest framework"
   homepage "http://nickbruun.dk/2012/02/07/easy-cpp-benchmarking"
-  url "https://github.com/nickbruun/hayai/archive/v0.1.0.tar.gz"
-  sha256 "739d6dc5126a0b3dfe6c944999f48e7ede823bfdbf1f8801f23af25bf355db65"
+  url "https://github.com/nickbruun/hayai/archive/v1.0.1.tar.gz"
+  sha256 "40798cb3a7b5fcd4e0be65f9358dad4efeef7c4ebe8319327d99a2b8e5dcea4c"
 
   bottle do
     sha256 "3bc74df44803916f35cba264b4785d3b370ead8fc8dcce37493140504f562a3f" => :yosemite
