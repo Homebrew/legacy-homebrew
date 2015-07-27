b9fd9397165beac06c116cc4b6f3ed19fceafba1
Bartek Rutkowski <bartekrutkowski@users.noreply.github.com>
2015-07-27 11:52:27 +0100

    xhyve: update 0.1.0 -> 0.2.0

--- Library/Formula/xhyve.rb
+++ Library/Formula/xhyve.rb
@@ -1,8 +1,8 @@
 class Xhyve < Formula
   desc "xhyve, lightweight OS X virtualization solution based on FreeBSD's bhyve"
   homepage "https://github.com/mist64/xhyve"
-  url "https://github.com/mist64/xhyve/archive/v0.1.0.tar.gz"
-  sha256 "75be0cf1ab345874aef00833e34cf337e71aad0f946d7f9bf8ade248fc4f6b59"
+  url "https://github.com/mist64/xhyve/archive/v0.2.0.tar.gz"
+  sha256 "32c390529a73c8eb33dbc1aede7baab5100c314f726cac14627d2204ad9d3b3c"
   head "https://github.com/mist64/xhyve.git"
 
   bottle do
