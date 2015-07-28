7e88d850676f74daff9f70822c3b39f64d746ca2
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-28 13:29:57 +0100

    vice: fix style nits

--- Library/Formula/vice.rb
+++ Library/Formula/vice.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Vice < Formula
   desc "Versatile Commodore Emulator"
-  homepage 'http://vice-emu.sourceforge.net/'
-  url 'http://www.zimmers.net/anonftp/pub/cbm/crossplatform/emulators/VICE/vice-2.4.tar.gz'
-  sha256 'ff8b8d5f0f497d1f8e75b95bbc4204993a789284a08a8a59ba727ad81dcace10'
+  homepage "http://vice-emu.sourceforge.net/"
+  url "http://www.zimmers.net/anonftp/pub/cbm/crossplatform/emulators/VICE/vice-2.4.tar.gz"
+  sha256 "ff8b8d5f0f497d1f8e75b95bbc4204993a789284a08a8a59ba727ad81dcace10"
   revision 2
 
   bottle do
@@ -14,11 +12,11 @@ class Vice < Formula
     sha256 "de32b3004dbc9a1dad21a546c983ba55d3559eae78f898a54be96c8f2c278b3b" => :mountain_lion
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'jpeg'
-  depends_on 'libpng'
-  depends_on 'giflib'
-  depends_on 'lame' => :optional
+  depends_on "pkg-config" => :build
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "giflib"
+  depends_on "lame" => :optional
 
   fails_with :llvm do
     build 2335
@@ -37,12 +35,13 @@ class Vice < Formula
                           # http://sourceforge.net/tracker/?func=detail&aid=3585471&group_id=223021&atid=1057617
                           "--disable-ffmpeg"
     system "make"
-    system "make bindist"
-    prefix.install Dir['vice-macosx-*/*']
-    bin.install_symlink Dir[prefix/'tools/*']
+    system "make", "bindist"
+    prefix.install Dir["vice-macosx-*/*"]
+    bin.install_symlink Dir[prefix/"tools/*"]
   end
 
-  def caveats
-    "Cocoa apps for these emulators have been installed to #{prefix}."
+  def caveats; <<-EOS.undent
+    Cocoa apps for these emulators have been installed to #{prefix}.
+  EOS
   end
 end
