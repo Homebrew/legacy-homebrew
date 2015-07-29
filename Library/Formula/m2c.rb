8b29bb82d4ec0ae13654b5b45d75b4f83a6873b1
Franklin Chen <franklinchen@franklinchen.com>
2015-07-29 02:59:52 -0400

    Cleanup of new m2c formula.

--- Library/Formula/m2c.rb
+++ Library/Formula/m2c.rb
@@ -5,13 +5,37 @@ class M2c < Formula
   sha256 "b725ed617f376e1a321e059bf1985098e950965d5edab161c6b24526f10a59bc"
 
   def install
-    system "./configure", "+cc=gcc"
+    # The config for "gcc" works for clang also.
+    cp "config/generic-gcc.h", "config/generic-clang.h"
+    system "./configure", "+cc=#{ENV.cc}"
 
     # Makefile is buggy!
     inreplace "Makefile", "install: all uninstall", "install: all"
     inreplace "Makefile", "mkdir", "mkdir -p"
     mkdir_p prefix/"include"
 
-    system "make", "install", "prefix=#{prefix}"
+    system "make", "install", "prefix=#{prefix}", "man1dir=#{man1}"
+  end
+
+  test do
+    hello_mod = testpath/"hello.mod"
+    hello = testpath/"hello"
+
+    hello_mod.write <<-EOF.undent
+      MODULE Hello;
+
+      FROM InOut IMPORT
+        WriteLn, WriteString;
+
+      BEGIN
+        WriteString ("Hello world!");
+        WriteLn;
+      END HiThere.
+    EOF
+
+    system "#{bin}/m2c", "-make", hello_mod, "-o", hello
+
+    output = shell_output(hello)
+    assert output.equal?("Hello world!")
   end
 end
