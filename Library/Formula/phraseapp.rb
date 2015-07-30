dfa2eade37bbe12e0ae770c1783c3b882bcbcf7c
sacry-dyn <matthias.nitsche@dynport.de>
2015-07-30 14:39:12 +0200

    phraseapp 0.2.0.beta9 (new formula)

---
+++ Library/Formula/phraseapp.rb
@@ -0,0 +1,15 @@
+class Phraseapp < Formula
+  desc "Command-line tool for PhraseApp localization service"
+  homepage "http://docs.phraseapp.com/api/v2"
+  url "https://github.com/phrase/phraseapp-client/releases/download/0.2.0.beta9/phraseapp_macosx_amd64.tar.gz"
+  version "0.2.0.beta9"
+  sha256 "e4e9f47a5cfc5c13cafa3a5f28f4873498eb46cf6fe2253d069c14a35c4a3e3d"
+
+  def install
+    bin.install "phraseapp"
+  end
+
+  test do
+    system bin/"phraseapp", "help"
+  end
+end
