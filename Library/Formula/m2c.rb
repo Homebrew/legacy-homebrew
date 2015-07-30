607a989ee78ea802864a9780f3dee47cf3b87b5e
Franklin Chen <franklinchen@franklinchen.com>
2015-07-30 09:59:23 -0400

    Clean up formula, add note about interim hacks.

--- Library/Formula/m2c.rb
+++ Library/Formula/m2c.rb
@@ -3,7 +3,10 @@ class M2c < Formula
   homepage "https://savannah.nongnu.org/projects/m2c/"
   url "http://download.savannah.gnu.org/releases/m2c/0.7/m2c-0.7.tar.gz"
   sha256 "b725ed617f376e1a321e059bf1985098e950965d5edab161c6b24526f10a59bc"
+  head "http://git.savannah.gnu.org/cgit/m2c.git"
 
+  # Hacks purely for this 0.7 release. Git head already fixes installation glitches.
+  # Will remove hacks on release of next version.
   def install
     # The config for "gcc" works for clang also.
     cp "config/generic-gcc.h", "config/generic-clang.h"
@@ -12,33 +15,29 @@ class M2c < Formula
     # Makefile is buggy!
     inreplace "Makefile", "install: all uninstall", "install: all"
     inreplace "Makefile", "mkdir", "mkdir -p"
-    mkdir_p prefix/"include"
+    include.mkpath
 
     system "make", "install", "prefix=#{prefix}", "man1dir=#{man1}"
   end
 
   test do
-    # -make mode for now needs running in source directory.
-    cd testpath do
-      hello_mod = "Hello.mod"
-      hello_exe = "Hello"
+    hello_mod = "Hello.mod"
+    hello_exe = "Hello"
 
-      (testpath/hello_mod).write <<-EOF.undent
-        MODULE Hello;
+    (testpath/hello_mod).write <<-EOF.undent
+      MODULE Hello;
 
-        FROM InOut IMPORT
-          WriteLn, WriteString;
+      FROM InOut IMPORT
+        WriteLn, WriteString;
 
-        BEGIN
-          WriteString ("Hello world!");
-          WriteLn;
-        END Hello.
-      EOF
+      BEGIN
+        WriteString ("Hello world!");
+        WriteLn;
+      END Hello.
+    EOF
 
-      system "#{bin}/m2c", "-make", hello_mod, "-o", testpath/hello_exe
+    system "#{bin}/m2c", "-make", hello_mod, "-o", hello_exe
 
-      output = shell_output(testpath/hello_exe)
-      assert_equal "Hello world!\n", output
-    end
+    assert_equal "Hello world!\n", shell_output(hello_exe)
   end
 end
