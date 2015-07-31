82a924bf4f915df2f29f47ef8c7313dfad7073de
David Adrian <davidcadrian@gmail.com>
2015-07-31 10:34:05 -0400

    Shorten bgpdump description, add make check

--- Library/Formula/bgpdump.rb
+++ Library/Formula/bgpdump.rb
@@ -1,6 +1,6 @@
 class Bgpdump < Formula
+  desc "A  C library for analyzing MRT/Zebra/Quagga dump files"
   homepage "https://bitbucket.org/ripencc/bgpdump/wiki/Home"
-  desc "A  C library designed to help with analyzing dump files produced by Zebra/Quagga or MRT"
   url "https://bitbucket.org/ripencc/bgpdump/get/1.4.99.15.tar.gz"
   sha256 "6da4ef4020345c68a2551460919ec02b7b63c194324e209fc73bea9889fb5c7c"
   depends_on "autoconf" => :build
@@ -12,6 +12,7 @@ class Bgpdump < Formula
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make"
+    system "make", "check"
     system "make", "install"
   end
 
