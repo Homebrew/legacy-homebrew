39bef99503641a4980efdeb38fc469d0453cfbb3
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-31 16:30:48 +0100

    asciitex: use pkgshare
    Closes #42303.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/asciitex.rb
+++ Library/Formula/asciitex.rb
@@ -1,19 +1,17 @@
-require 'formula'
-
 class Asciitex < Formula
   desc "Generate ASCII-art representations of mathematical equations"
-  homepage 'http://asciitex.sourceforge.net'
-  url 'https://downloads.sourceforge.net/project/asciitex/asciiTeX-0.21.tar.gz'
-  sha1 'b37d300910952c117f82f77dd2da99a7b9a79db7'
+  homepage "http://asciitex.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/asciitex/asciiTeX-0.21.tar.gz"
+  sha256 "abf964818833d8b256815eb107fb0de391d808fe131040fb13005988ff92a48d"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-gtk"
-    inreplace 'Makefile', 'man/asciiTeX_gui.1', ''
-    system "make install"
-    prefix.install 'EXAMPLES'
+    inreplace "Makefile", "man/asciiTeX_gui.1", ""
+    system "make", "install"
+    pkgshare.install "EXAMPLES"
   end
 
   test do
-    system "#{bin}/asciiTeX", "-f", "#{prefix}/EXAMPLES"
+    system "#{bin}/asciiTeX", "-f", "#{pkgshare}/EXAMPLES"
   end
 end
