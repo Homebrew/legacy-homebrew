f6561e7a2b2c4829be83b3020d5246ea71ede712
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-31 02:37:51 +0100

    gauche: update 0.9.4 bottle.

--- Library/Formula/gauche.rb
+++ Library/Formula/gauche.rb
@@ -5,9 +5,10 @@ class Gauche < Formula
   sha256 "7b18bcd70beaced1e004594be46c8cff95795318f6f5830dd2a8a700410fc149"
 
   bottle do
-    sha1 "844ce90625ae0fd6ab27afc965edc7c05e6d283d" => :mavericks
-    sha1 "e039cf0ff8ab34db5053fe2a010822ec3205b5e3" => :mountain_lion
-    sha1 "087b1f85a18485fe2226892d9b96066a027de606" => :lion
+    revision 1
+    sha256 "4018f9ae2b0a179ef5df0482e6734186f0069e773496f2f6092971f10a4c7b11" => :yosemite
+    sha256 "e1258b3e8f0c5a61c8df484e173837e6fb3c7ecad7cd1fee277d1f8da56e98f9" => :mavericks
+    sha256 "ffd47c0dcb63a508ae95adfc1e9c7825e75dd40fa10e46c68fcb8bd9b00637d5" => :mountain_lion
   end
 
   def install
