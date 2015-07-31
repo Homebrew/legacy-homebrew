37cc9102d77742fe3c76526e9424c620eacfb153
Tomasz Pajor <tomek@polishgeeks.com>
2015-07-31 08:03:44 +0200

    logstash 1.5.3

--- Library/Formula/logstash.rb
+++ Library/Formula/logstash.rb
@@ -1,12 +1,14 @@
 class Logstash < Formula
   desc "Tool for managing events and logs"
   homepage "https://www.elastic.co/products/logstash"
-  url "https://download.elasticsearch.org/logstash/logstash/logstash-1.5.2.tar.gz"
-  sha256 "1d1805d388392a69f5049b35c176186389a7f8bf7347c4528c255edc1f9b0d6a"
+  url "https://download.elasticsearch.org/logstash/logstash/logstash-1.5.3.tar.gz"
+  sha256 "eb3c366074e561d777348bfe9db3d4d1cccbf2fa8e7406776f500b4ca639c4aa"
+
+  depends_on :java => "1.7+"
 
   def install
-    inreplace %w{bin/logstash}, /^\. "\$\(cd `dirname \$0`\/\.\.; pwd\)\/bin\/logstash\.lib\.sh\"/, ". #{libexec}/bin/logstash.lib.sh"
-    inreplace %w{bin/logstash.lib.sh}, /^LOGSTASH_HOME=.*$/, "LOGSTASH_HOME=#{libexec}"
+    inreplace %w[bin/logstash], %r{^\. "\$\(cd `dirname \$0`\/\.\.; pwd\)\/bin\/logstash\.lib\.sh\"}, ". #{libexec}/bin/logstash.lib.sh"
+    inreplace %w[bin/logstash.lib.sh], /^LOGSTASH_HOME=.*$/, "LOGSTASH_HOME=#{libexec}"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/logstash"
   end
@@ -20,6 +22,6 @@ class Logstash < Formula
 
   test do
     system "#{bin}/logstash", "--version"
-    system "/bin/echo 'hello world' | #{bin}/logstash agent -e 'input { stdin { type => stdin } } output { stdout { codec => rubydebug } }'"
+    system "/bin/echo", "'hello world' | #{bin}/logstash agent -e 'input { stdin { type => stdin } } output { stdout { codec => rubydebug } }'"
   end
 end
