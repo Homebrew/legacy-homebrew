03a5d5c5949f874b46bd733956cf00702176f59a
Grant Slater <grant.slater@wearefriday.com>
2015-07-31 13:13:29 +0100

    zbackup 1.4.2
    Closes #42298.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/zbackup.rb
+++ Library/Formula/zbackup.rb
@@ -3,8 +3,8 @@ require "formula"
 class Zbackup < Formula
   desc "Globally-deduplicating backup tool (based on ideas in rsync)"
   homepage "http://zbackup.org"
-  url "https://github.com/zbackup/zbackup/archive/1.4.1.tar.gz"
-  sha1 "6fc5c7f4bb23ce52bd4e14cb9b07c77662e1703c"
+  url "https://github.com/zbackup/zbackup/archive/1.4.2.tar.gz"
+  sha256 "6259e8206ef996c8f925a536dda09d955bd6308d1e9b3b5b6bcdfd00e8ac8f41"
 
   depends_on "cmake" => :build
   depends_on "openssl"
@@ -15,12 +15,12 @@ class Zbackup < Formula
   # These fixes are upstream and can be removed in the next released version.
   patch do
     url "https://github.com/zbackup/zbackup/commit/7e6adda6b1df9c7b955fc06be28fe6ed7d8125a2.diff"
-    sha1 "eb03aad6e9c543914a7332b02f277bb76103e1f2"
+    sha256 "564c494b02be7b159b21f1cfcc963df29350061e050e66b7b3d96ed829552351"
   end
 
   patch do
     url "https://github.com/zbackup/zbackup/commit/f4ff7bd8ec63b924a49acbf3a4f9cf194148ce18.diff"
-    sha1 "4d8c584e7f35ca2994dc751b8398b0b091119316"
+    sha256 "47f760aa03a0a1550f05e30b1fa127afa1eda5a802d0d6edd9be07f3762008fb"
   end
 
   def install
