b9a610a088bdbd53e038a85d4f71c5fd50d8bb58
Hiroshi Hatake <hatake@clear-code.com>
2015-07-31 10:24:06 +0900

    gauche: fix strict audit
    Closes #42282.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/gauche.rb
+++ Library/Formula/gauche.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Gauche < Formula
   desc "R5RS Scheme implementation, developed to be a handy script interpreter"
-  homepage 'http://practical-scheme.net/gauche/'
-  url 'https://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.4.tgz'
-  sha1 '2f0068d19adbc8e7fd3c04ab8e6576d0fac21ad6'
+  homepage "http://practical-scheme.net/gauche/"
+  url "https://downloads.sourceforge.net/gauche/Gauche/Gauche-0.9.4.tgz"
+  sha256 "7b18bcd70beaced1e004594be46c8cff95795318f6f5830dd2a8a700410fc149"
 
   bottle do
     sha1 "844ce90625ae0fd6ab27afc965edc7c05e6d283d" => :mavericks
@@ -13,10 +11,15 @@ class Gauche < Formula
   end
 
   def install
-    system './configure', "--prefix=#{prefix}", '--disable-dependency-tracking',
-                          '--enable-multibyte=utf-8'
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking",
+                          "--enable-multibyte=utf-8"
     system "make"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    output = shell_output("gosh -V")
+    assert_match /Gauche scheme shell, version #{version}/, output
   end
 end
