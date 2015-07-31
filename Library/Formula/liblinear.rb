460ed1e63a6f2c638cb496354258e6f93a1e5f82
Chih-Wei Chang <bert.cwchang@gmail.com>
2015-07-31 15:32:55 +0800

    Add HEAD version from LIBLINEAR GitHub repo.

--- Library/Formula/liblinear.rb
+++ Library/Formula/liblinear.rb
@@ -6,6 +6,8 @@ class Liblinear < Formula
   url "http://www.csie.ntu.edu.tw/~cjlin/liblinear/oldfiles/liblinear-2.01.tar.gz"
   sha256 "ebc71999224f5779574b11e248f1e2ef647b5d839c0380c1c5a4ac8789aa95a9"
 
+  head "https://github.com/cjlin1/liblinear.git"
+
   bottle do
     cellar :any
     sha256 "72fe174f6e5cb9b3ace806b7e8670754c99a99845b77e3e66e3727722ecf1cb4" => :yosemite
