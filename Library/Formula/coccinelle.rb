04206d3b2e9de325145f9021b31dedd889ffb252
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-01 05:15:45 +0100

    coccinelle: use sha256

--- Library/Formula/coccinelle.rb
+++ Library/Formula/coccinelle.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Coccinelle < Formula
   desc "Program matching and transformation engine for C code"
-  homepage 'http://coccinelle.lip6.fr/'
-  url 'http://coccinelle.lip6.fr/distrib/coccinelle-1.0.0-rc21.tgz'
-  sha1 'edc008da552eb8f4ef7712fc99b4dc630ab6fb35'
+  homepage "http://coccinelle.lip6.fr/"
+  url "http://coccinelle.lip6.fr/distrib/coccinelle-1.0.0-rc21.tgz"
+  sha256 "a6609a1f800f84d058c9b395edd0597171594b685f551a9b9c03728a1b416783"
   revision 1
 
   bottle do
