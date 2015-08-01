ea5a4264c1e1440c3bd58b98f67cc63196be9a2b
Lawrence D'Anna <larry@elder-gods.org>
2015-07-31 19:30:42 -0700

    webarchiver: dependency is actually on xcode, not os x
    Signed-off-by: Lawrence D'Anna <larry@elder-gods.org>

--- Library/Formula/webarchiver.rb
+++ Library/Formula/webarchiver.rb
@@ -5,7 +5,7 @@ class Webarchiver < Formula
   sha256 "8ea826038e923c72e75a4bbb1416910368140a675421f6aaa51fd0dea703f75c"
   head "https://github.com/newzealandpaul/webarchiver.git"
 
-  depends_on :macos => :mavericks
+  depends_on :xcode => ["6.0.1", :build]
 
   def install
     xcodebuild
