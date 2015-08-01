015ac738318d0a82e7fde4350b50f0bdf098aa28
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-01 22:02:26 +0100

    webarchiver: add 0.9 bottle.

--- Library/Formula/webarchiver.rb
+++ Library/Formula/webarchiver.rb
@@ -5,6 +5,12 @@ class Webarchiver < Formula
   sha256 "8ea826038e923c72e75a4bbb1416910368140a675421f6aaa51fd0dea703f75c"
   head "https://github.com/newzealandpaul/webarchiver.git"
 
+  bottle do
+    cellar :any
+    sha256 "829859d0f3fd2f52d1d611c74f3678f5e078f0af519a60ac334da09947cdc99f" => :yosemite
+    sha256 "a599a339bc07e0b464b3255ebae4d23d8bd221787069a755d7bbb52d2aefe778" => :mavericks
+  end
+
   depends_on :xcode => ["6.0.1", :build]
 
   def install
