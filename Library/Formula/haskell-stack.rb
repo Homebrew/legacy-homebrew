84c68bd068519a76d8409e31a8c6dfbb80ca3641
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-01 06:40:10 +0100

    haskell-stack: add 0.1.2.0 bottle.

--- Library/Formula/haskell-stack.rb
+++ Library/Formula/haskell-stack.rb
@@ -10,6 +10,12 @@ class HaskellStack < Formula
 
   head "https://github.com/commercialhaskell/stack.git"
 
+  bottle do
+    sha256 "1def37b5df9d9423081b8d5a8fa3d5958fcecfa664b75695d6fdde2fb5877e4a" => :yosemite
+    sha256 "4c106bef7f02774b35ddae28f409b5b51d0630a77e0436befb65ac7430277001" => :mavericks
+    sha256 "86d6ea580bbfa3b645ea6a3558cfde24979b8d87fd6eb211ffc82415d721fcaa" => :mountain_lion
+  end
+
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
 
