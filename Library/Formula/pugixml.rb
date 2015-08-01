59a96599e1050179f2f974cf89d9d4352fe6628e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-01 14:30:02 +0100

    pugixml: add 1.6 bottle.

--- Library/Formula/pugixml.rb
+++ Library/Formula/pugixml.rb
@@ -4,6 +4,13 @@ class Pugixml < Formula
   url "https://github.com/zeux/pugixml/releases/download/v1.6/pugixml-1.6.tar.gz"
   sha256 "473705c496d45ee6a74f73622b175dfb5dde0de372c4dc61a5acb964516cd9de"
 
+  bottle do
+    cellar :any
+    sha256 "27a9b98dde133f5871b13af86d5e9eee120681c3e3f3522bb3ed8469a5057191" => :yosemite
+    sha256 "a8e856b317d735c1c6f6d6d0f10827c2f093235daa678e3bc67055195989fe19" => :mavericks
+    sha256 "abefae3200fb57a4ef0d6a3b6f8d3bf85a6d46170ecedf383068de9c049d76ca" => :mountain_lion
+  end
+
   option "with-shared", "Build shared instead of static library"
 
   depends_on "cmake" => :build
