2a793595871c5a0330500fc33c0d9bf6b2e20312
Xu Cheng <xucheng@me.com>
2015-08-02 16:34:45 +0800

    tab: check mxcl/master synonym unconditionally

--- Library/Homebrew/tab.rb
+++ Library/Homebrew/tab.rb
@@ -43,10 +43,10 @@ class Tab < OpenStruct
     tapped_from = attributes["tapped_from"]
     unless tapped_from.nil? || tapped_from == "path or URL"
       attributes["source"]["tap"] = attributes.delete("tapped_from")
+    end
 
-      if attributes["source"]["tap"] == "mxcl/master"
-        attributes["source"]["tap"] == "Homebrew/homebrew"
-      end
+    if attributes["source"]["tap"] == "mxcl/master"
+      attributes["source"]["tap"] = "Homebrew/homebrew"
     end
 
     if attributes["source"]["spec"].nil?
