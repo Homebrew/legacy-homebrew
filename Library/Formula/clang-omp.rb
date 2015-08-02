4afbff4dc0aef805536df3ad197578ee0bf25cfc
metacollin <metacollin@gmail.com>
2015-08-02 13:33:04 -0600

    Added --HEAD option to clang-omp

--- Library/Formula/clang-omp.rb
+++ Library/Formula/clang-omp.rb
@@ -1,32 +1,50 @@
 class ClangOmp < Formula
+  desc "OpenMP C/C++ language extensions in Clang/LLVM compiler"
   homepage "https://clang-omp.github.io/"
+  head "https://github.com/clang-omp/llvm_trunk.git"
   url "https://github.com/clang-omp/llvm/archive/2015-04-01.tar.gz"
   version "2015-04-01"
   sha256 "37f990ad99b3213507ec88f86702c5a057ce397cc16638eeee5c88906572daec"
 
   bottle do
-    revision 1
-    sha256 "dfc5f4c5ca5f91b71f6e6c71934371f823f3d9f922184954289c0fa58c610738" => :yosemite
-    sha256 "0cf85df1cade74005852efe44292babf82582e46f105fd752699e3b6d7258ce5" => :mavericks
-    sha256 "9bc657cd6162cfe4755733e22c2292748ec4073cc1372737c2e07094f10958b6" => :mountain_lion
+    revision 2
+    sha256 "2d29cc7bcde757610a325e57c0ea0162cee97c2cf04ea424f8e4010c57ae2295" => :yosemite
+    sha256 "3989eb56c26ca2903ea1bde7aaf0b02c05a5dbfc1eae59056860abca9c6d5fec" => :mavericks
+    sha256 "2a848d5efa46b5cc6e906f2c71608c8098238a397e6979912dcef2b5b1d2807c" => :mountain_lion
   end
 
   depends_on "libiomp"
   depends_on "cmake" => :build
 
-  resource "compiler-rt" do
-    url "https://github.com/clang-omp/compiler-rt/archive/2015-04-01.tar.gz"
-    sha256 "5a8d39ff6ce524e23fae32870f85b18d43f2795da2011d3cbb6b29d471bb27b7"
+  stable do
+    resource "compiler-rt" do
+      url "https://github.com/clang-omp/compiler-rt/archive/2015-04-01.tar.gz"
+      sha256 "5a8d39ff6ce524e23fae32870f85b18d43f2795da2011d3cbb6b29d471bb27b7"
+    end
+
+    resource "clang" do
+      url "https://github.com/clang-omp/clang/archive/2015-04-01.tar.gz"
+      sha256 "2717115e5ba491e3b8119311f0d792420ba41be34a89733b9880eb3d3c09fbe5"
+    end
+
+    resource "libcxx" do
+      url "https://github.com/llvm-mirror/libcxx/archive/release_35.tar.gz"
+      sha256 "df23b356ae1953de671d1dc9093568330e074bbe48cd6d93d16173a793550c71"
+    end
   end
 
-  resource "clang" do
-    url "https://github.com/clang-omp/clang/archive/2015-04-01.tar.gz"
-    sha256 "2717115e5ba491e3b8119311f0d792420ba41be34a89733b9880eb3d3c09fbe5"
-  end
+  head do
+    resource "compiler-rt" do
+      url "https://github.com/clang-omp/compiler-rt_trunk.git"
+    end
 
-  resource "libcxx" do
-    url "https://github.com/llvm-mirror/libcxx/archive/release_35.tar.gz"
-    sha256 "df23b356ae1953de671d1dc9093568330e074bbe48cd6d93d16173a793550c71"
+    resource "clang" do
+      url "https://github.com/clang-omp/clang_trunk.git"
+    end
+
+    resource "libcxx" do
+      url "https://github.com/llvm-mirror/libcxx.git"
+    end
   end
 
   needs :cxx11
@@ -59,7 +77,7 @@ class ClangOmp < Formula
   end
 
   test do
-    testfile = <<-EOS.undent
+    (testpath/"test.c").write <<-EOS.undent
       #include <stdlib.h>
       #include <stdio.h>
       #include <omp.h>
@@ -72,14 +90,11 @@ class ClangOmp < Formula
           return EXIT_SUCCESS;
       }
     EOS
-    (testpath/"test.c").write(testfile)
-    system "#{bin}/clang-omp", "-L/usr/local/lib", "-liomp5", "-fopenmp", "-Werror", "-Wall", "test.c", "-o", "test"
-    system "./test>#{testpath}/testresult"
 
-    testresult = (testpath/"testresult").read
-    testresult_lines = testresult.split "\n"
-    sorted_testresult_lines = testresult_lines.sort
-    sorted_testresult = sorted_testresult_lines.join "\n"
+    system "#{bin}/clang-omp", "-liomp5", "-fopenmp", "test.c", "-o", "test"
+    testresult = shell_output("./test")
+
+    sorted_testresult = testresult.split("\n").sort.join("\n")
     expected_result = <<-EOS.undent
       Hello from thread 0, nthreads 4
       Hello from thread 1, nthreads 4
