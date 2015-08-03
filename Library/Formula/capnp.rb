0a6e25859b16ddae6def66c2849e309e1280ac6f
Ian Lancaster <ilancaster@gmail.com>
2015-08-02 21:12:22 -0400

    capnp 0.5.3
    Closes #42373.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/capnp.rb
+++ Library/Formula/capnp.rb
@@ -1,8 +1,8 @@
 class Capnp < Formula
   desc "Data interchange format and capability-based RPC system"
   homepage "https://capnproto.org/"
-  url "https://capnproto.org/capnproto-c++-0.5.2.tar.gz"
-  sha256 "23145a0127c2b1629c4b72e6000e04991640e551947fa39fd06710d64bd942a8"
+  url "https://capnproto.org/capnproto-c++-0.5.3.tar.gz"
+  sha256 "cdb17c792493bdcd4a24bcd196eb09f70ee64c83a3eccb0bc6534ff560536afb"
 
   bottle do
     sha256 "df70e870b12a65442ced879e21418554e1594de20e86691f68c6a1ec17e60bf4" => :yosemite
@@ -11,21 +11,16 @@ class Capnp < Formula
   end
 
   needs :cxx11
-  option "without-shared", "Disable building shared library variant"
+  depends_on "cmake" => :build
 
   def install
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --prefix=#{prefix}
-    ]
+    ENV.cxx11
 
-    args << "--disable-shared" if build.without? "shared"
-
-    system "./configure", *args
-    system "make", "check"
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "check"
+      system "make", "install"
+    end
   end
 
   test do
