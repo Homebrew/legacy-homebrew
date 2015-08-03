61cc60ce506bed62b6f35181ad49762154f13f84
Stuart Williams <stuart@sidereal.ca>
2015-08-03 01:45:46 -0400

    grails 3.0.4, and added test
    Closes #42393.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v3.0.3/grails-3.0.3.zip"
-  sha256 "3de5392a6f9dbc325668503684ab6dba913b38e68b964242eab1130712d55996"
+  url "https://github.com/grails/grails-core/releases/download/v3.0.4/grails-3.0.4.zip"
+  sha256 "7fa8581a200e532c4701e1d56fc12305872369d520d3a7e624608f4dae87a4e4"
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
@@ -15,5 +15,9 @@ class Grails < Formula
     The GRAILS_HOME directory is:
       #{opt_libexec}
     EOS
+  end
+
+  test do
+    assert_match /Application created/, shell_output("#{bin}/grails create-app testApp")
   end
 end
