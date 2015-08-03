824eaae41342646a0b05adadf30d02f9ad95f4d1
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-03 20:05:38 +0100

    scmpuff: add 0.1.1 bottle.

--- Library/Formula/scmpuff.rb
+++ Library/Formula/scmpuff.rb
@@ -4,6 +4,13 @@ class Scmpuff < Formula
   url "https://github.com/mroth/scmpuff/archive/v0.1.1.tar.gz"
   sha256 "cec3c9df41acb1735f2e8c1c9840d0481af0d996690f5a19a0a8fc4f06f97370"
 
+  bottle do
+    cellar :any
+    sha256 "f00364efe1b324301ed9ebac6cf1035d1f021fe58b0a5986d039f849a17e1965" => :yosemite
+    sha256 "3cb9284bdfc2b933422f91d6777b63925c0b9835af887a321709a0170966759b" => :mavericks
+    sha256 "8445d1788508e9dd7fdbb5385f7d3aec5c329f219daecefc9a25bc9bbf97e716" => :mountain_lion
+  end
+
   depends_on "go" => :build
 
   def install
