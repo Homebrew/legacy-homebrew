9c8511234223c638857f567f7cbb036cedb87a72
kret <andreskee@gmail.com>
2015-08-03 21:33:34 +0200

    tomee-webprofile 1.7.2
    Closes #42426.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/tomee-webprofile.rb
+++ Library/Formula/tomee-webprofile.rb
@@ -1,13 +1,15 @@
 class TomeeWebprofile < Formula
   desc "All-Apache Java EE 6 Web Profile stack"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.1/apache-tomee-1.7.1-webprofile.tar.gz"
-  version "1.7.1"
-  sha256 "5364e583f8e7838cd5671ee0354b106351bc63aef665bb7ee1e3c746f01bb62a"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.2/apache-tomee-1.7.2-webprofile.tar.gz"
+  version "1.7.2"
+  sha256 "9802fef834a3d2944fc325440e1aadbd3b00956e5ef43f5ef9eea8b91a12d230"
 
   def install
     # Remove Windows scripts
     rm_rf Dir["bin/*.bat"]
+    rm_rf Dir["bin/*.bat.original"]
+    rm_rf Dir["bin/*.exe"]
 
     # Install files
     prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
