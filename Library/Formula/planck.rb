aea2bd5e2ce77c27e21f7f65d124a7097a9b0489
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-03 22:51:34 +0100

    planck: add 1.3 bottle.

--- Library/Formula/planck.rb
+++ Library/Formula/planck.rb
@@ -4,6 +4,11 @@ class Planck < Formula
   url "https://github.com/mfikes/planck/archive/1.3.tar.gz"
   sha256 "82dabffc03cf16527a867782714f81b93e7bb9e7a5db9d3992b74cc74c169790"
 
+  bottle do
+    cellar :any
+    sha256 "20a1f49178c32772373aa9b5917d2502f0828626d176f5884f1c25a4e4e6361d" => :yosemite
+  end
+
   depends_on "leiningen" => :build
 
   depends_on :xcode => :build
