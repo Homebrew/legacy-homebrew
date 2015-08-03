7bf2583487b414a5301adb37c396b22258f55f08
kret <andreskee@gmail.com>
2015-08-03 21:54:25 +0200

    tomee-plus 1.7.2

--- Library/Formula/tomee-plus.rb
+++ Library/Formula/tomee-plus.rb
@@ -1,13 +1,15 @@
 class TomeePlus < Formula
   desc "Everything in TomEE Web Profile and JAX-RS, plus more"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.1/apache-tomee-1.7.1-plus.tar.gz"
-  version "1.7.1"
-  sha256 "638f70c55d3927ff8e15281e4a24aa17f632a9eabaa3c52f448b7009d1d289eb"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.2/apache-tomee-1.7.2-plus.tar.gz"
+  version "1.7.2"
+  sha256 "deb551788f56e5051e4f9cd6a642b9543c7635819a58015b72d8506de08c693a"
 
   def install
     # Remove Windows scripts
     rm_rf Dir["bin/*.bat"]
+    rm_rf Dir["bin/*.bat.original"]
+    rm_rf Dir["bin/*.exe"]
 
     # Install files
     prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
