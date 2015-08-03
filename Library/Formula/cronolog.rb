1b18406c2e1d68b91ad116fe0f1e326877dfbbc8
Andrew Janke <andrew@apjanke.net>
2015-08-03 01:57:17 -0400

    cronolog: update URL and fix audits
    Closes #42395.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/cronolog.rb
+++ Library/Formula/cronolog.rb
@@ -3,13 +3,14 @@ require "formula"
 class Cronolog < Formula
   desc "Web log rotation"
   homepage "http://web.archive.org/web/20140209202032/http://cronolog.org/"
-  url "http://fossies.org/linux/www/cronolog-1.6.2.tar.gz"
-  sha1 "6422b7c5e87241eb31d76809a2e0eea77ae4c64e"
+  url "https://fossies.org/linux/www/old/cronolog-1.6.2.tar.gz"
+  sha256 "65e91607643e5aa5b336f17636fa474eb6669acc89288e72feb2f54a27edb88e"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--mandir=#{man}"
+                          "--mandir=#{man}",
+                          "--infodir=#{info}"
     system "make install"
   end
 end
