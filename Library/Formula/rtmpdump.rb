c11d5dae52b5c143afbb2a15787b036931ae4cb4
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-03 19:43:24 +0100

    rtmpdump: use secure links

--- Library/Formula/rtmpdump.rb
+++ Library/Formula/rtmpdump.rb
@@ -3,9 +3,12 @@
 class Rtmpdump < Formula
   desc "Tool for downloading RTMP streaming media"
   homepage "https://rtmpdump.mplayerhq.hu"
-  url "http://ftp.debian.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20150115.gita107cef.orig.tar.gz"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/r/rtmpdump/rtmpdump_2.4+20150115.gita107cef.orig.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/r/rtmpdump/rtmpdump_2.4%2b20150115.gita107cef.orig.tar.gz"
   version "2.4+20150115"
   sha256 "d47ef3a07815079bf73eb5d053001c4341407fcbebf39f34e6213c4b772cb29a"
+
+  head "git://git.ffmpeg.org/rtmpdump"
 
   bottle do
     cellar :any
@@ -13,8 +16,6 @@ class Rtmpdump < Formula
     sha256 "f906ce07d4ab1e365f22afabfa594fffba1caf0d3e7fa749a76b07a944891aba" => :mavericks
     sha256 "90f87f1c3e8c68385576812bdfadc39152d3bd9166cafb982761d1a6cc915710" => :mountain_lion
   end
-
-  head "git://git.ffmpeg.org/rtmpdump"
 
   depends_on "openssl"
 
