9abe6c74c153f0a3f90dbb7476e27dd8fb84d6ee
ZHANG RT <4399.omi@gmail.com>
2015-08-04 23:39:07 +0800

    sexy→concise
    sexy→concise

--- Library/Formula/musicbox.rb
+++ Library/Formula/musicbox.rb
@@ -1,5 +1,5 @@
 class MusicBox < Formula
-  desc "A sexy command line interface musicbox"
+  desc "A concise command line interface musicbox"
   homepage "https://github.com/darknessomi/musicbox"
   url "https://pypi.python.org/packages/source/N/NetEase-MusicBox/NetEase-MusicBox-0.1.6.5.tar.gz#md5=f731f7c94804a42d2acde2825dba37e1"
   version "0.1.6.5"
