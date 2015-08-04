61721d1d62540de72c5909c14d4f2834a16f4302
ZHANG RT <4399.omi@gmail.com>
2015-08-04 23:51:26 +0800

    install fix

--- Library/Formula/musicbox.rb
+++ Library/Formula/musicbox.rb
@@ -1,7 +1,7 @@
 class MusicBox < Formula
   desc "A concise command line interface musicbox"
   homepage "https://github.com/darknessomi/musicbox"
-  url "https://pypi.python.org/packages/source/N/NetEase-MusicBox/NetEase-MusicBox-0.1.6.5.tar.gz#md5=f731f7c94804a42d2acde2825dba37e1"
+  url "https://pypi.python.org/packages/source/N/NetEase-MusicBox/NetEase-MusicBox-0.1.6.5.tar.gz"
   version "0.1.6.5"
   sha256 "8383d773bd1d4f5e1f4b41897fe34e3dac25c47d3f1b59f1cef62c35ce14a07e"
   sha1 "d2f8cf412ba4a963ed76ec4dee2dc6fcd34a5652"
@@ -9,7 +9,11 @@ class MusicBox < Formula
   depends_on :mpg123 
 
   def install
-    system "sudo", "python", "setup.py", "install"
+    xy = Language::Python.major_minor_version "python"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
 end
