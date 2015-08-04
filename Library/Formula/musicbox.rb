23d679807196b1ebd43b949be98319a114e15cab
ZHANG RT <4399.omi@gmail.com>
2015-08-04 23:25:06 +0800

    musicbox 0.1.6.5 (new formula)
    musicbox 0.1.6.5 (new formula)

---
+++ Library/Formula/musicbox.rb
@@ -0,0 +1,14 @@
+class MusicBox < Formula
+  desc "A sexy command line interface musicbox"
+  homepage "https://github.com/darknessomi/musicbox"
+  url "https://pypi.python.org/packages/source/N/NetEase-MusicBox/NetEase-MusicBox-0.1.6.5.tar.gz#md5=f731f7c94804a42d2acde2825dba37e1"
+  version "0.1.6.5"
+  sha256 "8383d773bd1d4f5e1f4b41897fe34e3dac25c47d3f1b59f1cef62c35ce14a07e"
+
+  depends_on :mpg123 
+
+  def install
+    system "sudo", "python", "setup.py", "install"
+  end
+
+end
