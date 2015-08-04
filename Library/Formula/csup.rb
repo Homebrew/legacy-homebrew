4995e599bd8a83896b43dbace9fe3810988a79e4
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-04 03:19:01 +0100

    csup: revision for openssl
    Linking against the defunct system OpenSSL on <10.11? Bad.
    Linking against the defunct system OpenSSL headers on 10.11? Fatal.
    
    Closes #42396.
    
    Closes #42435.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/csup.rb
+++ Library/Formula/csup.rb
@@ -3,7 +3,11 @@ class Csup < Formula
   homepage "https://bitbucket.org/mux/csup"
   url "https://bitbucket.org/mux/csup/get/REL_20120305.tar.gz"
   sha256 "6b9a8fa2d2e70d89b2780cbc3f93375915571497f59c77230d4233a27eef77ef"
+  revision 1
+
   head "https://bitbucket.org/mux/csup", :using => :hg
+
+  depends_on "openssl"
 
   def install
     system "make"
