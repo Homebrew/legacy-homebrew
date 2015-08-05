1f44de1efe609c013ac9b742239049c08fb84de6
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-05 17:21:57 +0100

    gtmess: revision for system openssl linking
    Closes #42503.

--- Library/Formula/gtmess.rb
+++ Library/Formula/gtmess.rb
@@ -3,10 +3,23 @@ class Gtmess < Formula
   homepage "http://gtmess.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/gtmess/gtmess/0.97/gtmess-0.97.tar.gz"
   sha256 "606379bb06fa70196e5336cbd421a69d7ebb4b27f93aa1dfd23a6420b3c6f5c6"
+  revision 1
+
+  head do
+    url "https://github.com/geotz/gtmess.git"
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "openssl"
 
   def install
+    system "autoreconf", "-fvi" if build.head?
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--with-ssl=#{Formula["openssl"].opt_prefix}"
     system "make", "install"
   end
 end
