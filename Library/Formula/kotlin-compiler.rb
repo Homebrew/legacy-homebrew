13d0534e238b00e84e0d0de35b2e7e0ef23a9d11
Ferenc Boldog <fboldog@happy-mbp.local>
2015-08-05 08:29:01 +0200

    kotlin-compiler 0.12.613
    Kotlin M12 bugfix update (0.12.613)
    
    Closes #42483.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/kotlin-compiler.rb
+++ Library/Formula/kotlin-compiler.rb
@@ -1,8 +1,8 @@
 class KotlinCompiler < Formula
   desc "Statically typed programming language for the JVM"
   homepage "http://kotlinlang.org/"
-  url "https://github.com/JetBrains/kotlin/releases/download/build-0.12.200/kotlin-compiler-0.12.200.zip"
-  sha256 "d14a74859698c2a88f004dfb7dd35d35234bc5b99f452694159f2db1ed5a1060"
+  url "https://github.com/JetBrains/kotlin/releases/download/build-0.12.613/kotlin-compiler-0.12.613.zip"
+  sha256 "e72b02661d8d995f51dec9c3d63b901d13a451c1d2a99faf7b9b9ab4e3ef87f5"
 
   bottle do
     cellar :any
