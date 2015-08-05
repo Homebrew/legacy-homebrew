51674a786071d8c4f158bbda33076d4efabc421c
謝致邦 <Yeking@Red54.com>
2015-08-06 00:29:22 +0800

    Update Library/Formula/waon.rb.

--- Library/Formula/waon.rb
+++ Library/Formula/waon.rb
@@ -1,8 +1,9 @@
 class Waon < Formula
   desc "Wave-to-notes transcriber"
-  homepage "http://waon.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/waon/waon/0.10/waon-0.10.tar.gz"
-  sha256 "e0a3ba2988351dd40fd60c14b69fa4139eb3ea207711fef365f37d0069d5d4cd"
+  homepage "http://kichiki.github.io/WaoN/"
+  url "https://github.com/kichiki/WaoN/archive/0a45b687add1cdbfdefc993554053a2153934e44.tar.gz"
+  version "2013-03-03"
+  sha256 "9e585a4ab38917599e4c376f21e9049c7c94e98892459b61efda866267f6376b"
 
   depends_on "fftw"
   depends_on "libsndfile"
