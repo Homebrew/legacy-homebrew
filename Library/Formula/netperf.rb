1d223efbbc301f6e61ada85a6d4e98203bf44454
Ahmed Kamal <email.ahmedkamal@googlemail.com>
2015-08-06 17:09:39 +0200

    netperf 2.7.0 with sha256sum

--- Library/Formula/netperf.rb
+++ Library/Formula/netperf.rb
@@ -4,7 +4,7 @@ class Netperf < Formula
   desc "Benchmarks performance of many different types of networking"
   homepage "http://netperf.org"
   url "ftp://ftp.netperf.org/netperf/netperf-2.7.0.tar.bz2"
-  sha1 "415ecd4877cf8e24cefe6943564e511e6f1e2215"
+  sha256 "842af17655835c8be7203808c3393e6cb327a8067f3ed1f1053eb78b4e40375a"
 
   def install
     system "./configure", "--disable-dependency-tracking",
