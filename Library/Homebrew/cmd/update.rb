1a4327064770c16b388a9eab328296000eb8c6b9
Xu Cheng <xucheng@me.com>
2015-08-06 23:49:37 +0800

    update: handle more exceptions
    Closes #42558.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Homebrew/cmd/update.rb
+++ Library/Homebrew/cmd/update.rb
@@ -206,7 +206,7 @@ class Updater
             new_version = formula.pkg_version
             old_version = FormulaVersions.new(formula).formula_at_revision(@initial_revision, &:pkg_version)
             next if new_version == old_version
-          rescue LoadError, FormulaUnavailableError => e
+          rescue FormulaUnavailableError, *FormulaVersions::IGNORED_EXCEPTIONS => e
             onoe e if ARGV.homebrew_developer?
           end
           map[:M] << file
