2a330c754bf3251f8262b52ba66c8fb902ad9b79
Ahmed Kamal <email.ahmedkamal@googlemail.com>
2015-08-06 17:04:42 +0200

    netperf 2.7.0

--- Library/Formula/netperf.rb
+++ Library/Formula/netperf.rb
@@ -3,8 +3,8 @@ require "formula"
 class Netperf < Formula
   desc "Benchmarks performance of many different types of networking"
   homepage "http://netperf.org"
-  url "ftp://ftp.netperf.org/netperf/netperf-2.6.0.tar.bz2"
-  sha1 "3e1be4e7c3f7a838c4d5c00c6d20a619b320bfef"
+  url "ftp://ftp.netperf.org/netperf/netperf-2.7.0.tar.bz2"
+  sha1 "415ecd4877cf8e24cefe6943564e511e6f1e2215"
 
   def install
     system "./configure", "--disable-dependency-tracking",
