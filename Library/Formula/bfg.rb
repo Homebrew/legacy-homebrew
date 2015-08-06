a0940f2d19bfab0e14f17a4ca0f0bd6a56085a2a
Roberto Tyley <roberto.tyley@gmail.com>
2015-08-06 09:50:13 +0100

    bfg 1.12.4
    Note that this increases the Java version requirement from 1.6 to 1.7 - https://github.com/rtyley/bfg-repo-cleaner/commit/8651ad26
    
    Closes #42541.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/bfg.rb
+++ Library/Formula/bfg.rb
@@ -1,12 +1,14 @@
 class Bfg < Formula
-  desc "Alternative to git-filter-branch, written in Scala"
+  desc "Removes large files or passwords from Git history like git-filter-branch does, but faster."
   homepage "https://rtyley.github.io/bfg-repo-cleaner/"
-  url "https://repo1.maven.org/maven2/com/madgag/bfg/1.12.3/bfg-1.12.3.jar"
-  sha256 "2e237631235cfb8cc2d49dabb986389851dca8a28a07083345274b443dfa4911"
+  url "https://repo1.maven.org/maven2/com/madgag/bfg/1.12.4/bfg-1.12.4.jar"
+  sha256 "41e01861a7021a73db28860575fdeacab48dbcdcd48e658cf93df5ca06484b21"
+
+  depends_on :java => "1.7+"
 
   def install
-    libexec.install "bfg-1.12.3.jar"
-    bin.write_jar_script libexec/"bfg-1.12.3.jar", "bfg"
+    libexec.install "bfg-1.12.4.jar"
+    bin.write_jar_script libexec/"bfg-1.12.4.jar", "bfg"
   end
 
   test do
