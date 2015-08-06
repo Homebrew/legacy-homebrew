80fcc1f3938be5ff3cb4ad8f977689d151cc1583
ZHANG RT <4399.omi@gmail.com>
2015-08-06 14:56:09 +0800

    del test do

--- Library/Formula/musicbox.rb
+++ Library/Formula/musicbox.rb
@@ -43,10 +43,5 @@ class MusicBox < Formula
     bin.install Dir["#{libexec}/bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
-
-
-  test do
-    system "#{bin}/musicbox", shell_output(bin/"musicbox ok")
-  end
   
 end
