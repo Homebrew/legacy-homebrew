40214a96f3ec0af17050a8dd7e41e7e20cf46d37
Alex Dunn <dunn.alex@gmail.com>
2015-08-06 15:32:45 -0700

    asn1c 0.9.27
    Closes #43960.

--- Library/Formula/asn1c.rb
+++ Library/Formula/asn1c.rb
@@ -1,8 +1,9 @@
 class Asn1c < Formula
   desc "Compile ASN.1 specifications into C source code"
   homepage "http://lionet.info/asn1c/blog/"
-  url "http://lionet.info/soft/asn1c-0.9.26.tar.gz"
-  sha256 "6ee33727d6fb81be5cec20acdb73e87ce387a53a3e689124855be888672eb833"
+  url "https://github.com/vlm/asn1c/releases/download/v0.9.27/asn1c-0.9.27.tar.gz"
+  mirror "http://lionet.info/soft/asn1c-0.9.27.tar.gz"
+  sha256 "025f64e1c27211b36e181350b52fde34ad23f4330fff96b2563ed3fda7b0db9e"
 
   bottle do
     revision 2
@@ -11,7 +12,15 @@ class Asn1c < Formula
     sha1 "705befca80634eaa5a1aa4cebe18a75a0d618b99" => :mountain_lion
   end
 
+  head do
+    url "https://github.com/vlm/asn1c.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
   def install
+    system "autoreconf", "-iv" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
