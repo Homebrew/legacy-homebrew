0493f1402d51edd43db90e5d95ac25d2f743273f
Ahmed Kamal <email.ahmedkamal@googlemail.com>
2015-08-06 17:04:42 +0200

    netperf 2.7.0
    Closes #42555.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/netperf.rb
+++ Library/Formula/netperf.rb
@@ -3,8 +3,8 @@ require "formula"
 class Netperf < Formula
   desc "Benchmarks performance of many different types of networking"
   homepage "http://netperf.org"
-  url "ftp://ftp.netperf.org/netperf/netperf-2.6.0.tar.bz2"
-  sha1 "3e1be4e7c3f7a838c4d5c00c6d20a619b320bfef"
+  url "ftp://ftp.netperf.org/netperf/netperf-2.7.0.tar.bz2"
+  sha256 "842af17655835c8be7203808c3393e6cb327a8067f3ed1f1053eb78b4e40375a"
 
   def install
     system "./configure", "--disable-dependency-tracking",
