557508b82b9f48ab9d0ecbab480c9e547210c2f8
Alex Dunn <dunn.alex@gmail.com>
2015-08-06 11:13:22 -0700

    aria2 1.19.0
    Closes #42572.

--- Library/Formula/aria2.rb
+++ Library/Formula/aria2.rb
@@ -1,8 +1,9 @@
 class Aria2 < Formula
   desc "Download with resuming and segmented downloading"
   homepage "http://aria2.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/aria2/stable/aria2-1.18.9/aria2-1.18.9.tar.bz2"
-  sha256 "e24bca91edbe8fff52615420085317ce22faec4a0a9c34843f86dbcc2a79c4b2"
+  url "https://downloads.sourceforge.net/project/aria2/stable/aria2-1.19.0/aria2-1.19.0.tar.bz2"
+  mirror "https://mirrors.kernel.org/debian/pool/main/a/aria2/aria2_1.19.0.orig.tar.bz2"
+  sha256 "ae2b6fce7a0974c9156415cccf2395cd258580ab34eec2b34a8e76120b7240ce"
 
   bottle do
     cellar :any
