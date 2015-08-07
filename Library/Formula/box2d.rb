be700922aa1bcfa91ae105751d91fc860be32fcf
Alex Dunn <dunn.alex@gmail.com>
2015-08-07 12:38:36 -0700

    box2d 2.3.1
    Closes #42628.

--- Library/Formula/box2d.rb
+++ Library/Formula/box2d.rb
@@ -1,18 +1,26 @@
 class Box2d < Formula
   desc "2D physics engine for games"
   homepage "http://www.box2d.org/"
-  url "https://box2d.googlecode.com/files/Box2D_v2.3.0.7z"
-  sha256 "2ebdb30863b7f5478e99b4425af210f8c32ef62faf1e0d2414c653072fff403d"
+  url "https://github.com/erincatto/Box2D/archive/v2.3.1.tar.gz"
+  sha256 "2c61505f03ef403b54cf0e510d83d6f567e37882ad79b5b2d486acbc7d5eedea"
+  head "https://github.com/erincatto/Box2D.git"
 
   depends_on "cmake" => :build
 
   def install
-    cd "Box2D_v#{version}/Box2D" do
+    cd "Box2D" do
       system "cmake", "-DBOX2D_INSTALL=ON",
                       "-DBOX2D_BUILD_SHARED=ON",
                       "-DBOX2D_BUILD_EXAMPLES=OFF",
                       *std_cmake_args
       system "make", "install"
     end
+    libexec.install "Box2D/HelloWorld"
+  end
+
+  test do
+    system ENV.cxx, "-L#{lib}", "-lbox2d",
+           libexec/"HelloWorld/HelloWorld.cpp", "-o", testpath/"test"
+    system "./test"
   end
 end
