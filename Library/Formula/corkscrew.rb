d2fd6782b706ff54ac41a60c1a1f5030d9b8a961
Xu Cheng <xucheng@me.com>
2015-08-07 22:25:17 +0800

    corkscrew: add test

--- Library/Formula/corkscrew.rb
+++ Library/Formula/corkscrew.rb
@@ -13,4 +13,27 @@ class Corkscrew < Formula
                           "--prefix=#{prefix}"
     system "make", "install"
   end
+
+  test do
+    require "open3"
+    require "webrick"
+    require "webrick/httpproxy"
+
+    pid = fork do
+      proxy = WEBrick::HTTPProxyServer.new :Port => 8080
+      proxy.start
+    end
+
+    sleep 1
+
+    begin
+      Open3.popen3("#{bin}/corkscrew 127.0.0.1 8080 www.google.com 80") do |stdin, stdout, _|
+        stdin.write "GET /index.html HTTP/1.1\r\n\r\n"
+        assert_match "HTTP/1.1", stdout.gets("\r\n\r\n")
+      end
+    ensure
+      Process.kill 9, pid
+      Process.wait pid
+    end
+  end
 end
