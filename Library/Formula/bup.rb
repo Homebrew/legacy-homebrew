95d8db8d75c3ae8ab70fab37edc09f12e8ed77e1
Alex Dunn <dunn.alex@gmail.com>
2015-08-07 13:50:02 -0700

    bup 0.27

--- Library/Formula/bup.rb
+++ Library/Formula/bup.rb
@@ -1,18 +1,10 @@
 class Bup < Formula
   desc "Backup tool"
   homepage "https://github.com/bup/bup"
+  url "https://github.com/bup/bup/archive/0.27.tar.gz"
+  sha256 "ae8a744a3415ce4766d1d896f3d48b9b2ae6167c7bc65d8d1a112f37b42720fb"
+
   head "https://github.com/bup/bup.git"
-
-  stable do
-    url "https://github.com/bup/bup/archive/0.26.tar.gz"
-    sha256 "8c25551fa723cfe5dcaaa671c5f0964d8caff22b068923df007a13169d8f015c"
-
-    # Fix compilation on 10.10
-    patch do
-      url "https://github.com/bup/bup/commit/75d089e7cdb7a7eb4d69c352f56dad5ad3aa1f97.diff"
-      sha256 "9a4615e91b7b2f43e5447e30aa4096f1b0bf65dc2081e7c08d852830ee217716"
-    end
-  end
 
   option "with-tests", "Run unit tests after compilation"
   option "with-pandoc", "Build and install the manpages"
