60b5d29069898d6aea817abf671656afc960455a
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-08 03:34:34 +0100

    csshx: update upstream links

--- Library/Formula/csshx.rb
+++ Library/Formula/csshx.rb
@@ -1,12 +1,26 @@
 class Csshx < Formula
   desc "Cluster ssh tool for Terminal.app"
-  homepage "https://code.google.com/p/csshx/"
+  homepage "https://github.com/brockgr/csshx"
   url "https://csshx.googlecode.com/files/csshX-0.74.tgz"
+  mirror "https://distfiles.macports.org/csshX/csshX-0.74.tgz"
   sha256 "eaa9e52727c8b28dedc87398ed33ffa2340d6d0f3ea9d261749c715cb7a0e9c8"
 
-  head "https://code.google.com/p/csshx/", :using => :git
+  head "https://github.com/brockgr/csshx.git"
 
   def install
     bin.install "csshX"
+  end
+
+  def caveats
+    s = ""
+
+    if ENV["TMUX"]
+      s << <<-EOS.undent
+        Csshx will fail to run under Tmux. See:
+          https://github.com/brockgr/csshx/issues/61
+      EOS
+    end
+
+    s
   end
 end
