9d90594350b25ff758321f07da851a11345d13e6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-08 04:03:03 +0100

    box2d: add 2.3.1 bottle.

--- Library/Formula/box2d.rb
+++ Library/Formula/box2d.rb
@@ -5,6 +5,13 @@ class Box2d < Formula
   sha256 "2c61505f03ef403b54cf0e510d83d6f567e37882ad79b5b2d486acbc7d5eedea"
   head "https://github.com/erincatto/Box2D.git"
 
+  bottle do
+    cellar :any
+    sha256 "1559be0d79e66e8074a051f36db5bbf21a9d556013803cce7e04184bd28028a9" => :yosemite
+    sha256 "40effd7d4952d37d15e7b34da4b9c207956b1d79bdd856f9f03307ecf52a5b3f" => :mavericks
+    sha256 "143949e28a0b74432015b56f6372682ce69cfb451ccd82a3f4b1fb1c69e24310" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
