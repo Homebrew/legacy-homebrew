dc65055fc7b4c7319b58ec350bd5b4651c6d0c8c
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-08 03:53:50 +0100

    dhcpdump: add mirror
    Closes #42475.

--- Library/Formula/dhcpdump.rb
+++ Library/Formula/dhcpdump.rb
@@ -2,6 +2,7 @@ class Dhcpdump < Formula
   desc "Monitor DHCP traffic for debugging purposes"
   homepage "http://www.mavetju.org"
   url "http://www.mavetju.org/download/dhcpdump-1.8.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/d/dhcpdump/dhcpdump_1.8.orig.tar.gz"
   sha256 "6d5eb9418162fb738bc56e4c1682ce7f7392dd96e568cc996e44c28de7f77190"
 
   def install
