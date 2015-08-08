962fff58a017d43ae9ed6742038b063608f055ee
Viktor Szakats <vszakats@users.noreply.github.com>
2015-08-08 10:56:24 +0200

    blink1: follow redirect, use https in homepage
    Closes #42650.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/blink1.rb
+++ Library/Formula/blink1.rb
@@ -1,6 +1,6 @@
 class Blink1 < Formula
   desc "Control blink(1) indicator light"
-  homepage "http://thingm.com/products/blink-1.html"
+  homepage "https://blink1.thingm.com/"
   url "https://github.com/todbot/blink1/archive/v1.97.tar.gz"
   sha256 "974722b466249ee47ec27659b160d2e4fd0c2bc3732d009f180f8ffb53dc5d92"
   head "https://github.com/todbot/blink1.git"
