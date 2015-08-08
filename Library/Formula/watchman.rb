8ba5223211c729287ffa4df0ac0001fa21e388be
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-08 03:06:35 +0100

    watchman: nits

--- Library/Formula/watchman.rb
+++ Library/Formula/watchman.rb
@@ -1,10 +1,11 @@
 class Watchman < Formula
   desc "Watch files and take action when they change"
   homepage "https://github.com/facebook/watchman"
-  head "https://github.com/facebook/watchman.git"
-  version "3.7.0"
   url "https://github.com/facebook/watchman/archive/v3.7.0-brew.tar.gz"
+  version "3.7.0"
   sha256 "55a3ea1ee3990a5e5c11f1a37ad5bafbb63e8002f48f449c083e598f6f332154"
+
+  head "https://github.com/facebook/watchman.git"
 
   bottle do
     sha256 "988bc7562fe67087f281935d2b064c172161b036a22539c50ec04b215235749c" => :yosemite
@@ -27,9 +28,12 @@ class Watchman < Formula
   end
 
   test do
+    # Currently fails under HOMEBREW_SANDBOX: Operation not permitted
+    # "Failed to open /path/to/LaunchAgents/plist for write"
     system "#{bin}/watchman", "shutdown-server"
     system "#{bin}/watchman", "watch", testpath
-    list = `#{bin}/watchman watch-list`
+
+    list = shell_output("#{bin}/watchman watch-list")
     if list.index(testpath).nil?
       raise "failed to watch tmpdir"
     end
