24a4f5c0c475e326744fe79206e2d66668ebf12f
Alex Dunn <dunn.alex@gmail.com>
2015-08-08 11:31:45 -0700

    aws-cloudsearch: remove lib path rewrite
    Fixes #42210.
    
    Closes #43198.

--- Library/Formula/aws-cloudsearch.rb
+++ Library/Formula/aws-cloudsearch.rb
@@ -4,18 +4,18 @@ class AwsCloudsearch < Formula
   url "https://s3.amazonaws.com/amazon-cloudsearch-data/cloud-search-tools-v2-2.0.1.0-2014.10.27.tar.gz"
   version "2.0.1.0-2014.10.27"
   sha256 "882a6442172957b27c0b3cd1f271531112092d4d227b528ce912b2e7ea886d51"
+  revision 1
 
   depends_on :java => "1.7+"
 
   def install
-    inreplace "bin/cs-cmd", 'LIBDIR="${CS_HOME}/lib"', 'LIBDIR="${CS_HOME}/jars/lib"'
-    (libexec/"jars").install "lib"
-    libexec.install %w[conf help third-party]
+    libexec.install %w[conf help third-party lib]
     bin.install Dir["bin/*"]
     bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.7+").merge(:CS_HOME => libexec))
   end
 
   test do
-    system "#{bin}/cs-configure-from-batches", "-h"
+    system bin/"cs-configure-from-batches", "-h"
+    system bin/"cs-import-documents", "-h"
   end
 end
