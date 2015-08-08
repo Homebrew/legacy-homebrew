02753e7bcba21fde6854b3abc194bae610291f65
Alex Dunn <dunn.alex@gmail.com>
2015-08-08 10:15:10 -0700

    activemq: remove broken plist
    Closes #39477.
    Closes #42658.

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -13,30 +13,6 @@ class Activemq < Formula
     (bin/"activemq-admin").write_env_script libexec/"bin/activemq-admin", Language::Java.java_home_env("1.6+")
   end
 
-  plist_options :manual => "activemq start"
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>WorkingDirectory</key>
-        <string>#{libexec}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/activemq</string>
-          <string>start</string>
-        </array>
-      </dict>
-    </plist>
-    EOS
-  end
-
   test do
     system "#{bin}/activemq-admin", "browse", "-h"
   end
