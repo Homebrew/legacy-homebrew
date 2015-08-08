42fdbcae8d83f362de7868dd7d5111dfb1c7a684
Kyungdahm Yun <tomyun@gmail.com>
2015-08-07 18:31:41 -0700

    sdl_ttf: add patch for broken font rendering

--- Library/Formula/sdl_ttf.rb
+++ Library/Formula/sdl_ttf.rb
@@ -18,8 +18,16 @@ class SdlTtf < Formula
   depends_on "sdl"
   depends_on "freetype"
 
+  # Fix broken TTF_RenderGlyph_Shaded()
+  # https://bugzilla.libsdl.org/show_bug.cgi?id=1433
+  patch do
+    url "https://gist.githubusercontent.com/tomyun/a8d2193b6e18218217c4/raw/8292c48e751c6a9939db89553d01445d801420dd/sdl_ttf-fix-1433.diff"
+    sha256 "4c2e38bb764a23bc48ae917b3abf60afa0dc67f8700e7682901bf9b03c15be5f"
+  end
+
   def install
     ENV.universal_binary if build.universal?
+    inreplace "SDL_ttf.pc.in", "@prefix@", HOMEBREW_PREFIX
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
