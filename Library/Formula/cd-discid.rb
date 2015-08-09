75d8cbc8ac5d2d3f7cbdae931d1b5c81a479b7e9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-09 21:14:42 +0100

    cd-discid: add 1.4 bottle.

--- Library/Formula/cd-discid.rb
+++ Library/Formula/cd-discid.rb
@@ -6,6 +6,13 @@ class CdDiscid < Formula
   sha256 "ffd68cd406309e764be6af4d5cbcc309e132c13f3597c6a4570a1f218edd2c63"
   head "https://github.com/taem/cd-discid.git"
 
+  bottle do
+    cellar :any
+    sha256 "0647971b092ef9505401fef5987a99ff3a9e4bc2956fc167899a85e9361c335b" => :yosemite
+    sha256 "d16b9fb0eb3820f7531ce223e0e0c18ad85bb6e24020319151afff09e8d2e80f" => :mavericks
+    sha256 "b12730d1530c45bce73cb6ae8cb7337c6063dea9d1fe50981111dd843269c1f1" => :mountain_lion
+  end
+
   def install
     system "make", "CC=#{ENV.cc}"
     bin.install "cd-discid"
