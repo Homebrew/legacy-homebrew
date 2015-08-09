6eb43b50b0d4dc7b2a2b5207ea8b32982327ee66
Alex Dunn <dunn.alex@gmail.com>
2015-08-09 13:56:55 -0700

    chinadns-c 1.3.2
    Closes #42709.

--- Library/Formula/chinadns-c.rb
+++ Library/Formula/chinadns-c.rb
@@ -1,8 +1,8 @@
 class ChinadnsC < Formula
   desc "Port of ChinaDNS to C: fix irregularities with DNS in China"
   homepage "https://github.com/clowwindy/ChinaDNS-C"
-  url "https://github.com/clowwindy/ChinaDNS/releases/download/1.3.1/chinadns-1.3.1.tar.gz"
-  sha256 "0e72bc2b9f45f5ed3ef871ae52f228dce9aba1f7cb544d261a3999ebfec1cc8e"
+  url "https://github.com/clowwindy/ChinaDNS/releases/download/1.3.2/chinadns-1.3.2.tar.gz"
+  sha256 "abfd433e98ac0f31b8a4bd725d369795181b0b6e8d1b29142f1bb3b73bbc7230"
 
   bottle do
     cellar :any
@@ -11,7 +11,14 @@ class ChinadnsC < Formula
     sha256 "1c6594b10d571de1d838869f684fef343c6da4431cfc1294e0950ee19f823f00" => :mountain_lion
   end
 
+  head do
+    url "https://github.com/clowwindy/ChinaDNS.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
+
   def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
