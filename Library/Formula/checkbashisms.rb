71d55047ca103b5c10c635897674fe2d2e9b08b6
Alex Dunn <dunn.alex@gmail.com>
2015-08-09 14:36:24 -0700

    checkbashisms 2.15.8
    Closes #42712.

--- Library/Formula/checkbashisms.rb
+++ Library/Formula/checkbashisms.rb
@@ -1,9 +1,10 @@
 class Checkbashisms < Formula
   desc "Checks for bashisms in shell scripts"
-  homepage "http://manpages.ubuntu.com/manpages/natty/man1/checkbashisms.1.html"
-  # Get upgrades at https://launchpad.net/ubuntu/+source/devscripts/
-  url "https://launchpad.net/ubuntu/+archive/primary/+files/devscripts_2.13.4.tar.xz"
-  sha256 "6575a71023d7de62986b33c2db47074fe2be96023b51e427ad653ce6a0e73527"
+  homepage "https://launchpad.net/ubuntu/+source/devscripts/"
+  url "https://launchpad.net/ubuntu/+archive/primary/+files/devscripts_2.15.8.tar.xz"
+  sha256 "7d2df363f9a725096d281321e0c2a41e1613e645955c3956a78bd91715bc87ff"
+
+  head "lp:ubuntu/devscripts", :using => :bzr
 
   bottle do
     cellar :any
@@ -20,6 +21,16 @@ class Checkbashisms < Formula
   end
 
   test do
-    system "#{bin}/checkbashisms --version | grep #{version}"
+    (testpath/"test.sh").write <<-EOS.undent
+      #!/bin/sh
+
+      if [[ "home == brew" ]]; then
+        echo "dog"
+      fi
+    EOS
+    expected = <<-EOS.undent
+      (alternative test command ([[ foo ]] should be [ foo ])):
+    EOS
+    assert_match expected, shell_output("#{bin}/checkbashisms #{testpath}/test.sh 2>&1", 1)
   end
 end
