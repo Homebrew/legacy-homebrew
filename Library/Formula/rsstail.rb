5f20463786fe50ed5e7c763673284084c90341a3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-09 03:30:43 +0100

    rsstail: update 2.0 bottle.

--- Library/Formula/rsstail.rb
+++ Library/Formula/rsstail.rb
@@ -8,9 +8,9 @@ class Rsstail < Formula
 
   bottle do
     cellar :any
-    sha256 "0d18005ca03757ba45acb78737587eeb9abbe31e5346b0a0738dbc63951a3311" => :yosemite
-    sha256 "1ec19466e92bbeae9ed3b9e4f9ea9af18eabfd7c8c86e74eda409271028d1b3e" => :mavericks
-    sha256 "b17a2996711143d24d0a85ecfe26592b4ad9927e2f42e22af7289abe4e5bc4f5" => :mountain_lion
+    sha256 "8ed1aaca7c992bf95097a85fef0afb82601056217e0a5992c03276be407cedfa" => :yosemite
+    sha256 "40d3d3b8f001e4be990b5280faa46cc577d8f77c4b82445759bb2a017ebb8d56" => :mavericks
+    sha256 "35d9f648c3081a208b555739b80fd26002a0182a3c8571e836e9f7152c38c76f" => :mountain_lion
   end
 
   depends_on "libmrss"
