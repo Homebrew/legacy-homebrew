2165341975d1130f8d11a2e5a3f118dd1ebaad65
Andrew Janke <andrew@apjanke.net>
2015-08-09 05:01:05 -0400

    Add conflict: go-app-engine-32, go-app-engine-64, google-app-engine

--- Library/Formula/go-app-engine-64.rb
+++ Library/Formula/go-app-engine-64.rb
@@ -4,6 +4,9 @@ class GoAppEngine64 < Formula
   url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.24.zip"
   sha256 "6a072ef4cdb0cc42d0b4a32494cbe1500d702208e345cef196b5878917df32f8"
 
+  conflicts_with "go-app-engine-32", :because => "multiple conflicting files"
+  conflicts_with "google-app-engine", :because => "multiple conflicting files"
+
   bottle do
     cellar :any
     sha256 "570879ba2f65c72dd6810f66634acaf4ab26490a1bba56abb7dc9af64b0a9246" => :yosemite
