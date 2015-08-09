1677f94cd3e3473ffe8f38cbed93de8120943ef9
Alex Dunn <dunn.alex@gmail.com>
2015-08-08 20:51:41 -0700

    cd-discid 1.4
    Closes #42676.

--- Library/Formula/cd-discid.rb
+++ Library/Formula/cd-discid.rb
@@ -1,18 +1,18 @@
 class CdDiscid < Formula
   desc "Read CD and get CDDB discid information"
   homepage "http://linukz.org/cd-discid.shtml"
-  url "http://linukz.org/download/cd-discid-1.1.tar.gz"
-  sha256 "5d28ea26947ea364edfcd1af617d369fad92cb7b1944faa971d66cfbff627d69"
-
-  patch :p0 do
-    url "https://trac.macports.org/export/70630/trunk/dports/audio/cd-discid/files/patch-cd-discid.c.diff"
-    sha256 "45719b39bf70ee6d247027102dfd76ccdda10cb40c047ce25e5e0d77218cbf5b"
-  end
+  url "http://linukz.org/download/cd-discid-1.4.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/c/cd-discid/cd-discid_1.4.orig.tar.gz"
+  sha256 "ffd68cd406309e764be6af4d5cbcc309e132c13f3597c6a4570a1f218edd2c63"
+  head "https://github.com/taem/cd-discid.git"
 
   def install
-    system "make", "prefix=#{prefix}",
-                   "CC=#{ENV.cc}",
-                   "CFLAGS=#{ENV.cflags}",
-                   "install"
+    system "make", "CC=#{ENV.cc}"
+    bin.install "cd-discid"
+    man1.install "cd-discid.1"
+  end
+
+  test do
+    assert_equal "cd-discid #{version}.", shell_output("#{bin}/cd-discid --version 2>&1").chomp
   end
 end
