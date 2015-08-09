ba988414333e42dc33eacf466c26a8e7d5c48d39
Alex Dunn <dunn.alex@gmail.com>
2015-08-08 19:11:37 -0700

    rsstail 2.0

--- Library/Formula/rsstail.rb
+++ Library/Formula/rsstail.rb
@@ -1,8 +1,8 @@
 class Rsstail < Formula
   desc "Monitors an RSS feed and emits new entries when detected"
   homepage "http://www.vanheusden.com/rsstail/"
-  url "http://www.vanheusden.com/rsstail/rsstail-1.8.tgz"
-  sha256 "19284f3eca4bfa649f53848e19e6ee134bce17ccf2a22919cc8c600684877801"
+  url "http://www.vanheusden.com/rsstail/rsstail-2.0.tgz"
+  sha256 "647537197fb9fb72b08e04710d462ad9314a6335c0a66fb779fe9d822c19ee2a"
 
   head "https://github.com/flok99/rsstail.git"
 
@@ -22,9 +22,7 @@ class Rsstail < Formula
   end
 
   test do
-    actual = shell_output(
-      "#{bin}/rsstail -1u http://feed.nashownotes.com/rss.xml"
-    )
-    assert_match /^Title: NA-\d\d\d-\d\d\d\d-\d\d-\d\d$/, actual
+    assert_match(/^Title: NA-\d\d\d-\d\d\d\d-\d\d-\d\d$/,
+                 shell_output("#{bin}/rsstail -1u http://feed.nashownotes.com/rss.xml"))
   end
 end
