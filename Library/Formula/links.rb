4e7fc606d7ad51c4f73d81f263be02543b46e454
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-09 20:20:09 +0100

    links 2.10
    Closes #42698.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/links.rb
+++ Library/Formula/links.rb
@@ -1,9 +1,9 @@
 class Links < Formula
   desc "Lynx-like WWW browser that supports tables, menus, etc."
   homepage "http://links.twibright.com/"
-  url "http://links.twibright.com/download/links-2.9.tar.bz2"
-  mirror "https://mirrors.kernel.org/debian/pool/main/l/links2/links2_2.9.orig.tar.bz2"
-  sha256 "4360ead54d5f41da16b2f4c0033317ca775f40c1e658a29e9e7f8ce4bc23878f"
+  url "http://links.twibright.com/download/links-2.10.tar.bz2"
+  mirror "https://mirrors.kernel.org/debian/pool/main/l/links2/links2_2.10.orig.tar.bz2"
+  sha256 "840e0b0cc804a58567bdfed1fa8012c9ea2d34a853d4e90f857786ab4b791d53"
 
   bottle do
     cellar :any
@@ -13,9 +13,11 @@ class Links < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "openssl"
+  depends_on "openssl" => :recommended
+  depends_on "libressl" => :optional
   depends_on "libtiff" => :optional
   depends_on "jpeg" => :optional
+  depends_on "librsvg" => :optional
   depends_on :x11 => :optional
 
   def install
@@ -24,18 +26,25 @@ class Links < Formula
       --disable-dependency-tracking
       --prefix=#{prefix}
       --mandir=#{man}
-      --with-ssl=#{Formula["openssl"].opt_prefix}
     ]
+
+    if build.with? "libressl"
+      args << "--with-ssl=#{Formula["libressl"].opt_prefix}"
+    else
+      args << "--with-ssl=#{Formula["openssl"].opt_prefix}"
+    end
 
     args << "--enable-graphics" if build.with? "x11"
     args << "--without-libtiff" if build.without? "libtiff"
     args << "--without-libjpeg" if build.without? "jpeg"
+    args << "--without-librsvg" if build.without? "librsvg"
 
     system "./configure", *args
     system "make", "install"
+    doc.install Dir["doc/*"]
   end
 
   test do
-    system bin/"links", "https://duckduckgo.com"
+    system bin/"links", "-dump", "https://duckduckgo.com"
   end
 end
