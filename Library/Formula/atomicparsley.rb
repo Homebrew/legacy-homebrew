a2f4ea2c1053d9fdf14586708a587f16f042a66e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-10 20:50:12 +0100

    atomicparsley: add 0.9.6 bottle.

--- Library/Formula/atomicparsley.rb
+++ Library/Formula/atomicparsley.rb
@@ -4,6 +4,13 @@ class Atomicparsley < Formula
   url "https://bitbucket.org/dinkypumpkin/atomicparsley/downloads/atomicparsley-0.9.6.tar.bz2"
   sha256 "49187a5215520be4f732977657b88b2cf9203998299f238067ce38f948941562"
 
+  bottle do
+    cellar :any
+    sha256 "5399733312c1c3731707421c8a0bb831a7885a0f077dc8bca87304221d748c0b" => :yosemite
+    sha256 "8cc6e77f10ebf9e171354fe0a222fa205a26a1cb52b490fddd4a32aa554746f5" => :mavericks
+    sha256 "faf6a14d51e5f5eb9908d038891cedb7658ccb2b1cc3af3e390c6ee10916355b" => :mountain_lion
+  end
+
   head "https://bitbucket.org/wez/atomicparsley", :using => :hg
 
   depends_on "autoconf" => :build
