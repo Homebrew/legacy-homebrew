1ca16c303ca53ea3bd7778617c6ae2726c7eae43
Viktor Szakats <vszakats@users.noreply.github.com>
2015-08-10 22:36:50 +0200

    cronolog: use https homepage

--- Library/Formula/cronolog.rb
+++ Library/Formula/cronolog.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Cronolog < Formula
   desc "Web log rotation"
-  homepage "http://web.archive.org/web/20140209202032/http://cronolog.org/"
+  homepage "https://web.archive.org/web/20140209202032/http://cronolog.org/"
   url "https://fossies.org/linux/www/old/cronolog-1.6.2.tar.gz"
   sha256 "65e91607643e5aa5b336f17636fa474eb6669acc89288e72feb2f54a27edb88e"
 
