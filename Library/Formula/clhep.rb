a9117559c0de122531fc339854760f215c0f7392
Alex Dunn <dunn.alex@gmail.com>
2015-08-09 22:21:39 -0700

    clhep 2.2.0.8
    Closes #42726.

--- Library/Formula/clhep.rb
+++ Library/Formula/clhep.rb
@@ -1,8 +1,8 @@
 class Clhep < Formula
   desc "Class Library for High Energy Physics"
   homepage "http://proj-clhep.web.cern.ch/proj-clhep/"
-  url "http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.2.0.4.tgz"
-  sha256 "9bf7fcd9892313c8d1436bc4a4a285a016c4f8e81e1fc65bdf6783207ae57550"
+  url "http://proj-clhep.web.cern.ch/proj-clhep/DISTRIBUTION/tarFiles/clhep-2.2.0.8.tgz"
+  sha256 "2be2a0386967720df8fc06e16ea0ccaf97822425b2f5e0d4919ba00ca057e892"
 
   bottle do
     cellar :any
@@ -11,12 +11,45 @@ class Clhep < Formula
     sha1 "e77d0e5f516cb41ac061e1050c8f37d0fb65b796" => :mountain_lion
   end
 
+  head do
+    url "http://git.cern.ch/pub/CLHEP", :using => :git
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
+
   depends_on "cmake" => :build
 
   def install
-    mkdir "clhep-build" do
-      system "cmake", "../CLHEP", *std_cmake_args
+    # CLHEP is super fussy and doesn't allow source tree builds
+    dir = Dir.mktmpdir
+    cd dir do
+      args = std_cmake_args
+      if build.stable?
+        args << "#{buildpath}/CLHEP"
+      else
+        args << "#{buildpath}"
+      end
+      system "cmake", *args
       system "make", "install"
     end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <iostream>
+      #include <Vector/ThreeVector.h>
+
+      int main() {
+        CLHEP::Hep3Vector aVec(1, 2, 3);
+        std::cout << "r: " << aVec.mag();
+        std::cout << " phi: " << aVec.phi();
+        std::cout << " cos(theta): " << aVec.cosTheta() << std::endl;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-L#{lib}", "-lCLHEP", "-I#{include}/CLHEP",
+           testpath/"test.cpp", "-o", "test"
+    assert_equal "r: 3.74166 phi: 1.10715 cos(theta): 0.801784",
+                 shell_output("./test").chomp
   end
 end
