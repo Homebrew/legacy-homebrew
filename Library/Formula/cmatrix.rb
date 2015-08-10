53352084a0f703fc26c9091fa98b71edfb29944b
Alex Dunn <dunn.alex@gmail.com>
2015-08-09 23:07:35 -0700

    cmatrix: add mirror

--- Library/Formula/cmatrix.rb
+++ Library/Formula/cmatrix.rb
@@ -2,6 +2,7 @@ class Cmatrix < Formula
   desc "Console Matrix"
   homepage "http://www.asty.org/cmatrix/"
   url "http://www.asty.org/cmatrix/dist/cmatrix-1.2a.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/c/cmatrix/cmatrix_1.2a.orig.tar.gz"
   sha256 "1fa6e6caea254b6fe70a492efddc1b40ad7ccb950a5adfd80df75b640577064c"
 
   bottle do
