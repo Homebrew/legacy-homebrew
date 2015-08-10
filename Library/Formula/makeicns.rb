abcc103b8cb2d7af40dc6447628893c6c65d09b2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-10 20:50:12 +0100

    makeicns: add 1.4.10a bottle.

--- Library/Formula/makeicns.rb
+++ Library/Formula/makeicns.rb
@@ -3,6 +3,13 @@ class Makeicns < Formula
   homepage "https://bitbucket.org/mkae/makeicns"
   url "https://bitbucket.org/mkae/makeicns/downloads/makeicns-1.4.10a.tar.bz2"
   sha256 "10e44b8d84cb33ed8d92b9c2cfa42f46514586d2ec11ae9832683b69996ddeb8"
+  bottle do
+    cellar :any
+    sha256 "40c3d4befe2d4625d7013ea40f307b4f5b26e122a6dad51706a25bb22734f075" => :yosemite
+    sha256 "8c54ce9e5f819dda4eb274f8bf8a22d49e1d0086e33300f236840acf1a46837f" => :mavericks
+    sha256 "dfffe46a25b846de31dc220c279a628b719a3b353aa220ed31189bc8ce5da4b2" => :mountain_lion
+  end
+
   head "https://bitbucket.org/mkae/makeicns", :using => :hg
 
   patch :p0 do
