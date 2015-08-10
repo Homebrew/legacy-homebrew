98bfeb482f87bb503d3a593a254fe39ff1b58111
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-10 20:50:12 +0100

    ode: update 0.13.1 bottle.

--- Library/Formula/ode.rb
+++ Library/Formula/ode.rb
@@ -6,9 +6,10 @@ class Ode < Formula
 
   bottle do
     cellar :any
-    sha1 "f4c142dc1276434e7482955e6932182065c9131c" => :yosemite
-    sha1 "f69f17cd9bf1eed526959049c41cf3f28caed0da" => :mavericks
-    sha1 "ca4743cc4e6cdce4ada98187f139c3e2acb4facb" => :mountain_lion
+    revision 1
+    sha256 "ffc8af301c91530661b4d83bba10af79b51a1204ee3111bcb1d73714a9073137" => :yosemite
+    sha256 "3daad0d75cba00147966ed9f5ff27c825f25142c664d41341481943eb70f5ac4" => :mavericks
+    sha256 "1956cbf2602b3a8ab1a1bc770a6bfa005eb9f484362b543176f9477ce3bd797e" => :mountain_lion
   end
 
   head do
