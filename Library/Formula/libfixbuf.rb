73ab8ecf67b35c33e06f67cc4b3dea5a4f7ac293
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-10 23:17:51 +0100

    libfixbuf: update 1.7.0 bottle.

--- Library/Formula/libfixbuf.rb
+++ Library/Formula/libfixbuf.rb
@@ -6,9 +6,9 @@ class Libfixbuf < Formula
 
   bottle do
     cellar :any
-    sha1 "2984798716e4bea8aaeec7e7db201a3867b435f1" => :yosemite
-    sha1 "5da62db9b86f601cf987a8972925dcbbdcc2d6c7" => :mavericks
-    sha1 "694b147a1d8563d56602f44e1f02923b84238d2d" => :mountain_lion
+    sha256 "3711773aba1acc3212b07ba5029e6368a7f22306609da9f7e9f3fae0e21a48dc" => :yosemite
+    sha256 "e67dbd4fa9c4f77f840adfeb0e85d300a380429f843f7d82d1e7b1b902b3437a" => :mavericks
+    sha256 "5e4ec8f9f0068350f26b8a578621e65506adac352d929d0b3bf007aa7f2c9b08" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
