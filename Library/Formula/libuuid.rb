43215288f4442256fccdf59c628079d23ae0dc69
Shaun Jackman <sjackman@gmail.com>
2015-08-09 21:17:18 -0700

    libuuid 1.0.3: Add tag linuxbrew

--- Library/Formula/libuuid.rb
+++ Library/Formula/libuuid.rb
@@ -1,7 +1,9 @@
 class Libuuid < Formula
   homepage "https://sourceforge.net/projects/libuuid/"
+  # tag "linuxbrew"
+
   url "https://downloads.sourceforge.net/project/libuuid/libuuid-1.0.3.tar.gz"
-  sha1 "46eaedb875ae6e63677b51ec583656199241d597"
+  sha256 "46af3275291091009ad7f1b899de3d0cea0252737550e7919d17237997db5644"
 
   conflicts_with "ossp-uuid", :because => "both install lib/libuuid.a"
 
