a0abb562b932ba3dd823e5d09cb087d453649a3d
Michele Venturi <dardo82@gmail.com>
2015-08-10 08:11:48 +0200

    chrome-cli: add support for chromium
    See https://github.com/Homebrew/homebrew/issues/42349 for more info.
    
    Closes #42731.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/chrome-cli.rb
+++ Library/Formula/chrome-cli.rb
@@ -22,11 +22,18 @@ class ChromeCli < Formula
     bin.install "build/Release/chrome-cli"
 
     # Canary builds; see:
-    # https://github.com/prasmussen/chrome-cli/issues/15#issuecomment-35202217
+    # https://github.com/prasmussen/chrome-cli/issues/15
     rm_rf "build"
     inreplace "chrome-cli/App.m", "com.google.Chrome", "com.google.Chrome.canary"
     xcodebuild "SDKROOT=", "SYMROOT=build"
     bin.install "build/Release/chrome-cli" => "chrome-canary-cli"
+
+    # Chromium builds; see:
+    # https://github.com/prasmussen/chrome-cli/issues/31
+    rm_rf "build"
+    inreplace "chrome-cli/App.m", "com.google.Chrome", "org.Chromium.chromium"
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/chrome-cli" => "chromium-cli"
   end
 
   test do
