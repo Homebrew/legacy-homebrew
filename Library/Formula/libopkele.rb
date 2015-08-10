7582872cbb2091b6e9ac94d5ba071829928770c3
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-10 01:13:29 +0100

    libopkele: revision for system openssl linking
    Closes #42684
    
    Closes #42718.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libopkele.rb
+++ Library/Formula/libopkele.rb
@@ -1,11 +1,13 @@
 class Libopkele < Formula
   desc "C++ implementation of OpenID decentralized identity system"
   homepage "http://kin.klever.net/libopkele/"
+  revision 1
 
   stable do
     url "http://kin.klever.net/dist/libopkele-2.0.4.tar.bz2"
     sha256 "102e22431e4ec6f1f0baacb6b1b036476f5e5a83400f2174807a090a14f4dc67"
 
+    # Fix argument-lookup failure on gcc 4.7.1
     patch do
       url "https://github.com/hacker/libopkele/commit/9ff6244998b0d41e71f7cc7351403ad590e990e4.diff"
       sha256 "381b5533a2704a96f0be9ddec16afbc2b853240084168ff40c04dcb34417b9a3"
@@ -29,10 +31,14 @@ class Libopkele < Formula
 
   depends_on "pkg-config" => :build
   depends_on "doxygen" => :build if build.with? "docs"
+  depends_on "openssl"
+
+  # It rejects the tr1/memory that ships on 10.9 & above
+  # and refuses to compile. It can use Boost, per configure.
+  depends_on "boost" if MacOS.version > :mountain_lion
 
   def install
     system "./autogen.bash" if build.head?
-
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
