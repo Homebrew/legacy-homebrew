4350de2e048a200204d3a2afdacdaead71aa775d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-10 14:25:51 +0100

    natalie: add 0.2 bottle.

--- Library/Formula/natalie.rb
+++ Library/Formula/natalie.rb
@@ -5,6 +5,11 @@ class Natalie < Formula
   sha256 "a4dd246db70e8818ecde34df68a60d8bf15c51465b365f466e606f74e41714e9"
   head "https://github.com/krzyzanowskim/Natalie.git"
 
+  bottle do
+    cellar :any
+    sha256 "4524f95293b7d838886370600f7350cfec1796e06bac2e67c3199f6fc73f9bb2" => :yosemite
+  end
+
   depends_on :xcode => "6.3"
 
   def install
