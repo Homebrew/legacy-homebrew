16cd3f9979f6ccc60f92eaf69d74754eb8da8024
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-11 07:19:12 +0100

    imessage-ruby: add 0.2.0 bottle.

--- Library/Formula/imessage-ruby.rb
+++ Library/Formula/imessage-ruby.rb
@@ -5,6 +5,12 @@ class ImessageRuby < Formula
   sha256 "5d67df55a53d0fa657ea439c240a3b322dd0b83a102fa881915569f44431eb00"
   head "https://github.com/linjunpop/imessage.git"
 
+  bottle do
+    cellar :any
+    sha256 "ea733fabe5eb0fa1800ec04070586edd6f77de13b22f48064de08d8e8c1b4bd0" => :yosemite
+    sha256 "b88419c5ee493a9d96bde86a81ec88866315f41ab91f19eef0402eeab5491d08" => :mavericks
+  end
+
   depends_on :macos => :mavericks
 
   def install
