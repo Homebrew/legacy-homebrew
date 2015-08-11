8d72bb7350512c46d39ec51b0d8417d77ccf9e07
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-11 18:50:40 +0100

    scrypt: add test

--- Library/Formula/scrypt.rb
+++ Library/Formula/scrypt.rb
@@ -17,4 +17,23 @@ class Scrypt < Formula
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.sh").write <<-EOS.undent
+      #!/usr/bin/expect -f
+      set timeout -1
+      spawn #{bin}/scrypt enc homebrew.txt homebrew.txt.enc
+      expect -exact "Please enter passphrase: "
+      send -- "Testing\n"
+      expect -exact "\r
+      Please confirm passphrase: "
+      send -- "Testing\n"
+      expect eof
+    EOS
+    chmod 0755, testpath/"test.sh"
+    touch "homebrew.txt"
+
+    system "./test.sh"
+    assert File.exist?("homebrew.txt.enc")
+  end
 end
