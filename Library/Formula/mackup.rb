f7a451c45d12e0c5afb7599363d5615d0a47fbef
Corey Quinn <corey@sequestered.net>
2015-08-11 13:54:56 -0700

    mackup 0.8.9
    Upstream bump to 0.8.9
    
    Closes #42825.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mackup.rb
+++ Library/Formula/mackup.rb
@@ -1,8 +1,8 @@
 class Mackup < Formula
   desc "Keep your Mac's application settings in sync"
   homepage "https://github.com/lra/mackup"
-  url "https://pypi.python.org/packages/source/m/mackup/mackup-0.8.8.tar.gz"
-  sha256 "26a71b56e68690a64a9a114012411e3ca50560f5af661fbe8ccc944ac70441de"
+  url "https://github.com/lra/mackup/archive/0.8.9.tar.gz"
+  sha256 "9c8a9fe64446256cf86a116296105e2009e8e5c9421512962219690af7372741"
 
   head "https://github.com/lra/mackup.git"
 
