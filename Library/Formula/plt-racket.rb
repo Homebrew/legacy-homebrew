7f31123f8578422f2563173b75ebdf246521ca24
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-11 09:26:53 +0100

    plt-racket: update 6.2.1 bottle.

--- Library/Formula/plt-racket.rb
+++ Library/Formula/plt-racket.rb
@@ -6,9 +6,9 @@ class PltRacket < Formula
   version "6.2.1"
 
   bottle do
-    sha256 "e8817da253500c3b51bf818059cfb4174252a939433b1755f965fd06f381a7c2" => :yosemite
-    sha256 "e775405462072cf43df41bc70b2faad9d065f4bb095bf21acd2fbba64be78777" => :mavericks
-    sha256 "165a42022c0543bfcc05954f62315b419efdc789e29a5c6e6aec14af52ee85b2" => :mountain_lion
+    sha256 "80af5793e7bfa204023d9151e7cf6094ab9be9fa593287b0d6f0b66d9667baae" => :yosemite
+    sha256 "a416f438831e81834a24a378bdd9f65fc3c3876bf67e2db753b88c08a359da20" => :mavericks
+    sha256 "066229247a0787b7a05f610713da183b97253a44a0f40fc1cd4e15e6d3108511" => :mountain_lion
   end
 
   def install
