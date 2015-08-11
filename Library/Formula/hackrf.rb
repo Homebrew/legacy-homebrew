0f65490b7d60bbe96b8940aaaa09f0a7da454d3b
Gareth <me@gareth.codes>
2015-08-10 19:13:16 -0700

    hackrf tar instead of zip

--- Library/Formula/hackrf.rb
+++ Library/Formula/hackrf.rb
@@ -1,8 +1,8 @@
 class Hackrf < Formula
   desc "Low cost software radio platform"
   homepage "https://github.com/mossmann/hackrf"
-  url "https://github.com/mossmann/hackrf/archive/v2015.07.2.zip"
-  sha256 "e564e5edf2162e2ce60bbd8a436b731709aafeda516a833b8a0e3520ef3b8fa9"
+  url "https://github.com/mossmann/hackrf/archive/v2015.07.2.tar.gz"
+  sha256 "00eaca20eceb3f2ed4c23c80353b20dac3a29458b8d33654ff287699d2ed8877"
 
   bottle do
     cellar :any
