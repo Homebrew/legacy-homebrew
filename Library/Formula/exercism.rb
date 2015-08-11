2815fc3bd33aef47736970609530877e903c7629
Leo Correa <lcorr005@gmail.com>
2015-08-11 10:24:16 -0400

    exercism 2.2.1
    exercism v2.2.1
    
    Closes #42808.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/exercism.rb
+++ Library/Formula/exercism.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Exercism < Formula
   desc "command-line tool to interact with exercism.io"
   homepage "http://cli.exercism.io"
-  url "https://github.com/exercism/cli/archive/v2.2.0.tar.gz"
-  sha256 "f044645c7e775abcf721939f015fb51df8733a0acf828961e6ebefee57194b03"
+  url "https://github.com/exercism/cli/archive/v2.2.1.tar.gz"
+  sha256 "a4d71b98fc3e28f96868bcef140b3888a6fb85f942c4cbe168abaea253177aab"
   head "https://github.com/exercism.git"
 
   bottle do
