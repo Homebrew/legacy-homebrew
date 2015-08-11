6bc19649ee12007cac729e261a4861dc612d31ef
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-11 20:59:02 +0100

    tika: add 1.10 bottle.

--- Library/Formula/tika.rb
+++ Library/Formula/tika.rb
@@ -4,6 +4,13 @@ class Tika < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.10.jar"
   sha256 "21ac3c156c2b53c451599f1b2c441c7898fce8efb7e912f6894e5e543d7cd48c"
 
+  bottle do
+    cellar :any
+    sha256 "149bb5712c16304d1f71fa3047dd58ed9fa1b013679309ad2b2e03ea98d8d047" => :yosemite
+    sha256 "b42c447ae4519b9663c94dbb64087e812a2123221778e4b60be34727a708994b" => :mavericks
+    sha256 "f0295de87032c10300884c875427872c6b12368c19126342930c3770b3f95e77" => :mountain_lion
+  end
+
   resource "server" do
     url "https://repo1.maven.org/maven2/org/apache/tika/tika-server/1.10/tika-server-1.10.jar"
     sha256 "d1977896455d78c4a080886d460726393360329a4b8e7d0b290ee24571aede39"
