de1638d22e8eeafa1f9c73e9afd463280b77a3f3
Hiroshi Hatake <hatake@clear-code.com>
2015-08-11 18:44:03 +0900

    embulk 0.6.22
    Closes #42797.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/embulk.rb
+++ Library/Formula/embulk.rb
@@ -1,8 +1,8 @@
 class Embulk < Formula
   desc "Data transfer between various databases, file formats and services"
   homepage "http://www.embulk.org/"
-  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.21.jar"
-  sha256 "742763b69f7f416b1c91fd446bec56ddec89001003742043e614d6dc48089fa6"
+  url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.22.jar"
+  sha256 "f86518cb82a8677c3d0d07e1554c6336c8afe603841c0c7d8c8ab75813cd1b5e"
 
   depends_on :java
 
