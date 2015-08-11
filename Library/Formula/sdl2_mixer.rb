35cc7ca98b8a3d6178446496511175509cc90842
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-11 07:50:23 +0100

    sdl2_mixer: add 2.0.0 bottle.

--- Library/Formula/sdl2_mixer.rb
+++ Library/Formula/sdl2_mixer.rb
@@ -4,6 +4,13 @@ class Sdl2Mixer < Formula
   url "http://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz"
   sha256 "a8ce0e161793791adeff258ca6214267fdd41b3c073d2581cd5265c8646f725b"
 
+  bottle do
+    cellar :any
+    sha256 "96cb856887d20982443ac02647e20cec3df1496bca263b3b1f733c3e125bd064" => :yosemite
+    sha256 "5a9fadfc66d69b7c2665029ea957d1b1d1b1a3381f932be8ab74c1e2c344370e" => :mavericks
+    sha256 "b66197fe45e0a83fb658c94c95c5d755ace4fef16fb1d5c94f3b0f68876e0afc" => :mountain_lion
+  end
+
   head "http://hg.libsdl.org/SDL_mixer", :using => :hg
 
   option :universal
