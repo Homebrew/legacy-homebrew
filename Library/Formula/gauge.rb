64a55a22161799e176ba7006aa1b327a921159d9
ApoorvaM <apoorva_rarefriend@yahoo.com>
2015-08-11 11:35:03 +0530

    removing caveats.

--- Library/Formula/gauge.rb
+++ Library/Formula/gauge.rb
@@ -76,13 +76,6 @@ class Gauge < Formula
     end
   end
 
-  def caveats; <<-EOS.undent
-    Installing gauge with homebrew doesn't install plugins required to run Gauge specs.
-    To install Gauge plugin, please explicitly run `gauge --install <plugin_name>`.
-    Language plugins supported are `java`, `csharp` and `ruby`.
-    Report plugins supported are `html-report` and `xml-report`.
-  EOS
-  end
   test do
     system "gauge", "--init", "java"
   end
