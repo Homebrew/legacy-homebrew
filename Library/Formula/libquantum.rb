89f731b1cbd5d418bf8c98a427c298715718d318
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-11 15:51:24 +0100

    libquantum: add 1.0.0 bottle.

--- Library/Formula/libquantum.rb
+++ Library/Formula/libquantum.rb
@@ -4,6 +4,13 @@ class Libquantum < Formula
   url "http://www.libquantum.de/files/libquantum-1.0.0.tar.gz"
   sha256 "b0f1a5ec9768457ac9835bd52c3017d279ac99cc0dffe6ce2adf8ac762997b2c"
 
+  bottle do
+    cellar :any
+    sha256 "ce31c2a7df81599bc4930ad4aef206f22e006db41d32d05ef1f2f3e72ff6d29d" => :yosemite
+    sha256 "2347b6f64ac6a2463cded1679de4390f5bda4b07a74f304efd4ea3bc536af3df" => :mavericks
+    sha256 "a7df989c22406155638d24ac755e851208d4cb7f72a8f1f17a985d172270006f" => :mountain_lion
+  end
+
   devel do
     url "http://www.libquantum.de/files/libquantum-1.1.1.tar.gz"
     sha256 "d8e3c4407076558f87640f1e618501ec85bc5f4c5a84db4117ceaec7105046e5"
