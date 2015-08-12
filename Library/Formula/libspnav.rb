54b7b1d5c32a164ef156ef8ba62d896b78df0ec5
Justin Buchanan <justbuchanan@gmail.com>
2015-08-12 19:10:09 -0400

    Added formula for libspnav

---
+++ Library/Formula/libspnav.rb
@@ -0,0 +1,43 @@
+
+class Libspnav < Formula
+  desc "A free, compatible alternative for 3Dconnexion's 3D input device drivers and SDK"
+  homepage "http://spacenav.sourceforge.net/index.html"
+  url "https://downloads.sourceforge.net/project/spacenav/spacenav%20library%20%28SDK%29/libspnav%200.2.3/libspnav-0.2.3.tar.gz"
+  sha256 "7ae4d7bb7f6a5dda28b487891e01accc856311440f582299760dace6ee5f1f93"
+
+  option "with-x11", "Enable support for sending mouse events through the x11 protocol"
+
+  depends_on :x11 => :optional
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+    ]
+
+    if build.without? "x11"
+      args << "--disable-x11"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <spnav.h>
+
+      int main() {
+        bool connected = spnav_open() != -1;
+        if (connected) spnav_close();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-I#{include}", "-L#{lib}", "-lspnav", "-o", "test"
+    system "./test"
+  end
+end
+
