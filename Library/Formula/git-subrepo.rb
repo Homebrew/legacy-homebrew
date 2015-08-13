450a267cc4b6941d8ed97a7cc6a8ccf23638f6af
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-13 06:06:55 +0100

    git-subrepo: update 0.2.3 bottle.

--- Library/Formula/git-subrepo.rb
+++ Library/Formula/git-subrepo.rb
@@ -7,9 +7,9 @@ class GitSubrepo < Formula
 
   bottle do
     cellar :any
-    sha256 "18c9f095154cd8d6bfa5154fa4284b5d7b9207e1e3efe8f2f00ffc24a16f9efb" => :yosemite
-    sha256 "c5db6617c659c0ba16344e71c5ee56af8e044d7496c2417441ef7165b3cdebea" => :mavericks
-    sha256 "e5b2ae1cc7ebbcd2001542b352255865cf562a2855a61ae50c36dc2f4624c019" => :mountain_lion
+    sha256 "36f76d5672fa97915a9836f7cf1c505bbfe6e5a7b81d980f23257f91b1d07aa9" => :yosemite
+    sha256 "ef0d2e945a72c4e191e62ec7a423bc032f98ad7831a3dc5b72c3d8108e34ab8c" => :mavericks
+    sha256 "e6bbfc45133c83005afbc55e5f8618eb4c6e0cacf2ed76e9e1ea179c0b1c1265" => :mountain_lion
   end
 
   def install
