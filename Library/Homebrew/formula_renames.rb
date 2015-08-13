6708e602b2c4d350fcadf86b3975e7627508855d
Vlad Shablinsky <vladshablinsky@gmail.com>
2015-08-13 12:59:28 +0300

    app-engine-go-64: rename from go-app-engine-64

--- Library/Homebrew/formula_renames.rb
+++ Library/Homebrew/formula_renames.rb
@@ -2,6 +2,7 @@ FORMULA_RENAMES = {
   "cv" => "progress",
   "google-perftools" => "gperftools",
   "go-app-engine-32" => "app-engine-go-32",
+  "go-app-engine-64" => "app-engine-go-64",
   "libcppa" => "caf",
   "objective-caml" => "ocaml",
   "mpich2" => "mpich",
