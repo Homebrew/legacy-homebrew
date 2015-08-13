698cee5d5be4f6aaf264c2e8e0b04f045d224f1b
Alex Dunn <dunn.alex@gmail.com>
2015-08-12 22:08:40 -0700

    cproto 4.7m
    Closes #42884.

--- Library/Formula/cproto.rb
+++ Library/Formula/cproto.rb
@@ -1,8 +1,9 @@
 class Cproto < Formula
   desc "Generate function prototypes for functions in input files"
   homepage "http://invisible-island.net/cproto"
-  url "ftp://invisible-island.net/cproto/cproto-4.7l.tgz"
-  sha256 "823a1e0480bbb39958c6c322c665932dd74286a8eb6dfe652528b6b8c3d873a5"
+  url "ftp://invisible-island.net/cproto/cproto-4.7m.tgz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/c/cproto/cproto_4.7m.orig.tar.gz"
+  sha256 "4b482e80f1b492e94f8dcda74d25a7bd0381c870eb500c18e7970ceacdc07c89"
 
   bottle do
     cellar :any
@@ -22,8 +23,6 @@ class Cproto < Formula
 
   test do
     (testpath/"woot.c").write("int woot() {\n}")
-
-    assert_match(/int woot.void.;/,
-                 shell_output("#{bin}/cproto woot.c"))
+    assert_match(/int woot.void.;/, shell_output("#{bin}/cproto woot.c"))
   end
 end
