ceed125a8337db1fe28848ed8aac20620bc25bf5
Francesco Banconi <francesco.banconi@canonical.com>
2015-08-13 18:32:52 +0200

    juju-quickstart 2.2.1

--- Library/Formula/juju-quickstart.rb
+++ Library/Formula/juju-quickstart.rb
@@ -1,8 +1,8 @@
 class JujuQuickstart < Formula
   desc "Opinionated command-line tool for quickly starting Juju"
   homepage "https://launchpad.net/juju-quickstart"
-  url "https://pypi.python.org/packages/source/j/juju-quickstart/juju-quickstart-2.2.0.tar.gz"
-  sha256 "651ed1e0fe5fd90b333c880df0d7c3a96a925086f8b832e2a375c480c54dba0d"
+  url "https://pypi.python.org/packages/source/j/juju-quickstart/juju-quickstart-2.2.1.tar.gz"
+  sha256 "5e1bba8ae3caba89cb8e772bc944bb6362d82c560671929e6a523af27fc7e5c9"
 
   bottle do
     cellar :any
