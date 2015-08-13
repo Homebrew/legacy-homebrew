1207d123dfadab7c417be6fdfdf8380a0c2cc9f9
Alex Dunn <dunn.alex@gmail.com>
2015-08-12 22:18:09 -0700

    cpanminus 1.7039
    Closes #42885.

--- Library/Formula/cpanminus.rb
+++ Library/Formula/cpanminus.rb
@@ -1,8 +1,8 @@
 class Cpanminus < Formula
   desc "Get, unpack, build, and install modules from CPAN"
   homepage "https://github.com/miyagawa/cpanminus"
-  url "https://github.com/miyagawa/cpanminus/archive/1.7038.tar.gz"
-  sha256 "00becccef59a628134a4099522c549e8aec60662e5558daa7999ad8a33ad339c"
+  url "https://github.com/miyagawa/cpanminus/archive/1.7039.tar.gz"
+  sha256 "b3326c363a1b100008596e21cd3be182f712f1a2b9cb5d608e463ac3111dcc9c"
 
   head "https://github.com/miyagawa/cpanminus.git"
 
@@ -11,6 +11,6 @@ class Cpanminus < Formula
   end
 
   test do
-    system "#{bin}/cpanm", "-V"
+    system "#{bin}/cpanm", "Test::More"
   end
 end
