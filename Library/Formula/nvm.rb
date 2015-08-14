fe55cd45e90bed004ac98a96f3201b2ef18855f5
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-14 03:37:15 +0100

    nvm 0.26.0
    Closes #42926.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/nvm.rb
+++ Library/Formula/nvm.rb
@@ -1,14 +1,14 @@
 class Nvm < Formula
   desc "Manage multiple Node.js versions"
   homepage "https://github.com/creationix/nvm"
-  url "https://github.com/creationix/nvm/archive/v0.25.4.tar.gz"
-  sha256 "6df007c5e9e36a8409f3daeced1d50d66b0d03ebecd780533d4546c519d351a7"
+  url "https://github.com/creationix/nvm/archive/v0.26.0.tar.gz"
+  sha256 "2838e8d54e9c748b20bc9915a23714fb8d75d1539b2b76f0ff11493222011d79"
   head "https://github.com/creationix/nvm.git"
 
   resource "nvm-exec" do
-    url "https://raw.githubusercontent.com/creationix/nvm/v0.25.4/nvm-exec"
+    url "https://raw.githubusercontent.com/creationix/nvm/v0.26.0/nvm-exec"
     sha256 "a0581795f10114b9759992a82a43496daf4b4a52ad381a3072d8eee9866a28c5"
-    version "0.25.4"
+    version "0.26.0"
   end
 
   def install
