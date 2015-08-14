a2615000f5496ca67eb422026998cba945131557
Mike McQuaid <mike@mikemcquaid.com>
2015-08-14 15:13:44 +0100

    install: mention if installed formula isn't migrated.
    Closes #42940.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Homebrew/cmd/install.rb
+++ Library/Homebrew/cmd/install.rb
@@ -71,7 +71,13 @@ module Homebrew
 
         if f.installed?
           msg = "#{f.full_name}-#{f.installed_version} already installed"
-          msg << ", it's just not linked" unless f.linked_keg.symlink? || f.keg_only?
+          unless f.linked_keg.symlink?
+            if Pathname.new("#{HOMEBREW_CELLAR}/#{f.oldname}").exist?
+              msg << ", it's just not migrated"
+            elsif !f.keg_only?
+              msg << ", it's just not linked"
+            end
+          end
           opoo msg
         else
           formulae << f
