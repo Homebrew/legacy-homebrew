a5a70f7005957d91f108cb39863dd42cb8010d2d
Stuart Williams <stuart@sidereal.ca>
2015-08-14 13:24:29 -0400

    opencc 1.0.3

--- Library/Formula/opencc.rb
+++ Library/Formula/opencc.rb
@@ -1,8 +1,8 @@
 class Opencc < Formula
   desc "Simplified-traditional Chinese conversion tool"
   homepage "https://github.com/BYVoid/OpenCC"
-  url "https://dl.bintray.com/byvoid/opencc/opencc-1.0.2.tar.gz"
-  sha256 "e0976ec214961d00dfcd34be100cf62f8f71ef5f56b3f27229e7be5e4f07f1e2"
+  url "https://dl.bintray.com/byvoid/opencc/opencc-1.0.3.tar.gz"
+  sha256 "5738c6996c6576539e8c5dea103456d86005b0656b476b9d97c65d468a9e7d97"
 
   bottle do
     sha1 "f49556768692c346a700382cec6746ee3d425ff3" => :yosemite
@@ -19,5 +19,12 @@ class Opencc < Formula
     system "cmake", ".", "-DBUILD_DOCUMENTATION:BOOL=OFF", *std_cmake_args
     system "make"
     system "make", "install"
+  end
+
+  test do
+    input = "中国鼠标软件打印机"
+    output = shell_output("echo #{input} | #{bin}/opencc")
+    output = output.force_encoding("UTF-8") if output.respond_to?(:force_encoding)
+    assert_match "中國鼠標軟件打印機", output
   end
 end
