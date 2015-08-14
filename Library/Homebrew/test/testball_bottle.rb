032209f63b131f50f60bb30cb0ac3a10412f8f03
William Woodruff <william@tuffbizz.com>
2015-08-14 14:21:39 -0400

    Add tests for pouring bottle without Xcode
    add testball_bottle formula for upcoming bottle unit testing, as well
    as bottles directory containing test bottle (and symlinks)

---
+++ Library/Homebrew/test/testball_bottle.rb
@@ -0,0 +1,17 @@
+class TestballBottle < Formula
+  def initialize(name = "testball_bottle", path = Pathname.new(__FILE__).expand_path, spec = :stable)
+    self.class.instance_eval do
+      stable.url "file://#{File.expand_path("..", __FILE__)}/tarballs/testball-0.1.tbz"
+      stable.sha256 "1dfb13ce0f6143fe675b525fc9e168adb2215c5d5965c9f57306bb993170914f"
+      stable.bottle do
+        cellar :any_skip_relocation
+        root_url "file://#{File.expand_path("..", __FILE__)}/bottles"
+        sha256 "9abc8ce779067e26556002c4ca6b9427b9874d25f0cafa7028e05b5c5c410cb4" => :yosemite
+        sha256 "9abc8ce779067e26556002c4ca6b9427b9874d25f0cafa7028e05b5c5c410cb4" => :mavericks
+        sha256 "9abc8ce779067e26556002c4ca6b9427b9874d25f0cafa7028e05b5c5c410cb4" => :mountain_lion
+      end
+      cxxstdlib_check :skip
+    end
+    super
+  end
+end
