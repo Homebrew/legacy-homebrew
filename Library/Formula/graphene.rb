df825b01db05955d873d5f9d20cfd2b00eca97c8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-14 23:02:38 +0100

    graphene: add 1.2.6 bottle.

--- Library/Formula/graphene.rb
+++ Library/Formula/graphene.rb
@@ -4,6 +4,13 @@ class Graphene < Formula
   url "https://download.gnome.org/sources/graphene/1.2/graphene-1.2.6.tar.xz"
   sha256 "987a83be0b9e634805d8bb949d11422a42e627fd40f78c68c3b1ac7b3243cda2"
 
+  bottle do
+    cellar :any
+    sha256 "5c1242c087ac5940a65f5206753b2257b1e7a7527bb243f539ccdbe26df91a76" => :yosemite
+    sha256 "ba71259f21f30ffc272dae108d1cad4993b3207ccf7b48202feb07d9dedd2d0e" => :mavericks
+    sha256 "41aca31352fe31cb7efe693e897652ccef7b34b6ccb3bad9192b864a5bbc7337" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
 
