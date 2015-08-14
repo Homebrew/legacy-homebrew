93db4524c7a700295097ab5dcd9f1c2609be6863
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-14 19:02:14 +0100

    libspnav: add 0.2.3 bottle.

--- Library/Formula/libspnav.rb
+++ Library/Formula/libspnav.rb
@@ -4,6 +4,13 @@ class Libspnav < Formula
   url "https://downloads.sourceforge.net/project/spacenav/spacenav%20library%20%28SDK%29/libspnav%200.2.3/libspnav-0.2.3.tar.gz"
   sha256 "7ae4d7bb7f6a5dda28b487891e01accc856311440f582299760dace6ee5f1f93"
 
+  bottle do
+    cellar :any
+    sha256 "87bf93469bb14eef1a24de81cd521f6a62363a6aa7c04a319f3f18905de039b1" => :yosemite
+    sha256 "f425659deb611eacb94f2245f0c8f8235aa0169a422874f2aa2c32f8d207b84a" => :mavericks
+    sha256 "0d0a4943d1eee96936b7ccf0a200d353a3fd35bbf67d46695e0e4e41d498df16" => :mountain_lion
+  end
+
   option "with-x11", "Enable support for sending mouse events through the x11 protocol"
 
   depends_on :x11 => :optional
