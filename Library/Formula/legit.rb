2b02d8656a46875a749e2e2223bc11d125809529
Albert <albertnigma@gmail.com>
2015-08-14 17:15:58 +0500

    legit 0.2.0

--- Library/Formula/legit.rb
+++ Library/Formula/legit.rb
@@ -1,8 +1,8 @@
 class Legit < Formula
   desc "Command-line interface for Git, optimized for workflow simplicity"
   homepage "http://www.git-legit.org/"
-  url "https://github.com/downloads/kennethreitz/legit/legit-v0.1.0-darwin-x86.tar.bz2"
-  version "0.1.0"
+  url "https://github.com/kennethreitz/legit/archive/v0.2.0.tar.gz"
+  version "0.2.0"
   sha256 "9b25a4c8e12d6703627ba24f4547a7d4350ca1ef9fa44415512b1bcc57b06506"
 
   def install
