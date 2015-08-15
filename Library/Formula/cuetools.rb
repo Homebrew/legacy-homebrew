1b09c4a977f571cf227803968f358095e6bcfdc9
Alex Dunn <dunn.alex@gmail.com>
2015-08-15 11:18:59 -0700

    cuetools 1.4.1
    Closes #42974.

--- Library/Formula/cuetools.rb
+++ Library/Formula/cuetools.rb
@@ -1,8 +1,8 @@
 class Cuetools < Formula
   desc "Utilities for .cue and .toc files"
   homepage "https://github.com/svend/cuetools"
-  url "https://github.com/svend/cuetools/archive/1.4.0.tar.gz"
-  sha256 "5dc86e9736929757b338827efa0ecae1b2fcee2273debc718511749a4178ddf6"
+  url "https://github.com/svend/cuetools/archive/1.4.1.tar.gz"
+  sha256 "24a2420f100c69a6539a9feeb4130d19532f9f8a0428a8b9b289c6da761eb107"
 
   head "https://github.com/svend/cuetools.git"
 
@@ -24,6 +24,16 @@ class Cuetools < Formula
     system "autoreconf", "-i"
     system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cue").write <<-EOS.undent
+      FILE "sampleimage.bin" BINARY
+        TRACK 01 MODE1/2352
+          INDEX 01 00:00:00
+    EOS
+    system "cueconvert", testpath/"test.cue", testpath/"test.toc"
+    File.exist? "test.toc"
   end
 end
 
