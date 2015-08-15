e9c2a258ed74324cd525caebc11b030ea507a7ae
Thierry Moisan <thierry.moisan@gmail.com>
2015-08-14 21:00:15 -0400

    Use pkgshare instead of share

--- Library/Formula/ibex.rb
+++ Library/Formula/ibex.rb
@@ -23,19 +23,19 @@ class Ibex < Formula
     system "./waf", "install"
 
     # copy and compile examples and benchmark for the test
-    share.install "examples"
-    share.install "benchs"
+    pkgshare.install "examples"
+    pkgshare.install "benchs"
     cxxflags = "-frounding-math -ffloat-store -I#{include} -I#{include}/ibex"
     libflags = "-L#{lib} -libex -lprim -lClp -lCoinUtils -lm"
-    cd "#{share}/examples"
+    cd "#{pkgshare}/examples"
     system "make", "defaultsolver",
       "LIBS=#{libflags}",
       "CXXFLAGS=#{cxxflags}"
   end
 
   test do
-    cp_r "#{share}/examples/.", "#{testpath}"
-    cp "#{share}/benchs/cyclohexan3D.bch", "#{testpath}"
+    cp_r "#{pkgshare}/examples/.", "#{testpath}"
+    cp "#{pkgshare}/benchs/cyclohexan3D.bch", "#{testpath}"
     system "./defaultsolver", "cyclohexan3D.bch", "1e-05", "10"
   end
 end
