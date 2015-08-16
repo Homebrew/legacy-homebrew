c92a2daab8163ab92055c91a43c8749688643dff
Ori Livneh <ori@wikimedia.org>
2015-08-16 09:35:40 -0700

    Update symlinks.rb

--- Library/Formula/symlinks.rb
+++ Library/Formula/symlinks.rb
@@ -3,7 +3,6 @@ class Symlinks < Formula
   homepage "https://github.com/brandt/symlinks"
   url "https://github.com/brandt/symlinks/archive/v1.4.3.tar.gz"
   head "https://github.com/brandt/symlinks.git"
-  version "1.4.3"
   sha256 "27105b2898f28fd53d52cb6fa77da1c1f3b38e6a0fc2a66bf8a25cd546cb30b2"
 
   def install
