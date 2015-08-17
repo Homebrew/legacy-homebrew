fd4e69e881f00a8933c68e9cda7753ac58dd7120
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-17 07:10:06 +0100

    embulk: add 0.6.25 bottle.

--- Library/Formula/embulk.rb
+++ Library/Formula/embulk.rb
@@ -4,6 +4,13 @@ class Embulk < Formula
   url "https://bintray.com/artifact/download/embulk/maven/embulk-0.6.25.jar"
   sha256 "5c64622a0b1df117b6928e2710d5d6a9cd8dd55b8e2ee01e79eada40b303e335"
 
+  bottle do
+    cellar :any
+    sha256 "119f18cd8960821c6471c4ac96dcbc3801e786b4853e3a68891c68e3a5ec06a5" => :yosemite
+    sha256 "e6bcbf6bb5af67f2d497cb05408fc1c4d626fd3011a7ee65e76fda2e866eb1f2" => :mavericks
+    sha256 "507ba927665752e730843b9cb36fade80ff2849b2a5b8e30eb4c34d2df2beb34" => :mountain_lion
+  end
+
   depends_on :java
 
   skip_clean "libexec"
