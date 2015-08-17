83daa2a8fd405f3d19a91dde38a07f2b1be1c99d
Alex Dunn <dunn.alex@gmail.com>
2015-08-17 10:54:23 -0700

    datomic 0.9.5206
    Closes #43031.

--- Library/Formula/datomic.rb
+++ Library/Formula/datomic.rb
@@ -1,15 +1,16 @@
 class Datomic < Formula
-  desc "Database that simplifies by separating transactions, storage and queries"
+  desc "Database separating transactions, storage and queries"
   homepage "http://www.datomic.com/"
-  url "https://my.datomic.com/downloads/free/0.9.5198"
-  sha256 "1d42979079bd62c2b4df9fbc3c18cdc15d49ac04905eceab4c75720e23ed8fc7"
-  version "0.9.5198"
+  url "https://my.datomic.com/downloads/free/0.9.5206"
+  version "0.9.5206"
+  sha256 "b5b406d301b3e3fd4ff61d3e20946fb67fc5adae269bc2ee3bbca0819e02de3c"
 
   depends_on :java
 
   def install
     libexec.install Dir["*"]
     (bin/"datomic").write_env_script libexec/"bin/datomic", Language::Java.java_home_env
+
     %w[transactor repl repl-jline rest shell groovysh maven-install].each do |file|
       (bin/"datomic-#{file}").write_env_script libexec/"bin/#{file}", Language::Java.java_home_env
     end
@@ -26,7 +27,11 @@ class Datomic < Formula
   end
 
   test do
-    help = pipe_output("#{bin}/datomic-shell", "Shell.help();\n")
-    assert_match(/^\* Basics/, help)
+    IO.popen("#{bin}/datomic-repl", "r+") do |pipe|
+      assert_equal "Clojure 1.6.0", pipe.gets.chomp
+      pipe.puts "^C"
+      pipe.close_write
+      pipe.close
+    end
   end
 end
