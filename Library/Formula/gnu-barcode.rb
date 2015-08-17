80dd9f884cc8d96123a694ff4c0927e5e91a9f31
John Cavalieri <john.cavalieri@gmail.com>
2015-08-17 08:33:41 -0500

    added comment to gnu barcode formula

--- Library/Formula/gnu-barcode.rb
+++ Library/Formula/gnu-barcode.rb
@@ -5,6 +5,9 @@ class GnuBarcode < Formula
   mirror "https://ftp.gnu.org/gnu/barcode/barcode-0.99.tar.gz"
   sha256 "7c031cf3eb811242f53664379aebbdd9fae0b7b26b5e5d584c31a9f338154b64"
 
+  # Patch and ac_cv_func_calloc_0_nonnull config addresses the following issue:
+  # https://github.com/Homebrew/homebrew/issues/41115
+
   patch :DATA
 
   def install
