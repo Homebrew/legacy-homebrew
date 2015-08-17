6c1e070193aa7a8c87903516327dd0f316eb61be
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-17 19:38:20 +0100

    librasterlite: add lsqlite3 pointer
    Closes #42686.
    
    Closes #43037.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/librasterlite.rb
+++ Library/Formula/librasterlite.rb
@@ -1,7 +1,7 @@
 class Librasterlite < Formula
   desc "Library to store and retrieve huge raster coverages"
   homepage "https://www.gaia-gis.it/fossil/librasterlite/index"
-  url "http://www.gaia-gis.it/gaia-sins/librasterlite-sources/librasterlite-1.1g.tar.gz"
+  url "https://www.gaia-gis.it/gaia-sins/librasterlite-sources/librasterlite-1.1g.tar.gz"
   sha256 "0a8dceb75f8dec2b7bd678266e0ffd5210d7c33e3d01b247e9e92fa730eebcb3"
   revision 1
 
@@ -17,11 +17,12 @@ class Librasterlite < Formula
   depends_on "libpng"
   depends_on "libgeotiff"
   depends_on "libspatialite"
+  depends_on "sqlite"
 
   def install
     # Ensure Homebrew SQLite libraries are found before the system SQLite
     sqlite = Formula["sqlite"]
-    ENV.append "LDFLAGS", "-L#{sqlite.opt_lib}"
+    ENV.append "LDFLAGS", "-L#{sqlite.opt_lib} -lsqlite3"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
