e870f14601abf8ac33027069301c17881b0dd7c5
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-18 04:04:07 +0100

    rem: fix bottle spacing

--- Library/Formula/rem.rb
+++ Library/Formula/rem.rb
@@ -3,6 +3,7 @@ class Rem < Formula
   homepage "https://github.com/kykim/rem"
   url "https://github.com/kykim/rem/archive/20150618.tar.gz"
   sha256 "e57173a26d2071692d72f3374e36444ad0b294c1284e3b28706ff3dbe38ce8af"
+
   bottle do
     cellar :any
     sha256 "d9a6303ff3935923ba53d093e95387caaf24460a4cd7fb7d330fa5c3988b551c" => :yosemite
