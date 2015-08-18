d631d5cd44a05f0bfe3db998d85ebdef2b63fd2f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-18 09:05:27 +0100

    desktop-file-utils: add 0.22 bottle.

--- Library/Formula/desktop-file-utils.rb
+++ Library/Formula/desktop-file-utils.rb
@@ -4,6 +4,12 @@ class DesktopFileUtils < Formula
   url "http://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-0.22.tar.xz"
   sha256 "843532672692f98e9b2d6ae6cc8658da562dfde1606c7f33d9d227a344de56c5"
 
+  bottle do
+    sha256 "39c97a4f9d452978c76113f4e5496c23ed4c3302cc02aee0a96a191039ca395a" => :yosemite
+    sha256 "7d426c9fbb60b1e2f090e299ba06b93177376b6d2ec72085ca052d72227785a0" => :mavericks
+    sha256 "438df3ef55ac9f1acbb7c997e699e5876d44c7767dcfb8358e88b04c120c10c0" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
 
