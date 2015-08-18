157443f3646e6a9f619eb472834a10d56f8eb884
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-18 18:06:02 +0100

    inspircd: fix bottle inreplace
    Will fix the CI error caused by https://github.com/Homebrew/homebrew/commit/b75af6b13036b6208f4448e6bdc424bbc2bbdc53
    
    The username in the bottles is still `brew`, which causes the error.

--- Library/Formula/inspircd.rb
+++ Library/Formula/inspircd.rb
@@ -3,7 +3,6 @@ class Inspircd < Formula
   homepage "http://www.inspircd.org"
   url "https://github.com/inspircd/inspircd/archive/v2.0.20.tar.gz"
   sha256 "5156e2da5da4cfa377705ecd633aee41cdcd785d12627497d55cab5f70dd686f"
-
   head "https://github.com/inspircd/inspircd.git"
 
   bottle do
@@ -15,6 +14,8 @@ class Inspircd < Formula
   skip_clean "data"
   skip_clean "logs"
 
+  option "without-ldap", "Build without ldap support"
+
   depends_on "pkg-config" => :build
   depends_on "geoip" => :optional
   depends_on "gnutls" => :optional
@@ -25,16 +26,13 @@ class Inspircd < Formula
   depends_on "sqlite" => :optional
   depends_on "tre" => :optional
 
-  option "without-ldap", "Build without ldap support"
-
   def install
     modules = []
     modules << "m_geoip.cpp" if build.with? "geoip"
     modules << "m_ssl_gnutls.cpp" if build.with? "gnutls"
     modules << "m_mysql.cpp" if build.with? "mysql"
     modules << "m_ssl_openssl.cpp" if build.with? "openssl"
-    modules << "m_ldapauth.cpp" if build.with? "ldap"
-    modules << "m_ldapoper.cpp" if build.with? "ldap"
+    modules << "m_ldapauth.cpp" << "m_ldapoper.cpp" if build.with? "ldap"
     modules << "m_regex_pcre.cpp" if build.with? "pcre"
     modules << "m_pgsql.cpp" if build.with? "postgresql"
     modules << "m_sqlite3.cpp" if build.with? "sqlite"
