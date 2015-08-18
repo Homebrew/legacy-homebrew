c6d47fbf580000d6f275ec2bd403154e56c29d62
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-18 09:05:27 +0100

    gucharmap: add 3.16.2 bottle.

--- Library/Formula/gucharmap.rb
+++ Library/Formula/gucharmap.rb
@@ -4,6 +4,12 @@ class Gucharmap < Formula
   url "https://download.gnome.org/sources/gucharmap/3.16/gucharmap-3.16.2.tar.xz"
   sha256 "0a3fa996ae271c8f4812a7d8dc573ece0c000d63185b4865fa99387455e1b13d"
 
+  bottle do
+    sha256 "6e08b565a355742462cfe5babc444708d1c9f6a42faa07e3179106bb1e966843" => :yosemite
+    sha256 "6a6522fbee8e9bc39dee9cb4fb9458022299991670c85a88bfffc2637ad3da50" => :mavericks
+    sha256 "14ac6d6474a7d2aef39c7fe23d959b89fc862b77a94ce5b353ac72a94d6b8238" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "itstool" => :build
