42c3e5e59f0a334cfb0e532a8b338068b74d160d
William Woodruff <william@tuffbizz.com>
2015-08-18 03:18:54 -0400

    unstub test_formula_installer_bottle.rb

--- Library/Homebrew/test/testball_bottle.rb
+++ Library/Homebrew/test/testball_bottle.rb
@@ -3,14 +3,15 @@ class TestballBottle < Formula
     self.class.instance_eval do
       stable.url "file://#{File.expand_path("..", __FILE__)}/tarballs/testball-0.1.tbz"
       stable.sha256 "1dfb13ce0f6143fe675b525fc9e168adb2215c5d5965c9f57306bb993170914f"
+      stable.bottle do
+        cellar :any_skip_relocation
+        root_url "file://#{File.expand_path("..", __FILE__)}/bottles"
+        sha256 "9abc8ce779067e26556002c4ca6b9427b9874d25f0cafa7028e05b5c5c410cb4" => :yosemite
+        sha256 "9abc8ce779067e26556002c4ca6b9427b9874d25f0cafa7028e05b5c5c410cb4" => :mavericks
+        sha256 "9abc8ce779067e26556002c4ca6b9427b9874d25f0cafa7028e05b5c5c410cb4" => :mountain_lion
+      end
+      cxxstdlib_check :skip
     end
     super
-  end
-
-  bottle do
-    root_url "file://#{File.expand_path("..", __FILE__)}/bottles"
-    sha256 "7cf1d893838bc940beb3671f28a85de8e636489843b272772fbcba4bfb056c4a" => :yosemite
-    sha256 "7cf1d893838bc940beb3671f28a85de8e636489843b272772fbcba4bfb056c4a" => :mavericks
-    sha256 "7cf1d893838bc940beb3671f28a85de8e636489843b272772fbcba4bfb056c4a" => :mountain_lion
   end
 end
