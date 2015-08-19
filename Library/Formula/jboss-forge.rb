2c4ba27ec2fa9574a7d182ba1fd46982a5867c88
George Gastaldi <gegastaldi@gmail.com>
2015-08-19 12:02:48 -0300

    jboss-forge 2.18.0.Final
    Closes #43092.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -1,9 +1,9 @@
 class JbossForge < Formula
   desc "Tools to help set up and configure a project"
   homepage "http://forge.jboss.org/"
-  url "https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.17.0.Final&e=zip&c=offline"
-  version "2.17.0.Final"
-  sha256 "c0d8cadb315b23d99fb582db670ef32beea51b9b06ea9313ec81daa3998bedd8"
+  url "https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=2.18.0.Final&e=zip&c=offline"
+  version "2.18.0.Final"
+  sha256 "c191d741ed29eee5bbc2a31ba2fd83ad81b6672b79b47acdc4abe520fe057cee"
 
   bottle do
     cellar :any
@@ -19,6 +19,7 @@ class JbossForge < Formula
   end
 
   test do
+    ENV["FORGE_OPTS"] = "-Duser.home=#{testpath}"
     assert_match "org.jboss.forge.addon:core", shell_output("#{bin}/forge --list")
   end
 end
