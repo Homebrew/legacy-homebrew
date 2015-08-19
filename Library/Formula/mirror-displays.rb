77124281e51098da9d3f26a8b65ff65f1a01838d
jasonm23 <jasonm23@gmail.com>
2015-08-19 14:54:11 +0800

    flush whitespace, add test case

--- Library/Formula/mirror-displays.rb
+++ Library/Formula/mirror-displays.rb
@@ -1,11 +1,8 @@
 class MirrorDisplays < Formula
-
-  homepage "https://github.com/fcanas/mirror-displays"
   desc "Toggle/set/query OSX display mirroring"
+  homepage "https://github.com/fcanas/mirror-displays"
   url "https://github.com/fcanas/mirror-displays/archive/v1.1.tar.gz"
-
   sha256 "84d4de59da3cdb655d2b38d59fc0d38c89abf6f21acf1f8fc1bff4ab9f37fbee"
-
   head "https://github.com/fcanas/mirror-displays.git"
 
   depends_on :xcode => :build
@@ -17,7 +14,9 @@ class MirrorDisplays < Formula
   end
 
   test do
-    system "#{bin}/mirror -h"
+    help_text = "Toggle mirroring (default behavior)"
+    out = system "#{bin}/mirror -h"
+    assert true, out.include?(help_text)
   end
 
 end
