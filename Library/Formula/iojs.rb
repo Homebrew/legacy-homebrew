d3cce068b4e1604830b1988646d88298bb5a3d1e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-20 21:40:53 +0100

    iojs: update 3.1.0 bottle.

--- Library/Formula/iojs.rb
+++ Library/Formula/iojs.rb
@@ -5,9 +5,9 @@ class Iojs < Formula
   sha256 "8d0e11833dd86517868ac95ea87732c0f9614bc3c10150abf1d133ce378dfe48"
 
   bottle do
-    sha256 "ee9137a3070298fa97f2a09a3ec2b90589fd2ba6f9821c95c9de25c6f1823f0a" => :yosemite
-    sha256 "0936029971e177721eb89133e309eecd15a6ffa33418d84d1427e275b18ac196" => :mavericks
-    sha256 "080cfc436e299b2ba2530ac929fd7ff75c6226f551ca46177a91944f407cab3d" => :mountain_lion
+    sha256 "c18ede09560539014d6ea794c01ba9f1e4fc057f06f4040a90c7d6b95283f7cd" => :yosemite
+    sha256 "1805b8b51362715bcb20a14f45570bbe660a5da51b1c83bbb866391bef9741ae" => :mavericks
+    sha256 "e3ad95292fe426fbf23faffed2e692fb841e0e17c70a2fc4d6d202b3b0d434c5" => :mountain_lion
   end
 
   keg_only "iojs conflicts with node (which is currently more established)"
