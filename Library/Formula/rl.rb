1e9fdf4929d4d23b102a802c2e26b2542dd002ae
Freyr Bergsteinsson <fresnik@gmail.com>
2015-08-20 15:16:38 +0000

    Removing redundant version and shortened the description field according to audit.

--- Library/Formula/rl.rb
+++ Library/Formula/rl.rb
@@ -1,8 +1,7 @@
 class Rl < Formula
-  desc "rl is a command-line tool that reads lines from an input file or stdin, randomizes the lines and outputs a specified number of lines."
+  desc "rl is a command-line tool that randomizes the lines from the input"
   homepage "http://arthurdejong.org/rl/"
   url "http://arthurdejong.org/rl/rl-0.2.7.tar.gz"
-  version "0.2.7"
   sha256 "1cfca23d6a14acd190c5a6261923757d20cb94861c9b2066991ec7a7cae33bc8"
 
   def install
