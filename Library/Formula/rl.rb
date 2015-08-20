f16296b5c53abfd97bd918f360f673359fce1be1
Freyr Bergsteinsson <freyrb@jayne.local>
2015-08-20 10:26:58 +0000

    Removing unnecessary configure flags.

---
+++ Library/Formula/rl.rb
@@ -0,0 +1,19 @@
+class Rl < Formula
+  desc "rl is a command-line tool that reads lines from an input file or stdin, randomizes the lines and outputs a specified number of lines."
+  homepage "http://arthurdejong.org/rl/"
+  url "http://arthurdejong.org/rl/rl-0.2.7.tar.gz"
+  version "0.2.7"
+  sha256 "1cfca23d6a14acd190c5a6261923757d20cb94861c9b2066991ec7a7cae33bc8"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    system bin/"rl", "--version"
+  end
+end
