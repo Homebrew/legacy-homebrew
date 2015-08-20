42accab1e4f1ca6324504e59988fecb651455297
Misty De Meo <mistydemeo@gmail.com>
2015-08-19 21:45:39 -0700

    go 1.5
    Closes #43095.

--- Library/Formula/go.rb
+++ Library/Formula/go.rb
@@ -1,10 +1,10 @@
 class Go < Formula
   desc "Go programming environment"
   homepage "https://golang.org"
-  url "https://storage.googleapis.com/golang/go1.4.2.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.4.2.src.tar.gz"
-  version "1.4.2"
-  sha256 "299a6fd8f8adfdce15bc06bde926e7b252ae8e24dd5b16b7d8791ed79e7b5e9b"
+  url "https://storage.googleapis.com/golang/go1.5.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.5.src.tar.gz"
+  version "1.5"
+  sha256 "be81abec996d5126c05f2d36facc8e58a94d9183a56f026fc9441401d80062db"
 
   head "https://github.com/golang/go.git"
 
@@ -15,24 +15,13 @@ class Go < Formula
     sha256 "7913ecbc952e22f9d6e5df114b857ed23ad97adcbc88419fa3b6425b856ee38e" => :mountain_lion
   end
 
-  devel do
-    url "https://storage.googleapis.com/golang/go1.5rc1.src.tar.gz"
-    version "1.5rc1"
-    sha256 "c00c4762f70658ce1a7a06322ac1909d38fafca3b016ab20e62fffedcf09f9e4"
-  end
-
-  option "with-cc-all", "Build with cross-compilers and runtime support for all supported platforms"
-  option "with-cc-common", "Build with cross-compilers and runtime support for darwin, linux and windows"
   option "without-cgo", "Build without cgo"
   option "without-godoc", "godoc will not be installed for you"
   option "without-vet", "vet will not be installed for you"
 
-  deprecated_option "cross-compile-all" => "with-cc-all"
-  deprecated_option "cross-compile-common" => "with-cc-common"
-
   resource "gotools" do
     url "https://go.googlesource.com/tools.git",
-    :revision => "69db398fe0e69396984e3967724820c1f631e971"
+    :revision => "d02228d1857b9f49cd0252788516ff5584266eb6"
   end
 
   resource "gobootstrap" do
@@ -46,55 +35,23 @@ class Go < Formula
   end
 
   def install
-    # host platform (darwin) must come last in the targets list
-    if build.with? "cc-all"
-      targets = [
-        ["linux",   ["386", "amd64", "arm"]],
-        ["freebsd", ["386", "amd64", "arm"]],
-        ["netbsd",  ["386", "amd64", "arm"]],
-        ["openbsd", ["386", "amd64"]],
-        ["windows", ["386", "amd64"]],
-        ["dragonfly", ["386", "amd64"]],
-        ["plan9",   ["386", "amd64"]],
-        ["solaris", ["amd64"]],
-        ["darwin",  ["386", "amd64"]]
-      ]
-    elsif build.with? "cc-common"
-      targets = [
-        ["linux",   ["386", "amd64", "arm"]],
-        ["windows", ["386", "amd64"]],
-        ["darwin",  ["386", "amd64"]]
-      ]
-    else
-      targets = [["darwin", [""]]]
-    end
+    # GOROOT_FINAL must be overidden later on real Go install
+    ENV["GOROOT_FINAL"] = buildpath/"gobootstrap"
 
-    if build.head? || build.devel?
-      # GOROOT_FINAL must be overidden later on real Go install
-      ENV["GOROOT_FINAL"] = buildpath/"gobootstrap"
-
-      # build the gobootstrap toolchain Go >=1.4
-      (buildpath/"gobootstrap").install resource("gobootstrap")
-      cd "#{buildpath}/gobootstrap/src" do
-        system "./make.bash", "--no-clean"
-      end
-      # This should happen after we build the test Go, just in case
-      # the bootstrap toolchain is aware of this variable too.
-      ENV["GOROOT_BOOTSTRAP"] = ENV["GOROOT_FINAL"]
+    # build the gobootstrap toolchain Go >=1.4
+    (buildpath/"gobootstrap").install resource("gobootstrap")
+    cd "#{buildpath}/gobootstrap/src" do
+      system "./make.bash", "--no-clean"
     end
+    # This should happen after we build the test Go, just in case
+    # the bootstrap toolchain is aware of this variable too.
+    ENV["GOROOT_BOOTSTRAP"] = ENV["GOROOT_FINAL"]
 
     cd "src" do
-      targets.each do |os, archs|
-        cgo_enabled = os == "darwin" && build.with?("cgo") ? "1" : "0"
-        archs.each do |arch|
-          ENV["GOROOT_FINAL"] = libexec
-          ENV["GOOS"]         = os
-          ENV["GOARCH"]       = arch
-          ENV["CGO_ENABLED"]  = cgo_enabled
-          ohai "Building go for #{arch}-#{os}"
-          system "./make.bash", "--no-clean"
-        end
-      end
+      ENV["GOROOT_FINAL"] = libexec
+      ENV["GOOS"]         = "darwin"
+      ENV["CGO_ENABLED"]  = build.with?("cgo") ? "1" : "0"
+      system "./make.bash", "--no-clean"
     end
 
     (buildpath/"pkg/obj").rmtree
