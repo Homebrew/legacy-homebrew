df3b2af73441161df35a157183efbdab4245ef66
Aldrin Piri <aldrinpiri@gmail.com>
2015-08-21 12:09:47 -0400

    Removing indefinite article from description as per audit.

--- Library/Formula/nifi.rb
+++ Library/Formula/nifi.rb
@@ -1,5 +1,5 @@
 class Nifi < Formula
-  desc "An easy to use, powerful, and reliable system to process and distribute data."
+  desc "Easy to use, powerful, and reliable system to process and distribute data."
   homepage "https://nifi.apache.org"
   url "https://www.apache.org/dyn/closer.cgi?path=/nifi/0.2.1/nifi-0.2.1-bin.tar.gz"
   sha256 "e151dab553a8ea466f7462d75145e2aa08ced938499aef184850aa4d3209c605"
