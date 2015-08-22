3eeed83120e23696539f448a62e9e610a8cbfcb9
Alex Dunn <dunn.alex@gmail.com>
2015-08-22 15:33:55 -0700

    di 4.36
    Closes #43186.

--- Library/Formula/di.rb
+++ Library/Formula/di.rb
@@ -1,12 +1,11 @@
 class Di < Formula
   desc "Advanced df-like disk information utility"
   homepage "http://www.gentoo.com/di/"
-  url "http://gentoo.com/di/di-4.35.tar.gz"
-  sha256 "2cdfface7a85e3a359cb228277b090a0648cabe18520e43b09919bdaf67b71d2"
+  url "http://gentoo.com/di/di-4.36.tar.gz"
+  sha256 "eb03d2ac0a3df531cdcb64b3667dbaebede60a4d3a4626393639cecb954c6d86"
 
   def install
-    system "make", "prefix=#{prefix}", "DI_MANDIR=#{man1}"
-    system "make", "install", "prefix=#{prefix}", "DI_MANDIR=#{man1}"
+    system "make", "install", "prefix=#{prefix}"
   end
 
   test do
