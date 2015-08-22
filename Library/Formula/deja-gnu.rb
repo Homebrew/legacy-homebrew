78a36c02565962c0ddcd520bf09c8fa2372b000b
Alex Dunn <dunn.alex@gmail.com>
2015-08-22 09:31:31 -0700

    deja-gnu 1.5.3
    Closes #43177.

--- Library/Formula/deja-gnu.rb
+++ Library/Formula/deja-gnu.rb
@@ -1,14 +1,19 @@
 class DejaGnu < Formula
   desc "Framework for testing other programs"
   homepage "https://www.gnu.org/software/dejagnu/"
-  url "http://ftpmirror.gnu.org/dejagnu/dejagnu-1.5.2.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/dejagnu/dejagnu-1.5.2.tar.gz"
-  sha256 "90b5f3ccd7a4a2def5099a57ae99c7b8f587eb170c3c8df874c5934e1e5d33a2"
+  url "http://ftpmirror.gnu.org/dejagnu/dejagnu-1.5.3.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/dejagnu/dejagnu-1.5.3.tar.gz"
+  sha256 "099b8e364ca1d6248f8e1d32168c4b12677abff4253bbbb4a8ac8cdd321e3f19"
 
-  head "http://git.sv.gnu.org/r/dejagnu.git"
+  head do
+    url "http://git.savannah.gnu.org/r/dejagnu.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
 
   def install
     ENV.j1 # Or fails on Mac Pro
+    system "autoreconf", "-iv" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
