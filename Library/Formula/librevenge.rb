c4f1281ad8a552ebfa772dffd0b96f6a6dff5070
Tim D. Smith <git@tim-smith.us>
2015-08-23 16:41:24 -0700

    librevenge: fix build with Boost 1.59
    https://sourceforge.net/p/libwpd/tickets/6/
    https://sourceforge.net/p/libwpd/librevenge/ci/0beee70d1bf52f0d81b60ee8c373e477991fe546/
    
    Closes #43214.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/librevenge.rb
+++ Library/Formula/librevenge.rb
@@ -15,6 +15,11 @@ class Librevenge < Formula
   depends_on "pkg-config" => :build
   depends_on "boost" => :build
 
+  # Fix build with Boost 1.59
+  # https://sourceforge.net/p/libwpd/tickets/6/
+  # https://sourceforge.net/p/libwpd/librevenge/ci/0beee70d1bf52f0d81b60ee8c373e477991fe546/
+  patch :DATA
+
   def install
     system "./configure", "--without-docs",
                           "--disable-dependency-tracking",
@@ -37,3 +42,17 @@ class Librevenge < Formula
                    "-I#{include}/librevenge-0.0", "-L#{lib}"
   end
 end
+__END__
+diff --git a/src/lib/Makefile.in b/src/lib/Makefile.in
+index d05d38f..692fcd0 100644
+--- a/src/lib/Makefile.in
++++ b/src/lib/Makefile.in
+@@ -467,7 +467,7 @@ librevenge_@RVNG_MAJOR_VERSION@_@RVNG_MINOR_VERSION@_include_HEADERS = \
+ AM_CXXFLAGS = -I$(top_srcdir)/inc $(DEBUG_CXXFLAGS) $(ZLIB_CFLAGS) \
+	$(am__append_1)
+ librevenge_@RVNG_MAJOR_VERSION@_@RVNG_MINOR_VERSION@_la_CPPFLAGS =  \
+-	-DLIBREVENGE_BUILD $(am__append_2)
++	-DLIBREVENGE_BUILD -DBOOST_ERROR_CODE_HEADER_ONLY $(am__append_2)
+ librevenge_@RVNG_MAJOR_VERSION@_@RVNG_MINOR_VERSION@_la_LIBADD = @LIBREVENGE_WIN32_RESOURCE@
+ librevenge_@RVNG_MAJOR_VERSION@_@RVNG_MINOR_VERSION@_la_DEPENDENCIES = @LIBREVENGE_WIN32_RESOURCE@
+ librevenge_@RVNG_MAJOR_VERSION@_@RVNG_MINOR_VERSION@_la_LDFLAGS = $(version_info) -export-dynamic $(no_undefined)
