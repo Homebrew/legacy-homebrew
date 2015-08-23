5db3922758c9d5a0207dbb3366e9559a6cf136b3
Anatoliy Bulukin <bulukin@gmail.com>
2015-08-23 22:42:12 +0300

    update metashell 2.1.0

--- Library/Formula/metashell.rb
+++ Library/Formula/metashell.rb
@@ -1,15 +1,8 @@
 class Metashell < Formula
   desc "Metaprogramming shell for C++ templates"
   homepage "https://github.com/sabel83/metashell"
-  url "https://github.com/sabel83/metashell/archive/v2.0.0.tar.gz"
-  sha256 "5388a6d92b0daecc5e2092554041b70905a1d8ee08bd5ca52c756bde35b4e455"
-
-  bottle do
-    sha1 "35cb28330f73ec44ecf27244347805dc2561439f" => :yosemite
-    sha1 "0d8a62d552ae6ff960ac26fe8344181ed62895d5" => :mavericks
-    sha1 "76874d93ced1815ccfcbddb6da75e8c56aff44e4" => :mountain_lion
-  end
-
+  url "https://github.com/sabel83/metashell/archive/v2.1.0.tar.gz"
+  sha256 "64d3680a536a254de8556a9792c5d35e6709f2f347d7187614271123d87246ee"
   depends_on "cmake" => :build
 
   needs :cxx11
@@ -17,11 +10,16 @@ class Metashell < Formula
   def install
     ENV.cxx11
     # Build internal Clang
-    mkdir "templight/build" do
+    mkdir "3rd/templight/build" do
       system "cmake", "../llvm", "-DLIBCLANG_BUILD_STATIC=ON", *std_cmake_args
       system "make", "clang"
       system "make", "libclang"
       system "make", "libclang_static"
+      system "make", "templight"
+    end
+
+    if MacOS.version == :yosemite
+      system "tools/clang_default_path", "--gcc=clang", ">", "lib/core/extra_sysinclude.hpp"
     end
 
     mkdir "build" do
