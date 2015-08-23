60a6f1014f00dd31470a3a7ba31a2af8c8779591
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-23 16:00:03 +0100

    isync: add 1.2.0 bottle.

--- Library/Formula/isync.rb
+++ Library/Formula/isync.rb
@@ -4,6 +4,13 @@ class Isync < Formula
   url "https://downloads.sourceforge.net/project/isync/isync/1.2.0/isync-1.2.0.tar.gz"
   sha256 "833878de1647d403cb56984757cc416094ee037c5388a0f1d1f74084f6e60e59"
 
+  bottle do
+    cellar :any
+    sha256 "fc87f507e7829efabb36b97c5a6e10647d161146f31cc66b52277823ef8c3633" => :yosemite
+    sha256 "f67194154a8b332576088dd8eca6ed862a5c34c40ab19aa55c70bcaf698bda75" => :mavericks
+    sha256 "efc6fc775a01acbd16f20adf3b6c0e32b13d8400519a913a420fee81fc4b54f9" => :mountain_lion
+  end
+
   head do
     url "git://git.code.sf.net/p/isync/isync"
 
