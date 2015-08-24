771c39e2eb172585bcf249884b06db8dabd2ce72
JP Simard <jp@jpsim.com>
2015-08-24 11:41:51 -0700

    sourcekitten 0.4.5
    bump Xcode requirement to 6.4
    
    Closes #43228.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/sourcekitten.rb
+++ Library/Formula/sourcekitten.rb
@@ -1,7 +1,7 @@
 class Sourcekitten < Formula
   desc "Framework and command-line tool for interacting with SourceKit"
   homepage "https://github.com/jpsim/SourceKitten"
-  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.4.3", :revision => "d3dd7c76ca17b8e43165588a61bd65772e67cdac"
+  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.4.5", :revision => "35508f8c95ecd220719db45b3955d1821f523b7d"
   head "https://github.com/jpsim/SourceKitten.git"
 
   bottle do
@@ -9,7 +9,7 @@ class Sourcekitten < Formula
     sha256 "eddf7ddfb7e49f8219530732fe502ccf181afb8a54f9c6b7ac24b85e7f36bc10" => :yosemite
   end
 
-  depends_on :xcode => ["6.3", :build]
+  depends_on :xcode => ["6.4", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SourceKitten.dst"
