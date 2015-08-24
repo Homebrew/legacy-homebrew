2662eb876450f27632186670feec1cd7b4ee7b19
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-24 19:09:51 +0100

    advancecomp: follow homepage redirect

--- Library/Formula/advancecomp.rb
+++ Library/Formula/advancecomp.rb
@@ -1,6 +1,6 @@
 class Advancecomp < Formula
   desc "Recompression utilities for .PNG, .MNG, .ZIP, and .GZ files"
-  homepage "http://advancemame.sourceforge.net/comp-readme.html"
+  homepage "http://www.advancemame.it/comp-readme.html"
   url "https://github.com/amadvance/advancecomp/releases/download/v1.20/advancecomp-1.20.tar.gz"
   sha256 "590a447cfc7ab3a37ec707e13967a0046a81a888c561ebaff5415b1e946da67b"
 
@@ -13,8 +13,7 @@ class Advancecomp < Formula
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--enable-bzip2",
-                          "--prefix=#{prefix}", "--mandir=#{man}"
+                          "--enable-bzip2", "--prefix=#{prefix}", "--mandir=#{man}"
     system "make", "install"
   end
 
