0bca65c76693f2331ab6ebb0a72e9e1d0ab22ec8
JP Simard <jp@jpsim.com>
2015-08-24 13:24:09 -0700

    swiftlint 0.1.2
    SwiftLint 0.1.2
    
    Closes #43232.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/swiftlint.rb
+++ Library/Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/realm/SwiftLint"
-  url "https://github.com/realm/SwiftLint.git", :tag => "0.1.1", :revision => "76939117e8db4cff7ccd5d362340cbc6a7af878c"
+  url "https://github.com/realm/SwiftLint.git", :tag => "0.1.2", :revision => "4c91500e7b90a1d651d16e9c604ab927da050e8f"
   head "https://github.com/realm/SwiftLint.git"
 
   bottle do
@@ -9,7 +9,7 @@ class Swiftlint < Formula
     sha256 "c2d3dfadfe93370770d98d0aa8c562a79a279228f7aa4d04ae2226c5d276bad0" => :yosemite
   end
 
-  depends_on :xcode => ["6.3", :build]
+  depends_on :xcode => ["6.4", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
