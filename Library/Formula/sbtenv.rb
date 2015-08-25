b40dc4bbc0364ed0a54678766ab180204b8f674c
Dan Martinez <dfm@razorwind.org>
2015-08-25 14:26:34 -0700

    sbtenv: add a description
    Closes #43281.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/sbtenv.rb
+++ Library/Formula/sbtenv.rb
@@ -1,4 +1,5 @@
 class Sbtenv < Formula
+  desc "Command-line tool for managing sbt environments"
   homepage "https://github.com/mazgi/sbtenv"
   url "https://github.com/mazgi/sbtenv/archive/version/0.0.9.tar.gz"
   sha256 "0c5809eda41a0041d073bb22e92e8de00a8f17b91af2b78c32f0cf5ebea2cd54"
