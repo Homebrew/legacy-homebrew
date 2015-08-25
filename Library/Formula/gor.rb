e1c659aec167d2c96611708a3a578c4ef8bb70e4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-25 21:07:31 +0100

    gor: add 0.9.8 bottle.

--- Library/Formula/gor.rb
+++ Library/Formula/gor.rb
@@ -7,6 +7,13 @@ class Gor < Formula
   sha256 "be29561352db89d5d8892443d1a6b1bb8454ec28b00854d2aecb7d91d570f4b3"
   head "https://github.com/buger/gor.git"
 
+  bottle do
+    cellar :any
+    sha256 "0ce768fad5dc8294e36a401d8135b02586e75525d402c8cd71818a1ac1e1b40a" => :yosemite
+    sha256 "a6d61251f498b304a65c2e295af2c464f16d4647a8ed9559f6caa69e58f6d1fa" => :mavericks
+    sha256 "97de5a69f364547fe8823e07c01be2d574cd10bb114fadf7268307218c6bb93b" => :mountain_lion
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/bitly/go-hostpool" do
