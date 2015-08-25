5d550689d6b8f58881e3aa872da992be81fadd84
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-25 04:18:46 +0100

    formula: use which over locate

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -768,12 +768,12 @@ class Formula
     # New Homebrew installs may not be a git repo yet
     return false unless File.exist? "#{HOMEBREW_PREFIX}/.git"
 
-    git = MacOS.locate("git")
+    git = which("git")
     # git isn't installed by older Xcodes
     return false if git.nil?
 
     # /usr/bin/git is a popup stub when Xcode/CLT aren't installed, so bail out
-    return false if git.dirname.to_s == "/usr/bin" && !MacOS.has_apple_developer_tools?
+    return false if git == "/usr/bin/git" && !MacOS.has_apple_developer_tools?
 
     path.parent.cd do
       diff = Utils.popen_read("git", "diff", "origin/master", "--", "#{path}")
