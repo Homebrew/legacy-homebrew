80c0e6b79c9e6f1dbe65f51be779cf42f39ebc65
Stuart Williams <stuart@sidereal.ca>
2015-08-25 00:50:34 -0400

    pig 0.15.0
    Closes #43250.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/pig.rb
+++ Library/Formula/pig.rb
@@ -1,9 +1,9 @@
 class Pig < Formula
   desc "Platform for analyzing large data sets"
   homepage "https://pig.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=pig/pig-0.14.0/pig-0.14.0.tar.gz"
-  mirror "https://archive.apache.org/dist/pig/pig-0.14.0/pig-0.14.0.tar.gz"
-  sha256 "6aea66dda4791f82bad9654ec5290efc6179d333077b0ce9f07624c9c8b071a0"
+  url "https://www.apache.org/dyn/closer.cgi?path=pig/pig-0.15.0/pig-0.15.0.tar.gz"
+  mirror "https://archive.apache.org/dist/pig/pig-0.15.0/pig-0.15.0.tar.gz"
+  sha256 "c52112ca618daaca298cf068e6451449fe946e8dccd812d56f8f537aa275234b"
 
   bottle do
     cellar :any
@@ -13,8 +13,6 @@ class Pig < Formula
   end
 
   depends_on :java
-
-  patch :DATA
 
   def install
     (libexec/"bin").install "bin/pig"
@@ -26,27 +24,6 @@ class Pig < Formula
     (testpath/"test.pig").write <<-EOS.undent
       sh echo "Hello World"
     EOS
-    system bin/"pig", "-x", "local", "test.pig"
+    assert_match /Hello World/, shell_output("#{bin}/pig -x local test.pig")
   end
 end
-
-# There's something weird with Pig's launch script, it doesn't find the correct
-# path. This patch finds PIG_HOME from the pig binary path's symlink.
-__END__
-diff -u a/bin/pig b/bin/pig
---- a/bin/pig 2011-09-30 08:55:58.000000000 +1000
-+++ b/bin/pig 2011-11-28 11:18:36.000000000 +1100
-@@ -77,11 +77,8 @@
-
- # resolve links - $0 may be a softlink
- this="${BASH_SOURCE-$0}"
--
--# convert relative path to absolute path
--bin=$(cd -P -- "$(dirname -- "$this")">/dev/null && pwd -P)
--script="$(basename -- "$this")"
--this="$bin/$script"
-+here=$(dirname $this)
-+this="$here"/$(readlink $this)
-
- # the root of the Pig installation
- export PIG_HOME=`dirname "$this"`/..
