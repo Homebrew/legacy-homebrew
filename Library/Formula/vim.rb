e3bdd616c71b55851209c25e805934a58189a245
Saket Choudhary <saketkc@gmail.com>
2015-08-25 22:00:14 -0700

    vim 7.4.838

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi \"workalike\" with many additional features"
   homepage "http://www.vim.org/"
   # This package tracks debian-unstable: https://packages.debian.org/unstable/vim
-  url "https://mirrors.kernel.org/debian/pool/main/v/vim/vim_7.4.826.orig.tar.gz"
-  sha256 "02f07b60eff53f45d58686e43b72e83aa8f24a94acfa69b95fa84dc020671a38"
+  url "https://github.com/vim/vim/archive/v7.4.838.tar.gz"
+  sha256 "81475125e84af264c6d287d7f0c1e11aaa73c24e27eaf0babe4639ec766b6a59"
   head "https://github.com/vim/vim.git"
 
   # We only have special support for finding depends_on :python, but not yet for
