fcba135b15706fbafe95c9c77a737e4fc600d535
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-26 01:29:13 +0100

    bro: use var for spool/logs
    Closes #42179.

--- Library/Formula/bro.rb
+++ Library/Formula/bro.rb
@@ -1,12 +1,9 @@
 class Bro < Formula
   desc "Network security monitor"
   homepage "https://www.bro.org"
+  url "https://www.bro.org/downloads/release/bro-2.4.tar.gz"
+  sha256 "740c0d0b0bec279c2acef5e1b6b4d0016c57cd02a729f5e2924ae4a922e208b2"
   head "https://github.com/bro/bro.git"
-
-  stable do
-    url "https://www.bro.org/downloads/release/bro-2.4.tar.gz"
-    sha256 "740c0d0b0bec279c2acef5e1b6b4d0016c57cd02a729f5e2924ae4a922e208b2"
-  end
 
   bottle do
     sha256 "6ebeb99f7435427fea693cb6a7a1484cbf065a501af389ff6a86b139a16a7cbe" => :yosemite
@@ -16,11 +13,12 @@ class Bro < Formula
 
   depends_on "cmake" => :build
   depends_on "swig" => :build
-  depends_on "geoip" => :recommended
   depends_on "openssl"
+  depends_on "geoip" => :recommended
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--with-openssl=#{Formula["openssl"].opt_prefix}"
+    system "./configure", "--prefix=#{prefix}", "--with-openssl=#{Formula["openssl"].opt_prefix}",
+                          "--localstatedir=#{var}", "--conf-files-dir=#{etc}"
     system "make", "install"
   end
 
