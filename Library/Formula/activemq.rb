9226e9b69e10ccb0478a208cb5a5216fbe68120d
Stuart Williams <stuart@sidereal.ca>
2015-08-27 01:33:15 -0400

    activemq 5.12.0

--- Library/Formula/activemq.rb
+++ Library/Formula/activemq.rb
@@ -1,8 +1,8 @@
 class Activemq < Formula
   desc "Apache ActiveMQ: powerful open source messaging server"
   homepage "https://activemq.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=/activemq/5.11.1/apache-activemq-5.11.1-bin.tar.gz"
-  sha256 "5ae90f4ea6caa3af7d9f79d1cc55b575dd44170b1451f096494e1a356828d35f"
+  url "https://www.apache.org/dyn/closer.cgi?path=/activemq/5.12.0/apache-activemq-5.12.0-bin.tar.gz"
+  sha256 "b14bbeef71800365cd4278465f5bfb31757f56ebd5206f3e01ae353041c5a297"
 
   depends_on :java => "1.6+"
 
@@ -10,10 +10,20 @@ class Activemq < Formula
     rm_rf Dir["bin/linux-x86-*"]
     libexec.install Dir["*"]
     (bin/"activemq").write_env_script libexec/"bin/activemq", Language::Java.java_home_env("1.6+")
-    (bin/"activemq-admin").write_env_script libexec/"bin/activemq-admin", Language::Java.java_home_env("1.6+")
   end
 
   test do
-    system "#{bin}/activemq-admin", "browse", "-h"
+    ENV["ACTIVEMQ_PIDFILE"] = testpath/"activemq.pid"
+    pid = fork do
+      exec "#{bin}/activemq start"
+    end
+    sleep 5
+
+    begin
+      assert_match /ActiveMQ is running/, shell_output("#{bin}/activemq status")
+    ensure
+      system "#{bin}/activemq", "stop"
+      Process.wait(pid)
+    end
   end
 end
