c032822d34bf2775dad6ab7a71dd28ae17c2c3e6
Otto Jongerius <ojongerius@atlassian.com>
2015-08-27 18:29:47 +1000

    Install new version of Ngrok.

--- Library/Formula/ngrok.rb
+++ Library/Formula/ngrok.rb
@@ -2,8 +2,8 @@ class Ngrok < Formula
   desc "Expose localhost to the internet and capture traffic for replay"
   homepage "https://ngrok.com"
   head "https://github.com/inconshreveable/ngrok.git"
-  url "https://github.com/inconshreveable/ngrok/archive/1.7.tar.gz"
-  sha256 "da9a063bc2f2cd6f14018c4e00304225d949fe42d525ed40d9be8f35daf4c23c"
+  url "https://github.com/inconshreveable/ngrok/archive/1.7.1.tar.gz"
+  sha256 "ffb1ad90d5aa3c8b3a9bfd8109a71027645a78d5e61fccedd0873fee060da665"
 
   bottle do
     sha1 "71920267073415ccc1ea1305bac6ce51e00d4833" => :mavericks
