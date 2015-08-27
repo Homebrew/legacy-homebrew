74a124ce3cbd01b4e4a2a1883695220e89ce7ed8
Tzu Dot <tzudot@protonmail.com>
2015-08-27 15:45:23 -0400

    speex: version bump to 1.2rc3

--- Library/Formula/speex.rb
+++ Library/Formula/speex.rb
@@ -1,12 +1,11 @@
 class Speex < Formula
   desc "Audio codec designed for speech"
   homepage "http://speex.org"
-  url "http://downloads.us.xiph.org/releases/speex/speex-1.2rc1.tar.gz"
-  sha256 "342f30dc57bd4a6dad41398365baaa690429660b10d866b7d508e8f1179cb7a6"
+  url "http://downloads.xiph.org/releases/speex/speexdsp-1.2rc3.tar.gz"
+  sha256 "4ae688600039f5d224bdf2e222d2fbde65608447e4c2f681585e4dca6df692f1"
 
   bottle do
     cellar :any
-    revision 2
     sha1 "035c405657c5debb5e41d291bb44f508797a7b51" => :yosemite
     sha1 "123e086d2548614ff66691f46e6f6e3dce3fa362" => :mavericks
     sha1 "d9cb07f7de4d226c25d0b8ddbddd3fb0de5f5c53" => :mountain_lion
@@ -17,8 +16,11 @@ class Speex < Formula
 
   def install
     ENV.j1
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    args = ["--disable-debug",
+            "--disable-dependency-tracking",
+            "--prefix=#{prefix}"]
+
+    system "./configure", *args
     system "make", "install"
   end
 end
