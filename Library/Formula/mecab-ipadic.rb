9a555d1e6c27e91a7d5b14eae31d06f1691a0322
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-27 13:14:08 +0100

    mecab-ipadic: add 2.7.0-20070801 bottle.

--- Library/Formula/mecab-ipadic.rb
+++ Library/Formula/mecab-ipadic.rb
@@ -4,6 +4,13 @@ class MecabIpadic < Formula
   url "https://downloads.sourceforge.net/project/mecab/mecab-ipadic/2.7.0-20070801/mecab-ipadic-2.7.0-20070801.tar.gz"
   sha256 "b62f527d881c504576baed9c6ef6561554658b175ce6ae0096a60307e49e3523"
 
+  bottle do
+    cellar :any
+    sha256 "55703c812de3e7cff503b9cd1eafa0656b3f17c4885165ce4d8e4d2b2356050e" => :yosemite
+    sha256 "0a9ea36b7cc03f73ae34f72e078b7e84ebe814cf8e1cfbea2d5f876c1893b1c5" => :mavericks
+    sha256 "c70d627c447086a66b6eaca37cd98e1da099f65bfb6b87f2c47d1901d5e4a090" => :mountain_lion
+  end
+
   # Via ./configure --help, valid choices are utf8 (default), euc-jp, sjis
   option "charset=", "Select charset: utf8 (default), euc-jp, or sjis"
 
