5045fd02ad0faf3188768648f3ff8e7bc0c73365
Xu Cheng <xucheng@me.com>
2015-08-27 17:16:28 +0800

    mecab-ko: fix for sandbox

--- Library/Formula/mecab-ko.rb
+++ Library/Formula/mecab-ko.rb
@@ -5,19 +5,24 @@ class MecabKo < Formula
   version "0.996-ko-0.9.2"
   sha256 "d0e0f696fc33c2183307d4eb87ec3b17845f90b81bf843bd0981e574ee3c38cb"
 
-  def install
-    # https://bitbucket.org/eunjeon/mecab-ko/pull-request/1/mecab-ko-ipadic-ipadic/diff
-    # Upstream decided not to comment out the dicdir path but replaced 'ipadic'
-    # with 'mecab-ko-dic' instead in mecabrc file. Though the change resolves
-    # the error of mecab-ko-dic path by source installation, it still doesn't
-    # fit with Homebrew as it's expected installed under /usr/local/ with
-    # mecab-ko-dic regardless of version.
-    inreplace "mecabrc.in",
-      "@prefix@/lib/mecab/dic/mecab-ko-dic",
-      Formula["mecab-ko-dic"].opt_prefix
+  conflicts_with "mecab", :becasue => "both install mecab binaries"
 
+  def install
     system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
     system "make", "install"
+
+    # Put dic files in HOMEBREW_PREFIX/lib instead of lib
+    inreplace "#{bin}/mecab-config", "${exec_prefix}/lib/mecab/dic", "#{HOMEBREW_PREFIX}/lib/mecab/dic"
+    inreplace "#{etc}/mecabrc", "#{lib}/mecab/dic", "#{HOMEBREW_PREFIX}/lib/mecab/dic"
+  end
+
+  def post_install
+    (HOMEBREW_PREFIX/"lib/mecab/dic").mkpath
+  end
+
+  test do
+    assert_equal "#{HOMEBREW_PREFIX}/lib/mecab/dic", shell_output("#{bin}/mecab-config --dicdir").chomp
   end
 end
