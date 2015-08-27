2c922b0d330f0143bb55cf15467909be05f7869b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-27 13:14:08 +0100

    mecab-ko: add 0.996-ko-0.9.2 bottle.

--- Library/Formula/mecab-ko.rb
+++ Library/Formula/mecab-ko.rb
@@ -5,6 +5,13 @@ class MecabKo < Formula
   version "0.996-ko-0.9.2"
   sha256 "d0e0f696fc33c2183307d4eb87ec3b17845f90b81bf843bd0981e574ee3c38cb"
 
+  bottle do
+    cellar :any
+    sha256 "d57dab9422f254ad789d64a18595d1ebe264042261781b1583ce6bdaf6ee8bdf" => :yosemite
+    sha256 "f8fc6c61e958af698fef737c4eb133a46cdd3f907beaeac5765e9056f816fd97" => :mavericks
+    sha256 "5193bbfc4308dc54ce44f1a9c46f89040e3c8bd0a2d389afd44ef240e98f6838" => :mountain_lion
+  end
+
   conflicts_with "mecab", :becasue => "both install mecab binaries"
 
   def install
