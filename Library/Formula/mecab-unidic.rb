3ad2bd7ffea5072a5f92af72681644e00e3b025e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-27 13:14:08 +0100

    mecab-unidic: add 2.1.2 bottle.

--- Library/Formula/mecab-unidic.rb
+++ Library/Formula/mecab-unidic.rb
@@ -4,6 +4,13 @@ class MecabUnidic < Formula
   url "http://sourceforge.jp/frs/redir.php?m=iij&f=%2Funidic%2F58338%2Funidic-mecab-2.1.2_src.zip"
   sha256 "6cce98269214ce7de6159f61a25ffc5b436375c098cc86d6aa98c0605cbf90d4"
 
+  bottle do
+    cellar :any
+    sha256 "6c41c6ebd2ebe27af0b92beae18b1eb8abb0d0a4830b78f16b8a3feb1f42c8fe" => :yosemite
+    sha256 "de02ba717c7f724d305b337ef944983d310e78fb6c58169abd1aea62f0a197e4" => :mavericks
+    sha256 "9bb2a59eb6345f1a0d3da5042c0b586cad2b8227512fcb907c0c2b83bc100c29" => :mountain_lion
+  end
+
   depends_on "mecab"
 
   link_overwrite "lib/mecab/dic"
