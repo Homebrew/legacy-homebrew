1b0f38b598cfd412cd72ce1604e46c81cd60fe59
Tomasz Pajor <tomek@polishgeeks.com>
2015-08-28 08:06:13 +0200

    pdfgrep 1.4.1

--- Library/Formula/pdfgrep.rb
+++ Library/Formula/pdfgrep.rb
@@ -1,15 +1,8 @@
 class Pdfgrep < Formula
   desc "Search PDFs for strings matching a regular expression"
   homepage "https://pdfgrep.org/"
-  url "https://pdfgrep.org/download/pdfgrep-1.3.2.tar.gz"
-  sha256 "386b167434443dd299d389a0ef292d708123255cbab0e179e11b65ba51d9b386"
-
-  head do
-    url "https://gitlab.com/pdfgrep/pdfgrep.git"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-    depends_on "asciidoc" => :build
-  end
+  url "https://pdfgrep.org/download/pdfgrep-1.4.1.tar.gz"
+  sha256 "db04a210e6bb7b77cd6c54b17f0f6fed0d123a85f97a541b270736a5d3840f2c"
 
   bottle do
     cellar :any
@@ -19,7 +12,15 @@ class Pdfgrep < Formula
     sha256 "e1a02806c1024b3f3a1cf745617d2e4118687db1d0130ef369c0f171ce52fc8a" => :mountain_lion
   end
 
+  head do
+    url "https://gitlab.com/pdfgrep/pdfgrep.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "asciidoc" => :build
+  end
+
   depends_on "pkg-config" => :build
+  depends_on "pcre"
   depends_on "poppler"
 
   def install
