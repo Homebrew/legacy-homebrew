00bde7364dac4dcc1df867e67d3df56d0dff6494
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-28 17:56:02 +0200

    camlistore: use new URL, add conflict with 'hello'

--- Library/Formula/camlistore.rb
+++ Library/Formula/camlistore.rb
@@ -1,15 +1,17 @@
 class Camlistore < Formula
   desc "Content-addressable multi-layer indexed storage"
   homepage "http://camlistore.org"
-  head "https://camlistore.googlesource.com/camlistore", :using => :git
-  url "https://github.com/bradfitz/camlistore/archive/0.8.tar.gz"
+  url "https://github.com/camlistore/camlistore/archive/0.8.tar.gz"
   sha256 "61b75708ae25ac4dc1c5c31c1cf8f806ccaafaaacf618caf1aa9d31489fec50f"
+  head "https://camlistore.googlesource.com/camlistore", :using => :git
 
   bottle do
     sha1 "0e23421d8dcd222bdaebbd9cdd4027f570e9c76d" => :mavericks
     sha1 "2833a6aadcb6b11fe31fc7b8adef4a104dd06023" => :mountain_lion
     sha1 "f72efd9e3c4654a0520b1ecf9991e2a23ea4cdad" => :lion
   end
+
+  conflicts_with "hello", :because => "both install `hello` binaries"
 
   depends_on "pkg-config" => :build
   depends_on "go" => :build
