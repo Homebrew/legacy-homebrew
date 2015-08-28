15592bc4e72ec08e37385cce26afa6d9cfd06448
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-28 12:14:07 +0100

    deja-gnu: add 1.5.3 bottle.

--- Library/Formula/deja-gnu.rb
+++ Library/Formula/deja-gnu.rb
@@ -5,6 +5,13 @@ class DejaGnu < Formula
   mirror "https://ftp.gnu.org/gnu/dejagnu/dejagnu-1.5.3.tar.gz"
   sha256 "099b8e364ca1d6248f8e1d32168c4b12677abff4253bbbb4a8ac8cdd321e3f19"
 
+  bottle do
+    cellar :any
+    sha256 "a77ab52f9f7db8a6862122e8b675229b544c6b02b5a8e7b6016af825b502c4a5" => :yosemite
+    sha256 "eb5ee1df1704093d1332728ef12e497ae824a78895e62eacabe60a4442ff8ddd" => :mavericks
+    sha256 "6c93bd2a93a51f94d1b980fe0b9172a1bf91107777b309fbbf9342a4d085c498" => :mountain_lion
+  end
+
   head do
     url "http://git.savannah.gnu.org/r/dejagnu.git"
     depends_on "automake" => :build
