db5a71c9fd8dba0116dc2d2bf59704ac44c9e219
Drew Sh <drewshg@gmail.com>
2015-08-29 11:31:26 -0700

    ntopng: fix HEAD
    Closes #43383.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/ntopng.rb
+++ Library/Formula/ntopng.rb
@@ -3,12 +3,19 @@ class Ntopng < Formula
   homepage "http://www.ntop.org/products/ntop/"
   url "https://downloads.sourceforge.net/project/ntop/ntopng/ntopng-2.0.tar.gz"
   sha256 "3cbfd6de1bc44d65f7c7f0de282d122d11f493f1261ba137c3b5b202e08e0251"
-  head "https://github.com/ntop/ntopng.git"
 
   bottle do
     sha256 "c3a9c8be354c9fd3cbed7f84a6b9585d2b2d9bd1111a44e68c37694ea85707ba" => :yosemite
     sha256 "9fbf8cad7c28f36c59bb1db44464d2bece35566721b3f4c54c91deb473f76641" => :mavericks
     sha256 "355c68400b80698448e8197316a015cb88563600cb8a619df20d0bef2e79b0ca" => :mountain_lion
+  end
+
+  head do
+    url "https://github.com/ntop/ntopng.git", :branch => "dev"
+
+    resource "nDPI" do
+      url "https://github.com/ntop/nDPI.git", :branch => "dev"
+    end
   end
 
   depends_on "autoconf" => :build
@@ -27,6 +34,13 @@ class Ntopng < Formula
   depends_on "redis"
 
   def install
+    if build.head?
+      resource("nDPI").stage do
+        system "./autogen.sh"
+        system "make"
+        (buildpath/"nDPI").install Dir["*"]
+      end
+    end
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make"
