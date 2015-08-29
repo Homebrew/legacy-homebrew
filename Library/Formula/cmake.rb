205b50f0f03038ac27687c603e381ab9b4c1702a
Alex Dunn <dunn.alex@gmail.com>
2015-08-28 23:22:26 -0700

    cmake: add mirror

--- Library/Formula/cmake.rb
+++ Library/Formula/cmake.rb
@@ -2,6 +2,7 @@ class Cmake < Formula
   desc "Cross-platform make"
   homepage "http://www.cmake.org/"
   url "http://www.cmake.org/files/v3.3/cmake-3.3.1.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/c/cmake/cmake_3.3.1.orig.tar.gz"
   sha256 "cd65022c6a0707f1c7112f99e9c981677fdd5518f7ddfa0f778d4cee7113e3d6"
   head "http://cmake.org/cmake.git"
 
