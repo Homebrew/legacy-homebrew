f5736bfefc3de72f41a43205543d9594faf91a50
Xu Cheng <xucheng@me.com>
2015-08-29 15:43:56 +0800

    capstone: fix test

--- Library/Formula/capstone.rb
+++ Library/Formula/capstone.rb
@@ -29,7 +29,34 @@ class Capstone < Formula
   end
 
   test do
-    # Given the build issues around prefix, check is actually in the Cellar.
-    assert File.exist? "#{lib}/libcapstone.a"
+    # code comes from http://www.capstone-engine.org/lang_c.html
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <inttypes.h>
+      #include <capstone/capstone.h>
+      #define CODE "\\x55\\x48\\x8b\\x05\\xb8\\x13\\x00\\x00"
+
+      int main()
+      {
+        csh handle;
+        cs_insn *insn;
+        size_t count;
+        if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK)
+          return -1;
+        count = cs_disasm(handle, CODE, sizeof(CODE)-1, 0x1000, 0, &insn);
+        if (count > 0) {
+          size_t j;
+          for (j = 0; j < count; j++) {
+            printf("0x%"PRIx64":\\t%s\\t\\t%s\\n", insn[j].address, insn[j].mnemonic,insn[j].op_str);
+          }
+          cs_free(insn, count);
+        } else
+          printf("ERROR: Failed to disassemble given code!\\n");
+        cs_close(&handle);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lcapstone", "-o", "test"
+    system "./test"
   end
 end
