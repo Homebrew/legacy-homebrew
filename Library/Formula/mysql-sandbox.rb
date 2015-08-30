4931088542d001e8b508c9b995a83f73dd9e5d32
Denis Denisov <denji0k@gmail.com>
2015-08-30 04:23:47 +0300

    mysql-sandbox 3.1.0

--- Library/Formula/mysql-sandbox.rb
+++ Library/Formula/mysql-sandbox.rb
@@ -1,8 +1,9 @@
 class MysqlSandbox < Formula
   desc "Install one or more MySQL servers"
   homepage "http://mysqlsandbox.net"
-  url "https://launchpad.net/mysql-sandbox/mysql-sandbox-3/mysql-sandbox-3/+download/MySQL-Sandbox-3.0.50.tar.gz"
-  sha256 "c709c4dec711ab37265c5c596330ad3af01866f418e8158cdf949efcdcab96d8"
+  url "https://github.com/datacharmer/mysql-sandbox/archive/3.1.00.tar.gz"
+  sha256 "50934d23ec40b209cc08c19a7b38ea70e1f15f00845f91da0581e7e1cba4cf29"
+  head "https://github.com/datacharmer/mysql-sandbox.git"
 
   bottle do
     sha256 "1ad86e367e36e62fe32b15fc93129f0f8ef5ab43f0e2ab171df22361fc3b48ff" => :yosemite
@@ -11,11 +12,17 @@ class MysqlSandbox < Formula
   end
 
   def install
-    ENV.prepend_create_path "PERL5LIB", "#{HOMEBREW_PREFIX}/lib/perl5/site_perl"
+    # Theoretically, we could reinsert a patch here submitted upstream previously
+    # but the check for PERL_LIB remains in place and incompatible with Homebrew.
+    # Using an env and scripting is a solution less likely to break over time.
+    # Both variables need to be set. One is compile-time, the other run-time.
+    ENV["PERL_LIBDIR"] = libexec/"lib/perl5"
+    ENV.prepend_create_path "PERL5LIB", libexec+"lib/perl5"
 
     system "perl", "Makefile.PL", "PREFIX=#{prefix}"
     system "make", "test", "install"
 
+    bin.install Dir["#{libexec}/bin/*"]
     bin.env_script_all_files(libexec+"bin", :PERL5LIB => ENV["PERL5LIB"])
   end
 end
