11a33d0cf9717a2985c2f8fe0f3aa407f553976f
Dominyk Tiller <dominyktiller@gmail.com>
2015-08-30 01:45:23 +0100

    clucene: update patch links
    Closes #43359.
    
    Closes #43388.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/clucene.rb
+++ Library/Formula/clucene.rb
@@ -3,6 +3,7 @@ class Clucene < Formula
   homepage "http://clucene.sourceforge.net"
   url "https://downloads.sourceforge.net/project/clucene/clucene-core-unstable/2.3/clucene-core-2.3.3.4.tar.gz"
   sha256 "ddfdc433dd8ad31b5c5819cc4404a8d2127472a3b720d3e744e8c51d79732eab"
+  head "git://clucene.git.sourceforge.net/gitroot/clucene/clucene"
 
   bottle do
     cellar :any
@@ -11,24 +12,26 @@ class Clucene < Formula
     sha1 "0564c414eca5a3d65eb8a217c03c114ffba1641e" => :lion
   end
 
-  head "git://clucene.git.sourceforge.net/gitroot/clucene/clucene"
-
   depends_on "cmake" => :build
 
   # Portability fixes for 10.9+
   # Upstream ticket: http://sourceforge.net/p/clucene/bugs/219/
   patch do
-    url "https://trac.macports.org/export/126047/trunk/dports/devel/clucene/files/patch-src-shared-CLucene-LuceneThreads.h.diff"
+    url "https://raw.githubusercontent.com/DomT4/scripts/46d9672f7/Homebrew_Resources/MacPorts_Import/clucene/r126047/patch-src-shared-CLucene-LuceneThreads.h.diff"
+    mirror "https://trac.macports.org/export/126047/trunk/dports/devel/clucene/files/patch-src-shared-CLucene-LuceneThreads.h.diff"
     sha256 "42cb23fa6bd66ca8ea1d83a57a650f71e0ad3d827f5d74837b70f7f72b03b490"
   end
 
   patch do
-    url "https://trac.macports.org/export/126047/trunk/dports/devel/clucene/files/patch-src-shared-CLucene-config-repl_tchar.h.diff"
+    url "https://raw.githubusercontent.com/DomT4/scripts/46d9672f7/Homebrew_Resources/MacPorts_Import/clucene/r126047/patch-src-shared-CLucene-config-repl_tchar.h.diff"
+    mirror "https://trac.macports.org/export/126047/trunk/dports/devel/clucene/files/patch-src-shared-CLucene-config-repl_tchar.h.diff"
     sha256 "b7dc735f431df409aac63dcfda9737726999eed4fdae494e9cbc1d3309e196ad"
   end
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
   end
 end
