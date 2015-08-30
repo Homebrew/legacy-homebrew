9faadeccd50c5f79879a0ad0611fe4e6acde8790
Jun Lin <linjunpop@gmail.com>
2015-08-30 15:20:21 +0800

    imessage-ruby 0.3.0
    Closes #43396.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/imessage-ruby.rb
+++ Library/Formula/imessage-ruby.rb
@@ -1,8 +1,8 @@
 class ImessageRuby < Formula
   desc "Command-line tool to send iMessage"
   homepage "https://github.com/linjunpop/imessage"
-  url "https://github.com/linjunpop/imessage/archive/v0.2.0.tar.gz"
-  sha256 "5d67df55a53d0fa657ea439c240a3b322dd0b83a102fa881915569f44431eb00"
+  url "https://github.com/linjunpop/imessage/archive/v0.3.1.tar.gz"
+  sha256 "74ccd560dec09dcf0de28cd04fc4d512812c3348fc5618cbb73b6b36c43e14ef"
   head "https://github.com/linjunpop/imessage.git"
 
   bottle do
@@ -14,7 +14,7 @@ class ImessageRuby < Formula
   depends_on :macos => :mavericks
 
   def install
-    rake "install", "prefix=#{prefix}"
+    rake "standalone:install", "prefix=#{prefix}"
   end
 
   test do
