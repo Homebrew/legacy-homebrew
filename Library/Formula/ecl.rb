32279deb4eeaf3fc90f395c4c7b935f7fde34b1a
Tomasz Pajor <tomek@polishgeeks.com>
2015-08-30 13:22:20 +0200

    ecl 16.0.0

--- Library/Formula/ecl.rb
+++ Library/Formula/ecl.rb
@@ -1,8 +1,8 @@
 class Ecl < Formula
   desc "Embeddable Common Lisp"
-  homepage "http://ecls.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/ecls/ecls/15.3/ecl-15.3.7.tgz"
-  sha256 "2dc6ffbbf1e0a7b1323d49a991ba1f005127ca3e153651d91ba9e65bdaec948f"
+  homepage "https://common-lisp.net/project/ecl/"
+  url "https://common-lisp.net/project/ecl/files/ecl-16.0.0.tgz"
+  sha256 "343ed4c3e4906562757a6039b85ce16d33dd5e8001d74004936795983e3af033"
 
   head "https://gitlab.com/embeddable-common-lisp/ecl.git"
 
@@ -10,11 +10,6 @@ class Ecl < Formula
     sha256 "8b84486abced53a7bbb733113dfa2c1e521214ae116bca83d98d91611ae7c143" => :yosemite
     sha256 "b3686d7469e616ab4bbabd67b4c19a7d65c617f5ae1b5253cff847da03b85ed0" => :mavericks
     sha256 "8fa7523cebcef944a2a6538ef89dd59119bfce64ae5abcaaaf36c5c5c7def4bf" => :mountain_lion
-  end
-
-  devel do
-    url "https://common-lisp.net/project/ecl/files/ecl-16.0.0-rc1.tgz"
-    sha256 "014acdd8ae85d70b90a83008dcd3389aeca0ce2f255b15c8d3d25bec8b222c74"
   end
 
   depends_on "gmp"
