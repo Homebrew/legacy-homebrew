392ad6a3c2e7015ef20be8c016c3e0da6de45f9c
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-08-31 15:54:33 +0530

    evince 3.16.1 (new formula)
    has libspectre as dependency
    libsecret, which is also a dependency, was outdated
    
    Closes #43437.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Library/Formula/evince.rb
@@ -0,0 +1,43 @@
+class Evince < Formula
+  desc "GNOME document viewer"
+  homepage "https://wiki.gnome.org/Apps/Evince"
+  url "https://download.gnome.org/sources/evince/3.16/evince-3.16.1.tar.xz"
+  sha256 "06ff75065b2a30cd588c402f6bd2ea88ee3166181805e0cc00bd54f71dbf6130"
+
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "itstool" => :build
+  depends_on "poppler"
+  depends_on "libxml2" => "with-python"
+  depends_on "gtk+3"
+  depends_on "hicolor-icon-theme"
+  depends_on "gnome-icon-theme"
+  depends_on "libsecret"
+  depends_on "libspectre"
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    # forces use of gtk3-update-icon-cache instead of gtk-update-icon-cache. No bugreport should
+    # be filed for this since it only occurs because Homebrew renames gtk+3's gtk-update-icon-cache
+    # to gtk3-update-icon-cache in order to avoid a collision between gtk+ and gtk+3.
+    inreplace "data/Makefile.in", "gtk-update-icon-cache", "gtk3-update-icon-cache"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--disable-nautilus",
+                          "--disable-schemas-compile",
+                          "--disable-browser-plugin"
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
+    system "make", "install"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+  end
+
+  test do
+    assert_match /#{version}/, shell_output("#{bin}/evince --version")
+  end
+end
