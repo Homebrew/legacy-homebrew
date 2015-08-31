2c93134238aa436af3f8f35f24552fce47fb2b6c
CNA-Bld <cna.bld@gmail.com>
2015-09-01 03:25:19 +0800

    implement prioritizing for requirement too

--- Library/Homebrew/software_spec.rb
+++ Library/Homebrew/software_spec.rb
@@ -48,6 +48,7 @@ class SoftwareSpec
     @name = owner.name
     @full_name = owner.full_name
     @dependency_collector.deps.each { |dep| dep.owner_tap = owner.tap }
+    @dependency_collector.requirements.each { |req| req.owner_tap = owner.tap }
     @bottle_specification.tap = owner.tap
     @owner = owner
     @resource.owner = self
