3fa424480c53ec5676c46d3915ccf870c83591ec
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-31 08:32:14 +0100

    rdiff-backup: add 1.2.8 bottle.

--- Library/Formula/rdiff-backup.rb
+++ Library/Formula/rdiff-backup.rb
@@ -4,6 +4,13 @@ class RdiffBackup < Formula
   url "http://savannah.nongnu.org/download/rdiff-backup/rdiff-backup-1.2.8.tar.gz"
   sha256 "0d91a85b40949116fa8aaf15da165c34a2d15449b3cbe01c8026391310ac95db"
 
+  bottle do
+    cellar :any
+    sha256 "7589f9ae980251f1db709ff5cccddac7de1b6554834665adb3646809da429ee5" => :yosemite
+    sha256 "51755eff43315161e3a734de4bcdd41853eb90c836160e3f0a5be9191d1a0bf0" => :mavericks
+    sha256 "64297003e3f84894ee4d63f1f536c3777dcd9906042541eebead0cb52b902ba8" => :mountain_lion
+  end
+
   devel do
     url "http://savannah.nongnu.org/download/rdiff-backup/rdiff-backup-1.3.3.tar.gz"
     sha256 "ee030ce638df0eb1047cf72578e0de15d9a3ee9ab24da2dc0023e2978be30c06"
