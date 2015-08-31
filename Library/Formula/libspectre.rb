c7132ec942cb140e45fbb33b674693d0e894ffeb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-31 14:39:44 +0100

    libspectre: add 0.2.7 bottle.

--- Library/Formula/libspectre.rb
+++ Library/Formula/libspectre.rb
@@ -4,6 +4,13 @@ class Libspectre < Formula
   url "http://libspectre.freedesktop.org/releases/libspectre-0.2.7.tar.gz"
   sha256 "e81b822a106beed14cf0fec70f1b890c690c2ffa150fa2eee41dc26518a6c3ec"
 
+  bottle do
+    cellar :any
+    sha256 "7f81fd034a403e3460b589e0d1b3ee49f45baca81fc287b3f3abb7517a56cc5f" => :yosemite
+    sha256 "d5978d2bd2eeae441bdd64d6d96066c85ffd0a7006a955c0e84646e75d9b1767" => :mavericks
+    sha256 "32ad21d70cf7d08dbc3fd6158002f2c102a684c8d61993962db3a94d0bfa8394" => :mountain_lion
+  end
+
   depends_on "ghostscript"
 
   def install
