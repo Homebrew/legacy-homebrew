111beee88bd255dd0bfb4a83fb116e17dfc960bf
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-01 07:08:47 +0100

    file-roller: add 3.16.3 bottle.

--- Library/Formula/file-roller.rb
+++ Library/Formula/file-roller.rb
@@ -4,6 +4,12 @@ class FileRoller < Formula
   url "https://download.gnome.org/sources/file-roller/3.16/file-roller-3.16.3.tar.xz"
   sha256 "2b3a1111caba26e67b96559a3118a700dbfb6a4c6ad7ebd3e509df227995411c"
 
+  bottle do
+    sha256 "28b7c7a17489bf7bf443d921778c97dca8607be10a48bc17a7515c24109a12d4" => :yosemite
+    sha256 "e4d0be17adc6a550d1abbe5c934cd35edbd46624c731c284ee5357145ae5ad33" => :mavericks
+    sha256 "717007431c4b510af5dc653b0141f84c6f01e11e4cb16daf370e1d7e44af5c08" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "itstool" => :build
