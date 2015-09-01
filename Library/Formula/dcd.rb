3b3fa9f69c2cec5eeeb83da2d9760194bd5830c5
cspencer51 <cspencer@economicmodeling.com>
2015-09-01 15:47:10 -0700

    dcd 0.7.0
    Closes #43490.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/dcd.rb
+++ Library/Formula/dcd.rb
@@ -2,21 +2,14 @@ class Dcd < Formula
   desc "Auto-complete program for the D programming language"
   homepage "https://github.com/Hackerpilot/DCD"
   url "https://github.com/Hackerpilot/DCD.git",
-      :tag => "v0.6.0",
-      :revision => "633b1667ef223e6eda7bcfd2d2d746f59036571f"
+      :tag => "v0.7.0",
+      :revision => "5310b346304e060c7633521fe3fd5afc2a16de88"
   head "https://github.com/Hackerpilot/dcd.git", :shallow => false
 
   bottle do
     sha256 "b2bfea971d1cce8f221a524d3ddd10f5b2e6775acfe29754d765785d875a6bb6" => :yosemite
     sha256 "f159386dfbfb7d225010f3fa24e9b0dc911a718bd81a059be243edb6af4f880c" => :mavericks
     sha256 "84a24c63e3b05ce812ffb7453f1fd5edfb47dfe053bd6c02401f264dd9febc60" => :mountain_lion
-  end
-
-  devel do
-    url "https://github.com/Hackerpilot/DCD.git",
-        :tag => "v0.7.0-rc1",
-        :revision => "70c78a2a109cd2cc83534f43350d24c879c41eaa"
-    version "0.7.0-rc1"
   end
 
   depends_on "dmd" => :build
