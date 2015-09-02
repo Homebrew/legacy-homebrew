e7c28cf9d166603f22af6ad735fe834166ce939a
Alex Dunn <dunn.alex@gmail.com>
2015-09-01 22:30:45 -0700

    fio 2.2.9

--- Library/Formula/fio.rb
+++ Library/Formula/fio.rb
@@ -1,8 +1,8 @@
 class Fio < Formula
   desc "I/O benchmark and stress test"
   homepage "http://freecode.com/projects/fio"
-  url "http://brick.kernel.dk/snaps/fio-2.2.7.tar.bz2"
-  sha256 "6e3cd5bda0747e61cbfd42c7b3cfe044ea8981d8d3a486f0a2f2fafdb954296f"
+  url "http://brick.kernel.dk/snaps/fio-2.2.9.tar.bz2"
+  sha256 "c881d9cf15500bd4436970d0467c356c5c021417131587931ba78845e965bf56"
   head "git://git.kernel.dk/fio.git"
 
   bottle do
