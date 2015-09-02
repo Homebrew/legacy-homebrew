95764dba76fbce1413ef37e8a5ecf16bf7db5132
Alex Dunn <dunn.alex@gmail.com>
2015-09-01 21:40:31 -0700

    fb-client 1.5.0
    Closes #43498.

--- Library/Formula/fb-client.rb
+++ Library/Formula/fb-client.rb
@@ -1,8 +1,10 @@
 class FbClient < Formula
   desc "Shell-script client for http://paste.xinu.at"
   homepage "https://paste.xinu.at"
-  url "https://paste.xinu.at/data/client/fb-1.4.3.tar.gz"
-  sha256 "4466bc6192f525bb9aaee053d7e7637a97da7e751168dda6ecd7debe74f93986"
+  url "https://paste.xinu.at/data/client/fb-1.5.0.tar.gz"
+  sha256 "205514e7ae6d2ce687c05a5f581248d0f06c29c4e8e004f768ba0b54a39ed2f3"
+
+  head "https://git.server-speed.net/users/flo/fb", :using => :git
 
   bottle do
     cellar :any
@@ -10,8 +12,6 @@ class FbClient < Formula
     sha256 "e6dd96a901a2fe55a560c5ca5dba219d25a6447f63d655714f9b4638821c288e" => :mavericks
     sha256 "b4bca38efef773ea10969185df3a4db669dd699df1f14883d8d847a6261003f5" => :mountain_lion
   end
-
-  head "https://git.server-speed.net/users/flo/fb", :using => :git
 
   conflicts_with "findbugs",
     :because => "findbugs and fb-client both install a `fb` binary"
