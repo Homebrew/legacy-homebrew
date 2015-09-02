81978ba055d55d627ed1b780146ef31caecde168
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-02 01:54:44 +0100

    physfs: add 2.0.3 bottle.

--- Library/Formula/physfs.rb
+++ Library/Formula/physfs.rb
@@ -3,6 +3,14 @@ class Physfs < Formula
   homepage "https://icculus.org/physfs/"
   url "https://icculus.org/physfs/downloads/physfs-2.0.3.tar.bz2"
   sha256 "ca862097c0fb451f2cacd286194d071289342c107b6fe69079c079883ff66b69"
+
+  bottle do
+    cellar :any
+    sha256 "72cf56b0e25ad508b8852b025886ed7a11a25afd3784e799dbee15cdeb53e222" => :yosemite
+    sha256 "69f7775baf000521234dce1d4ebd51c2466bf798dcd91656b37d54964afafd06" => :mavericks
+    sha256 "a9639a67469ae197cbed16faec7381417e5c6d662c5568ec721ef4bc194e4467" => :mountain_lion
+  end
+
   head "https://hg.icculus.org/icculus/physfs/", :using => :hg
 
   depends_on "cmake" => :build
