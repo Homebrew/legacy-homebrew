c69ffcdd580c5800099fb6337830c3b67b72fc99
Rouven Weiler <weiler@manzana.local>
2015-09-02 17:59:18 +0200

    Add formula for puddletag.

---
+++ Library/Formula/puddletag.rb
@@ -0,0 +1,53 @@
+class Puddletag < Formula
+  desc "Powerful, simple, audio tag editor."
+  homepage "http://puddletag.sf.net"
+  url "https://github.com/keithgg/puddletag/archive/v1.0.5.tar.gz"
+  version "1.0.5"
+  sha256 "f94ebcc4ed31389574c187197b99256bec1f96e1e7d4dd61730e88f79deeaba2"
+
+  depends_on "pyqt" 
+
+  resource "pyparsing" do
+    url "https://pypi.python.org/packages/source/p/pyparsing/pyparsing-1.5.7.tar.gz"
+    sha256 "646e14f90b3689b005c19ac9b6b390c9a39bf976481849993e277d7380e6e79f"
+  end
+
+  resource "mutagen" do
+    url "https://bitbucket.org/lazka/mutagen/downloads/mutagen-1.21.tar.gz"
+    sha256 "4dd30af3a291c0a152838f2bbf1d592bf6ede752b11a159cbf84e75815bcc2b5"
+  end
+
+  resource "configobj" do
+    url "https://pypi.python.org/packages/source/c/configobj/configobj-5.0.5.tar.gz"
+    sha256 "766eff273f2cbb007a3ea8aa69429ee9b1553aa96fe282c6ace3769b9ac47b08"
+  end
+
+  def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    %w[pyparsing mutagen configobj].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    system "cp", "-r", buildpath/"source/", buildpath 
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test puddletag`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "puddletag"
+  end
+end
