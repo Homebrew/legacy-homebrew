c6b3704ba31f6470ff80cb19ec58fd87a75cbb99
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-03 13:02:42 +0100

    iso-codes: update 3.61 bottle.

--- Library/Formula/iso-codes.rb
+++ Library/Formula/iso-codes.rb
@@ -8,9 +8,9 @@ class IsoCodes < Formula
 
   bottle do
     cellar :any
-    sha256 "0f9b751320df718306bab937dea5b3289eccdf7f166ddd73a7d1e05810e0cbd5" => :yosemite
-    sha256 "83ecd6c02b005a87ceb02bc780a942e8d6309ee487e998b4598e21d25ed65bc8" => :mavericks
-    sha256 "e528769ebea62fa694eb6a7ab1761becf6e21010bb646e71bac0dabd8123e7fd" => :mountain_lion
+    sha256 "de96d8f33f363621e11c881f8f013a68bffbd58e0d0e9371eaa8f6f00c5b6c5f" => :yosemite
+    sha256 "d5a5beccd811923c51c655d9f3e9f3b1279340e4f67da87b8d0f334222e236a9" => :mavericks
+    sha256 "f0a590da2b2f1e86683b17ebc5cd419fe06110b90160adbcce3a551e0136a26e" => :mountain_lion
   end
 
   depends_on "gettext" => :build
