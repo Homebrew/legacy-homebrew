f067bf1e0b8274315003a8cea6fc5660365c229f
Alex Dunn <dunn.alex@gmail.com>
2015-09-04 13:22:46 -0700

    duck: note how to find stable version, fix test
    Closes #43589.

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -1,6 +1,7 @@
 class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
+  # check the changelog for the latest stable version: https://cyberduck.io/changelog/
   url "https://dist.duck.sh/duck-src-4.7.2.18007.tar.gz"
   sha256 "2b30071ef58aed42805724f318b1e3f013749cc534e54265ee305dfc2ec86d69"
   head "https://svn.cyberduck.io/trunk/"
@@ -24,8 +25,7 @@ class Duck < Formula
   end
 
   test do
-    filename = (testpath/"test")
-    system "#{bin}/duck", "--download", stable.url, filename
-    filename.verify_checksum stable.checksum
+    system "#{bin}/duck", "--download", Formula["when"].stable.url, testpath/"test"
+    (testpath/"test").verify_checksum Formula["when"].stable.checksum
   end
 end
