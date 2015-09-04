7edb73b108b0762b17f6886b7b04925ebaebe6a8
Alex Dunn <dunn.alex@gmail.com>
2015-09-04 14:43:31 -0700

    fleetctl 0.11.5
    Closes #43590.

--- Library/Formula/fleetctl.rb
+++ Library/Formula/fleetctl.rb
@@ -1,8 +1,8 @@
 class Fleetctl < Formula
   desc "Distributed init system"
   homepage "https://github.com/coreos/fleet"
-  url "https://github.com/coreos/fleet/archive/v0.11.4.tar.gz"
-  sha256 "2b386c6eec23741095df7b87db01ef9263c3423921b6804218ff4eca8c82f159"
+  url "https://github.com/coreos/fleet/archive/v0.11.5.tar.gz"
+  sha256 "a6a785099df71645b5fe8755a36baa6c11138749bc02ae4990fd3f52663c0394"
   head "https://github.com/coreos/fleet.git"
 
   bottle do
