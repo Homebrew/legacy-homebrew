b1e20be27593d651d14acc2f18e1ed0538cb107e
Alex Dunn <dunn.alex@gmail.com>
2015-09-05 15:11:45 -0700

    fq 0.9.2, HEAD
    Closes #43620.

--- Library/Formula/fq.rb
+++ Library/Formula/fq.rb
@@ -1,8 +1,9 @@
 class Fq < Formula
-  desc "A brokered message queue written in C"
+  desc "Brokered message queue optimized for performance"
   homepage "https://github.com/circonus-labs/fq"
-  url "https://github.com/circonus-labs/fq/archive/v0.9.1.tar.gz"
-  sha256 "9fd8d51211a5ebe776c0e4e61a9d4475ba39e4597fe5cd45ea9a1043da3cf4ef"
+  url "https://github.com/circonus-labs/fq/archive/v0.9.2.tar.gz"
+  sha256 "146fc72ad2e31f6c6b6badbf1a412e6b88cb7ef5a0850475efae32e0c561c091"
+  head "https://github.com/circonus-labs/fq.git"
 
   bottle do
     sha256 "908fdba172e7d559382baeab3d36a5a9792042a3051dc539489f7c922a82fc1a" => :yosemite
@@ -22,7 +23,7 @@ class Fq < Formula
   end
 
   test do
-    pid = fork { exec sbin/"fqd", "-D" }
+    pid = fork { exec sbin/"fqd", "-D", "-c", testpath/"test.sqlite" }
     sleep 1
     begin
       assert_match /Circonus Fq Operational Dashboard/, shell_output("curl 127.0.0.1:8765")
