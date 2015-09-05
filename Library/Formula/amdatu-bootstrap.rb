feb92f2de19cca6fcf5c1b5e2a744681de713abb
Paul Bakker <paul.bakker@luminis.eu>
2015-09-05 20:12:20 +0200

    Renamed formula to amdatu-bootstrap

--- Library/Formula/amdatu-bootstrap.rb
+++ Library/Formula/amdatu-bootstrap.rb
@@ -1,11 +1,11 @@
-class Bootstrap < Formula
+class AmdatuBootstrap < Formula
   desc "Bootstrapping OSGi development"
   homepage "http://amdatu.org/bootstrap/intro.html"
   url "https://bitbucket.org/amdatu/amdatu-bootstrap/downloads/bootstrap-r8-bin-brew.zip"
   sha256 "386c9f69a8bc1e12908d0a9b41c40c126c0f95627bc81d3d15bd868649798f91"
 
   def install
-    libexec.install %w[bootstrap bootstrap.jar conf]
-    bin.install_symlink "#{libexec}/bootstrap" => "bootstrap"
+    libexec.install %w[amdatu-bootstrap bootstrap.jar conf]
+    bin.install_symlink "#{libexec}/amdatu-bootstrap" => "amdatu-bootstrap"
   end
 end
