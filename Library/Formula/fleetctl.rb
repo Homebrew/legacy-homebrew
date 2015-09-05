409dbeefddc797edeee143d7f8931bef1742f6f8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-05 04:00:37 +0100

    fleetctl: update 0.11.5 bottle.

--- Library/Formula/fleetctl.rb
+++ Library/Formula/fleetctl.rb
@@ -7,9 +7,9 @@ class Fleetctl < Formula
 
   bottle do
     cellar :any
-    sha256 "4bbaed28515a733e185f458957255c5d204809f9e3d8d7311c135b27f396994a" => :yosemite
-    sha256 "777e5941b1887ea48526a529f540604eec39bf29512de85af20ae6594548a4be" => :mavericks
-    sha256 "fd0cea420fbb86293b012c3c58ba391894f61444eb45d40eef968347d05bd0e7" => :mountain_lion
+    sha256 "66d1adc5c8b70ea5e28b0f7bd7a73f0baeb557198e90cc3a0bb87733524d5350" => :yosemite
+    sha256 "b444e18c6ad84389b3c7056ed190b22cf3427d77bc77323e30615d5794649e6e" => :mavericks
+    sha256 "0c48461c277d601a0abff0ff43110618316abd5bcbd498538a791ad91b5ce71d" => :mountain_lion
   end
 
   depends_on "go" => :build
