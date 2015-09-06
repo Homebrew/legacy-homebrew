56a19d55db67d265467755a9203cf03026e29df8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-06 05:03:51 +0100

    fuzzy-find: add 0.6.0 bottle.

--- Library/Formula/fuzzy-find.rb
+++ Library/Formula/fuzzy-find.rb
@@ -7,6 +7,13 @@ class FuzzyFind < Formula
 
   head "https://github.com/silentbicycle/ff.git"
 
+  bottle do
+    cellar :any
+    sha256 "1b447f73c929866935af122bec4c15390f0001f049a0c737880fcfd4d7bafdb2" => :yosemite
+    sha256 "75aa5bedb0bd3e2869f24f1363b02ddf88f7612549c97b8f93f5d7aae0e42e63" => :mavericks
+    sha256 "8262f63ca68495d17b24aa74e3bad1b1f1b4c538375cc05e02639685f3942f51" => :mountain_lion
+  end
+
   def install
     system "make"
     bin.install "ff"
