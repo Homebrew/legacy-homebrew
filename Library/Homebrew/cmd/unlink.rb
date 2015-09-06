3946da8c6b3716e217b9cd4462465086970e51b4
Ethan Piekarski <ethan.piekarski@genworth.com>
2015-09-06 15:25:36 +0800

    Added --dry-run to unlink
    Closes #43561.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Homebrew/cmd/unlink.rb
+++ Library/Homebrew/cmd/unlink.rb
@@ -1,12 +1,23 @@
+require "ostruct"
+
 module Homebrew
   def unlink
     raise KegUnspecifiedError if ARGV.named.empty?
 
+    mode = OpenStruct.new
+    mode.dry_run = true if ARGV.dry_run?
+
     ARGV.kegs.each do |keg|
+      if mode.dry_run
+        puts "Would remove:"
+        keg.unlink(mode)
+        next
+      end
+
       keg.lock do
         print "Unlinking #{keg}... "
         puts if ARGV.verbose?
-        puts "#{keg.unlink} symlinks removed"
+        puts "#{keg.unlink(mode)} symlinks removed"
       end
     end
   end
