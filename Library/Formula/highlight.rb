c28f85df81392f0b0b7c55e11c590653b11e27b7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-06 17:10:30 +0100

    highlight: update 3.23 bottle.

--- Library/Formula/highlight.rb
+++ Library/Formula/highlight.rb
@@ -7,9 +7,9 @@ class Highlight < Formula
   head "svn://svn.code.sf.net/p/syntaxhighlight/code/highlight/"
 
   bottle do
-    sha256 "f530e34bbd1769fc6f434e65cbb7b81eef505d1508bfe24ca74277685407b6a3" => :yosemite
-    sha256 "8a9d9ecea3d0045dd4736859535cda66cb47cb1fc222b6244458350be62c2d39" => :mavericks
-    sha256 "81d3718fd0a1258a75d7ebce4ca36adb4ff0577e9c25dae5beae981dbdf61ade" => :mountain_lion
+    sha256 "a00e8653281375665d4a39ed9e4e1fdc30fb1d64f3d7f09992fb258e0de2859f" => :yosemite
+    sha256 "7d5297d4b5978e9055ee552d73561d2441456c1a7b263107504e48d77150ecf4" => :mavericks
+    sha256 "855e131bf67b5f67be9907cde35a69e40d16a8fa6b714a0e36e8da77475ef296" => :mountain_lion
   end
 
   depends_on "pkg-config" => :build
