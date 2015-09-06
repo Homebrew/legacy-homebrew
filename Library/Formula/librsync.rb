badbd9273963f00b8deaab2470b6ae4c63911354
Tomasz Pajor <tomek@polishgeeks.com>
2015-09-06 12:08:59 +0200

    librsync 2.0.0

--- Library/Formula/librsync.rb
+++ Library/Formula/librsync.rb
@@ -1,8 +1,8 @@
 class Librsync < Formula
   desc "Library that implements the rsync remote-delta algorithm"
   homepage "http://librsync.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/librsync/librsync/0.9.7/librsync-0.9.7.tar.gz"
-  sha256 "6633e4605662763a03bb6388529cbdfd3b11a9ec55b8845351c1bd9a92bc41d6"
+  url "https://github.com/librsync/librsync/archive/v2.0.0.tar.gz"
+  sha256 "b5c4dd114289832039397789e42d4ff0d1108ada89ce74f1999398593fae2169"
 
   bottle do
     cellar :any
@@ -15,24 +15,13 @@ class Librsync < Formula
 
   option :universal
 
+  depends_on "cmake" => :build
   depends_on "popt"
 
   def install
     ENV.universal_binary if build.universal?
 
-    ENV.append "CFLAGS", "-std=gnu89"
-
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--enable-shared"
-
-    inreplace "libtool" do |s|
-      s.gsub! /compiler_flags=$/, "compiler_flags=' #{ENV.cflags}'"
-      s.gsub! /linker_flags=$/, "linker_flags=' #{ENV.ldflags}'"
-    end
-
+    system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 end
