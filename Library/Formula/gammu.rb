fdc943ee77470f24f244a705c636f5ba75d4a8d3
Alex Dunn <dunn.alex@gmail.com>
2015-09-06 15:56:19 -0700

    gammu 1.36.5
    Closes #43672.

--- Library/Formula/gammu.rb
+++ Library/Formula/gammu.rb
@@ -1,11 +1,19 @@
 class Gammu < Formula
   desc "Command-line utility to control a phone"
   homepage "http://wammu.eu/gammu/"
-  url "https://dl.cihar.com/gammu/releases/gammu-1.36.0.tar.xz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/g/gammu/gammu_1.36.0.orig.tar.xz"
-  sha256 "9c89fd204e190db5b301d28b793e8d0f2b05069a5b2b91fde451a6dae7f7d633"
-
   head "https://github.com/gammu/gammu.git"
+
+  stable do
+    url "http://dl.cihar.com/gammu/releases/gammu-1.36.5.tar.xz"
+    mirror "https://mirrors.kernel.org/debian/pool/main/g/gammu/gammu_1.36.5.orig.tar.xz"
+    sha256 "42e626f8f8b97c7a0afc5547a6056aa061725e8e3ace91dbcf9e83816a70ab86"
+
+    # To customize bash completion location; remove in next stable release
+    patch do
+      url "https://github.com/gammu/gammu/commit/470c53aa640f5c53671ae22e06a7159f39774042.diff"
+      sha256 "ed3a402f36a8314a1a3df187827988ed8f5c9a5638397fe684ed4f0ed5ffbf44"
+    end
+  end
 
   bottle do
     sha256 "c5e3744176b3609902070f07ced499d02570a8dc0d5aa72e02383b8e9bcce5a4" => :yosemite
@@ -19,12 +27,8 @@ class Gammu < Formula
   depends_on "openssl"
 
   def install
-    args = std_cmake_args
-    args << "-DINSTALL_BASH_COMPLETION=OFF"
-    args << "-DWITH_PYTHON=OFF"
-
     mkdir "build" do
-      system "cmake", "..", *args
+      system "cmake", "..", "-DBASH_COMPLETION_COMPLETIONSDIR:PATH=#{bash_completion}", *std_cmake_args
       system "make", "install"
     end
   end
