5986329832ecfa0aa4cf6437442bf42d7c5a4245
Paul Bakker <paul.bakker@luminis.eu>
2015-09-06 22:36:09 +0200

    Fixed sha for new zip file

--- Library/Formula/amdatu-bootstrap.rb
+++ Library/Formula/amdatu-bootstrap.rb
@@ -2,7 +2,7 @@ class AmdatuBootstrap < Formula
   desc "Bootstrapping OSGi development"
   homepage "http://amdatu.org/bootstrap/intro.html"
   url "https://bitbucket.org/amdatu/amdatu-bootstrap/downloads/bootstrap-r8-bin-brew.zip"
-  sha256 "386c9f69a8bc1e12908d0a9b41c40c126c0f95627bc81d3d15bd868649798f91"
+  sha256 "f2f66af5a6b9725e260509c0a0e9996c1e3b7f696a96309b0a2a399975a9a732"
 
   def install
     libexec.install %w[amdatu-bootstrap bootstrap.jar conf]
