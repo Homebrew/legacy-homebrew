4eb8c12f7401594e3a3063516cf7d1f1969f9470
Alex Dunn <dunn.alex@gmail.com>
2015-09-05 18:00:40 -0700

    funcoeszz 15.5
    Adjusted installation based on upstream instructions: http://funcoeszz.net/download/
    
    Closes #43626.

--- Library/Formula/funcoeszz.rb
+++ Library/Formula/funcoeszz.rb
@@ -1,21 +1,25 @@
 class Funcoeszz < Formula
   desc "Dozens of command-line mini-applications (Portuguese)"
   homepage "http://funcoeszz.net/"
-  url "http://funcoeszz.net/download/funcoeszz-13.2.sh"
-  sha256 "c790bafb8ba8bafa78e48179e46e670ba5cad0d13e8a631b84f7e3fe21b8d86d"
+  url "http://funcoeszz.net/download/funcoeszz-15.5.sh"
+  sha256 "13e6347018b43c54f5032d4700d45255f7e26e1e8f1eacb25e324d4fbe07e15d"
 
   depends_on "bash"
 
   def install
-    prefix.install "funcoeszz-#{version}.sh" => "funcoeszz.sh"
+    bin.install "funcoeszz-#{version}.sh" => "funcoeszz"
   end
 
   def caveats; <<-EOS.undent
     To use this software add to your profile:
-      export ZZPATH="#{opt_prefix}/funcoeszz.sh"
+      export ZZPATH="#{opt_bin}/funcoeszz"
       source "$ZZPATH"
 
     Usage of a newer Bash than the OS X default is required.
     EOS
+  end
+
+  test do
+    assert_equal "15", shell_output("#{bin}/funcoeszz zzcalcula 10+5").chomp
   end
 end
