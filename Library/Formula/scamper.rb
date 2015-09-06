fdb22d1225402d073ff81233c1972b0bfe63fbf7
Tomasz Pajor <tomek@polishgeeks.com>
2015-09-06 14:18:09 +0200

    scamper 20141211a
    Closes #43642.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/scamper.rb
+++ Library/Formula/scamper.rb
@@ -1,8 +1,8 @@
 class Scamper < Formula
   desc "Advanced traceroute and network measurement utility"
   homepage "http://www.caida.org/tools/measurement/scamper/"
-  url "http://www.caida.org/tools/measurement/scamper/code/scamper-cvs-20141101.tar.gz"
-  sha256 "aae527dbbc827b735196e0bee47518daad4583cd1feda367442ad43252fd7278"
+  url "http://www.caida.org/tools/measurement/scamper/code/scamper-cvs-20141211a.tar.gz"
+  sha256 "1073d4889abd9d4ec0d8f988921b6ad4f3535030cf007abb99e67f9e6b2052b8"
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
