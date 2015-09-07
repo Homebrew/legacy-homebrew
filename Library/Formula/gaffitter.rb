62f83444ea7e71b8a268f18c26c68c10fe424428
Alex Dunn <dunn.alex@gmail.com>
2015-09-06 17:47:35 -0700

    gaffitter 1.0.0
    Closes #43657.

--- Library/Formula/gaffitter.rb
+++ Library/Formula/gaffitter.rb
@@ -1,11 +1,19 @@
 class Gaffitter < Formula
   desc "Efficiently fit files/folders to fixed size volumes (like DVDs)"
   homepage "http://gaffitter.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/gaffitter/gaffitter/0.6.0/gaffitter-0.6.0.tar.bz2"
-  sha256 "61236240942d0319fb1c0a7e3da8424ddad7fe5abc82c2e1e3f2a89ccc9fe275"
+  url "https://downloads.sourceforge.net/project/gaffitter/gaffitter/1.0.0/gaffitter-1.0.0.tar.gz"
+  sha256 "c85d33bdc6c0875a7144b540a7cce3e78e7c23d2ead0489327625549c3ab23ee"
+
+  depends_on "cmake" => :build
 
   def install
-    system "make"
-    bin.install "src/gaffitter"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system bin/"fit", "-t", "10m", "--show-size", testpath
   end
 end
