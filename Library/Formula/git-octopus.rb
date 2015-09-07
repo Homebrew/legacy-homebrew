d497c64efc049c51f3afa0b559a153c0b8ff1c27
Mathieu Bolla <mathieu.bolla@gmail.com>
2015-09-07 17:25:47 +0200

    git-octopus 1.1 (updated formula)
    Amended following pull-request comments:
    - Leave bottles in place
    - Use one-line installs

--- Library/Formula/git-octopus.rb
+++ Library/Formula/git-octopus.rb
@@ -4,13 +4,17 @@ class GitOctopus < Formula
   url "https://github.com/lesfurets/git-octopus/archive/v1.1.tar.gz"
   sha256 "cc0f64be23a385a18e8cd7e913a54e582f9c8c07460b2c50b0df46b83fc3769d"
 
+  bottle do
+    cellar :any
+    sha256 "be33fe6c653675fb777b830ba7b5ba5d41986e87bcd2d79d5887240862c287a9" => :yosemite
+    sha256 "8fa36a0ea95851074f36b0375f0138293f7c3aa0fc6f819a14987e4c1dc520ee" => :mavericks
+    sha256 "7c953c0d60e50045229f63d9efecb9a9b25791f336efde09b91026067102682e" => :mountain_lion
+  end
+
   def install
     system "make", "build"
-    bin.install "bin/git-octopus"
-    bin.install "bin/git-conflict"
-    bin.install "bin/git-apply-conflict-resolution"
-    man1.install "doc/git-octopus.1"
-    man1.install "doc/git-conflict.1"
+    bin.install "bin/git-octopus", "bin/git-conflict", "bin/git-apply-conflict-resolution"
+    man1.install "doc/git-octopus.1", "doc/git-conflict.1"
   end
 
   test do
