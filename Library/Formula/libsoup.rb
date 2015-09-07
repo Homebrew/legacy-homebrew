52f9f62050df16e56853e0c1f3ab8a684f053911
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-07 13:38:10 +0200

    libsoup 2.50.0
    version bump
    test added

--- Library/Formula/libsoup.rb
+++ Library/Formula/libsoup.rb
@@ -1,8 +1,8 @@
 class Libsoup < Formula
   desc "HTTP client/server library for GNOME"
   homepage "https://live.gnome.org/LibSoup"
-  url "https://download.gnome.org/sources/libsoup/2.48/libsoup-2.48.1.tar.xz"
-  sha256 "9b0d14b36e36a3131a06c6e3aa7245716e6904e3e636df81c0b6c8bd3f646f9a"
+  url "https://download.gnome.org/sources/libsoup/2.50/libsoup-2.50.0.tar.xz"
+  sha256 "1e01365ac4af3817187ea847f9d3588c27eee01fc519a5a7cb212bb78b0f667b"
 
   bottle do
     sha1 "58bfb3f803cdb164301d33754d80cf0b12b44f4c" => :yosemite
@@ -15,7 +15,7 @@ class Libsoup < Formula
   depends_on "glib-networking"
   depends_on "gnutls"
   depends_on "sqlite"
-  depends_on "gobject-introspection" => :optional
+  depends_on "gobject-introspection" => :recommended
 
   def install
     args = [
@@ -35,5 +35,37 @@ class Libsoup < Formula
 
     system "./configure", *args
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <libsoup/soup.h>
+
+      int main(int argc, char *argv[]) {
+        guint version = soup_get_major_version();
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/libsoup-2.4
+      -D_REENTRANT
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lintl
+      -lsoup-2.4
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
   end
 end
