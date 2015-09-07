f6b23e21ee33dd90fd537869fe292ebfe92f171f
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-07 15:36:36 +0200

    ghex 3.10.1 (new formula)
    Closes #43666.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/ghex.rb
@@ -0,0 +1,42 @@
+class Ghex < Formula
+  desc "GNOME hex editor"
+  homepage "https://wiki.gnome.org/Apps/Ghex"
+  url "https://download.gnome.org/sources/ghex/3.10/ghex-3.10.1.tar.xz"
+  sha256 "34b66cb5c84410c420df72f229d25aee5979e58048a246ed719b046f0c241132"
+
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "itstool" => :build
+  depends_on "libxml2" => [:build, "with-python"]
+  depends_on :python => :build if MacOS.version <= :snow_leopard
+  depends_on "gtk+3"
+  depends_on "hicolor-icon-theme"
+  depends_on "gnome-themes-standard" => :optional
+
+  def install
+    # forces use of gtk3-update-icon-cache instead of gtk-update-icon-cache. No bugreport should
+    # be filed for this since it only occurs because Homebrew renames gtk+3's gtk-update-icon-cache
+    # to gtk3-update-icon-cache in order to avoid a collision between gtk+ and gtk+3.
+    inreplace "icons/Makefile.in", "gtk-update-icon-cache", "gtk3-update-icon-cache"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--disable-schemas-compile",
+                          "--prefix=#{prefix}"
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
+    system "make", "install"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    # HighContrast is provided by gnome-themes-standard
+    if File.file?("#{HOMEBREW_PREFIX}/share/icons/HighContrast/.icon-theme.cache")
+      system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/HighContrast"
+    end
+  end
+
+  test do
+    system "#{bin}/ghex", "--help"
+  end
+end
