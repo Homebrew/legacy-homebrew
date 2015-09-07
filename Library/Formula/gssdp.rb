340f46e589f6b2602d346b3fb7084b2714f2aa32
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-07 18:36:35 +0200

    gssdp 0.14.11
    version bump
    test added

--- Library/Formula/gssdp.rb
+++ Library/Formula/gssdp.rb
@@ -1,8 +1,8 @@
 class Gssdp < Formula
   desc "GUPnP library for resource discovery and announcement over SSDP"
   homepage "https://wiki.gnome.org/GUPnP/"
-  url "https://download.gnome.org/sources/gssdp/0.14/gssdp-0.14.8.tar.xz"
-  sha256 "4c3ffa01435e84dc31c954e669e1ca0749b962f76a333e74f5c2cb0de5803a13"
+  url "https://download.gnome.org/sources/gssdp/0.14/gssdp-0.14.11.tar.xz"
+  sha256 "7bf5aeaf2119fe0bec5f3632ecf39dae15bc85276c72c2ad8dd4b0e345c6535a"
 
   bottle do
     cellar :any
@@ -21,5 +21,30 @@ class Gssdp < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <libgssdp/gssdp.h>
+
+      int main(int argc, char *argv[]) {
+        GType type = gssdp_client_get_type();
+        return 0;
+      }
+    EOS
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/gssdp-1.0
+      -D_REENTRANT
+      -L#{lib}
+      -lgssdp-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
   end
 end
