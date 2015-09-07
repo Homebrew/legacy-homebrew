2ff609d2140501c193ed5fdd0f102bff9c443cc5
Nyx0uf <Nyx0uf@users.noreply.github.com>
2015-09-07 10:55:36 +0200

    ffmpeg: add libbs2b option.
    Closes #43670.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ffmpeg.rb
+++ Library/Formula/ffmpeg.rb
@@ -71,6 +71,7 @@ class Ffmpeg < Formula
   depends_on "libssh" => :optional
   depends_on "webp" => :optional
   depends_on "zeromq" => :optional
+  depends_on "libbs2b" => :optional
 
   def install
     args = ["--prefix=#{prefix}",
@@ -116,6 +117,7 @@ class Ffmpeg < Formula
     args << "--enable-libx265" if build.with? "x265"
     args << "--enable-libwebp" if build.with? "webp"
     args << "--enable-libzmq" if build.with? "zeromq"
+    args << "--enable-libbs2b" if build.with? "libbs2b"
     args << "--disable-indev=qtkit" if build.without? "qtkit"
 
     if build.with? "openjpeg"
