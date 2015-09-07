159f5da7ae48b7ed2784bef1b0369aabb4b25c2f
Mathieu Bolla <mathieu.bolla@gmail.com>
2015-09-07 15:01:35 +0200

    git-octopus 1.1
    Closes #43665.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/git-octopus.rb
+++ Library/Formula/git-octopus.rb
@@ -1,10 +1,8 @@
 class GitOctopus < Formula
   desc "Extends git-merge with branch naming patterns"
   homepage "https://github.com/lesfurets/git-octopus"
-  url "https://github.com/lesfurets/git-octopus/archive/v1.0.tar.gz"
-  sha256 "c83ab4aa3d8ec770f4f05619239a7852c871216a108b4e287c13efb10e3db5db"
-
-  head "https://github.com/lesfurets/git-octopus.git"
+  url "https://github.com/lesfurets/git-octopus/archive/v1.1.tar.gz"
+  sha256 "cc0f64be23a385a18e8cd7e913a54e582f9c8c07460b2c50b0df46b83fc3769d"
 
   bottle do
     cellar :any
@@ -14,8 +12,9 @@ class GitOctopus < Formula
   end
 
   def install
-    bin.install "bin/git-octopus"
-    man1.install "man/man1/git-octopus.1"
+    system "make", "build"
+    bin.install "bin/git-octopus", "bin/git-conflict", "bin/git-apply-conflict-resolution"
+    man1.install "doc/git-octopus.1", "doc/git-conflict.1"
   end
 
   test do
