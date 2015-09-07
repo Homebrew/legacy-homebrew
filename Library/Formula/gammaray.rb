79e8d0968cbf69595c3c93b27fe1c6c41ec4c7ee
Alex Dunn <dunn.alex@gmail.com>
2015-09-06 17:29:14 -0700

    gammaray 2.3.0, HEAD
    Garbage new test because there's no longer anything installed outside
    the app bundle and the bundled executables have no command-line uses.
    
    Closes #43655.

--- Library/Formula/gammaray.rb
+++ Library/Formula/gammaray.rb
@@ -1,8 +1,10 @@
 class Gammaray < Formula
   desc "Examine and manipulate Qt application internals at runtime"
   homepage "https://www.kdab.com/kdab-products/gammaray/"
-  url "https://github.com/KDAB/GammaRay/archive/v2.2.0.tar.gz"
-  sha256 "43a370cf31f799763388d6c01626219352375cffdea74710ccec82cb705e2e1b"
+  url "https://github.com/KDAB/GammaRay/releases/download/v2.3.0/gammaray-2.3.0.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/g/gammaray/gammaray_2.3.0.orig.tar.gz"
+  sha256 "d3da28ff1f7ac5534e518a9d4a7b5d7d22267490d3ab3ae094b0592d33244799"
+  head "https://github.com/KDAB/GammaRay.git"
 
   bottle do
     sha1 "07bfe7c133e5a72e116f07b5cd65e70b6e5ee00b" => :yosemite
@@ -19,10 +21,14 @@ class Gammaray < Formula
   depends_on "qt" if build.with? "qt4"
   depends_on "qt5" if build.without? "qt4"
   depends_on "graphviz" => :recommended
+
   # VTK needs to have Qt support, and it needs to match GammaRay's
   depends_on "homebrew/science/vtk" => [:optional, ((build.with? "qt4") ? "with-qt" : "with-qt5")]
 
   def install
+    # For Mountain Lion
+    ENV.libcxx
+
     args = std_cmake_args
     args << "-DGAMMARAY_ENFORCE_QT4_BUILD=" + ((build.with? "qt4") ? "ON" : "OFF")
     args << "-DCMAKE_DISABLE_FIND_PACKAGE_VTK=" + ((build.without? "vtk") ? "ON" : "OFF")
@@ -35,6 +41,6 @@ class Gammaray < Formula
   end
 
   test do
-    assert_match /^qt/, `#{bin}/gammaray --list-probes`.chomp
+    (prefix/"GammaRay.app/Contents/MacOS/GammaRay").executable?
   end
 end
