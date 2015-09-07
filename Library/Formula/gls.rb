7002f64100e211476836dce4a6771b7562a6bb62
Olivier Poitrey <rs@dailymotion.com>
2015-09-07 03:31:16 -0700

    Fix SHA1

--- Library/Formula/gls.rb
+++ Library/Formula/gls.rb
@@ -2,7 +2,7 @@ class Gls < Formula
   desc "A graphical ls command for iTerm2"
   homepage "https://github.com/rs/gls"
   url "https://github.com/rs/gls/archive/1.0.tar.gz"
-  sha256 "d5cb69827f57fd61d354418b08ff8ebf854e08daaeebb08aaa5a9d3450c06a54"
+  sha256 "2c8fcd7c964b8c9d62dd20cb7c719b736800f23c5aa9d657995f33cedb149f1b"
   depends_on :xcode => :build
 
   def install
