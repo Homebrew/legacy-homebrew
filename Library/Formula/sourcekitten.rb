fb043dde2b1621128bbaf166737085fb6ed70478
JP Simard <jp@jpsim.com>
2015-09-07 17:22:04 -0700

    sourcekitten 0.5.0
    SourceKitten 0.5.0
    
    Hopefully Homebrew supports formulas requiring Xcode 7 Beta 6. Thanks!
    
    Closes #43684.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/sourcekitten.rb
+++ Library/Formula/sourcekitten.rb
@@ -1,7 +1,7 @@
 class Sourcekitten < Formula
   desc "Framework and command-line tool for interacting with SourceKit"
   homepage "https://github.com/jpsim/SourceKitten"
-  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.4.5", :revision => "35508f8c95ecd220719db45b3955d1821f523b7d"
+  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.5.0", :revision => "4106ae36b4fc7a98406ed9bdd1d64074a04a35be"
   head "https://github.com/jpsim/SourceKitten.git"
 
   bottle do
@@ -9,7 +9,7 @@ class Sourcekitten < Formula
     sha256 "4507206b8f5127ba67f0a6a8d4634bf78eec16aa6fe8c807c4c307a89ef668fc" => :yosemite
   end
 
-  depends_on :xcode => ["6.4", :build]
+  depends_on :xcode => ["7.0", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SourceKitten.dst"
