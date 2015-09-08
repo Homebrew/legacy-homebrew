0c6d9dccf8765b7838b0b8b149358ffa25e340ca
Olivier Poitrey <rs@dailymotion.com>
2015-09-07 21:39:08 -0700

    Add HEAD target to gls formula

--- Library/Formula/gls.rb
+++ Library/Formula/gls.rb
@@ -2,6 +2,7 @@ class Gls < Formula
   desc "A graphical ls command for iTerm2"
   homepage "https://github.com/rs/gls"
   url "https://github.com/rs/gls/archive/1.0.tar.gz"
+  head "https://github.com/rs/gls/archive/master.zip"
   sha256 "2c8fcd7c964b8c9d62dd20cb7c719b736800f23c5aa9d657995f33cedb149f1b"
   depends_on :xcode => :build
 
