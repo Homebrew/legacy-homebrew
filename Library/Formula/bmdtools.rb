e9fc1357407029d3454519968a523374d32e918f
Dave Rice <dave@dericed.com>
2015-09-08 12:00:07 -0400

    bmdtools: remove unnecessary options

--- Library/Formula/bmdtools.rb
+++ Library/Formula/bmdtools.rb
@@ -5,9 +5,7 @@ class Bmdtools < Formula
   url 'https://github.com/lu-zero/bmdtools/archive/v0.1.zip'
   sha256 'cc2a2f9c91b019dadea6dff591b07ceb7db05ce8c25ff405f715206ee81c7664'
   head 'https://github.com/lu-zero/bmdtools.git'
- 
-  env :std
- 
+
   depends_on 'pkg-config' => :build
   depends_on 'decklinksdk' => :build
   depends_on 'libav' => :build
