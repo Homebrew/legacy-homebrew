9cc280f2647d407a15fbf042ee7cccf77f163470
Olivier Poitrey <rs@dailymotion.com>
2015-09-07 21:48:26 -0700

    Fix some homebrew complience issues

--- Library/Formula/gls.rb
+++ Library/Formula/gls.rb
@@ -1,13 +1,18 @@
 class Gls < Formula
-  desc "A graphical ls command for iTerm2"
+  desc "Graphical \"ls\" command for iTerm2"
   homepage "https://github.com/rs/gls"
-  url "https://github.com/rs/gls/archive/1.0.tar.gz"
   head "https://github.com/rs/gls/archive/master.zip"
+  url "https://github.com/rs/gls/archive/1.0.tar.gz"
   sha256 "2c8fcd7c964b8c9d62dd20cb7c719b736800f23c5aa9d657995f33cedb149f1b"
+
   depends_on :xcode => :build
 
   def install
     xcodebuild "-project", "gls.xcodeproj", "SYMROOT=build"
     bin.install "build/Release/gls"
+  end
+
+  test do
+    system "#{bin}/gls"
   end
 end
