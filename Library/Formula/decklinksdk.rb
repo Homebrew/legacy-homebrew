d935d34e44f7e77af103e81adf2dfa0f7fd8a6a7
Dave Rice <dave@dericed.com>
2015-09-08 11:35:00 -0400

    decklinksdk: skip installation of proof-of-concept binaries

--- Library/Formula/decklinksdk.rb
+++ Library/Formula/decklinksdk.rb
@@ -6,15 +6,6 @@ class Decklinksdk < Formula
   sha256 '65d4517a454f3809fffea1c2e1ceab11887e1e4530f278078a63cd52bb24ff73'
 
   def install
-      
-    # blackmagic's DeckControl bin names conflict with deckcontrol by BAVC
-    mv "Mac/Samples/bin/DeckControl", "Mac/Samples/bin/DeckControlsdk"
-    
-    bin.install 'Mac/Samples/bin/BypassControl'
-    bin.install 'Mac/Samples/bin/CaptureFromTape'
-    bin.install 'Mac/Samples/bin/DeckControlsdk'
-    bin.install 'Mac/Samples/bin/DeviceList'
-    bin.install 'Mac/Samples/bin/ExportToTape'
     include.install Dir['Mac/include/*']
   end
 end
