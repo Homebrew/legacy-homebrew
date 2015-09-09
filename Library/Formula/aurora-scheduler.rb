adfd1396ae8bd76d9fecc2e1a7bfeef6fc51b4bf
thinker0 <thinker0@gmail.com>
2015-09-09 19:01:58 +0900

    comment remove

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -1,7 +1,3 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class AuroraScheduler < Formula
   desc "Apache Aurora Scheduler Client"
   homepage "http://aurora.apache.org/"
@@ -10,24 +6,10 @@ class AuroraScheduler < Formula
   sha256 "16040866f3a799226452b1541892eb80ed3c61f47c33f1ccb0687fb5cf82767c"
 
   depends_on "python" => :build
-  #depends_on "gradle" => :build
 
   def install
-    # TODO aurora-schduler
-	#system "sed \"s/<=/</g\" buildSrc/build.gradle \> buildSrc/build.gradle"
-    #system "gradle", "wrapper"
-    #system "./gradlew", "installDist"
-
-    # aurora client
     system "./pants", "binary", "src/main/python/apache/aurora/admin:kaurora_admin"
     system "./pants", "binary", "src/main/python/apache/aurora/client/cli:kaurora"
-
-    # TODO thermos
-    #system "./pants", "binary", "src/main/python/apache/aurora/executor/bin:thermos_executor"
-    #system "./pants", "binary", "src/main/python/apache/thermos/bin:thermos_runner"
-    #system "./pants", "binary", "src/main/python/apache/aurora/tools:thermos_observer"
-
-	#prefix.install Dir["dist/install/aurora-scheduler/*"]
     system "mv", "dist/kaurora.pex", "dist/aurora"
     system "mv", "dist/kaurora_admin.pex", "dist/aurora_admin"
     bin.install "dist/aurora"
