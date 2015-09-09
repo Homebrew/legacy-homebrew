e8a0012c4eebf4d92b7f6501ae791712f1a691b0
Olivier Poitrey <rs@dailymotion.com>
2015-09-08 18:46:29 -0700

    Fix a few more issues in gls formula

--- Library/Formula/gls.rb
+++ Library/Formula/gls.rb
@@ -1,14 +1,14 @@
 class Gls < Formula
   desc "Graphical \"ls\" command for iTerm2"
   homepage "https://github.com/rs/gls"
-  head "https://github.com/rs/gls/archive/master.zip"
   url "https://github.com/rs/gls/archive/1.0.tar.gz"
   sha256 "2c8fcd7c964b8c9d62dd20cb7c719b736800f23c5aa9d657995f33cedb149f1b"
+  head "https://github.com/rs/gls/archive/master.tar.gz"
 
   depends_on :xcode => :build
 
   def install
-    xcodebuild "-project", "gls.xcodeproj", "SYMROOT=build"
+    xcodebuild "-project", "gls.xcodeproj", "SDKROOT=", "SYMROOT=build"
     bin.install "build/Release/gls"
   end
 
