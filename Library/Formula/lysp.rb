09148f1cef4c041ba5cb206e4e0e8f900e619d2a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-09 18:51:56 +0100

    lysp: add 1.1 bottle.

--- Library/Formula/lysp.rb
+++ Library/Formula/lysp.rb
@@ -4,6 +4,13 @@ class Lysp < Formula
   url "http://www.piumarta.com/software/lysp/lysp-1.1.tar.gz"
   sha256 "436a8401f8a5cc4f32108838ac89c0d132ec727239d6023b9b67468485509641"
 
+  bottle do
+    cellar :any
+    sha256 "373ab2ceec5a09ff44688f4b7bb74b3c0d3f3ecad879feb9af64e7b228b8a99e" => :yosemite
+    sha256 "4bc277151bf52e860522427d92e621887437748265da7161a823810d27af4453" => :mavericks
+    sha256 "153ead5e48fe4eceda1635f07ae4cd111699662ee845c4d370359e5d128b9b07" => :mountain_lion
+  end
+
   depends_on "bdw-gc"
   depends_on "gcc"
 
