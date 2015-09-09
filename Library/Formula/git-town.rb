16dd01350c2e658f177206c8e71592858595d9ed
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-09 04:01:39 +0100

    git-town: add 0.7.3 bottle.

--- Library/Formula/git-town.rb
+++ Library/Formula/git-town.rb
@@ -4,6 +4,13 @@ class GitTown < Formula
   url "https://github.com/Originate/git-town/archive/v0.7.3.tar.gz"
   sha256 "947fd6dac603dcb02467cc2969ef26db1ccdc72312b6b0d70fb8d11179eb09ba"
 
+  bottle do
+    cellar :any
+    sha256 "330ad68d6262f44e7e8b32176335ce96beb96bd05526e3ca11f7a726d904f62c" => :yosemite
+    sha256 "64c9fdae40b5a88cc42a5b2d9d6b6d8934a8d5da5f6810294c7968c562e69aab" => :mavericks
+    sha256 "09560a9c1c363f097b8a06d2ff86f60bcdbfe592e848fb61cf0a89d1bcd2c727" => :mountain_lion
+  end
+
   depends_on "dialog" => :recommended
 
   def install
