f8b0ff3ef63e60a1da17ec8d8e68d949b1cebc27
Kyungdahm Yun <tomyun@gmail.com>
2015-09-09 13:20:00 -0700

    dmd 2.068.1
    Closes #43756.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/dmd.rb
+++ Library/Formula/dmd.rb
@@ -3,22 +3,22 @@ class Dmd < Formula
   homepage "http://dlang.org"
 
   stable do
-    url "https://github.com/D-Programming-Language/dmd/archive/v2.068.0.tar.gz"
-    sha256 "5a18228dfae4e4fde873da442a0a793b9d18c7d0c406a7b9f38230bba724be26"
+    url "https://github.com/D-Programming-Language/dmd/archive/v2.068.1.tar.gz"
+    sha256 "8f2865d480392aa821b5a028956d92a52400dc73d17b23c12d06d136100324ad"
 
     resource "druntime" do
-      url "https://github.com/D-Programming-Language/druntime/archive/v2.068.0.tar.gz"
-      sha256 "59ff14c4393a1724801e2f783013a951d07d54a145f20da956303789822809ed"
+      url "https://github.com/D-Programming-Language/druntime/archive/v2.068.1.tar.gz"
+      sha256 "3730b763717e3eb08c19c207bc23162b8b9fe7368d70a3bb8498a26e0377eb18"
     end
 
     resource "phobos" do
-      url "https://github.com/D-Programming-Language/phobos/archive/v2.068.0.tar.gz"
-      sha256 "e001bca05f5e25448ea0b80417fed3b6224673ef00b440b01f937f3e0da59035"
+      url "https://github.com/D-Programming-Language/phobos/archive/v2.068.1.tar.gz"
+      sha256 "5326f509e42193ad56cb5fdbde26ec403fd1f1eac559fbc56f957a7f9bd14c8a"
     end
 
     resource "tools" do
-      url "https://github.com/D-Programming-Language/tools/archive/v2.068.0.tar.gz"
-      sha256 "af08eb5a9b38321dd2683b01857cf6567ce7b4025e1a3a363751bf83aee72d55"
+      url "https://github.com/D-Programming-Language/tools/archive/v2.068.1.tar.gz"
+      sha256 "a0b4362291949f1e88e32d89280860c08dfd00f34a235ebe276c8555b9862f4c"
     end
   end
 
