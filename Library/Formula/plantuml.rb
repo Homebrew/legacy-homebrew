269c8b994eebea27034a23d0890a56e4dbf81c3c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-09 20:22:54 +0100

    plantuml: update 8029 bottle.

--- Library/Formula/plantuml.rb
+++ Library/Formula/plantuml.rb
@@ -6,9 +6,9 @@ class Plantuml < Formula
 
   bottle do
     cellar :any
-    sha256 "fdaebfab0679dfe6022b0698e2a9574688a91e9b913697712e97c51b70156e0a" => :yosemite
-    sha256 "62c5af2c0bea0e45170312ec7c7c9605f2d98d7d78bf60585213a96d354ddac0" => :mavericks
-    sha256 "871d67e7675e0d9935c5394d4eb64b4048efd979c058b613b3729d4710d8d532" => :mountain_lion
+    sha256 "07710e1822d0d984aebfee3a59583a1f593530a3e08a21b7ce3b8b60e4138aec" => :yosemite
+    sha256 "60cbe90a7e1f0020db1c56f5bfd0b4a5dec946456dc7afd019a40245bfff57fc" => :mavericks
+    sha256 "747e7004c64f56517e9868785497bc978755aaec15c0fae86c7cc4045ae9b8b4" => :mountain_lion
   end
 
   depends_on "graphviz"
