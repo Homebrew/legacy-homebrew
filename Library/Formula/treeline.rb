7c629fbdb345a790549e1426b2983b534c7165af
Tim D. Smith <git@tim-smith.us>
2015-09-09 05:01:57 -0700

    treeline: set up env explicitly

--- Library/Formula/treeline.rb
+++ Library/Formula/treeline.rb
@@ -4,19 +4,20 @@ class Treeline < Formula
   url "https://downloads.sourceforge.net/project/treeline/2.0.0/treeline-2.0.0.tar.gz"
   sha256 "71af995fca9e0eaf4e6205d72eb4ee6a979a45ea2a1f6600ed8a39bb1861d118"
 
-  bottle do
-    cellar :any
-    sha256 "271f5380e8ab3bf777a20f8dd077462ac1768baf749c7625100a7aeddb067190" => :yosemite
-    sha256 "f46d47da8720bd8cfbf54c14f5e0c2d6116be8d362d3eaebb5039f4996fb9800" => :mavericks
-    sha256 "f37b950321dca64236f8844eee5f62ec9e6bca115dde0d17160e519b0dbf95fe" => :mountain_lion
-  end
-
   depends_on :python3
   depends_on "sip" => "with-python3"
   depends_on "pyqt" => "with-python3"
 
   def install
-    system "./install.py", "-p#{prefix}"
+    pyver = Language::Python.major_minor_version "python3"
+    ENV.delete "PYTHONPATH"
+    %w[sip pyqt].each do |f|
+      ENV.append_path "PYTHONPATH", "#{Formula[f].opt_lib}/python#{pyver}/site-packages"
+    end
+
+    system "./install.py", "-p#{libexec}"
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
