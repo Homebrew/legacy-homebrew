246ea32dfcb6faf6d1de8b5322f25fb7f2da0062
thinker0 <thinker0@gmail.com>
2015-09-10 14:54:14 +0900

    fixed

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -1,18 +1,22 @@
 class AuroraScheduler < Formula
   desc "Apache Aurora Scheduler Client"
-  homepage "http://aurora.apache.org/"
-  url "http://www.us.apache.org/dist/aurora/0.9.0/apache-aurora-0.9.0.tar.gz"
-  version "0.9.0"
+  homepage "https://aurora.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=dist/aurora/0.9.0/apache-aurora-0.9.0.tar.gz"
   sha256 "16040866f3a799226452b1541892eb80ed3c61f47c33f1ccb0687fb5cf82767c"
 
-  depends_on "python" => :build
+  depends_on :python => :build
 
   def install
-    system "./pants", "binary", "src/main/python/apache/aurora/admin:kaurora_admin"
+    system "export LC_ALL=en_US.UTF-8"
     system "./pants", "binary", "src/main/python/apache/aurora/client/cli:kaurora"
-    system "mv", "dist/kaurora.pex", "dist/aurora"
-    system "mv", "dist/kaurora_admin.pex", "dist/aurora_admin"
-    bin.install "dist/aurora"
-    bin.install "dist/aurora_admin"
+    system "./pants", "binary", "src/main/python/apache/aurora/admin:kaurora_admin"
+    #system "mv", "dist/kaurora.pex", "dist/aurora"
+    #system "mv", "dist/kaurora_admin.pex", "dist/aurora_admin"
+    bin.install "dist/kaurora.pex" => "aurora"
+    bin.install "dist/kaurora_admin.pex" => "aurora_admin"
+  end
+
+  test do
+    system "echo Aurora Client : ", "$(aurora --version)"
   end
 end
