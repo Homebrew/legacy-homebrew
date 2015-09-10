d4032c64ab7d69c2ca0e51042566c51b317dd565
Dan <dan@wadoku.de>
2015-09-10 20:33:58 +0200

    mariadb 10.1.7 --devel
    Closes #43793.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/mariadb.rb
+++ Library/Formula/mariadb.rb
@@ -11,8 +11,8 @@ class Mariadb < Formula
   end
 
   devel do
-    url "http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.6/source/mariadb-10.1.6.tar.gz"
-    sha256 "492f28f0d7aee5bf0a0efd21c542ca4f291f349e66063695c5003df16e064959"
+    url "http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.7/source/mariadb-10.1.7.tar.gz"
+    sha256 "5bd3b80cf0f312751271a3446c12579c7081f93406e59a0cdfda8e133423c88f"
     # fix compilation failure with clang in mroonga storage engine
     # https://mariadb.atlassian.net/projects/MDEV/issues/MDEV-8551
     patch :DATA
@@ -231,7 +231,7 @@ index ebe7f6b..609f77d 100644
  endif()
 
 +if(CMAKE_COMPILER_IS_CLANGCXX)
-+  check_cxxflag("-fexceptions")
++  MY_CHECK_AND_SET_COMPILER_FLAG("-fexceptions")
 +endif()
 +
  if(NOT DEFINED CMAKE_C_COMPILE_OPTIONS_PIC)
