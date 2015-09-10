2eb616f3f9e18c9b94e03236bca0f17184d91d88
Xu Cheng <xucheng@me.com>
2015-09-10 21:20:34 +0800

    add dev-cmd folder

--- Library/Homebrew/cmd/command.rb
+++ Library/Homebrew/cmd/command.rb
@@ -3,7 +3,9 @@ module Homebrew
     cmd = ARGV.first
     cmd = HOMEBREW_INTERNAL_COMMAND_ALIASES.fetch(cmd, cmd)
 
-    if (path = HOMEBREW_LIBRARY_PATH/"cmd/#{cmd}.rb") && path.file?
+    if (path = HOMEBREW_LIBRARY_PATH/"cmd/#{cmd}.rb").file?
+      puts path
+    elsif ARGV.homebrew_developer? && (path = HOMEBREW_LIBRARY_PATH/"dev-cmd/#{cmd}.rb").file?
       puts path
     elsif (path = which("brew-#{cmd}") || which("brew-#{cmd}.rb"))
       puts path
