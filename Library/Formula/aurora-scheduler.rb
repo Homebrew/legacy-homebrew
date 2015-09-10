20b0e8497c6c3ebe0030cf66210f6bdb55ed1411
thinker0 <thinker0@gmail.com>
2015-09-10 15:03:47 +0900

    comment remove

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -10,8 +10,6 @@ class AuroraScheduler < Formula
     system "export LC_ALL=en_US.UTF-8"
     system "./pants", "binary", "src/main/python/apache/aurora/client/cli:kaurora"
     system "./pants", "binary", "src/main/python/apache/aurora/admin:kaurora_admin"
-    #system "mv", "dist/kaurora.pex", "dist/aurora"
-    #system "mv", "dist/kaurora_admin.pex", "dist/aurora_admin"
     bin.install "dist/kaurora.pex" => "aurora"
     bin.install "dist/kaurora_admin.pex" => "aurora_admin"
   end
