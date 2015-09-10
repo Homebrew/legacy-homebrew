15be4d0e0c2692f664fc1aa37d51d90b71bf24a1
Dominyk Tiller <dominyktiller@gmail.com>
2015-09-11 00:14:58 +0100

    typesafe-activator: fix version placement

--- Library/Formula/typesafe-activator.rb
+++ Library/Formula/typesafe-activator.rb
@@ -1,13 +1,12 @@
 class TypesafeActivator < Formula
   desc "Tools for working with Typesafe Reactive Platform"
   homepage "https://typesafe.com/activator"
-  version "1.3.6"
   url "https://downloads.typesafe.com/typesafe-activator/1.3.6/typesafe-activator-1.3.6-minimal.zip"
+  version "1.3.6"
   sha256 "3fc80ea6f4068955db65d1355ccc06a1f7e1a0ff05d71dd0861fb30cea415512"
 
   def install
     rm Dir["*.bat"] # Remove Windows .bat files
-    prefix.install_metafiles
     libexec.install Dir["*"]
     chmod 0755, libexec/"activator"
     bin.write_exec_script libexec/"activator"
