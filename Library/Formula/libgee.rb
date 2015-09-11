c01a32b1d3ab7d52c51a081d66a722551f15cbe3
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-11 17:06:13 +0200

    libgee 0.18.0

--- Library/Formula/libgee.rb
+++ Library/Formula/libgee.rb
@@ -1,8 +1,8 @@
 class Libgee < Formula
   desc "Collection library providing GObject-based interfaces"
   homepage "https://wiki.gnome.org/Projects/Libgee"
-  url "https://download.gnome.org/sources/libgee/0.16/libgee-0.16.0.tar.xz"
-  sha256 "a26243eac4280f2d2ebd34b377fc5dc7a73633bcc4c339af6162031943d76aae"
+  url "https://download.gnome.org/sources/libgee/0.18/libgee-0.18.0.tar.xz"
+  sha256 "4ad99ef937d071b4883c061df40bfe233f7649d50c354cf81235f180b4244399"
 
   bottle do
     cellar :any
@@ -17,9 +17,45 @@ class Libgee < Formula
   depends_on "gobject-introspection"
 
   def install
+    # ensures that the gobject-introspection files remain within the keg
+    inreplace "gee/Makefile.in" do |s|
+      s.gsub! "@HAVE_INTROSPECTION_TRUE@girdir = @INTROSPECTION_GIRDIR@",
+              "@HAVE_INTROSPECTION_TRUE@girdir = $(datadir)/gir-1.0"
+      s.gsub! "@HAVE_INTROSPECTION_TRUE@typelibdir = @INTROSPECTION_TYPELIBDIR@",
+              "@HAVE_INTROSPECTION_TRUE@typelibdir = $(libdir)/girepository-1.0"
+    end
+
     system "./configure", "--prefix=#{prefix}",
-                          "--disable-dependency-tracking",
-                          "--enable-introspection=yes"
+                          "--disable-dependency-tracking"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <gee.h>
+
+      int main(int argc, char *argv[]) {
+        GType type = gee_traversable_stream_get_type();
+        return 0;
+      }
+    EOS
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/gee-0.8
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -lgee-0.8
+      -lglib-2.0
+      -lgobject-2.0
+      -lintl
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
   end
 end
