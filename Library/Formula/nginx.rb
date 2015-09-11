6008b491d3e767da6a9b87ece653de6689c83c77
Mike McQuaid <mike@mikemcquaid.com>
2015-09-11 14:59:12 +0100

    nginx: add El Capitan bottle.

--- Library/Formula/nginx.rb
+++ Library/Formula/nginx.rb
@@ -7,6 +7,7 @@ class Nginx < Formula
 
   bottle do
     revision 1
+    sha256 "e6bb9cc02def5205747dc20ffd7c24a3c0e34a7494355a9b0dade58dd8ce9dcf" => :el_capitan
     sha256 "6df7b883f9214c5c0969559f6cc9ed56f9537263a1ef4882dae77b0b7af5a1df" => :yosemite
     sha256 "68741910d6ee58f7ee1134c7b4c62dfd490ee337c9c0beb0d188edc418b0bd93" => :mavericks
     sha256 "a622d0e1bfd55d8634520d72b41319da19828b9ab11063b6243d5b5f3d77ad08" => :mountain_lion
