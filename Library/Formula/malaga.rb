cf02230af1394fc5483bd50cb3f034fd2f73967e
Juhani Simola <juhani.simola@tilaajavastuu.fi>
2015-09-12 01:58:21 +0300

    malaga: use archive url
    The original download site at Bjorn Beutel's homepage under arcor.de
    seems to be gone. Switching to Ubuntu mirror.

--- Library/Formula/malaga.rb
+++ Library/Formula/malaga.rb
@@ -1,7 +1,7 @@
 class Malaga < Formula
   desc "Grammar development environment for natural languages"
   homepage "http://home.arcor.de/bjoern-beutel/malaga/"
-  url "http://home.arcor.de/bjoern-beutel/malaga/malaga-7.12.tgz"
+  url "https://launchpad.net/ubuntu/+archive/primary/+files/malaga_7.12.orig.tar.gz"
   sha256 "8811e5feaae03e1b6e3008116fdc7471a53b6c0c5036751c637b15058f855ace"
 
   bottle do
