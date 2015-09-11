6e1fd40523ba552bc3f24aaffde0a233271a989c
David Adrian <davidcadrian@gmail.com>
2015-09-11 11:47:59 -0400

    zmap 2.1.1
    Closes #43827.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/zmap.rb
+++ Library/Formula/zmap.rb
@@ -1,8 +1,8 @@
 class Zmap < Formula
   desc "Network scanner for Internet-wide network studies"
   homepage "https://zmap.io"
-  url "https://github.com/zmap/zmap/archive/v1.2.1.tar.gz"
-  sha256 "ff01f2b139e98a7e64ed2872ae332929c500dd6120888d57900fac82dccfeea8"
+  url "https://github.com/zmap/zmap/archive/v2.1.1.tar.gz"
+  sha256 "29627520c81101de01b0213434adb218a9f1210bfd3f2dcfdfc1f975dbce6399"
 
   head "https://github.com/zmap/zmap.git"
 
@@ -16,26 +16,30 @@ class Zmap < Formula
   depends_on "cmake" => :build
   depends_on "gengetopt" => :build
   depends_on "byacc" => :build
+  depends_on "pkg-config" => :build
   depends_on "gmp"
   depends_on "libdnet"
-  depends_on "json-c" => :optional
+  depends_on "json-c"
   depends_on "hiredis" => :optional
+  depends_on "mongo-c" => :optional
 
   def install
-    inreplace ["conf/zmap.conf", "src/zmap.c", "src/zopt.ggo"], "/etc", etc
+    inreplace ["conf/zmap.conf", "src/zmap.c", "src/zopt.ggo.in" ], "/etc", etc
 
     args = std_cmake_args
+    args << "-DENABLE_DEVELOPMENT=OFF"
     args << "-DRESPECT_INSTALL_PREFIX_CONFIG=ON"
-    args << "-DWITH_JSON=ON" if build.with? "json-c"
     args << "-DWITH_REDIS=ON" if build.with? "hiredis"
+    args << "-DWITH_MONGO=ON" if build.with? "mongo-c"
 
     system "cmake", ".", *args
+    system "make"
     system "make", "install"
   end
 
   test do
     system "#{sbin}/zmap", "--version"
-    assert_match /json/, `#{sbin}/zmap --list-output-modules` if build.with? "json-c"
-    assert_match /redis/, `#{sbin}/zmap --list-output-modules` if build.with? "hiredis"
+    assert_match /redis-csv/, `#{sbin}/zmap --list-output-modules` if build.with? "hiredis"
+    assert_match /mongo/, `#{sbin}/zmap --list-output-modules` if build.with? "mongo-c"
   end
 end
