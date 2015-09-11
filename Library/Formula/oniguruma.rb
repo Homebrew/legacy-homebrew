75b38311d56352c075d35370dd6967b672f38ed6
Mike McQuaid <mike@mikemcquaid.com>
2015-09-11 14:59:22 +0100

    oniguruma: add El Capitan bottle.

--- Library/Formula/oniguruma.rb
+++ Library/Formula/oniguruma.rb
@@ -7,6 +7,7 @@ class Oniguruma < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "653e3734097028be0863cdab51a8d3c92c1e3206cfec50072c18ab9e4e36bfc3" => :el_capitan
     sha256 "6a645e4c7bcf88d3d50aee632de816d884af7edf094fe793b451256e55bb8531" => :yosemite
     sha256 "ed3f1ef356a9dcae1ffff0b9bd853314b07289dfacdbf6b403515bcd35233227" => :mavericks
     sha256 "5a2ac152702eea6000bfa493607393f610fbec9684f1852cceb87834f20c9321" => :mountain_lion
