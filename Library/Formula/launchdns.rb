3b893599e16bcd9fdff4db2ca49058bfe93eb74b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-11 15:53:58 +0100

    launchdns: update 1.0.1 bottle.

--- Library/Formula/launchdns.rb
+++ Library/Formula/launchdns.rb
@@ -6,9 +6,12 @@ class Launchdns < Formula
   sha256 "e96d1b92819a294f1e325df629ae4bf202fd137b8504cf4ddd00cda7e47f7099"
 
   bottle do
-    sha1 "592ca0ff9d89f00613dd850be91fa15a8d2cfc6a" => :yosemite
-    sha1 "9ff650e25b17a1f29e9965c5e5bc678fc82d32aa" => :mavericks
-    sha1 "ffb5f5e33aa4429c4ada07f5992dbef2853b2cb5" => :mountain_lion
+    cellar :any
+    revision 1
+    sha256 "e09023cdbc899359414a8838b20e0221243a4bf2d34f6d3fc2f5671f0f64c04a" => :el_capitan
+    sha256 "3b6276a21a4a1523d6e14d34ae7332c7f39ee2544b61bfad2b32dcb9f0761576" => :yosemite
+    sha256 "7075bad8ec1f01f4b8deb59492b406bf4915d1e249aa0ea44d2615646af25bde" => :mavericks
+    sha256 "3b7e3ac513be0817414d7930f524887da956f331a7344df555e4c4c1083a7e43" => :mountain_lion
   end
 
   def install
