4fabd2ffe8ace95622609f12d43310ac8349ff2a
Mike McQuaid <mike@mikemcquaid.com>
2015-09-11 14:58:01 +0100

    go: add El Capitan bottle.

--- Library/Formula/go.rb
+++ Library/Formula/go.rb
@@ -9,6 +9,7 @@ class Go < Formula
   head "https://github.com/golang/go.git"
 
   bottle do
+    sha256 "aaebe29b5cf2bb1f57f7d81c7552be8a31e5f1fd570a74cc5b02b2a1c70d6b78" => :el_capitan
     sha256 "416903e6db83af6e6cd894b255f4dcab2a3c3cc00e96df2fec7ec0ef3316b4fa" => :yosemite
     sha256 "3dcba89c51ad223cc854d85a3354e2222de6925d6d8a161b68e84d1cfae3b54f" => :mavericks
     sha256 "6dcf1ea79a81501016369c342e2f3610e6148b1d0a9831f2896fc5380ab21654" => :mountain_lion
