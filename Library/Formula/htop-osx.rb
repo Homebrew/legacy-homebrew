6b4e6eb33a1c7684a0dd562261fb33f18235363a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 00:18:40 +0100

    htop-osx: update 0.8.2.7 bottle.

--- Library/Formula/htop-osx.rb
+++ Library/Formula/htop-osx.rb
@@ -5,6 +5,7 @@ class HtopOsx < Formula
   sha256 "a93be5c9d8a68081590b1646a0f10fb90e966e306560c3b141a61b3849446b72"
 
   bottle do
+    sha256 "d127accb5266fc5522cecc6e93a6039b978a7abbd6be9765b9991b72602a3459" => :el_capitan
     sha256 "c4f4c2be9f6bda38bef8e57570cb02ec2a60738b5e3e6b27c189e493582daf66" => :yosemite
     sha256 "1e3fa7862bfcef0eed646b7c2b7f1d3ec491404b2c9312ae1045bb78b8059f30" => :mavericks
     sha256 "611c4ee686babb880828510fd3b1cfa247aa1e0ba6cb60401e8ad5c6cac1fc75" => :mountain_lion
