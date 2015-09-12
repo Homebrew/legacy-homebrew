6007579d993f9e1c846cecaaa02f3a7f75fad6ca
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-12 18:00:40 +0100

    tree: update 1.7.0 bottle.

--- Library/Formula/tree.rb
+++ Library/Formula/tree.rb
@@ -6,10 +6,12 @@ class Tree < Formula
   sha256 "6957c20e82561ac4231638996e74f4cfa4e6faabc5a2f511f0b4e3940e8f7b12"
 
   bottle do
-    cellar :any
-    sha1 "d2e9699fce73145917f2b15f7f932765a47e1be6" => :mavericks
-    sha1 "7bdbb4ca0cc98d12c7fae1926c654786600beb0d" => :mountain_lion
-    sha1 "c92ed70a7a7d15f7ce5ee75a6851b59de11cf431" => :lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "2721bafcbe1db12e444f5025bdae81aa3c7cff6572aa7423e401baeb10a43157" => :el_capitan
+    sha256 "c1ad4f32c4922cbd1e37aa55b8fa6e0cc8a04c96a24f0d9e7957b50b311d854d" => :yosemite
+    sha256 "0bcb8a1de3ed51295917d6dd997ea492048f90a5ee1084676171a39dbd489654" => :mavericks
+    sha256 "c3bd091797c487bb1ac5586d0e524da341c6043b0298269a9ded271646274fa3" => :mountain_lion
   end
 
   def install
