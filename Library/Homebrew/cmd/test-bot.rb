edcef02b89f1d44ebdd04bb7fc812324a8426e4d
Xu Cheng <xucheng@me.com>
2015-09-12 22:15:23 +0800

    test-bot: always run brew tests

--- Library/Homebrew/cmd/test-bot.rb
+++ Library/Homebrew/cmd/test-bot.rb
@@ -573,10 +573,10 @@ module Homebrew
     def homebrew
       @category = __method__
       return if @skip_homebrew
+      test "brew", "tests"
       if @tap
         test "brew", "readall", @tap
       else
-        test "brew", "tests"
         test "brew", "tests", "--no-compat"
         readall_args = ["--aliases"]
         readall_args << "--syntax" if MacOS.version >= :mavericks
