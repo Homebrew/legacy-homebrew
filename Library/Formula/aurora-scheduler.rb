ed791766e44d396baae31d3263011655d50a5b5a
North <north.c@daumkakao.com>
2015-09-12 20:31:58 +0900

    deeepends_on maven

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -4,9 +4,10 @@ class AuroraScheduler < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=aurora/0.9.0/apache-aurora-0.9.0.tar.gz"
   sha256 "16040866f3a799226452b1541892eb80ed3c61f47c33f1ccb0687fb5cf82767c"
 
+  depends_on :java => "1.8+"
+  depends_on "maven" => :build
   depends_on "mesos" => :test
   depends_on "zookeeper" => :test
-  depends_on :java => "1.8+"
   depends_on "gradle" => :build
 
   def install
@@ -131,6 +132,10 @@ class AuroraScheduler < Formula
     system "aurora", "--version"
     system "aurora", "--help"
     system "aurora_admin", "--help"
+    #system "#{bin}/aurora", "config", "#{testpath}/hello_world.aurora"
+    #system "#{bin}/aurora", "job", "create", "devcluster/www-data/test/hello_world", "#{testpath}/hello_world.aurora", "--verbose"
+    #system "#{bin}/aurora", "job", "killall", "devcluster/www-data/test/hello_world"
+    #system "#{bin}/aurora_admin", "get_cluster_config", "--sh", "--export", "devcluster"
     system "zkServer", "stop"
 
     Process.kill("TERM", master)
