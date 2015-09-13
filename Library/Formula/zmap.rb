3616d02b7f35fc1e8674cc126c494cb36a68ea85
David Adrian <davidcadrian@gmail.com>
2015-09-12 23:19:24 -0400

    Revert zmap bottle checksums, remove unused var

--- Library/Formula/zmap.rb
+++ Library/Formula/zmap.rb
@@ -8,7 +8,9 @@ class Zmap < Formula
 
   bottle do
     cellar :any
-    sha256 "829eb6548654b367cffb7eefeaf04ed41425e97c4f276043c0a6bba467a2d68e" => :yosemite
+    sha1 "67ee1c3239a4109088a7b5f9d9db4884640c1920" => :mavericks
+    sha1 "3d780dc9a1125d96294d7071b1cdc359ccf4f302" => :mountain_lion
+    sha1 "b0482d7fedc3063f7218997f4ecdcb93043f898a" => :lion
   end
 
   depends_on "cmake" => :build
@@ -22,7 +24,6 @@ class Zmap < Formula
   depends_on "mongo-c" => :optional
 
   def install
-    zmap_etc = "#{etc}/zmap"
     inreplace ["conf/zmap.conf", "src/zmap.c", "src/zopt.ggo.in" ], "/etc", etc
 
     args = std_cmake_args
