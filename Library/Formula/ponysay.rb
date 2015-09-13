eac41a7d9ac84e70166e20f6c09ba69808606db3
Tim D. Smith <git@tim-smith.us>
2015-09-13 11:30:24 -0700

    ponysay: fix sandbox install
    Closes #43883.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/ponysay.rb
+++ Library/Formula/ponysay.rb
@@ -14,6 +14,13 @@ class Ponysay < Formula
   depends_on :python3
   depends_on "coreutils"
 
+  # fix shell completion install paths
+  # https://github.com/erkin/ponysay/pull/225
+  patch do
+    url "https://github.com/tdsmith/ponysay/commit/44fb0f85821eb34a811abb27d2c601a5d30af1f1.diff"
+    sha256 "0570b94a1179c189291cd9bad28cb93762aeed5ad6bbc3536027e178d0e6b9df"
+  end
+
   def install
     system "./setup.py",
            "--freedom=partial",
