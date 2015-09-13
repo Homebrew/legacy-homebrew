61844dbc1d1c2a1577a2aa2c533516fc2c7476ec
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 09:52:38 +0100

    cloog: update 0.18.3 bottle.

--- Library/Formula/cloog.rb
+++ Library/Formula/cloog.rb
@@ -7,6 +7,7 @@ class Cloog < Formula
   bottle do
     cellar :any
     revision 3
+    sha256 "249e851798964b001db4906934fd4907162fe7b43da41547804fa341a16a03ac" => :el_capitan
     sha256 "969fb3f488c9efb95cf1d631f6f2e577fa60f09c858b6cda5a477aec9f3552b3" => :yosemite
     sha256 "63786794ea49b86abd2c0e0168b36ff3489779f1de2b3dfd4ede509dcf6a5bfa" => :mavericks
     sha256 "00248fca2b492a7a32e9170e2875db55391b4881656cce3f4990f38ca04ec110" => :mountain_lion
