715ce3925686f34819d1a2aab448a2505b681ef8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 09:52:38 +0100

    autoconf: update 2.69 bottle.

--- Library/Formula/autoconf.rb
+++ Library/Formula/autoconf.rb
@@ -8,6 +8,7 @@ class Autoconf < Formula
   bottle do
     cellar :any_skip_relocation
     revision 4
+    sha256 "ded69c7dac4bc8747e52dca37d6d561e55e3162649d3805572db0dc2f940a4b8" => :el_capitan
     sha256 "daf70656aa9ff8b2fb612324222aa6b5e900e2705c9f555198bcd8cd798d7dd0" => :yosemite
     sha256 "d153b3318754731ff5e91b45b2518c75880993fa9d1f312a03696e2c1de0c9d5" => :mavericks
     sha256 "37e77a2e7ca6d479f0a471d5f5d828efff621bd051c1884ff1363d77c5c4675e" => :mountain_lion
