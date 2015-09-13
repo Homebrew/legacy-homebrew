b578d2ad4d696cd9ddb5667f532b3ca2b6291682
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 11:03:55 +0100

    jbig2dec: update 0.12 bottle.

--- Library/Formula/jbig2dec.rb
+++ Library/Formula/jbig2dec.rb
@@ -7,6 +7,7 @@ class Jbig2dec < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "38ad008992a9c273162238783b31bbcc4be8558d82c0a87b947ef0be699c437c" => :el_capitan
     sha256 "d1de5bcbceaca8669c847ec754e7d44b844ad08abdef377efdd704e768d13c86" => :yosemite
     sha256 "e42e117812549edeae1f60e1900b0692994c75ebae186f611e16528fe0521c89" => :mavericks
     sha256 "42039ee0b62ad6b4a153c5a5e93609ac1b668626b044a23a450a58d4d71338a5" => :mountain_lion
