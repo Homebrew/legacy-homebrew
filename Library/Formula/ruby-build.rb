dbee23779ab8fef2166cc78ed0c1d6edbbd901b2
Mike McQuaid <mike@mikemcquaid.com>
2015-09-13 10:10:33 +0100

    ruby-build: remove unnecessary bottle.

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -6,13 +6,6 @@ class RubyBuild < Formula
 
   head "https://github.com/sstephenson/ruby-build.git"
 
-  bottle do
-    cellar :any
-    sha256 "db4d6cdc4df9f5aa35401c8bda7350f6102547e51a6a03ae566c4d80ad4e5252" => :yosemite
-    sha256 "ec327bbd77d3ffd3d61d8d76281ef49d6ddf3617e82345d0755e046aba11c1b1" => :mavericks
-    sha256 "3a8e9b41353761ffde1118c7759913b002a4b2a6f098e5cded3c28efa5717cc6" => :mountain_lion
-  end
-
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
   depends_on "openssl" => :recommended
