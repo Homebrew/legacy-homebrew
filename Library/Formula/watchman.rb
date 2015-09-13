2bdfb1946597ab1744d484d5a3951c02946219d8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 12:33:52 +0100

    watchman: update 3.7.0 bottle.

--- Library/Formula/watchman.rb
+++ Library/Formula/watchman.rb
@@ -8,6 +8,7 @@ class Watchman < Formula
   head "https://github.com/facebook/watchman.git"
 
   bottle do
+    sha256 "72447a975cbe1ee5edf462e61f2a0182e291e60cb0195b47f57bca4e987a5ff6" => :el_capitan
     sha256 "988bc7562fe67087f281935d2b064c172161b036a22539c50ec04b215235749c" => :yosemite
     sha256 "ae7410503f560b5ad62277e468a477c6cf7457986834ca7e2d13871661d7a131" => :mavericks
     sha256 "cc77325547e91585adbdf7341808abbced3c352ff73742365592b1654184f2dd" => :mountain_lion
