8abda8bb60d48804700cebd0c5dabf6f09893b05
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 20:35:34 +0100

    d-bus: update 1.10.0 bottle.

--- Library/Formula/d-bus.rb
+++ Library/Formula/d-bus.rb
@@ -10,6 +10,7 @@ class DBus < Formula
   end
 
   bottle do
+    sha256 "68500e3670555c2bf5eaeae7541111f9052e92c4a7b28410a0ded25fd7cac544" => :el_capitan
     sha256 "5a49bcf55334b90e3cd6725b7cc6f0383bcf0160ca7a8d12611107ac7f6a022a" => :yosemite
     sha256 "a2b9e6c8e84a6e199dcd22d748b3c617285a1eba4ec0b042fa16e9171e96bd91" => :mavericks
     sha256 "8f571788a6bd6209e27b4e4cbdd4e1d2179b7f3bbdb35262c3159ff28336b76a" => :mountain_lion
