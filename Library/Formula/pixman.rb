bca51dc19fca66981e6de48a28a0a837d8db6a8b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 09:52:40 +0100

    pixman: update 0.32.6 bottle.

--- Library/Formula/pixman.rb
+++ Library/Formula/pixman.rb
@@ -7,6 +7,7 @@ class Pixman < Formula
   bottle do
     cellar :any
     revision 2
+    sha256 "377f9b602abd3f1ba92c0cce5671e12aaf8815b07882a09c5f61e6774bc4c686" => :el_capitan
     sha256 "3a5221db33dcb4684bbae580a7f2c58f532cb936126481ad8a13c72935049367" => :yosemite
     sha256 "4e65fd79c76a05313bfc16d2e1ae28f60f82f0fb6909f1c9769d09e61e6562d0" => :mavericks
     sha256 "074a35377da2e6637fbafde6d2221ac1ae73af7d24e50398be192cd6847dd670" => :mountain_lion
