5937b5cc682526eba7eaf7801213d7224178f3ff
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 09:52:39 +0100

    libgcrypt: update 1.6.4 bottle.

--- Library/Formula/libgcrypt.rb
+++ Library/Formula/libgcrypt.rb
@@ -8,6 +8,7 @@ class Libgcrypt < Formula
 
   bottle do
     cellar :any
+    sha256 "c239866860860e717e646856b9870e7cd9ee0729b8700a40f8be47a174d29146" => :el_capitan
     sha256 "e8559d5c93be44c94a4652f74d8835a416b1771e492446c2a2d9da725460d5ca" => :yosemite
     sha256 "09a01dd58c81f0efb278d2e1270983ae4e477bb5fc4c54489dc6582084a147bb" => :mavericks
     sha256 "1c79948cbb7bb2750f23a6b3a91aafbd49ef0eb4d5868cabd91dcfb7592dac19" => :mountain_lion
