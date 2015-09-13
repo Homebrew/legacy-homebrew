f35c74d6d2e3abc4586c7c090ae8aadd09841253
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 09:52:39 +0100

    gobject-introspection: update 1.44.0 bottle.

--- Library/Formula/gobject-introspection.rb
+++ Library/Formula/gobject-introspection.rb
@@ -6,6 +6,7 @@ class GobjectIntrospection < Formula
 
   bottle do
     revision 2
+    sha256 "2b70ddf6f20e91dbded15f54fbf770879e815a677f8eb9eec0f587b31aa1c17c" => :el_capitan
     sha256 "e29497a4aa084f25f7d53988beab1999c4b3145896f0ef6a993b0d7736269cbd" => :yosemite
     sha256 "1e0e84d4d114f39d89549bc5a6bfae59a84655a1aefce926d8dd6e53495390ae" => :mavericks
     sha256 "3dcfedfe989ec4d9c6558def0190ef3bd3214bafa4d2f53fd28aa1abbc1403f2" => :mountain_lion
