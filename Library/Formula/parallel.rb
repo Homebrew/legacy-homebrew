901b8ed2786a3b150d58515ef18e53da35e9acaf
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-13 20:35:36 +0100

    parallel: update 20150822 bottle.

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -7,7 +7,8 @@ class Parallel < Formula
   head "http://git.savannah.gnu.org/r/parallel.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "420e4f174531d23a4ee759bd553966dac737b3d7f013eb172734c6315739df7b" => :el_capitan
     sha256 "c6dd1794c15b00c77e9a5556bfe0edac97d422f408d34cbb00992e69ce2614a2" => :yosemite
     sha256 "5eb6f10c81f5fb608d476ad075fd9c922f6144be1bfdc39c1f41303e4dacd8df" => :mavericks
     sha256 "8ce7a360d18600da0dd69dd87ecadc82282dfb7b876ab76817ef3b25e06f2520" => :mountain_lion
