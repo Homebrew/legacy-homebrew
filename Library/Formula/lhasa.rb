283418be6b6686116693d25fbc554a83ce25caa6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-14 17:56:37 +0100

    lhasa: update 0.3.0 bottle.

--- Library/Formula/lhasa.rb
+++ Library/Formula/lhasa.rb
@@ -7,9 +7,9 @@ class Lhasa < Formula
 
   bottle do
     cellar :any
-    sha1 "31020074660df16a6a8103ec7cc27e243d053d38" => :mavericks
-    sha1 "b1758087df7ecdd4e206391c0fb6a6fa4d36bdc0" => :mountain_lion
-    sha1 "ca05444eab009813f610815c319c91705bb397a1" => :lion
+    sha256 "e165d272782133234ee3064fcb3dcac1923c3fe5d79891cb181619b6d8e79dde" => :yosemite
+    sha256 "c7c28b850933f06b27a7af32a73a840437663aa334010eb097ecb7f3ff6dd4c1" => :mavericks
+    sha256 "af8aac8f99b3cf42dde5e69d0f18e1e72370cde24b3b00fb1577fd5a99e54489" => :mountain_lion
   end
 
   conflicts_with "lha", :because => "both install a `lha` binary"
