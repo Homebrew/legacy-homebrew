bee19afff9abb0564e15142680bc8c015e586aa5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-14 22:10:01 +0100

    lrdf: add 0.5.0_1 bottle.

--- Library/Formula/lrdf.rb
+++ Library/Formula/lrdf.rb
@@ -5,6 +5,13 @@ class Lrdf < Formula
   sha256 "ba803af936fd53a8b31651043732e6d6cec3d24fa24d2cb8c1506c2d1675e2a2"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "cfb4b27e2dcf30a7c6d81bef96b750339d044de3c31961d1af80634ee8943014" => :yosemite
+    sha256 "e3ef1224928a2a5d00753bbc85263acc1208aa6f31ea6750f518f57176836cbf" => :mavericks
+    sha256 "31f0395487e70ca4a1f615d5f8ebe3f287faaed7edffca372396b60da2ab4720" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "automake" => :build
   depends_on "autoconf" => :build
