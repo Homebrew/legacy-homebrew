5c69f9bef01d4138fc102381cd74aaba3415e9e1
Andrew Janke <andrew@apjanke.net>
2015-09-14 05:26:14 -0400

    lrdf: add openssl dependency

--- Library/Formula/lrdf.rb
+++ Library/Formula/lrdf.rb
@@ -3,12 +3,14 @@ class Lrdf < Formula
   homepage "https://github.com/swh/LRDF"
   url "https://github.com/swh/LRDF/archive/0.5.0.tar.gz"
   sha256 "ba803af936fd53a8b31651043732e6d6cec3d24fa24d2cb8c1506c2d1675e2a2"
+  revision 1
 
   depends_on "pkg-config" => :build
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
   depends_on "raptor"
+  depends_on "openssl"
 
   def install
     system "glibtoolize"
