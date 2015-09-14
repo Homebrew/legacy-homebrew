216fd2638bb82e44f9ccbc26b92c3709d2e6b43c
Tim D. Smith <git@tim-smith.us>
2015-09-13 22:33:07 -0700

    pyqt5: bump revision for python 3.5

--- Library/Formula/pyqt5.rb
+++ Library/Formula/pyqt5.rb
@@ -3,6 +3,7 @@ class Pyqt5 < Formula
   homepage "http://www.riverbankcomputing.co.uk/software/pyqt/download5"
   url "https://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.5/PyQt-gpl-5.5.tar.gz"
   sha256 "cdd1bb55b431acdb50e9210af135428a13fb32d7b1ab86e972ac7101f6acd814"
+  revision 1
 
   bottle do
     revision 1
