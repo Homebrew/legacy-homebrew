109aadcf7f988efaa8b914dbb4fb8d425377916c
Xu Cheng <xucheng@me.com>
2015-09-14 11:22:50 +0800

    pyenv: make var symlinks in post_install
    Also remove unnecessary bottles
    
    Closes #43900.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/pyenv.rb
+++ Library/Formula/pyenv.rb
@@ -5,12 +5,6 @@ class Pyenv < Formula
   url "https://github.com/yyuu/pyenv/archive/v20150913.tar.gz"
   sha256 "7ccee5b7c6c18dd79cfe504f69649e1f644aa495104aa58672c93e35c1eae0a0"
 
-  bottle do
-    sha256 "e730fabf21db78a7adf1aa8d2108495084584c5bd164bc7a65b97e9626ecd01b" => :yosemite
-    sha256 "79ad45e31f8ac9f9720f849c29f8d1a3a7f0867e4f4fc9152c4327b265babd11" => :mavericks
-    sha256 "34b6a13c138dfd1b18973b42a5119418fcf1fb980b2f0f503ca869e3ae5f7779" => :mountain_lion
-  end
-
   depends_on "autoconf" => [:recommended, :run]
   depends_on "pkg-config" => [:recommended, :run]
 
@@ -19,18 +13,20 @@ class Pyenv < Formula
     prefix.install "bin", "completions", "libexec"
     prefix.install "plugins" => "default-plugins"
 
-    var_lib = "#{HOMEBREW_PREFIX}/var/lib/pyenv"
+    %w[pyenv-install pyenv-uninstall python-build].each do |cmd|
+      bin.install_symlink "#{prefix}/default-plugins/python-build/bin/#{cmd}"
+    end
+  end
+
+  def post_install
+    var_lib = HOMEBREW_PREFIX/"var/lib/pyenv"
     %w[plugins versions].each do |dir|
       var_dir = "#{var_lib}/#{dir}"
       mkdir_p var_dir
       ln_sf var_dir, "#{prefix}/#{dir}"
     end
 
-    rm_f "#{var_lib}/plugins/python-build"
-    ln_sf "#{prefix}/default-plugins/python-build", "#{var_lib}/plugins/python-build"
-    %w[pyenv-install pyenv-uninstall python-build].each do |cmd|
-      bin.install_symlink "#{prefix}/default-plugins/python-build/bin/#{cmd}"
-    end
+    (var_lib/"plugins").install_symlink "#{prefix}/default-plugins/python-build"
   end
 
   def caveats; <<-EOS.undent
