1b602f88a590f19826935f945cc16b82decf4226
Andrew Janke <andrew@apjanke.net>
2015-09-14 06:22:17 -0400

    mktorrent: add openssl dependency
    Closes #43928.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/mktorrent.rb
+++ Library/Formula/mktorrent.rb
@@ -3,6 +3,9 @@ class Mktorrent < Formula
   homepage "http://mktorrent.sourceforge.net/"
   url "https://downloads.sourceforge.net/mktorrent/mktorrent-1.0.tar.gz"
   sha256 "6f8e562af6366e0d9bde76e434f740b55722c6c3c555860dbe80083f9d1d119f"
+  revision 1
+
+  depends_on "openssl"
 
   def install
     system "make USE_PTHREADS=1 USE_OPENSSL=1 USE_LONG_OPTIONS=1"
