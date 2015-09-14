8e124d7e755a70be77d6b7aca9a47ca20ece132b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-14 08:08:00 +0100

    libassuan: update 2.3.0_1 bottle.

--- Library/Formula/libassuan.rb
+++ Library/Formula/libassuan.rb
@@ -9,6 +9,7 @@ class Libassuan < Formula
 
   bottle do
     cellar :any
+    sha256 "be7ca3b1e5d4c70d4ff7fd43b657bc4d0127ac069a8cb14aff4022c326800a13" => :el_capitan
     sha256 "04861a8ccc1eb1abdd90f72dc6460d446c43f69e302217db7d801482797f0943" => :yosemite
     sha256 "25abeb3328bf5c5c156436f74c19ea9e4ecb565f69bc0c5ec5a35373e0aa8c71" => :mavericks
     sha256 "e1cdee9fbe7d6dbadd0e3aea87e935584ae8c1469f7b47e00e462767aef5c195" => :mountain_lion
