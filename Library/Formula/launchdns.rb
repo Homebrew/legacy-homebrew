3782368394499962b33f44be915ac49819f565b2
Mike McQuaid <mike@mikemcquaid.com>
2015-09-14 19:23:02 +0100

    launchdns: remove broken El Capitan bottle.

--- Library/Formula/launchdns.rb
+++ Library/Formula/launchdns.rb
@@ -8,7 +8,6 @@ class Launchdns < Formula
   bottle do
     cellar :any
     revision 1
-    sha256 "e09023cdbc899359414a8838b20e0221243a4bf2d34f6d3fc2f5671f0f64c04a" => :el_capitan
     sha256 "3b6276a21a4a1523d6e14d34ae7332c7f39ee2544b61bfad2b32dcb9f0761576" => :yosemite
     sha256 "7075bad8ec1f01f4b8deb59492b406bf4915d1e249aa0ea44d2615646af25bde" => :mavericks
     sha256 "3b7e3ac513be0817414d7930f524887da956f331a7344df555e4c4c1083a7e43" => :mountain_lion
