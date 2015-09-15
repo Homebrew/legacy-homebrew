aabdff26e7f2a84e3bcec17de577300362b23629
rpau <rpau@ac.upc.edu>
2015-09-15 09:30:43 +0300

    review changes applied and brew audit command as well

--- Library/Formula/walkmod.rb
+++ Library/Formula/walkmod.rb
@@ -1,22 +1,20 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Walkmod < Formula
+  desc "Java-based project to apply code conventions"
   homepage "http://www.walkmod.com"
   url "https://bitbucket.org/rpau/walkmod/downloads/walkmod-1.3.0-installer.zip"
-  version "1.3.0"
-  sha1 "1b23b4791f0090b4db183d4c894c3e0f623b5e63"
-
+  sha256 "01ea47c99f7d7bffeb0cd0d960fdab4f09a7d51a1e0979c6b4ff71d0aeb049fd"
   depends_on :java
 
   def install
-     #Remove windows files
-     rm_f Dir["bin/*.bat"]
-     
-     libexec.install Dir["*"]
+    # Remove windows files
+    rm_f Dir["bin/*.bat"]
 
-     bin.install_symlink libexec+"bin/walkmod"
+    libexec.install Dir["*"]
+
+    bin.install_symlink libexec+"bin/walkmod"
   end
 
+  test do
+    system bin/"walkmod", "--version"
+  end
 end
