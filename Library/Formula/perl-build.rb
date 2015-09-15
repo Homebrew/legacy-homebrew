9967656184ce1240d6fa96810afefe181d81e76f
moltar <moltar@moltar.net>
2015-09-15 10:58:00 -0400

    Update perl-build to 1.12

--- Library/Formula/perl-build.rb
+++ Library/Formula/perl-build.rb
@@ -1,7 +1,7 @@
 class PerlBuild < Formula
   desc "Perl builder"
   homepage "https://github.com/tokuhirom/Perl-Build"
-  url "https://github.com/tokuhirom/Perl-Build/archive/1.10.tar.gz"
+  url "https://github.com/tokuhirom/Perl-Build/archive/1.12.tar.gz"
   sha256 "f005a730ee32b5dfc79af662f0862b31f078606ae3e78986b4be359a1e5e2bb9"
 
   head "https://github.com/tokuhirom/perl-build.git"
