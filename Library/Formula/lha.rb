339541d5bfddcbefdbdf9e1e954c450f60e9d1fd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-15 06:12:26 +0100

    lha: add 1.14i-ac20050924p1 bottle.

--- Library/Formula/lha.rb
+++ Library/Formula/lha.rb
@@ -5,6 +5,13 @@ class Lha < Formula
   version "1.14i-ac20050924p1"
   sha256 "b5261e9f98538816aa9e64791f23cb83f1632ecda61f02e54b6749e9ca5e9ee4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9cb516a73d1d117c39f63d16b3211df626783c9bb1a7038f524dd9c36045b1ac" => :yosemite
+    sha256 "bd26a5a48396d06019f7998f4c9bf511a74ef237814fee5f5c8ba9df31b30a37" => :mavericks
+    sha256 "3def5e7035ef758428c7e158b93ad5459d6d221ba4698608cc1d14526cbfd5f8" => :mountain_lion
+  end
+
   head do
     url "http://scm.osdn.jp/gitroot/lha/lha.git"
     depends_on "autoconf" => :build
