13e3aacc43b99b902e52aba15f72081863937cef
Zac Stewart <zgstewart@gmail.com>
2015-09-15 16:40:20 -0400

    uru 0.8.0
    Upgrade uru to 0.8.0
    
    Closes #43984.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/uru.rb
+++ Library/Formula/uru.rb
@@ -1,8 +1,8 @@
 class Uru < Formula
   desc "Use multiple rubies on multiple platforms"
   homepage "https://bitbucket.org/jonforums/uru"
-  url "https://bitbucket.org/jonforums/uru/get/v0.7.6.tar.gz"
-  sha256 "b6243315801ce28ec7f7c12c02de558188752b14dba9da0813da83ccafdb91cd"
+  url "https://bitbucket.org/jonforums/uru/get/v0.8.0.tar.gz"
+  sha256 "7201ee957ad35131f6461ca35cc0fe77ca8e38d4e1603bad02871f5222413ee4"
 
   bottle do
     cellar :any
@@ -16,7 +16,7 @@ class Uru < Formula
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/bitbucket.org/jonforums/uru").install Dir["*"]
-    system "go", "build", "-ldflags", "-s", "bitbucket.org/jonforums/uru"
+    system "go", "build", "-ldflags", "-s", "bitbucket.org/jonforums/uru/cmd/uru"
     bin.install "uru" => "uru_rt"
   end
 
