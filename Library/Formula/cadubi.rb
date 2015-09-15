6171377a1225ba849acf608df0dcf0002ed974dd
Baptiste Fontaine <batifon@yahoo.fr>
2015-09-16 00:31:35 +0200

    cadubi 1.3.1

--- Library/Formula/cadubi.rb
+++ Library/Formula/cadubi.rb
@@ -1,8 +1,10 @@
 class Cadubi < Formula
   desc "Creative ASCII drawing utility"
-  homepage "http://langworth.com/pub/cadubi/"
-  url "https://github.com/statico/cadubi/releases/download/v1.3/cadubi-1.3.tar.gz"
-  sha256 "ca8b6ea305e0eccb11add7fc165beeee7ef33f9f0106e84efa1b364f082df0ab"
+  homepage "https://github.com/statico/cadubi"
+  url "https://github.com/statico/cadubi/archive/v1.3.1.tar.gz"
+  sha256 "162c3ba748bbd2ab1699c95d4ad0e257ffe183959e6ce084ab91efbd3eb73f8a"
+
+  head "https://github.com/statico/cadubi.git"
 
   bottle :unneeded
 
@@ -10,5 +12,12 @@ class Cadubi < Formula
     inreplace "cadubi", "$Bin/help.txt", "#{doc}/help.txt"
     bin.install "cadubi"
     doc.install "help.txt"
+    man1.install "cadubi.1"
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    assert_match version.to_s,
+      shell_output("script -q /dev/null #{bin}/cadubi --version 2>&1", 1)
   end
 end
