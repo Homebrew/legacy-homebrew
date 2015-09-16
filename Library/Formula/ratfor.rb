d92f623c1dee1ef9bbd1ef52a9cd15e53c4bada6
Franklin Chen <franklinchen@franklinchen.com>
2015-09-16 16:42:13 -0400

    ratfor (new formula)

---
+++ Library/Formula/ratfor.rb
@@ -0,0 +1,15 @@
+class Ratfor < Formula
+  version "1.02"
+  desc "Rational Fortran"
+  homepage "http://www.dgate.org/ratfor/"
+  url "http://www.dgate.org/ratfor/ratfor.tar.gz"
+  sha256 "daf2971df48c3b3908c6788c4c6b3cdfb4eaad21ec819eee70a060956736ea1c"
+
+  depends_on "gcc"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "check"
+    system "make", "install"
+  end
+end
