4d1143a80009e5954d3fa2de0de357335fb7ecf4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-16 17:41:35 +0100

    jboss-forge: update 2.19.1.Final bottle.

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -6,10 +6,10 @@ class JbossForge < Formula
   sha256 "3047e54edd929262e80c31aeb8adc9f835395a102109a3653d690111c522c421"
 
   bottle do
-    cellar :any
-    sha256 "313805bbb8c57c9080418e93c8fcab563879f65842c45c37fc3e269c1402780e" => :yosemite
-    sha256 "058bd3d6b6b50262028b5ea2e4438b4b800abb578422845bfbd8c73819d56d8f" => :mavericks
-    sha256 "82cec34e430ccd330c5928080521988cf654944ddd379014993aa37f03996ef1" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "ddf3d51ad0c59f4d627fa889b21d0044ebe93b09c605cde25cdf0d9ab282a99c" => :el_capitan
+    sha256 "c33be3fc0799f9d1fee87301031ccb7808186965b9ce4a6b0429e78cb2b50349" => :yosemite
+    sha256 "e1fee1c35f6d3846368397625f662bf76aa27d6cd1777aae4f55f820d35caf48" => :mavericks
   end
 
   def install
