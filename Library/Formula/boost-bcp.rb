277878a8e21ca57db09cbe970d4394a12fdff789
David Larson <dplarson@ucsd.edu>
2015-09-15 21:19:56 -0700

    boost-bcp 1.59.0

--- Library/Formula/boost-bcp.rb
+++ Library/Formula/boost-bcp.rb
@@ -1,8 +1,9 @@
 class BoostBcp < Formula
   desc "Utility for extracting subsets of the Boost library"
   homepage "http://www.boost.org/doc/tools/bcp/"
-  url "https://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.bz2"
-  sha256 "fdfc204fc33ec79c99b9a74944c3e54bd78be4f7f15e260c0e2700a36dc7d3e5"
+  url "https://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.tar.bz2"
+  sha256 "727a932322d94287b62abb1bd2d41723eec4356a7728909e38adb65ca25241ca"
+
 
   head "https://github.com/boostorg/boost.git"
 
