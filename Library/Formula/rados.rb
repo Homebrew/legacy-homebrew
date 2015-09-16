14e6dea0cf6b595fa5f7677913a693d7e23e7d89
Bert JW Regeer <bertjw@regeer.org>
2015-09-16 17:30:16 -0600

    Add rados as a Formula

---
+++ Library/Formula/rados.rb
@@ -0,0 +1,53 @@
+class Rados < Formula
+  desc "RADOS is a library for connecting and utilizing Ceph"
+  homepage "http://ceph.com/"
+  head "https://github.com/ceph/ceph.git"
+
+  depends_on "openssl"
+  depends_on "snappy"
+  depends_on "cryptopp"
+  depends_on "boost"
+  depends_on "leveldb"
+  depends_on "libtool"
+  depends_on "automake"
+  depends_on "pkgconfig"
+  depends_on "python"
+
+
+  def install
+    system "./autogen.sh"
+    
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    system "./configure", "--without-fuse",
+                          "--without-tcmalloc",
+                          "--without-libaio",
+                          "--without-libxfs",
+                          "--with-rados",
+                          "--without-rbd",
+                          "--without-cephfs",
+                          "--without-radosgw",
+                          "--without-selinux",
+                          "--without-radosstriper",
+                          "--without-mon",
+                          "--without-mds",
+                          "--without-debug",
+                          "--without-libatomic-ops",
+                          "--disable-client",
+                          "--disable-server",
+                          "--disable-coverage",
+                          "--disable-pgrefdebugging",
+                          "--disable-cephfs-java",
+                          "--disable-xio",
+                          "--disable-valgrind",
+                          "--without-libzfs",
+                          "--without-lttng",
+                          "--without-babeltrace",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/rados"
+  end
+end
