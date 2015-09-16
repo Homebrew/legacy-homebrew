399e10640bab12f9ee7ca3cbb492e4c80fe76536
Andrew Janke <andrew@apjanke.net>
2015-09-16 16:27:01 -0500

    xlslib: fix download url

--- Library/Formula/xlslib.rb
+++ Library/Formula/xlslib.rb
@@ -3,7 +3,7 @@ require "formula"
 class Xlslib < Formula
   desc "C++/C library to construct Excel .xls files in code"
   homepage "http://sourceforge.net/projects/xlslib"
-  url "https://downloads.sourceforge.net/project/xlslib/xlslib-package-2.4.0.zip"
+  url "https://downloads.sourceforge.net/project/xlslib/xlslib-old/xlslib-package-2.4.0.zip"
   sha1 "73447c5c632c0e92c1852bd2a2cada7dd25f5492"
 
   bottle do
