4ea756bd9617572d65169e70e492c7123d81fd54
Ryan Schlesinger <ryan@aceofsales.com>
2015-09-17 13:44:24 -0700

    zsh 5.1.1
    Closes #44081.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/zsh.rb
+++ Library/Formula/zsh.rb
@@ -1,9 +1,9 @@
 class Zsh < Formula
   desc "UNIX shell (command interpreter)"
   homepage "http://www.zsh.org/"
-  url "https://downloads.sourceforge.net/project/zsh/zsh/5.1/zsh-5.1.tar.gz"
-  mirror "http://www.zsh.org/pub/zsh-5.1.tar.gz"
-  sha256 "e3731381810e690fb955cedfa8be51b0934bfa1ff38c709f54138960e3decd99"
+  url "https://downloads.sourceforge.net/project/zsh/zsh/5.1.1/zsh-5.1.1.tar.gz"
+  mirror "http://www.zsh.org/pub/zsh-5.1.1.tar.gz"
+  sha256 "94ed5b412023761bc8d2f03c173f13d625e06e5d6f0dff2c7a6e140c3fa55087"
 
   bottle do
     sha256 "1509f461b1dee825cb66183409c04820f71e559c116201c4073ccd97eeb67704" => :el_capitan
