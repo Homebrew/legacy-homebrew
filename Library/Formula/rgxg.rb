faec1d0fe7d56b3996d4f2dd4d8db622dc53ec55
Jerome Wittmann <jrwittmann@me.com>
2015-09-17 21:46:23 +0200

    Adapted formula description

--- Library/Formula/rgxg.rb
+++ Library/Formula/rgxg.rb
@@ -1,5 +1,5 @@
 class Rgxg < Formula
-  desc "rgxg (ReGular eXpression Generator)"
+  desc "C library and command-line tool to generate (extended) regular expressions"
   homepage "http://rgxg.sourceforge.net"
   url "https://downloads.sourceforge.net/project/rgxg/rgxg/rgxg-0.1.tar.gz"
   sha256 "4adbc128faf87e44ec80d9dfd3b34871c84634c2ae0f9cfaedd16b07d13f9484"
