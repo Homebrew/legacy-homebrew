f5dc5b0340aefed0611caff7d9a14d1cc188955c
Franklin Chen <franklinchen@franklinchen.com>
2015-09-17 09:02:47 -0400

    Fix formula, adding versioned URL and using :fortran.

--- Library/Formula/ratfor.rb
+++ Library/Formula/ratfor.rb
@@ -2,10 +2,10 @@ class Ratfor < Formula
   version "1.02"
   desc "Rational Fortran"
   homepage "http://www.dgate.org/ratfor/"
-  url "http://www.dgate.org/ratfor/ratfor.tar.gz"
+  url "http://www.dgate.org/ratfor/tars/ratfor-1.02.tar.gz"
   sha256 "daf2971df48c3b3908c6788c4c6b3cdfb4eaad21ec819eee70a060956736ea1c"
 
-  depends_on "gcc"
+  depends_on :fortran
 
   def install
     system "./configure", "--prefix=#{prefix}"
