e504a0c969e7ff87d3ec525acdac9f6e05a7cdae
Dominyk Tiller <dominyktiller@gmail.com>
2015-09-18 05:14:17 +0100

    kotlin-compiler: remove unnecessary bottles

--- Library/Formula/kotlin-compiler.rb
+++ Library/Formula/kotlin-compiler.rb
@@ -4,13 +4,6 @@ class KotlinCompiler < Formula
   url "https://github.com/JetBrains/kotlin/releases/download/build-0.13.1513/kotlin-compiler-0.13.1513.zip"
   sha256 "ac93ad3360f38ba2692a7f8bc7b7c74e8ca4cdb03e6e71377c51cd44bf842698"
 
-  bottle do
-    cellar :any
-    sha256 "c4dd4a056800031c1a40ab9cf8245512f7058ab498662d59de9cb06d4dd76617" => :yosemite
-    sha256 "3afbc461229e3d9263cacc3fe616230aa34853dd3fea8a4a5ee102254cd711ba" => :mavericks
-    sha256 "b21f3ef222bd11e995e47439d2b8ae42e7142fbc716feef0e33eb4b7192eda40" => :mountain_lion
-  end
-
   def install
     libexec.install %w[bin lib]
     rm Dir["#{libexec}/bin/*.bat"]
