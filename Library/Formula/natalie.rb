dc56513be70de2e0a2da2bdc4ea7f5334bc1e80d
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-09-18 18:08:51 +0200

    Update Natalie to v. 0.3

--- Library/Formula/natalie.rb
+++ Library/Formula/natalie.rb
@@ -1,8 +1,8 @@
 class Natalie < Formula
   desc "Storyboard Code Generator (for Swift)"
   homepage "https://github.com/krzyzanowskim/Natalie"
-  url "https://github.com/krzyzanowskim/Natalie/archive/0.2.tar.gz"
-  sha256 "a4dd246db70e8818ecde34df68a60d8bf15c51465b365f466e606f74e41714e9"
+  url "https://github.com/krzyzanowskim/Natalie/archive/0.3.tar.gz"
+  sha256 "8fa28a53e963fb9bcee73b040fb6c6fbbe20d20472c8f42e0c3795603a2295f4"
   head "https://github.com/krzyzanowskim/Natalie.git"
 
   bottle do
@@ -10,7 +10,7 @@ class Natalie < Formula
     sha256 "4524f95293b7d838886370600f7350cfec1796e06bac2e67c3199f6fc73f9bb2" => :yosemite
   end
 
-  depends_on :xcode => "6.3"
+  depends_on :xcode => "7.0"
 
   def install
     mv "natalie.swift", "natalie-script.swift"
