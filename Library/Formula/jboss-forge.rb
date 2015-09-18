16d3da81c68b88a83f874a74e05cd5c589f34ef5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-18 22:30:18 +0100

    jboss-forge: update 2.19.2.Final bottle.

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -7,9 +7,9 @@ class JbossForge < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ddf3d51ad0c59f4d627fa889b21d0044ebe93b09c605cde25cdf0d9ab282a99c" => :el_capitan
-    sha256 "c33be3fc0799f9d1fee87301031ccb7808186965b9ce4a6b0429e78cb2b50349" => :yosemite
-    sha256 "e1fee1c35f6d3846368397625f662bf76aa27d6cd1777aae4f55f820d35caf48" => :mavericks
+    sha256 "700167db26aa4ee2b502807ff444879662fb645e3e126b9cff0929c9fbcc6c4b" => :el_capitan
+    sha256 "709aa0df9229e169fe1005763d51fe33be652348f6c45662955e41392ef8e9ab" => :yosemite
+    sha256 "e100821705db0ec9db49eb0f1ae05746b2f2bbfcbf74f1df6fa14bf86ad85a42" => :mavericks
   end
 
   def install
