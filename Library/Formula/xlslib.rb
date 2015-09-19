7b9675d7c884adf6586abe633cf6b0c43afb5ac0
Dominyk Tiller <dominyktiller@gmail.com>
2015-09-19 17:44:39 +0100

    xlslib: add mirror

--- Library/Formula/xlslib.rb
+++ Library/Formula/xlslib.rb
@@ -2,6 +2,7 @@ class Xlslib < Formula
   desc "C++/C library to construct Excel .xls files in code"
   homepage "http://sourceforge.net/projects/xlslib"
   url "https://downloads.sourceforge.net/project/xlslib/xlslib-old/xlslib-package-2.4.0.zip"
+  mirror "https://dl.bintray.com/homebrew/mirror/xlslib-package-2.4.0.zip"
   sha256 "acc92e31294f91d8ac8adbbfc84f7a8917f7ad649a6c97b71c9f95c25887f840"
 
   bottle do
