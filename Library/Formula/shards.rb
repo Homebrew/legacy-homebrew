d97b52f7d3f4815e6b7aa5e835c9512b7af9b044
TakiuchiGenki <genki@s21g.com>
2015-09-20 07:57:28 +0900

    Added missing dependencies and test

--- Library/Formula/shards.rb
+++ Library/Formula/shards.rb
@@ -2,13 +2,17 @@ class Shards < Formula
   desc "Dependency manager for the Crystal language"
   homepage "https://github.com/ysbaddaden/shards"
   url "https://github.com/ysbaddaden/shards/archive/v0.4.0.tar.gz"
-  version "0.4.0"
   sha256 "fff0f3c6562023fd279c80becf3683d2ac922ff097dda12f544f51f4b383ae5c"
 
   depends_on "manastech/crystal/crystal-lang"
+  depends_on "libyaml"
 
   def install
     system "make"
     bin.install "bin/shards"
+  end
+
+  test do
+    assert(`#{bin/"shards"} --version`.chomp == "Shards 0.4.0 (2015-09-19)")
   end
 end
