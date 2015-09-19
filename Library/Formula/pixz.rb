95782b66611d246fae7290c4d7611bf058f05325
Justin Mayer <entroP@gmail.com>
2015-09-19 13:40:00 -0700

    pixz 1.0.4
    Closes #44149.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/pixz.rb
+++ Library/Formula/pixz.rb
@@ -1,13 +1,14 @@
 class Pixz < Formula
   desc "Parallel, indexed, xz compressor"
   homepage "https://github.com/vasi/pixz"
-  url "https://downloads.sourceforge.net/project/pixz/pixz-1.0.2.tgz"
-  sha256 "af9dac41edd6bf57953471f7fcbd4793810003bf911593ba4c84f7cccb5f74af"
+  url "https://github.com/vasi/pixz/releases/download/v1.0.4/pixz-1.0.4.tar.gz"
+  sha256 "38cabb2f065bd226c80db7984619fe1788c50da2b58558efeda3c16dcd768ce9"
 
   head "https://github.com/vasi/pixz.git"
 
   option "with-docs", "Build man pages using asciidoc and DocBook"
 
+  depends_on "pkg-config" => :build
   depends_on "libarchive"
   depends_on "xz"
 
@@ -17,17 +18,22 @@ class Pixz < Formula
   end
 
   def install
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
-    bin.install "pixz"
+    system "make", "install"
 
     if build.with? "docs"
       ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
-      system "a2x --doctype manpage --format manpage pixz.1.asciidoc"
-      man1.install "pixz.1"
+      system "a2x", "--doctype", "manpage", "--format", "manpage", "src/pixz.1.asciidoc"
+      man1.install "src/pixz.1"
     end
   end
 
   test do
-    system "#{bin}/pixz"
+    ENV["LC_ALL"] = "en_US.UTF-8"
+    testfile = testpath/"file.txt"
+    testfile.write "foo"
+    system "#{bin}/pixz", testfile, "#{testpath}/file.xz"
   end
 end
