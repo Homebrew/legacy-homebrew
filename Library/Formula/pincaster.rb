55213ea594af2b21f212c67aa38978515a24f3ca
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-19 19:48:51 +0100

    pincaster: add 0.6_1 bottle.

--- Library/Formula/pincaster.rb
+++ Library/Formula/pincaster.rb
@@ -5,6 +5,13 @@ class Pincaster < Formula
   sha256 "c88be055ecf357b50b965afe70b5fc15dff295fbe2b6f0c473cf7e4a795a9f97"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "7dc56017217e1080245e800b022a341dec65b4653fe6399dd97b1f3a9c7ba778" => :el_capitan
+    sha256 "eacb8d4301eb15b9683f407f88aee5aea363a8b3b290c1cafd015a60cd202f73" => :yosemite
+    sha256 "f1ba4aac8ac24e5aadcb2dc5e6192e50e00c267d765d5c2fdb1692b24a868a92" => :mavericks
+  end
+
   depends_on "openssl"
 
   conflicts_with "libevent", :because => "Pincaster embeds libevent"
