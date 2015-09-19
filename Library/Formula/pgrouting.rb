bc6597c16a0dc08d1b204af2652c82912b00c4c9
Tomasz Pajor <tomek@polishgeeks.com>
2015-09-19 21:35:04 +0200

    pgrouting 2.1.0

--- Library/Formula/pgrouting.rb
+++ Library/Formula/pgrouting.rb
@@ -1,8 +1,8 @@
 class Pgrouting < Formula
   desc "Provides geospatial routing for PostGIS/PostgreSQL database"
   homepage "http://www.pgrouting.org"
-  url "https://github.com/pgRouting/pgrouting/archive/v2.0.0.tar.gz"
-  sha256 "606309e8ece04abec062522374b48179c16bddb30dd4c5080b89a4298e8d163b"
+  url "https://github.com/pgRouting/pgrouting/archive/pgrouting-2.1.0.tar.gz"
+  sha256 "36a6d77ed7f682ca9af79b390f5b8194bdb42e005f19a49624d369e16755f86b"
 
   def pour_bottle?
     # Postgres extensions must live in the Postgres prefix, which precludes
@@ -10,10 +10,6 @@ class Pgrouting < Formula
     # Overcoming this will likely require changes in Postgres itself.
     false
   end
-
-  # work around function name conflict from Postgres
-  # https://github.com/pgRouting/pgrouting/issues/274
-  patch :DATA
 
   depends_on "cmake" => :build
   depends_on "boost"
@@ -28,39 +24,3 @@ class Pgrouting < Formula
     end
   end
 end
-__END__
-diff --git a/src/astar/src/astar.h b/src/astar/src/astar.h
-index d5872bb..34a0621 100644
---- a/src/astar/src/astar.h
-+++ b/src/astar/src/astar.h
-@@ -21,6 +21,7 @@
-
- #define _ASTAR_H
-
-+#include <unistd.h>
- #include "postgres.h"
- #include "dijkstra.h"
-
-diff --git a/src/dijkstra/src/dijkstra.h b/src/dijkstra/src/dijkstra.h
-index ca5bea4..09ac6f1 100644
---- a/src/dijkstra/src/dijkstra.h
-+++ b/src/dijkstra/src/dijkstra.h
-@@ -22,6 +22,7 @@
- #ifndef _DIJKSTRA_H
- #define _DIJKSTRA_H
-
-+#include <unistd.h>
- #include "postgres.h"
-
- typedef struct edge
-diff --git a/src/driving_distance/src/drivedist.h b/src/driving_distance/src/drivedist.h
-index e85bdd7..ce20b8b 100644
---- a/src/driving_distance/src/drivedist.h
-+++ b/src/driving_distance/src/drivedist.h
-@@ -22,6 +22,7 @@
- #ifndef _DRIVEDIST_H
- #define _DRIVEDIST_H
-
-+#include <unistd.h>
- #include "postgres.h"
- #include "dijkstra.h"
