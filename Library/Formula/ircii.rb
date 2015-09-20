f218cd22e589697b0e85fa02cf9db18b72ade7b0
Tomasz Pajor <tomek@polishgeeks.com>
2015-09-20 13:16:22 +0200

    ircii 20150903
    Closes #44160.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ircii.rb
+++ Library/Formula/ircii.rb
@@ -1,8 +1,8 @@
 class Ircii < Formula
   desc "IRC and ICB client"
   homepage "http://www.eterna.com.au/ircii/"
-  url "http://ircii.warped.com/ircii-20141122.tar.bz2"
-  sha256 "74c20bbc59d960038b4629c88df2d3f93a7d81a343bc0eaf51ca418ace52d622"
+  url "http://ircii.warped.com/ircii-20150903.tar.bz2"
+  sha256 "617502e17788d619510f3f5efc1217e6c9d3a55e8227ece591c56981b0901324"
 
   bottle do
     sha1 "a91d564a3a3241e6684785d0a3a7a256ee319def" => :yosemite
