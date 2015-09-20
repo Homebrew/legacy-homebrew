7e729b24a3d489a8fc791969f1d36a66aa76f230
Jianwei Han <hanjianwei@gmail.com>
2015-09-20 21:53:55 +0800

    nodenv 0.3.4
    Update nodenv to 0.3.4
    
    Closes #44176.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/nodenv.rb
+++ Library/Formula/nodenv.rb
@@ -2,8 +2,8 @@ class Nodenv < Formula
   desc "NodeJS version management"
   homepage "https://github.com/wfarr/nodenv"
   head "https://github.com/wfarr/nodenv.git"
-  url "https://github.com/wfarr/nodenv/archive/v0.3.3.tar.gz"
-  sha256 "50e18fe85e15b9c2e1a81b39279ab46413280b6c78434a63b1942984ff4d65bf"
+  url "https://github.com/wfarr/nodenv/archive/v0.3.4.tar.gz"
+  sha256 "6d3946960beed4e69596a5f0519ad4ca7606db4e4ab1b85a3bf4add06d64290b"
 
   def install
     prefix.install "bin", "libexec"
