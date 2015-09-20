3989c11824d92a7f201661539899a8ffc152fdee
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 14:19:19 +0100

    ddrescue: update 1.20 bottle.

--- Library/Formula/ddrescue.rb
+++ Library/Formula/ddrescue.rb
@@ -6,10 +6,10 @@ class Ddrescue < Formula
   sha256 "83f993b1a167865d76e92e7c1406436331a8b3b337b9868fd6ca1ec2c45460bd"
 
   bottle do
-    cellar :any
-    sha1 "e3a30bbbd40b51f4ec0d71f215406e0e511f5d04" => :mavericks
-    sha1 "9917eb5ba28f1acb4f02b52b9cab9cb8e7768dfc" => :mountain_lion
-    sha1 "6cca70194ffc701ab511665630db71cdd46425ae" => :lion
+    cellar :any_skip_relocation
+    sha256 "a98bfe8ce9183904ec2cc9eb1049b69aa1d6d6f2973ae234af30333765b81cdb" => :el_capitan
+    sha256 "7c6845909d1ce7f5444c1d4be2db26a6a1a765363bc1ebf6730c7770f3d45d76" => :yosemite
+    sha256 "5f6feecff7532f206d76d858e8a8be1bfdd9256737de9da9feb303aa0451db55" => :mavericks
   end
 
   def install
