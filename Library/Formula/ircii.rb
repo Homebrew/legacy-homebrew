98731a001e7bd3d6171981ea4c7fbbe3e955fa51
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-20 14:16:09 +0100

    ircii: update 20150903 bottle.

--- Library/Formula/ircii.rb
+++ Library/Formula/ircii.rb
@@ -5,9 +5,9 @@ class Ircii < Formula
   sha256 "617502e17788d619510f3f5efc1217e6c9d3a55e8227ece591c56981b0901324"
 
   bottle do
-    sha1 "a91d564a3a3241e6684785d0a3a7a256ee319def" => :yosemite
-    sha1 "4691d4bbbe491b729af842870773b6d899a95433" => :mavericks
-    sha1 "826c088f54ddcf68afabc989c1915beb3ee0265d" => :mountain_lion
+    sha256 "62e2eddce9afdcb017823462fee587f140a195c0b98bfdf328e1a4112331ccdb" => :el_capitan
+    sha256 "c55179c8ef451aac157747a8cd17f7d0e90913226c05140e222e72f2339d3907" => :yosemite
+    sha256 "64550bbd1cce33f9c63e5b1a68eca19fbae4ff3709ce77b594ce47b57621d710" => :mavericks
   end
 
   depends_on "openssl"
