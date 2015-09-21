5535163c296e59b66c8f16dde79a30c496753211
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 14:04:06 +0100

    usbmuxd: update 1.0.10 bottle.

--- Library/Formula/usbmuxd.rb
+++ Library/Formula/usbmuxd.rb
@@ -7,6 +7,7 @@ class Usbmuxd < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "187e9dd2acbe0e80a92d72a1e0a2f61b37ff04d1defe22a88e5e26af4ca29e97" => :el_capitan
     sha1 "f9cb14678e5e2dbee3f189641d8d390bee938fd3" => :yosemite
     sha1 "1ee806476cda65d5904431815f949dd6074e7a7e" => :mavericks
     sha1 "09014c177d40d10c0a10b5540c92023316382a5f" => :mountain_lion
