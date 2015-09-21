005a79b61b3a08e97d52f234e48ccd47a5307566
rpau <rpau@ac.upc.edu>
2015-09-21 08:58:39 +0200

    updating description

--- Library/Formula/walkmod.rb
+++ Library/Formula/walkmod.rb
@@ -1,5 +1,5 @@
 class Walkmod < Formula
-  desc "Java-based project to apply code conventions"
+  desc "Java-based project to apply and share code conventions"
   homepage "http://www.walkmod.com"
   url "https://bitbucket.org/rpau/walkmod/downloads/walkmod-1.3.0-installer.zip"
   sha256 "01ea47c99f7d7bffeb0cd0d960fdab4f09a7d51a1e0979c6b4ff71d0aeb049fd"
