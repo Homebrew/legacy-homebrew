c729dda8c012ee81ffb8d7a283b13db09f8fb241
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 02:17:15 +0100

    pixz: add 1.0.4 bottle.

--- Library/Formula/pixz.rb
+++ Library/Formula/pixz.rb
@@ -6,6 +6,13 @@ class Pixz < Formula
 
   head "https://github.com/vasi/pixz.git"
 
+  bottle do
+    cellar :any
+    sha256 "67f3a69e7d2443d5f1956af5fddf85d29ff4a515b01af3ffa24ace710364fbe1" => :el_capitan
+    sha256 "130d33c9ee2fc8b5e02d36aab52314b4a1a323b4e5b79d9f55bb6853ceaf9e53" => :yosemite
+    sha256 "4e8d29305549eb2e836bdef4d2075a2864f1333bf1bc763c0d886acee1e12cf1" => :mavericks
+  end
+
   option "with-docs", "Build man pages using asciidoc and DocBook"
 
   depends_on "pkg-config" => :build
