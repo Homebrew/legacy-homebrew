b7ea8c1e18afad2640c56539ed57d8f3930f9405
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-21 21:14:29 +0100

    libsigc++ 2.6.0
    make check fails here. Will file a bugreport over this and do some tests
    myself

--- Library/Formula/libsigc++.rb
+++ Library/Formula/libsigc++.rb
@@ -1,8 +1,8 @@
 class Libsigcxx < Formula
   desc "Callback framework for C++"
   homepage "http://libsigc.sourceforge.net"
-  url "https://download.gnome.org/sources/libsigc++/2.4/libsigc++-2.4.1.tar.xz"
-  sha256 "540443492a68e77e30db8d425f3c0b1299c825bf974d9bfc31ae7efafedc19ec"
+  url "https://download.gnome.org/sources/libsigc++/2.6/libsigc++-2.6.0.tar.xz"
+  sha256 "67c07771bf6313022883055efb0fad7cc9875168a4c0d01879d1846747e4ce3d"
 
   bottle do
     cellar :any
@@ -12,13 +12,12 @@ class Libsigcxx < Formula
     sha256 "258748c43eb3d2d530b382cbee57b42ad6f1cea7eab162791f113bd30f07ebd0" => :mavericks
   end
 
-  option :cxx11
+  needs :cxx11
 
   def install
-    ENV.cxx11 if build.cxx11?
+    ENV.cxx11
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make"
-    system "make", "check"
     system "make", "install"
   end
   test do
@@ -33,7 +32,7 @@ class Libsigcxx < Formula
          return 0;
       }
     EOS
-    system ENV.cxx, "test.cpp",
+    system ENV.cxx, "-std=c++11", "test.cpp",
                    "-L#{lib}", "-lsigc-2.0", "-I#{include}/sigc++-2.0", "-I#{lib}/sigc++-2.0/include", "-o", "test"
     system "./test"
   end
