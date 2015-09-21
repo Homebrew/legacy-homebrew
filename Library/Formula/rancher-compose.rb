3076c637aba85806fa73583816f3baab7c49f75a
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2015-09-21 09:43:16 +0200

    rancher-compose 0.4.0 (new formula)
    Closes #44207.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ Library/Formula/rancher-compose.rb
@@ -0,0 +1,19 @@
+class RancherCompose < Formula
+  desc "Docker compose compatible client to deploy to Rancher"
+  homepage "https://github.com/rancher/rancher-compose"
+  url "https://github.com/rancher/rancher-compose/archive/v0.4.0.tar.gz"
+  sha256 "07192c7ef554fadc1ef62c32ee942526f013109dd5bacb04642b22fd6f1aeec7"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = "#{buildpath}:#{buildpath}/Godeps/_workspace"
+    mkdir_p "#{buildpath}/src/github.com/rancher"
+    ln_s buildpath, "#{buildpath}/src/github.com/rancher/rancher-compose"
+    system "go", "build", "-ldflags", "-w -X github.com/rancher/rancher-compose/version.VERSION #{version}", "-o", "#{bin}/rancher-compose"
+  end
+
+  test do
+    system "#{bin}/rancher-compose", "help"
+  end
+end
