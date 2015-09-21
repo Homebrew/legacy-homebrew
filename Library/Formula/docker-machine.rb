8ab6319dfcc14725784311530bd5dca835202336
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 14:04:04 +0100

    docker-machine: update 0.4.1 bottle.

--- Library/Formula/docker-machine.rb
+++ Library/Formula/docker-machine.rb
@@ -8,7 +8,8 @@ class DockerMachine < Formula
   head "https://github.com/docker/machine.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "1999548d84ddade819c21e6b6bc89f4590d20efed575a8996e9e87398afd3458" => :el_capitan
     sha256 "ec656857e23724204cb94cf6eea10feb887b9db0ae6027c751b95ae62e77d315" => :yosemite
     sha256 "4890545e0d50d8920394803313b95af81368414aa3a27f3fa16475e4fe9b69a7" => :mavericks
     sha256 "f282138ad3860b2b05c86f1a070c6fe3722220ea358c61ece16e8098b0798430" => :mountain_lion
