e3636576c474e8302268494f63047f413c537317
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-21 15:19:22 +0100

    popt: update 1.16 bottle.

--- Library/Formula/popt.rb
+++ Library/Formula/popt.rb
@@ -6,6 +6,7 @@ class Popt < Formula
 
   bottle do
     revision 1
+    sha256 "60a7f19e8fecafd92a5beb7d6438efac915e8f3afe3d83575fb64bb4a6190aab" => :el_capitan
     sha1 "20ebf6ad6a0e618c6e14249179ebfaa49ceea1a0" => :yosemite
     sha1 "ffa33727245492f9583a7e6905bbeef7454b96c8" => :mavericks
     sha1 "d4736bc9459f25b0d4c267d364798e6614fbbbda" => :mountain_lion
