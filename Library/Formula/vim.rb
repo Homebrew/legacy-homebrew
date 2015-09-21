638c6b64ce4f9a03c16b8c7bfa5c8d52edc0cdfd
Drew Neil <andrew.jr.neil@gmail.com>
2015-09-21 10:12:09 +0100

    vim 7.4.873
    vim: Update to version 7.4.873
    
    Closes #44208.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi \"workalike\" with many additional features"
   homepage "http://www.vim.org/"
   # *** Vim should be updated no more than once every 7 days ***
-  url "https://github.com/vim/vim/archive/v7.4.865.tar.gz"
-  sha256 "b3c92b2bc6ba7933b38fdf1879f8d7014efeb7b94aead6ef82f7c4a954fb75cf"
+  url "https://github.com/vim/vim/archive/v7.4.873.tar.gz"
+  sha256 "06f2f8ad1ad888532fe84db9ef89d416fd5576dcf8a138505863857996f871d8"
   head "https://github.com/vim/vim.git"
 
   # We only have special support for finding depends_on :python, but not yet for
