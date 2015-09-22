cc0381eaea17698d405699eef4707a24ee1513d8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 23:39:03 +0100

    fzf: update 0.10.5 bottle.

--- Library/Formula/fzf.rb
+++ Library/Formula/fzf.rb
@@ -9,6 +9,7 @@ class Fzf < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "4ccf966e9cddf4296fa48898c19683b23b5c1d49922300c34e868cb0ef6e724c" => :el_capitan
     sha256 "7318f09c5170600833287540a812a7d4a86874462dbb01b6a15c59f5a02d0bd3" => :yosemite
     sha256 "e768063541ea6a55f31ba3b3a5a15721de4f81cc2f616a1df693e71a41b11336" => :mavericks
     sha256 "6b9f36d48d385c327ccce01060533864165d6ee6f7da6762fbbbb90a314f0799" => :mountain_lion
