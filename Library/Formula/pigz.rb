cd89225482357ecfd2bd262df8ae23621f8b24f1
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 23:39:04 +0100

    pigz: update 2.3.3 bottle.

--- Library/Formula/pigz.rb
+++ Library/Formula/pigz.rb
@@ -5,7 +5,8 @@ class Pigz < Formula
   sha256 "4e8b67b432ce7907575a549f3e1cac4709781ba0f6b48afea9f59369846b509c"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "b69db2b8ee2df836ab5b267cd574929cbd5091be64e31bf5784521ff3b55f1be" => :el_capitan
     sha1 "90c40dbdaa9c9722496ef7e2f73da037d8996db1" => :yosemite
     sha1 "5a641215041ea36eb15942ceb4dfcae1cd41a0a4" => :mavericks
     sha1 "e4a79a31ddc5a540b7fbb74bb64837522c932ee9" => :mountain_lion
