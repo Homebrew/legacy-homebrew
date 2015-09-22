56deb881926828d7a0b44387bd2a98d13a8f72b9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 17:27:12 +0100

    fortune: update 9708 bottle.

--- Library/Formula/fortune.rb
+++ Library/Formula/fortune.rb
@@ -5,6 +5,7 @@ class Fortune < Formula
   sha256 "1a98a6fd42ef23c8aec9e4a368afb40b6b0ddfb67b5b383ad82a7b78d8e0602a"
 
   bottle do
+    sha256 "6ccf821f74aca9c1d6f79573b4243911f81d8a1ddfbc1082e27ee7e7c1178549" => :el_capitan
     sha256 "bfae1a94a0de8d9d53f28b5cfdea3b04190420dde0b83c862d6ef536c03d59ef" => :yosemite
     sha256 "90e758b8875a309116e89f475d7c88da5e7e1417601705eb45abf20a5be11a8a" => :mavericks
     sha256 "25958474483a88a12bf8585f21864d190179547271527af8c292bd7ecf6d5677" => :mountain_lion
