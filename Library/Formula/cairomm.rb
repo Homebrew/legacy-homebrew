a75b4f37f7c976d2f39c20df04c183233df4261a
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-22 13:08:49 +0100

    cairomm 1.12.0

--- Library/Formula/cairomm.rb
+++ Library/Formula/cairomm.rb
@@ -1,9 +1,8 @@
 class Cairomm < Formula
   desc "Vector graphics library with cross-device output support"
   homepage "http://cairographics.org/cairomm/"
-  url "http://cairographics.org/releases/cairomm-1.11.2.tar.gz"
-  sha256 "ccf677098c1e08e189add0bd146f78498109f202575491a82f1815b6bc28008d"
-  revision 1
+  url "https://download.gnome.org/sources/cairomm/1.12/cairomm-1.12.0.tar.xz"
+  sha256 "a54ada8394a86182525c0762e6f50db6b9212a2109280d13ec6a0b29bfd1afe6"
 
   bottle do
     cellar :any
@@ -13,20 +12,16 @@ class Cairomm < Formula
     sha256 "7ecc1bc6775f3286561b178012109d249935ad8d56fb5996d43071ac897ffb2b" => :mavericks
   end
 
-  option :cxx11
+  needs :cxx11
 
   depends_on "pkg-config" => :build
-  if build.cxx11?
-    depends_on "libsigc++" => "c++11"
-  else
-    depends_on "libsigc++"
-  end
+  depends_on "libsigc++"
 
   depends_on "libpng"
   depends_on "cairo"
 
   def install
-    ENV.cxx11 if build.cxx11?
+    ENV.cxx11
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
@@ -72,7 +67,7 @@ class Cairomm < Formula
       -lcairomm-1.0
       -lsigc-2.0
     ]
-    system ENV.cxx, "test.cpp", "-o", "test", *flags
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
     system "./test"
   end
 end
