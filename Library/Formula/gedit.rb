0f7a7f4e187ce659170cd0c7a697256d90f8eeeb
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-22 12:39:47 +0100

    gedit 3.18.0

--- Library/Formula/gedit.rb
+++ Library/Formula/gedit.rb
@@ -1,8 +1,8 @@
 class Gedit < Formula
   desc "The GNOME text editor"
   homepage "https://wiki.gnome.org/Apps/Gedit"
-  url "https://download.gnome.org/sources/gedit/3.16/gedit-3.16.3.tar.xz"
-  sha256 "c28df44a29ee5707441f89e94b513079d709111afec4bd4b079a1dff0242cdb2"
+  url "https://download.gnome.org/sources/gedit/3.18/gedit-3.18.0.tar.xz"
+  sha256 "9abd4f1478385f8b6c983298206f4ab1a25c682b9c87fb00d759b7db5b949533"
 
   bottle do
     sha256 "a181e2b41c720e0022adfe06a4125fac8e02f4dc096c180d82f65de6049b4ec1" => :el_capitan
@@ -24,10 +24,6 @@ class Gedit < Formula
   depends_on "gtksourceview3"
   depends_on "gsettings-desktop-schemas"
   depends_on "gnome-icon-theme"
-
-  # ensures that gtk-mac-integration has been linked against gtk+3
-  # filed upstream as https://bugzilla.gnome.org/show_bug.cgi?id=751431
-  patch :DATA
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -129,58 +125,3 @@ class Gedit < Formula
     system "./test"
   end
 end
-
-__END__
-diff --git a/configure b/configure
-index 2a16d57..9ad2e56 100755
---- a/configure
-+++ b/configure
-@@ -13107,12 +13107,12 @@ if test -n "$GTK_MAC_CFLAGS"; then
-     pkg_cv_GTK_MAC_CFLAGS="$GTK_MAC_CFLAGS"
-  elif test -n "$PKG_CONFIG"; then
-     if test -n "$PKG_CONFIG" && \
--    { { $as_echo "$as_me:${as_lineno-$LINENO}: \$PKG_CONFIG --exists --print-errors \"gtk-mac-integration\""; } >&5
--  ($PKG_CONFIG --exists --print-errors "gtk-mac-integration") 2>&5
-+    { { $as_echo "$as_me:${as_lineno-$LINENO}: \$PKG_CONFIG --exists --print-errors \"gtk-mac-integration-gtk3\""; } >&5
-+  ($PKG_CONFIG --exists --print-errors "gtk-mac-integration-gtk3") 2>&5
-   ac_status=$?
-   $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
-   test $ac_status = 0; }; then
--  pkg_cv_GTK_MAC_CFLAGS=`$PKG_CONFIG --cflags "gtk-mac-integration" 2>/dev/null`
-+  pkg_cv_GTK_MAC_CFLAGS=`$PKG_CONFIG --cflags "gtk-mac-integration-gtk3" 2>/dev/null`
-		      test "x$?" != "x0" && pkg_failed=yes
- else
-   pkg_failed=yes
-@@ -13124,12 +13124,12 @@ if test -n "$GTK_MAC_LIBS"; then
-     pkg_cv_GTK_MAC_LIBS="$GTK_MAC_LIBS"
-  elif test -n "$PKG_CONFIG"; then
-     if test -n "$PKG_CONFIG" && \
--    { { $as_echo "$as_me:${as_lineno-$LINENO}: \$PKG_CONFIG --exists --print-errors \"gtk-mac-integration\""; } >&5
--  ($PKG_CONFIG --exists --print-errors "gtk-mac-integration") 2>&5
-+    { { $as_echo "$as_me:${as_lineno-$LINENO}: \$PKG_CONFIG --exists --print-errors \"gtk-mac-integrationi-gtk3\""; } >&5
-+  ($PKG_CONFIG --exists --print-errors "gtk-mac-integration-gtk3") 2>&5
-   ac_status=$?
-   $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
-   test $ac_status = 0; }; then
--  pkg_cv_GTK_MAC_LIBS=`$PKG_CONFIG --libs "gtk-mac-integration" 2>/dev/null`
-+  pkg_cv_GTK_MAC_LIBS=`$PKG_CONFIG --libs "gtk-mac-integration-gtk3" 2>/dev/null`
-		      test "x$?" != "x0" && pkg_failed=yes
- else
-   pkg_failed=yes
-@@ -13150,14 +13150,14 @@ else
-         _pkg_short_errors_supported=no
- fi
-         if test $_pkg_short_errors_supported = yes; then
--	        GTK_MAC_PKG_ERRORS=`$PKG_CONFIG --short-errors --print-errors --cflags --libs "gtk-mac-integration" 2>&1`
-+	        GTK_MAC_PKG_ERRORS=`$PKG_CONFIG --short-errors --print-errors --cflags --libs "gtk-mac-integration-gtk3" 2>&1`
-         else
--	        GTK_MAC_PKG_ERRORS=`$PKG_CONFIG --print-errors --cflags --libs "gtk-mac-integration" 2>&1`
-+	        GTK_MAC_PKG_ERRORS=`$PKG_CONFIG --print-errors --cflags --libs "gtk-mac-integration-gtk3" 2>&1`
-         fi
-	# Put the nasty error message in config.log where it belongs
-	echo "$GTK_MAC_PKG_ERRORS" >&5
-
--	as_fn_error $? "Package requirements (gtk-mac-integration) were not met:
-+	as_fn_error $? "Package requirements (gtk-mac-integration-gtk3) were not met:
-
- $GTK_MAC_PKG_ERRORS
