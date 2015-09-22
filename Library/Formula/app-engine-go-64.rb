d36a3c302b05b17a94abcfc7e0de1cb149df792e
Michael Hendricks <michael@ndrix.org>
2015-09-22 09:12:05 -0600

    app-engine-go-64 1.9.26
    Closes #44248.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/app-engine-go-64.rb
+++ Library/Formula/app-engine-go-64.rb
@@ -1,8 +1,8 @@
 class AppEngineGo64 < Formula
   desc "Google App Engine SDK for Go!"
   homepage "https://cloud.google.com/appengine/docs/go/"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.24.zip"
-  sha256 "6a072ef4cdb0cc42d0b4a32494cbe1500d702208e345cef196b5878917df32f8"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.26.zip"
+  sha256 "ae3648c4df3a20acad4207493586a90214a50b6ca1edb887ae4ca062bbd7a7e5"
 
   conflicts_with "go-app-engine-32", :because => "multiple conflicting files"
   conflicts_with "google-app-engine", :because => "multiple conflicting files"
