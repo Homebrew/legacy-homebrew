ae07687c4a7a1c9b3a0c8b2e9c30b4d74cef1371
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-22 13:05:33 +0100

    atkmm 2.24.1
    test added

--- Library/Formula/atkmm.rb
+++ Library/Formula/atkmm.rb
@@ -1,8 +1,8 @@
 class Atkmm < Formula
   desc "Official C++ interface for the ATK accessibility toolkit library"
   homepage "http://www.gtkmm.org"
-  url "https://download.gnome.org/sources/atkmm/2.22/atkmm-2.22.7.tar.xz"
-  sha256 "bfbf846b409b4c5eb3a52fa32a13d86936021969406b3dcafd4dd05abd70f91b"
+  url "https://download.gnome.org/sources/atkmm/2.24/atkmm-2.24.1.tar.xz"
+  sha256 "26c41d8da37d04eef9f219c6ce87d94852e1cacaad823050e520e1c08a36ed23"
 
   bottle do
     sha1 "6c47047d111b4b950e1cb15425365b13b11f6a1b" => :mavericks
@@ -14,8 +14,55 @@ class Atkmm < Formula
   depends_on "atk"
   depends_on "glibmm"
 
+  needs :cxx11
+
   def install
+    ENV.cxx11
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <atkmm/init.h>
+
+      int main(int argc, char *argv[])
+      {
+         Atk::init();
+         return 0;
+      }
+    EOS
+    atk = Formula["atk"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    glibmm = Formula["glibmm"]
+    libsigcxx = Formula["libsigc++"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{glibmm.opt_include}/glibmm-2.4
+      -I#{glibmm.opt_lib}/glibmm-2.4/include
+      -I#{include}/atkmm-1.6
+      -I#{libsigcxx.opt_include}/sigc++-2.0
+      -I#{libsigcxx.opt_lib}/sigc++-2.0/include
+      -L#{atk.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{glibmm.opt_lib}
+      -L#{libsigcxx.opt_lib}
+      -L#{lib}
+      -latk-1.0
+      -latkmm-1.6
+      -lglib-2.0
+      -lglibmm-2.4
+      -lgobject-2.0
+      -lintl
+      -lsigc-2.0
+    ]
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test", *flags
+    system "./test"
   end
 end
