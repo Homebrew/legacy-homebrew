522d67631bd547e654f00c2d0c5aa1824ab2606d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 17:27:12 +0100

    gpp: update 2.24 bottle.

--- Library/Formula/gpp.rb
+++ Library/Formula/gpp.rb
@@ -5,8 +5,9 @@ class Gpp < Formula
   sha256 "9bc2db874ab315ddd1c03daba6687f5046c70fb2207abdcbd55d0e9ad7d0f6bc"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     revision 1
+    sha256 "6925eb92be766ed9fe61a9a98dc7bc3c22793079abf63f462cb7001017cac28c" => :el_capitan
     sha1 "481357229fc529fbc72fd129e5fce856db2920c1" => :yosemite
     sha1 "61bc9c993cdb79a20b81351e77c6d0b92827910e" => :mavericks
     sha1 "6cce4a597e3c424471172be048a556e03a1afafc" => :mountain_lion
