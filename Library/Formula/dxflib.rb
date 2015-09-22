6efe0d84a8a2d56d6ac6e5a85d38143204aef8fe
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 12:50:24 +0100

    dxflib: update 2.5.0.0-1 bottle.

--- Library/Formula/dxflib.rb
+++ Library/Formula/dxflib.rb
@@ -5,8 +5,9 @@ class Dxflib < Formula
   sha256 "20ad9991eec6b0f7a3cc7c500c044481a32110cdc01b65efa7b20d5ff9caefa9"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     revision 1
+    sha256 "ba2cbde3c7ba4183e5d44325791632bdb3aa3570bfbdd4b731596144cddd4881" => :el_capitan
     sha1 "58c0add8f6f8c5211ca4e59bc185468531431765" => :yosemite
     sha1 "2184e3835e92a57a52854ad82f966a9e186c30e0" => :mavericks
     sha1 "238319c0040cde6d76fa085f086adef65c3c15f0" => :mountain_lion
