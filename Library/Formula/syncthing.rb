533f564a2532a83aa5ffac5265ccd284ea34bb74
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 17:27:15 +0100

    syncthing: update 0.11.25 bottle.

--- Library/Formula/syncthing.rb
+++ Library/Formula/syncthing.rb
@@ -8,6 +8,7 @@ class Syncthing < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a5cc2c79858e42bb48e4fb84aefe9464664488dedd8af6cacf505ca213bbfdbb" => :el_capitan
     sha256 "2846f015a7fdb8c3943889241a2f677cb2437d2a9de6bb3392f0828e86c0a065" => :yosemite
     sha256 "d3328742f1114f7b3c1264908bc280d98a5de06295802046bf235828f12d20e3" => :mavericks
     sha256 "f14b91ad2d0c2b5027221e6d09ae31700a18b04766a52177ffe1bb002824613e" => :mountain_lion
