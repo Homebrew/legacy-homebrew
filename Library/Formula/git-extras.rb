7d2d77b799d19c6e7cc946c560f2a0344f405397
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 12:50:25 +0100

    git-extras: update 3.0.0 bottle.

--- Library/Formula/git-extras.rb
+++ Library/Formula/git-extras.rb
@@ -6,7 +6,8 @@ class GitExtras < Formula
   head "https://github.com/tj/git-extras.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "518782b4a797275a9ea54a01956f15fa166bdd68398c26d5a890948f4306dc98" => :el_capitan
     sha256 "3eabd97b6e574274665ed178b72076648ed03da53c7dd29425835331592c3378" => :yosemite
     sha256 "14471fe41a1162813ed803fe9edc91aca493d7529340e8f15e2cea9aa269d586" => :mavericks
     sha256 "7655c7e7f926d58b28a8ef503b406ff2c0f6fb2102a2915cb7d05b36b26f9d9b" => :mountain_lion
