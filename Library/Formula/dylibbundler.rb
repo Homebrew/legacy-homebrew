9a1cacb414fe365baf60f69ae00b177acaee8674
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 12:50:25 +0100

    dylibbundler: update 0.4.4 bottle.

--- Library/Formula/dylibbundler.rb
+++ Library/Formula/dylibbundler.rb
@@ -6,8 +6,9 @@ class Dylibbundler < Formula
   head "https://github.com/auriamg/macdylibbundler.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     revision 1
+    sha256 "5dff018e62a9787871e45f4ae976358cfc3f7f85972a0aa0d4e039f97d4b8e0f" => :el_capitan
     sha1 "7796af4a80af52b2cda87f9a6af74919811cddb4" => :yosemite
     sha1 "af56868cede7747f2559320659654e017cb0144a" => :mavericks
     sha1 "81c315d6fbaea3b56ba78e967953f3f8a11d08ec" => :mountain_lion
