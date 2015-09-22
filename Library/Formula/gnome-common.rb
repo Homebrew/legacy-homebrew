74f4422a1b64d16a9b2941d347c1ce8bc43c6989
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-22 10:13:24 +0100

    gnome-common 3.18.0

--- Library/Formula/gnome-common.rb
+++ Library/Formula/gnome-common.rb
@@ -1,8 +1,8 @@
 class GnomeCommon < Formula
   desc "Core files for GNOME"
   homepage "https://git.gnome.org/browse/gnome-common/"
-  url "https://download.gnome.org/sources/gnome-common/3.14/gnome-common-3.14.0.tar.xz"
-  sha256 "4c00242f781bb441289f49dd80ed1d895d84de0c94bfc2c6818a104c9e39262c"
+  url "https://download.gnome.org/sources/gnome-common/3.18/gnome-common-3.18.0.tar.xz"
+  sha256 "22569e370ae755e04527b76328befc4c73b62bfd4a572499fde116b8318af8cf"
 
   def install
     system "./configure", "--prefix=#{prefix}"
