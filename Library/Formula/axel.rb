40b28d9ed1e7bce8fffc0cbe8e273ba0774c92a8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 23:39:02 +0100

    axel: update 2.4 bottle.

--- Library/Formula/axel.rb
+++ Library/Formula/axel.rb
@@ -6,6 +6,7 @@ class Axel < Formula
   sha256 "359a57ab4e354bcb6075430d977c59d33eb3e2f1415a811948fa8ae657ca8036"
 
   bottle do
+    sha256 "11e0633be4ae1d70e295dfc7166ce870efdfcb03cb72c6b3c9475459cf385725" => :el_capitan
     sha1 "419b8e2f1f17909363fb48307eb74f9672ceb25e" => :yosemite
     sha1 "0307ac6796e0b7e5fafac6c6085d0ba5230e50e5" => :mavericks
     sha1 "22c1e32858c443a04eaec96617773e99e036bf0b" => :mountain_lion
