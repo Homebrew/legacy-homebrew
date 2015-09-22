40b3c47ca9cff8284a9520f4f76c6c2e881c2177
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-22 12:50:25 +0100

    gnome-icon-theme: update 3.16.2.1 bottle.

--- Library/Formula/gnome-icon-theme.rb
+++ Library/Formula/gnome-icon-theme.rb
@@ -5,7 +5,8 @@ class GnomeIconTheme < Formula
   sha256 "b4556dfbf555d4fac12d4d5c12f7519de0d43ec42a1b649611439a50bf7acb96"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "f2044306ce437167a3a35b955e4fdd5231a1a264cc6d76d7633c7074d8cb509a" => :el_capitan
     sha256 "280b69b14c0c526ca652ce6246c64e4a9c3446836f50d8e8bb050fe9cd69489a" => :yosemite
     sha256 "91d0523ab7ba7d8443bcd031530a70842a7d6a708576b65828dc991cbec83042" => :mavericks
     sha256 "0860138b37323829ea795e18f6bc46a23d5da03369bfa197796e3bd2347add5e" => :mountain_lion
