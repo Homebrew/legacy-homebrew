2dc9260ff6741e094107eee6769243bf5abd524e
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-23 09:20:20 +0100

    ghex 3.18.0

--- Library/Formula/ghex.rb
+++ Library/Formula/ghex.rb
@@ -1,8 +1,8 @@
 class Ghex < Formula
   desc "GNOME hex editor"
   homepage "https://wiki.gnome.org/Apps/Ghex"
-  url "https://download.gnome.org/sources/ghex/3.10/ghex-3.10.1.tar.xz"
-  sha256 "34b66cb5c84410c420df72f229d25aee5979e58048a246ed719b046f0c241132"
+  url "https://download.gnome.org/sources/ghex/3.18/ghex-3.18.0.tar.xz"
+  sha256 "c5b1eb50a8dd1334880b37617871498b778ea137f79bb43894ec68e4f63dc925"
 
   bottle do
     sha256 "e52e0de4433e01d625f49a5bb871b132b395fb57b8dfbcddaf01e97adc9f08e6" => :yosemite
@@ -17,13 +17,8 @@ class Ghex < Formula
   depends_on :python => :build if MacOS.version <= :snow_leopard
   depends_on "gtk+3"
   depends_on "hicolor-icon-theme"
-  depends_on "gnome-themes-standard" => :optional
 
   def install
-    # forces use of gtk3-update-icon-cache instead of gtk-update-icon-cache. No bugreport should
-    # be filed for this since it only occurs because Homebrew renames gtk+3's gtk-update-icon-cache
-    # to gtk3-update-icon-cache in order to avoid a collision between gtk+ and gtk+3.
-    inreplace "icons/Makefile.in", "gtk-update-icon-cache", "gtk3-update-icon-cache"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
@@ -36,10 +31,6 @@ class Ghex < Formula
   def post_install
     system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
     system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
-    # HighContrast is provided by gnome-themes-standard
-    if File.file?("#{HOMEBREW_PREFIX}/share/icons/HighContrast/.icon-theme.cache")
-      system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/HighContrast"
-    end
   end
 
   test do
