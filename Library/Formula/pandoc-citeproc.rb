409d8864ed63cfbb099f9c6d59e79b88a588e738
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 21:36:14 +0100

    pandoc-citeproc: update 0.7.3 bottle.

--- Library/Formula/pandoc-citeproc.rb
+++ Library/Formula/pandoc-citeproc.rb
@@ -9,6 +9,7 @@ class PandocCiteproc < Formula
   sha256 "72fc81d962812d037bb78c6d38d602ca2c6895f0436c8be94cb71600ed288e68"
 
   bottle do
+    sha256 "fce1c154458a6afdc3635754867b94195eea6bc95df7ce5b5581623ccaedf59b" => :el_capitan
     sha256 "216ad46562f7ee3ae5a0e19b855b3f8c27c8d2907beedf9dd41d5634179ab9a4" => :yosemite
     sha256 "0f5cc2caa5ba5c9f687d057a3dfe462e6e01623dc42d556148e8e6e23332622d" => :mavericks
     sha256 "40e380d4a8c5e63a81f00358d8cffa743381e4b825a8e925a63b66d0da232154" => :mountain_lion
