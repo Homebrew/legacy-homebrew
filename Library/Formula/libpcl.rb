bfebc34986184b6a26eae13d567632138dc1e987
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 17:30:11 +0100

    libpcl: update 1.12 bottle.

--- Library/Formula/libpcl.rb
+++ Library/Formula/libpcl.rb
@@ -7,6 +7,7 @@ class Libpcl < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "1975baf018352fd1f1ca88bd39fc02db384e2f6be4017976184dda3365c60608" => :el_capitan
     sha1 "f765f414f926e08424a150ef9d6ed0c781c747a5" => :yosemite
     sha1 "659570156b38f819a880f0cb4e8650129b4c6d29" => :mavericks
     sha1 "144245dc5c42c66e42144a7ccfa648fc96550752" => :mountain_lion
