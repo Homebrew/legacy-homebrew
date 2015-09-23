1ebf6ce52b6e3d450c5f0982d812367511c3455b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 12:50:22 +0100

    python3: update 3.5.0 bottle.

--- Library/Formula/python3.rb
+++ Library/Formula/python3.rb
@@ -5,10 +5,10 @@ class Python3 < Formula
   sha256 "d6d7aa1634a5eeeca6ed4fca266982a04f84bd8f3945a9179e20b24ad2e2be91"
 
   bottle do
-    revision 2
-    sha256 "1efc591e4eb4664cc9249234fd4ad97a8e0598f0dc3bc0c5e848caf675d7b6ee" => :el_capitan
-    sha256 "a9d801291940a8aed07edfb8f96218aebcea1ac6da4dc085a34a55839cde2c34" => :yosemite
-    sha256 "4ea81ec1ef16b0a5fb5cd6ae8f76c94974d51dcc5ff30e76617fab5fce314ac8" => :mavericks
+    revision 3
+    sha256 "5a955a6800431f4b38f7dcc4910deaf3c9e3a47f68b5a4b2117527c57290c4dd" => :el_capitan
+    sha256 "956b2bfe8289da7584089768e6143f852b9586ffd552d9b1e4c5e116f7c52587" => :yosemite
+    sha256 "98e2f771b7bc6e33f5eae068788b9197619eb037f043a292790482aa82577528" => :mavericks
   end
 
   head "https://hg.python.org/cpython", :using => :hg
