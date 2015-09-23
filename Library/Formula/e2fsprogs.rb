8cd8592b7e34d3b9d1be3aa0601977dc6b44c7aa
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 13:10:37 +0100

    e2fsprogs: update 1.42.12 bottle.

--- Library/Formula/e2fsprogs.rb
+++ Library/Formula/e2fsprogs.rb
@@ -8,6 +8,7 @@ class E2fsprogs < Formula
   head "https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git"
 
   bottle do
+    sha256 "2d90841860a9a2f2efc8769a7e2a8d8638d29b4bd5674f3dae277dbd5a53025b" => :el_capitan
     sha256 "d2e1bcb92be39ebf971032b385fe5ffc6b0d69ec5df23fd0f4c5fb739f7f031e" => :yosemite
     sha256 "e77469345bab82888029836b5b221cfd07ac9a3e3dea79bb0893762312d31094" => :mavericks
     sha256 "fe5542c41eb4a541ef533d07a086fb3e6c9cdc51157ae9c32f62ac049842a74f" => :mountain_lion
