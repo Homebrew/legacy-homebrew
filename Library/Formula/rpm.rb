bbce78c672cd9b097c974950e06815b21a3e66b2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 14:36:22 +0100

    rpm: update 5.4.15 bottle.

--- Library/Formula/rpm.rb
+++ Library/Formula/rpm.rb
@@ -20,6 +20,7 @@ class Rpm < Formula
   sha256 "d4ae5e9ed5df8ab9931b660f491418d20ab5c4d72eb17ed9055b80b71ef6c4ee"
 
   bottle do
+    sha256 "29c05e064c80738733182e6688a82cef3a2c933b40acbeb43d3a842693ca91f4" => :el_capitan
     sha256 "ac5e32d13f8d61c4a7bfae758a98f4be00622e02a2db6e64430429a0ed17cc30" => :yosemite
     sha256 "26cb3e750a1333f5c66fd2c125f34a546ed1a200eeee7c950a0616ea7699453b" => :mavericks
     sha256 "67743955785cdb2f2c532d0a9cdd8c05adab1da9c10c9a2f5af18d53f3abaea5" => :mountain_lion
