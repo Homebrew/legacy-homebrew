42b7c5c0f3f2a293d0cc10203bc32ec0380b3065
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 17:30:11 +0100

    goaccess: update 0.9.3 bottle.

--- Library/Formula/goaccess.rb
+++ Library/Formula/goaccess.rb
@@ -5,6 +5,7 @@ class Goaccess < Formula
   sha256 "aa5819d47d4d554d13b5d4d79524b907ade685accf8c89d399fba0e2a318bc46"
 
   bottle do
+    sha256 "b794786dbfd4f88154abe3d06ae166d82bb57729a891dc9c8e438f17c9fcd7ff" => :el_capitan
     sha256 "a8f198a65bbc1f78374f4875de7ae292bc4c6468eafc30e1548733cc3c8d4d72" => :yosemite
     sha256 "c3b9794ea9b90361e81630e9550a3ac6d7a3e413e5c61d7dc00fd3f942b1c802" => :mavericks
     sha256 "c4da90f9f07798cc89c94eb158b878a30609f05f8be00b38454256bbe2203b0a" => :mountain_lion
