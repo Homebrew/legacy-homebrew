ed5f5bcfa390f6c31e28a5b54b6a7df45cce4114
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 17:30:11 +0100

    juju: update 1.24.5 bottle.

--- Library/Formula/juju.rb
+++ Library/Formula/juju.rb
@@ -5,7 +5,8 @@ class Juju < Formula
   sha256 "3ffecd4613961f588d67f7f44877080b409f77379d59d239c64d88afe94c187f"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "e69b88bb8dce93389f2b5be826010580bcb9d8aaca312c256a3d94d2c0ca41c9" => :el_capitan
     sha256 "0edfb2f7cbf65f9e71e96831280ef2c748a0c2aa1424b9131c0f9ca9b3b54c89" => :yosemite
     sha256 "1c93c724ad0f68a11fc45db936a8130a56a9646a9c2f979accbb6641356fc762" => :mavericks
     sha256 "ff279800b3e7f5014e66cbe63ab00ac5fe7ae8b81c177e8fc58c54bb9db674c1" => :mountain_lion
