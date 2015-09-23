0a4a85c38c2ee715c520642503ebae89bf5d25b2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-23 10:59:29 +0100

    zookeeper: update 3.4.6_1 bottle.

--- Library/Formula/zookeeper.rb
+++ Library/Formula/zookeeper.rb
@@ -18,10 +18,11 @@ class Zookeeper < Formula
   end
 
   bottle do
-    revision 1
-    sha1 "4ef32a515592565bdf88546b9773725e4079a080" => :yosemite
-    sha1 "c2db3e5bf5f66cd575a827842d667ec7f1acf6d0" => :mavericks
-    sha1 "ac54b226852c9ec51477de8263bec2c767f7e842" => :mountain_lion
+    cellar :any
+    revision 2
+    sha256 "12241242d81eb12e9d6f520bfd9a20c7a52682bc08435952109fe49e8257b7c3" => :el_capitan
+    sha256 "568d84b98466d842c3a1fa799892e01769bfa06069809fcd2d52f15c40285e50" => :yosemite
+    sha256 "49ae8ae95b5f631b2883f88ca0a91c23aea84c334f018a24734a16e828ca8dbd" => :mavericks
   end
 
   head do
