4e9caca70c0258de9dc4e19d7e675844ddc7da90
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 20:47:43 +0100

    hardlink-osx: update 0.1.1 bottle.

--- Library/Formula/hardlink-osx.rb
+++ Library/Formula/hardlink-osx.rb
@@ -5,7 +5,8 @@ class HardlinkOsx < Formula
   sha256 "5876554e6dafb6627a94670ac33e750a7efeb3a5fbde5ede3e145cdb5131d1ba"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "edf85db2b0586c410dd96f8ab50cf4cc0f34d1494b3b91a5ef0b00ae16fed3c0" => :el_capitan
     sha1 "b8e5b31796d36c818c302bddbd45f227dc943a13" => :yosemite
     sha1 "d61a54cff4e7ff52a0fc3131188d6adecf5ad35a" => :mavericks
     sha1 "15910f042ea3aac09d8a2e94c783ec30e47e1f3a" => :mountain_lion
