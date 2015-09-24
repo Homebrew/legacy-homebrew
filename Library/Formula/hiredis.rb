10b65514a2c770e3838f4bd5aa4f3b0f8c779fb8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 06:04:46 +0100

    hiredis: update 0.13.1 bottle.

--- Library/Formula/hiredis.rb
+++ Library/Formula/hiredis.rb
@@ -9,6 +9,7 @@ class Hiredis < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "811867ba0865d4718d23f7539de8615c713aa8e6d0a719b3a2a2f35705e11caf" => :el_capitan
     sha256 "0574fa67843dfe0070123d2bf60a7087701f90195b46ad2d7e7b9f9da321d4d7" => :yosemite
     sha256 "832daedc5036ef20e9cf166ce3817ec2a5da8b0b0808b39e6911154550bb59c7" => :mavericks
     sha256 "ab30e930930d581ae6e86b6a0440692c152004915e14d066f16edc029f9aad6d" => :mountain_lion
