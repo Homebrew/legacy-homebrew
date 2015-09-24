daf9412b7289ab58a2a1b66bde6200a4892af6a1
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 06:04:54 +0100

    sdl_gfx: update 2.0.24 bottle.

--- Library/Formula/sdl_gfx.rb
+++ Library/Formula/sdl_gfx.rb
@@ -7,6 +7,7 @@ class SdlGfx < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "202f917c2ceef69a4ec07de2a9a61e8def2c9d8499cc15633c5efbb3c4db914c" => :el_capitan
     sha1 "6c70b7a9048bc066ecbb3c037c3a1a0fe2d3166a" => :yosemite
     sha1 "364758460ad6f2cfc83c58d5fc8e793688cd9862" => :mavericks
     sha1 "342e8a64a908f0e39ba6a1d8318ed111679067a1" => :mountain_lion
