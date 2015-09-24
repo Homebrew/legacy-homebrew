75e955b5d8db65eb51251d1c615e47274c80e635
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 14:56:16 +0100

    lcov: update 1.11 bottle.

--- Library/Formula/lcov.rb
+++ Library/Formula/lcov.rb
@@ -7,7 +7,8 @@ class Lcov < Formula
   head "https://github.com/linux-test-project/lcov.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "922c6785490b66abc36ca3189c7a8d3f1c28b7aa952936ae327ca1f99b2ac606" => :el_capitan
     sha1 "edad7ab819deb6032734442ea88c343e6779f60c" => :mavericks
     sha1 "61cb990e2928ad0a1b29e131790790994b5a95d6" => :mountain_lion
     sha1 "b417a6801535d439dc26200213e63a4536989d11" => :lion
