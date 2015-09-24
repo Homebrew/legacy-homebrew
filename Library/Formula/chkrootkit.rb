baee426ef072055e374ca9de1cc962b42d17243e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 20:47:42 +0100

    chkrootkit: update 0.50 bottle.

--- Library/Formula/chkrootkit.rb
+++ Library/Formula/chkrootkit.rb
@@ -5,7 +5,8 @@ class Chkrootkit < Formula
   sha256 "9548fc922b0cb8ddf055faff4a4887f140a31c45f2f5e3aa64aad91ecfa56cc7"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "d631f10284719bd507c6e08bfadc68507adda611d1488dfe6f59f2cdf3b7d1e9" => :el_capitan
     sha256 "e55c78bfac4b081cef6b51e46cf3ff7fed13b4150f8772535fa401e83bcfe213" => :yosemite
     sha256 "db4476a3cfc7d10f9991628c95068784c702662bc0be79b124fd775c56c65ecf" => :mavericks
     sha256 "8aa44d8d76a8d6ddbc27adac04e64e3120c4599b2d617ff0fa82d15f8229957b" => :mountain_lion
