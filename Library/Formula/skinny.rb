9f487ec5384d6c792c05e0569de683ad4aaf6b71
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 14:56:17 +0100

    skinny: update 1.3.20 bottle.

--- Library/Formula/skinny.rb
+++ Library/Formula/skinny.rb
@@ -5,7 +5,8 @@ class Skinny < Formula
   sha256 "b47d3df42fa9b54c77e72bd921261131b565a51a0e4578acac699a83c7365233"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "b1e2dd61a6e1ee7baf9bace68908c0cc81fbec4fc5a72841b1eec34be8e05ddf" => :el_capitan
     sha256 "d111d7e0cd3a6c8d759a3eeb2a4039f186c79b4a2ae69458dff3eeef773d9364" => :yosemite
     sha256 "3f5fe80d9a51b7f7b9c82d0e547ca26933d9ce9be3f15fa3ad24c62dd5436f42" => :mavericks
     sha256 "9ae8759b4feef01177fcfda6c84f450a028bc71a4bb3de6a413a8105e99b87e7" => :mountain_lion
