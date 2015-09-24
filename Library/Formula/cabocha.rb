9ee5828d4e79369cf628a1451e055c7311e4587b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 14:56:14 +0100

    cabocha: update 0.69 bottle.

--- Library/Formula/cabocha.rb
+++ Library/Formula/cabocha.rb
@@ -5,6 +5,7 @@ class Cabocha < Formula
   sha256 "9db896d7f9d83fc3ae34908b788ae514ae19531eb89052e25f061232f6165992"
 
   bottle do
+    sha256 "bf3ed6bc9333b43919264913c40a86997a7601a83abf6dcfa1dfe14745b3fc7c" => :el_capitan
     sha1 "c6d6a98dedfe7466c454101174b3d5cbc2752f9b" => :yosemite
     sha1 "de55a785d8dcce5696a36f69b67168c913405259" => :mavericks
     sha1 "40106c50d68d5bd03941946378679ff490ae679a" => :mountain_lion
