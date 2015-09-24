53d5a673da80504d4c5a53773626f49cc497ce68
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 14:56:16 +0100

    libbson: update 1.1.7 bottle.

--- Library/Formula/libbson.rb
+++ Library/Formula/libbson.rb
@@ -6,6 +6,7 @@ class Libbson < Formula
 
   bottle do
     cellar :any
+    sha256 "0fe2c73915572317e0e708bfe387fb3320ac8474afc4173b70d0b018ec0bdb33" => :el_capitan
     sha256 "00c6bf92d3d324c39fce886b061de737090ea98f3cadf02a360857ac0f302dc6" => :yosemite
     sha256 "78e2c2b112d80180c5578f9a1581b68bfd73d2fe7561903d2b4e177ccdd8932d" => :mavericks
     sha256 "5fd3ce491e4a7c7980359b9870d74d2ed7f90f35876d7de28ffd50f6c3e8eaff" => :mountain_lion
