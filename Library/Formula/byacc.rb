d8bc0fdbadb492115741f18f3ddf987c0a6bbf77
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 06:04:39 +0100

    byacc: update 20141128 bottle.

--- Library/Formula/byacc.rb
+++ Library/Formula/byacc.rb
@@ -5,7 +5,8 @@ class Byacc < Formula
   sha256 "f517fc21f08c1a1f010177357df58fc64eb1131011e5dcd48c19fe44c47198d0"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "21dacda27da29077ee9c0bc1d892a0b09c119b4233e4731488a681b87828333b" => :el_capitan
     sha1 "730c5b32d50fc5ebc21f519ac5828d03c50e57ef" => :yosemite
     sha1 "76ef279d3d8466aefa7a921aa2cf663e86486156" => :mavericks
     sha1 "69414e8c76e803da7b0102e6fc4d0096613a0128" => :mountain_lion
