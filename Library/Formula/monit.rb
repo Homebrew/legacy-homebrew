92beee5f75c08cca0a82888744a07614a67153a2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 06:04:50 +0100

    monit: update 5.14 bottle.

--- Library/Formula/monit.rb
+++ Library/Formula/monit.rb
@@ -8,6 +8,7 @@ class Monit < Formula
   bottle do
     cellar :any
     revision 1
+    sha256 "9a399f661346e85d8e4c48e7c142c938576a99475d7655b2195f325603c9936a" => :el_capitan
     sha256 "5422a56375ac88034709bd09932faa9823753ec3faaa55497db48644ea6bde2f" => :yosemite
     sha256 "a4640c1d7b7471895e3523fe407d2f7e93364761172300f71dfcb34264de73ae" => :mavericks
     sha256 "b46c1de20298b7fbb000cefe985dc925b63e8e013459a19a3314afb57124a873" => :mountain_lion
