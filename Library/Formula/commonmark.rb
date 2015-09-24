ee027548e5f66d4f05498cf24fb8cc7860a8100b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-24 20:47:42 +0100

    commonmark: update 0.22.0 bottle.

--- Library/Formula/commonmark.rb
+++ Library/Formula/commonmark.rb
@@ -6,6 +6,7 @@ class Commonmark < Formula
 
   bottle do
     cellar :any
+    sha256 "1eb4a3d846123344c005233eb85f18beefb0734322f7412b34666385f4b52265" => :el_capitan
     sha256 "dd40ba6bf439e6232c4c46347be0eefc2bf71577400993a480d8b6beee9349a4" => :yosemite
     sha256 "810716f1e9da14265ac4d4f1c9c13270929a7291199ff80f399774d7ad94d892" => :mavericks
     sha256 "89779b880634f3052c1d2f50298d93e00a1347d83768fb1eeae46aa39e02136f" => :mountain_lion
