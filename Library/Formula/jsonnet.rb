e2be4c9ebdad6c3d880821543361a4d9ac5dd99f
Stefan Novak <stefan@novak.io>
2015-09-24 17:09:00 -0700

    cleanup formula

--- Library/Formula/jsonnet.rb
+++ Library/Formula/jsonnet.rb
@@ -1,20 +1,23 @@
 class Jsonnet < Formula
-  desc "A domain specific configuration language for defining JSON data."
+  desc "Domain specific configuration language for defining JSON data."
   homepage "https://google.github.io/jsonnet/doc/"
   url "https://github.com/google/jsonnet/archive/v0.7.9.tar.gz"
   sha256 "103a262636b8db3bfc7dcef7a5d93912d6bf713dd468e188760f6622a9889e44"
 
+  needs :cxx11
+
   depends_on :macos => :yosemite
 
   def install
+    ENV.cxx11
     system "make"
     bin.install "jsonnet"
   end
 
   test do
-    require 'json'
+    require "json"
 
-    (testpath/"example.jsonnet").write <<-PAYLOAD
+    (testpath/"example.jsonnet").write <<-EOS
       {
         person1: {
           name: "Alice",
@@ -22,9 +25,9 @@ class Jsonnet < Formula
         },
         person2: self.person1 { name: "Bob" },
       }
-    PAYLOAD
+    EOS
 
-    expected_output = <<-PAYLOAD
+    expected_output = <<-EOS
       {
         "person1": {
           "name": "Alice",
@@ -35,9 +38,9 @@ class Jsonnet < Formula
           "welcome": "Hello Bob!"
         }
       }
-    PAYLOAD
+    EOS
 
-    output = `"#{bin}/jsonnet" #{testpath}/example.jsonnet`
+    output = shell_output("#{bin}/jsonnet #{testpath}/example.jsonnet")
 
     assert_equal JSON.parse(expected_output), JSON.parse(output)
   end
