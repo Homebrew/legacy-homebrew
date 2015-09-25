987c46326324dd4c54a1b7d36f8121fee0bbe5c2
Guo Xiao <guoxiao08@gmail.com>
2015-09-25 17:04:06 +0800

    nghttp2 1.3.4
    Closes #44321.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/nghttp2.rb
+++ Library/Formula/nghttp2.rb
@@ -1,8 +1,8 @@
 class Nghttp2 < Formula
   desc "HTTP/2 C Library"
   homepage "https://nghttp2.org/"
-  url "https://github.com/tatsuhiro-t/nghttp2/releases/download/v1.3.3/nghttp2-1.3.3.tar.xz"
-  sha256 "80be5a32dbfce84bcf75753732dc9f3b2b8588102943b0af2eb525692251f264"
+  url "https://github.com/tatsuhiro-t/nghttp2/releases/download/v1.3.4/nghttp2-1.3.4.tar.xz"
+  sha256 "87803308317247f90cab37cdba2eb45cf03a7f083fac5ed4e40f0937a9c37867"
 
   bottle do
     sha256 "ae782d3c7c53effc171b058632f3f4ae9c338d2975dc4280b6e6762baca0df8d" => :el_capitan
