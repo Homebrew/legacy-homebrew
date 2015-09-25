49bc24812659118be88f89c9ed59717d22f2991a
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-25 10:51:45 +0100

    libsigc++ dependents: revision bumps
    these all need C++11 compliant compilers

--- Library/Formula/net6.rb
+++ Library/Formula/net6.rb
@@ -3,13 +3,16 @@ class Net6 < Formula
   homepage "http://gobby.0x539.de"
   url "http://releases.0x539.de/net6/net6-1.3.14.tar.gz"
   sha256 "155dd82cbe1f8354205c79ab2bb54af4957047422250482596a34b0e0cc61e21"
-  revision 1
+  revision 2
 
   depends_on "pkg-config" => :build
   depends_on "gnutls"
   depends_on "libsigc++"
 
+  needs :cxx11
+
   def install
+    ENV.cxx11
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
