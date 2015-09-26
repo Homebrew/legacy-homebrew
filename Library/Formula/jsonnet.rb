69959b8e442efeeac2a95981e380f36222ab469e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-26 16:53:23 +0100

    jsonnet: add 0.8.1 bottle.

--- Library/Formula/jsonnet.rb
+++ Library/Formula/jsonnet.rb
@@ -4,6 +4,13 @@ class Jsonnet < Formula
   url "https://github.com/google/jsonnet/archive/v0.8.1.tar.gz"
   sha256 "c844623f3d21d81488b20ba5e32b388d6ffcf68fc6f2216e65c23b7196663382"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6c3ed87edc644e760d41f59ca52fa3d04bef21908ba9511ad24eacacac05a4f5" => :el_capitan
+    sha256 "0888a627040f4824ce7b68045060d2c8e8dbb779e60c1858dcee9115c67119ee" => :yosemite
+    sha256 "f75e9d814e801e60cdfd3eeb5752e6ae861672cc6ab5d14440b1754c01888102" => :mavericks
+  end
+
   needs :cxx11
 
   depends_on :macos => :mavericks
