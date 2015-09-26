780aa2f241bc33685198d3556daffd583b43e133
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 00:33:42 +0100

    viennacl: add 1.7.0 bottle.

--- Library/Formula/viennacl.rb
+++ Library/Formula/viennacl.rb
@@ -5,6 +5,13 @@ class Viennacl < Formula
   sha256 "0dd062770f8cf92309b2473d5defc7a6b4c874170e350e6a7ad0f4c791c49eff"
   head "https://github.com/viennacl/viennacl-dev.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0fa897aa195c281bc8aab8f14b5a824c9618f2bddc3fe859341d16a389223d52" => :el_capitan
+    sha256 "4cf0ec1d9cd95e267de40fd83977fa499c01aff3eeb5cac09fad245a6ed67f2f" => :yosemite
+    sha256 "9383c36ab840e0ce9599d283190711d7c3d6a25d1d3bd99970b409028ebb2bc9" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on :macos => :snow_leopard
 
