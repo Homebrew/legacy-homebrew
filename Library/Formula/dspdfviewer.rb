99835d0d700e48526991acc9e2b41a3eb165da81
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-27 22:58:25 +0100

    dspdfviewer: add 1.13.1 bottle.

--- Library/Formula/dspdfviewer.rb
+++ Library/Formula/dspdfviewer.rb
@@ -6,6 +6,12 @@ class Dspdfviewer < Formula
 
   head "https://github.com/dannyedel/dspdfviewer.git"
 
+  bottle do
+    sha256 "8d6696c179bad40c541e4d32107b66ee64a83fd0b6619e1af80f04ddeb3fffa1" => :el_capitan
+    sha256 "13fd6d702d95478e38dcda6c35e208d2d99a799a296e544b83bd35a6d31181fd" => :yosemite
+    sha256 "f014c5237582f5f591c59835e5695e339c07415f8990ca026d88d0838935e2ef" => :mavericks
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "poppler" => "with-qt"
