4c93e78289bfac012ca881184167c2654d84807c
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-27 16:51:11 +0100

    gst-python 1.6.0

--- Library/Formula/gst-python.rb
+++ Library/Formula/gst-python.rb
@@ -1,9 +1,8 @@
 class GstPython < Formula
   desc "GStreamer Python overrides for gobject-introspection-based pygst bindings"
   homepage "http://gstreamer.freedesktop.org/modules/gst-python.html"
-  url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.4.0.tar.xz"
-  sha256 "b1e40c29ceb41b03f08d38aca6056054f0341d0706276326dceeec6ac8d53d3e"
-  revision 1
+  url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.6.0.tar.xz"
+  sha256 "60dbc7f5ac2b6655ed4a7ed2cee63ee5a525d37cb16eaa6b9df7d7dfe4e9605f"
 
   bottle do
     revision 1
