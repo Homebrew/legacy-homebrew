d27e5b31ae6e36fd88f90f252fc440597055cda2
Harry Whelchel <harrywhelchel@gmail.com>
2015-09-27 16:40:00 -0400

    fix style issues

--- Library/Formula/libgraphqlparser.rb
+++ Library/Formula/libgraphqlparser.rb
@@ -1,4 +1,4 @@
-require 'open3'
+require "open3"
 
 class Libgraphqlparser < Formula
   desc "GraphQL query parser in C++ with C and C++ APIs"
@@ -34,6 +34,6 @@ class Libgraphqlparser < Formula
 {"kind":"Document","loc":{"start":1,"end":2},"definitions":[{"kind":"OperationDefinition","loc":{"start":1,"end":2},"operation":"query","name": null,"variableDefinitions":null,"directives":null,"selectionSet":{"kind":"SelectionSet","loc":{"start":1,"end":2},"selections":[{"kind":"Field","loc":{"start":3,"end":4},"alias":null,"name":{"kind":"Name","loc":{"start":3,"end":7},"value":"user"},"arguments":[{"kind":"Argument","loc":{"start":8,"end":13},"name":{"kind":"Name","loc":{"start":8,"end":10},"value":"id"},"value":{"kind":"IntValue","loc":{"start":12,"end":13},"value":"1"}}],"directives":null,"selectionSet":{"kind":"SelectionSet","loc":{"start":15,"end":4},"selections":[{"kind":"Field","loc":{"start":5,"end":9},"alias":null,"name":{"kind":"Name","loc":{"start":5,"end":9},"value":"name"},"arguments":null,"directives":null,"selectionSet":null},{"kind":"Field","loc":{"start":5,"end":8},"alias":null,"name":{"kind":"Name","loc":{"start":5,"end":8},"value":"age"},"arguments":null,"directives":null,"selectionSet":null},{"kind":"Field","loc":{"start":5,"end":6},"alias":null,"name":{"kind":"Name","loc":{"start":5,"end":12},"value":"friends"},"arguments":null,"directives":null,"selectionSet":{"kind":"SelectionSet","loc":{"start":13,"end":6},"selections":[{"kind":"Field","loc":{"start":7,"end":11},"alias":null,"name":{"kind":"Name","loc":{"start":7,"end":11},"value":"name"},"arguments":null,"directives":null,"selectionSet":null}]}}]}}]}}]}
     AST
 
-    Open3.capture2("dump_json_ast", stdin_data: SAMPLE_QUERY).first == SAMPLE_AST
+    Open3.capture2("dump_json_ast", :stdin_data => SAMPLE_QUERY).first == SAMPLE_AST
   end
 end
