eba572ad7d3d81ad13215608d4023cde51e62ac3
Tim D. Smith <git@tim-smith.us>
2015-09-27 16:30:56 -0700

    pyside: use opt path for qt
    Fixes #43805.

--- Library/Formula/pyside.rb
+++ Library/Formula/pyside.rb
@@ -4,6 +4,7 @@ class Pyside < Formula
   url "https://download.qt.io/official_releases/pyside/pyside-qt4.8+1.2.2.tar.bz2"
   mirror "https://distfiles.macports.org/py-pyside/pyside-qt4.8+1.2.2.tar.bz2"
   sha256 "a1a9df746378efe52211f1a229f77571d1306fb72830bbf73f0d512ed9856ae1"
+  revision 1
 
   head "https://github.com/PySide/PySide.git"
 
@@ -68,6 +69,8 @@ class Pyside < Formula
         system "make", "install"
       end
     end
+
+    inreplace include/"PySide/pyside_global.h", Formula["qt"].prefix, Formula["qt"].opt_prefix
   end
 
   test do
