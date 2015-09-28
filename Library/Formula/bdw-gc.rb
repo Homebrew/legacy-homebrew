baa55432af7213af338f93901dfb875f8a9f7832
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-09-28 11:26:29 -0300

    bdw-gc: add head
    Closes #44407.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/bdw-gc.rb
+++ Library/Formula/bdw-gc.rb
@@ -4,9 +4,6 @@ class BdwGc < Formula
   url "http://www.hboehm.info/gc/gc_source/gc-7.4.2.tar.gz"
   sha256 "63320ad7c45460e4a40e03f5aa4c6893783f21a16416c3282b994f933312afa2"
 
-  depends_on "pkg-config" => :build
-  depends_on "libatomic_ops" => :build
-
   bottle do
     revision 2
     sha256 "ba0257546369cd1879d66d3f1302194ae39767ccb5b012a20d16fdf5595b4326" => :el_capitan
@@ -15,10 +12,22 @@ class BdwGc < Formula
     sha256 "e5725f4c6b23ce7dc75e3e8fff51cd1f9f90858bad20d1ce00cf33499edf8f6b" => :mountain_lion
   end
 
+  head do
+    url "https://github.com/ivmai/bdwgc.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool"  => :build
+  end
+
   option :universal
+
+  depends_on "pkg-config" => :build
+  depends_on "libatomic_ops" => :build
 
   def install
     ENV.universal_binary if build.universal?
+
+    system "./autogen.sh" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
