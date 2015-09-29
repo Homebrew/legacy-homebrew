ad7aa335f7e313cde493d44570afe663495e5334
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-09-29 17:30:24 +0100

    scrollkeeper and docbook-xsl: XML_CATALOG_FILES
    environment variable needs to be set for xmlcatalog to work
    properly on El Capitan

--- Library/Formula/scrollkeeper.rb
+++ Library/Formula/scrollkeeper.rb
@@ -3,6 +3,7 @@ class Scrollkeeper < Formula
   homepage "http://scrollkeeper.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/scrollkeeper/scrollkeeper/0.3.14/scrollkeeper-0.3.14.tar.gz"
   sha256 "4a0bd3c3a2c5eca6caf2133a504036665485d3d729a16fc60e013e1b58e7ddad"
+  revision 1
 
   depends_on "gettext"
   depends_on "docbook"
@@ -11,6 +12,7 @@ class Scrollkeeper < Formula
     :because => "scrollkeeper and rarian install the same binaries."
 
   def install
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--with-xml-catalog=#{etc}/xml/catalog"
