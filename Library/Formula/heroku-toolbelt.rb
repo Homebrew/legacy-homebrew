3a8de8f9c5241b368bed919967369e10ad4afe53
dickeyxxx <jeff@dickeyxxx.com>
2015-09-29 11:50:53 -0400

    heroku-toolbelt 3.42.17

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -1,8 +1,8 @@
 class HerokuToolbelt < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://toolbelt.heroku.com/other"
-  url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.42.10.tgz"
-  sha256 "fb979a603e955bd5f6561ad2bb1b8e88bec2c57d844264671323690f770d76f4"
+  url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.42.17.tgz"
+  sha256 "b77a52ae53c9de1da180d1f8b5124ac6107899254cf747daa7548aec99ffb831"
   head "https://github.com/heroku/heroku.git"
 
   depends_on :ruby => "1.9"
