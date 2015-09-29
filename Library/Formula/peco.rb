cc747075e0266d5eb8a816067c8784cd5a6a3237
Dominyk Tiller <dominyktiller@gmail.com>
2015-09-29 22:16:58 +0100

    peco 0.3.4
    Closes #44459.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/peco.rb
+++ Library/Formula/peco.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Peco < Formula
   desc "Simplistic interactive filtering tool"
   homepage "https://github.com/peco/peco"
-  url "https://github.com/peco/peco/archive/v0.3.3.tar.gz"
-  sha256 "3d30a8788c2c7cf4ac44bc5ad09415b9b6245c6e842e46b4e234cb844e929c84"
+  url "https://github.com/peco/peco/archive/v0.3.4.tar.gz"
+  sha256 "48a864ea92dd12356411ac477b3991d48070528f05d5ca5dfff0ad88e874d470"
 
   head "https://github.com/peco/peco.git"
 
@@ -20,22 +20,22 @@ class Peco < Formula
 
   go_resource "github.com/google/btree" do
     url "https://github.com/google/btree.git",
-      :revision => "cc6329d4279e3f025a53a83c397d2339b5705c45"
+      :revision => "0c05920fc3d98100a5e3f7fd339865a6e2aaa671"
   end
 
   go_resource "github.com/jessevdk/go-flags" do
     url "https://github.com/jessevdk/go-flags.git",
-      :revision => "1b89bf73cd2c3a911d7b2a279ab085c4a18cf539"
+      :revision => "8ec9564882e7923e632f012761c81c46dcf5bec1"
   end
 
   go_resource "github.com/mattn/go-runewidth" do
     url "https://github.com/mattn/go-runewidth.git",
-      :revision => "5890272cd41c5103531cd7b79e428d99c9e97f76"
+      :revision => "58a0da4ed7b321c9b5dfeffb7e03ee188fae1c60"
   end
 
   go_resource "github.com/nsf/termbox-go" do
     url "https://github.com/nsf/termbox-go.git",
-      :revision => "785b5546a97f27460cfbc4c77132a46b90beb834"
+      :revision => "10f14d7408b64a659b7c694a771f5006952d336c"
   end
 
   def install
