e2a0d751b9fb41a33cece1f22f0e50a404cd4361
rsmz <remik.ziemlinski@gmail.com>
2015-09-29 15:11:06 -0400

    nccmp 1.7.4.1 (new formula)
    1. Upstream now builds in parallel.
    2. Made functional test.

--- Library/Formula/nccmp.rb
+++ Library/Formula/nccmp.rb
@@ -1,21 +1,24 @@
 class Nccmp < Formula
-  desc "nccmp compares 2 NetCDF files bitwise, semantically or with a tolerance."
+  desc "Compare two NetCDF files bitwise, semantically or with a tolerance."
   homepage "http://nccmp.sourceforge.net/"
-  url "https://downloads.sourceforge.net/projects/nccmp/files/nccmp-1.7.4.0.tar.gz"
-  sha256 "7760b65e54f6b07ee764fe37a71eaa1710f4c6440c6fa5ee05796912865748fb"
+  url "https://downloads.sourceforge.net/project/nccmp/nccmp-1.7.4.1.tar.gz"
+  sha256 "985b50505dd7517710c5699cb9ee1028c6d75b06a17649bb911d838c4908dc69"
 
-  depends_on 'netcdf'
+  depends_on "netcdf"
 
   def install
-    ENV.deparallelize
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
+    system "make"
     system "make", "check"
     system "make", "install"
   end
 
   test do
-    system "true"
+    (testpath/"test.cdl").write("netcdf {}")
+    system "ncgen", "-o", testpath/"test1.nc", testpath/"test.cdl"
+    system "ncgen", "-o", testpath/"test2.nc", testpath/"test.cdl"
+    system "#{bin}/nccmp", "-mds", testpath/"test1.nc", testpath/"test2.nc"
   end
 end
