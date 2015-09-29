4b5ecb0b9816be25bf7080300b952fe75a28df0a
mindrones <mindrones@users.noreply.github.com>
2015-09-29 13:27:09 +0200

    neo4j-2.3.0-M03 (devel)

--- Library/Formula/neo4j.rb
+++ Library/Formula/neo4j.rb
@@ -6,16 +6,16 @@ class Neo4j < Formula
   sha256 "7fadc119f465a3d6adceb610401363fb158a5ed25081f9893d4f56ac4989a998"
 
   devel do
-    url "http://dist.neo4j.org/neo4j-community-2.3.0-M02-unix.tar.gz"
-    sha256 "54047565659e1230c7a196ff696765e042da5679cf287966efad9e36a8f07046"
-    version "2.3.0-M02"
+    url "http://dist.neo4j.org/neo4j-community-2.3.0-M03-unix.tar.gz"
+    sha256 "cd47ae541a0c5da0d82212c30c1c2fb19ea91ddb1b174346c120e596e04b5dfc"
+    version "2.3.0-M03"
   end
 
   option "with-neo4j-shell-tools", "Add neo4j-shell-tools to the standard neo4j-shell"
 
   resource "neo4j-shell-tools" do
     url "http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_2.2.zip"
-    sha256 "a84bd306754701c1748a26dcf207c136c9859f60cdd60e003771f0df0a83fb00"
+    sha256 "e295f724b6c5e35db5d471eefc976bd5adcd002763722c726885455e3f38f2ca"
   end
 
   def install
