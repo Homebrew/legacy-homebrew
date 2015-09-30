90dc07da86adb1d57979c86ed1ed93f0dbc99189
Patrick Hart <thrifus@gmail.com>
2015-09-30 13:15:35 -0500

    Upgrade form 1.0.0 to 1.0.1

--- Library/Formula/tortle.rb
+++ Library/Formula/tortle.rb
@@ -1,8 +1,8 @@
 class Tortle < Formula
   desc "A tiny utility for easily enabling and disabling Tor on OS X"
   homepage "https://thrifus.github.io/Tortle"
-  url "https://github.com/thrifus/Tortle/archive/1.0.0.tar.gz"
-  version "1.0.0"
+  url "https://github.com/thrifus/Tortle/archive/1.0.1.tar.gz"
+  version "1.0.1"
   sha256 "01328162a017868f713e04a65fa7ea3315adb285046edbf13917fd26e849cca4"
 
   depends_on "tor"
