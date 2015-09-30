a9cbdbf67201f13e167b8df3999ec22cbe11f56b
Patrick Hart <thrifus@gmail.com>
2015-09-30 11:55:16 -0500

    Initial commit
    Turtle is a tiny utility for enabling and disabling Tor on OS X
    
    Homepage:
    http://thrifus.github.io/Tortle
    
    Documentation:
    https://github.com/thrifus/Tortle/blob/master/README.md

---
+++ Library/Formula/tortle.rb
@@ -0,0 +1,12 @@
+class Tortle < Formula
+  desc "A tiny utility for enabling and disabling Tor on OS X"
+  homepage "https://thrifus.github.io/Tortle"
+  url "https://github.com/thrifus/Tortle/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "01328162a017868f713e04a65fa7ea3315adb285046edbf13917fd26e849cca4"
+
+  depends_on "tor"
+  def install
+    bin.install "tortle"
+  end
+end
