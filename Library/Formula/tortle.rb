d726bdc16c8d3457badb3800ccf03a0a42aeff72
Patrick Hart <thrifus@gmail.com>
2015-09-30 18:45:53 -0500

    Update turtle.rb
    - Remove “version” line
    - Add empty line below “depends_on” line

--- Library/Formula/tortle.rb
+++ Library/Formula/tortle.rb
@@ -2,10 +2,10 @@ class Tortle < Formula
   desc "Tiny utility for easily enabling or disabling Tor"
   homepage "https://thrifus.github.io/Tortle"
   url "https://github.com/thrifus/Tortle/archive/1.0.2.tar.gz"
-  version "1.0.2"
   sha256 "f9cb1de7586f99b9a1aa746a7e2b7f1939179841c479b243a3c8a36eff0c3186"
 
   depends_on "tor"
+  
   def install
     bin.install "tortle"
   end
