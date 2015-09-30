fd86b2f91a5637f28b27cdc72b5214af6beeb1bb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-09-30 06:45:37 +0100

    viewvc: add 1.1.23 bottle.

--- Library/Formula/viewvc.rb
+++ Library/Formula/viewvc.rb
@@ -4,6 +4,13 @@ class Viewvc < Formula
   url "http://viewvc.tigris.org/files/documents/3330/49392/viewvc-1.1.23.tar.gz"
   sha256 "d14290b23f44e8f5301a7ca9fc946bcd0f8bb8206d9b9c111ac56e19f2588b7d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "89041cf76c25bcc0c6ebf4a5991af5a5dc0d5cb0f436149d57d563e2401f20d8" => :el_capitan
+    sha256 "77d7571e6a36dc12e5e34f06d1fa76caeb4f34d046689eef08656d6596987757" => :yosemite
+    sha256 "56b8feafaa2282718d8db6a4612f58de01efdc0b86b62cb80e5715d5085c8a56" => :mavericks
+  end
+
   # swig is a dependency of subversion --with-python, but due to a
   # bug it needs to also be specified here.
   # https://github.com/Homebrew/homebrew/issues/42915
