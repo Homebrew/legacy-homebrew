2d356e0040f468c7af078f1c0323d529bc778273
Thierry Moisan <thierry.moisan@gmail.com>
2015-10-01 18:51:14 -0400

    hbase 1.1.2
    Closes #44511.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/hbase.rb
+++ Library/Formula/hbase.rb
@@ -1,8 +1,8 @@
 class Hbase < Formula
   desc "Hadoop database: a distributed, scalable, big data store"
   homepage "https://hbase.apache.org"
-  url "https://www.apache.org/dyn/closer.cgi?path=hbase/1.0.1.1/hbase-1.0.1.1-bin.tar.gz"
-  sha256 "fd20fd98e9c11d96d0281077e3040c81f45bafcc1e4f14318cede31e45819fdf"
+  url "https://www.apache.org/dyn/closer.cgi?path=hbase/1.1.2/hbase-1.1.2-bin.tar.gz"
+  sha256 "8ca5bf0203cef86b4a0acbba89afcd5977488ebc73eec097e93c592b16f8bede"
 
   depends_on :java => "1.6+"
   depends_on "hadoop"
