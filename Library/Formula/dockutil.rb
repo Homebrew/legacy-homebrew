9bc827a4699ba0a3dd4f035f7e4877433d2cea30
Keith Smiley <keithbsmiley@gmail.com>
2015-09-30 22:32:01 -0700

    Remove broken dockutil dependency
    plistlib has been removed from pypi. As far as I can tell, the default
    OS X python ships with plistlib, so this isn't necessary. I wouldn't be
    surprised if this wasn't the case for some of the older versions
    supported by this formula.

--- Library/Formula/dockutil.rb
+++ Library/Formula/dockutil.rb
@@ -13,15 +13,8 @@ class Dockutil < Formula
 
   depends_on :python if MacOS.version <= :snow_leopard
 
-  resource "plistlib" do
-    url "https://pypi.python.org/packages/source/p/plist/plist-0.2.tar.gz"
-    sha256 "531595d63ee4b7de6a168fc4ca715c475be9700de93455a7c73a176a1e1f3345"
-  end
-
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
-
-    resource("plistlib").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
 
     bin.install "scripts/dockutil"
   end
