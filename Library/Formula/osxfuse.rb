6b3908838e3e41034e4aaf30bd44699b74da8eb9
Manuel Giffels <giffels@gmail.com>
2015-10-01 11:57:49 +0200

    osxfuse 2.8.0
    Closes #44493.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/osxfuse.rb
+++ Library/Formula/osxfuse.rb
@@ -1,8 +1,8 @@
 class Osxfuse < Formula
   desc "FUSE for OS X: extend native file handling via 3rd-party file systems"
   homepage "https://osxfuse.github.io/"
-  url "https://github.com/osxfuse/osxfuse.git", :tag => "osxfuse-2.7.6",
-                                                :revision => "6ba1962e153e7a76f43bc2f80b0ba0ef2da2cef9"
+  url "https://github.com/osxfuse/osxfuse.git", :tag => "osxfuse-2.8.0",
+                                                :revision => "6a1e7793079d446950f6aaa250e4762710ad326e"
 
   head "https://github.com/osxfuse/osxfuse.git", :branch => "osxfuse-2"
 
