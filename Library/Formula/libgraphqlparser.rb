9d1021797c30283204941acb016bd7e065488ab7
Harry Whelchel <harrywhelchel@gmail.com>
2015-09-30 22:23:45 -0400

    simplify sample query

--- Library/Formula/libgraphqlparser.rb
+++ Library/Formula/libgraphqlparser.rb
@@ -1,6 +1,3 @@
-require "open3"
-require "json"
-
 class Libgraphqlparser < Formula
   desc "GraphQL query parser in C++ with C and C++ APIs"
   homepage "https://github.com/graphql/libgraphqlparser"
@@ -9,7 +6,7 @@ class Libgraphqlparser < Formula
 
   depends_on "cmake" => :build
   depends_on "bison" => :recommended
-  depends_on "flex"  => :recommended
+  depends_on "flex" => :recommended
 
   def install
     system "cmake", ".", *std_cmake_args
@@ -19,185 +16,36 @@ class Libgraphqlparser < Formula
   end
 
   test do
+    require "open3"
+    require "json"
+
     sample_query = <<-EOS.undent
-      {
-        user(id: 1) {
-          name
-          age
-          friends {
-            name
-          }
-        }
-      }
+      { user }
     EOS
 
-    sample_ast = {
-      "kind" => "Document",
-      "loc" => {
-        "start" => 1,
-        "end" => 2,
-      },
-      "definitions" => [
-        {
-          "kind" => "OperationDefinition",
-          "loc" => {
-            "start" => 1,
-            "end" => 2,
-          },
-          "operation" => "query",
-          "name" => nil,
-          "variableDefinitions" => nil,
-          "directives" => nil,
-          "selectionSet" => {
-            "kind" => "SelectionSet",
-            "loc" => {
-              "start" => 1,
-              "end" => 2,
-            },
-            "selections" => [
-              {
-                "kind" => "Field",
-                "loc" => {
-                  "start" => 3,
-                  "end" => 4,
-                },
-                "alias" => nil,
-                "name" => {
-                  "kind" => "Name",
-                  "loc" => {
-                    "start" => 3,
-                    "end" => 7,
-                  },
-                  "value" => "user",
-                },
-                "arguments" => [
-                  {
-                    "kind" => "Argument",
-                    "loc" => {
-                      "start" => 8,
-                      "end" => 13,
-                    },
-                    "name" => {
-                      "kind" => "Name",
-                      "loc" => {
-                        "start" => 8,
-                        "end" => 10,
-                      },
-                      "value" => "id",
-                    },
-                    "value" => {
-                      "kind" => "IntValue",
-                      "loc" => {
-                        "start" => 12,
-                        "end" => 13,
-                      },
-                      "value" => "1",
-                    },
-                  }
-                ],
-                "directives" => nil,
-                "selectionSet" => {
-                  "kind" => "SelectionSet",
-                  "loc" => {
-                    "start" => 15,
-                    "end" => 4,
-                  },
-                  "selections" => [
-                    {
-                      "kind" => "Field",
-                      "loc" => {
-                        "start" => 5,
-                        "end" => 9,
-                      },
-                      "alias" => nil,
-                      "name" => {
-                        "kind" => "Name",
-                        "loc" => {
-                          "start" => 5,
-                          "end" => 9,
-                        },
-                        "value" => "name",
-                      },
-                      "arguments" => nil,
-                      "directives" => nil,
-                      "selectionSet" => nil,
-                    },
-                    {
-                      "kind" => "Field",
-                      "loc" => {
-                        "start" => 5,
-                        "end" => 8,
-                      },
-                      "alias" => nil,
-                      "name" => {
-                        "kind" => "Name",
-                        "loc" => {
-                          "start" => 5,
-                          "end" => 8,
-                        },
-                        "value" => "age",
-                      },
-                      "arguments" => nil,
-                      "directives" => nil,
-                      "selectionSet" => nil,
-                    },
-                    {
-                      "kind" => "Field",
-                      "loc" => {
-                        "start" => 5,
-                        "end" => 6,
-                      },
-                      "alias" => nil,
-                      "name" => {
-                        "kind" => "Name",
-                        "loc" => {
-                          "start" => 5,
-                          "end" => 12,
-                        },
-                        "value" => "friends",
-                      },
-                      "arguments" => nil,
-                      "directives" => nil,
-                      "selectionSet" => {
-                        "kind" => "SelectionSet",
-                        "loc" => {
-                          "start" => 13,
-                          "end" => 6,
-                        },
-                        "selections" => [
-                          {
-                            "kind" => "Field",
-                            "loc" => {
-                              "start" => 7,
-                              "end" => 11,
-                            },
-                            "alias" => nil,
-                            "name" => {
-                              "kind" => "Name",
-                              "loc" => {
-                                "start" => 7,
-                                "end" => 11,
-                              },
-                              "value" => "name",
-                            },
-                            "arguments" => nil,
-                            "directives" => nil,
-                            "selectionSet" => nil,
-                          }
-                        ],
-                      },
-                    },
-                  ],
-                },
-              }
-            ],
-          },
-        }
-      ],
-    }
+    sample_ast = {"kind"=>"Document",
+      "loc"=>{"start"=>1, "end"=>9},
+      "definitions"=>
+        [{"kind"=>"OperationDefinition",
+          "loc"=>{"start"=>1, "end"=>9},
+          "operation"=>"query",
+          "name"=>nil,
+          "variableDefinitions"=>nil,
+          "directives"=>nil,
+          "selectionSet"=>
+           {"kind"=>"SelectionSet",
+            "loc"=>{"start"=>1, "end"=>9},
+            "selections"=>
+             [{"kind"=>"Field",
+               "loc"=>{"start"=>3, "end"=>7},
+               "alias"=>nil,
+               "name"=>
+                {"kind"=>"Name", "loc"=>{"start"=>3, "end"=>7}, "value"=>"user"},
+               "arguments"=>nil,
+               "directives"=>nil,
+               "selectionSet"=>nil}]}}]}
 
     test_ast = JSON.parse(pipe_output("#{libexec}/dump_json_ast", sample_query))
-
     assert_equal sample_ast, test_ast
   end
 end
