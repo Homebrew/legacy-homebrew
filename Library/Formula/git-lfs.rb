0188c0d5781ac7c029f8456c6e9bc9cb7ccedf03
risk danger olson <technoweenie@gmail.com>
2015-10-01 09:46:41 -0700

    bump git-lfs

--- Library/Formula/git-lfs.rb
+++ Library/Formula/git-lfs.rb
@@ -1,8 +1,8 @@
 class GitLfs < Formula
   desc "Git extension for versioning large files"
   homepage "https://github.com/github/git-lfs"
-  url "https://github.com/github/git-lfs/archive/v0.6.0.tar.gz"
-  sha256 "cfb270be1e480c70f0cea8eacf9828c2d7ef590532b8a88931ca58e1505183c1"
+  url "https://github.com/github/git-lfs/archive/v1.0.0.tar.gz"
+  sha256 "bd9d0d597b36421babf282680d34d66b626facea63f68fc64399ac9fe70d2dd3"
 
   bottle do
     cellar :any_skip_relocation
