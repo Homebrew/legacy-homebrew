694ff0655b029636b45f461ecad61ce9b5735c82
Dan <dan@wadoku.de>
2015-10-02 10:40:02 +0200

    tomcat 8.0.27
    Closes #44517.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/tomcat.rb
+++ Library/Formula/tomcat.rb
@@ -1,9 +1,9 @@
 class Tomcat < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.0.26/bin/apache-tomcat-8.0.26.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.26/bin/apache-tomcat-8.0.26.tar.gz"
-  sha256 "9f11588f0ff767adde63cd6919462c0c2742897560f4b367a0ffffdd8b1ed382"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-8/v8.0.27/bin/apache-tomcat-8.0.27.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.27/bin/apache-tomcat-8.0.27.tar.gz"
+  sha256 "8ae7885596feb61d9f8944f928bcd6af7c799de33034fb7435c9991b34aa729a"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,10 +16,10 @@ class Tomcat < Formula
   option "with-fulldocs", "Install full documentation locally"
 
   resource "fulldocs" do
-    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-8/v8.0.26/bin/apache-tomcat-8.0.26-fulldocs.tar.gz"
-    mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.26/bin/apache-tomcat-8.0.26-fulldocs.tar.gz"
-    version "8.0.26"
-    sha256 "813513d61e6def5ccf01adc95bf9d28594fce71ff32f5e23dc1482c7ec2f129b"
+    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-8/v8.0.27/bin/apache-tomcat-8.0.27-fulldocs.tar.gz"
+    mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.27/bin/apache-tomcat-8.0.27-fulldocs.tar.gz"
+    version "8.0.27"
+    sha256 "4707b2c2f8c5b241001f8909bb52791d77aee1a481abd4c0065283477d75bee9"
   end
 
   def install
