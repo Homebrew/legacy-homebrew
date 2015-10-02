1fb7f75b88178fc66457aae6179a76533ad79332
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-02 16:03:54 +0100

    ghex: update 3.18.0 bottle.

--- Library/Formula/ghex.rb
+++ Library/Formula/ghex.rb
@@ -5,9 +5,9 @@ class Ghex < Formula
   sha256 "c5b1eb50a8dd1334880b37617871498b778ea137f79bb43894ec68e4f63dc925"
 
   bottle do
-    sha256 "e52e0de4433e01d625f49a5bb871b132b395fb57b8dfbcddaf01e97adc9f08e6" => :yosemite
-    sha256 "1a85bc508918b1fe7ed481082bc1c0b7f952ecfbd7a514ec9e9ac52f95cc8daa" => :mavericks
-    sha256 "7b0c21b630bc1e020362b42e5d2a52d0c04a764c52b03a041a434b4f719a09e7" => :mountain_lion
+    sha256 "9eb6931e7ac0fb1ea0e5403cc02cd403a19874269b9d23946a2cfb17c3b8c588" => :el_capitan
+    sha256 "2bf38162e119817cf5151c7ac1152fcb722fd9305a5555db865e3a83b027ab6b" => :yosemite
+    sha256 "d20ac82df9e830d9579a4ad7689a9162744f1373d601f91edd18b80e3d4fe386" => :mavericks
   end
 
   depends_on "pkg-config" => :build
