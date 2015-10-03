ef3f9cb582914452fcd9922d288182a8c4c8e195
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-03 03:21:43 +0100

    ratfor: add 1.02 bottle.

--- Library/Formula/ratfor.rb
+++ Library/Formula/ratfor.rb
@@ -4,6 +4,13 @@ class Ratfor < Formula
   url "http://www.dgate.org/ratfor/tars/ratfor-1.02.tar.gz"
   sha256 "daf2971df48c3b3908c6788c4c6b3cdfb4eaad21ec819eee70a060956736ea1c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6998ed33f7547a097ced8ce5407756c50145d21ece1c8cd3474e6c9eeefd89c7" => :el_capitan
+    sha256 "2d368db5719c280340140998d525279a5f5178c0acccdecc7281f38f3d07c563" => :yosemite
+    sha256 "0544b9e974932e28f090aad1c54dd0c6708ebf1d7a0d3657a150cdb4fdb0cf36" => :mavericks
+  end
+
   depends_on :fortran
 
   def install
