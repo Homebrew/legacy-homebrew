82fbc5e3296c2b293167458c787540e5f4174738
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-03 03:01:02 +0100

    mspdebug: add 0.23 bottle.

--- Library/Formula/mspdebug.rb
+++ Library/Formula/mspdebug.rb
@@ -4,6 +4,12 @@ class Mspdebug < Formula
   url "https://github.com/dlbeer/mspdebug/archive/v0.23.tar.gz"
   sha256 "e4db9ac519d5989aa48e43e92f04a6821a5dc7dff8c8b3795508a77ec4edde84"
 
+  bottle do
+    sha256 "ca534bad59641ade8811ce3e78ba7738746ed826cd9f8e4cf49750f0ea40ee7b" => :el_capitan
+    sha256 "d867fe5b0ec60ee3c4bb60c247f5f6f8539bd33447107e271e01d91b8f5718e2" => :yosemite
+    sha256 "c0c42979013ec17216dd7e5ec7cb9bec2284f72324048ebe48d411e23035cedb" => :mavericks
+  end
+
   depends_on "libusb-compat"
 
   def install
