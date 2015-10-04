641b22a0464b7d084de9741b5f28c94e8fad2dc0
Dominyk Tiller <dominyktiller@gmail.com>
2015-10-04 20:18:12 +0100

    wordnet: fix bottle placement

--- Library/Formula/wordnet.rb
+++ Library/Formula/wordnet.rb
@@ -2,15 +2,15 @@ class Wordnet < Formula
   desc "Lexical database for the English language"
   homepage "http://wordnet.princeton.edu/"
   url "http://wordnetcode.princeton.edu/3.0/WordNet-3.0.tar.bz2"
+  # Version 3.1 is version 3.0 with the 3.1 dictionary.
+  version "3.1"
+  sha256 "6c492d0c7b4a40e7674d088191d3aa11f373bb1da60762e098b8ee2dda96ef22"
+
   bottle do
     sha256 "412b4cc4b65d5083176aa69647ab5a15b96b63b758fa8900c80b402c5a9d2cb6" => :el_capitan
     sha256 "876de343c8e2d508af818a7aacdcc8015f7e662edf8f08e068ca7800f48d50d4" => :yosemite
     sha256 "786bc9b811d958b71888cc87e0ef75a6cd66ebc05202278b7827f847f6b4dfe5" => :mavericks
   end
-
-  # Version 3.1 is version 3.0 with the 3.1 dictionary.
-  version "3.1"
-  sha256 "6c492d0c7b4a40e7674d088191d3aa11f373bb1da60762e098b8ee2dda96ef22"
 
   depends_on :x11
 
