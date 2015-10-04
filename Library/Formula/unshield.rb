03330b1625876782d99ce313bb29853d0748fc63
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-04 19:19:28 +0200

    unshield 1.3
    Closes #44593.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/unshield.rb
+++ Library/Formula/unshield.rb
@@ -1,18 +1,18 @@
 class Unshield < Formula
   desc "Extract files from InstallShield cabinet files"
   homepage "https://github.com/twogood/unshield"
-  url "https://github.com/twogood/unshield/archive/1.0.tar.gz"
-  sha256 "add720222f44b35f81e52332aa545f3dbe39da48489fca319cdfd6d730087c07"
+  url "https://github.com/twogood/unshield/archive/1.3.tar.gz"
+  sha256 "31a49c43b60e86b3ed731e0a1b988b88d35b755c85d103e93e1507278328bf73"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "openssl"
+  depends_on "cmake" => :build
 
   def install
-    system "./bootstrap"
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "cmake", *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/unshield -V")
   end
 end
