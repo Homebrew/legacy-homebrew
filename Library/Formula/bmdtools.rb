b653bbf01739ade2fb7162ad28c4d663ecccb182
Dave Rice <dave@dericed.com>
2015-10-05 19:18:18 -0400

    cleanup based on --audit recommendations
    double quotes, spacing, tar.gz>zip, https>http, added descriptions

--- Library/Formula/bmdtools.rb
+++ Library/Formula/bmdtools.rb
@@ -1,17 +1,18 @@
 class Bmdtools < Formula
-  homepage 'http://github.com/lu-zero/bmdtools'
-  url 'https://github.com/lu-zero/bmdtools/archive/v0.1.zip'
-  sha256 'd79b4a9e4a1e6c5e4b54847015af6ff9f19ace586d50ce95068d790ebcbb79ec'
-  head 'https://github.com/lu-zero/bmdtools.git'
+  desc "Basic capture and play programs for Blackmagic Design Decklink"
+  homepage "https://github.com/lu-zero/bmdtools"
+  url "https://github.com/lu-zero/bmdtools/archive/v0.1.tar.gz"
+  sha256 "5576bb626fe7a69a54decb7eade7ab4254228cba333eebc292ffcbc492ef142f"
+  head "https://github.com/lu-zero/bmdtools.git"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'decklinksdk' => :build
-  depends_on 'ffmpeg'
- 
+  depends_on "pkg-config" => :build
+  depends_on "decklinksdk" => :build
+  depends_on "ffmpeg"
+
   def install
-     system "make", "SDK_PATH=#{Formula["decklinksdk"].opt_include}"
-     bin.install 'bmdcapture'
-     bin.install 'bmdgenlock'
-     bin.install 'bmdplay'
+    system "make", "SDK_PATH=#{Formula["decklinksdk"].opt_include}"
+    bin.install "bmdcapture"
+    bin.install "bmdgenlock"
+    bin.install "bmdplay"
   end
 end
--- Library/Formula/deckcontrol.rb
+++ Library/Formula/deckcontrol.rb
@@ -1,13 +1,14 @@
 class Deckcontrol < Formula
-    homepage 'https://github.com/bavc/deckcontrol'
-    url "https://github.com/bavc/deckcontrol/archive/v0.3.zip"
-    sha256 "6d84028726abc4f68c0b122a48cfea261e6bbe8e642ca66a71285ccb500c4933"
-    head 'https://github.com/bavc/deckcontrol.git'
+  desc "Control an attached videodeck via Blackmagic Design Decklink SDK"
+  homepage "https://github.com/bavc/deckcontrol"
+  url "https://github.com/bavc/deckcontrol/archive/v0.3.tar.gz"
+  sha256 "3fe68d35a29051f81d997d4e28a4775f7b3c7899bdc87d60877c554fd1d4c816"
+  head "https://github.com/bavc/deckcontrol.git"
 
-    depends_on 'decklinksdk' => :build
+  depends_on "decklinksdk" => :build
 
-    def install
-        system "make"
-        bin.install 'deckcontrol'
-    end
+  def install
+    system "make"
+    bin.install "deckcontrol"
+  end
 end
