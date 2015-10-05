32ea7c8ee7e248d4620641e14a70f8a95dfc6116
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-05 21:53:44 +0100

    acpica: update 20150717 bottle.

--- Library/Formula/acpica.rb
+++ Library/Formula/acpica.rb
@@ -6,10 +6,11 @@ class Acpica < Formula
   head "https://github.com/acpica/acpica.git"
 
   bottle do
-    cellar :any
-    sha256 "7fbe5d6e05d227b549b50469b6bc33713fbc82781db6c9491069667393e2b135" => :yosemite
-    sha256 "a484e1f4d6750e2a99ccba5680696da9f3bca0dbe47053bc37e609100afb3db3" => :mavericks
-    sha256 "5fcda590ea6dca0191e58604eedb6a03318964adf175502e56eda34174295628" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "cc725e69697566a45bc3398020ed869428096585f1a719c327cca44179bb4ab6" => :el_capitan
+    sha256 "97f48c365f61d024a47dc37d074bf12b3946b006e4f047f6b06ab47e98c58dfe" => :yosemite
+    sha256 "2dee2a8911b99c8cc52b1534d9f6f0e87c04521d81a1b89c5ac46f7a149cd4db" => :mavericks
   end
 
   def install
