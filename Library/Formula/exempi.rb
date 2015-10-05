aed333811d4b78d7a60a09357b93caa4b6725a4f
Andrew Janke <andrew@apjanke.net>
2015-10-05 09:37:41 -0400

    audit fixes: E 04: evas-generic-loaders - exodriver

--- Library/Formula/exempi.rb
+++ Library/Formula/exempi.rb
@@ -1,6 +1,6 @@
 class Exempi < Formula
   desc "Library to parse XMP metadata"
-  homepage "http://libopenraw.freedesktop.org/wiki/Exempi"
+  homepage "https://wiki.freedesktop.org/libopenraw/Exempi/"
   url "http://libopenraw.freedesktop.org/download/exempi-2.2.2.tar.bz2"
   sha256 "0e7ad0e5e61b6828e38d31a8cc59c26c9adeed7edf4b26708c400beb6a686c07"
 
