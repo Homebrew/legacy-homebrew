ee1683e1bea13c9932eb93608dafd808ebbd8dbe
Andrew Janke <andrew@apjanke.net>
2015-10-05 08:44:55 -0400

    audit fixes: C 06: csup - cups-pdf

--- Library/Formula/ctags.rb
+++ Library/Formula/ctags.rb
@@ -5,6 +5,15 @@ class Ctags < Formula
   sha256 "0e44b45dcabe969e0bbbb11e30c246f81abe5d32012db37395eb57d66e9e99c7"
   revision 1
 
+  stable do
+    # also fixes http://sourceforge.net/tracker/?func=detail&aid=3247256&group_id=6556&atid=106556
+    # merged upstream but not yet in stable
+    patch :p2 do
+      url "https://gist.githubusercontent.com/naegelejd/9a0f3af61954ae5a77e7/raw/16d981a3d99628994ef0f73848b6beffc70b5db8/Ctags%20r782"
+      sha256 "26d196a75fa73aae6a9041c1cb91aca2ad9d9c1de8192fce8cdc60e4aaadbcbb"
+    end
+  end
+
   bottle do
     cellar :any_skip_relocation
     sha256 "e1582f148434de71bfa2516f6fad0598b41115f21164ad59c847e3282d550586" => :el_capitan
@@ -20,15 +29,6 @@ class Ctags < Formula
 
   # fixes http://sourceforge.net/tracker/?func=detail&aid=3247256&group_id=6556&atid=106556
   patch :p2, :DATA
-
-  stable do
-    # also fixes http://sourceforge.net/tracker/?func=detail&aid=3247256&group_id=6556&atid=106556
-    # merged upstream but not yet in stable
-    patch :p2 do
-      url "https://gist.githubusercontent.com/naegelejd/9a0f3af61954ae5a77e7/raw/16d981a3d99628994ef0f73848b6beffc70b5db8/Ctags%20r782"
-      sha256 "26d196a75fa73aae6a9041c1cb91aca2ad9d9c1de8192fce8cdc60e4aaadbcbb"
-    end
-  end
 
   def install
     if build.head?
