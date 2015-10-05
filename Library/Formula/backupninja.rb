068a326b7cfdd47672b18c79980619658cca6884
Andrew Janke <andrew@apjanke.net>
2015-10-05 08:03:06 -0400

    audit fixes: B: backupninja - bigdata
    audit fixes for: backupninja, bashdb, bats, bfg, bgpdump, bgrep, bigdata
    
    Closes #44690.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/backupninja.rb
+++ Library/Formula/backupninja.rb
@@ -1,9 +1,9 @@
 class Backupninja < Formula
   desc "Backup automation tool"
   homepage "https://labs.riseup.net/code/projects/backupninja"
-  head "git://labs.riseup.net/backupninja.git"
   url "https://labs.riseup.net/code/attachments/download/275/backupninja-1.0.1.tar.gz"
   sha256 "10fa5dbcd569a082b8164cd30276dd04a238c7190d836bcba006ea3d1235e525"
+  head "git://labs.riseup.net/backupninja.git"
 
   depends_on "dialog"
   depends_on "gawk"
--- Library/Formula/bashdb.rb
+++ Library/Formula/bashdb.rb
@@ -2,8 +2,8 @@ class Bashdb < Formula
   desc "Bash shell debugger"
   homepage "http://bashdb.sourceforge.net"
   url "https://downloads.sourceforge.net/project/bashdb/bashdb/4.3-0.91/bashdb-4.3-0.91.tar.bz2"
-  sha256 "60117745813f29070a034c590c9d70153cc47f47024ae54bfecdc8cd86d9e3ea"
   version "4.3-0.91"
+  sha256 "60117745813f29070a034c590c9d70153cc47f47024ae54bfecdc8cd86d9e3ea"
 
   depends_on "bash"
   depends_on :macos => :mountain_lion
