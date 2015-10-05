1176224b8e13c86c9c84f60ca8c074bfcf8453d4
Andrew Janke <andrew@apjanke.net>
2015-10-05 09:17:43 -0400

    formulae: various audit fixes
    Closes #44768.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/dvorak7min.rb
+++ Library/Formula/dvorak7min.rb
@@ -2,8 +2,8 @@ class Dvorak7min < Formula
   desc "Dvorak (simplified keyboard layout) typing tutor"
   homepage "http://dvorak7min.sourcearchive.com/"
   url "http://ftp.de.debian.org/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
-  sha256 "4cdef8e4c8c74c28dacd185d1062bfa752a58447772627aded9ac0c87a3b8797"
   version "1.6.1"
+  sha256 "4cdef8e4c8c74c28dacd185d1062bfa752a58447772627aded9ac0c87a3b8797"
 
   bottle do
     cellar :any
