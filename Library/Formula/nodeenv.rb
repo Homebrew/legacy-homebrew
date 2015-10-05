33c767f0f29cf6831f40b1ddf473481f7a4607dc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-05 05:50:34 +0100

    nodeenv: add 0.13.6 bottle.

--- Library/Formula/nodeenv.rb
+++ Library/Formula/nodeenv.rb
@@ -4,6 +4,13 @@ class Nodeenv < Formula
   url "https://pypi.python.org/packages/source/n/nodeenv/nodeenv-0.13.6.tar.gz"
   sha256 "feaafb0486d776360ef939bd85ba34cff9b623013b13280d1e3770d381ee2b7f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dd60c53b8476a285f79b85bc363a30b06bffd164d2e4daf2ed3d2078bf5b04cb" => :el_capitan
+    sha256 "f21ca5ce7ccf6d681c890161e980194f2cde92b0976a0420b8ad86baafa9f12b" => :yosemite
+    sha256 "96f6bb2453b18615c4b5201e15b7c81ffa541888ce73ad660982824424d86759" => :mavericks
+  end
+
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
     system "python", *Language::Python.setup_install_args(libexec)
