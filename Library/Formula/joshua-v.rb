b477d59d37f09df37bcd592f843128e97b4e51a4
Lewis John McGibbney <lewis.j.mcgibbney@jpl.nasa.gov>
2015-10-04 23:37:41 -0700

    Formular for Joshua Decoder v6.0.4

--- Library/Formula/joshua-v.rb
+++ Library/Formula/joshua-v.rb
@@ -11,16 +11,22 @@ class JoshuaV < Formula
 
   depends_on "ant" => :build
   depends_on "boost" => :build
+  depends_on "md5sha1sum" => :build
   depends_on :python => :build if MacOS.version <= :snow_leopard
 
   resource "es-en-phrase-pack" do
     url "http://cs.jhu.edu/~post/language-packs/language-pack-es-en-phrase-2015-03-06.tgz"
-    sha256 "213E05BBDCFBFA05B31E263C31F10A0315695FEE26C2F37B0A78FB918BAD9B5D"
+    sha256 "213e05bbdcfbfa05b31e263c31f10a0315695fee26C2f37b0a78fb918bad9b5d"
   end
 
   resource "ar-en-phrase-pack" do
     url "http://cs.jhu.edu/~post/language-packs/language-pack-ar-en-phrase-2015-03-18.tgz"
-    sha256 "2B6665B58B11E4C25D48191D3D5B62B7C591851A9767B14F9CCEBF1951FDDF90"
+    sha256 "2b6665b58b11e4c25d48191d3d5b62b7c591851a9767b14f9ccebf1951fddf90"
+  end
+
+  resource "indian-parallel-corpora" do
+    url "https://github.com/joshua-decoder/indian-parallel-corpora/archive/1.0.tar.gz"
+    sha256 "b6472648d9c13d72682554fd2434e2dc124433b6f6d9f39c16859759f30720c6"
   end
 
   def install
@@ -33,13 +39,11 @@ class JoshuaV < Formula
     elsif build.with? "ar-en-phrase-pack"
       resource("ar-en-phrase-pack").stage { share.install "language-pack-ar-en-phrase-2015-03-18", share/"joshualanguagepacks" }
     end
+    prefix.install Dir["*"]
   end
 
   test do    
-    system "mkdir -p ~/models/bn-en"
-    system "cd ~/models/bn-en"
-    system "curl -L https://github.com/joshua-decoder/indian-parallel-corpora/tarball/master > indian-languages.tgz"
-    system "tar xf indian-languages.tgz"
+    resource("indian-parallel-corpora").stage { share.install "indian-parallel-corpora-1.0", share/"corpora" }
     system "ln -s joshua-decoder-indian-parallel-corpora-* input"
     system "$JOSHUA/bin/pipeline.pl --source bn --target en \
     --no-prepare --aligner berkeley \
