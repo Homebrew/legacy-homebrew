526f5786536fe9ecbb2a1d0b3104ac7f7ee61f84
Andrew Janke <andrew@apjanke.net>
2015-10-05 08:42:14 -0400

    audit fixes: C 03: chruby-fish - clockywock

--- Library/Formula/chruby-fish.rb
+++ Library/Formula/chruby-fish.rb
@@ -1,9 +1,9 @@
 class ChrubyFish < Formula
   desc "Thin wrapper around chruby to make it work with the Fish shell"
   homepage "https://github.com/JeanMertz/chruby-fish#readme"
-  head "https://github.com/JeanMertz/chruby-fish.git"
   url "https://github.com/JeanMertz/chruby-fish/archive/v0.6.0.tar.gz"
   sha256 "9549a195bf2ffc2d613124f891253c85a1e17066fa100f5bf423537dffd7e6d8"
+  head "https://github.com/JeanMertz/chruby-fish.git"
 
   depends_on "chruby" => :recommended
 
