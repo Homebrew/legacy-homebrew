e6f1500728e48f4be59bb4ca2b476198f6559840
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-05 21:57:56 +0100

    apache-forrest: add 0.9 bottle.

--- Library/Formula/apache-forrest.rb
+++ Library/Formula/apache-forrest.rb
@@ -4,6 +4,13 @@ class ApacheForrest < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=forrest/apache-forrest-0.9-sources.tar.gz"
   sha256 "c6ac758db2eb0d4d91bd1733bbbc2dec4fdb33603895c464bcb47a34490fb64d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "615ab5a39fcc19a110ebab166c05a614149374e65c5f9a7a4522fa400b8d4118" => :el_capitan
+    sha256 "cd0e4ceeb9e01118fe69bee3f80ccff63a951e01bf76c87146185ae6fe474c09" => :yosemite
+    sha256 "a0e06c41204932f1427e38d47b3c4442dea7f7c3312f959faf1b725d35d85a52" => :mavericks
+  end
+
   depends_on :java
 
   resource "deps" do
