a42c0acc659980446e1023419b77ab814834ca83
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-06 19:06:31 +0200

    jcal: use https homepage
    Closes #44675.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/jcal.rb
+++ Library/Formula/jcal.rb
@@ -1,6 +1,6 @@
 class Jcal < Formula
   desc "UNIX-cal-like tool to display Jalali calendar"
-  homepage "http://savannah.nongnu.org/projects/jcal/"
+  homepage "https://savannah.nongnu.org/projects/jcal/"
   url "http://download.savannah.gnu.org/releases/jcal/jcal-0.4.1.tar.gz"
   sha256 "e8983ecad029b1007edc98458ad13cd9aa263d4d1cf44a97e0a69ff778900caa"
 
