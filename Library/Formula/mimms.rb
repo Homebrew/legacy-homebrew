b79ab5c01147783946586d51f17838d91850af48
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-06 19:05:42 +0200

    mimms: use https urls

--- Library/Formula/mimms.rb
+++ Library/Formula/mimms.rb
@@ -1,6 +1,6 @@
 class Mimms < Formula
   desc "Mms stream downloader"
-  homepage "http://savannah.nongnu.org/projects/mimms"
+  homepage "https://savannah.nongnu.org/projects/mimms"
   url "https://launchpad.net/mimms/trunk/3.2.1/+download/mimms-3.2.1.tar.bz2"
   sha256 "92cd3e1800d8bd637268274196f6baec0d95aa8e709714093dd96ba8893c2354"
 
@@ -8,7 +8,7 @@ class Mimms < Formula
   depends_on "libmms"
 
   # Switch shared library loading to Mach-O naming convention (.dylib)
-  # Matching upstream bug report: http://savannah.nongnu.org/bugs/?29684
+  # Matching upstream bug report: https://savannah.nongnu.org/bugs/?29684
   patch :DATA
 
   def install
