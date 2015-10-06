912ab6c29f19e713227be44d9d54b854f0001a02
Kirill A. Korinsky <kirill@korins.ky>
2015-10-06 12:30:34 +0400

    Use https, shortest description and add test block

--- Library/Formula/docutils.rb
+++ Library/Formula/docutils.rb
@@ -1,11 +1,14 @@
 class Docutils < Formula
-  desc "Text processing system for processing plaintext documentation into useful formats"
+  desc "Text processing system for reStructuredText"
   homepage "http://docutils.sourceforge.net"
-  url "http://downloads.sourceforge.net/project/docutils/docutils/0.12/docutils-0.12.tar.gz"
+  url "https://downloads.sourceforge.net/project/docutils/docutils/0.12/docutils-0.12.tar.gz"
   sha256 "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa"
 
   def install
     system "python", "setup.py", "install", "--prefix=#{prefix}"
   end
-end
 
+  test do
+    system "#{bin}/rst2man.py", "#{prefix}/HISTORY.txtx"
+  end
+end
