bec6504a43d1caab393a5734b85b3846ea0a174e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-07 18:39:33 +0100

    autocode: add 0.20.3 bottle.

--- Library/Formula/autocode.rb
+++ Library/Formula/autocode.rb
@@ -4,6 +4,13 @@ class Autocode < Formula
   url "https://github.com/crystal/autocode/archive/v0.20.3.tar.gz"
   sha256 "f82f0fa7a2573d5f939a4821769756719ff98a98b0f1903001ee1e3981b30a23"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5aac71c4a2e4f3e084568de4757822c5f1aaa733db7b36a0b187151516499bac" => :el_capitan
+    sha256 "686a3de9b0eba6088f4fe2d130e76c75edb4233960db2daa615a8f5b5b99e590" => :yosemite
+    sha256 "e613bae16cfbe747f3e7ae43c4e1d34a9cbe736610641063737adfe788897dab" => :mavericks
+  end
+
   depends_on "node"
 
   def install
