2f0017a4160d4742685b3dbaf6822f60dcb240f0
Andrew Janke <andrew@apjanke.net>
2015-10-07 02:37:28 -0400

    libtomcrypt: add head
    Closes #44697.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libtomcrypt.rb
+++ Library/Formula/libtomcrypt.rb
@@ -5,6 +5,7 @@ class Libtomcrypt < Formula
   url "http://libtom.org/files/crypt-1.17.tar.bz2"
   mirror "https://distfiles.macports.org/libtomcrypt/crypt-1.17.tar.bz2"
   sha256 "e33b47d77a495091c8703175a25c8228aff043140b2554c08a3c3cd71f79d116"
+  head "https://github.com/libtom/libtomcrypt.git"
 
   bottle do
     cellar :any
