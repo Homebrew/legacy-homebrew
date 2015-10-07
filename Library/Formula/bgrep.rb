82fe44abc5e3ac277f9516734e0d6299c2e980b9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-07 06:43:03 +0100

    bgrep: add 0.2 bottle.

--- Library/Formula/bgrep.rb
+++ Library/Formula/bgrep.rb
@@ -4,6 +4,13 @@ class Bgrep < Formula
   url "https://github.com/tmbinc/bgrep/archive/bgrep-0.2.tar.gz"
   sha256 "24c02393fb436d7a2eb02c6042ec140f9502667500b13a59795388c1af91f9ba"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "29f0b2d7ab307eae228a03d4f42f677d9ff0884edc5c96771da36182cb592cd2" => :el_capitan
+    sha256 "af4dab94130c48930d064074da8492c5531842a348747b0dd39420db738f6ae9" => :yosemite
+    sha256 "b166d637dda09833c3b2c3396670347b087fef6366576303f87fb704b1d3eede" => :mavericks
+  end
+
   def install
     system ENV.cc, ENV.cflags, "-o", "bgrep", "bgrep.c"
     bin.install "bgrep"
