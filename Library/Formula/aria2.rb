673ecff78f8634c53d6680c428204c8b961a5d56
187j3x1 <187j3x1@gmail.com>
2015-10-08 14:51:24 +0800

    aria2 1.19.2
    Closes #44734.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/aria2.rb
+++ Library/Formula/aria2.rb
@@ -1,8 +1,8 @@
 class Aria2 < Formula
   desc "Download with resuming and segmented downloading"
   homepage "http://aria2.sourceforge.net/"
-  url "https://github.com/tatsuhiro-t/aria2/releases/download/release-1.19.1/aria2-1.19.1.tar.xz"
-  sha256 "f731f64940ccfc78e8777190de354ba833fe248851b935d5d8885a7917fc101f"
+  url "https://github.com/tatsuhiro-t/aria2/releases/download/release-1.19.2/aria2-1.19.2.tar.xz"
+  sha256 "3605486dd495cd8c2f672b7d0b763397989d831396862f15730697ebcf0ad53e"
 
   bottle do
     cellar :any_skip_relocation
