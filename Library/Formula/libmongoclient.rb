e893beffc8fa9b10fb65d6b64cbca9a562b469df
Dominyk Tiller <dominyktiller@gmail.com>
2015-10-08 05:25:19 +0100

    libmongoclient 2.6.11
    Closes #44724.
    
    Closes #44733.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libmongoclient.rb
+++ Library/Formula/libmongoclient.rb
@@ -1,10 +1,18 @@
 class Libmongoclient < Formula
   desc "C and C++ driver for MongoDB"
   homepage "https://www.mongodb.org"
-  url "https://github.com/mongodb/mongo-cxx-driver/archive/legacy-0.0-26compat-2.6.9.tar.gz"
-  sha256 "fcbc8032afe7e3a45464aacf6ef34cfb7a3cf2afdd2a09d7cdaf23f6c7a24376"
-
   head "https://github.com/mongodb/mongo-cxx-driver.git", :branch => "26compat"
+
+  stable do
+    url "https://github.com/mongodb/mongo-cxx-driver/archive/legacy-0.0-26compat-2.6.11.tar.gz"
+    sha256 "b73ffaab09dca318cf286d607fb8b8aa8ace0ab08cef77637d58d33d1c449f2c"
+
+    # Adds recognition for OS X 10.11.
+    patch do
+      url "https://github.com/mongodb/mongo-cxx-driver/commit/3fe5139.diff"
+      sha256 "3ba04f8b4155ef9f06b91c077a89ae863d06959dec30d787551e30f41680c7a5"
+    end
+  end
 
   bottle do
     sha256 "8a627a9d8146d29946db5c1835a38266af2e2080d947917c45d165759d5f6eb7" => :yosemite
@@ -39,7 +47,7 @@ class Libmongoclient < Formula
       # --osx-version-min is required to override --osx-version-min=10.6 added
       # by SConstruct which causes "invalid deployment target for -stdlib=libc++"
       # when using libc++
-      "--osx-version-min=#{MacOS.version}"
+      "--osx-version-min=#{MacOS.version}",
     ]
 
     args << "--libc++" if MacOS.version >= :mavericks
