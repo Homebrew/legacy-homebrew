4edc1c28229adedbb16bda7ff9e4257d21bb28a6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-08 18:20:08 +0100

    fluent-bit: add 0.2.0 bottle.

--- Library/Formula/fluent-bit.rb
+++ Library/Formula/fluent-bit.rb
@@ -6,6 +6,13 @@ class FluentBit < Formula
 
   head "https://github.com/fluent/fluent-bit.git"
 
+  bottle do
+    cellar :any
+    sha256 "322c8a2b7ea5cf42a6d5b7e770b8310519f541b8fe22f4ae1ca40a81963f739b" => :el_capitan
+    sha256 "0ac92206415b3da97ac1c11c68b952302b59aba4abaa7b90737e5d6de2211102" => :yosemite
+    sha256 "39561b29681e1d8f6a390da38d6be4fcc58fdda661ffbfaf319ca03eda53dafb" => :mavericks
+  end
+
   depends_on "cmake" => :build
 
   def install
