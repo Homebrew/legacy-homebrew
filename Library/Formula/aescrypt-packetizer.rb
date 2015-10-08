88aecca648108eebb882555d21792bc04223abc4
Andrew Janke <andrew@apjanke.net>
2015-10-07 22:45:25 -0400

    aescrypt-packetizer: minor brew audit fix

--- Library/Formula/aescrypt-packetizer.rb
+++ Library/Formula/aescrypt-packetizer.rb
@@ -19,9 +19,9 @@ class AescryptPacketizer < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on :xcode => :build
-
   option "with-default-names", "Build with the binaries named as expected upstream"
+
+  depends_on :xcode => :build
 
   def install
     if build.head?
