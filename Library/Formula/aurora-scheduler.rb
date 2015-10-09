83a694df1fc33e51f76245cb33d5fde431254ec8
thinker0 <thinker0@gmail.com>
2015-10-09 22:22:58 +0900

    .

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -4,6 +4,7 @@ class AuroraScheduler < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=aurora/0.9.0/apache-aurora-0.9.0.tar.gz"
   sha256 "16040866f3a799226452b1541892eb80ed3c61f47c33f1ccb0687fb5cf82767c"
 
+  depends_on "gradle" => :build
   depends_on "mesos"
   depends_on :java => "1.8+"
 
@@ -12,6 +13,10 @@ class AuroraScheduler < Formula
   end
 
   def install
+    remove_file("buildSrc/build.gradle", true)
+    touch("buildSrc/build.gradle")
+    system "gradle"
+    system "gradle", "installDist"
     ENV["LC_ALL"] = "en_US.UTF-8"
     ENV["CFLAGS"] = "-Qunused-arguments"
     ENV["CPPFLAGS"] = "-Qunused-arguments"
@@ -21,11 +26,8 @@ class AuroraScheduler < Formula
     system "./pants", "binary", "src/main/python/apache/aurora/tools:thermos_observer"
     system "./pants", "binary", "src/main/python/apache/thermos/bin:thermos_runner"
     system "./build-support/release/make-python-sdists"
-    system "./build-support/embed_runner_in_executor.py"
-    libexec.install Dir["dist/install/aurora-scheduler/*"]
     bin.install "dist/kaurora.pex" => "aurora"
     bin.install "dist/kaurora_admin.pex" => "aurora_admin"
-    libexec.install "dist/thermos_executor.pex" => "bin/thermos_executor"
     bin.install "dist/thermos_runner.pex" => "thermos_runner"
     bin.install "dist/thermos_observer.pex" => "thermos_observer"
   end
@@ -33,6 +35,7 @@ class AuroraScheduler < Formula
   test do
     mkdir_p("~/.aurora/")
     touch("~/.aurora/clusters.json")
+    File.open("~/.aurora/clusters.json", 'w') {|f| f.write("[{ \"name\": \"devcluster\", \"slave_root\": \"/tmp/mesos/\", \"zk\": \"127.0.0.1\", \"scheduler_zk_path\": \"/aurora/scheduler\", \"auth_mechanism\": \"UNAUTHENTICATED\" }]") }
     system "#{bin}/aurora_admin", "get_cluster_config", "devcluster"
   end
 end
