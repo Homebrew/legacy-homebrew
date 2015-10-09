7d86aa82ecc59ae4f350375254537a4c444a58a3
thinker0 <thinker0@gmail.com>
2015-10-09 15:43:47 +0900

    grade build error
    http://bot.brew.sh/job/Homebrew%20Pull%20Requests/34468/version=el_capitan/console

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -4,7 +4,6 @@ class AuroraScheduler < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=aurora/0.9.0/apache-aurora-0.9.0.tar.gz"
   sha256 "16040866f3a799226452b1541892eb80ed3c61f47c33f1ccb0687fb5cf82767c"
 
-  depends_on "gradle"
   depends_on "mesos"
   depends_on :java => "1.8+"
 
@@ -13,10 +12,6 @@ class AuroraScheduler < Formula
   end
 
   def install
-    remove_file("buildSrc/build.gradle", true)
-    touch("buildSrc/build.gradle")
-    system "gradle"
-    system "gradle", "installDist"
     ENV["LC_ALL"] = "en_US.UTF-8"
     ENV["CFLAGS"] = "-Qunused-arguments"
     ENV["CPPFLAGS"] = "-Qunused-arguments"
