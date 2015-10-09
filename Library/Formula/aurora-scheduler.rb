27b818446cec973a8ed338c757d53ba662822737
thinker0 <thinker0@gmail.com>
2015-10-09 14:46:51 +0900

    .

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -5,9 +5,12 @@ class AuroraScheduler < Formula
   sha256 "16040866f3a799226452b1541892eb80ed3c61f47c33f1ccb0687fb5cf82767c"
 
   depends_on "gradle"
-  depends_on "zookeeper"
   depends_on "mesos"
   depends_on :java => "1.8+"
+
+  def pour_bottle?
+    quiet_system("/usr/libexec/java_home --version 1.8 --failfast")
+  end
 
   def install
     remove_file("buildSrc/build.gradle", true)
