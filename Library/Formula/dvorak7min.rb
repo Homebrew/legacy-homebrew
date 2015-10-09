4d278602fbc3c7a70557fb7425ead5e885a8ecf3
Dominyk Tiller <dominyktiller@gmail.com>
2015-10-09 18:38:53 +0100

    dvorak7min: use secure links
    Closes #44794.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/dvorak7min.rb
+++ Library/Formula/dvorak7min.rb
@@ -1,7 +1,8 @@
 class Dvorak7min < Formula
   desc "Dvorak (simplified keyboard layout) typing tutor"
   homepage "http://dvorak7min.sourcearchive.com/"
-  url "http://ftp.de.debian.org/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/d/dvorak7min/dvorak7min_1.6.1+repack.orig.tar.gz"
   version "1.6.1"
   sha256 "4cdef8e4c8c74c28dacd185d1062bfa752a58447772627aded9ac0c87a3b8797"
 
