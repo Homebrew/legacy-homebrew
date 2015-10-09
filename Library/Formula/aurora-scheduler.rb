967b7769a8d7b2e4835abeff1ef8035bcf43ea31
thinker0 <thinker0@gmail.com>
2015-10-09 21:45:49 +0900

    .

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -31,6 +31,8 @@ class AuroraScheduler < Formula
   end
 
   test do
+    mkdir_p("~/.aurora/")
+    touch("~/.aurora/clusters.json")
     system "#{bin}/aurora_admin", "get_cluster_config", "devcluster"
   end
 end
