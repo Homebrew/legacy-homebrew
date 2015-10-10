dd52a27607448de0026681d9515dc723eee5c8fd
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-10 12:00:03 +0200

    dnsperf: use secure homepage url and follow redir

--- Library/Formula/dnsperf.rb
+++ Library/Formula/dnsperf.rb
@@ -1,6 +1,6 @@
 class Dnsperf < Formula
   desc "Measure DNS performance by simulating network conditions"
-  homepage "http://nominum.com/support/measurement-tools/"
+  homepage "https://nominum.com/measurement-tools/"
   url "ftp://ftp.nominum.com/pub/nominum/dnsperf/2.0.0.0/dnsperf-src-2.0.0.0-1.tar.gz"
   sha256 "23d486493f04554d11fca97552e860028f18c5ed6e35348e480a7448fa8cfaad"
 
