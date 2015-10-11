729142456a85cd07ba2b499922f7f1937abae8b8
Rob Hunter <hunter@sparemind.co>
2015-10-11 18:00:26 +1100

    innoextract 1.5
    Closes #44845.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/innoextract.rb
+++ Library/Formula/innoextract.rb
@@ -1,8 +1,8 @@
 class Innoextract < Formula
   desc "Tool to unpack installers created by Inno Setup"
   homepage "http://constexpr.org/innoextract/"
-  url "https://github.com/dscharrer/innoextract/archive/1.4.tar.gz"
-  sha256 "7f7eabd5e2f138c9739ac0df457b0f81548ed021068b2b1ec640584747887f52"
+  url "http://constexpr.org/innoextract/files/innoextract-1.5.tar.gz"
+  sha256 "f2594e992ccf2859455475794803b29a67393fadb69d4df1eec34c451ffa48cf"
   head "https://github.com/dscharrer/innoextract.git"
 
   depends_on "cmake" => :build
