82eec5ede5eed2b0519162091304b3603f21b3d2
thinker0 <thinker0@gmail.com>
2015-10-11 14:27:26 +0900

    * fine working
    brew reinstall --only-dependencies --verbose --build-bottle aurora-scheduler
    brew reinstall --verbose --build-bottle aurora-scheduler
    brew audit aurora-scheduler --strict --online
    brew bottle --verbose --rb aurora-scheduler
    brew test --verbose aurora-scheduler

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -13,15 +13,16 @@ class AuroraScheduler < Formula
   end
 
   def install
+    ENV["PIP_DEFAULT_TIMEOUT"] = "60"
     ENV["LC_ALL"] = "en_US.UTF-8"
     ENV["CFLAGS"] = "-Qunused-arguments"
     ENV["CPPFLAGS"] = "-Qunused-arguments"
+    system "./build-support/release/make-python-sdists"
     system "./pants", "binary", "src/main/python/apache/aurora/client/cli:kaurora"
     system "./pants", "binary", "src/main/python/apache/aurora/admin:kaurora_admin"
     system "./pants", "binary", "src/main/python/apache/aurora/tools:thermos"
     system "./pants", "binary", "src/main/python/apache/aurora/tools:thermos_observer"
     system "./pants", "binary", "src/main/python/apache/thermos/bin:thermos_runner"
-    system "./build-support/release/make-python-sdists"
     bin.install "dist/kaurora.pex" => "aurora"
     bin.install "dist/kaurora_admin.pex" => "aurora_admin"
     bin.install "dist/thermos_runner.pex" => "thermos_runner"
@@ -29,9 +30,16 @@ class AuroraScheduler < Formula
   end
 
   test do
-    mkdir_p("~/.aurora/")
-    touch("~/.aurora/clusters.json")
-    File.open("~/.aurora/clusters.json", "w") { |f| f.write("[{ \"name\": \"devcluster\", \"slave_root\": \"/tmp/mesos/\", \"zk\": \"127.0.0.1\", \"scheduler_zk_path\": \"/aurora/scheduler\", \"auth_mechanism\": \"UNAUTHENTICATED\" }]") }
+    ENV["AURORA_CONFIG_ROOT"] = "#{testpath}"
+    (testpath/"clusters.json").write <<-EOS.undent
+        [{
+          "name": "devcluster",
+          "slave_root": "/tmp/mesos/",
+          "zk": "172.16.64.185",
+          "scheduler_zk_path": "/aurora/scheduler",
+          "auth_mechanism": "UNAUTHENTICATED"
+        }]
+    EOS
     system "#{bin}/aurora_admin", "get_cluster_config", "devcluster"
   end
 end
