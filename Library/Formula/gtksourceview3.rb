476b7f76994fc67d59311a09128bcd3384d4a26c
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-10-11 10:55:26 +0100

    gtksourceview3 3.18.1
    Closes #44823.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/gtksourceview3.rb
+++ Library/Formula/gtksourceview3.rb
@@ -1,8 +1,8 @@
 class Gtksourceview3 < Formula
   desc "Text view with syntax, undo/redo, and text marks"
   homepage "https://projects.gnome.org/gtksourceview/"
-  url "https://download.gnome.org/sources/gtksourceview/3.18/gtksourceview-3.18.0.tar.xz"
-  sha256 "54b111264e6985e26a878dec88ff94fd0a9ae0dc4cfcdf08f4a6b5f655d4b693"
+  url "https://download.gnome.org/sources/gtksourceview/3.18/gtksourceview-3.18.1.tar.xz"
+  sha256 "7be95faf068b9f0ac7540cc1e8d607baa98a482850ef11a6471b53c9327aede6"
 
   bottle do
     sha256 "809a9a7c643c4740bd2c1c746496bce955b2611c2ec593df2f3b8856bd4099d7" => :el_capitan
@@ -14,9 +14,6 @@ class Gtksourceview3 < Formula
   depends_on "intltool" => :build
   depends_on "gettext"
   depends_on "gtk+3"
-
-  # reported upstream as https://bugzilla.gnome.org/show_bug.cgi?id=755308
-  patch :DATA
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -90,86 +87,3 @@ class Gtksourceview3 < Formula
     system "./test"
   end
 end
-
-__END__
-diff --git a/configure b/configure
-index ad97334..9faade8 100755
---- a/configure
-+++ b/configure
-@@ -12636,76 +12636,6 @@ fi
-
-
-
--for flag in          -Wl,--no-as-needed              ; do
--  as_CACHEVAR=`$as_echo "ax_cv_check_cflags_$ax_compiler_flags_test_$flag" | $as_tr_sh`
--{ $as_echo "$as_me:${as_lineno-$LINENO}: checking whether C compiler accepts $flag" >&5
--$as_echo_n "checking whether C compiler accepts $flag... " >&6; }
--if eval \${$as_CACHEVAR+:} false; then :
--  $as_echo_n "(cached) " >&6
--else
--
--  ax_check_save_flags=$CFLAGS
--  CFLAGS="$CFLAGS $ax_compiler_flags_test $flag"
--  cat confdefs.h - <<_ACEOF >conftest.$ac_ext
--/* end confdefs.h.  */
--
--int
--main ()
--{
--
--  ;
--  return 0;
--}
--_ACEOF
--if ac_fn_c_try_compile "$LINENO"; then :
--  eval "$as_CACHEVAR=yes"
--else
--  eval "$as_CACHEVAR=no"
--fi
--rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
--  CFLAGS=$ax_check_save_flags
--fi
--eval ac_res=\$$as_CACHEVAR
--	       { $as_echo "$as_me:${as_lineno-$LINENO}: result: $ac_res" >&5
--$as_echo "$ac_res" >&6; }
--if eval test \"x\$"$as_CACHEVAR"\" = x"yes"; then :
--
--if ${WARN_LDFLAGS+:} false; then :
--
--  case " $WARN_LDFLAGS " in #(
--  *" $flag "*) :
--    { { $as_echo "$as_me:${as_lineno-$LINENO}: : WARN_LDFLAGS already contains \$flag"; } >&5
--  (: WARN_LDFLAGS already contains $flag) 2>&5
--  ac_status=$?
--  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
--  test $ac_status = 0; } ;; #(
--  *) :
--
--     as_fn_append WARN_LDFLAGS " $flag"
--     { { $as_echo "$as_me:${as_lineno-$LINENO}: : WARN_LDFLAGS=\"\$WARN_LDFLAGS\""; } >&5
--  (: WARN_LDFLAGS="$WARN_LDFLAGS") 2>&5
--  ac_status=$?
--  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
--  test $ac_status = 0; }
--     ;;
--esac
--
--else
--
--  WARN_LDFLAGS=$flag
--  { { $as_echo "$as_me:${as_lineno-$LINENO}: : WARN_LDFLAGS=\"\$WARN_LDFLAGS\""; } >&5
--  (: WARN_LDFLAGS="$WARN_LDFLAGS") 2>&5
--  ac_status=$?
--  $as_echo "$as_me:${as_lineno-$LINENO}: \$? = $ac_status" >&5
--  test $ac_status = 0; }
--
--fi
--
--else
--  :
--fi
--
--done
-
-
-     if test "$ax_enable_compile_warnings" != "no"; then :
