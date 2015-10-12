8cdd7bbe55fe61e6c4fe48aade4a382b3901f64c
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:39:17 +0200

    libpuzzle: use an https url

--- Library/Formula/libpuzzle.rb
+++ Library/Formula/libpuzzle.rb
@@ -1,7 +1,7 @@
 class Libpuzzle < Formula
   desc "Library to find visually similar images"
   homepage "http://libpuzzle.pureftpd.org/project/libpuzzle"
-  url "http://download.pureftpd.org/pub/pure-ftpd/misc/libpuzzle/releases/libpuzzle-0.11.tar.bz2"
+  url "https://download.pureftpd.org/pub/pure-ftpd/misc/libpuzzle/releases/libpuzzle-0.11.tar.bz2"
   sha256 "ba628268df6956366cbd44ae48c3f1bab41e70b4737041a1f33dac9832c44781"
 
   bottle do
