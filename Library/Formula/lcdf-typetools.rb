75c7ad5341d36531162e56fabd9d2f43c3ae7563
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:39:07 +0200

    lcdf-typetools: use an https url

--- Library/Formula/lcdf-typetools.rb
+++ Library/Formula/lcdf-typetools.rb
@@ -1,7 +1,7 @@
 class LcdfTypetools < Formula
   desc "Manipulate OpenType and multiple-master fonts"
   homepage "http://www.lcdf.org/type/"
-  url "http://www.lcdf.org/type/lcdf-typetools-2.104.tar.gz"
+  url "https://www.lcdf.org/type/lcdf-typetools-2.104.tar.gz"
   sha256 "d7985458ead0850cb9549ff1d619ffc18da5d7be892be5e1fce6048d510f0fff"
 
   conflicts_with "open-mpi", :because => "both install same set of binaries."
