18d508c3832dfde1a5d20609edc64663210599a8
Realignist <realignist@gmail.com>
2015-10-12 17:21:27 +0900

    repo 1.22
    Closes #44842.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/repo.rb
+++ Library/Formula/repo.rb
@@ -1,10 +1,9 @@
 class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
-  url "https://raw.githubusercontent.com/android/tools_repo/v1.12.21/repo"
-  version "1.21"
-  sha256 "44d1ac8b948e6154a495469dad05870dc6182712c0610da8d255b5471dd5bcb9"
-  revision 1
+  url "https://raw.githubusercontent.com/android/tools_repo/v1.12.32/repo"
+  version "1.22"
+  sha256 "9907c36d63cf5222d73ab270fee5249dc84f6b4580cda03ddd7ac309f11b289e"
 
   def install
     bin.install "repo"
