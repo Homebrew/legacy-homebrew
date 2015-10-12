bcb1c45a816725813ed8a04aac9e54f6eeb69f66
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-12 19:48:37 +0200

    innoextract: use secure urls
    Closes #44856.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/innoextract.rb
+++ Library/Formula/innoextract.rb
@@ -1,7 +1,7 @@
 class Innoextract < Formula
   desc "Tool to unpack installers created by Inno Setup"
-  homepage "http://constexpr.org/innoextract/"
-  url "http://constexpr.org/innoextract/files/innoextract-1.5.tar.gz"
+  homepage "https://constexpr.org/innoextract/"
+  url "https://constexpr.org/innoextract/files/innoextract-1.5.tar.gz"
   sha256 "f2594e992ccf2859455475794803b29a67393fadb69d4df1eec34c451ffa48cf"
   head "https://github.com/dscharrer/innoextract.git"
 
