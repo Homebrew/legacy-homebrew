b8dcdb18cb85273a91db2c825157e0ff815ae350
Benjamin Tan <tanbamboo@gmail.com>
2015-10-12 15:12:22 +0800

    Fixed bug when travis do ci
    move head below sha256.
    add test

--- Library/Formula/flink.rb
+++ Library/Formula/flink.rb
@@ -1,10 +1,10 @@
 class Flink < Formula
   desc "Platform for scalable batch and stream data processing"
   homepage "https://flink.apache.org"
-  head "https://github.com/apache/flink.git"
   url "https://www.apache.org/dyn/closer.cgi?path=flink/flink-0.9.0/flink-0.9.0-bin-hadoop2.tgz"
   version "0.9.0"
   sha256 "1edf33d154b863dd108ba618ed3b7dce3fce6c116b45368aef164b47779b65f9"
+  head "https://github.com/apache/flink.git"
 
   def install
     rm_f Dir["bin/*.cmd"]
@@ -14,5 +14,8 @@ class Flink < Formula
   end
 
   test do
+    system "#{bin}/start-local.sh"
+    system "#{bin}/flink list"
+    system "#{bin}/stop-local.sh"
   end
 end
