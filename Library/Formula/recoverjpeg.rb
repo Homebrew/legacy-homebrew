920b7634b7c586efaf3a25ddfa12772133fa19fd
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:40:08 +0200

    recoverjpeg: use an https url

--- Library/Formula/recoverjpeg.rb
+++ Library/Formula/recoverjpeg.rb
@@ -1,7 +1,7 @@
 class Recoverjpeg < Formula
   desc "Tool to recover JPEG images from a file system image"
   homepage "http://www.rfc1149.net/devel/recoverjpeg.html"
-  url "http://www.rfc1149.net/download/recoverjpeg/recoverjpeg-2.3.tar.gz"
+  url "https://www.rfc1149.net/download/recoverjpeg/recoverjpeg-2.3.tar.gz"
   sha256 "d6a63f0362c1f62ba9d022e044bf6cd404250547a921f25aa2d0087d08faf1ab"
 
   def install
