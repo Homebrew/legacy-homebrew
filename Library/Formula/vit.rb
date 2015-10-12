dfb1b5cf886bfaf37b998bff12c667613db5f72a
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:41:07 +0200

    vit: use an https url

--- Library/Formula/vit.rb
+++ Library/Formula/vit.rb
@@ -2,7 +2,7 @@ class Vit < Formula
   desc "Front-end for Task Warrior"
   homepage "http://taskwarrior.org/news/news.20140406.html"
   head "https://git.tasktools.org/scm/ex/vit.git"
-  url "http://taskwarrior.org/download/vit-1.2.tar.gz"
+  url "https://taskwarrior.org/download/vit-1.2.tar.gz"
   sha256 "a78dee573130c8d6bc92cf60fafac0abc78dd2109acfba587cb0ae202ea5bbd0"
   revision 1
 
