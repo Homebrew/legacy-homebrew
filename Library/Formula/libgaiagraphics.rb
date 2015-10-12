126cea4d726221920d32cd2d78d0cdc78ab59201
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:39:14 +0200

    libgaiagraphics: use an https url

--- Library/Formula/libgaiagraphics.rb
+++ Library/Formula/libgaiagraphics.rb
@@ -1,7 +1,7 @@
 class Libgaiagraphics < Formula
   desc "Library supporting common-utility raster handling"
   homepage "https://www.gaia-gis.it/fossil/libgaiagraphics/index"
-  url "http://www.gaia-gis.it/gaia-sins/gaiagraphics-sources/libgaiagraphics-0.5.tar.gz"
+  url "https://www.gaia-gis.it/gaia-sins/gaiagraphics-sources/libgaiagraphics-0.5.tar.gz"
   sha256 "ccab293319eef1e77d18c41ba75bc0b6328d0fc3c045bb1d1c4f9d403676ca1c"
   revision 1
 
