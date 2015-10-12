1bc099fb148b0c92608b09d11b8bc184db5b4aa2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-12 18:59:53 +0100

    wiki: add 1.3.0 bottle.

--- Library/Formula/wiki.rb
+++ Library/Formula/wiki.rb
@@ -6,6 +6,13 @@ class Wiki < Formula
   url "https://github.com/walle/wiki/archive/1.3.0.tar.gz"
   sha256 "c12edcaed5c9d5e69fc43e77713a68948a399017467d248ba59367b5d458a9e6"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "97f41b03a165bc89869253240e906e0075ec936069a08ecd483c516bb987e3ce" => :el_capitan
+    sha256 "36d2954d46a09d460740d16771792ed832b3b81770a5b28a86b28f3e2bd65a53" => :yosemite
+    sha256 "271812a50a0455e1947006624f0442f084c69ab387cc858d41be26a4cce195f0" => :mavericks
+  end
+
   go_resource "github.com/mattn/go-colorable" do
     url "https://github.com/mattn/go-colorable.git",
       :revision => "40e4aedc8fabf8c23e040057540867186712faa5"
