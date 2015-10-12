157f94a20a84e00652b549c22edf658944994d01
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:39:10 +0200

    libftdi: use an https url

--- Library/Formula/libftdi.rb
+++ Library/Formula/libftdi.rb
@@ -1,7 +1,7 @@
 class Libftdi < Formula
   desc "Library to talk to FTDI chips"
   homepage "http://www.intra2net.com/en/developer/libftdi"
-  url "http://www.intra2net.com/en/developer/libftdi/download/libftdi1-1.1.tar.bz2"
+  url "https://www.intra2net.com/en/developer/libftdi/download/libftdi1-1.1.tar.bz2"
   sha256 "c0b1af1a13e2c6682a1d8041e5b164a1e0d90267cd378bb51e059bd62f821e21"
 
   bottle do
