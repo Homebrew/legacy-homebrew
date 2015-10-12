a316a03292294715a32405b4b0fc43712e9315a7
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:40:51 +0200

    task: use an https url

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -1,7 +1,7 @@
 class Task < Formula
   desc "Feature-rich console based todo list manager"
   homepage "http://www.taskwarrior.org/"
-  url "http://taskwarrior.org/download/task-2.4.4.tar.gz"
+  url "https://taskwarrior.org/download/task-2.4.4.tar.gz"
   sha256 "7ff406414e0be480f91981831507ac255297aab33d8246f98dbfd2b1b2df8e3b"
   head "https://git.tasktools.org/scm/tm/task.git", :branch => "2.4.5", :shallow => false
 
