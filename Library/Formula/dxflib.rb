8b78a436a6c555d0345c9351a2feddb320e4f32d
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:38:33 +0200

    dxflib: use an https url

--- Library/Formula/dxflib.rb
+++ Library/Formula/dxflib.rb
@@ -1,7 +1,7 @@
 class Dxflib < Formula
   desc "C++ library for parsing DXF files"
   homepage "http://www.ribbonsoft.com/en/what-is-dxflib"
-  url "http://www.ribbonsoft.com/archives/dxflib/dxflib-2.5.0.0-1.src.tar.gz"
+  url "https://www.ribbonsoft.com/archives/dxflib/dxflib-2.5.0.0-1.src.tar.gz"
   sha256 "20ad9991eec6b0f7a3cc7c500c044481a32110cdc01b65efa7b20d5ff9caefa9"
 
   bottle do
