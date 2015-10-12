ba7aee1587a88e6661b4d24e149c648d8ed47561
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:39:42 +0200

    mpdas: use an https url

--- Library/Formula/mpdas.rb
+++ Library/Formula/mpdas.rb
@@ -1,7 +1,7 @@
 class Mpdas < Formula
   desc "C++ client to submit tracks to audioscrobbler"
   homepage "http://www.50hz.ws/mpdas/"
-  url "http://www.50hz.ws/mpdas/mpdas-0.3.1.tar.bz2"
+  url "https://www.50hz.ws/mpdas/mpdas-0.3.1.tar.bz2"
   sha256 "eaf01afbeac02e6a2023fd05be81042eee94b30abd82667f2220b06955f52ab9"
 
   head "https://github.com/hrkfdn/mpdas.git"
