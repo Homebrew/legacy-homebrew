4624ab223457e521b313b48c95c516666f6822ce
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-12 02:03:10 +0100

    libbladerf: add 2015.07 bottle.

--- Library/Formula/libbladerf.rb
+++ Library/Formula/libbladerf.rb
@@ -4,6 +4,12 @@ class Libbladerf < Formula
   url "https://github.com/Nuand/bladeRF/archive/2015.07.tar.gz"
   sha256 "9e15911ab39ba1eb4aa1bcbf518a0eac5396207fc4a58c32b2550fe0a65f9d22"
 
+  bottle do
+    sha256 "a0f5f3a24237452816c8d25caf2d615d00bf54b1f5f1afaac3aeed3db24dfb06" => :el_capitan
+    sha256 "da20c9f91d238d89aaf62499c7c2db6584685868c25289a680a1c2d816ef88cc" => :yosemite
+    sha256 "c41f40739255834f95fb69b19c6ec2827fd40b84ef24b22640620b3a66fe7c19" => :mavericks
+  end
+
   option :universal
 
   depends_on "pkg-config" => :build
