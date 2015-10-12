b66e1fc99e0489a305f5506044428dd287dab510
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:41:00 +0200

    tevent: use an https url

--- Library/Formula/tevent.rb
+++ Library/Formula/tevent.rb
@@ -1,7 +1,7 @@
 class Tevent < Formula
   desc "Event system based on the talloc memory management library"
   homepage "http://tevent.samba.org/"
-  url "http://www.samba.org/ftp/tevent/tevent-0.9.21.tar.gz"
+  url "https://www.samba.org/ftp/tevent/tevent-0.9.21.tar.gz"
   sha256 "f2be7463573dab2d8210cb57fe7e7e2aeb323274cbdc865a6e29ddcfb977f0f4"
 
   depends_on "pkg-config" => :build
