c6e5a457aa25a7721420279a140786c9f8c544b1
Realignist <realignist@gmail.com>
2015-10-12 21:39:44 +0900

    Set original version numbering to repo.
    Set original version numbering to repo.

--- Library/Formula/repo.rb
+++ Library/Formula/repo.rb
@@ -2,6 +2,7 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://raw.githubusercontent.com/android/tools_repo/v1.12.32/repo"
+  version "1.22"
   sha256 "9907c36d63cf5222d73ab270fee5249dc84f6b4580cda03ddd7ac309f11b289e"
   revision 1
 
