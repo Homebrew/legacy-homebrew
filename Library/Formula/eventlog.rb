7143754892c072d87b5c24e098c3d0777ad6218f
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-12 23:38:38 +0200

    eventlog: use an https url

--- Library/Formula/eventlog.rb
+++ Library/Formula/eventlog.rb
@@ -1,7 +1,7 @@
 class Eventlog < Formula
   desc "Replacement for syslog API providing structure to messages"
   homepage "http://www.balabit.com/downloads/files/eventlog/"
-  url "http://www.balabit.com/downloads/files/syslog-ng/sources/3.4.3/source/eventlog_0.2.13.tar.gz"
+  url "https://www.balabit.com/downloads/files/syslog-ng/sources/3.4.3/source/eventlog_0.2.13.tar.gz"
   sha256 "7cb4e6f316daede4fa54547371d5c986395177c12dbdec74a66298e684ac8b85"
 
   def install
