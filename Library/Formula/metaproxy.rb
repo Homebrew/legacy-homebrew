141551ed039272edb2f0b01ac5e1d9fa55e4489b
Sven-S. Porst <ssp-web@earthlingsoft.net>
2015-10-12 22:36:05 +0200

    metaproxy 1.10.0

--- Library/Formula/metaproxy.rb
+++ Library/Formula/metaproxy.rb
@@ -1,9 +1,8 @@
 class Metaproxy < Formula
   desc "Z39.50 proxy and router utilizing Yaz toolkit"
   homepage "http://www.indexdata.com/metaproxy"
-  url "http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.8.8.tar.gz"
-  sha256 "c82849e2901607d585967c1708eabcdac9b4a5a780aeb6f2ec044047455f9811"
-  revision 1
+  url "http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.10.0.tar.gz"
+  sha256 "83a282a9aefa71fd073adc2ef1c474e8b594c921da0c2c4b977821bfc3cf5a5e"
 
   bottle do
     cellar :any
