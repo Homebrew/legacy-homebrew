f1be338ed885069058b8130ab391ac014f03b7bf
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-14 01:39:46 +0200

    task: use https homepage

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -1,6 +1,6 @@
 class Task < Formula
   desc "Feature-rich console based todo list manager"
-  homepage "http://www.taskwarrior.org/"
+  homepage "https://www.taskwarrior.org/"
   url "https://taskwarrior.org/download/task-2.4.4.tar.gz"
   sha256 "7ff406414e0be480f91981831507ac255297aab33d8246f98dbfd2b1b2df8e3b"
   head "https://git.tasktools.org/scm/tm/task.git", :branch => "2.4.5", :shallow => false
