51be1a80a8dfadfdc3b91e5dc51f16512dd9fc3c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-13 03:39:12 +0100

    dbxml: add 6.0.17 bottle.

--- Library/Formula/dbxml.rb
+++ Library/Formula/dbxml.rb
@@ -4,6 +4,13 @@ class Dbxml < Formula
   url "http://download.oracle.com/berkeley-db/dbxml-6.0.17.tar.gz"
   sha256 "97c79a850ae92ef4945fcf558fbfed41469dd2bda6ad85bd05837cd428e594e8"
 
+  bottle do
+    cellar :any
+    sha256 "e61af38290271e289d8f8e0fbb39f727fe6c9999d7a0cc9531caa59d4ed7c90b" => :el_capitan
+    sha256 "9b17ab965313607cee1419b5172e81b54a2f1abc951d55471192571a55aa06b9" => :yosemite
+    sha256 "7132f822ef547094f01340f3a4324bbf3726ec6ed9b39a569c44b3afd7736f63" => :mavericks
+  end
+
   depends_on "xerces-c"
   depends_on "xqilla"
   depends_on "berkeley-db"
