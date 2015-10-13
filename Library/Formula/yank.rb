74703d17c9de0826fe9da572490e763ba7596ff3
Carl Dong <dongcarl@fastmail.com>
2015-10-13 14:52:48 -0700

    removed unneeded comment

--- Library/Formula/yank.rb
+++ Library/Formula/yank.rb
@@ -2,7 +2,6 @@ class Yank < Formula
   desc "Yank terminal output to clipboard"
   homepage "https://github.com/mptre/yank"
   url "https://github.com/mptre/yank/archive/v0.4.0.tar.gz"
-  # version "0.4.0"
   sha256 "1adf1b07f7cb9401daeed7a05bad492db8ed77ead4728d9b45f541d56bc2e8c5"
 
   def install
