b83871b3feede62fd67ef0d45541d928cf3c7ca8
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-13 19:56:52 +0200

    mecab-unidic: follow redirect + use https homepage

--- Library/Formula/mecab-unidic.rb
+++ Library/Formula/mecab-unidic.rb
@@ -1,6 +1,6 @@
 class MecabUnidic < Formula
   desc "Morphological analyzer for MeCab"
-  homepage "http://sourceforge.jp/projects/unidic/"
+  homepage "https://osdn.jp/projects/unidic/"
   url "https://sourceforge.jp/frs/redir.php?m=iij&f=%2Funidic%2F58338%2Funidic-mecab-2.1.2_src.zip"
   sha256 "6cce98269214ce7de6159f61a25ffc5b436375c098cc86d6aa98c0605cbf90d4"
 
