fe03f8754c9e5c6c2e79f1429c6400e56205da34
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-13 19:56:52 +0200

    mecab-unidic: follow redirect
    Closes #44901.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/mecab-unidic.rb
+++ Library/Formula/mecab-unidic.rb
@@ -1,7 +1,7 @@
 class MecabUnidic < Formula
   desc "Morphological analyzer for MeCab"
-  homepage "http://sourceforge.jp/projects/unidic/"
-  url "https://sourceforge.jp/frs/redir.php?m=iij&f=%2Funidic%2F58338%2Funidic-mecab-2.1.2_src.zip"
+  homepage "https://osdn.jp/projects/unidic/"
+  url "http://dl.osdn.jp/unidic/58338/unidic-mecab-2.1.2_src.zip"
   sha256 "6cce98269214ce7de6159f61a25ffc5b436375c098cc86d6aa98c0605cbf90d4"
 
   bottle do
