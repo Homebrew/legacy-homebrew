e005a187b235391abadc6c41b86d69b057c6f1cf
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-14 01:39:47 +0200

    xtail: use https homepage
    Closes #44916.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/xtail.rb
+++ Library/Formula/xtail.rb
@@ -1,6 +1,6 @@
 class Xtail < Formula
   desc "Watch growth of multiple files or directories (like `tail -f`)"
-  homepage "http://www.unicom.com/sw/xtail"
+  homepage "https://www.unicom.com/sw/xtail"
   url "https://www.unicom.com/files/xtail-2.1.tar.gz"
   sha256 "75184926dffd89e9405769b24f01c8ed3b25d3c4a8eac60271fc5bb11f6c2d53"
 
