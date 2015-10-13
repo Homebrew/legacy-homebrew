c7227c16424dc40304405ab49f9af8edbc487f56
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-14 01:39:46 +0200

    taskd: use https homepage

--- Library/Formula/taskd.rb
+++ Library/Formula/taskd.rb
@@ -1,6 +1,6 @@
 class Taskd < Formula
   desc "Client-server synchronization for todo lists"
-  homepage "http://taskwarrior.org/projects/show/taskwarrior"
+  homepage "https://taskwarrior.org/projects/show/taskwarrior"
   url "https://taskwarrior.org/download/taskd-1.1.0.tar.gz"
   sha256 "7b8488e687971ae56729ff4e2e5209ff8806cf8cd57718bfd7e521be130621b4"
 
