d51f3c2a9189b9d999adabcbb54fe0b8e0fb3ec2
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-14 01:39:45 +0200

    svdlibc: use https homepage
    Closes #44918.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/svdlibc.rb
+++ Library/Formula/svdlibc.rb
@@ -1,6 +1,6 @@
 class Svdlibc < Formula
   desc "C library to perform singular value decomposition"
-  homepage "http://tedlab.mit.edu/~dr/SVDLIBC/"
+  homepage "https://tedlab.mit.edu/~dr/SVDLIBC/"
   url "https://tedlab.mit.edu/~dr/SVDLIBC/svdlibc.tgz"
   version "1.4"
   sha256 "aa1a49a95209801803cc2d9f8792e482b0e8302da8c7e7c9a38e25e5beabe5f8"
