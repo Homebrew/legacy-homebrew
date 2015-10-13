37b91fb74f234047ac499381f6b7a60fc874a062
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-13 20:12:31 +0200

    ogmtools: use https homepage

--- Library/Formula/ogmtools.rb
+++ Library/Formula/ogmtools.rb
@@ -1,6 +1,6 @@
 class Ogmtools < Formula
   desc "OGG media streams manipulation tools"
-  homepage "http://www.bunkus.org/videotools/ogmtools/"
+  homepage "https://www.bunkus.org/videotools/ogmtools/"
   url "https://www.bunkus.org/videotools/ogmtools/ogmtools-1.5.tar.bz2"
   sha256 "c8d61d1dbceb981dc7399c1a85e43b509fd3d071fb8d3ca89ea9385e6e40fdea"
 
