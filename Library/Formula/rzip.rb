4780fe15657b258f51ab03cf36e66220364adc8d
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-13 20:16:46 +0200

    rzip: use https homepage

--- Library/Formula/rzip.rb
+++ Library/Formula/rzip.rb
@@ -1,6 +1,6 @@
 class Rzip < Formula
   desc "File compression tool (like gzip or bzip2)"
-  homepage "http://rzip.samba.org/"
+  homepage "https://rzip.samba.org/"
   url "https://rzip.samba.org/ftp/rzip/rzip-2.1.tar.gz"
   sha256 "4bb96f4d58ccf16749ed3f836957ce97dbcff3e3ee5fd50266229a48f89815b7"
 
