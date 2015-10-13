e5a2380f3efb8694e86093537d2f723fd8c1f133
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-13 19:49:39 +0200

    zsh-lovers: use https homepage
    Closes #44897.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/zsh-lovers.rb
+++ Library/Formula/zsh-lovers.rb
@@ -1,6 +1,6 @@
 class ZshLovers < Formula
   desc "Tips, tricks, and examples for zsh"
-  homepage "http://grml.org/zsh/#zshlovers"
+  homepage "https://grml.org/zsh/#zshlovers"
   url "https://grml.org/zsh/zsh-lovers.1"
   version "0.9.0"
   sha256 "b40802dd83f1f2eded9823776ae90af8e0b91488e97f98b3a0629f8d11953e81"
