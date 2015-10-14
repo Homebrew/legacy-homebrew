17de262fa638bdeeac30949d60524b80c33687a5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-14 07:38:13 +0100

    mpdas: add 0.3.1 bottle.

--- Library/Formula/mpdas.rb
+++ Library/Formula/mpdas.rb
@@ -6,6 +6,13 @@ class Mpdas < Formula
 
   head "https://github.com/hrkfdn/mpdas.git"
 
+  bottle do
+    cellar :any
+    sha256 "e82e85475795c700ad560280d551c94eac4a7e376eac2848d214e46747023644" => :el_capitan
+    sha256 "ce6d4b85c76698e1dd16325d3e4fc0b560a53e5e7a9608aa9060833f7059ff5d" => :yosemite
+    sha256 "7844d826d96940c932d89af353e456b17df5cd6ee67627aa51f264d1b713456e" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libmpd"
 
