7966619728532ccfb82daaba7aa30244aa011afc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-14 11:56:23 +0100

    xlslib: update 2.5.0 bottle.

--- Library/Formula/xlslib.rb
+++ Library/Formula/xlslib.rb
@@ -7,10 +7,9 @@ class Xlslib < Formula
 
   bottle do
     cellar :any
-    revision 2
-    sha256 "4eec9d433ab0e89aedf4fa6ccaf7d9be555519c574a4c0cd1cd2cd80a57eab77" => :el_capitan
-    sha256 "41398d8c2e6c8349ea752eb0d2d9cdbc560ec43cb91e0622b786a2b28d6341de" => :yosemite
-    sha256 "a984b3016532ffaeee04cffb60b19e61aa44ab70ad4fcf91a6f8daa87319e744" => :mavericks
+    sha256 "a4d5714e19c1d4e44d67bbe9cda064120dc01e9cf207771ae5ef208e76ed2cd9" => :el_capitan
+    sha256 "cfd77ea71da12276124cb9cb08bb9ad51ab38b17e912d4dd8c4fec8a428714a1" => :yosemite
+    sha256 "b23da4149e9db9bf4152cfc8943ac83d254796d88741592949c3928ae7988149" => :mavericks
   end
 
   depends_on "autoconf" => :build
