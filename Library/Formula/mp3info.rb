38ed5fa5ae04318eb72ffd380585db2206dd50a5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-14 07:38:13 +0100

    mp3info: update 0.8.5a bottle.

--- Library/Formula/mp3info.rb
+++ Library/Formula/mp3info.rb
@@ -5,10 +5,9 @@ class Mp3info < Formula
   sha256 "0438ac68e9f04947fb14ca5573d27c62454cb9db3a93b7f1d2c226cd3e0b4e10"
 
   bottle do
-    cellar :any
-    sha1 "760a13887c10169cf2a3553b294a5ec4a7f8cf9f" => :yosemite
-    sha1 "6cca8a83250afeba2e85a996c169865d09743d7e" => :mavericks
-    sha1 "2f2840e15a53c1d3f52cced47d943604c7b6fdf9" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "30c85d8b2afd6e6ad03e473de3bd83ef9c6c607b979570798cfc778ad887b902" => :el_capitan
   end
 
   patch :p0 do
