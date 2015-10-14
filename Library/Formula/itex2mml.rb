c70f566bf92086aea53ff54ce277e6ef04a447f7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-14 08:53:05 +0100

    itex2mml: add 1.5.1 bottle.

--- Library/Formula/itex2mml.rb
+++ Library/Formula/itex2mml.rb
@@ -10,6 +10,13 @@ class Itex2mml < Formula
   url "https://golem.ph.utexas.edu/~distler/blog/files/itexToMML-1.5.1.tar.gz"
   sha256 "09f39f9db83d2693c9c80288644a8121cd268e956e44684722d29cd6343f524c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2fac6420289da65bf725409fff047eb99b5aef6e13b495e7ebe5e9e5bc3d2def" => :el_capitan
+    sha256 "af798ea3f8d4f0128dbccef9d26857f472caa76bde9f601f5ab9cdcb315ee31f" => :yosemite
+    sha256 "f1767334c2aa53a666064e28a298b75e734f671cd6b6ca60ca9b3af91e40cc90" => :mavericks
+  end
+
   def install
     bin.mkpath
     cd "itex-src" do
