236a7f6547c62801f6ecf2b7aec5d97217c8ab5b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-14 05:36:43 +0100

    vit: add 1.2_1 bottle.

--- Library/Formula/vit.rb
+++ Library/Formula/vit.rb
@@ -6,6 +6,13 @@ class Vit < Formula
   sha256 "a78dee573130c8d6bc92cf60fafac0abc78dd2109acfba587cb0ae202ea5bbd0"
   revision 1
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "148f01bcfe731892cbfbc63eb9e8d95fded12f07c2d56a7429f8ddea27207f51" => :el_capitan
+    sha256 "e91023aac9f44f67570d248255fc61ed614091fdfafb16003b49064d90866d91" => :yosemite
+    sha256 "3f7e65dd15708aaf63ed1d3d3bc948cd020371b35c4537a1366d34a94181767e" => :mavericks
+  end
+
   depends_on "task"
 
   resource "Curses" do
