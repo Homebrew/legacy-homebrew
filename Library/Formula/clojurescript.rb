81ba2d04fffd24d11495b85061ed6406da38809a
Stian Eikeland <stian@eikeland.se>
2015-10-15 23:29:33 +0200

    clojurescript 1.7.145
    Closes #45007.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/clojurescript.rb
+++ Library/Formula/clojurescript.rb
@@ -1,9 +1,9 @@
 class Clojurescript < Formula
   desc "Clojure to JS compiler"
   homepage "https://github.com/clojure/clojurescript"
-  url "https://github.com/clojure/clojurescript/releases/download/r1.7.48/cljs.jar"
-  version "1.7.48"
-  sha256 "91d56866aa0d91b09673598b37f6cc59682b11f79e25cef4b6d058df87cd54b3"
+  url "https://github.com/clojure/clojurescript/releases/download/r1.7.145/cljs.jar"
+  version "1.7.145"
+  sha256 "67e33f4e68459d4772f912a658230df5b3d78996d072b34bb9080c337da7bcbb"
   head "https://github.com/clojure/clojurescript.git"
 
   bottle do
