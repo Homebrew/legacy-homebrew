c7cdd9739f62cd22c917a976e7ab81a0d79755c8
George Gastaldi <gegastaldi@gmail.com>
2015-10-15 16:36:26 -0300

    jboss-forge 2.20.0.Final
    Closes #45005.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -1,9 +1,9 @@
 class JbossForge < Formula
   desc "Tools to help set up and configure a project"
   homepage "http://forge.jboss.org/"
-  url "https://downloads.jboss.org/forge/releases/2.19.2.Final/forge-distribution-2.19.2.Final-offline.zip"
-  version "2.19.2.Final"
-  sha256 "be3b079ae57f3c3d9c18c6f1d8c0c71914e8fcdd09554321b93457e168572f58"
+  url "https://downloads.jboss.org/forge/releases/2.20.0.Final/forge-distribution-2.20.0.Final-offline.zip"
+  version "2.20.0.Final"
+  sha256 "ee4f9b2fff8ac72714cfd0c852735fcdd04e1b4462de8a6ba0807cbe3df4503f"
 
   bottle do
     cellar :any_skip_relocation
