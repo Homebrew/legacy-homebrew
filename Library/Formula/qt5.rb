01f0faab565c8233c2bf31d73aa1e14afaf88d94
Martin Afanasjew <martin@afanasjew.de>
2015-10-15 18:30:35 +0200

    qt5 5.5.1
    Closes #45026.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/qt5.rb
+++ Library/Formula/qt5.rb
@@ -15,48 +15,17 @@ class Qt5 < Formula
   desc "Version 5 of the Qt framework"
   homepage "https://www.qt.io/"
   head "https://code.qt.io/qt/qt5.git", :branch => "5.5", :shallow => false
-  revision 1
 
   stable do
-    url "https://download.qt.io/official_releases/qt/5.5/5.5.0/single/qt-everywhere-opensource-src-5.5.0.tar.xz"
-    mirror "https://www.mirrorservice.org/sites/download.qt-project.org/official_releases/qt/5.5/5.5.0/single/qt-everywhere-opensource-src-5.5.0.tar.xz"
-    sha256 "7ea2a16ecb8088e67db86b0835b887d5316121aeef9565d5d19be3d539a2c2af"
-
-    # Apple's 3.6.0svn based clang doesn't support -Winconsistent-missing-override
-    # https://bugreports.qt.io/browse/QTBUG-46833
-    # This is fixed in 5.5 branch and below patch should be removed
-    # when this formula is updated to 5.5.1
-    patch :DATA
-
-    # Build error: Fix build with clang 3.7.
-    # https://codereview.qt-project.org/#/c/121545/
-    # Should land in the 5.5.1 release.
-    patch do
-      url "https://raw.githubusercontent.com/DomT4/scripts/2107043e8/Homebrew_Resources/Qt5/qt5_el_capitan.diff"
-      sha256 "bd8fd054247ec730f60778e210d58cba613265e5df04ec93f4110421fb03b14a"
-    end
+    url "https://download.qt.io/official_releases/qt/5.5/5.5.1/single/qt-everywhere-opensource-src-5.5.1.tar.xz"
+    mirror "https://www.mirrorservice.org/sites/download.qt-project.org/official_releases/qt/5.5/5.5.1/single/qt-everywhere-opensource-src-5.5.1.tar.xz"
+    sha256 "6f028e63d4992be2b4a5526f2ef3bfa2fe28c5c757554b11d9e8d86189652518"
 
     # Build error: Fix library paths with Xcode 7 for QtWebEngine.
     # https://codereview.qt-project.org/#/c/122729/
     patch do
       url "https://raw.githubusercontent.com/Homebrew/patches/2fcc1f8ec1df1c90785f4fa6632cebac68772fa9/qt5/el-capitan-2.diff"
       sha256 "b8f04efd047eeed7cfd15b029ece20b5fe3c0960b74f7a5cb98bd36475463227"
-    end
-
-    # Build error: Fix CGEventCreateMouseEvent use with 10.11 SDK.
-    # https://codereview.qt-project.org/#/c/115138/
-    # Should land in the 5.5.1 release.
-    patch do
-      url "https://gist.githubusercontent.com/UniqMartin/baf089e326f572150971/raw/1de52d53929bc3472cc7f345c16f068c37c75263/qtbug-47641.patch"
-      sha256 "c74c73b2d540788f0be2f1f137d0844feca8f5022a044851366380bf2972ead0"
-    end
-
-    # Runtime error: Make tooltips transparent for mouse events.
-    # https://codereview.qt-project.org/#/c/124274/
-    # Should land in the 5.5.1 release.
-    patch do
-      url "https://gist.githubusercontent.com/swallat/6b7d10fd929a0087fea4/raw/9b201a2848f8b8e16067855f30588a7b6dc607ec/qt5.5-qnsview-tooltip-cocoa.patch"
-      sha256 "5fa4511ee0c91491358d569f884dad9e4088eafa329e7dbe2b38a62afeef899d"
     end
 
     # Fix for qmake producing broken pkg-config files, affecting Poppler et al.
@@ -200,18 +169,3 @@ class Qt5 < Formula
     system "./hello"
   end
 end
-
-__END__
-diff --git a/qtbase/src/corelib/global/qcompilerdetection.h b/qtbase/src/corelib/global/qcompilerdetection.h
-index 7ff1b67..060af29 100644
---- a/qtbase/src/corelib/global/qcompilerdetection.h
-+++ b/qtbase/src/corelib/global/qcompilerdetection.h
-@@ -155,7 +155,7 @@
- /* Clang also masquerades as GCC */
- #    if defined(__apple_build_version__)
- #      /* http://en.wikipedia.org/wiki/Xcode#Toolchain_Versions */
--#      if __apple_build_version__ >= 6020049
-+#      if __apple_build_version__ >= 7000053
- #        define Q_CC_CLANG 306
- #      elif __apple_build_version__ >= 6000051
- #        define Q_CC_CLANG 305
