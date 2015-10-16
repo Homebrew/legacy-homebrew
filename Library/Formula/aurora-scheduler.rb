580aafc4d202bd8b0822bd5a4d9b2fdf6875ad9f
thinker0 <thinker0@gmail.com>
2015-10-16 23:14:47 +0900

    java version, python check on snow_leopard

--- Library/Formula/aurora-scheduler.rb
+++ Library/Formula/aurora-scheduler.rb
@@ -4,6 +4,9 @@ class AuroraScheduler < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=aurora/0.9.0/apache-aurora-0.9.0.tar.gz"
   sha256 "16040866f3a799226452b1541892eb80ed3c61f47c33f1ccb0687fb5cf82767c"
 
+  depends_on :java => "1.8+"
+  depends_on :python if MacOS.version <= :snow_leopard
+
   # https://issues.apache.org/jira/browse/AURORA-1508
   patch :DATA
 
