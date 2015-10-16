0c3cec3a1d30fd67101e5fb376b4ff7610cfeb53
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-16 15:29:57 +0200

    metaproxy: use an https homepage url
    It redirects to HTTP for now.

--- Library/Formula/metaproxy.rb
+++ Library/Formula/metaproxy.rb
@@ -1,6 +1,6 @@
 class Metaproxy < Formula
   desc "Z39.50 proxy and router utilizing Yaz toolkit"
-  homepage "http://www.indexdata.com/metaproxy"
+  homepage "https://www.indexdata.com/metaproxy"
   url "http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.10.0.tar.gz"
   sha256 "83a282a9aefa71fd073adc2ef1c474e8b594c921da0c2c4b977821bfc3cf5a5e"
 
