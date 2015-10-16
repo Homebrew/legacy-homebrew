7b961bc376d3634d29c92d911295c9ffc65ebca3
Guillaume Khayat <guillaume.kh.alt@gmail.com>
2015-10-16 12:37:23 +0200

    Whatmp3 3.6 removed useless mkdir in test

--- Library/Formula/whatmp3.rb
+++ Library/Formula/whatmp3.rb
@@ -21,9 +21,7 @@ class Whatmp3 < Formula
 
   test do
     # Create dummy FLAC
-    Dir.mkdir("flac")
     (testpath/"flac/file.flac").write "fLaC\x00\x00\x00\"\x04\x80\x04\x80\x00\x00\f\x00\x00\f\x01\xF4\x00\xF0\x00\x00\x00\x01\xF3\x8B\xE3\xDBM\x93\xE40\\~$\xBE\x94\xEF\x01\x9A\x84\x00\x00( \x00\x00\x00reference libFLAC 1.2.1 20070917\x00\x00\x00\x00\xFF\xF8d\b\x00\x00\xE3\x03\x01\xFD\xEC\x10"
-    # Decode with Whatmp3
     system "#{bin}/whatmp3", "--notorrent", "--V0", "flac"
     assert (testpath/"V0/file.mp3").exist?
   end
