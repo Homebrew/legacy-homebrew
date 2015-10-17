c10778f368887ff778289e96fb31ff5199808020
Bai <baishuai.io@gmail.com>
2015-10-17 22:52:07 +0800

    racer: install to libexec, add head, remove key_only

--- Library/Formula/racer.rb
+++ Library/Formula/racer.rb
@@ -1,10 +1,15 @@
 class Racer < Formula
   desc "Rust Code Completion utility"
   homepage "https://github.com/phildawes/racer"
-  url "https://github.com/phildawes/racer/archive/v1.0.0.tar.gz"
-  sha256 "78895296ed688eeccbaf7745235f0fc503407bfa718f53583a4dcc9e1246b7f5"
 
-  keg_only "You should set path in your special editor which use racer"
+  stable do
+    url "https://github.com/phildawes/racer/archive/v1.0.0.tar.gz"
+    sha256 "78895296ed688eeccbaf7745235f0fc503407bfa718f53583a4dcc9e1246b7f5"
+  end
+
+  head do
+    url "https://github.com/phildawes/racer.git"
+  end
 
   option "without-src", "Do not get rust source for racer from www.rust-lang.org"
 
@@ -18,24 +23,31 @@ class Racer < Formula
   def install
     if build.with?("src")
       resource("rustsrc").stage do
-        rm_rf "./src/llvm/test/"
-        (prefix/"rust-src").install Dir["./src/*"]
+        Dir.glob(["src/llvm/*", "src/test/*"]) do |f|
+          if File.directory?(f)
+            rm_r(f)
+          else
+            File.delete(f)
+          end
+        end
+        (share/"rustsrc").install Dir["./src/*"]
       end
     end
 
     system "cargo", "build", "--release"
-    bin.install "target/release/racer"
+    libexec.install "target/release/racer"
   end
 
   def caveats; <<-EOS.undent
-    Rust source is installed at #{opt_prefix}/rust-src by default.
-    You should export RUST_SRC_PATH=#{opt_prefix}/rust-src or set
+    racer in installed at #{opt_prefix}/libexec/racer, set it to path if needed.
+    Rust source is installed at #{opt_prefix}/share/rustsrc by default.
+    You should export RUST_SRC_PATH=#{opt_prefix}/share/rustsrc or set
     rust source path in your special editor which use racer
     EOS
   end
 
   test do
-    ENV["RUST_SRC_PATH"] = "#{opt_prefix}/rust-src"
-    assert_match /^MATCH BufReader/, shell_output("#{bin}/racer complete std::io::B")
+    ENV["RUST_SRC_PATH"] = "#{opt_prefix}/share/rustsrc"
+    assert_match /^MATCH BufReader/, shell_output("#{libexec}/racer complete std::io::B")
   end
 end
