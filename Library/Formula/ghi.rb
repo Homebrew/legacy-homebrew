80b5ad7549453c440f1f09f502056c4fa57e09d8
Alex Chesters <alex.chesters@bbc.co.uk>
2015-10-17 01:55:26 +0100

    ghi 1.0.0

--- Library/Formula/ghi.rb
+++ Library/Formula/ghi.rb
@@ -1,9 +1,9 @@
 class Ghi < Formula
   desc "Work on GitHub issues on the command-line"
   homepage "https://github.com/stephencelis/ghi"
-  url "https://github.com/stephencelis/ghi/archive/0.9.4.tar.gz"
+  url "https://github.com/stephencelis/ghi/archive/1.0.0.tar.gz"
   head "https://github.com/stephencelis/ghi.git"
-  sha256 "253eb84c28f4135d41e5276c5e0eb5a8723b77fe07f650204458b4776d9872cc"
+  sha256 "6227c783a4e50288cee0ea7d6f00462881d8dcb144ae6126f3da19ad9119fd3d"
 
   def install
     bin.install "ghi"
