8c9f3742c63fc362a070d311ec4515c8311a5e28
Adam Demasi <adam@hbang.com.au>
2015-10-18 20:53:44 +1030

    ldid: fix code style

--- Library/Formula/ldid.rb
+++ Library/Formula/ldid.rb
@@ -5,7 +5,7 @@ class Ldid < Formula
     :tag => "v1.1.2",
     :revision => "604cc486bdefb246c984a21dbb30cdaf8b0a7f4d"
 
-  head "git://git.saurik.com/ldid.git", :branch => "master"
+  head "git://git.saurik.com/ldid.git"
 
   depends_on "openssl"
 
@@ -17,7 +17,7 @@ class Ldid < Formula
   test do
     (testpath/"test.c").write <<-EOS.undent
       int main(int argc, char **argv) { return 0; }
-      EOS
+    EOS
 
     system ENV.cc, "test.c", "-o", "test"
     system bin/"ldid", "-S", "test"
