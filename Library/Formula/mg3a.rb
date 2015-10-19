52a9d5366382f314b808c373739f35eb7877b3c5
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-19 22:19:26 +0200

    mg3a: style, test added
    Closes #45158.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mg3a.rb
+++ Library/Formula/mg3a.rb
@@ -13,22 +13,32 @@ class Mg3a < Formula
   option "with-all", "Include all fancy stuff"
 
   def install
-    mg3aopts=" -DDIRED -DPREFIXREGION -DUSER_MODES -DUSER_MACROS"
-    mg3aopts << " -DLANGMODE_C" if build.with?("c-mode")
-    mg3aopts << " -DLANGMODE_PYTHON" if build.with?("python-mode") || build.with?("most")
-    mg3aopts << " -DLANGMODE_CLIKE" if build.with?("clike-mode") || build.with?("most")
-    mg3aopts = "-DALL" if build.with?("all")
-    # mg3aopts << " -DEMACS_QUIT"
+    if build.with?("all")
+      mg3aopts = "-DALL" if build.with?("all")
+    else
+      mg3aopts = %w[-DDIRED -DPREFIXREGION -DUSER_MODES -DUSER_MACROS]
+      mg3aopts << "-DLANGMODE_C" if build.with?("c-mode")
+      mg3aopts << "-DLANGMODE_PYTHON" if build.with?("python-mode") || build.with?("most")
+      mg3aopts << "-DLANGMODE_CLIKE" if build.with?("clike-mode") || build.with?("most")
+    end
 
-    system "make", "CDEFS=#{mg3aopts}", "LIBS=-lncurses", "COPT=-O3"
+    system "make", "CDEFS=#{mg3aopts * " "}", "LIBS=-lncurses", "COPT=-O3"
     bin.install "mg"
     doc.install Dir["bl/dot.*"]
     doc.install Dir["README*"]
   end
 
   test do
-    # ENV["TERM"]="xterm"
-    # system "script", "-q", "/dev/null", "mg", "-e", "save-buffers-kill-emacs"
-    # system true
+    (testpath/"command.sh").write <<-EOS.undent
+      #!/usr/bin/expect -f
+      set timeout -1
+      spawn #{bin}/mg
+      match_max 100000
+      send -- "\u0018\u0003"
+      expect eof
+    EOS
+    (testpath/"command.sh").chmod 0755
+
+    system testpath/"command.sh"
   end
 end
