67e6fd23f0e924f752b32ccef24c41d192558e62
Paul Bakker <paul.bakker@luminis.eu>
2015-10-19 16:53:04 +0200

    Added test

--- Library/Formula/amdatu-bootstrap.rb
+++ Library/Formula/amdatu-bootstrap.rb
@@ -2,10 +2,14 @@ class AmdatuBootstrap < Formula
   desc "Bootstrapping OSGi development"
   homepage "http://amdatu.org/bootstrap/intro.html"
   url "https://bitbucket.org/amdatu/amdatu-bootstrap/downloads/bootstrap-r8-bin-brew.zip"
-  sha256 "f2f66af5a6b9725e260509c0a0e9996c1e3b7f696a96309b0a2a399975a9a732"
+  sha256 "5c89667e10b57060c67fc54188ded3b0b281f88fab062bfb4290994e8df2295a"
 
   def install
     libexec.install %w[amdatu-bootstrap bootstrap.jar conf]
     bin.install_symlink "#{libexec}/amdatu-bootstrap" => "amdatu-bootstrap"
+  end
+
+  test do
+    assert_match "Amdatu Bootstrap R8", shell_output("#{bin}/amdatu-bootstrap --info")
   end
 end
