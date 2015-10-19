844b8dbe9752b5551cdb6d9f3cfbfc80204895a5
Alex Chesters <AlexChesters@users.noreply.github.com>
2015-10-19 21:06:43 +0100

    ghi 1.0.3
    Closes #45157.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ghi.rb
+++ Library/Formula/ghi.rb
@@ -1,9 +1,9 @@
 class Ghi < Formula
   desc "Work on GitHub issues on the command-line"
   homepage "https://github.com/stephencelis/ghi"
-  url "https://github.com/stephencelis/ghi/archive/1.0.2.tar.gz"
+  url "https://github.com/stephencelis/ghi/archive/1.0.3.tar.gz"
   head "https://github.com/stephencelis/ghi.git"
-  sha256 "7ab047dafb501bfa34e5d5fedd64954ad9c1dbb47faabe2d9789ac8b53796d11"
+  sha256 "98ddf904bfd78d7363986b75ecea294933c8b98d9c1d2828419fddcaeadc0fdd"
 
   def install
     bin.install "ghi"
