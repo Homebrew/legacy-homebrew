8535fa4a1c239c3f89c7f13a13ba36d2183a4357
Albin Stigo <albin.stigo@gmail.com>
2015-10-20 11:11:57 +0200

    Codec2 0.5 (new formula)
    * Changed http to https.
    * Split test command arguments.

--- Library/Formula/codec2.rb
+++ Library/Formula/codec2.rb
@@ -1,7 +1,7 @@
 class Codec2 < Formula
   desc "Open source speech codec."
   homepage "http://www.rowetel.com/blog/?page_id=452"
-  url "http://files.freedv.org/codec2/codec2-0.5.tar.xz"
+  url "https://files.freedv.org/codec2/codec2-0.5.tar.xz"
   sha256 "1ffda04ec6629f5ad5a38349c6d9d38d29bfbc1c677c12014ff20d480a343f17"
 
   depends_on "cmake" => :build
@@ -16,6 +16,6 @@ class Codec2 < Formula
   test do
     # 8 bytes of raw audio data (silence).
     (testpath/"test.raw").write([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00].pack('C*'))
-    system "#{bin}/c2enc 2400 test.raw test.c2"
+    system "#{bin}/c2enc", "2400",  "test.raw",  "test.c2"
   end
 end
