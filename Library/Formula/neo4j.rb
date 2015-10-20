416e1c8541b70dd71e0b0a29a0840338a573fad0
mindrones <mindrones@users.noreply.github.com>
2015-10-20 14:37:40 +0200

    neo4j: remove neo4j-shell-tools
    See https://github.com/Homebrew/homebrew/pull/44445
    
    Closes #44445.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/neo4j.rb
+++ Library/Formula/neo4j.rb
@@ -11,13 +11,6 @@ class Neo4j < Formula
     version "2.3.0-M03"
   end
 
-  option "with-neo4j-shell-tools", "Add neo4j-shell-tools to the standard neo4j-shell"
-
-  resource "neo4j-shell-tools" do
-    url "http://dist.neo4j.org/jexp/shell/neo4j-shell-tools_2.2.zip"
-    sha256 "e295f724b6c5e35db5d471eefc976bd5adcd002763722c726885455e3f38f2ca"
-  end
-
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
@@ -27,14 +20,6 @@ class Neo4j < Formula
 
     # Symlink binaries
     bin.install_symlink Dir["#{libexec}/bin/neo4j{,-shell,-import}"]
-
-    # Eventually, install neo4j-shell-tools
-    # omiting "opencsv-2.3.jar" because it already comes with neo4j (see libexec/lib)
-    if build.with? "neo4j-shell-tools"
-      resource("neo4j-shell-tools").stage do
-        (libexec/"lib").install "geoff-0.5.0.jar", "import-tools-2.2.jar", "mapdb-0.9.3.jar"
-      end
-    end
 
     # Adjust UDC props
     open("#{libexec}/conf/neo4j-wrapper.conf", "a") do |f|
