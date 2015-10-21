1d5e08ec2163b962dd0a7a88542ba47b3ecd8947
Xu Cheng <xucheng@me.com>
2015-10-21 14:59:31 +0800

    gradle: bottle unneeded

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -4,6 +4,8 @@ class Gradle < Formula
   url "https://downloads.gradle.org/distributions/gradle-2.8-bin.zip"
   sha256 "a88db9c2f104defdaa8011c58cf6cda6c114298ae3695ecfb8beb30da3a903cb"
 
+  bottle :unneeded
+
   def install
     libexec.install %w[bin lib]
     bin.install_symlink libexec+"bin/gradle"
