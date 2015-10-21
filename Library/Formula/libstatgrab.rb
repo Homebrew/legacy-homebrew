26e08654ee7f575d9c3aad93202b51d611f09dd5
Adam Flott <adam@adamflott.com>
2015-10-21 14:20:41 -0500

    libstatgrab: description changes from @bfontaine and @dunn

--- Library/Formula/libstatgrab.rb
+++ Library/Formula/libstatgrab.rb
@@ -1,5 +1,5 @@
 class Libstatgrab < Formula
-  desc "provides cross platform access to statistics about the system."
+  desc "Provides cross-platform access to statistics about the system."
   homepage "http://www.i-scream.org/libstatgrab/"
   url "http://ftp.i-scream.org/pub/i-scream/libstatgrab/libstatgrab-0.91.tar.gz"
   sha256 "03e9328e4857c2c9dcc1b0347724ae4cd741a72ee11acc991784e8ef45b7f1ab"
