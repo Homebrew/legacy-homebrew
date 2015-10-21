bed18bf9313c49c9246aa58d209442ff5ed865e7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-21 06:33:52 +0100

    icmake: add 7.23.02 bottle.

--- Library/Formula/icmake.rb
+++ Library/Formula/icmake.rb
@@ -4,6 +4,12 @@ class Icmake < Formula
   url "https://github.com/fbb-git/icmake/archive/7.23.02.tar.gz"
   sha256 "f030086d0630c2c1755519556b9cf88b8495bc8debf36b257e0c69d3b199c46b"
 
+  bottle do
+    sha256 "a8d78a2d192e97d98abc4c02b6ef4411ea47f17056591f44c3bc755bd695fecb" => :el_capitan
+    sha256 "cbc0de675413c996b4988fe061c2c15b774ea63cd230fc363858ae3e0e04c089" => :yosemite
+    sha256 "8e25ce4cb2d564ef4ed938b69b1b0566520383dd9d754af77a4271ab6281784e" => :mavericks
+  end
+
   depends_on "gnu-sed" => :build
 
   def install
