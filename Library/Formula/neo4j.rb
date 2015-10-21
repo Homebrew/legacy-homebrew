3fe2623c21640115c67686cb1213741d312acd44
mindrones <mindrones@users.noreply.github.com>
2015-10-21 10:19:15 +0200

    neo4j 2.2.6 + neo4j 2.3.0-RC1 (devel)

--- Library/Formula/neo4j.rb
+++ Library/Formula/neo4j.rb
@@ -1,14 +1,14 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "http://neo4j.com"
-  url "http://dist.neo4j.org/neo4j-community-2.2.5-unix.tar.gz"
-  version "2.2.5"
-  sha256 "7fadc119f465a3d6adceb610401363fb158a5ed25081f9893d4f56ac4989a998"
+  url "http://dist.neo4j.org/neo4j-community-2.2.6-unix.tar.gz"
+  version "2.2.6"
+  sha256 "77f8371318c843bdd8fbd5f95be95e9a1467bb84030d4a3ff9dcec805b4003b9"
 
   devel do
-    url "http://dist.neo4j.org/neo4j-community-2.3.0-M03-unix.tar.gz"
-    sha256 "cd47ae541a0c5da0d82212c30c1c2fb19ea91ddb1b174346c120e596e04b5dfc"
-    version "2.3.0-M03"
+    url "http://dist.neo4j.org/neo4j-community-2.3.0-RC1-unix.tar.gz"
+    sha256 "fed10ec7d7c0a0c39041a8079672812962f3ceff053f050617b42b5d0bd22ae9"
+    version "2.3.0-RC1"
   end
 
   def install
