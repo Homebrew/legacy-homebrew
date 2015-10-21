f6ff9c4c4d1d3f5fd4b2a3ed20f43bf82298d08b
Jenkins CI <noreply@cyberduck.io>
2015-10-21 16:25:45 +0200

    duck 4.7.3.18396
    Closes #45215.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/duck.rb
+++ Library/Formula/duck.rb
@@ -2,8 +2,8 @@ class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
   # check the changelog for the latest stable version: https://cyberduck.io/changelog/
-  url "https://dist.duck.sh/duck-src-4.7.2.18007.tar.gz"
-  sha256 "2b30071ef58aed42805724f318b1e3f013749cc534e54265ee305dfc2ec86d69"
+  url "https://dist.duck.sh/duck-src-4.7.3.18396.tar.gz"
+  sha256 "47e25f0a28393c388f37d319c9d51dd51eebdf15198ee48df3995af8a60bcc16"
   head "https://svn.cyberduck.io/trunk/"
 
   bottle do
@@ -14,19 +14,19 @@ class Duck < Formula
     sha256 "b2a3135eee96ba4ab7d66b049f16467e61c6cb69a6bbfd98b32c46ef570be9f0" => :mountain_lion
   end
 
-  depends_on :java => ["1.7+", :build]
+  depends_on :java => ["1.8+", :build]
   depends_on :xcode => :build
   depends_on "ant" => :build
 
   def install
     revision = version.to_s.rpartition(".").last
-    system "ant", "-Dbuild.compile.target=1.7", "-Drevision=#{revision}", "cli"
+    system "ant", "-Dbuild.compile.target=1.8", "-Drevision=#{revision}", "cli"
     libexec.install Dir["build/duck.bundle/*"]
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
 
   test do
-    system "#{bin}/duck", "--download", Formula["when"].stable.url, testpath/"test"
-    (testpath/"test").verify_checksum Formula["when"].stable.checksum
+    system "#{bin}/duck", "--download", Formula["wget"].stable.url, testpath/"test"
+    (testpath/"test").verify_checksum Formula["wget"].stable.checksum
   end
 end
