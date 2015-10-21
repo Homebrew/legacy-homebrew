e4399ae76bcd27b23cba19bc1645bbb9157fbdea
Mike McQuaid <mike@mikemcquaid.com>
2015-10-21 09:50:29 +0100

    android-sdk: don't need a bottle.

--- Library/Formula/android-sdk.rb
+++ Library/Formula/android-sdk.rb
@@ -7,12 +7,7 @@ class AndroidSdk < Formula
   version "24.4.1"
   sha256 "ce1638cb48526a0e55857fc46b57eda4349e6512006244ad13dd6c1361c74104"
 
-  bottle do
-    cellar :any
-    sha256 "6813bb19eb0127850832a32cddd55cfd0106a8d079726c3a9e8e18855efe0165" => :el_capitan
-    sha256 "7210e0118c1bbba916add540c9a1b7dc9b14b0c2cfbdbea9a1e0ca0e6b0143ec" => :yosemite
-    sha256 "e148339aa960c019ae70e967b3cadcd57fe93838067453b21264a8e330818a42" => :mavericks
-  end
+  bottle :unneeded
 
   conflicts_with "android-platform-tools",
     :because => "The Android Platform-Tools need to be installed as part of the SDK."
