b8d80a6c2f2dff801bb2774d9b5cc8f864c2d15f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-22 04:34:16 +0100

    parallel: update 20151022 bottle.

--- Library/Formula/parallel.rb
+++ Library/Formula/parallel.rb
@@ -8,9 +8,9 @@ class Parallel < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "90c9e106fcfa69afa562b316d17d5086df0c73d47c36029486376d1ce126f5ba" => :el_capitan
-    sha256 "4472b8e99d1f130f644d6db0f947c110af4a31445ec8911d8234b3dc8afb543c" => :yosemite
-    sha256 "809bb9988650900a45e44175866912a43186da03048b0be867fa8fb970b6a4d9" => :mavericks
+    sha256 "16b5500ff77aef1176ed4ff833673668c59acb54577cde7e618117ab32599c58" => :el_capitan
+    sha256 "df1e6595f2b1f23409aa9a6bdf4969e200c89ee64f8f3bd59faceffaf66658ea" => :yosemite
+    sha256 "d62094bce716ea935c2a8571370ba19c39c8956733b2d9391864cdeff261b1dd" => :mavericks
   end
 
   conflicts_with "moreutils", :because => "both install a 'parallel' executable."
