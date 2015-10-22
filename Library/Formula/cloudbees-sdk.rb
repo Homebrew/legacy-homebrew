3a2d2ef971b6bed332a603a55cdbc8e6342b0ff8
Xu Cheng <xucheng@me.com>
2015-10-22 21:12:05 +0800

    cloudbees-sdk: bottle unneeded

--- Library/Formula/cloudbees-sdk.rb
+++ Library/Formula/cloudbees-sdk.rb
@@ -4,6 +4,8 @@ class CloudbeesSdk < Formula
   url "https://cloudbees-downloads.s3.amazonaws.com/sdk/cloudbees-sdk-1.5.2-bin.zip"
   sha256 "48deb95a01e314c24ffa84b7ac99f98bfa24d9eea8e5883a18c7bd09a240ea8b"
 
+  bottle :unneeded
+
   def shim_script(target)
     <<-EOS.undent
       #!/bin/bash
