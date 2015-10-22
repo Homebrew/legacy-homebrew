8b726c468b70becbe36acdeb43e7f071f7c2a893
Xu Cheng <xucheng@me.com>
2015-10-22 21:55:06 +0800

    mozilla-addon-sdk: bottle unneeded

--- Library/Formula/mozilla-addon-sdk.rb
+++ Library/Formula/mozilla-addon-sdk.rb
@@ -4,6 +4,8 @@ class MozillaAddonSdk < Formula
   url "https://ftp.mozilla.org/pub/mozilla.org/labs/jetpack/addon-sdk-1.17.zip"
   sha256 "16e29d92214a556c8422db156b541fb8c47addfcb3cd879e0a4ca879d6a31f65"
 
+  bottle :unneeded
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/cfx"
