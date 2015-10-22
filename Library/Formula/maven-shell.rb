d8b437912459b6c09f8ead1d192ecc8d68f5170f
Xu Cheng <xucheng@me.com>
2015-10-22 21:52:44 +0800

    maven-shell: bottle unneeded

--- Library/Formula/maven-shell.rb
+++ Library/Formula/maven-shell.rb
@@ -4,11 +4,11 @@ class MavenShell < Formula
   url "https://repo1.maven.org/maven2/org/sonatype/maven/shell/dist/mvnsh-assembly/1.1.0/mvnsh-assembly-1.1.0-bin.tar.gz"
   sha256 "584008d726bf6f90271f4ccd03b549773cbbe62ba7e92bf131e67df3ac5a41ac"
 
+  bottle :unneeded
+
   def install
     # Remove windows files.
     rm_f Dir["bin/*.bat"]
-
-    prefix.install_metafiles
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/mvnsh"
   end
