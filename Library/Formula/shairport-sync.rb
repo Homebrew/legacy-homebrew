0aa9089321791cde37e0d575e67f10f96cd3bdde
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-22 14:30:23 +0100

    shairport-sync: add 2.6 bottle.

--- Library/Formula/shairport-sync.rb
+++ Library/Formula/shairport-sync.rb
@@ -5,6 +5,12 @@ class ShairportSync < Formula
   sha256 "d04036241e5a811240c43a3ddfb05a119a6043e8c5f1f354872a88e6cbdaef07"
   head "https://github.com/mikebrady/shairport-sync.git"
 
+  bottle do
+    sha256 "a97e873dc066b75114dec9d8ac929a9e882baaa4ec92f9df8b883f906cc2e24d" => :el_capitan
+    sha256 "961094f0cc116f3da6b42279e0011951f5d9533a8ffea924286bc08b9e384ac3" => :yosemite
+    sha256 "de98889a8902489520b8e8148fd76f8e65379f7ea980735fdc383dddd612155f" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
