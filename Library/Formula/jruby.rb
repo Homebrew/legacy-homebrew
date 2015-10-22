d37e215fc0724e3f393f283c6bd1c8db419c5d8c
Xu Cheng <xucheng@me.com>
2015-10-22 21:47:57 +0800

    jruby: bottle unneeded

--- Library/Formula/jruby.rb
+++ Library/Formula/jruby.rb
@@ -4,6 +4,8 @@ class Jruby < Formula
   url "https://s3.amazonaws.com/jruby.org/downloads/9.0.1.0/jruby-bin-9.0.1.0.tar.gz"
   sha256 "3cab8ead2f080eb9cdf16fc30cbc1974c36a4a4e6c9d321d5a3bbd973b64527f"
 
+  bottle :unneeded
+
   depends_on :java => "1.7+"
 
   def install
