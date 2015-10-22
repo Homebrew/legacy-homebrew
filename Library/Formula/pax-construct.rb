b75b86e2aa516a471097351d1f2ff01d9938f914
Xu Cheng <xucheng@me.com>
2015-10-22 21:58:13 +0800

    pax-construct: bottle unneeded

--- Library/Formula/pax-construct.rb
+++ Library/Formula/pax-construct.rb
@@ -4,6 +4,8 @@ class PaxConstruct < Formula
   url "https://repo1.maven.org/maven2/org/ops4j/pax/construct/scripts/1.5/scripts-1.5.zip"
   sha256 "d0325bbe571783097d4be782576569ea0a61529695c14e33a86bbebfe44859d1"
 
+  bottle :unneeded
+
   def install
     rm_rf Dir["bin/*.bat"]
     prefix.install_metafiles "bin" # Don't put these in bin!
