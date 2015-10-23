fa3847d73e8a81f0a17fcb369cb0e87e14410f38
Xu Cheng <xucheng@me.com>
2015-10-23 16:27:01 +0800

    antlr: bottle unneeded

--- Library/Formula/antlr.rb
+++ Library/Formula/antlr.rb
@@ -4,6 +4,8 @@ class Antlr < Formula
   url "http://www.antlr.org/download/antlr-4.5.1-complete.jar"
   sha256 "9cff6c76bc5aafcbf51cac7f0974ae01e4f6119402e75436abbb97f8ab15c211"
 
+  bottle :unneeded
+
   def install
     prefix.install "antlr-#{version}-complete.jar"
     bin.write_jar_script prefix/"antlr-#{version}-complete.jar", "antlr4"
