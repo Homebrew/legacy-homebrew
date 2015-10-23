353ac4283137cd4ea70cf8a6db4afcc4daa103a2
Xu Cheng <xucheng@me.com>
2015-10-23 16:26:24 +0800

    ansiweather: bottle unneeded

--- Library/Formula/ansiweather.rb
+++ Library/Formula/ansiweather.rb
@@ -3,8 +3,9 @@ class Ansiweather < Formula
   homepage "https://github.com/fcambus/ansiweather"
   url "https://github.com/fcambus/ansiweather/archive/1.04.tar.gz"
   sha256 "a85b0aba801f1fa3d386e92017c9b6af242b0243961a2a2f2e44096a8d7ddd75"
-
   head "https://github.com/fcambus/ansiweather.git"
+
+  bottle :unneeded
 
   depends_on "jq"
 
