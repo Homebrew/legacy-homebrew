c1270e34d35915c5f294cd95c05de5a8b8a078c6
Xu Cheng <xucheng@me.com>
2015-10-23 21:00:09 +0800

    sendemail: bottle unneeded

--- Library/Formula/sendemail.rb
+++ Library/Formula/sendemail.rb
@@ -4,6 +4,8 @@ class Sendemail < Formula
   url "http://caspian.dotconf.net/menu/Software/SendEmail/sendEmail-v1.56.tar.gz"
   sha256 "6dd7ef60338e3a26a5e5246f45aa001054e8fc984e48202e4b0698e571451ac0"
 
+  bottle :unneeded
+
   # Reported upstream: http://caspian.dotconf.net/menu/Software/SendEmail/#comment-1119965648
   patch do
     url "https://raw.githubusercontent.com/mogaal/sendemail/e785a6d284884688322c9b39c0f64e20a43ea825/debian/patches/fix_ssl_version.patch"
