bac8314c92bba44b909e61baa8eeb3b3a47d361f
Xu Cheng <xucheng@me.com>
2015-10-23 15:29:25 +0800

    storm: bottle unneeded

--- Library/Formula/storm.rb
+++ Library/Formula/storm.rb
@@ -4,6 +4,8 @@ class Storm < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=storm/apache-storm-0.9.5/apache-storm-0.9.5.tar.gz"
   sha256 "2e8337126de8d1e180abe77fb81af7c971f8c4b2dad94e446ac86c0f02ba3fb2"
 
+  bottle :unneeded
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/storm"
