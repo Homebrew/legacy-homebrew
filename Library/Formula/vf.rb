ac00edab48f28f61f3dd530c65a3a554e1655138
Xu Cheng <xucheng@me.com>
2015-10-23 16:05:50 +0800

    vf: bottle unneeded

--- Library/Formula/vf.rb
+++ Library/Formula/vf.rb
@@ -3,8 +3,9 @@ class Vf < Formula
   homepage "https://github.com/glejeune/vf"
   url "https://github.com/glejeune/vf/archive/0.0.1.tar.gz"
   sha256 "6418d188b88d5f3885b7a8d24520ac47accadb5144ae24e836aafbea4bd41859"
-
   head "https://github.com/glejeune/vf.git"
+
+  bottle :unneeded
 
   def install
     # Since the shell file is sourced instead of run
