365006aa67a66fe4287971c9f0aaa2359232300b
Xu Cheng <xucheng@me.com>
2015-10-23 19:00:51 +0800

    fleet-db: bottle unneeded

--- Library/Formula/fleet-db.rb
+++ Library/Formula/fleet-db.rb
@@ -4,6 +4,8 @@ class FleetDb < Formula
   url "https://fleetdb.s3.amazonaws.com/fleetdb-standalone-0.2.0.jar"
   sha256 "240df0ee690eedf5eed1fd791eca34f18d36852a88a38a521ce9d03748de07a8"
 
+  bottle :unneeded
+
   def install
     libexec.install "fleetdb-standalone-#{version}.jar"
     (bin+"fleetdb-server").write <<-EOS.undent
