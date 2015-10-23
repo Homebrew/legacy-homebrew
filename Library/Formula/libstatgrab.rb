f73c04f780583b3617cd1bab636f52f372306a47
Dominyk Tiller <dominyktiller@gmail.com>
2015-10-24 00:23:08 +0100

    libstatgrab: add secure mirror

--- Library/Formula/libstatgrab.rb
+++ Library/Formula/libstatgrab.rb
@@ -2,6 +2,7 @@ class Libstatgrab < Formula
   desc "Provides cross-platform access to statistics about the system."
   homepage "http://www.i-scream.org/libstatgrab/"
   url "http://ftp.i-scream.org/pub/i-scream/libstatgrab/libstatgrab-0.91.tar.gz"
+  mirror "https://www.mirrorservice.org/pub/i-scream/libstatgrab/libstatgrab-0.91.tar.gz"
   sha256 "03e9328e4857c2c9dcc1b0347724ae4cd741a72ee11acc991784e8ef45b7f1ab"
 
   bottle do
