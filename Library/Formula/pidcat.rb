271ddcdbe876d53e6239cc63d8a1f2c76c18aba5
Xu Cheng <xucheng@me.com>
2015-10-23 20:23:15 +0800

    pidcat: bottle unneeded

--- Library/Formula/pidcat.rb
+++ Library/Formula/pidcat.rb
@@ -1,9 +1,11 @@
 class Pidcat < Formula
   desc "Colored logcat script to show entries only for specified app"
   homepage "https://github.com/JakeWharton/pidcat"
-  head "https://github.com/JakeWharton/pidcat.git"
   url "https://github.com/JakeWharton/pidcat/archive/2.0.0.tar.gz"
   sha256 "4bb3d7bab7e124e355892ee9cf87de1d6f39bea201b03fce6449ca2486470656"
+  head "https://github.com/JakeWharton/pidcat.git"
+
+  bottle :unneeded
 
   def install
     bin.install "pidcat.py" => "pidcat"
