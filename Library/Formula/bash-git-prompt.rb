b99d521086b1f143bc1ae874d940dbd15c998210
Xu Cheng <xucheng@me.com>
2015-10-23 16:31:09 +0800

    bash-git-prompt: bottle unneeded

--- Library/Formula/bash-git-prompt.rb
+++ Library/Formula/bash-git-prompt.rb
@@ -5,6 +5,8 @@ class BashGitPrompt < Formula
   sha256 "35a8fe71a7b36b982b6cf53a0fb0c7f89559c29d65cd6cd5c95b26a19964b5ef"
   head "https://github.com/magicmonty/bash-git-prompt.git"
 
+  bottle :unneeded
+
   def install
     share.install "gitprompt.sh", "gitprompt.fish", "git-prompt-help.sh",
                   "gitstatus.sh", "prompt-colors.sh"
