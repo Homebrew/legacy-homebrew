31f004d7ca8c573a4e50ad0acae738ae38a8770f
Xu Cheng <xucheng@me.com>
2015-10-23 20:59:57 +0800

    selenium-server-standalone: bottle unneeded

--- Library/Formula/selenium-server-standalone.rb
+++ Library/Formula/selenium-server-standalone.rb
@@ -4,6 +4,8 @@ class SeleniumServerStandalone < Formula
   url "https://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar"
   sha256 "dd68dcfcd687b732683cf56722c599a8e36e510c0906ac476c2083cb5df0bb4e"
 
+  bottle :unneeded
+
   def install
     libexec.install "selenium-server-standalone-#{version}.jar"
     bin.write_jar_script libexec/"selenium-server-standalone-#{version}.jar", "selenium-server"
