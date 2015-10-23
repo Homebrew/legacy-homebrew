c7ad7938f1b24066cc1d2b3227c349994e1eaee5
Xu Cheng <xucheng@me.com>
2015-10-23 16:34:23 +0800

    brew-gem: bottle unneeded

--- Library/Formula/brew-gem.rb
+++ Library/Formula/brew-gem.rb
@@ -4,6 +4,8 @@ class BrewGem < Formula
   url "https://github.com/sportngin/brew-gem/archive/v0.5.2.tar.gz"
   sha256 "77ed5b2e4f3b15a9d316691c9e0fb8c25367d313d7f6426d03d733866ec4f75f"
 
+  bottle :unneeded
+
   def install
     bin.install "bin/brew-gem"
   end
