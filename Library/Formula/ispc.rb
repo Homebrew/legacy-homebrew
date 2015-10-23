14addb8d7e23a490f863c761db470bed2f5a8681
Xu Cheng <xucheng@me.com>
2015-10-23 19:54:47 +0800

    ispc: bottle unneeded

--- Library/Formula/ispc.rb
+++ Library/Formula/ispc.rb
@@ -4,6 +4,8 @@ class Ispc < Formula
   url "https://downloads.sourceforge.net/project/ispcmirror/v1.8.2/ispc-v1.8.2-osx.tar.gz"
   sha256 "9e42d7cca73e598d26727e4e5ab2f8b8e24d009fcb64424e566fa0f45329c2ec"
 
+  bottle :unneeded
+
   def install
     bin.install "ispc"
   end
