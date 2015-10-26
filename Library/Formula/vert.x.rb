e1ccf33edafa38e6b675e1c05f423e4e9be88f1b
Xu Cheng <xucheng@me.com>
2015-10-26 20:08:35 +0800

    vert.x: bottle unneeded

--- Library/Formula/vert.x.rb
+++ Library/Formula/vert.x.rb
@@ -4,6 +4,8 @@ class VertX < Formula
   url "https://dl.bintray.com/vertx/downloads/vert.x-3.1.0-full.tar.gz"
   sha256 "292eadeb7df7e3947142b8860d0088b2bfecd83d81e1cb9c9b2e5e79fde5731b"
 
+  bottle :unneeded
+
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin client conf lib]
