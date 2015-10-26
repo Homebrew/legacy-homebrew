f9f184277cfd91717af1f79f26f11e387babba3c
Viktor Szakats <vszakats@users.noreply.github.com>
2015-10-26 22:31:02 +0100

    git-tf: use secure urls
    Closes #45355.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/git-tf.rb
+++ Library/Formula/git-tf.rb
@@ -1,7 +1,7 @@
 class GitTf < Formula
   desc "Share changes between TFS and git"
-  homepage "http://gittf.codeplex.com/"
-  url "http://download.microsoft.com/download/A/E/2/AE23B059-5727-445B-91CC-15B7A078A7F4/git-tf-2.0.3.20131219.zip"
+  homepage "https://gittf.codeplex.com/"
+  url "https://download.microsoft.com/download/A/E/2/AE23B059-5727-445B-91CC-15B7A078A7F4/git-tf-2.0.3.20131219.zip"
   sha256 "91fd12e7db19600cc908e59b82104dbfbb0dbfba6fd698804a8330d6103aae74"
 
   head do
