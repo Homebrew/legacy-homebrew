e7e6ae00c384643806413f30a6218d9d9d0ecac2
Scott Dyer <sdyer@oscars.org>
2015-10-26 14:29:07 -0700

    aces_container 1.0.1
    Closes #45356.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/aces_container.rb
+++ Library/Formula/aces_container.rb
@@ -1,8 +1,8 @@
 class AcesContainer < Formula
-  desc "Reference implementation of SMPTE S2065-4"
+  desc "Reference implementation of SMPTE ST2065-4"
   homepage "https://github.com/ampas/aces_container"
-  url "https://github.com/ampas/aces_container/archive/v1.0.tar.gz"
-  sha256 "9d310605ce15b03689b1e5b7b3cf735e236b4d11a78654018321ae593af69aba"
+  url "https://github.com/ampas/aces_container/archive/v1.0.1.tar.gz"
+  sha256 "c1a5c08a128911faa5c8a1f9189c566178f19979ab627346238272c54343390a"
 
   depends_on "cmake" => :build
 
