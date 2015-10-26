421a892c9f25676034266ee86acb03b56c2e9933
Martin Klepsch <martinklepsch@googlemail.com>
2015-10-26 23:40:49 +0100

    boot: update to 2.4.0

--- Library/Formula/boot-clj.rb
+++ Library/Formula/boot-clj.rb
@@ -1,9 +1,9 @@
 class BootClj < Formula
   desc "Build tooling for Clojure"
   homepage "http://boot-clj.com"
-  url "https://github.com/boot-clj/boot/releases/download/2.2.0/boot.sh",
+  url "https://github.com/boot-clj/boot/releases/download/2.4.0/boot.sh",
       :using => :nounzip
-  sha256 "2e7d9c501ba3e59ae9f23ce21a8a9f8d24177a346238ee3710f7dd0adbddea33"
+  sha256 "7c6259cad9fbdeec0f95eb30936b0d936975f462666bc9f8ec4ba8170e128197"
 
   bottle :unneeded
 
