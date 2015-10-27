ac0c321f841af76ffb48438aa2fbf791e380c6ca
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-27 23:23:46 +0000

    carina: add 0.8.0 bottle.

--- Library/Formula/carina.rb
+++ Library/Formula/carina.rb
@@ -6,6 +6,13 @@ class Carina < Formula
         :revision => "094a0d2cfb24e245015f980127b9492889ed86f9"
   head "https://github.com/getcarina/carina.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a79c2155216ed381a75d80e1c5bff343d1c568b2a7bdb433902fb99c10b10680" => :el_capitan
+    sha256 "7d0d8c3221259b5ad1c69f278db6e53d51ff2ac976fcf60e1df4e626d77e8814" => :yosemite
+    sha256 "e756c8d3773b337634b9a00c31dec695fe1761c7604d7f813ee5f95bf5fbd625" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
