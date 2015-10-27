d9bb85ef1b2ed3432b96021396f2ad62546cd8f6
Kyle Kelley <rgbkrk@gmail.com>
2015-10-27 16:05:57 +0900

    Formula for Carina CLI

---
+++ Library/Formula/carina.rb
@@ -0,0 +1,27 @@
+class Carina < Formula
+  desc "Work with Swarm clusters on Carina"
+  homepage "https://github.com/getcarina/carina"
+  url "https://github.com/getcarina/carina.git",
+        :tag => "v0.8.0",
+        :revision => "094a0d2cfb24e245015f980127b9492889ed86f9"
+  head "https://github.com/getcarina/carina.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    carinapath = buildpath/"src/github.com/getcarina/carina"
+    carinapath.install Dir["{*,.git}"]
+
+    cd carinapath do
+      system "make", "get-deps"
+      system "make", "carina", "VERSION=#{version}"
+      bin.install "carina"
+    end
+  end
+
+  test do
+    system "#{bin}/carina", "--version"
+  end
+end
