d84f4bced14fd5828eda35080de6eba5b390a46a
Xu Cheng <xucheng@me.com>
2015-10-27 20:48:04 +0800

    tika: nit

--- Library/Formula/tika.rb
+++ Library/Formula/tika.rb
@@ -4,9 +4,9 @@ class Tika < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=tika/tika-app-1.11.jar"
   sha256 "76f23849c0fe116efda3d89f9021d74415727f6f98b2860c24247c299cae719c"
 
-  depends_on :java => "1.7+"
-
   bottle :unneeded
+
+  depends_on :java => "1.7+"
 
   resource "server" do
     url "https://repo1.maven.org/maven2/org/apache/tika/tika-server/1.11/tika-server-1.11.jar"
