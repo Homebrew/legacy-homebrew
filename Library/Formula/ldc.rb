a3e97b00b05e71f469fad852c151ce8322b0a356
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-28 09:03:30 +0000

    ldc: update 0.16.1 bottle.

--- Library/Formula/ldc.rb
+++ Library/Formula/ldc.rb
@@ -7,9 +7,9 @@ class Ldc < Formula
   head "https://github.com/ldc-developers/ldc.git", :shallow => false
 
   bottle do
-    sha256 "785ffe1214f8b4eaf48d1eae2dd32322c31e9a8d88578c686d406e7aa686b49f" => :el_capitan
-    sha256 "06bfbcc223532a3d47705c8e1e729fa74c6ae6ad8585fa92ad6012b80311e203" => :yosemite
-    sha256 "6403ea94c78c34e30b40007ff27898569627fc9d837b23717af2966dad3b88aa" => :mavericks
+    sha256 "d358950e5bc80c8c51303b033f20f68496c65c15cdb74a06c8ad7b386ca9907b" => :el_capitan
+    sha256 "0ed16e8ad04729ce88eef3ee436fbe14acf99941fffbcffa40b94724c105176a" => :yosemite
+    sha256 "0d1f8e211e525e8eb106aa1ec6553240d8813e82dfec8bca0adc3cda590fc9a8" => :mavericks
   end
 
   needs :cxx11
