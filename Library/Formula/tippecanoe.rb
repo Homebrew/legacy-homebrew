f81a192be710d7495bc23a6a3bb6d3140067e82f
Eric Fischer <enf@pobox.com>
2015-10-29 11:40:45 -0700

    tippecanoe 1.3.1

--- Library/Formula/tippecanoe.rb
+++ Library/Formula/tippecanoe.rb
@@ -1,8 +1,8 @@
 class Tippecanoe < Formula
   desc "Build vector tilesets from collections of GeoJSON features"
   homepage "https://github.com/mapbox/tippecanoe"
-  url "https://github.com/mapbox/tippecanoe/archive/v1.2.0.tar.gz"
-  sha256 "237510a1a92a8626407c29c8d8047e73bbb22c1a8af8f9d1b8931d994c8fac2d"
+  url "https://github.com/mapbox/tippecanoe/archive/v1.3.1.tar.gz"
+  sha256 "6c393e99ab7a75c9a3a5a7da4681794df283c0f2312d58edfcf2910a6726c676"
 
   bottle do
     cellar :any
