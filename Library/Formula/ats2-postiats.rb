5877b467921fdeae4bc7663f7fe5c720b319c305
Hiroshi Hatake <hatake@clear-code.com>
2015-10-29 10:50:13 +0900

    ats2-postiats 0.2.4
    Closes #45436.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/ats2-postiats.rb
+++ Library/Formula/ats2-postiats.rb
@@ -1,8 +1,8 @@
 class Ats2Postiats < Formula
   desc "ATS programming language implementation"
   homepage "http://www.ats-lang.org/"
-  url "https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.2.3/ATS2-Postiats-0.2.3.tgz"
-  sha256 "a2a50305ddfc8c88d475e0378a9f476887d11c0a64f381b849f2b9f1746258cd"
+  url "https://downloads.sourceforge.net/project/ats2-lang/ats2-lang/ats2-postiats-0.2.4/ATS2-Postiats-0.2.4.tgz"
+  sha256 "33365e1cab4f897ab24176a0988980dc4830bf8b35bf16daa8fb745006abb6a8"
 
   bottle do
     cellar :any
