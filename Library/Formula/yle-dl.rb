02081b952df3024ef95f04d177717314a394eeee
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-29 10:57:30 +0100

    yle-dl 2.9.0

--- Library/Formula/yle-dl.rb
+++ Library/Formula/yle-dl.rb
@@ -1,8 +1,8 @@
 class YleDl < Formula
   desc "Download Yle videos from the command-line"
   homepage "https://aajanki.github.io/yle-dl/index-en.html"
-  url "https://github.com/aajanki/yle-dl/archive/2.7.2.tar.gz"
-  sha256 "7c4bea154ce91f171876c98d80d8a7cc56d0ffe925ed41302a09866226bdda92"
+  url "https://github.com/aajanki/yle-dl/archive/2.9.0.tar.gz"
+  sha256 "085d0fd58d2f04447d2ecf0c1dc49fdc373b819b8bf18cd7185a67956981c8b2"
 
   head "https://github.com/aajanki/yle-dl.git"
 
@@ -19,8 +19,8 @@ class YleDl < Formula
   resource "AdobeHDS.php" do
     # NOTE: yle-dl always installs the HEAD version of AdobeHDS.php. We use a specific commit.
     # Check if there are bugfixes at https://github.com/K-S-V/Scripts/commits/master/AdobeHDS.php
-    url "https://raw.githubusercontent.com/K-S-V/Scripts/460814d210e8e19a98bddd12c178fc3b5f43b0bb/AdobeHDS.php"
-    sha256 "7087e3857ef7335717e142996e27c5546806b0babd92cd540d783ae4ce2bcf3f"
+    url "https://raw.githubusercontent.com/K-S-V/Scripts/4a2f5199c815d8df71fb68a948ea475b9755e85c/AdobeHDS.php"
+    sha256 "510418a1f4f925aabcef5400c77ee49ecbc9aacadd01016bd8f48edd592e511c"
   end
 
   resource "pycrypto" do
