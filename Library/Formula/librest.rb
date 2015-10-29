4d9b7f5fed9d510155d7c05770ecb6e3212b59d9
Martin Müller <mail@mueller-martin.net>
2015-10-30 00:06:56 +0100

    librest 0.7.93
    librest (sometimes just called rest) is a rather small library to access
    RESTful APIs as a part of the GNOME project.
    
    After issues #31480 and #31501 weren't successful in adding librest to
    Homebrew. Here is my try ;)
    
    The test is rather useless (doesn't do anything useful) but should
    compile if the library is installed correctly and works.

---
+++ Library/Formula/librest.rb
@@ -0,0 +1,40 @@
+class Librest < Formula
+  desc "librest is a library access web services that claim to be RESTful"
+  homepage "https://wiki.gnome.org/Projects/Librest"
+  url "https://download.gnome.org/sources/rest/0.7/rest-0.7.93.tar.xz"
+  sha256 "c710644455340a44ddc005c645c466f05c0d779993138ea21a62c6082108b216"
+
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+  depends_on "libsoup"
+  depends_on "gobject-introspection"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--without-gnome",
+                          "--without-ca-certificates",
+                          "--enable-introspection=yes"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <stdlib.h>
+      #include <rest/rest-proxy.h>
+
+      int main(int argc, char *argv[]) {
+        RestProxy *proxy = rest_proxy_new("http://localhost", FALSE);
+
+        g_object_unref(proxy);
+
+        return EXIT_SUCCESS;
+      }
+    EOS
+    flags = (ENV.cflags || "").split + (ENV.ldflags || "").split
+    flags += `pkg-config --cflags --libs rest-0.7`.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
