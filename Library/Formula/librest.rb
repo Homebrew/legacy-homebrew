64d2bdf876b8d5ce53c6afb68881dfea89766658
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-30 02:03:26 +0000

    librest: add 0.7.93 bottle.

--- Library/Formula/librest.rb
+++ Library/Formula/librest.rb
@@ -4,6 +4,12 @@ class Librest < Formula
   url "https://download.gnome.org/sources/rest/0.7/rest-0.7.93.tar.xz"
   sha256 "c710644455340a44ddc005c645c466f05c0d779993138ea21a62c6082108b216"
 
+  bottle do
+    sha256 "9a85440762ae254d1dca4442499cef96777539cb6266e70a4b8dd72496ed1d25" => :el_capitan
+    sha256 "31ffb12406788d8352991ce2342a1c971db18033bd140a19bfd7486664cd8235" => :yosemite
+    sha256 "5350da99c8645c1b2f3ca7d00b5c9e11e25c7b8c9802dc697721a2f8bea7f2c0" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glib"
   depends_on "libsoup"
