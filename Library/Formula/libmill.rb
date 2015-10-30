b26f46e93a604948882079e4e33c94e5b7fb318e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-30 14:10:43 +0000

    libmill: add 1.0 bottle.

--- Library/Formula/libmill.rb
+++ Library/Formula/libmill.rb
@@ -5,6 +5,13 @@ class Libmill < Formula
   sha256 "746fd55c4ae31b8c2142c9a413a4dacfb47e3db970ec7ea5bc4053525f9c12e0"
   head "https://github.com/sustrik/libmill.git"
 
+  bottle do
+    cellar :any
+    sha256 "64b90de816220a7e02a627678fa588468359ba61a738206946d2eac9c68ba196" => :el_capitan
+    sha256 "5c47d4104804d83b950ccf37ca7e0b39c9b9103365f92bd618f8e1f016b455cb" => :yosemite
+    sha256 "6a564800bfb635852693969f87d0c3c95980bfb0a054f187e28b8414567a9bf0" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
