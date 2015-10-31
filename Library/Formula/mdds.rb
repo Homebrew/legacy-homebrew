ef01d1d92e8f57da8f75d3fb70c934f29ef875d8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-31 10:42:55 +0000

    mdds: add 1.0.0 bottle.

--- Library/Formula/mdds.rb
+++ Library/Formula/mdds.rb
@@ -4,6 +4,13 @@ class Mdds < Formula
   url "http://kohei.us/files/mdds/src/mdds_1.0.0.tar.bz2"
   sha256 "ef8abc1236b54c7ca16ae1ee38abfb9cdbc5d1e6a2427c65b92b8c1003e3bf56"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fac0ca8a8b5fd716123480278668ff6e91a2f9c8550e1566fa4d77d364dca7a3" => :el_capitan
+    sha256 "b5f47377a54114b6145bd613fd827626506264c5febe8a6cf7a52619415e8505" => :yosemite
+    sha256 "9b85733acd96f6cf98db27e6b19aeb03fa83f63d5522dd3684933a1a1bd95523" => :mavericks
+  end
+
   depends_on "boost"
   needs :cxx11
 
