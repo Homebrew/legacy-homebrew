0a0422407cf428eb1f31ff28a6d8e37911c975cc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-31 13:06:45 +0000

    libunwind-headers: update 35.3 bottle.

--- Library/Formula/libunwind-headers.rb
+++ Library/Formula/libunwind-headers.rb
@@ -6,11 +6,9 @@ class LibunwindHeaders < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
-    sha256 "1808a41cead20a776ed07529489c823ba6f1856782ffa920e9ad8f4e7f567d1e" => :el_capitan
-    sha1 "48f3fb9243e6a5cd3255a9bc43755aedde545a5d" => :yosemite
-    sha1 "022d85a3a7e2368bd9a3cbc679d69f074534f50f" => :mavericks
-    sha1 "260e0407173d01261b9a2cb79ccc73ca92866437" => :mountain_lion
+    sha256 "308e12c8084e3dd179e322320d45b7ebf3393b284cbb6a5754ccde9e577ad90e" => :el_capitan
+    sha256 "0a920420880c1222c365fb886eaf0cbc215529e7a7be39280520dc770386fe75" => :yosemite
+    sha256 "2b4ae1b1a438269ae833a52e73cafd6357b0d30dd5e8eb33ef29271cdc259f7c" => :mavericks
   end
 
   keg_only :provided_by_osx,
