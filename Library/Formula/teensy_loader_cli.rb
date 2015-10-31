cc1dca697e3b7ca087fa19e20c94d3acbadfaade
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-31 20:02:31 +0000

    teensy_loader_cli: add 2.1 bottle.

--- Library/Formula/teensy_loader_cli.rb
+++ Library/Formula/teensy_loader_cli.rb
@@ -5,6 +5,13 @@ class TeensyLoaderCli < Formula
   sha256 "dafd040d6748b52e0d4a01846d4136f3354ca27ddc36a55ed00d0a0af0902d46"
   head "https://github.com/PaulStoffregen/teensy_loader_cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6328eeb1ed51edb8527874bd7c0f1a45cdf4052f0c35863dd1a67b5e0644c57e" => :el_capitan
+    sha256 "90d5b5bf9adbece0001da72b1881617406bb9eeb76ff97ad5989e779179f5590" => :yosemite
+    sha256 "dcd10140babb4d2937ce376c89e9c24a2e8046d2cabdad2cfdbc2542afa14471" => :mavericks
+  end
+
   def install
     ENV["OS"] = "MACOSX"
     ENV["SDK"] = MacOS.sdk_path || "/"
