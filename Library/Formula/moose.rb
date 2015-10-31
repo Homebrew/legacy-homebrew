fe85fc3c6dd8775f62eff7146878fe22db0e0df7
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-31 10:24:28 +0530

    Fixed indentation issues.

--- Library/Formula/moose.rb
+++ Library/Formula/moose.rb
@@ -1,13 +1,12 @@
 class Moose < Formula
-  desc "Multiscale Object Oriented Simulation Environment for simulating multiscale neural systems."
+  desc "Multiscale Object Oriented Simulation Environment for  multiscale neural systems."
   homepage "http://moose.ncbs.res.in"
   url "https://github.com/BhallaLab/moose-core/archive/ghevar-3.0.2-alpha.zip"
-  version "3.0.2"
   sha256 "febc7fc41a2513aeb2b6998731f61c7e7f2a6b5169a77e1612c189779fb0d1f1"
 
   depends_on "cmake" => :build
-  depends_on "gsl" 
-  depends_on "homebrew/science/hdf5" 
+  depends_on "gsl"
+  depends_on "homebrew/science/hdf5"
   depends_on "homebrew/science/libsbml" => :optional
   depends_on "homebrew/python/matplotlib"
   depends_on "homebrew/python/numpy"
@@ -20,7 +19,6 @@ class Moose < Formula
   end
 
   resource "examples" do
-    version "0.9.0"
     url "https://github.com/BhallaLab/moose-examples/archive/0.9.0.zip"
     sha256 "6330fb610368537e61327f9fa4667caedea0d590c490e2b1c4816d9a0d5adf35"
   end
@@ -36,11 +34,11 @@ class Moose < Formula
       system "python", *Language::Python.setup_install_args(prefix)
     }
 
-    libexec.install resource("gui")  
-    doc.install resource("examples")  
+    libexec.install resource("gui")
+    doc.install resource("examples")
 
     # A wrapper script to launch moose gui.
-    (bin/"moosegui").write <<-EOS.undent 
+    (bin/"moosegui").write <<-EOS.undent
       #!/bin/bash
       BASEDIR="#{libexec}"
       (cd $BASEDIR && python mgui.py)
@@ -50,7 +48,7 @@ class Moose < Formula
     bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
-  def test 
+  def test
     Dir.cd("tests/python") {
       system "bash", "./test_all.sh"
     }
