485f9981743274ec83335dae304768788ad95b1e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-01 10:03:40 +0000

    ykneomgr: add 0.1.8 bottle.

--- Library/Formula/ykneomgr.rb
+++ Library/Formula/ykneomgr.rb
@@ -4,6 +4,13 @@ class Ykneomgr < Formula
   url "https://developers.yubico.com/libykneomgr/Releases/libykneomgr-0.1.8.tar.gz"
   sha256 "2749ef299a1772818e63c0ff5276f18f1694f9de2137176a087902403e5df889"
 
+  bottle do
+    cellar :any
+    sha256 "56a5439c432c82b45b9722f22157b7194ed5604d0e67ab4189a9a07f2abf0325" => :el_capitan
+    sha256 "cd80f8068e58d2c4982198eb398a85e816f772306efd7263e10a38d680c67190" => :yosemite
+    sha256 "8c9a67b67b45f981fec4f6fa3a8a512e2d39055866b8c3df941fc16bcbf5c4c0" => :mavericks
+  end
+
   head do
     url "https://github.com/Yubico/libykneomgr.git"
     depends_on "autoconf" => :build
