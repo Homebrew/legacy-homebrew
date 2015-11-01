2ed97d359262be4e549c078a7e2d4d326a85c384
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-01 23:13:08 +0100

    elixir-build: conflicts with narwhal

--- Library/Formula/elixir-build.rb
+++ Library/Formula/elixir-build.rb
@@ -5,6 +5,8 @@ class ElixirBuild < Formula
   url "https://github.com/mururu/elixir-build/archive/v20141001.tar.gz"
   sha256 "825637780a580b7ebe8c5265a43d37ceff9f3876e771aa2f824079e504ad7347"
 
+  conflicts_with "narwhal", :because => "both install `json` binaries"
+
   def install
     ENV["PREFIX"] = prefix
     system "./install.sh"
