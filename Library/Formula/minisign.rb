0b4073b017c9a31c425112c0b2064988c0b4d981
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-01 23:49:07 +0000

    minisign: bump revision for libsodium

--- Library/Formula/minisign.rb
+++ Library/Formula/minisign.rb
@@ -3,6 +3,7 @@ class Minisign < Formula
   homepage "https://jedisct1.github.io/minisign/"
   url "https://github.com/jedisct1/minisign/archive/0.4.tar.gz"
   sha256 "dc7695513e715654a51d07ad3e6b0083f9cb38b1a5bc9f16e1177d15af992dcc"
+  revision 1
 
   bottle do
     cellar :any
