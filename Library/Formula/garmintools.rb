7e5b6395f8b4486c227f184f2ea61f01d6e11ae9
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-02 00:31:17 +0100

    garmintools: test added
    Closes #45592.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/garmintools.rb
+++ Library/Formula/garmintools.rb
@@ -1,16 +1,19 @@
-require 'formula'
-
 class Garmintools < Formula
   desc "Interface to the Garmin Forerunner GPS units"
-  homepage 'http://code.google.com/p/garmintools/'
-  url 'https://garmintools.googlecode.com/files/garmintools-0.10.tar.gz'
-  sha1 'f59a761b09575d27abbf5d76811f7ec25a1cbd26'
+  homepage "https://code.google.com/p/garmintools/"
+  url "https://garmintools.googlecode.com/files/garmintools-0.10.tar.gz"
+  sha256 "ffd50b7f963fa9b8ded3223c4786b07906c887ed900de64581a24ff201444cee"
 
-  depends_on 'libusb-compat'
+  depends_on "libusb-compat"
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/garmin_dump"
   end
 end
