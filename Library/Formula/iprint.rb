a057ea6824527a614aa00225471a7bc1f67a55c5
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-01 20:53:13 +0000

    iprint: remove redundant line

--- Library/Formula/iprint.rb
+++ Library/Formula/iprint.rb
@@ -1,10 +1,10 @@
 class Iprint < Formula
   desc "Provides a print_one function"
   homepage "https://www.samba.org/ftp/unpacked/junkcode/i.c"
-  url "https://mirrors.kernel.org/debian/pool/main/i/iprint/iprint_1.3.orig.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/iprint/iprint_1.3.orig.tar.gz"
-  sha256 "1079b2b68f4199bc286ed08abba3ee326ce3b4d346bdf77a7b9a5d5759c243a3"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/iprint/iprint_1.3.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/i/iprint/iprint_1.3.orig.tar.gz"
   version "1.3-9"
+  sha256 "1079b2b68f4199bc286ed08abba3ee326ce3b4d346bdf77a7b9a5d5759c243a3"
 
   bottle do
     cellar :any
@@ -15,14 +15,14 @@ class Iprint < Formula
   end
 
   patch do
-    url "https://mirrors.kernel.org/debian/pool/main/i/iprint/iprint_1.3-9.diff.gz"
+    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/iprint/iprint_1.3-9.diff.gz"
+    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/i/iprint/iprint_1.3-9.diff.gz"
     sha256 "3a1ff260e6d639886c005ece754c2c661c0d3ad7f1f127ddb2943c092e18ab74"
   end
 
   def install
     system "make"
     bin.install "i"
-    man1.mkpath
     man1.install "i.1"
   end
 
