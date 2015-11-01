77133f689b2dedbff397f4bd953a7d53199d993f
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2015-11-01 08:57:22 +0000

    gst-python 1.6.1

--- Library/Formula/gst-python.rb
+++ Library/Formula/gst-python.rb
@@ -1,8 +1,8 @@
 class GstPython < Formula
   desc "GStreamer Python overrides for gobject-introspection-based pygst bindings"
   homepage "http://gstreamer.freedesktop.org/modules/gst-python.html"
-  url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.6.0.tar.xz"
-  sha256 "60dbc7f5ac2b6655ed4a7ed2cee63ee5a525d37cb16eaa6b9df7d7dfe4e9605f"
+  url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.6.1.tar.xz"
+  sha256 "3cbe332e18fd2eaf23ddeee96c414f79ac1edc2f7d990582fa0ec5f977bd04f1"
 
   bottle do
     sha256 "2493558defcd65f0b0f486344e679f594feef4b550fd15f4f6946dbaaeab2ef9" => :el_capitan
@@ -14,6 +14,9 @@ class GstPython < Formula
   depends_on "pygobject3"
 
   def install
+    # ensure files are kept inside the keg
+    # this patch is necessary as long as gobject-introspection requires :python on older OS X releases
+    inreplace "gi/overrides/Makefile.in", "$(PYGI_OVERRIDES_DIR)", "@libdir@/python2.7/site-packages/gi/overrides" if MacOS.version <= :mavericks
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
