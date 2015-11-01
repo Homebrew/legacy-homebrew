247f6c721c076d613a343b8ef89a161e488f67ba
forkbomber <forkbomber@users.noreply.github.com>
2015-11-01 00:39:40 -0700

    ykneomgr 0.1.8
    Closes #45572.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/ykneomgr.rb
+++ Library/Formula/ykneomgr.rb
@@ -1,12 +1,11 @@
-require 'formula'
-
 class Ykneomgr < Formula
-  homepage 'https://developers.yubico.com/libykneomgr/'
-  url 'https://developers.yubico.com/libykneomgr/Releases/libykneomgr-0.1.6.tar.gz'
-  sha1 '9e0d39544421788223baaea176c3ecafc4290589'
+  desc "CLI and C library to interact with the CCID-part of the YubiKey NEO"
+  homepage "https://developers.yubico.com/libykneomgr/"
+  url "https://developers.yubico.com/libykneomgr/Releases/libykneomgr-0.1.8.tar.gz"
+  sha256 "2749ef299a1772818e63c0ff5276f18f1694f9de2137176a087902403e5df889"
 
   head do
-    url 'https://github.com/Yubico/libykneomgr.git'
+    url "https://github.com/Yubico/libykneomgr.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
@@ -17,7 +16,7 @@ class Ykneomgr < Formula
 
   depends_on "help2man" => :build  # <- not supposed to, but it does
   depends_on "pkg-config" => :build
-  depends_on 'libzip'
+  depends_on "libzip"
 
   def install
     ENV.universal_binary if build.universal?
@@ -28,5 +27,9 @@ class Ykneomgr < Formula
                           "--prefix=#{prefix}"
     system "make", "check"
     system "make", "install"
+  end
+
+  test do
+    assert_match "0.1.8", shell_output("#{bin}/ykneomgr --version")
   end
 end
