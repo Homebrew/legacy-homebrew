2fe37a7d7af08f0380bc4a3152b48a8d1632104a
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-01 20:28:17 +0000

    axel: use secure mirror

--- Library/Formula/axel.rb
+++ Library/Formula/axel.rb
@@ -1,8 +1,8 @@
 class Axel < Formula
   desc "Light UNIX download accelerator"
   homepage "https://packages.debian.org/sid/axel"
-  url "https://mirrors.kernel.org/debian/pool/main/a/axel/axel_2.4.orig.tar.gz"
-  mirror "http://ftp.de.debian.org/debian/pool/main/a/axel/axel_2.4.orig.tar.gz"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/a/axel/axel_2.4.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/a/axel/axel_2.4.orig.tar.gz"
   sha256 "359a57ab4e354bcb6075430d977c59d33eb3e2f1415a811948fa8ae657ca8036"
 
   bottle do
