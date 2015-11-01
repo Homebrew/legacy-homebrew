944ef40d7bd8e7444e8b11af02a1477b75f97936
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-01 20:35:12 +0000

    debianutils: prefer berkeley debian mirror

--- Library/Formula/debianutils.rb
+++ Library/Formula/debianutils.rb
@@ -1,8 +1,8 @@
 class Debianutils < Formula
   desc "Miscellaneous utilities specific to Debian"
-  homepage "https://packages.debian.org/unstable/utils/debianutils"
-  url "https://mirrors.kernel.org/debian/pool/main/d/debianutils/debianutils_4.5.1.tar.xz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/debianutils/debianutils_4.5.1.tar.xz"
+  homepage "https://packages.debian.org/sid/debianutils"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/debianutils/debianutils_4.5.1.tar.xz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/d/debianutils/debianutils_4.5.1.tar.xz"
   sha256 "a531c23e0105fe01cfa928457a8343a1e947e2621b3cd4d05f4e9656020c63b7"
 
   bottle do
@@ -14,10 +14,11 @@ class Debianutils < Formula
 
   def install
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make"
 
-    # some commands are Debian Linux specific and we don't want them, so install specific tools
+    # Some commands are Debian Linux specific and we don't want them, so install specific tools
     bin.install "run-parts", "ischroot", "tempfile"
     man1.install "ischroot.1", "tempfile.1"
     man8.install "run-parts.8"
