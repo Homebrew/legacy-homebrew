ab480eefa4cbaca9ec5bc3b4f7ab5e437133644c
Xu Cheng <xucheng@me.com>
2015-11-01 20:33:24 +0800

    add Formula#bottle_defined?

--- Library/Homebrew/software_spec.rb
+++ Library/Homebrew/software_spec.rb
@@ -75,6 +75,10 @@ class SoftwareSpec
     @bottle_disable_reason
   end
 
+  def bottle_defined?
+    bottle_specification.collector.keys.any?
+  end
+
   def bottled?
     bottle_specification.tag?(bottle_tag) && \
       (bottle_specification.compatible_cellar? || ARGV.force_bottle?)
