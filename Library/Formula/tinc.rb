7f6bb955779443215785163a49bde502b10686cc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-02 14:15:10 +0000

    tinc: update 1.0.26 bottle.

--- Library/Formula/tinc.rb
+++ Library/Formula/tinc.rb
@@ -5,9 +5,9 @@ class Tinc < Formula
   sha256 "2b4319ddb3bd2867e72532a233f640a58c2f4d83f1088183ae544b268646ba21"
 
   bottle do
-    sha1 "359656333342ffc8fa80ca09a89390833816b163" => :yosemite
-    sha1 "c70ebe54e832e8e920a74e92764306439d9811a4" => :mavericks
-    sha1 "762f671f4ec7065728fdd9450e8a363a2db24601" => :mountain_lion
+    sha256 "028353499ec60475476a59643463ab35cbd562c352b00ed6700b7e9ba8748a15" => :el_capitan
+    sha256 "2985e4a5eb9784448887857ed7ae3a7dc0ef9df4505d3592dec865f6f9bbaed0" => :yosemite
+    sha256 "0313b51953e300bf60c87722c8432c902c20d6b0fc484a03dc5796e3bbb87b6b" => :mavericks
   end
 
   devel do
