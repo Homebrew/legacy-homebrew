24d9b76d4f5c64870ffd18eb7a8a03d152f8962d
JP Simard <jp@jpsim.com>
2015-11-03 13:19:27 -0800

    [update] swiftlint 0.3.0

--- Library/Formula/swiftlint.rb
+++ Library/Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/realm/SwiftLint"
-  url "https://github.com/realm/SwiftLint.git", :tag => "0.2.0", :revision => "a71c9fbf015405de3ae2a92d051876074cfbf4ec"
+  url "https://github.com/realm/SwiftLint.git", :tag => "0.3.0", :revision => "ae595d67edc24383446a346e218d66a00994bab5"
   head "https://github.com/realm/SwiftLint.git"
 
   bottle do
@@ -10,7 +10,7 @@ class Swiftlint < Formula
     sha256 "b02a94fc5e994bb939dbe425edef01cd819d31beb3126abc3f62158bafa8dc82" => :yosemite
   end
 
-  depends_on :xcode => ["7.0", :build]
+  depends_on :xcode => ["7.1", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
