842ab5c45741668b593513a4e8cf93a47ddf809a
Shaun Wheelhouse <s@shaunwheelhou.se>
2015-11-03 17:42:26 +1100

    Updated source to conform to project style.

--- Library/Formula/unicorn.rb
+++ Library/Formula/unicorn.rb
@@ -1,16 +1,11 @@
 class Unicorn < Formula
-  desc "A lightweight multi-platform, multi-architecture CPU emulation framework"
-  head "https://github.com/unicorn-engine/unicorn.git"
+  desc "Lightweight multi-architecture CPU emulation framework"
+  homepage "http://www.unicorn-engine.org"
   url "https://github.com/unicorn-engine/unicorn/archive/0.9.tar.gz"
   sha256 "1ca03b1c8f6360335567b528210713461e839d47c4eb7c676ba3aa4f72b8cf10"
-  homepage "http://www.unicorn-engine.org"
+  head "https://github.com/unicorn-engine/unicorn.git"
 
-  depends_on 'capstone'
-  depends_on 'glib'
-  depends_on 'pkg-config' => :build
-  depends_on 'python'
-
-  option "debug", "Create a debug build"
+  option "with-debug", "Create a debug build"
   option "without-x86", "Build without x86 support"
   option "without-x86_64", "Build without x86_64 support"
   option "without-static", "Don't build static libraries"
@@ -23,21 +18,30 @@ class Unicorn < Formula
   option "with-shared", "Build with shared library support"
   option "with-sparc", "Build with SPARC support"
 
+  depends_on "capstone"
+  depends_on "glib"
+  depends_on "pkg-config" => :build
+  depends_on "python"
+
   def install
     archs = []
-    ['x86', 'x86_64'].each do |arch|
+    ["x86", "x86_64"].each do |arch|
       archs << arch if build.with? arch
     end
-    ['arm', 'aarch64', 'm64k', 'mips', 'ppc', 'sparc'].each do |arch|
-      if build.with? 'all' or build.with? arch
+    ["arm", "aarch64", "m64k", "mips", "ppc", "sparc"].each do |arch|
+      if (build.with? "all") || (build.with? arch)
         archs << arch
       end
     end
-    ENV['PREFIX'] = prefix
-    ENV['UNICORN_ARCHS'] = archs.join ' '
-    ['static', 'shared', 'debug'].each do |condition|
-      ENV["UNICORN_#{condition.upcase}"] = build.with?(condition) ? 'yes' : 'no'
+    ENV["PREFIX"] = prefix
+    ENV["UNICORN_ARCHS"] = archs.join " "
+    ["static", "shared", "debug"].each do |condition|
+      ENV["UNICORN_#{condition.upcase}"] = build.with?(condition) ? "yes" : "no"
     end
     system "make", "install"
+  end
+
+  test do
+    system "make", "test"
   end
 end
