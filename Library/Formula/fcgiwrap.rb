4075c7fe74da9889ef26a1c394dd12db496a9332
Thierry Moisan <thierry.moisan@gmail.com>
2015-11-03 22:12:37 -0500

    fcgiwrap: update homepage
    Closes #45669.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/fcgiwrap.rb
+++ Library/Formula/fcgiwrap.rb
@@ -1,6 +1,6 @@
 class Fcgiwrap < Formula
   desc "CGI support for Nginx"
-  homepage "http://nginx.localdomain.pl/wiki/FcgiWrap"
+  homepage "https://www.nginx.com/resources/wiki/start/topics/examples/fcgiwrap/"
   url "https://github.com/gnosek/fcgiwrap/archive/1.1.0.tar.gz"
   sha256 "4c7de0db2634c38297d5fcef61ab4a3e21856dd7247d49c33d9b19542bd1c61f"
 
