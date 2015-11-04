2219f40b553c551baab57c75cd0d073b30719b8e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-04 06:49:25 +0000

    appledoc: add 2.2.1 bottle.

--- Library/Formula/appledoc.rb
+++ Library/Formula/appledoc.rb
@@ -6,6 +6,12 @@ class Appledoc < Formula
 
   head "https://github.com/tomaz/appledoc.git"
 
+  bottle do
+    sha256 "c723bec6cdeb0eb067d7c67cee472f20a6f1935e1bbd6b0a3aa0ec7f77fea583" => :el_capitan
+    sha256 "ada12050d25be7a3c9920b1b4e2aa8d8a1efa7d59d9e67325f4e83dab14d0f59" => :yosemite
+    sha256 "dede0bad06c61e56350c5fc812e1c507d3b2e0b73b6d062eedfe8e47f39b74fb" => :mavericks
+  end
+
   depends_on :xcode => :build
   depends_on :macos => :lion
 
