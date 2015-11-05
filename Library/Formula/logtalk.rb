6d5534ae93fb6992a122c6732894608472164af2
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-05 19:08:12 +0000

    logtalk: use new option syntax

--- Library/Formula/logtalk.rb
+++ Library/Formula/logtalk.rb
@@ -2,8 +2,8 @@ class Logtalk < Formula
   desc "Object-oriented logic programming language"
   homepage "http://logtalk.org"
   url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3020stable.tar.gz"
-  sha256 "4d0c5ad34653eeb71b3c90c084b734cb77e0676f07aa21a5849d6d2ba41763fb"
   version "3.02.0"
+  sha256 "4d0c5ad34653eeb71b3c90c084b734cb77e0676f07aa21a5849d6d2ba41763fb"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,10 +12,13 @@ class Logtalk < Formula
     sha256 "586e8e7c5648fcacd5e6f2ac0d83c0cf11dfdfd9eb4484035ca2f47ebba9803c" => :mavericks
   end
 
-  option "swi-prolog", "Build using SWI Prolog as backend"
-  option "gnu-prolog", "Build using GNU Prolog as backend (Default)"
+  option "with-swi-prolog", "Build using SWI Prolog as backend"
+  option "with-gnu-prolog", "Build using GNU Prolog as backend (Default)"
 
-  if build.include?("swi-prolog")
+  deprecated_option "swi-prolog" => "with-swi-prolog"
+  deprecated_option "gnu-prolog" => "with-gnu-prolog"
+
+  if build.with? "swi-prolog"
     depends_on "swi-prolog"
   else
     depends_on "gnu-prolog"
