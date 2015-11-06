87fb1fc79d871db017440eb1ee741ebbfbf391fd
George White <georgeunix@gmail.com>
2015-11-06 09:56:18 +0400

    Updated formula for 'premake'.
    The premake formula now uses the downloads available on their website.

--- Library/Formula/premake.rb
+++ Library/Formula/premake.rb
@@ -1,8 +1,8 @@
 class Premake < Formula
-  desc "Premake is a build script generator"
-  homepage "http://industriousone.com/premake"
-  url "https://downloads.sourceforge.net/project/premake/Premake/4.3/premake-4.3-src.zip"
-  sha256 "36536490f8928d8ecde135da80cd8b751ea5bebe50cabba5c0de49cd41cb2780"
+  desc "Write once, build anywhere Lua-based build system"
+  homepage "https://premake.github.io/"
+  url "https://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip"
+  sha256 "0fa1ed02c5229d931e87995123cdb11d44fcc8bd99bba8e8bb1bbc0aaa798161"
 
   bottle do
     cellar :any
@@ -12,18 +12,17 @@ class Premake < Formula
   end
 
   devel do
-    url "https://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip"
-    sha256 "0fa1ed02c5229d931e87995123cdb11d44fcc8bd99bba8e8bb1bbc0aaa798161"
+    url "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-src.zip"
+    sha256 "9c13372699d25824cba1c16a0483507a6a28903e2556ffb148b288c189403aee"
   end
 
   def install
-    unless build.devel?
-      # Linking against stdc++-static causes a library not found error on 10.7
-      inreplace "build/gmake.macosx/Premake4.make", "-lstdc++-static ", ""
-    end
     system "make -C build/gmake.macosx"
-
-    # Premake has no install target, but its just a single file that is needed
-    bin.install "bin/release/premake4"
+    
+    if build.devel?
+      bin.install "bin/release/premake5"
+    else
+      bin.install "bin/release/premake4"
+    end
   end
 end
