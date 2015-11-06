2634bc3d4e0792aec2d49d3e30f3784466a8fa73
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-06 23:16:47 +0000

    logcheck: sandbox fix

--- Library/Formula/logcheck.rb
+++ Library/Formula/logcheck.rb
@@ -1,8 +1,8 @@
 class Logcheck < Formula
   desc "Mail anomalies in the system logfiles to the administrator"
   homepage "https://logcheck.alioth.debian.org/"
-  url "https://mirrors.kernel.org/debian/pool/main/l/logcheck/logcheck_1.3.17.tar.xz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/l/logcheck/logcheck_1.3.17.tar.xz"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/l/logcheck/logcheck_1.3.17.tar.xz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/l/logcheck/logcheck_1.3.17.tar.xz"
   sha256 "c2d3fc323e8c6555e91d956385dbfd0f67b55872ed0f6a7ad8ad2526a9faf03a"
 
   bottle do
@@ -21,6 +21,7 @@ class Logcheck < Formula
   end
 
   test do
-    system "#{sbin}/logtail", "-f", "#{HOMEBREW_REPOSITORY}/README.md"
+    cp HOMEBREW_REPOSITORY/"README.md", testpath
+    system "#{sbin}/logtail", "-f", "README.md"
   end
 end
