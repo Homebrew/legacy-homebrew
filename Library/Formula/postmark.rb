60866f3a772522fbbd2dad66c7970e19192fc64d
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-06 23:54:20 +0000

    postmark 1.53

--- Library/Formula/postmark.rb
+++ Library/Formula/postmark.rb
@@ -1,9 +1,9 @@
 class Postmark < Formula
   desc "File system benchmark from NetApp"
-  homepage "https://packages.debian.org/stable/utils/postmark"
-  url "https://mirrors.kernel.org/debian/pool/main/p/postmark/postmark_1.51.orig.tar.gz"
-  mirror "http://ftp.us.debian.org/debian/pool/main/p/postmark/postmark_1.51.orig.tar.gz"
-  sha256 "7cb7c31d4e7725ce8d8e11fb7df62ed700dee4dbd5ca1e31bf3a9161fc890b41"
+  homepage "https://packages.debian.org/sid/postmark"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/p/postmark/postmark_1.53.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/p/postmark/postmark_1.53.orig.tar.gz"
+  sha256 "8a88fd322e1c5f0772df759de73c42aa055b1cd36cbba4ce6ee610ac5a3c47d3"
 
   bottle do
     cellar :any
@@ -15,10 +15,17 @@ class Postmark < Formula
   def install
     system ENV.cc, "-o", "postmark", "postmark-#{version}.c"
     bin.install "postmark"
+    man1.install "postmark.1"
   end
 
   test do
-    assert_match(/PostMark v#{version}/,
-                 pipe_output("#{bin}/postmark", "run\n", 0))
+    (testpath/"config").write <<-EOS.undent
+      set transactions 50
+      set location #{testpath}
+      run
+    EOS
+
+    output = pipe_output("#{bin}/postmark #{testpath}/config")
+    assert_match /(50 per second)/, output
   end
 end
