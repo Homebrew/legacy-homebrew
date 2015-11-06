6ae60dee766bdef592ee528c817a8b0554b9c198
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-06 16:41:56 +0000

    haproxy: add 1.6.2 bottle.

--- Library/Formula/haproxy.rb
+++ Library/Formula/haproxy.rb
@@ -4,6 +4,13 @@ class Haproxy < Formula
   url "http://www.haproxy.org/download/1.6/src/haproxy-1.6.2.tar.gz"
   sha256 "bd4a7eee79e1bfd25af59d956bb60e82acbb6f146f3fb3b30636036f4c9548d3"
 
+  bottle do
+    cellar :any
+    sha256 "6f015f42e4157bb98c501af6872f2e9f1a820a9003ed19e5d3797da9e579069f" => :el_capitan
+    sha256 "bd4bf6a9b565f6d784ff982a55102bd8caafedcd3d558411ee9285e9222583b2" => :yosemite
+    sha256 "89fdf17cbcc9110e5cc6d2a2e3110d39f892050f968c9e5fb46ce0adc7077fda" => :mavericks
+  end
+
   depends_on "openssl"
   depends_on "pcre"
 
