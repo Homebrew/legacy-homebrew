0bc1ea2ed6167253cb0be0e47b098547f8906ebf
Yuki Nagai <ynagai@r.recruit.co.jp>
2015-11-06 18:54:30 +0900

    Carthage 0.10

--- Library/Formula/carthage.rb
+++ Library/Formula/carthage.rb
@@ -1,12 +1,12 @@
 class Carthage < Formula
   desc "Decentralized dependency manager for Cocoa"
   homepage "https://github.com/Carthage/Carthage"
-  url "https://github.com/Carthage/Carthage.git", :tag => "0.8",
-                                                  :revision => "52e1a006158793eff3c9890f1934f0b7cb64d2b8",
+  url "https://github.com/Carthage/Carthage.git", :tag => "0.10",
+                                                  :revision => "f27cc938b4b8bf9bdd20498c3cc0000346cd9c91",
                                                   :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
-  depends_on :xcode => ["6.3", :build]
+  depends_on :xcode => ["7.1", :build]
 
   bottle do
     cellar :any
