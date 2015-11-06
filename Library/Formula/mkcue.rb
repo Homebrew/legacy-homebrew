d19051c0dfbda0c81d81d7bc15aee179090d4ce2
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-06 23:27:47 +0000

    mkcue: prefer berkeley debian mirror

--- Library/Formula/mkcue.rb
+++ Library/Formula/mkcue.rb
@@ -1,8 +1,8 @@
 class Mkcue < Formula
   desc "Generate a CUE sheet from a CD"
-  homepage "https://packages.debian.org/source/stable/mkcue"
-  url "https://mirrors.kernel.org/debian/pool/main/m/mkcue/mkcue_1.orig.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mkcue/mkcue_1.orig.tar.gz"
+  homepage "https://packages.debian.org/sid/mkcue"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mkcue/mkcue_1.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/mkcue/mkcue_1.orig.tar.gz"
   version "1"
   sha256 "2aaf57da4d0f2e24329d5e952e90ec182d4aa82e4b2e025283e42370f9494867"
 
