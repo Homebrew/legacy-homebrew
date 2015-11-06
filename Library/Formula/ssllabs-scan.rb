dbd78c21f119fb2e015f10cc3ca3d86bcd45d3bd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-06 09:42:42 +0000

    ssllabs-scan: add 1.2.0 bottle.

--- Library/Formula/ssllabs-scan.rb
+++ Library/Formula/ssllabs-scan.rb
@@ -4,6 +4,13 @@ class SsllabsScan < Formula
   url "https://github.com/ssllabs/ssllabs-scan/archive/v1.2.0.tar.gz"
   sha256 "c8becd57a4aa65f5c4c32824e392d8373ae169ff055192107e9411032dfcd017"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "25db5e475aa97258a5012a41ae8b54a152ce58cf148609c4d276492158fdbb7f" => :el_capitan
+    sha256 "16583421f5bb71ec82b174e82675462d832dcd18768e54b5ce931040b100473b" => :yosemite
+    sha256 "369c25ecefbea1e9cd09783b9a5c57120c9e2d7f0fa80967e912a77c5e37771a" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
