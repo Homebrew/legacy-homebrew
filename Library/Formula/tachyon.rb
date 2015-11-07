23a7b75716cb0da5debe0f88d8d885457803e756
Kashif Rasul <kashif.rasul@gmail.com>
2015-11-07 11:32:32 +0100

    tachyon 0.8.1

--- Library/Formula/tachyon.rb
+++ Library/Formula/tachyon.rb
@@ -1,15 +1,15 @@
 class Tachyon < Formula
-  desc "Distributed storage system to enable data sharing across cluster frameworks"
+  desc "A memory-centric distributed storage system enabling reliable data sharing at memory-speed across cluster frameworks"
   homepage "http://tachyon-project.org/"
-  url "https://github.com/amplab/tachyon/releases/download/v0.6.4/tachyon-0.6.4-bin.tar.gz"
-  sha256 "c105e2e984cc7139933e1d1debc4b393fe22ef33fb90776edc5be7148829b820"
+  url "http://tachyon-project.org/downloads/files/0.8.1/tachyon-0.8.1-bin.tar.gz"
+  sha256 "82c289b49fdd29a8969c404203d20ac29f7136a3af72597f25537a9263cf0c74"
 
   bottle :unneeded
 
   def install
-    libexec.install %w[bin client conf core libexec]
-    bin.write_exec_script Dir["#{libexec}/bin/*"]
     doc.install Dir["docs/*"]
+    libexec.install Dir["*"]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
 
     (etc/"tachyon").install libexec/"conf/tachyon-env.sh.template" => "tachyon-env.sh"
     ln_sf "#{etc}/tachyon/tachyon-env.sh", "#{libexec}/conf/tachyon-env.sh"
