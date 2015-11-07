961eb5d7520bb039c037d6cf0f760a1dc79cfa14
Thierry Moisan <thierry.moisan@gmail.com>
2015-11-07 13:54:48 -0500

    pax-construct: update homepage

--- Library/Formula/pax-construct.rb
+++ Library/Formula/pax-construct.rb
@@ -1,6 +1,6 @@
 class PaxConstruct < Formula
   desc "Tools to setup and develop OSGi projects quickly"
-  homepage "http://wiki.ops4j.org/display/paxconstruct/Pax+Construct"
+  homepage "https://ops4j1.jira.com/wiki/display/paxconstruct/Pax+Construct"
   url "https://repo1.maven.org/maven2/org/ops4j/pax/construct/scripts/1.5/scripts-1.5.zip"
   sha256 "d0325bbe571783097d4be782576569ea0a61529695c14e33a86bbebfe44859d1"
 
