a195ee29ebe566a4facd240d491a0fa3ca1522a3
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-07 00:09:44 +0000

    urlview: prefer berkeley debian mirror
    Closes #45769.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/urlview.rb
+++ Library/Formula/urlview.rb
@@ -1,8 +1,8 @@
 class Urlview < Formula
   desc "URL extractor/launcher"
   homepage "https://packages.debian.org/sid/misc/urlview"
-  url "https://mirrors.kernel.org/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/u/urlview/urlview_0.9.orig.tar.gz"
   sha256 "746ff540ccf601645f500ee7743f443caf987d6380e61e5249fc15f7a455ed42"
 
   bottle do
@@ -13,7 +13,8 @@ class Urlview < Formula
   end
 
   patch do
-    url "https://mirrors.kernel.org/debian/pool/main/u/urlview/urlview_0.9-19.diff.gz"
+    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/u/urlview/urlview_0.9-19.diff.gz"
+    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/u/urlview/urlview_0.9-19.diff.gz"
     sha256 "056883c17756f849fb9235596d274fbc5bc0d944fcc072bdbb13d1e828301585"
   end
 
