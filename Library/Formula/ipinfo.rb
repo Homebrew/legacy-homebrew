40afc6e6e51010aa92f030a3ff11bf1e3b360c01
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-07 05:47:50 +0000

    ipinfo: update 1.2 bottle.

--- Library/Formula/ipinfo.rb
+++ Library/Formula/ipinfo.rb
@@ -5,10 +5,10 @@ class Ipinfo < Formula
   sha256 "19e6659f781a48b56062a5527ff463a29c4dcc37624fab912d1dce037b1ddf2d"
 
   bottle do
-    cellar :any
-    sha1 "9982cd38077f1d542736f5db32f76db350c2d987" => :mavericks
-    sha1 "562457c3ac4bcdd8dc8ab856272d37b16e799b7d" => :mountain_lion
-    sha1 "f7fe28e279d45b77d5e74b5710682e11a7814fe8" => :lion
+    cellar :any_skip_relocation
+    sha256 "ecb331ae035cf5963afc8e8adf371d80f936960bf0d5ba379b18761263a1b040" => :el_capitan
+    sha256 "e1ce332c726d060521e97a5402746a60778d91beaf28704d9ce5bb6e17451fb3" => :yosemite
+    sha256 "686fe99fef85ecbfdcc9c922f6cda898362d70bb9f5b9b7e1aeba8e30c284196" => :mavericks
   end
 
   def install
