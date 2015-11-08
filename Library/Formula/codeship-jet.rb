137ea1026d03a16271443a792317bb78d35280f3
Papp David <pappdav@gmail.com>
2015-11-08 14:59:19 +0100

    URL redundant fix

--- Library/Formula/codeship-jet.rb
+++ Library/Formula/codeship-jet.rb
@@ -1,7 +1,7 @@
 class CodeshipJet < Formula
   desc "Running Codeship Locally for Development"
   homepage "https://codeship.com"
-  url "https://s3.amazonaws.com/codeship-jet-releases/0.11.5/jet-darwin_amd64_0.11.5.tar.gz"
+  url "https://s3.amazonaws.com/codeship-jet-homebrew/0.11.5/jet-darwin_amd64.tar.gz"
   version "0.11.5"
   sha256 "b844855284a078b4dce8418b76cdabbbf1428ce67772ab3e32bdfc3a40d0cb4a"
 
