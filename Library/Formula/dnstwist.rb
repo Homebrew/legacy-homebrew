133ff2e7e70864694e59ba9d492038c14da6775c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-08 22:00:27 +0000

    dnstwist: add 1.01 bottle.

--- Library/Formula/dnstwist.rb
+++ Library/Formula/dnstwist.rb
@@ -4,6 +4,13 @@ class Dnstwist < Formula
   url "https://github.com/elceef/dnstwist/archive/v1.01.tar.gz"
   sha256 "6fae9f722ee711598fd23a93169a8539eaf2210410b75af22d9a4241d23da842"
 
+  bottle do
+    cellar :any
+    sha256 "6d9fba8db2440ebd7f942fe60e0dc9d418697acf5226968b2c57170c2343f3d4" => :el_capitan
+    sha256 "1aee3219cb6f784486d9630c73b1ca39aaeb9e6df0dc27949a4c90d974ac5eb4" => :yosemite
+    sha256 "f7de05a469c237b2305d1ef7fb937d75dc4a7ec98905c8520b78f8b86bd72147" => :mavericks
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "ssdeep"
   depends_on "geoip"
