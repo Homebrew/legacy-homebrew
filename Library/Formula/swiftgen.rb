42f98fbc075d86be99a7efc7cdbde84c5c5929ef
Olivier Halligon <olivier@halligon.net>
2015-11-08 15:17:07 +0100

    swiftgen 0.7.0
    Closes #45799.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/swiftgen.rb
+++ Library/Formula/swiftgen.rb
@@ -1,8 +1,8 @@
 class Swiftgen < Formula
   desc "Collection of Swift tools to generate Swift code"
   homepage "https://github.com/AliSoftware/SwiftGen"
-  url "https://github.com/AliSoftware/SwiftGen/archive/0.6.0.tar.gz"
-  sha256 "4adf379c5d41b360c7fed32e21ba3476dc5e311b5e8a14cb755b0a6addd68e90"
+  url "https://github.com/AliSoftware/SwiftGen/archive/0.7.0.tar.gz"
+  sha256 "c1aceb05dea975d23853a24509b62583b8ac6d857ea78c6d36fc61e08881cd14"
   head "https://github.com/AliSoftware/SwiftGen.git"
 
   bottle do
@@ -34,16 +34,16 @@ class Swiftgen < Formula
 
     fixtures = pkgshare/"fixtures"
 
-    output = shell_output("#{bin}/swiftgen images #{fixtures}/Images.xcassets").strip
+    output = shell_output("#{bin}/swiftgen images -p #{pkgshare/"templates/images-default.stencil"} #{fixtures}/Images.xcassets").strip
     assert_equal output, (fixtures/"Images-File-Defaults.swift.out").read.strip, "swiftgen images failed"
 
-    output = shell_output("#{bin}/swiftgen colors #{fixtures}/colors.txt").strip
+    output = shell_output("#{bin}/swiftgen colors -p #{pkgshare/"templates/colors-default.stencil"} #{fixtures}/colors.txt").strip
     assert_equal output, (fixtures/"Colors-File-Defaults.swift.out").read.strip, "swiftgen colors failed"
 
-    output = shell_output("#{bin}/swiftgen strings #{fixtures}/Localizable.strings").strip
+    output = shell_output("#{bin}/swiftgen strings -p #{pkgshare/"templates/strings-default.stencil"} #{fixtures}/Localizable.strings").strip
     assert_equal output, (fixtures/"Strings-File-Defaults.swift.out").read.strip, "swiftgen strings failed"
 
-    output = shell_output("#{bin}/swiftgen storyboards #{fixtures}/Message.storyboard").strip
+    output = shell_output("#{bin}/swiftgen storyboards -p #{pkgshare/"templates/storyboards-default.stencil"} #{fixtures}/Message.storyboard").strip
     assert_equal output, (fixtures/"Storyboards-Message-Defaults.swift.out").read.strip, "swiftgen storyboards failed"
   end
 end
