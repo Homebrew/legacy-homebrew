e992837351a3932c8ee9553d994cf3c9a28da0c3
Romain Lespinasse <romain.lespinasse@gmail.com>
2015-11-10 15:50:51 +0100

    golo 3.0.0-incubation

--- Library/Formula/golo.rb
+++ Library/Formula/golo.rb
@@ -1,31 +1,21 @@
 class Golo < Formula
   desc "Lightweight dynamic language for the JVM"
   homepage "http://golo-lang.org"
-  url "https://repo1.maven.org/maven2/org/golo-lang/golo/2.1.0/golo-2.1.0-distribution.tar.gz"
-  sha256 "59c7324b7eac88dfe2cb9612468cf5639ae0b84e44319b2ee617e0e054eed422"
-
-  devel do
-    url "https://www.eclipse.org/downloads/download.php?file=/golo/golo-3.0.0-incubation-M3.zip&r=1"
-    sha256 "70a1dbb24901e30b3a75f312be25afc1c4cfe8d69ca9e251153e93491adbd880"
-    version "3.0.0-incubation-M3"
-    depends_on :java => "1.8+"
-  end
+  url "https://www.eclipse.org/downloads/download.php?file=/golo/golo-3.0.0-incubation.zip&r=1"
+  sha256 "d52d33749c9ca05b9c4d16008acdb36f15b81519ddfcd62524426df365b0673d"
 
   head do
     url "https://github.com/eclipse/golo-lang.git"
-    depends_on :java => "1.8+"
   end
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8+"
 
   def install
     if build.head?
       system "./gradlew", "installDist"
       libexec.install %w[build/install/golo/bin build/install/golo/docs build/install/golo/lib]
-    elsif build.devel?
-      libexec.install %w[bin docs lib]
     else
-      libexec.install %w[bin doc lib]
+      libexec.install %w[bin docs lib]
     end
     libexec.install %w[share samples]
 
