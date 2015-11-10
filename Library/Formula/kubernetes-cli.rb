84c7511c9009d7a77f362cb81e9d83c407bd20c8
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-10 18:14:42 +0000

    kubernetes-cli: fix audit

--- Library/Formula/kubernetes-cli.rb
+++ Library/Formula/kubernetes-cli.rb
@@ -8,17 +8,17 @@ class KubernetesCli < Formula
     sha256 "9b293a37a4782b1906f3f6e0412a064e15a0d55113c6c20ee035edd06e5579fb"
   end
 
-  devel do
-    url "https://github.com/kubernetes/kubernetes/archive/v1.2.0-alpha.3.tar.gz"
-    sha256 "57dae812e9ab46e4bca6fa42b563461b69580247cad142b47a33bf57da44e803"
-    version "1.2.0-alpha.3"
-  end
-
   bottle do
     cellar :any_skip_relocation
     sha256 "b5826dc1866428c7c425e7f32e80fe3cb9673d115d5d65d8f5dc1c0986aa66ef" => :el_capitan
     sha256 "38c6cee6369dcf82f039864b86011eab42312391425ef29f6ebfcadc9a991680" => :yosemite
     sha256 "9818b4ec9ef23410d383ec6bccf035654776431af758eb2eec7022d1d33c7609" => :mavericks
+  end
+
+  devel do
+    url "https://github.com/kubernetes/kubernetes/archive/v1.2.0-alpha.3.tar.gz"
+    sha256 "57dae812e9ab46e4bca6fa42b563461b69580247cad142b47a33bf57da44e803"
+    version "1.2.0-alpha.3"
   end
 
   depends_on "go" => :build
