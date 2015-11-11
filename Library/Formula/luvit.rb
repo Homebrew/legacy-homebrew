da8cfb61d65287a14e71c87c14a9ff67f5a7ca65
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-11 10:46:00 +0000

    luvit: update 2.6.0 bottle.

--- Library/Formula/luvit.rb
+++ Library/Formula/luvit.rb
@@ -6,10 +6,10 @@ class Luvit < Formula
   head "https://github.com/luvit/luvit.git"
 
   bottle do
-    cellar :any
-    sha256 "3c7250314f67d320d8bc36f14e8d055860a2098bf5528b53eb58ff7d93244881" => :yosemite
-    sha256 "1f4616aa1be7802088900bcec2f284e8a85a4b82ee6fc9873a4c20d30ac6ac47" => :mavericks
-    sha256 "ef23e4e0cf252074738d85b1b2b2e02fd58e3a5239e00362d6f1e4dc2c58862c" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "1554c4302457a34ee3731d9f0a3543a3f8132033b5116227d7744eb07638dbd7" => :el_capitan
+    sha256 "833442db5e6a85c0fb09b0da6e48a824f78e8f3ed19c3ecda9329ddb0f50b6af" => :yosemite
+    sha256 "f9589b35d50dd85879b37055edc374e5358f9981b646ad8c95dcb7b4c0dfdd3f" => :mavericks
   end
 
   depends_on "pkg-config" => :build
