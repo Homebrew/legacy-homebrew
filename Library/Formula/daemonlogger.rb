ab7c68d0ce661ce0f491c9a900bbb56eb8e01b8f
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:33:28 +0100

    daemonlogger: use secure homepage

--- Library/Formula/daemonlogger.rb
+++ Library/Formula/daemonlogger.rb
@@ -1,6 +1,6 @@
 class Daemonlogger < Formula
   desc "Network packet logger and soft tap daemon"
-  homepage "http://sourceforge.net/projects/daemonlogger/"
+  homepage "https://sourceforge.net/projects/daemonlogger/"
   url "https://downloads.sourceforge.net/project/daemonlogger/daemonlogger-1.2.1.tar.gz"
   sha256 "79fcd34d815e9c671ffa1ea3c7d7d50f895bb7a79b4448c4fd1c37857cf44a0b"
 
