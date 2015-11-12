1e6a608c2ec32001a924d398b7cf424afe4cb9e9
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:37:15 +0100

    fastjar: use secure homepage

--- Library/Formula/fastjar.rb
+++ Library/Formula/fastjar.rb
@@ -1,6 +1,6 @@
 class Fastjar < Formula
   desc "Implementation of Sun's jar tool"
-  homepage "http://sourceforge.net/projects/fastjar/"
+  homepage "https://sourceforge.net/projects/fastjar/"
   url "https://downloads.sourceforge.net/project/fastjar/fastjar/0.94/fastjar-0.94.tar.gz"
   sha256 "5a217fc3e3017efb18fd1316b38d2aaa7370280fcf5732ad8fff7e27ec867b95"
 
