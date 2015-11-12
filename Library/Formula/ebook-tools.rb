92d040afa05927d4a34b6d1729a6d22a0383a5b2
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:36:41 +0100

    ebook-tools: use secure homepage

--- Library/Formula/ebook-tools.rb
+++ Library/Formula/ebook-tools.rb
@@ -1,6 +1,6 @@
 class EbookTools < Formula
   desc "Access and convert several ebook formats"
-  homepage "http://sourceforge.net/projects/ebook-tools"
+  homepage "https://sourceforge.net/projects/ebook-tools/"
   url "https://downloads.sourceforge.net/project/ebook-tools/ebook-tools/0.2.2/ebook-tools-0.2.2.tar.gz"
   sha256 "cbc35996e911144fa62925366ad6a6212d6af2588f1e39075954973bbee627ae"
   revision 1
