5e3298498e60a4abfa1a9372c480e7624a23ed23
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:33:58 +0100

    dc3dd: use secure homepage

--- Library/Formula/dc3dd.rb
+++ Library/Formula/dc3dd.rb
@@ -1,6 +1,6 @@
 class Dc3dd < Formula
   desc "Patched GNU dd that is intended for forensic acquisition of data"
-  homepage "http://sourceforge.net/projects/dc3dd/"
+  homepage "https://sourceforge.net/projects/dc3dd/"
   url "https://downloads.sourceforge.net/project/dc3dd/dc3dd/7.1.0/dc3dd-7.1.614.tar.gz"
   sha256 "f6fb4d921928e6354e9f8ff2a8415ebc42b129959996e1ab0d10899c11aac198"
 
