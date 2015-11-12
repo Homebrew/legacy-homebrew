d88ac5f3bf8c861646d5a83ad1af11bed651c7b4
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:32:46 +0100

    bsponmpi: use secure homepage

--- Library/Formula/bsponmpi.rb
+++ Library/Formula/bsponmpi.rb
@@ -1,6 +1,6 @@
 class Bsponmpi < Formula
   desc "Implements the BSPlib standard on top of MPI"
-  homepage "http://sourceforge.net/projects/bsponmpi"
+  homepage "https://sourceforge.net/projects/bsponmpi/"
   url "https://downloads.sourceforge.net/project/bsponmpi/bsponmpi/0.3/bsponmpi-0.3.tar.gz"
   sha256 "bc90ca22155be9ff65aca4e964d8cd0bef5f0facef0a42bc1db8b9f822c92a90"
 
