45f360ef475776a06444d647e1c2a5219ecdd82b
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 01:53:48 +0100

    syck: use secure url

--- Library/Formula/syck.rb
+++ Library/Formula/syck.rb
@@ -1,7 +1,7 @@
 class Syck < Formula
   desc "Extension for reading and writing YAML"
   homepage "https://wiki.github.com/indeyets/syck/"
-  url "http://cloud.github.com/downloads/indeyets/syck/syck-0.70.tar.gz"
+  url "https://cloud.github.com/downloads/indeyets/syck/syck-0.70.tar.gz"
   sha256 "4c94c472ee8314e0d76eb2cca84f6029dc8fc58bfbc47748d50dcb289fda094e"
 
   fails_with :llvm do
