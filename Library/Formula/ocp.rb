f14fc38c435a8bb547950f582b97485cde329e75
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:45:30 +0100

    ocp: use secure homepage

--- Library/Formula/ocp.rb
+++ Library/Formula/ocp.rb
@@ -1,6 +1,6 @@
 class Ocp < Formula
   desc "UNIX port of the Open Cubic Player"
-  homepage "http://sourceforge.net/p/opencubicplayer/home/"
+  homepage "https://sourceforge.net/projects/opencubicplayer/"
   url "https://downloads.sourceforge.net/project/opencubicplayer/ocp-0.1.21/ocp-0.1.21.tar.bz2"
   sha256 "d88eeaed42902813869911e888971ab5acd86a56d03df0821b376f2ce11230bf"
 
