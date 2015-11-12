ddf10d4a160c04af1e1349c78a76f9c7c92451cf
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:36:55 +0100

    esniper: use secure homepage

--- Library/Formula/esniper.rb
+++ Library/Formula/esniper.rb
@@ -1,6 +1,6 @@
 class Esniper < Formula
   desc "Snipe eBay auctions from the command-line"
-  homepage "http://sourceforge.net/projects/esniper/"
+  homepage "https://sourceforge.net/projects/esniper/"
   url "https://downloads.sourceforge.net/project/esniper/esniper/2.31.0/esniper-2-31-0.tgz"
   version "2.31"
   sha256 "30d2378c700b72b5363c8af59e7566564d9ec8cd4b44cd389c2830907d7bc676"
