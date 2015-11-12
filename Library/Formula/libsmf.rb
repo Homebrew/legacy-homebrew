e67952a4a72c9e45e3c78f5f5abcde49980b6ada
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:41:38 +0100

    libsmf: use secure homepage

--- Library/Formula/libsmf.rb
+++ Library/Formula/libsmf.rb
@@ -1,6 +1,6 @@
 class Libsmf < Formula
   desc "C library for handling SMF ('*.mid') files"
-  homepage "http://sourceforge.net/projects/libsmf/"
+  homepage "https://sourceforge.net/projects/libsmf/"
   url "https://downloads.sourceforge.net/project/libsmf/libsmf/1.3/libsmf-1.3.tar.gz"
   sha256 "d3549f15de94ac8905ad365639ac6a2689cb1b51fdfa02d77fa6640001b18099"
 
