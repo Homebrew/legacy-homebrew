dd7b9b276cf30fd623d41debbe57719565b595ff
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:46:58 +0100

    thrulay: use secure homepage

--- Library/Formula/thrulay.rb
+++ Library/Formula/thrulay.rb
@@ -1,6 +1,6 @@
 class Thrulay < Formula
   desc "Measure performance of a network"
-  homepage "http://sourceforge.net/projects/thrulay/"
+  homepage "https://sourceforge.net/projects/thrulay/"
   url "https://downloads.sourceforge.net/project/thrulay/thrulay/0.9/thrulay-0.9.tar.gz"
   sha256 "373d5613dfe371f6b4f48fc853f6c27701b2981ba4100388c9881cb802d1780d"
 
