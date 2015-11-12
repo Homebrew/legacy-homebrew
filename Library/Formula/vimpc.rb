7586f7ae21a95952c78aad7fc1c27abdecedbd33
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:47:13 +0100

    vimpc: use secure homepage

--- Library/Formula/vimpc.rb
+++ Library/Formula/vimpc.rb
@@ -1,6 +1,6 @@
 class Vimpc < Formula
   desc "Ncurses based mpd client with vi like key bindings"
-  homepage "http://sourceforge.net/projects/vimpc/"
+  homepage "https://sourceforge.net/projects/vimpc/"
   url "https://downloads.sourceforge.net/project/vimpc/Release%200.09.1/vimpc-0.09.1.tar.gz"
   sha256 "082fa9974e01bf563335ebf950b2f9bc129c0d05c0c15499f7827e8418306031"
 
