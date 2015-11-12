a5dab65dfc35de26c2fbf9f8fdb635c0dd26039c
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:43:08 +0100

    libsoxr: use secure homepage, update mirror

--- Library/Formula/libsoxr.rb
+++ Library/Formula/libsoxr.rb
@@ -1,8 +1,8 @@
 class Libsoxr < Formula
   desc "High quality, one-dimensional sample-rate conversion library"
-  homepage "http://sourceforge.net/projects/soxr/"
+  homepage "https://sourceforge.net/projects/soxr/"
   url "https://downloads.sourceforge.net/project/soxr/soxr-0.1.1-Source.tar.xz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/libs/libsoxr/libsoxr_0.1.1.orig.tar.xz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/libs/libsoxr/libsoxr_0.1.1.orig.tar.xz"
   sha256 "dcc16868d1a157079316f84233afcc2b52dd0bd541dd8439dc25bceb306faac2"
 
   bottle do
