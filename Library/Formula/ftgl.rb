39565e7b5f33c4ddb69dcdcd52cefc2d056c2abc
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:37:51 +0100

    ftgl: use secure homepage

--- Library/Formula/ftgl.rb
+++ Library/Formula/ftgl.rb
@@ -1,6 +1,6 @@
 class Ftgl < Formula
   desc "Freetype / OpenGL bridge"
-  homepage "http://sourceforge.net/projects/ftgl/"
+  homepage "https://sourceforge.net/projects/ftgl/"
   url "https://downloads.sourceforge.net/project/ftgl/FTGL%20Source/2.1.3~rc5/ftgl-2.1.3-rc5.tar.gz"
   sha256 "5458d62122454869572d39f8aa85745fc05d5518001bcefa63bd6cbb8d26565b"
 
