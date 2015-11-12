51465fbfebd0aa9f34ea00b68315d9aa3634515b
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:39:33 +0100

    libmms: use secure homepage

--- Library/Formula/libmms.rb
+++ Library/Formula/libmms.rb
@@ -1,6 +1,6 @@
 class Libmms < Formula
   desc "Library for parsing mms:// and mmsh:// network streams"
-  homepage "http://sourceforge.net/projects/libmms/"
+  homepage "https://sourceforge.net/projects/libmms/"
   url "https://downloads.sourceforge.net/project/libmms/libmms/0.6.4/libmms-0.6.4.tar.gz"
   sha256 "3c05e05aebcbfcc044d9e8c2d4646cd8359be39a3f0ba8ce4e72a9094bee704f"
 
