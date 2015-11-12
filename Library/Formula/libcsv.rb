465a5535753cab176064785ffb14e169006c3da4
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:39:05 +0100

    libcsv: use secure homepage

--- Library/Formula/libcsv.rb
+++ Library/Formula/libcsv.rb
@@ -1,6 +1,6 @@
 class Libcsv < Formula
   desc "CSV library in ANSI C89"
-  homepage "http://sourceforge.net/projects/libcsv/"
+  homepage "https://sourceforge.net/projects/libcsv/"
   url "https://downloads.sourceforge.net/project/libcsv/libcsv/libcsv-3.0.3/libcsv-3.0.3.tar.gz"
   sha256 "d9c0431cb803ceb9896ce74f683e6e5a0954e96ae1d9e4028d6e0f967bebd7e4"
 
