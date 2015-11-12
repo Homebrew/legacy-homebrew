ebbe6e4a3cb459a2a0abad657a264759a7c4ef91
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-12 03:38:10 +0100

    hilite: use secure homepage

--- Library/Formula/hilite.rb
+++ Library/Formula/hilite.rb
@@ -1,6 +1,6 @@
 class Hilite < Formula
   desc "CLI tool that runs a command and highlights STDERR output"
-  homepage "http://sourceforge.net/projects/hilite/"
+  homepage "https://sourceforge.net/projects/hilite/"
   url "https://downloads.sourceforge.net/project/hilite/hilite/1.5/hilite.c"
   sha256 "e15bdff2605e8d23832d6828a62194ca26dedab691c9d75df2877468c2f6aaeb"
 
