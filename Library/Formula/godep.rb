9ddcf6d82afe78bb7f502a080436ee77ab920f17
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-14 11:12:41 +0000

    godep: add 28 bottle.

--- Library/Formula/godep.rb
+++ Library/Formula/godep.rb
@@ -5,6 +5,13 @@ class Godep < Formula
   sha256 "cf5a63943061add896d349704ccd8b4d9f4341257d028e63e53fbf7a81b366aa"
   head "https://github.com/tools/godep.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "48aa501daffc0931ef6b111b706a309b40f5de59060b08d5427bd4ad43ace59b" => :el_capitan
+    sha256 "83410b9f432581d0db443773875f5180e32f5c039aed651d404603813b79411c" => :yosemite
+    sha256 "d56277d4da05352acb6f0ed64f3bcc5cf27722e344e305a7e3cac20d9447c599" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
