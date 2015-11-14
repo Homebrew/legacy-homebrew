d66c856c342adc24d98af2cdc6d5c01acc13733b
Andre LaBranche <dre@mac.com>
2015-11-14 04:05:04 -0800

    fprobe 1.1 (new formula)
    fprobe is a libcap-based NetFlow probe that monitors the local system's
    network interfaces and can send NetFlow data to one or more collectors.
    fprobe supports NetFlow versions 1, 5, and 7.
    
    Closes #45990.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/fprobe.rb
@@ -0,0 +1,19 @@
+class Fprobe < Formula
+  desc "Libpcap-based NetFlow probe"
+  homepage "https://sourceforge.net/projects/fprobe/"
+  url "https://downloads.sourceforge.net/project/fprobe/fprobe/1.1/fprobe-1.1.tar.bz2"
+  sha256 "3a1cedf5e7b0d36c648aa90914fa71a158c6743ecf74a38f4850afbac57d22a0"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--mandir=#{man}",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match /NetFlow/, shell_output("#{sbin}/fprobe -h").strip
+  end
+end
