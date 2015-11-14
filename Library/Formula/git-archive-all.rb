8b837e5ab00b8077525ca9a27b94f2bd8516923c
Alex Dunn <dunn.alex@gmail.com>
2015-11-13 23:23:10 -0800

    git-archive-all 1.13

--- Library/Formula/git-archive-all.rb
+++ Library/Formula/git-archive-all.rb
@@ -1,9 +1,8 @@
 class GitArchiveAll < Formula
   desc "Archive a project and its submodules"
   homepage "https://github.com/Kentzo/git-archive-all"
-  url "https://github.com/Kentzo/git-archive-all/archive/1.12.tar.gz"
-  sha256 "d17859736aa6628d21f80ce44e35cbdca495c90f5db23ebd8a8b18b3398fcf13"
-
+  url "https://github.com/Kentzo/git-archive-all/archive/1.13.tar.gz"
+  sha256 "69bbe8039b71440b89ca7113b9c2178aa0602959f5f6c83b1cd52492384830eb"
   head "https://github.com/Kentzo/git-archive-all.git"
 
   bottle do
@@ -15,5 +14,20 @@ class GitArchiveAll < Formula
 
   def install
     system "make", "prefix=#{prefix}", "install"
+  end
+
+  test do
+    (testpath/".gitconfig").write <<-EOS.undent
+      [user]
+        name = Real Person
+        email = notacat@hotmail.cat
+      EOS
+    system "git", "init"
+    touch "homebrew"
+    system "git", "add", "homebrew"
+    system "git", "commit", "--message", "brewing"
+
+    assert_equal "#{testpath}/homebrew => archive/homebrew",
+                 shell_output("#{bin}/git-archive-all --dry-run ./archive", 0).chomp
   end
 end
