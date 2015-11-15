5a51cb40f2ed8170b3fb5b4720bfa1ba4916ee36
Carsten Jørgensen <carstenj@gmail.com>
2015-11-15 17:05:43 +0100

    Update jags.rb to latest version 4.0.1

--- Library/Formula/jags.rb
+++ Library/Formula/jags.rb
@@ -1,13 +1,14 @@
 class Jags < Formula
   desc "Just Another Gibbs Sampler for Bayesian MCMC simulation"
   homepage "http://mcmc-jags.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/mcmc-jags/JAGS/3.x/Source/JAGS-3.4.0.tar.gz"
-  sha256 "2beaa9a2672c2c95efc55ffa4c8b597a872f20232373daebd17ad539d3d7d82b"
+  url "https://downloads.sourceforge.net/project/mcmc-jags/JAGS/4.x/Source/JAGS-4.0.1.tar.gz"
+  sha256 "3619312cd9ce8163db4d5a51c961078061f6256382a2e93041ee22f7c09ba00e"
   bottle do
     cellar :any
-    sha1 "ea59e194feb354ef474a6bd58d4556d0e68adfb0" => :yosemite
-    sha1 "e3d0935caaf8a06b11efd1338d5444fdce87dc63" => :mavericks
-    sha1 "49977edf0dc1571ae846f758c3ba74cf59a2f0f5" => :mountain_lion
+    sha256 "0fe2c73915572317e0e708bfe387fb3320ac8474afc4173b70d0b018ec0bdb33" => :el_capitan
+    sha256 "00c6bf92d3d324c39fce886b061de737090ea98f3cadf02a360857ac0f302dc6" => :yosemite
+    sha256 "78e2c2b112d80180c5578f9a1581b68bfd73d2fe7561903d2b4e177ccdd8932d" => :mavericks
+    sha256 "5fd3ce491e4a7c7980359b9870d74d2ed7f90f35876d7de28ffd50f6c3e8eaff" => :mountain_lion
   end
 
   revision 1
@@ -19,4 +20,4 @@ class Jags < Formula
                           "--prefix=#{prefix}"
     system "make", "install"
   end
-end
+end 
