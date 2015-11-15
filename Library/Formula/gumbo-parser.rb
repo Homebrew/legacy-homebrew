f54db88b4879f2090d01fc433d8aeed129ae884c
Sven Schwyn <schwyn@bitcetera.com>
2015-11-15 12:54:48 +0100

    gumbo-parser 0.10.1
    Closes #46004.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/gumbo-parser.rb
+++ Library/Formula/gumbo-parser.rb
@@ -1,8 +1,8 @@
 class GumboParser < Formula
   desc "C99 library for parsing HTML5"
   homepage "https://github.com/google/gumbo-parser"
-  url "https://github.com/google/gumbo-parser/archive/v0.9.3.tar.gz"
-  sha256 "038354c0394591b270eafc9203aef4516afbf78a66fcb2c4fff5180593b003f0"
+  url "https://github.com/google/gumbo-parser/archive/v0.10.1.tar.gz"
+  sha256 "28463053d44a5dfbc4b77bcf49c8cee119338ffa636cc17fc3378421d714efad"
 
   bottle do
     cellar :any
