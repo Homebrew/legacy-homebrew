b4293930de659bee60c9c4e631b9e48371381926
Kashif Rasul <kashif.rasul@gmail.com>
2015-11-15 11:04:45 +0100

    fixed desc

--- Library/Formula/tachyon.rb
+++ Library/Formula/tachyon.rb
@@ -1,5 +1,5 @@
 class Tachyon < Formula
-  desc "A memory-centric storage system for data sharing across cluster frameworks"
+  desc "Memory-centric storage system for data sharing across cluster frameworks"
   homepage "http://tachyon-project.org/"
   url "http://tachyon-project.org/downloads/files/0.8.2/tachyon-0.8.2-bin.tar.gz"
   sha256 "52823ba1b5764a3c4be6738a0becb80d88de636bb605310718f6bf0fdbb632a0"
