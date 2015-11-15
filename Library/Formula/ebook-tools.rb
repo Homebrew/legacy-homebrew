95b18f8ca7e23f512ac08ada8377d162b844a27d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-15 16:17:43 +0000

    ebook-tools: update 0.2.2_1 bottle.

--- Library/Formula/ebook-tools.rb
+++ Library/Formula/ebook-tools.rb
@@ -7,9 +7,10 @@ class EbookTools < Formula
 
   bottle do
     cellar :any
-    sha256 "2f88f3ae90a7ba51931938bfb72bc5ab15d3f25cf288f8842325cb15fa7eb391" => :el_capitan
-    sha256 "d7a3d58d4626049940515bde5a33ef9dfce2e4adcfa34237fd0f66e1e9e260cf" => :yosemite
-    sha256 "9bf576c168575ccbf8b9c3abce0374ace176669ae0f84d33d2ff10f81d1a2cdb" => :mavericks
+    revision 1
+    sha256 "5e8c1713c5d418a039181ca164e5a9f04691f430c2262a778627457e928104b9" => :el_capitan
+    sha256 "66d084a7aed69b423d60ab2d193c73751a68a489c717178db36662f50ad9cdb3" => :yosemite
+    sha256 "09c68b7cea454f3181f5df5eae0b5b9ece75e697bcc876354de8de0fc6aa9e9a" => :mavericks
   end
 
   depends_on "libzip"
