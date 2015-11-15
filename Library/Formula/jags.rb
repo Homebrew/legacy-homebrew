8b16913272b3b95d86f9dd5df3536da24c4eac62
Carsten Jørgensen <carstenj@gmail.com>
2015-11-15 17:20:57 +0100

    Upd

--- Library/Formula/jags.rb
+++ Library/Formula/jags.rb
@@ -1,13 +1,9 @@
 class Jags < Formula
   desc "Just Another Gibbs Sampler for Bayesian MCMC simulation"
   homepage "http://mcmc-jags.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/mcmc-jags/JAGS/3.x/Source/JAGS-3.4.0.tar.gz"
-  sha256 "2beaa9a2672c2c95efc55ffa4c8b597a872f20232373daebd17ad539d3d7d82b"
+  url "https://downloads.sourceforge.net/project/mcmc-jags/JAGS/4.x/Source/JAGS-4.0.1.tar.gz"
   bottle do
     cellar :any
-    sha1 "ea59e194feb354ef474a6bd58d4556d0e68adfb0" => :yosemite
-    sha1 "e3d0935caaf8a06b11efd1338d5444fdce87dc63" => :mavericks
-    sha1 "49977edf0dc1571ae846f758c3ba74cf59a2f0f5" => :mountain_lion
   end
 
   revision 1
