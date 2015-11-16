168478a194e73ebcd4445ecf09168e935c23f24d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-16 13:08:32 +0000

    wslay: update 1.0.0 bottle.

--- Library/Formula/wslay.rb
+++ Library/Formula/wslay.rb
@@ -6,9 +6,10 @@ class Wslay < Formula
 
   bottle do
     cellar :any
-    sha1 "d5996cbcaefa8fb31052257c83eccb1121721a35" => :yosemite
-    sha1 "0352cd0da3febe6bfc917f620acd3b694899bcd4" => :mavericks
-    sha1 "04cfebe7140b51febd90b99e2a398bca966759dc" => :mountain_lion
+    revision 1
+    sha256 "30939fd620cff4702d15d61a75774b71e1b226fb7b2b3fab8a3acdf96bdd9b7d" => :el_capitan
+    sha256 "294d4646dcf7d352368de8b422a5354e72b3027a8678e993d68ea8d55646388e" => :yosemite
+    sha256 "410634e15d5ce6f680ccd5d96b97fc2226aad9530aebe42690d3ceb4d7011e69" => :mavericks
   end
 
   option "without-docs", "Don't generate or install documentation"
