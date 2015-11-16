b4f134dd1e4a35548d3feb1f7656de616a3b04cc
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-16 20:41:09 +0000

    psql2csv: bottle unneeded

--- Library/Formula/psql2csv.rb
+++ Library/Formula/psql2csv.rb
@@ -4,6 +4,8 @@ class Psql2csv < Formula
   url "https://github.com/fphilipe/psql2csv/archive/v0.8.tar.gz"
   sha256 "266d27baf4ca20b7dd9383efccd4119619e8e7cd1c6a1f246d8cd12534bd4f66"
 
+  bottle :unneeded
+
   depends_on "postgresql"
 
   def install
