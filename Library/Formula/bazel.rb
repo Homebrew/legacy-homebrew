090be204c586318e47d4c1603f0a35edcb3ed7cf
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-16 22:15:57 +0000

    bazel: add 0.1.1 bottle.

--- Library/Formula/bazel.rb
+++ Library/Formula/bazel.rb
@@ -4,6 +4,13 @@ class Bazel < Formula
   url "https://github.com/bazelbuild/bazel/archive/0.1.1.tar.gz"
   sha256 "49d11d467cf9e32dea618727198592577fbe76ff2e59217c53e3515ddf61cd95"
 
+  bottle do
+    cellar :any
+    sha256 "8b65246774756d31cc97f29bddc6de705ca2fc9517d7a9d997e21aacad39855b" => :el_capitan
+    sha256 "35f5743ed99b97df7a41b489d61677e8ea337d7eb04f95a1ff8089da60adca62" => :yosemite
+    sha256 "ca1cd3f07bfe772cea1ce8e307891adb26081fc0012d5d9f438306ca36a52fef" => :mavericks
+  end
+
   depends_on :java => "1.8+"
 
   def install
