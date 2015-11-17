b0b59f5839591ae2e05c045a13e7fa05ac63c10d
Josh Mervine <joshua@mervine.net>
2015-11-17 07:56:34 -0800

    adding shml formula

---
+++ Library/Formula/shml.rb
@@ -0,0 +1,16 @@
+class Shml < Formula
+  desc "Style Framework for The Terminal"
+  homepage "http://shml.xyz"
+  url "https://github.com/MaxCDN/shml/archive/1.0.4.tar.gz"
+  sha256 "583fefbbf2adf45d989b052696be0f20f695903878558a4d2e9f74255b91ed51"
+
+  def install
+    bin.install "shml.sh"
+    ln_s "#{bin}/shml.sh", "#{bin}/shml"
+  end
+
+  test do
+    assert_match "1.0.4", pipe_output("#{bin}/shml", "-v")
+    assert_match "1.0.4", pipe_output("#{bin}/shml.sh", "-v")
+  end
+end
