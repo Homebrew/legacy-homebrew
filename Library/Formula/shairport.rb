d1287ceaca95c7809b7b8168a8eccf2ce6e460c5
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-17 11:17:43 +0100

    shairport 1.1.1 (import from head-only)

---
+++ Library/Formula/shairport.rb
@@ -0,0 +1,22 @@
+class Shairport < Formula
+  desc "Airtunes emulator"
+  homepage "https://github.com/abrasive/shairport"
+  url "https://github.com/abrasive/shairport/archive/1.1.1.tar.gz"
+  sha256 "1b60df6d40bab874c1220d7daecd68fcff3e47bda7c6d7f91db0a5b5c43c0c72"
+
+  head "https://github.com/abrasive/shairport.git"
+
+  depends_on "pkg-config" => :build
+  depends_on "pulseaudio" => :optional
+  depends_on "libao" => :optional
+  depends_on "openssl"
+
+  def install
+    system "./configure"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/shairport", "-h"
+  end
+end
