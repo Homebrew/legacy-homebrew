587ef1ce8c79a561fcbc0bde9d22c60bd11df7db
leantk <leantk@microsoft.com>
2015-11-17 14:56:10 -0500

    Updated script creation and homepage url

--- Library/Formula/git-credential-manager.rb
+++ Library/Formula/git-credential-manager.rb
@@ -1,6 +1,6 @@
 class GitCredentialManager < Formula
   desc "Stores credentials for Git on Visual Studio Online (VSO)"
-  homepage "http://java.visualstudio.com/Docs/tools/intro"
+  homepage "https://java.visualstudio.com/Docs/tools/intro"
   url "https://github.com/Microsoft/Git-Credential-Manager-for-Mac-and-Linux/releases/download/git-credential-manager-1.2.0/git-credential-manager-1.2.0.jar"
   sha256 "537c066469f3a232818cd876c9787ecf323e8e7b0cfb1bff4028fbb2315e07fc"
 
@@ -10,10 +10,7 @@ class GitCredentialManager < Formula
 
   def install
     libexec.install "git-credential-manager-#{version}.jar"
-    (bin/"git-credential-manager").write <<-EOS.undent
-      #!/bin/sh
-      java -jar "#{libexec}/git-credential-manager-#{version}.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"git-credential-manager-#{version}.jar", "git-credential-manager"
   end
 
   test do
