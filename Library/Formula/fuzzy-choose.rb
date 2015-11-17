aee284b1222eeffcb1c695beadd5ce0ee01e28fa
Steven Degutis <sbdegutis@gmail.com>
2015-11-17 14:31:00 -0600

    Adding fuzzy-choose 1.0

---
+++ Library/Formula/fuzzy-choose.rb
@@ -0,0 +1,17 @@
+class FuzzyChoose < Formula
+  desc "Hybrid string fuzzy matcher that uses both std{in,out} and a native GUI"
+  homepage "https://github.com/sdegutis/choose"
+  url "https://github.com/sdegutis/choose/archive/1.0.tar.gz"
+  sha256 "b1d16c6e143e2a9e9b306cd169ce54535689321d8f016308ff26c82c3d2931bf"
+
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "SDKROOT=", "SYMROOT=build"
+    bin.install "build/Release/choose"
+  end
+
+  test do
+    system "#{bin}/choose", "-h"
+  end
+end
