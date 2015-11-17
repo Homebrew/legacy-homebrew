a6dbfc208e8edb29a15b3033311fc63076006e5c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-17 10:43:01 +0000

    tcpstat: add 0.1 bottle.

--- Library/Formula/tcpstat.rb
+++ Library/Formula/tcpstat.rb
@@ -7,6 +7,13 @@ class Tcpstat < Formula
 
   head "https://github.com/jtt/tcpstat.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e483bf39d0e42a8124c3e2e50f117e66b285bada33df94c1b070460c6df622ea" => :el_capitan
+    sha256 "313fe3a9402b65b6f44b583c49ba83d301b63708b2e0a554100a5e83c03559d8" => :yosemite
+    sha256 "d090af2c66892bd831257dcd3ea109ab8c85cfdd7be6d2c7c2295e13b1c0b7b8" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "tcpstat"
