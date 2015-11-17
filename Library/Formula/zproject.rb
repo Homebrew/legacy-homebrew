3034ec921a90c647b4f5eaa8ac58659eac5f499a
Patrik Wenger <paddor@gmail.com>
2015-11-17 21:34:39 +0100

    add formula for zproject (from @zeromq / @imatix)

---
+++ Library/Formula/zproject.rb
@@ -0,0 +1,19 @@
+class Zproject < Formula
+  desc "CLASS Project Generator"
+  homepage "https://github.com/zeromq/zproject"
+  head "https://github.com/zeromq/zproject.git"
+
+  depends_on "imatix-gsl"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+end
