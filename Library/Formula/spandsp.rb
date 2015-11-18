23856306fbb20e7b7274677b3f99dd63446f8d77
Alexis Hildebrandt <afh@surryhill.net>
2015-11-18 18:23:13 +0100

    spandsp 0.0.6 (new formula)
    DSP functions library for telephony
    
    Closes #46125.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

---
+++ Library/Formula/spandsp.rb
@@ -0,0 +1,34 @@
+class Spandsp < Formula
+  desc "DSP functions library for telephony"
+  homepage "http://www.soft-switch.org"
+  url "http://www.soft-switch.org/downloads/spandsp/spandsp-0.0.6.tar.gz"
+  sha256 "cc053ac67e8ac4bb992f258fd94f275a7872df959f6a87763965feabfdcc9465"
+
+  depends_on "libtiff"
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #define SPANDSP_EXPOSE_INTERNAL_STRUCTURES
+      #include <spandsp.h>
+
+      int main()
+      {
+        t38_terminal_state_t t38;
+        memset(&t38, 0, sizeof(t38));
+        return (t38_terminal_init(&t38, 0, NULL, NULL) == NULL) ? 0 : 1;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lspandsp", "-o", "test"
+    system "./test"
+  end
+end
