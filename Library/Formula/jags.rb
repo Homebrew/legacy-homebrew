17c179e5cc3c30cf82f2b0ffce69e502a785c956
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-18 18:26:36 +0000

    jags: fix bottle spacing

--- Library/Formula/jags.rb
+++ Library/Formula/jags.rb
@@ -3,6 +3,7 @@ class Jags < Formula
   homepage "http://mcmc-jags.sourceforge.net"
   url "https://downloads.sourceforge.net/project/mcmc-jags/JAGS/4.x/Source/JAGS-4.0.1.tar.gz"
   sha256 "3619312cd9ce8163db4d5a51c961078061f6256382a2e93041ee22f7c09ba00e"
+
   bottle do
     cellar :any
     sha256 "577a74d952f74bf91cdae2f2875e6f043b3cb2b1e36fc6526b4e7b4e4a229916" => :el_capitan
