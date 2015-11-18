ba8b345d91ae849aee65e3d792fdc7b04a6d635c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-18 14:32:01 +0000

    aurora-cli: add 0.10.0 bottle.

--- Library/Formula/aurora-cli.rb
+++ Library/Formula/aurora-cli.rb
@@ -4,6 +4,13 @@ class AuroraCli < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=aurora/0.10.0/apache-aurora-0.10.0.tar.gz"
   sha256 "de2ce397ded50c0327c0e5f8599fc7e9b0e1fd0683271023e885e6827972c62c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "55cdd3e6ba2f0d2ceb9eafc8bab0ec2c754c5f8a789d5db3b9284f8615465e4c" => :el_capitan
+    sha256 "928d8922021aa8e080d00025ed9d40904815d94a3313094ad1844a2c74d03578" => :yosemite
+    sha256 "f92482b9042058b5dc7b70cb34235c96c84eb53dcad264dc02d0549f7328f99d" => :mavericks
+  end
+
   depends_on :java => "1.8+"
   depends_on :python if MacOS.version <= :snow_leopard
 
