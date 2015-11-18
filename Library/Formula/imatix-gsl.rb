8af9165afb66dc7b9eb27bd70021eb97c1f3adaf
Patrik Wenger <paddor@gmail.com>
2015-11-18 04:18:21 +0100

    move test-related constants into the test block

--- Library/Formula/imatix-gsl.rb
+++ Library/Formula/imatix-gsl.rb
@@ -10,16 +10,16 @@ class ImatixGsl < Formula
     end
   end
 
-  TEST_SCRIPT_NAME = "hello_world.gsl"
-  TEST_OUTPUT_NAME = "hello_world.txt"
-  TEST_SCRIPT = <<-GSL
+  test do
+    TEST_SCRIPT_NAME = "hello_world.gsl"
+    TEST_OUTPUT_NAME = "hello_world.txt"
+    TEST_SCRIPT = <<-GSL
 .output "#{TEST_OUTPUT_NAME}"
 .who = "world"
 >Hello $(who)!
     GSL
-  TEST_OUTPUT = "Hello world!\n"
+    TEST_OUTPUT = "Hello world!\n"
 
-  test do
     (testpath/TEST_SCRIPT_NAME).write(TEST_SCRIPT)
     system bin/"gsl", testpath/TEST_SCRIPT_NAME
     assert_equal TEST_OUTPUT, (testpath/TEST_OUTPUT_NAME).read
