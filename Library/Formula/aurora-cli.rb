c2c8c2131a38f1785241c32ae4d4487e85a77f65
thinker0 <thinker0@gmail.com>
2015-11-18 23:26:38 +0900

    Apache Aurora Client Tools

---
+++ Library/Formula/aurora-cli.rb
@@ -0,0 +1,30 @@
+class AuroraCli < Formula
+  desc "Apache Aurora Scheduler Client"
+  homepage "https://aurora.apache.org"
+  url "https://www.apache.org/dyn/closer.cgi?path=aurora/0.10.0/apache-aurora-0.10.0.tar.gz"
+  sha256 "de2ce397ded50c0327c0e5f8599fc7e9b0e1fd0683271023e885e6827972c62c"
+
+  depends_on :java => "1.8+"
+  depends_on :python if MacOS.version <= :snow_leopard
+
+  def install
+    system "./pants", "binary", "src/main/python/apache/aurora/kerberos:kaurora"
+    system "./pants", "binary", "src/main/python/apache/aurora/kerberos:kaurora_admin"
+    bin.install "dist/kaurora.pex" => "aurora"
+    bin.install "dist/kaurora_admin.pex" => "aurora_admin"
+  end
+
+  test do
+    ENV["AURORA_CONFIG_ROOT"] = "#{testpath}"
+    (testpath/"clusters.json").write <<-EOS.undent
+        [{
+          "name": "devcluster",
+          "slave_root": "/tmp/mesos/",
+          "zk": "172.16.64.185",
+          "scheduler_zk_path": "/aurora/scheduler",
+          "auth_mechanism": "UNAUTHENTICATED"
+        }]
+    EOS
+    system "#{bin}/aurora_admin", "get_cluster_config", "devcluster"
+  end
+end
