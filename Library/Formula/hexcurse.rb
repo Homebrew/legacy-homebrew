90659a6b6b4c6a4461d33b04b8421526ff52eb3a
Kerrigan29a <kerrigan29a@gmail.com>
2015-11-18 22:34:58 +0100

    Updated to 1.60

--- Library/Formula/hexcurse.rb
+++ Library/Formula/hexcurse.rb
@@ -1,8 +1,8 @@
 class Hexcurse < Formula
   desc "Ncurses-based console hex editor"
   homepage "https://github.com/LonnyGomes/hexcurse"
-  url "https://github.com/LonnyGomes/hexcurse/archive/hexcurse-1.58.tar.gz"
-  sha256 "90470384628e5e31c56c188aa9765ef6939648093f951d6cad8cbbe11fb6bc13"
+  url "https://github.com/LonnyGomes/hexcurse/archive/v1.60.0.tar.gz"
+  sha256 "f6919e4a824ee354f003f0c42e4c4cef98a93aa7e3aa449caedd13f9a2db5530"
 
   def install
     system "./configure", "--disable-dependency-tracking",
