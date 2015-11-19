2793c4ed1b6858edb219ef86831f2c3982a60767
Hannah Howard <hannah@hannahhoward.net>
2015-11-19 00:27:07 -0800

    fswatch 1.7.0
    Update fswatch to latest 1.7.0
    
    Closes #46157.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/fswatch.rb
+++ Library/Formula/fswatch.rb
@@ -1,8 +1,8 @@
 class Fswatch < Formula
   desc "Monitor a directory for changes and run a shell command"
   homepage "https://github.com/emcrisostomo/fswatch"
-  url "https://github.com/emcrisostomo/fswatch/releases/download/1.5.1/fswatch-1.5.1.tar.gz"
-  sha256 "29ffbe88878b92c330a032e1d63b8e569a4f7df880494fb2e9c0d3b768541697"
+  url "https://github.com/emcrisostomo/fswatch/releases/download/1.7.0/fswatch-1.7.0.tar.gz"
+  sha256 "3b42c6f2fb42a5abfcbf51170b189f3f45d6181fb8af39860c47c766473c6010"
 
   bottle do
     cellar :any
