d1233a0418cd00577b624ce75f11f566b4bbd822
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-19 04:02:10 +0100

    slowhttptest: use secure url in test, update homepage, add head

--- Library/Formula/slowhttptest.rb
+++ Library/Formula/slowhttptest.rb
@@ -1,8 +1,9 @@
 class Slowhttptest < Formula
   desc "Simulates application layer denial of service attacks"
-  homepage "https://code.google.com/p/slowhttptest/"
+  homepage "https://github.com/shekyan/slowhttptest"
   url "https://slowhttptest.googlecode.com/files/slowhttptest-1.6.tar.gz"
   sha256 "77c54a64cfa5f12a84729833d9b98d5f27f828f51a5e42ad5914482d0b2bd0d6"
+  head "https://github.com/shekyan/slowhttptest.git"
   revision 1
 
   bottle do
@@ -20,6 +21,6 @@ class Slowhttptest < Formula
   end
 
   test do
-    system "#{bin}/slowhttptest", *%w[-u http://google.com -p 1 -r 1 -l 1 -i 1]
+    system "#{bin}/slowhttptest", *%w[-u https://google.com -p 1 -r 1 -l 1 -i 1]
   end
 end
