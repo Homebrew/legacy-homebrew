661c8a0dbe65508adeff6ee1859737d3220eabf4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-19 03:51:06 +0000

    reaver: add 1.4 bottle.

--- Library/Formula/reaver.rb
+++ Library/Formula/reaver.rb
@@ -4,6 +4,12 @@ class Reaver < Formula
   url "https://reaver-wps.googlecode.com/files/reaver-1.4.tar.gz"
   sha256 "add3050a4a05fe0ab6bfb291ee2de8e9b8a85f1e64ced93ee27a75744954b22d"
 
+  bottle do
+    sha256 "d9adddf27928b284492cc87b565d2748490c1017b0b463bc15223c935f63bb6c" => :el_capitan
+    sha256 "4fbf7b0225730d7a37bfb71bec7b99f78f0b0946df7bcb3e5f274795692e1b3f" => :yosemite
+    sha256 "036092600e415ebba4bc372cff4b7645783e9285c1af56990b0f064db61a0ca4" => :mavericks
+  end
+
   # Adds general support for Mac OS X in reaver:
   # https://code.google.com/p/reaver-wps/issues/detail?id=245
   patch do
