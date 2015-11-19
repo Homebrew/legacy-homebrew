b58e44b90531b2b701ac805d826dad0da6fe698c
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-19 04:13:18 +0100

    aoeui: use secure url in comment

--- Library/Formula/aoeui.rb
+++ Library/Formula/aoeui.rb
@@ -1,6 +1,6 @@
 class Aoeui < Formula
   desc "Lightweight text editor optimized for Dvorak and QWERTY keyboards"
-  homepage "http://aoeui.googlecode.com/"
+  homepage "https://aoeui.googlecode.com/"
   url "https://aoeui.googlecode.com/files/aoeui-1.6.tgz"
   sha256 "0655c3ca945b75b1204c5f25722ac0a07e89dd44bbf33aca068e918e9ef2a825"
   head "http://aoeui.googlecode.com/svn/trunk/"
