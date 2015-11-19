e30de44334e5afff3d573c9bf3742ae4ac43329a
Viktor Szakats <vszakats@users.noreply.github.com>
2015-11-19 03:02:17 +0100

    treecc: use secure homepage

--- Library/Formula/treecc.rb
+++ Library/Formula/treecc.rb
@@ -1,6 +1,6 @@
 class Treecc < Formula
   desc "Aspect-oriented approach to writing compilers"
-  homepage "http://gnu.org/software/dotgnu/treecc/treecc.html"
+  homepage "https://gnu.org/software/dotgnu/treecc/treecc.html"
   url "http://download.savannah.gnu.org/releases/dotgnu-pnet/treecc-0.3.10.tar.gz"
   sha256 "5e9d20a6938e0c6fedfed0cabc7e9e984024e4881b748d076e8c75f1aeb6efe7"
 
