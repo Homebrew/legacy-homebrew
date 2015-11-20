475ed9ed24f68e36f2a96a61cb6621fbe0a5bda5
Blair Hamilton <bhamilton@draftkings.com>
2015-11-19 21:25:51 -0500

    fixed typo in url

--- Library/Formula/nuget.rb
+++ Library/Formula/nuget.rb
@@ -1,6 +1,6 @@
 class Nuget < Formula
   desc "NuGet is the package manager for the Microsoft development platform including .NET"
-  homepage "https://wwww.nuget.org/"
+  homepage "https://www.nuget.org"
   url "https://s3.amazonaws.com/homebrew-nuget/nuget-3.2.0.10516.tar.gz"
   version "3.2.0.10516"
   sha256 "1a76e705a3253d020c5ee8a92816f9c10b8604226eab7fcd196ae29e8bfacbda"
