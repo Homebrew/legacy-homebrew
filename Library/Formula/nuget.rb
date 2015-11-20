e1e984b710e7d1f8e7cc202f1d60cb4841de450e
Blair Hamilton <bhamilton@draftkings.com>
2015-11-19 21:41:54 -0500

    added missing parameter for the check

--- Library/Formula/nuget.rb
+++ Library/Formula/nuget.rb
@@ -2,7 +2,6 @@ class Nuget < Formula
   desc "NuGet is the package manager for the Microsoft development platform including .NET"
   homepage "https://www.nuget.org"
   url "https://s3.amazonaws.com/homebrew-nuget/nuget-3.2.0.10516.tar.gz"
-  version "3.2.0.10516"
   sha256 "1a76e705a3253d020c5ee8a92816f9c10b8604226eab7fcd196ae29e8bfacbda"
 
   depends_on "mono"
@@ -12,6 +11,6 @@ class Nuget < Formula
   end
 
   test do
-    system "#{bin}/nuget"
+    system "#{bin}/nuget", "-v"
   end
 end
