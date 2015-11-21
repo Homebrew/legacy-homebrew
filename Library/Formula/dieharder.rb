eb93306ddeb7bbcb44aae1d215a8bc0c132b2e10
Alex Dunn <dunn.alex@gmail.com>
2015-11-20 21:19:39 -0800

    gsl 2.1

--- Library/Formula/dieharder.rb
+++ Library/Formula/dieharder.rb
@@ -3,6 +3,7 @@ class Dieharder < Formula
   homepage "https://www.phy.duke.edu/~rgb/General/dieharder.php"
   url "https://www.phy.duke.edu/~rgb/General/dieharder/dieharder-3.31.1.tgz"
   sha256 "6cff0ff8394c553549ac7433359ccfc955fb26794260314620dfa5e4cd4b727f"
+  revision 1
 
   bottle do
     cellar :any
--- Library/Formula/gsl.rb
+++ Library/Formula/gsl.rb
@@ -1,9 +1,9 @@
 class Gsl < Formula
   desc "Numerical library for C and C++"
   homepage "https://www.gnu.org/software/gsl/"
-  url "http://ftpmirror.gnu.org/gsl/gsl-1.16.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/gsl/gsl-1.16.tar.gz"
-  sha256 "73bc2f51b90d2a780e6d266d43e487b3dbd78945dd0b04b14ca5980fe28d2f53"
+  url "http://ftpmirror.gnu.org/gsl/gsl-2.1.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/gsl/gsl-2.1.tar.gz"
+  sha256 "59ad06837397617f698975c494fe7b2b698739a59e2fcf830b776428938a0c66"
 
   bottle do
     cellar :any
@@ -20,7 +20,7 @@ class Gsl < Formula
     ENV.universal_binary if build.universal?
 
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make" # A GNU tool which doesn't support just make install! Shameful!
+    system "make"
     system "make", "install"
   end
 
