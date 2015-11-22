a8095c4ba2a72d6b4bd7eea4218edc53c16b63f5
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-22 02:19:48 +0000

    gst-python: sandbox fix
    Closes #46164.

--- Library/Formula/gst-python.rb
+++ Library/Formula/gst-python.rb
@@ -1,5 +1,5 @@
 class GstPython < Formula
-  desc "GStreamer Python overrides for gobject-introspection-based pygst bindings"
+  desc "Python overrides for gobject-introspection-based pygst bindings"
   homepage "http://gstreamer.freedesktop.org/modules/gst-python.html"
   url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-1.6.1.tar.xz"
   sha256 "3cbe332e18fd2eaf23ddeee96c414f79ac1edc2f7d990582fa0ec5f977bd04f1"
@@ -13,18 +13,18 @@ class GstPython < Formula
   depends_on "gst-plugins-base"
   depends_on "pygobject3"
 
+  link_overwrite "lib/python2.7/site-packages/gi/overrides"
+
   def install
-    # ensure files are kept inside the keg
-    # this patch is necessary as long as gobject-introspection requires :python on older OS X releases
-    inreplace "gi/overrides/Makefile.in", "$(PYGI_OVERRIDES_DIR)", "@libdir@/python2.7/site-packages/gi/overrides" if MacOS.version <= :mavericks
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
+    # pygi-overrides-dir switch ensures files don't break out of sandbox.
+    system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--with-pygi-overrides-dir=#{lib}/python2.7/site-packages/gi/overrides"
     system "make", "install"
   end
 
   test do
-    system "gst-inspect-1.0", "python"
+    system "#{Formula["gstreamer"].opt_bin}/gst-inspect-1.0", "python"
   end
 end
