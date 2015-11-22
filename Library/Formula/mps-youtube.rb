4790eb49cc35ac275bf866c599e0f069a263a966
Alex Balgavy <a.balgavy@gmail.com>
2015-11-22 12:41:47 +0100

    Changed install function from `system "python"` to `system "python3"` to match installation requirements.

--- Library/Formula/mps-youtube.rb
+++ Library/Formula/mps-youtube.rb
@@ -23,12 +23,12 @@ class MpsYoutube < Formula
 
     res.each do |r|
       resource(r).stage do
-        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
 
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
+    system "python3", *Language::Python.setup_install_args(libexec)
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
