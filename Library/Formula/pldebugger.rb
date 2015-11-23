9bb185639d9794968dc41250a6563caa55d43c39
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-23 10:49:17 +0000

    pldebugger: add 1.0 bottle.

--- Library/Formula/pldebugger.rb
+++ Library/Formula/pldebugger.rb
@@ -4,6 +4,13 @@ class Pldebugger < Formula
   url "http://git.postgresql.org/git/pldebugger.git",
       :tag => "REL-9_5_0",
       :revision => "85d7b3b2821301e182d5974d9e6f353d7a241eff"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2f2b2a88b1501f0285350d4034b02ee799bb0ca2b20f81e8063098c134504ff0" => :el_capitan
+    sha256 "86a9c396d0d5e3efbf55b2071e25a0c35e0d35177d3d0c2077f686fc42dad27e" => :yosemite
+    sha256 "cb047f3816d9643777ed5adedf38a68f4924ea29c0d29c2866185eb3aae5937f" => :mavericks
+  end
+
   version "1.0" # See default_version field in pldbgapi.control
   sha256 "2bb8e27aa8f8434a4861fdbc70adb9cb4b47e1dfe472910d62d6042cb80a2ee1"
 
