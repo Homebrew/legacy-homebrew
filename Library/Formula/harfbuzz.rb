4d98c6239047615a0f4ab4d3a1ab534408448172
Denis Jacquerye <moyogo@gmail.com>
2015-11-24 20:43:45 +0000

    harfbuzz: 1.1.1

--- Library/Formula/harfbuzz.rb
+++ Library/Formula/harfbuzz.rb
@@ -1,8 +1,8 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://wiki.freedesktop.org/www/Software/HarfBuzz/"
-  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.0.4.tar.bz2"
-  sha256 "b030373457e7c00d3a7920f15e6fcd35defac3c4e44cd14ed85869030df74381"
+  url "http://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.1.1.tar.bz2"
+  sha256 "0c366a77276fa69d40006cf8a2f2dbcd82f3fb406f3960c3c3a0d34dd3a0f06c"
 
   head do
     url "https://github.com/behdad/harfbuzz.git"
