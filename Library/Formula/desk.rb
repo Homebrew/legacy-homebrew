ba1dabf0f0a0e71c6503aa2ac68543d23c41916c
Christian Michael Paul Bankester <cbankester@immense.net>
2015-11-24 13:41:02 -0600

    desk 0.3.1 (new formula)

---
+++ Library/Formula/desk.rb
@@ -0,0 +1,18 @@
+class Desk < Formula
+  desc "Lightweight workspace manager for the shell"
+  homepage "https://github.com/jamesob/desk"
+  url "https://github.com/jamesob/desk/archive/v0.3.1.tar.gz"
+  sha256 "b687e2cfa742f763d689391f67a5b5225324e282a0fed100487b1570988d7758"
+
+  def install
+    bin.install "desk"
+  end
+
+  test do
+    system "mkdir", "-p", testpath/".desk/desks"
+    (testpath/".desk/desks/test-desk.sh").write("#\n# Description: A test desk\n#")
+    list = pipe_output("#{bin}/desk list")
+    assert_match /test-desk/, list
+    assert_match /A test desk/, list
+  end
+end
