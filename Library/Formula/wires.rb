3b3d3f6f670ecd0c624c2626f5f90ae018640bcf
Anthony Green <anthony.green@bbc.co.uk>
2015-11-25 11:47:16 +0000

    Add formula for Marionette wires

---
+++ Library/Formula/wires.rb
@@ -0,0 +1,19 @@
+class Wires < Formula
+  desc "WebDriver <-> Marionette proxy"
+  homepage "https://github.com/jgraham/wires"
+  url "https://github.com/jgraham/wires/archive/v0.4.2.tar.gz"
+  sha256 "f86fdac8f69f94bd57dc9fdb45851585a24790b069605d033288e419653c9efe"
+
+  depends_on "rust" => :build
+
+  def install
+    puts "#{bin}"
+    system "cargo build"
+    bin.install "target/debug/wires"
+  end
+
+  test do
+    system "#{bin}/wires", "--help"
+  end
+
+end
