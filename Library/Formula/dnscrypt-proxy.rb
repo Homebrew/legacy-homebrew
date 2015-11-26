0e526bbbbdd8c087be746d18365cbb7af23d4d94
notDavid <david@staron.nl>
2015-11-26 18:46:39 +0100

    Dnscrypt - exclude more stuff if not build with minisign..

--- Library/Formula/dnscrypt-proxy.rb
+++ Library/Formula/dnscrypt-proxy.rb
@@ -41,25 +41,27 @@ class DnscryptProxy < Formula
     system "./configure", *args
     system "make", "install"
 
-    (bin/"dnscrypt-update-resolvers").write <<-EOS.undent
-        #!/bin/sh
-        RESOLVERS_UPDATES_BASE_URL=https://download.dnscrypt.org/dnscrypt-proxy
-        RESOLVERS_LIST_BASE_DIR=#{pkgshare}
-        RESOLVERS_LIST_PUBLIC_KEY="RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"
+    if build.with? "minisign"
+      (bin/"dnscrypt-update-resolvers").write <<-EOS.undent
+          #!/bin/sh
+          RESOLVERS_UPDATES_BASE_URL=https://download.dnscrypt.org/dnscrypt-proxy
+          RESOLVERS_LIST_BASE_DIR=#{pkgshare}
+          RESOLVERS_LIST_PUBLIC_KEY="RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"
 
-        curl -L --max-redirs 5 -4 -m 30 --connect-timeout 30 -s \
-          "${RESOLVERS_UPDATES_BASE_URL}/dnscrypt-resolvers.csv" > \
-          "${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.tmp" && \
-        curl -L --max-redirs 5 -4 -m 30 --connect-timeout 30 -s \
-          "${RESOLVERS_UPDATES_BASE_URL}/dnscrypt-resolvers.csv.minisig" > \
-          "${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.minisig" && \
-        minisign -Vm ${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.tmp \
-          -x "${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.minisig" \
-          -P "$RESOLVERS_LIST_PUBLIC_KEY" -q && \
-        mv -f ${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.tmp \
-          ${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv
-      EOS
-      chmod 0775, bin/"dnscrypt-update-resolvers"
+          curl -L --max-redirs 5 -4 -m 30 --connect-timeout 30 -s \
+            "${RESOLVERS_UPDATES_BASE_URL}/dnscrypt-resolvers.csv" > \
+            "${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.tmp" && \
+          curl -L --max-redirs 5 -4 -m 30 --connect-timeout 30 -s \
+            "${RESOLVERS_UPDATES_BASE_URL}/dnscrypt-resolvers.csv.minisig" > \
+            "${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.minisig" && \
+          minisign -Vm ${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.tmp \
+            -x "${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.minisig" \
+            -P "$RESOLVERS_LIST_PUBLIC_KEY" -q && \
+          mv -f ${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv.tmp \
+            ${RESOLVERS_LIST_BASE_DIR}/dnscrypt-resolvers.csv
+        EOS
+        chmod 0775, bin/"dnscrypt-update-resolvers"
+    end
   end
 
   def post_install
@@ -68,7 +70,7 @@ class DnscryptProxy < Formula
     end
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; s = <<-EOS.undent
     After starting dnscrypt-proxy, you will need to point your
     local DNS server to 127.0.0.1. You can do this by going to
     System Preferences > "Network" and clicking the "Advanced..."
@@ -79,19 +81,26 @@ class DnscryptProxy < Formula
     and under the "nobody" user using the dnscrypt.eu-dk DNSCrypt-enabled
     resolver. If you would like to change these settings, you will have to edit
     the plist file (e.g., --resolver-address, --provider-name, --provider-key, etc.)
-
-    If at some point the resolver file gets outdated, it can be updated to the
-    latest version by running: #{bin}/dnscrypt-update-resolvers
-
-    To check that dnscrypt-proxy is working correctly, open Terminal and enter the
-    following command. Replace en1 with whatever network interface you're using:
-
-        sudo tcpdump -i en1 -vvv 'port 443'
-
-    You should see a line in the result that looks like this:
-
-        resolver2.dnscrypt.eu.https
     EOS
+    if build.with? "minisign"
+      s += <<-EOS.undent
+
+        If at some point the resolver file gets outdated, it can be updated to the
+        latest version by running: #{opt_bin}/dnscrypt-update-resolvers
+      EOS
+    end
+    s += <<-EOS.undent
+
+      To check that dnscrypt-proxy is working correctly, open Terminal and enter the
+      following command. Replace en1 with whatever network interface you're using:
+
+          sudo tcpdump -i en1 -vvv 'port 443'
+
+      You should see a line in the result that looks like this:
+
+          resolver2.dnscrypt.eu.https
+    EOS
+    s
   end
 
   plist_options :startup => true
