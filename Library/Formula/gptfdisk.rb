7d0ce47a329350461db72a1f9feeccd5abae56bc
Alex Dunn <dunn.alex@gmail.com>
2015-11-25 22:05:01 -0800

    gptfdisk 1.0.1

--- Library/Formula/gptfdisk.rb
+++ Library/Formula/gptfdisk.rb
@@ -1,9 +1,8 @@
 class Gptfdisk < Formula
   desc "Text-mode partitioning tools"
   homepage "http://www.rodsbooks.com/gdisk/"
-  url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.0/gptfdisk-1.0.0.tar.gz"
-  sha256 "5b66956743a799fc0471cdb032665c1391e82f9c5b3f1d7d726d29fe2ba01d6c"
-  revision 1
+  url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.1/gptfdisk-1.0.1.tar.gz"
+  sha256 "864c8aee2efdda50346804d7e6230407d5f42a8ae754df70404dd8b2fdfaeac7"
 
   bottle do
     cellar :any
@@ -13,10 +12,15 @@ class Gptfdisk < Formula
     sha256 "95593d9ce977a9529b11c9de8ee1089e56c67d76a642e13bdac31097aa5c7f69" => :mountain_lion
   end
 
+  # Due to SIP
+  depends_on MaximumMacOSRequirement => :yosemite
+
   depends_on "popt"
   depends_on "icu4c"
 
   def install
+    inreplace "Makefile.mac", "/opt/local/lib/libncurses.a", "/usr/lib/libncurses.dylib"
+
     system "make", "-f", "Makefile.mac"
     sbin.install "gdisk", "cgdisk", "sgdisk", "fixparts"
     man8.install Dir["*.8"]
