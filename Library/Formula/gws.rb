2d06522101ca26fe6018e588d429e6202537ae05
Alex Dunn <dunn.alex@gmail.com>
2015-11-26 15:39:46 -0800

    gws 0.1.8
    Closes #46410.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/gws.rb
+++ Library/Formula/gws.rb
@@ -1,8 +1,8 @@
 class Gws < Formula
   desc "Manage workspaces composed of git repositories"
   homepage "https://streakycobra.github.io/gws/"
-  url "https://github.com/StreakyCobra/gws/archive/0.1.7.tar.gz"
-  sha256 "36c25392f5cb93a39441a1973842249a62b605a2a7e369a040c78450683697d0"
+  url "https://github.com/StreakyCobra/gws/archive/0.1.8.tar.gz"
+  sha256 "c240601b0adcc5ae402199217fc0d5fd6775ada69919a860ad3c1c4b16805e63"
 
   bottle :unneeded
 
