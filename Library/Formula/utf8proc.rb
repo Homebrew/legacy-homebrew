048bac9a457c449e3ac6c4730597c40bda9d0f7f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-26 08:22:35 +0000

    utf8proc: add 1.3.1 bottle.

--- Library/Formula/utf8proc.rb
+++ Library/Formula/utf8proc.rb
@@ -4,6 +4,13 @@ class Utf8proc < Formula
   url "https://github.com/JuliaLang/utf8proc/archive/v1.3.1.tar.gz"
   sha256 "83b60fe21fd8a017b8ad469515873893c8e911a5bef336a427594d398b5688cc"
 
+  bottle do
+    cellar :any
+    sha256 "ebecca82cd8d532a928cf5137e8589fdf968cff9ad723fae6d6fbfdd76148709" => :el_capitan
+    sha256 "8d117faedfa60b37e7b25598aed8b3d9d4dd048ecbb474e42c1014b20ec141c2" => :yosemite
+    sha256 "45176943620b894aa86b50a2774e1241d0a7ebd85d0edeb1e56665c8240d9c31" => :mavericks
+  end
+
   def install
     system "make", "install", "prefix=#{prefix}"
   end
