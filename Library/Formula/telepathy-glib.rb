b66d6921df7ca6ad5f4e5008382d17adcbbb7427
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-26 14:29:44 +0100

    telepathy-glib 0.99.11
    Closes #46392.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/telepathy-glib.rb
+++ Library/Formula/telepathy-glib.rb
@@ -1,8 +1,8 @@
 class TelepathyGlib < Formula
   desc "Telepathy GLib library for clients and connection managers"
-  homepage "http://telepathy.freedesktop.org/wiki/"
-  url "http://telepathy.freedesktop.org/releases/telepathy-glib/telepathy-glib-0.24.0.tar.gz"
-  sha256 "ae0002134991217f42e503c43dea7817853afc18863b913744d51ffa029818cf"
+  homepage "https://wiki.freedesktop.org/telepathy/"
+  url "http://telepathy.freedesktop.org/releases/telepathy-glib/telepathy-glib-0.99.11.tar.gz"
+  sha256 "268cbf0199804ecb6001c7c11e5596bc3cea40a600091c144d14f837ac90cd97"
 
   bottle do
     revision 1
@@ -18,16 +18,12 @@ class TelepathyGlib < Formula
   depends_on "dbus-glib"
 
   def install
-    args = %W[
-      --disable-debug
-      --prefix=#{prefix}
-      --enable-introspection=yes
-      --disable-installed-tests
-      --disable-installed-examples
-      --disable-gtk-doc-html
-    ]
-
-    system "./configure", *args
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--enable-introspection=yes",
+                          "--disable-installed-tests",
+                          "--disable-installed-examples",
+                          "--disable-gtk-doc-html"
     system "make", "install"
   end
 end
