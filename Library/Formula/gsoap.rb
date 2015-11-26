7e5f9f0751b1980d8f0175f98ae886cd07b3aa62
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-26 06:25:15 +0000

    gsoap: add 2.8.25 bottle.

--- Library/Formula/gsoap.rb
+++ Library/Formula/gsoap.rb
@@ -4,6 +4,12 @@ class Gsoap < Formula
   url "https://downloads.sourceforge.net/project/gsoap2/gSOAP/gsoap_2.8.25.zip"
   sha256 "396b66eb7ab410d89a38d1319443d99fd8b43cab8743f9c69f03a2ed3215db05"
 
+  bottle do
+    sha256 "bb0550d7f9a92f910758f6735a753d70876adcfd15bbc2461b15eb3917e94ee2" => :el_capitan
+    sha256 "308b3ba08bab0f13ae65b0c4f4638c79ae24acb45295d4548687d91c3bd9dc0c" => :yosemite
+    sha256 "2bd60448e21d2b37eadc3193aae093b7c95ed9b499b97d21de6bbe0234569046" => :mavericks
+  end
+
   depends_on "openssl"
 
   def install
