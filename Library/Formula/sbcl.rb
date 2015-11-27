13944c47aeb1980f89a252e91ed2780d756241a0
Auvi R <getauvi23@gmail.com>
2015-11-27 17:41:05 -0500

    sbcl 1.3.1
    Closes #46446.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/sbcl.rb
+++ Library/Formula/sbcl.rb
@@ -1,8 +1,8 @@
 class Sbcl < Formula
   desc "Steel Bank Common Lisp system"
   homepage "http://www.sbcl.org/"
-  url "https://downloads.sourceforge.net/project/sbcl/sbcl/1.3.0/sbcl-1.3.0-source.tar.bz2"
-  sha256 "d60caf6a0a495e41fc9e7f6a797e7c4188cef1f9de84ca263ff654d4d7ae99b3"
+  url "https://downloads.sourceforge.net/project/sbcl/sbcl/1.3.1/sbcl-1.3.1-source.tar.bz2"
+  sha256 "a2e547e471a368349a43b1feee78ca6139aae0c60b8fcaa6ab0fd0e5b8e0ed3d"
 
   head "git://sbcl.git.sourceforge.net/gitroot/sbcl/sbcl.git"
 
