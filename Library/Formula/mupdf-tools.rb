8fd1a0e9d777498884ad1dbb0d693452f811f340
Adler Medrado <adler@adlermedrado.com.br>
2015-11-28 17:58:58 -0200

    Updating URL for mupdf-tools download

--- Library/Formula/mupdf-tools.rb
+++ Library/Formula/mupdf-tools.rb
@@ -1,7 +1,7 @@
 class MupdfTools < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "http://mupdf.com"
-  url "http://mupdf.com/downloads/archive/mupdf-1.7a-source.tar.gz"
+  url "http://mupdf.com/downloads/archive/mupdf-1.7a-source.tar"
   sha256 "8c035ffa011fc44f8a488f70da3e6e51889508bbf66fe6b90a63e0cfa6c17d1c"
   head "git://git.ghostscript.com/mupdf.git"
 
