18097e852f9d04da8aa45a71da672cb67f265aec
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-28 16:40:03 +0000

    gupnp-tools: update 0.8.11 bottle.

--- Library/Formula/gupnp-tools.rb
+++ Library/Formula/gupnp-tools.rb
@@ -5,9 +5,9 @@ class GupnpTools < Formula
   sha256 "f4ce8799cf32077eb2ab8076c3420d6b8fc3dcdc3fffbfe84a2ac59764ec8d3d"
 
   bottle do
-    sha256 "f136c24128d85cf73a05cde6e7debbafe69cd11cb9a58c2b8a70d027b171af20" => :yosemite
-    sha256 "b1b64ff86b74b5bd9ccec60ab77eaa5f61ea593ee029f5e31bedb178422c741d" => :mavericks
-    sha256 "790821aad6ce8ea5b1b677372b1790133134ef3e7e2e4119ec3fcea2c7172aae" => :mountain_lion
+    sha256 "ddfff4bfbf6da35a8439146f578d34fb80cc3625086b14b89f24d8f4d58db926" => :el_capitan
+    sha256 "a4bae3296dc5d8062f3826a4265e622330098ac1d1f3756ce8dc59404d1ac1d6" => :yosemite
+    sha256 "aa42be7da9a3110a14546bf9a45f9fff2e484ed5b5a7c1360f4b02a94daa5f75" => :mavericks
   end
 
   # filed upstream as https://bugzilla.gnome.org/show_bug.cgi?id=758779
