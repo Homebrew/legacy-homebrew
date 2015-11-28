d90374213991f2bb9a510a1b6647f33cb01ef5e2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-28 01:55:27 +0000

    camlp4: update 4.02.3+6 bottle.

--- Library/Formula/camlp4.rb
+++ Library/Formula/camlp4.rb
@@ -8,10 +8,9 @@ class Camlp4 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
-    sha256 "2fbb2011ee0a44abdc5ce6cbdea7b1f271a00b903bd14dab9ecf3f5318571204" => :el_capitan
-    sha256 "3a3bcb70dad5b897a8d1db8bf28fe15794de5f14249d00b88ce5a8411e5e4a9d" => :yosemite
-    sha256 "08ca2278024819f625b4a0ca52b2cb931279c9d7ec1d9a55c9e9748c5f55cb15" => :mavericks
+    sha256 "bbfa590ccdfdfee06dbd5a751201bb8e74b469ef2e2b86755225ffb2eb4720da" => :el_capitan
+    sha256 "6194932d62c0002c8286e7c7bf3d04272301669d2a036b7f9d717af3b1370ea4" => :yosemite
+    sha256 "a791cec5c4ff4cec0bdff1f25ac2b1a6e906b3e9c485bac285d1b527c9caeeaa" => :mavericks
   end
 
   depends_on "ocaml"
