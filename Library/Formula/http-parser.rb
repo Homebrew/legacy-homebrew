49ff1a58da87b76953942400ad5e4db0a6f72b25
Alex Dunn <dunn.alex@gmail.com>
2015-11-29 14:37:32 -0800

    http-parser 2.6.0

--- Library/Formula/http-parser.rb
+++ Library/Formula/http-parser.rb
@@ -1,8 +1,8 @@
 class HttpParser < Formula
   desc "HTTP request/response parser for c"
-  homepage "https://github.com/joyent/http-parser"
-  url "https://github.com/joyent/http-parser/archive/v2.5.0.tar.gz"
-  sha256 "e3b4ba58f4e6ee5fbec781df020e5cb74c3a799a07f059e1e125127a0b801481"
+  homepage "https://github.com/nodejs/http-parser"
+  url "https://github.com/nodejs/http-parser/archive/v2.6.0.tar.gz"
+  sha256 "a11c5ccb9808496f3de66d54ea1f89271919923307e31c75de2a3a77a6754c97"
 
   bottle do
     cellar :any
@@ -14,11 +14,6 @@ class HttpParser < Formula
   end
 
   depends_on "coreutils" => :build
-
-  patch do
-    url "https://patch-diff.githubusercontent.com/raw/joyent/http-parser/pull/247.diff"
-    sha256 "0f081d0cbf44575b2db2889b318245e692706b756725ba64d1277f571c8b921f"
-  end
 
   def install
     system "make", "install", "PREFIX=#{prefix}", "INSTALL=ginstall"
