25d696cb8d65a75ecad4e28ee84aca7d7b7e96ce
gunnaraasen <gunnar.r.aasen@gmail.com>
2015-11-29 21:48:08 -0800

    influxdb 0.9.5.1
    Closes #46499.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/influxdb.rb
+++ Library/Formula/influxdb.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Influxdb < Formula
   desc "Time series, events, and metrics database"
   homepage "https://influxdb.com"
-  url "https://github.com/influxdb/influxdb/archive/v0.9.5.tar.gz"
-  sha256 "701620608cd3c2ee973b59835dc3d4677ec8719dc50e4b5760b56e64b05e0372"
+  url "https://github.com/influxdb/influxdb/archive/v0.9.5.1.tar.gz"
+  sha256 "d823288a826188526831602ee834195266abb7d4a0db7c3638d8f86793a0ffd6"
 
   bottle do
     cellar :any_skip_relocation
@@ -15,6 +15,11 @@ class Influxdb < Formula
 
   head do
     url "https://github.com/influxdb/influxdb.git"
+
+    go_resource "github.com/paulbellamy/ratecounter" do
+      url "https://github.com/paulbellamy/ratecounter.git",
+        :revision => "5a11f585a31379765c190c033b6ad39956584447"
+    end
   end
 
   depends_on "go" => :build
@@ -36,12 +41,12 @@ class Influxdb < Formula
 
   go_resource "github.com/boltdb/bolt" do
     url "https://github.com/boltdb/bolt.git",
-      :revision => "0b00effdd7a8270ebd91c24297e51643e370dd52"
+      :revision => "6e1ca38c6a73025366cd8705553b404746ee6e63"
   end
 
   go_resource "github.com/dgryski/go-bits" do
     url "https://github.com/dgryski/go-bits.git",
-      :revision => "2c7641e7dfe3945a0fe755f58c85ab306624956d"
+      :revision => "dffe1d50093678b150156c4ad97029473994f459"
   end
 
   go_resource "github.com/dgryski/go-bitstream" do
@@ -96,7 +101,7 @@ class Influxdb < Formula
 
   go_resource "golang.org/x/crypto" do
     url "https://go.googlesource.com/crypto.git",
-      :revision => "346896d57731cb5670b36c6178fc5519f3225980"
+      :revision => "beef0f4390813b96e8e68fd78570396d0f4751fc"
   end
 
   go_resource "gopkg.in/fatih/pool.v2" do
@@ -125,7 +130,7 @@ class Influxdb < Formula
       if build.head?
         system "go", "install", "-ldflags", "-X main.version 0.9.6-HEAD -X main.branch master -X main.commit #{`git rev-parse HEAD`.strip}", "./..."
       else
-        system "go", "install", "-ldflags", "-X main.version 0.9.5 -X main.branch 0.9.5 -X main.commit 770b0ecf9f1f61a9d3a97136d5b6d7dd21afea1e", "./..."
+        system "go", "install", "-ldflags", "-X main.version 0.9.5.1 -X main.branch 0.9.5 -X main.commit 9eab56311373ee6f788ae5dfc87e2240038f0eb4", "./..."
       end
     end
 
