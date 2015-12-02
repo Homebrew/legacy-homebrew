98b6b75e0ac4833e9270f1c692ba1b3e0725d50e
Mike McQuaid <mike@mikemcquaid.com>
2015-12-02 17:24:14 +0200

    diffoscope: use upstream committed patch.

--- Library/Formula/diffoscope.rb
+++ Library/Formula/diffoscope.rb
@@ -16,6 +16,11 @@ class Diffoscope < Formula
   depends_on "gnu-tar"
   depends_on :python3
 
+  patch do
+    url "https://anonscm.debian.org/cgit/reproducible/diffoscope.git/patch/?id=261be7"
+    sha256 "aeaffa34a774e05477c9ef78df35174b006670b2963b9064c9c4c13484825b0b"
+  end
+
   resource "libarchive-c" do
     url "https://pypi.python.org/packages/source/l/libarchive-c/libarchive-c-2.2.tar.gz"
     sha256 "5d54aa6f6ab21e3bd12c2f8b6c4e80316b049c2b60ab0a4418cb34d8c63e997c"
@@ -27,8 +32,6 @@ class Diffoscope < Formula
   end
 
   def install
-    # Submitted upstream to lunar@debian.org
-    inreplace "diffoscope/difference.py", "'diff', '-au7'", "'diff', '-aU7'"
     inreplace "diffoscope/comparators/tar.py", "'tar'", "'gtar'"
 
     pyver = Language::Python.major_minor_version "python3"
