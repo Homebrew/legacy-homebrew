9df27201549e7f92c05b62c10a1d311e4b553b40
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-04 09:24:34 +0000

    coccinelle: update 1.0.4 bottle.

--- Library/Formula/coccinelle.rb
+++ Library/Formula/coccinelle.rb
@@ -5,9 +5,10 @@ class Coccinelle < Formula
   sha256 "7f823813a2ea299c0f6c01d8419b83c4dc6617116d32ba99d726443a1c22b06d"
 
   bottle do
-    sha256 "8345aa22d8966d5a812a09c7eba291e3b4c62ca233795f2f4ac1ad7f5f718098" => :yosemite
-    sha256 "7a6ab8f68cc53737b6f64246d720aa4090246014f9309e69293e0c5516193f62" => :mavericks
-    sha256 "8a91d91164c21682355d050b84752a672d725027df95a32654a53aea02ff394f" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "c7ffdc23dd19c52170042169d4c2d1a63113db688edb31b46e7f808fde0c6b05" => :el_capitan
+    sha256 "6613d4b067b2454909ba9512d9ccb81028ad0b80468eb0059876f4fa477eb1f1" => :yosemite
+    sha256 "895063e8e6f23dfd401d9473b752eb6c96d7c5abdc6cc98d85d4db65d64baa88" => :mavericks
   end
 
   depends_on "ocaml"
