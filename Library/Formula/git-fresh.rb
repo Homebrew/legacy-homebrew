77331e8f0df7f4293888b0bbe6a1fe12f36ffb69
Mike McQuaid <mike@mikemcquaid.com>
2015-12-04 18:16:30 +0000

    git-fresh: bottle unneeded.

--- Library/Formula/git-fresh.rb
+++ Library/Formula/git-fresh.rb
@@ -4,6 +4,8 @@ class GitFresh < Formula
   url "https://github.com/imsky/git-fresh/archive/v1.2.3.tar.gz"
   sha256 "19f41d6c6f82b57fe02e6024f5ec46e72315e17fe01f0ddb29756bfd723c6a90"
 
+  bottle :unneeded
+
   def install
     system "./install", bin
   end
