a8c7a4fca458ec9134209fa6d1a6fc095babf4bc
Ephexeve M <ephexeve@gmail.com>
2015-12-04 11:35:47 -0200

    Fixed tab issue

--- Library/Formula/i2p.rb
+++ Library/Formula/i2p.rb
@@ -1,9 +1,9 @@
 class I2p < Formula
   desc "An anonymous overlay network - a network within a network"
   homepage "https://geti2p.net"
-	url "https://download.i2p2.de/releases/0.9.23/i2pinstall_0.9.23.jar"
+  url "https://download.i2p2.de/releases/0.9.23/i2pinstall_0.9.23.jar"
   sha256 "843a8059830b009d10c47cc7c85e260ad88f7c3c16e289bbf80c0eb178318823"
-
+  
   depends_on :java => "1.6+"
 
   def install
