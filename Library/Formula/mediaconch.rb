8a5ef42339a4242e87d33b185a1f5df86402462b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-04 20:48:44 +0000

    mediaconch: add 15.11 bottle.

--- Library/Formula/mediaconch.rb
+++ Library/Formula/mediaconch.rb
@@ -5,6 +5,13 @@ class Mediaconch < Formula
   version "15.11"
   sha256 "06f76ac63a41eb5b7e2c31fd16e450a2d7ae93db832710497d140c1b2c47bf82"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "54f45f88f933e2d9d66e11d2eaa33ce5606fd20ebd07c3c8622c74f7793af3ef" => :el_capitan
+    sha256 "0e0ea9b251f1cad8e5fc7358fa26c9a50a752407fc556cd7b1ee8bb303623ad1" => :yosemite
+    sha256 "9da30167b4741cc6fba1030a4ba0dac1b78ac5dd59f6cd54b936967616907018" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   # fails to build against Leopard's older libcurl
   depends_on "curl" if MacOS.version < :snow_leopard
