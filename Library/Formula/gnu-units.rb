dd998dcb8d83897e672537e7f932dca0b640ff2a
Fenhl <fenhl@fenhl.net>
2015-12-04 06:46:20 +0000

    gnu-units: Fix Python 2 path for units_cur, fixes #46527

--- Library/Formula/gnu-units.rb
+++ Library/Formula/gnu-units.rb
@@ -28,6 +28,11 @@ class GnuUnits < Formula
   patch :DATA
 
   def install
+    # OSX doesn't provide 'python2', but on some Linux distributions
+    # 'python' is an alias for python3 so this won't be changed
+    # upstream
+    inreplace "units_cur", "#!/usr/bin/python2", "#!/usr/bin/env python"
+
     args = ["--prefix=#{prefix}"]
     args << "--program-prefix=g" if build.without? "default-names"
 
