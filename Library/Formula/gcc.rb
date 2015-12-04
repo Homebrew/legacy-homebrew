da1822c740230e0c18e1fd85327cb8c9d3665c34
Izaak Beekman <zbeekman@gmail.com>
2015-12-04 12:46:50 -0500

    gcc 5.3
    Closes #46670.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Library/Formula/gcc.rb
+++ Library/Formula/gcc.rb
@@ -21,9 +21,9 @@ class Gcc < Formula
 
   desc "GNU compiler collection"
   homepage "https://gcc.gnu.org"
-  url "http://ftpmirror.gnu.org/gcc/gcc-5.2.0/gcc-5.2.0.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/gcc/gcc-5.2.0/gcc-5.2.0.tar.bz2"
-  sha256 "5f835b04b5f7dd4f4d2dc96190ec1621b8d89f2dc6f638f9f8bc1b1014ba8cad"
+  url "http://ftpmirror.gnu.org/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2"
+  sha256 "b84f5592e9218b73dbae612b5253035a7b34a9a1f7688d2e1bfaaf7267d5c4db"
 
   head "svn://gcc.gnu.org/svn/gcc/trunk"
 
