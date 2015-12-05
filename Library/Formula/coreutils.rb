60d968715fa90c5453c696d26596123ef37a55df
chdiza <chdiza@gmail.com>
2015-12-05 15:17:16 -0500

    coreutils: Add workaround for unremovable dir tree problem
    Closes #46709.

--- Library/Formula/coreutils.rb
+++ Library/Formula/coreutils.rb
@@ -31,6 +31,16 @@ class Coreutils < Formula
   depends_on "gmp" => :optional
 
   def install
+    # Work around unremovable, nested dirs bug that affects lots of
+    # GNU projects. See:
+    # https://github.com/Homebrew/homebrew/issues/45273
+    # https://github.com/Homebrew/homebrew/issues/44993
+    # This is thought to be an el_captian bug:
+    # http://lists.gnu.org/archive/html/bug-tar/2015-10/msg00017.html
+    if MacOS.version == :el_capitan
+      ENV["gl_cv_func_getcwd_abort_bug"] = "no"
+    end
+
     system "./bootstrap" if build.head?
     args = %W[
       --prefix=#{prefix}
