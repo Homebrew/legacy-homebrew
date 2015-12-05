b26c8045b7567d33cb4d20915f73b84f2e7d9f1c
Izaak Beekman <zbeekman@gmail.com>
2015-12-05 15:01:09 -0500

    cloog 0.18.4

--- Library/Formula/cloog.rb
+++ Library/Formula/cloog.rb
@@ -1,8 +1,8 @@
 class Cloog < Formula
   desc "Generate code for scanning Z-polyhedra"
   homepage "http://www.cloog.org/"
-  url "http://www.bastoul.net/cloog/pages/download/count.php3?url=./cloog-0.18.3.tar.gz"
-  sha256 "460c6c740acb8cdfbfbb387156b627cf731b3837605f2ec0001d079d89c69734"
+  url "http://www.bastoul.net/cloog/pages/download/count.php3?url=./cloog-0.18.4.tar.gz"
+  sha256 "325adf3710ce2229b7eeb9e84d3b539556d093ae860027185e7af8a8b00a750e"
 
   bottle do
     cellar :any
