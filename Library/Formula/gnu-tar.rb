9869fede2ddac73a824acd62072b88eedbf207af
chdiza <chdiza@gmail.com>
2015-12-05 13:53:10 -0500

    gnu-tar: Add workaround for unremovable dir tree problem

--- Library/Formula/gnu-tar.rb
+++ Library/Formula/gnu-tar.rb
@@ -23,6 +23,16 @@ class GnuTar < Formula
   end
 
   def install
+    # Work around unremovable, nested dirs bug that affects lots of
+    # GNU projects. See:
+    # https://github.com/Homebrew/homebrew/issues/45273
+    # https://github.com/Homebrew/homebrew/issues/44993
+    # This is thought to be an el_captian bug:
+    # http://lists.gnu.org/archive/html/bug-tar/2015-10/msg00017.html
+    if MacOS.version == :el_capitan
+      ENV["gl_cv_func_getcwd_abort_bug"] = "no"
+    end
+
     args = ["--prefix=#{prefix}", "--mandir=#{man}"]
     args << "--program-prefix=g" if build.without? "default-names"
 
