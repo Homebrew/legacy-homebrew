1aaca0005c5b60f97290ad8c543fd642622267f2
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-05 03:08:50 +0000

    mupdf-tools: update 1.8 bottle.
    Closes #46685.

--- Library/Formula/mupdf-tools.rb
+++ Library/Formula/mupdf-tools.rb
@@ -7,9 +7,10 @@ class MupdfTools < Formula
 
   bottle do
     cellar :any
-    sha256 "21ff024a9bd04efa139395d6e5a83384ea24ebe3be3f4fd30baa2c5317ba20ce" => :el_capitan
-    sha256 "337686c7beaa9aea353c270991110c8a15b7769125ef61945f16bd1d0cd53bd8" => :yosemite
-    sha256 "808814594e6b2ccfbf644f45b0676c6913cf33c16766a0b70ed62449b69fa2bf" => :mavericks
+    revision 1
+    sha256 "7ef90b7f58164276f38a6fa90324b352960c41bfc19b57b883386add52cf0148" => :el_capitan
+    sha256 "2d94022c4922f42bdc64576f5893750c5ee2e0c8c545f305d4a4ac5634073ed0" => :yosemite
+    sha256 "d442f5e49898025714b9664ea70304ca433ca07589e360c528915efc81298f34" => :mavericks
   end
 
   depends_on :macos => :snow_leopard
