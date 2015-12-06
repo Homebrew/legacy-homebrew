570018b2cffb2478f455719100775e9f13762c75
Leonid Komarovsky <shpoont@users.noreply.github.com>
2015-12-07 01:25:13 +0200

    bash-preexec 0.2.3

--- Library/Formula/bash-preexec.rb
+++ Library/Formula/bash-preexec.rb
@@ -1,8 +1,8 @@
 class BashPreexec < Formula
   desc "preexec and precmd functions for Bash just like Zsh."
   homepage "https://github.com/rcaloras/bash-preexec"
-  url "https://github.com/rcaloras/bash-preexec/archive/0.2.2.tar.gz"
-  sha256 "68e748308754437bad0f948cde8dd3a903ad53d919b4e0ce7b18e012895e6fa8"
+  url "https://github.com/rcaloras/bash-preexec/archive/0.2.3.tar.gz"
+  sha256 "33fad9f4e024d4d23972ca656406bfdacecd5a2fcdf290d81bb56c14a5c8d094"
 
   head "https://github.com/rcaloras/bash-preexec.git"
 
