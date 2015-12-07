49e7d4d3c1bf7f53efc2f524ed9906c7355a2e09
Simon <simonrohrer@yahoo.co.uk>
2015-12-07 17:48:27 +0000

    mongodb: add support for SASL client.
    Closes #46765.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/mongodb.rb
+++ Library/Formula/mongodb.rb
@@ -84,6 +84,7 @@ class Mongodb < Formula
       args << "CXX=#{ENV.cxx}"
     end
 
+    args << "--use-sasl-client" if build.with? "sasl"
     args << "--use-system-boost" if build.with? "boost"
     args << "--use-new-tools"
     args << "--disable-warnings-as-errors" if MacOS.version >= :yosemite
