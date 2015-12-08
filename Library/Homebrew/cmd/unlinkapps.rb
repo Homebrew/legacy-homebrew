8f73c226dc66362899352cf7495d903c0a108bdc
Martin Afanasjew <martin@afanasjew.de>
2015-12-08 09:11:33 +0100

    prune: handle broken app symlinks
    Remove broken symlinks from `/Applications` and `~/Applications` that
    were previously created by `brew linkapps`, but are no longer valid
    because formulae were uninstalled or the provided apps have changed.

--- Library/Homebrew/cmd/unlinkapps.rb
+++ Library/Homebrew/cmd/unlinkapps.rb
@@ -9,22 +9,30 @@ module Homebrew
 
   private
 
+  def unlinkapps_prune(opts = {})
+    opts = opts.merge(:prune => true)
+    unlinkapps_from_dir(linkapps_target(:local => false), opts)
+    unlinkapps_from_dir(linkapps_target(:local => true), opts)
+  end
+
   def unlinkapps_from_dir(target_dir, opts = {})
     return unless target_dir.directory?
     dry_run = opts.fetch(:dry_run, false)
+    quiet = opts.fetch(:quiet, false)
 
     apps = Pathname.glob("#{target_dir}/*.app").select do |app|
-      unlinkapps_unlink?(app)
+      unlinkapps_unlink?(app, opts)
     end
 
     ObserverPathnameExtension.reset_counts!
 
+    app_kind = opts.fetch(:prune, false) ? " (broken link)" : ""
     apps.each do |app|
       app.extend(ObserverPathnameExtension)
       if dry_run
-        puts "Would unlink: #{app}"
+        puts "Would unlink#{app_kind}: #{app}"
       else
-        puts "Unlinking: #{app}"
+        puts "Unlinking#{app_kind}: #{app}" unless quiet
         app.unlink
       end
     end
@@ -44,12 +52,14 @@ module Homebrew
     #{HOMEBREW_PREFIX}/opt/
   ].freeze
 
-  def unlinkapps_unlink?(target_app)
+  def unlinkapps_unlink?(target_app, opts = {})
     # Skip non-symlinks and symlinks that don't point into the Homebrew prefix.
     app = "#{target_app.readlink}" if target_app.symlink?
     return false unless app && app.start_with?(*UNLINKAPPS_PREFIXES)
 
-    if ARGV.named.empty?
+    if opts.fetch(:prune, false)
+      !File.exist?(app) # Remove only broken symlinks in prune mode.
+    elsif ARGV.named.empty?
       true
     else
       ARGV.kegs.any? { |keg| app.start_with?("#{keg}/", "#{keg.opt_record}/") }
--- Library/Homebrew/manpages/brew.1.md
+++ Library/Homebrew/manpages/brew.1.md
@@ -340,7 +340,9 @@ With `--verbose` or `-v`, many commands print extra debugging information. Note 
 
   * `prune` [`--dry-run`]:
     Remove dead symlinks from the Homebrew prefix. This is generally not
-    needed, but can be useful when doing DIY installations.
+    needed, but can be useful when doing DIY installations. Also remove broken
+    app symlinks from `/Applications` and `~/Applications` that were previously
+    created by `brew linkapps`.
 
     If `--dry-run` or `-n` is passed, show what would be removed, but do not
     actually remove anything.
