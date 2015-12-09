f7d989170268acc84fdf0e5ca9b602b850baa7dc
Jacob Salmela <jacobsalmela@users.noreply.github.com>
2015-12-08 23:02:18 -0600

    ncutil 2.4

---
+++ Library/Formula/ncutil.rb
@@ -0,0 +1,15 @@
+class Ncutil < Formula
+  desc "Modify OS X's Notification Center: Add/remove apps, get/set alert styles, suppress notifications, etc."
+  homepage "https://github.com/jacobsalmela/NCutil"
+  url "https://github.com/jacobsalmela/NCutil/archive/2.4.tar.gz"
+  version "2.4"
+  sha256 "ebea1fe1163e401dd4e7833416001dbf9d60614237700755b55b46d0abf423bc"
+
+  def install
+	bin.install "NCutil.py" => "ncutil"
+  end
+
+  test do
+	system "#{bin}/ncutil", "--help"
+  end
+end
