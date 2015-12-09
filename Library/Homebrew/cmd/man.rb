847b964ee4b69988732fc8092fd0164509ab8ae8
Martin Afanasjew <martin@afanasjew.de>
2015-12-09 05:40:26 +0100

    man: remove broken --verbose functionality

--- Library/Homebrew/cmd/man.rb
+++ Library/Homebrew/cmd/man.rb
@@ -25,15 +25,12 @@ module Homebrew
       puts "Writing HTML fragments to #{DOC_PATH}"
       puts "Writing manpages to #{TARGET_PATH}"
 
-      target_file = nil
       Dir["#{SOURCE_PATH}/*.md"].each do |source_file|
         target_html = DOC_PATH/"#{File.basename(source_file, ".md")}.html"
         safe_system "ronn --fragment --pipe --organization='Homebrew' --manual='brew' #{source_file} > #{target_html}"
         target_man = TARGET_PATH/File.basename(source_file, ".md")
         safe_system "ronn --roff --pipe --organization='Homebrew' --manual='brew' #{source_file} > #{target_man}"
       end
-
-      system "man", target_file if ARGV.flag? "--verbose"
     end
   end
 end
