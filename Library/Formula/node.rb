8e48fd8889ad04dc6f9327b61c11f98088484271
Aayush Kapoor <xeoneux@icloud.com>
2015-12-09 15:54:07 +0000

    node 5.2.0
    Closes #46838.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Platform built on the V8 JavaScript runtime to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v5.1.1/node-v5.1.1.tar.gz"
-  sha256 "a779e024f800b5ec51f375fa1c14eda7254216daa36a1960cc1e4195b9fc22c3"
+  url "https://nodejs.org/dist/v5.2.0/node-v5.2.0.tar.gz"
+  sha256 "5df5682f9fdd8e747f652e0b09fed46478a1e3df159797852787a074b8787664"
   head "https://github.com/nodejs/node.git"
 
   bottle do
