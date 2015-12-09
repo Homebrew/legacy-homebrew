394090eaeb7da5673e25c5bd151330895ccc38c0
Jacob Salmela <jacobsalmela@users.noreply.github.com>
2015-12-09 12:27:46 -0600

    remove version and run a better test

--- Library/Formula/ncutil.rb
+++ Library/Formula/ncutil.rb
@@ -2,7 +2,6 @@ class Ncutil < Formula
   desc "Modify OS X's Notification Center from the command-line"
   homepage "https://github.com/jacobsalmela/NCutil"
   url "https://github.com/jacobsalmela/NCutil/archive/2.4.tar.gz"
-  version "2.4"
   sha256 "ebea1fe1163e401dd4e7833416001dbf9d60614237700755b55b46d0abf423bc"
 
   def install
@@ -10,6 +9,6 @@ class Ncutil < Formula
   end
 
   test do
-    system "#{bin}/ncutil", "--help"
+    system "#{bin}/ncutil", "--list"
   end
 end
