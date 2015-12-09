6f37b050e3408acf3f3682a871f0c617364514a3
Jacob Salmela <jacobsalmela@users.noreply.github.com>
2015-12-09 11:06:56 -0600

    spaces instead of tab

--- Library/Formula/ncutil.rb
+++ Library/Formula/ncutil.rb
@@ -6,10 +6,10 @@ class Ncutil < Formula
   sha256 "ebea1fe1163e401dd4e7833416001dbf9d60614237700755b55b46d0abf423bc"
 
   def install
-	bin.install "NCutil.py" => "ncutil"
+    bin.install "NCutil.py" => "ncutil"
   end
 
   test do
-	system "#{bin}/ncutil", "--help"
+    system "#{bin}/ncutil", "--help"
   end
 end
