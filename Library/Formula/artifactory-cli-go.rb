25ea80f3c375c77b510290d0e6dbf23ef5fd9c4a
Jainish Shah <Jainishshah@yahoo.com>
2015-12-10 15:05:10 -0800

    Update artifactory-cli-go.rb

--- Library/Formula/artifactory-cli-go.rb
+++ Library/Formula/artifactory-cli-go.rb
@@ -1,13 +1,25 @@
 class ArtifactoryCliGo < Formula
-    desc "Artifactory CLI provides a command line interface for uploading and downloading artifacts to and from Artifactory."
-    homepage "https://github.com/JFrogDev/artifactory-cli-go"
-    url "https://bintray.com/artifact/download/jfrog/artifactory-cli-go/1.2.0/artifactory-cli-mac-386/art"
-    sha256 "79f3084291a4d54cfbf72dc1bbd16e14097077aa52897ed9b4e6fcf448758776"
-    def install
-        bin.install "art"
-        bin.install_symlink libexec/"bin/art"
-    end
-    test do
-        system bin/"art", "--version"
-    end
+  desc "Artifactory CLI provides a command line interface for uploading and downloading artifacts to and from Artifactory."
+  homepage "https://www.jfrog.com/"
+  url "https://github.com/JFrogDev/artifactory-cli-go/archive/1.2.0.tar.gz"
+  sha256 "173a24e0b821c6ff2bb85fa4cbe733fa681a8284029b77387baf0fa5f2fcfc4e"
+
+  depends_on "go"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GOBIN"] = buildpath
+    ENV["GO15VENDOREXPERIMENT"] = "1"
+    (buildpath/"src/github.com/JFrogDev/artifactory-cli-go/").install Dir["*"]
+    system "go", "build", "-v", "github.com/JFrogDev/artifactory-cli-go/art/"
+    bin.install "art"
+  end
+
+  test do
+    system "#{bin}/art", "-v"
+    actual = pipe_output("#{bin}/art upload")
+    expected = "Wrong number of arguments. Try 'art upload --help'.\n"
+    assert_equal expected, actual
+  end
+
 end
