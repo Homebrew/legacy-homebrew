de4be1c21debc6184dbb24187092de1dd16f593a
Mike McQuaid <mike@mikemcquaid.com>
2015-12-11 15:13:20 +0000

    amdatu-bootstrap: bottle unneeded and URL comment.

--- Library/Formula/amdatu-bootstrap.rb
+++ Library/Formula/amdatu-bootstrap.rb
@@ -1,8 +1,11 @@
 class AmdatuBootstrap < Formula
   desc "Bootstrapping OSGi development"
   homepage "http://amdatu.org/bootstrap/intro.html"
+  # The next release should not use a zip with "brew" in the name.
   url "https://bitbucket.org/amdatu/amdatu-bootstrap/downloads/bootstrap-r8-bin-brew.zip"
   sha256 "5c89667e10b57060c67fc54188ded3b0b281f88fab062bfb4290994e8df2295a"
+
+  bottle :unneeded
 
   def install
     libexec.install %w[amdatu-bootstrap bootstrap.jar conf]
