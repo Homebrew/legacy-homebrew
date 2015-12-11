bac35e543aaadbb4bb6b6aac1674642418d23ceb
Mike McQuaid <mike@mikemcquaid.com>
2015-12-11 15:13:05 +0000

    testssl: bottle unneeded.

--- Library/Formula/testssl.rb
+++ Library/Formula/testssl.rb
@@ -4,6 +4,8 @@ class Testssl < Formula
   url "https://github.com/drwetter/testssl.sh/archive/v2.6.tar.gz"
   sha256 "286b3285f096a5d249de1507eee88b14848514696bc5bbc4faceffa46b563ebd"
 
+  bottle :unneeded
+
   depends_on "openssl"
 
   def install
