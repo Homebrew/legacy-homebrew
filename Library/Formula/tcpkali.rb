f0fa62a3c3c5447e0d9e214d5e18d8ad19f71ce3
Lev Walkin <vlm@lionet.info>
2015-12-11 04:28:18 -0800

    tcpkali 0.7 (new formula)
    tcpkali is a high performance TCP and WebSocket protocol generator and
    sink, allowing to create millions of connections from a single host,
    and saturate most kinds of physical networks.

---
+++ Library/Formula/tcpkali.rb
@@ -0,0 +1,18 @@
+class Tcpkali < Formula
+  desc "High performance TCP and WebSocket load generator and sink"
+  homepage "https://github.com/machinezone/tcpkali"
+  url "https://github.com/machinezone/tcpkali/releases/download/v0.7/tcpkali-0.7.tar.gz"
+  sha256 "c917e2384b171d4c8906192266376cab6b1f9191810a4f209d7371639e3f2185"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/tcpkali", "127.1:1237", "-l1237", "-T0.1"
+  end
+end
