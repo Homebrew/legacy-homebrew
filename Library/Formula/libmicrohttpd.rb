d0143dbebe6754281990f0821f8190c14bb49871
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-12-11 23:48:14 +0700

    libmicrohttpd 0.9.47

--- Library/Formula/libmicrohttpd.rb
+++ Library/Formula/libmicrohttpd.rb
@@ -1,9 +1,10 @@
 class Libmicrohttpd < Formula
   desc "Light HTTP/1.1 server library"
   homepage "https://www.gnu.org/software/libmicrohttpd/"
-  url "http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.42.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.42.tar.gz"
-  sha256 "00352073367003da40607319e4090a6a8308748b59246ae80e9871f34dad7d5b"
+  url "http://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.47.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.47.tar.gz"
+  sha256 "96bdab4352a09fd3952a346bc01898536992f50127d0adea1c3096a8ec9f658c"
+  revision 1
 
   bottle do
     cellar :any
