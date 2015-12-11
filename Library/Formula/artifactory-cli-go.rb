cedb4248b82790b5b0922db3d1a08ce9d4d271bc
Jainish Shah <Jainishshah@yahoo.com>
2015-12-11 10:34:52 -0800

    Update artifactory-cli-go.rb

--- Library/Formula/artifactory-cli-go.rb
+++ Library/Formula/artifactory-cli-go.rb
@@ -1,9 +1,11 @@
 class ArtifactoryCliGo < Formula
-  desc "Provides a command-line interface."
-  homepage "https://www.jfrog.com"
+  desc "Artifactory-CLI provides a command-line interface."
+  homepage "https://github.com/JFrogDev/artifactory-cli-go"
   url "https://github.com/JFrogDev/artifactory-cli-go/archive/1.2.0.tar.gz"
   sha256 "173a24e0b821c6ff2bb85fa4cbe733fa681a8284029b77387baf0fa5f2fcfc4e"
+
   depends_on "go"
+
   def install
     ENV["GOPATH"] = buildpath
     ENV["GOBIN"] = buildpath
@@ -12,7 +14,10 @@ class ArtifactoryCliGo < Formula
     system "go", "build", "-v", "github.com/JFrogDev/artifactory-cli-go/art/"
     bin.install "art"
   end
+
   test do
-    system "#{bin}/art", "-v"
+    actual = pipe_output("#{bin}/art upload")
+    expected = "The --url option is mandatory\n"
+    assert_equal expected, actual
   end
 end
