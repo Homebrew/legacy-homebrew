8e8b382a5c1fac51d0373cab5f873c5890f0d710
Damjan Znidarsic <damjan.znidarsic@dlabs.si>
2015-12-14 00:17:09 +0100

    consul-template 0.12.0
    Consul Template 0.12.0
    
    Closes #46977.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/consul-template.rb
+++ Library/Formula/consul-template.rb
@@ -4,8 +4,8 @@ class ConsulTemplate < Formula
   desc "Generic template rendering and notifications with Consul"
   homepage "https://github.com/hashicorp/consul-template"
   url "https://github.com/hashicorp/consul-template.git",
-      :tag => "v0.11.1",
-      :revision => "134aecc73e607f09986370600232ecb8f44d0940"
+      :tag => "v0.12.0",
+      :revision => "fb5915763cb0f8ecb3bff6f04aa71a6c2e6abaa3"
 
   head "https://github.com/hashicorp/consul-template.git"
 
@@ -20,72 +20,82 @@ class ConsulTemplate < Formula
 
   go_resource "github.com/mitchellh/gox" do
     url "https://github.com/mitchellh/gox.git",
-        :revision => "ef1967b9f538fe467e6a82fc42ec5dff966ad4ea"
-  end
-
-  go_resource "github.com/mitchellh/iochan" do
-    url "https://github.com/mitchellh/iochan.git",
-        :revision => "87b45ffd0e9581375c491fef3d32130bb15c5bd7"
+      :revision => "54b619477e8932bbb6314644c867e7e6db7a9c71"
   end
 
   go_resource "github.com/fatih/structs" do
     url "https://github.com/fatih/structs.git",
-        :revision => "c701457aaa1ff6709d5e35fccb5c129448e0a37b"
+      :revision => "9a7733345ff091c5457cb963f498a79ecd0bdbaa"
   end
 
   go_resource "github.com/hashicorp/consul" do
     url "https://github.com/hashicorp/consul.git",
-        :revision => "ab63122a320928b290d681aaac1145b19a69cc0d"
+      :revision => "94f33972bdc1574738b00290b5b9ae1df445435e"
   end
 
   go_resource "github.com/hashicorp/errwrap" do
     url "https://github.com/hashicorp/errwrap.git",
-        :revision => "7554cd9344cec97297fa6649b055a8c98c2a1e55"
+      :revision => "7554cd9344cec97297fa6649b055a8c98c2a1e55"
   end
 
   go_resource "github.com/hashicorp/go-cleanhttp" do
     url "https://github.com/hashicorp/go-cleanhttp.git",
-        :revision => "5df5ddc69534f1a4697289f1dca2193fbb40213f"
+      :revision => "5df5ddc69534f1a4697289f1dca2193fbb40213f"
   end
 
   go_resource "github.com/hashicorp/go-multierror" do
     url "https://github.com/hashicorp/go-multierror.git",
-        :revision => "d30f09973e19c1dfcd120b2d9c4f168e68d6b5d5"
+      :revision => "d30f09973e19c1dfcd120b2d9c4f168e68d6b5d5"
+  end
+
+  go_resource "github.com/hashicorp/go-reap" do
+    url "https://github.com/hashicorp/go-reap.git",
+      :revision => "e300e334a8de28a7931fc05331345f9e985128de"
   end
 
   go_resource "github.com/hashicorp/go-syslog" do
     url "https://github.com/hashicorp/go-syslog.git",
-        :revision => "42a2b573b664dbf281bd48c3cc12c086b17a39ba"
+      :revision => "42a2b573b664dbf281bd48c3cc12c086b17a39ba"
   end
 
   go_resource "github.com/hashicorp/hcl" do
     url "https://github.com/hashicorp/hcl.git",
-        :revision => "1688f22977e3b0bbdf1aaa5e2528cf10c2e93e78"
+      :revision => "17c5c97b5a51e82c238678127e413b7635c6d5f6"
   end
 
   go_resource "github.com/hashicorp/logutils" do
     url "https://github.com/hashicorp/logutils.git",
-        :revision => "0dc08b1671f34c4250ce212759ebd880f743d883"
+      :revision => "0dc08b1671f34c4250ce212759ebd880f743d883"
   end
 
   go_resource "github.com/hashicorp/serf" do
     url "https://github.com/hashicorp/serf.git",
-        :revision => "a72c0453da2ba628a013e98bf323a76be4aa1443"
+      :revision => "e9ac4bb0c5721826d5cd3d1cd582c12fbc664bb8"
   end
 
   go_resource "github.com/hashicorp/vault" do
     url "https://github.com/hashicorp/vault.git",
-        :revision => "93f196fd75d0a13d1489c02bf05d24d2d6168466"
+      :revision => "2c96d1fde30cf5c1cec23d525d6a52a3f37c29e5"
   end
 
   go_resource "github.com/mitchellh/mapstructure" do
     url "https://github.com/mitchellh/mapstructure.git",
-        :revision => "281073eb9eb092240d33ef253c404f1cca550309"
+      :revision => "281073eb9eb092240d33ef253c404f1cca550309"
+  end
+
+  go_resource "golang.org/x/sys" do
+    url "https://go.googlesource.com/sys.git",
+      :revision => "833a04a10549a95dc34458c195cbad61bbb6cb4d"
   end
 
   go_resource "gopkg.in/yaml.v2" do
     url "https://gopkg.in/yaml.v2.git",
-        :revision => "53feefa2559fb8dfa8d81baad31be332c97d6c77"
+      :revision => "f7716cbe52baa25d2e9b0d0da546fcf909fc16b4"
+  end
+
+  go_resource "github.com/mitchellh/iochan" do
+    url "https://github.com/mitchellh/iochan.git",
+      :revision => "87b45ffd0e9581375c491fef3d32130bb15c5bd7"
   end
 
   def install
@@ -95,18 +105,22 @@ class ConsulTemplate < Formula
     # For the gox buildtool used by consul-template, which doesn't need to
     # get installed permanently
     ENV.append_path "PATH", buildpath
+    contents = Dir["{*,.git,.gitignore}"]
+    gopath = buildpath/"gopath"
+    (gopath/"src/github.com/hashicorp/consul-template").install contents
 
-    ctpath = buildpath/"src/github.com/hashicorp/consul-template"
-    ctpath.install Dir["{*,.git}"]
-    Language::Go.stage_deps resources, buildpath/"src"
+    ENV["GOPATH"] = gopath
+    ENV.prepend_create_path "PATH", gopath/"bin"
+
+    Language::Go.stage_deps resources, gopath/"src"
     mkdir_p buildpath/"bin"
 
-    cd "src/github.com/mitchellh/gox" do
+    cd gopath/"src/github.com/mitchellh/gox" do
       system "go", "build"
       buildpath.install "gox"
     end
 
-    cd "src/github.com/hashicorp/consul-template" do
+    cd gopath/"src/github.com/hashicorp/consul-template" do
       system "gox",
              "-os=darwin",
              "-arch=#{arch}",
