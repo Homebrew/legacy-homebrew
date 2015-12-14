7cb54aa77d3a6c499504c6d061a35dc3339b64ca
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-14 07:28:07 +0000

    libcroco: update 0.6.10 bottle.

--- Library/Formula/libcroco.rb
+++ Library/Formula/libcroco.rb
@@ -6,9 +6,9 @@ class Libcroco < Formula
 
   bottle do
     cellar :any
-    sha256 "a3473ef024c5223325a1de3240d68182f4137f573314d68a792b71bd60573a3a" => :el_capitan
-    sha256 "8799bb46166eedd4a65a54b397bd1acf08a423ae3c05a50bcedbde300c43ed72" => :yosemite
-    sha256 "cfce473c43e9c832f594d6a5f3ebe4654296fe71ccf8f217aa6fd020d84af2b6" => :mavericks
+    sha256 "fd0eadca9804cfbfd338b95b365d28080a80a7ed3bf3421595fdb0717cf80131" => :el_capitan
+    sha256 "a3f6a8e4d231a33ff0081cc7e967510e9805c61b9e4717fa144add6e2640b672" => :yosemite
+    sha256 "8b9ea38c7c54700e5148dea73a2ae118b473083a01bcaa2a19ab2ef98a8184e2" => :mavericks
   end
 
   depends_on "pkg-config" => :build
