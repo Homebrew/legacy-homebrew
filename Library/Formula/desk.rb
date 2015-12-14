6490910f7284caa8f2f16d20e2e24836b29bda98
Christian Michael Paul Bankester <cbankester@immense.net>
2015-12-14 14:41:34 -0600

    desk 0.4.0

--- Library/Formula/desk.rb
+++ Library/Formula/desk.rb
@@ -1,15 +1,15 @@
 class Desk < Formula
   desc "Lightweight workspace manager for the shell"
   homepage "https://github.com/jamesob/desk"
-  url "https://github.com/jamesob/desk/archive/v0.3.1.tar.gz"
-  sha256 "b687e2cfa742f763d689391f67a5b5225324e282a0fed100487b1570988d7758"
+  url "https://github.com/jamesob/desk/archive/v0.4.0.tar.gz"
+  sha256 "1589cb02d1104b4008d9d1a1ee1a315d0bf5a8e8d6f8408d9f60201a4f04c896"
 
   bottle :unneeded
 
   def install
     bin.install "desk"
-    bash_completion.install "completions/bash/desk"
-    zsh_completion.install "completions/zsh/_desk"
+    bash_completion.install "shell_plugins/bash/desk"
+    zsh_completion.install "shell_plugins/zsh/_desk"
   end
 
   test do
