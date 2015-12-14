f1dfeeaec97b6dca346558493dca9653d79e2fa0
heinzf <heinz.fiedler@gmail.com>
2015-12-13 23:09:32 -0500

    stow 2.2.2
    Stow 2.2.2
    
    Closes #46986.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/stow.rb
+++ Library/Formula/stow.rb
@@ -1,9 +1,9 @@
 class Stow < Formula
   desc "Organize software neatly under a single directory tree (e.g. /usr/local)"
   homepage "https://www.gnu.org/software/stow/"
-  url "http://ftpmirror.gnu.org/stow/stow-2.2.0.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/stow/stow-2.2.0.tar.gz"
-  sha256 "8b89d79939cf9ae87d2f223bb36a3b2d0c66775b62aeb9953c6d33dab40d3c2b"
+  url "http://ftpmirror.gnu.org/stow/stow-2.2.2.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/stow/stow-2.2.2.tar.gz"
+  sha256 "e2f77649301b215b9adbc2f074523bedebad366812690b9dc94457af5cf273df"
 
   bottle do
     cellar :any_skip_relocation
