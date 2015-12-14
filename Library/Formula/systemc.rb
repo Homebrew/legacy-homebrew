6733510da287031d9a4a4dc9d55aeef6cacff8f7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-14 10:19:02 +0000

    systemc: add 2.3.1 bottle.

--- Library/Formula/systemc.rb
+++ Library/Formula/systemc.rb
@@ -4,6 +4,13 @@ class Systemc < Formula
   url "http://accellera.org/images/downloads/standards/systemc/systemc-2.3.1.tgz"
   sha256 "7ce0f68fd4759e746a9808936b54e62d498f5b583e83fc47758ca86917b4f800"
 
+  bottle do
+    cellar :any
+    sha256 "3228efad334a8e66478c85245205e3b2d9f45fd227da07ac35aa8a9cca43565e" => :el_capitan
+    sha256 "6f97dab7564f10fe17f43289576e4ae8a2c748bbb907538d04e79e0640744ac6" => :yosemite
+    sha256 "a63276361d0399c899b61d068bf3fe92aab9882b218b749a6520920ae4c5e56b" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
