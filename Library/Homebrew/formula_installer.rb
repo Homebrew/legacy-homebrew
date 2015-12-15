1df4379bd9a62ac568183aad457a19c43edff36f
Martin Afanasjew <martin@afanasjew.de>
2015-12-15 05:36:05 +0100

    formula_installer: prefer 'Dependable#run?'
    Both `req.tags.include?(:run)` and `req.run?` are currently equivalent,
    but the latter relies less on implementation details.

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -286,7 +286,7 @@ class FormulaInstaller
   def install_requirement_default_formula?(req, dependent, build)
     return false unless req.default_formula?
     return true unless req.satisfied?
-    return false if req.tags.include?(:run)
+    return false if req.run?
     install_bottle_for?(dependent, build) || build_bottle?
   end
 
