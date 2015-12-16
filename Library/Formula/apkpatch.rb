4139b3cd48d4d7fc41ad871ae4ebdf43d50b7bbd
WuNan <geek5nan@gmail.com>
2015-12-17 00:11:21 +0800

    1.update desc 2.remove comments

--- Library/Formula/apkpatch.rb
+++ Library/Formula/apkpatch.rb
@@ -1,14 +1,12 @@
 class Apkpatch < Formula
-  desc "a tool for build/merge Android Patch file(.apatch)"
+  desc "Tool for build/merge Android Patch file(.apatch)"
   homepage "https://github.com/alibaba/AndFix"
   url "https://raw.githubusercontent.com/alibaba/AndFix/master/tools/apkpatch-1.0.3.zip"
   sha256 "0f94cd2a675af89ff3990f4b6490dd70877ad4eb109f0cdbefece1f9a73e4512"
 
   def install
-    # Remove Windows scripts
     rm_rf Dir["*.bat"]
 
-    # Install files
     chmod 0755, Dir["*"]
     libexec.install Dir["*"]
 
@@ -18,6 +16,6 @@ class Apkpatch < Formula
   end
 
   test do
-    system bin/"apkpatch"
+    system "#{bin}/apkpatch", "help"
   end
 end
