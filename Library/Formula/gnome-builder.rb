be46871f299f4b37fecc128d6ca50c6234278db3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-16 14:16:49 +0000

    gnome-builder: add 3.18.1 bottle.

--- Library/Formula/gnome-builder.rb
+++ Library/Formula/gnome-builder.rb
@@ -4,6 +4,12 @@ class GnomeBuilder < Formula
   url "https://download.gnome.org/sources/gnome-builder/3.18/gnome-builder-3.18.1.tar.xz"
   sha256 "501c95220dcf8ca44a5748e863492377fe2c3aee78a95973d6819b1836e5407c"
 
+  bottle do
+    sha256 "f7d422a29b7c9b98c20bdce9e8a56b14265fad5d9ce174d77c686adb26d752ad" => :el_capitan
+    sha256 "294e0df51b9781d53996761965bde4a300485e92af2f39f79965cbee5203f4e5" => :yosemite
+    sha256 "0a7a3ea11c1db7a79faeb78240631b0ea55d59cc2b053a4f89e95769d1f1c26c" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "libgit2-glib"
