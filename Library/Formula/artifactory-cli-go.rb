93dd4d30e88e8bec42061cbb32573bb17b231b39
Jainish Shah <Jainishshah@yahoo.com>
2015-12-16 12:17:12 -0800

    Update artifactory-cli-go.rb

--- Library/Formula/artifactory-cli-go.rb
+++ Library/Formula/artifactory-cli-go.rb
@@ -1,5 +1,5 @@
 class ArtifactoryCliGo < Formula
-  desc "Provides a command-line interface."
+  desc "Provides a command-line interface for Artifactory."
   homepage "https://github.com/JFrogDev/artifactory-cli-go"
   url "https://github.com/JFrogDev/artifactory-cli-go/archive/1.2.0.tar.gz"
   sha256 "173a24e0b821c6ff2bb85fa4cbe733fa681a8284029b77387baf0fa5f2fcfc4e"
