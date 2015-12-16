443470a86df1abe4a152296b6d22802099b168ff
WuNan <me@devwu.com>
2015-12-16 21:04:11 +0800

    Update apkpatch formula

--- Library/Formula/apkpatch.rb
+++ Library/Formula/apkpatch.rb
@@ -1,5 +1,5 @@
 class Apkpatch < Formula
-  desc "AndFix provides a patch-making tool called apkpatch."
+  desc "a tool for build/merge Android Patch file(.apatch)"
   homepage "https://github.com/alibaba/AndFix"
   url "https://raw.githubusercontent.com/alibaba/AndFix/master/tools/apkpatch-1.0.3.zip"
   sha256 "0f94cd2a675af89ff3990f4b6490dd70877ad4eb109f0cdbefece1f9a73e4512"
@@ -9,7 +9,6 @@ class Apkpatch < Formula
     rm_rf Dir["*.bat"]
 
     # Install files
-    prefix.install_metafiles
     chmod 0755, Dir["*"]
     libexec.install Dir["*"]
 
