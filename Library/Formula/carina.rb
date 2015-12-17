143ea7ee54d7a0522e5f156f0ee81a255ea0520d
Kyle Kelley <rgbkrk@gmail.com>
2015-12-17 12:38:15 -0600

    carina 1.0.1

--- Library/Formula/carina.rb
+++ Library/Formula/carina.rb
@@ -2,8 +2,8 @@ class Carina < Formula
   desc "Work with Swarm clusters on Carina"
   homepage "https://github.com/getcarina/carina"
   url "https://github.com/getcarina/carina.git",
-        :tag => "v1.0.0",
-        :revision => "2d3d3e6bca49e977102ca89fc314537805d1f31b"
+        :tag => "v1.0.1",
+        :revision => "f36e13e5bbd0a06484fb26ee0cc2f3a708cc6615"
   head "https://github.com/getcarina/carina.git"
 
   bottle do
