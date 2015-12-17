c105b48503fc94de4a55743ec3fac5256917e4d1
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-17 11:12:40 +0100

    arping 2.16

--- Library/Formula/arping.rb
+++ Library/Formula/arping.rb
@@ -1,8 +1,8 @@
 class Arping < Formula
   desc "Utility to check whether MAC addresses are already taken on a LAN"
   homepage "https://github.com/ThomasHabets/arping"
-  url "https://github.com/ThomasHabets/arping/archive/arping-2.15.tar.gz"
-  sha256 "c6be96cff28cda27af89ecd60fc8466d2f8183208cd805a1aa40d8f190cdd2b6"
+  url "https://github.com/ThomasHabets/arping/archive/arping-2.16.tar.gz"
+  sha256 "6fd444250dd8ab9eca7d14f32fb1072d5a0c6e4a4f0846c87bfbc99f93d2d648"
 
   bottle do
     cellar :any
