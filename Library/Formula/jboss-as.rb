7ddb1e62b25be1fb19c7630705e00bb2f7492eb8
Viktor Szakats <vszakats@users.noreply.github.com>
2015-12-17 14:39:24 +0100

    jboss-as: use https homepage

--- Library/Formula/jboss-as.rb
+++ Library/Formula/jboss-as.rb
@@ -1,6 +1,6 @@
 class JbossAs < Formula
   desc "JBoss Application Server"
-  homepage "http://www.jboss.org/jbossas"
+  homepage "https://jbossas.jboss.org/"
   url "https://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.tar.gz"
   version "7.1.1.Final"
   sha256 "88fd3fdac4f7951cee3396eff3d70e8166c3319de82d77374a24e3b422e0b2ad"
