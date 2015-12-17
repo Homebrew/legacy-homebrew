58e9c321dfef4fde23f2136a94717f348e24f4fe
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-17 16:40:06 +0000

    rdup: add 1.1.14_1 bottle.

--- Library/Formula/rdup.rb
+++ Library/Formula/rdup.rb
@@ -5,6 +5,13 @@ class Rdup < Formula
   sha256 "b25e2b0656d2e6a9cb97a37f493913c4156468d4c21cea15a9a0c7b353e3742a"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "30ae4e73e58b80d081378978a93962ef798eb32ecc5b14eb015f707c766ee161" => :el_capitan
+    sha256 "a61678fecdd52fef40014327b604d03a96b506c7552ed2e2b8d22934d37cdbbb" => :yosemite
+    sha256 "979a3ef9ddaad9545d9cc9f943aef2fa0c7d69fabf05abd742740620239a8271" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "nettle"
   depends_on "pcre"
