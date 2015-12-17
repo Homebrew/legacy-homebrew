d4b990e39a2ba9188a82e4f8f81c4ea5fdaa963f
Viktor Szakats <vszakats@users.noreply.github.com>
2015-12-17 14:39:23 +0100

    wwwoffle: use https homepage

--- Library/Formula/wwwoffle.rb
+++ Library/Formula/wwwoffle.rb
@@ -1,6 +1,6 @@
 class Wwwoffle < Formula
   desc "Better browsing for computers with intermittent connections"
-  homepage "http://www.gedanken.org.uk/software/wwwoffle/"
+  homepage "https://www.gedanken.org.uk/software/wwwoffle/"
   url "https://www.gedanken.org.uk/software/wwwoffle/download/wwwoffle-2.9i.tgz"
   sha256 "e6341a4ec2631dc22fd5209c7e5ffe628a5832ad191d444c56aebc3837eed2ae"
 
