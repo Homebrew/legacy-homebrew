24c0412c06e05cae00184ec3e2067f3d00fd55e7
Vlad Shablinsky <vladshablinsky@gmail.com>
2015-12-18 17:04:01 +0300

    formula_resolver: add raw methods

--- Library/Homebrew/formula_resolver.rb
+++ Library/Homebrew/formula_resolver.rb
@@ -3,9 +3,16 @@ require "pathname"
 # Return actual name of some formula at commit commit
 
 class FormulaResolver
+  # name of the formula to be resolved
+  attr_reader :formula_name
+
+  # formula renames hashes for resolving current name of the formula
   attr_reader :sheets
+
   @sheets = {}
 
+  # {Entry} is used to store one entry in from renames file
+  # entry in file is a string `newname, commit`
   class Entry
     include Comparable
 
@@ -27,8 +34,18 @@ class FormulaResolver
     end
   end
 
+  # {Sheet} is a class for storing formula_renams
+  # entries grouped be one renames file
+  # entry_after and name_after find the nearest rename after given entry
   class Sheet
+    # name of the for renames of formula with that name
+    attr_reader :name
+
+    # entries from renames file
     attr_reader :entries
+
+    # last searched index in the sheet
+    # TODO remove or implement
     attr_reader :last_sarched_index
 
     def initialize(name)
@@ -39,11 +56,13 @@ class FormulaResolver
       end
     end
 
+    # get the first entry after another entry
     def entry_after(other)
       # TODO change linear search to binary
       entries.detect { |e| e > other }
     end
 
+    # get the first name after given entry
     def name_after(other)
       entry_after(other).name
     end
@@ -56,4 +75,35 @@ class FormulaResolver
     end
     result_entry.name
   end
+
+  def initialize(formula_name)
+    @formula_name = formula_name
+    @sheets[formula_name] = Sheet.new(formula_name)
+  end
+
+  def resolved_name
+    previous_entry = Entry.new(formula_name, get_installed_commit)
+    while (current_entry = sheets[previous_name].entry_after(previous_entry))
+      previous_entry = current_entry
+      sheets[pervious_entry] ||= Sheet.new(previous_entry.name)
+    end
+    previous_entry.name
+  end
+
+  # get the commit of installed formula with that name, which will be stored
+  # in INSTALLED_RECEIPT or some othe file
+  # `git rev-list -1 origin/master path/to/formula`
+  # TODO specify where to store that commit
+  # TODO write the commit for the formula, when we install it
+  # TODO write a corresponding comment in install/updgrade and commands that
+  # reinstalls package
+  # TODO implement method
+  def get_installed_commit
+
+  end
+
+  def self.for_name(formula_name)
+    FormulaResolver.new(formula_name)
+  end
+
 end
