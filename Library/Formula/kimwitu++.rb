5d1fd349fb83ad91a22cec51df71c3639b84faa0
Viktor Szakats <vszakats@users.noreply.github.com>
2015-12-18 20:03:46 +0100

    kimwitu++: use https homepage

--- Library/Formula/kimwitu++.rb
+++ Library/Formula/kimwitu++.rb
@@ -1,6 +1,6 @@
 class Kimwituxx < Formula
   desc "Tool for processing trees (i.e. terms)"
-  homepage "http://www2.informatik.hu-berlin.de/sam/kimwitu++/"
+  homepage "https://www2.informatik.hu-berlin.de/sam/kimwitu++/"
   url "http://download.savannah.gnu.org/releases/kimwitu-pp/kimwitu++-2.3.13.tar.gz"
   sha256 "3f6d9fbb35cc4760849b18553d06bc790466ca8b07884ed1a1bdccc3a9792a73"
 
