5087007b0b076caca8b2c84614350d04bd69e4e8
Viktor Szakats <vszakats@users.noreply.github.com>
2015-12-18 20:30:13 +0100

    vifm: use https homepage

--- Library/Formula/vifm.rb
+++ Library/Formula/vifm.rb
@@ -1,6 +1,6 @@
 class Vifm < Formula
   desc "Ncurses based file manager with vi like keybindings"
-  homepage "http://vifm.info/"
+  homepage "https://vifm.info/"
   url "https://downloads.sourceforge.net/project/vifm/vifm/vifm-0.8.tar.bz2"
   mirror "https://github.com/vifm/vifm/releases/download/v0.8/vifm-0.8.tar.bz2"
   sha256 "69eb6b50dcf462f4233ff987f0b6a295df08a27bc42577ebef725bfe58dbdeeb"
