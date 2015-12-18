ac48fc669443563ac14833092e381ae9330bca70
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-18 13:22:34 +0100

    link-grammar 5.3.2

--- Library/Formula/link-grammar.rb
+++ Library/Formula/link-grammar.rb
@@ -1,8 +1,8 @@
 class LinkGrammar < Formula
   desc "Carnegie Mellon University's link grammar parser"
   homepage "http://www.abisource.com/projects/link-grammar/"
-  url "http://www.abisource.com/downloads/link-grammar/4.7.14/link-grammar-4.7.14.tar.gz"
-  sha256 "6fe8b46c6f134c5c1e43fc0eaae048fe746c533a0cae8d63ad07fc2a3dff7667"
+  url "http://www.abisource.com/downloads/link-grammar/5.3.2/link-grammar-5.3.2.tar.gz"
+  sha256 "59c6a2e5aca78ced741709317e1858ea57f2cefcece277daa8197c5247dec11d"
 
   bottle do
     sha256 "4104f550c5765f474e9fe430f50d6bd150d13fa0f74a2e34053d8e8d1791769d" => :mavericks
@@ -10,10 +10,32 @@ class LinkGrammar < Formula
     sha256 "b4121c9596d3c6d2e46b2eab6f65f29c588fd37da2f0b9a7a79d005217928282" => :lion
   end
 
+  # Can be removed with the next release
+  # Bug fix in the test_enabled macro
+  # https://github.com/opencog/link-grammar/issues/255
+  patch do
+    url "https://github.com/opencog/link-grammar/commit/61a788512b78b12b94429f8863ca6c3ee6eafe05.patch"
+    sha256 "8da30ed3a86120101b8f5253af4bf42e23b5da2a9f2f0336fd9e8761c1d37399"
+  end
+
+  # Can be removed with the next release
+  # Disable Perl bindings by default
+  # https://github.com/opencog/link-grammar/issues/255
+  patch do
+    url "https://github.com/ampli/link-grammar/commit/adabf00ddb05b32c98b34da6691f0638781b0994.patch"
+    sha256 "6d9ec0de8349ab694035d75e3a3afefe7c031661b447eda1c0e673d4d0e1e2a4"
+  end
+
   depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "autoconf-archive" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on :ant => :build
 
   def install
+    inreplace "autogen.sh", "libtoolize", "glibtoolize"
+    system "./autogen.sh", "--no-configure"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
