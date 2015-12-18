0e9c5b5eb27566d39ec5aaa88dec092c7197b2be
Viktor Szakats <vszakats@users.noreply.github.com>
2015-12-18 20:31:31 +0100

    wait_on: use https homepage

--- Library/Formula/wait_on.rb
+++ Library/Formula/wait_on.rb
@@ -1,6 +1,6 @@
 class WaitOn < Formula
   desc "Provides shell scripts with access to kqueue(3)"
-  homepage "http://www.freshports.org/sysutils/wait_on/"
+  homepage "https://www.freshports.org/sysutils/wait_on/"
   url "http://distcache.freebsd.org/ports-distfiles/wait_on-1.1.tar.gz"
   sha256 "d7f40655f5c11e882890340826d1163050e2748de66b292c15b10d32feb6490f"
 
