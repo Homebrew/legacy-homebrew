63a1f3a2289cb13e0572d9c2c4ccf72d69e4b648
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-19 00:14:09 +0000

    moz-git-tools: add 0.1 bottle.

--- Library/Formula/moz-git-tools.rb
+++ Library/Formula/moz-git-tools.rb
@@ -5,6 +5,13 @@ class MozGitTools < Formula
     :tag => "v0.1", :revision => "cfe890e6f81745c8b093b20a3dc22d28f9fc0032"
   head "https://github.com/mozilla/moz-git-tools.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c5ddb2e842a6fb26ba5feacdee6bac287d94732abd888bd11bc5c80be4f100a4" => :el_capitan
+    sha256 "91f89ec1014d6c7b395571210c0f21b1e701f4bfb90540a94fa3daafd4472d3b" => :yosemite
+    sha256 "8df4c14355c7b6291964609122f8643f61d77e05c2b6b68517710e5653a1423e" => :mavericks
+  end
+
   def install
     # Install all the executables, except git-root since that conflicts with git-extras
     bin_array = Dir.glob("git*").push("hg-patch-to-git-patch")
