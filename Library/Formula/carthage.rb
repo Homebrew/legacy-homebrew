09c09d73779d3854cd54206c41e38668cd4d2d0c
Andrew Janke <andrew@apjanke.net>
2015-12-19 01:34:12 -0500

    carthage: audit fix

--- Library/Formula/carthage.rb
+++ Library/Formula/carthage.rb
@@ -6,13 +6,13 @@ class Carthage < Formula
                                                   :shallow => false
   head "https://github.com/Carthage/Carthage.git", :shallow => false
 
-  depends_on :xcode => ["7.1", :build]
-
   bottle do
     cellar :any
     sha256 "cde78d8aa8b7e920b4d97c4076344c8bf86303e70666f027e4c70426710e1f22" => :el_capitan
     sha256 "767ce68adfdc5aa770c1a8a036428f2db2a9d06b3ab692525ea99968578eccf8" => :yosemite
   end
+
+  depends_on :xcode => ["7.1", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}"
