c544651510b659cba82655a760d79c5fb4a45b6e
Andrew Janke <andrew@apjanke.net>
2015-12-19 01:29:01 -0500

    cake: audit fixes

--- Library/Formula/cake.rb
+++ Library/Formula/cake.rb
@@ -1,5 +1,5 @@
 class Cake < Formula
-  desc "Cake (C# Make) is a build automation system with a C# DSL."
+  desc "'C# Make' is a build automation system with a C# DSL."
   homepage "http://cakebuild.net/"
   url "https://github.com/cake-build/cake/releases/download/v0.6.4/Cake-bin-v0.6.4.zip"
   sha256 "d2135cac859abcb002833c89d47ac0ab22fd6d849e4bd72aa9a21d8e0e5b0aa6"
