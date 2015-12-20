38e7f87cd34a15eeb185e79bbed94d46aedc829f
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2015-12-20 11:50:37 +0100

    iso-codes 3.63

--- Library/Formula/iso-codes.rb
+++ Library/Formula/iso-codes.rb
@@ -1,8 +1,8 @@
 class IsoCodes < Formula
   desc "ISO language, territory, currency, script codes, and their translations"
   homepage "https://pkg-isocodes.alioth.debian.org/"
-  url "https://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.61.tar.xz"
-  sha256 "a79bf119afdc20feef12965f26f9d97868819003a76355a6f027a14a6539167d"
+  url "https://pkg-isocodes.alioth.debian.org/downloads/iso-codes-3.63.tar.xz"
+  sha256 "60600e56952dc92b3a8cd8a7044348f7cfa35be528bab2491c3c18582fb5277f"
 
   head "git://git.debian.org/git/iso-codes/iso-codes.git", :shallow => false
 
