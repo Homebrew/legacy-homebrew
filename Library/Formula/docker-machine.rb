87cd1f5d756850f322680dbe0c53a6192fd326db
Jon Gretarsson <jgretarsson@jgretarsso-ltm2.internal.salesforce.com>
2015-12-20 08:50:55 -0800

    docker-machine 0.5.4
    Closes #46598.
    Closes #47085.
    Closes #47204.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/docker-machine.rb
+++ Library/Formula/docker-machine.rb
@@ -3,8 +3,8 @@ require "language/go"
 class DockerMachine < Formula
   desc "Create Docker hosts locally and on cloud providers"
   homepage "https://docs.docker.com/machine"
-  url "https://github.com/docker/machine/archive/v0.5.1.tar.gz"
-  sha256 "cd515d9b2d14edb9ce3429865cb9cdadc81d7c4ba685422fbd1ee10025987460"
+  url "https://github.com/docker/machine/archive/v0.5.4.tar.gz"
+  sha256 "050640764c9f55e76b9475b04ebd9d6069e63cf7e2b54c2d07eda9254722d90e"
   head "https://github.com/docker/machine.git"
 
   bottle do
@@ -17,13 +17,25 @@ class DockerMachine < Formula
   depends_on "go" => :build
   depends_on "automake" => :build
 
-  def install
-    ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/docker/"
-    ln_sf buildpath, buildpath/"src/github.com/docker/machine"
+  go_resource "github.com/codegangsta/cli" do
+    url "https://github.com/codegangsta/cli.git",
+      :revision => "bca61c476e3c752594983e4c9bcd5f62fb09f157"
+  end
 
-    system "make", "build"
-    bin.install Dir["bin/*"]
+  def install
+    ENV["GOBIN"] = bin
+    ENV["GOPATH"] = buildpath
+    ENV["GOHOME"] = buildpath
+
+    path = buildpath/"src/github.com/docker/machine"
+    path.install Dir["*"]
+
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd path do
+      system "make", "build"
+      bin.install Dir["bin/*"]
+    end
 
     bash_completion.install Dir["contrib/completion/bash/*.bash"]
   end
