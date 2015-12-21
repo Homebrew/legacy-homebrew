841f4052d01c46fa467bc8674bcce7ada272dfb4
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-21 09:20:47 +0100

    jailkit 2.19
    Closes #47214.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/jailkit.rb
+++ Library/Formula/jailkit.rb
@@ -1,8 +1,8 @@
 class Jailkit < Formula
   desc "Utilities to create limited user accounts in a chroot jail"
   homepage "http://olivier.sessink.nl/jailkit/"
-  url "http://olivier.sessink.nl/jailkit/jailkit-2.17.tar.bz2"
-  sha256 "5db1d130b144c49136450369da55ab9d8c4861da3351bb4e11ab1e0921764cba"
+  url "http://olivier.sessink.nl/jailkit/jailkit-2.19.tar.bz2"
+  sha256 "bebbf6317a5a15057194dd2cf6201821c48c022dbc64c12756eb13b61eff9bf9"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
