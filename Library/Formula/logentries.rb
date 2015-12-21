5b465b7c494784861d51bd654fd8828488e4f076
Jimmy Tang <jimmy_tang@rapid7.com>
2015-12-21 14:22:34 +0000

    logentries 1.4.27
    Add plist too.
    
    Closes #47230.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/logentries.rb
+++ Library/Formula/logentries.rb
@@ -1,8 +1,9 @@
 class Logentries < Formula
   desc "Utility for access to logentries logging infrastructure"
   homepage "https://logentries.com/doc/agent/"
-  url "https://github.com/logentries/le/archive/v1.4.19.tar.gz"
-  sha256 "400765eef31ec7b453bb41f2fa31f8e6dda797561280af2e8feb643fde84a217"
+  url "https://github.com/logentries/le/archive/v1.4.27.tar.gz"
+  sha256 "218ca34395445312aeb77eb23a9bf589771da5fac9feca9b82646f57867abc2f"
+  head "https://github.com/logentries/le.git"
 
   bottle do
     cellar :any
@@ -19,5 +20,32 @@ class Logentries < Formula
 
     bin.install Dir["#{libexec}/bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  plist_options :manual => "le monitor"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+        <string>#{opt_bin}/le</string>
+        <string>monitor</string>
+        </array>
+        <key>KeepAlive</key>
+        <true/>
+        <key>RunAtLoad</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    shell_output("#{bin}/le --help", 4)
   end
 end
