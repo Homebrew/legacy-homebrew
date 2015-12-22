10fbe95067b555df6bd41fbfe1108e4f7154bab1
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-12-22 11:29:36 +0100

    gradle 2.10
    use java_cache env for test
    
    Closes #47253.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -1,8 +1,8 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-2.9-bin.zip"
-  sha256 "c9159ec4362284c0a38d73237e224deae6139cbde0db4f0f44e1c7691dd3de2f"
+  url "https://downloads.gradle.org/distributions/gradle-2.10-bin.zip"
+  sha256 "66406247f745fc6f05ab382d3f8d3e120c339f34ef54b86f6dc5f6efc18fbb13"
 
   bottle :unneeded
 
@@ -12,6 +12,7 @@ class Gradle < Formula
   end
 
   test do
+    ENV.java_cache
     output = shell_output("#{bin}/gradle --version")
     assert_match /Gradle #{version}/, output
   end
