bf74affd8bf5edbd01d650a7b8430576a57bed52
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-22 12:02:24 +0100

    mal4s: bump revision

--- Library/Formula/mal4s.rb
+++ Library/Formula/mal4s.rb
@@ -3,7 +3,7 @@ class Mal4s < Formula
   homepage "https://github.com/secure411dotorg/mal4s/"
   url "https://service.dissectcyber.com/mal4s/mal4s-1.2.8.tar.gz"
   sha256 "1c40ca9d11d113278c4fbd5c7ec9ce0edc78d6c8bd1aa7d85fb6b9473e60f0f1"
-  revision 2
+  revision 3
 
   head "https://github.com/secure411dotorg/mal4s.git"
 
