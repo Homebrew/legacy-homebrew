df353fe921c9387644cc9507266433943c5d2b06
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-23 11:34:36 +0100

    ode 0.14
    Closes #44265.
    Closes #43312.
    Closes #47289.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ode.rb
+++ Library/Formula/ode.rb
@@ -1,8 +1,9 @@
 class Ode < Formula
   desc "Library for simulating articulated rigid body dynamics"
   homepage "http://www.ode.org/"
-  url "https://bitbucket.org/odedevs/ode/downloads/ode-0.13.1.tar.gz"
-  sha256 "35e55e05c6c6ebb813a546017285a7aceedb3e8e55c8ff102e80e26bd84c5658"
+  url "https://bitbucket.org/odedevs/ode/downloads/ode-0.14.tar.gz"
+  sha256 "1072fc98d9d00262a0d6136e7b9ff7f5d953bbdb23b646f426909d28c0b4f6db"
+  head "https://bitbucket.org/odedevs/ode/", :using => :hg
 
   bottle do
     cellar :any
@@ -12,36 +13,30 @@ class Ode < Formula
     sha256 "1956cbf2602b3a8ab1a1bc770a6bfa005eb9f484362b543176f9477ce3bd797e" => :mountain_lion
   end
 
-  head do
-    url "https://bitbucket.org/odedevs/ode/", :using => :hg
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
   option "with-double-precision", "Compile ODE with double precision"
   option "with-shared", "Compile ODE with shared library support"
   option "with-libccd", "Enable all libccd colliders (except box-cylinder)"
+  option "with-x11", "Build the drawstuff library and demos"
 
   deprecated_option "enable-double-precision" => "with-double-precision"
   deprecated_option "enable-shared" => "with-shared"
   deprecated_option "enable-libccd" => "with-libccd"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on :x11 => :optional
 
   def install
-    args = ["--prefix=#{prefix}",
-            "--disable-demos"]
+    args = ["--prefix=#{prefix}"]
     args << "--enable-double-precision" if build.with? "double-precision"
     args << "--enable-shared" if build.with? "shared"
     args << "--enable-libccd" if build.with? "libccd"
+    args << "--with-demos" if build.with? "x11"
 
-    if build.head?
-      ENV["LIBTOOLIZE"] = "glibtoolize"
-      inreplace "bootstrap", "libtoolize", "$LIBTOOLIZE"
-      system "./bootstrap"
-    end
+    inreplace "bootstrap", "libtoolize", "glibtoolize"
+    system "./bootstrap"
 
     system "./configure", *args
     system "make"
