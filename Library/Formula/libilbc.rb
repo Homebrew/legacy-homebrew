f15cc25cdce1cf4f86f5fb63b4be46a52a636513
Manav Rathi <mx4492@gmail.com>
2015-12-24 13:28:35 +0530

    libilbc 2.0.2 (new formula)
    Closes #47325.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

---
+++ Library/Formula/libilbc.rb
@@ -0,0 +1,30 @@
+class Libilbc < Formula
+  desc "Packaged version of iLBC codec from the WebRTC project"
+  homepage "https://github.com/TimothyGu/libilbc"
+  url "https://github.com/TimothyGu/libilbc/releases/download/v2.0.2/libilbc-2.0.2.tar.gz"
+  sha256 "84d5c99087a35bda3463bcb993245b4f8c28c04fe037215a5ec56570f68ce519"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <ilbc.h>
+      #include <stdio.h>
+
+      int main() {
+        char version[255];
+
+        WebRtcIlbcfix_version(version);
+        printf("%s", version);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lilbc", "-o", "test"
+    system "./test"
+  end
+end
