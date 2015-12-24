17ce50a5e461c63b9e32a893924b41c1fa943446
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-12-24 03:12:29 +0100

    rbenv: improve in-formula documentation.
    - Don't suggest adding the `eval` line verbatim to user's profile
      because the syntax might not be valid for all shells (e.g. fish).
      It's best that the user runs `rbenv init` and follows the
      instructions there, which are guaranteed to be up-to-date and
      shell-aware.
    
    - Rephrase the RBENV_ROOT instructions to avoid making it sound like
      this is something users should do. Most users shouldn't set
      RBENV_ROOT explicitly. Those that choose to do so should have a
      reason and know what they're doing.

--- Library/Formula/rbenv.rb
+++ Library/Formula/rbenv.rb
@@ -1,6 +1,6 @@
 class Rbenv < Formula
-  desc "Ruby environment tool"
-  homepage "https://github.com/rbenv/rbenv"
+  desc "Ruby version manager"
+  homepage "https://github.com/rbenv/rbenv#readme"
   url "https://github.com/rbenv/rbenv/archive/v0.4.0.tar.gz"
   sha256 "d40fe637cc799b828498fc5793548fab70d9e2431efc6a3d3f4a671d670fa9ff"
   head "https://github.com/rbenv/rbenv.git"
@@ -15,11 +15,12 @@ class Rbenv < Formula
   end
 
   def caveats; <<-EOS.undent
-    To use Homebrew's directories rather than ~/.rbenv add to your profile:
+    Rbenv stores data under `~/.rbenv' by default. If you absolutely need to instead
+    store everything under the Homebrew prefix, include this in your profile:
       export RBENV_ROOT=#{var}/rbenv
 
-    To enable shims and autocompletion add to your profile:
-      if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+    To enable shims and autocompletion, run this and follow the instructions:
+      rbenv init
     EOS
   end
 
