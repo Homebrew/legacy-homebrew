0069e4ab8122c39223e6c61c1205e4cf12fbbe12
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-24 08:12:56 +0000

    gdm: add 1.0 bottle.

--- Library/Formula/gdm.rb
+++ Library/Formula/gdm.rb
@@ -6,6 +6,13 @@ class Gdm < Formula
   url "https://github.com/sparrc/gdm/archive/v1.0.tar.gz"
   sha256 "e0dbec8deed27af3ff4cb16782bc38a4fd8eeb0db8e024b3083a00d36f877cab"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "70883b5c6e28deadb5c9dde5772b077f6ddd8a98a33fca55ee7e5400cc21f41a" => :el_capitan
+    sha256 "fbd0eaebd1c242041ad1116fcb208ea4f5e797b9691a4d46dbb4fb5d48a4d5b0" => :yosemite
+    sha256 "1ac0bd076d46710e030d3d65f92177a9ffb582ec1403d13ec958939ba6e8ddf4" => :mavericks
+  end
+
   depends_on "go"
 
   go_resource "golang.org/x/tools" do
