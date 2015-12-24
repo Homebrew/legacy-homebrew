9203aef0d3d3a6487467b989be7dfb343e3a81de
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-24 10:42:30 +0000

    ntl: update 9.6.2 bottle.

--- Library/Formula/ntl.rb
+++ Library/Formula/ntl.rb
@@ -5,10 +5,10 @@ class Ntl < Formula
   sha256 "804c11f6ee8621e492e481a447faa836d165f77d4c84bd575417a5dcb200c6e1"
 
   bottle do
-    cellar :any
-    sha256 "caa258ebeb39f602cef0627b3f3391136a79bd1ce9d81bc727f104a6e65f8feb" => :yosemite
-    sha256 "92fa4146b889fd7a0b4dd50aed248cfe310fb3e7cb3b65d608aca26298331b46" => :mavericks
-    sha256 "dd1f71b5dd429df7cc7254a2b5c99ceb844ad2ba8cdc4e258fb22db4ccf68ec3" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "55a64d1bd9ae612707e1a91d00099246d670296421d6e22fb926046e1bc0b0f7" => :el_capitan
+    sha256 "223b2f97a78cea6fa2c85728159058b83943c75aea30c7c7a289b49c90579919" => :yosemite
+    sha256 "b229f7cf772ec9ab10fd4abb641e914b3477848dc6be8e762d8ef8757367943b" => :mavericks
   end
 
   depends_on "gmp" => :optional
