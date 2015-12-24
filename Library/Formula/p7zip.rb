5b6a0c03f842c7d444cd701996f4b37a6c7d869b
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-24 15:54:57 +0100

    p7zip 15.09
    Closes #47344.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/p7zip.rb
+++ Library/Formula/p7zip.rb
@@ -1,8 +1,8 @@
 class P7zip < Formula
   desc "7-Zip (high compression file archiver) implementation"
   homepage "http://p7zip.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/p7zip/p7zip/9.20.1/p7zip_9.20.1_src_all.tar.bz2"
-  sha256 "49557e7ffca08100f9fc687f4dfc5aea703ca207640c76d9dee7b66f03cb4782"
+  url "https://downloads.sourceforge.net/project/p7zip/p7zip/15.09/p7zip_15.09_src_all.tar.bz2"
+  sha256 "8783acf747e210e00150f7311cc06c4cd8ecf7b0c27b4adf2194284cc49b4d6f"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,26 +12,8 @@ class P7zip < Formula
     sha256 "c621e245f8b0912e135861756e3b3f443858f3d8291887d2438acbf3c09f4ee3" => :mavericks
   end
 
-  devel do
-    url "https://downloads.sourceforge.net/project/p7zip/p7zip/15.09/p7zip_15.09_src_all.tar.bz2"
-    sha256 "8783acf747e210e00150f7311cc06c4cd8ecf7b0c27b4adf2194284cc49b4d6f"
-  end
-
-  option "32-bit"
-
   def install
-    if build.devel?
-      mv "makefile.macosx_llvm_64bits", "makefile.machine"
-    else
-      if Hardware.is_32_bit? || build.build_32_bit?
-        mv "makefile.macosx_32bits", "makefile.machine"
-      else
-        mv "makefile.macosx_64bits", "makefile.machine"
-      end
-      # install.sh chmods to 444, which is bad and breaks uninstalling
-      inreplace "install.sh", /chmod (444|555).*/, ""
-    end
-
+    mv "makefile.macosx_llvm_64bits", "makefile.machine"
     system "make", "all3",
                    "CC=#{ENV.cc} $(ALLFLAGS)",
                    "CXX=#{ENV.cxx} $(ALLFLAGS)"
