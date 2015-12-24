fc41a59ee4de043e137e91991b6c7f03cb5e36eb
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-24 15:43:28 +0100

    latex2html 2015
    Closes #47343.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/latex2html.rb
+++ Library/Formula/latex2html.rb
@@ -1,8 +1,8 @@
 class Latex2html < Formula
   desc "LaTeX-to-HTML translator"
-  homepage "http://ctan.uib.no/help/Catalogue/entries/latex2html.html"
-  url "http://ctan.uib.no/support/latex2html/latex2html-2012.tgz"
-  sha256 "36b4b633507dba19a356d4ec5c6c145b242406b2b47cbe44aabf5e7efa2aa16f"
+  homepage "https://www.ctan.org/pkg/latex2html"
+  url "http://mirrors.ctan.org/support/latex2html/latex2html-2015.tar.gz"
+  sha256 "248cf84c70dd31221ddc69ef7ce0e720aadd26b572ee412827eae62f0eefb8dd"
 
   bottle do
     sha256 "a34ed6cad64be682a18ef461cf96e91bb41cdd5766256a5255b279870efc7c51" => :yosemite
