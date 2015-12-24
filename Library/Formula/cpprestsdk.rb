b4c17b48988c8cb17dc4af68e9c11f275f570214
Michael Yang <ohmyarchlinux@gmail.com>
2015-12-24 16:27:26 +0800

    cpprestsdk 2.7.0 (new formula)

--- Library/Formula/cpprestsdk.rb
+++ Library/Formula/cpprestsdk.rb
@@ -11,18 +11,16 @@ class Cpprestsdk < Formula
     system "cmake", "-DBUILD_SAMPLES=OFF", "Release", *std_cmake_args
     system "make", "install"
   end
-  
   test do
     (testpath/"test.cc").write <<-EOS.undent
       #include <iostream>
       #include <cpprest/http_client.h>
       int main() {
         web::http::client::http_client client(U("https://github.com/"));
-        
         std::cout << client.request(web::http::methods::GET).get().extract_string().get() << std::endl;
       }
     EOS
-    flags = ["-stdlib=libc++", "-std=c++14", "-I#{include}", "-I#{Formula["boost"].include}", "-I/usr/local/opt/openssl/include", "-L#{lib}", "-L/usr/local/opt/openssl/lib", "-L#{Formula["boost"].lib}", "-lssl", "-lcrypto", "-lboost_random", "-lboost_chrono", "-lboost_thread-mt", "-lboost_system-mt", "-lboost_regex", "-lboost_filesystem", "-lcpprest"] + ENV.cflags.to_s.split
+    flags = ["-stdlib=libc++", "-std=c++11", "-I#{include}", "-I#{Formula["boost"].include}", "-I/usr/local/opt/openssl/include", "-L#{lib}", "-L/usr/local/opt/openssl/lib", "-L#{Formula["boost"].lib}", "-lssl", "-lcrypto", "-lboost_random", "-lboost_chrono", "-lboost_thread-mt", "-lboost_system-mt", "-lboost_regex", "-lboost_filesystem", "-lcpprest"] + ENV.cflags.to_s.split
     system ENV.cxx, "-o", "test_cpprest", "test.cc", *flags
     system "./test_cpprest"
   end
