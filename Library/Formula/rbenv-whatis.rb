5a4ef65a53d48f67812a4819ff6350a409a3dcd4
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-12-24 12:57:17 +0100

    Add `head` directive to rbenv-use, rbenv-whatis

--- Library/Formula/rbenv-whatis.rb
+++ Library/Formula/rbenv-whatis.rb
@@ -3,6 +3,7 @@ class RbenvWhatis < Formula
   homepage "https://github.com/rkh/rbenv-whatis"
   url "https://github.com/rkh/rbenv-whatis/archive/v1.0.0.tar.gz"
   sha256 "1a09f824d1dcbca360565930fa66e93a9a2840c1bb45935e2ee989ce57d1f6e6"
+  head "https://github.com/rkh/rbenv-whatis.git"
 
   bottle :unneeded
 
