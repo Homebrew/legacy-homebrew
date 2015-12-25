9b8bd801b87a4e69ed239f81baefa70795e2ca5f
Mike McQuaid <mike@mikemcquaid.com>
2015-12-25 20:11:35 +0000

    cpprestsdk: add missing whitespace.

--- Library/Formula/cpprestsdk.rb
+++ Library/Formula/cpprestsdk.rb
@@ -3,6 +3,7 @@ class Cpprestsdk < Formula
   homepage "https://github.com/Microsoft/cpprestsdk"
   url "https://github.com/Microsoft/cpprestsdk/archive/v2.7.0.tar.gz"
   sha256 "d53593fda17bcac8f68af8bc3ba4ac638ea7e7480f43aa7f3350f6f200b0c63e"
+
   bottle do
     cellar :any
     sha256 "f3d0e1fa77b872b9f0bf58ea363d5a6dce656e6f68e90cdb584d090f419d27e4" => :el_capitan
@@ -18,6 +19,7 @@ class Cpprestsdk < Formula
     system "cmake", "-DBUILD_SAMPLES=OFF", "Release", *std_cmake_args
     system "make", "install"
   end
+
   test do
     (testpath/"test.cc").write <<-EOS.undent
       #include <iostream>
@@ -27,7 +29,13 @@ class Cpprestsdk < Formula
         std::cout << client.request(web::http::methods::GET).get().extract_string().get() << std::endl;
       }
     EOS
-    flags = ["-stdlib=libc++", "-std=c++11", "-I#{include}", "-I#{Formula["boost"].include}", "-I/usr/local/opt/openssl/include", "-L#{lib}", "-L/usr/local/opt/openssl/lib", "-L#{Formula["boost"].lib}", "-lssl", "-lcrypto", "-lboost_random", "-lboost_chrono", "-lboost_thread-mt", "-lboost_system-mt", "-lboost_regex", "-lboost_filesystem", "-lcpprest"] + ENV.cflags.to_s.split
+    flags = ["-stdlib=libc++", "-std=c++11", "-I#{include}",
+             "-I#{Formula["boost"].include}",
+             "-I#{Formula["openssl"].include}", "-L#{lib}",
+             "-L#{Formula["openssl"].lib}", "-L#{Formula["boost"].lib}",
+             "-lssl", "-lcrypto", "-lboost_random", "-lboost_chrono",
+             "-lboost_thread-mt", "-lboost_system-mt", "-lboost_regex",
+             "-lboost_filesystem", "-lcpprest"] + ENV.cflags.to_s.split
     system ENV.cxx, "-o", "test_cpprest", "test.cc", *flags
     system "./test_cpprest"
   end
