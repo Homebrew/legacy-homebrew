c2caee30c44601a08878c257fdee5137f8306b66
Jimmy Tang <jcftang@gmail.com>
2015-12-25 20:47:40 +0000

    logentries: Add plist_options

--- Library/Formula/logentries.rb
+++ Library/Formula/logentries.rb
@@ -22,6 +22,8 @@ class Logentries < Formula
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
+  plist_options :manual => "le monitor"
+
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
