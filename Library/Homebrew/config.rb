f482e93f429977df74d15308c12937aef2154f45
Mike McQuaid <mike@mikemcquaid.com>
2015-12-25 22:42:00 +0000

    bin/brew: set more HOMEBREW_* env from ya Bash.

--- Library/Homebrew/config.rb
+++ Library/Homebrew/config.rb
@@ -31,21 +31,16 @@ unless defined? HOMEBREW_BREW_FILE
 end
 
 # Where we link under
-HOMEBREW_PREFIX = Pathname.new(HOMEBREW_BREW_FILE).dirname.parent
+HOMEBREW_PREFIX = Pathname.new(ENV["HOMEBREW_PREFIX"])
 
 # Where .git is found
-HOMEBREW_REPOSITORY = Pathname.new(HOMEBREW_BREW_FILE).realpath.dirname.parent
+HOMEBREW_REPOSITORY = Pathname.new(ENV["HOMEBREW_REPOSITORY"])
 
-HOMEBREW_LIBRARY = HOMEBREW_REPOSITORY/"Library"
+HOMEBREW_LIBRARY = Pathname.new(ENV["HOMEBREW_LIBRARY"])
 HOMEBREW_CONTRIB = HOMEBREW_REPOSITORY/"Library/Contributions"
 
-# Where we store built products; /usr/local/Cellar if it exists,
-# otherwise a Cellar relative to the Repository.
-HOMEBREW_CELLAR = if (HOMEBREW_PREFIX+"Cellar").exist?
-  HOMEBREW_PREFIX+"Cellar"
-else
-  HOMEBREW_REPOSITORY+"Cellar"
-end
+# Where we store built products
+HOMEBREW_CELLAR = Pathname.new(ENV["HOMEBREW_CELLAR"])
 
 HOMEBREW_LOGS = Pathname.new(ENV["HOMEBREW_LOGS"] || "~/Library/Logs/Homebrew/").expand_path
 
