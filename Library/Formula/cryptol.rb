f44812b4e9a05faa4445408b07695a424227722e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-25 00:48:34 +0000

    cryptol: update 2.2.6 bottle.

--- Library/Formula/cryptol.rb
+++ Library/Formula/cryptol.rb
@@ -11,9 +11,9 @@ class Cryptol < Formula
   head "https://github.com/GaloisInc/cryptol.git"
 
   bottle do
-    sha256 "68e7672e7481772a3fc758682d76f55480ea9d125b889c011340f9165ff0cf76" => :yosemite
-    sha256 "d5f1fa017614729c6c849bda23d568689c17d782e1f2157254795c3486affe57" => :mavericks
-    sha256 "eeae281e614864184053d9814c75ccf796dd79303f0281234bf4cda85cfbe996" => :mountain_lion
+    sha256 "58595e61bb8681a1871668217a3462db76a84f7a437b58014fa1589ba3863b06" => :el_capitan
+    sha256 "2c3f5331ff9c08ee17307c48dabd23bc7768388901789973e0054a4890981bb6" => :yosemite
+    sha256 "5af2fca89194f8b0be00311d96990a7e99ed725983631a08777ad28d2163020f" => :mavericks
   end
 
   depends_on "ghc" => :build
