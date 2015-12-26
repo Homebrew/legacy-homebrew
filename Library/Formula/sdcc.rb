4f21816b439098452e2d3f5e27d490efd2efc4fb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-26 22:49:04 +0000

    sdcc: update 3.5.0 bottle.

--- Library/Formula/sdcc.rb
+++ Library/Formula/sdcc.rb
@@ -7,9 +7,10 @@ class Sdcc < Formula
   head "https://sdcc.svn.sourceforge.net/svnroot/sdcc/trunk/sdcc/"
 
   bottle do
-    sha256 "f22ecc49004e7e760c3510d90a8818289974c702e18887a46d04104d11e3a69b" => :yosemite
-    sha256 "2ad5b170ee4c018faeb6c5ec3a1b7dc01a34cec0b635ea8c654f5d290b801f19" => :mavericks
-    sha256 "79f62fa7c8972a9b8b25bf1a96b92262ddc916517bda7567b2c761c5ea211ffe" => :mountain_lion
+    revision 1
+    sha256 "d46fdad8f291ea90162e7218ec3d43468de8b85680da5f1088617be8521005f5" => :el_capitan
+    sha256 "b5cd6950c3dd7b2d7399e0a7eabdd02992a34b8ec0c1c9fc9e46ddc7f561fce6" => :yosemite
+    sha256 "73aecffe0f2ec715f532c09ef0f95d3a045582cc65b5e10c48208eac5bfe655e" => :mavericks
   end
 
   option "with-avr-port", "Enables the AVR port (UNSUPPORTED, MAY FAIL)"
