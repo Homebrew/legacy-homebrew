d1a12ab1b08d8ddbd1e45ee3ed4e7f8767f9b384
Dominyk Tiller <dominyktiller@gmail.com>
2015-12-26 23:38:33 +0000

    twemcache 2.6.2

--- Library/Formula/twemcache.rb
+++ Library/Formula/twemcache.rb
@@ -1,31 +1,34 @@
 class Twemcache < Formula
   desc "Twitter fork of memcached"
   homepage "https://github.com/twitter/twemcache"
+  url "https://github.com/twitter/twemcache/archive/v2.6.2.tar.gz"
+  sha256 "49905ceb89bf5d0fde25fa4b8843b2fe553915c0dc75c813de827bd9c0c85e26"
   head "https://github.com/twitter/twemcache.git"
-  url "https://github.com/twitter/twemcache/archive/v2.6.0.tar.gz"
-  sha256 "6e0e9361bda46bdaa577c7eed6cd829aeca442b2c3b4f84b250039c86027ce05"
 
-  option "enable-debug", "Debug mode with assertion panics enabled"
+  option "with-debug", "Debug mode with assertion panics enabled"
+
+  deprecated_option "enable-debug" => "with-debug"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libevent"
 
   def install
-    # After the deprecation of GitHub Downloads, we don't have distribution
-    # downloads anymore.  Everything is now a repository download from version
-    # tags.
-    system "autoreconf", "-ivf"
+    args = %W[--prefix=#{prefix}]
 
-    args = ["--prefix=#{prefix}"]
-    if build.include? "enable-debug"
+    if build.with? "debug"
       ENV.O0
       ENV.append "CFLAGS", "-ggdb3"
       args << "--enable-debug=full"
     end
 
+    system "autoreconf", "-fvi"
     system "./configure", *args
     system "make"
     system "make", "install"
+  end
+
+  test do
+    system bin/"twemcache", "--help"
   end
 end
