1024ea126226741bd3b7ce840a539b26e96b108d
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2015-12-26 10:59:11 +0100

    zimg 2.0.2 (new formula)

---
+++ Library/Formula/zimg.rb
@@ -0,0 +1,33 @@
+class Zimg < Formula
+  desc "Scaling, colorspace conversion, and dithering library"
+  homepage "https://github.com/sekrit-twc/zimg"
+  url "https://github.com/sekrit-twc/zimg/archive/release-2.0.2.tar.gz"
+  sha256 "b9c7bac9e6ad53dfa94215c28440167d72d41109df10278673789f8e531f2142"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <assert.h>
+      #include <zimg.h>
+
+      int main()
+      {
+        zimg_image_format format;
+        zimg_image_format_default(&format, ZIMG_API_VERSION);
+        assert(ZIMG_MATRIX_UNSPECIFIED == format.matrix_coefficients);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lzimg", "-o", "test"
+    system "./test"
+  end
+end
