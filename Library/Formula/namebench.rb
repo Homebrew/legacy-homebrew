a35819502690b61b9a42c4fe92778c1ba131662f
Dominyk Tiller <dominyktiller@gmail.com>
2015-12-26 21:32:47 +0000

    namebench: use without-xyz syntax

--- Library/Formula/namebench.rb
+++ Library/Formula/namebench.rb
@@ -4,17 +4,19 @@ class Namebench < Formula
   url "https://namebench.googlecode.com/files/namebench-1.3.1-source.tgz"
   sha256 "30ccf9e870c1174c6bf02fca488f62bba280203a0b1e8e4d26f3756e1a5b9425"
 
-  option "no-third-party", "Do not install bundled third-party modules"
+  option "without-third-party", "Do not install bundled third-party modules"
+
+  deprecated_option "no-third-party" => "without-third-party"
 
   def install
-    ENV["PYTHONPATH"] = lib+"python2.7/site-packages"
-    ENV["NO_THIRD_PARTY"]="1" if build.include?("no-third-party")
+    ENV["NO_THIRD_PARTY"] = "1" if build.without? "third-party"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
 
-    system "python", "setup.py", "install", "--prefix=#{prefix}",
-                                            "--install-data=#{lib}/python2.7/site-packages"
+    system "python", "setup.py", "install", "--prefix=#{libexec}",
+                     "--install-data=#{libexec}/lib/python2.7/site-packages"
 
     bin.install "namebench.py" => "namebench"
-    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
