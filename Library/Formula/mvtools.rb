e9b4188c934a3044666ec092deca8909b04d89c8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-26 20:17:05 +0000

    mvtools: add 9 bottle.

--- Library/Formula/mvtools.rb
+++ Library/Formula/mvtools.rb
@@ -5,6 +5,13 @@ class Mvtools < Formula
   sha256 "e417764cddcc2b24ee5a91c1136e95237ce1424f5d7f49ceb62ff092db18d907"
   head "https://github.com/dubhater/vapoursynth-mvtools.git"
 
+  bottle do
+    cellar :any
+    sha256 "1edd2c510374f7f7100b702e81ef68ea1b7461b2f4d93fac013052bbb8c2f4f5" => :el_capitan
+    sha256 "a9fe49ecb6275bd8f183caf6ad7ea662abb7ba8417757186681b5b517eeedb2f" => :yosemite
+    sha256 "cbb7dd3106bf1407eeded6b894454c9c7dbec765e00c9a6909301db2d2c83cd1" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "yasm" => :build
   depends_on "vapoursynth"
