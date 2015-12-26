554a320d7678957fb98fafc780f09022f1d01633
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-26 22:49:04 +0000

    namebench: add 1.3.1 bottle.

--- Library/Formula/namebench.rb
+++ Library/Formula/namebench.rb
@@ -4,6 +4,13 @@ class Namebench < Formula
   url "https://namebench.googlecode.com/files/namebench-1.3.1-source.tgz"
   sha256 "30ccf9e870c1174c6bf02fca488f62bba280203a0b1e8e4d26f3756e1a5b9425"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3333ef2615f6fbf294cede389d8545487474779a52c18108feb83a4697530cdc" => :el_capitan
+    sha256 "8d400aed171038f248e9d91718fb42625fc1f278df538b34259f26918b245f66" => :yosemite
+    sha256 "ac3d993b71305c18b47fa671ecb4c5875b80fd7ea87a6fff0f123c3c2cfdcb43" => :mavericks
+  end
+
   option "without-third-party", "Do not install bundled third-party modules"
 
   deprecated_option "no-third-party" => "without-third-party"
