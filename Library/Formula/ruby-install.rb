f44ea98f1b9e96b9b97f68cdd368612792603e6c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-26 00:53:16 +0000

    ruby-install: add 0.6.0 bottle.

--- Library/Formula/ruby-install.rb
+++ Library/Formula/ruby-install.rb
@@ -6,6 +6,13 @@ class RubyInstall < Formula
 
   head "https://github.com/postmodern/ruby-install.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "38703ce6849bf87c8a67e0e1b9f093f0cba1b5d9cceb88b44fb9949ff4fafbe4" => :el_capitan
+    sha256 "c2e7fd7648f01527d596f416405418bdbee463604f3013c1cd860b4e2b0543c2" => :yosemite
+    sha256 "ec0f19e5ab2fb76c1449e22a412505d2061180c9bb4d89896f73316dd15a4926" => :mavericks
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
