75acc66a3a032eafe71a6e4837b3a5d43dce2ce9
Paulo Moura <pmoura@logtalk.org>
2015-12-26 21:52:50 +0000

    logtalk 3.02.2
    Closes #47400.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/logtalk.rb
+++ Library/Formula/logtalk.rb
@@ -1,9 +1,9 @@
 class Logtalk < Formula
   desc "Object-oriented logic programming language"
   homepage "http://logtalk.org"
-  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3021stable.tar.gz"
-  version "3.02.1"
-  sha256 "d35d60b0f1c32e1365bd11f1f3fda234d4902c317429a2d66df792309854c5c1"
+  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3022stable.tar.gz"
+  version "3.02.2"
+  sha256 "45fbb107156ae3baf088c84475cc1ea0a8e519cc09ae929e2480d0c62047e5e8"
 
   bottle do
     cellar :any_skip_relocation
