14e06d82c51e22e29388cff54d5e884898104616
Dan <dan@wadoku.de>
2015-12-27 20:03:46 +0100

    mariadb 10.1.10
    Closes #48349.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/mariadb.rb
+++ Library/Formula/mariadb.rb
@@ -1,8 +1,8 @@
 class Mariadb < Formula
   desc "Drop-in replacement for MySQL"
   homepage "https://mariadb.org/"
-  url "http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.9/source/mariadb-10.1.9.tar.gz"
-  sha256 "8e9c5826722cedb4209bf06ae61069664513149479a6634e3d8115961edfe110"
+  url "http://ftp.osuosl.org/pub/mariadb/mariadb-10.1.10/source/mariadb-10.1.10.tar.gz"
+  sha256 "d2c26fc76ff7397bdf25924161174c30a2b7fbd59893048f50145fc0a8278f76"
 
   bottle do
     sha256 "b650d5ba130641a2bdb00f4816f2b2f909ba31eeb45b03c494e3d028ed101f09" => :el_capitan
@@ -30,6 +30,13 @@ class Mariadb < Formula
   conflicts_with "mysql-connector-c",
     :because => "both install MySQL client libraries"
   conflicts_with "mytop", :because => "both install `mytop` binaries"
+
+  patch do
+    # fix compilation error https://mariadb.atlassian.net/browse/MDEV-9322
+    # fixed in 10.1.11
+    url "https://github.com/Buggynours/MariaDB/commit/3e76d54b98e328768b1999344c0affc7a8f04b69.patch"
+    sha256 "78b32a1628ed7b2ea30c4a7fdf876d3e05db2cfe7bfa5a286aa213afda4b37b2"
+  end
 
   def install
     # Don't hard-code the libtool path. See:
