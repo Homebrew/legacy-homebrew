6383d7307088d8cc44b9efe31286c357e2b68fcf
Tomasz Pajor <tomek@polishgeeks.com>
2015-12-27 10:19:05 +0100

    x265 1.8

--- Library/Formula/x265.rb
+++ Library/Formula/x265.rb
@@ -1,9 +1,9 @@
 class X265 < Formula
   desc "H.265/HEVC encoder"
   homepage "http://x265.org"
-  url "https://bitbucket.org/multicoreware/x265/downloads/x265_1.7.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/x/x265/x265_1.7.orig.tar.gz"
-  sha256 "a52ca95e8e64219c1e8e73a9abf0bb7151ced2c93756a623cf2b7e5cf8226585"
+  url "https://bitbucket.org/multicoreware/x265/downloads/x265_1.8.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/x/x265/x265_1.8.orig.tar.gz"
+  sha256 "760e6280c688f1ea90c492d19fc6d4084ca1c4b0ea9b2e3c736d32565c30d857"
 
   head "https://bitbucket.org/multicoreware/x265", :using => :hg
 
@@ -27,7 +27,7 @@ class X265 < Formula
     args = std_cmake_args
     args << "-DHIGH_BIT_DEPTH=ON" if build.with? "16-bit"
 
-    system "cmake", "source",  *args
+    system "cmake", "source", *args
     system "make", "install"
   end
 
