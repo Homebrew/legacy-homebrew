08fa43b45d5a131c8d70b368eef8e26c131de03a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-27 19:54:41 +0000

    tokyo-tyrant: add 1.1.41_1 bottle.

--- Library/Formula/tokyo-tyrant.rb
+++ Library/Formula/tokyo-tyrant.rb
@@ -5,6 +5,13 @@ class TokyoTyrant < Formula
   sha256 "42af70fb9f2795d4e05c3e37941ce392a9eaafc991e230c48115370f6d64b88f"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "57a41c1059c3b2d43f7fc8dfc1b3bae442ddf94a5cd2bb8e91015972ba45d483" => :el_capitan
+    sha256 "faeb9efd37fbb0d163f541a3436c3ede81ebcd049094e172a03a0fc7a00e986a" => :yosemite
+    sha256 "5d490b81052af030dcbdfd01b93bc6b8fdc37f52e9a561a62d631ce1fd67f902" => :mavericks
+  end
+
   depends_on "tokyo-cabinet"
 
   def install
