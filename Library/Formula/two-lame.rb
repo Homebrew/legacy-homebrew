f6816a4ed6a80a7a611bb8b294b03f8ec07576e8
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-27 04:24:37 +0000

    two-lame: add 0.3.13 bottle.

--- Library/Formula/two-lame.rb
+++ Library/Formula/two-lame.rb
@@ -1,5 +1,12 @@
 class TwoLame < Formula
   desc "Optimized MPEG Audio Layer 2 (MP2) encoder"
+  bottle do
+    cellar :any
+    sha256 "f42b6a20cbe1d020ed818145180be176361ceda2c203a09bddc0bc1f4c903530" => :el_capitan
+    sha256 "7d5a39e1189f77321a2603b77984c57970f897a69aee9bccc008e84a807dbf59" => :yosemite
+    sha256 "b03ee3602cad74094fc1141c3fd9aac92f2e298bf0f98a3281a6ba3f547511cd" => :mavericks
+  end
+
   # Homepage down since at least December 2015
   # homepage "http://www.twolame.org/"
   homepage "https://sourceforge.net/projects/twolame/"
