6bbeb63d6417e808947889a511251aa98ee04783
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-27 03:46:50 +0000

    liberasurecode: add 1.1.0 bottle.

--- Library/Formula/liberasurecode.rb
+++ Library/Formula/liberasurecode.rb
@@ -4,6 +4,13 @@ class Liberasurecode < Formula
   url "https://bitbucket.org/tsg-/liberasurecode/downloads/liberasurecode-1.1.0.tar.gz"
   sha256 "3f8aadab190b82a3c18fdfc2c36a908a39e478d68e91f5d89a59849e91449039"
 
+  bottle do
+    cellar :any
+    sha256 "824ffcb696c9951aa380b6f5806a72573726b7026e0a051e7e6da8abcd542fc5" => :el_capitan
+    sha256 "57683d16fd2dba4def2a9b6edd012aa35697dacd839ae1921b2ee4998dba1965" => :yosemite
+    sha256 "f6b2d2aa09eb404e323634ae08aa413e7ca8db7a860cb2af43f58e4a3cd1a624" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
