1c728e7c36ff71ddec4d9997004ba31b99fed606
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-27 04:24:37 +0000

    twemcache: add 2.6.2 bottle.

--- Library/Formula/twemcache.rb
+++ Library/Formula/twemcache.rb
@@ -5,6 +5,13 @@ class Twemcache < Formula
   sha256 "49905ceb89bf5d0fde25fa4b8843b2fe553915c0dc75c813de827bd9c0c85e26"
   head "https://github.com/twitter/twemcache.git"
 
+  bottle do
+    cellar :any
+    sha256 "8257030de4e6251c2899c235cf860b8aae1def40922d0ebd474bba5b92133982" => :el_capitan
+    sha256 "458cedc005b03da5a0dbbc2bdc916b30f04b1dc37916dbcaf38b6270712b4bd2" => :yosemite
+    sha256 "4489ad8862e470f2c9ccab1bdf5ac072ab42133283a6f89b4f2cd831af902e67" => :mavericks
+  end
+
   option "with-debug", "Debug mode with assertion panics enabled"
 
   deprecated_option "enable-debug" => "with-debug"
