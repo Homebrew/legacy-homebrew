4e6397e32540c0a5b39578d6a9846d0ea6b6bfe4
Tim D. Smith <git@tim-smith.us>
2015-12-27 13:02:39 -0500

    ansible: restore cryptography package
    Fixes #47258.
    
    Closes #47424.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/ansible.rb
+++ Library/Formula/ansible.rb
@@ -70,11 +70,47 @@ class Ansible < Formula
   end
 
   #
-  # Resources required by docker-py and pyrax (see below)
+  # Resources required by docker-py, pyrax, and shade (see below).
+  # Install requests with [security]
   #
+  resource "cffi" do
+    url "https://pypi.python.org/packages/source/c/cffi/cffi-1.4.2.tar.gz"
+    sha256 "8f1d177d364ea35900415ae24ca3e471be3d5334ed0419294068c49f45913998"
+  end
+
+  resource "cryptography" do
+    url "https://pypi.python.org/packages/source/c/cryptography/cryptography-1.1.2.tar.gz"
+    sha256 "7f51459f84d670444275e615839f4542c93547a12e938a0a4906dafe5f7de153"
+  end
+
+  resource "enum34" do
+    url "https://pypi.python.org/packages/source/e/enum34/enum34-1.1.2.tar.gz"
+    sha256 "2475d7fcddf5951e92ff546972758802de5260bf409319a9f1934e6bbc8b1dc7"
+  end
+
+  resource "idna" do
+    url "https://pypi.python.org/packages/source/i/idna/idna-2.0.tar.gz"
+    sha256 "16199aad938b290f5be1057c0e1efc6546229391c23cea61ca940c115f7d3d3b"
+  end
+
+  resource "ndg-httpsclient" do
+    url "https://pypi.python.org/packages/source/n/ndg-httpsclient/ndg_httpsclient-0.4.0.tar.gz"
+    sha256 "e8c155fdebd9c4bcb0810b4ed01ae1987554b1ee034dd7532d7b8fdae38a6274"
+  end
+
+  resource "pyasn1" do
+    url "https://pypi.python.org/packages/source/p/pyasn1/pyasn1-0.1.9.tar.gz"
+    sha256 "853cacd96d1f701ddd67aa03ecc05f51890135b7262e922710112f12a2ed2a7f"
+  end
+
+  resource "pycparser" do
+    url "https://pypi.python.org/packages/source/p/pycparser/pycparser-2.14.tar.gz"
+    sha256 "7959b4a74abdc27b312fed1c21e6caf9309ce0b29ea86b591fd2e99ecdf27f73"
+  end
+
   resource "requests" do
-    url "https://pypi.python.org/packages/source/r/requests/requests-2.8.1.tar.gz"
-    sha256 "84fe8d5bf4dcdcc49002446c47a146d17ac10facf00d9086659064ac43b6c25b"
+    url "https://pypi.python.org/packages/source/r/requests/requests-2.9.1.tar.gz"
+    sha256 "c577815dd00f1394203fc44eb979724b098f88264a9ef898ee45b8e5e9cf587f"
   end
 
   resource "six" do
@@ -485,13 +521,17 @@ class Ansible < Formula
   end
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    vendor_site_packages = libexec/"vendor/lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", vendor_site_packages
 
     resources.each do |r|
       r.stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
+
+    # ndg is a namespace package
+    touch vendor_site_packages/"ndg/__init__.py"
 
     inreplace "lib/ansible/constants.py" do |s|
       s.gsub! "/usr/share/ansible", share/"ansible"
