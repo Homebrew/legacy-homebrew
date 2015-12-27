4cf2fb30db6a02cd5315287c8771d9ef9241d7a3
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-27 03:46:49 +0000

    jerasure: add 2.0 bottle.

--- Library/Formula/jerasure.rb
+++ Library/Formula/jerasure.rb
@@ -5,6 +5,13 @@ class Jerasure < Formula
   version "2.0"
   sha256 "61b2fbb25affeddc2d94d6f67778098597b14ff5440f39d8fba3dbdbaa6739b6"
 
+  bottle do
+    cellar :any
+    sha256 "f9b3b18ac272a9a3c96d4fdef5b78c3dca4719b4493b7c1e5171bfa7e5d1731b" => :el_capitan
+    sha256 "4bbf04ae52e868a4b356eb242bb98ede4c2efb744dd32c979b7658cf64a8dfb6" => :yosemite
+    sha256 "8f9b0282b6db808f94a915b83e937ae2e1413a2e5b49e72f2196235f0cff2962" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
