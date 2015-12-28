6a112cc53794deeb8924af21fd0d0104a4782363
Ciaran Treanor <ciaran.treanor@danutech.com>
2015-12-28 17:08:54 +0000

    solr: replaced tabs with spaces to keep brew audit happy

--- Library/Formula/solr.rb
+++ Library/Formula/solr.rb
@@ -20,16 +20,15 @@ class Solr < Formula
     prefix.install "#{libexec}/example"
     prefix.install "#{libexec}/server"
 
-
     # Fix the classpath for the post tool
     inreplace "#{bin}/post", '"$SOLR_TIP/dist"', "#{libexec}/dist"
 
     # Fix the paths in the sample solrconfig.xml files
     Dir.glob(["#{prefix}/example/**/solrconfig.xml",
-    	"#{prefix}/**/data_driven_schema_configs/**/solrconfig.xml",
-    	"#{prefix}/**/sample_techproducts_configs/**/solrconfig.xml"]) do |f|
-    	inreplace f, ":../../../..}/", "}/libexec/"
-    end    
+      "#{prefix}/**/data_driven_schema_configs/**/solrconfig.xml",
+      "#{prefix}/**/sample_techproducts_configs/**/solrconfig.xml"]) do |f|
+      inreplace f, ":../../../..}/", "}/libexec/"
+    end
   end
 
   plist_options :manual => "solr start"
