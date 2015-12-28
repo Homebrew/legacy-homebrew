ea4cd5bf998a3206503c98efa953d12a5146770f
Cory Donnelly <cory.donnelly@capitalone.com>
2015-12-28 11:17:31 -0500

    vramsteg 1.1.0
    Closes #47461.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/vramsteg.rb
+++ Library/Formula/vramsteg.rb
@@ -1,13 +1,19 @@
 class Vramsteg < Formula
   desc "Add progress bars to command-line applications"
   homepage "https://tasktools.org/projects/vramsteg.html"
-  url "https://taskwarrior.org/download/vramsteg-1.0.1.tar.gz"
-  sha256 "bc47e078079a845fa9c9cc5e4c9f4585402430ac6efc82ea6ff607506af8bdb9"
+  url "https://tasktools.org/download/vramsteg-1.1.0.tar.gz"
+  sha256 "9cc82eb195e4673d9ee6151373746bd22513033e96411ffc1d250920801f7037"
+  head "https://git.tasktools.org/scm/ut/vramsteg.git", :branch => "1.1.1"
 
   depends_on "cmake" => :build
 
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    # Check to see if vramsteg can obtain the current time as epoch
+    assert_match /^\d+$/, shell_output("#{bin}/vramsteg --now")
   end
 end
