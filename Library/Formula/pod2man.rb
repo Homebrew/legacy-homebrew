701175e8f8dc781d25542bc0efb638217f83a706
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-28 00:08:33 +0000

    pod2man: add 4.03 bottle.

--- Library/Formula/pod2man.rb
+++ Library/Formula/pod2man.rb
@@ -4,6 +4,13 @@ class Pod2man < Formula
   url "http://archives.eyrie.org/software/perl/podlators-4.03.tar.xz"
   sha256 "4694326b35f569dafc4226398b25d5c82c86ed485039effdeb4f14c2ae5b7032"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cc054b30603815cb2bb3aec5df885e16a7d93cf8140c11ded05e9ac781d25580" => :el_capitan
+    sha256 "45c095c3b709ff0588930c34749fe6990d5f73067f46140892774b14049539de" => :yosemite
+    sha256 "9e937ed310b36a4e587ae769a0237a2e2948eabbfe7c21dba00ca894084ef82a" => :mavericks
+  end
+
   keg_only :provided_by_osx
 
   def install
