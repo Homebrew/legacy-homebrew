7cb291f70a6f04450e9ba015c354ce1f4e5dfa50
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-28 08:56:53 +0000

    sngrep: update 1.2.0 bottle.

--- Library/Formula/sngrep.rb
+++ Library/Formula/sngrep.rb
@@ -5,10 +5,10 @@ class Sngrep < Formula
   sha256 "f7f9a05b4be9542ad0651d1deac3bb4cf40c9b1ebc40cfaa922d75a6997f1a7a"
 
   bottle do
-    cellar :any
-    sha256 "23b07b96e2aba627f3929c6d92ff9b1390e1a4fda71b56841d329c955b6d8407" => :yosemite
-    sha256 "73b228cd5e6cf2d8d2114375d4399b0b6cc847d1c07e00a146706c99e4cc73bc" => :mavericks
-    sha256 "d1527802672ba74c6aa5872fb6a9077b6b6f56f08f512f2de1678c317e0d39a3" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "13b6e49fd6544efef8d064f1f727b554362460ddf3ddb8f5b09ca3f7dc5e8c46" => :el_capitan
+    sha256 "29c33c51721537406e6ba9765f773682c41698c022f2d609bc222630040e4a70" => :yosemite
+    sha256 "ced61e1c9760359edf8e63983c68b9513dd68d4aa647559146336dda0dbda1e6" => :mavericks
   end
 
   depends_on "autoconf" => :build
