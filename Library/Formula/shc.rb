e7a6790be78098234aad91cecb0a200c4b8943cc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-30 22:53:42 +0000

    shc: add 3.9.2 bottle.

--- Library/Formula/shc.rb
+++ Library/Formula/shc.rb
@@ -6,6 +6,13 @@ class Shc < Formula
 
   head "https://github.com/neurobin/shc.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c4c83fbb72f572817e485f2c77e385d021fa89bff28bda69ef1650c289e4114d" => :el_capitan
+    sha256 "5d7427f85c6c9dadeea9aac1574ec9cc81376f05020f83013d1f824ba3e5a8ca" => :yosemite
+    sha256 "a0108e4234ce15d00a0e0864e5a5d36ffedf5d7cb4c68baec3251c4c3e49ebfe" => :mavericks
+  end
+
   def install
     # Fix install scripts' permissions.
     # submitted upstream: https://github.com/neurobin/shc/pull/8
