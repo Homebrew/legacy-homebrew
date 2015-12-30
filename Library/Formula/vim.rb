9ebdb28926a5568a486983ab673c6c3fb8c0661a
Drew Neil <andrew.jr.neil@gmail.com>
2015-12-30 01:31:36 +0000

    vim 7.4.998
    Closing in on patch 1000...
    
    Closes #47518.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi \"workalike\" with many additional features"
   homepage "http://www.vim.org/"
   # *** Vim should be updated no more than once every 7 days ***
-  url "https://github.com/vim/vim/archive/v7.4.979.tar.gz"
-  sha256 "b91bd610a07d6d72cb113fe06f2db722a130f8218568e0958a84c266758bd75b"
+  url "https://github.com/vim/vim/archive/v7.4.998.tar.gz"
+  sha256 "800414066506c1870212d64bc0cf868bf7fca3370d48327753bfea29d6dde403"
   head "https://github.com/vim/vim.git"
 
   bottle :disable, "To use the user's Python."
