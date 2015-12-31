067034478cd3cbf822732730b65395eff864c49f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-31 01:20:23 +0000

    libgig: add 3.3.0 bottle.

--- Library/Formula/libgig.rb
+++ Library/Formula/libgig.rb
@@ -4,6 +4,13 @@ class Libgig < Formula
   url "http://download.linuxsampler.org/packages/libgig-3.3.0.tar.bz2"
   sha256 "d22a3c7ba13d920c1d4b6c218107ad105622ae9d1236ffbce007b98547774425"
 
+  bottle do
+    cellar :any
+    sha256 "3f669e4d7c16bd6eff156c5e36c62969e68c06177a4518424dab1c7ed12e7f43" => :el_capitan
+    sha256 "b1483446d24800cffa43ff8d3fa94d0c2ee906fefbc84cf84ea93d046e58b2f4" => :yosemite
+    sha256 "52cfa0b813caee97ecbae5128f39329a0124ba60f3328c1db2bf296e839403ed" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libsndfile"
 
