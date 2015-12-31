8c7d9f8d317106aee7ec8014357e342fcf80951a
Chris Huang <tianx2@users.noreply.github.com>
2015-12-31 00:49:36 -0500

    sshuttle 0.73
    Closes #47550.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/sshuttle.rb
+++ Library/Formula/sshuttle.rb
@@ -1,17 +1,26 @@
 class Sshuttle < Formula
   desc "Proxy server that works as a poor man's VPN"
   homepage "https://github.com/sshuttle/sshuttle"
-  url "https://github.com/sshuttle/sshuttle/archive/sshuttle-0.72.tar.gz"
-  sha256 "3ea217fa98e3887b0fb2229eb65b9548a4beea1947ed5949501e5ceec360ba19"
+  url "https://github.com/sshuttle/sshuttle/archive/v0.73.tar.gz"
+  sha256 "d78a6aa76f93ab69fd9685ce11b428a9b549c049a6c0104740c06c9b354a5ae3"
   head "https://github.com/sshuttle/sshuttle.git"
 
   bottle :unneeded
+
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
     # Building the docs requires installing
     # markdown & BeautifulSoup Python modules
     # so we don't.
-    libexec.install Dir["*"]
-    bin.write_exec_script libexec/"src/sshuttle"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    shell_output("#{bin}/sshuttle -h", 97)
   end
 end
