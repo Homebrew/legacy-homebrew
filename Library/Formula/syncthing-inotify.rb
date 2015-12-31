c3d3757d582d098d3cff6043e41249b2cb8e078c
Lukáš Dvořák <dvorakluk@gmail.com>
2015-12-31 12:03:35 +0100

    syncthing-inotify 0.6.7

--- Library/Formula/syncthing-inotify.rb
+++ Library/Formula/syncthing-inotify.rb
@@ -1,10 +1,10 @@
-require "language/go"
-
 class SyncthingInotify < Formula
   desc "File watcher intended for use with Syncthing"
   homepage "https://github.com/syncthing/syncthing-inotify"
-  url "https://github.com/syncthing/syncthing-inotify/archive/v0.6.5.tar.gz"
-  sha256 "430297896bb05396268fd29cc555eba6542b42263489784c9843f4daf625ac5c"
+  url "https://github.com/syncthing/syncthing-inotify/archive/v0.6.7.tar.gz"
+  sha256 "33f51b34906548fe69b4aab2dbbb24397b523b357d4c9137324c1fddda9022b0"
+
+  head "https://github.com/syncthing/syncthing-inotify.git"
 
   bottle do
     cellar :any
@@ -15,22 +15,11 @@ class SyncthingInotify < Formula
 
   depends_on "go" => :build
 
-  go_resource "github.com/cenkalti/backoff" do
-    url "https://github.com/cenkalti/backoff.git",
-        :revision => "6c45d6bc1e78d94431dff8fc28a99f20bafa355a" # not sure !
-  end
-
-  go_resource "github.com/zillode/notify" do
-    url "https://github.com/Zillode/notify.git",
-      :revision => "f06b1e3b795091f2e1414067b08e5f07332cdb05"   # not sure !
-  end
-
   def install
     ENV["GOPATH"] = buildpath
-    ENV.append_path "PATH", buildpath
     bin_name = "syncthing-inotify"
-    Language::Go.stage_deps resources, buildpath/"src"
-    system "go", "build", "-o", bin_name
+    system "go", "get", "-d"
+    system "go", "build", "-ldflags", "-w -X main.Version #{version}", "-o", bin_name
     bin.install bin_name
   end
 
