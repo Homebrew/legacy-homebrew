ebbe58288c072a826023322d9da8eaf0c208c67b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-31 12:19:58 +0000

    tcpflow: add 1.4.5 bottle.

--- Library/Formula/tcpflow.rb
+++ Library/Formula/tcpflow.rb
@@ -4,6 +4,13 @@ class Tcpflow < Formula
   url "http://digitalcorpora.org/downloads/tcpflow/tcpflow-1.4.5.tar.gz"
   sha256 "f39fed437911b858c97937bc902f68f9a690753617abe825411a8483a7f70c72"
 
+  bottle do
+    cellar :any
+    sha256 "ea92e38288a2fea16c85b9a937951b8ecc0c5ca619ccff050d36590866543356" => :el_capitan
+    sha256 "d5e07b6218d3160b27d12e154910286af4f3edbbbc70fe5879852849a046cfae" => :yosemite
+    sha256 "b0e5f0a0e6f6fc81be55627483028a578a679d1c342a7127aa3a983983acef1a" => :mavericks
+  end
+
   head do
     url "https://github.com/simsong/tcpflow.git"
     depends_on "autoconf" => :build
