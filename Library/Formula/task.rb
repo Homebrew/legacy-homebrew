c0b5bf7958fd6b19a0eae91abcc0fd54a2956189
Misty De Meo <mistydemeo@gmail.com>
2015-12-30 22:49:30 -0400

    task: fix build on older OS Xs
    Refs #37303.
    
    Closes #47547.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- Library/Formula/task.rb
+++ Library/Formula/task.rb
@@ -19,6 +19,12 @@ class Task < Formula
 
   needs :cxx11
 
+  # Fixes the following issues:
+  # https://bug.tasktools.org/browse/TW-1748 - libc++ used unconditionally on OS X
+  # https://bug.tasktools.org/browse/TW-1749 - PATH_MAX not defined on some OS Xs
+  # https://bug.tasktools.org/browse/TW-1750 - REG_ENHANCED flag not supported on all OS Xs
+  patch :DATA
+
   def install
     args = std_cmake_args
     args << "-DENABLE_SYNC=OFF" if build.without? "gnutls"
@@ -35,3 +41,53 @@ class Task < Formula
     assert_match "Write a test", shell_output("#{bin}/task list")
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 5558f6b..f56fe45 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -43,11 +43,14 @@ else (_HAS_CXX11)
+  message (FATAL_ERROR "C++11 support missing. Try upgrading your C++ compiler. If you have a good reason for using an outdated compiler, please let us know at support@taskwarrior.org.")
+ endif (_HAS_CXX11)
+
++if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
++  set (_CXX11_FLAGS "${_CXX11_FLAGS} -stdlib=libc++")
++endif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
++
+ if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+   set (LINUX true)
+ elseif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+   set (DARWIN true)
+-  set (_CXX11_FLAGS "${_CXX11_FLAGS} -stdlib=libc++")
+ elseif (${CMAKE_SYSTEM_NAME} MATCHES "kFreeBSD")
+   set (KFREEBSD true)
+ elseif (${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
+diff --git a/src/FS.cpp b/src/FS.cpp
+index d73dd87..2f89873 100644
+--- a/src/FS.cpp
++++ b/src/FS.cpp
+@@ -46,6 +46,10 @@
+ #include <limits.h>
+ #endif
+
++#if defined __APPLE__
++#include <sys/syslimits.h>
++#endif
++
+ // Fixes build with musl libc.
+ #ifndef GLOB_TILDE
+ #define GLOB_TILDE 0
+diff --git a/src/RX.cpp b/src/RX.cpp
+index 4f0ebe9..b5e1efa 100644
+--- a/src/RX.cpp
++++ b/src/RX.cpp
+@@ -85,7 +85,7 @@ void RX::compile ()
+
+     int result;
+     if ((result = regcomp (&_regex, _pattern.c_str (),
+-#ifdef DARWIN
++#if defined REG_ENHANCED
+                            REG_ENHANCED | REG_EXTENDED | REG_NEWLINE |
+ #else
+                            REG_EXTENDED | REG_NEWLINE |
