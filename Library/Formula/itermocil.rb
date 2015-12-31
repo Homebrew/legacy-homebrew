d2ddb45d7b344fcae72727defc9d31c0de316258
Tom Anthony <git@tomanthony.co.uk>
2015-12-31 13:14:25 +0100

    Add itermocil 0.1.8

--- Library/Formula/itermocil.rb
+++ Library/Formula/itermocil.rb
@@ -1,8 +1,8 @@
 class Itermocil < Formula
   desc "iTermocil allows you setup pre-configured layouts of panes in iTerm2."
   homepage "https://github.com/TomAnthony/itermocil"
-  url "https://github.com/TomAnthony/itermocil/archive/0.1.6.tar.gz"
-  sha256 "c22dec9156cd8934713858f3efe772d9aebcba53782d53456ec6bf7040235dec"
+  url "https://github.com/TomAnthony/itermocil/archive/0.1.8.tar.gz"
+  sha256 "6ad203858734ace6ff103b89d3ee54805aeadb94dde78b29dbdb43351b122607"
 
   resource "PyYAML" do
     url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
@@ -10,15 +10,16 @@ class Itermocil < Formula
   end
 
   def install
-    resource("PyYAML").stage { system "python", *Language::Python.setup_install_args(libexec/"vendor") }
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+
     %w[PyYAML].each do |r|
       resource(r).stage do
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
 
-    ENV.prepend_create_path "PYTHONPATH", libexec
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
 
     bin.install "itermocil"
     bin.install "itermocil.py"
