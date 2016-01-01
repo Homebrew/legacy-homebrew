a9ed940692cb4c279c6c72d5bfedb084cbc3633f
castor4bit <castor.4bit@gmail.com>
2016-01-02 02:35:13 +0900

    apache-drill 1.4.0
    Closes #47592.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/apache-drill.rb
+++ Library/Formula/apache-drill.rb
@@ -1,9 +1,9 @@
 class ApacheDrill < Formula
   desc "Schema-free SQL query engine for Hadoop and NoSQL"
   homepage "https://drill.apache.org/download/"
-  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.2.0/apache-drill-1.2.0.tar.gz"
-  mirror "http://getdrill.org/drill/download/apache-drill-1.2.0.tar.gz"
-  sha256 "81e1b6e41efa9340ad45c18e6db6c5dcb14eef4fecc8959fc6ac328083e6182a"
+  url "https://www.apache.org/dyn/closer.cgi?path=drill/drill-1.4.0/apache-drill-1.4.0.tar.gz"
+  mirror "http://getdrill.org/drill/download/apache-drill-1.4.0.tar.gz"
+  sha256 "4ffe85865c8e2c453ba0fdb9f701af556836a0293b8d78c214a51ef81f3844ea"
 
   bottle :unneeded
 
