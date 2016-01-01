c969199df267238cd254a57fac95781fd9badb77
Brian van Burken <brianvanburken@outlook.com>
2016-01-01 12:14:42 +0100

    vim 7.4.1016
    Updated vim to v7.4.1016.
    
    Closes #47583.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi \"workalike\" with many additional features"
   homepage "http://www.vim.org/"
   # *** Vim should be updated no more than once every 7 days ***
-  url "https://github.com/vim/vim/archive/v7.4.998.tar.gz"
-  sha256 "800414066506c1870212d64bc0cf868bf7fca3370d48327753bfea29d6dde403"
+  url "https://github.com/vim/vim/archive/v7.4.1016.tar.gz"
+  sha256 "df486e9e8b23d62daaf4f32220884ebb59f5f8f73b6ec3baef42e533561f4989"
   head "https://github.com/vim/vim.git"
 
   bottle :disable, "To use the user's Python."
