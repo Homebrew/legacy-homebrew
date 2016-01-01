e92489a3056d0f07ca6e79627039a40546633436
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-01 13:56:30 +0100

    zsh-completion: don't hardcode /usr/local

--- Library/Formula/zsh-completions.rb
+++ Library/Formula/zsh-completions.rb
@@ -25,7 +25,7 @@ class ZshCompletions < Formula
     Additionally, if you receive "zsh compinit: insecure directories" warnings when attempting
     to load these completions, you may need to run this:
 
-      chmod go-w /usr/local/share
+      chmod go-w '#{HOMEBREW_PREFIX}/share'
     EOS
   end
 
