b74d21b9c45ae8def0482b7655e54b2c2ee20736
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2016-01-02 07:14:29 +0100

    z3: Fix audit problems
    Moved the bottle block before options and removed unneeded variable

--- Library/Formula/z3.rb
+++ Library/Formula/z3.rb
@@ -5,6 +5,13 @@ class Z3 < Formula
   sha256 "50967cca12c5c6e1612d0ccf8b6ebf5f99840a783d6cf5216336a2b59c37c0ce"
   head "https://github.com/Z3Prover/z3.git"
 
+  bottle do
+    cellar :any
+    sha256 "ea169ccefdbebdd17213b4fab603dce2029b03bde0b62fa98920cbaf431d4771" => :el_capitan
+    sha256 "e8f726245f283d43efe68f2516ebf1fc62fd2ab486a850befc0c388ef9f5c1ed" => :yosemite
+    sha256 "2c67f6d604e3b478bac87e223891f3252a8f29a048564a91a8ea57f6c3b9a8ba" => :mavericks
+  end
+
   option "without-python", "Build without python 2 support"
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
   depends_on :python3 => :optional
@@ -13,17 +20,10 @@ class Z3 < Formula
     odie "z3: --with-python3 must be specified when using --without-python"
   end
 
-  bottle do
-    cellar :any
-    sha256 "ea169ccefdbebdd17213b4fab603dce2029b03bde0b62fa98920cbaf431d4771" => :el_capitan
-    sha256 "e8f726245f283d43efe68f2516ebf1fc62fd2ab486a850befc0c388ef9f5c1ed" => :yosemite
-    sha256 "2c67f6d604e3b478bac87e223891f3252a8f29a048564a91a8ea57f6c3b9a8ba" => :mavericks
-  end
-
   def install
     inreplace "scripts/mk_util.py", "dist-packages", "site-packages"
 
-    Language::Python.each_python(build) do |python, version|
+    Language::Python.each_python(build) do |python, _|
       system python, "scripts/mk_make.py", "--prefix=#{prefix}"
       cd "build" do
         system "make"
