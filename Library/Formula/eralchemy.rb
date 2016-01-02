3a58c0e333e3100b96680d667298a55e3beaad73
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-02 12:43:35 +0000

    eralchemy: add 1.0.1 bottle.

--- Library/Formula/eralchemy.rb
+++ Library/Formula/eralchemy.rb
@@ -4,6 +4,13 @@ class Eralchemy < Formula
   url "https://pypi.python.org/packages/source/E/ERAlchemy/ERAlchemy-1.0.1.tar.gz"
   sha256 "98425b21fe0f12ff33e72789c8a308baa1c2d5fdf157388162deaa510551da4f"
 
+  bottle do
+    cellar :any
+    sha256 "53e20a26f69a3530b499e540a9e33eeb4cf4a125625c0ff9369204c540192eff" => :el_capitan
+    sha256 "0d84f7a2fdf7985630487934b280205a603ec591a38c0068921b0be4a1ccc0a2" => :yosemite
+    sha256 "9fe62f2286c31683490a44275f99cc92f79ce449b58099f48b1801bd2b9c8e7e" => :mavericks
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "graphviz"
   depends_on "pkg-config" => :build
