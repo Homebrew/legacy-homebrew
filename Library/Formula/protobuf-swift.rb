3c68bff52e376e41e8b816205de4ffa3933c81ba
Alexey Khokhlov <alexeyxo@gmail.com>
2016-01-03 15:06:15 +0300

    protobuf-swift 2.4
    protobuf-swift 2.4 (update formula)
    
    Closes #47626.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/protobuf-swift.rb
+++ Library/Formula/protobuf-swift.rb
@@ -1,8 +1,8 @@
 class ProtobufSwift < Formula
-  desc "Implementation of Protocol Buffers in Swift."
+  desc "Implementation of Protocol Buffers in Apple Swift."
   homepage "https://github.com/alexeyxo/protobuf-swift"
-  url "https://github.com/alexeyxo/protobuf-swift/archive/2.2.tar.gz"
-  sha256 "652173269234f2c6bbe37b85d8772823a323f5379cef48fa33a96237c1148682"
+  url "https://github.com/alexeyxo/protobuf-swift/archive/2.4.tar.gz"
+  sha256 "1395483f2b31b7beebe1578cd5fdd3d80441fe0ac583ae18a33f378615804e4d"
 
   bottle do
     cellar :any
