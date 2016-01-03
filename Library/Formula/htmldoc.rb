79b0433408d623e7b9a2b2c6ce6c310cff1d9fe3
Tomasz Pajor <tomek@polishgeeks.com>
2016-01-03 14:04:16 +0100

    htmldoc 1.8.29
    Closes #47628.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/htmldoc.rb
+++ Library/Formula/htmldoc.rb
@@ -1,16 +1,11 @@
 class Htmldoc < Formula
   desc "Convert HTML to PDF or PostScript"
   homepage "https://www.msweet.org/projects.php?Z1"
-  url "https://www.msweet.org/files/project1/htmldoc-1.8.28-source.tar.bz2"
-  sha256 "2a688bd820ad6f7bdebb274716102dafbf4d5fcfa20a5b8d87a56b030d184732"
-  revision 1
+  url "https://www.msweet.org/files/project1/htmldoc-1.8.29-source.tar.bz2"
+  sha256 "e8c96ad740d19169eab8305c8e2ee1c795c4afa59ba99d18786ad191a2853f31"
 
   depends_on "libpng"
   depends_on "jpeg"
-
-  # El Cap Security Framework changes silently break the `htmldoc` compile.
-  # https://github.com/Homebrew/homebrew/issues/45334
-  depends_on MaximumMacOSRequirement => :yosemite
 
   def install
     system "./configure", "--disable-debug",
