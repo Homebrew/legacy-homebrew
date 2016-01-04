2d77c3fb1bd6c66bfd5be68b0234bf7a33ce46ad
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-04 03:14:08 +0000

    abcde: don't patch head

--- Library/Formula/abcde.rb
+++ Library/Formula/abcde.rb
@@ -1,11 +1,25 @@
 class Abcde < Formula
   desc "Better CD Encoder"
   homepage "http://abcde.einval.com"
-  url "http://abcde.einval.com/download/abcde-2.7.1.tar.gz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/a/abcde/abcde_2.7.1.orig.tar.gz"
-  sha256 "3401e39785b20edee843d4d875b47d2b559f764681c482c4491a8c8ba605f250"
   revision 1
+
   head "http://git.einval.com/git/abcde.git"
+
+  stable do
+    url "http://abcde.einval.com/download/abcde-2.7.1.tar.gz"
+    mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/a/abcde/abcde_2.7.1.orig.tar.gz"
+    sha256 "3401e39785b20edee843d4d875b47d2b559f764681c482c4491a8c8ba605f250"
+
+    # Fix "Expansion of $REDIR quoted on MacOSX breaks cp" bug
+    # (http://abcde.einval.com/bugzilla/show_bug.cgi?id=22)
+    # Obsolete once a new version is released as upstream already merged this
+    # patch in c024365a846faae390f23c86f32235d6209e6edf
+    # (http://git.einval.com/cgi-bin/gitweb.cgi?p=abcde.git;a=commit;h=c024365a846faae390f23c86f32235d6209e6edf)
+    patch do
+      url "http://abcde.einval.com/bugzilla/attachment.cgi?id=12"
+      sha256 "4620dd5ef7ab32b6511782da85831661ccee292fadff1f58acc3b4992486af62"
+    end
+  end
 
   bottle do
     cellar :any_skip_relocation
@@ -22,16 +36,6 @@ class Abcde < Formula
   depends_on "lame" => :optional
   depends_on "vorbis-tools" => :optional
   depends_on "glyr" => :optional
-
-  # Fix "Expansion of $REDIR quoted on MacOSX breaks cp" bug
-  # (http://abcde.einval.com/bugzilla/show_bug.cgi?id=22)
-  # Obsolete once a new version is released as upstream already merged this
-  # patch in c024365a846faae390f23c86f32235d6209e6edf
-  # (http://git.einval.com/cgi-bin/gitweb.cgi?p=abcde.git;a=commit;h=c024365a846faae390f23c86f32235d6209e6edf)
-  patch do
-    url "http://abcde.einval.com/bugzilla/attachment.cgi?id=12"
-    sha256 "4620dd5ef7ab32b6511782da85831661ccee292fadff1f58acc3b4992486af62"
-  end
 
   def install
     system "make", "install", "prefix=#{prefix}", "etcdir=#{etc}"
