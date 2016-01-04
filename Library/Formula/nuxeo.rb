6e94da026e293fa9f9227162e98d372680b7119d
Arnaud Kervern <akervern@nuxeo.com>
2016-01-04 14:46:39 +0100

    Use write_env_script method to generate wrapper

--- Library/Formula/nuxeo.rb
+++ Library/Formula/nuxeo.rb
@@ -17,14 +17,9 @@ class Nuxeo < Formula
   def install
     libexec.install Dir["#{buildpath}/*"]
 
-    bin.install_symlink "#{libexec}/bin/nuxeoctl"
+    (bin/"nuxeoctl").write_env_script "#{libexec}/bin/nuxeoctl",
+      {:NUXEO_HOME => "#{libexec}", :NUXEO_CONF => "#{etc}/nuxeo.conf"}
 
-    # TODO: https://jira.nuxeo.com/browse/NXBT-966 Use a wrapper
-    inreplace "#{libexec}/bin/nuxeoctl" do |s|
-      # See https://jira.nuxeo.com/browse/NXP-18418 nuxeoctl symlink is not correctly resolved and breaks NUXEO_HOME resolution.
-      s.gsub! /\${NUXEO_HOME:=.*$/, '${NUXEO_HOME:=$(cd "$(dirname `PRG=$0; while [ -h "$PRG" ] ;do ls=\\\\`ls -ld "$PRG"\\\\`; link=\\\\`expr "$ls" : ".*-> \\\\(.*\)\$"\\\\`; if expr "$link" : "/.*" &> /dev/null; then PRG="$link"; else PRG="\\\\`dirname "$PRG"\\\\`/$link"; fi; done; echo $PRG`)"/..; pwd -P)}'
-      s.gsub! /\${NUXEO_CONF:=.*$/, "${NUXEO_CONF:=\"#{etc}/nuxeo.conf\"}"
-    end
 
     inreplace "#{libexec}/bin/nuxeo.conf" do |s|
       s.gsub! /#nuxeo\.log\.dir.*/, "nuxeo.log.dir=#{var}/log/nuxeo"
@@ -46,14 +41,7 @@ class Nuxeo < Formula
   end
 
   test do
-    # Copy configuration file to test path, due to some automatic writes on it.
-    cp "#{etc}/nuxeo.conf", "#{testpath}/nuxeo.conf"
-    inreplace "#{testpath}/nuxeo.conf" do |s|
-      s.gsub! /#{var}/, testpath
-    end
-    ENV["NUXEO_CONF"] = "#{testpath}/nuxeo.conf"
-
-    assert_equal "#{testpath}/nuxeo.conf", shell_output("#{bin}/nuxeoctl config -q --get nuxeo.conf").strip
+    assert_equal "#{etc}/nuxeo.conf", shell_output("#{bin}/nuxeoctl config -q --get nuxeo.conf").strip
     assert_equal "#{libexec}", shell_output("#{bin}/nuxeoctl config -q --get nuxeo.home").strip
   end
 end
