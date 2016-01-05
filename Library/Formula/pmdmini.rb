40a89b75694ffc3f50905c82d1d698ffe71dceb7
Misty De Meo <mistydemeo@gmail.com>
2016-01-05 18:23:09 -0400

    pmdmini 1.0.0

--- Library/Formula/pmdmini.rb
+++ Library/Formula/pmdmini.rb
@@ -1,31 +1,29 @@
 class Pmdmini < Formula
   desc "Plays music in PC-88/98 PMD chiptune format"
   homepage "https://github.com/BouKiCHi/mdxplayer"
-  url "https://github.com/BouKiCHi/mdxplayer/archive/aa55d9d3128f06aac4a15d5cefc083bd7b66d814.tar.gz"
-  version "20120115"
-  sha256 "df886dd2dd65f40860bba7d80db8273992f27fcd3f3aedf613c4da3a6336ff64"
+  url "https://github.com/mistydemeo/pmdmini/archive/v1.0.0.tar.gz"
+  sha256 "526cb2be1a7e32be9782908cbaeae89b3aca20cad8e42f238916ce9b6d17679c"
 
-  option "lib-only", "Do not build commandline player"
+  option "with-lib-only", "Do not build commandline player"
+  deprecated_option "lib-only" => "with-lib-only"
 
-  depends_on "sdl" unless build.include? "lib-only"
+  depends_on "sdl" unless build.without? "lib-only"
 
   def install
-    cd "jni/pmdmini" do
-      # Specify Homebrew's cc
-      inreplace "mak/general.mak", "gcc", ENV.cc
-      if build.include? "lib-only"
-        system "make", "-f", "Makefile.lib"
-      else
-        system "make"
-      end
-
-      # Makefile doesn't build a dylib
-      system "#{ENV.cc} -dynamiclib -install_name #{lib}/libpmdmini.dylib -o libpmdmini.dylib -undefined dynamic_lookup obj/*.o"
-
-      bin.install "pmdplay" unless build.include? "lib-only"
-      lib.install "libpmdmini.a", "libpmdmini.dylib"
-      (include+"libpmdmini").install Dir["src/*.h"]
-      (include+"libpmdmini/pmdwin").install Dir["src/pmdwin/*.h"]
+    # Specify Homebrew's cc
+    inreplace "mak/general.mak", "gcc", ENV.cc
+    if build.with? "lib-only"
+      system "make", "-f", "Makefile.lib"
+    else
+      system "make"
     end
+
+    # Makefile doesn't build a dylib
+    system "#{ENV.cc} -dynamiclib -install_name #{lib}/libpmdmini.dylib -o libpmdmini.dylib -undefined dynamic_lookup obj/*.o"
+
+    bin.install "pmdplay" unless build.with? "lib-only"
+    lib.install "libpmdmini.a", "libpmdmini.dylib"
+    (include+"libpmdmini").install Dir["src/*.h"]
+    (include+"libpmdmini/pmdwin").install Dir["src/pmdwin/*.h"]
   end
 end
