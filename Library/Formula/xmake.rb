34c2ce9b565fa088f9da1c763ce6d9360a0c9afb
ruki <waruqi@gmail.com>
2016-01-05 19:11:48 +0800

    xmake: add v1.0.4

---
+++ Library/Formula/xmake.rb
@@ -0,0 +1,15 @@
+class Xmake < Formula
+  desc "The Automatic Cross-platform Build Tool"
+  homepage "https://github.com/waruqi/xmake"
+  url "https://github.com/waruqi/xmake/archive/v1.0.4.tar.gz"
+  mirror "http://tboox.net/release/xmake/xmake-1.0.4.tar.gz"
+  sha256 "17527514245803a1fc46a6261fd73086022482e19645601c3d187cda96331321"
+
+  def install
+    system "./install"
+  end
+
+  test do
+    system "xmake", "--help"
+  end
+end
