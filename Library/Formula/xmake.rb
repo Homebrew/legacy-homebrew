93e26b8aa42c68addac95ee7e7cd91aec4227375
ruki <waruqi@gmail.com>
2016-01-05 20:31:37 +0800

    xmake: audit fixes

--- Library/Formula/xmake.rb
+++ Library/Formula/xmake.rb
@@ -3,13 +3,13 @@ class Xmake < Formula
   homepage "https://github.com/waruqi/xmake"
   url "https://github.com/waruqi/xmake/archive/v1.0.4.tar.gz"
   mirror "http://tboox.net/release/xmake/xmake-1.0.4.tar.gz"
-  sha256 "17527514245803a1fc46a6261fd73086022482e19645601c3d187cda96331321"
+  sha256 "79c521e6096b213c36184d27c9096e6db43c70c1d58c7bfd67a19951ccdce7c0"
 
   def install
-    system "./install"
+    system "./install", "#{prefix}"
   end
 
   test do
-    system "xmake", "--help"
+    system "#{bin}/xmake", "--help"
   end
 end
