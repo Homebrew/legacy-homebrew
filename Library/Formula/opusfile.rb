f392a01bab7b4242a8ea4de40c1f6572e224c10b
Ralph Giles <giles@mozilla.com>
2016-01-05 08:23:41 -0800

    opusfile 0.7
    Closes #47724.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/opusfile.rb
+++ Library/Formula/opusfile.rb
@@ -1,8 +1,8 @@
 class Opusfile < Formula
   desc "API for decoding and seeking in .opus files"
   homepage "https://www.opus-codec.org/"
-  url "http://downloads.xiph.org/releases/opus/opusfile-0.6.tar.gz"
-  sha256 "2428717b356e139f18ed2fdb5ad990b5654a238907a0058200b39c46a7d03ea6"
+  url "https://archive.mozilla.org/pub/opus/opusfile-0.7.tar.gz"
+  sha256 "9e2bed13bc729058591a0f1cab2505e8cfd8e7ac460bf10a78bcc3b125e7c301"
 
   bottle do
     cellar :any
