d56a891eeb1f6ce5f7bc64eb665b685ac2b7e1e1
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-06 23:02:01 +0000

    node 5.4.0
    Closes #47790.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Library/Formula/node.rb
+++ Library/Formula/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Platform built on the V8 JavaScript runtime to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v5.3.0/node-v5.3.0.tar.gz"
-  sha256 "cc05ff06149c638345835788f448471d264a7e011bf083394f86d5be51975c7e"
+  url "https://nodejs.org/dist/v5.4.0/node-v5.4.0.tar.gz"
+  sha256 "1dfe37a00cf0ed62beb73071f571ac56697f544a98cc2ff3318faec6363d72ab"
   head "https://github.com/nodejs/node.git"
 
   bottle do
