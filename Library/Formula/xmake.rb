dd692ee7a0cf753c2f0bed6dd36ffb62f7f910be
ruki <waruqi@gmail.com>
2016-01-06 12:00:49 +0800

    add head url

--- Library/Formula/xmake.rb
+++ Library/Formula/xmake.rb
@@ -4,6 +4,7 @@ class Xmake < Formula
   url "https://github.com/waruqi/xmake/archive/v1.0.4.tar.gz"
   mirror "http://tboox.net/release/xmake/xmake-1.0.4.tar.gz"
   sha256 "0fb8b71ed1af4db2ad154a7a50386bf66de54f548a171c9d730c115641f4466f"
+  head "https://github.com/waruqi/xmake.git"
 
   def install
     system "./install", prefix
