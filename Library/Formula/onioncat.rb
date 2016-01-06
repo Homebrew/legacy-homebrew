380181b771338f84f65c708d1833bda8e7a87a90
Greg V <greg@unrelenting.technology>
2016-01-06 22:02:00 +0300

    Add onioncat

---
+++ Library/Formula/onioncat.rb
@@ -0,0 +1,19 @@
+class Onioncat < Formula
+  desc "A VPN-adapter that provides location privacy using Tor or I2P"
+  homepage "https://www.onioncat.org"
+  url "https://www.cypherpunk.at/ocat/download/Source/current/onioncat-0.2.2.r569.tar.gz"
+  sha256 "377777de0d3c731fd2253db02b25562a2ed17e82901d0569308754215223f0bb"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    rm_f "#{bin}/gcat" # just a symlink that does the same as ocat -I
+  end
+
+  test do
+    system "#{bin}/ocat", "-i", "fncuwbiisyh6ak3i.onion" # convert keybase's address to IPv6 address format
+  end
+end
