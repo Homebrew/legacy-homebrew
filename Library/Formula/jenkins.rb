97efe1f2204406367518bcbcd32b7a8345fe5db5
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-06 18:14:57 +0000

    jenkins: sandbox head compile

--- Library/Formula/jenkins.rb
+++ Library/Formula/jenkins.rb
@@ -15,6 +15,7 @@ class Jenkins < Formula
 
   def install
     if build.head?
+      ENV.java_cache
       system "mvn", "clean", "install", "-pl", "war", "-am", "-DskipTests"
     else
       system "jar", "xvf", "jenkins.war"
