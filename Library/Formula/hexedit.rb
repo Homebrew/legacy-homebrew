6d602ff5ed4eb3cfdf0acaca445bd13917a63107
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-06 01:58:04 +0000

    hexedit: update 1.2.13 bottle.

--- Library/Formula/hexedit.rb
+++ Library/Formula/hexedit.rb
@@ -7,10 +7,11 @@ class Hexedit < Formula
   sha256 "6a126da30a77f5c0b08038aa7a881d910e3b65d13767fb54c58c983963b88dd7"
 
   bottle do
-    cellar :any
-    sha256 "4fd961580544c94e94e0d8099a429d94b278cf29832c8959f1f6d6eedbe59cbf" => :yosemite
-    sha256 "2c1122682c502ffb24957f6a76da4d616b72e592ae1b4f8eedf3079f143c81a9" => :mavericks
-    sha256 "f6627519d855ecb6cbe3dac2b4b770ee191c4f12e001534162b3c5f2ca188ba4" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "078d2bd1b7fd56db28b0b4d972826aedd117e810d9a885b5b2545cd8e5e5ccd5" => :el_capitan
+    sha256 "4f06836e7a2f4a280084fe8f9f5ff3903272a6e9995f24bf93156afc56d7b996" => :yosemite
+    sha256 "1931661462fffa57fb8b0b6b7cb3c4439ed72b93f5a0a6db94e9bb2f5fa1cd4d" => :mavericks
   end
 
   def install
