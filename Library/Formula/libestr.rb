fc9f9a1d7592188778af40c0811713c3f2c949c4
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-06 12:59:30 +0100

    libestr 0.1.10
    Closes #47764.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/libestr.rb
+++ Library/Formula/libestr.rb
@@ -1,8 +1,8 @@
 class Libestr < Formula
   desc "C library for string handling (and a bit more)"
   homepage "http://libestr.adiscon.com"
-  url "http://libestr.adiscon.com/files/download/libestr-0.1.9.tar.gz"
-  sha256 "822c6e2d01eaca1e72201f403a2ca01f3e86410b880e508e5204e3c2694d751a"
+  url "http://libestr.adiscon.com/files/download/libestr-0.1.10.tar.gz"
+  sha256 "bd655e126e750edd18544b88eb1568d200a424a0c23f665eb14bbece07ac703c"
 
   bottle do
     cellar :any
@@ -12,14 +12,27 @@ class Libestr < Formula
     sha256 "99af2bd65207f3f4b7ed2250cdc1cdf8fd01868259d251714851d93dfee1581b" => :mountain_lion
   end
 
-  depends_on "pkg-config" => :build
-
   option :universal
+
+  depends_on "pkg-config" => :build
 
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include "stdio.h"
+      #include <libestr.h>
+      int main() {
+        printf("%s\\n", es_version());
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lestr", "-o", "test"
+    system "./test"
   end
 end
