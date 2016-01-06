fbc7763a3a74e9d64b44f31a1f54dfbdf95e60af
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-06 01:00:51 +0000

    apktool: bottle unneeded

--- Library/Formula/apktool.rb
+++ Library/Formula/apktool.rb
@@ -4,12 +4,7 @@ class Apktool < Formula
   url "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.0.3.jar", :using => :nounzip
   sha256 "ceca3e49bfc697c918aaff160c28244f27a2c1c741af62a70e1558a066a16dd3"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "a7a126b2f51d9bb040d4ed3e65927af3c59d729ae5ff4db632ae99d5b1360a78" => :el_capitan
-    sha256 "e381c8bfa9be3f6c0698a8ae55d8be96e0d68acf372d410b3e5460122a3933ea" => :yosemite
-    sha256 "07acb5961b0ee3174ed381157cfc38e7644cb8582c207a519e00ef41e7c4b74b" => :mavericks
-  end
+  bottle :unneeded
 
   resource "sample.apk" do
     url "https://github.com/downloads/stephanenicolas/RoboDemo/robodemo-sample-1.0.1.apk", :using => :nounzip
