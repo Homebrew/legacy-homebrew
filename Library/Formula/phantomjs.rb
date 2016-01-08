bedfc91747bd202d0defd3a4a78085c1ce061d6c
Mike McQuaid <mike@mikemcquaid.com>
2016-01-08 10:19:13 +0000

    phantomjs 2.0.1
    Use Vitallium's fork temporarily until 2.1.0 gets released.

--- Library/Formula/phantomjs.rb
+++ Library/Formula/phantomjs.rb
@@ -2,22 +2,9 @@ class Phantomjs < Formula
   desc "Headless WebKit scriptable with a JavaScript API"
   homepage "http://phantomjs.org/"
   head "https://github.com/ariya/phantomjs.git"
-
-  stable do
-    url "https://github.com/ariya/phantomjs/archive/2.0.0.tar.gz"
-    sha256 "0a1338464ca37314037d139b3e0f7368325f5d8810628d9d9f2df9f9f535d407"
-
-    # https://github.com/Homebrew/homebrew/issues/42249
-    depends_on MaximumMacOSRequirement => :yosemite
-
-    # Qt Yosemite build fix. Upstream commit/PR:
-    # https://qt.gitorious.org/qt/qtbase/commit/70e442
-    # https://github.com/ariya/phantomjs/pull/12934
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/patches/480b7142c4e2ae07de6028f672695eb927a34875/phantomjs/yosemite.patch"
-      sha256 "f54bd1592185f031552d3ad5c8809ff27e8f3be4f1c05c81b59bf7dbc4a59de1"
-    end
-  end
+  # Temporarily use Vitallium's fork (who is a maintainer) until 2.1.0.
+  url "https://github.com/Vitallium/phantomjs.git", :tag => "2.0.1",
+    :revision => "33aaaff64a197b20076faab1b08b8757516aa976"
 
   bottle do
     cellar :any
@@ -27,8 +14,7 @@ class Phantomjs < Formula
   end
 
   def install
-    system "./build.sh", "--confirm", "--jobs", ENV.make_jobs,
-      "--qt-config", "-openssl-linked"
+    system "./build.sh", "--confirm", "--jobs", ENV.make_jobs
     bin.install "bin/phantomjs"
     (share+"phantomjs").install "examples"
   end
