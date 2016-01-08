5bdc814d9e0c7b5ec9fc8bea5102f9a695228f65
ruki <waruqi@gmail.com>
2016-01-08 10:28:15 +0800

    xmake: modify sha256

--- Library/Formula/xmake.rb
+++ Library/Formula/xmake.rb
@@ -3,20 +3,20 @@ class Xmake < Formula
   homepage "https://github.com/waruqi/xmake"
   url "https://github.com/waruqi/xmake/archive/v1.0.4.tar.gz"
   mirror "http://tboox.net/release/xmake/xmake-v1.0.4.tar.gz"
-  sha256 "adb4714ce89d6c9fcb24f7ae641ac9221d4172d728add441e29a6453a908f76f"
+  sha256 "9d55ca647f6f00bebdb6d9499c6607b71cfa8f42b6539bc0d17a472108f89e60"
   head "https://github.com/waruqi/xmake.git"
 
   def install
     # install to the output directory first
-    system "./install", "./output"
+    system "./install", "output"
 
-    # install the xmake directory
+    # install the xmake scripts
     share.mkdir
     (share/"xmake").mkdir
-    (share/"xmake").install Dir["./xmake/*"]
+    (share/"xmake").install Dir["xmake/*"]
 
     # install the xmake binary
-    libexec.install "./output/share/xmake/xmake"
+    libexec.install "output/share/xmake/xmake"
     bin.install Dir[libexec/"*"]
     bin.env_script_all_files(libexec, :XMAKE_PROGRAM_DIR =>"#{share}/xmake")
   end
