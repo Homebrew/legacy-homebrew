9ae0dec6c81cb7314a0ee3392d9f93e525887683
rpau <rpau@ac.upc.edu>
2016-01-08 10:53:45 +0100

    walkmod: 2.0.2

--- Library/Formula/walkmod.rb
+++ Library/Formula/walkmod.rb
@@ -1,8 +1,8 @@
 class Walkmod < Formula
   desc "Java-based project to apply and share code conventions"
   homepage "http://www.walkmod.com"
-  url "https://bitbucket.org/rpau/walkmod/downloads/walkmod-2.0.2-installer.zip"
-  sha256 "ba4cdb2962902c13ee793d41b43ffe605f1e2806e8f64b6fb227c65cf4d365b3"
+  url "https://bitbucket.org/rpau/walkmod/downloads/walkmod-2.2.0-installer.zip"
+  sha256 "923a0956bfefef6f083f63a8696ee5019b19d2798692c6d456c1557078ed37d6"
 
   bottle :unneeded
 
