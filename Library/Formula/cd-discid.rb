07a39abaec405cc732a8fc5b0a51279041128fa1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-09 04:25:58 +0000

    cd-discid: update 1.4_2 bottle.

--- Library/Formula/cd-discid.rb
+++ Library/Formula/cd-discid.rb
@@ -15,9 +15,9 @@ class CdDiscid < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5f8552d8d7c590fedc966ceecfd5f9840d63e0760c575759f4ba2fcffdd54fd9" => :el_capitan
-    sha256 "cb22bb523253be82c72aadd9b016cdbb74e9f861fd109d9a3e0f78a611a306a3" => :yosemite
-    sha256 "1b18004d18aa98eacb9eb638c0bce844e57ea277c551965843eefe2378ef315d" => :mavericks
+    sha256 "f0c17cfc3c345c661104a6f29562b766cac2a80747feea0c26cda04ece3c8326" => :el_capitan
+    sha256 "3331be095997a1e5e6acb9f82f5e5473ed51c0f35976229371dc1d0c703c2e3b" => :yosemite
+    sha256 "86f0066d344a2a0a37e3c00d08255d4a505b41cc2c38e7d33ac643d16af8ad71" => :mavericks
   end
 
   def install
