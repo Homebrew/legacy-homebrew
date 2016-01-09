f75ec8619e13529d592014aca422fc835d9a98fc
Tomasz Pajor <tomek@polishgeeks.com>
2016-01-09 13:18:47 +0100

    libxmp-lite 4.3.10
    Closes #47895.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/libxmp-lite.rb
+++ Library/Formula/libxmp-lite.rb
@@ -1,8 +1,8 @@
 class LibxmpLite < Formula
   desc "Lite libxmp"
   homepage "http://xmp.sourceforge.net"
-  url "https://downloads.sourceforge.net/project/xmp/libxmp/4.3.5/libxmp-lite-4.3.5.tar.gz"
-  sha256 "e3ce0f3099432afefa2459c8de37958ff95daeb231ef97b7df10a58fa1e416da"
+  url "https://downloads.sourceforge.net/project/xmp/libxmp/4.3.10/libxmp-lite-4.3.10.tar.gz"
+  sha256 "039fc58e32cd6a0cc4fdc4bfc62ed23cfad87e45d3d2b95e91df4ac477628cbd"
 
   def install
     system "./configure", "--prefix=#{prefix}"
