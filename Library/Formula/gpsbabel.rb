15b99684a5862a088e650fc786c5e14edbac44cc
Johnathan Conley <johnathan.conley@gmail.com>
2016-01-09 17:31:56 -0600

    gpsbabel 1.5.3
    Closes #47903.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/gpsbabel.rb
+++ Library/Formula/gpsbabel.rb
@@ -1,10 +1,10 @@
 class Gpsbabel < Formula
   desc "GPSBabel converts/uploads GPS waypoints, tracks, and routes"
   homepage "https://www.gpsbabel.org/"
-  url "http://gpsbabel.googlecode.com/svn/trunk/gpsbabel", :revision => "4962"
-  version "1.5.2"
+  url "https://github.com/gpsbabel/gpsbabel/archive/gpsbabel_1_5_3.tar.gz"
+  sha256 "10b7aaca44ce557fa1175fec37297b8df55611ab2c51cb199753a22dbf2d3997"
 
-  head "http://gpsbabel.googlecode.com/svn/trunk/gpsbabel"
+  head "https://github.com/gpsbabel/gpsbabel.git"
 
   bottle do
     sha256 "029bee003e90047bfe0d68958e72c1aa638d8eb57aa70a176ef0585d71a9a548" => :yosemite
@@ -17,7 +17,7 @@ class Gpsbabel < Formula
 
   def install
     args = ["--disable-debug", "--disable-dependency-tracking",
-            "--prefix=#{prefix}", "--with-zlib=system"]
+            "--prefix=#{prefix}"]
     args << "--without-libusb" if build.without? "libusb"
     system "./configure", *args
     system "make", "install"
