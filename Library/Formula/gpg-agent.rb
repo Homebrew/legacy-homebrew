3addb2d94b1bba5ccf7fafd0a9e9e7121bd8fa13
Koichi Shiraishi <zchee.io@gmail.com>
2016-01-09 15:05:11 +0900

    Upgrade gpg-agent to 2.1.9

--- Library/Formula/gpg-agent.rb
+++ Library/Formula/gpg-agent.rb
@@ -1,9 +1,9 @@
 class GpgAgent < Formula
   desc "GPG key agent"
   homepage "https://www.gnupg.org/"
-  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.0.29.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnupg/gnupg-2.0.29.tar.bz2"
-  sha256 "68ed6b386ba78425b05a60e8ee22785ff0fef190bdc6f1c612f19a58819d4ac9"
+  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.9.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.9.tar.bz2"
+  sha256 "1cb7633a57190beb66f9249cb7446603229b273d4d89331b75c652fa4a29f7b6"
 
   bottle do
     sha256 "9bebbc754c440be3ac5eada58600a2843d7922aed2a3c68a3329b5a94fbf6871" => :el_capitan
@@ -16,7 +16,7 @@ class GpgAgent < Formula
   depends_on "libgcrypt"
   depends_on "libksba"
   depends_on "libassuan"
-  depends_on "pth"
+  depends_on "npth"
   depends_on "pinentry"
 
   # Adjust package name to fit our scheme of packaging both
@@ -48,7 +48,7 @@ end
 
 __END__
 diff --git a/configure b/configure
-index c022805..96ea7ed 100755
+index 2d9dbf1..e2904dc 100755
 --- a/configure
 +++ b/configure
 @@ -578,8 +578,8 @@ MFLAGS=
@@ -59,6 +59,6 @@ index c022805..96ea7ed 100755
 -PACKAGE_TARNAME='gnupg'
 +PACKAGE_NAME='gpg-agent'
 +PACKAGE_TARNAME='gpg-agent'
- PACKAGE_VERSION='2.0.29'
- PACKAGE_STRING='gnupg 2.0.29'
+ PACKAGE_VERSION='2.1.9'
+ PACKAGE_STRING='gnupg 2.1.9'
  PACKAGE_BUGREPORT='http://bugs.gnupg.org'
