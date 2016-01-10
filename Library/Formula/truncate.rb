a2f1e9b46a72081d5171b03bf3cb5e619b9d118f
Yuuki Galaxy <galaxy001@gmail.com>
2016-01-10 19:32:09 +0800

    Now use the github source. And I set the new version to 0.9 .

--- Library/Formula/truncate.rb
+++ Library/Formula/truncate.rb
@@ -1,10 +1,10 @@
 class Truncate < Formula
   desc "truncates a file to a given size"
   homepage "http://www.vanheusden.com/truncate"
-  url "http://www.vanheusden.com/truncate/truncate-0.8.tgz"
-  sha256 "165914d643351324d69096427d3ce5d3ab13aed3f7c990a71ba3bd0c6f278f00"
-
-  patch :DATA # remove missing file; rename syscalls for OSX which are natively 64bit
+  url "https://github.com/flok99/truncate/archive/fc30a29dab789249f0c5f6863fdc5c8d4c9c3f69.zip"
+  version "0.9"
+  sha256 "f63a767555aa2edae2f340bf349465b98c636e54d211e00dd6501b702fa1837d"
+  head "https://github.com/flok99/truncate.git"
 
   def install
     system "make"
@@ -16,24 +16,3 @@ class Truncate < Formula
     system "#{bin}/truncate", "-s", "1k", "testfile"
   end
 end
-__END__
-diff -Naur truncate-0.8/truncate.c truncate/truncate.c
---- truncate-0.8/truncate.c	2009-04-03 14:39:56 +0800
-+++ truncate/truncate.c	2016-01-09 12:26:28 +0800
-@@ -9,7 +9,15 @@
- #include <sys/stat.h>
- #include <fcntl.h>
-
--#include "error.h"
-+//#include "error.h"
-+void error_exit(char *format, ...);
-+
-+#ifdef __APPLE__
-+#define off64_t off_t
-+#define stat64 stat
-+#define ftruncate64 ftruncate
-+#define fstat64 fstat
-+#endif
-
- off64_t get_file_size(char *file_name)
- {
