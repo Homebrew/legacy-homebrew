53793670678c92139147d8b44a0767f84247647e
Tomasz Pajor <tomek@polishgeeks.com>
2016-01-10 12:47:39 +0100

    sdl2_mixer 2.0.1
    Closes #47920.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/sdl2_mixer.rb
+++ Library/Formula/sdl2_mixer.rb
@@ -1,34 +1,9 @@
 class Sdl2Mixer < Formula
   desc "Sample multi-channel audio mixer library"
   homepage "https://www.libsdl.org/projects/SDL_mixer/"
+  url "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz"
+  sha256 "5a24f62a610249d744cbd8d28ee399d8905db7222bf3bdbc8a8b4a76e597695f"
   head "https://hg.libsdl.org/SDL_mixer", :using => :hg
-
-  stable do
-    url "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz"
-    sha256 "a8ce0e161793791adeff258ca6214267fdd41b3c073d2581cd5265c8646f725b"
-
-    # Next 4 patches are required to build sdl2_mixer 2.0.0 with libmodplug.
-    # Fix is already in upstream, see changeset 695 (https://hg.libsdl.org/SDL_mixer/rev/6a5e6d8d6a35).
-    patch do
-      url "https://hg.libsdl.org/SDL_mixer/raw-diff/6a5e6d8d6a35/configure"
-      sha256 "b5b47486dc84725b0b5464997111b5b5fad5fa97c5ca8b22a7dac0d89c1452bd"
-    end
-
-    patch do
-      url "https://hg.libsdl.org/SDL_mixer/raw-diff/6a5e6d8d6a35/configure.in"
-      sha256 "be60dace4d169afcf40a0c9974a09a2d4f1976cc94f37d7b40e4ffc3a2cb77e7"
-    end
-
-    patch do
-      url "https://hg.libsdl.org/SDL_mixer/raw-diff/6a5e6d8d6a35/dynamic_modplug.h"
-      sha256 "cb0c1251d7f018aa8a98c8f7b3dc9553bb22629ca32f2c89235e61daeda42fe2"
-    end
-
-    patch do
-      url "https://hg.libsdl.org/SDL_mixer/raw-diff/6a5e6d8d6a35/music_modplug.h"
-      sha256 "c6318a877effd5b5186ca49dbd040ecb1cfd39e73d03a380c80331fb86bfc00d"
-    end
-  end
 
   bottle do
     cellar :any
