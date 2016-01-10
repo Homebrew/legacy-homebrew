81cf9d81af38d85eb2c045885ecfdf4cff746152
xicilion <lion@9465.net>
2016-01-10 14:01:55 +0800

    set version

--- Library/Formula/fibjs.rb
+++ Library/Formula/fibjs.rb
@@ -2,6 +2,7 @@ class Fibjs < Formula
   desc "JavaScript on Fiber"
   homepage "http://fibjs.org"
   url "https://github.com/xicilion/fibjs/releases/download/v0.1.9/fullsrc.zip"
+  version "0.1.9"
   sha256 "e7fb5b5513aa09bf36552a14bbd55b177612e085ecf52f95e84f901c830f8fd7"
 
   head "https://github.com/xicilion/fibjs.git"
@@ -13,19 +14,4 @@ class Fibjs < Formula
     sha256 "e98f75ad475496d0f16b90b7c66cbd4a800490ba51c1634a23eaacf923e0b477" => :mavericks
   end
 
-  depends_on "cmake" => :build
-
-  def install
-    system "./build", "Release", "-j#{ENV.make_jobs}"
-    bin.install "bin/Darwin_Release/fibjs"
-  end
-
-  test do
-    path = testpath/"test.js"
-    path.write "console.log('hello');"
-
-    output = `#{bin}/fibjs #{path}`.strip
-    assert_equal "hello", output
-    assert_equal 0, $?.exitstatus
-  end
-end
+  depends_on "c
