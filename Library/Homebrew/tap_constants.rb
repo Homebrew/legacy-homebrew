eb32c7a8df4db64c248d5994cbc060a9efca26d9
Tommy Sparber <tommy@sparber.net>
2016-01-11 20:34:55 +1100

    Search also in Caskroom/versions Tap
    Extending the current search functionallity to search in
    https://github.com/caskroom/homebrew-versions

--- Library/Homebrew/tap_constants.rb
+++ Library/Homebrew/tap_constants.rb
@@ -4,5 +4,5 @@ HOMEBREW_TAP_FORMULA_REGEX = %r{^([\w-]+)/([\w-]+)/([\w+-.]+)$}
 HOMEBREW_TAP_DIR_REGEX = %r{#{Regexp.escape(HOMEBREW_LIBRARY.to_s)}/Taps/([\w-]+)/([\w-]+)}
 # match taps' formula paths, e.g. HOMEBREW_LIBRARY/Taps/someuser/sometap/someformula
 HOMEBREW_TAP_PATH_REGEX = Regexp.new(HOMEBREW_TAP_DIR_REGEX.source + %r{/(.*)}.source)
-# match the default brew-cask tap e.g. Caskroom/cask
-HOMEBREW_CASK_TAP_FORMULA_REGEX = %r{^(Caskroom)/(cask)/([\w+-.]+)$}
+# match the default and the versions brew-cask tap e.g. Caskroom/cask or Caskroom/versions
+HOMEBREW_CASK_TAP_FORMULA_REGEX = %r{^(Caskroom)/(cask|versions)/([\w+-.]+)$}
