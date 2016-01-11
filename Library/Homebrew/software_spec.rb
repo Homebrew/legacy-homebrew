7dccc9f2ca50fb107a3a5c2cea8b9bb0fe85898d
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-11 14:38:51 +0100

    Bottle: allow creating with a specific tag

--- Library/Homebrew/software_spec.rb
+++ Library/Homebrew/software_spec.rb
@@ -243,13 +243,13 @@ class Bottle
   def_delegators :resource, :url, :fetch, :verify_download_integrity
   def_delegators :resource, :cached_download, :clear_cache
 
-  def initialize(formula, spec)
+  def initialize(formula, spec, tag=bottle_tag)
     @name = formula.name
     @resource = Resource.new
     @resource.owner = formula
     @spec = spec
 
-    checksum, tag = spec.checksum_for(bottle_tag)
+    checksum, tag = spec.checksum_for(tag)
 
     filename = Filename.create(formula, tag, spec.revision)
     @resource.url(build_url(spec.root_url, filename))
