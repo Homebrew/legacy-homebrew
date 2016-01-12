83b98c6bca84ec05a5235f19e42e58c5e692766e
Xu Cheng <xucheng@me.com>
2016-01-12 17:44:32 +0800

    mongrel2: style nits

--- Library/Formula/mongrel2.rb
+++ Library/Formula/mongrel2.rb
@@ -1,24 +1,24 @@
 class Mongrel2 < Formula
   desc "Application, language, and network architecture agnostic web server"
   homepage "http://mongrel2.org/"
-  url "https://github.com/mongrel2/mongrel2/releases/download/v1.11.0/mongrel2-v1.11.0.tar.bz2"
-  sha256 "917f2ce07c0908cae63ac03f3039815839355d46568581902377ba7e41257bed"
-
   head "https://github.com/mongrel2/mongrel2.git", :branch => "develop"
+
+  stable do
+    url "https://github.com/mongrel2/mongrel2/releases/download/v1.11.0/mongrel2-v1.11.0.tar.bz2"
+    sha256 "917f2ce07c0908cae63ac03f3039815839355d46568581902377ba7e41257bed"
+
+    # ensure unit tests work on 1.11.0. remove after next release
+    patch do
+      url "https://github.com/mongrel2/mongrel2/commit/7cb8532e2ecc341d77885764b372a363fbc72eff.patch"
+      sha256 "b1861a49c7edff66cfd6bd898c8ce2e0ed7a5e9ecc454fdab337fb70af2346cd"
+    end
+  end
 
   bottle do
     cellar :any
     sha256 "7a6880cbc814b084a3ac91e379b7a720438951e31a18119c232f976fded229c3" => :el_capitan
     sha256 "0b2926fe3d79ab934e95f0e5c067e8bb23b6900b99255482defee9388a0dee07" => :yosemite
     sha256 "dd07092a2384c243fcd8c54ed67f2a728f3da698276540fc1c9b201eb3c5cbbb" => :mavericks
-  end
-
-  stable do
-    # ensure unit tests work on 1.11.0. remove after next release
-    patch do
-      url "https://github.com/mongrel2/mongrel2/commit/7cb8532e2ecc341d77885764b372a363fbc72eff.patch"
-      sha256 "b1861a49c7edff66cfd6bd898c8ce2e0ed7a5e9ecc454fdab337fb70af2346cd"
-    end
   end
 
   depends_on "zeromq"
