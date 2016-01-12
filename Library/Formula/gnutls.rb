d1c3ef7b5b0f23308fc464ea55e9515f1f2ccd38
Alex Dunn <dunn.alex@gmail.com>
2016-01-12 07:04:06 -0800

    gnutls: remove p11-kit option
    Closes #47990.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/gnutls.rb
+++ Library/Formula/gnutls.rb
@@ -21,7 +21,6 @@ class Gnutls < Formula
   depends_on "gmp"
   depends_on "nettle"
   depends_on "guile" => :optional
-  depends_on "p11-kit" => :optional
   depends_on "unbound" => :optional
 
   fails_with :llvm do
