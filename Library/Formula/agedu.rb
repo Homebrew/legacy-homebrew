4fc480d799ed8bd3264fef1ec783893e6e5fa512
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-12 01:38:58 +0000

    agedu: add 20151213 bottle.

--- Library/Formula/agedu.rb
+++ Library/Formula/agedu.rb
@@ -7,6 +7,13 @@ class Agedu < Formula
 
   head "git://git.tartarus.org/simon/agedu.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b140250d0f1318a6201c7c9f8fb38db23bd0d5bcbda667c93fe9d4a2bbd13722" => :el_capitan
+    sha256 "d87a2e0a8661ddf654a19dde92c03a70dea2b4e9fbca03bc1915367e31dd8d1f" => :yosemite
+    sha256 "d003c0fe2435ba2af53bb3e94d87959c634530ffcdcc30a16923974cdb17036d" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "halibut" => :build
