cfb4d5175ffa7aa041758e117f5afce7112bb4dc
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-01-12 12:07:19 +0530

    libnotify: formula cleanup/remove x11 dep

--- Library/Formula/libnotify.rb
+++ Library/Formula/libnotify.rb
@@ -1,12 +1,11 @@
 class Libnotify < Formula
   homepage "https://developer.gnome.org/libnotify"
-  url "http://ftp.gnome.org/pub/gnome/sources/libnotify/0.7/libnotify-0.7.6.tar.xz"
+  url "https://download.gnome.org/sources/libnotify/0.7/libnotify-0.7.6.tar.xz"
   sha256 "0ef61ca400d30e28217979bfa0e73a7406b19c32dd76150654ec5b2bdf47d837"
 
-  depends_on :x11
   depends_on "pkg-config" => :build
-  depends_on "gtk+3"
-  depends_on "gtk-doc" => :optional
+  depends_on "gtk+3" => :build
+  depends_on "gdk-pixbuf"
 
   def install
     system "./configure", "--disable-dependency-tracking",
