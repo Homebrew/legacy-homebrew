4823d315b1585fdeaeb3d6d683ce8aaf7a6ef240
Tomasz Pajor <tomek@polishgeeks.com>
2016-01-12 11:06:21 +0100

    librem 0.4.7

--- Library/Formula/librem.rb
+++ Library/Formula/librem.rb
@@ -1,8 +1,8 @@
 class Librem < Formula
   desc "Toolkit library for real-time audio and video processing"
   homepage "http://www.creytiv.com"
-  url "http://www.creytiv.com/pub/rem-0.4.6.tar.gz"
-  sha256 "7ce86f1eb8a3ba8cb14c490b80abe4d2389de306f385bfbb8c601c8b6ff2f865"
+  url "http://www.creytiv.com/pub/rem-0.4.7.tar.gz"
+  sha256 "5d084f5ee17b839680ab6b978357c095c2a85d04bdf61fa03192019e3435954e"
 
   bottle do
     cellar :any
