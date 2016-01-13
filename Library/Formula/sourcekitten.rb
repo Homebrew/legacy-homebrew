f9170ed9985c89dfa4e451479664f9577fcaf3ba
JP Simard <jp@jpsim.com>
2016-01-12 17:37:49 -0800

    sourcekitten 0.7.4

--- Library/Formula/sourcekitten.rb
+++ Library/Formula/sourcekitten.rb
@@ -1,7 +1,7 @@
 class Sourcekitten < Formula
   desc "Framework and command-line tool for interacting with SourceKit"
   homepage "https://github.com/jpsim/SourceKitten"
-  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.7.3", :revision => "0ed186552d8f9f3e128ab382581102c55c2d3c73"
+  url "https://github.com/jpsim/SourceKitten.git", :tag => "0.7.4", :revision => "9b5c8d05813fb9e817f24ebe2142915f03132bf1"
   head "https://github.com/jpsim/SourceKitten.git"
 
   bottle do
