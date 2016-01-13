48bf0d6670527313ec4d0abc52718ad65cad20b3
Matan Katz <matank@jfrog.com>
2016-01-13 16:06:28 +0200

    artifactory 4.4.1
    artifactory 4.4.1
    
    Closes #48030.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/artifactory.rb
+++ Library/Formula/artifactory.rb
@@ -1,8 +1,8 @@
 class Artifactory < Formula
   desc "Manages binaries"
   homepage "https://www.jfrog.com/artifactory/"
-  url "https://dl.bintray.com/jfrog/artifactory/jfrog-artifactory-oss-4.4.0.zip"
-  sha256 "fe25e57bfce830839e20c083f1e020093b5ab7c8b3fc5db89d58603429a01cfd"
+  url "https://dl.bintray.com/jfrog/artifactory/jfrog-artifactory-oss-4.4.1.zip"
+  sha256 "2e96267336c6e62eccd27f31ee83dc2d4f60a845e3a866a1da42a7b5176b5217"
 
   bottle :unneeded
 
