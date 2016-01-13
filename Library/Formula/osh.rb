14c6bda6d020bad7601560c2e97c21d4ec89e246
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-13 17:33:53 +0000

    osh: update 20160108 bottle.

--- Library/Formula/osh.rb
+++ Library/Formula/osh.rb
@@ -6,9 +6,9 @@ class Osh < Formula
   head "https://github.com/JNeitzel/v6shell.git"
 
   bottle do
-    sha256 "554a72a3c717bf1f29df8d7d15be31aed86d9538b4924bb242545b5c0891739b" => :yosemite
-    sha256 "6f70c00a27888c8c9b12ba28d872942a062aefd85180253d928afb50fa06a506" => :mavericks
-    sha256 "5a7d41cd9a354d647faff4021a7601b756af0255f906354bb3f79a33639e3ffc" => :mountain_lion
+    sha256 "abcfe34bbddad2d1dea4da6e8a852a9cd63df92fcdc67de01e77c6b0173922be" => :el_capitan
+    sha256 "9cdf662f481f3d5903dc44bfe1b8085074d3053a4612a59f830e1c4c58fd7409" => :yosemite
+    sha256 "1ca3cd79dc33ec932357ceb64ad5f05c002d7c6d19f6603c98d15415255efacb" => :mavericks
   end
 
   option "with-examples", "Build with shell examples"
