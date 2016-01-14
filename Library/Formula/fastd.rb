3960dd8113281077cd0fd6672bb44adc51611a41
Moritz Fago <github@mfago.org>
2016-01-14 15:42:33 +0100

    fastd 17 (new formula)

---
+++ Library/Formula/fastd.rb
@@ -0,0 +1,32 @@
+class Fastd < Formula
+  desc "Fast and Secure Tunnelling Daemon"
+  homepage "https://projects.universe-factory.net/projects/fastd"
+  url "https://projects.universe-factory.net/attachments/download/81/fastd-17.tar.xz"
+  sha256 "26d4a8bf2f8cc52872f836f6dba55f3b759f8c723699b4e4decaa9340d3e5a2d"
+  head "git://git.universe-factory.net/fastd/"
+
+  depends_on "cmake" => :build
+  depends_on "libuecc"
+  depends_on "libsodium"
+  depends_on "bison" => :build # fastd requires bison >= 2.5
+  depends_on "pkg-config" => :build
+  depends_on "json-c"
+  depends_on "openssl" => :optional
+  depends_on :tuntap => :recommended
+
+  def install
+    args = std_cmake_args
+    args << "-DENABLE_LTO=ON"
+    args << "-DENABLE_OPENSSL=ON" if build.with? "openssl"
+    args << buildpath
+    mkdir "fastd-build" do
+      system "cmake", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/fastd"
+  end
+end
