f67ffe787e6819fd4cbbb94a887a392136dcc967
Claudemiro <dimiro1@gmail.com>
2016-01-14 20:10:29 -0200

    Updated go devel version to 1.6

--- Library/Formula/go.rb
+++ Library/Formula/go.rb
@@ -15,9 +15,9 @@ class Go < Formula
   end
 
   devel do
-    url "https://storage.googleapis.com/golang/go1.6beta1.src.tar.gz"
-    version "1.6beta1"
-    sha256 "a6b4ff21a8a2ad044c58b051858e0f350a32786716b14e9492a6f45cf2017107"
+    url "https://storage.googleapis.com/golang/go1.6beta2.src.tar.gz"
+    version "1.6beta2"
+    sha256 "8b23d15a3edf1d154ceea5e9ca6370fc60e7f57fb1c28aa8a44c40f8f3167c6d"
   end
 
   option "without-cgo", "Build without cgo"
@@ -51,7 +51,7 @@ class Go < Formula
     # This should happen after we build the test Go, just in case
     # the bootstrap toolchain is aware of this variable too.
     ENV["GOROOT_BOOTSTRAP"] = ENV["GOROOT_FINAL"]
-
+b
     cd "src" do
       ENV["GOROOT_FINAL"] = libexec
       ENV["GOOS"]         = "darwin"
