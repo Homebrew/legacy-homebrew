fd8480042892da7108ce33f9a7b31f8a3b4de2c3
Greg V <greg@unrelenting.technology>
2016-01-14 17:32:20 +0300

    add version

--- Library/Formula/onioncat.rb
+++ Library/Formula/onioncat.rb
@@ -2,6 +2,7 @@ class Onioncat < Formula
   desc "VPN-adapter that provides location privacy using Tor or I2P"
   homepage "https://www.onioncat.org"
   url "https://www.cypherpunk.at/ocat/download/Source/current/onioncat-0.2.2.r569.tar.gz"
+  version "0.2.2.r569"
   sha256 "377777de0d3c731fd2253db02b25562a2ed17e82901d0569308754215223f0bb"
 
   depends_on "tor" => [:recommended, :run]
