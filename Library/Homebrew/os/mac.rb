d3616f65e970c214e51c0adc653a2606429294f3
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-14 13:57:18 +0100

    os/mac: use start_with? when possible
    Closes #48071.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Homebrew/os/mac.rb
+++ Library/Homebrew/os/mac.rb
@@ -141,7 +141,7 @@ module OS
       @gcc_42_build_version ||=
         begin
           gcc = MacOS.locate("gcc-4.2") || HOMEBREW_PREFIX.join("opt/apple-gcc42/bin/gcc-4.2")
-          if gcc.exist? && gcc.realpath.basename.to_s !~ /^llvm/
+          if gcc.exist? && !gcc.realpath.basename.to_s.start_with?("llvm")
             `#{gcc} --version`[/build (\d{4,})/, 1].to_i
           end
         end
@@ -150,7 +150,7 @@ module OS
 
     def llvm_build_version
       @llvm_build_version ||=
-        if (path = locate("llvm-gcc")) && path.realpath.basename.to_s !~ /^clang/
+        if (path = locate("llvm-gcc")) && !path.realpath.basename.to_s.start_with?("clang")
           `#{path} --version`[/LLVM build (\d{4,})/, 1].to_i
         end
     end
