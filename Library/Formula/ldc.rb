6a506358a8fa7dec5cd116b3be976939a42286ca
John Colvin <john.loughran.colvin@gmail.com>
2016-01-14 21:03:50 +0000

    ldc 0.17.0-beta2 (devel)
    Closes #48086.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ldc.rb
+++ Library/Formula/ldc.rb
@@ -1,10 +1,15 @@
 class Ldc < Formula
   desc "Portable D programming language compiler"
   homepage "http://wiki.dlang.org/LDC"
-  url "https://github.com/ldc-developers/ldc/releases/download/v0.16.1/ldc-0.16.1-src.tar.gz"
-  sha256 "e66cea99f0b1406bbd265ad5fe6aa1412bae31ac86d8a678eb6751f304b6f95b"
-
   head "https://github.com/ldc-developers/ldc.git", :shallow => false
+
+  stable do
+    url "https://github.com/ldc-developers/ldc/releases/download/v0.16.1/ldc-0.16.1-src.tar.gz"
+    sha256 "e66cea99f0b1406bbd265ad5fe6aa1412bae31ac86d8a678eb6751f304b6f95b"
+
+    # Fixes build errors in release mode, fixed in 0.17.0-beta2
+    patch :DATA
+  end
 
   bottle do
     revision 1
@@ -14,9 +19,9 @@ class Ldc < Formula
   end
 
   devel do
-    url "https://github.com/ldc-developers/ldc/releases/download/v0.17.0-alpha1/ldc-0.17.0-alpha1-src.tar.gz"
-    sha256 "68b13ece2d1aae6876cce3e143949966282a287169472b84c5bce5c119340532"
-    version "0.17.0-a1"
+    url "https://github.com/ldc-developers/ldc/releases/download/v0.17.0-beta2/ldc-0.17.0-beta2-src.tar.gz"
+    sha256 "6d00b29928556f1220332a230dd743169c30f18333724254ac3f58244d98a6d7"
+    version "0.17.0-beta2"
   end
 
   needs :cxx11
@@ -50,3 +55,16 @@ class Ldc < Formula
     system "./test"
   end
 end
+__END__
+diff --git a/cmake/Modules/FindLLVM.cmake b/cmake/Modules/FindLLVM.cmake
+index a1a5118..fe9902e 100644
+--- a/cmake/Modules/FindLLVM.cmake
++++ b/cmake/Modules/FindLLVM.cmake
+@@ -151,6 +151,7 @@ else()
+     llvm_set(HOST_TARGET host-target)
+     llvm_set(INCLUDE_DIRS includedir true)
+     llvm_set(ROOT_DIR prefix true)
++    llvm_set(ENABLE_ASSERTIONS assertion-mode)
+
+     if(${LLVM_VERSION_STRING} MATCHES "^3\\.[0-2][\\.0-9A-Za-z]*")
+         # Versions below 3.3 do not support components objcarcopts, option
