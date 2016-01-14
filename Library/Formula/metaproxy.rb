8ddcd6aca7ee0d5ba4fbb6da1ccbaa06893e9bbd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-14 07:32:20 +0000

    metaproxy: update 1.10.0_1 bottle.

--- Library/Formula/metaproxy.rb
+++ Library/Formula/metaproxy.rb
@@ -7,9 +7,9 @@ class Metaproxy < Formula
 
   bottle do
     cellar :any
-    sha256 "f6cbc71c09aa50f4bd91fdeda648eea5cd58ce6ec33944971dcf436d4cf9d343" => :el_capitan
-    sha256 "c1538d34944254ecde70adbd1715f8da2607e1b7513c18ff722f8e37944f33bf" => :yosemite
-    sha256 "04c814ff74ccc537a16276f7263d0c0eda416afc2d24390d2525cf9dcf9fbaea" => :mavericks
+    sha256 "51d08e2530cb5ca1508e202f2e4c2ccbd10e1b390d007d7bda836f1baa509fd7" => :el_capitan
+    sha256 "b4e8a8061d63fb432a034a2c25d2ab27f8075af72d92888b4cc7955da23fc27a" => :yosemite
+    sha256 "9c867f515b0aa5dfc5348df0ce384a9bb12ea589fc27d292c18c6d0eadd1284d" => :mavericks
   end
 
   depends_on "pkg-config" => :build
