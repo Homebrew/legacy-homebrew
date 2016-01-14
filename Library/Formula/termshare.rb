8492e16928e7ee23604f074cca89e973531a7074
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-14 04:19:47 +0000

    termshare: revision for go

--- Library/Formula/termshare.rb
+++ Library/Formula/termshare.rb
@@ -1,8 +1,11 @@
+require "language/go"
+
 class Termshare < Formula
   desc "Interactive or view-only terminal sharing via client or web"
   homepage "https://termsha.re"
   url "https://github.com/progrium/termshare/archive/v0.2.0.tar.gz"
   sha256 "fa09a5492d6176feff32bbcdb3b2dc3ff1b5ab2d1cf37572cc60eb22eb531dcd"
+  revision 1
 
   head "https://github.com/progrium/termshare.git"
 
@@ -15,18 +18,38 @@ class Termshare < Formula
   depends_on "go" => :build
   depends_on :hg => :build
 
+  go_resource "code.google.com/p/go.net" do
+    url "https://code.google.com/p/go.net",
+    :using => :hg,
+    :revision => "937a34c9de13"
+  end
+
+  go_resource "github.com/heroku/hk" do
+    url "https://github.com/heroku/hk.git",
+    :revision => "406190e9c93802fb0a49b5c09611790aee05c491"
+  end
+
+  go_resource "github.com/kr/pty" do
+    url "https://github.com/kr/pty.git",
+    :revision => "f7ee69f31298ecbe5d2b349c711e2547a617d398"
+  end
+
+  go_resource "github.com/nu7hatch/gouuid" do
+    url "https://github.com/nu7hatch/gouuid.git",
+    :revision => "179d4d0c4d8d407a32af483c2354df1d2c91e6c3"
+  end
+
   def install
     ENV["GOPATH"] = buildpath
 
-    # Install Go dependencies
-    system "go", "get", "code.google.com/p/go.net/websocket"
-    system "go", "get", "github.com/heroku/hk/term"
-    system "go", "get", "github.com/kr/pty"
-    system "go", "get", "github.com/nu7hatch/gouuid"
+    path = buildpath/"src/github.com/progrium/termshare"
+    path.install Dir["*"]
+    Language::Go.stage_deps resources, buildpath/"src"
 
-    # Build and install termshare
-    system "go", "build", "-o", "termshare"
-    bin.install "termshare"
+    cd path do
+      system "go", "build", "-o", "termshare"
+      bin.install "termshare"
+    end
   end
 
   test do
