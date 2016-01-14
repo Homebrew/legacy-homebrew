b7b4a974dbf6042865e52d85dbd0acc0374d4c9c
Yuuki Galaxy <galaxy001@gmail.com>
2016-01-14 18:13:36 +0800

    Now use github released source.tar.gz

--- Library/Formula/truncate.rb
+++ Library/Formula/truncate.rb
@@ -1,9 +1,8 @@
 class Truncate < Formula
   desc "truncates a file to a given size"
   homepage "http://www.vanheusden.com/truncate"
-  url "https://github.com/flok99/truncate/archive/fc30a29dab789249f0c5f6863fdc5c8d4c9c3f69.zip"
-  version "0.9"
-  sha256 "f63a767555aa2edae2f340bf349465b98c636e54d211e00dd6501b702fa1837d"
+  url "https://github.com/flok99/truncate/archive/0.9.tar.gz"
+  sha256 "a959d50cf01a67ed1038fc7814be3c9a74b26071315349bac65e02ca23891507"
   head "https://github.com/flok99/truncate.git"
 
   def install
