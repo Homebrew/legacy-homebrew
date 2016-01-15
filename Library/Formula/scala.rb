b2eddf22575b7750fd3c1705f64df18049c30698
Dan Poggi <dan.poggi@detroitlabs.com>
2016-01-15 12:45:29 -0500

    Reorganize Scala formula to appease audit

--- Library/Formula/scala.rb
+++ Library/Formula/scala.rb
@@ -1,5 +1,5 @@
 class Scala < Formula
-  desc "Scala programming language"
+  desc "JVM-based programming language"
   homepage "http://www.scala-lang.org/"
 
   if build.devel?
@@ -7,17 +7,6 @@ class Scala < Formula
   else
     depends_on :java => "1.6+"
   end
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "dcc350cf8dcc527b283b52d81ce27f0ee19403f1b805c6de74cbbd1c00571483" => :el_capitan
-    sha256 "abe3bdb7c49c2d8542731b5bff8ddd2b64b361e5fbc104217ca2f2423b73fbb9" => :yosemite
-    sha256 "87619ccc086a0636f89fec974759ae952dc1979948567f7e7d6200b7be64dffc" => :mavericks
-    sha256 "44c9502a3195a7cd25699162948b4eb80676547a24ed0a001520320d6a54aac7" => :mountain_lion
-  end
-
-  option "with-docs", "Also install library documentation"
-  option "with-src", "Also install sources for IDE support"
 
   stable do
     url "http://www.scala-lang.org/files/archive/scala-2.11.7.tgz"
@@ -32,6 +21,19 @@ class Scala < Formula
       url "https://github.com/scala/scala/archive/v2.11.7.tar.gz"
       sha256 "1679ee604bc4e881b0d325e164c39c02dcfa711d53cd3115f5a6c9676c5915ef"
     end
+  end
+
+  resource "completion" do
+    url "https://raw.githubusercontent.com/scala/scala-dist/v2.11.4/bash-completion/src/main/resources/completion.d/2.9.1/scala"
+    sha256 "95aeba51165ce2c0e36e9bf006f2904a90031470ab8d10b456e7611413d7d3fd"
+  end
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dcc350cf8dcc527b283b52d81ce27f0ee19403f1b805c6de74cbbd1c00571483" => :el_capitan
+    sha256 "abe3bdb7c49c2d8542731b5bff8ddd2b64b361e5fbc104217ca2f2423b73fbb9" => :yosemite
+    sha256 "87619ccc086a0636f89fec974759ae952dc1979948567f7e7d6200b7be64dffc" => :mavericks
+    sha256 "44c9502a3195a7cd25699162948b4eb80676547a24ed0a001520320d6a54aac7" => :mountain_lion
   end
 
   devel do
@@ -52,10 +54,8 @@ class Scala < Formula
     end
   end
 
-  resource "completion" do
-    url "https://raw.githubusercontent.com/scala/scala-dist/v2.11.4/bash-completion/src/main/resources/completion.d/2.9.1/scala"
-    sha256 "95aeba51165ce2c0e36e9bf006f2904a90031470ab8d10b456e7611413d7d3fd"
-  end
+  option "with-docs", "Also install library documentation"
+  option "with-src", "Also install sources for IDE support"
 
   def install
     rm_f Dir["bin/*.bat"]
