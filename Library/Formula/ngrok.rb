5362a15cda8cc3f00504b8173df3378118471fca
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-15 04:25:15 +0000

    ngrok: revision for go

--- Library/Formula/ngrok.rb
+++ Library/Formula/ngrok.rb
@@ -4,9 +4,11 @@
 class Ngrok < Formula
   desc "Expose localhost to the internet and capture traffic for replay"
   homepage "https://ngrok.com"
-  head "https://github.com/inconshreveable/ngrok.git"
   url "https://github.com/inconshreveable/ngrok/archive/1.7.1.tar.gz"
   sha256 "ffb1ad90d5aa3c8b3a9bfd8109a71027645a78d5e61fccedd0873fee060da665"
+  revision 1
+
+  head "https://github.com/inconshreveable/ngrok.git"
 
   bottle do
     cellar :any_skip_relocation
