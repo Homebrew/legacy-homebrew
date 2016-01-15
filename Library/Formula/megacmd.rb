483cf0ecec8cf05e9b4afe99d8ae508c8ebffecc
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-15 04:23:34 +0000

    megacmd: revision for go

--- Library/Formula/megacmd.rb
+++ Library/Formula/megacmd.rb
@@ -1,8 +1,12 @@
+require "language/go"
+
 class Megacmd < Formula
   desc "Command-line client for mega.co.nz storage service"
   homepage "https://github.com/t3rm1n4l/megacmd"
   url "https://github.com/t3rm1n4l/megacmd/archive/0.012.tar.gz"
   sha256 "804861f2a7a36eef53a7310e52627e790fa9de66acf8565f697089389d2709a0"
+  revision 1
+
   head "https://github.com/t3rm1n4l/megacmd.git"
 
   bottle do
@@ -13,13 +17,29 @@ class Megacmd < Formula
 
   depends_on "go" => :build
 
+  go_resource "github.com/t3rm1n4l/go-humanize" do
+    url "https://github.com/t3rm1n4l/go-humanize.git",
+    :revision => "e7ed15be05eb554fbaa83ac9b335556d6390fb9f"
+  end
+
+  go_resource "github.com/t3rm1n4l/go-mega" do
+    url "https://github.com/t3rm1n4l/go-mega.git",
+    :revision => "551abb8f1c87053be3f24282d198a6614c0ca14f"
+  end
+
+  go_resource "github.com/t3rm1n4l/megacmd" do
+    url "https://github.com/t3rm1n4l/megacmd.git",
+    :revision => "d7f3f3a2427cc52b71cad90b26889e2a33fc3565"
+  end
+
   def install
     ENV["GOPATH"] = buildpath
-    system "make"
-    bin.install File.basename(buildpath) => "megacmd"
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    system "go", "build", "-o", bin/"megacmd"
   end
 
   test do
-    system "megacmd", "--version"
+    system bin/"megacmd", "--version"
   end
 end
