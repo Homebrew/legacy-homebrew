ebd3a33dd121441e1b65279f7f42c5f0f774a163
ruki <waruqi@gmail.com>
2016-01-16 22:52:17 +0800

    tbox: disable all third-party libraries

--- Library/Formula/libtbox.rb
+++ Library/Formula/libtbox.rb
@@ -7,19 +7,12 @@ class Libtbox < Formula
   head "https://github.com/waruqi/tbox.git"
 
   option "with-debug", "Build with debug support"
-  option "without-zlib", "Disable Zlib compression library"
-  option "without-mysql", "Disable Mysql database"
-  option "without-sqlite", "Disable Sqlite database"
-  option "without-openssl", "Disable Openssl SSL/TLS cryptography library"
 
   depends_on "xmake" => :build
 
   def install
-    args = ["--demo=false", "--polarssl=false", "--pcre=false", "--pcre2=false", "--mysql=false"]
+    args = ["--demo=false", "--polarssl=false", "--pcre=false", "--pcre2=false", "--mysql=false", "--sqlite3=false", "--zlib=false", "--openssl=false"]
     args << "--mode=debug" if build.with? "debug"
-    args << "--zlib=false" if build.without? "zlib"
-    args << "--sqlite3=false" if build.without? "sqlite"
-    args << "--openssl=false" if build.without? "openssl"
 
     system "xmake", "config", *args
     system "xmake", "install", "-o", prefix
@@ -38,28 +31,6 @@ class Libtbox < Formula
         return 0;
       }
     EOS
-    (testpath/"xmake.lua").write <<-EOS.undent
-      add_option("zlib")
-        set_option_showmenu(true)
-        add_option_links("z")
-        add_option_linkdirs("#{lib}")
-      add_option("openssl")
-        set_option_showmenu(true)
-        add_option_links("ssl", "crypto")
-        add_option_linkdirs("#{lib}")
-      add_option("sqlite3")
-        set_option_showmenu(true)
-        add_option_links("sqlite3")
-        add_option_linkdirs("#{lib}")
-      add_target("test")
-        set_kind("binary")
-        add_files("test.c")
-        add_links("tbox")
-        add_linkdirs("#{lib}")
-        add_includedirs("#{include}")
-        add_options("openssl", "sqlite3", "zlib")
-    EOS
-    system "xmake"
-    system "xmake", "run", "test"
+    system ENV.cc, "test.c", "-L#{lib}", "-ltbox", "-I#{include}", "-o", "test"
   end
 end
