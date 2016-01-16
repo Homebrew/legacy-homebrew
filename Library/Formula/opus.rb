639d498a8909793287779945c5333abe7b5d06e9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-16 06:25:06 +0000

    opus: update 1.1.2 bottle.

--- Library/Formula/opus.rb
+++ Library/Formula/opus.rb
@@ -6,9 +6,9 @@ class Opus < Formula
 
   bottle do
     cellar :any
-    sha256 "41f6ff91b8b9b318a366fc692fdbdf768bc92ef61c37fa494f1dffc4babee21f" => :el_capitan
-    sha256 "e347b0546ee09d1782e60c99d9e946ee81c6c4fb91c1e067e62585bfddbc04d6" => :yosemite
-    sha256 "42bcd3d6fa7500e1c6e58a2d6d8a520d068ab5e2d7264b0ddf909b8302930d0f" => :mavericks
+    sha256 "bf658ab20eb10724c16b619eaac1c10f1f746e7656b03be5ae1ef408178f405b" => :el_capitan
+    sha256 "252c20e9dfcfee3b628cf4344bb3586b5adf83d5772b493b853f0968e5ebf698" => :yosemite
+    sha256 "8a43607dcf83bc516b823893d3c7f80d2ec9a5a2a7802d314f026858360c9303" => :mavericks
   end
 
   option "with-custom-modes", "Enable custom-modes for opus see http://www.opus-codec.org/docs/html_api-1.1.0/group__opus__custom.html"
