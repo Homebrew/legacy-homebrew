6f210208ff13a061eff23a590ad5be04cea083c0
ruki <waruqi@gmail.com>
2016-01-16 21:53:56 +0800

    tbox: modify test

--- Library/Formula/libtbox.rb
+++ Library/Formula/libtbox.rb
@@ -8,16 +8,17 @@ class Libtbox < Formula
 
   option "with-debug", "Build with debug support"
   option "without-zlib", "Disable Zlib compression library"
+  option "without-mysql", "Disable Mysql database"
+  option "without-sqlite", "Disable Sqlite database"
   option "without-openssl", "Disable Openssl SSL/TLS cryptography library"
 
   depends_on "xmake" => :build
-  depends_on "lzlib" => :recommended
-  depends_on "openssl" => :recommended
 
   def install
-    args = ["--demo=false", "--pcre=false", "--pcre2=false", "--mysql=false", "--polarssl=false", "--sqlite3=false"]
+    args = ["--demo=false", "--polarssl=false", "--pcre=false", "--pcre2=false", "--mysql=false"]
     args << "--mode=debug" if build.with? "debug"
     args << "--zlib=false" if build.without? "zlib"
+    args << "--sqlite3=false" if build.without? "sqlite"
     args << "--openssl=false" if build.without? "openssl"
 
     system "xmake", "config", *args
@@ -37,7 +38,28 @@ class Libtbox < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-L#{lib}", "-ltbox", "-lssl", "-lcrypto", "-lz", "-I#{include}", "-o", "test"
-    system "./test"
+    (testpath/"xmake.lua").write <<-EOS.undent
+      add_option("zlib")
+        set_option_showmenu(true)
+        add_option_links("z")
+        add_option_linkdirs("#{lib}")
+      add_option("openssl")
+        set_option_showmenu(true)
+        add_option_links("ssl", "crypto")
+        add_option_linkdirs("#{lib}")
+      add_option("sqlite3")
+        set_option_showmenu(true)
+        add_option_links("sqlite3")
+        add_option_linkdirs("#{lib}")
+      add_target("test")
+        set_kind("binary")
+        add_files("test.c")
+        add_links("tbox")
+        add_linkdirs("#{lib}")
+        add_includedirs("#{include}")
+        add_options("openssl", "sqlite3", "zlib")
+    EOS
+    system "xmake"
+    system "xmake", "run", "test"
   end
 end
