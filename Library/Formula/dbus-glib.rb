b9e1e10816fa38689e6212cb368bfeb649bf777a
Tomasz Pajor <tomek@polishgeeks.com>
2016-01-16 17:31:19 +0100

    dbus-glib 0.106
    Closes #48151.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/dbus-glib.rb
+++ Library/Formula/dbus-glib.rb
@@ -1,8 +1,8 @@
 class DbusGlib < Formula
   desc "GLib bindings for the D-Bus message bus system"
   homepage "https://wiki.freedesktop.org/www/Software/DBusBindings/"
-  url "http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.104.tar.gz"
-  sha256 "bfc1f1a82bfc3ec3ecafe04d0e87bab7e999f50dce4f4a34d0b89caf6bd821f6"
+  url "http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-0.106.tar.gz"
+  sha256 "b38952706dcf68bad9c302999ef0f420b8cf1a2428227123f0ac4764b689c046"
 
   bottle do
     cellar :any
