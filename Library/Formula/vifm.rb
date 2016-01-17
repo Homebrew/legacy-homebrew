4d54d7b82d3663973e2d308072522aaf8f967f02
cfillion <cfillion@users.noreply.github.com>
2016-01-17 15:34:01 -0500

    vifm 0.8.1
    https://github.com/vifm/vifm/releases/tag/v0.8.1
    
    Closes #48186.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/vifm.rb
+++ Library/Formula/vifm.rb
@@ -1,21 +1,15 @@
 class Vifm < Formula
   desc "Ncurses based file manager with vi like keybindings"
   homepage "https://vifm.info/"
-  url "https://downloads.sourceforge.net/project/vifm/vifm/vifm-0.8.tar.bz2"
-  mirror "https://github.com/vifm/vifm/releases/download/v0.8/vifm-0.8.tar.bz2"
-  sha256 "69eb6b50dcf462f4233ff987f0b6a295df08a27bc42577ebef725bfe58dbdeeb"
+  url "https://downloads.sourceforge.net/project/vifm/vifm/vifm-0.8.1.tar.bz2"
+  mirror "https://github.com/vifm/vifm/releases/download/v0.8.1/vifm-0.8.1.tar.bz2"
+  sha256 "9918ac96290f9bb2da0fdbd6e579af19de32d9eca41d0ceb5e2cb7cf08ebc379"
 
   bottle do
     revision 1
     sha256 "57d2224f685ad01c2cded9a3840c6999a75e3544111c501822412ac6bac9f460" => :el_capitan
     sha256 "d875cfb97b83552d78ff3ed9d1c2ec8faa35ed2b4bd27dabbeb48eca7af820f0" => :yosemite
     sha256 "032ed7e7c2256a7e352322b65e68fd220ce35d59ea9a558d2112df86275fbe45" => :mavericks
-  end
-
-  devel do
-    url "https://downloads.sourceforge.net/project/vifm/vifm/vifm-0.8.1-beta.tar.bz2"
-    mirror "https://github.com/vifm/vifm/releases/download/v0.8.1-beta/vifm-0.8.1-beta.tar.bz2"
-    sha256 "e0e3e4d7cc06ac75e4acdd1c03f1b91e433b0df3ef817024e27bdc268f32b80f"
   end
 
   head do
