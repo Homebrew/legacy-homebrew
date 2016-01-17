fb4d3b18e91d42b850193a201a88950e403dc3be
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-17 21:31:37 +0000

    mg: add 20160103 bottle.

--- Library/Formula/mg.rb
+++ Library/Formula/mg.rb
@@ -4,6 +4,13 @@ class Mg < Formula
   url "http://devio.us/~bcallah/mg/mg-20160103.tar.gz"
   sha256 "4abd059ba3e0d59626104a21812ae33a37ee1f6ddaafdb33511f38d21057fae6"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "54e088aa608c93e2c78ae0a6d7e838e5364998b10163ca360ef014818c383c0e" => :el_capitan
+    sha256 "26f35442ecd13cd1de86b530998251da80f72b2854ed1ae28c8f0eec436adaa0" => :yosemite
+    sha256 "6a2e606dce9970e35b767739b66f76c5174e3bb0f7ce2e09d4d683fdbe077525" => :mavericks
+  end
+
   conflicts_with "mg3a", :because => "both install `mg` binaries"
 
   def install
