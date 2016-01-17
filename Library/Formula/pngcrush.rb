1d4b1de1d12648e255a724c86ecb9f4bcfebad91
Tomasz Pajor <tomek@polishgeeks.com>
2016-01-17 12:20:15 +0100

    pngcrush 1.8.0
    Closes #48173.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/pngcrush.rb
+++ Library/Formula/pngcrush.rb
@@ -1,8 +1,8 @@
 class Pngcrush < Formula
   desc "Optimizer for PNG files"
   homepage "http://pmt.sourceforge.net/pngcrush/"
-  url "https://downloads.sourceforge.net/project/pmt/pngcrush/1.7.92/pngcrush-1.7.92.tar.gz"
-  sha256 "384e1e1a9cde0ece8750ea7663cabba6cec9756620e6f381043a32fcf42e1235"
+  url "https://downloads.sourceforge.net/project/pmt/pngcrush/1.8.0/pngcrush-1.8.0.tar.gz"
+  sha256 "6e7e95933f69615e563f14f62572820e229a6d586826418ef7016842d96b0d92"
 
   bottle do
     cellar :any_skip_relocation
