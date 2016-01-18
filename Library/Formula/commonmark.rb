d0756e859e8fed0abe914425c9da343474ad6b3b
Zhiming Wang <zmwangx@gmail.com>
2016-01-18 10:02:18 -0800

    cmark 0.24.1

--- Library/Formula/commonmark.rb
+++ Library/Formula/commonmark.rb
@@ -1,8 +1,8 @@
 class Commonmark < Formula
   desc "Strongly specified, highly compatible implementation of Markdown"
   homepage "http://commonmark.org"
-  url "https://github.com/jgm/cmark/archive/0.24.0.tar.gz"
-  sha256 "6408e8019585d28518521c3cb8036651296c04ca0ea56fe105b2ed1bb27b0bdb"
+  url "https://github.com/jgm/cmark/archive/0.24.1.tar.gz"
+  sha256 "7566d307ffff07567faa0381b248f60164eddcc0e0f5eb6e27d6f58e1269ab2a"
 
   bottle do
     cellar :any
