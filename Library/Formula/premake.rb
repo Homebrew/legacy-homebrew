2cfb129e9d9c242685b1a04240ad6c62074ebb5c
George White <georgeunix@gmail.com>
2016-01-18 15:38:39 +0400

    Update premake to 4.4-beta6
    According to premake's website, beta6 is the latest stable.

--- Library/Formula/premake.rb
+++ Library/Formula/premake.rb
@@ -1,18 +1,34 @@
 class Premake < Formula
   desc "Write once, build anywhere Lua-based build system"
   homepage "https://premake.github.io/"
-  url "https://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip"
-  sha256 "0fa1ed02c5229d931e87995123cdb11d44fcc8bd99bba8e8bb1bbc0aaa798161"
-
-  bottle do
-    cellar :any
-    sha256 "d57b33a77188b36ad9302906ebdd16b454611ee1dc146b1fc008807347bfb141" => :mavericks
-    sha256 "dc7affe6284df57b61d81fe6ba00588c79a2c817f57bff1b5dec1a39cd7e5053" => :mountain_lion
-    sha256 "626bcb3c9d5244c91468f07e8705ad1726b3250d3018b0d11392df5374679a28" => :lion
+  
+  stable do
+    url "https://downloads.sourceforge.net/project/premake/Premake/4.4/premake-4.4-beta5-src.zip"
+    sha256 "0fa1ed02c5229d931e87995123cdb11d44fcc8bd99bba8e8bb1bbc0aaa798161"
   end
-
+  
+  devel do
+    url "https://github.com/premake/premake-core/releases/download/v5.0.0-alpha6/premake-5.0.0-alpha6-src.zip"
+    sha256 "9c13372699d25824cba1c16a0483507a6a28903e2556ffb148b288c189403aee"
+  end
+  
+  head do
+    url "https://github.com/premake/premake-core.git"
+  end
+  
   def install
-    system "make -C build/gmake.macosx"
-    bin.install "bin/release/premake4"
+    if build.head?
+      system "make", "-f", "Bootstrap.mak"
+      system "./premake5", "gmake"
+    end
+    
+    system "make", "-C", "build/gmake.macosx"
+    bin.install "bin/release/premake5" if build.devel? or build.head?
+    bin.install "bin/release/premake4" if build.stable?
+  end
+  
+  test do
+    system "premake5", "test" if build.devel? or build.head?
+    system "premake4", "test" if build.stable?
   end
 end
