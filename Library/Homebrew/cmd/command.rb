3d550e30cfc58fca438a7b4612bb5b463b791fcb
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-18 01:22:35 +0100

    command: support .sh commands

--- Library/Homebrew/cmd/command.rb
+++ Library/Homebrew/cmd/command.rb
@@ -4,14 +4,25 @@ module Homebrew
     cmd = ARGV.first
     cmd = HOMEBREW_INTERNAL_COMMAND_ALIASES.fetch(cmd, cmd)
 
-    if (path = HOMEBREW_LIBRARY_PATH/"cmd/#{cmd}.rb").file?
-      puts path
-    elsif ARGV.homebrew_developer? && (path = HOMEBREW_LIBRARY_PATH/"dev-cmd/#{cmd}.rb").file?
+    if (path = internal_command_path cmd)
       puts path
     elsif (path = which("brew-#{cmd}") || which("brew-#{cmd}.rb"))
       puts path
     else
       odie "Unknown command: #{cmd}"
     end
+  end
+
+  private
+
+  def internal_command_path(cmd)
+    extensions = %w[rb sh]
+    paths = extensions.map { |ext| HOMEBREW_LIBRARY_PATH/"cmd/#{cmd}.#{ext}" }
+
+    if ARGV.homebrew_developer?
+      paths += extensions.map { |ext| HOMEBREW_LIBRARY_PATH/"dev-cmd/#{cmd}.#{ext}" }
+    end
+
+    paths.find { |p| p.file? }
   end
 end
