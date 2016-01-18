34c48eed59bedeed131dffc083d5d53eca6f1a90
Ramkumar R. Aiyengar <andyetitmoves@gmail.com>
2016-01-18 18:16:29 +0000

    Fix a few audit issues

--- Library/Formula/libproxy.rb
+++ Library/Formula/libproxy.rb
@@ -1,18 +1,18 @@
 class Libproxy < Formula
   desc "Library that provides automatic proxy configuration management"
   homepage "https://libproxy.github.io/libproxy/"
-  url "https://github.com/libproxy/libproxy/archive/0.4.12.tar.gz"
-  sha256 "add9c5e30767c17b00f842f6280d818ece1eb23ab92e1fc68661204c95d7e22b"
+
+  head "https://github.com/libproxy/libproxy.git"
 
   stable do
+    url "https://github.com/libproxy/libproxy/archive/0.4.12.tar.gz"
+    sha256 "add9c5e30767c17b00f842f6280d818ece1eb23ab92e1fc68661204c95d7e22b"
     # Fix compilation errors with CLang on MacOSX
     patch do
       url "https://github.com/libproxy/libproxy/commit/48a441652edfd73654f0c21e51cc96c69d5070c3.diff"
       sha256 "ac714f2cc010a5428556b4b66a42f4df043a6f42540269bba7da11a773f7529c"
     end
   end
-
-  head "https://github.com/libproxy/libproxy.git"
 
   depends_on "cmake" => :build
 
