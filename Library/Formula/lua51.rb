4769ab018fb4be4b3875f3fe0c2cdd7adb50c7c0
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-19 11:12:40 +0000

    lua51: revision for mkpath fix
    Closes #48242.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/lua51.rb
+++ Library/Formula/lua51.rb
@@ -6,7 +6,7 @@ class Lua51 < Formula
   url "http://www.lua.org/ftp/lua-5.1.5.tar.gz"
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/l/lua5.1/lua5.1_5.1.5.orig.tar.gz"
   sha256 "2640fc56a795f29d28ef15e13c34a47e223960b0240e8cb0a82d9b0738695333"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
