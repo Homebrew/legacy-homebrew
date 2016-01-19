454e6734b6524b7b7fb9361aa5c654f2f51343a2
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-19 09:01:06 +0000

    fleetctl: revision for go

--- Library/Formula/fleetctl.rb
+++ Library/Formula/fleetctl.rb
@@ -3,6 +3,8 @@ class Fleetctl < Formula
   homepage "https://github.com/coreos/fleet"
   url "https://github.com/coreos/fleet/archive/v0.11.5.tar.gz"
   sha256 "a6a785099df71645b5fe8755a36baa6c11138749bc02ae4990fd3f52663c0394"
+  revision 1
+
   head "https://github.com/coreos/fleet.git"
 
   bottle do
