a3e396c345f01a32d384a9689f0989e880284b19
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-19 03:06:38 +0000

    nethogs: add 0.8.1 bottle.

--- Library/Formula/nethogs.rb
+++ Library/Formula/nethogs.rb
@@ -4,6 +4,13 @@ class Nethogs < Formula
   url "https://github.com/raboof/nethogs/archive/v0.8.1.tar.gz"
   sha256 "4c30ef43814549974a5b01fb1a94eb72ff08628c5a421085b1ce3bfe0524df42"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1a4a5fbe64015f701ebb4dde35905f333c66ed2365b5880d54f1ffccd1261616" => :el_capitan
+    sha256 "95ed6efb5d64d447d4c5b86e65746282b4e146610ce008e37a73c75ffcfd2db5" => :yosemite
+    sha256 "8b885955dcf5abcc9bdf424a1db9f03f94e287ffddaef9fd281d55c4ed8928fd" => :mavericks
+  end
+
   # OSX compatibility
   patch do
     url "https://github.com/raboof/nethogs/commit/9eae9e13434bc2e27fc28731a67de2ae83737783.patch"
