9a3e79b8ecd6f28ca72d8e62011da1da0811d851
Vinh <kurei@axcoto.com>
2016-01-18 22:56:33 -0800

    Update

--- Library/Formula/sup.rb
+++ Library/Formula/sup.rb
@@ -3,14 +3,9 @@ require "language/go"
 class Sup < Formula
   desc "Super simple deployment tool - just Unix - think of it like 'make' for a network of servers."
   homepage "https://github.com/pressly/sup"
-  #url "https://github.com/pressly/sup/archive/v0.3.zip"
   url "https://github.com/pressly/sup/archive/4ee5083c8321340bc2a6410f24d8a760f7ad3847.zip"
-  sha256 "876c9c2bb8bf71de2889fca97d42da696b31700930dfe84131c133c762ecea45"
   version "0.3.1"
-
-  head do
-    url "https://github.com/pressly/sup.git"
-  end
+  sha256 "7fa17c20fdcd9e24d8c2fe98081e1300e936da02b3f2cf9c5a11fd699cbc487e"
 
   depends_on "go"  => :build
 
@@ -37,14 +32,10 @@ class Sup < Formula
     ln_sf buildpath, buildpath/"src/github.com/pressly/sup"
     Language::Go.stage_deps resources, buildpath/"src"
 
-    system "make build"
-    bin.install "bin/sup" => "sup"
-
-    puts "Ready to go! Learn more at https://github.com/pressly/sup"
+    system "go", "build", "-o", bin/"sup", "./cmd/sup"
   end
 
   test do
-    assert File.exist?("/usr/local/bin/sup")
-    assert_equal(0, "/usr/local/bin/sup")
+    assert_equal "0.3", shell_output("#{bin}/bin/sup")
   end
 end
