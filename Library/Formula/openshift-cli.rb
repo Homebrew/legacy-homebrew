0ef03674d95d082afbe0c51a9f25626fdcf015ef
Eli Young <elyscape@gmail.com>
2016-01-18 18:53:50 -0800

    openshift-cli 1.1.1 (new formula)
    Based on https://github.com/nak3/homebrew-openshift.
    
    Closes #48231.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

---
+++ Library/Formula/openshift-cli.rb
@@ -0,0 +1,31 @@
+class OpenshiftCli < Formula
+  desc "OpenShift command-line interface tools"
+  homepage "https://www.openshift.com/"
+  url "https://github.com/openshift/origin.git",
+    :tag => "v1.1.1",
+    :revision => "e1d9873c1d5711b83fd3dd7eefe83a88ceb92c08"
+
+  head "https://github.com/openshift/origin.git"
+
+  depends_on "go" => :build
+
+  def install
+    # this is necessary to avoid having the version marked as dirty
+    (buildpath/".git/info/exclude").atomic_write "/.brew_home"
+
+    system "make", "all", "WHAT=cmd/openshift", "GOFLAGS=-v"
+
+    arch = MacOS.prefer_64_bit? ? "amd64" : "x86"
+    bin.install "_output/local/bin/darwin/#{arch}/openshift"
+    bin.install_symlink "openshift" => "oc"
+    bin.install_symlink "openshift" => "oadm"
+
+    bash_completion.install Dir["contrib/completions/bash/*"]
+  end
+
+  test do
+    assert_match /^oc v#{version}$/, shell_output("#{bin}/oc version")
+    assert_match /^oadm v#{version}$/, shell_output("#{bin}/oadm version")
+    assert_match /^openshift v#{version}$/, shell_output("#{bin}/openshift version")
+  end
+end
