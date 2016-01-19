092b7c27e1b80e8af13abefa3821914a1e9f44a8
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-19 09:56:00 +0000

    png2ico: cleanup

--- Library/Formula/png2ico.rb
+++ Library/Formula/png2ico.rb
@@ -3,14 +3,14 @@ class Png2ico < Formula
   homepage "http://www.winterdrache.de/freeware/png2ico/"
   url "http://www.winterdrache.de/freeware/png2ico/data/png2ico-src-2002-12-08.tar.gz"
   sha256 "d6bc2b8f9dacfb8010e5f5654aaba56476df18d88e344ea1a32523bb5843b68e"
+  revision 1
+
   bottle do
     cellar :any
     sha256 "092829cdf12cc8bf24460451078eb5b34d12f5b8b60f134e270bfee3a05683f4" => :yosemite
     sha256 "0257cd8aa4f18fa941400b7d0db2f333d9d6e053d38e264bd83642d400e48173" => :mavericks
     sha256 "9f87f833f8474ef15a81f32f25c7794b68fa956093dd532c5874b0da9ebaf970" => :mountain_lion
   end
-
-  revision 1
 
   depends_on "libpng"
 
