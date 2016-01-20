29d953db0ee76b6460e383a9f74ef61dc09c4976
Alex Dunn <dunn.alex@gmail.com>
2016-01-20 09:27:25 -0800

    shellcheck 0.4.3

--- Library/Formula/shellcheck.rb
+++ Library/Formula/shellcheck.rb
@@ -5,8 +5,8 @@ class Shellcheck < Formula
 
   desc "Static analysis and lint tool, for (ba)sh scripts"
   homepage "http://www.shellcheck.net"
-  url "https://github.com/koalaman/shellcheck/archive/v0.4.2.tar.gz"
-  sha256 "ea3eca5f2cce13ab6b4332b3cb5fb419c7ebf3f2e3d4d01c52b6cf5938aa96cf"
+  url "https://github.com/koalaman/shellcheck/archive/v0.4.3.tar.gz"
+  sha256 "229a3bd5e38c4da2ce7ddf43c06ca388454e0406f3dbfc44865208b6b334109e"
   head "https://github.com/koalaman/shellcheck.git"
 
   bottle do
