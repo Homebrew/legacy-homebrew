d528510d37f32a90b5451555f0c9c93a51fff3ac
Tomas Harkema <tomas@harkema.in>
2016-01-21 16:04:58 +0100

    fix audit problems
    ```==> audit problems
    rswift:
     * Stable: Git should specify :revision when a :tag is specified.
     * Description is too long. "name: desc" should be less than 80 characters.
    Length is calculated as rswift + desc. (currently 97)
     * use "xcodebuild *args" instead of "system 'xcodebuild', *args"
     * 12: Trailing whitespace was found```

--- Library/Formula/rswift.rb
+++ Library/Formula/rswift.rb
@@ -1,15 +1,15 @@
 class Rswift < Formula
-  desc "Get strong typed, autocompleted resources like images, fonts and segues in Swift projects"
+  desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
   url "https://github.com/mac-cain13/R.swift.git", :tag => "v1.1.0"
 
   depends_on :xcode => "7.0"
 
   def install
-    system "xcodebuild", "-configuration", "Release", "-scheme", "rswift", "SYMROOT=symroot", "OBJROOT=objroot"
+    xcodebuild, "-configuration", "Release", "-scheme", "rswift", "SYMROOT=symroot", "OBJROOT=objroot"
     bin.install "symroot/Release/rswift"
   end
-  
+
   test do
     system "rswift", "-h"
   end
