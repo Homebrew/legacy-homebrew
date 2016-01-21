0f683d78934128d73fdac036c13fdf27fac7c250
Tomas Harkema <tomas@harkema.in>
2016-01-21 21:11:36 +0100

    fix audit and test

--- Library/Formula/rswift.rb
+++ Library/Formula/rswift.rb
@@ -1,8 +1,7 @@
 class Rswift < Formula
   desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
-  url "https://github.com/mac-cain13/R.swift.git", :tag => "v1.1.0"
-
+  url "https://github.com/mac-cain13/R.swift.git", :tag => "v1.1.1", :revision => "3a6db62164d8f50ccdf43e59894fffb672fd5e3f"
   depends_on :xcode => "7.0"
 
   def install
