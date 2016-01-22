aada0afd0b18a085a01ae6880124febe2167042d
Jason Karns <jason.karns@gmail.com>
2016-01-22 17:26:38 -0500

    update urls
    nodenv has moved from oinutter to nodenv github org

--- Library/Formula/nodenv.rb
+++ Library/Formula/nodenv.rb
@@ -1,9 +1,9 @@
 class Nodenv < Formula
   desc "Manage multiple NodeJS versions"
-  homepage "https://github.com/OiNutter/nodenv"
+  homepage "https://github.com/nodenv/nodenv"
   url "https://github.com/nodenv/nodenv/archive/v1.0.0.tar.gz"
   sha256 "1b7d0a43f27d92d12af2658ac297b397b6b5b1e25af48e77de2e7e8675083586"
-  head "https://github.com/OiNutter/nodenv.git"
+  head "https://github.com/nodenv/nodenv.git"
 
   bottle :unneeded
 
