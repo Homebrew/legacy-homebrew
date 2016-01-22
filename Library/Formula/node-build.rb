60a74dfe78b9097fcf3d654d5206568304ad331f
Jason Karns <jason@karns.name>
2016-01-22 16:16:15 -0500

    node-build 2.1.3
    Closes #48379.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/node-build.rb
+++ Library/Formula/node-build.rb
@@ -1,9 +1,9 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
-  homepage "https://github.com/OiNutter/node-build"
-  url "https://github.com/OiNutter/node-build/archive/v2.1.2.tar.gz"
-  sha256 "640a7a452f7dd613a6382a673925142404373fd2dca13e841930156cd6d962da"
-  head "https://github.com/OiNutter/node-build.git"
+  homepage "https://github.com/nodenv/node-build"
+  url "https://github.com/nodenv/node-build/archive/v2.1.3.tar.gz"
+  sha256 "50613580cfb4c5a9aea44ac29ce0e4b6412996a91258bf17d118befcc1c86bd8"
+  head "https://github.com/nodenv/node-build.git"
 
   bottle :unneeded
 
