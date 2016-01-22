c26b3a3deaa1e347593a0ab28e0322d1a4bc29ca
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-22 12:42:08 +0000

    graphite2: prefer berkeley mirror

--- Library/Formula/graphite2.rb
+++ Library/Formula/graphite2.rb
@@ -2,7 +2,7 @@ class Graphite2 < Formula
   desc "Smart font renderer for non-Roman scripts"
   homepage "https://scripts.sil.org/cms/scripts/page.php?site_id=projects&item_id=graphite_home"
   url "https://github.com/silnrsi/graphite/archive/1.3.5.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/g/graphite2/graphite2_1.3.5.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/g/graphite2/graphite2_1.3.5.orig.tar.gz"
   sha256 "1c9064c4eb2b3ca03e7fdcfff47125c2304378f6cd4d76c5eba7d4f84d59324b"
   head "https://github.com/silnrsi/graphite.git"
 
