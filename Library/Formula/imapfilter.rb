8b39592075de20fa92cb6f31a86e8fa9a99bb58a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-22 13:03:58 +0000

    imapfilter: update 2.6.5 bottle.

--- Library/Formula/imapfilter.rb
+++ Library/Formula/imapfilter.rb
@@ -5,9 +5,9 @@ class Imapfilter < Formula
   sha256 "0f4b49e1be75ebfd5e3a3bdbff01d68aaafdb7acddc35fc40dcf437029708055"
 
   bottle do
-    sha256 "69a5c8c331a20e7c9bdc4ff32ff7deadee1b0ef8960eafe5a7e2da7be7ead831" => :el_capitan
-    sha256 "cb366a1071b07551e8c1a5775bec6e8baaed0073655caa4d4bcf0fd68123fbf3" => :yosemite
-    sha256 "b57f3f946564b137feeb1f51efb826424d5b1f68de29de86d2efac4fbcf98271" => :mavericks
+    sha256 "2e28a6d04591cd54e0287f1d98487a10d613cd3ac55cc72657ddc9c3dbb95a80" => :el_capitan
+    sha256 "0f35fc488ba1e743aa2f1f4e174679b0db1665f79a502f619c5a3b81b7896ee1" => :yosemite
+    sha256 "e6e262409f8377a23c6b636b60f4269d6f75f02268afa5b2ac8cd9dc52a80606" => :mavericks
   end
 
   depends_on "lua"
