2e38a789fd7612a73af3f44b185245020a7aaafe
Tomas Harkema <tomas@harkema.in>
2016-01-22 13:17:32 +0100

    fix multiline issue

--- Library/Formula/rswift.rb
+++ Library/Formula/rswift.rb
@@ -1,7 +1,9 @@
 class Rswift < Formula
   desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
-  url "https://github.com/mac-cain13/R.swift.git", :tag => "v1.1.1", :revision => "3a6db62164d8f50ccdf43e59894fffb672fd5e3f"
+  url "https://github.com/mac-cain13/R.swift.git",
+    :tag => "v1.1.1",
+    :revision => "3a6db62164d8f50ccdf43e59894fffb672fd5e3f"
   depends_on :xcode => "7.0"
 
   def install
