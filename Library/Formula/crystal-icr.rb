48c5f0691791ee6b67139279ae15813346bbb69c
Marcelo Boeira <me@marceloboeira.com>
2016-01-23 02:02:42 -0200

    crystal-icr: fix CI concerns

--- Library/Formula/crystal-icr.rb
+++ Library/Formula/crystal-icr.rb
@@ -4,12 +4,13 @@ class CrystalIcr < Formula
   url "https://github.com/greyblake/crystal-icr/archive/v0.1.0.tar.gz"
   sha256 "f0189e16f466563b075b26ae71e81572ae120952f8f4b5571bbee41011f471c1"
 
-  version "0.1.0"
-
   depends_on "crystal-lang" => :build
 
   def install
     system "make"
     bin.install "bin/icr"
+  end
+
+  test do
   end
 end
