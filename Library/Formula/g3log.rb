552c2dae318646ac3ca5837bf4f6242022741f94
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-24 15:45:56 +0000

    g3log: add 1.1 bottle.

--- Library/Formula/g3log.rb
+++ Library/Formula/g3log.rb
@@ -4,6 +4,13 @@ class G3log < Formula
   url "https://github.com/KjellKod/g3log/archive/v1.1.tar.gz"
   sha256 "b6374819ec9ba56807eeaa6f11e889f33550b1296524a5e7831ce9adc1218a9c"
 
+  bottle do
+    cellar :any
+    sha256 "bf7272abe06968088c686a82b8eb3e39655adda4d8f6d148b73b34cbf5fd35a0" => :el_capitan
+    sha256 "7476145ed1b74e5b1d1f848392368101eaf0b746e6d2f0d498f6e1d2a1052605" => :yosemite
+    sha256 "12b9bffc6cbd76355294c3bbd127ea9c44f3d694b15e65f9e9fd9c613a2eacff" => :mavericks
+  end
+
   depends_on "cmake" => :build
 
   def install
