79604631b3e7a9e5e08c5363d9e8532089c39c5c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-25 13:51:23 +0000

    ocproxy: add 1.50 bottle.

--- Library/Formula/ocproxy.rb
+++ Library/Formula/ocproxy.rb
@@ -5,6 +5,13 @@ class Ocproxy < Formula
   sha256 "b061d59e0b5729d7a8950d8d4e0004a9739c092a4d5daf58a3bc61b177b24e4f"
   head "https://github.com/cernekee/ocproxy.git"
 
+  bottle do
+    cellar :any
+    sha256 "00c9d71151182359e906514712aa6cbde06f005b0a3fadd948c152c587d6f1fe" => :el_capitan
+    sha256 "e4ae0c97d43496157428d5bf3bbd9d175a1f107f88d9d3bcd6a0bf7efb50ba79" => :yosemite
+    sha256 "1777ecb325d1b0025b73445a9d31808ed6bd3351a6a398e7128f8ba836b33381" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libevent"
