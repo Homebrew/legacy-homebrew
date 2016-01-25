e00d89397c3c38ece35d56ac6ad3573f37859792
maarten-k <maarten.kooyman@surfsara.nl>
2016-01-25 17:13:57 +0100

    uberftp 2.8
    Closes  #41277 and is based on work desribed in #40848.
    The single quotes are replaced by double quotes. The patch JasonAlt/UberFTP#8 mentioned in #40848 solves the problem partly: there is still  another patch needed which obfuscate the formula even more then the replaces now.

--- Library/Formula/uberftp.rb
+++ Library/Formula/uberftp.rb
@@ -1,15 +1,15 @@
 class Uberftp < Formula
   desc "Interactive GridFTP client"
-  homepage 'http://dims.ncsa.illinois.edu/set/uberftp/'
-  url 'https://github.com/JasonAlt/UberFTP/archive/Version_2_8.tar.gz'
-  sha256 '8a397d6ef02bb714bb0cbdb259819fc2311f5d36231783cd520d606c97759c2a'
+  homepage "http://dims.ncsa.illinois.edu/set/uberftp/"
+  url "https://github.com/JasonAlt/UberFTP/archive/Version_2_8.tar.gz"
+  sha256 "8a397d6ef02bb714bb0cbdb259819fc2311f5d36231783cd520d606c97759c2a"
 
   depends_on "globus-toolkit"
 
   def install
     globus = Formula["globus-toolkit"].opt_prefix
 
-    #patches needed since location changed with globus-toolkit versions >= 6.0, patch to upstream not yet merged https://github.com/JasonAlt/UberFTP/pull/8
+    #patches needed since location changed with globus-toolkit versions >= 6.0, patch to upstream not yet merged https://github.com/JasonAlt/UberFTP/pull/8, but solves not whole problem
     inreplace "configure", "globus_location/include/globus/gcc64dbg", "globus_location/libexec/include"
     inreplace "configure", "globus_location/lib64", "globus_location/libexec/lib"
 
