6a50b94885d08fe39705672dbcd4c333e647caf1
Marcelo Boeira <me@marceloboeira.com>
2016-01-25 16:22:22 -0200

    crystal-icr: update crystal dependency

--- Library/Formula/crystal-icr.rb
+++ Library/Formula/crystal-icr.rb
@@ -4,7 +4,7 @@ class CrystalIcr < Formula
   url "https://github.com/greyblake/crystal-icr/archive/v0.1.2.tar.gz"
   sha256 "10a564e3d34524f480cd13214666d48530cb0120bb8139179624a228c6a6b373"
 
-  depends_on "crystal-lang" => :build
+  depends_on "crystal-lang"
 
   def install
     system "make"
