27a1ffa0c8a6280ff1f76ff3b22290cfb8936a15
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-25 14:24:13 +0000

    noti: add 1.3.0 bottle.

--- Library/Formula/noti.rb
+++ Library/Formula/noti.rb
@@ -7,6 +7,13 @@ class Noti < Formula
   sha256 "78ffba8f6d8cb27df06a7c1ebe9d58cf605175c056eca1be241a5f348a117b8f"
   head "https://github.com/variadico/noti.git", :branch => "dev"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7d892998f3bbed1ba19b9e6a1dfe00094d0123cecfa058e608538e05a38524e6" => :el_capitan
+    sha256 "0aaa37cb4cfde091dce33a9a883f1dc2c1f27756400acdc84198c418c8cb2c4c" => :yosemite
+    sha256 "2cef02d159190232c99658be80e397e6f70e78e2e80a71685c710e920afa6c40" => :mavericks
+  end
+
   devel do
     url "https://github.com/variadico/noti/archive/v2.0.0-rc.1.tar.gz"
     sha256 "5ac154855f05ed60c05642030d4f66f9665e51b7032fc57e999c81160dcdf611"
