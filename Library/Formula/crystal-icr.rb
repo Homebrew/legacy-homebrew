c02cbb3082cda8309d70faad2256b71a5ab6f0ff
Marcelo Boeira <me@marceloboeira.com>
2016-01-25 16:20:39 -0200

    crystal-icr: update to v0.1.2

--- Library/Formula/crystal-icr.rb
+++ Library/Formula/crystal-icr.rb
@@ -1,8 +1,8 @@
 class CrystalIcr < Formula
   desc "Interactive console for Crystal programming language"
   homepage "https://github.com/greyblake/crystal-icr"
-  url "https://github.com/greyblake/crystal-icr/archive/v0.1.0.tar.gz"
-  sha256 "f0189e16f466563b075b26ae71e81572ae120952f8f4b5571bbee41011f471c1"
+  url "https://github.com/greyblake/crystal-icr/archive/v0.1.2.tar.gz"
+  sha256 "10a564e3d34524f480cd13214666d48530cb0120bb8139179624a228c6a6b373"
 
   depends_on "crystal-lang" => :build
 
