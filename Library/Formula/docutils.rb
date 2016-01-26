a165bdd5b7200f972923fdef9f988ce6035f36f2
Lee Hanxue <hanxue.developer@gmail.com>
2016-01-26 17:12:48 +0800

    New Formula docutils0.12
    See original PR #44665
    Updated to follow standard Python package convention
    As an example, see [closure-linter](./closure-linter.rb)

--- Library/Formula/docutils.rb
+++ Library/Formula/docutils.rb
@@ -4,8 +4,22 @@ class Docutils < Formula
   url "https://downloads.sourceforge.net/project/docutils/docutils/0.12/docutils-0.12.tar.gz"
   sha256 "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa"
 
+  resource "docutils" do
+    url "https://downloads.sourceforge.net/project/docutils/docutils/0.12/docutils-0.12.tar.gz"
+    sha256 "c7db717810ab6965f66c8cf0398a98c9d8df982da39b4cd7f162911eb89596fa"
+  end
+
   def install
-    system "python", *Language::Python.setup_install_args(prefix)
+    ENV["PYTHONPATH"] = libexec+"lib/python2.7/site-packages"
+
+    resources.each do |r|
+      r.stage { system "python", *Language::Python.setup_install_args(libexec) }
+    end
+
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/rst2*.py"]
+    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
