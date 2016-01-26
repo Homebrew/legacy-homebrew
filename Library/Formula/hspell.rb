7d588570321f74aa6cb7290803f635e597a7fa4a
Roy Shilkrot <roy.shil@gmail.com>
2016-01-26 10:55:37 -0500

    Update hspell.rb

--- Library/Formula/hspell.rb
+++ Library/Formula/hspell.rb
@@ -1,5 +1,5 @@
 class Hspell < Formula
-  desc "The Hspell project is a free Hebrew linguistic project."
+  desc "Free Hebrew linguistic project"
   homepage "http://hspell.ivrix.org.il/"
   url "http://hspell.ivrix.org.il/hspell-1.3.tar.gz"
   sha256 "603c25dcbaa65d171d9065da7369cfe0dc21bda8378bade13b42eda69c8b2fe7"
@@ -15,7 +15,7 @@ class Hspell < Formula
 
     # autoconf needs to pick up on the patched configure.in and create a new ./configure
     # script
-    system "/usr/local/bin/autoconf"
+    system "autoconf"
 
     system "./configure", "--prefix=#{prefix}",
                           "--enable-shared",
@@ -28,7 +28,7 @@ class Hspell < Formula
     File.open("test.txt", "w:ISO8859-8") do |f|
       f.write "שלום"
     end
-    system "hspell", "-l", "test.txt"
+    system "#{bin}/hspell", "-l", "test.txt"
   end
 end
 __END__
