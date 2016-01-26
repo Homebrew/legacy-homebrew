71586ec746a997b26d722533996d587eaba6cad6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-26 00:58:38 +0000

    uberftp: add 2.8 bottle.

--- Library/Formula/uberftp.rb
+++ Library/Formula/uberftp.rb
@@ -4,6 +4,13 @@ class Uberftp < Formula
   url "https://github.com/JasonAlt/UberFTP/archive/Version_2_8.tar.gz"
   sha256 "8a397d6ef02bb714bb0cbdb259819fc2311f5d36231783cd520d606c97759c2a"
 
+  bottle do
+    cellar :any
+    sha256 "e8dce3fad5462c096d9e1c4e605280679f9a79b6b9204401fb10d3449807d2d9" => :el_capitan
+    sha256 "84ab25a3cae1ac0d4aeb2b967b151b396301eb1f7877bb6225ed202847a35cff" => :yosemite
+    sha256 "9c7f1cbb83c268b00a137e608497bafe066ac41a034c256ca55075d1eeb72cfe" => :mavericks
+  end
+
   depends_on "globus-toolkit"
 
   def install
