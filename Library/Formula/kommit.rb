6609b6771b236dd408a76695c6671cceb8fc00b9
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-01-26 08:43:49 +0200

    kommit 0.1.4 (new formula)
        - fixed audit issues for build

--- Library/Formula/kommit.rb
+++ Library/Formula/kommit.rb
@@ -2,13 +2,15 @@ class Kommit < Formula
   desc "More detailed commit messages without committing!"
   homepage "https://github.com/bilgi-webteam/kommit"
   url "https://github.com/bilgi-webteam/kommit/archive/v0.1.4.tar.gz"
-  version "0.1.4"
   sha256 "ffc017790b24c238848f83e483e22874ece73af22d3261a59e465d667e9f3c13"
 
   def install
     bin.install "bin/kommit"
   end
-
+  def caveats; <<-EOS.undent
+    If you like to combine with git, create a symlink;
+      ln -s #{bin}/kommit #{bin}/git-kommit
+    EOS
   test do
     system "git", "init"
     system "#{bin}/kommit", "-m", "Hello"
