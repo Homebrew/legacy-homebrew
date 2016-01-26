78b53e64cfb7ecdf15bb50bdd215f89c066a095c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-26 02:11:05 +0000

    dshb: add 0.1.0 bottle.

--- Library/Formula/dshb.rb
+++ Library/Formula/dshb.rb
@@ -4,6 +4,12 @@ class Dshb < Formula
   url "https://github.com/beltex/dshb/releases/download/v0.1.0/dshb-0.1.0-source.zip"
   sha256 "efed42a2be0dbc6de3b22b314b582b1d6517922b72e08e063f3d1d3810a782f9"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "491d5a425463fb4f328503f91cfe6e28a0785fdbf8ec7a323a366bab54e4158c" => :el_capitan
+    sha256 "94085328f6ef593ca0d00923fabfe43586c1c4b51eaf555ce9c7db9d7db1f486" => :yosemite
+  end
+
   depends_on :xcode => ["7.0", :build]
 
   def install
