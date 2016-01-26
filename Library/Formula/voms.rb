1a51c0d20aa43c8eae3ed394b943e14801319b6f
maarten-k <maarten.kooyman@surfsara.nl>
2016-01-26 13:30:49 +0100

    voms 3.0.6
    Update to VOMS 3.0.6 build with maven

--- Library/Formula/voms.rb
+++ Library/Formula/voms.rb
@@ -1,9 +1,8 @@
 class Voms < Formula
   desc "Virtual organization membership service"
   homepage "https://github.com/italiangrid/voms"
-  url "https://github.com/italiangrid/voms/archive/2_0_11.tar.gz"
-  sha256 "f665712f68fee445bb3247c214a64ecf00a9f858dadd585d26ff076d29e870e0"
-  revision 1
+  url "https://github.com/italiangrid/voms-clients/archive/v3.0.6.tar.gz"
+  sha256 "0faad42ca34ab6492d8db391a6ba31fed52bc79936d9e6f95ff736f92767b39a"
 
   bottle do
     cellar :any
@@ -12,14 +11,20 @@ class Voms < Formula
     sha256 "01ed7c6185bbd2609e958065c1d58bdd8617abccdcfd3d8875faae532d327dea" => :mavericks
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on :java
+  depends_on "maven" => :build
   depends_on "openssl"
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
-    system "make", "install"
+    system "mvn", "package", "-Dmaven.repo.local=$(pwd)/m2repo/", "-Dmaven.javadoc.skip=true"
+    system "tar", "-xf", "target/voms-clients.tar.gz"
+    share.install "voms-clients/share/java"
+    man5.install Dir["voms-clients/share/man/man5/*.5"]
+    man1.install Dir["voms-clients/share/man/man1/*.1"]
+    bin.install Dir["voms-clients/bin/*"]
+  end
+
+  test do
+    system "#{bin}/voms-proxy-info", "--version"
   end
 end
