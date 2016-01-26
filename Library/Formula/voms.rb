dd8f990eaa4973bded47252722af8b2f048f6ccc
Maarten <maarten-k@users.noreply.github.com>
2016-01-26 20:11:37 +0100

    set bottle to unneeded
    - building failed and took inspiration from maven package where bottle:unneeded is used
    - removed whitespace

--- Library/Formula/voms.rb
+++ Library/Formula/voms.rb
@@ -4,13 +4,9 @@ class Voms < Formula
   url "https://github.com/italiangrid/voms-clients/archive/v3.0.6.tar.gz"
   sha256 "0faad42ca34ab6492d8db391a6ba31fed52bc79936d9e6f95ff736f92767b39a"
 
-  bottle do
-    cellar :any
-    sha256 "1324515f992f331be76152f62d01ef9364f0654c82c0995fc8622748e7c7cdd6" => :el_capitan
-    sha256 "eaa56dee0ee58a47babfdf49307d79fddd30d1273512f73021888f24f1d8ee08" => :yosemite
-    sha256 "01ed7c6185bbd2609e958065c1d58bdd8617abccdcfd3d8875faae532d327dea" => :mavericks
-  end
-
+  bottle :unneeded
+  
+  depends_on :java
   depends_on "maven" => :build
   depends_on "openssl"
 
@@ -22,7 +18,7 @@ class Voms < Formula
     man1.install Dir["voms-clients/share/man/man1/*.1"]
     bin.install Dir["voms-clients/bin/*"]
   end
-  
+
   test do
     system "#{bin}/voms-proxy-info", "--version"
   end
