664ce35c3464744f5fadbe5a27a04c2ba295b1f7
takahi-i <takahikoito@gmail.com>
2016-01-27 12:19:36 +0900

    redpen 1.4.3
    Closes #48504.
    
    Signed-off-by: Martin Afanasjew <martin@afanasjew.de>

--- Library/Formula/redpen.rb
+++ Library/Formula/redpen.rb
@@ -1,8 +1,8 @@
 class Redpen < Formula
   desc "Proofreading tool to help writers of technical documentation"
   homepage "http://redpen.cc/"
-  url "https://github.com/recruit-tech/redpen/releases/download/v1.4.2/redpen-1.4.2.tar.gz"
-  sha256 "20f672387555d0f5d5dc25271d60bec149a359278cc9df82248f2885c128da52"
+  url "https://github.com/redpen-cc/redpen/releases/download/redpen-1.4.3/redpen-1.4.3.tar.gz"
+  sha256 "31652d23367f05e77cd9ab2be045844136d2d7772513e23ef503200a54f8c298"
 
   bottle :unneeded
 
