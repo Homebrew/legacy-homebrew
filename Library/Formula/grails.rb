e23585467f5e1a6fabf2cbe29b3f906763dffc63
Christian-Manuel Butzke <chris@fruwe.com>
2016-01-27 10:28:42 +0900

    grails 3.0.12

--- Library/Formula/grails.rb
+++ Library/Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v3.0.9/grails-3.0.9.zip"
-  sha256 "f1bfdec6efd45283c810e29be4433f134118344d2eabea870ae553fb66c864b1"
+  url "https://github.com/grails/grails-core/releases/download/v3.0.12/grails-3.0.12.zip"
+  sha256 "d3b0cf0966a42e3e76ecac593e81c96655b222fa2cc5b5940d1842c8a4753c11"
 
   bottle :unneeded
 
