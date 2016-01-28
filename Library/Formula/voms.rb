96a3736376410c9e3c9dd23cd3d7d7e7da1b47cd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-28 22:16:19 +0000

    voms: update 3.0.6 bottle.

--- Library/Formula/voms.rb
+++ Library/Formula/voms.rb
@@ -5,10 +5,10 @@ class Voms < Formula
   sha256 "0faad42ca34ab6492d8db391a6ba31fed52bc79936d9e6f95ff736f92767b39a"
 
   bottle do
-    cellar :any
-    sha256 "1324515f992f331be76152f62d01ef9364f0654c82c0995fc8622748e7c7cdd6" => :el_capitan
-    sha256 "eaa56dee0ee58a47babfdf49307d79fddd30d1273512f73021888f24f1d8ee08" => :yosemite
-    sha256 "01ed7c6185bbd2609e958065c1d58bdd8617abccdcfd3d8875faae532d327dea" => :mavericks
+    cellar :any_skip_relocation
+    sha256 "e2a6c2ebe242fccfc3935156373a0086adf7f001cb78a219e60f676963d6d3d6" => :el_capitan
+    sha256 "b3fc4b76fd0e255a6da44e535e970b9c03f4380e50096d94fcb6492cc545d33a" => :yosemite
+    sha256 "ada0fac58d31d9e8d794c5236cc42816722e358f1b5c4c26931a641a885e8fdd" => :mavericks
   end
 
   depends_on :java
