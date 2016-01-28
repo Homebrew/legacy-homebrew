88df0230c805fc3c55cf7b920ec5379efe08c331
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-28 13:53:42 +0000

    mpdviz: fix strict audit
    Closes #48551.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/mpdviz.rb
+++ Library/Formula/mpdviz.rb
@@ -6,6 +6,10 @@ class Mpdviz < Formula
   url "https://github.com/lucy/mpdviz/archive/0.4.6.tar.gz"
   sha256 "c34243ec3f3d91adbc36d608d5ba7082ff78870f2fd76a6650d5fb3218cc2ba3"
 
+  depends_on "pkg-config" => :build
+  depends_on "go" => :build
+  depends_on "fftw"
+
   go_resource "github.com/lucy/go-fftw" do
     url "https://github.com/lucy/go-fftw.git",
       :revision => "37bfa0d3053b133f7067e9524611a7a963294124"
@@ -25,10 +29,6 @@ class Mpdviz < Formula
     url "https://github.com/mattn/go-runewidth.git",
       :revision => "36f63b8223e701c16f36010094fb6e84ffbaf8e0"
   end
-
-  depends_on "pkg-config" => :build
-  depends_on "go" => :build
-  depends_on "fftw"
 
   def install
     ENV["GOPATH"] = buildpath
