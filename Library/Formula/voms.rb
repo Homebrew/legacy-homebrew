896d232ed423a46bf76817216735eafc67640ad5
Maarten <maarten-k@users.noreply.github.com>
2016-01-28 12:44:48 +0100

    specify maven repository path
    - specify maven repository path  so it will not create a dir in the home path when building
    - re-enabled the bottles since I do not see a reason it should not work any more

--- Library/Formula/voms.rb
+++ Library/Formula/voms.rb
@@ -4,14 +4,19 @@ class Voms < Formula
   url "https://github.com/italiangrid/voms-clients/archive/v3.0.6.tar.gz"
   sha256 "0faad42ca34ab6492d8db391a6ba31fed52bc79936d9e6f95ff736f92767b39a"
 
-  bottle :unneeded
-  
+  bottle do
+    cellar :any
+    sha256 "1324515f992f331be76152f62d01ef9364f0654c82c0995fc8622748e7c7cdd6" => :el_capitan
+    sha256 "eaa56dee0ee58a47babfdf49307d79fddd30d1273512f73021888f24f1d8ee08" => :yosemite
+    sha256 "01ed7c6185bbd2609e958065c1d58bdd8617abccdcfd3d8875faae532d327dea" => :mavericks
+  end
+
   depends_on :java
   depends_on "maven" => :build
   depends_on "openssl"
 
   def install
-    system "mvn", "package"
+    system "mvn", "package", "-Dmaven.repo.local=$(pwd)/m2repo/"
     system "tar", "-xf", "target/voms-clients.tar.gz"
     share.install "voms-clients/share/java"
     man5.install Dir["voms-clients/share/man/man5/*.5"]
