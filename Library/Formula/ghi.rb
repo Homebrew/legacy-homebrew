d336d4e1f14de718daf14de7b82ad1c6cc6a98b1
Alex Chesters <AlexChesters@users.noreply.github.com>
2016-01-29 08:09:51 +0000

    ghi 1.1.0
    Update ghi to 1.1.0
    
    Closes #48584.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ghi.rb
+++ Library/Formula/ghi.rb
@@ -1,8 +1,8 @@
 class Ghi < Formula
   desc "Work on GitHub issues on the command-line"
   homepage "https://github.com/stephencelis/ghi"
-  url "https://github.com/stephencelis/ghi/archive/1.0.4.tar.gz"
-  sha256 "6fd0442a4b64a66ee27f0d09caf27bcc117737bd997653b94e8404ef7795f963"
+  url "https://github.com/stephencelis/ghi/archive/1.1.0.tar.gz"
+  sha256 "90ee10cefe9b83514eadb185b339a7147627c54c508e28f292e56719af6bfcb6"
   head "https://github.com/stephencelis/ghi.git"
 
   bottle :unneeded
