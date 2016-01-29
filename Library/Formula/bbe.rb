99d9c3a96f59d3883486416336965be9cc76dee7
Guillaume Hain <zedtux@zedroot.org>
2016-01-30 00:35:22 +0100

    bbe: fix the homepage
    The homepage wasn't reachable (getaddrinfo: Name or service not known)
    
    Closes #48616.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/bbe.rb
+++ Library/Formula/bbe.rb
@@ -1,6 +1,6 @@
 class Bbe < Formula
   desc "Sed-like editor for binary files"
-  homepage "http://bbe-.sourceforge.net"
+  homepage "http://sourceforge.net/projects/bbe-/"
   url "https://downloads.sourceforge.net/project/bbe-/bbe/0.2.2/bbe-0.2.2.tar.gz"
   sha256 "baaeaf5775a6d9bceb594ea100c8f45a677a0a7d07529fa573ba0842226edddb"
 
