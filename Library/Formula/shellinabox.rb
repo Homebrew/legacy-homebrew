0096d793a7ce17fef415424fae804b21630de82c
Massimo Santini <massimo.santini@gmail.com>
2016-01-29 21:33:12 +0100

    shellinabox 2.19
    shellinabox 2.19
    
    Bumped version and updated homepage from `code.google.com´ to ´github.com´.
    
    Closes #48613.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/shellinabox.rb
+++ Library/Formula/shellinabox.rb
@@ -1,12 +1,16 @@
 class Shellinabox < Formula
   desc "Export command-line tools to web based terminal emulator"
-  homepage "https://code.google.com/p/shellinabox/"
-  url "https://shellinabox.googlecode.com/files/shellinabox-2.14.tar.gz"
-  sha256 "4126eb7070869787c161102cc2781d24d1d50c8aef4e5a3e1b5446e65d691071"
+  homepage "https://github.com/shellinabox/shellinabox"
+  url "https://github.com/shellinabox/shellinabox/archive/v2.19.tar.gz"
+  sha256 "d25ba9f72f04471fc1a8a564c65ef466c4553280ff3eeb365ed9c897d05ed2da"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "autoreconf", "-fiv"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 
