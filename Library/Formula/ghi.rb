18d39a5f0dc3d986d24d130baf85facd826957eb
Alex Chesters <AlexChesters@users.noreply.github.com>
2016-01-29 08:10:12 +0000

    Update ghi to 1.1.0

--- Library/Formula/ghi.rb
+++ Library/Formula/ghi.rb
@@ -1,7 +1,7 @@
 class Ghi < Formula
   desc "Work on GitHub issues on the command-line"
   homepage "https://github.com/stephencelis/ghi"
-  url "https://github.com/stephencelis/ghi/archive/1.0.4.tar.gz"
+  url "https://github.com/stephencelis/ghi/archive/1.1.0.tar.gz"
   sha256 "90ee10cefe9b83514eadb185b339a7147627c54c508e28f292e56719af6bfcb6"
   head "https://github.com/stephencelis/ghi.git"
 
