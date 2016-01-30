6723363136fda6d1d2ab8c145a8d312b824c7c6b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-30 21:54:45 +0000

    sagittarius-scheme: add 0.7.0 bottle.

--- Library/Formula/sagittarius-scheme.rb
+++ Library/Formula/sagittarius-scheme.rb
@@ -5,6 +5,13 @@ class SagittariusScheme < Formula
   sha256 "365ccb0c0779717b04111ea86962d24a4841c07289426003f24782295fbe4658"
   head "https://bitbucket.org/ktakashi/sagittarius-scheme", :using => :hg
 
+  bottle do
+    cellar :any
+    sha256 "e89e4a85294dfcb8c82be1ead9c76a05deee6a225de7dc2f51d640c38749eea4" => :el_capitan
+    sha256 "7943af9ab8b85d06f8ed191835b74ba3d255f09382915c91e3a24e5b79880309" => :yosemite
+    sha256 "7e6ee2e6e99136ce7c72588df855a440610e90e0e2737dfd22967cafc3713b4b" => :mavericks
+  end
+
   option "without-docs", "Build without HTML docs"
 
   depends_on "cmake" => :build
