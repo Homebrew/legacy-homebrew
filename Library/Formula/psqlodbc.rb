c1edc713fa509c435d243451c113870ad62a0e24
Dominyk Tiller <dominyktiller@gmail.com>
2016-01-30 15:14:16 +0000

    psqlodbc 09.05.0100
    Closes #48376.
    
    Closes #48639.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/psqlodbc.rb
+++ Library/Formula/psqlodbc.rb
@@ -1,17 +1,14 @@
 class Psqlodbc < Formula
   desc "Official PostgreSQL ODBC driver"
   homepage "https://odbc.postgresql.org"
+  url "https://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.05.0100.tar.gz"
+  sha256 "c53612db422826bfa3023ea8c75cb6c61f113a797a3323002ed645133491d1bd"
 
-  stable do
-    url "https://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.03.0400.tar.gz"
-    sha256 "de77dfa89dba0a159afc57b2e312ca6e9075dd92b761c7cc700c0450ba02b56b"
-
-    # fixes error "conflicting types for '__builtin___strlcat_chk'"
-    # reported by email and fixed in HEAD on 6 May 2015
-    patch do
-      url "http://git.postgresql.org/gitweb/?p=psqlodbc.git;a=commitdiff_plain;h=355ac88016a53bf2aef79c84b69435ce52414a16;hp=0035432595095dfb862aa464836ba3215affd8bf"
-      sha256 "07406527c043074b8492902f67bbc1a74ac3d7cdf1633535749564b63b0189c2"
-    end
+  bottle do
+    cellar :any
+    sha256 "cb3f7f8239315732ba9e380b149edc0d14e74a3e7b8c87563dad5f23a463e8ec" => :yosemite
+    sha256 "4a1d5a33b5910e87caebf9414bfe1f699b1b15b8da6401ccb683babe6b652206" => :mavericks
+    sha256 "03cfeabccdb83370de666f11525b12f4775c7c79977740c1725334f90f4e1b91" => :mountain_lion
   end
 
   head do
@@ -19,13 +16,6 @@ class Psqlodbc < Formula
     depends_on "automake" => :build
     depends_on "autoconf" => :build
     depends_on "libtool" => :build
-  end
-
-  bottle do
-    cellar :any
-    sha256 "cb3f7f8239315732ba9e380b149edc0d14e74a3e7b8c87563dad5f23a463e8ec" => :yosemite
-    sha256 "4a1d5a33b5910e87caebf9414bfe1f699b1b15b8da6401ccb683babe6b652206" => :mavericks
-    sha256 "03cfeabccdb83370de666f11525b12f4775c7c79977740c1725334f90f4e1b91" => :mountain_lion
   end
 
   depends_on "openssl"
@@ -41,6 +31,7 @@ class Psqlodbc < Formula
   end
 
   test do
-    assert_equal "SUCCESS: Loaded #{lib}/psqlodbcw.so\n", shell_output("#{Formula["unixodbc"].bin}/dltest #{lib}/psqlodbcw.so")
+    output = shell_output("#{Formula["unixodbc"].bin}/dltest #{lib}/psqlodbcw.so")
+    assert_equal "SUCCESS: Loaded #{lib}/psqlodbcw.so\n", output
   end
 end
