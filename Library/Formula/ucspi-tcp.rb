e473afedc2d6b47fc804437ed53b8de12cb8dfdb
Viktor Szakats <vszakats@users.noreply.github.com>
2016-01-30 17:12:16 +0100

    ucspi-tcp: use https urls
    Closes #48648.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ucspi-tcp.rb
+++ Library/Formula/ucspi-tcp.rb
@@ -1,11 +1,11 @@
 class UcspiTcp < Formula
   desc "Tools for building TCP client-server applications"
-  homepage "http://cr.yp.to/ucspi-tcp.html"
-  url "http://cr.yp.to/ucspi-tcp/ucspi-tcp-0.88.tar.gz"
+  homepage "https://cr.yp.to/ucspi-tcp.html"
+  url "https://cr.yp.to/ucspi-tcp/ucspi-tcp-0.88.tar.gz"
   sha256 "4a0615cab74886f5b4f7e8fd32933a07b955536a3476d74ea087a3ea66a23e9c"
 
   patch do
-    url "http://www.fefe.de/ucspi/ucspi-tcp-0.88-ipv6.diff19.bz2"
+    url "https://www.fefe.de/ucspi/ucspi-tcp-0.88-ipv6.diff19.bz2"
     sha256 "35952cd290d714452c840580126004cbae8db65b1632df67ac9c8fad7d1f9ace"
   end
 
