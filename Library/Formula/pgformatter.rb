608d79b0352f23121d7be0601417d19fce2a716c
Matthias Nüßler <m.nuessler@web.de>
2016-01-31 00:53:44 +0100

    pgformatter 1.5
    Closes #48658.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/pgformatter.rb
+++ Library/Formula/pgformatter.rb
@@ -1,14 +1,23 @@
 class Pgformatter < Formula
   desc "PostgreSQL syntax beautifier"
   homepage "http://sqlformat.darold.net/"
-  url "https://github.com/darold/pgFormatter/archive/v1.4.tar.gz"
-  sha256 "d8bb04f1f0d35cbae9aeb8ae91273f3d13ce07305226605c1b2db161010bedc9"
+  url "https://github.com/darold/pgFormatter/archive/v1.5.tar.gz"
+  sha256 "ab57195a1489ed4daf2356642d5b74885f497e39b94f5edc39c2488755261d03"
 
   def install
+    # Fix path to Perl modules. Per default, the script expects to
+    # find them in a lib directory beneath it's own path.
+    inreplace "pg_format", "$FindBin::Bin/lib", libexec
+
     system "perl", "Makefile.PL", "DESTDIR=."
     system "make", "install"
+
     bin.install "blib/script/pg_format"
+    libexec.install "blib/lib/pgFormatter"
     man1.install "blib/man1/pg_format.1"
+    man3.install "blib/man3/pgFormatter::Beautify.3pm"
+    man3.install "blib/man3/pgFormatter::CGI.3pm"
+    man3.install "blib/man3/pgFormatter::CLI.3pm"
   end
 
   test do
