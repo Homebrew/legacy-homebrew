25860002c316dee2b7819203cd6d9d1d27c86a77
Viktor Szakats <vszakats@users.noreply.github.com>
2016-01-30 17:11:48 +0100

    djbdns: use https urls

--- Library/Formula/djbdns.rb
+++ Library/Formula/djbdns.rb
@@ -1,7 +1,7 @@
 class Djbdns < Formula
   desc "D.J. Bernstein's DNS tools"
-  homepage "http://cr.yp.to/djbdns.html"
-  url "http://cr.yp.to/djbdns/djbdns-1.05.tar.gz"
+  homepage "https://cr.yp.to/djbdns.html"
+  url "https://cr.yp.to/djbdns/djbdns-1.05.tar.gz"
   sha256 "3ccd826a02f3cde39be088e1fc6aed9fd57756b8f970de5dc99fcd2d92536b48"
 
   bottle do
