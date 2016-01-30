5858d296c4f1cbf349619ecb1a1bdb8d19dbf4b1
Viktor Szakats <vszakats@users.noreply.github.com>
2016-01-30 17:11:35 +0100

    daemontools: use https urls

--- Library/Formula/daemontools.rb
+++ Library/Formula/daemontools.rb
@@ -1,7 +1,7 @@
 class Daemontools < Formula
   desc "Collection of tools for managing UNIX services"
-  homepage "http://cr.yp.to/daemontools.html"
-  url "http://cr.yp.to/daemontools/daemontools-0.76.tar.gz"
+  homepage "https://cr.yp.to/daemontools.html"
+  url "https://cr.yp.to/daemontools/daemontools-0.76.tar.gz"
   sha256 "a55535012b2be7a52dcd9eccabb9a198b13be50d0384143bd3b32b8710df4c1f"
 
   bottle do
