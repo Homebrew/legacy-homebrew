1e9c3e264e76c576c69d7a1c4a4bdf0dba2fd1a1
cytopia <cytopia@everythingcli.org>
2016-01-31 18:42:13 +0100

    description length

--- Library/Formula/mysqldump-secure.rb
+++ Library/Formula/mysqldump-secure.rb
@@ -1,5 +1,5 @@
 class MysqldumpSecure < Formula
-  desc "Encrypted mysqldump with compression, logging and Nagios integration"
+  desc "Encrypted mysqldump with compression and logging"
   homepage "https://github.com/cytopia/mysqldump-secure"
   url "https://github.com/cytopia/mysqldump-secure/archive/0.11.3.tar.gz"
   sha256 "046eb5da52d17f0a22d24a53380d820ce50dcff21b55047549ac61366fbc16a1"
