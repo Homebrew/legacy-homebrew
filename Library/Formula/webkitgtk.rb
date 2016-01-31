32e5b3b2b04e8759c09363de6b670a675caa2108
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-01-31 15:49:14 +0530

    webkitgtk 2.10.7

--- Library/Formula/webkitgtk.rb
+++ Library/Formula/webkitgtk.rb
@@ -1,8 +1,8 @@
 class Webkitgtk < Formula
   desc "Full-featured Gtk+ port of the WebKit rendering engine"
   homepage "http://webkitgtk.org"
-  url "http://webkitgtk.org/releases/webkitgtk-2.10.6.tar.xz"
-  sha256 "0b77abcc72a274e8f11e9d9f0f6b116dadeeb8b9d5f22b17b6604ed9ae3fe83c"
+  url "http://webkitgtk.org/releases/webkitgtk-2.10.7.tar.xz"
+  sha256 "990d62c82ed6dede31a6ff0a82d847f16b812842ff3e1093d17113627652864e"
 
   bottle do
     sha256 "a885beb57503411e9004816e6ce65c48626b9d08941af8d14be2f509cd5da97f" => :el_capitan
