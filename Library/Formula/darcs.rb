3970e0e199a7d09f882578abff104ec83608b358
BrewTestBot <brew-test-bot@googlegroups.com>
2016-01-31 19:03:04 +0000

    darcs: update 2.10.3 bottle.

--- Library/Formula/darcs.rb
+++ Library/Formula/darcs.rb
@@ -9,9 +9,9 @@ class Darcs < Formula
   sha256 "ca00c40d08276f94868c7c1bbc6dbd9b6b41a15c1907c34947aaa51d4dbbf642"
 
   bottle do
-    sha256 "7233b3d0fd8fbae5c3dd6f8229fd2755ce047961c6692eaeeb63064f7184fd29" => :el_capitan
-    sha256 "62a0c990b8585130d6c8955d2884ec07030dd1b3c5b0f711126a9dd9b5762d02" => :yosemite
-    sha256 "edd6a36b7146e20cf5a6a3bbe70737f6013f855bbb881523bdf460e416d9626a" => :mavericks
+    sha256 "a65c9d857fd868ff6768c3076511b6bfe5d11f893b58a3d943ae7b0319db73d3" => :el_capitan
+    sha256 "7e76c59e699d4941880fea6986d13d62ceb4d0b60736e54f813f4d79ec4810da" => :yosemite
+    sha256 "c7f60a61ab519b61d7ea229e3e67c9d1c75b1c39d49da5b07dfcdf8fe4e11658" => :mavericks
   end
 
   depends_on "ghc" => :build
