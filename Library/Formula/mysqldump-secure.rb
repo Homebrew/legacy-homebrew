44aa45a7b7a8d89ff7d5213ae3db6c6cea6d8482
cytopia <cytopia@everythingcli.org>
2016-01-31 18:32:55 +0100

    FIxed whitespace problems

--- Library/Formula/mysqldump-secure.rb
+++ Library/Formula/mysqldump-secure.rb
@@ -1,5 +1,5 @@
 class MysqldumpSecure < Formula
-  desc "Encrypted mysqldump with compression, logging and Nagios integration."
+  desc "Encrypted mysqldump with compression, logging and Nagios integration"
   homepage "https://github.com/cytopia/mysqldump-secure"
   url "https://github.com/cytopia/mysqldump-secure/archive/0.11.3.tar.gz"
   sha256 "046eb5da52d17f0a22d24a53380d820ce50dcff21b55047549ac61366fbc16a1"
@@ -9,9 +9,8 @@ class MysqldumpSecure < Formula
     system "make"
     system "make", "reinstall"
   end
-  
+
   test do
     system bin/"mysqldump-secure 2>&1 | grep ''"
   end
-
 end
