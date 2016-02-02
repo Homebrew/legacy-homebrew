3cf75d70764a9d8ae2bcb373c6644712d3930d6f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-02 11:35:19 +0000

    wires: update 0.6.2 bottle.

--- Library/Formula/wires.rb
+++ Library/Formula/wires.rb
@@ -6,9 +6,9 @@ class Wires < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0b58c4a03483a791a67a3883ebb07b70c0e16a125d3424f20a299bb2c25f50ab" => :el_capitan
-    sha256 "1bbcdac65c0d297932d132e16ee09753d6c0ae2eaa98d1a124a23eab50486d52" => :yosemite
-    sha256 "cd4173ade452891eae61ef95480abd45cf84cd1716ad8d603e30e10bce356c3b" => :mavericks
+    sha256 "b7408f7a32326df3200fc88ace88a3d19a285cbba1a79631947b150422b43cef" => :el_capitan
+    sha256 "4037fdfaaa442386412bfcb66ed40ab6527b584d2b76e41d5399d018d03b1eba" => :yosemite
+    sha256 "f265f4acd2265a1be067ab453d47aada5db299c948843a8d32e74754e45374d2" => :mavericks
   end
 
   depends_on "rust" => :build
