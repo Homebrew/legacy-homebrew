51a6e1e07da5d6d71283a1beeaf9b335876f939a
Elethom Hunter <elethomhunter@gmail.com>
2016-02-03 10:51:08 +0800

    contacts-phonetic: Update 0.1.1

--- Library/Formula/contacts-phonetic.rb
+++ Library/Formula/contacts-phonetic.rb
@@ -1,8 +1,8 @@
 class ContactsPhonetic < Formula
   desc "Add phonetic names to contacts."
   homepage "https://github.com/Elethom/ContactsUtil-PhoneticName"
-  url "https://github.com/Elethom/ContactsUtil-PhoneticName/archive/0.1.tar.gz"
-  sha256 "c593ebfbedb3beac202f1de51189cf2b92c4a8d3b9da2fad633bb31f6b58e731"
+  url "https://github.com/Elethom/ContactsUtil-PhoneticName/archive/0.1.1.tar.gz"
+  sha256 "3816152a62674870489b5780aa10b6bdb5adff9e55f79c19b6529a833859e15b"
 
   depends_on :macos => :el_capitan
 
