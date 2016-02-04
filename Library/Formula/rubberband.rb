ee4023476d41e3b3eafef0db79cbdea755e2f1de
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-04 04:59:45 +0000

    rubberband: update 1.8.1 bottle.

--- Library/Formula/rubberband.rb
+++ Library/Formula/rubberband.rb
@@ -17,9 +17,10 @@ class Rubberband < Formula
   end
   bottle do
     cellar :any
-    sha256 "e1890156ef9c638993fc9410bc4b5ccee113a14bb59861ebd25a6d68501f83d7" => :el_capitan
-    sha256 "746b65592ab6a93e990a7930a22aaa5c7c559e1bfdff7da8d3f292168e79e512" => :yosemite
-    sha256 "06c1b9d0bea22637edec00a3bd41b118472caee55902b6c1f8ea8941d922a3f7" => :mavericks
+    revision 1
+    sha256 "ec6ec212a0173ba661601b2fb5ae1dace5dab1100688d3b5c9a460796eae705b" => :el_capitan
+    sha256 "6a62c8da1d779672bf0ef276656b2dfa5edf885e704a875c606a27b9aea863fe" => :yosemite
+    sha256 "5ca9579f1b84a3a843e5b52654f41b25e4c02fdc5df05a0966c6d8627843dac4" => :mavericks
   end
 
   depends_on "pkg-config" => :build
