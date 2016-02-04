Homebrew/legacy-homebrew

commit 3f6a3553ecad4f61b24b77a3df117e076905e672

config: fail when HOMEBREW_BREW_FILE is unset.
Not sure why this is happening (beyond the Chef cookbook stupidly
deciding to not call through `bin/brew`) but fail and print a scary
looking error to hope to point people in the right direction.

Closes #48261.

Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

Library/Homebrew/config.rb
@@ -26,8 +26,9 @@ undef cache
 # Where brews installed via URL are cached
 HOMEBREW_CACHE_FORMULA = HOMEBREW_CACHE+"Formula"
 
-unless defined? HOMEBREW_BREW_FILE
-  HOMEBREW_BREW_FILE = ENV["HOMEBREW_BREW_FILE"] || which("brew").to_s
+HOMEBREW_BREW_FILE = ENV["HOMEBREW_BREW_FILE"]
+unless HOMEBREW_BREW_FILE
+  odie "HOMEBREW_BREW_FILE was not exported! Please call bin/brew directly!"
 end
 
 # Where we link under

Library/brew.rb
@@ -2,8 +2,6 @@
 
 std_trap = trap("INT") { exit! 130 } # no backtrace thanks
 
-HOMEBREW_BREW_FILE = ENV["HOMEBREW_BREW_FILE"]
-
 require "pathname"
 HOMEBREW_LIBRARY_PATH = Pathname.new(__FILE__).realpath.parent.join("Homebrew")
 $:.unshift(HOMEBREW_LIBRARY_PATH.to_s)

commit 2a2872e14126d91c55fcc6c801a2469a88e99b8b

HOMEBREW_BREW_FILE was unset for the tests
This fixes a regression introduced by 3f6a355, which caused
HOMEBREW_BREW_FILE to be unset while running the tests.

Patch provided by Xu Cheng.

Library/Homebrew/test/lib/config.rb
@@ -1,6 +1,7 @@
 require "tmpdir"
 require "pathname"
 
+HOMEBREW_BREW_FILE = ENV["HOMEBREW_BREW_FILE"]
 HOMEBREW_TEMP = Pathname.new(ENV["HOMEBREW_TEMP"] || Dir.tmpdir)
 
 TEST_TMPDIR = ENV.fetch("HOMEBREW_TEST_TMPDIR") { |k|

Library/Homebrew/test/test_integration_cmds.rb
@@ -7,6 +7,12 @@ require "pathname"
 class IntegrationCommandTests < Homebrew::TestCase
   def setup
     @cmd_id_index = 0 # Assign unique IDs to invocations of `cmd_output`.
+    (HOMEBREW_PREFIX/"bin").mkpath
+    FileUtils.touch HOMEBREW_PREFIX/"bin/brew"
+  end
+
+  def teardown
+    (HOMEBREW_PREFIX/"bin").rmtree
   end
 
   def cmd_id_from_args(args)