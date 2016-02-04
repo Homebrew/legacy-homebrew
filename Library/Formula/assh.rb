8a1d2fb76bd979782685f80aaeb3da6ccf399d40
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-04 15:18:59 +0000

    assh: add 2.2.0 bottle.

--- Library/Formula/assh.rb
+++ Library/Formula/assh.rb
@@ -8,6 +8,13 @@ class Assh < Formula
 
   head "https://github.com/moul/advanced-ssh-config.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3327d87a59a3b905b88b3c356448acec7af015888c2baac10dac21584342d4db" => :el_capitan
+    sha256 "9ebee2d29a5cc75e7406f52a9ec77da25913b688579e345c385820e08a2fb55c" => :yosemite
+    sha256 "002eb1502db3837377c08b829096f6ba7b1e6c23d3b314afdef616b293ba04c2" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
