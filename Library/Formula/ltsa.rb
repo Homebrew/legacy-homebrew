5ead31bea25219b3b2bab1a9ec53d288d282280e
Ollie Ford <me@ojford.com>
2016-02-05 21:53:59 +0000

    download just required jar

--- Library/Formula/ltsa.rb
+++ Library/Formula/ltsa.rb
@@ -1,14 +1,13 @@
 class Ltsa < Formula
   desc "LTS Analyser"
   homepage "http://www.doc.ic.ac.uk/~jnm/book/ltsa/LTSA_applet.html"
-  url "http://www.doc.ic.ac.uk/~jnm/book/ltsa/ltsatool.zip"
+  url "http://www.doc.ic.ac.uk/~jnm/book/ltsa/ltsa.jar"
   version "3.0"
-  sha256 "9ed894c4f2ae22e119a4f48e6e3f36b38b08f8fe85a6ac85564f4c5045fe9046"
-
+  sha256 "2068bfd818f2ea3558ceb78f33ec127a8fc0869c944ee98eacf1ab6d762347de"
   depends_on :java => "2"
 
   def install
-    prefix.install Dir["*"]
+    prefix.install "ltsa.jar"
     system "echo '#!/bin/sh\\nnohup java -jar #{prefix}/ltsa.jar &\' > ltsa"
     bin.install "ltsa"
   end
