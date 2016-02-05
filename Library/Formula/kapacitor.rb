a19913ca537ced8512c0fc8f33a36bba6674704e
gunnaraasen <gunnar.r.aasen@gmail.com>
2016-02-05 12:00:56 -0800

    kapacitor 0.10.0
    Closes #48888.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/kapacitor.rb
+++ Library/Formula/kapacitor.rb
@@ -2,12 +2,12 @@ require "language/go"
 
 class Kapacitor < Formula
   desc "Open source time series data processor"
-  homepage "https://github.com/influxdb/kapacitor"
-  url "https://github.com/influxdb/kapacitor.git",
-    :tag => "v0.2.0",
-    :revision => "18b2061efb263aae326f51bd74d7023a221996ef"
+  homepage "https://github.com/influxdata/kapacitor"
+  url "https://github.com/influxdata/kapacitor.git",
+    :tag => "v0.10.0",
+    :revision => "35c9c6fe6543f9d65779d10723ea3a70657d8bba"
 
-  head "https://github.com/influxdb/kapacitor.git"
+  head "https://github.com/influxdata/kapacitor.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,119 +18,124 @@ class Kapacitor < Formula
 
   depends_on "go" => :build
 
+  go_resource "github.com/BurntSushi/toml" do
+    url "https://github.com/BurntSushi/toml.git",
+    :revision => "5c4df71dfe9ac89ef6287afc05e4c1b16ae65a1e"
+  end
+
   go_resource "github.com/armon/go-metrics" do
     url "https://github.com/armon/go-metrics.git",
-      :revision => "6c5fa0d8f48f4661c9ba8709799c88d425ad20f0"
+    :revision => "345426c77237ece5dab0e1605c3e4b35c3f54757"
   end
 
   go_resource "github.com/boltdb/bolt" do
     url "https://github.com/boltdb/bolt.git",
-      :revision => "6e1ca38c6a73025366cd8705553b404746ee6e63"
-  end
-
-  go_resource "github.com/gogo/protobuf" do
-    url "https://github.com/gogo/protobuf.git",
-      :revision => "d3235f01ecae4901dd9f7ea6af57a352c0189deb"
-  end
-
-  go_resource "github.com/gorhill/cronexpr" do
-    url "https://github.com/gorhill/cronexpr.git",
-      :revision => "a557574d6c024ed6e36acc8b610f5f211c91568a"
-  end
-
-  go_resource "github.com/hashicorp/go-msgpack" do
-    url "https://github.com/hashicorp/go-msgpack.git",
-      :revision => "fa3f63826f7c23912c15263591e65d54d080b458"
-  end
-
-  go_resource "github.com/hashicorp/raft" do
-    url "https://github.com/hashicorp/raft.git",
-      :revision => "d136cd15dfb7876fd7c89cad1995bc4f19ceb294"
-  end
-
-  go_resource "github.com/hashicorp/raft-boltdb" do
-    url "https://github.com/hashicorp/raft-boltdb.git",
-      :revision => "d1e82c1ec3f15ee991f7cc7ffd5b67ff6f5bbaee"
-  end
-
-  go_resource "github.com/influxdb/enterprise-client" do
-    url "https://github.com/influxdb/enterprise-client.git",
-      :revision => "25665cba4f54fa822546c611c9414ac31aa10faa"
-  end
-
-  go_resource "github.com/influxdb/influxdb" do
-    url "https://github.com/influxdb/influxdb.git",
-      :revision => "45502d288f79b60051a7ec9260ad59dacb5b1a6c"
-  end
-
-  go_resource "github.com/twinj/uuid" do
-    url "https://github.com/twinj/uuid.git",
-      :revision => "89173bcdda19db0eb88aef1e1cb1cb2505561d31"
-  end
-
-  go_resource "golang.org/x/crypto" do
-    url "https://go.googlesource.com/crypto.git",
-      :revision => "beef0f4390813b96e8e68fd78570396d0f4751fc"
-  end
-
-  go_resource "gopkg.in/fatih/pool.v2" do
-    url "https://gopkg.in/fatih/pool.v2.git",
-      :revision => "cba550ebf9bce999a02e963296d4bc7a486cb715"
-  end
-
-  go_resource "github.com/BurntSushi/toml" do
-    url "https://github.com/BurntSushi/toml.git",
-      :revision => "056c9bc7be7190eaa7715723883caffa5f8fa3e4"
-  end
-
-  go_resource "github.com/kimor79/gollectd" do
-    url "https://github.com/kimor79/gollectd.git",
-      :revision => "61d0deeb4ffcc167b2a1baa8efd72365692811bc"
-  end
-
-  go_resource "gopkg.in/gomail.v2" do
-    url "https://gopkg.in/gomail.v2.git",
-      :revision => "41f3572897373c5538c50a2402db15db079fa4fd"
-  end
-
-  go_resource "github.com/serenize/snaker" do
-    url "https://github.com/serenize/snaker.git",
-      :revision => "d88cc11617e4f179fa2c821a60908d5fabd7bdad"
-  end
-
-  go_resource "github.com/shurcooL/markdownfmt" do
-    url "https://github.com/shurcooL/markdownfmt.git",
-      :revision => "c6ad3c69d14e5089336af1e41e595f0826df1b82"
+    :revision => "ee4a0888a9abe7eefe5a0992ca4cb06864839873"
   end
 
   go_resource "github.com/dustin/go-humanize" do
     url "https://github.com/dustin/go-humanize.git",
-      :revision => "64dbdae0d393b7d71480a6dace78456396b55286"
+    :revision => "8929fe90cee4b2cb9deb468b51fb34eba64d1bf0"
+  end
+
+  go_resource "github.com/gogo/protobuf" do
+    url "https://github.com/gogo/protobuf.git",
+    :revision => "82d16f734d6d871204a3feb1a73cb220cc92574c"
+  end
+
+  go_resource "github.com/gorhill/cronexpr" do
+    url "https://github.com/gorhill/cronexpr.git",
+    :revision => "a557574d6c024ed6e36acc8b610f5f211c91568a"
+  end
+
+  go_resource "github.com/hashicorp/go-msgpack" do
+    url "https://github.com/hashicorp/go-msgpack.git",
+    :revision => "fa3f63826f7c23912c15263591e65d54d080b458"
+  end
+
+  go_resource "github.com/hashicorp/raft" do
+    url "https://github.com/hashicorp/raft.git",
+    :revision => "057b893fd996696719e98b6c44649ea14968c811"
+  end
+
+  go_resource "github.com/hashicorp/raft-boltdb" do
+    url "https://github.com/hashicorp/raft-boltdb.git",
+    :revision => "d1e82c1ec3f15ee991f7cc7ffd5b67ff6f5bbaee"
+  end
+
+  go_resource "github.com/influxdb/influxdb" do
+    url "https://github.com/influxdb/influxdb.git",
+    :revision => "28ae8b6fe06705999a444d47c0766cc47b007643"
+  end
+
+  go_resource "github.com/influxdb/usage-client" do
+    url "https://github.com/influxdb/usage-client.git",
+    :revision => "475977e68d79883d9c8d67131c84e4241523f452"
+  end
+
+  go_resource "github.com/kimor79/gollectd" do
+    url "https://github.com/kimor79/gollectd.git",
+    :revision => "61d0deeb4ffcc167b2a1baa8efd72365692811bc"
   end
 
   go_resource "github.com/mattn/go-runewidth" do
     url "https://github.com/mattn/go-runewidth.git",
-      :revision => "d96d1bd051f2bd9e7e43d602782b37b93b1b5666"
+    :revision => "e882a96ec18dd43fa283187b66af74497c9101c0"
   end
 
   go_resource "github.com/russross/blackfriday" do
     url "https://github.com/russross/blackfriday.git",
-      :revision => "300106c228d52c8941d4b3de6054a6062a86dda3"
+    :revision => "006144af03eeeff1037240a71865a9fd61f1c25f"
+  end
+
+  go_resource "github.com/serenize/snaker" do
+    url "https://github.com/serenize/snaker.git",
+    :revision => "d88cc11617e4f179fa2c821a60908d5fabd7bdad"
   end
 
   go_resource "github.com/shurcooL/go" do
     url "https://github.com/shurcooL/go.git",
-      :revision => "ee4d3ba2f39466dd17fc55dd260e89b8963640f8"
+    :revision => "4d829e4f99c3765122ba92c78b8f69cc0392983d"
+  end
+
+  go_resource "github.com/shurcooL/markdownfmt" do
+    url "https://github.com/shurcooL/markdownfmt.git",
+    :revision => "b191517d76ef6b1ac2599ea819579d6256fb160e"
   end
 
   go_resource "github.com/shurcooL/sanitized_anchor_name" do
     url "https://github.com/shurcooL/sanitized_anchor_name.git",
-      :revision => "10ef21a441db47d8b13ebcc5fd2310f636973c77"
+    :revision => "10ef21a441db47d8b13ebcc5fd2310f636973c77"
+  end
+
+  go_resource "github.com/stretchr/testify" do
+    url "https://github.com/stretchr/testify.git",
+    :revision => "a1f97990ddc16022ec7610326dd9bce31332c116"
+  end
+
+  go_resource "github.com/twinj/uuid" do
+    url "https://github.com/twinj/uuid.git",
+    :revision => "89173bcdda19db0eb88aef1e1cb1cb2505561d31"
+  end
+
+  go_resource "golang.org/x/crypto" do
+    url "https://go.googlesource.com/crypto.git",
+    :revision => "1f22c0103821b9390939b6776727195525381532"
+  end
+
+  go_resource "gopkg.in/fatih/pool.v2" do
+    url "https://gopkg.in/fatih/pool.v2.git",
+    :revision => "cba550ebf9bce999a02e963296d4bc7a486cb715"
+  end
+
+  go_resource "gopkg.in/gomail.v2" do
+    url "https://gopkg.in/gomail.v2.git",
+    :revision => "fbb71ddc63acd07dd0ed49ababdf02c551e2539a"
   end
 
   def install
     ENV["GOPATH"] = buildpath
-    kapacitor_path = buildpath/"src/github.com/influxdb/kapacitor"
+    kapacitor_path = buildpath/"src/github.com/influxdata/kapacitor"
     kapacitor_path.install Dir["*"]
     revision = `git rev-parse HEAD`
 
