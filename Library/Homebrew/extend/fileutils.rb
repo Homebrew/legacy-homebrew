fea5fa2ce7393993ce349c8aa1b0aae654fe1118
Xu Cheng <xucheng@me.com>
2016-02-05 22:11:04 +0800

    make HOMEBREW_BREW_FILE a Pathname object
    Currently HOMEBREW_BREW_FILE is a String, while other of HOMEBREW_*
    variables are all Pathname. This commit unifies them all as Pathname,
    so it will not cause any confusion.

--- Library/Homebrew/extend/fileutils.rb
+++ Library/Homebrew/extend/fileutils.rb
@@ -17,8 +17,8 @@ module FileUtils
     # Reference from `man 2 open`
     # > When a new file is created, it is given the group of the directory which
     # contains it.
-    group_id = if File.grpowned? HOMEBREW_BREW_FILE
-      File.stat(HOMEBREW_BREW_FILE).gid
+    group_id = if HOMEBREW_BREW_FILE.grpowned?
+      HOMEBREW_BREW_FILE.stat.gid
     else
       Process.gid
     end
