a00b8142f48ec66f99fa038d8ebc77952e2e779c
Baptiste Fontaine <batifon@yahoo.fr>
2016-02-05 12:35:29 +0100

    log4cplus: homepage fixed

--- Library/Formula/log4cplus.rb
+++ Library/Formula/log4cplus.rb
@@ -1,6 +1,6 @@
 class Log4cplus < Formula
   desc "Logging Framework for C++"
-  homepage "http://log4cplus.sourceforge.net/"
+  homepage "http://sourceforge.net/p/log4cplus/wiki/Home/"
   url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/1.2.0/log4cplus-1.2.0.tar.xz"
   sha256 "93aa5b26425f0b1596c5e5b3d58916988fdd83b359a02ca59878eb5c7c2ec6c2"
 
