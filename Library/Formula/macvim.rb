ee4f751bf38fa6dcf9f19f1dca1719671c9c82fc
Raúl Acuña <reacuna@crac.cl>
2016-02-06 18:41:59 -0300

    macvim 7.4-96
    Closes #48931.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/macvim.rb
+++ Library/Formula/macvim.rb
@@ -15,9 +15,9 @@ end
 class Macvim < Formula
   desc "GUI for vim, made for OS X"
   homepage "https://github.com/macvim-dev/macvim"
-  url "https://github.com/macvim-dev/macvim/archive/snapshot-95.tar.gz"
-  version "7.4-95"
-  sha256 "66996e8c6788d047567d1fb1577b3e24128d5a8ac43f1488647522b99ae5300e"
+  url "https://github.com/macvim-dev/macvim/archive/snapshot-96.tar.gz"
+  version "7.4-96"
+  sha256 "c594b6b76c54820cee09a397974c901ea310254c626466033edc63a7e52992c7"
 
   head "https://github.com/macvim-dev/macvim.git"
 
