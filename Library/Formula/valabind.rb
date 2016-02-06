64f5197e876cbcc7318712f034020705c0f80757
David Holm <dholmster@gmail.com>
2016-02-06 16:20:55 +0100

    valabind 0.10.0
    Closes #48920.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/valabind.rb
+++ Library/Formula/valabind.rb
@@ -1,17 +1,12 @@
 class Valabind < Formula
   desc "Vala bindings for radare, reverse engineering framework"
   homepage "http://radare.org/"
-  url "https://github.com/radare/valabind/archive/0.9.2.tar.gz"
-  sha256 "84cc2be21acb671e737dab50945b3717f1c68917faf23af443d3911774f5e578"
-  revision 2
+  url "http://www.radare.org/get/valabind-0.10.0.tar.gz"
+  sha256 "35517455b4869138328513aa24518b46debca67cf969f227336af264b8811c19"
 
   head "https://github.com/radare/valabind.git"
 
   bottle do
-    cellar :any
-    sha256 "17cf6fefcd3b9ebd6cdf1d6934cf70a3c02f7988e95809705a7fbb9549c560bb" => :el_capitan
-    sha256 "95fe16f73f5f17fdab2297259df01bd53ffd413a4c20a1366afbbdbbf3432234" => :yosemite
-    sha256 "d0137a646c1b71861e8127c4eb3804f528db5e511fe88afdc491736a807e051e" => :mavericks
   end
 
   depends_on "pkg-config" => :build
