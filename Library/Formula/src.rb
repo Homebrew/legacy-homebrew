4d1f3b034f3d60edb02aa55d1e920697a347e64c
Tomasz Pajor <tomek@polishgeeks.com>
2016-02-06 08:35:20 +0100

    src 1.4
    Closes #48908.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/src.rb
+++ Library/Formula/src.rb
@@ -1,19 +1,19 @@
 class Src < Formula
   desc "Simple revision control: RCS reloaded with a modern UI"
   homepage "http://www.catb.org/~esr/src/"
-  url "http://www.catb.org/~esr/src/src-0.19.tar.gz"
-  sha256 "3d9c5c2fe816b3f273aab17520b917a774e90776c766f165efb6ae661378a65c"
-
-  head do
-    url "git://thyrsus.com/repositories/src.git"
-    depends_on "asciidoc" => :build
-  end
+  url "http://www.catb.org/~esr/src/src-1.4.tar.gz"
+  sha256 "9c9fbc1255b35c5250ff28c2c8cbdafa6eedb2b037b21985ad93bd6c45350666"
 
   bottle do
     cellar :any
     sha256 "654b13895e6e8e7c1f99a007a17d0c6ab26625e2dd732631016dcaeddd5d942d" => :yosemite
     sha256 "91d3fd19e7d8189930d72f03ab1b092a1291252b47076c72c1c1988f69d822e5" => :mavericks
     sha256 "22e3c9dd3842b0d308c56832a04116f7ce8b0a6055a95c4b42c2d3548f02fe5d" => :mountain_lion
+  end
+
+  head do
+    url "git://thyrsus.com/repositories/src.git"
+    depends_on "asciidoc" => :build
   end
 
   conflicts_with "srclib", :because => "both install a 'src' binary"
