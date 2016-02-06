250a0010032e1f75242f79a95e4ff2b5f113916d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-06 17:02:52 +0000

    src: update 1.4 bottle.

--- Library/Formula/src.rb
+++ Library/Formula/src.rb
@@ -5,10 +5,10 @@ class Src < Formula
   sha256 "9c9fbc1255b35c5250ff28c2c8cbdafa6eedb2b037b21985ad93bd6c45350666"
 
   bottle do
-    cellar :any
-    sha256 "654b13895e6e8e7c1f99a007a17d0c6ab26625e2dd732631016dcaeddd5d942d" => :yosemite
-    sha256 "91d3fd19e7d8189930d72f03ab1b092a1291252b47076c72c1c1988f69d822e5" => :mavericks
-    sha256 "22e3c9dd3842b0d308c56832a04116f7ce8b0a6055a95c4b42c2d3548f02fe5d" => :mountain_lion
+    cellar :any_skip_relocation
+    sha256 "05c18ecaa3de91ae186f19c5c85958d8e8dd8a8e0354f154aaae1c551203d8c8" => :el_capitan
+    sha256 "e65242bb07c2f607aeb2bda4546eb51e85b5b7f9c6d4f91c3f26495a8696ddde" => :yosemite
+    sha256 "b96e3da3b99e4ad8b3ac283acb6923c234af07f011a0b47cfc0a270af1999135" => :mavericks
   end
 
   head do
