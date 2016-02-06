bb5b041fc794af21f296e35f7f535ee18ef67f96
Baptiste Fontaine <batifon@yahoo.fr>
2016-02-06 18:40:40 +0100

    tests: monkey-patch exec to get coverage reports

--- Library/Homebrew/test/lib/integration_mocks.rb
+++ Library/Homebrew/test/lib/integration_mocks.rb
@@ -6,4 +6,12 @@ module Homebrew
       end
     end
   end
+
+  def exec(*args)
+    if ENV["HOMEBREW_TESTS_COVERAGE"] && ENV["HOMEBREW_INTEGRATION_TEST"]
+      # Ensure we get coverage results before replacing the current process.
+      SimpleCov.result
+    end
+    Kernel.exec(*args)
+  end
 end
