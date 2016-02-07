51dc274f96d9d32443a4f60ebae4bb45005ee703
Marvin Ritter <marvin.ritter@gmail.com>
2016-02-07 16:12:30 -0500

    pfile-utils: Add test

--- Library/Formula/pfile-utils.rb
+++ Library/Formula/pfile-utils.rb
@@ -16,6 +16,16 @@ class PfileUtils < Formula
   end
 
   test do
-    system "false"
+    # Create a simple pfile
+    (testpath/"data.txt").write <<-EOS.undent
+      0 0 0 0 0
+      0 1 0 1 1
+      0 2 1 0 1
+      0 3 1 1 0
+    EOS
+    system "#{bin}/pfile_create", "-f", "2", "-l", "1", "-i", "data.txt", "-o", testpath/"data.pfile"
+    # Check if it worked
+    assert_equal "data.pfile\n1 sentences, 4 frames, 1 label(s), 2 features\n", `pfile_info data.pfile`
+    assert_equal "Processing sentence 0\n0 0 0 0 0\n0 1 0 1 1\n0 2 1 0 1\n0 3 1 1 0\n", `pfile_print -i data.pfile`
   end
 end
