5bcf17c4828164bc0200168488fd9325bc520ba4
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-07 19:48:56 +0000

    truecrack: nest files dumped in share
    Closes #48956.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/truecrack.rb
+++ Library/Formula/truecrack.rb
@@ -1,9 +1,9 @@
 class Truecrack < Formula
   desc "Brute-force password cracker for TrueCrypt"
-  homepage "https://code.google.com/p/truecrack/"
+  homepage "https://github.com/lvaccaro/truecrack"
   url "https://truecrack.googlecode.com/files/truecrack_v35.tar.gz"
-  sha256 "25bf270fa3bc3591c3d795e5a4b0842f6581f76c0b5d17c0aef260246fe726b3"
   version "3.5"
+  sha256 "25bf270fa3bc3591c3d795e5a4b0842f6581f76c0b5d17c0aef260246fe726b3"
 
   bottle do
     cellar :any
@@ -13,17 +13,21 @@ class Truecrack < Formula
   end
 
   # Fix missing return value compilation issue
-  # https://code.google.com/p/truecrack/issues/detail?id=41
+  # https://github.com/lvaccaro/truecrack/issues/41
   patch do
     url "https://gist.githubusercontent.com/anonymous/b912a1ede06eb1e8eb38/raw/1394a8a6bedb7caae8ee034f512f76a99fe55976/truecrack-return-value-fix.patch"
     sha256 "8aa608054f9b822a1fb7294a5087410f347ba632bbd4b46002aada76c289ed77"
   end
 
   def install
+    # Re datarootdir override: Dumps two files in top-level share
+    # (autogen.sh and cudalt.py) which could cause conflict elsewhere.
     system "./configure", "--enable-cpu",
                           "--disable-debug",
                           "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+                          "--prefix=#{prefix}",
+                          "--datarootdir=#{pkgshare}",
+                          "--mandir=#{man}"
     system "make", "install"
   end
 
