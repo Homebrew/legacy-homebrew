8d5e98cdf0ab963806c1114a3893491af1dbc62d
Caleb Morris <caleb.morris.g@gmail.com>
2016-02-07 18:16:59 -0700

    Cleaned up formula after receive comments

--- Library/Formula/liblaxjson.rb
+++ Library/Formula/liblaxjson.rb
@@ -1,16 +1,13 @@
-
 class Liblaxjson < Formula
   desc "C library for parsing relaxed JSON config file"
   homepage "https://github.com/andrewrk/liblaxjson"
   url "https://github.com/andrewrk/liblaxjson/archive/1.0.5.tar.gz"
-  version "1.0.5"
   sha256 "ffc495b5837e703b13af3f5a5790365dc3a6794f12f0fa93fb8593b162b0b762"
 
   depends_on "cmake" => :build
 
   def install
-    system "mkdir", "build"
-    cd "build" do
+    mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
