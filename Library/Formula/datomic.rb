6d44c5ec6a3091c0e48a28b2e0b13316cbba34e6
Matthias Nüßler <m.nuessler@web.de>
2016-02-08 22:10:30 +0100

    datomic 0.9.5344
    Closes #48982.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/datomic.rb
+++ Library/Formula/datomic.rb
@@ -1,9 +1,9 @@
 class Datomic < Formula
   desc "Database separating transactions, storage and queries"
   homepage "http://www.datomic.com/"
-  url "https://my.datomic.com/downloads/free/0.9.5302"
-  version "0.9.5302"
-  sha256 "6fc4fa8d6fa290c19405dbc11cf7f20dbe94077bd5dddc65cfd10b8958f42950"
+  url "https://my.datomic.com/downloads/free/0.9.5344"
+  version "0.9.5344"
+  sha256 "7fa7d09b2aeae8f2a189f09a8b09c485cdee202e00aacc80c213d25735030498"
 
   bottle :unneeded
 
@@ -30,7 +30,7 @@ class Datomic < Formula
 
   test do
     IO.popen("#{bin}/datomic-repl", "r+") do |pipe|
-      assert_equal "Clojure 1.6.0", pipe.gets.chomp
+      assert_equal "Clojure 1.7.0", pipe.gets.chomp
       pipe.puts "^C"
       pipe.close_write
       pipe.close
