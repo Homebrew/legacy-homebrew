ba729ca54c436f3276805c6670f95603e1256ae9
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-08 14:29:27 +0100

    curl 7.47.1
    * with https enabled on the main site, that became the primary download URL again
    * releases are no longer made available on GitHub
    
    Closes #48970.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -1,9 +1,8 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
-  url "https://github.com/curl/curl/releases/download/curl-7_47_0/curl-7.47.0.tar.bz2"
-  mirror "https://curl.haxx.se/download/curl-7.47.0.tar.bz2"
-  sha256 "2b096f9387fb9b2be08d17e518c62b6537b1f4d4bb59111d5b4fa0272f383f66"
+  url "https://curl.haxx.se/download/curl-7.47.1.tar.bz2"
+  sha256 "ddc643ab9382e24bbe4747d43df189a0a6ce38fcb33df041b9cb0b3cd47ae98f"
 
   bottle do
     cellar :any
