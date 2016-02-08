f9dcb874aeab2bea6573f2e9b6b5b3ecde244bab
Justin Schneck <jschneck@mac.com>
2016-02-08 10:09:47 -0500

    fwup 0.6.0
    Closes #48972.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/fwup.rb
+++ Library/Formula/fwup.rb
@@ -1,8 +1,8 @@
 class Fwup < Formula
   desc "Configurable embedded Linux firmware update creator and runner"
   homepage "https://github.com/fhunleth/fwup"
-  url "https://github.com/fhunleth/fwup/archive/v0.5.2.tar.gz"
-  sha256 "e7735e503f894f19b5a3717c085aa9f6f18702696de91f17bd5405f95f8d6f98"
+  url "https://github.com/fhunleth/fwup/releases/download/v0.6.0/fwup-0.6.0.tar.gz"
+  sha256 "11d8d3a6e7464584b89d88571ee2d2dc1da758fe98525191e77982c60f35bcf3"
 
   bottle do
     cellar :any
@@ -11,15 +11,11 @@ class Fwup < Formula
     sha256 "18972917d65e578a7b2bf2d2a37fb9723e0bc7fce3f90b26f4c5435c50cdce50" => :mavericks
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "confuse"
   depends_on "libarchive"
   depends_on "libsodium"
 
   def install
-    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make", "install"
   end
