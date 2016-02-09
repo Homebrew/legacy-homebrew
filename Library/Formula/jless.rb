022cd34a519686bc38b8cad51dcae4ce8831063b
stevenatwork <dont.fire.steven@gmail.com>
2016-02-09 15:00:51 +1100

    jless: use sha256
    Closes #48991.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/jless.rb
+++ Library/Formula/jless.rb
@@ -9,16 +9,16 @@ class Jless < Formula
   sha256 "d33cc51f220647d015fd526b3782cde03c5d8b18c5233e6388a0047a70446fe7"
 
   [
-    %w[258       961275c26c6f6ec2e04821d9d9d1b10505214b1f],
-    %w[258-259   7256998c59f9743e61e83e9e5ebc5183e8eeb269],
-    %w[259-260   56e5537fae516ee738b1512eed303f43ed0d4575],
-    %w[260-261   3a889e178a32c3a273b89fd396976362a975f627],
-    %w[261-262   d5cc92e5dcbfde26257eec5f126df5bf73779a89],
-    %w[262.ext03 dc35204274ecf02da5f7ffb583da680ea6f4356c]
+    %w[258       0681485f47237aeee42a34bbf0ab55215933c7713d1be80ac202a31c7a6fdc31],
+    %w[258-259   845bb97f407727febd4ee4014618715884bf393d1531089e18faf4e895e19cec],
+    %w[259-260   e81a5de1fdc276cf0f189c674f37459cf1a7314ac1b804c077e23c519df87ec8],
+    %w[260-261   b595378ae65a5a7256df3b6279d755d6fdce66ab6ea0bba4f51eeb9ee0307c03],
+    %w[261-262   e7249de419acc3b8c4a11bed376d778711fd031887d0df0624d62a6d07356842],
+    %w[262.ext03 82fb77f21ea1f3281224fcbfad073aeab12fb40d6623c9b40698be952401f821]
   ].each do |name, sha|
     patch do
       url "http://ftp.netbsd.org/pub/pkgsrc/distfiles/less-382-iso#{name}.patch.gz"
-      sha1 sha
+      sha256 sha
     end
   end
 
