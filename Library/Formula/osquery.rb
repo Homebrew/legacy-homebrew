690737cc328e264d1d1cea625c5a975ac16032b1
Teddy Reed <teddy@prosauce.org>
2016-02-08 18:26:22 -0800

    osquery 1.7.0
    Closes #48992.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/osquery.rb
+++ Library/Formula/osquery.rb
@@ -3,8 +3,8 @@ class Osquery < Formula
   homepage "https://osquery.io"
   # pull from git tag to get submodules
   url "https://github.com/facebook/osquery.git",
-    :tag => "1.6.2",
-    :revision => "5a66d5b8383fa6b5b2164b98a7c415279178bdc4"
+    :tag => "1.7.0",
+    :revision => "62b3c89a7ee1ef21cbc7bc218b9098a489f5bca1"
 
   bottle do
     cellar :any
