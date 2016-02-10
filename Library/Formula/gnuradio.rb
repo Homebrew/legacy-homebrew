b7a2686f923e40cada20ecf2334c59b7292ee184
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-10 08:05:10 +0000

    gnuradio: update 3.7.9.1 bottle.

--- Library/Formula/gnuradio.rb
+++ Library/Formula/gnuradio.rb
@@ -5,6 +5,9 @@ class Gnuradio < Formula
   sha256 "9c06f0f1ec14113203e0486fd526dd46ecef216dfe42f12d78d9b781b1ef967e"
 
   bottle do
+    sha256 "c9fdb8e1a9d96dafe29dd828038bcedc5e03b21612f6cf81843c85238dac9204" => :el_capitan
+    sha256 "2ae7c146eba77b93fb3c4bde743fc166779f09e3d8f61ae1ff55717fc19d137a" => :yosemite
+    sha256 "d5e243974f0089fc503133939c7275019c89d9107ff3eb7406eb4a00a0eae3e7" => :mavericks
   end
 
   # These python extension modules were linked directly to a Python
