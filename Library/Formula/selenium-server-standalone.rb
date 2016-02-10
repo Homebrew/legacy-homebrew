ffdaf53fbe43bf2b3e334dd9f9d99e73dbae8983
Xiaoxing Hu <dawnstar.hu@gmail.com>
2016-02-11 10:30:41 +1300

    Update to version 2.51

--- Library/Formula/selenium-server-standalone.rb
+++ Library/Formula/selenium-server-standalone.rb
@@ -1,8 +1,8 @@
 class SeleniumServerStandalone < Formula
   desc "Browser automation for testing purposes"
   homepage "http://seleniumhq.org/"
-  url "https://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.0.jar"
-  sha256 "e085ece95c77655d8f33289e82691f9b06896032078d158a570378aeef8846f0"
+  url "https://selenium-release.storage.googleapis.com/2.51/selenium-server-standalone-2.51.0.jar"
+  sha256 "919752369eae7ae8fd391eb5d50c88830cf21c0ea2e01cd50e7beb7fceb48991"
 
   bottle :unneeded
 
