ff9c642fcfef0c1c50adc3f4be97c092a57f1178
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-02-10 10:43:59 +0200

    Version bump to : 1.0.0
        - renamed "bin/kommit" to "bin/git-kommit" for full `git`
          integration.

--- Library/Formula/kommit.rb
+++ Library/Formula/kommit.rb
@@ -1,22 +1,16 @@
 class Kommit < Formula
   desc "More detailed commit messages without committing!"
   homepage "https://github.com/bilgi-webteam/kommit"
-  url "https://github.com/bilgi-webteam/kommit/archive/v0.1.4.tar.gz"
-  sha256 "ffc017790b24c238848f83e483e22874ece73af22d3261a59e465d667e9f3c13"
+  url "https://github.com/bilgi-webteam/kommit/archive/v1.0.0.tar.gz"
+  sha256 "d079ba1dcfdf31a35e3d42dbd6adf2450d305e998adb679fb9dc8ea68fa23c22"
 
   def install
-    bin.install "bin/kommit"
-  end
-
-  def caveats; <<-EOS.undent
-    If you like to combine with git, create a symlink;
-      ln -s #{bin}/kommit #{bin}/git-kommit
-    EOS
+    bin.install "bin/git-kommit"
   end
 
   test do
     system "git", "init"
-    system "#{bin}/kommit", "-m", "Hello"
-    assert_match /Hello/, shell_output("#{bin}/kommit -s /dev/null 2>&1")
+    system "#{bin}/git-kommit", "-m", "Hello"
+    assert_match /Hello/, shell_output("#{bin}/git-kommit -s /dev/null 2>&1")
   end
 end
