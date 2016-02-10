accb16beb9b78bf2f495426f38882f6bb8ad57c4
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-10 23:15:00 +0000

    libnotify: add desc

--- Library/Formula/libnotify.rb
+++ Library/Formula/libnotify.rb
@@ -1,4 +1,5 @@
 class Libnotify < Formula
+  desc "Library that sends desktop notifications to a notification daemon"
   homepage "https://developer.gnome.org/libnotify"
   url "https://download.gnome.org/sources/libnotify/0.7/libnotify-0.7.6.tar.xz"
   sha256 "0ef61ca400d30e28217979bfa0e73a7406b19c32dd76150654ec5b2bdf47d837"
