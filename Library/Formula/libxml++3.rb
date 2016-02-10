c0fe6fc0d82f09206f67d9b7147e7398cfcd4611
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-10 00:35:39 +0000

    libxml++3: add 3.0.0 bottle.

--- Library/Formula/libxml++3.rb
+++ Library/Formula/libxml++3.rb
@@ -4,6 +4,13 @@ class Libxmlxx3 < Formula
   url "https://download.gnome.org/sources/libxml++/3.0/libxml++-3.0.0.tar.xz"
   sha256 "2ff3640417729d357bada2e3049061642e0b078c323a8e0d37ae68df96547952"
 
+  bottle do
+    cellar :any
+    sha256 "1eee12f428fea153679ce88eaf7f334f00ceb64aea6cf139346b535c5a664554" => :el_capitan
+    sha256 "e068fff70a7513e0225288bc6092f37a016b4269d3a76c3997be1ba5a964c4e9" => :yosemite
+    sha256 "8987284c0005cd2b8b80d91b81f13783e4e7d1ebff1f794b4686891381614b1a" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glibmm"
   # LibXML++ can't compile agains the version of LibXML shipped with Leopard
