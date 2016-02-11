e9a5cca0c8b34a5e29eac3e4c4960a0067543412
Fred McCann <fred@sharpnoodles.com>
2016-02-11 11:47:50 -0500

    bzrtools 2.3.0 (new formula) add bottle directive

--- Library/Formula/bzrtools.rb
+++ Library/Formula/bzrtools.rb
@@ -4,6 +4,7 @@ class Bzrtools < Formula
   url "https://launchpad.net/bzrtools/trunk/2.3.0/+download/bzrtools-2.3.0.tar.gz"
   sha256 "706270db274cdb4897eda1b8bb412383c71f6d1b3fac6ae3db587ac249ad5b79"
 
+  bottle :unneeded
   depends_on "bazaar"
 
   def install
