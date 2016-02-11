41697cd021be3a315016556478ef746c34dc957c
Fred McCann <fred@sharpnoodles.com>
2016-02-11 11:45:51 -0500

    qbzr 0.23.1 (new formula) clean up newline, make test flexible on version, add bottle directive

--- Library/Formula/qbzr.rb
+++ Library/Formula/qbzr.rb
@@ -4,6 +4,8 @@ class Qbzr < Formula
   url "https://launchpad.net/qbzr/0.23/0.23.1/+download/qbzr-0.23.1.tar.gz"
   sha256 "3211adef11c975dfbb6c80285651e2e6f3bfa99f1baa1a95371e8490ea8ff441"
 
+  bottle :unneeded
+
   depends_on "bazaar"
   depends_on "pyqt"
 
@@ -13,13 +15,12 @@ class Qbzr < Formula
 
   def caveats; <<-EOS.undent
     In order to use this plugin you must set your PYTHONPATH in your ~/.bashrc:
-
     export PYTHONPATH="#{HOMEBREW_PREFIX}/lib/python2.7/site-packages:$PYTHONPATH"
 
   EOS
   end
 
   test do
-    assert_match /qbzr 0.23.1/, shell_output("bzr plugins")
+    assert_match /qbzr #{version}/, shell_output("bzr plugins")
   end
 end
