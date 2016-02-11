2db7cfffe08aec6d8b3658cb15095452b68356d2
Fred McCann <fred@sharpnoodles.com>
2016-02-11 11:41:08 -0500

    bzr-xmloutput 0.8.8 (new formula) correct brew audit errors

--- Library/Formula/bzr-xmloutput.rb
+++ Library/Formula/bzr-xmloutput.rb
@@ -1,5 +1,5 @@
 class BzrXmloutput < Formula
-  desc "Bazaar plugin that provides a option to generate XML output for builtins commands"
+  desc "Bazaar plugin that provides a option to generate XML output"
   homepage "https://launchpad.net/bzr-xmloutput"
   url "https://launchpad.net/bzr-xmloutput/trunk/0.8.8/+download/bzr-xmloutput-0.8.8.tar.gz"
   sha256 "73b9b2f6ce4d9910031df7fd153d56d14f833c20a106f099bee5a33463f73b36"
@@ -17,7 +17,7 @@ class BzrXmloutput < Formula
       msg = "my commit"
       system "bzr", "add", "test.txt"
       system "bzr", "commit", "-m", msg
-      assert_match /<message><!\[CDATA\[my commit\]\]><\/message>/, shell_output("bzr log --xml")
+      assert_match /<message>.*#{msg}/, shell_output("bzr log --xml")
     end
   end
 end
