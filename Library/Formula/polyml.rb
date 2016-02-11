309b5b64a174535fa9ee9fd80f346eabf7307365
James Clarke <jrtc27@jrtc27.com>
2016-02-11 14:31:26 +0000

    polyml 5.6
    Closes #49074.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- Library/Formula/polyml.rb
+++ Library/Formula/polyml.rb
@@ -1,8 +1,9 @@
 class Polyml < Formula
   desc "Standard ML implementation"
   homepage "http://www.polyml.org"
-  url "https://downloads.sourceforge.net/project/polyml/polyml/5.5.2/polyml.5.5.2.tar.gz"
-  sha256 "73fd2be89f7e3ff0567e27ef525ef788775d9f963d6db54069cb34d53040a682"
+  url "https://github.com/polyml/polyml/archive/v5.6.tar.gz"
+  sha256 "20d7b98ae56fe030c64054dbe0644e9dc02bae781caa8994184ea65a94a0a615"
+  head "https://github.com/polyml/polyml.git"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--disable-debug",
