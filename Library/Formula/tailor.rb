96517e8d0cbd093cb5b11def2819c5b829250510
Sleekbot <team@sleekbyte.com>
2016-02-12 18:27:35 -0500

    tailor 0.6.0
    Closes #49127.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/tailor.rb
+++ Library/Formula/tailor.rb
@@ -1,8 +1,8 @@
 class Tailor < Formula
   desc "Cross-platform static analyzer and linter for Swift"
   homepage "https://tailor.sh"
-  url "https://github.com/sleekbyte/tailor/releases/download/v0.5.1/tailor-0.5.1.tar"
-  sha256 "649056946d39a6b12ba6ad1d86b0f184ed8dc76e77dfeb5e13d0802c9073a6ca"
+  url "https://github.com/sleekbyte/tailor/releases/download/v0.6.0/tailor-0.6.0.tar"
+  sha256 "3082448cbce0d7cea9c3fa119e5f1bb5e7ff53801fd506c94a2bfe3bef4f3145"
 
   bottle :unneeded
 
