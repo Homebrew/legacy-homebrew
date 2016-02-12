a37346e40fadf44ed4625e9c6ce14cca751a86d2
Ollie Ford <me@ojford.com>
2016-02-12 15:39:32 +0000

    updates description to match that on npm

--- Library/Formula/diff-so-fancy.rb
+++ Library/Formula/diff-so-fancy.rb
@@ -1,5 +1,5 @@
 class DiffSoFancy < Formula
-  desc "builds on the good-lookin' output of diff-highlight"
+  desc "Good-lookin' diffs with diff-highlight and more"
   homepage "https://github.com/stevemao/diff-so-fancy"
   url "https://github.com/stevemao/diff-so-fancy/archive/v0.1.2.tar.gz"
   sha256 "fa4ac51368ba6b942c6b346117e5f72cabb00ea3c29f834e3c4ca80972e19502"
