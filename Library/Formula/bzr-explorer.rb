67b0d5750ef49600088cb46ce4806f7f5b9f8396
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-13 09:52:49 +0000

    bzr-explorer: add 1.3.0 bottle.

--- Library/Formula/bzr-explorer.rb
+++ Library/Formula/bzr-explorer.rb
@@ -4,6 +4,13 @@ class BzrExplorer < Formula
   url "https://launchpad.net/bzr-explorer/1.3/1.3.0/+download/bzr-explorer-1.3.0.tar.gz"
   sha256 "e3584df263a5004765a224cc38d00449e0ad47495070edae59ecbcc4dac94086"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fa5fb7b5f5e4bd59218768e5edcdd1d1885c1660325410824dfe4dd2fe7d09ea" => :el_capitan
+    sha256 "da20c1601c723ed76c2ed821f6f5227410a1b94404f693c356c4249e1d12c083" => :yosemite
+    sha256 "70df82f8ac0c72265b2be43d3663e83ad5d34b839dc10706a6fd21806eb4539d" => :mavericks
+  end
+
   depends_on "bazaar"
   depends_on "qbzr"
   depends_on "gettext" => :build
