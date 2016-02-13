3e96c5ea8b530a496a9152a9cfdbfacc5a1f9b1d
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-13 04:03:45 +0000

    unittest-cpp 1.5.1
    Closes #48763.
    
    Closes #49135.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/unittest-cpp.rb
+++ Library/Formula/unittest-cpp.rb
@@ -1,17 +1,9 @@
 class UnittestCpp < Formula
   desc "Unit testing framework for C++"
   homepage "https://github.com/unittest-cpp/unittest-cpp"
-
-  stable do
-    url "https://github.com/unittest-cpp/unittest-cpp/archive/v1.4.tar.gz"
-    sha256 "e075760b7082380058e7bbe3a69f8739524c73dc82d8362b418aaa0f873c6c20"
-
-    # Clang failure fixed in the HEAD already
-    fails_with :clang do
-      build 600
-      cause "Failure in test: Expected 2 but was 0"
-    end
-  end
+  url "https://github.com/unittest-cpp/unittest-cpp/archive/v1.5.1.tar.gz"
+  sha256 "7021008424054fe17dbd10307b72367f172846173d20848e555f64b8a6b4a8a3"
+  head "https://github.com/unittest-cpp/unittest-cpp.git"
 
   bottle do
     cellar :any
@@ -20,33 +12,13 @@ class UnittestCpp < Formula
     sha256 "942d4451a5985818fd1a881be3405573eba6d4a93e2dabca04dfe7b9e3afc105" => :mountain_lion
   end
 
-  head do
-    url "https://github.com/unittest-cpp/unittest-cpp.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def install
-    if build.head?
-      system "autoreconf", "-fvi"
-      system "./configure", "--prefix=#{prefix}", "--disable-silent-rules"
-      system "make", "install"
-    end
-
-    if build.stable?
-      system "make"
-
-      # Install the headers
-      include.install Dir["src/*.h"]
-      include.install "src/Posix"
-
-      # Install the compiled library
-      lib.install "libUnitTest++.a"
-
-      # Install the documentation
-      doc.install "docs/UnitTest++.html"
-    end
+    system "autoreconf", "-fvi"
+    system "./configure", "--prefix=#{prefix}", "--disable-silent-rules"
+    system "make", "install"
   end
 end
