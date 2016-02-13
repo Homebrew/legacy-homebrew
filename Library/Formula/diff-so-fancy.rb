7249ac3f7a345cce350d70a935b9501a7bde753e
Ollie Ford <me@ojford.com>
2016-02-13 03:02:57 +0000

    updates to diff-so-fancy v2.0

--- Library/Formula/diff-so-fancy.rb
+++ Library/Formula/diff-so-fancy.rb
@@ -1,8 +1,8 @@
 class DiffSoFancy < Formula
   desc "Good-lookin' diffs with diff-highlight and more"
   homepage "https://github.com/stevemao/diff-so-fancy"
-  url "https://github.com/stevemao/diff-so-fancy/archive/v0.1.2.tar.gz"
-  sha256 "fa4ac51368ba6b942c6b346117e5f72cabb00ea3c29f834e3c4ca80972e19502"
+  url "https://github.com/stevemao/diff-so-fancy/archive/v0.2.0.tar.gz"
+  sha256 "9f23732b7f7cde8bbbb019b4e0473d37888a55afe1b9a07ad9c1bce8ccf2334b"
 
   depends_on "gnu-sed"
 
