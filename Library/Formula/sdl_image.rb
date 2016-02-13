41873b027015ad445017379ea2f166778a4a12b4
Domenic Barbuzzi <dbarbuzzi@gmail.com>
2016-02-13 09:14:08 -0500

    sdl_image: bump revision for webp update

--- Library/Formula/sdl_image.rb
+++ Library/Formula/sdl_image.rb
@@ -3,7 +3,7 @@ class SdlImage < Formula
   homepage "https://www.libsdl.org/projects/SDL_image"
   url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
   sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
