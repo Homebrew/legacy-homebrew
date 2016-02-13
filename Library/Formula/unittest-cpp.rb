85ad6b0d355cb875e1f955e391e30e590e68546b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-13 04:14:52 +0000

    unittest-cpp: update 1.5.1 bottle.

--- Library/Formula/unittest-cpp.rb
+++ Library/Formula/unittest-cpp.rb
@@ -7,9 +7,9 @@ class UnittestCpp < Formula
 
   bottle do
     cellar :any
-    sha256 "44d3ebdf38a4fe0f6e7c31385a6f2474eabbf228277e67fec8accb4437140f72" => :yosemite
-    sha256 "5e383d6c98685cab234a7c8ddf3839df42059e5e8db493e185ff4c2259fbbe3e" => :mavericks
-    sha256 "942d4451a5985818fd1a881be3405573eba6d4a93e2dabca04dfe7b9e3afc105" => :mountain_lion
+    sha256 "d8ab4cb9e4fb46354147c958cb6bc53edb9b3a61d13a0e03be2b40fd9880016f" => :el_capitan
+    sha256 "344530b7f3c3d7db96693e4325d86ccd81f2c9df1ed21c151edff8ae92d558ce" => :yosemite
+    sha256 "67f3d01e1d608e9f27d230e8e2bb35851248a69d526f120e78b13d9fdc2874b4" => :mavericks
   end
 
   depends_on "autoconf" => :build
