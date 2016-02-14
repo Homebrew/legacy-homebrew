d25ea05a555cd706a6f1009aed5111bbe692ad4a
Kim Walisch <kim.walisch@gmail.com>
2016-02-14 17:33:59 +0100

    Fix brew-test-bot failure
    `bottle modifier` (line 12) should be put before `depends_on` (line 7)

--- Library/Formula/primecount.rb
+++ Library/Formula/primecount.rb
@@ -4,12 +4,13 @@ class Primecount < Formula
   url "http://dl.bintray.com/kimwalisch/primecount/primecount-2.6.tar.gz"
   version "2.6"
   sha256 "ab5790e1fef22b21d50748795b1502d18e969fa9b1cd443305b08534c96430ec"
-  depends_on "primesieve"
 
   # Always build from source for maximum performance.
   # Enables OpenMP multi-threading if the compiler supports it
   # and uses the POPCNT instruction if the CPU supports it.
   bottle :disable, "Library and clients must be built on the same microarchitecture"
+
+  depends_on "primesieve"
 
   def install
     system "./configure", "--disable-debug",
