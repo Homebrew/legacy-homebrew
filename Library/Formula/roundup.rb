43f7ba6680d5c5ced4b791dcba01dd7cffbeb559
Johan Hammar <johan.hammar@gmail.com>
2016-02-15 13:06:16 +0100

    roundup: fix incorrect formula description

--- Library/Formula/roundup.rb
+++ Library/Formula/roundup.rb
@@ -1,5 +1,5 @@
 class Roundup < Formula
-  desc "Issue-tracking system"
+  desc "Unit testing tool"
   homepage "https://bmizerany.github.io/roundup"
   url "https://github.com/bmizerany/roundup/archive/v0.0.5.tar.gz"
   sha256 "f23397ab2a219508a7e6ccc6b40e0b1627fc4e8d25f68c4fe26316a644118e4f"
