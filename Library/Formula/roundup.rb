a551330371e0325d08f186ae0f7225b3f75de858
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-15 18:33:00 +0000

    roundup: add 0.0.5 bottle.

--- Library/Formula/roundup.rb
+++ Library/Formula/roundup.rb
@@ -6,6 +6,13 @@ class Roundup < Formula
 
   head "https://github.com/bmizerany/roundup.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "54fd2eea633548d627df8cd4cc3962c5ca3521c7d8c69c9e76018749ed1c8614" => :el_capitan
+    sha256 "86362d0a78da1dcb29dc1a421161446d6664feba9ad6be2f8392ad64daf0978d" => :yosemite
+    sha256 "ba15d496c1d2be929e477488d4917748b3652d4fb82703388228fd37992249c8" => :mavericks
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--bindir=#{bin}",
