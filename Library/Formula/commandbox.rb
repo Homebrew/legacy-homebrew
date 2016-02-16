57b5a9fb9d447906c5f47b7cbf4043039d4a5e49
Jon Clausen <jon_clausen@silowebworks.com>
2016-02-16 10:14:40 -0500

    syntax corrections

--- Library/Formula/commandbox.rb
+++ Library/Formula/commandbox.rb
@@ -1,5 +1,5 @@
 class Commandbox < Formula
-  desc "A CLI, package manager, app scaffolding tool with embedded CFML server"
+  desc "embedded server, package manager, and app scaffolding tools for CFML"
   homepage "http://www.ortussolutions.com/products/commandbox"
   url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.0.0/commandbox-bin-3.0.0.zip"
   sha1 "cb8d454b99d56c16e8501b4e1aa3c4eeb9b44451"
@@ -12,18 +12,19 @@ class Commandbox < Formula
     sha1 "2ffe33c1d3ec02c56a9879c99ffe5b7fc792959a"
   end
 
+  devel do
+    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.0/commandbox-bin-3.1.0.zip"
+    sha1 "2e3bab8443b7965931cd7c2f4edefc14c6e4297c"
+  end
+
   def install
     bin.install 'box'
     doc.install resource( "apidocs" )
   end
 
   test do
-    box install
-    box server start
-  end
-
-  def caveats
-    "You will need at least Java JDK 1.7+ to run CommandBox"
+    system "box", "install"
+    system "box", "--version"
   end
 
 end
