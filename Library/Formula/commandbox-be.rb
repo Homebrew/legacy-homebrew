8287b41bcf6f8e9178e3cfc91a14840c416e9a72
Jon Clausen <jon_clausen@silowebworks.com>
2016-02-15 22:23:48 -0500

    fixes formulae build audit results

--- Library/Formula/commandbox-be.rb
+++ Library/Formula/commandbox-be.rb
@@ -1,11 +1,11 @@
 class CommandboxBe < Formula
-  desc "CommandBox (Bleeding Edge Release) is a CLI, package manager, embedded CFML server, which provides application scaffolding and much, much more."
+  desc "A CLI, package manager, app scaffolding tool with embedded CFML server"
   homepage "http://www.ortussolutions.com/products/commandbox"
   url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.0/commandbox-bin-3.1.0.zip"
   sha1 "2e3bab8443b7965931cd7c2f4edefc14c6e4297c"
-  version "3.1.0"
   
   depends_on :arch => :x86_64
+  depends_on :java => "1.7+"
 
   resource 'apidocs' do
     url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.0/commandbox-apidocs-3.1.0.zip"
@@ -17,9 +17,13 @@ class CommandboxBe < Formula
     doc.install resource( "apidocs" )
   end
 
+  test do
+    box install
+    box server start
+  end
+
   def caveats
     "You will need at least Java JDK 1.7+ to run CommandBox"
   end
-
 
 end
--- Library/Formula/commandbox.rb
+++ Library/Formula/commandbox.rb
@@ -1,11 +1,11 @@
 class Commandbox < Formula
-  desc "CommandBox is a CLI, package manager, embedded CFML server, which provides application scaffolding and much, much more."
+  desc "A CLI, package manager, app scaffolding tool with embedded CFML server"
   homepage "http://www.ortussolutions.com/products/commandbox"
   url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.0.0/commandbox-bin-3.0.0.zip"
   sha1 "cb8d454b99d56c16e8501b4e1aa3c4eeb9b44451"
-  version "3.0.0"
   
   depends_on :arch => :x86_64
+  depends_on :java => "1.7+"
 
   resource 'apidocs' do
     url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.0.0/commandbox-apidocs-3.0.0.zip"
@@ -17,9 +17,13 @@ class Commandbox < Formula
     doc.install resource( "apidocs" )
   end
 
+  test do
+    box install
+    box server start
+  end
+
   def caveats
     "You will need at least Java JDK 1.7+ to run CommandBox"
   end
-
 
 end
