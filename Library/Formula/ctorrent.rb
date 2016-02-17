e90044413f5d3645cd8cafe8f918473280621bd8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-17 19:31:51 +0000

    ctorrent: update 3.3.2_2 bottle.

--- Library/Formula/ctorrent.rb
+++ Library/Formula/ctorrent.rb
@@ -7,9 +7,9 @@ class Ctorrent < Formula
 
   bottle do
     cellar :any
-    sha256 "47019268b9a4da02a0e36e884c9602c9ee1b8be1982e28634710c388944b333b" => :yosemite
-    sha256 "ee6156a026a912c76dcf93e24506098f25387bda9022c0bcd065d4d511601ca6" => :mavericks
-    sha256 "01c353acf408ba34f5ee8c4ac6acca99ab7cdb285f2e03366d9c18d9e44f221e" => :mountain_lion
+    sha256 "948dc4f46a2df2581d812bc09430c635d13c589448791bd744cadfa77c739baa" => :el_capitan
+    sha256 "ee33a6af8447cbf0f71451dfad655a14a9b28f642c799ca7912eefe9e0fc2330" => :yosemite
+    sha256 "8979644a0390f763c081556298ba1997f0db2ff9eada13f686d698af57777f32" => :mavericks
   end
 
   # This patch skips over negative integer values appearing before "info" section in torrent file
