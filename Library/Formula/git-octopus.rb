fc508483a8998b9a9bfa078fb4adff24da749089
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-17 10:28:18 +0000

    git-octopus: update 1.2 bottle.

--- Library/Formula/git-octopus.rb
+++ Library/Formula/git-octopus.rb
@@ -6,9 +6,9 @@ class GitOctopus < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0d65c7084a3bcf37dbeaa8588c37d0ca46a8aa4c547b3227cb459c7e383d4ca7" => :el_capitan
-    sha256 "84b80ab00ce8801ef6b222f073c849ebe52b7665f3241a9b791c15f6fe742880" => :yosemite
-    sha256 "13b574740d23c12dbac5359bc7c28fc2aa736f1fa9d82d7b3f82c3d1bda457e6" => :mavericks
+    sha256 "49b09ecce43923827192367c44f07ecf7c5c395c3369e6adb80852ef6d15fc80" => :el_capitan
+    sha256 "ef168793e40ba5728986c3360fcfe94a4380b397f512c8fc1a059e18a77e654e" => :yosemite
+    sha256 "652cf404f04177114e05e9b7b8d36f2018beed92ba8877d598023e259604e4e9" => :mavericks
   end
 
   def install
