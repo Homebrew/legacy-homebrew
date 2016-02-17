0eed435fcc6f657635f7d9d6109cecbf1f6c9f10
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-17 05:47:37 +0000

    babl: update 0.1.16 bottle.

--- Library/Formula/babl.rb
+++ Library/Formula/babl.rb
@@ -6,9 +6,9 @@ class Babl < Formula
   sha256 "7d6ba55ec53ee6f6bf6945beec28839d09ff72376f4d83035eb379cd4f3e980e"
 
   bottle do
-    sha256 "7f5aacab0effd76ebb60e3057f5a620570e2cafa41017617f60b3d666be1e633" => :el_capitan
-    sha256 "dd3073278a5a7f3e584e37cfcd2fb5268a98b3230d16b125aec806a5968668f4" => :yosemite
-    sha256 "060bc343437aeff92ed1f411c4d4e98f5cb0577494eb8e55b2398f398071bf1a" => :mavericks
+    sha256 "b6eaf46444795d0f3ad5f6dae51212403057baca537912d53c5b65ff87f9e694" => :el_capitan
+    sha256 "a47b7f4832814eb4effdf8b11fd5763f48ae6ab330e823dfd88b55923fa33c05" => :yosemite
+    sha256 "94cb19207e867b58f77fe6c2ba3629a6595a994722840ad657c5461743b5f2ac" => :mavericks
   end
 
   head do
