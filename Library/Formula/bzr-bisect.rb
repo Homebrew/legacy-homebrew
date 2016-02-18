a25cdb93e9babac48d12476c4677b0d82dcedf49
Fred McCann <fred@sharpnoodles.com>
2016-02-17 20:36:04 -0500

    bzr-bisect jelmer@samba.org-20120803130348-l0ydx0dn94f1xrjr (new formula)

---
+++ Library/Formula/bzr-bisect.rb
@@ -0,0 +1,46 @@
+class BzrBisect < Formula
+  desc "External merge tool support for Bazaar"
+  homepage "https://launchpad.net/bzr-bisect"
+  url "lp:bzr-bisect", :using => :bzr, :revision => "jelmer@samba.org-20120803130348-l0ydx0dn94f1xrjr"
+  version "jelmer@samba.org-20120803130348-l0ydx0dn94f1xrjr"
+
+  bottle :unneeded
+
+  depends_on "bazaar"
+
+  def install
+    (share/"bazaar/plugins/bisect").install Dir["*"]
+  end
+
+  test do
+    system "bzr", "whoami", "Homebrew"
+    system "bzr", "init", "foo"
+    file_path = (testpath/"foo/foo.txt").to_s
+
+    cd "foo" do
+      open(file_path, "w") { |f| f.puts "change" }
+      system "bzr", "add"
+      system "bzr", "commit", "-m", "change 1"
+
+      open(file_path, "a") { |f| f.puts "change" }
+      system "bzr", "commit", "-m", "change 2"
+
+      open(file_path, "a") { |f| f.puts "change" }
+      system "bzr", "commit", "-m", "change 3"
+
+      open(file_path, "a") { |f| f.puts "change" }
+      system "bzr", "commit", "-m", "change 4"
+
+      open(file_path, "a") { |f| f.puts "change" }
+      system "bzr", "commit", "-m", "change 5"
+
+      system "bzr", "bisect", "start"
+
+      assert_match(/On revision 2/, shell_output("bzr bisect yes"))
+      assert_match(/On revision 1/, shell_output("bzr bisect yes"))
+      assert_match(/On revision 1/, shell_output("bzr bisect no"))
+
+      system "bzr", "bisect", "reset"
+    end
+  end
+end
