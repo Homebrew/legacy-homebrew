acf81bdbe4909d546ebfeb05bed6ff9cc43f2090
Fred McCann <fred@sharpnoodles.com>
2016-02-17 19:45:45 -0500

    bzr-extmerge erik@plero.se-20100728145225-et6ipg7cjbshiz8f (new formula) add version

--- Library/Formula/bzr-extmerge.rb
+++ Library/Formula/bzr-extmerge.rb
@@ -1,7 +1,8 @@
 class BzrExtmerge < Formula
   desc "External merge tool support for Bazaar"
   homepage "https://launchpad.net/bzr-extmerge"
-  url "lp:bzr-extmerge", :using => :bzr, :revision => 'erik@plero.se-20100728145225-et6ipg7cjbshiz8f'
+  url "lp:bzr-extmerge", :using => :bzr, :revision => "erik@plero.se-20100728145225-et6ipg7cjbshiz8f"
+  version "erik@plero.se-20100728145225-et6ipg7cjbshiz8f"
 
   bottle :unneeded
 
