678072f12e5aa94a1b133261fdd6ab001ddd871a
Brandon Ooi <brandono@gmail.com>
2016-02-18 00:00:27 -0800

    Bumped to v0.4.0. This version is installed via pip so the install script is adjusted accordingly.

--- Library/Formula/b2-tools.rb
+++ Library/Formula/b2-tools.rb
@@ -1,11 +1,16 @@
 class B2Tools < Formula
   desc "B2 Cloud Storage Command-Line Tools"
   homepage "https://github.com/Backblaze/B2_Command_Line_Tool"
-  url "https://github.com/Backblaze/B2_Command_Line_Tool/archive/v0.3.12.tar.gz"
-  sha256 "6c382d211c5df09477d5df468e43fe5b9691e44edfb5df9694e9b747a488d362"
+  url "https://github.com/Backblaze/B2_Command_Line_Tool/archive/v0.4.0.tar.gz"
+  sha256 "0253dfc4352ae6bc23e7184c2402b713f7e6f9a0584c80693ed1fdcffcf61325"
 
   def install
-    bin.install "b2"
+    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+
     bash_completion.install "contrib/b2" => "b2-tools-completion.bash"
   end
 
