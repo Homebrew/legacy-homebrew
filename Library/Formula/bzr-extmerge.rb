8fb939131175618a7693a87c439a4b6e4a358a80
Fred McCann <fred@sharpnoodles.com>
2016-02-18 20:03:55 -0500

    bzr-extmerge 1.0.0 (new formula)

--- Library/Formula/bzr-extmerge.rb
+++ Library/Formula/bzr-extmerge.rb
@@ -1,8 +1,8 @@
 class BzrExtmerge < Formula
   desc "External merge tool support for Bazaar"
   homepage "https://launchpad.net/bzr-extmerge"
-  url "lp:bzr-extmerge", :using => :bzr, :revision => "erik@plero.se-20100728145225-et6ipg7cjbshiz8f"
-  version "erik@plero.se-20100728145225-et6ipg7cjbshiz8f"
+  url "https://launchpad.net/bzr-extmerge/trunk/1.0.0/+download/bzr-extmerge-1.0.0.tar.gz"
+  sha256 "1b86d3a54fe669db19bc2a42a09eae52e449cc3ece8234377fd213e834f69cc0"
 
   bottle :unneeded
 
