d62a952f1b969842fbe92672ecba46ff2df3b1e6
Zhiming Wang <zmwangx@gmail.com>
2016-02-19 15:39:48 -0800

    echoprint-codegen: revision for ffmpeg update

--- Library/Formula/echoprint-codegen.rb
+++ Library/Formula/echoprint-codegen.rb
@@ -3,7 +3,7 @@ class EchoprintCodegen < Formula
   homepage "http://echoprint.me"
   url "https://github.com/echonest/echoprint-codegen/archive/v4.12.tar.gz"
   sha256 "c40eb79af3abdb1e785b6a48a874ccfb0e9721d7d180626fe29c72a29acd3845"
-  revision 1
+  revision 2
   head "https://github.com/echonest/echoprint-codegen.git"
 
   bottle do
