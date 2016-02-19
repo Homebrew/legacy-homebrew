48df5a744db01f30606dc58d9eee2d71fab5e1ae
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-19 05:30:26 +0000

    tmate: add 2.2.0 bottle.

--- Library/Formula/tmate.rb
+++ Library/Formula/tmate.rb
@@ -6,6 +6,13 @@ class Tmate < Formula
 
   head "https://github.com/tmate-io/tmate.git"
 
+  bottle do
+    cellar :any
+    sha256 "772982dbe9cabdf4f57d3fd1f968970ee770df538930e3540cdbc349d6e7e5d6" => :el_capitan
+    sha256 "c558b7b57802d23d9365e895ee4e5fba2e2325f2b39caddcb84497030291c4f2" => :yosemite
+    sha256 "8be4af6f9b0aabeb8aa8b85e61cc708aa60fca19f581fff4c150060880980efc" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
