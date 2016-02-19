551db04a9f69c74e1a44f0903806a53a4a16579c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-19 04:21:38 +0000

    googler: add 2.1 bottle.

--- Library/Formula/googler.rb
+++ Library/Formula/googler.rb
@@ -4,6 +4,13 @@ class Googler < Formula
   url "https://github.com/jarun/googler/archive/v2.1.tar.gz"
   sha256 "4f42325b3961efa19c475138507f156891c27db7882ed6f75dfa256aef63016c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e80627b38cfd099190cc1af1046ff1ac68db9440647cad5861e678ddc73306bc" => :el_capitan
+    sha256 "73ed00aa66144356692d90992b5686aaee90356e0ec687b59a4260e53e59e027" => :yosemite
+    sha256 "c6414018bf2fa23c50e3e7fb76cfa81580f338dad9388fb18b84f586788cbf7c" => :mavericks
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
