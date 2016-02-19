5032eca9df380e804d41f9a1d58a773f3a294073
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-19 00:00:35 +0000

    docker-gen: add 0.6.0 bottle.

--- Library/Formula/docker-gen.rb
+++ Library/Formula/docker-gen.rb
@@ -4,6 +4,13 @@ class DockerGen < Formula
   url "https://github.com/jwilder/docker-gen/releases/download/0.6.0/docker-gen-darwin-amd64-0.6.0.tar.gz"
   sha256 "034758ac33adda981dccffcabeaf8b7ab5c461c19cb9daf455ea276207bb66ae"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2818b78c3f09503c3a6e73cec25fc2f8904ec6f446b322772d6683c2f4048459" => :el_capitan
+    sha256 "fb9a79daa051f526f414f36e6e2b609bc767699da385030a49767d14035b40b6" => :yosemite
+    sha256 "5e006090106182a3c7f8cac8feb128a90c7d9fc17fa749a786a7118f1e9f9369" => :mavericks
+  end
+
   def install
     bin.install "docker-gen"
   end
