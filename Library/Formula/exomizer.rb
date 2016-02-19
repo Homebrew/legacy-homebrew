8d5eb59dfbf0ec53bea255fa7e21de31b75a6ef0
Michał Kalbarczyk <fazibear@gmail.com>
2016-02-19 21:37:44 +0100

    cleanup whitespaces

--- Library/Formula/exomizer.rb
+++ Library/Formula/exomizer.rb
@@ -1,9 +1,9 @@
 class Exomizer < Formula
-  desc     "6502 compressor with CBM PET 4032 support."
+  desc "6502 compressor with CBM PET 4032 support."
   homepage "http://hem.bredband.net/magli143/exo/"
-  url      "http://hem.bredband.net/magli143/exo/exomizer209.zip"
-  version  "2.0.9"
-  sha256   "d2a95d4d168e4007fc396295e2f30a21b58f4648c28d1aadf84e7d497c5c7a34"
+  url "http://hem.bredband.net/magli143/exo/exomizer209.zip"
+  version "2.0.9"
+  sha256 "d2a95d4d168e4007fc396295e2f30a21b58f4648c28d1aadf84e7d497c5c7a34"
 
   def install
     cd "src" do
