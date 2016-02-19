9ccd03b917ae1ca99a30288678e8e6d2c6c4cc8e
Andrew Janke <andrew@apjanke.net>
2016-02-19 01:08:17 -0500

    docker-gen: spelling fix

--- Library/Formula/docker-gen.rb
+++ Library/Formula/docker-gen.rb
@@ -1,5 +1,5 @@
 class DockerGen < Formula
-  desc "Generate files from docker container meta-data"
+  desc "Generate files from docker container metadata"
   homepage "https://github.com/jwilder/docker-gen"
   url "https://github.com/jwilder/docker-gen/releases/download/0.6.0/docker-gen-darwin-amd64-0.6.0.tar.gz"
   sha256 "034758ac33adda981dccffcabeaf8b7ab5c461c19cb9daf455ea276207bb66ae"
