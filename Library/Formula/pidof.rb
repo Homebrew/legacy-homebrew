8ccff867047791499404a988447f5be56614614d
Tomasz Pajor <tomek@polishgeeks.com>
2016-02-20 23:14:55 +0100

    pidof: audit fixes

--- Library/Formula/pidof.rb
+++ Library/Formula/pidof.rb
@@ -2,8 +2,8 @@ class Pidof < Formula
   desc "Display the PID number for a given process name"
   homepage "http://www.nightproductions.net/cli.htm"
   url "http://www.nightproductions.net/downloads/pidof_source.tar.gz"
-  sha256 "2a2cd618c7b9130e1a1d9be0210e786b85cbc9849c9b6f0cad9cbde31541e1b8"
   version "0.1.4"
+  sha256 "2a2cd618c7b9130e1a1d9be0210e786b85cbc9849c9b6f0cad9cbde31541e1b8"
 
   bottle do
     cellar :any_skip_relocation
