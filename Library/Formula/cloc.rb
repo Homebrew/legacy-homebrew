82d4a31a2cbb01e8027486d7b70065e9737fb819
syscrusher <syscrusher@users.noreply.github.com>
2016-02-21 17:25:32 -0600

    cloc: v1.66 upgrade

--- Library/Formula/cloc.rb
+++ Library/Formula/cloc.rb
@@ -1,14 +1,12 @@
 class Cloc < Formula
   desc "Statistics utility to count lines of code"
   homepage "https://github.com/AlDanial/cloc/"
-  url "https://github.com/AlDanial/cloc/archive/1.64.tar.gz"
-  sha256 "4ebac0ee3124df0c5394410f6803ed2b4f82de9035f62a0b89d8ce54e0726709"
+  url "https://github.com/AlDanial/cloc/archive/v1.66.tar.gz"
+  sha256 "6d8b5e9fac8934478a0fd815f60d814b93d4a12344c4820f5b47a55048b21d53"
   head "https://github.com/AlDanial/cloc.git"
 
-  bottle :unneeded
-
   def install
-    bin.install "cloc"
+    system "make", "-C", "Unix", "prefix=#{prefix}", "install"
   end
 
   test do
