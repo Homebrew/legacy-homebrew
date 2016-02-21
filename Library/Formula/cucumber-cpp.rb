fbc875f60b31a4b0663fc03b473c8a0d61b6d824
Martin Delille <martin@phonations.com>
2016-02-21 17:28:06 +0100

    use last release

--- Library/Formula/cucumber-cpp.rb
+++ Library/Formula/cucumber-cpp.rb
@@ -1,9 +1,9 @@
 class CucumberCpp < Formula
   desc "Support for writing Cucumber step definitions in C++"
   homepage "https://cucumber.io"
-  url "https://github.com/cucumber/cucumber-cpp/archive/1e633c6b59c75b959b480dcc4e40123e7fc59032.tar.gz"
+  url "https://github.com/cucumber/cucumber-cpp/archive/v0.3.tar.gz"
   version "0.3"
-  sha256 "1e211db8b8c230f291c8774d1c2e2bbbcf264d92016fd6081557425e43569c19"
+  sha256 "1c0f9949627e7528017bf00cbe49693ba9cbc3e11087f70aa33b21df93f341d6"
 
   depends_on "cmake" => :build
   depends_on "ruby"
