b72facbc0431a8f8530ce661bf4b5ebe4e65394c
k4zzk <kosuke19952000@gmail.com>
2016-02-21 21:42:24 +0900

    ttygif: add ttygif 0.2.1 (new formula)

--- Library/Formula/ttygif.rb
+++ Library/Formula/ttygif.rb
@@ -1,9 +1,10 @@
 class Ttygif < Formula
 
+  desc "Convert terminal recordings to animated gifs"
   homepage "https://github.com/icholy/ttygif.git"
-  head "https://github.com/icholy/ttygif.git", :branch => "master"
   url "https://github.com/icholy/ttygif/archive/1.0.8.tar.gz"
   sha256 "32b3394ebaac3389c66aee225ab61846fc84b02e218d0018515a6e9345a9f114"
+  head "https://github.com/icholy/ttygif.git", :branch => "master"
 
   depends_on "imagemagick"
   depends_on "ttyrec"
