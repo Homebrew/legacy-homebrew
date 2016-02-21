32ed50b206fe5330a34203cb2b95c56a479eb476
k4zzk <kosuke19952000@gmail.com>
2016-02-21 21:31:53 +0900

    ttygif: add ttygif 0.2.0

--- Library/Formula/ttygif.rb
+++ Library/Formula/ttygif.rb
@@ -2,8 +2,8 @@ class Ttygif < Formula
 
   homepage "https://github.com/icholy/ttygif.git"
   head "https://github.com/icholy/ttygif.git", :branch => "master"
-  url "https://github.com/icholy/ttygif/archive/1.0.8.zip"
-  sha1 "00e76af8ac11d8522ff32d5c86cba07545bf54c6"
+  url "https://github.com/icholy/ttygif/archive/1.0.8.tar.gz"
+  sha256 "32b3394ebaac3389c66aee225ab61846fc84b02e218d0018515a6e9345a9f114"
 
   depends_on "imagemagick"
   depends_on "ttyrec"
