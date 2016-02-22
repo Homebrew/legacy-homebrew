a54eed4da6de62a55c331a71167f7109b6319d35
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-22 00:56:08 +0000

    mjpegtools: add 2.1.0_1 bottle.

--- Library/Formula/mjpegtools.rb
+++ Library/Formula/mjpegtools.rb
@@ -5,6 +5,13 @@ class Mjpegtools < Formula
   sha256 "864f143d7686377f8ab94d91283c696ebd906bf256b2eacc7e9fb4dddcedc407"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "58c10848fd5d1f9bd95c5ca5e046a1e68e1162ebe382b79b5f93e625c4c2855e" => :el_capitan
+    sha256 "f96946c117b2d08b387b57e58c9642968c74a1c061ceb651fa92de6e63f8cb46" => :yosemite
+    sha256 "3477b7df54eaef29d1502fb505eb5f55b2da05ab40cb529460d10523a1b5ab93" => :mavericks
+  end
+
   depends_on :x11 => :optional
 
   depends_on "pkg-config" => :build
