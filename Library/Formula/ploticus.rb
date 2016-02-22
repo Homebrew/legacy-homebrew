8ec1be501f6f9bc0e15f5e3c61b1d1cc4c85f730
Ned Konz <ned@bike-nomad.com>
2016-02-22 08:13:52 -0800

    Added revision bump to ploticus formula

--- Library/Formula/ploticus.rb
+++ Library/Formula/ploticus.rb
@@ -4,6 +4,7 @@ class Ploticus < Formula
   url "https://downloads.sourceforge.net/project/ploticus/ploticus/2.42/ploticus242_src.tar.gz"
   version "2.42"
   sha256 "3f29e4b9f405203a93efec900e5816d9e1b4381821881e241c08cab7dd66e0b0"
+  revision 1
 
   bottle do
     cellar :any
