8d0f6645811699bb6338d6508259713de8411774
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-22 02:55:24 +0000

    patchutils: update 0.3.4 bottle.

--- Library/Formula/patchutils.rb
+++ Library/Formula/patchutils.rb
@@ -15,10 +15,10 @@ class Patchutils < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "90200a612c126f69319f01d17b43365e5c6aacceb86fd256d81e85d4ee5c1421" => :el_capitan
-    sha256 "24f30129312fa91a40c2e4ccede817ea0ccaf51ecad8cf656089332dba78a84c" => :yosemite
-    sha256 "947e0db6b163b638d885a97b336c08d48bef820ee69b3668810f63d24623e300" => :mavericks
-    sha256 "58d815641c3a74f955f94cc5fa127bbc4da63547cd210bdd9385bd76ed825f64" => :mountain_lion
+    revision 1
+    sha256 "08689727cdf1f89cc075ec2e6e71ac456c5429067899ccb5dac12848a6c2abcd" => :el_capitan
+    sha256 "df5a0b90219fe01b6934696d27782920c3e9c45152980e72a1c542006c625ae9" => :yosemite
+    sha256 "84cae8e92f1e9b3f377fd6584c0811a3c3989fb898bb7596ba0d1192ae10a834" => :mavericks
   end
 
   def install
