2d520e0db30088f45728f18c4d1a1db2e5a0ba23
Martin Delille <martin@phonations.com>
2016-02-22 10:53:33 +0100

    add failing test

--- Library/Formula/cucumber-cpp.rb
+++ Library/Formula/cucumber-cpp.rb
@@ -6,6 +6,7 @@ class CucumberCpp < Formula
   sha256 "1c0f9949627e7528017bf00cbe49693ba9cbc3e11087f70aa33b21df93f341d6"
 
   depends_on "cmake" => :build
+  depends_on "boost"
 
   def install
     system "cmake", "-DCUKE_DISABLE_GTEST=on",
@@ -17,6 +18,29 @@ class CucumberCpp < Formula
   end
 
   test do
-    system "false"
+    (testpath/"features/test.features").write <<-EOS.undent
+      Feature: Test
+      Scenario Outline: Just for test
+        Given A given statement
+        When A when statement
+        Then A then statement
+    EOS
+    (testpath/"features/step_definiations/cucumber.wire").write <<-EOS.undent
+      host: localhost
+      port:3902
+    EOS
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <cucumber-cpp/defs.hpp>
+      GIVEN("^A given statement$") {
+      }
+      WHEN("^A when statement$") {
+      }
+      THEN("^A then statement$") {
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-L#{lib}", "-lcucumber-cpp", "-o", "test", "-stdlib=libstdc++", "-std=c++11",
+      "-lboost_unit_test_framework", "-lboost_regex", "-lboost_system"
+    system "./test&"
+    system "cucumber"
   end
 end
