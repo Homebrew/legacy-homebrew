f4e848f9596c45fcdfca33b1cfb460bfe7459c67
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-23 15:09:24 +0000

    bmake: update 20150606 bottle.

--- Library/Formula/bmake.rb
+++ Library/Formula/bmake.rb
@@ -6,6 +6,7 @@ class Bmake < Formula
   sha256 "c3147540fd5f64d5f2c1c7cd5d65d64d69bc5573e02707774d8eee349f043946"
 
   bottle do
+    sha256 "25e8cf1c38ece0c9dc4f55cb65dc61812ef74ab18af60f92f5aae0fa5a0e3517" => :el_capitan
     sha256 "d902fd473759c625fff2a6303a8b2a881f6e7eee050957bed8e5715531b00569" => :yosemite
     sha256 "cc1978660d087797042a19efe067a902fad1c4081851d57f331790278868fcd9" => :mavericks
     sha256 "3ca7512237d38d75a745b23e90e8fa6c1b4e572ebea7f4d0454e4cf8f7b017bf" => :mountain_lion
