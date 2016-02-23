21dbfda71c5e9e0e3aeb5f11dbdce590f13d7269
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-23 21:22:14 +0000

    cuty_capt: update 0.0.6 bottle.

--- Library/Formula/cuty_capt.rb
+++ Library/Formula/cuty_capt.rb
@@ -7,6 +7,7 @@ class CutyCapt < Formula
 
   bottle do
     cellar :any
+    sha256 "dc757b1c8e76ec43111f7eb8b232ed876b366a81f27ce53af1a828496eb8a8bb" => :el_capitan
     sha256 "0be50eae29e65d857d1c46e31ee611e58e4ab6d7a4f01720238b13ef12739d27" => :yosemite
     sha256 "bb1cc3c4e02ba780d9ba977445952c8bfa92e21eb5821a466beec0a376c45e43" => :mavericks
     sha256 "804e8194a1280f535be0851de27d66867f90ccf47167c3729a7ba3f87f73d9ff" => :mountain_lion
