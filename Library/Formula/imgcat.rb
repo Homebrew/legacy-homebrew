692797b71b1a1d72d4620e874d73690ee75ef187
Anastas Dancha <anapsix@random.io>
2016-02-23 16:03:13 -0500

    Bump version to 2.9, same as current version of iTerm

--- Library/Formula/imgcat.rb
+++ Library/Formula/imgcat.rb
@@ -2,8 +2,12 @@ class Imgcat < Formula
   desc "iTerm's imgcat"
   homepage "https://www.iterm2.com/images.html"
   url "https://raw.githubusercontent.com/gnachman/iTerm2/7fb596f04b8e8c7e730efe1a033317f121a2f943/tests/imgcat"
-  version "0.1"
+  version "2.9"
   sha256 "036ee8aec2487a02b40d62d72918090ae022fc018589337dfd8403e08ffdd0c0"
+
+  head do 
+    url "https://raw.githubusercontent.com/gnachman/iTerm2/HEAD/tests/imgcat", :using => :curl
+  end
 
   def install
     bin.install "imgcat"
