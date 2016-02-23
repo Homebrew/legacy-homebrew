8416c4103d09ea16855b0bbfdb7aa3af5935a49e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-23 13:02:09 +0000

    asciitex: update 0.21 bottle.

--- Library/Formula/asciitex.rb
+++ Library/Formula/asciitex.rb
@@ -5,7 +5,8 @@ class Asciitex < Formula
   sha256 "abf964818833d8b256815eb107fb0de391d808fe131040fb13005988ff92a48d"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "0ae267d7ffcf17769da97275af047dc2a4ba9e5086acdb53dd11ca41f3d40ddb" => :el_capitan
     sha256 "e9eadc960e449db67f305c3e1cc8d4f025288967bb8b6a37f5ba4bf5ad58493d" => :yosemite
     sha256 "31518a63d9b06f8e47ff57c5d6d22ca838abc2c1366d59db2bb8967971134d52" => :mavericks
     sha256 "cfcef4a17d2194a111da39891f06694d2056082915858d3cf938d3659b2d1a64" => :mountain_lion
