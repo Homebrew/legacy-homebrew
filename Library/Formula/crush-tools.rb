01c730fa88a147e94c89dee68388cdd298437b72
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-23 20:08:44 +0000

    crush-tools: update 20150716 bottle.

--- Library/Formula/crush-tools.rb
+++ Library/Formula/crush-tools.rb
@@ -6,6 +6,7 @@ class CrushTools < Formula
 
   bottle do
     cellar :any
+    sha256 "0c7c58b9f2ec87237934eda55932b200c6d7b7f6dbb07a35e0a49ed389e984d3" => :el_capitan
     sha256 "90c901bd6daf8178407232c6b3be7f3c5056e9cf2ab88750d09b151e0973d4ff" => :yosemite
     sha256 "f1319787a7aafc6610f0217299791c428e5784d11cc93c8cd623e8a5cba5c414" => :mavericks
     sha256 "4fe244ac195a6051551f610c9435de7bf65beaa0b3c00ef5ff39945e9d2f3b3f" => :mountain_lion
