2d12c91999ef2e2e2dec0636ce725bd49b065779
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-23 20:31:28 +0000

    csvprintf: update 1.0.3 bottle.

--- Library/Formula/csvprintf.rb
+++ Library/Formula/csvprintf.rb
@@ -5,7 +5,8 @@ class Csvprintf < Formula
   sha256 "484db6a5f0cdb1a09b375274b30fbbde3c886d5da974d3f247c83b0bf853ef83"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "6ad1c8501cb1f51bd6910edb2a9da1c55507186020d7340e83a178a4a036df4d" => :el_capitan
     sha256 "58691d8d4819618731ebdd034ae390ae8668b7e26017ef74d5edfe2c6daa4eb1" => :yosemite
     sha256 "269a3ceaa3acb2a0f8c3df760af3647cb64215d5dee4c8907af7b20200aab418" => :mavericks
     sha256 "762b98a66e435d7a9d4661565c73df656d8f7adc8ae7defe84e0d1ed0a31d280" => :mountain_lion
