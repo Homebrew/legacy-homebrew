6a3e52500a1126437cbfa7362d3378364e8d3425
Lars Eggert <lars@eggert.org>
2016-02-24 17:57:21 +0100

    pkt2flow 1.2

---
+++ Library/Formula/pkt2flow.rb
@@ -0,0 +1,17 @@
+class Pkt2flow < Formula
+  desc "Classify packets into flows"
+  homepage "https://github.com/caesar0301/pkt2flow"
+  url "https://github.com/caesar0301/pkt2flow/archive/v1.2.tar.gz"
+  sha256 "deb4c5dc2fd11c45e338e9d0c0127df03257040127c49016e40aaf491e5674a6"
+
+  depends_on "scons" => :build
+
+  def install
+    "scons"
+    bin.install "pkt2flow"
+  end
+
+  test do
+    system "pkt2flow"
+  end
+end
