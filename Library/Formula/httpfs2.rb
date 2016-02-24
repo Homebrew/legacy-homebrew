499920fa17d00b5025e9febfbae9818eae394a40
Red54 <Yeking@Red54.com>
2016-02-24 11:22:57 +0800

    fix httpfs2.rb

---
+++ Library/Formula/httpfs2.rb
@@ -0,0 +1,25 @@
+class Httpfs2 < Formula
+  desc "FUSE filesystem for mounting files from http servers"
+  homepage "http://httpfs.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/httpfs/httpfs2/httpfs2-0.1.5.tar.gz"
+  sha256 "01cb4bb38deb344f540da6f1464dc7edbdeb51213ad810b8c9c282c1e17e0fc1"
+
+  depends_on "pkg-config" => :build
+  depends_on "dpkg" => :build
+  depends_on "asciidoc" => :build
+  depends_on "docbook-xsl" => :build
+  depends_on "gnutls"
+  depends_on :osxfuse
+
+  def install
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+    system "make"
+    system "make", "httpfs2.1"
+    bin.install "httpfs2", "httpfs2-mt", "httpfs2-ssl", "httpfs2-ssl-mt"
+    man1.install Dir["*.1"]
+  end
+
+  test do
+    system "httpfs2-ssl-mt"
+  end
+end
