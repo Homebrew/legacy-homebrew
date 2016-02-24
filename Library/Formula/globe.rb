0f0b822e3ee4f3a2a580542264e52da1a16c11d6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-24 20:46:13 +0000

    globe: update 0.0.20140814 bottle.

--- Library/Formula/globe.rb
+++ Library/Formula/globe.rb
@@ -6,7 +6,8 @@ class Globe < Formula
   sha256 "5507a4caaf3e3318fd895ab1f8edfa5887c9f64547cad70cff3249350caa6c86"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "11acded7be5d1ba22260d039e3daf4fdc4cac49ebcd234c879da655a1532c22f" => :el_capitan
     sha256 "a3ccdf74813e704ab1c8d50bb32f3f9b3f62110c8a6a143e3df85eb6ab7ecd7d" => :yosemite
     sha256 "8ff4dd96c5ab3c846ac61ff66c866e3d4969560611405db6e20436ff75494715" => :mavericks
     sha256 "c37aa4067f2d9ce7b5fbed7246e67164d04e500bfbc193a4a1e19fdba741be85" => :mountain_lion
