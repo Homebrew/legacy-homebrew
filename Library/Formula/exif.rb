f7d394994c095602921d6b622c61ed21b1521ac4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-24 02:06:08 +0000

    exif: update 0.6.21 bottle.

--- Library/Formula/exif.rb
+++ Library/Formula/exif.rb
@@ -6,6 +6,7 @@ class Exif < Formula
 
   bottle do
     cellar :any
+    sha256 "607827dc887cae86a2bedc7892e4d061990cb04814fb0ac2fd5d0cd1fdefcb30" => :el_capitan
     sha256 "ebf412bb43ad3695f01f8ed1e5851e7c02218589fc7cbb175c9a3f1914894e37" => :yosemite
     sha256 "e24a0bdfee07a46a49620c29726748e73a4eca0937c59c1f5efeeb51ea9f6971" => :mavericks
     sha256 "0d5997d1d013822aee275d0e65403a1d99b579f53c2ee131b5b121d85377665d" => :mountain_lion
