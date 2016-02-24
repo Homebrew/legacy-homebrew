a8eb15c30959320ae348614b58a7b391b084c74c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-24 15:12:22 +0000

    fwknop: update 2.6.7 bottle.

--- Library/Formula/fwknop.rb
+++ Library/Formula/fwknop.rb
@@ -6,6 +6,7 @@ class Fwknop < Formula
   head "https://github.com/mrash/fwknop.git"
 
   bottle do
+    sha256 "e61c96906e247525f0ab63c4804ce59061372c7cbf0cf5b82decfadad6d12ad7" => :el_capitan
     sha256 "d00d233db7cb2cb04c50ee792756d76cfda37f2c6bc210e313375dca01d8f4d1" => :yosemite
     sha256 "a69bbd4435378eb2b6b5a8046d3562aef0f22152b50521c106700aa53f221724" => :mavericks
     sha256 "c003419be9572eab4fa791bb43b4faa63235caadac15f3cb1d1f38b555b927ef" => :mountain_lion
