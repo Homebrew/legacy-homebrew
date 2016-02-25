383fa20fa562d1bddd31cc290f5d8234f1615239
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-25 23:07:39 +0000

    clpbar: add 1.11.1 bottle.

--- Library/Formula/clpbar.rb
+++ Library/Formula/clpbar.rb
@@ -4,6 +4,13 @@ class Clpbar < Formula
   url "https://downloads.sourceforge.net/project/clpbar/clpbar/bar-1.11.1/bar_1.11.1.tar.gz"
   sha256 "fa0f5ec5c8400316c2f4debdc6cdcb80e186e668c2e4471df4fec7bfcd626503"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b54fa0ce24de6dda141e3fc025b67f2e0216b01a3664ec5992a98f8087881ddd" => :el_capitan
+    sha256 "edaa21e4d80bbf174e3c040d3c786eb48e381ca8e9477dfbafb06b4ec0bfd19f" => :yosemite
+    sha256 "36f1ece44bec7c54d2235fbfba5122d3a3d0430532498dc5c35601a59f9c4616" => :mavericks
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
