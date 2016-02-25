805b95d4d3a56d5542ebe13531d42ff5c34cba07
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-25 21:50:10 +0000

    libebml: fix head placement

--- Library/Formula/libebml.rb
+++ Library/Formula/libebml.rb
@@ -5,19 +5,19 @@ class Libebml < Formula
   mirror "https://www.bunkus.org/videotools/mkvtoolnix/sources/libebml-1.3.3.tar.bz2"
   sha256 "35fb44daa41961f94a0ac1b8f06801e88cc9bf2ad6f562ced8ab7c1f1a875499"
 
-  head do
-    url "https://github.com/Matroska-Org/libebml.git"
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-    depends_on "libtool" => :build
-  end
-
   bottle do
     cellar :any
     revision 1
     sha256 "9d18eec56b97f4b9304d1c9b92b1d3cd6e3aa6c2a844098c197c3524ec68fb82" => :el_capitan
     sha256 "2590306d51d005b74fb05256c2673b4346c875ae85c4835fd2af3ebd8077f3f5" => :yosemite
     sha256 "1a2dc78501374e502a94e9e48646e01ad08fbf28be22b2d0ea8e945608623784" => :mavericks
+  end
+
+  head do
+    url "https://github.com/Matroska-Org/libebml.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
   end
 
   option :cxx11
