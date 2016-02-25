ec8eacf122e0b34fea097dd4e0c8d74176319690
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-25 20:17:06 +0000

    clamz: follow archive redirect

--- Library/Formula/clamz.rb
+++ Library/Formula/clamz.rb
@@ -1,6 +1,6 @@
 class Clamz < Formula
   desc "Download MP3 files from Amazon's music store"
-  homepage "https://code.google.com/p/clamz/"
+  homepage "https://code.google.com/archive/p/clamz/"
   url "https://clamz.googlecode.com/files/clamz-0.5.tar.gz"
   sha256 "5a63f23f15dfa6c2af00ff9531ae9bfcca0facfe5b1aa82790964f050a09832b"
   revision 1
