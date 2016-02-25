2b3de637aa89799fd53cae200e4895a91bd458fd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-25 20:06:59 +0000

    mkclean: add 0.8.7 bottle.

--- Library/Formula/mkclean.rb
+++ Library/Formula/mkclean.rb
@@ -4,6 +4,13 @@ class Mkclean < Formula
   url "https://downloads.sourceforge.net/project/matroska/mkclean/mkclean-0.8.7.tar.bz2"
   sha256 "88713065a172d1ab7fd34c8854a42f6bf8d0e794957265340328a2f692ad46d9"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f8628b1bfb08d1624faa2037d199592bc6209759322bcfc53eb5649ad304e4bd" => :el_capitan
+    sha256 "cefecf33d4cb9fa15d582b0d03c26cf3a14228d02832ddbf3187d5c4ffd4a4c2" => :yosemite
+    sha256 "51b53b0e49a5fe451c6bd3589e780e51ac23c637493c8804233057cb79b9d40d" => :mavericks
+  end
+
   # Fixes compile error with XCode-4.3+, a hardcoded /Developer.  Reported as:
   # https://sourceforge.net/tracker/?func=detail&aid=3505611&group_id=68739&atid=522228
   patch :DATA if MacOS.prefer_64_bit?
