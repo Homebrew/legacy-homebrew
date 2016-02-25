2b2a0f0d1ba264b76d745d8421fd8ebf95caa768
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-25 20:13:10 +0000

    cjdns 17.3

--- Library/Formula/cjdns.rb
+++ Library/Formula/cjdns.rb
@@ -1,8 +1,9 @@
 class Cjdns < Formula
   desc "Advanced mesh routing system with cryptographic addressing"
   homepage "https://github.com/cjdelisle/cjdns/"
-  url "https://github.com/cjdelisle/cjdns/archive/cjdns-v17.1.tar.gz"
-  sha256 "cb058693ca4b12fd946544f8effc7df4c022750f7150f67cbed093e394952626"
+  url "https://github.com/cjdelisle/cjdns/archive/cjdns-v17.3.tar.gz"
+  sha256 "3193df651ad9c00f31ab04feb33f801645996f6647c89b63bcc327b48e17e602"
+  head "https://github.com/cjdelisle/cjdns.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,10 +17,10 @@ class Cjdns < Formula
   def install
     system "./do"
     bin.install "cjdroute"
-    (share+"test").install "build_darwin/test_testcjdroute_c" => "cjdroute_test"
+    (pkgshare/"test").install "build_darwin/test_testcjdroute_c" => "cjdroute_test"
   end
 
   test do
-    system "#{share}/test/cjdroute_test", "all"
+    system "#{pkgshare}/test/cjdroute_test", "all"
   end
 end
