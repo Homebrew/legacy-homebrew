4fe4e6170b388bbdd1b277c157e5dbce2ef2e5df
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-26 12:01:52 +0000

    cpulimit: add 0.2 bottle.

--- Library/Formula/cpulimit.rb
+++ Library/Formula/cpulimit.rb
@@ -6,6 +6,13 @@ class Cpulimit < Formula
 
   head "https://github.com/opsengine/cpulimit.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9d7320465152a12ba75ce924beada5a3ce365b14becaa75e08ee8334c2cb2f6a" => :el_capitan
+    sha256 "7ff9d929c5a1178b250b756cefcbecc4b202c72f03073e9eb43f4a47420930a8" => :yosemite
+    sha256 "c24a495cd69c62693bcb0bd2a44c41c5bca84f0b9754019681816c1d2b47fe3e" => :mavericks
+  end
+
   def install
     system "make"
     bin.install "src/cpulimit"
