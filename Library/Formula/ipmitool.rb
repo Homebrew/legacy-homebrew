c2523f65402d64e5b0311f342994bdc193cac457
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-26 19:28:49 +0000

    ipmitool: add 1.8.16 bottle.

--- Library/Formula/ipmitool.rb
+++ Library/Formula/ipmitool.rb
@@ -5,6 +5,13 @@ class Ipmitool < Formula
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/ipmitool/ipmitool_1.8.16.orig.tar.bz2"
   sha256 "3c5da6b067abf475bc24685120ec79f6e4ef6b3ea606aaa267e462023861223e"
 
+  bottle do
+    cellar :any
+    sha256 "c7018b064716f0b24941a54722527395ac99234b03f22553dfa59be90f70d44b" => :el_capitan
+    sha256 "a155567444aef5b7805c3585156ce4495087938472a4529943339ffc805ce159" => :yosemite
+    sha256 "a043b09b7ed3ade3c5aab3ef9d48a419e5ba1ed93ebceabab20cec3b8df82d84" => :mavericks
+  end
+
   depends_on "openssl"
 
   def install
