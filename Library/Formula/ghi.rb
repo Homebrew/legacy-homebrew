9963ceb3523e5c9f45764b345deeb0e59387a17a
Alex Chesters <AlexChesters@users.noreply.github.com>
2016-02-26 12:01:32 +0000

    ghi.rb 1.1.1

--- Library/Formula/ghi.rb
+++ Library/Formula/ghi.rb
@@ -1,8 +1,8 @@
 class Ghi < Formula
   desc "Work on GitHub issues on the command-line"
   homepage "https://github.com/stephencelis/ghi"
-  url "https://github.com/stephencelis/ghi/archive/1.1.0.tar.gz"
-  sha256 "90ee10cefe9b83514eadb185b339a7147627c54c508e28f292e56719af6bfcb6"
+  url "https://github.com/stephencelis/ghi/archive/1.1.1.tar.gz"
+  sha256 "a19fd947f1268d9f087d2a342964dfc1cb3aa96de970e82b8daf22461e07e49b"
   head "https://github.com/stephencelis/ghi.git"
 
   bottle :unneeded
