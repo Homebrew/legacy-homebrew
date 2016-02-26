3d92d5c223293d87acdf93b6c1f3369715d7916d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-26 04:54:39 +0000

    yeti: add 0.9.9.1 bottle.

--- Library/Formula/yeti.rb
+++ Library/Formula/yeti.rb
@@ -6,6 +6,13 @@ class Yeti < Formula
 
   head "https://github.com/mth/yeti.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "268781d28a766896e5ef40dc5c364b8a40617e6c28a3c96b693eba473c56e660" => :el_capitan
+    sha256 "a1ef95834d3631f7d04e18fd90786b0c8b0f4e492abe6f665a64d0d31ee768c5" => :yosemite
+    sha256 "9dd1a7662b45c454c47075ee029c554bf2655423376bd373cde6e15e716e7677" => :mavericks
+  end
+
   depends_on :ant => :build
   depends_on :java => "1.8"
 
