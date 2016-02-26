d367e176f73807242ef41dfa2eaaf17ec96334c2
Alexander Altman <alexanderaltman@me.com>
2016-02-25 19:43:10 -0800

    yeti 0.9.9.1

--- Library/Formula/yeti.rb
+++ Library/Formula/yeti.rb
@@ -1,13 +1,13 @@
 class Yeti < Formula
   desc "ML-style functional programming language that runs on the JVM"
   homepage "https://mth.github.io/yeti/"
-  url "https://github.com/mth/yeti/archive/v0.9.9.tar.gz"
-  sha256 "45854dc436c7204daa481fda5fab8f1ce11d9a353ab5136ba7fc75ff0e222454"
+  url "https://github.com/mth/yeti/archive/v0.9.9.1.tar.gz"
+  sha256 "c552018993570724313fc0624d225e266cd95e993d121850b34aa706f04e3dfe"
 
   head "https://github.com/mth/yeti.git"
 
   depends_on :ant => :build
-  depends_on :java => "1.7"
+  depends_on :java => "1.8"
 
   def install
     system "ant", "jar"
