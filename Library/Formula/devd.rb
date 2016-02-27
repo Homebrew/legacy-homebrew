346f9233e41057cfc83258ef61b7b3722ff9756c
Alexander D. Kanevskiy <kad@kad.name>
2016-02-27 18:56:04 +0200

    devd 0.4
    - upgrade to 0.4 release
    - update dependecnies
    - remove dependencies that are not needed anymore

--- Library/Formula/devd.rb
+++ Library/Formula/devd.rb
@@ -3,9 +3,8 @@ require "language/go"
 class Devd < Formula
   desc "Local webserver for developers"
   homepage "https://github.com/cortesi/devd"
-  url "https://github.com/cortesi/devd/archive/v0.3.tar.gz"
-  sha256 "e806421a4de6572eb3196b215a86a3b72bcd0f5a12956224d191e47663f9c4ab"
-  revision 1
+  url "https://github.com/cortesi/devd/archive/v0.4.tar.gz"
+  sha256 "8cf15ece9b707658ef4825e4c031ff8dd54b8ea7c4ff6a9b07bb68b400d15253"
 
   bottle do
     cellar :any_skip_relocation
@@ -18,18 +17,13 @@ class Devd < Formula
 
   go_resource "github.com/GeertJohan/go.rice" do
     url "https://github.com/GeertJohan/go.rice.git",
-        :revision => "ada95a01c963696fb73320ee662195af68be81ae"
+        :revision => "0f3f5fde32fd1f755632a3d31ba2ec6d449e387b"
   end
 
   # go.rice dependencies
   go_resource "github.com/daaku/go.zipexe" do
     url "https://github.com/daaku/go.zipexe.git",
         :revision => "a5fe2436ffcb3236e175e5149162b41cd28bd27d"
-  end
-
-  go_resource "github.com/kardianos/osext" do
-    url "https://github.com/kardianos/osext.git",
-        :revision => "6e7f843663477789fac7c02def0d0909e969b4e5"
   end
 
   go_resource "github.com/GeertJohan/go.incremental" do
@@ -44,39 +38,44 @@ class Devd < Formula
 
   go_resource "github.com/jessevdk/go-flags" do
     url "https://github.com/jessevdk/go-flags.git",
-        :revision => "fc93116606d0a71d7e9de0ad5734fdb4b8eae834"
+        :revision => "6b9493b3cb60367edd942144879646604089e3f7"
   end
 
   # devd dependencies
+  go_resource "github.com/kardianos/osext" do
+    url "https://github.com/kardianos/osext.git",
+        :revision => "29ae4ffbc9a6fe9fb2bc5029050ce6996ea1d3bc"
+  end
+
   go_resource "github.com/bmatcuk/doublestar" do
-    # v1.0.1
+    # v1.0.3
     url "https://github.com/bmatcuk/doublestar.git",
-        :revision => "4f612bd6c10e2ef68e2ea50aabc50c3681bbac86"
+        :revision => "ec3b4af762f792cb93bf2518ff9ac408dbdc2b4e"
   end
 
   go_resource "github.com/dustin/go-humanize" do
     url "https://github.com/dustin/go-humanize.git",
-        :revision => "64dbdae0d393b7d71480a6dace78456396b55286"
+        :revision => "8929fe90cee4b2cb9deb468b51fb34eba64d1bf0"
   end
 
   go_resource "github.com/fatih/color" do
     url "https://github.com/fatih/color.git",
-        :revision => "9aae6aaa22315390f03959adca2c4d395b02fcef"
+        :revision => "4f7bcef27eec7925456d0c30c5e7b0408b3339be"
   end
 
   go_resource "github.com/goji/httpauth" do
     url "https://github.com/goji/httpauth.git",
-        :revision => "c1b2bcd8769bd15cc56751223fd4b9f45ca987ca"
+        :revision => "fc389c3003535723411032f2e9300889389e30fa"
   end
 
   go_resource "github.com/gorilla/websocket" do
     url "https://github.com/gorilla/websocket.git",
-        :revision => "361d4c0ffd78338ebe0a9e6320cdbe115d7dc026"
+        :revision => "5c91b59efa232fa9a87b705d54101832c498a172"
   end
 
   go_resource "github.com/juju/ratelimit" do
     url "https://github.com/juju/ratelimit.git",
-        :revision => "772f5c38e468398c4511514f4f6aa9a4185bc0a0"
+        :revision => "77ed1c8a01217656d2080ad51981f6e99adaa177"
   end
 
   go_resource "github.com/mitchellh/go-homedir" do
@@ -86,7 +85,7 @@ class Devd < Formula
 
   go_resource "github.com/rjeczalik/notify" do
     url "https://github.com/rjeczalik/notify.git",
-        :revision => "1869adb163fffce8fb5b8755379d1042cdb4c4f8"
+        :revision => "efbcb15a545abd0c7d546f0a15f4115c713a4c59"
   end
 
   go_resource "github.com/toqueteos/webbrowser" do
@@ -97,7 +96,7 @@ class Devd < Formula
 
   go_resource "github.com/alecthomas/template" do
     url "https://github.com/alecthomas/template.git",
-        :revision => "b867cc6ab45cece8143cfcc6fc9c77cf3f2c23c0"
+        :revision => "14fd436dd20c3cc65242a9f396b61bfc8a3926fc"
   end
 
   go_resource "github.com/alecthomas/units" do
@@ -105,30 +104,31 @@ class Devd < Formula
         :revision => "2efee857e7cfd4f3d0138cc3cbb1b4966962b93a"
   end
 
-  go_resource "github.com/mattn/go-colorable" do
-    url "https://github.com/mattn/go-colorable.git",
-        :revision => "51a7e7a8b1665b25ca173debdc8d52d493348f15"
-  end
-
-  go_resource "github.com/mattn/go-isatty" do
-    url "https://github.com/mattn/go-isatty.git",
-        :revision => "d6aaa2f596ae91a0a58d8e7f2c79670991468e4f"
-  end
-
   go_resource "golang.org/x/crypto" do
     url "https://go.googlesource.com/crypto.git",
-        :revision => "575fdbe86e5dd89229707ebec0575ce7d088a4a6"
+        :revision => "5dc8cb4b8a8eb076cbb5a06bc3b8682c15bdbbd3"
   end
 
   go_resource "golang.org/x/net" do
     url "https://go.googlesource.com/net.git",
-        :revision => "c764672d0ee39ffd83cfcb375804d3181302b62b"
+        :revision => "6acef71eb69611914f7a30939ea9f6e194c78172"
   end
 
   go_resource "gopkg.in/alecthomas/kingpin.v2" do
-    # v2.4.1
+    # v2.1.11
     url "https://github.com/alecthomas/kingpin.git",
-        :revision => "95529ad11b3c862a5b828a2142b9e50db579cf2c"
+        :revision => "8cccfa8eb2e3183254457fb1749b2667fbc364c7"
+  end
+
+  go_resource "github.com/cortesi/modd" do
+    # v0.2
+    url "https://github.com/cortesi/modd.git",
+        :revision => "459621750bfdbd0a80d706db8092d4b3a34a55d6"
+  end
+
+  go_resource "github.com/cortesi/termlog" do
+    url "https://github.com/cortesi/termlog.git",
+        :revision => "898fe0decfa430637283eebf1fd19d4f6ce0a531"
   end
 
   def install
