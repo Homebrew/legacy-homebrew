8b28aa109379c278555699bf324a6aa17ad7c44c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-28 19:03:17 +0000

    classads: update 1.0.10 bottle.

--- Library/Formula/classads.rb
+++ Library/Formula/classads.rb
@@ -6,9 +6,10 @@ class Classads < Formula
 
   bottle do
     cellar :any
-    sha256 "136cc9cc765d8ee3d1f1567dc1352fdecebd5971fbb34bf7fb7c1df296f72e15" => :el_capitan
-    sha256 "149d6fca3c90785036075fe54d3bfb1f35a554d592dcc963628d21cc7096cfc9" => :yosemite
-    sha256 "296cbe16a30b149682d56720f0cfe552c74eb9dfe08566e30be622696d26e83e" => :mavericks
+    revision 1
+    sha256 "52bd3bb21e7a2491ad96f01988b802ab183c5e93d3123e9cc57b75e1a0076171" => :el_capitan
+    sha256 "2ec01b2285391e8c1a696c783db281dc69c05e0f2c483792129799b8ad304d7e" => :yosemite
+    sha256 "b2ba8857a9e07ece0c19ac04a78893322bb22b361e28d5eb23abfa2515e965c9" => :mavericks
   end
 
   depends_on "pcre"
