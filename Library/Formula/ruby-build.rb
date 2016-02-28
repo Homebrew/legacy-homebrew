2e6535f02032f0fcf81b82853cd0490a87eed674
Mislav Marohnić <mislav.marohnic@gmail.com>
2016-02-28 12:59:20 +0900

    ruby-build 20160228

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20160226.tar.gz"
-  sha256 "6c415bb1a3b3e619d434bf88c0c0fea0df1f8f5f4b166cc9103a936b6359d28a"
+  url "https://github.com/rbenv/ruby-build/archive/v20160228.tar.gz"
+  sha256 "ee819bfc7f7cfff41e077c05ba8e7e9e813c8c7f1e4af79be8775df3d41c0685"
 
   head "https://github.com/rbenv/ruby-build.git"
 
