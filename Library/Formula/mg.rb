51508ad99acaf5b9d726bccb2c4d4489d6bfbef7
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 02:25:02 +0100

    mg: use secure urls

--- Library/Formula/mg.rb
+++ Library/Formula/mg.rb
@@ -1,7 +1,7 @@
 class Mg < Formula
   desc "Small Emacs-like editor"
-  homepage "http://devio.us/~bcallah/mg/"
-  url "http://devio.us/~bcallah/mg/mg-20160103.tar.gz"
+  homepage "https://devio.us/~bcallah/mg/"
+  url "https://devio.us/~bcallah/mg/mg-20160103.tar.gz"
   sha256 "4abd059ba3e0d59626104a21812ae33a37ee1f6ddaafdb33511f38d21057fae6"
 
   bottle do
