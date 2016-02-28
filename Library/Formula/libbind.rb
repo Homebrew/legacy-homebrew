7bba3752844be4cd44b3fc18773caf55e1808895
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 02:30:12 +0100

    libbind: use secure url

--- Library/Formula/libbind.rb
+++ Library/Formula/libbind.rb
@@ -1,7 +1,7 @@
 class Libbind < Formula
   desc "Original resolver library from ISC"
   homepage "https://www.isc.org/software/libbind"
-  url "ftp://ftp.isc.org/isc/libbind/6.0/libbind-6.0.tar.gz"
+  url "https://ftp.isc.org/isc/libbind/6.0/libbind-6.0.tar.gz"
   sha256 "b98b6aa6e7c403f5a6522ffb68325785a87ea8b13377ada8ba87953a3e8cb29d"
 
   bottle do
