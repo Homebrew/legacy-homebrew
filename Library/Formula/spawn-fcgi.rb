23c35ec663081aaf72a332c1abcf84282d256779
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 03:45:58 +0100

    spawn-fcgi: use secure url

--- Library/Formula/spawn-fcgi.rb
+++ Library/Formula/spawn-fcgi.rb
@@ -1,7 +1,7 @@
 class SpawnFcgi < Formula
   desc "Spawn fast-CGI processes"
   homepage "https://redmine.lighttpd.net/projects/spawn-fcgi"
-  url "http://www.lighttpd.net/download/spawn-fcgi-1.6.4.tar.gz"
+  url "https://www.lighttpd.net/download/spawn-fcgi-1.6.4.tar.gz"
   sha256 "ab327462cb99894a3699f874425a421d934f957cb24221f00bb888108d9dd09e"
 
   bottle do
