1f5f5917d96ade217bada808ec1ef3504adb0da1
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 03:58:29 +0100

    ficy: use secure urls
    Closes #49611.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ipv6toolkit.rb
+++ Library/Formula/ipv6toolkit.rb
@@ -1,7 +1,7 @@
 class Ipv6toolkit < Formula
   desc "Security assessment and troubleshooting tool for IPv6"
-  homepage "http://www.si6networks.com/tools/ipv6toolkit/"
-  url "http://www.si6networks.com/tools/ipv6toolkit/ipv6toolkit-v2.0.tar.gz"
+  homepage "https://www.si6networks.com/tools/ipv6toolkit/"
+  url "https://www.si6networks.com/tools/ipv6toolkit/ipv6toolkit-v2.0.tar.gz"
   sha256 "16f13d3e7d17940ff53f028ef0090e4aa3a193a224c97728b07ea6e26a19e987"
 
   bottle do
