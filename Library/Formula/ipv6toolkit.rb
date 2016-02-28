60f3af8a327bf39755b96f5d684181dc2d5824f2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-28 08:22:24 +0000

    ipv6toolkit: update 2.0 bottle.

--- Library/Formula/ipv6toolkit.rb
+++ Library/Formula/ipv6toolkit.rb
@@ -5,10 +5,11 @@ class Ipv6toolkit < Formula
   sha256 "16f13d3e7d17940ff53f028ef0090e4aa3a193a224c97728b07ea6e26a19e987"
 
   bottle do
-    cellar :any
-    sha256 "405c13a00007d35252de0999782ee56ae57cc32f0bcb955c44857c3f72ef1d5d" => :yosemite
-    sha256 "11a2ac433ca270b0daa2bf446d519cff84b99b3613b90729b2111412e2f4ec77" => :mavericks
-    sha256 "db171dc5d8df41460364822cbd96a2c0cead032471ea639118a5f38654e53556" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "1d7d0b6b4d248ccff98362f4d7707821ae2109591717d56d3b49f0c1d6b83b16" => :el_capitan
+    sha256 "d337148bcff3080cd4e94fee0adb1b5226421020bcc6296f97bcf1986dcbea6a" => :yosemite
+    sha256 "cd4b85ebae536aea022ab34174c36d360b2db31a9f5c11ad58ae6aa4644ff2b0" => :mavericks
   end
 
   def install
