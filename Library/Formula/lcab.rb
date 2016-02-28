68362f4912abb233cf4cb1ac31e46ffe442051b3
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 02:34:59 +0100

    lcab: upgrade url to http (from ftp)

--- Library/Formula/lcab.rb
+++ Library/Formula/lcab.rb
@@ -1,7 +1,7 @@
 class Lcab < Formula
   desc "Cabinet file creation tool"
   homepage "http://ohnopub.net/~ohnobinki/lcab/"
-  url "ftp://mirror.ohnopub.net/mirror/lcab-1.0b12.tar.gz"
+  url "http://mirror.ohnopub.net/mirror/lcab-1.0b12.tar.gz"
   mirror "https://launchpad.net/ubuntu/intrepid/+source/lcab/1.0b12-3/+files/lcab_1.0b12.orig.tar.gz"
   sha256 "065f2c1793b65f28471c0f71b7cf120a7064f28d1c44b07cabf49ec0e97f1fc8"
 
