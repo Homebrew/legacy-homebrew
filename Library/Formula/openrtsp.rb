a4c55236f3865e80f180167f62ae0d4927faae11
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-28 14:43:19 +0000

    openrtsp: update 2016.02.22 bottle.

--- Library/Formula/openrtsp.rb
+++ Library/Formula/openrtsp.rb
@@ -6,9 +6,9 @@ class Openrtsp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "19ebd75fbe2409869df5c1c9a90c3ffd146bf8f329f223dc6ee251fa44d1a4fc" => :el_capitan
-    sha256 "0c1083767885bf851959a9e74590f89b5d3a02b3c04965f3a9309162ec80bb34" => :yosemite
-    sha256 "4b65fe1d10e54ee81c429768ae37e0d6836e1cf50fc3c6d168e86e82adfb952a" => :mavericks
+    sha256 "a2ead1f61b03f296f4256565e34edc0793316001584459166a90e5f2a3a68ffd" => :el_capitan
+    sha256 "7e238c49ca726642bebd349d8230d2a60979770c35df8b7a130225ea1fe63064" => :yosemite
+    sha256 "76f16900271a7f413655ad697c2cfb3a608eadb506c9f7d5acd960d8d32ec828" => :mavericks
   end
 
   option "32-bit"
