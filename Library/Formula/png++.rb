9ae7fd60fc730fe7d76e58f7370b19c7e2214f39
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 03:40:47 +0100

    png++: use secure url (redirected to cleartext)

--- Library/Formula/png++.rb
+++ Library/Formula/png++.rb
@@ -1,7 +1,7 @@
 class Pngxx < Formula
   desc "C++ wrapper for libpng library"
   homepage "http://www.nongnu.org/pngpp/"
-  url "http://download.savannah.nongnu.org/releases/pngpp/png++-0.2.5.tar.gz"
+  url "https://savannah.nongnu.org/download/pngpp/png++-0.2.5.tar.gz"
   sha256 "339fa2dff2cdd117efb43768cb272745faef4d02705b5e0e840537a2c1467b72"
   revision 1
 
