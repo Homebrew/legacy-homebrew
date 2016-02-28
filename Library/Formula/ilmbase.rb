25a1c7eb4c458f3d3ffd874407643a4cc984e876
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 03:37:32 +0100

    ilmbase: use secure urls

--- Library/Formula/ilmbase.rb
+++ Library/Formula/ilmbase.rb
@@ -1,8 +1,8 @@
 class Ilmbase < Formula
   desc "OpenEXR ILM Base libraries (high dynamic-range image file format)"
   homepage "http://www.openexr.com/"
-  url "http://download.savannah.nongnu.org/releases/openexr/ilmbase-2.2.0.tar.gz"
-  mirror "http://download-mirror.savannah.gnu.org/releases/openexr/ilmbase-2.2.0.tar.gz"
+  url "https://savannah.nongnu.org/download/openexr/ilmbase-2.2.0.tar.gz"
+  mirror "https://mirrorservice.org/sites/download.savannah.gnu.org/releases/openexr/ilmbase-2.2.0.tar.gz"
   sha256 "ecf815b60695555c1fbc73679e84c7c9902f4e8faa6e8000d2f905b8b86cedc7"
 
   bottle do
