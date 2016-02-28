8c6bb2ffe28d70dc0ad9999179526e31144c7765
Rohit Deshmukh <rohit.deshmukh@synerzip.com>
2016-02-28 08:41:06 +0530

    Update gradle.rb

--- Library/Formula/gradle.rb
+++ Library/Formula/gradle.rb
@@ -1,7 +1,7 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://downloads.gradle.org/distributions/gradle-2.11-bin.zip"
+  url "https://downloads.gradle.org/distributions/gradle-2.2-bin.zip"
   sha256 "8d7437082356c9fd6309a4479c8db307673965546daea445c6c72759cd6b1ed6"
 
   bottle :unneeded
