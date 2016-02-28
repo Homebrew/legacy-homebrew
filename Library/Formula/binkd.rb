d9c4c8bca3ff982857f0bbf7191320116f0abb34
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 02:34:34 +0100

    binkd: use secure url

--- Library/Formula/binkd.rb
+++ Library/Formula/binkd.rb
@@ -1,7 +1,7 @@
 class Binkd < Formula
   desc "TCP/IP FTN Mailer"
   homepage "http://binkd.grumbler.org/"
-  url "ftp://happy.kiev.ua/pub/fidosoft/mailer/binkd/binkd-1.0.4.tar.gz"
+  url "https://happy.kiev.ua/pub/fidosoft/mailer/binkd/binkd-1.0.4.tar.gz"
   sha256 "917e45c379bbd1a140d1fe43179a591f1b2ec4004b236d6e0c4680be8f1a0dc0"
 
   bottle do
