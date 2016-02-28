20600923c44b3cda2ffc9db7039bb12c3b86fdad
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-28 12:03:40 +0000

    montage: add 4.0 bottle.

--- Library/Formula/montage.rb
+++ Library/Formula/montage.rb
@@ -4,6 +4,13 @@ class Montage < Formula
   url "http://montage.ipac.caltech.edu/download/Montage_v4.0.tar.gz"
   sha256 "de143e4d4b65086f04bb75cf482dfa824965a5a402f3431f9bceb395033df5fe"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "503c3e946aa0d8f277b5e4a5aab75086d5c895551fa679a3129183b95f89b236" => :el_capitan
+    sha256 "7f9bb66eff925f20099f11ee247e4ba4c8b4821b74c7f2a3efd93d474e9a1b3f" => :yosemite
+    sha256 "30e68dcecc111af10a65b1edd33a0142457b2f2064e1bce45e33a6d3d11539d4" => :mavericks
+  end
+
   def install
     system "make"
     bin.install Dir["bin/m*"]
