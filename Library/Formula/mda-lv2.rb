ad610ad8b3fb59dcacb5a36b65402522b8ee1032
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 02:55:13 +0100

    mda-lv2: use secure urls

--- Library/Formula/mda-lv2.rb
+++ Library/Formula/mda-lv2.rb
@@ -1,7 +1,7 @@
 class MdaLv2 < Formula
   desc "LV2 port of the MDA plugins"
-  homepage "http://drobilla.net/software/mda-lv2/"
-  url "http://download.drobilla.net/mda-lv2-1.2.2.tar.bz2"
+  homepage "https://drobilla.net/software/mda-lv2/"
+  url "https://download.drobilla.net/mda-lv2-1.2.2.tar.bz2"
   sha256 "a476c31ed9f8b009ebacc32a02d06ba9584c0d0d03f03dd62b1354d10a030442"
 
   bottle do
