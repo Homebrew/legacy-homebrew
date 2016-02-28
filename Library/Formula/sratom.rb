8f46d2066ab22cfe1f6ab683993fb8a5b1291a21
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 02:56:06 +0100

    sratom: use secure urls

--- Library/Formula/sratom.rb
+++ Library/Formula/sratom.rb
@@ -1,7 +1,7 @@
 class Sratom < Formula
   desc "Library for serializing LV2 atoms to/from RDF"
-  homepage "http://drobilla.net/software/sratom/"
-  url "http://download.drobilla.net/sratom-0.4.6.tar.bz2"
+  homepage "https://drobilla.net/software/sratom/"
+  url "https://download.drobilla.net/sratom-0.4.6.tar.bz2"
   sha256 "a4b9beaeaedc4f651beb15cd1cfedff905b0726a9010548483475ad97d941220"
 
   bottle do
