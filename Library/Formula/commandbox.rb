6b2dc0d55ba4329696cd417d59f7e098183589ac
Viktor Szakats <vszakats@users.noreply.github.com>
2016-02-28 03:08:22 +0100

    commandbox: use secure urls

--- Library/Formula/commandbox.rb
+++ Library/Formula/commandbox.rb
@@ -1,11 +1,11 @@
 class Commandbox < Formula
   desc "CFML embedded server, package manager, and app scaffolding tools"
-  homepage "http://www.ortussolutions.com/products/commandbox"
+  homepage "https://www.ortussolutions.com/products/commandbox"
   url "http://downloads.ortussolutions.com/ortussolutions/commandbox/3.0.1/commandbox-bin-3.0.1.zip"
   sha256 "b5f821f5c327f5387a7cdf20855510fe39c1770dd2f0f383c4576519bae3d5d9"
 
   devel do
-    url "http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.0/commandbox-bin-3.1.0.zip"
+    url "https://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/3.1.0/commandbox-bin-3.1.0.zip"
     sha256 "37f2b7103d008daafa9f3ee279e701dcf9664fdcbde1e4da10fc85a8dd956819"
   end
 
