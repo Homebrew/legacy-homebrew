fcb2ada0306df790b196fd7ea52f7022f945432e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-02-28 22:07:07 +0000

    bde: add 3.0.0.0 bottle.

--- Library/Formula/bde.rb
+++ Library/Formula/bde.rb
@@ -4,6 +4,13 @@ class Bde < Formula
   url "https://github.com/bloomberg/bde/archive/BDE_3.0.0.0.tar.gz"
   sha256 "c6f295947c1af5f0d4e728e4d6801c4b29bb35a742faebc058f86b36722e8cdd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "40fa82df85e35baec799c061a67719d7ada109d150f7eb9043761c4a2e40b0a2" => :el_capitan
+    sha256 "3a95b87b049ba7aeb649a85a1948d80ba554d22603d0e336480f1b8787f75f4c" => :yosemite
+    sha256 "0a4219cc1a605d20000b22d6a8c1977216b60d99278a8b92553ac30f14b6c595" => :mavericks
+  end
+
   resource "bde-tools" do
     url "https://github.com/bloomberg/bde-tools/archive/v1.0.tar.gz"
     sha256 "9b3936fecef23f8c072e62208d2068decfd13d144b771125afc9e0fb9ad16d30"
