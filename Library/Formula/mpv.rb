08d9100a184aa6f52bd605a704f89680bae5e132
Alexis Nootens <me@axn.io>
2016-03-01 00:25:03 +0100

    mpv 0.16.0
    https://github.com/mpv-player/mpv/releases/tag/v0.16.0

--- Library/Formula/mpv.rb
+++ Library/Formula/mpv.rb
@@ -1,9 +1,8 @@
 class Mpv < Formula
   desc "Free, open source, and cross-platform media player"
   homepage "https://mpv.io"
-  url "https://github.com/mpv-player/mpv/archive/v0.15.0.tar.gz"
-  sha256 "7d31217ba8572f364fcea2955733f821374ae6d8c6d8f22f8bc63c44c0400bdc"
-  revision 1
+  url "https://github.com/mpv-player/mpv/archive/v0.16.0.tar.gz"
+  sha256 "fc3619de0ede16fbb023ac72589090e8e77fd9d9e03a81adc728105d50ef38ba"
   head "https://github.com/mpv-player/mpv.git"
 
   bottle do
