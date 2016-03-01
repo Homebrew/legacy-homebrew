c04d4b4222b01c136ab60714cccf4bd0756b543b
Hayden Bleasel <haydenbleasel@gmail.com>
2016-03-01 20:50:35 +1100

    pdf2htmlex 0.14.6
    Closes #49676.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/pdf2htmlex.rb
+++ Library/Formula/pdf2htmlex.rb
@@ -1,9 +1,8 @@
 class Pdf2htmlex < Formula
   desc "PDF to HTML converter"
   homepage "https://coolwanglu.github.io/pdf2htmlEX/"
-  url "https://github.com/coolwanglu/pdf2htmlEX/archive/v0.13.6.tar.gz"
-  sha256 "fc133a5791bfd76a4425af16c6a6a2460f672501b490cbda558213cb2b03d5d7"
-  revision 7
+  url "https://github.com/coolwanglu/pdf2htmlEX/archive/v0.14.6.tar.gz"
+  sha256 "320ac2e1c2ea4a2972970f52809d90073ee00a6c42ef6d9833fb48436222f0e5"
 
   head "https://github.com/coolwanglu/pdf2htmlEX.git"
 
@@ -43,11 +42,6 @@ class Pdf2htmlex < Formula
     cause "Compiling cvexportdlg.c fails with error: initializer element is not constant"
   end
 
-  # Fix a compilation failure with poppler 0.31.0+
-  # Upstream is aware of the issue and suggested this patch:
-  # https://github.com/coolwanglu/pdf2htmlEX/commit/d4fc82b#commitcomment-12239022
-  patch :DATA
-
   def install
     resource("fontforge").stage do
       args = %W[
@@ -84,18 +78,3 @@ class Pdf2htmlex < Formula
     system "#{bin}/pdf2htmlEX", test_fixtures("test.pdf")
   end
 end
-
-__END__
-diff --git a/3rdparty/poppler/git/CairoFontEngine.cc b/3rdparty/poppler/git/CairoFontEngine.cc
-index 229a86c..7cc448b 100644
---- a/3rdparty/poppler/git/CairoFontEngine.cc
-+++ b/3rdparty/poppler/git/CairoFontEngine.cc
-@@ -421,7 +421,7 @@ CairoFreeTypeFont *CairoFreeTypeFont::create(GfxFont *gfxFont, XRef *xref,
-   ref = *gfxFont->getID();
-   fontType = gfxFont->getType();
-
--  if (!(fontLoc = gfxFont->locateFont(xref, gFalse))) {
-+  if (!(fontLoc = gfxFont->locateFont(xref, nullptr))) {
-     error(errSyntaxError, -1, "Couldn't find a font for '{0:s}'",
-	gfxFont->getName() ? gfxFont->getName()->getCString()
-	                       : "(unnamed)");
