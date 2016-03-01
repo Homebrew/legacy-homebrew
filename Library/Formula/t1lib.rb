8f9c1d0b67554a863a6259e5c1fd3084e23f338a
Viktor Szakats <vszakats@users.noreply.github.com>
2016-03-01 02:36:00 +0100

    t1lib: use secure url and add cleartext mirror
    While I can't locally reproduce it, the CI is
    having a hard time with the secure URLs, hence
    the last resort FTP fallback.

--- Library/Formula/t1lib.rb
+++ Library/Formula/t1lib.rb
@@ -1,7 +1,8 @@
 class T1lib < Formula
   desc "C library to generate/rasterize bitmaps from Type 1 fonts"
   homepage "http://www.t1lib.org/"
-  url "http://www.ibiblio.org/pub/Linux/libs/graphics/t1lib-5.1.2.tar.gz"
+  url "https://www.ibiblio.org/pub/linux/libs/graphics/t1lib-5.1.2.tar.gz"
+  mirror "ftp://ftp.ibiblio.org/pub/linux/libs/graphics/t1lib-5.1.2.tar.gz"
   sha256 "821328b5054f7890a0d0cd2f52825270705df3641dbd476d58d17e56ed957b59"
 
   bottle do
