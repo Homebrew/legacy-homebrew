93cdc25b256af2bab65fe0f957fb52127770f1d2
m1a0yu3 <m1a0yu3@users.noreply.github.com>
2016-03-01 09:42:19 +0800

    modify the checksums to lower case
    modify the checksums to lower case

--- Library/Formula/mfcuk.rb
+++ Library/Formula/mfcuk.rb
@@ -2,7 +2,7 @@ class Mfcuk < Formula
   desc "MiFare Classic Universal toolKit"
   homepage "https://github.com/nfc-tools/mfcuk"
   url "https://github.com/nfc-tools/mfcuk/archive/mfcuk-0.3.8.tar.gz"
-  sha256 "C7091D1A16B132E1A4917EBC705065B60F3D1A0B449A776411BA39612A62EE89"
+  sha256 "c7091d1a16b132e1a4917ebc705065b60f3d1a0b449a776411ba39612a62ee89"
 
   depends_on "pkg-config" => :build
   depends_on "libnfc"
