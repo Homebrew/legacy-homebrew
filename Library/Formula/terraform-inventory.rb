c0591adb304736cb381a73f41c913f036590dbb2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-02 20:35:27 +0000

    terraform-inventory: add 0.6 bottle.

--- Library/Formula/terraform-inventory.rb
+++ Library/Formula/terraform-inventory.rb
@@ -7,6 +7,13 @@ class TerraformInventory < Formula
   sha256 "af09a2606db728fd046e0ce5a6960386e5f766f8d187eea15220764fd669187e"
   head "https://github.com/adammck/terraform-inventory.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4c2fb6c739555d94b6bf8518e166847dfaf12b826ac170b143bb37082f6035a0" => :el_capitan
+    sha256 "279085b3061be61b2b35a49a609f4e88d46d90bab14718548851e40ebe8da7a0" => :yosemite
+    sha256 "db95da0f598a7837337f137cae7284de1a9e8168105ebc93c1f66e74016a6b99" => :mavericks
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/stretchr/testify" do
