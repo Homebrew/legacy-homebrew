88d624bbf2bc62bbf8ea13b3418f6a6921fca5ba
Kyle Robson <kyledr@gmail.com>
2016-03-02 12:43:47 -0800

    irssi 0.8.18
    Closes #49734.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/irssi.rb
+++ Library/Formula/irssi.rb
@@ -1,10 +1,16 @@
 class Irssi < Formula
   desc "Modular IRC client"
   homepage "https://irssi.org/"
-  url "https://github.com/irssi-import/irssi/releases/download/0.8.17/irssi-0.8.17.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/i/irssi/irssi_0.8.17.orig.tar.gz"
-  sha256 "0ae01f76797fb6d6b8e0f2268b39c7afb90ac62658ec754c82acfc344b8203e9"
-  revision 2
+  url "https://github.com/irssi/irssi/releases/download/0.8.18/irssi-0.8.18.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/i/irssi/irssi_0.8.18.orig.tar.gz"
+  sha256 "30043784815bb864b1bb66a82c1e659c325be0a18ddcf76fc101812e36c39c20"
+
+  bottle do
+    revision 3
+    sha256 "3c70f0936f14cf71dcefdbd635e825ef0941a9c44e6693ae495cc68382c751bc" => :el_capitan
+    sha256 "b55f35cd938692dd577d553c3a6b48f89b5923125a96fa23697ab162d2a7cb61" => :yosemite
+    sha256 "1b30b5687ad71c6cb74ba6264673bc6244c053c30773a145f3cb008500989c9f" => :mavericks
+  end
 
   head do
     url "https://github.com/irssi/irssi.git"
@@ -12,13 +18,6 @@ class Irssi < Formula
     depends_on "autoconf" => :build
     depends_on "libtool" => :build
     depends_on "lynx" => :build
-  end
-
-  bottle do
-    revision 3
-    sha256 "3c70f0936f14cf71dcefdbd635e825ef0941a9c44e6693ae495cc68382c751bc" => :el_capitan
-    sha256 "b55f35cd938692dd577d553c3a6b48f89b5923125a96fa23697ab162d2a7cb61" => :yosemite
-    sha256 "1b30b5687ad71c6cb74ba6264673bc6244c053c30773a145f3cb008500989c9f" => :mavericks
   end
 
   option "with-dante", "Build with SOCKS support"
@@ -30,16 +29,6 @@ class Irssi < Formula
   depends_on "dante" => :optional
 
   def install
-    if build.stable?
-      # Make paths in man page Homebrew-specific
-      # (https://github.com/irssi/irssi/issues/251); can be removed in
-      # next stable release
-      inreplace "docs/irssi.1" do |s|
-        s.gsub! "/usr/share", "#{HOMEBREW_PREFIX}/share"
-        s.gsub! "/etc/irssi.conf", "#{HOMEBREW_PREFIX}/etc/irssi.conf"
-      end
-    end
-
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
