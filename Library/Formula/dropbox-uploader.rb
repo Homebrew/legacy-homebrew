5355149a69aa4d1b29037041cd9587334054759c
Andrew Titmuss <andrew@coffeetocode.me>
2016-03-02 18:00:57 +1100

    removed dep and made real test
    created a real test that creates a dummy config, then removes it
    using the script's removal tool

--- Library/Formula/dropbox-uploader.rb
+++ Library/Formula/dropbox-uploader.rb
@@ -4,13 +4,12 @@ class DropboxUploader < Formula
   url "https://github.com/andreafabrizi/Dropbox-Uploader/archive/0.16.tar.gz"
   sha256 "738aebdb57f75e9033cb9bb319b14fe1c3b471f951180acb48631e9810ebf1c3"
 
-  depends_on "curl"
-
   def install
     bin.install "dropbox_uploader.sh"
   end
 
   test do
-    system "true"
+    (testpath/".dropbox_uploader").write("APPKEY=a\nAPPSECRET=b\nACCESS_LEVEL=sandbox\nOAUTH_ACCESS_TOKEN=c\nOAUTH_ACCESS_TOKEN_SECRET=d")
+    system("yes | dropbox_uploader.sh unlink")
   end
 end
