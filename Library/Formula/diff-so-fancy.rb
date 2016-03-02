98e5c3e80a23e0d9258022512a77a5d50389a6c9
Steve Mao <maochenyan@gmail.com>
2016-03-02 19:49:14 +1100

    diff-so-fancy 0.5.2

--- Library/Formula/diff-so-fancy.rb
+++ Library/Formula/diff-so-fancy.rb
@@ -1,8 +1,8 @@
 class DiffSoFancy < Formula
   desc "Good-lookin' diffs with diff-highlight and more"
   homepage "https://github.com/so-fancy/diff-so-fancy"
-  url "https://github.com/so-fancy/diff-so-fancy/archive/v0.5.1.tar.gz"
-  sha256 "63c264f3a2989446713bd9ad6669fb5d71825c62c5716c2e27dc3803d5b8c7a0"
+  url "https://github.com/so-fancy/diff-so-fancy/archive/v0.5.2.tar.gz"
+  sha256 "ef91042af8197396be037f0bd8b5a362b57c82041c19179d6e0ac8a687e7f8b5"
 
   bottle :unneeded
 
