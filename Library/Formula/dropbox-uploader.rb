0f035f54685d20e9287f0bf67d68d5a84b0003df
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-03 13:26:55 +0000

    dropbox-uploader: bottle unneeded

--- Library/Formula/dropbox-uploader.rb
+++ Library/Formula/dropbox-uploader.rb
@@ -4,6 +4,8 @@ class DropboxUploader < Formula
   url "https://github.com/andreafabrizi/Dropbox-Uploader/archive/0.16.tar.gz"
   sha256 "738aebdb57f75e9033cb9bb319b14fe1c3b471f951180acb48631e9810ebf1c3"
 
+  bottle :unneeded
+
   def install
     bin.install "dropbox_uploader.sh"
   end
