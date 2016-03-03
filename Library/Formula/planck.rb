afcf1bbdf534af5c3c56828fc2e822717fd339c9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-03 03:49:28 +0000

    planck: update 1.10 bottle.

--- Library/Formula/planck.rb
+++ Library/Formula/planck.rb
@@ -7,9 +7,9 @@ class Planck < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "25251f8762f2d66e39551c6f3775bfb9ed48ee9fb23fb9a3b81c9e61039a3a62" => :el_capitan
-    sha256 "05a1e4834ecffa3f2428e5930eaca679dbcebd62adb56d71604a786191c2b4d8" => :yosemite
-    sha256 "ed75d94350e555ded5f90132d15430dd85aaf145d65b4d2b0765af85a6c67077" => :mavericks
+    sha256 "a3621f8a980bcc9cc34ed0567418dbdf53d2a64af42337f873fa796440f7dca6" => :el_capitan
+    sha256 "0e928b9a6313949a5f2f44e83624a1eab0fe83907159147bf4e1d6344bbd683f" => :yosemite
+    sha256 "8c97a1b4cb5b9dc5027daa1f0f296669b068db1314e2d655f4f53d957a0e8b8e" => :mavericks
   end
 
   depends_on "leiningen" => :build
