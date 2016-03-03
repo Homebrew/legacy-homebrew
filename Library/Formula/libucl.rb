a167cd75323dd33f4abd79eaa42144951ee33397
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-03 19:20:40 +0000

    libucl: add 0.8.0 bottle.

--- Library/Formula/libucl.rb
+++ Library/Formula/libucl.rb
@@ -4,6 +4,13 @@ class Libucl < Formula
   url "https://github.com/vstakhov/libucl/archive/0.8.0.tar.gz"
   sha256 "af361cd1f0b7b66c228a1c04a662ccaa9ee8af79842046c04446d915db349ee1"
 
+  bottle do
+    cellar :any
+    sha256 "2efba6c1eb7a6934e798bab1c4727d589febf61252e8db4203644c15386ca0a6" => :el_capitan
+    sha256 "a851aabab914417b6233313d100ca28771f471b61879038e1444e554d62b593a" => :yosemite
+    sha256 "726166a182e3d7eef29d36669fd1dde7536ec4c1108ec39f43c93519ea50355c" => :mavericks
+  end
+
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
