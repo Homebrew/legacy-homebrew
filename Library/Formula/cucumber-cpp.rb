11f2cbe5f80bd69f348b8353c3b0d8ed8d973b1e
Martin Delille <martin@phonations.com>
2016-03-03 19:49:49 +0100

    add *std_cmake_args

--- Library/Formula/cucumber-cpp.rb
+++ Library/Formula/cucumber-cpp.rb
@@ -8,7 +8,8 @@ class CucumberCpp < Formula
   depends_on "boost"
 
   def install
-    system "cmake", "-DCUKE_DISABLE_GTEST=on",
+    system "cmake", *std_cmake_args,
+                    "-DCUKE_DISABLE_GTEST=on",
                     "-DCUKE_DISABLE_CPPSPEC=on",
                     "-DCUKE_DISABLE_FUNCTIONAL=on",
                     "-DCUKE_DISABLE_BOOST_TEST=on",
