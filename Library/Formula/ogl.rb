ba6664fa0d72016d6d45dcaf907e019225690540
chris <beck.ct@gmail.com>
2016-03-04 10:08:52 -0600

    move test code to git repository (try to work around problems)

--- Library/Formula/ogl.rb
+++ Library/Formula/ogl.rb
@@ -1,17 +1,13 @@
 class Ogl < Formula
   desc "this is a test"
   homepage "http://www.khronos.org"
-  url "http://www.khronos.org"
-  version "0"
-  sha256 "955fa5550e7786f1d811fbcaa30c782313d08251600799843c91bc2764212410"
+  url "https://github.com/cbeck88/test_gl/archive/v0.tar.gz"
+  sha256 "6ff237be51dfa5a652e1abd349c80bde4a0c7d6b76077f498933283a963281f4"
+
+  head "https://github.com/cbeck88/test_gl.git"
 
   def install
-    (buildpath/"test.cpp").write <<-EOS.undent
-    #include <OpenGL/OpenGL.h>
-    int main() { return 0; }
-    EOS
-
-    system ENV.cc, "test.cpp", "-o", "test"
+    system "./build.sh"
     bin.install "test"
   end
 
