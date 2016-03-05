51e9663daa457780a78f97cdc8fcd9a8bf42df92
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-05 18:00:19 +0000

    exomizer: add 2.0.9 bottle.

--- Library/Formula/exomizer.rb
+++ Library/Formula/exomizer.rb
@@ -5,6 +5,13 @@ class Exomizer < Formula
   version "2.0.9"
   sha256 "d2a95d4d168e4007fc396295e2f30a21b58f4648c28d1aadf84e7d497c5c7a34"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7fc0744775aff849b63640a9ac89d63309eebf04dc544b2840d219f8331629c5" => :el_capitan
+    sha256 "5516edd49987f155735a83ef3a87fd01fb685d30067dcd770d07b07a4ddbbf19" => :yosemite
+    sha256 "8fdf034caf568b57fa6e8b2f8adabc47bdd845a300ab3901c0bb6fc9f6556185" => :mavericks
+  end
+
   def install
     cd "src" do
       system "make"
