a44ac76a5b9f53723a1a7b062dc7fb72dfc17256
Paul Merrill <pdm@pdm.me>
2016-03-07 00:24:26 -0800

    launch_socket_server: Remove plist
    It didn't contain an actual binary to run.

--- Library/Formula/launch_socket_server.rb
+++ Library/Formula/launch_socket_server.rb
@@ -18,48 +18,4 @@ class LaunchSocketServer < Formula
   test do
     assert_match /usage/, shell_output("#{sbin}/launch_socket_server")
   end
-
-  def plist; <<-EOS.undent
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_sbin}/launch_socket_server</string>
-          <string>/usr/local/bin/myserver</string>
-          <string>argument1</string>
-          <string>argument2</string>
-        </array>
-        <key>Sockets</key>
-        <dict>
-          <key>Socket</key>
-          <dict>
-            <key>SockNodeName</key>
-            <string>0.0.0.0</string>
-            <key>SockServiceName</key>
-            <string>80</string>
-          </dict>
-        </dict>
-        <key>EnvironmentVariables</key>
-        <dict>
-          <!--<key>LAUNCH_PROGRAM_SOCKET_PATH</key>-->
-          <!--<string>/tmp/myserver.sock</string>-->
-          <!--<key>LAUNCH_PROGRAM_TCP_ADDRESS</key>-->
-          <!--<string>127.0.0.1:8000</string>-->
-        </dict>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/launch_socket_server.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/launch_socket_server.log</string>
-      </dict>
-    </plist>
-    EOS
-  end
 end
