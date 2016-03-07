8b13b8c8bf618afeaffa695d54db4fa693f2c41b
Lukas Eipert <git@leipert.io>
2016-03-07 13:53:37 +0100

    dlite 1.1.4
    Closes #49850.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/dlite.rb
+++ Library/Formula/dlite.rb
@@ -1,8 +1,8 @@
 class Dlite < Formula
   desc "Provides a way to use docker on OS X without docker-machine"
   homepage "https://github.com/nlf/dlite"
-  url "https://github.com/nlf/dlite/archive/1.1.3.tar.gz"
-  sha256 "a87c4d7b7cd666082dc0fc44db2ca2b03efb4ee66676450c4844e7de0420d8e6"
+  url "https://github.com/nlf/dlite/archive/1.1.4.tar.gz"
+  sha256 "9fc39e4b94e141390a1f52863d4d4417f3f3c7bfd7e9622d5b8863c70f6c2c72"
 
   # DLite depends on the Hypervisor framework which only works on
   # OS X versions 10.10 (Yosemite) or newer
