9af7944f93a5bda97fdb28305b99a631fbefcb90
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-07 10:23:55 +0000

    prometheus: add 0.17.0 bottle.

--- Library/Formula/prometheus.rb
+++ Library/Formula/prometheus.rb
@@ -5,6 +5,13 @@ class Prometheus < Formula
     :tag => "0.17.0",
     :revision => "e11fab35d76d19c5c49b7d85e28275f894d3ada4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a5bd980c21a17cc3144c6f926630a043524473c597187a819023fe17bb1151cd" => :el_capitan
+    sha256 "de08265b7154e3b4036b2dc3a02902f6fe2ad6ae570e195bd1c3852f8658b4ec" => :yosemite
+    sha256 "fc21c6a2a8e36b7154308afa4215616455579fc9b83023627f05920df88d90d0" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
