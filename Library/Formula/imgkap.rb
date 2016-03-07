178fc6cbfea45436cb07799da96ad761a381be5b
Pavel Kalian <pavel@kalian.cz>
2016-03-07 17:23:06 -0300

    Update to upstream containing test data and change the test to a complete GIF->KAP conversion.

--- Library/Formula/imgkap.rb
+++ Library/Formula/imgkap.rb
@@ -1,17 +1,19 @@
 class Imgkap < Formula
   desc "Tool to create nautical charts in KAP format."
   homepage "https://github.com/nohal/imgkap"
-  url "https://github.com/nohal/imgkap/archive/v1.11.tar.gz"
-  sha256 "5edb67582ee4e7f294bbd6cd71d0027a92b8ab1b81b9d2570d08606eaba206e9"
+  url "https://github.com/nohal/imgkap/archive/v1.13.tar.gz"
+  sha256 "54f01060057c85467a1f198abc397a9dbc96107c8d0084e94b29552bdda44fb6"
 
   depends_on "freeimage"
 
   def install
     system "make", "imgkap"
     bin.install "imgkap"
+    pkgshare.install "examples"
   end
 
   test do
-    system "imgkap || true"
+    system "#{bin}/imgkap", "#{pkgshare}/examples/PWAE10.gif", 45, -95, "139;250", 25, -45, "1532;938", "natl.kap"
+    assert File.exist?("natl.kap")
   end
 end
