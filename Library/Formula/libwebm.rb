1f03d1be834eda56e8d063822953fad024e0c450
Francisco Facioni <fran6co@gmail.com>
2016-03-09 13:54:13 -0300

    libwebm 1.0.0.27

--- Library/Formula/libwebm.rb
+++ Library/Formula/libwebm.rb
@@ -1,8 +1,8 @@
 class Libwebm < Formula
   desc "WebM container"
   homepage "https://www.webmproject.org/code/"
-  url "https://github.com/webmproject/libwebm/archive/libwebm-1.0.0.26.tar.gz"
-  sha256 "1e4034e03836ac974bb078b11383a10306d63879b350a34fdd575ab44b132222"
+  url "https://github.com/webmproject/libwebm/archive/libwebm-1.0.0.27.tar.gz"
+  sha256 "1332f43742aeae215fd8df1be6e363e753b17abb37447190e789299fe3edec77"
 
   bottle do
     cellar :any
@@ -11,13 +11,17 @@ class Libwebm < Formula
     sha256 "3e2c2a2fd1854c158c33bd6667a2e0ae17838eb88a0f839ee48316f59d105c7e" => :mountain_lion
   end
 
-  def install
-    system "make"
+  depends_on "cmake" => :build
 
-    lib.install "libwebm.a"
+  def install
+    mkdir "macbuild" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      lib.install "libwebm.a"
+      bin.install %w[sample sample_muxer vttdemux webm2pes]
+    end
     include.install Dir.glob("mkv*.hpp")
     include.install Dir.glob("vtt*.h")
-    bin.install %w[dumpvtt samplemuxer vttdemux]
   end
 
   test do
