cdcf3943d392cdb1a9850470c1eb26c01d491874
Viktor Szakats <vszakats@users.noreply.github.com>
2016-03-10 00:12:59 +0100

    kstart: use secure homepage
    Closes #49932.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Library/Formula/kstart.rb
+++ Library/Formula/kstart.rb
@@ -1,6 +1,6 @@
 class Kstart < Formula
   desc "Modified version of kinit that can use keytabs to authenticate"
-  homepage "http://www.eyrie.org/~eagle/software/kstart/"
+  homepage "https://www.eyrie.org/~eagle/software/kstart/"
   url "https://archives.eyrie.org/software/kerberos/kstart-4.2.tar.gz"
   sha256 "2698bc1ab2fb36d49cc946b0cb864c56dd3a2f9ef596bfff59592e13d35315cd"
 
