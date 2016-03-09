77a3dfdac5e73201c82c01411ae7d11161cefd46
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-09 22:34:59 +0000

    asciinema2gif: add 0.2 bottle.

--- Library/Formula/asciinema2gif.rb
+++ Library/Formula/asciinema2gif.rb
@@ -5,6 +5,13 @@ class Asciinema2gif < Formula
   sha256 "fa5834aaec162b992768bed854d69eecff0862c733d4be0334dbc3b9fe86481c"
   head "https://github.com/tav/asciinema2gif.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "099cfce8b0f1523419e202d6a3c2ae2be5834eb973e1bcafa203acf2f47a8d3c" => :el_capitan
+    sha256 "3ae4862511bf5754fefe5a70ae39c5c2658902a2ea0706a685cfe2a308848faa" => :yosemite
+    sha256 "5d827a76fda0a7cd372cc4a64d39514a9ae881fc845307dc9be9d41b12a6a89d" => :mavericks
+  end
+
   depends_on "gifsicle"
   depends_on "imagemagick"
   depends_on "phantomjs"
