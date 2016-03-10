9f73d8f9af0701fa03f77ae5c555ce4662a50fb5
Chayoung You <yousbe@gmail.com>
2016-03-10 23:59:00 +0900

    activemq-cpp 3.9.2
    Closes #49963.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Library/Formula/activemq-cpp.rb
+++ Library/Formula/activemq-cpp.rb
@@ -1,8 +1,8 @@
 class ActivemqCpp < Formula
   desc "C++ API for message brokers such as Apache ActiveMQ"
   homepage "https://activemq.apache.org/cms/index.html"
-  url "https://www.apache.org/dyn/closer.cgi?path=activemq/activemq-cpp/3.8.4/activemq-cpp-library-3.8.4-src.tar.bz2"
-  sha256 "9fba18d57f7512ae4f17008d7745d1b4c957b858b585860deadbf9208cb733e3"
+  url "https://www.apache.org/dyn/closer.cgi?path=activemq/activemq-cpp/3.9.2/activemq-cpp-library-3.9.2-src.tar.bz2"
+  sha256 "b3f58fc938cf4fae37192ca317d3d9e4c77caeeeff804ec6f27642201d923bb8"
 
   bottle do
     cellar :any
