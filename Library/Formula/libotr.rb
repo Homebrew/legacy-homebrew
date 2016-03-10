850ff3d83e4c9049873a07794adff576afa294d1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-10 05:14:44 +0000

    libotr: update 4.1.1 bottle.

--- Library/Formula/libotr.rb
+++ Library/Formula/libotr.rb
@@ -6,10 +6,9 @@ class Libotr < Formula
 
   bottle do
     cellar :any
-    sha256 "6a800d4cc2c2239954d45769ee424431f72896a1f1cb41e58dbf84d3273545b8" => :el_capitan
-    sha256 "1f494d03865663bf360ae5e652a5bf840f18372a8767b209fc7ebf4f672aabaf" => :yosemite
-    sha256 "b5275f230e3524aa4ac19751de474674be9b86f51420ea3725f115e64f703d8b" => :mavericks
-    sha256 "8c41ed8fab1897946f9105ba18223d09867c4b383c9889c3ca84c7752a19a71e" => :mountain_lion
+    sha256 "43d7a166cd12b611e7bf15dfa3865d18e573a81a218e2aeb0061d51203ecde39" => :el_capitan
+    sha256 "b3f215fd3952f7a97af36500365c3c017f23de107162f4c76b0e48355bd2a358" => :yosemite
+    sha256 "12338de29acd18bb5d7a90552e33b1a353ae7de3f10ab0316dfd6bda379d919b" => :mavericks
   end
 
   depends_on "libgcrypt"
