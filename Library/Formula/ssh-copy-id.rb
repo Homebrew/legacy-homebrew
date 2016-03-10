a185011173d14e03cde8a2eae6d8dcc21bf77e73
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-10 23:48:41 +0000

    ssh-copy-id 7.2p2
    Closes #49999.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ssh-copy-id.rb
+++ Library/Formula/ssh-copy-id.rb
@@ -1,10 +1,10 @@
 class SshCopyId < Formula
   desc "Add a public key to a remote machine's authorized_keys file"
   homepage "http://www.openssh.com/"
-  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.1p1.tar.gz"
-  mirror "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.1p1.tar.gz"
-  version "7.1p1"
-  sha256 "fc0a6d2d1d063d5c66dffd952493d0cda256cad204f681de0f84ef85b2ad8428"
+  url "http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.2p2.tar.gz"
+  mirror "https://www.mirrorservice.org/pub/OpenBSD/OpenSSH/portable/openssh-7.2p2.tar.gz"
+  version "7.2p2"
+  sha256 "a72781d1a043876a224ff1b0032daa4094d87565a68528759c1c2cab5482548c"
 
   head "https://github.com/openssh/openssh-portable.git"
 
@@ -17,6 +17,6 @@ class SshCopyId < Formula
 
   test do
     output = shell_output("#{bin}/ssh-copy-id -h 2>&1", 1)
-    assert_match /identity_file/, output
+    assert_match "identity_file", output
   end
 end
