099740269f9be1f8f04f3837b7e84352c169a00a
Ľubomír Carik <Lubomir.Carik@anritsu.com>
2016-03-10 22:00:48 +0100

    pkg-config 0.29.1

--- Library/Formula/pkg-config.rb
+++ Library/Formula/pkg-config.rb
@@ -1,9 +1,9 @@
 class PkgConfig < Formula
   desc "Manage compile and link flags for libraries"
   homepage "https://freedesktop.org/wiki/Software/pkg-config/"
-  url "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.tar.gz"
-  mirror "https://fossies.org/linux/misc/pkg-config-0.29.tar.gz"
-  sha256 "c8507705d2a10c67f385d66ca2aae31e81770cc0734b4191eb8c489e864a006b"
+  url "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.1.tar.gz"
+  mirror "https://fossies.org/linux/misc/pkg-config-0.29.1.tar.gz"
+  sha256 "beb43c9e064555469bd4390dcfd8030b1536e0aa103f08d7abf7ae8cac0cb001"
 
   bottle do
     sha256 "088515dbb9db3977859d895b20eff9f486120641280d3b3a51fefe751197ece9" => :el_capitan
