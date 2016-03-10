db0feebc402d252e13aaaeb9aa5f218ebe2bc210
Chayoung You <yousbe@gmail.com>
2016-03-10 09:39:23 +0900

    abcmidi 2016.03.03
    Closes #49936.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/abcmidi.rb
+++ Library/Formula/abcmidi.rb
@@ -1,9 +1,9 @@
 class Abcmidi < Formula
   desc "Converts abc music notation files to MIDI files"
   homepage "http://www.ifdo.ca/~seymour/runabc/top.html"
-  url "http://ifdo.ca/~seymour/runabc/abcMIDI-2015-08-06.zip"
-  version "2015-08-06"
-  sha256 "6ada64028344b5d778714bca02e41ed98a97c656aa45ea0e4a21de926502304e"
+  url "http://www.ifdo.ca/~seymour/runabc/abcMIDI-2016.03.03.zip"
+  version "2016-03-03"
+  sha256 "a9070dbb49758474805252d1a3e837aef8dc1266f6415f8eccc7df118af3dc1e"
 
   bottle do
     cellar :any_skip_relocation
@@ -22,9 +22,6 @@ class Abcmidi < Formula
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
-    # The Makefile is broken when using --prefix (value is added to path twice).
-    # abcmidi author is notified (2012-06-20). In the meantime, here's a fix.
-    inreplace "Makefile", "$(DESTDIR)${prefix}", "$(DESTDIR)"
     system "make", "install"
   end
 
