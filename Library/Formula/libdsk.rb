4309bd86636b9dcf0e9217769e8d3fac4af9d4ec
ilovezfs <ilovezfs@icloud.com>
2016-03-10 08:48:02 -0800

    libdsk 1.4.0
    Closes #49976.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Library/Formula/libdsk.rb
+++ Library/Formula/libdsk.rb
@@ -1,8 +1,8 @@
 class Libdsk < Formula
   desc "Library for accessing discs and disc image files"
   homepage "http://www.seasip.info/Unix/LibDsk/"
-  url "http://www.seasip.info/Unix/LibDsk/libdsk-1.3.8.tar.gz"
-  sha256 "1472cb30534e3a1b3ac0c2e7e4a00a9c247955af5d407cb95c25391fb61e45d4"
+  url "http://www.seasip.info/Unix/LibDsk/libdsk-1.4.0.tar.gz"
+  sha256 "645612159ad9a990183f8f80876e686e185d6b6aa7f2ddd623da22f314563f64"
 
   bottle do
     sha256 "5b826726a3146916ec472d350c00656e878b0420d271b4c623d868fd91809637" => :yosemite
@@ -13,7 +13,13 @@ class Libdsk < Formula
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+    system "make"
+    system "make", "check"
     system "make", "install"
-    (share+name+"doc").install Dir["doc/*.{html,txt,pdf}"]
+    doc.install Dir["doc/*.{html,pdf,sample,txt}"]
+  end
+
+  test do
+    assert_equal "#{name} version #{version}\n", shell_output(bin/"dskutil --version")
   end
 end
