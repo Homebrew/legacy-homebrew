03491a9a79c63f5c428f119b1a0b974468253794
Jason Tedor <jason@tedor.me>
2016-03-11 12:14:36 -0500

    kibana: style fixes
    Closes #50027.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/kibana.rb
+++ Library/Formula/kibana.rb
@@ -23,23 +23,23 @@ class Kibana < Formula
     end
 
     # do not download binary installs of Node.js
-    inreplace buildpath/"tasks/build/index.js", %r{('_build:downloadNodeBuilds:\w+',)}, "// \\1"
+    inreplace buildpath/"tasks/build/index.js", /('_build:downloadNodeBuilds:\w+',)/, "// \\1"
 
     # do not build packages for other platforms
     platforms = Set.new(["darwin-x64", "linux-x64", "linux-x86", "windows"])
     if OS.mac? && Hardware::CPU.is_64_bit?
       platform = "darwin-x64"
     elsif OS.linux?
-      platform = if Hardware::CPU.is_64_bit? then "linux-x64" else "linux-x86" end
+      platform = Hardware::CPU.is_64_bit? ? "linux-x64" : "linux-x86"
     else
       raise "Installing Kibana via Homebrew is only supported on Darwin x86_64, Linux i386, Linux i686, and Linux x86_64"
     end
     platforms.delete(platform)
     sub = platforms.to_a.join("|")
-    inreplace buildpath/"tasks/config/platforms.js", %r{('(#{sub})',?(?!;))}, "// \\1"
+    inreplace buildpath/"tasks/config/platforms.js", /('(#{sub})',?(?!;))/, "// \\1"
 
     # do not build zip package
-    inreplace buildpath/"tasks/build/archives.js", %r{(await exec\('zip'.*)}, "// \\1"
+    inreplace buildpath/"tasks/build/archives.js", /(await exec\('zip'.*)/, "// \\1"
 
     ENV.prepend_path "PATH", prefix/"libexec/node/bin"
     system "npm", "install"
@@ -54,7 +54,7 @@ class Kibana < Formula
     inreplace "#{bin}/kibana", %r{/node/bin/node}, "/libexec/node/bin/node"
 
     cd prefix do
-      inreplace "config/kibana.yml", %{/var/run/kibana.pid}, var/"run/kibana.pid"
+      inreplace "config/kibana.yml", %(/var/run/kibana.pid), var/"run/kibana.pid"
       (etc/"kibana").install Dir["config/*"]
       rm_rf "config"
     end
