5a442c908c0b455fa64339e3f71f5ba2d8d74b14
Tako Schotanus <tako@codejive.org>
2016-03-11 14:46:02 +0100

    ceylon 1.2.2
    Update Ceylon formula to 1.2.2
    
    Closes #50018.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/ceylon.rb
+++ Library/Formula/ceylon.rb
@@ -1,8 +1,8 @@
 class Ceylon < Formula
   desc "Programming language for writing large programs in teams"
   homepage "http://ceylon-lang.org/"
-  url "http://ceylon-lang.org/download/dist/1_2_1"
-  sha256 "08379f76e5ce2db01f015efbfde05ecc7c8a8244441459eb7c4ea830eabaa1bb"
+  url "http://ceylon-lang.org/download/dist/1_2_2"
+  sha256 "68a7d56b2d3eca83f8832ef1a2e0e58124a71c3c9fc0cda7c377e4882b4feedb"
 
   bottle :unneeded
 
@@ -26,7 +26,7 @@ class Ceylon < Formula
     cd "#{libexec}/samples/helloworld" do
       system "#{bin}/ceylon", "compile", "--out", "#{testpath}/modules", "--encoding", "UTF-8", "com.example.helloworld"
       system "#{bin}/ceylon", "doc", "--out", "#{testpath}/modules", "--encoding", "UTF-8", "--non-shared", "com.example.helloworld"
-      system "#{bin}/ceylon", "run", "--rep", "#{testpath}/modules", "com.example.helloworld/1.2.1", "John"
+      system "#{bin}/ceylon", "run", "--rep", "#{testpath}/modules", "com.example.helloworld/1.0", "John"
     end
   end
 end
