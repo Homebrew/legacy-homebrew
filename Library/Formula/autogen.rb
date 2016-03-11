63ca4cf4fe28765158733725bd30e64391671d92
Chayoung You <yousbe@gmail.com>
2016-03-11 12:54:22 +0900

    autogen 5.18.7
    Closes #50006.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/autogen.rb
+++ Library/Formula/autogen.rb
@@ -1,9 +1,9 @@
 class Autogen < Formula
   desc "Automated text file generator"
   homepage "http://autogen.sourceforge.net"
-  url "http://ftpmirror.gnu.org/autogen/rel5.18.4/autogen-5.18.4.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/autogen/rel5.18.4/autogen-5.18.4.tar.xz"
-  sha256 "7fbaff0c25035aee5b96913de2c83d9a5cc973b8dc08d6b7489ecbcfd72eb84b"
+  url "http://ftpmirror.gnu.org/autogen/autogen-5.18.7.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/autogen/autogen-5.18.7.tar.xz"
+  sha256 "a7a580a5e18931cb341b255cec2fee2dfd81bea5ddbf0d8ad722703e19aaa405"
 
   bottle do
     revision 1
