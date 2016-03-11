6c12be4db8434effc805b16bd6dc96f7c804bef2
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-11 00:37:56 +0000

    tesseract: fix HEAD inreplace failure

--- Library/Formula/tesseract.rb
+++ Library/Formula/tesseract.rb
@@ -72,9 +72,9 @@ class Tesseract < Formula
     ENV.cxx11
 
     # Fix broken pkg-config file
-    # Can probably be removed with next version bump
+    # Can be removed with next version bump
     # https://github.com/tesseract-ocr/tesseract/issues/241
-    inreplace "tesseract.pc.in", "@OPENCL_LIB@", "@OPENCL_LDFLAGS@"
+    inreplace "tesseract.pc.in", "@OPENCL_LIB@", "@OPENCL_LDFLAGS@" if build.stable?
 
     system "./autogen.sh" if build.head?
 
