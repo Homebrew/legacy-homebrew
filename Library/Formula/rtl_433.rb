f16f4236ff9364c0875d8b9d7ed18739321c11e5
lmwangi <lmwangi@gmail.com>
2016-03-12 09:11:29 +0300

    Updated formula to use a tagged release.

--- Library/Formula/rtl_433.rb
+++ Library/Formula/rtl_433.rb
@@ -1,13 +1,14 @@
 class Rtl433 < Formula
   desc "Tool to talk to 433Mhz devices using RTL devices"
   homepage "https://github.com/merbanan/rtl_433"
-  url "https://github.com/merbanan/rtl_433.git", :revision => "922a76a81820c68e2e5c55a428e5d22135a8be9a"
+  url "https://github.com/merbanan/rtl_433/archive/0.01.tar.gz"
+  sha256 "10f29c0ea91684f91f42537cf2498b1236411ab2c48893f2d139126ad4e000e5"
+  head "https://github.com/merbanan/rtl_433.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "libtool"  => :build
-
   depends_on "librtlsdr"
 
   def install
@@ -15,5 +16,9 @@ class Rtl433 < Formula
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/rtl_433", "-h"
   end
 end
