5ebd5f6b75913300f2b4205d042e19c2524fa56e
Han Ngo <nntruonghan@gmail.com>
2016-03-12 10:38:27 +0700

    Update test for glod-cli and pump to version 1.0.3

--- Library/Formula/glod-cli.rb
+++ Library/Formula/glod-cli.rb
@@ -1,10 +1,10 @@
 require "language/go"
 
 class GlodCli < Formula
-
   desc "Glod command-line interface tools"
   homepage "https://github.com/dwarvesf/glod-cli"
-  url "https://github.com/dwarvesf/glod-cli/archive/1.0.1.1.tar.gz"
+  url "https://github.com/dwarvesf/glod-cli/archive/1.0.3.2.tar.gz"
+  sha256 "1826e8b5398f10a12d5f315a9f5a670f05ac3e0f6ead7c4edddf621c2260ae6c"
 
   depends_on "go" => :build
   depends_on "godep" => :build
@@ -25,6 +25,11 @@ class GlodCli < Formula
   end
 
   test do
-    system bin/"glod-cli", "help"
+    output = shell_output(bin/"glod-cli --version")
+    assert_match "glod-cli version #{version}\n", output
+
+    system bin/"glod-cli", "http://mp3.zing.vn/bai-hat/Hello-Vietnam-Pham-Quynh-Anh/ZWZ9C8EB.html"
+    sleep 2
+    assert File.exist?("Hello Vietnam.mp3")
   end
 end
