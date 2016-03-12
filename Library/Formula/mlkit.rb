8928c1f9a7e1f80a322f40bcbc8259380ebb5fa4
Martin Elsman <martin.elsman@gmail.com>
2016-03-12 02:23:15 +0100

    new upstream version with fixes

--- Library/Formula/mlkit.rb
+++ Library/Formula/mlkit.rb
@@ -1,22 +1,16 @@
 class Mlkit < Formula
   desc "Compiler for the Standard ML programming language"
   homepage "https://melsman.github.io/mlkit"
-  url "https://github.com/melsman/mlkit/archive/mlkit-4.3.8.tar.gz"
-  sha256 "337ea3d333c694871f05f9c3ecbf1c05d4bcd1a0993e87801507930a1637a5d4"
-
-  bottle do
-    sha256 "32bcf016846a33d8794230567d6b41ab7d703a13eff4568b318073ba2fe61426" => :el_capitan
-  end
+  url "https://github.com/melsman/mlkit/archive/mlkit-4.3.9.tar.gz"
+  sha256 "3c6adbeb9a85f7b3586d0961fd3b170ff31e09fa0ff12889b76b9ceb459059c4"
 
   depends_on "autoconf" => :build
   depends_on "mlton" => :build
-  depends_on :tex
   depends_on "gmp"
 
   def install
     system "./autobuild; true"
     system "./configure", "--prefix=#{prefix}"
-    ENV.deparallelize
     ENV.permit_arch_flags
     system "make", "mlkit"
     system "make", "mlkit_libs"
