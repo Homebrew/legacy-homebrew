e85ee5530af8a88c3720fc0476ee90e525bacf3c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-13 17:34:16 +0000

    jo: add 1.0 bottle.

--- Library/Formula/jo.rb
+++ Library/Formula/jo.rb
@@ -5,6 +5,13 @@ class Jo < Formula
   url "https://github.com/jpmens/jo/releases/download/v1.0/jo-1.0.tar.gz"
   sha256 "d66ec97258d1afad15643fb2d5b5e807153a732ba45c2417adc66669acbde52e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c94c62f3918266cbd688da0b54274226f0fc5d31b311b6be47f523d184f3e503" => :el_capitan
+    sha256 "c46c74107062d4b419255e5a3bc44caea10cbc2ee290e7fdfbaa1dd254b18e4b" => :yosemite
+    sha256 "25e096147b71cdfe6f6230b6340c577335517fabbbf05027f41db4878629f726" => :mavericks
+  end
+
   head do
     url "https://github.com/jpmens/jo.git"
 
