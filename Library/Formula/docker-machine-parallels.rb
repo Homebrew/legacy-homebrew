365e791cc19039c706537985ecb73c883b397e39
Alexander D. Kanevskiy <kad@kad.name>
2016-03-13 22:39:24 +0200

    docker-machine-parallels: build using tested vendored dependencies
    Closes #50051.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- Library/Formula/docker-machine-parallels.rb
+++ Library/Formula/docker-machine-parallels.rb
@@ -1,11 +1,9 @@
-require "language/go"
-
 class DockerMachineParallels < Formula
   desc "Docker Machine Parallels Driver"
   homepage "https://github.com/Parallels/docker-machine-parallels"
   url "https://github.com/Parallels/docker-machine-parallels/archive/v1.1.1.tar.gz"
   sha256 "a085bbc8c231f4629d3404677f73b49bff2414f7002f0e9acf9aee8cdae15829"
-  revision 1
+  revision 2
 
   head "https://github.com/Parallels/docker-machine-parallels.git"
 
@@ -18,35 +16,20 @@ class DockerMachineParallels < Formula
   end
 
   depends_on "go" => :build
-  depends_on "godep" => :build
   depends_on "docker-machine"
-
-  go_resource "github.com/docker/docker" do
-    url "https://github.com/docker/docker.git", :tag => "v1.9.1", :revision => "a34a1d598c6096ed8b5ce5219e77d68e5cd85462"
-  end
-
-  go_resource "github.com/docker/machine" do
-    url "https://github.com/docker/machine.git", :tag => "v0.5.5", :revision => "02c4254cb4c93a4bbb5dc4ca0467abeb12d72546"
-  end
-
-  go_resource "golang.org/x/crypto" do
-    url "https://go.googlesource.com/crypto.git", :revision => "f18420efc3b4f8e9f3d51f6bd2476e92c46260e9"
-  end
-
-  go_resource "github.com/codegangsta/cli" do
-    url "https://github.com/codegangsta/cli.git", :tag => "v1.2.0", :revision => "565493f259bf868adb54d45d5f4c68d405117adf"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
-    ENV["GO15VENDOREXPERIMENT"] = "0"
 
-    mkdir_p buildpath/"src/github.com/Parallels/"
-    ln_sf buildpath, buildpath/"src/github.com/Parallels/docker-machine-parallels"
-    Language::Go.stage_deps resources, buildpath/"src"
+    path = buildpath/"src/github.com/Parallels/docker-machine-parallels"
+    path.install Dir["*"]
 
-    system "godep", "go", "build", "-o", "./bin/docker-machine-driver-parallels", "./bin"
-    bin.install "bin/docker-machine-driver-parallels"
+    cd path do
+      system "make", "build"
+      bin.install "bin/docker-machine-driver-parallels"
+    end
+
+    prefix.install_metafiles path
   end
 
   test do
