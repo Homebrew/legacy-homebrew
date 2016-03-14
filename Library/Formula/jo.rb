913517471fec881ce37b9af1e10bf321d788d46e
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-14 16:18:03 +0000

    jo: pedantic spacing fix

--- Library/Formula/jo.rb
+++ Library/Formula/jo.rb
@@ -1,7 +1,6 @@
 class Jo < Formula
   desc "JSON output from a shell"
   homepage "https://github.com/jpmens/jo"
-
   url "https://github.com/jpmens/jo/releases/download/v1.0/jo-1.0.tar.gz"
   sha256 "d66ec97258d1afad15643fb2d5b5e807153a732ba45c2417adc66669acbde52e"
 
