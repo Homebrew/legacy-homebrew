696d51b285957ab2a0cd22289a716f5ba7cc57e2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-14 14:10:42 +0000

    libusrsctp: update 0.9.1 bottle.

--- Library/Formula/libusrsctp.rb
+++ Library/Formula/libusrsctp.rb
@@ -8,10 +8,10 @@ class Libusrsctp < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "7b34b5ecea1efc1010628940ae3dbba282c382447efdc95a421073c4968d49b8" => :yosemite
-    sha256 "a1de98e0d6131039baaf32092eca8f77a954f2eb3c93f59f8b6bb7bcf2b03d4a" => :mavericks
-    sha256 "420b3cd77be8a4db30463ce770c6927487d098c4c72fe2e72bb02f76dcb87e20" => :mountain_lion
+    revision 2
+    sha256 "bf05bbef72c45daec3f682b9c12a4ad838ad5c02e1814cbfa837e610140988a3" => :el_capitan
+    sha256 "7b681d68a1dca9792595dce4dc2d677e5a578ed5027dee6ba4dc312cd3f5119b" => :yosemite
+    sha256 "a42402bb65451da92fd33b86ba33a4c1f75d8931830437d866d5298f3c2818cf" => :mavericks
   end
 
   head do
