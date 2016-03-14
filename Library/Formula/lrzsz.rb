6cc7482ffc37ff1b582549dde3a4e5548e96d4a6
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-14 23:11:25 +0000

    lrzsz: use brew mirror
    We may boneyard this at some point if upstream doesn't sort out the SSL issue.
    
    Closes #50104.
    Closes #50010.
    Closes #50096.

--- Library/Formula/lrzsz.rb
+++ Library/Formula/lrzsz.rb
@@ -1,7 +1,10 @@
 class Lrzsz < Formula
   desc "Tools for zmodem/xmodem/ymodem file transfer"
+  # Homepage and URL redirect http to https, which is great.
+  # Less great: Upstream SSL cert expired Dec 2015 and now fails to download.
   homepage "http://www.ohse.de/uwe/software/lrzsz.html"
-  url "http://www.ohse.de/uwe/releases/lrzsz-0.12.20.tar.gz"
+  # url "http://www.ohse.de/uwe/releases/lrzsz-0.12.20.tar.gz"
+  url "https://dl.bintray.com/homebrew/mirror/lrzsz-0.12.20.tar.gz"
   sha256 "c28b36b14bddb014d9e9c97c52459852f97bd405f89113f30bee45ed92728ff1"
 
   def install
@@ -13,7 +16,10 @@ class Lrzsz < Formula
     bin.install "src/lrz", "src/lsz"
 
     system "make", "install"
-
     bin.install_symlink "lrz" => "rz", "lsz" => "sz"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lrb --help 2>&1")
   end
 end
