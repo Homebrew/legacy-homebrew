5b204d1ddd226e23a8672de24b4833b25d320d6a
Mike McQuaid <mike@mikemcquaid.com>
2016-03-15 12:17:33 +0000

    aws-keychain: bottle unneeded.

--- Library/Formula/aws-keychain.rb
+++ Library/Formula/aws-keychain.rb
@@ -4,6 +4,8 @@ class AwsKeychain < Formula
   url "https://github.com/pda/aws-keychain/archive/v3.0.0.tar.gz"
   sha256 "3c9882d3b516b629303ca9a045fc50f6eb75fda25cd2452f10c47eda205e051f"
 
+  bottle :unneeded
+
   def install
     bin.install "aws-keychain"
   end
