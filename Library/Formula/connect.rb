c5dd4ed1a8438de3a7b346b09bd4fde75d6a8d18
moofusecurity <git@moofusecurity.org>
2016-03-16 07:28:57 +1100

    Addresses comments by @mikemcquaid.
     - Adds link to pull request made to upstream to properly resolve
       make error
     - Removes comment about compilation warnings.

--- Library/Formula/connect.rb
+++ Library/Formula/connect.rb
@@ -5,9 +5,14 @@ class Connect < Formula
   sha256 "6ca5291ea8e4a402d875a5507ac78d635373584fd1912f2838b93e03b99730c8"
 
   def install
-    # remove check for windows version that throws error
+    # A pull request has been submitted upstream to resolve issues with the
+    # Makefile producing errors when executed on OS X, see:
+    # https://bitbucket.org/gotoh/connect/pull-requests/5/updates-to-allow-for-cross-compiling/diff
+    # 
+    # This simpler solution of simply removing the call to ver solves the 
+    # problem for installation on OS X until such time as the above pull 
+    # request is merged.
     inreplace "Makefile", "WINVER := $(shell ver)", ""
-    # gcc will produce warnings but the application will work as expected
     system "make"
     bin.install "connect"
   end
