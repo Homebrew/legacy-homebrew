0e464630848c41b428526fa18d2e563b3aff2f45
Chayoung You <yousbe@gmail.com>
2016-03-16 09:51:17 +0900

    avro-c 1.8.0
    Closes #50148.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/avro-c.rb
+++ Library/Formula/avro-c.rb
@@ -1,8 +1,8 @@
 class AvroC < Formula
   desc "Data serialization system"
   homepage "https://avro.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=avro/avro-1.7.7/c/avro-c-1.7.7.tar.gz"
-  sha256 "69b56580f4cc63acbc49825153664ead44abdf1ff8f6f3511d5877533a745a66"
+  url "https://www.apache.org/dyn/closer.cgi?path=avro/avro-1.8.0/c/avro-c-1.8.0.tar.gz"
+  sha256 "4fd93aa0b366e4f7c01a75065b10b9d34ea95ddcf81b7ca39287d6db4efe9bd4"
 
   bottle do
     cellar :any
@@ -13,6 +13,7 @@ class AvroC < Formula
 
   depends_on "pkg-config" => :build
   depends_on "cmake" => :build
+  depends_on "jansson" => :build
 
   def install
     system "cmake", ".", *std_cmake_args
