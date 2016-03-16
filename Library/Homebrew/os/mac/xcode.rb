cd9b4545d5863ddceb08e057d6175f2738a523a5
Tim D. Smith <git@tim-smith.us>
2016-03-15 23:50:08 -0700

    More specific test for default prefix

--- Library/Homebrew/os/mac/xcode.rb
+++ Library/Homebrew/os/mac/xcode.rb
@@ -144,7 +144,7 @@ module OS
         if version < "4.3"
           prefix.to_s.start_with? "/Developer"
         else
-          prefix.to_s.start_with? "/Applications/Xcode.app"
+          prefix.to_s == "/Applications/Xcode.app/Contents/Developer"
         end
       end
     end
