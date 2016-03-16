05a4a5a296affc1f066fd059d14cd9fdbf00efd3
George Gastaldi <gegastaldi@gmail.com>
2016-03-16 18:33:07 -0300

    jboss-forge 3.0.1.Final
    Closes #50172.
    
    Signed-off-by: Andrew Janke <andrew@apjanke.net>

--- Library/Formula/jboss-forge.rb
+++ Library/Formula/jboss-forge.rb
@@ -1,9 +1,9 @@
 class JbossForge < Formula
   desc "Tools to help set up and configure a project"
   homepage "http://forge.jboss.org/"
-  url "https://downloads.jboss.org/forge/releases/3.0.0.Final/forge-distribution-3.0.0.Final-offline.zip"
-  version "3.0.0.Final"
-  sha256 "f4244ad96073158d7f4358b6fdb00535343512587c352569395141b2107c99e4"
+  url "https://downloads.jboss.org/forge/releases/3.0.1.Final/forge-distribution-3.0.1.Final-offline.zip"
+  version "3.0.1.Final"
+  sha256 "2a3ee9702d3e9b32b0696920607e7a067d14b6d87fbbba634b6583089a4897c7"
 
   bottle :unneeded
 
