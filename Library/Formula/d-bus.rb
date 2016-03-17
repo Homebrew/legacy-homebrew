7658b66325eff8c681f1c90079466b427d6da197
Zac Bentley <zbbentley@gmail.com>
2016-03-17 19:47:10 -0400

    Fix backwards compatibility issues with dbus's plist files
    D-Bus includes a session daemon plist file. However, homebrew is not
    explicitly told about this file (though it figures it out),and the file
    contains the ServiceIPC key, which will emit "deprecated" warnings
    in launchd on modern OSX. This commit patches the included config file
    based on OSX version to ensure that files free of legacy cruft are
    distributed correctly.

--- Library/Formula/d-bus.rb
+++ Library/Formula/d-bus.rb
@@ -4,12 +4,30 @@ class DBus < Formula
   homepage "https://wiki.freedesktop.org/www/Software/dbus"
   url "https://dbus.freedesktop.org/releases/dbus/dbus-1.10.6.tar.gz"
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dbus/dbus_1.10.6.orig.tar.gz"
+  master "git://anongit.freedesktop.org/dbus/dbus.git"
   sha256 "b5fefa08a77edd76cd64d872db949eebc02cf6f3f8be82e4bbc641742af5d35f"
 
   bottle do
     sha256 "edf31f467954d3ead8d32fe9db3ef406211fa03eba81b2ab9cad842359a76829" => :el_capitan
     sha256 "e1818ef166c0d49071bd687e83ec51367052bb8846888a482cac823a991aa092" => :yosemite
     sha256 "08094b506bf81ce24aa9c38d9414b43726b35d19461440ecfc74d47d02298d50" => :mavericks
+  end
+
+  # Patches apply the config templating fixed in https://bugs.freedesktop.org/show_bug.cgi?id=94494
+  if MacOS.version >= :leopard
+    patch do
+      url "https://raw.githubusercontent.com/zbentley/dbus-osx-examples/master/homebrew-patches/org.freedesktop.dbus-session.plist.osx.diff"
+      sha256 "a8aa6fe3f2d8f873ad3f683013491f5362d551bf5d4c3b469f1efbc5459a20dc"
+    end
+  else
+    patch do
+      url "https://raw.githubusercontent.com/zbentley/dbus-osx-examples/master/homebrew-patches/org.freedesktop.dbus-session.plist.osx-old.diff"
+      sha256 "da17af8e014d942d6e916a406ad7c901eebe6c3c7780318069db29e6c1e9ca67"
+    end
+  end
+
+  def plist_name
+    "org.freedesktop.dbus-session"
   end
 
   def install
@@ -29,8 +47,6 @@ class DBus < Formula
     system "make"
     ENV.deparallelize
     system "make", "install"
-
-    (prefix/"org.freedesktop.dbus-session.plist").chmod 0644
   end
 
   def post_install
