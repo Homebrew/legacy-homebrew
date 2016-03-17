1a4e47332fa4fbb81b08c32e894f3de1710a8f63
Andrew Titmuss <andrew@coffeetocode.me>
2016-03-17 17:01:59 +1100

    neofetch 1.5
    updated neofetch to version 1.5
    
    Closes #50188.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/neofetch.rb
+++ Library/Formula/neofetch.rb
@@ -1,8 +1,8 @@
 class Neofetch < Formula
   desc "fast, highly customisable system info script"
   homepage "https://github.com/dylanaraps/neofetch"
-  url "https://github.com/dylanaraps/neofetch/archive/1.4.tar.gz"
-  sha256 "b0bda1ac5c3fa4357c6c2157767bcbf08d0e231e3d5e15c4408490f1c3175756"
+  url "https://github.com/dylanaraps/neofetch/archive/1.5.tar.gz"
+  sha256 "a29f9aa194892940a5ec34a449049fbce92bc50dc4a0f608549980318f4136d8"
   head "https://github.com/dylanaraps/neofetch.git"
 
   bottle do
