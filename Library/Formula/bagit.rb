719ac087a2f5123bbfd1ad832c2d714effc25c22
Chayoung You <yousbe@gmail.com>
2016-03-18 02:06:37 +0900

    bagit 4.12.0

--- Library/Formula/bagit.rb
+++ Library/Formula/bagit.rb
@@ -1,8 +1,8 @@
 class Bagit < Formula
   desc "Library for creation, manipulation, and validation of bags"
   homepage "https://github.com/LibraryOfCongress/bagit-java"
-  url "https://github.com/LibraryOfCongress/bagit-java/releases/download/bagit-4.9.0/bagit-4.9.0-bin.zip"
-  sha256 "31b435e965aa6fa0b95943b199194fac42ff8bfd7050319cd4f06c5b183a86e8"
+  url "https://github.com/LibraryOfCongress/bagit-java/releases/download/v4.12.0/bagit-4.12.0.zip"
+  sha256 "045efaff7375af8cb47c9dc7890bf17d4d49d149e4d3ac388862288087505695"
 
   bottle :unneeded
 
@@ -13,10 +13,10 @@ class Bagit < Formula
 
     libexec.install Dir["*"]
 
-    bin.install_symlink libexec/"bin/bag"
+    bin.install_symlink libexec/"bin/bagit"
   end
 
   test do
-    system bin/"bag"
+    system bin/"bagit"
   end
 end
