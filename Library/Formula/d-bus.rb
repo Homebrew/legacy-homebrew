06f9b0376fb997a2bb30ecb02dd68ad903e1b00c
Zac Bentley <zbbentley@gmail.com>
2016-03-17 19:47:10 -0400

    d-bus: fix backwards compatibility issues with plist
    D-Bus includes a session daemon plist file. However, homebrew is not
    explicitly told about this file (though it figures it out),and the file
    contains the ServiceIPC key, which will emit "deprecated" warnings
    in launchd on modern OSX. This commit patches the included config file
    based on OSX version to ensure that files free of legacy cruft are
    distributed correctly.
    
    Closes #50219.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/d-bus.rb
+++ Library/Formula/d-bus.rb
@@ -5,11 +5,19 @@ class DBus < Formula
   url "https://dbus.freedesktop.org/releases/dbus/dbus-1.10.6.tar.gz"
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/d/dbus/dbus_1.10.6.orig.tar.gz"
   sha256 "b5fefa08a77edd76cd64d872db949eebc02cf6f3f8be82e4bbc641742af5d35f"
+  head "https://anongit.freedesktop.org/git/dbus/dbus.git"
 
   bottle do
     sha256 "edf31f467954d3ead8d32fe9db3ef406211fa03eba81b2ab9cad842359a76829" => :el_capitan
     sha256 "e1818ef166c0d49071bd687e83ec51367052bb8846888a482cac823a991aa092" => :yosemite
     sha256 "08094b506bf81ce24aa9c38d9414b43726b35d19461440ecfc74d47d02298d50" => :mavericks
+  end
+
+  # Patch applies the config templating fixed in https://bugs.freedesktop.org/show_bug.cgi?id=94494
+  # Homebrew pr/issue: 50219
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/patches/0a8a55872e/d-bus/org.freedesktop.dbus-session.plist.osx.diff"
+    sha256 "a8aa6fe3f2d8f873ad3f683013491f5362d551bf5d4c3b469f1efbc5459a20dc"
   end
 
   def install
@@ -29,8 +37,6 @@ class DBus < Formula
     system "make"
     ENV.deparallelize
     system "make", "install"
-
-    (prefix/"org.freedesktop.dbus-session.plist").chmod 0644
   end
 
   def post_install
