04eee0c9d4b9d9ddd0210512419e9a9313ec6376
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-18 02:07:42 +0000

    libresample: use secure debian url

--- Library/Formula/libresample.rb
+++ Library/Formula/libresample.rb
@@ -1,7 +1,7 @@
 class Libresample < Formula
   desc "Audio resampling C library"
   homepage "https://ccrma.stanford.edu/~jos/resample/Available_Software.html"
-  url "http://ftp.de.debian.org/debian/pool/main/libr/libresample/libresample_0.1.3.orig.tar.gz"
+  url "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/libr/libresample/libresample_0.1.3.orig.tar.gz"
   sha256 "20222a84e3b4246c36b8a0b74834bb5674026ffdb8b9093a76aaf01560ad4815"
 
   bottle do
