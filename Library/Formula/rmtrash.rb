1bcbe5b2a7a02b89f5bd663ab7ca4df2d32f076f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-18 00:52:51 +0000

    rmtrash: update 0.3.3 bottle.

--- Library/Formula/rmtrash.rb
+++ Library/Formula/rmtrash.rb
@@ -6,10 +6,11 @@ class Rmtrash < Formula
   sha256 "9b30561454529e5923ffb62327d3fe009d23495263efc958534ac6b637e361d6"
 
   bottle do
-    cellar :any
-    sha256 "69021cde0c9835adda114798d90f1aaffb4ce1fff0204146ca1fc8afe7cb2960" => :yosemite
-    sha256 "319c82f0b70f6444962afeede525f57e028f3594500b55434b1903648b828b7d" => :mavericks
-    sha256 "45792bb368e03a9dbeea93e899b9d6b4aaeaba3d6b43face2cc00c3fba2babf6" => :mountain_lion
+    cellar :any_skip_relocation
+    revision 1
+    sha256 "a8b6815f555377a91e800085db9680cc9234b27a4b114ea13e6664a2b1b663d3" => :el_capitan
+    sha256 "adab3849ec805b7c2679e9b278e4d101249193dbd7210f078224e69fc3bb2f1f" => :yosemite
+    sha256 "102f54f713574d7009e3b4fd33738b6b631608a0c38f6ad0945ca1af5a8d6ef5" => :mavericks
   end
 
   def install
