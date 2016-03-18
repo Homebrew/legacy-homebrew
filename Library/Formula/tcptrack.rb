16c586db0ef7236461a4ce81139207efa07ca8b1
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-18 04:27:53 +0000

    tcptrack: use secure urls

--- Library/Formula/tcptrack.rb
+++ Library/Formula/tcptrack.rb
@@ -1,11 +1,13 @@
 class Tcptrack < Formula
   desc "Monitor status of TCP connections on a network interface"
   homepage "http://linux.die.net/man/1/tcptrack"
-  url "http://ftp.de.debian.org/debian/pool/main/t/tcptrack/tcptrack_1.4.2.orig.tar.gz"
+  url "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/t/tcptrack/tcptrack_1.4.2.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/t/tcptrack/tcptrack_1.4.2.orig.tar.gz"
   sha256 "6607b1e1c778c49d3e8795e119065cf66eb2db28b3255dbc56b1612527107049"
 
   def install
     ENV.libstdcxx
+
     # Fix IPv6 on MacOS. The patch was sent by email to the maintainer
     # (tcptrack2@s.rhythm.cx) on 2010-11-24 for inclusion.
     # Still not fixed in 1.4.2 - @adamv
@@ -15,7 +17,13 @@ class Tcptrack < Formula
     system "make", "install"
   end
 
-  def caveats
-    "Run tcptrack as root or via sudo in order for the program to obtain permissions on the network interface."
+  def caveats; <<-EOS.undent
+    Run tcptrack as root or via sudo in order for the program
+    to obtain permissions on the network interface.
+  EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tcptrack -v")
   end
 end
