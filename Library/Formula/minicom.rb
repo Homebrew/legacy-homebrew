c7694086dd453043d506c54185dfcc34f08faf94
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-18 05:16:08 +0000

    minicom: add 2.7 bottle.

--- Library/Formula/minicom.rb
+++ Library/Formula/minicom.rb
@@ -4,6 +4,12 @@ class Minicom < Formula
   url "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/minicom/minicom_2.7.orig.tar.gz"
   sha256 "9ac3a663b82f4f5df64114b4792b9926b536c85f59de0f2d2b321c7626a904f4"
 
+  bottle do
+    sha256 "81f7a4e2671a83ed8ee528b74dc47d6b5bf57995d066048b3130a097ff22203d" => :el_capitan
+    sha256 "ab76b20b7ad26d544946d6fa39722e3709fd2b2de5c8da3693c56c1829db8f8a" => :yosemite
+    sha256 "18f41c6b651d70c248efba991f044d3f1ed4226ec0bd916773609e865abf0c1a" => :mavericks
+  end
+
   def install
     # There is a silly bug in the Makefile where it forgets to link to iconv. Workaround below.
     ENV["LIBS"] = "-liconv"
