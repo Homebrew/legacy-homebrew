0a55a3bc86fb49c397f3bbe547f131b23c274112
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-18 05:16:08 +0000

    tcptrack: add 1.4.2 bottle.

--- Library/Formula/tcptrack.rb
+++ Library/Formula/tcptrack.rb
@@ -5,6 +5,13 @@ class Tcptrack < Formula
   mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/t/tcptrack/tcptrack_1.4.2.orig.tar.gz"
   sha256 "6607b1e1c778c49d3e8795e119065cf66eb2db28b3255dbc56b1612527107049"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "93022ecf618c41d508175e15bb6cdbc6d24206cebe9e6f77524b8ab066134f2f" => :el_capitan
+    sha256 "23ee40e3789155774146fee0e1b99cba1792bef8ddfe0aa9d98864a32545445e" => :yosemite
+    sha256 "f1d33c99082cbcff0030a40c164f680a6e6e995b9b92c6b414c1e2d4d25d67f6" => :mavericks
+  end
+
   def install
     ENV.libstdcxx
 
