3fb163ea67ea4e076c72a92382c2b1a8072fa876
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-18 04:23:32 +0000

    monkeysphere 0.37

--- Library/Formula/monkeysphere.rb
+++ Library/Formula/monkeysphere.rb
@@ -1,56 +1,42 @@
 class Monkeysphere < Formula
   desc "Use the OpenPGP web of trust to verify ssh connections"
   homepage "http://web.monkeysphere.info/"
-  url "http://archive.monkeysphere.info/debian/pool/monkeysphere/m/monkeysphere/monkeysphere_0.36.orig.tar.gz"
-  sha256 "6ae4edeff2cc29b6913346e15b61500ea7cc06d761a9f42e67de83b7d2607be7"
+  url "http://archive.monkeysphere.info/debian/pool/monkeysphere/m/monkeysphere/monkeysphere_0.37.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/m/monkeysphere/monkeysphere_0.37.orig.tar.gz"
+  sha256 "b510b8a414fb400356e80f1f882488785df72ac54078410b54d4c50a84686e59"
 
   head "git://git.monkeysphere.info/monkeysphere"
 
-  depends_on "Crypt::OpenSSL::Bignum" => :perl
+  depends_on "gnu-sed" => :build
+  depends_on "openssl"
 
-  patch :DATA
+  resource "Crypt::OpenSSL::Bignum" do
+    url "https://cpan.metacpan.org/authors/id/K/KM/KMX/Crypt-OpenSSL-Bignum-0.06.tar.gz"
+    sha256 "c7ccafa9108524b9a6f63bf4ac3377f9d7e978fee7b83c430af7e74c5fcbdf17"
+  end
 
   def install
-    ENV["PREFIX"] = "#{prefix}"
-    ENV["ETCPREFIX"] = "#{prefix}"
+    ENV.prepend_path "PATH", Formula["gnu-sed"].libexec/"gnubin"
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+
+    resource("Crypt::OpenSSL::Bignum").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make", "install"
+    end
+
+    ENV["PREFIX"] = prefix
+    ENV["ETCPREFIX"] = prefix
     system "make", "install"
+
+    # This software expects to be installed in a very specific, unusual way.
+    # Consequently, this is a bit of a naughty hack but the least worst option.
+    inreplace pkgshare/"keytrans", "#!/usr/bin/perl -T",
+                                   "#!/usr/bin/perl -T -I#{libexec}/lib/perl5"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/monkeysphere v")
+    # This just checks it finds the vendored Perl resource.
+    assert_match "We need at least", pipe_output("#{bin}/openpgp2pem --help 2>&1")
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index a37aa8f..36fe1ce 100755
---- a/Makefile
-+++ b/Makefile
-@@ -45 +45 @@ install: all installman
--	sed -i 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/bin/monkeysphere
-+	sed -i '' 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/bin/monkeysphere
-@@ -47 +47 @@ install: all installman
--	sed -i 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/sbin/monkeysphere-host
-+	sed -i '' 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/sbin/monkeysphere-host
-@@ -49 +49 @@ install: all installman
--	sed -i 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/sbin/monkeysphere-authentication
-+	sed -i '' 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/sbin/monkeysphere-authentication
-@@ -53,2 +53,2 @@ install: all installman
--	sed -i 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(PREFIX)/share/monkeysphere/defaultenv
--	sed -i 's:__SYSDATADIR_PREFIX__:$(LOCALSTATEDIR):' $(DESTDIR)$(PREFIX)/share/monkeysphere/defaultenv
-+	sed -i '' 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(PREFIX)/share/monkeysphere/defaultenv
-+	sed -i '' 's:__SYSDATADIR_PREFIX__:$(LOCALSTATEDIR):' $(DESTDIR)$(PREFIX)/share/monkeysphere/defaultenv
-@@ -61,2 +61,2 @@ install: all installman
--	sed -i 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/share/monkeysphere/transitions/0.23
--	sed -i 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/share/monkeysphere/transitions/0.28
-+	sed -i '' 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/share/monkeysphere/transitions/0.23
-+	sed -i '' 's:__SYSSHAREDIR_PREFIX__:$(PREFIX):' $(DESTDIR)$(PREFIX)/share/monkeysphere/transitions/0.28
-@@ -83 +83 @@ installman:
--	sed -i 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(MANPREFIX)/man1/monkeysphere.1
-+	sed -i '' 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(MANPREFIX)/man1/monkeysphere.1
-@@ -86,2 +86,2 @@ installman:
--	sed -i 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-host.8
--	sed -i 's:__SYSDATADIR_PREFIX__:$(LOCALSTATEDIR):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-host.8
-+	sed -i '' 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-host.8
-+	sed -i '' 's:__SYSDATADIR_PREFIX__:$(LOCALSTATEDIR):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-host.8
-@@ -90,2 +90,2 @@ installman:
--	sed -i 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-authentication.8
--	sed -i 's:__SYSDATADIR_PREFIX__:$(LOCALSTATEDIR):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-authentication.8
-+	sed -i '' 's:__SYSCONFDIR_PREFIX__:$(ETCPREFIX):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-authentication.8
-+	sed -i '' 's:__SYSDATADIR_PREFIX__:$(LOCALSTATEDIR):' $(DESTDIR)$(MANPREFIX)/man8/monkeysphere-authentication.8
