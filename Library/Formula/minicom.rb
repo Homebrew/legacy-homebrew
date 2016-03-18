6e5e037d0438b7245f3d581894a05ffd739216f0
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-18 02:12:23 +0000

    minicom: style nits

--- Library/Formula/minicom.rb
+++ Library/Formula/minicom.rb
@@ -13,25 +13,29 @@ class Minicom < Formula
                           "--mandir=#{man}"
     system "make", "install"
 
-    (prefix + "etc").mkdir
-    (prefix + "var").mkdir
-    (prefix + "etc/minirc.dfl").write "pu lock #{prefix}/var\npu escape-key Escape (Meta)\n"
+    (prefix/"etc").mkdir
+    (prefix/"var").mkdir
+    (prefix/"etc/minirc.dfl").write "pu lock #{prefix}/var\npu escape-key Escape (Meta)\n"
   end
 
-  def caveats; <<-EOS
-Terminal Compatibility
-======================
-If minicom doesn't see the LANG variable, it will try to fallback to
-make the layout more compatible, but uglier. Certain unsupported
-encodings will completely render the UI useless, so if the UI looks
-strange, try setting the following environment variable:
+  def caveats; <<-EOS.undent
+    Terminal Compatibility
+    ======================
+    If minicom doesn't see the LANG variable, it will try to fallback to
+    make the layout more compatible, but uglier. Certain unsupported
+    encodings will completely render the UI useless, so if the UI looks
+    strange, try setting the following environment variable:
 
-LANG="en_US.UTF-8"
+      LANG="en_US.UTF-8"
 
-Text Input Not Working
-======================
-Most development boards require Serial port setup -> Hardware Flow
-Control to be set to "No" to input text.
-    EOS
+    Text Input Not Working
+    ======================
+    Most development boards require Serial port setup -> Hardware Flow
+    Control to be set to "No" to input text.
+  EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/minicom -v", 1)
   end
 end
