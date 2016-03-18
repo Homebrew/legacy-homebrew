311ad712e581952595a364ce02db8cbfc4f37c0a
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-18 04:29:49 +0000

    tokyo-cabinet: use secure mirror

--- Library/Formula/tokyo-cabinet.rb
+++ Library/Formula/tokyo-cabinet.rb
@@ -2,7 +2,7 @@ class TokyoCabinet < Formula
   desc "Lightweight database library"
   homepage "http://fallabs.com/tokyocabinet/"
   url "http://fallabs.com/tokyocabinet/tokyocabinet-1.4.48.tar.gz"
-  mirror "http://ftp.de.debian.org/debian/pool/main/t/tokyocabinet/tokyocabinet_1.4.48.orig.tar.gz"
+  mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/t/tokyocabinet/tokyocabinet_1.4.48.orig.tar.gz"
   sha256 "a003f47c39a91e22d76bc4fe68b9b3de0f38851b160bbb1ca07a4f6441de1f90"
 
   bottle do
