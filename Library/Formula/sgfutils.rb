04293354e58a4d218816d8581b3e33c9729629e5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-18 09:33:43 +0000

    sgfutils: add 0.22 bottle.

--- Library/Formula/sgfutils.rb
+++ Library/Formula/sgfutils.rb
@@ -4,6 +4,13 @@ class Sgfutils < Formula
   url "http://homepages.cwi.nl/~aeb/go/sgfutils/sgfutils-0.22.tgz"
   sha256 "4ad83be91ac3a52dd8bd0766bfe403499430da96e680c5aea2f5381ba0b34923"
 
+  bottle do
+    cellar :any
+    sha256 "4bf26ce188f979f02ca05b77bf1d043e65109aa57097eb1d50e4d7a56e5080f2" => :el_capitan
+    sha256 "13c3312694adee8c1b34a5b1ea3b1b391637958db3e28dcd257a6c32f3904516" => :yosemite
+    sha256 "bd3341fad2702bf1e92f10b34d7c64be16dbeb74a404d0dc348560fd46e6853c" => :mavericks
+  end
+
   depends_on "openssl"
 
   def install
