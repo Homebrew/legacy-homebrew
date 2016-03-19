6ab016047aa352b88f03538e2276cb94c1bb305d
s172262 <s172262@users.noreply.github.com>
2016-03-19 12:35:41 +0900

    jpeg 9b

--- Library/Formula/jpeg.rb
+++ Library/Formula/jpeg.rb
@@ -2,14 +2,18 @@ class Jpeg < Formula
   desc "JPEG image manipulation library"
   homepage "http://www.ijg.org"
   url "http://www.ijg.org/files/jpegsrc.v9b.tar.gz"
-  version "9b"
   sha256 "240fd398da741669bf3c90366f58452ea59041cacc741a489b99f2f6a0bad052"
 
   option :universal
 
+  patch :p0 do
+      url "ftp://ftp.netbsd.org/pub/pkgsrc/current/pkgsrc/graphics/jpeg/patches/patch-jmorecfg.h"
+      sha256 "9b26396b4124fda61fe70dd7a672e56300d8178612ef464e33dac9b0601c2ad3"
+  end
+
   def install
     ENV.universal_binary if build.universal?
-     system "./configure", "--disable-debug",
+    system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
