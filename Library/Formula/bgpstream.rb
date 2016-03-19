393399999175cbc2a8ca6c7fa770742cf557a681
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-19 04:20:53 +0000

    bgpstream: add 1.1.0 bottle.

--- Library/Formula/bgpstream.rb
+++ Library/Formula/bgpstream.rb
@@ -4,6 +4,13 @@ class Bgpstream < Formula
   url "https://bgpstream.caida.org/bundles/caidabgpstreamwebhomepage/dists/bgpstream-1.1.0.tar.gz"
   sha256 "b89cef45bcc5ae4011aa3c42f689ae9fd7b5c8fd25e06ab18589577b5e077f89"
 
+  bottle do
+    cellar :any
+    sha256 "5a94e3a2f3e36a0aa93c9c06e33d126079307080a7a8237cf719c9eb20c23272" => :el_capitan
+    sha256 "c7fa0c3adcebc76cafb1cdb56116e8fcd111a62d144c82a00226676e5f6a1950" => :yosemite
+    sha256 "a0a45f0c9c135d94c64772d273bf0e3cb462eb6b83663388b343665f0894015a" => :mavericks
+  end
+
   depends_on "wandio"
 
   def install
