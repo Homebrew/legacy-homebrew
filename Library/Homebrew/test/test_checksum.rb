2bf10fca7bd91df0b96d0df50e4ea33eee067f95
Xu Cheng <xucheng@me.com>
2016-03-20 14:12:15 +0800

    fix tests for deprecating SHA1

--- Library/Homebrew/test/test_checksum.rb
+++ Library/Homebrew/test/test_checksum.rb
@@ -3,16 +3,16 @@ require "checksum"
 
 class ChecksumTests < Homebrew::TestCase
   def test_empty?
-    assert_empty Checksum.new(:sha1, "")
+    assert_empty Checksum.new(:sha256, "")
   end
 
   def test_equality
-    a = Checksum.new(:sha1, TEST_SHA1)
-    b = Checksum.new(:sha1, TEST_SHA1)
+    a = Checksum.new(:sha256, TEST_SHA256)
+    b = Checksum.new(:sha256, TEST_SHA256)
     assert_equal a, b
 
-    a = Checksum.new(:sha1, TEST_SHA1)
-    b = Checksum.new(:sha1, TEST_SHA1.reverse)
+    a = Checksum.new(:sha256, TEST_SHA256)
+    b = Checksum.new(:sha256, TEST_SHA256.reverse)
     refute_equal a, b
 
     a = Checksum.new(:sha1, TEST_SHA1)
