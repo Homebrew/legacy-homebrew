91626e633b08e4cb39340fd6d9fee6804ed41e35
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-20 06:41:33 +0000

    arabica: add 20160214 bottle.

--- Library/Formula/arabica.rb
+++ Library/Formula/arabica.rb
@@ -6,6 +6,13 @@ class Arabica < Formula
   sha256 "ea6940773ae95ec02c6736c0ba688bdfb5c7691e7d2c8da1b331eca74949d73a"
   head "https://github.com/jezhiggins/arabica.git"
 
+  bottle do
+    cellar :any
+    sha256 "185edd120b5759f25d1e69b9f24840eef6a404b1001c90e684546e359cf75928" => :el_capitan
+    sha256 "1882f30edf8da8d98df603a6006c3dce96e21941e4266103366930b3e5a922c2" => :yosemite
+    sha256 "5d247d4d5819106404bc7091e3b6141b4d298c77636bee39bfc524a3c5481e7f" => :mavericks
+  end
+
   option "without-test", "Skip compile-time make checks (Not Recommended)"
 
   depends_on "autoconf" => :build
