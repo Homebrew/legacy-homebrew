bb501e5c41384c51784b6bed155d44083ecb02f1
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-20 04:06:58 +0000

    arabica 20160214
    Closes #46376.

--- Library/Formula/arabica.rb
+++ Library/Formula/arabica.rb
@@ -1,21 +1,23 @@
 class Arabica < Formula
   desc "XML toolkit written in C++"
   homepage "http://www.jezuk.co.uk/cgi-bin/view/arabica"
-  url "https://downloads.sourceforge.net/project/arabica/arabica/November-12/arabica-2012-November.tar.gz"
-  version "20121126"
-  sha256 "2e64e38d773ff37f5d7da181fe07d6dde2c35e633b9772b604439a286cd4f98b"
+  url "https://github.com/jezhiggins/arabica/archive/2016-January.tar.gz"
+  version "20160214"
+  sha256 "ea6940773ae95ec02c6736c0ba688bdfb5c7691e7d2c8da1b331eca74949d73a"
+  head "https://github.com/jezhiggins/arabica.git"
 
-  head do
-    url "https://github.com/jezhiggins/arabica.git"
+  option "without-test", "Skip compile-time make checks (Not Recommended)"
 
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "boost" => :recommended
 
   def install
-    system "autoreconf", "-fvi" if build.head?
+    system "autoreconf", "-fvi"
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make", "check" if build.with? "test"
     system "make", "install"
   end
 end
