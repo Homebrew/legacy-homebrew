2fdfd723ae341b41d9c88e3a36338247f403828d
Clemens Horch <mail@clemenshorch.de>
2016-03-20 14:38:19 +0100

    duply 1.11.2
    - upgraded to new upstream version 1.11.2
    - added do test section to formula: "However, a bad test is better
      than no test at all." (Formula-Cookbook.md#add-a-test-to-the-formula)

--- Library/Formula/duply.rb
+++ Library/Formula/duply.rb
@@ -1,8 +1,8 @@
 class Duply < Formula
   desc "Frontend to the duplicity backup system"
   homepage "http://duply.net"
-  url "https://downloads.sourceforge.net/project/ftplicity/duply%20\(simple%20duplicity\)/1.11.x/duply_1.11.1.tgz"
-  sha256 "128792962bbd1509e875bd91e613be3aa1263e6edb57815957e07d54346bc02d"
+  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/1.11.x/duply_1.11.2.tgz"
+  sha256 "06843960ae5753395cb5be72a9c9f2fe78e6ea81a17c6d8d28f2ae49ab50d5aa"
 
   bottle :unneeded
 
@@ -10,5 +10,9 @@ class Duply < Formula
 
   def install
     bin.install "duply"
+  end
+
+  test do
+    system "#{bin}/duply", "-v"
   end
 end
