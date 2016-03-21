da5ca5ae6d54ee45362cea0bbf6be1683d35a638
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-03-21 18:35:33 +0000

    geocode-glib 3.20.0

--- Library/Formula/geocode-glib.rb
+++ Library/Formula/geocode-glib.rb
@@ -1,8 +1,8 @@
 class GeocodeGlib < Formula
   desc "GNOME library for gecoding and reverse geocoding"
   homepage "https://developer.gnome.org/geocode-glib"
-  url "https://download.gnome.org/sources/geocode-glib/3.18/geocode-glib-3.18.2.tar.xz"
-  sha256 "95b11ef2697ac5dbb2f397e7117e08e157b2168624c71507656928095012494e"
+  url "https://download.gnome.org/sources/geocode-glib/3.20/geocode-glib-3.20.0.tar.xz"
+  sha256 "5f2264238e9481a95ca260ba2c652bebd247ec1852a27ded5b126117eb1bb6ba"
 
   bottle do
     sha256 "dad6f3fbbb15e9964b4d754ce82de4466c3f3079f8d1686b96230c01fb2e914b" => :el_capitan
