7ee317dab76ea68d0f8b12b561d37e1d2407d3bf
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-03-22 12:51:54 +0000

    libsoup 2.54.0

--- Library/Formula/libsoup.rb
+++ Library/Formula/libsoup.rb
@@ -1,9 +1,8 @@
 class Libsoup < Formula
   desc "HTTP client/server library for GNOME"
   homepage "https://live.gnome.org/LibSoup"
-  url "https://download.gnome.org/sources/libsoup/2.52/libsoup-2.52.2.tar.xz"
-  sha256 "db55628b5c7d952945bb71b236469057c8dfb8dea0c271513579c6273c2093dc"
-  revision 1
+  url "https://download.gnome.org/sources/libsoup/2.54/libsoup-2.54.0.tar.xz"
+  sha256 "fbf1038efb10d2ffbbb88bb46e7ce32b683fde8e566f36bcf26f7f69a550ec56"
 
   bottle do
     sha256 "de28079d444082ea4f6f2aca4388152aa8d303926917baa5f656bdd7d222ef72" => :el_capitan
@@ -25,8 +24,8 @@ class Libsoup < Formula
       "--disable-dependency-tracking",
       "--disable-silent-rules",
       "--prefix=#{prefix}",
-      "--without-gnome",
       "--disable-tls-check",
+      "--enable-introspection=yes",
     ]
 
     # ensures that the vala files remain within the keg
