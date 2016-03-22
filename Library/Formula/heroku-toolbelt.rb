8157978a77b035a140b9e62ae015284c29b24506
Jeff Dickey <dickeyxxx@gmail.com>
2016-03-22 00:06:35 -0700

    heroku-toolbelt 3.42.44
    Closes #50315.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -1,8 +1,8 @@
 class HerokuToolbelt < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://toolbelt.heroku.com/other"
-  url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.42.38.tgz"
-  sha256 "fea1a6ad9c1fb2d1b2464d1c2acd637b61b638a1321da8638ccf5bb1b5f6b23f"
+  url "https://s3.amazonaws.com/assets.heroku.com/heroku-client/heroku-client-3.42.44.tgz"
+  sha256 "0970405e12759ae7327f3db136830a448bbc43fa93466f64909c74cef2d11477"
   head "https://github.com/heroku/heroku.git"
 
   bottle :unneeded
