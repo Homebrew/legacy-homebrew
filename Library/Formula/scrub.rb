81a1e10b8f681d0968358421611fe73278870623
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-22 23:07:06 +0000

    scrub: fix other style nits

--- Library/Formula/scrub.rb
+++ Library/Formula/scrub.rb
@@ -2,7 +2,7 @@ class Scrub < Formula
   desc "Writes patterns on magnetic media to thwart data recovery"
   homepage "https://code.google.com/p/diskscrub/"
   url "https://github.com/chaos/scrub/releases/download/2.6.1/scrub-2.6.1.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/s/scrub/scrub_2.6.1.orig.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/s/scrub/scrub_2.6.1.orig.tar.gz"
   sha256 "43d98d3795bc2de7920efe81ef2c5de4e9ed1f903c35c939a7d65adc416d6cb8"
 
   bottle do
@@ -31,9 +31,8 @@ class Scrub < Formula
     path = testpath/"foo.txt"
     path.write "foo"
 
-    output = `#{bin}/scrub -r -p dod #{path}`
+    output = shell_output("#{bin}/scrub -r -p dod #{path}")
     assert_match "scrubbing #{path}", output
-    assert_equal 0, $?.exitstatus
     assert !File.exist?(path)
   end
 end
