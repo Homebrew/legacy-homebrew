b69a1537988b4cab820268829708285652daa25b
Alexandre Espinosa Menor <aemenor@gmail.com>
2016-03-22 23:19:47 +0100

    galen 2.2.3
    galen version 2.2.3
    
    Closes #50330.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/galen.rb
+++ Library/Formula/galen.rb
@@ -1,8 +1,8 @@
 class Galen < Formula
   desc "Automated testing of look and feel for responsive websites"
   homepage "http://galenframework.com/"
-  url "https://github.com/galenframework/galen/releases/download/galen-2.2.1/galen-bin-2.2.1.zip"
-  sha256 "1c7cc172a1d3a91ca6b949e2c4f12d8d957c1e5fa4be512aa4a6a021b01a30f6"
+  url "https://github.com/galenframework/galen/releases/download/galen-2.2.3/galen-bin-2.2.3.zip"
+  sha256 "1857c44261dd7211318d572fa2c253f6349df374e4a5bbd0ce023d64f8ea7b8e"
 
   bottle do
     cellar :any_skip_relocation
