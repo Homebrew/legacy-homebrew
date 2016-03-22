a1e1312e7793718c08db1b3d74a458f4259173a0
s172262 <s172262@users.noreply.github.com>
2016-03-22 16:12:26 +0900

    autoconf-archive 2016.03.20
    Closes #50316.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/autoconf-archive.rb
+++ Library/Formula/autoconf-archive.rb
@@ -1,15 +1,12 @@
 class AutoconfArchive < Formula
   desc "Collection of over 500 reusable autoconf macros"
   homepage "https://savannah.gnu.org/projects/autoconf-archive/"
-  url "http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2015.09.25.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2015.09.25.tar.xz"
-  sha256 "7c0467a5dbd2340153bca5a477bd92fbc951d9ee3cbed92f16f6bf08ac0c350a"
+  url "http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2016.03.20.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/autoconf-archive/autoconf-archive-2016.03.20.tar.xz"
+  sha256 "88fb2efff640eddd28a52ae550ff5561bca3bd2bba09e1d7b0580e719875e437"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c006f30a366c500c6b3aa3737853c68b1191b71b99e4f3ab55603563fe27a131" => :el_capitan
-    sha256 "927856f2edfaacbea2b6bd8223f0c5ebc8ae5316ed8da76436af70afadc0fa7d" => :yosemite
-    sha256 "d60dff8a8be2ac5424cc1a95e424479bba1edd7a1a5101cad62d9a284971f509" => :mavericks
   end
 
   # autoconf-archive is useless without autoconf
