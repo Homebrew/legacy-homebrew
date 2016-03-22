c7b660948c7bd09957fbffe9039e945eb3a03c34
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-03-22 12:55:53 +0000

    at-spi2-core 2.20.0

--- Library/Formula/at-spi2-core.rb
+++ Library/Formula/at-spi2-core.rb
@@ -1,8 +1,8 @@
 class AtSpi2Core < Formula
   desc "Protocol definitions and daemon for D-Bus at-spi"
   homepage "http://a11y.org"
-  url "https://download.gnome.org/sources/at-spi2-core/2.18/at-spi2-core-2.18.3.tar.xz"
-  sha256 "ada26add94155f97d0f601a20cb7a0e3fd3ba1588c3520b7288316494027d629"
+  url "https://download.gnome.org/sources/at-spi2-core/2.20/at-spi2-core-2.20.0.tar.xz"
+  sha256 "dcc49fb7c08e582910b21ff1e4110b22ab66068a4c6f0db52b098d66794c609b"
 
   bottle do
     sha256 "96b67926248f6950a20ea28fde034e9a91b6c4c4a14eb3cd54e0aead53224ce6" => :el_capitan
