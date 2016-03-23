f262bd2815914f828019d3f8f6e961e2740a79ea
Ivan Malopinsky <imsky@users.noreply.github.com>
2016-03-22 22:21:35 -0400

    git-fresh 1.6.1
    update git-fresh to 1.6.1
    
    Closes #50337.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- Library/Formula/git-fresh.rb
+++ Library/Formula/git-fresh.rb
@@ -1,17 +1,16 @@
 class GitFresh < Formula
   desc "Utility to keep git repos fresh"
   homepage "https://github.com/imsky/git-fresh"
-  url "https://github.com/imsky/git-fresh/archive/v1.2.3.tar.gz"
-  sha256 "19f41d6c6f82b57fe02e6024f5ec46e72315e17fe01f0ddb29756bfd723c6a90"
+  url "https://github.com/imsky/git-fresh/archive/v1.6.1.tar.gz"
+  sha256 "4820bf8883bdb5623130ebeba397426e3cc0d639ce0f95313ac28f6d33ec17a2"
 
   bottle :unneeded
 
   def install
-    system "./install", bin
+    system "./install.sh", bin
   end
 
   test do
-    system "git", "init"
-    system "git", "fresh"
+    system "git-fresh", "-T"
   end
 end
