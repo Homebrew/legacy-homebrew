cdf03d6a34c58c88b9e00a6caae87776cb5eba40
Leigh Thompson <git@moofusecurity.org>
2016-03-25 07:04:41 +1100

    Update to 1.105 to resolve make & compile warnings
    The following pull request and issues were resolved in upstream:
     * https://bitbucket.org/gotoh/connect/pull-requests/5/updates-to-allow-for-cross-compiling
     * https://bitbucket.org/gotoh/connect/issues/7/fix-for-gcc-compiler-warnings
     * https://bitbucket.org/gotoh/connect/issues/8/tag-release-1105

--- Library/Formula/connect.rb
+++ Library/Formula/connect.rb
@@ -1,18 +1,10 @@
 class Connect < Formula
   desc "provides SOCKS and HTTPS proxy support to SSH."
   homepage "https://bitbucket.org/gotoh/connect/wiki/Home"
-  url "https://bitbucket.org/gotoh/connect/get/1.104.tar.gz"
-  sha256 "6ca5291ea8e4a402d875a5507ac78d635373584fd1912f2838b93e03b99730c8"
+  url "https://bitbucket.org/gotoh/connect/get/1.105.tar.gz"
+  sha256 "e7c98d31787f93b51c62ee05e0b558cfb577cda8198834a6d5b4d32528bf63ee"
 
   def install
-    # A pull request has been submitted upstream to resolve issues with the
-    # Makefile producing errors when executed on OS X, see:
-    # https://bitbucket.org/gotoh/connect/pull-requests/5/updates-to-allow-for-cross-compiling/diff
-    #
-    # This simpler solution of simply removing the call to ver solves the
-    # problem for installation on OS X until such time as the above pull
-    # request is merged.
-    inreplace "Makefile", "WINVER := $(shell ver)", ""
     system "make"
     bin.install "connect"
   end
