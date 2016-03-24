ff4644846b962721109a5c4960bdcc72d5c792bc
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-03-24 09:34:09 +0000

    pango 1.40.0

--- Library/Formula/pango.rb
+++ Library/Formula/pango.rb
@@ -3,8 +3,8 @@ class Pango < Formula
   homepage "http://www.pango.org/"
 
   stable do
-    url "https://download.gnome.org/sources/pango/1.38/pango-1.38.1.tar.xz"
-    sha256 "1320569f6c6d75d6b66172b2d28e59c56ee864ee9df202b76799c4506a214eb7"
+    url "https://download.gnome.org/sources/pango/1.40/pango-1.40.0.tar.xz"
+    sha256 "da17985df314cb07d066ab5424f59c21ce973ece05b7de4df04d798ec8511c8b"
   end
 
   bottle do
