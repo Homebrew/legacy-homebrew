f79d85a8037e87e94f15b098a0e92222792bbd58
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-24 16:06:55 +0000

    wolfssl 3.9.0
    Closes #50389.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/wolfssl.rb
+++ Library/Formula/wolfssl.rb
@@ -1,8 +1,8 @@
 class Wolfssl < Formula
   desc "Embedded SSL Library written in C"
   homepage "https://www.wolfssl.com/wolfSSL/Home.html"
-  url "https://github.com/wolfSSL/wolfssl/archive/v3.8.0.tar.gz"
-  sha256 "2a1c72c98bd8d473d227f98d0ce970432478e05fbee1ebd8559918b017864199"
+  url "https://github.com/wolfSSL/wolfssl/archive/v3.9.0.tar.gz"
+  sha256 "9d461209f663391d4c0349c23466556782c13964b9626fef694f2d420a176658"
   head "https://github.com/wolfSSL/wolfssl.git"
 
   bottle do
