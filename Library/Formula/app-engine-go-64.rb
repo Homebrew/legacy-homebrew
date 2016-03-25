c4549dd6d20f84f0f58bb7ec5fd0f1d5fc77e5e9
Dan McNevin <daniel.mcnevin@disney.com>
2016-03-24 19:35:35 -0700

    app-engine-go-64 1.9.35

--- Library/Formula/app-engine-go-64.rb
+++ Library/Formula/app-engine-go-64.rb
@@ -1,8 +1,8 @@
 class AppEngineGo64 < Formula
   desc "Google App Engine SDK for Go (AMD64)"
   homepage "https://cloud.google.com/appengine/docs/go/"
-  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.34.zip"
-  sha256 "961ea5776abd183cef76c020eceb8fb2b015006f589922fc4f6aff16dc951138"
+  url "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_darwin_amd64-1.9.35.zip"
+  sha256 "251699fd842f9e139c2e5dd8637bf0977e8b893d32ba6b0d4768b022dd276fab"
 
   bottle :unneeded
 
