75efa905a6ca067813a641c9fab9158368a256ec
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-03-25 13:39:51 +0000

    gst-validate 1.8.0
    Closes #50408.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/gst-validate.rb
+++ Library/Formula/gst-validate.rb
@@ -1,8 +1,8 @@
 class GstValidate < Formula
   desc "Tools to validate GstElements from GStreamer"
   homepage "https://gstreamer.freedesktop.org/data/doc/gstreamer/head/gst-validate/html/"
-  url "https://gstreamer.freedesktop.org/src/gst-validate/gst-validate-1.6.0.tar.xz"
-  sha256 "3baef8c7b5363293c20314a30afd54629849fc597669991fdcf92303602dafee"
+  url "https://gstreamer.freedesktop.org/src/gst-validate/gst-validate-1.8.0.tar.xz"
+  sha256 "7666b777bd4b05efe4520ef92669169d8879f69a68606578ec667ae7dc4d9edd"
 
   bottle do
     sha256 "338d8f489811e92491ad6d86a96c76ac6056b6e178bb5caa412f73feff9b7d8f" => :el_capitan
@@ -23,6 +23,10 @@ class GstValidate < Formula
   depends_on "gobject-introspection"
   depends_on "gstreamer"
   depends_on "gst-plugins-base"
+
+  # fixes a missing library in a linker command
+  # submitted upstream in https://bugzilla.gnome.org/show_bug.cgi?id=764192
+  patch :DATA
 
   def install
     inreplace "tools/gst-validate-launcher.in", "env python2", "env python"
@@ -53,3 +57,18 @@ class GstValidate < Formula
     system "#{bin}/gst-validate-launcher", "--usage"
   end
 end
+
+__END__
+diff --git a/gst-libs/gst/video/Makefile.in b/gst-libs/gst/video/Makefile.in
+index e55b8b1..0b1ee0f 100644
+--- a/gst-libs/gst/video/Makefile.in
++++ b/gst-libs/gst/video/Makefile.in
+@@ -487,7 +487,7 @@ validateplugindir = @validateplugindir@
+ libgstvalidatevideo_@GST_API_VERSION@_la_SOURCES = gstvalidatessim.c gssim.c
+ libgstvalidatevideo_@GST_API_VERSION@include_HEADERS = gstvalidatessim.h gssim.h
+ libgstvalidatevideo_@GST_API_VERSION@_la_CFLAGS = $(GST_ALL_CFLAGS) $(CAIRO_CFLAGS) $(GST_VIDEO_CFLAGS) -I$(top_builddir)
+-libgstvalidatevideo_@GST_API_VERSION@_la_LIBADD = $(GST_ALL_LIBS) $(CAIRO_LIBS) $(GST_VIDEO_LIBS) $(top_builddir)/gst/validate/libgstvalidate-@GST_API_VERSION@.la
++libgstvalidatevideo_@GST_API_VERSION@_la_LIBADD = $(GST_ALL_LIBS) $(CAIRO_LIBS) $(GST_VIDEO_LIBS) $(top_builddir)/gst/validate/libgstvalidate-@GST_API_VERSION@.la $(GIO_LIBS)
+ libgstvalidatevideo_@GST_API_VERSION@_la_LDFLAGS = $(GST_ALL_LDFLAGS) $(CAIRO_LDFLAGS) $(GST_VIDEO_LDFLAGS)
+ lib_LTLIBRARIES = libgstvalidatevideo-@GST_API_VERSION@.la
+ libgstvalidatevideo_@GST_API_VERSION@includedir = $(includedir)/gstreamer-@GST_API_VERSION@/lib/validate/video
