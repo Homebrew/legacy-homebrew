e34990fdf8cea42a257ee921cde961641d38fb5e
toshi0383 <t.suzuki326@gmail.com>
2016-03-26 13:03:20 +0900

    Add sniper version 1.0.0

---
+++ Library/Formula/sniper.rb
@@ -0,0 +1,7 @@
+class Sniper < Formula
+  desc "osx command line app terminator"
+  homepage "https://github.com/toshi0383/sniper"
+  url "https://github.com/toshi0383/sniper.git", :tag => "1.0.0", :revision => "8a655740145b0f9846b02eadfd491b08ef287e57"
+  head "https://github.com/toshi0383/sniper.git"
+
+end
