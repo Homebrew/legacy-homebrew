1433996dda2f5294026c376885abc35e30923835
Miguel Araújo <miguelarauj1o@gmail.com>
2016-03-26 14:56:00 -0300

    memcache-top: remove redundant version statement

--- Library/Formula/memcache-top.rb
+++ Library/Formula/memcache-top.rb
@@ -2,7 +2,6 @@ class MemcacheTop < Formula
   desc "Grab real-time stats from memcache"
   homepage "https://code.google.com/p/memcache-top/"
   url "https://memcache-top.googlecode.com/files/memcache-top-v0.6"
-  version "0.6"
   sha256 "d5f896a9e46a92988b782e340416312cc480261ce8a5818db45ccd0da8a0f22a"
 
   bottle :unneeded
