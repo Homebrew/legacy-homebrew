681419a2c4f741cf26a73d9028ff975fad1a8046
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-26 00:51:53 +0000

    nacl: build tweaks
    Closes #50429.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- Library/Formula/nacl.rb
+++ Library/Formula/nacl.rb
@@ -14,6 +14,16 @@ class Nacl < Formula
   end
 
   def install
+    # Print the build to stdout rather than the default logfile.
+    # Logfile makes it hard to debug and spot hangs. Applied by Debian:
+    # https://sources.debian.net/src/nacl/20110221-4.1/debian/patches/output-while-building/
+    # Also, like Debian, inreplace the hostname because it isn't used outside
+    # build process and adds an unpredictable factor.
+    inreplace "do" do |s|
+      s.gsub! 'exec >"$top/log"', 'exec | tee "$top/log"'
+      s.gsub! /^shorthostname=`.*$/, "shorthostname=brew"
+    end
+
     system "./do" # This takes a while since it builds *everything*
 
     # NaCL has an odd compilation model (software by djb, who'da thunk it?)
@@ -26,17 +36,16 @@ class Nacl < Formula
     # It also builds both x86 and x86_64 copies if your compiler can
     # handle it. Here we only install one copy, based on if you're a
     # 64bit system or not. A --universal could come later though I guess.
-    archstr  = Hardware.is_64_bit? ? "amd64" : "x86"
-    hoststr  = `hostname | sed 's/\\..*//' | tr -cd '[a-z][A-Z][0-9]'`.strip
+    archstr = Hardware.is_64_bit? ? "amd64" : "x86"
 
     # Don't include cpucycles.h
-    include.install Dir["build/#{hoststr}/include/#{archstr}/crypto_*.h"]
-    include.install "build/#{hoststr}/include/#{archstr}/randombytes.h"
+    include.install Dir["build/brew/include/#{archstr}/crypto_*.h"]
+    include.install "build/brew/include/#{archstr}/randombytes.h"
 
     # Add randombytes.o to the libnacl.a archive - I have no idea why it's separated,
     # but plenty of the key generation routines depend on it. Users shouldn't have to
     # know this.
-    nacl_libdir = "build/#{hoststr}/lib/#{archstr}"
+    nacl_libdir = "build/brew/lib/#{archstr}"
     system "ar", "-r", "#{nacl_libdir}/libnacl.a", "#{nacl_libdir}/randombytes.o"
     lib.install "#{nacl_libdir}/libnacl.a"
   end
