4959a80d847a9930ebf8a0b31de277288d6e1970
Alex Gaynor <alex.gaynor@gmail.com>
2016-03-27 12:59:46 -0400

    afl-fuzz 2.09b
    Closes #50469.
    
    Signed-off-by: Alex Dunn <adunn@ucsb.edu>

--- Library/Formula/afl-fuzz.rb
+++ Library/Formula/afl-fuzz.rb
@@ -1,8 +1,8 @@
 class AflFuzz < Formula
   desc "American fuzzy lop: Security-oriented fuzzer"
   homepage "http://lcamtuf.coredump.cx/afl/"
-  url "http://lcamtuf.coredump.cx/afl/releases/afl-2.08b.tgz"
-  sha256 "268acb69ccdcfa07d72e33ea203818a01e81e84b423b4f0bfb56f8e04a358f88"
+  url "http://lcamtuf.coredump.cx/afl/releases/afl-2.09b.tgz"
+  sha256 "f3de3717006f1204ad552a86546a84030a55bb0f17a2d78aadda73798cdb5eeb"
 
   bottle do
     sha256 "5921bb76782205c457c737d94fb98b3ff1b6b2c5705e4121b13cd53e1b43d303" => :el_capitan
