8a26671c16096d8b6b8bc51f76a3b21643cbd4af
Miguel Araújo <miguelarauj1o@gmail.com>
2016-03-27 09:50:54 -0300

    gdub: update description
    Closes #50461.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- Library/Formula/gdub.rb
+++ Library/Formula/gdub.rb
@@ -1,5 +1,5 @@
 class Gdub < Formula
-  desc "Gdub is a Gradle wrapper wrapper"
+  desc "A gradlew/gradle wrapper."
   homepage "http://www.gdub.rocks"
   url "https://github.com/dougborg/gdub/archive/v0.1.0.tar.gz"
   sha256 "ddf2572cc67b8df3293b1707720c6ef09d6caf73227fa869a73b16239df959c3"
