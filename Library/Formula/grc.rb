0eba0fa9370d2605e95d515821f5c791013dd699
Dave Wikoff <dgsims@gmail.com>
2016-03-27 18:59:41 -0400

    grc: support head and tests

--- Library/Formula/grc.rb
+++ Library/Formula/grc.rb
@@ -4,6 +4,8 @@ class Grc < Formula
   url "http://korpus.juls.savba.sk/~garabik/software/grc/grc_1.9.orig.tar.gz"
   sha256 "41626e571ca255e1a9fe0816f3c0dfd1a30d9564d0decaf4b7365e28e3c54f5b"
 
+  head "https://github.com/garabik/grc.git"
+
   bottle :unneeded
 
   conflicts_with "cc65", :because => "both install `grc` binaries"
@@ -17,39 +19,24 @@ class Grc < Formula
     (share+"grc").install Dir["conf.*"]
     man1.install %w[grc.1 grcat.1]
 
-    (prefix+"etc/grc.bashrc").write rc_script
-  end
-
-  def rc_script; <<-EOS.undent
-    GRC=`which grc`
-    if [ "$TERM" != dumb ] && [ -n "$GRC" ]
-    then
-        alias colourify="$GRC -es --colour=auto"
-        alias configure='colourify ./configure'
-        alias diff='colourify diff'
-        alias make='colourify make'
-        alias gcc='colourify gcc'
-        alias g++='colourify g++'
-        alias as='colourify as'
-        alias gas='colourify gas'
-        alias ld='colourify ld'
-        alias netstat='colourify netstat'
-        alias ping='colourify ping'
-        alias traceroute='colourify /usr/sbin/traceroute'
-        alias head='colourify head'
-        alias tail='colourify tail'
-        alias dig='colourify dig'
-        alias mount='colourify mount'
-        alias ps='colourify ps'
-        alias mtr='colourify mtr'
-        alias df='colourify df'
-    fi
-    EOS
+    etc.install "grc.bashrc"
+    etc.install "grc.zsh" if build.head?
   end
 
   def caveats; <<-EOS.undent
     New shell sessions will start using GRC after you add this to your profile:
       source "`brew --prefix`/etc/grc.bashrc"
     EOS
+  end
+
+  test do
+    require "open3"
+
+    # Test the 'ls' config
+    Open3.popen3("grcat", (share+"grc/conf.ls")) do |stdin, stdout, _|
+      stdin.write("root")
+      stdin.close
+      assert_equal "\e[0m\e[1m\e[31mroot\e[0m\n", stdout.read
+    end
   end
 end
