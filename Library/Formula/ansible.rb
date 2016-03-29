5ee88664c20678d911388e54ca1ec620aeeb891d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-03-29 21:19:23 +0100

    ansible: update 2.0.1.0 bottle.

--- Library/Formula/ansible.rb
+++ Library/Formula/ansible.rb
@@ -7,10 +7,10 @@ class Ansible < Formula
   head "https://github.com/ansible/ansible.git", :branch => "devel"
 
   bottle do
-    revision 1
-    sha256 "be446d21c04beaf79f24b9ee2b2cc8649d07f6997147e54517df6cda43afbaec" => :el_capitan
-    sha256 "e9dfd9b936aa1fde89b88de26dedeffd99870a4e194e4582319136751a39110d" => :yosemite
-    sha256 "a89fd9ab292556da3efaf609562350aec2408c700d1c08f80543ac63b0550811" => :mavericks
+    revision 2
+    sha256 "457a6bcbe29bde7543544dbd44d702e9d6ee51135dc59687cd923f8737a3d31d" => :el_capitan
+    sha256 "339d9bc27942b45d7322f6960422718523a92dbd167869bb3f5cb80d54075369" => :yosemite
+    sha256 "d0684bc344e8bfee3ecdc79316c24b57cf06c1417cc84a79643c10d81ea74d96" => :mavericks
   end
 
   devel do
