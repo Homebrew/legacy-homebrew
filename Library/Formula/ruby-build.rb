124cb949f7bdb720fea3048e9bbcc3c9b6d9ae47
Erik Michaels-Ober <sferik@gmail.com>
2016-03-30 11:35:50 -0700

    ruby-build 20160330

--- Library/Formula/ruby-build.rb
+++ Library/Formula/ruby-build.rb
@@ -1,8 +1,8 @@
 class RubyBuild < Formula
   desc "Install various Ruby versions and implementations"
   homepage "https://github.com/rbenv/ruby-build"
-  url "https://github.com/rbenv/ruby-build/archive/v20160228.tar.gz"
-  sha256 "ee819bfc7f7cfff41e077c05ba8e7e9e813c8c7f1e4af79be8775df3d41c0685"
+  url "https://github.com/rbenv/ruby-build/archive/v20160330.tar.gz"
+  sha256 "e6cbc01f6db45675927a4a75f3d52cd94f6f397f63dc6ddd7643a0c23c46bcab"
 
   head "https://github.com/rbenv/ruby-build.git"
 
