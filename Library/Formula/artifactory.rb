65fcb226fb2ccb484cc6d66be13d167512462c2f
Eli Givoni <elig@jfrog.org>
2016-03-31 21:31:18 +0300

    New version 4.7.0

--- Library/Formula/artifactory.rb
+++ Library/Formula/artifactory.rb
@@ -1,8 +1,8 @@
 class Artifactory < Formula
   desc "Manages binaries"
   homepage "https://www.jfrog.com/artifactory/"
-  url "https://dl.bintray.com/jfrog/artifactory/jfrog-artifactory-oss-4.6.1.zip"
-  sha256 "a4cf127698a4fc455e2a186bb95cb607f5cb7681cfb4d693dd2f5aee28666ce0"
+  url "https://dl.bintray.com/jfrog/artifactory/jfrog-artifactory-oss-4.7.0.zip"
+  sha256 "26cb86d9fdd39b471b7c80451b8afe0cd36e8e527ead76c49cc61ff774c1deb6"
 
   bottle :unneeded
 
