4787f7ddc25f735d7d94dbde9de5ca842cdb6cc3
Kyungdahm Yun <tomyun@gmail.com>
2016-03-31 17:51:07 -0700

    libagar 1.5.0
    Closes #50589.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- Library/Formula/libagar.rb
+++ Library/Formula/libagar.rb
@@ -1,8 +1,8 @@
 class Libagar < Formula
   desc "Cross-platform GUI toolkit"
   homepage "http://libagar.org/"
-  url "http://stable.hypertriton.com/agar/agar-1.4.1.tar.gz"
-  sha256 "b0e62b754f134c3c0dd070a4fa62fa552654356eebab3d55e32d5d9b151a275e"
+  url "http://stable.hypertriton.com/agar/agar-1.5.0.tar.gz"
+  sha256 "82342ded342c578141984befe9318f3d376176e5f427ae3278f8985f26663c00"
   head "http://dev.csoft.net/agar/trunk", :using => :svn
 
   bottle do
