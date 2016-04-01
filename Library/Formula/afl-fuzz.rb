949dfd76ec035b890ad9bbbbb40d3aeb7a2535bb
Alex Gaynor <alex.gaynor@gmail.com>
2016-03-31 22:36:01 -0400

    afl-fuzz 2.10b

--- Library/Formula/afl-fuzz.rb
+++ Library/Formula/afl-fuzz.rb
@@ -1,8 +1,8 @@
 class AflFuzz < Formula
   desc "American fuzzy lop: Security-oriented fuzzer"
   homepage "http://lcamtuf.coredump.cx/afl/"
-  url "http://lcamtuf.coredump.cx/afl/releases/afl-2.09b.tgz"
-  sha256 "f3de3717006f1204ad552a86546a84030a55bb0f17a2d78aadda73798cdb5eeb"
+  url "http://lcamtuf.coredump.cx/afl/releases/afl-2.10b.tgz"
+  sha256 "8141291646024dcdd2e67c51fb68b4205d34c52e4b5693c88f6ce4a6bf1ebfe3"
 
   bottle do
     sha256 "9f62b6c4a45272290e8bbcfcc59cea572c31c550f65e937379e914f662bf1ce6" => :el_capitan
