c270ce6a382abd2bc5c45d70fb828593f792738b
G-Square <G-Square@users.noreply.github.com>
2016-04-01 00:07:00 -0400

    update megatools version to 1.9.97

--- Library/Formula/megatools.rb
+++ Library/Formula/megatools.rb
@@ -1,14 +1,11 @@
 class Megatools < Formula
   desc "Command-line client for Mega.co.nz"
   homepage "https://megatools.megous.com/"
-  url "https://megatools.megous.com/builds/megatools-1.9.96.tar.gz"
-  sha256 "c657a0988223aada021a7a6ed408227fc66435de7248f23952ad7adf9e1242f0"
+  url "https://megatools.megous.com/builds/megatools-1.9.97.tar.gz"
+  sha256 "3316c56ee55adef0ab113aad41ef95eb28fc15218639f69b1e04362b1c725af3"
 
   bottle do
     cellar :any
-    sha256 "2e6e2bfa2b126d77b19bcf6338b3081f2ec0bef5d7eefbd5e258dcf077c70591" => :el_capitan
-    sha256 "63735bfe9e6c5a3a29e9b2902d4cf8f954383a3d0091b94cec03b4c47e023df1" => :yosemite
-    sha256 "2025daddcac02c741c6cd60d89e91787fde3ab54955bdebc34961edc349b2c90" => :mavericks
   end
 
   depends_on "pkg-config" => :build
